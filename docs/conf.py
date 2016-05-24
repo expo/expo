0442f7dbef7e8dba9c678c4db4cd760c31b9fd3a
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-05-24 11:19:07 -0700

    [docs] Use sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
