df9ca774c1f4e52440e36d111aec6f1080d342ed
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-05-31 19:14:21 -0700

    [docs] Write some SDK docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,11 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+# -- Exponent configuration -----------------------------------------------
+
+primary_domain = 'js'
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
