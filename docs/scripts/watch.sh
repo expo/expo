da3aef200d704fae292c9a55794c340d2f4ae0a5
Adam Miskiewicz <adam@exponentjs.com>
2016-06-17 12:34:05 -0700

    [docs] use "gcloud docker" for docker commands in scripts.

--- docs/scripts/watch.sh
+++ docs/scripts/watch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -ti --rm -v `pwd`:/root/docs -p 8000:8000 gcr.io/exponentjs/docs-builder make serve
+gcloud docker run -ti --rm -v `pwd`:/root/docs -p 8000:8000 gcr.io/exponentjs/docs-builder make serve
