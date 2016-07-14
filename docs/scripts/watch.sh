84753b840f84bd201f2dd1208094cf4cb0941f7b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-07-13 17:18:27 -0700

    [docs] Allow specifying version for `watch.sh`

--- docs/scripts/watch.sh
+++ docs/scripts/watch.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-gcloud docker -- run -ti --rm -v `pwd`:/root/docs -p 8000:8000 gcr.io/exponentjs/docs-builder make serve
+if [ -n $1 ]; then
+    VERSION_ARG="-e DEFAULT_VERSION=$1"
+else
+    VERSION_ARG=
+fi
+
+gcloud docker -- run -ti $VERSION_ARG --rm -v `pwd`:/root/docs -p 8000:8000 gcr.io/exponentjs/docs-builder make serve
