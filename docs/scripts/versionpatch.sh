248ef6bccf4732a170e158a4a7c85fc16e9a968b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-08-03 17:27:20 -0700

    [docs] Usage message in versionpatch.sh

--- docs/scripts/versionpatch.sh
+++ docs/scripts/versionpatch.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [[ $# -lt 2 ]]
+then
+    echo "applies the current git diff in source version in each target version"
+    echo "can be used to, for example, make changes in "'unversioned'" then"
+    echo "duplicate those changes in previous versions"
+    echo
+    echo "usage: from docs directory,"
+    echo "  ./scripts/versionpatch.sh <src> <target1> <target2> ..."
+    echo
+    echo "example:"
+    echo "  ./scripts/versionpatch.sh unversioned v8.0.0 v7.0.0"
+    exit
+fi
+
 src_version=$1
 
 shift
