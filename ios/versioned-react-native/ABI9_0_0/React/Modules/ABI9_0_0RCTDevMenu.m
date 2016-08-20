5f74224962bcdd4ee66bc644c06ed6c930e3047b
Ben Roth <spongebucket@gmail.com>
2016-08-20 00:48:25 +0000

    Actually use react-native#sdk-9 in SDK 9
    fbshipit-source-id: 0acbefa

--- ios/versioned-react-native/ABI9_0_0/React/Modules/ABI9_0_0RCTDevMenu.m
+++ ios/versioned-react-native/ABI9_0_0/React/Modules/ABI9_0_0RCTDevMenu.m
@@ -140,7 +140,7 @@ ABI9_0_0RCT_NOT_IMPLEMENTED(- (instancetype)init)
 
 @synthesize bridge = _bridge;
 
-ABI9_0_0RCT_EXPORT_MODULE()
++ (NSString *)moduleName { return @"RCTDevMenu"; }
 
 + (void)initialize
 {
