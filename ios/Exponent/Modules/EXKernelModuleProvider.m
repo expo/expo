ea4402ccd37cf00dcd2cacb6212d265b5e05bb40
Ben Roth <spongebucket@gmail.com>
2016-09-07 20:53:56 +0000

    Use NSUserDefaults initial kernel url if provided
    fbshipit-source-id: c89c5e0

--- ios/Exponent/Modules/EXKernelModuleProvider.m
+++ ios/Exponent/Modules/EXKernelModuleProvider.m
@@ -8,12 +8,26 @@
 #import "EXKernelModuleProvider.h"
 #import "EXLinkingManager.h"
 
+NSString * const kEXKernelLaunchUrlDefaultsKey = @"EXKernelLaunchUrlDefaultsKey";
+
 NSArray<id<RCTBridgeModule>> *(^EXKernelModuleProvider)(NSDictionary *) = ^NSArray<id<RCTBridgeModule>> *(NSDictionary *launchOptions) {
+  NSURL *initialKernelUrl;
+
+  // used by appetize - override the kernel initial url if there's something in NSUserDefaults
+  NSString *kernelInitialUrlDefaultsValue = [[NSUserDefaults standardUserDefaults] stringForKey:kEXKernelLaunchUrlDefaultsKey];
+  if (kernelInitialUrlDefaultsValue) {
+    initialKernelUrl = [NSURL URLWithString:kernelInitialUrlDefaultsValue];
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kEXKernelLaunchUrlDefaultsKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+  } else {
+    initialKernelUrl = [EXKernel initialUrlFromLaunchOptions:launchOptions];
+  }
+
   // Keep these in sync with EXVersionManager::extraModulesWithParams: as needed
   EXDisabledDevMenu *disabledDevMenu = [[EXDisabledDevMenu alloc] init];
   EXKernelModule *kernel = [[EXKernelModule alloc] init];
   kernel.delegate = [EXKernel sharedInstance];
-  EXLinkingManager *linkingManager = [[EXLinkingManager alloc] initWithInitialUrl:[EXKernel initialUrlFromLaunchOptions:launchOptions]];
+  EXLinkingManager *linkingManager = [[EXLinkingManager alloc] initWithInitialUrl:initialKernelUrl];
   EXConstants *constants = [[EXConstants alloc] initWithProperties:@{ @"deviceId": [EXKernel deviceInstallUUID] }];
   
   NSMutableArray *modules = [@[disabledDevMenu, kernel, linkingManager, constants] mutableCopy];
