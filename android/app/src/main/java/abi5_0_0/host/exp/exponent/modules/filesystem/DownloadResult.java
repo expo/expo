621cd1ca4d6c6db94c06fc513e93822f6d4ad2f8
Jesse Ruder <jesse@exp.host>
2016-09-09 20:11:15 +0000

    Kill SDK 5
    fbshipit-source-id: 77dc970

--- android/app/src/main/java/abi5_0_0/host/exp/exponent/modules/filesystem/DownloadResult.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
-
-package abi5_0_0.host.exp.exponent.modules.filesystem;
-
-public class DownloadResult {
-  public int statusCode;
-  public int bytesWritten;
-  public Exception exception;
-}
