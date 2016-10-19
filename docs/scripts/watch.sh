e541d9341b3cb06876be7b114617ad02911b941a
Adam Miskiewicz <adam@exponentjs.com>
2016-10-18 17:26:49 -0700

    [docs] Make docs docker image use universe as root

--- docs/scripts/watch.sh
+++ docs/scripts/watch.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-if [ -n $1 ]; then
+if [ -n "$1" ]; then
     VERSION_ARG="-e DEFAULT_VERSION=$1"
 else
     VERSION_ARG=
 fi
 
-gcloud docker -- run -ti $VERSION_ARG --rm -v `pwd`:/root/docs -p 8000:8000 gcr.io/exponentjs/docs-builder make serve
+gcloud docker -- run \
+    -ti $VERSION_ARG \
+    --rm \
+    -v "`pwd`/..:/root/universe" \
+    -w /root/universe/docs \
+    -p 8000:8000 \
+    gcr.io/exponentjs/docs-builder \
+    make serve
