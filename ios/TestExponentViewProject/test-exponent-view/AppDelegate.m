0a7c7c044f2a735acc038ce7ec4ce13d3da3bcea
Ben Roth <spongebucket@gmail.com>
2016-10-21 00:25:48 +0000

    Configure TestExponentViewProject to actually use exponent view
    fbshipit-source-id: 1d4cacc

--- ios/TestExponentViewProject/test-exponent-view/AppDelegate.m
+++ ios/TestExponentViewProject/test-exponent-view/AppDelegate.m
@@ -1,15 +1,26 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 
 #import "AppDelegate.h"
+#import "EXViewController.h"
 
 @interface AppDelegate ()
+
+@property (nonatomic, strong) EXViewController *rootViewController;
 
 @end
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    _window.backgroundColor = [UIColor whiteColor];
+    _rootViewController = [[EXViewController alloc] initWithLaunchOptions:@{}];
+    _window.rootViewController = _rootViewController;
+    
+    [_rootViewController loadReactApplication];
+    [_window makeKeyAndVisible];
+    
     return YES;
 }
 
