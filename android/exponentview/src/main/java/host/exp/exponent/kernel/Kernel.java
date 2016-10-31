f76976832177f288f1bbbc63dd5ebdb7050197e9
James Ide <ide@exp.host>
2016-10-31 08:10:48 +0000

    Update ExponentView code to reflect RN Android package name that changed
    fbshipit-source-id: 96bcdcd

--- android/exponentview/src/main/java/host/exp/exponent/kernel/Kernel.java
+++ android/exponentview/src/main/java/host/exp/exponent/kernel/Kernel.java
@@ -17,12 +17,12 @@ import android.util.Log;
 
 import com.facebook.common.internal.ByteStreams;
 import com.facebook.proguard.annotations.DoNotStrip;
-import com.facebook.react.LifecycleState;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.LifecycleState;
 import com.facebook.react.modules.network.OkHttpClientProvider;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
--- android/exponentview/src/main/java/versioned/host/exp/exponent/VersionedUtils.java
+++ android/exponentview/src/main/java/versioned/host/exp/exponent/VersionedUtils.java
@@ -2,8 +2,8 @@
 
 package versioned.host.exp.exponent;
 
-import com.facebook.react.LifecycleState;
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.common.LifecycleState;
 import com.facebook.react.shell.MainReactPackage;
 
 import host.exp.exponent.experience.ExperienceActivity;
