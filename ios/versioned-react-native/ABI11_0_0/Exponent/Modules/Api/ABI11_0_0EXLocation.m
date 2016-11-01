9daa092283a2cbce15f90ee97acabc033fbd8c48
Ben Roth <spongebucket@gmail.com>
2016-11-01 21:23:26 +0000

    Patch 'Alloc and manage CLLocationManager on main thread' to SDK 11
    fbshipit-source-id: b5eb270

--- ios/versioned-react-native/ABI11_0_0/Exponent/Modules/Api/ABI11_0_0EXLocation.m
+++ ios/versioned-react-native/ABI11_0_0/Exponent/Modules/Api/ABI11_0_0EXLocation.m
@@ -103,48 +103,50 @@ ABI11_0_0RCT_REMAP_METHOD(watchPositionImplAsync,
     reject(@"E_LOCATION_SERVICES_DISABLED", @"Location services are disabled", nil);
     return;
   }
-
-  CLLocationManager *locMgr = [[CLLocationManager alloc] init];
   if (!([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedAlways || [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse)) {
     reject(@"E_LOCATION_UNAUTHORIZED", @"Not authorized to use location services", nil);
     return;
   }
-
-  locMgr.distanceFilter = options[@"distanceInterval"] ? [ABI11_0_0RCTConvert double:options[@"distanceInterval"]] ?: kCLDistanceFilterNone : kCLLocationAccuracyHundredMeters;
-  locMgr.desiredAccuracy = [ABI11_0_0RCTConvert BOOL:options[@"enableHighAccuracy"]] ? kCLLocationAccuracyBest : kCLLocationAccuracyHundredMeters;
-
+  
   __weak typeof(self) weakSelf = self;
-  ABI11_0_0EXLocationDelegate *delegate = [[ABI11_0_0EXLocationDelegate alloc] initWithId:watchId withLocMgr:locMgr onUpdateLocations:^(NSArray<CLLocation *> *locations) {
-    if (locations.lastObject) {
-      CLLocation *loc = locations.lastObject;
-      NSDictionary *body = @{
-        @"watchId": watchId,
-        @"location": @{
-          @"coords": @{
-              @"latitude": @(loc.coordinate.latitude),
-              @"longitude": @(loc.coordinate.longitude),
-              @"altitude": @(loc.altitude),
-              @"accuracy": @(loc.horizontalAccuracy),
-              @"altitudeAccuracy": @(loc.verticalAccuracy),
-              @"heading": @(loc.course),
-              @"speed": @(loc.speed),
-              },
-          @"timestamp": @([loc.timestamp timeIntervalSince1970] * 1000),
-        },
-      };
-      [weakSelf sendEventWithName:ABI11_0_0EXLocationChangedEventName body:body];
-    }
-  } onError:^(NSError *error) {
-    // TODO: report errors
-    // (ben) error could be (among other things):
-    //   - kCLErrorDenied - we should use the same UNAUTHORIZED behavior as elsewhere
-    //   - kCLErrorLocationUnknown - we can actually ignore this error and keep tracking
-    //     location (I think -- my knowledge might be a few months out of date)
-  }];
-  _delegates[delegate.watchId] = delegate;
-  locMgr.delegate = delegate;
-  [locMgr startUpdatingLocation];
-  resolve(nil);
+  dispatch_async(dispatch_get_main_queue(), ^{
+    CLLocationManager *locMgr = [[CLLocationManager alloc] init];
+    
+    locMgr.distanceFilter = options[@"distanceInterval"] ? [ABI11_0_0RCTConvert double:options[@"distanceInterval"]] ?: kCLDistanceFilterNone : kCLLocationAccuracyHundredMeters;
+    locMgr.desiredAccuracy = [ABI11_0_0RCTConvert BOOL:options[@"enableHighAccuracy"]] ? kCLLocationAccuracyBest : kCLLocationAccuracyHundredMeters;
+    
+    ABI11_0_0EXLocationDelegate *delegate = [[ABI11_0_0EXLocationDelegate alloc] initWithId:watchId withLocMgr:locMgr onUpdateLocations:^(NSArray<CLLocation *> *locations) {
+      if (locations.lastObject) {
+        CLLocation *loc = locations.lastObject;
+        NSDictionary *body = @{
+                               @"watchId": watchId,
+                               @"location": @{
+                                   @"coords": @{
+                                       @"latitude": @(loc.coordinate.latitude),
+                                       @"longitude": @(loc.coordinate.longitude),
+                                       @"altitude": @(loc.altitude),
+                                       @"accuracy": @(loc.horizontalAccuracy),
+                                       @"altitudeAccuracy": @(loc.verticalAccuracy),
+                                       @"heading": @(loc.course),
+                                       @"speed": @(loc.speed),
+                                       },
+                                   @"timestamp": @([loc.timestamp timeIntervalSince1970] * 1000),
+                                   },
+                               };
+        [weakSelf sendEventWithName:ABI11_0_0EXLocationChangedEventName body:body];
+      }
+    } onError:^(NSError *error) {
+      // TODO: report errors
+      // (ben) error could be (among other things):
+      //   - kCLErrorDenied - we should use the same UNAUTHORIZED behavior as elsewhere
+      //   - kCLErrorLocationUnknown - we can actually ignore this error and keep tracking
+      //     location (I think -- my knowledge might be a few months out of date)
+    }];
+    weakSelf.delegates[delegate.watchId] = delegate;
+    locMgr.delegate = delegate;
+    [locMgr startUpdatingLocation];
+    resolve(nil);
+  });
 }
 
 ABI11_0_0RCT_REMAP_METHOD(removeWatchAsync,
@@ -154,9 +156,15 @@ ABI11_0_0RCT_REMAP_METHOD(removeWatchAsync,
 {
   ABI11_0_0EXLocationDelegate *delegate = _delegates[watchId];
   if (delegate) {
-    [delegate.locMgr stopUpdatingLocation];
-    delegate.locMgr.delegate = nil;
-    [_delegates removeObjectForKey:watchId];
+    __weak typeof(self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+      [delegate.locMgr stopUpdatingLocation];
+      delegate.locMgr.delegate = nil;
+      __strong typeof(self) strongSelf = weakSelf;
+      if (strongSelf) {
+        [strongSelf.delegates removeObjectForKey:watchId];
+      }
+    });
   }
 }
 
