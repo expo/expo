dc27fb755dff0c65088dec3f080a73a1ec445858
Ben Roth <spongebucket@gmail.com>
2016-11-09 23:20:33 +0000

    Prepare `exponent-view-template/ios` for automation
    fbshipit-source-id: 35f9757

---
+++ exponent-view-template/ios/exponent-view-template/AppDelegate.m
@@ -0,0 +1,58 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#import "AppDelegate.h"
+#import "ExponentViewManager.h"
+#import "EXViewController.h"
+
+@interface AppDelegate ()
+
+@property (nonatomic, strong) EXViewController *rootViewController;
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    _window.backgroundColor = [UIColor whiteColor];
+    [[ExponentViewManager sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    _rootViewController = [ExponentViewManager sharedInstance].rootViewController;
+    _window.rootViewController = _rootViewController;
+    
+    [_rootViewController loadReactApplication];
+    [_window makeKeyAndVisible];
+    
+    return YES;
+}
+
+#pragma mark - Handling URLs
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(id)annotation
+{
+    return [[ExponentViewManager sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray * _Nullable))restorationHandler
+{
+    return [[ExponentViewManager sharedInstance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+}
+
+#pragma mark - Notifications
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)token
+{
+    [[ExponentViewManager sharedInstance] application:application didRegisterForRemoteNotificationsWithDeviceToken:token];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)err
+{
+    [[ExponentViewManager sharedInstance] application:application didFailToRegisterForRemoteNotificationsWithError:err];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
+{
+    [[ExponentViewManager sharedInstance] application:application didReceiveRemoteNotification:notification];
+}
+
+@end
