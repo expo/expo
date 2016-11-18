e1f46e33229305cd4d63da5813af9233ce6a9a5f
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-11-18 18:24:31 +0530

    Install gulp-cli locally

--- android/run.sh
+++ android/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew ${1:-installDevDebug} && adb shell am start -n host.exp.exponent/host.exp.exponent.LauncherActivity
+./gradlew ${1:-installDevDebug} --stacktrace && adb shell am start -n host.exp.exponent/host.exp.exponent.LauncherActivity
