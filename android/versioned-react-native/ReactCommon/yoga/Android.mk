b3fe6dac97de70e6eb8444ac16773bdc1c63fd77
Jesse Ruder <jesse@getexponent.com>
2017-01-11 19:48:02 +0000

    Add yoga to packages-to-rename
    fbshipit-source-id: 48650ca

--- android/versioned-react-native/ReactCommon/yoga/Android.mk
+++ android/versioned-react-native/ReactCommon/yoga/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := yogacore
 
 LOCAL_SRC_FILES := \
@@ -9,8 +9,8 @@ LOCAL_SRC_FILES := \
   yoga/YGNodeList.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
-
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+
 LOCAL_CFLAGS := -Wall -Werror -fexceptions -frtti -std=c99 -O3
 
 include $(BUILD_STATIC_LIBRARY)
