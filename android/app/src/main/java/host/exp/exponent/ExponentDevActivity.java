62c7f01b5cbaf6b067af5cf6da19c80dec44b99a
Jesse Ruder <jesse@getexponent.com>
2017-01-24 02:37:41 +0000

    Use exp kernel
    fbshipit-source-id: 1a3539d

--- android/app/src/main/java/host/exp/exponent/ExponentDevActivity.java
+++ android/app/src/main/java/host/exp/exponent/ExponentDevActivity.java
@@ -46,29 +46,8 @@ public class ExponentDevActivity extends AppCompatActivity {
     mLinearLayout = (LinearLayout) findViewById(R.id.linearLayout);
     NativeModuleDepsProvider.getInstance().inject(ExponentDevActivity.class, this);
 
-    addCheckbox("Debug Mode Enabled" + (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ?
-            " (must enable \"Draw over other apps\" in Settings)" : ""),
-        ExponentSharedPreferences.DEBUG_MODE_KEY, debugModeChangeListener());
     addCheckbox("Use Internet Kernel", ExponentSharedPreferences.USE_INTERNET_KERNEL_KEY, null);
-    addCheckbox("Kernel Debug Mode Enabled", ExponentSharedPreferences.KERNEL_DEBUG_MODE_KEY,
-        debugModeChangeListener());
-    addKernelUrlInput();
     addReloadKernelButton();
-  }
-
-  private CompoundButton.OnCheckedChangeListener debugModeChangeListener() {
-    return new CompoundButton.OnCheckedChangeListener() {
-      @Override
-      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-          if (isChecked && !Settings.canDrawOverlays(ExponentDevActivity.this)) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:" + getPackageName()));
-            startActivity(intent);
-          }
-        }
-      }
-    };
   }
 
   private void addCheckbox(final String text, final String key, final CompoundButton.OnCheckedChangeListener listener) {
@@ -96,71 +75,6 @@ public class ExponentDevActivity extends AppCompatActivity {
     });
 
     mLinearLayout.addView(checkBox);
-  }
-
-  private void addKernelUrlInput() {
-    TextView kernelUrlLabel = new TextView(this);
-    kernelUrlLabel.setText("Local kernel URL:");
-    kernelUrlLabel.setTextColor(Color.WHITE);
-    mLinearLayout.addView(kernelUrlLabel);
-
-    final EditText kernelUrl = new EditText(this);
-    kernelUrl.setTextColor(Color.WHITE);
-    kernelUrl.setText(mExponentSharedPreferences.getString(ExponentSharedPreferences.LOCAL_KERNEL_URL_KEY,
-        Kernel.defaultLocalKernelUrl()));
-    kernelUrl.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-      }
-
-      @Override
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-      }
-
-      @Override
-      public void afterTextChanged(Editable s) {
-        if (s.toString().equals(Kernel.defaultLocalKernelUrl())) {
-          mExponentSharedPreferences.delete(ExponentSharedPreferences.LOCAL_KERNEL_URL_KEY);
-        } else {
-          mExponentSharedPreferences.setString(ExponentSharedPreferences.LOCAL_KERNEL_URL_KEY, s.toString());
-        }
-      }
-    });
-
-    mLinearLayout.addView(kernelUrl);
-
-    Button clearButton = (Button) getLayoutInflater().inflate(R.layout.exponent_button, null);
-    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-        LinearLayout.LayoutParams.WRAP_CONTENT,
-        LinearLayout.LayoutParams.WRAP_CONTENT);
-    params.setMargins(0, 0, 0, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20,
-        getResources().getDisplayMetrics()));
-    clearButton.setLayoutParams(params);
-
-    clearButton.setText("Clear");
-    clearButton.setOnClickListener(new Button.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        kernelUrl.setText("");
-      }
-    });
-
-    mLinearLayout.addView(clearButton);
-
-    Button resetButton = (Button) getLayoutInflater().inflate(R.layout.exponent_button, null);
-    resetButton.setLayoutParams(params);
-
-    resetButton.setText("Reset local kernel URL");
-    resetButton.setOnClickListener(new Button.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        kernelUrl.setText(Kernel.defaultLocalKernelUrl());
-      }
-    });
-
-    mLinearLayout.addView(resetButton);
   }
 
   private void addReloadKernelButton() {
