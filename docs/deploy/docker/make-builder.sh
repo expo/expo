2a70a5fa0fe17056627a7893f9897862e3bb54cb
Adam Miskiewicz <adam@exponentjs.com>
2017-01-25 16:45:29 -0800

    [docs] Re-enable CI pipeline

--- docs/deploy/docker/make-builder.sh
+++ docs/deploy/docker/make-builder.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-gcloud docker -- build -t gcr.io/exponentjs/docs-builder:latest -f _resources/builder.Dockerfile .
+gcloud docker -- build -t gcr.io/exponentjs/docs-builder:latest -f ./builder.Dockerfile .
