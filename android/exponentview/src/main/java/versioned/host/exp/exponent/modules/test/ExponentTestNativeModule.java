1a6b9dc9dc5808420f2a9e7829b67d11b273978e
Jesse Ruder <jesse@getexponent.com>
2017-02-08 23:13:20 +0000

    Copy bundled manifest and js
    fbshipit-source-id: dc9f6c8

--- android/exponentview/src/main/java/versioned/host/exp/exponent/modules/test/ExponentTestNativeModule.java
+++ android/exponentview/src/main/java/versioned/host/exp/exponent/modules/test/ExponentTestNativeModule.java
@@ -2,7 +2,6 @@
 
 package versioned.host.exp.exponent.modules.test;
 
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
