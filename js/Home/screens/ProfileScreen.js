5bd2b6ef4c99feabcdccc37d0c17cfc3d9bb217f
Brent Vatne <brentvatne@gmail.com>
2017-02-09 01:21:22 +0000

    Add report user option
    fbshipit-source-id: 892422f

--- js/Home/screens/ProfileScreen.js
+++ js/Home/screens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Alert,
   NativeModules,
   Platform,
   StyleSheet,
@@ -9,11 +10,11 @@ import {
   findNodeHandle,
 } from 'react-native';
 import {
+  Ionicons,
   MaterialIcons,
 } from '@exponent/vector-icons';
-import {
-  connect,
-} from 'react-redux';
+import { connect } from 'react-redux';
+import { connectActionSheet } from '@exponent/react-native-action-sheet';
 
 import AuthTokenActions from '../../Flux/AuthTokenActions';
 
@@ -40,19 +41,19 @@ export default class ProfileScreen extends React.Component {
       ...Platform.select({
         ios: {
           renderRight: ({params}) => {
-            if (!params.username) {
-              return (
-                <SignOutButtonIOS />
-              );
+            if (params.username) {
+              return <OptionsButtonIOS />;
+            } else {
+              return <SignOutButtonIOS />;
             }
           },
         },
         android: {
           renderRight: ({params}) => {
-            if (!params.username) {
-              return (
-                <SignOutButtonAndroid />
-              );
+            if (params.username) {
+              return <OptionsButtonAndroid />;
+            } else {
+              return <SignOutButtonAndroid />;
             }
           },
         },
@@ -111,6 +112,37 @@ class SignOutButtonAndroid extends React.Component {
   }
 }
 
+class OptionsButtonAndroid extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View
+          collapsable={false}
+          ref={view => { this._anchor = view; }}
+          style={{position: 'absolute', top: 5, left: 0}}
+        />
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={this._handlePress}>
+          <MaterialIcons name="more-vert" size={27} color="#4E9BDE" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  _handlePress = () => {
+    let handle = findNodeHandle(this._anchor);
+    NativeModules.UIManager.showPopupMenu(handle, ['Report this user'], (err) => {}, (action, selectedIndex) => {
+      if (selectedIndex === 0) {
+        Alert.alert(
+          'Thank you for your report',
+          'We will investigate the case as soon as we can.'
+        );
+      }
+    });
+  }
+}
+
 @onlyIfAuthenticated
 @connect()
 class SignOutButtonIOS extends React.Component {
@@ -118,7 +150,7 @@ class SignOutButtonIOS extends React.Component {
     return (
       <TouchableOpacity style={styles.buttonContainer} onPress={this._handlePress}>
         <Text style={{fontSize: 16, color: '#4E9BDE'}}>
-          Sign out
+          Sign Out
         </Text>
       </TouchableOpacity>
     );
@@ -126,6 +158,36 @@ class SignOutButtonIOS extends React.Component {
 
   _handlePress = () => {
     this.props.dispatch(AuthTokenActions.signOut());
+  }
+}
+
+@connectActionSheet
+class OptionsButtonIOS extends React.Component {
+  render() {
+    return (
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={this._handlePress}>
+        <Ionicons name="ios-more" size={27} color="#4E9BDE" />
+      </TouchableOpacity>
+    );
+  }
+
+  _handlePress = () => {
+    let options = ['Report this user', 'Cancel'];
+    let cancelButtonIndex = 1;
+    this.props.showActionSheetWithOptions({
+      options,
+      cancelButtonIndex,
+    },
+    async (buttonIndex) => {
+      if (buttonIndex === 0) {
+        Alert.alert(
+          'Thank you for your report',
+          'We will investigate the case as soon as we can.'
+        );
+      }
+    });
   }
 }
 
