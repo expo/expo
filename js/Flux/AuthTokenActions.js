2fa47dc44415bb0fb6b20f35ab6a7a2a4ddb0110
Brent Vatne <brentvatne@gmail.com>
2017-02-10 04:37:54 +0000

    Reset store on sign out
    fbshipit-source-id: c3eb4f7

--- js/Flux/AuthTokenActions.js
+++ js/Flux/AuthTokenActions.js
@@ -7,6 +7,7 @@
 
 import { action } from 'Flux';
 import LocalStorage from '../Storage/LocalStorage';
+import ApolloClient from '../Api/ApolloClient';
 
 let AuthTokenActions = {
   @action
@@ -24,6 +25,7 @@ let AuthTokenActions = {
   @action
   signOut() {
     LocalStorage.removeAuthTokensAsync();
+    ApolloClient.resetStore();
     return null;
   },
 };
