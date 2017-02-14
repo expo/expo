9847977e2050440542e031f318d89726d0c9fcdf
Ben Roth <spongebucket@gmail.com>
2017-02-14 21:54:30 +0000

    Remove exp stop from pod install script (untested)
    fbshipit-source-id: 821b6aa

--- exponent-view-template/ios/pod-install-exponent.sh
+++ exponent-view-template/ios/pod-install-exponent.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-pushd ..
-exp stop
-popd
-
 pod install
 
 pushd ..
 # resolve possible packager conflicts introduced by pod install
 exp prepare-detached-build --platform ios --skipXcodeConfig 1
-exp r -c
 popd
+
+echo "Finished installing and cleaning up dependencies. Be sure and serve or restart your project with `exp r -c`"
