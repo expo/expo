ce5841aaf292cc5bc2ac18fd9539e90ffc653987
Adam Miskiewicz <adam@exponentjs.com>
2017-02-16 08:11:40 +0000

    Add .flowconfig
    fbshipit-source-id: d467cd5

--- js/Home/utils/requestCameraPermissionsAsync.js
+++ js/Home/utils/requestCameraPermissionsAsync.js
@@ -2,6 +2,7 @@
 
 import { Permissions } from 'exponent';
 
+// TODO(@skevy): weird formatting here...see https://github.com/jlongster/prettier/issues/700
 export default (async function requestCameraPermissionsAsync() {
   let { status } = await Permissions.askAsync(Permissions.CAMERA);
   return status === 'granted';
