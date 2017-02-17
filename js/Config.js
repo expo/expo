f45ccd1f2fcd8d6013f78a785981779daf4124dd
James Ide <ide@exp.host>
2017-02-17 08:54:39 +0000

    Properly look up existing SNS device entry when figuring out whether to invalidate
    fbshipit-source-id: 225fe9a

--- js/Config.js
+++ js/Config.js
@@ -7,7 +7,7 @@
 
 export default {
   api: {
-    host: 'https://exp.host',
-    // host: 'https://af0c9c12.ngrok.io',
+    // host: 'https://exp.host',
+    host: 'https://ed56a018.ngrok.io',
   },
 };
