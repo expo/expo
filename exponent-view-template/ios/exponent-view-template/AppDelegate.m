cb1d76c681790a60d82b83565b188fc8690227be
Ben Roth <spongebucket@gmail.com>
2017-03-04 02:41:26 +0000

    Update ExpoKit tooling to reflect name change
    fbshipit-source-id: 2c3ed2a

--- exponent-view-template/ios/exponent-view-template/AppDelegate.m
+++ exponent-view-template/ios/exponent-view-template/AppDelegate.m
@@ -1,7 +1,7 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 
 #import "AppDelegate.h"
-#import "ExponentViewManager.h"
+#import "ExpoKit.h"
 #import "EXViewController.h"
 
 @interface AppDelegate ()
@@ -16,8 +16,8 @@
 {
     _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     _window.backgroundColor = [UIColor whiteColor];
-    [[ExponentViewManager sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
-    _rootViewController = [ExponentViewManager sharedInstance].rootViewController;
+    [[ExpoKit sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    _rootViewController = [ExpoKit sharedInstance].rootViewController;
     _window.rootViewController = _rootViewController;
     
     [_rootViewController loadReactApplication];
@@ -30,29 +30,29 @@
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(id)annotation
 {
-    return [[ExponentViewManager sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+    return [[ExpoKit sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity restorationHandler:(nonnull void (^)(NSArray * _Nullable))restorationHandler
 {
-    return [[ExponentViewManager sharedInstance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
+    return [[ExpoKit sharedInstance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
 }
 
 #pragma mark - Notifications
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)token
 {
-    [[ExponentViewManager sharedInstance] application:application didRegisterForRemoteNotificationsWithDeviceToken:token];
+    [[ExpoKit sharedInstance] application:application didRegisterForRemoteNotificationsWithDeviceToken:token];
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)err
 {
-    [[ExponentViewManager sharedInstance] application:application didFailToRegisterForRemoteNotificationsWithError:err];
+    [[ExpoKit sharedInstance] application:application didFailToRegisterForRemoteNotificationsWithError:err];
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
 {
-    [[ExponentViewManager sharedInstance] application:application didReceiveRemoteNotification:notification];
+    [[ExpoKit sharedInstance] application:application didReceiveRemoteNotification:notification];
 }
 
 @end
