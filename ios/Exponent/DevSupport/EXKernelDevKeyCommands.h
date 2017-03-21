3c77317608397b1daba26680ab5debb746c22968
Ben Roth <spongebucket@gmail.com>
2017-03-21 00:12:21 +0000

    Capture KeyCommands in EXKernel
    fbshipit-source-id: 68bd524

---
+++ ios/Exponent/DevSupport/EXKernelDevKeyCommands.h
@@ -0,0 +1,9 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface EXKernelDevKeyCommands : NSObject
+
++ (instancetype)sharedInstance;
+
+@end
