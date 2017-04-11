0a9d24293020083798063a490185bfe287c9c855
Janic Duplessis <janicduplessis@gmail.com>
2017-04-11 03:18:20 +0000

    Fix Contacts test
    fbshipit-source-id: 587758a

--- android/PrepareTestsApp/app/src/main/java/exp/host/preparetestsapp/MainActivity.java
+++ android/PrepareTestsApp/app/src/main/java/exp/host/preparetestsapp/MainActivity.java
@@ -2,7 +2,10 @@ package exp.host.preparetestsapp;
 
 import android.content.ContentProviderOperation;
 import android.content.ContentProviderResult;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        removeAllContacts();
         addContact("JESSE", "TEST", "1234567890", "jessetest@jesse.com");
+        addContact("BEN", "TEST", "1234567891", "bentest@jesse.com");
+        addContact("JAMES", "TEST", "1234567892", "jamestest@jesse.com");
+        addContact("BRENT", "TEST", "1234567894", "brenttest@jesse.com");
     }
 
     private void addContact(final String firstName, final String lastName, final String phoneNumnber, final String email) {
@@ -52,6 +59,23 @@ public class MainActivity extends AppCompatActivity {
             getContentResolver().applyBatch(ContactsContract.AUTHORITY, operationList);
         } catch(Exception e){
             e.printStackTrace();
+        }
+    }
+
+    private void removeAllContacts() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if (cur == null) {
+            return;
+        }
+        try {
+            while (cur.moveToNext()) {
+                String lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+                Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
+                cr.delete(uri, null, null);
+            }
+        } finally {
+            cur.close();
         }
     }
 }
