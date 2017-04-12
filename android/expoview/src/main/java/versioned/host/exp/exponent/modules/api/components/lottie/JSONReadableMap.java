0f4c9d0364b3a13194dd656c020f1471705af5c2
Janic Duplessis <janicduplessis@gmail.com>
2017-04-12 00:12:42 +0000

    Update lottie to latest version (#223)
    fbshipit-source-id: b63f37f

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/lottie/JSONReadableMap.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/lottie/JSONReadableMap.java
@@ -6,6 +6,10 @@ import com.facebook.react.bridge.ReadableType;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * This class is a thin wrapper around React Native's `ReadableMap` and `ReadableArray` classes,
@@ -20,9 +24,206 @@ import org.json.JSONObject;
  */
 class JSONReadableMap extends JSONObject {
   private final ReadableMap map;
+  private static final String NOT_SUPPORTED_MESSAGE = "JSONReadableMap does not implement this method";
 
   JSONReadableMap(ReadableMap map) {
     this.map = map;
+  }
+
+  public JSONReadableMap() {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  public JSONReadableMap(Map copyFrom) {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  public JSONReadableMap(JSONTokener readFrom) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  public JSONReadableMap(String json) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  public JSONReadableMap(JSONObject copyFrom, String[] names) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public int length() {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONObject put(String name, boolean value) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONObject put(String name, double value) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONObject put(String name, int value) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONObject put(String name, long value) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONObject put(String name, Object value) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONObject putOpt(String name, Object value) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONObject accumulate(String name, Object value) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public Object remove(String name) {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public boolean isNull(String name) {
+    return map.isNull(name);
+  }
+
+  @Override
+  public JSONArray toJSONArray(JSONArray names) throws JSONException {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public Iterator<String> keys() {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public JSONArray names() {
+    throw new UnsupportedOperationException(NOT_SUPPORTED_MESSAGE);
+  }
+
+  @Override
+  public Object opt(String name) {
+    if (!map.hasKey(name)) {
+      return null;
+    }
+    switch (map.getType(name)) {
+      case Array:
+        return new JSONReadableArray(map.getArray(name));
+      case Map:
+        return new JSONReadableMap(map.getMap(name));
+      case Boolean:
+        return map.getBoolean(name);
+      case Number:
+        try {
+          return map.getInt(name);
+        } catch (Exception e) {
+          return map.getDouble(name);
+        }
+      case String:
+        return map.getString(name);
+      case Null:
+      default:
+        return null;
+    }
+  }
+
+  @Override
+  public boolean optBoolean(String name) {
+    return optBoolean(name, false);
+  }
+
+  @Override
+  public boolean optBoolean(String name, boolean fallback) {
+    if (map.hasKey(name) && map.getType(name) == ReadableType.Boolean) {
+      return map.getBoolean(name);
+    }
+    return fallback;
+  }
+
+  @Override
+  public double optDouble(String name) {
+    return optDouble(name, Double.NaN);
+  }
+
+  @Override
+  public double optDouble(String name, double fallback) {
+    if (map.hasKey(name) && map.getType(name) == ReadableType.Number) {
+      return map.getDouble(name);
+    }
+    return fallback;
+  }
+
+  @Override
+  public int optInt(String name) {
+    return optInt(name, 0);
+  }
+
+  @Override
+  public int optInt(String name, int fallback) {
+    if (map.hasKey(name) && map.getType(name) == ReadableType.Number) {
+      return map.getInt(name);
+    }
+    return fallback;
+  }
+
+  @Override
+  public long optLong(String name) {
+    return optLong(name, 0L);
+  }
+
+  @Override
+  public long optLong(String name, long fallback) {
+    if (map.hasKey(name) && map.getType(name) == ReadableType.Number) {
+      try {
+        return map.getInt(name);
+      } catch (Exception e) {
+        return fallback;
+      }
+    }
+    return fallback;
+  }
+
+  @Override
+  public String optString(String name) {
+    return optString(name, "");
+  }
+
+  @Override
+  public String optString(String name, String fallback) {
+    if (map.hasKey(name) && map.getType(name) == ReadableType.String) {
+      return map.getString(name);
+    }
+    return fallback;
+  }
+
+  @Override
+  public JSONArray optJSONArray(String name) {
+    if (map.hasKey(name) && map.getType(name) == ReadableType.Array) {
+      return new JSONReadableArray(map.getArray(name));
+    }
+    return null;
+  }
+
+  @Override
+  public JSONObject optJSONObject(String name) {
+    if (map.hasKey(name) && map.getType(name) == ReadableType.Map) {
+      return new JSONReadableMap(map.getMap(name));
+    }
+    return null;
   }
 
   @Override public boolean has(String name) {
