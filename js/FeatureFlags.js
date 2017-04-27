4709d2dabde3709e64a5b2a07db763bdecae11f7
Brent Vatne <brentvatne@gmail.com>
2017-04-27 20:57:53 +0000

    Disable tabs on Explore on Android due to ViewPager bugs
    fbshipit-source-id: 96811f2

--- js/FeatureFlags.js
+++ js/FeatureFlags.js
@@ -1,5 +1,7 @@
+import { Platform } from 'react-native';
+
 export default {
-  HIDE_EXPLORE_TABS: false,
+  HIDE_EXPLORE_TABS: Platform.OS === 'android' ? true : false,
   DISPLAY_EXPERIMENTAL_EXPLORE_TABS: false,
   INFINITE_SCROLL_EXPLORE_TABS: false,
 };
