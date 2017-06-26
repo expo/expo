302135acc29937492ce478564f718bd4ac2d1748
Jesse Ruder <jesse@getexponent.com>
2017-06-26 19:07:54 +0000

    Fix int arrays in app.json
    fbshipit-source-id: 8a6814b

--- android/expoview/src/main/java/host/exp/exponent/utils/JSONBundleConverter.java
+++ android/expoview/src/main/java/host/exp/exponent/utils/JSONBundleConverter.java
@@ -44,13 +44,21 @@ public class JSONBundleConverter {
           bundle.putString(key, (String) value);
         } else if (clazz == Boolean.class) {
           bundle.putBoolean(key, (Boolean) value);
+        } else if (clazz == boolean.class) {
+          bundle.putBoolean(key, (boolean) value);
         } else if (clazz == Integer.class) {
           bundle.putInt(key, (Integer) value);
+        } else if (clazz == int.class) {
+          bundle.putInt(key, (int) value);
         } else if (clazz == Long.class) {
           bundle.putLong(key, (Long) value);
+        } else if (clazz == long.class) {
+          bundle.putLong(key, (long) value);
         } else if (clazz == Double.class) {
           bundle.putDouble(key, (Double) value);
-        } else if (clazz == String.class) {
+        } else if (clazz == double.class) {
+          bundle.putDouble(key, (double) value);
+        } else {
           throw new IllegalArgumentException("Unexpected type: " + clazz);
         }
       } catch (JSONException e) {
@@ -65,7 +73,7 @@ public class JSONBundleConverter {
     Object array = Array.newInstance(clazz, jsonArray.length());
     for (int i = 0; i < jsonArray.length(); i++) {
       Object current = jsonArray.get(i);
-      if (clazz.isAssignableFrom(current.getClass())) {
+      if (clazz.isAssignableFrom(unboxClass(current.getClass()))) {
         Array.set(array, i, current);
       } else {
         throw new IllegalArgumentException("Unexpected type in an array: " + current.getClass() + ". All array elements must be same type.");
@@ -75,6 +83,28 @@ public class JSONBundleConverter {
     return array;
   }
 
+  private static Class unboxClass(Class clazz) {
+    if (clazz == Byte.class) {
+      return byte.class;
+    } else if (clazz == Character.class) {
+      return char.class;
+    } else if (clazz == Float.class) {
+      return float.class;
+    } else if (clazz == Short.class) {
+      return short.class;
+    } else if (clazz == Boolean.class) {
+      return boolean.class;
+    } else if (clazz == Double.class) {
+      return double.class;
+    } else if (clazz == Integer.class) {
+      return int.class;
+    } else if (clazz == Long.class) {
+      return long.class;
+    } else {
+      return clazz;
+    }
+  }
+
   private static void setJSONArray(Bundle bundle, String key, JSONArray jsonArray) throws JSONException {
     // Empty list, can't even figure out the type, assume an ArrayList<String>
     if (jsonArray.length() == 0) {
@@ -82,24 +112,25 @@ public class JSONBundleConverter {
     } else {
       Object first = jsonArray.get(0);
 
-      // Blurghhhhh. We could do this all with reflection but this seems (slightly) better maybe?
-      if (byte.class.isAssignableFrom(first.getClass())) {
+      // We could do this all with reflection but this seems (slightly) better maybe?
+      Class clazz = unboxClass(first.getClass());
+      if (byte.class.isAssignableFrom(clazz)) {
         bundle.putByteArray(key, (byte[]) convertToJavaArray(byte.class, jsonArray));
-      } else if (char.class.isAssignableFrom(first.getClass())) {
+      } else if (char.class.isAssignableFrom(clazz)) {
         bundle.putCharArray(key, (char[]) convertToJavaArray(char.class, jsonArray));
-      } else if (float.class.isAssignableFrom(first.getClass())) {
+      } else if (float.class.isAssignableFrom(clazz)) {
         bundle.putFloatArray(key, (float[]) convertToJavaArray(float.class, jsonArray));
-      } else if (short.class.isAssignableFrom(first.getClass())) {
+      } else if (short.class.isAssignableFrom(clazz)) {
         bundle.putShortArray(key, (short[]) convertToJavaArray(short.class, jsonArray));
-      } else if (String.class.isAssignableFrom(first.getClass())) {
+      } else if (String.class.isAssignableFrom(clazz)) {
         bundle.putStringArray(key, (String[]) convertToJavaArray(String.class, jsonArray));
-      } else if (boolean.class.isAssignableFrom(first.getClass())) {
+      } else if (boolean.class.isAssignableFrom(clazz)) {
         bundle.putBooleanArray(key, (boolean[]) convertToJavaArray(boolean.class, jsonArray));
-      } else if (double.class.isAssignableFrom(first.getClass())) {
+      } else if (double.class.isAssignableFrom(clazz)) {
         bundle.putDoubleArray(key, (double[]) convertToJavaArray(double.class, jsonArray));
-      } else if (int.class.isAssignableFrom(first.getClass())) {
+      } else if (int.class.isAssignableFrom(clazz)) {
         bundle.putIntArray(key, (int[]) convertToJavaArray(int.class, jsonArray));
-      } else if (long.class.isAssignableFrom(first.getClass())) {
+      } else if (long.class.isAssignableFrom(clazz)) {
         bundle.putLongArray(key, (long[]) convertToJavaArray(long.class, jsonArray));
       } else if (first instanceof JSONObject) {
         ArrayList<Bundle> parcelableArrayList = new ArrayList<>();
