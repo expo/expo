1749fcb4b3f063f4a9fe8b421fd4b0b7c5a5e8ae
Jesse Ruder <jesse@getexponent.com>
2017-07-14 23:18:11 +0000

    Version SDK 19
    fbshipit-source-id: b9ba86f381

--- android/expoview/src/main/java/host/exp/exponent/Constants.java
+++ android/expoview/src/main/java/host/exp/exponent/Constants.java
@@ -41,6 +41,7 @@ public class Constants {
   static {
     List<String> abiVersions = new ArrayList<>();
     // THIS COMMENT IS USED BY android-build-aar.sh DO NOT MODIFY
+    abiVersions.add("19.0.0");
     abiVersions.add("18.0.0");
     abiVersions.add("17.0.0");
     abiVersions.add("16.0.0");
