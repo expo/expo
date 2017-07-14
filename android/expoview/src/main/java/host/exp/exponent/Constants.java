915c0a31ecbe33ef2d3f68238716f408f43c469e
Jesse Ruder <jesse@getexponent.com>
2017-07-14 18:15:30 +0000

    DELETE SDK 11 and 12
    fbshipit-source-id: 8a0df37a6c

--- android/expoview/src/main/java/host/exp/exponent/Constants.java
+++ android/expoview/src/main/java/host/exp/exponent/Constants.java
@@ -47,8 +47,6 @@ public class Constants {
     abiVersions.add("15.0.0");
     abiVersions.add("14.0.0");
     abiVersions.add("13.0.0");
-    abiVersions.add("12.0.0");
-    abiVersions.add("11.0.0");
 
     if (TEMPORARY_ABI_VERSION != null) {
       abiVersions.add(TEMPORARY_ABI_VERSION);
