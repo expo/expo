e844fdb6c4cf10b41f40deb98caffe6b9a5b5c6e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2017-07-28 21:23:32 +0000

    Add and use `GLObjectManagerModule`
    fbshipit-source-id: f314476

--- android/expoview/src/main/java/host/exp/exponent/exgl/EXGL.java
+++ android/expoview/src/main/java/host/exp/exponent/exgl/EXGL.java
@@ -10,4 +10,8 @@ public class EXGL {
   public static native int EXGLContextCreate(long jsCtxPtr);
   public static native void EXGLContextDestroy(int exglCtxId);
   public static native void EXGLContextFlush(int exglCtxId);
+
+  public static native int EXGLContextCreateObject(int exglCtxId);
+  public static native void EXGLContextDestroyObject(int exglCtxId, int exglObjId);
+  public static native void EXGLContextMapObject(int exglCtxId, int exglObjId, int glObj);
 }
--- android/expoview/src/main/jni/EXGL.c
+++ android/expoview/src/main/jni/EXGL.c
@@ -29,3 +29,21 @@ Java_host_exp_exponent_exgl_EXGL_EXGLContextFlush
 (JNIEnv *env, jclass clazz, jint exglCtxId) {
   UEXGLContextFlush(exglCtxId);
 }
+
+JNIEXPORT jint JNICALL
+Java_host_exp_exponent_exgl_EXGL_EXGLContextCreateObject
+(JNIEnv *env, jclass clazz, jint exglCtxId) {
+  return UEXGLContextCreateObject(exglCtxId);
+}
+
+JNIEXPORT void JNICALL
+Java_host_exp_exponent_exgl_EXGL_EXGLContextDestroyObject
+(JNIEnv *env, jclass clazz, jint exglCtxId, jint exglObjId) {
+  UEXGLContextDestroyObject(exglCtxId, exglObjId);
+}
+
+JNIEXPORT void JNICALL
+Java_host_exp_exponent_exgl_EXGL_EXGLContextMapObject
+(JNIEnv *env, jclass clazz, jint exglCtxId, jint exglObjId, jint glObj) {
+  UEXGLContextMapObject(exglCtxId, exglObjId, glObj);
+}
