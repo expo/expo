9e323e1446edc0096657b3bebca738a990f76f7d
Alicja Warchal <alicja.warchal@swmansion.com>
2017-09-11 11:56:15 +0000

    Do not initialize TTS before usage
    fbshipit-source-id: 90add01

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/SpeechModule.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/SpeechModule.java
@@ -14,56 +14,22 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Queue;
 
 public class SpeechModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
   private TextToSpeech mTextToSpeech;
   private boolean mTtsReady = false;
+  private Queue<Map<String, Object>> mDelayedUtterances = new ArrayDeque<>();
 
   public SpeechModule(ReactApplicationContext reactContext) {
     super(reactContext);
-
     reactContext.addLifecycleEventListener(this);
-    mTextToSpeech = new TextToSpeech(getReactApplicationContext(), new TextToSpeech.OnInitListener() {
-      @Override
-      public void onInit(int status) {
-        if (status == TextToSpeech.SUCCESS) {
-          mTtsReady = true;
-
-          mTextToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
-            @Override
-            public void onStart(String utteranceId) {
-              getReactApplicationContext()
-                  .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                  .emit("Exponent.speakingStarted", idToMap(utteranceId));
-            }
-
-            @Override
-            public void onDone(String utteranceId) {
-              getReactApplicationContext()
-                  .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                  .emit("Exponent.speakingDone", idToMap(utteranceId));
-            }
-
-            @Override
-            public void onStop(String utteranceId, boolean interrupted) {
-              getReactApplicationContext()
-                  .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                  .emit("Exponent.speakingStopped", idToMap(utteranceId));
-            }
-
-            @Override
-            public void onError(String utteranceId) {
-              getReactApplicationContext()
-                  .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                  .emit("Exponent.speakingError", idToMap(utteranceId));
-            }
-          });
-        }
-      }
-    });
   }
 
   @Override
@@ -74,54 +40,121 @@ public class SpeechModule extends ReactContextBaseJavaModule implements Lifecycl
   @ReactMethod
   public void speak(final String id, final String text, final ReadableMap options) {
     if (mTtsReady) {
-      if (options.hasKey("language")) {
-        Locale locale = new Locale(options.getString("language"));
-        int languageAvailable = mTextToSpeech.isLanguageAvailable(locale);
-        if (languageAvailable != TextToSpeech.LANG_MISSING_DATA &&
-            languageAvailable != TextToSpeech.LANG_NOT_SUPPORTED) {
-          mTextToSpeech.setLanguage(locale);
-        } else {
-          mTextToSpeech.setLanguage(Locale.getDefault());
+      speakOut(id, text, options);
+    } else {
+      mDelayedUtterances.add(Collections.unmodifiableMap(new HashMap<String, Object>() {
+        {
+          put("id", id);
+          put("text", text);
+          put("options", options);
         }
-      } else {
-        mTextToSpeech.setLanguage(Locale.getDefault());
-      }
-      if (options.hasKey("pitch")) {
-        mTextToSpeech.setPitch((float) options.getDouble("pitch"));
-      }
-      if (options.hasKey("rate")) {
-        mTextToSpeech.setSpeechRate((float) options.getDouble("rate"));
-      }
+      }));
+      // init TTS, speaking will be available only after onInit
+      getTextToSpeech();
+    }
+  }
 
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        mTextToSpeech.speak(
-            text,
-            TextToSpeech.QUEUE_ADD,
-            null,
-            id
-        );
+  private void speakOut(final String id, final String text, final ReadableMap options) {
+    TextToSpeech textToSpeech = getTextToSpeech();
+    if (options.hasKey("language")) {
+      Locale locale = new Locale(options.getString("language"));
+      int languageAvailable = textToSpeech.isLanguageAvailable(locale);
+      if (languageAvailable != TextToSpeech.LANG_MISSING_DATA &&
+          languageAvailable != TextToSpeech.LANG_NOT_SUPPORTED) {
+        textToSpeech.setLanguage(locale);
       } else {
-        HashMap<String, String> params = new HashMap<>();
-        params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, id);
-        mTextToSpeech.speak(
-            text,
-            TextToSpeech.QUEUE_ADD,
-            params
-        );
+        textToSpeech.setLanguage(Locale.getDefault());
       }
+    } else {
+      textToSpeech.setLanguage(Locale.getDefault());
+    }
+    if (options.hasKey("pitch")) {
+      textToSpeech.setPitch((float) options.getDouble("pitch"));
+    }
+    if (options.hasKey("rate")) {
+      textToSpeech.setSpeechRate((float) options.getDouble("rate"));
+    }
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      textToSpeech.speak(
+          text,
+          TextToSpeech.QUEUE_ADD,
+          null,
+          id
+      );
+    } else {
+      HashMap<String, String> params = new HashMap<>();
+      params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, id);
+      textToSpeech.speak(
+          text,
+          TextToSpeech.QUEUE_ADD,
+          params
+      );
     }
   }
 
   @ReactMethod
   public void isSpeaking(Promise promise) {
-    promise.resolve(mTtsReady && mTextToSpeech.isSpeaking());
+    promise.resolve(getTextToSpeech().isSpeaking());
   }
 
   @ReactMethod
   public void stop() {
-    if (mTtsReady) {
-      mTextToSpeech.stop();
+    getTextToSpeech().stop();
+  }
+
+  private TextToSpeech getTextToSpeech() {
+    if (mTextToSpeech == null) {
+      mTextToSpeech = new TextToSpeech(getReactApplicationContext(), new TextToSpeech.OnInitListener() {
+        @Override
+        public void onInit(int status) {
+          if (status == TextToSpeech.SUCCESS) {
+            // synchronize because in some cases this runs on another thread and mTTS is null
+            synchronized (SpeechModule.this) {
+              mTtsReady = true;
+              mTextToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onStart(String utteranceId) {
+                  getReactApplicationContext()
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("Exponent.speakingStarted", idToMap(utteranceId));
+                }
+
+                @Override
+                public void onDone(String utteranceId) {
+                  getReactApplicationContext()
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("Exponent.speakingDone", idToMap(utteranceId));
+                }
+
+                @Override
+                public void onStop(String utteranceId, boolean interrupted) {
+                  getReactApplicationContext()
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("Exponent.speakingStopped", idToMap(utteranceId));
+                }
+
+                @Override
+                public void onError(String utteranceId) {
+                  getReactApplicationContext()
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("Exponent.speakingError", idToMap(utteranceId));
+                }
+              });
+
+              for (Map<String, Object> arguments : mDelayedUtterances) {
+                speakOut(
+                  (String) arguments.get("id"),
+                  (String) arguments.get("text"),
+                  (ReadableMap) arguments.get("options")
+                );
+              }
+            }
+          }
+        }
+      });
     }
+    return mTextToSpeech;
   }
 
   @Override
@@ -132,7 +165,7 @@ public class SpeechModule extends ReactContextBaseJavaModule implements Lifecycl
 
   @Override
   public void onHostDestroy() {
-    mTextToSpeech.shutdown();
+    getTextToSpeech().shutdown();
   }
 
   private WritableMap idToMap(String id) {
