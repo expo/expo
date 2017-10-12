53703095100031e1a0b0e8c464497a6eef7e696e
Brent Vatne <brentvatne@gmail.com>
2017-10-12 01:18:05 +0000

    iOS11 SFAuthenticationSession support, first pass (#1230)
    fbshipit-source-id: 728d6e7dd3

--- ios/Exponent/Versioned/Modules/Api/EXWebBrowser.m
+++ ios/Exponent/Versioned/Modules/Api/EXWebBrowser.m
@@ -18,6 +18,11 @@ NSString *EXWebBrowserErrorCode = @"EXWebBrowser";
 @implementation EXWebBrowser
 {
   UIStatusBarStyle _initialStatusBarStyle;
+  
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpartial-availability"
+  SFAuthenticationSession *_authSession;
+#pragma clang diagnostic pop
 }
 
 RCT_EXPORT_MODULE(ExponentWebBrowser)
@@ -27,44 +32,70 @@ RCT_EXPORT_MODULE(ExponentWebBrowser)
   return dispatch_get_main_queue();
 }
 
-RCT_EXPORT_METHOD(openBrowserAsync:(NSString *)authURL
-                          resolver:(RCTPromiseResolveBlock)resolve
-                          rejecter:(RCTPromiseRejectBlock)reject)
-{
-  if (_redirectResolve) {
-    reject(EXWebBrowserErrorCode, @"Another WebBrowser is already being presented.", nil);
-    return;
-  }
-  _redirectReject = reject;
-  _redirectResolve = resolve;
-  _initialStatusBarStyle = RCTSharedApplication().statusBarStyle;
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-  [RCTSharedApplication() setStatusBarStyle:UIStatusBarStyleDefault animated: YES];
-#pragma clang diagnostic pop
-  
-  if ([SFSafariViewController class]) {
-    // Safari View Controller to authorize request
-    NSURL *url = [[NSURL alloc] initWithString:authURL];
-    SFSafariViewController *safariVC = [[SFSafariViewController alloc] initWithURL:url entersReaderIfAvailable:NO];
-    safariVC.delegate = self;
-    
-    // By setting the modal presentation style to OverFullScreen, we disable the "Swipe to dismiss"
-    // gesture that is causing a bug where sometimes `safariViewControllerDidFinish` is not called.
-    // There are bugs filed already about it on OpenRadar.
-    [safariVC setModalPresentationStyle: UIModalPresentationOverFullScreen];
 
-    // This is a hack to present the SafariViewController modally
-    UINavigationController *safariHackVC = [[UINavigationController alloc] initWithRootViewController:safariVC];
-    [safariHackVC setNavigationBarHidden:true animated:false];
-    [RCTPresentedViewController() presentViewController:safariHackVC animated:true completion:nil];
+RCT_EXPORT_METHOD(openAuthSessionAsync:(NSString *)authURL
+                  redirectURL:(NSString *)redirectURL
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  [self initializeWebBrowserWithResolver:resolve andRejecter:reject];
+
+
+  if (@available(iOS 11, *)) {
+    NSURL *url = [[NSURL alloc] initWithString: authURL];
+    _authSession = [[SFAuthenticationSession alloc]
+              initWithURL:url
+        callbackURLScheme:redirectURL
+        completionHandler:^(NSURL* _Nullable callbackURL, NSError* _Nullable error) {
+            if (!error) {
+                NSString *url = callbackURL.absoluteString;
+                _redirectResolve(@{
+                    @"type" : @"success",
+                    @"url" : url,
+                });
+            } else {
+                _redirectResolve(@{
+                    @"type" : @"cancel",
+                });
+            }
+            [self flowDidFinish];
+        }];
+    [_authSession start];
   } else {
-    // Opens Safari Browser when SFSafariViewController is not available (iOS 8)
-    [RCTSharedApplication() openURL:[NSURL URLWithString:authURL]];
+      resolve(@{
+          @"type" : @"cancel",
+          @"message" : @"openAuthSessionAsync requires iOS 11 or greater"
+      });
+      [self flowDidFinish];
   }
 }
 
-RCT_EXPORT_METHOD(dismissBrowser) {
+
+RCT_EXPORT_METHOD(openBrowserAsync:(NSString *)authURL
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  if (![self initializeWebBrowserWithResolver:resolve andRejecter:reject]) {
+    return;
+  }
+  
+  // Safari View Controller to authorize request
+  NSURL *url = [[NSURL alloc] initWithString:authURL];
+  SFSafariViewController *safariVC = [[SFSafariViewController alloc] initWithURL:url entersReaderIfAvailable:NO];
+  safariVC.delegate = self;
+  
+ // By setting the modal presentation style to OverFullScreen, we disable the "Swipe to dismiss"
+ // gesture that is causing a bug where sometimes `safariViewControllerDidFinish` is not called.
+ // There are bugs filed already about it on OpenRadar.
+ [safariVC setModalPresentationStyle: UIModalPresentationOverFullScreen];
+  
+ // This is a hack to present the SafariViewController modally
+ UINavigationController *safariHackVC = [[UINavigationController alloc] initWithRootViewController:safariVC];
+ [safariHackVC setNavigationBarHidden:true animated:false];
+ [RCTPresentedViewController() presentViewController:safariHackVC animated:true completion:nil];
+}
+
+- (void)_dismissBrowser {
   __weak typeof(self) weakSelf = self;
   [RCTPresentedViewController() dismissViewControllerAnimated:YES completion:^{
     __strong typeof(self) strongSelf = weakSelf;
@@ -77,6 +108,45 @@ RCT_EXPORT_METHOD(dismissBrowser) {
   }];
 }
 
+RCT_EXPORT_METHOD(dismissBrowser) {
+  [self _dismissBrowser];
+}
+
+RCT_EXPORT_METHOD(dismissAuthSession) {
+  if (@available(iOS 11, *)) {
+    [_authSession cancel];
+    if (_redirectResolve) {
+      _redirectResolve(@{
+        @"type": @"dismissed"
+      });
+
+      [self flowDidFinish];
+    }
+  } else {
+    [self dismissBrowser];
+  }
+}
+
+/**
+ * Helper that is used in openBrowserAsync and openAuthSessionAsync
+ */
+- (BOOL)initializeWebBrowserWithResolver:(RCTPromiseResolveBlock)resolve andRejecter:(RCTPromiseRejectBlock)reject {
+  if (_redirectResolve) {
+    reject(EXWebBrowserErrorCode, @"Another WebBrowser is already being presented.", nil);
+    return NO;
+  }
+  _redirectReject = reject;
+  _redirectResolve = resolve;
+  
+  _initialStatusBarStyle = RCTSharedApplication().statusBarStyle;
+  
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+  [RCTSharedApplication() setStatusBarStyle:UIStatusBarStyleDefault
+                                   animated:YES];
+#pragma clang diagnostic pop
+  return YES;
+}
 
 /**
  * Called when the user dismisses the SFVC without logging in.
