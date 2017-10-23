e057d0c96e493ba166851eb7244974282ef1ca90
Joshua Efiong <joshefiong@gmail.com>
2017-10-23 20:29:55 +0100

    Mirror argument on iOS

--- ios/Exponent/Versioned/Modules/Api/EXNotifications.m
+++ ios/Exponent/Versioned/Modules/Api/EXNotifications.m
@@ -50,6 +50,7 @@ EX_EXPORT_SCOPED_MODULE(ExponentNotifications, RemoteNotificationManager);
 }
 
 RCT_REMAP_METHOD(getDevicePushTokenAsync,
+                 unusedSenderIdArgument:(NSString *)senderId
                  getDevicePushTokenAsyncWithResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
