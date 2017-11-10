ea61cf2d4a1f565456e08e0119d343667572eddd
James Ide <ide@expo.io>
2017-11-10 23:54:32 +0000

    Update react-native-gesture-handler to alpha 30 for RN 0.50
    Tested in NCL on both iOS and Android
    
    fbshipit-source-id: 9c12f94

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/gesturehandler/react/RNGestureHandlerRegistry.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/gesturehandler/react/RNGestureHandlerRegistry.java
@@ -2,13 +2,9 @@ package versioned.host.exp.exponent.modules.api.components.gesturehandler.react;
 
 import android.util.SparseArray;
 import android.view.View;
-import android.view.ViewGroup;
 
-import com.facebook.react.uimanager.ReactPointerEventsView;
-import com.facebook.react.views.view.ReactViewGroup;
 import versioned.host.exp.exponent.modules.api.components.gesturehandler.GestureHandler;
 import versioned.host.exp.exponent.modules.api.components.gesturehandler.GestureHandlerRegistry;
-import versioned.host.exp.exponent.modules.api.components.gesturehandler.PointerEvents;
 
 import java.util.ArrayList;
 
