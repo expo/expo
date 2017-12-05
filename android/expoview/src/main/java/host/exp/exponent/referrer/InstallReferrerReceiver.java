b2173ab487748aacb9be8ec5956a2ef9dad0b9ea
Jesse Ruder <jesse@expo.io>
2017-12-05 20:24:20 +0000

    Move most of app to expoview
    fbshipit-source-id: b499ba2

--- android/expoview/src/main/java/host/exp/exponent/referrer/InstallReferrerReceiver.java
+++ android/expoview/src/main/java/host/exp/exponent/referrer/InstallReferrerReceiver.java
@@ -13,17 +13,11 @@ import io.branch.referral.InstallListener;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
-
 import javax.inject.Inject;
 
 import host.exp.exponent.Constants;
-import host.exp.exponent.ExponentApplication;
-import host.exp.exponent.ExponentManifest;
 import host.exp.exponent.analytics.EXL;
 import host.exp.exponent.di.NativeModuleDepsProvider;
-import host.exp.exponent.network.ExponentNetwork;
-import host.exp.exponent.kernel.Kernel;
 import host.exp.exponent.storage.ExponentSharedPreferences;
 import host.exp.expoview.Exponent;
 
@@ -38,7 +32,7 @@ public class InstallReferrerReceiver extends CampaignTrackingReceiver {
   public void onReceive(Context context, Intent intent) {
     super.onReceive(context, intent);
 
-    if (!(context.getApplicationContext() instanceof ExponentApplication)) {
+    if (!(context.getApplicationContext().getClass().getSimpleName().equals("ExponentApplication"))) {
       EXL.e(TAG, "InstallReferrerReceiver.context.getApplicationContext() not an instance of ExponentApplication");
       return;
     }
