adcf01fac12ec8acaa1db866b3e252af83d205a1
James Ide <ide@expo.io>
2017-12-05 22:12:49 +0000

    Update ReactAndroid to SDK 24
    fbshipit-source-id: 57cf59b

--- android/ReactAndroid/src/main/java/com/facebook/react/shell/MainReactPackage.java
+++ android/ReactAndroid/src/main/java/com/facebook/react/shell/MainReactPackage.java
@@ -101,7 +101,7 @@ public class MainReactPackage extends LazyReactPackage {
   @Override
   public List<ModuleSpec> getNativeModules(final ReactApplicationContext context) {
     return Arrays.asList(
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             AccessibilityInfoModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -109,7 +109,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new AccessibilityInfoModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             AppStateModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -117,7 +117,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new AppStateModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             BlobModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -125,7 +125,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new BlobModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             AsyncStorageModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -133,7 +133,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new AsyncStorageModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             CameraRollManager.class,
             new Provider<NativeModule>() {
               @Override
@@ -141,7 +141,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new CameraRollManager(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             ClipboardModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -149,7 +149,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new ClipboardModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             DatePickerDialogModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -157,7 +157,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new DatePickerDialogModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             DialogModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -165,7 +165,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new DialogModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             FrescoModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -174,7 +174,7 @@ public class MainReactPackage extends LazyReactPackage {
                     context, true, mConfig != null ? mConfig.getFrescoConfig() : null);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             I18nManagerModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -182,7 +182,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new I18nManagerModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             ImageEditingManager.class,
             new Provider<NativeModule>() {
               @Override
@@ -190,7 +190,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new ImageEditingManager(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             ImageLoaderModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -198,7 +198,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new ImageLoaderModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             ImageStoreManager.class,
             new Provider<NativeModule>() {
               @Override
@@ -206,7 +206,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new ImageStoreManager(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             IntentModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -214,7 +214,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new IntentModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             LocationModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -222,7 +222,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new LocationModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             NativeAnimatedModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -230,7 +230,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new NativeAnimatedModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             NetworkingModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -238,7 +238,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new NetworkingModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             NetInfoModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -246,7 +246,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new NetInfoModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             PermissionsModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -254,7 +254,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new PermissionsModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             ShareModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -262,7 +262,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new ShareModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             StatusBarModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -270,7 +270,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new StatusBarModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             TimePickerDialogModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -278,7 +278,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new TimePickerDialogModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             ToastModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -286,7 +286,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new ToastModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             VibrationModule.class,
             new Provider<NativeModule>() {
               @Override
@@ -294,7 +294,7 @@ public class MainReactPackage extends LazyReactPackage {
                 return new VibrationModule(context);
               }
             }),
-        new ModuleSpec(
+        ModuleSpec.nativeModuleSpec(
             WebSocketModule.class,
             new Provider<NativeModule>() {
               @Override
