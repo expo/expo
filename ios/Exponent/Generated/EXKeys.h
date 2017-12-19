fa76849b1344d7c9fcedb356162cc8655e289d5a
Piero Toffanin <pt@masseranolabs.com>
2017-12-19 18:47:15 -0500

    Removed xcode generated stuff

--- ios/Exponent/Generated/EXKeys.h
+++
@@ -1,5 +0,0 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
-
-#define AMPLITUDE_KEY @"1e246ef3dacaabe8648768d7c35fceb1"
-#define AMPLITUDE_DEV_KEY @"773a3e0a8a28b8b1c3497792df4a5271"
-#define GOOGLE_MAPS_IOS_API_KEY @""
