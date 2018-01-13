68b174dbb1d266a391a3264b2b48bacc4c39878a
Janic Duplessis <janicduplessis@gmail.com>
2018-01-13 01:01:29 +0000

    Improve assets bundle runtime implementation, support most asset types (#1691)
    fbshipit-source-id: d3045f7

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/FontLoaderModule.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/FontLoaderModule.java
@@ -15,6 +15,8 @@ import com.facebook.react.views.text.ReactFontManager;
 import java.io.File;
 
 public class FontLoaderModule extends ReactContextBaseJavaModule {
+  private static final String ASSET_SCHEME = "asset://";
+
   public FontLoaderModule(ReactApplicationContext reactContext) {
     super(reactContext);
   }
@@ -28,7 +30,15 @@ public class FontLoaderModule extends ReactContextBaseJavaModule {
   public void loadAsync(final String fontFamilyName, final String localUri, final Promise promise) {
     try {
       // TODO(nikki): make sure path is in experience's scope
-      Typeface typeface = Typeface.createFromFile(new File(Uri.parse(localUri).getPath()));
+      Typeface typeface;
+      if (localUri.startsWith(ASSET_SCHEME)) {
+        typeface = Typeface.createFromAsset(
+            getReactApplicationContext().getAssets(),
+            // Also remove the leading slash.
+            localUri.substring(ASSET_SCHEME.length() + 1));
+      } else {
+        typeface = Typeface.createFromFile(new File(Uri.parse(localUri).getPath()));
+      }
       ReactFontManager.getInstance().setTypeface("ExponentFont-" + fontFamilyName, Typeface.NORMAL, typeface);
       promise.resolve(Arguments.createMap());
     } catch (Exception e) {
---
+++ ios/Exponent/Kernel/Services/EXFileSystemManager.m
@@ -0,0 +1,46 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#import "EXShellManager.h"
+#import "EXFileSystemManager.h"
+
+// Returns if the experience id is the main shell experience.
+BOOL EXIsShellExperience(NSString *experienceId) {
+  return [[EXShellManager sharedInstance].shellManifestUrl containsString:experienceId];
+}
+
+@implementation EXFileSystemManager
+
+- (NSString *)bundleDirectoryForExperienceId:(NSString *)experienceId
+{
+  if (!EXIsShellExperience(experienceId)) {
+    return nil;
+  }
+  return [NSBundle mainBundle].bundlePath;
+}
+
+- (NSArray<NSString *> *)bundledAssetsForExperienceId:(NSString *)experienceId
+{
+  static NSArray<NSString *> *bundledAssets = nil;
+  static dispatch_once_t once;
+  dispatch_once(&once, ^{
+    if (!EXIsShellExperience(experienceId)) {
+      return;
+    }
+    NSString *manifestBundlePath = [[NSBundle mainBundle] pathForResource:kEXShellManifestResourceName ofType:@"json"];
+    NSData *data = [NSData dataWithContentsOfFile:manifestBundlePath];
+    if (data.length == 0) {
+      return;
+    }
+    __block NSError *error;
+    id manifest = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    if (error) {
+      NSLog(@"Error parsing bundled manifest: %@", error);
+      return;
+    }
+    bundledAssets = manifest[@"bundledAssets"];
+  });
+  return bundledAssets;
+}
+
+@end
+
