8e931e28d2451a66bc7114add98f06cf6463f183
Ben Roth <spongebucket@gmail.com>
2018-02-15 00:48:54 +0000

    Move Util tests to Localization, fix localization style
    fbshipit-source-id: d9e0a63

--- ios/Exponent/Versioned/Core/Api/EXLocalization.m
+++ ios/Exponent/Versioned/Core/Api/EXLocalization.m
@@ -9,9 +9,10 @@
 
 RCT_EXPORT_MODULE(ExponentLocalization);
 
-RCT_REMAP_METHOD(getCurrentLocaleAsync, getCurrentLocaleWithResolver
-                 : (RCTPromiseResolveBlock)resolve rejecter
-                 : (RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(getCurrentLocaleAsync,
+                 getCurrentLocaleWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
   NSArray<NSString *> *preferredLanguages = [NSLocale preferredLanguages];
   if (preferredLanguages.count > 0) {
     resolve(preferredLanguages[0]);
@@ -22,9 +23,9 @@ RCT_REMAP_METHOD(getCurrentLocaleAsync, getCurrentLocaleWithResolver
 }
 
 RCT_REMAP_METHOD(getCurrentDeviceCountryAsync,
-                 getCurrentDeviceCountryWithResolver
-                 : (RCTPromiseResolveBlock)resolve rejecter
-                 : (RCTPromiseRejectBlock)reject) {
+                 getCurrentDeviceCountryWithResolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
+{
   NSString *countryCode =
       [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
   if (countryCode) {
@@ -35,21 +36,24 @@ RCT_REMAP_METHOD(getCurrentDeviceCountryAsync,
   }
 }
 
-RCT_REMAP_METHOD(getPreferredLocalesAsync, getPreferredLocalesWithResolver
-                 : (RCTPromiseResolveBlock)resolve rejecter
-                 : (RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(getPreferredLocalesAsync,
+                 getPreferredLocalesWithResolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
+{
   resolve([NSLocale preferredLanguages]);
 }
 
-RCT_REMAP_METHOD(getISOCurrencyCodesAsync, getISOCurrencyCodesWithResolver
-                 : (RCTPromiseResolveBlock)resolve rejecter
-                 : (__unused RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(getISOCurrencyCodesAsync,
+                 getISOCurrencyCodesWithResolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (__unused RCTPromiseRejectBlock)reject)
+{
   resolve([NSLocale ISOCurrencyCodes]);
 }
 
-RCT_REMAP_METHOD(getCurrentTimeZoneAsync, getCurrentTimeZoneWithResolver
-                 : (RCTPromiseResolveBlock)resolve rejecter
-                 : (RCTPromiseRejectBlock)reject) {
+RCT_REMAP_METHOD(getCurrentTimeZoneAsync,
+                 getCurrentTimeZoneWithResolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
+{
   NSTimeZone *currentTimeZone = [NSTimeZone localTimeZone];
   if (currentTimeZone) {
     resolve(currentTimeZone.name);
