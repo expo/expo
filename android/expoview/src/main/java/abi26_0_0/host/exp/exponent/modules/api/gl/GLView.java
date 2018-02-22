1b6ed8072332e1e03147c7e69a8572d9f1ed89ae
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-02-22 23:56:43 +0000

    `GLView`: Use an `EGL10.EGL_DEPTH_SIZE` of `16`
    fbshipit-source-id: dd38139

--- android/expoview/src/main/java/abi26_0_0/host/exp/exponent/modules/api/gl/GLView.java
+++ android/expoview/src/main/java/abi26_0_0/host/exp/exponent/modules/api/gl/GLView.java
@@ -303,7 +303,7 @@ public class GLView extends TextureView implements TextureView.SurfaceTextureLis
       int[] configSpec = {
           EGL10.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
           EGL10.EGL_RED_SIZE, 8, EGL10.EGL_GREEN_SIZE, 8, EGL10.EGL_BLUE_SIZE, 8,
-          EGL10.EGL_ALPHA_SIZE, 8, EGL10.EGL_DEPTH_SIZE, 0, EGL10.EGL_STENCIL_SIZE, 0,
+          EGL10.EGL_ALPHA_SIZE, 8, EGL10.EGL_DEPTH_SIZE, 16, EGL10.EGL_STENCIL_SIZE, 0,
           EGL10.EGL_NONE,
       };
       EGLConfig eglConfig = null;
