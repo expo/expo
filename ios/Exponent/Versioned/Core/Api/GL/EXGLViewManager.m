c3ab0d6de74463ac8ce60e9e0060b8b598da4d52
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2018-02-26 21:59:57 +0000

    use default method queue in EXGLViewManager (#2133)
    fbshipit-source-id: 411d94a

--- ios/Exponent/Versioned/Core/Api/GL/EXGLViewManager.m
+++ ios/Exponent/Versioned/Core/Api/GL/EXGLViewManager.m
@@ -31,11 +31,6 @@ RCT_EXPORT_MODULE(ExponentGLViewManager);
   return NO;
 }
 
-- (dispatch_queue_t)methodQueue
-{
-  return dispatch_queue_create("host.exp.exponent.GLViewManager", DISPATCH_QUEUE_SERIAL);
-}
-
 - (UIView *)view
 {
   return [[EXGLView alloc] initWithManager:self];
