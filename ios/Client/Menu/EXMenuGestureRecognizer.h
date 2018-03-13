9b820133390d268e0d6cc5e2daeb3161cc09a738
Ben Roth <spongebucket@gmail.com>
2018-03-13 22:46:43 +0000

    Support legacy force touch gesture
    fbshipit-source-id: 91e2770

---
+++ ios/Client/Menu/EXMenuGestureRecognizer.h
@@ -0,0 +1,11 @@
+
+#import <UIKit/UIKit.h>
+
+@interface EXMenuGestureRecognizer : UIGestureRecognizer
+
+/**
+ *  True if it's possible to invoke the menu gesture and if the user has enabled the legacy menu gesture.
+ */
++ (BOOL)isLegacyMenuGestureAvailable;
+
+@end
