be0d55601c214a6b2f027adde8b98d7402fd522c
Eric Samelson <samelson.e@gmail.com>
2018-03-20 01:28:49 +0000

    re-versioned RN for SDK 26
    fbshipit-source-id: bfa0dc1

--- android/versioned-react-native/ReactCommon/cxxreact/Android.mk
+++ android/versioned-react-native/ReactCommon/cxxreact/Android.mk
@@ -18,18 +18,18 @@ LOCAL_SRC_FILES := \
   JSCTracing.cpp \
   JSCUtils.cpp \
   JSIndexedRAMBundle.cpp \
-  ModuleRegistry.cpp \
   MethodCall.cpp \
+  ModuleRegistry.cpp \
   NativeToJsBridge.cpp \
   Platform.cpp \
 	RAMBundleRegistry.cpp \
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/..
 
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/..
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
 LOCAL_CFLAGS := \
-
   -DLOG_TAG=\"ReactNative\"
+
 LOCAL_CFLAGS += -Wall -Werror -fexceptions -frtti
 CXX11_FLAGS := -std=c++11
 LOCAL_CFLAGS += $(CXX11_FLAGS)
--- android/versioned-react-native/ReactCommon/yoga/yoga/YGNode.cpp
+++ android/versioned-react-native/ReactCommon/yoga/yoga/YGNode.cpp
@@ -9,6 +9,7 @@
 
 #include "YGNode.h"
 #include <iostream>
+#include "Utils.h"
 
 void* YGNode::getContext() const {
   return context_;
@@ -34,6 +35,10 @@ YGBaselineFunc YGNode::getBaseline() const {
   return baseline_;
 }
 
+YGDirtiedFunc YGNode::getDirtied() const {
+  return dirtied_;
+}
+
 YGStyle& YGNode::getStyle() {
   return style_;
 }
@@ -52,6 +57,10 @@ YGNodeRef YGNode::getParent() const {
 
 YGVector YGNode::getChildren() const {
   return children_;
+}
+
+uint32_t YGNode::getChildrenCount() const {
+  return static_cast<uint32_t>(children_.size());
 }
 
 YGNodeRef YGNode::getChild(uint32_t index) const {
@@ -77,6 +86,93 @@ YGValue YGNode::getResolvedDimension(int index) {
 std::array<YGValue, 2> YGNode::getResolvedDimensions() const {
   return resolvedDimensions_;
 }
+
+float YGNode::getLeadingPosition(
+    const YGFlexDirection axis,
+    const float axisSize) {
+  if (YGFlexDirectionIsRow(axis)) {
+    const YGValue* leadingPosition =
+        YGComputedEdgeValue(style_.position, YGEdgeStart, &YGValueUndefined);
+    if (leadingPosition->unit != YGUnitUndefined) {
+      return YGResolveValue(*leadingPosition, axisSize);
+    }
+  }
+
+  const YGValue* leadingPosition =
+      YGComputedEdgeValue(style_.position, leading[axis], &YGValueUndefined);
+
+  return leadingPosition->unit == YGUnitUndefined
+      ? 0.0f
+      : YGResolveValue(*leadingPosition, axisSize);
+}
+
+float YGNode::getTrailingPosition(
+    const YGFlexDirection axis,
+    const float axisSize) {
+  if (YGFlexDirectionIsRow(axis)) {
+    const YGValue* trailingPosition =
+        YGComputedEdgeValue(style_.position, YGEdgeEnd, &YGValueUndefined);
+    if (trailingPosition->unit != YGUnitUndefined) {
+      return YGResolveValue(*trailingPosition, axisSize);
+    }
+  }
+
+  const YGValue* trailingPosition =
+      YGComputedEdgeValue(style_.position, trailing[axis], &YGValueUndefined);
+
+  return trailingPosition->unit == YGUnitUndefined
+      ? 0.0f
+      : YGResolveValue(*trailingPosition, axisSize);
+}
+
+bool YGNode::isLeadingPositionDefined(const YGFlexDirection axis) {
+  return (YGFlexDirectionIsRow(axis) &&
+          YGComputedEdgeValue(style_.position, YGEdgeStart, &YGValueUndefined)
+                  ->unit != YGUnitUndefined) ||
+      YGComputedEdgeValue(style_.position, leading[axis], &YGValueUndefined)
+          ->unit != YGUnitUndefined;
+}
+
+bool YGNode::isTrailingPosDefined(const YGFlexDirection axis) {
+  return (YGFlexDirectionIsRow(axis) &&
+          YGComputedEdgeValue(style_.position, YGEdgeEnd, &YGValueUndefined)
+                  ->unit != YGUnitUndefined) ||
+      YGComputedEdgeValue(style_.position, trailing[axis], &YGValueUndefined)
+          ->unit != YGUnitUndefined;
+}
+
+float YGNode::getLeadingMargin(
+    const YGFlexDirection axis,
+    const float widthSize) {
+  if (YGFlexDirectionIsRow(axis) &&
+      style_.margin[YGEdgeStart].unit != YGUnitUndefined) {
+    return YGResolveValueMargin(style_.margin[YGEdgeStart], widthSize);
+  }
+
+  return YGResolveValueMargin(
+      *YGComputedEdgeValue(style_.margin, leading[axis], &YGValueZero),
+      widthSize);
+}
+
+float YGNode::getTrailingMargin(
+    const YGFlexDirection axis,
+    const float widthSize) {
+  if (YGFlexDirectionIsRow(axis) &&
+      style_.margin[YGEdgeEnd].unit != YGUnitUndefined) {
+    return YGResolveValueMargin(style_.margin[YGEdgeEnd], widthSize);
+  }
+
+  return YGResolveValueMargin(
+      *YGComputedEdgeValue(style_.margin, trailing[axis], &YGValueZero),
+      widthSize);
+}
+
+float YGNode::getMarginForAxis(
+    const YGFlexDirection axis,
+    const float widthSize) {
+  return getLeadingMargin(axis, widthSize) + getTrailingMargin(axis, widthSize);
+}
+
 // Setters
 
 void YGNode::setContext(void* context) {
@@ -127,6 +223,10 @@ void YGNode::setBaseLineFunc(YGBaselineFunc baseLineFunc) {
   baseline_ = baseLineFunc;
 }
 
+void YGNode::setDirtiedFunc(YGDirtiedFunc dirtiedFunc) {
+  dirtied_ = dirtiedFunc;
+}
+
 void YGNode::setStyle(YGStyle style) {
   style_ = style;
 }
@@ -168,7 +268,13 @@ void YGNode::setConfig(YGConfigRef config) {
 }
 
 void YGNode::setDirty(bool isDirty) {
+  if (isDirty == isDirty_) {
+    return;
+  }
   isDirty_ = isDirty;
+  if (isDirty && dirtied_) {
+    dirtied_(this);
+  }
 }
 
 bool YGNode::removeChild(YGNodeRef child) {
@@ -230,6 +336,46 @@ void YGNode::setLayoutDimension(float dimension, int index) {
   layout_.dimensions[index] = dimension;
 }
 
+// If both left and right are defined, then use left. Otherwise return
+// +left or -right depending on which is defined.
+float YGNode::relativePosition(
+    const YGFlexDirection axis,
+    const float axisSize) {
+  return isLeadingPositionDefined(axis) ? getLeadingPosition(axis, axisSize)
+                                        : -getTrailingPosition(axis, axisSize);
+}
+
+void YGNode::setPosition(
+    const YGDirection direction,
+    const float mainSize,
+    const float crossSize,
+    const float parentWidth) {
+  /* Root nodes should be always layouted as LTR, so we don't return negative
+   * values. */
+  const YGDirection directionRespectingRoot =
+      parent_ != nullptr ? direction : YGDirectionLTR;
+  const YGFlexDirection mainAxis =
+      YGResolveFlexDirection(style_.flexDirection, directionRespectingRoot);
+  const YGFlexDirection crossAxis =
+      YGFlexDirectionCross(mainAxis, directionRespectingRoot);
+
+  const float relativePositionMain = relativePosition(mainAxis, mainSize);
+  const float relativePositionCross = relativePosition(crossAxis, crossSize);
+
+  setLayoutPosition(
+      getLeadingMargin(mainAxis, parentWidth) + relativePositionMain,
+      leading[mainAxis]);
+  setLayoutPosition(
+      getTrailingMargin(mainAxis, parentWidth) + relativePositionMain,
+      trailing[mainAxis]);
+  setLayoutPosition(
+      getLeadingMargin(crossAxis, parentWidth) + relativePositionCross,
+      leading[crossAxis]);
+  setLayoutPosition(
+      getTrailingMargin(crossAxis, parentWidth) + relativePositionCross,
+      trailing[crossAxis]);
+}
+
 YGNode::YGNode()
     : context_(nullptr),
       print_(nullptr),
@@ -237,6 +383,7 @@ YGNode::YGNode()
       nodeType_(YGNodeTypeDefault),
       measure_(nullptr),
       baseline_(nullptr),
+      dirtied_(nullptr),
       style_(gYGNodeStyleDefaults),
       layout_(gYGNodeLayoutDefaults),
       lineIndex_(0),
@@ -254,6 +401,7 @@ YGNode::YGNode(const YGNode& node)
       nodeType_(node.nodeType_),
       measure_(node.measure_),
       baseline_(node.baseline_),
+      dirtied_(node.dirtied_),
       style_(node.style_),
       layout_(node.layout_),
       lineIndex_(node.lineIndex_),
@@ -275,6 +423,7 @@ YGNode::YGNode(
     YGNodeType nodeType,
     YGMeasureFunc measure,
     YGBaselineFunc baseline,
+    YGDirtiedFunc dirtied,
     YGStyle style,
     YGLayout layout,
     uint32_t lineIndex,
@@ -290,6 +439,7 @@ YGNode::YGNode(
       nodeType_(nodeType),
       measure_(measure),
       baseline_(baseline),
+      dirtied_(dirtied),
       style_(style),
       layout_(layout),
       lineIndex_(lineIndex),
@@ -315,6 +465,7 @@ YGNode& YGNode::operator=(const YGNode& node) {
   nodeType_ = node.getNodeType();
   measure_ = node.getMeasure();
   baseline_ = node.getBaseline();
+  dirtied_ = node.getDirtied();
   style_ = node.style_;
   layout_ = node.layout_;
   lineIndex_ = node.getLineIndex();
@@ -369,6 +520,15 @@ void YGNode::resolveDimension() {
   }
 }
 
+YGDirection YGNode::resolveDirection(const YGDirection parentDirection) {
+  if (style_.direction == YGDirectionInherit) {
+    return parentDirection > YGDirectionInherit ? parentDirection
+                                                : YGDirectionLTR;
+  } else {
+    return style_.direction;
+  }
+}
+
 void YGNode::clearChildren() {
   children_.clear();
   children_.shrink_to_fit();
@@ -414,12 +574,19 @@ void YGNode::cloneChildrenIfNeeded() {
 
 void YGNode::markDirtyAndPropogate() {
   if (!isDirty_) {
-    isDirty_ = true;
+    setDirty(true);
     setLayoutComputedFlexBasis(YGUndefined);
     if (parent_) {
       parent_->markDirtyAndPropogate();
     }
   }
+}
+
+void YGNode::markDirtyAndPropogateDownwards() {
+  isDirty_ = true;
+  for_each(children_.begin(), children_.end(), [](YGNodeRef childNode) {
+    childNode->markDirtyAndPropogateDownwards();
+  });
 }
 
 float YGNode::resolveFlexGrow() {
@@ -448,4 +615,131 @@ float YGNode::resolveFlexShrink() {
     return -style_.flex;
   }
   return config_->useWebDefaults ? kWebDefaultFlexShrink : kDefaultFlexShrink;
+}
+
+bool YGNode::isNodeFlexible() {
+  return (
+      (style_.positionType == YGPositionTypeRelative) &&
+      (resolveFlexGrow() != 0 || resolveFlexShrink() != 0));
+}
+
+float YGNode::getLeadingBorder(const YGFlexDirection axis) {
+  if (YGFlexDirectionIsRow(axis) &&
+      style_.border[YGEdgeStart].unit != YGUnitUndefined &&
+      style_.border[YGEdgeStart].value >= 0.0f) {
+    return style_.border[YGEdgeStart].value;
+  }
+
+  return fmaxf(
+      YGComputedEdgeValue(style_.border, leading[axis], &YGValueZero)->value,
+      0.0f);
+}
+
+float YGNode::getTrailingBorder(const YGFlexDirection flexDirection) {
+  if (YGFlexDirectionIsRow(flexDirection) &&
+      style_.border[YGEdgeEnd].unit != YGUnitUndefined &&
+      style_.border[YGEdgeEnd].value >= 0.0f) {
+    return style_.border[YGEdgeEnd].value;
+  }
+
+  return fmaxf(
+      YGComputedEdgeValue(style_.border, trailing[flexDirection], &YGValueZero)
+          ->value,
+      0.0f);
+}
+
+float YGNode::getLeadingPadding(
+    const YGFlexDirection axis,
+    const float widthSize) {
+  if (YGFlexDirectionIsRow(axis) &&
+      style_.padding[YGEdgeStart].unit != YGUnitUndefined &&
+      YGResolveValue(style_.padding[YGEdgeStart], widthSize) >= 0.0f) {
+    return YGResolveValue(style_.padding[YGEdgeStart], widthSize);
+  }
+  return fmaxf(
+      YGResolveValue(
+          *YGComputedEdgeValue(style_.padding, leading[axis], &YGValueZero),
+          widthSize),
+      0.0f);
+}
+
+float YGNode::getTrailingPadding(
+    const YGFlexDirection axis,
+    const float widthSize) {
+  if (YGFlexDirectionIsRow(axis) &&
+      style_.padding[YGEdgeEnd].unit != YGUnitUndefined &&
+      YGResolveValue(style_.padding[YGEdgeEnd], widthSize) >= 0.0f) {
+    return YGResolveValue(style_.padding[YGEdgeEnd], widthSize);
+  }
+  return fmaxf(
+      YGResolveValue(
+          *YGComputedEdgeValue(style_.padding, trailing[axis], &YGValueZero),
+          widthSize),
+      0.0f);
+}
+
+float YGNode::getLeadingPaddingAndBorder(
+    const YGFlexDirection axis,
+    const float widthSize) {
+  return getLeadingPadding(axis, widthSize) + getLeadingBorder(axis);
+}
+
+float YGNode::getTrailingPaddingAndBorder(
+    const YGFlexDirection axis,
+    const float widthSize) {
+  return getTrailingPadding(axis, widthSize) + getTrailingBorder(axis);
+}
+
+bool YGNode::didUseLegacyFlag() {
+  bool didUseLegacyFlag = layout_.didUseLegacyFlag;
+  if (didUseLegacyFlag) {
+    return true;
+  }
+  for (const auto& child : children_) {
+    if (child->layout_.didUseLegacyFlag) {
+      didUseLegacyFlag = true;
+      break;
+    }
+  }
+  return didUseLegacyFlag;
+}
+
+void YGNode::setAndPropogateUseLegacyFlag(bool useLegacyFlag) {
+  config_->useLegacyStretchBehaviour = useLegacyFlag;
+  for_each(children_.begin(), children_.end(), [=](YGNodeRef childNode) {
+    childNode->getConfig()->useLegacyStretchBehaviour = useLegacyFlag;
+  });
+}
+
+void YGNode::setLayoutDoesLegacyFlagAffectsLayout(
+    bool doesLegacyFlagAffectsLayout) {
+  layout_.doesLegacyStretchFlagAffectsLayout = doesLegacyFlagAffectsLayout;
+}
+
+void YGNode::setLayoutDidUseLegacyFlag(bool didUseLegacyFlag) {
+  layout_.didUseLegacyFlag = didUseLegacyFlag;
+}
+
+bool YGNode::isLayoutTreeEqualToNode(const YGNode& node) const {
+  if (children_.size() != node.children_.size()) {
+    return false;
+  }
+  if (layout_ != node.layout_) {
+    return false;
+  }
+  if (children_.size() == 0) {
+    return true;
+  }
+
+  bool isLayoutTreeEqual = true;
+  YGNodeRef otherNodeChildren = nullptr;
+  for (std::vector<YGNodeRef>::size_type i = 0; i < children_.size(); ++i) {
+    otherNodeChildren = node.children_[i];
+    isLayoutTreeEqual =
+        children_[i]->isLayoutTreeEqualToNode(*otherNodeChildren);
+    if (!isLayoutTreeEqual) {
+      return false;
+    }
+  }
+  return isLayoutTreeEqual;
 }
