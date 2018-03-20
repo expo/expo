40f82d9b03aaf262481df68f0dc20274b82e7dad
Eric Samelson <samelson.e@gmail.com>
2018-03-20 01:23:43 +0000

    removed old sdk 26
    fbshipit-source-id: adaad7b

--- android/expoview/src/main/java/abi26_0_0/host/exp/exponent/modules/api/components/camera/tasks/BarCodeScannerAsyncTaskDelegate.java
+++
@@ -1,8 +0,0 @@
-package abi26_0_0.host.exp.exponent.modules.api.components.camera.tasks;
-
-import com.google.android.gms.vision.barcode.Barcode;
-
-public interface BarCodeScannerAsyncTaskDelegate {
-  void onBarCodeRead(Barcode barCode);
-  void onBarCodeScanningTaskCompleted();
-}
--- android/expoview/src/main/java/abi26_0_0/host/exp/exponent/modules/api/components/gesturehandler/ViewConfigurationHelper.java
+++
@@ -1,9 +0,0 @@
-package abi26_0_0.host.exp.exponent.modules.api.components.gesturehandler;
-
-import android.view.View;
-import android.view.ViewGroup;
-
-public interface ViewConfigurationHelper {
-  PointerEventsConfig getPointerEventsConfigForView(View view);
-  View getChildInDrawingOrderAtIndex(ViewGroup parent, int index);
-}
