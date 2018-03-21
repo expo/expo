96ca98879e8726b0398f9260e542fdef3807f2eb
James Ide <ide@expo.io>
2018-03-21 22:19:14 +0000

    Use getViewerUsernameAsync in profile screen instead of reading username from JWT
    We are moving to opaque session tokens that don't contain username data. We do this by reading from GraphQL, which is async. As a result we need the profile screen rendering flow to be async. Since we don't have React Suspense yet, just render a placeholder component while getting the username (this is fast in practice)
    
    fbshipit-source-id: fea6e6e

--- js/screens/ProfileScreen.js
+++ js/screens/ProfileScreen.js
@@ -23,9 +23,9 @@ import ProfileUnauthenticated from '../components/ProfileUnauthenticated';
 import MyProfileContainer from '../containers/MyProfileContainer';
 import OtherProfileContainer from '../containers/OtherProfileContainer';
 
+import getViewerUsernameAsync from '../utils/getViewerUsernameAsync';
 import onlyIfAuthenticated from '../utils/onlyIfAuthenticated';
 import isUserAuthenticated from '../utils/isUserAuthenticated';
-import isCurrentUser from '../utils/isCurrentUser';
 
 @connect((data, props) => ProfileScreen.getDataProps(data, props))
 export default class ProfileScreen extends React.Component {
@@ -63,22 +63,47 @@ export default class ProfileScreen extends React.Component {
 
   static getDataProps(data, props) {
     let isAuthenticated = isUserAuthenticated(data.authTokens, data.session);
-    let isOwnProfile = !props.username || isCurrentUser(data.authTokens, props.username);
 
     return {
       isAuthenticated,
-      isOwnProfile,
       username: props.username,
     };
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      // NOTE: An empty username prop means to display the viewer's profile. We use null to
+      // indicate we don't yet know if this is the viewer's own profile.
+      isOwnProfile: !props.username ? true : null,
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.isOwnProfile !== null) {
+      return;
+    }
+
+    getViewerUsernameAsync().then(
+      username => {
+        this.setState({ isOwnProfile: username === this.props.username });
+      },
+      error => {
+        this.setState({ isOwnProfile: false });
+        console.warn(`There was an error fetching the viewer's username`, error);
+      }
+    );
+  }
+
   render() {
-    if (!this.props.isAuthenticated && this.props.isOwnProfile) {
+    if (this.state.isOwnProfile === null) {
+      return <View style={styles.loadingContainer} />;
+    } else if (!this.props.isAuthenticated && this.state.isOwnProfile) {
       return <ProfileUnauthenticated />;
-    } else if (this.props.isOwnProfile) {
-      return <MyProfileContainer {...this.props} />;
+    } else if (this.state.isOwnProfile) {
+      return <MyProfileContainer {...this.props} isOwnProfile={this.state.isOwnProfile} />;
     } else {
-      return <OtherProfileContainer {...this.props} />;
+      return <OtherProfileContainer {...this.props} isOwnProfile={this.state.isOwnProfile} />;
     }
   }
 }
@@ -223,6 +248,9 @@ class OptionsButtonIOS extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+  },
   buttonContainer: {
     flex: 1,
     flexDirection: 'row',
