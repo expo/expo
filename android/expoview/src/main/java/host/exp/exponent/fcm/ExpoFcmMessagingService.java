9526b469f5eb3a57674ed81459e9209ec28afe17
Jesse Ruder <jesse@expo.io>
2018-03-30 23:00:04 +0000

    Don't register for FCM in BaseExperienceActivity
    fbshipit-source-id: 1512203

--- android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
+++ android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
@@ -3,12 +3,17 @@ package host.exp.exponent.fcm;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import host.exp.exponent.Constants;
 import host.exp.exponent.notifications.PushNotificationHelper;
 
 public class ExpoFcmMessagingService extends FirebaseMessagingService {
 
   @Override
   public void onMessageReceived(RemoteMessage remoteMessage) {
+    if (!Constants.FCM_ENABLED) {
+      return;
+    }
+
     PushNotificationHelper.getInstance().onMessageReceived(this, remoteMessage.getData().get("experienceId"), remoteMessage.getData().get("message"), remoteMessage.getData().get("body"), remoteMessage.getData().get("title"));
   }
 }
