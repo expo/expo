c1ee5bb986e162b3466c3305b7114f4ffb875a1d
jimmylee <send@jimmy.email>
2018-03-31 16:32:20 -0700

    [docs] removes duplicated global table styles

--- docs/components/base/page.js
+++ docs/components/base/page.js
@@ -106,44 +106,6 @@ class Page extends React.Component {
             }}
           />
 
-          {/* Table styles */}
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `
-
-              table {
-                margin-bottom: 4.45rem;
-                font-size: 1rem;
-                line-height: 1.45rem;
-                border-collapse: collapse;
-                border: 1px solid hsla(0,0%,0%,0.12);
-                width: 100%;
-              }
-
-              thead {
-                text-align: left;
-              }
-
-              th, td {
-                text-align: left;
-                border-bottom: 1px solid hsla(0,0%,0%,0.12);
-                font-feature-settings: "tnum";
-                -moz-font-feature-settings: "tnum";
-                -ms-font-feature-settings: "tnum";
-                -webkit-font-feature-settings: "tnum";
-                padding-left: 0.96667rem;
-                padding-right: 0.96667rem;
-                padding-top: 0.725rem;
-                padding-bottom: calc(0.725rem - 1px);
-              }
-
-              th {
-                font-weight: bold;
-              }
-          `,
-            }}
-          />
-
           {/* Fonts */}
           <style
             dangerouslySetInnerHTML={{
