09db2de04e76baa9f147024e2443a0a9aabcfaab
jimmylee <send@jimmy.email>
2018-04-01 02:12:51 -0700

    [docs] updates styles with permalinks

--- docs/components/base/permalink.js
+++ docs/components/base/permalink.js
@@ -1,3 +1,5 @@
+import styled, { keyframes, css } from 'react-emotion';
+
 import * as React from 'react';
 import * as Constants from '~/common/constants';
 import * as Utilities from '~/common/utilities';
@@ -20,6 +22,40 @@ class Permalink extends React.Component {
 
 // TODO(jim):
 // Remove styled-jsx here.
+const STYLES_CONTAINER = css`
+  margin-left: -20px;
+  display: flex;
+  flex-direction: row;
+
+  :hover > .permalink {
+    visibility: visible;
+  }
+
+  @media screen and (max-width: ${Constants.breakpoints.mobile}) {
+    margin-left: 0px;
+  }
+`;
+
+const STYLES_CONTAINER_ANCHOR = css`
+  color: inherit;
+  margin-right: 5px;
+  text-decoration: none;
+  text-align: center;
+  vertical-align: middle;
+  visibility: hidden;
+
+  @media screen and (max-width: ${Constants.breakpoints.mobile}) {
+    margin-left: -20px;
+    padding-left: 5px;
+  }
+`;
+
+const STYLES_CONTAINER_TARGET = css`
+  display: block;
+  margin-top: -100px;
+  visibility: hidden;
+`;
+
 export default props => {
   const component = props.children;
   const children = component.props.children || '';
@@ -31,57 +67,13 @@ export default props => {
 
   return (
     <Permalink component={component} data-components-heading>
-      <div className="container">
-        <span id={id} className="target" />
-        <a href={'#' + id} className="permalink">
+      <div className={STYLES_CONTAINER}>
+        <span id={id} className={STYLES_CONTAINER_TARGET} />
+        <a href={'#' + id} className={`permalink ${STYLES_CONTAINER_ANCHOR}`}>
           <PermalinkIcon />
         </a>
         <div style={{ lineHeight: '1.5em' }}>{children}</div>
       </div>
-
-      <style jsx>
-        {`
-          a {
-            color: inherit;
-            margin-right: 5px;
-            text-decoration: none;
-          }
-
-          .container {
-            margin-left: -20px;
-            display: flex;
-            flex-direction: row;
-          }
-
-          .container:hover > .permalink {
-            visibility: visible;
-          }
-
-          .permalink {
-            text-align: center;
-            vertical-align: middle;
-            visibility: hidden;
-          }
-
-          .target {
-            display: block;
-            margin-top: -100px;
-            visibility: hidden;
-          }
-
-          @media screen and (max-width: ${Constants.breakpoints.mobile}) {
-            .container {
-              margin-left: 0px;
-            }
-
-            .permalink {
-              margin-left: -20px;
-              padding-left: 5px;
-            }
-          }
-        `}
-      </style>
     </Permalink>
   );
 };
-
