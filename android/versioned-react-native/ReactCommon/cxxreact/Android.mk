77d280522266cda60ece3c625df95c0d9e22be28
Eric Samelson <samelson.e@gmail.com>
2018-04-19 00:08:57 +0000

    re-version sdk 27 with error screen fix
    fbshipit-source-id: 23fe3bf

--- android/versioned-react-native/ReactCommon/cxxreact/Android.mk
+++ android/versioned-react-native/ReactCommon/cxxreact/Android.mk
@@ -16,10 +16,10 @@ LOCAL_SRC_FILES := \
   JSCPerfStats.cpp \
   JSCSamplingProfiler.cpp \
   JSCTracing.cpp \
-  JSCUtils.cpp \
   JSIndexedRAMBundle.cpp \
-  ModuleRegistry.cpp \
+  JSCUtils.cpp \
   MethodCall.cpp \
+  ModuleRegistry.cpp \
   NativeToJsBridge.cpp \
   Platform.cpp \
 	RAMBundleRegistry.cpp \
@@ -43,8 +43,8 @@ include $(BUILD_STATIC_LIBRARY)
 $(call import-module,fb)
 $(call import-module,folly)
 $(call import-module,jsc)
-$(call import-module,glog)
 $(call import-module,jschelpers)
+$(call import-module,glog)
 $(call import-module,jsinspector)
 $(call import-module,privatedata)
 
