6280ef25597bf802515e4541af2f4b3ea5c69de0
Eric Samelson <samelson.e@gmail.com>
2018-05-10 23:18:18 +0000

    small rn-maps fixes for SDK 27 minor release
    fbshipit-source-id: 771807a

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/maps/AirMapMarker.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/maps/AirMapMarker.java
@@ -268,7 +268,7 @@ public class AirMapMarker extends AirMapFeature {
       iconBitmapDescriptor = null;
       update();
     } else if (uri.startsWith("http://") || uri.startsWith("https://") ||
-        uri.startsWith("file://")) {
+        uri.startsWith("file://") || uri.startsWith("asset://")) {
       ImageRequest imageRequest = ImageRequestBuilder
           .newBuilderWithSource(Uri.parse(uri))
           .build();
