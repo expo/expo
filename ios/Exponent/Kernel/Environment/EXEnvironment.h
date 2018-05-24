9b1cfd9463cc511c0109cc4c0ad1c3b9b261a9b0
Ben Roth <spongebucket@gmail.com>
2018-05-24 22:02:40 +0000

    Clean up EXEnvironment, remove deprecated shell development config behavior
    fbshipit-source-id: f4d4d01

--- ios/Exponent/Kernel/Environment/EXEnvironment.h
+++ ios/Exponent/Kernel/Environment/EXEnvironment.h
@@ -12,16 +12,55 @@ FOUNDATION_EXPORT NSString * const kEXEmbeddedManifestResourceName;
 
 + (instancetype)sharedEnvironment;
 
+/**
+ *  Whether the app is running as a detached/standalone app (true) or as a browser/Expo Client (false).
+ */
 @property (nonatomic, readonly) BOOL isDetached;
+
+/**
+ *  Whether the app was built with a Debug configuration.
+ */
 @property (nonatomic, readonly) BOOL isDebugXCodeScheme;
 
+/**
+ *  The manifest url of the standalone app (if isDetached == true).
+ */
 @property (nonatomic, readonly, nullable) NSString *standaloneManifestUrl;
+
+/**
+ *  The custom url scheme for the standalone app (if isDetached == true).
+ */
 @property (nonatomic, readonly, nullable) NSString *urlScheme;
+
+/**
+ *  The release channel from which to fetch the standalone app (if isDetached == true).
+ */
 @property (nonatomic, readonly, nonnull) NSString *releaseChannel;
+
+/**
+ *  The `bundleUrl` given by the embedded manifest that shipped with this NSBundle, if any.
+ */
 @property (nonatomic, readonly) NSString * _Nullable embeddedBundleUrl;
+
+/**
+ *  Contains `standaloneManifestUrl`, and may also contain a local development url for a detached project.
+ */
 @property (nonatomic, readonly, nonnull) NSArray *allManifestUrls;
+
+/**
+ *  True by default in ExpoKit apps created with `exp detach`, because the owner of the app needs to
+ *  manually modify their App Id to enable keychain sharing.
+ */
 @property (nonatomic, readonly) BOOL isManifestVerificationBypassed;
+
+/**
+ *  Whether remote updates are allowed at all for this standalone app.
+ */
 @property (nonatomic, readonly) BOOL areRemoteUpdatesEnabled;
+
+/**
+ *  Whether the app is running in a test environment (local Xcode test target, CI, or not at all).
+ */
 @property (nonatomic, assign) EXTestEnvironment testEnvironment;
 
 /**
--- ios/ExponentIntegrationTests/EXEnvironment/EXEnvironmentTests.m
+++ ios/ExponentIntegrationTests/EXEnvironment/EXEnvironmentTests.m
@@ -24,55 +24,55 @@
 
 - (void)testIsServiceConfigRespected
 {
-  [EXShellManagerMocks loadProdServiceConfig];
-  NSDictionary *expectedShellConfig = [EXShellManagerMocks shellConfig];
-  NSString *expectedUrlScheme = [EXShellManagerMocks prodUrlScheme];
-  NSDictionary *expectedEmbeddedManifest = [EXShellManagerMocks embeddedManifest];
-  XCTAssert(_environment.isDetached, @"Shell manager should indicate isDetached == true when a valid standalone config is provided");
-  XCTAssert([_environment.standaloneManifestUrl isEqualToString:expectedShellConfig[@"manifestUrl"]], @"Shell manager should adopt the manifest url specified in the config");
-  XCTAssert([_environment.releaseChannel isEqualToString:@"default"], @"Shell manager should use default release channel when none is specified");
-  XCTAssert([_environment.urlScheme isEqualToString:expectedUrlScheme], @"Shell manager should adopt url scheme %@ when configured", expectedUrlScheme);
-  XCTAssert([_environment.embeddedBundleUrl isEqualToString:expectedEmbeddedManifest[@"bundleUrl"]], @"Shell manager should adopt the bundle url specified in the embedded manifest");
+  [EXEnvironmentMocks loadProdServiceConfig];
+  NSDictionary *expectedShellConfig = [EXEnvironmentMocks shellConfig];
+  NSString *expectedUrlScheme = [EXEnvironmentMocks prodUrlScheme];
+  NSDictionary *expectedEmbeddedManifest = [EXEnvironmentMocks embeddedManifest];
+  XCTAssert(_environment.isDetached, @"EXEnvironment should indicate isDetached == true when a valid standalone config is provided");
+  XCTAssert([_environment.standaloneManifestUrl isEqualToString:expectedShellConfig[@"manifestUrl"]], @"EXEnvironment should adopt the manifest url specified in the config");
+  XCTAssert([_environment.releaseChannel isEqualToString:@"default"], @"EXEnvironment should use default release channel when none is specified");
+  XCTAssert([_environment.urlScheme isEqualToString:expectedUrlScheme], @"EXEnvironment should adopt url scheme %@ when configured", expectedUrlScheme);
+  XCTAssert([_environment.embeddedBundleUrl isEqualToString:expectedEmbeddedManifest[@"bundleUrl"]], @"EXEnvironment should adopt the bundle url specified in the embedded manifest");
 }
 
 - (void)testIsDevDetachConfigRespected
 {
-  [EXShellManagerMocks loadDevDetachConfig];
-  NSString *expectedUrl = [EXShellManagerMocks expoKitDevUrl];
-  NSString *expectedUrlScheme = [EXShellManagerMocks prodUrlScheme];
-  NSDictionary *expectedEmbeddedManifest = [EXShellManagerMocks embeddedManifest];
-  XCTAssert(_environment.isDetached, @"Shell manager should indicate isDetached == true when a dev detach config is provided");
-  XCTAssert([_environment.standaloneManifestUrl isEqualToString:expectedUrl], @"Shell manager should adopt the local dev url when a dev detach config is provided");
-  XCTAssert([_environment.releaseChannel isEqualToString:@"default"], @"Shell manager should use default release channel when a dev detach config is provided");
-  XCTAssert([_environment.urlScheme isEqualToString:expectedUrlScheme], @"Shell manager should adopt url scheme %@ when configured", expectedUrlScheme);
-  XCTAssert([_environment.embeddedBundleUrl isEqualToString:expectedEmbeddedManifest[@"bundleUrl"]], @"Shell manager should adopt the bundle url specified in the embedded manifest");
+  [EXEnvironmentMocks loadDevDetachConfig];
+  NSString *expectedUrl = [EXEnvironmentMocks expoKitDevUrl];
+  NSString *expectedUrlScheme = [EXEnvironmentMocks prodUrlScheme];
+  NSDictionary *expectedEmbeddedManifest = [EXEnvironmentMocks embeddedManifest];
+  XCTAssert(_environment.isDetached, @"EXEnvironment should indicate isDetached == true when a dev detach config is provided");
+  XCTAssert([_environment.standaloneManifestUrl isEqualToString:expectedUrl], @"EXEnvironment should adopt the local dev url when a dev detach config is provided");
+  XCTAssert([_environment.releaseChannel isEqualToString:@"default"], @"EXEnvironment should use default release channel when a dev detach config is provided");
+  XCTAssert([_environment.urlScheme isEqualToString:expectedUrlScheme], @"EXEnvironment should adopt url scheme %@ when configured", expectedUrlScheme);
+  XCTAssert([_environment.embeddedBundleUrl isEqualToString:expectedEmbeddedManifest[@"bundleUrl"]], @"EXEnvironment should adopt the bundle url specified in the embedded manifest");
 }
 
 - (void)testDoesMissingDevDetachUrlThrow
 {
   // local dev detach with nil dev url
   XCTAssertThrows(
-                  [_environment _loadShellConfig:[EXShellManagerMocks shellConfig]
-                                   withInfoPlist:[EXShellManagerMocks infoPlist]
+                  [_environment _loadShellConfig:[EXEnvironmentMocks shellConfig]
+                                   withInfoPlist:[EXEnvironmentMocks infoPlist]
                                withExpoKitDevUrl:nil
-                            withEmbeddedManifest:[EXShellManagerMocks embeddedManifest]
+                            withEmbeddedManifest:[EXEnvironmentMocks embeddedManifest]
                                       isDetached:YES
                               isDebugXCodeScheme:YES
                                     isUserDetach:YES]
-                  , @"Configuring shell manager as a local detached project in Debug mode should throw if no development url is provided");
+                  , @"Configuring EXEnvironment as a local detached project in Debug mode should throw if no development url is provided");
 }
 
 - (void)testDoesProdDetachUseProdUrl
 {
-  NSDictionary *expectedShellConfig = [EXShellManagerMocks shellConfig];
+  NSDictionary *expectedShellConfig = [EXEnvironmentMocks shellConfig];
   [_environment _loadShellConfig:expectedShellConfig
-                   withInfoPlist:[EXShellManagerMocks infoPlist]
-               withExpoKitDevUrl:[EXShellManagerMocks expoKitDevUrl]
-            withEmbeddedManifest:[EXShellManagerMocks embeddedManifest]
+                   withInfoPlist:[EXEnvironmentMocks infoPlist]
+               withExpoKitDevUrl:[EXEnvironmentMocks expoKitDevUrl]
+            withEmbeddedManifest:[EXEnvironmentMocks embeddedManifest]
                       isDetached:YES
               isDebugXCodeScheme:NO
                     isUserDetach:YES];
-  XCTAssert([_environment.standaloneManifestUrl isEqualToString:expectedShellConfig[@"manifestUrl"]], @"Shell manager should ignore the ExpoKit dev url when using a prod build scheme");
+  XCTAssert([_environment.standaloneManifestUrl isEqualToString:expectedShellConfig[@"manifestUrl"]], @"EXEnvironment should ignore the ExpoKit dev url when using a prod build scheme");
 }
 
 @end
