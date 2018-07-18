6241dfa238e328d3f5170dc16fcfd806af0c2129
Jesse Ruder <jesse@expo.io>
2018-07-18 19:09:08 +0000

    Remove sdk 22
    fbshipit-source-id: 995b97e

--- android/expoview/src/main/java/abi22_0_0/host/exp/exponent/modules/api/components/gesturehandler/ViewConfigurationHelper.java
+++
@@ -1,9 +0,0 @@
-package abi22_0_0.host.exp.exponent.modules.api.components.gesturehandler;
-
-import android.view.View;
-import android.view.ViewGroup;
-
-public interface ViewConfigurationHelper {
-  PointerEvents getPointerEventsConfigForView(View view);
-  View getChildInDrawingOrderAtIndex(ViewGroup parent, int index);
-}
