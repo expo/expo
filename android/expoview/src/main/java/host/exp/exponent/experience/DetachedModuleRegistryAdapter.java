48f32d3a263310b100bff00fda46c18a6fd9b5be
Jesse Ruder <jesse@expo.io>
2018-07-27 00:12:50 +0000

    Fix constants in detach
    fbshipit-source-id: 3eafcf3

--- android/expoview/src/main/java/host/exp/exponent/experience/DetachedModuleRegistryAdapter.java
+++ android/expoview/src/main/java/host/exp/exponent/experience/DetachedModuleRegistryAdapter.java
@@ -1,5 +1,8 @@
 package host.exp.exponent.experience;
 
+import android.content.pm.PackageManager;
+import android.support.annotation.Nullable;
+
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 
@@ -10,8 +13,11 @@ import java.util.Map;
 
 import expo.core.ModuleRegistry;
 import expo.core.ModuleRegistryProvider;
+import expo.interfaces.constants.ConstantsInterface;
+import expo.modules.constants.ConstantsService;
 import host.exp.exponent.kernel.ExperienceId;
 import host.exp.exponent.utils.ScopedContext;
+import versioned.host.exp.exponent.modules.universal.ConstantsBinding;
 import versioned.host.exp.exponent.modules.universal.ExpoModuleRegistryAdapter;
 
 public class DetachedModuleRegistryAdapter extends ExpoModuleRegistryAdapter {
@@ -20,11 +26,14 @@ public class DetachedModuleRegistryAdapter extends ExpoModuleRegistryAdapter {
   }
 
   @Override
-  public List<NativeModule> createNativeModules(ScopedContext scopedContext, ExperienceId experienceId, Map<String, Object> experienceProperties, JSONObject manifest, List<NativeModule> otherModules) {
+  public List<NativeModule> createNativeModules(final ScopedContext scopedContext, ExperienceId experienceId, Map<String, Object> experienceProperties, JSONObject manifest, List<NativeModule> otherModules) {
     ReactApplicationContext reactApplicationContext = (ReactApplicationContext) scopedContext.getContext();
 
     // We only use React application context, because we're detached -- no scopes
     ModuleRegistry moduleRegistry = mModuleRegistryProvider.get(reactApplicationContext);
+    
+    moduleRegistry.registerInternalModule(new ConstantsBinding(scopedContext, experienceProperties, manifest));
+
     return getNativeModulesFromModuleRegistry(reactApplicationContext, moduleRegistry);
   }
 }
