724d283319af5a44ad3b6f3263a9f1d4c98d5c27
Nick Novitski <nick@expo.io>
2018-08-10 17:43:31 -0700

    Replace kubernetes deployment scripts

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env bash
 
-set -xeo pipefail
+set -xeuo pipefail
 
 environment=$1
-
-if [ "$environment" == "production" ]; then
-  export INGRESS_HOSTNAME=docs.expo.io
-elif [ "$environment" == "staging" ]; then
-  export INGRESS_HOSTNAME=staging.docs.expo.io
-else
-  echo "Unrecognized environment $environment"
-  exit 1
-fi
-
 export TAG=$2
 
-echo "Checking for image..."
-
-if ! gcloud container images describe "gcr.io/exponentjs/exponent-docs-v2:$TAG"; then
-  echo "Unable to find image tagged with $TAG"
-  exit 2
-fi
-
-echo "Environment set, found image, deploying..."
-
-envsubst < ./docs.k8s.template.yml | kubectl apply --namespace $environment -f -
+nix run expo.k8s-services.docs.$environment.deploy --command deploy
