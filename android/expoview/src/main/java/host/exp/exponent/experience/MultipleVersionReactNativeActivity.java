c43d4ba151c4bf71817996ef8b66a19ec9f7d2a9
Stanisław Chmiela <sjchmiela@gmail.com>
2018-08-13 19:46:42 +0200

    wip

--- android/expoview/src/main/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
+++ android/expoview/src/main/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
@@ -6,8 +6,8 @@ package host.exp.exponent.experience;
 public class MultipleVersionReactNativeActivity extends ReactNativeActivity
     implements
     // abi30_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
-    abi29_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
-//    abi28_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
+    abi29_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
+   abi28_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 //    abi27_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
 //    abi26_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
 //    abi25_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
