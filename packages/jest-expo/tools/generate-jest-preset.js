b59941f57a923d4bafe96163dd50fa60431a90bb
James Ide <ide@expo.io>
2018-08-30 23:55:27 +0000

    Get Expo SDK JS tests passing in OSS
    - Changed the Jest preset to work with Yarn workspaces (regular node_modules resolution)
    - Updated the Jest-preset generator to make the aforementioned preset
    - Added a .babelrc to the workspace root so that it applies to all files by default -- would like to change this though with Babel 7, perhaps
    - Skipped importing modules that cause issues in the generic import test
    
    Verified that tests pass in Universe CI
    
    Closes expo/expo#2157
    
    fbshipit-source-id: 142a295

--- packages/jest-expo/tools/generate-jest-preset.js
+++ packages/jest-expo/tools/generate-jest-preset.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 function generateJestPreset() {
   // Load the React Native Jest preset
-  const rnLabPath = path.resolve(__dirname, '../../../exponent/react-native-lab');
+  const rnLabPath = path.resolve(__dirname, '../../../react-native-lab');
   const rnJestPresetPath = path.join(rnLabPath, 'react-native/jest-preset.json');
   const rnJestPreset = require(rnJestPresetPath);
 
@@ -21,25 +21,18 @@ function generateJestPreset() {
   if (!expoJestPreset.moduleNameMapper) {
     expoJestPreset.moduleNameMapper = {};
   }
-  expoJestPreset.moduleNameMapper['^react$'] = '<rootDir>/node_modules/react';
-  expoJestPreset.moduleNameMapper['^expo-([a-z-]+)$'] = '<rootDir>/node_modules/expo-$1';
-  expoJestPreset.moduleNameMapper['^react-native$'] = '<rootDir>/node_modules/react-native';
-  expoJestPreset.moduleNameMapper['^react-native/(.*)$'] = '<rootDir>/node_modules/react-native/$1';
-  expoJestPreset.moduleNameMapper['^expo-react-native-adapter$'] =
-    '<rootDir>/node_modules/expo-react-native-adapter';
 
   if (!expoJestPreset.transform) {
     expoJestPreset.transform = {};
   }
 
-  const defaultAssetNamePattern = "^[./a-zA-Z0-9$_-]+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$";
+  const defaultAssetNamePattern = '^[./a-zA-Z0-9$_-]+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$';
   assert(expoJestPreset.transform.hasOwnProperty(defaultAssetNamePattern));
   delete expoJestPreset.transform[defaultAssetNamePattern];
 
   const assetNamePattern =
-    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$'
-  expoJestPreset.transform[assetNamePattern] =
-    '<rootDir>/node_modules/jest-expo/src/assetFileTransformer.js';
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$';
+  expoJestPreset.transform[assetNamePattern] = 'jest-expo/src/assetFileTransformer.js';
 
   assert(Array.isArray(expoJestPreset.transformIgnorePatterns));
   assert.deepEqual(expoJestPreset.transformIgnorePatterns, [
@@ -51,8 +44,12 @@ function generateJestPreset() {
 
   if (!expoJestPreset.setupFiles) {
     expoJestPreset.setupFiles = [];
+  } else {
+    expoJestPreset.setupFiles = expoJestPreset.setupFiles.map(setupFile =>
+      setupFile.replace(/^<rootDir>\/node_modules\//, '')
+    );
   }
-  expoJestPreset.setupFiles.push('<rootDir>/node_modules/jest-expo/src/setup.js');
+  expoJestPreset.setupFiles.push('jest-expo/src/setup.js');
 
   // Save the Expo Jest preset
   fs.writeFileSync(
