e9ab932341b15bd2bb286083cfae30e15fa8d096
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2018-09-07 09:09:03 +0000

    Notify modules of experience backgrounding/foregrounding (#3106)
    fbshipit-source-id: bb2ef5b

---
+++ ios/Exponent/Versioned/Core/UniversalModules/EXScopedReactNativeAdapter.h
@@ -0,0 +1,7 @@
+// Copyright © 2018 650 Industries. All rights reserved.
+
+#import <EXReactNativeAdapter/EXReactNativeAdapter.h>
+
+@interface EXScopedReactNativeAdapter : EXReactNativeAdapter
+
+@end
