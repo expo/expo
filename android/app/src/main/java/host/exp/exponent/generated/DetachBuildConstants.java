2c7749b92acdec6e4acc9c6aa339264d80c99c03
szymon <szymon.kapala@swmansion.com>
2018-09-26 15:10:14 +0200

    fixed return url in example with expokit

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp9d1ea7e1de174f31828a156ea7a38e11://192.168.83.158:19000";
 
 }
