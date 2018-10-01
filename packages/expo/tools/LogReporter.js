c6298b3c1bc5e68fc53cdc7a72eebb5ce0c7b0ad
James Ide <ide@expo.io>
2018-10-01 14:49:49 -0700

    [dev] Run all errors through serialize-error before logging
    JS Error objects don't include their name, message, and stack when JSON-serialized so we need to explicitly include those fields, which serialize-error takes care of. Parts of Metro do this but not the `bundling_error` code path, which results in Expo CLI's printing `undefined`, which is useless.
    
    See https://github.com/facebook/metro/commit/b7248380a642737d60ba2eb819b904cdbe63556f for a related fix.

--- packages/expo/tools/LogReporter.js
+++ packages/expo/tools/LogReporter.js
@@ -1,7 +1,13 @@
+const serializeError = require('serialize-error');
+
 class LogReporter {
   update(event) {
     if (event.type === 'bundle_built') {
       event.type = 'bundle_build_done';
+    }
+
+    if (event.error instanceof Error) {
+      event.error = serializeError(event.error);
     }
 
     console.log(JSON.stringify(event));
