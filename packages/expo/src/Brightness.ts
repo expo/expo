6cbeb488689eaa6013710c152522b2c1a4409e77
Eric Samelson <samelson.e@gmail.com>
2018-10-09 19:01:42 -0700

    [android][sdk] first pass over java and ts code

--- packages/expo/src/Brightness.ts
+++ packages/expo/src/Brightness.ts
@@ -1,19 +1,25 @@
 import { NativeModules, Platform } from 'react-native';
 
+export enum BrightnessMode {
+  UNKNOWN = 0,
+  AUTOMATIC = 1,
+  MANUAL = 2,
+};
+
 export async function getBrightnessAsync(): Promise<number> {
-  return await NativeModules.ExponentBrightness.getBrightnessAsync();
+  return await NativeModules.ExpoBrightness.getBrightnessAsync();
 }
 
 export async function setBrightnessAsync(brightnessValue: number): Promise<void> {
   brightnessValue = Math.max(0, Math.min(brightnessValue, 1));
-  return await NativeModules.ExponentBrightness.setBrightnessAsync(brightnessValue);
+  return await NativeModules.ExpoBrightness.setBrightnessAsync(brightnessValue);
 }
 
 export async function getSystemBrightnessAsync(): Promise<number> {
   if (Platform.OS !== 'android') {
     return await getBrightnessAsync();
   }
-  return await NativeModules.ExponentBrightness.getSystemBrightnessAsync();
+  return await NativeModules.ExpoBrightness.getSystemBrightnessAsync();
 }
 
 export async function setSystemBrightnessAsync(brightnessValue: number): Promise<void> {
@@ -21,6 +27,30 @@ export async function setSystemBrightnessAsync(brightnessValue: number): Promise
   if (Platform.OS !== 'android') {
     return await setBrightnessAsync(brightnessValue);
   } else {
-    return await NativeModules.ExponentBrightness.setSystemBrightnessAsync(brightnessValue);
+    return await NativeModules.ExpoBrightness.setSystemBrightnessAsync(brightnessValue);
+  }
+}
+
+export async function useSystemBrightnessAsync(): Promise<void> {
+  if (Platform.OS !== 'android') {
+    return;
+  } else {
+    return await NativeModules.ExpoBrightness.useSystemBrightnessAsync();
+  }
+}
+
+export async function getSystemBrightnessModeAsync(): Promise<BrightnessMode> {
+  if (Platform.OS !== 'android') {
+    return BrightnessMode.UNKNOWN;
+  } else {
+    return await NativeModules.ExpoBrightness.getSystemBrightnessAsync();
+  }
+}
+
+export async function setSystemBrightnessModeAsync(brightnessMode: BrightnessMode): Promise<void> {
+  if (Platform.OS !== 'android' || brightnessMode === BrightnessMode.UNKNOWN) {
+    return;
+  } else {
+    return await NativeModules.ExpoBrightness.setSystemBrightnessModeAsync(brightnessMode);
   }
 }
