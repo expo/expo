19c293b99364bbe8c42726e15976c1c2d6c78f92
Seifer <alexey.shikov@gmail.com>
2018-10-18 16:46:25 +0300

    Use full YUV to NV21 conversion (apply U channel)

--- android/cameraview/cameraview/src/main/api21/com/google/android/cameraview/Camera2.java
+++ android/cameraview/cameraview/src/main/api21/com/google/android/cameraview/Camera2.java
@@ -204,16 +204,19 @@ class Camera2 extends CameraViewImpl implements MediaRecorder.OnInfoListener, Me
     private static byte[] YUV_420_888toNV21(Image image) {
         byte[] nv21;
         ByteBuffer yBuffer = image.getPlanes()[0].getBuffer();
+        ByteBuffer uBuffer = image.getPlanes()[1].getBuffer();
         ByteBuffer vBuffer = image.getPlanes()[2].getBuffer();
 
         int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
         int vSize = vBuffer.remaining();
 
-        nv21 = new byte[ySize + vSize];
+        nv21 = new byte[ySize + uSize + vSize];
 
         // U and V are swapped
         yBuffer.get(nv21, 0, ySize);
         vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
 
         return nv21;
     }
