a05c34377c31649d694abdb3df9253b987dcd1e8
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2018-10-24 13:05:00 +0200

    Add EXFontScalersManager to centralize UIFont method swizzling (#2480)
    * Remove swizzling UIFont method implementation in lifecycle methods
    
    * Move UIFont+EXFontLoader to a separate EXFontScaler
    
    * Create EXFontScalersManager singleton module for centralized swizzling UIFont method implementation
    
    * Use EXFontScalersManager in EXFontLoader
    
    * Backport unversioned EXFont changes to ABI30_0_0
    
    * Run pod install after all the changes
    
    * Run pod install
    
    * Move initializeCurrentFontScalersOnce to the only method where it is used
    
    * Fix processor spelling in (ABI30_0_0)?EXFont

---
+++ packages/expo-font/ios/EXFont/Singletons/EXFontScalersManager.h
@@ -0,0 +1,8 @@
+// Copyright 2018-present 650 Industries. All rights reserved.
+
+#import <EXCore/EXSingletonModule.h>
+#import <EXFontInterface/EXFontScalersManagerInterface.h>
+
+@interface EXFontScalersManager : EXSingletonModule <EXFontScalersManagerInterface>
+
+@end
