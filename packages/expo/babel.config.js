922d69d35241ca0bfd7da85137be021812c716df
James Ide <ide@expo.io>
2018-10-26 16:12:41 -0700

    [expo] Add back lazy imports for the umbrella Expo.js module
    This compiles the Expo.js module (and only that module) with the import/export transform with the "lazy" option enabled. This way apps don't pay the evaluation cost of modules they don't use.
    
    Test plan: Unit tests still work with the extra plugin enabled first.

--- packages/expo/babel.config.js
+++ packages/expo/babel.config.js
@@ -1,6 +1,12 @@
+const assert = require('assert');
+const process = require('process');
+
 module.exports = function(api) {
+  assert.equal(process.env.NODE_ENV, 'test');
   api.cache(true);
+
   return {
+    plugins: [['@babel/plugin-transform-modules-commonjs', { lazy: true }]],
     presets: ['babel-preset-expo'],
   };
 };
