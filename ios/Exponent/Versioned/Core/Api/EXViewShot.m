7ca7b35334cb92c62a1ff3398fc04d6c8280be4b
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2018-10-26 11:39:13 +0200

    [ios][android][sdk][ncl] Upgrade react-native-view-shot to 2.5.0 (#2518)
    * [android] Upgrade react-native-view-shot to v2.5.0
    
    * [ios] Upgrade react-native-view-shot to v2.5.0
    
    * [sdk] Upgrade takeSnapshotAsync to use react-native-view-shot
    
    * [changelog] Add changelog entry
    
    * [android] Scope createTempFile properly
    
    * [ncl] Add react-native-view-shot example
    
    * [sdk] Fix review comment

--- ios/Exponent/Versioned/Core/Api/EXViewShot.m
+++ ios/Exponent/Versioned/Core/Api/EXViewShot.m
@@ -1,15 +1,15 @@
-
 #import "EXViewShot.h"
 #import <AVFoundation/AVFoundation.h>
+#import <React/RCTLog.h>
 #import <React/UIView+React.h>
 #import <React/RCTUtils.h>
 #import <React/RCTConvert.h>
+#import <React/RCTScrollView.h>
 #import <React/RCTUIManager.h>
+#if __has_include(<React/RCTUIManagerUtils.h>)
+#import <React/RCTUIManagerUtils.h>
+#endif
 #import <React/RCTBridge.h>
-
-#import "EXModuleRegistryBinding.h"
-#import <EXFileSystemInterface/EXFileSystemInterface.h>
-
 
 @implementation EXViewShot
 
@@ -19,99 +19,155 @@ RCT_EXPORT_MODULE(RNViewShot)
 
 - (dispatch_queue_t)methodQueue
 {
-  return self.bridge.uiManager.methodQueue;
+  return RCTGetUIManagerQueue();
 }
 
-// forked from RN implementation
-// https://github.com/facebook/react-native/blob/f35b372883a76b5666b016131d59268b42f3c40d/React/Modules/RCTUIManager.m#L1367
+RCT_EXPORT_METHOD(captureScreen: (NSDictionary *)options
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) 
+{
+  [self captureRef: [NSNumber numberWithInt:-1] withOptions:options resolve:resolve reject:reject];
+}
 
-RCT_EXPORT_METHOD(takeSnapshot:(nonnull NSNumber *)target
+RCT_EXPORT_METHOD(releaseCapture:(nonnull NSString *)uri)
+{
+  NSString *directory = [NSTemporaryDirectory() stringByAppendingPathComponent:@"ReactNative"];
+  // Ensure it's a valid file in the tmp directory
+  if ([uri hasPrefix:directory] && ![uri isEqualToString:directory]) {
+    NSFileManager *fileManager = [NSFileManager new];
+    if ([fileManager fileExistsAtPath:uri]) {
+      [fileManager removeItemAtPath:uri error:NULL];
+    }
+  }
+}
+
+RCT_EXPORT_METHOD(captureRef:(nonnull NSNumber *)target
                   withOptions:(NSDictionary *)options
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
   [self.bridge.uiManager addUIBlock:^(__unused RCTUIManager *uiManager, NSDictionary<NSNumber *, UIView *> *viewRegistry) {
-    
+
     // Get view
     UIView *view;
-    view = viewRegistry[target];
+
+    if ([target intValue] == -1) {
+      UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+      view = window.rootViewController.view;
+    } else {
+      view = viewRegistry[target];
+    }
+
     if (!view) {
       reject(RCTErrorUnspecified, [NSString stringWithFormat:@"No view found with reactTag: %@", target], nil);
       return;
     }
-    
+
     // Get options
     CGSize size = [RCTConvert CGSize:options];
-    NSString *format = [RCTConvert NSString:options[@"format"] ?: @"png"];
-    NSString *result = [RCTConvert NSString:options[@"result"] ?: @"file"];
-    
+    NSString *format = [RCTConvert NSString:options[@"format"]];
+    NSString *result = [RCTConvert NSString:options[@"result"]];
+    BOOL snapshotContentContainer = [RCTConvert BOOL:options[@"snapshotContentContainer"]];
+
     // Capture image
-    if (size.width < 0.1 || size.height < 0.1) {
-      size = view.bounds.size;
-    }
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
-    BOOL success = [view drawViewHierarchyInRect:(CGRect){CGPointZero, size} afterScreenUpdates:YES];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    if (!success || !image) {
-      reject(RCTErrorUnspecified, @"Failed to capture view snapshot", nil);
-      return;
-    }
-    
-    // Convert image to data (on a background thread)
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-      
-      NSData *data;
-      if ([format isEqualToString:@"png"]) {
-        data = UIImagePNGRepresentation(image);
-      } else if ([format isEqualToString:@"jpeg"] || [format isEqualToString:@"jpg"]) {
-        CGFloat quality = [RCTConvert CGFloat:options[@"quality"] ?: @1];
-        data = UIImageJPEGRepresentation(image, quality);
-      } else {
-        reject(RCTErrorUnspecified, [NSString stringWithFormat:@"Unsupported image format: %@. Try one of: png | jpg | jpeg", format], nil);
+    BOOL success;
+
+    UIView* rendered;
+    UIScrollView* scrollView;
+    if (snapshotContentContainer) {
+      if (![view isKindOfClass:[RCTScrollView class]]) {
+        reject(RCTErrorUnspecified, [NSString stringWithFormat:@"snapshotContentContainer can only be used on a RCTScrollView. instead got: %@", view], nil);
         return;
       }
-      
+      RCTScrollView* rctScrollView = view;
+      scrollView = rctScrollView.scrollView;
+      rendered = scrollView;
+    }
+    else {
+      rendered = view;
+    }
+
+    if (size.width < 0.1 || size.height < 0.1) {
+      size = snapshotContentContainer ? scrollView.contentSize : view.bounds.size;
+    }
+    if (size.width < 0.1 || size.height < 0.1) {
+      reject(RCTErrorUnspecified, [NSString stringWithFormat:@"The content size must not be zero or negative. Got: (%g, %g)", size.width, size.height], nil);
+      return;
+    }
+
+    CGPoint savedContentOffset;
+    CGRect savedFrame;
+    if (snapshotContentContainer) {
+      // Save scroll & frame and set it temporarily to the full content size
+      savedContentOffset = scrollView.contentOffset;
+      savedFrame = scrollView.frame;
+      scrollView.contentOffset = CGPointZero;
+      scrollView.frame = CGRectMake(0, 0, scrollView.contentSize.width, scrollView.contentSize.height);
+    }
+
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    
+    success = [rendered drawViewHierarchyInRect:(CGRect){CGPointZero, size} afterScreenUpdates:YES];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    if (snapshotContentContainer) {
+      // Restore scroll & frame
+      scrollView.contentOffset = savedContentOffset;
+      scrollView.frame = savedFrame;
+    }
+
+    if (!success) {
+      reject(RCTErrorUnspecified, @"The view cannot be captured. drawViewHierarchyInRect was not successful. This is a potential technical or security limitation.", nil);
+      return;
+    }
+
+    if (!image) {
+      reject(RCTErrorUnspecified, @"Failed to capture view snapshot. UIGraphicsGetImageFromCurrentImageContext() returned nil!", nil);
+      return;
+    }
+
+    // Convert image to data (on a background thread)
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+
+      NSData *data;
+      if ([format isEqualToString:@"jpg"]) {
+        CGFloat quality = [RCTConvert CGFloat:options[@"quality"]];
+        data = UIImageJPEGRepresentation(image, quality);
+      }
+      else {
+        data = UIImagePNGRepresentation(image);
+      }
+
       NSError *error = nil;
       NSString *res = nil;
-      if ([result isEqualToString:@"file"]) {
-        // Save to a temp file
-        NSString *extension = [@"." stringByAppendingString:format];
-        id<EXFileSystemInterface> fileSystem = [self.bridge.scopedModules.moduleRegistry getModuleImplementingProtocol:@protocol(EXFileSystemInterface)];
-        if (!fileSystem) {
-          reject(@"E_MISSING_MODULE", @"No FileSystem module.", nil);
-          return;
-        }
-        NSString *directory = [fileSystem.cachesDirectory stringByAppendingPathComponent:@"ViewShot"];
-        [fileSystem ensureDirExistsWithPath:directory];
-        NSString *path = [fileSystem generatePathInDirectory:directory withExtension:extension];
-        if (path) {
-          if ([data writeToFile:path options:(NSDataWritingOptions)0 error:&error]) {
-            res = [NSURL fileURLWithPath:path].absoluteString;
-          }
-        }
-      }
-      else if ([result isEqualToString:@"base64"]) {
+      if ([result isEqualToString:@"base64"]) {
         // Return as a base64 raw string
         res = [data base64EncodedStringWithOptions: NSDataBase64Encoding64CharacterLineLength];
       }
       else if ([result isEqualToString:@"data-uri"]) {
         // Return as a base64 data uri string
         NSString *base64 = [data base64EncodedStringWithOptions: NSDataBase64Encoding64CharacterLineLength];
-        res = [NSString stringWithFormat:@"data:image/%@;base64,%@", format, base64];
+        NSString *imageFormat = ([format isEqualToString:@"jpg"]) ? @"jpeg" : format;
+        res = [NSString stringWithFormat:@"data:image/%@;base64,%@", imageFormat, base64];
       }
       else {
-        reject(RCTErrorUnspecified, [NSString stringWithFormat:@"Unsupported result: %@. Try one of: file | base64 | data-uri", result], nil);
-        return;
+        // Save to a temp file
+        NSString *path = RCTTempFilePath(format, &error);
+        if (path && !error) {
+          if ([data writeToFile:path options:(NSDataWritingOptions)0 error:&error]) {
+            res = path;
+          }
+        }
       }
-      if (res != nil) {
+
+      if (res && !error) {
         resolve(res);
         return;
       }
-      
+
       // If we reached here, something went wrong
-      if (error != nil) reject(RCTErrorUnspecified, error.localizedDescription, error);
+      if (error) reject(RCTErrorUnspecified, error.localizedDescription, error);
       else reject(RCTErrorUnspecified, @"viewshot unknown error", nil);
     });
   }];
--- packages/expo/src/takeSnapshotAsync.ts
+++ packages/expo/src/takeSnapshotAsync.ts
@@ -1,32 +1,20 @@
 import * as React from 'react';
-import { NativeModules, findNodeHandle } from 'react-native';
-
-const { RNViewShot } = NativeModules;
+import { captureRef } from 'react-native-view-shot';
 
 type ReactNativeNodeHandle = number;
 
 type SnapshotOptions = {
   width?: number;
   height?: number;
-  format?: 'png' | 'jpg' | 'jpeg' | 'webm';
-  quality?: number;
-  result?: 'file' | 'base64' | 'data-uri';
+  format: 'png' | 'jpg' | 'raw' | 'webm';
+  quality: number;
+  snapshotContentContainer: boolean;
+  result: "tmpfile" | "base64" | "data-uri" | "zip-base64";
 };
 
 export default async function takeSnapshotAsync(
   node: ReactNativeNodeHandle | React.Component,
   options?: SnapshotOptions
 ): Promise<string> {
-  let handle = typeof node === 'number' ? node : _getNodeHandle(node);
-  return RNViewShot.takeSnapshot(handle, options);
-}
-
-function _getNodeHandle(component: React.Component): ReactNativeNodeHandle {
-  let handle = findNodeHandle(component);
-  if (handle === null) {
-    throw new Error(
-      `Could not find the React node handle for the component to snapshot: ${component}`
-    );
-  }
-  return handle;
+  return captureRef(node, options);
 }
