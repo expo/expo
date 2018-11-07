460c75d0c8122cbc4f889a41a98e766d8aee2436
James Ide <ide@expo.io>
2018-11-06 18:03:31 -0800

    [ts] Add TypeScript+Jest to expo-module-scripts and migrate expo-sms to TS
    expo-module-scripts now includes a base tsconfig.base.json file and a base babel.config.base.js file for all unimodules in this repo. This ensures that we can make a change to tsconfig.base.json and have it apply to all packages so that our attention is not split across multiple different tsconfig files.
    
    expo-module-scripts also sets up ts-jest and configures TypeScript and Babel to run as part of Jest tests. During Jest tests, Jest will use ts-jest to compile TS to ES2018 JS. Then it will use Babel to compile ES2018 JS using babel-preset-expo -- this matches the same transform as used for devices, helping us verify that the preset does not introduce bugs into our modules.
    
    Because the Jest tests run Babel, we need a Babel configuration file in each module. To keep things simple, I added a one-line configuration file that refers back to expo-module-scripts. This way, we have one source of truth for all unimodules.
    
    Test plan: Ran `yarn build` and `yarn test` in the expo-sms package.

---
+++ packages/expo-module-scripts/jest-preset.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+const jestExpoPreset = require('jest-expo/jest-preset');
+const path = require('path');
+const { jsWithBabel: tsJestPreset } = require('ts-jest/presets');
+
+module.exports = _createJestPresetSafe();
+
+function _createJestPresetSafe() {
+  // Explicitly catch and log errors since Jest sometimes suppresses error messages
+  try {
+    return _createJestPreset();
+  } catch (e) {
+    console.error(`${e.name}: ${e.message}`);
+    throw e;
+  }
+}
+
+function _createJestPreset() {
+  // Jest does not support chained presets so we flatten this preset before exporting it
+  return {
+    ...jestExpoPreset,
+    clearMocks: true,
+    roots: ['<rootDir>/src'],
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    testMatch: tsJestPreset.testMatch,
+    moduleFileExtensions: tsJestPreset.moduleFileExtensions,
+    globals: {
+      'ts-jest': {
+        tsConfig: _createTypeScriptConfiguration(),
+        babelConfig: _createBabelConfiguration(),
+      },
+    },
+  };
+}
+
+function _createBabelConfiguration() {
+  // "true" signals that ts-jest should use Babel's default configuration lookup, which will use the
+  // configuration file written above
+  return true;
+}
+
+function _createTypeScriptConfiguration() {
+  let tsConfigFileName = 'tsconfig.json';
+  // The path to tsconfig.json is resolved relative to cwd
+  let tsConfigPath = path.resolve(tsConfigFileName);
+
+  let jestTsConfig = {
+    extends: tsConfigPath,
+    compilerOptions: {
+      // Explicitly specify "module" so that ts-jest doesn't provide its default
+      module: 'es2015',
+    },
+  };
+
+  let jestTsConfigJson = JSON.stringify(jestTsConfig, null, 2);
+  // The TypeScript configuration needs to be under the project directory so that TypeScript finds
+  // type declaration packages that are installed in the project or workspace root (writing it to a
+  // temporary directory would not work, for example)
+  let jestTsConfigPath = path.join('.expo', 'tsconfig.jest.json');
+
+  fs.mkdirSync(path.dirname(jestTsConfigPath), { recursive: true });
+  fs.writeFileSync(jestTsConfigPath, jestTsConfigJson);
+  return jestTsConfigPath;
+}
