13abaa4d64f055f659686c1e184464376cc93866
Evan Bacon <baconbrix@gmail.com>
2018-11-09 13:19:19 -0800

    Simplify PR changes

--- apps/native-component-list/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import App from './src/App';
- import renderer from 'react-test-renderer';
- it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
--- apps/native-component-list/src/App.web.js
+++ apps/native-component-list/src/App.web.js
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Constants } from 'expo-constants';
-
 class App extends React.Component {
-  componentDidMount() {
-    // TODO: Bacon: getters aren't working. Constants.name is returning `undefined`
-    console.log(Constants.name, Constants);
-  }
   render() {
     return (
       <View style={styles.container}>
@@ -24,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
