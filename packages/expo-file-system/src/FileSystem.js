ad398dab9ac0c411fc166ffaef08aa4a736e7658
Evan Bacon <baconbrix@gmail.com>
2018-11-12 12:24:28 -0800

    [Web][Unimodule] Added web shim for FileSystem (#2674)
    * added web shim to `ExponentFileSystem`
    * moved `index` to `src/FileSystem`
    * moved NativeModule into seperate file
    * added web shim for ExponentFileSystem
    * fixed general `FileSystem` format issues
    * removed `/FileSystem` in favor of `/src/FileSystem`
    * exporting module as an object literal
    * added errors to methods that aren't implemented

--- packages/expo-file-system/src/FileSystem.js
+++ packages/expo-file-system/src/FileSystem.js
@@ -1,9 +1,8 @@
 // @flow
 
-import { NativeModulesProxy, EventEmitter } from 'expo-core';
+import { EventEmitter } from 'expo-core';
 import UUID from 'uuid-js';
-
-const FS = NativeModulesProxy.ExponentFileSystem;
+import FS from './ExponentFileSystem';
 
 const normalizeEndingSlash = p => p.replace(/\/*$/, '') + '/';
 
@@ -15,9 +14,9 @@ export const cacheDirectory = FS.cacheDirectory;
 export const bundledAssets = FS.bundledAssets;
 export const bundleDirectory = FS.bundleDirectory;
 export const EncodingTypes = {
-  UTF8: "utf8",
-  Base64: "base64",
-}
+  UTF8: 'utf8',
+  Base64: 'base64',
+};
 
 type FileInfo =
   | {
@@ -32,18 +31,16 @@ type FileInfo =
       isDirectory: false,
     };
 
-export type EncodingType =
-  | typeof EncodingTypes.UTF8
-  | typeof EncodingTypes.Base64
+export type EncodingType = typeof EncodingTypes.UTF8 | typeof EncodingTypes.Base64;
 
 export type ReadingOptions = {
   encoding?: EncodingType,
   position?: number,
-  length?: number
-}
+  length?: number,
+};
 export type WritingOptions = {
   encoding?: EncodingType,
-}
+};
 
 export function getInfoAsync(fileUri: string, options: { md5?: boolean } = {}): Promise<FileInfo> {
   return FS.getInfoAsync(fileUri, options);
@@ -53,7 +50,11 @@ export function readAsStringAsync(fileUri: string, options?: ReadingOptions): Pr
   return FS.readAsStringAsync(fileUri, options || {});
 }
 
-export function writeAsStringAsync(fileUri: string, contents: string, options?: WritingOptions): Promise<void> {
+export function writeAsStringAsync(
+  fileUri: string,
+  contents: string,
+  options?: WritingOptions
+): Promise<void> {
   return FS.writeAsStringAsync(fileUri, contents, options || {});
 }
 
