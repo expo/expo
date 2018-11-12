cead33d1406693cf2a22ed81804f3f567bd213e9
Evan Bacon <baconbrix@gmail.com>
2018-11-12 12:13:35 -0800

    Exporting module as an object literal. Methods throw errors. Updated flowtypes

--- packages/expo-constants/src/ExponentConstants.web.js
+++ packages/expo-constants/src/ExponentConstants.web.js
@@ -4,53 +4,43 @@ import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
 import uuidv4 from 'uuid/v4';
 import parser from 'ua-parser-js';
 
-class ExponentConstants {
-  _sessionId = uuidv4();
-
-  get name() {
+export default {
+  _sessionId: uuidv4(),
+  get name(): string {
     return 'ExponentConstants';
-  }
-
-  get sessionId() {
+  },
+  get sessionId(): string {
     return this._sessionId;
-  }
-
-  get platform() {
+  },
+  get platform(): object {
     return { web: parser(navigator.userAgent) };
-  }
-
-  get isDevice() {
+  },
+  get isDevice(): boolean {
     return true;
-  }
-
-  get expoVersion() {
+  },
+  get expoVersion(): string {
     return '32';
-  }
-
-  constructor() {
-    this.linkingUri = global.location.href.split('?')[0].split('#')[0];
-
-    const invalidConstants = [
-      'expoRuntimeVersion',
-      'deviceName',
-      'systemFonts',
-      'statusBarHeight',
-      'deviceYearClass',
-    ];
-    for (let constantName of invalidConstants) {
-      Object.defineProperty(this, constantName, {
-        get() {
-          console.warn(`${this.name}.${constantName} is not implemented`);
-          return null;
-        },
-      });
-    }
-  }
-
+  },
+  get linkingUri(): string {
+    return global.location.href.split('?')[0].split('#')[0];
+  },
+  get expoRuntimeVersion(): ?string {
+    throw new Error(`ExponentConstants.expoRuntimeVersion: is unimplemented on this platform.`);
+  },
+  get deviceName(): ?string {
+    throw new Error(`ExponentConstants.deviceName: is unimplemented on this platform.`);
+  },
+  get systemFonts(): ?Array<string> {
+    throw new Error(`ExponentConstants.systemFonts: is unimplemented on this platform.`);
+  },
+  get statusBarHeight(): number {
+    return 0;
+  },
+  get deviceYearClass(): ?string {
+    throw new Error(`ExponentConstants.deviceYearClass: is unimplemented on this platform.`);
+  },
   getWebViewUserAgentAsync(): Promise {
     const { navigator = {} } = global;
     return canUseDOM ? navigator.userAgent : null;
-  }
-}
-
-export default new ExponentConstants();
+  },
+};
