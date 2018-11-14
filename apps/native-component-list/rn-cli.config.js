4ce242b0d4926b7d47430dd397e1a13ef55299d6
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2018-11-14 23:54:00 +0100

    [ncl] fix rn-cli.config.js in ncl

--- apps/native-component-list/rn-cli.config.js
+++ apps/native-component-list/rn-cli.config.js
@@ -1,12 +1,3 @@
 const { createReactNativeConfiguration } = require('expo-yarn-workspaces');
 
-module.exports = {
-  ...createReactNativeConfiguration(__dirname),
-
-  getTransformModulePath() {
-    return require.resolve('react-native-typescript-transformer');
-  },
-  getSourceExts() {
-    return ['ts', 'tsx'];
-  },
-};
+module.exports = createReactNativeConfiguration(__dirname);
