64efaa4e7f0a106ef5662f16f61689061aa74204
Evan Bacon <baconbrix@gmail.com>
2018-11-16 02:57:03 -0800

    Removed webfontloader

---
+++ packages/expo-font/src/PlatformModule.web.js
@@ -0,0 +1,43 @@
+// @flow
+
+function isFunction(functionToCheck) {
+  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
+
+function createWebStyle(fontFamily, resource) {
+  const fontStyle = `@font-face {
+    font-family: ${fontFamily};
+    src: url(${resource});
+  }`;
+
+  const styleElement = document.createElement('style');
+  styleElement.type = 'text/css';
+  if (styleElement.styleSheet) {
+    styleElement.styleSheet.cssText = fontStyle;
+  } else {
+    const textNode = document.createTextNode(fontStyle);
+    styleElement.appendChild(textNode);
+  }
+  return styleElement;
+}
+
+export default {
+  get name(): string {
+    return 'ExpoFontLoader';
+  },
+  loadAsync(fontFamilyName: string, resource: string): Promise {
+    const canInjectStyle =
+      document &&
+      document.head &&
+      isFunction(document.head.appendChild) &&
+      isFunction(document.createElement) &&
+      isFunction(document.createTextNode);
+
+    if (!canInjectStyle) {
+      throw new Error('E_FONT_CREATION_FAILED : document element cannot support injecting fonts');
+    }
+    const style = createWebStyle(fontFamilyName, resource);
+    document.head.appendChild(style);
+    return Promise.resolve();
+  },
+};
