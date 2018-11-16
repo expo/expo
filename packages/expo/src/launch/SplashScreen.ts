938e5099e8f21815b6a1de2088846031af47bd2a
Evan Bacon <baconbrix@gmail.com>
2018-11-16 01:02:31 -0800

    Added shim for `AppLoading` and `SplashScreen`

--- packages/expo/src/launch/SplashScreen.ts
+++ packages/expo/src/launch/SplashScreen.ts
@@ -1,11 +1,17 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
-const { ExponentSplashScreen: SplashScreen } = NativeModules;
+const { ExponentSplashScreen: SplashScreen = {} } = NativeModules;
+
+const isWeb = Platform.OS === 'web';
 
 export function preventAutoHide() {
-  SplashScreen.preventAutoHide();
+  if (!isWeb) {
+    SplashScreen.preventAutoHide();
+  }
 }
 
 export function hide() {
-  SplashScreen.hide();
+  if (!isWeb) {
+    SplashScreen.hide();
+  }
 }
