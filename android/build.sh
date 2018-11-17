b5646ae1a75dc9d3cb8a58d8580fe5106aced58f
Nick Novitski <nick@expo.io>
2018-11-16 19:46:59 -0800

    [android] Release gradle builds are always zipaligned and signed

--- android/build.sh
+++ android/build.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 # download dependencies in retry loop to reduce network-caused errors
 for i in {1..3}; do ((i > 1)) && sleep 5; ./gradlew --stacktrace :app:preBuild && break; done
 
-./gradlew --stacktrace :app:assembleProdKernelRelease
+./gradlew --stacktrace :app:assembleProdKernel${1:-Debug}
