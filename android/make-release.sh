6be4fea17cb15fd3f542c1c9e4d0c8d07e272cfe
Nick Novitski <nick@expo.io>
2018-11-16 19:46:59 -0800

    [android] Release gradle builds are always zipaligned and signed

--- android/make-release.sh
+++ android/make-release.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./build.sh
+echo $ANDROID_KEYSTORE_B64 | base64 -d > app/release-key.jks
 
-zipalign -f -v -p 4 app/build/outputs/apk/prodKernel/release/app-prodKernel-release-unsigned.apk app-prod-release-unsigned-aligned.apk
-
-apksigner sign \
-  --ks <(echo $ANDROID_KEYSTORE_B64 | base64 -d) \
-  --ks-key-alias $ANDROID_KEY_ALIAS --ks-pass env:ANDROID_KEYSTORE_PASSWORD \
-  --key-pass env:ANDROID_KEY_PASSWORD \
-  --out exponent-release.apk app-prod-release-unsigned-aligned.apk
+./build.sh Release
