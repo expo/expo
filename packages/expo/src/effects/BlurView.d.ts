bff21945ff6752a963e5680628242d2380a87471
Evan Bacon <baconbrix@gmail.com>
2018-11-19 14:07:28 -0600

    updated `BlurView` type

--- packages/expo/src/effects/BlurView.d.ts
+++ packages/expo/src/effects/BlurView.d.ts
@@ -1,9 +1,10 @@
-import BlurViewAndroid from './BlurView.android';
-import BlurViewIOS from './BlurView.ios';
-
 import { ElementProps } from 'react';
 
+import BlurViewAndroid from './BlurView.android';
+import BlurViewIOS from './BlurView.ios';
+import BlurViewWeb from './BlurView.web';
+
 type Narrow<T1, T2> = T1 extends T2 ? T1 : (T2 extends T1 ? T2 : never);
-type CommonBlurView = Narrow<BlurViewIOS, BlurViewAndroid>;
+type CommonBlurView = Narrow<BlurViewIOS, BlurViewAndroid, BlurViewWeb>;
 
 export default class BlurView extends React.Component<ElementProps<CommonBlurView>> {}
