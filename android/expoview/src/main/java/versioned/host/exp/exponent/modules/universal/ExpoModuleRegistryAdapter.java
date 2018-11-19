8b517681d32f573bcaafe0ecaaafc5727a399f67
szymon <szymon.kapala@swmansion.com>
2018-11-19 14:28:49 +0100

    Relaxed file system permissions when in expokit

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/universal/ExpoModuleRegistryAdapter.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/universal/ExpoModuleRegistryAdapter.java
@@ -17,6 +17,7 @@ import expo.adapters.react.ReactModuleRegistryProvider;
 import expo.core.ModuleRegistry;
 import expo.core.interfaces.InternalModule;
 import expo.core.interfaces.ModuleRegistryConsumer;
+import expo.modules.filesystem.ExpokitFilePermissionChecker;
 import host.exp.exponent.ExponentManifest;
 import host.exp.exponent.kernel.ExperienceId;
 import host.exp.exponent.utils.ScopedContext;
@@ -52,6 +53,9 @@ public class ExpoModuleRegistryAdapter extends ModuleRegistryAdapter implements 
 
     // Overriding expo-constants/ConstantsService -- binding provides manifest and other expo-related constants
     moduleRegistry.registerInternalModule(new ConstantsBinding(scopedContext, experienceProperties, manifest));
+
+    // Overriding expo-file-system FilePermissionChecker
+    moduleRegistry.registerInternalModule(new ExpoClientFilePermissionChecker());
 
     // ReactAdapterPackage requires ReactContext
     ReactApplicationContext reactContext = (ReactApplicationContext) scopedContext.getContext();
---
+++ packages/expo-file-system-interface/android/src/main/Permission.java
@@ -0,0 +1,5 @@
+package main;
+
+public enum Permission {
+  READ, WRITE,
+}
