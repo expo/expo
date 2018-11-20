9042ea13f0502483dd56ed33c550a1688b596877
Stanisław Chmiela <sjchmiela@gmail.com>
2018-11-20 10:36:30 +0100

    Build expo-sensors

---
+++ packages/expo-sensors/build/index.js
@@ -0,0 +1,7 @@
+export { default as Accelerometer } from './Accelerometer';
+export { default as DeviceMotion } from './DeviceMotion';
+export { default as Gyroscope } from './Gyroscope';
+export { default as Magnetometer } from './Magnetometer';
+export { default as MagnetometerUncalibrated } from './MagnetometerUncalibrated';
+export { default as Pedometer } from './Pedometer';
+//# sourceMappingURL=index.js.map
