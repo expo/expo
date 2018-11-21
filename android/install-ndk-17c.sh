f2813909e88740f8cac4e757c0a335e22a0e3b4a
Nick Novitski <nick@expo.io>
2018-11-20 16:26:28 -0800

    [android] Make direnv optional

--- android/install-ndk-17c.sh
+++ android/install-ndk-17c.sh
@@ -5,7 +5,7 @@ scriptdir="$(dirname ${BASH_SOURCE[0]})"
 shellnix="$scriptdir/shell.nix"
 
 ndk=${1:-$(nix-build "$shellnix" -A ndk --no-out-link)}
-ndkRoot=${2:-$(nix eval --file "$shellnix" --raw ndk_root)}
+ndkRoot=${2:-$(nix eval --file "$shellnix" --raw ANDROID_NDK_ROOT)}
 
 mkdir -p "$ANDROID_NDK_ROOT" # Create full path if necessary
 
