622cbea8e9f0e661e2f31b012fe9698489da4ab9
Evan Bacon <baconbrix@gmail.com>
2018-11-23 16:59:07 -0600

    [Web] Fingerprint
    needs `UnsupportedError` to be merged

--- packages/expo/src/Fingerprint.ts
+++ packages/expo/src/Fingerprint.ts
@@ -1,21 +1,38 @@
 import invariant from 'invariant';
 import { NativeModules, Platform } from 'react-native';
+import UnsupportedError from './UnsupportedError';
 
-const { ExponentFingerprint } = NativeModules;
+const {
+  ExponentFingerprint = {
+    get name() {
+      return 'ExponentFingerprint';
+    },
+  },
+} = NativeModules;
 
-type FingerprintAuthenticationResult = { success: true } | { success: false, error: string };
+type FingerprintAuthenticationResult = { success: true } | { success: false; error: string };
 
 export function hasHardwareAsync(): Promise<boolean> {
+  if (!ExponentFingerprint.hasHardwareAsync) {
+    throw new UnsupportedError('Fingerprint', 'hasHardwareAsync');
+  }
   return ExponentFingerprint.hasHardwareAsync();
 }
 
 export function isEnrolledAsync(): Promise<boolean> {
+  if (!ExponentFingerprint.isEnrolledAsync) {
+    throw new UnsupportedError('Fingerprint', 'isEnrolledAsync');
+  }
   return ExponentFingerprint.isEnrolledAsync();
 }
 
 export function authenticateAsync(
   promptMessageIOS: string = 'Authenticate'
 ): Promise<FingerprintAuthenticationResult> {
+  if (!ExponentFingerprint.authenticateAsync) {
+    throw new UnsupportedError('Fingerprint', 'authenticateAsync');
+  }
+
   if (Platform.OS === 'ios') {
     invariant(
       typeof promptMessageIOS === 'string' && promptMessageIOS.length,
@@ -28,5 +45,8 @@ export function authenticateAsync(
 }
 
 export function cancelAuthenticate(): void {
-  ExponentFingerprint.cancelAuthenticate();
+  if (!ExponentFingerprint.cancelAuthenticate) {
+    throw new UnsupportedError('Fingerprint', 'cancelAuthenticate');
+  }
+  return ExponentFingerprint.cancelAuthenticate();
 }
