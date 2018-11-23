8f2eb62c5f8a7168b8753841192d9da3cee92d57
Evan Bacon <baconbrix@gmail.com>
2018-11-23 16:36:29 -0600

    [Web] ImagePicker

--- packages/expo/src/ImagePicker.ts
+++ packages/expo/src/ImagePicker.ts
@@ -1,6 +1,13 @@
 import { NativeModules } from 'react-native';
+import UnsupportedError from './UnsupportedError';
 
-const { ExponentImagePicker } = NativeModules;
+const {
+  ExponentImagePicker = {
+    get name() {
+      return 'ExponentImagePicker';
+    },
+  },
+} = NativeModules;
 
 const MEDIA_TYPE_OPTIONS = {
   All: 'All',
@@ -9,33 +16,39 @@ const MEDIA_TYPE_OPTIONS = {
 };
 
 type ImageInfo = {
-  uri: string,
-  width: number,
-  height: number,
+  uri: string;
+  width: number;
+  height: number;
 };
 
 type ImageResult = { cancelled: true } | ({ cancelled: false } & ImageInfo);
 
 type ImageLibraryOptions = {
-  allowsEditing?: boolean,
-  aspect?: [number, number],
-  quality?: number,
-  mediaTypes?: keyof (typeof MEDIA_TYPE_OPTIONS),
+  allowsEditing?: boolean;
+  aspect?: [number, number];
+  quality?: number;
+  mediaTypes?: keyof (typeof MEDIA_TYPE_OPTIONS);
 };
 
 export async function launchImageLibraryAsync(
   options: ImageLibraryOptions = {}
 ): Promise<ImageResult> {
+  if (!ExponentImagePicker.launchImageLibraryAsync) {
+    throw new UnsupportedError('ImagePicker', 'launchImageLibraryAsync');
+  }
   return ExponentImagePicker.launchImageLibraryAsync(options);
 }
 
 type CameraOptions = {
-  allowsEditing?: boolean,
-  aspect?: [number, number],
-  quality?: number,
+  allowsEditing?: boolean;
+  aspect?: [number, number];
+  quality?: number;
 };
 
 export async function launchCameraAsync(options: CameraOptions = {}): Promise<ImageResult> {
+  if (!ExponentImagePicker.launchCameraAsync) {
+    throw new UnsupportedError('ImagePicker', 'launchCameraAsync');
+  }
   return ExponentImagePicker.launchCameraAsync(options);
 }
 
