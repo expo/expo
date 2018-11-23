fa2ce9bd007b389e502d1635fe327a9f74129dac
Evan Bacon <baconbrix@gmail.com>
2018-11-23 16:35:10 -0600

    [Web] ErrorRecovery

--- packages/expo/src/ErrorRecovery.ts
+++ packages/expo/src/ErrorRecovery.ts
@@ -1,9 +1,20 @@
 import { NativeModules } from 'react-native';
+import UnsupportedError from './UnsupportedError';
 
-const { ExponentErrorRecovery } = NativeModules;
+const {
+  ExponentErrorRecovery = {
+    get name() {
+      return 'ExponentErrorRecovery';
+    },
+  },
+} = NativeModules;
 
 export default {
   setRecoveryProps(props: { [key: string]: any }): void {
+    if (!ExponentErrorRecovery.setRecoveryProps) {
+      throw new UnsupportedError('ErrorRecovery', 'setRecoveryProps');
+    }
+
     return ExponentErrorRecovery.setRecoveryProps(props);
   },
 };
