e03604c8bd1ec64b91c55f0842881d5881687b5e
Evan Bacon <baconbrix@gmail.com>
2018-11-23 16:57:04 -0600

    [Web] Facebook
    needs `UnsupportedError` to be merged

--- packages/expo/src/Facebook.ts
+++ packages/expo/src/Facebook.ts
@@ -1,6 +1,32 @@
 import { NativeModules } from 'react-native';
+import UnsupportedError from './UnsupportedError';
 
-const { ExponentFacebook } = NativeModules;
+/*
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : 'your-app-id',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v3.2'
+  });
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+*/
+
+const {
+  ExponentFacebook = {
+    get name() {
+      return 'ExponentFacebook';
+    },
+  },
+} = NativeModules;
 
 type FacebookLoginResult = {
   type: string;
@@ -17,6 +43,9 @@ export async function logInWithReadPermissionsAsync(
   appId: string,
   options?: FacebookOptions
 ): Promise<FacebookLoginResult> {
+  if (!ExponentFacebook.logInWithReadPermissionsAsync) {
+    throw new UnsupportedError('Facebook', 'logInWithReadPermissionsAsync');
+  }
   if (typeof appId !== 'string') {
     console.warn(
       `logInWithReadPermissionsAsync: parameter 'appId' must be a string, was '${typeof appId}''.`
