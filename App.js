91b8950f3ede2f2ed0a8e30c7bacfd47abecc683
Ville Immonen <ville.immonen@iki.fi>
2018-11-27 14:34:30 +0000

    [templates] Rename project templates and publish them to npm (#3313)

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
