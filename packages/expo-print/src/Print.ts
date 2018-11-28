47b0f4aadc2c68417ca85ce89b06c096f67f5946
szymon <szymon.kapala@swmansion.com>
2018-11-28 11:35:55 +0100

    fixed indentation

--- packages/expo-print/src/Print.ts
+++ packages/expo-print/src/Print.ts
@@ -39,25 +39,25 @@ type FilePrintResult = {
 const Orientation: OrientationConstant = ExponentPrint.Orientation
 
 async function printAsync(options: PrintOptions): Promise<void> {
-    if (!options.uri && !options.html && (Platform.OS === 'ios' && !options.markupFormatterIOS)) {
-      throw new Error('Must provide either `html` or `uri` to print');
-    }
-    if (options.uri && options.html) {
-      throw new Error('Must provide exactly one of `html` and `uri` but both were specified');
-    }
-    return ExponentPrint.print(options);
+  if (!options.uri && !options.html && (Platform.OS === 'ios' && !options.markupFormatterIOS)) {
+    throw new Error('Must provide either `html` or `uri` to print');
+  }
+  if (options.uri && options.html) {
+    throw new Error('Must provide exactly one of `html` and `uri` but both were specified');
+  }
+  return ExponentPrint.print(options);
 }
 
 async function selectPrinterAsync(): Promise<SelectResult> {
-    if (Platform.OS === 'ios') {
-      return ExponentPrint.selectPrinter();
-    } else {
-      throw new Error('Selecting the printer in advance is not available on Android.');
-    }
+  if (Platform.OS === 'ios') {
+    return ExponentPrint.selectPrinter();
+  } else {
+    throw new Error('Selecting the printer in advance is not available on Android.');
+  }
 }
 
 async function printToFileAsync(options: FilePrintOptions = {}): Promise<FilePrintResult> {
-    return ExponentPrint.printToFileAsync(options);
+  return ExponentPrint.printToFileAsync(options);
 }
 
 export default {
