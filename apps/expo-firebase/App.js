6293e457b1535226335023d9474454d6205b7403
Evan Bacon <baconbrix@gmail.com>
2018-11-28 13:25:09 -0800

    added JS files to test firebase

--- apps/expo-firebase/App.js
+++ apps/expo-firebase/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firebase from 'expo-firebase-app';
+
 export default class App extends React.Component {
+  componentDidMount() {
+    console.log(firebase.analytics());
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Expo Firebase</Text>
       </View>
     );
   }
