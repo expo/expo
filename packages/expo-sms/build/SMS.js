8e6e78efcaa9cd87b88a589d32a3e9939131fafb
James Ide <ide@expo.io>
2018-11-29 21:10:05 -0800

    [sms] Commit updated `build` files

--- packages/expo-sms/build/SMS.js
+++ packages/expo-sms/build/SMS.js
@@ -2,10 +2,10 @@ import { Platform } from 'expo-core';
 import ExpoSMS from './ExpoSMS';
 export async function sendSMSAsync(addresses, message) {
     const finalAddresses = Array.isArray(addresses) ? addresses : [addresses];
-    if (ExpoSMS.sendSMSAsync) {
-        return ExpoSMS.sendSMSAsync(finalAddresses, message);
+    if (!ExpoSMS.sendSMSAsync) {
+        throw new Error(`SMS.sendSMSAsync is not supported on ${Platform.OS}`);
     }
-    throw new Error(`SMS.sendSMSAsync is not supported on ${Platform.OS}`);
+    return ExpoSMS.sendSMSAsync(finalAddresses, message);
 }
 export async function isAvailableAsync() {
     return ExpoSMS.isAvailableAsync();
