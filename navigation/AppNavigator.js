12210b1cab78c1d8278fa96c99ffc1b1ee3b795c
Quinlan Jung <quin@expo.io>
2018-11-30 12:02:42 -0800

    [expo-template-tabs] apply changes from watanabeyu

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 
-export default createSwitchNavigator({
+export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
-});
+}));
