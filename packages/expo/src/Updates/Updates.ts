81f37d0567fcc9b39404afb096127be746a02904
Evan Bacon <baconbrix@gmail.com>
2018-12-04 15:09:40 -0800

    Update Updates.ts

--- packages/expo/src/Updates/Updates.ts
+++ packages/expo/src/Updates/Updates.ts
@@ -1,6 +1,6 @@
+import { UnavailabilityError } from 'expo-errors';
 import { EventEmitter, EventSubscription } from 'fbemitter';
 import { DeviceEventEmitter } from 'react-native';
-import { UnavailabilityError } from 'expo-errors';
 
 import ExponentUpdates from './ExponentUpdates';
 
