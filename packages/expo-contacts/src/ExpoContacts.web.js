3892ba27c212c0553ac1025e6a37225e8f933e36
Evan Bacon <baconbrix@gmail.com>
2018-12-03 19:44:14 -0800

    Update ExpoContacts.web.js

--- packages/expo-contacts/src/ExpoContacts.web.js
+++ packages/expo-contacts/src/ExpoContacts.web.js
@@ -1,11 +1,7 @@
 // @flow
 
 export default {
-  async shareContactAsync(contactId: string, message: string) {},
-  async getContactsAsync(contactQuery: Object) {},
-  async addContactAsync(contact: Object, containerId: string) {},
-  async updateContactAsync(contact: Object) {},
-  async removeContactAsync(contactId: string) {},
-  async writeContactToFileAsync(contactQuery: Object) {},
-  async presentFormAsync(contactId: string, contact: Object, adjustedOptions: Object) {},
+  get name(): string {
+    return 'ExpoContacts';
+  },
 };
