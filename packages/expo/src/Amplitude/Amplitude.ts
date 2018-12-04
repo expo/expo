9591ce0934dfa6b6ebc3ee7028e4196bee732caf
Evan Bacon <baconbrix@gmail.com>
2018-12-03 20:02:15 -0800

    Added expo-errors

--- packages/expo/src/Amplitude/Amplitude.ts
+++ packages/expo/src/Amplitude/Amplitude.ts
@@ -1,52 +1,52 @@
-import UnsupportedError from '../UnsupportedError';
+import { UnavailabilityError } from 'expo-errors';
 import ExponentAmplitude from './ExponentAmplitude';
 
 export default {
   initialize(apiKey: string): void {
     if (!ExponentAmplitude.initialize) {
-      throw new UnsupportedError('Amplitude', 'initialize');
+      throw new UnavailabilityError('Amplitude', 'initialize');
     }
     return ExponentAmplitude.initialize(apiKey);
   },
 
   setUserId(userId: string): void {
     if (!ExponentAmplitude.setUserId) {
-      throw new UnsupportedError('Amplitude', 'setUserId');
+      throw new UnavailabilityError('Amplitude', 'setUserId');
     }
     return ExponentAmplitude.setUserId(userId);
   },
 
   setUserProperties(userProperties: { [name: string]: any }): void {
     if (!ExponentAmplitude.setUserProperties) {
-      throw new UnsupportedError('Amplitude', 'setUserProperties');
+      throw new UnavailabilityError('Amplitude', 'setUserProperties');
     }
     return ExponentAmplitude.setUserProperties(userProperties);
   },
 
   clearUserProperties(): void {
     if (!ExponentAmplitude.clearUserProperties) {
-      throw new UnsupportedError('Amplitude', 'clearUserProperties');
+      throw new UnavailabilityError('Amplitude', 'clearUserProperties');
     }
     return ExponentAmplitude.clearUserProperties();
   },
 
   logEvent(eventName: string): void {
     if (!ExponentAmplitude.logEvent) {
-      throw new UnsupportedError('Amplitude', 'logEvent');
+      throw new UnavailabilityError('Amplitude', 'logEvent');
     }
     return ExponentAmplitude.logEvent(eventName);
   },
 
   logEventWithProperties(eventName: string, properties: { [name: string]: any }): void {
     if (!ExponentAmplitude.logEventWithProperties) {
-      throw new UnsupportedError('Amplitude', 'logEventWithProperties');
+      throw new UnavailabilityError('Amplitude', 'logEventWithProperties');
     }
     return ExponentAmplitude.logEventWithProperties(eventName, properties);
   },
 
   setGroup(groupType: string, groupNames: string[]): void {
     if (!ExponentAmplitude.setGroup) {
-      throw new UnsupportedError('Amplitude', 'setGroup');
+      throw new UnavailabilityError('Amplitude', 'setGroup');
     }
     return ExponentAmplitude.setGroup(groupType, groupNames);
   },
--- packages/expo/src/UnsupportedError.ts
+++
@@ -1,7 +0,0 @@
-import { Platform } from 'expo-core';
-
-export default class UnsupportedError extends Error {
-  constructor(moduleName, methodName) {
-    super(`${moduleName}.${methodName}: is not supported on ${Platform.OS}`);
-  }
-}
