cdd85638231b6273415ac335d30e7412e4de1784
Evan Bacon <baconbrix@gmail.com>
2018-12-05 11:32:00 -0800

    Update symbolicateStackTrace.web.ts

--- packages/expo/src/logs/symbolicateStackTrace.web.ts
+++ packages/expo/src/logs/symbolicateStackTrace.web.ts
@@ -3,5 +3,3 @@ import { StackFrame } from 'react-native/Libraries/Core/Devtools/parseErrorStack
 export default async function symbolicateStackTrace(): Promise<StackFrame[] | null> {
   return null;
 }
-
-export default symbolicateStackTrace;
