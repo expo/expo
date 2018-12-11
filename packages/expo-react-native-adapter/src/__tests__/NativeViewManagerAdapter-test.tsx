7310cac4986eee5327930ca0fcf4f04423893c14
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2018-12-11 15:39:11 +0100

    [expo-react-native-adapter] Forward refs to underlying native components (#2937)
    # Why
    
    Refs passed to Expo native views were ignored.
    
    # How
    
    Forward refs to underlying native components.
    
    # Test Plan
    
    Taking picture in Camerja in NCL works (previously it didn't, `ref` was not being called).

--- packages/expo-react-native-adapter/src/__tests__/NativeViewManagerAdapter-test.tsx
+++ packages/expo-react-native-adapter/src/__tests__/NativeViewManagerAdapter-test.tsx
@@ -19,8 +19,15 @@ jest.mock('react-native', () => {
 
 describe('requireNativeViewManager', () => {
   it(`sets the "displayName" of the native component`, () => {
-    const TestView = requireNativeViewManager('ExpoTestView');
-    expect(TestView.displayName).toBe('Adapter<ExpoTestView>');
+    // USING REACT INTERNALS HERE! `getComponentName()` isn't exported from
+    // React, so we can't test the resulting component name the way React will
+    // definitely calculate it. For now, let's use the fact that we know that
+    // TestView will be a ForwardRef which stores the underlying render function
+    // under the `render` property and that's how the component name is
+    // calculated.
+    // https://github.com/facebook/react/blob/769b1f270e1251d9dbdce0fcbd9e92e502d059b8/packages/shared/getComponentName.js#L81
+    const TestView: any = requireNativeViewManager('ExpoTestView');
+    expect(TestView.render.displayName).toBe('Adapter<ExpoTestView>');
   });
 
   it(`partitions props into React Native and custom props`, () => {
@@ -38,7 +45,7 @@ describe('requireNativeViewManager', () => {
     // React Native props
     expect(testNativeComponent.props.testID).toBe('test');
     expect(React.Children.toArray(testNativeComponent.props.children)).toHaveLength(1);
-  
+
     // Custom props
     expect(testNativeComponent.props.proxiedProperties).toEqual({ custom: 'hello' });
     expect(testNativeComponent.props).not.toHaveProperty('custom');
