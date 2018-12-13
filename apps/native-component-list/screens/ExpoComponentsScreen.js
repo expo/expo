06d5bc20ff26f323fc836c6e5685025d12c32321
Evan Bacon <baconbrix@gmail.com>
2018-12-12 21:37:38 -0800

    Updated which modules are shown on web

--- apps/native-component-list/screens/ExpoComponentsScreen.js
+++ apps/native-component-list/screens/ExpoComponentsScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Platform } from 'react-native';
 import ComponentListScreen from './ComponentListScreen';
 import { Layout } from '../constants';
 
 export default class ExpoComponentsScreen extends React.Component {
+  static path = '';
+
   static navigationOptions = {
     title: Layout.isSmallDevice ? 'Expo SDK Components' : 'Components in Expo SDK',
   };
@@ -13,24 +15,39 @@ export default class ExpoComponentsScreen extends React.Component {
   }
 
   _getApis = () => {
-    return [
-      'AdMob',
-      'BarCodeScanner',
-      'BlurView',
-      'Camera',
-      'FacebookAds',
-      'GestureHandlerList',
-      'GestureHandlerPinch',
-      'GestureHandlerSwipeable',
-      'ImagePreview',
-      'Gif',
-      'GL',
-      'LinearGradient',
-      'Lottie',
-      'Maps',
-      'Screens',
-      'SVG',
-      'Video',
-    ];
+    return Platform.select({
+      web: [
+        'AdMob',
+        'BlurView',
+        'Camera',
+        'ImagePreview',
+        'Gif',
+        'GL',
+        'LinearGradient',
+        'Lottie',
+        'Maps',
+        'SVG',
+        'Video',
+      ],
+      default: [
+        'AdMob',
+        'BarCodeScanner',
+        'BlurView',
+        'Camera',
+        'FacebookAds',
+        'GestureHandlerList',
+        'GestureHandlerPinch',
+        'GestureHandlerSwipeable',
+        'ImagePreview',
+        'Gif',
+        'GL',
+        'LinearGradient',
+        'Lottie',
+        'Maps',
+        'Screens',
+        'SVG',
+        'Video',
+      ],
+    });
   };
 }
