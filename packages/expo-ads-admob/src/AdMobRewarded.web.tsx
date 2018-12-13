4021aa798a2344c4c3dfcb66cfbde64405b4742d
Evan Bacon <baconbrix@gmail.com>
2018-12-12 20:44:34 -0800

    [Web] Added Ads (#2968)

---
+++ packages/expo-ads-admob/src/AdMobRewarded.web.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class AdMobRewarded extends React.Component<{}> {
+  render() {
+    return (
+      <View>
+        <Text>AdMobRewarded component not supported on the web</Text>
+      </View>
+    );
+  }
+}
