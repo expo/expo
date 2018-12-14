c8e34fbeb9cb298e9f87b45a93e312b0f7b625b3
Evan Bacon <baconbrix@gmail.com>
2018-12-13 17:05:27 -0800

    [Web] native-component-list (#2677)
    * [NCL] added web target
    * [NCL] removed .babelrc
    * [FileSystem] Added shim
    * [Asset] Added shim
    * [Constants] Added shim
    * [Font] Added shim
    * Moved `FileSystem` changes to another PR #2674
    * Moved `Constants` changes to another PR #2675
    * Moved `Font` changes to another PR #2676
    * Update packages/expo-asset/src/Asset.web.js
    * Simplify PR changes
    * Removed third-party expo extension
    * fixed babel for modules in root
    * Removed duplicate babel plugins
    * [NCL] removed `src/`
    * Added web template to `expo-yarn-workspaces`
    * Changed web entry point to target `AppEntry`
    * Update apps/native-component-list/webpack.config.js
    * added `react-navigation`
    * Slipping a `FileSystem` fix in o.o
    * webpack updates
    * removed service worker
    * added support for jsx
    * explicitly defined babel
    * moved webpack to dev dependencies
    * removed css
    * fixed `__DEV__` debug value
    * [Web] Updated for icons
    * [Web] fix for constants transform
    * [Web] Moved web entry point in to expo
    * moved `KeepAwake` to #2732
    * Added methods for using RN functions with RNWeb modules
    * Made NCL components more platform friendly
    * Moved changes to #2830
    * Added simple web entry file
    * updated gitignore for web
    * relativePath -> absolutePath
    * added support for `.json`
    * `wrapWithExpoRoot` -> `withExpoRoot`
    * providing `exp` from web
    * Renamed `public` to `web`
    * Added web version of `Systrace`
    * Made the babel config exclusively a part of `babel.config.js`
    * Fixed Notification error
    * Update AppEntry.template.js
    * Moved NCL changes to another PR
    * Update registerRootComponent.web.tsx
    * Added code to support routing
    * Updated PWA icons

--- apps/native-component-list/babel.config.js
+++ apps/native-component-list/babel.config.js
@@ -1,7 +1,7 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@babel/plugin-transform-runtime'],
+    plugins: ['babel-plugin-react-native-web', '@babel/plugin-transform-runtime'],
   };
 };
