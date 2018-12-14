1a34cb8524c7edcd93ed538cd3de54a1b2d6fe5b
Stanisław Chmiela <sjchmiela@gmail.com>
2018-12-14 16:36:51 +0100

    [ios] Regenerate Podfile and install Pods without SDK25

--- ios/Pods/Headers/Private/ReactABI25_0_0/ABI25_0_0RCTAnimation/ABI25_0_0RCTAdditionAnimatedNode.h
+++
@@ -1,1 +0,0 @@
-../../../../../versioned-react-native/ABI25_0_0/Libraries/NativeAnimation/Nodes/ABI25_0_0RCTAdditionAnimatedNode.h
--- ios/Pods/Headers/Public/ReactABI25_0_0/ABI25_0_0EXPedometer.h
+++
@@ -1,1 +0,0 @@
-../../../../versioned-react-native/ABI25_0_0/Expo/Core/Api/Sensors/ABI25_0_0EXPedometer.h
