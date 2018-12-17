bd9f50b363460b8da964604680b71200ca5544fe
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2018-12-17 09:56:29 +0100

    [android] fix MultipleVersionReactNativeActivity

--- android/expoview/src/main/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
+++ android/expoview/src/main/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
@@ -3,7 +3,7 @@
 package host.exp.exponent.experience;
 
 // Implement for each version.
-public class MultipleVersionReactNativeActivity extends ReactNativeActivity implements abi32_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
+public class MultipleVersionReactNativeActivity extends ReactNativeActivity implements
     // The 4-space indentation is used by android-build-aar.sh.
     // WHEN_DISTRIBUTING_REMOVE_FROM_HERE
     // WHEN_PREPARING_SHELL_REMOVE_FROM_HERE
@@ -25,6 +25,9 @@ public class MultipleVersionReactNativeActivity extends ReactNativeActivity impl
     // BEGIN_SDK_31
     abi31_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
     // END_SDK_31
+    // BEGIN_SDK_32
+    abi32_0_0.com.facebook.react.modules.core.DefaultHardwareBackBtnHandler,
+    // END_SDK_32
     // WHEN_PREPARING_SHELL_REMOVE_TO_HERE
     // WHEN_DISTRIBUTING_REMOVE_TO_HERE
     com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
