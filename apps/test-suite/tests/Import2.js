f963ff0f28592750b9808d3996a39eca5ac1e084
Evan Bacon <baconbrix@gmail.com>
2018-12-18 14:03:21 -0800

    Update Import2.js

--- apps/test-suite/tests/Import2.js
+++ apps/test-suite/tests/Import2.js
@@ -4,17 +4,9 @@ export const name = 'Import1';
 
 export function test(t) {
   t.describe(`import Expo from 'expo';`, () => {
-    t.it(`const { Constants } = Expo;`, () => {
-      const originalConsoleWarn = console.warn;
-      console.warn = (...args) => {
-        if (typeof args[0] === 'string' && args[0].indexOf('deprecated import syntax') > -1) {
-          return;
-        }
-        originalConsoleWarn(...args);
-      };
-      const { Constants } = require('expo').default;
-      t.expect(Constants.expoVersion).toBeDefined();
-      console.warn = originalConsoleWarn;
+    t.it(`Deprecated: const { Constants } = Expo;`, () => {
+      const Expo = require('expo').default;
+      t.expect(Expo).toBeUndefined();
     });
     t.it(`Expo.Constants`, () => {
       t.expect(require('expo').Constants.expoVersion).toBeDefined();
