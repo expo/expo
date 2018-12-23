e7d2643c95ba84a356963b5e430a7421d3ca8b94
Evan Bacon <baconbrix@gmail.com>
2018-12-22 18:25:13 -0600

    Updated errors

--- apps/test-suite/components/Portal.js
+++ apps/test-suite/components/Portal.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 export default class Portal extends React.PureComponent {
   render() {
-    const styles = {
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      position: 'absolute',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'rgb(255, 255, 255)',
-      opacity: this.props.isVisible ? 0.5 : 0,
-    };
+    const style = StyleSheet.flatten([
+      styles.container,
+      { opacity: this.props.isVisible ? 0.5 : 0 },
+    ]);
 
     return (
-      <View style={styles} pointerEvents="none">
+      <View style={style} pointerEvents="none">
         {this.props.children}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgb(255, 255, 255)',
+  },
+});
--- apps/test-suite/components/RunnerError.js
+++ apps/test-suite/components/RunnerError.js
@@ -1,21 +1,27 @@
 import { Constants } from 'expo';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class RunnerError extends React.Component {
   render() {
     const { children } = this.props;
 
     return (
-      <View
-        style={{
-          flex: 1,
-          marginTop: Constants.statusBarHeight || 18,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text style={{ color: 'red' }}>{children}</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>{children}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight || 18,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+  },
+});
