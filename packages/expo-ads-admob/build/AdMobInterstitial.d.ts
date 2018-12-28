7077c51150cf3ad60a022ebf6e94715f473b862f
Evan Bacon <baconbrix@gmail.com>
2018-12-27 20:49:08 -0600

    [Web] Added Ads

--- packages/expo-ads-admob/build/AdMobInterstitial.d.ts
+++ packages/expo-ads-admob/build/AdMobInterstitial.d.ts
@@ -1,14 +1,14 @@
 declare type EventNameType = 'interstitialDidLoad' | 'interstitialDidFailToLoad' | 'interstitialDidOpen' | 'interstitialDidClose' | 'interstitialWillLeaveApplication';
 declare type EventListener = (...args: any[]) => void;
 declare const _default: {
-    setAdUnitID: (id: string) => Promise<void>;
-    setTestDeviceID: (id: string) => Promise<void>;
-    requestAdAsync: () => Promise<void>;
-    showAdAsync: () => Promise<void>;
-    dismissAdAsync: () => Promise<void>;
-    getIsReadyAsync: () => Promise<boolean>;
-    addEventListener: (type: EventNameType, handler: EventListener) => void;
-    removeEventListener: (type: EventNameType, handler: EventListener) => void;
-    removeAllListeners: () => void;
+    setAdUnitID(id: string): Promise<void>;
+    setTestDeviceID(id: string): Promise<void>;
+    requestAdAsync(): Promise<void>;
+    showAdAsync(): Promise<void>;
+    dismissAdAsync(): Promise<void>;
+    getIsReadyAsync(): Promise<boolean>;
+    addEventListener(type: EventNameType, handler: EventListener): void;
+    removeEventListener(type: EventNameType, handler: EventListener): void;
+    removeAllListeners(): void;
 };
 export default _default;
--- packages/expo-ads-admob/build/AdMobInterstitial.js
+++ packages/expo-ads-admob/build/AdMobInterstitial.js
@@ -1,6 +1,7 @@
-import { EventEmitter, NativeModulesProxy, Platform } from 'expo-core';
-const AdMobInterstitialManager = NativeModulesProxy.ExpoAdsAdMobInterstitialManager;
-const adMobInterstitialEmitter = new EventEmitter(AdMobInterstitialManager);
+import { EventEmitter } from 'expo-core';
+import { UnavailabilityError } from 'expo-errors';
+import AdMobNativeModule from './ExpoAdsAdMobInterstitialManager';
+const moduleName = 'AdMobInterstitial';
 const eventNames = [
     'interstitialDidLoad',
     'interstitialDidFailToLoad',
@@ -8,45 +9,68 @@ const eventNames = [
     'interstitialDidClose',
     'interstitialWillLeaveApplication',
 ];
+const eventEmitter = new EventEmitter(AdMobNativeModule);
 const eventHandlers = {};
-eventNames.forEach(eventName => {
+for (const eventName of eventNames) {
     eventHandlers[eventName] = new Map();
-});
-const addEventListener = (type, handler) => {
-    if (eventNames.includes(type)) {
-        eventHandlers[type].set(handler, adMobInterstitialEmitter.addListener(type, handler));
-    }
-    else {
-        console.log(`Event with type ${type} does not exist.`);
-    }
-};
-const removeEventListener = (type, handler) => {
-    const eventSubscription = eventHandlers[type].get(handler);
-    if (!eventHandlers[type].has(handler) || !eventSubscription) {
-        return;
-    }
-    eventSubscription.remove();
-    eventHandlers[type].delete(handler);
-};
-const removeAllListeners = () => eventNames.forEach(eventName => adMobInterstitialEmitter.removeAllListeners(eventName));
+}
 export default {
-    setAdUnitID: (id) => AdMobInterstitialManager.setAdUnitID(id),
-    setTestDeviceID: (id) => AdMobInterstitialManager.setTestDeviceID(id),
-    requestAdAsync: () => AdMobInterstitialManager.requestAd(),
-    showAdAsync: () => AdMobInterstitialManager.showAd(),
-    dismissAdAsync: () => new Promise((resolve, reject) => {
-        if (Platform.OS === 'ios') {
-            AdMobInterstitialManager.dismissAd()
-                .then(resolve)
-                .catch(reject);
+    async setAdUnitID(id) {
+        if (!AdMobNativeModule.setAdUnitID) {
+            throw new UnavailabilityError(moduleName, 'setAdUnitID');
+        }
+        await AdMobNativeModule.setAdUnitID(id);
+    },
+    async setTestDeviceID(id) {
+        if (!AdMobNativeModule.setTestDeviceID) {
+            throw new UnavailabilityError(moduleName, 'setTestDeviceID');
+        }
+        await AdMobNativeModule.setTestDeviceID(id);
+    },
+    async requestAdAsync() {
+        if (!AdMobNativeModule.requestAd) {
+            throw new UnavailabilityError(moduleName, 'requestAdAsync');
+        }
+        await AdMobNativeModule.requestAd();
+    },
+    async showAdAsync() {
+        if (!AdMobNativeModule.showAd) {
+            throw new UnavailabilityError(moduleName, 'showAdAsync');
+        }
+        await AdMobNativeModule.showAd();
+    },
+    async dismissAdAsync() {
+        if (!AdMobNativeModule.dismissAd) {
+            throw new UnavailabilityError(moduleName, 'dismissAdAsync');
+        }
+        await AdMobNativeModule.dismissAd();
+    },
+    async getIsReadyAsync() {
+        if (!AdMobNativeModule.getIsReady) {
+            throw new UnavailabilityError(moduleName, 'getIsReadyAsync');
+        }
+        return await AdMobNativeModule.getIsReady();
+    },
+    addEventListener(type, handler) {
+        if (eventNames.includes(type)) {
+            eventHandlers[type].set(handler, eventEmitter.addListener(type, handler));
         }
         else {
-            reject(new Error('Dismissing ads programmatically is supported only on iOS.'));
+            console.log(`Event with type ${type} does not exist.`);
         }
-    }),
-    getIsReadyAsync: () => AdMobInterstitialManager.getIsReady(),
-    addEventListener,
-    removeEventListener,
-    removeAllListeners,
+    },
+    removeEventListener(type, handler) {
+        const eventSubscription = eventHandlers[type].get(handler);
+        if (!eventHandlers[type].has(handler) || !eventSubscription) {
+            return;
+        }
+        eventSubscription.remove();
+        eventHandlers[type].delete(handler);
+    },
+    removeAllListeners() {
+        for (const eventName of eventNames) {
+            eventEmitter.removeAllListeners(eventName);
+        }
+    },
 };
 //# sourceMappingURL=AdMobInterstitial.js.map
