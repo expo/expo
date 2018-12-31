5688aaa05ffcebb59457e5bf2baf63bcf9b88010
Evan Bacon <baconbrix@gmail.com>
2018-12-31 15:30:41 -0600

    [Web] Added Ads (#3074)
    * [Web] Added Ads
    * Ran Prettier

--- packages/expo-ads-admob/src/AdMobRewarded.ts
+++ packages/expo-ads-admob/src/AdMobRewarded.ts
@@ -1,8 +1,9 @@
-import { EventEmitter, NativeModulesProxy, Platform, Subscription } from 'expo-core';
+import { EventEmitter, Subscription } from 'expo-core';
+import { UnavailabilityError } from 'expo-errors';
 
-const AdMobRewardedVideoAdManager: any = NativeModulesProxy.ExpoAdsAdMobRewardedVideoAdManager;
+import AdMobNativeModule from './ExpoAdsAdMobRewardedVideoAdManager';
 
-const adMobRewardedEventEmitter = new EventEmitter(AdMobRewardedVideoAdManager);
+const moduleName = 'AdMobRewarded';
 
 const eventNames = [
   'rewardedVideoDidRewardUser',
@@ -23,51 +24,77 @@ type EventNameType =
   | 'rewardedVideoDidClose'
   | 'rewardedVideoWillLeaveApplication';
 
+const eventEmitter = new EventEmitter(AdMobNativeModule);
+
 type EventListener = (...args: any[]) => void;
 
 const eventHandlers: { [eventName: string]: Map<EventListener, Subscription> } = {};
 
-eventNames.forEach(eventName => {
+for (const eventName of eventNames) {
   eventHandlers[eventName] = new Map();
-});
-
-const addEventListener = (type: EventNameType, handler: EventListener) => {
-  if (eventNames.includes(type)) {
-    eventHandlers[type].set(handler, adMobRewardedEventEmitter.addListener(type, handler));
-  } else {
-    console.log(`Event with type ${type} does not exist.`);
-  }
-};
-
-const removeEventListener = (type: EventNameType, handler: EventListener) => {
-  const eventSubscription = eventHandlers[type].get(handler);
-  if (!eventHandlers[type].has(handler) || !eventSubscription) {
-    return;
-  }
-  eventSubscription.remove();
-  eventHandlers[type].delete(handler);
-};
-
-const removeAllListeners = () =>
-  eventNames.forEach(eventName => adMobRewardedEventEmitter.removeAllListeners(eventName));
+}
 
 export default {
-  setAdUnitID: (id: string): Promise<void> => AdMobRewardedVideoAdManager.setAdUnitID(id),
-  setTestDeviceID: (id: string): Promise<void> => AdMobRewardedVideoAdManager.setTestDeviceID(id),
-  requestAdAsync: (): Promise<void> => AdMobRewardedVideoAdManager.requestAd(),
-  showAdAsync: (): Promise<void> => AdMobRewardedVideoAdManager.showAd(),
-  dismissAdAsync: (): Promise<void> =>
-    new Promise((resolve, reject) => {
-      if (Platform.OS === 'ios') {
-        AdMobRewardedVideoAdManager.dismissAd()
-          .then(resolve)
-          .catch(reject);
-      } else {
-        reject(new Error('Dismissing ads programmatically is supported only on iOS.'));
-      }
-    }),
-  getIsReadyAsync: (): Promise<boolean> => AdMobRewardedVideoAdManager.getIsReady(),
-  addEventListener,
-  removeEventListener,
-  removeAllListeners,
+  async setAdUnitID(id: string): Promise<void> {
+    if (!AdMobNativeModule.setAdUnitID) {
+      throw new UnavailabilityError(moduleName, 'setAdUnitID');
+    }
+
+    await AdMobNativeModule.setAdUnitID(id);
+  },
+  async setTestDeviceID(id: string): Promise<void> {
+    if (!AdMobNativeModule.setTestDeviceID) {
+      throw new UnavailabilityError(moduleName, 'setTestDeviceID');
+    }
+
+    await AdMobNativeModule.setTestDeviceID(id);
+  },
+  async requestAdAsync(): Promise<void> {
+    if (!AdMobNativeModule.requestAd) {
+      throw new UnavailabilityError(moduleName, 'requestAdAsync');
+    }
+
+    await AdMobNativeModule.requestAd();
+  },
+  async showAdAsync(): Promise<void> {
+    if (!AdMobNativeModule.showAd) {
+      throw new UnavailabilityError(moduleName, 'showAdAsync');
+    }
+
+    await AdMobNativeModule.showAd();
+  },
+  async dismissAdAsync(): Promise<void> {
+    if (!AdMobNativeModule.dismissAd) {
+      throw new UnavailabilityError(moduleName, 'dismissAdAsync');
+    }
+
+    await AdMobNativeModule.dismissAd();
+  },
+  async getIsReadyAsync(): Promise<boolean> {
+    if (!AdMobNativeModule.getIsReady) {
+      throw new UnavailabilityError(moduleName, 'getIsReadyAsync');
+    }
+
+    return await AdMobNativeModule.getIsReady();
+  },
+  addEventListener(type: EventNameType, handler: EventListener) {
+    if (eventNames.includes(type)) {
+      eventHandlers[type].set(handler, eventEmitter.addListener(type, handler));
+    } else {
+      console.log(`Event with type ${type} does not exist.`);
+    }
+  },
+  removeEventListener(type: EventNameType, handler: EventListener) {
+    const eventSubscription = eventHandlers[type].get(handler);
+    if (!eventHandlers[type].has(handler) || !eventSubscription) {
+      return;
+    }
+    eventSubscription.remove();
+    eventHandlers[type].delete(handler);
+  },
+  removeAllListeners() {
+    for (const eventName of eventNames) {
+      eventEmitter.removeAllListeners(eventName);
+    }
+  },
 };
