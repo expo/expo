459a6652eb58fe6d3eb617767a0f58c497001d8c
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2019-01-04 15:12:33 +0100

    Release SDK32 (#3101)
    * [tools-public] fix generate-dynamic-macros-android.sh
    
    * [sdk] bump react-native-reanimated JS version to 1.0.0-alpha.11
    
    * [ncl] add missing setMinimumIntervalAsync call to background fetch example
    
    * [packages] Do not publish babel.config.js in universal modules (#3099)
    
    # Why
    
    With `babel.config.js` published inside the packages, Jest transpiles their source code with included configuration (which isn't, and isn't supposed to be, valid outside of Expo monorepo).
    
    # How
    
    Added `.npmignore` file, excluding `babel.config.js` to `expo-module-scripts/templates`, ran:
    
    ```sh
    # in packages
    for package in ./*; do pushd $package; yarn; popd; done
    ```
    
    # Test Plan
    
    - [x] Publish prerelease packages
    - [x] Confirm that tests work now out of the box
    
    * [expokit-npm-package] bump version to v32.0.0
    
    * [fastlane] fix `open` function throwing error when trying to open from uri
    
    * Publish SDK32
    
    expo-ads-admob@2.0.0
    expo-app-auth@2.0.0
    expo-app-loader-provider@1.0.0
    expo-background-fetch@1.0.0
    expo-barcode-scanner@2.0.0
    expo-barcode-scanner-interface@2.0.0
    expo-camera@2.0.0
    expo-camera-interface@2.0.0
    expo-constants@2.0.0
    expo-constants-interface@2.0.0
    expo-contacts@2.0.0
    expo-core@2.0.0
    expo-errors@1.0.0
    expo-face-detector@2.0.0
    expo-face-detector-interface@2.0.0
    expo-file-system@2.0.0
    expo-file-system-interface@2.0.0
    expo-firebase-analytics@2.0.0
    expo-firebase-app@2.0.0
    expo-firebase-auth@2.0.0
    expo-firebase-crashlytics@2.0.0
    expo-firebase-database@2.0.0
    expo-firebase-firestore@2.0.0
    expo-firebase-functions@2.0.0
    expo-firebase-instance-id@2.0.0
    expo-firebase-invites@2.0.0
    expo-firebase-links@2.0.0
    expo-firebase-messaging@2.0.0
    expo-firebase-notifications@2.0.0
    expo-firebase-performance@2.0.0
    expo-firebase-remote-config@2.0.0
    expo-firebase-storage@2.0.0
    expo-font@2.0.0
    expo-font-interface@2.0.0
    expo-google-sign-in@2.0.0
    expo-gl@2.0.0
    expo-gl-cpp@2.0.0
    expo-image-loader-interface@2.0.0
    expo-local-authentication@2.0.0
    expo-localization@2.0.0
    expo-location@2.0.0
    expo-media-library@2.0.0
    expo-payments-stripe@2.0.0
    expo-permissions@2.0.0
    expo-permissions-interface@2.0.0
    expo-print@2.0.0
    expo-react-native-adapter@2.0.0
    expo-analytics-segment@2.0.0
    expo-sensors@2.0.0
    expo-sensors-interface@2.0.0
    expo-sms@2.0.0
    expo-task-manager@1.0.0
    expo-task-manager-interface@1.0.0
    expo-asset@2.0.0
    expo-module-template@2.0.0
    expo@32.0.0
    
    * [jest-expo] Publish jest-expo@32.0.0
    
    * [expokit] Update for final SDK32 (#3107)
    
    * [expokit] Update for final SDK32
    
    * [expokit] Publish expokit@32.0.1-rc.0
    
    * [expokit] Publish expokit@32.0.1
    
    * [wip][docs] Update (#3108)
    
    [docs] add Android ExpoKit upgrade steps and missing docs for notification categories
    
    * Update CHANGELOG.md
    
    Co-Authored-By: tsapeta <tsapeta@users.noreply.github.com>

--- packages/jest-expo/src/setup.js
+++ packages/jest-expo/src/setup.js
@@ -164,6 +164,40 @@ jest.mock('react-native/Libraries/Image/AssetRegistry', () => ({
   })),
 }));
 
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
+
 jest.doMock('react-native/Libraries/BatchedBridge/NativeModules', () => mockNativeModules);
 
 jest.mock('expo-react-native-adapter', () => {
