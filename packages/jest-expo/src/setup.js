1a755ec85db0f58bff753e2369da062fa9f651f2
Stanisław Chmiela <sjchmiela@gmail.com>
2019-01-04 10:56:00 +0100

    [jest-expo] Publish jest-expo@32.0.0-rc.3 with fixed out-of-scope var access

--- packages/jest-expo/src/setup.js
+++ packages/jest-expo/src/setup.js
@@ -11,7 +11,6 @@ global.Headers = Headers;
 global.fetch = fetch;
 
 const mockNativeModules = require('react-native/Libraries/BatchedBridge/NativeModules');
-const View = require('react-native/Libraries/Components/View/View');
 
 const createMockConstants = require('./createMockConstants');
 
@@ -165,37 +164,39 @@ jest.mock('react-native/Libraries/Image/AssetRegistry', () => ({
   })),
 }));
 
-jest.mock('react-native-gesture-handler', () => ({
-  Swipeable: View,
-  DrawerLayout: View,
-  State: {},
-  ScrollView: View,
-  Slider: View,
-  Switch: View,
-  TextInput: View,
-  ToolbarAndroid: View,
-  ViewPagerAndroid: View,
-  DrawerLayoutAndroid: View,
-  WebView: View,
-  NativeViewGestureHandler: View,
-  TapGestureHandler: View,
-  FlingGestureHandler: View,
-  ForceTouchGestureHandler: View,
-  LongPressGestureHandler: View,
-  PanGestureHandler: View,
-  PinchGestureHandler: View,
-  RotationGestureHandler: View,
-  State: {},
-  /* Buttons */
-  RawButton: View,
-  BaseButton: View,
-  RectButton: View,
-  BorderlessButton: View,
-  /* Other */
-  FlatList: View,
-  gestureHandlerRootHOC: jest.fn(),
-  Directions: {},
-}));
+jest.mock('react-native-gesture-handler', () => {
+  const View = require('react-native/Libraries/Components/View/View');
+  return {
+    Swipeable: View,
+    DrawerLayout: View,
+    State: {},
+    ScrollView: View,
+    Slider: View,
+    Switch: View,
+    TextInput: View,
+    ToolbarAndroid: View,
+    ViewPagerAndroid: View,
+    DrawerLayoutAndroid: View,
+    WebView: View,
+    NativeViewGestureHandler: View,
+    TapGestureHandler: View,
+    FlingGestureHandler: View,
+    ForceTouchGestureHandler: View,
+    LongPressGestureHandler: View,
+    PanGestureHandler: View,
+    PinchGestureHandler: View,
+    RotationGestureHandler: View,
+    /* Buttons */
+    RawButton: View,
+    BaseButton: View,
+    RectButton: View,
+    BorderlessButton: View,
+    /* Other */
+    FlatList: View,
+    gestureHandlerRootHOC: jest.fn(),
+    Directions: {},
+  };
+});
 
 jest.doMock('react-native/Libraries/BatchedBridge/NativeModules', () => mockNativeModules);
 
