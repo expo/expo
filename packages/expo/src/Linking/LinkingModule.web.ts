8153aaba7c8d9e510f9be92dbc80c87c5d20b5ef
Evan Bacon <baconbrix@gmail.com>
2019-01-04 13:54:07 -0800

    Added event listeners to Linking API

--- packages/expo/src/Linking/LinkingModule.web.ts
+++ packages/expo/src/Linking/LinkingModule.web.ts
@@ -1,30 +1,58 @@
+import findIndex from 'array-find-index';
 import invariant from 'invariant';
 
-class Linking {
-  constructor() {}
+// TODO: Bacon: Is there enough coverage to use Array.prototype.findIndex() directly: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex#Browser_compatibility
 
-  async canOpenURL(url: string): Promise<boolean> {
-    this._validateURL(url);
-    return true;
-  }
+type NativeEvent = { data: any; origin: string; source: any };
+type EventType = { url: string; nativeEvent: NativeEvent };
+type HandlerFunction = (event: EventType) => {};
 
-  async getInitialURL(): Promise<string> {
-    return window.location.href;
-  }
+// TODO: Bacon: Should we alias url to `hashchange` or `popstate`: https://stackoverflow.com/a/44318564/4047926
 
-  async openURL(url: string): Promise<void> {
-    this._validateURL(url);
-    window.location.href = url;
-  }
+const EventTypes = ['url'];
 
-  addEventListener(type: string, handler: Function): void {}
+const messageEventKey = 'message';
 
-  removeEventListener(type: string, handler: Function): void {}
+const listeners = [];
 
-  _validateURL(url: string): void {
-    invariant(typeof url === 'string', 'Invalid URL: should be a string. Was: ' + url);
-    invariant(url, 'Invalid URL: cannot be empty');
-  }
+function _validateURL(url: string): void {
+  invariant(typeof url === 'string', `Invalid URL: should be a string. Instead found: ${url}`);
+  invariant(url, 'Invalid URL: cannot be empty');
 }
 
-export default new Linking();
+export default {
+  addEventListener(type: 'url', handler: HandlerFunction): void {
+    invariant(
+      EventTypes.indexOf(type) !== -1,
+      `Linking.addEventListener(): ${type} is not a valid event`
+    );
+    const callback = nativeEvent => handler({ url: window.location.href, nativeEvent });
+    listeners.push([handler, callback]);
+    window.addEventListener(messageEventKey, callback, false);
+  },
+  removeEventListener(type: 'url', handler: HandlerFunction): void {
+    invariant(
+      EventTypes.indexOf(type) !== -1,
+      `Linking.removeEventListener(): ${type} is not a valid event`
+    );
+    const listenerIndex = findIndex(listeners, pair => pair[0] === handler);
+    invariant(
+      listenerIndex !== -1,
+      'cannot invoke Linking.removeEventListener with an unregistered event handler.'
+    );
+    const callback = listeners[listenerIndex][1];
+    window.removeEventListener(messageEventKey, callback, false);
+    listeners.splice(listenerIndex, 1);
+  },
+  async canOpenURL(url: string): Promise<boolean> {
+    _validateURL(url);
+    return true;
+  },
+  async getInitialURL(): Promise<string> {
+    return window.location.href;
+  },
+  async openURL(url: string): Promise<void> {
+    _validateURL(url);
+    window.location.href = url;
+  },
+};
