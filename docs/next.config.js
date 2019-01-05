019dc3bf1468b889ab597ba32785412d39c09d73
Nick Novitski <nick@expo.io>
2019-01-04 18:07:10 -0800

    [docs] ".html" fix also works for "/latest" paths

--- docs/next.config.js
+++ docs/next.config.js
@@ -19,7 +19,12 @@ module.exports = {
 
         // For every path for the latest version, create a matching `latest` one
         if (pathname.match(LATEST_VERSION)) {
-          result[pathname.replace(LATEST_VERSION, 'latest')] = page;
+          latestPath = pathname.replace(LATEST_VERSION, 'latest');
+          result[latestPath] = page;
+          // TODO: fix duplication
+          if (! pathname.match(/\.html$/) && pathname.match(/[a-z]$/)) {
+            result[latestPath + ".html"] = page;
+          }
         }
 
         return result;
