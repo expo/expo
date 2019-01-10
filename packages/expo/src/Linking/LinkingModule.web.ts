be5c6fde6652d8970debd7d17aff279fee13253c
Evan Bacon <baconbrix@gmail.com>
2019-01-10 15:18:40 -0800

    Update LinkingModule.web.ts

--- packages/expo/src/Linking/LinkingModule.web.ts
+++ packages/expo/src/Linking/LinkingModule.web.ts
@@ -7,7 +7,7 @@ const EventTypes = ['url'];
 // TODO: Bacon: Should we also listen to `hashchange`: https://stackoverflow.com/a/44318564/4047926
 const messageEventKey = 'message';
 
-const listeners: Array<[Listener, any]> = [];
+const listeners: Array<{ listener: Listener; nativeListener: NativeListener }> = [];
 
 function _validateURL(url: string): void {
   invariant(typeof url === 'string', `Invalid URL: should be a string. Instead found: ${url}`);
@@ -23,7 +23,7 @@ class Linking {
     );
     const nativeListener: NativeListener = nativeEvent =>
       listener({ url: window.location.href, nativeEvent });
-    listeners.push([listener, nativeListener]);
+    listeners.push({ listener, nativeListener });
     window.addEventListener(messageEventKey, nativeListener, false);
   }
 
@@ -32,12 +32,12 @@ class Linking {
       EventTypes.indexOf(type) !== -1,
       `Linking.removeEventListener(): ${type} is not a valid event.`
     );
-    const listenerIndex = listeners.findIndex(pair => pair[0] === listener);
+    const listenerIndex = listeners.findIndex(pair => pair.listener === listener);
     invariant(
       listenerIndex !== -1,
       'Linking.removeEventListener(): cannot remove an unregistered event listener.'
     );
-    const nativeListener = listeners[listenerIndex][1];
+    const nativeListener = listeners[listenerIndex].nativeListener;
     window.removeEventListener(messageEventKey, nativeListener, false);
     listeners.splice(listenerIndex, 1);
   }
