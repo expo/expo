89c9a9ecee2e15ad0e5f7567b7efaa0defb21f43
Nick Novitski <nick@expo.io>
2019-01-11 11:17:48 -0800

    [docs] Simplify server.js
    - remove category aliases (all links are fixed)
    - remove production logic (we now export static files for webserver)
    - 404 on links that would redirect in production

--- docs/server.js
+++ docs/server.js
@@ -1,75 +1,24 @@
 const { parse } = require('url');
 const next = require('next');
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const compression = require('compression');
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const { createServer } = require('http');
+const app = next({ dev: true });
 const port = 3000;
 const handle = app.getRequestHandler();
 
-const { WORKFLOW, DISTRIBUTION, EXPOKIT } = require('./transition/sections');
-const CATEGORY_ALIASES = [
-  { path: 'workflow', files: WORKFLOW },
-  { path: 'distribution', files: DISTRIBUTION },
-  { path: 'expokit', files: EXPOKIT },
-];
-
-const mutateCategoryWithRedirectAlias = (category, post) => {
-  if (category.toLowerCase() === 'guides') {
-    for (let i = 0; i < CATEGORY_ALIASES.length; i++) {
-      const alias = CATEGORY_ALIASES[i];
-      if (alias.files.indexOf(post) > -1) {
-        category = alias.path;
-      }
-    }
-  }
-
-  return category;
-};
-
 app.prepare().then(() => {
-  const server = express();
-
-  server.use('/static', express.static('static'));
-  server.use(
-    bodyParser.urlencoded({
-      extended: false,
-    })
-  );
-  server.use(
-    cors({
-      origin: '*',
-    })
-  );
-
-  if (!dev) {
-    server.use(compression());
-  }
-
-  // NOTE(jim): Mutations have to line up with FS paths provided by mdjs.
-  server.get('/versions/:version/:category/:post/', (req, res) => {
-    const { query } = parse(req.url, true);
-    let { version, category, post } = req.params;
-
-    category = mutateCategoryWithRedirectAlias(category, post);
-
-    const updatedPath = `/versions/${version}/${category}/${post}`;
-    app.render(req, res, updatedPath, query);
-  });
-
-  server.get('/', (req, res) => {
+  createServer((req, res) => {
     const { pathname, query } = parse(req.url, true);
-    app.render(req, res, pathname, query)
-  })
-
-  server.get('*', (req, res) => {
-    const { pathname, query } = parse(req.url, true);
-    app.render(req, res, pathname.slice(0,-1), query);
-  });
-
-  server.listen(port, err => {
+    if (pathname === '/' || pathname.startsWith('/static') || pathname.startsWith('/_next')) {
+      handle(req, res);
+    } else if (pathname.endsWith('/')) {
+      // Respond to `foo/`, matching production behavior
+      // (If next could be configured to do this we could delete this entire file)
+      app.render(req, res, pathname.slice(0, -1), query);
+    } else {
+      // 404 for page `foo` even if `foo/` exists to notice link mistakes more easily in dev
+      app.render404(req, res);
+    }
+  }).listen(port, err => {
     if (err) {
       throw err;
     }
