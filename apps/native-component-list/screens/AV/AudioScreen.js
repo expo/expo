7bbf5bf129b5b7d57e944eec7e4ed986020bc7fb
Stanisław Chmiela <sjchmiela@gmail.com>
2019-01-17 12:01:42 +0100

    [ncl] Add basic Video screen

--- apps/native-component-list/screens/AV/AudioScreen.js
+++ apps/native-component-list/screens/AV/AudioScreen.js
@@ -45,24 +45,6 @@ const styles = StyleSheet.create({
   contentContainer: {
     padding: 10,
   },
-  text: {
-    marginVertical: 15,
-    marginHorizontal: 10,
-  },
-  faintText: {
-    color: '#888',
-    marginHorizontal: 30,
-  },
-  oopsTitle: {
-    fontSize: 25,
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-  oopsText: {
-    textAlign: 'center',
-    marginTop: 10,
-    marginHorizontal: 30,
-  },
   player: {
     borderBottomWidth: 1.0 / PixelRatio.get(),
     borderBottomColor: '#cccccc',
