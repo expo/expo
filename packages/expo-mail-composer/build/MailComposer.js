739000c3609d641530f0c3405a1d2dd34efacc89
Stanisław Chmiela <sjchmiela@gmail.com>
2019-01-18 09:36:27 +0100

    [expo-mail-composer] Move MailComposer from expo to expo-mail-composer

---
+++ packages/expo-mail-composer/build/MailComposer.js
@@ -0,0 +1,5 @@
+import ExponentMailComposer from './ExponentMailComposer';
+export async function composeAsync(options) {
+    return await ExponentMailComposer.composeAsync(options);
+}
+//# sourceMappingURL=MailComposer.js.map
