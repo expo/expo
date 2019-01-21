f61bc6c01ad6256f930ac5d19ad6c678bc59990b
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2019-01-21 13:41:24 +0100

    Apply suggestions from code review
    Co-Authored-By: Szymon20000 <szymon20000@gmail.com>

--- packages/expo-sqlite/ios/EXSQlite/EXSQLite.m
+++ packages/expo-sqlite/ios/EXSQlite/EXSQLite.m
@@ -91,8 +91,8 @@ EX_EXPORT_METHOD_AS(exec,
 
 EX_EXPORT_METHOD_AS(close,
                     close:(NSString *)dbName
-                 resolver:(EXPromiseResolveBlock)resolve
-                 rejecter:(EXPromiseRejectBlock)reject)
+                    resolver:(EXPromiseResolveBlock)resolve
+                    rejecter:(EXPromiseRejectBlock)reject)
 {
   @synchronized(self) {
     [cachedDatabases removeObjectForKey:dbName];
