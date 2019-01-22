a1cb149ae4e24047fca35684a61ff1ca7f9274fa
Evan Bacon <baconbrix@gmail.com>
2019-01-22 15:13:10 -0800

    Added web support to `babel-preset-expo`
    * Bump `5.0.0` -> `5.1.0`
    * Removed `react-native-web` preset from NCL
    * Added `react-native-web` preset to `babel-preset-expo`

--- apps/native-component-list/babel.config.js
+++ apps/native-component-list/babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
-  const plugins = process.env.REACT_NATIVE_APP_ROOT
-    ? ['@babel/plugin-transform-runtime']
-    : ['babel-plugin-react-native-web', '@babel/plugin-transform-runtime']
   return {
     presets: ['babel-preset-expo'],
-    plugins,
+    plugins: ['@babel/plugin-transform-runtime'],
   };
 };
--- packages/babel-preset-expo/index.js
+++ packages/babel-preset-expo/index.js
@@ -1,16 +1,23 @@
 module.exports = function() {
+  const plugins = [
+    [
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          'react-native-vector-icons': '@expo/vector-icons',
+        },
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+  ];
+
+  // On web add the `react-native-web` plugin.
+  if (!process.env.REACT_NATIVE_APP_ROOT) {
+    plugins.push('babel-plugin-react-native-web');
+  }
+
   return {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'babel-plugin-module-resolver',
-        {
-          alias: {
-            'react-native-vector-icons': '@expo/vector-icons',
-          },
-        },
-      ],
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ],
+    plugins,
   };
 };
