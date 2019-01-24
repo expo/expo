7af7d0b686832007b69f1a949ec9dca2d57c5171
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2019-01-24 15:39:37 +0100

    [android][ios][ts][docs][ncl] implement background fetch on Android (#3281)
    # Why
    
    Closes https://expo.canny.io/feature-requests/p/android-background-fetch-support
    
    # How
    
    Android doesn't have its own background fetch feature, however I could easily imitate its behavior using `AlarmManager` with wakeup option.
    I also had to change a few things in `TaskManager`, the most important one is the change in pending intents. Now, two pending intents created for the same task will be equal (see https://developer.android.com/reference/android/content/Intent#filterEquals(android.content.Intent)). Thanks to this, it's easier to cancel existing pending intents.
    
    # Test Plan
    
    Tested against `BackgroundFetch` example in `native-component-list`

--- packages/expo-background-fetch/android/src/main/java/expo/modules/backgroundfetch/BackgroundFetchModule.java
+++ packages/expo-background-fetch/android/src/main/java/expo/modules/backgroundfetch/BackgroundFetchModule.java
@@ -2,9 +2,18 @@ package expo.modules.backgroundfetch;
 
 import android.content.Context;
 
-import expo.core.ExportedModule;
+import java.util.Map;
 
-class BackgroundFetchModule extends ExportedModule {
+import expo.core.ExportedModule;
+import expo.core.ModuleRegistry;
+import expo.core.Promise;
+import expo.core.interfaces.ExpoMethod;
+import expo.core.interfaces.ModuleRegistryConsumer;
+import expo.interfaces.taskManager.TaskManagerInterface;
+
+class BackgroundFetchModule extends ExportedModule implements ModuleRegistryConsumer {
+  private TaskManagerInterface mTaskManager;
+
   public BackgroundFetchModule(Context context) {
     super(context);
   }
@@ -12,5 +21,30 @@ class BackgroundFetchModule extends ExportedModule {
   @Override
   public String getName() {
     return "ExpoBackgroundFetch";
+  }
+
+  @Override
+  public void setModuleRegistry(ModuleRegistry moduleRegistry) {
+    mTaskManager = moduleRegistry.getModule(TaskManagerInterface.class);
+  }
+
+  @ExpoMethod
+  public void registerTaskAsync(String taskName, Map<String, Object> options, final Promise promise) {
+    try {
+      mTaskManager.registerTask(taskName, BackgroundFetchTaskConsumer.class, options);
+      promise.resolve(null);
+    } catch (Exception e) {
+      promise.reject(e);
+    }
+  }
+
+  @ExpoMethod
+  public void unregisterTaskAsync(String taskName, final Promise promise) {
+    try {
+      mTaskManager.unregisterTask(taskName, BackgroundFetchTaskConsumer.class);
+      promise.resolve(null);
+    } catch (Exception e) {
+      promise.reject(e);
+    }
   }
 }
