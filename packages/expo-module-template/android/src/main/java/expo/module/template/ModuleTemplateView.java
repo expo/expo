a1dd2616fb85a985f5a79b54bc219dd8346bedd1
Bartłomiej Bukowski <bartlomiejbukowski.b@gmail.com>
2019-01-24 10:06:40 +0100

    [expo-module-template] Create new typescript ready universal module template (#3060)
    * Create TS universal module template
    
    * [docs] Add logs for new module generation command
    
    * Fix Android & iOS template
    
    * Adjust expo-module-template
    
    * Adjust docs

---
+++ packages/expo-module-template/android/src/main/java/expo/module/template/ModuleTemplateView.java
@@ -0,0 +1,12 @@
+package expo.module.template;
+
+import android.content.Context;
+import android.view.View;
+
+import expo.core.ModuleRegistry;
+
+public class ModuleTemplateView extends View {
+  public ModuleTemplateView(Context context, ModuleRegistry moduleRegistry) {
+    super(context);
+  }
+}
