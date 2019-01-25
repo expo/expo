cc166296bf9932d74e07c26553177b6f31b2b74e
Evan Bacon <baconbrix@gmail.com>
2019-01-25 12:11:07 -0800

    [web] Added web support to `babel-preset-expo` (#3293)
    * Added web support to `babel-preset-expo`
    
    * Bump `5.0.0` -> `5.1.0`
    * Removed `react-native-web` preset from NCL
    * Added `react-native-web` preset to `babel-preset-expo`
    
    * Merge
    
    * reverted modules

--- packages/babel-preset-expo/index.js
+++ packages/babel-preset-expo/index.js
@@ -1,19 +1,26 @@
 module.exports = function(api) {
-  const isWeb = api.caller(isTargetWeb); // eslint-disable-line no-unused-vars
+  const isWeb = api.caller(isTargetWeb);
+
+  const plugins = [
+    [
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          'react-native-vector-icons': '@expo/vector-icons',
+        },
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+  ];
+
+  // On web add the `react-native-web` plugin.
+  if (isWeb) {
+    plugins.push('babel-plugin-react-native-web');
+  }
 
   return {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      [
-        'babel-plugin-module-resolver',
-        {
-          alias: {
-            'react-native-vector-icons': '@expo/vector-icons',
-          },
-        },
-      ],
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ],
+    plugins,
   };
 };
 
