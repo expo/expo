d04634b8c94daffff8ad9832a8bf4d524d02bc5d
Evan Bacon <baconbrix@gmail.com>
2019-01-25 13:18:41 -0800

    Update Haptic-test.ts

--- packages/expo/src/__tests__/Haptic-test.ts
+++ packages/expo/src/__tests__/Haptic-test.ts
@@ -1,57 +1,77 @@
 import { UnavailabilityError } from 'expo-errors';
-import { mockPlatformAndroid, mockPlatformWeb, mockProperty, unmockAllProperties } from 'jest-expo';
-import { NativeModules, Platform } from 'react-native';
+import {
+  mockPlatformAndroid,
+  mockPlatformIOS,
+  mockPlatformWeb,
+  mockProperty,
+  unmockAllProperties,
+} from 'jest-expo';
+import { NativeModules } from 'react-native';
 
 import * as Haptic from '../Haptic/Haptic';
 
-describe('Haptic', () => {
-  describe('iOS', () => {
-    describe('selection()', () => {
-      it("Doesn't throw an error", async () => {
-        await Haptic.selection();
-      });
-    });
-    describe('impact()', () => {
-      it("Doesn't throw an error", async () => {
-        await Haptic.impact();
-      });
-      it('Throws an error when an unexpected type is provided', async () => {
-        const invalidInput: any = 'invalid input';
-        expect(Haptic.impact(invalidInput)).rejects.toThrowError('Invariant Violation');
-      });
-      it('Can perform any impact style', async () => {
-        const { Light, Medium, Heavy } = Haptic.ImpactFeedbackStyle;
-        for (const style of [Light, Medium, Heavy]) {
-          await Haptic.impact(style as Haptic.ImpactFeedbackStyle);
-        }
-      });
-    });
-    describe('notification()', () => {
-      it("Doesn't throw an error", async () => {
-        await Haptic.notification();
-      });
-      it('Throws an error when an unexpected type is provided', async () => {
-        const invalidInput: any = 'invalid input';
-        expect(Haptic.notification(invalidInput)).rejects.toThrowError('Invariant Violation');
-      });
-      it('Can perform any notification type', async () => {
-        const { Warning, Success, Error } = Haptic.NotificationFeedbackType;
-        for (const type of [Warning, Success, Error]) {
-          await Haptic.notification(type);
-        }
-      });
+describe(' Haptic', () => {
+  beforeEach(() => {
+    mockPlatformIOS();
+  });
+
+  afterEach(() => {
+    unmockAllProperties();
+  });
+
+  describe(`selection()`, () => {
+    it(`doesn't throw an error`, async () => {
+      await Haptic.selection();
     });
   });
 
-  const { selection, impact, notification } = Haptic;
-  const methods = {
-    selection,
-    impact,
-    notification,
-  };
+  describe(`impact()`, () => {
+    it(`doesn't throw an error`, async () => {
+      await Haptic.impact();
+    });
+
+    it(`throws an error when an unexpected type is provided`, async () => {
+      const invalidInput: any = 'invalid input';
+      expect(Haptic.impact(invalidInput)).rejects.toThrowError('Invariant Violation');
+    });
+
+    it(`can perform any impact style`, async () => {
+      const { Light, Medium, Heavy } = Haptic.ImpactFeedbackStyle;
+      for (const style of [Light, Medium, Heavy]) {
+        await Haptic.impact(style as Haptic.ImpactFeedbackStyle);
+      }
+    });
+  });
+
+  describe(`notification()`, () => {
+    it(`doesn't throw an error`, async () => {
+      await Haptic.notification();
+    });
+
+    it(`throws an error when an unexpected type is provided`, async () => {
+      const invalidInput: any = 'invalid input';
+      expect(Haptic.notification(invalidInput)).rejects.toThrowError('Invariant Violation');
+    });
+
+    it(`can perform any notification type`, async () => {
+      const { Warning, Success, Error } = Haptic.NotificationFeedbackType;
+      for (const type of [Warning, Success, Error]) {
+        await Haptic.notification(type);
+      }
+    });
+  });
+});
+
+const { selection, impact, notification } = Haptic;
+const methods = {
+  selection,
+  impact,
+  notification,
+};
+describeUnsupportedPlatforms(`Haptic`, () => {
   for (const methodName in methods) {
-    describeUnsupportedPlatforms(`Haptic.${methodName}()`, () => {
-      it(`is unavailable on ${Platform.OS}`, async () => {
+    describe(`${methodName}()`, () => {
+      it(`is unavailable`, async () => {
         try {
           const method = methods[methodName];
           await method();
@@ -65,7 +85,6 @@ describe('Haptic', () => {
 });
 
 function applyMocks() {
-  mockPlatformWeb();
   ['selection', 'impact', 'notification'].forEach(methodName => {
     mockProperty(NativeModules.ExponentHaptic, methodName, null);
   });
@@ -73,15 +92,19 @@ function applyMocks() {
 
 export function describeUnsupportedPlatforms(message, tests) {
   describe(`🕸  ${message}`, () => {
-    beforeEach(applyMocks);
-    mockPlatformWeb();
+    beforeEach(() => {
+      applyMocks();
+      mockPlatformWeb();
+    });
     tests();
-    afterAll(unmockAllProperties);
+    afterEach(unmockAllProperties);
   });
   describe(`🤖 ${message}`, () => {
-    beforeEach(applyMocks);
-    mockPlatformAndroid();
+    beforeEach(() => {
+      applyMocks();
+      mockPlatformAndroid();
+    });
     tests();
-    afterAll(unmockAllProperties);
+    afterEach(unmockAllProperties);
   });
 }
