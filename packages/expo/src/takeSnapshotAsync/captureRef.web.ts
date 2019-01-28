ae1793d0c7779b4f1c0bf86f57c7d69914a73884
Evan Bacon <baconbrix@gmail.com>
2019-01-27 22:44:04 -0800

    Added full-screen

--- packages/expo/src/takeSnapshotAsync/captureRef.web.ts
+++ packages/expo/src/takeSnapshotAsync/captureRef.web.ts
@@ -3,11 +3,13 @@ import { CaptureOptions } from 'react-native-view-shot';
 
 import * as Creator from './Creator.web';
 
+declare const document: Document;
+
 export default async function captureRef(
   component: Element,
   options: CaptureOptions = {}
 ): Promise<string | Uint8ClampedArray | Blob> {
-  const element = getElement(component);
+  const element = getElement(component || document.body);
   const { format = 'png' } = options;
   const finalFormat = format.toLowerCase();
   switch (finalFormat) {
