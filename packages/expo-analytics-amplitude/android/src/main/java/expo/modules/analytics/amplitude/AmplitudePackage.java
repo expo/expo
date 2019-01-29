b10c7ab3791afcedb732e0b1263104dac6d9da70
Stanisław Chmiela <sjchmiela@gmail.com>
2019-01-29 14:26:16 +0100

    [expo-analytics-amplitude] Move Android code to expo-analytics-amplitude

--- packages/expo-analytics-amplitude/android/src/main/java/expo/modules/analytics/amplitude/AmplitudePackage.java
+++ packages/expo-analytics-amplitude/android/src/main/java/expo/modules/analytics/amplitude/AmplitudePackage.java
@@ -7,16 +7,10 @@ import java.util.List;
 
 import expo.core.BasePackage;
 import expo.core.ExportedModule;
-import expo.core.ViewManager;
 
 public class AmplitudePackage extends BasePackage {
   @Override
   public List<ExportedModule> createExportedModules(Context context) {
     return Collections.singletonList((ExportedModule) new AmplitudeModule(context));
-  }
-
-  @Override
-  public List<ViewManager> createViewManagers(Context context) {
-    return Collections.singletonList((ViewManager) new AmplitudeViewManager());
   }
 }
