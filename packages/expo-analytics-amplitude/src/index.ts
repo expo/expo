8510ccc77994eb3c216001d3083c2b419921ec72
Stanisław Chmiela <sjchmiela@gmail.com>
2019-01-29 12:22:09 +0100

    [expo-analytics-amplitude] Move TS code to expo-analytics-amplitude

--- packages/expo-analytics-amplitude/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { NativeModulesProxy } from 'expo-core';
-
-const { ExpoAmplitude } = NativeModulesProxy;
-
-export { default as ExpoAmplitudeView } from './ExpoAmplitudeView';
-
-export async function someGreatMethodAsync(options: any) {
-  return await ExpoAmplitude.someGreatMethodAsync(options);
-}
