b3b798a65a051ee0ee23f1c60cd3c6ab15e5be33
Evan Bacon <baconbrix@gmail.com>
2019-01-29 02:07:23 -0800

    move tests

--- packages/expo-firebase-database/tests/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Transactions } from './Transactions';
-export { default as Snapshot } from './Snapshot';
-export { default as RefSet } from './RefSet';
-export { default as RefOnce } from './RefOnce';
