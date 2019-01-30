02ef6e17d065bd681850083b0e630e9d84aa6895
Evan Bacon <baconbrix@gmail.com>
2019-01-30 15:57:23 -0800

    [web] Resolving all redirected modules to `expo` (#3297)
    * Resolving all redirected modules to `expo`
    
    * Removed extra redirects
    * Added web Platform to expo/web
    * Created better `AssetSourceResolver`, and other image utils
    * Fixed Linking bug
    
    * Reduced babel included files to save ~20 seconds on the initial build
    
    * Removed android

--- packages/expo/src/web/Network/RCTNetworking.ts
+++ packages/expo/src/web/Network/RCTNetworking.ts
@@ -1,2 +1,17 @@
-import { EventEmitter } from 'expo-core';
-export default new EventEmitter({ addListener() {}, removeListeners() {} });
+import RCTDeviceEventEmitter from 'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter';
+import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
+
+/*
+ * Used for sending native-layer events to API-layer NativeEventEmitter listeners.
+ */
+class GlobalPlatformEmitter extends EventEmitter {
+  constructor() {
+    super(RCTDeviceEventEmitter.sharedSubscriber);
+  }
+
+  emit(eventName: string, props: any): void {
+    super.emit(eventName, props);
+  }
+}
+
+export default new GlobalPlatformEmitter();
