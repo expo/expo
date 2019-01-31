b163c95543618a4845c1af7db946312ad105c801
Evan Bacon <baconbrix@gmail.com>
2019-01-30 20:38:10 -0800

    Added Action Sheet example (#3369)

--- apps/native-component-list/screens/ExpoApisScreen.js
+++ apps/native-component-list/screens/ExpoApisScreen.js
@@ -68,6 +68,7 @@ export default class ExpoApisScreen extends React.Component {
   _getApis = () => {
     return Platform.select({
       web: [
+        'ActionSheet',
         'AuthSession',
         'Constants',
         'DocumentPicker',
@@ -92,6 +93,7 @@ export default class ExpoApisScreen extends React.Component {
         'Util',
       ],
       default: [
+        'ActionSheet',
         'AppAuth',
         'Audio',
         'AuthSession',
