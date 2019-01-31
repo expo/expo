791a2b5a3a632a59cd6ddc213d2e9c595a1d0a27
Stanisław Chmiela <sjchmiela@gmail.com>
2019-01-31 11:13:39 +0100

    [expo-web-browser] Move iOS code to expo-web-browser

--- packages/expo-web-browser/ios/EXWebBrowser/EXWebBrowserViewManager.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2016-present 650 Industries. All rights reserved.
-
-#import <EXCore/EXViewManager.h>
-#import <EXCore/EXModuleRegistryConsumer.h>
-
-@interface EXWebBrowserViewManager : EXViewManager <EXModuleRegistryConsumer>
-
-@end
