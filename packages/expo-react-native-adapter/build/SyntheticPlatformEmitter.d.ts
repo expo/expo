e82c48075dd1da65909059ec04e27c3f0afe1546
Evan Bacon <baconbrix@gmail.com>
2019-01-31 17:47:11 -0800

    Created `SyntheticPlatformEmitter` (#3368)
    * Created `SyntheticPlatformEmitter`
    
    * Updated docs
    
    * Made emitter not extend EventEmitter

---
+++ packages/expo-react-native-adapter/build/SyntheticPlatformEmitter.d.ts
@@ -0,0 +1,6 @@
+declare class SyntheticPlatformEmitter {
+    _emitter: any;
+    emit(eventName: string, props: any): void;
+}
+declare const _default: SyntheticPlatformEmitter;
+export default _default;
