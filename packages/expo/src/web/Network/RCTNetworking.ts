6e65eae4d6378be6e74879aeb471d159a8789bc1
Evan Bacon <baconbrix@gmail.com>
2019-01-31 18:04:08 -0800

    Migrated to SyntheticPlatformEmitter

--- packages/expo/src/web/Network/RCTNetworking.ts
+++ packages/expo/src/web/Network/RCTNetworking.ts
@@ -1,17 +1,2 @@
-import RCTDeviceEventEmitter from 'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter';
-import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
-
-/*
- * Used for sending native-layer events to API-layer NativeEventEmitter listeners.
- */
-class GlobalPlatformEmitter extends EventEmitter {
-  constructor() {
-    super(RCTDeviceEventEmitter.sharedSubscriber);
-  }
-
-  emit(eventName: string, props: any): void {
-    super.emit(eventName, props);
-  }
-}
-
-export default new GlobalPlatformEmitter();
+import { SyntheticPlatformEmitter } from 'expo-core';
+export default SyntheticPlatformEmitter;
