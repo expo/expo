0d913cfe944385eb37e85919e9daa093d599d1e9
Evan Bacon <baconbrix@gmail.com>
2019-02-04 19:10:21 -0800

    Update LocalAuthentication-test.ts

--- packages/expo-local-authentication/src/__tests__/LocalAuthentication-test.ts
+++ packages/expo-local-authentication/src/__tests__/LocalAuthentication-test.ts
@@ -3,18 +3,19 @@ import { mockPlatformAndroid, mockPlatformIOS, unmockAllProperties } from 'jest-
 import ExpoLocalAuthentication from '../ExpoLocalAuthentication';
 import * as LocalAuthentication from '../LocalAuthentication';
 
-afterEach(unmockAllProperties);
-
 beforeEach(() => {
   ExpoLocalAuthentication.authenticateAsync.mockImplementation(async () => ({}));
 });
+
+afterEach(unmockAllProperties);
+
 it(`uses message on iOS`, async () => {
   mockPlatformIOS();
 
   const message = '<DEBUG_MESSAGE>';
   await LocalAuthentication.authenticateAsync(message);
 
-  expect(ExpoLocalAuthentication.authenticateAsync).toHaveBeenCalledWith(message);
+  expect(ExpoLocalAuthentication.authenticateAsync).toHaveBeenLastCalledWith(message);
 });
 
 it(`throws when an invalid message on is used iOS`, async () => {
@@ -22,6 +23,8 @@ it(`throws when an invalid message on is used iOS`, async () => {
 
   expect(LocalAuthentication.authenticateAsync(undefined)).rejects.toThrow();
   expect(LocalAuthentication.authenticateAsync('')).rejects.toThrow();
+  expect(LocalAuthentication.authenticateAsync({} as any)).rejects.toThrow();
+  expect(LocalAuthentication.authenticateAsync(123 as any)).rejects.toThrow();
 });
 
 it(`doesn't use a message on Android`, async () => {
@@ -30,5 +33,5 @@ it(`doesn't use a message on Android`, async () => {
   const message = '<DEBUG_MESSAGE>';
   await LocalAuthentication.authenticateAsync(message);
 
-  expect(ExpoLocalAuthentication.authenticateAsync).toHaveBeenCalledWith();
+  expect(ExpoLocalAuthentication.authenticateAsync).toHaveBeenLastCalledWith();
 });
