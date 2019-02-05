8b6eb81fd1f8d46047070b5e173feb9b72d206dc
Nick Novitski <nick@expo.io>
2019-02-05 13:21:08 -0800

    `./update-dependencies` and fixes
    - Remove custom xcpretty package
    - Add custom Android NDK version 17c package
    - Add custom cocoapods 1.5.3 package

--- android/install-ndk-17c.sh
+++ android/install-ndk-17c.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 scriptdir="$(dirname ${BASH_SOURCE[0]})"
 shellnix="$scriptdir/shell.nix"
 
-ndk=${1:-$(nix eval --file "$shellnix" --raw ndk)}
+ndkPkg=${1:-$(nix-build "$shellnix" -A ndk --no-out-link)}
+ndkRoot=${2:-$(nix eval --file "$shellnix" --raw ndkRoot)}
 
 mkdir -p "$(dirname $ANDROID_NDK_ROOT)" # Create full path if necessary
 
 # Replace any existing link or directory with link
 rm -rf "$ANDROID_NDK_ROOT"
-ln -sn "$ndk" "$ANDROID_NDK_ROOT"
+ln -sn "$ndkRoot" "$ANDROID_NDK_ROOT"
