01c0e9f006e5700972c1056b76c88e42fcb1c74b
Evan Bacon <baconbrix@gmail.com>
2019-02-07 13:47:03 -0800

    Migrate expo-localization to TypeScript (#3398)
    * Migrated to TypeScript
    
    * Added tests
    
    * Update config.yml
    
    * Update LocalizationScreen.js
    
    * Update Expo.ts
    
    * Update packages/expo-localization/README.md
    
    Co-Authored-By: EvanBacon <baconbrix@gmail.com>
    
    * Fixed API
    
    * Update Localization-test.ts
    
    * Remoevd type export

--- packages/expo-localization/src/ExpoLocalization.web.ts
+++ packages/expo-localization/src/ExpoLocalization.web.ts
@@ -1,46 +1,46 @@
-// @flow
 import * as rtlDetect from 'rtl-detect';
+
+import { Localization } from './Localization.types';
 
 export default {
   get isRTL(): boolean {
     return rtlDetect.isRtlLang(this.locale);
   },
   get locale(): string {
-    const { navigator = {} } = global;
-    let locale =
+    const locale =
       navigator.language ||
-      navigator.systemLanguage ||
-      navigator.browserLanguage ||
-      navigator.userLanguage ||
+      navigator['systemLanguage'] ||
+      navigator['browserLanguage'] ||
+      navigator['userLanguage'] ||
       this.locales[0];
 
     // The native format is en-US
     return locale.replace('_', '-');
   },
-  get locales(): Array<string> {
-    const { navigator = {} } = global;
-    return navigator.languages || [];
+  get locales(): string[] {
+    const { languages = [] } = navigator;
+    return Array.from(languages);
   },
-  get timezone(): null {
+  get timezone(): string {
     const defaultTimeZone = 'Etc/UTC';
     if (typeof Intl === 'undefined') {
       return defaultTimeZone;
     }
     return Intl.DateTimeFormat().resolvedOptions().timeZone || defaultTimeZone;
   },
-  get isoCurrencyCodes(): Array<string> {
+  get isoCurrencyCodes(): string[] {
     // TODO: Bacon: Add this - very low priority
     return [];
   },
-  get country(): string | null {
+  get country(): string | undefined {
     const { locale } = this;
     if (typeof locale === 'string' && locale.length) {
       const isoCountryCode = locale.substring(locale.lastIndexOf('-') + 1);
       return isoCountryCode.toUpperCase();
     }
-    return null;
+    return undefined;
   },
-  async getLocalizationAsync(): Promise<{ [string]: any }> {
+  async getLocalizationAsync(): Promise<Localization> {
     const { country, isoCurrencyCodes, timezone, locales, locale, isRTL } = this;
     return {
       country,
