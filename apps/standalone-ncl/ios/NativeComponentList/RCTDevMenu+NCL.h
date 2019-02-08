048a8fc3a9b977b09b2a9fb4380ae5f18fe44ab3
Stanisław Chmiela <sjchmiela@gmail.com>
2019-02-08 13:04:07 +0100

    [sncl] Revert https://github.com/facebook/react-native/commit/ad3c89c7ed4d96327531d556897a600c0ad041ce

---
+++ apps/standalone-ncl/ios/NativeComponentList/RCTDevMenu+NCL.h
@@ -0,0 +1,11 @@
+// see https://github.com/facebook/react-native/commit/ad3c89c7ed4d96327531d556897a600c0ad041ce
+
+#import <React/RCTDevMenu.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTDevMenu (NCL)
+
+@end
+
+NS_ASSUME_NONNULL_END
