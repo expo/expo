8200a6756f02ab33bda604f7f133d7875b96deab
Evan Bacon <baconbrix@gmail.com>
2019-02-19 17:42:12 -0800

    Update App.js
    * Ensure that the browser has access to WebGL before attempting to run the GLView tests.

--- apps/test-suite/App.js
+++ apps/test-suite/App.js
@@ -10,6 +10,18 @@ import * as TestUtils from './TestUtils';
 
 let { ExponentTest } = NativeModules;
 
+function browserSupportsWebGL() {
+  try {
+    const canvas = document.createElement('canvas');
+    return (
+      !!window.WebGLRenderingContext &&
+      (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
+    );
+  } catch (e) {
+    return false;
+  }
+}
+
 // List of all modules for tests. Each file path must be statically present for
 // the packager to pick them all up.
 async function getTestModulesAsync() {
@@ -22,12 +34,16 @@ async function getTestModulesAsync() {
   }
 
   if (Platform.OS === 'web') {
-    return [
+    let modules = [
       require('./tests/Import1'),
-      require('./tests/GLView'),
       require('./tests/Crypto'),
       require('./tests/Random'),
     ];
+
+    if (browserSupportsWebGL()) {
+      modules.push(require('./tests/GLView'));
+    }
+    return modules;
   }
 
   let modules = [
