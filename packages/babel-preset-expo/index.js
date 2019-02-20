46f138cd746a1bd641b3353358a530c773b3ae5a
Evan Bacon <baconbrix@gmail.com>
2019-02-20 13:46:55 -0800

    Added disableImportExportTransform

--- packages/babel-preset-expo/index.js
+++ packages/babel-preset-expo/index.js
@@ -13,13 +13,15 @@ module.exports = function(api) {
     ['@babel/plugin-proposal-decorators', { legacy: true }],
   ];
 
+  let disableImportExportTransform = undefined;
   // On web add the `react-native-web` plugin.
   if (isWeb) {
     plugins.push('babel-plugin-react-native-web');
+    disableImportExportTransform = true;
   }
 
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:metro-react-native-babel-preset', { disableImportExportTransform }],
     plugins,
   };
 };
