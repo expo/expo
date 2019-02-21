ae0c686fcf579cdddc16c83267e3700ca79fa8ef
Evan Bacon <baconbrix@gmail.com>
2019-02-20 19:53:45 -0800

    Removed IgnorePlugin

--- apps/native-component-list/webpack/webpack.common.js
+++ apps/native-component-list/webpack/webpack.common.js
@@ -329,12 +329,6 @@ module.exports = {
       analyzerMode: 'static',
       openAnalyzer: false,
     }),
-    // Moment.js is an extremely popular library that bundles large locale files
-    // by default due to how Webpack interprets its code. This is a practical
-    // solution that requires the user to opt into importing specific locales.
-    // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-    // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
   resolve: {
     symlinks: false,
