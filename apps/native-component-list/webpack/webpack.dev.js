1f90d490f7f26c18352e5e1f493df2c097889c74
Evan Bacon <baconbrix@gmail.com>
2019-02-20 19:53:31 -0800

    Update webpack.dev.js

--- apps/native-component-list/webpack/webpack.dev.js
+++ apps/native-component-list/webpack/webpack.dev.js
@@ -15,8 +15,8 @@ module.exports = merge(common, {
   entry: [require.resolve('react-dev-utils/webpackHotDevClient'), locations.appMain],
   devtool: 'cheap-module-source-map',
   output: {
-    filename: 'static/[chunkhash].js',
-    sourceMapFilename: '[chunkhash].map',
+    filename: 'static/[hash].js',
+    sourceMapFilename: '[hash].map',
     // There are also additional JS chunk files if you use code splitting.
     chunkFilename: 'static/[id].[chunkhash].js',
   },
