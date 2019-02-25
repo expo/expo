771430a71ba1ec74325d4f9de71dd064224e77d6
Evan Bacon <baconbrix@gmail.com>
2019-02-25 14:00:01 -0800

    Update index.js

--- packages/babel-preset-expo/index.js
+++ packages/babel-preset-expo/index.js
@@ -2,124 +2,13 @@ module.exports = function(api) {
   const isWeb = api.caller(isTargetWeb);
 
   if (isWeb) {
-    const defaultPlugins = [
-      [
-        'babel-plugin-module-resolver',
-        {
-          alias: {
-            'react-native-vector-icons': '@expo/vector-icons',
-            /** Alias direct react-native imports to react-native-web */
-            'react-native$': 'react-native-web',
-            /** Add polyfills for modules that react-native-web doesn't support */
-            'react-native/Libraries/Image/AssetSourceResolver$':
-              'expo-asset/build/AssetSourceResolver',
-            'react-native/Libraries/Image/assetPathUtils$': 'expo-asset/build/Image/assetPathUtils',
-            'react-native/Libraries/Image/resolveAssetSource$':
-              'expo-asset/build/resolveAssetSource',
-          },
-        },
-      ],
-      ['@babel/plugin-syntax-flow'],
-      ['@babel/plugin-proposal-optional-catch-binding'],
-      ['@babel/plugin-transform-block-scoping'],
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      [
-        '@babel/plugin-proposal-class-properties',
-        // use `this.foo = bar` instead of `this.defineProperty('foo', ...)`
-        { loose: true },
-      ],
-      ['@babel/plugin-syntax-dynamic-import'],
-      ['@babel/plugin-syntax-export-default-from'],
-      ['@babel/plugin-transform-computed-properties'],
-      ['@babel/plugin-transform-destructuring'],
-      ['@babel/plugin-transform-function-name'],
-      ['@babel/plugin-transform-literals'],
-      ['@babel/plugin-transform-parameters'],
-      ['@babel/plugin-transform-shorthand-properties'],
-      ['@babel/plugin-transform-react-jsx'],
-      ['@babel/plugin-transform-regenerator'],
-      ['@babel/plugin-transform-sticky-regex'],
-      ['@babel/plugin-transform-unicode-regex'],
-      'babel-plugin-react-native-web',
-    ];
-
-    const otherPlugins = [
-      ['@babel/plugin-proposal-export-default-from'],
-      [
-        '@babel/plugin-transform-modules-commonjs',
-        {
-          strict: false,
-          strictMode: false, // prevent "use strict" injections
-          lazy: true,
-          allowTopLevelThis: true, // dont rewrite global `this` -> `undefined`
-        },
-      ],
-
-      ['@babel/plugin-transform-arrow-functions'],
-      ['@babel/plugin-transform-classes'],
-      ['@babel/plugin-transform-for-of', { loose: true }],
-      ['@babel/plugin-transform-spread'],
-      [
-        '@babel/plugin-transform-template-literals',
-        { loose: true }, // dont 'a'.concat('b'), just use 'a'+'b'
-      ],
-      ['@babel/plugin-transform-exponentiation-operator'],
-      ['@babel/plugin-transform-object-assign'],
-      ['@babel/plugin-proposal-object-rest-spread'],
-      ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
-      ['@babel/plugin-proposal-optional-chaining', { loose: true }],
-      ['@babel/plugin-transform-react-display-name'],
-      ['@babel/plugin-transform-react-jsx-source'],
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          corejs: false,
-          helpers: true,
-          regenerator: true,
-          useESModules: false,
-        },
-      ],
-    ];
-
-    return {
-      comments: false,
-      compact: true,
-
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            modules: false, //'commonjs',
-            useBuiltIns: false,
-            targets: {
-              esmodules: true,
-            },
-          },
-        ],
-      ],
-      overrides: [
-        {
-          plugins: ['@babel/plugin-transform-flow-strip-types'],
-        },
-        {
-          plugins: defaultPlugins,
-        },
-        {
-          test: isTypeScriptSource,
-          plugins: [['@babel/plugin-transform-typescript', { isTSX: false }]],
-        },
-        {
-          test: isTSXSource,
-          plugins: [['@babel/plugin-transform-typescript', { isTSX: true }]],
-        },
-        {
-          plugins: otherPlugins,
-        },
-      ],
-    };
+    return getWebConfig();
   }
 
-  /** Native config  */
+  return getNativeConfig();
+};
+
+function getNativeConfig() {
   return {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
@@ -132,10 +21,126 @@ module.exports = function(api) {
         },
       ],
       ['@babel/plugin-proposal-decorators', { legacy: true }],
-      'babel-plugin-react-native-web',
     ],
   };
-};
+}
+
+function getWebConfig() {
+  const defaultPlugins = [
+    [
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          'react-native-vector-icons': '@expo/vector-icons',
+          /** Alias direct react-native imports to react-native-web */
+          'react-native$': 'react-native-web',
+          /** Add polyfills for modules that react-native-web doesn't support */
+          'react-native/Libraries/Image/AssetSourceResolver$':
+            'expo-asset/build/AssetSourceResolver',
+          'react-native/Libraries/Image/assetPathUtils$': 'expo-asset/build/Image/assetPathUtils',
+          'react-native/Libraries/Image/resolveAssetSource$': 'expo-asset/build/resolveAssetSource',
+        },
+      },
+    ],
+    '@babel/plugin-syntax-flow',
+    '@babel/plugin-proposal-optional-catch-binding',
+    '@babel/plugin-transform-block-scoping',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      '@babel/plugin-proposal-class-properties',
+      // use `this.foo = bar` instead of `this.defineProperty('foo', ...)`
+      { loose: true },
+    ],
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-export-default-from',
+    '@babel/plugin-transform-computed-properties',
+    '@babel/plugin-transform-destructuring',
+    '@babel/plugin-transform-function-name',
+    '@babel/plugin-transform-literals',
+    '@babel/plugin-transform-parameters',
+    '@babel/plugin-transform-shorthand-properties',
+    '@babel/plugin-transform-react-jsx',
+    '@babel/plugin-transform-regenerator',
+    '@babel/plugin-transform-sticky-regex',
+    '@babel/plugin-transform-unicode-regex',
+    'babel-plugin-react-native-web',
+  ];
+
+  const otherPlugins = [
+    '@babel/plugin-proposal-export-default-from',
+    [
+      '@babel/plugin-transform-modules-commonjs',
+      {
+        strict: false,
+        strictMode: false, // prevent "use strict" injections
+        lazy: true,
+        allowTopLevelThis: true, // dont rewrite global `this` -> `undefined`
+      },
+    ],
+
+    '@babel/plugin-transform-arrow-functions',
+    '@babel/plugin-transform-classes',
+    ['@babel/plugin-transform-for-of', { loose: true }],
+    '@babel/plugin-transform-spread',
+    [
+      '@babel/plugin-transform-template-literals',
+      { loose: true }, // dont 'a'.concat('b'), just use 'a'+'b'
+    ],
+    '@babel/plugin-transform-exponentiation-operator',
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-proposal-object-rest-spread',
+    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
+    ['@babel/plugin-proposal-optional-chaining', { loose: true }],
+    '@babel/plugin-transform-react-display-name',
+    '@babel/plugin-transform-react-jsx-source',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: true,
+        useESModules: false,
+      },
+    ],
+  ];
+
+  return {
+    comments: false,
+    compact: true,
+
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false, //'commonjs',
+          useBuiltIns: false,
+          targets: {
+            esmodules: true,
+          },
+        },
+      ],
+    ],
+    overrides: [
+      {
+        plugins: '@babel/plugin-transform-flow-strip-types',
+      },
+      {
+        plugins: defaultPlugins,
+      },
+      {
+        test: isTypeScriptSource,
+        plugins: [['@babel/plugin-transform-typescript', { isTSX: false }]],
+      },
+      {
+        test: isTSXSource,
+        plugins: [['@babel/plugin-transform-typescript', { isTSX: true }]],
+      },
+      {
+        plugins: otherPlugins,
+      },
+    ],
+  };
+}
 
 function isTargetWeb(caller) {
   return caller && caller.name === 'babel-loader';
