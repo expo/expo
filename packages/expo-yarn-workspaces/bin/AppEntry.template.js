56f01d2e6aa6173a4eaf263473195ed297c5bc27
Evan Bacon <baconbrix@gmail.com>
2019-02-25 16:31:36 -0800

    Update AppEntry.template.js

--- packages/expo-yarn-workspaces/bin/AppEntry.template.js
+++ packages/expo-yarn-workspaces/bin/AppEntry.template.js
@@ -1,9 +1,10 @@
-import { KeepAwake, registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo';
 
 import App from '{{relativeProjectPath}}/App';
 
 if (__DEV__) {
-  KeepAwake.activate();
+  const { activate } = require('expo-keep-awake');
+  activate();
 }
 
 registerRootComponent(App);
