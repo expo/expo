83ff9e1d41450a97145cae510ef528d71da5c62e
Evan Bacon <baconbrix@gmail.com>
2019-02-26 14:15:40 -0800

    Fixed camera importing all of React Native

--- packages/expo-camera/build/ExponentCamera.web.js
+++ packages/expo-camera/build/ExponentCamera.web.js
@@ -1,17 +1,7 @@
 import React from 'react';
+import { findNodeHandle, StyleSheet, View } from 'react-native';
 import CameraModule from './CameraModule/CameraModule';
 import CameraManager from './ExponentCameraManager.web';
-let findNodeHandle = function (node) { };
-let flatten = data => data;
-let View = props => <div {...props}/>;
-// Without react-native-web
-try {
-    const ReactNative = require('react-native');
-    findNodeHandle = ReactNative.findNodeHandle;
-    flatten = ReactNative.StyleSheet.flatten;
-    View = ReactNative.View;
-}
-catch (error) { }
 export default class ExponentCamera extends React.Component {
     constructor() {
         super(...arguments);
@@ -99,7 +89,7 @@ export default class ExponentCamera extends React.Component {
     }
     render() {
         const transform = this.state.type === CameraManager.Type.front ? 'rotateY(180deg)' : 'none';
-        const reactStyle = flatten(this.props.style);
+        const reactStyle = StyleSheet.flatten(this.props.style);
         const style = {
             position: 'absolute',
             top: 0,
