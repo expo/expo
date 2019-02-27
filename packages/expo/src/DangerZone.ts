d5e0b96a8db4599d4513125a348bbe57037d6ae8
Evan Bacon <baconbrix@gmail.com>
2019-02-27 02:06:03 -0800

    Fixed symlinks

--- packages/expo/src/DangerZone.ts
+++ packages/expo/src/DangerZone.ts
@@ -3,48 +3,11 @@
  * up to date if you plan to use any of these.
  */
 
-export default {
-  get Lottie() {
-    return require('lottie-react-native');
-  },
-  get Branch() {
-    return require('./Branch').default;
-  },
-  get GestureHandler() {
-    console.warn(
-      'GestureHandler is no longer in DangerZone, you can now import it directly from the expo package.'
-    );
-    return require('./GestureHandler').GestureHandler;
-  },
-  get Stripe() {
-    return require('expo-payments-stripe').PaymentsStripe;
-  },
-  get Print() {
-    console.warn(
-      'Print is no longer in DangerZone, you can now import it directly from the expo package.'
-    );
-    return require('expo-print').Print;
-  },
-  get DeviceMotion() {
-    return require('expo-sensors').DeviceMotion;
-  },
+// export { Lottie } from 'lottie-react-native';
+export { default as Branch } from './Branch';
+export { PaymentsStripe as Stripe } from 'expo-payments-stripe';
+export { DeviceMotion } from 'expo-sensors';
+export { default as Animated } from 'react-native-reanimated';
+export { Easing } from 'react-native-reanimated';
 
-  // react-native-reanimated
-  get Animated() {
-    return require('react-native-reanimated').default;
-  },
-  get Easing() {
-    return require('react-native-reanimated').Easing;
-  },
-
-  // react-native-screens
-  get Screen() {
-    return require('react-native-screens').Screen;
-  },
-  get ScreenContainer() {
-    return require('react-native-screens').ScreenContainer;
-  },
-  get ScreenStack() {
-    return require('react-native-screens').ScreenStack;
-  },
-};
+export { Screen, ScreenContainer } from 'react-native-screens';
