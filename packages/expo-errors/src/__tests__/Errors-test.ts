4fc7b116d289d323e428a9300cf1c641da9cecd7
Evan Bacon <baconbrix@gmail.com>
2019-02-26 19:15:29 -0800

    Updated snapshots

--- packages/expo-errors/src/__tests__/Errors-test.ts
+++ packages/expo-errors/src/__tests__/Errors-test.ts
@@ -10,82 +10,81 @@ describe('UnavailabilityError', () => {
 });
 
 describe('deprecate', () => {
-  const originalWarning = console.warn;
-  function isWarnedAsync() {
-    return new Promise(resolve => {
-      console.warn = (...args: any[]) => {
-        resolve(args);
-      };
+  describe('warnings', () => {
+    const originalWarning = console.warn;
+    beforeEach(() => {
+      console.warn = originalWarning;
     });
-  }
-  beforeEach(() => {
-    console.warn = originalWarning;
-  });
-  afterAll(() => {
-    console.warn = originalWarning;
-  });
+    afterAll(() => {
+      console.warn = originalWarning;
+    });
 
-  it(`warns of a future replacement`, async () => {
-    setTimeout(() => {
+    it(`warns of a future replacement`, async () => {
+      console.warn = jest.fn();
+
       Errors.deprecate('expo-errors', 'foo', {
         replacement: 'bar',
         currentVersion: '1.0.0',
         versionToRemove: '2.0.0',
       });
-    });
-    const warning = await isWarnedAsync();
-    expect(warning).toBeDefined();
-  });
 
-  it(`warns of a future deprecation`, async () => {
-    setTimeout(() => {
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      expect((console.warn as any).mock.calls[0]).toMatchSnapshot();
+    });
+
+    it(`warns of a future deprecation`, async () => {
+      console.warn = jest.fn();
+
       Errors.deprecate('expo-errors', 'foo', {
         currentVersion: '1.0.0',
         versionToRemove: '2.0.0',
       });
+
+      expect(console.warn).toHaveBeenCalledTimes(1);
+      expect((console.warn as any).mock.calls[0]).toMatchSnapshot();
     });
-    const warning = await isWarnedAsync();
-    expect(warning).toBeDefined();
   });
 
-  it(`throws a deprecation error without expiration`, async () => {
-    expect(() => Errors.deprecate('expo-errors', 'foo')).toThrowErrorMatchingSnapshot();
-  });
+  describe('errors', () => {
+    it(`throws a deprecation error without expiration`, async () => {
+      expect(() => Errors.deprecate('expo-errors', 'foo')).toThrowErrorMatchingSnapshot();
+    });
 
-  it(`throws an error with the replacement`, async () => {
-    expect(() =>
-      Errors.deprecate('expo-errors', 'foo', { replacement: 'bar' })
-    ).toThrowErrorMatchingSnapshot();
-  });
+    it(`throws an error with the replacement`, async () => {
+      expect(() =>
+        Errors.deprecate('expo-errors', 'foo', { replacement: 'bar' })
+      ).toThrowErrorMatchingSnapshot();
+    });
 
-  it(`throws an error with the replacement after the expiration`, async () => {
-    expect(() =>
-      Errors.deprecate('expo-errors', 'foo', {
-        replacement: 'bar',
-        currentVersion: '2.0.0',
-        versionToRemove: '1.0.0',
-      })
-    ).toThrowErrorMatchingSnapshot();
-  });
+    it(`throws an error with the replacement after the expiration`, async () => {
+      expect(() =>
+        Errors.deprecate('expo-errors', 'foo', {
+          replacement: 'bar',
+          currentVersion: '2.0.0',
+          versionToRemove: '1.0.0',
+        })
+      ).toThrowErrorMatchingSnapshot();
+    });
 
-  it(`throws a deprecation after expiration`, async () => {
-    expect(() =>
-      Errors.deprecate('expo-errors', 'foo', {
-        currentVersion: '2.0.0',
-        versionToRemove: '1.0.0',
-      })
-    ).toThrowErrorMatchingSnapshot();
-  });
+    it(`throws a deprecation error after expiration`, async () => {
+      expect(() =>
+        Errors.deprecate('expo-errors', 'foo', {
+          currentVersion: '2.0.0',
+          versionToRemove: '1.0.0',
+        })
+      ).toThrowErrorMatchingSnapshot();
+    });
 
-  it(`throws a CodedError with expected code`, async () => {
-    const libraries = ['expo-camera', 'Expo.Camera'];
-    for (const library of libraries) {
-      const error = await getErrorAsync(() => {
-        Errors.deprecate(library, 'foo');
-      });
-      expect(error).toBeDefined();
-      expect(error!.code).toBe('ERR_DEPRECATED_API_EXPO_CAMERA');
-    }
+    it(`throws a CodedError with expected code`, async () => {
+      const libraries = ['expo-camera', 'Expo.Camera'];
+      for (const library of libraries) {
+        const error = await getErrorAsync(() => {
+          Errors.deprecate(library, 'foo');
+        });
+        expect(error).toBeDefined();
+        expect(error!.code).toBe('ERR_DEPRECATED_API_EXPO_CAMERA');
+      }
+    });
   });
 });
 
