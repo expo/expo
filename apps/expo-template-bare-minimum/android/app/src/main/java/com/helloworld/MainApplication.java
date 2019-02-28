62174dd10bff56ef1804342d584c9d6ef90e8a12
Brent Vatne <brentvatne@gmail.com>
2019-02-28 13:53:27 -0800

    [templates] Update bare-minimum

--- apps/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
+++ apps/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -8,10 +8,25 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import expo.adapters.react.ModuleRegistryAdapter;
+import expo.adapters.react.ReactAdapterPackage;
+import expo.adapters.react.ReactModuleRegistryProvider;
+import expo.core.interfaces.Package;
+import expo.core.interfaces.SingletonModule;
+import expo.modules.constants.ConstantsPackage;
+import expo.modules.filesystem.FileSystemPackage;
+import expo.modules.permissions.PermissionsPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(Arrays.<Package>asList(
+     new ReactAdapterPackage(),
+     new ConstantsPackage(),
+     new PermissionsPackage(),
+     new FileSystemPackage()
+  ), Arrays.<SingletonModule>asList());
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -22,7 +37,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ModuleRegistryAdapter(mModuleRegistryProvider)
       );
     }
 
