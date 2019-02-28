f1553990077d1e9fe9ff53b6d3ae71b42d66be29
Evan Bacon <baconbrix@gmail.com>
2019-02-27 17:39:13 -0800

    Removed web index

--- packages/expo-ads-facebook/build/index.web.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function withNativeAd(): void;
-export declare function AdMediaView(): void;
-export declare function AdIconView(): void;
-export declare function AdTriggerView(): void;
-export declare function AdSettings(): void;
-export declare function NativeAdsManager(): void;
-export declare function InterstitialAdManager(): void;
-export declare function BannerAd(): void;
