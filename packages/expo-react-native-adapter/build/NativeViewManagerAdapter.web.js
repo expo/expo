e235c74e643096a96104aa033fc83c61c148f0d9
Evan Bacon <baconbrix@gmail.com>
2019-02-28 16:06:02 -0800

    Added shim for web requireNativeViewManager

---
+++ packages/expo-react-native-adapter/build/NativeViewManagerAdapter.web.js
@@ -0,0 +1,5 @@
+import { UnavailabilityError } from 'expo-errors';
+export function requireNativeViewManager(viewName) {
+    throw new UnavailabilityError('expo-react-native-adapter', 'requireNativeViewManager');
+}
+//# sourceMappingURL=NativeViewManagerAdapter.web.js.map
