a68ec6e84453e9cdaa54ec9504c387b22ea785cc
Evan Bacon <baconbrix@gmail.com>
2019-03-05 14:35:52 -0800

    Moved expo-bluetooth-utils to #3661

--- packages/expo-bluetooth-utils/build/Bluetooth.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as Characteristics } from './Characteristics';
-export { default as Descriptors } from './Descriptors';
-export { default as Services } from './Services';
-export declare function JSONToNative(str: string): string;
-export declare function nativeToJSON(str: string): string;
