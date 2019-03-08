d1e6362c17959f54f08c5262067217fdf830cd74
Nick Novitski <nick@expo.io>
2019-03-08 13:14:11 -0800

    [docs] deploy.sh redirect changes
    - Store redirects in associative array
    - Use 404.html as dummy object

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -2,21 +2,28 @@
 
 set -euo pipefail
 
-touch out/versions/latest/introduction/installation.html
+scriptdir=$(dirname "${BASH_SOURCE[0]}")
+bucket="docs.expo.io"
 
-aws s3 sync out s3://docs.expo.io --delete
+aws s3 sync "${scriptdir}/out" "s3://${bucket}" --delete
 
 aws s3 cp \
   --recursive \
   --metadata-directive REPLACE \
   --cache-control "public,max-age=31536000,immutable" \
-  s3://docs.expo.io/_next/static/ \
-  s3://docs.expo.io/_next/static/
+  "s3://${bucket}/_next/static/" \
+  "s3://${bucket}/_next/static/"
+
+declare -A redirects # associative array variable
 
 # Temporarily create a redirect for a page that Home links to
+redirects[versions/latest/introduction/installation.html]=versions/latest/introduction/installation/
 
-aws s3 cp \
-  --metadata-directive REPLACE \
-  --website-redirect /versions/latest/introduction/installation/ \
-  s3://docs.expo.io/versions/latest/introduction/installation.html \
-  s3://docs.expo.io/versions/latest/introduction/installation.html
+for i in "${!redirects[@]}" # iterate over keys
+do
+  aws s3 cp \
+    --metadata-directive REPLACE \
+    --website-redirect "/${redirects[$i]}" \
+    "${scriptdir}/out/404.html" \
+    "s3://${bucket}/${i}"
+done
