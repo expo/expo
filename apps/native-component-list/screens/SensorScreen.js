43f8b6911e74949de9b1a37074937e97543cabdd
Evan Bacon <baconbrix@gmail.com>
2019-03-11 15:48:34 -0700

    Update SensorScreen.js

--- apps/native-component-list/screens/SensorScreen.js
+++ apps/native-component-list/screens/SensorScreen.js
@@ -38,12 +38,6 @@ export default class SensorScreen extends React.Component {
     );
   }
 }
-// <SensorVisualizer
-// onStop={() => Accelerometer.removeAllListeners()}
-// onStart={callback => {
-//   Accelerometer.addListener(callback);
-// }}
-// />
 
 class SensorBlock extends React.Component {
   state = {
@@ -214,83 +208,3 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
 });
-
-const COUNT = 5;
-const ITEM_SIZE = Dimensions.get('window').width / COUNT;
-
-class SensorVisualizer extends React.Component {
-  static defaultProps = {
-    perspective: 200,
-    numItems: COUNT,
-  };
-
-  state = {
-    items: [],
-  };
-
-  componentWillUnmount() {
-    this.props.onStop();
-  }
-
-  componentWillMount() {
-    const items = [];
-    for (let i = 0; i < this.props.numItems; i++) {
-      items.push({ position: new Animated.ValueXY() });
-    }
-    this.setState({ items });
-  }
-
-  componentDidMount() {
-    this.props.onStart(({ x, y }) => {
-      this.state.items.forEach((box, index) => {
-        // All that matters is that the values are the same on iOS, Android, Web, ect...
-
-        const { perspective } = this.props;
-        const nIndex = index + 1;
-
-        Animated.spring(this.state.items[index].position, {
-          toValue: {
-            x: (x.toFixed(1) * perspective * nIndex) / COUNT,
-            y: (-y.toFixed(1) * perspective * nIndex) / COUNT,
-          },
-          friction: 7,
-        }).start();
-      });
-    });
-  }
-
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text style={{ position: 'absolute', top: 24, left: 24, right: 24, textAlign: 'center' }}>
-          The stack should move against the direction of the device. If you lift the bottom of the
-          phone up, the stack should translate down towards the bottom of the screen
-        </Text>
-        {this.state.items.map((val, index) => {
-          return (
-            <Animated.View
-              key={`item-${index}`}
-              style={{
-                position: 'absolute',
-                width: ITEM_SIZE,
-                height: ITEM_SIZE,
-                borderRadius: ITEM_SIZE,
-                backgroundColor: 'red',
-                opacity: (index + 1) / COUNT,
-                transform: [
-                  { translateX: this.state.items[index].position.x },
-                  { translateY: this.state.items[index].position.y },
-                ],
-              }}
-            />
-          );
-        })}
-      </View>
-    );
-  }
-}
