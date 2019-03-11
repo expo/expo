857ee18f2f2b01c9b0db700397267a4cf59fe524
Evan Bacon <baconbrix@gmail.com>
2019-03-11 15:44:11 -0700

    Update createTabNavigator.js

--- apps/native-component-list/navigation/createTabNavigator.js
+++ apps/native-component-list/navigation/createTabNavigator.js
@@ -1,4 +1,2 @@
 import { createBottomTabNavigator } from 'react-navigation';
 export default createBottomTabNavigator;
-// import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-// export default createMaterialBottomTabNavigator;
