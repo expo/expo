0794aea0f8f54ab5055e38f9f30c427824a08281
Evan Bacon <baconbrix@gmail.com>
2019-03-11 15:30:56 -0700

    Updated web NCL components

---
+++ apps/native-component-list/navigation/createTabNavigator.js
@@ -0,0 +1,4 @@
+import { createBottomTabNavigator } from 'react-navigation';
+export default createBottomTabNavigator;
+// import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+// export default createMaterialBottomTabNavigator;
