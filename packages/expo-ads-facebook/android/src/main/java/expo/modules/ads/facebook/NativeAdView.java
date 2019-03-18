214dcb3a4677fa3e7ca46d74787e45ad9e0558af
szymon <szymon.kapala@swmansion.com>
2019-03-18 14:47:30 +0100

    AdChoicesView -> AdOptionsView

--- packages/expo-ads-facebook/android/src/main/java/expo/modules/ads/facebook/NativeAdView.java
+++ packages/expo-ads-facebook/android/src/main/java/expo/modules/ads/facebook/NativeAdView.java
@@ -19,7 +19,7 @@ import java.util.List;
 import org.unimodules.core.ModuleRegistry;
 import org.unimodules.core.interfaces.services.EventEmitter;
 
-public class NativeAdView extends ViewGroup {
+public class NativeAdView extends NativeAdLayout {
   /**
    * @{NativeAd} received from the ads manager
    **/
