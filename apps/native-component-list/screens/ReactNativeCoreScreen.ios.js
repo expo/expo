d60a82767724b4d920154c4c140ba2b040bfdc46
Stanisław Chmiela <sjchmiela@gmail.com>
2019-03-19 13:15:59 +0100

    [ncl] Add a simple react-native-webview screen

--- apps/native-component-list/screens/ReactNativeCoreScreen.ios.js
+++ apps/native-component-list/screens/ReactNativeCoreScreen.ios.js
@@ -21,9 +21,8 @@ import {
   TouchableOpacity,
   TouchableHighlight,
   View,
-  // WebView,
+  WebView,
 } from 'react-native';
-import { WebView } from 'expo';
 import TouchableBounce from 'react-native/Libraries/Components/Touchable/TouchableBounce';
 import { ScrollView as NavigationScrollView } from 'react-navigation';
 
