f5c86327104ae308d128342ba9e58eb028d2ef1b
Nick Novitski <nick@expo.io>
2019-03-21 15:02:00 -0700

    [android] Fix ndk install script
    Without this change the created symlink is self-referential.

--- android/install-ndk-17c.sh
+++ android/install-ndk-17c.sh
@@ -4,11 +4,10 @@ set -euo pipefail
 scriptdir="$(dirname ${BASH_SOURCE[0]})"
 shellnix="$scriptdir/shell.nix"
 
-ndk=${1:-$(nix-build "$shellnix" -A ndk --no-out-link)}
-ndkRoot=${2:-$(nix eval --file "$shellnix" --raw ANDROID_NDK_ROOT)}
+ndk=${1:-$(nix eval --file "$shellnix" --raw ndk)}
 
-mkdir -p "$ANDROID_NDK_ROOT" # Create full path if necessary
+mkdir -p "$(dirname $ANDROID_NDK_ROOT)" # Create full path if necessary
 
 # Replace any existing link or directory with link
-rm -r "$ANDROID_NDK_ROOT"
-ln -sn "$ndkRoot" "$ANDROID_NDK_ROOT"
+rm -rf "$ANDROID_NDK_ROOT"
+ln -sn "$ndk" "$ANDROID_NDK_ROOT"
