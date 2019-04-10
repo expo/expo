93080c6d071af456410f0d3186c58088244089bd
szymon <szymon.kapala@swmansion.com>
2019-04-10 09:59:41 +0200

    applied requested changes

--- packages/expo-ads-facebook/build/AdOptionsView.web.js
+++ packages/expo-ads-facebook/build/AdOptionsView.web.js
@@ -1,5 +1,5 @@
 import { UnavailabilityError } from '@unimodules/core';
 export default function AdIconView() {
-    throw new UnavailabilityError('expo-ads-facebook', 'AdChoiceView');
+    throw new UnavailabilityError('expo-ads-facebook', 'AdOptionsView');
 }
 //# sourceMappingURL=AdOptionsView.web.js.map
