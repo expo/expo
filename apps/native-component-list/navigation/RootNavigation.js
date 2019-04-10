e335b7bac4847b084bb3d96b8ba6f82807225d67
Evan Bacon <baconbrix@gmail.com>
2019-04-10 02:29:24 -0700

    Added routing

--- apps/native-component-list/navigation/RootNavigation.js
+++ apps/native-component-list/navigation/RootNavigation.js
@@ -1,5 +1,11 @@
 import { createAppContainer } from 'react-navigation';
+import { createBrowserApp } from '@react-navigation/web';
+import { Platform } from 'react-native';
 
 import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(MainTabNavigator);
+const createApp = Platform.select({
+  default: createAppContainer,
+  web: createBrowserApp,
+});
+export default createApp(MainTabNavigator);
