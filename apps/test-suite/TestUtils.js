f8b699962e26f2bde66a9dfcaa37bc5f13a98721
Sergei Chestakov <sergei332@gmail.com>
2019-04-09 17:00:29 -0700

    Update test-suite app  (#3932)
    * Create new component for selecting tests to run
    
    * Add buttons to select all and run tests
    
    * Implement selecting and deselecting
    
    * Add runTests function and pass selected values to parent component
    
    * Run only selected tests
    
    * Fix missing name exports for test modules
    
    * Remove async call in getTestModules and generate data array
    
    * Add react navigation and create seperate screens
    
    * Add styles object and done message
    
    * Refactor select screen to use set instead of map and clear on navigation
    
    * Check if the component is mounted before calling setState
    
    * Move screens to new directory and Modules.js -> TestUtils.js
    
    * Use checkboxes to indicate selected and clean up UI
    
    * Add default case to support web
    
    * Remove Ripple function call for ListItem background on Android
    
    * Add support for /all deeplink that runs all modules in test-suite
    
    * Add suggested changes for PR and run JSC test on Android emulator

--- apps/test-suite/TestUtils.js
+++ apps/test-suite/TestUtils.js
@@ -1,15 +1,93 @@
 'use strict';
 
-import { NativeModules } from 'react-native';
+import { Platform, NativeModules } from 'react-native';
+import { Constants } from 'expo';
 
-let { ExponentTest } = NativeModules;
+const { ExponentTest } = NativeModules;
+
+function browserSupportsWebGL() {
+  try {
+    const canvas = document.createElement('canvas');
+    return (
+      !!window.WebGLRenderingContext &&
+      (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
+    );
+  } catch (e) {
+    return false;
+  }
+}
+
+// List of all modules for tests. Each file path must be statically present for
+// the packager to pick them all up.
+export function getTestModules() {
+  if (Platform.OS === 'web') {
+    const modules = [
+      require('./tests/Import1'),
+      require('./tests/Crypto'),
+      require('./tests/Random'),
+    ];
+
+    if (browserSupportsWebGL()) {
+      modules.push(require('./tests/GLView'));
+    }
+    return modules;
+  }
+
+  const modules = [
+    require('./tests/Basic1'),
+    require('./tests/Basic2'),
+    require('./tests/Import1'),
+    require('./tests/Import2'),
+    require('./tests/Import3'),
+    require('./tests/Asset'),
+    require('./tests/Audio'),
+    require('./tests/Calendar'),
+    require('./tests/Constants'),
+    require('./tests/Contacts'),
+    require('./tests/Crypto'),
+    require('./tests/FileSystem'),
+    require('./tests/GLView'),
+    require('./tests/GoogleSignIn'),
+    require('./tests/Haptics'),
+    require('./tests/Localization'),
+    require('./tests/Location'),
+    require('./tests/Linking'),
+    require('./tests/Recording'),
+    require('./tests/ScreenOrientation'),
+    require('./tests/SecureStore'),
+    require('./tests/Segment'),
+    require('./tests/Speech'),
+    require('./tests/SQLite'),
+    require('./tests/Random'),
+    require('./tests/Payments'),
+    require('./tests/AdMobInterstitial'),
+    require('./tests/AdMobBanner'),
+    require('./tests/AdMobPublisherBanner'),
+    require('./tests/AdMobRewarded'),
+    require('./tests/Video'),
+    require('./tests/Permissions'),
+    require('./tests/MediaLibrary'),
+    require('./tests/Notifications'),
+    require('./tests/FBNativeAd'),
+    require('./tests/FBBannerAd'),
+    require('./tests/TaskManager'),
+  ];
+  if (Platform.OS === 'android') modules.push(require('./tests/JSC'));
+  if (Constants.isDevice) {
+    modules.push(require('./tests/Brightness'));
+    modules.push(require('./tests/BarCodeScanner'));
+    // The Camera tests are flaky on iOS, i.e. they fail randomly
+    if (Platform.OS === 'android') modules.push(require('./tests/Camera'));
+  }
+  return modules;
+}
 
 export async function acceptPermissionsAndRunCommandAsync(fn) {
   if (!ExponentTest) {
     return await fn();
   }
 
-  let results = await Promise.all([
+  const results = await Promise.all([
     ExponentTest.action({
       selectorType: 'text',
       selectorValue: 'Allow',
