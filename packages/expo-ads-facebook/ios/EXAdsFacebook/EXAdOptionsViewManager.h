540f45a1c53136f51397fe2e28a0ec6f5179ceac
Szymon20000 <szymon20000@gmail.com>
2019-04-11 16:26:47 +0200

    [expo-ads-facebook] Upgrade to 5.1.1 on Android and add support for AdOptionsView (#3843)
    # Why
    
    Resolves : https://github.com/expo/expo/issues/3441

---
+++ packages/expo-ads-facebook/ios/EXAdsFacebook/EXAdOptionsViewManager.h
@@ -0,0 +1,9 @@
+// Copyright 2016-present 650 Industries. All rights reserved.
+#import <UMCore/UMViewManager.h>
+#import <FBAudienceNetwork/FBAudienceNetwork.h>
+#import <UMCore/UMModuleRegistryConsumer.h>
+#import <UIKit/UIKit.h>
+
+@interface EXAdOptionsViewManager : UMViewManager <UMModuleRegistryConsumer>
+@end
+
