6747d3ae73bf870ebfd88e0f32028aaf0b1f8f24
Bartłomiej Bukowski <bartlomiejbukowski.b@gmail.com>
2019-04-12 13:48:35 +0200

    [expo-image-picker] Add missing manifest configuration for bare react-native (#3743)

---
+++ packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/ImagePickerFileProvider.java
@@ -0,0 +1,8 @@
+package expo.modules.imagepicker;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Dummy class for proving files for this module.
+ */
+public class ImagePickerFileProvider extends FileProvider {}
