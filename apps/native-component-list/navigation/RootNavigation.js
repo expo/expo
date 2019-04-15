f27e5d16b8464e66cd3eaccb2501a510cdb8f4c6
Stanisław Chmiela <sjchmiela@gmail.com>
2019-04-15 17:35:25 +0200

    [ncl] Fix navigation

--- apps/native-component-list/navigation/RootNavigation.js
+++ apps/native-component-list/navigation/RootNavigation.js
@@ -1,11 +1,5 @@
 import { createAppContainer } from 'react-navigation';
-import { createBrowserApp } from '@react-navigation/web';
-import { Platform } from 'react-native';
 
 import MainTabNavigator from './MainTabNavigator';
 
-const createApp = Platform.select({
-  default: createAppContainer,
-  web: createBrowserApp,
-});
-export default createApp(MainTabNavigator);
+export default createAppContainer(MainTabNavigator);
