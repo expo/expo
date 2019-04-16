68551c6fe50522703211762cff9a606296fc124b
Evan Bacon <baconbrix@gmail.com>
2019-04-16 12:42:32 -0700

    Removed test-suite storybook

--- apps/test-suite/stories/ui-explorer/insertBetween.js
+++
@@ -1,9 +0,0 @@
-const insertBetween = (item, array) =>
-  array.reduce((acc, curr, i, { length }) => {
-    if (i && i < length) {
-      return [...acc, item(), curr];
-    }
-    return [...acc, curr];
-  }, []);
-
-export default insertBetween;
