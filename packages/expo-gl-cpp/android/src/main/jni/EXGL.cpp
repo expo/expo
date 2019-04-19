22912452d8896cf4f57f4618cb5ea2125a07bbdb
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2019-04-19 12:07:57 +0200

    [gl] make expo-gl-cpp compatible with all SDKs

--- packages/expo-gl-cpp/android/src/main/jni/EXGL.cpp
+++ packages/expo-gl-cpp/android/src/main/jni/EXGL.cpp
@@ -24,10 +24,10 @@ Java_expo_modules_gl_cpp_EXGL_EXGLContextCreate
 }
 
 JNIEXPORT jint JNICALL
-Java_expo_modules_gl_cpp_EXGL_EXGLJSCContextCreate
+Java_expo_modules_gl_cpp_EXGL_EXGLContextCreateV2
 (JNIEnv *env, jclass clazz, jlong jsCtxPtr) {
-  // In react-native 0.59 jsCtxPtr is pointing to runtime object (in case of jsc it's JSCRuntime class)
-  // implementing JSI interface. Real jsc context ref is extracted by offset from that object.
+  // In react-native 0.59 jsCtxPtr is pointing to runtime object (in case of JSC it's JSCRuntime class)
+  // implementing JSI interface. Real JSC context ref is extracted by offset from that object.
   // WARNING: This is temporary solution that may break with new react-native releases.
   JSGlobalContextRef jsCtx = *(reinterpret_cast<JSGlobalContextRef*>(jsCtxPtr)+1);
   if (jsCtx) {
