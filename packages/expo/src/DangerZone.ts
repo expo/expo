92046128cf6da43b955fd373e8cd64ef6cf8f053
Ville Immonen <ville.immonen@iki.fi>
2019-04-23 23:56:04 +0300

    Revert "Deprecate importing extracted out native modules from Expo"
    This reverts commit 30424181b998f506673a1d110abbcd6741cdbbc1.

--- packages/expo/src/DangerZone.ts
+++ packages/expo/src/DangerZone.ts
@@ -2,74 +2,49 @@
  * Modules exported here are experimental and COULD break in the future. Make sure you keep your app
  * up to date if you plan to use any of these.
  */
-import deprecatedModule from './deprecatedModule';
 
 export default {
   get Lottie() {
-    deprecatedModule(
-      `DangerZone.Lottie -> import Lottie from 'lottie-react-native'`,
-      'lottie-react-native'
-    );
     return require('lottie-react-native');
   },
   get Branch() {
-    deprecatedModule(
-      `DangerZone.Branch -> import Branch, { BranchEvent } from 'react-native-branch'`,
-      'react-native-branch'
-    );
     return require('./Branch').default;
   },
-  get Stripe() {
-    deprecatedModule(
-      `DangerZone.Stripe -> import { PaymentsStripe } from 'expo-payments-stripe'`,
-      'expo-payments-stripe'
+  get GestureHandler() {
+    console.warn(
+      'GestureHandler is no longer in DangerZone, you can now import it directly from the expo package.'
     );
+    return require('./GestureHandler').GestureHandler;
+  },
+  get Stripe() {
     return require('expo-payments-stripe').PaymentsStripe;
   },
-  get DeviceMotion() {
-    deprecatedModule(
-      `DangerZone.DeviceMotion -> import { DeviceMotion } from 'expo-sensors'`,
-      'expo-sensors'
+  get Print() {
+    console.warn(
+      'Print is no longer in DangerZone, you can now import it directly from the expo package.'
     );
+    return require('expo-print').Print;
+  },
+  get DeviceMotion() {
     return require('expo-sensors').DeviceMotion;
   },
 
   // react-native-reanimated
   get Animated() {
-    deprecatedModule(
-      `DangerZone.Animated -> import Animated from 'react-native-reanimated'`,
-      'react-native-reanimated'
-    );
     return require('react-native-reanimated').default;
   },
   get Easing() {
-    deprecatedModule(
-      `DangerZone.Easing -> import { Easing } from 'react-native-reanimated'`,
-      'react-native-reanimated'
-    );
     return require('react-native-reanimated').Easing;
   },
 
   // react-native-screens
   get Screen() {
-    deprecatedModule(
-      `DangerZone.Screen -> import { Screen } from 'react-native-screens'`,
-      'react-native-screens'
-    );
     return require('react-native-screens').Screen;
   },
   get ScreenContainer() {
-    deprecatedModule(
-      `DangerZone.ScreenContainer -> import { ScreenContainer } from 'react-native-screens'`,
-      'react-native-screens'
-    );
     return require('react-native-screens').ScreenContainer;
   },
   get ScreenStack() {
-    deprecatedModule(
-      `DangerZone.ScreenStack -> import { ScreenStack } from 'react-native-screens'`,
-      'react-native-screens'
-    );
     return require('react-native-screens').ScreenStack;
   },
 };
