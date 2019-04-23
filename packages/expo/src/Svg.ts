30424181b998f506673a1d110abbcd6741cdbbc1
Ville Immonen <ville.immonen@iki.fi>
2019-04-23 20:27:26 +0300

    Deprecate importing extracted out native modules from Expo

--- packages/expo/src/Svg.ts
+++ packages/expo/src/Svg.ts
@@ -1,10 +1,22 @@
 import * as SvgModules from 'react-native-svg';
 import { ComponentClass } from 'react';
 
+import deprecatedModule from './deprecatedModule';
+
 const { Svg } = SvgModules;
 
 for (const key in SvgModules) {
   if (key !== 'default' && key !== 'Svg') {
+    Object.defineProperty(Svg, key, {
+      enumerable: true,
+      get() {
+        deprecatedModule(
+          `Svg.${key} -> import { ${key} } from 'react-native-svg'`,
+          'react-native-svg'
+        );
+        return SvgModules[key];
+      },
+    });
     Svg[key] = SvgModules[key];
   }
 }
