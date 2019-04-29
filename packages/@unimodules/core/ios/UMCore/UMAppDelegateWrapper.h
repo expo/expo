fb27dcc97db8f1ad248d1863487db64b34aba888
Szymon20000 <szymon20000@gmail.com>
2019-04-29 16:18:23 +0200

    [unimodules][ios] Create universal modules-compatible integration with AppDelegate methods (#3917)
    We want to provide all functionalities which are available for an iOS developer to a Unimodule developer.
    One of such functionalities is communication with iOS system through AppDelegate.

---
+++ packages/@unimodules/core/ios/UMCore/UMAppDelegateWrapper.h
@@ -0,0 +1,10 @@
+// Copyright © 2018 650 Industries. All rights reserved.
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UMAppDelegateWrapper : UIResponder <UIApplicationDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ packages/expo-task-manager/ios/EXTaskManager/EXTaskManagerAppDelegate.h
@@ -0,0 +1,13 @@
+// Copyright 2018-present 650 Industries. All rights reserved.
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UMCore/UMSingletonModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EXTaskManagerAppDelegate : UMSingletonModule <UIApplicationDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
