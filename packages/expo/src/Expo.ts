0fdb42df0dff407463cf99cd01a3dbbc9a5cc3f3
Evan Bacon <baconbrix@gmail.com>
2019-04-30 00:54:43 -0700

    Updated ErrorRecovery

--- packages/expo/src/Expo.ts
+++ packages/expo/src/Expo.ts
@@ -15,68 +15,71 @@ import * as MailComposer from 'expo-mail-composer';
 import * as SecureStore from 'expo-secure-store';
 import * as AV from 'expo-av';
 import { BlurView } from 'expo-blur';
-import * as AR from './AR';
 import * as Brightness from 'expo-brightness';
 import * as FileSystem from 'expo-file-system';
-import * as Google from './Google';
 import * as Haptics from 'expo-haptics';
 import * as Sharing from 'expo-sharing';
 import * as ImageManipulator from 'expo-image-manipulator';
 import * as IntentLauncher from 'expo-intent-launcher';
 import * as LocalAuthentication from 'expo-local-authentication';
-import * as ScreenOrientation from './ScreenOrientation/ScreenOrientation';
-import * as StoreReview from './StoreReview/StoreReview';
-import * as Updates from './Updates/Updates';
 import * as FacebookAds from 'expo-ads-facebook';
-import * as SplashScreen from './launch/SplashScreen';
 import * as Sensors from 'expo-sensors';
 import * as WebBrowser from 'expo-web-browser';
-export { AdMobBanner, AdMobInterstitial, AdMobRewarded, PublisherBanner } from 'expo-ads-admob';
 import * as Segment from 'expo-analytics-segment';
+import * as AppAuth from 'expo-app-auth';
+import * as Contacts from 'expo-contacts';
+import * as FaceDetector from 'expo-face-detector';
+import * as GL from 'expo-gl';
+import * as GoogleSignIn from 'expo-google-sign-in';
+import * as ImagePicker from 'expo-image-picker';
+import * as Localization from 'expo-localization';
+import * as Crypto from 'expo-crypto';
+import * as MediaLibrary from 'expo-media-library';
+import * as Permissions from 'expo-permissions';
+import * as Print from 'expo-print';
+import * as GestureHandler from 'react-native-gesture-handler';
+import * as Random from 'expo-random';
+import * as Icon from '@expo/vector-icons';
+import * as ErrorRecovery from './ErrorRecovery/ErrorRecovery';
+import * as SplashScreen from './launch/SplashScreen';
+import * as Updates from './Updates/Updates';
+import * as StoreReview from './StoreReview/StoreReview';
+import * as ScreenOrientation from './ScreenOrientation/ScreenOrientation';
+import * as Google from './Google';
+import * as AR from './AR';
+export { AdMobBanner, AdMobInterstitial, AdMobRewarded, PublisherBanner } from 'expo-ads-admob';
 export { Segment };
 export { Asset } from 'expo-asset';
-import * as AppAuth from 'expo-app-auth';
 export { AppAuth };
 export { BackgroundFetch };
 export { BarCodeScanner } from 'expo-barcode-scanner';
 export { Calendar };
 export { Camera } from 'expo-camera';
 export { Constants };
-import * as Contacts from 'expo-contacts';
 export { Contacts };
 export { DocumentPicker };
-import * as FaceDetector from 'expo-face-detector';
 export { FaceDetector };
 export { FileSystem };
 export { Font };
-import * as GL from 'expo-gl';
 const GLView = GL.GLView;
 export { GL, GLView };
-import * as GoogleSignIn from 'expo-google-sign-in';
 export { GoogleSignIn };
 export { ImageManipulator };
 export { Haptics };
-import * as ImagePicker from 'expo-image-picker';
 export { ImagePicker };
 export { LocalAuthentication };
 export { IntentLauncher };
-import * as Localization from 'expo-localization';
 export { Localization };
-import * as Crypto from 'expo-crypto';
 export { Crypto };
 export { Location };
-import * as MediaLibrary from 'expo-media-library';
 export { MediaLibrary };
-import * as Permissions from 'expo-permissions';
 export { Permissions };
-import * as Print from 'expo-print';
 export { Print };
 export { Sensors };
 export { SQLite } from 'expo-sqlite';
 export { SMS };
 export { Speech };
 export { TaskManager };
-import * as GestureHandler from 'react-native-gesture-handler';
 export { GestureHandler };
 export { default as MapView } from './Maps/MapView';
 export { AR };
@@ -84,11 +87,9 @@ export { Amplitude };
 export { default as AuthSession } from './AuthSession';
 export { Brightness };
 export { default as DangerZone } from './DangerZone';
-export { default as ErrorRecovery } from './ErrorRecovery/ErrorRecovery';
+export { ErrorRecovery };
 export { Facebook };
 export { Google };
-import * as Random from 'expo-random';
-import * as Icon from '@expo/vector-icons';
 export { Icon };
 export { Random };
 export { Sharing };
@@ -120,7 +121,6 @@ export { WebView } from './WebView';
 declare var module: any;
 
 if (module && module.exports) {
-
   //@ts-ignore
   Object.defineProperties(module.exports, {
     // Accelerometer: {
@@ -141,14 +141,18 @@ if (module && module.exports) {
     Haptic: {
       enumerable: false,
       get() {
-        console.log('Module name `Haptic` is deprecated. Use `Haptics` instead. Expo.Haptic will be removed in SDK 34');
+        console.log(
+          'Module name `Haptic` is deprecated. Use `Haptics` instead. Expo.Haptic will be removed in SDK 34'
+        );
         return require('expo-haptics');
       },
     },
     IntentLauncherAndroid: {
       enumerable: true,
       get() {
-        console.warn(`Module name 'IntentLauncherAndroid' is deprecated, use 'IntentLauncher' instead. Expo.IntentLauncherAndroid will be removed in SDK 34`);
+        console.warn(
+          `Module name 'IntentLauncherAndroid' is deprecated, use 'IntentLauncher' instead. Expo.IntentLauncherAndroid will be removed in SDK 34`
+        );
         return require('expo-intent-launcher');
       },
     },
