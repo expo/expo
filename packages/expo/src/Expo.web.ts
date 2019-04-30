fd7c742bc83d9641dffcf76f7c9c458d67aa2c6f
Ville Immonen <ville.immonen@iki.fi>
2019-04-30 20:01:48 +0300

    Move deprecated exports to a separate module

--- packages/expo/src/Expo.web.ts
+++ packages/expo/src/Expo.web.ts
@@ -1,116 +1,158 @@
 import './Expo.fx';
 
-import Constants from 'expo-constants';
-import * as Amplitude from 'expo-analytics-amplitude';
-import * as BackgroundFetch from 'expo-background-fetch';
-import * as Calendar from 'expo-calendar';
-import * as DocumentPicker from 'expo-document-picker';
-import * as Font from 'expo-font';
-import * as Location from 'expo-location';
-import * as SMS from 'expo-sms';
-import * as Speech from 'expo-speech';
-import * as TaskManager from 'expo-task-manager';
-import * as Facebook from 'expo-facebook';
-import * as MailComposer from 'expo-mail-composer';
-import * as SecureStore from 'expo-secure-store';
-import { BlurView } from 'expo-blur';
 import * as AR from './AR';
-import * as Brightness from 'expo-brightness';
-import * as FileSystem from 'expo-file-system';
 import * as Google from './Google/Google';
-import * as Haptics from 'expo-haptics';
-import * as Sharing from 'expo-sharing';
-import * as ImageManipulator from 'expo-image-manipulator';
-import * as IntentLauncher from 'expo-intent-launcher';
-import * as LocalAuthentication from 'expo-local-authentication';
 import * as ScreenOrientation from './ScreenOrientation/ScreenOrientation';
 import * as StoreReview from './StoreReview/StoreReview';
 import * as Updates from './Updates/Updates';
-import * as FacebookAds from 'expo-ads-facebook';
 import * as SplashScreen from './launch/SplashScreen';
-import * as Sensors from 'expo-sensors';
-import * as WebBrowser from 'expo-web-browser';
-export { AdMobBanner, AdMobInterstitial, AdMobRewarded, PublisherBanner } from 'expo-ads-admob';
-import * as Segment from 'expo-analytics-segment';
-export { Segment };
-export { Asset } from 'expo-asset';
-import * as AppAuth from 'expo-app-auth';
-export { AppAuth };
-export { BackgroundFetch };
-export { BarCodeScanner } from 'expo-barcode-scanner';
-export { Calendar };
-export { Camera } from 'expo-camera';
-export { Constants };
-import * as Contacts from 'expo-contacts';
-export { Contacts };
-export { DocumentPicker };
-import * as FaceDetector from 'expo-face-detector';
-export { FaceDetector };
-export { FileSystem };
-export { Font };
-import * as GL from 'expo-gl';
-const GLView = GL.GLView;
-export { GL, GLView };
-import * as GoogleSignIn from 'expo-google-sign-in';
-export { GoogleSignIn };
-export { ImageManipulator };
-export { Haptics };
-import * as ImagePicker from 'expo-image-picker';
-export { ImagePicker };
-export { LocalAuthentication };
-export { IntentLauncher };
-import * as Localization from 'expo-localization';
-export { Localization };
-import * as Crypto from 'expo-crypto';
-export { Crypto };
-export { Location };
-import * as MediaLibrary from 'expo-media-library';
-export { MediaLibrary };
-import * as Permissions from 'expo-permissions';
-export { Permissions };
-import * as Print from 'expo-print';
-export { Print };
-export { Sensors };
-export { SQLite } from 'expo-sqlite';
-export { SMS };
-export { Speech };
-export { TaskManager };
-export { GestureHandler } from './GestureHandler';
-export { default as MapView } from './Maps/MapView';
+
 export { AR };
-export { Amplitude };
+export { Google };
+export { ScreenOrientation };
+export { SplashScreen };
+export { StoreReview };
+export { Updates };
+
+export { default as apisAreAvailable } from './apisAreAvailable';
+export { default as AppLoading } from './launch/AppLoading';
 export { default as AuthSession } from './AuthSession';
-export { Brightness };
 export { default as DangerZone } from './DangerZone';
 export { default as ErrorRecovery } from './ErrorRecovery/ErrorRecovery';
-export { Facebook };
-export { Google };
-import * as Random from 'expo-random';
-export { default as Icon } from './Icon';
-export { Random };
-export { Sharing };
-export { default as KeepAwake, activate, deactivate } from 'expo-keep-awake';
 export { default as Linking } from './Linking/Linking';
-export { MailComposer };
-export { default as Notifications } from './Notifications/Notifications';
-export { ScreenOrientation };
-export { SecureStore };
-export { StoreReview };
-export { default as Svg } from './Svg';
-export { Updates };
-export { WebBrowser };
-export { default as apisAreAvailable } from './apisAreAvailable';
-export { default as takeSnapshotAsync } from './takeSnapshotAsync/takeSnapshotAsync';
-export { Audio, Video } from 'expo-av';
-export { BlurView };
-export { LinearGradient } from 'expo-linear-gradient';
-export { FacebookAds };
-export { default as AppLoading } from './launch/AppLoading';
-export { SplashScreen };
-export { default as registerRootComponent } from './launch/registerRootComponent';
 export { default as Logs } from './logs/Logs';
+export { default as Notifications } from './Notifications/Notifications';
 export { default as Pedometer } from './Pedometer';
-export { WebView } from './WebView';
+export { default as registerRootComponent } from './launch/registerRootComponent';
+export { default as takeSnapshotAsync } from './takeSnapshotAsync/takeSnapshotAsync';
+
+// @ts-ignore
+export {
+  // @ts-ignore
+  Accelerometer,
+  // @ts-ignore
+  AdMobBanner,
+  // @ts-ignore
+  AdMobInterstitial,
+  // @ts-ignore
+  AdMobRewarded,
+  // @ts-ignore
+  Amplitude,
+  // @ts-ignore
+  AppAuth,
+  // @ts-ignore
+  Asset,
+  // @ts-ignore
+  Audio,
+  // @ts-ignore
+  BackgroundFetch,
+  // @ts-ignore
+  BarCodeScanner,
+  // @ts-ignore
+  Barometer,
+  // @ts-ignore
+  BlurView,
+  // @ts-ignore
+  Brightness,
+  // @ts-ignore
+  Calendar,
+  // @ts-ignore
+  Camera,
+  // @ts-ignore
+  Constants,
+  // @ts-ignore
+  Contacts,
+  // @ts-ignore
+  Crypto,
+  // @ts-ignore
+  DocumentPicker,
+  // @ts-ignore
+  Facebook,
+  // @ts-ignore
+  FacebookAds,
+  // @ts-ignore
+  FaceDetector,
+  // @ts-ignore
+  FileSystem,
+  // @ts-ignore
+  Font,
+  // @ts-ignore
+  GestureHandler,
+  // @ts-ignore
+  GL,
+  // @ts-ignore
+  GLView,
+  // @ts-ignore
+  GoogleSignIn,
+  // @ts-ignore
+  Gyroscope,
+  // @ts-ignore
+  Haptic,
+  // @ts-ignore
+  Haptics,
+  // @ts-ignore
+  Icon,
+  // @ts-ignore
+  ImageManipulator,
+  // @ts-ignore
+  ImagePicker,
+  // @ts-ignore
+  IntentLauncher,
+  // @ts-ignore
+  IntentLauncherAndroid,
+  // @ts-ignore
+  KeepAwake,
+  // @ts-ignore
+  LinearGradient,
+  // @ts-ignore
+  LocalAuthentication,
+  // @ts-ignore
+  Localization,
+  // @ts-ignore
+  Location,
+  // @ts-ignore
+  Magnetometer,
+  // @ts-ignore
+  MagnetometerUncalibrated,
+  // @ts-ignore
+  MailComposer,
+  // @ts-ignore
+  MapView,
+  // @ts-ignore
+  MediaLibrary,
+  // @ts-ignore
+  Permissions,
+  // @ts-ignore
+  Print,
+  // @ts-ignore
+  PublisherBanner,
+  // @ts-ignore
+  Random,
+  // @ts-ignore
+  SecureStore,
+  // @ts-ignore
+  Segment,
+  // @ts-ignore
+  Sensors,
+  // @ts-ignore
+  Sharing,
+  // @ts-ignore
+  SMS,
+  // @ts-ignore
+  Speech,
+  // @ts-ignore
+  SQLite,
+  // @ts-ignore
+  Svg,
+  // @ts-ignore
+  TaskManager,
+  // @ts-ignore
+  Video,
+  // @ts-ignore
+  WebBrowser,
+  // @ts-ignore
+  WebView,
+} from './deprecated';
 
 declare var module: any;
 
