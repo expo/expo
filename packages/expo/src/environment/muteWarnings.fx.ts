6e952433e4fee547127126905fe8c4db6c3f44b3
James Ide <ide@expo.io>
2019-04-30 18:13:44 -0700

    [expo] Properly mute spurious warnings
    Imported `muteWarnings.fx` from `Expo.fx` after `logging.fx` since we need to enable remote CLI debugging before we add our patch to filter out spurious warnings. Also made `muteWarnings.fx` filter out Lean Core deprecations, since they are unactionable. Lastly, imported `AppRegistry` in `muteWarnings.fx` since it installs YellowBox, which patches console.warn/error itself -- we want to override these methods after YellowBox has patched them.
    
    Test Plan: opened Home and saw all node_modules cycle warnings go away as well as warnings about deprecated Lean Core components.

--- packages/expo/src/environment/muteWarnings.fx.ts
+++ packages/expo/src/environment/muteWarnings.fx.ts
@@ -1,3 +1,9 @@
+// AppRegistry transitively installs YellowBox as a side effect, which overrides various console
+// methods that we need to set up before we override them
+import { AppRegistry } from 'react-native';
+
+const _unusedAppRegistry = AppRegistry;
+
 // NOTE(2018-10-29): temporarily filter out cyclic dependency warnings here since they are noisy and
 // each warning symbolicates a stack trace, which is slow when there are many warnings
 const originalWarn = console.warn;
@@ -10,4 +16,16 @@ console.warn = function warn(...args) {
     return;
   }
   originalWarn.apply(console, args);
+};
+
+const originalError = console.error;
+console.error = function error(...args) {
+  if (
+    args.length > 0 &&
+    typeof args[0] === 'string' &&
+    /^Warning: .* has been extracted/.test(args[0])
+  ) {
+    return;
+  }
+  originalError.apply(console, args);
 };
