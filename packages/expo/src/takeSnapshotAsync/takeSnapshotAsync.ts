1a96c01556e8180d30d785cd0b12628f33e3ca9e
Stanisław Chmiela <sjchmiela@gmail.com>
2019-05-09 19:19:55 +0200

    [expo] Deprecate takeSnapshotAsync in favor of react-native-view-shot.captureRef

--- packages/expo/src/takeSnapshotAsync/takeSnapshotAsync.ts
+++ packages/expo/src/takeSnapshotAsync/takeSnapshotAsync.ts
@@ -1,18 +1,3 @@
-import * as React from 'react';
-import { CaptureOptions } from 'react-native-view-shot';
-import captureRef from './captureRef';
-type ReactNativeNodeHandle = number;
+import { captureRef } from 'react-native-view-shot';
 
-export default async function takeSnapshotAsync<T>(
-  node?: ReactNativeNodeHandle | React.Component | React.RefObject<T>,
-  options?: CaptureOptions
-): Promise<string> {
-  if (node && typeof node === 'object' && 'current' in node && node.current) {
-    // React.RefObject
-    // @ts-ignore: captureRef's type doesn't include node handles
-    return captureRef(node.current, options);
-  }
-
-  // @ts-ignore: captureRef's type doesn't include node handles
-  return captureRef(node, options);
-}
+export default captureRef;
