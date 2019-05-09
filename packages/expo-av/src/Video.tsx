16341b726b93ff307d7c42bf767beb1d066b0c31
Bartłomiej Bukowski <bartlomiejbukowski.b@gmail.com>
2019-05-09 11:39:19 +0200

    [expo-av] Fix video default export (#4143)

--- packages/expo-av/src/Video.tsx
+++ packages/expo-av/src/Video.tsx
@@ -23,10 +23,10 @@ import {
   FullscreenUpdateEvent,
   NativeProps,
   NaturalSize,
-  VideoPlaybackProps,
+  VideoProps,
   ReadyForDisplayEvent,
   ResizeMode,
-  VideoPlaybackState,
+  VideoState,
 } from './Video.types';
 
 export {
@@ -34,10 +34,10 @@ export {
   FullscreenUpdateEvent,
   NativeProps,
   NaturalSize,
-  VideoPlaybackProps,
+  VideoProps,
   ReadyForDisplayEvent,
   ResizeMode,
-  VideoPlaybackState,
+  VideoState,
 }
 
 export const FULLSCREEN_UPDATE_PLAYER_WILL_PRESENT = 0;
@@ -76,7 +76,21 @@ const _STYLES = StyleSheet.create({
 const ExpoVideoManagerConstants = ExpoVideoManager;
 const ExpoVideoViewManager = ExpoVideoManager;
 
-export class VideoPlayback extends React.Component<VideoPlaybackProps, VideoPlaybackState> implements Playback {
+export default class Video extends React.Component<VideoProps, VideoState> implements Playback {
+  static RESIZE_MODE_CONTAIN = ResizeMode.CONTAIN;
+  static RESIZE_MODE_COVER = ResizeMode.COVER;
+  static RESIZE_MODE_STRETCH = ResizeMode.STRETCH;
+
+  static IOS_FULLSCREEN_UPDATE_PLAYER_WILL_PRESENT = IOS_FULLSCREEN_UPDATE_PLAYER_WILL_PRESENT;
+  static IOS_FULLSCREEN_UPDATE_PLAYER_DID_PRESENT = IOS_FULLSCREEN_UPDATE_PLAYER_DID_PRESENT;
+  static IOS_FULLSCREEN_UPDATE_PLAYER_WILL_DISMISS = IOS_FULLSCREEN_UPDATE_PLAYER_WILL_DISMISS;
+  static IOS_FULLSCREEN_UPDATE_PLAYER_DID_DISMISS = IOS_FULLSCREEN_UPDATE_PLAYER_DID_DISMISS;
+
+  static FULLSCREEN_UPDATE_PLAYER_WILL_PRESENT = FULLSCREEN_UPDATE_PLAYER_WILL_PRESENT;
+  static FULLSCREEN_UPDATE_PLAYER_DID_PRESENT = FULLSCREEN_UPDATE_PLAYER_DID_PRESENT;
+  static FULLSCREEN_UPDATE_PLAYER_WILL_DISMISS = FULLSCREEN_UPDATE_PLAYER_WILL_DISMISS;
+  static FULLSCREEN_UPDATE_PLAYER_DID_DISMISS = FULLSCREEN_UPDATE_PLAYER_DID_DISMISS;
+
   static propTypes = {
     // Source stuff
     source: PropTypes.oneOfType([
@@ -140,7 +154,7 @@ export class VideoPlayback extends React.Component<VideoPlaybackProps, VideoPlay
 
   // componentOrHandle: null | number | React.Component<any, any> | React.ComponentClass<any>
 
-  constructor(props: VideoPlaybackProps) {
+  constructor(props: VideoProps) {
     super(props);
     this.state = {
       showPoster: !!props.usePoster,
@@ -410,4 +424,4 @@ export class VideoPlayback extends React.Component<VideoPlaybackProps, VideoPlay
   }
 }
 
-Object.assign(VideoPlayback.prototype, PlaybackMixin);
+Object.assign(Video.prototype, PlaybackMixin);
