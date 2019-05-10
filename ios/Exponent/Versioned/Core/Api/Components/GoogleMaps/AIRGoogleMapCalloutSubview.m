32d8f7715b6b6c1e5503cb14ee061835c921ce8c
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2019-05-10 16:41:00 +0200

    [expo] Upgrade react-native-maps to 0.24.2 (#4158)
    * [ios] Upgrade react-native-maps to 0.24.2
    
    * [android] Update react-native-maps to 0.24.2
    
    * [expo] Update react-natie-maps dependency to 0.24.2
    
    * [changelog] Update changelog entry
    
    * fix merge
    
    * [ios] Add HAVE_GOOGLE_MAPS definition
    
    * [ios] Update react-native-maps
    
    * Update CHANGELOG.md
    
    Co-Authored-By: Tomasz Sapeta <tsapeta@users.noreply.github.com>
    
    * Update CHANGELOG.md

---
+++ ios/Exponent/Versioned/Core/Api/Components/GoogleMaps/AIRGoogleMapCalloutSubview.m
@@ -0,0 +1,19 @@
+//
+//  AIRGoogleMapCalloutSubview.m
+//  AirMaps
+//
+//  Created by Denis Oblogin on 10/8/18.
+//
+//
+
+#ifdef HAVE_GOOGLE_MAPS
+
+#import "AIRGoogleMapCalloutSubview.h"
+#import <React/RCTUtils.h>
+#import <React/RCTView.h>
+#import <React/RCTBridge.h>
+
+@implementation AIRGoogleMapCalloutSubview
+@end
+
+#endif
