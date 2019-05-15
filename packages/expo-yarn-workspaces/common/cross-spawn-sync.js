55bd734df17e3b6030586c222b08dd9f928a64ed
James Ide <ide@users.noreply.github.com>
2019-05-15 14:07:18 -0700

    Use "platform" instead of "os" since os refers to a module

--- packages/expo-yarn-workspaces/common/cross-spawn-sync.js
+++ packages/expo-yarn-workspaces/common/cross-spawn-sync.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const child_process = require('child_process');
-const os = require('os').platform();
+const platform = require('os').platform();
 
 module.exports = function spawnSync(cmd, args, options) {
-  cmd = os === 'win32' ? cmd + '.cmd' : cmd;
+  cmd = platform === 'win32' ? cmd + '.cmd' : cmd;
 
   return child_process.spawnSync(cmd, args, options);
 };
