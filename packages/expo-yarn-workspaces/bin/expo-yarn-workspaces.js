c4d7a02cfc0ddfde0df2be41a4254c9e09f63692
Nathan <14285307+nattyrice@users.noreply.github.com>
2019-05-15 02:20:27 -0400

    Fix spawnSync calls in expo-yarn-workspaces.js

--- packages/expo-yarn-workspaces/bin/expo-yarn-workspaces.js
+++ packages/expo-yarn-workspaces/bin/expo-yarn-workspaces.js
@@ -10,16 +10,16 @@ const spawnSync = require('../common/cross-spawn-sync');
 let argv = minimist(process.argv.slice(2));
 switch (argv._[0]) {
   case 'check-workspace-dependencies':
-    spawnSync('node', path.join(__dirname, 'check-workspace-dependencies.js'), {
+    spawnSync('node', [path.join(__dirname, 'check-workspace-dependencies.js')], {
       stdio: 'inherit',
     });
     break;
 
   case 'postinstall':
-    spawnSync('node', path.join(__dirname, 'symlink-necessary-packages.js'), {
+    spawnSync('node', [path.join(__dirname, 'symlink-necessary-packages.js')], {
       stdio: 'inherit',
     });
-    spawnSync('node', path.join(__dirname, 'make-entry-module.js'), {
+    spawnSync('node', [path.join(__dirname, 'make-entry-module.js')], {
       stdio: 'inherit',
     });
     break;
