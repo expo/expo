8cb425430858702bd113405c7afc7fd8ae97e024
Nathan <14285307+nattyrice@users.noreply.github.com>
2019-05-15 01:52:05 -0400

    [expo-yarn-workspaces] Prettified expo-yarn-workspaces.js

--- packages/expo-yarn-workspaces/bin/expo-yarn-workspaces.js
+++ packages/expo-yarn-workspaces/bin/expo-yarn-workspaces.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
-const minimist = require("minimist");
-const process = require("process");
-const path = require("path");
+const minimist = require('minimist');
+const process = require('process');
+const path = require('path');
 
-const spawnSync = require("../common/cross-spawn-sync");
+const spawnSync = require('../common/cross-spawn-sync');
 
 let argv = minimist(process.argv.slice(2));
 switch (argv._[0]) {
-  case "check-workspace-dependencies":
-    spawnSync("node", path.join(__dirname, "check-workspace-dependencies.js"), {
-      stdio: "inherit"
+  case 'check-workspace-dependencies':
+    spawnSync('node', path.join(__dirname, 'check-workspace-dependencies.js'), {
+      stdio: 'inherit',
     });
     break;
 
-  case "postinstall":
-    spawnSync("node", path.join(__dirname, "symlink-necessary-packages.js"), {
-      stdio: "inherit"
+  case 'postinstall':
+    spawnSync('node', path.join(__dirname, 'symlink-necessary-packages.js'), {
+      stdio: 'inherit',
     });
-    spawnSync("node", path.join(__dirname, "make-entry-module.js"), {
-      stdio: "inherit"
+    spawnSync('node', path.join(__dirname, 'make-entry-module.js'), {
+      stdio: 'inherit',
     });
     break;
 }
