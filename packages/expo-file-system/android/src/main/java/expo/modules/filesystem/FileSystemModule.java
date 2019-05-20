d9138587c0d22b06ebea185d514477731b98f98e
Stanisław Chmiela <sjchmiela@gmail.com>
2019-05-20 11:35:42 +0200

    [expo-file-system] Instantiate OkHttpClient lazily

--- packages/expo-file-system/android/src/main/java/expo/modules/filesystem/FileSystemModule.java
+++ packages/expo-file-system/android/src/main/java/expo/modules/filesystem/FileSystemModule.java
@@ -82,16 +82,6 @@ public class FileSystemModule extends ExportedModule implements ModuleRegistryCo
   @Override
   public void setModuleRegistry(ModuleRegistry moduleRegistry) {
     mModuleRegistry = moduleRegistry;
-    CookieHandler cookieHandler = mModuleRegistry.getModule(CookieHandler.class);
-    OkHttpClient.Builder builder =
-        new OkHttpClient.Builder()
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(60, TimeUnit.SECONDS)
-            .writeTimeout(60, TimeUnit.SECONDS);
-    if (cookieHandler != null) {
-      builder.cookieJar(new JavaNetCookieJar(cookieHandler));
-    }
-    mClient = builder.build();
   }
 
   @Override
@@ -494,7 +484,7 @@ public class FileSystemModule extends ExportedModule implements ModuleRegistryCo
             requestBuilder.addHeader(key, headers.get(key).toString());
           }
         }
-        mClient.newCall(requestBuilder.build()).enqueue(new Callback() {
+        getOkHttpClient().newCall(requestBuilder.build()).enqueue(new Callback() {
           @Override
           public void onFailure(Call call, IOException e) {
             Log.e(TAG, e.getMessage());
@@ -568,7 +558,7 @@ public class FileSystemModule extends ExportedModule implements ModuleRegistryCo
       };
 
       OkHttpClient client =
-          mClient.newBuilder()
+          getOkHttpClient().newBuilder()
               .addNetworkInterceptor(new Interceptor() {
                 @Override
                 public Response intercept(Chain chain) throws IOException {
@@ -789,6 +779,23 @@ public class FileSystemModule extends ExportedModule implements ModuleRegistryCo
 
   interface ProgressListener {
     void update(long bytesRead, long contentLength, boolean done);
+  }
+
+  private synchronized OkHttpClient getOkHttpClient() {
+    if (mClient == null) {
+      OkHttpClient.Builder builder =
+          new OkHttpClient.Builder()
+              .connectTimeout(60, TimeUnit.SECONDS)
+              .readTimeout(60, TimeUnit.SECONDS)
+              .writeTimeout(60, TimeUnit.SECONDS);
+
+      CookieHandler cookieHandler = mModuleRegistry.getModule(CookieHandler.class);
+      if (cookieHandler != null) {
+        builder.cookieJar(new JavaNetCookieJar(cookieHandler));
+      }
+      mClient = builder.build();
+    }
+    return mClient;
   }
 
   private String md5(File file) throws IOException {
