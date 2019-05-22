8a5ed4ce8e9bdbf4725d58b912f7eb57f268af49
Evan Bacon <baconbrix@gmail.com>
2019-05-22 16:25:40 +0200

    Update ExpoWebBrowser.web.ts

--- packages/expo-web-browser/src/ExpoWebBrowser.web.ts
+++ packages/expo-web-browser/src/ExpoWebBrowser.web.ts
@@ -1,5 +1,15 @@
+import { BrowserResult, OpenBrowserParams } from './WebBrowser.types';
+
 export default {
   get name() {
     return 'ExpoWebBrowser';
+  },
+  async openBrowserAsync(
+    url: string,
+    browserParams: OpenBrowserParams = {}
+  ): Promise<BrowserResult> {
+    const { target = ' ', features, replace } = browserParams;
+    window.open(url, target, features, replace);
+    return { type: 'dismiss' };
   },
 };
