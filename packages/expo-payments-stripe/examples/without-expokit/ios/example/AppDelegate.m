adb098790ffc781039f72f847a6da51edf0165c0
Szymon20000 <szymon20000@gmail.com>
2019-05-23 13:23:48 +0200

    [expo-payments-stripe] Update to latest unimodules API (#4290)

--- packages/expo-payments-stripe/examples/without-expokit/ios/example/AppDelegate.m
+++ packages/expo-payments-stripe/examples/without-expokit/ios/example/AppDelegate.m
@@ -11,14 +11,14 @@
 #import <React/RCTRootView.h>
 #import <Stripe/Stripe.h>
 
-#import <EXCore/EXModuleRegistry.h>
-#import <EXReactNativeAdapter/EXNativeModulesProxy.h>
-#import <EXReactNativeAdapter/EXModuleRegistryAdapter.h>
+#import <UMCore/UMModuleRegistry.h>
+#import <UMReactNativeAdapter/UMNativeModulesProxy.h>
+#import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
 #import <EXPermissions/EXRemoteNotificationRequester.h>
 
 @interface AppDelegate () <RCTBridgeDelegate>
 
-@property (nonatomic, strong) EXModuleRegistryAdapter *moduleRegistryAdapter;
+@property (nonatomic, strong) UMModuleRegistryAdapter *moduleRegistryAdapter;
 
 @end
 
@@ -26,7 +26,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleRegistryAdapter = [[EXModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[EXModuleRegistryProvider alloc] init]];
+  self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"example" initialProperties:nil];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
@@ -51,7 +51,7 @@
 
 - (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
 {
-  NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge andExperience:nil];
+  NSArray<id<RCTBridgeModule>> *extraModules = [_moduleRegistryAdapter extraModulesForBridge:bridge];
   // If you'd like to export some custom RCTBridgeModules that are not Expo modules, add them here!
   return extraModules;
 }
