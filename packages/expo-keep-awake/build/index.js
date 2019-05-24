436bf6c125b5445513bede70c471e08ea4571e77
James Ide <ide@expo.io>
2019-05-24 15:19:21 -0700

    Make AppEntry import from expo-keep-awake; update KeepAwake API to use hooks; docs
    With modular imports, we expect application code to import from `expo-keep-awake` instead of the `expo` package. This commit updates AppEntry -- both in the `expo` package and the one we internally use with `expo-yarn-workspaces` to import from `expo-keep-awake`. Tested this by deleting `__generated__` in native-component-list, running `yarn` to regenerate it, and loading NCL with no message about importing from `expo-keep-awake`.
    
    Since we are updating the files that import and use KeepAwake, I took this opportunity to revisit the KeepAwake API. I converted the component to a hook, which now works with React. (It is very easy to re-implement most of the component -- just do `function KeepAwake(tag) { useKeepAwake(tag); return null; }`.) I also renamed `activate` and `deactivate` to `activate/deactivateKeepAwake` so the names make sense afar from the import statement.
    
    Added deprecation messages to the old exports and updated the docs.

--- packages/expo-keep-awake/build/index.js
+++ packages/expo-keep-awake/build/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ExpoKeepAwake from './ExpoKeepAwake';
 // NOTE(brentvatne): in tests this value won't be reset because we
 // can render a component and never unmount it.
@@ -6,27 +6,51 @@ let __keepAwakeMountedCount = 0;
 const ExpoKeepAwakeTag = 'ExpoKeepAwakeDefaultTag';
 export default class KeepAwake extends React.PureComponent {
     componentDidMount() {
-        __keepAwakeMountedCount = __keepAwakeMountedCount + 1;
+        console.warn(`The KeepAwake component has been deprecated in favor of the useKeepAwake hook and will be removed in SDK 35`);
+        __keepAwakeMountedCount++;
         if (__keepAwakeMountedCount === 1) {
-            ExpoKeepAwake.activate();
+            activateKeepAwake();
         }
     }
     componentWillUnmount() {
-        __keepAwakeMountedCount = __keepAwakeMountedCount - 1;
-        if (__keepAwakeMountedCount == 0) {
-            ExpoKeepAwake.deactivate();
+        __keepAwakeMountedCount--;
+        if (__keepAwakeMountedCount === 0) {
+            deactivateKeepAwake();
         }
     }
     render() {
         return null;
     }
 }
-KeepAwake.activate = activate;
-KeepAwake.deactivate = deactivate;
+KeepAwake.activate = (tag) => {
+    console.warn(`The "KeepAwake.activate" static method has been deprecated in favor of the "activateKeepAwake" function exported from expo-keep-awake and will be removed in SDK 35`);
+    activateKeepAwake(tag);
+};
+KeepAwake.deactivate = (tag) => {
+    console.warn(`The "KeepAwake.deactivate" static method has been deprecated in favor of the "deactivateKeepAwake" function exported from expo-keep-awake and will be removed in SDK 35`);
+    deactivateKeepAwake(tag);
+};
+export function useKeepAwake(tag = generateUniqueTag()) {
+    useEffect(() => {
+        activateKeepAwake(tag);
+        return () => deactivateKeepAwake(tag);
+    }, []);
+}
+export function activateKeepAwake(tag = ExpoKeepAwakeTag) {
+    ExpoKeepAwake.activate(tag);
+}
+export function deactivateKeepAwake(tag = ExpoKeepAwakeTag) {
+    ExpoKeepAwake.deactivate(tag);
+}
 export function activate(tag) {
-    ExpoKeepAwake.activate(tag || ExpoKeepAwakeTag);
+    console.warn(`"activate" from expo-keep-awake has been deprecated in favor of "activateKeepAwake" and will be removed in SDK 35`);
+    activateKeepAwake(tag);
 }
 export function deactivate(tag) {
-    ExpoKeepAwake.deactivate(tag || ExpoKeepAwakeTag);
+    console.warn(`"deactivate" from expo-keep-awake has been deprecated in favor of "deactivateKeepAwake" and will be removed in SDK 35`);
+    deactivateKeepAwake(tag);
+}
+function generateUniqueTag() {
+    return `random-${Math.floor(Math.random() * (2 ** 53 - 1))}`;
 }
 //# sourceMappingURL=index.js.map
