a804526d7672453ec3e19140683e21fb535178d1
James Ide <ide@expo.io>
2019-05-25 00:04:54 -0700

    [template] Use a function component in blank template's App.js
    The blank template is intended to be minimal -- removing the class extension slims this down further.

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
