404125fb3ca5f05a8c44999f1fa4f37ceeb92094
Sergei Chestakov <sergei332@gmail.com>
2019-05-27 01:16:25 -0700

    [expo-module-template] Fix Expo Module Template on iOS (#4311)
    # Why
    
    After I ran `expo generate-module` and tried to build the iOS client with my module as a dependency in the Podfile, I ran into a bunch of compilation errors. Turns out we seemed to have switched the library with the core native module functions from `EXCore` to `UMCore` and similarly changed all of the function names/classes from the format `EXExportedModule` to `UMExportedModule`.
    
    # How
    
    Rename the imported libraries, classes, and method names with the `UM` prefix rather than `EX`.
    
    # Test Plan
    
    After making these changes, my iOS client successfully compiled with the new unimodule included on my test iPhone X.

--- packages/expo-module-template/ios/EXModuleTemplate/EXModuleTemplateView.h
+++ packages/expo-module-template/ios/EXModuleTemplate/EXModuleTemplateView.h
@@ -1,9 +1,9 @@
 // Copyright 2016-present 650 Industries. All rights reserved.
 
-#import <EXCore/EXModuleRegistry.h>
+#import <UMCore/UMModuleRegistry.h>
 
 @interface EXModuleTemplateView : UIView
 
-- (instancetype)initWithModuleRegistry:(EXModuleRegistry *)moduleRegistry;
+- (instancetype)initWithModuleRegistry:(UMModuleRegistry *)moduleRegistry;
 
 @end
