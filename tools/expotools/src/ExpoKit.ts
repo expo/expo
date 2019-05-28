7e01288fb6432f80f504feb59759b065ce07cd27
Stanisław Chmiela <sjchmiela@gmail.com>
2019-05-28 15:58:44 +0200

    [expotools] Add new update-react-native-unimodules-expokit task

--- tools/expotools/src/ExpoKit.ts
+++ tools/expotools/src/ExpoKit.ts
@@ -40,6 +40,26 @@ export async function updateExpoKitIosAsync(
   await Versions.setVersionsAsync(versions);
 }
 
+export async function updateReactNativeUnimodulesAsync(
+  expoDir: string,
+  reactNativeUnimodulesVersion: string,
+  sdkVersion: string
+): Promise<void> {
+  process.env.EXPO_STAGING = '1';
+  Config.api.host = 'staging.exp.host';
+  let versions = await Versions.versionsAsync();
+  if (!versions.sdkVersions[sdkVersion]) {
+    throw new Error(`SDK version ${sdkVersion} not found in versions JSON`);
+  }
+
+  if (!versions.sdkVersions[sdkVersion].packagesToInstallWhenEjecting) {
+    versions.sdkVersions[sdkVersion].packagesToInstallWhenEjecting = {};
+  }
+
+  versions.sdkVersions[sdkVersion].packagesToInstallWhenEjecting['react-native-unimodules'] = reactNativeUnimodulesVersion;
+  await Versions.setVersionsAsync(versions);
+}
+
 export async function updateExpoKitAndroidAsync(
   expoDir: string,
   appVersion: string,
