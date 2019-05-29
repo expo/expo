2dcf173f8a9a030a74fb0e6beeee73645b1a15af
Evan Bacon <baconbrix@gmail.com>
2019-05-29 15:54:17 +0200

    Update ExpoWebBrowser.web.js

--- packages/expo-web-browser/build/ExpoWebBrowser.web.js
+++ packages/expo-web-browser/build/ExpoWebBrowser.web.js
@@ -2,5 +2,10 @@ export default {
     get name() {
         return 'ExpoWebBrowser';
     },
+    async openBrowserAsync(url, browserParams = {}) {
+        const { windowName = '_blank', windowFeatures, replace } = browserParams;
+        window.open(url, windowName, windowFeatures, replace);
+        return { type: 'dismiss' };
+    },
 };
 //# sourceMappingURL=ExpoWebBrowser.web.js.map
