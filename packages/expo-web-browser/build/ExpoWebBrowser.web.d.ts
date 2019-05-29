a7524ecbc3767a6d38b75169d43cea763eb39610
Evan Bacon <baconbrix@gmail.com>
2019-05-29 15:54:21 +0200

    Update ExpoWebBrowser.web.d.ts

--- packages/expo-web-browser/build/ExpoWebBrowser.web.d.ts
+++ packages/expo-web-browser/build/ExpoWebBrowser.web.d.ts
@@ -1,4 +1,6 @@
+import { BrowserResult, OpenBrowserParams } from './WebBrowser.types';
 declare const _default: {
     readonly name: string;
+    openBrowserAsync(url: string, browserParams?: OpenBrowserParams): Promise<BrowserResult>;
 };
 export default _default;
