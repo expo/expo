96d59290b1df951293fc2c8782cf5278a541a7e0
Evan Bacon <baconbrix@gmail.com>
2019-05-29 15:53:59 +0200

    Update WebBrowser.d.ts

--- packages/expo-web-browser/build/WebBrowser.d.ts
+++ packages/expo-web-browser/build/WebBrowser.d.ts
@@ -1,29 +1,4 @@
-declare type OpenBrowserParams = {
-    toolbarColor?: string;
-    browserPackage?: string;
-    enableBarCollapsing?: boolean;
-    showTitle?: boolean;
-};
-declare type AuthSessionResult = RedirectResult | BrowserResult;
-declare type CustomTabsBrowsersResults = {
-    defaultBrowserPackage?: string;
-    preferredBrowserPackage?: string;
-    browserPackages: string[];
-    servicePackages: string[];
-};
-declare type BrowserResult = {
-    type: 'cancel' | 'dismiss';
-};
-declare type RedirectResult = {
-    type: 'success';
-    url: string;
-};
-declare type ServiceActionResult = {
-    servicePackage?: string;
-};
-declare type MayInitWithUrlResult = ServiceActionResult;
-declare type WarmUpResult = ServiceActionResult;
-declare type CoolDownResult = ServiceActionResult;
+import { OpenBrowserParams, AuthSessionResult, CustomTabsBrowsersResults, BrowserResult, MayInitWithUrlResult, WarmUpResult, CoolDownResult } from './WebBrowser.types';
 export declare function getCustomTabsSupportingBrowsersAsync(): Promise<CustomTabsBrowsersResults>;
 export declare function warmUpAsync(browserPackage?: string): Promise<WarmUpResult>;
 export declare function mayInitWithUrlAsync(url: string, browserPackage?: string): Promise<MayInitWithUrlResult>;
@@ -32,4 +7,3 @@ export declare function openBrowserAsync(url: string, browserParams?: OpenBrowse
 export declare function dismissBrowser(): void;
 export declare function openAuthSessionAsync(url: string, redirectUrl: string): Promise<AuthSessionResult>;
 export declare function dismissAuthSession(): void;
-export {};
