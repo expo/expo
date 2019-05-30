bedd9e76d99093136c0b7a80a3bf36d9774e57e4
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2019-05-30 11:08:24 +0200

    [templates] Move project templates from the universe repo (#4335)
    Moving `expo-template-blank` and `expo-template-tabs` from the universe repo, so all project templates will be at the same place.
    Unfortunately there are some duplicated commits because I wanted to keep the history and was unable to move these two folders at once.
    
    Also added `publish-project-templates` script to `expotools`. Demo: 
    ![Screen Shot 2019-05-30 at 09 54 05](https://user-images.githubusercontent.com/1714764/58618307-8e0d1880-82c2-11e9-871a-efef88d4222a.png)

---
+++ templates/expo-template-tabs/navigation/MainTabNavigator.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Links',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
