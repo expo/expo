ad8ca8aa6f5b15502279cb1c43aa46448f0bd46e
Simen Bekkhus <sbekkhus91@gmail.com>
2019-06-03 16:21:10 +0200

    Update registerRootComponent.web.tsx

--- packages/expo/src/launch/registerRootComponent.web.tsx
+++ packages/expo/src/launch/registerRootComponent.web.tsx
@@ -5,7 +5,7 @@ import withExpoRoot from './withExpoRoot';
 import { InitialProps } from './withExpoRoot.types';
 
 export default function registerRootComponent<P extends InitialProps>(
-  component: React.ComponentClass<P>
+  component: React.ComponentType<P>
 ): void {
   const App = withExpoRoot(component);
   // @ts-ignore: TypeScript says ComponentClass<P> does not satisfy ComponentClass<any>
