a6fcbd7821a050742044987fe33431cb27c1d377
Evan Bacon <baconbrix@gmail.com>
2019-06-02 18:40:28 -0700

    [storybook] Jest snapshots (#4379)
    * added jest snapshots
    
    * Removed loki command
    
    * deleted loki
    
    * Update babel.config.js
    
    * Update config.js
    
    * Update addons.js
    
    * Update babel.config.js
    
    * [WIP]
    
    * Fixed storybook in workspace
    
    * Update config.js
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update yarn.lock
    
    * Update storyshots.test.js
    
    * Create storyshots-test-js-image-ap-is-constants-1-snap.png
    
    * Create storyshots-test-js-image-components-blur-view-1-snap.png
    
    * Create storyshots-test-js-image-components-linear-gradient-1-snap.png
    
    * Update storyshots.test.js.snap
    
    * Added storybook test to CI
    
    * Update config.yml
    
    * Update storyshots-test-js-image-components-blur-view-1-snap.png
    
    * Update storyshots.test.js
    
    * Update package.json
    
    * Added image params for CI
    
    * Update storyshots.test.js.snap
    
    * Update storyshots.test.js
    
    * Update black-128x256.png
    
    * Update example_image_1.jpg
    
    * Update exponent-icon@2x.png
    
    * Update exponent-icon@3x.png
    
    * Update app.png
    
    * Update loading.png
    
    * Update qrcode_photo_wikipedia.jpg
    
    * Update roadster.jpg
    
    * Update package.json
    
    * Update BlurViewScreen.js
    
    * Update LinearGradientScreen.js
    
    * Update package.json
    
    * Update storyshots-test-js-image-components-blur-view-1-snap.png
    
    * Update BlurViewScreen.js
    
    * Update LinearGradientScreen.js
    
    * Update AppText.js
    
    * Update Code.js
    
    * Update DocItem.js
    
    * Create rem.js
    
    * Update Section.js
    
    * Update StyleList.js
    
    * Update TextList.js
    
    * Update UIExplorer.js
    
    * Update package.json
    
    * Update storyshots.test.js.snap
    
    * Made style more generic
    
    * Disabled visual regression testing in CI

--- apps/storybook/.storybook/config.js
+++ apps/storybook/.storybook/config.js
@@ -1,12 +1,13 @@
-import { setOptions } from '@storybook/addon-options';
+import { withOptions } from '@storybook/addon-options';
 import centered from './decorator-centered';
 import { configure, addDecorator } from '@storybook/react';
+import requireContext from 'require-context.macro';
 
-const context = require.context('../stories', true, /Screen\.js$/);
+const context = requireContext('../stories', true, /Screen\.js$/);
 
 addDecorator(centered);
 
-setOptions({
+withOptions({
   name: 'Expo',
   url: 'https://',
   goFullScreen: false,
