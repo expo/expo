c196ef2c15e3386b73614ce824a2aa22d42e635f
Simen Bekkhus <sbekkhus91@gmail.com>
2019-06-03 16:21:36 +0200

    Update withExpoRoot.tsx

--- packages/expo/src/launch/withExpoRoot.tsx
+++ packages/expo/src/launch/withExpoRoot.tsx
@@ -7,7 +7,7 @@ import RootErrorBoundary from './RootErrorBoundary';
 import { InitialProps } from './withExpoRoot.types';
 
 export default function withExpoRoot<P extends InitialProps>(
-  AppRootComponent: React.ComponentClass<P>
+  AppRootComponent: React.ComponentType<P>
 ): React.ComponentClass<P> {
   return class ExpoRootComponent extends React.Component<P> {
     componentWillMount() {
