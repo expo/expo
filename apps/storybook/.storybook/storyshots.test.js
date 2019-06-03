f20f3cec71749c25612931ad540f754ce9f67c24
Evan Bacon <baconbrix@gmail.com>
2019-06-02 17:13:11 -0700

    Added image params for CI

--- apps/storybook/.storybook/storyshots.test.js
+++ apps/storybook/.storybook/storyshots.test.js
@@ -6,14 +6,43 @@ initStoryshots({
   suite: 'JSON',
 });
 
+// In the build command we export a variable which helps us detect a web env.
+// visual regression testing only works in the browser
 if (process.env.BABEL_ENV === 'test:web') {
+  // Locally it's better to start the server and then run the tests.
+  // In CI we want to build a static bundle and test against that.
   const storybookUrl = process.env.IS_EXPO_CI
     ? 'file://' + path.resolve(__dirname, '..', 'storybook-static')
     : 'http://localhost:6006/';
+
+  // TODO: Bacon: Only capture the components, the description seems less than useful.
+  const getMatchOptions = ({ context: { kind, story }, url }) => {
+    return {
+      failureThreshold: 0.2,
+      failureThresholdType: 'percent',
+    };
+  };
+
+  const getScreenshotOptions = ({ context, url }) => ({
+    fullPage: true, // Do not take the full page screenshot. Default is 'true' in Storyshots.
+  });
+
+  const devices = require('puppeteer/DeviceDescriptors');
+
+  const device = devices['iPad Pro'];
+  // const device = devices['iPhone X'];
+
+  function customizePage(page) {
+    return page.emulate(device);
+  }
+
   initStoryshots({
     suite: 'Image',
     test: imageSnapshot({
       storybookUrl,
+      getMatchOptions,
+      getScreenshotOptions,
+      customizePage,
     }),
   });
 }
