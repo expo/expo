6c210243d11ca2b21f9d086753c762c365642c03
Evan Bacon <baconbrix@gmail.com>
2019-06-03 12:21:35 -0700

    [storybook] Migrate components (#4391)
    * Update yarn.lock
    
    * Update jest.config.web.js
    
    * Update UIExplorer.js
    
    * Create Table.js
    
    * Create ListButton.js
    
    * Update index.js
    
    * Update DocItem.js
    
    * Added storybook components
    
    * [WIP] update casing names
    
    * Revert category names
    
    * Update storyshots.test.js.snap

---
+++ apps/storybook/stories/APIs/Brightness.stories.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export const title = 'Brightness';
+export const packageJson = require('expo-brightness/package.json');
+export const component = () => <View />;
