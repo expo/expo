51c59460342554a50f4528e7d999f28451be7f40
Simen Bekkhus <sbekkhus91@gmail.com>
2019-06-03 16:18:29 +0200

    `registerRootComponent` should accept FunctionalComponent

--- packages/expo/src/launch/registerRootComponent.tsx
+++ packages/expo/src/launch/registerRootComponent.tsx
@@ -5,7 +5,7 @@ import withExpoRoot from './withExpoRoot';
 import { InitialProps } from './withExpoRoot.types';
 
 export default function registerRootComponent<P extends InitialProps>(
-  component: React.ComponentClass<P>
+  component: React.ComponentType<P>
 ): void {
   // @ts-ignore: TypeScript says ComponentClass<P> does not satisfy ComponentClass<any>
   AppRegistry.registerComponent('main', () => withExpoRoot(component));
