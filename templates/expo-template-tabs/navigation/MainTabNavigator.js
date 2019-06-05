5b02f815d1132139d158c77d8319d2ac451a4021
Drake Boley <drakens6666@gmail.com>
2019-06-04 20:22:11 -0700

    Update MainTabNavigator.js
    Make this driven by an array instead of by repeating code - enforce a better standard that could be adapted to a data source :)

--- templates/expo-template-tabs/navigation/MainTabNavigator.js
+++ templates/expo-template-tabs/navigation/MainTabNavigator.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import {
-  createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
@@ -14,50 +11,46 @@ const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
+const components = [
+  {
+    component: HomeScreen,
+    icon: 'information-circle',
+    tabBarLabel: 'Home',
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
+  },
+  {
+    component: LinksScreen,
+    icon: 'link',
+    tabBarLabel: 'Links',
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
+  },
+  {
+    component: SettingsScreen,
+    icon: 'options',
+    tabBarLabel: 'Settings',
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
+  },
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-};
+]
+const stacks = {}
+components.forEach((item) => {
+  let stack = createStackNavigator({
+    [item.tabBarLabel]: item.component,
+  })
+  stack.navigationOptions = {
+    tabBarLabel: item.tabBarLabel,
+    tabBarIcon: ({ focused }) => (
+      <TabBarIcon
+        focused={focused}
+        name={
+          Platform.OS === 'ios'
+            ? `ios-${item.icon}`
+            : `md-${item.icon}`
+        }
+      />
+    )
+  }
+  stacks[item.tabBarLabel] = stack
+})
 
-export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
-});
+export default createBottomTabNavigator(stacks);
