34d4f9de001931d564d32dec35172a18650a9ce4
vivianzzhu <tongyu@usc.edu>
2019-06-04 19:15:03 -0700

    add console message and revert files

--- packages/expo/src/logs/LogSerialization.ts
+++ packages/expo/src/logs/LogSerialization.ts
@@ -73,12 +73,14 @@ function _stringifyLogData(data: unknown[]): string[] {
     if (typeof item === 'string') {
       return item;
     } else {
+      // define the max length for log msg to be first 10000 characters
+      const LOG_MESSAGE_MAX_LENGTH = 10000;
       let result = prettyFormat(item, { plugins: [ReactNodeFormatter] });
       // check the size of string returned
-      if (result.length > 10000) {
-        let truncatedResult = result.substring(0, 10000);
-        // truncate the result string to the first 10000 characters
-        truncatedResult += '...';
+      if (result.length > LOG_MESSAGE_MAX_LENGTH) {
+        let truncatedResult = result.substring(0, LOG_MESSAGE_MAX_LENGTH);
+        // truncate the result string to the max length
+        truncatedResult += `...(truncated to the first ${LOG_MESSAGE_MAX_LENGTH} characters)`;
         return truncatedResult;
       } else {
         return result;
