6f2961aade2b1965a4bf6c2bfb90fe65d794164c
cruzach <charliecruzan@gmail.com>
2019-06-06 10:22:53 -0700

    fix: add haptics redirect

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -29,6 +29,8 @@ redirects[versions/latest/guides/offline-support.html]=versions/latest/guides/of
 redirects[versions/latest/sdk/]=versions/latest/sdk/overview/
 # upgrading expo -> upgrading sdk walkthrough
 redirects[versions/latest/workflow/upgrading-expo]=versions/latest/workflow/upgrading-expo-sdk-walkthrough/
+# Haptic Link from Google Search -> correct Haptic link
+redirects[versions/latest/sdk/haptic]=versions/latest/sdk/haptics/
 
 for i in "${!redirects[@]}" # iterate over keys
 do
