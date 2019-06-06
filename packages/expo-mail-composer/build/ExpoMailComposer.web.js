2d6811534cfe30741a6f9be49d0c55e262363b25
James Ide <ide@expo.io>
2019-06-05 17:58:30 -0700

    [mailcomposer] Temporarily ts-ignore expo-mailcomposer
    This needs to be fixed, but expo depends on 5.0.1 of mailcomposer so we need to republish 5.0.0 as 5.0.1.

--- packages/expo-mail-composer/build/ExpoMailComposer.web.js
+++ packages/expo-mail-composer/build/ExpoMailComposer.web.js
@@ -18,6 +18,7 @@ export default {
             subject: options.subject,
             body: options.body,
         });
+        // @ts-ignore: Fix this -- just patching to get publishing working for now.
         const query = qs.stringify(email);
         const queryComponent = query ? '?' + query : '';
         const to = checkValue(options.recipients);
