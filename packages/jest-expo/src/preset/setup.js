59706745f0d47abdbfc2e70d8a7d687d8ef03886
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2019-06-10 16:51:11 +0200

    [jest] Fix expo-file-system mocks by unscoping methods (#4520)
    # Why
    
    Fixes #4503 
    
    # How
    
    Updated `expo-file-system` mock in `jest-expo`
    
    # Test Plan
    
    It must work 😅

--- packages/jest-expo/src/preset/setup.js
+++ packages/jest-expo/src/preset/setup.js
@@ -148,18 +148,16 @@ mockNativeModules.NativeUnimoduleProxy.modulesConstants = {
 };
 
 jest.mock('expo-file-system', () => ({
-  FileSystem: {
-    downloadAsync: jest.fn(() => Promise.resolve({ md5: 'md5', uri: 'uri' })),
-    getInfoAsync: jest.fn(() => Promise.resolve({ exists: true, md5: 'md5', uri: 'uri' })),
-    readAsStringAsync: jest.fn(),
-    writeAsStringAsync: jest.fn(),
-    deleteAsync: jest.fn(),
-    moveAsync: jest.fn(),
-    copyAsync: jest.fn(),
-    makeDirectoryAsync: jest.fn(),
-    readDirectoryAsync: jest.fn(),
-    createDownloadResumable: jest.fn(),
-  },
+  downloadAsync: jest.fn(() => Promise.resolve({ md5: 'md5', uri: 'uri' })),
+  getInfoAsync: jest.fn(() => Promise.resolve({ exists: true, md5: 'md5', uri: 'uri' })),
+  readAsStringAsync: jest.fn(() => Promise.resolve()),
+  writeAsStringAsync: jest.fn(() => Promise.resolve()),
+  deleteAsync: jest.fn(() => Promise.resolve()),
+  moveAsync: jest.fn(() => Promise.resolve()),
+  copyAsync: jest.fn(() => Promise.resolve()),
+  makeDirectoryAsync: jest.fn(() => Promise.resolve()),
+  readDirectoryAsync: jest.fn(() => Promise.resolve()),
+  createDownloadResumable: jest.fn(() => Promise.resolve()),
 }));
 
 jest.mock('react-native/Libraries/Image/AssetRegistry', () => ({
