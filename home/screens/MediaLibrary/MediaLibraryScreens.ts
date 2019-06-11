d6ff2eabfbb11e70cc55ccad27d172b6aea4658e
Evan Bacon <baconbrix@gmail.com>
2019-06-10 22:52:34 -0700

    [home] Added image banner for Expo home. (#4424)
    * Shimmed out NativeModules.ExponentKernel when running home in home
    
    * shim session storage
    
    * Added banner photo
    
    * Update Profile.js
    
    * Added parallax header
    
    * Updated imports
    
    * Added default banner
    
    * Removed chosen log
    
    * Delete ExponentKernel.js
    
    * Update SearchBar.js
    
    * Update SearchResults.js
    
    * Removed MonoText
    
    * Delete MediaDetailsScreen.tsx
    
    * Update MediaLibraryScreens.ts
    
    * Update MediaLibraryScreen.tsx
    
    * Fixed android bugs
    
    * Update banner-image.png
    
    * Fixed tests
    
    * Update Store.js
    
    * Update ProfileReducer.js
    
    * Update MediaAlbumsScreen.tsx

---
+++ home/screens/MediaLibrary/MediaLibraryScreens.ts
@@ -0,0 +1,7 @@
+import MediaAlbumsScreen from './MediaAlbumsScreen';
+import MediaLibraryScreen from './MediaLibraryScreen';
+
+export default {
+  MediaLibrary: MediaLibraryScreen,
+  MediaAlbums: MediaAlbumsScreen,
+};
