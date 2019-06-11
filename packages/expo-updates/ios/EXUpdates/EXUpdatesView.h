392cc762aa911819b5839a5aa4b7316ba4f28388
Eric Samelson <samelson.e@gmail.com>
2019-06-11 11:20:23 -0700

    [expo-updates] remove view and viewmanager

--- packages/expo-updates/ios/EXUpdates/EXUpdatesView.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-present 650 Industries. All rights reserved.
-
-#import <UMCore/UMModuleRegistry.h>
-
-@interface EXUpdatesView : UIView
-
-- (instancetype)initWithModuleRegistry:(UMModuleRegistry *)moduleRegistry;
-
-@end
--- packages/expo-updates/ios/EXUpdates/EXUpdatesViewManager.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2016-present 650 Industries. All rights reserved.
-
-#import <UMCore/UMViewManager.h>
-#import <UMCore/UMModuleRegistryConsumer.h>
-
-@interface EXUpdatesViewManager : UMViewManager <UMModuleRegistryConsumer>
-
-@end
