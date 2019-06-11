c9eb7925e387c732be9bd5558957a7861e357cd3
Evan Bacon <baconbrix@gmail.com>
2019-06-10 22:59:30 -0700

    Removed the albums screen

--- home/screens/MediaLibrary/MediaLibraryScreens.ts
+++
@@ -1,7 +0,0 @@
-import MediaAlbumsScreen from './MediaAlbumsScreen';
-import MediaLibraryScreen from './MediaLibraryScreen';
-
-export default {
-  MediaLibrary: MediaLibraryScreen,
-  MediaAlbums: MediaAlbumsScreen,
-};
