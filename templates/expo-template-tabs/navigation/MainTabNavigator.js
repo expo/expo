07a085047e7a8049efeb3aca2cdef515b4c5d49b
Drake Boley <drakens6666@gmail.com>
2019-06-13 05:03:36 -0700

    Ran prettier over file
    As per request.

--- templates/expo-template-tabs/navigation/MainTabNavigator.js
+++ templates/expo-template-tabs/navigation/MainTabNavigator.js
@@ -1,56 +1,51 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import LinksScreen from "../screens/LinksScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 
 const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+  Home: HomeScreen
 });
 
 const components = [
   {
     component: HomeScreen,
-    icon: 'information-circle',
-    tabBarLabel: 'Home',
-
+    icon: "information-circle",
+    tabBarLabel: "Home"
   },
   {
     component: LinksScreen,
-    icon: 'link',
-    tabBarLabel: 'Links',
-
+    icon: "link",
+    tabBarLabel: "Links"
   },
   {
     component: SettingsScreen,
-    icon: 'options',
-    tabBarLabel: 'Settings',
-
-  },
-
-]
-const stacks = {}
-components.forEach((item) => {
+    icon: "options",
+    tabBarLabel: "Settings"
+  }
+];
+const stacks = {};
+components.forEach(item => {
   let stack = createStackNavigator({
-    [item.tabBarLabel]: item.component,
-  })
+    [item.tabBarLabel]: item.component
+  });
   stack.navigationOptions = {
     tabBarLabel: item.tabBarLabel,
     tabBarIcon: ({ focused }) => (
       <TabBarIcon
         focused={focused}
-        name={
-          Platform.OS === 'ios'
-            ? `ios-${item.icon}`
-            : `md-${item.icon}`
-        }
+        name={Platform.OS === "ios" ? `ios-${item.icon}` : `md-${item.icon}`}
       />
     )
-  }
-  stacks[item.tabBarLabel] = stack
-})
+  };
+  stacks[item.tabBarLabel] = stack;
+});
 
 export default createBottomTabNavigator(stacks);
