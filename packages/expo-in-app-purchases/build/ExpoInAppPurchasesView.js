aea06c3d8c397b92f65d65a3fe9743a6366270eb
Sergei Chestakov <schestakov@ucdavis.edu>
2019-06-17 22:39:15 -0700

    Delete unused view build files

--- packages/expo-in-app-purchases/build/ExpoInAppPurchasesView.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { requireNativeViewManager } from '@unimodules/core';
-export default class ExpoModuleTemplateView extends React.Component {
-    render() {
-        return (<ExpoModuleTemplateView.NativeView />);
-    }
-}
-ExpoModuleTemplateView.NativeView = requireNativeViewManager('ExpoModuleTemplateView');
-//# sourceMappingURL=ExpoInAppPurchasesView.js.map
--- packages/expo-in-app-purchases/build/ExpoModuleTemplateView.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { requireNativeViewManager } from '@unimodules/core';
-export default class ExpoInAppPurchasesView extends React.Component {
-    render() {
-        return (<ExpoInAppPurchasesView.NativeView />);
-    }
-}
-ExpoInAppPurchasesView.NativeView = requireNativeViewManager('ExpoInAppPurchasesView');
-//# sourceMappingURL=ExpoModuleTemplateView.js.map
