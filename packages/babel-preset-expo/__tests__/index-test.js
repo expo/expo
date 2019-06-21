e0fb0a04cf3436c4a92a77cf73b0659ff357880f
Yifei He <yifei@hesyifei.com>
2019-06-21 13:59:36 -0700

    [babel-preset-expo] Add tests for `lazy` option

--- packages/babel-preset-expo/__tests__/index-test.js
+++ packages/babel-preset-expo/__tests__/index-test.js
@@ -72,3 +72,48 @@ import 'react-native-vector-icons';
   expect(code).toMatch(/"@expo\/vector-icons"/);
   expect(code).toMatchSnapshot();
 });
+
+it(`uses lazy - default (no option) matches option "null"`, () => {
+  let testFilename = path.join(path.resolve(__dirname, 'samples'), 'Lazy.js');
+  let optionsDefault = {
+    babelrc: false,
+    presets: [preset],
+  };
+  let { codeDefault } = babel.transformFileSync(testFilename, optionsDefault);
+
+  let optionsNull = {
+    babelrc: false,
+    presets: [[preset, { lazy: null }]],
+  };
+  let { codeNull } = babel.transformFileSync(testFilename, optionsNull);
+
+  expect(codeDefault).toEqual(codeNull);
+});
+
+it(`uses lazy - different values`, () => {
+  let testFilename = path.join(path.resolve(__dirname, 'samples'), 'Lazy.js');
+  let lazy_options = [
+    null,
+    false,
+    true,
+    ['inline-comp', './inline-func', '../inline-func-with-side-effects.fx.ts'],
+    name => {
+      if (name.endsWith('.fx') || name.endsWith('.fx.js') || name.endsWith('.fx.ts')) {
+        return false;
+      }
+      return true;
+    },
+  ];
+
+  for (let lazy_option of lazy_options) {
+    let options = {
+      babelrc: false,
+      presets: [[preset, { lazy: lazy_option }]],
+      // Make the snapshot easier to read
+      retainLines: true,
+    };
+
+    let { code } = babel.transformFileSync(testFilename, options);
+    expect(code).toMatchSnapshot();
+  }
+});
