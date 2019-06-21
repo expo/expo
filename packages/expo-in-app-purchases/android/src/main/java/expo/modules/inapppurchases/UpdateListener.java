17d5c9ae14ce4fc25165428e595c6debf50dcd8c
Sergei Chestakov <schestakov@ucdavis.edu>
2019-06-21 10:27:21 -0700

    Reformat Android project to use 2 spaces

--- packages/expo-in-app-purchases/android/src/main/java/expo/modules/inapppurchases/UpdateListener.java
+++ packages/expo-in-app-purchases/android/src/main/java/expo/modules/inapppurchases/UpdateListener.java
@@ -16,39 +16,40 @@ import org.unimodules.core.Promise;
  * Handler to billing updates
  */
 public class UpdateListener implements BillingManager.BillingUpdatesListener {
-    private static final String TAG = "UpdateListener";
-    private EventEmitter mEventEmitter;
+  private static final String TAG = "UpdateListener";
+  private EventEmitter mEventEmitter;
 
-    public UpdateListener(EventEmitter eventEmitter) {
-        mEventEmitter = eventEmitter;
+  public UpdateListener(EventEmitter eventEmitter) {
+    mEventEmitter = eventEmitter;
+  }
+
+  @Override
+  public void onBillingClientSetupFinished() {
+  }
+
+  @Override
+  public void onConsumeFinished(String token, BillingResult result) {
+    Bundle response = new Bundle();
+    response.putInt("responseCode", result.getResponseCode());
+    response.putString("token", token);
+
+    Promise promise = BillingManager.promises.get(BillingManager.ACKNOWLEDGING_PURCHASE);
+    if (promise != null) {
+      BillingManager.promises.put(BillingManager.ACKNOWLEDGING_PURCHASE, null);
+      promise.resolve(response);
     }
+  }
 
-    @Override
-    public void onBillingClientSetupFinished() {}
-
-    @Override
-    public void onConsumeFinished(String token, BillingResult result) {
-        Bundle response = new Bundle();
-        response.putInt("responseCode", result.getResponseCode());
-        response.putString("token", token);
-
-        Promise promise = BillingManager.promises.get(BillingManager.ACKNOWLEDGING_PURCHASE);
-        if (promise != null) {
-            BillingManager.promises.put(BillingManager.ACKNOWLEDGING_PURCHASE, null);
-            promise.resolve(response);
-        }
+  @Override
+  public void onPurchasesUpdated(List<Purchase> purchaseList) {
+    Bundle response = new Bundle();
+    ArrayList<Bundle> results = new ArrayList<>();
+    for (Purchase purchase : purchaseList) {
+      results.add(BillingManager.purchaseToBundle(purchase));
     }
+    response.putParcelableArrayList("results", results);
+    response.putInt("responseCode", BillingResponseCode.OK);
 
-    @Override
-    public void onPurchasesUpdated(List<Purchase> purchaseList) {
-        Bundle response = new Bundle();
-        ArrayList<Bundle> results = new ArrayList<>();
-        for (Purchase purchase : purchaseList) {
-            results.add(BillingManager.purchaseToBundle(purchase));
-        }
-        response.putParcelableArrayList("results", results);
-        response.putInt("responseCode", BillingResponseCode.OK);
-
-        mEventEmitter.emit(BillingManager.PURCHASES_UPDATED_EVENT, response);
-    }
+    mEventEmitter.emit(BillingManager.PURCHASES_UPDATED_EVENT, response);
+  }
 }
