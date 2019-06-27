bf3e8710343c1c03ff7ef792a71f8524c6d7a067
Eric Samelson <esamelson@users.noreply.github.com>
2019-06-27 12:40:29 -0700

    [expo-updates] add blank module (#4603)

---
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/UpdatesModule.java
@@ -0,0 +1,33 @@
+package expo.modules.updates;
+
+import android.content.Context;
+
+import org.unimodules.core.ExportedModule;
+import org.unimodules.core.ModuleRegistry;
+import org.unimodules.core.Promise;
+import org.unimodules.core.interfaces.ExpoMethod;
+
+public class UpdatesModule extends ExportedModule {
+  private static final String NAME = "ExpoUpdates";
+  private static final String TAG = UpdatesModule.class.getSimpleName();
+
+  private ModuleRegistry mModuleRegistry;
+
+  public UpdatesModule(Context context) {
+    super(context);
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public void onCreate(ModuleRegistry moduleRegistry) {
+    mModuleRegistry = moduleRegistry;
+  }
+
+  @ExpoMethod
+  public void someGreatMethodAsync(Map<String, Object> options, final Promise promise) {
+  }
+}
