23cb15e38781922b552236461255bc2d21356d84
Yifei He <yifei@hesyifei.com>
2019-06-27 16:05:42 -0700

    [ios] Add support for displaying rich content
    This is done by creating a `UNNotificationServiceExtension`.
    
    Ref:
    - https://docs.leanplum.com/docs/adding-images-to-push-notifications
    - https://developer.apple.com/documentation/usernotifications/unnotificationattachment
    - https://stackoverflow.com/a/40734571/2603230

---
+++ ios/ExpoNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,71 @@
+//
+//  NotificationService.swift
+//  ExpoNotificationServiceExtension
+//
+//  Created by Yifei He (hesyifei) on 06/27/2019.
+//  Copyright © 2019 650 Industries. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+	var contentHandler: ((UNNotificationContent) -> Void)?
+	var bestAttemptContent: UNMutableNotificationContent?
+
+	override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+		self.contentHandler = contentHandler
+		bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+		if let bestAttemptContent = bestAttemptContent, let notificationBody = request.content.userInfo["body"] as? [String: Any] {
+			// Display priority: video > audio > image
+			if let richContent = notificationBody["_richContent"] as? [String: Any], let attachmentUrlString = (richContent["video"] ?? richContent["audio"] ?? richContent["image"]) as? String {
+				downloadAttachment(url: attachmentUrlString) { attachment in
+					if let attachment = attachment {
+						bestAttemptContent.attachments = [attachment]
+					}
+					contentHandler(bestAttemptContent)
+				}
+			} else {
+				contentHandler(bestAttemptContent)
+			}
+		}
+	}
+
+	func downloadAttachment(url attachmentUrlString: String?, callback: ((UNNotificationAttachment?) -> Void)?) {
+		if let attachmentUrlString = attachmentUrlString, let attachmentUrl = URL(string: attachmentUrlString) {
+			URLSession.shared.downloadTask(with: attachmentUrl) { (location, response, downloadError) in
+				if let downloadError = downloadError {
+					print("URLSession: Error with downloading the rich content attachment: \(downloadError.localizedDescription)")
+					callback?(nil)
+					return
+				}
+
+				if let location = location {
+					// Prefix the filename with a random UUID to avoid having file with the same file name during `moveItem`.
+					let temporaryUrl = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString + attachmentUrl.lastPathComponent)
+					do {
+						try FileManager.default.moveItem(at: location, to: temporaryUrl)
+						let attachment = try? UNNotificationAttachment(identifier: attachmentUrl.absoluteString, url: temporaryUrl)
+						callback?(attachment)
+						try? FileManager.default.removeItem(at: temporaryUrl)
+					} catch let attachmentError {
+						print("Error with using the rich content attachment: \(attachmentError)")
+						callback?(nil)
+					}
+				}
+				}.resume()
+		} else {
+			callback?(nil)
+		}
+	}
+
+	override func serviceExtensionTimeWillExpire() {
+		// Called just before the extension will be terminated by the system.
+		// Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+		if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+			contentHandler(bestAttemptContent)
+		}
+	}
+
+}
