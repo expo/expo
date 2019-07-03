ae16010821eebf4165278c1fca3735d52ac7e3a6
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2019-07-03 13:45:33 +0200

    [expotools] Updating pbxproj when updating vendored modules

--- tools/expotools/src/commands/UpdateVendoredModule.ts
+++ tools/expotools/src/commands/UpdateVendoredModule.ts
@@ -2,6 +2,7 @@ import os from 'os';
 import path from 'path';
 import chalk from 'chalk';
 import fs from 'fs-extra';
+import xcode from 'xcode';
 import glob from 'glob-promise';
 import JsonFile from '@expo/json-file';
 import spawnAsync from '@expo/spawn-async';
@@ -14,6 +15,7 @@ interface ActionOptions {
   module: string;
   platform: 'ios' | 'android' | 'all';
   commit: string;
+  pbxproj: boolean;
 }
 
 interface VendoredModuleUpdateStep {
@@ -278,6 +280,84 @@ async function findAndroidFilesAsync(dir: string): Promise<string[]> {
   return await glob(pattern);
 }
 
+async function loadXcodeprojFileAsync(file: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    const pbxproj = xcode.project(file);
+    pbxproj.parse(err => err ? reject(err) : resolve(pbxproj));
+  });
+}
+
+function pbxGroupChild(file) {
+  const obj = Object.create(null);
+  obj.value = file.fileRef;
+  obj.comment = file.basename;
+  return obj;
+}
+
+function pbxGroupHasChildWithRef(group: any, ref: string): boolean {
+  return group.children.some(child => child.value === ref);
+}
+
+async function addFileToPbxprojAsync(filePath: string, targetDir: string, pbxproj: any): Promise<void> {
+  const fileName = path.basename(filePath);
+
+  // The parent group of the target directory that should already be created in the project, e.g. `Components` or `Api`.
+  const targetGroup = pbxproj.pbxGroupByName(path.basename(path.dirname(targetDir)));
+
+  if (!pbxproj.hasFile(fileName)) {
+    console.log(`Adding ${chalk.magenta(fileName)} to pbxproj configuration ...`);
+
+    const fileOptions = {
+      // Mute warnings from 3rd party modules.
+      compilerFlags: '-w',
+    };
+
+    // The group name is mostly just a basename of the path.
+    const groupName = path.basename(path.dirname(filePath));
+
+    // Add a file to pbxproj tree.
+    const file = path.extname(fileName) === '.h'
+      ? pbxproj.addHeaderFile(fileName, fileOptions, groupName)
+      : pbxproj.addSourceFile(fileName, fileOptions, groupName);
+
+    // Search for the group where the file should be placed.
+    const group = pbxproj.pbxGroupByName(groupName);
+
+    // Our files has `includeInIndex` set to 1, so let's continue doing that.
+    file.includeInIndex = 1;
+
+    if (group) {
+      // Add a file if it is not there already.
+      if (!pbxGroupHasChildWithRef(group, file.fileRef)) {
+        group.children.push(pbxGroupChild(file));
+      }
+    } else {
+      // Create a pbx group with this file.
+      const { uuid } = pbxproj.addPbxGroup([file.path], groupName, groupName);
+
+      // Add newly created group to the parent group.
+      if (!pbxGroupHasChildWithRef(targetGroup, uuid)) {
+        targetGroup.children.push(pbxGroupChild({ fileRef: uuid, basename: groupName }));
+      }
+    }
+  }
+}
+
+async function copyFilesAsync(files: string[], sourceDir: string, targetDir: string): Promise<void> {
+  for (const file of files) {
+    const fileRelativePath = path.relative(sourceDir, file);
+    const fileTargetPath = path.join(targetDir, fileRelativePath);
+
+    await fs.mkdirs(path.dirname(fileTargetPath));
+    await fs.copy(file, fileTargetPath);
+
+    console.log(
+      chalk.yellow('>'),
+      chalk.magenta(path.relative(targetDir, fileTargetPath)),
+    );
+  }
+}
+
 async function action(options: ActionOptions) {
   if (options.list) {
     const bundledNativeModules = await getBundledNativeModulesAsync();
@@ -344,13 +424,23 @@ async function action(options: ActionOptions) {
       console.log('Copying iOS files ...');
 
       const objcFiles = await findObjcFilesAsync(sourceDir, step.recursive);
+      const pbxprojPath = path.join(IOS_DIR, 'Exponent.xcodeproj', 'project.pbxproj');
+      const pbxproj = await loadXcodeprojFileAsync(pbxprojPath);
 
-      for (const objcFile of objcFiles) {
-        const objcFileRelativePath = path.relative(sourceDir, objcFile);
-        const objcFileTargetPath = path.join(targetDir, objcFileRelativePath);
-        
-        await fs.mkdirs(path.dirname(objcFileTargetPath));
-        await fs.copy(objcFile, objcFileTargetPath);
+      await copyFilesAsync(objcFiles, sourceDir, targetDir);
+
+      if (options.pbxproj) {
+        console.log(`\nUpdating pbxproj configuration ...`);
+
+        for (const file of objcFiles) {
+          const fileRelativePath = path.relative(sourceDir, file);
+          const fileTargetPath = path.join(targetDir, fileRelativePath);
+
+          await addFileToPbxprojAsync(fileTargetPath, targetDir, pbxproj);
+        }
+
+        console.log(`Saving updated pbxproj structure to the file ${chalk.magenta(path.relative(IOS_DIR, pbxprojPath))} ...`);
+        await fs.writeFile(pbxprojPath, pbxproj.writeSync());
       }
 
       if (step.iosPrefix) {
@@ -382,13 +472,7 @@ async function action(options: ActionOptions) {
 
       const javaFiles = await findAndroidFilesAsync(sourceDir);
 
-      for (const javaFile of javaFiles) {
-        const javaFileRelativePath = path.relative(sourceDir, javaFile);
-        const javaFileTargetPath = path.join(targetDir, javaFileRelativePath);
-        
-        await fs.mkdirs(path.dirname(javaFileTargetPath));
-        await fs.copy(javaFile, javaFileTargetPath);
-      }
+      await copyFilesAsync(javaFiles, sourceDir, targetDir, () => {});
 
       const files = await findAndroidFilesAsync(targetDir);
       
@@ -425,5 +509,6 @@ export default (program: Command) => {
     .option('-m, --module <string>', 'Name of the module to update.')
     .option('-p, --platform <string>', 'A platform on which the vendored module will be updated.', 'all')
     .option('-c, --commit <string>', 'Git reference on which to checkout when copying 3rd party module.', 'master')
+    .option('--no-pbxproj', 'Whether to skip updating project.pbxproj file.', false)
     .asyncAction(action);
 };
