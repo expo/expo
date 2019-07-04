cc6d1278b20054b81d3fc9aeb307a8906d59e3c1
James Ide <ide@expo.io>
2019-07-03 18:14:58 -0700

    [home] Convert `getViewerUsernameAsync` to TS, avoid server call if there's no session
    If we don't have a session secret, then the viewer is obviously logged out and we can fast-path out and return null. Also converted this file to TS.

--- home/utils/getViewerUsernameAsync.ts
+++ home/utils/getViewerUsernameAsync.ts
@@ -1,8 +1,14 @@
 import gql from 'graphql-tag';
 
 import ApolloClient from '../api/ApolloClient';
+import Store from '../redux/Store';
 
-export default async function getViewerUsernameAsync() {
+export default async function getViewerUsernameAsync(): Promise<string | null> {
+  const { sessionSecret } = Store.getState().session;
+  if (!sessionSecret) {
+    return null;
+  }
+
   const result = await ApolloClient.query({
     query: gql`
       query Home_ViewerUsername {
