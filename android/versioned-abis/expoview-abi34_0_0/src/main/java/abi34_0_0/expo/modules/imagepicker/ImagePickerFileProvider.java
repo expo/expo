1f83aa5987e8d8ee213452113b05d372b8390267
Bartłomiej Bukowski <bartlomiejbukowski.b@gmail.com>
2019-07-04 15:14:19 +0200

    [android] Generate versioned-abi for SDK 34

---
+++ android/versioned-abis/expoview-abi34_0_0/src/main/java/abi34_0_0/expo/modules/imagepicker/ImagePickerFileProvider.java
@@ -0,0 +1,8 @@
+package abi34_0_0.expo.modules.imagepicker;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Dummy class for proving files for this module.
+ */
+public class ImagePickerFileProvider extends FileProvider {}
