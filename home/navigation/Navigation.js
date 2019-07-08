e0cc53f329ecc349eaacd00e6d77b69a8bda0445
James Ide <ide@expo.io>
2019-07-08 13:12:56 -0400

    [home] Revert profile banner to unblock release
    We needed to roll back Home during the last release because of FaceDetector issues. We don't really need this module since instead of using Camera and MediaLibrary directly, we can use ImagePicker (which also needs CAMERA and CAMERA_ROLL permissions anyway).
    
    This reverts commit c0a0d3372589306092e8d3a4165aef5d3cba9ffb.

--- home/navigation/Navigation.js
+++ home/navigation/Navigation.js
@@ -17,8 +17,6 @@ import GeofencingScreen from '../screens/GeofencingScreen';
 import LocationDiagnosticsScreen from '../screens/LocationDiagnosticsScreen';
 import ExploreScreen from '../screens/ExploreScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import CameraScreen from '../screens/CameraScreen';
-import MediaLibraryScreen from '../screens/MediaLibrary/MediaLibraryScreen';
 import SearchScreen from '../screens/SearchScreen';
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
@@ -107,16 +105,13 @@ const ExploreStack = createStackNavigator(
 const ProfileStack = createStackNavigator(
   {
     Profile: ProfileScreen,
-    Camera: CameraScreen,
     UserSettings: UserSettingsScreen,
     ProjectsForUser: ProjectsForUserScreen,
     SnacksForUser: SnacksForUserScreen,
-    MediaLibrary: MediaLibraryScreen,
   },
   {
     initialRouteName: 'Profile',
     defaultNavigationOptions,
-    headerMode: 'screen',
     navigationOptions: {
       tabBarIcon: ({ focused }) => renderIcon(Ionicons, 'ios-person', 26, focused),
       tabBarLabel: 'Profile',
