fc25f814958dcbb415a366c1ee188e3b7e45d016
Evan Bacon <baconbrix@gmail.com>
2019-07-08 16:34:38 -0700

    [web] Fixed react-native-svg in storybook

--- apps/storybook/stories/Components/SVG.stories.js
+++ apps/storybook/stories/Components/SVG.stories.js
@@ -1,5 +1,5 @@
 import { color, number } from '@storybook/addon-knobs/react';
-import Svg from 'expo/build/Svg';
+import Svg, { Circle, Rect } from 'react-native-svg';
 import React from 'react';
 
 import { Section } from '../ui-explorer';
@@ -11,7 +11,7 @@ export const packageJson = require('expo/package.json');
 export const component = () => (
   <Section>
     <Svg height={100} width={100}>
-      <Svg.Circle
+      <Circle
         cx={number('circle cx', 50)}
         cy={number('circle cy', 50)}
         r={number('circle r', 45)}
@@ -19,7 +19,7 @@ export const component = () => (
         stroke={color('circle stroke', '#e74c3c')}
         fill={color('circle fill', '#f1c40f')}
       />
-      <Svg.Rect
+      <Rect
         x={number('rect x', 15)}
         y={number('rect y', 15)}
         width={number('rect width', 70)}
