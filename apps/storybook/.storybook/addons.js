9fb83cb9fb896c20449615069cb43725f74cd55a
Evan Bacon <baconbrix@gmail.com>
2019-07-08 20:11:51 -0700

    Changed testing order

--- apps/storybook/.storybook/addons.js
+++ apps/storybook/.storybook/addons.js
@@ -5,7 +5,4 @@ import '@storybook/addon-notes/register';
 
 // The order will dictate how they are listed in the tab bar
 import '@storybook/addon-knobs/register';
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-events/register';
-import '@storybook/addon-actions/register';
 // import '@storybook/addon-backgrounds/register';
