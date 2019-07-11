0d6bca7815d388ffa0b8d9ad2f763ecebd3c449d
Robert L <1998.robert@gmail.com>
2019-07-11 11:25:05 -0700

    Add isRootedExperimentalAsync() method
    This is pretty experimental, needs to be tested. Referenced from https://github.com/thii/DTTJailbreakDetection/blob/master/Classes/DTTJailbreakDetection.m

--- packages/expo-device/ios/EXDevice/EXDevice.m
+++ packages/expo-device/ios/EXDevice/EXDevice.m
@@ -140,6 +140,74 @@ UM_EXPORT_METHOD_AS(getUptimeAsync, getUptimeAsyncWithResolver:(UMPromiseResolve
   resolve([self systemUptime]);
 }
 
+UM_EXPORT_METHOD_AS(isRootedExperimentalAsync, isRootedExperimentalAsyncWithResolver:(UMPromiseResolveBlock)resolve rejecter:(UMPromiseRejectBlock)reject)
+{
+#if !(TARGET_IPHONE_SIMULATOR)
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  if ([fileManager fileExistsAtPath:@"/Applications/Cydia.app"]) {
+    return YES;
+  } else if ([fileManager fileExistsAtPath:@"/Library/MobileSubstrate/MobileSubstrate.dylib"]) {
+    return YES;
+  } else if ([fileManager fileExistsAtPath:@"/bin/bash"]) {
+    return YES;
+  } else if ([fileManager fileExistsAtPath:@"/usr/sbin/sshd"]) {
+    return YES;
+  } else if ([fileManager fileExistsAtPath:@"/etc/apt"]) {
+    return YES;
+  } else if ([fileManager fileExistsAtPath:@"/usr/bin/ssh"]) {
+    return YES;
+  } else if ([fileManager fileExistsAtPath:@"/private/var/lib/apt/"]) {
+    return YES;
+  }
+  
+  FILE *file = fopen("/Applications/Cydia.app", "r");
+  if (file) {
+    fclose(file);
+    return YES;
+  }
+  file = fopen("/Library/MobileSubstrate/MobileSubstrate.dylib", "r");
+  if (file) {
+    fclose(file);
+    return YES;
+  }
+  file = fopen("/bin/bash", "r");
+  if (file) {
+    fclose(file);
+    return YES;
+  }
+  file = fopen("/usr/sbin/sshd", "r");
+  if (file) {
+    fclose(file);
+    return YES;
+  }
+  file = fopen("/etc/apt", "r");
+  if (file) {
+    fclose(file);
+    return YES;
+  }
+  file = fopen("/usr/bin/ssh", "r");
+  if (file) {
+    fclose(file);
+    return YES;
+  }
+  
+  // Check if the app can access outside of its sandbox
+  NSError *error = nil;
+  NSString *string = @".";
+  [string writeToFile:@"/private/jailbreak.txt" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+  if (!error) {
+    [fileManager removeItemAtPath:@"/private/jailbreak.txt" error:nil];
+    return YES;
+  }
+  
+  // Check if the app can open a Cydia's URL scheme
+  if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"cydia://package/com.example.package"]]) {
+    return YES;
+  }
+#endif
+  return NO;
+}
+
 - (NSNumber *)systemUptime
 {
   return [NSNumber numberWithDouble:[NSProcessInfo processInfo].systemUptime];
