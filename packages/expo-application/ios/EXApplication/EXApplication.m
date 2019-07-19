6892ad040aa80e8e2fa7e8b32628561c9d32e5e5
Vivian Zhu <tongyu@usc.edu>
2019-07-19 11:30:00 -0700

    [ios] cleanup inline methods and handle error with NSError

--- packages/expo-application/ios/EXApplication/EXApplication.m
+++ packages/expo-application/ios/EXApplication/EXApplication.m
@@ -17,46 +17,23 @@ UM_EXPORT_MODULE(ExpoApplication);
   return dispatch_get_main_queue();
 }
 
-- (NSString *)appleIFV {
-  if(NSClassFromString(@"UIDevice") && [UIDevice instancesRespondToSelector:@selector(identifierForVendor)]) {
-    return [[UIDevice currentDevice].identifierForVendor UUIDString];
-  }
-  return nil;
-}
-
-- (NSString *) applicationName
-{
-  return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"] ?: [NSNull null];
-}
-
-- (NSString *) bundleId
-{
-  return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"] ?: [NSNull null];
-}
-
-- (NSString *) appVersion
-{
-  return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"] ?: [NSNull null];
-}
-
-- (NSString *) buildVersion
-{
-  return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]?: [NSNull null];
-}
-
-
 UM_EXPORT_METHOD_AS(getIosIdForVendorAsync, getIosIdForVendorAsyncWithResolver:(UMPromiseResolveBlock)resolve rejecter:(UMPromiseRejectBlock)reject)
 {
-  NSString* identifierForVendor = [self appleIFV];
-  resolve(identifierForVendor);
+  if(NSClassFromString(@"UIDevice") && [UIDevice instancesRespondToSelector:@selector(identifierForVendor)]) {
+    resolve([[UIDevice currentDevice].identifierForVendor UUIDString]);
+  }
+  resolve([NSNull null]);
 }
 
 UM_EXPORT_METHOD_AS(getFirstInstallTimeAsync, getFirstInstallTimeAsyncWithResolver:(UMPromiseResolveBlock)resolve rejecter:(UMPromiseRejectBlock)reject)
 {
   NSURL* urlToDocumentsFolder = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-  __autoreleasing NSError *error;
-  NSDate *installDate = [[[NSFileManager defaultManager] attributesOfItemAtPath:urlToDocumentsFolder.path error:&error] objectForKey:NSFileCreationDate];
-  NSTimeInterval timeInMiliseconds = [installDate timeIntervalSince1970]*1000;
+  NSError *error = nil;
+  NSDate *firstInstallDate = [[[NSFileManager defaultManager] attributesOfItemAtPath:urlToDocumentsFolder.path error:&error] objectForKey:NSFileCreationDate];
+  if(error){
+    reject(@"ERR_APPLICATION", @"Unable to get first install time of this application.", error);
+  }
+  NSTimeInterval timeInMiliseconds = [firstInstallDate timeIntervalSince1970] * 1000;
   NSNumber* timeInterval = @(timeInMiliseconds);
   resolve(timeInterval);
 }
@@ -64,10 +41,10 @@ UM_EXPORT_METHOD_AS(getFirstInstallTimeAsync, getFirstInstallTimeAsyncWithResolv
 - (NSDictionary *)constantsToExport
 {
   return @{
-           @"applicationName": [self applicationName],
-           @"bundleId": [self bundleId],
-           @"nativeAppVersion": [self appVersion],
-           @"nativeBuildVersion": [self buildVersion],
+           @"applicationName": [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"] ?: [NSNull null],
+           @"bundleId": [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"] ?: [NSNull null],
+           @"nativeApplicationVersion": [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"] ?: [NSNull null],
+           @"nativeBuildVersion": [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]?: [NSNull null],
            };
 }
 
