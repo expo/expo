84051d8bb9127eb3847aef0dd646bc7d2f52a070
Evan Bacon <baconbrix@gmail.com>
2019-07-22 16:45:03 -0700

    [web] Removed all config in favor of an identical universal solution (#5012)
    * Removed all config in favor of an identical universal solution
    
    * Update index-test.js
    
    * Reverted yarn
    
    * Change import style
    
    * Update index.js
    
    * disableImportExportTransform: true

--- packages/babel-preset-expo/__tests__/index-test.js
+++ packages/babel-preset-expo/__tests__/index-test.js
@@ -3,72 +3,77 @@ const fs = require('fs');
 const path = require('path');
 
 const preset = require('..');
+// Used to distinguish webpack from metro
+const WEBPACK_CALLER = { name: 'babel-loader' };
 
-it(`compiles sample files`, () => {
-  let options = {
-    babelrc: false,
-    presets: [preset],
-    sourceMaps: true,
-  };
+describe.each([['metro'], ['webpack', WEBPACK_CALLER]])('%s', (_name, caller) => {
+  it(`compiles sample files`, () => {
+    let options = {
+      babelrc: false,
+      presets: [preset],
+      sourceMaps: true,
+      caller,
+    };
+    let samplesPath = path.resolve(__dirname, 'samples');
+    let filenames = fs.readdirSync(samplesPath);
 
-  let samplesPath = path.resolve(__dirname, 'samples');
-  let filenames = fs.readdirSync(samplesPath);
+    for (let filename of filenames) {
+      let { code, map, ast } = babel.transformFileSync(path.join(samplesPath, filename), options);
 
-  for (let filename of filenames) {
-    let { code, map, ast } = babel.transformFileSync(path.join(samplesPath, filename), options);
+      expect(code).toBeDefined();
+      expect(map).toBeDefined();
+      expect(ast).toBeDefined();
+    }
+  });
 
-    expect(code).toBeDefined();
-    expect(map).toBeDefined();
-    expect(ast).toBeDefined();
-  }
-});
+  it(`aliases @expo/vector-icons`, () => {
+    let options = {
+      babelrc: false,
+      presets: [preset],
+      filename: 'unknown',
+      // Make the snapshot easier to read
+      retainLines: true,
+      caller,
+    };
 
-it(`aliases @expo/vector-icons`, () => {
-  let options = {
-    babelrc: false,
-    presets: [preset],
-    filename: 'unknown',
-    // Make the snapshot easier to read
-    retainLines: true,
-  };
-
-  let sourceCode = `
+    let sourceCode = `
 import 'react-native-vector-icons';
 require('react-native-vector-icons');
-
 imposter.require('react-native-vector-icons');
 imposter.import('react-native-vector-icons');
 `;
-  let { code } = babel.transform(sourceCode, options);
+    let { code } = babel.transform(sourceCode, options);
 
-  expect(code).toMatch(/"@expo\/vector-icons"/);
-  expect(code).toMatchSnapshot();
-});
+    expect(code).toMatch(/"@expo\/vector-icons"/);
+    expect(code).toMatchSnapshot();
+  });
 
-it(`composes with babel-plugin-module-resolver`, () => {
-  let options = {
-    babelrc: false,
-    presets: [preset],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: { rn: 'react-native' },
-        },
+  it(`composes with babel-plugin-module-resolver`, () => {
+    let options = {
+      babelrc: false,
+      presets: [preset],
+      plugins: [
+        [
+          'module-resolver',
+          {
+            alias: { rn: 'react-native' },
+          },
+        ],
       ],
-    ],
-    filename: 'unknown',
-    // Make the snapshot easier to read
-    retainLines: true,
-  };
+      filename: 'unknown',
+      // Make the snapshot easier to read
+      retainLines: true,
+      caller,
+    };
 
-  let sourceCode = `
+    let sourceCode = `
 import 'rn';
 import 'react-native-vector-icons';
 `;
-  let { code } = babel.transform(sourceCode, options);
+    let { code } = babel.transform(sourceCode, options);
 
-  expect(code).toMatch(/"react-native"/);
-  expect(code).toMatch(/"@expo\/vector-icons"/);
-  expect(code).toMatchSnapshot();
+    expect(code).toMatch(/"react-native"/);
+    expect(code).toMatch(/"@expo\/vector-icons"/);
+    expect(code).toMatchSnapshot();
+  });
 });
