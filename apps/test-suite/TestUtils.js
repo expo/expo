a96bef021df51e6b1feec8238cebef352c92e5f9
Evan Bacon <baconbrix@gmail.com>
2019-07-24 01:33:48 -0700

    Added speech tests (#4966)
    * Added speech tests
    
    * Remove speech tests in CI
    
    * update format
    
    * Update index.js
    
    * Added new logs
    
    * Update TestUtils.js
    
    * Update index.js
    
    * Update Localization.js
    
    * Update apps/test-suite/tests/Localization.js
    
    Co-Authored-By: James Ide <ide@users.noreply.github.com>
    
    * Update packages/expo-speech/src/Speech/Speech.types.ts
    
    Co-Authored-By: James Ide <ide@users.noreply.github.com>
    
    * Update Speech.types.ts
    
    * updated build

--- apps/test-suite/TestUtils.js
+++ apps/test-suite/TestUtils.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import { Platform, NativeModules } from 'react-native';
+import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 
-const { ExponentTest } = NativeModules;
+import ExponentTest from './ExponentTest';
 
 function browserSupportsWebGL() {
   try {
@@ -22,17 +22,22 @@ function browserSupportsWebGL() {
 export function getTestModules() {
   if (Platform.OS === 'web') {
     const modules = [
-      require('./tests/SVG'),
       require('./tests/Basic'),
-      require('./tests/Contacts'),
       require('./tests/Constants'),
+      require('./tests/SVG'),
+      require('./tests/Contacts'),
       require('./tests/Crypto'),
       require('./tests/Font'),
       require('./tests/Random'),
+      require('./tests/Localization'),
     ];
 
     if (browserSupportsWebGL()) {
       modules.push(require('./tests/GLView'));
+    }
+
+    if (ExponentTest && !ExponentTest.isInCI) {
+      // modules.push(require('./tests/Speech'));
     }
     return modules;
   }
