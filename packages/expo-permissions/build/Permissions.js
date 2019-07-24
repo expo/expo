f3874050c5b5b41571d4fe8f3a11554743867661
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-07-24 16:22:54 +0200

    [Permissions] Android fix

--- packages/expo-permissions/build/Permissions.js
+++ packages/expo-permissions/build/Permissions.js
@@ -1,5 +1,6 @@
 import { coalesceExpirations, coalesceStatuses } from './CoalescedPermissions';
 import Permissions from './ExpoPermissions';
+import { Platform } from 'react-native';
 import { PermissionStatus, } from './Permissions.types';
 export { PermissionStatus, };
 export const CAMERA = 'camera';
@@ -13,20 +14,26 @@ export const CALENDAR = 'calendar';
 export const REMINDERS = 'reminders';
 export const SYSTEM_BRIGHTNESS = 'systemBrightness';
 export async function getAsync(...types) {
-    return await _handleMultiPermissionsRequestAsync(types, Permissions.getAsync);
+    if (Platform.OS === "ios") {
+        return await _handleMultiPermissionsRequestIOSAsync(types, Permissions.getAsync);
+    }
+    return await _handlePermissionsRequestAsync(types, Permissions.getAsync);
 }
 export async function askAsync(...types) {
-    return await _handleMultiPermissionsRequestAsync(types, Permissions.askAsync);
+    if (Platform.OS === "ios") {
+        return await _handleMultiPermissionsRequestIOSAsync(types, Permissions.askAsync);
+    }
+    return await _handlePermissionsRequestAsync(types, Permissions.askAsync);
 }
-async function _handleSinglePermissionRequestAsync(type, handlePermission) {
+async function _handleSinglePermissionRequestIOSAsync(type, handlePermission) {
     return handlePermission(type);
 }
-async function _handleMultiPermissionsRequestAsync(types, handlePermission) {
+async function _handleMultiPermissionsRequestIOSAsync(types, handlePermission) {
     if (!types.length) {
         throw new Error('At least one permission type must be specified');
     }
     return await Promise.all(types.map(async (type) => ({
-        [type]: await _handleSinglePermissionRequestAsync(type, handlePermission),
+        [type]: await _handleSinglePermissionRequestIOSAsync(type, handlePermission),
     })))
         .then(permissions => permissions.reduce((permission, acc) => ({ ...acc, ...permission }), {}))
         .then(permissions => ({
@@ -34,5 +41,16 @@ async function _handleMultiPermissionsRequestAsync(types, handlePermission) {
         expires: coalesceExpirations(permissions),
         permissions: { ...permissions },
     }));
+}
+async function _handlePermissionsRequestAsync(types, handlePermissions) {
+    if (!types.length) {
+        throw new Error('At least one permission type must be specified');
+    }
+    const permissions = await handlePermissions(types);
+    return {
+        status: coalesceStatuses(permissions),
+        expires: coalesceExpirations(permissions),
+        permissions,
+    };
 }
 //# sourceMappingURL=Permissions.js.map
