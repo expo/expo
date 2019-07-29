b66959d66f9945576a32b68a2c6d87a306d78409
Yifei He <yifei@hesyifei.com>
2019-07-29 10:57:11 -0700

    [web][push] Update Expo/device token when `pushsubscriptionchange`

--- packages/expo/build/Notifications/ExponentNotifications.fx.web.js
+++ packages/expo/build/Notifications/ExponentNotifications.fx.web.js
@@ -1,7 +1,12 @@
 import { emitNotification } from './Notifications';
+import { getExponentPushTokenAsync } from './ExponentNotificationsHelper.web';
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.addEventListener('message', event => {
         emitNotification(event.data);
+    });
+    navigator.serviceWorker.addEventListener('pushsubscriptionchange', async (event) => {
+        // Updates Expo token with new device token.
+        await getExponentPushTokenAsync();
     });
 }
 //# sourceMappingURL=ExponentNotifications.fx.web.js.map
--- packages/expo/src/Notifications/ExponentNotifications.fx.web.ts
+++ packages/expo/src/Notifications/ExponentNotifications.fx.web.ts
@@ -1,7 +1,13 @@
 import { emitNotification } from './Notifications';
+import { getExponentPushTokenAsync } from './ExponentNotificationsHelper.web';
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.addEventListener('message', event => {
     emitNotification(event.data);
+  });
+
+  navigator.serviceWorker.addEventListener('pushsubscriptionchange', async event => {
+    // Updates Expo token with new device token.
+    await getExponentPushTokenAsync();
   });
 }
