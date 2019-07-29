c62c2bc40f82ac283ac23c00d3cc8257c6bf24ad
Robert L <1998.robert@gmail.com>
2019-07-29 12:23:43 -0700

    [ios] implement constants

--- packages/expo-cellular/ios/EXCellular/EXCellularModule.m
+++ packages/expo-cellular/ios/EXCellular/EXCellularModule.m
@@ -2,6 +2,10 @@
 
 #import <EXCellular/EXCellularModule.h>
 
+#import <CoreTelephony/CTCarrier.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+
+
 @interface EXCellularModule ()
 
 @property (nonatomic, weak) UMModuleRegistry *moduleRegistry;
@@ -17,13 +21,51 @@ UM_EXPORT_MODULE(ExpoCellular);
   _moduleRegistry = moduleRegistry;
 }
 
-UM_EXPORT_METHOD_AS(someGreatMethodAsync,
-                    options:(NSDictionary *)options
-                    resolve:(UMPromiseResolveBlock)resolve
-                    reject:(UMPromiseRejectBlock)reject)
+- (dispatch_queue_t)methodQueue
 {
+  return dispatch_get_main_queue();
 }
 
+UM_EXPORT_METHOD_AS(getCellularGenerationAsync, getCellularGenerationAsyncWithResolver:(UMPromiseResolveBlock)resolve rejecter:(UMPromiseRejectBlock)reject)
+{
+  resolve(@"generation here");
+}
 
+- (NSDictionary *)constantsToExport
+{
+  CTCarrier *carrier = [self carrier];
+  
+  return @{
+           @"allowsVoip": @(carrier.allowsVOIP),
+           @"carrier": carrier.carrierName,
+           @"isoCountryCode": carrier.isoCountryCode,
+           @"mobileCountryCode": carrier.mobileCountryCode,
+           @"mobileNetworkCode": carrier.mobileNetworkCode,
+           };
+}
+
+- (CTCarrier *)carrier
+{
+  CTTelephonyNetworkInfo *info = [[CTTelephonyNetworkInfo alloc] init];
+  NSDictionary<NSString *, CTCarrier *> *serviceSubscriberCellularProviders;
+  CTCarrier *carrier;
+  
+  if (@available(iOS 12.0, *)) {
+    serviceSubscriberCellularProviders = info.serviceSubscriberCellularProviders;
+    carrier = serviceSubscriberCellularProviders.allValues.firstObject;
+  } else {
+    // Fallback on earlier versions
+    /*
+     @property (nonatomic, readonly, retain, nullable) NSString *carrierName;
+     @property (nonatomic, readonly, retain, nullable) NSString *mobileCountryCode;
+     @property (nonatomic, readonly, retain, nullable) NSString *mobileNetworkCode;
+     @property (nonatomic, readonly, retain, nullable) NSString* isoCountryCode;
+     @property (nonatomic, readonly, assign) BOOL allowsVOIP;
+     */
+    carrier = info.subscriberCellularProvider;
+  }
+  
+  return carrier;
+}
 
 @end
