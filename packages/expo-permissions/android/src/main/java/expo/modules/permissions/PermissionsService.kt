167213140bba73f00b5c235dfbd60667c70d8abb
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-07-30 15:30:26 +0200

    [Permissions] Remove permissions manager

--- packages/expo-permissions/android/src/main/java/expo/modules/permissions/PermissionsService.kt
+++ packages/expo-permissions/android/src/main/java/expo/modules/permissions/PermissionsService.kt
@@ -3,24 +3,26 @@ package expo.modules.permissions
 
 import android.content.Context
 import android.content.pm.PackageManager
+import com.facebook.react.modules.core.PermissionAwareActivity
+
 import org.unimodules.core.ModuleRegistry
+import org.unimodules.core.interfaces.ActivityProvider
 import org.unimodules.core.interfaces.InternalModule
 import org.unimodules.interfaces.permissions.Permissions
-import org.unimodules.interfaces.permissions.PermissionsManager
 
 private const val PERMISSIONS_REQUEST: Int = 13
 
 open class PermissionsService(context: Context): InternalModule, Permissions {
-  private var mPermissionsManager: PermissionsManager? = null
-  private var mPermissions :Permissions? = null
+  private var mPermissions: Permissions? = null
   protected val mContext: Context = context
+  private var mActivityProvider: ActivityProvider? = null
 
   override fun getExportedInterfaces(): List<Class<out Any>>
       = listOf<Class<out Any>>(Permissions::class.java)
 
   override fun onCreate(moduleRegistry: ModuleRegistry) {
-    mPermissionsManager = moduleRegistry.getModule(PermissionsManager::class.java)
     mPermissions = moduleRegistry.getModule(Permissions::class.java)
+    mActivityProvider = moduleRegistry.getModule(ActivityProvider::class.java)
   }
 
   override fun getPermissions(permissions: Array<String>): IntArray
@@ -30,14 +32,24 @@ open class PermissionsService(context: Context): InternalModule, Permissions {
     = mPermissions?.getPermission(permission) ?: PackageManager.PERMISSION_DENIED
 
   override fun askForPermissions(permissions: Array<String>, listener: Permissions.PermissionsRequestListener) {
-    val askedPermissions = mPermissionsManager?.requestPermissions(
-      permissions,
-      PERMISSIONS_REQUEST
-    ) { _, grantResults -> listener.onPermissionsResult(grantResults) }
+    val currentActivity = mActivityProvider?.currentActivity
 
-    if (askedPermissions == null || !askedPermissions) {
-      val results = IntArray(permissions.size) { PackageManager.PERMISSION_DENIED }
-      listener.onPermissionsResult(results)
+    if (currentActivity != null && currentActivity is PermissionAwareActivity) {
+      val activity = currentActivity as PermissionAwareActivity
+      activity.requestPermissions(permissions, PERMISSIONS_REQUEST) { requestCode, permissions, grantResults ->
+        when (PERMISSIONS_REQUEST) {
+          requestCode -> {
+            listener.onPermissionsResult(grantResults)
+            true
+          }
+          else -> {
+            listener.onPermissionsResult(IntArray(permissions.size) { PackageManager.PERMISSION_DENIED })
+            false
+          }
+        }
+      }
+    } else {
+      listener.onPermissionsResult(IntArray(permissions.size) { PackageManager.PERMISSION_DENIED })
     }
   }
 
