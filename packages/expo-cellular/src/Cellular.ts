ca82cd2e990f7c42c2194af12f16b4042d4b1896
Robert L <1998.robert@gmail.com>
2019-07-30 11:17:45 -0700

    [ts/js] get rid of double ternary
    gave us some bugs in which `false` -> null for Cellular.allowsVoip

--- packages/expo-cellular/src/Cellular.ts
+++ packages/expo-cellular/src/Cellular.ts
@@ -3,27 +3,11 @@ import { UnavailabilityError } from '@unimodules/core';
 import ExpoCellular from './ExpoCellular';
 import { CellularGeneration } from './Cellular.types';
 
-export const allowsVoip = ExpoCellular
-  ? ExpoCellular.allowsVoip
-    ? ExpoCellular.allowsVoip
-    : null
-  : null;
-export const carrier = ExpoCellular ? (ExpoCellular.carrier ? ExpoCellular.carrier : null) : null;
-export const isoCountryCode = ExpoCellular
-  ? ExpoCellular.isoCountryCode
-    ? ExpoCellular.isoCountryCode
-    : null
-  : null;
-export const mobileCountryCode = ExpoCellular
-  ? ExpoCellular.mobileCountryCode
-    ? ExpoCellular.mobileCountryCode
-    : null
-  : null;
-export const mobileNetworkCode = ExpoCellular
-  ? ExpoCellular.mobileNetworkCode
-    ? ExpoCellular.mobileNetworkCode
-    : null
-  : null;
+export const allowsVoip = ExpoCellular ? ExpoCellular.allowsVoip : null;
+export const carrier = ExpoCellular ? ExpoCellular.carrier : null;
+export const isoCountryCode = ExpoCellular ? ExpoCellular.isoCountryCode : null;
+export const mobileCountryCode = ExpoCellular ? ExpoCellular.mobileCountryCode : null;
+export const mobileNetworkCode = ExpoCellular ? ExpoCellular.mobileNetworkCode : null;
 
 export async function getCellularGenerationAsync(): Promise<CellularGeneration> {
   if (!ExpoCellular.getCellularGenerationAsync) {
