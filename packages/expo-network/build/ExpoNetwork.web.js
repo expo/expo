7e232dc0156c8e98404c0b4a104d2c3a90b5d5fa
Vivian Zhu <tongyu@usc.edu>
2019-08-05 12:23:28 -0700

    [web] simplify promise resolve/reject

--- packages/expo-network/build/ExpoNetwork.web.js
+++ packages/expo-network/build/ExpoNetwork.web.js
@@ -1,33 +1,27 @@
 import { NetworkStateType } from './Network.types';
 export default {
     async getIpAddressAsync() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const resp = await fetch('https://api.ipify.org?format=json');
-                const data = await resp.json();
-                resolve(data.ip);
-            }
-            catch (e) {
-                reject(e);
-            }
-        });
+        try {
+            const resp = await fetch('https://api.ipify.org?format=json');
+            const data = await resp.json();
+            return data.ip;
+        }
+        catch (e) {
+            throw e;
+        }
     },
     async getNetworkStateAsync() {
         let type = navigator.onLine ? NetworkStateType.UNKNOWN : NetworkStateType.NONE;
         let isConnected = navigator.onLine;
         let isInternetReachable = isConnected;
-        return new Promise((resolve) => {
-            resolve({
-                type,
-                isConnected,
-                isInternetReachable,
-            });
-        });
+        return {
+            type,
+            isConnected,
+            isInternetReachable,
+        };
     },
     async getMacAddressAsync() {
-        return new Promise((resolve) => {
-            resolve(null);
-        });
-    }
+        return null;
+    },
 };
 //# sourceMappingURL=ExpoNetwork.web.js.map
--- packages/expo-network/src/ExpoNetwork.web.ts
+++ packages/expo-network/src/ExpoNetwork.web.ts
@@ -2,32 +2,25 @@ import { NetworkState, NetworkStateType } from './Network.types';
 
 export default {
   async getIpAddressAsync(): Promise<string> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const resp = await fetch('https://api.ipify.org?format=json');
-        const data = await resp.json();
-        resolve(data.ip);
-      } catch (e) {
-        reject(e);
-      }
+    try {
+      const resp = await fetch('https://api.ipify.org?format=json');
+      const data = await resp.json();
+      return data.ip;
+    } catch (e) {
+      throw e;
     }
-    );
   },
   async getNetworkStateAsync(): Promise<NetworkState> {
     let type = navigator.onLine ? NetworkStateType.UNKNOWN : NetworkStateType.NONE;
     let isConnected = navigator.onLine;
     let isInternetReachable = isConnected;
-    return new Promise((resolve) => {
-      resolve({
-        type,
-        isConnected,
-        isInternetReachable,
-      });
-    });
+    return {
+      type,
+      isConnected,
+      isInternetReachable,
+    };
   },
   async getMacAddressAsync(): Promise<null> {
-    return new Promise((resolve) => {
-      resolve(null);
-    });
-  }
+    return null;
+  },
 };
