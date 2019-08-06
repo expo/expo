df7800b61aa13b49c381c46c78425768c3202837
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2019-08-06 13:56:38 +0200

    [file-system] Publish 6.0.2
    expo-file-system@6.0.2

--- ios/Pods/Target Support Files/EXGoogleSignIn/EXGoogleSignIn-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EXGoogleSignIn : NSObject
-@end
-@implementation PodsDummy_EXGoogleSignIn
-@end
--- ios/Pods/Target Support Files/EXHaptics/EXHaptics-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EXHaptics : NSObject
-@end
-@implementation PodsDummy_EXHaptics
-@end
