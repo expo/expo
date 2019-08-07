da8f3ae243de54f13ee6d33cc41d4a2941e80554
Robert L <1998.robert@gmail.com>
2019-08-07 16:29:18 -0700

    update tests
    updated tests with accordance to PR feedback

--- apps/test-suite/tests/Battery.js
+++ apps/test-suite/tests/Battery.js
@@ -1,104 +1,184 @@
-/* eslint-disable prettier/prettier */
 import * as Battery from 'expo-battery';
 
 export const name = 'Battery';
 
 export async function test({ describe, it, expect, jasmine }) {
-    describe(name, () => {
-        describe('Test Getters', () => {
-            it('Gets current battery level', async () => {
-                let batteryLevel;
-                let wasRejected = false;
-                try {
-                    batteryLevel = await Battery.getBatteryLevelAsync();
-                } catch (error) {
-                    wasRejected = true;
-                }
-                expect(batteryLevel).toBeLessThanOrEqual(1);
-                expect(batteryLevel).toBeGreaterThan(0);
-                expect(wasRejected).toBe(false);
-            });
-            it('Gets battery state', async () => {
-                let batteryState;
-                let wasRejected = false;
-                try {
-                    batteryState = await Battery.getBatteryStateAsync();
-                } catch (error) {
-                    wasRejected = true;
-                }
-                expect(batteryState).toBeDefined();
-                expect(batteryState).toEqual(jasmine.any(String));
-                expect(wasRejected).toBe(false);
-            });
-            it('Gets low power mode', async () => {
-                let lowPowerMode;
-                let wasRejected = false;
-                try {
-                    lowPowerMode = await Battery.getLowPowerModeStatusAsync();
-                } catch (error) {
-                    wasRejected = true;
-                }
-                expect(lowPowerMode).toBeDefined();
-                expect(lowPowerMode).toEqual(jasmine.any(String));
-                expect(wasRejected).toBe(false);
-            });
-            it('Gets power state', async () => {
-                let powerState;
-                let wasRejected = false;
-                try {
-                    powerState = await Battery.getPowerStateAsync();
-                } catch (error) {
-                    wasRejected = true;
-                }
-                expect(powerState).toBeDefined();
-                expect(wasRejected).toBe(false);
-            });
-        });
-
-        describe('Event listeners', () => {
-            let _subscriptionBatteryLevel, _subscriptionBatteryState, _subscriptionPowerMode;
-
-            //event listeners do register
-            it('Registers event listeners', () => {
-                let hasError = false;
-                try {
-                    _subscriptionPowerMode = Battery.watchPowerModeChange(({ lowPowerMode }) => {
-                        console.log('powerMode changed!', lowPowerMode);
-                    });
-                    _subscriptionBatteryState = Battery.watchBatteryStateChange(({ batteryState }) => {
-                        console.log('batteryState changed!', batteryState);
-                    });
-                    _subscriptionBatteryLevel = Battery.watchBatteryLevelChange(({ batteryLevel }) => {
-                        console.log('batteryLevel changed!', batteryLevel);
-                    });
-                } catch(e) {
-                    console.log(e);
-                    hasError = true;
-                }
-                expect(hasError).toBe(false);
-                expect(_subscriptionBatteryLevel).toBeDefined();
-                expect(_subscriptionBatteryState).toBeDefined();
-                expect(_subscriptionPowerMode).toBeDefined();
-            })
-
-            //Event listeners can unsubscribe
-            it('Remove all event listeners', async () => {
-                let hasError = false;
-                try {
-                    _subscriptionPowerMode && _subscriptionPowerMode.remove();
-                    _subscriptionPowerMode = null;
-                    _subscriptionBatteryState && _subscriptionBatteryState.remove();
-                    _subscriptionBatteryState = null;
-                    _subscriptionBatteryLevel && _subscriptionBatteryLevel.remove();
-                    _subscriptionBatteryLevel = null;
-                } catch (e) {
-                    hasError = true;
-                }
-                expect(_subscriptionBatteryLevel).toBeNull();
-                expect(_subscriptionBatteryState).toBeNull();
-                expect(_subscriptionPowerMode).toBeNull();
-                expect(hasError).toBe(false);
-            });
-        });
+  describe(name, () => {
+    describe('Battery.getBatteryLevelAsync()', () => {
+      it('returns a number between 0 and 1', async () => {
+        let batteryLevel;
+        let wasRejected = false;
+        try {
+          batteryLevel = await Battery.getBatteryLevelAsync();
+        } catch (error) {
+          wasRejected = true;
+        }
+        expect(wasRejected).toBe(false);
+        expect(batteryLevel).toEqual(jasmine.any(Number))
+        expect(batteryLevel).toBeLessThanOrEqual(1);
+        expect(batteryLevel).toBeGreaterThan(0);
+      });
     });
+    describe('Battery.getBatteryState()', () => {
+      it('returns a valid BatteryState enum value', async () => {
+        let batteryState;
+        let wasRejected = false;
+        try {
+          batteryState = await Battery.getBatteryStateAsync();
+        } catch (error) {
+          wasRejected = true;
+        }
+        expect(wasRejected).toBe(false);
+        expect(batteryState).toBeDefined();
+        expect(batteryState).toEqual(jasmine.any(Number))
+        expect(batteryState).toBeGreaterThanOrEqual(0);
+        expect(batteryState).toBeLessThanOrEqual(4);
+      });
+    });
+    describe('Battery.isLowPowerModeEnabledAsync()',() => {
+      it('returns a boolean low power mode', async () => {
+        let lowPowerMode;
+        let wasRejected = false;
+        try {
+          lowPowerMode = await Battery.isLowPowerModeEnabledAsync();
+        } catch (error) {
+          wasRejected = true;
+        }
+        expect(wasRejected).toBe(false);
+        expect(lowPowerMode).toBeDefined();
+        expect(lowPowerMode).toEqual(jasmine.any(Boolean));
+      });
+    });
+    describe('Battery.getPowerStateAsync()', () => {
+      it('returns a valid PowerState object', async () => {
+        let powerState;
+        let wasRejected = false;
+        try {
+          powerState = await Battery.getPowerStateAsync();
+        } catch (error) {
+          wasRejected = true;
+        }
+        expect(wasRejected).toBe(false);
+        expect(powerState).toBeDefined();
+        expect(powerState).toEqual(jasmine.any(Object))
+      });
+    });
+
+    describe('Event listeners', () => {
+      let _subscriptionBatteryLevel, _subscriptionBatteryState, _subscriptionPowerMode;
+
+      //event listeners do register
+      it('addLowPowerModeListener() registers', () => {
+        let hasError = false;
+        try {
+          _subscriptionPowerMode = Battery.addLowPowerModeListener(({ lowPowerMode }) => {
+            console.log('powerMode changed!', lowPowerMode);
+          });
+        } catch (e) {
+          console.log(e);
+          hasError = true;
+        }
+        expect(hasError).toBe(false);
+        expect(_subscriptionPowerMode).toBeDefined();
+
+        // clean up
+        _subscriptionPowerMode && _subscriptionPowerMode.remove();
+        _subscriptionPowerMode = null;
+      });
+
+      it('addBatteryStateListener() registers', () => {
+        let hasError = false;
+        try {
+          _subscriptionBatteryState = Battery.addBatteryStateListener(({ batteryState }) => {
+            console.log('batteryState changed!', batteryState);
+          });
+        } catch (e) {
+          console.log(e);
+          hasError = true;
+        }
+        expect(hasError).toBe(false);
+        expect(_subscriptionBatteryState).toBeDefined();
+
+        // clean up
+        _subscriptionBatteryState && _subscriptionBatteryState.remove();
+        _subscriptionBatteryState = null;
+      });
+
+      it('addBatteryLevelListener() registers', () => {
+        let hasError = false;
+        try {
+          _subscriptionBatteryLevel = Battery.addBatteryLevelListener(({ batteryLevel }) => {
+            console.log('batteryLevel changed!', batteryLevel);
+          });
+        } catch (e) {
+          console.log(e);
+          hasError = true;
+        }
+        expect(hasError).toBe(false);
+        expect(_subscriptionBatteryLevel).toBeDefined();
+
+        // clean up
+        _subscriptionBatteryLevel && _subscriptionBatteryLevel.remove();
+        _subscriptionBatteryLevel = null;
+      });
+
+
+      // Event listeners can unsubscribe
+      it('low power mode listener can unsubscribe', () => {
+        let hasError = false;
+
+        //subscribe
+        _subscriptionPowerMode = Battery.addLowPowerModeListener(({ lowPowerMode }) => {
+          console.log('powerMode changed!', lowPowerMode);
+        });
+
+        try {
+          _subscriptionPowerMode && _subscriptionPowerMode.remove();
+          _subscriptionPowerMode = null;
+        } catch (e) {
+          hasError = true;
+        }
+        expect(hasError).toBe(false);
+        expect(_subscriptionPowerMode).toBeNull();
+      });
+
+      it('battery level listener can unsubscribe', () => {
+        let hasError = false;
+
+        //subscribe
+        _subscriptionBatteryLevel = Battery.addBatteryLevelListener(({ batteryLevel }) => {
+          console.log('batteryLevel changed!', batteryLevel);
+        });
+
+        try {
+          _subscriptionBatteryLevel && _subscriptionBatteryLevel.remove();
+          _subscriptionBatteryLevel = null;
+        } catch (e) {
+          hasError = true;
+        }
+        expect(hasError).toBe(false);
+        expect(_subscriptionBatteryLevel).toBeNull();
+      });
+      it('battery state listener can unsubscribe', () => {
+        let hasError = false;
+
+        //subscribe
+        _subscriptionBatteryState = Battery.addBatteryStateListener(({ batteryState }) => {
+          console.log('batteryState changed!', batteryState);
+        });
+
+        try {
+          _subscriptionBatteryState && _subscriptionBatteryState.remove();
+          _subscriptionBatteryState = null;
+        } catch (e) {
+          hasError = true;
+        }
+        expect(hasError).toBe(false);
+        expect(_subscriptionBatteryState).toBeNull();
+      });
+
+      // TODO: check that events don't get fired after we unsubscribe
+      // but we currently don't have a way to programmatically set battery statuses
+    });
+  });
 }
