5116860dfc9768904405ab3264d23c4ac878366f
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-08-08 15:57:11 +0200

    [Permissions] Rewrite android native api (#5186)
    * Move logic form module to service
    
    * Remove static var from PermissionsService
    
    * Make getPermission not throwable
    
    * Store asked permissions

--- packages/expo-permissions/android/src/main/java/expo/modules/permissions/requesters/SystemBrightnessRequester.kt
+++ packages/expo-permissions/android/src/main/java/expo/modules/permissions/requesters/SystemBrightnessRequester.kt
@@ -3,38 +3,43 @@ package expo.modules.permissions.requesters
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import expo.modules.permissions.PermissionsModule
 import expo.modules.permissions.PermissionsTypes.SYSTEM_BRIGHTNESS
 import expo.modules.permissions.DENIED_VALUE
 import expo.modules.permissions.EXPIRES_KEY
 import expo.modules.permissions.GRANTED_VALUE
 import expo.modules.permissions.PERMISSION_EXPIRES_NEVER
+import expo.modules.permissions.PermissionsService
 import expo.modules.permissions.STATUS_KEY
 import expo.modules.permissions.UNDETERMINED_VALUE
 import org.unimodules.core.interfaces.ActivityProvider
 
-class SystemBrightnessRequester(private val activityProvider: ActivityProvider) : PermissionRequester {
+class SystemBrightnessRequester(private val permissionsService: PermissionsService,
+                                private val activityProvider: ActivityProvider) : PermissionRequester {
   override fun getPermissionToAsk(): Array<String> = emptyArray() // this permission is handled in different way
 
   // checkSelfPermission does not return accurate status of WRITE_SETTINGS
   override fun getPermission(): Bundle {
     return Bundle().apply {
       putString(EXPIRES_KEY, PERMISSION_EXPIRES_NEVER)
-      var statusKey: String = GRANTED_VALUE
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        statusKey = when {
-          Settings.System.canWrite(activityProvider.currentActivity.applicationContext) -> {
-            GRANTED_VALUE
-          }
-          PermissionsModule.didAsk(SYSTEM_BRIGHTNESS.type) -> {
-            DENIED_VALUE
-          }
-          else -> {
-            UNDETERMINED_VALUE
-          }
+      putString(STATUS_KEY, when {
+        hasWritePermission() -> {
+          GRANTED_VALUE
         }
-      }
-      putString(STATUS_KEY, statusKey)
+        permissionsService.didAsk(SYSTEM_BRIGHTNESS.type) -> {
+          DENIED_VALUE
+        }
+        else -> {
+          UNDETERMINED_VALUE
+        }
+      })
+    }
+  }
+
+  private fun hasWritePermission(): Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      Settings.System.canWrite(activityProvider.currentActivity.applicationContext)
+    } else {
+      true
     }
   }
 }
