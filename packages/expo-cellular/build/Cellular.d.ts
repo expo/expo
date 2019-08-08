fe2c939b7ffa4c227668760f538dafbb43280604
Tongyu Zhu <tongyu@usc.edu>
2019-08-08 15:09:06 -0700

    [expo-cellular] Create expo-cellular unimodule (#5098)
    * create new unimodule
    
    * [docs] set up readme
    
    * set up config
    
    update package.json and unimodule.json, we should update expo-tools to auto-generate this stuff
    
    * [js/ts] set up js
    
    * [ios] set up pods
    
    ran pod install
    
    * [ios] set up unimodule
    
    * [enum] change null to unknown for cellulargeneration enum
    
    * [ios] implement constants
    
    * [android] add cellularGeneration implementation
    
    * [android] update constant implementation
    
    * [docs] update js undefined impl and docs description
    
    * [ts/js] update enum
    
    * [ios] implement getCellularGenerationAsync
    
    [ios] Add support for iOS 12+ for CTTelelphonyNetworkInfo
    
    * (-)currentRadioAccessTechnology will be deprecated in iOS 13 in favor of (-)serviceCurrentRadioAccessTechnology
    
    * [ts/js] get rid of double ternary
    
    gave us some bugs in which `false` -> null for Cellular.allowsVoip
    
    * [android] fix when SIM_STATE_READY is false
    
    * Update cellular.md
    
    add info about null values
    
    * [ios] account for nullable cases
    
    * [web] implement getCellularGenerationAsync on web
    
    * fix docs and code format
    
    * [web] add check to avoid null crash
    
    * [docs] add browser compatibility link
    
    * [ios] regenerate pods
    
    ran pod install
    
    * update code format
    
    * [ios] delete commented code
    
    * [ios] update pods
    
    * update pods
    
    * [docs] update carrier info
    
    * delete unrelated files
    
    * add new line at EOF to package.json
    
    * [android] upgrade compileSdkVersion
    
    * [ios] reinstall pods

---
+++ packages/expo-cellular/build/Cellular.d.ts
@@ -0,0 +1,7 @@
+import { CellularGeneration } from './Cellular.types';
+export declare const allowsVoip: any;
+export declare const carrier: any;
+export declare const isoCountryCode: any;
+export declare const mobileCountryCode: any;
+export declare const mobileNetworkCode: any;
+export declare function getCellularGenerationAsync(): Promise<CellularGeneration>;
