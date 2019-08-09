9eb84fba01587c0f7463588719f3c9fd31c73199
Brent Vatne <brentvatne@gmail.com>
2019-08-09 09:56:43 -0700

    [docs] Better formatting on error page

--- docs/pages/_error.js
+++ docs/pages/_error.js
@@ -19,11 +19,20 @@ export default class Error extends React.Component {
 
   render() {
     return (
-      <div style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <div
+        style={{
+          display: 'flex',
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          flexDirection: 'column'
+        }}
+      >
         <p style={{ textAlign: 'center' }}>
           Uh oh, we couldn't find this page! Maybe it doesn't exist anymore! 😔
         </p>
-        <p style={{textAlign: 'center'}}>
+        <p style={{ textAlign: 'center', marginTop: 10 }}>
           <a href="/">Go back to the Expo documentation</a>
         </p>
       </div>
