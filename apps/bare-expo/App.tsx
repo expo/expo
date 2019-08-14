ef48a713f7638464a2fee1f7b6a36b6851a74cfa
Evan Bacon <baconbrix@gmail.com>
2019-08-13 19:29:58 -0700

    Update App.tsx

--- apps/bare-expo/App.tsx
+++ apps/bare-expo/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.ftsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
