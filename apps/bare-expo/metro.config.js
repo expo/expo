54634881d6b3c3316c9a6db223d572f42452a0ec
Evan Bacon <baconbrix@gmail.com>
2019-08-14 18:00:08 -0700

    Update metro.config.js

--- apps/bare-expo/metro.config.js
+++ apps/bare-expo/metro.config.js
@@ -4,20 +4,32 @@ const { createMetroConfiguration } = require('expo-yarn-workspaces');
 const blacklist = require('metro-config/src/defaults/blacklist');
 const config = createMetroConfiguration(__dirname);
 
-const blacklistRE = blacklist([
-  // [Custom] Prevent the haste collision in `ios/` versioned code.
-  // Otherwise you will get an error from Metro bundler.
-  /.*\/android\/ReactAndroid\/.*/,
-  /.*\/versioned-react-native\/.*/,
-]);
+const blacklistRE = () =>
+  blacklist([
+    // [Custom] Prevent the haste collision in `ios/` versioned code.
+    // Otherwise you will get an error from Metro bundler.
+    /.*\/android\/ReactAndroid\/.*/,
+    /.*\/versioned-react-native\/.*/,
+  ]);
+
 module.exports = {
   ...config,
-  watchFolders: [path.resolve(__dirname, '../../packages')],
+  watchFolders: [
+    path.resolve(__dirname, '../../packages'),
+    path.resolve(__dirname, '../../react-native-lab'),
+    path.resolve(__dirname, '../../node_modules'),
+  ],
   resolver: {
     ...(config.resolver || {}),
-    blacklistRE,
+    blacklistRE: blacklistRE(),
   },
   getBlackListRE() {
-    return blacklistRE;
+    return blacklistRE();
+  },
+  transformer: {
+    ...(config.transformer || {}),
+    getBlacklistRE() {
+      return blacklistRE();
+    },
   },
 };
