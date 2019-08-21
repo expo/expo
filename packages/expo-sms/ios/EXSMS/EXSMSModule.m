c8a69091b2dbcfedfa7bf48b894298ef708c8635
Evan Bacon <baconbrix@gmail.com>
2019-08-20 21:25:01 -0700

    Auto format ios

--- packages/expo-sms/ios/EXSMS/EXSMSModule.m
+++ packages/expo-sms/ios/EXSMS/EXSMSModule.m
@@ -18,75 +18,75 @@ UM_EXPORT_MODULE(ExpoSMS);
 
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
 {
-  _utils = [moduleRegistry getModuleImplementingProtocol:@protocol(UMUtilitiesInterface)];
+    _utils = [moduleRegistry getModuleImplementingProtocol:@protocol(UMUtilitiesInterface)];
 }
 
 UM_EXPORT_METHOD_AS(isAvailableAsync,
                     isAvailable:(UMPromiseResolveBlock)resolve
-                       rejecter:(UMPromiseRejectBlock)reject)
+                    rejecter:(UMPromiseRejectBlock)reject)
 {
-  BOOL canCompose = ![MFMessageComposeViewController canSendText];
-  resolve(@(canCompose));
+    BOOL canCompose = ![MFMessageComposeViewController canSendText];
+    resolve(@(canCompose));
 }
 
 UM_EXPORT_METHOD_AS(sendSMSAsync,
                     sendSMS:(NSArray<NSString *> *)addresses
                     message:(NSString *)message
-                   resolver:(UMPromiseResolveBlock)resolve
-                   rejecter:(UMPromiseRejectBlock)reject)
+                    resolver:(UMPromiseResolveBlock)resolve
+                    rejecter:(UMPromiseRejectBlock)reject)
 {
-   if (![MFMessageComposeViewController canSendText]) {
-     reject(@"E_SMS_UNAVAILABLE", @"SMS service not available", nil);
-     return;
-   }
-
-  if (_resolve != nil || _reject != nil) {
-    reject(@"E_SMS_SENDING_IN_PROGRESS", @"Different SMS sending in progress. Await the old request and then try again.", nil);
-    return;
-  }
-
-  _resolve = resolve;
-  _reject = reject;
-  
-  MFMessageComposeViewController *messageComposeViewController = [[MFMessageComposeViewController alloc] init];
-  messageComposeViewController.messageComposeDelegate = self;
-  messageComposeViewController.recipients = addresses;
-  messageComposeViewController.body = message;
-
-  UM_WEAKIFY(self);
-  [UMUtilities performSynchronouslyOnMainThread:^{
-    UM_ENSURE_STRONGIFY(self);
-    [self.utils.currentViewController presentViewController:messageComposeViewController animated:YES completion:nil];
-  }];
+    if (![MFMessageComposeViewController canSendText]) {
+        reject(@"E_SMS_UNAVAILABLE", @"SMS service not available", nil);
+        return;
+    }
+    
+    if (_resolve != nil || _reject != nil) {
+        reject(@"E_SMS_SENDING_IN_PROGRESS", @"Different SMS sending in progress. Await the old request and then try again.", nil);
+        return;
+    }
+    
+    _resolve = resolve;
+    _reject = reject;
+    
+    MFMessageComposeViewController *messageComposeViewController = [[MFMessageComposeViewController alloc] init];
+    messageComposeViewController.messageComposeDelegate = self;
+    messageComposeViewController.recipients = addresses;
+    messageComposeViewController.body = message;
+    
+    UM_WEAKIFY(self);
+    [UMUtilities performSynchronouslyOnMainThread:^{
+        UM_ENSURE_STRONGIFY(self);
+        [self.utils.currentViewController presentViewController:messageComposeViewController animated:YES completion:nil];
+    }];
 }
 
 - (void)messageComposeViewController:(MFMessageComposeViewController *)controller
                  didFinishWithResult:(MessageComposeResult)result
 {
-  NSDictionary *resolveData;
-  NSString *rejectMessage;
-  switch (result) {
-    case MessageComposeResultCancelled:
-      resolveData = @{@"result": @"cancelled"};
-      break;
-    case MessageComposeResultFailed:
-      rejectMessage = @"User's attempt to save or send an SMS was unsuccessful.";
-      break;
-    case MessageComposeResultSent:
-      resolveData = @{@"result": @"sent"};
-      break;
-  }
-  UM_WEAKIFY(self);
-  [controller dismissViewControllerAnimated:YES completion:^{
-    UM_ENSURE_STRONGIFY(self);
-    if (rejectMessage) {
-      self->_reject(@"E_SMS_SENDING_FAILED", rejectMessage, nil);
-    } else {
-      self->_resolve(resolveData);
+    NSDictionary *resolveData;
+    NSString *rejectMessage;
+    switch (result) {
+        case MessageComposeResultCancelled:
+            resolveData = @{@"result": @"cancelled"};
+            break;
+        case MessageComposeResultFailed:
+            rejectMessage = @"User's attempt to save or send an SMS was unsuccessful.";
+            break;
+        case MessageComposeResultSent:
+            resolveData = @{@"result": @"sent"};
+            break;
     }
-    self->_reject = nil;
-    self->_resolve = nil;
-  }];
+    UM_WEAKIFY(self);
+    [controller dismissViewControllerAnimated:YES completion:^{
+        UM_ENSURE_STRONGIFY(self);
+        if (rejectMessage) {
+            self->_reject(@"E_SMS_SENDING_FAILED", rejectMessage, nil);
+        } else {
+            self->_resolve(resolveData);
+        }
+        self->_reject = nil;
+        self->_resolve = nil;
+    }];
 }
 
 @end
