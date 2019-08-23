befa75814553d495adb305717498e33ddcbd2f8e
Evan Bacon <baconbrix@gmail.com>
2019-08-22 21:44:17 -0700

    Updated build

--- apps/bare-expo/scripts/setup-project.sh
+++ apps/bare-expo/scripts/setup-project.sh
@@ -28,11 +28,11 @@ fi
 if [ -d "node_modules/react-native/android" ]; then
     echo " ✅ React Android is installed"
 else
-    echo " ⚠️  Compiling React Android..."
+    echo " ⚠️  Compiling React Android (~5-10 minutes)..."
     # Go to our fork of React Native
     cd ../../react-native-lab/react-native
-    # Build the AARs (~10 minutes)
-    gradlew :ReactAndroid:installArchives 
+    # Build the AARs (~5-10 minutes)
+    ./gradlew :ReactAndroid:installArchives 
     # Come back to the project
     cd ../../apps/bare-expo/android
     
