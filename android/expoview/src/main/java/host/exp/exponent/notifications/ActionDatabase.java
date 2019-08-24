f9c4d4d919dd226f4eec6d2db2c86588d4b59997
Yifei He <yifei@hesyifei.com>
2019-08-23 17:08:08 -0700

    [android] Add migration script to `ActionDatabase`

--- android/expoview/src/main/java/host/exp/exponent/notifications/ActionDatabase.java
+++ android/expoview/src/main/java/host/exp/exponent/notifications/ActionDatabase.java
@@ -1,9 +1,25 @@
 package host.exp.exponent.notifications;
 
 import com.raizlabs.android.dbflow.annotation.Database;
+import com.raizlabs.android.dbflow.annotation.Migration;
+import com.raizlabs.android.dbflow.sql.SQLiteType;
+import com.raizlabs.android.dbflow.sql.migration.AlterTableMigration;
 
 @Database(version = ActionDatabase.VERSION)
 public class ActionDatabase {
   public static final String NAME = "ExpoNotificationActions";
-  public static final int VERSION = 1;
+  public static final int VERSION = 2;
+
+  @Migration(version = 2, database = ActionDatabase.class)
+  public static class Migration2 extends AlterTableMigration<ActionObject> {
+
+    public Migration2(Class<ActionObject> table) {
+      super(table);
+    }
+
+    @Override
+    public void onPreMigrate() {
+      addColumn(SQLiteType.INTEGER, "isBackgroundAction");
+    }
+  }
 }
