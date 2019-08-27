576809d51348573df3ac24c11fba7f65bb7ea87e
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2019-08-27 02:38:58 +0200

    [expo-mail-composer] Fix adding attachments by implementing a FileProvider (#5449)
    # Why
    
    Closes https://github.com/expo/expo/pull/5346 and the problem described there.
    
    # How
    
    Implemented similar solution to https://github.com/expo/expo/commit/6747d3ae73bf870ebfd88e0f32028aaf0b1f8f24.
    
    # Test Plan
    
    Native code builds.

---
+++ packages/expo-mail-composer/android/src/main/java/expo/modules/mailcomposer/MailComposerFileProvider.java
@@ -0,0 +1,9 @@
+package expo.modules.mailcomposer;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Dummy class for proving files for this module.
+ */
+public class MailComposerFileProvider extends FileProvider {
+}
