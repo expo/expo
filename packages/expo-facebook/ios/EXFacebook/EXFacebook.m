6a92cef273156305675b4858d8e8e58287001614
Stanisław Chmiela <sjchmiela@gmail.com>
2019-08-29 15:05:13 -0700

    [expo-facebook] Remove support for different behaviors

--- packages/expo-facebook/ios/EXFacebook/EXFacebook.m
+++ packages/expo-facebook/ios/EXFacebook/EXFacebook.m
@@ -8,7 +8,7 @@
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 NSString * const EXFacebookLoginErrorDomain = @"E_FBLOGIN";
-NSString * const EXFacebookLoginBehaviorErrorDomain = @"E_FBLOGIN_BEHAVIOR";
+NSString * const EXFacebookLoginAppIdErrorDomain = @"E_FBLOGIN_APP_ID";
 
 @implementation EXFacebook
 
@@ -25,47 +25,23 @@ UM_EXPORT_METHOD_AS(logInWithReadPermissionsAsync,
     permissions = @[@"public_profile", @"email"];
   }
 
-  NSString *behavior = config[@"behavior"];
-
   // FB SDK requires login to run on main thread
   // Needs to not race with other mutations of this global FB state
   dispatch_async(dispatch_get_main_queue(), ^{
-    [FBSDKAccessToken setCurrentAccessToken:nil];
     [FBSDKSettings setAppID:appId];
     FBSDKLoginManager *loginMgr = [[FBSDKLoginManager alloc] init];
-
-    loginMgr.loginBehavior = FBSDKLoginBehaviorSystemAccount;
-    if (behavior) {
-      // TODO: Support other logon behaviors?
-      //       - browser is problematic because it navigates to fb<appid>:// when done
-      //       - system is problematic because it asks whether to give 'Exponent' permissions,
-      //         just a weird user-facing UI
-      if ([behavior isEqualToString:@"native"]) {
-        loginMgr.loginBehavior = FBSDKLoginBehaviorNative;
-      } else if ([behavior isEqualToString:@"browser"]) {
-        loginMgr.loginBehavior = FBSDKLoginBehaviorBrowser;
-      } else if ([behavior isEqualToString:@"system"]) {
-        loginMgr.loginBehavior = FBSDKLoginBehaviorSystemAccount;
-      } else if ([behavior isEqualToString:@"web"]) {
-        loginMgr.loginBehavior = FBSDKLoginBehaviorWeb;
-      }
-    }
-
-    if (loginMgr.loginBehavior != FBSDKLoginBehaviorWeb) {
-      if (![[self class] facebookAppIdFromNSBundle]) {
-        // We can't reliably execute non-web login
-        // without an appId in Info.plist.
-        NSString *message = [NSString stringWithFormat:
-                             @"Tried to perform Facebook login with behavior `%@`, but "
-                             "no Facebook app id was provided. Specify Facebook app id in Info.plist "
-                             "or switch to `web` behavior.", behavior];
-        reject(EXFacebookLoginBehaviorErrorDomain, message, UMErrorWithMessage(message));
-        return;
-      }
+    [loginMgr logOut];
+    if (![[self class] facebookAppIdFromNSBundle]) {
+      // We can't reliably execute login without an appId in Info.plist.
+      NSString *message = [NSString stringWithFormat:
+                           @"Tried to perform Facebook login, but no Facebook app id was provided."
+                           " Specify Facebook app id in Info.plist."];
+      reject(EXFacebookLoginAppIdErrorDomain, message, UMErrorWithMessage(message));
+      return;
     }
 
     @try {
-      [loginMgr logInWithReadPermissions:permissions fromViewController:nil handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+      [loginMgr logInWithPermissions:permissions fromViewController:nil handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
         if (error) {
           reject(EXFacebookLoginErrorDomain, @"Error with Facebook login", error);
           return;
