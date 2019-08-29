72e69e10026c80a2a24e6bbabf72bf321c11105e
szymon <szymon.kapala@swmansion.com>
2019-08-29 13:45:17 +0200

    several fixes

--- tools/expotools/src/versioning/android/android-copy-unimodule.sh
+++ tools/expotools/src/versioning/android/android-copy-unimodule.sh
@@ -9,8 +9,8 @@ TOOLS_DIR=`pwd`
 
 pushd $EXPO_ROOT_DIR/android
 
-cp -r $2/src/main/java/* $VERSIONED_ABI_PATH/src/main/java/$ABI_VERSION
-cp -r $2/src/main/AndroidManifest.xml $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml
+cp -r $2/src/main/java/* $VERSIONED_ABI_PATH/src/main/java/$ABI_VERSION;
+cp -r $2/src/main/AndroidManifest.xml $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml;
 
 # Rename references to other packages previously under versioned.host.exp.exponent
 
@@ -19,26 +19,28 @@ find $VERSIONED_ABI_PATH/src/main/java/$ABI_VERSION -iname 'flutter' -type d -pr
 while read PACKAGE
 do
   find $VERSIONED_ABI_PATH/src/main/java/$ABI_VERSION \( -iname '*.java' -or -iname '*.kt' \) -type f -print0 | xargs -0 sed -i '' "s/\([, ^\(<]\)$PACKAGE/\1temporarydonotversion.$PACKAGE/g"
-  sed -i '' "s/\([, ^\(]\)$PACKAGE/\1temporarydonotversion.$PACKAGE/g" $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml
+  BETTER_PACKAGE=$(sed -E  "s/\./\\\./g" <<< $PACKAGE);
+  sed -E -i '' "s/\"($BETTER_PACKAGE.*)/\"temporarydonotversion\.\1/g" $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml
 done < $TOOLS_DIR/android-packages-to-keep.txt
 
-# Rename references to react native
 while read PACKAGE
 do
   find $VERSIONED_ABI_PATH/src/main/java/$ABI_VERSION \( -iname '*.java' -or -iname '*.kt' \) -type f -print0 | xargs -0 sed -i '' "s/\([, ^\(<]\)$PACKAGE/\1$ABI_VERSION.$PACKAGE/g"
-  sed -i '' "s/\([, ^\(]\)$PACKAGE/\1$ABI_VERSION.$PACKAGE/g" $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml
+  BETTER_PACKAGE=$(sed -E  "s/\./\\\./g" <<< $PACKAGE);
+  sed -E -i '' "s/\"($BETTER_PACKAGE.*)/\"$ABI_VERSION.\1/g" $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml;
 done < $TOOLS_DIR/android-packages-to-rename.txt
 
 while read PACKAGE
 do
   find $VERSIONED_ABI_PATH/src/main/java/$ABI_VERSION \( -iname '*.java' -or -iname '*.kt' \) -type f -print0 | xargs -0 sed -i '' "s/\([, ^\(<]\)temporarydonotversion.$PACKAGE/\1$PACKAGE/g"
-  sed -i '' "s/\([, ^\(]\)temporarydonotversion.$PACKAGE/\1$PACKAGE/g" $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml
+  BETTER_PACKAGE=$(sed -E  "s/\./\\\./g" <<< $PACKAGE);
+  sed -E -i '' "s/temporarydonotversion\.(.*)/\1/g" $VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml
 done < $TOOLS_DIR/android-packages-to-keep.txt
 
-DIR=$(pwd)
+DIR=$(pwd);
 
-popd
+popd;
 
-java -jar mergerer/target/manifest-merger-jar-with-dependencies.jar  --main $DIR/$VERSIONED_ABI_PATH/src/main/AndroidManifest.xml  --libs $DIR/$VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml --out $DIR/$VERSIONED_ABI_PATH/src/main/AndroidManifest.xml --log WARNING
+java -jar mergerer/target/manifest-merger-jar-with-dependencies.jar  --main $DIR/$VERSIONED_ABI_PATH/src/main/AndroidManifest.xml  --libs $DIR/$VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml --out $DIR/$VERSIONED_ABI_PATH/src/main/AndroidManifest.xml --log WARNING;
 
 rm $DIR/$VERSIONED_ABI_PATH/src/main/AndroidManifestTemp.xml
