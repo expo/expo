5a1827269ac6e9c3150c9016b5f7b0943ff1dee2
Evan Bacon <baconbrix@gmail.com>
2019-08-29 22:52:39 -0700

    Update wait-for-emulator.sh

--- apps/bare-expo/scripts/wait-for-emulator.sh
+++ apps/bare-expo/scripts/wait-for-emulator.sh
@@ -3,13 +3,13 @@
 set +e
 
 bootanim=""
-failcounter=0
+connectionAttempts=0
 until [[ "$bootanim" =~ "stopped" ]]; do
    bootanim=`$ANDROID_HOME/platform-tools/adb -e shell getprop init.svc.bootanim 2>&1`
    echo " ☛  $bootanim"
    if [[ "$bootanim" =~ "not found" ]]; then
-      let "failcounter += 1"
-      if [[ $failcounter -gt 15 ]]; then
+      let "connectionAttempts += 1"
+      if [[ $connectionAttempts -gt 15 ]]; then
         echo " 🛑  Failed to start emulator"
         exit 1
       fi
