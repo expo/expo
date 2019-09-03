35931cae9b930f9f23b764e39907e515675ced17
Evan Bacon <baconbrix@gmail.com>
2019-09-03 17:32:45 -0500

    Bare Expo run scripts (#5428)
    * [WIP] setup bare-expo initial flow
    
    * Updated build
    
    * Removed sync step
    
    * Fixed first link
    
    * Update package.json
    
    * Update README.md
    
    * Updated detox script names
    
    * Update MainApplication.java
    
    * Update deep-link.sh
    
    * Create DetoxTest.java
    
    * Updated test suite for physical devices
    
    * Updated context to use hooks
    
    * Update TestUtils.js
    
    * Update apps/bare-expo/scripts/start-emulator.sh
    
    Co-Authored-By: James Ide <ide@users.noreply.github.com>
    
    * Update Brightness.js
    
    * Move test-suite changes to #5483
    
    * Remove iOS changes
    
    * Update yarn.lock
    
    * fix defaults
    
    * Revert main nav
    
    * Updated bash
    
    * Update wait-for-emulator.sh
    
    * Update start-metro.sh

--- apps/bare-expo/MainNavigator.ts
+++ apps/bare-expo/MainNavigator.ts
@@ -1,4 +1,3 @@
-import { View } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import TestSuite from '../test-suite/App.bare';
@@ -7,13 +6,11 @@ import TestSuite from '../test-suite/App.bare';
 
 const MainNavigator = createStackNavigator(
   {
-    None: View,
     TestSuite: { screen: TestSuite, path: 'test-suite' },
     // NativeComponentList: { screen: NativeComponentList, path: 'native-component-list' },
   },
   {
     // @ts-ignore
-    initialRouteName: DETOX ? 'None' : 'TestSuite',
     headerMode: 'none',
     transitionConfig: () => ({
       transitionSpec: {
