f111654909631dad6eec1b0d0c2a60031505e89b
Evan Bacon <baconbrix@gmail.com>
2019-09-07 11:46:49 -0700

    Added watch plugins

--- packages/expo-module-scripts/universal/jest-preset.js
+++ packages/expo-module-scripts/universal/jest-preset.js
@@ -1,9 +1,10 @@
+const { withWatchPlugins } = require('jest-expo/config');
 const withTypeScript = require('../withTypeScript');
-module.exports = {
+module.exports = withWatchPlugins({
   projects: [
     withTypeScript(require('jest-expo/ios/jest-preset')),
     withTypeScript(require('jest-expo/android/jest-preset')),
     withTypeScript(require('jest-expo/web/jest-preset')),
     withTypeScript(require('jest-expo/node/jest-preset')),
   ],
-};
+});
