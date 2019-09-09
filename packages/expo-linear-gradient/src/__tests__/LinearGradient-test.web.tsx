31e99f141b3064bddec0b0e92ffefdb800b06231
Evan Bacon <baconbrix@gmail.com>
2019-09-09 13:51:38 -0700

    Update LinearGradient-test.web.tsx

--- packages/expo-linear-gradient/src/__tests__/LinearGradient-test.web.tsx
+++ packages/expo-linear-gradient/src/__tests__/LinearGradient-test.web.tsx
@@ -1,4 +1,3 @@
-/* eslint-env jasmine, jest */
 import { mount } from 'enzyme';
 import React from 'react';
 import { StyleSheet } from 'react-native';
@@ -7,15 +6,15 @@ import { LinearGradient } from '..';
 
 const getStyleProp = (component, prop) => StyleSheet.flatten(component.prop('style'))[prop];
 
-it(`a basic gradient`, () => {
+it(`renders a simple two-tone gradient background with alpha`, () => {
     const component = mount(<LinearGradient colors={['white', 'rgba(0,0,0,0)']} />);
     expect(getStyleProp(component.find('View'), 'backgroundImage')).toMatchSnapshot();
 });
   
-it(`a complex gradient`, () => {
+it(`renders a complex gradient with angles and locations`, () => {
     const component = mount(
         <LinearGradient
-        colors={['red', 'blue']}
+        colors={['red', 'rgba(0,255,255,0)']}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         locations={[0.5, 1]}
