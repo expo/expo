034d2790acf9c65570ab72525bd30439f3e089dd
Szymon20000 <szymon20000@gmail.com>
2019-09-10 00:31:45 +0200

    [sqlite] Improve exported types in expo-sqlite module. (#5544)
    # Why
    
    Resolves #5264 
    
    # How
    
    Copy several types from @types/websql.

--- packages/expo/build/globals.web.js
+++ packages/expo/build/globals.web.js
@@ -8,10 +8,11 @@
 import Constants from 'expo-constants';
 import * as Font from 'expo-font';
 import * as Icon from '@expo/vector-icons';
+import * as SQLite from 'expo-sqlite';
 export { Asset } from 'expo-asset';
 export { Constants };
 export { Font };
 export { Icon };
 export { LinearGradient } from 'expo-linear-gradient';
-export { SQLite } from 'expo-sqlite';
+export { SQLite };
 //# sourceMappingURL=globals.web.js.map
