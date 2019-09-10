5748ea02e78ae65c5d4d581ab638f836191ed321
Evan Bacon <baconbrix@gmail.com>
2019-09-10 12:20:01 -0700

    Added script to make CI fail when builds fail

---
+++ apps/bare-expo/scripts/build-detox-ios.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Debug or Release
+configuration=$1
+# YES or NO
+UseModernBuildSystem=${2:-"NO"}
+
+xcodebuild \
+    -workspace ios/BareExpo.xcworkspace \
+    -scheme BareExpo \
+    -configuration $configuration \
+    -sdk iphonesimulator \
+    -derivedDataPath "ios/build" \
+    -UseModernBuildSystem=$UseModernBuildSystem 2>&1 | tee result.txt | xcpretty -k
+
+if ! grep '\*\* BUILD SUCCEEDED \*\*' result.txt; then
+    echo 'Build Failed'
+    cat result.txt
+    rm result.txt
+    set +e
+    exit 1
+fi
+rm result.txt
+
+echo 'Build Succeeded'
