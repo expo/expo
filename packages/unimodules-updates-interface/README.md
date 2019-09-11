76ed9b10ebcd7b5cf2bce80ab6074d7d3683afb6
Michał Czernek <czernekmichal@gmail.com>
2019-09-11 11:16:26 +0200

    Remove obsolete module.

--- packages/unimodules-updates-interface/README.md
+++
@@ -1,5 +0,0 @@
-# unimodules-updates-interface
-
-`unimodules-updates-interface` provides some great stuff.
-
-See [<ModuleName> docs](https://docs.expo.io/versions/latest/sdk/<module-docs-name>) for documentation of this universal module's API.
--- packages/unimodules-updates-interface/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { NativeModulesProxy } from '@unimodules/core';
-
-const { UnimodulesUpdatesInterface } = NativeModulesProxy;
-
-export { default as UnimodulesUpdatesInterfaceView } from './UnimodulesUpdatesInterfaceView';
-
-export async function someGreatMethodAsync(options: any) {
-  return await UnimodulesUpdatesInterface.someGreatMethodAsync(options);
-}
