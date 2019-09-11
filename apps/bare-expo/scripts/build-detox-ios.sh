18cf43b8b5faacaf332dd8bbc2fa7cec5df6ae7b
James Ide <ide@expo.io>
2019-09-11 15:07:04 -0700

    [ios][detox] Use PIPESTATUS instead of grepping a file to get xcodebuild's exit code
    This uses the actual exit code instead of grepping for strings in the output. Tested

--- apps/bare-expo/scripts/build-detox-ios.sh
+++ apps/bare-expo/scripts/build-detox-ios.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# This script is design to wrap the xcodebuild command
-# and exit with non-zero if the build fails. 
-#
-# This ensures that CI fails on the correct step 
-# instead of attempting to run Detox tests without a build.
 
-set -e
+# This script wraps the xcodebuild command and exits with non-zero if the build fails. 
+#
+# This ensures that CI fails on the correct step instead of attempting to run Detox tests without a
+# build.
+
+set -eu
 
 # Debug or Release
 configuration=$1
@@ -13,20 +13,17 @@ configuration=$1
 UseModernBuildSystem=${2:-"NO"}
 
 xcodebuild \
-    -workspace ios/BareExpo.xcworkspace \
-    -scheme BareExpo \
-    -configuration $configuration \
-    -sdk iphonesimulator \
-    -derivedDataPath "ios/build" \
-    -UseModernBuildSystem=$UseModernBuildSystem 2>&1 | tee result.txt | xcpretty -k
+  -workspace ios/BareExpo.xcworkspace \
+  -scheme BareExpo \
+  -configuration "$configuration" \
+  -sdk iphonesimulator \
+  -derivedDataPath "ios/build" \
+  -UseModernBuildSystem="$UseModernBuildSystem" 2>&1 | xcpretty --knock
 
-if ! grep '\*\* BUILD SUCCEEDED \*\*' result.txt; then
-    echo 'Build Failed'
-    # cat result.txt | xcpretty -k
-    rm result.txt
-    set +e
-    exit 1
+if [ "${PIPESTATUS[0]}" -ne "0" ]; then
+  echo 'Build Failed'
+  set +e
+  exit 1
 fi
-rm result.txt
 
 echo 'Build Succeeded'
