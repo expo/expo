86ef0964e004301190147b4663787add7db8ec3a
Evan Bacon <baconbrix@gmail.com>
2019-09-10 18:51:45 -0700

    Updated workflow

--- scripts/remap-submodules-to-https.sh
+++ scripts/remap-submodules-to-https.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Copyright 2019-present 650 Industries. All rights reserved.
 
-# exit when any command fails
 set -e
 
-sed 's#git@github.com:#https://github.com/#g' $1 > $2
+sed 's#git@github.com:#https://github.com/#g' $1
