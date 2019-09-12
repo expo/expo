a61feb59d24b457f95439346ca5ab3c98e263a9f
James Ide <ide@expo.io>
2019-09-11 20:08:24 -0700

    Revert "[ios][detox] Restore watch option for tests"
    This reverts commit 771f8820787ade0d575484fb33b2c67263ec6bf2.

--- apps/bare-expo/scripts/start-simulator.sh
+++ apps/bare-expo/scripts/start-simulator.sh
@@ -27,7 +27,7 @@ if [ "${CURRENT_ENV}" = "test" ]; then
         echo " ✅ Debug Detox project is built for iOS"
     else
         echo " ⚠️  Building the debug Detox project..."
-        yarn run ios:detox:build:debug
+        yarn run ios:detox:build:debug --watch
     fi
 
     echo " ☛  Opening the iOS simulator app"
@@ -36,7 +36,7 @@ if [ "${CURRENT_ENV}" = "test" ]; then
 
     echo " ☛  Starting Detox in watch mode"
     # Run our default E2E tests
-    yarn run ios:detox:test:debug --watch
+    yarn run ios:detox:test:debug
 else 
 
     echo " ☛  Running the iOS project..."
