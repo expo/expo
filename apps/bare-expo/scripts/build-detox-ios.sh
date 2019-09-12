e6cb0b81374cea5c6d0ca717b12356408daa646a
James Ide <ide@expo.io>
2019-09-11 20:08:25 -0700

    Revert "[ios][detox] Use PIPESTATUS instead of grepping a file to get xcodebuild's exit code"
    This reverts commit 27258aaaa1bf50b4715472ee6be011663d12b485.

--- apps/bare-expo/scripts/build-detox-ios.sh
+++ apps/bare-expo/scripts/build-detox-ios.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
-# This script wraps the xcodebuild command and exits with non-zero if the build fails. 
+# This script is design to wrap the xcodebuild command
+# and exit with non-zero if the build fails. 
 #
-# This ensures that CI fails on the correct step instead of attempting to run Detox tests without a
-# build.
+# This ensures that CI fails on the correct step 
+# instead of attempting to run Detox tests without a build.
 
-set -eu
+set -e
 
 # Debug or Release
 configuration=$1
@@ -13,17 +13,20 @@ configuration=$1
 UseModernBuildSystem=${2:-"NO"}
 
 xcodebuild \
-  -workspace ios/BareExpo.xcworkspace \
-  -scheme BareExpo \
-  -configuration "$configuration" \
-  -sdk iphonesimulator \
-  -derivedDataPath "ios/build" \
-  -UseModernBuildSystem="$UseModernBuildSystem" 2>&1 | xcpretty --knock
+    -workspace ios/BareExpo.xcworkspace \
+    -scheme BareExpo \
+    -configuration $configuration \
+    -sdk iphonesimulator \
+    -derivedDataPath "ios/build" \
+    -UseModernBuildSystem=$UseModernBuildSystem 2>&1 | tee result.txt | xcpretty -k
 
-if [ "${PIPESTATUS[0]}" -ne "0" ]; then
-  echo 'Build Failed'
-  set +e
-  exit 1
+if ! grep '\*\* BUILD SUCCEEDED \*\*' result.txt; then
+    echo 'Build Failed'
+    # cat result.txt | xcpretty -k
+    rm result.txt
+    set +e
+    exit 1
 fi
+rm result.txt
 
 echo 'Build Succeeded'
