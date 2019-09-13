ef1c44f8655f84c8533486879842161bfaa72f8c
Evan Bacon <baconbrix@gmail.com>
2019-09-13 13:49:54 -0700

    Updated SecureStore tests

--- packages/expo-secure-store/src/__tests__/SecureStore-test.native.ts
+++ packages/expo-secure-store/src/__tests__/SecureStore-test.native.ts
@@ -1,7 +1,6 @@
 import { NativeModulesProxy } from '@unimodules/core';
-import * as SecureStore from '../SecureStore';
 
-import { mockPlatformIOS } from '../../test/mocking';
+import * as SecureStore from '../SecureStore';
 
 it(`sets values`, async () => {
   const testKey = 'key-test_0.0';
@@ -68,22 +67,4 @@ it(`checks for invalid values`, async () => {
   await expect(SecureStore.setItemAsync('key', (() => {}) as any)).rejects.toMatchSnapshot();
 
   expect(NativeModulesProxy.ExpoSecureStore.setValueWithKeyAsync).not.toHaveBeenCalled();
-});
-
-it(`exports accessibility options on iOS`, () => {
-  mockPlatformIOS();
-
-  expect(SecureStore.AFTER_FIRST_UNLOCK).toMatchSnapshot('AFTER_FIRST_UNLOCK');
-  expect(SecureStore.AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY).toMatchSnapshot(
-    'AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY'
-  );
-  expect(SecureStore.ALWAYS).toMatchSnapshot('ALWAYS');
-  expect(SecureStore.ALWAYS_THIS_DEVICE_ONLY).toMatchSnapshot('ALWAYS_THIS_DEVICE_ONLY');
-  expect(SecureStore.WHEN_UNLOCKED).toMatchSnapshot('WHEN_UNLOCKED');
-  expect(SecureStore.WHEN_UNLOCKED_THIS_DEVICE_ONLY).toMatchSnapshot(
-    'WHEN_UNLOCKED_THIS_DEVICE_ONLY'
-  );
-  expect(SecureStore.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY).toMatchSnapshot(
-    'WHEN_PASSCODE_SET_THIS_DEVICE_ONLY'
-  );
 });
