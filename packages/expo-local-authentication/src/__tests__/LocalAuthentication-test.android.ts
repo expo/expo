857e1856eff05b62439fcc8290af5b8d37dd5ef5
Evan Bacon <baconbrix@gmail.com>
2019-09-13 13:35:37 -0700

    Updated Local Authentication tests

---
+++ packages/expo-local-authentication/src/__tests__/LocalAuthentication-test.android.ts
@@ -0,0 +1,14 @@
+import ExpoLocalAuthentication from '../ExpoLocalAuthentication';
+import * as LocalAuthentication from '../LocalAuthentication';
+
+beforeEach(() => {
+  ExpoLocalAuthentication.authenticateAsync.mockImplementation(async () => ({}));
+});
+
+it(`doesn't use a message on Android`, async () => {
+  await LocalAuthentication.authenticateAsync({
+    promptMessage: 'Authentication is required',
+  });
+
+  expect(ExpoLocalAuthentication.authenticateAsync).toHaveBeenLastCalledWith();
+});
--- packages/expo-local-authentication/src/__tests__/LocalAuthentication-test.ios.ts
+++ packages/expo-local-authentication/src/__tests__/LocalAuthentication-test.ios.ts
@@ -1,5 +1,3 @@
-import { mockPlatformAndroid, mockPlatformIOS, unmockAllProperties } from 'jest-expo';
-
 import ExpoLocalAuthentication from '../ExpoLocalAuthentication';
 import * as LocalAuthentication from '../LocalAuthentication';
 
@@ -7,11 +5,7 @@ beforeEach(() => {
   ExpoLocalAuthentication.authenticateAsync.mockImplementation(async () => ({}));
 });
 
-afterEach(unmockAllProperties);
-
 it(`uses promptMessage and fallbackLabel on iOS`, async () => {
-  mockPlatformIOS();
-
   const options = {
     promptMessage: 'Authentication is required',
     fallbackLabel: 'Use passcode',
@@ -22,20 +16,8 @@ it(`uses promptMessage and fallbackLabel on iOS`, async () => {
 });
 
 it(`throws when an invalid message is used on iOS`, async () => {
-  mockPlatformIOS();
-
   expect(LocalAuthentication.authenticateAsync(undefined)).rejects.toThrow();
   expect(LocalAuthentication.authenticateAsync('' as any)).rejects.toThrow();
   expect(LocalAuthentication.authenticateAsync({} as any)).rejects.toThrow();
   expect(LocalAuthentication.authenticateAsync(123 as any)).rejects.toThrow();
-});
-
-it(`doesn't use a message on Android`, async () => {
-  mockPlatformAndroid();
-
-  await LocalAuthentication.authenticateAsync({
-    promptMessage: 'Authentication is required',
-  });
-
-  expect(ExpoLocalAuthentication.authenticateAsync).toHaveBeenLastCalledWith();
 });
