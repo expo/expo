4dcb7e9834559b440eb03055027817618e53eddb
Evan Bacon <baconbrix@gmail.com>
2019-09-13 13:52:05 -0700

    Updated Contacts tests

--- packages/expo-contacts/src/__tests__/Contacts-test.ios.ts
+++ packages/expo-contacts/src/__tests__/Contacts-test.ios.ts
@@ -1,13 +1,8 @@
-import { mockPlatformIOS, unmockAllProperties } from 'jest-expo';
-
 import * as Contacts from '../Contacts';
 import ExpoContacts from '../ExpoContacts';
 
 describe('presentFormAsync', () => {
-  afterEach(unmockAllProperties);
   it(`should invalidate contact when ID is provided`, async () => {
-    mockPlatformIOS();
-
     const contactId = '<DEBUG>';
     const contact = {
       id: '<DEBUG_ID>',
