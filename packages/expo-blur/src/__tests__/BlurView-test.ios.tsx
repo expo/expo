d98d9ee81ba7234e99150aebc573b01b30cc9c51
Evan Bacon <baconbrix@gmail.com>
2019-09-13 18:06:01 -0700

    Added BlurView feedback

--- packages/expo-blur/src/__tests__/BlurView-test.ios.tsx
+++ packages/expo-blur/src/__tests__/BlurView-test.ios.tsx
@@ -1,11 +1,11 @@
-/* eslint-env jasmine, jest */
 import { mount } from 'enzyme';
 import React from 'react';
+
 import { BlurView } from '..';
 
 it(`renders a native blur view`, () => {
-  const component = mount(<BlurView tint={'light'} intensity={0.65} />);
-  // console.log('component:', component.debug({ verbose: true }));
+  const component = mount(<BlurView tint="light" intensity={0.65} />);
+
   expect(
     component.find('ViewManagerAdapter_ExpoBlurView').prop('proxiedProperties')
   ).toMatchSnapshot();
--- packages/expo-blur/src/__tests__/BlurView-test.web.tsx
+++ packages/expo-blur/src/__tests__/BlurView-test.web.tsx
@@ -1,4 +1,3 @@
-/* eslint-env jasmine, jest */
 import React from 'react';
 import { mount } from 'enzyme';
 import { StyleSheet } from 'react-native';
@@ -7,28 +6,33 @@ import { BlurView } from '..';
 
 const getStyleProp = (component, prop) => StyleSheet.flatten(component.prop('style'))[prop];
 
-it(`uses a transparent background color when filters aren't supported`, () => {
-  // @ts-ignore
-  let originalCSS = global.CSS;
+// @ts-ignore
+const originalCSS = global.CSS;
+
+beforeAll(() => {
   // @ts-ignore
   global.CSS = {
     supports() {
       return true;
     },
   };
+});
 
-  const withNativeBlur = mount(<BlurView tint={'light'} />);
+afterAll(() => {
+  // @ts-ignore
+  global.CSS = originalCSS;
+});
+
+it(`uses a transparent background color when filters aren't supported`, () => {
+  const withNativeBlur = mount(<BlurView tint="light" />);
   expect(getStyleProp(withNativeBlur.find('div'), 'WebkitBackdropFilter')).toBeDefined();
   expect(getStyleProp(withNativeBlur.find('div'), 'backdropFilter')).toBeDefined();
 
   // @ts-ignore
   global.CSS = undefined;
 
-  const withoutNativeBlur = mount(<BlurView tint={'light'} />);
+  const withoutNativeBlur = mount(<BlurView tint="light" />);
   expect(getStyleProp(withoutNativeBlur.find('div'), 'WebkitBackdropFilter')).not.toBeDefined();
   expect(getStyleProp(withoutNativeBlur.find('div'), 'backdropFilter')).not.toBeDefined();
   expect(getStyleProp(withoutNativeBlur.find('div'), 'backgroundColor')).toBeDefined();
-
-  // @ts-ignore
-  global.CSS = originalCSS;
 });
