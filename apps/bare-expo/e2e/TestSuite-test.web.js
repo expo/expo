8cb96155f6651faccc3b240fa6ad68708d729bf6
Evan Bacon <baconbrix@gmail.com>
2019-09-17 18:24:08 -0700

    universal core tests (#5587)
    * Run core tests on web/native e2e
    
    * Use universal testing for expo-file-system
    
    * Add universal tests to expo-font
    
    * Update TestSuite-test.native.js
    
    * Removed extra pods
    
    * Update TestUtils.js
    
    * Update SelectScreen.js
    
    * Added tests back
    
    * Removed SVG

--- apps/bare-expo/e2e/TestSuite-test.web.js
+++ apps/bare-expo/e2e/TestSuite-test.web.js
@@ -3,17 +3,33 @@ import { setDefaultOptions } from 'expect-puppeteer';
 
 import config from '../jest-puppeteer.config';
 import { expectResults } from './utils/report';
+import { runTestsAsync } from './Utils';
 
 const TESTS = [
-  'Basic',
-  'Constants',
-  'Contacts',
-  'Crypto',
-  //   'Haptics',
-  'Localization',
-  //   'SecureStore',
-  'Random',
-  //   'Permissions',
+  {
+    name: 'Sanity',
+    tests: ['Basic'],
+  },
+  {
+    name: 'Core',
+    tests: ['Asset', 'Constants', 'FileSystem', 'Font', 'Permissions'],
+  },
+  {
+    name: 'API',
+    tests: ['Localization', 'SecureStore', 'Contacts', 'Random', 'Crypto'],
+  },
+  // {
+  //   name: 'Components',
+  //   tests: [
+  //     'GLView',
+  //   ]
+  // },
+  // {
+  //   name: 'Third-Party',
+  //   tests: [
+  //     'Segment',
+  //   ]
+  // }
 ];
 
 // This is how long we allocate for the actual tests to be run after the test screen has mounted.
@@ -28,34 +44,28 @@ function matchID(id, ...props) {
   return expect(page).toMatchElement(`div[data-testid="${id}"]`, ...props);
 }
 
-describe('test-suite', () => {
-  TESTS.map(testName => {
-    it(
-      `passes ${testName}`,
-      async () => {
-        /// Pause the timeout
-        // await jestPuppeteer.debug();
+// Add some extra time for page content loading (page.goto) and parsing the DOM
+runTestsAsync(TESTS, (MIN_TIME + RENDER_MOUNTING_TIMEOUT) * 1.2, async ({ testName }) => {
+  /// Pause the timeout
 
-        await page.goto(`${config.url}/test-suite/select/${testName}`);
+  await page.goto(`${config.url}/test-suite/select/${testName}`);
+  // await jestPuppeteer.debug();
+  // console.log(testName);
 
-        // Ensure the app linked to the testing screen (give it 100ms for navigation mounting)
-        await matchID('test_suite_container', { visible: true, timeout: RENDER_MOUNTING_TIMEOUT });
-        // Wait for the final result to be rendered. This means all the tests have finished.
-        await matchID('test_suite_final_results', { visible: true, timeout: MIN_TIME });
-        // Get the DOM element matching the testID prop.
-        // This text will contain the final results (parity with native)
-        const element = await page.$(`div[data-testid="test_suite_final_results"]`);
-        // Read the text contents and parse them as JSON
-        const input = await (await element.getProperty('textContent')).jsonValue();
+  // Ensure the app linked to the testing screen (give it 100ms for navigation mounting)
+  await matchID('test_suite_container', { visible: true, timeout: RENDER_MOUNTING_TIMEOUT });
+  // Wait for the final result to be rendered. This means all the tests have finished.
+  await matchID('test_suite_final_results', { visible: true, timeout: MIN_TIME });
+  // Get the DOM element matching the testID prop.
+  // This text will contain the final results (parity with native)
+  const element = await page.$(`div[data-testid="test_suite_final_results"]`);
+  // Read the text contents and parse them as JSON
+  const input = await (await element.getProperty('textContent')).jsonValue();
 
-        // Parse, expect, and print the results of our tests
-        expectResults({
-          testName,
-          input,
-        });
-      },
-      // Add some extra time for page content loading (page.goto) and parsing the DOM
-      (MIN_TIME + RENDER_MOUNTING_TIMEOUT) * 1.2
-    );
+  console.log(input);
+  // Parse, expect, and print the results of our tests
+  expectResults({
+    testName,
+    input,
   });
 });
