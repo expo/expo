451e5f24132e32a5527443bc5ab550e1811c95c2
Evan Bacon <baconbrix@gmail.com>
2019-09-17 19:27:00 -0700

    Update report.js

--- apps/bare-expo/e2e/utils/report.js
+++ apps/bare-expo/e2e/utils/report.js
@@ -10,10 +10,11 @@ export function reportMagic({ testName, failed, results, failures }) {
       .split(` ${testName} `)
       .join(chalk.magenta.bold(` ${testName} `))
       .replace(/toBe:\s/g, chalk.bold.green('toBe: '));
-  console.log(chalk.bgMagenta.bold.black(`\n RESULTS \n\n`), formatResults(results));
+
+  console.log(`\n${chalk.bgMagenta.bold.black(` RESULTS `)}\n\n${formatResults(results)}`);
 
   if (failed > 0) {
-    console.log(chalk.bgRed.bold.black('\n FAILED \n\n'), formatResults(failures));
+    console.log(`\n${chalk.bgRed.bold.black(` FAILED `)}\n\n${formatResults(failures)}`);
   }
 }
 
