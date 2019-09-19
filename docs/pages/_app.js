2168cc43304dd433f1817f48cb69dd01116caed3
Evan Bacon <baconbrix@gmail.com>
2019-09-19 14:32:51 -0700

    Updated docs to next@9

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,6 +1,6 @@
-import React from 'react'
 import * as Sentry from '@sentry/browser';
-import App, { Container } from 'next/app'
+import App from 'next/app';
+import React from 'react';
 
 Sentry.init({dsn: "https://67e35a01698649d5aa33aaab61777851@sentry.io/1526800"});
 
@@ -9,9 +9,7 @@ class MyApp extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
         <Component {...pageProps} />
-      </Container>
     )
   }
 }
