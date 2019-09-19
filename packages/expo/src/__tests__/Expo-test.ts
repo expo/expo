11fa39eb588ac74bc522cbffc1c8c14adcc9be75
James Ide <ide@expo.io>
2019-09-19 10:59:46 -0700

    [ci] Induce intentional failure to test and debug Slack notifications

--- packages/expo/src/__tests__/Expo-test.ts
+++ packages/expo/src/__tests__/Expo-test.ts
@@ -108,6 +108,10 @@ describe(`importing Expo`, () => {
     jest.resetModules();
   });
 
+  it(`intentionally fails`, () => {
+    throw new Error('intentional failure');
+  });
+
   it(`throws a clear error in bare React Native`, () => {
     const clearPropertiesInPlace = aThing => {
       const propertyNames = Object.keys(aThing);
