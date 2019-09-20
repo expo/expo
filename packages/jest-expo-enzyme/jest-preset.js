db6a56ea4b05d4840e4226adabb3ca50919d11f2
Evan Bacon <baconbrix@gmail.com>
2019-09-19 23:29:39 -0700

    Update jest-preset.js

--- packages/jest-expo-enzyme/jest-preset.js
+++ packages/jest-expo-enzyme/jest-preset.js
@@ -1,5 +1,5 @@
 const { withWatchPlugins } = require('jest-expo/config');
-const { withEnzyme } = require('./');
+const { withEnzyme } = require('.');
 
 module.exports = withWatchPlugins({
   projects: [
