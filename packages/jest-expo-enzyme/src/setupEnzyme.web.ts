eeafa6c2723f3b3ca5e3d286866ab218a7c1e98a
Evan Bacon <baconbrix@gmail.com>
2019-09-20 00:01:30 -0700

    updated types

--- packages/jest-expo-enzyme/src/setupEnzyme.web.ts
+++ packages/jest-expo-enzyme/src/setupEnzyme.web.ts
@@ -2,8 +2,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 import serializer from './serializer';
 
-declare const global: { expect: any };
-
 Enzyme.configure({ adapter: new Adapter() });
 
-global.expect.addSnapshotSerializer(serializer);
+// @ts-ignore: test types are not available in src/
+expect.addSnapshotSerializer(serializer);
