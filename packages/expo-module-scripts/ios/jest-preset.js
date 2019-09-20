56df16d6c91d8fe464b3a9a8ebe40df447612a12
Evan Bacon <baconbrix@gmail.com>
2019-09-19 22:57:50 -0700

    Update jest-preset.js

--- packages/expo-module-scripts/ios/jest-preset.js
+++ packages/expo-module-scripts/ios/jest-preset.js
@@ -1,7 +1,6 @@
-const { withWatchPlugins } = require('jest-expo/config');
 const createJestPreset = require('../createJestPreset');
 
 console.warn(
   'The Jest preset "expo-module-scripts/ios" is deprecated, please convert your tests to universal tests and use "expo-module-scripts" instead'
 );
-module.exports = withWatchPlugins(createJestPreset(require('jest-expo/ios/jest-preset')));
+module.exports = createJestPreset(require('jest-expo/ios/jest-preset'));
