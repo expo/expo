92dab9df61c9990a348d2ee6a2c1292e40d9d06c
Evan Bacon <baconbrix@gmail.com>
2019-09-20 18:54:28 -0700

    [jest] Convert jest-expo-enzyme to TypeScript (#5677)
    * Convert jest-expo-enzyme to TypeScript
    
    * Remove use-strict
    
    * Update yarn.lock
    
    * Fixed dependencies
    
    * lint
    
    * Update jest-preset.js
    
    * Update package.json
    
    * updated types
    
    * Removed ts-ignores
    
    * Fixed jest tests
    
    * Update yarn.lock
    
    * Fixed failing tests
    
    * jest-expo-enzyme blur
    
    * Added jest-expo-enzyme build files

--- packages/jest-expo-enzyme/src/setupEnzyme.web.ts
+++ packages/jest-expo-enzyme/src/setupEnzyme.web.ts
@@ -4,4 +4,5 @@ import serializer from './serializer';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+// @ts-ignore: test types are not available in src/
 expect.addSnapshotSerializer(serializer);
