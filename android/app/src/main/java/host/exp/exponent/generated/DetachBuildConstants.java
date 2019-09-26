3770ec58e5fdc521c025efe5ac4c16541bb3e3a2
Stanisław Chmiela <sjchmiela@gmail.com>
2019-09-26 11:26:45 +0200

    copy bare template to android/bare, reconfigure Gradle configuration

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "http://192.168.90.22:19000";
 
 }
---
+++ android/bare/src/main/java/com/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.helloworld;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "HelloWorld";
+    }
+
+//    @Override
+//    protected ReactActivityDelegate createReactActivityDelegate() {
+//        return new ReactActivityDelegate(this, getMainComponentName()) {
+//            @Override
+//            protected ReactRootView createRootView() {
+//                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+//            }
+//        };
+//    }
+}
