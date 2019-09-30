b76adaff18e5c33040007db0ec232535dc0cfbff
Eric Samelson <esamelson@users.noreply.github.com>
2019-09-30 16:29:13 -0700

    [tools][expotools] move Android SDK versioning script to expotools (#5787)

--- tools/expotools/src/versioning/android/index.ts
+++ tools/expotools/src/versioning/android/index.ts
@@ -1,13 +1,19 @@
-import path from 'path';
-import fs from 'fs-extra';
 import chalk from 'chalk';
-import semver from 'semver';
+import fs from 'fs-extra';
 import glob from 'glob-promise';
+import inquirer from 'inquirer';
+import path from 'path';
+import semver from 'semver';
+import spawnAsync from '@expo/spawn-async';
 
-import { getExpoRepositoryRootDir, getAndroidDir } from '../../Directories';
+import { JniLibNames, getJavaPackagesToRename } from './libraries';
+import * as Directories from '../../Directories';
+import { getListOfPackagesAsync } from '../../Packages';
 
-const EXPO_DIR = getExpoRepositoryRootDir();
-const ANDROID_DIR = getAndroidDir();
+const EXPO_DIR = Directories.getExpoRepositoryRootDir();
+const ANDROID_DIR = Directories.getAndroidDir();
+const EXPOTOOLS_DIR = Directories.getExpotoolsDir();
+const SCRIPT_DIR = path.join(EXPOTOOLS_DIR, 'src/versioning/android');
 
 const appPath = path.join(ANDROID_DIR, 'app');
 const expoviewPath = path.join(ANDROID_DIR, 'expoview');
@@ -23,6 +29,12 @@ const sdkVersionsPath = path.join(ANDROID_DIR, 'sdkVersions.json');
 const rnActivityPath = path.join(expoviewPath, 'src/main/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java');
 const expoviewConstantsPath = path.join(expoviewPath, 'src/main/java/host/exp/exponent/Constants.java');
 const testSuiteTestsPath = path.join(appPath, 'src/androidTest/java/host/exp/exponent/TestSuiteTests.java');
+const reactAndroidPath = path.join(ANDROID_DIR, 'ReactAndroid');
+const reactCommonPath = path.join(ANDROID_DIR, 'ReactCommon');
+const versionedReactAndroidPath = path.join(ANDROID_DIR, 'versioned-react-native/ReactAndroid');
+const versionedReactAndroidJniPath = path.join(versionedReactAndroidPath, 'src/main');
+const versionedReactAndroidJavaPath = path.join(versionedReactAndroidJniPath, 'java');
+const versionedReactCommonPath = path.join(ANDROID_DIR, 'versioned-react-native/ReactCommon');
 
 async function transformFileAsync(filePath: string, regexp: RegExp, replacement: string = '') {
   const fileContent = await fs.readFile(filePath, 'utf8');
@@ -171,4 +183,323 @@ export async function removeVersionAsync(version: string) {
       chalk.yellow(`Please review all of these references and remove them manually if possible!\n`),
     );
   }
+}
+
+function renameLib(lib: string, abiVersion: string) {
+  for (let i = 0; i < JniLibNames.length; i++) {
+    if (lib.endsWith(JniLibNames[i])) {
+      return `${lib}_abi${abiVersion}`;
+    }
+  }
+
+  return lib;
+}
+
+function processLine(line: string, abiVersion: string) {
+  if (
+    line.startsWith('LOCAL_MODULE') ||
+    line.startsWith('LOCAL_SHARED_LIBRARIES') ||
+    line.startsWith('LOCAL_STATIC_LIBRARIES')
+  ) {
+    let splitLine = line.split(':=');
+    let libs = splitLine[1].split(' ');
+    for (let i = 0; i < libs.length; i++) {
+      libs[i] = renameLib(libs[i], abiVersion);
+    }
+    splitLine[1] = libs.join(' ');
+    line = splitLine.join(':=');
+  }
+
+  return line;
+}
+
+async function processMkFileAsync(filename: string, abiVersion: string) {
+  let file = await fs.readFile(filename);
+  let fileString = file.toString();
+  await fs.truncate(filename, 0);
+  let lines = fileString.split('\n');
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    line = processLine(line, abiVersion);
+    await fs.appendFile(filename, `${line}\n`);
+  }
+}
+
+async function processJavaCodeAsync(libName: string, abiVersion: string) {
+  return spawnAsync(
+    `find ${versionedReactAndroidJavaPath} -iname '*.java' -type f -print0 | ` +
+    `xargs -0 sed -i '' 's/"${libName}"/"${libName}_abi${abiVersion}"/g'`,
+    [],
+    { shell: true }
+  );
+}
+
+async function updateVersionedReactNativeAsync() {
+  await fs.remove(versionedReactAndroidPath);
+  await fs.remove(versionedReactCommonPath);
+  await fs.copy(reactAndroidPath, versionedReactAndroidPath);
+  await fs.copy(reactCommonPath, versionedReactCommonPath);
+}
+
+async function renameJniLibsAsync(version: string) {
+  const abiVersion = version.replace(/\./g, '_');
+
+  // Update JNI methods
+  const packagesToRename = await getJavaPackagesToRename();
+  for (const javaPackage of packagesToRename) {
+    const pathForPackage = javaPackage.replace(/\./g, '\\/');
+    await spawnAsync(
+      `find ${versionedReactCommonPath} ${versionedReactAndroidJniPath} -type f ` +
+      `\\( -name \*.java -o -name \*.h -o -name \*.cpp -o -name \*.mk \\) -print0 | ` +
+      `xargs -0 sed -i '' 's/${pathForPackage}/abi${abiVersion}\\/${pathForPackage}/g'`,
+      [],
+      { shell: true }
+    );
+  }
+
+  // Update LOCAL_MODULE, LOCAL_SHARED_LIBRARIES, LOCAL_STATIC_LIBRARIES fields in .mk files
+  let [reactCommonMkFiles, reactAndroidMkFiles] = await Promise.all([
+    glob(path.join(versionedReactCommonPath, '**/*.mk')),
+    glob(path.join(versionedReactAndroidJniPath, '**/*.mk')),
+  ]);
+  let filenames = [...reactCommonMkFiles, ...reactAndroidMkFiles];
+  await Promise.all(filenames.map(filename => processMkFileAsync(filename, abiVersion)));
+
+  // Rename references to JNI libs in Java code
+  for (let i = 0; i < JniLibNames.length; i++) {
+    let libName = JniLibNames[i];
+    await processJavaCodeAsync(libName, abiVersion);
+  }
+
+  // 'fbjni' is loaded without the 'lib' prefix in com.facebook.jni.Prerequisites
+  await processJavaCodeAsync('fbjni', abiVersion);
+  await processJavaCodeAsync('fb', abiVersion);
+
+  console.log('\nThese are the JNI lib names we modified:');
+  await spawnAsync(
+    `find ${versionedReactAndroidJavaPath} -name "*.java" | xargs grep -i "_abi${abiVersion}"`,
+    [],
+    { shell: true, stdio: 'inherit' }
+  );
+
+  console.log('\nAnd here are all instances of loadLibrary:');
+  await spawnAsync(
+    `find ${versionedReactAndroidJavaPath} -name "*.java" | xargs grep -i "loadLibrary"`,
+    [],
+    { shell: true, stdio: 'inherit' }
+  );
+
+  const { isCorrect } = await inquirer.prompt<{ isCorrect: boolean }>([
+    {
+      type: 'confirm',
+      name: 'isCorrect',
+      message: 'Does all that look correct?',
+      default: false,
+    },
+  ]);
+  if (!isCorrect) {
+    throw new Error('Fix JNI libs');
+  }
+}
+
+async function copyUnimodulesAsync(version: string) {
+  const packages = await getListOfPackagesAsync();
+  for (const pkg of packages) {
+    if (
+      pkg.isUnimodule &&
+      pkg.isSupportedOnPlatform('android') &&
+      pkg.isIncludedInExpoClientOnPlatform('android') &&
+      pkg.isVersionableOnPlatform('android')
+    ) {
+      await spawnAsync(
+        './android-copy-unimodule.sh',
+        [version, path.join(pkg.path, pkg.androidSubdirectory)],
+        {
+          shell: true,
+          cwd: SCRIPT_DIR,
+        }
+      );
+      console.log(`   ✅  Created versioned ${pkg.packageName}`)
+    }
+  }
+}
+
+async function addVersionedActivitesToManifests(version: string) {
+  const abiVersion = version.replace(/\./g, '_');
+  const abiName = `abi${abiVersion}`;
+  const majorVersion = semver.major(version);
+
+  const versionedAndroidManifestPath = path.join(
+    versionedExpoviewAbiPath(abiName),
+    'src/main/AndroidManifest.xml'
+  );
+
+  await transformFileAsync(
+    versionedAndroidManifestPath,
+    new RegExp('<!-- ADD_VERSIONED_APP_AUTH_ACTIVITY_HERE -->'),
+    `<activity
+      android:name="${abiName}.expo.modules.appauth.AppAuthBrowserActivity"
+      android:exported="false"
+      android:launchMode="singleTask"
+      android:theme="@android:style/Theme.Translucent.NoTitleBar"/>`
+  );
+
+  await transformFileAsync(
+    templateManifestPath,
+    new RegExp('<!-- ADD DEV SETTINGS HERE -->'),
+    `<!-- ADD DEV SETTINGS HERE -->
+    <!-- BEGIN_SDK_${majorVersion} -->
+    <activity android:name="${abiName}.com.facebook.react.devsupport.DevSettingsActivity"/>
+    <!-- END_SDK_${majorVersion} -->`
+  );
+
+  await transformFileAsync(
+    templateManifestPath,
+    new RegExp('<!-- Versioned Activity for Stripe -->'),
+    `<!-- Versioned Activity for Stripe -->
+    <!-- BEGIN_SDK_${majorVersion} -->
+    <activity
+      android:exported="true"
+      android:launchMode="singleTask"
+      android:name="${abiName}.expo.modules.payments.stripe.RedirectUriReceiver"
+      android:theme="@android:style/Theme.Translucent.NoTitleBar.Fullscreen">
+      <intent-filter>
+        <action android:name="android.intent.action.VIEW" />
+        <category android:name="android.intent.category.DEFAULT" />
+        <category android:name="android.intent.category.BROWSABLE" />
+        <data android:scheme="${abiName}.expo.modules.payments.stripe" />
+      </intent-filter>
+    </activity>
+    <!-- END_SDK_${majorVersion} -->`
+  );
+}
+
+async function cleanUpAsync(version: string) {
+  const abiVersion = version.replace(/\./g, '_');
+  const abiName = `abi${abiVersion}`;
+
+  const versionedAbiSrcPath = path.join(
+    versionedExpoviewAbiPath(abiName),
+    'src/main/java',
+    abiName
+  );
+
+  let filesToDelete: string[] = [];
+
+  // delete PrintDocumentAdapter*Callback.java
+  // their package is `android.print` and therefore they are not changed by the versioning script
+  // so we will have duplicate classes
+  const printCallbackFiles = await glob(path.join(
+    versionedAbiSrcPath,
+    'expo/modules/print/*Callback.java'
+  ));
+  for (const file of printCallbackFiles) {
+    const contents = await fs.readFile(file, 'utf8');
+    if (!contents.includes(`package ${abiName}`)) {
+      filesToDelete.push(file);
+    } else {
+      console.log(`Skipping deleting ${file} because it appears to have been versioned`);
+    }
+  }
+
+  // delete versioned loader providers since we don't need them
+  filesToDelete.push(path.join(versionedAbiSrcPath, 'expo/loaders'));
+
+  console.log('Deleting the following files and directories:');
+  console.log(filesToDelete);
+
+  for (const file of filesToDelete) {
+    await fs.remove(file);
+  }
+
+  // misc fixes for versioned code
+  const versionedExponentPackagePath = path.join(
+    versionedAbiSrcPath,
+    'host/exp/exponent/ExponentPackage.java'
+  );
+  await transformFileAsync(
+    versionedExponentPackagePath,
+    new RegExp('// WHEN_VERSIONING_REMOVE_FROM_HERE', 'g'),
+    '/* WHEN_VERSIONING_REMOVE_FROM_HERE'
+  );
+  await transformFileAsync(
+    versionedExponentPackagePath,
+    new RegExp('// WHEN_VERSIONING_REMOVE_TO_HERE', 'g'),
+    'WHEN_VERSIONING_REMOVE_TO_HERE */'
+  );
+
+  await transformFileAsync(
+    path.join(versionedAbiSrcPath, 'host/exp/exponent/VersionedUtils.java'),
+    new RegExp('// DO NOT EDIT THIS COMMENT - used by versioning scripts[^,]+,[^,]+,'),
+    'null, null,'
+  );
+
+  await transformFileAsync(
+    path.join(versionedAbiSrcPath, 'expo/modules/payments/stripe/PayFlow.java'),
+    new RegExp('// ADD BUILDCONFIG IMPORT HERE'),
+    `import ${abiName}.host.exp.expoview.BuildConfig;`
+  );
+
+  // replace abixx_x_x...R with abixx_x_x.host.exp.expoview.R
+  await spawnAsync(
+    `find ${versionedAbiSrcPath} -iname '*.java' -type f -print0 | ` +
+    `xargs -0 sed -i '' 's/import ${abiName}\.[^;]*\.R;/import ${abiName}.host.exp.expoview.R;/g'`,
+    [],
+    { shell: true }
+  );
+
+  // add new versioned maven to build.gradle
+  await transformFileAsync(
+    buildGradlePath,
+    new RegExp('// For old expoviews to work'),
+    `// For old expoviews to work
+    maven {
+      url "$rootDir/versioned-abis/expoview-${abiName}/maven"
+    }`
+  );
+}
+
+export async function addVersionAsync(version: string) {
+  console.log(' 🛠   1/7: Updating android/versioned-react-native...');
+  await updateVersionedReactNativeAsync();
+  console.log(' ✅  1/7: Finished\n\n');
+
+  console.log(' 🛠   2/7: Renaming JNI libs in android/versioned-react-native...');
+  await renameJniLibsAsync(version);
+  console.log(' ✅  2/7: Finished\n\n');
+
+  console.log(' 🛠   3/7: Building versioned ReactAndroid AAR...');
+  await spawnAsync(
+    './android-build-aar.sh',
+    [version],
+    {
+      shell: true,
+      cwd: SCRIPT_DIR,
+      stdio: 'inherit',
+    }
+  );
+  console.log(' ✅  3/7: Finished\n\n');
+
+  console.log(' 🛠   4/7: Creating versioned expoview package...');
+  await spawnAsync(
+    './android-copy-expoview.sh',
+    [version],
+    {
+      shell: true,
+      cwd: SCRIPT_DIR,
+    }
+  );
+  console.log(' ✅  4/7: Finished\n\n');
+
+  console.log(' 🛠   5/7: Creating versioned unimodule packages...');
+  await copyUnimodulesAsync(version);
+  console.log(' ✅  5/7: Finished\n\n');
+
+  console.log(' 🛠   6/7: Adding extra versioned activites to AndroidManifest...');
+  await addVersionedActivitesToManifests(version);
+  console.log(' ✅  6/7: Finished\n\n');
+
+  console.log(' 🛠   7/7: Misc cleanup...');
+  await cleanUpAsync(version);
+  console.log(' ✅  7/7: Finished');
 }
--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -4,26 +4,11 @@ const gulp = require('gulp');
 const { resolve } = require('path');
 const shell = require('gulp-shell');
 const argv = require('minimist')(process.argv.slice(2));
-const { Modules } = require('@expo/xdl');
-const chalk = require('chalk');
 const fs = require('fs-extra');
 
-const { renameJNILibsAsync } = require('./android-tasks');
 const outdatedVendoredNativeModules = require('./outdated-vendored-native-modules');
 const AndroidExpolib = require('./android-versioning/android-expolib');
 const androidVersionLibraries = require('./android-versioning/android-version-libraries');
-
-function renameJNILibsWithABIArgument() {
-  if (!argv.abi) {
-    throw new Error('Must run with `--abi ABI_VERSION`');
-  }
-
-  return renameJNILibsAsync(argv.abi);
-}
-
-function runShellScriptWithABIArgument(script) {
-  return gulp.series('assert-abi-argument', shell.task([`${script} ${argv.abi}`]));
-}
 
 gulp.task('assert-abi-argument', function(done) {
   if (!argv.abi) {
@@ -41,40 +26,6 @@ gulp.task(
       AndroidExpolib.namespaceExpolibImportsAsync,
       AndroidExpolib.namespaceExpolibGradleDependenciesAsync
     )
-  )
-);
-
-// Versioning (android)
-gulp.task(
-  'android-update-versioned-rn',
-  shell.task([
-    'rm -rf ../android/versioned-react-native/{ReactAndroid,ReactCommon}',
-    'cp -r ../android/ReactCommon ../android/versioned-react-native/ReactCommon',
-    'cp -r ../android/ReactAndroid ../android/versioned-react-native/ReactAndroid',
-  ])
-);
-gulp.task('android-rename-jni-libs', renameJNILibsWithABIArgument);
-gulp.task('android-build-aar', runShellScriptWithABIArgument('./android-build-aar.sh'));
-gulp.task(
-  'android-copy-native-modules',
-  runShellScriptWithABIArgument('./android-copy-native-modules.sh')
-);
-gulp.task(
-  'android-copy-universal-modules',
-  gulp.series(...Modules.getVersionableModulesForPlatform('android', argv.abi || 'UNVERSIONED').map(
-    module => shell.task([
-      `./android-copy-universal-module.sh ${argv.abi} ../packages/${module.libName}/${module.subdirectory}`
-    ])
-  ))
-);
-gulp.task(
-  'android-add-rn-version',
-  gulp.series(
-    'android-update-versioned-rn',
-    'android-rename-jni-libs',
-    'android-build-aar',
-    'android-copy-native-modules',
-    'android-copy-universal-modules'
   )
 );
 
