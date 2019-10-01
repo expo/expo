c147efa96a33f8769eee551a19bf9fc1dc027067
Stanisław Chmiela <sjchmiela@gmail.com>
2019-10-01 09:36:35 +0200

    Replace an array with a set

--- packages/expo-notifications/ios/EXNotifications/AppDelegate/EXNotificationTokenManager.m
+++ packages/expo-notifications/ios/EXNotifications/AppDelegate/EXNotificationTokenManager.m
@@ -5,7 +5,7 @@
 
 @interface EXNotificationTokenManager ()
 
-@property (nonatomic, strong) NSPointerArray *listeners;
+@property (nonatomic, strong) NSHashTable<id<UMNotificationTokenListener>> *listeners;
 
 @end
 
@@ -16,7 +16,7 @@ UM_REGISTER_SINGLETON_MODULE(NotificationTokenManager);
 - (instancetype)init
 {
   if (self = [super init]) {
-    _listeners = [NSPointerArray weakObjectsPointerArray];
+    _listeners = [NSHashTable weakObjectsHashTable];
   }
   return self;
 }
@@ -24,16 +24,14 @@ UM_REGISTER_SINGLETON_MODULE(NotificationTokenManager);
 # pragma mark - UIApplicationDelegate
 
 - (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-  for (int i = 0; i < _listeners.count; i++) {
-    id<UMNotificationTokenListener> pointer = [_listeners pointerAtIndex:i];
-    [pointer onNewToken:deviceToken];
+  for (id<UMNotificationTokenListener> listener in [_listeners setRepresentation]) {
+    [listener onNewToken:deviceToken];
   }
 }
 
 - (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-  for (int i = 0; i < _listeners.count; i++) {
-    id<UMNotificationTokenListener> pointer = [_listeners pointerAtIndex:i];
-    [pointer onFailedToRegisterWithError:error];
+  for (id<UMNotificationTokenListener> listener in [_listeners setRepresentation]) {
+    [listener onFailedToRegisterWithError:error];
   }
 }
 
@@ -41,21 +39,12 @@ UM_REGISTER_SINGLETON_MODULE(NotificationTokenManager);
 
 - (void)addListener:(id<UMNotificationTokenListener>)listener
 {
-  [_listeners addPointer:(__bridge void * _Nullable)(listener)];
+  [_listeners addObject:listener];
 }
 
 - (void)removeListener:(id<UMNotificationTokenListener>)listener
 {
-  for (int i = 0; i < _listeners.count; i++) {
-    id pointer = [_listeners pointerAtIndex:i];
-    if (pointer == (__bridge void * _Nullable)(listener) || !pointer) {
-      [_listeners removePointerAtIndex:i];
-      i--;
-    }
-  }
-  // compact doesn't work, that's why we need the `|| !pointer` above
-  // http://www.openradar.me/15396578
-  [_listeners compact];
+  [_listeners removeObject:listener];
 }
 
 @end
