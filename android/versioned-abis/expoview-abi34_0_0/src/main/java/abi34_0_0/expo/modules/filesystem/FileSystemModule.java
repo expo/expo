47985588fcbd13789c473092da2af43f28d285f7
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2019-10-02 17:55:56 +0200

    [file-system][Android] Backport changes

--- android/versioned-abis/expoview-abi34_0_0/src/main/java/abi34_0_0/expo/modules/filesystem/FileSystemModule.java
+++ android/versioned-abis/expoview-abi34_0_0/src/main/java/abi34_0_0/expo/modules/filesystem/FileSystemModule.java
@@ -723,6 +723,7 @@ public class FileSystemModule extends ExportedModule {
         result.putInt("status", response.code());
         result.putBundle("headers", translateHeaders(response.headers()));
 
+        response.close();
         promise.resolve(result);
         return null;
       } catch (Exception e) {
