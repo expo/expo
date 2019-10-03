36da93c66c43a99863343a9744c068ccdf5ea1e6
Adam Navarro <adamnavarrowisc@gmail.com>
2019-10-03 13:10:34 -0500

    Work in progress expo-cli docs formatting

--- docs/global-styles/extras.js
+++ docs/global-styles/extras.js
@@ -13,4 +13,34 @@ export const globalExtras = `
   .react-player > video {
     outline: none;
   }
+
+
+  details {
+    margin-bottom: 20px;
+    padding-top: 20px;
+  }
+
+  details summary {
+    outline: none;
+    cursor: pointer;
+    margin-bottom: 15px;
+  }
+
+  details summary h3 {
+    font-size: 1.2rem;
+    font-family: expo-brand-bold,sans-serif;
+    display: inline;
+  }
+
+  details summary p {
+    margin-top: 10px;
+  }
+
+  details summary:hover {
+    opacity: 0.7;
+  }
+
+  details p {
+    margin-left: 15px;
+  }
 `;
