a7a94c87ded37d116dfcb581b64e395264679897
Amaury Martiny <amaury.martiny@protonmail.com>
2019-10-04 22:00:09 +0200

    Use partial TS interface

--- packages/expo-analytics-amplitude/build/Amplitude.d.ts
+++ packages/expo-analytics-amplitude/build/Amplitude.d.ts
@@ -1,20 +1,20 @@
 interface TrackingOptions {
-    disableCarrier: boolean;
-    disableCity: boolean;
-    disableCountry: boolean;
-    disableDeviceManufacturer: boolean;
-    disableDeviceModel: boolean;
-    disableDMA: boolean;
-    disableIDFA: boolean;
-    disableIDFV: boolean;
-    disableIPAddress: boolean;
-    disableLanguage: boolean;
-    disableLatLng: boolean;
-    disableOSName: boolean;
-    disableOSVersion: boolean;
-    disablePlatform: boolean;
-    disableRegion: boolean;
-    disableVersionName: boolean;
+    disableCarrier?: boolean;
+    disableCity?: boolean;
+    disableCountry?: boolean;
+    disableDeviceManufacturer?: boolean;
+    disableDeviceModel?: boolean;
+    disableDMA?: boolean;
+    disableIDFA?: boolean;
+    disableIDFV?: boolean;
+    disableIPAddress?: boolean;
+    disableLanguage?: boolean;
+    disableLatLng?: boolean;
+    disableOSName?: boolean;
+    disableOSVersion?: boolean;
+    disablePlatform?: boolean;
+    disableRegion?: boolean;
+    disableVersionName?: boolean;
 }
 export declare function initialize(apiKey: string): Promise<void>;
 export declare function setUserId(userId: string): Promise<void>;
