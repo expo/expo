03a3f163fe4d0564ba1acf6795543c5a2f71e8b1
Evan Bacon <baconbrix@gmail.com>
2019-10-08 18:02:56 -0700

    Update EXSMSModule.m

--- packages/expo-sms/ios/EXSMS/EXSMSModule.m
+++ packages/expo-sms/ios/EXSMS/EXSMSModule.m
@@ -18,14 +18,14 @@ UM_EXPORT_MODULE(ExpoSMS);
 
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
 {
-    _utils = [moduleRegistry getModuleImplementingProtocol:@protocol(UMUtilitiesInterface)];
+  _utils = [moduleRegistry getModuleImplementingProtocol:@protocol(UMUtilitiesInterface)];
 }
 
 UM_EXPORT_METHOD_AS(isAvailableAsync,
                     isAvailable:(UMPromiseResolveBlock)resolve
                     rejecter:(UMPromiseRejectBlock)reject)
 {
-    resolve(@([MFMessageComposeViewController canSendText]));
+  resolve(@([MFMessageComposeViewController canSendText]));
 }
 
 UM_EXPORT_METHOD_AS(sendSMSAsync,
@@ -34,61 +34,61 @@ UM_EXPORT_METHOD_AS(sendSMSAsync,
                     resolver:(UMPromiseResolveBlock)resolve
                     rejecter:(UMPromiseRejectBlock)reject)
 {
-    if (![MFMessageComposeViewController canSendText]) {
-        reject(@"E_SMS_UNAVAILABLE", @"SMS service not available", nil);
-        return;
-    }
+  if (![MFMessageComposeViewController canSendText]) {
+    reject(@"E_SMS_UNAVAILABLE", @"SMS service not available", nil);
+    return;
+  }
+  
+  if (_resolve != nil || _reject != nil) {
+    reject(@"E_SMS_SENDING_IN_PROGRESS", @"Different SMS sending in progress. Await the old request and then try again.", nil);
+    return;
+  }
+  
+  _resolve = resolve;
+  _reject = reject;
     
-    if (_resolve != nil || _reject != nil) {
-        reject(@"E_SMS_SENDING_IN_PROGRESS", @"Different SMS sending in progress. Await the old request and then try again.", nil);
-        return;
-    }
+  MFMessageComposeViewController *messageComposeViewController = [[MFMessageComposeViewController alloc] init];
+  messageComposeViewController.messageComposeDelegate = self;
+  messageComposeViewController.recipients = addresses;
+  messageComposeViewController.body = message;
     
-    _resolve = resolve;
-    _reject = reject;
-    
-    MFMessageComposeViewController *messageComposeViewController = [[MFMessageComposeViewController alloc] init];
-    messageComposeViewController.messageComposeDelegate = self;
-    messageComposeViewController.recipients = addresses;
-    messageComposeViewController.body = message;
-    
-    UM_WEAKIFY(self);
-    [UMUtilities performSynchronouslyOnMainThread:^{
-        UM_ENSURE_STRONGIFY(self);
-        [self.utils.currentViewController presentViewController:messageComposeViewController animated:YES completion:nil];
-    }];
+  UM_WEAKIFY(self);
+  [UMUtilities performSynchronouslyOnMainThread:^{
+    UM_ENSURE_STRONGIFY(self);
+    [self.utils.currentViewController presentViewController:messageComposeViewController animated:YES completion:nil];
+  }];
 }
 
 - (void)messageComposeViewController:(MFMessageComposeViewController *)controller
                  didFinishWithResult:(MessageComposeResult)result
 {
-    NSDictionary *resolveData;
-    NSString *rejectMessage;
-    switch (result) {
-        case MessageComposeResultCancelled:
-            resolveData = @{@"result": @"cancelled"};
-            break;
-        case MessageComposeResultSent:
-            resolveData = @{@"result": @"sent"};
-            break;
-        case MessageComposeResultFailed:
-            rejectMessage = @"User's attempt to save or send an SMS was unsuccessful. This can occur when the device loses connection to Wifi or Cellular.";
-            break;
-        default:
-            rejectMessage = @"SMS message sending failed with unknown error";
-            break;
+  NSDictionary *resolveData;
+  NSString *rejectMessage;
+  switch (result) {
+    case MessageComposeResultCancelled:
+      resolveData = @{@"result": @"cancelled"};
+      break;
+    case MessageComposeResultSent:
+      resolveData = @{@"result": @"sent"};
+      break;
+    case MessageComposeResultFailed:
+      rejectMessage = @"User's attempt to save or send an SMS was unsuccessful. This can occur when the device loses connection to Wifi or Cellular.";
+      break;
+    default:
+      rejectMessage = @"SMS message sending failed with unknown error";
+      break;
+  }
+  UM_WEAKIFY(self);
+  [controller dismissViewControllerAnimated:YES completion:^{
+    UM_ENSURE_STRONGIFY(self);
+    if (rejectMessage) {
+      self->_reject(@"E_SMS_SENDING_FAILED", rejectMessage, nil);
+    } else {
+      self->_resolve(resolveData);
     }
-    UM_WEAKIFY(self);
-    [controller dismissViewControllerAnimated:YES completion:^{
-        UM_ENSURE_STRONGIFY(self);
-        if (rejectMessage) {
-            self->_reject(@"E_SMS_SENDING_FAILED", rejectMessage, nil);
-        } else {
-            self->_resolve(resolveData);
-        }
-        self->_reject = nil;
-        self->_resolve = nil;
-    }];
+    self->_reject = nil;
+    self->_resolve = nil;
+  }];
 }
 
 @end
