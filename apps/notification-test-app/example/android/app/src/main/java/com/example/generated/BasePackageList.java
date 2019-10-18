e443870ec86aeb17126c3d0cd427afa479396a37
szymon <szymon.kapala@swmansion.com>
2019-10-18 15:03:02 +0200

    remove unnecessary character from AndroidManifest.xml

---
+++ apps/notification-test-app/example/android/app/src/main/java/com/example/generated/BasePackageList.java
@@ -0,0 +1,15 @@
+package com.example.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.permissions.PermissionsPackage()
+    );
+  }
+}
