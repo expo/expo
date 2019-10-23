f78a0dbb6193c4199e149e3ee31e2ca827742527
Brad Bumbalough <brad@teamgantt.com>
2019-10-23 11:26:58 -0500

    [ios][expo-battery] isSupported false in simulator
    - Updated docs and test-suite to allow for isSupported being false.

--- apps/test-suite/tests/Battery.js
+++ apps/test-suite/tests/Battery.js
@@ -1,75 +1,92 @@
 import * as Battery from 'expo-battery';
+import * as Device from 'expo-device';
+import { Platform } from '@unimodules/core';
 
 export const name = 'Battery';
 
 export async function test({ describe, it, expect, jasmine }) {
-  describe(`getBatteryLevelAsync()`, () => {
-    it(`returns a number between 0 and 1`, async () => {
-      let batteryLevel = await Battery.getBatteryLevelAsync();
-      expect(batteryLevel).toEqual(jasmine.any(Number));
-      expect(batteryLevel).toBeLessThanOrEqual(1);
-      expect(batteryLevel).toBeGreaterThan(0);
-    });
-  });
-  describe(`getBatteryState()`, () => {
-    it(`returns a valid BatteryState enum value`, async () => {
-      const batteryState = await Battery.getBatteryStateAsync();
-      expect(
-        [
-          Battery.BatteryState.CHARGING,
-          Battery.BatteryState.FULL,
-          Battery.BatteryState.UNPLUGGED,
-        ].includes(batteryState)
-      ).toBe(true);
-    });
-  });
-  describe(`isLowPowerModeEnabledAsync()`, () => {
-    it(`returns a boolean low power mode`, async () => {
-      const lowPowerMode = await Battery.isLowPowerModeEnabledAsync();
-      expect(lowPowerMode).toEqual(jasmine.any(Boolean));
-    });
-  });
-  describe(`getPowerStateAsync()`, () => {
-    it(`returns a valid PowerState object`, async () => {
-      const powerState = await Battery.getPowerStateAsync();
-      expect(powerState).toEqual(
-        jasmine.objectContaining({
-          batteryLevel: jasmine.any(Number),
-          batteryState: jasmine.any(Number),
-          lowPowerMode: jasmine.any(Boolean),
-        })
-      );
-    });
+  const isSupported = Device.isDevice || Platform.OS === 'android';
+
+  describe(`isSupported`, () => {
+    it(
+      isSupported
+        ? `should be true for android or ios devices`
+        : `should be false on ios simulator`,
+      () => {
+        expect(Battery.isSupported).toEqual(isSupported);
+      }
+    );
   });
 
-  describe(`Event listeners`, () => {
-    // TODO(Bacon) Add detox & spies
-    // TODO: check that events don't get fired after we unsubscribe
-    // but we currently don't have a way to programmatically set battery statuses
+  if (isSupported) {
+    describe(`getBatteryLevelAsync()`, () => {
+      it(`returns a number between 0 and 1`, async () => {
+        let batteryLevel = await Battery.getBatteryLevelAsync();
+        expect(batteryLevel).toEqual(jasmine.any(Number));
+        expect(batteryLevel).toBeLessThanOrEqual(1);
+        expect(batteryLevel).toBeGreaterThan(0);
+      });
+    });
+    describe(`getBatteryState()`, () => {
+      it(`returns a valid BatteryState enum value`, async () => {
+        const batteryState = await Battery.getBatteryStateAsync();
+        expect(
+          [
+            Battery.BatteryState.CHARGING,
+            Battery.BatteryState.FULL,
+            Battery.BatteryState.UNPLUGGED,
+          ].includes(batteryState)
+        ).toBe(true);
+      });
+    });
+    describe(`isLowPowerModeEnabledAsync()`, () => {
+      it(`returns a boolean low power mode`, async () => {
+        const lowPowerMode = await Battery.isLowPowerModeEnabledAsync();
+        expect(lowPowerMode).toEqual(jasmine.any(Boolean));
+      });
+    });
+    describe(`getPowerStateAsync()`, () => {
+      it(`returns a valid PowerState object`, async () => {
+        const powerState = await Battery.getPowerStateAsync();
+        expect(powerState).toEqual(
+          jasmine.objectContaining({
+            batteryLevel: jasmine.any(Number),
+            batteryState: jasmine.any(Number),
+            lowPowerMode: jasmine.any(Boolean),
+          })
+        );
+      });
+    });
 
-    it(`addLowPowerModeListener() registers`, () => {
-      const listener = Battery.addLowPowerModeListener(({ lowPowerMode }) => {
-        console.log('powerMode changed!', lowPowerMode);
+    describe(`Event listeners`, () => {
+      // TODO(Bacon) Add detox & spies
+      // TODO: check that events don't get fired after we unsubscribe
+      // but we currently don't have a way to programmatically set battery statuses
+
+      it(`addLowPowerModeListener() registers`, () => {
+        const listener = Battery.addLowPowerModeListener(({ lowPowerMode }) => {
+          console.log('powerMode changed!', lowPowerMode);
+        });
+        // TODO: Invoke callback somehow
+        expect(listener).toBeDefined();
+        listener.remove();
       });
-      // TODO: Invoke callback somehow
-      expect(listener).toBeDefined();
-      listener.remove();
-    });
-    it(`addBatteryStateListener() registers`, () => {
-      const listener = Battery.addBatteryStateListener(({ batteryState }) => {
-        console.log('batteryState changed!', batteryState);
+      it(`addBatteryStateListener() registers`, () => {
+        const listener = Battery.addBatteryStateListener(({ batteryState }) => {
+          console.log('batteryState changed!', batteryState);
+        });
+        // TODO: Invoke callback somehow
+        expect(listener).toBeDefined();
+        listener.remove();
       });
-      // TODO: Invoke callback somehow
-      expect(listener).toBeDefined();
-      listener.remove();
-    });
-    it(`addBatteryLevelListener() registers`, () => {
-      const listener = Battery.addBatteryLevelListener(({ batteryLevel }) => {
-        console.log('batteryLevel changed!', batteryLevel);
+      it(`addBatteryLevelListener() registers`, () => {
+        const listener = Battery.addBatteryLevelListener(({ batteryLevel }) => {
+          console.log('batteryLevel changed!', batteryLevel);
+        });
+        // TODO: Invoke callback somehow
+        expect(listener).toBeDefined();
+        listener.remove();
       });
-      // TODO: Invoke callback somehow
-      expect(listener).toBeDefined();
-      listener.remove();
     });
-  });
+  }
 }
