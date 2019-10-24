c8f1311a415c8aa889b0aa3ec38d79ddb3427fff
Michał Czernek <czernekmichal@gmail.com>
2019-10-24 14:33:06 +0200

    [WIP]

--- apps/expo-ota-example-app/android/app/src/main/java/com/otabare/generated/BasePackageList.java
+++ apps/expo-ota-example-app/android/app/src/main/java/com/otabare/generated/BasePackageList.java
@@ -7,9 +7,6 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
-        new expo.modules.constants.ConstantsPackage(),
-        new expo.modules.filesystem.FileSystemPackage(),
-        new expo.modules.permissions.PermissionsPackage()
     );
   }
 }
