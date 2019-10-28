88ebfc06a5e1eaa81bee1c2e2f6d546c669ec11c
Michał Czernek <czernekmichal@gmail.com>
2019-10-28 15:10:02 +0100

    Create JS layer for expo-ota.

---
+++ packages/expo-ota/build/ExpoOTA.d.ts
@@ -0,0 +1,5 @@
+export declare function checkForUpdateAsync(): Promise<any>;
+export declare function fetchUpdatesAsync(): Promise<any>;
+export declare function reload(): Promise<any>;
+export declare function clearUpdateCacheAsync(): Promise<any>;
+export declare function readCurrentManifestAsync(): Promise<any>;
--- packages/expo-ota/src/ExpoOTA.ts
+++ packages/expo-ota/src/ExpoOTA.ts
@@ -1,2 +1,37 @@
-import { NativeModulesProxy } from '@unimodules/core';
-export default NativeModulesProxy.ExpoOTA;
+import { NativeModulesProxy, UnavailabilityError } from '@unimodules/core';
+const OTA = NativeModulesProxy.ExpoOta;
+
+export async function checkForUpdateAsync() {
+    if (!OTA.checkForUpdateAsync) {
+        throw new UnavailabilityError('WebBrowser', 'checkForUpdateAsync');
+    }
+    return OTA.checkForUpdateAsync();
+}
+
+export async function fetchUpdatesAsync() {
+    if (!OTA.fetchUpdatesAsync) {
+        throw new UnavailabilityError('WebBrowser', 'getCustomTabsSupportingBrowsersAsync');
+    }
+    return OTA.fetchUpdatesAsync();
+}
+
+export async function reload() {
+    if (!OTA.reload) {
+        throw new UnavailabilityError('WebBrowser', 'reload');
+    }
+    return OTA.reload();
+}
+
+export async function clearUpdateCacheAsync() {
+    if (!OTA.clearUpdateCacheAsync) {
+        throw new UnavailabilityError('WebBrowser', 'clearUpdateCacheAsync');
+    }
+    return OTA.clearUpdateCacheAsync();
+}
+
+export async function readCurrentManifestAsync() {
+    if (!OTA.readCurrentManifestAsync) {
+        throw new UnavailabilityError('WebBrowser', 'getCustomTabsSupportingBrowsersAsync');
+    }
+    return OTA.readCurrentManifestAsync();
+}
