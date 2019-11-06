4c680a9d3b8c84399fc131072a16d4e8fa978524
danibonilha <bonilha@work.co>
2019-11-06 15:28:39 -0300

    [ios][store-review] Refactor isSupported to isAvailableAsync

--- packages/expo-store-review/build/StoreReview.d.ts
+++ packages/expo-store-review/build/StoreReview.d.ts
@@ -1,4 +1,5 @@
-export declare function isSupported(): boolean;
+export declare function isAvailableAsync(): Promise<boolean>;
+export declare function isSupported(): void;
 export declare function requestReview(): Promise<void>;
 export declare function storeUrl(): string | null;
-export declare function hasAction(): boolean;
+export declare function hasAction(): Promise<boolean>;
