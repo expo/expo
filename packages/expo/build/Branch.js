c11c4015029158ae2a1699e10f0b78217f39339c
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2019-11-07 14:17:14 +0100

    [expo] Remove unexported Branch from expo package (#6190)
    # Why
    
    `expo` package doesn't export `Branch` anymore.
    
    # How
    
    Figured it out while preparing https://github.com/expo/expo/pull/6189. Removed `react-native-branch` dependency and `Branch{.ts,.web.ts}` files. Ran `yarn` in `/packages/expo`, the package compiled without any problems.
    
    # Test Plan
    
    TS compiled.

--- packages/expo/build/Branch.js
+++
@@ -1,6 +0,0 @@
-import Branch, { BranchEvent } from 'react-native-branch';
-// Branch has a default and named export, add `BranchEvent` to the default export so it looks
-// similar.
-Branch.BranchEvent = BranchEvent;
-export default Branch;
-//# sourceMappingURL=Branch.js.map
