339e9dab1ee41361d2dfe79bd81fa4bf2c013f04
Michał Czernek <czernekmichal@gmail.com>
2019-11-12 16:07:45 +0100

    Make unit tests runnable in expo-ota from within example app.

---
+++ packages/expo-ota/android/src/test/java/expo/modules/ota/OtaUpdaterTestCase.kt
@@ -0,0 +1,20 @@
+package expo.modules.ota
+
+import junit.framework.TestCase
+import org.junit.Test
+
+class OtaUpdaterTestCase: TestCase() {
+
+    @Test
+    fun testTests() {
+        assert(true)
+//        verify { storage.readString("") }
+//        confirmVerified(storage)
+    }
+
+    @Test
+    fun testSecond() {
+        assert(true)
+    }
+
+}
