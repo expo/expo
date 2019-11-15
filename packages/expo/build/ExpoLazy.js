fb6e74f5d7743b3da2c5fb7bcc01a7f3cc05d1fa
Brent Vatne <brentvatne@gmail.com>
2019-11-15 10:06:31 -0800

    [expo] Import Expo.fx in AppEntry (#6278)

--- packages/expo/build/ExpoLazy.js
+++ packages/expo/build/ExpoLazy.js
@@ -505,19 +505,4 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-// The following should all be extracted from this package
-// @ts-ignore
-if (module && module.exports) {
-  if (global) {
-    const globals = require('./globals'); // @ts-ignore
-
-
-    global.__exponent = globals; // @ts-ignore
-
-    global.__expo = globals; // @ts-ignore
-
-    global.Expo = globals;
-  }
-}
-
 //# sourceMappingURL=ExpoLazy.js.map
