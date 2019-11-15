680adc5728d7bca1425c8bc31becfa1f1845dbb4
Brent Vatne <brentvatne@gmail.com>
2019-11-14 21:17:24 -0800

    [test-suite] StoreReview should be available on Android

--- apps/test-suite/tests/StoreReview.js
+++ apps/test-suite/tests/StoreReview.js
@@ -1,11 +1,18 @@
 import * as StoreReview from 'expo-store-review';
+import { Platform } from 'react-native';
 
 export const name = 'StoreReview';
 
 export function test({ describe, it, expect }) {
   describe(`isAvailableAsync()`, () => {
-    it(`is not available`, async () => {
-      expect(await StoreReview.isAvailableAsync()).toBe(false);
-    });
+    if (Platform.OS === 'android') {
+      it(`is is available`, async () => {
+        expect(await StoreReview.isAvailableAsync()).toBe(true);
+      });
+    } else {
+      it(`is is not available`, async () => {
+        expect(await StoreReview.isAvailableAsync()).toBe(false);
+      });
+    }
   });
 }
