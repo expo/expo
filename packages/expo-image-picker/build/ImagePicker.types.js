ed385980a5fc5779be7c3052b4d4d3d4e52c44e9
kyletsang <6854874+kyletsang@users.noreply.github.com>
2019-11-20 11:06:46 -0800

    Update build output

--- packages/expo-image-picker/build/ImagePicker.types.js
+++ packages/expo-image-picker/build/ImagePicker.types.js
@@ -1,9 +1,9 @@
-import { PermissionStatus } from 'unimodules-permissions-interface';
+import { PermissionResponse, PermissionStatus } from 'unimodules-permissions-interface';
 export var MediaTypeOptions;
 (function (MediaTypeOptions) {
     MediaTypeOptions["All"] = "All";
     MediaTypeOptions["Videos"] = "Videos";
     MediaTypeOptions["Images"] = "Images";
 })(MediaTypeOptions || (MediaTypeOptions = {}));
-export { PermissionStatus };
+export { PermissionResponse, PermissionStatus };
 //# sourceMappingURL=ImagePicker.types.js.map
