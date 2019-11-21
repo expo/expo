41f8180b17ae360939539aeb722e09c2cef5ff38
Brent Vatne <brentvatne@gmail.com>
2019-11-21 10:44:32 -0800

    [store-review] isAvailableAsync should work on every platform (#6282)
    Co-Authored-By: James Ide <ide@users.noreply.github.com>

--- apps/test-suite/tests/StoreReview.ios.js
+++ apps/test-suite/tests/StoreReview.ios.js
@@ -1,16 +1,16 @@
 import * as StoreReview from 'expo-store-review';
-import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 
 export const name = 'StoreReview';
 
 export function test({ describe, it, expect }) {
   describe(`isAvailableAsync()`, () => {
-    if (Platform.OS === 'android') {
-      it(`is is available`, async () => {
+    if (Constants.platform.ios && Constants.platform.ios.systemVersion >= 10.3) {
+      it(`has access to iOS StoreReview API`, async () => {
         expect(await StoreReview.isAvailableAsync()).toBe(true);
       });
     } else {
-      it(`is not available`, async () => {
+      it(`is not available on previous than 10.3 iOS versions'`, async () => {
         expect(await StoreReview.isAvailableAsync()).toBe(false);
       });
     }
