06cdd747e435eac5d59376577b9a117263f71c77
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-11-22 17:46:27 +0100

    [calendar][ios] Reject when user provides an invalid time zone (#6326)
    # Why
    
    Related to https://github.com/expo/expo/issues/6302.
    
    User should know if he provided the correct time zone. Because when he called `createEventAsync()` with incorrect `time zone` setting it will cause surprising behaviour, which is hard to debug.

--- apps/test-suite/tests/Calendar.js
+++ apps/test-suite/tests/Calendar.js
@@ -35,15 +35,17 @@ async function pickCalendarSourceIdAsync() {
   }
 }
 
-async function createTestEventAsync(calendarId) {
+async function createTestEventAsync(calendarId, customArgs = {}) {
   return await Calendar.createEventAsync(calendarId, {
     title: 'App.js Conference',
     startDate: +new Date(2019, 3, 4), // 4th April 2019, months are counted from 0
     endDate: +new Date(2019, 3, 5), // 5th April 2019
+    timeZone: 'Europe/Warsaw',
     allDay: true,
     location: 'Qubus Hotel, Nadwiślańska 6, 30-527 Kraków, Poland',
     notes: 'The very first Expo & React Native conference in Europe',
     availability: Calendar.Availability.BUSY,
+    ...customArgs,
   });
 }
 
@@ -312,6 +314,19 @@ export async function test(t) {
         t.expect(eventId).toBeDefined();
         t.expect(typeof eventId).toBe('string');
       });
+
+      if (Platform.OS === 'ios') {
+        t.it('rejects when time zone is invalid', async () => {
+          let error;
+          try {
+            await createTestEventAsync(calendarId, { timeZone: '' });
+          } catch (e) {
+            error = e;
+          }
+          t.expect(error).toBeDefined();
+          t.expect(error.code).toBe('E_EVENT_INVALID_TIMEZONE');
+        });
+      }
 
       t.afterAll(async () => {
         await Calendar.deleteCalendarAsync(calendarId);
