645aab3fbefb88e449401bd64a4ebf2b51f49b20
Eric Samelson <eric@expo.io>
2019-12-03 14:03:27 -0800

    [tmp][ios] comment out RCTPackagerConnection usage in EXVersionManager which is causing shell app build to fail. revert me!!!

--- ios/Exponent/Versioned/Core/EXVersionManager.m
+++ ios/Exponent/Versioned/Core/EXVersionManager.m
@@ -21,7 +21,7 @@
 #import <React/RCTExceptionsManager.h>
 #import <React/RCTLog.h>
 #import <React/RCTRedBox.h>
-#import <React/RCTPackagerConnection.h>
+//#import <React/RCTPackagerConnection.h>
 #import <React/RCTModuleData.h>
 #import <React/RCTUtils.h>
 
@@ -112,10 +112,10 @@ void EXRegisterScopedModule(Class moduleClass, ...)
      postNotificationName:EX_UNVERSIONED(@"EXReloadActiveAppRequest") object:nil];
   }];
 
-  if ([self _isDevModeEnabledForBridge:bridge]) {
-    // Set the bundle url for the packager connection manually
-    [[RCTPackagerConnection sharedPackagerConnection] setBundleURL:[bridge bundleURL]];
-  }
+//  if ([self _isDevModeEnabledForBridge:bridge]) {
+//    // Set the bundle url for the packager connection manually
+//    [[RCTPackagerConnection sharedPackagerConnection] setBundleURL:[bridge bundleURL]];
+//  }
 
   // Manually send a "start loading" notif, since the real one happened uselessly inside the RCTBatchedBridge constructor
   [[NSNotificationCenter defaultCenter]
