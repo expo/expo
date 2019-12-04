6b4cb9736060a29232960de792cee43983eeb61d
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2019-12-04 19:56:58 +0100

    [templates] Update project templates to SDK36

--- templates/expo-template-tabs/navigation/MainTabNavigator.js
+++ templates/expo-template-tabs/navigation/MainTabNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
