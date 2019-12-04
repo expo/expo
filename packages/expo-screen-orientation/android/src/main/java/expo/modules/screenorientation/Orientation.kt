1dc3bcd1f02fa1b6e98ef4d35d82296401382fca
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-12-04 12:33:22 +0100

    [expo-screen-orientation] Rewrite to kotlin

---
+++ packages/expo-screen-orientation/android/src/main/java/expo/modules/screenorientation/Orientation.kt
@@ -0,0 +1,11 @@
+package expo.modules.screenorientation
+
+enum class Orientation {
+  PORTRAIT,
+  PORTRAIT_UP,
+  PORTRAIT_DOWN,
+  LANDSCAPE,
+  LANDSCAPE_LEFT,
+  LANDSCAPE_RIGHT,
+  UNKNOWN
+}
---
+++ packages/expo-screen-orientation/android/src/main/java/expo/modules/screenorientation/OrientationLock.kt
@@ -0,0 +1,15 @@
+package expo.modules.screenorientation
+
+enum class OrientationLock {
+  DEFAULT,
+  ALL,
+  PORTRAIT,
+  PORTRAIT_UP,
+  PORTRAIT_DOWN,
+  LANDSCAPE,
+  LANDSCAPE_LEFT,
+  LANDSCAPE_RIGHT,
+  OTHER,
+  ALL_BUT_UPSIDE_DOWN
+  // deprecated
+}
