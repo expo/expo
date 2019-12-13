982b5b58e72d2ff8b0554e7f0f5eed7a73e81abd
Evan Bacon <baconbrix@gmail.com>
2019-12-13 11:05:32 +0100

    Update SuiteResult.js

--- apps/test-suite/components/SuiteResult.js
+++ apps/test-suite/components/SuiteResult.js
@@ -1,56 +1,69 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
 import SpecResult from './SpecResult';
 
-export default class SuiteResult extends React.Component {
-  _renderSpecResult = r => {
-    const status = r.get('status');
-    const key = r.get('id');
-    const description = r.get('description');
-    const failedExpectations = r.get('failedExpectations');
+export default function SuiteResult({ r, depth }) {
+  const renderSpecResult = React.useMemo(
+    () => r => {
+      const status = r.get('status');
+      const key = r.get('id');
+      const description = r.get('description');
+      const failedExpectations = r.get('failedExpectations');
 
-    return (
-      <SpecResult
-        key={`spec-result-${key}`}
-        status={status}
-        description={description}
-        failedExpectations={failedExpectations}
-      />
-    );
-  };
+      return (
+        <SpecResult
+          key={`spec-result-${key}`}
+          status={status}
+          description={description}
+          failedExpectations={failedExpectations}
+        />
+      );
+    },
+    []
+  );
 
-  render() {
-    const { r, depth } = this.props;
-    const result = r.get('result');
-    const description = result.get('description');
-    const specs = r.get('specs');
-    const children = r.get('children');
+  const renderSuiteResult = React.useMemo(
+    () => r => <SuiteResult key={r.get('result').get('id')} r={r} depth={depth + 1} />,
+    [depth]
+  );
 
-    const titleStyle =
-      depth === 0
-        ? { marginBottom: 8, fontSize: 16, fontWeight: 'bold' }
-        : { marginVertical: 8, fontSize: 16 };
-    const containerStyle =
-      depth === 0
-        ? {
-            paddingLeft: 16,
-            paddingVertical: 16,
-            borderBottomWidth: 1,
-            borderColor: '#ddd',
-          }
-        : { paddingLeft: 16 };
+  const result = r.get('result');
+  const description = result.get('description');
+  const specs = r.get('specs');
+  const children = r.get('children');
 
-    // console.log('Result', result);
-    return (
-      <View testID="test_suite_view_suite_container" style={containerStyle}>
-        <Text testID="test_suite_text_suite_description" style={titleStyle}>
-          {description}
-        </Text>
-        {specs.map(this._renderSpecResult)}
-        {children.map(r => (
-          <SuiteResult key={r.get('result').get('id')} r={r} depth={depth + 1} />
-        ))}
-      </View>
-    );
-  }
+  const titleStyle = depth === 0 ? styles.titleParent : styles.titleChild;
+  const containerStyle = depth === 0 ? styles.containerParent : styles.containerChild;
+
+  return (
+    <View testID="test_suite_view_suite_container" style={containerStyle}>
+      <Text testID="test_suite_text_suite_description" style={titleStyle}>
+        {description}
+      </Text>
+      {specs.map(renderSpecResult)}
+      {children.map(renderSuiteResult)}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  containerParent: {
+    paddingLeft: 16,
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+  },
+  containerChild: {
+    paddingLeft: 16,
+  },
+  titleParent: {
+    marginBottom: 8,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  titleChild: {
+    marginVertical: 8,
+    fontSize: 16,
+  },
+});
