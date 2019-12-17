7303b256ec406f404f95648fd628c01af95a8e9e
Evan Bacon <baconbrix@gmail.com>
2019-12-17 23:11:11 +0100

    Add isAvailableAsync to expo-mail-composer (#6552)
    * Add isAvailableAsync to expo-mail-composer
    
    * update build

--- packages/expo-mail-composer/src/MailComposer.ts
+++ packages/expo-mail-composer/src/MailComposer.ts
@@ -4,3 +4,13 @@ import { ComposeOptions, ComposeResult } from './MailComposer.types';
 export async function composeAsync(options: ComposeOptions): Promise<ComposeResult> {
   return await ExpoMailComposer.composeAsync(options);
 }
+
+/**
+ * Can the device compose an email.
+ * - Returns `true` on iOS when the device has a default email setup for sending mail.
+ * - Can return `false` on iOS if an MDM profile is setup to block outgoing mail. If this is the case, you may want to use the Linking API instead.
+ * - Always returns `true` in the browser and on Android
+ */
+export async function isAvailableAsync(): Promise<boolean> {
+  return await ExpoMailComposer.isAvailableAsync();
+}
