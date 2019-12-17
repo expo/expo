cb689a59f60859ab1286f3f4f23e8eecdf21b0bc
Stanisław Chmiela <sjchmiela@gmail.com>
2019-12-17 12:34:27 +0100

    [expo-notifications] Generate module, cleanup template

---
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
@@ -0,0 +1,17 @@
+package expo.modules.notifications;
+
+import android.content.Context;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.unimodules.core.BasePackage;
+import org.unimodules.core.ExportedModule;
+import org.unimodules.core.ViewManager;
+
+public class NotificationsPackage extends BasePackage {
+  @Override
+  public List<ExportedModule> createExportedModules(Context context) {
+    return Collections.singletonList((ExportedModule) new NotificationsModule(context));
+  }
+}
