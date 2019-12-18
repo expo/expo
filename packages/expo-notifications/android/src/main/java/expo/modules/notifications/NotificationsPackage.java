b898f32b278251e57ed212df045899de3cda15f0
Stanisław Chmiela <sjchmiela@gmail.com>
2019-12-18 13:23:42 +0100

    [expo-notifications][android] Implement token fetching and emitting

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
@@ -2,16 +2,24 @@ package expo.modules.notifications;
 
 import android.content.Context;
 
+import org.unimodules.core.BasePackage;
+import org.unimodules.core.ExportedModule;
+import org.unimodules.core.interfaces.SingletonModule;
+
 import java.util.Collections;
 import java.util.List;
 
-import org.unimodules.core.BasePackage;
-import org.unimodules.core.ExportedModule;
-import org.unimodules.core.ViewManager;
+import expo.modules.notifications.tokens.PushTokenManager;
+import expo.modules.notifications.tokens.PushTokenModule;
 
 public class NotificationsPackage extends BasePackage {
   @Override
   public List<ExportedModule> createExportedModules(Context context) {
-    return Collections.singletonList((ExportedModule) new NotificationsModule(context));
+    return Collections.singletonList((ExportedModule) new PushTokenModule(context));
+  }
+
+  @Override
+  public List<SingletonModule> createSingletonModules(Context context) {
+    return Collections.singletonList((SingletonModule) new PushTokenManager());
   }
 }
