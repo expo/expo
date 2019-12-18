516fd0acc30216219d6751a99d84db2f94d18094
Stanisław Chmiela <sjchmiela@gmail.com>
2019-12-18 13:33:34 +0100

    [test-suite] Add expo-notifications test

--- apps/test-suite/TestUtils.js
+++ apps/test-suite/TestUtils.js
@@ -64,7 +64,8 @@ export function getTestModules() {
       require('./tests/Localization'),
       require('./tests/SecureStore'),
       require('./tests/SMS'),
-      require('./tests/StoreReview')
+      require('./tests/StoreReview'),
+      require('./tests/NewNotifications')
     );
     return modules;
   }
@@ -102,7 +103,8 @@ export function getTestModules() {
     optionalRequire(() => require('./tests/Payments')),
     optionalRequire(() => require('./tests/AdMobInterstitial')),
     optionalRequire(() => require('./tests/AdMobRewarded')),
-    optionalRequire(() => require('./tests/FBBannerAd'))
+    optionalRequire(() => require('./tests/FBBannerAd')),
+    optionalRequire(() => require('./tests/NewNotifications'))
   );
 
   if (!isDeviceFarm()) {
---
+++ apps/test-suite/tests/NewNotifications.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import { Platform } from '@unimodules/core';
+import * as Notifications from 'expo-notifications';
+
+import * as TestUtils from '../TestUtils';
+
+export const name = 'expo-notifications';
+
+export async function test(t) {
+  const shouldSkipTestsRequiringPermissions = await TestUtils.shouldSkipTestsRequiringPermissionsAsync();
+  const describeWithPermissions = shouldSkipTestsRequiringPermissions ? t.xdescribe : t.describe;
+
+  describeWithPermissions('expo-notifications', () => {
+    t.describe('getDevicePushTokenAsync', () => {
+      if (Platform.OS === 'android' || Platform.OS === 'ios') {
+        t.it('resolves with a string', async () => {
+          const devicePushToken = await Notifications.getDevicePushTokenAsync();
+          t.expect(devicePushToken.data).toBe('string');
+        });
+      }
+
+      if (Platform.OS === 'web') {
+        t.it('resolves with an object', async () => {
+          const devicePushToken = await Notifications.getDevicePushTokenAsync();
+          t.expect(devicePushToken.data).toBe('object');
+        });
+      }
+    });
+  });
+}
