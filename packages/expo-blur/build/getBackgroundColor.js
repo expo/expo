d3cf4568bd7cc51ca935e8bb1b08f4b99bf7a278
Evan Bacon <baconbrix@gmail.com>
2019-12-24 01:32:43 -0800

    [expo-blur] update web colors (#6549)
    * Update web colors to match UIKit better
    
    * updated build
    
    * fix lint

--- packages/expo-blur/build/getBackgroundColor.js
+++ packages/expo-blur/build/getBackgroundColor.js
@@ -2,11 +2,11 @@ export default function getBackgroundColor(intensity, tint) {
     const opacity = intensity / 100;
     switch (tint) {
         case 'dark':
-            return `rgba(0,0,0,${opacity * 0.5})`;
+            return `rgba(28,28,28,${opacity * 0.73})`;
         case 'light':
-            return `rgba(255,255,255,${opacity * 0.7})`;
+            return `rgba(247,247,247,${opacity * 0.8})`;
         case 'default':
-            return `rgba(255,255,255,${opacity * 0.4})`;
+            return `rgba(255,255,255,${opacity * 0.3})`;
     }
     throw new Error(`Unsupported tint provided: ${tint}`);
 }
