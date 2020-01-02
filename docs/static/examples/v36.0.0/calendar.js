ec8992bf148b5e1f94f41290a8cf026255223a98
Charlie Cruzan <35579283+cruzach@users.noreply.github.com>
2020-01-02 16:24:44 -0500

    update calendar.js example sdk 36

--- docs/static/examples/v36.0.0/calendar.js
+++ docs/static/examples/v36.0.0/calendar.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, Platform } from 'react-native';
 import * as Calendar from 'expo-calendar';
 
 export default function App() {
   useEffect(() => {
     (async () => {
-      const { status } = await Calendar.requestPermissionsAsync();
+      const { status } = await Calendar.requestCalendarPermissionsAsync();
       if (status === 'granted') {
         const calendars = await Calendar.getCalendarsAsync();
+        console.log('Here are all your calendars:');
         console.log({ calendars });
       }
     })();
@@ -19,9 +20,36 @@ export default function App() {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-around',
       }}>
       <Text>Calendar Module Example</Text>
+      <Button title="Create a new calendar" onPress={createCalendar} />
     </View>
   );
+}
+
+async function getDefaultCalendarSource() {
+  const calendars = await Calendar.getCalendarsAsync();
+  const defaultCalendars = calendars.filter(
+    each => each.source.name === 'Default'
+  );
+  return defaultCalendars[0].source;
+}
+
+async function createCalendar() {
+  const defaultCalendarSource =
+    Platform.OS === 'ios'
+      ? await getDefaultCalendarSource()
+      : { isLocalAccount: true, name: 'Expo Calendar' };
+  const newCalendarID = await Calendar.createCalendarAsync({
+    title: 'Expo Calendar',
+    color: 'blue',
+    entityType: Calendar.EntityTypes.EVENT,
+    sourceId: defaultCalendarSource.id,
+    source: defaultCalendarSource,
+    name: 'internalCalendarName',
+    ownerAccount: 'personal',
+    accessLevel: Calendar.CalendarAccessLevel.OWNER,
+  });
+  console.log(`Your new calendar ID is: ${newCalendarID}`);
 }
