0c856e40dc02397e069994f7980c0ff601770b6b
Evan Bacon <baconbrix@gmail.com>
2020-01-02 17:11:45 -0800

    fixup build

--- packages/expo-firebase-analytics/build/Analytics.d.ts
+++ packages/expo-firebase-analytics/build/Analytics.d.ts
@@ -1,7 +1,19 @@
-export declare function initAppAsync(config: {
+/**
+ * Similar to `firebase.initializeApp()` on web but works to start a native Firebase app while the app is running.
+ * This can be used to test the native iOS Firebase app in the Expo client.
+ * This method should not be used in production, instead the app should be bundled with the native Google Services files via the `app.json`.
+ *
+ * @param googleServices Platform specific Google Services file for starting a Firebase app during runtime
+ */
+export declare function initializeAppDangerously(googleServices: {
     [key: string]: any;
 }): Promise<void>;
-export declare function deleteAppAsync(config: {
+/**
+ * Delete a running Firebase app instance. Only works for the default app. If no default app is running then nothing happens.
+ *
+ * @param googleServices Platform specific Google Services file.
+ */
+export declare function deleteApp(googleServices: {
     [key: string]: any;
 }): Promise<void>;
 /**
@@ -44,42 +56,27 @@ export declare function deleteAppAsync(config: {
  *     supported. `String` parameter values can be up to 100 characters long. The "firebase_",
  *     "google_", and "ga_" prefixes are reserved and should not be used for parameter names.
  */
-export declare function logEventAsync(name: string, properties?: {
+export declare function logEvent(name: string, properties?: {
     [key: string]: any;
 }): Promise<void>;
 /**
  * Sets whether analytics collection is enabled for this app on this device. This setting is
  * persisted across app sessions. By default it is enabled.
  *
- * @param analyticsCollectionEnabled A flag that enables or disables Analytics collection.
+ * @param isEnabled A flag that enables or disables Analytics collection.
  */
-export declare function setAnalyticsCollectionEnabledAsync(analyticsCollectionEnabled: boolean): Promise<void>;
+export declare function setAnalyticsCollectionEnabled(isEnabled: boolean): Promise<void>;
 /**
  * Sets the current screen name, which specifies the current visual context in your app. This helps
  * identify the areas in your app where users spend their time and how they interact with your app.
- * Must be called on the main thread.
- *
- * Note that screen reporting is enabled automatically and records the class name of the current
- * UIViewController for you without requiring you to call this method. If you implement
- * viewDidAppear in your UIViewController but do not call [super viewDidAppear:], that screen class
- * will not be automatically tracked. The class name can optionally be overridden by calling this
- * method in the viewDidAppear callback of your UIViewController and specifying the
- * screenClassOverride parameter. setScreenName:screenClass: must be called after
- * [super viewDidAppear:].
- *
- * If your app does not use a distinct UIViewController for each screen, you should call this
- * method and specify a distinct screenName each time a new screen is presented to the user.
- *
- * The screen name and screen class remain in effect until the current UIViewController changes or
- * a new call to setScreenName:screenClass: is made.
  *
  * @param screenName The name of the current screen. Should contain 1 to 100 characters. Set to `undefined`
  *     to clear the current screen name.
  * @param screenClassOverride The name of the screen class. Should contain 1 to 100 characters. By
- *     default this is the class name of the current UIViewController. Set to `undefined` to revert to the
+ *     default this is the class name of the current screen (UIViewController on iOS). Set to `undefined` to revert to the
  *     default class name.
  */
-export declare function setCurrentScreenAsync(screenName?: string, screenClassOverride?: string): Promise<void>;
+export declare function setCurrentScreen(screenName?: string, screenClassOverride?: string): Promise<void>;
 /**
  * Sets the interval of inactivity in seconds that terminates the current session. The default
  * value is 1800000 milliseconds (30 minutes).
@@ -87,18 +84,18 @@ export declare function setCurrentScreenAsync(screenName?: string, screenClassOv
  * @param sessionTimeoutInterval The custom time of inactivity in milliseconds before the current
  *     session terminates.
  */
-export declare function setSessionTimeoutDurationAsync(sessionTimeoutInterval: number): Promise<void>;
+export declare function setSessionTimeoutDuration(sessionTimeoutInterval: number): Promise<void>;
 /**
  * Sets the user ID property. This feature must be used in accordance with
  * [Google's Privacy Policy](https://www.google.com/policies/privacy)
  *
  * @param userID The user ID to ascribe to the user of this app on this device, which must be
- *     non-empty and no more than 256 characters long. Setting userID to nil removes the user ID.
+ *     non-empty and no more than 256 characters long. Setting userID to null removes the user ID.
  */
-export declare function setUserIdAsync(userID: string): Promise<void>;
+export declare function setUserId(userId: string | null): Promise<void>;
 /**
  * Sets a user property to a given value. Up to 25 user property names are supported. Once set,
- * user property values persist throughout the app lifecycle and across sessions.
+ * user property values persist throughout the app life-cycle and across sessions.
  *
  * The following user property names are reserved and cannot be used:
  *
@@ -110,13 +107,18 @@ export declare function setUserIdAsync(userID: string): Promise<void>;
  *     or underscores and must start with an alphabetic character. The "firebase_", "google_", and
  *     "ga_" prefixes are reserved and should not be used for user property names.
  * @param value The value of the user property. Values can be up to 36 characters long. Setting the
- *     value to nil removes the user property.
+ *     value to null removes the user property.
  */
-export declare function setUserPropertyAsync(name: string, value: string): Promise<void>;
+export declare function setUserProperty(name: string, value: string): Promise<void>;
 /**
  * Clears all analytics data for this instance from the device and resets the app instance ID.
  */
-export declare function resetAnalyticsDataAsync(): Promise<void>;
-export declare function setUserPropertiesAsync(properties: {
+export declare function resetAnalyticsData(): Promise<void>;
+/**
+ * Sets multiple user properties to the supplied values. This is a web-only method that's polyfilled on native to use `setUserProperty`.
+ *
+ * @param properties key/value set of user properties
+ */
+export declare function setUserProperties(properties: {
     [key: string]: string;
 }): Promise<void>;
--- packages/expo-firebase-analytics/build/Analytics.js
+++ packages/expo-firebase-analytics/build/Analytics.js
@@ -1,21 +1,33 @@
 import { UnavailabilityError } from '@unimodules/core';
 import ExpoFirebaseAnalytics from './ExpoFirebaseAnalytics';
 import parseConfig from './parseConfig';
-export async function initAppAsync(config) {
+/**
+ * Similar to `firebase.initializeApp()` on web but works to start a native Firebase app while the app is running.
+ * This can be used to test the native iOS Firebase app in the Expo client.
+ * This method should not be used in production, instead the app should be bundled with the native Google Services files via the `app.json`.
+ *
+ * @param googleServices Platform specific Google Services file for starting a Firebase app during runtime
+ */
+export async function initializeAppDangerously(googleServices) {
     // @ts-ignore
     if (global.__DEV__ !== true) {
-        console.warn('initAppAsync should only be used in dev mode');
+        console.warn('initializeAppDangerously should only be used in dev mode');
     }
-    if (!ExpoFirebaseAnalytics.initAppAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'initAppAsync');
+    if (!ExpoFirebaseAnalytics.initializeAppDangerously) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'initializeAppDangerously');
     }
-    return await ExpoFirebaseAnalytics.initAppAsync(parseConfig(config));
+    return await ExpoFirebaseAnalytics.initializeAppDangerously(parseConfig(googleServices));
 }
-export async function deleteAppAsync(config) {
-    if (!ExpoFirebaseAnalytics.deleteAppAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'deleteAppAsync');
+/**
+ * Delete a running Firebase app instance. Only works for the default app. If no default app is running then nothing happens.
+ *
+ * @param googleServices Platform specific Google Services file.
+ */
+export async function deleteApp(googleServices) {
+    if (!ExpoFirebaseAnalytics.deleteApp) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'deleteApp');
     }
-    return await ExpoFirebaseAnalytics.deleteAppAsync(parseConfig(config));
+    return await ExpoFirebaseAnalytics.deleteApp(parseConfig(googleServices));
 }
 /**
  * Logs an app event. The event can have up to 25 parameters. Events with the same name must have
@@ -57,54 +69,39 @@ export async function deleteAppAsync(config) {
  *     supported. `String` parameter values can be up to 100 characters long. The "firebase_",
  *     "google_", and "ga_" prefixes are reserved and should not be used for parameter names.
  */
-export async function logEventAsync(name, properties) {
-    if (!ExpoFirebaseAnalytics.logEventAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'logEventAsync');
+export async function logEvent(name, properties) {
+    if (!ExpoFirebaseAnalytics.logEvent) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'logEvent');
     }
-    return await ExpoFirebaseAnalytics.logEventAsync(name, properties);
+    return await ExpoFirebaseAnalytics.logEvent(name, properties);
 }
 /**
  * Sets whether analytics collection is enabled for this app on this device. This setting is
  * persisted across app sessions. By default it is enabled.
  *
- * @param analyticsCollectionEnabled A flag that enables or disables Analytics collection.
+ * @param isEnabled A flag that enables or disables Analytics collection.
  */
-export async function setAnalyticsCollectionEnabledAsync(analyticsCollectionEnabled) {
-    if (!ExpoFirebaseAnalytics.setAnalyticsCollectionEnabledAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'setAnalyticsCollectionEnabledAsync');
+export async function setAnalyticsCollectionEnabled(isEnabled) {
+    if (!ExpoFirebaseAnalytics.setAnalyticsCollectionEnabled) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'setAnalyticsCollectionEnabled');
     }
-    return await ExpoFirebaseAnalytics.setAnalyticsCollectionEnabledAsync(analyticsCollectionEnabled);
+    return await ExpoFirebaseAnalytics.setAnalyticsCollectionEnabled(isEnabled);
 }
 /**
  * Sets the current screen name, which specifies the current visual context in your app. This helps
  * identify the areas in your app where users spend their time and how they interact with your app.
- * Must be called on the main thread.
- *
- * Note that screen reporting is enabled automatically and records the class name of the current
- * UIViewController for you without requiring you to call this method. If you implement
- * viewDidAppear in your UIViewController but do not call [super viewDidAppear:], that screen class
- * will not be automatically tracked. The class name can optionally be overridden by calling this
- * method in the viewDidAppear callback of your UIViewController and specifying the
- * screenClassOverride parameter. setScreenName:screenClass: must be called after
- * [super viewDidAppear:].
- *
- * If your app does not use a distinct UIViewController for each screen, you should call this
- * method and specify a distinct screenName each time a new screen is presented to the user.
- *
- * The screen name and screen class remain in effect until the current UIViewController changes or
- * a new call to setScreenName:screenClass: is made.
  *
  * @param screenName The name of the current screen. Should contain 1 to 100 characters. Set to `undefined`
  *     to clear the current screen name.
  * @param screenClassOverride The name of the screen class. Should contain 1 to 100 characters. By
- *     default this is the class name of the current UIViewController. Set to `undefined` to revert to the
+ *     default this is the class name of the current screen (UIViewController on iOS). Set to `undefined` to revert to the
  *     default class name.
  */
-export async function setCurrentScreenAsync(screenName, screenClassOverride) {
-    if (!ExpoFirebaseAnalytics.setCurrentScreenAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'setCurrentScreenAsync');
+export async function setCurrentScreen(screenName, screenClassOverride) {
+    if (!ExpoFirebaseAnalytics.setCurrentScreen) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'setCurrentScreen');
     }
-    return await ExpoFirebaseAnalytics.setCurrentScreenAsync(screenName, screenClassOverride);
+    return await ExpoFirebaseAnalytics.setCurrentScreen(screenName, screenClassOverride);
 }
 /**
  * Sets the interval of inactivity in seconds that terminates the current session. The default
@@ -113,28 +110,28 @@ export async function setCurrentScreenAsync(screenName, screenClassOverride) {
  * @param sessionTimeoutInterval The custom time of inactivity in milliseconds before the current
  *     session terminates.
  */
-export async function setSessionTimeoutDurationAsync(sessionTimeoutInterval) {
-    if (!ExpoFirebaseAnalytics.setSessionTimeoutDurationAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'setSessionTimeoutDurationAsync');
+export async function setSessionTimeoutDuration(sessionTimeoutInterval) {
+    if (!ExpoFirebaseAnalytics.setSessionTimeoutDuration) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'setSessionTimeoutDuration');
     }
-    return await ExpoFirebaseAnalytics.setSessionTimeoutDurationAsync(sessionTimeoutInterval);
+    return await ExpoFirebaseAnalytics.setSessionTimeoutDuration(sessionTimeoutInterval);
 }
 /**
  * Sets the user ID property. This feature must be used in accordance with
  * [Google's Privacy Policy](https://www.google.com/policies/privacy)
  *
  * @param userID The user ID to ascribe to the user of this app on this device, which must be
- *     non-empty and no more than 256 characters long. Setting userID to nil removes the user ID.
+ *     non-empty and no more than 256 characters long. Setting userID to null removes the user ID.
  */
-export async function setUserIdAsync(userID) {
-    if (!ExpoFirebaseAnalytics.setUserIdAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'setUserIdAsync');
+export async function setUserId(userId) {
+    if (!ExpoFirebaseAnalytics.setUserId) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'setUserId');
     }
-    return await ExpoFirebaseAnalytics.setUserIdAsync(userID);
+    return await ExpoFirebaseAnalytics.setUserId(userId);
 }
 /**
  * Sets a user property to a given value. Up to 25 user property names are supported. Once set,
- * user property values persist throughout the app lifecycle and across sessions.
+ * user property values persist throughout the app life-cycle and across sessions.
  *
  * The following user property names are reserved and cannot be used:
  *
@@ -146,29 +143,34 @@ export async function setUserIdAsync(userID) {
  *     or underscores and must start with an alphabetic character. The "firebase_", "google_", and
  *     "ga_" prefixes are reserved and should not be used for user property names.
  * @param value The value of the user property. Values can be up to 36 characters long. Setting the
- *     value to nil removes the user property.
+ *     value to null removes the user property.
  */
-export async function setUserPropertyAsync(name, value) {
-    if (!ExpoFirebaseAnalytics.setUserPropertyAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'setUserPropertyAsync');
+export async function setUserProperty(name, value) {
+    if (!ExpoFirebaseAnalytics.setUserProperty) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'setUserProperty');
     }
-    return await ExpoFirebaseAnalytics.setUserPropertyAsync(name, value);
+    return await ExpoFirebaseAnalytics.setUserProperty(name, value);
 }
 /**
  * Clears all analytics data for this instance from the device and resets the app instance ID.
  */
-export async function resetAnalyticsDataAsync() {
-    if (!ExpoFirebaseAnalytics.resetAnalyticsDataAsync) {
-        throw new UnavailabilityError('expo-firebase-analytics', 'resetAnalyticsDataAsync');
+export async function resetAnalyticsData() {
+    if (!ExpoFirebaseAnalytics.resetAnalyticsData) {
+        throw new UnavailabilityError('expo-firebase-analytics', 'resetAnalyticsData');
     }
-    return await ExpoFirebaseAnalytics.resetAnalyticsDataAsync();
+    return await ExpoFirebaseAnalytics.resetAnalyticsData();
 }
-export async function setUserPropertiesAsync(properties) {
-    if (!ExpoFirebaseAnalytics.setUserPropertiesAsync) {
+/**
+ * Sets multiple user properties to the supplied values. This is a web-only method that's polyfilled on native to use `setUserProperty`.
+ *
+ * @param properties key/value set of user properties
+ */
+export async function setUserProperties(properties) {
+    if (!ExpoFirebaseAnalytics.setUserProperties) {
         // Attempt to polyfill this command for native iOS and Android platforms.
-        await Promise.all(Object.keys(properties).map(key => setUserPropertyAsync(key, properties[key])));
+        await Promise.all(Object.keys(properties).map(key => setUserProperty(key, properties[key])));
         return;
     }
-    return await ExpoFirebaseAnalytics.setUserPropertiesAsync(properties);
+    return await ExpoFirebaseAnalytics.setUserProperties(properties);
 }
 //# sourceMappingURL=Analytics.js.map
