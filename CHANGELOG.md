e64d0c8f7d85b4291b411d2efe188f0895b5e12d
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-01-03 16:11:39 +0100

    [Android][status-bar][kav] Fix behaviour in standalone apps (#6506)
    * [Android][home] Make StatusBar transparent by default
    
    * [Android][home] Make statusBar & KAV behave correctly in Home/Experiences/Standalone apps
    
    * [CHANGELOG] Update
    
    * Apply PR suggestions
    
    * [docs] Text style corrections
    
    * [Android][home] Adjust StatusBar color for Android API below 23
    
    * [Android] Fix indent rules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@ This is the log of notable changes to the Expo client that are developer-facing.
 
 ### 🛠 Breaking changes
 
+- Enriched `androidStatusBar` configuration in `app.json`. ([#6506](https://github.com/expo/expo/pull/6506) [@bbarthec](https://github.com/bbarthec))
+
 ### 🎉 New features
 
 - Added support for badge numbers. ([#4562](https://github.com/expo/expo/pull/4562) by [@jaulz](https://github.com/jaulz))
@@ -20,6 +22,7 @@ This is the log of notable changes to the Expo client that are developer-facing.
 - Removed SpongyCastle (BouncyCastle repackaging) from among Android dependencies. ([#6464](https://github.com/expo/expo/pull/6464) by [@sjchmiela](https://github.com/sjchmiela))
 - Fixed fullscreen events on iOS for native controls. ([#6504](https://github.com/expo/expo/pull/6504) by [@mczernek](https://github.com/mczernek))
 - Fixed `Camera.takePictureAsync()` not saving metadata on iOS. ([#6428](https://github.com/expo/expo/pull/6428) by [@lukmccall](https://github.com/lukmccall))
+- Fixed `KeyboardAvoidingView` in standalone Android builds. ([#6506](https://github.com/expo/expo/pull/6506) [@bbarthec](https://github.com/bbarthec))
 
 ## 36.0.0
 
--- android/expoview/src/main/java/host/exp/exponent/experience/ExperienceActivity.java
+++ android/expoview/src/main/java/host/exp/exponent/experience/ExperienceActivity.java
@@ -310,26 +310,21 @@ public class ExperienceActivity extends BaseExperienceActivity implements Expone
    */
 
   public void setLoadingScreenManifest(final JSONObject manifest) {
-    runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        if (!isInForeground()) {
-          return;
-        }
-
-        if (!mShouldShowLoadingScreenWithOptimisticManifest) {
-          return;
-        }
-
-        // grab SDK version from optimisticManifest -- in this context we just need to know ensure it's above 5.0.0 (which it should always be)
-        String optimisticSdkVersion = manifest.optString(ExponentManifest.MANIFEST_SDK_VERSION_KEY);
-        ExperienceActivityUtils.setWindowTransparency(optimisticSdkVersion, manifest, ExperienceActivity.this);
-        ExperienceActivityUtils.setNavigationBar(manifest, ExperienceActivity.this);
-
-        showLoadingScreen(manifest);
-
-        ExperienceActivityUtils.setTaskDescription(mExponentManifest, manifest, ExperienceActivity.this);
+    runOnUiThread(() -> {
+      if (!isInForeground()) {
+        return;
       }
+
+      if (!mShouldShowLoadingScreenWithOptimisticManifest) {
+        return;
+      }
+
+      ExperienceActivityUtils.configureStatusBar(manifest, ExperienceActivity.this);
+      ExperienceActivityUtils.setNavigationBar(manifest, ExperienceActivity.this);
+
+      showLoadingScreen(manifest);
+
+      ExperienceActivityUtils.setTaskDescription(mExponentManifest, manifest, ExperienceActivity.this);
     });
   }
 
@@ -433,48 +428,43 @@ public class ExperienceActivity extends BaseExperienceActivity implements Expone
 
     BranchManager.handleLink(this, mIntentUri, mDetachSdkVersion);
 
-    runOnUiThread(new Runnable() {
-      @Override
-      public void run() {
-        if (!isInForeground()) {
-          return;
-        }
-
-        if (mReactInstanceManager.isNotNull()) {
-          mReactInstanceManager.onHostDestroy();
-          mReactInstanceManager.assign(null);
-        }
-
-        mReactRootView = new RNObject("host.exp.exponent.ReactUnthemedRootView");
-        mReactRootView.loadVersion(mDetachSdkVersion).construct(ExperienceActivity.this);
-        setView((View) mReactRootView.get());
-
-        String id;
-        try {
-          id = Exponent.getInstance().encodeExperienceId(mExperienceIdString);
-        } catch (UnsupportedEncodingException e) {
-          KernelProvider.getInstance().handleError("Can't URL encode manifest ID");
-          return;
-        }
-
-        boolean hasCachedBundle;
-        if (isDebugModeEnabled()) {
-          hasCachedBundle = false;
-          mNotification = finalNotificationObject;
-          waitForDrawOverOtherAppPermission("");
-        } else {
-          mTempNotification = finalNotificationObject;
-          mIsReadyForBundle = true;
-          AsyncCondition.notify(READY_FOR_BUNDLE);
-        }
-
-        ExperienceActivityUtils.setWindowTransparency(mDetachSdkVersion, manifest, ExperienceActivity.this);
-        ExperienceActivityUtils.setNavigationBar(manifest, ExperienceActivity.this);
-        showLoadingScreen(manifest);
-
-        ExperienceActivityUtils.setTaskDescription(mExponentManifest, manifest, ExperienceActivity.this);
-        handleExperienceOptions(kernelOptions);
+    runOnUiThread(() -> {
+      if (!isInForeground()) {
+        return;
       }
+
+      if (mReactInstanceManager.isNotNull()) {
+        mReactInstanceManager.onHostDestroy();
+        mReactInstanceManager.assign(null);
+      }
+
+      mReactRootView = new RNObject("host.exp.exponent.ReactUnthemedRootView");
+      mReactRootView.loadVersion(mDetachSdkVersion).construct(ExperienceActivity.this);
+      setView((View) mReactRootView.get());
+
+      String id;
+      try {
+        id = Exponent.getInstance().encodeExperienceId(mExperienceIdString);
+      } catch (UnsupportedEncodingException e) {
+        KernelProvider.getInstance().handleError("Can't URL encode manifest ID");
+        return;
+      }
+
+      if (isDebugModeEnabled()) {
+        mNotification = finalNotificationObject;
+        waitForDrawOverOtherAppPermission("");
+      } else {
+        mTempNotification = finalNotificationObject;
+        mIsReadyForBundle = true;
+        AsyncCondition.notify(READY_FOR_BUNDLE);
+      }
+
+      ExperienceActivityUtils.configureStatusBar(manifest, ExperienceActivity.this);
+      ExperienceActivityUtils.setNavigationBar(manifest, ExperienceActivity.this);
+      showLoadingScreen(manifest);
+
+      ExperienceActivityUtils.setTaskDescription(mExponentManifest, manifest, ExperienceActivity.this);
+      handleExperienceOptions(kernelOptions);
     });
   }
 
