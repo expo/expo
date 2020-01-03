501c070ec1419b927b94a87caa9465982818e3bf
Evan Bacon <baconbrix@gmail.com>
2020-01-03 15:15:30 -0800

    Fix SSR support

--- packages/expo-error-recovery/src/ExpoErrorRecovery.web.ts
+++ packages/expo-error-recovery/src/ExpoErrorRecovery.web.ts
@@ -1,6 +1,8 @@
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
 const LOCAL_STORAGE_KEY = 'EXPO_ERROR_RECOVERY_STORAGE';
 
 function _consumeRecoveryProps(): string | null {
+  if (!canUseDOM) return null;
   const props = localStorage.getItem(LOCAL_STORAGE_KEY);
   localStorage.removeItem(LOCAL_STORAGE_KEY);
   return props;
@@ -12,6 +14,7 @@ export default {
   },
 
   saveRecoveryProps(props: string): void {
+    if (!canUseDOM) return;
     localStorage.setItem(LOCAL_STORAGE_KEY, props);
   },
 
