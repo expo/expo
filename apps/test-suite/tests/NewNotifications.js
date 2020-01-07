d76791e265be6669541049055ae9bff5dbac2d41
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-07 17:48:32 +0100

    [test-suite] Add token emitting test to NewNotifications

--- apps/test-suite/tests/NewNotifications.js
+++ apps/test-suite/tests/NewNotifications.js
@@ -4,6 +4,7 @@ import { Platform } from '@unimodules/core';
 import * as Notifications from 'expo-notifications';
 
 import * as TestUtils from '../TestUtils';
+import { waitFor } from './helpers';
 
 export const name = 'expo-notifications';
 
@@ -13,19 +14,45 @@ export async function test(t) {
 
   describeWithPermissions('expo-notifications', () => {
     t.describe('getDevicePushTokenAsync', () => {
+      let subscription = null;
+      let tokenFromEvent = null;
+      let tokenFromMethodCall = null;
+
+      t.beforeAll(() => {
+        subscription = Notifications.addTokenListener(newEvent => {
+          tokenFromEvent = newEvent;
+        });
+      });
+
+      t.afterAll(() => {
+        if (subscription) {
+          subscription.remove();
+          subscription = null;
+        }
+      });
+
       if (Platform.OS === 'android' || Platform.OS === 'ios') {
         t.it('resolves with a string', async () => {
           const devicePushToken = await Notifications.getDevicePushTokenAsync();
-          t.expect(devicePushToken.data).toBe('string');
+          t.expect(typeof devicePushToken.data).toBe('string');
+          tokenFromMethodCall = devicePushToken;
         });
       }
 
       if (Platform.OS === 'web') {
         t.it('resolves with an object', async () => {
           const devicePushToken = await Notifications.getDevicePushTokenAsync();
-          t.expect(devicePushToken.data).toBe('object');
+          t.expect(typeof devicePushToken.data).toBe('object');
+          tokenFromMethodCall = devicePushToken;
         });
       }
+
+      t.it('emits an event with token (or not, if getDevicePushTokenAsync failed)', async () => {
+        // It would be better to do `if (!tokenFromMethodCall) { pending(); } else { ... }`
+        // but `t.pending()` still doesn't work.
+        await waitFor(500);
+        t.expect(tokenFromEvent).toEqual(tokenFromMethodCall);
+      });
     });
   });
 }
