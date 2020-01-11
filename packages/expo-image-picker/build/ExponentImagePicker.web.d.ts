6f2be67ae275b8840392a1df21cfbbbc9988a9f2
Brent Vatne <brentvatne@gmail.com>
2020-01-10 17:06:22 -0800

    [image-picker] Add permissions shortcut methods to web

--- packages/expo-image-picker/build/ExponentImagePicker.web.d.ts
+++ packages/expo-image-picker/build/ExponentImagePicker.web.d.ts
@@ -1,7 +1,13 @@
+import * as Permissions from 'expo-permissions';
+import { PermissionResponse } from 'unimodules-permissions-interface';
 import { ImagePickerResult, ImagePickerOptions } from './ImagePicker.types';
 declare const _default: {
     readonly name: string;
     launchImageLibraryAsync({ mediaTypes, allowsMultipleSelection, }: ImagePickerOptions): Promise<ImagePickerResult>;
     launchCameraAsync({ mediaTypes, allowsMultipleSelection, }: ImagePickerOptions): Promise<ImagePickerResult>;
+    getCameraPermissionAsync(): Promise<Permissions.PermissionResponse>;
+    requestCameraPermissionsAsync(): Promise<Permissions.PermissionResponse>;
+    getCameraRollPermissionsAsync(): Promise<PermissionResponse>;
+    requestCameraRollPermissionsAsync(): Promise<PermissionResponse>;
 };
 export default _default;
