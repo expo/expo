470c2db6792b18df36efdb51a88fdf412af9b100
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-01-14 12:23:57 +0100

    [@unimodules/react-native-adapter] Export ProxyNativeModule (#6759)
    # Why
    
    `ProxyNativeModule` type may come in handy in other type-safe unimodules which want to rely on the listener functionality.
    
    # How
    
    Exported `ProxyNativeModule`, rebuilt all the packages.
    
    # Test Plan
    
    Code compiles, so ✅

--- packages/@unimodules/react-native-adapter/build/NativeModulesProxy.d.ts
+++ packages/@unimodules/react-native-adapter/build/NativeModulesProxy.d.ts
@@ -1,4 +1,4 @@
-declare type ProxyNativeModule = {
+export declare type ProxyNativeModule = {
     [propertyName: string]: any;
     addListener: (eventName: string) => void;
     removeListeners: (count: number) => void;
