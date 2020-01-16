a7a3e11672c800ea3634d1bdc3d6d2cc25b9204f
Stanisław Chmiela <sjchmiela@gmail.com>
2020-01-16 12:46:54 +0100

    [expo-notifications][android] Add "private" getInstallationIdAsync
    Shared preferences filename and key match those used in ExponentSharedPreferences, so we'll use ExpoKit's values if they're present

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
@@ -6,16 +6,21 @@ import org.unimodules.core.BasePackage;
 import org.unimodules.core.ExportedModule;
 import org.unimodules.core.interfaces.SingletonModule;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import expo.modules.notifications.installationid.InstallationIdProvider;
 import expo.modules.notifications.tokens.PushTokenManager;
 import expo.modules.notifications.tokens.PushTokenModule;
 
 public class NotificationsPackage extends BasePackage {
   @Override
   public List<ExportedModule> createExportedModules(Context context) {
-    return Collections.singletonList((ExportedModule) new PushTokenModule(context));
+    return Arrays.asList(
+        new PushTokenModule(context),
+        new InstallationIdProvider(context)
+    );
   }
 
   @Override
