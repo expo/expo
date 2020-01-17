5dda629f98c6357caef8faa0221ed5c24ecce840
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-01-17 10:55:09 +0100

    [expo-notifications] Post-review fixes (#6779)
    # Why
    
    [A new review](https://github.com/expo/expo/pull/6577#pullrequestreview-343656373) came in.
    
    # How
    
    Applied all the suggestions.
    
    # Test Plan
    
    Code compiled.

--- packages/expo-notifications/src/TokenEmitter.ts
+++ packages/expo-notifications/src/TokenEmitter.ts
@@ -2,23 +2,23 @@ import { EventEmitter, Subscription, Platform } from '@unimodules/core';
 import { DevicePushToken } from './getDevicePushTokenAsync';
 import PushTokenManager from './PushTokenManager';
 
-export type TokenListener = (token: DevicePushToken) => void;
+export type PushTokenListener = (token: DevicePushToken) => void;
 
 // Web uses SyntheticEventEmitter
 const tokenEmitter = new EventEmitter(PushTokenManager);
 const newTokenEventName = 'onDevicePushToken';
 
-export function addTokenListener(listener: TokenListener): Subscription {
+export function addPushTokenListener(listener: PushTokenListener): Subscription {
   const wrappingListener = ({ devicePushToken }) =>
     // @ts-ignore: TS can't decide what Platform.OS is.
     listener({ data: devicePushToken, type: Platform.OS });
   return tokenEmitter.addListener(newTokenEventName, wrappingListener);
 }
 
-export function removeTokenSubscription(subscription: Subscription) {
+export function removePushTokenSubscription(subscription: Subscription) {
   tokenEmitter.removeSubscription(subscription);
 }
 
-export function removeAllTokenListeners() {
+export function removeAllPushTokenListeners() {
   tokenEmitter.removeAllListeners(newTokenEventName);
 }
