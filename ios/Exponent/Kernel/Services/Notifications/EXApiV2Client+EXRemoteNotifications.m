bb8de834044dbef5068959a28c3ee9bbebe9b630
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-01-21 08:56:17 +0100

    [expo-application] Add methods for app release type and APNs environment (#6724)
    # Why
    
    APNs environment will be useful for `expo-notifications` module. App release type could be useful for users' analytics. 
    
    # How
    
    In the gist of things, I moved `EXProvisioningProfile` from `ExpoKit` to `EXApplication`. Kernel either uses `EXApplication/EXProvisioningProfile` (if it's present) or uses sensible fallback values.
    
    # Test Plan
    
    Added tests to `test-suite`, verified in `bare-expo` that they pass.

--- ios/Exponent/Kernel/Services/Notifications/EXApiV2Client+EXRemoteNotifications.m
+++ ios/Exponent/Kernel/Services/Notifications/EXApiV2Client+EXRemoteNotifications.m
@@ -2,8 +2,10 @@
 
 #import "EXApiV2Client+EXRemoteNotifications.h"
 #import "EXKernel.h"
-#import "EXProvisioningProfile.h"
 #import "NSData+EXRemoteNotifications.h"
+#if __has_include(<EXApplication/EXProvisioningProfile.h>)
+#import <EXApplication/EXProvisioningProfile.h>
+#endif
 
 @implementation EXApiV2Client (EXRemoteNotifications)
 
@@ -15,9 +17,18 @@
     @"deviceToken": deviceToken.apnsTokenString,
     @"type": @"apns",
   }];
-  if ([EXProvisioningProfile mainProvisioningProfile].development) {
+  // Presence of this file is assured in Expo client
+  // and in ejected projects Expo Push Notifications don't work anyway
+  // so this codepath shouldn't be executed at all.
+#if __has_include(<EXApplication/EXProvisioningProfile.h>)
+  NSString *environment = [[EXProvisioningProfile mainProvisioningProfile] notificationServiceEnvironment];
+  if (!environment) {
+    DDLogWarn(@"aps-environment is missing from the entitlements; ensure that the provisioning profile enables push notifications");
+  } else if ([environment isEqualToString:@"development"]) {
     arguments[@"development"] = @YES;
   }
+#endif
+
   
   return [self callRemoteMethod:@"push/updateDeviceToken"
                       arguments:arguments
@@ -39,9 +50,17 @@
     @"deviceToken": deviceToken.apnsTokenString,
     @"type": @"apns",
   }];
-  if ([EXProvisioningProfile mainProvisioningProfile].development) {
+  // Presence of this file is assured in Expo client
+  // and in ejected projects Expo Push Notifications don't work anyway
+  // so this codepath shouldn't be executed at all.
+#if __has_include(<EXApplication/EXProvisioningProfile.h>)
+  NSString *environment = [[EXProvisioningProfile mainProvisioningProfile] notificationServiceEnvironment];
+  if (!environment) {
+    DDLogWarn(@"aps-environment is missing from the entitlements; ensure that the provisioning profile enables push notifications");
+  } else if ([environment isEqualToString:@"development"]) {
     arguments[@"development"] = @YES;
   }
+#endif
   
   return [self callRemoteMethod:@"push/getExpoPushToken"
                       arguments:arguments
