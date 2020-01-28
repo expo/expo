4f848e7848d33366e94ecbe31976de652aa08fc8
Evan Bacon <baconbrix@gmail.com>
2020-01-27 22:11:01 -0800

    Updated doc blocks

--- packages/expo-facebook/build/Facebook.types.d.ts
+++ packages/expo-facebook/build/Facebook.types.d.ts
@@ -58,35 +58,61 @@ export declare type FacebookOptions = {
     permissions?: string[];
 };
 export declare type SDKScriptURLOptions = {
+    /**
+     * Web, Android: Sets the base Facebook domain to use when making Web requests.
+     * Defaults to: 'connect.facebook.net'
+     */
     domain?: string;
+    /**
+     * Web only: Chooses which language of the FBSDK bundle to use.
+     * Defaults to: `'en_US'`
+     */
     language?: string;
+    /**
+     * Web only: Enables the [customer chat plugin](https://developers.facebook.com/docs/messenger-platform/discovery/customer-chat-plugin/sdk/).
+     * Defaults to `false`
+     */
     isCustomerSupportChatEnabled?: boolean;
     /**
-     * To improve performance, the JavaScript SDK is loaded minified.
-     * You can also load a debug version of the JavaScript SDK that includes more logging and stricter argument checking as well as being non-minified.
-     *
-     * The debug version should not be used in your production environment, as its payload is larger and is worse for the performance of your page.
+     * Web only: Loads the JS SDK in an non-minified format with more logs and stricter type checking.
+     * This shouldn't be enabled in your production environment, as its payload is larger and is worse for the performance of your page.
+     * Defaults to `false`
      */
     isDebugEnabled?: boolean;
 };
 export declare type SDKInitOptions = {
-    appId?: string;
-    autoLogAppEvents?: boolean;
     /**
-     * With xfbml set to true, the SDK will parse your page's DOM to find and initialize any social plugins that have been added using XFBML.
-     * If you're not using social plugins on the page, setting xfbml to false will improve page load times.
-     * You can find out more about this by looking at Social Plugins.
+     * Application ID used to initialize the FBSDK app.
+     * On web this is required, not providing it will result in a `ERR_FB_CONF` error.
+     *
+     * On native if you don't provide this, Facebook SDK will try to use `appId` from native
+     * app resources (which in standalone apps you would define in `app.json`, in Expo client are unavailable, and in bare you configure yourself according to Facebook setup documentation for [iOS](https://developers.facebook.com/docs/facebook-login/ios#4--configure-your-project) and [Android](https://developers.facebook.com/docs/facebook-login/android#manifest)).
+     * If it fails to find one, the promise will be rejected.
      */
-    xfbml?: boolean;
+    appId?: string;
     /**
-     * Required for web.
+     * Required for web. Selects the version of FBSDK to use.
+     *
      * https://developers.facebook.com/docs/javascript/reference/FB.init/v5.0
      */
     version?: string;
+    /**
+     * Sets whether Facebook SDK should log app events. App events involve app eg. installs, app launches (more info [here](https://developers.facebook.com/docs/app-events/getting-started-app-events-android/#auto-events) and [here](https://developers.facebook.com/docs/app-events/getting-started-app-events-ios#auto-events)).
+     * In some cases, you may want to disable or delay the collection of automatically logged events, such as to obtain user consent or fulfill legal obligations.
+     * This method corresponds to [this](https://developers.facebook.com/docs/app-events/getting-started-app-events-ios#disable-auto-events) and [this](https://developers.facebook.com/docs/app-events/getting-started-app-events-android/#disable-auto-events) native SDK methods.
+     */
+    autoLogAppEvents?: boolean;
+    /**
+     * Web only: With xfbml set to true, the SDK will parse your page's DOM to find and initialize any social plugins that have been added using XFBML.
+     * If you're not using social plugins on the page, setting `xfbml` to `false` will improve page load times.
+     * You can find out more about this by looking at [Social Plugins](https://developers.facebook.com/docs/plugins/).
+     * Defaults to `true`
+     */
+    xfbml?: boolean;
 };
 export declare type NativeInitOptions = {
     /**
-     * An optional Facebook App Name argument
+     * An optional Facebook App Name argument for iOS and Android.
      */
     appName?: string;
 };
