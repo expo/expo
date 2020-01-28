521a4c310695c6d3aaf79b40fe7182712602102c
Charlie Cruzan <35579283+cruzach@users.noreply.github.com>
2020-01-28 11:37:49 -0500

    [TS][location][docs] add `maximumAge` to TS, `timeout` to docs, & fix docs error (#6808)
    * [location] add TS for maximumAge
    
    * update docs for timeout option
    
    * add unique key prop to TableOfContentSection

--- packages/expo-location/build/Location.d.ts
+++ packages/expo-location/build/Location.d.ts
@@ -10,6 +10,7 @@ export interface ProviderStatus {
 }
 export interface LocationOptions {
     accuracy?: LocationAccuracy;
+    maximumAge?: number;
     enableHighAccuracy?: boolean;
     timeInterval?: number;
     distanceInterval?: number;
