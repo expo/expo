62ba264bd4dbbcc0d87ad093aa4f4d84ef906505
Hein Rutjes <hrutjes@gmail.com>
2020-02-11 11:30:25 +0100

    [firebase-core] Add tests to test-suite

--- apps/test-suite/TestUtils.js
+++ apps/test-suite/TestUtils.js
@@ -52,7 +52,8 @@ export function getTestModules() {
     require('./tests/Blur'),
     require('./tests/LinearGradient'),
     require('./tests/Facebook'),
-    require('./tests/HTML')
+    require('./tests/HTML'),
+    require('./tests/FirebaseCore')
   );
 
   if (Platform.OS === 'android') {
