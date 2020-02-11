322ea448d6798ed8894792ff579a15b123bb1107
Stanisław Chmiela <sjchmiela@gmail.com>
2020-02-11 16:44:53 +0100

    [expo-notifications] Add support for vibration pattern override

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/presentation/builders/ExpoNotificationBuilder.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/presentation/builders/ExpoNotificationBuilder.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.unimodules.core.ModuleRegistry;
 import org.unimodules.interfaces.imageloader.ImageLoader;
@@ -101,6 +103,11 @@ public class ExpoNotificationBuilder implements NotificationBuilder {
       builder.setVibrate(NO_VIBRATE_PATTERN);
     }
 
+    long[] vibrationPatternOverride = getVibrationPatternOverride();
+    if (shouldVibrate() && vibrationPatternOverride != null) {
+      builder.setVibrate(vibrationPatternOverride);
+    }
+
     if (shouldSetBadge()) {
       // TODO: Set badge as an effect of presenting notification,
       //       not as an effect of building a notification.
@@ -175,5 +182,22 @@ public class ExpoNotificationBuilder implements NotificationBuilder {
 
   private boolean shouldSetBadge() {
     return !mNotificationRequest.isNull(BADGE_KEY);
+  }
+
+  private long[] getVibrationPatternOverride() {
+    try {
+      JSONArray vibrateJsonArray = mNotificationRequest.optJSONArray(VIBRATE_KEY);
+      if (vibrateJsonArray != null) {
+        long[] pattern = new long[vibrateJsonArray.length()];
+        for (int i = 0; i < vibrateJsonArray.length(); i++) {
+          pattern[i] = vibrateJsonArray.getLong(i);
+        }
+        return pattern;
+      }
+    } catch (JSONException e) {
+      Log.w("expo-notifications", "Failed to set custom vibration pattern from the notification: " + e.getMessage());
+    }
+
+    return null;
   }
 }
