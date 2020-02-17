797443a692e78554ed5b70739440f25b7af8438f
Stanisław Chmiela <sjchmiela@gmail.com>
2020-02-17 19:33:51 +0100

    [expo-notifications] Remove notification builder factory

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/NotificationsPackage.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import org.unimodules.core.BasePackage;
 import org.unimodules.core.ExportedModule;
-import org.unimodules.core.interfaces.InternalModule;
 import org.unimodules.core.interfaces.SingletonModule;
 
 import java.util.Arrays;
@@ -14,24 +13,12 @@ import expo.modules.notifications.installationid.InstallationIdProvider;
 import expo.modules.notifications.notifications.NotificationManager;
 import expo.modules.notifications.notifications.emitting.NotificationsEmitter;
 import expo.modules.notifications.notifications.handling.NotificationsHandler;
-import expo.modules.notifications.notifications.presentation.ExpoNotificationBuilderFactory;
-import expo.modules.notifications.notifications.presentation.ExpoNotificationPresentationEffectsManager;
 import expo.modules.notifications.notifications.presentation.ExpoNotificationPresentationModule;
-import expo.modules.notifications.notifications.presentation.effects.SetBadgeCountNotificationEffect;
 import expo.modules.notifications.permissions.NotificationPermissionsModule;
 import expo.modules.notifications.tokens.PushTokenManager;
 import expo.modules.notifications.tokens.PushTokenModule;
 
 public class NotificationsPackage extends BasePackage {
-  @Override
-  public List<InternalModule> createInternalModules(Context context) {
-    return Arrays.asList(
-        new ExpoNotificationBuilderFactory(),
-        new ExpoNotificationPresentationEffectsManager(),
-        new SetBadgeCountNotificationEffect(context)
-    );
-  }
-
   @Override
   public List<ExportedModule> createExportedModules(Context context) {
     return Arrays.asList(
