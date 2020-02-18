504aeb6e78df224d7292fcbdbd0bd5916a533834
Hein Rutjes <hrutjes@gmail.com>
2020-02-18 15:18:29 +0100

    [firebase-analytics] Update & integrate Pure JS Analytics tracker

--- packages/expo-firebase-analytics/src/ExpoFirebaseAnalytics.ts
+++ packages/expo-firebase-analytics/src/ExpoFirebaseAnalytics.ts
@@ -1,6 +1,9 @@
 import { NativeModulesProxy, UnavailabilityError, CodedError } from '@unimodules/core';
-import { DEFAULT_APP_NAME, DEFAULT_APP_OPTIONS } from 'expo-firebase-core';
+import Constants from 'expo-constants';
+import { DEFAULT_APP_NAME, DEFAULT_APP_OPTIONS, getDefaultWebOptions } from 'expo-firebase-core';
 import { Platform } from 'react-native';
+
+import FirebaseAnalyticsJS from './FirebaseAnalyticsJS';
 const { ExpoFirebaseAnalytics } = NativeModulesProxy;
 
 if (!ExpoFirebaseAnalytics) {
@@ -9,6 +12,7 @@ if (!ExpoFirebaseAnalytics) {
   );
 }
 
+let pureJSAnalyticsTracker: FirebaseAnalyticsJS | void;
 let isUnavailabilityLoggingEnabled = true;
 let isUnavailabilityWarningLogged = false;
 
@@ -31,10 +35,22 @@ function callAnalyticsModule(funcName: string, ...args) {
   // In that case we show a warning and log the analytics events to the console.
   // The user can disable these by calling `setUnavailabilityLogging(false)`.
   if (DEFAULT_APP_NAME !== '[DEFAULT]') {
+    const defaultWebOptions = getDefaultWebOptions();
+    if (defaultWebOptions && !pureJSAnalyticsTracker) {
+      pureJSAnalyticsTracker = new FirebaseAnalyticsJS(defaultWebOptions, {
+        clientId: Constants.sessionId,
+        strictNativeEmulation: true,
+        appName: Constants.manifest?.name || 'Expo Client',
+        appVersion: Constants.nativeAppVersion || undefined,
+      });
+    }
+    if (pureJSAnalyticsTracker) {
+      return pureJSAnalyticsTracker[funcName].call(pureJSAnalyticsTracker, ...args);
+    }
     if (isUnavailabilityLoggingEnabled) {
       if (!isUnavailabilityWarningLogged) {
         console.warn(
-          'Firebase Analytics is not available in the Expo client. To test Firebase Analytics create a standalone build or custom client. To suppress this warning use `setUnavailabilityLogging(false)`.'
+          `Firebase Analytics is not available in the Expo client. See "https://docs.expo.io/versions/unversioned/sdk/firebase-analytics#expo_client_limitations_configuration" on more information on setting up Firebase Analytics with the standard Expo Client.`
         );
         isUnavailabilityWarningLogged = true;
       }
--- packages/expo-firebase-analytics/src/FirebaseAnalyticsJS.ts
+++ packages/expo-firebase-analytics/src/FirebaseAnalyticsJS.ts
@@ -1,116 +1,8 @@
-type FirebaseAnalyticsJSCodedEvent = { [key: string]: any };
-
-interface FirebaseAnalyticsJSConfig {
-  /**
-   * **(Required)** Measurement-Id as found in the web Firebase-config.
-   * The format is G-XXXXXXXXXX.
-   */
-  measurementId: string;
-}
-
-interface FirebaseAnalyticsJSOptions {
-  /**
-   * **(Required)** Anonymously identifies a particular user, device, or browser instance.
-   * For the web, this is generally stored as a first-party cookie with a two-year expiration.
-   * For mobile apps, this is randomly generated for each particular instance of an application install.
-   * The value of this field should be a random UUID (version 4) as described in http://www.ietf.org/rfc/rfc4122.txt.
-   * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#cid
-   */
-  clientId: string;
-
-  /**
-   * Max cache time in msec (default = 5000).
-   * Caches events fired within a certain time-frame and then
-   * sends them to the Google Measurement API in a single batch.
-   */
-  maxCacheTime?: number;
-
-  /**
-   * Enables strict data format checks for logEvent and setUserProperties.
-   * When enabled, causes `logEvent` and `setUserProperties` to strictly check
-   * whether any event- names & values and user-properties conform to the
-   * native SDK requirements.
-   */
-  strictNativeEmulation?: boolean;
-
-  /**
-   * Document title (e.g. "My Awesome Page").
-   * This is a browser specific field.
-   * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#dt
-   */
-  docTitle?: string;
-
-  /**
-   * Document location URL (e.g. "https://myawesomeapp.com").
-   * This is a browser specific field.
-   * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#dl
-   */
-  docLocation?: string;
-
-  /**
-   * Screen-resolution in the format "WxH" (e.g "2000x1440").
-   * This is a browser specific field.
-   * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#sr
-   */
-  screenRes?: string;
-
-  /**
-   * Application name (e.g. "My Awesome App").
-   * This is a mobile app specific field.
-   * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#an
-   */
-  appName?: string;
-
-  /**
-   * Application version (e.g. "1.2").
-   * This is a mobile app specific field.
-   * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#av
-   */
-  appVersion?: string;
-
-  /**
-   * User language (e.g. "en-us").
-   * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#ul
-   */
-  userLanguage?: string;
-
-  /**
-   * Origin (default =  "firebase").
-   */
-  origin?: string;
-
-  /**
-   * Custom query arguments that are appended to the POST request that is send to the
-   * Google Measurement API v2.
-   *
-   * @example
-   * ```
-   * const analytics = new FirebaseAnalyticsJS(config, {
-   *   appName: 'My Awesome App',
-   *   customArg: {
-   *     vp: '123x456', // Add viewport-size
-   *     sd: '24-bits' // Add screen-colors
-   *   }
-   * });
-   * ```
-   */
-  customArgs?: { [key: string]: any };
-}
-
-function encodeQueryArgs(queryArgs: FirebaseAnalyticsJSCodedEvent): string {
-  const now = Date.now();
-  return Object.keys(queryArgs)
-    .map(key => {
-      return `${key}=${encodeURIComponent(
-        key === '_et' ? Math.max(now - queryArgs[key], 0) : queryArgs[key]
-      )}`;
-    })
-    .join('&');
-}
-
-const SHORT_EVENT_PARAMS = {
-  currency: 'cu',
-};
+import {
+  FirebaseAnalyticsJSCodedEvent,
+  FirebaseAnalyticsJSConfig,
+  FirebaseAnalyticsJSOptions,
+} from './FirebaseAnalyticsJS.types';
 
 /**
  * A pure JavaScript Google Firebase Analytics implementation that uses
@@ -196,13 +88,14 @@ class FirebaseAnalyticsJS {
       };
     }
     const args = encodeQueryArgs(queryArgs);
-    //console.log(`FirebaseAnalyticsJS: ${args}...`);
-    await fetch(`${this.url}?${args}`, {
+    console.log(`FirebaseAnalyticsJS: ${args}...`);
+    if (body) console.log(`FirebaseAnalyticsJS body: ${body}...`);
+    const response = await fetch(`${this.url}?${args}`, {
       method: 'POST',
       cache: 'no-cache',
       body,
     });
-    //console.log(`FirebaseAnalyticsJS: response: ${response.status}`);
+    console.log(`FirebaseAnalyticsJS: response: ${response.status}`);
   }
 
   private async addEvent(event: FirebaseAnalyticsJSCodedEvent) {
@@ -407,5 +300,20 @@ class FirebaseAnalyticsJS {
     this.userProperties = undefined;
   }
 }
+
+function encodeQueryArgs(queryArgs: FirebaseAnalyticsJSCodedEvent): string {
+  const now = Date.now();
+  return Object.keys(queryArgs)
+    .map(key => {
+      return `${key}=${encodeURIComponent(
+        key === '_et' ? Math.max(now - queryArgs[key], 0) : queryArgs[key]
+      )}`;
+    })
+    .join('&');
+}
+
+const SHORT_EVENT_PARAMS = {
+  currency: 'cu',
+};
 
 export default FirebaseAnalyticsJS;
