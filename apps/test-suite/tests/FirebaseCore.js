68d43e1d9f96b4a067a3b96c359be7245bca60f1
Hein Rutjes <hrutjes@gmail.com>
2020-02-18 14:44:08 +0100

    [firebase-core] Add web-support & code cleanup

--- apps/test-suite/tests/FirebaseCore.js
+++ apps/test-suite/tests/FirebaseCore.js
@@ -1,4 +1,3 @@
-import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 import * as FirebaseCore from 'expo-firebase-core';
 
@@ -8,39 +7,6 @@ const SYSTEM_APP_NAME = '[DEFAULT]';
 
 const SANDBOX_APP_PREFIX = '__sandbox';
 
-/*
-function getTestSuiteFirebaseAppOptions() {
-  if (Platform.OS === 'android') {
-    //const googleServicesJson = require('../google-services.json');
-    //const options = FirebaseCore.FirebaseOptions.parseAndroidGoogleServices(googleServicesJson);
-    ///return options;
-    throw new Error('TODO ANDROID');
-  } else if (Platform.OS === 'ios') {
-    // TODO, load PLIST using babel-loader?
-    return {
-      clientId: '1082251606918-ktto2c4d3tit64uikmki48j7520qensp.apps.googleusercontent.com',
-      apiKey: 'AIzaSyBH7Pa-tgLgL2QK6DgGhKipuDTbKqU6Wlk',
-      storageBucket: 'expo-test-suite.appspot.com',
-      projectId: 'expo-test-suite',
-      appId: '1:1082251606918:ios:f448eb8df0adab41e24a07',
-      databaseURL: 'https://expo-test-suite.firebaseio.com',
-      messagingSenderId: '1082251606918',
-    };
-  } else {
-    throw new Error('Platform not supported');
-  }
-}
-
-function expectFirebaseOptions(expect, options1, options2) {
-  expect(options1.appId).toBe(options2.appId);
-  expect(options1.messagingSenderId).toBe(options2.messagingSenderId);
-  expect(options1.apiKey).toBe(options2.apiKey);
-  expect(options1.projectId).toBe(options2.projectId);
-  expect(options1.clientId).toBe(options2.clientId);
-  expect(options1.storageBucket).toBe(options2.storageBucket);
-  expect(options1.databaseURL).toBe(options2.databaseURL);
-}*/
-
 export async function test({ describe, it, xit, expect, beforeAll }) {
   if (!FirebaseCore.DEFAULT_APP_OPTIONS) {
     describe(name, () => {
@@ -48,7 +14,7 @@ export async function test({ describe, it, xit, expect, beforeAll }) {
         Platform.select({
           ios: `No Google services configuration found. In order to run this test, set the 'ios.googleServicesFile' field in 'app.json'. When you are running a bare project, add 'GoogleService-Info.plist' to your XCode project.`,
           android: `No Google services configuration found. In order to run this test, set the 'android.googleServicesFile' field in 'app.json'. When you are running a bare project, add 'google-services.json' to 'android/app'`,
-          web: `The web platform is currently not supported for this test`,
+          web: `No Firebase configuration found. In order to run this test, set the 'web.config.firebase' field in 'app.json'.`,
         }),
         async () => {}
       );
@@ -56,7 +22,7 @@ export async function test({ describe, it, xit, expect, beforeAll }) {
     return;
   }
 
-  const isSandboxed = Constants.appOwnership === 'expo';
+  const isSandboxed = Constants.appOwnership === 'expo' && Platform.OS !== 'web';
   const itWhenSandboxed = isSandboxed ? it : xit;
   const itWhenNotSandboxed = isSandboxed ? xit : it;
 
@@ -113,16 +79,6 @@ export async function test({ describe, it, xit, expect, beforeAll }) {
         }
         expect(error).toBeNull();
       });
-      /*itWhenSandboxed(`returns the firebase options from the test-suite`, async () => {
-        let error = null;
-        try {
-          const { DEFAULT_APP_OPTIONS } = FirebaseCore;
-          expectFirebaseOptions(expect, DEFAULT_APP_OPTIONS, getTestSuiteFirebaseAppOptions());
-        } catch (e) {
-          error = e;
-        }
-        expect(error).toBeNull();
-      });*/
     });
   });
 }
--- packages/expo-firebase-core/src/FirebaseOptions.ts
+++ packages/expo-firebase-core/src/FirebaseOptions.ts
@@ -1,5 +1,4 @@
-//import { CodedError } from '@unimodules/core';
-//import Constants from 'expo-constants';
+import Constants from 'expo-constants';
 
 export interface IFirebaseOptions {
   appId: string;
@@ -9,48 +8,10 @@ export interface IFirebaseOptions {
   messagingSenderId: string;
   storageBucket: string;
   projectId: string;
-  //authDomain: string;
+  authDomain: string;
+  measurementId: string;
 }
 
-export class FirebaseOptions {
-  /*static parseAndroidGoogleServices(json: any, packageName?: string): IFirebaseOptions {
-    // TODO make this solid
-    const projectInfo = json['project_info'];
-    const client = json.client[0];
-    const projectId = projectInfo.project_id;
-    const messagingSenderId = projectInfo.project_number;
-    const databaseURL = projectInfo.firebase_url;
-    const storageBucket = projectInfo.storage_bucket;
-    const appId = client.client_info.mobilesdk_app_id;
-    const analyticsService = client.services['analytics-service'];
-    const trackingId = analyticsService
-      ? analyticsService.analytics_property.tracking_id
-      : undefined;
-    const apiKey = client.api_key[0].current_key;
-    //const authDomain = `${projectId}.firebaseapp.com`;
-    return {
-      appId,
-      trackingId,
-      apiKey,
-      projectId,
-      messagingSenderId,
-      databaseURL,
-      storageBucket,
-      //authDomain,
-    };
-  }*/
+export function getDefaultWebOptions(): IFirebaseOptions | void {
+  return Constants.manifest?.web?.config?.firebase;
 }
-
-/*export function getFirebaseOptionsFromAndroidManifest(): FirebaseOptions {
-  const { manifest } = Constants;
-  if (!manifest) throw new CodedError('ERR_MANIFEST', 'Manifest not available');
-  const { googleServicesFile } = manifest;
-  if (!googleServicesFile)
-    throw new CodedError('ERR_MANIFEST', 'GoogleServicesFile is not configured in app.json');
-  const json = JSON.parse(googleServicesFile);
-  return getFirebaseOptionsFromAndroidGoogleServicesFile(json);
-}
-
-export function getFirebaseOptionsFromManifest(): FirebaseOptions {
-  return getFirebaseOptionsFromAndroidManifest();
-}*/
