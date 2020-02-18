fb2a9a86300c33811fe6caa4b516203583123fe8
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-02-18 10:37:36 +0100

    [expo-notifications] Updating badge count from notification (#6849)
    # Why
    
    We want developers to be able to set badge count by sending notifications conforming to a format.
    
    # How
    
    - Added dependency on [`ShortcutBadger`](https://github.com/leolin310148/ShortcutBadger/), which we also use in Expo client.
    - Added a `SetBadgeCountNotificationEffect` which will get notified of notification presentation success/failure and set the badge according to badge count set by `ExpoNotificationBuilder` in `notification.extras`.
    - Unfortunately, supporting Xiaomi devices requires us to also apply badge count to the `Notification` object itself. (See [ShortcutBadger wiki](https://github.com/leolin310148/ShortcutBadger/wiki/Xiaomi-Device-Support).) To make this matter more clear, I have created a separate `BadgeSettingNotificationBuilder < ExpoNotificationBuilder` class, which handles badge setting and forwarding information to the `SetBadgeCountNotificationEffect` effect.
    
    # Test Plan
    
    Had to install a supported launcher (used ADW Launcher, then had to install ADW Notifier), but:
    
    ![Screenshot_20200218-101118](https://user-images.githubusercontent.com/1151041/74721289-6e929300-5237-11ea-803f-03b5ae5e6dfb.png)

---
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/presentation/builders/BadgeSettingNotificationBuilder.java
@@ -0,0 +1,57 @@
+package expo.modules.notifications.notifications.presentation.builders;
+
+import android.app.Notification;
+import android.content.Context;
+import android.os.Bundle;
+
+import org.unimodules.core.ModuleRegistry;
+
+import androidx.core.app.NotificationCompat;
+import me.leolin.shortcutbadger.ShortcutBadger;
+
+public class BadgeSettingNotificationBuilder extends ExpoNotificationBuilder {
+  private static final String BADGE_KEY = "badge";
+
+  public static final String EXTRAS_BADGE_KEY = "badge";
+
+  public BadgeSettingNotificationBuilder(Context context, ModuleRegistry moduleRegistry) {
+    super(context, moduleRegistry);
+  }
+
+  @Override
+  protected NotificationCompat.Builder createBuilder() {
+    NotificationCompat.Builder builder = super.createBuilder();
+
+    if (shouldSetBadge()) {
+      // Forward information about badge count to set
+      // to SetBadgeCountNotificationEffect.
+      Bundle extras = builder.getExtras();
+      extras.putInt(EXTRAS_BADGE_KEY, getBadgeCount());
+      builder.setExtras(extras);
+    }
+
+    return builder;
+  }
+
+  @Override
+  public Notification build() {
+    Notification notification = super.build();
+
+    if (shouldSetBadge()) {
+      // Xiaomi devices require this extra notification configuration step
+      // https://github.com/leolin310148/ShortcutBadger/wiki/Xiaomi-Device-Support
+      // Badge for other devices is set as an effect in SetBadgeCountNotificationEffect
+      ShortcutBadger.applyNotification(getContext(), notification, getBadgeCount());
+    }
+
+    return notification;
+  }
+
+  private boolean shouldSetBadge() {
+    return getNotificationRequest().has(BADGE_KEY);
+  }
+
+  private int getBadgeCount() {
+    return getNotificationRequest().optInt(BADGE_KEY);
+  }
+}
