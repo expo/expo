5c294c92ae93556b86e5c1aa3f87f966a3079fc5
Michał Czernek <czernekmichal@gmail.com>
2020-02-19 14:52:31 +0100

    Correct expo-notifications build output.

--- packages/expo-notifications/build/index.d.ts
+++ packages/expo-notifications/build/index.d.ts
@@ -1,9 +1,6 @@
 export { default as getDevicePushTokenAsync } from './getDevicePushTokenAsync';
 export { default as getExpoPushTokenAsync } from './getExpoPushTokenAsync';
-<<<<<<< HEAD
-=======
 export { default as presentNotificationAsync } from './presentNotificationAsync';
->>>>>>> master
 export * from './TokenEmitter';
 export * from './NotificationsEmitter';
 export * from './NotificationsHandler';
