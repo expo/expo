eeb1ae76f07b0ab5e68271f7f75503fd9b88d780
Hein Rutjes <hrutjes@gmail.com>
2020-02-19 17:45:56 +0100

    [firebase-core] Simplify JS code

--- packages/expo-firebase-core/src/ExpoFirebaseCore.ts
+++ packages/expo-firebase-core/src/ExpoFirebaseCore.ts
@@ -1,27 +1,3 @@
-import { NativeModulesProxy, UnavailabilityError } from '@unimodules/core';
+import { NativeModulesProxy } from '@unimodules/core';
 
-import { IFirebaseOptions } from './FirebaseOptions';
-
-const { ExpoFirebaseCore } = NativeModulesProxy;
-
-if (!ExpoFirebaseCore) {
-  console.warn(
-    'No native ExpoFirebaseCore module found, are you sure the expo-firebase-core module is linked properly?'
-  );
-}
-
-export default {
-  get DEFAULT_APP_NAME(): string {
-    if (!ExpoFirebaseCore) {
-      throw new UnavailabilityError('FirebaseCore', 'DEFAULT_APP_NAME');
-    }
-    return ExpoFirebaseCore.DEFAULT_APP_NAME;
-  },
-
-  get DEFAULT_APP_OPTIONS(): IFirebaseOptions | void {
-    if (!ExpoFirebaseCore) {
-      throw new UnavailabilityError('FirebaseCore', 'DEFAULT_APP_OPTIONS');
-    }
-    return ExpoFirebaseCore.DEFAULT_APP_OPTIONS;
-  },
-};
+export default NativeModulesProxy.ExpoFirebaseCore;
