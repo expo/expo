d9ca8902c3127a41370a9e67346a4c1f654d3850
Michał Czernek <czernekmichal@gmail.com>
2020-02-19 10:45:33 +0100

    Remove JS code from new App Loader unimodule. Remove dependency on old one.

--- packages/unimodules-app-loader/src/__tests__/Unimodule-test.ts
+++
@@ -1,7 +0,0 @@
-import * as Unimodule from '../Unimodule';
-
-describe('Unimodule', () => {
-  it('someGreatMethodAsync is defined', async () => {
-    expect(Unimodule.someGreatMethodAsync).toBeDefined();
-  });
-});
