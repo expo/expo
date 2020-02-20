bf3f7ba864864e9fbd9307b3d1c99463f5e3d001
Tomasz Sapeta <1714764+tsapeta@users.noreply.github.com>
2020-02-20 13:07:17 +0100

    [android] 🤏 Update react-native-gesture-handler to 1.6.0 (#7121)
    # Why
    
    Part of #7006 
    
    # How
    
    - Used `et update-vendored-module` to update `react-native-gesture-handler`.
    - Updated versions in some apps.
    - Tried to run it on Android... and it turned out there is a new `RNGHModalUtils` file scoped under `com.facebook.react` package so I've copied this file manually to appropriate directory in `expoview`.
    - Added a warning saying that `com.facebook.react` needs to be manually updated, we already do the same in `react-native-reanimated`.
    - Added changelog entry.
    
    # Test Plan
    
    Tested examples in `native-component-list`.

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/gesturehandler/react/RNGestureHandlerModule.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/gesturehandler/react/RNGestureHandlerModule.java
@@ -3,6 +3,7 @@ package versioned.host.exp.exponent.modules.api.components.gesturehandler.react;
 import android.content.Context;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.facebook.react.ReactRootView;
 import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
@@ -555,7 +556,7 @@ public class RNGestureHandlerModule extends ReactContextBaseJavaModule {
       while (!mRoots.isEmpty()) {
         int sizeBefore = mRoots.size();
         RNGestureHandlerRootHelper root = mRoots.get(0);
-        ReactRootView reactRootView = root.getRootView();
+        ViewGroup reactRootView = root.getRootView();
         if (reactRootView instanceof RNGestureHandlerEnabledRootView) {
           ((RNGestureHandlerEnabledRootView) reactRootView).tearDown();
         } else {
@@ -579,7 +580,8 @@ public class RNGestureHandlerModule extends ReactContextBaseJavaModule {
     synchronized (mRoots) {
       for (int i = 0; i < mRoots.size(); i++) {
         RNGestureHandlerRootHelper root = mRoots.get(i);
-        if (root.getRootView().getRootViewTag() == rootViewTag) {
+        ViewGroup rootView = root.getRootView();
+        if (rootView instanceof ReactRootView && ((ReactRootView) rootView).getRootViewTag() == rootViewTag) {
           // we have found root helper registered for a given react root, we don't need to
           // initialize a new one then
           return;
@@ -638,7 +640,8 @@ public class RNGestureHandlerModule extends ReactContextBaseJavaModule {
     synchronized (mRoots) {
       for (int i = 0; i < mRoots.size(); i++) {
         RNGestureHandlerRootHelper root = mRoots.get(i);
-        if (root.getRootView().getRootViewTag() == rootViewTag) {
+        ViewGroup rootView = root.getRootView();
+        if (rootView instanceof ReactRootView && ((ReactRootView) rootView).getRootViewTag() == rootViewTag) {
           return root;
         }
       }
