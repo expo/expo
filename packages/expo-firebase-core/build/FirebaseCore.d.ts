da1c025f45d8f88de5ed48213f5ea3c6d0896546
Hein Rutjes <hrutjes@gmail.com>
2020-02-20 09:40:42 +0100

    [firebase-core] Update JS build files

--- packages/expo-firebase-core/build/FirebaseCore.d.ts
+++ packages/expo-firebase-core/build/FirebaseCore.d.ts
@@ -1,2 +1,39 @@
-export * from './FirebaseOptions';
-export declare const DEFAULT_APP_NAME: any, DEFAULT_APP_OPTIONS: any;
+import { IFirebaseOptions } from './FirebaseOptions';
+export { IFirebaseOptions } from './FirebaseOptions';
+/**
+ * Name of the default Firebase app (e.g. `[DEFAULT]`).
+ * On the Expo Client a Firebase App is created for each project that is loaded, and a unique name for each project is returned.
+ */
+export declare const DEFAULT_APP_NAME: string | void;
+/**
+ * Firebase options with which the default app was initialized. If no Google services configuration was provided, `undefined` is returned.
+ *
+ * Depending on the platform, the options are read from the following files and app.json keys.
+ *
+ * | Platform | File                       | App.json key                 |
+ * | -------- | -------------------------- | ---------------------------- |
+ * | iOS      | `GoogleService-Info.plist` | `ios.googleServicesFile`     |
+ * | Android  | `google-services.json`     | `android.googleServicesFile` |
+ * | Web      |                            | `web.config.firebase`        |
+ *
+ * **example**
+ *
+ * ```javascript
+ * console.log(FirebaseCore.DEFAULT_APP_OPTIONS);
+ * {
+ *   appId: "1:1082251606918:ios:a2800bc715889446e24a07",
+ *   apiKey: "AIzaXXXXXXXX-xxxxxxxxxxxxxxxxxxx",
+ *   clientId: "000000000000-0000000000000.apps.googleusercontent.com",
+ *   trackingId: 12345567890,
+ *   databaseURL: "https://myexpoapp777.firebaseio.com",
+ *   storageBucket:  "myexpoapp777.appspot.com",
+ *   projectId: "myexpoapp777",
+ *   messagingSenderId:  123454321
+ * }
+ * ```
+ */
+export declare const DEFAULT_APP_OPTIONS: IFirebaseOptions | void;
+/**
+ * The default Firebase options as defined in `web.config.firebase` in `app.json`.
+ */
+export declare const DEFAULT_WEB_APP_OPTIONS: IFirebaseOptions | void;
