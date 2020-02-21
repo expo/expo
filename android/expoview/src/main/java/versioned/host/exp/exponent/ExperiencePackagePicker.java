24ce00c40418e9b206ddeedf6bb2df3ac4b6307b
Hein Rutjes <IjzerenHein@users.noreply.github.com>
2020-02-21 17:48:13 +0100

    [firebase-core] Add core functionality for supporting native Firebase packages (#7013)
    * [bare-expo] Fix database-url in GoogleService-Info.plist
    
    * [firebase-core] Add `expo-firebase-core` package
    
    * [firebase-core] Add tests to test-suite
    
    * [firebase-core] Add scoped firebase-core to Expo client to create native sandboxed firebase apps
    
    * [firebase-core] Update bare-expo BasePackageList
    
    * [firebase-core] Add firebase-core to bundledNativeModules.json
    
    * [firebase-core] Add SDK documentation
    
    * [firebase-core] Add firebase-core to Expo client iOS project file
    
    * [firebase-core] Add js build files
    
    * [firebase-core] Update SDK doc to refer to native firebase guide
    
    * [firebase-core] Add web-support & code cleanup
    
    * [docs] Fixes to firebase-core docs
    
    * [firebase-core] Update build files
    
    * [firebase-core] Fix test in test-suite
    
    * [fireabase-core] Cleanup COMPARISON.md
    
    * [firebase-core] Improve google-services.json parsing and client selection
    
    * [firebase-core] Updated SDK docs
    
    * [fireabse-core] Update readme description
    
    * [firebase-core] Removed outcommented line on Android
    
    * [firebase-core] Minor iOS code improvements after review
    
    * [firebase-core] Fixes after review to unit tests
    
    * [firebase-core] Updated iOS code after review feedback
    
    * [firebase-core] Fix tests
    
    * [firebase-core] Simplify JS code
    
    * [firebase-core] Update js build output
    
    * [fireabse-core] Add DEFAULT_WEB_APP_OPTIONS constant + doc improvements
    
    * [firebase-core] Update JS build files
    
    * [firebase-core] Fix unimodulePeerDependencies in package.json
    
    * [firebase-core] Android build.gradle fixes
    
    * [firebase-core] Android base64 encoding code improvement
    
    * [firebase-core] Prevent hard javascript import error when native lib not linked
    
    * [firebase-core] Update various Android methods to be protected i.s.o. public
    
    * [firebase-core] Android protected DEFAULT_APP_NAME and re-use in scoped
    
    * [firebase-core] Update changelog
    
    * [fireabse-core] Update JS build files
    
    * [firebase-android] Fix Firebase App cleanup when multiple projects are loaded on Android
    
    * [ios] Update pods

--- android/expoview/src/main/java/versioned/host/exp/exponent/ExperiencePackagePicker.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/ExperiencePackagePicker.java
@@ -32,6 +32,7 @@ import expo.modules.documentpicker.DocumentPickerPackage;
 import expo.modules.facebook.FacebookPackage;
 import expo.modules.facedetector.FaceDetectorPackage;
 import expo.modules.filesystem.FileSystemPackage;
+import expo.modules.firebase.core.FirebaseCorePackage;
 import expo.modules.font.FontLoaderPackage;
 import expo.modules.gl.GLPackage;
 import expo.modules.google.signin.GoogleSignInPackage;
@@ -85,6 +86,7 @@ public class ExperiencePackagePicker {
       new FacebookPackage(),
       new FaceDetectorPackage(),
       new FileSystemPackage(),
+      new FirebaseCorePackage(),
       new FontLoaderPackage(),
       new GLPackage(),
       new GoogleSignInPackage(),
