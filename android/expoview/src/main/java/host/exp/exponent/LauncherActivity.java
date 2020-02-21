351b39ebddf808b635382a691eceddae8bf910a6
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2020-02-21 09:00:48 +0100

    [android] Revert changes in LauncherActivity

--- android/expoview/src/main/java/host/exp/exponent/LauncherActivity.java
+++ android/expoview/src/main/java/host/exp/exponent/LauncherActivity.java
@@ -2,23 +2,31 @@
 
 package host.exp.exponent;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.core.content.ContextCompat;
 
-import host.exp.exponent.experience.HomeActivity;
+import javax.inject.Inject;
+
+import host.exp.exponent.di.NativeModuleDepsProvider;
+import host.exp.exponent.kernel.Kernel;
 
 // This activity is transparent. It uses android:style/Theme.Translucent.NoTitleBar.
 // Calls finish() once it is done processing Intent.
-public class LauncherActivity extends HomeActivity {
+public class LauncherActivity extends Activity {
+
+  @Inject
+  Kernel mKernel;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
     if (host.exp.expoview.BuildConfig.DEBUG) {
       // Need WRITE_EXTERNAL_STORAGE for method tracing
       if (Constants.DEBUG_METHOD_TRACING) {
@@ -30,7 +38,9 @@ public class LauncherActivity extends HomeActivity {
       }
     }
 
-    super.onCreate(savedInstanceState);
+    NativeModuleDepsProvider.getInstance().inject(LauncherActivity.class, this);
+
+    mKernel.setActivityContext(this);
 
     mKernel.handleIntent(this, getIntent());
 
@@ -40,32 +50,36 @@ public class LauncherActivity extends HomeActivity {
     startService(ExponentIntentService.getActionStayAwake(getApplicationContext()));
 
     // Delay to prevent race condition where finish() is called before service starts.
-    new Handler(getMainLooper()).postDelayed(() -> {
-      try {
-        // Crash with NoSuchFieldException instead of hard crashing at task.getTaskInfo().numActivities
-        ActivityManager.RecentTaskInfo.class.getDeclaredField("numActivities");
+    new Handler(getMainLooper()).postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          // Crash with NoSuchFieldException instead of hard crashing at task.getTaskInfo().numActivities
+          ActivityManager.RecentTaskInfo.class.getDeclaredField("numActivities");
 
-        for (ActivityManager.AppTask task : mKernel.getTasks()) {
-          if (task.getTaskInfo().id == getTaskId()) {
-            if (task.getTaskInfo().numActivities == 1) {
-              finishAndRemoveTask();
-              return;
-            } else {
-              break;
+          for (ActivityManager.AppTask task : mKernel.getTasks()) {
+            if (task.getTaskInfo().id == getTaskId()) {
+              if (task.getTaskInfo().numActivities == 1) {
+                finishAndRemoveTask();
+                return;
+              } else {
+                break;
+              }
             }
           }
+        } catch (Exception e) {
+          // just go straight to finish()
         }
-      } catch (Exception e) {
-        // just go straight to finish()
-      }
 
-      finish();
+        finish();
+      }
     }, 100);
   }
 
   @Override
-  @SuppressLint("MissingSuperCall")
   public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+
     // We shouldn't ever get here, since we call finish() in onCreate. Just want to be safe
     // since this Activity is singleTask and there might be some edge case where this is called.
     mKernel.handleIntent(this, intent);
