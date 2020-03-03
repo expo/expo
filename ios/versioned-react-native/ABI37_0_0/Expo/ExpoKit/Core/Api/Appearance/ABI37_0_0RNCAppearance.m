68764827e78eb14753395eca30bfacc095bb1c32
Tomasz Sapeta <1714764+tsapeta@users.noreply.github.com>
2020-03-03 10:51:08 +0100

    [ios] Update react-native-appearance to 0.3.3 (#7250)
    # Why
    
    Followup https://github.com/expo/react-native-appearance/pull/39
    
    # How
    
    Updated vendored module using `et uvm` and backported changes to versioned code for SDK37.
    
    # Test Plan
    
    Issue that I've found (see description of linked PR) is solved now, I also tested whether it works with SDK36 and older - yes, it does.

--- ios/versioned-react-native/ABI37_0_0/Expo/ExpoKit/Core/Api/Appearance/ABI37_0_0RNCAppearance.m
+++ ios/versioned-react-native/ABI37_0_0/Expo/ExpoKit/Core/Api/Appearance/ABI37_0_0RNCAppearance.m
@@ -75,14 +75,16 @@ ABI37_0_0RCT_EXPORT_MODULE();
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(appearanceChanged:)
                                                  name:ABI37_0_0RNCUserInterfaceStyleDidChangeNotification
-                                               object:nil];
+                                               object:self.bridge];
   }
 }
 
 - (void)stopObserving
 {
   if (@available(iOS 13.0, *)) {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:ABI37_0_0RNCUserInterfaceStyleDidChangeNotification
+                                                  object:self.bridge];
   }
 }
 
--- ios/versioned-react-native/ABI37_0_0/Expo/ExpoKit/Core/Api/Appearance/ABI37_0_0RNCAppearanceProvider.h
+++ ios/versioned-react-native/ABI37_0_0/Expo/ExpoKit/Core/Api/Appearance/ABI37_0_0RNCAppearanceProvider.h
@@ -1,9 +1,12 @@
 #import <UIKit/UIKit.h>
 #import <ABI37_0_0React/ABI37_0_0RCTView.h>
+#import <ABI37_0_0React/ABI37_0_0RCTBridge.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ABI37_0_0RNCAppearanceProvider : ABI37_0_0RCTView
+
+- (instancetype)initWithBridge:(nonnull ABI37_0_0RCTBridge *)bridge;
 
 @end
 
