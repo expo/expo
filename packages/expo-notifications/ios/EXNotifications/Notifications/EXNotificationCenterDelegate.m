d3e2968681877780fce6672bcf82c021b48d17fe
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-03 11:31:08 +0100

    [expo-notifications] Save pending notification responses for first delegate

--- packages/expo-notifications/ios/EXNotifications/Notifications/EXNotificationCenterDelegate.m
+++ packages/expo-notifications/ios/EXNotifications/Notifications/EXNotificationCenterDelegate.m
@@ -7,6 +7,7 @@
 @interface EXNotificationCenterDelegate ()
 
 @property (nonatomic, strong) NSPointerArray *delegates;
+@property (nonatomic, strong) NSMutableArray<UNNotificationResponse *> *pendingNotificationResponses;
 
 @end
 
@@ -18,6 +19,7 @@ UM_REGISTER_SINGLETON_MODULE(NotificationCenterDelegate);
 {
   if (self = [super init]) {
     _delegates = [NSPointerArray weakObjectsPointerArray];
+    _pendingNotificationResponses = [NSMutableArray array];
   }
   return self;
 }
@@ -121,6 +123,11 @@ UM_REGISTER_SINGLETON_MODULE(NotificationCenterDelegate);
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
 {
+  if ([_delegates count] == 0) {
+    [_pendingNotificationResponses addObject:response];
+    completionHandler();
+    return;
+  }
   __block int delegatesCalled = 0;
   __block int delegatesCompleted = 0;
   __block BOOL delegatingCompleted = NO;
@@ -167,6 +174,13 @@ UM_REGISTER_SINGLETON_MODULE(NotificationCenterDelegate);
 - (void)addDelegate:(id<EXNotificationsDelegate>)delegate
 {
   [_delegates addPointer:(__bridge void * _Nullable)(delegate)];
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  for (UNNotificationResponse *response in _pendingNotificationResponses) {
+    [delegate userNotificationCenter:center didReceiveNotificationResponse:response withCompletionHandler:^{
+      // completion handler doesn't need to do anything
+    }];
+  }
+  [_pendingNotificationResponses removeAllObjects];
 }
 
 - (void)removeDelegate:(id<EXNotificationsDelegate>)delegate
