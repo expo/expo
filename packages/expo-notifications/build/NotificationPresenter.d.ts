e6d14fa3ce5e12e99b1df7cce49458fcf1263fc6
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-05 10:44:18 +0100

    [expo-notifications] Export JS methods allowing dismissal of notifications

--- packages/expo-notifications/build/NotificationPresenter.d.ts
+++ packages/expo-notifications/build/NotificationPresenter.d.ts
@@ -2,6 +2,8 @@ import { ProxyNativeModule } from '@unimodules/core';
 import { NotificationRequest } from './NotificationPresenter.types';
 export interface NotificationPresenterModule extends ProxyNativeModule {
     presentNotificationAsync: (identifier: string, notificationRequest: NotificationRequest) => Promise<void>;
+    dismissNotificationAsync: (identifier: string) => Promise<void>;
+    dismissAllNotificationsAsync: () => Promise<void>;
 }
 declare const _default: NotificationPresenterModule;
 export default _default;
---
+++ packages/expo-notifications/build/dismissNotificationAsync.d.ts
@@ -0,0 +1,1 @@
+export default function dismissNotificationAsync(notificationIdentifier: string): Promise<void>;
