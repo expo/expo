e2f9f2bc06058dc4abef29e19f86e30628a65edf
jonniebigodes <jonniebigodes@hotmail.com>
2020-03-07 15:59:17 +0000

    fix template to address issue

--- templates/expo-template-tabs/__tests__/App-test.js
+++ templates/expo-template-tabs/__tests__/App-test.js
@@ -1,29 +1,25 @@
-import * as React from 'react';
-import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
-import renderer from 'react-test-renderer';
+import * as React from "react";
+import { act, create } from "react-test-renderer";
+import App from "../App";
 
-import App from '../App';
-
-jest.mock('expo', () => ({
-  AppLoading: 'AppLoading',
+// mocks expo related configuration
+jest.mock("expo", () => ({
+  AppLoading: "AppLoading",
+  Linking: {
+    makeUrl: () => "/"
+  }
 }));
 
-jest.mock('../navigation/AppNavigator', () => 'AppNavigator');
+// mocks navigation component
+jest.mock("../navigation/BottomTabNavigator", () => "BottomTabNavigator");
 
-describe('App', () => {
+describe("App", () => {
   jest.useFakeTimers();
-
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
-  });
-
-  it(`renders the loading screen`, () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it(`renders the root without loading screen`, () => {
-    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
+  let tree;
+  it(`renders correctly`, () => {
+    act(() => {
+      tree = create(<App skipLoadingScreen/>);
+    });
+    expect(tree.toJSON()).toMatchSnapshot();
   });
 });
