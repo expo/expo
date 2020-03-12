deae99635c61a28191f4f81354aacb21c0085025
Hein Rutjes <hrutjes@gmail.com>
2020-03-12 11:31:05 +0100

    [expo-impage] Add source tests

--- apps/native-component-list/src/screens/Image/tests/index.tsx
+++ apps/native-component-list/src/screens/Image/tests/index.tsx
@@ -3,10 +3,11 @@ import AndroidTests from './android';
 import AppearanceTests from './appearance';
 import EventTests from './events';
 import IOSTests from './ios';
+import SourceTests from './source';
 
 const tests: ImageTestGroup = {
   name: 'Image',
-  tests: [AppearanceTests, EventTests, IOSTests, AndroidTests],
+  tests: [AppearanceTests, SourceTests, EventTests, IOSTests, AndroidTests],
 };
 
 export default tests;
