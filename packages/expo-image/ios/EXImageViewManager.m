488b896beebe1f0d1f35a97fc10ed3daaf51a4f6
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-17 17:04:29 +0100

    [expo-image][ios] Register SVGKitCoder

--- packages/expo-image/ios/EXImageViewManager.m
+++ packages/expo-image/ios/EXImageViewManager.m
@@ -2,6 +2,8 @@
 
 #import <expo-image/EXImageViewManager.h>
 #import <expo-image/EXImageView.h>
+#import <SDWebImageSVGKitPlugin/SDImageSVGKCoder.h>
+#import <SDWebImage/SDImageCodersManager.h>
 
 #import <React/RCTImageShadowView.h>
 
@@ -12,6 +14,14 @@ RCT_EXPORT_MODULE(ExpoImage)
 - (RCTShadowView *)shadowView
 {
   return [RCTImageShadowView new];
+}
+
++ (void)initialize
+{
+  SDImageSVGKCoder *svgCoder = [SDImageSVGKCoder sharedCoder];
+  if (![[SDImageCodersManager sharedManager].coders containsObject:svgCoder]) {
+    [[SDImageCodersManager sharedManager] addCoder:svgCoder];
+  }
 }
 
 RCT_EXPORT_VIEW_PROPERTY(source, NSDictionary)
