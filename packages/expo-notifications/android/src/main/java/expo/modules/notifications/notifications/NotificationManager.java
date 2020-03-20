c968b31bbe00f3263f98226e2135aaaa5ebccd34
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-03-20 11:57:38 +0100

    [expo-notifications] Handle interactions with notifications (#7251)
    # Why
    
    Developers should be able to know when a user interacts with a notification.
    
    # How
    
    On iOS most of the implementation was already there, the only change is to use `expo.modules.notifications.actions.DEFAULT` when the action is a default action (so that API is the same between platforms). One other thing that had to be done was saving pending notification responses till the registration of the first delegate.
    
    The initial notification iOS flow now looks like this:
    - `EXNotificationCenterDelegate` sets itself as the `UNUserNotificationCenter`'s delegate in `application:didFinishLaunchingWithOptions:`
    - soon after that `UNUserNoticationCenter` calls `userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:` on `EXNotificationCenterDelegate`
    - most probably (due to JS downloading, latency), JS didn't set up a listener on `EXNotificationsEmitter`, so it hasn't yet registered as a listener at `EXNotificationCenterDelegate`, so…
    - …`EXNotificationCenterDelegate` doesn't have any listeners registered right now
    - since we want to forward the response to JS, we have to save it for when the JS is finally run, so `EXNotificationCenterDelegate` saves the response in an array and dispatches the notification when the first listener registers.
    
    On Android we have to implement all the stuff ourselves, so there are:
    
    - new `NotificationResponse` class: `(actionIdentifier, notification)`
    - which is passed in an `Intent` triggering `NotificationResponseReceiver` as the `contentIntent` of the notification
    - `NotificationResponseReceiver` opens the app and handles the response to `BaseNotificationsService`
    - `BaseNotificationsService` forwards the response to `NotificationManager`s which forward the response to `NotificationListener`s
    - `NotificationHandler` does nothing
    - `NotificationEmitter` emits the response
    
    # Test Plan
    
    I added a rather simple interactive test letting us test if when a notification is tapped on, a listener is called. Apart from that `bare-expo` implements another notification received listener which `console.info`s received listeners (so it's easy to test initial notifications).

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/NotificationManager.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/NotificationManager.java
@@ -3,10 +3,14 @@ package expo.modules.notifications.notifications;
 import org.unimodules.core.interfaces.SingletonModule;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.WeakHashMap;
 
 import expo.modules.notifications.notifications.interfaces.NotificationListener;
 import expo.modules.notifications.notifications.model.Notification;
+import expo.modules.notifications.notifications.model.NotificationResponse;
 import expo.modules.notifications.notifications.service.ExpoNotificationsService;
 
 public class NotificationManager implements SingletonModule, expo.modules.notifications.notifications.interfaces.NotificationManager {
@@ -17,6 +21,7 @@ public class NotificationManager implements SingletonModule, expo.modules.notifi
    * is already registered and to iterate over on new token.
    */
   private WeakHashMap<NotificationListener, WeakReference<NotificationListener>> mListenerReferenceMap;
+  private Collection<NotificationResponse> mPendingNotificationResponses = new ArrayList<>();
 
   public NotificationManager() {
     mListenerReferenceMap = new WeakHashMap<>();
@@ -43,6 +48,13 @@ public class NotificationManager implements SingletonModule, expo.modules.notifi
     if (!mListenerReferenceMap.containsKey(listener)) {
       WeakReference<NotificationListener> listenerReference = new WeakReference<>(listener);
       mListenerReferenceMap.put(listener, listenerReference);
+      if (!mPendingNotificationResponses.isEmpty()) {
+        Iterator<NotificationResponse> responseIterator = mPendingNotificationResponses.iterator();
+        while (responseIterator.hasNext()) {
+          listener.onNotificationResponseReceived(responseIterator.next());
+          responseIterator.remove();
+        }
+      }
     }
   }
 
@@ -69,6 +81,26 @@ public class NotificationManager implements SingletonModule, expo.modules.notifi
       NotificationListener listener = listenerReference.get();
       if (listener != null) {
         listener.onNotificationReceived(notification);
+      }
+    }
+  }
+
+  /**
+   * Used by {@link ExpoNotificationsService} to notify of new notification responses.
+   * Calls {@link NotificationListener#onNotificationResponseReceived(NotificationResponse)} on all values
+   * of {@link NotificationManager#mListenerReferenceMap}.
+   *
+   * @param response Notification response received
+   */
+  public void onNotificationResponseReceived(NotificationResponse response) {
+    if (mListenerReferenceMap.isEmpty()) {
+      mPendingNotificationResponses.add(response);
+    } else {
+      for (WeakReference<NotificationListener> listenerReference : mListenerReferenceMap.values()) {
+        NotificationListener listener = listenerReference.get();
+        if (listener != null) {
+          listener.onNotificationResponseReceived(response);
+        }
       }
     }
   }
