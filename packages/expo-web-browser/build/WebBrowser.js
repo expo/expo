e8190f19c25d04dba4af7c46dd52993f9a12773b
Luca Colonnello <luca.colonnello91@gmail.com>
2020-03-22 10:19:12 +0000

    Store only if AppState is available instead of every state change

--- packages/expo-web-browser/build/WebBrowser.js
+++ packages/expo-web-browser/build/WebBrowser.js
@@ -106,14 +106,14 @@ let _redirectHandler = null;
 // Store the `resolve` function from a Promise to fire when the AppState
 // returns to active
 let _onWebBrowserCloseAndroid = null;
-// Store previous app state to check whether the listener has ever been attached
-let _previousAppState = AppState.currentState;
+// If the initial AppState.currentState is null, we assume that the first call to
+// AppState#change event is not actually triggered by a real change,
+// is triggered instead by the bridge capturing the current state
+// (https://facebook.github.io/react-native/docs/appstate#basic-usage)
+let _isAppStateAvailable = AppState.currentState !== null;
 function _onAppStateChangeAndroid(state) {
-    // if _previousAppState is null, we assume that the first call to
-    // AppState#change event is not actually triggered by a real change
-    // (https://facebook.github.io/react-native/docs/appstate#basic-usage)
-    if (_previousAppState === null) {
-        _previousAppState = state;
+    if (!_isAppStateAvailable) {
+        _isAppStateAvailable = true;
         return;
     }
     if (state === 'active' && _onWebBrowserCloseAndroid) {
--- packages/expo-web-browser/src/WebBrowser.ts
+++ packages/expo-web-browser/src/WebBrowser.ts
@@ -149,15 +149,14 @@ let _redirectHandler: ((event: RedirectEvent) => void) | null = null;
 // returns to active
 let _onWebBrowserCloseAndroid: null | (() => void) = null;
 
-// Store previous app state to check whether the listener has ever been attached
-let _previousAppState: null | string = AppState.currentState;
-
+// If the initial AppState.currentState is null, we assume that the first call to
+// AppState#change event is not actually triggered by a real change,
+// is triggered instead by the bridge capturing the current state
+// (https://facebook.github.io/react-native/docs/appstate#basic-usage)
+let _isAppStateAvailable: boolean = AppState.currentState !== null;
 function _onAppStateChangeAndroid(state: AppStateStatus) {
-  // if _previousAppState is null, we assume that the first call to
-  // AppState#change event is not actually triggered by a real change
-  // (https://facebook.github.io/react-native/docs/appstate#basic-usage)
-  if (_previousAppState === null) {
-    _previousAppState = state;
+  if (!_isAppStateAvailable) {
+    _isAppStateAvailable = true;
     return;
   }
 
