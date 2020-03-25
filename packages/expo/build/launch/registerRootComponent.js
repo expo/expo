ebe726beafbb06fc5598448b57c069d41e9e45e4
Eric Samelson <esamelson@users.noreply.github.com>
2020-03-25 15:37:10 -0700

    [expo] make expo AppEntry work in bare workflow (#7464)

--- packages/expo/build/launch/registerRootComponent.js
+++ packages/expo/build/launch/registerRootComponent.js
@@ -1,6 +1,6 @@
+import 'expo/build/Expo.fx';
 import { AppRegistry } from 'react-native';
-import withExpoRoot from './withExpoRoot';
 export default function registerRootComponent(component) {
-    AppRegistry.registerComponent('main', () => withExpoRoot(component));
+    AppRegistry.registerComponent('main', () => component);
 }
 //# sourceMappingURL=registerRootComponent.js.map
