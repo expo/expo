9cc48405165d9e3d9c8d4d91c619f568504b6f7b
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-27 14:23:23 +0100

    [expo-notifications][ios] Present notifications if presentNotificationAsync is used

--- packages/expo-notifications/ios/EXNotifications/Notifications/Presenting/EXNotificationPresentationModule.m
+++ packages/expo-notifications/ios/EXNotifications/Notifications/Presenting/EXNotificationPresentationModule.m
@@ -4,16 +4,28 @@
 
 #import <EXNotifications/EXNotificationBuilder.h>
 #import <EXNotifications/EXNotificationSerializer.h>
+#import <EXNotifications/EXNotificationCenterDelegate.h>
 
 @interface EXNotificationPresentationModule ()
 
 @property (nonatomic, weak) id<EXNotificationBuilder> notificationBuilder;
+
+@property (nonatomic, strong) NSCountedSet<NSString *> *presentedNotifications;
+@property (nonatomic, weak) id<EXNotificationCenterDelegate> notificationCenterDelegate;
 
 @end
 
 @implementation EXNotificationPresentationModule
 
 UM_EXPORT_MODULE(ExpoNotificationPresenter);
+
+- (instancetype)init
+{
+  if (self = [super init]) {
+    _presentedNotifications = [NSCountedSet set];
+  }
+  return self;
+}
 
 # pragma mark - Exported methods
 
@@ -26,8 +38,12 @@ UM_EXPORT_METHOD_AS(presentNotificationAsync,
   UNNotificationContent *content = [_notificationBuilder notificationContentFromRequest:notificationSpec];
   UNNotificationTrigger *trigger = nil;
   UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+  [_presentedNotifications addObject:identifier];
+  __weak EXNotificationPresentationModule *weakSelf = self;
   [[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
     if (error) {
+      // If there was no error, willPresentNotification: callback will remove the identifier from the set
+      [weakSelf.presentedNotifications removeObject:identifier];
       NSString *message = [NSString stringWithFormat:@"Notification could not have been presented: %@", error.description];
       reject(@"ERR_NOTIF_PRESENT", message, error);
     } else {
@@ -72,6 +88,25 @@ UM_EXPORT_METHOD_AS(dismissAllNotificationsAsync,
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
 {
   _notificationBuilder = [moduleRegistry getModuleImplementingProtocol:@protocol(EXNotificationBuilder)];
+
+  id<EXNotificationCenterDelegate> notificationCenterDelegate = (id<EXNotificationCenterDelegate>)[moduleRegistry getSingletonModuleForName:@"NotificationCenterDelegate"];
+  [notificationCenterDelegate addDelegate:self];
 }
+
+# pragma mark - EXNotificationsDelegate
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
+{
+  UNNotificationPresentationOptions presentationOptions = UNNotificationPresentationOptionNone;
+
+  NSString *identifier = notification.request.identifier;
+  if ([_presentedNotifications containsObject:identifier]) {
+    [_presentedNotifications removeObject:identifier];
+    presentationOptions = UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge;
+  }
+
+  completionHandler(presentationOptions);
+}
+
 
 @end
