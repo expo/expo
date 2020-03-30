347b872b169a1e29dfa1b96f75656321f73b0de0
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-30 20:33:26 +0200

    [expo-notifications] Resolve presentNotificationAsync with the notification identifier

--- packages/expo-notifications/build/presentNotificationAsync.d.ts
+++ packages/expo-notifications/build/presentNotificationAsync.d.ts
@@ -1,2 +1,2 @@
 import { NotificationContentInput } from './Notifications.types';
-export default function presentNotificationAsync(content: NotificationContentInput, identifier?: string): Promise<void>;
+export default function presentNotificationAsync(content: NotificationContentInput, identifier?: string): Promise<string>;
