390e3fa510c9e7820ae569d8ce2efc2dd3245cad
Stanisław Chmiela <sjchmiela@gmail.com>
2020-03-31 17:06:30 +0200

    [bare-expo] Remove expo-asset side effect

--- apps/bare-expo/env.js
+++ apps/bare-expo/env.js
@@ -1,7 +1,14 @@
-import { _setShouldThrowAnErrorOutsideOfExpo } from '~expo/build/environment/validatorState';
+// Import expo-asset and register the custom source transformer
+import 'expo-asset';
 
+import { setCustomSourceTransformer } from 'expo-asset/build/resolveAssetSource';
 // @ts-ignore: Swap this out for an environment variable in the future
 import Constants from 'expo-constants'; // eslint-disable-line import/order
+
+import { _setShouldThrowAnErrorOutsideOfExpo } from '~expo/build/environment/validatorState';
+
 _setShouldThrowAnErrorOutsideOfExpo(false);
+// Remove custom source transformer added by expo-asset
+setCustomSourceTransformer(null);
 
 global.DETOX = !Constants.isDevice;
