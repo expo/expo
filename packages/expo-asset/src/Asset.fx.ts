467d96407afd73c3b8e76b36285e54bb45f0a9fb
Brent Vatne <brentvatne@gmail.com>
2020-03-30 22:05:14 -0700

    [asset] Make sure it continues to work in bare w/o updates (#7536)

--- packages/expo-asset/src/Asset.fx.ts
+++ packages/expo-asset/src/Asset.fx.ts
@@ -1,11 +1,16 @@
 import { Asset } from './Asset';
 import { setCustomSourceTransformer } from './resolveAssetSource';
 
-// Override React Native's asset resolution for `Image` components
+// Override React Native's asset resolution for `Image` components in contexts where it matters
 setCustomSourceTransformer(resolver => {
   try {
-    const asset = Asset.fromMetadata(resolver.asset);
-    return resolver.fromSource(asset.downloaded ? asset.localUri! : asset.uri);
+    // Bundler is using the hashAssetFiles plugin if and only if the fileHashes property exists
+    if (resolver.asset.fileHashes) {
+      const asset = Asset.fromMetadata(resolver.asset);
+      return resolver.fromSource(asset.downloaded ? asset.localUri! : asset.uri);
+    } else {
+      return resolver.defaultAsset();
+    }
   } catch (e) {
     return resolver.defaultAsset();
   }
