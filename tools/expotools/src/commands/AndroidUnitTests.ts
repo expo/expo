a1f4627de4964fcbc13996185039532f61484d02
Michał Czernek <czernekmichal@gmail.com>
2020-03-31 11:16:36 +0200

    Apply review suggestions.

--- tools/expotools/src/commands/AndroidUnitTests.ts
+++ tools/expotools/src/commands/AndroidUnitTests.ts
@@ -7,30 +7,32 @@ import * as Packages from '../Packages';
 const ANDROID_DIR = Directories.getAndroidDir();
 
 const excludedInTests = [
-  "expo-module-template",
-  "expo-bluetooth",
-  "expo-notifications",
-  "expo-in-app-purchases",
-  "expo-updates"
+  'expo-module-template',
+  'expo-bluetooth',
+  'expo-notifications',
+  'expo-in-app-purchases',
+  'expo-splash-screen',
+  'expo-updates'
 ]
 
 async function action() {
   const unimodules = await Packages.getListOfPackagesAsync();
 
-  function consoleErrorOutput(output: string, label: string, color: (string) => string): void {
+  function consoleErrorOutput(output: string, label: string, colorifyLine: (string) => string): void {
     const lines = output.trim().split(/\r\n?|\n/g);
-    console.error(lines.map(line => `${chalk.gray(label)} ${color(line)}`).join('\n'));
+    console.error(lines.map(line => `${chalk.gray(label)} ${colorifyLine(line)}`).join('\n'));
   }
 
-  let androidPackages: string[] = [];
+  let androidPackages = unimodules.filter(unimodule => {
+    const unimoduleName = unimodule?.unimoduleJson?.name;
+    return unimoduleName && unimodule.isSupportedOnPlatform('android') && !excludedInTests.includes(unimoduleName)
+  }).map(unimodule => unimodule?.unimoduleJson?.name);
+
   console.log(chalk.green('Unimodules to test: '));
-  for (const pkg of unimodules) {
-    const unimoduleName = pkg.unimoduleJson ? pkg.unimoduleJson.name : undefined;
-    if(unimoduleName && pkg.isSupportedOnPlatform('android') && !excludedInTests.includes(unimoduleName)) {
-      androidPackages = [...androidPackages, unimoduleName];
-      console.log(chalk.yellow(unimoduleName));
-    }
-  }
+  androidPackages.forEach(unimoduleName => {
+    console.log(chalk.yellow(unimoduleName))
+  });
+
   try {
     await spawnAsync('./gradlew', androidPackages.map(it => `:${it}:test`), {
       cwd: ANDROID_DIR,
@@ -50,6 +52,6 @@ async function action() {
 export default (program: any) => {
   program
     .command('android-unit-tests')
-    .description('Runs unit tests for unimodules supporting android.')
+    .description('Runs Android native unit tests for each unimodules that provides them.')
     .asyncAction(action);
 };
