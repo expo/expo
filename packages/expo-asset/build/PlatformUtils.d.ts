07135faaa2091cd0e99c42a2296e672bc57d1e74
Eric Samelson <eric@expo.io>
2020-03-30 19:45:11 -0700

    commit build

--- packages/expo-asset/build/PlatformUtils.d.ts
+++ packages/expo-asset/build/PlatformUtils.d.ts
@@ -1,4 +1,7 @@
 export declare const IS_MANAGED_ENV = true;
+export declare const IS_BARE_ENV_WITH_UPDATES = false;
+export declare const IS_BARE_ENV_WITHOUT_UPDATES = false;
+export declare function getLocalAssets(): any;
 export declare function getManifest(): {
     [key: string]: any;
 };
