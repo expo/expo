cbd1b95c8c61c95d61752deffb749e1a4b6b0e60
Evan Bacon <baconbrix@gmail.com>
2020-03-31 17:23:10 -0700

    Revert expo-google-sign-in

--- packages/expo-google-sign-in/src/GoogleSignIn.types.ts
+++ packages/expo-google-sign-in/src/GoogleSignIn.types.ts
@@ -6,22 +6,14 @@ export type GoogleSignInOptions = {
   scopes?: string[];
   webClientId?: string;
   hostedDomain?: string;
-  /**
-   * If the user's email address is known ahead of time, it can be supplied to be the default option.
-   * If the user has approved access for this app in the past then auth may return without any further interaction.
-   */
   accountName?: string;
+
   // Android
   signInType?: GoogleSignInType;
   isOfflineEnabled?: boolean;
   isPromptEnabled?: boolean;
   // iOS
   clientId?: string;
-  /**
-   * Language for the sign in UI, in the form of ISO 639-1 language code optionally followed by a dash
-   * and ISO 3166-1 alpha-2 region code, such as 'it' or 'pt-PT'.
-   * Only set this value if it's different from the system default (which you can access via expo-localization).
-   */
   language?: string;
   openIdRealm?: string;
 };
