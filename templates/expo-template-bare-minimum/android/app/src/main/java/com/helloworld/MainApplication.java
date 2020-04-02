553f0f7ca3d0ee3a947c5e014a5c5c040b6551c1
Hein Rutjes <hrutjes@gmail.com>
2020-04-02 17:46:25 +0200

    [templates] Update expo-template-bare-minimum to rn 0.61.5

--- templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
+++ templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -76,9 +76,36 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
 
     if (!BuildConfig.DEBUG) {
       UpdatesController.initialize(this);
+    }
+  }
+
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
