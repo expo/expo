e4b969cbe5bd5afa60f59acd548fac95e276d1b8
Hein Rutjes <hrutjes@gmail.com>
2020-04-03 18:06:36 +0200

    [firebase-polyfill] Cap all setTimeout calls to 60000 msec on Android

--- packages/expo-firebase-polyfill/README.md
+++ packages/expo-firebase-polyfill/README.md
@@ -19,6 +19,13 @@ import "expo-firebase-polyfill";
 import * as firebase from "firebase";
 ```
 
+Besides polyfilling API's, this package also mitigates the long running timers problem on Android by capping all `setTimeout` calls to `60000` milleseconds.
+
+> ```
+> Setting a timer for a long period of time, i.e. multiple minutes, is a performance and correctness issue on Android as it keeps the timer module awake, and timers can only be called when the app is in the foreground. See https://github.com/facebook/react-native/issues/12981 for more info.
+> ```
+
+
 # Contributing
 
 Contributions are very welcome! Please refer to guidelines described in the [contributing guide]( https://github.com/expo/expo#contributing).
--- packages/expo-firebase-polyfill/src/FirebasePolyfill.ts
+++ packages/expo-firebase-polyfill/src/FirebasePolyfill.ts
@@ -1,22 +1,44 @@
+import { Platform } from 'react-native';
+
 import { btoa, atob } from './Base64';
 import { getRandomValues } from './Crypto';
 
+// Polyfill `btoa`
 // @ts-ignore
 if (!global.btoa) {
   // @ts-ignore
   global.btoa = btoa;
 }
 
+// Polyfill `atob`
 // @ts-ignore
 if (!global.atob) {
   // @ts-ignore
   global.atob = atob;
 }
 
+// Polyfill `Crypto.getRandomValues`
 // @ts-ignore
 if (!global.crypto || !global.crypto.getRandomValues) {
   // @ts-ignore
   global.crypto = global.crypto || {};
   // @ts-ignore
   global.crypto.getRandomValues = getRandomValues;
+}
+
+// Firebase schedules timers with excessive timeouts. On Android this leads
+// to unnecessary battery drain by keeping the phone awake. React-native
+// warns for this by throwing the "Setting a timer for a long period of time.."
+// warning. To mitigate this problem, all timers are capped to 1 minute.
+if (Platform.OS === 'android') {
+  if (__DEV__) {
+    console.info(
+      `All setTimeout calls are capped to 60000 milliseconds (see https://github.com/expo/expo/tree/master/packages/expo-firebase-polyfill/)`
+    );
+  }
+  const originalSetTimeout = global.setTimeout;
+  global.setTimeout = (callback, timeout) => {
+    timeout = Math.min(60000, timeout);
+    return originalSetTimeout(callback, timeout);
+  };
 }
