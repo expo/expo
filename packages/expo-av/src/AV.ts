2f6e8299472dbab8cf97ddbdf6d4e0aeb20c6f67
Alec Winograd <awinograd@gmail.com>
2020-04-06 09:03:53 -0500

    run prettier / lint

--- packages/expo-av/src/AV.ts
+++ packages/expo-av/src/AV.ts
@@ -224,7 +224,11 @@ export interface AV {
 
 export interface Playback extends AV {
   playAsync(): Promise<AVPlaybackStatus>;
-  loadAsync(source: AVPlaybackSource, initialStatus: AVPlaybackStatusToSet, downloadAsync: boolean): Promise<AVPlaybackStatus>;
+  loadAsync(
+    source: AVPlaybackSource,
+    initialStatus: AVPlaybackStatusToSet,
+    downloadAsync: boolean
+  ): Promise<AVPlaybackStatus>;
   unloadAsync(): Promise<AVPlaybackStatus>;
   playFromPositionAsync(
     positionMillis: number,
