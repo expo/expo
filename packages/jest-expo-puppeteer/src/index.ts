736bb8f173f4486b6c07e150fe38340a1f1c2d66
Evan Bacon <baconbrix@gmail.com>
2020-04-08 12:44:43 -0700

    use getConfig and getWebOutputPath

--- packages/jest-expo-puppeteer/src/index.ts
+++ packages/jest-expo-puppeteer/src/index.ts
@@ -1,4 +1,4 @@
-import { readConfigJson } from '@expo/config';
+import { getWebOutputPath, getConfig } from '@expo/config';
 import fs from 'fs';
 import { boolish } from 'getenv';
 import path from 'path';
@@ -37,10 +37,6 @@ export function withExpoPuppeteer(config: any = {}): { [key: string]: any } {
   } = config;
   const projectPath = path.resolve(projectRoot || process.cwd());
 
-  const {
-    exp: { web = {} },
-  } = readConfigJson(projectPath);
-
   const { port: serverPort = 5000 } = server;
   let defaultURL;
   let command;
@@ -51,7 +47,8 @@ export function withExpoPuppeteer(config: any = {}): { [key: string]: any } {
   if (mode === 'production') {
     defaultURL = `http://localhost:${serverPort}`;
 
-    const outputBuildPath = (web.build || {}).output || 'web-build';
+    const { exp } = getConfig(projectPath, { skipSDKVersionRequirement: true });
+    const outputBuildPath = getWebOutputPath(exp);
 
     const buildFolder = path.resolve(projectPath, outputBuildPath);
     const serveCommand = `serve ${buildFolder}`;
