c8b8e9b68dbd97bcd51e5e5e58cd97c2a67b288a
Evan Bacon <baconbrix@gmail.com>
2020-04-08 19:43:42 -0700

    Remove build files

--- packages/expo-auth-session/build/Storage.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function setItemAsync(name: string, value: string): Promise<void>;
-export declare function deleteItemAsync(name: string): Promise<void>;
-export declare function getItemAsync(name: string): Promise<string | null>;
