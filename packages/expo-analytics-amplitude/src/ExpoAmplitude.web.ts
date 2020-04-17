1be028a9e0c0c51ced3dbe9bbf2590a8d613c633
Chris Drackett <chrisdrackett@users.noreply.github.com>
2020-04-17 14:02:54 -0700

    adding amplitude for web
    closes #6886

--- packages/expo-analytics-amplitude/src/ExpoAmplitude.web.ts
+++ packages/expo-analytics-amplitude/src/ExpoAmplitude.web.ts
@@ -1,5 +1,20 @@
-export default {
-  get name(): string {
-    return 'ExpoAmplitude';
+import amplitude from 'amplitude-js'
+
+const ExpoAmplitude = {
+  initialize: (apiKey: string, options?: amplitude.Config) => {
+    amplitude.getInstance().init(apiKey, undefined, options)
   },
-};
+  setUserId: amplitude.getInstance().setUserId,
+  setUserProperties: amplitude.getInstance().setUserProperties,
+  clearUserProperties: amplitude.getInstance().clearUserProperties,
+  logEvent: amplitude.getInstance().logEvent,
+  logEventWithProperties: (
+    eventName: string,
+    properties: { [name: string]: any },
+  ) => {
+    amplitude.getInstance().logEvent(eventName, properties)
+  },
+  setGroup: amplitude.getInstance().setGroup,
+}
+
+export default ExpoAmplitude
