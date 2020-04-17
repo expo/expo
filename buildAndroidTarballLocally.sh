c761abb283ec86d5cebad49b6a92f05f0c4896e1
Stanisław Chmiela <sjchmiela@gmail.com>
2020-04-17 12:30:24 +0200

    [ci] Make shell app smaller by not including obsolete artifacts

--- buildAndroidTarballLocally.sh
+++ buildAndroidTarballLocally.sh
@@ -10,18 +10,19 @@ mkdir -p $ARTIFACTS_DIR
 rm -rf $TEMP_DIR
 mkdir -p $TEMP_DIR
 ln -s ${ROOT_DIR}/tools-public $TEMP_DIR/tools-public
-ln -s ${ROOT_DIR}/secrets $TEMP_DIR/secrets
 ln -s ${ROOT_DIR}/template-files $TEMP_DIR/template-files
 ln -s ${ROOT_DIR}/android $TEMP_DIR/android
 ln -s ${ROOT_DIR}/package.json $TEMP_DIR/package.json
 ln -s ${ROOT_DIR}/expokit-npm-package $TEMP_DIR/expokit-npm-package
 ln -s ${ROOT_DIR}/packages $TEMP_DIR/packages
 
+# We wouldn't want to use any versioned ABI in a standalone app
+# and it makes the shell app smaller.
+rm -rf $TEMP_DIR/android/versioned-abis
+
 # generate dynamic macros
 mkdir -p $TEMP_DIR/android/expoview/src/main/java/host/exp/exponent/generated/
-cd $TEMP_DIR/tools-public
 et android-generate-dynamic-macros --configuration release
-rm -rf $TEMP_DIR/secrets
 
 cd $TEMP_DIR; tar -czhf $ARTIFACTS_DIR/android-shell-builder.tar.gz .
 rm -rf $TEMP_DIR
