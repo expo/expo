786db5822404453f5999740e0dfbcc87d7ea55ba
Evan Bacon <baconbrix@gmail.com>
2020-04-20 16:58:39 -0700

    Created expo-linking package

---
+++ packages/expo-linking/README.md
@@ -0,0 +1,11 @@
+# expo-linking
+
+Great new module!
+
+# API documentation
+
+# Installation in managed Expo projects
+
+# Installation in bare React Native projects
+
+# Contributing
---
+++ packages/expo-linking/src/__tests__/Linking-test.ts
@@ -0,0 +1,7 @@
+import * as Linking from '../Linking';
+
+describe('Linking', () => {
+  it('someGreatMethodAsync is defined', async () => {
+    expect(Linking.someGreatMethodAsync).toBeDefined();
+  });
+});
