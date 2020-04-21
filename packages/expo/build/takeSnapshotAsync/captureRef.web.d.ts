e0bb13065c9f7ac2ffefd3528ed4de3df98dc78f
Evan Bacon <baconbrix@gmail.com>
2020-04-21 16:16:21 -0700

    Remove unused takeSnapshotAsync web code

--- packages/expo/build/takeSnapshotAsync/captureRef.web.d.ts
+++
@@ -1,7 +0,0 @@
-import { CaptureOptions } from 'react-native-view-shot';
-/**
- * Taking a snapshot of DOM is not part of native browser behavior.
- * This is a hack to best emulate mobile functionality.
- * This implementation is based on https://github.com/pbakaus/domvas by Paul Bakaus http://paulbakaus.com/
- */
-export default function captureRef(component: Element, options?: CaptureOptions): Promise<string | Uint8ClampedArray | Blob>;
