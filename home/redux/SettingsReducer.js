f553ac329074558d441bb1d6c349d1b82b701169
Tomasz Sapeta <1714764+tsapeta@users.noreply.github.com>
2020-04-21 22:23:05 +0200

    [ios][home] Add three finger gesture to open the dev menu + some refactoring (#7891)
    # Why
    
    Fixes #7644 and brings us closer to the separate `expo-dev-menu` package that can live without kernel.
    Also fixed https://twitter.com/bantingGamer/status/1251154340129062917
    
    # How
    
    - Added `EXDevMenuManager` singleton that manages the dev menu. Most parts of it were in kernel or some others classes strongly connected with kernel. Now, kernel is just its delegate.
    - Refactored `EXKernelDevMotionHandler` to `EXDevMenuMotionInterceptor` which is much clearer and makes it possible to uninstall the interceptor (these interceptors are kinda hacky, I tried not to do them this way but well, RN does the same 🤷).
    - Added `EXDevMenuGestureRecognizer` for long press gesture, with minimum press duration set to 0.5s and with light impact haptic feedback.
    - Added `EXDevMenuGestureInterceptor` that swaps the default implementation of `UIView#gestureRecognizers`, but only for `UIWindow`s. This is needed to make three-finger gesture work everywhere in the app.
    - Renamed `EXMenuViewController` to `EXDevMenuViewController` (all dev menu classes should be prefixed by `EXDevMenu`) and again, removed some connections to kernel.
    - Added UI in user settings to select dev menu gestures (can select them separately).
    - Fixed an issue with dev menu view being below modals in the view hierarchy.
    
    # Test Plan
    
    So far I've tested it only with published experience. I'm gonna do smoke tests on dev apps later.
    I mostly focused on testing whether:
    - the dev menu works in general (after refactoring)
    - three-finger gesture works on the initial screen of the app, on the modals and on the dev menu itself
    - all combinations of selected gestures work as expected
    - selected gestures persist after killing and relaunching the client

--- home/redux/SettingsReducer.js
+++ home/redux/SettingsReducer.js
@@ -2,6 +2,7 @@ import { Record } from 'immutable';
 
 const SettingsState = Record({
   preferredAppearance: 'no-preference',
+  devMenuSettings: null,
 });
 
 export default (state, action) => {
@@ -11,6 +12,10 @@ export default (state, action) => {
     case 'setPreferredAppearance': {
       const { preferredAppearance } = action.payload;
       return state.merge({ preferredAppearance });
+    }
+    case 'setDevMenuSettings': {
+      const devMenuSettings = state.get('devMenuSettings');
+      return state.set('devMenuSettings', { ...devMenuSettings, ...action.payload });
     }
     default:
       return state || new SettingsState();
---
+++ ios/Client/Menu/EXDevMenuGestureRecognizer.h
@@ -0,0 +1,7 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@interface EXDevMenuGestureRecognizer : UILongPressGestureRecognizer
+
+@end
