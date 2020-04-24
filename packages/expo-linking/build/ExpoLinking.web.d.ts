f60b83a3f353f4a7fe2bc9df80d3da8f90bcd331
Evan Bacon <baconbrix@gmail.com>
2020-04-24 16:01:23 -0700

    Create expo-linking (#7934)
    * Created expo-linking package
    
    * Move tests
    
    * Deprecate expo Linking
    
    * Updated config
    
    * updated build
    
    * Updated linking
    
    * Updated linking tests
    
    * Updated build files
    
    * Updated docs
    
    * Fix expo build files
    
    * Update Expo-test.native.ts
    
    * reexport Linking from expo
    
    * Updated linking API to match Expo standard library
    
    * Update linking.md
    
    * Update linking.md
    
    * Remove unused bare case
    
    * Fix build files
    
    * Added jest mocks
    
    * Updated linking binding and demo
    
    * fix build files
    
    * Just use Linking

---
+++ packages/expo-linking/build/ExpoLinking.web.d.ts
@@ -0,0 +1,9 @@
+import { URLListener } from './Linking.types';
+declare const _default: {
+    addEventListener(type: "url", listener: URLListener): void;
+    removeEventListener(type: "url", listener: URLListener): void;
+    canOpenURL(url: string): Promise<boolean>;
+    getInitialURL(): Promise<string>;
+    openURL(url: string): Promise<void>;
+};
+export default _default;
