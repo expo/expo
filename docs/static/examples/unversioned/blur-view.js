085eca39cf5e1bbe39175002867ed83851652d5b
Cedric van Putten <me@bycedric.com>
2020-04-24 21:01:01 +0200

    [docs] fix constants in blurview example (#7988)

--- docs/static/examples/unversioned/blur-view.js
+++ docs/static/examples/unversioned/blur-view.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, Text, StyleSheet, View } from 'react-native';
+import Constants from 'expo-constants';
 import { BlurView } from 'expo-blur';
+
 const uri = 'https://s3.amazonaws.com/exp-icon-assets/ExpoEmptyManifest_192.png';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image style={{ width: 192, height: 192 }} source={{ uri }} />
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Image style={styles.blurredImage} source={{ uri }} />
 
-      {/* Adjust the tint and intensity */}
-      <BlurView tint="light" intensity={50} style={styles.notBlurred}>
-        <Image style={{ width: 96, height: 96 }} source={{ uri }} />
-      </BlurView>
+        {/* Adjust the tint and intensity */}
+        <BlurView intensity={100} style={[StyleSheet.absoluteFill, styles.nonBlurredContent]}>
+          <Text>Hello! I am bluring contents underneath</Text>
+        </BlurView>
+      </View>
     </View>
   );
 }
@@ -19,11 +23,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  notBlurred: {
-    ...StyleSheet.absoluteFill,
-    top: Constants.statusBarHeight,
+  blurredImage: {
+    width: 192,
+    height: 192,
+  },
+  nonBlurredContent: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
