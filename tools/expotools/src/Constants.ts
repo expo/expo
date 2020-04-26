f7592f5fe2bc384100ec264383ce04d32c924fca
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2020-04-26 22:17:58 +0200

    [et] Add TasksRunner performing given tasks and helping in managing backups of their state

--- tools/expotools/src/Constants.ts
+++ tools/expotools/src/Constants.ts
@@ -4,6 +4,7 @@ export const STAGING_API_HOST = 'staging.exp.host';
 export const PRODUCTION_API_HOST = 'exp.host';
 
 export const EXPO_DIR = Directories.getExpoRepositoryRootDir();
+export const EXPOTOOLS_DIR = Directories.getExpotoolsDir();
 export const IOS_DIR = Directories.getIosDir();
 export const ANDROID_DIR = Directories.getAndroidDir();
 export const TEMPLATES_DIR = Directories.getTemplatesDir();
