00fa3922c17fc0b02d4395b6968379187aa71cc8
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2020-04-26 21:48:18 +0200

    [et] Extend Changelogs.ts with new features

--- tools/expotools/src/Changelogs.ts
+++ tools/expotools/src/Changelogs.ts
@@ -1,4 +1,5 @@
 import fs from 'fs-extra';
+import semver from 'semver';
 
 import * as Markdown from './Markdown';
 
@@ -26,6 +27,18 @@ export type ChangelogEntry = {
    * The changelog section which contains this entry.
    */
   version: string;
+};
+
+/**
+ * Type of the objects representing changelog entries.
+ */
+export type ChangelogChanges = {
+  totalCount: number;
+  versions: {
+    [key: string]: {
+      [key in ChangeType]?: string[];
+    };
+  };
 };
 
 /**
@@ -63,14 +76,107 @@ export class Changelog {
     this.filePath = filePath;
   }
 
+  /**
+   * Resolves to `true` if changelog file exists, `false` otherwise.
+   */
+  async fileExistsAsync(): Promise<boolean> {
+    return await fs.pathExists(this.filePath);
+  }
+
+  /**
+   * Lexifies changelog content and returns resulting tokens.
+   */
   async getTokensAsync(): Promise<Markdown.Tokens> {
     if (!this.tokens) {
-      await fs.access(this.filePath, fs.constants.R_OK);
-      this.tokens = Markdown.lexify(await fs.readFile(this.filePath, 'utf8'));
+      try {
+        await fs.access(this.filePath, fs.constants.R_OK);
+        this.tokens = Markdown.lexify(await fs.readFile(this.filePath, 'utf8'));
+      } catch (error) {
+        this.tokens = [];
+      }
     }
     return this.tokens;
   }
 
+  /**
+   * Reads versions headers, collects those versions and returns them.
+   */
+  async getVersionsAsync(): Promise<string[]> {
+    const tokens = await this.getTokensAsync();
+    const versionTokens = tokens.filter(
+      (token) => token.type === Markdown.TokenType.HEADING && token.depth === VERSION_HEADING_DEPTH
+    ) as Markdown.HeadingToken[];
+
+    return versionTokens.map((token) => token.text);
+  }
+
+  /**
+   * Returns the last version in changelog.
+   */
+  async getLastPublishedVersionAsync(): Promise<string | null> {
+    const versions = await this.getVersionsAsync();
+    return versions.find((version) => semver.valid(version)) ?? null;
+  }
+
+  /**
+   * Reads changes between two given versions and returns them in JS object format.
+   * If called without params, then only unpublished changes are returned.
+   */
+  async getChangesAsync(
+    fromVersion?: string,
+    toVersion: string = UNPUBLISHED_VERSION_NAME
+  ): Promise<ChangelogChanges> {
+    const tokens = await this.getTokensAsync();
+    const versions = {};
+    const changes: ChangelogChanges = { totalCount: 0, versions };
+
+    let currentVersion: string | null = null;
+    let currentSection: string | null = null;
+
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+
+      if (token.type === Markdown.TokenType.HEADING) {
+        if (token.depth === VERSION_HEADING_DEPTH) {
+          if (token.text !== toVersion && (!fromVersion || token.text === fromVersion)) {
+            // We've iterated over everything we needed, stop the loop.
+            break;
+          }
+
+          currentVersion = token.text === UNPUBLISHED_VERSION_NAME ? 'unpublished' : token.text;
+          currentSection = null;
+
+          if (!versions[currentVersion]) {
+            versions[currentVersion] = {};
+          }
+        } else if (currentVersion && token.depth === CHANGE_TYPE_HEADING_DEPTH) {
+          currentSection = token.text;
+
+          if (!versions[currentVersion][currentSection]) {
+            versions[currentVersion][currentSection] = [];
+          }
+        }
+        continue;
+      }
+
+      if (currentVersion && currentSection && token.type === Markdown.TokenType.LIST_ITEM_START) {
+        i++;
+        for (; tokens[i].type !== Markdown.TokenType.LIST_ITEM_END; i++) {
+          const token = tokens[i] as Markdown.TextToken;
+
+          if (token.text) {
+            changes.totalCount++;
+            versions[currentVersion][currentSection].push(token.text);
+          }
+        }
+      }
+    }
+    return changes;
+  }
+
+  /**
+   * Inserts given entry to changelog.
+   */
   async addChangeAsync(entry: ChangelogEntry): Promise<void> {
     const tokens = [...(await this.getTokensAsync())];
     const sectionIndex = tokens.findIndex(
@@ -149,6 +255,49 @@ export class Changelog {
     }
 
     throw new Error(`Cound't find '${entry.type}' section.`);
+  }
+
+  /**
+   * Renames header of unpublished changes to given version and adds new section with unpublished changes on top.
+   */
+  async cutOffAsync(version: string): Promise<void> {
+    const tokens = [...(await this.getTokensAsync())];
+    const firstVersionHeadingIndex = tokens.findIndex(
+      (token) => token.type === Markdown.TokenType.HEADING && token.depth === VERSION_HEADING_DEPTH
+    );
+    const newSectionTokens: Markdown.Tokens = [
+      {
+        type: Markdown.TokenType.HEADING,
+        depth: VERSION_HEADING_DEPTH,
+        text: UNPUBLISHED_VERSION_NAME,
+      },
+      {
+        type: Markdown.TokenType.HEADING,
+        depth: CHANGE_TYPE_HEADING_DEPTH,
+        text: ChangeType.BREAKING_CHANGES,
+      },
+      {
+        type: Markdown.TokenType.HEADING,
+        depth: CHANGE_TYPE_HEADING_DEPTH,
+        text: ChangeType.NEW_FEATURES,
+      },
+      {
+        type: Markdown.TokenType.HEADING,
+        depth: CHANGE_TYPE_HEADING_DEPTH,
+        text: ChangeType.BUG_FIXES,
+      },
+    ];
+
+    if (firstVersionHeadingIndex !== -1) {
+      (tokens[firstVersionHeadingIndex] as Markdown.HeadingToken).text = version;
+    }
+
+    tokens.splice(firstVersionHeadingIndex, 0, ...newSectionTokens);
+    await fs.outputFile(this.filePath, Markdown.parse(tokens));
+
+    // Reset cached tokens as we just modified the file.
+    // We could use an array with new tokens here, but just for safety, let them be reloaded.
+    this.tokens = null;
   }
 }
 
