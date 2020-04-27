fc280efd289febd34d7bced59e9a3a5982403044
cruzach <charliecruzan@gmail.com>
2020-04-27 15:50:09 -0400

    [home] fix old docs link so they don't trigger a redirect or 404 (#8043)

--- home/components/NoProjectTools.js
+++ home/components/NoProjectTools.js
@@ -29,7 +29,7 @@ export default class QRCodeButton extends React.Component {
   }
 
   _handlePressAsync = async () => {
-    Linking.openURL('https://docs.expo.io/versions/latest/introduction/installation/');
+    Linking.openURL('https://docs.expo.io/get-started/installation/');
   };
 }
 
