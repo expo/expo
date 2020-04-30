ac316b4284c6e56dd501d15480ec93fd3cfc0b56
Cedric van Putten <me@bycedric.com>
2020-04-30 23:51:21 +0200

    [expo] add apploading polyfill for bare projects

---
+++ packages/expo/src/launch/SplashScreen.expo.ts
@@ -0,0 +1,15 @@
+import { NativeModules } from 'react-native';
+
+const { ExponentSplashScreen: SplashScreen = {} } = NativeModules;
+
+export function preventAutoHide() {
+  if (SplashScreen.preventAutoHide) {
+    SplashScreen.preventAutoHide();
+  }
+}
+
+export function hide() {
+  if (SplashScreen.hide) {
+    SplashScreen.hide();
+  }
+}
