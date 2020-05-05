c2b98915f845c92bae3385834a18d0a050dd37d5
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-05-05 16:45:40 +0200

    [splash-screen-command] Remove module in favour of new expo/expo-cli#@expo/configure-splash-screen

--- packages/expo-splash-screen-command/bin/configureIosSplashScreen.d.ts
+++
@@ -1,6 +0,0 @@
-import { ResizeMode } from './constants';
-export default function configureIosSplashScreen({ imagePath, resizeMode, backgroundColor, }: {
-    imagePath?: string;
-    resizeMode: ResizeMode;
-    backgroundColor: string;
-}): Promise<void>;
