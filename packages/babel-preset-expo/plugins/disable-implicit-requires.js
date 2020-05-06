5bc227a7f4fc1bf50d93ece002e5a4030ab3dec1
Evan Bacon <baconbrix@gmail.com>
2020-05-05 19:14:29 -0700

    Update disable-implicit-requires.js

--- packages/babel-preset-expo/plugins/disable-implicit-requires.js
+++ packages/babel-preset-expo/plugins/disable-implicit-requires.js
@@ -4,18 +4,23 @@
 module.exports = () => ({
   visitor: {
     AssignmentExpression(path) {
-      if (path.node.operator !== '=' || path.node.left.type !== 'MemberExpression') {
-        return;
-      }
-
-      const { object } = path.node.left;
-
-      if (
-        (object.type === 'TypeCastExpression' && object.expression.name === 'require') ||
-        (object.type === 'Identifier' && object.name === 'require')
-      ) {
+      if (!isValidRequire(path) && (isCastedRequire(path) || isChainedRequire(path))) {
         path.remove();
       }
     },
   },
 });
+
+function isValidRequire(path) {
+  return path.node.operator !== '=' || path.node.left.type !== 'MemberExpression';
+}
+
+function isCastedRequire(path) {
+  const { object } = path.node.left;
+  return object.type === 'TypeCastExpression' && object.expression.name === 'require';
+}
+
+function isChainedRequire(path) {
+  const { object } = path.node.left;
+  return object.type === 'Identifier' && object.name === 'require';
+}
