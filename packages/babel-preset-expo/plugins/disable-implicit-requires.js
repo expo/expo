8251280051c39056ae4001fde1e08fd2552d4741
Evan Bacon <baconbrix@gmail.com>
2020-05-05 19:11:46 -0700

    Update disable-implicit-requires.js

--- packages/babel-preset-expo/plugins/disable-implicit-requires.js
+++ packages/babel-preset-expo/plugins/disable-implicit-requires.js
@@ -4,17 +4,11 @@
 module.exports = () => ({
   visitor: {
     AssignmentExpression(path) {
-      if (path.node.operator !== '=') {
+      if (path.node.operator !== '=' || path.node.left.type !== 'MemberExpression') {
         return;
       }
 
-      const { left } = path.node;
-
-      if (left.type !== 'MemberExpression') {
-        return;
-      }
-
-      const { object } = left;
+      const { object } = path.node.left;
 
       if (
         (object.type === 'TypeCastExpression' && object.expression.name === 'require') ||
