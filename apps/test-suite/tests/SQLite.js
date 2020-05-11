95cd0306586afa1cc2473b5bfac753c950a98c18
Manvir Singh <gurs1kh@live.com>
2020-05-10 21:32:14 -0700

    [expo-sqlite][tests] updated expo-sqlite test to test multiple types of params

--- apps/test-suite/tests/SQLite.js
+++ apps/test-suite/tests/SQLite.js
@@ -9,57 +9,260 @@ export const name = 'SQLite';
 // TODO: Only tests successful cases, needs to test error cases like bad database name etc.
 export function test(t) {
   t.describe('SQLite', () => {
-    t.it('should be able to drop + create a table, insert, query', async () => {
-      const db = SQLite.openDatabase('test.db');
-      await new Promise((resolve, reject) => {
-        db.transaction(
-          tx => {
-            const nop = () => {};
-            const onError = (tx, error) => reject(error);
+    const parameters = [
+      {
+        describe: 'when fileInfo is name as string',
+        fileInfo: 'test.db',
+      },
+      {
+        describe: 'when fileInfo is object with just the name ',
+        fileInfo: { name: 'test.db' },
+      },
+      {
+        describe: 'when fileInfo is object with name and empty key',
+        fileInfo: { name: 'test.db', key: '' },
+      },
+      {
+        describe: 'when fileInfo is object with name and actual key',
+        fileInfo: { name: 'test.db', key: 'qwertyuiop' },
+      },
+    ];
 
-            tx.executeSql('DROP TABLE IF EXISTS Users;', [], nop, onError);
-            tx.executeSql(
-              'CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64), k INT, j REAL);',
-              [],
-              nop,
-              onError
-            );
-            tx.executeSql(
-              'INSERT INTO Users (name, k, j) VALUES (?, ?, ?)',
-              ['Tim Duncan', 1, 23.4],
-              nop,
-              onError
-            );
-            tx.executeSql(
-              'INSERT INTO Users (name, k, j) VALUES ("Manu Ginobili", 5, 72.8)',
-              [],
-              nop,
-              onError
-            );
-            tx.executeSql(
-              'INSERT INTO Users (name, k, j) VALUES ("Nikhilesh Sigatapu", 7, 42.14)',
-              [],
-              nop,
-              onError
-            );
+    parameters.forEach(({ describe, fileInfo }) => {
+      t.describe(describe + JSON.stringify(fileInfo), () => {
+        t.it('should be able to drop + create a table, insert, query', async () => {
+          const db = SQLite.openDatabase(fileInfo);
+          await new Promise((resolve, reject) => {
+            db.transaction(
+              tx => {
+                const nop = () => {};
+                const onError = (tx, error) => reject(error);
 
-            tx.executeSql(
-              'SELECT * FROM Users',
-              [],
-              (tx, results) => {
-                t.expect(results.rows.length).toEqual(3);
-                t.expect(results.rows._array[0].j).toBeCloseTo(23.4);
+                tx.executeSql('DROP TABLE IF EXISTS Users;', [], nop, onError);
+                tx.executeSql(
+                  'CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64), k INT, j REAL);',
+                  [],
+                  nop,
+                  onError
+                );
+                tx.executeSql(
+                  'INSERT INTO Users (name, k, j) VALUES (?, ?, ?)',
+                  ['Tim Duncan', 1, 23.4],
+                  nop,
+                  onError
+                );
+                tx.executeSql(
+                  'INSERT INTO Users (name, k, j) VALUES ("Manu Ginobili", 5, 72.8)',
+                  [],
+                  nop,
+                  onError
+                );
+                tx.executeSql(
+                  'INSERT INTO Users (name, k, j) VALUES ("Nikhilesh Sigatapu", 7, 42.14)',
+                  [],
+                  nop,
+                  onError
+                );
+
+                tx.executeSql(
+                  'SELECT * FROM Users',
+                  [],
+                  (tx, results) => {
+                    t.expect(results.rows.length).toEqual(3);
+                    t.expect(results.rows._array[0].j).toBeCloseTo(23.4);
+                  },
+                  onError
+                );
               },
-              onError
+              reject,
+              resolve
             );
-          },
-          reject,
-          resolve
-        );
-      });
+          });
 
-      const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
-      t.expect(exists).toBeTruthy();
+          const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
+          t.expect(exists).toBeTruthy();
+        });
+
+        t.it('should be able to recreate db from scratch by deleting file', async () => {
+          {
+            const db = SQLite.openDatabase(fileInfo);
+            await new Promise((resolve, reject) => {
+              db.transaction(
+                tx => {
+                  const nop = () => {};
+                  const onError = (tx, error) => reject(error);
+
+                  tx.executeSql('DROP TABLE IF EXISTS Users;', [], nop, onError);
+                  tx.executeSql(
+                    'CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64), k INT, j REAL);',
+                    [],
+                    nop,
+                    onError
+                  );
+                  tx.executeSql(
+                    'INSERT INTO Users (name, k, j) VALUES (?, ?, ?)',
+                    ['Tim Duncan', 1, 23.4],
+                    nop,
+                    onError
+                  );
+
+                  tx.executeSql(
+                    'SELECT * FROM Users',
+                    [],
+                    (tx, results) => {
+                      t.expect(results.rows.length).toEqual(1);
+                    },
+                    onError
+                  );
+                },
+                reject,
+                resolve
+              );
+            });
+          }
+
+          {
+            const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
+            t.expect(exists).toBeTruthy();
+          }
+
+          {
+            await FS.deleteAsync(`${FS.documentDirectory}SQLite/test.db`);
+            const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
+            t.expect(exists).toBeFalsy();
+          }
+
+          {
+            const db = SQLite.openDatabase(fileInfo);
+            await new Promise((resolve, reject) => {
+              db.transaction(
+                tx => {
+                  const nop = () => {};
+                  const onError = (tx, error) => reject(error);
+
+                  tx.executeSql('DROP TABLE IF EXISTS Users;', [], nop, onError);
+                  tx.executeSql(
+                    'CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64), k INT, j REAL);',
+                    [],
+                    nop,
+                    onError
+                  );
+                  tx.executeSql(
+                    'SELECT * FROM Users',
+                    [],
+                    (tx, results) => {
+                      t.expect(results.rows.length).toEqual(0);
+                    },
+                    onError
+                  );
+
+                  tx.executeSql(
+                    'INSERT INTO Users (name, k, j) VALUES (?, ?, ?)',
+                    ['Tim Duncan', 1, 23.4],
+                    nop,
+                    onError
+                  );
+                  tx.executeSql(
+                    'SELECT * FROM Users',
+                    [],
+                    (tx, results) => {
+                      t.expect(results.rows.length).toEqual(1);
+                    },
+                    onError
+                  );
+                },
+                reject,
+                resolve
+              );
+            });
+          }
+        });
+
+        t.it('should maintain correct type of potentialy null bind parameters', async () => {
+          const db = SQLite.openDatabase(fileInfo);
+          await new Promise((resolve, reject) => {
+            db.transaction(
+              tx => {
+                const nop = () => {};
+                const onError = (tx, error) => reject(error);
+
+                tx.executeSql('DROP TABLE IF EXISTS Nulling;', [], nop, onError);
+                tx.executeSql(
+                  'CREATE TABLE IF NOT EXISTS Nulling (id INTEGER PRIMARY KEY NOT NULL, x NUMERIC, y NUMERIC)',
+                  [],
+                  nop,
+                  onError
+                );
+                tx.executeSql('INSERT INTO Nulling (x, y) VALUES (?, ?)', [null, null], nop, onError);
+                tx.executeSql('INSERT INTO Nulling (x, y) VALUES (null, null)', [], nop, onError);
+
+                tx.executeSql(
+                  'SELECT * FROM Nulling',
+                  [],
+                  (tx, results) => {
+                    t.expect(results.rows._array[0].x).toBeNull();
+                    t.expect(results.rows._array[0].y).toBeNull();
+                    t.expect(results.rows._array[1].x).toBeNull();
+                    t.expect(results.rows._array[1].y).toBeNull();
+                  },
+                  onError
+                );
+              },
+              reject,
+              resolve
+            );
+          });
+
+          const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
+          t.expect(exists).toBeTruthy();
+        });
+
+        t.it('should support PRAGMA statements', async () => {
+          const db = SQLite.openDatabase(fileInfo);
+          await new Promise((resolve, reject) => {
+            db.transaction(
+              tx => {
+                const nop = () => {};
+                const onError = (tx, error) => reject(error);
+
+                tx.executeSql('DROP TABLE IF EXISTS SomeTable;', [], nop, onError);
+                tx.executeSql(
+                  'CREATE TABLE IF NOT EXISTS SomeTable (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64));',
+                  [],
+                  nop,
+                  onError
+                );
+                // a result-returning pragma
+                tx.executeSql(
+                  'PRAGMA table_info(SomeTable);',
+                  [],
+                  (tx, results) => {
+                    t.expect(results.rows.length).toEqual(2);
+                    t.expect(results.rows._array[0].name).toEqual('id');
+                    t.expect(results.rows._array[1].name).toEqual('name');
+                  },
+                  onError
+                );
+                // a no-result pragma
+                tx.executeSql('PRAGMA case_sensitive_like = true;', [], nop, onError);
+                // a setter/getter pragma
+                tx.executeSql('PRAGMA user_version = 123;', [], nop, onError);
+                tx.executeSql(
+                  'PRAGMA user_version;',
+                  [],
+                  (tx, results) => {
+                    t.expect(results.rows.length).toEqual(1);
+                    t.expect(results.rows._array[0].user_version).toEqual(123);
+                  },
+                  onError
+                );
+              },
+              reject,
+              resolve
+            );
+          });
+        });
+      });
     });
 
     t.it(
@@ -93,185 +296,5 @@ export function test(t) {
       },
       30000
     );
-
-    t.it('should be able to recreate db from scratch by deleting file', async () => {
-      {
-        const db = SQLite.openDatabase('test.db');
-        await new Promise((resolve, reject) => {
-          db.transaction(
-            tx => {
-              const nop = () => {};
-              const onError = (tx, error) => reject(error);
-
-              tx.executeSql('DROP TABLE IF EXISTS Users;', [], nop, onError);
-              tx.executeSql(
-                'CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64), k INT, j REAL);',
-                [],
-                nop,
-                onError
-              );
-              tx.executeSql(
-                'INSERT INTO Users (name, k, j) VALUES (?, ?, ?)',
-                ['Tim Duncan', 1, 23.4],
-                nop,
-                onError
-              );
-
-              tx.executeSql(
-                'SELECT * FROM Users',
-                [],
-                (tx, results) => {
-                  t.expect(results.rows.length).toEqual(1);
-                },
-                onError
-              );
-            },
-            reject,
-            resolve
-          );
-        });
-      }
-
-      {
-        const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
-        t.expect(exists).toBeTruthy();
-      }
-
-      {
-        await FS.deleteAsync(`${FS.documentDirectory}SQLite/test.db`);
-        const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
-        t.expect(exists).toBeFalsy();
-      }
-
-      {
-        const db = SQLite.openDatabase('test.db');
-        await new Promise((resolve, reject) => {
-          db.transaction(
-            tx => {
-              const nop = () => {};
-              const onError = (tx, error) => reject(error);
-
-              tx.executeSql('DROP TABLE IF EXISTS Users;', [], nop, onError);
-              tx.executeSql(
-                'CREATE TABLE IF NOT EXISTS Users (user_id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64), k INT, j REAL);',
-                [],
-                nop,
-                onError
-              );
-              tx.executeSql(
-                'SELECT * FROM Users',
-                [],
-                (tx, results) => {
-                  t.expect(results.rows.length).toEqual(0);
-                },
-                onError
-              );
-
-              tx.executeSql(
-                'INSERT INTO Users (name, k, j) VALUES (?, ?, ?)',
-                ['Tim Duncan', 1, 23.4],
-                nop,
-                onError
-              );
-              tx.executeSql(
-                'SELECT * FROM Users',
-                [],
-                (tx, results) => {
-                  t.expect(results.rows.length).toEqual(1);
-                },
-                onError
-              );
-            },
-            reject,
-            resolve
-          );
-        });
-      }
-    });
-
-    t.it('should maintain correct type of potentialy null bind parameters', async () => {
-      const db = SQLite.openDatabase('test.db');
-      await new Promise((resolve, reject) => {
-        db.transaction(
-          tx => {
-            const nop = () => {};
-            const onError = (tx, error) => reject(error);
-
-            tx.executeSql('DROP TABLE IF EXISTS Nulling;', [], nop, onError);
-            tx.executeSql(
-              'CREATE TABLE IF NOT EXISTS Nulling (id INTEGER PRIMARY KEY NOT NULL, x NUMERIC, y NUMERIC)',
-              [],
-              nop,
-              onError
-            );
-            tx.executeSql('INSERT INTO Nulling (x, y) VALUES (?, ?)', [null, null], nop, onError);
-            tx.executeSql('INSERT INTO Nulling (x, y) VALUES (null, null)', [], nop, onError);
-
-            tx.executeSql(
-              'SELECT * FROM Nulling',
-              [],
-              (tx, results) => {
-                t.expect(results.rows._array[0].x).toBeNull();
-                t.expect(results.rows._array[0].y).toBeNull();
-                t.expect(results.rows._array[1].x).toBeNull();
-                t.expect(results.rows._array[1].y).toBeNull();
-              },
-              onError
-            );
-          },
-          reject,
-          resolve
-        );
-      });
-
-      const { exists } = await FS.getInfoAsync(`${FS.documentDirectory}SQLite/test.db`);
-      t.expect(exists).toBeTruthy();
-    });
-
-    t.it('should support PRAGMA statements', async () => {
-      const db = SQLite.openDatabase('test.db');
-      await new Promise((resolve, reject) => {
-        db.transaction(
-          tx => {
-            const nop = () => {};
-            const onError = (tx, error) => reject(error);
-
-            tx.executeSql('DROP TABLE IF EXISTS SomeTable;', [], nop, onError);
-            tx.executeSql(
-              'CREATE TABLE IF NOT EXISTS SomeTable (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(64));',
-              [],
-              nop,
-              onError
-            );
-            // a result-returning pragma
-            tx.executeSql(
-              'PRAGMA table_info(SomeTable);',
-              [],
-              (tx, results) => {
-                t.expect(results.rows.length).toEqual(2);
-                t.expect(results.rows._array[0].name).toEqual('id');
-                t.expect(results.rows._array[1].name).toEqual('name');
-              },
-              onError
-            );
-            // a no-result pragma
-            tx.executeSql('PRAGMA case_sensitive_like = true;', [], nop, onError);
-            // a setter/getter pragma
-            tx.executeSql('PRAGMA user_version = 123;', [], nop, onError);
-            tx.executeSql(
-              'PRAGMA user_version;',
-              [],
-              (tx, results) => {
-                t.expect(results.rows.length).toEqual(1);
-                t.expect(results.rows._array[0].user_version).toEqual(123);
-              },
-              onError
-            );
-          },
-          reject,
-          resolve
-        );
-      });
-    });
   });
 }
