c143c175f097fa78b8bfc5a217b8c2aaee0fa3b9
Hein Rutjes <hrutjes@gmail.com>
2020-05-18 15:48:55 +0200

    [docs] Fix await usage in battery snack example (#8345)
    The battery snack example used `await` in componentDidMount without `async` defined on that function. This causes a compile error, see #7904
    
    This updates the battery example to not exhibit any compile errors or warnings. It also removes the unneccesary fat-arrow syntax from _subscribe/_unsubscribe and adds a newline to the end of the file.

--- docs/static/examples/v37.0.0/battery.js
+++ docs/static/examples/v37.0.0/battery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import * as Battery from 'expo-battery';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -8,8 +8,6 @@ export default class App extends React.Component {
   };
 
   componentDidMount() {
-    let batteryLevel = await Battery.getBatteryLevelAsync();
-    this.setState({ batteryLevel });
     this._subscribe();
   }
 
@@ -17,17 +15,19 @@ export default class App extends React.Component {
     this._unsubscribe();
   }
 
-  _subscribe = () => {
+  async _subscribe() {
+    const batteryLevel = await Battery.getBatteryLevelAsync();
+    this.setState({ batteryLevel });
     this._subscription = Battery.addBatteryLevelListener(({ batteryLevel }) => {
       this.setState({ batteryLevel });
       console.log('batteryLevel changed!', batteryLevel);
     });
-  };
+  }
 
-  _unsubscribe = () => {
+  _unsubscribe() {
     this._subscription && this._subscription.remove();
     this._subscription = null;
-  };
+  }
 
   render() {
     return (
