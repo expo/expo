a484bf01d89a87df79d1bbd1d9461239a2a2b83f
Janic Duplessis <janicduplessis@gmail.com>
2020-05-21 19:56:10 -0400

    [expo-updates] Support monorepos (#8419)
    * [expo-updates] Support monorepos on iOS
    
    * [expo-updates] Support monorepos on Android

--- packages/expo-updates/scripts/create-manifest-ios.sh
+++ packages/expo-updates/scripts/create-manifest-ios.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 DEST="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
 ASSETS_URL="http://localhost:8081/index.assets?platform=ios&dev=false"
 
-"${NODE_BINARY:-node}" ../node_modules/expo-updates/scripts/createManifest.js ios "$ASSETS_URL" "$DEST"
+"${NODE_BINARY:-node}" "$(dirname "${BASH_SOURCE[0]}")/createManifest.js" ios "$ASSETS_URL" "$DEST"
