1b4ae7e8163aba2de640190e0f58a0ce4164cfe9
Janic Duplessis <janicduplessis@gmail.com>
2020-05-21 19:57:02 -0400

    [updates] Support ENTRY_FILE env in create-manifest-ios.sh (#8415)

--- packages/expo-updates/scripts/create-manifest-ios.sh
+++ packages/expo-updates/scripts/create-manifest-ios.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 DEST="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
-ASSETS_URL="http://localhost:8081/index.assets?platform=ios&dev=false"
+ENTRY_FILE=${ENTRY_FILE:-index.js}
+ASSETS_URL="http://localhost:8081/"${ENTRY_FILE%.js}".assets?platform=ios&dev=false"
 
 "${NODE_BINARY:-node}" "$(dirname "${BASH_SOURCE[0]}")/createManifest.js" ios "$ASSETS_URL" "$DEST"
