64482e836c86c6eed471b5ed690ded416d2e591b
Hein Rutjes <hrutjes@gmail.com>
2020-05-22 13:55:15 +0200

    [react-native-shared-element] Upgrade react-native-shared-element to 0.7.0 (#8427)
    Fixes various native issue on iOS and Android and introduces separate border-radii transitions on iOS.

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/sharedelement/RNSharedElementNodeManager.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/sharedelement/RNSharedElementNodeManager.java
@@ -10,42 +10,42 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.uimanager.NativeViewHierarchyManager;
 
 class RNSharedElementNodeManager {
-    private Map<Integer, RNSharedElementNode> mNodes = new HashMap<Integer, RNSharedElementNode>();
-    private NativeViewHierarchyManager mNativeViewHierarchyManager;
-    private Context mContext;
+  private Map<Integer, RNSharedElementNode> mNodes = new HashMap<Integer, RNSharedElementNode>();
+  private NativeViewHierarchyManager mNativeViewHierarchyManager;
+  private Context mContext;
 
-    RNSharedElementNodeManager(Context context) {
-        mContext = context;
-    }
+  RNSharedElementNodeManager(Context context) {
+    mContext = context;
+  }
 
-    void setNativeViewHierarchyManager(NativeViewHierarchyManager nativeViewHierarchyManager) {
-        mNativeViewHierarchyManager = nativeViewHierarchyManager;
-    }
+  void setNativeViewHierarchyManager(NativeViewHierarchyManager nativeViewHierarchyManager) {
+    mNativeViewHierarchyManager = nativeViewHierarchyManager;
+  }
 
-    NativeViewHierarchyManager getNativeViewHierarchyManager() {
-        return mNativeViewHierarchyManager;
-    }
+  NativeViewHierarchyManager getNativeViewHierarchyManager() {
+    return mNativeViewHierarchyManager;
+  }
 
-    RNSharedElementNode acquire(int reactTag, View view, boolean isParent, View ancestor, ReadableMap styleConfig) {
-        synchronized (mNodes) {
-            RNSharedElementNode node = mNodes.get(reactTag);
-            if (node != null) {
-                node.addRef();
-                return node;
-            }
-            node = new RNSharedElementNode(mContext, reactTag, view, isParent, ancestor, styleConfig);
-            mNodes.put(reactTag, node);
-            return node;
-        }
+  RNSharedElementNode acquire(int reactTag, View view, boolean isParent, View ancestor, ReadableMap styleConfig) {
+    synchronized (mNodes) {
+      RNSharedElementNode node = mNodes.get(reactTag);
+      if (node != null) {
+        node.addRef();
+        return node;
+      }
+      node = new RNSharedElementNode(mContext, reactTag, view, isParent, ancestor, styleConfig);
+      mNodes.put(reactTag, node);
+      return node;
     }
+  }
 
-    int release(RNSharedElementNode node) {
-        synchronized (mNodes) {
-            int refCount = node.releaseRef();
-            if (refCount == 0) {
-                mNodes.remove(node.getReactTag());
-            }
-            return refCount;
-        }
+  int release(RNSharedElementNode node) {
+    synchronized (mNodes) {
+      int refCount = node.releaseRef();
+      if (refCount == 0) {
+        mNodes.remove(node.getReactTag());
+      }
+      return refCount;
     }
+  }
 }
