4437ca0bdbb39e1526495206c2646cada42baf10
Evan Bacon <baconbrix@gmail.com>
2020-05-26 16:46:03 -0700

    Swap native with web

--- packages/@unimodules/react-native-adapter/build/NativeModulesProxy.d.ts
+++ packages/@unimodules/react-native-adapter/build/NativeModulesProxy.d.ts
@@ -1,9 +1,2 @@
-export declare type ProxyNativeModule = {
-    [propertyName: string]: any;
-    addListener: (eventName: string) => void;
-    removeListeners: (count: number) => void;
-};
-declare const NativeModulesProxy: {
-    [moduleName: string]: ProxyNativeModule;
-};
-export default NativeModulesProxy;
+declare const _default: {};
+export default _default;
