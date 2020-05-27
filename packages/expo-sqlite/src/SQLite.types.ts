a11708432f121c81370a06aa56bd15c9722bbcbd
Stanisław Chmiela <sjchmiela@gmail.com>
2020-05-27 19:50:19 +0200

    [expo-sqlite] Add explanations around a separate SQLite.types.ts

--- packages/expo-sqlite/src/SQLite.types.ts
+++ packages/expo-sqlite/src/SQLite.types.ts
@@ -1,4 +1,9 @@
-// Definitions by: TeamworkGuy2 <https://github.com/TeamworkGuy2>
+// Definitions copied from `@types/websql` as we want
+// to expose a custom version of the API that:
+// - uses primitive `string` instead of `String`
+// - excludes some methods that are not exposed by our API.
+//
+// Original definitions by: TeamworkGuy2 <https://github.com/TeamworkGuy2>
 
 export interface DatabaseCallback {
   (database: Database): void;
