01231736b1df9031971660edf1dd80a1a3048d02
Eric Samelson <eric@expo.io>
2020-05-27 14:48:00 -0700

    [expo-updates] add better error message in create-manifest-ios.sh if Xcode shell cannot find node installation

--- packages/expo-updates/scripts/create-manifest-ios.sh
+++ packages/expo-updates/scripts/create-manifest-ios.sh
@@ -5,5 +5,13 @@ set -eo pipefail
 DEST="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
 ENTRY_FILE=${ENTRY_FILE:-index.js}
 ASSETS_URL="http://localhost:8081/"${ENTRY_FILE%.js}".assets?platform=ios&dev=false"
+NODE_BINARY=${NODE_BINARY:-node}
 
-"${NODE_BINARY:-node}" "$(dirname "${BASH_SOURCE[0]}")/createManifest.js" ios "$ASSETS_URL" "$DEST"
+if ! [ -x "$(command -v $NODE_BINARY)" ]; then
+  echo 'Error: cannot find the node binary. Try setting the NODE_BINARY variable in the ' \
+  '"Bundle React Native code and images" Build Phase to the absolute path to your node binary. ' \
+  'You can find it by executing "which node" in a terminal window.' >&2
+  exit 1
+fi
+
+"$NODE_BINARY" "$(dirname "${BASH_SOURCE[0]}")/createManifest.js" ios "$ASSETS_URL" "$DEST"
