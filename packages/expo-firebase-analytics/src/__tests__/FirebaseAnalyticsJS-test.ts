843c3bd4ba342ca4dd712a4046bdb5cc7406176b
Thorben Primke <thorben@primke.com>
2020-05-28 07:19:53 -0700

    Updated per feedback
    - pulled out logic into isValidName
    - added more tests

--- packages/expo-firebase-analytics/src/__tests__/FirebaseAnalyticsJS-test.ts
+++ packages/expo-firebase-analytics/src/__tests__/FirebaseAnalyticsJS-test.ts
@@ -1,27 +1,69 @@
 import FirebaseAnalyticsJS from '../FirebaseAnalyticsJS';
 
-it(`Verfies parseEvent eventName validation`, async () => {
-  expect.assertions(2);
-  const expectedErrorMessage =
-    'Invalid event-name specified. Should contain 1 to 40 alphanumeric characters or underscores. The name must start with an alphabetic character.';
-  const options = {
-    clientId: '0',
-    sessionId: '0',
-  };
+let eventName = '';
+const expectedErrorMessage = (name, eventName, maxLength) =>
+  `Invalid ${name} (${eventName}) specified. Should contain 1 to ${maxLength} alphanumeric characters or underscores. The name must start with an alphabetic character.`;
+const options = {
+  clientId: '0',
+  sessionId: '0',
+};
 
+it(`Verfies parseEvent eventName validation`, async () => {
+  const name = 'event-name';
+  expect.assertions(3);
   FirebaseAnalyticsJS.parseEvent(options, 'MyAnalyticsEvent');
 
   try {
-    FirebaseAnalyticsJS.parseEvent(options, '_MyAnalyticsEvent');
+    eventName = '_MyAnalyticsEvent';
+    FirebaseAnalyticsJS.parseEvent(options, eventName);
   } catch (error) {
-    expect(error.message).toBe(expectedErrorMessage);
+    expect(error.message).toBe(expectedErrorMessage(name, eventName, 40));
   }
 
   try {
-    FirebaseAnalyticsJS.parseEvent(options, '0MyAnalyticsEvent');
+    eventName = '0MyAnalyticsEvent';
+    FirebaseAnalyticsJS.parseEvent(options, eventName);
   } catch (error) {
-    expect(error.message).toBe(expectedErrorMessage);
+    expect(error.message).toBe(expectedErrorMessage(name, eventName, 40));
   }
 
   FirebaseAnalyticsJS.parseEvent(options, 'MyAnalyticsEvent0');
+
+  try {
+    eventName = '0SuperLongNameThatIsMoreThan40CharactersInLength';
+    FirebaseAnalyticsJS.parseEvent(options, eventName);
+  } catch (error) {
+    expect(error.message).toBe(expectedErrorMessage(name, eventName, 40));
+  }
+});
+
+it(`Verfies parseUserProperty userPropertyName validation`, async () => {
+  const name = 'user-property name';
+  expect.assertions(3);
+  const value = 'value';
+
+  FirebaseAnalyticsJS.parseUserProperty(options, 'MyAnalyticsEvent', value);
+
+  try {
+    eventName = '_MyAnalyticsEvent';
+    FirebaseAnalyticsJS.parseUserProperty(options, eventName, value);
+  } catch (error) {
+    expect(error.message).toBe(expectedErrorMessage(name, eventName, 24));
+  }
+
+  try {
+    eventName = '0MyAnalyticsEvent';
+    FirebaseAnalyticsJS.parseUserProperty(options, eventName, value);
+  } catch (error) {
+    expect(error.message).toBe(expectedErrorMessage(name, eventName, 24));
+  }
+
+  FirebaseAnalyticsJS.parseUserProperty(options, 'MyAnalyticsEvent0', value);
+
+  try {
+    eventName = '0LongNameThatIsMoreThan24ButLessThan40';
+    FirebaseAnalyticsJS.parseUserProperty(options, eventName, value);
+  } catch (error) {
+    expect(error.message).toBe(expectedErrorMessage(name, eventName, 24));
+  }
 });
