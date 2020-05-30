f7cded2bba587bec0602765809109d7b332de9f0
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2020-05-30 13:39:21 +0200

    [test-suite] Make SMS tests more reliable

--- apps/test-suite/tests/SMS.js
+++ apps/test-suite/tests/SMS.js
@@ -3,6 +3,8 @@ import { Platform } from 'react-native';
 
 import { isInteractive } from '../utils/Environment';
 import {
+  loadAttachmentsAsync,
+  cleanupAttachmentsAsync,
   testSMSComposeWithSingleImageAttachment,
   testSMSComposeWithAudioAttachment,
   testSMSComposeWithTwoImageAttachments,
@@ -10,37 +12,43 @@ import {
 
 export const name = 'SMS';
 
-export function test({ describe, it, expect }) {
-  if (!isInteractive()) {
-    describe(`sendSMSAsync()`, () => {
-      it(`opens an SMS composer`, async () => {
-        await SMS.sendSMSAsync(['0123456789', '9876543210'], 'test');
-      });
+export function test({ describe, it, expect, beforeAll, afterAll }) {
+  describe('SMS', () => {
+    if (isInteractive()) {
+      describe(`sendSMSAsync()`, () => {
+        beforeAll(() => loadAttachmentsAsync(expect));
 
-      it(`opens an SMS composer with single image attachment`, async () => {
-        await testSMSComposeWithSingleImageAttachment(expect);
-      });
+        it(`opens an SMS composer`, async () => {
+          await SMS.sendSMSAsync(['0123456789', '9876543210'], 'test');
+        });
 
-      it(`opens an SMS composer with two image attachments. Only first one is used.`, async () => {
-        await testSMSComposeWithTwoImageAttachments(expect);
-      });
+        it(`opens an SMS composer with single image attachment`, async () => {
+          await testSMSComposeWithSingleImageAttachment(expect);
+        });
 
-      it(`opens an SMS composer with audio attachment`, async () => {
-        await testSMSComposeWithAudioAttachment(expect);
-      });
-    });
-  }
+        it(`opens an SMS composer with two image attachments. Only first one is used.`, async () => {
+          await testSMSComposeWithTwoImageAttachments(expect);
+        });
 
-  describe(`isAvailableAsync()`, () => {
-    if (Platform.OS === 'android' && isInteractive()) {
-      // TODO(Bacon): Not sure if this works in an emulator or not
-      it(`has a telephony radio with data communication support`, async () => {
-        expect(await SMS.isAvailableAsync()).toBe(true);
-      });
-    } else {
-      it(`is not supported on ${Platform.OS}`, async () => {
-        expect(await SMS.isAvailableAsync()).toBe(false);
+        it(`opens an SMS composer with audio attachment`, async () => {
+          await testSMSComposeWithAudioAttachment(expect);
+        });
+
+        afterAll(() => cleanupAttachmentsAsync(expect));
       });
     }
+
+    describe(`isAvailableAsync()`, () => {
+      if (Platform.OS === 'android' && isInteractive()) {
+        // TODO(Bacon): Not sure if this works in an emulator or not
+        it(`has a telephony radio with data communication support`, async () => {
+          expect(await SMS.isAvailableAsync()).toBe(true);
+        });
+      } else {
+        it(`is not supported on ${Platform.OS}`, async () => {
+          expect(await SMS.isAvailableAsync()).toBe(false);
+        });
+      }
+    });
   });
 }
