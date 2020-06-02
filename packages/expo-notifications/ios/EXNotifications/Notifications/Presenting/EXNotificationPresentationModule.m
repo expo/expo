c695e618115a6ec713ac9be263294fb0e3bd333d
macintoshhelper <macintoshhelper@users.noreply.github.com>
2020-06-02 15:03:22 +0100

    [ios][notifications] add createCategoryAsync (+ delete) native bridge functions

--- packages/expo-notifications/ios/EXNotifications/Notifications/Presenting/EXNotificationPresentationModule.m
+++ packages/expo-notifications/ios/EXNotifications/Notifications/Presenting/EXNotificationPresentationModule.m
@@ -13,6 +13,7 @@
 // Remove once presentNotificationAsync is removed
 @property (nonatomic, strong) NSCountedSet<NSString *> *presentedNotifications;
 @property (nonatomic, weak) id<EXNotificationCenterDelegate> notificationCenterDelegate;
+//@property (nonatomic, weak) id <EXUserNotificationCenterService> userNotificationCenter;
 
 @end
 
@@ -82,6 +83,71 @@ UM_EXPORT_METHOD_AS(dismissAllNotificationsAsync,
   resolve(nil);
 }
 
+# pragma mark - Categories
+
+UM_EXPORT_METHOD_AS(createCategoryAsync,
+                    createCategoryWithCategoryId:(NSString *)categoryId
+                    actions:(NSArray *)actions
+//                    previewPlaceholder: (NSString *)previewPlaceholder
+                    resolve:(UMPromiseResolveBlock)resolve
+                    reject:(UMPromiseRejectBlock)reject)
+{
+  NSMutableArray<UNNotificationAction *> *actionsArray = [[NSMutableArray alloc] init];
+  for (NSDictionary<NSString *, id> *actionParams in actions) {
+    [actionsArray addObject:[self parseNotificationActionFromParams:actionParams]];
+  }
+
+  UNNotificationCategory *newCategory;
+  if (@available(iOS 11, *)) {
+//    newCategory = [UNNotificationCategory categoryWithIdentifier:[self internalIdForIdentifier:categoryId]
+//                                                                     actions:actionsArray
+//                                                                     intentIdentifiers:@[]
+//                                                                     hiddenPreviewsBodyPlaceholder: previewPlaceholder
+//                                                                     options:UNNotificationCategoryOptionNone];
+    newCategory = [UNNotificationCategory categoryWithIdentifier:[self categoryIdForIdentifier:categoryId]
+                                                                  actions:actionsArray
+                                                                  intentIdentifiers:@[]
+                                                                  options:UNNotificationCategoryOptionNone];
+  } else {
+    newCategory = [UNNotificationCategory categoryWithIdentifier:[self categoryIdForIdentifier:categoryId]
+                                                                  actions:actionsArray
+                                                                  intentIdentifiers:@[]
+                                                                  options:UNNotificationCategoryOptionNone];
+  }
+
+  [[UNUserNotificationCenter currentNotificationCenter] getNotificationCategoriesWithCompletionHandler:^(NSSet<UNNotificationCategory *> *categories) {
+    NSMutableSet<UNNotificationCategory *> *newCategories = [categories mutableCopy];
+    for (UNNotificationCategory *category in newCategories) {
+      if ([category.identifier isEqualToString:newCategory.identifier]) {
+        [newCategories removeObject:category];
+        break;
+      }
+    }
+    [newCategories addObject:newCategory];
+    [[UNUserNotificationCenter currentNotificationCenter] setNotificationCategories:newCategories];
+    resolve(nil);
+  }];
+}
+
+UM_EXPORT_METHOD_AS(deleteCategoryAsync,
+                 deleteCategoryWithCategoryId:(NSString *)categoryId
+                 resolve:(UMPromiseResolveBlock)resolve
+                 reject:(UMPromiseRejectBlock)reject)
+{
+  NSString *internalCategoryId = [self internalIdForIdentifier:categoryId];
+  [[UNUserNotificationCenter currentNotificationCenter] getNotificationCategoriesWithCompletionHandler:^(NSSet<UNNotificationCategory *> *categories) {
+    NSMutableSet<UNNotificationCategory *> *newCategories = [categories mutableCopy];
+    for (UNNotificationCategory *category in newCategories) {
+      if ([category.identifier isEqualToString:internalCategoryId]) {
+        [newCategories removeObject:category];
+        break;
+      }
+    }
+    [[UNUserNotificationCenter currentNotificationCenter] setNotificationCategories:newCategories];
+    resolve(nil);
+  }];
+}
+
 # pragma mark - UMModuleRegistryConsumer
 
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
@@ -118,6 +184,45 @@ UM_EXPORT_METHOD_AS(dismissAllNotificationsAsync,
     [serializedNotifications addObject:[EXNotificationSerializer serializedNotification:notification]];
   }
   return serializedNotifications;
+}
+
+- (NSString *)internalIdForIdentifier:(NSString *)identifier {
+  NSString *prefix = @"expo.modules.notifications.actions.";
+  return [prefix stringByAppendingString:identifier];
+//  return [_notificationsIdentifiersManager internalIdForIdentifier:identifier experienceId:self.experienceId];
+}
+
+- (NSString *)categoryIdForIdentifier:(NSString *)identifier {
+  NSString *prefix = @"";
+  return [prefix stringByAppendingString:identifier];
+//  return [_notificationsIdentifiersManager internalIdForIdentifier:identifier experienceId:self.experienceId];
+}
+
+- (UNNotificationAction *)parseNotificationActionFromParams:(NSDictionary *)params
+{
+  NSString *actionId = [self internalIdForIdentifier:params[@"actionId"]];
+  NSString *buttonTitle = params[@"buttonTitle"];
+
+  UNNotificationActionOptions options = UNNotificationActionOptionNone;
+  if (![params[@"doNotOpenInForeground"] boolValue]) {
+    options += UNNotificationActionOptionForeground;
+  }
+  if ([params[@"isDestructive"] boolValue]) {
+    options += UNNotificationActionOptionDestructive;
+  }
+  if ([params[@"isAuthenticationRequired"] boolValue]) {
+    options += UNNotificationActionOptionAuthenticationRequired;
+  }
+
+  if ([params[@"textInput"] isKindOfClass:[NSDictionary class]]) {
+    return [UNTextInputNotificationAction actionWithIdentifier:actionId
+                                                         title:buttonTitle
+                                                       options:options
+                                          textInputButtonTitle:params[@"textInput"][@"submitButtonTitle"]
+                                          textInputPlaceholder:params[@"textInput"][@"placeholder"]];
+  }
+
+  return [UNNotificationAction actionWithIdentifier:actionId title:buttonTitle options:options];
 }
 
 @end
