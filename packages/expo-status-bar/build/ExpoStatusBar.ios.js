eb44615a0981f4ef669d29f901855b7c14550828
Brent Vatne <brentvatne@gmail.com>
2020-06-04 15:45:05 -0700

    Use useColorScheme instead of Appearance.getColorScheme in order to update automatically when scheme changes

--- packages/expo-status-bar/build/ExpoStatusBar.ios.js
+++ packages/expo-status-bar/build/ExpoStatusBar.ios.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Appearance, StatusBar } from 'react-native';
+import { useColorScheme, StatusBar } from 'react-native';
 export default function ExpoStatusBar(props) {
     let { barStyle, ...otherProps } = props;
+    const colorScheme = useColorScheme();
     // Pick appropriate 'default' depending on current theme, so if we are locked to light mode
     // we don't end up with a light status bar
     if (props.barStyle === 'default') {
-        barStyle = Appearance.getColorScheme() === 'light' ? 'dark-content' : 'light-content';
+        barStyle = colorScheme === 'light' ? 'dark-content' : 'light-content';
     }
     return React.createElement(StatusBar, Object.assign({ barStyle: barStyle }, otherProps));
 }
--- packages/expo-status-bar/src/ExpoStatusBar.ios.tsx
+++ packages/expo-status-bar/src/ExpoStatusBar.ios.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Appearance, StatusBar, StatusBarProps } from 'react-native';
+import { useColorScheme, StatusBar, StatusBarProps } from 'react-native';
 
 export default function ExpoStatusBar(props: StatusBarProps) {
   let { barStyle, ...otherProps } = props;
+  const colorScheme = useColorScheme();
 
   // Pick appropriate 'default' depending on current theme, so if we are locked to light mode
   // we don't end up with a light status bar
   if (props.barStyle === 'default') {
-    barStyle = Appearance.getColorScheme() === 'light' ? 'dark-content' : 'light-content';
+    barStyle = colorScheme === 'light' ? 'dark-content' : 'light-content';
   }
 
   return <StatusBar barStyle={barStyle} {...otherProps} />;
