25972874851a525e212749dd45a9b4a9d2cbcb70
Evan Bacon <baconbrix@gmail.com>
2020-06-03 18:40:23 -0700

    Updated build files

--- packages/@unimodules/react-native-adapter/build/Platform.d.ts
+++ packages/@unimodules/react-native-adapter/build/Platform.d.ts
@@ -1,4 +1,30 @@
+export declare type PlatformOSType = 'ios' | 'android' | 'web' | 'native' | 'macos' | 'windows' | 'electron';
+export declare type PlatformSelect = <T>(specifics: ({
+    [platform in PlatformOSType]?: T;
+} & {
+    default: T;
+}) | {
+    [platform in PlatformOSType]: T;
+}) => T;
 declare const Platform: {
+    /**
+     * Denotes the currently running platform.
+     * Can be one of ios, android, web.
+     */
     OS: "ios" | "android" | "windows" | "macos" | "web";
+    /**
+     * Returns the value with the matching platform.
+     * Object keys can be any of ios, android, native, web, default.
+     *
+     * @ios ios, native, default
+     * @android android, native, default
+     * @web web, default
+     */
+    select: PlatformSelect;
+    /**
+     * Used for delegating node actions when browser APIs aren't available
+     * like in SSR websites. DOM is not available in native React runtimes.
+     */
+    isDOMAvailable: boolean;
 };
 export default Platform;
--- packages/@unimodules/react-native-adapter/build/Platform.js
+++ packages/@unimodules/react-native-adapter/build/Platform.js
@@ -1,6 +1,25 @@
 import { Platform as ReactNativePlatform } from 'react-native';
+import { isDOMAvailable } from './environment/browser';
 const Platform = {
+    /**
+     * Denotes the currently running platform.
+     * Can be one of ios, android, web.
+     */
     OS: ReactNativePlatform.OS,
+    /**
+     * Returns the value with the matching platform.
+     * Object keys can be any of ios, android, native, web, default.
+     *
+     * @ios ios, native, default
+     * @android android, native, default
+     * @web web, default
+     */
+    select: ReactNativePlatform.select,
+    /**
+     * Used for delegating node actions when browser APIs aren't available
+     * like in SSR websites. DOM is not available in native React runtimes.
+     */
+    isDOMAvailable,
 };
 export default Platform;
 //# sourceMappingURL=Platform.js.map
