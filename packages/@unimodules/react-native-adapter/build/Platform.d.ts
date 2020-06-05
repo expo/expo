88ed402693ae30219d6ad54b82ee9afc393c82ee
Evan Bacon <baconbrix@gmail.com>
2020-06-04 21:09:34 -0700

    Update Platform.d.ts

--- packages/@unimodules/react-native-adapter/build/Platform.d.ts
+++ packages/@unimodules/react-native-adapter/build/Platform.d.ts
@@ -8,7 +8,7 @@ declare const Platform: {
      * Denotes the currently running platform.
      * Can be one of ios, android, web.
      */
-    OS: PlatformOSType;
+    OS: "ios" | "android" | "windows" | "macos" | "web";
     /**
      * Returns the value with the matching platform.
      * Object keys can be any of ios, android, native, web, default.
