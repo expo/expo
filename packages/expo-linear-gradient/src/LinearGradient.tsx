c3a117e258145938a6d235cabee617104f841fd9
Evan Bacon <baconbrix@gmail.com>
2020-06-04 21:43:49 -0700

    Convert LinearGradient to functional component

--- packages/expo-linear-gradient/src/LinearGradient.tsx
+++ packages/expo-linear-gradient/src/LinearGradient.tsx
@@ -12,29 +12,31 @@ export type LinearGradientProps = {
 
 export type LinearGradienPoint = { x: number; y: number } | [number, number];
 
-export class LinearGradient extends React.Component<LinearGradientProps> {
-  render() {
-    let { colors, locations, start, end, ...props } = this.props;
-
-    if (locations && colors.length !== locations.length) {
-      console.warn('LinearGradient colors and locations props should be arrays of the same length');
-      locations = locations.slice(0, colors.length);
-    }
-
-    return (
-      <NativeLinearGradient
-        {...props}
-        colors={Platform.select({
-          web: colors as any,
-          default: colors.map(processColor),
-        })}
-        locations={locations}
-        startPoint={_normalizePoint(start)}
-        endPoint={_normalizePoint(end)}
-      />
-    );
+export const LinearGradient: React.FC<LinearGradientProps> = ({
+  colors,
+  locations,
+  start,
+  end,
+  ...props
+}: LinearGradientProps) => {
+  if (locations && colors.length !== locations.length) {
+    console.warn('LinearGradient colors and locations props should be arrays of the same length');
+    locations = locations.slice(0, colors.length);
   }
-}
+
+  return (
+    <NativeLinearGradient
+      {...props}
+      colors={Platform.select({
+        web: colors as any,
+        default: colors.map(processColor),
+      })}
+      locations={locations}
+      startPoint={_normalizePoint(start)}
+      endPoint={_normalizePoint(end)}
+    />
+  );
+};
 
 function _normalizePoint(
   point: LinearGradienPoint | null | undefined
