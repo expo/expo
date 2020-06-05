e672068466e55b7193642bd8ae475333b9dd2659
Brent Vatne <brentvatne@gmail.com>
2020-06-04 17:38:36 -0700

    Add warning if incompatible version of Expo / React Native is being used

--- packages/expo-status-bar/build/ExpoStatusBar.ios.js
+++ packages/expo-status-bar/build/ExpoStatusBar.ios.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { useColorScheme, StatusBar } from 'react-native';
+import { useColorScheme as maybeUseColorScheme, StatusBar } from 'react-native';
+if (!maybeUseColorScheme) {
+    console.warn('expo-status-bar is only supported on Expo SDK >= 38 and React Native >= 0.62. You are seeing this message because useColorScheme from react-native is not available. Some features may not work as expected.');
+}
+const fallbackUseColorScheme = () => 'light';
+const useColorScheme = maybeUseColorScheme ?? fallbackUseColorScheme;
 export default function ExpoStatusBar(props) {
     let { barStyle, ...otherProps } = props;
-    // If useColorScheme isn't defined, then this is an old version of React Native
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const colorScheme = useColorScheme ? useColorScheme() : 'light';
+    const colorScheme = useColorScheme();
     // Pick appropriate 'default' depending on current theme, so if we are locked to light mode
     // we don't end up with a light status bar
     if (props.barStyle === 'default') {
--- packages/expo-status-bar/src/ExpoStatusBar.ios.tsx
+++ packages/expo-status-bar/src/ExpoStatusBar.ios.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { useColorScheme, StatusBar, StatusBarProps } from 'react-native';
+import { useColorScheme as maybeUseColorScheme, StatusBar, StatusBarProps } from 'react-native';
+
+if (!maybeUseColorScheme) {
+  console.warn(
+    'expo-status-bar is only supported on Expo SDK >= 38 and React Native >= 0.62. You are seeing this message because useColorScheme from react-native is not available. Some features may not work as expected.'
+  );
+}
+
+const fallbackUseColorScheme = () => 'light';
+const useColorScheme = maybeUseColorScheme ?? fallbackUseColorScheme;
 
 export default function ExpoStatusBar(props: StatusBarProps) {
   let { barStyle, ...otherProps } = props;
 
-  // If useColorScheme isn't defined, then this is an old version of React Native
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const colorScheme = useColorScheme ? useColorScheme() : 'light';
+  const colorScheme = useColorScheme();
 
   // Pick appropriate 'default' depending on current theme, so if we are locked to light mode
   // we don't end up with a light status bar
