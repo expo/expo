aace5898af43bec44a5e10bf784817bc449b90c5
Evan Bacon <baconbrix@gmail.com>
2020-06-08 13:18:48 -0700

    [linear-gradient] Migrate to functional components (#8673)
    * Remove index re-export
    
    * Created shim element for unsupported platforms
    
    * Convert LinearGradient to functional component
    
    * Convert all react imports to star imports
    
    * Unify types and add doc blocks
    
    * Update NativeLinearGradient.web.tsx
    
    * Updated build files
    
    * Updated docs for linear gradient
    
    * Update linear-gradient.md
    
    * Update CHANGELOG.md
    
    * Use nullish
    
    * Clean up function export
    
    * Apply suggestions from code review
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>
    
    * Updated docs
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>

--- packages/expo-linear-gradient/src/NativeLinearGradient.android.tsx
+++ packages/expo-linear-gradient/src/NativeLinearGradient.android.tsx
@@ -1,52 +1,49 @@
 import { requireNativeViewManager } from '@unimodules/core';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-type Props = {
-  colors: number[];
-  locations?: number[] | null;
-  startPoint?: Point | null;
-  endPoint?: Point | null;
-} & React.ComponentProps<typeof View>;
+import { NativeLinearGradientProps } from './NativeLinearGradient.types';
 
-type Point = [number, number];
+export default function NativeLinearGradient({
+  colors,
+  locations,
+  startPoint,
+  endPoint,
+  children,
+  style,
+  ...props
+}: NativeLinearGradientProps): React.ReactElement {
+  // TODO: revisit whether we need to inherit the container's borderRadius since this issue has
+  // been resolved: https://github.com/facebook/react-native/issues/3198
+  const flatStyle = StyleSheet.flatten(style) ?? {};
+  const borderRadius = flatStyle.borderRadius ?? 0;
 
-export default class NativeLinearGradient extends React.Component<Props> {
-  render() {
-    const { colors, locations, startPoint, endPoint, children, style, ...props } = this.props;
+  // This is the format from:
+  // https://developer.android.com/reference/android/graphics/Path.html#addRoundRect(android.graphics.RectF,%20float[],%20android.graphics.Path.Direction)
+  const borderRadiiPerCorner = [
+    flatStyle.borderTopLeftRadius ?? borderRadius,
+    flatStyle.borderTopLeftRadius ?? borderRadius,
+    flatStyle.borderTopRightRadius ?? borderRadius,
+    flatStyle.borderTopRightRadius ?? borderRadius,
+    flatStyle.borderBottomRightRadius ?? borderRadius,
+    flatStyle.borderBottomRightRadius ?? borderRadius,
+    flatStyle.borderBottomLeftRadius ?? borderRadius,
+    flatStyle.borderBottomLeftRadius ?? borderRadius,
+  ];
 
-    // TODO: revisit whether we need to inherit the container's borderRadius since this issue has
-    // been resolved: https://github.com/facebook/react-native/issues/3198
-    const flatStyle = StyleSheet.flatten(style) || {};
-    const borderRadius = flatStyle.borderRadius || 0;
-
-    // This is the format from:
-    // https://developer.android.com/reference/android/graphics/Path.html#addRoundRect(android.graphics.RectF,%20float[],%20android.graphics.Path.Direction)
-    const borderRadiiPerCorner = [
-      flatStyle.borderTopLeftRadius || borderRadius,
-      flatStyle.borderTopLeftRadius || borderRadius,
-      flatStyle.borderTopRightRadius || borderRadius,
-      flatStyle.borderTopRightRadius || borderRadius,
-      flatStyle.borderBottomRightRadius || borderRadius,
-      flatStyle.borderBottomRightRadius || borderRadius,
-      flatStyle.borderBottomLeftRadius || borderRadius,
-      flatStyle.borderBottomLeftRadius || borderRadius,
-    ];
-
-    return (
-      <View {...props} style={style}>
-        <BaseNativeLinearGradient
-          style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}
-          colors={colors}
-          startPoint={startPoint}
-          endPoint={endPoint}
-          locations={locations}
-          borderRadii={borderRadiiPerCorner}
-        />
-        {children}
-      </View>
-    );
-  }
+  return (
+    <View {...props} style={style}>
+      <BaseNativeLinearGradient
+        style={StyleSheet.absoluteFill}
+        colors={colors}
+        startPoint={startPoint}
+        endPoint={endPoint}
+        locations={locations}
+        borderRadii={borderRadiiPerCorner}
+      />
+      {children}
+    </View>
+  );
 }
 
 const BaseNativeLinearGradient = requireNativeViewManager('ExpoLinearGradient');
