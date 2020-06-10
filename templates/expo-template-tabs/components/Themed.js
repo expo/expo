ec9ec38f0a3c593b4283fd2e16965814a59d8283
Brent Vatne <brentvatne@gmail.com>
2020-06-10 16:54:07 -0700

    Use expo-status-bar and support themes in tabs template

---
+++ templates/expo-template-tabs/components/Themed.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Text as DefaultText, View as DefaultView, useColorScheme } from 'react-native';
+
+import Colors from '../constants/Colors';
+
+export function useThemeColor(props, colorName) {
+  const theme = useColorScheme();
+  const colorFromProps = props[theme];
+
+  if (colorFromProps) {
+    return colorFromProps;
+  } else {
+    return Colors[theme][colorName];
+  }
+}
+
+export function Text(props) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+
+  console.log({ color, lightColor, darkColor });
+
+  return <DefaultText style={[{ color }, style]} {...otherProps} />;
+}
+
+export function View(props) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
--- templates/expo-template-tabs/constants/Colors.js
+++ templates/expo-template-tabs/constants/Colors.js
@@ -1,14 +1,19 @@
-const tintColor = '#2f95dc';
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
 
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
 };
