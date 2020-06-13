9db48c0a3c36d9e6484d86877a6ad7cb53e6f24c
Brent Vatne <brentvatne@gmail.com>
2020-06-12 20:40:21 -0700

    Add missing build files

---
+++ packages/expo/build/ExpoLazy.js
@@ -0,0 +1,506 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "apisAreAvailable", {
+  enumerable: true,
+  get: function () {
+    return _apisAreAvailable.default;
+  }
+});
+Object.defineProperty(exports, "registerRootComponent", {
+  enumerable: true,
+  get: function () {
+    return _registerRootComponent.default;
+  }
+});
+Object.defineProperty(exports, "Notifications", {
+  enumerable: true,
+  get: function () {
+    return _Notifications.default;
+  }
+});
+Object.defineProperty(exports, "AppLoading", {
+  enumerable: true,
+  get: function () {
+    return _AppLoading.default;
+  }
+});
+Object.defineProperty(exports, "DangerZone", {
+  enumerable: true,
+  get: function () {
+    return _DangerZone.default;
+  }
+});
+Object.defineProperty(exports, "Updates", {
+  enumerable: true,
+  get: function () {
+    return _deprecated.Updates;
+  }
+});
+Object.defineProperty(exports, "Linking", {
+  enumerable: true,
+  get: function () {
+    return _deprecated.Linking;
+  }
+});
+Object.defineProperty(exports, "Accelerometer", {
+  enumerable: true,
+  get: function () {
+    return _removed.Accelerometer;
+  }
+});
+Object.defineProperty(exports, "AdMobBanner", {
+  enumerable: true,
+  get: function () {
+    return _removed.AdMobBanner;
+  }
+});
+Object.defineProperty(exports, "AdMobInterstitial", {
+  enumerable: true,
+  get: function () {
+    return _removed.AdMobInterstitial;
+  }
+});
+Object.defineProperty(exports, "AdMobRewarded", {
+  enumerable: true,
+  get: function () {
+    return _removed.AdMobRewarded;
+  }
+});
+Object.defineProperty(exports, "Animated", {
+  enumerable: true,
+  get: function () {
+    return _removed.Animated;
+  }
+});
+Object.defineProperty(exports, "Amplitude", {
+  enumerable: true,
+  get: function () {
+    return _removed.Amplitude;
+  }
+});
+Object.defineProperty(exports, "AppAuth", {
+  enumerable: true,
+  get: function () {
+    return _removed.AppAuth;
+  }
+});
+Object.defineProperty(exports, "AR", {
+  enumerable: true,
+  get: function () {
+    return _removed.AR;
+  }
+});
+Object.defineProperty(exports, "Asset", {
+  enumerable: true,
+  get: function () {
+    return _removed.Asset;
+  }
+});
+Object.defineProperty(exports, "Audio", {
+  enumerable: true,
+  get: function () {
+    return _removed.Audio;
+  }
+});
+Object.defineProperty(exports, "BackgroundFetch", {
+  enumerable: true,
+  get: function () {
+    return _removed.BackgroundFetch;
+  }
+});
+Object.defineProperty(exports, "BarCodeScanner", {
+  enumerable: true,
+  get: function () {
+    return _removed.BarCodeScanner;
+  }
+});
+Object.defineProperty(exports, "Barometer", {
+  enumerable: true,
+  get: function () {
+    return _removed.Barometer;
+  }
+});
+Object.defineProperty(exports, "BlurView", {
+  enumerable: true,
+  get: function () {
+    return _removed.BlurView;
+  }
+});
+Object.defineProperty(exports, "Brightness", {
+  enumerable: true,
+  get: function () {
+    return _removed.Brightness;
+  }
+});
+Object.defineProperty(exports, "Calendar", {
+  enumerable: true,
+  get: function () {
+    return _removed.Calendar;
+  }
+});
+Object.defineProperty(exports, "Camera", {
+  enumerable: true,
+  get: function () {
+    return _removed.Camera;
+  }
+});
+Object.defineProperty(exports, "Constants", {
+  enumerable: true,
+  get: function () {
+    return _removed.Constants;
+  }
+});
+Object.defineProperty(exports, "Contacts", {
+  enumerable: true,
+  get: function () {
+    return _removed.Contacts;
+  }
+});
+Object.defineProperty(exports, "Crypto", {
+  enumerable: true,
+  get: function () {
+    return _removed.Crypto;
+  }
+});
+Object.defineProperty(exports, "DocumentPicker", {
+  enumerable: true,
+  get: function () {
+    return _removed.DocumentPicker;
+  }
+});
+Object.defineProperty(exports, "Easing", {
+  enumerable: true,
+  get: function () {
+    return _removed.Easing;
+  }
+});
+Object.defineProperty(exports, "Facebook", {
+  enumerable: true,
+  get: function () {
+    return _removed.Facebook;
+  }
+});
+Object.defineProperty(exports, "FacebookAds", {
+  enumerable: true,
+  get: function () {
+    return _removed.FacebookAds;
+  }
+});
+Object.defineProperty(exports, "FaceDetector", {
+  enumerable: true,
+  get: function () {
+    return _removed.FaceDetector;
+  }
+});
+Object.defineProperty(exports, "FileSystem", {
+  enumerable: true,
+  get: function () {
+    return _removed.FileSystem;
+  }
+});
+Object.defineProperty(exports, "Font", {
+  enumerable: true,
+  get: function () {
+    return _removed.Font;
+  }
+});
+Object.defineProperty(exports, "GestureHandler", {
+  enumerable: true,
+  get: function () {
+    return _removed.GestureHandler;
+  }
+});
+Object.defineProperty(exports, "GL", {
+  enumerable: true,
+  get: function () {
+    return _removed.GL;
+  }
+});
+Object.defineProperty(exports, "GLView", {
+  enumerable: true,
+  get: function () {
+    return _removed.GLView;
+  }
+});
+Object.defineProperty(exports, "GoogleSignIn", {
+  enumerable: true,
+  get: function () {
+    return _removed.GoogleSignIn;
+  }
+});
+Object.defineProperty(exports, "Google", {
+  enumerable: true,
+  get: function () {
+    return _removed.Google;
+  }
+});
+Object.defineProperty(exports, "Gyroscope", {
+  enumerable: true,
+  get: function () {
+    return _removed.Gyroscope;
+  }
+});
+Object.defineProperty(exports, "Haptic", {
+  enumerable: true,
+  get: function () {
+    return _removed.Haptic;
+  }
+});
+Object.defineProperty(exports, "Haptics", {
+  enumerable: true,
+  get: function () {
+    return _removed.Haptics;
+  }
+});
+Object.defineProperty(exports, "Icon", {
+  enumerable: true,
+  get: function () {
+    return _removed.Icon;
+  }
+});
+Object.defineProperty(exports, "ImageManipulator", {
+  enumerable: true,
+  get: function () {
+    return _removed.ImageManipulator;
+  }
+});
+Object.defineProperty(exports, "ImagePicker", {
+  enumerable: true,
+  get: function () {
+    return _removed.ImagePicker;
+  }
+});
+Object.defineProperty(exports, "IntentLauncher", {
+  enumerable: true,
+  get: function () {
+    return _removed.IntentLauncher;
+  }
+});
+Object.defineProperty(exports, "IntentLauncherAndroid", {
+  enumerable: true,
+  get: function () {
+    return _removed.IntentLauncherAndroid;
+  }
+});
+Object.defineProperty(exports, "KeepAwake", {
+  enumerable: true,
+  get: function () {
+    return _removed.KeepAwake;
+  }
+});
+Object.defineProperty(exports, "LinearGradient", {
+  enumerable: true,
+  get: function () {
+    return _removed.LinearGradient;
+  }
+});
+Object.defineProperty(exports, "LocalAuthentication", {
+  enumerable: true,
+  get: function () {
+    return _removed.LocalAuthentication;
+  }
+});
+Object.defineProperty(exports, "Localization", {
+  enumerable: true,
+  get: function () {
+    return _removed.Localization;
+  }
+});
+Object.defineProperty(exports, "Location", {
+  enumerable: true,
+  get: function () {
+    return _removed.Location;
+  }
+});
+Object.defineProperty(exports, "Magnetometer", {
+  enumerable: true,
+  get: function () {
+    return _removed.Magnetometer;
+  }
+});
+Object.defineProperty(exports, "MagnetometerUncalibrated", {
+  enumerable: true,
+  get: function () {
+    return _removed.MagnetometerUncalibrated;
+  }
+});
+Object.defineProperty(exports, "MailComposer", {
+  enumerable: true,
+  get: function () {
+    return _removed.MailComposer;
+  }
+});
+Object.defineProperty(exports, "MapView", {
+  enumerable: true,
+  get: function () {
+    return _removed.MapView;
+  }
+});
+Object.defineProperty(exports, "MediaLibrary", {
+  enumerable: true,
+  get: function () {
+    return _removed.MediaLibrary;
+  }
+});
+Object.defineProperty(exports, "Pedometer", {
+  enumerable: true,
+  get: function () {
+    return _removed.Pedometer;
+  }
+});
+Object.defineProperty(exports, "Permissions", {
+  enumerable: true,
+  get: function () {
+    return _removed.Permissions;
+  }
+});
+Object.defineProperty(exports, "Print", {
+  enumerable: true,
+  get: function () {
+    return _removed.Print;
+  }
+});
+Object.defineProperty(exports, "PublisherBanner", {
+  enumerable: true,
+  get: function () {
+    return _removed.PublisherBanner;
+  }
+});
+Object.defineProperty(exports, "Random", {
+  enumerable: true,
+  get: function () {
+    return _removed.Random;
+  }
+});
+Object.defineProperty(exports, "ScreenOrientation", {
+  enumerable: true,
+  get: function () {
+    return _removed.ScreenOrientation;
+  }
+});
+Object.defineProperty(exports, "SecureStore", {
+  enumerable: true,
+  get: function () {
+    return _removed.SecureStore;
+  }
+});
+Object.defineProperty(exports, "Segment", {
+  enumerable: true,
+  get: function () {
+    return _removed.Segment;
+  }
+});
+Object.defineProperty(exports, "Sensors", {
+  enumerable: true,
+  get: function () {
+    return _removed.Sensors;
+  }
+});
+Object.defineProperty(exports, "Sharing", {
+  enumerable: true,
+  get: function () {
+    return _removed.Sharing;
+  }
+});
+Object.defineProperty(exports, "SMS", {
+  enumerable: true,
+  get: function () {
+    return _removed.SMS;
+  }
+});
+Object.defineProperty(exports, "Speech", {
+  enumerable: true,
+  get: function () {
+    return _removed.Speech;
+  }
+});
+Object.defineProperty(exports, "SQLite", {
+  enumerable: true,
+  get: function () {
+    return _removed.SQLite;
+  }
+});
+Object.defineProperty(exports, "Svg", {
+  enumerable: true,
+  get: function () {
+    return _removed.Svg;
+  }
+});
+Object.defineProperty(exports, "takeSnapshotAsync", {
+  enumerable: true,
+  get: function () {
+    return _removed.takeSnapshotAsync;
+  }
+});
+Object.defineProperty(exports, "TaskManager", {
+  enumerable: true,
+  get: function () {
+    return _removed.TaskManager;
+  }
+});
+Object.defineProperty(exports, "Transition", {
+  enumerable: true,
+  get: function () {
+    return _removed.Transition;
+  }
+});
+Object.defineProperty(exports, "Transitioning", {
+  enumerable: true,
+  get: function () {
+    return _removed.Transitioning;
+  }
+});
+Object.defineProperty(exports, "Video", {
+  enumerable: true,
+  get: function () {
+    return _removed.Video;
+  }
+});
+Object.defineProperty(exports, "WebBrowser", {
+  enumerable: true,
+  get: function () {
+    return _removed.WebBrowser;
+  }
+});
+Object.defineProperty(exports, "WebView", {
+  enumerable: true,
+  get: function () {
+    return _removed.WebView;
+  }
+});
+exports.Logs = exports.SplashScreen = void 0;
+
+require("./Expo.fx");
+
+var SplashScreen = _interopRequireWildcard(require("./launch/SplashScreen"));
+
+exports.SplashScreen = SplashScreen;
+
+var Logs = _interopRequireWildcard(require("./logs/Logs"));
+
+exports.Logs = Logs;
+
+var _apisAreAvailable = _interopRequireDefault(require("./apisAreAvailable"));
+
+var _registerRootComponent = _interopRequireDefault(require("./launch/registerRootComponent"));
+
+var _Notifications = _interopRequireDefault(require("./Notifications/Notifications"));
+
+var _AppLoading = _interopRequireDefault(require("./launch/AppLoading"));
+
+var _DangerZone = _interopRequireDefault(require("./DangerZone"));
+
+var _deprecated = require("./deprecated");
+
+var _removed = require("./removed");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+//# sourceMappingURL=ExpoLazy.js.map
