3f47d6cf2d3d34fa7fd460cd3412711a422fbec2
Stanisław Chmiela <sjchmiela@gmail.com>
2020-06-15 10:37:23 +0200

    [expo] Fix logs showing up in both consoles when debugging remotely

--- packages/expo/build/environment/logging.fx.js
+++ packages/expo/build/environment/logging.fx.js
@@ -3,8 +3,8 @@ import * as Logs from '../logs/Logs';
 import RemoteLogging from '../logs/RemoteLogging';
 if (Constants.manifest && Constants.manifest.logUrl) {
     // Enable logging to the Expo dev tools only if this JS is not running in a web browser (ex: the
-    // remote debugger)
-    if (!navigator.hasOwnProperty('userAgent')) {
+    // remote debugger). In Expo Web we don't show console logs in the CLI, so there's no special case needed.
+    if (!isRunningInWebBrowser()) {
         Logs.enableExpoCliLogging();
     }
     else {
@@ -12,5 +12,11 @@ if (Constants.manifest && Constants.manifest.logUrl) {
             'You are now debugging remotely; check your browser console for your application logs.',
         ]);
     }
+}
+/**
+ * In web browsers the `atob` function is defined, as per https://stackoverflow.com/a/42839384/1123156.
+ */
+function isRunningInWebBrowser() {
+    return typeof atob !== 'undefined';
 }
 //# sourceMappingURL=logging.fx.js.map
--- packages/expo/src/environment/logging.fx.ts
+++ packages/expo/src/environment/logging.fx.ts
@@ -5,12 +5,19 @@ import RemoteLogging from '../logs/RemoteLogging';
 
 if (Constants.manifest && Constants.manifest.logUrl) {
   // Enable logging to the Expo dev tools only if this JS is not running in a web browser (ex: the
-  // remote debugger)
-  if (!navigator.hasOwnProperty('userAgent')) {
+  // remote debugger). In Expo Web we don't show console logs in the CLI, so there's no special case needed.
+  if (!isRunningInWebBrowser()) {
     Logs.enableExpoCliLogging();
   } else {
     RemoteLogging.enqueueRemoteLogAsync('info', {}, [
       'You are now debugging remotely; check your browser console for your application logs.',
     ]);
   }
+}
+
+/**
+ * In web browsers the `atob` function is defined, as per https://stackoverflow.com/a/42839384/1123156.
+ */
+function isRunningInWebBrowser() {
+  return typeof atob !== 'undefined';
 }
