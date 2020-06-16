faa32dabc2eb5f0a81d73b15d95383ccc0a844f7
Evan Bacon <baconbrix@gmail.com>
2020-06-15 20:04:30 -0700

    Added node support

--- packages/expo-localization/CHANGELOG.md
+++ packages/expo-localization/CHANGELOG.md
@@ -4,13 +4,19 @@
 
 ### 🛠 Breaking changes
 
+- `Localization.region` changed from `undefined | string` to `null | string` on web to match iOS.
+
 ### 🎉 New features
+
+- Added doc blocks for everything.
+- Added support for SSR environments.
+- `Localization.isRTL` defaults to `false` in node environments.
 
 ### 🐛 Bug fixes
 
 ## 8.2.1 — 2020-05-29
 
-*This version does not introduce any user-facing changes.*
+_This version does not introduce any user-facing changes._
 
 ## 8.2.0 — 2020-05-27
 
--- packages/expo-localization/build/ExpoLocalization.js
+++ packages/expo-localization/build/ExpoLocalization.js
@@ -1,3 +1,57 @@
-import { NativeModulesProxy } from '@unimodules/core';
-export default NativeModulesProxy.ExpoLocalization;
+/* eslint-env browser */
+import { Platform } from '@unimodules/core';
+import * as rtlDetect from 'rtl-detect';
+export default {
+    get isRTL() {
+        return rtlDetect.isRtlLang(this.locale) ?? false;
+    },
+    get locale() {
+        if (!Platform.isDOMAvailable) {
+            return '';
+        }
+        const locale = navigator.language ||
+            navigator['systemLanguage'] ||
+            navigator['browserLanguage'] ||
+            navigator['userLanguage'] ||
+            this.locales[0];
+        return locale;
+    },
+    get locales() {
+        if (!Platform.isDOMAvailable) {
+            return [];
+        }
+        const { languages = [] } = navigator;
+        return Array.from(languages);
+    },
+    get timezone() {
+        const defaultTimeZone = 'Etc/UTC';
+        if (typeof Intl === 'undefined') {
+            return defaultTimeZone;
+        }
+        return Intl.DateTimeFormat().resolvedOptions().timeZone || defaultTimeZone;
+    },
+    get isoCurrencyCodes() {
+        // TODO(Bacon): Add this - very low priority
+        return [];
+    },
+    get region() {
+        const { locale } = this;
+        if (typeof locale === 'string' && locale.length) {
+            const isoCountryCode = locale.substring(locale.lastIndexOf('-') + 1);
+            return isoCountryCode.toUpperCase();
+        }
+        return null;
+    },
+    async getLocalizationAsync() {
+        const { region, isoCurrencyCodes, timezone, locales, locale, isRTL } = this;
+        return {
+            region,
+            isoCurrencyCodes,
+            timezone,
+            locales,
+            locale,
+            isRTL,
+        };
+    },
+};
 //# sourceMappingURL=ExpoLocalization.js.map
