87bbf56f7e258f91899a61b29d117f598c8afe36
Stanisław Chmiela <sjchmiela@gmail.com>
2020-06-17 15:52:38 +0200

    [expo-notifications] Fix types and add web support for NotificationChannelManager

--- packages/expo-notifications/src/NotificationChannelManager.ts
+++ packages/expo-notifications/src/NotificationChannelManager.ts
@@ -1,15 +1,5 @@
-import { NativeModulesProxy, ProxyNativeModule } from '@unimodules/core';
+import { NativeModulesProxy } from '@unimodules/core';
 
-import { NotificationChannel, NotificationChannelInput } from './NotificationChannelManager.types';
-
-export interface NotificationChannelManager extends ProxyNativeModule {
-  getNotificationChannelsAsync: () => Promise<NotificationChannel[] | null>;
-  getNotificationChannelAsync: (channelId: string) => Promise<NotificationChannel | null>;
-  setNotificationChannelAsync: (
-    channelId: string,
-    channelConfiguration: NotificationChannelInput
-  ) => Promise<NotificationChannel | null>;
-  deleteNotificationChannelAsync: (channelId: string) => Promise<void>;
-}
+import { NotificationChannelManager } from './NotificationChannelManager.types';
 
 export default (NativeModulesProxy.ExpoNotificationChannelManager as any) as NotificationChannelManager;
