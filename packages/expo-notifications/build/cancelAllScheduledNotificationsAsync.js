a603e9aee24dc55d21a631da6976998cb2acc358
Stanisław Chmiela <sjchmiela@gmail.com>
2020-06-17 17:51:27 +0200

    [expo-notifications] Fix a copy-paste typo

--- packages/expo-notifications/build/cancelAllScheduledNotificationsAsync.js
+++ packages/expo-notifications/build/cancelAllScheduledNotificationsAsync.js
@@ -1,6 +1,6 @@
 import { UnavailabilityError } from '@unimodules/core';
 import NotificationScheduler from './NotificationScheduler';
-export default async function cancelScheduledNotificationAsync() {
+export default async function cancelAllScheduledNotificationsAsync() {
     if (!NotificationScheduler.cancelAllScheduledNotificationsAsync) {
         throw new UnavailabilityError('Notifications', 'cancelAllScheduledNotificationsAsync');
     }
--- packages/expo-notifications/src/cancelAllScheduledNotificationsAsync.ts
+++ packages/expo-notifications/src/cancelAllScheduledNotificationsAsync.ts
@@ -2,7 +2,7 @@ import { UnavailabilityError } from '@unimodules/core';
 
 import NotificationScheduler from './NotificationScheduler';
 
-export default async function cancelScheduledNotificationAsync(): Promise<void> {
+export default async function cancelAllScheduledNotificationsAsync(): Promise<void> {
   if (!NotificationScheduler.cancelAllScheduledNotificationsAsync) {
     throw new UnavailabilityError('Notifications', 'cancelAllScheduledNotificationsAsync');
   }
