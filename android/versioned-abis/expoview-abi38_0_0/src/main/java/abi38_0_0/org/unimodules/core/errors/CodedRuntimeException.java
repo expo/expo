10830f9f784f75ed58d1d2280d323f2d3e655b54
Eric Samelson <eric@expo.io>
2020-06-17 11:26:34 -0700

    [android] redo SDK 38 versioning

---
+++ android/versioned-abis/expoview-abi38_0_0/src/main/java/abi38_0_0/org/unimodules/core/errors/CodedRuntimeException.java
@@ -0,0 +1,26 @@
+package abi38_0_0.org.unimodules.core.errors;
+
+import abi38_0_0.org.unimodules.core.interfaces.CodedThrowable;
+
+/**
+ * Base class that can be extended to create coded runtime errors that
+ * promise.reject can handle.
+ */
+
+public abstract class CodedRuntimeException extends RuntimeException implements CodedThrowable {
+  public CodedRuntimeException(String message) {
+    super(message);
+  }
+
+  public CodedRuntimeException(Throwable cause) {
+    super(cause);
+  }
+
+  public CodedRuntimeException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public String getCode() {
+    return "ERR_UNSPECIFIED_ANDROID_EXCEPTION";
+  }
+}
---
+++ android/versioned-abis/expoview-abi38_0_0/src/main/java/abi38_0_0/org/unimodules/interfaces/sensors/SensorService.java
@@ -0,0 +1,7 @@
+package abi38_0_0.org.unimodules.interfaces.sensors;
+
+import android.hardware.SensorEventListener2;
+
+public interface SensorService {
+  SensorServiceSubscription createSubscriptionForListener(SensorEventListener2 sensorEventListener);
+}
