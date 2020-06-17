62e4025303d1ae1d8eede5e2156ec92007c9c8b7
Brent Vatne <brentvatne@gmail.com>
2020-06-16 17:28:31 -0700

    [templates] Update tabs template to TypeScript and bump TypeScript version

--- templates/expo-template-tabs/components/Themed.tsx
+++ templates/expo-template-tabs/components/Themed.tsx
@@ -4,7 +4,10 @@ import { Text as DefaultText, View as DefaultView } from 'react-native';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 
-export function useThemeColor(props, colorName) {
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+) {
   const theme = useColorScheme();
   const colorFromProps = props[theme];
 
@@ -15,14 +18,22 @@ export function useThemeColor(props, colorName) {
   }
 }
 
-export function Text(props) {
+type ThemeProps = {
+  lightColor?: string;
+  darkColor?: string;
+};
+
+export type TextProps = ThemeProps & DefaultText['props'];
+export type ViewProps = ThemeProps & DefaultView['props'];
+
+export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return <DefaultText style={[{ color }, style]} {...otherProps} />;
 }
 
-export function View(props) {
+export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
