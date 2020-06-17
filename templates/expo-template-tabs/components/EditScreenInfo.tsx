e536bd748f5a90c24b0b4e1e305236350957730b
Brent Vatne <brentvatne@gmail.com>
2020-06-16 17:28:31 -0700

    [templates] Update tabs template to TypeScript and bump TypeScript version

--- templates/expo-template-tabs/components/EditScreenInfo.tsx
+++ templates/expo-template-tabs/components/EditScreenInfo.tsx
@@ -6,7 +6,7 @@ import Colors from '../constants/Colors';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
-export default function EditScreenInfo({ path }) {
+export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
       <View style={styles.getStartedContainer}>
