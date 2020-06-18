836a4913d4f9fb61ffd552186ae8e2c5bcd76bd0
Stanisław Chmiela <sjchmiela@gmail.com>
2020-06-18 10:02:28 +0200

    [expo-notifications] Fix types and add non-supported fallback for getExpoPushToken and InstallationIdProvider

---
+++ packages/expo-notifications/build/InstallationIdProvider.native.js
@@ -0,0 +1,3 @@
+import { NativeModulesProxy } from '@unimodules/core';
+export default NativeModulesProxy.NotificationsInstallationIdProvider;
+//# sourceMappingURL=InstallationIdProvider.native.js.map
