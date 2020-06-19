849c6834102fc564aac8da46440ab00b7495c8da
Brent Vatne <brentvatne@gmail.com>
2020-06-19 16:13:21 -0700

    [expo] Add support for enabling the unstable logbox via app.json

--- packages/expo/build/launch/registerRootComponent.expo.js
+++ packages/expo/build/launch/registerRootComponent.expo.js
@@ -1,10 +1,22 @@
-import 'expo/build/Expo.fx';
+import Constants from 'expo-constants';
 import { activateKeepAwake } from 'expo-keep-awake';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import withExpoRoot from './withExpoRoot';
 if (__DEV__) {
+    if (Constants.manifest?.experiments?.redesignedLogBox) {
+        // @ts-ignore: This needs to be run before Expo.fx, and it's not included on external types
+        YellowBox.__unstable_enableLogBox();
+    }
+    else {
+        // Replace the YellowBox.__unstable_enableLogBox function to make discovery easier if
+        // someone is following the React Native 62 blog post.
+        // @ts-ignore
+        YellowBox.__unstable_enableLogBox = () => console.warn('To enable the redesigned LogBox in your app, add "experiments": {"redesignedLogBox": true} to your app.json or app.config.js.');
+    }
     activateKeepAwake();
 }
+// Require side-effects
+require('expo/build/Expo.fx');
 export default function registerRootComponent(component) {
     AppRegistry.registerComponent('main', () => withExpoRoot(component));
 }
