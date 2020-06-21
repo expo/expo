a8d80f71df737fc344ed722e4efa4fad43582e88
Brent Vatne <brentvatne@gmail.com>
2020-06-20 23:15:14 -0700

    [expo] Add support for enabling the unstable logbox via app.json (#8887)
    * [expo] Add support for enabling the unstable logbox via app.json
    
    * [expo] Move LogBox related code to LogBox.fx.ts
    
    * [expo] Fix tests
    
    * [expo] Fix lint error

--- packages/expo/src/Expo.fx.expo.tsx
+++ packages/expo/src/Expo.fx.expo.tsx
@@ -1,6 +1,7 @@
 import './environment/validate.fx';
 import './environment/logging.fx';
 import './environment/muteWarnings.fx';
+import './environment/LogBox.fx';
 
 // load expo-asset immediately to set a custom `source` transformer in React Native
 import 'expo-asset';
