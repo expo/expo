eea086c5ba68ede8011fb398019673f85ad013ce
Brent Vatne <brentvatne@gmail.com>
2020-06-20 18:54:16 -0700

    [expo] Fix tests

--- packages/expo/build/Expo.fx.expo.d.ts
+++ packages/expo/build/Expo.fx.expo.d.ts
@@ -1,5 +1,5 @@
-import './environment/LogBox.fx';
 import './environment/validate.fx';
 import './environment/logging.fx';
 import './environment/muteWarnings.fx';
+import './environment/LogBox.fx';
 import 'expo-asset';
