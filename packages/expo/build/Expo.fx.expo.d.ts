60164f0790c2b3d1a5d535b13b71d7bd831bae39
Evan Bacon <baconbrix@gmail.com>
2020-06-23 16:14:03 -0700

    [expo][chore] Rename logging wrapper (#8935)
    * Give muteWarnings a more informative name
    
    * Update build

--- packages/expo/build/Expo.fx.expo.d.ts
+++ packages/expo/build/Expo.fx.expo.d.ts
@@ -1,5 +1,5 @@
 import './environment/validate.fx';
 import './environment/logging.fx';
-import './environment/muteWarnings.fx';
+import './environment/react-native-logs.fx';
 import './environment/LogBox.fx';
 import 'expo-asset';
