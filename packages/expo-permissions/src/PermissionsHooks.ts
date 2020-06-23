744a0c343c23adbc622d1c69127e66a271add672
Cedric van Putten <me@bycedric.com>
2020-06-23 12:37:25 +0200

    [permissions] Update usePermission hook example

--- packages/expo-permissions/src/PermissionsHooks.ts
+++ packages/expo-permissions/src/PermissionsHooks.ts
@@ -9,7 +9,14 @@ import { PermissionType, PermissionResponse, askAsync, getAsync } from './Permis
  * To ask the user permission, use the `askPermission` callback or `ask` option.
  *
  * @see https://docs.expo.io/versions/latest/sdk/permissions/
- * @example const [permission, askPermission, getPermission] = usePermissions(...);
+ * @example
+ * ```tsx
+ * const [permission, askPermission, getPermission] = usePermissions(Permissions.CAMERA);
+ *
+ * return permission?.granted
+ *   ? <Camera ... />
+ *   : <Button onPress={askPermission} />;
+ * ```
  */
 export function usePermissions(
   type: PermissionType | PermissionType[],
