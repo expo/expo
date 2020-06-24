1697956e147b361cd7070119cac1c8f0583d4cb1
Brent Vatne <brentvatne@gmail.com>
2020-06-24 15:46:56 -0700

    [templates] Minor fixes to templates

--- templates/expo-template-bare-minimum/App.js
+++ templates/expo-template-bare-minimum/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
