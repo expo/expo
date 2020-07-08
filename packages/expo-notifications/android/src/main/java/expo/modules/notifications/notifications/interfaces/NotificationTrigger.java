8b4b1b658516b97c9663ffb9a57194c055cd1a8d
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-07-08 10:30:51 +0200

    [expo-notifications] Get `channelId` from remote message (#9080)
    # Why
    
    Fixes https://github.com/expo/expo/issues/8990.
    
    # How
    
    - Get `channelId` from the remote message.
    
    # Test Plan
    
    - send notification via expo.io/notifications and check the channel ✅
    
    # Changelog 
    
    - Fixed remote notifications ignoring the `channelId` parameter.

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/interfaces/NotificationTrigger.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/interfaces/NotificationTrigger.java
@@ -1,10 +1,19 @@
 package expo.modules.notifications.notifications.interfaces;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Parcelable;
+
+import androidx.annotation.Nullable;
 
 /**
  * An interface specifying source of the notification, to be implemented
  * by concrete classes.
  */
 public interface NotificationTrigger extends Parcelable {
+  @Nullable
+  @TargetApi(Build.VERSION_CODES.O)
+  default String getNotificationChannel() {
+    return null;
+  }
 }
