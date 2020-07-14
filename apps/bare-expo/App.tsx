565ee1631a5b1a81f1f90f54a9f37d387f8855b4
Michał Czernek <czernekmichal@gmail.com>
2020-07-14 13:56:03 +0200

    [WIP]

--- apps/bare-expo/App.tsx
+++ apps/bare-expo/App.tsx
@@ -1,52 +1,91 @@
+import * as Notifications from 'expo-notifications';
 import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
 
-import MainNavigator from './MainNavigator';
-import { createProxy, startAsync, addListener } from './relapse/client';
-let Notifications;
-try {
-  Notifications = require('expo-notifications');
-} catch (e) {
-  // do nothing
-}
+const showNotification = () => {
+  Notifications.presentNotificationAsync({
+    title: 'Notification',
+  });
+};
+
+const scheduleNotification = async () => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Here is a scheduled notification!',
+      body: 'This is the body',
+      data: {
+        hello: 'there',
+        future: 'self',
+      },
+      sound: true,
+    },
+    trigger: null,
+  });
+};
 
 export default function Main() {
   // @ts-ignore
-  if (global.DETOX) {
-    React.useEffect(() => {
-      addListener(data => {
-        if (data.globals) {
-          for (const moduleName of data.globals) {
-            // @ts-ignore
-            global[moduleName] = createProxy(moduleName);
-          }
-        }
-      });
+  // if (global.DETOX) {
+  //   React.useEffect(() => {
+  //     addListener(data => {
+  //       if (data.globals) {
+  //         for (const moduleName of data.globals) {
+  //           // @ts-ignore
+  //           global[moduleName] = createProxy(moduleName);
+  //         }
+  //       }
+  //     });
 
-      let stop;
-      startAsync().then(_stop => (stop = _stop));
+  //     let stop;
+  //     startAsync().then(_stop => (stop = _stop));
 
-      return () => stop && stop();
-    }, []);
-  }
+  //     return () => stop && stop();
+  //   }, []);
+  // }
 
-  React.useEffect(() => {
-    try {
-      const subscription = Notifications.addNotificationResponseReceivedListener(
-        ({ notification, actionIdentifier }) => {
-          console.info(
-            `User interacted with a notification (action = ${actionIdentifier}): ${JSON.stringify(
-              notification,
-              null,
-              2
-            )}`
-          );
-        }
-      );
-      return () => subscription?.remove();
-    } catch (e) {
-      console.debug('Could not have added a listener for received notification responses.', e);
-    }
-  }, []);
+  // React.useEffect(() => {
+  //   try {
+  //     const subscription = Notifications.addNotificationResponseReceivedListener(
+  //       ({ notification, actionIdentifier }) => {
+  //         console.info(
+  //           `User interacted with a notification (action = ${actionIdentifier}): ${JSON.stringify(
+  //             notification,
+  //             null,
+  //             2
+  //           )}`
+  //         );
+  //       }
+  //     );
+  //     return () => subscription?.remove();
+  //   } catch (e) {
+  //     console.debug('Could not have added a listener for received notification responses.', e);
+  //   }
+  // }, []);
 
-  return <MainNavigator uriPrefix="bareexpo://" />;
+  // return <MainNavigator uriPrefix="bareexpo://" />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonWrapper}>
+        <Button title="Show notification" onPress={showNotification} />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title="Schedule notification" onPress={scheduleNotification} />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    padding: 10,
+  },
+  button: {
+    alignSelf: 'center',
+  },
+});
