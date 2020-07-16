05e784e209f0c3d4ce9a323a583bc02f6eb79054
Brent Vatne <brentvatne@gmail.com>
2020-07-16 15:46:46 -0700

    [templates] Update for the latest version of expo-splash-screen

--- templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainActivity.java
+++ templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends ReactActivity {
     super.onCreate(savedInstanceState);
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class);
+    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, false);
   }
 
 
