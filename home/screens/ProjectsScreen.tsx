e78732180461081773e91d3aa7bc942c18f53916
Evan Bacon <baconbrix@gmail.com>
2020-07-17 18:19:21 -0700

    [home] Disable clipboard polling on iOS 14 devices (#9258)
    * Disable clipboard polling on iOS 14 devices
    
    * Enable opening
    
    * Update home/utils/UrlUtils.js
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>
    
    * Disable clipboard polling completely on iOS
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>

--- home/screens/ProjectsScreen.tsx
+++ home/screens/ProjectsScreen.tsx
@@ -242,7 +242,10 @@ export default class ProjectsScreen extends React.Component<Props, State> {
     if (IS_RESTRICTED) {
       return <NoProjectTools />;
     } else {
-      return <ProjectTools pollForUpdates={this.props.isFocused} />;
+      // Disable polling the clipboard on iOS because it presents a notification every time the clipboard is read.
+      const pollForUpdates = this.props.isFocused && Platform.OS !== 'ios';
+
+      return <ProjectTools pollForUpdates={pollForUpdates} />;
     }
   };
 
