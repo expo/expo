69e28dd2b5acbb9caca33a798e71d57d1d43814d
Evan Bacon <baconbrix@gmail.com>
2020-07-21 13:56:36 -0700

    Convert more utils to TypeScript

--- home/utils/addListenerWithNativeCallback.ts
+++ home/utils/addListenerWithNativeCallback.ts
@@ -2,7 +2,10 @@ import { DeviceEventEmitter } from 'react-native';
 
 import * as Kernel from '../kernel/Kernel';
 
-const addListenerWithNativeCallback = (eventName, eventListener) => {
+const addListenerWithNativeCallback = (
+  eventName: string,
+  eventListener: (event: any) => Promise<any>
+) => {
   DeviceEventEmitter.addListener(eventName, async event => {
     try {
       let result = await eventListener(event);
--- home/utils/getSnackId.ts
+++ home/utils/getSnackId.ts
@@ -1,8 +1,9 @@
 import Constants from 'expo-constants';
 import sha1 from 'sha1';
 
-let _snackId;
-export default function getSnackId() {
+let _snackId: string | undefined;
+
+export default function getSnackId(): string {
   if (!_snackId) {
     const hash = sha1(Constants.installationId).toLowerCase();
     _snackId = `${hash.slice(0, 4)}-${hash.slice(4, 8)}`;
