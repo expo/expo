93cacaec4b8eccf7c215bcf3caa0e8922a80eb80
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-07-24 13:11:46 -0700

    [dev-client-test] Initial minimal bare template

---
+++ apps/dev-client-test/android/app/src/main/java/com/devclienttest/generated/BasePackageList.java
@@ -0,0 +1,24 @@
+package com.devclienttest.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.location.LocationPackage(),
+        new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
+        new expo.modules.sqlite.SQLitePackage(),
+        new expo.modules.updates.UpdatesPackage()
+    );
+  }
+}
