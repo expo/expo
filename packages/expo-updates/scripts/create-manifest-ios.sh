db13c284e40386beca3732a795ab64c34478c1eb
Brent Vatne <brentvatne@gmail.com>
2020-07-24 12:27:01 -0700

    [expo-updates] Fetch asset manifest through programmatic CLI interface instead of depending on a running React Native CLI server (#9372)
    * Fetch asset manifest through programmatic CLI interface instead of depending on a running React Native CLI server
    
    * Update CHANGELOG

--- packages/expo-updates/scripts/create-manifest-ios.sh
+++ packages/expo-updates/scripts/create-manifest-ios.sh
@@ -5,9 +5,10 @@ set -eo pipefail
 DEST="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
 ENTRY_FILE=${ENTRY_FILE:-index.js}
 RCT_METRO_PORT=${RCT_METRO_PORT:=8081}
-REACT_NATIVE_PACKAGER_HOSTNAME=${REACT_NATIVE_PACKAGER_HOSTNAME:-"localhost:$RCT_METRO_PORT"}
-ASSETS_URL="http://$REACT_NATIVE_PACKAGER_HOSTNAME/"${ENTRY_FILE%.js}".assets?platform=ios&dev=false"
 NODE_BINARY=${NODE_BINARY:-node}
+
+# Related to: https://github.com/facebook/react-native/blob/c9f869f9c7c8b035a669980382af4bbd4afecb89/scripts/react-native-xcode.sh#L59-L69
+PROJECT_ROOT=${PROJECT_ROOT:-$PWD/..}
 
 if ! [ -x "$(command -v $NODE_BINARY)" ]; then
   echo 'Error: cannot find the node binary. Try setting the NODE_BINARY variable in the ' \
@@ -16,4 +17,4 @@ if ! [ -x "$(command -v $NODE_BINARY)" ]; then
   exit 1
 fi
 
-"$NODE_BINARY" "$(dirname "${BASH_SOURCE[0]}")/createManifest.js" ios "$ASSETS_URL" "$DEST"
+"$NODE_BINARY" "$(dirname "${BASH_SOURCE[0]}")/createManifest.js" ios "$PROJECT_ROOT" "$DEST"
