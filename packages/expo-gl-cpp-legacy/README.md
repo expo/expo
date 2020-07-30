c012df158ee8b67ffe20caa4e32741c538dfcac8
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2020-07-30 12:33:24 +0200

    copy code to expo-gl-legacy

---
+++ packages/expo-gl-cpp-legacy/README.md
@@ -0,0 +1,36 @@
+# expo-gl-cpp
+
+C++ bindings for WebGL 2.0 used in Expo GL module
+
+# API documentation
+
+- [Documentation for the master branch](https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/sdk/gl-view.md)
+- [Documentation for the latest stable release](https://docs.expo.io/versions/latest/sdk/gl-view/)
+
+# Installation in managed Expo projects
+
+For managed [managed](https://docs.expo.io/versions/latest/introduction/managed-vs-bare/) Expo projects, please follow the installation instructions in the [API documentation for the latest stable release](https://docs.expo.io/versions/latest/sdk/gl-view/).
+
+# Installation in bare React Native projects
+
+For bare React Native projects, you must ensure that you have [installed and configured the `react-native-unimodules` package](https://github.com/unimodules/react-native-unimodules) before continuing.
+
+### Add the package to your npm dependencies
+
+Note: this should be used in conjunction with [expo-gl](https://github.com/expo/expo/tree/master/packages/expo-gl).
+
+```
+expo install expo-gl-cpp
+```
+
+### Configure for iOS
+
+Run `npx pod-install` after installing the npm package.
+
+### Configure for Android
+
+No additional set up necessary.
+
+# Contributing
+
+Contributions are very welcome! Please refer to guidelines described in the [contributing guide](https://github.com/expo/expo#contributing).
---
+++ packages/expo-gl-cpp-legacy/cpp/UEXGL.h
@@ -0,0 +1,87 @@
+#ifndef __UEXGL_H__
+#define __UEXGL_H__
+
+
+#ifdef __ANDROID__
+#include <GLES3/gl3.h>
+#endif
+#ifdef __APPLE__
+#include <OpenGLES/ES3/gl.h>
+#endif
+
+#ifdef __cplusplus
+#include <functional>
+#endif
+
+#include <JavaScriptCore/JSBase.h>
+
+
+// NOTE: The symbols exposed by this header are named with a `UEX` prefix rather than an `EX`
+//       prefix so that they are unaffected by the automated iOS versioning script when
+//       referenced in versioned Objective-C code. The EXGL C/C++ library is not versioned
+//       and there is only one copy of its code in the binary form of the Expo application.
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Identifies an EXGL context. No EXGL context has the id 0, so that can be
+// used as a 'null' value.
+typedef unsigned int UEXGLContextId;
+
+// Identifies an EXGL object. EXGL objects represent virtual mappings to underlying OpenGL objects.
+// No EXGL object has the id 0, so that can be used as a 'null' value.
+typedef unsigned int UEXGLObjectId;
+
+// [JS thread] Create an EXGL context and return its id number. Saves the
+// JavaScript interface object (has a WebGLRenderingContext-style API) at
+// `global.__EXGLContexts[id]` in JavaScript.
+UEXGLContextId UEXGLContextCreate(JSGlobalContextRef jsCtx);
+
+#ifdef __cplusplus
+// [JS thread] Pass function to cpp that will run GL operations on GL thread
+void UEXGLContextSetFlushMethod(UEXGLContextId exglCtxId, std::function<void(void)> flushMethod);
+#endif
+
+#ifdef __APPLE__
+// Objective-C wrapper for UEXGLContextSetFlushMethod
+typedef void(^UEXGLFlushMethodBlock)(void);
+void UEXGLContextSetFlushMethodObjc(UEXGLContextId exglCtxId, UEXGLFlushMethodBlock flushMethod);
+#endif
+
+// [Any thread] Check whether we should redraw the surface
+bool UEXGLContextNeedsRedraw(UEXGLContextId exglCtxId);
+
+// [GL thread] Tell cpp that we finished drawing to the surface
+void UEXGLContextDrawEnded(UEXGLContextId exglCtxId);
+
+// [Any thread] Release the resources for an EXGL context. The same id is never
+// reused.
+void UEXGLContextDestroy(UEXGLContextId exglCtxId);
+
+// [GL thread] Perform one frame's worth of queued up GL work
+void UEXGLContextFlush(UEXGLContextId exglCtxId);
+
+// [GL thread] Set the default framebuffer (used when binding 0). Allows using
+// platform-specific extensions on the default framebuffer, such as MSAA.
+void UEXGLContextSetDefaultFramebuffer(UEXGLContextId exglCtxId, GLint framebuffer);
+
+// [Any thread] Create an EXGL object. Initially maps to the OpenGL object zero.
+UEXGLObjectId UEXGLContextCreateObject(UEXGLContextId exglCtxId);
+
+// [GL thread] Destroy an EXGL object.
+void UEXGLContextDestroyObject(UEXGLContextId exglCtxId, UEXGLObjectId exglObjId);
+
+// [GL thread] Set the underlying OpenGL object an EXGL object maps to.
+void UEXGLContextMapObject(UEXGLContextId exglCtxId, UEXGLObjectId exglObjId, GLuint glObj);
+
+// [GL thread] Get the underlying OpenGL object an EXGL object maps to.
+GLuint UEXGLContextGetObject(UEXGLContextId exglCtxId, UEXGLObjectId exglObjId);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
