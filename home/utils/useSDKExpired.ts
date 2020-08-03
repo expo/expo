899d93dc66697eafbfdeed38c2a9ce3588033232
Evan Bacon <baconbrix@gmail.com>
2020-08-03 13:36:56 -0700

    Update useSDKExpired.ts

--- home/utils/useSDKExpired.ts
+++ home/utils/useSDKExpired.ts
@@ -6,7 +6,7 @@ export function useSDKExpired(sdkVersion?: string): [boolean, number | null] {
     // undefined or UNVERSIONED will be false since we don't know.
     const majorVersionString = sdkVersion?.split('.').shift();
     if (majorVersionString) {
-      const sdkNumber = parseInt(majorVersionString);
+      const sdkNumber = parseInt(majorVersionString, 10);
       return [sdkNumber < Environment.lowestSupportedSdkVersion, sdkNumber];
     }
     return [false, null];
