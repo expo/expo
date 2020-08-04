54f755d3c2d80615c2c3b48faa38d46b9e7c2078
Stanisław Chmiela <sjchmiela@gmail.com>
2020-08-04 15:49:56 +0200

    Update DateTimePickerScreen with example screen from the library

--- apps/native-component-list/src/screens/DateTimePickerScreen.tsx
+++ apps/native-component-list/src/screens/DateTimePickerScreen.tsx
@@ -1,29 +1,52 @@
-import DateTimePicker, {
-  AndroidNativeProps,
-  IOSNativeProps,
-} from '@react-native-community/datetimepicker';
-import moment from 'moment';
+/* eslint-disable */
+// @ts-nocheck
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  Platform,
+  TextInput,
+  useColorScheme,
+} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import React, { useState } from 'react';
-import { Button, Platform, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import moment from 'moment';
 
 // This example is a refactored copy from https://github.com/react-native-community/react-native-datetimepicker/tree/master/example
 // Please try to keep it up to date when updating @react-native-community/datetimepicker package :)
 
-const DateTimePickerScreen = () => {
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState<IOSNativeProps['mode'] | AndroidNativeProps['mode']>('date');
-  const [show, setShow] = useState(false);
-  const [color, setColor] = useState<string | undefined>();
-  const [display, setDisplay] = useState('default');
+const ThemedText = props => {
+  const isDarkMode = useColorScheme() === 'dark';
 
-  const onChange = (event: any, selectedDate: Date | undefined) => {
+  const textColorByMode = { color: isDarkMode ? Colors.white : Colors.black };
+
+  const TextElement = React.createElement(Text, props);
+  return React.cloneElement(TextElement, {
+    style: [props.style, textColorByMode],
+  });
+};
+
+export default () => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [color, setColor] = useState();
+  const [display, setDisplay] = useState('default');
+  const [interval, setMinInterval] = useState(undefined);
+
+  const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
 
     setShow(Platform.OS === 'ios');
     setDate(currentDate);
   };
 
-  const showMode = (currentMode: IOSNativeProps['mode'] | AndroidNativeProps['mode']) => {
+  const showMode = currentMode => {
     setShow(true);
     setMode(currentMode);
   };
@@ -45,25 +68,49 @@ const DateTimePickerScreen = () => {
 
   const showTimepickerSpinner = () => {
     showMode('time');
+    setMinInterval(undefined);
     setDisplay('spinner');
   };
 
+  const showTimepickerClockModeWithInterval = () => {
+    showMode('time');
+    setMinInterval(5);
+    setDisplay('clock');
+  };
+
+  const showTimepickerSpinnerWithInterval = () => {
+    showMode('time');
+    setMinInterval(5);
+    setDisplay('spinner');
+  };
+
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.dark : Colors.lighter,
+  };
+
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      {(global as any).HermesInternal == null ? null : (
-        <View style={styles.engine}>
-          <Text testID="hermesIndicator" style={styles.footer}>
-            Engine: Hermes
-          </Text>
-        </View>
-      )}
-      <View>
-        <View testID="appRootView" style={styles.container}>
+    <SafeAreaView style={[backgroundStyle, { flex: 1 }]}>
+      <StatusBar barStyle="dark-content" />
+      <ScrollView>
+        {global.HermesInternal != null && (
+          <View style={styles.engine}>
+            <Text testID="hermesIndicator" style={styles.footer}>
+              Engine: Hermes
+            </Text>
+          </View>
+        )}
+        <View
+          testID="appRootView"
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
           <View style={styles.header}>
-            <Text style={styles.text}>Example DateTime Picker</Text>
+            <ThemedText style={styles.text}>Example DateTime Picker</ThemedText>
           </View>
           <View style={styles.header}>
-            <Text style={{ margin: 10, flex: 1 }}>text color (iOS only)</Text>
+            <ThemedText style={{ margin: 10, flex: 1 }}>text color (iOS only)</ThemedText>
             <TextInput
               value={color}
               style={{ height: 60, flex: 1 }}
@@ -97,39 +144,63 @@ const DateTimePickerScreen = () => {
               title="Show time picker spinner!"
             />
           </View>
+          <View style={styles.button}>
+            <Button
+              testID="timePickerDefaultIntervalButton"
+              onPress={showTimepickerClockModeWithInterval}
+              title="Show time picker as clock (with 5 min interval)!"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              testID="timePickerSpinnerIntervalButton"
+              onPress={showTimepickerSpinnerWithInterval}
+              title="Show time picker as spinner (with 5 min interval)!"
+            />
+          </View>
           <View style={styles.header}>
-            <Text testID="dateTimeText" style={styles.dateTimeText}>
-              {mode === 'time' && moment.utc(date).format('HH:mm')}
-              {mode === 'date' && moment.utc(date).format('MM/DD/YYYY')}
-            </Text>
+            <ThemedText testID="dateText" style={styles.dateTimeText}>
+              {moment.utc(date).format('MM/DD/YYYY')}
+            </ThemedText>
+            <Text> </Text>
+            <ThemedText testID="timeText" style={styles.dateTimeText}>
+              {moment.utc(date).format('HH:mm')}
+            </ThemedText>
             <Button testID="hidePicker" onPress={() => setShow(false)} title="hide picker" />
           </View>
           {show && (
             <DateTimePicker
               testID="dateTimePicker"
               timeZoneOffsetInMinutes={0}
+              minuteInterval={interval}
               value={date}
               mode={mode}
-              {...(mode === 'time' ? { is24Hour: true } : {})}
-              {...(Platform.OS === 'android' ? { display } : {})}
-              minuteInterval={15}
+              is24Hour
+              display={display}
               onChange={onChange}
               style={styles.iOsPicker}
               textColor={color || undefined}
             />
           )}
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
   engine: {
     position: 'absolute',
     right: 0,
   },
+  body: {
+    backgroundColor: Colors.white,
+  },
   footer: {
+    color: Colors.dark,
     fontSize: 12,
     fontWeight: '600',
     padding: 4,
@@ -140,6 +211,14 @@ const styles = StyleSheet.create({
     marginTop: 32,
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  containerWindows: {
+    marginTop: 32,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   header: {
     justifyContent: 'center',
@@ -158,17 +237,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   dateTimeText: {
-    paddingRight: 20,
     fontSize: 16,
     fontWeight: 'normal',
   },
   iOsPicker: {
     flex: 1,
   },
+  windowsPicker: {
+    flex: 1,
+    paddingTop: 10,
+    width: 350,
+  },
 });
-
-DateTimePickerScreen.navigationOptions = {
-  title: 'DateTimePicker',
-};
-
-export default DateTimePickerScreen;
