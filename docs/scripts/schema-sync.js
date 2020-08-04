ddc3c0d9081a2bc56bf43cbb506dab08a9e00afa
tommybru <tbru@stanford.edu>
2020-08-04 13:55:36 -0400

    updates schema-sync to be exported

--- docs/scripts/schema-sync.js
+++ docs/scripts/schema-sync.js
@@ -1,49 +1,57 @@
 /*
-This script updates the necessary schema for the passed-in version.
+This script updates the necessary xdl schemas.
 
-yarn run schema-sync 38 -> updates the schema that versions/v38.0.0/sdk/app-config.md uses
-yarn run schema-sync unversioned -> updates the schema that versions/unversioned/sdk/app-config.md uses
+At build time (or when running a dev server), next.config.js will 
+automatically runs this script for every SDK version
 */
 
 const axios = require('axios');
-let fsExtra = require('fs-extra');
+const fsExtra = require('fs-extra');
 
-let version = process.argv[2];
+const schema_sync = function(version) {
+  if (!version) {
+    console.log('Please enter a version number\n');
+    console.log('E.g., "yarn run schema-sync 38" \nor, "yarn run schema-sync unversioned"');
+    return;
+  }
 
-if (!version) {
-  console.log('Please enter a version number\n');
-  console.log('E.g., "yarn run schema-sync 38" \nor, "yarn run schema-sync unversioned"');
-  return;
-}
+  if (version === 'unversioned') {
+    axios
+      .get(`http://exp.host/--/api/v2/project/configuration/schema/UNVERSIONED`)
+      .then(async ({ data }) => {
+        await fsExtra.writeFile(
+          `scripts/schemas/unversioned/app-config-schema.js`,
+          'export default ',
+          'utf8'
+        );
+        await fsExtra.appendFile(
+          `scripts/schemas/unversioned/app-config-schema.js`,
+          JSON.stringify(data.data.schema.properties),
+          'utf8'
+        );
+      });
+  } else {
+    axios
+      .get(`http://exp.host/--/api/v2/project/configuration/schema/${version}.0.0`)
+      .then(async ({ data }) => {
+        await fsExtra.writeFile(
+          `scripts/schemas/v${version}.0.0/app-config-schema.js`,
+          'export default ',
+          'utf8'
+        );
+        await fsExtra.appendFile(
+          `scripts/schemas/v${version}.0.0/app-config-schema.js`,
+          JSON.stringify(data.data.schema.properties),
+          'utf8'
+        );
+      });
+  }
+};
 
-if (version === 'unversioned') {
-  axios
-    .get(`http://exp.host/--/api/v2/project/configuration/schema/UNVERSIONED`)
-    .then(async ({ data }) => {
-      await fsExtra.writeFile(
-        `scripts/schemas/unversioned/app-config-schema.js`,
-        'export default ',
-        'utf8'
-      );
-      await fsExtra.appendFile(
-        `scripts/schemas/unversioned/app-config-schema.js`,
-        JSON.stringify(data.data.schema.properties),
-        'utf8'
-      );
-    });
-} else {
-  axios
-    .get(`http://exp.host/--/api/v2/project/configuration/schema/${version}.0.0`)
-    .then(async ({ data }) => {
-      await fsExtra.writeFile(
-        `scripts/schemas/v${version}.0.0/app-config-schema.js`,
-        'export default ',
-        'utf8'
-      );
-      await fsExtra.appendFile(
-        `scripts/schemas/v${version}.0.0/app-config-schema.js`,
-        JSON.stringify(data.data.schema.properties),
-        'utf8'
-      );
-    });
+module.exports = { schema_sync };
+
+// if running script manually, include version parameter
+// e.g., node ./scripts/schema-sync.js 38
+if (require.main === module) {
+  schema_sync(process.argv[2]);
 }
