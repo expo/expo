e9c2b6fb38fe6c07d5bdf77e5acf2725289f6367
Tommy Bruzzese <56043296+tommybru@users.noreply.github.com>
2020-08-05 15:56:05 -0400

    [packages][expo-error-recovery] handle SecurityError exception (#9257)
    * [packages][expo-error-recovery] handle SecurityError exception
    
    * fixes return for typescript
    
    * updates CHANGELOG, rebuilds
    
    * removes console.log, replaces w comment
    
    * fixes CHANGELOG typo, rebuilds
    
    * adds no-op note to unversioned, sdk38 docs

--- packages/expo-error-recovery/src/ExpoErrorRecovery.web.ts
+++ packages/expo-error-recovery/src/ExpoErrorRecovery.web.ts
@@ -3,9 +3,14 @@ const LOCAL_STORAGE_KEY = 'EXPO_ERROR_RECOVERY_STORAGE';
 
 function _consumeRecoveryProps(): string | null {
   if (!canUseDOM) return null;
-  const props = localStorage.getItem(LOCAL_STORAGE_KEY);
-  localStorage.removeItem(LOCAL_STORAGE_KEY);
-  return props;
+  try {
+    const props = localStorage.getItem(LOCAL_STORAGE_KEY);
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    return props;
+  } catch (e) {
+    // Catches localStorage SecurityError https://github.com/expo/expo/issues/8355
+  }
+  return null;
 }
 
 export default {
@@ -15,7 +20,11 @@ export default {
 
   saveRecoveryProps(props: string): void {
     if (!canUseDOM) return;
-    localStorage.setItem(LOCAL_STORAGE_KEY, props);
+    try {
+      localStorage.setItem(LOCAL_STORAGE_KEY, props);
+    } catch (e) {
+      // Catches localStorage SecurityError https://github.com/expo/expo/issues/8355
+    }
   },
 
   recoveredProps: _consumeRecoveryProps(),
