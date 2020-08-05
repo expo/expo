5673b822bcffd02a92f5e8b38f7fbdef3ca476ad
Stanisław Chmiela <sjchmiela@gmail.com>
2020-08-05 11:45:49 +0200

    Upgrade library with expotools

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/netinfo/NetInfoUtils.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/netinfo/NetInfoUtils.java
@@ -6,6 +6,12 @@
  */
 package versioned.host.exp.exponent.modules.api.netinfo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.content.ContextCompat;
+
 public class NetInfoUtils {
     public static void reverseByteArray(byte[] array) {
         for (int i = 0; i < array.length / 2; i++) {
@@ -13,5 +19,10 @@ public class NetInfoUtils {
             array[i] = array[array.length - i - 1];
             array[array.length - i - 1] = temp;
         }
+    }
+
+    public static boolean isAccessWifiStatePermissionGranted(Context context) {
+        return ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_WIFI_STATE) == PackageManager.PERMISSION_GRANTED;
     }
 }
