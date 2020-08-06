a2e6b20aa1b0166ce3ecc82886cfa2d4f12def00
Evan Bacon <baconbrix@gmail.com>
2020-08-05 17:59:07 -0700

    Add web permissions to notifications module

--- packages/expo-notifications/build/NotificationPermissionsModule.js
+++ packages/expo-notifications/build/NotificationPermissionsModule.js
@@ -1,5 +1,58 @@
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
+import { PermissionStatus } from 'unimodules-permissions-interface';
+function convertPermissionStatus(status) {
+    switch (status) {
+        case 'granted':
+            return {
+                status: PermissionStatus.GRANTED,
+                expires: 'never',
+                canAskAgain: false,
+                granted: true,
+            };
+        case 'denied':
+            return {
+                status: PermissionStatus.DENIED,
+                expires: 'never',
+                canAskAgain: false,
+                granted: false,
+            };
+        default:
+            return {
+                status: PermissionStatus.UNDETERMINED,
+                expires: 'never',
+                canAskAgain: true,
+                granted: false,
+            };
+    }
+}
+async function resolvePermissionAsync(shouldAsk) {
+    if (!canUseDOM) {
+        return convertPermissionStatus('denied');
+    }
+    if (typeof Notification.requestPermission !== 'undefined') {
+        let status = Notification.permission;
+        if (shouldAsk) {
+            status = await Notification.requestPermission();
+        }
+        return convertPermissionStatus(status);
+    }
+    else if (typeof navigator === 'undefined' ||
+        typeof navigator.permissions === 'undefined' ||
+        typeof navigator.permissions.query === 'undefined') {
+        const query = await navigator.permissions.query({ name: 'notifications' });
+        return convertPermissionStatus(query.state);
+    }
+    // Platforms like iOS Safari don't support Notifications so return denied.
+    return convertPermissionStatus('denied');
+}
 export default {
     addListener: () => { },
     removeListeners: () => { },
+    async getPermissionsAsync() {
+        return resolvePermissionAsync(false);
+    },
+    async requestPermissionsAsync(request) {
+        return resolvePermissionAsync(true);
+    },
 };
 //# sourceMappingURL=NotificationPermissionsModule.js.map
--- packages/expo-notifications/src/NotificationPermissionsModule.ts
+++ packages/expo-notifications/src/NotificationPermissionsModule.ts
@@ -1,6 +1,72 @@
+import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment';
+import { PermissionStatus } from 'unimodules-permissions-interface';
+
+import {
+  NativeNotificationPermissionsRequest,
+  NotificationPermissionsStatus,
+} from './NotificationPermissions.types';
 import { NotificationPermissionsModule } from './NotificationPermissionsModule.types';
+
+function convertPermissionStatus(
+  status?: NotificationPermission | 'prompt'
+): NotificationPermissionsStatus {
+  switch (status) {
+    case 'granted':
+      return {
+        status: PermissionStatus.GRANTED,
+        expires: 'never',
+        canAskAgain: false,
+        granted: true,
+      };
+    case 'denied':
+      return {
+        status: PermissionStatus.DENIED,
+        expires: 'never',
+        canAskAgain: false,
+        granted: false,
+      };
+    default:
+      return {
+        status: PermissionStatus.UNDETERMINED,
+        expires: 'never',
+        canAskAgain: true,
+        granted: false,
+      };
+  }
+}
+
+async function resolvePermissionAsync(shouldAsk: boolean): Promise<NotificationPermissionsStatus> {
+  if (!canUseDOM) {
+    return convertPermissionStatus('denied');
+  }
+
+  if (typeof Notification.requestPermission !== 'undefined') {
+    let status = Notification.permission;
+    if (shouldAsk) {
+      status = await Notification.requestPermission();
+    }
+    return convertPermissionStatus(status);
+  } else if (
+    typeof navigator === 'undefined' ||
+    typeof navigator.permissions === 'undefined' ||
+    typeof navigator.permissions.query === 'undefined'
+  ) {
+    const query = await navigator.permissions.query({ name: 'notifications' });
+    return convertPermissionStatus(query.state);
+  }
+  // Platforms like iOS Safari don't support Notifications so return denied.
+  return convertPermissionStatus('denied');
+}
 
 export default {
   addListener: () => {},
   removeListeners: () => {},
+  async getPermissionsAsync(): Promise<NotificationPermissionsStatus> {
+    return resolvePermissionAsync(false);
+  },
+  async requestPermissionsAsync(
+    request: NativeNotificationPermissionsRequest
+  ): Promise<NotificationPermissionsStatus> {
+    return resolvePermissionAsync(true);
+  },
 } as NotificationPermissionsModule;
