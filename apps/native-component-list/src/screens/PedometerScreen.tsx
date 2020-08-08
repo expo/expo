6c26ce6a3569305256f6a50592422c9726b85316
Evan Bacon <baconbrix@gmail.com>
2020-08-08 14:44:25 -0700

    Improved the pedometer screen in NCL

--- apps/native-component-list/src/screens/PedometerScreen.tsx
+++ apps/native-component-list/src/screens/PedometerScreen.tsx
@@ -1,61 +1,114 @@
 import { Pedometer } from 'expo-sensors';
-import React from 'react';
-import { Alert, ScrollView, Text } from 'react-native';
+import * as React from 'react';
+import { ScrollView, Text, View } from 'react-native';
 
 import ListButton from '../components/ListButton';
+import { useResolvedValue } from '../utilities/useResolvedValue';
+import { H2 } from '@expo/html-elements';
 
-interface State {
-  stepCount?: number;
+function usePedometer({ isActive }: { isActive: boolean }): Pedometer.PedometerResult | null {
+  const [data, setData] = React.useState<Pedometer.PedometerResult | null>(null);
+  const listener = React.useRef<Pedometer.PedometerListener | null>(null);
+
+  React.useEffect(() => {
+    return () => {
+      listener.current?.remove();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (isActive) {
+      listener.current = Pedometer.watchStepCount(setData);
+    } else {
+      listener.current?.remove();
+    }
+  }, [isActive]);
+
+  return data;
 }
 
-export default class PedometerScreen extends React.Component<object, State> {
-  static navigationOptions = {
-    title: 'Pedometer',
-  };
+function usePedometerHistory({
+  start,
+  end,
+}: {
+  start: Date;
+  end: Date;
+}): Pedometer.PedometerResult | null {
+  const [data, setData] = React.useState<Pedometer.PedometerResult | null>(null);
+  const isMounted = React.useRef(true);
 
-  readonly state: State = {};
+  React.useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
-  listener?: Pedometer.PedometerListener;
+  React.useEffect(() => {
+    Pedometer.getStepCountAsync(start, end).then(data => {
+      if (isMounted.current) {
+        setData(data);
+      }
+    });
+  }, [start, end]);
 
-  render() {
+  return data;
+}
+
+function StepTrackerView() {
+  const [isActive, setActive] = React.useState(false);
+  const data = usePedometer({ isActive });
+  const message = data?.steps ? `Total steps ${data.steps}` : `Waiting...`;
+  return (
+    <View style={{ padding: 10 }}>
+      <H2>Step Tracker</H2>
+      <ListButton onPress={() => setActive(true)} disabled={isActive} title="Start" />
+      <ListButton onPress={() => setActive(false)} disabled={!isActive} title="Stop" />
+      <Text style={{ paddingTop: 10, fontWeight: 'bold' }}>{message}</Text>
+    </View>
+  );
+}
+
+function StepHistoryMessage() {
+  const today = React.useMemo(() => {
+    return new Date();
+  }, []);
+  const yesterday = React.useMemo(() => {
+    const yesterday = new Date();
+    yesterday.setDate(today.getDate() - 1);
+    return yesterday;
+  }, [today]);
+
+  const data = usePedometerHistory({ start: yesterday, end: today });
+
+  const message = data ? `Steps in the last day: ${data.steps}` : `Loading health data...`;
+  return (
+    <View style={{ padding: 10 }}>
+      <H2>Step History</H2>
+      <Text>{message}</Text>
+    </View>
+  );
+}
+
+export default function PedometerScreen() {
+  const [value] = useResolvedValue(Pedometer.isAvailableAsync);
+
+  if (value === null) {
+    return null;
+  } else if (value === false) {
     return (
-      <ScrollView style={{ padding: 10 }}>
-        <ListButton
-          onPress={async () => {
-            const result = await Pedometer.isAvailableAsync();
-            Alert.alert('Pedometer result', `Is available: ${result}`);
-          }}
-          title="Is available"
-        />
-        <ListButton
-          onPress={async () => {
-            const end = new Date();
-            const start = new Date();
-            start.setDate(end.getDate() - 1);
-            const result = await Pedometer.getStepCountAsync(start, end);
-            Alert.alert('Pedometer result', `Number of steps for the last day: ${result.steps}`);
-          }}
-          title="Get steps count"
-        />
-        <ListButton
-          onPress={async () => {
-            this.listener = Pedometer.watchStepCount(data => {
-              this.setState({ stepCount: data.steps });
-            });
-          }}
-          title="Listen for step count updates"
-        />
-        <ListButton
-          onPress={async () => {
-            if (this.listener) {
-              this.listener.remove();
-              this.listener = undefined;
-            }
-          }}
-          title="Stop listening for step count updates"
-        />
-        {this.state.stepCount !== undefined && <Text>Total steps {this.state.stepCount}</Text>}
-      </ScrollView>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Pedometer is not available on this platform</Text>
+      </View>
     );
   }
+  return (
+    <ScrollView>
+      <StepTrackerView />
+      <StepHistoryMessage />
+    </ScrollView>
+  );
 }
+
+PedometerScreen.navigationOptions = {
+  title: 'Pedometer',
+};
