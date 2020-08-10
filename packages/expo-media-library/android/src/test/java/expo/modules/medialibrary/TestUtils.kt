581a7f4777ccf0d6d57bf497192b17953d127974
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2020-08-10 15:34:09 +0200

    [media-library] Unit tests for changes

--- packages/expo-media-library/android/src/test/java/expo/modules/medialibrary/TestUtils.kt
+++ packages/expo-media-library/android/src/test/java/expo/modules/medialibrary/TestUtils.kt
@@ -9,7 +9,7 @@ import org.robolectric.fakes.RoboCursor
 
 typealias CursorResults = Array<Array<out Any?>>
 
-fun mockCursor(cursorResults: CursorResults): Cursor {
+fun mockCursor(cursorResults: CursorResults): RoboCursor {
   val cursor = RoboCursor()
   cursor.setColumnNames(MediaLibraryConstants.ASSET_PROJECTION.toMutableList())
   cursor.setResults(cursorResults)
@@ -20,8 +20,7 @@ fun mockContentResolver(cursor: Cursor?): ContentResolver {
   val contentResolver = mockk<ContentResolver>()
   every { contentResolver.query(
     MediaLibraryConstants.EXTERNAL_CONTENT,
-    MediaLibraryConstants.ASSET_PROJECTION,
-    any(), any(), any())
+    any(), any(), any(), any())
   } returns cursor
   return contentResolver
 }
