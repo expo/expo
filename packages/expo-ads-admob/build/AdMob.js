de565da472abb762df9cae6d225844ae9c81ee2f
Evan Bacon <baconbrix@gmail.com>
2020-08-11 15:22:30 -0700

    Added isAvailableAsync method

--- packages/expo-ads-admob/build/AdMob.js
+++ packages/expo-ads-admob/build/AdMob.js
@@ -1,5 +1,13 @@
 import { UnavailabilityError } from '@unimodules/core';
 import ExpoAdsAdMob from './ExpoAdsAdMob';
+/**
+ * Returns whether the AdMob API is enabled on the current device. This does not check the native manifest.
+ *
+ * @returns Async `boolean`, indicating whether the AdMob API is available on the current device. Currently this resolves `true` on iOS and Android only.
+ */
+export async function isAvailableAsync() {
+    return !!ExpoAdsAdMob.setTestDeviceIDAsync;
+}
 export async function setTestDeviceIDAsync(testDeviceID) {
     if (!ExpoAdsAdMob.setTestDeviceIDAsync) {
         throw new UnavailabilityError('expo-ads-admob', 'setTestDeviceIDAsync');
