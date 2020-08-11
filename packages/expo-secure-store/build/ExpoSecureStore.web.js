373c997ab5baa83089ff2c897808b347d9c33917
Evan Bacon <baconbrix@gmail.com>
2020-08-10 18:52:50 -0700

    Remove unused secure store shim on web

--- packages/expo-secure-store/build/ExpoSecureStore.web.js
+++ packages/expo-secure-store/build/ExpoSecureStore.web.js
@@ -2,8 +2,5 @@ export default {
     get name() {
         return 'ExpoSecureStore';
     },
-    async getValueWithKeyAsync() {
-        return null;
-    },
 };
 //# sourceMappingURL=ExpoSecureStore.web.js.map
