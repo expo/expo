c7f024ace0fee5bc2c16ac728ef10afd4808633b
Cedric van Putten <me@bycedric.com>
2020-08-14 13:19:45 +0200

    [docs] Separate MDX and Next/preval caches

--- docs/next.config.js
+++ docs/next.config.js
@@ -16,9 +16,17 @@ module.exports = withCSS({
   // Rather than use `@zeit/next-mdx`, we replicate it
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   webpack: (config, options) => {
+    // Create a copy of the babel loader, to separate MDX and Next/Preval caches
+    const babelMdxLoader = {
+      ...options.defaultLoaders.babel,
+      options: {
+        ...options.defaultLoaders.babel.options,
+        cacheDirectory: 'node_modules/.cache/babel-mdx-loader',
+      },
+    };
     config.module.rules.push({
       test: /.mdx?$/, // load both .md and .mdx files
-      use: [options.defaultLoaders.babel, '@mdx-js/loader', join(__dirname, './common/md-loader')],
+      use: [babelMdxLoader, '@mdx-js/loader', join(__dirname, './common/md-loader')],
     });
     config.node = {
       fs: 'empty',
