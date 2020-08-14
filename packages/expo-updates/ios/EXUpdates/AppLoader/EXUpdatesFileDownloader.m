7dd53ba801f284b97e39f36064da360f43b2b079
Eric Samelson <eric@expo.io>
2020-08-13 21:45:58 -0700

    [expo-updates] rename variables missed in 79271b192d48a04a95eba21282db42ada5a60532

--- packages/expo-updates/ios/EXUpdates/AppLoader/EXUpdatesFileDownloader.m
+++ packages/expo-updates/ios/EXUpdates/AppLoader/EXUpdatesFileDownloader.m
@@ -202,7 +202,7 @@ NSTimeInterval const EXUpdatesDefaultTimeoutInterval = 60;
   }
 
   if (error) {
-    *error = [NSError errorWithDomain:kEXUpdatesFileDownloaderErrorDomain code:1009 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"No compatible update found at %@. Only %@ are supported.", _config.updateUrl.absoluteString, _config.sdkVersion]}];
+    *error = [NSError errorWithDomain:EXUpdatesFileDownloaderErrorDomain code:1009 userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"No compatible update found at %@. Only %@ are supported.", _config.updateUrl.absoluteString, _config.sdkVersion]}];
   }
   return nil;
 }
