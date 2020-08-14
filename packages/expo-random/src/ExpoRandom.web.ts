53457bea0f931e91f8f065332ed43d9f73a36337
Brent Vatne <brentvatne@gmail.com>
2020-08-14 14:36:06 -0700

    Fix lint error

--- packages/expo-random/src/ExpoRandom.web.ts
+++ packages/expo-random/src/ExpoRandom.web.ts
@@ -10,6 +10,6 @@ export default {
   async getRandomBytesAsync(length: number): Promise<Uint8Array> {
     const array = new Uint8Array(length);
     // @ts-ignore
-    return (window.crypto ?? window.msCrypto).getRandomValues(array)
+    return (window.crypto ?? window.msCrypto).getRandomValues(array);
   },
 };
