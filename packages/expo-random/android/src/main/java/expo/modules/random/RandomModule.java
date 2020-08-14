3f0b06528e0ecf62091085e47e9dd5d6047f7554
Brent Vatne <brentvatne@gmail.com>
2020-08-14 11:44:32 -0700

    Address feedback on RandomModule

--- packages/expo-random/android/src/main/java/expo/modules/random/RandomModule.java
+++ packages/expo-random/android/src/main/java/expo/modules/random/RandomModule.java
@@ -11,6 +11,8 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 public class RandomModule extends ReactContextBaseJavaModule {
+  SecureRandom mSecureRandom;
+
   public RandomModule(ReactApplicationContext context) {
     super(context);
   }
@@ -20,21 +22,20 @@ public class RandomModule extends ReactContextBaseJavaModule {
     return "ExpoRandom";
   }
 
-  private String _getRandomBase64String(int randomByteCount) throws NoSuchAlgorithmException {
-    byte[] output = new byte[randomByteCount];
-    SecureRandom random = new SecureRandom();
-    random.nextBytes(output);
-
-    return Base64.encodeToString(output, Base64.NO_WRAP);
-  }
-
   @ReactMethod
-  public void getRandomBase64StringAsync(int randomByteCount, Promise promise) throws NoSuchAlgorithmException {
-    promise.resolve(_getRandomBase64String(randomByteCount));
+  public void getRandomBase64StringAsync(int randomByteCount, Promise promise) {
+    promise.resolve(getRandomBase64String(randomByteCount));
   }
 
   @ReactMethod(isBlockingSynchronousMethod = true)
-  public String getRandomBase64String(int randomByteCount) throws NoSuchAlgorithmException {
-    return _getRandomBase64String(randomByteCount);
+  public String getRandomBase64String(int randomByteCount) {
+    if (mSecureRandom == null) {
+      mSecureRandom = new SecureRandom();
+    }
+
+    byte[] output = new byte[randomByteCount];
+    mSecureRandom.nextBytes(output);
+
+    return Base64.encodeToString(output, Base64.NO_WRAP);
   }
 }
