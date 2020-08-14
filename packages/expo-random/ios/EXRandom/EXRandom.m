2df74b3e8f5b0ade06820c6fd5ae52c03651a78f
Brent Vatne <brentvatne@gmail.com>
2020-08-14 13:14:40 -0700

    Address feedback from James

--- packages/expo-random/ios/EXRandom/EXRandom.m
+++ packages/expo-random/ios/EXRandom/EXRandom.m
@@ -4,34 +4,47 @@
 
 @implementation EXRandom
 
+static NSString * const EXRandomError = @"ERR_RANDOM";
+static NSString * const EXRandomRandomBytesFailedError = @"Failed to create secure random number";
+
 RCT_EXPORT_MODULE(ExpoRandom);
 
 RCT_EXPORT_METHOD(getRandomBase64StringAsync:(NSUInteger)length
                   resolve:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    @try {
-        resolve([self _getRandomBase64String:length]);
-    }
-    @catch(NSException *e) {
-        reject(e.name, e.reason, nil);
+    NSError *error;
+    NSString *value = [self _getRandomBase64String:length error:&error];
+    if (value != nil) {
+        resolve(value);
+    } else {
+        reject(EXRandomError, EXRandomRandomBytesFailedError, error);
     }
 }
 
 RCT_EXPORT_SYNCHRONOUS_TYPED_METHOD(NSString*, getRandomBase64String:(NSUInteger)length) {
-    return [self _getRandomBase64String:length];
+    NSError *error;
+    NSString *value = [self _getRandomBase64String:length error:&error];
+
+    if (value != nil) {
+        return value;
+    } else {
+        @throw [NSException exceptionWithName:EXRandomError reason:EXRandomRandomBytesFailedError userInfo:@{@"errorCode": @(error.code)}];
+    }
 }
 
 #pragma mark - Internal methods
 
-- (NSString *)_getRandomBase64String:(NSUInteger)length {
+- (NSString *)_getRandomBase64String:(NSUInteger)length
+                              error:(NSError **)error {
     NSMutableData *bytes = [NSMutableData dataWithLength:length];
 
     OSStatus result = SecRandomCopyBytes(kSecRandomDefault, length, [bytes mutableBytes]);
     if (result == errSecSuccess) {
-      return [bytes base64EncodedStringWithOptions:0];
-    } else {
-      @throw([NSException exceptionWithName:@"expo-random" reason:@"Failed to create secure random string" userInfo:nil]);
+        return [bytes base64EncodedStringWithOptions:0];
+    } else if (error) {
+        *error = [NSError errorWithDomain:@"expo-random" code:result userInfo:nil];
     }
+    return nil;
 }
 
 @end
