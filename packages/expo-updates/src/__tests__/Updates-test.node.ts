e5b0db1abd1f4841927a5d4c195314c873dd00ad
Eric Samelson <eric@expo.io>
2020-08-14 15:58:56 -0700

    [expo-updates] fix tests

--- packages/expo-updates/src/__tests__/Updates-test.node.ts
+++ packages/expo-updates/src/__tests__/Updates-test.node.ts
@@ -1,5 +1,12 @@
 import * as Updates from '../Updates';
 
-it(`doesn't throw when reloadAsync is called`, async () => {
+it(`doesn't throw when reloadAsync is called in production`, async () => {
+  const old__DEV__ = __DEV__;
+  //@ts-ignore
+  __DEV__ = false;
+
   await Updates.reloadAsync();
+
+  //@ts-ignore
+  __DEV__ = old__DEV__;
 });
