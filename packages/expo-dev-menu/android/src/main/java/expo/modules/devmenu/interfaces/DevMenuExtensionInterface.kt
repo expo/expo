63e5c9d71ca491618ab1a9dace482c5f2890486a
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-08-20 11:22:25 +0200

    [expo-dev-menu] Add missing resources and improve getting extensions

--- packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/interfaces/DevMenuExtensionInterface.kt
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/interfaces/DevMenuExtensionInterface.kt
@@ -4,6 +4,11 @@ import expo.modules.devmenu.extensions.items.DevMenuItem
 
 interface DevMenuExtensionInterface {
   /**
+   * Returns a name of the module and the extension. Also required by [com.facebook.react.bridge.ReactContextBaseJavaModule].
+   */
+  fun getName(): String
+
+  /**
    * Returns an array of the dev menu items to show.
    * It's called only once for the extension instance — results are being cached on first dev menu launch.
    */
