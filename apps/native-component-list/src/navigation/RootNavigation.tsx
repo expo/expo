06642451e0bc77a5326cdd36903c789cf4b6dde7
Evan Bacon <baconbrix@gmail.com>
2020-08-21 13:00:36 -0700

    [NCL][chore] Created search screen (#9869)
    * Created search screen in NCL
    
    * optimize search for web

--- apps/native-component-list/src/navigation/RootNavigation.tsx
+++ apps/native-component-list/src/navigation/RootNavigation.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { Text } from 'react-native';
 
 import RedirectScreen from '../screens/RedirectScreen';
+import SearchScreen from '../screens/SearchScreen';
 import MainNavigators from './MainNavigators';
 import MainTabNavigator from './MainTabNavigator';
 
@@ -21,6 +22,11 @@ export const linking = {
           components: MainNavigators.components.linking,
         },
       },
+      search: {
+        screens: {
+          search: 'search',
+        },
+      },
     },
   },
 };
@@ -28,9 +34,14 @@ export const linking = {
 export default function RootNavigation() {
   return (
     <NavigationContainer linking={linking} fallback={<Text>Loading…</Text>}>
-      <Switch.Navigator headerMode="none">
+      <Switch.Navigator mode="modal" headerMode="none">
         <Switch.Screen name="main" component={MainTabNavigator} />
         <Switch.Screen name="redirect" component={RedirectScreen} />
+        <Switch.Screen
+          name="search"
+          component={SearchScreen}
+          options={{ cardStyle: { backgroundColor: 'transparent' } }}
+        />
       </Switch.Navigator>
     </NavigationContainer>
   );
--- apps/native-component-list/src/navigation/StackConfig.tsx
+++ apps/native-component-list/src/navigation/StackConfig.tsx
@@ -1,5 +1,7 @@
+import { Ionicons } from '@expo/vector-icons';
 import { HeaderStyleInterpolators } from '@react-navigation/stack';
-import { Platform, StyleSheet } from 'react-native';
+import * as React from 'react';
+import { Platform, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { Colors } from '../constants';
 
@@ -22,7 +24,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const StackConfig = {
+const StackConfig = ({ navigation }) => ({
   cardStyle: styles.card,
   screenOptions: () => ({
     headerStyleInterpolator: HeaderStyleInterpolators.forUIKit,
@@ -30,7 +32,16 @@ const StackConfig = {
     headerTintColor: Colors.tintColor,
     headerTitleStyle: styles.headerTitle,
     headerPressColorAndroid: Colors.tintColor,
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('search')} style={{ marginRight: 16 }}>
+        <Ionicons
+          name="md-search"
+          size={Platform.OS === 'ios' ? 22 : 25}
+          color={Colors.tintColor}
+        />
+      </TouchableOpacity>
+    ),
   }),
-};
+});
 
 export default StackConfig;
