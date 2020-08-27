e79a23a563f220b9454653e70ec048cb01a04d25
Stanisław Chmiela <sjchmiela@gmail.com>
2020-08-27 16:32:17 +0200

    [ncl] Prettify ReanimatedImagePreviewScreen

--- apps/native-component-list/src/screens/Reanimated/ReanimatedImagePreviewScreen.tsx
+++ apps/native-component-list/src/screens/Reanimated/ReanimatedImagePreviewScreen.tsx
@@ -72,10 +72,7 @@ function scaleFriction(value: any, rest: any, delta: any) {
   const MAX_VALUE = 0.5;
   const res = multiply(value, delta);
   const howFar = abs(sub(rest, value));
-  const f = max(
-    1,
-    min(MAX_FRICTION, add(1, multiply(howFar, (MAX_FRICTION - 1) / MAX_VALUE)))
-  );
+  const f = max(1, min(MAX_FRICTION, add(1, multiply(howFar, (MAX_FRICTION - 1) / MAX_VALUE))));
   return cond(lessThan(0, howFar), multiply(value, add(1, divide(add(delta, -1), f))), res);
 }
 
@@ -225,7 +222,7 @@ function bouncy(
 const WIDTH = 300;
 const HEIGHT = 300;
 
-class Viewer extends React.Component<{ source: string }> {
+class Viewer extends React.Component<{ source: number }> {
   pinchRef = React.createRef<PinchGestureHandler>();
   panRef = React.createRef<PanGestureHandler>();
 
@@ -345,16 +342,14 @@ class Viewer extends React.Component<{ source: string }> {
           ref={this.pinchRef}
           simultaneousHandlers={this.panRef}
           onGestureEvent={this._onPinchEvent}
-          onHandlerStateChange={this._onPinchEvent}
-        >
+          onHandlerStateChange={this._onPinchEvent}>
           <Animated.View>
             <PanGestureHandler
               ref={this.panRef}
               avgTouches
               simultaneousHandlers={this.pinchRef}
               onGestureEvent={this._onPanEvent}
-              onHandlerStateChange={this._onPanEvent}
-            >
+              onHandlerStateChange={this._onPanEvent}>
               <Animated.Image
                 style={[
                   styles.image,
