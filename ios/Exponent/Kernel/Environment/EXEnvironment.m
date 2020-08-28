e1520cecaff02883798c67c2fa7eefa1f7e6a38e
Eric Samelson <eric@expo.io>
2020-08-28 16:46:54 -0700

    [android][ios] pass app.json updates config to expo-updates in standalone apps

--- ios/Exponent/Kernel/Environment/EXEnvironment.m
+++ ios/Exponent/Kernel/Environment/EXEnvironment.m
@@ -51,6 +51,8 @@ NSString * const kEXEmbeddedManifestResourceName = @"shell-app-manifest";
   _standaloneManifestUrl = nil;
   _urlScheme = nil;
   _areRemoteUpdatesEnabled = YES;
+  _updatesCheckAutomatically = YES;
+  _updatesFallbackToCacheTimeout = @(0);
   _allManifestUrls = @[];
   _isDebugXCodeScheme = NO;
   _releaseChannel = @"default";
@@ -202,6 +204,13 @@ NSString * const kEXEmbeddedManifestResourceName = @"shell-app-manifest";
   _areRemoteUpdatesEnabled = (shellConfig[@"areRemoteUpdatesEnabled"] == nil)
     ? YES
     : [shellConfig[@"areRemoteUpdatesEnabled"] boolValue];
+  _updatesCheckAutomatically = (shellConfig[@"updatesCheckAutomatically"] == nil)
+    ? YES
+    : [shellConfig[@"updatesCheckAutomatically"] boolValue];
+  _updatesFallbackToCacheTimeout = (shellConfig[@"updatesFallbackToCacheTimeout"] == nil &&
+                                    [shellConfig[@"updatesFallbackToCacheTimeout"] isKindOfClass:[NSNumber class]])
+    ? @(0)
+    : shellConfig[@"updatesFallbackToCacheTimeout"];
   if (infoPlist[@"ExpoReleaseChannel"]) {
     _releaseChannel = infoPlist[@"ExpoReleaseChannel"];
   } else {
