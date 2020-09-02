c258dc1363887a1f7360cd95fce913b127a75928
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-09-02 18:26:13 +0200

    [bare-expo] Reinstall cocoapods after reanimated@2.0.0 removal

--- apps/bare-expo/ios/Pods/Headers/Private/RNReanimated/FrozenObject.h
+++
@@ -1,1 +0,0 @@
-../../../../../../../node_modules/react-native-reanimated/Common/cpp/headers/SharedItems/FrozenObject.h
--- apps/bare-expo/ios/Pods/Headers/Public/RNReanimated/PlatformDepMethodsHolder.h
+++
@@ -1,1 +0,0 @@
-../../../../../../../node_modules/react-native-reanimated/Common/cpp/headers/Tools/PlatformDepMethodsHolder.h
