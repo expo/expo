37a9b3ae998909b202311c704ff1d3f72dacc9a1
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2020-09-02 18:16:23 +0200

    Publish expo-media-library
    expo-media-library@9.2.1

---
+++ apps/bare-expo/ios/Pods/Headers/Private/RNReanimated/NativeReanimatedModule.h
@@ -0,0 +1,1 @@
+../../../../../../../node_modules/react-native-reanimated/Common/cpp/headers/NativeModules/NativeReanimatedModule.h
---
+++ apps/bare-expo/ios/Pods/Headers/Public/RNReanimated/NativeProxy.h
@@ -0,0 +1,1 @@
+../../../../../../../node_modules/react-native-reanimated/ios/native/NativeProxy.h
