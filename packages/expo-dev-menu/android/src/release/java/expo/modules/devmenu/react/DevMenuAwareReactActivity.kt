3977693bbf92707a4e8cf59ace21be9718b74de9
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-09-07 17:31:43 +0200

    [expo-dev-menu][android] Remove from release builds

---
+++ packages/expo-dev-menu/android/src/release/java/expo/modules/devmenu/react/DevMenuAwareReactActivity.kt
@@ -0,0 +1,9 @@
+package expo.modules.devmenu.react
+
+import com.facebook.react.ReactActivity
+
+/**
+ * Basic [ReactActivity] which knows about expo-dev-menu.
+ * It can detect a long press and dispatch key events.
+ */
+abstract class DevMenuAwareReactActivity : ReactActivity()
