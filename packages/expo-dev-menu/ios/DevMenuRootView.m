a2866994263784dd052bff59dd38f81ebcb8f9d8
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-09-07 11:48:49 +0200

    [expo-dev-menu][ios] Prevent registering multiple root views when running bundler

--- packages/expo-dev-menu/ios/DevMenuRootView.m
+++ packages/expo-dev-menu/ios/DevMenuRootView.m
@@ -4,6 +4,7 @@
 
 #import <React/RCTAssert.h>
 #import <React/RCTRootContentView.h>
+#import <React/UIView+React.h>
 
 @implementation DevMenuRootView
 
@@ -14,7 +15,12 @@
   // Use the (batched) bridge that's sent in the notification payload, so the
   // RCTRootContentView is scoped to the right bridge
   RCTBridge *bridge = notification.userInfo[@"bridge"];
-  if (bridge != ((RCTRootContentView *)self.contentView).bridge) {
+  RCTRootContentView *rootView = self.contentView;
+  if (bridge != rootView.bridge) {
+    if (self.reactTag == rootView.reactTag) {
+      // Clear the reactTag so it can be re-assigned
+      self.reactTag = nil; 
+    }
     [super bundleFinishedLoading:bridge];
   }
 }
