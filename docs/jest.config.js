8af1d0427cdf1994a80a10c1821b7150aa90284b
Cedric van Putten <me@bycedric.com>
2020-09-08 01:09:33 +0200

    [docs] Replace babel module resolver with built-in version

--- docs/jest.config.js
+++ docs/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  rootDir: __dirname,
   displayName: 'docs',
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.(js|ts|tsx)'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   clearMocks: true,
   coverageDirectory: '<rootDir>/coverage',
+  moduleNameMapper: {
+    '^~(.*)$': '<rootDir>/$1',
+  },
 };
