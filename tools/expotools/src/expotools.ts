505e5e74eb5573bb6bed5354543e8298a5ace5eb
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-09-08 16:36:35 +0200

    [expotools] Upgrade XDL to include latest iOS shell app changes (#10096)
    - removal of `DevSupport` from iOS shell apps
    - removal of `expo-module-template` from iOS shell apps
    
    Published `@expo/xdl`, upgraded version in `expotools`. Removed code depending on removed code, upgraded `ClientInstall` command.
    
    In a second I will cherry-pick this commit to `sdk-39` and build an iOS shell app there.
    
    I have also verified that `et client-install` uninstalled and reinstalled Expo Client on an Android device connected to my computer.

--- tools/expotools/src/expotools.ts
+++ tools/expotools/src/expotools.ts
@@ -2,7 +2,6 @@
 import * as Directories from './Directories';
 import * as ExpoKit from './ExpoKit';
 import * as FirebaseTestLab from './FirebaseTestLab';
-import * as Fixtures from './Fixtures';
 import * as HashDirectory from './HashDirectory';
 import * as IOSSimulator from './IOSSimulator';
 import * as IOSSimulatorTestSuite from './IOSSimulatorTestSuite';
@@ -10,7 +9,6 @@ import * as Log from './Log';
 import * as Packages from './Packages';
 import * as ProjectVersions from './ProjectVersions';
 import * as S3 from './S3';
-import * as TestServer from './TestServer';
 import * as TestSuite from './TestSuite';
 import * as XDL from './XDL';
 
@@ -18,7 +16,6 @@ export {
   Directories,
   ExpoKit,
   FirebaseTestLab,
-  Fixtures,
   HashDirectory,
   IOSSimulator,
   IOSSimulatorTestSuite,
@@ -26,7 +23,6 @@ export {
   Packages,
   ProjectVersions,
   S3,
-  TestServer,
   TestSuite,
   XDL,
 };
