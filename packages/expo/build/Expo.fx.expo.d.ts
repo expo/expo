c12be014f833a59cce43d78846fb7311d9bd1a3e
cruzach <charliecruzan@gmail.com>
2020-09-08 19:34:48 -0400

    remove console warning

--- packages/expo/build/Expo.fx.expo.d.ts
+++ packages/expo/build/Expo.fx.expo.d.ts
@@ -2,5 +2,4 @@ import './environment/validate.fx';
 import './environment/logging.fx';
 import './environment/LogBox.fx';
 import './environment/react-native-logs.fx';
-import './environment/TurboModulesWarning.fx';
 import 'expo-asset';
