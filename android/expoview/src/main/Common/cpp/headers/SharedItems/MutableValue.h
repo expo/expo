445dd369bf8ff5c9acd08da996a714a0a4d58e2d
Stanisław Chmiela <sjchmiela@gmail.com>
2020-09-09 13:09:32 +0200

    [react-native-reanimated] Upgrade to latest (2.0.0-alpha.6 + some fixes)

--- android/expoview/src/main/Common/cpp/headers/SharedItems/MutableValue.h
+++ android/expoview/src/main/Common/cpp/headers/SharedItems/MutableValue.h
@@ -4,6 +4,7 @@
 #include "MutableValueSetterProxy.h"
 #include <mutex>
 #include <jsi/jsi.h>
+#include <map>
 
 using namespace facebook;
 
@@ -17,7 +18,7 @@ class MutableValue : public jsi::HostObject, public std::enable_shared_from_this
   std::shared_ptr<ShareableValue> value;
   jsi::Value setter;
   jsi::Value animation;
-  std::vector<std::pair<unsigned long, std::function<void()>>> listeners;
+  std::map<unsigned long, std::function<void()>> listeners;
 
   void setValue(jsi::Runtime &rt, const jsi::Value &newValue);
   jsi::Value getValue(jsi::Runtime &rt);
@@ -29,7 +30,7 @@ class MutableValue : public jsi::HostObject, public std::enable_shared_from_this
   void set(jsi::Runtime &rt, const jsi::PropNameID &name, const jsi::Value &value);
   jsi::Value get(jsi::Runtime &rt, const jsi::PropNameID &name);
   std::vector<jsi::PropNameID> getPropertyNames(jsi::Runtime &rt);
-  unsigned long addListener(std::function<void()> listener);
+  unsigned long addListener(unsigned long listenerId, std::function<void()> listener);
   void removeListener(unsigned long listenerId);
 };
 
--- ios/Exponent/Versioned/Core/Api/Reanimated/REAIOSScheduler.mm
+++ ios/Exponent/Versioned/Core/Api/Reanimated/REAIOSScheduler.mm
@@ -6,20 +6,13 @@ using namespace facebook;
 using namespace react;
 
 REAIOSScheduler::REAIOSScheduler(std::shared_ptr<CallInvoker> jsInvoker) {
-  this->jsInvoker = jsInvoker;
+  this->jsCallInvoker_ = jsInvoker;
 }
 
 void REAIOSScheduler::scheduleOnUI(std::function<void()> job) {
   Scheduler::scheduleOnUI(job);
   dispatch_async(dispatch_get_main_queue(), ^{
     triggerUI();
-  });
-}
-
-void REAIOSScheduler::scheduleOnJS(std::function<void()> job) {
-  Scheduler::scheduleOnJS(job);
-  jsInvoker->invokeAsync([this]{
-    triggerJS();
   });
 }
 
