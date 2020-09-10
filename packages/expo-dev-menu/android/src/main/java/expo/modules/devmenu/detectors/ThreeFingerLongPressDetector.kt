ef147105bcf7524bfe9a47f98f707d5839eabbfa
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-09-10 15:15:00 +0200

    [expo-dev-menu][android] Remove from release builds (#10091)
    # Why
    
    We don't want to include `dev-menu` in the release build. 
    
    # Test Plan
    
    - bare-expo ✅

--- packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/detectors/ThreeFingerLongPressDetector.kt
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/detectors/ThreeFingerLongPressDetector.kt
@@ -23,18 +23,9 @@ class ThreeFingerLongPressDetector(val longPressListener: () -> Unit) {
   private var startPosition = Array(3) { MotionEvent.PointerCoords() }
 
   /**
-   * Whether to enable detector.
-   */
-  var isEnabled: Boolean = true
-
-  /**
    * Handles touch event. If it detects long press then [longPressListener] is called.
    */
   fun onTouchEvent(event: MotionEvent?) {
-    if (!isEnabled) {
-      return
-    }
-
     if (!startedDetecting && event?.action == MotionEvent.ACTION_MOVE && event.pointerCount == 3) {
       startedDetecting = true
       startTime = SystemClock.uptimeMillis()
