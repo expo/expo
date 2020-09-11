ea4062ad9b207dea786f90306d24749e7a16a0fb
Will Schurman <wschurman@gmail.com>
2020-09-11 09:05:19 -0700

    [home] Add 2FA for login (#10125)

--- home/api/ApiV2Error.ts
+++ home/api/ApiV2Error.ts
@@ -3,6 +3,7 @@ import ExtendableError from 'es6-error';
 export default class ApiV2Error extends ExtendableError {
   code: string;
   serverStack?: string;
+  metadata?: object;
 
   constructor(message: string, code: string = 'UNKNOWN') {
     super(message);
