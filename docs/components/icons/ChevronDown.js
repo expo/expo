dc44c00ba170701599d6c7a0c73c6e4a1bf846a6
Cedric van Putten <me@bycedric.com>
2020-09-14 21:05:53 +0200

    [docs] Replace anonymous components with named components (#10172)

--- docs/components/icons/ChevronDown.js
+++ docs/components/icons/ChevronDown.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default props => (
+const ChevronDown = props => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     height={props.size}
@@ -16,3 +16,5 @@ export default props => (
     <polyline points="6 9 12 15 18 9" />
   </svg>
 );
+
+export default ChevronDown;
--- docs/components/page-higher-order/withDocumentationElements.js
+++ docs/components/page-higher-order/withDocumentationElements.js
@@ -7,22 +7,24 @@ import DocumentationPage from '~/components/DocumentationPage';
 import { SluggerContext } from '~/components/page-higher-order/withSlugger';
 import * as components from '~/common/translate-markdown';
 
-export default meta =>
-  withRouter(
-    class DocumentationPageHOC extends React.Component {
-      render() {
-        const { router } = this.props;
-        return (
-          <DocumentationPage
-            title={meta.title}
-            url={router}
-            asPath={router.asPath}
-            sourceCodeUrl={meta.sourceCodeUrl}>
-            <SluggerContext.Provider value={new GithubSlugger()}>
-              <MDXProvider components={components}>{this.props.children}</MDXProvider>
-            </SluggerContext.Provider>
-          </DocumentationPage>
-        );
-      }
-    }
-  );
+const withDocumentationElements = (meta) => {
+  const DocumentationElementsHOC = withRouter((props) => {
+    const { router } = props;
+
+    return (
+      <DocumentationPage
+        title={meta.title}
+        url={router}
+        asPath={router.asPath}
+        sourceCodeUrl={meta.sourceCodeUrl}>
+        <SluggerContext.Provider value={new GithubSlugger()}>
+          <MDXProvider components={components}>{props.children}</MDXProvider>
+        </SluggerContext.Provider>
+      </DocumentationPage>
+    )
+  })
+
+  return DocumentationElementsHOC;
+};
+
+export default withDocumentationElements;
