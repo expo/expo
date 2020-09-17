f9b34a3bc23515456c962785a2e9af569c1971eb
Eric Samelson <eric@expo.io>
2020-09-17 12:40:41 -0700

    [templates][bare] update to RN 0.63.2

--- templates/expo-template-bare-minimum/ios/HelloWorld/AppDelegate.m
+++ templates/expo-template-bare-minimum/ios/HelloWorld/AppDelegate.m
@@ -10,7 +10,7 @@
 #import <EXSplashScreen/EXSplashScreenService.h>
 #import <UMCore/UMModuleRegistryProvider.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -40,7 +40,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
   
