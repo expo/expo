3b8ffadc16061912884be8610e07b2e34b0fa2d4
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-09-18 13:03:45 +0200

    [expo-dev-menu] Mock safe area view

--- packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/DevMenuHost.kt
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/DevMenuHost.kt
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.devsupport.DevServerHelper
 import com.facebook.react.shell.MainReactPackage
 import expo.modules.devmenu.react.DevMenuReactInternalSettings
+import expo.modules.devmenu.safearea.MockedSafeAreaPackage
 import java.io.BufferedReader
 import java.io.FileNotFoundException
 import java.io.InputStreamReader
@@ -24,8 +25,7 @@ class DevMenuHost(application: Application) : ReactNativeHost(application) {
       DevMenuPackage(),
       getVendoredPackage("com.swmansion.reanimated.ReanimatedPackage"),
       getVendoredPackage("com.swmansion.gesturehandler.react.RNGestureHandlerPackage"),
-      // todo: remove it
-      getSafeArea() // react-navigation using the safe area.
+      MockedSafeAreaPackage()
     )
   }
 
