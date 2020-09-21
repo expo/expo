63a3d591fc8bbc2c00d2b18a52c2a43d0e5ee0a8
Stanisław Chmiela <sjchmiela@gmail.com>
2020-09-21 14:57:28 +0200

    [expo-notifications] Move installation ID storage from NSUserDefaults to a file

--- packages/expo-notifications/ios/EXNotifications/EXInstallationIdProvider.m
+++ packages/expo-notifications/ios/EXNotifications/EXInstallationIdProvider.m
@@ -2,6 +2,8 @@
 
 #import <EXNotifications/EXInstallationIdProvider.h>
 
+static NSString * const kEXInstallationIdFileName = @"expo-notifications-installation-id.txt";
+static NSUInteger const kEXInstallationIdEncoding = NSUTF8StringEncoding;
 static NSString * const kEXDeviceInstallUUIDKey = @"EXDeviceInstallUUIDKey";
 
 @implementation EXInstallationIdProvider
@@ -15,12 +17,75 @@ UM_EXPORT_METHOD_AS(getInstallationIdAsync, getInstallationIdAsyncWithResolver:(
 
 - (NSString *)getInstallationId
 {
-  NSString *uuid = [[NSUserDefaults standardUserDefaults] stringForKey:kEXDeviceInstallUUIDKey];
-  if (!uuid) {
-    uuid = [[NSUUID UUID] UUIDString];
-    [[NSUserDefaults standardUserDefaults] setObject:uuid forKey:kEXDeviceInstallUUIDKey];
+  NSString *installationId = [self fetchInstallationId];
+  // We have already migrated from NSUserDefaults to local file.
+  if (installationId) {
+    return installationId;
   }
-  return uuid;
+  
+  // Migration section - from NSUserDefaults (legacy storage) to file-based storage
+  // which allows us to exclude the installation ID from device backup.
+  NSString *legacyInstallationId = [self fetchLegacyInstallationId];
+  // If there's a legacy installation ID let's try to move it to new storage.
+  if (legacyInstallationId) {
+    NSError *error = [self persistInstallationId:legacyInstallationId];
+    if (error) {
+      NSLog(@"[expo-notifications] Error encountered while trying to persist an installation ID (%@): %@", legacyInstallationId, error.debugDescription);
+      NSLog(@"[expo-notifications] Bailing out from removing this legacy notification ID from storage.");
+    } else {
+      // If there was no error when persisting legacy installation ID in new storage
+      // we are safe to clear the legacy storage.
+      [self removeLegacyInstallationId];
+    }
+    return legacyInstallationId;
+  }
+  
+  // There's no installation ID available, neither legacy nor current one.
+  installationId = [self generateNewInstallationId];
+  NSError *error = [self persistInstallationId:legacyInstallationId];
+  if (error) {
+    NSLog(@"[expo-notifications] Error encountered while trying to persist an installation ID (%@): %@", installationId, error.debugDescription);
+    NSLog(@"[expo-notifications] Installation ID may change.");
+  }
+  return installationId;
+}
+
+- (NSString *)generateNewInstallationId
+{
+  return [[NSUUID UUID] UUIDString];
+}
+
+# pragma mark - Installation ID storage methods
+
+- (NSString *)fetchInstallationId
+{
+  return [NSString stringWithContentsOfURL:[self installationIdFileUrl] encoding:kEXInstallationIdEncoding error:NULL];
+}
+
+- (NSError *)persistInstallationId:(NSString *)installationId
+{
+  NSError *error = nil;
+  [installationId writeToURL:[self installationIdFileUrl] atomically:YES encoding:kEXInstallationIdEncoding error:&error];
+  return error;
+}
+
+- (NSURL *)installationIdFileUrl
+{
+  NSString *documentDirectory = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+  NSURL *installationIdUrl = [NSURL fileURLWithPath:kEXInstallationIdFileName relativeToURL:[NSURL fileURLWithPath:documentDirectory]];
+  return installationIdUrl;
+}
+
+# pragma mark - Legacy installation ID storage methods
+
+- (NSString *)fetchLegacyInstallationId
+{
+  return [[NSUserDefaults standardUserDefaults] stringForKey:kEXDeviceInstallUUIDKey];
+}
+
+- (void)removeLegacyInstallationId
+{
+  [[NSUserDefaults standardUserDefaults] removeObjectForKey:kEXDeviceInstallUUIDKey];
 }
 
 @end
