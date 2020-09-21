68bbc088198750a074500055c659743218be45e8
Stanisław Chmiela <sjchmiela@gmail.com>
2020-09-21 18:47:23 +0200

    [expo-notifications] Migrate installation ID to a separate SharedPreferences file

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/installationid/InstallationId.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/installationid/InstallationId.java
@@ -6,12 +6,16 @@ import android.content.SharedPreferences;
 import java.util.UUID;
 
 public class InstallationId {
-  private static final String PREFERENCES_KEY = "host.exp.exponent.SharedPreferences";
+  private static final String LEGACY_PREFERENCES_KEY = "host.exp.exponent.SharedPreferences";
+  private static final String PREFERENCES_KEY = "expo.modules.notifications.InstallationId";
   private static final String UUID_KEY = "uuid";
+
   private SharedPreferences mSharedPreferences;
+  private SharedPreferences mLegacySharedPreferences;
 
   public InstallationId(Context context) {
     mSharedPreferences = context.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
+    mLegacySharedPreferences = context.getSharedPreferences(LEGACY_PREFERENCES_KEY, Context.MODE_PRIVATE);
   }
 
   public String getId() {
@@ -22,6 +26,13 @@ public class InstallationId {
     String uuid = mSharedPreferences.getString(UUID_KEY, null);
     if (uuid != null) {
       return uuid;
+    }
+
+    String legacyUuid = mLegacySharedPreferences.getString(UUID_KEY, null);
+    if (legacyUuid != null) {
+      mSharedPreferences.edit().putString(UUID_KEY, legacyUuid).apply();
+      mLegacySharedPreferences.edit().remove(UUID_KEY).apply();
+      return legacyUuid;
     }
 
     uuid = UUID.randomUUID().toString();
