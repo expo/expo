aeb71459b05aa0e3feda4d9946e7677695f19ad9
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-09-22 20:02:05 +0200

    [expo-notifications] Fix requestPermissions on Safari (at least) (#10296)
    # Why
    
    Part of https://github.com/expo/expo/issues/6895.
    
    # How
    
    Noticed <kbd>Request permissions</kbd> button returns `status: undetermined` on Safari even though I granted the permissions. It turned out `Notification.requestPermission` has two signatures: a legacy, deprecated one `requestPermission(callback): void` and a new one `requestPermission(): Promise`. This code should cover both method implementations.
    
    # Test Plan
    
    I have verified that the <kbd>Request permissions</kbd> button works as expected in Safari and Chrome.

--- packages/expo-notifications/src/NotificationPermissionsModule.ts
+++ packages/expo-notifications/src/NotificationPermissionsModule.ts
@@ -48,7 +48,19 @@ async function resolvePermissionAsync({
   if (typeof Notification.requestPermission !== 'undefined') {
     let status = Notification.permission;
     if (shouldAsk) {
-      status = await Notification.requestPermission();
+      status = await new Promise((resolve, reject) => {
+        let resolved = false;
+        function resolveOnce(status: string) {
+          if (!resolved) {
+            resolved = true;
+            resolve(status);
+          }
+        }
+        // Some browsers require a callback argument and some return a Promise
+        Notification.requestPermission(resolveOnce)
+          ?.then(resolveOnce)
+          ?.catch(reject);
+      });
     }
     return convertPermissionStatus(status);
   } else if (typeof navigator !== 'undefined' && navigator?.permissions?.query) {
