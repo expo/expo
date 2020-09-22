c5ace485c05cff309138ed83a7b5cccef24c735b
Brent Vatne <brentvatne@gmail.com>
2020-09-22 09:42:13 -0700

    [expo] Remove redesigned logbox effect (#10273)

--- packages/expo/build/environment/LogBox.fx.expo.js
+++ packages/expo/build/environment/LogBox.fx.expo.js
@@ -1,15 +1,7 @@
 import Constants from 'expo-constants';
-import { YellowBox } from 'react-native';
 if (__DEV__) {
     if (Constants.manifest?.experiments?.redesignedLogBox) {
-        // @ts-ignore: This needs to be run before Expo.fx, and it's not included on external types
-        YellowBox.__unstable_enableLogBox();
-    }
-    else {
-        // Replace the YellowBox.__unstable_enableLogBox function to make discovery easier if
-        // someone is following the React Native 62 blog post.
-        // @ts-ignore
-        YellowBox.__unstable_enableLogBox = () => console.warn('To enable the redesigned LogBox in your app, add "experiments": {"redesignedLogBox": true} to your app.json or app.config.js.');
+        console.warn('LogBox is enabled by default on SDK 39 and higher. You can now remove the experiments.redesignedLogBox from your app configuration to get rid of this warning.');
     }
 }
 //# sourceMappingURL=LogBox.fx.expo.js.map
