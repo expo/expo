411fbb0591117bfd745c6644e662df70c9fb5696
Dan Maas <danmaas@users.noreply.github.com>
2020-09-22 10:27:25 -0700

    Update error message text
    Co-authored-by: Evan Bacon <baconbrix@gmail.com>

--- packages/expo-store-review/android/src/main/java/expo/modules/storereview/StoreReviewModule.kt
+++ packages/expo-store-review/android/src/main/java/expo/modules/storereview/StoreReviewModule.kt
@@ -49,7 +49,7 @@ class StoreReviewModule(private val mContext: Context)
           if (task.isSuccessful) {
             promise.resolve(null)
           } else {
-            promise.reject("ERR_REVIEW_FLOW_FAILED", "The ReviewManager flow was not successful")
+            promise.reject("E_STORE_REVIEW_FAILED", "Android ReviewManager task failed")
           }
         }
       } else {
