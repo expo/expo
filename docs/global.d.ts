f711de17d8cd1040cbbe0e389be8af9b09a700a6
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2020-09-23 13:44:11 +0200

    Tape patch window globals

---
+++ docs/global.d.ts
@@ -0,0 +1,15 @@
+interface Window {
+  __NEXT_DATA__: any;
+  NProgress: any;
+  ExpoSnack: any;
+  opera: any;
+
+  __sidebarScroll: number;
+  sidebarState: Record<string, boolean>;
+}
+
+declare module NodeJS {
+  interface Global {
+    tippy: any;
+  }
+}
