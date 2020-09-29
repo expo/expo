abe2ed81d8c35162ea9c376e3c064aac8798c96a
Stanisław Chmiela <sjchmiela@gmail.com>
2020-09-29 21:33:22 +0200

    [expo-notifications] Implement getNextTriggerDateAsync TS method

---
+++ packages/expo-notifications/src/getNextTriggerDateAsync.ts
@@ -0,0 +1,15 @@
+import { UnavailabilityError } from '@unimodules/core';
+
+import NotificationScheduler from './NotificationScheduler';
+import { NotificationTriggerInput } from './Notifications.types';
+import { parseTrigger } from './scheduleNotificationAsync';
+
+export default async function getNextTriggerDateAsync(
+  trigger: NotificationTriggerInput
+): Promise<number> {
+  if (!NotificationScheduler.getNextTriggerDateAsync) {
+    throw new UnavailabilityError('ExpoNotifications', 'getNextTriggerDateAsync');
+  }
+
+  return await NotificationScheduler.getNextTriggerDateAsync(parseTrigger(trigger));
+}
