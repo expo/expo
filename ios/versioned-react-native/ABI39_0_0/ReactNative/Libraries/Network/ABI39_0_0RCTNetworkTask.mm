829acd688d99334d68090ecc83757c000a606e99
Brent Vatne <brentvatne@gmail.com>
2020-09-30 14:30:00 -0700

    [ios] iOS blob / xhr fix a31165a7

--- ios/versioned-react-native/ABI39_0_0/ReactNative/Libraries/Network/ABI39_0_0RCTNetworkTask.mm
+++ ios/versioned-react-native/ABI39_0_0/ReactNative/Libraries/Network/ABI39_0_0RCTNetworkTask.mm
@@ -99,12 +99,6 @@ ABI39_0_0RCT_NOT_IMPLEMENTED(- (instancetype)init)
 {
   BOOL valid = YES;
   if (_requestToken == nil) {
-    if (requestToken == nil) {
-      if (ABI39_0_0RCT_DEBUG) {
-        ABI39_0_0RCTLogError(@"Missing request token for request: %@", _request);
-      }
-      valid = NO;
-    }
     _requestToken = requestToken;
   } else if (![requestToken isEqual:_requestToken]) {
     if (ABI39_0_0RCT_DEBUG) {
