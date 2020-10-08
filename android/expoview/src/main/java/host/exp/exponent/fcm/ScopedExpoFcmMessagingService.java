14aed69d36950702c0da58da3c66f6626465447e
cruzach <charliecruzan@gmail.com>
2020-10-08 11:13:00 -0400

    optimize imports

--- android/expoview/src/main/java/host/exp/exponent/fcm/ScopedExpoFcmMessagingService.java
+++ android/expoview/src/main/java/host/exp/exponent/fcm/ScopedExpoFcmMessagingService.java
@@ -1,24 +1,14 @@
 package host.exp.exponent.fcm;
 
-import com.google.firebase.messaging.RemoteMessage;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.Map;
 
-import expo.modules.notifications.FirebaseListenerService;
+import androidx.annotation.NonNull;
+
 import expo.modules.notifications.notifications.model.NotificationContent;
 import expo.modules.notifications.notifications.model.NotificationRequest;
 import expo.modules.notifications.notifications.model.triggers.FirebaseNotificationTrigger;
-import host.exp.exponent.ABIVersion;
-import host.exp.exponent.Constants;
-import host.exp.exponent.analytics.EXL;
 import host.exp.exponent.kernel.ExperienceId;
-import host.exp.exponent.notifications.PushNotificationHelper;
 import host.exp.exponent.notifications.model.ScopedNotificationRequest;
-import host.exp.exponent.storage.ExperienceDBObject;
-import host.exp.exponent.storage.ExponentDB;
 
 
 public class ScopedExpoFcmMessagingService extends ExpoFcmMessagingService {
