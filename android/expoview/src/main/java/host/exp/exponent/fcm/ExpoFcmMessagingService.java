80eb596c270fba53ca19941011d656da6a6f5caf
cruzach <charliecruzan@gmail.com>
2020-10-09 17:31:11 -0400

    revert changes to messaging service

--- android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
+++ android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
@@ -5,12 +5,19 @@ import com.google.firebase.messaging.RemoteMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Map;
+
 import androidx.annotation.NonNull;
 import expo.modules.notifications.FirebaseListenerService;
+import expo.modules.notifications.notifications.model.NotificationContent;
+import expo.modules.notifications.notifications.model.NotificationRequest;
+import expo.modules.notifications.notifications.model.triggers.FirebaseNotificationTrigger;
 import host.exp.exponent.ABIVersion;
 import host.exp.exponent.Constants;
 import host.exp.exponent.analytics.EXL;
+import host.exp.exponent.kernel.ExperienceId;
 import host.exp.exponent.notifications.PushNotificationHelper;
+import host.exp.exponent.notifications.model.ScopedNotificationRequest;
 import host.exp.exponent.storage.ExperienceDBObject;
 import host.exp.exponent.storage.ExponentDB;
 
@@ -73,5 +80,19 @@ public class ExpoFcmMessagingService extends FirebaseListenerService {
 
   private void dispatchToLegacyNotificationModule(RemoteMessage remoteMessage) {
     PushNotificationHelper.getInstance().onMessageReceived(this, remoteMessage.getData().get("experienceId"), remoteMessage.getData().get("channelId"), remoteMessage.getData().get("message"), remoteMessage.getData().get("body"), remoteMessage.getData().get("title"), remoteMessage.getData().get("categoryId"));
+  }
+
+  @NonNull
+  @Override
+  protected NotificationRequest createNotificationRequest(@NonNull String identifier, @NonNull NotificationContent content, FirebaseNotificationTrigger notificationTrigger) {
+    ExperienceId experienceId;
+    Map<String, String> data = notificationTrigger.getRemoteMessage().getData();
+    if (!data.containsKey("experienceId")) {
+      experienceId = null;
+    } else {
+      experienceId = ExperienceId.create(data.get("experienceId"));
+    }
+    String experienceIdString = experienceId == null ? null : experienceId.get();
+    return new ScopedNotificationRequest(identifier, content, notificationTrigger, experienceIdString);
   }
 }
