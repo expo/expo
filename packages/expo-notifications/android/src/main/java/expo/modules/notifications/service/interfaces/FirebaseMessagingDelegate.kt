babd0c1836eaac93243042d63e51bf0f15cbf618
Stanisław Chmiela <sjchmiela@gmail.com>
2020-10-12 11:21:31 +0200

    [expo-notifications] Add description for FirebaseMessagingDelegate interface

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/service/interfaces/FirebaseMessagingDelegate.kt
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/service/interfaces/FirebaseMessagingDelegate.kt
@@ -3,6 +3,9 @@ package expo.modules.notifications.service.interfaces
 import com.google.firebase.messaging.RemoteMessage
 import expo.modules.notifications.service.NotificationsService
 
+/**
+ * A delegate to [NotificationsService] responsible for handling Firebase events.
+ */
 interface FirebaseMessagingDelegate {
   /**
    * Called on new token, dispatches it to [NotificationsService.sTokenListenersReferences].
