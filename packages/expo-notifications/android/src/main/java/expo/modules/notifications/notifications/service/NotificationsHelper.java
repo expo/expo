14f93c86dccaa1fa6ee9f53b2d9986a34207e074
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2020-10-13 16:56:07 +0200

    [expo-notifications] Extract notification presentation implementation to service delegate (#10645)
    # Why
    
    A prerequisite for streamlined single-threaded notifications handling.
    
    # How
    
    Moved `NotificationsHelper` methods implementation to `ExpoPresentationDelegate` which I added as another delegate/component of `NotificationsService`. Then not to do a too-big refactor I replaced implementation of `NotificationsHelper` methods with calls to `NotificationsService.enqueue…`. `NotificationsHelper` and will be removed altogether in a separate pull request as it will 
    
    # Test Plan
    
    I have confirmed manually that notifications are still presented, fetched and dismissed correctly.

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/service/NotificationsHelper.java
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/service/NotificationsHelper.java
@@ -1,47 +1,28 @@
 package expo.modules.notifications.notifications.service;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.os.ResultReceiver;
-import android.service.notification.StatusBarNotification;
 import android.util.Log;
-import android.util.Pair;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Date;
 import java.util.Iterator;
-import java.util.Objects;
 import java.util.WeakHashMap;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ProcessLifecycleOwner;
 import expo.modules.notifications.notifications.NotificationManager;
-import expo.modules.notifications.notifications.enums.NotificationPriority;
 import expo.modules.notifications.notifications.interfaces.NotificationsReconstructor;
-import expo.modules.notifications.notifications.interfaces.NotificationsScoper;
 import expo.modules.notifications.notifications.model.Notification;
 import expo.modules.notifications.notifications.model.NotificationBehavior;
 import expo.modules.notifications.notifications.model.NotificationCategory;
-import expo.modules.notifications.notifications.model.NotificationContent;
-import expo.modules.notifications.notifications.model.NotificationRequest;
 import expo.modules.notifications.notifications.model.NotificationResponse;
-import expo.modules.notifications.notifications.presentation.builders.ExpoNotificationBuilder;
-
-import static android.content.Context.NOTIFICATION_SERVICE;
+import expo.modules.notifications.service.NotificationsService;
 
 /**
  * A notification service foundation handling incoming intents
@@ -57,17 +38,9 @@ public class NotificationsHelper {
   public static final String NOTIFICATIONS_KEY = "notifications";
   public static final String CATEGORIES_KEY = "categories";
 
-  public static final String INTERNAL_IDENTIFIER_SCHEME = "expo-notifications";
-  public static final String INTERNAL_IDENTIFIER_AUTHORITY = "foreign_notifications";
-  public static final String INTERNAL_IDENTIFIER_TAG_KEY = "tag";
-  public static final String INTERNAL_IDENTIFIER_ID_KEY = "id";
-
-  protected static final int ANDROID_NOTIFICATION_ID = 0;
-
-  private Context mContext;
-  private NotificationsReconstructor mNotificationsReconstructor;
   private SharedPreferencesNotificationCategoriesStore mStore;
   private NotificationsHelperLifecycleObserver mLifecycleObserver;
+  private Context mContext;
 
   /**
    * A weak map of listeners -> reference. Used to check quickly whether given listener
@@ -79,7 +52,6 @@ public class NotificationsHelper {
 
   public NotificationsHelper(Context context, NotificationsReconstructor notificationsReconstructor) {
     this.mContext = context.getApplicationContext();
-    this.mNotificationsReconstructor = notificationsReconstructor;
     mStore = new SharedPreferencesNotificationCategoriesStore(context);
 
     // Note we're not removing the observer anywhere because NotificationsHelper
@@ -131,9 +103,7 @@ public class NotificationsHelper {
    * @param receiver A receiver to which send the notifications
    */
   public void getAllPresented(@Nullable ResultReceiver receiver) {
-    Bundle bundle = new Bundle();
-    bundle.putParcelableArrayList(NOTIFICATIONS_KEY, new ArrayList<>(getDisplayedNotifications()));
-    notifyReceiverSuccess(receiver, bundle);
+    NotificationsService.Companion.enqueueGetAllPresented(mContext, receiver);
   }
 
   /**
@@ -144,9 +114,7 @@ public class NotificationsHelper {
    * @param receiver     A receiver to which send the result of presenting the notification
    */
   public void presentNotification(@NonNull Notification notification, @Nullable NotificationBehavior behavior, @Nullable ResultReceiver receiver) {
-    String tag = notification.getNotificationRequest().getIdentifier();
-    NotificationManagerCompat.from(mContext).notify(tag, ANDROID_NOTIFICATION_ID, getNotification(mContext, notification, behavior));
-    notifyReceiverSuccess(receiver, null);
+    NotificationsService.Companion.enqueuePresent(mContext, notification, behavior, receiver);
   }
 
   /**
@@ -169,10 +137,11 @@ public class NotificationsHelper {
       for (NotificationManager listener : getListeners()) {
         listener.onNotificationReceived(notification);
       }
+      notifyReceiverSuccess(receiver, null);
     } else {
-      presentNotification(notification, null, null);
+      // Receiver is notified by NotificationsService
+      NotificationsService.Companion.enqueuePresent(mContext, notification, null, receiver);
     }
-    notifyReceiverSuccess(receiver, null);
   }
 
   /**
@@ -181,8 +150,7 @@ public class NotificationsHelper {
    * @param identifier Notification identifier
    */
   public void dismiss(@NonNull String identifier, @Nullable ResultReceiver receiver) {
-    dismiss(identifier);
-    notifyReceiverSuccess(receiver, null);
+    NotificationsService.Companion.enqueueDismiss(mContext, new String[]{identifier}, receiver);
   }
 
   /**
@@ -191,29 +159,14 @@ public class NotificationsHelper {
    * @param identifiers Notification identifiers
    */
   public void enqueueDismissSelected(@NonNull String[] identifiers, @Nullable ResultReceiver receiver) {
-    for (String identifier : identifiers) {
-      dismiss(identifier);
-    }
-    notifyReceiverSuccess(receiver, null);
-  }
-
-  private void dismiss(@NonNull String identifier) {
-    Pair<String, Integer> foreignNotification = parseNotificationIdentifier(identifier);
-    if (foreignNotification != null) {
-      // Foreign notification identified by us
-      NotificationManagerCompat.from(mContext).cancel(foreignNotification.first, foreignNotification.second);
-    } else {
-      // Let's hope it's our notification, we have no reason to believe otherwise
-      NotificationManagerCompat.from(mContext).cancel(identifier, ANDROID_NOTIFICATION_ID);
-    }
+    NotificationsService.Companion.enqueueDismiss(mContext, identifiers, receiver);
   }
 
   /**
    * A helper function for dispatching all notification
    */
   public void dismissAll(@Nullable ResultReceiver receiver) {
-    NotificationManagerCompat.from(mContext).cancelAll();
-    notifyReceiverSuccess(receiver, null);
+    NotificationsService.Companion.enqueueDismissAll(mContext, receiver);
   }
 
   /**
@@ -248,38 +201,6 @@ public class NotificationsHelper {
     }
   }
 
-  /**
-   * Callback called to fetch a collection of currently displayed notifications.
-   *
-   * <b>Note:</b> This feature is only supported on Android 23+.
-   *
-   * @return A collection of currently displayed notifications.
-   */
-  protected Collection<Notification> getDisplayedNotifications() {
-    // getActiveNotifications() is not supported on platforms below Android 23
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-      return Collections.emptyList();
-    }
-
-    android.app.NotificationManager manager = (android.app.NotificationManager) mContext.getSystemService(NOTIFICATION_SERVICE);
-    Collection<Notification> notifications = new ArrayList<>();
-    StatusBarNotification[] activeNotifications = manager.getActiveNotifications();
-    for (StatusBarNotification statusBarNotification : activeNotifications) {
-      Notification notification = getNotification(statusBarNotification);
-      if (notification != null) {
-        notifications.add(notification);
-      }
-    }
-    return notifications;
-  }
-
-  protected android.app.Notification getNotification(Context context, Notification notification, NotificationBehavior behavior) {
-    return NotificationsScoper.create(context).createBuilderCreator().get(context, mStore)
-      .setNotification(notification)
-      .setAllowedBehavior(behavior)
-      .build();
-  }
-
   public Collection<NotificationCategory> getCategories() {
     return mStore.getAllNotificationCategories();
   }
@@ -298,65 +219,6 @@ public class NotificationsHelper {
     return mStore.removeNotificationCategory(identifier);
   }
 
-  @Nullable
-  protected Notification getNotification(StatusBarNotification statusBarNotification) {
-    android.app.Notification notification = statusBarNotification.getNotification();
-    byte[] notificationRequestByteArray = notification.extras.getByteArray(ExpoNotificationBuilder.EXTRAS_MARSHALLED_NOTIFICATION_REQUEST_KEY);
-    if (notificationRequestByteArray != null) {
-      try {
-        Parcel parcel = Parcel.obtain();
-        parcel.unmarshall(notificationRequestByteArray, 0, notificationRequestByteArray.length);
-        parcel.setDataPosition(0);
-        NotificationRequest request = mNotificationsReconstructor.reconstructNotificationRequest(parcel);
-        parcel.recycle();
-        Date notificationDate = new Date(statusBarNotification.getPostTime());
-        return new Notification(request, notificationDate);
-      } catch (Exception e) {
-        // Let's catch all the exceptions -- there's nothing we can do here
-        // and we'd rather return an array without a single, invalid notification
-        // than throw an exception and return none.
-        @SuppressLint("DefaultLocale")
-        String message = String.format("Could not have unmarshalled NotificationRequest from (%s, %d).", statusBarNotification.getTag(), statusBarNotification.getId());
-        Log.e("expo-notifications", message);
-      }
-    } else {
-      // It's not our notification. Let's do what we can.
-      NotificationContent content = new NotificationContent.Builder()
-        .setTitle(notification.extras.getString(android.app.Notification.EXTRA_TITLE))
-        .setText(notification.extras.getString(android.app.Notification.EXTRA_TEXT))
-        .setSubtitle(notification.extras.getString(android.app.Notification.EXTRA_SUB_TEXT))
-        // using deprecated field
-        .setPriority(NotificationPriority.fromNativeValue(notification.priority))
-        // using deprecated field
-        .setVibrationPattern(notification.vibrate)
-        // using deprecated field
-        .setSound(notification.sound)
-        .setAutoDismiss((notification.flags & android.app.Notification.FLAG_AUTO_CANCEL) != 0)
-        .setSticky((notification.flags & android.app.Notification.FLAG_ONGOING_EVENT) != 0)
-        .setBody(fromBundle(notification.extras))
-        .build();
-      NotificationRequest request = new NotificationRequest(getInternalIdentifierKey(statusBarNotification), content, null);
-      return new Notification(request, new Date(statusBarNotification.getPostTime()));
-    }
-    return null;
-  }
-
-  /**
-   * Creates an identifier for given {@link StatusBarNotification}. It's supposed to be parsable
-   * by {@link NotificationsHelper#parseNotificationIdentifier(String)}.
-   *
-   * @param notification Notification to be identified
-   * @return String identifier
-   */
-  protected String getInternalIdentifierKey(StatusBarNotification notification) {
-    Uri.Builder builder = Uri.parse(INTERNAL_IDENTIFIER_SCHEME + "://" + INTERNAL_IDENTIFIER_AUTHORITY).buildUpon();
-    if (notification.getTag() != null) {
-      builder.appendQueryParameter(INTERNAL_IDENTIFIER_TAG_KEY, notification.getTag());
-    }
-    builder.appendQueryParameter(INTERNAL_IDENTIFIER_ID_KEY, Integer.toString(notification.getId()));
-    return builder.toString();
-  }
-
   private Collection<NotificationManager> getListeners() {
     Collection<NotificationManager> listeners = new ArrayList<>();
     for (WeakReference<NotificationManager> reference : sListenersReferences.values()) {
@@ -367,39 +229,4 @@ public class NotificationsHelper {
     }
     return listeners;
   }
-
-  /**
-   * Tries to parse given identifier as an internal foreign notification identifier
-   * created by us in {@link NotificationsHelper#getInternalIdentifierKey(StatusBarNotification)}.
-   *
-   * @param identifier String identifier of the notification
-   * @return Pair of (notification tag, notification id), if the identifier could be parsed. null otherwise.
-   */
-  public static Pair<String, Integer> parseNotificationIdentifier(String identifier) {
-    Uri parsedIdentifier = Uri.parse(identifier);
-    try {
-      if (INTERNAL_IDENTIFIER_SCHEME.equals(parsedIdentifier.getScheme()) && INTERNAL_IDENTIFIER_AUTHORITY.equals(parsedIdentifier.getAuthority())) {
-        String tag = parsedIdentifier.getQueryParameter(INTERNAL_IDENTIFIER_TAG_KEY);
-        int id = Integer.parseInt(Objects.requireNonNull(parsedIdentifier.getQueryParameter(INTERNAL_IDENTIFIER_ID_KEY)));
-        return new Pair<>(tag, id);
-      }
-    } catch (NullPointerException | NumberFormatException | UnsupportedOperationException e) {
-      Log.e("expo-notifications", "Malformed foreign notification identifier: " + identifier, e);
-    }
-    return null;
-  }
-
-  protected JSONObject fromBundle(Bundle bundle) {
-    JSONObject json = new JSONObject();
-    for (String key : bundle.keySet()) {
-      try {
-        json.put(key, JSONObject.wrap(bundle.get(key)));
-      } catch (JSONException e) {
-        // can't do anything about it apart from logging it
-        Log.d("expo-notifications", "Error encountered while serializing Android notification extras: " + key + " -> " + bundle.get(key), e);
-      }
-    }
-    return json;
-  }
-
 }
--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/service/NotificationsService.kt
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/service/NotificationsService.kt
@@ -1,16 +1,190 @@
 package expo.modules.notifications.service
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.ResultReceiver
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import expo.modules.notifications.notifications.model.Notification
+import expo.modules.notifications.notifications.model.NotificationBehavior
+import expo.modules.notifications.service.delegates.ExpoPresentationDelegate
 import expo.modules.notifications.service.interfaces.FirebaseMessagingDelegate
+import expo.modules.notifications.service.interfaces.PresentationDelegate
 
 /**
  * Subclass of FirebaseMessagingService, central dispatcher for all the notifications-related actions.
  */
 open class NotificationsService : FirebaseMessagingService() {
+  companion object {
+    const val NOTIFICATION_EVENT_ACTION = "expo.modules.notifications.NOTIFICATION_EVENT"
+
+    // Event types
+    private const val GET_ALL_DISPLAYED_TYPE = "getAllDisplayed"
+    private const val PRESENT_TYPE = "present"
+    private const val DISMISS_SELECTED_TYPE = "dismissSelected"
+    private const val DISMISS_ALL_TYPE = "dismissAll"
+
+    // Messages parts
+    const val SUCCESS_CODE = 0
+    const val ERROR_CODE = 1
+    const val EVENT_TYPE_KEY = "type"
+    const val EXCEPTION_KEY = "exception"
+    const val RECEIVER_KEY = "receiver"
+
+    // Specific messages parts
+    const val NOTIFICATION_KEY = "notification"
+    const val IDENTIFIERS_KEY = "identifiers"
+    const val NOTIFICATION_BEHAVIOR_KEY = "notificationBehavior"
+    const val NOTIFICATIONS_KEY = "notifications"
+
+    /**
+     * A helper function for dispatching a "fetch all displayed notifications" command to the service.
+     *
+     * @param context  Context where to start the service.
+     * @param receiver A receiver to which send the notifications
+     */
+    fun enqueueGetAllPresented(context: Context, receiver: ResultReceiver? = null) {
+      enqueueWork(context, Intent(NOTIFICATION_EVENT_ACTION, getUriBuilder().build()).also {
+        it.putExtra(EVENT_TYPE_KEY, GET_ALL_DISPLAYED_TYPE)
+        it.putExtra(RECEIVER_KEY, receiver)
+      })
+    }
+
+    /**
+     * A helper function for dispatching a "present notification" command to the service.
+     *
+     * @param context      Context where to start the service.
+     * @param notification Notification to present
+     * @param behavior     Allowed notification behavior
+     * @param receiver     A receiver to which send the result of presenting the notification
+     */
+    fun enqueuePresent(context: Context, notification: Notification, behavior: NotificationBehavior? = null, receiver: ResultReceiver? = null) {
+      val data = getUriBuilderForIdentifier(notification.notificationRequest.identifier).appendPath("present").build()
+      enqueueWork(context, Intent(NOTIFICATION_EVENT_ACTION, data).also { intent ->
+        intent.putExtra(EVENT_TYPE_KEY, PRESENT_TYPE)
+        intent.putExtra(NOTIFICATION_KEY, notification)
+        intent.putExtra(NOTIFICATION_BEHAVIOR_KEY, behavior)
+        intent.putExtra(RECEIVER_KEY, receiver)
+      })
+    }
+
+    /**
+     * A helper function for dispatching a "dismiss notification" command to the service.
+     *
+     * @param context    Context where to start the service.
+     * @param identifier Notification identifier
+     * @param receiver   A receiver to which send the result of the action
+     */
+    fun enqueueDismiss(context: Context, identifiers: Array<String>, receiver: ResultReceiver? = null) {
+      val data = getUriBuilder().appendPath("dismiss").build()
+      enqueueWork(context, Intent(NOTIFICATION_EVENT_ACTION, data).also { intent ->
+        intent.putExtra(EVENT_TYPE_KEY, DISMISS_SELECTED_TYPE)
+        intent.putExtra(IDENTIFIERS_KEY, identifiers)
+        intent.putExtra(RECEIVER_KEY, receiver)
+      })
+    }
+
+    /**
+     * A helper function for dispatching a "dismiss notification" command to the service.
+     *
+     * @param context    Context where to start the service.
+     * @param receiver   A receiver to which send the result of the action
+     */
+    fun enqueueDismissAll(context: Context, receiver: ResultReceiver? = null) {
+      val data = getUriBuilder().appendPath("dismiss").build()
+      enqueueWork(context, Intent(NOTIFICATION_EVENT_ACTION, data).also { intent ->
+        intent.putExtra(EVENT_TYPE_KEY, DISMISS_ALL_TYPE)
+        intent.putExtra(RECEIVER_KEY, receiver)
+      })
+    }
+
+    /**
+     * Sends the intent to the best service to handle the {@link #NOTIFICATION_EVENT_ACTION} intent.
+     *
+     * @param context Context where to start the service
+     * @param intent  Intent to dispatch
+     */
+    fun enqueueWork(context: Context, intent: Intent) {
+      val searchIntent = Intent(intent.action).setPackage(context.packageName)
+      context.packageManager.resolveService(searchIntent, 0)?.serviceInfo?.let {
+        intent.component = ComponentName(it.packageName, it.name)
+        context.startService(intent)
+        return
+      }
+      Log.e("expo-notifications", "No service capable of handling notifications found (intent = ${intent.action}). Ensure that you have configured your AndroidManifest.xml properly.")
+    }
+
+    protected fun getUriBuilder(): Uri.Builder {
+      return Uri.parse("expo-notifications://notifications/").buildUpon()
+    }
+
+    protected fun getUriBuilderForIdentifier(identifier: String): Uri.Builder {
+      return getUriBuilder().appendPath(identifier)
+    }
+  }
+
   protected open val firebaseMessagingDelegate: FirebaseMessagingDelegate by lazy {
     expo.modules.notifications.service.delegates.FirebaseMessagingDelegate(this)
   }
+  protected open val presentationDelegate: PresentationDelegate by lazy {
+    ExpoPresentationDelegate(this)
+  }
+
+  override fun getStartCommandIntent(intent: Intent?): Intent {
+    if (intent?.action === NOTIFICATION_EVENT_ACTION) {
+      return intent
+    }
+    return super.getStartCommandIntent(intent)
+  }
+
+  override fun handleIntent(intent: Intent?) {
+    if (intent?.action === NOTIFICATION_EVENT_ACTION) {
+      val receiver: ResultReceiver? = intent.extras?.get(RECEIVER_KEY) as? ResultReceiver
+      try {
+        var resultData: Bundle? = null
+        when (val eventType = intent.getStringExtra(EVENT_TYPE_KEY)) {
+          GET_ALL_DISPLAYED_TYPE -> {
+            resultData = Bundle().also {
+              it.putParcelableArrayList(NOTIFICATIONS_KEY, ArrayList(onGetAllPresentedNotifications()))
+            }
+          }
+
+          PRESENT_TYPE -> onPresentNotification(
+            intent.extras?.getParcelable(NOTIFICATION_KEY)!!, // throw exception if empty
+            intent.extras?.getParcelable(NOTIFICATION_BEHAVIOR_KEY)
+          )
+
+          DISMISS_SELECTED_TYPE -> onDismissNotifications(
+            intent.extras?.getStringArray(IDENTIFIERS_KEY)!!.asList() // throw exception if empty
+          )
+
+          DISMISS_ALL_TYPE -> onDismissAllNotifications()
+
+          else -> throw IllegalArgumentException("Received event of unrecognized type: $eventType. Ignoring.")
+        }
+
+        // If we ended up here, the callbacks must have completed successfully
+        receiver?.send(SUCCESS_CODE, resultData)
+      } catch (e: Exception) {
+        Log.e("expo-notifications", "Action ${intent.action} failed: ${e.message}")
+        e.printStackTrace()
+
+        receiver?.send(ERROR_CODE, Bundle().also { it.putSerializable(EXCEPTION_KEY, e) })
+      }
+    } else {
+      super.handleIntent(intent)
+    }
+  }
+
+  open fun onPresentNotification(notification: Notification, behavior: NotificationBehavior?) = presentationDelegate.presentNotification(notification, behavior)
+  open fun onGetAllPresentedNotifications() = presentationDelegate.getAllPresentedNotifications()
+  open fun onDismissNotifications(identifiers: Collection<String>) = presentationDelegate.dismissNotifications(identifiers)
+  open fun onDismissAllNotifications() = presentationDelegate.dismissAllNotifications()
+
   override fun onMessageReceived(remoteMessage: RemoteMessage) = firebaseMessagingDelegate.onMessageReceived(remoteMessage)
   override fun onNewToken(token: String) = firebaseMessagingDelegate.onNewToken(token)
   override fun onDeletedMessages() = firebaseMessagingDelegate.onDeletedMessages()
