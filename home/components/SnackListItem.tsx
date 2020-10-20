0298361c09401b9d0dc6cbbf696e3c6c9d91ae0c
Hein Rutjes <hrutjes@gmail.com>
2020-10-20 15:56:32 +0200

    [home] Display Snack draft status (#10691)
    * [home] Display Snack draft status
    
    * [home] Add Badge component

--- home/components/SnackListItem.tsx
+++ home/components/SnackListItem.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
-import { Linking, Share } from 'react-native';
+import { Linking, Share, View, StyleSheet } from 'react-native';
 
 import * as UrlUtils from '../utils/UrlUtils';
+import Badge from './Badge';
 import ListItem from './ListItem';
 
 type Props = React.ComponentProps<typeof ListItem> & {
   url: string;
+  isDraft?: boolean;
 };
 
 function normalizeDescription(description?: string): string | undefined {
   return !description || description === 'No description' ? undefined : description;
 }
 
-function SnackListItem({ url, subtitle, ...props }: Props) {
+function SnackListItem({ url, subtitle, isDraft, ...props }: Props) {
   const handlePressProject = () => {
     Linking.openURL(UrlUtils.normalizeUrl(url));
   };
@@ -29,11 +31,27 @@ function SnackListItem({ url, subtitle, ...props }: Props) {
   return (
     <ListItem
       subtitle={normalizeDescription(subtitle)}
+      rightContent={
+        isDraft && (
+          <View style={styles.rightContentContainer}>
+            <Badge text="Draft" />
+          </View>
+        )
+      }
       onPress={handlePressProject}
       onLongPress={handleLongPressProject}
       {...props}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  rightContentContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginEnd: 10,
+    marginStart: 5,
+  },
+});
 
 export default SnackListItem;
