af4eef6f031a875562156810d5ce0d163a34bc2e
Stanisław Chmiela <sjchmiela@gmail.com>
2020-10-23 21:53:30 +0200

    [expoview] Add ExpoNotificationsService overriding NotificationsService

---
+++ android/expoview/src/main/java/host/exp/exponent/notifications/ExpoNotificationsService.kt
@@ -0,0 +1,6 @@
+package host.exp.exponent.notifications
+
+import android.content.Context
+import expo.modules.notifications.service.NotificationsService
+class ExpoNotificationsService : NotificationsService() {
+}
