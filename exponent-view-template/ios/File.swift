11b680e2efee5e7a1f74bc2e0e8f2309cbfec53e
cruzach <charliecruzan@gmail.com>
2020-11-07 18:13:35 -0500

    make shell app compatible with swift

---
+++ exponent-view-template/ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  exponent-view-template
+//
+//  Created by Charlie Cruzan on 11/5/20.
+//  Copyright © 2020 650 Industries, Inc. All rights reserved.
+//
+
+import Foundation
