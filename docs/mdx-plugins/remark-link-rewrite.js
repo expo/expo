5afb39eeeadbbacc8b3b5d752cdae259222bf987
Cedric van Putten <me@bycedric.com>
2020-11-11 14:37:47 +0100

    [docs] Simplify internal MDX link rewrite

--- docs/mdx-plugins/remark-link-rewrite.js
+++ docs/mdx-plugins/remark-link-rewrite.js
@@ -4,20 +4,17 @@ const { URL } = require('url');
 
 const DEFAULT_OPTIONS = {
   extension: 'md',
-  pageDir: 'pages',
+  pagesDir: 'pages',
   trailingSlash: true,
 };
 
 /**
- * This rewrites relative MDX links to something Next would expect.
- *   - Replaces the `.md` extension with a trailing slash.
- *     e.g. from: /push-notifications/overview.md, to: ./using-fcm.md -> ../using-fcm/
- *   - Rewrites to absolute path from the page directory (backwards compatibility)
- *     e.g. from: /push-notifications/overview.md, to ../using-fcm/ -> /push-notifications/using-fcm/
+ * This rewrites internal MDX links to absolute URLs from the root domain.
+ * It's the similar behavior of GitHub MDX linking, but for Next.
  *
  * @param {object} options
  * @param {string} [options.extension="md"]
- * @param {string} [options.pageDir="pages"]
+ * @param {string} [options.pagesDir="pages"]
  * @param {boolean} [options.trailingSlash=true]
  * @returns {function} remark plugin
  */
@@ -25,69 +22,36 @@ module.exports = function remarkLinkRewrite(options) {
   const settings = Object.assign({}, DEFAULT_OPTIONS, options);
 
   return (tree, file) => {
-    // get the relative path, from pages dir to the current file
-    const filePrefix = getFilePrefix(settings, file);
+    // we can't rewrite files without knowing where the file exists
+    if (!file.cwd || !file.history || !file.history.length) {
+      return;
+    }
+    // index references should be ignored, it's handled by Next
+    const ignoredIndex = 'index' + (settings.trailingSlash ? '/' : '');
 
     visit(tree, 'link', node => {
+      // only rewrite internal non-url nodes
       if (!isFullUrl(node)) {
-        // remove the `.md` or replace it with a trailing `/`
-        node.url = getPathWithoutExt(settings, node.url);
-        // if the path is relative, we need to clean it for Next
-        node.url = getPathRelativeUrl(filePrefix, node.url);
+        // resolve the absolute path to the linked md file (supports hashes)
+        const absolute = path.resolve(path.dirname(file.history[0]), node.url);
+        // resolve the relative path between the linked file and our pages dir (root of the website)
+        const relative = path.relative(path.join(file.cwd, settings.pagesDir), absolute);
+        // rewrite the URL without the `.md` extension, using trailing slash or nothing
+        let url = relative.replace(`.${settings.extension}`, settings.trailingSlash ? '/' : '');
+
+        // if the url is referencing the ignored index, remove it
+        if (url.includes(ignoredIndex)) {
+          url = url.replace(ignoredIndex, '');
+        }
+
+        node.url = `/${url}`;
       }
     });
   };
 };
 
 /**
- * Remove the extension from the path, if it has one.
- * It removes or replaces it with a trailing slash, if that's enabled.
- */
-function getPathWithoutExt(options, url) {
-  return url.replace(`.${options.extension}`, options.trailingSlash ? '/' : '');
-}
-
-/**
- * If the path starts with either `./` or `../` we need to clean this.
- * Next doesn't work nicely with these kinds of paths.
- *   - from: /push-notifications/overview/, to: sending-notifications/ -> sending-notifications/
- *   - from: /push-notifications/overview/, to: ./sending-notifications/ -> /push-notifications/sending-notifications/
- *   - from: /push-notifications/overview/, to: ../sending-notifications/ -> /push-notifications/sending-notifications/
- *
- * @todo remove this if we only have direct file reference links, this is required for backwards compatibility
- */
-function getPathRelativeUrl(filePrefix, url) {
-  if (!url.startsWith('./') && !url.startsWith('../')) {
-    return url;
-  }
-  // both `./` and `../` must be handled as "go 1 up", because of how we anticipated trailing `/` exports
-  return path.join(filePrefix || '/', url.replace(/^\.\//, '../'));
-}
-
-/**
- * Get the prefix of the file that should be added in all URLs.
- * This is calculated from the absolute file path and Next `pages` directory.
- *   - /path/to/docs/pages/push-notifications/overview.md -> /push-notifications/overview/
- *   - /path/to/docs/pages/index.md -> /
- */
-function getFilePrefix(settings, file) {
-  if (!file.history || !file.history.length) {
-    return;
-  }
-
-  const pagePath = path.join(file.cwd, settings.pageDir);
-  const filePath = path.relative(pagePath, file.history[0]);
-  const fileUrl = getPathWithoutExt(settings, `/${filePath}`);
-  const ignored = settings.trailingSlash ? '/index/' : '/index';
-
-  if (fileUrl !== ignored) {
-    return fileUrl;
-  }
-}
-
-/**
  * Determine if the node contains a valid URL.
- * If so, we have to skip this, we only want to rewrite internal links.
  */
 function isFullUrl(node) {
   try {
--- docs/mdx-plugins/remark-link-rewrite.test.js
+++ docs/mdx-plugins/remark-link-rewrite.test.js
@@ -28,67 +28,7 @@ describe('only rewrites internal links', () => {
   });
 });
 
-describe('from index page', () => {
-  const file = makeFile('index.md');
-
-  it('resolves guides to guides', () => {
-    expect(rewrite(file, 'guides')).toBe('guides');
-  });
-
-  it('resolves guides.md to guides/', () => {
-    expect(rewrite(file, 'guides.md')).toBe('guides/');
-  });
-
-  it('resolves ./guides.md to /guides/', () => {
-    expect(rewrite(file, './guides.md')).toBe('/guides/');
-  });
-
-  // legacy method of linking, hardcoding trailing slash
-  it('resolves ../guides.md to /guides/', () => {
-    expect(rewrite(file, '../guides.md')).toBe('/guides/');
-  });
-
-  // legacy method of linking, hardcoding trailing slash
-  it('resolves ../guides/ to /guides/', () => {
-    expect(rewrite(file, '../guides/')).toBe('/guides/');
-  });
-});
-
-describe('from nested page', () => {
-  const file = makeFile('push-notifications/overview.md');
-
-  it('resolves using-fcm to using-fcm', () => {
-    expect(rewrite(file, 'using-fcm')).toBe('using-fcm');
-  });
-
-  it('resolves using-fcm.md to using-fcm/', () => {
-    expect(rewrite(file, 'using-fcm.md')).toBe('using-fcm/');
-  });
-
-  it('resolves ./using-fcm.md to /push-notifications/using-fcm/', () => {
-    expect(rewrite(file, './using-fcm.md')).toBe('/push-notifications/using-fcm/');
-  });
-
-  // legacy method of linking, hardcoding trailing slash
-  it('resolves ../using-fcm.md to /push-notifications/using-fcm/', () => {
-    expect(rewrite(file, '../using-fcm.md')).toBe('/push-notifications/using-fcm/');
-  });
-
-  // legacy method of linking, hardcoding trailing slash
-  it('resolves ../using-fcm/ to /push-notifications/using-fcm/', () => {
-    expect(rewrite(file, '../using-fcm/')).toBe('/push-notifications/using-fcm/');
-  });
-
-  it('resolves ./ to /push-notifications/', () => {
-    expect(rewrite(file, './')).toBe('/push-notifications/');
-  });
-
-  it('resolves ../ to /push-notifications/', () => {
-    expect(rewrite(file, '../')).toBe('/push-notifications/');
-  });
-});
-
-describe('header reference', () => {
+describe('maintains header reference', () => {
   it('resolves hash for index', () => {
     const file = makeFile('index.md');
     expect(rewrite(file, './#a-header')).toBe('/#a-header');
@@ -99,5 +39,82 @@ describe('header reference', () => {
     expect(rewrite(file, './using-fcm.md#a-header')).toBe(
       '/push-notifications/using-fcm/#a-header'
     );
+  });
+
+  it('resolves hash from nested to index', () => {
+    const file = makeFile('push-notifications/overview.md');
+    expect(rewrite(file, '../index.md#a-header')).toBe('/#a-header');
+  });
+});
+
+describe('from pages/index.md', () => {
+  const file = makeFile('index.md');
+
+  it('resolves guides to /guides', () => {
+    expect(rewrite(file, 'guides')).toBe('/guides');
+  });
+
+  it('resolves guides.md to /guides/', () => {
+    expect(rewrite(file, 'guides.md')).toBe('/guides/');
+  });
+
+  it('resolves ./guides.md to /guides/', () => {
+    expect(rewrite(file, './guides.md')).toBe('/guides/');
+  });
+
+  it('resolves ./nested/reference.md to /nested/reference/', () => {
+    expect(rewrite(file, './nested/reference.md')).toBe('/nested/reference/');
+  });
+});
+
+describe('from pages/push-notifications/overview.md', () => {
+  const file = makeFile('push-notifications/overview.md');
+
+  it('resolves using-fcm to /push-notifications/using-fcm', () => {
+    expect(rewrite(file, 'using-fcm')).toBe('/push-notifications/using-fcm');
+  });
+
+  it('resolves using-fcm.md to /push-notifications/using-fcm/', () => {
+    expect(rewrite(file, 'using-fcm.md')).toBe('/push-notifications/using-fcm/');
+  });
+
+  it('resolves ./using-fcm.md to /push-notifications/using-fcm/', () => {
+    expect(rewrite(file, './using-fcm.md')).toBe('/push-notifications/using-fcm/');
+  });
+
+  it('resolves ../guides.md to /guides/', () => {
+    expect(rewrite(file, '../guides.md')).toBe('/guides/');
+  });
+
+  it('resolves ./ to /push-notifications', () => {
+    expect(rewrite(file, './')).toBe('/push-notifications');
+  });
+
+  it('resolves ../ to /', () => {
+    expect(rewrite(file, '../')).toBe('/');
+  });
+
+  it('resolves ../index.md to /', () => {
+    expect(rewrite(file, '../index.md')).toBe('/');
+  });
+});
+
+describe('from pages/versions/latest/sdk/app-auth.md', () => {
+  const file = makeFile('versions/latest/sdk/app-auth.md');
+
+  it('resolves app-loading.md to /versions/latest/sdk/app-loading/', () => {
+    expect(rewrite(file, 'app-loading.md')).toBe('/versions/latest/sdk/app-loading/');
+  });
+
+  it('resolves ./app-loading.md to /versions/latest/sdk/app-loading/', () => {
+    expect(rewrite(file, './app-loading.md')).toBe('/versions/latest/sdk/app-loading/');
+  });
+
+  it('resolves ../config/app.md#android to /versions/latest/config/app/#android', () => {
+    expect(rewrite(file, '../config/app.md#android')).toBe('/versions/latest/config/app/#android');
+  });
+
+  it('resolves ../../../workflow/debugging.md to /workflow/debugging/', () => {
+    expect(rewrite(file, '../../../workflow/debugging.md')).toBe('/workflow/debugging/');
   });
 });
