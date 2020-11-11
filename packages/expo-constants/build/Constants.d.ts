64a8dd39a97a81e953160e0d00c636834d14cd10
Eric Samelson <eric@expo.io>
2020-11-10 16:46:59 -0800

    add ExecutionEnvironment type

--- packages/expo-constants/build/Constants.d.ts
+++ packages/expo-constants/build/Constants.d.ts
@@ -1,4 +1,4 @@
-import { AndroidManifest, AppOwnership, Constants, IOSManifest, NativeConstants, PlatformManifest, UserInterfaceIdiom, WebManifest } from './Constants.types';
-export { AndroidManifest, AppOwnership, Constants, IOSManifest, NativeConstants, PlatformManifest, UserInterfaceIdiom, WebManifest, };
+import { AndroidManifest, AppOwnership, Constants, ExecutionEnvironment, IOSManifest, NativeConstants, PlatformManifest, UserInterfaceIdiom, WebManifest } from './Constants.types';
+export { AndroidManifest, AppOwnership, Constants, ExecutionEnvironment, IOSManifest, NativeConstants, PlatformManifest, UserInterfaceIdiom, WebManifest, };
 declare const _default: Constants;
 export default _default;
