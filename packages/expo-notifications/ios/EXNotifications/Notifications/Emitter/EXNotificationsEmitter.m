expo/expo

commit dfda625f3e7c51fa6c766b60c26dbf09c2987523

[expo-notifications] Fetch latest response from native module, resolving from undefined state more quickly
BTW also making EXNotificationsEmitter always listen to events from EXNotificationCenterDelegate

packages/expo-notifications/ios/EXNotifications/Notifications/Emitter/EXNotificationsEmitter.m
@@ -10,10 +10,9 @@
 
 @property (nonatomic, weak) id<EXNotificationCenterDelegate> notificationCenterDelegate;
 
-@property (nonatomic, assign) BOOL isListening;
-@property (nonatomic, assign) BOOL isBeingObserved;
-
 @property (nonatomic, weak) id<UMEventEmitterService> eventEmitter;
+
+@property (nonatomic, strong) UNNotificationResponse *lastNotificationResponse;
 
 @end
 
@@ -21,12 +20,20 @@
 
 UM_EXPORT_MODULE(ExpoNotificationsEmitter);
 
+UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
+                    getLastNotificationResponseAsyncWithResolver:(UMPromiseResolveBlock)resolve reject:(UMPromiseRejectBlock)reject)
+{
+  resolve(_lastNotificationResponse ? [EXNotificationSerializer serializedNotificationResponse:_lastNotificationResponse] : [NSNull null]);
+}
+
 # pragma mark - UMModuleRegistryConsumer
 
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
 {
   _eventEmitter = [moduleRegistry getModuleImplementingProtocol:@protocol(UMEventEmitterService)];
+
   _notificationCenterDelegate = [moduleRegistry getSingletonModuleForName:@"NotificationCenterDelegate"];
+  [_notificationCenterDelegate addDelegate:self];
 }
 
 # pragma mark - UMEventEmitter
@@ -38,25 +45,10 @@ UM_EXPORT_MODULE(ExpoNotificationsEmitter);
 
 - (void)startObserving
 {
-  [self setIsBeingObserved:YES];
 }
 
 - (void)stopObserving
 {
-  [self setIsBeingObserved:NO];
-}
-
-- (void)setIsBeingObserved:(BOOL)isBeingObserved
-{
-  _isBeingObserved = isBeingObserved;
-  BOOL shouldListen = _isBeingObserved;
-  if (shouldListen && !_isListening) {
-    [_notificationCenterDelegate addDelegate:self];
-    _isListening = YES;
-  } else if (!shouldListen && _isListening) {
-    [_notificationCenterDelegate removeDelegate:self];
-    _isListening = NO;
-  }
 }
 
 # pragma mark - EXNotificationsDelegate
@@ -69,6 +61,7 @@ UM_EXPORT_MODULE(ExpoNotificationsEmitter);
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
 {
+  _lastNotificationResponse = response;
   [_eventEmitter sendEventWithName:onDidReceiveNotificationResponse body:[EXNotificationSerializer serializedNotificationResponse:response]];
   completionHandler();
 }

packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/emitting/NotificationsEmitter.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import org.unimodules.core.ExportedModule;
 import org.unimodules.core.ModuleRegistry;
+import org.unimodules.core.Promise;
+import org.unimodules.core.interfaces.ExpoMethod;
 import org.unimodules.core.interfaces.services.EventEmitter;
 
 import expo.modules.notifications.notifications.NotificationSerializer;
@@ -21,6 +23,7 @@ public class NotificationsEmitter extends ExportedModule implements Notification
   private final static String MESSAGES_DELETED_EVENT_NAME = "onNotificationsDeleted";
 
   private NotificationManager mNotificationManager;
+  private NotificationResponse mLastNotificationResponse;
   private EventEmitter mEventEmitter;
 
   public NotificationsEmitter(Context context) {
@@ -47,6 +50,11 @@ public class NotificationsEmitter extends ExportedModule implements Notification
     mNotificationManager.removeListener(this);
   }
 
+  @ExpoMethod
+  public void getLastNotificationResponseAsync(Promise promise) {
+    promise.resolve(mLastNotificationResponse != null ? NotificationSerializer.toBundle(mLastNotificationResponse) : null);
+  }
+
   /**
    * Callback called when {@link NotificationManager} gets notified of a new notification.
    * Emits a {@link NotificationsEmitter#NEW_MESSAGE_EVENT_NAME} event.
@@ -69,6 +77,7 @@ public class NotificationsEmitter extends ExportedModule implements Notification
    */
   @Override
   public boolean onNotificationResponseReceived(NotificationResponse response) {
+    mLastNotificationResponse = response;
     if (mEventEmitter != null) {
       mEventEmitter.emit(NEW_RESPONSE_EVENT_NAME, NotificationSerializer.toBundle(response));
       return true;

packages/expo-notifications/build/NotificationsEmitterModule.types.d.ts
@@ -1,3 +1,5 @@
 import { ProxyNativeModule } from '@unimodules/core';
+import { NotificationResponse } from './Notifications.types';
 export interface NotificationsEmitterModule extends ProxyNativeModule {
+    getLastNotificationResponseAsync?: () => Promise<NotificationResponse | null>;
 }

packages/expo-notifications/build/NotificationsEmitterModule.types.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"NotificationsEmitterModule.types.js","sourceRoot":"","sources":["../src/NotificationsEmitterModule.types.ts"],"names":[],"mappings":"","sourcesContent":["import { ProxyNativeModule } from '@unimodules/core';\n\nexport interface NotificationsEmitterModule extends ProxyNativeModule {}\n"]}
+{"version":3,"file":"NotificationsEmitterModule.types.js","sourceRoot":"","sources":["../src/NotificationsEmitterModule.types.ts"],"names":[],"mappings":"","sourcesContent":["import { ProxyNativeModule } from '@unimodules/core';\n\nimport { NotificationResponse } from './Notifications.types';\n\nexport interface NotificationsEmitterModule extends ProxyNativeModule {\n  getLastNotificationResponseAsync?: () => Promise<NotificationResponse | null>;\n}\n"]}

packages/expo-notifications/build/useLastNotificationResponse.js
@@ -1,5 +1,6 @@
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { addNotificationResponseReceivedListener } from './NotificationsEmitter';
+import NotificationsEmitterModule from './NotificationsEmitterModule';
 /**
  * Return value of this hook may be one of three types:
  * - `undefined` until we know what to return
@@ -17,6 +18,17 @@ export default function useLastNotificationResponse() {
         return () => {
             subscription.remove();
         };
+    }, []);
+    // On each mount of this hook we fetch last notification response
+    // from the native module which is an "always active listener"
+    // and always returns the most recent response.
+    useEffect(() => {
+        NotificationsEmitterModule.getLastNotificationResponseAsync?.().then(response => {
+            // We only update the state with the resolved value if it's empty,
+            // because if it's not empty it must have been populated by the `useLayoutEffect`
+            // listener which returns "live" values.
+            setLastNotificationResponse(currentResponse => currentResponse ?? response);
+        });
     }, []);
     return lastNotificationResponse;
 }

packages/expo-notifications/build/useLastNotificationResponse.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"useLastNotificationResponse.js","sourceRoot":"","sources":["../src/useLastNotificationResponse.ts"],"names":[],"mappings":"AAAA,OAAO,EAAa,eAAe,EAAE,QAAQ,EAAE,MAAM,OAAO,CAAC;AAG7D,OAAO,EAAE,uCAAuC,EAAE,MAAM,wBAAwB,CAAC;AAGjF;;;;;;GAMG;AACH,MAAM,CAAC,OAAO,UAAU,2BAA2B;IACjD,MAAM,CAAC,wBAAwB,EAAE,2BAA2B,CAAC,GAAG,QAAQ,CAEtE,SAAS,CAAC,CAAC;IAEb,yEAAyE;IACzE,eAAe,CAAC,GAAG,EAAE;QACnB,MAAM,YAAY,GAAG,uCAAuC,CAAC,QAAQ,CAAC,EAAE;YACtE,2BAA2B,CAAC,QAAQ,CAAC,CAAC;QACxC,CAAC,CAAC,CAAC;QACH,OAAO,GAAG,EAAE;YACV,YAAY,CAAC,MAAM,EAAE,CAAC;QACxB,CAAC,CAAC;IACJ,CAAC,EAAE,EAAE,CAAC,CAAC;IAEP,OAAO,wBAAwB,CAAC;AAClC,CAAC","sourcesContent":["import { useEffect, useLayoutEffect, useState } from 'react';\n\nimport { NotificationResponse } from './Notifications.types';\nimport { addNotificationResponseReceivedListener } from './NotificationsEmitter';\nimport NotificationsEmitterModule from './NotificationsEmitterModule';\n\n/**\n * Return value of this hook may be one of three types:\n * - `undefined` until we know what to return\n * - `null` until the app receives any notification response\n * - an object of `NotificationResponse` type - the response\n *   that has been received by the app most recently.\n */\nexport default function useLastNotificationResponse() {\n  const [lastNotificationResponse, setLastNotificationResponse] = useState<\n    NotificationResponse | null | undefined\n  >(undefined);\n\n  // useLayoutEffect ensures the listener is registered as soon as possible\n  useLayoutEffect(() => {\n    const subscription = addNotificationResponseReceivedListener(response => {\n      setLastNotificationResponse(response);\n    });\n    return () => {\n      subscription.remove();\n    };\n  }, []);\n\n  return lastNotificationResponse;\n}\n"]}
+{"version":3,"file":"useLastNotificationResponse.js","sourceRoot":"","sources":["../src/useLastNotificationResponse.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,SAAS,EAAE,eAAe,EAAE,QAAQ,EAAE,MAAM,OAAO,CAAC;AAG7D,OAAO,EAAE,uCAAuC,EAAE,MAAM,wBAAwB,CAAC;AACjF,OAAO,0BAA0B,MAAM,8BAA8B,CAAC;AAEtE;;;;;;GAMG;AACH,MAAM,CAAC,OAAO,UAAU,2BAA2B;IACjD,MAAM,CAAC,wBAAwB,EAAE,2BAA2B,CAAC,GAAG,QAAQ,CAEtE,SAAS,CAAC,CAAC;IAEb,yEAAyE;IACzE,eAAe,CAAC,GAAG,EAAE;QACnB,MAAM,YAAY,GAAG,uCAAuC,CAAC,QAAQ,CAAC,EAAE;YACtE,2BAA2B,CAAC,QAAQ,CAAC,CAAC;QACxC,CAAC,CAAC,CAAC;QACH,OAAO,GAAG,EAAE;YACV,YAAY,CAAC,MAAM,EAAE,CAAC;QACxB,CAAC,CAAC;IACJ,CAAC,EAAE,EAAE,CAAC,CAAC;IAEP,iEAAiE;IACjE,8DAA8D;IAC9D,+CAA+C;IAC/C,SAAS,CAAC,GAAG,EAAE;QACb,0BAA0B,CAAC,gCAAgC,EAAE,EAAE,CAAC,IAAI,CAAC,QAAQ,CAAC,EAAE;YAC9E,kEAAkE;YAClE,iFAAiF;YACjF,wCAAwC;YACxC,2BAA2B,CAAC,eAAe,CAAC,EAAE,CAAC,eAAe,IAAI,QAAQ,CAAC,CAAC;QAC9E,CAAC,CAAC,CAAC;IACL,CAAC,EAAE,EAAE,CAAC,CAAC;IAEP,OAAO,wBAAwB,CAAC;AAClC,CAAC","sourcesContent":["import { useEffect, useLayoutEffect, useState } from 'react';\n\nimport { NotificationResponse } from './Notifications.types';\nimport { addNotificationResponseReceivedListener } from './NotificationsEmitter';\nimport NotificationsEmitterModule from './NotificationsEmitterModule';\n\n/**\n * Return value of this hook may be one of three types:\n * - `undefined` until we know what to return\n * - `null` until the app receives any notification response\n * - an object of `NotificationResponse` type - the response\n *   that has been received by the app most recently.\n */\nexport default function useLastNotificationResponse() {\n  const [lastNotificationResponse, setLastNotificationResponse] = useState<\n    NotificationResponse | null | undefined\n  >(undefined);\n\n  // useLayoutEffect ensures the listener is registered as soon as possible\n  useLayoutEffect(() => {\n    const subscription = addNotificationResponseReceivedListener(response => {\n      setLastNotificationResponse(response);\n    });\n    return () => {\n      subscription.remove();\n    };\n  }, []);\n\n  // On each mount of this hook we fetch last notification response\n  // from the native module which is an \"always active listener\"\n  // and always returns the most recent response.\n  useEffect(() => {\n    NotificationsEmitterModule.getLastNotificationResponseAsync?.().then(response => {\n      // We only update the state with the resolved value if it's empty,\n      // because if it's not empty it must have been populated by the `useLayoutEffect`\n      // listener which returns \"live\" values.\n      setLastNotificationResponse(currentResponse => currentResponse ?? response);\n    });\n  }, []);\n\n  return lastNotificationResponse;\n}\n"]}

packages/expo-notifications/src/NotificationsEmitterModule.types.ts
@@ -1,3 +1,7 @@
 import { ProxyNativeModule } from '@unimodules/core';
 
-export interface NotificationsEmitterModule extends ProxyNativeModule {}
+import { NotificationResponse } from './Notifications.types';
+
+export interface NotificationsEmitterModule extends ProxyNativeModule {
+  getLastNotificationResponseAsync?: () => Promise<NotificationResponse | null>;
+}

packages/expo-notifications/src/useLastNotificationResponse.ts
@@ -26,5 +26,17 @@ export default function useLastNotificationResponse() {
     };
   }, []);
 
+  // On each mount of this hook we fetch last notification response
+  // from the native module which is an "always active listener"
+  // and always returns the most recent response.
+  useEffect(() => {
+    NotificationsEmitterModule.getLastNotificationResponseAsync?.().then(response => {
+      // We only update the state with the resolved value if it's empty,
+      // because if it's not empty it must have been populated by the `useLayoutEffect`
+      // listener which returns "live" values.
+      setLastNotificationResponse(currentResponse => currentResponse ?? response);
+    });
+  }, []);
+
   return lastNotificationResponse;
 }

commit a45edb70189870d9f798993f207995b1e51b965e

[expo-notifications] Silence RN warning regression introduced in dfda625f3e7c51fa6c766b60c26dbf09c2987523

packages/expo-notifications/ios/EXNotifications/Notifications/Emitter/EXNotificationsEmitter.m
@@ -10,6 +10,8 @@
 
 @property (nonatomic, weak) id<EXNotificationCenterDelegate> notificationCenterDelegate;
 
+@property (nonatomic, assign) BOOL isBeingObserved;
+
 @property (nonatomic, weak) id<UMEventEmitterService> eventEmitter;
 
 @property (nonatomic, strong) UNNotificationResponse *lastNotificationResponse;
@@ -45,10 +47,12 @@ UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
 
 - (void)startObserving
 {
+  _isBeingObserved = YES;
 }
 
 - (void)stopObserving
 {
+  _isBeingObserved = NO;
 }
 
 # pragma mark - EXNotificationsDelegate
@@ -62,14 +66,23 @@ UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
 {
   _lastNotificationResponse = response;
-  [_eventEmitter sendEventWithName:onDidReceiveNotificationResponse body:[EXNotificationSerializer serializedNotificationResponse:response]];
+  [self sendEventWithName:onDidReceiveNotificationResponse body:[EXNotificationSerializer serializedNotificationResponse:response]];
   completionHandler();
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
 {
-  [_eventEmitter sendEventWithName:onDidReceiveNotification body:[EXNotificationSerializer serializedNotification:notification]];
+  [self sendEventWithName:onDidReceiveNotification body:[EXNotificationSerializer serializedNotification:notification]];
   completionHandler(UNNotificationPresentationOptionNone);
+}
+
+- (void)sendEventWithName:(NSString *)eventName body:(id)body
+{
+  // Silence React Native warning: "Sending ... with no listeners registered."
+  // See: https://github.com/expo/expo/pull/10883#pullrequestreview-529183413
+  if (_isBeingObserved) {
+    [_eventEmitter sendEventWithName:eventName body:body];
+  }
 }
 
 @end