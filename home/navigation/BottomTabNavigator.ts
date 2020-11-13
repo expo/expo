b07f8d3ad1eb7433d5ca90c105f6c629ca58a18e
Brent Vatne <brentvatne@gmail.com>
2020-11-12 21:00:06 -0800

    [home] Keyboard should not hide tab bar

--- home/navigation/BottomTabNavigator.ts
+++ home/navigation/BottomTabNavigator.ts
@@ -7,5 +7,5 @@ export default BottomTabNavigator;
 export const getNavigatorProps = (props: {
   theme: string;
 }): Partial<ComponentProps<typeof BottomTabNavigator.Navigator>> => ({
-  tabBarOptions: { labelStyle: { fontWeight: '600' } },
+  tabBarOptions: { labelStyle: { fontWeight: '600' }, keyboardHidesTabBar: false },
 });
