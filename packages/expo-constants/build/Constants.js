e514a35a6b0b696aefd779aa315debbf17326d1f
Eric Samelson <esamelson@users.noreply.github.com>
2020-11-13 17:31:05 -0800

    [expo-constants] use Updates.manifest if it exists and is non-empty (#10668)

--- packages/expo-constants/build/Constants.js
+++ packages/expo-constants/build/Constants.js
@@ -1,13 +1,28 @@
+import { NativeModulesProxy } from '@unimodules/core';
 import { AppOwnership, ExecutionEnvironment, UserInterfaceIdiom, } from './Constants.types';
 import ExponentConstants from './ExponentConstants';
 export { AppOwnership, ExecutionEnvironment, UserInterfaceIdiom, };
 if (!ExponentConstants) {
     console.warn("No native ExponentConstants module found, are you sure the expo-constants's module is linked properly?");
 }
-// On Android we pass the manifest in JSON form so this step is necessary
 let manifest = null;
-if (ExponentConstants && ExponentConstants.manifest) {
+// If expo-updates defines a non-empty manifest, prefer that one
+if (NativeModulesProxy.ExpoUpdates) {
+    let updatesManifest;
+    if (NativeModulesProxy.ExpoUpdates.manifest) {
+        updatesManifest = NativeModulesProxy.ExpoUpdates.manifest;
+    }
+    else if (NativeModulesProxy.ExpoUpdates.manifestString) {
+        updatesManifest = JSON.parse(NativeModulesProxy.ExpoUpdates.manifestString);
+    }
+    if (updatesManifest && Object.keys(updatesManifest).length > 0) {
+        manifest = updatesManifest;
+    }
+}
+// Fall back to ExponentConstants.manifest if we don't have one from Updates
+if (!manifest && ExponentConstants && ExponentConstants.manifest) {
     manifest = ExponentConstants.manifest;
+    // On Android we pass the manifest in JSON form so this step is necessary
     if (typeof manifest === 'string') {
         manifest = JSON.parse(manifest);
     }
