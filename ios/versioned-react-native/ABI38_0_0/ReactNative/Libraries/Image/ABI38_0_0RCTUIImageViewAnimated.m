572bd817fcad6967970747056e84760654d66c18
cruzach <charliecruzan@gmail.com>
2020-11-16 14:16:40 -0500

    [ios][react-native] Fix image cannot show in iOS 14 on sdks 37 and 38

--- ios/versioned-react-native/ABI38_0_0/ReactNative/Libraries/Image/ABI38_0_0RCTUIImageViewAnimated.m
+++ ios/versioned-react-native/ABI38_0_0/ReactNative/Libraries/Image/ABI38_0_0RCTUIImageViewAnimated.m
@@ -275,6 +275,8 @@ static NSUInteger ABI38_0_0RCTDeviceFreeMemory() {
   if (_currentFrame) {
     layer.contentsScale = self.animatedImageScale;
     layer.contents = (__bridge id)_currentFrame.CGImage;
+  } else {
+    [super displayLayer:layer];
   }
 }
 
