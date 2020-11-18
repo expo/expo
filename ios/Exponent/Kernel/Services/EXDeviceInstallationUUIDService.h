1ce49bc57fcfa37113f047cec91c5d0568331591
Stanisław Chmiela <sjchmiela@gmail.com>
2020-11-18 21:02:47 +0100

    [ios] Let device installation UUID kernel service serve only versioned constants

--- ios/Exponent/Kernel/Services/EXDeviceInstallationUUIDService.h
+++ ios/Exponent/Kernel/Services/EXDeviceInstallationUUIDService.h
@@ -1,13 +1,14 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 
 #import <Foundation/Foundation.h>
-#import "EXScopedInstallationIdProvider.h"
-#import "EXConstantsBinding.h"
+#import <ABI39_0_0ExpoKit/ABI39_0_0EXConstantsBinding.h>
+#import <ABI38_0_0ExpoKit/ABI38_0_0EXConstantsBinding.h>
+#import <ABI37_0_0ExpoKit/ABI37_0_0EXConstantsBinding.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-// A kernel service allowing modules to access device installation UUID
-@interface EXDeviceInstallationUUIDService : NSObject <EXDeviceInstallationUUIDManager, EXConstantsDeviceInstallationUUIDManager>
+// A kernel service allowing versioned expo-constants to access device installation UUID
+@interface EXDeviceInstallationUUIDService : NSObject <ABI37_0_0EXConstantsDeviceInstallationUUIDManager, ABI38_0_0EXConstantsDeviceInstallationUUIDManager, ABI39_0_0EXConstantsDeviceInstallationUUIDManager>
 
 @end
 
