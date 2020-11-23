expo/expo

commit 83ac459ea8f369467396c9cabd60db8deaa03bc6

[repo] Deprecate installationId (#10997)
# Why

https://github.com/expo/expo/pull/6749#discussion_r367214765

# How

Where it was obvious how to, I replaced it with custom implementation. Where it wasn't obvious:

https://github.com/expo/expo/blob/2a0044694dfddc82997bd3f78a69967cca16ad45/packages/expo-firebase-analytics/src/ExpoFirebaseAnalytics.ts#L51-L62

I left it as it was.

# Test Plan

TS compiles.

packages/expo/src/environment/getInstallationIdAsync.ts
@@ -1,0 +1,26 @@
+import * as Application from 'expo-application';
+import { v5 as uuidv5 } from 'uuid';
+
+let installationId: string | null;
+const UUID_NAMESPACE = '29cc8a0d-747c-5f85-9ff9-f2f16636d963'; // uuidv5(0, "expo")
+
+export default async function getInstallationIdAsync() {
+  if (installationId) {
+    return installationId;
+  }
+
+  const identifierForVendor = await Application.getIosIdForVendorAsync();
+  const bundleIdentifier = Application.applicationId!;
+
+  // It's unlikely identifierForVendor will be null (it returns null if the
+  // device has been restarted but not yet unlocked), but let's handle this
+  // case.
+  if (identifierForVendor) {
+    installationId = uuidv5(`${bundleIdentifier}-${identifierForVendor}`, UUID_NAMESPACE);
+  } else {
+    const installationTime = await Application.getInstallationTimeAsync();
+    installationId = uuidv5(`${bundleIdentifier}-${installationTime.getTime()}`, UUID_NAMESPACE);
+  }
+
+  return installationId;
+}

packages/expo-application/src/Application.ts
@@ -23,11 +23,11 @@ export async function getInstallReferrerAsync(): Promise<string> {
   return await ExpoApplication.getInstallReferrerAsync();
 }
 
-export async function getIosIdForVendorAsync(): Promise<string> {
+export async function getIosIdForVendorAsync(): Promise<string | null> {
   if (!ExpoApplication.getIosIdForVendorAsync) {
     throw new UnavailabilityError('expo-application', 'getIosIdForVendorAsync');
   }
-  return await ExpoApplication.getIosIdForVendorAsync();
+  return (await ExpoApplication.getIosIdForVendorAsync()) ?? null;
 }
 
 export enum ApplicationReleaseType {

packages/expo/src/environment/getInstallationIdAsync.android.ts
@@ -1,0 +1,5 @@
+import Application from 'expo-application';
+
+export default function getInstallationIdAsync() {
+  return Application.androidId!;
+}

packages/expo/src/environment/getInstallationIdAsync.web.ts
@@ -1,0 +1,37 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const INSTALLATION_ID_KEY = 'installationId';
+
+let installationId: string | null = null;
+
+export default async function getInstallationIdAsync() {
+  // Already cached value
+  if (installationId) {
+    return installationId;
+  }
+
+  try {
+    // No cached value, fetch from persisted storage
+    installationId = localStorage.getItem(INSTALLATION_ID_KEY);
+    if (installationId) {
+      return installationId;
+    }
+  } catch (error) {
+    // If we weren't able to fetch one (for whatever reason)
+    // let's create a new one.
+  }
+
+  // No persisted value, set the cached value...
+  installationId = uuidv4();
+  // ...and try to persist it. Ignore the errors.
+  try {
+    localStorage.setItem(INSTALLATION_ID_KEY, installationId);
+  } catch (error) {
+    console.debug(
+      '[expo-notifications] Could not save installation ID in persisted storage, it will get reset.',
+      error
+    );
+  }
+
+  return installationId;
+}

packages/expo/src/logs/RemoteLogging.ts
@@ -1,8 +1,9 @@
 import Constants from 'expo-constants';
 import { EventEmitter, EventSubscription } from 'fbemitter';
 import invariant from 'invariant';
-import uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 
+import getInstallationIdAsync from '../environment/getInstallationIdAsync';
 import LogSerialization from './LogSerialization';
 
 export type LogLevel = 'debug' | 'info' | 'warn' | 'error';
@@ -109,7 +110,7 @@ async function _sendNextLogBatchAsync(batch: LogEntry[], logUrl: string): Promis
     Connection: 'keep-alive',
     'Proxy-Connection': 'keep-alive',
     Accept: 'application/json',
-    'Device-Id': Constants.installationId,
+    'Device-Id': await getInstallationIdAsync(),
     'Session-Id': _sessionId,
   };
   if (Constants.deviceName) {

packages/expo/src/logs/__tests__/RemoteLogging-test.native.ts
@@ -1,7 +1,13 @@
 import LogSerialization from '../LogSerialization';
 import RemoteLogging, { __waitForEmptyLogQueueAsync } from '../RemoteLogging';
 
-jest.mock('uuid/v4', () => () => 'c0d50576-7ddc-4196-8b1d-01c2d1786631');
+jest.mock('uuid', () => {
+  const originalModule = jest.requireActual('uuid');
+  return {
+    ...originalModule,
+    v4: () => 'c0d50576-7ddc-4196-8b1d-01c2d1786631',
+  };
+});
 
 jest.mock('../LogSerialization', () => ({
   serializeLogDataAsync: jest.fn(async data => {
@@ -13,6 +19,11 @@ jest.mock('../LogSerialization', () => ({
 }));
 
 jest.mock('expo-constants', () => require('../../__mocks__/Constants-development'));
+jest.mock('expo-application', () => ({
+  androidId: 'i-am-unique',
+  applicationId: 'me-too',
+  getIosIdForVendorAsync: async () => 'me-too!',
+}));
 
 let originalFetch;
 

packages/expo/src/logs/__tests__/RemoteLogs-test.ts
@@ -6,6 +6,11 @@ jest.mock('react-native/Libraries/Core/Devtools/symbolicateStackTrace', () =>
 );
 
 jest.mock('expo-constants', () => require('../../__mocks__/Constants-development'));
+jest.mock('expo-application', () => ({
+  androidId: 'i-am-unique',
+  applicationId: 'me-too',
+  getIosIdForVendorAsync: async () => 'me-too!',
+}));
 
 let originalFetch;
 

packages/expo/src/logs/__tests__/__snapshots__/RemoteLogging-test.native.ts.snap.android
@@ -5,7 +5,7 @@ Object {
   "Accept": "application/json",
   "Connection": "keep-alive",
   "Content-Type": "application/json",
-  "Device-Id": "a01650bb-918d-40be-87be-cf376ab6189f",
+  "Device-Id": "i-am-unique",
   "Device-Name": "Test Phone",
   "Proxy-Connection": "keep-alive",
   "Session-Id": "c0d50576-7ddc-4196-8b1d-01c2d1786631",

commit b38537800080bac1f6184fdfeed774561e741031

[expo] Fix bug introduced in 83ac459, using uuid 8.3.0 without polyfill

packages/expo/src/environment/getInstallationIdAsync.ts
@@ -1,5 +1,5 @@
 import * as Application from 'expo-application';
-import { v5 as uuidv5 } from 'uuid';
+import uuidv5 from 'uuid/v5';
 
 let installationId: string | null;
 const UUID_NAMESPACE = '29cc8a0d-747c-5f85-9ff9-f2f16636d963'; // uuidv5(0, "expo")