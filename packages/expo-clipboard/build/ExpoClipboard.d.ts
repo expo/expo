86cc5501d14a92455d925a4701e72e53ea8e82c6
cruzach <charliecruzan@gmail.com>
2020-12-04 12:10:52 -0500

    yarn build

---
+++ packages/expo-clipboard/build/ExpoClipboard.d.ts
@@ -0,0 +1,2 @@
+import Clipboard from 'react-native/Libraries/Components/Clipboard/Clipboard';
+export default Clipboard;
---
+++ packages/expo-clipboard/build/ExpoClipboard.js
@@ -0,0 +1,4 @@
+// Temporary for SDK 40 until we make our own native implementation
+import Clipboard from 'react-native/Libraries/Components/Clipboard/Clipboard';
+export default Clipboard;
+//# sourceMappingURL=ExpoClipboard.js.map
