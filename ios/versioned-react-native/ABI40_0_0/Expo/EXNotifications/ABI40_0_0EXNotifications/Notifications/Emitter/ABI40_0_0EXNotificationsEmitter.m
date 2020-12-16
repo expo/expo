expo/expo

commit 1037e824587892944cb06e0e7ccd1ebb3b218f3c

[expo-notifications] Add useLastNotificationResponse hook (#10883)
# Why

Idea inspired by @cruzach sounded! https://github.com/expo/expo/pull/10811#pullrequestreview-518819031

# How

- Since there will be two hooks now that depend on the notification response listener being registered in the global scope I moved it to a `SharedNotificationResponseListener.fx.ts` file
- Changed `useInitialNotificationResponse` implementation to rely on it
- Added the ability to listen for incoming responses to the shared listener
- Implemented `useLastNotificationResponse` which is like a notification response swiss knife:
  - if registered early enough judging by its return values states you can deduce whether there was this special "initial notification response" or not
  - always returns the latest notification response received (or something falsy, depending on whether you care about the "initiality" or "initialness" of the response you can ignore the difference between `undefined` or `null` or not)

Frankly speaking I'm not 100% sure how big of a deal is leaving a dangling listener active, but I can't think of any reason why it would be a problem.

# Test Plan

I have verified this hook returns latest notification response if the hook is mounted as (early in the app as possible and after some time from starting the app) ✖️ (the app is started in response to notification tap, notification tap while the app is running)

packages/expo-notifications/ios/EXNotifications/Notifications/Emitter/EXNotificationsEmitter.m
@@ -10,10 +10,11 @@
 
 @property (nonatomic, weak) id<EXNotificationCenterDelegate> notificationCenterDelegate;
 
-@property (nonatomic, assign) BOOL isListening;
 @property (nonatomic, assign) BOOL isBeingObserved;
 
 @property (nonatomic, weak) id<UMEventEmitterService> eventEmitter;
+
+@property (nonatomic, strong) UNNotificationResponse *lastNotificationResponse;
 
 @end
 
@@ -21,12 +22,20 @@
 
 UM_EXPORT_MODULE(ExpoNotificationsEmitter);
 
+UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
+                    getLastNotificationResponseAsyncWithResolver:(UMPromiseResolveBlock)resolve reject:(UMPromiseRejectBlock)reject)
+{
+  resolve(_lastNotificationResponse ? [self serializedNotificationResponse:_lastNotificationResponse] : [NSNull null]);
+}
+
 # pragma mark - UMModuleRegistryConsumer
 
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
 {
   _eventEmitter = [moduleRegistry getModuleImplementingProtocol:@protocol(UMEventEmitterService)];
+
   _notificationCenterDelegate = [moduleRegistry getSingletonModuleForName:@"NotificationCenterDelegate"];
+  [_notificationCenterDelegate addDelegate:self];
 }
 
 # pragma mark - UMEventEmitter
@@ -38,25 +47,12 @@ UM_EXPORT_MODULE(ExpoNotificationsEmitter);
 
 - (void)startObserving
 {
-  [self setIsBeingObserved:YES];
+  _isBeingObserved = YES;
 }
 
 - (void)stopObserving
 {
-  [self setIsBeingObserved:NO];
-}
-
-- (void)setIsBeingObserved:(BOOL)isBeingObserved
-{
-  _isBeingObserved = isBeingObserved;
-  BOOL shouldListen = _isBeingObserved;
-  if (shouldListen && !_isListening) {
-    [_notificationCenterDelegate addDelegate:self];
-    _isListening = YES;
-  } else if (!shouldListen && _isListening) {
-    [_notificationCenterDelegate removeDelegate:self];
-    _isListening = NO;
-  }
+  _isBeingObserved = NO;
 }
 
 # pragma mark - EXNotificationsDelegate
@@ -69,14 +65,34 @@ UM_EXPORT_MODULE(ExpoNotificationsEmitter);
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
 {
-  [_eventEmitter sendEventWithName:onDidReceiveNotificationResponse body:[EXNotificationSerializer serializedNotificationResponse:response]];
+  _lastNotificationResponse = response;
+  [self sendEventWithName:onDidReceiveNotificationResponse body:[self serializedNotificationResponse:response]];
   completionHandler();
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
 {
-  [_eventEmitter sendEventWithName:onDidReceiveNotification body:[EXNotificationSerializer serializedNotification:notification]];
+  [self sendEventWithName:onDidReceiveNotification body:[self serializedNotification:notification]];
   completionHandler(UNNotificationPresentationOptionNone);
+}
+
+- (void)sendEventWithName:(NSString *)eventName body:(id)body
+{
+  // Silence React Native warning: "Sending ... with no listeners registered."
+  // See: https://github.com/expo/expo/pull/10883#pullrequestreview-529183413
+  if (_isBeingObserved) {
+    [_eventEmitter sendEventWithName:eventName body:body];
+  }
+}
+
+- (NSDictionary *)serializedNotification:(UNNotification *)notification
+{
+  return [EXNotificationSerializer serializedNotification:notification];
+}
+
+- (NSDictionary *)serializedNotificationResponse:(UNNotificationResponse *)notificationResponse
+{
+  return [EXNotificationSerializer serializedNotificationResponse:notificationResponse];
 }
 
 @end

ios/Exponent/Versioned/Core/UniversalModules/EXNotifications/EXScopedNotificationsEmitter.m
@@ -12,6 +12,13 @@
 
 @end
 
+@interface EXNotificationsEmitter (Protected)
+
+- (NSDictionary *)serializedNotification:(UNNotification *)notification;
+- (NSDictionary *)serializedNotificationResponse:(UNNotificationResponse *)notificationResponse;
+
+@end
+
 @implementation EXScopedNotificationsEmitter
 
 - (instancetype)initWithExperienceId:(NSString *)experienceId
@@ -26,19 +33,29 @@
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
 {
   if ([EXScopedNotificationsUtils shouldNotification:response.notification beHandledByExperience:_experienceId]) {
-    [self.eventEmitter sendEventWithName:onDidReceiveNotificationResponse body:[EXScopedNotificationSerializer serializedNotificationResponse:response]];
+    [super userNotificationCenter:center didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
+  } else {
+    completionHandler();
   }
-  
-  completionHandler();
 }
 
 - (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
 {
   if ([EXScopedNotificationsUtils shouldNotification:notification beHandledByExperience:_experienceId]) {
-    [self.eventEmitter sendEventWithName:onDidReceiveNotification body:[EXScopedNotificationSerializer serializedNotification:notification]];
+    [super userNotificationCenter:center willPresentNotification:notification withCompletionHandler:completionHandler];
+  } else {
+    completionHandler(UNNotificationPresentationOptionNone);
   }
-  
-  completionHandler(UNNotificationPresentationOptionNone);
+}
+
+- (NSDictionary *)serializedNotification:(UNNotification *)notification
+{
+  return [EXScopedNotificationSerializer serializedNotification:notification];
+}
+
+- (NSDictionary *)serializedNotificationResponse:(UNNotificationResponse *)notificationResponse
+{
+  return [EXScopedNotificationSerializer serializedNotificationResponse:notificationResponse];
 }
 
 @end

packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/emitting/NotificationsEmitter.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import org.unimodules.core.ExportedModule;
 import org.unimodules.core.ModuleRegistry;
+import org.unimodules.core.Promise;
+import org.unimodules.core.interfaces.ExpoMethod;
 import org.unimodules.core.interfaces.services.EventEmitter;
 
 import expo.modules.notifications.notifications.NotificationSerializer;
@@ -21,6 +23,7 @@ public class NotificationsEmitter extends ExportedModule implements Notification
   private final static String MESSAGES_DELETED_EVENT_NAME = "onNotificationsDeleted";
 
   private NotificationManager mNotificationManager;
+  private NotificationResponse mLastNotificationResponse;
   private EventEmitter mEventEmitter;
 
   public NotificationsEmitter(Context context) {
@@ -47,6 +50,11 @@ public class NotificationsEmitter extends ExportedModule implements Notification
     mNotificationManager.removeListener(this);
   }
 
+  @ExpoMethod
+  public void getLastNotificationResponseAsync(Promise promise) {
+    promise.resolve(mLastNotificationResponse != null ? NotificationSerializer.toBundle(mLastNotificationResponse) : null);
+  }
+
   /**
    * Callback called when {@link NotificationManager} gets notified of a new notification.
    * Emits a {@link NotificationsEmitter#NEW_MESSAGE_EVENT_NAME} event.
@@ -69,6 +77,7 @@ public class NotificationsEmitter extends ExportedModule implements Notification
    */
   @Override
   public boolean onNotificationResponseReceived(NotificationResponse response) {
+    mLastNotificationResponse = response;
     if (mEventEmitter != null) {
       mEventEmitter.emit(NEW_RESPONSE_EVENT_NAME, NotificationSerializer.toBundle(response));
       return true;

packages/expo-notifications/build/NotificationsEmitter.d.ts
@@ -5,4 +5,3 @@ export declare function addNotificationReceivedListener(listener: (event: Notifi
 export declare function addNotificationsDroppedListener(listener: () => void): Subscription;
 export declare function addNotificationResponseReceivedListener(listener: (event: NotificationResponse) => void): Subscription;
 export declare function removeNotificationSubscription(subscription: Subscription): void;
-export declare function removeAllNotificationListeners(): void;

packages/expo-notifications/build/NotificationsEmitter.js
@@ -18,9 +18,4 @@ export function addNotificationResponseReceivedListener(listener) {
 export function removeNotificationSubscription(subscription) {
     emitter.removeSubscription(subscription);
 }
-export function removeAllNotificationListeners() {
-    emitter.removeAllListeners(didReceiveNotificationEventName);
-    emitter.removeAllListeners(didDropNotificationsEventName);
-    emitter.removeAllListeners(didReceiveNotificationResponseEventName);
-}
 //# sourceMappingURL=NotificationsEmitter.js.map

packages/expo-notifications/build/NotificationsEmitter.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"NotificationsEmitter.js","sourceRoot":"","sources":["../src/NotificationsEmitter.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,YAAY,EAAgB,MAAM,kBAAkB,CAAC;AAG9D,OAAO,0BAA0B,MAAM,8BAA8B,CAAC;AAEtE,iCAAiC;AACjC,MAAM,OAAO,GAAG,IAAI,YAAY,CAAC,0BAA0B,CAAC,CAAC;AAE7D,MAAM,+BAA+B,GAAG,0BAA0B,CAAC;AACnE,MAAM,6BAA6B,GAAG,wBAAwB,CAAC;AAC/D,MAAM,uCAAuC,GAAG,kCAAkC,CAAC;AAEnF,MAAM,CAAC,MAAM,yBAAyB,GAAG,4CAA4C,CAAC;AAEtF,MAAM,UAAU,+BAA+B,CAC7C,QAAuC;IAEvC,OAAO,OAAO,CAAC,WAAW,CAAe,+BAA+B,EAAE,QAAQ,CAAC,CAAC;AACtF,CAAC;AAED,MAAM,UAAU,+BAA+B,CAAC,QAAoB;IAClE,OAAO,OAAO,CAAC,WAAW,CAAO,6BAA6B,EAAE,QAAQ,CAAC,CAAC;AAC5E,CAAC;AAED,MAAM,UAAU,uCAAuC,CACrD,QAA+C;IAE/C,OAAO,OAAO,CAAC,WAAW,CACxB,uCAAuC,EACvC,QAAQ,CACT,CAAC;AACJ,CAAC;AAED,MAAM,UAAU,8BAA8B,CAAC,YAA0B;IACvE,OAAO,CAAC,kBAAkB,CAAC,YAAY,CAAC,CAAC;AAC3C,CAAC;AAED,MAAM,UAAU,8BAA8B;IAC5C,OAAO,CAAC,kBAAkB,CAAC,+BAA+B,CAAC,CAAC;IAC5D,OAAO,CAAC,kBAAkB,CAAC,6BAA6B,CAAC,CAAC;IAC1D,OAAO,CAAC,kBAAkB,CAAC,uCAAuC,CAAC,CAAC;AACtE,CAAC","sourcesContent":["import { EventEmitter, Subscription } from '@unimodules/core';\n\nimport { Notification, NotificationResponse } from './Notifications.types';\nimport NotificationsEmitterModule from './NotificationsEmitterModule';\n\n// Web uses SyntheticEventEmitter\nconst emitter = new EventEmitter(NotificationsEmitterModule);\n\nconst didReceiveNotificationEventName = 'onDidReceiveNotification';\nconst didDropNotificationsEventName = 'onNotificationsDeleted';\nconst didReceiveNotificationResponseEventName = 'onDidReceiveNotificationResponse';\n\nexport const DEFAULT_ACTION_IDENTIFIER = 'expo.modules.notifications.actions.DEFAULT';\n\nexport function addNotificationReceivedListener(\n  listener: (event: Notification) => void\n): Subscription {\n  return emitter.addListener<Notification>(didReceiveNotificationEventName, listener);\n}\n\nexport function addNotificationsDroppedListener(listener: () => void): Subscription {\n  return emitter.addListener<void>(didDropNotificationsEventName, listener);\n}\n\nexport function addNotificationResponseReceivedListener(\n  listener: (event: NotificationResponse) => void\n): Subscription {\n  return emitter.addListener<NotificationResponse>(\n    didReceiveNotificationResponseEventName,\n    listener\n  );\n}\n\nexport function removeNotificationSubscription(subscription: Subscription) {\n  emitter.removeSubscription(subscription);\n}\n\nexport function removeAllNotificationListeners() {\n  emitter.removeAllListeners(didReceiveNotificationEventName);\n  emitter.removeAllListeners(didDropNotificationsEventName);\n  emitter.removeAllListeners(didReceiveNotificationResponseEventName);\n}\n"]}
+{"version":3,"file":"NotificationsEmitter.js","sourceRoot":"","sources":["../src/NotificationsEmitter.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,YAAY,EAAgB,MAAM,kBAAkB,CAAC;AAG9D,OAAO,0BAA0B,MAAM,8BAA8B,CAAC;AAEtE,iCAAiC;AACjC,MAAM,OAAO,GAAG,IAAI,YAAY,CAAC,0BAA0B,CAAC,CAAC;AAE7D,MAAM,+BAA+B,GAAG,0BAA0B,CAAC;AACnE,MAAM,6BAA6B,GAAG,wBAAwB,CAAC;AAC/D,MAAM,uCAAuC,GAAG,kCAAkC,CAAC;AAEnF,MAAM,CAAC,MAAM,yBAAyB,GAAG,4CAA4C,CAAC;AAEtF,MAAM,UAAU,+BAA+B,CAC7C,QAAuC;IAEvC,OAAO,OAAO,CAAC,WAAW,CAAe,+BAA+B,EAAE,QAAQ,CAAC,CAAC;AACtF,CAAC;AAED,MAAM,UAAU,+BAA+B,CAAC,QAAoB;IAClE,OAAO,OAAO,CAAC,WAAW,CAAO,6BAA6B,EAAE,QAAQ,CAAC,CAAC;AAC5E,CAAC;AAED,MAAM,UAAU,uCAAuC,CACrD,QAA+C;IAE/C,OAAO,OAAO,CAAC,WAAW,CACxB,uCAAuC,EACvC,QAAQ,CACT,CAAC;AACJ,CAAC;AAED,MAAM,UAAU,8BAA8B,CAAC,YAA0B;IACvE,OAAO,CAAC,kBAAkB,CAAC,YAAY,CAAC,CAAC;AAC3C,CAAC","sourcesContent":["import { EventEmitter, Subscription } from '@unimodules/core';\n\nimport { Notification, NotificationResponse } from './Notifications.types';\nimport NotificationsEmitterModule from './NotificationsEmitterModule';\n\n// Web uses SyntheticEventEmitter\nconst emitter = new EventEmitter(NotificationsEmitterModule);\n\nconst didReceiveNotificationEventName = 'onDidReceiveNotification';\nconst didDropNotificationsEventName = 'onNotificationsDeleted';\nconst didReceiveNotificationResponseEventName = 'onDidReceiveNotificationResponse';\n\nexport const DEFAULT_ACTION_IDENTIFIER = 'expo.modules.notifications.actions.DEFAULT';\n\nexport function addNotificationReceivedListener(\n  listener: (event: Notification) => void\n): Subscription {\n  return emitter.addListener<Notification>(didReceiveNotificationEventName, listener);\n}\n\nexport function addNotificationsDroppedListener(listener: () => void): Subscription {\n  return emitter.addListener<void>(didDropNotificationsEventName, listener);\n}\n\nexport function addNotificationResponseReceivedListener(\n  listener: (event: NotificationResponse) => void\n): Subscription {\n  return emitter.addListener<NotificationResponse>(\n    didReceiveNotificationResponseEventName,\n    listener\n  );\n}\n\nexport function removeNotificationSubscription(subscription: Subscription) {\n  emitter.removeSubscription(subscription);\n}\n"]}

packages/expo-notifications/build/NotificationsEmitterModule.types.d.ts
@@ -1,3 +1,5 @@
 import { ProxyNativeModule } from '@unimodules/core';
+import { NotificationResponse } from './Notifications.types';
 export interface NotificationsEmitterModule extends ProxyNativeModule {
+    getLastNotificationResponseAsync?: () => Promise<NotificationResponse | null>;
 }

packages/expo-notifications/build/NotificationsEmitterModule.types.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"NotificationsEmitterModule.types.js","sourceRoot":"","sources":["../src/NotificationsEmitterModule.types.ts"],"names":[],"mappings":"","sourcesContent":["import { ProxyNativeModule } from '@unimodules/core';\n\nexport interface NotificationsEmitterModule extends ProxyNativeModule {}\n"]}
+{"version":3,"file":"NotificationsEmitterModule.types.js","sourceRoot":"","sources":["../src/NotificationsEmitterModule.types.ts"],"names":[],"mappings":"","sourcesContent":["import { ProxyNativeModule } from '@unimodules/core';\n\nimport { NotificationResponse } from './Notifications.types';\n\nexport interface NotificationsEmitterModule extends ProxyNativeModule {\n  getLastNotificationResponseAsync?: () => Promise<NotificationResponse | null>;\n}\n"]}

commit 5acb3fcf051af9e4ac867fdebef7a47661d40b8c

[expo-notifications] Register emitter as center delegate's delegate only once JS subscribes to it (#11382)
# Why

Fixes a bug introduced in https://github.com/expo/expo/pull/10883 which causes initial notification responses to get lost in standalone iOS apps.

Along with #11378 I believe it fixes #11343 and may also finally fix #9866.

# How

Using native breakpoints I've noticed `EXNotificationCenterDelegate`'s `addDelegate:` is called more times than I would expect it to.

It turned out before `EXScopedNotificationsEmitter` was subscribing to `EXNotificationCenterDelegate` (it would then receive pending notification response) an unscoped `EXNotificationsEmitter` was subscribing and consuming pending responses. Since unscoped emitter wasn't being used and exposed to the app later, it consumed the response into the void.

Most probably unimodules have a bug where instances of overridden modules are still kept in memory (since `setModuleRegistry:` is called on them). Fixing this bug is out of the scope of this pull request.

These changes are based on how it was implemented before https://github.com/expo/expo/pull/10883/commits/dfda625f3e7c51fa6c766b60c26dbf09c2987523. I can't see any reason why `EXNotificationsEmitter` would need to register for notifications as soon as module registry is ready, so let's roll back to what we know worked well.

# Test Plan

I have verified in an ExpoKit workspace that when the app is opened from a notification, the notification response is delivered to `useLastNotificationResponse`:

https://www.loom.com/share/586a906c538f47f0940b2cc83582fb29

ios/versioned-react-native/ABI40_0_0/Expo/EXNotifications/ABI40_0_0EXNotifications/Notifications/Emitter/ABI40_0_0EXNotificationsEmitter.m
@@ -11,6 +11,7 @@
 @property (nonatomic, weak) id<ABI40_0_0EXNotificationCenterDelegate> notificationCenterDelegate;
 
 @property (nonatomic, assign) BOOL isBeingObserved;
+@property (nonatomic, assign) BOOL isListening;
 
 @property (nonatomic, weak) id<ABI40_0_0UMEventEmitterService> eventEmitter;
 
@@ -47,12 +48,25 @@ ABI40_0_0UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
 
 - (void)startObserving
 {
-  _isBeingObserved = YES;
+  [self setIsBeingObserved:YES];
 }
 
 - (void)stopObserving
 {
-  _isBeingObserved = NO;
+  [self setIsBeingObserved:NO];
+}
+
+- (void)setIsBeingObserved:(BOOL)isBeingObserved
+{
+  _isBeingObserved = isBeingObserved;
+  BOOL shouldListen = _isBeingObserved;
+  if (shouldListen && !_isListening) {
+    [_notificationCenterDelegate addDelegate:self];
+    _isListening = YES;
+  } else if (!shouldListen && _isListening) {
+    [_notificationCenterDelegate removeDelegate:self];
+    _isListening = NO;
+  }
 }
 
 # pragma mark - ABI40_0_0EXNotificationsDelegate
@@ -80,6 +94,10 @@ ABI40_0_0UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
 {
   // Silence ABI40_0_0React Native warning: "Sending ... with no listeners registered."
   // See: https://github.com/expo/expo/pull/10883#pullrequestreview-529183413
+  // While in practice we don't need to verify this, as as of the end of 2020
+  // we wouldn't send any event to JS if we weren't being observed because
+  // we wouldn't be subscribed to the notification center delegate it's nice
+  // to be sure this problem won't ever arise.
   if (_isBeingObserved) {
     [_eventEmitter sendEventWithName:eventName body:body];
   }

packages/expo-notifications/ios/EXNotifications/Notifications/Emitter/EXNotificationsEmitter.m
@@ -11,6 +11,7 @@
 @property (nonatomic, weak) id<EXNotificationCenterDelegate> notificationCenterDelegate;
 
 @property (nonatomic, assign) BOOL isBeingObserved;
+@property (nonatomic, assign) BOOL isListening;
 
 @property (nonatomic, weak) id<UMEventEmitterService> eventEmitter;
 
@@ -33,9 +34,7 @@ UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
 {
   _eventEmitter = [moduleRegistry getModuleImplementingProtocol:@protocol(UMEventEmitterService)];
-
   _notificationCenterDelegate = [moduleRegistry getSingletonModuleForName:@"NotificationCenterDelegate"];
-  [_notificationCenterDelegate addDelegate:self];
 }
 
 # pragma mark - UMEventEmitter
@@ -47,12 +46,25 @@ UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
 
 - (void)startObserving
 {
-  _isBeingObserved = YES;
+  [self setIsBeingObserved:YES];
 }
 
 - (void)stopObserving
 {
-  _isBeingObserved = NO;
+  [self setIsBeingObserved:NO];
+}
+
+- (void)setIsBeingObserved:(BOOL)isBeingObserved
+{
+  _isBeingObserved = isBeingObserved;
+  BOOL shouldListen = _isBeingObserved;
+  if (shouldListen && !_isListening) {
+    [_notificationCenterDelegate addDelegate:self];
+    _isListening = YES;
+  } else if (!shouldListen && _isListening) {
+    [_notificationCenterDelegate removeDelegate:self];
+    _isListening = NO;
+  }
 }
 
 # pragma mark - EXNotificationsDelegate
@@ -80,6 +92,10 @@ UM_EXPORT_METHOD_AS(getLastNotificationResponseAsync,
 {
   // Silence React Native warning: "Sending ... with no listeners registered."
   // See: https://github.com/expo/expo/pull/10883#pullrequestreview-529183413
+  // While in practice we don't need to verify this, as as of the end of 2020
+  // we wouldn't send any event to JS if we weren't being observed because
+  // we wouldn't be subscribed to the notification center delegate it's nice
+  // to be sure this problem won't ever arise.
   if (_isBeingObserved) {
     [_eventEmitter sendEventWithName:eventName body:body];
   }