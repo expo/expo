75b27ec054cb265e6233fc2f15534b49b5e0946e
cruzach <charliecruzan@gmail.com>
2020-12-17 11:27:05 -0500

    rebuild

--- packages/@unimodules/react-native-adapter/build/Platform.js
+++ packages/@unimodules/react-native-adapter/build/Platform.js
@@ -1,5 +1,5 @@
 import { Platform as ReactNativePlatform } from 'react-native';
-import { isDOMAvailable } from './environment/browser';
+import { isDOMAvailable, canUseEventListeners, canUseViewport } from './environment/browser';
 const Platform = {
     /**
      * Denotes the currently running platform.
@@ -20,6 +20,18 @@ const Platform = {
      * The DOM is not available in native React runtimes and Node.js.
      */
     isDOMAvailable,
+    /**
+     * Denotes if the current environment can attach event listeners
+     * to the window. This will return false in native React
+     * runtimes and Node.js.
+     */
+    canUseEventListeners,
+    /**
+     * Denotes if the current environment can inspect properties of the
+     * screen on which the current window is being rendered. This will
+     * return false in native React runtimes and Node.js.
+     */
+    canUseViewport,
 };
 export default Platform;
 //# sourceMappingURL=Platform.js.map
