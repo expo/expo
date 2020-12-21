4a98ff93ac6a41b37c258d846b67c8f6fbb7cf96
Evan Bacon <baconbrix@gmail.com>
2020-12-21 15:38:19 -0800

    disable splash on android since it doesn't work

--- apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainActivity.java
+++ apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainActivity.java
@@ -35,9 +35,9 @@ public class MainActivity extends DevMenuAwareReactActivity {
       @Override
       protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    // SplashScreen.show(...) has to be called after super.onCreate(...)
-    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.COVER, ReactRootView.class, false);
+        // SplashScreen.show(...) has to be called after super.onCreate(...)
+        // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
+        // SplashScreen.show(this, SplashScreenImageResizeMode.COVER, ReactRootView.class, false);
       }
     };
 
