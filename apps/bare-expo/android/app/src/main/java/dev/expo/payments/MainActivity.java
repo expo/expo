750636197609f51f911246d4957c2a4f9f1e0bf3
Evan Bacon <baconbrix@gmail.com>
2020-12-21 15:06:35 -0800

    expo eject --no-install -p android

--- apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainActivity.java
+++ apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainActivity.java
@@ -10,6 +10,9 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.devlauncher.DevLauncherController;
 import expo.modules.devmenu.react.DevMenuAwareReactActivity;
 
+import expo.modules.splashscreen.singletons.SplashScreen;
+import expo.modules.splashscreen.SplashScreenImageResizeMode;
+
 public class MainActivity extends DevMenuAwareReactActivity {
 
   /**
@@ -32,6 +35,9 @@ public class MainActivity extends DevMenuAwareReactActivity {
       @Override
       protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    // SplashScreen.show(...) has to be called after super.onCreate(...)
+    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
+    SplashScreen.show(this, SplashScreenImageResizeMode.COVER, ReactRootView.class, false);
       }
     };
 
