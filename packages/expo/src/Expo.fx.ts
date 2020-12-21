d1522d97b3bbae8e8662df473e41499190e520c6
Eric Samelson <esamelson@users.noreply.github.com>
2020-12-21 11:41:37 -0800

    Apply suggestions from code review
    Co-authored-by: James Ide <ide@users.noreply.github.com>

--- packages/expo/src/Expo.fx.ts
+++ packages/expo/src/Expo.fx.ts
@@ -11,8 +11,8 @@ if (NativeModulesProxy.ExpoUpdates?.isMissingRuntimeVersion) {
     Platform.select({ ios: 'Expo.plist', android: 'AndroidManifest.xml' }) +
     ' before OTA updates will work properly.';
   if (__DEV__) {
-    console.warn(`Warning: ${message}`);
+    console.warn(message);
   } else {
-    throw new Error(`Error: ${message}`);
+    throw new Error(message);
   }
 }
