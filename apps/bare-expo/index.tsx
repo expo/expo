76d1063aea7f424d8a8b71ce63548b7239dc79d6
Evan Bacon <baconbrix@gmail.com>
2020-12-21 21:00:25 -0800

    Update index.tsx

--- apps/bare-expo/index.tsx
+++ apps/bare-expo/index.tsx
@@ -1,4 +1,5 @@
-import registerRootComponent from 'expo/build/launch/registerRootComponent';
+// @ts-ignore
+import registerRootComponent from '~expo/build/launch/registerRootComponent';
 
 import App from './App';
 
