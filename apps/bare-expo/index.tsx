3c197674c721e0d71c5e7e247e6a81db776ed732
Evan Bacon <baconbrix@gmail.com>
2020-12-21 17:00:04 -0800

    use registerRootComponent for bare-expo entry

--- apps/bare-expo/index.tsx
+++ apps/bare-expo/index.tsx
@@ -1,11 +1,8 @@
-import { AppRegistry, Platform } from 'react-native';
+import { registerRootComponent } from 'expo';
 
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
-
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication(appName, { rootTag });
-}
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
