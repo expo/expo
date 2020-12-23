5b99f5a64c2e21100035d9f6e110e4b8c9d733dc
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-12-24 00:02:03 +0100

    [dev-menu] Fix import of the umbrella header

--- packages/expo-dev-menu/ios/Headers/EXDevMenu.h
+++ packages/expo-dev-menu/ios/Headers/EXDevMenu.h
@@ -1,5 +1,5 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 
-#if __has_include("EXDevMenu-umbrella.h")
-#import "EXDevMenu-umbrella.h"
+#if __has_include(<EXDevMenu/expo-dev-menu-umbrella.h>)
+#import <EXDevMenu/expo-dev-menu-umbrella.h>
 #endif
