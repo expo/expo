3c60d22ae974c864b250a1e7ab31ad5c68a5fed8
Stanisław Chmiela <sjchmiela@gmail.com>
2020-12-31 10:53:15 +0100

    [unimodules-sensors-interface] Add explanatory console.warn to the deprecated package

--- packages/unimodules-sensors-interface/index.js
+++ packages/unimodules-sensors-interface/index.js
@@ -1,1 +1,5 @@
+console.warn(
+  '[unimodules-sensors-interface] This package has been deprecated in favor of keeping all relevant interfaces directly in expo-sensors. It should not be needed in projects depending on expo-sensors@>9.2.1. If you depend on it directly you should be safe to remove the dependency requirement.'
+);
+
 module.exports = null;
