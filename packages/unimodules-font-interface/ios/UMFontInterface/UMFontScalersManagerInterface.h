70727ad09329dc3432ba3ca1be7bdd278daa74ae
Stanisław Chmiela <sjchmiela@gmail.com>
2020-12-31 14:15:45 +0100

    [unimodules-font-interface] Remove package content

--- packages/unimodules-font-interface/ios/UMFontInterface/UMFontScalersManagerInterface.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2018-present 650 Industries. All rights reserved.
-
-#import <UMFontInterface/UMFontScalerInterface.h>
-
-@protocol UMFontScalersManagerInterface
-
-- (void)registerFontScaler:(id<UMFontScalerInterface>)scaler;
-
-@end
