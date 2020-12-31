64ffb7a3db2919ba38717a20d1bb0eabaff3c05c
Stanisław Chmiela <sjchmiela@gmail.com>
2020-12-31 14:15:59 +0100

    [unimodules-font-interface] Add deprecation warning

--- packages/unimodules-font-interface/index.js
+++ packages/unimodules-font-interface/index.js
@@ -1,1 +1,5 @@
+onsole.warn(
+  '[unimodules-font-interface] This package has been deprecated in favor of keeping all relevant interfaces directly in expo-font. It should not be needed in projects depending on expo-font@>8.4.0. If you depend on it directly you should be safe to remove the dependency requirement.'
+);
+
 module.exports = null;
