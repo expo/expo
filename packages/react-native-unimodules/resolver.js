759e0b60340186212a607f958f7795242db256a5
Evan Bacon <baconbrix@gmail.com>
2021-01-04 12:37:35 -0800

    format

--- packages/react-native-unimodules/resolver.js
+++ packages/react-native-unimodules/resolver.js
@@ -6,17 +6,15 @@ const findUp = require('find-up');
 const resolveFrom = require('resolve-from');
 
 if (module === require.main) {
-    (async () => {
-        const projectRoot = await findUp('package.json')
-        const args = process.argv.slice(2);
-        const moduleName = args[0];
-        let modulePath = resolveFrom.silent(projectRoot, moduleName);
-        if (modulePath) {
-            const moduleRootPkgJson = await findUp('package.json', { cwd: modulePath})
-            const moduleRoot = path.dirname(moduleRootPkgJson)
-            modulePath = moduleRoot
-            // modulePath = path.relative(projectRoot, moduleRoot)
-        }
-        console.log(modulePath);
-    })()
+  (async () => {
+    const projectRoot = await findUp('package.json');
+    const args = process.argv.slice(2);
+    const moduleName = args[0];
+    let modulePath = resolveFrom.silent(projectRoot, moduleName);
+    if (modulePath) {
+      const moduleRootPkgJson = await findUp('package.json', { cwd: modulePath });
+      modulePath = path.dirname(moduleRootPkgJson);
+    }
+    console.log(modulePath);
+  })();
 }
--- packages/react-native-unimodules/workspace-root.js
+++ packages/react-native-unimodules/workspace-root.js
@@ -6,21 +6,13 @@ const findUp = require('find-up');
 const findYarnWorkspaceRoot = require('find-yarn-workspace-root');
 
 if (module === require.main) {
-    (async () => {
-        const moduleRootPkgJson = await findUp('package.json')
-        const projectRoot = path.dirname(moduleRootPkgJson)
-        const root = findYarnWorkspaceRoot(projectRoot);
-        const rootNodeModules =path.join(root, 'node_modules')
-            const modulePathRelative = path.relative(process.cwd(),rootNodeModules)
-        
-        // const args = process.argv.slice(2);
-        // const moduleName = args[0];
-        // let modulePath = resolveFrom.silent(projectRoot, moduleName);
-        // if (modulePath) {
-        //     const moduleRootPkgJson = await findUp('package.json', { cwd: modulePath})
-        //     const moduleRoot = path.dirname(moduleRootPkgJson)
-        //     modulePath = moduleRoot
-        // }
-        console.log(modulePathRelative);
-    })()
+  (async () => {
+    const moduleRootPkgJson = await findUp('package.json');
+    const projectRoot = path.dirname(moduleRootPkgJson);
+    const root = findYarnWorkspaceRoot(projectRoot);
+    const rootNodeModules = path.join(root, 'node_modules');
+    const modulePathRelative = path.relative(process.cwd(), rootNodeModules);
+
+    console.log(modulePathRelative);
+  })();
 }
