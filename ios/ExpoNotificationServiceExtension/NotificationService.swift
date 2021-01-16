073f23b1399d8449c645e9ce5a648dfb1e02702c
cruzach <charliecruzan@gmail.com>
2021-01-16 14:52:50 -0500

    Scope category ID in client if provided

--- ios/ExpoNotificationServiceExtension/NotificationService.swift
+++ ios/ExpoNotificationServiceExtension/NotificationService.swift
@@ -1,36 +1,39 @@
-//
-//  NotificationService.swift
-//  ExpoNotificationServiceExtension
-//
-//  Created by Charlie Cruzan on 1/16/21.
-//  Copyright © 2021 650 Industries. All rights reserved.
-//
+/*
+ * This class allows you to intercept and mutate incoming remote notifications.
+ * didReceive() has ~30 seconds to modify the payload and call the contentHandler,
+ * otherwise serviceExtensionTimeWillExpire() will be triggered.
+ *
+ * The notification payload must contain:
+ *    "mutable-content" : 1
+ *    "alert" : { ... }
+ * to trigger this handler.
+ */
 
 import UserNotifications
 
 class NotificationService: UNNotificationServiceExtension {
-
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
+  
+  var contentHandler: ((UNNotificationContent) -> Void)?
+  var bestAttemptContent: UNMutableNotificationContent?
+  
+  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    self.contentHandler = contentHandler
+    bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
     
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
+    if let bestAttemptContent = bestAttemptContent {
+      // Modify notification content here...
+      if (!request.content.categoryIdentifier.isEmpty && (request.content.userInfo["experienceId"]) != nil) {
+        bestAttemptContent.categoryIdentifier = "\(request.content.userInfo["experienceId"] as! String)-\(request.content.categoryIdentifier)"
+      }
+      contentHandler(bestAttemptContent)
     }
-
+  }
+  
+  override func serviceExtensionTimeWillExpire() {
+    // Called just before the extension will be terminated by the system.
+    if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+      contentHandler(bestAttemptContent)
+    }
+  }
+  
 }
