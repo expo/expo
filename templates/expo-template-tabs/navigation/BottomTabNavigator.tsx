bf8bd1a9a6dd5806ae6566bd442c99159cba0eb2
Brent Vatne <brentvatne@gmail.com>
2021-01-16 18:46:43 -0800

    [templates] Fix TabBarIcon type

--- templates/expo-template-tabs/navigation/BottomTabNavigator.tsx
+++ templates/expo-template-tabs/navigation/BottomTabNavigator.tsx
@@ -38,7 +38,7 @@ export default function BottomTabNavigator() {
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
-function TabBarIcon(props: { name: string; color: string }) {
+function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
