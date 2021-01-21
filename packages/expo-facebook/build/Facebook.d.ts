e48cf0b8557af0a7621c6ffb734c0f3fd08b74bd
Stan Wu <dreamolight@fb.com>
2021-01-21 00:23:04 -0800

    [expo-facebook] Updated version of Facebook SDK to 9.0 and provide wrapper function for setAdvertiserTrackingEnabled

--- packages/expo-facebook/build/Facebook.d.ts
+++ packages/expo-facebook/build/Facebook.d.ts
@@ -12,6 +12,20 @@ export declare function getAuthenticationCredentialAsync(): Promise<FacebookAuth
  */
 export declare function logOutAsync(): Promise<void>;
 /**
+ * Sets whether Facebook SDK should enable advertising tracking,
+ * (more info [here](https://developers.facebook.com/docs/app-events/guides/advertising-tracking-enabled)).
+ *
+ * Limitations:
+ * 1. AdvertiserTrackingEnabled is only available for iOS 14+.
+ * 2. For iOS 13 or earlier, AdvertiserTrackingEnabled uses the Limit Ad Tracking setting as its value.
+ *
+ * This method corresponds to [this](https://developers.facebook.com/docs/app-events/guides/advertising-tracking-enabled)
+ *
+ * @param enabled Whether advertising tracking of the Facebook SDK should be enabled
+ * @return Whether the value is set successfully. It will always return false in Android, iOS 13 and below.
+ */
+export declare function setAdvertiserTrackingEnabledAsync(enabled: boolean): Promise<boolean>;
+/**
  * Sets whether Facebook SDK should log app events. App events involve eg. app installs,
  * app launches (more info [here](https://developers.facebook.com/docs/app-events/getting-started-app-events-android/#auto-events)
  * and [here](https://developers.facebook.com/docs/app-events/getting-started-app-events-ios#auto-events)).
--- packages/expo-facebook/ios/EXFacebook.podspec
+++ packages/expo-facebook/ios/EXFacebook.podspec
@@ -15,8 +15,8 @@ Pod::Spec.new do |s|
 
   s.dependency 'UMCore'
   s.dependency 'UMConstantsInterface'
-  s.dependency 'FBSDKCoreKit', '~> 5.8.0'
-  s.dependency 'FBSDKLoginKit', '~> 5.8.0'
+  s.dependency 'FacebookSDK/CoreKit', '~> 9.0'
+  s.dependency 'FacebookSDK/LoginKit', '~> 9.0'
 
   if !$ExpoUseSources&.include?(package['name']) && ENV['EXPO_USE_SOURCE'].to_i == 0 && File.exist?("#{s.name}.xcframework") && Gem::Version.new(Pod::VERSION) >= Gem::Version.new('1.10.0')
     s.source_files = "#{s.name}/**/*.h"
