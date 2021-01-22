aa88eaee97fa4be61836efb0c9acea5893f13760
Eric Samelson <eric@expo.io>
2021-01-21 17:54:28 -0800

    [android] add SelectionPolicyFilterAware with tests

---
+++ packages/expo-updates/android/src/androidTest/java/expo/modules/updates/launcher/SelectionPolicyFilterAwareTest.java
@@ -0,0 +1,113 @@
+package expo.modules.updates.launcher;
+
+import android.net.Uri;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import expo.modules.updates.UpdatesConfiguration;
+import expo.modules.updates.db.entity.UpdateEntity;
+import expo.modules.updates.manifest.Manifest;
+import expo.modules.updates.manifest.NewManifest;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class SelectionPolicyFilterAwareTest {
+  JSONObject manifestFilters;
+  SelectionPolicyFilterAware selectionPolicy;
+
+  UpdateEntity updateDefault1;
+  UpdateEntity updateDefault2;
+  UpdateEntity updateRollout1;
+  UpdateEntity updateRollout2;
+
+  @Before
+  public void setup() throws JSONException {
+    manifestFilters = new JSONObject("{\"releaseName\": \"rollout\"}");
+    selectionPolicy = new SelectionPolicyFilterAware("1.0");
+
+    HashMap<String, Object> configMap = new HashMap<>();
+    configMap.put("updateUrl", Uri.parse("https://exp.host/@test/test"));
+    UpdatesConfiguration config = new UpdatesConfiguration().loadValuesFromMap(configMap);
+
+    JSONObject manifestJsonDefault1 = new JSONObject("{\"manifest\":{\"id\":\"079cde35-8433-4c17-81c8-7117c1513e72\",\"createdAt\":\"2021-01-11T19:39:22.480Z\",\"runtimeVersion\":\"1.0\",\"launchAsset\":{\"hash\":\"DW5MBgKq155wnX8rCP1lnsW6BsTbfKLXxGXRQx1RcOA\",\"key\":\"0436e5821bff7b95a84c21f22a43cb96.bundle\",\"contentType\":\"application/javascript\",\"url\":\"https://url.to/bundle\"},\"assets\":[{\"hash\":\"JSeRsPNKzhVdHP1OEsDVsLH500Zfe4j1O7xWfa14oBo\",\"key\":\"3261e570d51777be1e99116562280926.png\",\"contentType\":\"image/png\",\"url\":\"https://url.to/asset\"}],\"updateMetadata\":{\"releaseName\":\"default\"}},\"manifestFilters\":{\"releaseName\":\"default\"},\"protocolVersion\":0}");
+    updateDefault1 = NewManifest.fromManifestJson(manifestJsonDefault1, config).getUpdateEntity();
+
+    JSONObject manifestJsonRollout1 = new JSONObject("{\"manifest\":{\"id\":\"079cde35-8433-4c17-81c8-7117c1513e73\",\"createdAt\":\"2021-01-12T19:39:22.480Z\",\"runtimeVersion\":\"1.0\",\"launchAsset\":{\"hash\":\"DW5MBgKq155wnX8rCP1lnsW6BsTbfKLXxGXRQx1RcOA\",\"key\":\"0436e5821bff7b95a84c21f22a43cb96.bundle\",\"contentType\":\"application/javascript\",\"url\":\"https://url.to/bundle\"},\"assets\":[{\"hash\":\"JSeRsPNKzhVdHP1OEsDVsLH500Zfe4j1O7xWfa14oBo\",\"key\":\"3261e570d51777be1e99116562280926.png\",\"contentType\":\"image/png\",\"url\":\"https://url.to/asset\"}],\"updateMetadata\":{\"releaseName\":\"rollout\"}},\"manifestFilters\":{\"releaseName\":\"rollout\"},\"protocolVersion\":0}");
+    updateRollout1 = NewManifest.fromManifestJson(manifestJsonRollout1, config).getUpdateEntity();
+
+    JSONObject manifestJsonDefault2 = new JSONObject("{\"manifest\":{\"id\":\"079cde35-8433-4c17-81c8-7117c1513e74\",\"createdAt\":\"2021-01-13T19:39:22.480Z\",\"runtimeVersion\":\"1.0\",\"launchAsset\":{\"hash\":\"DW5MBgKq155wnX8rCP1lnsW6BsTbfKLXxGXRQx1RcOA\",\"key\":\"0436e5821bff7b95a84c21f22a43cb96.bundle\",\"contentType\":\"application/javascript\",\"url\":\"https://url.to/bundle\"},\"assets\":[{\"hash\":\"JSeRsPNKzhVdHP1OEsDVsLH500Zfe4j1O7xWfa14oBo\",\"key\":\"3261e570d51777be1e99116562280926.png\",\"contentType\":\"image/png\",\"url\":\"https://url.to/asset\"}],\"updateMetadata\":{\"releaseName\":\"default\"}},\"manifestFilters\":{\"releaseName\":\"default\"},\"protocolVersion\":0}");
+    updateDefault2 = NewManifest.fromManifestJson(manifestJsonDefault2, config).getUpdateEntity();
+
+    JSONObject manifestJsonRollout2 = new JSONObject("{\"manifest\":{\"id\":\"079cde35-8433-4c17-81c8-7117c1513e75\",\"createdAt\":\"2021-01-14T19:39:22.480Z\",\"runtimeVersion\":\"1.0\",\"launchAsset\":{\"hash\":\"DW5MBgKq155wnX8rCP1lnsW6BsTbfKLXxGXRQx1RcOA\",\"key\":\"0436e5821bff7b95a84c21f22a43cb96.bundle\",\"contentType\":\"application/javascript\",\"url\":\"https://url.to/bundle\"},\"assets\":[{\"hash\":\"JSeRsPNKzhVdHP1OEsDVsLH500Zfe4j1O7xWfa14oBo\",\"key\":\"3261e570d51777be1e99116562280926.png\",\"contentType\":\"image/png\",\"url\":\"https://url.to/asset\"}],\"updateMetadata\":{\"releaseName\":\"rollout\"}},\"manifestFilters\":{\"releaseName\":\"rollout\"},\"protocolVersion\":0}");
+    updateRollout2 = NewManifest.fromManifestJson(manifestJsonRollout2, config).getUpdateEntity();
+  }
+
+  @Test
+  public void testSelectUpdateToLaunch() {
+    // should pick the newest update that matches the manifest filters
+    UpdateEntity expected = updateRollout1;
+    UpdateEntity actual = selectionPolicy.selectUpdateToLaunch(Arrays.asList(updateDefault1, expected, updateDefault2), manifestFilters);
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testSelectUpdatesToDelete_OlderMatching() {
+    // if there is an older update that matches the manifest filters, keep that one over any newer ones that don't match
+    List<UpdateEntity> updatesToDelete = selectionPolicy.selectUpdatesToDelete(Arrays.asList(updateDefault1, updateRollout1, updateDefault2, updateRollout2), updateRollout2, manifestFilters);
+
+    Assert.assertEquals(2, updatesToDelete.size());
+    Assert.assertTrue(updatesToDelete.contains(updateDefault1));
+    Assert.assertFalse(updatesToDelete.contains(updateRollout1));
+    Assert.assertTrue(updatesToDelete.contains(updateDefault2));
+    Assert.assertFalse(updatesToDelete.contains(updateRollout2));
+  }
+
+  @Test
+  public void testSelectUpdatesToDelete_NoneOlderMatching() {
+    // if there is no older update that matches the manifest filters, just keep the next newest one
+    List<UpdateEntity> updatesToDelete = selectionPolicy.selectUpdatesToDelete(Arrays.asList(updateDefault1, updateDefault2, updateRollout2), updateRollout2, manifestFilters);
+
+    Assert.assertEquals(1, updatesToDelete.size());
+    Assert.assertTrue(updatesToDelete.contains(updateDefault1));
+    Assert.assertFalse(updatesToDelete.contains(updateDefault2));
+    Assert.assertFalse(updatesToDelete.contains(updateRollout2));
+  }
+
+  @Test
+  public void testShouldLoadNewUpdate_NoneMatchingFilters() {
+    // should choose to load an older update if the current update doesn't match the manifest filters
+    boolean actual = selectionPolicy.shouldLoadNewUpdate(updateRollout1, updateDefault2, manifestFilters);
+    Assert.assertTrue(actual);
+  }
+
+  @Test
+  public void testShouldLoadNewUpdate_NewerExists() {
+    boolean actual = selectionPolicy.shouldLoadNewUpdate(updateRollout1, updateRollout2, manifestFilters);
+    Assert.assertFalse(actual);
+  }
+
+  @Test
+  public void testShouldLoadNewUpdate_OlderExistsMatchingFilters() {
+    // should not choose to load a new update that doesn't match the manifest filters
+    // if there is an existing older update that matches the filters
+    boolean actual = selectionPolicy.shouldLoadNewUpdate(updateDefault2, updateRollout1, manifestFilters);
+    Assert.assertFalse(actual);
+  }
+
+  @Test
+  public void testShouldLoadNewUpdate_NoneExistsMatchingFilters() {
+    // should choose to load a new update that doesn't match the manifest filters
+    // if no existing updates match the manifest filters
+    boolean actual = selectionPolicy.shouldLoadNewUpdate(updateDefault1, null, manifestFilters);
+    Assert.assertTrue(actual);
+  }
+}
---
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/launcher/SelectionPolicyFilterAware.java
@@ -0,0 +1,132 @@
+package expo.modules.updates.launcher;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import expo.modules.updates.db.entity.UpdateEntity;
+import expo.modules.updates.manifest.Manifest;
+
+/**
+ * Update selection policy which chooses an update to launch based on the manifest filters
+ * provided by the server. If multiple updates meet the criteria, the newest one is chosen, but the
+ * manifest filters are always taken into account before the commit time.
+ */
+public class SelectionPolicyFilterAware implements SelectionPolicy {
+
+  private static final String TAG = SelectionPolicyFilterAware.class.getSimpleName();
+
+  private List<String> mRuntimeVersions;
+
+  public SelectionPolicyFilterAware(List<String> runtimeVersions) {
+    mRuntimeVersions = runtimeVersions;
+  }
+
+  public SelectionPolicyFilterAware(String runtimeVersion) {
+    this(Collections.singletonList(runtimeVersion));
+  }
+
+  @Override
+  public UpdateEntity selectUpdateToLaunch(List<UpdateEntity> updates, JSONObject filters) {
+    UpdateEntity updateToLaunch = null;
+    for (UpdateEntity update : updates) {
+      if (!mRuntimeVersions.contains(update.runtimeVersion) || isUpdateManifestFiltered(update, filters)) {
+        continue;
+      }
+      if (updateToLaunch == null || updateToLaunch.commitTime.before(update.commitTime)) {
+        updateToLaunch = update;
+      }
+    }
+    return updateToLaunch;
+  }
+
+  @Override
+  public List<UpdateEntity> selectUpdatesToDelete(List<UpdateEntity> updates, UpdateEntity launchedUpdate, JSONObject filters) {
+    if (launchedUpdate == null) {
+      return new ArrayList<>();
+    }
+
+    List<UpdateEntity> updatesToDelete = new ArrayList<>();
+    // keep the launched update and one other, to be safe and make rollbacks faster
+    // keep the next newest update that matches all the manifest filters, unless no other updates do
+    // in which case, keep the next newest across all updates
+    UpdateEntity nextNewestUpdate = null;
+    UpdateEntity nextNewestUpdateMatchingFilters = null;
+    for (UpdateEntity update : updates) {
+      if (update.commitTime.before(launchedUpdate.commitTime)) {
+        updatesToDelete.add(update);
+        if (nextNewestUpdate == null || nextNewestUpdate.commitTime.before(update.commitTime)) {
+          nextNewestUpdate = update;
+        }
+        if (!isUpdateManifestFiltered(update, filters) &&
+          (nextNewestUpdateMatchingFilters == null ||  nextNewestUpdateMatchingFilters.commitTime.before(update.commitTime))) {
+          nextNewestUpdateMatchingFilters = update;
+        }
+      }
+    }
+
+    if (nextNewestUpdateMatchingFilters != null) {
+      updatesToDelete.remove(nextNewestUpdateMatchingFilters);
+    } else if (nextNewestUpdate != null) {
+      updatesToDelete.remove(nextNewestUpdate);
+    }
+    return updatesToDelete;
+  }
+
+  @Override
+  public boolean shouldLoadNewUpdate(UpdateEntity newUpdate, UpdateEntity launchedUpdate, JSONObject filters) {
+    if (newUpdate == null) {
+      return false;
+    }
+    // if the current update doesn't pass the manifest filters
+    // we should load the new update no matter the commitTime
+    if (launchedUpdate == null || isUpdateManifestFiltered(launchedUpdate, filters)) {
+      return true;
+    }
+    return newUpdate.commitTime.after(launchedUpdate.commitTime);
+  }
+
+  private boolean isUpdateManifestFiltered(UpdateEntity update, JSONObject manifestFilters) {
+    if (manifestFilters == null || update.metadata == null || !update.metadata.has("updateMetadata")) {
+      return false;
+    }
+    try {
+      JSONObject updateMetadata = update.metadata.getJSONObject("updateMetadata");
+      Iterator<String> keySet = manifestFilters.keys();
+      while (keySet.hasNext()) {
+        boolean passes = true;
+        String key = keySet.next();
+        if (updateMetadata.has(key)) {
+          passes = manifestFilters.get(key).equals(updateMetadata.get(key));
+        } else if (key.indexOf('.') > -1) {
+          // manifest filters might have nested keys
+          String[] nestedKeys = key.split(".");
+          JSONObject nestedObject = updateMetadata;
+          for (int i = 0; i < nestedKeys.length; i++) {
+            String nestedKey = nestedKeys[i];
+            if (!nestedObject.has(nestedKey) || !(nestedObject.get(nestedKey) instanceof JSONObject)) {
+              passes = true;
+            } else if (i + 1 == nestedKeys.length) {
+              passes = manifestFilters.get(key).equals(nestedObject.get(nestedKey));
+            } else {
+              nestedObject = nestedObject.getJSONObject(nestedKey);
+            }
+          }
+        }
+
+        if (!passes) {
+          return true;
+        }
+      }
+    } catch (Exception e) {
+      Log.e(TAG, "Error filtering manifest using server data", e);
+      return false;
+    }
+    return false;
+  }
+}
