6ded8414295317864ff15cdf95fac81decaf0dc3
Evan Bacon <baconbrix@gmail.com>
2021-01-22 12:15:11 -0700

    [sdk] convert plugins to typescript (#11715)
    * Converted plugins to TypeScript
    
    * Built packages
    
    * Updated CHANGELOGs

--- packages/expo-brightness/app.plugin.js
+++ packages/expo-brightness/app.plugin.js
@@ -1,8 +1,1 @@
-const pkg = require('./package.json');
-const { createRunOncePlugin, AndroidConfig } = require('@expo/config-plugins');
-
-const withBrightness = config => {
-  return AndroidConfig.Permissions.withPermissions(config, ['android.permission.WRITE_SETTINGS']);
-};
-
-module.exports = createRunOncePlugin(withBrightness, pkg.name, pkg.version);
+module.exports = require('./plugin/build/withBrightness');
