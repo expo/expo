fb8e21d943975471a89fe4a0d027a14495e3e80c
Evan Bacon <baconbrix@gmail.com>
2021-01-26 17:17:40 -0800

    use static configuration for splash screen

--- apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainActivity.java
+++ apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainActivity.java
@@ -12,9 +12,6 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.devlauncher.DevLauncherController;
 import expo.modules.devmenu.react.DevMenuAwareReactActivity;
 
-import expo.modules.splashscreen.singletons.SplashScreen;
-import expo.modules.splashscreen.SplashScreenImageResizeMode;
-
 public class MainActivity extends DevMenuAwareReactActivity {
 
   /**
@@ -38,9 +35,6 @@ public class MainActivity extends DevMenuAwareReactActivity {
       @Override
       protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // SplashScreen.show(...) has to be called after super.onCreate(...)
-        // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-        SplashScreen.show(activity, SplashScreenImageResizeMode.COVER, ReactRootView.class, false);
       }
     };
 
