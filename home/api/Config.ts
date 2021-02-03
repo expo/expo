26cfe7f36445ed298336bf09e8dd4072c77f781d
Will Schurman <wschurman@gmail.com>
2021-02-03 11:24:12 -0800

    [home] Change authentication flows to use expo-web-browser (#11753)

--- home/api/Config.ts
+++ home/api/Config.ts
@@ -1,11 +1,18 @@
 const host = 'exp.host';
-//const host = 'staging.exp.host';
+// const host = 'staging.exp.host';
+// const host = 'localhost:3000';
 //const host = 'ed56a018.ngrok.io';
 const origin = `https://${host}`;
+// const origin = 'http://localhost:3000';
+const websiteOrigin = 'https://expo.io';
+// const websiteOrigin = 'http://localhost:3001';
 
 export default {
   api: {
     host,
     origin,
+  },
+  website: {
+    origin: websiteOrigin,
   },
 };
