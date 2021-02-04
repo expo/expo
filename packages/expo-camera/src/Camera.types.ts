961afca94a9ac9819814643ac756f3265d5249ec
Zeeshan Ali <makzee@gmail.com>
2021-02-04 18:42:19 -0500

    [android][video] ability to modify video bitrate on android (Fixed conflict issues in expo/expo/pull/4878.)

--- packages/expo-camera/src/Camera.types.ts
+++ packages/expo-camera/src/Camera.types.ts
@@ -71,6 +71,8 @@ export type CameraRecordingOptions = {
   quality?: number | string;
   mute?: boolean;
   mirror?: boolean;
+  // Android
+  videoBitrate?: number;
 };
 
 export type CameraCapturedPicture = {
