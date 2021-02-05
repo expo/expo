a963900eb2a2540dd831e2ae82b486ca6fded431
Evan Bacon <baconbrix@gmail.com>
2021-02-05 14:10:12 -0800

    fix lint

--- packages/expo/src/Expo.fx.tsx
+++ packages/expo/src/Expo.fx.tsx
@@ -5,17 +5,17 @@ import './environment/react-native-logs.fx';
 // load expo-asset immediately to set a custom `source` transformer in React Native
 import 'expo-asset';
 
-import React from 'react';
 import { NativeModulesProxy, Platform } from '@unimodules/core';
 import Constants, { ExecutionEnvironment } from 'expo-constants';
+import * as Font from 'expo-font';
+import { installWebGeolocationPolyfill } from 'expo-location';
+import React from 'react';
 import { AppRegistry, StyleSheet } from 'react-native';
+
+import DevAppContainer from './environment/DevAppContainer';
 
 // install globals
 declare let module: any;
-
-import DevAppContainer from './environment/DevAppContainer';
-import { installWebGeolocationPolyfill } from 'expo-location';
-import * as Font from 'expo-font';
 // Represents an app running in the store client or an app built with the legacy `expo build` command.
 // `false` when running in bare workflow, custom dev clients, or `eas build`s (managed or bare).
 // This should be used to ensure code that _should_ exist is treated as such.
@@ -43,7 +43,7 @@ installWebGeolocationPolyfill();
 
 if (module && module.exports && global) {
   try {
-    // This is more risky so we wrap it in a try/catch in bare workflow. 
+    // This is more risky so we wrap it in a try/catch in bare workflow.
     // No one should be using this global anymore and it'll be deprecated.
     const globals = require('./globals');
 
