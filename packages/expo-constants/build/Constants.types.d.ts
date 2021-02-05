bc09f597992846079c342d04639c6854c075dfad
Evan Bacon <baconbrix@gmail.com>
2021-02-05 15:03:37 -0800

    use config-types package for ExpoConfig type (#11810)
    * use config-types package for ExpoConfig type
    
    * Update package.json
    
    * Update CHANGELOG.md
    
    * add ts-ignores

--- packages/expo-constants/build/Constants.types.d.ts
+++ packages/expo-constants/build/Constants.types.d.ts
@@ -1,3 +1,4 @@
+import { ExpoConfig } from '@expo/config-types';
 export declare enum AppOwnership {
     Standalone = "standalone",
     Expo = "expo",
@@ -28,29 +29,11 @@ export interface AndroidManifest {
 export interface WebManifest {
     [key: string]: any;
 }
-export interface AppManifest {
-    name?: string;
-    description?: string;
-    slug?: string;
-    sdkVersion?: string;
-    version?: string;
+export interface AppManifest extends ExpoConfig {
     /** Published Apps Only */
     releaseId?: string;
     revisionId?: string;
     releaseChannel?: string;
-    orientation?: string;
-    primaryColor?: string;
-    icon?: string;
-    notification?: {
-        icon?: string;
-        color?: string;
-        [key: string]: any;
-    };
-    loading?: {
-        icon?: string;
-        [key: string]: any;
-    };
-    entryPoint?: string;
     packagerOpts?: {
         hostType?: string;
         dev?: boolean;
