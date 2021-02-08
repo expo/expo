11ebaa12aef8ff0d9e21526d1982f6faacfa39a9
Evan Bacon <baconbrix@gmail.com>
2021-02-08 12:30:18 -0800

    added comment

--- packages/expo/build/launch/registerRootComponent.js
+++ packages/expo/build/launch/registerRootComponent.js
@@ -3,6 +3,7 @@ import { activateKeepAwake } from 'expo-keep-awake';
 import { AppRegistry } from 'react-native';
 import withExpoRoot from './withExpoRoot';
 if (__DEV__) {
+    // TODO: Make this not a side-effect
     activateKeepAwake();
 }
 export default function registerRootComponent(component) {
