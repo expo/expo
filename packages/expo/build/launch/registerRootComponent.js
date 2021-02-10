6d9cad9b2b251bc721027ca1eff9466b571e3bde
Evan Bacon <baconbrix@gmail.com>
2021-02-09 23:28:55 -0800

    remove keep-awake

--- packages/expo/build/launch/registerRootComponent.js
+++ packages/expo/build/launch/registerRootComponent.js
@@ -1,11 +1,6 @@
 import 'expo/build/Expo.fx';
-import { activateKeepAwake } from 'expo-keep-awake';
 import { AppRegistry } from 'react-native';
 import withExpoRoot from './withExpoRoot';
-if (__DEV__) {
-    // TODO: Make this not a side-effect
-    activateKeepAwake();
-}
 export default function registerRootComponent(component) {
     AppRegistry.registerComponent('main', () => withExpoRoot(component));
 }
--- packages/expo/src/launch/registerRootComponent.tsx
+++ packages/expo/src/launch/registerRootComponent.tsx
@@ -1,15 +1,9 @@
 import 'expo/build/Expo.fx';
-import { activateKeepAwake } from 'expo-keep-awake';
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
 
 import withExpoRoot from './withExpoRoot';
 import { InitialProps } from './withExpoRoot.types';
-
-if (__DEV__) {
-  // TODO: Make this not a side-effect
-  activateKeepAwake();
-}
 
 export default function registerRootComponent<P extends InitialProps>(
   component: React.ComponentType<P>
