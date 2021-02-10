d33be4c3fbaf8e152100c6e89310bfc15a0a6115
Evan Bacon <baconbrix@gmail.com>
2021-02-10 15:03:04 -0800

    [expo] Combine web and native registerRootComponent (#11871)
    * Combine web and native registerRootComponent
    
    * fix lint

--- packages/expo/src/launch/registerRootComponent.tsx
+++ packages/expo/src/launch/registerRootComponent.tsx
@@ -1,6 +1,6 @@
 import 'expo/build/Expo.fx';
 import * as React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 
 import withExpoRoot from './withExpoRoot';
 import { InitialProps } from './withExpoRoot.types';
@@ -9,4 +9,8 @@ export default function registerRootComponent<P extends InitialProps>(
   component: React.ComponentType<P>
 ): void {
   AppRegistry.registerComponent('main', () => withExpoRoot(component));
+  if (Platform.OS === 'web') {
+    const rootTag = document.getElementById('root') ?? document.getElementById('main');
+    AppRegistry.runApplication('main', { rootTag });
+  }
 }
