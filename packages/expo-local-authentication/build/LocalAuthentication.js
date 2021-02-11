b873d54df6cf785ab4509d1053f28eb988d5f2ff
Evan Bacon <baconbrix@gmail.com>
2021-02-10 16:11:47 -0800

    [local-auth] Remove deprecation warning for authenticateAsync

--- packages/expo-local-authentication/build/LocalAuthentication.js
+++ packages/expo-local-authentication/build/LocalAuthentication.js
@@ -31,11 +31,6 @@ export async function authenticateAsync(options = {}) {
     if (!ExpoLocalAuthentication.authenticateAsync) {
         throw new UnavailabilityError('expo-local-authentication', 'authenticateAsync');
     }
-    // Warn if using an old API - to be removed in SDK35.
-    if (typeof options === 'string') {
-        console.warn('String argument in LocalAuthentication.authenticateAsync has been deprecated. Please use options object with `promptMessage` key instead.');
-        options = { promptMessage: options };
-    }
     if (options.hasOwnProperty('promptMessage')) {
         invariant(typeof options.promptMessage === 'string' && options.promptMessage.length, 'LocalAuthentication.authenticateAsync : `options.promptMessage` must be a non-empty string.');
     }
