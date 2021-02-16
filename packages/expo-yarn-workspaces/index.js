54d54e907dad91fde7a9a5f7939e6bf9780186fe
Evan Bacon <baconbrix@gmail.com>
2021-02-16 12:22:05 -0800

    Use @expo/metro-config directly

--- packages/expo-yarn-workspaces/index.js
+++ packages/expo-yarn-workspaces/index.js
@@ -3,7 +3,9 @@
 const debug = require('debug')('workspaces');
 const findYarnWorkspaceRoot = require('find-yarn-workspace-root');
 const fs = require('fs');
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
+// TODO: Use the vendored metro config in a future version after SDK 41 is released
+// const { getDefaultConfig } = require('expo/metro-config');
 const blacklist = require('metro-config/src/defaults/blacklist');
 const { assetExts } = require('metro-config/src/defaults/defaults');
 const path = require('path');
