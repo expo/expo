3e6ad89adc2319f7305659005c2be520c3850d0f
Evan Bacon <baconbrix@gmail.com>
2021-02-22 11:49:48 -0800

    Added remote-notification to plugin

--- packages/expo-notifications/plugin/src/withNotificationsIOS.ts
+++ packages/expo-notifications/plugin/src/withNotificationsIOS.ts
@@ -1,9 +1,33 @@
-import { ConfigPlugin, withEntitlementsPlist } from '@expo/config-plugins';
+import { ConfigPlugin, withEntitlementsPlist, withInfoPlist } from '@expo/config-plugins';
+
+/**
+ * Add `remote-notification` to UIBackgroundModes in the iOS Info.plist.
+ *
+ * Without this, you'll see the runtime warning:
+ * You've implemented `-[<UIApplicationDelegate> application:didReceiveRemoteNotification:fetchCompletionHandler:]`, but you still need to add "remote-notification" to the list of your supported UIBackgroundModes in your Info.plist.
+ *
+ * @param config
+ */
+export const withRemoteNotificationBackgroundMode: ConfigPlugin = config => {
+  return withInfoPlist(config, config => {
+    const infoPlist = config.modResults as any;
+    if (!Array.isArray(infoPlist.UIBackgroundModes)) {
+      infoPlist.UIBackgroundModes = [];
+    }
+    if (!infoPlist.UIBackgroundModes.includes('remote-notification')) {
+      infoPlist.UIBackgroundModes.push('remote-notification');
+    }
+    config.modResults = infoPlist;
+    return config;
+  });
+};
 
 export const withNotificationsIOS: ConfigPlugin<{ mode: 'production' | 'development' }> = (
   config,
   { mode }
 ) => {
+  config = withRemoteNotificationBackgroundMode(config);
+
   return withEntitlementsPlist(config, config => {
     config.modResults['aps-environment'] = mode;
     return config;
