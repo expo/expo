c163d5ed1991fd086a8407bf98c5ebe17c6f6cba
Evan Bacon <baconbrix@gmail.com>
2021-02-22 11:10:41 -0800

    Update ErrorScreen.tsx

--- apps/native-component-list/src/screens/ErrorScreen.tsx
+++ apps/native-component-list/src/screens/ErrorScreen.tsx
@@ -7,17 +7,23 @@ export default function ErrorScreen() {
   return (
     <Page>
       <Section title="Errors">
-        <Button title="JavaScript error" onPress={() => {
-          // Should present a full screen error message that can be dismissed.
+        <Button
+          title="JavaScript error"
+          onPress={() => {
+            // Should present a full screen error message that can be dismissed.
             const foo = {};
             // @ts-ignore: should error out
-            const b = foo.bar.lol; 
+            const b = foo.bar.lol;
             console.log(b);
-        }} />
-        <Button title="Console error" onPress={() => {
+          }}
+        />
+        <Button
+          title="Console error"
+          onPress={() => {
             // Should present a toast.
-            console.error(new Error('Hello'))
-        }} />
+            console.error(new Error('Hello'));
+          }}
+        />
       </Section>
     </Page>
   );
