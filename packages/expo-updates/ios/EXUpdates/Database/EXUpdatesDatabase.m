d579157d78cd36955eaacfaef29de7da93705b07
Eric Samelson <eric@expo.io>
2021-03-03 10:41:13 -0800

    [ios] split some database methods into EXUpdatesDatabaseUtils

--- packages/expo-updates/ios/EXUpdates/Database/EXUpdatesDatabase.m
+++ packages/expo-updates/ios/EXUpdates/Database/EXUpdatesDatabase.m
@@ -1,6 +1,7 @@
 //  Copyright © 2019 650 Industries. All rights reserved.
 
 #import <EXUpdates/EXUpdatesDatabase.h>
+#import <EXUpdates/EXUpdatesDatabaseUtils.h>
 
 #import <sqlite3.h>
 
@@ -549,138 +550,12 @@ static NSString * const EXUpdatesDatabaseServerDefinedHeadersKey = @"serverDefin
 {
   NSAssert(_db, @"Missing database handle");
   dispatch_assert_queue(_databaseQueue);
-  sqlite3_stmt *stmt;
-  if (sqlite3_prepare_v2(_db, [sql UTF8String], -1, &stmt, NULL) != SQLITE_OK) {
-    if (error != nil) {
-      *error = [self _errorFromSqlite:_db];
-    }
-    return nil;
-  }
-  if (args) {
-    if (![self _bindStatement:stmt withArgs:args]) {
-      if (error != nil) {
-        *error = [self _errorFromSqlite:_db];
-      }
-      return nil;
-    }
-  }
-
-  NSMutableArray *rows = [NSMutableArray arrayWithCapacity:0];
-  NSMutableArray *columnNames = [NSMutableArray arrayWithCapacity:0];
-
-  int columnCount = 0;
-  BOOL didFetchColumns = NO;
-  int result;
-  BOOL hasMore = YES;
-  BOOL didError = NO;
-  while (hasMore) {
-    result = sqlite3_step(stmt);
-    switch (result) {
-      case SQLITE_ROW: {
-        if (!didFetchColumns) {
-          // get all column names once at the beginning
-          columnCount = sqlite3_column_count(stmt);
-
-          for (int i = 0; i < columnCount; i++) {
-            [columnNames addObject:[NSString stringWithUTF8String:sqlite3_column_name(stmt, i)]];
-          }
-          didFetchColumns = YES;
-        }
-        NSMutableDictionary *entry = [NSMutableDictionary dictionary];
-        for (int i = 0; i < columnCount; i++) {
-          id columnValue = [self _getValueWithStatement:stmt column:i];
-          entry[columnNames[i]] = columnValue;
-        }
-        [rows addObject:entry];
-        break;
-      }
-      case SQLITE_DONE:
-        hasMore = NO;
-        break;
-      default:
-        didError = YES;
-        hasMore = NO;
-        break;
-    }
-  }
-
-  if (didError && error != nil) {
-    *error = [self _errorFromSqlite:_db];
-  }
-
-  sqlite3_finalize(stmt);
-
-  return didError ? nil : rows;
-}
-
-- (id)_getValueWithStatement:(sqlite3_stmt *)stmt column:(int)column
-{
-  int columnType = sqlite3_column_type(stmt, column);
-  switch (columnType) {
-    case SQLITE_INTEGER:
-      return @(sqlite3_column_int64(stmt, column));
-    case SQLITE_FLOAT:
-      return @(sqlite3_column_double(stmt, column));
-    case SQLITE_BLOB:
-      NSAssert(sqlite3_column_bytes(stmt, column) == 16, @"SQLite BLOB value should be a valid UUID");
-      return [[NSUUID alloc] initWithUUIDBytes:sqlite3_column_blob(stmt, column)];
-    case SQLITE_TEXT:
-      return [[NSString alloc] initWithBytes:(char *)sqlite3_column_text(stmt, column)
-                                      length:sqlite3_column_bytes(stmt, column)
-                                    encoding:NSUTF8StringEncoding];
-  }
-  return [NSNull null];
-}
-
-- (BOOL)_bindStatement:(sqlite3_stmt *)stmt withArgs:(NSArray *)args
-{
-  __block BOOL success = YES;
-  [args enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-    if ([obj isKindOfClass:[NSUUID class]]) {
-      uuid_t bytes;
-      [((NSUUID *)obj) getUUIDBytes:bytes];
-      if (sqlite3_bind_blob(stmt, (int)idx + 1, bytes, 16, SQLITE_TRANSIENT) != SQLITE_OK) {
-        success = NO;
-        *stop = YES;
-      }
-    } else if ([obj isKindOfClass:[NSNumber class]]) {
-      if (sqlite3_bind_int64(stmt, (int)idx + 1, [((NSNumber *)obj) longLongValue]) != SQLITE_OK) {
-        success = NO;
-        *stop = YES;
-      }
-    } else if ([obj isKindOfClass:[NSDictionary class]]) {
-      NSError *error;
-      NSData *jsonData = [NSJSONSerialization dataWithJSONObject:(NSDictionary *)obj options:kNilOptions error:&error];
-      if (!error && sqlite3_bind_text(stmt, (int)idx + 1, jsonData.bytes, (int)jsonData.length, SQLITE_TRANSIENT) != SQLITE_OK) {
-        success = NO;
-        *stop = YES;
-      }
-    } else if ([obj isKindOfClass:[NSNull class]]) {
-      if (sqlite3_bind_null(stmt, (int)idx + 1) != SQLITE_OK) {
-        success = NO;
-        *stop = YES;
-      }
-    } else {
-      // convert to string
-      NSString *string = [obj isKindOfClass:[NSString class]] ? (NSString *)obj : [obj description];
-      NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
-      if (sqlite3_bind_text(stmt, (int)idx + 1, data.bytes, (int)data.length, SQLITE_TRANSIENT) != SQLITE_OK) {
-        success = NO;
-        *stop = YES;
-      }
-    }
-  }];
-  return success;
+  return [EXUpdatesDatabaseUtils executeSql:sql withArgs:args onDatabase:_db error:error];
 }
 
 - (NSError *)_errorFromSqlite:(struct sqlite3 *)db
 {
-  int code = sqlite3_errcode(db);
-  int extendedCode = sqlite3_extended_errcode(db);
-  NSString *message = [NSString stringWithUTF8String:sqlite3_errmsg(db)];
-  return [NSError errorWithDomain:EXUpdatesDatabaseErrorDomain
-                              code:extendedCode
-                          userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:@"Error code %i: %@ (extended error code %i)", code, message, extendedCode]}];
+  return [EXUpdatesDatabaseUtils errorFromSqlite:db];
 }
 
 - (EXUpdatesUpdate *)_updateWithRow:(NSDictionary *)row config:(EXUpdatesConfig *)config
