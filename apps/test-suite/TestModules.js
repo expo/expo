47b314f0ff52f2b8ad2fadc29a448b83caed1904
Charlie Cruzan <35579283+cruzach@users.noreply.github.com>
2021-03-03 19:38:25 -0500

    [expo] stop exporting LegacyNotifications in SDK 41 (#11807)

--- apps/test-suite/TestModules.js
+++ apps/test-suite/TestModules.js
@@ -86,7 +86,7 @@ export function getTestModules() {
       require('./tests/SecureStore'),
       require('./tests/SMS'),
       require('./tests/StoreReview'),
-      require('./tests/NewNotifications')
+      require('./tests/Notifications')
     );
     return modules;
   }
@@ -96,7 +96,7 @@ export function getTestModules() {
       require('./tests/Contacts'),
       // require('./tests/SVG'),
       require('./tests/Localization'),
-      optionalRequire(() => require('./tests/NewNotifications')),
+      optionalRequire(() => require('./tests/Notifications')),
       LocationTestScreen
     );
 
@@ -127,7 +127,7 @@ export function getTestModules() {
     optionalRequire(() => require('./tests/AdMobInterstitial')),
     optionalRequire(() => require('./tests/AdMobRewarded')),
     optionalRequire(() => require('./tests/FBBannerAd')),
-    optionalRequire(() => require('./tests/NewNotifications'))
+    optionalRequire(() => require('./tests/Notifications'))
   );
 
   if (!isDeviceFarm()) {
@@ -151,7 +151,6 @@ export function getTestModules() {
     modules.push(optionalRequire(() => require('./tests/Calendar')));
     modules.push(optionalRequire(() => require('./tests/CalendarReminders')));
     modules.push(optionalRequire(() => require('./tests/MediaLibrary')));
-    modules.push(optionalRequire(() => require('./tests/Notifications')));
 
     modules.push(optionalRequire(() => require('./tests/Battery')));
     if (Constants.isDevice) {
