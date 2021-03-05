5e94fdc316757267b10f313e5e567980501268aa
Eric Samelson <eric@expo.io>
2021-03-04 19:43:24 -0800

    Revert "Revert "[expo-updates] add SQLite migrations to make `assets.key` nullable (#12084)""
    This reverts commit d7721e9987847b35a92867266cdbef22a3ba2f18.

---
+++ packages/expo-updates/ios/EXUpdates/Database/Migrations/EXUpdatesDatabaseMigration.h
@@ -0,0 +1,15 @@
+//  Copyright © 2021 650 Industries. All rights reserved.
+
+#import <sqlite3.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol EXUpdatesDatabaseMigration
+
+@property (nonatomic, strong, readonly) NSString *filename;
+
+- (BOOL)runMigrationOnDatabase:(struct sqlite3 *)db error:(NSError ** _Nullable)error;
+
+@end
+
+NS_ASSUME_NONNULL_END
