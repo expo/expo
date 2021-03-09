82801649c31d7e9aec1779ea963ec8c1f7f38827
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-03-09 15:01:37 +0100

    Apply requested changes

--- packages/expo-dev-menu/app/DevMenuWebBrowser.ts
+++ packages/expo-dev-menu/app/DevMenuWebBrowser.ts
@@ -2,36 +2,36 @@ import { NativeModules, AppState, Linking } from 'react-native';
 
 const DevMenu = NativeModules.ExpoDevMenuInternal;
 
-let _redirectHandler: ((event: any) => void) | null = null;
-let _onWebBrowserCloseAndroid: null | (() => void) = null;
-let _isAppStateAvailable: boolean = AppState.currentState !== null;
+let redirectHandler: ((event: any) => void) | null = null;
+let onWebBrowserCloseAndroid: null | (() => void) = null;
+let isAppStateAvailable: boolean = AppState.currentState !== null;
 
-function _onAppStateChangeAndroid(state: any) {
-  if (!_isAppStateAvailable) {
-    _isAppStateAvailable = true;
+function onAppStateChangeAndroid(state: any) {
+  if (!isAppStateAvailable) {
+    isAppStateAvailable = true;
     return;
   }
 
-  if (state === 'active' && _onWebBrowserCloseAndroid) {
-    _onWebBrowserCloseAndroid();
+  if (state === 'active' && onWebBrowserCloseAndroid) {
+    onWebBrowserCloseAndroid();
   }
 }
 
-function _stopWaitingForRedirect() {
-  if (!_redirectHandler) {
+function stopWaitingForRedirect() {
+  if (!redirectHandler) {
     throw new Error(
       `The WebBrowser auth session is in an invalid state with no redirect handler when one should be set`
     );
   }
 
-  Linking.removeEventListener('url', _redirectHandler);
-  _redirectHandler = null;
+  Linking.removeEventListener('url', redirectHandler);
+  redirectHandler = null;
 }
 
-async function _openBrowserAndWaitAndroidAsync(startUrl: string): Promise<any> {
+async function openBrowserAndWaitAndroidAsync(startUrl: string): Promise<any> {
   const appStateChangedToActive = new Promise(resolve => {
-    _onWebBrowserCloseAndroid = resolve;
-    AppState.addEventListener('change', _onAppStateChangeAndroid);
+    onWebBrowserCloseAndroid = resolve;
+    AppState.addEventListener('change', onAppStateChangeAndroid);
   });
 
   let result = { type: 'cancel' };
@@ -43,41 +43,41 @@ async function _openBrowserAndWaitAndroidAsync(startUrl: string): Promise<any> {
     result = { type: 'dissmiss' };
   }
 
-  AppState.removeEventListener('change', _onAppStateChangeAndroid);
-  _onWebBrowserCloseAndroid = null;
+  AppState.removeEventListener('change', onAppStateChangeAndroid);
+  onWebBrowserCloseAndroid = null;
   return result;
 }
 
-function _waitForRedirectAsync(returnUrl: string): Promise<any> {
+function waitForRedirectAsync(returnUrl: string): Promise<any> {
   return new Promise(resolve => {
-    _redirectHandler = (event: any) => {
+    redirectHandler = (event: any) => {
       if (event.url.startsWith(returnUrl)) {
         resolve({ url: event.url, type: 'success' });
       }
     };
 
-    Linking.addEventListener('url', _redirectHandler);
+    Linking.addEventListener('url', redirectHandler);
   });
 }
 
-async function _openAuthSessionPolyfillAsync(startUrl: string, returnUrl: string): Promise<any> {
-  if (_redirectHandler) {
+async function openAuthSessionPolyfillAsync(startUrl: string, returnUrl: string): Promise<any> {
+  if (redirectHandler) {
     throw new Error(
       `The WebBrowser's auth session is in an invalid state with a redirect handler set when it should not be`
     );
   }
 
-  if (_onWebBrowserCloseAndroid) {
+  if (onWebBrowserCloseAndroid) {
     throw new Error(`WebBrowser is already open, only one can be open at a time`);
   }
 
   try {
     return await Promise.race([
-      _openBrowserAndWaitAndroidAsync(startUrl),
-      _waitForRedirectAsync(returnUrl),
+      openBrowserAndWaitAndroidAsync(startUrl),
+      waitForRedirectAsync(returnUrl),
     ]);
   } finally {
-    _stopWaitingForRedirect();
+    stopWaitingForRedirect();
   }
 }
 
@@ -87,5 +87,5 @@ export async function openAuthSessionAsync(url: string, returnUrl: string): Prom
     return await DevMenu.openAuthSessionAsync(url, returnUrl);
   }
   // Android
-  return await _openAuthSessionPolyfillAsync(url, returnUrl);
+  return await openAuthSessionPolyfillAsync(url, returnUrl);
 }
