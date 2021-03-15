a9a910781c5f731c891a8fdf6102297c685ea2e2
Eric Samelson <esamelson@users.noreply.github.com>
2021-03-15 10:42:36 -0700

    Revert "[ios] Remove legacy ScreenOrientation code (#11371)" (#12217)
    This reverts commit 454e16d504aee225b3bdc8ee3fedbbdc051c7209.
    
    # Why
    
    After #11371, `lockAsync` is broken in iOS Expo Go; it causes the current orientation to change but does **not** persist the lock, which causes various issues. Additionally, the app.json `orientation` setting has no effect either. Essentially, all apps are permanently locked to `DEFAULT` orientation.
    
    # Test Plan
    
    - [x] NCL tests pass
    - [x] test-suite tests pass
    - [x] app.json `orientation` setting has an effect

--- ios/Exponent/Kernel/Views/EXAppViewController.m
+++ ios/Exponent/Kernel/Views/EXAppViewController.m
@@ -17,6 +17,7 @@
 #import "EXKernel.h"
 #import "EXKernelUtil.h"
 #import "EXReactAppManager.h"
+#import "EXScreenOrientationManager.h"
 #import "EXVersions.h"
 #import "EXUpdatesManager.h"
 #import "EXUtil.h"
@@ -36,6 +37,8 @@
 #if __has_include(<ABI39_0_0React/ABI39_0_0RCTAppearance.h>)
 #import <ABI39_0_0React/ABI39_0_0RCTAppearance.h>
 #endif
+
+#define EX_INTERFACE_ORIENTATION_USE_MANIFEST 0
 
 // when we encounter an error and auto-refresh, we may actually see a series of errors.
 // we only want to trigger refresh once, so we debounce refresh on a timer.
@@ -88,12 +91,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation EXAppViewController
 
+@synthesize supportedInterfaceOrientations = _supportedInterfaceOrientations;
+
 #pragma mark - Lifecycle
 
 - (instancetype)initWithAppRecord:(EXKernelAppRecord *)record
 {
   if (self = [super init]) {
     _appRecord = record;
+    _supportedInterfaceOrientations = EX_INTERFACE_ORIENTATION_USE_MANIFEST;
     _isStandalone = [EXEnvironment sharedEnvironment].isDetached;
   }
   return self;
@@ -253,6 +259,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)appStateDidBecomeActive
 {
   dispatch_async(dispatch_get_main_queue(), ^{
+    [self _enforceDesiredDeviceOrientation];
+
     // Reset the root view background color and window color if we switch between Expo home and project
     [self _setBackgroundColor:self.view];
   });
@@ -271,6 +279,7 @@ NS_ASSUME_NONNULL_BEGIN
     dispatch_async(dispatch_get_main_queue(), ^{
       [self _overrideUserInterfaceStyleOf:self];
       [self _overrideAppearanceModuleBehaviour];
+      [self _enforceDesiredDeviceOrientation];
       [self _invalidateRecoveryTimer];
       [[EXKernel sharedInstance] logAnalyticsEvent:@"LOAD_EXPERIENCE" forAppRecord:self.appRecord];
       [self.appRecord.appManager rebuildBridge];
@@ -519,6 +528,12 @@ NS_ASSUME_NONNULL_BEGIN
     return [screenOrientationRegistry requiredOrientationMask];
   }
 #endif
+
+  // TODO: Remove once sdk 37 is phased out
+  if (_supportedInterfaceOrientations != EX_INTERFACE_ORIENTATION_USE_MANIFEST) {
+    return _supportedInterfaceOrientations;
+  }
+
   return [self orientationMaskFromManifestOrDefault];
 }
 
@@ -537,6 +552,13 @@ NS_ASSUME_NONNULL_BEGIN
   return UIInterfaceOrientationMaskAllButUpsideDown;
 }
 
+// TODO: Remove once sdk 37 is phased out
+- (void)setSupportedInterfaceOrientations:(UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+  _supportedInterfaceOrientations = supportedInterfaceOrientations;
+  [self _enforceDesiredDeviceOrientation];
+}
+
 - (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
   if ((self.traitCollection.verticalSizeClass != previousTraitCollection.verticalSizeClass)
@@ -546,7 +568,54 @@ NS_ASSUME_NONNULL_BEGIN
       EXScreenOrientationRegistry *screenOrientationRegistryController = (EXScreenOrientationRegistry *)[UMModuleRegistryProvider getSingletonModuleForClass:[EXScreenOrientationRegistry class]];
       [screenOrientationRegistryController traitCollectionDidChangeTo:self.traitCollection];
     #endif
+
+    // TODO: Remove once sdk 37 is phased out
+    [[EXKernel sharedInstance].serviceRegistry.screenOrientationManager handleScreenOrientationChange:self.traitCollection];
   }
+}
+
+// TODO: Remove once sdk 37 is phased out
+- (void)_enforceDesiredDeviceOrientation
+{
+  RCTAssertMainQueue();
+  UIInterfaceOrientationMask mask = [self supportedInterfaceOrientations];
+  UIDeviceOrientation currentOrientation = [[UIDevice currentDevice] orientation];
+  UIInterfaceOrientation newOrientation = UIInterfaceOrientationUnknown;
+  switch (mask) {
+    case UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskPortraitUpsideDown:
+      if (!UIDeviceOrientationIsPortrait(currentOrientation)) {
+        newOrientation = UIInterfaceOrientationPortrait;
+      }
+      break;
+    case UIInterfaceOrientationMaskPortrait:
+      newOrientation = UIInterfaceOrientationPortrait;
+      break;
+    case UIInterfaceOrientationMaskPortraitUpsideDown:
+      newOrientation = UIInterfaceOrientationPortraitUpsideDown;
+      break;
+    case UIInterfaceOrientationMaskLandscape:
+      if (!UIDeviceOrientationIsLandscape(currentOrientation)) {
+        newOrientation = UIInterfaceOrientationLandscapeLeft;
+      }
+      break;
+    case UIInterfaceOrientationMaskLandscapeLeft:
+      newOrientation = UIInterfaceOrientationLandscapeLeft;
+      break;
+    case UIInterfaceOrientationMaskLandscapeRight:
+      newOrientation = UIInterfaceOrientationLandscapeRight;
+      break;
+    case UIInterfaceOrientationMaskAllButUpsideDown:
+      if (currentOrientation == UIDeviceOrientationFaceDown) {
+        newOrientation = UIInterfaceOrientationPortrait;
+      }
+      break;
+    default:
+      break;
+  }
+  if (newOrientation != UIInterfaceOrientationUnknown) {
+    [[UIDevice currentDevice] setValue:@(newOrientation) forKey:@"orientation"];
+  }
+  [UIViewController attemptRotationToDeviceOrientation];
 }
 
 #pragma mark - RCTAppearanceModule
