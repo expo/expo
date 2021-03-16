526f07530b286396b4880803fedd4110d5a56f04
simek <gosimek@gmail.com>
2021-03-17 00:01:03 +0100

    cleanup docs-generator code

--- packages/docs-generator/src/index.ts
+++ packages/docs-generator/src/index.ts
@@ -1,31 +1,25 @@
 import { exec } from 'child_process';
 
-const executeCommand = (pkgName, entryPoint = 'index', wholeModule = false, modeHtml = false, json = false) => {
+const executeCommand = (pkgName, entryPoint = 'index', wholeModule = false) => {
   const entry = wholeModule ? `../${pkgName}/src` : `../${pkgName}/src/${entryPoint}`;
-  const mode = modeHtml ? 'html' : 'md';
-  const modeFlags = modeHtml
-    ? '--plugin none'
-    : `--entryDocument ${entryPoint.split('.')[0]}.md --hidePageTitle true --hideInPageTOC true --hideBreadcrumbs true --publicPath ./`;
   const docsPath = '../../docs/data';
-  // const docsPath = '../../docs/pages/versions/unversioned/partials';
-  // const exportJson = json ? ' --json ${docsPath}/${pkgName}.json' : '';
 
   exec(
-    `yarn command ${entry} --tsconfig ../${pkgName}/tsconfig.json --json ${docsPath}/${pkgName}.json --out ${docsPath}/${pkgName} ${modeFlags}`,
+    `yarn command ${entry} --tsconfig ../${pkgName}/tsconfig.json --json ${docsPath}/${pkgName}.json --out ${docsPath}/${pkgName} --plugin none`,
     (error, stdout, stderr) => {
       if (error && error.message && error.message.length > 0) {
         console.error(error.message);
       } else if (stderr && stderr.length > 0) {
         console.error(stderr);
       }
-      console.log(
-        `Successful extraction of ${entryPoint} from ${pkgName}! [${mode.toUpperCase()}]`
-      );
+      console.log(`Successful extraction of ${entryPoint} from ${pkgName}!`);
     }
   );
 };
 
-executeCommand('expo-mail-composer', 'MailComposer.ts', false, false, true);
+executeCommand('expo-mail-composer', 'MailComposer.ts');
+
+// TODO
 // executeCommand('expo-sensors', 'index.ts', true);
 // executeCommand('expo-barcode-scanner', 'BarCodeScanner.tsx');
 // executeCommand('expo-random', 'Random.ts');
