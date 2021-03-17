4fdbf71681ee98558006121ce35c0c4b4e5eae23
Evan Bacon <baconbrix@gmail.com>
2021-03-17 16:46:54 -0700

    copied plugin over to expo-ads-admob

--- packages/expo-ads-admob/plugin/src/withAdMob.ts
+++ packages/expo-ads-admob/plugin/src/withAdMob.ts
@@ -2,6 +2,7 @@ import { ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
 
 import { withAdMobAndroid } from './withAdMobAndroid';
 import { withAdMobIOS, withUserTrackingPermission } from './withAdMobIOS';
+import { withSKAdNetworkIdentifiers } from './withSKAdNetworkIdentifiers';
 
 const pkg = require('expo-ads-admob/package.json');
 
@@ -11,6 +12,8 @@ const withAdMob: ConfigPlugin<{
   config = withAdMobAndroid(config);
   config = withAdMobIOS(config);
   config = withUserTrackingPermission(config, props);
+  // https://developers.google.com/admob/ios/ios14
+  config = withSKAdNetworkIdentifiers(config, ['cstr6suwn9.skadnetwork']);
   return config;
 };
 
