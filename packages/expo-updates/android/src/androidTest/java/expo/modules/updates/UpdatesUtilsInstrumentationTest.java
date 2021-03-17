0a87ff1b5fe26929a6216917e052f4e5d6796726
Jonathan 小林 Hales <jonathan@expo.io>
2021-03-17 09:31:00 -0700

    add tests

--- packages/expo-updates/android/src/androidTest/java/expo/modules/updates/UpdatesUtilsInstrumentationTest.java
+++ packages/expo-updates/android/src/androidTest/java/expo/modules/updates/UpdatesUtilsInstrumentationTest.java
@@ -6,8 +6,13 @@ import org.junit.runner.RunWith;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import static expo.modules.updates.UpdatesUtils.getMapFromStringifiedJSON;
+import static org.hamcrest.CoreMatchers.is;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class UpdatesUtilsInstrumentationTest {
@@ -26,5 +31,34 @@ public class UpdatesUtilsInstrumentationTest {
   public void testParseDateString_writtenTimezoneWithColon() throws ParseException {
     Assert.assertEquals(new Date(1605053874699L), UpdatesUtils.parseDateString("2020-11-11T00:17:54.699+00:00"));
     Assert.assertEquals(new Date(1605050274699L), UpdatesUtils.parseDateString("2020-11-11T00:17:54.699+01:00"));
+  }
+
+  @Test
+  public void testGetMapFromStringifiedJSON_empty() throws Exception {
+    Map<String,String> emptyMap = getMapFromStringifiedJSON("{}");
+    Assert.assertThat(emptyMap, is(new HashMap<>()));
+  }
+
+  @Test
+  public void testGetMapFromStringifiedJSON_expectedFormat() throws Exception {
+    Map<String, String> expected = new HashMap<>();
+    expected.put("expo-channel-name","main");
+
+    Map<String,String> emptyMap = getMapFromStringifiedJSON("{'expo-channel-name':'main'}");
+    Assert.assertThat(emptyMap, is(expected));
+  }
+
+  @Test
+  public void testGetMapFromStringifiedJSON_stringifiesUnexpectedValue() throws Exception {
+    Map<String, String> expected = new HashMap<>();
+    expected.put("expo-channel-name","[\"main\"]");
+
+    Map<String,String> emptyMap = getMapFromStringifiedJSON("{'expo-channel-name':['main']}");
+    Assert.assertThat(emptyMap, is(expected));
+  }
+
+  @Test(expected = Exception.class)
+  public void testGetMapFromStringifiedJSON_throwsNonStringKey() throws Exception {
+    getMapFromStringifiedJSON("{7:['main']}");
   }
 }
