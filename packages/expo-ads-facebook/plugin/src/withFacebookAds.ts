1e88133b1e7662872e0cbff66dbefe1fdf2a34c6
Evan Bacon <baconbrix@gmail.com>
2021-03-17 16:44:17 -0700

    copied plugin over to expo-ads-facebook

--- packages/expo-ads-facebook/plugin/src/withFacebookAds.ts
+++ packages/expo-ads-facebook/plugin/src/withFacebookAds.ts
@@ -1,6 +1,7 @@
 import { ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
 
 import { withUserTrackingPermission } from './withFacebookAdsIOS';
+import { withSKAdNetworkIdentifiers } from './withSKAdNetworkIdentifiers';
 
 const pkg = require('expo-ads-facebook/package.json');
 
@@ -8,6 +9,8 @@ const withFacebookAds: ConfigPlugin<{
   userTrackingPermission?: string;
 } | void> = (config, props) => {
   config = withUserTrackingPermission(config, props);
+  // https://developers.facebook.com/docs/SKAdNetwork
+  config = withSKAdNetworkIdentifiers(config, ['v9wttpbfk9.skadnetwork', 'n38lu8286q.skadnetwork']);
   return config;
 };
 
