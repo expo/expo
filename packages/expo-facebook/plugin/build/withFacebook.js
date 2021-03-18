6ef02cb6ff677fbbdf7f3c34cd99ac9c1a05a911
Evan Bacon <baconbrix@gmail.com>
2021-03-18 13:48:16 -0700

    Created noop file plugin for facebook

--- packages/expo-facebook/plugin/build/withFacebook.js
+++ packages/expo-facebook/plugin/build/withFacebook.js
@@ -4,6 +4,7 @@ const config_plugins_1 = require("@expo/config-plugins");
 const withFacebookAndroid_1 = require("./withFacebookAndroid");
 const withFacebookIOS_1 = require("./withFacebookIOS");
 const withSKAdNetworkIdentifiers_1 = require("./withSKAdNetworkIdentifiers");
+const withNoopSwiftFile_1 = require("./withNoopSwiftFile");
 const pkg = require('expo-facebook/package.json');
 const withFacebook = config => {
     config = withFacebookAndroid_1.withFacebookAppIdString(config);
@@ -12,6 +13,7 @@ const withFacebook = config => {
     config = withFacebookIOS_1.withUserTrackingPermission(config);
     // https://developers.facebook.com/docs/SKAdNetwork
     config = withSKAdNetworkIdentifiers_1.withSKAdNetworkIdentifiers(config, ['v9wttpbfk9.skadnetwork', 'n38lu8286q.skadnetwork']);
+    config = withNoopSwiftFile_1.withNoopSwiftFile(config);
     return config;
 };
 exports.default = config_plugins_1.createRunOncePlugin(withFacebook, pkg.name, pkg.version);
