1f0d8c07af9589058ecf93b32f760b630ebc1293
simek <gosimek@gmail.com>
2021-03-23 22:13:38 +0100

    update generated files

--- packages/expo-sharing/build/Sharing.js
+++ packages/expo-sharing/build/Sharing.js
@@ -1,8 +1,8 @@
 import { UnavailabilityError } from '@unimodules/core';
 import Sharing from './ExpoSharing';
-/*
+/**
  * Determine if the sharing API can be used in this app.
- * @return A promise that resolves to true if the sharing API can be used, and false otherwise.
+ * @return A promise that resolves to `true` if the sharing API can be used, and `false` otherwise.
  */
 export async function isAvailableAsync() {
     if (Sharing) {
@@ -13,9 +13,10 @@ export async function isAvailableAsync() {
     }
     return false;
 }
-/*
+/**
  * Opens action sheet to share file to different applications which can handle this type of file.
  * @param url Local file URL to share.
+ * @param options A map of share options.
  */
 export async function shareAsync(url, options = {}) {
     if (!Sharing || !Sharing.shareAsync) {
