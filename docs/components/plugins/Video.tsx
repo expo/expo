8f83ecb76b9e0641c4f991f5eaf4f2e342e10c99
simek <gosimek@gmail.com>
2021-04-06 22:13:59 +0200

    [docs] bump eslint and prettier, fix warnings

--- docs/components/plugins/Video.tsx
+++ docs/components/plugins/Video.tsx
@@ -9,7 +9,7 @@ function mobileAndTabletCheck() {
   }
 
   let check = false;
-  (function(a) {
+  (function (a) {
     if (
       /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
         a
