e4b8e3d572adf5a04a1313ee128f2e7b10ba2657
Axel Delafosse <axeldelafosse@gmail.com>
2021-04-09 10:32:17 +0200

    [docs] remove font awesome

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -50,13 +50,6 @@ export default class MyDocument extends Document<{ css?: string }> {
             ]}
           />
 
-          <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-            crossOrigin="anonymous"
-          />
-
           {getInitGoogleScriptTag({ id: 'UA-107832480-3' })}
           {getGoogleScriptTag()}
         </Head>
