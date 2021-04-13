11cafae882081e33090626e13b172a5109a3a449
Evan Bacon <baconbrix@gmail.com>
2021-04-12 22:05:36 -0600

    tree-shake Logs module

--- packages/expo/src/environment/logging.fx.ts
+++ packages/expo/src/environment/logging.fx.ts
@@ -1,20 +1,21 @@
 import Constants from 'expo-constants';
 
-import * as Logs from '../logs/Logs';
-import RemoteLogging from '../logs/RemoteLogging';
-
-if (Constants.__unsafeNoWarnManifest?.logUrl) {
-  // Enable logging to the Expo dev tools only if this JS is not running in a web browser (ex: the
-  // remote debugger). In Expo Web we don't show console logs in the CLI, so there's no special case needed.
-  if (!isRunningInWebBrowser()) {
-    Logs.enableExpoCliLogging();
-  } else {
-    RemoteLogging.enqueueRemoteLogAsync('info', {}, [
-      'You are now debugging remotely; check your browser console for your application logs.',
-    ]);
+// Metro and terser don't seem to be capable of shaking the imports unless they're wrapped in __DEV__.
+if (__DEV__) {
+  if (Constants.__unsafeNoWarnManifest?.logUrl) {
+    // Enable logging to the Expo dev tools only if this JS is not running in a web browser (ex: the
+    // remote debugger). In Expo Web we don't show console logs in the CLI, so there's no special case needed.
+    if (!isRunningInWebBrowser()) {
+      const Logs = require('../logs/Logs');
+      Logs.enableExpoCliLogging();
+    } else {
+      const RemoteLogging = require('../logs/RemoteLogging').default;
+      RemoteLogging.enqueueRemoteLogAsync('info', {}, [
+        'You are now debugging remotely; check your browser console for your application logs.',
+      ]);
+    }
   }
 }
-
 /**
  * In all web browsers navigator.product is "Gecko" for compatibility reasons.
  * See https://developer.mozilla.org/en-US/docs/Web/API/NavigatorID/product
