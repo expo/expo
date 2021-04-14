805873f320f0ba20efcc333b0c98af4f86fc99a8
Evan Bacon <baconbrix@gmail.com>
2021-04-14 11:29:16 -0600

    Added currentFullName to mock constants

--- packages/jest-expo/src/preset/createMockConstants.js
+++ packages/jest-expo/src/preset/createMockConstants.js
@@ -26,6 +26,7 @@ module.exports = function createMockConstants() {
     linkingUri: mockLinkingUri,
     manifest: {
       id: mockId,
+      currentFullName: mockId,
       slug: mockSlug,
       extra: expoConfig.extra,
       hostUri: mockHostUri,
