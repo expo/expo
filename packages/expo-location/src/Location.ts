1315cdbeb2f899d3cd2e441b2c736d38833d6d6a
Tom <thomasaeh1@gmail.com>
2021-04-15 01:51:51 +0100

    Improved clarity of methods to use in deprecation messgaes in expo-location

--- packages/expo-location/src/Location.ts
+++ packages/expo-location/src/Location.ts
@@ -155,7 +155,7 @@ export async function reverseGeocodeAsync(
 
 /**
  * Gets the current state of location permissions.
- * @deprecated Use `getForegroundPermissions` or `getBackgroundPermissions` instead.
+ * @deprecated Use `getForegroundPermissionsAsync()` or `getBackgroundPermissionsAsync()` instead.
  */
 export async function getPermissionsAsync(): Promise<LocationPermissionResponse> {
   console.warn(
@@ -166,7 +166,7 @@ export async function getPermissionsAsync(): Promise<LocationPermissionResponse>
 
 /**
  * Requests the user to grant location permissions.
- * @deprecated Use `requestForegroundPermissionsAsync` or `requestBackgroundPermissionsAsync` instead.
+ * @deprecated Use `requestForegroundPermissionsAsync()` or `requestBackgroundPermissionsAsync()` instead.
  */
 export async function requestPermissionsAsync(): Promise<LocationPermissionResponse> {
   console.warn(
