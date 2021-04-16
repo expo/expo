a33277e177960d9ab018de0ebf37a81a190e460e
Axel Delafosse <axeldelafosse@gmail.com>
2021-04-16 09:54:03 +0200

    [docs] preload inter regular (#12473)

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -49,6 +49,14 @@ export default class MyDocument extends Document<{ css?: string }> {
             ]}
           />
 
+          <link
+            rel="preload"
+            href="/static/fonts/Inter-Regular.woff2?v=3.15"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
           {getInitGoogleScriptTag({ id: 'UA-107832480-3' })}
           {getGoogleScriptTag()}
         </Head>
