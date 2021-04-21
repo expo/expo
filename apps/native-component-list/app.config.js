3b1c9761a261a3738671b7d5ba376552bfacc9f7
Evan Bacon <baconbrix@gmail.com>
2021-04-21 17:12:04 -0600

    Added dev menu to NCL for testing

--- apps/native-component-list/app.config.js
+++ apps/native-component-list/app.config.js
@@ -3,12 +3,17 @@ export default ({ config }) => {
   // app.json defines the sdkVersion as UNVERSIONED, we can override it here dynamically if we need to,
   // for example with an environment variable.
   // config.sdkVersion = '41.0.0';
-  config.plugins = [
+  if (!Array.isArray(config.plugins)) {
+    config.plugins = [];
+  }
+
+  config.plugins.push(
+    
     // iOS plugins
     // Add a plugin to modify the AppDelegate.
     './plugins/withNotFoundModule',
     // Add the React DevMenu back to the client.
-    './plugins/withDevMenu',
+    // './plugins/withDevMenu',
     // Add AsyncStorage
     './plugins/withExpoAsyncStorage',
     // Set the minimum version to 11 for Google Sign-In support -- TODO: Maybe this belongs in expo-google-sign-in?
@@ -34,7 +39,7 @@ export default ({ config }) => {
         packagePath: '../../../packages/unimodules-test-core/android',
       },
     ],
-  ];
+  );
 
   // NOTE(brentvatne):
   // This adds an ios.scheme property to manifest, which does not validate
@@ -46,13 +51,6 @@ export default ({ config }) => {
     {
       scheme: 'ncl-payments',
       merchantId: 'merchant.com.example.development',
-    },
-  ]);
-
-  config.plugins.push([
-    'expo-document-picker',
-    {
-      appleTeamId: 'XXXXXX',
     },
   ]);
 
