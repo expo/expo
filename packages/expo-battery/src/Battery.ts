80cc369d65a6790aa8d8f1bc46694c3bf1726135
Bartosz Kaszubowski <gosimek@gmail.com>
2021-04-21 10:19:36 +0200

    Update comment
    Co-authored-by: Brent Vatne <brentvatne@gmail.com>

--- packages/expo-battery/src/Battery.ts
+++ packages/expo-battery/src/Battery.ts
@@ -13,7 +13,7 @@ const BatteryEventEmitter = new EventEmitter(ExpoBattery);
 
 // @needsAudit
 /**
- * Resolves with whether this battery API is available on the current device. The value of this
+ * Resolves with whether the battery API is available on the current device. The value of this
  * property is `true` on Android and physical iOS devices and `false` on iOS simulators. On web,
  * it depends on whether the browser supports the web battery API.
  */
