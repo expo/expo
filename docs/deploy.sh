876346473fa20db25ebd908d1c217d591813adb2
tcdavis <thomas.c.davis@gmail.com>
2021-04-22 09:26:39 -0700

    [Docs] Return 301s rather than 404s for moved faq pages (#12670)
    * Return 301s rather than 404s for moved faq pages
    
    * remove trailing slashes

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -84,6 +84,14 @@ redirects[versions/latest/introduction/project-lifecycle/]=versions/latest/intro
 # exp-cli is now expo-cli
 redirects[versions/latest/guides/exp-cli.html]=versions/latest/workflow/expo-cli/
 redirects[versions/latest/guides/exp-cli]=versions/latest/workflow/expo-cli/
+# Migrated FAQ pages
+redirects[faq/image-background]=ui-programming/image-background/
+redirects[faq/react-native-styling-buttons]=ui-programming/react-native-styling-buttons/
+redirects[faq/react-native-version-mismatch]=troubleshooting/react-native-version-mismatch/
+redirects[faq/clear-cache-windows]=troubleshooting/clear-cache-windows/
+redirects[faq/clear-cache-macos-linux]=troubleshooting/clear-cache-macos-linux/
+redirects[faq/application-has-not-been-registered]=troubleshooting/application-has-not-been-registered/
+
 
 echo "::group::[5/5] Add custom redirects"
 for i in "${!redirects[@]}" # iterate over keys
