7976a8bec39b350c776599a2231f25370ac1c360
Evan Bacon <baconbrix@gmail.com>
2021-04-26 15:25:50 -0600

    Remove expo-status-bar from blank template

--- templates/expo-template-bare-minimum/App.js
+++ templates/expo-template-bare-minimum/App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,7 +5,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
