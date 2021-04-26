1e9619eff504ac55e2b3653bb5746db5e88ce2ad
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-04-26 17:33:06 +0200

    [expo-dev-launcher] Fix bundled images on Android

--- packages/expo-dev-launcher/write_embedded_bundle.sh
+++ packages/expo-dev-launcher/write_embedded_bundle.sh
@@ -16,7 +16,8 @@ npx react-native bundle \
     --platform android \
     --dev false \
     --entry-file index.js \
-    --bundle-output android/src/debug/res/raw/expo_dev_launcher_android.bundle
+    --bundle-output android/src/debug/assets/expo_dev_launcher_android.bundle \
+    --assets-dest android/src/debug/res
 
 
 
