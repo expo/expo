67162567926c587cf3644ba1a2bb7ea0f7640dd6
Axel Delafosse <axeldelafosse@gmail.com>
2021-04-28 18:36:25 +0200

    [docs] clean code

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,8 +1,6 @@
-/// <reference types="google.analytics" />
-
 import { ThemeProvider } from '@expo/styleguide';
 import * as Sentry from '@sentry/browser';
-import App from 'next/app';
+import App, { NextWebVitalsMetric } from 'next/app';
 import React from 'react';
 
 import { preprocessSentryError } from '~/common/sentry-utilities';
@@ -17,17 +15,7 @@ Sentry.init({
   beforeSend: preprocessSentryError,
 });
 
-export function reportWebVitals({
-  id,
-  name,
-  label,
-  value,
-}: {
-  id: string;
-  name: string;
-  label: string;
-  value: number;
-}) {
+export function reportWebVitals({ id, name, label, value }: NextWebVitalsMetric) {
   window?.ga?.('send', 'event', {
     eventCategory: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
     eventAction: name,
