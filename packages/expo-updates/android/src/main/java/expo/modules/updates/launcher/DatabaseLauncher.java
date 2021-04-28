5cc47f680e050f786e318ecd1fe1fa5f2762bcf0
Eric Samelson <eric@expo.io>
2021-04-27 17:45:29 -0700

    [android] add ReaperSelectionPolicyDevelopmentClient, implement in Expo Go

--- packages/expo-updates/android/src/main/java/expo/modules/updates/launcher/DatabaseLauncher.java
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/launcher/DatabaseLauncher.java
@@ -84,6 +84,8 @@ public class DatabaseLauncher implements Launcher {
       return;
     }
 
+    database.updateDao().markUpdateAccessed(mLaunchedUpdate);
+
     if (mLaunchedUpdate.status == UpdateStatus.EMBEDDED) {
       mBundleAssetName = EmbeddedLoader.BARE_BUNDLE_FILENAME;
       if (mLocalAssetFiles != null) {
