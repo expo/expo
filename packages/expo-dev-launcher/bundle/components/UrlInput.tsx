6c0cfea3071188d0598ca6ffb98c7769b6e1f452
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-05-02 16:37:43 +0200

    [dev-launcher] Add url validation

--- packages/expo-dev-launcher/bundle/components/UrlInput.tsx
+++ packages/expo-dev-launcher/bundle/components/UrlInput.tsx
@@ -9,11 +9,30 @@ import Button from './Button';
 type Props = {
   onPress: (url: string) => void;
 };
+
+const urlPattern = new RegExp(
+  '^((\\w+)?:\\/\\/)?' + // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+([a-z]{2,})?|' + // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', // fragment locator
+  'i'
+);
+
+function validateURL(str: string): boolean {
+  return urlPattern.test(str);
+}
+
 export default ({ onPress }: Props) => {
   const themeName = useThemeName();
   const [textInputUrl, setTextInputUrl] = React.useState('');
+  const [isValid, setIsValid] = React.useState(true);
+
   const borderColor = themeName === 'dark' ? Colors.dark.border : Colors.light.border;
-  const color = themeName === 'dark' ? Colors.dark.text : Colors.light.text;
+  const textColorName = isValid ? 'text' : 'error';
+  const color = themeName === 'dark' ? Colors.dark[textColorName] : Colors.light[textColorName];
+
   return (
     <View>
       <MainText style={styles.textMarginBottom}>
@@ -23,10 +42,19 @@ export default ({ onPress }: Props) => {
         style={[styles.urlTextInput, { borderColor, color }]}
         placeholder="exp://192..."
         placeholderTextColor="#b0b0ba"
-        value={textInputUrl}
-        onChangeText={text => setTextInputUrl(text)}
+        autoCorrect={false}
+        autoCompleteType="off"
+        defaultValue={textInputUrl}
+        onChangeText={text => {
+          setTextInputUrl(text);
+          setIsValid(validateURL(text));
+        }}
       />
-      <Button onPress={() => onPress(textInputUrl)} label="Connect to URL" />
+      <Button
+        disabled={!isValid || !textInputUrl}
+        onPress={() => onPress(textInputUrl)}
+        label="Connect to URL"
+      />
     </View>
   );
 };
