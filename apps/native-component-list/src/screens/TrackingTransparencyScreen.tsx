e62817ededb1f6d6ed12a976c44d13d602539a44
cruzach <charliecruzan@gmail.com>
2021-05-07 13:44:12 -0400

    fix lint for ncl

--- apps/native-component-list/src/screens/TrackingTransparencyScreen.tsx
+++ apps/native-component-list/src/screens/TrackingTransparencyScreen.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View } from 'react-native';
 import {
   requestTrackingPermissionsAsync,
   getTrackingPermissionsAsync,
 } from 'expo-tracking-transparency';
+import React from 'react';
+import { View } from 'react-native';
 
 import SimpleActionDemo from '../components/SimpleActionDemo';
 
