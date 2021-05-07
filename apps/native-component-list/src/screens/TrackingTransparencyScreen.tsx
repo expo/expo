fa703b13eed44885de4b4f0e09142b520583f2b5
cruzach <charliecruzan@gmail.com>
2021-05-07 13:04:09 -0400

    update NCL

---
+++ apps/native-component-list/src/screens/TrackingTransparencyScreen.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View } from 'react-native';
+import {
+  requestTrackingPermissionsAsync,
+  getTrackingPermissionsAsync,
+} from 'expo-tracking-transparency';
+
+import SimpleActionDemo from '../components/SimpleActionDemo';
+
+export default class TrackingTransparencyScreen extends React.Component {
+  static navigationOptions = {
+    title: 'TrackingTransparency',
+  };
+
+  render() {
+    return (
+      <View style={{ padding: 10 }}>
+        <SimpleActionDemo
+          title="get tracking permissions"
+          action={async () => await getTrackingPermissionsAsync()}
+        />
+        <SimpleActionDemo
+          title="request tracking permissions"
+          action={async () => await requestTrackingPermissionsAsync()}
+        />
+      </View>
+    );
+  }
+}
