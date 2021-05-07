d1545f461b49961ae074eb7b8b074de2761c28e4
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-05-07 12:33:31 +0200

    Update packages/expo-dev-launcher/bundle/components/UrlInput.tsx
    Co-authored-by: Tomasz Sapeta <1714764+tsapeta@users.noreply.github.com>

--- packages/expo-dev-launcher/bundle/components/UrlInput.tsx
+++ packages/expo-dev-launcher/bundle/components/UrlInput.tsx
@@ -16,7 +16,7 @@ const urlPattern = new RegExp(
   '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
   '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
   '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', // fragment locator
+  '(\\#[-a-z\\d_]*)?$', // fragment locator
   'i'
 );
 
