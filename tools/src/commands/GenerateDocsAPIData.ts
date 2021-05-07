9efb513cffaefb9040bd0ca4c736624742ac12b8
cruzach <charliecruzan@gmail.com>
2021-05-07 11:25:28 -0400

    add to GenerateDocsAPIData command

--- tools/src/commands/GenerateDocsAPIData.ts
+++ tools/src/commands/GenerateDocsAPIData.ts
@@ -96,6 +96,7 @@ async function action({ packageName }: ActionOptions) {
     'expo-splash-screen': ['SplashScreen.ts'],
     'expo-store-review': ['StoreReview.ts'],
     'expo-task-manager': ['TaskManager.ts'],
+    'expo-tracking-transparency': ['TrackingTransparency.ts'],
   };
 
   try {
