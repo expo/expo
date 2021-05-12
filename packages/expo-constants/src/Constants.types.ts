d83fb02821ad445f3dca24df697970a92a578e81
Will Schurman <wschurman@gmail.com>
2021-05-11 21:08:03 -0700

    Update packages/expo-constants/src/Constants.types.ts
    Co-authored-by: James Ide <ide@users.noreply.github.com>

--- packages/expo-constants/src/Constants.types.ts
+++ packages/expo-constants/src/Constants.types.ts
@@ -129,7 +129,7 @@ export interface NativeConstants {
    */
   manifest: AppManifest | null;
   /**
-   * New manifest for Expo apps using expo-updates.
+   * New manifest for Expo apps using modern Expo Updates.
    * Returns `null` in bare workflow and when `manifest` is non-null.
    */
   manifest2: Manifest | null;
