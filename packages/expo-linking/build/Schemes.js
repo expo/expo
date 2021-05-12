b49b5135475ceb76756e9027406f3d61f65503bd
Brent Vatne <brentvatne@gmail.com>
2021-05-12 12:34:44 -0700

    [linking] Commit build files again

--- packages/expo-linking/build/Schemes.js
+++ packages/expo-linking/build/Schemes.js
@@ -92,11 +92,11 @@ export function resolveScheme(props) {
     const nativeAppId = getNativeAppIdScheme();
     if (!manifestSchemes.length) {
         if (__DEV__ && !props.isSilent) {
-            // Assert a config warning if no scheme is setup yet.
+            // Assert a config warning if no scheme is setup yet. `isSilent` is used for warnings, but we'll ignore it for exceptions.
             console.warn(`Linking requires a build-time setting \`scheme\` in the project's Expo config (app.config.js or app.json) for production apps, if it's left blank, your app may crash. The scheme does not apply to development in the Expo client but you should add it as soon as you start working with Linking to avoid creating a broken build. Learn more: ${LINKING_GUIDE_URL}`);
         }
         else if (!__DEV__ || Constants.executionEnvironment !== ExecutionEnvironment.StoreClient) {
-            // Throw in production, use the __DEV__ flag so users can test this functionality with `expo start --no-dev`
+            // Throw in production or when not in store client. Use the __DEV__ flag so users can test this functionality with `expo start --no-dev`,
             throw new Error('Cannot make a deep link into a standalone app with no custom scheme defined');
         }
     }
