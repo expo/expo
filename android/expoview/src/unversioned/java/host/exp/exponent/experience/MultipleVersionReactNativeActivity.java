52026d939c5afb2beb0eacb0dec33f06725152bd
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-05-13 10:42:25 +0200

    Add versioning flavors
    Extract `flavors.gradle` and fix versioned

---
+++ android/expoview/src/unversioned/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
@@ -0,0 +1,10 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+package host.exp.exponent.experience;
+
+// Implement for each version.
+public class MultipleVersionReactNativeActivity extends ReactNativeActivity implements
+    // The 4-space indentation is used by android-build-aar.sh.
+    com.facebook.react.modules.core.DefaultHardwareBackBtnHandler {
+
+}
