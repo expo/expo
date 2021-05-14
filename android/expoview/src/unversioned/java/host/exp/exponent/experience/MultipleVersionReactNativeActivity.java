4d4f418d80b86de0dd4263c3a6bd1e5a144f55c4
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-05-14 12:31:28 +0200

    Update CI to use flavors

--- android/expoview/src/unversioned/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
+++ android/expoview/src/unversioned/java/host/exp/exponent/experience/MultipleVersionReactNativeActivity.java
@@ -2,7 +2,8 @@
 
 package host.exp.exponent.experience;
 
-// Implement for each version.
+// Empty stub in `unversioned` flavor. See src/versioned/... for versioned variant
+// or change android flavor to `versioned`.
 public class MultipleVersionReactNativeActivity extends ReactNativeActivity implements
     // The 4-space indentation is used by android-build-aar.sh.
     com.facebook.react.modules.core.DefaultHardwareBackBtnHandler {
