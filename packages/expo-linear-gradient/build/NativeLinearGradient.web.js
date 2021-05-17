bf99f2847249cc60c9dafe678584b7915b0c0872
Petr Bela <bela.petr@gmail.com>
2021-05-17 19:19:27 +0200

    [web][linear-gradient] fix: useMemo instead of useEffect (#12625)
    * useMemo instead of useEffect
    
    Unless I'm missing something, the intent of the code was to only recalculate gradient/angle when some of the props change. For that, `useMemo` is the correct way to do it. With `useMemo`, the first render already has the correct result, instead of waiting for the `useEffect` to run. This also fixes SSR.
    
    * Build package

--- packages/expo-linear-gradient/build/NativeLinearGradient.web.js
+++ packages/expo-linear-gradient/build/NativeLinearGradient.web.js
@@ -3,10 +3,8 @@ import { View } from 'react-native';
 import normalizeColor from 'react-native-web/src/modules/normalizeColor';
 export default function NativeLinearGradient({ colors, locations, startPoint, endPoint, ...props }) {
     const [layout, setLayout] = React.useState(null);
-    const [gradientColors, setGradientColors] = React.useState([]);
-    const [pseudoAngle, setPseudoAngle] = React.useState(0);
     const { width = 1, height = 1 } = layout ?? {};
-    React.useEffect(() => {
+    const pseudoAngle = React.useMemo(() => {
         const getControlPoints = () => {
             let correctedStartPoint = [0, 0];
             if (Array.isArray(startPoint)) {
@@ -31,10 +29,10 @@ export default function NativeLinearGradient({ colors, locations, startPoint, en
         end[1] *= height;
         const py = end[1] - start[1];
         const px = end[0] - start[0];
-        setPseudoAngle(90 + (Math.atan2(py, px) * 180) / Math.PI);
+        return 90 + (Math.atan2(py, px) * 180) / Math.PI;
     }, [width, height, startPoint, endPoint]);
-    React.useEffect(() => {
-        const nextGradientColors = colors.map((color, index) => {
+    const gradientColors = React.useMemo(() => {
+        return colors.map((color, index) => {
             const hexColor = normalizeColor(color);
             let output = hexColor;
             if (locations && locations[index]) {
@@ -45,7 +43,6 @@ export default function NativeLinearGradient({ colors, locations, startPoint, en
             }
             return output;
         });
-        setGradientColors(nextGradientColors);
     }, [colors, locations]);
     const colorStyle = gradientColors.join(',');
     const backgroundImage = `linear-gradient(${pseudoAngle}deg, ${colorStyle})`;
