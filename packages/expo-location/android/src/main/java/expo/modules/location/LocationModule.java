d63a208b1d735abd31831aebb47dc732f089e1ea
Will Schurman <wschurman@gmail.com>
2021-05-28 10:10:13 -0600

    [spelling] azimut -> azimuth (#13080)

--- packages/expo-location/android/src/main/java/expo/modules/location/LocationModule.java
+++ packages/expo-location/android/src/main/java/expo/modules/location/LocationModule.java
@@ -114,7 +114,7 @@ public class LocationModule extends ExportedModule implements LifecycleEventList
   private float[] mGravity;
   private float[] mGeomagnetic;
   private int mHeadingId;
-  private float mLastAzimut = 0;
+  private float mLastAzimuth = 0;
   private int mAccuracy = 0;
   private long mLastUpdate = 0;
   private boolean mGeocoderPaused = false;
@@ -753,8 +753,8 @@ public class LocationModule extends ExportedModule implements LifecycleEventList
 
       // Make sure Delta is big enough to warrant an update
       // Currently: 50ms and ~2 degrees of change (android has a lot of useless updates block up the sending)
-      if ((Math.abs(orientation[0] - mLastAzimut)) > DEGREE_DELTA && (System.currentTimeMillis() - mLastUpdate) > TIME_DELTA) {
-        mLastAzimut = orientation[0];
+      if ((Math.abs(orientation[0] - mLastAzimuth)) > DEGREE_DELTA && (System.currentTimeMillis() - mLastUpdate) > TIME_DELTA) {
+        mLastAzimuth = orientation[0];
         mLastUpdate = System.currentTimeMillis();
         float magneticNorth = calcMagNorth(orientation[0]);
         float trueNorth = calcTrueNorth(magneticNorth);
@@ -771,9 +771,9 @@ public class LocationModule extends ExportedModule implements LifecycleEventList
     }
   }
 
-  private float calcMagNorth(float azimut) {
-    float azimutDeg = (float) Math.toDegrees(azimut);
-    return (azimutDeg + 360) % 360;
+  private float calcMagNorth(float azimuth) {
+    float azimuthDeg = (float) Math.toDegrees(azimuth);
+    return (azimuthDeg + 360) % 360;
   }
 
   private float calcTrueNorth(float magNorth) {
@@ -798,7 +798,7 @@ public class LocationModule extends ExportedModule implements LifecycleEventList
     mGeomagnetic = null;
     mGeofield = null;
     mHeadingId = 0;
-    mLastAzimut = 0;
+    mLastAzimuth = 0;
     mAccuracy = 0;
   }
 
