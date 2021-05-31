0c2d76d623e6dcc7730a2cd58bf46c601a5b8767
Cedric van Putten <me@bycedric.com>
2021-05-31 16:48:59 +0200

    [background-fetch] Refactor background fetch docs to API generated docs

--- packages/expo-background-fetch/src/BackgroundFetch.ts
+++ packages/expo-background-fetch/src/BackgroundFetch.ts
@@ -8,6 +8,12 @@ import {
 } from './BackgroundFetch.types';
 import ExpoBackgroundFetch from './ExpoBackgroundFetch';
 
+// @needsAudit
+/**
+ * Gets a status of background fetch.
+ *
+ * @returns A promise with the background fetch status enum, or `null` when unavailable.
+ */
 export async function getStatusAsync(): Promise<BackgroundFetchStatus | null> {
   if (Platform.OS === 'android') {
     return BackgroundFetchStatus.Available;
@@ -15,6 +21,16 @@ export async function getStatusAsync(): Promise<BackgroundFetchStatus | null> {
   return ExpoBackgroundFetch.getStatusAsync();
 }
 
+/**
+ * Sets the minimum number of seconds that must elapse before another background fetch can be initiated. This value is advisory only and does not indicate the exact amount of time expected between fetch operations.
+ *
+ * _This method doesn't take any effect on Android._
+ *
+ * _It is a global value which means that it can overwrite settings from another application opened through Expo Go._
+ *
+ * @param minimumInterval Number of seconds that must elapse before another background fetch can be called.
+ * @returns A promise resolving once the minimum interval is set.
+ */
 export async function setMinimumIntervalAsync(minimumInterval: number): Promise<void> {
   if (!ExpoBackgroundFetch.setMinimumIntervalAsync) {
     return;
@@ -23,6 +39,14 @@ export async function setMinimumIntervalAsync(minimumInterval: number): Promise<
   await ExpoBackgroundFetch.setMinimumIntervalAsync(minimumInterval);
 }
 
+// @needsAudit
+/**
+ * Registers background fetch task with given name. Registered tasks are saved in persistent storage and restored once the app is initialized.
+ *
+ * @param taskName Name of the task to register. The task needs to be defined first - see `TaskManager.defineTask` for more details.
+ * @param options The background fetch task options.
+ * @returns Returns a promise that resolves once the task is registered and rejects in case of any errors.
+ */
 export async function registerTaskAsync(
   taskName: string,
   options: BackgroundFetchOptions = {}
@@ -38,6 +62,13 @@ export async function registerTaskAsync(
   await ExpoBackgroundFetch.registerTaskAsync(taskName, options);
 }
 
+// @needsAudit
+/**
+ * Unregisters background fetch task, so the application will no longer be executing this task.
+ *
+ * @param taskName Name of the task to unregister.
+ * @returns A promise resolving when the task is fully unregistered.
+ */
 export async function unregisterTaskAsync(taskName: string): Promise<void> {
   if (!ExpoBackgroundFetch.unregisterTaskAsync) {
     throw new UnavailabilityError('BackgroundFetch', 'unregisterTaskAsync');
--- packages/expo-background-fetch/src/BackgroundFetch.types.ts
+++ packages/expo-background-fetch/src/BackgroundFetch.types.ts
@@ -1,17 +1,47 @@
+// @needsAudit
 export enum BackgroundFetchResult {
+  /** There was no new data to download. */
   NoData = 1,
+  /** New data was successfully downloaded. */
   NewData = 2,
+  /** An attempt to download data was made but that attempt failed. */
   Failed = 3,
 }
 
+// @needsAudit
 export enum BackgroundFetchStatus {
+  /**
+   * The user explicitly disabled background behavior for this app or for the whole system.
+   */
   Denied = 1,
+  /**
+   * Background updates are unavailable and the user cannot enable them again.
+   * This status can occur when, for example, parental controls are in effect for the current user.
+   */
   Restricted = 2,
+  /** Background updates are available for the app. */
   Available = 3,
 }
 
+// @needsAudit
 export interface BackgroundFetchOptions {
+  /**
+   * Inexact interval in seconds between subsequent repeats of the background fetch alarm.
+   * The final interval may differ from the specified one to minimize wakeups and battery usage.
+   *
+   * On Android it defaults to **15 minutes**.
+   *
+   * On iOS it calls `BackgroundFetch.setMinimumIntervalAsync` behind the scenes
+   * and the default value is the smallest fetch interval supported by the
+   * system (**10-15 minutes**).
+   */
   minimumInterval?: number;
+  /**
+   * Whether to stop receiving background fetch events after user terminates the app. Defaults to `true`. (**Android only**)
+   */
   stopOnTerminate?: boolean;
+  /**
+   * Whether to restart background fetch events when the device has finished booting. Defaults to `false`. (**Android only**)
+   */
   startOnBoot?: boolean;
 }
