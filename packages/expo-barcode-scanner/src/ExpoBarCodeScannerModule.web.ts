b8b92f4459d1947c12773793c7c22a27c9635d42
Cedric van Putten <me@bycedric.com>
2021-06-02 14:39:21 +0200

    [barcode-scanner] Add empty permission responses for web

--- packages/expo-barcode-scanner/src/ExpoBarCodeScannerModule.web.ts
+++ packages/expo-barcode-scanner/src/ExpoBarCodeScannerModule.web.ts
@@ -1,3 +1,12 @@
+import { PermissionResponse, PermissionStatus } from 'expo-modules-core';
+
+const noPermissionResponse: PermissionResponse = {
+  status: PermissionStatus.UNDETERMINED,
+  canAskAgain: true,
+  granted: false,
+  expires: 'never',
+};
+
 export default {
   get name(): string {
     return 'ExpoBarCodeScannerModule';
@@ -24,5 +33,11 @@ export default {
   },
   get Type() {
     return { front: 'front', back: 'back' };
+  },
+  async getPermissionsAsync(): Promise<PermissionResponse> {
+    return noPermissionResponse;
+  },
+  async requestPermissionsAsync(): Promise<PermissionResponse> {
+    return noPermissionResponse;
   },
 };
