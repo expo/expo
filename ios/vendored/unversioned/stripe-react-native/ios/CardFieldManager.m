bded001a44fb70753d81b691940f5c94b7d1db37
cruzach <charliecruzan@gmail.com>
2021-06-04 14:20:43 -0400

    upgrade stripe-react-native

--- ios/vendored/unversioned/stripe-react-native/ios/CardFieldManager.m
+++ ios/vendored/unversioned/stripe-react-native/ios/CardFieldManager.m
@@ -8,4 +8,6 @@ RCT_EXPORT_VIEW_PROPERTY(onCardChange, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onFocusChange, RCTDirectEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(cardStyle, NSDictionary)
 RCT_EXPORT_VIEW_PROPERTY(placeholder, NSDictionary)
+RCT_EXPORT_VIEW_PROPERTY(autofocus, BOOL)
+RCT_EXPORT_VIEW_PROPERTY(dangerouslyGetFullCardDetails, BOOL)
 @end
--- ios/vendored/unversioned/stripe-react-native/ios/Errors.swift
+++ ios/vendored/unversioned/stripe-react-native/ios/Errors.swift
@@ -1,3 +1,5 @@
+import Stripe
+
 enum ConfirmPaymentErrorType: String {
     case Failed, Canceled, Unknown
 }
@@ -18,17 +20,66 @@ enum RetrievePaymentIntentErrorType: String {
     case Unknown
 }
 
+enum RetrieveSetupIntentErrorType: String {
+    case Unknown
+}
+
 enum PaymentSheetErrorType: String {
     case Failed, Canceled
 }
 
+enum CreateTokenErrorType: String {
+    case Failed
+}
+
 class Errors {
-    class func createError (code: String, message: String) -> NSDictionary {
-        let error: NSDictionary = [
+    class func createError (_ code: String, _ message: String?) -> NSDictionary {
+        let value: NSDictionary = [
             "code": code,
-            "message": message,
+            "message": message ?? NSNull(),
+            "localizedMessage": message ?? NSNull(),
+            "declineCode": NSNull(),
+            "stripeErrorCode": NSNull(),
+            "type": NSNull()
         ]
         
-        return error
+        return ["error": value]
+    }
+    class func createError (_ code: String, _ error: NSError?) -> NSDictionary {
+        let value: NSDictionary = [
+            "code": code,
+            "message": error?.userInfo["com.stripe.lib:ErrorMessageKey"] ?? error?.userInfo["NSLocalizedDescription"] ?? NSNull(),
+            "localizedMessage": error?.userInfo["NSLocalizedDescription"] ?? NSNull(),
+            "declineCode": error?.userInfo["com.stripe.lib:DeclineCodeKey"] ?? NSNull(),
+            "stripeErrorCode": error?.userInfo["com.stripe.lib:StripeErrorCodeKey"] ?? NSNull(),
+            "type": error?.userInfo["com.stripe.lib:StripeErrorTypeKey"] ?? NSNull(),
+        ]
+        
+        return ["error": value]
+    }
+    class func createError (_ code: String, _ error: STPSetupIntentLastSetupError?) -> NSDictionary {
+        let value: NSDictionary = [
+            "code": code,
+            "message": error?.message ?? NSNull(),
+            "localizedMessage": error?.message ?? NSNull(),
+            "declineCode": error?.declineCode ?? NSNull(),
+            "stripeErrorCode": error?.code ?? NSNull(),
+            "type": Mappers.mapFromSetupIntentLastPaymentErrorType(error?.type) ?? NSNull()
+        ]
+        
+        return ["error": value]
+    }
+    
+    class func createError (_ code: String, _ error: STPPaymentIntentLastPaymentError?) -> NSDictionary {
+        let value: NSDictionary = [
+            "code": code,
+            "message": error?.message ?? NSNull(),
+            "localizedMessage": error?.message ?? NSNull(),
+            "declineCode": error?.declineCode ?? NSNull(),
+            "stripeErrorCode": error?.code ?? NSNull(),
+            "type": Mappers.mapFromPaymentIntentLastPaymentErrorType(error?.type) ?? NSNull()
+        ]
+        
+        return ["error": value]
     }
 }
