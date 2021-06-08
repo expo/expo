ac43a3f3af6e33a9ba9ab4e748f4521f87cee9df
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-06-08 08:30:46 +0200

    [barcode-scanner][web] Add permission requester (#13109)
    # Why
    
    Part of ENG-1113.
    
    # How
    
    Added a camera permission requester to the web implementation of the BarCodeScanner.
    
    > Note: I think that the BarCodeScanner isn't available on the web. However, you can scan the code using the `expo-camera`. That's why I decided to add a requester who actually asking for camera permissions.
    
    # Test Plan
    
    - NCL

--- packages/expo-barcode-scanner/build/ExpoBarCodeScannerModule.web.js
+++ packages/expo-barcode-scanner/build/ExpoBarCodeScannerModule.web.js
@@ -1,3 +1,91 @@
+import { UnavailabilityError } from '@unimodules/core';
+import { PermissionStatus } from 'expo-modules-core';
+function getUserMedia(constraints) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        return navigator.mediaDevices.getUserMedia(constraints);
+    }
+    // Some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    // Here, we will just add the getUserMedia property if it's missing.
+    // First get ahold of the legacy getUserMedia, if present
+    const getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        function () {
+            const error = new Error('Permission unimplemented');
+            error.code = 0;
+            error.name = 'NotAllowedError';
+            throw error;
+        };
+    return new Promise((resolve, reject) => {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+}
+function handleGetUserMediaError({ message }) {
+    // name: NotAllowedError
+    // code: 0
+    if (message === 'Permission dismissed') {
+        return {
+            status: PermissionStatus.UNDETERMINED,
+            expires: 'never',
+            canAskAgain: true,
+            granted: false,
+        };
+    }
+    else {
+        return {
+            status: PermissionStatus.DENIED,
+            expires: 'never',
+            canAskAgain: true,
+            granted: false,
+        };
+    }
+}
+async function handleRequestPermissionsAsync() {
+    try {
+        await getUserMedia({
+            video: true,
+        });
+        return {
+            status: PermissionStatus.GRANTED,
+            expires: 'never',
+            canAskAgain: true,
+            granted: true,
+        };
+    }
+    catch ({ message }) {
+        return handleGetUserMediaError({ message });
+    }
+}
+async function handlePermissionsQueryAsync() {
+    if (!navigator?.permissions?.query) {
+        throw new UnavailabilityError('expo-barcode-scanner', 'navigator.permissions API is not available');
+    }
+    const { state } = await navigator.permissions.query({ name: 'camera' });
+    switch (state) {
+        case 'prompt':
+            return {
+                status: PermissionStatus.UNDETERMINED,
+                expires: 'never',
+                canAskAgain: true,
+                granted: false,
+            };
+        case 'granted':
+            return {
+                status: PermissionStatus.GRANTED,
+                expires: 'never',
+                canAskAgain: true,
+                granted: true,
+            };
+        case 'denied':
+            return {
+                status: PermissionStatus.DENIED,
+                expires: 'never',
+                canAskAgain: true,
+                granted: false,
+            };
+    }
+}
 export default {
     get name() {
         return 'ExpoBarCodeScannerModule';
@@ -24,6 +112,12 @@ export default {
     },
     get Type() {
         return { front: 'front', back: 'back' };
+    },
+    async requestPermissionsAsync() {
+        return handleRequestPermissionsAsync();
+    },
+    async getPermissionsAsync() {
+        return handlePermissionsQueryAsync();
     },
 };
 //# sourceMappingURL=ExpoBarCodeScannerModule.web.js.map
