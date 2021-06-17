3d99adbfb546492ac5813a04feea5e5d731198a6
andy <40680668+ajsmth@users.noreply.github.com>
2021-06-17 09:44:32 -0700

    Update packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/singletons/SplashScreen.kt
    Co-authored-by: Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>

--- packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/singletons/SplashScreen.kt
+++ packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/singletons/SplashScreen.kt
@@ -90,11 +90,11 @@ object SplashScreen : SingletonModule {
   @JvmStatic
   @JvmOverloads
   fun show(
-          activity: Activity,
-          splashScreenViewController: SplashScreenViewController,
-          statusBarTranslucent: Boolean,
-          successCallback: () -> Unit = {},
-          failureCallback: (reason: String) -> Unit = { Log.w(TAG, it) }
+  activity: Activity,
+  splashScreenViewController: SplashScreenViewController,
+  statusBarTranslucent: Boolean,
+  successCallback: () -> Unit = {},
+  failureCallback: (reason: String) -> Unit = { Log.w(TAG, it) }
   ) {
     // SplashScreen.show can only be called once per activity
     if (controllers.containsKey(activity)) {
