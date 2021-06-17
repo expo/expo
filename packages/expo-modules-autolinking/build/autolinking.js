3d3028763879273bd26500967e503cd6d8168100
Tomasz Sapeta <1714764+tsapeta@users.noreply.github.com>
2021-06-18 01:03:23 +0200

    [autolinking] Support for expo-module.config.json (#13323)

--- packages/expo-modules-autolinking/build/autolinking.js
+++ packages/expo-modules-autolinking/build/autolinking.js
@@ -9,8 +9,8 @@ const fast_glob_1 = __importDefault(require("fast-glob"));
 const find_up_1 = __importDefault(require("find-up"));
 const fs_extra_1 = __importDefault(require("fs-extra"));
 const path_1 = __importDefault(require("path"));
-// TODO: Rename to `expo-module.json`
-const EXPO_MODULE_CONFIG_FILENAME = 'unimodule.json';
+// Names of the config files. From lowest to highest priority.
+const EXPO_MODULE_CONFIG_FILENAMES = ['unimodule.json', 'expo-module.config.json'];
 /**
  * Resolves autolinking search paths. If none is provided, it accumulates all node_modules when
  * going up through the path components. This makes workspaces work out-of-the-box without any configs.
@@ -49,12 +49,22 @@ async function findModulesAsync(providedOptions) {
     const options = await mergeLinkingOptionsAsync(providedOptions);
     const results = {};
     for (const searchPath of options.searchPaths) {
-        const paths = await fast_glob_1.default([`*/${EXPO_MODULE_CONFIG_FILENAME}`, `@*/*/${EXPO_MODULE_CONFIG_FILENAME}`], {
+        const bracedFilenames = '{' + EXPO_MODULE_CONFIG_FILENAMES.join(',') + '}';
+        const paths = await fast_glob_1.default([`*/${bracedFilenames}`, `@*/*/${bracedFilenames}`], {
             cwd: searchPath,
         });
-        for (const packageConfigPath of paths) {
+        // If the package has multiple configs (e.g. `unimodule.json` and `expo-module.config.json` during the transition time)
+        // then we want to give `expo-module.config.json` the priority.
+        const uniqueConfigPaths = Object.values(paths.reduce((acc, configPath) => {
+            const dirname = path_1.default.dirname(configPath);
+            if (!acc[dirname] || configPriority(configPath) > configPriority(acc[dirname])) {
+                acc[dirname] = configPath;
+            }
+            return acc;
+        }, {}));
+        for (const packageConfigPath of uniqueConfigPaths) {
             const packagePath = await fs_extra_1.default.realpath(path_1.default.join(searchPath, path_1.default.dirname(packageConfigPath)));
-            const packageConfig = require(path_1.default.join(packagePath, EXPO_MODULE_CONFIG_FILENAME));
+            const packageConfig = require(path_1.default.join(packagePath, path_1.default.basename(packageConfigPath)));
             const { name, version } = require(path_1.default.join(packagePath, 'package.json'));
             if (options.exclude?.includes(name) || !packageConfig.platforms?.includes(options.platform)) {
                 continue;
@@ -151,4 +161,10 @@ async function generatePackageListAsync(modules, options) {
     }
 }
 exports.generatePackageListAsync = generatePackageListAsync;
+/**
+ * Returns the priority of the config at given path. Higher number means higher priority.
+ */
+function configPriority(fullpath) {
+    return EXPO_MODULE_CONFIG_FILENAMES.indexOf(path_1.default.basename(fullpath));
+}
 //# sourceMappingURL=autolinking.js.map
