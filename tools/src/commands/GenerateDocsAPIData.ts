406779149d7b3b93ba1ee2836bfb72e5fd72a799
Bartosz Kaszubowski <gosimek@gmail.com>
2021-06-17 10:30:41 +0200

    Update tools/src/commands/GenerateDocsAPIData.ts
    Co-authored-by: Tomasz Sapeta <1714764+tsapeta@users.noreply.github.com>

--- tools/src/commands/GenerateDocsAPIData.ts
+++ tools/src/commands/GenerateDocsAPIData.ts
@@ -5,7 +5,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import recursiveOmitBy from 'recursive-omit-by';
 
-import {EXPO_DIR, PACKAGES_DIR} from '../Constants';
+import { EXPO_DIR, PACKAGES_DIR } from '../Constants';
 import logger from '../Logger';
 
 type ActionOptions = {
