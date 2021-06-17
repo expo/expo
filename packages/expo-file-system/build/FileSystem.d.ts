7fbcde8e2598aca3309ae84a14e73e231704e6c9
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-06-17 15:32:02 +0200

    [file-system][js] Make network task cancelable

--- packages/expo-file-system/build/FileSystem.d.ts
+++ packages/expo-file-system/build/FileSystem.d.ts
@@ -1,6 +1,5 @@
-import { EventEmitter, Subscription } from 'expo-modules-core';
-import { DownloadOptions, DownloadPauseState, DownloadProgressCallback, DownloadProgressData, DownloadResult, EncodingType, FileInfo, FileSystemAcceptedUploadHttpMethod, FileSystemDownloadResult, FileSystemRequestDirectoryPermissionsResult, FileSystemSessionType, FileSystemUploadOptions, FileSystemUploadResult, FileSystemUploadType, ProgressEvent, ReadingOptions, WritingOptions } from './FileSystem.types';
-export { DownloadOptions, DownloadPauseState, DownloadProgressCallback, DownloadProgressData, DownloadResult, EncodingType, FileInfo, FileSystemDownloadResult, FileSystemRequestDirectoryPermissionsResult, FileSystemAcceptedUploadHttpMethod, FileSystemSessionType, FileSystemUploadOptions, FileSystemUploadResult, FileSystemUploadType, ProgressEvent, ReadingOptions, WritingOptions, };
+import { DownloadOptions, DownloadPauseState, DownloadProgressCallback, NetworkTaskProgressCallback, DownloadProgressData, UploadProgressData, DownloadResult, EncodingType, FileInfo, FileSystemAcceptedUploadHttpMethod, FileSystemDownloadResult, FileSystemRequestDirectoryPermissionsResult, FileSystemSessionType, FileSystemUploadOptions, FileSystemUploadResult, FileSystemUploadType, ReadingOptions, WritingOptions } from './FileSystem.types';
+export { DownloadOptions, DownloadPauseState, DownloadProgressCallback, DownloadProgressData, DownloadResult, EncodingType, FileInfo, FileSystemDownloadResult, FileSystemRequestDirectoryPermissionsResult, FileSystemAcceptedUploadHttpMethod, FileSystemSessionType, FileSystemUploadOptions, FileSystemUploadResult, FileSystemUploadType, ReadingOptions, WritingOptions, };
 export declare const documentDirectory: string | null;
 export declare const cacheDirectory: string | null;
 export declare const bundledAssets: string | null, bundleDirectory: string | null;
@@ -31,23 +30,45 @@ export declare function getFreeDiskStorageAsync(): Promise<number>;
 export declare function getTotalDiskCapacityAsync(): Promise<number>;
 export declare function downloadAsync(uri: string, fileUri: string, options?: DownloadOptions): Promise<FileSystemDownloadResult>;
 export declare function uploadAsync(url: string, fileUri: string, options?: FileSystemUploadOptions): Promise<FileSystemUploadResult>;
-export declare function createDownloadResumable(uri: string, fileUri: string, options?: DownloadOptions, callback?: DownloadProgressCallback, resumeData?: string): DownloadResumable;
-export declare class DownloadResumable {
-    _uuid: string;
-    _url: string;
-    _fileUri: string;
-    _options: DownloadOptions;
-    _resumeData?: string;
-    _callback?: DownloadProgressCallback;
-    _subscription?: Subscription | null;
-    _emitter: EventEmitter;
-    constructor(url: string, fileUri: string, options?: DownloadOptions, callback?: DownloadProgressCallback, resumeData?: string);
+export declare function createDownloadResumable(uri: string, fileUri: string, options?: DownloadOptions, callback?: NetworkTaskProgressCallback<DownloadProgressData>, resumeData?: string): DownloadResumable;
+export declare function createUploadTask(url: string, fileUri: string, options?: FileSystemUploadOptions, callback?: NetworkTaskProgressCallback<UploadProgressData>): UploadTask;
+export declare abstract class NetworkTask<T extends DownloadProgressData | UploadProgressData> {
+    private _uuid;
+    protected taskWasCanceled: boolean;
+    private emitter;
+    private subscription?;
+    cancelAsync(): Promise<void>;
+    protected checkIfTaskWasCanceled(): boolean;
+    protected get uuid(): string;
+    protected abstract getEventName(): string;
+    protected abstract getCallback(): NetworkTaskProgressCallback<T> | undefined;
+    protected addSubscription(): void;
+    protected removeSubscription(): void;
+}
+export declare class UploadTask extends NetworkTask<UploadProgressData> {
+    private url;
+    private fileUri;
+    private callback?;
+    private options;
+    constructor(url: string, fileUri: string, options?: FileSystemUploadOptions, callback?: NetworkTaskProgressCallback<UploadProgressData> | undefined);
+    protected getEventName(): string;
+    protected getCallback(): NetworkTaskProgressCallback<UploadProgressData> | undefined;
+    uploadAsync(): Promise<FileSystemUploadResult | undefined>;
+}
+export declare class DownloadResumable extends NetworkTask<DownloadProgressData> {
+    private url;
+    private _fileUri;
+    private options;
+    private callback?;
+    private resumeData?;
+    constructor(url: string, _fileUri: string, options?: DownloadOptions, callback?: NetworkTaskProgressCallback<DownloadProgressData> | undefined, resumeData?: string | undefined);
+    get fileUri(): string;
+    protected getEventName(): string;
+    protected getCallback(): NetworkTaskProgressCallback<DownloadProgressData> | undefined;
     downloadAsync(): Promise<FileSystemDownloadResult | undefined>;
     pauseAsync(): Promise<DownloadPauseState>;
     resumeAsync(): Promise<FileSystemDownloadResult | undefined>;
     savable(): DownloadPauseState;
-    _addSubscription(): void;
-    _removeSubscription(): void;
 }
 /**
  * Android only
