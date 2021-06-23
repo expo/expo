f703d3932e0bec261b4de35f3bf74c548b4dc607
Cedric van Putten <me@bycedric.com>
2021-06-23 18:56:09 +0200

    [docs] Add basic typography and link from website

---
+++ docs/ui/foundations/breakpoints.ts
@@ -0,0 +1,11 @@
+export const breakpoints = {
+  narrowMobile: 340,
+  mobile: 420,
+  mobileWithGutters: 468,
+  narrow: 740,
+  narrowWithGutters: 788,
+  single: 960,
+  singleWithGutters: 1008,
+  max: 1200,
+  maxWithGutters: 1248,
+};
