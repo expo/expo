3eb7510f95e861668dbac05dd5fadfb214a42f47
Eric Samelson <esamelson@users.noreply.github.com>
2021-06-24 11:09:24 -0700

    Apply suggestions from code review

--- packages/expo-dev-launcher/src/DevLauncher.ts
+++ packages/expo-dev-launcher/src/DevLauncher.ts
@@ -3,6 +3,6 @@ export { disableErrorHandling } from './DevLauncherErrorManager';
 
 export function registerErrorHandlers() {
   console.warn(
-    'DevLauncher.registerErrorHandlers was deprecated. To enable error handlers you need to import "expo-dev-launcher" on top of your index.js.'
+    'DevLauncher.registerErrorHandlers has been deprecated. To enable error handlers you need to import "expo-dev-launcher" at the top of your index.js.'
   );
 }
