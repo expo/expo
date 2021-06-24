a61c675b3fee76fede384690e394a37144f51e3c
Eric Samelson <eric@expo.io>
2021-06-24 11:13:34 -0700

    build

--- packages/expo-dev-launcher/build/DevLauncher.js
+++ packages/expo-dev-launcher/build/DevLauncher.js
@@ -1,6 +1,6 @@
 import './setUpErrorHandler.fx';
 export { disableErrorHandling } from './DevLauncherErrorManager';
 export function registerErrorHandlers() {
-    console.warn('DevLauncher.registerErrorHandlers was deprecated. To enable error handlers you need to import "expo-dev-launcher" on top of your index.js.');
+    console.warn('DevLauncher.registerErrorHandlers has been deprecated. To enable error handlers you need to import "expo-dev-launcher" at the top of your index.js.');
 }
 //# sourceMappingURL=DevLauncher.js.map
