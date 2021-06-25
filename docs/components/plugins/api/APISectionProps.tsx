ffc1cfa86e7a9f8f3bd7b9035b93bcaa97889203
Bartosz Kaszubowski <gosimek@gmail.com>
2021-06-25 12:28:37 +0200

    [app-loading] add doc comments in source (#13396)
    # Why
    
    To utilize the auto generation feature for API section the package source code must contain the relevant documentation.
    
    Refs #12239
    
    Requires #13337 🤓 (fix for the duplicate `Types` section for components)
    
    Fixes ENG-1412
    
    # How
    
    This PR moves the current documentation of `AppLoading` component API - https://docs.expo.io/versions/latest/sdk/app-loading - (with minor tweaks and addition) to the package source code.
    
    The changeset also includes move of props to separate type file and an export of component `Props` as `AppLoadingProps` (similar to the `BlurView` and `LinearGradient` components).
    
    There were also small changes to the `APISection` component:
    * add ability to extract props data from Class Component (in the similar way which `defaultProps` were extracted before)
    * more generic support for deprecation notes (display no longer matter on the entry kind)
    * deduplicate component props if props type is set to `union`
    * addition of `Error` type to non-linkable types list, new test for `resolveTypeName` with non-linkable type
    * small simplification of the `PropsData` type and structure
    
    # Test Plan 
    
    The changes and generated content have been tests running docs website on `localhost`.
    
    # Preview
    
    <img width="1507" alt="Screenshot 2021-06-24 at 17 46 30" src="https://user-images.githubusercontent.com/719641/123293406-3f1f2500-d514-11eb-90ea-1a496bc7335f.png">

--- docs/components/plugins/api/APISectionProps.tsx
+++ docs/components/plugins/api/APISectionProps.tsx
@@ -10,7 +10,6 @@ import {
   PropData,
   PropsDefinitionData,
   TypeDeclarationData,
-  TypePropertyData,
 } from '~/components/plugins/api/APIDataTypes';
 import {
   CommentTextBlock,
@@ -34,7 +33,7 @@ const extractDefaultPropValue = (
     return annotationDefault[0].text;
   }
   return defaultProps?.type?.declaration?.children?.filter(
-    (defaultProp: TypePropertyData) => defaultProp.name === name
+    (defaultProp: PropData) => defaultProp.name === name
   )[0]?.defaultValue;
 };
 
@@ -64,14 +63,17 @@ const renderProps = (
   { name, type }: PropsDefinitionData,
   defaultValues: DefaultPropsDefinitionData
 ): JSX.Element => {
-  const propsDeclarations = type.types?.filter((e: TypeDeclarationData) => e.declaration);
+  const propsDeclarations = type.types
+    ?.filter((t: TypeDeclarationData) => t.declaration)
+    .map(def => def?.declaration?.children)
+    .flat()
+    .filter((dec, i, arr) => arr.findIndex(t => t?.name === dec?.name) === i);
+
   return (
     <div key={`props-definition-${name}`}>
       <UL>
-        {propsDeclarations?.map((def: TypeDeclarationData) =>
-          def.declaration?.children?.map((prop: PropData) =>
-            renderProp(prop, extractDefaultPropValue(prop, defaultValues))
-          )
+        {propsDeclarations?.map(prop =>
+          prop ? renderProp(prop, extractDefaultPropValue(prop, defaultValues)) : null
         )}
       </UL>
       {renderInheritedProps(type.types)}
