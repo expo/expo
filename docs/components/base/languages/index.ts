af3546da23401de372b8593c932229a90253f7e3
Evan Bacon <baconbrix@gmail.com>
2021-07-06 12:19:58 -0600

    Added info about manifest merging to config plugin docs (#13444)
    * Added info about manifest merging to config plugin docs
    
    * Added properties language
    
    * Added example
    
    * Fixed comments

--- docs/components/base/languages/index.ts
+++ docs/components/base/languages/index.ts
@@ -4,12 +4,14 @@ import { installGroovy } from './groovy';
 import { installJson5 } from './json5';
 import { installKotlin } from './kotlin';
 import { installObjectiveC } from './objectivec';
+import { installProperties } from './properties';
 import { installRuby } from './ruby';
 import { installSwift } from './swift';
 
 export function installLanguages(prism: typeof Prism) {
   installGroovy(prism);
   installObjectiveC(prism);
+  installProperties(prism);
   installRuby(prism);
   installJson5(prism);
   installKotlin(prism);
