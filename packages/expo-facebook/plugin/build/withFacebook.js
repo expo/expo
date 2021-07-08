46f0d85d2ebca41155020f19c1ee53329307a484
Evan Bacon <baconbrix@gmail.com>
2021-07-07 19:29:27 -0600

    Remove swift noop plugin

--- packages/expo-facebook/plugin/build/withFacebook.js
+++ packages/expo-facebook/plugin/build/withFacebook.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config_plugins_1 = require("@expo/config-plugins");
 const withFacebookAndroid_1 = require("./withFacebookAndroid");
 const withFacebookIOS_1 = require("./withFacebookIOS");
-const withNoopSwiftFile_1 = require("./withNoopSwiftFile");
 const withSKAdNetworkIdentifiers_1 = require("./withSKAdNetworkIdentifiers");
 const pkg = require('expo-facebook/package.json');
 const withFacebook = (config, props) => {
@@ -13,7 +12,6 @@ const withFacebook = (config, props) => {
     config = withFacebookIOS_1.withUserTrackingPermission(config, props);
     // https://developers.facebook.com/docs/SKAdNetwork
     config = withSKAdNetworkIdentifiers_1.withSKAdNetworkIdentifiers(config, ['v9wttpbfk9.skadnetwork', 'n38lu8286q.skadnetwork']);
-    config = withNoopSwiftFile_1.withNoopSwiftFile(config);
     return config;
 };
 exports.default = config_plugins_1.createRunOncePlugin(withFacebook, pkg.name, pkg.version);
