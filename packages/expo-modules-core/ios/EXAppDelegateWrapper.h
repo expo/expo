2eb711de66456d27fa8b8d16101753c01018d43a
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-09 13:25:02 +0200

    Revert "Move `ModuleRegistryAdapter` from `AppDelegate.swift` to `EXAppDelegateWrapper.h`"
    This reverts commit c61d820f1dac3a3bcde75ea0768b5efb1d9dd999.

--- packages/expo-modules-core/ios/EXAppDelegateWrapper.h
+++ packages/expo-modules-core/ios/EXAppDelegateWrapper.h
@@ -2,14 +2,11 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "EXModuleRegistryAdapter.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 NS_SWIFT_NAME(AppDelegateWrapper)
 @interface EXAppDelegateWrapper : UIResponder <UIApplicationDelegate>
-
-@property (nonatomic, strong) EXModuleRegistryAdapter *moduleRegistryAdapter;
 
 @end
 
