e5dd4d6a7bc07510a1593511ae399b6f8ac76496
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-12 15:22:42 +0200

    Create Hybrid Class

---
+++ packages/expo-av/android/src/main/cpp/JMediaPlayerData.cpp
@@ -0,0 +1,67 @@
+//
+// Created by Marc Rousavy on 12.07.21.
+//
+
+#include "JMediaPlayerData.h"
+
+#include <jni.h>
+#include <fbjni/fbjni.h>
+
+#include <memory>
+#include <string>
+
+namespace expo {
+
+using namespace facebook;
+using namespace jni;
+
+using TSelf = local_ref<HybridClass<expo::JMediaPlayerData>::jhybriddata>;
+
+TSelf JMediaPlayerData::initHybrid(alias_ref<HybridClass::jhybridobject> jThis) {
+    return makeCxxInstance(jThis);
+}
+
+void JMediaPlayerData::registerNatives() {
+    registerHybrid({
+       makeNativeMethod("initHybrid", JMediaPlayerData::initHybrid),
+       makeNativeMethod("sampleBufferCallback", JMediaPlayerData::sampleBufferCallback),
+    });
+}
+
+void JMediaPlayerData::sampleBufferCallback(jint sampleBuffer) {
+    if (sampleBufferCallback_ == nullptr) {
+        __android_log_write(ANDROID_LOG_WARN, TAG, "Sample Buffer Callback is null!");
+        setEnableSampleBufferCallback(false);
+        return;
+    }
+
+    auto sampleBufferStrong = make_local(sampleBuffer);
+    try {
+        sampleBufferCallback_(sampleBufferStrong);
+    } catch (const std::exception& exception) {
+        auto message = "Sample Buffer Callback threw an error! " + std::string(exception.what());
+        __android_log_write(ANDROID_LOG_ERROR, TAG, message.c_str());
+    }
+}
+
+void JMediaPlayerData::setEnableSampleBufferCallback(bool enable) {
+    if (enable) {
+        __android_log_write(ANDROID_LOG_INFO, TAG, "Enabling Sample Buffer Callback...");
+    } else {
+        __android_log_write(ANDROID_LOG_INFO, TAG, "Disabling Sample Buffer Callback...");
+    }
+    static const auto javaMethod = javaPart_->getClass()->getMethod<void(bool)>("setEnableSampleBufferCallback");
+    javaMethod(javaPart_.get(), enable);
+}
+
+void JMediaPlayerData::setFrameProcessor(const FrameProcessor&& frameProcessor) {
+    sampleBufferCallback_ = frameProcessor;
+    setEnableSampleBufferCallback(true);
+}
+
+void JMediaPlayerData::unsetSampleBufferCallback() {
+    sampleBufferCallback_ = nullptr;
+    setEnableSampleBufferCallback(false);
+}
+
+} // namespace expo
