274479a430f5daf82973ba8de972fc825fc5dcaa
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-13 11:10:01 +0200

    Initialize JNI

---
+++ packages/expo-av/android/src/main/cpp/EXAV.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Marc Rousavy on 13.07.21.
+//
+
+#include <jni.h>
+#include <fbjni/fbjni.h>
+#include "JAVManager.h"
+#include "JMediaPlayerData.h"
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
+    return facebook::jni::initialize(vm, [] {
+        expo::JAVManager::registerNatives();
+        expo::JMediaPlayerData::registerNatives();
+    });
+}
