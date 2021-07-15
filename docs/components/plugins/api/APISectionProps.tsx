1ef472c36504020442ffa364bd2071a6345936ee
Bartosz Kaszubowski <gosimek@gmail.com>
2021-07-15 21:22:30 +0200

    [docs] add "Components" section to the generated docs, migrate `AppleAuthenticationButton` (#13567)
    Co-authored-by: Bartłomiej Bukowski <bartlomiejbukowski.b@gmail.com>

--- docs/components/plugins/api/APISectionProps.tsx
+++ docs/components/plugins/api/APISectionProps.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { InlineCode } from '~/components/base/code';
 import { LI, UL } from '~/components/base/list';
 import { P } from '~/components/base/paragraph';
-import { H2, H4 } from '~/components/plugins/Headings';
+import { H2, H4, H4Code } from '~/components/plugins/Headings';
 import {
   CommentTagData,
   DefaultPropsDefinitionData,
@@ -19,14 +19,15 @@ import {
 
 export type APISectionPropsProps = {
   data: PropsDefinitionData[];
-  defaultProps: DefaultPropsDefinitionData;
+  defaultProps?: DefaultPropsDefinitionData;
+  header?: string;
 };
 
 const UNKNOWN_VALUE = '...';
 
 const extractDefaultPropValue = (
   { comment, name }: PropData,
-  defaultProps: DefaultPropsDefinitionData
+  defaultProps?: DefaultPropsDefinitionData
 ): string | undefined => {
   const annotationDefault = comment?.tags?.filter((tag: CommentTagData) => tag.tag === 'default');
   if (annotationDefault?.length) {
@@ -38,11 +39,15 @@ const extractDefaultPropValue = (
 };
 
 const renderInheritedProp = (ip: TypeDefinitionData) => {
-  return ip?.typeArguments ? (
+  return (
     <LI key={`inherited-prop-${ip.name}-${ip.type}`}>
-      <InlineCode>{resolveTypeName(ip)}</InlineCode>
+      {ip?.typeArguments ? (
+        <InlineCode>{resolveTypeName(ip)}</InlineCode>
+      ) : (
+        <InlineCode>{ip.name}</InlineCode>
+      )}
     </LI>
-  ) : null;
+  );
 };
 
 const renderInheritedProps = (data: TypeDefinitionData[] | undefined): JSX.Element | undefined => {
@@ -60,10 +65,12 @@ const renderInheritedProps = (data: TypeDefinitionData[] | undefined): JSX.Eleme
 
 const renderProps = (
   { name, type }: PropsDefinitionData,
-  defaultValues: DefaultPropsDefinitionData
+  defaultValues?: DefaultPropsDefinitionData
 ): JSX.Element => {
-  const propsDeclarations = type.types
-    ?.filter((t: TypeDefinitionData) => t.declaration)
+  const baseTypes = type.types
+    ? type.types?.filter((t: TypeDefinitionData) => t.declaration)
+    : [type];
+  const propsDeclarations = baseTypes
     .map(def => def?.declaration?.children)
     .flat()
     .filter((dec, i, arr) => arr.findIndex(t => t?.name === dec?.name) === i);
@@ -97,10 +104,23 @@ const renderProp = ({ comment, name, type, flags }: PropData, defaultValue?: str
   </LI>
 );
 
-const APISectionProps: React.FC<APISectionPropsProps> = ({ data, defaultProps }) =>
+const APISectionProps: React.FC<APISectionPropsProps> = ({
+  data,
+  defaultProps,
+  header = 'Props',
+}) =>
   data?.length ? (
     <>
-      <H2 key="props-header">Props</H2>
+      {header === 'Props' ? (
+        <H2 key="props-header">{header}</H2>
+      ) : (
+        <>
+          <H4Code key={`${header}-props-header`}>
+            <InlineCode>{header}</InlineCode>
+          </H4Code>
+          <br />
+        </>
+      )}
       {data.map((propsDefinition: PropsDefinitionData) =>
         renderProps(propsDefinition, defaultProps)
       )}
