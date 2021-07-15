4c56f83d092d7242f411cc6af1d197197c011ceb
Kacper Kafara <kacper.kafara@swmansion.com>
2021-07-15 15:40:10 +0200

    [error-recovery][android] Extend unit tests

--- packages/expo-error-recovery/android/src/test/java/expo/modules/errorrecovery/ErrorRecoveryModuleTest.kt
+++ packages/expo-error-recovery/android/src/test/java/expo/modules/errorrecovery/ErrorRecoveryModuleTest.kt
@@ -1,37 +1,70 @@
 package expo.modules.errorrecovery
 
+import android.app.Application
 import androidx.test.core.app.ApplicationProvider
-import io.mockk.mockk
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.robolectric.RobolectricTestRunner
 import org.unimodules.test.core.PromiseMock
 import org.unimodules.test.core.assertResolved
-import org.unimodules.test.core.mockPromise
+
+private const val RECOVERY_STORE_KEY = "recoveredProps"
 
 @RunWith(RobolectricTestRunner::class)
 internal class ErrorRecoveryModuleTest {
-
-  private lateinit var module: ErrorRecoveryModule
-
-  @Before
-  fun initializeMock() {
-    module = ErrorRecoveryModule(ApplicationProvider.getApplicationContext())
-  }
-
   @Test
-  fun `saveRecoveryProps should resolve provided promise when props=null`() {
+  fun `saveRecoveryProps resolves provided promise when props=null`() {
+    val module = ErrorRecoveryModule(ApplicationProvider.getApplicationContext())
     val promise = PromiseMock()
     module.saveRecoveryProps(null, promise)
     assertResolved(promise)
   }
 
   @Test
-  fun `saveRecoveryProps should resolve provided promise`() {
+  fun `saveRecoveryProps resolves provided promise with null when props=null`() {
+    val module = ErrorRecoveryModule(ApplicationProvider.getApplicationContext())
+    val promise = PromiseMock()
+    module.saveRecoveryProps(null, promise)
+    assertResolved(promise)
+    assertNull(promise.resolveValue)
+  }
+
+  @Test
+  fun `saveRecoveryProps resolves provided promise when props!=null`() {
+    val module = ErrorRecoveryModule(ApplicationProvider.getApplicationContext())
     val promise = PromiseMock()
     module.saveRecoveryProps("coding is fun they said", promise)
     assertResolved(promise)
+  }
+
+  @Test
+  fun `saveRecoveryProps resolves provided promise with null when props!=null`() {
+    val module = ErrorRecoveryModule(ApplicationProvider.getApplicationContext())
+    val promise = PromiseMock()
+    module.saveRecoveryProps("coding is fun they said", promise)
+    assertResolved(promise)
+    assertNull(promise.resolveValue)
+  }
+
+  @Test
+  fun `props are saved when props!=null`() {
+    val promise = PromiseMock()
+    val mockProps = "test props"
+    val module = ErrorRecoveryModule(ApplicationProvider.getApplicationContext())
+    module.saveRecoveryProps(mockProps,  promise)
+    val constants = module.constants
+    assertEquals(constants[RECOVERY_STORE_KEY], mockProps)
+  }
+
+  @Test
+  fun `props are saved when props=null`() {
+    val promise = PromiseMock()
+    val module = ErrorRecoveryModule(ApplicationProvider.getApplicationContext())
+    module.saveRecoveryProps(null,  promise)
+    val constants = module.constants
+    assertNull(constants[RECOVERY_STORE_KEY])
   }
 }
