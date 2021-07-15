5c9495095e849eea21b99c28a5156d464c93634e
Kamil Owczarz <kamil.owczarz@swmansion.com>
2021-07-15 13:46:40 +0200

    Rename .java to .kt

--- packages/expo-cellular/android/src/main/java/expo/modules/cellular/CellularPackage.kt
+++
@@ -1,9 +0,0 @@
-package expo.modules.cellular
-
-import android.content.Context
-import org.unimodules.core.BasePackage
-
-class CellularPackage : BasePackage() {
-  override fun createExportedModules(context: Context) =
-    listOf(CellularModule(context))
-}
