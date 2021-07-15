63c1afaf81cff58ee0d2bc4cc72e01e24d8c5fa9
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-15 13:26:20 +0200

    Take viewPort dimensions into consideration on Android

--- packages/expo-image/android/src/main/java/expo/modules/image/svg/SVGDecoder.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/svg/SVGDecoder.kt
@@ -4,6 +4,7 @@ import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.ResourceDecoder
 import com.bumptech.glide.load.engine.Resource
 import com.bumptech.glide.load.resource.SimpleResource
+import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.caverock.androidsvg.SVG
 import com.caverock.androidsvg.SVGParseException
 import java.io.IOException
@@ -22,7 +23,14 @@ class SVGDecoder : ResourceDecoder<InputStream, SVG> {
   @Throws(IOException::class)
   override fun decode(source: InputStream, width: Int, height: Int, options: Options): Resource<SVG>? {
     return try {
-      SimpleResource(SVG.getFromInputStream(source))
+      val svg: SVG = SVG.getFromInputStream(source)
+      if (width != SIZE_ORIGINAL) {
+        svg.documentWidth = width.toFloat();
+      }
+      if (height != SIZE_ORIGINAL) {
+        svg.documentHeight = height.toFloat();
+      }
+      SimpleResource(svg)
     } catch (ex: SVGParseException) {
       throw IOException("Cannot load SVG from stream", ex)
     }
