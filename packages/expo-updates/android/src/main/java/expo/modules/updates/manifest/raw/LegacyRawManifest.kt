383f2540397a3ee816452417b02919413ad32c81
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-07-19 16:13:33 +0200

    [expo-updates] Fix Android tests

--- packages/expo-updates/android/src/main/java/expo/modules/updates/manifest/raw/LegacyRawManifest.kt
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/manifest/raw/LegacyRawManifest.kt
@@ -4,7 +4,7 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
-class LegacyRawManifest(json: JSONObject) : BaseLegacyRawManifest(json) {
+open class LegacyRawManifest(json: JSONObject) : BaseLegacyRawManifest(json) {
   @Throws(JSONException::class)
   fun getBundleKey(): String? = if (json.has("bundleKey")) {
     json.getString("bundleKey")
