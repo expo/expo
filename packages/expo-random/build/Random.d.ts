ec662e3c536a80b62b430249b8a43f00ffdf86be
stdavis <stdavis@utah.gov>
2021-07-20 08:19:35 -0600

    chore: build

--- packages/expo-random/build/Random.d.ts
+++ packages/expo-random/build/Random.d.ts
@@ -1,6 +1,7 @@
 /**
  * Generates completely random bytes using native implementations. The `byteCount` property
  * is a `number` indicating the number of bytes to generate in the form of a `Uint8Array`.
+ * Falls back to `Math.random` during development to prevent issues with React Native Debugger.
  * @param byteCount - A number within the range from `0` to `1024`. Anything else will throw a `TypeError`.
  * @return An array of random bytes with the same length as the `byteCount`.
  */
