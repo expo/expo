ef4460f9c8571dff1692622345e099c1bb3c25cb
Kamil Owczarz <kamil.owczarz@swmansion.com>
2021-07-22 14:11:14 +0200

    Rebase fix

--- packages/expo-cellular/android/src/main/java/expo/modules/cellular/CellularPackage.kt
+++ packages/expo-cellular/android/src/main/java/expo/modules/cellular/CellularPackage.kt
@@ -2,9 +2,8 @@ package expo.modules.cellular
 
 import android.content.Context
 import org.unimodules.core.BasePackage
-import org.unimodules.core.ExportedModule
 
 class CellularPackage : BasePackage() {
   override fun createExportedModules(context: Context) =
-    listOf(CellularModule(context) as ExportedModule)
+    listOf(CellularModule(context))
 }
