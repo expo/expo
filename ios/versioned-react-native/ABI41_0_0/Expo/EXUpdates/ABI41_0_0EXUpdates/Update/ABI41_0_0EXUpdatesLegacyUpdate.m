3ee251f652af1b6cb90b2ca4067407d4494c5702
Test User <jonathan@expo.io>
2021-07-22 13:39:36 -0700

    all versions

--- ios/versioned-react-native/ABI41_0_0/Expo/EXUpdates/ABI41_0_0EXUpdates/Update/ABI41_0_0EXUpdatesLegacyUpdate.m
+++ ios/versioned-react-native/ABI41_0_0/Expo/EXUpdates/ABI41_0_0EXUpdates/Update/ABI41_0_0EXUpdatesLegacyUpdate.m
@@ -95,7 +95,7 @@ static NSString * const ABI41_0_0EXUpdatesExpoTestDomain = @"expo.test";
 
     NSURL *url = [bundledAssetBaseUrl URLByAppendingPathComponent:hash];
 
-    NSString *key = hash;
+    NSString *key = [NSString stringWithFormat:@"%@.%@", hash, type];
     ABI41_0_0EXUpdatesAsset *asset = [[ABI41_0_0EXUpdatesAsset alloc] initWithKey:key type:(NSString *)type];
     asset.url = url;
     asset.mainBundleFilename = filename;
