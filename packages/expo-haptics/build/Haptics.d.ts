5f5e75e40aa0f8902ae71b0fd37a358fc5ff9cb9
Bartosz Kaszubowski <gosimek@gmail.com>
2021-07-28 17:54:16 +0200

    [tools] TypeDoc bump, API data regeneration (#13811)
    * [tools] TypeDoc bump, API data regeneration
    
    * haptics SDK 42 data update

--- packages/expo-haptics/build/Haptics.d.ts
+++ packages/expo-haptics/build/Haptics.d.ts
@@ -2,14 +2,15 @@ import { NotificationFeedbackType, ImpactFeedbackStyle } from './Haptics.types';
 /**
  * The kind of notification response used in the feedback.
  * @param type A notification feedback type that on iOS is directly mapped to [UINotificationFeedbackType](https://developer.apple.com/documentation/uikit/uinotificationfeedbacktype),
- * while on Android these are simulated using [Vibrator](vibrator).
+ * while on Android these are simulated using [Vibrator](https://developer.android.com/reference/android/os/Vibrator).
  * You can use one of `Haptics.NotificationFeedbackType.{Success, Warning, Error}`.
  * @return A `Promise` which fulfils once native size haptics functionality is triggered.
  */
 export declare function notificationAsync(type?: NotificationFeedbackType): Promise<void>;
 /**
  * @param style A collision indicator that on iOS is directly mapped to [`UIImpactFeedbackStyle`](https://developer.apple.com/documentation/uikit/uiimpactfeedbackstyle),
- * while on Android these are simulated using [Vibrator](vibrator). You can use one of `Haptics.ImpactFeedbackStyle.{Light, Medium, Heavy}`.
+ * while on Android these are simulated using [Vibrator](https://developer.android.com/reference/android/os/Vibrator).
+ * You can use one of `Haptics.ImpactFeedbackStyle.{Light, Medium, Heavy}`.
  * @return A `Promise` which fulfils once native size haptics functionality is triggered.
  */
 export declare function impactAsync(style?: ImpactFeedbackStyle): Promise<void>;
