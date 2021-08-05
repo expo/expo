fb7d6b43cea561b4be2ac07359b70a94543ef262
Eric Samelson <eric@expo.io>
2021-08-04 20:03:44 -0700

    [ios] fix scopeKey issues

---
+++ packages/expo-dev-launcher/ios/EXDevLauncherUpdatesHelper.h
@@ -0,0 +1,13 @@
+// Copyright 2021-present 650 Industries. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EXDevLauncherUpdatesHelper : NSObject
+
++ (NSDictionary *)createUpdatesConfigurationWithURL:(NSURL *)url;
+
+@end
+
+NS_ASSUME_NONNULL_END
