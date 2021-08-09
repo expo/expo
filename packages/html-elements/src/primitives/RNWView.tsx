d944ffdd1f1b14e5271674df71b8bb358567b2cd
Evan Bacon <baconbrix@gmail.com>
2021-08-09 13:50:04 -0600

    Update RNWView.tsx

--- packages/html-elements/src/primitives/RNWView.tsx
+++ packages/html-elements/src/primitives/RNWView.tsx
@@ -28,7 +28,6 @@ const forwardPropsList = {
   ...forwardedProps.mouseProps,
   ...forwardedProps.touchProps,
   ...forwardedProps.styleProps,
-  __element: true,
   lang: true,
   onScroll: true,
   onWheel: true,
