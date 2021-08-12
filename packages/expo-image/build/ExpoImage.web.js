d03694a436e4fcd928da083aaa4e958761591afb
andy <40680668+ajsmth@users.noreply.github.com>
2021-08-12 13:14:21 -0700

    cleanup

--- packages/expo-image/build/ExpoImage.web.js
+++ packages/expo-image/build/ExpoImage.web.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Image } from 'react-native';
 export default function ExpoImage({ source, ...props }) {
     const resolvedSource = source ?? {};
-    // @ts-ignore - this is being fixed in the near future anyways
+    // @ts-expect-error - expo-image is being reworked so these types should be revisited
     return React.createElement(Image, { ...props, source: resolvedSource });
 }
 //# sourceMappingURL=ExpoImage.web.js.map
