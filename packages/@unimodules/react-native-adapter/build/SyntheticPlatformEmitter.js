999ead72a1ca2d6936f2e94200bdcc712eef4e70
Evan Bacon <baconbrix@gmail.com>
2021-08-12 12:06:56 -0600

    Upgrade to react native web@0.17.1 (#13947)
    * react-native-web@0.17.1
    
    * babel upgrades
    
    * Update yarn.lock
    
    * Fixed event emitter bugs on web
    
    * Fixed GL tests
    
    * Fixed HTML tests
    
    * Update RNWView.tsx

--- packages/@unimodules/react-native-adapter/build/SyntheticPlatformEmitter.js
+++ packages/@unimodules/react-native-adapter/build/SyntheticPlatformEmitter.js
@@ -1,16 +1,6 @@
-import { DeviceEventEmitter } from 'react-native';
-import { RCTEventEmitter } from './nativeEmitters';
 /**
  * This emitter is used for sending synthetic native events to listeners
  * registered in the API layer with `NativeEventEmitter`.
  */
-class SyntheticPlatformEmitter {
-    constructor() {
-        this._emitter = new RCTEventEmitter(DeviceEventEmitter.sharedSubscriber);
-    }
-    emit(eventName, props) {
-        this._emitter.emit(eventName, props);
-    }
-}
-export default new SyntheticPlatformEmitter();
+export { default } from 'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter';
 //# sourceMappingURL=SyntheticPlatformEmitter.js.map
