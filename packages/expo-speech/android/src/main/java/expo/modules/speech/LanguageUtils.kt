cb0c64cfc802e00f69a946bb685fd697ec6e58e2
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-08-13 11:19:52 +0200

    [expo-speech] Add comments to `LanguageUtils`
    A supplement to #14008
    
    - Added comments about null-unsafe helpers
    - Used getter methods for ISO Language and Country instead of _property access syntax_, because of readibility (`locale.getISO3Language()` vs `locale.isO3Language`)

--- packages/expo-speech/android/src/main/java/expo/modules/speech/LanguageUtils.kt
+++ packages/expo-speech/android/src/main/java/expo/modules/speech/LanguageUtils.kt
@@ -7,22 +7,24 @@ object LanguageUtils {
   private val countryISOCodes: Map<String, Locale> by lazy {
     Locale.getISOCountries().map { country ->
       val locale = Locale("", country)
-      locale.isO3Country.toUpperCase(locale) to locale
+      locale.getISO3Country().toUpperCase(locale) to locale
     }.toMap()
   }
   private val languageISOCodes: Map<String, Locale> by lazy {
     Locale.getISOLanguages().map { language ->
       val locale = Locale(language)
-      locale.isO3Language to locale
+      locale.getISO3Language() to locale
     }.toMap()
   }
 
+  // NOTE: These helpers are null-unsafe and should be called ONLY with codes
+  // returned by Locale.getISO3Country() and Locale.getISO3Language() respectively
   private fun transformCountryISO(code: String) = countryISOCodes[code]!!.country
   private fun transformLanguageISO(code: String) = languageISOCodes[code]!!.language
 
   fun getISOCode(locale: Locale): String {
-    var language = transformLanguageISO(locale.isO3Language)
-    val country = locale.isO3Country
+    var language = transformLanguageISO(locale.getISO3Language())
+    val country = locale.getISO3Country()
     if (country != "") {
       val countryCode = transformCountryISO(country)
       language += "-$countryCode"
