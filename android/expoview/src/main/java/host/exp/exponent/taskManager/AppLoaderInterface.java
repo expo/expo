2dd697a6e9fbd64723fc472cec4d99ff876e0249
Will Schurman <wschurman@gmail.com>
2021-08-17 15:10:20 -0600

    [android] Kotlinize headless and detached app stuff (#14000)

--- android/expoview/src/main/java/host/exp/exponent/taskManager/AppLoaderInterface.java
+++
@@ -1,9 +0,0 @@
-package host.exp.exponent.taskManager;
-
-import expo.modules.apploader.AppLoaderProvider;
-
-import java.util.Map;
-
-public interface AppLoaderInterface {
-  AppRecordInterface loadApp(String appUrl, Map<String, Object> options, AppLoaderProvider.Callback callback);
-}
