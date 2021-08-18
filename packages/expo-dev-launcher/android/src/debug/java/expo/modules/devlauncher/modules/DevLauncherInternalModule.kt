d86f8d47f694aacd0334eced4d2f951705e8dd54
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-08-18 15:42:04 +0200

    [dev-launcher] Fix intent that started activity isn't passed further

--- packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/modules/DevLauncherInternalModule.kt
+++ packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/modules/DevLauncherInternalModule.kt
@@ -100,7 +100,11 @@ class DevLauncherInternalModule(reactContext: ReactApplicationContext?)
 
   @ReactMethod
   fun getPendingDeepLink(promise: Promise) {
-    promise.resolve(intentRegistry.intent?.data?.toString())
+    intentRegistry.intent?.data?.let {
+      promise.resolve(it.toString())
+    }
+
+    promise.resolve(intentRegistry.intent?.action)
   }
 
   private fun onNewPendingIntent(intent: Intent) {
