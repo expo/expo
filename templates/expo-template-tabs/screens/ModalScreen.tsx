42d0b16fde8eed3cd81a55577187cb4fb1fc09e5
Brent Vatne <brentvatne@gmail.com>
2021-08-20 11:14:00 -0700

    [template] Add light status bar for modal

--- templates/expo-template-tabs/screens/ModalScreen.tsx
+++ templates/expo-template-tabs/screens/ModalScreen.tsx
@@ -1,5 +1,6 @@
+import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -10,6 +11,9 @@ export default function ModalScreen() {
       <Text style={styles.title}>Modal</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/ModalScreen.tsx" />
+
+      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
 }
