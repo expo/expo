87708b77d182a487786b8288d07ba8f608afa6dc
Evan Bacon <9664363+EvanBacon@users.noreply.github.com>
2021-08-20 16:53:42 -0600

    Move background location to the expo-location package

--- packages/expo-location/plugin/src/withLocation.ts
+++ packages/expo-location/plugin/src/withLocation.ts
@@ -1,12 +1,30 @@
-import { AndroidConfig, ConfigPlugin, createRunOncePlugin } from '@expo/config-plugins';
+import {
+  AndroidConfig,
+  ConfigPlugin,
+  createRunOncePlugin,
+  withInfoPlist,
+} from '@expo/config-plugins';
 
 const pkg = require('expo-location/package.json');
 const LOCATION_USAGE = 'Allow $(PRODUCT_NAME) to access your location';
+
+const withBackgroundLocation: ConfigPlugin = config => {
+  return withInfoPlist(config, config => {
+    if (!Array.isArray(config.modResults.UIBackgroundModes)) {
+      config.modResults.UIBackgroundModes = [];
+    }
+    if (!config.modResults.UIBackgroundModes.includes('location')) {
+      config.modResults.UIBackgroundModes.push('location');
+    }
+    return config;
+  });
+};
 
 const withLocation: ConfigPlugin<{
   locationAlwaysAndWhenInUsePermission?: string;
   locationAlwaysPermission?: string;
   locationWhenInUsePermission?: string;
+  isIosBackgroundLocationEnabled?: boolean;
   isAndroidBackgroundLocationEnabled?: boolean;
 } | void> = (
   config,
@@ -14,23 +32,30 @@ const withLocation: ConfigPlugin<{
     locationAlwaysAndWhenInUsePermission,
     locationAlwaysPermission,
     locationWhenInUsePermission,
+    isIosBackgroundLocationEnabled,
     isAndroidBackgroundLocationEnabled,
   } = {}
 ) => {
-  if (!config.ios) config.ios = {};
-  if (!config.ios.infoPlist) config.ios.infoPlist = {};
-  config.ios.infoPlist.NSLocationAlwaysAndWhenInUseUsageDescription =
-    locationAlwaysAndWhenInUsePermission ||
-    config.ios.infoPlist.NSLocationAlwaysAndWhenInUseUsageDescription ||
-    LOCATION_USAGE;
-  config.ios.infoPlist.NSLocationAlwaysUsageDescription =
-    locationAlwaysPermission ||
-    config.ios.infoPlist.NSLocationAlwaysUsageDescription ||
-    LOCATION_USAGE;
-  config.ios.infoPlist.NSLocationWhenInUseUsageDescription =
-    locationWhenInUsePermission ||
-    config.ios.infoPlist.NSLocationWhenInUseUsageDescription ||
-    LOCATION_USAGE;
+  if (isIosBackgroundLocationEnabled) {
+    config = withBackgroundLocation(config);
+  }
+
+  config = withInfoPlist(config, config => {
+    config.modResults.NSLocationAlwaysAndWhenInUseUsageDescription =
+      locationAlwaysAndWhenInUsePermission ||
+      config.modResults.NSLocationAlwaysAndWhenInUseUsageDescription ||
+      LOCATION_USAGE;
+    config.modResults.NSLocationAlwaysUsageDescription =
+      locationAlwaysPermission ||
+      config.modResults.NSLocationAlwaysUsageDescription ||
+      LOCATION_USAGE;
+    config.modResults.NSLocationWhenInUseUsageDescription =
+      locationWhenInUsePermission ||
+      config.modResults.NSLocationWhenInUseUsageDescription ||
+      LOCATION_USAGE;
+
+    return config;
+  });
 
   return AndroidConfig.Permissions.withPermissions(
     config,
