1632d19cbb6af7758ef75741e64f153ca0465a50
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-08-22 20:51:19 +0200

    [skip ci] Fix `unable to connect to bundler` on iOS

--- packages/expo-dev-client/e2e/DevLauncher.e2e.ts
+++ packages/expo-dev-client/e2e/DevLauncher.e2e.ts
@@ -10,6 +10,9 @@ function getInvocationManager() {
 }
 
 function getLocalIPAddress(): string {
+  if (device.getPlatform() === 'ios') {
+    return 'http://localhost:8081';
+  }
   return require('os')
     .networkInterfaces()
     .en0.find((elm: { family: string }) => elm.family === 'IPv4').address;
