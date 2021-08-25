22df9292affab47f52741058280db2fe56564c98
Will Schurman <wschurman@gmail.com>
2021-08-24 21:46:12 -0700

    Revert "[android][1/n] Remove unused expoview module duplicates (#14172)" (#14175)
    This reverts commit 178af6f148d590b833a5928a0443779efa94c955.

--- android/expoview/src/main/java/versioned/host/exp/exponent/ExponentPackage.kt
+++ android/expoview/src/main/java/versioned/host/exp/exponent/ExponentPackage.kt
@@ -46,6 +46,7 @@ import versioned.host.exp.exponent.modules.api.notifications.NotificationsModule
 import versioned.host.exp.exponent.modules.api.reanimated.ReanimatedModule
 import versioned.host.exp.exponent.modules.api.safeareacontext.SafeAreaContextPackage
 import versioned.host.exp.exponent.modules.api.screens.RNScreensPackage
+import versioned.host.exp.exponent.modules.api.viewshot.RNViewShotModule
 import versioned.host.exp.exponent.modules.internal.DevMenuModule
 import versioned.host.exp.exponent.modules.test.ExponentTestNativeModule
 import versioned.host.exp.exponent.modules.universal.ExpoModuleRegistryAdapter
@@ -105,7 +106,11 @@ class ExponentPackage : ReactPackage {
 
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
     val isVerified = manifest.isVerified() ?: false
-    val nativeModules: MutableList<NativeModule> = mutableListOf()
+    val nativeModules: MutableList<NativeModule> = mutableListOf(
+      URLHandlerModule(reactContext),
+      ShakeModule(reactContext),
+      KeyboardModule(reactContext)
+    )
 
     if (isKernel) {
       // WHEN_VERSIONING_REMOVE_FROM_HERE
@@ -122,6 +127,7 @@ class ExponentPackage : ReactPackage {
         val experienceKey = ExperienceKey.fromRawManifest(manifest)
         val scopedContext = ScopedContext(reactContext, experienceKey)
         nativeModules.add(NotificationsModule(reactContext, experienceKey, manifest.getStableLegacyID(), experienceProperties))
+        nativeModules.add(RNViewShotModule(reactContext, scopedContext))
         nativeModules.add(RandomModule(reactContext))
         nativeModules.add(ExponentTestNativeModule(reactContext))
         nativeModules.add(PedometerModule(reactContext))
