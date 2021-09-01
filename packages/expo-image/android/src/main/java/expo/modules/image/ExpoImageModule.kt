b3468951ea6d68a05e86d82e8f6dcf41ba2e8500
Kacper Kafara <kacper.kafara@swmansion.com>
2021-09-01 16:24:45 +0200

    [android][image] Add mock `getSize` method to `ExpoImage` component

--- packages/expo-image/android/src/main/java/expo/modules/image/ExpoImageModule.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/ExpoImageModule.kt
@@ -3,15 +3,18 @@ package expo.modules.image
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.request.FutureTarget
+
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.WritableNativeMap
+
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runInterruptible
-import java.lang.Exception
+import kotlin.Exception
 
 /**
  * We need to convert blocking java.util.concurrent.Future result
@@ -40,6 +43,18 @@ class ExpoImageModule(val context: ReactApplicationContext) : ReactContextBaseJa
       } catch (e: Exception) {
         promise.reject("ERR_IMAGE_PREFETCH_FAILURE", "Failed to prefetch the image: ${e.message}", e)
       }
+    }
+  }
+
+  @ReactMethod
+  fun getSize(url: String, promise: Promise) {
+    val sizes = WritableNativeMap()
+    try {
+      sizes.putInt("width", 80)
+      sizes.putInt("height", 100)
+      promise.resolve(sizes)
+    } catch (e: Exception) {
+      promise.reject("ERR_IMAGE_GETSIZE_FAILURE", "Failed to get size of the image: $url", e)
     }
   }
 }
