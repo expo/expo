6482eb9779300e2ddb1906ef691ceb341f4056cc
andy <40680668+ajsmth@users.noreply.github.com>
2021-09-02 10:43:49 -0700

    [picker] upgrade react-native-picker to v1.16.7

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/picker/PickerBlurEvent.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/picker/PickerBlurEvent.java
@@ -21,7 +21,17 @@ public class PickerBlurEvent extends Event<PickerBlurEvent> {
     rctEventEmitter.receiveEvent(getViewTag(), getEventName(), getEventData());
   }
 
-  private WritableMap getEventData() {
+  /**
+   * In RN version 0.65+ getEventData method is introduced
+   * https://github.com/facebook/react-native/commit/72d0ddc16f2f631003c3486e0a59e50c145ec613
+   *
+   * In order to keep compatibility with 0.65+ and not introduce breaking change for that lib,
+   * the only change is changing access modifier from `private` to `protected` without using @override annotation
+   * (idk if it will not bring any "method is hidden, but should overriden" warning, but for now it just works)
+   *
+   * In future versions, where `dispatch` method will be removed, there will be need to make a breaking change here
+   */
+  protected WritableMap getEventData() {
     WritableMap eventData = Arguments.createMap();
     eventData.putInt("target", getViewTag());
     return eventData;
