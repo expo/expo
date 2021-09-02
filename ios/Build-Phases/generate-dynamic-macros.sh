192fd21c9bc6b559db50a2c246ba0d788099d594
andy <40680668+ajsmth@users.noreply.github.com>
2021-09-02 15:47:10 -0700

    Revert "[segmented-control] upgrade segmented control to v2.4.0"
    This reverts commit dc3e7e2341b1cb660f1b54b18354df116f44158d.

--- ios/Build-Phases/generate-dynamic-macros.sh
+++ ios/Build-Phases/generate-dynamic-macros.sh
@@ -7,9 +7,9 @@ if [ -z "$EXPO_TOOLS_DIR" ]; then
 fi
 
 # Sourcing login scripts on macOS-11 runners is broken but can be omitted.
-#if [ -z "$CI" ]; then
-#  source ${EXPO_TOOLS_DIR}/source-login-scripts.sh
-#fi
+if [ -z "$CI" ]; then
+  source ${EXPO_TOOLS_DIR}/source-login-scripts.sh
+fi
 
 export PATH="${SRCROOT}/../bin:$PATH"
 
