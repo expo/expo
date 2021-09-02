05d87d2f31d3995e59f968f1ba7cac39778a60de
Kacper Kafara <kacper.kafara@swmansion.com>
2021-09-02 15:51:52 +0200

    [android][image] Code style adjustments in `BitmapSizeDecoder` class

--- packages/expo-image/android/src/main/java/expo/modules/image/decoding/BitmapSizeDecoder.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/decoding/BitmapSizeDecoder.kt
@@ -1,10 +1,12 @@
 package expo.modules.image.decoding
 
 import android.graphics.BitmapFactory
+
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.ResourceDecoder
 import com.bumptech.glide.load.engine.Resource
 import com.bumptech.glide.load.resource.SimpleResource
+
 import java.io.File
 import java.io.IOException
 
@@ -13,8 +15,7 @@ class BitmapSizeDecoder : ResourceDecoder<File, BitmapFactory.Options> {
   override fun handles(source: File, options: Options): Boolean = true
 
   override fun decode(source: File, width: Int, height: Int, options: Options): Resource<BitmapFactory.Options>? {
-    val bitmapOptions: BitmapFactory.Options = BitmapFactory.Options()
-    bitmapOptions.inJustDecodeBounds = true
+    val bitmapOptions = BitmapFactory.Options().apply { inJustDecodeBounds = true }
     BitmapFactory.decodeFile(source.absolutePath, bitmapOptions)
     return SimpleResource(bitmapOptions)
   }
