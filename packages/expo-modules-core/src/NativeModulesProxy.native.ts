14a22428ae5e63998a3454d5a943b47db45f8016
Kudo Chien <kudo@expo.dev>
2021-09-03 19:27:40 +0800

    [expo-modules-core] Fix UMReactNativeEventEmitter not found issue

--- packages/expo-modules-core/src/NativeModulesProxy.native.ts
+++ packages/expo-modules-core/src/NativeModulesProxy.native.ts
@@ -33,9 +33,9 @@ if (NativeProxy) {
     // On Android only {start,stop}Observing are called on the native module
     // and these should be exported as Expo methods.
     NativeModulesProxy[moduleName].addListener = (...args) =>
-      NativeModules.UMReactNativeEventEmitter.addProxiedListener(moduleName, ...args);
+      NativeModules.EXReactNativeEventEmitter.addProxiedListener(moduleName, ...args);
     NativeModulesProxy[moduleName].removeListeners = (...args) =>
-      NativeModules.UMReactNativeEventEmitter.removeProxiedListeners(moduleName, ...args);
+      NativeModules.EXReactNativeEventEmitter.removeProxiedListeners(moduleName, ...args);
   });
 } else {
   console.warn(
