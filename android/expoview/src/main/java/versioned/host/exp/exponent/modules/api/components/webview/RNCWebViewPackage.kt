f03b25f7b3a7f88825c8cc0cb35b5af7f5d50ee1
Kudo Chien <kudo@expo.dev>
2021-09-06 18:35:04 +0800

    ./gradlew spotlessApply

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/webview/RNCWebViewPackage.kt
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/webview/RNCWebViewPackage.kt
@@ -3,8 +3,7 @@ package versioned.host.exp.exponent.modules.api.components.webview
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.ReactApplicationContext
 
-
-class RNCWebViewPackage: ReactPackage {
+class RNCWebViewPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext) = listOf(
     RNCWebViewModule(reactContext)
   )
