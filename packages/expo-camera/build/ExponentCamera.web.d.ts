5bf6bde1ee593be3482a81962f77e1b396b38d47
Bartosz Kaszubowski <gosimek@gmail.com>
2021-09-15 10:18:14 +0200

    [chore] add `yarn-deduplicate`, deduplicate workspace lock (#14377)
    [home] bump bundle-visualizer

--- packages/expo-camera/build/ExponentCamera.web.d.ts
+++ packages/expo-camera/build/ExponentCamera.web.d.ts
@@ -6,5 +6,5 @@ export interface ExponentCameraRef {
     resumePreview: () => Promise<void>;
     pausePreview: () => Promise<void>;
 }
-declare const ExponentCamera: React.ForwardRefExoticComponent<Pick<CameraNativeProps, "pointerEvents" | "style" | "onCameraReady" | "onMountError" | "onBarCodeScanned" | "onFacesDetected" | "onFaceDetectionError" | "onPictureSaved" | "type" | "flashMode" | "autoFocus" | "focusDepth" | "zoom" | "whiteBalance" | "pictureSize" | "barCodeScannerSettings" | "faceDetectorSettings" | "barCodeScannerEnabled" | "faceDetectorEnabled" | "ratio" | "useCamera2Api" | "poster"> & React.RefAttributes<ExponentCameraRef>>;
+declare const ExponentCamera: React.ForwardRefExoticComponent<Pick<CameraNativeProps, "type" | "flashMode" | "autoFocus" | "whiteBalance" | "pointerEvents" | "style" | "zoom" | "ratio" | "focusDepth" | "onCameraReady" | "useCamera2Api" | "pictureSize" | "onMountError" | "barCodeScannerSettings" | "onBarCodeScanned" | "faceDetectorSettings" | "onFacesDetected" | "poster" | "onFaceDetectionError" | "onPictureSaved" | "barCodeScannerEnabled" | "faceDetectorEnabled"> & React.RefAttributes<ExponentCameraRef>>;
 export default ExponentCamera;
