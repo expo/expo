0b170961ff7d6429077af2c67e1087dafc0310ef
Hein Rutjes <hrutjes@gmail.com>
2021-09-16 15:44:14 +0200

    update build files

--- packages/expo-asset/build/resolveAssetSource.web.js
+++ packages/expo-asset/build/resolveAssetSource.web.js
@@ -3,15 +3,14 @@ import { getAssetByID } from './AssetRegistry';
 import AssetSourceResolver from './AssetSourceResolver';
 let _customSourceTransformer;
 let _serverURL;
-let _scriptURL;
 let _sourceCodeScriptURL;
 function getSourceCodeScriptURL() {
     if (_sourceCodeScriptURL) {
         return _sourceCodeScriptURL;
     }
-    let sourceCode = nativeExtensions && nativeExtensions.SourceCode;
+    let sourceCode = typeof nativeExtensions !== 'undefined' ? nativeExtensions.SourceCode : null;
     if (!sourceCode) {
-        sourceCode = NativeModules && NativeModules.SourceCode;
+        sourceCode = NativeModules?.SourceCode;
     }
     _sourceCodeScriptURL = sourceCode.scriptURL;
     return _sourceCodeScriptURL;
@@ -31,27 +30,6 @@ function getDevServerURL() {
     }
     return _serverURL;
 }
-function _coerceLocalScriptURL(scriptURL) {
-    if (scriptURL) {
-        if (scriptURL.startsWith('assets://')) {
-            // android: running from within assets, no offline path to use
-            return null;
-        }
-        scriptURL = scriptURL.substring(0, scriptURL.lastIndexOf('/') + 1);
-        if (!scriptURL.includes('://')) {
-            // Add file protocol in case we have an absolute file path and not a URL.
-            // This shouldn't really be necessary. scriptURL should be a URL.
-            scriptURL = 'file://' + scriptURL;
-        }
-    }
-    return null;
-}
-function getScriptURL() {
-    if (_scriptURL === undefined) {
-        _scriptURL = _coerceLocalScriptURL(getSourceCodeScriptURL());
-    }
-    return _scriptURL;
-}
 export function setCustomSourceTransformer(transformer) {
     _customSourceTransformer = transformer;
 }
@@ -67,7 +45,7 @@ export default function resolveAssetSource(source) {
     if (!asset) {
         return undefined;
     }
-    const resolver = new AssetSourceResolver(getDevServerURL(), getScriptURL(), asset);
+    const resolver = new AssetSourceResolver(getDevServerURL(), null, asset);
     if (_customSourceTransformer) {
         return _customSourceTransformer(resolver);
     }
