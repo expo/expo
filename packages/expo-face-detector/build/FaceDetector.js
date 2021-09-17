d90d65fcdbd81d887b23724b757fbcade1361293
andy <40680668+ajsmth@users.noreply.github.com>
2021-09-17 15:07:10 -0700

    [face-detector] add numeric enum values for face detector options

--- packages/expo-face-detector/build/FaceDetector.js
+++ packages/expo-face-detector/build/FaceDetector.js
@@ -3,20 +3,20 @@ import ExpoFaceDetector from './ExpoFaceDetector';
 // @docsMissing
 export var FaceDetectorMode;
 (function (FaceDetectorMode) {
-    FaceDetectorMode["fast"] = "fast";
-    FaceDetectorMode["accurate"] = "accurate";
+    FaceDetectorMode[FaceDetectorMode["fast"] = 1] = "fast";
+    FaceDetectorMode[FaceDetectorMode["accurate"] = 2] = "accurate";
 })(FaceDetectorMode || (FaceDetectorMode = {}));
 // @docsMissing
 export var FaceDetectorLandmarks;
 (function (FaceDetectorLandmarks) {
-    FaceDetectorLandmarks["all"] = "all";
-    FaceDetectorLandmarks["none"] = "none";
+    FaceDetectorLandmarks[FaceDetectorLandmarks["all"] = 1] = "all";
+    FaceDetectorLandmarks[FaceDetectorLandmarks["none"] = 2] = "none";
 })(FaceDetectorLandmarks || (FaceDetectorLandmarks = {}));
 // @docsMissing
 export var FaceDetectorClassifications;
 (function (FaceDetectorClassifications) {
-    FaceDetectorClassifications["all"] = "all";
-    FaceDetectorClassifications["none"] = "none";
+    FaceDetectorClassifications[FaceDetectorClassifications["all"] = 1] = "all";
+    FaceDetectorClassifications[FaceDetectorClassifications["none"] = 2] = "none";
 })(FaceDetectorClassifications || (FaceDetectorClassifications = {}));
 // @needsAudit
 /**
