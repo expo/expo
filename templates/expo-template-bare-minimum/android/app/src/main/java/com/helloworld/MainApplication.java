9781212eb3ec89a0b184da712ec7ca4e5ecc725b
Kudo Chien <kudo@expo.io>
2021-09-21 00:30:06 +0800

    [templates] Update templates for the next SDK (#14409)
    # Why
    
    Update templates for the next SDK
    
    # How
    
    - `et update-project-templates`
    - manually updated some dependencies like `react`, `react-dom`, `react-native`, `react-native-web`, `@types/react`
    - migrated bare template from unimodules to expo modules
    - [android] migrate unimodules to expo-modules
    - [android] integrate ReactNativeHostWrapper / ReactActivityDelegateWrapper
    - replace splash screen integration by in-module setup
    - replace expo-updates integration by in-module setup
    - add Podfile.properties.json with hermes support
    - fix other `node_modules` resolution where breaking monorepo support, e.g. hermes-engine or maven repo. 
    - use mavenCentral before jcenter because [jcenter will be a read-only mirror repository](https://jfrog.com/blog/into-the-sunset-bintray-jcenter-gocenter-and-chartcenter/)
    
     Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>
     Co-authored-by: Cedric van Putten <me@bycedric.com>
    
    # Test Plan
    
    ```sh
    expo init -t /path/to/templates/expo-template-bare-minimum sdk43
    cd sdk43
    expo run:android
    expo run:android --variant release
    expo run:ios
    expo run:ios --configuration Release
    ```

--- templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
+++ templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -2,38 +2,29 @@ package com.helloworld;
 
 import android.app.Application;
 import android.content.Context;
-import android.net.Uri;
+import android.content.res.Configuration;
+import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.helloworld.generated.BasePackageList;
 
-import org.unimodules.adapters.react.ReactAdapterPackage;
-import org.unimodules.adapters.react.ModuleRegistryAdapter;
-import org.unimodules.adapters.react.ReactModuleRegistryProvider;
-import org.unimodules.core.interfaces.Package;
-import org.unimodules.core.interfaces.SingletonModule;
-import expo.modules.updates.UpdatesController;
+import expo.modules.ApplicationLifecycleDispatcher;
+import expo.modules.ReactNativeHostWrapper;
 
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 import java.util.List;
-import javax.annotation.Nullable;
 
 public class MainApplication extends Application implements ReactApplication {
-  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(
-    new BasePackageList().getPackageList()
-  );
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
+    this,
+    new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -41,8 +32,10 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
       return packages;
     }
 
@@ -55,25 +48,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected JSIModulePackage getJSIModulePackage() {
       return new ReanimatedJSIModulePackage();
     }
-
-    @Override
-    protected @Nullable String getJSBundleFile() {
-      if (BuildConfig.DEBUG) {
-        return super.getJSBundleFile();
-      } else {
-        return UpdatesController.getInstance().getLaunchAssetFile();
-      }
-    }
-
-    @Override
-    protected @Nullable String getBundleAssetName() {
-      if (BuildConfig.DEBUG) {
-        return super.getBundleAssetName();
-      } else {
-        return UpdatesController.getInstance().getBundleAssetName();
-      }
-    }
-  };
+  });
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -85,11 +60,14 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
 
-    if (!BuildConfig.DEBUG) {
-      UpdatesController.initialize(this);
-    }
-
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    ApplicationLifecycleDispatcher.onApplicationCreate(this);
+  }
+
+  @Override
+  public void onConfigurationChanged(@NonNull Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
   }
 
   /**
--- templates/expo-template-bare-minimum/ios/HelloWorld/AppDelegate.h
+++ templates/expo-template-bare-minimum/ios/HelloWorld/AppDelegate.h
@@ -1,10 +1,9 @@
 #import <Foundation/Foundation.h>
-#import <EXUpdates/EXUpdatesAppController.h>
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-#import <UMCore/UMAppDelegateWrapper.h>
+#import <Expo/Expo.h>
 
-@interface AppDelegate : UMAppDelegateWrapper <RCTBridgeDelegate, EXUpdatesAppControllerDelegate>
+@interface AppDelegate : EXAppDelegateWrapper <RCTBridgeDelegate>
 
 @end
