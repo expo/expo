76824aade533e1f7dbae475ab580c1c7757d0548
Will Schurman <wschurman@gmail.com>
2021-09-20 16:36:47 -0700

    [bare-expo] Run full test-suite

--- apps/bare-expo/e2e/TestSuite-test.native.js
+++ apps/bare-expo/e2e/TestSuite-test.native.js
@@ -3,71 +3,44 @@ import { by, device, element, expect as detoxExpect, waitFor } from 'detox';
 import { sleepAsync } from './Utils';
 import { expectResults } from './utils/report';
 
-let TESTS = [
-  'Basic',
-  // 'Asset',
-  // 'FileSystem',
-  // 'Font',
-  'Permissions',
-  'Blur',
-  'LinearGradient',
-  'Constants',
-  // 'Contacts',
-  'Crypto',
-  // 'GLView',
-  'Haptics',
-  'Localization',
-  // 'SecureStore',
-  // 'Segment',
-  // 'SQLite',
-  'Random',
-  'Permissions',
-  'KeepAwake',
-  'FirebaseCore',
-  'FirebaseAnalytics',
-  // 'Audio',
-  'HTML',
-];
-
+const NUM_TESTS = 67;
 const MIN_TIME = 50000;
 
-describe('test-suite', () => {
-  TESTS.map(testName => {
-    it(
-      `passes ${testName}`,
-      async () => {
-        const platform = device.getPlatform();
-        await device.launchApp({
-          newInstance: true,
-          url: `bareexpo://test-suite/run?tests=${testName}`,
-        });
+test(
+  'test-suite',
+  async () => {
+    const platform = device.getPlatform();
+    await device.launchApp({
+      newInstance: true,
+      url: `bareexpo://test-suite/all`,
+    });
 
-        const launchWaitingTime = platform === 'ios' ? 100 : 3000;
-        await sleepAsync(launchWaitingTime);
+    const launchWaitingTime = platform === 'ios' ? 100 : 3000;
+    await sleepAsync(launchWaitingTime);
 
-        await detoxExpect(element(by.id('test_suite_container'))).toExist();
-        try {
-          await waitFor(element(by.id('test_suite_text_results')))
-            .toExist()
-            .withTimeout(MIN_TIME);
-        } catch (err) {
-          // test hasn't completed within the timeout
-          // continue and log the intermediate results
-        }
+    await detoxExpect(element(by.id('test_suite_container'))).toExist();
+    try {
+      await waitFor(element(by.id('test_suite_text_results')))
+        .toExist()
+        .withTimeout(MIN_TIME);
+    } catch (err) {
+      // test hasn't completed within the timeout
+      // continue and log the intermediate results
+    }
 
-        if (platform === 'ios') {
-          const attributes = await element(by.id('test_suite_final_results')).getAttributes();
-          const input = attributes.text;
-          expectResults({
-            testName,
-            input,
-          });
-        } else {
-          // Platforms do no support `getAttributes()`, using text matching instead
-          await detoxExpect(element(by.id('test_suite_text_results'))).toHaveText('Complete: 0 tests failed.');
-        }
-      },
-      MIN_TIME * 1.5
-    );
-  });
-});
+    if (platform === 'ios') {
+      const attributes = await element(by.id('test_suite_final_results')).getAttributes();
+      const input = attributes.text;
+      expectResults({
+        testName,
+        input,
+      });
+    } else {
+      // Platforms do no support `getAttributes()`, using text matching instead
+      await detoxExpect(element(by.id('test_suite_text_results'))).toHaveText(
+        'Complete: 0 tests failed.'
+      );
+    }
+  },
+  MIN_TIME * 1.5 * NUM_TESTS
+);
