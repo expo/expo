0ffc9fc9ef070350dd2bf07ef54872448b4a35d6
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-09-21 09:43:50 +0200

    Kotlinize `CheckIfAlbumShouldBeMigrated`

---
+++ packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/CheckIfAlbumShouldBeMigrated.kt
@@ -0,0 +1,26 @@
+package expo.modules.medialibrary
+
+import android.content.Context
+import android.os.AsyncTask
+import android.os.Build
+import androidx.annotation.RequiresApi
+import expo.modules.core.Promise
+import expo.modules.medialibrary.MediaLibraryUtils
+import expo.modules.medialibrary.MediaLibraryConstants
+
+@RequiresApi(Build.VERSION_CODES.R)
+class CheckIfAlbumShouldBeMigrated(
+    private val context: Context,
+    private val albumId: String,
+    private val promise: Promise
+) : AsyncTask<Void?, Void?, Void?>() {
+  public override fun doInBackground(vararg voids: Void?): Void? {
+    val albumDir = MediaLibraryUtils.getAlbumFile(context, albumId)
+    if (albumDir == null) {
+      promise.reject(MediaLibraryConstants.ERROR_NO_ALBUM, "Couldn't find album")
+    } else {
+      promise.resolve(!albumDir.canWrite())
+    }
+    return null
+  }
+}
