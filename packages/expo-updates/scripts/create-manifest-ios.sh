bd232901082327411cce7a14088b23e0747c0354
Brent Vatne <brentvatne@gmail.com>
2021-09-22 14:34:51 -0700

    Revert "[constants][updates] Disable build phase for simulator debug builds (#14116)"
    This reverts commit 1b4b2ebbf1ebc50e4424f4e6418b2311a73a583c.

--- packages/expo-updates/scripts/create-manifest-ios.sh
+++ packages/expo-updates/scripts/create-manifest-ios.sh
@@ -2,17 +2,6 @@
 
 set -eo pipefail
 
-if [[ "$SKIP_BUNDLING" ]]; then
-  echo "SKIP_BUNDLING enabled; skipping create-manifest-ios.sh."
-  exit 0;
-elif [[ "$CONFIGURATION" == *Debug* ]]; then
-  if [[ "$FORCE_BUNDLING" ]]; then
-    echo "FORCE_BUNDLING enabled; continuing create-manifest-ios.sh."
-  else
-    exit 0;
-  fi
-fi
-
 DEST="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
 ENTRY_FILE=${ENTRY_FILE:-index.js}
 RCT_METRO_PORT=${RCT_METRO_PORT:=8081}
@@ -22,7 +11,7 @@ NODE_BINARY=${NODE_BINARY:-node}
 PROJECT_ROOT=${PROJECT_ROOT:-$PWD}
 cd "$PROJECT_ROOT" || exit
 
-if ! [ -x "$(command -v "$NODE_BINARY")" ]; then
+if ! [ -x "$(command -v $NODE_BINARY)" ]; then
   echo 'Error: cannot find the node binary. Try setting the NODE_BINARY variable in the ' \
   '"Bundle React Native code and images" Build Phase to the absolute path to your node binary. ' \
   'You can find it by executing "which node" in a terminal window.' >&2
