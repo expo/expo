d85af99d3bfcbec1630f2c4050f6c68e3b97f562
Federkun <federkun@gmail.com>
2021-09-23 03:16:36 +0100

    [web][speech] Build

--- packages/expo-speech/build/ExponentSpeech.web.d.ts
+++ packages/expo-speech/build/ExponentSpeech.web.d.ts
@@ -2,7 +2,7 @@ import { SpeechOptions, WebVoice } from './Speech.types';
 declare const _default: {
     readonly name: string;
     speak(id: string, text: string, options: SpeechOptions): Promise<SpeechSynthesisUtterance>;
-    getVoices(): WebVoice[];
+    getVoices(): Promise<WebVoice[]>;
     isSpeaking(): Promise<boolean>;
     stop(): Promise<void>;
     pause(): Promise<void>;
