b4af2bb59a2dcb09c1a005e8a99e2ed31f7c939d
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-09-28 10:05:43 +0200

    Kotlinize `RemoveAssetsFromAlbum`

---
+++ packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/RemoveAssetsFromAlbum.kt
@@ -0,0 +1,23 @@
+package expo.modules.medialibrary
+
+import android.content.Context
+import android.os.AsyncTask
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media
+import android.text.TextUtils
+import expo.modules.core.Promise
+
+internal class RemoveAssetsFromAlbum(
+    private val context: Context,
+    private val assetIds: Array<String>,
+    private val albumId: String,
+    private val promise: Promise
+) : AsyncTask<Void?, Void?, Void?>() {
+  override fun doInBackground(vararg params: Void?): Void? {
+    val bucketSelection = "${Media.BUCKET_ID}=? AND ${Media._ID} IN (${assetIds.joinToString(",")} )"
+    val bucketId = arrayOf(albumId)
+
+    MediaLibraryUtils.deleteAssets(context, bucketSelection, bucketId, promise)
+    return null
+  }
+}
