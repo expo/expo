d1839c3d13b9e4050b3003b5b1996403a39ea556
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-09-28 10:52:57 +0200

    Move utility function

--- packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/albums/migration/CheckIfAlbumShouldBeMigrated.kt
+++ packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/albums/migration/CheckIfAlbumShouldBeMigrated.kt
@@ -3,10 +3,11 @@ package expo.modules.medialibrary.albums.migration
 import android.content.Context
 import android.os.AsyncTask
 import android.os.Build
+import android.provider.MediaStore
 import androidx.annotation.RequiresApi
 import expo.modules.core.Promise
-import expo.modules.medialibrary.MediaLibraryUtils
 import expo.modules.medialibrary.MediaLibraryConstants
+import java.io.File
 
 @RequiresApi(Build.VERSION_CODES.R)
 class CheckIfAlbumShouldBeMigrated(
@@ -15,7 +16,7 @@ class CheckIfAlbumShouldBeMigrated(
   private val promise: Promise
 ) : AsyncTask<Void?, Void?, Void?>() {
   public override fun doInBackground(vararg voids: Void?): Void? {
-    val albumDir = MediaLibraryUtils.getAlbumFile(context, albumId)
+    val albumDir = getAlbumDirectory(context, albumId)
     if (albumDir == null) {
       promise.reject(MediaLibraryConstants.ERROR_NO_ALBUM, "Couldn't find album")
     } else {
@@ -23,4 +24,31 @@ class CheckIfAlbumShouldBeMigrated(
     }
     return null
   }
+}
+
+/**
+ * Returns directory for given Album ID (`BUCKET_ID` column) or `null` if album not found.
+ */
+private fun getAlbumDirectory(context: Context, albumId: String): File? {
+  val selection =
+    "${MediaStore.Files.FileColumns.MEDIA_TYPE} != ${MediaStore.Files.FileColumns.MEDIA_TYPE_NONE}" +
+      " AND ${MediaStore.MediaColumns.BUCKET_ID}=?"
+  val selectionArgs = arrayOf(albumId)
+  val projection = arrayOf(MediaStore.MediaColumns.DATA)
+  context.contentResolver.query(
+    MediaLibraryConstants.EXTERNAL_CONTENT,
+    projection,
+    selection,
+    selectionArgs,
+    null
+  ).use { albumCursor ->
+    if (albumCursor != null && albumCursor.moveToNext()) {
+      val dataColumnIndex = albumCursor.getColumnIndex(MediaStore.Images.Media.DATA)
+      val fileInAlbum = File(albumCursor.getString(dataColumnIndex))
+      if (fileInAlbum.isFile) {
+        return File(fileInAlbum.parent ?: return null)
+      }
+    }
+  }
+  return null
 }
