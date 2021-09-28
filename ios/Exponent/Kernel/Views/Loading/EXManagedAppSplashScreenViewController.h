420a5672e9df08c53491b9e75e7cfd12bee60e67
andy <40680668+ajsmth@users.noreply.github.com>
2021-09-28 15:29:29 -0700

    Update ios/Exponent/Kernel/Views/Loading/EXManagedAppSplashScreenViewController.h
    Co-authored-by: Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>

--- ios/Exponent/Kernel/Views/Loading/EXManagedAppSplashScreenViewController.h
+++ ios/Exponent/Kernel/Views/Loading/EXManagedAppSplashScreenViewController.h
@@ -5,7 +5,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface EXManagedAppSplashScreenViewController : EXSplashScreenViewController
 
--(void) startSplashScreenVisibleTimer;
+- (void)startSplashScreenVisibleTimer;
 
 @end
 
