1272ede9aa93526ce011e6f44e1f6fdd5c1e2972
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-09-28 09:42:06 +0200

    Kotlinize `CreateAlbum`

---
+++ packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/CreateAlbum.kt
@@ -0,0 +1,67 @@
+package expo.modules.medialibrary
+
+import android.content.Context
+import android.os.AsyncTask
+import expo.modules.medialibrary.MediaLibraryUtils.FileStrategy
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.provider.MediaStore
+import expo.modules.core.Promise
+import expo.modules.medialibrary.MediaLibraryConstants.*
+import java.io.File
+import java.io.IOException
+
+internal class CreateAlbum(
+    private val context: Context,
+    private val albumName: String,
+    private val assetId: String,
+    copyAsset: Boolean,
+    private val promise: Promise
+) : AsyncTask<Void?, Void?, Void?>() {
+  private val mStrategy = if (copyAsset) MediaLibraryUtils.copyStrategy else MediaLibraryUtils.moveStrategy
+
+  private fun createAlbum(mimeType: String): File? {
+    val albumDir = MediaLibraryUtils
+      .getEnvDirectoryForAssetType(mimeType, false)
+      .ifNull {
+        promise.reject(ERROR_NO_ALBUM, "Could not guess asset type.")
+        return null
+      }
+
+    val album = File(albumDir.path, albumName)
+        .takeIf { it.exists() || it.mkdirs() }
+        .ifNull {
+          promise.reject(ERROR_NO_ALBUM, "Could not create album directory.")
+          return null
+        }
+    return album
+  }
+
+  public override fun doInBackground(vararg params: Void?): Void? {
+    try {
+      val files = MediaLibraryUtils.getAssetsById(context, promise, assetId) ?: return null
+      val albumCreator = files[0]
+      val album = createAlbum(albumCreator.mimeType) ?: return null
+      val newFile = mStrategy.apply(albumCreator, album, context)
+      MediaScannerConnection.scanFile(
+          context,
+          arrayOf(newFile.path),
+          null
+      ) { path: String, uri: Uri? ->
+        if (uri == null) {
+          promise.reject(ERROR_UNABLE_TO_SAVE, "Could not add image to album.")
+          return@scanFile
+        }
+        val selection = "${MediaStore.Images.Media.DATA}=?"
+        val args = arrayOf(path)
+        MediaLibraryUtils.queryAlbum(context, selection, args, promise)
+      }
+    } catch (e: SecurityException) {
+      promise.reject(ERROR_UNABLE_TO_LOAD_PERMISSION,
+          "Could not create album: need WRITE_EXTERNAL_STORAGE permission.", e)
+    } catch (e: IOException) {
+      promise.reject(ERROR_UNABLE_TO_LOAD, "Could not read file or parse EXIF tags", e)
+    }
+    return null
+  }
+}
