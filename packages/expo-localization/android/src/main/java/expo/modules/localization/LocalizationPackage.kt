ff375c849907b30b4f153590a8bd6ea458d31bca
Michał Stach <michal.stach@swmansion.com>
2021-09-30 14:49:31 +0200

    Run spotlessApply

--- packages/expo-localization/android/src/main/java/expo/modules/localization/LocalizationPackage.kt
+++ packages/expo-localization/android/src/main/java/expo/modules/localization/LocalizationPackage.kt
@@ -3,7 +3,6 @@ package expo.modules.localization
 import android.content.Context
 import expo.modules.core.BasePackage
 import expo.modules.core.ExportedModule
-import expo.modules.localization.LocalizationModule
 
 class LocalizationPackage : BasePackage() {
   override fun createExportedModules(context: Context): List<ExportedModule> {
