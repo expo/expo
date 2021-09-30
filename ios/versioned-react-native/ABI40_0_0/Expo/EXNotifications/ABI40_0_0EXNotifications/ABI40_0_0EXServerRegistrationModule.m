afdd181580bf5f54e09c0f04e286d45f27c2ffee
Will Schurman <wschurman@gmail.com>
2021-09-30 09:02:43 -0700

    [ios][expo-notifications] Fix accidentally-versioned constant in EXServerRegistrationModule (#14576)

--- ios/versioned-react-native/ABI40_0_0/Expo/EXNotifications/ABI40_0_0EXNotifications/ABI40_0_0EXServerRegistrationModule.m
+++ ios/versioned-react-native/ABI40_0_0/Expo/EXNotifications/ABI40_0_0EXNotifications/ABI40_0_0EXServerRegistrationModule.m
@@ -5,6 +5,7 @@
 static NSString * const kEXDeviceInstallationUUIDKey = @"EXDeviceInstallationUUIDKey";
 static NSString * const kEXDeviceInstallationUUIDLegacyKey = @"EXDeviceInstallUUIDKey";
 
+// this constant value being versioned is incorrect and is fixed in SDK >=43
 static NSString * const kEXRegistrationInfoKey = @"ABI40_0_0EXNotificationRegistrationInfoKey";
 
 @implementation ABI40_0_0EXServerRegistrationModule
