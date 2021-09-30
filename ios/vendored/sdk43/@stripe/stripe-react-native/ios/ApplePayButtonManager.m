e13ec06d04b2e4eee9d006e146916faade01bb13
Charlie Cruzan <35579283+cruzach@users.noreply.github.com>
2021-09-30 09:13:08 -0400

    [ios][versioning] Fix some vendored modules not being versioned correctly (#14578)
    * fix versioning scripts and dependencies.rb
    
    * Revendor stripe
    
    * pod install
    
    * fix iOS builds: rn segmented control

--- ios/vendored/sdk43/@stripe/stripe-react-native/ios/ApplePayButtonManager.m
+++ ios/vendored/sdk43/@stripe/stripe-react-native/ios/ApplePayButtonManager.m
@@ -2,7 +2,7 @@
 #import <ABI43_0_0React/ABI43_0_0RCTBridgeModule.h>
 #import <ABI43_0_0React/ABI43_0_0RCTViewManager.h>
 
-@interface ABI43_0_0RCT_EXTERN_MODULE(ApplePayButtonManager, ABI43_0_0RCTViewManager)
+@interface ABI43_0_0RCT_EXTERN_REMAP_MODULE(ApplePayButtonManager, ABI43_0_0ApplePayButtonManager, ABI43_0_0RCTViewManager)
 ABI43_0_0RCT_EXPORT_VIEW_PROPERTY(onPressAction, ABI43_0_0RCTDirectEventBlock)
 ABI43_0_0RCT_EXPORT_VIEW_PROPERTY(type, NSNumber)
 ABI43_0_0RCT_EXPORT_VIEW_PROPERTY(buttonStyle, NSNumber)
