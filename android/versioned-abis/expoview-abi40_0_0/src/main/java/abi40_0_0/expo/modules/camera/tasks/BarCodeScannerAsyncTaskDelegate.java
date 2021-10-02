31e7f7e6aaada2383c0fc812d222a98a5744c186
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2021-10-02 23:06:51 +0200

    [android] Drop SDK40 (#14624)

--- android/versioned-abis/expoview-abi40_0_0/src/main/java/abi40_0_0/expo/modules/camera/tasks/BarCodeScannerAsyncTaskDelegate.java
+++
@@ -1,8 +0,0 @@
-package abi40_0_0.expo.modules.camera.tasks;
-
-import abi40_0_0.org.unimodules.interfaces.barcodescanner.BarCodeScannerResult;
-
-public interface BarCodeScannerAsyncTaskDelegate {
-  void onBarCodeScanned(BarCodeScannerResult barCode);
-  void onBarCodeScanningTaskCompleted();
-}
--- android/versioned-abis/expoview-abi40_0_0/src/main/java/abi40_0_0/expo/modules/splashscreen/exceptions/NoContentViewException.kt
+++
@@ -1,9 +0,0 @@
-package abi40_0_0.expo.modules.splashscreen.exceptions
-
-import abi40_0_0.org.unimodules.core.errors.CodedException
-
-class NoContentViewException : CodedException("ContentView is not yet available. Call 'SplashScreen.show(...)' once 'setContentView()' is called.") {
-  override fun getCode(): String {
-    return "ERR_NO_CONTENT_VIEW_FOUND"
-  }
-}
