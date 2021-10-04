b1e330bb036a577b0bb69dc02ac1250c9f70279b
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-10-04 09:09:19 +0200

    [modules-core][android] Make `onActivityResult` intent nullable (#14612)

--- packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ActivityEventListener.java
+++ packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ActivityEventListener.java
@@ -3,8 +3,10 @@ package expo.modules.core.interfaces;
 import android.app.Activity;
 import android.content.Intent;
 
+import androidx.annotation.Nullable;
+
 public interface ActivityEventListener {
-  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data);
+  public void onActivityResult(Activity activity, int requestCode, int resultCode, @Nullable Intent data);
 
   // Called when a new intent is passed to the activity
   public void onNewIntent(Intent intent);
