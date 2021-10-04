a71ca98f1db974e2c99d04c305c94ae6068ee6a9
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-10-04 19:08:42 +0200

    [media-library] Refactor to Kotlin 1 - Base module (#14562)
    * [media-library] Kotlinize base module
    
    * Apply spotless ktlint
    
    * Add kotlin utilities
    
    * Apply fix from master
    
    * Apply suggestions from review

---
+++ packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/KotlinUtils.kt
@@ -0,0 +1,8 @@
+package expo.modules.medialibrary
+
+// This will be moved to MediaLibraryUtils when it is kotlinized
+
+/**
+ * Returns block result if the receiver is null
+ */
+inline fun <T> T?.ifNull(block: () -> T): T = this ?: block()
