0a1a84f3f5f858eec6393ca934938e8ada6d0565
Kudo Chien <kudo@expo.dev>
2021-10-06 17:22:43 +0800

    spotlessApply

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/pagerview/event/PageSelectedEvent.kt
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/pagerview/event/PageSelectedEvent.kt
@@ -5,7 +5,6 @@ import com.facebook.react.bridge.WritableMap
 import com.facebook.react.uimanager.events.Event
 import com.facebook.react.uimanager.events.RCTEventEmitter
 
-
 /**
  * Event emitted by [ReactViewPager] when selected page changes.
  *
@@ -30,5 +29,4 @@ class PageSelectedEvent(viewTag: Int, private val mPosition: Int) : Event<PageSe
   companion object {
     const val EVENT_NAME = "topPageSelected"
   }
-
 }
