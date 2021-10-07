6a80d9fd96340e6f8ba62cd133ace93c6fbbb258
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-10-07 10:44:14 +0200

    [dev-launcher][ios] Adjust config plugin for new bare template - SDK 43 (#14633)
    # Why
    
    Closes ENG-2077.
    
    # How
    
    Apply changes describe here: https://github.com/expo/expo/pull/14617.
    
    # Test Plan
    
    - https://exponent-internal.slack.com/archives/C011V63429H/p1633039856117100 ✅
    - added tests ✅

---
+++ packages/expo-dev-launcher/plugin/build/utils.d.ts
@@ -0,0 +1,2 @@
+export declare function addLines(content: string, find: string | RegExp, offset: number, toAdd: string[]): string;
+export declare function replaceLine(content: string, find: string | RegExp, replace: string): string;
