30fbcb1c3c1768b1524f7f88af942b8f0b66a669
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2021-10-07 16:50:00 +0200

    Apply proper Swift file formatting

--- ios/Exponent/Kernel/Views/Loading/EXAppLoadingProgressWindowViewController.swift
+++ ios/Exponent/Kernel/Views/Loading/EXAppLoadingProgressWindowViewController.swift
@@ -3,14 +3,11 @@ import UIKit
 @objc
 class EXAppLoadingProgressWindowViewController : UIViewController {
   override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-    get {
-      let visibleAppSupportedInterfaceOrientations =
-        EXKernel
-          .sharedInstance()
-          .visibleApp
-          .viewController
-          .supportedInterfaceOrientations
-      return visibleAppSupportedInterfaceOrientations
-    }
+    let visibleAppSupportedInterfaceOrientations = EXKernel
+      .sharedInstance()
+      .visibleApp
+      .viewController
+      .supportedInterfaceOrientations
+    return visibleAppSupportedInterfaceOrientations
   }
 }
