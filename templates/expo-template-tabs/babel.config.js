e997de7e2fd9e7989f4b13d88d0004581caa1c37
Brent Vatne <brentvatne@gmail.com>
2021-10-07 12:56:08 -0700

    [templates] Remove reanimated plugin from tabs template

--- templates/expo-template-tabs/babel.config.js
+++ templates/expo-template-tabs/babel.config.js
@@ -1,7 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: ['babel-preset-expo']
   };
 };
