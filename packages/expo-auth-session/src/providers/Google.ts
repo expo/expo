25b776643a3dcf95f03f4827112f3aa0cbf2f249
Simek <gosimek@gmail.com>
2021-10-12 13:32:39 +0200

    [auth-session] tests and providers fixes

--- packages/expo-auth-session/src/providers/Google.ts
+++ packages/expo-auth-session/src/providers/Google.ts
@@ -321,10 +321,7 @@ export function useAuthRequest(
     }
 
     // has a code to exchange and doesn't have an authentication yet.
-    const couldAutoExchange =
-      result?.type === 'success' && result.params.code && !result.authentication;
-
-    return couldAutoExchange;
+    return result?.type === 'success' && result.params.code && !result.authentication;
   }, [config.shouldAutoExchangeCode, result?.type]);
 
   useEffect(() => {
@@ -337,8 +334,7 @@ export function useAuthRequest(
         scopes: config.scopes,
         code: result.params.code,
         extraParams: {
-          // @ts-ignore: allow for instances where PKCE is disabled
-          code_verifier: request.codeVerifier,
+          code_verifier: request?.codeVerifier || '',
         },
       });
       exchangeRequest.performAsync(discovery).then((authentication) => {
@@ -346,8 +342,7 @@ export function useAuthRequest(
           setFullResult({
             ...result,
             params: {
-              // @ts-ignore: provide a singular interface for getting the id_token across all workflows that request it.
-              id_token: authentication.idToken,
+              id_token: authentication?.idToken || '',
               access_token: authentication.accessToken,
               ...result.params,
             },
