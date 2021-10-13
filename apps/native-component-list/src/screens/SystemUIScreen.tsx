e49b8da5121dbcded7506321a8540e0062be037a
Marc Rousavy <marcrousavy@hotmail.com>
2021-10-13 10:34:27 +0200

    Update apps/native-component-list/src/screens/SystemUIScreen.tsx
    Co-authored-by: Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>

--- apps/native-component-list/src/screens/SystemUIScreen.tsx
+++ apps/native-component-list/src/screens/SystemUIScreen.tsx
@@ -74,7 +74,7 @@ function getRandomColor(): string {
   const letters = '0123456789ABCDEF';
   let color = '#';
   for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * letters.length)];
   }
   return color;
 }
