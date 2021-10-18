53bc12205f3b8d6464e4977efa53e8e364381099
Kacper Kafara <kacper.kafara@swmansion.com>
2021-10-18 17:15:38 +0200

    Kotlinize `UrlAddressModel`

--- packages/expo-contacts/android/src/main/java/expo/modules/contacts/models/UrlAddressModel.kt
+++ packages/expo-contacts/android/src/main/java/expo/modules/contacts/models/UrlAddressModel.kt
@@ -2,26 +2,25 @@ package expo.modules.contacts.models
 
 import android.database.Cursor
 import android.provider.ContactsContract.CommonDataKinds
+
 import expo.modules.contacts.EXColumns
 
 class UrlAddressModel : BaseModel() {
   override val contentType: String
     get() = CommonDataKinds.Website.CONTENT_ITEM_TYPE
+
   override val dataAlias: String
     get() = "url"
 
-  override fun getLabelFromCursor(cursor: Cursor): String? {
-    val label = super.getLabelFromCursor(cursor)
-    return label
-        ?: when (cursor.getInt(cursor.getColumnIndex(EXColumns.TYPE))) {
-          CommonDataKinds.Website.TYPE_HOME -> "home"
-          CommonDataKinds.Website.TYPE_WORK -> "work"
-          CommonDataKinds.Website.TYPE_BLOG -> "blog"
-          CommonDataKinds.Website.TYPE_FTP -> "ftp"
-          CommonDataKinds.Website.TYPE_HOMEPAGE -> "homepage"
-          CommonDataKinds.Website.TYPE_PROFILE -> "profile"
-          CommonDataKinds.Website.TYPE_OTHER -> "other"
-          else -> "unknown"
-        }
-  }
+  override fun getLabelFromCursor(cursor: Cursor) =
+    super.getLabelFromCursor(cursor) ?: when (cursor.getInt(cursor.getColumnIndex(EXColumns.TYPE))) {
+      CommonDataKinds.Website.TYPE_HOME -> "home"
+      CommonDataKinds.Website.TYPE_WORK -> "work"
+      CommonDataKinds.Website.TYPE_BLOG -> "blog"
+      CommonDataKinds.Website.TYPE_FTP -> "ftp"
+      CommonDataKinds.Website.TYPE_HOMEPAGE -> "homepage"
+      CommonDataKinds.Website.TYPE_PROFILE -> "profile"
+      CommonDataKinds.Website.TYPE_OTHER -> "other"
+      else -> "unknown"
+    }
 }
