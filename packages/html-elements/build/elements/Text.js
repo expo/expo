7b617c7d145612a20a32a955efd8133ebe79a18f
Evan Bacon <bacon@expo.io>
2021-10-20 11:17:42 -0600

    Use Courier New for Code font in iOS 15

--- packages/html-elements/build/elements/Text.js
+++ packages/html-elements/build/elements/Text.js
@@ -60,7 +60,7 @@ const styles = StyleSheet.create({
         fontStyle: 'italic',
     },
     code: {
-        fontFamily: Platform.select({ default: 'Courier', android: 'monospace' }),
+        fontFamily: Platform.select({ ios: 'Courier New', android: 'monospace' }),
         fontWeight: '500',
     },
     pre: {
