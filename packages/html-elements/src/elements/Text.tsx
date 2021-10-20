6a6d55146cef9a51018f0e516b206e16d277e9d2
Evan Bacon <bacon@expo.io>
2021-10-20 12:07:39 -0600

    updated tests

--- packages/html-elements/src/elements/Text.tsx
+++ packages/html-elements/src/elements/Text.tsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     fontStyle: 'italic',
   },
   code: {
-    fontFamily: Platform.select({ ios: 'Courier New', android: 'monospace' }),
+    fontFamily: Platform.select({ default: 'Courier', ios: 'Courier New', android: 'monospace' }),
     fontWeight: '500',
   },
   pre: {
