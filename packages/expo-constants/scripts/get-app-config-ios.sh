15279dad7e8e089abb3701111fbe3540b83da5e5
Filip Engberg <filip.engberg@gmail.com>
2021-10-27 20:45:38 +0200

    Source source-login-scripts.sh before checking NODE_BINARY

--- packages/expo-constants/scripts/get-app-config-ios.sh
+++ packages/expo-constants/scripts/get-app-config-ios.sh
@@ -4,13 +4,11 @@ set -eo pipefail
 
 DEST="$CONFIGURATION_BUILD_DIR"
 RESOURCE_BUNDLE_NAME="EXConstants.bundle"
+# ref: https://github.com/facebook/react-native/blob/c974cbff04a8d90ac0f856dbada3fc5a75c75b49/scripts/react-native-xcode.sh#L59-L65
+# Path to expo-constants folder inside node_modules
+EXPO_CONSTANTS_PACKAGE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-# Fix for machines using nvm
-if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-. "$HOME/.nvm/nvm.sh"
-elif [[ -x "$(command -v brew)" && -s "$(brew --prefix nvm)/nvm.sh" ]]; then
-. "$(brew --prefix nvm)/nvm.sh"
-fi
+source "$EXPO_CONSTANTS_PACKAGE_DIR/scripts/source-login-scripts.sh"
 
 NODE_BINARY=${NODE_BINARY:-node}
 
@@ -31,9 +29,6 @@ if [ "x$PROJECT_DIR_BASENAME" != "xPods" ]; then
   exit 0
 fi
 
-# ref: https://github.com/facebook/react-native/blob/c974cbff04a8d90ac0f856dbada3fc5a75c75b49/scripts/react-native-xcode.sh#L59-L65
-# Path to expo-constants folder inside node_modules
-EXPO_CONSTANTS_PACKAGE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # If PROJECT_ROOT is not specified, fallback to use Xcode PROJECT_DIR
 PROJECT_ROOT=${PROJECT_ROOT:-"$PROJECT_DIR/../.."}
 PROJECT_ROOT=${PROJECT_ROOT:-"$EXPO_CONSTANTS_PACKAGE_DIR/../.."}
