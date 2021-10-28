4f80e81bb54b6f2717a2cce168bfba61ce6d2afe
Eric Samelson <eric@expo.io>
2021-10-28 15:44:13 -0700

    [ios] fix logic in EXUpdatesErrorRecovery (apply timeout for all remote loads)

--- packages/expo-updates/ios/EXUpdates/EXUpdatesErrorRecovery.m
+++ packages/expo-updates/ios/EXUpdates/EXUpdatesErrorRecovery.m
@@ -145,22 +145,23 @@ static NSInteger const EXUpdatesErrorRecoveryRemoteLoadTimeoutMs = 5000;
 - (void)_waitForRemoteLoaderToFinish
 {
   dispatch_assert_queue(_errorRecoveryQueue);
-  if (_delegate.remoteLoadStatus == EXUpdatesRemoteLoadStatusLoading) {
-    _isWaitingForRemoteUpdate = YES;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, _remoteLoadTimeout * NSEC_PER_MSEC), _errorRecoveryQueue, ^{
-      if (!self->_isWaitingForRemoteUpdate) {
-        return;
-      }
-      self->_isWaitingForRemoteUpdate = NO;
-      [self->_pipeline removeObject:@(EXUpdatesErrorRecoveryTaskLaunchNew)];
-      [self _runNextTask];
-    });
-    return;
-  } else if (_delegate.remoteLoadStatus == EXUpdatesRemoteLoadStatusNewUpdateLoaded) {
+  if (_delegate.remoteLoadStatus == EXUpdatesRemoteLoadStatusNewUpdateLoaded) {
     [self _runNextTask];
-  } else if (_delegate.config.checkOnLaunch != EXUpdatesCheckAutomaticallyConfigNever) {
+  } else if (_delegate.config.checkOnLaunch != EXUpdatesCheckAutomaticallyConfigNever ||
+             _delegate.remoteLoadStatus == EXUpdatesRemoteLoadStatusLoading) {
     _isWaitingForRemoteUpdate = YES;
-    [_delegate loadRemoteUpdate];
+    if (_delegate.remoteLoadStatus != EXUpdatesRemoteLoadStatusLoading) {
+      [_delegate loadRemoteUpdate];
+    }
+      dispatch_after(dispatch_time(DISPATCH_TIME_NOW, _remoteLoadTimeout * NSEC_PER_MSEC), _errorRecoveryQueue, ^{
+        if (!self->_isWaitingForRemoteUpdate) {
+          return;
+        }
+        self->_isWaitingForRemoteUpdate = NO;
+        [self->_pipeline removeObject:@(EXUpdatesErrorRecoveryTaskLaunchNew)];
+        [self _runNextTask];
+      });
+      return;
   } else {
     // there's no remote update, so move to the next step in the pipeline
     [self->_pipeline removeObject:@(EXUpdatesErrorRecoveryTaskLaunchNew)];
