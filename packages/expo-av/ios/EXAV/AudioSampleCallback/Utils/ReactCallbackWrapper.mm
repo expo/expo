237591e2cf8a564fa73462be77a6c542ff604575
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-11-04 14:24:44 +0100

    [no ci] Fix comment

--- packages/expo-av/ios/EXAV/AudioSampleCallback/Utils/ReactCallbackWrapper.mm
+++ packages/expo-av/ios/EXAV/AudioSampleCallback/Utils/ReactCallbackWrapper.mm
@@ -1,9 +1,9 @@
-//
-//  ReactCallbackWrapper.m
-//  EXAV
-//
-//  Created by Bartłomiej Klocek on 29/10/2021.
-//
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #import <EXAV/ReactCallbackWrapper.h>
 
