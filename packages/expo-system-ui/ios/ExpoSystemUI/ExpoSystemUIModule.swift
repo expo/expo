13c5c8bd7d1d5694ab7844a999d1f6479842e813
Evan Bacon <baconbrix@gmail.com>
2021-11-05 10:49:49 -0700

    Created expo-system-ui module (#14952)
    * Created expo-system-ui
    
    * Added iOS support
    
    * Delete withNavigationBar-test.ts
    
    * Update package.json
    
    * Update package.json
    
    * Remove reflection code
    
    * Added docs
    
    * Create system-ui.md
    
    * Added ios plugin
    
    * Added e2e tests
    
    * Drop setUserInterfaceStyleAsync method
    
    * Convert to new format
    
    * android lint
    
    * Update SystemUIScreen.tsx
    
    * updated docs and naming
    
    * Remove unused android code
    
    * Update apps/native-component-list/src/screens/SystemUIScreen.tsx
    
    * Normalize web color

---
+++ packages/expo-system-ui/ios/ExpoSystemUI/ExpoSystemUIModule.swift
@@ -0,0 +1,25 @@
+// Copyright 2021-present 650 Industries. (AKA Expo) All rights reserved.
+
+import ExpoModulesCore
+
+public class ExpoSystemUIModule: Module {
+  public func definition() -> ModuleDefinition {
+    name("ExpoSystemUI")
+
+    method("getBackgroundColorAsync") { () -> String? in
+      var color: String? = nil
+      EXUtilities.performSynchronously {
+        if let backgroundColor = self.appContext?.utilities?.currentViewController()?.view.backgroundColor?.cgColor {
+          color = EXUtilities.hexString(with: backgroundColor)
+        }
+      }
+      return color
+    }
+
+    method("setBackgroundColorAsync") { (color: Int) in
+      EXUtilities.performSynchronously {
+        self.appContext?.utilities?.currentViewController()?.view.backgroundColor = EXUtilities.uiColor(color)
+      }
+    }
+  }
+}
