297c5911b087e75cb787a0c482e902a9b77d79ca
Robert Herber <robert@kingstinct.com>
2021-11-08 01:48:03 +0100

    Added detection of catalyst and m1 macs

--- packages/expo-device/ios/EXDevice/EXDevice.m
+++ packages/expo-device/ios/EXDevice/EXDevice.m
@@ -157,6 +157,14 @@ EX_EXPORT_METHOD_AS(isRootedExperimentalAsync,
     case UIUserInterfaceIdiomPhone:
       return EXDeviceTypePhone;
     case UIUserInterfaceIdiomPad:
+      if (TARGET_OS_MACCATALYST) {
+          return EXDeviceTypeDesktop;
+      }
+      if (@available(iOS 14.0, *)) {
+          if ([NSProcessInfo processInfo].isiOSAppOnMac) {
+              return EXDeviceTypeDesktop;
+          }
+      }
       return EXDeviceTypeTablet;
     case UIUserInterfaceIdiomTV:
       return EXDeviceTypeTV;
