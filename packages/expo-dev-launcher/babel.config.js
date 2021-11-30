ea44eaf7d0b0a9439b765962620754a0dcccc7fe
andy <40680668+ajsmth@users.noreply.github.com>
2021-11-30 09:27:12 -0800

    remove babel config for vendoring

--- packages/expo-dev-launcher/babel.config.js
+++ packages/expo-dev-launcher/babel.config.js
@@ -1,28 +1,7 @@
-function tryResolveModule(module) {
-  try {
-    return require.resolve(module);
-  } catch (e) {
-    console.error(`Couldn't resolve vendored module - ${module}.`);
-    return null;
-  }
-}
-
 module.exports = function (api) {
   api.cache(true);
 
-  const rnSvg = tryResolveModule('./vendored/react-native-svg/src/index.ts');
-
-  const alias = {};
-  if (rnSvg) {
-    alias['react-native-svg'] = rnSvg;
-  }
-
-  const moduleResolverConfig = {
-    alias,
-  };
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['babel-plugin-module-resolver', moduleResolverConfig]],
   };
 };
