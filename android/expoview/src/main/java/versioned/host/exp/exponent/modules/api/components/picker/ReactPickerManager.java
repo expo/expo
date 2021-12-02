f524ebf901a88639bade4c22b15a8cb47352608c
Cedric van Putten <me@bycedric.com>
2021-12-02 15:20:56 +0100

    [android][ios] Update @react-native-picker/picker from 2.0.1 to 2.2.1

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/picker/ReactPickerManager.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/picker/ReactPickerManager.java
@@ -32,7 +32,6 @@ import com.facebook.react.uimanager.events.EventDispatcher;
 import java.util.Map;
 
 import javax.annotation.Nullable;
-import host.exp.expoview.R;
 
 /**
  * {@link ViewManager} for the {@link ReactPicker} view. This is abstract because the
