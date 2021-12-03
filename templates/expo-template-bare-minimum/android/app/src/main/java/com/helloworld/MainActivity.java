2a0079132dc301457b86a256aaa51494b360a417
Kudo Chien <kudo@expo.dev>
2021-12-03 16:31:24 +0800

    [templates] Align Android back button behavior with classic build (#15378)
    # Why
    
    fix #15326 where we have the default Android implementation on eas build but move activities to background on [classic build](https://github.com/expo/expo/blob/28d5976df2379540936083e599b100a50bd3575d/android/expoview/src/main/java/host/exp/exponent/experience/BaseExperienceActivity.kt#L93-L103). actually, finishing activities is the default behavior on Android, so i kinda dunno which solution is better. after i found that on Android S, [the default behavior for root activities is also moving background](https://developer.android.com/about/versions/12/behavior-changes-all#back-press), i would try to align the design with Android S.
    
    # How
    
    - for android <= 11 + root activities, move to background.
    - for android <= 11 + non-root activities, use the platform default implementation (`finish()`)
    - for android > 11, use the platform default implementation. (assuming to move root activities in background) 
    
    # Test Plan
    
    use the [example repo](https://github.com/psnet/eastest) from #15326 + this patch
    - test `expo run:android`
    - test `eas build`
    
    Co-authored-by: Brent Vatne <brentvatne@gmail.com>
    Co-authored-by: Evan Bacon <baconbrix@gmail.com>

--- templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainActivity.java
+++ templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.helloworld;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
@@ -38,5 +39,25 @@ public class MainActivity extends ReactActivity {
         return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     });
+  }
+
+  /**
+   * Align the back button behavior with Android S
+   * where moving root activities to background instead of finishing activities.
+   * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+   */
+  @Override
+  public void invokeDefaultOnBackPressed() {
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+      if (!moveTaskToBack(false)) {
+        // For non-root activities, use the default implementation to finish them.
+        super.invokeDefaultOnBackPressed();
+      }
+      return;
+    }
+
+    // Use the default back button implementation on Android S
+    // because it's doing more than {@link Activity#moveTaskToBack} in fact.
+    super.invokeDefaultOnBackPressed();
   }
 }
