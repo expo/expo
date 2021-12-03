b9d7301d74bf3137f188a5a39766021515baefdb
Evan Bacon <baconbrix@gmail.com>
2021-12-02 17:19:00 -0700

    Add expo-navigation-bar to Expo Go Android (#15384)

--- android/expoview/src/main/java/versioned/host/exp/exponent/ExperiencePackagePicker.kt
+++ android/expoview/src/main/java/versioned/host/exp/exponent/ExperiencePackagePicker.kt
@@ -57,6 +57,7 @@ import expo.modules.storereview.StoreReviewPackage
 import expo.modules.taskManager.TaskManagerPackage
 import expo.modules.updates.UpdatesPackage
 import expo.modules.manifests.core.Manifest
+import expo.modules.navigationbar.NavigationBarPackage
 import expo.modules.systemui.SystemUIPackage
 import expo.modules.videothumbnails.VideoThumbnailsPackage
 import expo.modules.webbrowser.WebBrowserPackage
@@ -101,6 +102,7 @@ object ExperiencePackagePicker {
     LocationPackage(),
     MailComposerPackage(),
     MediaLibraryPackage(),
+    NavigationBarPackage(),
     NetworkPackage(),
     NotificationsPackage(),
     PermissionsPackage(),
