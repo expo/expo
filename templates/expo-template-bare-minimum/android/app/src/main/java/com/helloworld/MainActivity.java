a2dc4da9465c84437985996d7d9522e2f658130b
Brent Vatne <brentvatne@gmail.com>
2021-12-03 15:26:30 -0800

    [template] First pass at removing dependencies from bare template (#15383)

--- templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainActivity.java
+++ templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
@@ -31,14 +30,9 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(
-      this,
-      new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    });
+    return new ReactActivityDelegateWrapper(this,
+      new ReactActivityDelegate(this, getMainComponentName())
+    );
   }
 
   /**
