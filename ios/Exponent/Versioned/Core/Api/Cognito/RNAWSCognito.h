803248b44ef5397fd7d041d7e6fb3248888f7a0d
Kudo Chien <kudo@expo.dev>
2021-12-07 13:29:03 +0800

    update comments

--- ios/Exponent/Versioned/Core/Api/Cognito/RNAWSCognito.h
+++ ios/Exponent/Versioned/Core/Api/Cognito/RNAWSCognito.h
@@ -1,6 +1,6 @@
 /**
  NOTE: the imports are slightly changed by hand for expo versioning.
- since cognito sdk does not change frequently, these changes do not include in `update-vendored-module` script and you should update manually after upgrading the module.
+ since cognito sdk does not change frequently, these changes are not included in `update-vendored-module` script. you should modify manually whenever upgrading the module.
  changes we did:
    - replace imports from double-quote "" to bracket <> for xcode to find the correct versioning headers and clang modules.
  */
