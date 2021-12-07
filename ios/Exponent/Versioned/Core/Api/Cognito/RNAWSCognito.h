0cb6cae4deb31b8232818b1696990890edf97163
Kudo Chien <kudo@expo.dev>
2021-12-07 17:11:34 +0800

    [ios] fix ios shell app error from RNAWSCognito.h (#15458)
    # Why
    
    fix ios shell app building error in `RNAWSCognito.m` which duplicated react definitions from different file paths.
    the error was like this: https://github.com/expo/expo/runs/4439955300
    
    # How
    
    should use `#import <React/RCTLog.h>` instead of `#import "RCTLog.h"` and make xcode to find correct headers / clang modules. 
    
    # Test Plan
    
    - expo go ios versioned build passed
    - ios shell app build passed: https://github.com/expo/expo/runs/4440313881

--- ios/Exponent/Versioned/Core/Api/Cognito/RNAWSCognito.h
+++ ios/Exponent/Versioned/Core/Api/Cognito/RNAWSCognito.h
@@ -1,21 +1,14 @@
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+/**
+ NOTE: the imports are slightly changed by hand for expo versioning.
+ since cognito sdk does not change frequently, these changes are not included in `update-vendored-module` script. you should modify manually whenever upgrading the module.
+ changes we did:
+   - replace imports from double-quote "" to bracket <> for xcode to find the correct versioning headers and clang modules.
+ */
+
 #import <React/RCTBridgeModule.h>
-#endif
-
-#if __has_include("RCTLog.h")
-#import "RCTLog.h"
-#else
 #import <React/RCTLog.h>
-#endif
-
-#if __has_include("RCTUtils.h")
-#import "RCTUtils.h"
-#else
 #import <React/RCTUtils.h>
-#endif
-// Must use brackets instead of quotes for importing JKBigInteger otherwise this is broken by versioning
+
 #import <JKBigInteger.h>
 
 @interface RNAWSCognito : NSObject <RCTBridgeModule>
