1640a18451f67b18020db73fde78ea021a454133
Kudo Chien <kudo@expo.dev>
2021-12-07 17:11:34 +0800

    [ios] fix ios shell app error from RNAWSCognito.h (#15458)
    # Why
    
    fix ios shell app building error in `RNAWSCognito.m` which duplicated react definitions from different file paths.
    the error was like this: https://github.com/expo/expo/runs/4439955300
    
    # How
    
    should use `#import <React/RCTLog.h>` instead of `#import "RCTLog.h"` and make xcode to find correct headers / clang modules. 
    
    # Test Plan
    
    - expo go ios versioned build passed
    - ios shell app build passed: https://github.com/expo/expo/runs/4440313881

--- ios/versioned-react-native/ABI41_0_0/Expo/ExpoKit/Core/Api/Cognito/ABI41_0_0RNAWSCognito.h
+++ ios/versioned-react-native/ABI41_0_0/Expo/ExpoKit/Core/Api/Cognito/ABI41_0_0RNAWSCognito.h
@@ -1,20 +1,13 @@
-#if __has_include("ABI41_0_0RCTBridgeModule.h")
-#import "ABI41_0_0RCTBridgeModule.h"
-#else
+/**
+ NOTE: the imports are slightly changed by hand for expo versioning.
+ since cognito sdk does not change frequently, these changes are not included in `update-vendored-module` script. you should modify manually whenever upgrading the module.
+ changes we did:
+   - replace imports from double-quote "" to bracket <> for xcode to find the correct versioning headers and clang modules.
+ */
+
 #import <ABI41_0_0React/ABI41_0_0RCTBridgeModule.h>
-#endif
-
-#if __has_include("ABI41_0_0RCTLog.h")
-#import "ABI41_0_0RCTLog.h"
-#else
 #import <ABI41_0_0React/ABI41_0_0RCTLog.h>
-#endif
-
-#if __has_include("ABI41_0_0RCTUtils.h")
-#import "ABI41_0_0RCTUtils.h"
-#else
 #import <ABI41_0_0React/ABI41_0_0RCTUtils.h>
-#endif
 
 #import <JKBigInteger.h>
 
