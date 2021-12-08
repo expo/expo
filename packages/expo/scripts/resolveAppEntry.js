455ee1b9b2568b55ed1c3976d34fd70b9f1569cc
Evan Bacon <bacon@expo.io>
2021-12-07 19:52:56 -0500

    Use CLI type interface

--- packages/expo/scripts/resolveAppEntry.js
+++ packages/expo/scripts/resolveAppEntry.js
@@ -1,12 +1,38 @@
+#!/usr/bin/env node
+
+// Basic node CLI script to resolve the native app entry file.
+//
+// Usage:
+//   node expo/scripts/resolveAppEntry.js path/to/project-root <platform> <format>
+//
+// Example:
+//   node expo/scripts/resolveAppEntry.js path/to/project-root/ android absolute
+//
+// Returns:
+//   The resolved entry file path.
+//
+// Limitations:
+//   Currently only supports android and ios.
+
 const { resolveEntryPoint } = require('@expo/config/paths');
 const path = require('path');
 
-// A script for resolving the main JS file for an Expo project
-// uses the same resolution algorithm as expo-cli for expo start.
-module.exports = (projectRoot, { platform, relative }) => {
-  const entry = resolveEntryPoint(projectRoot, { platform });
+const projectRoot = process.argv[1];
+const platform = process.argv[2];
+const absolute = process.argv[3] === 'absolute';
 
-  if (!entry) return null;
+if (!platform || !projectRoot) {
+  console.error(
+    'Usage: node expo/scripts/resolveAppEntry.js <projectRoot> <platform> <relative|absolute>'
+  );
+  process.exit(1);
+}
 
-  return relative ? path.relative(projectRoot, entry) : entry;
-};
+const entry = resolveEntryPoint(projectRoot, { platform });
+
+if (entry) {
+  console.log(absolute ? path.resolve(entry) : path.relative(projectRoot, entry));
+} else {
+  console.error(`Error: Could not find entry file for project at: ${projectRoot}`);
+  process.exit(1);
+}
