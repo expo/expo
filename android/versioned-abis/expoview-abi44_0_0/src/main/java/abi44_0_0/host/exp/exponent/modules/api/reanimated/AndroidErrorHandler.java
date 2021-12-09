a794c346fcded066decb0b0554655edc5c6594a6
Kudo Chien <kudo@expo.dev>
2021-12-10 04:01:30 +0800

    [android] add reanimated sdk 44 versioned code for android (#15494)
    # Why
    
    sdk 44 reanimated support on android
    
    # How
    
    based on #15489
    `et remove-sdk -p android -s 44.0.0 ; et add-sdk -p android -s 44.0.0` and pick reanimated related files
    
    # Test Plan
    
    android versioned expo go + ncl reanimated layout animation test
    
    (cherry picked from commit c75e7e529b65fbd93c9ce3505c7296ec7a1c2cfe)

--- android/versioned-abis/expoview-abi44_0_0/src/main/java/abi44_0_0/host/exp/exponent/modules/api/reanimated/AndroidErrorHandler.java
+++ android/versioned-abis/expoview-abi44_0_0/src/main/java/abi44_0_0/host/exp/exponent/modules/api/reanimated/AndroidErrorHandler.java
@@ -2,8 +2,7 @@ package abi44_0_0.host.exp.exponent.modules.api.reanimated;
 
 public class AndroidErrorHandler {
 
-    public static void raise(String message) {
-        throw new RuntimeException(message);
-    }
-    
+  public static void raise(String message) {
+    throw new RuntimeException(message);
+  }
 }
--- android/versioned-abis/expoview-abi44_0_0/src/main/java/abi44_0_0/host/exp/exponent/modules/api/reanimated/MapUtils.java
+++ android/versioned-abis/expoview-abi44_0_0/src/main/java/abi44_0_0/host/exp/exponent/modules/api/reanimated/MapUtils.java
@@ -3,13 +3,11 @@ package abi44_0_0.host.exp.exponent.modules.api.reanimated;
 import abi44_0_0.com.facebook.react.bridge.JSApplicationCausedNativeException;
 import abi44_0_0.com.facebook.react.bridge.NoSuchKeyException;
 import abi44_0_0.com.facebook.react.bridge.ReadableMap;
-import abi44_0_0.com.facebook.react.bridge.ReadableType;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class MapUtils {
-  public static int getInt(ReadableMap map,  @Nonnull String name, String errorMsg) {
+  public static int getInt(ReadableMap map, @Nonnull String name, String errorMsg) {
     try {
       return map.getInt(name);
     } catch (NoSuchKeyException e) {
@@ -18,7 +16,7 @@ public class MapUtils {
   }
 
   @Nullable
-  public static String getString(ReadableMap map,  @Nonnull String name, String errorMsg) {
+  public static String getString(ReadableMap map, @Nonnull String name, String errorMsg) {
     try {
       return map.getString(name);
     } catch (NoSuchKeyException e) {
