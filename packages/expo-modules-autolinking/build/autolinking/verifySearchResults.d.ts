dc3aeecfef243bd8c2ecf28616997d446a56a3ab
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-12-10 21:52:09 +0100

    [autolinking] Restructure code

---
+++ packages/expo-modules-autolinking/build/autolinking/verifySearchResults.d.ts
@@ -0,0 +1,5 @@
+import { SearchResults } from '../types';
+/**
+ * Verifies the search results by checking whether there are no duplicates.
+ */
+export declare function verifySearchResults(searchResults: SearchResults): number;
