7fac7c3f48588c69292a547dcff8de76646cbd78
Eric Samelson <eric@expo.io>
2021-12-13 12:39:10 -0800

    [expo-dev-menu] add deviceId to dev sessions query on Android

--- packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/constants/DevMenuExpoApiConstants.kt
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/constants/DevMenuExpoApiConstants.kt
@@ -5,5 +5,6 @@ import android.net.Uri
 private const val origin = "https://exp.host"
 
 internal const val AuthHeader = "expo-session"
+internal const val DeviceIDQuery = "deviceId"
 internal val GraphQLEndpoint = Uri.parse("$origin/--/graphql")
 internal val RESTEndpoint = Uri.parse("$origin/--/api/v2/")
