91f176d90e093c5f4d5d97624b67877267e2c572
Eric Samelson <eric@expo.io>
2021-12-14 13:50:57 -0800

    [expo-dev-launcher] include expo-platform header in manifest requests

--- packages/expo-dev-launcher/android/src/main/java/expo/modules/devlauncher/launcher/manifest/DevLauncherManifestParser.kt
+++ packages/expo-dev-launcher/android/src/main/java/expo/modules/devlauncher/launcher/manifest/DevLauncherManifestParser.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import expo.modules.devlauncher.helpers.await
 import expo.modules.devlauncher.helpers.fetch
 import expo.modules.manifests.core.Manifest
+import okhttp3.Headers
 import okhttp3.OkHttpClient
 import org.json.JSONObject
 import java.io.Reader
@@ -13,7 +14,7 @@ class DevLauncherManifestParser(
   private val url: Uri
 ) {
   suspend fun isManifestUrl(): Boolean {
-    val response = fetch(url, "HEAD").await(httpClient)
+    val response = fetch(url, "HEAD", getHeaders()).await(httpClient)
     val contentType = response.header("Content-Type")
     // published projects may respond unsuccessfully to HEAD requests sent with no headers
     return !response.isSuccessful
@@ -22,7 +23,7 @@ class DevLauncherManifestParser(
   }
 
   private suspend fun downloadManifest(): Reader {
-    val response = fetch(url, "GET").await(httpClient)
+    val response = fetch(url, "GET", getHeaders()).await(httpClient)
     if (!response.isSuccessful) {
       throw Exception("Failed to open app.\n\nIf you are trying to load the app from a development server, check your network connectivity and make sure you can access the server from your device.\n\nIf you are trying to open a published project, install a compatible version of expo-updates and follow all setup and integration steps.")
     }
@@ -34,5 +35,9 @@ class DevLauncherManifestParser(
     downloadManifest().use {
       return Manifest.fromManifestJson(JSONObject(it.readText()))
     }
+  }
+
+  private fun getHeaders(): Headers {
+    return Headers.of(mapOf("expo-platform" to "android"))
   }
 }
