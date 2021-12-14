b0f66be95b0889e7e986c0401b3adb4823e8d0aa
Evan Bacon <baconbrix@gmail.com>
2021-12-14 16:43:51 -0700

    [templates] drop react imports (#15543)
    * Drop unused React imports
    
    * Remove jest-expo
    
    * Added tests back
    
    * Update yarn.lock

--- templates/expo-template-tabs/components/Themed.tsx
+++ templates/expo-template-tabs/components/Themed.tsx
@@ -3,7 +3,6 @@
  * https://docs.expo.io/guides/color-schemes/
  */
 
-import * as React from 'react';
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '../constants/Colors';
--- templates/expo-template-tabs/screens/TabTwoScreen.tsx
+++ templates/expo-template-tabs/screens/TabTwoScreen.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
