83f8eb56baf0a28a66611f6795187e05cd09d27f
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2021-12-16 12:56:25 +0100

    [autolinking] Restructure package code (#15502)
    * [autolinking] Restructure code
    
    * Fix relative require paths
    
    * Apply suggestions from base PR

---
+++ packages/expo-modules-autolinking/build/autolinking/index.d.ts
@@ -0,0 +1,5 @@
+export { findModulesAsync } from './findModules';
+export { generatePackageListAsync } from './generatePackageList';
+export { mergeLinkingOptionsAsync } from './mergeLinkingOptions';
+export { resolveModulesAsync } from './resolveModules';
+export { verifySearchResults } from './verifySearchResults';
