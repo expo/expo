a17e4d4e5c2caa4bc7f10e6bf21366d678b978cc
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2021-12-21 12:05:39 +0100

    [dev-launcher] Reimplemented the native blue screen on iOS (#15618)
    # Why
    
    Closes ENG-2396.
    
    # How
    
    Reimplemented a native blue screen on iOS

--- packages/expo-dev-launcher/ios/Errors/EXDevLauncherErrorViewController.swift
+++ packages/expo-dev-launcher/ios/Errors/EXDevLauncherErrorViewController.swift
@@ -3,25 +3,14 @@
 import Foundation
 
 @objc
-public class EXDevLauncherErrorViewController: UIViewController {
+public class EXDevLauncherErrorViewController: UIViewController, UITableViewDataSource {
   internal weak var manager: EXDevLauncherErrorManager?
+  var error: EXDevLauncherAppError?
   
-  public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "createLogView" {
-      let logView = segue.destination as? EXDevLauncherErrorLogViewController
-      logView?.manager = manager
-    }
-  }
-  
-  @IBSegueAction func createLogView(_ coder: NSCoder) -> EXDevLauncherErrorLogViewController? {
-    let logView = EXDevLauncherErrorLogViewController(coder: coder)
-    logView?.manager = manager
-    return logView
-  }
-  
-  @IBAction func onReload(_ sender: UIButton) {
-    sender.isEnabled = false
-    manager?.clearErros()
+  @IBOutlet weak var errorInformation: UILabel!
+  @IBOutlet weak var errorStack: UITableView!
+
+  @IBAction func reload(_ sender: Any) {
     guard let appUrl = manager?.controller?.sourceUrl() else {
       // We don't have app url. So we fallback to launcher.
       // Shoudn't happen.
@@ -34,17 +23,41 @@ public class EXDevLauncherErrorViewController: UIViewController {
     })
   }
   
-  @IBAction func onBackToLauncher(_ sender: UIButton) {
-    sender.isEnabled = false
-    manager?.clearErros()
+  @IBAction func goToHome(_ sender: Any) {
     navigateToLauncher()
   }
-  
-  public override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    
+  public override func viewDidLoad() {
+    error = manager?.consumeError()
+    
+    errorInformation.text = error?.message ?? "Unknown error";
+    errorStack?.dataSource = self
   }
   
+  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return error?.stack?.count ?? 0
+  }
+  
+  public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! EXDevLauncherStackTrace
+    let frame = error!.stack![indexPath.item]
+    cell.function.text = frame.methodName
+    cell.file.text = frame.file
+    return cell
+  }
+  
+  @objc
+  public static func create(forManager manager: EXDevLauncherErrorManager) -> EXDevLauncherErrorViewController? {
+    guard let bundle = EXDevLauncherUtils.resourcesBundle() else {
+      return nil
+    }
+
+    let storyboard = UIStoryboard(name: "EXDevLauncherErrorView", bundle: bundle)
+    let vc = storyboard.instantiateViewController(withIdentifier: "EXDevLauncherErrorView") as? EXDevLauncherErrorViewController
+    
+    vc?.manager = manager
+    return vc
+  }
   
   private func navigateToLauncher() {
     manager?.controller?.navigateToLauncher()
