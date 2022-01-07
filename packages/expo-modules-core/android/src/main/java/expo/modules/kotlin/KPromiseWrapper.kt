cd90d5cd0ecd5d655a0e54de645a15706fba2421
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-01-07 13:05:19 +0100

    [sweet][kotlin] Add naive `Record` to `ReadableMap` conversion

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/KPromiseWrapper.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/KPromiseWrapper.kt
@@ -2,17 +2,23 @@ package expo.modules.kotlin
 
 import android.os.Bundle
 import com.facebook.react.bridge.Arguments
+import expo.modules.kotlin.records.Record
+import expo.modules.kotlin.records.toJSMap
 
 class KPromiseWrapper(
   private val bridgePromise: com.facebook.react.bridge.Promise
 ) : Promise {
 
+  @Suppress("UNCHECKED_CAST")
   override fun resolve(value: Any?) {
     bridgePromise.resolve(
       when (value) {
         is Unit -> null
-        is Bundle -> Arguments.fromBundle(value as Bundle?)
-        is List<*> -> Arguments.fromList(value as List<*>?)
+        is Bundle -> Arguments.fromBundle(value)
+        is List<*> -> Arguments.fromList(value)
+        is Array<*> -> Arguments.fromArray(value)
+        is Map<*, *> -> Arguments.makeNativeMap(value as Map<String, Any?>) // TODO(@lukmccall): add more sophisticated conversion method
+        is Record -> value.toJSMap()
         else -> value
       }
     )
