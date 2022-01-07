a6c5df7a6936875762a8b3540df2b2e35c7649f6
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-01-07 15:39:15 +0100

    [modules-core][iOS] Expose logger
    Expose EXLogManager to Sweet API to able to log events without promise context

--- packages/expo-modules-core/ios/Swift/AppContext.swift
+++ packages/expo-modules-core/ios/Swift/AppContext.swift
@@ -87,6 +87,13 @@ public final class AppContext {
   public var eventEmitter: EXEventEmitterService? {
     return legacyModule(implementing: EXEventEmitterService.self)
   }
+  
+  /**
+   Provides access to the logger from legacy module registry.
+   */
+  public var logger: EXLogManager {
+    return legacyModuleRegistry?.getSingletonModule(forName: EXLogManager.name()) as! EXLogManager
+  }
 
   /**
    Starts listening to `UIApplication` notifications.
