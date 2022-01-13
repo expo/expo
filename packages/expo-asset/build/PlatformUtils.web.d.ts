df458ad09565a476e5d8cae47709b70b05de763c
Jon Samp <sampjon@gmail.com>
2022-01-13 13:41:22 -0500

    [expo-asset] Adds getManifest2 to web (#15891)

--- packages/expo-asset/build/PlatformUtils.web.d.ts
+++ packages/expo-asset/build/PlatformUtils.web.d.ts
@@ -4,3 +4,4 @@ export declare const IS_ENV_WITHOUT_UPDATES_ENABLED = false;
 export declare const manifestBaseUrl: null;
 export declare function downloadAsync(uri: any, hash: any, type: any, name: any): Promise<string>;
 export declare function getManifest(): {};
+export declare function getManifest2(): {};
