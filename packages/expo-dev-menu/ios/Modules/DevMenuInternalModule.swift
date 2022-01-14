bfd9544ec7ab5ea070ce1b54aa301060cb3c193d
andy <40680668+ajsmth@users.noreply.github.com>
2022-01-14 12:41:36 -0800

    [dev-menu] add ios functions for dev menu

--- packages/expo-dev-menu/ios/Modules/DevMenuInternalModule.swift
+++ packages/expo-dev-menu/ios/Modules/DevMenuInternalModule.swift
@@ -202,4 +202,35 @@ public class DevMenuInternalModule: NSObject, RCTBridgeModule {
 
     resolve(DevMenuInternalModule.defaultScheme)
   }
+  
+  @objc
+  func getBuildInfoAsync(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+    let bridge = manager.delegate?.appBridge?(forDevMenuManager: self.manager)
+    let manifest = manager.session?.appInfo ?? [:]
+    
+    let buildInfo = EXDevMenuBuildInfo.getFor(bridge as! RCTBridge, andManifest: manifest as Any as! [AnyHashable : Any])
+        
+    resolve([
+      "appName": buildInfo["appName"],
+      "appIcon": buildInfo["appIcon"],
+      "appVersion": buildInfo["appVersion"],
+      "runtimeVersion": buildInfo["runtimeVersion"],
+      "sdkVersion": buildInfo["sdkVersion"],
+      "hostUrl": buildInfo["hostUrl"],
+    ])
+  }
+  
+  @objc
+  func getDevSettingsAsync(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+    let bridge = manager.delegate?.appBridge?(forDevMenuManager: self.manager)
+    
+    if let devSettings = bridge?.module(forName: "DevSettings") as? RCTDevSettings {
+      resolve([
+        "isDebuggingRemotely": devSettings.isDebuggingRemotely,
+        "isElementInspectorShown": devSettings.isElementInspectorShown,
+        "isHotLoadingEnabled": devSettings.isHotLoadingEnabled,
+        "isPerfMonitorShown": devSettings.isPerfMonitorShown,
+      ])
+    }
+  }
 }
