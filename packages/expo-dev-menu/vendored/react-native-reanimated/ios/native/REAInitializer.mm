4906438d90e482af6e6675afbe2ddde9b2e78333
andy <40680668+ajsmth@users.noreply.github.com>
2022-01-21 15:34:56 -0800

    revert reainitializer change

--- packages/expo-dev-menu/vendored/react-native-reanimated/ios/native/REAInitializer.mm
+++ packages/expo-dev-menu/vendored/react-native-reanimated/ios/native/REAInitializer.mm
@@ -17,16 +17,16 @@ JSIExecutor::RuntimeInstaller REAJSIExecutorRuntimeInstaller(
 {
     [bridge moduleForClass:[RCTEventDispatcher class]];
     RCTEventDispatcher *eventDispatcher = [DevMenuREAEventDispatcher new];
-//#if RNVERSION >= 66
-//    RCTCallableJSModules *callableJSModules = [RCTCallableJSModules new];
-//    [bridge setValue:callableJSModules forKey:@"_callableJSModules"];
-//    [callableJSModules setBridge:bridge];
-//    [eventDispatcher setValue:callableJSModules forKey:@"_callableJSModules"];
-//    [eventDispatcher setValue:bridge forKey:@"_bridge"];
-//    [eventDispatcher initialize];
-//#else
-//#endif
+#if RNVERSION >= 66
+   RCTCallableJSModules *callableJSModules = [RCTCallableJSModules new];
+   [bridge setValue:callableJSModules forKey:@"_callableJSModules"];
+   [callableJSModules setBridge:bridge];
+   [eventDispatcher setValue:callableJSModules forKey:@"_callableJSModules"];
+   [eventDispatcher setValue:bridge forKey:@"_bridge"];
+   [eventDispatcher initialize];
+#else
   [eventDispatcher setBridge:bridge];
+#endif
 
     [bridge updateModuleWithInstance:eventDispatcher];
     _devmenu_bridge_reanimated = bridge;
