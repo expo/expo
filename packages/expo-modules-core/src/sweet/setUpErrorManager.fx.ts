09f1ceb4f16140cdde8c90f1e299a4aa2767a33d
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-01-25 22:48:15 +0100

    Fix check-packages

--- packages/expo-modules-core/src/sweet/setUpErrorManager.fx.ts
+++ packages/expo-modules-core/src/sweet/setUpErrorManager.fx.ts
@@ -2,7 +2,7 @@ import { EventEmitter } from '../EventEmitter';
 import Platform from '../Platform';
 import NativeErrorManager from './NativeErrorManager';
 
-if (__DEV__ && Platform.OS === 'android') {
+if (__DEV__ && Platform.OS === 'android' && NativeErrorManager) {
   const onNewException = 'ExpoModulesCoreErrorManager.onNewException';
   const eventEmitter = new EventEmitter(NativeErrorManager);
 
