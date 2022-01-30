77661e0bf17241b8568d0f42544366441adad8e4
Cedric van Putten <me@bycedric.com>
2022-01-30 14:56:26 +0100

    [docs] Update Link tests

--- docs/ui/components/Link/Link.test.tsx
+++ docs/ui/components/Link/Link.test.tsx
@@ -1,12 +1,12 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 
-import { Link } from '.';
+import { LinkBase } from '.';
 
 describe('Link', () => {
   test('renders <a> tag with correct external href', () => {
     const href = 'https://github.com/expo';
-    const { container } = render(<Link href={href} />);
+    const { container } = render(<LinkBase href={href} />);
 
     expect(container.querySelector('a')).toBeTruthy();
     expect(container.querySelector('a')?.getAttribute('href')).toBe(href);
@@ -14,7 +14,7 @@ describe('Link', () => {
 
   test('renders <a> tag with correct internal href', () => {
     const href = '/tools';
-    const { container } = render(<Link href={href} />);
+    const { container } = render(<LinkBase href={href} />);
 
     expect(container.querySelector('a')).toBeTruthy();
     expect(container.querySelector('a')?.getAttribute('href')).toBe(href);
