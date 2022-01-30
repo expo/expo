36f47b0c10ee9396595c7ce5b4623e2e448eaabe
Cedric van Putten <me@bycedric.com>
2022-01-30 14:55:38 +0100

    [docs] Clean up Link component and add missing duration

--- docs/ui/components/Link/index.tsx
+++ docs/ui/components/Link/index.tsx
@@ -3,19 +3,16 @@ import { theme } from '@expo/styleguide';
 import NextLink from 'next/link';
 import React, { forwardRef } from 'react';
 
+import { durations } from '~/ui/foundations/durations';
+
 export type LinkProps = React.PropsWithChildren<{
   target?: string;
   tabIndex?: number;
   href?: string;
   title?: string;
   rel?: string;
-  as?: string;
   isStyled?: boolean;
   onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
-  onMouseMove?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
-  onMouseEnter?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
-  onMouseLeave?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
-  onTouchStart?: (event: React.TouchEvent<HTMLAnchorElement>) => void;
   testID?: string;
   style?: React.CSSProperties;
   className?: string;
@@ -26,12 +23,6 @@ export type LinkProps = React.PropsWithChildren<{
 export const LinkBase = forwardRef<HTMLAnchorElement, LinkProps>(function Link(props, ref) {
   const { href, openInNewTab = false } = props;
   const relProp = props.target === '_blank' && !props.rel ? 'noopener' : props.rel;
-
-  function onKeyDown(event: React.KeyboardEvent<HTMLAnchorElement>) {
-    if (event.key === 'Enter' && props.onClick) {
-      props.onClick(event as any);
-    }
-  }
 
   return (
     <NextLink href={href ?? ''} passHref={Boolean(href)}>
@@ -46,8 +37,7 @@ export const LinkBase = forwardRef<HTMLAnchorElement, LinkProps>(function Link(p
         tabIndex={props.tabIndex}
         data-testid={props.testID}
         target={openInNewTab ? '_blank' : props.target}
-        rel={openInNewTab ? 'noopener noreferrer' : relProp}
-      >
+        rel={openInNewTab ? 'noopener noreferrer' : relProp}>
         {props.children}
       </a>
     </NextLink>
