e43901048d12a40470aa30a295357edcbdb6de07
Cedric van Putten <me@bycedric.com>
2022-02-01 14:26:13 +0100

    [docs] Clean up row styling

--- docs/ui/components/Table/Row.tsx
+++ docs/ui/components/Table/Row.tsx
@@ -7,12 +7,6 @@ type RowProps = PropsWithChildren<object>;
 export const Row = ({ children }: RowProps) => <tr css={tableRowStyle}>{children}</tr>;
 
 const tableRowStyle = css({
-  borderBottomWidth: 1,
-  borderBottomStyle: 'solid',
-  borderBottomColor: theme.border.default,
-  '&:last-child': {
-    borderWidth: 0,
-  },
   '&:nth-of-type(2n)': {
     backgroundColor: theme.background.secondary,
   },
