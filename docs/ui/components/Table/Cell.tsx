6b2422e07d619aeb0280be6e5af89915f1f9424b
Cedric van Putten <me@bycedric.com>
2022-02-01 16:09:36 +0100

    [docs] Add separate table head component

--- docs/ui/components/Table/Cell.tsx
+++ docs/ui/components/Table/Cell.tsx
@@ -7,8 +7,12 @@ type CellProps = PropsWithChildren<{
   textAlign?: TextAlign;
 }>;
 
-export const Cell = ({ children, textAlign }: CellProps) => (
-  <td css={css({ borderBottom: 0, verticalAlign: 'middle', wordBreak: 'break-word', textAlign })}>
-    {children}
-  </td>
+export const Cell = ({ children, textAlign = TextAlign.Left }: CellProps) => (
+  <td css={[tableCellStyle, { textAlign }]}>{children}</td>
 );
+
+const tableCellStyle = css({
+  borderBottom: 0,
+  verticalAlign: 'middle',
+  wordBreak: 'break-word',
+});
--- docs/ui/components/Table/index.ts
+++ docs/ui/components/Table/index.ts
@@ -2,4 +2,5 @@ export { Cell } from './Cell';
 export { HeaderCell } from './HeaderCell';
 export { Row } from './Row';
 export { Table } from './Table';
+export { TableHead } from './TableHead';
 export { TextAlign } from './types';
