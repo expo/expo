6b3b771ac7bfe80e7a6e5a97c9fddc49dce79b67
kbrandwijk <kim.brandwijk@gmail.com>
2022-02-03 01:10:57 +0100

    [expo-camera] improve android emulator detection

--- packages/expo-camera/android/src/main/java/expo/modules/camera/CameraModule.kt
+++ packages/expo-camera/android/src/main/java/expo/modules/camera/CameraModule.kt
@@ -79,6 +79,31 @@ class CameraModule(
     )
   }
 
+  private val isRunningOnEmulator: Boolean
+    get() = Build.FINGERPRINT.startsWith("generic")
+      || Build.FINGERPRINT.startsWith("unknown")
+      || Build.MODEL.contains("google_sdk")
+      || Build.MODEL.lowercase(Locale.ROOT).contains("droid4x")
+      || Build.MODEL.contains("Emulator")
+      || Build.MODEL.contains("Android SDK built for x86")
+      || Build.MANUFACTURER.contains("Genymotion")
+      || Build.HARDWARE.contains("goldfish")
+      || Build.HARDWARE.contains("ranchu")
+      || Build.HARDWARE.contains("vbox86")
+      || Build.PRODUCT.contains("sdk")
+      || Build.PRODUCT.contains("google_sdk")
+      || Build.PRODUCT.contains("sdk_google")
+      || Build.PRODUCT.contains("sdk_x86")
+      || Build.PRODUCT.contains("vbox86p")
+      || Build.PRODUCT.contains("emulator")
+      || Build.PRODUCT.contains("simulator")
+      || Build.BOARD.lowercase(Locale.ROOT).contains("nox")
+      || Build.BOOTLOADER.lowercase(Locale.ROOT).contains("nox")
+      || Build.HARDWARE.lowercase(Locale.ROOT).contains("nox")
+      || Build.PRODUCT.lowercase(Locale.ROOT).contains("nox")
+      || Build.SERIAL.lowercase(Locale.ROOT).contains("nox")
+      || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+
   @ExpoMethod
   fun takePicture(options: Map<String, Any>, viewTag: Int, promise: Promise) {
     val cacheDirectory = context.cacheDir
@@ -86,7 +111,7 @@ class CameraModule(
       viewTag,
       object : UIManager.UIBlock<ExpoCameraView> {
         override fun resolve(view: ExpoCameraView) {
-          if (!Build.FINGERPRINT.contains("generic")) {
+          if (!isRunningOnEmulator) {
             if (view.isCameraOpened) {
               view.takePicture(options, promise, cacheDirectory)
             } else {
