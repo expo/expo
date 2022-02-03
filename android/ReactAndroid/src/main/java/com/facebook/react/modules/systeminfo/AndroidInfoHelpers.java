111b486266d464aa92919ae636dead79f41566b2
kbrandwijk <kim.brandwijk@gmail.com>
2022-02-03 01:16:18 +0100

    [ReactAndroid] improve android emulator detection

--- android/ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/AndroidInfoHelpers.java
+++ android/ReactAndroid/src/main/java/com/facebook/react/modules/systeminfo/AndroidInfoHelpers.java
@@ -37,7 +37,29 @@ public class AndroidInfoHelpers {
     }
 
     private static boolean isRunningOnStockEmulator() {
-        return Build.FINGERPRINT.contains("generic");
+        return Build.FINGERPRINT.startsWith("generic")
+        || Build.FINGERPRINT.startsWith("unknown")
+        || Build.MODEL.contains("google_sdk")
+        || Build.MODEL.toLowerCase(Locale.ROOT).contains("droid4x")
+        || Build.MODEL.contains("Emulator")
+        || Build.MODEL.contains("Android SDK built for x86")
+        || Build.MANUFACTURER.contains("Genymotion")
+        || Build.HARDWARE.contains("goldfish")
+        || Build.HARDWARE.contains("ranchu")
+        || Build.HARDWARE.contains("vbox86")
+        || Build.PRODUCT.contains("sdk")
+        || Build.PRODUCT.contains("google_sdk")
+        || Build.PRODUCT.contains("sdk_google")
+        || Build.PRODUCT.contains("sdk_x86")
+        || Build.PRODUCT.contains("vbox86p")
+        || Build.PRODUCT.contains("emulator")
+        || Build.PRODUCT.contains("simulator")
+        || Build.BOARD.toLowerCase(Locale.ROOT).contains("nox")
+        || Build.BOOTLOADER.toLowerCase(Locale.ROOT).contains("nox")
+        || Build.HARDWARE.toLowerCase(Locale.ROOT).contains("nox")
+        || Build.PRODUCT.toLowerCase(Locale.ROOT).contains("nox")
+        || Build.SERIAL.toLowerCase(Locale.ROOT).contains("nox")
+        || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"));
     }
 
     public static String getServerHost(Integer port) {
