ea6720c2b32ac6ff08745154529f3447cb969616
Cedric van Putten <me@bycedric.com>
2022-02-04 13:50:47 +0100

    [docs] Add semi-optimized active node detection

--- docs/ui/components/Navigation/Navigation.tsx
+++ docs/ui/components/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
-import React, { ComponentType } from 'react';
+import { useRouter } from 'next/router';
+import React, { ComponentType, useMemo } from 'react';
 
 import { GroupList } from './GroupList';
 import { PageLink } from './PageLink';
@@ -11,7 +12,9 @@ export type NavigationProps = {
 };
 
 export function Navigation({ routes }: NavigationProps) {
-  return <nav>{routes.map(navigationRenderer)}</nav>;
+  const router = useRouter();
+  const activeRoutes = useMemo(() => findActiveRoute(routes, router.pathname), [router.pathname]);
+  return <nav>{routes.map(route => navigationRenderer(route, activeRoutes))}</nav>;
 }
 
 const renderers: Record<NavigationType, ComponentType<NavigationRenderProps>> = {
@@ -20,14 +23,67 @@ const renderers: Record<NavigationType, ComponentType<NavigationRenderProps>> = 
   page: PageLink,
 };
 
-function navigationRenderer(route: NavigationNode) {
+function navigationRenderer(
+  route: NavigationNode,
+  activeRoutes: Record<NavigationType, NavigationNode | null>
+) {
   if (route.hidden) return null;
   const Component = renderers[route.type];
   const routeKey = `${route.type}-${route.name}`;
+  const isActive = activeRoutes[route.type] === route;
   const hasChildren = route.type !== 'page' && route.children.length;
   return (
-    <Component key={routeKey} route={route}>
-      {hasChildren && route.children.map(navigationRenderer)}
+    <Component key={routeKey} route={route} isActive={isActive}>
+      {hasChildren && route.children.map(nested => navigationRenderer(nested, activeRoutes))}
     </Component>
   );
+}
+
+/**
+ * Find the active routes by pathname, and do it once.
+ * This will iterate the tree and find the nodes which are "active".
+ *   - Page -> if the pathname matches the page's href property
+ *   - Group -> if the group contains an active page
+ *   - Section -> if the section contains an active group or page
+ */
+export function findActiveRoute(routes: NavigationNode[], pathname: string) {
+  const activeRoutes: Record<NavigationType, NavigationNode | null> = {
+    page: null,
+    group: null,
+    section: null,
+  };
+
+  for (const route of routes) {
+    // Try to exit early on hidden routes
+    if (route.hidden) continue;
+
+    switch (route.type) {
+      case 'page':
+        if (route.href === pathname) activeRoutes.page = route;
+        break;
+
+      case 'group':
+        {
+          const nestedActiveRoutes = findActiveRoute(route.children, pathname);
+          if (nestedActiveRoutes.page) {
+            activeRoutes.page = nestedActiveRoutes.page;
+            activeRoutes.group = route;
+          }
+        }
+        break;
+
+      case 'section':
+        {
+          const nestedActiveRoutes = findActiveRoute(route.children, pathname);
+          if (nestedActiveRoutes.group || nestedActiveRoutes.page) {
+            activeRoutes.page = nestedActiveRoutes.page;
+            activeRoutes.group = nestedActiveRoutes.group;
+            activeRoutes.section = route;
+          }
+        }
+        break;
+    }
+  }
+
+  return activeRoutes;
 }
