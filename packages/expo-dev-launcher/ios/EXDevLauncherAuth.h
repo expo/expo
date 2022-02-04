9422484288009253bd5d7b27b1f56b118314c615
andy <40680668+ajsmth@users.noreply.github.com>
2022-02-04 10:08:26 -0800

    Update packages/expo-dev-launcher/ios/EXDevLauncherAuth.h
    Co-authored-by: Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>

--- packages/expo-dev-launcher/ios/EXDevLauncherAuth.h
+++ packages/expo-dev-launcher/ios/EXDevLauncherAuth.h
@@ -1,9 +1,4 @@
-//
-//  EXDevLauncherAuth.h
-//  Pods
-//
-//  Created by andrew on 2022-02-02.
-//
+// Copyright 2015-present 650 Industries. All rights reserved.
 
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
