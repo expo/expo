3192babc625821064c8ec310915487892065bde2
Simek <gosimek@gmail.com>
2022-02-08 16:05:42 +0100

    apply suggested export tweak

--- packages/expo-av/build/index.js
+++ packages/expo-av/build/index.js
@@ -1,5 +1,5 @@
 import * as Audio from './Audio';
 export { Audio };
-export { InterruptionModeAndroid, InterruptionModeIOS } from './Audio.types';
+export * from './Audio.types';
 export { default as Video, ResizeMode, } from './Video';
 //# sourceMappingURL=index.js.map
