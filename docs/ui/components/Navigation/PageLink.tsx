701022139cc53b096dcba7550348da173502c61f
Cedric van Putten <me@bycedric.com>
2022-02-09 06:26:18 -0800

    [docs] Partially add redesigned sidebar navigation (#16192)
    * [docs] Add first draft sidebar navigation components
    
    * [docs] Add active page marker and fix spacing
    
    * [docs] Add navigation sidebar renderer and fix section list
    
    * [docs] Add semi-optimized active node detection
    
    * [docs] Add tests for navigation active node detection
    
    * [docs] Add navigation render tests
    
    * [docs] Fix linting issues
    
    * [docs] Stop the iteration early if the active nodes are found
    
    * [docs] Align group titles with text
    
    * [docs] Make the error message in navigation renders a bit clearer

---
+++ docs/ui/components/Navigation/PageLink.tsx
@@ -0,0 +1,59 @@
+import { css } from '@emotion/react';
+import { borderRadius, iconSize, shadows, spacing, theme, typography } from '@expo/styleguide';
+import React from 'react';
+
+import { NavigationRenderProps } from './types';
+
+import { A, CALLOUT } from '~/ui/components/Text';
+
+export function PageLink({ route, isActive }: NavigationRenderProps) {
+  if (route.type !== 'page') {
+    throw new Error(`Navigation route is not a page`);
+  }
+
+  return (
+    <A css={[linkStyle, isActive && linkStyleActive]} href={route.href}>
+      <i css={[markerStyle, isActive && markerStyleActive]} />
+      <CALLOUT css={[textStyle, isActive && textStyleActive]} tag="span">
+        {route.sidebarTitle || route.name}
+      </CALLOUT>
+    </A>
+  );
+}
+
+const linkStyle = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  borderRadius: borderRadius.medium,
+  padding: `${spacing[1.5]}px ${spacing[2]}px`,
+  margin: `${spacing[1]}px ${spacing[4]}px`,
+});
+
+const linkStyleActive = css({
+  backgroundColor: theme.background.default,
+  boxShadow: shadows.micro,
+});
+
+const markerStyle = css({
+  flexShrink: 0,
+  backgroundColor: theme.icon.secondary,
+  borderRadius: iconSize.micro,
+  width: iconSize.micro / 2,
+  height: iconSize.micro / 2,
+  marginRight: spacing[2],
+  visibility: 'hidden',
+});
+
+const markerStyleActive = css({
+  visibility: 'visible',
+});
+
+const textStyle = css({
+  color: theme.text.secondary,
+});
+
+const textStyleActive = css({
+  ...typography.utility.weight.medium,
+  color: theme.text.default,
+});
---
+++ docs/ui/components/Navigation/SectionList.tsx
@@ -0,0 +1,56 @@
+import { css } from '@emotion/react';
+import { iconSize, spacing, typography } from '@expo/styleguide';
+import React, { PropsWithChildren } from 'react';
+
+import { NavigationRenderProps } from '.';
+
+import { CALLOUT } from '~/ui/components/Text';
+import { durations } from '~/ui/foundations/durations';
+import { ChevronDownIcon } from '~/ui/foundations/icons';
+
+type SectionListProps = PropsWithChildren<NavigationRenderProps>;
+
+export function SectionList({ route, isActive, children }: SectionListProps) {
+  if (route.type !== 'section') {
+    throw new Error(`Navigation route is not a section`);
+  }
+
+  return (
+    <details css={detailsStyle} open={isActive || route.collapsed !== true}>
+      <summary css={summaryStyle}>
+        <ChevronDownIcon css={iconStyle} size={iconSize.small} />
+        <CALLOUT css={textStyle} tag="span">
+          {route.name}
+        </CALLOUT>
+      </summary>
+      <div>{children}</div>
+    </details>
+  );
+}
+
+const detailsStyle = css({
+  paddingTop: spacing[3],
+  marginBottom: spacing[3],
+});
+
+const summaryStyle = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  listStyle: 'none',
+  userSelect: 'none',
+  margin: `0 ${spacing[4]}px`,
+});
+
+const iconStyle = css({
+  flexShrink: 0,
+  transform: 'rotate(-90deg)',
+  transition: `transform ${durations.hover}`,
+
+  'details[open] &': { transform: 'rotate(0)' },
+});
+
+const textStyle = css({
+  ...typography.utility.weight.medium,
+  padding: spacing[1.5],
+});
