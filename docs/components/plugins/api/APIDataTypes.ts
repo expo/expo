1886d870e63b8014003abb6f1b599560fd691438
Bartosz Kaszubowski <gosimek@gmail.com>
2022-02-09 12:03:45 +0100

    [auth-session] add doc comments in source (#16223)

--- docs/components/plugins/api/APIDataTypes.ts
+++ docs/components/plugins/api/APIDataTypes.ts
@@ -109,12 +109,13 @@ export type InterfaceDefinitionData = {
   children: PropData[];
   comment?: CommentData;
   kind: TypeDocKind;
+  extendedTypes?: TypeDefinitionData[];
+  implementedTypes?: TypeDefinitionData[];
 };
 
 // Classes section
 
 export type ClassDefinitionData = InterfaceDefinitionData & {
-  extendedTypes?: TypeDefinitionData[];
   type?: TypeDefinitionData;
 };
 
@@ -151,6 +152,8 @@ export type PropData = {
   defaultValue?: string;
   signatures?: MethodSignatureData[];
   overwrites?: TypeDefinitionData;
+  implementationOf?: TypeDefinitionData;
+  inheritedFrom?: TypeGeneralData;
 };
 
 export type DefaultPropsDefinitionData = {
