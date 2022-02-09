72b01a812030599fbb4da4b7330ef9e4e9c0678d
Cedric van Putten <me@bycedric.com>
2022-02-09 06:48:41 -0800

    [docs] Add renewed version selector to navigation (#16194)
    * [docs] Add renewed version selector to navigation
    
    * [docs] Fix linting issue

---
+++ docs/ui/components/Navigation/ApiVersionSelect.tsx
@@ -0,0 +1,94 @@
+import { css } from '@emotion/react';
+import { borderRadius, iconSize, shadows, spacing, theme } from '@expo/styleguide';
+import React from 'react';
+
+import { VERSIONS, LATEST_VERSION, BETA_VERSION } from '~/constants/versions';
+import { usePageApiVersion } from '~/providers/page-api-version';
+import { LABEL } from '~/ui/components/Text';
+import { ChevronDownIcon } from '~/ui/foundations/icons';
+
+// TODO(cedric): move this to a generic select input, so we can reuse it in the color scheme selector
+
+export function ApiVersionSelect() {
+  const { version, hasVersion, setVersion } = usePageApiVersion();
+
+  if (!hasVersion) {
+    return null;
+  }
+
+  return (
+    <div css={containerStyle}>
+      <label css={labelStyle} htmlFor="api-version-select">
+        <LABEL css={labelTextStyle}>{versionToText(version)}</LABEL>
+        <ChevronDownIcon css={labelIconStyle} size={iconSize.small} />
+      </label>
+      <select
+        id="api-version-select"
+        css={selectStyle}
+        value={version}
+        onChange={event => setVersion(event.target.value)}>
+        {VERSIONS.map(version => (
+          <option key={version} value={version}>
+            {versionToText(version)}
+          </option>
+        ))}
+      </select>
+      {/* Changing versions is a JS only mechanism. To help crawlers find other versions, we add hidden links. */}
+      {VERSIONS.map(version => (
+        <a css={crawlerLinkStyle} key={version} href={`/versions/${version}`} />
+      ))}
+    </div>
+  );
+}
+
+function versionToText(version: string): string {
+  if (version === 'unversioned') {
+    return 'Unversioned';
+  } else if (version === 'latest') {
+    return `${versionToText(LATEST_VERSION)} (latest)`;
+  } else if (version === BETA_VERSION) {
+    return `${versionToText(BETA_VERSION)} (beta)`;
+  }
+  return `SDK ${version.substring(1, 3)}`;
+}
+
+const containerStyle = css({
+  position: 'relative',
+  background: theme.background.default,
+  border: `1px solid ${theme.border.default}`,
+  borderRadius: borderRadius.medium,
+  boxShadow: shadows.input,
+  margin: spacing[4],
+  padding: `${spacing[2]}px ${spacing[3]}px`,
+});
+
+const labelStyle = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+});
+
+const labelTextStyle = css({
+  flex: 1,
+});
+
+const labelIconStyle = css({
+  flexShrink: 0,
+});
+
+const crawlerLinkStyle = css({
+  display: 'none',
+});
+
+const selectStyle = css({
+  borderRadius: 0,
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  opacity: 0,
+  cursor: 'pointer',
+});
