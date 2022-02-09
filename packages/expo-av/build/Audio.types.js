6e9b176fd3c8eb7d1a7ff64ffb77d6ef287e4537
Bartosz Kaszubowski <gosimek@gmail.com>
2022-02-09 12:43:15 +0100

    [av] audio: refactor interruption modes to enums, export missing type (#16145)

---
+++ packages/expo-av/build/Audio.types.js
@@ -0,0 +1,12 @@
+export var InterruptionModeIOS;
+(function (InterruptionModeIOS) {
+    InterruptionModeIOS[InterruptionModeIOS["MixWithOthers"] = 0] = "MixWithOthers";
+    InterruptionModeIOS[InterruptionModeIOS["DoNotMix"] = 1] = "DoNotMix";
+    InterruptionModeIOS[InterruptionModeIOS["DuckOthers"] = 2] = "DuckOthers";
+})(InterruptionModeIOS || (InterruptionModeIOS = {}));
+export var InterruptionModeAndroid;
+(function (InterruptionModeAndroid) {
+    InterruptionModeAndroid[InterruptionModeAndroid["DoNotMix"] = 1] = "DoNotMix";
+    InterruptionModeAndroid[InterruptionModeAndroid["DuckOthers"] = 2] = "DuckOthers";
+})(InterruptionModeAndroid || (InterruptionModeAndroid = {}));
+//# sourceMappingURL=Audio.types.js.map
