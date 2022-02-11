f9b3b8685a69260d01d1163533c56d7608ccc9d8
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-02-11 14:57:57 +0000

    [ncl] Refactor examples for WebBrowser (part 1)

---
+++ apps/native-component-list/src/screens/WebBrowser/OpenAuthSessionAsyncDemo.tsx
@@ -0,0 +1,59 @@
+import * as Linking from 'expo-linking';
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import Button from 'src/components/Button';
+import Configurator, { ConfiguratorChoiceType } from 'src/components/Configurator';
+import HeadingText from 'src/components/HeadingText';
+import MonoText from 'src/components/MonoText';
+
+const openAuthSessionConfigurationChoices: ConfiguratorChoiceType[] = [
+  {
+    name: 'shouldPrompt',
+    title: 'Should prompt',
+    initial: false,
+  },
+  {
+    name: 'createTask',
+    title: 'Create task',
+    initial: true,
+  },
+];
+
+export default function OpenAuthSessionAsyncDemo() {
+  const [options, setOptions] = React.useState<{ shouldPrompt?: boolean; createTask?: boolean }>(
+    {}
+  );
+  const [authResult, setAuthResult] = React.useState<WebBrowser.WebBrowserAuthSessionResult | null>(
+    null
+  );
+
+  const openAuthSession = React.useCallback(async () => {
+    const redirectUrl = Linking.makeUrl('redirect');
+    const result = await WebBrowser.openAuthSessionAsync(
+      `https://fake-auth.netlify.com?state=faker&redirect_uri=${encodeURIComponent(
+        redirectUrl
+      )}&prompt=${options.shouldPrompt ? 'consent' : 'none'}`,
+      redirectUrl,
+      { createTask: options.createTask }
+    );
+
+    setAuthResult(result);
+  }, [options]);
+
+  return (
+    <>
+      <HeadingText>openAuthSessionAsync</HeadingText>
+      <Configurator choices={openAuthSessionConfigurationChoices} onChange={setOptions} />
+      <Button style={styles.button} onPress={openAuthSession} title="Open web auth session" />
+      {authResult && <MonoText>result: {JSON.stringify(authResult, null, 2)}</MonoText>}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    marginVertical: 10,
+    alignItems: 'flex-start',
+  },
+});
