6179307460206307e5716f2cb86d8db8f762038c
Eric Samelson <eric@expo.io>
2022-02-14 15:40:17 -0800

    ReactActivityHandler -> ReactActivityDelegateHandler

--- packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ReactActivityDelegateHandler.java
+++ packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ReactActivityDelegateHandler.java
@@ -8,10 +8,10 @@ import com.facebook.react.ReactRootView;
 import androidx.annotation.Nullable;
 
 /**
- * A handler API for modules to override default ReactActivity behaviors.
+ * A handler API for modules to override default ReactActivityDelegate behaviors.
  * Used by {@link ReactActivityDelegateWrapper}
  */
-public interface ReactActivityHandler {
+public interface ReactActivityDelegateHandler {
   /**
    * Given modules a chance to override the default {@link ReactRootView}
    * @return the override ReactRootView instance or null if not to override
