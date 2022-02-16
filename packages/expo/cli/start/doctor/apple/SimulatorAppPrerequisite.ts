4f3988552d86e17531d2b14e3219a0b41b140440
Evan Bacon <bacon@expo.io>
2022-02-16 15:35:47 -0700

    decouple doctor from platform

--- packages/expo/cli/start/doctor/apple/SimulatorAppPrerequisite.ts
+++ packages/expo/cli/start/doctor/apple/SimulatorAppPrerequisite.ts
@@ -1,8 +1,8 @@
 import { execAsync } from '@expo/osascript';
+import spawnAsync from '@expo/spawn-async';
 
 import * as Log from '../../../log';
 import { CommandError } from '../../../utils/errors';
-import * as SimControl from '../../platforms/ios/simctl';
 import { Prerequisite } from './Prerequisite';
 
 async function getSimulatorAppIdAsync(): Promise<string | null> {
@@ -22,6 +22,7 @@ export class SimulatorAppPrerequisite extends Prerequisite {
     if (!result) {
       // This error may occur in CI where the users intends to install just the simulators but no Xcode.
       throw new CommandError(
+        'VALIDATE_SIMULATOR_APP',
         "Can't determine id of Simulator app; the Simulator is most likely not installed on this machine. Run `sudo xcode-select -s /Applications/Xcode.app`"
       );
     }
@@ -30,22 +31,20 @@ export class SimulatorAppPrerequisite extends Prerequisite {
       result !== 'com.apple.CoreSimulator.SimulatorTrampoline'
     ) {
       throw new CommandError(
+        'VALIDATE_SIMULATOR_APP',
         "Simulator is installed but is identified as '" + result + "'; don't know what that is."
       );
     }
 
-    // make sure we can run simctl
     try {
-      await SimControl.simctlAsync(['help']);
+      // make sure we can run simctl
+      await spawnAsync('xcrun', ['simctl', 'help']);
     } catch (e) {
-      if (e instanceof CommandError) {
-        throw e;
-      } else {
-        Log.warn(`Unable to run simctl: ${e.toString()}`);
-        throw new CommandError(
-          'xcrun may not be configured correctly. Try running `sudo xcode-select --reset` and running this again.'
-        );
-      }
+      Log.warn(`Unable to run simctl:\n${e.toString()}`);
+      throw new CommandError(
+        'VALIDATE_SIMCTL',
+        'xcrun is not configured correctly. Ensure `sudo xcode-select --reset` works before running this command again.'
+      );
     }
   }
 }
--- packages/expo/cli/start/doctor/apple/__tests__/SimulatorAppPrerequisite-test.ts
+++ packages/expo/cli/start/doctor/apple/__tests__/SimulatorAppPrerequisite-test.ts
@@ -1,53 +1,42 @@
 import { execAsync } from '@expo/osascript';
+import spawnAsync from '@expo/spawn-async';
 
-import { simctlAsync } from '../../../platforms/ios/simctl';
 import { SimulatorAppPrerequisite } from '../SimulatorAppPrerequisite';
 
 const asMock = (fn: any): jest.Mock => fn;
 
 jest.mock(`../../../../log`);
-
-jest.mock('../../../platforms/ios/simctl', () => {
-  return {
-    simctlAsync: jest.fn(),
-  };
-});
-
 jest.mock(`@expo/osascript`);
-
-beforeEach(() => {
-  jest.mock('../../../../utils/prompts').resetAllMocks();
-  jest.mock('@expo/osascript').resetAllMocks();
-  jest.mock('../../../platforms/ios/simctl').resetAllMocks();
-});
+jest.mock('@expo/spawn-async');
 
 it(`detects that Simulator.app is installed`, async () => {
   // Mock Simulator.app installed for CI
-  asMock(execAsync).mockReturnValueOnce(`com.apple.CoreSimulator.SimulatorTrampoline`);
-  asMock(simctlAsync).mockReturnValueOnce(`usage: ...`);
+  asMock(execAsync).mockReset().mockReturnValueOnce(`com.apple.CoreSimulator.SimulatorTrampoline`);
+  asMock(spawnAsync).mockReset().mockReturnValueOnce(`usage: ...`);
 
   await SimulatorAppPrerequisite.instance.assertImplementation();
 
   expect(execAsync).toBeCalledWith('id of app "Simulator"');
-  expect(simctlAsync).toBeCalledWith(['help']);
+  expect(spawnAsync).toBeCalledWith('xcrun', ['simctl', 'help']);
 });
 
 it(`asserts that Simulator.app is installed with invalid Simulator.app`, async () => {
   // Mock Simulator.app installed with invalid binary
   asMock(execAsync).mockReturnValueOnce(`com.apple.CoreSimulator.bacon`);
+  asMock(spawnAsync).mockReset();
 
   await expect(SimulatorAppPrerequisite.instance.assertImplementation()).rejects.toThrow(/\.bacon/);
-  expect(simctlAsync).not.toBeCalled();
+  expect(spawnAsync).not.toBeCalled();
 });
 
 it(`asserts that Simulator.app is installed but simctl doesn't work`, async () => {
   // Mock Simulator.app installed for CI
   asMock(execAsync).mockReturnValueOnce(`com.apple.CoreSimulator.SimulatorTrampoline`);
-  asMock(simctlAsync).mockImplementationOnce(() => {
+  asMock(spawnAsync).mockImplementationOnce(() => {
     throw new Error('foobar');
   });
 
   await expect(SimulatorAppPrerequisite.instance.assertImplementation()).rejects.toThrow(
-    /xcrun may not be configured correctly/
+    /xcrun is not configured correctly/
   );
 });
