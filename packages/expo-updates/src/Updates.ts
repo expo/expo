eb77733b3d8ba100b337c351deaf3642984ce249
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-02-18 09:29:59 +0100

    Update packages/expo-updates/src/Updates.ts
    Co-authored-by: Eric Samelson <esamelson@users.noreply.github.com>

--- packages/expo-updates/src/Updates.ts
+++ packages/expo-updates/src/Updates.ts
@@ -82,8 +82,8 @@ export const manifest: Partial<Manifest> =
 /**
  * If `expo-updates` is enabled, this is a `Date` object representing the creation time of the update that's currently running (whether it was embedded or downloaded at runtime).
  *
- * In development mode, or any other environment in which `expo-updates` is disabled, this object is
- * empty.
+ * In development mode, or any other environment in which `expo-updates` is disabled, this value is
+ * null.
  */
 export const creationTime: Date | null = ExpoUpdates.commitTime
   ? new Date(ExpoUpdates.commitTime)
