5043e0da28b9185ad1a498cf8cfa8e4471d5a87f
andy <40680668+ajsmth@users.noreply.github.com>
2022-02-23 12:01:54 -0800

    test todos

--- packages/expo-dev-client-components/src/__tests__/create-primitive.test.tsx
+++ packages/expo-dev-client-components/src/__tests__/create-primitive.test.tsx
@@ -1,66 +1,6 @@
-import { render } from '@testing-library/react-native';
-import * as React from 'react';
-import { Text, View } from 'react-native';
 
-import { create } from '../create-primitive';
-
-test('it renders the given component', async () => {
-  const Heading = create(Text, {});
-  const Box = create(View, {});
-
-  const { toJSON: textJSON } = render(<Heading>Hi</Heading>);
-  let json = textJSON();
-
-  expect(json.type).toEqual('Text');
-
-  let { toJSON: viewJSON } = render(<Box />);
-  json = viewJSON();
-
-  expect(json.type).toEqual('View');
-});
-
-test('it passes variant style props', async () => {
-  const Heading = create(Text, {
-    variants: {
-      size: {
-        large: {
-          fontSize: 20,
-        },
-      },
-    },
-  });
-
-  const { toJSON } = render(<Heading size="large">Hi</Heading>);
-
-  const json = toJSON();
-
-  expect(json.props.style.fontSize).toEqual(20);
-});
-
-test('it passes base style props', async () => {
-  const Heading = create(Text, {
-    base: {
-      fontFamily: 'Helvetica',
-    },
-  });
-
-  const { toJSON } = render(<Heading>Hi</Heading>);
-  const json = toJSON();
-  expect(json.props.style).toEqual({ fontFamily: 'Helvetica' });
-});
-
-test('it passes non-style props', () => {
-  const Heading = create(Text, {
-    props: {
-      accessibilityRole: 'header',
-    },
-  });
-
-  const { toJSON } = render(<Heading>Hi</Heading>);
-  const json = toJSON();
-
-  expect(json.props.accessibilityRole).toEqual('header');
-});
-
+test.todo('it renders the given component');
+test.todo('it passes base style props');
+test.todo('it passes non-style props');
 test.todo('selector api');
 test.todo('forwarding refs');
