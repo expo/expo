2892ad7171584810521cd83132cdb651358cd4e2
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-02-24 15:00:54 +0100

    [web-browser] Extracted static demo components configurations from the render functions

--- apps/native-component-list/src/screens/WebBrowser/OpenAuthSessionAsyncDemo.tsx
+++ apps/native-component-list/src/screens/WebBrowser/OpenAuthSessionAsyncDemo.tsx
@@ -2,40 +2,37 @@ import * as Linking from 'expo-linking';
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 
-import FunctionDemo from '../../components/FunctionDemo';
+import FunctionDemo, { FunctionDescription } from '../../components/FunctionDemo';
+
+const FUNCTION_DESCRIPTION: FunctionDescription = {
+  name: 'openAuthSessionAsync',
+  parameters: [
+    {
+      name: 'url',
+      type: 'constant',
+      value: 'url',
+    },
+    { name: 'redirectUrl', type: 'constant', value: Linking.createURL('redirect') },
+    {
+      name: 'options',
+      type: 'object',
+      properties: [{ name: 'createTask', type: 'boolean', initial: true }],
+    },
+  ],
+  additionalParameters: [{ name: 'shouldPrompt', type: 'boolean', initial: false }],
+  actions: (
+    _: string,
+    redirectUrl: string,
+    options: WebBrowser.WebBrowserOpenOptions,
+    shouldPrompt: boolean
+  ) => {
+    const url = `https://fake-auth.netlify.com?state=faker&redirect_uri=${encodeURIComponent(
+      redirectUrl
+    )}&prompt=${shouldPrompt ? 'consent' : 'none'}`;
+    return WebBrowser.openAuthSessionAsync(url, redirectUrl, options);
+  },
+};
 
 export default function OpenAuthSessionAsyncDemo() {
-  const redirectUrl = Linking.createURL('redirect');
-
-  return (
-    <FunctionDemo
-      namespace="WebBrowser"
-      name="openAuthSessionAsync"
-      parameters={[
-        {
-          name: 'url',
-          type: 'constant',
-          value: 'url',
-        },
-        { name: 'redirectUrl', type: 'constant', value: redirectUrl },
-        {
-          name: 'options',
-          type: 'object',
-          properties: [{ name: 'createTask', type: 'boolean', initial: true }],
-        },
-      ]}
-      additionalParameters={[{ name: 'shouldPrompt', type: 'boolean', initial: false }]}
-      actions={(
-        _: string,
-        redirectUrl: string,
-        options: WebBrowser.WebBrowserOpenOptions,
-        shouldPrompt: boolean
-      ) => {
-        const url = `https://fake-auth.netlify.com?state=faker&redirect_uri=${encodeURIComponent(
-          redirectUrl
-        )}&prompt=${shouldPrompt ? 'consent' : 'none'}`;
-        return WebBrowser.openAuthSessionAsync(url, redirectUrl, options);
-      }}
-    />
-  );
+  return <FunctionDemo namespace="WebBrowser" {...FUNCTION_DESCRIPTION} />;
 }
--- apps/native-component-list/src/screens/WebBrowser/OpenBrowserAsyncDemo.tsx
+++ apps/native-component-list/src/screens/WebBrowser/OpenBrowserAsyncDemo.tsx
@@ -1,116 +1,115 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 
-import FunctionDemo from '../../components/FunctionDemo';
+import FunctionDemo, { FunctionDescription } from '../../components/FunctionDemo';
 import Colors from '../../constants/Colors';
 
 const URL = 'https://blog.expo.dev/expo-sdk-44-4c4b8306584a';
 
-export default function OpenBrowserAsyncDemo() {
-  return (
-    <FunctionDemo
-      namespace="WebBrowser"
-      name="openBrowserAsync"
-      parameters={[
+const FUNCTIONS_DESCRIPTION: FunctionDescription = {
+  name: 'openBrowserAsync',
+  parameters: [
+    {
+      type: 'constant',
+      name: 'url',
+      value: URL,
+    },
+    {
+      type: 'object',
+      name: 'options',
+      properties: [
         {
-          type: 'constant',
-          name: 'url',
-          value: URL,
-        },
-        {
-          type: 'object',
-          name: 'options',
-          properties: [
+          name: 'toolbarColor',
+          type: 'enum',
+          values: [
             {
-              name: 'toolbarColor',
-              type: 'enum',
-              values: [
-                {
-                  name: 'undefined',
-                  value: undefined,
-                },
-                {
-                  name: `"${Colors.tintColor}"`,
-                  value: Colors.tintColor,
-                },
-              ],
+              name: 'undefined',
+              value: undefined,
             },
             {
-              name: 'secondaryToolbarColor',
-              platforms: ['android'],
-              type: 'enum',
-              values: [
-                {
-                  name: 'undefined',
-                  value: undefined,
-                },
-                {
-                  name: `"${Colors.highlightColor}"`,
-                  value: Colors.highlightColor,
-                },
-              ],
-            },
-            {
-              name: 'controlsColor',
-              type: 'enum',
-              values: [
-                {
-                  name: 'undefined',
-                  value: undefined,
-                },
-                { name: `"${Colors.headerTitle}"`, value: Colors.headerTitle },
-              ],
-            },
-            {
-              type: 'boolean',
-              name: 'showTitle',
-              platforms: ['android'],
-              initial: false,
-            },
-            {
-              type: 'boolean',
-              name: 'showInRecents',
-              platforms: ['android'],
-              initial: false,
-            },
-            {
-              type: 'boolean',
-              name: 'enableBarCollapsing',
-              initial: false,
-            },
-            {
-              type: 'boolean',
-              name: 'readerMode',
-              platforms: ['ios'],
-              initial: false,
-            },
-            {
-              type: 'boolean',
-              name: 'enableDefaultShareMenuItem',
-              platforms: ['android'],
-              initial: false,
+              name: `"${Colors.tintColor}"`,
+              value: Colors.tintColor,
             },
           ],
         },
-      ]}
-      actions={[
-        { name: 'Open', action: WebBrowser.openBrowserAsync },
         {
-          name: 'Open and dismiss',
-          action: async (url: string, openOptions: WebBrowser.WebBrowserOpenOptions) => {
-            await WebBrowser.openBrowserAsync(url, openOptions);
-            return WebBrowser.dismissBrowser();
-          },
+          name: 'secondaryToolbarColor',
+          platforms: ['android'],
+          type: 'enum',
+          values: [
+            {
+              name: 'undefined',
+              value: undefined,
+            },
+            {
+              name: `"${Colors.highlightColor}"`,
+              value: Colors.highlightColor,
+            },
+          ],
         },
         {
-          name: 'Open twice',
-          action: (url: string, openOptions: WebBrowser.WebBrowserOpenOptions) => {
-            WebBrowser.openBrowserAsync(url, openOptions);
-            return WebBrowser.openBrowserAsync(url, openOptions);
-          },
+          name: 'controlsColor',
+          type: 'enum',
+          values: [
+            {
+              name: 'undefined',
+              value: undefined,
+            },
+            { name: `"${Colors.headerTitle}"`, value: Colors.headerTitle },
+          ],
         },
-        { name: 'Dismiss (no-op)', action: async () => WebBrowser.dismissBrowser() },
-      ]}
-    />
-  );
+        {
+          type: 'boolean',
+          name: 'showTitle',
+          platforms: ['android'],
+          initial: false,
+        },
+        {
+          type: 'boolean',
+          name: 'showInRecents',
+          platforms: ['android'],
+          initial: false,
+        },
+        {
+          type: 'boolean',
+          name: 'enableBarCollapsing',
+          initial: false,
+        },
+        {
+          type: 'boolean',
+          name: 'readerMode',
+          platforms: ['ios'],
+          initial: false,
+        },
+        {
+          type: 'boolean',
+          name: 'enableDefaultShareMenuItem',
+          platforms: ['android'],
+          initial: false,
+        },
+      ],
+    },
+  ],
+  actions: [
+    { name: 'Open', action: WebBrowser.openBrowserAsync },
+    {
+      name: 'Open and dismiss',
+      action: async (url: string, openOptions: WebBrowser.WebBrowserOpenOptions) => {
+        await WebBrowser.openBrowserAsync(url, openOptions);
+        return WebBrowser.dismissBrowser();
+      },
+    },
+    {
+      name: 'Open twice',
+      action: (url: string, openOptions: WebBrowser.WebBrowserOpenOptions) => {
+        WebBrowser.openBrowserAsync(url, openOptions);
+        return WebBrowser.openBrowserAsync(url, openOptions);
+      },
+    },
+    { name: 'Dismiss (no-op)', action: async () => WebBrowser.dismissBrowser() },
+  ],
+};
+
+export default function OpenBrowserAsyncDemo() {
+  return <FunctionDemo namespace="WebBrowser" {...FUNCTIONS_DESCRIPTION} />;
 }
