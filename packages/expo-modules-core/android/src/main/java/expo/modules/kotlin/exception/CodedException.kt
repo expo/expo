bd03de4a6d7f659e306336987ef73a281964a61d
Bartosz Kaszubowski <gosimek@gmail.com>
2022-03-01 15:44:19 +0100

    [emc] suppress Kotlin warning (#16485)

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/exception/CodedException.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/exception/CodedException.kt
@@ -36,6 +36,7 @@ open class CodedException(
     internal fun inferCode(clazz: Class<*>): String {
       val name = requireNotNull(clazz.simpleName) { "Cannot infer code name from class name. We don't support anonymous classes." }
 
+      @Suppress("Deprecation")
       return "ERR_" + name
         .replace("(Exception)$".toRegex(), "")
         .replace("(.)([A-Z])".toRegex(), "$1_$2")
