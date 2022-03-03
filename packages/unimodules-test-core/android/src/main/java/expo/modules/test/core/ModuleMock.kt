594496f90d029a8c358c106b529ec8d69202ec0c
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-03-03 12:29:51 +0100

    [modules-core][android] Improve test library (#16416)
    # Why
    
    Part of ENG-4140.
    
    This PR introduces a new way of mocking an expo module. Currently, the code lives in the `unimodules-core-test` package. I plan to rename that later. 
    
    # How
    
    Existing mocking mechanism forces as to call methods via their names and mock the promise. It works fine, however, we can do it better. Our modules API promotes writing promises-less functions (promises are hidden from users). So why do we have to deal with promises even if the call on the native side is 100% sync? Therefore, I've created an abstraction that uses the invocation handler to remove promises.
    
    ## How does it work?
    
    Firstly, you have to create a module test interface that will be used to call functions exported via DSL.  You can think of that as a native interface for js exported functions. 
    For instance module like this:
    ```kt
    class MyModule : Module() {
      override fun definition() = ModuleDefinition {
        function("f1") {
          return@function "string"
        }
    
        function("f2") { param: String ->
          return@function true
        }
      }
    }
    ```
    can be typed using an interface like this:
    ```kt
    interface MyModuleTestInterface {
    	fun f1(): String
    	fun f2(param: String): Bool
    }
    ```
    The test interface could be generated in the future, but for now, it is beyond the scope.
    
    After that, you are ready to create a mocked module:
    ```kt
    ModuleMock.createMock(MyModuleTestInterface::class, MyModule()) {
    	// here you can access to the mocked object
    }
    ```
    In the `createMock` block you have access to the `MyModuleTestInterface` object and if you call something on that it will be delegated to the exported method.
    
    # Test Plan
    
    - run tests for `expo-clipboard`

--- packages/unimodules-test-core/android/src/main/java/expo/modules/test/core/ModuleMock.kt
+++ packages/unimodules-test-core/android/src/main/java/expo/modules/test/core/ModuleMock.kt
@@ -2,98 +2,85 @@ package expo.modules.test.core
 
 import android.os.Bundle
 import androidx.test.core.app.ApplicationProvider
-import com.facebook.react.bridge.JavaOnlyArray
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReadableArray
 import expo.modules.core.interfaces.services.EventEmitter
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.ModuleHolder
-import expo.modules.kotlin.events.EventName
 import expo.modules.kotlin.modules.Module
-import expo.modules.kotlin.records.Record
-import expo.modules.kotlin.records.toJSMap
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
-import java.lang.IllegalArgumentException
 import java.lang.ref.WeakReference
+import java.lang.reflect.Proxy
+import kotlin.reflect.KClass
 
-class ModuleMock<T : Module>(
-  module: T,
-  autoOnCreate: Boolean = false
-) {
-  private val moduleHolder: ModuleHolder
-  val eventEmitter: EventEmitter = mockk(relaxed = true)
-
-  init {
-    val context = ReactApplicationContext(ApplicationProvider.getApplicationContext())
-    val appContext = spyk(
-      AppContext(
+class ModuleMock {
+  companion object {
+    fun <T : Any> createMock(
+      moduleTestInterface: KClass<T>,
+      module: Module,
+      customAppContext: AppContext? = null,
+      customEventEmitter: EventEmitter? = null,
+    ): Triple<T, AppContext, EventEmitter> {
+      val context = ReactApplicationContext(ApplicationProvider.getApplicationContext())
+      val appContext = customAppContext ?: AppContext(
         modulesProvider = mockk(relaxed = true),
         legacyModuleRegistry = mockk(relaxed = true),
         reactContextHolder = WeakReference(context)
       )
-    )
 
-    // NOTE (barthap): This initialization is often called in @Before method
-    // and Context sometimes isn't initialized yet. This workaround
-    // loads current context every time it is accessed
-    // TODO: Find a better solution for this
-    every { appContext getProperty "reactContext" } answers {
-      ReactApplicationContext(ApplicationProvider.getApplicationContext())
-    }
+      val eventEmitter: EventEmitter = customEventEmitter ?: mockk(relaxed = true)
 
-    val moduleSpy = spyk<Module>(module)
-    every { moduleSpy getProperty "appContext" } returns appContext
-    every { moduleSpy.sendEvent(any(), any()) } answers { call ->
-      val (eventName, eventBody) = call.invocation.args
-      eventEmitter.emit(eventName as String, eventBody as? Bundle)
-    }
-    moduleHolder = ModuleHolder(moduleSpy)
-
-    if (autoOnCreate) {
-      callOnCreate()
-    }
-  }
-
-  fun callOnCreate() {
-    moduleHolder.post(EventName.MODULE_CREATE)
-  }
-
-  fun activityGoesBackground() {
-    moduleHolder.post(EventName.ACTIVITY_ENTERS_BACKGROUND)
-  }
-
-  fun activityGoesForeground() {
-    moduleHolder.post(EventName.ACTIVITY_ENTERS_FOREGROUND)
-  }
-
-  fun callFunction(name: String, vararg args: Any?): PromiseMock {
-    val promise = PromiseMock()
-
-    val arguments = convertArgs(args.asIterable())
-    moduleHolder.call(name, arguments, promise)
-
-    return promise
-  }
-
-  // TODO: Maybe use some existing type conversion, add more sophisticated conversion method
-  private fun convertArgs(args: Iterable<Any?>): ReadableArray {
-    val argsList = JavaOnlyArray()
-
-    for (arg in args) {
-      when (arg) {
-        null -> argsList.pushNull()
-        is Int -> argsList.pushInt(arg)
-        is Number -> argsList.pushDouble(arg.toDouble())
-        is Boolean -> argsList.pushBoolean(arg)
-        is String -> argsList.pushString(arg)
-        is Record -> argsList.pushMap(arg.toJSMap())
-        is Iterable<*> -> argsList.pushArray(convertArgs(arg))
-        else -> throw IllegalArgumentException("Unsupported argument type ${arg.javaClass} in ModuleMock")
+      val moduleSpy = spyk(module)
+      every { moduleSpy getProperty "appContext" } returns appContext
+      every { moduleSpy.sendEvent(any(), any()) } answers { call ->
+        val (eventName, eventBody) = call.invocation.args
+        eventEmitter.emit(eventName as String, eventBody as? Bundle)
       }
+
+      val holder = ModuleHolder(moduleSpy)
+      val moduleControllerImpl = ModuleControllerImpl(holder)
+
+      val invocationHandler = ModuleMockInvocationHandler(
+        moduleTestInterface,
+        moduleControllerImpl,
+        holder
+      )
+      @Suppress("UNCHECKED_CAST")
+      return Triple(
+        Proxy
+          .newProxyInstance(
+            moduleTestInterface.java.classLoader,
+            arrayOf(moduleTestInterface.java, ModuleController::class.java),
+            invocationHandler
+          ) as T,
+        appContext,
+        eventEmitter
+      )
     }
 
-    return argsList
+    inline fun <T : Any> createMock(
+      moduleTestInterface: KClass<T>,
+      module: Module,
+      autoOnCreate: Boolean = true,
+      customAppContext: AppContext? = null,
+      customEventEmitter: EventEmitter? = null,
+      block: ModuleMockHolder<T>.() -> Unit
+    ) {
+      val (mock, appContext, eventEmitter) = createMock(
+        moduleTestInterface,
+        module,
+        customAppContext,
+        customEventEmitter
+      )
+      val controller = mock as ModuleController
+      val holder = ModuleMockHolder<T>(mock, controller, appContext, eventEmitter)
+
+      if (autoOnCreate) {
+        controller.onCreate()
+      }
+
+      block.invoke(holder)
+    }
   }
 }
