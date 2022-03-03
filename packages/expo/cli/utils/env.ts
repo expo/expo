2326aca9c15926272bc15c3052acf79d47a007c4
Evan Bacon <bacon@expo.io>
2022-03-03 15:33:07 -0700

    Update env.ts

--- packages/expo/cli/utils/env.ts
+++ packages/expo/cli/utils/env.ts
@@ -30,9 +30,6 @@ export const EXPO_NO_TYPESCRIPT_SETUP = () => boolish('EXPO_NO_TYPESCRIPT_SETUP'
 /** Disable telemetry (analytics) */
 export const EXPO_NO_TELEMETRY = boolish('EXPO_NO_TELEMETRY', false);
 
-/** Is running in non-interactive CI mode */
-export const LOCAL_XDL_SCHEMA = boolish('LOCAL_XDL_SCHEMA', false);
-
 /** local directory to the universe repo for testing locally */
 export const EXPO_UNIVERSE_DIR = string('EXPO_UNIVERSE_DIR', '');
 
