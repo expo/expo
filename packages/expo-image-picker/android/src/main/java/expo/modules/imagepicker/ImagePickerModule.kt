3a55d2a7cd03e4fac22b3be9f6d2480deee6482f
John Park <horaeng0.0@gmail.com>
2022-03-04 19:44:04 -0600

    Update ImagePickerModule.kt
    Match `getMediaLibraryPermissions` to [getManifestPermissions](https://github.com/expo/expo/blob/master/packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/MediaLibraryModule.kt#L342-L348)
    
    in other words, making `MediaLibrary.requestPermissionsAsync()` and `ImagePicker.requestMediaLibraryPermissionsAsync()` identical on Android because it is already on iOS.
    
    Ref: https://github.com/expo/expo/issues/11504#issuecomment-844555347

--- packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/ImagePickerModule.kt
+++ packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/ImagePickerModule.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
@@ -179,11 +180,11 @@ class ImagePickerModule(
   //region helpers
 
   private fun getMediaLibraryPermissions(writeOnly: Boolean): Array<String> {
-    return if (writeOnly) {
-      arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    } else {
-      arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
-    }
+    return listOfNotNull(
+      Manifest.permission.WRITE_EXTERNAL_STORAGE,
+      Manifest.permission.READ_EXTERNAL_STORAGE.takeIf { !writeOnly },
+      Manifest.permission.ACCESS_MEDIA_LOCATION.takeIf { Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q }
+    ).toTypedArray()
   }
 
   private fun launchCameraWithPermissionsGranted(promise: Promise, cameraIntent: Intent, pickerOptions: ImagePickerOptions) {
