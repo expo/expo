71ab4b729259a164c9162265d4f4db7a71ed378c
Juwan Wheatley <datwheat@gmail.com>
2022-03-07 17:58:04 -0500

    add spacers to recently opened project list

--- home/screens/HomeScreen/RecentlyOpenedSection.tsx
+++ home/screens/HomeScreen/RecentlyOpenedSection.tsx
@@ -1,3 +1,4 @@
+import { Spacer } from 'expo-dev-client-components';
 import React from 'react';
 import { Linking } from 'react-native';
 
@@ -11,28 +12,31 @@ type Props = {
 export function RecentlyOpenedSection({ recentHistory }: Props) {
   return (
     <>
-      {recentHistory.map((project) => {
+      {recentHistory.map((project, i) => {
         if (!project) return null;
 
         return (
-          <RecentlyOpenedListItem
-            key={project.manifestUrl}
-            url={project.manifestUrl}
-            image={
-              // TODO(wschurman): audit for new manifests
-              project.manifest && 'iconUrl' in project.manifest
-                ? project.manifest.iconUrl
-                : undefined
-            }
-            title={
-              // TODO(wschurman): audit for new manifests
-              project.manifest && 'name' in project.manifest ? project.manifest.name : undefined
-            }
-            onPress={() => {
-              // TODO(fiberjw): navigate to the project details screen
-              Linking.openURL(project.url);
-            }}
-          />
+          <>
+            <RecentlyOpenedListItem
+              key={project.manifestUrl}
+              url={project.manifestUrl}
+              image={
+                // TODO(wschurman): audit for new manifests
+                project.manifest && 'iconUrl' in project.manifest
+                  ? project.manifest.iconUrl
+                  : undefined
+              }
+              title={
+                // TODO(wschurman): audit for new manifests
+                project.manifest && 'name' in project.manifest ? project.manifest.name : undefined
+              }
+              onPress={() => {
+                // TODO(fiberjw): navigate to the project details screen
+                Linking.openURL(project.url);
+              }}
+            />
+            {i < recentHistory.count() - 1 && <Spacer.Vertical size="small" />}
+          </>
         );
       })}
     </>
