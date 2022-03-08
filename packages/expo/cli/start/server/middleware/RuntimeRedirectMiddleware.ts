734fae36c4614043d409ee4971824ec2d234fd1c
Evan Bacon <bacon@expo.io>
2022-03-08 12:12:42 -0700

    Added interstitial middleware

---
+++ packages/expo/cli/start/server/middleware/RuntimeRedirectMiddleware.ts
@@ -0,0 +1,45 @@
+import { parse } from 'url';
+
+import * as Log from '../../../log';
+import { disableResponseCache, ExpoMiddleware } from './ExpoMiddleware';
+import { assertRuntimePlatform, parsePlatformHeader, RuntimePlatform } from './resolvePlatform';
+import { ServerRequest, ServerResponse } from './server.types';
+
+/** Runtime to target: expo = Expo Go, custom = Dev Client. */
+type RuntimeTarget = 'expo' | 'custom';
+
+export class RuntimeRedirectMiddleware extends ExpoMiddleware {
+  constructor(
+    protected projectRoot: string,
+    protected options: {
+      onDeepLink: (props: {
+        runtime: RuntimeTarget;
+        platform: RuntimePlatform;
+      }) => void | Promise<void>;
+      getLocation: (props: { runtime: RuntimeTarget }) => string;
+    }
+  ) {
+    super(projectRoot, ['/_expo/link']);
+  }
+
+  async handleRequestAsync(req: ServerRequest, res: ServerResponse): Promise<void> {
+    const { query } = parse(req.url!, true);
+    const isDevClient = query['choice'] === 'expo-dev-client';
+    const platform = parsePlatformHeader(req);
+    assertRuntimePlatform(platform);
+    const runtime = isDevClient ? 'custom' : 'expo';
+
+    const redirect = this.options.getLocation({ runtime });
+    Log.debug('Redirect ->', redirect);
+    res.setHeader('Location', redirect);
+
+    this.options.onDeepLink({ runtime, platform });
+
+    // Disable caching
+    disableResponseCache(res);
+
+    // 'Temporary Redirect'
+    res.statusCode = 307;
+    res.end();
+  }
+}
---
+++ packages/expo/cli/start/server/middleware/__tests__/InterstitialPageMiddleware-test.ts
@@ -0,0 +1,123 @@
+import { getConfig, getNameFromConfig } from '@expo/config';
+import { getRuntimeVersionNullable } from '@expo/config-plugins/build/utils/Updates';
+import { vol } from 'memfs';
+
+import { InterstitialPageMiddleware } from '../InterstitialPageMiddleware';
+import { ServerRequest, ServerResponse } from '../server.types';
+
+jest.mock('@expo/config', () => ({
+  getProjectConfigDescriptionWithPaths: jest.fn(),
+  getConfig: jest.fn(() => ({
+    pkg: {},
+    exp: {
+      sdkVersion: '45.0.0',
+      name: 'my-app',
+      slug: 'my-app',
+    },
+  })),
+  getNameFromConfig: jest.fn(() => 'my-app'),
+}));
+jest.mock('@expo/config-plugins/build/utils/Updates', () => ({
+  getRuntimeVersionNullable: jest.fn(),
+}));
+
+const asReq = (req: Partial<ServerRequest>) => req as ServerRequest;
+
+const asMock = <T extends (...args: any[]) => any>(fn: T): jest.MockedFunction<T> =>
+  fn as jest.MockedFunction<T>;
+
+const originalCwd = process.cwd();
+
+beforeAll(() => {
+  process.chdir('/');
+});
+
+beforeEach(() => {
+  vol.reset();
+});
+
+afterAll(() => {
+  process.chdir(originalCwd);
+});
+
+describe('_shouldContinue', () => {
+  const middleware = new InterstitialPageMiddleware('/');
+  it(`returns false when the middleware should not handle`, () => {
+    for (const req of [
+      asReq({}),
+      asReq({ url: 'http://localhost:19000' }),
+      asReq({ url: 'http://localhost:19000/' }),
+    ]) {
+      expect(middleware._shouldContinue(req)).toBe(false);
+    }
+  });
+  it(`returns true when the middleware should handle`, () => {
+    for (const req of [asReq({ url: 'http://localhost:19000/_expo/loading' })]) {
+      expect(middleware._shouldContinue(req)).toBe(true);
+    }
+  });
+});
+
+describe('_getProjectOptions', () => {
+  it('returns the project settings from the config', async () => {
+    asMock(getNameFromConfig).mockReturnValueOnce({ appName: 'my-app' });
+    asMock(getRuntimeVersionNullable).mockReturnValueOnce('123');
+
+    const middleware = new InterstitialPageMiddleware('/');
+
+    expect(middleware._getProjectOptions('ios')).toEqual({
+      runtimeVersion: '123',
+      appName: 'my-app',
+    });
+    expect(getConfig).toBeCalled();
+    expect(getRuntimeVersionNullable).toBeCalledWith(
+      { name: 'my-app', sdkVersion: '45.0.0', slug: 'my-app' },
+      'ios'
+    );
+  });
+});
+
+describe('_getPageAsync', () => {
+  it('returns the static HTML with templates filled in', async () => {
+    const projectRoot = '/';
+    vol.fromJSON(
+      {
+        'node_modules/expo/static/loading-page/index.html':
+          'AppName: "{{ AppName }}", RuntimeVersion "{{ RuntimeVersion }}", Path: {{ Path }}',
+      },
+      projectRoot
+    );
+
+    const middleware = new InterstitialPageMiddleware(projectRoot);
+    await expect(
+      middleware._getPageAsync({
+        appName: 'App',
+        runtimeVersion: '123',
+      })
+    ).resolves.toEqual('AppName: "App", RuntimeVersion "123", Path: /');
+  });
+});
+
+describe('handleRequestAsync', () => {
+  it('returns the interstitial page', async () => {
+    const middleware = new InterstitialPageMiddleware('/');
+
+    middleware._getProjectOptions = jest.fn(() => ({
+      runtimeVersion: '123',
+      appName: 'App',
+    }));
+
+    middleware._getPageAsync = jest.fn(async () => 'mock-value');
+
+    const response = {
+      setHeader: jest.fn(),
+      end: jest.fn(),
+      statusCode: 200,
+    } as unknown as ServerResponse;
+
+    await middleware.handleRequestAsync(asReq({}), response);
+    expect(response.statusCode).toBe(200);
+    expect(response.end).toBeCalledWith('mock-value');
+    expect(response.setHeader).toBeCalledTimes(4);
+  });
+});
