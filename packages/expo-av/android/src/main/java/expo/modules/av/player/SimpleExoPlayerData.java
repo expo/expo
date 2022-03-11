99ad2fd4d28ffe149bbeceb782204de507bb8c4c
Artiya Thinkumpang <artiya4u@gmail.com>
2022-03-11 18:16:13 +0700

    [av][Android] Bump `com.google.android.exoplayer:exoplayer` from `2.9.2` to `2.13.3` (#16123)

--- packages/expo-av/android/src/main/java/expo/modules/av/player/SimpleExoPlayerData.java
+++ packages/expo-av/android/src/main/java/expo/modules/av/player/SimpleExoPlayerData.java
@@ -83,8 +83,8 @@ class SimpleExoPlayerData extends PlayerData
     mLoadCompletionListener = loadCompletionListener;
 
     final Context context = mAVModule.getContext();
-    final BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter.Builder(context).build();
-    final TrackSelector trackSelector = new DefaultTrackSelector(context, new AdaptiveTrackSelection.Factory());
+    final DefaultBandwidthMeter.Builder(context).setInitialBitrateEstimate(C.NETWORK_TYPE_UNKNOWN, 1_500_000).build();
+    final TrackSelector trackSelector = new DefaultTrackSelector(context, new AdaptiveTrackSelection.Factory(500, 2_000, 25_000, 0.6f, 0.75f, 500));
 
     // Create the player
     mSimpleExoPlayer = new SimpleExoPlayer.Builder(context)
