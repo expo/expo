640beec3322ce78deb06bba925359f0ab105fb80
Eric Samelson <eric@expo.io>
2022-03-14 16:25:25 -0700

    add retry logic to app

--- packages/expo-updates/.tests/e2e/__tests__/fixtures/App.js
+++ packages/expo-updates/.tests/e2e/__tests__/fixtures/App.js
@@ -1,8 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const RETRY_COUNT = 5;
+
 export default function App() {
-  fetch('http://10.0.2.2:UPDATES_PORT/notify/test');
+  useEffect(async () => {
+    for (let i = 0; i < RETRY_COUNT; i++) {
+      const response = await fetch('http://10.0.2.2:UPDATES_PORT/notify/test');
+      if (response.status === 200) {
+        break;
+      }
+      // wait 50 ms and then try again
+      await new Promise((resolve) => setTimeout(resolve, 50));
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
