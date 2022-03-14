390d5b54db70a8f0c0106dc10a8d695fd58a1194
Łukasz Kosmaty <kosmatylukasz@gmail.com>
2022-03-14 15:24:29 +0100

    Fix CI

--- packages/unimodules-test-core/android/src/main/java/expo/modules/test/core/TestJSContainerProvider.kt
+++ packages/unimodules-test-core/android/src/main/java/expo/modules/test/core/TestJSContainerProvider.kt
@@ -6,7 +6,7 @@ import com.facebook.react.bridge.WritableArray
 import com.facebook.react.bridge.WritableMap
 import expo.modules.kotlin.types.JSTypeConverter
 
-object TestJSContainerProvider: JSTypeConverter.ContainerProvider {
+object TestJSContainerProvider : JSTypeConverter.ContainerProvider {
   override fun createMap(): WritableMap = JavaOnlyMap()
   override fun createArray(): WritableArray = JavaOnlyArray()
 }
