4cfe096222dc6061998092ab85c952b2271e7b3b
Łukasz Kosmaty <kosmatylukasz@gmail.com>
2022-03-15 12:48:50 +0100

    Fix CI

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/events/EventEmitter.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/events/EventEmitter.kt
@@ -3,7 +3,6 @@ package expo.modules.kotlin.events
 import com.facebook.react.bridge.WritableMap
 import expo.modules.kotlin.records.Record
 
-
 // We want to decorate a legacy event emitter interface to support advanced conversion between types in events.
 // For instance, users will be able to create `Callback<Record>` that will be converted to the `WritableMap`.
 interface EventEmitter : expo.modules.core.interfaces.services.EventEmitter {
