15e26df8acba1943d664737d6d9c8947ecb6216b
Evan Bacon <bacon@expo.io>
2022-03-16 10:27:43 -0500

    [cli] Added Metro dev server module (#16631)
    * Added Metro dev server module
    
    * Update packages/expo/CHANGELOG.md
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>
    
    * Update DevServerManager.ts
    
    * remove unused optionals
    
    * fix tests
    
    * Update MetroBundlerDevServer.ts
    
    * Update packages/expo/cli/start/server/BundlerDevServer.ts
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>
    
    * Update packages/expo/cli/start/server/BundlerDevServer.ts
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>
    
    * Update packages/expo/cli/start/server/BundlerDevServer.ts
    
    Co-authored-by: James Ide <ide@users.noreply.github.com>
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    Co-authored-by: James Ide <ide@users.noreply.github.com>

---
+++ packages/expo/cli/start/server/metro/__tests__/MetroBundlerDevServer-test.ts
@@ -0,0 +1,52 @@
+import { vol } from 'memfs';
+
+import { BundlerStartOptions } from '../../BundlerDevServer';
+import { MetroBundlerDevServer } from '../MetroBundlerDevServer';
+import { instantiateMetroAsync } from '../instantiateMetro';
+
+jest.mock('../instantiateMetro', () => ({
+  instantiateMetroAsync: jest.fn(async () => ({
+    middleware: { use: jest.fn() },
+    server: { listen: jest.fn(), close: jest.fn() },
+  })),
+}));
+jest.mock('../../../../log');
+
+beforeEach(() => {
+  vol.reset();
+});
+
+async function getStartedDevServer(options: Partial<BundlerStartOptions> = {}) {
+  const devServer = new MetroBundlerDevServer('/', false);
+  devServer['getAvailablePortAsync'] = jest.fn(() => Promise.resolve(3000));
+  // Tested in the superclass
+  devServer['postStartAsync'] = jest.fn(async () => {});
+  await devServer.startAsync({ location: {}, ...options });
+  return devServer;
+}
+
+describe('startAsync', () => {
+  it(`starts metro`, async () => {
+    const devServer = await getStartedDevServer();
+
+    expect(devServer['postStartAsync']).toHaveBeenCalled();
+
+    expect(devServer.getInstance()).toEqual({
+      location: {
+        host: 'localhost',
+        port: 19000,
+        protocol: 'http',
+        url: 'http://localhost:19000',
+      },
+      middleware: {
+        use: expect.any(Function),
+      },
+      server: {
+        close: expect.any(Function),
+        listen: expect.any(Function),
+      },
+    });
+
+    expect(instantiateMetroAsync).toHaveBeenCalled();
+  });
+});
