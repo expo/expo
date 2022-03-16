eeb38541b500a55817d8885014622d25c72a6238
Juwan Wheatley <datwheat@gmail.com>
2022-03-16 16:08:51 -0400

    [home] add border to pressable diagnostic options (#16597)

--- home/screens/RedesignedDiagnosticsScreen/DiagnosticsButton.tsx
+++ home/screens/RedesignedDiagnosticsScreen/DiagnosticsButton.tsx
@@ -1,13 +1,6 @@
 import { borderRadius } from '@expo/styleguide-native';
 import { PressableOpacity } from 'components/PressableOpacity';
-import {
-  ChevronRightIcon,
-  Row,
-  Spacer,
-  Text,
-  useExpoTheme,
-  View,
-} from 'expo-dev-client-components';
+import { ChevronRightIcon, Row, Spacer, Text, useExpoTheme } from 'expo-dev-client-components';
 import * as React from 'react';
 import { Platform } from 'react-native';
 
@@ -21,24 +14,25 @@ export function DiagnosticButton({ title, description, onPress }: Props) {
   const theme = useExpoTheme();
 
   return (
-    <PressableOpacity borderRadius={borderRadius.large} onPress={onPress}>
-      <View bg="default" padding="medium">
-        <Row justify="between" align="center">
-          <Text
-            style={{
-              fontSize: 14,
-              lineHeight: 14 * 1.5,
-              fontWeight: Platform.OS === 'android' ? 'bold' : '600',
-            }}>
-            {title}
-          </Text>
-          <ChevronRightIcon size="small" style={{ tintColor: theme.icon.secondary }} />
-        </Row>
-        <Spacer.Vertical size="tiny" />
-        <Text style={{ fontSize: 14, lineHeight: 14 * 1.5 }} color="secondary" size="small">
-          {description}
+    <PressableOpacity
+      borderRadius={borderRadius.large}
+      onPress={onPress}
+      containerProps={{ border: 'hairline', bg: 'default', padding: 'medium' }}>
+      <Row justify="between" align="center">
+        <Text
+          style={{
+            fontSize: 14,
+            lineHeight: 14 * 1.5,
+            fontWeight: Platform.OS === 'android' ? 'bold' : '600',
+          }}>
+          {title}
         </Text>
-      </View>
+        <ChevronRightIcon size="small" style={{ tintColor: theme.icon.secondary }} />
+      </Row>
+      <Spacer.Vertical size="tiny" />
+      <Text style={{ fontSize: 14, lineHeight: 14 * 1.5 }} color="secondary" size="small">
+        {description}
+      </Text>
     </PressableOpacity>
   );
 }
