188ce14366364934e66d4a20c1b7b461e1d883f4
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-03-16 14:47:08 +0100

    [sweet api][Kotlin] Support non-bundle payload in view events (#16636)
    # Why
    
    So far we only supported callbacks where the payload was the Bundle object. In this PR, I introduced support for all types which are convertible to simple types or `WritableMap`.  
    
    # How
    
    - Decorated `expo.modules.core.interfaces.services.EventEmitter` to add support for more payload type. 
    - Converted an arg in `ViewCallback` to `WritableMap` (if a simple type was provided, it will be wrapped in the WritableMap and stored under the `payload` key). 
    
    # Test Plan
    
    - simple module with multiple payload types like:
      - String 
      - Record
      - WritableMap
      - List<String>

---
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/events/EventEmitter.kt
@@ -0,0 +1,13 @@
+package expo.modules.kotlin.events
+
+import com.facebook.react.bridge.WritableMap
+import expo.modules.kotlin.records.Record
+
+// We want to decorate a legacy event emitter interface to support advanced conversion between types in events.
+// For instance, users will be able to create `Callback<Record>` that will be converted to the `WritableMap`.
+interface EventEmitter : expo.modules.core.interfaces.services.EventEmitter {
+  fun emit(eventName: String, eventBody: WritableMap?)
+  fun emit(eventName: String, eventBody: Record?)
+  fun emit(eventName: String, eventBody: Map<*, *>?)
+  fun emit(viewId: Int, eventName: String, eventBody: WritableMap?)
+}
