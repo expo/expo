8671fec220ac1cef12b5332f5d59192d6a45ea94
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-03-17 16:17:32 +0100

    Drop SDK42 on iOS

--- ios/versioned-react-native/ABI42_0_0/Expo/EXAdsAdMob/ABI42_0_0EXAdsAdMob/ABI42_0_0EXAdsAdMobBannerViewManager.h
+++
@@ -1,6 +0,0 @@
-#import <AVFoundation/AVFoundation.h>
-#import <ABI42_0_0UMCore/ABI42_0_0UMViewManager.h>
-
-@interface ABI42_0_0EXAdsAdMobBannerViewManager : ABI42_0_0UMViewManager
-
-@end
--- ios/versioned-react-native/ABI42_0_0/Expo/EXBarCodeScanner/ABI42_0_0EXBarCodeScanner/ABI42_0_0EXBarCodeScannerProvider.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-present 650 Industries. All rights reserved.
-
-#import <Foundation/Foundation.h>
-#import <ABI42_0_0UMCore/ABI42_0_0UMInternalModule.h>
-#import <ABI42_0_0ExpoModulesCore/ABI42_0_0EXBarCodeScannerProviderInterface.h>
-
-@interface ABI42_0_0EXBarCodeScannerProvider : NSObject <ABI42_0_0UMInternalModule, ABI42_0_0EXBarCodeScannerProviderInterface>
-
-@end
