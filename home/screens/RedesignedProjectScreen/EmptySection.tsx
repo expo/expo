d181421fb6e3af53502adfe68f54b6376e0471c3
Juwan Wheatley <datwheat@gmail.com>
2022-03-19 17:57:56 -0400

    redesign empty section

--- home/screens/RedesignedProjectScreen/EmptySection.tsx
+++ home/screens/RedesignedProjectScreen/EmptySection.tsx
@@ -1,22 +1,39 @@
+import { spacing } from '@expo/styleguide-native';
+import { PressableOpacity } from 'components/PressableOpacity';
 import dedent from 'dedent';
-import { Text } from 'expo-dev-client-components';
+import { useExpoTheme, Text, Spacer, View } from 'expo-dev-client-components';
+import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 
 const NO_PUBLISHES_TEXT = dedent`
-  This project has not yet been published.
+This project has not yet been published.
 `;
 
 export function EmptySection() {
+  const theme = useExpoTheme();
+
   return (
-    <Text
-      align="center"
-      style={[
-        {
-          marginVertical: 16,
-        },
-      ]}
-      type="InterRegular">
-      {NO_PUBLISHES_TEXT}
-    </Text>
+    <View bg="default" border="hairline" rounded="medium" padding="medium" margin="medium">
+      <Text color="warning" type="InterRegular">
+        {NO_PUBLISHES_TEXT}
+      </Text>
+      <Spacer.Vertical size="medium" />
+      <PressableOpacity
+        onPress={() => {
+          WebBrowser.openBrowserAsync('https://docs.expo.dev/workflow/publishing/');
+        }}
+        containerProps={{
+          style: {
+            padding: spacing[2],
+            alignSelf: 'flex-start',
+            backgroundColor: theme.button.tertiary.background,
+          },
+          rounded: 'small',
+        }}>
+        <Text type="InterSemiBold" style={{ color: theme.button.tertiary.foreground }} size="small">
+          Learn more
+        </Text>
+      </PressableOpacity>
+    </View>
   );
 }
