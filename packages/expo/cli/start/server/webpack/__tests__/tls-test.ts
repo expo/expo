e75cf86a7563bd9e65f56e2ad3b6ac39370dee61
evanbacon <bacon@expo.io>
2022-03-21 10:56:55 -0500

    Update tls-test.ts

--- packages/expo/cli/start/server/webpack/__tests__/tls-test.ts
+++ packages/expo/cli/start/server/webpack/__tests__/tls-test.ts
@@ -2,7 +2,7 @@ import { certificateFor } from '@expo/devcert';
 import { vol } from 'memfs';
 
 import * as Log from '../../../../log';
-import { ensureEnvironmentSupportsSSLAsync, getSSLCertAsync } from '../ssl';
+import { ensureEnvironmentSupportsTLSAsync, getTLSCertAsync } from '../tls';
 
 jest.mock('../../../../log');
 
@@ -17,7 +17,7 @@ beforeEach(() => {
   delete process.env.SSL_KEY_FILE;
 });
 
-describe(ensureEnvironmentSupportsSSLAsync, () => {
+describe(ensureEnvironmentSupportsTLSAsync, () => {
   beforeEach(() => {
     delete process.env.SSL_CRT_FILE;
     delete process.env.SSL_KEY_FILE;
@@ -25,37 +25,37 @@ describe(ensureEnvironmentSupportsSSLAsync, () => {
   afterAll(() => {
     process.env = originalEnv;
   });
-  it(`skips SSL if the environment variables are already configured`, async () => {
+  it(`skips TLS if the environment variables are already configured`, async () => {
     process.env.SSL_CRT_FILE = 'foo';
     process.env.SSL_KEY_FILE = 'bar';
-    await ensureEnvironmentSupportsSSLAsync('/');
+    await ensureEnvironmentSupportsTLSAsync('/');
     expect(certificateFor).toBeCalledTimes(0);
   });
 
-  it(`generates SSL if the environment variables are not set`, async () => {
-    await ensureEnvironmentSupportsSSLAsync('/');
+  it(`generates TLS if the environment variables are not set`, async () => {
+    await ensureEnvironmentSupportsTLSAsync('/');
 
     expect(certificateFor).toBeCalledTimes(1);
-    expect(process.env.SSL_CRT_FILE).toBe('/.expo/ssl/cert-localhost.pem');
-    expect(process.env.SSL_KEY_FILE).toBe('/.expo/ssl/key-localhost.pem');
+    expect(process.env.SSL_CRT_FILE).toBe('/.expo/tls/cert-localhost.pem');
+    expect(process.env.SSL_KEY_FILE).toBe('/.expo/tls/key-localhost.pem');
   });
 });
 
-describe(getSSLCertAsync, () => {
-  it(`creates an SSL cert for the computer`, async () => {
+describe(getTLSCertAsync, () => {
+  it(`creates an TLS cert for the computer`, async () => {
     asMock(Log.log).mockReset();
-    await expect(getSSLCertAsync('/')).resolves.toEqual({
-      certPath: '/.expo/ssl/cert-localhost.pem',
-      keyPath: '/.expo/ssl/key-localhost.pem',
+    await expect(getTLSCertAsync('/')).resolves.toEqual({
+      certPath: '/.expo/tls/cert-localhost.pem',
+      keyPath: '/.expo/tls/key-localhost.pem',
     });
 
     expect(vol.toJSON()).toEqual({
-      '/.expo/ssl/cert-localhost.pem': 'cert',
-      '/.expo/ssl/key-localhost.pem': 'key',
+      '/.expo/tls/cert-localhost.pem': 'cert',
+      '/.expo/tls/key-localhost.pem': 'key',
       '/.expo/README.md': expect.any(String),
     });
     expect(Log.log).toHaveBeenLastCalledWith(
-      expect.stringContaining('Creating SSL certificate for localhost')
+      expect.stringContaining('Creating TLS certificate for localhost')
     );
   });
 });
