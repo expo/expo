8d307f52e5bea7085149fb5e1d98fc2b2b90e4b9
Evan Bacon <bacon@expo.io>
2022-03-23 11:50:13 -0500

    chore: refactor `expo/cli` to `@expo/cli` package (#16717)
    * chore: refactor `expo/cli` to `@expo/cli` package
    
    * Update CODEOWNERS
    
    * fix linking
    
    * fix lint script
    
    * fix build file
    
    * fix e2e tests
    
    * lint before build
    
    * use mock version

--- packages/@expo/cli/e2e/__tests__/logout-test.ts
+++ packages/@expo/cli/e2e/__tests__/logout-test.ts
@@ -14,18 +14,18 @@ afterAll(() => {
 });
 
 it('loads expected modules by default', async () => {
-  const modules = await getLoadedModulesAsync(`require('../../build-cli/cli/logout');`);
+  const modules = await getLoadedModulesAsync(`require('../../build/src/logout');`);
   expect(modules).toStrictEqual([
-    'node_modules/ansi-styles/index.js',
-    'node_modules/arg/index.js',
-    'node_modules/chalk/source/index.js',
-    'node_modules/chalk/source/util.js',
-    'node_modules/has-flag/index.js',
-    'node_modules/supports-color/index.js',
-    'packages/expo/build-cli/cli/log.js',
-    'packages/expo/build-cli/cli/logout/index.js',
-    'packages/expo/build-cli/cli/utils/args.js',
-    'packages/expo/build-cli/cli/utils/errors.js',
+    '../node_modules/ansi-styles/index.js',
+    '../node_modules/arg/index.js',
+    '../node_modules/chalk/source/index.js',
+    '../node_modules/chalk/source/util.js',
+    '../node_modules/has-flag/index.js',
+    '../node_modules/supports-color/index.js',
+    '@expo/cli/build/src/log.js',
+    '@expo/cli/build/src/logout/index.js',
+    '@expo/cli/build/src/utils/args.js',
+    '@expo/cli/build/src/utils/errors.js',
   ]);
 });
 
