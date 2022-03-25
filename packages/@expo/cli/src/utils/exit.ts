48104a0ca1ae771b61ea218cdeb180d0e3621d37
evanbacon <bacon@expo.io>
2022-03-25 12:49:20 -0500

    fix: use queue to prevent process event listener warning

--- packages/@expo/cli/src/utils/exit.ts
+++ packages/@expo/cli/src/utils/exit.ts
@@ -1,17 +1,36 @@
+import * as Log from '../log';
+import { guardAsync } from './fn';
+
+type AsyncExitHook = (signal: NodeJS.Signals) => void | Promise<void>;
+
+// We create a queue since Node.js throws an error if we try to append too many listeners:
+// (node:4405) MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 SIGINT listeners added to [process]. Use emitter.setMaxListeners() to increase limit
+const queue: AsyncExitHook[] = [];
+
 /** Add functions that run before the process exits. Returns a function for removing the listeners. */
-export function installExitHooks(listener: NodeJS.SignalsListener): () => void {
-  const killSignals: ['SIGINT', 'SIGTERM'] = ['SIGINT', 'SIGTERM'];
-  for (const signal of killSignals) {
-    process.on(signal, listener);
-  }
-  return () => {
-    if (!listener) {
-      return;
+export function installExitHooks(hook: AsyncExitHook): void {
+  queue.push(hook);
+}
+
+const preExitHook = (signal: NodeJS.Signals) =>
+  guardAsync(async () => {
+    Log.debug(`pre-exit (signal: ${signal}, queue length: ${queue.length})`);
+
+    for (const [index, hookAsync] of queue.entries()) {
+      try {
+        await hookAsync(signal);
+      } catch (error: any) {
+        Log.debug(`Error in exit hook: %O (queue: ${index})`, error);
+      }
     }
-    for (const signal of killSignals) {
-      process.off(signal, listener);
-    }
-    // Allow the listener to be GC'd
-    listener = null;
-  };
+
+    Log.debug(`post-exit (code: ${process.exitCode ?? 0})`);
+
+    process.exit();
+  });
+
+const PRE_EXIT_SIGNALS: NodeJS.Signals[] = ['SIGHUP', 'SIGINT', 'SIGTERM', 'SIGBREAK'];
+
+for (const signal of PRE_EXIT_SIGNALS) {
+  process.on(signal, preExitHook(signal));
 }
