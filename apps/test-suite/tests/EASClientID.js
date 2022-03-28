ebf8011293a5f5e704113f9fd65077f3311ba936
Will Schurman <wschurman@gmail.com>
2022-03-28 12:31:41 -0700

    [eas-client-id] Move back to constants (#16795)

--- apps/test-suite/tests/EASClientID.js
+++ apps/test-suite/tests/EASClientID.js
@@ -7,9 +7,9 @@ export const name = 'EASClientID';
 export function test(t) {
   t.describe('EASClientID', () => {
     t.it('gets the EAS client ID', async () => {
-      const clientId = await EASClientID.getClientIDAsync();
+      const clientId = EASClientID.clientID;
       t.expect(clientId).toBeTruthy();
-      const clientId2 = await EASClientID.getClientIDAsync();
+      const clientId2 = EASClientID.clientID;
       t.expect(clientId).toEqual(clientId2);
     });
   });
