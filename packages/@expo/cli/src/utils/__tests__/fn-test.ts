88b2de8efbe0e08fc16be98f0f11a69004aafdf5
Evan Bacon <bacon@expo.io>
2022-03-29 10:54:37 -0500

    fix(cli): use queue to prevent process event listener warning (#16753)
    * fix: use queue to prevent process event listener warning
    
    * Added full clean up
    
    * Update packages/@expo/cli/CHANGELOG.md
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    
    * Update exit.ts
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/cli/src/utils/__tests__/fn-test.ts
+++ packages/@expo/cli/src/utils/__tests__/fn-test.ts
@@ -1,4 +1,4 @@
-import { memoize } from '../fn';
+import { memoize, guardAsync } from '../fn';
 
 describe(memoize, () => {
   it(`memoizes`, () => {
@@ -7,6 +7,17 @@ describe(memoize, () => {
     expect(memoized()).toEqual('d');
     expect(memoized()).toEqual('d');
     expect(memoized()).toEqual('d');
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe(guardAsync, () => {
+  it(`guards async function`, async () => {
+    const fn = jest.fn(async () => 'd');
+    const guard = guardAsync(fn);
+    expect(await guard()).toEqual('d');
+    expect(await guard()).toEqual('d');
+    expect(await guard()).toEqual('d');
     expect(fn).toHaveBeenCalledTimes(1);
   });
 });
--- packages/@expo/cli/src/utils/exit.ts
+++ packages/@expo/cli/src/utils/exit.ts
@@ -1,17 +1,67 @@
+import * as Log from '../log';
+import { guardAsync } from './fn';
+
+type AsyncExitHook = (signal: NodeJS.Signals) => void | Promise<void>;
+
+const PRE_EXIT_SIGNALS: NodeJS.Signals[] = ['SIGHUP', 'SIGINT', 'SIGTERM', 'SIGBREAK'];
+
+// We create a queue since Node.js throws an error if we try to append too many listeners:
+// (node:4405) MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 SIGINT listeners added to [process]. Use emitter.setMaxListeners() to increase limit
+const queue: AsyncExitHook[] = [];
+
+let unsubscribe: (() => void) | null = null;
+
 /** Add functions that run before the process exits. Returns a function for removing the listeners. */
-export function installExitHooks(listener: NodeJS.SignalsListener): () => void {
-  const killSignals: ['SIGINT', 'SIGTERM'] = ['SIGINT', 'SIGTERM'];
-  for (const signal of killSignals) {
-    process.on(signal, listener);
+export function installExitHooks(asyncExitHook: AsyncExitHook): () => void {
+  // We need to instantiate the master listener the first time the queue is used.
+  if (!queue.length) {
+    // Track the master listener so we can remove it later.
+    unsubscribe = attachMasterListener();
+  }
+
+  queue.push(asyncExitHook);
+
+  return () => {
+    const index = queue.indexOf(asyncExitHook);
+    if (index >= 0) {
+      queue.splice(index, 1);
+    }
+    // Clean up the master listener if we don't need it anymore.
+    if (!queue.length) {
+      unsubscribe?.();
+    }
+  };
+}
+
+// Create a function that runs before the process exits and guards against running multiple times.
+function createExitHook(signal: NodeJS.Signals) {
+  return guardAsync(async () => {
+    Log.debug(`pre-exit (signal: ${signal}, queue length: ${queue.length})`);
+
+    for (const [index, hookAsync] of Object.entries(queue)) {
+      try {
+        await hookAsync(signal);
+      } catch (error: any) {
+        Log.debug(`Error in exit hook: %O (queue: ${index})`, error);
+      }
+    }
+
+    Log.debug(`post-exit (code: ${process.exitCode ?? 0})`);
+
+    process.exit();
+  });
+}
+
+function attachMasterListener() {
+  const hooks: [NodeJS.Signals, () => any][] = [];
+  for (const signal of PRE_EXIT_SIGNALS) {
+    const hook = createExitHook(signal);
+    hooks.push([signal, hook]);
+    process.on(signal, hook);
   }
   return () => {
-    if (!listener) {
-      return;
+    for (const [signal, hook] of hooks) {
+      process.removeListener(signal, hook);
     }
-    for (const signal of killSignals) {
-      process.off(signal, listener);
-    }
-    // @ts-expect-error: Allow the listener to be GC'd
-    listener = null;
   };
 }
