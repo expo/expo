630b036c3ed9180728d718d747e30401d2303a51
andy <40680668+ajsmth@users.noreply.github.com>
2022-04-01 15:10:27 -0700

    [dcc] remove build script (#16885)

--- packages/expo-dev-client-components/build/icons/DebugIcon.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { Image } from '../Image';
-export declare function DebugIcon(props: Partial<React.ComponentProps<typeof Image>>): JSX.Element;
-//# sourceMappingURL=DebugIcon.d.ts.map
