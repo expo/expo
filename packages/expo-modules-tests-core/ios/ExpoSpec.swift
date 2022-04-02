26aab6384a132d3ad14821bf2c001d333ed1795f
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-04-02 12:26:28 +0200

    Set up expo-modules-test-core for iOS

---
+++ packages/expo-modules-tests-core/ios/ExpoSpec.swift
@@ -0,0 +1,7 @@
+// Copyright 2022-present 650 Industries. All rights reserved.
+
+@_exported import Quick
+@_exported import Nimble
+@_exported @testable import ExpoModulesCore
+
+open class ExpoSpec: QuickSpec {}
