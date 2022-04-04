63b7e8de120729f0c63b09af66827636d08b7cb4
andy <40680668+ajsmth@users.noreply.github.com>
2022-04-04 13:48:15 -0700

    [dcc] remove dcc build script (#16912)

--- packages/expo-dev-client-components/build/icons/QuestionMarkIcon.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { Image } from '../Image';
-export declare function QuestionMarkIcon(props: Partial<React.ComponentProps<typeof Image>>): JSX.Element;
-//# sourceMappingURL=QuestionMarkIcon.d.ts.map
