354a3c1a913e89e9029f0b3929f158cb147ecdc7
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-04-05 09:54:21 +0200

    Native part

--- packages/expo-web-browser/ios/WebBrowserOptions.swift
+++ packages/expo-web-browser/ios/WebBrowserOptions.swift
@@ -18,6 +18,10 @@ struct WebBrowserOptions: Record {
 
   @Field
   var controlsColor: UIColor?
+  
+  // Defaults to .overFullScreen to keep backwards compatibility
+  @Field
+  var presentationStyle: PresentationStyle = .overFullScreen
 }
 
 enum DismissButtonStyle: String, EnumArgument {
@@ -33,6 +37,45 @@ enum DismissButtonStyle: String, EnumArgument {
       return .close
     case .cancel:
       return .cancel
+    }
+  }
+}
+
+internal enum PresentationStyle: Int, EnumArgument {
+  case fullScreen = 0
+  case pageSheet = 1
+  case formSheet = 2
+  case currentContext = 3
+  case overFullScreen = 5
+  case overCurrentContext = 6
+  case popover = 7
+  case none = -1
+  case automatic = -2
+
+  func toPresentationStyle() -> UIModalPresentationStyle {
+    switch self {
+    case .fullScreen:
+      return .fullScreen
+    case .pageSheet:
+      return .pageSheet
+    case .formSheet:
+      return .formSheet
+    case .currentContext:
+      return .currentContext
+    case .overFullScreen:
+      return .overFullScreen
+    case .overCurrentContext:
+      return .overCurrentContext
+    case .popover:
+      return .popover
+    case .none:
+      return .none
+    case .automatic:
+      if #available(iOS 13.0, *) {
+        return .automatic
+      }
+      // default prior iOS 13
+      return .fullScreen
     }
   }
 }
