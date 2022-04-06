2ee46a1caf1b9a5f2a19f9e8c89565bd483ad4cb
Juwan Wheatley <datwheat@gmail.com>
2022-04-06 16:11:58 -0400

    add pull-to-refresh to branch details page to get new updates

--- home/screens/BranchDetailsScreen/BranchDetailsView.tsx
+++ home/screens/BranchDetailsScreen/BranchDetailsView.tsx
@@ -1,9 +1,10 @@
+import { NetworkStatus } from '@apollo/client';
 import { spacing } from '@expo/styleguide-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import dedent from 'dedent';
 import { Spacer, Text, useExpoTheme, View } from 'expo-dev-client-components';
 import * as React from 'react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, RefreshControl } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
 import { SectionHeader } from '../../components/SectionHeader';
@@ -22,55 +23,68 @@ type Props = {
   loading: boolean;
   error?: Error;
   data?: BranchDetailsQuery;
+  branchName: string;
+  networkStatus: number;
+  refetch: () => Promise<unknown>;
 } & StackScreenProps<HomeStackRoutes, 'BranchDetails'>;
 
-export function BranchDetailsView({ loading, error, data }: Props) {
+export function BranchDetailsView({
+  loading,
+  error,
+  data,
+  refetch,
+  branchName,
+  networkStatus,
+}: Props) {
   const theme = useExpoTheme();
 
-  let contents;
+  const refetching = networkStatus === NetworkStatus.refetch;
 
   if (error && !data?.app?.byId.updateBranchByName) {
     console.error(error);
-    contents = (
-      <Text
-        align="center"
-        style={{ marginVertical: spacing[4], marginHorizontal: spacing[4] }}
-        type="InterRegular">
-        {ERROR_TEXT}
-      </Text>
-    );
-  } else if (loading || !data?.app?.byId.updateBranchByName) {
-    contents = (
+    return (
       <View flex="1" align="centered">
-        <ActivityIndicator size="large" color={theme.highlight.accent} />
-      </View>
-    );
-  } else {
-    contents = (
-      <View style={{ flex: 1, backgroundColor: theme.background.screen }}>
-        <BranchHeader
-          name={data.app.byId.updateBranchByName.name}
-          manifestPermalink={data.app.byId.updateBranchByName.updates[0].manifestPermalink}
-        />
-        <FlatList<typeof data.app.byId.updateBranchByName.updates[number]>
-          data={data.app.byId.updateBranchByName.updates}
-          ListHeaderComponent={<SectionHeader header="Updates" style={{ paddingTop: 0 }} />}
-          keyExtractor={(update) => update.id}
-          contentContainerStyle={{ padding: spacing[4] }}
-          ItemSeparatorComponent={() => <Spacer.Vertical size="small" />}
-          renderItem={({ item: update }) => (
-            <UpdateListItem
-              id={update.id}
-              message={update.message ?? undefined}
-              manifestPermalink={update.manifestPermalink}
-              createdAt={update.createdAt}
-            />
-          )}
-        />
-        {!data.app.byId.updateBranchByName!.updates.length && <EmptySection />}
+        <Text
+          align="center"
+          style={{ marginVertical: spacing[4], marginHorizontal: spacing[4] }}
+          type="InterRegular">
+          {ERROR_TEXT}
+        </Text>
       </View>
     );
   }
 
-  return <View flex="1">{contents}</View>;
+  if ((!refetching && loading) || !data?.app?.byId.updateBranchByName) {
+    return (
+      <View flex="1" align="centered">
+        <ActivityIndicator size="large" color={theme.highlight.accent} />
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: theme.background.screen }}>
+      <BranchHeader
+        name={branchName}
+        manifestPermalink={data.app.byId.updateBranchByName.updates[0].manifestPermalink}
+      />
+      <FlatList
+        data={data.app.byId.updateBranchByName.updates}
+        refreshControl={<RefreshControl onRefresh={refetch} refreshing={refetching} />}
+        ListHeaderComponent={<SectionHeader header="Updates" style={{ paddingTop: 0 }} />}
+        keyExtractor={(update) => update.id}
+        contentContainerStyle={{ padding: spacing[4] }}
+        ItemSeparatorComponent={() => <Spacer.Vertical size="small" />}
+        ListEmptyComponent={() => <EmptySection />}
+        renderItem={({ item: update }) => (
+          <UpdateListItem
+            id={update.id}
+            message={update.message ?? undefined}
+            manifestPermalink={update.manifestPermalink}
+            createdAt={update.createdAt}
+          />
+        )}
+      />
+    </View>
+  );
 }
