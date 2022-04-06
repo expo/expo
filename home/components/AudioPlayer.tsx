203d73807fbac88e536a88558618c3bce56db053
Juwan Wheatley <datwheat@gmail.com>
2022-04-06 15:36:49 -0400

    don't import vector icons from index file

--- home/components/AudioPlayer.tsx
+++ home/components/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import { Ionicons } from '@expo/vector-icons';
+import Ionicons from '@expo/vector-icons/build/Ionicons';
 import { Audio, AVPlaybackStatus } from 'expo-av';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, StyleProp, ViewStyle } from 'react-native';
