9cb2c7651508a546917f8e6a6d55dc4ed6b4a415
Kudo Chien <kudo@expo.dev>
2022-04-08 10:40:19 +0800

    [ci] Use ubuntu runner for android e2e test (#16862)
    # Why
    
    we have limited macos runners to run ci jobs. nowadays we have more e2e test jobs and it gets easier to block pr ci checks. this pr tries to use ubuntu runners for some android e2e test.
    
    # How
    
    - use ubuntu runner for updates-e2e ci job
    - merge android instrumented test into android unit test and use ubuntu runner.
    - i found x86 emulator is faster than x86_64. it gets about 10mins to boot up which is not so bad.
    - comment out more expo-clipboard random failed test.
    - android instrumented test is not perfect on ubuntu runners. it will sometimes failed like the following error.
    ```
     > : No compatible devices connected.[TestRunner] FAILED 
    Found 1 connected device(s), 0 of which were compatible.
    ```
    
    ### CI jobs spending time
    
    #### Before
     - Updates e2e: ~21m
     - Android instrumented test: ~41m
     - Android unit test: ~19m
    
    #### After
     - Updates e2e: ~20m
     - Android unit test (with instrumented test): ~45m
     
    
    # Test Plan
    
    ci passed

--- packages/expo-updates/e2e/__tests__/Updates-e2e-android.test.ts
+++ packages/expo-updates/e2e/__tests__/Updates-e2e-android.test.ts
@@ -5,12 +5,22 @@ import * as Server from './utils/server';
 
 const SERVER_PORT = parseInt(process.env.UPDATES_PORT, 10);
 const APK_PATH = process.env.TEST_APK_PATH;
-const ADB_PATH = process.env.HOME
-  ? path.join(process.env.HOME, 'Library', 'Android', 'sdk', 'platform-tools', 'adb')
-  : 'adb';
+const ADB_PATH = (function () {
+  if (process.env.ADB_PATH) {
+    return process.env.ADB_PATH;
+  }
+  if (process.env.ANDROID_SDK_ROOT) {
+    return path.join(process.env.ANDROID_SDK_ROOT, 'platform-tools', 'adb');
+  }
+  if (process.env.HOME) {
+    return path.join(process.env.HOME, 'Library', 'Android', 'sdk', 'platform-tools', 'adb');
+  }
+  return 'adb';
+})();
 
 const PACKAGE_NAME = 'dev.expo.updatese2e';
 const ACTIVITY_NAME = `${PACKAGE_NAME}/${PACKAGE_NAME}.MainActivity`;
+const TIMEOUT_BIAS = process.env.CI ? 10 : 1;
 
 async function installAndroidApk(apkPath: string) {
   await spawnAsync(ADB_PATH, ['install', apkPath]);
@@ -36,17 +46,17 @@ afterEach(async () => {
 });
 
 test('starts app, stops, and starts again', async () => {
-  jest.setTimeout(300000);
+  jest.setTimeout(300000 * TIMEOUT_BIAS);
   Server.start(SERVER_PORT);
   await installAndroidApk(APK_PATH);
   await startActivity(ACTIVITY_NAME);
-  const response = await Server.waitForResponse(10000);
+  const response = await Server.waitForResponse(10000 * TIMEOUT_BIAS);
   expect(response).toBe('test');
   await stopApplication(PACKAGE_NAME);
 
-  await expect(Server.waitForResponse(5000)).rejects.toThrow('Timed out waiting for response');
+  await expect(Server.waitForResponse(5000 * TIMEOUT_BIAS)).rejects.toThrow('Timed out waiting for response');
 
   await startActivity(ACTIVITY_NAME);
-  const response2 = await Server.waitForResponse(10000);
+  const response2 = await Server.waitForResponse(10000 * TIMEOUT_BIAS);
   expect(response2).toBe('test');
 });
