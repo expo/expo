2615ab2ae08c7320b4f7986f3bd7f4a28f5597d9
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-04-10 22:50:23 +0200

    Run unit tests on the single target [skip ci]

---
+++ ios/UnitTests/UnitTests.swift
@@ -0,0 +1,36 @@
+//
+//  UnitTests.swift
+//  UnitTests
+//
+//  Created by Tomasz Sapeta on 10/04/2022.
+//  Copyright © 2022 650 Industries. All rights reserved.
+//
+
+import XCTest
+
+class UnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
