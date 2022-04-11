ac20be6e3b65f1e49b3a7ee3edaa3f97ed2d0210
Kudo Chien <kudo@expo.dev>
2022-04-11 22:37:15 +0800

    add `getJavaScriptExecutorFactory` for module to override the js executor factory

--- packages/expo/android/src/main/java/expo/modules/ReactNativeHostWrapperBase.kt
+++ packages/expo/android/src/main/java/expo/modules/ReactNativeHostWrapperBase.kt
@@ -44,7 +44,9 @@ open class ReactNativeHostWrapperBase(
   }
 
   override fun getJavaScriptExecutorFactory(): JavaScriptExecutorFactory? {
-    return invokeDelegateMethod("getJavaScriptExecutorFactory")
+    return reactNativeHostHandlers.asSequence()
+      .mapNotNull { it.javaScriptExecutorFactory }
+      .firstOrNull() ?: invokeDelegateMethod("getJavaScriptExecutorFactory")
   }
 
   @Suppress("DEPRECATION")
