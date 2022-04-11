9e7220cf896a3c3f9719b4355cd8534a5562325a
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-04-11 13:52:48 +0200

    [iOS][firebase-analytics] Backport `setCurrentScreen` method removal
    Redirect `setCurrentScreen` method call to `logEvent` with default `screen name` and `screen class` arguments names.

--- ios/versioned/sdk43/EXFirebaseAnalytics/EXFirebaseAnalytics/ABI43_0_0EXFirebaseAnalytics.m
+++ ios/versioned/sdk43/EXFirebaseAnalytics/EXFirebaseAnalytics/ABI43_0_0EXFirebaseAnalytics.m
@@ -95,16 +95,10 @@ ABI43_0_0EX_EXPORT_METHOD_AS(setCurrentScreen,
                     screenClass:(NSString *)screenClassOverview
                     resolver:(ABI43_0_0EXPromiseResolveBlock)resolve
                     rejecter:(ABI43_0_0EXPromiseRejectBlock)reject) {
-  if ([self getAppOrReject:reject] == nil) return;
-  [ABI43_0_0EXUtilities performSynchronouslyOnMainThread:^{
-    @try {
-      [FIRAnalytics setScreenName:screenName screenClass:screenClassOverview];
-      resolve([NSNull null]);
-    } @catch (NSException *exception) {
-      [self reject:reject withException:exception];
-      return;
-    }
-  }];
+  [self logEvent:@"screen view"
+      parameters:@{ @"screen name": screenName, @"screen class": screenClassOverview }
+        resolver:resolve
+        rejecter:reject];
 }
 
 ABI43_0_0EX_EXPORT_METHOD_AS(setUserId,
