516fe224340307418ee02b91bfe5c095396ad5fb
Evan Bacon <bacon@expo.io>
2022-04-11 13:55:46 -0500

    fix(cli): fix web support in `expo start` command (#17006)
    * fix(cli): fix web start command
    
    * Update packages/@expo/cli/CHANGELOG.md
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/cli/src/start/platforms/android/ADBServer.ts
+++ packages/@expo/cli/src/start/platforms/android/ADBServer.ts
@@ -46,7 +46,10 @@ export class ADBServer {
 
   /** Kill the ADB server. */
   async stopAsync(): Promise<boolean> {
+    Log.debug('Stopping ADB server');
+
     if (!this.isRunning) {
+      Log.debug('ADB server is not running');
       return false;
     }
     this.removeExitHook();
@@ -57,6 +60,7 @@ export class ADBServer {
       Log.error('Failed to stop ADB server: ' + error.message);
       return false;
     } finally {
+      Log.debug('Stopped ADB server');
       this.isRunning = false;
     }
   }
