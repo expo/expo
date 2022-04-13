9bc9ec4207381ae844b572c7ac4f3e0c4f91d06d
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-04-13 20:01:11 +0200

    [ios] Implement more functionalities to JavaScriptObject (#16991)

--- packages/expo-modules-core/ios/JSI/EXJavaScriptObject.mm
+++ packages/expo-modules-core/ios/JSI/EXJavaScriptObject.mm
@@ -34,33 +34,50 @@
   return _jsObjectPtr.get();
 }
 
-#pragma mark - Subscripting
+#pragma mark - Accessing object properties
 
-- (nullable id)objectForKeyedSubscript:(nonnull NSString *)key
+- (BOOL)hasProperty:(nonnull NSString *)name
 {
-  auto runtime = [_runtime get];
-  auto callInvoker = [_runtime callInvoker];
-
-  if (runtime && callInvoker) {
-    auto value = _jsObjectPtr->getProperty(*runtime, [key UTF8String]);
-    return expo::convertJSIValueToObjCObject(*runtime, value, callInvoker);
-  }
-  return nil;
+  return _jsObjectPtr->hasProperty(*[_runtime get], [name UTF8String]);
 }
 
-- (void)setObject:(nullable id)obj forKeyedSubscript:(nonnull NSString *)key
+- (nonnull EXJavaScriptValue *)getProperty:(nonnull NSString *)name
 {
-  auto runtime = [_runtime get];
+  std::shared_ptr<jsi::Value> value = std::make_shared<jsi::Value>(_jsObjectPtr->getProperty(*[_runtime get], [name UTF8String]));
+  return [[EXJavaScriptValue alloc] initWithRuntime:_runtime value:value];
+}
 
-  if (!runtime) {
-    NSLog(@"Cannot set '%@' property when the EXJavaScript runtime is no longer available.", key);
-    return;
-  }
-  if ([obj isKindOfClass:[EXJavaScriptObject class]]) {
-    _jsObjectPtr->setProperty(*runtime, [key UTF8String], *[obj get]);
-  } else {
-    _jsObjectPtr->setProperty(*runtime, [key UTF8String], expo::convertObjCObjectToJSIValue(*runtime, obj));
-  }
+- (nonnull NSArray<NSString *> *)getPropertyNames
+{
+  jsi::Runtime *runtime = [_runtime get];
+  jsi::Array propertyNamesArray = _jsObjectPtr->getPropertyNames(*[_runtime get]);
+  return expo::convertJSIArrayToNSArray(*runtime, propertyNamesArray, nullptr);
+}
+
+#pragma mark - Modifying object properties
+
+- (void)setProperty:(nonnull NSString *)name value:(nullable id)value
+{
+  jsi::Value jsiValue = expo::convertObjCObjectToJSIValue(*[_runtime get], value);
+  _jsObjectPtr->setProperty(*[_runtime get], [name UTF8String], jsiValue);
+}
+
+- (void)defineProperty:(nonnull NSString *)name value:(nullable id)value options:(EXJavaScriptObjectPropertyDescriptor)options
+{
+  jsi::Runtime *runtime = [_runtime get];
+  jsi::Object global = runtime->global();
+  jsi::Object objectClass = global.getPropertyAsObject(*runtime, "Object");
+  jsi::Function definePropertyFunction = objectClass.getPropertyAsFunction(*runtime, "defineProperty");
+  jsi::Object descriptor = [self preparePropertyDescriptorWithOptions:options];
+
+  descriptor.setProperty(*runtime, "value", expo::convertObjCObjectToJSIValue(*runtime, value));
+
+  // This call is basically the same as `Object.defineProperty(object, name, descriptor)` in JS
+  definePropertyFunction.callWithThis(*runtime, objectClass, {
+    jsi::Value(*runtime, *_jsObjectPtr.get()),
+    jsi::String::createFromUtf8(*runtime, [name UTF8String]),
+    std::move(descriptor),
+  });
 }
 
 #pragma mark - Functions
@@ -87,6 +104,18 @@
   }
   jsi::Function function = [_runtime createSyncFunction:name argsCount:argsCount block:block];
   _jsObjectPtr->setProperty(*[_runtime get], [name UTF8String], function);
+}
+
+#pragma mark - Private helpers
+
+- (jsi::Object)preparePropertyDescriptorWithOptions:(EXJavaScriptObjectPropertyDescriptor)options
+{
+  jsi::Runtime *runtime = [_runtime get];
+  jsi::Object descriptor(*runtime);
+  descriptor.setProperty(*runtime, "configurable", (bool)(options & EXJavaScriptObjectPropertyDescriptorConfigurable));
+  descriptor.setProperty(*runtime, "enumerable", (bool)(options & EXJavaScriptObjectPropertyDescriptorEnumerable));
+  descriptor.setProperty(*runtime, "writable", (bool)(options & EXJavaScriptObjectPropertyDescriptorWritable));
+  return descriptor;
 }
 
 @end
