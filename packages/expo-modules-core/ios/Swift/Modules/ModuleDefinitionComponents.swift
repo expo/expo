edd9849c279fa06ae714dfc5ace1c9f59081e02d
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-04-17 15:57:34 +0200

    [ios] Capitalize definition components

--- packages/expo-modules-core/ios/Swift/Modules/ModuleDefinitionComponents.swift
+++ packages/expo-modules-core/ios/Swift/Modules/ModuleDefinitionComponents.swift
@@ -12,6 +12,7 @@ extension AnyModule {
   /**
    Sets the name of the module that is exported to the JavaScript world.
    */
+  @available(*, deprecated, renamed: "Name")
   public func name(_ name: String) -> AnyDefinition {
     return ModuleNameDefinition(name: name)
   }
@@ -21,6 +22,7 @@ extension AnyModule {
   /**
    Creates module's lifecycle listener that is called right after module initialization.
    */
+  @available(*, deprecated, renamed: "OnCreate")
   public func onCreate(_ closure: @escaping () -> Void) -> AnyDefinition {
     return EventListener(.moduleCreate, closure)
   }
@@ -28,6 +30,7 @@ extension AnyModule {
   /**
    Creates module's lifecycle listener that is called when the module is about to be deallocated.
    */
+  @available(*, deprecated, renamed: "OnDestroy")
   public func onDestroy(_ closure: @escaping () -> Void) -> AnyDefinition {
     return EventListener(.moduleDestroy, closure)
   }
@@ -35,6 +38,7 @@ extension AnyModule {
   /**
    Creates module's lifecycle listener that is called when the app context owning the module is about to be deallocated.
    */
+  @available(*, deprecated, renamed: "OnAppContextDestroys")
   public func onAppContextDestroys(_ closure: @escaping () -> Void) -> AnyDefinition {
     return EventListener(.appContextDestroys, closure)
   }
@@ -42,6 +46,7 @@ extension AnyModule {
   /**
    Creates a listener that is called when the app is about to enter the foreground mode.
    */
+  @available(*, deprecated, renamed: "OnAppEntersBackground")
   public func onAppEntersForeground(_ closure: @escaping () -> Void) -> AnyDefinition {
     return EventListener(.appEntersForeground, closure)
   }
@@ -49,6 +54,7 @@ extension AnyModule {
   /**
    Creates a listener that is called when the app becomes active again.
    */
+  @available(*, deprecated, renamed: "OnAppBecomesActive")
   public func onAppBecomesActive(_ closure: @escaping () -> Void) -> AnyDefinition {
     return EventListener(.appBecomesActive, closure)
   }
@@ -56,6 +62,7 @@ extension AnyModule {
   /**
    Creates a listener that is called when the app enters the background mode.
    */
+  @available(*, deprecated, renamed: "OnAppEntersBackground")
   public func onAppEntersBackground(_ closure: @escaping () -> Void) -> AnyDefinition {
     return EventListener(.appEntersBackground, closure)
   }
@@ -65,12 +72,76 @@ extension AnyModule {
   /**
    Creates the view manager definition that scopes other view-related definitions.
    */
+  @available(*, deprecated, renamed: "ViewManager")
   public func viewManager(@ViewManagerDefinitionBuilder _ closure: @escaping () -> ViewManagerDefinition) -> AnyDefinition {
     return closure()
   }
 }
 
-// TODO: - Remove deprecated `method` component once SDK44 is out.
+// MARK: - Module name
+
+/**
+ Sets the name of the module that is exported to the JavaScript world.
+ */
+public func Name(_ name: String) -> AnyDefinition {
+  return ModuleNameDefinition(name: name)
+}
+
+// MARK: - Module's lifecycle
+
+/**
+ Creates module's lifecycle listener that is called right after module initialization.
+ */
+public func OnCreate(_ closure: @escaping () -> Void) -> AnyDefinition {
+  return EventListener(.moduleCreate, closure)
+}
+
+/**
+ Creates module's lifecycle listener that is called when the module is about to be deallocated.
+ */
+public func OnDestroy(_ closure: @escaping () -> Void) -> AnyDefinition {
+  return EventListener(.moduleDestroy, closure)
+}
+
+/**
+ Creates module's lifecycle listener that is called when the app context owning the module is about to be deallocated.
+ */
+public func OnAppContextDestroys(_ closure: @escaping () -> Void) -> AnyDefinition {
+  return EventListener(.appContextDestroys, closure)
+}
+
+/**
+ Creates a listener that is called when the app is about to enter the foreground mode.
+ */
+public func OnAppEntersForeground(_ closure: @escaping () -> Void) -> AnyDefinition {
+  return EventListener(.appEntersForeground, closure)
+}
+
+/**
+ Creates a listener that is called when the app becomes active again.
+ */
+public func OnAppBecomesActive(_ closure: @escaping () -> Void) -> AnyDefinition {
+  return EventListener(.appBecomesActive, closure)
+}
+
+/**
+ Creates a listener that is called when the app enters the background mode.
+ */
+public func OnAppEntersBackground(_ closure: @escaping () -> Void) -> AnyDefinition {
+  return EventListener(.appEntersBackground, closure)
+}
+
+// MARK: - View Manager
+
+/**
+ Creates the view manager definition that scopes other view-related definitions.
+ */
+public func ViewManager(@ViewManagerDefinitionBuilder _ closure: @escaping () -> ViewManagerDefinition) -> AnyDefinition {
+  return closure()
+}
+
+// MARK: - Deprecated (remove deprecated `method` component once SDK44 is out)
+
 public extension AnyModule {
   /**
    Function without arguments.
