f84df809f7a5ee32bb4feb09cb5a34d584efed72
Brent Vatne <brentvatne@gmail.com>
2022-04-19 12:41:39 -0400

    Use TurboModuleRegistry.get instead of NativeModule API

--- packages/expo/build/environment/DevLoadingView.js
+++ packages/expo/build/environment/DevLoadingView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef, useMemo } from 'react';
-import { Animated, StyleSheet, Text, NativeModules, NativeEventEmitter, View, } from 'react-native';
+import { Animated, StyleSheet, Text, NativeModules, NativeEventEmitter, TurboModuleRegistry, View, } from 'react-native';
 export default function DevLoadingView() {
     const [isDevLoading, setIsDevLoading] = useState(false);
     const [isAnimating, setIsAnimating] = useState(false);
@@ -67,10 +67,10 @@ export default function DevLoadingView() {
 }
 /**
  * This is a hack to get the safe area insets without explicitly depending on react-native-safe-area-context.
- * The following code is lifted from: https://git.io/Jzk4k
  **/
-const RNCSafeAreaContext = NativeModules.RNCSafeAreaContext;
-const initialWindowMetrics = RNCSafeAreaContext?.initialWindowMetrics;
+const RNCSafeAreaContext = TurboModuleRegistry.get('RNCSafeAreaContext');
+// @ts-ignore: we're not using the spec so the return type of getConstants() is {}
+const initialWindowMetrics = RNCSafeAreaContext?.getConstants?.().initialWindowMetrics;
 const styles = StyleSheet.create({
     animatedContainer: {
         position: 'absolute',
