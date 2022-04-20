b87b8c575593ef896d803a7333ab8660ba7bd847
Kudo Chien <kudo@expo.dev>
2022-04-20 10:26:48 +0800

    [tools] move ios versioned `FBReactNativeSpec` out from build directory (#17133)
    # Why
    
    unlike react-native's `FBReactNativeSpec` where placed inside build/, our versioned code is committed into git and should not place inside build/.
    
    # How
    
    move generated FBReactNativeSpec from `ios/build/ABI45_0_0/generated/ios/ABI45_0_0FBReactNativeSpec/ABI45_0_0FBReactNativeSpec.h` to `ios/versioned-react-native/ABI45_0_0/ReactNative/codegen/ios/ABI45_0_0FBReactNativeSpec/ABI45_0_0FBReactNativeSpec.h`
    
    # Test Plan
    
    `et add-sdk -p ios -s 45.0.0` + versioned ios expo go build

--- tools/src/versioning/ios/index.ts
+++ tools/src/versioning/ios/index.ts
@@ -7,6 +7,7 @@ import inquirer from 'inquirer';
 import path from 'path';
 import semver from 'semver';
 
+import { runReactNativeCodegenAsync } from '../../Codegen';
 import { EXPO_DIR, IOS_DIR, VERSIONED_RN_IOS_DIR } from '../../Constants';
 import logger from '../../Logger';
 import { getListOfPackagesAsync, Package } from '../../Packages';
@@ -22,7 +23,6 @@ import {
   MODULES_PROVIDER_POD_NAME,
   versionExpoModulesProviderAsync,
 } from './versionExpoModulesProvider';
-import { runReactNativeCodegenAsync } from './versionReactNative';
 import {
   versionVendoredModulesAsync,
   removeVersionedVendoredModulesAsync,
@@ -222,11 +222,17 @@ async function generateVersionedReactNativeAsync(versionName: string): Promise<v
   await Promise.all(jsFiles.map((jsFile) => fs.remove(jsFile)));
 
   console.log('Running react-native-codegen');
-  await runReactNativeCodegenAsync(
-    path.join(EXPO_DIR, RELATIVE_RN_PATH),
-    versionedReactNativePath,
-    versionName
-  );
+  await runReactNativeCodegenAsync({
+    reactNativeRoot: path.join(EXPO_DIR, RELATIVE_RN_PATH),
+    codegenPkgRoot: path.join(EXPO_DIR, RELATIVE_RN_PATH, 'packages', 'react-native-codegen'),
+    outputDir: path.join(versionedReactNativePath, 'codegen', 'ios'),
+    outputDirBaseName: `${versionName}FBReactNativeSpec`,
+    name: 'FBReactNativeSpec',
+    type: 'modules',
+    platform: 'ios',
+    jsSrcsDir: path.join(EXPO_DIR, RELATIVE_RN_PATH, 'Libraries'),
+    keepIntermediateSchema: true,
+  });
 
   console.log(
     `Copying cpp libraries from ${chalk.magenta(path.join(RELATIVE_RN_PATH, 'ReactCommon'))} ...`
@@ -327,7 +333,7 @@ async function generateReactNativePodScriptAsync(
   targetSource = targetSource
     .replace(
       "$CODEGEN_OUTPUT_DIR = 'build/generated/ios'",
-      `$CODEGEN_OUTPUT_DIR = 'build/${versionName}/generated/ios'`
+      `$CODEGEN_OUTPUT_DIR = '${path.relative(IOS_DIR, versionedReactNativePath)}/codegen/ios'`
     )
     .replace(/\$(CODEGEN_OUTPUT_DIR)\b/g, `$${versionName}$1`)
     .replace(/\b(React-Codegen)\b/g, `${versionName}$1`)
@@ -907,11 +913,6 @@ export async function addVersionAsync(versionNumber: string, packages: Package[]
   // Namespace the new React clone
   console.log('Namespacing/transforming files...');
   await transformReactNativeAsync(newVersionPath, versionName, versionedPodNames);
-  await transformReactNativeAsync(
-    path.join(IOS_DIR, 'build', versionName, 'generated', 'ios'),
-    versionName,
-    versionedPodNames
-  );
 
   // Generate Ruby scripts with versioned dependencies and postinstall actions that will be evaluated in the Expo client's Podfile.
   console.log('Adding dependency to root Podfile...');
