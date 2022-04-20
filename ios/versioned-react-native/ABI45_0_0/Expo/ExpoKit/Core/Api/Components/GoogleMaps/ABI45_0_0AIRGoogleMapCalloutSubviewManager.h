b272c709a82fad19e017ef859f0343079afae727
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-04-20 11:19:15 +0200

    [ios] Add versioned code for SDK45

---
+++ ios/versioned-react-native/ABI45_0_0/Expo/ExpoKit/Core/Api/Components/GoogleMaps/ABI45_0_0AIRGoogleMapCalloutSubviewManager.h
@@ -0,0 +1,17 @@
+//
+//  ABI45_0_0AIRGoogleMapCalloutSubviewManager.h
+//  AirMaps
+//
+//  Created by Denis Oblogin on 10/8/18.
+//
+//
+
+#ifdef ABI45_0_0HAVE_GOOGLE_MAPS
+
+#import <ABI45_0_0React/ABI45_0_0RCTViewManager.h>
+
+@interface ABI45_0_0AIRGoogleMapCalloutSubviewManager : ABI45_0_0RCTViewManager
+
+@end
+
+#endif
