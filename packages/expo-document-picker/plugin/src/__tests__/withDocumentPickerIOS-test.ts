ddcbf05963df009a4ba8aa39ed6823b05e3c279c
evanbacon <bacon@expo.io>
2022-04-21 11:56:26 -0400

    Update withDocumentPickerIOS-test.ts

--- packages/expo-document-picker/plugin/src/__tests__/withDocumentPickerIOS-test.ts
+++ packages/expo-document-picker/plugin/src/__tests__/withDocumentPickerIOS-test.ts
@@ -2,21 +2,24 @@ import { setICloudEntitlements } from '../withDocumentPickerIOS';
 
 describe(setICloudEntitlements, () => {
   it(`skips setting the iCloud entitlements if the flag isn't enabled`, () => {
-    expect(setICloudEntitlements({ ios: {} }, { appleTeamId: 'X1X2X3X4X' }, {})).toStrictEqual({});
+    expect(
+      setICloudEntitlements({ ios: {} }, { iCloudContainerEnvironment: 'Production' }, {})
+    ).toStrictEqual({});
   });
   it(`sets the iCloud entitlements`, () => {
     expect(
       setICloudEntitlements(
         { ios: { usesIcloudStorage: true, bundleIdentifier: 'com.bacon.foobar' } },
-        { appleTeamId: 'X1X2X3X4X', iCloudContainerEnvironment: 'Production' },
+        { iCloudContainerEnvironment: 'Production' },
         {}
       )
     ).toStrictEqual({
       'com.apple.developer.icloud-container-environment': 'Production',
-      'com.apple.developer.icloud-container-identifiers': ['iCloud.com.bacon.foobar'],
+      'com.apple.developer.icloud-container-identifiers': ['iCloud.$(CFBundleIdentifier)'],
       'com.apple.developer.icloud-services': ['CloudDocuments'],
-      'com.apple.developer.ubiquity-container-identifiers': ['iCloud.com.bacon.foobar'],
-      'com.apple.developer.ubiquity-kvstore-identifier': 'X1X2X3X4X.com.bacon.foobar',
+      'com.apple.developer.ubiquity-container-identifiers': ['iCloud.$(CFBundleIdentifier)'],
+      'com.apple.developer.ubiquity-kvstore-identifier':
+        '$(TeamIdentifierPrefix)$(CFBundleIdentifier)',
     });
   });
 });
