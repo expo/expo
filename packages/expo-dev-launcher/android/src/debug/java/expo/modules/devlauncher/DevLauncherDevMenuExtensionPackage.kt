1284af682778a9b34d6892c740f6695aa3569b42
andy <40680668+ajsmth@users.noreply.github.com>
2022-04-21 10:40:09 -0700

    [dev-menu] rebundle dev menu w/ minor ui changes

---
+++ packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/DevLauncherDevMenuExtensionPackage.kt
@@ -0,0 +1,24 @@
+package expo.modules.devlauncher
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+import expo.modules.core.interfaces.Package
+import expo.modules.devlauncher.modules.DevLauncherDevMenuExtension
+
+
+class DevLauncherDevMenuExtensionPackage : Package, ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+    return mutableListOf(
+      DevLauncherDevMenuExtension(reactContext)
+    )
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> {
+    return mutableListOf()
+  }
+
+}
