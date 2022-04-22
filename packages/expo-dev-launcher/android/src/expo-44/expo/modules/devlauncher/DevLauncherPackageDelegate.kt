2fd1cacb392f537166bcb30ba4d5041ba6e23154
Łukasz Kosmaty <kosmatylukasz@gmail.com>
2022-04-22 16:02:46 +0200

    [client] Remove `DevLauncherDevMenuExtensionPackage` & fix crashes when using Hermes

--- packages/expo-dev-launcher/android/src/expo-44/expo/modules/devlauncher/DevLauncherPackageDelegate.kt
+++ packages/expo-dev-launcher/android/src/expo-44/expo/modules/devlauncher/DevLauncherPackageDelegate.kt
@@ -6,7 +6,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import expo.modules.core.interfaces.ApplicationLifecycleListener
 import expo.modules.core.interfaces.ReactActivityHandler
 import expo.modules.core.interfaces.ReactActivityLifecycleListener
-import expo.modules.devlauncher.modules.DevLauncherDevMenuExtensions
+import expo.modules.devlauncher.modules.DevLauncherDevMenuExtension
 import expo.modules.devlauncher.modules.DevLauncherInternalModule
 import expo.modules.devlauncher.modules.DevLauncherModule
 import expo.modules.devlauncher.modules.DevLauncherAuth
@@ -21,7 +21,7 @@ object DevLauncherPackageDelegate {
     listOf(
       DevLauncherModule(reactContext),
       DevLauncherInternalModule(reactContext),
-      DevLauncherDevMenuExtensions(reactContext),
+      DevLauncherDevMenuExtension(reactContext),
       DevLauncherAuth(reactContext),
       DevMenuPreferences(reactContext)
     )
