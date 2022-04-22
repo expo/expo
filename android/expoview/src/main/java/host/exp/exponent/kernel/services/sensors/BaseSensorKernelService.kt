97b7a2eb971b380d00399eab15624245d5dca0b8
Kudo Chien <kudo@expo.dev>
2022-04-22 16:26:21 +0800

    [android][sensors] Use SENSOR_DELAY_FASTEST only if permission.HIGH_SAMPLING_RATE_SENSORS presented

--- android/expoview/src/main/java/host/exp/exponent/kernel/services/sensors/BaseSensorKernelService.kt
+++ android/expoview/src/main/java/host/exp/exponent/kernel/services/sensors/BaseSensorKernelService.kt
@@ -1,16 +1,21 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 package host.exp.exponent.kernel.services.sensors
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import host.exp.exponent.kernel.services.BaseKernelService
 
 abstract class BaseSensorKernelService internal constructor(reactContext: Context) : BaseKernelService(reactContext), SensorEventListener {
   private var sensor: Sensor? = null
   private val sensorManager: SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+  private val samplingPeriodUs: Int
+    get() = if (hasHighSamplingRateSensorsPermission()) SensorManager.SENSOR_DELAY_FASTEST else SensorManager.SENSOR_DELAY_NORMAL
 
   abstract val sensorType: Int
   abstract fun onSensorDataChanged(sensorEvent: SensorEvent)
@@ -18,12 +23,27 @@ abstract class BaseSensorKernelService internal constructor(reactContext: Contex
   protected fun startObserving() {
     sensor = sensorManager.getDefaultSensor(sensorType)
     if (sensor != null) {
-      sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+      sensorManager.registerListener(this, sensor, samplingPeriodUs)
     }
   }
 
   protected fun stopObserving() {
     sensorManager.unregisterListener(this)
+  }
+
+  private fun hasHighSamplingRateSensorsPermission(): Boolean {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+      return false
+    }
+
+    try {
+      context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)?.run {
+        return requestedPermissions.contains(Manifest.permission.HIGH_SAMPLING_RATE_SENSORS)
+      }
+      return false
+    } catch (e: PackageManager.NameNotFoundException) {
+      return false
+    }
   }
 
   // android.hardware.SensorEventListener
--- packages/expo-sensors/android/src/main/java/expo/modules/sensors/services/BaseSensorService.kt
+++ packages/expo-sensors/android/src/main/java/expo/modules/sensors/services/BaseSensorService.kt
@@ -1,14 +1,19 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 package expo.modules.sensors.services
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEventListener2
 import android.hardware.SensorManager
+import android.os.Build
 
 abstract class BaseSensorService internal constructor(reactContext: Context?) : BaseService(reactContext!!), SensorEventListener2 {
   private var mSensor: Sensor? = null
   private val mSensorManager: SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+  private val samplingPeriodUs: Int
+    get() = if (hasHighSamplingRateSensorsPermission()) SensorManager.SENSOR_DELAY_FASTEST else SensorManager.SENSOR_DELAY_NORMAL
 
   // Abstract methods that subclasses should implement
   abstract val sensorType: Int
@@ -16,11 +21,26 @@ abstract class BaseSensorService internal constructor(reactContext: Context?) : 
   // Public API
   protected fun startObserving() {
     if (mSensorManager.getDefaultSensor(sensorType).also { mSensor = it } != null) {
-      mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_FASTEST)
+      mSensorManager.registerListener(this, mSensor, samplingPeriodUs)
     }
   }
 
   protected fun stopObserving() {
     mSensorManager.unregisterListener(this)
+  }
+
+  private fun hasHighSamplingRateSensorsPermission(): Boolean {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+      return false
+    }
+
+    try {
+      context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)?.run {
+        return requestedPermissions.contains(Manifest.permission.HIGH_SAMPLING_RATE_SENSORS)
+      }
+      return false
+    } catch (e: PackageManager.NameNotFoundException) {
+      return false
+    }
   }
 }
