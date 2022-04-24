091d1acebb5cdaacaf45decc1fb179ce6770d082
Evan Bacon <bacon@expo.io>
2022-04-24 15:53:28 -0500

    [image-picker] Added ability to disable config plugin permissions (#17168)
    * Added ability to disable config plugin permissions.
    
    * Drop all AndroidManifest modifications in favor of AndroidManifest merge.
    
    Update README.md
    
    fix files
    
    fix files
    
    * Update packages/expo-image-picker/CHANGELOG.md
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    
    * Added tests
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-image-picker/plugin/build/withImagePicker.d.ts
+++ packages/expo-image-picker/plugin/build/withImagePicker.d.ts
@@ -1,8 +1,9 @@
-import { ConfigPlugin, AndroidConfig } from '@expo/config-plugins';
-export declare function setImagePickerManifestActivity(androidManifest: AndroidConfig.Manifest.AndroidManifest): AndroidConfig.Manifest.AndroidManifest;
-declare const _default: ConfigPlugin<void | {
-    photosPermission?: string | undefined;
-    cameraPermission?: string | undefined;
-    microphonePermission?: string | undefined;
-}>;
+import { ConfigPlugin, InfoPlist } from '@expo/config-plugins';
+declare type Props = {
+    photosPermission?: string | false;
+    cameraPermission?: string | false;
+    microphonePermission?: string | false;
+};
+export declare function setImagePickerInfoPlist(infoPlist: InfoPlist, { cameraPermission, microphonePermission, photosPermission }: Props): InfoPlist;
+declare const _default: ConfigPlugin<void | Props>;
 export default _default;
--- packages/expo-image-picker/plugin/build/withImagePicker.js
+++ packages/expo-image-picker/plugin/build/withImagePicker.js
@@ -1,56 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.setImagePickerManifestActivity = void 0;
+exports.setImagePickerInfoPlist = void 0;
 const config_plugins_1 = require("@expo/config-plugins");
 const pkg = require('expo-image-picker/package.json');
 const CAMERA_USAGE = 'Allow $(PRODUCT_NAME) to access your camera';
 const MICROPHONE_USAGE = 'Allow $(PRODUCT_NAME) to access your microphone';
 const READ_PHOTOS_USAGE = 'Allow $(PRODUCT_NAME) to access your photos';
-function setImagePickerManifestActivity(androidManifest) {
-    const app = config_plugins_1.AndroidConfig.Manifest.getMainApplicationOrThrow(androidManifest);
-    if (!app.activity) {
-        app.activity = [];
+function setImagePickerInfoPlist(infoPlist, { cameraPermission, microphonePermission, photosPermission }) {
+    if (photosPermission === false) {
+        delete infoPlist.NSPhotoLibraryUsageDescription;
     }
-    if (!app.activity.find(({ $ }) => $['android:name'] === 'com.canhub.cropper.CropImageActivity')) {
-        app.activity.push({
-            $: {
-                'android:name': 'com.canhub.cropper.CropImageActivity',
-                'android:theme': '@style/Base.Theme.AppCompat',
-            },
-        });
+    else {
+        infoPlist.NSPhotoLibraryUsageDescription =
+            photosPermission || infoPlist.NSPhotoLibraryUsageDescription || READ_PHOTOS_USAGE;
     }
-    return androidManifest;
+    if (cameraPermission === false) {
+        delete infoPlist.NSCameraUsageDescription;
+    }
+    else {
+        infoPlist.NSCameraUsageDescription =
+            cameraPermission || infoPlist.NSCameraUsageDescription || CAMERA_USAGE;
+    }
+    if (microphonePermission === false) {
+        delete infoPlist.NSMicrophoneUsageDescription;
+    }
+    else {
+        infoPlist.NSMicrophoneUsageDescription =
+            microphonePermission || infoPlist.NSMicrophoneUsageDescription || MICROPHONE_USAGE;
+    }
+    return infoPlist;
 }
-exports.setImagePickerManifestActivity = setImagePickerManifestActivity;
-const withImagePickerManifestActivity = (config) => {
-    // This plugin has no ability to remove the activity that it adds.
-    return (0, config_plugins_1.withAndroidManifest)(config, async (config) => {
-        config.modResults = setImagePickerManifestActivity(config.modResults);
+exports.setImagePickerInfoPlist = setImagePickerInfoPlist;
+const withImagePicker = (config, { photosPermission, cameraPermission, microphonePermission } = {}) => {
+    config = (0, config_plugins_1.withInfoPlist)(config, (config) => {
+        config.modResults = setImagePickerInfoPlist(config.modResults, {
+            photosPermission,
+            cameraPermission,
+            microphonePermission,
+        });
         return config;
     });
-};
-const withImagePicker = (config, { photosPermission, cameraPermission, microphonePermission } = {}) => {
-    if (!config.ios)
-        config.ios = {};
-    if (!config.ios.infoPlist)
-        config.ios.infoPlist = {};
-    config.ios.infoPlist.NSPhotoLibraryUsageDescription =
-        photosPermission || config.ios.infoPlist.NSPhotoLibraryUsageDescription || READ_PHOTOS_USAGE;
-    config.ios.infoPlist.NSCameraUsageDescription =
-        cameraPermission || config.ios.infoPlist.NSCameraUsageDescription || CAMERA_USAGE;
-    config.ios.infoPlist.NSMicrophoneUsageDescription =
-        microphonePermission || config.ios.infoPlist.NSMicrophoneUsageDescription || MICROPHONE_USAGE;
-    return (0, config_plugins_1.withPlugins)(config, [
-        [
-            config_plugins_1.AndroidConfig.Permissions.withPermissions,
-            [
-                'android.permission.CAMERA',
-                'android.permission.READ_EXTERNAL_STORAGE',
-                'android.permission.WRITE_EXTERNAL_STORAGE',
-                'android.permission.RECORD_AUDIO',
-            ],
-        ],
-        withImagePickerManifestActivity,
-    ]);
+    return config_plugins_1.AndroidConfig.Permissions.withPermissions(config, [microphonePermission !== false && 'android.permission.RECORD_AUDIO'].filter(Boolean));
 };
 exports.default = (0, config_plugins_1.createRunOncePlugin)(withImagePicker, pkg.name, pkg.version);
