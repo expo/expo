413e991a1ad31dd76af58bf77d6a4be85f3f0ae8
evanbacon <bacon@expo.io>
2022-04-27 11:59:12 -0500

    Update index-test.ts

--- packages/@expo/cli/e2e/__tests__/index-test.ts
+++ packages/@expo/cli/e2e/__tests__/index-test.ts
@@ -29,7 +29,7 @@ it('runs `npx expo --help`', async () => {
           $ npx expo <command>
 
         Available commands
-          config, export, install, login, logout, prebuild, register, start, whoami
+          config, export, install, login, logout, prebuild, register, run:android, start, whoami
 
         Options
           --version, -v   Version number
