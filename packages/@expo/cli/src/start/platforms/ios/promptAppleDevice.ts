1d33cf264a375690315da8bbf78d752503d39cbd
evanbacon <bacon@expo.io>
2022-04-28 17:26:11 -0500

    Update promptAppleDevice.ts

--- packages/@expo/cli/src/start/platforms/ios/promptAppleDevice.ts
+++ packages/@expo/cli/src/start/platforms/ios/promptAppleDevice.ts
@@ -1,5 +1,4 @@
 import chalk from 'chalk';
-import { Log } from '../../../log';
 
 import { createSelectionFilter, promptAsync } from '../../../utils/prompts';
 import { getBestSimulatorAsync } from './getBestSimulator';
