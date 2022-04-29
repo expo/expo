b11d739a71811f13bf68592f39afd331b82337e1
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-04-29 11:42:29 +0200

    Make considerations mandatory [no ci]

--- tools/src/code-review/reviewers/checkMissingChangelogs.ts
+++ tools/src/code-review/reviewers/checkMissingChangelogs.ts
@@ -36,7 +36,7 @@ export default async function ({ pullRequest, diff }: ReviewInput): Promise<Revi
     title: 'Missing changelog entries',
     body: `Your changes should be noted in the changelog. 
       Read [Updating Changelogs](https://github.com/expo/expo/blob/main/guides/contributing/Updating%20Changelogs.md) 
-      guide and consider (it's optional) adding an appropriate entry to the following changelogs:
+      guide and consider adding an appropriate entry to the following changelogs:
       \n${changelogLinks}`,
   };
 }
