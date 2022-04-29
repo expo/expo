822d40ba77ba5c9fa574cfa7bce2970993db6700
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-04-29 12:09:58 +0200

    [tools] Fix expo-bot changelog suggestion display (#17275)
    # Why
    
    expo-bot displayed the first changelog suggestion inline
    
    # How
    
    - Include the bullet list in the newline (was inline before)
    - As suggested, removed the note about changelog update being optional
    - Removed unnecessary line-breaks in the message content
    
    # Test Plan
    
    - Make dummy changes in the PR
    - Ensure the bot now displays them correctly (`et review`)
    - Revert these changes before merge

--- tools/src/code-review/reviewers/checkMissingChangelogs.ts
+++ tools/src/code-review/reviewers/checkMissingChangelogs.ts
@@ -34,9 +34,10 @@ export default async function ({ pullRequest, diff }: ReviewInput): Promise<Revi
   return {
     status: ReviewStatus.WARN,
     title: 'Missing changelog entries',
-    body: `Your changes should be noted in the changelog. 
-      Read [Updating Changelogs](https://github.com/expo/expo/blob/main/guides/contributing/Updating%20Changelogs.md) 
-      guide and consider (it's optional) adding an appropriate entry to the following changelogs: ${changelogLinks}`,
+    body:
+      'Your changes should be noted in the changelog. ' +
+      'Read [Updating Changelogs](https://github.com/expo/expo/blob/main/guides/contributing/Updating%20Changelogs.md) ' +
+      `guide and consider adding an appropriate entry to the following changelogs: \n${changelogLinks}`,
   };
 }
 
