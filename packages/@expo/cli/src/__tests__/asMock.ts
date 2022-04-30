5b8f0c60d8923e4b8253e1dc5875ed9dd7aafd12
Will Schurman <wschurman@gmail.com>
2022-04-29 22:10:15 -0700

    Address comments

--- packages/@expo/cli/src/__tests__/asMock.ts
+++ packages/@expo/cli/src/__tests__/asMock.ts
@@ -1,3 +1,3 @@
-// TODO: replace with jest.mocked when jest 28 is upgraded to
+// TODO: replace with jest.mocked when jest 27+ is upgraded to
 export const asMock = <T extends (...args: any[]) => any>(fn: T): jest.MockedFunction<T> =>
   fn as jest.MockedFunction<T>;
