3e922f4bd4de86d15315a5d7a9462ee4cb9e9795
Cedric van Putten <me@bycedric.com>
2022-04-30 19:30:48 +0200

    [docs] Update jest config to use next transformer

--- docs/jest.config.js
+++ docs/jest.config.js
@@ -1,9 +1,16 @@
+const nextJest = require('next/jest');
+
 /** @type {import('@jest/types').Config.InitialOptions} */
-module.exports = {
+const jestConfig = {
   displayName: 'docs',
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.(js|ts|tsx)'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', './jest.setup.js'],
   clearMocks: true,
   coverageDirectory: '<rootDir>/coverage',
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/$1',
+  },
 };
+
+module.exports = nextJest({ dir: './' })(jestConfig);
