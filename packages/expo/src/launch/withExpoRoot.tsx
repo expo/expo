c9abe5d86dfe003f76576c1d8b70f0df4a688301
Kudo Chien <kudo@expo.dev>
2022-05-04 00:54:33 +0800

    [expo][keep-awake] suppress warning

--- packages/expo/src/launch/withExpoRoot.tsx
+++ packages/expo/src/launch/withExpoRoot.tsx
@@ -5,13 +5,13 @@ import { InitialProps } from './withExpoRoot.types';
 
 // This hook can be optionally imported because __DEV__ never changes during runtime.
 // Using __DEV__ like this enables tree shaking to remove the hook in production.
-let useDevKeepAwake: (tag?: string) => void = () => {};
+let useDevKeepAwake: (tag?: string) => void;
 
 if (__DEV__) {
   try {
     // Optionally import expo-keep-awake
-    const { useKeepAwake } = require('expo-keep-awake');
-    useDevKeepAwake = useKeepAwake;
+    const { useKeepAwake, ExpoKeepAwakeTag } = require('expo-keep-awake');
+    useDevKeepAwake = () => useKeepAwake(ExpoKeepAwakeTag, { suppressDeactivateWarnings: true });
   } catch {}
 }
 
