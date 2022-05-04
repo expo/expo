c5a3e80dc07d525003df6ba30d546b0368ffbbed
Kudo Chien <kudo@expo.dev>
2022-05-04 20:59:51 +0800

    [android] Fix more FLAG_MUTABLE exception in PendingIntent

--- android/versioned-abis/expoview-abi44_0_0/src/main/java/abi44_0_0/expo/modules/appauth/AppAuthModule.java
+++ android/versioned-abis/expoview-abi44_0_0/src/main/java/abi44_0_0/expo/modules/appauth/AppAuthModule.java
@@ -5,6 +5,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+
 import androidx.annotation.Nullable;
 
 import net.openid.appauth.AppAuthConfiguration;
@@ -327,7 +329,9 @@ public class AppAuthModule extends ExportedModule {
 
       AuthorizationRequest authorizationRequest = authReqBuilder.build();
       int hash = authorizationRequest.hashCode();
-      PendingIntent pendingIntent = PendingIntent.getActivity(activity, hash, postAuthIntent, 0);
+      // We're defaulting to the behaviour prior API 31 (mutable) even though Android recommends immutability
+      int mutableFlag = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? PendingIntent.FLAG_MUTABLE : 0;
+      PendingIntent pendingIntent = PendingIntent.getActivity(activity, hash, postAuthIntent, mutableFlag);
       AuthorizationService authorizationService = new AuthorizationService(activity, authConfig);
       authorizationService.performAuthorizationRequest(authorizationRequest, pendingIntent, pendingIntent);
     } catch (Exception e) {
