30c1fe4022ff8eb21573ec616a489cb4a961c798
Kudo Chien <kudo@expo.dev>
2022-05-05 02:02:51 +0800

    rollback accidental copied change which break release build

--- packages/expo/build/launch/withExpoRoot.js
+++ packages/expo/build/launch/withExpoRoot.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { attachRecoveredProps } from './RecoveryProps';
 // This hook can be optionally imported because __DEV__ never changes during runtime.
 // Using __DEV__ like this enables tree shaking to remove the hook in production.
-let useDevKeepAwake;
+let useDevKeepAwake = () => { };
 if (__DEV__) {
     try {
         // Optionally import expo-keep-awake
--- packages/expo/src/launch/withExpoRoot.tsx
+++ packages/expo/src/launch/withExpoRoot.tsx
@@ -5,7 +5,7 @@ import { InitialProps } from './withExpoRoot.types';
 
 // This hook can be optionally imported because __DEV__ never changes during runtime.
 // Using __DEV__ like this enables tree shaking to remove the hook in production.
-let useDevKeepAwake: (tag?: string) => void;
+let useDevKeepAwake: (tag?: string) => void = () => {};
 
 if (__DEV__) {
   try {
