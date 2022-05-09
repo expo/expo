1e2fd3c0990292cd143b3b4c0ff7fe0fb1ea88dc
andy <40680668+ajsmth@users.noreply.github.com>
2022-05-09 11:18:37 -0700

    [stories] reorganize package to include components and metro config i… (#17413)

---
+++ packages/expo-stories/build/clients/app/screens/SelectedStoryDetails.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+import { RouteProp } from '@react-navigation/native';
+import { RootStackParamList } from '../types';
+declare type SelectedStoriesDetailProps = {
+    route: RouteProp<RootStackParamList, 'Stories Detail'>;
+};
+export declare function SelectedStoriesDetail({ route }: SelectedStoriesDetailProps): JSX.Element;
+export {};
+//# sourceMappingURL=SelectedStoryDetails.d.ts.map
