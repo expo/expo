aee7fa0e0ee36d87eb7d237816540117c79d8291
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-05-12 10:04:10 +0200

    [ios] Add implementation of the native logger (#17457)

--- packages/expo-modules-core/ios/Swift/Exceptions/CodedError.swift
+++ packages/expo-modules-core/ios/Swift/Exceptions/CodedError.swift
@@ -3,7 +3,7 @@ import Foundation
 /**
  A protocol for errors specyfing its `code` and providing the `description`.
  */
-public protocol CodedError: Error {
+public protocol CodedError: Error, CustomStringConvertible {
   var code: String { get }
   var description: String { get }
 }
