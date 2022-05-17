cb2a0081552ca20f17485429deb26995b8f48394
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-17 15:53:36 +0200

    [emc][Android] Deprecate `ActivityProvider` altogether

--- packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ActivityProvider.java
+++ packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ActivityProvider.java
@@ -2,6 +2,14 @@ package expo.modules.core.interfaces;
 
 import android.app.Activity;
 
+import expo.modules.kotlin.providers.AppCompatActivityProvider;
+
+/**
+ * @deprecated When using `expo-modules-core` use {@link AppCompatActivityProvider}
+ */
 public interface ActivityProvider {
+  /**
+   * @deprecated Use {@link AppCompatActivityProvider#getAppCompatActivity()}
+   */
   Activity getCurrentActivity();
 }
