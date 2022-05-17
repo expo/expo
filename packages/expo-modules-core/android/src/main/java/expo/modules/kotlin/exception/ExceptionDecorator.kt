a4294dc08e8a56b011184f1a2917cd95e56787ba
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-17 16:10:57 +0200

    [emc][Android] Catch and properly handle `InvocationTargetException` (coming from coroutines) in our exception decoration pipeline

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/exception/ExceptionDecorator.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/exception/ExceptionDecorator.kt
@@ -1,5 +1,7 @@
 package expo.modules.kotlin.exception
 
+import java.lang.reflect.InvocationTargetException
+
 internal inline fun <T> exceptionDecorator(decoratorBlock: (e: CodedException) -> Throwable, block: () -> T): T {
   return try {
     block()
@@ -7,6 +9,9 @@ internal inline fun <T> exceptionDecorator(decoratorBlock: (e: CodedException) -
     throw decoratorBlock(e)
   } catch (e: expo.modules.core.errors.CodedException) {
     throw decoratorBlock(CodedException(e.code, e.message, e.cause))
+  } catch (e: InvocationTargetException) {
+    val throwable = e.targetException
+    throw decoratorBlock(if (throwable is CodedException) throwable else UnexpectedException(throwable) )
   } catch (e: Throwable) {
     throw decoratorBlock(UnexpectedException(e))
   }
