08a167192010e7128ed3913cbdb944a08f004a9c
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-17 16:16:07 +0200

    [image-picker][Android] Refactor file providers
    Possibly to removed altogether.

--- packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/fileproviders/CacheFileProvider.kt
+++ packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/fileproviders/CacheFileProvider.kt
@@ -1,14 +1,11 @@
 package expo.modules.imagepicker.fileproviders
 
-import expo.modules.imagepicker.ImagePickerConstants
-import expo.modules.core.utilities.FileUtilities
+import expo.modules.imagepicker.createOutputFile
 import java.io.File
 
 class CacheFileProvider(
   private val cacheFolder: File,
   private val extension: String
 ) : FileProvider {
-  override fun generateFile() = File(
-    FileUtilities.generateOutputPath(cacheFolder, ImagePickerConstants.CACHE_DIR_NAME, extension)
-  )
+  override fun generateFile() = createOutputFile(cacheFolder, extension)
 }
--- packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/fileproviders/FileProvider.kt
+++ packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/fileproviders/FileProvider.kt
@@ -3,7 +3,13 @@ package expo.modules.imagepicker.fileproviders
 import java.io.File
 import java.io.IOException
 
+/**
+ * Interface that allows to generate a new file to store a picked media asset as the original
+ * file with the media asset might not be readable/editable by the user's application.
+ */
 interface FileProvider {
-  @Throws(IOException::class)
+  /**
+   * Generates a new file to store picked media asset.
+   */
   fun generateFile(): File
 }
