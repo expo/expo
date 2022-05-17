a0a41c2fec3cab0bdfb5cf29d34e95568140b136
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-17 16:12:33 +0200

    [emc][Android] Force olf permissions interface accept new Sweet API Promise

--- packages/expo-modules-core/android/src/main/java/expo/modules/interfaces/permissions/Permissions.java
+++ packages/expo-modules-core/android/src/main/java/expo/modules/interfaces/permissions/Permissions.java
@@ -14,12 +14,41 @@ public interface Permissions {
     permissionsManager.getPermissionsWithPromise(promise, permissions);
   }
 
+  /**
+   * Compatibility method that accepts expo.modules.kotlin.Promise, but forward the logic to the other method
+   */
+  static void getPermissionsWithPermissionsManager(
+      @Nullable Permissions permissionsManager,
+      @NonNull final expo.modules.kotlin.Promise promise,
+      @NonNull String... permissions
+  ) {
+    getPermissionsWithPermissionsManager(permissionsManager, new Promise() {
+      @Override public void resolve(Object value) { promise.resolve(value); }
+      @Override public void reject(String c, String m, Throwable e) { promise.reject(c, m, e); }
+    }, permissions);
+  }
+
+
   static void askForPermissionsWithPermissionsManager(@Nullable Permissions permissionsManager, @NonNull final Promise promise, @NonNull String... permissions) {
     if (permissionsManager == null) {
       promise.reject("E_NO_PERMISSIONS", "Permissions module is null. Are you sure all the installed Expo modules are properly linked?");
       return;
     }
     permissionsManager.askForPermissionsWithPromise(promise, permissions);
+  }
+
+  /**
+   * Compatibility method that accepts expo.modules.kotlin.Promise, but forward the logic to the other method
+   */
+  static void askForPermissionsWithPermissionsManager(
+      @Nullable Permissions permissionsManager,
+      @NonNull final expo.modules.kotlin.Promise promise,
+      @NonNull String... permissions
+  ) {
+    askForPermissionsWithPermissionsManager(permissionsManager, new Promise() {
+      @Override public void resolve(Object value) { promise.resolve(value); }
+      @Override public void reject(String c, String m, Throwable e) { promise.reject(c, m, e); }
+    }, permissions);
   }
 
   void getPermissionsWithPromise(final Promise promise, String... permissions);
