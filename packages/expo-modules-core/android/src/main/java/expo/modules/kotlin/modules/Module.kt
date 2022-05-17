c134d984c642cb69d4e57e55ae8009aec648e2d3
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-17 15:54:22 +0200

    [emc][Android] Make `Module` implement `AppContextProvider`, `ContextProvider` and `CoroutineScopeProvider`
    Each module should have access to `AppContext`, `Context` or `CoroutineScope` by default.

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/modules/Module.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/modules/Module.kt
@@ -1,24 +1,47 @@
 package expo.modules.kotlin.modules
 
+import android.content.Context
 import android.os.Bundle
 import expo.modules.core.errors.ModuleDestroyedException
 import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.providers.AppContextProvider
+import expo.modules.kotlin.providers.ContextProvider
+import expo.modules.kotlin.providers.CoroutineScopeProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
+import java.lang.IllegalStateException
 
-abstract class Module {
+abstract class Module: AppContextProvider, ContextProvider, CoroutineScopeProvider {
+
+// region AppContextProvider
+
   @Suppress("PropertyName")
   internal var _appContext: AppContext? = null
 
-  private val moduleEventEmitter by lazy { appContext.eventEmitter(this) }
-
-  val appContext: AppContext
+  override val appContext: AppContext
     get() = requireNotNull(_appContext) { "The module wasn't created! You can't access the app context." }
+
+// endregion
+
+// region ContextProvider
+
+  override val context: Context
+    get() = requireNotNull(appContext.reactContext) { "React Application Context is null. You can't access the context at this point." }
+
+// endregion
+
+// region CoroutineScopeProvider
 
   @Suppress("PropertyName")
   @PublishedApi
   internal lateinit var coroutineScopeDelegate: Lazy<CoroutineScope>
-  val coroutineScope get() = coroutineScopeDelegate.value
+
+  override val coroutineScope: CoroutineScope
+    get() = coroutineScopeDelegate.value
+
+// endregion
+
+  private val moduleEventEmitter by lazy { appContext.eventEmitter(this) }
 
   fun sendEvent(name: String, body: Bundle?) {
     moduleEventEmitter?.emit(name, body)
@@ -28,7 +51,11 @@ abstract class Module {
 
   internal fun cleanUp() {
     if (coroutineScopeDelegate.isInitialized()) {
-      coroutineScope.cancel(ModuleDestroyedException())
+      try {
+        coroutineScope.cancel(ModuleDestroyedException())
+      } catch (cause: IllegalStateException) {
+        // cancelling coroutine without an active job would cause IllegalStateException
+      }
     }
   }
 }
