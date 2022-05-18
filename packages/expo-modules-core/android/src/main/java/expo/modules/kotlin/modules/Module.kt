e52616fb2e74cbc36064c02b3fcd971975d8a39a
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-18 12:24:39 +0200

    [emc][Android] Make `Module` implement `ReactApplicationContextProvider`

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/modules/Module.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/modules/Module.kt
@@ -1,13 +1,15 @@
 package expo.modules.kotlin.modules
 
 import android.os.Bundle
+import com.facebook.react.bridge.ReactApplicationContext
 import expo.modules.core.errors.ModuleDestroyedException
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.providers.AppContextProvider
+import expo.modules.kotlin.providers.ReactApplicationContextProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 
-abstract class Module : AppContextProvider {
+abstract class Module : AppContextProvider, ReactApplicationContextProvider {
 
   // region AppContextProvider
 
@@ -16,6 +18,13 @@ abstract class Module : AppContextProvider {
 
   override val appContext: AppContext
     get() = requireNotNull(_appContext) { "The module wasn't created! You can't access the app context." }
+
+  // endregion
+
+  // region ReactApplicationContextProvider
+
+  override val reactApplicationContext: ReactApplicationContext
+    get() = requireNotNull(appContext.reactApplicationContext) { "React Application Context is null. You can't access the context at this point." }
 
   // endregion
 
