3b8e980d0558c8f151d5c7a65ecb4463f1f973b8
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-18 11:47:58 +0200

    [emc][Android] Update the `appContext` description

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/providers/AppContextProvider.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/providers/AppContextProvider.kt
@@ -3,11 +3,12 @@ package expo.modules.kotlin.providers
 import expo.modules.kotlin.AppContext
 
 /**
- * Provider that allows us to access [AppContext] and possibly other underlying things like [AppContext.reactContext]
+ * Provider that allows accessing [AppContext] and all it's public parts (e.g. [AppContext.reactContext]).
  */
 interface AppContextProvider {
   /**
-   * Non nullable [AppContext] reference.
+   * [AppContext] reference. If it's not possible to access the [AppContext], because it's null
+   * then it's an invalid situation and should result in throwing descriptive error.
    */
   val appContext: AppContext
 }
