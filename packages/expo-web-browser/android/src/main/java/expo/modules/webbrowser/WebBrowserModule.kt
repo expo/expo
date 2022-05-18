cb5792c54fc8c39b819f4aae24c70d84a170eb60
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-05-18 08:57:12 +0200

    [web-browser][android] Rewrite module to Sweet API (#17454)
    - Created `ModuleDefinition` for `WebBrowserModule`, moved methods there, and updated `expo-module.config.json` to take this class into account
    - Replaced `ReadableArguments` with `OpenBrowserOptions` record for `openBrowserAsync`
    - Made all exceptions inherit from Sweet `CodedException` (in favor of legacy unimodules counterpart). Kept error codes for backwards compatibility.
    - Replaced `CustomTabsActivitiesHelper` and `CustomTabsConnectionHelper` internal unimodules with plain class instantiations.
      Merged their interface+implementation:
      `CustomTabsActivitiesHelper (interface)` + `InternalCustomTabsActivitiesHelper (impl)` -> just `CustomTabsActivitiesHelper (impl)`
    - Migrated native unit tests
    - Updated `ExperiencePackagePicker` to use expo-module
    - Deleted the `WebBrowserPackage` from both `ExperiencePackagePicker` and `HomeActivity`.
       > It was listed as a package to be added to modules used in Home, but from what I discovered, sweet expo-modules are always loaded.

--- packages/expo-web-browser/android/src/main/java/expo/modules/webbrowser/WebBrowserModule.kt
+++ packages/expo-web-browser/android/src/main/java/expo/modules/webbrowser/WebBrowserModule.kt
@@ -1,100 +1,73 @@
 package expo.modules.webbrowser
 
-import android.content.Context
-import expo.modules.core.ExportedModule
-import expo.modules.core.interfaces.ExpoMethod
-import expo.modules.webbrowser.error.NoPreferredPackageFound
 import expo.modules.core.errors.CurrentActivityNotFoundException
-import expo.modules.webbrowser.error.PackageManagerNotFoundException
-import expo.modules.core.arguments.ReadableArguments
 import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.browser.customtabs.CustomTabsIntent
-import expo.modules.core.ModuleRegistry
-import expo.modules.core.Promise
+import androidx.core.os.bundleOf
 import expo.modules.core.utilities.ifNull
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
 import java.lang.IllegalArgumentException
 
-private const val BROWSER_PACKAGE_KEY = "browserPackage"
 private const val SERVICE_PACKAGE_KEY = "servicePackage"
 private const val BROWSER_PACKAGES_KEY = "browserPackages"
 private const val SERVICE_PACKAGES_KEY = "servicePackages"
 private const val PREFERRED_BROWSER_PACKAGE = "preferredBrowserPackage"
 private const val DEFAULT_BROWSER_PACKAGE = "defaultBrowserPackage"
-private const val SHOW_IN_RECENTS = "showInRecents"
-private const val CREATE_TASK = "createTask"
-private const val DEFAULT_SHARE_MENU_ITEM = "enableDefaultShareMenuItem"
-private const val TOOLBAR_COLOR_KEY = "toolbarColor"
-private const val SECONDARY_TOOLBAR_COLOR_KEY = "secondaryToolbarColor"
-private const val ERROR_CODE = "EXWebBrowser"
-private const val TAG = "ExpoWebBrowser"
-private const val SHOW_TITLE_KEY = "showTitle"
-private const val ENABLE_BAR_COLLAPSING_KEY = "enableBarCollapsing"
-private const val NO_PREFERRED_PACKAGE_MSG = "Cannot determine preferred package without satisfying it."
 
-class WebBrowserModule(context: Context?) : ExportedModule(context) {
-  private lateinit var customTabsResolver: CustomTabsActivitiesHelper
-  private lateinit var connectionHelper: CustomTabsConnectionHelper
+private const val MODULE_NAME = "ExpoWebBrowser"
 
-  override fun getName() = TAG
+class WebBrowserModule : Module() {
+  override fun definition() = ModuleDefinition {
+    Name(MODULE_NAME)
 
-  override fun onCreate(moduleRegistry: ModuleRegistry) {
-    customTabsResolver = moduleRegistry.getModule(CustomTabsActivitiesHelper::class.java)
-    connectionHelper = moduleRegistry.getModule(CustomTabsConnectionHelper::class.java)
-  }
+    OnCreate {
+      customTabsResolver = CustomTabsActivitiesHelper(appContext.activityProvider)
+      connectionHelper = CustomTabsConnectionHelper(
+        requireNotNull(appContext.reactContext) {
+          "Cannot initialize WebBrowser, ReactContext is null"
+        }
+      )
+    }
 
-  @ExpoMethod
-  fun warmUpAsync(packageName: String?, promise: Promise) {
-    try {
+    OnActivityDestroys {
+      connectionHelper.destroy()
+    }
+
+    AsyncFunction("warmUpAsync") { packageName: String? ->
       val resolvedPackageName = givenOrPreferredPackageName(packageName)
       connectionHelper.warmUp(resolvedPackageName)
-      val result = Bundle().apply {
-        putString(SERVICE_PACKAGE_KEY, resolvedPackageName)
-      }
-      promise.resolve(result)
-    } catch (ex: NoPreferredPackageFound) {
-      promise.reject(ex)
+      return@AsyncFunction bundleOf(
+        SERVICE_PACKAGE_KEY to resolvedPackageName
+      )
     }
-  }
 
-  @ExpoMethod
-  fun coolDownAsync(packageName: String?, promise: Promise) {
-    try {
+    AsyncFunction("coolDownAsync") { packageName: String? ->
       val resolvedPackageName = givenOrPreferredPackageName(packageName)
-      if (connectionHelper.coolDown(resolvedPackageName)) {
-        val result = Bundle().apply {
-          putString(SERVICE_PACKAGE_KEY, resolvedPackageName)
-        }
-        promise.resolve(result)
+      val result = if (connectionHelper.coolDown(resolvedPackageName)) {
+        bundleOf(
+          SERVICE_PACKAGE_KEY to resolvedPackageName
+        )
       } else {
-        promise.resolve(Bundle())
+        Bundle()
       }
-    } catch (ex: NoPreferredPackageFound) {
-      promise.reject(ex)
+      return@AsyncFunction result
     }
-  }
 
-  @ExpoMethod
-  fun mayInitWithUrlAsync(url: String?, packageName: String?, promise: Promise) {
-    try {
+    AsyncFunction("mayInitWithUrlAsync") { url: String, packageName: String? ->
       val resolvedPackageName = givenOrPreferredPackageName(packageName)
-      // TODO (barthap): This might throw NPE when url is null
       connectionHelper.mayInitWithUrl(resolvedPackageName, Uri.parse(url))
-      val result = Bundle().apply {
-        putString(SERVICE_PACKAGE_KEY, resolvedPackageName)
-      }
-      promise.resolve(result)
-    } catch (ex: NoPreferredPackageFound) {
-      promise.reject(ex)
+      return@AsyncFunction bundleOf(
+        SERVICE_PACKAGE_KEY to resolvedPackageName
+      )
     }
-  }
 
-  @ExpoMethod
-  fun getCustomTabsSupportingBrowsersAsync(promise: Promise) {
-    try {
+    // throws CurrentActivityNotFoundException
+    AsyncFunction("getCustomTabsSupportingBrowsersAsync") {
       val activities = customTabsResolver.customTabsResolvingActivities
       val services = customTabsResolver.customTabsResolvingServices
       val preferredPackage = customTabsResolver.getPreferredCustomTabsResolvingActivity(activities)
@@ -103,63 +76,41 @@ class WebBrowserModule(context: Context?) : ExportedModule(context) {
       // It might happen, that default activity does not support Chrome Tabs. Then it will be ResolvingActivity and we don't want to return it as a result.
       val defaultCustomTabsPackage: String? = defaultPackage.takeIf { activities.contains(it) }
 
-      val result = Bundle().apply {
+      return@AsyncFunction Bundle().apply {
         putStringArrayList(BROWSER_PACKAGES_KEY, activities)
         putStringArrayList(SERVICE_PACKAGES_KEY, services)
         putString(PREFERRED_BROWSER_PACKAGE, preferredPackage)
         putString(DEFAULT_BROWSER_PACKAGE, defaultCustomTabsPackage)
       }
-      promise.resolve(result)
-    } catch (ex: CurrentActivityNotFoundException) {
-      promise.reject(ex)
-    } catch (ex: PackageManagerNotFoundException) {
-      promise.reject(ex)
     }
-  }
 
-  /**
-   * @param url Url to be opened by WebBrowser
-   * @param arguments Required arguments are:
-   * toolbarColor: String;
-   * browserPackage: String;
-   * enableBarCollapsing: Boolean;
-   * showTitle: Boolean;
-   * enableDefaultShareMenuItem: Boolean;
-   * showInRecents: Boolean;
-   * @param promise
-   */
-  @ExpoMethod
-  fun openBrowserAsync(url: String?, arguments: ReadableArguments, promise: Promise) {
-    val intent = createCustomTabsIntent(arguments)
-    intent.data = Uri.parse(url)
-    try {
-      if (customTabsResolver.canResolveIntent(intent)) {
-        customTabsResolver.startCustomTabs(intent)
-        val result = Bundle().apply {
-          putString("type", "opened")
-        }
-        promise.resolve(result)
-      } else {
-        promise.reject(ERROR_CODE, "No matching activity!")
+    // throws CurrentActivityNotFoundException
+    AsyncFunction("openBrowserAsync") { url: String, options: OpenBrowserOptions ->
+      val intent = createCustomTabsIntent(options).apply {
+        data = Uri.parse(url)
       }
-    } catch (ex: CurrentActivityNotFoundException) {
-      promise.reject(ex)
-    } catch (ex: PackageManagerNotFoundException) {
-      promise.reject(ex)
+
+      if (!customTabsResolver.canResolveIntent(intent)) {
+        throw NoMatchingActivityException()
+      }
+
+      customTabsResolver.startCustomTabs(intent)
+
+      return@AsyncFunction bundleOf(
+        "type" to "opened"
+      )
     }
   }
 
-  private fun createCustomTabsIntent(arguments: ReadableArguments): Intent {
-    val color = arguments.getString(TOOLBAR_COLOR_KEY)
-    val secondaryColor = arguments.getString(SECONDARY_TOOLBAR_COLOR_KEY)
-    val packageName = arguments.getString(BROWSER_PACKAGE_KEY)
-    val showTitle = arguments.getBoolean(SHOW_TITLE_KEY, false)
-    val addDefaultShareMenuItem = arguments.getBoolean(DEFAULT_SHARE_MENU_ITEM, false)
-    val enableBarCollapsing = arguments.getBoolean(ENABLE_BAR_COLLAPSING_KEY, false)
-    val shouldCreateTask = arguments.getBoolean(CREATE_TASK, true)
-    val showInRecents = arguments.getBoolean(SHOW_IN_RECENTS, false)
+  // these must be `internal` to be able to be injected in tests
+  internal lateinit var customTabsResolver: CustomTabsActivitiesHelper
+  internal lateinit var connectionHelper: CustomTabsConnectionHelper
 
+  private fun createCustomTabsIntent(options: OpenBrowserOptions): Intent {
     val builder = CustomTabsIntent.Builder()
+    val color = options.toolbarColor
+    val secondaryColor = options.secondaryToolbarColor
+
     try {
       if (!TextUtils.isEmpty(color)) {
         val intColor = Color.parseColor(color)
@@ -171,42 +122,49 @@ class WebBrowserModule(context: Context?) : ExportedModule(context) {
       }
     } catch (ignored: IllegalArgumentException) {
     }
-    builder.setShowTitle(showTitle)
-    if (addDefaultShareMenuItem) {
+
+    builder.setShowTitle(options.showTitle)
+
+    if (options.enableDefaultShareMenuItem) {
       builder.addDefaultShareMenuItem()
     }
 
-    val intent = builder.build().intent
+    return builder.build().intent.apply {
+      // We cannot use the builder's method enableUrlBarHiding, because there is
+      // no corresponding disable method and some browsers enable it by default.
+      putExtra(CustomTabsIntent.EXTRA_ENABLE_URLBAR_HIDING, options.enableBarCollapsing)
 
-    // We cannot use builder's method enableUrlBarHiding, because there is no corresponding disable method and some browsers enables it by default.
-    intent.putExtra(CustomTabsIntent.EXTRA_ENABLE_URLBAR_HIDING, enableBarCollapsing)
-    if (!TextUtils.isEmpty(packageName)) {
-      intent.setPackage(packageName)
-    }
+      val packageName = options.browserPackage
+      if (!TextUtils.isEmpty(packageName)) {
+        setPackage(packageName)
+      }
 
-    if (shouldCreateTask) {
-      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-      if (!showInRecents) {
-        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+      if (options.shouldCreateTask) {
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        if (!options.showInRecents) {
+          addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+          addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+        }
       }
     }
-    return intent
   }
 
-  @Throws(NoPreferredPackageFound::class)
+  /**
+   * @throws NoPreferredPackageFound
+   */
   private fun givenOrPreferredPackageName(packageName: String?): String {
     val resolvedPackageName: String? = try {
       packageName?.takeIf { it.isNotEmpty() }.ifNull {
         customTabsResolver.getPreferredCustomTabsResolvingActivity(null)
       }
     } catch (ex: CurrentActivityNotFoundException) {
-      throw NoPreferredPackageFound(NO_PREFERRED_PACKAGE_MSG)
+      throw NoPreferredPackageFound()
     } catch (ex: PackageManagerNotFoundException) {
-      throw NoPreferredPackageFound(NO_PREFERRED_PACKAGE_MSG)
+      throw NoPreferredPackageFound()
     }
 
     return resolvedPackageName?.takeIf { it.isNotEmpty() }
-      ?: throw NoPreferredPackageFound(NO_PREFERRED_PACKAGE_MSG)
+      ?: throw NoPreferredPackageFound()
   }
 }
