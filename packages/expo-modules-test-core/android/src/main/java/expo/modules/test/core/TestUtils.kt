c680c1844124ac7360a56bbb430e75fe849afa0e
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-05-19 21:32:08 +0200

    Clean up test utils, leverage contracts

--- packages/expo-modules-test-core/android/src/main/java/expo/modules/test/core/TestUtils.kt
+++ packages/expo-modules-test-core/android/src/main/java/expo/modules/test/core/TestUtils.kt
@@ -3,51 +3,45 @@ package expo.modules.test.core
 import expo.modules.kotlin.exception.CodedException
 import org.junit.Assert
 import java.lang.reflect.UndeclaredThrowableException
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
-fun assertResolved(promise: PromiseMock) {
-  Assert.assertEquals(PromiseState.RESOLVED, promise.state)
-}
-
-fun assertRejected(promise: PromiseMock) {
-  Assert.assertEquals(PromiseState.REJECTED, promise.state)
-}
-
-inline fun <reified ResolveType> promiseResolved(promise: PromiseMock, with: (ResolveType) -> Unit) {
-  assertResolved(promise)
-  Assert.assertTrue(
-    "Promise resolved with incorrect type: ${ResolveType::class.simpleName}",
-    promise.resolveValue is ResolveType
-  )
-  with(promise.resolveValue as ResolveType)
-}
-
-fun promiseRejected(promise: PromiseMock, with: (PromiseMock) -> Unit) {
-  assertRejected(promise)
-  with(promise)
-}
-
-fun assertRejectedWithCode(promise: PromiseMock, rejectCode: String) {
-  promiseRejected(promise) {
-    Assert.assertTrue("Promise has no rejection code", it.rejectCodeSet)
-    Assert.assertEquals(it.rejectCode, rejectCode)
+/**
+ * Asserts that provided [exception] is a [CodedException] and
+ */
+@OptIn(ExperimentalContracts::class)
+fun assertCodedException(exception: Throwable?) {
+  contract {
+    returns() implies (exception is CodedException)
   }
-}
 
-inline fun assertCodedException(exception: Throwable?, block: (exception: CodedException) -> Unit = {}) {
   Assert.assertNotNull("Expected exception, received null", exception)
 
   if (exception is UndeclaredThrowableException) {
     Assert.fail(
-      "Expected CodedException, got UndeclaredThrowableException. " +
-        "Did you forget to add '@Throws' annotations to module test interface methods?"
+            "Expected CodedException, got UndeclaredThrowableException. " +
+                    "Did you forget to add '@Throws' annotations to module test interface methods?"
     )
   }
 
   if (exception !is CodedException) {
     Assert.fail(
-      "Expected CodedException, got ${exception!!::class.simpleName}. " +
-        "Full stack trace:\n${exception.stackTraceToString()}"
+            "Expected CodedException, got ${exception!!::class.simpleName}. " +
+                    "Full stack trace:\n${exception.stackTraceToString()}"
     )
   }
-  block(exception as CodedException)
+}
+
+/**
+ * Asserts that provided [exception] is a [CodedException] and then executes a block with
+ * the [exception] as an argument
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun assertCodedException(exception: Throwable?, block: (exception: CodedException) -> Unit) {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
+  assertCodedException(exception)
+  block(exception)
 }
