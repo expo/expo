555d5fb5a6e2982e165af5402ef2766d2ad7c14b
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-19 16:51:16 +0200

    [emc][Android] Add missing `launchForActivityResult` to the `AppContextActivityResultCaller`

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/activityresult/AppContextActivityResultCaller.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/activityresult/AppContextActivityResultCaller.kt
@@ -5,6 +5,8 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.registerForActivityResult
 import androidx.annotation.MainThread
+import androidx.lifecycle.Lifecycle
+import expo.modules.kotlin.AppContextActivityResult
 
 /**
  * This interface is direct based on [ActivityResultCaller], but due to incompatibility of ReactNative
@@ -17,11 +19,23 @@ interface AppContextActivityResultCaller {
    * that additionally takes an input right away, producing a launcher that doesn't take any
    * additional input when called.
    *
-   * @see ActivityResultCaller.registerForActivityResult
+   * @see ActivityResultCaller.registerForActivityResult from `androidx.activity:activity-ktx:1.4.0`.
    */
   @MainThread
   fun <I, O> registerForActivityResult(
     contract: ActivityResultContract<I, O>,
     callback: AppContextActivityResultCallback<O>
   ): ActivityResultLauncher<I>
+
+  /**
+   * Suspend version of [registerForActivityResult], but with the default [AppContextActivityResultCallback]
+   * that should simply forward the result as a result of this suspending function.
+   *
+   *
+   * @see ActivityResultCaller.registerForActivityResult
+   */
+  @MainThread
+  suspend fun <O> launchForActivityResult(
+    contract: ActivityResultContract<Any?, O>
+  ): AppContextActivityResult<O>
 }
