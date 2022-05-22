44dd5deb65aa0383f715290ba712fbc5c7b12db1
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-05-22 21:35:22 +0200

    more tests

--- packages/expo-clipboard/ios/Tests/ClipboardModuleSpec.swift
+++ packages/expo-clipboard/ios/Tests/ClipboardModuleSpec.swift
@@ -3,6 +3,7 @@ import ExpoModulesTestCore
 @testable import ExpoModulesCore
 @testable import ExpoClipboard
 import UIKit
+import MobileCoreServices
 
 class MockUIPasteboard: UIPasteboard {
   var _items: [String : Any] = [:]
@@ -39,6 +40,24 @@ class MockUIPasteboard: UIPasteboard {
   
   override var hasURLs: Bool
   { get { return _items["url"] != nil } }
+  
+  override func value(forPasteboardType pasteboardType: String) -> Any? {
+    return _items[pasteboardType]
+  }
+  
+  override func data(forPasteboardType pasteboardType: String) -> Data? {
+    return _items[pasteboardType] as? Data
+  }
+  
+  override func setItems(_ items: [[String : Any]], options: [UIPasteboard.OptionsKey : Any] = [:]) {
+    self.items = items
+  }
+  
+  override func contains(pasteboardTypes: [String]) -> Bool {
+    return _items.contains(where: { (key, _) in
+      pasteboardTypes.contains(key)
+    })
+  }
 }
 
 extension UIPasteboard {
@@ -54,6 +73,11 @@ class ClipboardModuleSpec: ExpoSpec {
   override func spec() {
     let appContext = AppContext()
     let holder = ModuleHolder(appContext: appContext, module: ClipboardModule(appContext: appContext))
+    /**
+     * Base64-encoded PNG data - it's a 1x1 image (a black pixel), 8 bit depth
+     * the data has length of 68 bytes after decoding
+     */
+    let rawPngData = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
     
     func testModuleFunction<T>(_ functionName: String, args: [Any], _ block: @escaping (T?) -> ()) {
       waitUntil { done in
@@ -92,6 +116,21 @@ class ClipboardModuleSpec: ExpoSpec {
           expect(result) == expectedString
         }
       }
+      
+      it("returns html from the clipboard") {
+        let expectedHtml = "<p>hello</p>"
+        UIPasteboard.general.items = [[
+          kUTTypeHTML as String: expectedHtml
+        ]]
+
+//        let options = GetStringOptions(preferredFormat: .plainText)
+        let options = [
+          "preferredFormat": "html"
+        ]
+        testModuleFunction("getStringAsync", args: [options]) { (result: String?) in
+          expect(result) == expectedHtml
+        }
+      }
 
       it("returns empty string if no text in clipboard") {
         UIPasteboard.general.items = []
@@ -102,6 +141,35 @@ class ClipboardModuleSpec: ExpoSpec {
         ]
         testModuleFunction("getStringAsync", args: [options]) { (result: String?) in
           expect(result) == ""
+        }
+      }
+    }
+    
+    describe("setStringAsync") {
+      it("copies string to clipboard") {
+        let expectedString = "hello"
+        let options = [
+          "inputFormat": "plainText"
+        ]
+        testModuleFunction("setStringAsync", args: [expectedString, options]) { (result: Bool?) in
+          expect(result) == true
+          expect(UIPasteboard.general.hasStrings) == true
+          expect(UIPasteboard.general.string) == expectedString
+        }
+      }
+      
+      it("copies HTML to clipboard") {
+        let expectedHtml = "<p>hello</p>"
+        let options = [
+          "inputFormat": "html"
+        ]
+        testModuleFunction("setStringAsync", args: [expectedHtml, options]) { (result: Bool?) in
+          let mockPasteboard = UIPasteboard.StaticVars.mockPastebaord
+          expect(result) == true
+          expect(mockPasteboard._items.count) == 3
+          expect(mockPasteboard._items[kUTTypeRTF as String]).notTo(beNil())
+          expect(mockPasteboard._items[kUTTypeHTML as String] as? String).to(contain("hello"))
+          expect(mockPasteboard._items[kUTTypeUTF8PlainText as String] as? String).to(contain("hello"))
         }
       }
     }
@@ -135,6 +203,64 @@ class ClipboardModuleSpec: ExpoSpec {
     
     
     // MARK: - Images
+    
+    describe("setImageAsync") {
+      it("copies image to clipboard") {
+
+        testModuleFunction("setImageAsync", args: [rawPngData]) { (_: Any?) in
+          let pasteboardImgData = UIPasteboard.general.image?.pngData()?.base64EncodedString()
+          expect(UIPasteboard.general.hasImages) == true
+          expect(pasteboardImgData?.prefix(10)) == rawPngData.prefix(10)
+        }
+      }
+    }
+    
+    describe("getImageAsync") {
+      it("returns PNG image from the clipboard") {
+        let expectedImg = UIImage(data: Data(base64Encoded: rawPngData)!)!
+        let expectedImgData = expectedImg.pngData()!.base64EncodedString()
+        UIPasteboard.general.image = expectedImg
+
+        let options = [
+          "format": "png"
+        ]
+        testModuleFunction("getImageAsync", args: [options]) { (result: [String: Any?]?) in
+          let imgData = result!["data"]! as? String?
+          let imgSize = result!["size"]! as? [String: Any]
+          expect(imgSize!["width"] as? CGFloat) == CGFloat(1)
+          expect(imgSize!["height"] as? CGFloat) == CGFloat(1)
+          expect(imgData!).to(beginWith("data:image/png;base64,\(expectedImgData)"))
+        }
+      }
+      
+      it("returns JPEG image from the clipboard") {
+        let expectedImg = UIImage(data: Data(base64Encoded: rawPngData)!)!
+        let expectedImgData = expectedImg.jpegData(compressionQuality: 1.0)!.base64EncodedString()
+        UIPasteboard.general.image = expectedImg
+
+        let options = [
+          "format": "jpeg"
+        ]
+        testModuleFunction("getImageAsync", args: [options]) { (result: [String: Any?]?) in
+          let imgData = result!["data"]! as? String?
+          let imgSize = result!["size"]! as? [String: Any]
+          expect(imgSize!["width"] as? CGFloat) == CGFloat(1)
+          expect(imgSize!["height"] as? CGFloat) == CGFloat(1)
+          expect(imgData!).to(beginWith("data:image/jpeg;base64,\(expectedImgData)"))
+        }
+      }
+
+      it("returns nil if no text in clipboard") {
+        UIPasteboard.general.items = []
+
+        let options = [
+          "imageFormat": "png"
+        ]
+        testModuleFunction("getImageAsync", args: [options]) { (result: UIImage?) in
+          expect(result).to(beNil())
+        }
+      }
+    }
     
     describe("hasImageAsync") {
       it("returns true when there is image") {
