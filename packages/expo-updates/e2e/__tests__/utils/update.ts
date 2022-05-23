31fd8363a6e6267a27c8e22d27c40c4fcde4fba3
Eric Samelson <esamelson@users.noreply.github.com>
2022-05-23 14:03:26 -0700

    [updates-e2e] move test project setup into js (#17496)

--- packages/expo-updates/e2e/__tests__/utils/update.ts
+++ packages/expo-updates/e2e/__tests__/utils/update.ts
@@ -5,28 +5,25 @@ import path from 'path';
 import * as Simulator from './simulator';
 
 const EXPORT_PUBLIC_URL = 'https://u.expo.dev/dummy-url';
-const UPDATE_DIST_PATH = process.env.TEST_UPDATE_DIST_PATH;
 let bundlePath: string | null = null;
 
-function findBundlePath(): string {
+function findBundlePath(updateDistPath: string): string {
   if (!bundlePath) {
-    const classicManifest = require(path.join(
-      UPDATE_DIST_PATH,
-      Simulator.ExportedManifestFilename
-    ));
+    const classicManifest = require(path.join(updateDistPath, Simulator.ExportedManifestFilename));
     const { bundleUrl }: { bundleUrl: string } = classicManifest;
-    bundlePath = path.join(UPDATE_DIST_PATH, bundleUrl.replace(EXPORT_PUBLIC_URL, ''));
+    bundlePath = path.join(updateDistPath, bundleUrl.replace(EXPORT_PUBLIC_URL, ''));
   }
   return bundlePath;
 }
 
 export async function copyBundleToStaticFolder(
+  updateDistPath: string,
   filename: string,
   notifyString?: string
 ): Promise<string> {
   const staticFolder = path.resolve(__dirname, '..', '.static');
   await fs.mkdir(staticFolder, { recursive: true });
-  let bundleString = await fs.readFile(findBundlePath(), 'utf-8');
+  let bundleString = await fs.readFile(findBundlePath(updateDistPath), 'utf-8');
   if (notifyString) {
     bundleString = bundleString.replace('/notify/test', `/notify/${notifyString}`);
   }
