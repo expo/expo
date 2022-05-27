e531e0658adbeb0fa8a0071b83ac753d684de359
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-05-27 17:08:32 +0200

    Adjust `Constants` and `Exceptions`

--- packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/ImagePickerExceptions.kt
+++ packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/ImagePickerExceptions.kt
@@ -6,14 +6,17 @@ import java.io.File
 internal class FailedToDeduceTypeException :
   CodedException("Can not deduce type of the returned file")
 
-internal class FailedToCreateFileException :
-  CodedException("Failed to create the file")
+internal class FailedToCreateFileException(path: String, cause: Throwable? = null) :
+  CodedException("Failed to create the file '${path}'", cause)
 
 internal class FailedToExtractVideoMetadataException(cause: Throwable? = null) :
   CodedException("Failed to extract video metadata", cause)
 
 internal class FailedToWriteFileException(file: File? = null, cause: Throwable? = null) :
   CodedException("Failed to write a file '${file?.absolutePath ?: ""}'", cause)
+
+internal class FailedToReadFileException(file: File, cause: Throwable? = null) :
+  CodedException("Failed to read a file '${file.absolutePath}", cause)
 
 internal class MissingActivityToHandleIntent(intentType: String?) :
   CodedException("Failed to resolve activity to handle the intent of type '$intentType'")
@@ -26,12 +29,3 @@ internal class MissingModuleException(moduleName: String) :
 
 internal class UserRejectedPermissionsException :
   CodedException("User rejected permissions")
-
-internal class CroppingFailedException(cause: Throwable? = null) :
-  CodedException("Cropping operation failed", cause)
-
-internal class FailedToReadDataException :
-  CodedException("Reading image data failed")
-
-internal class UnknownException(cause: Throwable) :
-  CodedException("Unknown exception", cause)
