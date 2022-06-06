ed916244c07a672799cb511379b9ec02dd8f0a16
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-06-06 15:09:57 +0200

    [expo-crypto] Add sync version of the `digestString` function (#17761)
    # Why
    
    Adds a sync version of the  `digestString` function.

--- packages/expo-crypto/android/src/main/java/expo/modules/crypto/CryptoModule.kt
+++ packages/expo-crypto/android/src/main/java/expo/modules/crypto/CryptoModule.kt
@@ -9,20 +9,23 @@ class CryptoModule : Module() {
   override fun definition() = ModuleDefinition {
     Name("ExpoCrypto")
 
-    AsyncFunction("digestStringAsync") { algorithm: DigestAlgorithm, data: String, options: DigestOptions ->
-      val messageDigest = MessageDigest.getInstance(algorithm.value).apply { update(data.toByteArray()) }
+    Function("digestString", this@CryptoModule::digestString)
+    AsyncFunction("digestStringAsync", this@CryptoModule::digestString)
+  }
 
-      val digest: ByteArray = messageDigest.digest()
-      return@AsyncFunction when (options.encoding) {
-        DigestOptions.Encoding.BASE64 -> {
-          Base64.encodeToString(digest, Base64.NO_WRAP)
-        }
-        DigestOptions.Encoding.HEX -> {
-          digest.joinToString(separator = "") { byte ->
-            ((byte.toInt() and 0xff) + 0x100)
-              .toString(radix = 16)
-              .substring(startIndex = 1)
-          }
+  private fun digestString(algorithm: DigestAlgorithm, data: String, options: DigestOptions): String {
+    val messageDigest = MessageDigest.getInstance(algorithm.value).apply { update(data.toByteArray()) }
+
+    val digest: ByteArray = messageDigest.digest()
+    return when (options.encoding) {
+      DigestOptions.Encoding.BASE64 -> {
+        Base64.encodeToString(digest, Base64.NO_WRAP)
+      }
+      DigestOptions.Encoding.HEX -> {
+        digest.joinToString(separator = "") { byte ->
+          ((byte.toInt() and 0xff) + 0x100)
+            .toString(radix = 16)
+            .substring(startIndex = 1)
         }
       }
     }
