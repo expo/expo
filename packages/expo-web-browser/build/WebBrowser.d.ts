8607cb282a747ca6ba3eff24bef5a78335d5d42f
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-06-23 21:58:37 +0200

    [web-browser] Make `redirectUrl` auth session argument optional (#17953)
    I'm personally not convinced about this change, but use it at your own risk.
    
    - Updated TypeScript to make this argument optional
    - Slightly clarified docs (default value is only provided on web)
    - On iOS made this argument optional. The `ASWebAuthenticationSession` seems not to require it, although it's not documented what happens then. From what I tested - The web auth session just does not close automatically after logged in
    - On Android, it was possible to make it optional - when it's `null`, the _wait for redirect Promise_ never resolves, but as it's part of a `Promise.race()`, then finally another promise resolves (wins the race) when auth session window is closed manually. This is the same behaviour as on iOS so it should be OK.
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-web-browser/build/WebBrowser.d.ts
+++ packages/expo-web-browser/build/WebBrowser.d.ts
@@ -102,7 +102,7 @@ export declare function dismissBrowser(): void;
  *
  * @param url The url to open in the web browser. This should be a login page.
  * @param redirectUrl _Optional_ - The url to deep link back into your app.
- * By default, this will be set to output of [`Linking.createURL("")`](./linking/#linkingcreateurlpath-namedparameters).
+ * On web, this defaults to the output of [`Linking.createURL("")`](./linking/#linkingcreateurlpath-namedparameters).
  * @param options _Optional_ - An object extending the [`WebBrowserOpenOptions`](#webbrowseropenoptions).
  * If there is no native AuthSession implementation available (which is the case on Android)
  * these params will be used in the browser polyfill. If there is a native AuthSession implementation,
@@ -114,7 +114,7 @@ export declare function dismissBrowser(): void;
  * - If the browser is closed using [`dismissBrowser`](#webbrowserdismissbrowser),
  * the Promise fulfills with `{ type: 'dismiss' }` object.
  */
-export declare function openAuthSessionAsync(url: string, redirectUrl: string, options?: AuthSessionOpenOptions): Promise<WebBrowserAuthSessionResult>;
+export declare function openAuthSessionAsync(url: string, redirectUrl?: string | null, options?: AuthSessionOpenOptions): Promise<WebBrowserAuthSessionResult>;
 export declare function dismissAuthSession(): void;
 /**
  * Possibly completes an authentication session on web in a window popup. The method
