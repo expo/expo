b9a49634d0ddfb4dd8b8491a8a0bdca8d2245e9b
evanbacon <bacon@expo.io>
2022-06-24 10:21:38 +0200

    fix jest config

--- packages/@expo/config/jest.config.js
+++ packages/@expo/config/jest.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  preset: '../cli/jest.config',
+  testEnvironment: 'node',
+  testRegex: '/__tests__/.*(test|spec)\\.[jt]sx?$',
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  clearMocks: true,
   rootDir: path.resolve(__dirname),
   displayName: require('./package').name,
   roots: ['__mocks__', 'src'],
