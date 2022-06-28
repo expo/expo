3089b02ade3c699a55a9b4ca2f4110a5a18054d5
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-06-28 12:43:36 +0200

    [sweet][Kotlin] Use `unordered_map` instead of `map` (#17990)
    # Why
    
    Starts using `unordered_map` instead of `map` to cached data. 
    It isn't a huge change, but in my opinion, the `unordered_map` (hash map) fits better when we just want to cache data 🤷 
    
    # Test Plan
    
    - unit tests ✅

--- packages/expo-modules-core/android/src/main/cpp/CachedReferencesRegistry.cpp
+++ packages/expo-modules-core/android/src/main/cpp/CachedReferencesRegistry.cpp
@@ -32,7 +32,10 @@ void CachedReferencesRegistry::loadJClass(
   // Note this clazz variable points to a leaked global reference.
   // This is appropriate for classes that are never unloaded which is any class in an Android app.
   auto clazz = (jclass) env->NewGlobalRef(env->FindClass(name.c_str()));
-  std::map<std::pair<std::string, std::string>, jmethodID> methods;
+
+  MethodHashMap methods;
+  methods.reserve(methodsNames.size());
+
   for (auto &method: methodsNames) {
     methods.insert(
       {method, env->GetMethodID(clazz, method.first.c_str(), method.second.c_str())}
@@ -55,7 +58,8 @@ jmethodID CachedReferencesRegistry::CachedJClass::getMethod(const std::string &n
   return methods.at({name, signature});
 }
 
-CachedReferencesRegistry::CachedJClass::CachedJClass(jclass clazz,
-                                                     std::map<std::pair<std::string, std::string>, jmethodID> methods
+CachedReferencesRegistry::CachedJClass::CachedJClass(
+  jclass clazz,
+  MethodHashMap methods
 ) : clazz(clazz), methods(std::move(methods)) {}
 } // namespace expo
