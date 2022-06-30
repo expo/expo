6adee594a4697e937e2d9206a4ab1f9ed211cce0
Dominik Garcia <hello@dmnkgrc.com>
2022-06-30 14:43:06 +0200

    [ios][autolinking] Use regex to match modules in expo_patch_react_imports! (#17968)
    # Why
    
    The previous version of the script would ignore modules if the absolute path contained ignored names even though the string had nothing to do with the ignored modules, for example:
    
    - /Users/username/code/react-native/node_modules/react-native-permissions
    - /Users/username/code/expo-learning/node_modules/react-native-paypal
    
    This led me to temporarily having to change the structure and naming of my projects since I like to organize them by technology 😅 
    
    # How
    
    This PR changes the implementation to use a regex that instead will make sure that the excluded package names are located inside the `node_modules` directory. This way users of the packages can name and strucuture their projects in any way without being affected by the script.

--- packages/expo-modules-autolinking/scripts/ios/react_import_patcher.rb
+++ packages/expo-modules-autolinking/scripts/ios/react_import_patcher.rb
@@ -54,12 +54,13 @@ module Expo
         end
       end
 
-      return result.select { |dir|
+      result.reject do |dir|
         # Exclude known dirs unnecessary to patch and reduce processing time
-        !dir.include?('/react-native/') &&
-        !dir.end_with?('/react-native') &&
-        !dir.include?('/expo-')
-      }
+        # Since we are using real (absolute) pathnames we need to assert that we are inside of the node_modules
+        # directory to not collide with other directories in the user's filesystem.
+        # We reject the react-native package and packages starting with expo-
+        dir.match(%r{^.*/node_modules/(react-native(/.*)?|expo-.*)$})
+      end
     end
 
   end # class ReactImportPatcher
