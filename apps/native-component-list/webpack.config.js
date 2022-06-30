564e88f89916921971ca0d7cf63357dbd517dc02
Kudo Chien <kudo@expo.dev>
2022-06-30 10:31:52 +0800

    Revert "[NCL] Fix web bundle error"
    This reverts commit c48d0a70514b9183784930b82aa5b1218196bb8a.

--- apps/native-component-list/webpack.config.js
+++ apps/native-component-list/webpack.config.js
@@ -6,14 +6,5 @@ module.exports = async (env) => {
   if (config.devServer) {
     delete config.devServer.watchOptions;
   }
-
-  config.resolve.alias = {
-    ...config.resolve.alias,
-
-    // Workaround bundle error where reanimated tries to import internal module,
-    // alias ReactFabric to an ordinary module in react-native-web.
-    'react-native/Libraries/Renderer/shims/ReactFabric': 'react-native-web/dist/cjs/exports/render',
-  };
-
   return config;
 };
