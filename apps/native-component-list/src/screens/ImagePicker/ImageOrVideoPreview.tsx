6c4083643e30dbe1d73f0bc51437c18d43c1eb89
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-07-05 11:53:19 +0200

    [ncl][ImagePicker] Preview multiple images (#18096)

--- apps/native-component-list/src/screens/ImagePicker/ImageOrVideoPreview.tsx
+++ apps/native-component-list/src/screens/ImagePicker/ImageOrVideoPreview.tsx
@@ -1,8 +1,24 @@
 import { ResizeMode, Video } from 'expo-av';
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, FlatList } from 'react-native';
 
-export default function ImageOrVideo(result: unknown) {
+export default function ImageOrVideo(result: unknown): JSX.Element | void {
+  if (isMultipleSelectionResult(result)) {
+    if (result.selected.length === 1) {
+      return ImageOrVideo(result.selected[0]);
+    }
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          horizontal
+          data={result.selected}
+          renderItem={({ item, index }) => <View key={index}>{ImageOrVideo(item) ?? null}</View>}
+        />
+      </View>
+    );
+  }
+
   if (!isAnObjectWithUriAndType(result)) {
     return;
   }
@@ -30,6 +46,15 @@ function isAnObjectWithUriAndType(obj: unknown): obj is { uri: string; type: str
     obj !== null &&
     typeof (obj as any).uri === 'string' &&
     typeof (obj as any).type === 'string'
+  );
+}
+
+function isMultipleSelectionResult(obj: unknown): obj is { cancelled: false; selected: unknown[] } {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    (obj as any).cancelled === false &&
+    Array.isArray((obj as any).selected)
   );
 }
 
