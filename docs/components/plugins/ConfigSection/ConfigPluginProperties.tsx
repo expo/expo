feceaaa359f08804f483e9e5dcfceb420e90883f
Simek <gosimek@gmail.com>
2022-07-06 14:18:48 +0200

    lint fix

--- docs/components/plugins/ConfigSection/ConfigPluginProperties.tsx
+++ docs/components/plugins/ConfigSection/ConfigPluginProperties.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 
 import { InlineCode } from '~/components/base/code';
-import { B, P } from '~/components/base/paragraph';
+import { P } from '~/components/base/paragraph';
 import { H3 } from '~/components/plugins/Headings';
 import { PlatformTags } from '~/components/plugins/api/APISectionPlatformTags';
 import { Cell, HeaderCell, Row, Table, TableHead } from '~/ui/components/Table';
