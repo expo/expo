3a29300a79c95ac20ea6b57dc2c7dad84b1e1398
Kudo Chien <kudo@expo.dev>
2022-07-06 12:20:28 +0800

    [android] Add @shopify/react-native-skia vendoring support for Expo Go  (#17964)
    # Why
    
    like #17704 but for android
    close ENG-4889
    
    # How
    
    - introduce vendor modules autolinking for expo go.
    previously, reanimated is the only vendor module having cxx code, we had a fixed versioning script for reanimated. because react-native-skia also has cxx code, i'm trying to keep the versioning more modularized and like what we did for ios versioning. ideally, a vendor module should have a dedicated gradle project. that's why i introduced a vendor module autolinking here.
    - vendoring tool: add react-native-skia support
    - versioning tool: trying to add a minimum support for new style versioning like ios.
    currently, after transforming the code, i still prebuild the *.so to `jniLibs`. in the future when we commit `android/versioned-react-native/ABI46_0_0` to git, we can always build the versioned *.so from source. 
    
    # Test Plan
    
    - android unversioned expo go + NCL skia
    - android versioned expo go + NCL skia
    
    Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

--- tools/src/versioning/android/copyExpoview.ts
+++ tools/src/versioning/android/copyExpoview.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 import { copyFileWithTransformsAsync } from '../../Transforms';
 import { searchFilesAsync } from '../../Utils';
-import { expoviewTransforms } from './expoviewTransforms';
+import { expoviewTransforms } from './transforms/expoviewTransforms';
 
 export async function copyExpoviewAsync(sdkVersion: string, androidDir: string): Promise<void> {
   const abiVersion = `abi${sdkVersion.replace(/\./g, '_')}`;
@@ -40,6 +40,16 @@ export async function copyExpoviewAsync(sdkVersion: string, androidDir: string):
       settingsGradle.replace(
         /ADD_NEW_SUPPORTED_ABIS_HERE/,
         `ADD_NEW_SUPPORTED_ABIS_HERE\n    "${abiVersion}",`
+      )
+    );
+  }
+  const vendoredLinking = `useVendoredModulesForSettingsGradle('sdk${sdkVersion.split('.')[0]}')`;
+  if (!settingsGradle.match(vendoredLinking)) {
+    await fs.writeFile(
+      settingsGradlePath,
+      settingsGradle.replace(
+        /(^useVendoredModulesForSettingsGradle\('unversioned'\))/gm,
+        `$1\n${vendoredLinking}`
       )
     );
   }
---
+++ tools/src/versioning/android/transforms/vendoredModulesTransforms.ts
@@ -0,0 +1,36 @@
+import { FileTransforms, StringTransform } from '../../../Transforms.types';
+
+export function vendoredModulesTransforms(prefix: string): Record<string, FileTransforms> {
+  return {
+    '@shopify/react-native-skia': {
+      content: [
+        {
+          paths: 'build.gradle',
+          find: `def nodeModules = Paths.get(projectDir.getPath(), '../../../../../..', 'react-native-lab').toString()`,
+          replaceWith: `def nodeModules = Paths.get(projectDir.getPath(), '../../../../..').toString()`,
+        },
+        {
+          paths: 'build.gradle',
+          find: 'sourceBuild = true',
+          replaceWith: 'sourceBuild = false',
+        },
+        {
+          paths: 'ExponentPackage.kt',
+          find: 'import com.shopify',
+          replaceWith: `import ${prefix}.com.shopify`,
+        },
+      ],
+    },
+  };
+}
+
+export function exponentPackageTransforms(prefix: string): Record<string, StringTransform[]> {
+  return {
+    '@shopify/react-native-skia': [
+      {
+        find: 'import com.shopify',
+        replaceWith: `import ${prefix}.com.shopify`,
+      },
+    ],
+  };
+}
