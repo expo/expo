expo/expo

commit b17064cf13f93ec3b795736d6abbecaef2ea35ff

[expotools] Add optional sdkVersion flag to publish-dev-home [skip ci]

tools/src/commands/PublishDevExpoHomeCommand.ts
@@ -46,8 +46,11 @@ export async function findTargetSdkVersionAsync(): Promise<string> {
 /**
  * Sets `sdkVersion` and `version` fields in app configuration if needed.
  */
-export async function maybeUpdateHomeSdkVersionAsync(appJson: AppConfig): Promise<void> {
-  const targetSdkVersion = await findTargetSdkVersionAsync();
+export async function maybeUpdateHomeSdkVersionAsync(
+  appJson: AppConfig,
+  explicitSdkVersion?: string | null
+): Promise<void> {
+  const targetSdkVersion = explicitSdkVersion ?? (await findTargetSdkVersionAsync());
 
   if (appJson.expo.sdkVersion !== targetSdkVersion) {
     console.log(`Updating home's sdkVersion to ${chalk.cyan(targetSdkVersion)}...`);
@@ -164,7 +167,7 @@ async function action(options: ActionOptions): Promise<void> {
   console.log('Getting expo-cli state of the current session...');
   const cliStateBackup = await getExpoCliStateAsync();
 
-  await maybeUpdateHomeSdkVersionAsync(appJson);
+  await maybeUpdateHomeSdkVersionAsync(appJson, options.sdkVersion);
 
   console.log(`Modifying home's slug to ${chalk.green(slug)}...`);
   appJson.expo.slug = slug;
@@ -229,6 +232,10 @@ export default (program: Command) => {
       '-d, --dry',
       'Whether to skip `expo publish` command. Despite this, some files might be changed after running this script.',
       false
+    )
+    .option(
+      '-s, --sdkVersion [string]',
+      'SDK version the published app should use. Defaults to the newest available SDK set in the Expo Go project.'
     )
     .asyncAction(action);
 };

commit 94a74fc4e156db9fec84199a7f64b9f60fbdd175

[tools] Fix type error introduced in b17064cf

tools/src/commands/PublishDevExpoHomeCommand.ts
@@ -16,6 +16,7 @@ import AppConfig from '../typings/AppConfig';
 
 type ActionOptions = {
   dry: boolean;
+  sdkVersion?: string;
 };
 
 type ExpoCliStateObject = {