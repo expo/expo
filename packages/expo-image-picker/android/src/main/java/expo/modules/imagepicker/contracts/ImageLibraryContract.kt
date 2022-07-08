2a5d158730f538ae9ac068b1f43b04b8f1122ea3
Bartłomiej Bukowski <bartlomiej.bukowski@swmansion.com>
2022-07-08 13:05:45 +0200

    [image-picker] Handle multiple selection in `ImageLibraryContract`

--- packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/contracts/ImageLibraryContract.kt
+++ packages/expo-image-picker/android/src/main/java/expo/modules/imagepicker/contracts/ImageLibraryContract.kt
@@ -14,7 +14,8 @@ import java.io.Serializable
  * An [androidx.activity.result.contract.ActivityResultContract] to prompt the user to pick single or multiple image(s) or/and video(s),
  * receiving a `content://` [Uri] for each piece of content.
  *
- * @see [androidx.activity.result.contract.ActivityResultContracts.GetContent]
+ * @see [androidx.activity.result.contract.ActivityResultContracts.GetContent],
+ * @see [androidx.activity.result.contract.ActivityResultContracts.GetMultipleContents]
  */
 internal class ImageLibraryContract(
   private val appContextProvider: AppContextProvider,
@@ -23,17 +24,48 @@ internal class ImageLibraryContract(
     Intent(Intent.ACTION_GET_CONTENT)
       .addCategory(Intent.CATEGORY_OPENABLE)
       .setType(input.options.mediaTypes.toMimeType())
+      .apply {
+        if (input.options.allowsMultipleSelection) {
+          this.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        }
+      }
 
   override fun parseResult(input: ImageLibraryContractOptions, resultCode: Int, intent: Intent?) =
     if (resultCode == Activity.RESULT_CANCELED) {
       ImagePickerContractResult.Cancelled()
+    } else if (input.options.allowsMultipleSelection) {
+      val uris = requireNotNull(intent).getClipDataUris()
+      val contentResolver = requireNotNull(appContextProvider.appContext.reactContext) { "React Application Context is null. "}.contentResolver
+      ImagePickerContractResult.Success(uris.map { uri -> uri.toMediaType(contentResolver) to uri })
     } else {
       val uri = requireNotNull(requireNotNull(intent).data)
       val contentResolver = requireNotNull(appContextProvider.appContext.reactContext) { "React Application Context is null. " }.contentResolver
       val type = uri.toMediaType(contentResolver)
-
-      ImagePickerContractResult.Success(type to uri)
+      ImagePickerContractResult.Success(listOf(type to uri))
     }
+}
+
+/**
+ * Copied from [androidx.activity.result.contract.ActivityResultContracts.GetMultipleContents.getClipDataUris]
+ */
+internal fun Intent.getClipDataUris(): List<Uri> {
+  // Use a LinkedHashSet to maintain any ordering that may be present in the ClipData
+  val resultSet = LinkedHashSet<Uri>()
+  data?.let { data ->
+    resultSet.add(data)
+  }
+  val clipData = clipData
+  if (clipData == null && resultSet.isEmpty()) {
+    return emptyList()
+  } else if (clipData != null) {
+    for (i in 0 until clipData.itemCount) {
+      val uri = clipData.getItemAt(i).uri
+      if (uri != null) {
+        resultSet.add(uri)
+      }
+    }
+  }
+  return ArrayList(resultSet)
 }
 
 internal data class ImageLibraryContractOptions(
