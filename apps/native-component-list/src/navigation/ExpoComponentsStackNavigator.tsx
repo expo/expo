ab2b3ffc171265c9c7cdc0740726cf535b90c9c6
Brent Vatne <brentvatne@gmail.com>
2022-07-08 15:57:04 -0700

    [ncl][docs] Remove admob, amplitude, branch, facebook-ads, facebook, google-sign-in, segment

--- apps/native-component-list/src/navigation/ExpoComponentsStackNavigator.tsx
+++ apps/native-component-list/src/navigation/ExpoComponentsStackNavigator.tsx
@@ -13,21 +13,9 @@ const Stack = createStackNavigator();
 export const Screens = [
   {
     getComponent() {
-      return optionalRequire(() => require('../screens/FacebookAdsScreen'));
-    },
-    name: 'FacebookAds',
-  },
-  {
-    getComponent() {
       return optionalRequire(() => require('../screens/DrawerLayoutAndroidScreen'));
     },
     name: 'DrawerLayoutAndroid',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/AdMobScreen'));
-    },
-    name: 'Admob',
   },
   {
     getComponent() {
