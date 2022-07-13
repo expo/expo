527facdd335c60abd3b3de59b42e67373c72ce1c
Juwan Wheatley <datwheat@gmail.com>
2022-07-13 06:39:05 -0400

    [home] use RN FlatList instead of RNGH to make pagination work (#18194)

--- home/screens/BranchListScreen/BranchListView.tsx
+++ home/screens/BranchListScreen/BranchListView.tsx
@@ -1,11 +1,10 @@
 import { spacing } from '@expo/styleguide-native';
 import { Divider, useExpoTheme, View } from 'expo-dev-client-components';
 import * as React from 'react';
-import { ActivityIndicator, View as RNView } from 'react-native';
+import { FlatList, ActivityIndicator, View as RNView } from 'react-native';
 import InfiniteScrollView from 'react-native-infinite-scroll-view';
 
 import { BranchListItem } from '../../components/BranchListItem';
-import { FlatList } from '../../components/FlatList';
 import { BranchesForProjectQuery } from '../../graphql/types';
 
 type BranchManifest = {
