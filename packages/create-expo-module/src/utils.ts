8d706d7c48c3d41843d186aa75fa3fab242d96d4
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-07-13 13:10:52 +0200

    [create-expo-module] Use CLI spinners to indicate the progress

---
+++ packages/create-expo-module/src/utils.ts
@@ -0,0 +1,21 @@
+import chalk from 'chalk';
+import ora from 'ora';
+
+export type StepOptions = ora.Options;
+
+export async function newStep<Result>(
+  title: string,
+  action: (step: ora.Ora) => Promise<Result> | Result,
+  options: StepOptions = {}
+): Promise<Result> {
+  const disabled = process.env.CI || process.env.EXPO_DEBUG;
+  const step = ora({
+    text: chalk.bold(title),
+    isEnabled: !disabled,
+    stream: disabled ? process.stdout : process.stderr,
+    ...options,
+  });
+
+  step.start();
+  return await action(step);
+}
