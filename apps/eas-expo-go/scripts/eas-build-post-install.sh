872a2a77d6d99a0a6256eeb1b83012094d884842
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-07-14 11:42:33 +0200

    rerrun

--- apps/eas-expo-go/scripts/eas-build-post-install.sh
+++ apps/eas-expo-go/scripts/eas-build-post-install.sh
@@ -10,8 +10,27 @@ cat << EOF > ~/.config/direnv/direnv.toml
 prefix = [ "/" ]
 EOF
 
+ensure_prebuilt_heremes() {
+  ANDROID_DIR="$ROOT_DIR/android"
+  CURRENT_VERSION=$(test -f $ANDROID_DIR/ReactAndroid/prebuiltHermes/.hermesversion && cat $ANDROID_DIR/ReactAndroid/prebuiltHermes/.hermesversion) || true
+  TARGET_VERSION=$(test -f $ANDROID_DIR/sdks/.hermesversion && cat $ANDROID_DIR/sdks/.hermesversion) || true
+  if [[ $CURRENT_VERSION != $TARGET_VERSION ]]; then 
+    pushd $ANDROID_DIR
+    rm -rf $ANDROID_DIR/ReactAndroid/prebuiltHermes
+    mkdir -p $ANDROID_DIR/ReactAndroid/prebuiltHermes
+    cp -f $ANDROID_DIR/sdks/.hermesversion $ANDROID_DIR/ReactAndroid/prebuiltHermes/
+    ./gradlew :ReactAndroid:hermes-engine:assembleRelease
+    ./gradlew :ReactAndroid:hermes-engine:assembleDebug
+    cp -f $ANDROID_DIR/ReactAndroid/hermes-engine/build/outputs/aar/hermes-engine-release.aar $ANDROID_DIR/ReactAndroid/prebuiltHermes/
+    cp -f $ANDROID_DIR/ReactAndroid/hermes-engine/build/outputs/aar/hermes-engine-debug.aar $ANDROID_DIR/ReactAndroid/prebuiltHermes/
+    popd
+  fi
+}
+
 if [ "$EAS_BUILD_PLATFORM" = "ios" ]; then
   direnv exec . et ios-generate-dynamic-macros
+elif [ "$EAS_BUILD_PLATFORM" = "android" ]; then
+  ensure_prebuilt_heremes
 fi
 
 if [ "$EAS_BUILD_PROFILE" = "versioned-client-add-sdk" ]; then
