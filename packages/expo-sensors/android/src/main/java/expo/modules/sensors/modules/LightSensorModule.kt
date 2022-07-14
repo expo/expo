8015098d06b772b91d6894af397bd25eebb21b33
gilteixeira <gilteixeira@ua.pt>
2022-07-14 11:13:18 +0100

    ran build on expo sensors and gradlew on bare expo

--- packages/expo-sensors/android/src/main/java/expo/modules/sensors/modules/LightSensorModule.kt
+++ packages/expo-sensors/android/src/main/java/expo/modules/sensors/modules/LightSensorModule.kt
@@ -20,13 +20,11 @@ class LightSensorModule(reactContext: Context?) : BaseSensorModule(reactContext)
     return moduleRegistry.getModule(LightSensorServiceInterface::class.java)
   }
 
-
   override fun eventToMap(sensorEvent: SensorEvent): Bundle {
     return Bundle().apply {
       putDouble("illuminance", sensorEvent.values[0].toDouble())
     }
   }
-
 
   @ExpoMethod
   fun startObserving(promise: Promise) {
