7bcc9f0a975112d1059ede4604184422b72a6788
Kudo Chien <kudo@expo.dev>
2022-07-14 11:08:02 +0800

    [tools] fix android versioning (#18227)
    # Why
    
    fix for sdk46 android versioning
    
    # How
    
    - fix that settings.gradle content be overwrote from my change of vendoring skia
    - update expo-modules-core patch
    - fix vendor modules versioning for @shopify/flash-list
    
    # Test Plan
    
    1. `et add-sdk -p android -s 46.0.0`
    2. versioned android expo go + sdk46 ncl launch test

--- tools/src/versioning/android/copyExpoview.ts
+++ tools/src/versioning/android/copyExpoview.ts
@@ -33,24 +33,20 @@ export async function copyExpoviewAsync(sdkVersion: string, androidDir: string):
   }
 
   const settingsGradlePath = path.join(androidDir, 'settings.gradle');
-  const settingsGradle = await fs.readFile(settingsGradlePath, 'utf-8');
+  let settingsGradle = await fs.readFile(settingsGradlePath, 'utf-8');
   if (!settingsGradle.match(abiVersion)) {
-    await fs.writeFile(
-      settingsGradlePath,
-      settingsGradle.replace(
-        /ADD_NEW_SUPPORTED_ABIS_HERE/,
-        `ADD_NEW_SUPPORTED_ABIS_HERE\n    "${abiVersion}",`
-      )
+    settingsGradle = settingsGradle.replace(
+      /ADD_NEW_SUPPORTED_ABIS_HERE/,
+      `ADD_NEW_SUPPORTED_ABIS_HERE\n    "${abiVersion}",`
     );
   }
   const vendoredLinking = `useVendoredModulesForSettingsGradle('sdk${sdkVersion.split('.')[0]}')`;
   if (!settingsGradle.match(vendoredLinking)) {
-    await fs.writeFile(
-      settingsGradlePath,
-      settingsGradle.replace(
-        /(^useVendoredModulesForSettingsGradle\('unversioned'\))/gm,
-        `$1\n${vendoredLinking}`
-      )
+    settingsGradle = settingsGradle.replace(
+      /(^useVendoredModulesForSettingsGradle\('unversioned'\))/gm,
+      `$1\n${vendoredLinking}`
     );
   }
+
+  await fs.writeFile(settingsGradlePath, settingsGradle);
 }
