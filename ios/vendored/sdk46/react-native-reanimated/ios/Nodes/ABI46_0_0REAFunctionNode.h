eae4134059c6fcdac6316d47219f13e6cb0fc391
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-07-15 14:49:41 +0200

    [ios] Add versioned code for SDK46

---
+++ ios/vendored/sdk46/react-native-reanimated/ios/Nodes/ABI46_0_0REAFunctionNode.h
@@ -0,0 +1,6 @@
+
+#import <ABI46_0_0RNReanimated/ABI46_0_0REANode.h>
+
+@interface ABI46_0_0REAFunctionNode : ABI46_0_0REANode
+
+@end
---
+++ ios/vendored/sdk46/react-native-reanimated/ios/Nodes/ABI46_0_0REAPropsNode.h
@@ -0,0 +1,9 @@
+#import <ABI46_0_0RNReanimated/ABI46_0_0REANode.h>
+
+@interface ABI46_0_0REAPropsNode : ABI46_0_0REANode <ABI46_0_0REAFinalNode>
+
+- (void)connectToView:(NSNumber *_Nonnull)viewTag viewName:(NSString *_Nonnull)viewName;
+
+- (void)disconnectFromView:(NSNumber *_Nonnull)viewTag;
+
+@end
