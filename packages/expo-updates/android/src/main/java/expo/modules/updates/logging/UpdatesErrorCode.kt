f1311eeee249d3838d17b4dd18444c16e5e88a6c
Douglas Lowder <douglowder@mac.com>
2022-07-21 15:52:24 -0700

    [expo-updates][Android] Logging and log reading (#18318)

---
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/logging/UpdatesErrorCode.kt
@@ -0,0 +1,15 @@
+package expo.modules.updates.logging
+
+/**
+ * Error codes for expo-updates logs
+ */
+enum class UpdatesErrorCode(val code: String) {
+  None("None"),
+  NoUpdatesAvailable("NoUpdatesAvailable"),
+  UpdateAssetsNotAvailable("UpdateAssetsNotAvailable"),
+  UpdateServerUnreachable("UpdateServerUnreachable"),
+  UpdateHasInvalidSignature("UpdateHasInvalidSignature"),
+  UpdateFailedToLoad("UpdateFailedToLoad"),
+  AssetsFailedToLoad("AssetsFailedToLoad"),
+  JSRuntimeError("JSRuntimeError")
+}
