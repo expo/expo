93f993bd2ec214158ac98f1f29ce3b0f15ca97ba
Eric Samelson <eric@expo.io>
2022-07-21 17:13:13 -0700

    use runInBand instead of single test file

--- packages/expo-updates/e2e/__tests__/Updates-e2e.test.ts
+++
@@ -1,7 +0,0 @@
-import basic from './tests/basic';
-
-// run test suites from a single file in order to keep them entirely sequential;
-// --runInBand is not enough on its own
-describe('Updates e2e', () => {
-  basic();
-});
