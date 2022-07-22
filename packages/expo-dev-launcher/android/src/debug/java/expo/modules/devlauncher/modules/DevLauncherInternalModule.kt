e685c7838a19af5786dada77c17b23486093c864
Keith Kurak <keith.kurak@gmail.com>
2022-07-22 10:37:58 -0400

    Revert "[expo-dev-launcher] improve android emulator detection"
    This reverts commit 29a6fcfc5dacdbda8767740aaaad2420573fcaa8.

--- packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/modules/DevLauncherInternalModule.kt
+++ packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/modules/DevLauncherInternalModule.kt
@@ -52,32 +52,11 @@ class DevLauncherInternalModule(reactContext: ReactApplicationContext?) :
   override fun hasConstants(): Boolean = true
 
   override fun getConstants(): Map<String, Any> {
-    val isRunningOnEmulator = Build.FINGERPRINT.startsWith("generic")
-      || Build.FINGERPRINT.startsWith("unknown")
-      || Build.MODEL.contains("google_sdk")
-      || Build.MODEL.lowercase(Locale.ROOT).contains("droid4x")
-      || Build.MODEL.contains("Emulator")
-      || Build.MODEL.contains("Android SDK built for x86")
-      || Build.MANUFACTURER.contains("Genymotion")
-      || Build.HARDWARE.contains("goldfish")
-      || Build.HARDWARE.contains("ranchu")
-      || Build.HARDWARE.contains("vbox86")
-      || Build.PRODUCT.contains("sdk")
-      || Build.PRODUCT.contains("google_sdk")
-      || Build.PRODUCT.contains("sdk_google")
-      || Build.PRODUCT.contains("sdk_x86")
-      || Build.PRODUCT.contains("vbox86p")
-      || Build.PRODUCT.contains("emulator")
-      || Build.PRODUCT.contains("simulator")
-      || Build.BOARD.lowercase(Locale.ROOT).contains("nox")
-      || Build.BOOTLOADER.lowercase(Locale.ROOT).contains("nox")
-      || Build.HARDWARE.lowercase(Locale.ROOT).contains("nox")
-      || Build.PRODUCT.lowercase(Locale.ROOT).contains("nox")
-      || Build.SERIAL.lowercase(Locale.ROOT).contains("nox")
-      || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+    val isRunningOnGenymotion = Build.FINGERPRINT.contains("vbox")
+    val isRunningOnStockEmulator = Build.FINGERPRINT.contains("generic")
     return mapOf(
       "installationID" to installationIDHelper.getOrCreateInstallationID(reactApplicationContext),
-      "isDevice" to (!isRunningOnEmulator),
+      "isDevice" to (!isRunningOnGenymotion && !isRunningOnStockEmulator),
       "updatesConfig" to getUpdatesConfig(),
     )
   }
