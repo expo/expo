8b50cb835c85cd87a9c191372f3eead4738f3974
Eric Samelson <esamelson@users.noreply.github.com>
2022-07-25 11:02:56 -0700

    [updates-e2e] add a second test build in preparation for new test suite (#18303)

--- packages/expo-updates/e2e/__tests__/setup/index.js
+++ packages/expo-updates/e2e/__tests__/setup/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const { buildAsync: buildAndroidAsync } = require('./build-android');
 const { buildAsync: buildIosAsync } = require('./build-ios');
-const { setupAsync } = require('./project');
+const { initAsync, setupBasicAppAsync, setupAssetsAppAsync } = require('./project');
 
 const repoRoot = process.env.EXPO_REPO_ROOT;
 const artifactsDest = process.env.ARTIFACTS_DEST;
@@ -10,8 +10,40 @@ const artifactsDest = process.env.ARTIFACTS_DEST;
 const workingDir = path.resolve(repoRoot, '..');
 const runtimeVersion = '1.0.0';
 
+/**
+ * To setup e2e tests locally, export the following environment variables:
+ * $ export UPDATES_HOST=$(ifconfig -l | xargs -n1 ipconfig getifaddr)
+ * $ export UPDATES_PORT=4747
+ * $ export EXPO_REPO_ROOT=<path to local expo repo>
+ * $ export ARTIFACTS_DEST=<path to any temp artifacts dir>
+ *
+ * Then execute this file to setup the test project and builds.
+ *
+ * Afterwards, tests can be run on a booted simulator or (Android) connected device with
+ * $ yarn test --config packages/expo-updates/e2e/jest.config.android.js --runInBand
+ * $ yarn test --config packages/expo-updates/e2e/jest.config.ios.js --runInBand
+ */
+
 (async function () {
-  const projectRoot = await setupAsync(workingDir, repoRoot, runtimeVersion);
-  await buildAndroidAsync(projectRoot, artifactsDest);
-  await buildIosAsync(projectRoot, artifactsDest);
+  if (
+    !process.env.ARTIFACTS_DEST ||
+    !process.env.EXPO_REPO_ROOT ||
+    !process.env.UPDATES_HOST ||
+    !process.env.UPDATES_PORT
+  ) {
+    throw new Error(
+      'Missing one or more environment variables; see instructions in e2e/__tests__/setup/index.js'
+    );
+  }
+  const projectRoot = await initAsync(workingDir, repoRoot, runtimeVersion);
+
+  // Order is somewhat important here as the `basic` and `assets` apps are created by modifying the
+  // same project (not creating a new one).
+  await setupBasicAppAsync(projectRoot);
+  await buildAndroidAsync(projectRoot, artifactsDest, 'basic');
+  await buildIosAsync(projectRoot, artifactsDest, 'basic');
+
+  await setupAssetsAppAsync(projectRoot);
+  await buildAndroidAsync(projectRoot, artifactsDest, 'assets');
+  await buildIosAsync(projectRoot, artifactsDest, 'assets');
 })();
