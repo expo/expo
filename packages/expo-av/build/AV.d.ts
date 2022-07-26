d2508b2b3e5fc826a3996d9fcaaea58401f08f19
DominickVale <dominickveil@gmail.com>
2022-07-26 20:49:54 +0200

    [expo-av] Build with updated documentation
    Signed-off-by: DominickVale <dominickveil@gmail.com>

--- packages/expo-av/build/AV.d.ts
+++ packages/expo-av/build/AV.d.ts
@@ -126,8 +126,10 @@ export interface Playback extends AV {
      */
     setRateAsync(rate: number, shouldCorrectPitch: boolean, pitchCorrectionQuality?: PitchCorrectionQuality): Promise<AVPlaybackStatus>;
     /**
-     * This is equivalent to `playbackObject.setStatusAsync({ volume })`.
+     * This is equivalent to `playbackObject.setStatusAsync({ volume, audioPan })`.
+     * Note: `audioPan` is currently only supported on Android using `androidImplementation: 'MediaPlayer'`
      * @param volume A number between `0.0` (silence) and `1.0` (maximum volume).
+     * @param audioPan A number between `-1.0` (full left) and `1.0` (full right).
      */
     setVolumeAsync(volume: number, audioPan?: number): Promise<AVPlaybackStatus>;
     /**
