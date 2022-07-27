dfb3ef8620ab5527bb651a7633f309c8e0a1288a
Kudo Chien <kudo@expo.dev>
2022-07-28 00:23:47 +0800

    change props in app

--- apps/fabric-tester/App.js
+++ apps/fabric-tester/App.js
@@ -1,14 +1,17 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const isFabricEnabled = global.nativeFabricUIManager != null;
+  const [reverse, setReverse] = useState(false);
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>isFabricEnabled: {isFabricEnabled + ''}</Text>
-      <LinearGradient style={styles.gradient} colors={['blue', 'yellow']} />
+      <Button title="toggle style" onPress={() => { setReverse(!reverse); }} />
+      <LinearGradient style={styles.gradient} colors={reverse ? ['yellow', 'blue'] : ['blue', 'yellow']} end={reverse ? { x: 1.0, y: 0.5 } : { x: 0.5, y: 1.0 }} />
     </View>
   );
 }
