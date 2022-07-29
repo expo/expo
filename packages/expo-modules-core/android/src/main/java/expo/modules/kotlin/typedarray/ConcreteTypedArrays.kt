089b5a0f3f19daefdb09948a124ce7e8aa9f4744
lukmccall <kosmatylukasz@gmail.com>
2022-07-29 11:08:28 +0200

    Apply requested changes

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/typedarray/ConcreteTypedArrays.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/typedarray/ConcreteTypedArrays.kt
@@ -13,12 +13,12 @@ class Int8Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<Byte> {
   override operator fun get(index: Int): Byte {
     checkIfInRange(index)
-    return readByte(index)
+    return readByte(index * Byte.SIZE_BYTES)
   }
 
   override operator fun set(index: Int, value: Byte) {
     checkIfInRange(index)
-    writeByte(index, value)
+    writeByte(index * Byte.SIZE_BYTES, value)
   }
 }
 
@@ -26,12 +26,12 @@ class Int16Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<Short> {
   override operator fun get(index: Int): Short {
     checkIfInRange(index)
-    return read2Byte(index * 2)
+    return read2Byte(index * Short.SIZE_BYTES)
   }
 
   override operator fun set(index: Int, value: Short) {
     checkIfInRange(index)
-    write2Byte(index * 2, value)
+    write2Byte(index * Short.SIZE_BYTES, value)
   }
 }
 
@@ -39,12 +39,12 @@ class Int32Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<Int> {
   override operator fun get(index: Int): Int {
     checkIfInRange(index)
-    return read4Byte(index * 4)
+    return read4Byte(index * Int.SIZE_BYTES)
   }
 
   override operator fun set(index: Int, value: Int) {
     checkIfInRange(index)
-    write4Byte(index * 4, value)
+    write4Byte(index * Int.SIZE_BYTES, value)
   }
 }
 
@@ -52,12 +52,12 @@ class Uint8Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<UByte> {
   override operator fun get(index: Int): UByte {
     checkIfInRange(index)
-    return readByte(index).toUByte()
+    return readByte(index * UByte.SIZE_BYTES).toUByte()
   }
 
   override operator fun set(index: Int, value: UByte) {
     checkIfInRange(index)
-    writeByte(index, value.toByte())
+    writeByte(index * UByte.SIZE_BYTES, value.toByte())
   }
 }
 
@@ -65,12 +65,12 @@ class Uint8ClampedArray(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<UByte> {
   override operator fun get(index: Int): UByte {
     checkIfInRange(index)
-    return readByte(index).toUByte()
+    return readByte(index * UByte.SIZE_BYTES).toUByte()
   }
 
   override operator fun set(index: Int, value: UByte) {
     checkIfInRange(index)
-    writeByte(index, value.toByte())
+    writeByte(index * UByte.SIZE_BYTES, value.toByte())
   }
 }
 
@@ -78,12 +78,12 @@ class Uint16Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<UShort> {
   override operator fun get(index: Int): UShort {
     checkIfInRange(index)
-    return read2Byte(index * 2).toUShort()
+    return read2Byte(index * UShort.SIZE_BYTES).toUShort()
   }
 
   override operator fun set(index: Int, value: UShort) {
     checkIfInRange(index)
-    write2Byte(index * 2, value.toShort())
+    write2Byte(index * UShort.SIZE_BYTES, value.toShort())
   }
 }
 
@@ -91,12 +91,12 @@ class Uint32Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<UInt> {
   override operator fun get(index: Int): UInt {
     checkIfInRange(index)
-    return read4Byte(index * 4).toUInt()
+    return read4Byte(index * UInt.SIZE_BYTES).toUInt()
   }
 
   override operator fun set(index: Int, value: UInt) {
     checkIfInRange(index)
-    write4Byte(index * 4, value.toInt())
+    write4Byte(index * UInt.SIZE_BYTES, value.toInt())
   }
 }
 
@@ -104,12 +104,12 @@ class Float32Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<Float> {
   override operator fun get(index: Int): Float {
     checkIfInRange(index)
-    return readFloat(index * 4)
+    return readFloat(index * Float.SIZE_BYTES)
   }
 
   override operator fun set(index: Int, value: Float) {
     checkIfInRange(index)
-    writeFloat(index * 4, value)
+    writeFloat(index * Float.SIZE_BYTES, value)
   }
 }
 
@@ -118,12 +118,12 @@ class Float64Array(private val rawArray: JavaScriptTypedArray) :
 
   override operator fun get(index: Int): Double {
     checkIfInRange(index)
-    return readDouble(index * 8)
+    return readDouble(index * Double.SIZE_BYTES)
   }
 
   override operator fun set(index: Int, value: Double) {
     checkIfInRange(index)
-    writeDouble(index * 8, value)
+    writeDouble(index * Double.SIZE_BYTES, value)
   }
 }
 
@@ -131,12 +131,12 @@ class BigInt64Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<Long> {
   override operator fun get(index: Int): Long {
     checkIfInRange(index)
-    return read8Byte(index * 8)
+    return read8Byte(index * Long.SIZE_BYTES)
   }
 
   override operator fun set(index: Int, value: Long) {
     checkIfInRange(index)
-    write8Byte(index * 8, value)
+    write8Byte(index * Long.SIZE_BYTES, value)
   }
 }
 
@@ -144,11 +144,11 @@ class BigUint64Array(private val rawArray: JavaScriptTypedArray) :
   AnyTypedArray by rawArray, GenericTypedArray<ULong> {
   override operator fun get(index: Int): ULong {
     checkIfInRange(index)
-    return read8Byte(index * 8).toULong()
+    return read8Byte(index * ULong.SIZE_BYTES).toULong()
   }
 
   override operator fun set(index: Int, value: ULong) {
     checkIfInRange(index)
-    write8Byte(index * 8, value.toLong())
+    write8Byte(index * ULong.SIZE_BYTES, value.toLong())
   }
 }
