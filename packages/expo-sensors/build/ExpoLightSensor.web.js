9b4552a3d718bad1188678af6b584d5073981de4
bearkillerPT <59414986+bearkillerPT@users.noreply.github.com>
2022-08-02 11:00:13 +0100

    [sensors][android] Add Light sensor (#18225)
    Added Android support for the light sensor, based on the discussion: https://github.com/expo/expo/discussions/18101
    This is mostly copy-paste of the Barometer implementation.
    Does not contain Expo Go scoped sensor implementation, as it seems to work without it.
    More details in the PR description.
    
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>
    Co-authored-by: Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>

---
+++ packages/expo-sensors/build/ExpoLightSensor.web.js
@@ -0,0 +1,11 @@
+export default {
+    get name() {
+        return 'ExpoLightSensor';
+    },
+    async isAvailableAsync() {
+        return false;
+    },
+    startObserving() { },
+    stopObserving() { },
+};
+//# sourceMappingURL=ExpoLightSensor.web.js.map
---
+++ packages/expo-sensors/build/LightSensor.d.ts
@@ -0,0 +1,9 @@
+import DeviceSensor from './DeviceSensor';
+export interface LightSensorMeasurement {
+    illuminance: number;
+}
+declare class LightSensor extends DeviceSensor<LightSensorMeasurement> {
+}
+declare const _default: LightSensor;
+export default _default;
+//# sourceMappingURL=LightSensor.d.ts.map
