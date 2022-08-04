77c1175b6ad1295af47e231f1a81e2deab4eba04
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-08-04 15:46:53 +0200

    Make fabric-tester app a submodule

--- apps/fabric-tester/ios/fabrictester/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
-
-#import <Expo/Expo.h>
-
-@interface AppDelegate : EXAppDelegateWrapper <RCTBridgeDelegate>
-
-@end
