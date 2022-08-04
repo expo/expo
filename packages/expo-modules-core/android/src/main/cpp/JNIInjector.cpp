d386f234ab0e448fa9b11eceb598ebf447b21ad6
Kudo Chien <kudo@expo.dev>
2022-08-05 03:21:52 +0800

    wip

--- packages/expo-modules-core/android/src/main/cpp/JNIInjector.cpp
+++ packages/expo-modules-core/android/src/main/cpp/JNIInjector.cpp
@@ -7,6 +7,10 @@
 #include "JavaScriptTypedArray.h"
 #include "JavaReferencesCache.h"
 
+#if IS_NEW_ARCHITECTURE_ENABLED
+#include "FabricComponentsRegistry.h"
+#endif
+
 #include <jni.h>
 #include <fbjni/fbjni.h>
 
@@ -21,5 +25,8 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
     expo::JavaScriptValue::registerNatives();
     expo::JavaScriptObject::registerNatives();
     expo::JavaScriptTypedArray::registerNatives();
+#if IS_NEW_ARCHITECTURE_ENABLED
+    expo::FabricComponentsRegistry::registerNatives();
+#endif
   });
 }
--- packages/expo-modules-core/android/src/main/java/expo/modules/adapters/react/ModuleRegistryAdapter.java
+++ packages/expo-modules-core/android/src/main/java/expo/modules/adapters/react/ModuleRegistryAdapter.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import expo.modules.BuildConfig;
 import expo.modules.adapters.react.views.SimpleViewManagerAdapter;
 import expo.modules.adapters.react.views.ViewGroupManagerAdapter;
 import expo.modules.core.ModuleRegistry;
@@ -30,6 +31,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
   private NativeModulesProxy mModulesProxy;
   // We need to save all view holders to update them when the new kotlin module registry will be created.
   private List<ViewWrapperDelegateHolder> mWrapperDelegateHolders = null;
+  private FabricComponentsRegistry mFabricComponentsRegistry = null;
 
   public ModuleRegistryAdapter(List<Package> packageList) {
     mModuleRegistryProvider = new ReactModuleRegistryProvider(packageList, null);
@@ -100,6 +102,13 @@ public class ModuleRegistryAdapter implements ReactPackage {
     mWrapperDelegateHolders = kotlinInteropModuleRegistry.extractViewManagersDelegateHolders(kViewManager);
     viewManagerList.addAll(kViewManager);
 
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      List<ViewManager<?, ?>> viewManagers = new ArrayList<>();
+      for (ViewManager viewManager : viewManagerList) {
+        viewManagers.add(viewManager);
+      }
+      mFabricComponentsRegistry = new FabricComponentsRegistry(viewManagers);
+    }
     return viewManagerList;
   }
 
