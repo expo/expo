e3236d204ccc863165482f515219485c2d61fb37
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-08-05 15:03:42 +0200

    [ncl] Add light sensor (#18452)

---
+++ packages/expo-sensors/build/ExpoLightSensor.ios.d.ts
@@ -0,0 +1,8 @@
+declare const _default: {
+    readonly name: string;
+    isAvailableAsync(): Promise<boolean>;
+    startObserving(): void;
+    stopObserving(): void;
+};
+export default _default;
+//# sourceMappingURL=ExpoLightSensor.ios.d.ts.map
---
+++ packages/expo-sensors/build/ExpoLightSensor.ios.js
@@ -0,0 +1,12 @@
+// TODO: (barthap) Create this stub on iOS native side instead
+export default {
+    get name() {
+        return 'ExpoLightSensor';
+    },
+    async isAvailableAsync() {
+        return false;
+    },
+    startObserving() { },
+    stopObserving() { },
+};
+//# sourceMappingURL=ExpoLightSensor.ios.js.map
