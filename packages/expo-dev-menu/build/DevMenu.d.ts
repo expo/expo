4e66c8782208230ee9015e63bed2ca504cc5fcf9
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-08-09 09:51:55 +0200

    [menu] Remove `openSettings` and `openProfile` (#18565)
    # Why
    
    Closes https://github.com/expo/expo/issues/18242.
    
    # How
    
    Removes two unworking functions - openSettings and openProfile. Those functions don't work and don't make sense anymore - settings and profile UI were moved into the dev-launcher package.
    
    # Test Plan
    
    - build TS

--- packages/expo-dev-menu/build/DevMenu.d.ts
+++ packages/expo-dev-menu/build/DevMenu.d.ts
@@ -1,6 +1,4 @@
 import { ExpoDevMenuItem } from './ExpoDevMenu.types';
 export declare function openMenu(): void;
-export declare function openProfile(): void;
-export declare function openSettings(): void;
 export declare function registerDevMenuItems(items: ExpoDevMenuItem[]): Promise<any>;
 //# sourceMappingURL=DevMenu.d.ts.map
--- packages/expo-dev-menu/build/ExpoDevMenu.web.js
+++ packages/expo-dev-menu/build/ExpoDevMenu.web.js
@@ -3,12 +3,6 @@ export default {
     openMenu() {
         throw new WebUnsupportedError();
     },
-    openProfile() {
-        throw new WebUnsupportedError();
-    },
-    openSettings() {
-        throw new WebUnsupportedError();
-    },
     addDevMenuCallbacks() {
         throw new WebUnsupportedError();
     },
