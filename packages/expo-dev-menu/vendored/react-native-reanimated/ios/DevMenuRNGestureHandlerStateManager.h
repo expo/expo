45f418d459dea87121223f6fccbd7948535a03e1
lukmccall <kosmatylukasz@gmail.com>
2022-08-09 09:53:56 +0200

    Remove file

--- packages/expo-dev-menu/vendored/react-native-reanimated/ios/DevMenuRNGestureHandlerStateManager.h
+++
@@ -1,7 +0,0 @@
-// This interface is located in the gesture handler package.
-// We have to remove it from reanimated to avoid duplicate files error.
-//@protocol DevMenuRNGestureHandlerStateManager
-//
-//- (void)setGestureState:(int)state forHandler:(int)handlerTag;
-//
-//@end
