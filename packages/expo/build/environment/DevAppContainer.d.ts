6c7e3a0e53ce3cb3c95e5a57dd3db9ebdfe264f3
Evan Bacon <bacon@expo.io>
2022-08-11 12:17:37 +0200

    chore(expo): convert `DevAppContainer` to functional React component (#18597)
    * chore(expo): convert `DevAppContainer` to functional React component
    
    * Apply suggestions from code review
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    Co-authored-by: Bartosz Kaszubowski <gosimek@gmail.com>
    
    * fix
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    Co-authored-by: Bartosz Kaszubowski <gosimek@gmail.com>

--- packages/expo/build/environment/DevAppContainer.d.ts
+++ packages/expo/build/environment/DevAppContainer.d.ts
@@ -1,7 +1,3 @@
 import * as React from 'react';
-export default class DevAppContainer extends React.Component<{
-    children?: React.ReactNode;
-}> {
-    render(): JSX.Element;
-}
+export default function DevAppContainer({ children }: React.PropsWithChildren<object>): JSX.Element;
 //# sourceMappingURL=DevAppContainer.d.ts.map
--- packages/expo/build/environment/DevAppContainer.js
+++ packages/expo/build/environment/DevAppContainer.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import DevLoadingView from '../environment/DevLoadingView';
-export default class DevAppContainer extends React.Component {
-    render() {
-        return (React.createElement(React.Fragment, null,
-            this.props.children,
-            React.createElement(DevLoadingView, null)));
-    }
+export default function DevAppContainer({ children }) {
+    return (React.createElement(React.Fragment, null,
+        children,
+        React.createElement(DevLoadingView, null)));
 }
 //# sourceMappingURL=DevAppContainer.js.map
