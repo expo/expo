4e5b2e2d7edbb4ef2b9eb5a78432b81f90c58f0c
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-08-15 11:52:27 +0200

    [modules] Support old-style view callbacks for views written in Objective-C (#18636)

--- packages/expo-modules-core/ios/Swift/Views/ComponentData.swift
+++ packages/expo-modules-core/ios/Swift/Views/ComponentData.swift
@@ -73,6 +73,21 @@ public final class ComponentData: RCTComponentData {
  */
 private func createEventSetter(eventName: String, bridge: RCTBridge?) -> RCTPropBlockAlias {
   return { [weak bridge] (target: RCTComponent, value: Any) in
+    // Callback handler that dispatches the actual event.
+    let handler: AnyCallbackHandlerType = { [weak target] (body: [String: Any]) in
+      if let target = target {
+        let componentEvent = RCTComponentEvent(name: eventName, viewTag: target.reactTag, body: body)
+        bridge?.eventDispatcher().send(componentEvent)
+      }
+    }
+
+    // This is to handle events in views written in Objective-C.
+    // Note that the property should be of type EXDirectEventBlock.
+    if let target = target as? NSObject, target.responds(to: Selector(eventName)) {
+      target.setValue(handler, forKey: eventName)
+      return
+    }
+
     // Find view's property that is named as the prop and is wrapped by `Event`.
     let child = Mirror(reflecting: target).children.first {
       $0.label == "_\(eventName)"
@@ -83,12 +98,7 @@ private func createEventSetter(eventName: String, bridge: RCTBridge?) -> RCTProp
 
     // For callbacks React Native passes a bool value whether the prop is specified or not.
     if value as? Bool == true {
-      event.settle { [weak target] (body: Any) in
-        if let target = target {
-          let componentEvent = RCTComponentEvent(name: eventName, viewTag: target.reactTag, body: ["payload": body])
-          bridge?.eventDispatcher().send(componentEvent)
-        }
-      }
+      event.settle(handler)
     } else {
       event.invalidate()
     }
