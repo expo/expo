821b581c826b4fe289f8aeb208d2b3a8abdaf239
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-08-22 15:08:13 +0200

    [ci] fix EAS Build CI workflows (#18744)

--- apps/eas-expo-go/app.config.ts
+++ apps/eas-expo-go/app.config.ts
@@ -10,26 +10,51 @@ const mapBuildProfileToConfig: Record<string, ExpoConfig> = {
     ...base,
     slug: 'versioned-expo-go-add-sdk',
     name: 'Expo Go (versioned) + add sdk',
+    extra: {
+      eas: {
+        projectId: '8550a402-d28f-4437-af87-2fb77b576c3f',
+      },
+    },
   },
   'versioned-client': {
     ...base,
     slug: 'versioned-expo-go',
     name: 'Expo Go (versioned)',
+    extra: {
+      eas: {
+        projectId: '97ab66f4-49e2-4ec7-85cc-922c56a68bae',
+      },
+    },
   },
   'versioned-client-signed': {
     ...base,
     slug: 'versioned-expo-go',
     name: 'Expo Go (versioned)',
+    extra: {
+      eas: {
+        projectId: '97ab66f4-49e2-4ec7-85cc-922c56a68bae',
+      },
+    },
   },
   'versioned-client-signed-apk': {
     ...base,
     slug: 'versioned-expo-go',
     name: 'Expo Go (versioned)',
+    extra: {
+      eas: {
+        projectId: '97ab66f4-49e2-4ec7-85cc-922c56a68bae',
+      },
+    },
   },
   'unversioned-client': {
     ...base,
     slug: 'unversioned-expo-go',
     name: 'Expo Go (unversioned)',
+    extra: {
+      eas: {
+        projectId: '09066dbe-ef65-460e-9201-b7aa931abbf4',
+      },
+    },
   },
 };
 
