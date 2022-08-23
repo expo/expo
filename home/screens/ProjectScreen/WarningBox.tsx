ab4a25eb4aacbf386d302a0962e4b2634556c84f
Juwan Wheatley <datwheat@gmail.com>
2022-08-23 16:47:12 -0400

    [home] make outdated classic update warning more informative

--- home/screens/ProjectScreen/WarningBox.tsx
+++ home/screens/ProjectScreen/WarningBox.tsx
@@ -1,18 +1,13 @@
 import { spacing } from '@expo/styleguide-native';
-import { useExpoTheme, Text, Spacer, Row, View } from 'expo-dev-client-components';
+import { useExpoTheme, Text, Spacer, View } from 'expo-dev-client-components';
 import * as React from 'react';
-import { Platform } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-import { Ionicons } from '../../components/Icons';
-
 export function WarningBox({
-  title,
   message,
   showLearnMore,
   onLearnMorePress,
 }: {
-  title: string;
   message: string;
   showLearnMore?: boolean;
   onLearnMorePress?: () => void;
@@ -37,23 +32,8 @@ export function WarningBox({
     </>
   ) : null;
   return (
-    <View bg="warning" border="warning" rounded="medium" padding="medium">
-      <Row align="center">
-        <Ionicons
-          name={Platform.select({ ios: 'ios-warning', default: 'md-warning' })}
-          size={18}
-          lightColor={theme.text.warning}
-          darkColor={theme.text.warning}
-          style={{
-            marginRight: 4,
-          }}
-        />
-        <Text color="warning" type="InterSemiBold">
-          {title}
-        </Text>
-      </Row>
-      <Spacer.Vertical size="small" />
-      <Text color="warning" type="InterRegular">
+    <View bg="warning" border="warning" rounded="large" padding="medium">
+      <Text color="warning" type="InterRegular" size="medium">
         {message}
       </Text>
       {learnMoreButton}
