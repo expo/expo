5be0dd6720a46dad5b1d9325e0230045b553e35f
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-08-24 14:37:02 +0200

    [ios] Remove versioned code for SDK44 [skip ci]

--- ios/versioned/sdk44/EXScreenOrientation/EXScreenOrientation/ScreenOrientationReactDelegateHandler.swift
+++
@@ -1,9 +0,0 @@
-// Copyright 2018-present 650 Industries. All rights reserved.
-
-import ABI44_0_0ExpoModulesCore
-
-public class ScreenOrientationReactDelegateHandler: ExpoReactDelegateHandler {
-  public override func createRootViewController(reactDelegate: ExpoReactDelegate) -> UIViewController? {
-    return ABI44_0_0EXScreenOrientationViewController(defaultScreenOrientationFromPlist: ());
-  }
-}
