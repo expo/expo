expo/expo

commit 03793af81bc574cfb2b90fa4f869e6cf98e463f8

[av] Sweetify VideoViewManager on Android (#18798)
# Why

#18633 for android where to support fabric for expo-av

# How

- rewrite `VideoViewWrapper.java` to kotlin because the wrapped view delegated callbacks only support kotlin.
- rewrite VideoViewManager as sweet api.

# Test Plan

- ✅ NCL
- ✅ Video test suite
- fabric-tester is not fully working. adding event supports in #18814

packages/expo-av/android/src/main/java/expo/modules/av/AVManager.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.View;
 
 import com.facebook.jni.HybridData;
 
@@ -46,6 +47,7 @@ import expo.modules.av.video.VideoViewWrapper;
 import expo.modules.interfaces.permissions.Permissions;
 import expo.modules.interfaces.permissions.PermissionsResponseListener;
 
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.turbomodule.core.CallInvokerHolderImpl;
 
 import static android.media.MediaRecorder.MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED;
@@ -499,33 +501,9 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   // Unified playback API - Video
 
-  private interface VideoViewCallback {
-    void runWithVideoView(final VideoView videoView);
-  }
-
-  // Rejects the promise if the VideoView is not found, otherwise executes the callback.
-  private void tryRunWithVideoView(final Integer tag, final VideoViewCallback callback, final Promise promise) {
-    if (mModuleRegistry != null) {
-      UIManager uiManager = getUIManager();
-      if (uiManager != null) {
-        uiManager.addUIBlock(tag, new UIManager.UIBlock<VideoViewWrapper>() {
-          @Override
-          public void resolve(VideoViewWrapper videoViewWrapper) {
-            callback.runWithVideoView(videoViewWrapper.getVideoViewInstance());
-          }
-
-          @Override
-          public void reject(Throwable throwable) {
-            promise.reject("E_VIDEO_TAGINCORRECT", "Invalid view returned from registry.");
-          }
-        }, VideoViewWrapper.class);
-      }
-    }
-  }
-
   @Override
   public void loadForVideo(final Integer tag, final ReadableArguments source, final ReadableArguments status, final Promise promise) {
-    tryRunWithVideoView(tag, new VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setSource(source, status, promise);
@@ -535,7 +513,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void unloadForVideo(final Integer tag, final Promise promise) {
-    tryRunWithVideoView(tag, new VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setSource(null, null, promise);
@@ -545,7 +523,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void setStatusForVideo(final Integer tag, final ReadableArguments status, final Promise promise) {
-    tryRunWithVideoView(tag, new VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setStatus(status, promise);
@@ -555,7 +533,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void replayVideo(final Integer tag, final ReadableArguments status, final Promise promise) {
-    tryRunWithVideoView(tag, new VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setStatus(status, promise);
@@ -565,7 +543,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void getStatusForVideo(final Integer tag, final Promise promise) {
-    tryRunWithVideoView(tag, new VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         promise.resolve(videoView.getStatus());

packages/expo-av/android/src/main/java/expo/modules/av/ViewUtils.kt
@@ -1,0 +1,43 @@
+package expo.modules.av
+
+import androidx.annotation.AnyThread
+import androidx.annotation.UiThread
+import com.facebook.react.bridge.ReactContext
+import com.facebook.react.bridge.UiThreadUtil
+import com.facebook.react.uimanager.UIManagerHelper
+import expo.modules.av.video.VideoView
+import expo.modules.av.video.VideoViewWrapper
+import expo.modules.core.Promise
+
+object ViewUtils {
+  interface VideoViewCallback {
+    fun runWithVideoView(videoView: VideoView): Unit
+  }
+
+  @UiThread
+  private fun tryRunWithVideoViewOnUiThread(reactContext: ReactContext, viewTag: Int, callback: VideoViewCallback, promise: Promise) {
+    val videoWrapperView = UIManagerHelper.getUIManagerForReactTag(reactContext, viewTag)?.resolveView(viewTag) as VideoViewWrapper?
+    val videoView = videoWrapperView?.videoViewInstance
+    if (videoView != null) {
+      callback.runWithVideoView(videoView)
+    } else {
+      promise.reject("E_VIDEO_TAGINCORRECT", "Invalid view returned from registry.")
+    }
+  }
+
+  /**
+   * Rejects the promise if the VideoView is not found, otherwise executes the callback.
+   */
+  @JvmStatic
+  @AnyThread
+  @Deprecated("Use `dispatchCommands` in favor of finding view with imperative calls")
+  fun tryRunWithVideoView(reactContext: ReactContext, viewTag: Int, callback: VideoViewCallback, promise: Promise) {
+    if (UiThreadUtil.isOnUiThread()) {
+      tryRunWithVideoViewOnUiThread(reactContext, viewTag, callback, promise)
+    } else {
+      UiThreadUtil.runOnUiThread {
+        tryRunWithVideoViewOnUiThread(reactContext, viewTag, callback, promise)
+      }
+    }
+  }
+}

packages/expo-av/android/src/main/java/expo/modules/av/video/VideoManager.java
@@ -1,11 +1,16 @@
 package expo.modules.av.video;
 
 import android.content.Context;
+import android.view.View;
+
 import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.ReactContext;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import expo.modules.av.ViewUtils;
 import expo.modules.av.video.scalablevideoview.ScalableType;
 import expo.modules.core.ExportedModule;
 import expo.modules.core.ModuleRegistry;
@@ -46,7 +51,7 @@ public class VideoManager extends ExportedModule {
 
   @ExpoMethod
   public void setFullscreen(final Integer tag, final Boolean shouldBeFullscreen, final Promise promise) {
-    tryRunWithVideoView(tag, new VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         FullscreenVideoPlayerPresentationChangeProgressListener listener = new FullscreenVideoPlayerPresentationChangeProgressListener() {
@@ -89,24 +94,5 @@ public class VideoManager extends ExportedModule {
         }
       }
     }, promise);
-  }
-
-  private interface VideoViewCallback {
-    void runWithVideoView(final VideoView videoView);
-  }
-
-  // Rejects the promise if the VideoView is not found, otherwise executes the callback.
-  private void tryRunWithVideoView(final Integer tag, final VideoViewCallback callback, final Promise promise) {
-    mModuleRegistry.getModule(UIManager.class).addUIBlock(tag, new UIManager.UIBlock<VideoViewWrapper>() {
-      @Override
-      public void resolve(VideoViewWrapper videoViewWrapper) {
-        callback.runWithVideoView(videoViewWrapper.getVideoViewInstance());
-      }
-
-      @Override
-      public void reject(Throwable throwable) {
-        promise.reject("E_VIDEO_TAGINCORRECT", "Invalid view returned from registry.");
-      }
-    }, VideoViewWrapper.class);
   }
 }

packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/services/UIManager.java
@@ -20,8 +20,10 @@ public interface UIManager {
     void execute(ViewHolder viewHolder);
   }
 
+  @Deprecated
   <T> void addUIBlock(int viewTag, UIBlock<T> block, Class<T> tClass);
 
+  @Deprecated
   void addUIBlock(GroupUIBlock block);
 
   void runOnUiQueueThread(Runnable runnable);

packages/expo-av/android/src/main/java/expo/modules/av/AVPackage.java
@@ -12,7 +12,6 @@ import expo.modules.core.ViewManager;
 import expo.modules.core.interfaces.InternalModule;
 import expo.modules.av.player.datasource.SharedCookiesDataSourceFactoryProvider;
 import expo.modules.av.video.VideoManager;
-import expo.modules.av.video.VideoViewManager;
 
 public class AVPackage extends BasePackage {
 
@@ -34,6 +33,6 @@ public class AVPackage extends BasePackage {
 
   @Override
   public List<ViewManager> createViewManagers(Context context) {
-    return Collections.singletonList((ViewManager) new VideoViewManager());
+    return Collections.emptyList();
   }
 }

packages/expo-av/android/src/main/java/expo/modules/av/video/FullscreenPlayerUpdate.kt
@@ -1,0 +1,8 @@
+package expo.modules.av.video
+
+internal enum class FullscreenPlayerUpdate(val jsValue: Int) {
+  FULLSCREEN_PLAYER_WILL_PRESENT(0),
+  FULLSCREEN_PLAYER_DID_PRESENT(1),
+  FULLSCREEN_PLAYER_WILL_DISMISS(2),
+  FULLSCREEN_PLAYER_DID_DISMISS(3);
+}

packages/expo-av/android/src/main/java/expo/modules/av/video/FullscreenVideoPlayer.java
@@ -13,6 +13,7 @@ import java.lang.ref.WeakReference;
 import expo.modules.core.ModuleRegistry;
 import expo.modules.core.interfaces.services.KeepAwakeManager;
 import expo.modules.av.player.PlayerData;
+import expo.modules.kotlin.AppContext;
 
 public class FullscreenVideoPlayer extends Dialog {
   private static class KeepScreenOnUpdater implements Runnable {
@@ -32,7 +33,8 @@ public class FullscreenVideoPlayer extends Dialog {
           boolean isPlaying =
               fullscreenVideoPlayer.mVideoView.getStatus().containsKey(PlayerData.STATUS_IS_PLAYING_KEY_PATH)
                   && fullscreenVideoPlayer.mVideoView.getStatus().getBoolean(PlayerData.STATUS_IS_PLAYING_KEY_PATH);
-          ModuleRegistry moduleRegistry = fullscreenVideoPlayer.mModuleRegistry;
+          AppContext appContext = fullscreenVideoPlayer.mAppContext.get();
+          ModuleRegistry moduleRegistry = appContext != null ? appContext.getLegacyModuleRegistry() : null;
           if (moduleRegistry != null) {
             KeepAwakeManager keepAwakeManager = moduleRegistry.getModule(KeepAwakeManager.class);
             boolean keepAwakeIsActivated = keepAwakeManager != null && keepAwakeManager.isActivated();
@@ -52,14 +54,14 @@ public class FullscreenVideoPlayer extends Dialog {
   private Runnable mKeepScreenOnUpdater;
 
   private FrameLayout mParent;
-  private ModuleRegistry mModuleRegistry;
+  private WeakReference<AppContext> mAppContext;
   private final VideoView mVideoView;
   private final FrameLayout mContainerView;
   private WeakReference<FullscreenVideoPlayerPresentationChangeListener> mUpdateListener;
 
-  FullscreenVideoPlayer(@NonNull Context context, VideoView videoView, ModuleRegistry moduleRegistry) {
+  FullscreenVideoPlayer(@NonNull Context context, VideoView videoView, AppContext appContext) {
     super(context, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
-    mModuleRegistry = moduleRegistry;
+    mAppContext = new WeakReference<>(appContext);
 
     setCancelable(false);
 

commit 512e1329940ce158edcdb91bc91a3d4990578eab

[android][av][core] Fix a couple issues for expo go (#18823)
# Why

fix a couple issues for expo go.

# How

- a launch crash because Permission module not found. this is a regression for #18472 in `ModuleRegistryAdapter::getNativeModulesFromModuleRegistry()` where custom moduleRegistry is not be used.
- VideoViewModule is not registered. a regression from #18798
- Video play/fullscreen does not work. because the [context](https://github.com/expo/expo/pull/18798/files#diff-92ed6ed63af93be93cb457680bd3640d56ce064cf6e6a51fa134f44148556521R506) in expo go is not ReactContext but ScopedContext. since VideoManager and AVManager is still written in legacy module, i still have to use UIManager to get the correct ReactContext.

# Test Plan

- unversioned android expo go + NCL video
- fabric-tester VideoExample

android/expoview/src/main/java/versioned/host/exp/exponent/ExperiencePackagePicker.kt
@@ -2,6 +2,7 @@ package versioned.host.exp.exponent
 
 import expo.modules.application.ApplicationPackage
 import expo.modules.av.AVPackage
+import expo.modules.av.video.VideoViewModule
 import expo.modules.backgroundfetch.BackgroundFetchPackage
 import expo.modules.barcodescanner.BarCodeScannerPackage
 import expo.modules.battery.BatteryPackage
@@ -138,6 +139,7 @@ object ExperiencePackagePicker : ModulesProvider {
     LinearGradientModule::class.java,
     LocalizationModule::class.java,
     RandomModule::class.java,
+    VideoViewModule::class.java,
     WebBrowserModule::class.java,
   )
 }

packages/expo-av/android/src/main/java/expo/modules/av/AVManager.java
@@ -503,7 +503,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void loadForVideo(final Integer tag, final ReadableArguments source, final ReadableArguments status, final Promise promise) {
-    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView(mModuleRegistry, tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setSource(source, status, promise);
@@ -513,7 +513,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void unloadForVideo(final Integer tag, final Promise promise) {
-    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView(mModuleRegistry, tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setSource(null, null, promise);
@@ -523,7 +523,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void setStatusForVideo(final Integer tag, final ReadableArguments status, final Promise promise) {
-    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView(mModuleRegistry, tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setStatus(status, promise);
@@ -533,7 +533,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void replayVideo(final Integer tag, final ReadableArguments status, final Promise promise) {
-    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView(mModuleRegistry, tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         videoView.setStatus(status, promise);
@@ -543,7 +543,7 @@ public class AVManager implements LifecycleEventListener, AudioManager.OnAudioFo
 
   @Override
   public void getStatusForVideo(final Integer tag, final Promise promise) {
-    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView(mModuleRegistry, tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         promise.resolve(videoView.getStatus());

packages/expo-av/android/src/main/java/expo/modules/av/ViewUtils.kt
@@ -2,12 +2,12 @@ package expo.modules.av
 
 import androidx.annotation.AnyThread
 import androidx.annotation.UiThread
-import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.UiThreadUtil
-import com.facebook.react.uimanager.UIManagerHelper
 import expo.modules.av.video.VideoView
 import expo.modules.av.video.VideoViewWrapper
+import expo.modules.core.ModuleRegistry
 import expo.modules.core.Promise
+import expo.modules.core.interfaces.services.UIManager
 
 object ViewUtils {
   interface VideoViewCallback {
@@ -15,8 +15,8 @@ object ViewUtils {
   }
 
   @UiThread
-  private fun tryRunWithVideoViewOnUiThread(reactContext: ReactContext, viewTag: Int, callback: VideoViewCallback, promise: Promise) {
-    val videoWrapperView = UIManagerHelper.getUIManagerForReactTag(reactContext, viewTag)?.resolveView(viewTag) as VideoViewWrapper?
+  private fun tryRunWithVideoViewOnUiThread(moduleRegistry: ModuleRegistry, viewTag: Int, callback: VideoViewCallback, promise: Promise) {
+    val videoWrapperView = moduleRegistry.getModule(UIManager::class.java).resolveView(viewTag) as VideoViewWrapper?
     val videoView = videoWrapperView?.videoViewInstance
     if (videoView != null) {
       callback.runWithVideoView(videoView)
@@ -31,12 +31,12 @@ object ViewUtils {
   @JvmStatic
   @AnyThread
   @Deprecated("Use `dispatchCommands` in favor of finding view with imperative calls")
-  fun tryRunWithVideoView(reactContext: ReactContext, viewTag: Int, callback: VideoViewCallback, promise: Promise) {
+  fun tryRunWithVideoView(moduleRegistry: ModuleRegistry, viewTag: Int, callback: VideoViewCallback, promise: Promise) {
     if (UiThreadUtil.isOnUiThread()) {
-      tryRunWithVideoViewOnUiThread(reactContext, viewTag, callback, promise)
+      tryRunWithVideoViewOnUiThread(moduleRegistry, viewTag, callback, promise)
     } else {
       UiThreadUtil.runOnUiThread {
-        tryRunWithVideoViewOnUiThread(reactContext, viewTag, callback, promise)
+        tryRunWithVideoViewOnUiThread(moduleRegistry, viewTag, callback, promise)
       }
     }
   }

packages/expo-av/android/src/main/java/expo/modules/av/video/VideoManager.java
@@ -51,7 +51,7 @@ public class VideoManager extends ExportedModule {
 
   @ExpoMethod
   public void setFullscreen(final Integer tag, final Boolean shouldBeFullscreen, final Promise promise) {
-    ViewUtils.tryRunWithVideoView((ReactContext) getContext(), tag, new ViewUtils.VideoViewCallback() {
+    ViewUtils.tryRunWithVideoView(mModuleRegistry, tag, new ViewUtils.VideoViewCallback() {
       @Override
       public void runWithVideoView(final VideoView videoView) {
         FullscreenVideoPlayerPresentationChangeProgressListener listener = new FullscreenVideoPlayerPresentationChangeProgressListener() {

packages/expo-modules-core/android/src/main/java/expo/modules/adapters/react/ModuleRegistryAdapter.java
@@ -1,5 +1,7 @@
 package expo.modules.adapters.react;
 
+import androidx.annotation.Nullable;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -56,7 +58,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
 
     List<NativeModule> nativeModules = getNativeModulesFromModuleRegistry(reactContext, moduleRegistry);
     if (mWrapperDelegateHolders != null) {
-      KotlinInteropModuleRegistry kotlinInteropModuleRegistry = getOrCreateNativeModulesProxy(reactContext).getKotlinInteropModuleRegistry();
+      KotlinInteropModuleRegistry kotlinInteropModuleRegistry = getOrCreateNativeModulesProxy(reactContext, null).getKotlinInteropModuleRegistry();
       kotlinInteropModuleRegistry.updateModuleHoldersInViewManagers(mWrapperDelegateHolders);
     }
 
@@ -66,7 +68,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
   protected List<NativeModule> getNativeModulesFromModuleRegistry(ReactApplicationContext reactContext, ModuleRegistry moduleRegistry) {
     List<NativeModule> nativeModulesList = new ArrayList<>(2);
 
-    nativeModulesList.add(getOrCreateNativeModulesProxy(reactContext));
+    nativeModulesList.add(getOrCreateNativeModulesProxy(reactContext, moduleRegistry));
 
     // Add listener that will notify expo.modules.core.ModuleRegistry when all modules are ready
     nativeModulesList.add(new ModuleRegistryReadyNotifier(moduleRegistry));
@@ -95,7 +97,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
       }
     }
 
-    NativeModulesProxy modulesProxy = Objects.requireNonNull(getOrCreateNativeModulesProxy(reactContext));
+    NativeModulesProxy modulesProxy = Objects.requireNonNull(getOrCreateNativeModulesProxy(reactContext, null));
     KotlinInteropModuleRegistry kotlinInteropModuleRegistry = modulesProxy.getKotlinInteropModuleRegistry();
     List<ViewManager<?, ?>> kViewManager = kotlinInteropModuleRegistry.exportViewManagers();
     // Saves all holders that needs to be in sync with module registry
@@ -109,13 +111,14 @@ public class ModuleRegistryAdapter implements ReactPackage {
     return viewManagerList;
   }
 
-  private NativeModulesProxy getOrCreateNativeModulesProxy(ReactApplicationContext reactContext) {
+  private NativeModulesProxy getOrCreateNativeModulesProxy(ReactApplicationContext reactContext,
+                                                           @Nullable ModuleRegistry moduleRegistry) {
     if (mModulesProxy == null) {
-      ModuleRegistry moduleRegistry = mModuleRegistryProvider.get(reactContext);
+      ModuleRegistry registry = moduleRegistry != null ? moduleRegistry : mModuleRegistryProvider.get(reactContext);
       if (mModulesProvider != null) {
-        mModulesProxy = new NativeModulesProxy(reactContext, moduleRegistry, mModulesProvider);
+        mModulesProxy = new NativeModulesProxy(reactContext, registry, mModulesProvider);
       } else {
-        mModulesProxy = new NativeModulesProxy(reactContext, moduleRegistry);
+        mModulesProxy = new NativeModulesProxy(reactContext, registry);
       }
     }
     return mModulesProxy;

packages/expo-modules-core/android/src/main/java/expo/modules/adapters/react/services/UIManagerModuleWrapper.java
@@ -5,11 +5,20 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.turbomodule.core.CallInvokerHolderImpl;
 import com.facebook.react.uimanager.IllegalViewOperationException;
 import com.facebook.react.uimanager.NativeViewHierarchyManager;
+import com.facebook.react.uimanager.UIManagerHelper;
 import com.facebook.react.uimanager.UIManagerModule;
+
+import java.lang.ref.WeakReference;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 import expo.modules.core.interfaces.ActivityEventListener;
 import expo.modules.core.interfaces.ActivityProvider;
@@ -17,12 +26,6 @@ import expo.modules.core.interfaces.InternalModule;
 import expo.modules.core.interfaces.JavaScriptContextProvider;
 import expo.modules.core.interfaces.LifecycleEventListener;
 import expo.modules.core.interfaces.services.UIManager;
-
-import java.lang.ref.WeakReference;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.WeakHashMap;
 
 public class UIManagerModuleWrapper implements
     ActivityProvider,
@@ -96,6 +99,16 @@ public class UIManagerModuleWrapper implements
         });
       }
     });
+  }
+
+  @Nullable
+  @Override
+  public View resolveView(int viewTag) {
+    final com.facebook.react.bridge.UIManager uiManager = UIManagerHelper.getUIManagerForReactTag(getContext(), viewTag);
+    if (uiManager == null) {
+      return null;
+    }
+    return uiManager.resolveView(viewTag);
   }
 
   @Override

packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/services/UIManager.java
@@ -2,6 +2,8 @@ package expo.modules.core.interfaces.services;
 
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
 import expo.modules.core.interfaces.ActivityEventListener;
 import expo.modules.core.interfaces.LifecycleEventListener;
 
@@ -25,6 +27,10 @@ public interface UIManager {
 
   @Deprecated
   void addUIBlock(GroupUIBlock block);
+
+  @Deprecated
+  @Nullable
+  View resolveView(int viewTag);
 
   void runOnUiQueueThread(Runnable runnable);
 