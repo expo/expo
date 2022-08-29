e16914768c885c3a74cef07fd0dcdd5d73f773b4
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-08-29 12:24:16 +0200

    [modules][ios] Remove deprecated module definition components that started with a lowercase letter (#18851)

--- packages/expo-modules-core/ios/Swift/Views/ViewManagerDefinitionComponents.swift
+++ packages/expo-modules-core/ios/Swift/Views/ViewManagerDefinitionComponents.swift
@@ -5,34 +5,11 @@
 /**
  Defines the factory creating a native view when the module is used as a view.
  */
-@available(*, deprecated, renamed: "View")
-public func view(_ closure: @escaping () -> UIView) -> ViewManagerDefinitionComponent {
-  return ViewFactory(closure)
-}
-
-/**
- Defines the factory creating a native view when the module is used as a view.
- */
 public func View<ViewType: UIView>(_ closure: @escaping () -> ViewType) -> ViewManagerDefinitionComponent {
   return ViewFactory(closure)
 }
 
 // MARK: Props
-
-/**
- Creates a view prop that defines its name and setter.
- */
-@available(*, deprecated, renamed: "Prop")
-public func prop<ViewType: UIView, PropType: AnyArgument>(
-  _ name: String,
-  _ setter: @escaping (ViewType, PropType) -> Void
-) -> ViewManagerDefinitionComponent {
-  return ConcreteViewProp(
-    name: name,
-    propType: ~PropType.self,
-    setter: setter
-  )
-}
 
 /**
  Creates a view prop that defines its name and setter.
