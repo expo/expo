7c3afa20f30d33fea9791bfd337f2611431dbf19
Simek <gosimek@gmail.com>
2022-08-30 13:31:39 +0200

    fix lint, regenerate snapshots on macOS

--- docs/ui/components/Snippet/blocks/Terminal.tsx
+++ docs/ui/components/Snippet/blocks/Terminal.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import {darkTheme, spacing, typography} from '@expo/styleguide';
+import { darkTheme, spacing } from '@expo/styleguide';
 import React from 'react';
 
 import { Snippet } from '../Snippet';
