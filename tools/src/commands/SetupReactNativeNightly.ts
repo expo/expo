614b85404ad380d1c2d0b0445796918ca1e402f6
Kudo Chien <kudo@expo.dev>
2022-08-31 20:37:55 +0800

    [tools] Add setup-react-native-nightly command

---
+++ tools/src/commands/SetupReactNativeNightly.ts
@@ -0,0 +1,128 @@
+import { Command } from '@expo/commander';
+import JsonFile from '@expo/json-file';
+import fs from 'fs-extra';
+import path from 'path';
+
+import { EXPO_DIR } from '../Constants';
+import logger from '../Logger';
+import { getPackageViewAsync } from '../Npm';
+import { transformFileAsync } from '../Transforms';
+import { installAsync as workspaceInstallAsync } from '../Workspace';
+
+export default (program: Command) => {
+  program
+    .command('setup-react-native-nightly')
+    .description('Setup expo/expo monorepo to install react-native nightly build for testing')
+    .asyncAction(main);
+};
+
+async function main() {
+  await addReactNativeNightlyAsync();
+  await updateReactNativePackageAsync();
+
+  await patchReanimatedAsync();
+
+  // Workaround for deprecated `Linking.removeEventListener`
+  // Remove this after we migrate to @react-navigation/native@^6.0.12
+  // https://linear.app/expo/issue/ENG-4148/upgrade-react-navigation-across-expoexpo-monorepo
+  // https://github.com/react-navigation/react-navigation/commit/bd5cd55e130cba2c6c35bbf360e3727a9fcf00e4
+  await patchReactNavigationAsync();
+
+  logger.log('Setting up project files for bare-expo.');
+  await updateBareExpoAsync();
+}
+
+async function installPinnedPackageVersionAsync(name: string, version: string) {
+  logger.log(`Installing pinned ${name}@${version}.`);
+  const workspacePackageJsonPath = path.join(EXPO_DIR, 'package.json');
+  const json = await JsonFile.readAsync(workspacePackageJsonPath);
+  json.resolutions ||= {};
+  json.resolutions[name] = version;
+  await JsonFile.writeAsync(workspacePackageJsonPath, json);
+  await workspaceInstallAsync();
+}
+
+async function addReactNativeNightlyAsync() {
+  const version = (await getPackageViewAsync('react-native'))?.['dist-tags'].nightly;
+  if (!version) {
+    throw new Error('Unable to get react-native nightly version.');
+  }
+  await installPinnedPackageVersionAsync('react-native', version);
+}
+
+async function updateReactNativePackageAsync() {
+  const root = path.join(EXPO_DIR, 'node_modules', 'react-native');
+
+  // Third party libraries used to use react-native minor version, update the version 1000.999.0 as the latest version
+  await transformFileAsync(path.join(root, 'package.json'), [
+    {
+      find: '"version": "0.0.0-',
+      replaceWith: '"version": "1000.999.0-',
+    },
+  ]);
+  await transformFileAsync(path.join(root, 'ReactAndroid', 'gradle.properties'), [
+    {
+      find: 'VERSION_NAME=1000.0.0-',
+      replaceWith: 'VERSION_NAME=1000.999.0-',
+    },
+  ]);
+
+  // Build hermes source from the main branch
+  await fs.writeFile(path.join(root, 'sdks', '.hermesversion'), 'main');
+  await transformFileAsync(path.join(root, 'sdks', 'hermes-engine', 'hermes-engine.podspec'), [
+    {
+      // Use the fake version to force building hermes from source
+      find: "version = package['version']",
+      replaceWith: "version = '1000.0.0'",
+    },
+  ]);
+}
+
+async function patchReanimatedAsync() {
+  // Remove this after we upgrade reanimated
+  await installPinnedPackageVersionAsync('react-native-reanimated', '2.10.0');
+
+  // Workaround for reanimated doesn't support the hermes where building from source
+  const root = path.join(EXPO_DIR, 'node_modules', 'react-native-reanimated');
+  await transformFileAsync(path.join(root, 'android', 'build.gradle'), [
+    {
+      find: /\bdef hermesAAR = file\(.+\)/g,
+      replaceWith:
+        'def hermesAAR = file("$reactNative/ReactAndroid/hermes-engine/build/outputs/aar/hermes-engine-debug.aar")',
+    },
+  ]);
+
+  // Remove this after reanimated support react-native 0.71
+  await transformFileAsync(path.join(root, 'android', 'CMakeLists.txt'), [
+    {
+      find: /(\s*"\$\{NODE_MODULES_DIR\}\/react-native\/ReactAndroid\/src\/main\/jni")/g,
+      replaceWith:
+        '$1\n        "${NODE_MODULES_DIR}/react-native/ReactAndroid/src/main/jni/react/turbomodule"',
+    },
+  ]);
+}
+
+async function patchReactNavigationAsync() {
+  const root = path.join(EXPO_DIR, 'node_modules', '@react-navigation');
+  await transformFileAsync(path.join(root, 'native', 'src', 'useLinking.native.tsx'), [
+    {
+      find: `\
+      Linking.addEventListener('url', callback);
+
+      return () => Linking.removeEventListener('url', callback);`,
+      replaceWith: `\
+      const subscription = Linking.addEventListener('url', callback);
+
+      return () => subscription.remove();`,
+    },
+  ]);
+}
+
+async function updateBareExpoAsync() {
+  const gradlePropsFile = path.join(EXPO_DIR, 'apps', 'bare-expo', 'android', 'gradle.properties');
+  let content = await fs.readFile(gradlePropsFile, 'utf8');
+  if (!content.match('reactNativeNightly=true')) {
+    content += `\nreactNativeNightly=true\n`;
+    await fs.writeFile(gradlePropsFile, content);
+  }
+}
