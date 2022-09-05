ee4a129d9645c3ed4cc66a7b6a0ff61fd4ea5163
Simek <gosimek@gmail.com>
2022-09-05 20:40:46 +0200

    config tweaks and cleanup

--- docs/jest.config.js
+++ docs/jest.config.js
@@ -1,27 +1,17 @@
 import nextJest from 'next/jest.js';
 
-/** @type {import('@jest/types').Config.InitialOptions} */
+/** @type {import('jest').Config} */
 const jestConfig = {
   displayName: 'docs',
   testEnvironment: 'jsdom',
   testMatch: ['**/*.test.(js|ts|tsx)'],
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   clearMocks: true,
-  coverageDirectory: '<rootDir>/coverage',
   moduleNameMapper: {
-    // 'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
     '^~/(.*)$': '<rootDir>/$1',
   },
-  // transformIgnorePatterns: [
-  //   'node_modules/(?!react-markdown)/'
-  // ],
-  transform: {},
+  // transform: {},
   // extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  // globals: {
-  //   'ts-jest': {
-  //     useESM: true,
-  //   },
-  // },
   snapshotSerializers: ['@emotion/jest/serializer'],
 };
 
