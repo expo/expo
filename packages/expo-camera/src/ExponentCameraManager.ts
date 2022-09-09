b5e7f58d66ca2360528c51c4b94f8cc87a7c794d
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-09-09 14:40:09 +0200

    [camera][Android] Combine `CameraModule` with `CameraViewManager` (#19049)
    # Why
    
    A follow-up to the https://github.com/expo/expo/pull/18946.
    
    Combines the `CameraModule` with the `CameraViewManager` on Android. Makes sure that we use the same set of module names on both platforms.
    
    Fixes:
    ```
    Cannot read property 'back' of undefined
    ```
    
    # How
    
    - Copied content of the `CameraModule` into the `CameraViewManager`.
    - Adjusted the js layer.
    
    # Test Plan
    
    - NCL ✅

--- packages/expo-camera/src/ExponentCameraManager.ts
+++ packages/expo-camera/src/ExponentCameraManager.ts
@@ -1,6 +1,5 @@
-import { NativeModulesProxy } from 'expo-modules-core';
+import { requireNativeModule } from 'expo-modules-core';
 
-const CameraManager: Record<string, any> =
-  NativeModulesProxy.ExponentCamera || NativeModulesProxy.ExponentCameraModule;
+const CameraManager: Record<string, any> = requireNativeModule('ExponentCamera');
 
 export default CameraManager;
