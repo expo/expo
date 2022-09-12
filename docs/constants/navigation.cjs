02155a46742b48d1ca690da7ba79dd8241fd77e9
Brent Vatne <brentvatne@gmail.com>
2022-09-12 16:07:54 -0700

    [docs] Delete setting up continuous integration guide and redirect

--- docs/constants/navigation.cjs
+++ docs/constants/navigation.cjs
@@ -149,7 +149,6 @@ const general = [
     makePage('guides/how-expo-works.md'),
     makePage('guides/linking.md'),
     makePage('guides/deep-linking.md'),
-    makePage('guides/setting-up-continuous-integration.md'),
     makePage('guides/troubleshooting-proxies.md'),
     makePage('guides/config-plugins.md'),
     makePage('guides/monorepos.md'),
