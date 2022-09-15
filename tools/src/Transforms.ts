expo/expo

commit 21bc6d175837af8eaf3fdf301cf8840011ef1d60

refactor filteredContentTransforms into getTransformedFileContentAsync()

tools/src/Transforms.ts
@@ -43,8 +43,17 @@ async function getTransformedFileContentAsync(
   filePath: string,
   transforms: FileTransform[]
 ): Promise<string> {
+  // Filter out transforms that don't match paths patterns.
+  const filteredContentTransforms =
+    transforms.filter(
+      ({ paths }) =>
+        !paths ||
+        arrayize(paths).some((pattern) => minimatch(filePath, pattern, { matchBase: true }))
+    ) ?? [];
+
+  // Transform source content.
   let result = await fs.readFile(filePath, 'utf8');
-  for (const transform of transforms) {
+  for (const transform of filteredContentTransforms) {
     const beforeTransformation = result;
     result = applySingleTransform(result, transform);
     await maybePrintDebugInfoAsync(beforeTransformation, result, filePath, transform);
@@ -106,17 +115,10 @@ export async function transformFileAsync(
  */
 export async function transformFilesAsync(files: string[], transforms: FileTransform[]) {
   for (const file of files) {
-    // Filter out transforms that don't match paths patterns.
-    const filteredContentTransforms =
-      transforms.filter(
-        ({ paths }) =>
-          !paths || arrayize(paths).some((pattern) => minimatch(file, pattern, { matchBase: true }))
-      ) ?? [];
-
     // Transform source content.
-    const content = await getTransformedFileContentAsync(file, filteredContentTransforms);
+    const content = await getTransformedFileContentAsync(file, transforms);
 
-    // Save transformed source file at renamed target path.
+    // Save transformed content
     await fs.outputFile(file, content);
   }
 }
@@ -134,16 +136,8 @@ export async function copyFileWithTransformsAsync(
   const targetFile = transformString(sourceFile, transforms.path);
   const targetPath = path.join(targetDirectory, targetFile);
 
-  // Filter out transforms that don't match paths patterns.
-  const filteredContentTransforms =
-    transforms.content?.filter(
-      ({ paths }) =>
-        !paths ||
-        arrayize(paths).some((pattern) => minimatch(sourceFile, pattern, { matchBase: true }))
-    ) ?? [];
-
   // Transform source content.
-  const content = await getTransformedFileContentAsync(sourcePath, filteredContentTransforms);
+  const content = await getTransformedFileContentAsync(sourcePath, transforms.content ?? []);
 
   // Save transformed source file at renamed target path.
   await fs.outputFile(targetPath, content);

commit 37accfce4aa84cf8f1aa35303b7f880a8fe46c7b

[tools] Fix copyFileWithTransformsAsync regression (#19107)
# Why

`copyFileWithTransformsAsync` has a regression issue from https://github.com/expo/expo/pull/19078/commits/21bc6d175837af8eaf3fdf301cf8840011ef1d60. this breaks android versioning.

# How

for `copyFileWithTransformsAsync`, it uses the relative `sourceFile` to match the `transform.paths`. my previous change passes `sourcePath`. this pr adds an optional `options.transformMatchPath` to specify the path to do pattern matching.

tools/src/Transforms.ts
@@ -41,15 +41,19 @@ export function transformString(
 
 async function getTransformedFileContentAsync(
   filePath: string,
-  transforms: FileTransform[]
+  transforms: FileTransform[],
+  options?: {
+    // File path to match the `transforms.paths` pattern, e.g. use relative path here
+    transformMatchPath?: string;
+  }
 ): Promise<string> {
   // Filter out transforms that don't match paths patterns.
-  const filteredContentTransforms =
-    transforms.filter(
-      ({ paths }) =>
-        !paths ||
-        arrayize(paths).some((pattern) => minimatch(filePath, pattern, { matchBase: true }))
-    ) ?? [];
+  const sourceFile = options?.transformMatchPath ?? filePath;
+  const filteredContentTransforms = transforms.filter(
+    ({ paths }) =>
+      !paths ||
+      arrayize(paths).some((pattern) => minimatch(sourceFile, pattern, { matchBase: true }))
+  );
 
   // Transform source content.
   let result = await fs.readFile(filePath, 'utf8');
@@ -137,7 +141,9 @@ export async function copyFileWithTransformsAsync(
   const targetPath = path.join(targetDirectory, targetFile);
 
   // Transform source content.
-  const content = await getTransformedFileContentAsync(sourcePath, transforms.content ?? []);
+  const content = await getTransformedFileContentAsync(sourcePath, transforms.content ?? [], {
+    transformMatchPath: sourceFile,
+  });
 
   // Save transformed source file at renamed target path.
   await fs.outputFile(targetPath, content);