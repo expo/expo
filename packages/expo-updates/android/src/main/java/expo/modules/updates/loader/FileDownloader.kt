expo/expo

commit 9bd3e43c43822938bc66f7a0e415ff4fb9099130

[expo-updates] Add log lines for the error scenarios we want to track (#18810)

packages/expo-updates/android/src/main/java/expo/modules/updates/loader/FileDownloader.kt
@@ -1,7 +1,6 @@
 package expo.modules.updates.loader
 
 import android.content.Context
-import android.util.Log
 import expo.modules.jsonutils.require
 import expo.modules.updates.UpdatesConfiguration
 import expo.modules.updates.UpdatesUtils
@@ -25,11 +24,14 @@ import expo.modules.jsonutils.getNullable
 import expo.modules.updates.codesigning.ValidationResult
 import expo.modules.updates.db.UpdatesDatabase
 import expo.modules.updates.db.entity.UpdateEntity
+import expo.modules.updates.logging.UpdatesErrorCode
+import expo.modules.updates.logging.UpdatesLogger
 import expo.modules.updates.manifest.*
 import java.security.cert.CertificateException
 
-open class FileDownloader(private val client: OkHttpClient) {
-  constructor(context: Context) : this(OkHttpClient.Builder().cache(getCache(context)).build())
+open class FileDownloader(context: Context) {
+  private val client = OkHttpClient.Builder().cache(getCache(context)).build()
+  private val logger = UpdatesLogger(context)
 
   interface FileDownloadCallback {
     fun onFailure(e: Exception)
@@ -76,7 +78,7 @@ open class FileDownloader(private val client: OkHttpClient) {
               callback.onSuccess(destination, hash)
             }
           } catch (e: Exception) {
-            Log.e(TAG, "Failed to download file to destination $destination", e)
+            logger.error("Failed to download file to destination $destination: ${e.localizedMessage}", UpdatesErrorCode.AssetsFailedToLoad, e)
             callback.onFailure(e)
           }
         }
@@ -90,9 +92,11 @@ open class FileDownloader(private val client: OkHttpClient) {
     if (isMultipart) {
       val boundaryParameter = ParameterParser().parse(contentType, ';')["boundary"]
       if (boundaryParameter == null) {
+        val message = "Missing boundary in multipart manifest content-type"
+        logger.error(message, UpdatesErrorCode.UpdateFailedToLoad)
         callback.onFailure(
-          "Missing boundary in multipart manifest content-type",
-          IOException("Missing boundary in multipart manifest content-type")
+          message,
+          IOException(message)
         )
         return
       }
@@ -143,7 +147,7 @@ open class FileDownloader(private val client: OkHttpClient) {
         val output = ByteArrayOutputStream()
         multipartStream.readBodyData(output)
 
-        val contentDispositionValue = headers.get("content-disposition")
+        val contentDispositionValue = headers["content-disposition"]
         if (contentDispositionValue != null) {
           val contentDispositionParameterMap = ParameterParser().parse(contentDispositionValue, ';')
           val contentDispositionName = contentDispositionParameterMap["name"]
@@ -158,23 +162,29 @@ open class FileDownloader(private val client: OkHttpClient) {
         nextPart = multipartStream.readBoundary()
       }
     } catch (e: Exception) {
+      val message = "Error while reading multipart manifest response"
+      logger.error(message, UpdatesErrorCode.UpdateFailedToLoad, e)
       callback.onFailure(
-        "Error while reading multipart manifest response",
+        message,
         e
       )
       return
     }
 
     if (manifestPartBodyAndHeaders == null) {
-      callback.onFailure("Multipart manifest response missing manifest part", IOException("Malformed multipart manifest response"))
+      val message = "Multipart manifest response missing manifest part"
+      logger.error(message, UpdatesErrorCode.UpdateFailedToLoad)
+      callback.onFailure(message, IOException("Malformed multipart manifest response"))
       return
     }
 
     val extensions = try {
       extensionsBody?.let { JSONObject(it) }
     } catch (e: Exception) {
+      val message = "Failed to parse multipart manifest extensions"
+      logger.error(message, UpdatesErrorCode.UpdateFailedToLoad)
       callback.onFailure(
-        "Failed to parse multipart manifest extensions",
+        message,
         e
       )
       return
@@ -246,8 +256,10 @@ open class FileDownloader(private val client: OkHttpClient) {
                   callback.onFailure("Failed to parse manifest data", e)
                 }
               } else {
+                val message = "Manifest signature is invalid; aborting"
+                logger.error(message, UpdatesErrorCode.UpdateHasInvalidSignature)
                 callback.onFailure(
-                  "Manifest signature is invalid; aborting",
+                  message,
                   Exception("Manifest signature is invalid")
                 )
               }
@@ -258,8 +270,10 @@ open class FileDownloader(private val client: OkHttpClient) {
         checkCodeSigningAndCreateManifest(manifestBody, preManifest, manifestHeaderData, extensions, certificateChainFromManifestResponse, false, configuration, callback)
       }
     } catch (e: Exception) {
+      val message = "Failed to parse manifest data: ${e.localizedMessage}"
+      logger.error(message, UpdatesErrorCode.UpdateFailedToLoad, e)
       callback.onFailure(
-        "Failed to parse manifest data",
+        message,
         e
       )
     }
@@ -276,8 +290,10 @@ open class FileDownloader(private val client: OkHttpClient) {
         createRequestForManifest(configuration, extraHeaders, context),
         object : Callback {
           override fun onFailure(call: Call, e: IOException) {
+            val message = "Failed to download manifest from URL: ${configuration.updateUrl}: ${e.localizedMessage}"
+            logger.error(message, UpdatesErrorCode.UpdateFailedToLoad, e)
             callback.onFailure(
-              "Failed to download manifest from URL: " + configuration.updateUrl,
+              message,
               e
             )
           }
@@ -285,8 +301,10 @@ open class FileDownloader(private val client: OkHttpClient) {
           @Throws(IOException::class)
           override fun onResponse(call: Call, response: Response) {
             if (!response.isSuccessful) {
+              val message = "Failed to download manifest from URL: ${configuration.updateUrl}"
+              logger.error(message, UpdatesErrorCode.UpdateFailedToLoad)
               callback.onFailure(
-                "Failed to download manifest from URL: " + configuration.updateUrl,
+                message,
                 Exception(
                   response.body!!.string()
                 )
@@ -299,8 +317,10 @@ open class FileDownloader(private val client: OkHttpClient) {
         }
       )
     } catch (e: Exception) {
+      val message = "Failed to download manifest from URL: ${configuration.updateUrl}: ${e.localizedMessage}"
+      logger.error(message, UpdatesErrorCode.UpdateFailedToLoad, e)
       callback.onFailure(
-        "Failed to download manifest from URL " + configuration.updateUrl.toString(),
+        message,
         e
       )
     }
@@ -314,7 +334,9 @@ open class FileDownloader(private val client: OkHttpClient) {
     callback: AssetDownloadCallback
   ) {
     if (asset.url == null) {
-      callback.onFailure(Exception("Could not download asset " + asset.key + " with no URL"), asset)
+      val message = "Could not download asset " + asset.key + " with no URL"
+      logger.error(message, UpdatesErrorCode.AssetsFailedToLoad)
+      callback.onFailure(Exception(message), asset)
       return
     }
     val filename = UpdatesUtils.createFilenameForAsset(asset)
@@ -342,6 +364,7 @@ open class FileDownloader(private val client: OkHttpClient) {
           }
         )
       } catch (e: Exception) {
+        logger.error("Failed to download asset ${asset.key}: ${e.localizedMessage}", UpdatesErrorCode.AssetsFailedToLoad, e)
         callback.onFailure(e, asset)
       }
     }
@@ -458,7 +481,7 @@ open class FileDownloader(private val client: OkHttpClient) {
         for (i in 0 until manifestArray.length()) {
           val manifestCandidate = manifestArray.getJSONObject(i)
           val sdkVersion = manifestCandidate.getString("sdkVersion")
-          if (configuration.sdkVersion != null && configuration.sdkVersion!!.split(",").contains(sdkVersion)
+          if (configuration.sdkVersion != null && configuration.sdkVersion.split(",").contains(sdkVersion)
           ) {
             return manifestCandidate
           }

packages/expo-updates/android/src/main/java/expo/modules/updates/UpdatesController.kt
@@ -30,7 +30,9 @@ import expo.modules.updates.loader.LoaderTask
 import expo.modules.updates.loader.LoaderTask.BackgroundUpdateStatus
 import expo.modules.updates.loader.LoaderTask.LoaderTaskCallback
 import expo.modules.updates.loader.RemoteLoader
+import expo.modules.updates.logging.UpdatesErrorCode
 import expo.modules.updates.logging.UpdatesLogReader
+import expo.modules.updates.logging.UpdatesLogger
 import expo.modules.updates.manifest.UpdateManifest
 import expo.modules.updates.selectionpolicy.SelectionPolicy
 import expo.modules.updates.selectionpolicy.SelectionPolicyFactory
@@ -71,6 +73,7 @@ class UpdatesController private constructor(
     }
   }
 
+  private val logger = UpdatesLogger(context)
   private var isStarted = false
   private var loaderTask: LoaderTask? = null
   private var remoteLoadStatus = ErrorRecoveryDelegate.RemoteLoadStatus.IDLE
@@ -80,7 +83,7 @@ class UpdatesController private constructor(
     UpdatesUtils.getRuntimeVersion(updatesConfiguration)
   )
   val fileDownloader: FileDownloader = FileDownloader(context)
-  private val errorRecovery: ErrorRecovery = ErrorRecovery()
+  private val errorRecovery: ErrorRecovery = ErrorRecovery(context)
 
   private fun setRemoteLoadStatus(status: ErrorRecoveryDelegate.RemoteLoadStatus) {
     remoteLoadStatus = status
@@ -240,6 +243,7 @@ class UpdatesController private constructor(
       selectionPolicy,
       object : LoaderTaskCallback {
         override fun onFailure(e: Exception) {
+          logger.error("UpdatesController loaderTask onFailure: ${e.localizedMessage}", UpdatesErrorCode.None)
           launcher = NoDatabaseLauncher(context, updatesConfiguration, e)
           isEmergencyLaunch = true
           notifyController()
@@ -271,6 +275,7 @@ class UpdatesController private constructor(
               if (exception == null) {
                 throw AssertionError("Background update with error status must have a nonnull exception object")
               }
+              logger.error("UpdatesController onBackgroundUpdateFinished: Error: ${exception.localizedMessage}", UpdatesErrorCode.Unknown, exception)
               remoteLoadStatus = ErrorRecoveryDelegate.RemoteLoadStatus.IDLE
               val params = Arguments.createMap()
               params.putString("message", exception.message)
@@ -281,12 +286,14 @@ class UpdatesController private constructor(
                 throw AssertionError("Background update with error status must have a nonnull update object")
               }
               remoteLoadStatus = ErrorRecoveryDelegate.RemoteLoadStatus.NEW_UPDATE_LOADED
+              logger.info("UpdatesController onBackgroundUpdateFinished: Update available", UpdatesErrorCode.None)
               val params = Arguments.createMap()
               params.putString("manifestString", update.manifest.toString())
               UpdatesUtils.sendEventToReactNative(reactNativeHost, UPDATE_AVAILABLE_EVENT, params)
             }
             BackgroundUpdateStatus.NO_UPDATE_AVAILABLE -> {
               remoteLoadStatus = ErrorRecoveryDelegate.RemoteLoadStatus.IDLE
+              logger.error("UpdatesController onBackgroundUpdateFinished: No update available", UpdatesErrorCode.NoUpdatesAvailable)
               UpdatesUtils.sendEventToReactNative(
                 reactNativeHost,
                 UPDATE_NO_UPDATE_AVAILABLE_EVENT,
@@ -321,6 +328,7 @@ class UpdatesController private constructor(
         val remoteLoader = RemoteLoader(context, updatesConfiguration, database, fileDownloader, updatesDirectory, launchedUpdate)
         remoteLoader.start(object : Loader.LoaderCallback {
           override fun onFailure(e: Exception) {
+            logger.error("UpdatesController loadRemoteUpdate onFailure: ${e.localizedMessage}", UpdatesErrorCode.UpdateFailedToLoad, launchedUpdate?.loggingId, null)
             setRemoteLoadStatus(ErrorRecoveryDelegate.RemoteLoadStatus.IDLE)
             releaseDatabase()
           }

packages/expo-updates/android/src/main/java/expo/modules/updates/db/entity/UpdateEntity.kt
@@ -36,6 +36,11 @@ class UpdateEntity(
 
   var keep = false
 
+  val loggingId: String
+    get() {
+      return id.toString().lowercase()
+    }
+
   @ColumnInfo(name = "last_accessed")
   var lastAccessed: Date = Date()
 

packages/expo-updates/android/src/main/java/expo/modules/updates/errorrecovery/ErrorRecovery.kt
@@ -1,5 +1,6 @@
 package expo.modules.updates.errorrecovery
 
+import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
@@ -8,12 +9,17 @@ import com.facebook.react.bridge.DefaultNativeModuleCallExceptionHandler
 import com.facebook.react.bridge.ReactMarker
 import com.facebook.react.bridge.ReactMarkerConstants
 import com.facebook.react.devsupport.DisabledDevSupportManager
+import expo.modules.updates.logging.UpdatesErrorCode
+import expo.modules.updates.logging.UpdatesLogger
 import java.lang.ref.WeakReference
 import kotlin.Exception
 
-class ErrorRecovery {
+class ErrorRecovery(
+  private val context: Context
+) {
   internal val handlerThread = HandlerThread("expo-updates-error-recovery")
   internal lateinit var handler: Handler
+  internal val logger = UpdatesLogger(context)
 
   private var weakReactInstanceManager: WeakReference<ReactInstanceManager>? = null
   private var previousExceptionHandler: DefaultNativeModuleCallExceptionHandler? = null
@@ -21,7 +27,7 @@ class ErrorRecovery {
   fun initialize(delegate: ErrorRecoveryDelegate) {
     if (!::handler.isInitialized) {
       handlerThread.start()
-      handler = ErrorRecoveryHandler(handlerThread.looper, delegate)
+      handler = ErrorRecoveryHandler(handlerThread.looper, delegate, logger)
     }
   }
 
@@ -31,10 +37,12 @@ class ErrorRecovery {
   }
 
   fun notifyNewRemoteLoadStatus(newStatus: ErrorRecoveryDelegate.RemoteLoadStatus) {
+    logger.info("ErrorRecovery: remote load status changed: $newStatus")
     handler.sendMessage(handler.obtainMessage(ErrorRecoveryHandler.MessageType.REMOTE_LOAD_STATUS_CHANGED, newStatus))
   }
 
   internal fun handleException(exception: Exception) {
+    logger.error("ErrorRecovery: exception encountered: ${exception.localizedMessage}", UpdatesErrorCode.Unknown, exception)
     handler.sendMessage(handler.obtainMessage(ErrorRecoveryHandler.MessageType.EXCEPTION_ENCOUNTERED, exception))
   }
 

packages/expo-updates/android/src/main/java/expo/modules/updates/errorrecovery/ErrorRecoveryHandler.kt
@@ -5,11 +5,14 @@ import android.os.Looper
 import android.os.Message
 import expo.modules.updates.UpdatesConfiguration
 import expo.modules.updates.launcher.Launcher
+import expo.modules.updates.logging.UpdatesErrorCode
+import expo.modules.updates.logging.UpdatesLogger
 import java.lang.RuntimeException
 
 internal class ErrorRecoveryHandler(
   looper: Looper,
-  private val delegate: ErrorRecoveryDelegate
+  private val delegate: ErrorRecoveryDelegate,
+  private val logger: UpdatesLogger
 ) : Handler(looper) {
   private val pipeline = arrayListOf(
     Task.WAIT_FOR_REMOTE_UPDATE,
@@ -65,10 +68,22 @@ internal class ErrorRecoveryHandler(
 
   private fun runNextTask() {
     when (val nextTask = pipeline.removeAt(0)) {
-      Task.WAIT_FOR_REMOTE_UPDATE -> waitForRemoteUpdate()
-      Task.LAUNCH_NEW_UPDATE -> tryRelaunchFromCache() // only called after a new update is downloaded and added to the cache, so the implementation is equivalent
-      Task.LAUNCH_CACHED_UPDATE -> tryRelaunchFromCache()
-      Task.CRASH -> crash()
+      Task.WAIT_FOR_REMOTE_UPDATE -> {
+        logger.info("UpdatesErrorRecovery: attempting to fetch a new update, waiting")
+        waitForRemoteUpdate()
+      }
+      Task.LAUNCH_NEW_UPDATE -> {
+        logger.info("UpdatesErrorRecovery: launching new update")
+        tryRelaunchFromCache()
+      } // only called after a new update is downloaded and added to the cache, so the implementation is equivalent
+      Task.LAUNCH_CACHED_UPDATE -> {
+        logger.info("UpdatesErrorRecovery: falling back to older update")
+        tryRelaunchFromCache()
+      }
+      Task.CRASH -> {
+        logger.error("UpdatesErrorRecovery: could not recover from error, crashing", UpdatesErrorCode.Unknown)
+        crash()
+      }
       else -> throw RuntimeException("ErrorRecoveryHandler cannot perform task $nextTask")
     }
   }

packages/expo-updates/android/src/main/java/expo/modules/updates/logging/UpdatesErrorCode.kt
@@ -11,5 +11,6 @@ enum class UpdatesErrorCode(val code: String) {
   UpdateHasInvalidSignature("UpdateHasInvalidSignature"),
   UpdateFailedToLoad("UpdateFailedToLoad"),
   AssetsFailedToLoad("AssetsFailedToLoad"),
-  JSRuntimeError("JSRuntimeError")
+  JSRuntimeError("JSRuntimeError"),
+  Unknown("Unknown")
 }

packages/expo-updates/android/src/main/java/expo/modules/updates/logging/UpdatesLogger.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import expo.modules.core.logging.LogType
 import expo.modules.core.logging.Logger
 import expo.modules.core.logging.LoggerOptions
+import java.lang.Exception
 import java.util.*
 
 /**
@@ -15,14 +16,14 @@ class UpdatesLogger(
 
   fun trace(
     message: String,
-    code: UpdatesErrorCode
+    code: UpdatesErrorCode = UpdatesErrorCode.None
   ) {
     trace(message, code, null, null)
   }
 
   fun trace(
     message: String,
-    code: UpdatesErrorCode,
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
     updateId: String?,
     assetId: String?
   ) {
@@ -31,14 +32,14 @@ class UpdatesLogger(
 
   fun debug(
     message: String,
-    code: UpdatesErrorCode
+    code: UpdatesErrorCode = UpdatesErrorCode.None
   ) {
     debug(message, code, null, null)
   }
 
   fun debug(
     message: String,
-    code: UpdatesErrorCode,
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
     updateId: String?,
     assetId: String?
   ) {
@@ -47,14 +48,14 @@ class UpdatesLogger(
 
   fun info(
     message: String,
-    code: UpdatesErrorCode
+    code: UpdatesErrorCode = UpdatesErrorCode.None
   ) {
     info(message, code, null, null)
   }
 
   fun info(
     message: String,
-    code: UpdatesErrorCode,
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
     updateId: String?,
     assetId: String?
   ) {
@@ -63,14 +64,14 @@ class UpdatesLogger(
 
   fun warn(
     message: String,
-    code: UpdatesErrorCode
+    code: UpdatesErrorCode = UpdatesErrorCode.None
   ) {
     warn(message, code, null, null)
   }
 
   fun warn(
     message: String,
-    code: UpdatesErrorCode,
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
     updateId: String?,
     assetId: String?
   ) {
@@ -79,34 +80,38 @@ class UpdatesLogger(
 
   fun error(
     message: String,
-    code: UpdatesErrorCode
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
+    exception: Exception? = null
   ) {
-    error(message, code, null, null)
+    error(message, code, null, null, exception)
   }
 
   fun error(
     message: String,
-    code: UpdatesErrorCode,
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
     updateId: String?,
-    assetId: String?
+    assetId: String?,
+    exception: Exception? = null
   ) {
-    logger.error(logEntryString(message, code, LogType.Error, updateId, assetId))
+    logger.error(logEntryString(message, code, LogType.Error, updateId, assetId, exception))
   }
 
   fun fatal(
     message: String,
-    code: UpdatesErrorCode
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
+    exception: Exception? = null
   ) {
-    fatal(message, code, null, null)
+    fatal(message, code, null, null, exception)
   }
 
   fun fatal(
     message: String,
-    code: UpdatesErrorCode,
+    code: UpdatesErrorCode = UpdatesErrorCode.None,
     updateId: String?,
-    assetId: String?
+    assetId: String?,
+    exception: Exception? = null
   ) {
-    logger.fatal(logEntryString(message, code, LogType.Fatal, updateId, assetId))
+    logger.fatal(logEntryString(message, code, LogType.Fatal, updateId, assetId, exception))
   }
 
   // Private methods and fields
@@ -122,15 +127,18 @@ class UpdatesLogger(
     code: UpdatesErrorCode,
     level: LogType,
     updateId: String?,
-    assetId: String?
+    assetId: String?,
+    exception: Exception? = null
   ): String {
     val timestamp = Date().time
 
+    val throwable = exception as? Throwable ?: Throwable()
+
     val stacktrace = when (level) {
       // Limit stack to 20 frames
-      LogType.Error -> Throwable().stackTrace.take(MAX_FRAMES_IN_STACKTRACE)
+      LogType.Error -> throwable.stackTrace.take(MAX_FRAMES_IN_STACKTRACE)
         .map { f -> f.toString() }
-      LogType.Fatal -> Throwable().stackTrace.take(MAX_FRAMES_IN_STACKTRACE)
+      LogType.Fatal -> throwable.stackTrace.take(MAX_FRAMES_IN_STACKTRACE)
         .map { f -> f.toString() }
       else -> {
         null

packages/expo-updates/build/Updates.types.d.ts
@@ -149,7 +149,8 @@ export declare enum UpdatesLogEntryCode {
     UPDATE_HAS_INVALID_SIGNATURE = "UpdateHasInvalidSignature",
     UPDATE_FAILED_TO_LOAD = "UpdateFailedToLoad",
     ASSETS_FAILED_TO_LOAD = "AssetsFailedToLoad",
-    JS_RUNTIME_ERROR = "JSRuntimeError"
+    JS_RUNTIME_ERROR = "JSRuntimeError",
+    UNKNOWN = "Unknown"
 }
 /**
  * The possible log levels for expo-updates log entries

commit c64cec7b032275945c2855d8dde117269738ffac

[updates] Fix android instrumented test error (#19187)
# Why

android instrumented ci tests are broken, e.g. https://github.com/expo/expo/actions/runs/3084322899/jobs/4986338917
this is a regression from #18810 

# How

fix build error

# Test Plan

android instrumented ci test passed: https://github.com/expo/expo/actions/runs/3087190849/jobs/4992309986

packages/expo-updates/android/src/main/java/expo/modules/updates/loader/FileDownloader.kt
@@ -29,8 +29,8 @@ import expo.modules.updates.logging.UpdatesLogger
 import expo.modules.updates.manifest.*
 import java.security.cert.CertificateException
 
-open class FileDownloader(context: Context) {
-  private val client = OkHttpClient.Builder().cache(getCache(context)).build()
+open class FileDownloader(context: Context, private val client: OkHttpClient) {
+  constructor(context: Context) : this(context, OkHttpClient.Builder().cache(getCache(context)).build())
   private val logger = UpdatesLogger(context)
 
   interface FileDownloadCallback {

packages/expo-updates/android/src/androidTest/java/expo/modules/updates/errorrecovery/ErrorRecoveryTest.kt
@@ -2,8 +2,10 @@ package expo.modules.updates.errorrecovery
 
 import android.os.Message
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
 import expo.modules.updates.UpdatesConfiguration
 import expo.modules.updates.launcher.Launcher
+import expo.modules.updates.logging.UpdatesLogger
 import io.mockk.*
 import org.junit.Before
 import org.junit.Test
@@ -12,14 +14,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class ErrorRecoveryTest {
   private var mockDelegate: ErrorRecoveryDelegate = mockk()
-  private var errorRecovery: ErrorRecovery = ErrorRecovery()
+  private val context = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+  private var errorRecovery: ErrorRecovery = ErrorRecovery(context)
 
   @Before
   fun setup() {
     mockDelegate = mockk(relaxed = true)
-    errorRecovery = ErrorRecovery()
+    errorRecovery = ErrorRecovery(context)
     errorRecovery.initialize(mockDelegate)
-    errorRecovery.handler = spyk(ErrorRecoveryHandler(errorRecovery.handlerThread.looper, mockDelegate))
+    errorRecovery.handler = spyk(ErrorRecoveryHandler(errorRecovery.handlerThread.looper, mockDelegate, UpdatesLogger(context)))
     // make handler run synchronously
     val messageSlot = slot<Message>()
     every { errorRecovery.handler.sendMessageAtTime(capture(messageSlot), any()) } answers {

packages/expo-updates/android/src/androidTest/java/expo/modules/updates/loader/FileDownloaderTest.kt
@@ -220,7 +220,7 @@ class FileDownloaderTest {
     var error: Exception? = null
     var didSucceed = false
 
-    FileDownloader(client).downloadAsset(
+    FileDownloader(context, client).downloadAsset(
       assetEntity, File(context.cacheDir, "test"), config, context,
       object : FileDownloader.AssetDownloadCallback {
         override fun onFailure(e: Exception, assetEntity: AssetEntity) {
@@ -268,7 +268,7 @@ class FileDownloaderTest {
     var error: Exception? = null
     var didSucceed = false
 
-    FileDownloader(client).downloadAsset(
+    FileDownloader(context, client).downloadAsset(
       assetEntity, File(context.cacheDir, "test"), config, context,
       object : FileDownloader.AssetDownloadCallback {
         override fun onFailure(e: Exception, assetEntity: AssetEntity) {