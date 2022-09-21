4b553f4125f1b98acce59b20c7f15a90e2589079
evanbacon <bacon@expo.io>
2022-09-21 17:00:16 -0500

    refactor dev tools

--- packages/expo/src/launch/registerRootComponent.tsx
+++ packages/expo/src/launch/registerRootComponent.tsx
@@ -1,13 +1,29 @@
 import 'expo/build/Expo.fx';
+
 import * as React from 'react';
 import { AppRegistry, Platform } from 'react-native';
 
-import withRestoration, { InitialProps } from './withRestoration';
+type InitialProps = {
+  exp: {
+    notification?: any;
+    manifestString?: string;
+    [key: string]: any;
+  };
+  shell?: boolean;
+  shellManifestUrl?: string;
+  [key: string]: any;
+};
 
 export default function registerRootComponent<P extends InitialProps>(
   component: React.ComponentType<P>
 ): void {
-  AppRegistry.registerComponent('main', () => withRestoration(component));
+  if (process.env.NODE_ENV === 'production') {
+    AppRegistry.registerComponent('main', () => component);
+  } else {
+    const { withDevTools } = require('./withDevTools') as typeof import('./withDevTools');
+    AppRegistry.registerComponent('main', () => withDevTools(component));
+  }
+
   if (Platform.OS === 'web') {
     const rootTag = document.getElementById('root') ?? document.getElementById('main');
     AppRegistry.runApplication('main', { rootTag });
