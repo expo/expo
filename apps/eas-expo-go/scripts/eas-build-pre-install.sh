c992750dbc5eae4e741e2e01b988c91363b6ed9c
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2022-09-28 10:59:01 +0200

    [ci] Fix building on EAS with disabled npm cache (#19270)

--- apps/eas-expo-go/scripts/eas-build-pre-install.sh
+++ apps/eas-expo-go/scripts/eas-build-pre-install.sh
@@ -30,7 +30,7 @@ EOF
 
 git submodule update --init
 
-if [ ! -z "$EAS_BUILD_NPM_CACHE_URL" ]; then
+if [ -n "${EAS_BUILD_NPM_CACHE_URL-}" ]; then
   sed -i -e "s#https://registry.yarnpkg.com#$EAS_BUILD_NPM_CACHE_URL#g" $ROOT_DIR/yarn.lock || true
 fi
 
