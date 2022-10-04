expo/expo

commit f9fed44e91dd8caf47d29408a830684d08d04b97

[core] Fix new architecture mode errors on android (#18472)
# Why

on sdk 46, the new architecture mode has a couple errors.

# How

- FabricUIManager is not correctly initialized
  - we return the JSI modules from `ReactNativeHostWrapper`. that is for the `FabricJSIModuleProvider` in `MainApplicationReactNativeHost`.
  - in fabric mode, there's an early call path for `ModuleRegistryAdapter.createViewManagers()`. in the case, the `NativeModulesProxy` is still null.
- crash because [`getDefaultJSExecutorFactory`](https://github.com/facebook/react-native/blob/8524177910a980d6b0a2494d8d9845d833adfdde/ReactAndroid/src/main/java/com/facebook/react/ReactInstanceManagerBuilder.java#L387-L389) calls twice. the static initialization is not catchable.
  - the `MainApplicationReactNativeHost` calls `getReactInstanceManager()` inside its closure, the instance is not the one we created in the ReactNativeHostWrapper. this pr uses reflection to replace the internal `mReactInstanceManager` value for the wrapper ReactNativeHost.
- crash when expo-modules-core setup `ExpoModules` property through jsi to hermes
  - when building expo-modules-core, the hermes is still the old one from `hermes-engine`. in my case i will have crash like `Object::setProperty(Runtime& runtime, const char* name, T&& value)` calls to `symbolToString` (it should be `createStringFromAscii`) because it's inline template in *jsi-inl.h*. this pr tries to use the building from source hermes for expo-modules-core.

# Test Plan

```
$ yarn create expo-app -t blank@sdk-46 sdk46`
$ npx expo prebuild -p android
$ cd android && ./gradlew -PnewArchEnabled=true :app:installDebug
```

(cherry picked from commit 7d2800949d9937b326a68c013883cfcac1febb5d)

packages/expo-modules-core/android/src/main/java/expo/modules/adapters/react/ModuleRegistryAdapter.java
@@ -54,7 +54,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
 
     List<NativeModule> nativeModules = getNativeModulesFromModuleRegistry(reactContext, moduleRegistry);
     if (mWrapperDelegateHolders != null) {
-      KotlinInteropModuleRegistry kotlinInteropModuleRegistry = mModulesProxy.getKotlinInteropModuleRegistry();
+      KotlinInteropModuleRegistry kotlinInteropModuleRegistry = getOrCreateNativeModulesProxy(reactContext).getKotlinInteropModuleRegistry();
       kotlinInteropModuleRegistry.updateModuleHoldersInViewManagers(mWrapperDelegateHolders);
     }
 
@@ -64,8 +64,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
   protected List<NativeModule> getNativeModulesFromModuleRegistry(ReactApplicationContext reactContext, ModuleRegistry moduleRegistry) {
     List<NativeModule> nativeModulesList = new ArrayList<>(2);
 
-    mModulesProxy = createNativeModulesProxy(reactContext, moduleRegistry);
-    nativeModulesList.add(mModulesProxy);
+    nativeModulesList.add(getOrCreateNativeModulesProxy(reactContext));
 
     // Add listener that will notify expo.modules.core.ModuleRegistry when all modules are ready
     nativeModulesList.add(new ModuleRegistryReadyNotifier(moduleRegistry));
@@ -94,8 +93,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
       }
     }
 
-    // We assume that `createNativeModules` was called first.
-    NativeModulesProxy modulesProxy = Objects.requireNonNull(mModulesProxy);
+    NativeModulesProxy modulesProxy = Objects.requireNonNull(getOrCreateNativeModulesProxy(reactContext));
     KotlinInteropModuleRegistry kotlinInteropModuleRegistry = modulesProxy.getKotlinInteropModuleRegistry();
     List<ViewManager<?, ?>> kViewManager = kotlinInteropModuleRegistry.exportViewManagers();
     // Saves all holders that needs to be in sync with module registry
@@ -105,11 +103,15 @@ public class ModuleRegistryAdapter implements ReactPackage {
     return viewManagerList;
   }
 
-  private NativeModulesProxy createNativeModulesProxy(ReactApplicationContext reactContext, ModuleRegistry moduleRegistry) {
-    if (mModulesProvider != null) {
-      return new NativeModulesProxy(reactContext, moduleRegistry, mModulesProvider);
-    } else {
-      return new NativeModulesProxy(reactContext, moduleRegistry);
+  private NativeModulesProxy getOrCreateNativeModulesProxy(ReactApplicationContext reactContext) {
+    if (mModulesProxy == null) {
+      ModuleRegistry moduleRegistry = mModuleRegistryProvider.get(reactContext);
+      if (mModulesProvider != null) {
+        mModulesProxy = new NativeModulesProxy(reactContext, moduleRegistry, mModulesProvider);
+      } else {
+        mModulesProxy = new NativeModulesProxy(reactContext, moduleRegistry);
+      }
     }
+    return mModulesProxy;
   }
 }

packages/expo-modules-core/android/CMakeLists.txt
@@ -47,7 +47,7 @@ target_include_directories(
         "${REACT_NATIVE_DIR}/ReactCommon/react/nativemodule/core"
         "${REACT_NATIVE_DIR}/ReactCommon/callinvoker"
         "${REACT_NATIVE_DIR}/ReactCommon/jsi"
-        "${HERMES_DIR}/android/include"
+        "${HERMES_HEADER_DIR}"
         "${BUILD_DIR}/third-party-ndk/boost/boost_${BOOST_VERSION}"
         "${BUILD_DIR}/third-party-ndk/double-conversion"
         "${BUILD_DIR}/third-party-ndk/folly"

packages/expo-modules-core/android/build.gradle
@@ -71,8 +71,15 @@ def reactNativeArchitectures() {
   return value ? value.split(",") : ["armeabi-v7a", "x86", "x86_64", "arm64-v8a"]
 }
 
+// HERMES
+def prebuiltHermesDir = findProperty("expo.prebuiltHermesDir") ?: file("${REACT_NATIVE_DIR}/ReactAndroid/prebuiltHermes")
+def prebuiltHermesVersion = file("${prebuiltHermesDir}/.hermesversion").exists() ? file("${prebuiltHermesDir}/.hermesversion").text : null
+def currentHermesVersion = file("${REACT_NATIVE_DIR}/sdks/.hermesversion").exists() ? file("${REACT_NATIVE_DIR}/sdks/.hermesversion").text : null
+def hasHermesProject = findProject(":ReactAndroid:hermes-engine") != null
+def prebuiltHermesCacheHit = hasHermesProject && currentHermesVersion == prebuiltHermesVersion
+
 def FOR_HERMES = false
-def HERMES_ENGINE_DIR = null
+def HERMES_HEADER_DIR = null
 def HERMES_AAR = null
 if (findProject(":app")) {
   def appProjectExt = project(":app").ext
@@ -86,23 +93,30 @@ if (findProject(":app")) {
 }
 
 if (FOR_HERMES) {
-  // The `hermes-engine` package doesn't contain the correct version of the Hermes.
-  // The AAR we need to import is located in the React Native package.
-  // However, the version from RN doesn't include header files.
-  // To get those, we must fall back to the `hermes-engine` package.
-  HERMES_ENGINE_DIR = new File(["node", "--print", "require.resolve('hermes-engine/package.json')"].execute(null, rootDir).text.trim()).parent
-  if (REACT_NATIVE_BUILD_FROM_SOURCE) {
-    // TODO(@lukmccall): Use Hermes from the `ReactAndroid:hermes-engine`
-    HERMES_AAR = file("$HERMES_ENGINE_DIR/android/hermes-debug.aar")
+  if (prebuiltHermesCacheHit) {
+    HERMES_HEADER_DIR = file("${thirdPartyNdkDir}/hermes/prefab/modules/libhermes/include")
+    HERMES_AAR = file("${prebuiltHermesDir}/hermes-engine-debug.aar")
+  } else if (hasHermesProject) {
+    HERMES_HEADER_DIR = file("${thirdPartyNdkDir}/hermes/prefab/modules/libhermes/include")
+    HERMES_AAR = file("${REACT_NATIVE_DIR}/ReactAndroid/hermes-engine/build/outputs/aar/hermes-engine-debug.aar")
   } else {
+    // The `hermes-engine` package doesn't contain the correct version of the Hermes.
+    // The AAR we need to import is located in the React Native package.
+    // However, the version from RN doesn't include header files.
+    // To get those, we must fall back to the `hermes-engine` package.
+    def hermesEngineDir = new File(["node", "--print", "require.resolve('hermes-engine/package.json')"].execute(null, rootDir).text.trim()).parent
+
     def prebuiltAAR = fileTree(REACT_NATIVE_DIR).matching { include "**/hermes-engine/**/hermes-engine-*-debug.aar" }
     if (prebuiltAAR.any()) {
       HERMES_AAR = prebuiltAAR.singleFile
     } else {
-      HERMES_AAR = file("$HERMES_ENGINE_DIR/android/hermes-debug.aar")
+      HERMES_AAR = file("${hermesEngineDir}/android/hermes-debug.aar")
     }
+    HERMES_HEADER_DIR = file("${hermesEngineDir}/android/include")
   }
 }
+// END HERMES
+
 
 // Creating sources with comments
 task androidSourcesJar(type: Jar) {
@@ -156,7 +170,7 @@ android {
           "-DREACT_NATIVE_TARGET_VERSION=${REACT_NATIVE_TARGET_VERSION}",
           "-DBOOST_VERSION=${BOOST_VERSION}",
           "-DFOR_HERMES=${FOR_HERMES}",
-          "-DHERMES_DIR=${HERMES_ENGINE_DIR}"
+          "-DHERMES_HEADER_DIR=${HERMES_HEADER_DIR}"
       }
     }
   }
@@ -264,7 +278,8 @@ dependencies {
   androidTestImplementation "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0"
 
   if (FOR_HERMES) {
-    androidTestImplementation files(HERMES_AAR)
+    def hermesProject = findProject(":ReactAndroid:hermes-engine")
+    androidTestImplementation (hermesProject ?: files(HERMES_AAR))
   } else {
     androidTestImplementation "org.webkit:android-jsc:+"
   }
@@ -427,12 +442,14 @@ task prepareHermes(dependsOn: createNativeDepsDirectories) {
     return
   }
 
-  def soFiles = zipTree(HERMES_AAR).matching({ it.include "**/*.so" })
+  doLast {
+    def files = zipTree(HERMES_AAR).matching({ it.include "**/*.so", "prefab/modules/libhermes/include/**/*" })
 
-  copy {
-    from soFiles
-    from "$REACT_NATIVE_DIR/ReactAndroid/src/main/jni/first-party/hermes/Android.mk"
-    into "$thirdPartyNdkDir/hermes"
+    copy {
+      from files
+      from "$REACT_NATIVE_DIR/ReactAndroid/src/main/jni/first-party/hermes/Android.mk"
+      into "$thirdPartyNdkDir/hermes"
+    }
   }
 }
 
@@ -441,6 +458,9 @@ task prepareThirdPartyNdkHeaders(dependsOn: [prepareBoost, prepareDoubleConversi
 afterEvaluate {
   extractAARHeaders.dependsOn(prepareThirdPartyNdkHeaders)
   extractJNIFiles.dependsOn(prepareThirdPartyNdkHeaders)
+  if (hasHermesProject && !prebuiltHermesCacheHit) {
+    prepareHermes.dependsOn(":ReactAndroid:hermes-engine:assembleDebug")
+  }
 }
 
 tasks.whenTaskAdded { task ->

packages/expo/android/src/main/java/expo/modules/ReactNativeHostWrapperBase.kt
@@ -35,6 +35,8 @@ open class ReactNativeHostWrapperBase(
       handler.onDidCreateReactInstanceManager(result, developerSupport)
     }
 
+    injectHostReactInstanceManager(result)
+
     return result
   }
 
@@ -93,8 +95,7 @@ open class ReactNativeHostWrapperBase(
       reactNativeHostHandlers.forEach { handler ->
         handler.onRegisterJSIModules(reactApplicationContext, jsContext, useDeveloperSupport)
       }
-      userJSIModulePackage?.getJSIModules(reactApplicationContext, jsContext)
-      return emptyList()
+      return userJSIModulePackage?.getJSIModules(reactApplicationContext, jsContext)?.toList() ?: emptyList()
     }
   }
 
@@ -107,6 +108,16 @@ open class ReactNativeHostWrapperBase(
       methodMap[name] = method
     }
     return method!!.invoke(host) as T
+  }
+
+  /**
+   * Inject the @{ReactInstanceManager} from the wrapper to the wrapped host.
+   * In case the wrapped host to call `getReactInstanceManager` inside its methods.
+   */
+  fun injectHostReactInstanceManager(reactInstanceManager: ReactInstanceManager) {
+    val mReactInstanceManagerField = ReactNativeHost::class.java.getDeclaredField("mReactInstanceManager")
+    mReactInstanceManagerField.isAccessible = true
+    mReactInstanceManagerField.set(host, reactInstanceManager)
   }
 
   //endregion

commit d98d2d3f680ae075df19bb153f19c721b265e33e

[core] Fix module registry initialized twice (#19384)
# Why

regression from https://github.com/expo/expo/commit/f9fed44e91dd8caf47d29408a830684d08d04b97 and fixed in main. this pr is cherry-picking the relevant commits to sdk-46.

# How

- partially cherry-pick 512e1329940ce158edcdb91bc91a3d4990578eab
- cherry-pick fd55187745b6f953ab41bff4219853571b73accc
- cherry-pick 72841a140323949bdc1863a8173f627a399b1599

# Test Plan

- [bare-expo] add breakpoint at `NotificationsHandler.onCreate` and check how many times it is called when starting up.

Co-authored-by: Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>

packages/expo-modules-core/android/src/main/cpp/JSIInteropModuleRegistry.cpp
@@ -59,7 +59,7 @@ void JSIInteropModuleRegistry::installJSI(
 
 void JSIInteropModuleRegistry::installJSIForTests() {
 #if !UNIT_TEST
-  throw std::logic_error("The function is only avaiable when UNIT_TEST is defined.");
+  throw std::logic_error("The function is only available when UNIT_TEST is defined.");
 #else
   runtimeHolder = std::make_shared<JavaScriptRuntime>();
   jsi::Runtime &jsiRuntime = *runtimeHolder->get();

packages/expo-modules-core/android/src/main/java/expo/modules/adapters/react/ModuleRegistryAdapter.java
@@ -1,5 +1,8 @@
 package expo.modules.adapters.react;
 
+import android.util.Log;
+import androidx.annotation.Nullable;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -46,7 +49,8 @@ public class ModuleRegistryAdapter implements ReactPackage {
 
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    ModuleRegistry moduleRegistry = mModuleRegistryProvider.get(reactContext);
+    NativeModulesProxy proxy = getOrCreateNativeModulesProxy(reactContext, null);
+    ModuleRegistry moduleRegistry = proxy.getModuleRegistry();
 
     for (InternalModule internalModule : mReactAdapterPackage.createInternalModules(reactContext)) {
       moduleRegistry.registerInternalModule(internalModule);
@@ -54,7 +58,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
 
     List<NativeModule> nativeModules = getNativeModulesFromModuleRegistry(reactContext, moduleRegistry);
     if (mWrapperDelegateHolders != null) {
-      KotlinInteropModuleRegistry kotlinInteropModuleRegistry = getOrCreateNativeModulesProxy(reactContext).getKotlinInteropModuleRegistry();
+      KotlinInteropModuleRegistry kotlinInteropModuleRegistry = proxy.getKotlinInteropModuleRegistry();
       kotlinInteropModuleRegistry.updateModuleHoldersInViewManagers(mWrapperDelegateHolders);
     }
 
@@ -64,7 +68,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
   protected List<NativeModule> getNativeModulesFromModuleRegistry(ReactApplicationContext reactContext, ModuleRegistry moduleRegistry) {
     List<NativeModule> nativeModulesList = new ArrayList<>(2);
 
-    nativeModulesList.add(getOrCreateNativeModulesProxy(reactContext));
+    nativeModulesList.add(getOrCreateNativeModulesProxy(reactContext, moduleRegistry));
 
     // Add listener that will notify expo.modules.core.ModuleRegistry when all modules are ready
     nativeModulesList.add(new ModuleRegistryReadyNotifier(moduleRegistry));
@@ -93,7 +97,7 @@ public class ModuleRegistryAdapter implements ReactPackage {
       }
     }
 
-    NativeModulesProxy modulesProxy = Objects.requireNonNull(getOrCreateNativeModulesProxy(reactContext));
+    NativeModulesProxy modulesProxy = Objects.requireNonNull(getOrCreateNativeModulesProxy(reactContext, null));
     KotlinInteropModuleRegistry kotlinInteropModuleRegistry = modulesProxy.getKotlinInteropModuleRegistry();
     List<ViewManager<?, ?>> kViewManager = kotlinInteropModuleRegistry.exportViewManagers();
     // Saves all holders that needs to be in sync with module registry
@@ -103,15 +107,26 @@ public class ModuleRegistryAdapter implements ReactPackage {
     return viewManagerList;
   }
 
-  private NativeModulesProxy getOrCreateNativeModulesProxy(ReactApplicationContext reactContext) {
+  private synchronized NativeModulesProxy getOrCreateNativeModulesProxy(
+    ReactApplicationContext reactContext,
+    @Nullable ModuleRegistry moduleRegistry
+  ) {
+    if (mModulesProxy != null && mModulesProxy.getKotlinInteropModuleRegistry().getWasDestroyed()) {
+      mModulesProxy = null;
+    }
     if (mModulesProxy == null) {
-      ModuleRegistry moduleRegistry = mModuleRegistryProvider.get(reactContext);
+      ModuleRegistry registry = moduleRegistry != null ? moduleRegistry : mModuleRegistryProvider.get(reactContext);
       if (mModulesProvider != null) {
-        mModulesProxy = new NativeModulesProxy(reactContext, moduleRegistry, mModulesProvider);
+        mModulesProxy = new NativeModulesProxy(reactContext, registry, mModulesProvider);
       } else {
-        mModulesProxy = new NativeModulesProxy(reactContext, moduleRegistry);
+        mModulesProxy = new NativeModulesProxy(reactContext, registry);
       }
     }
+
+    if (moduleRegistry != null && moduleRegistry != mModulesProxy.getModuleRegistry()) {
+      Log.e("expo-modules-core", "NativeModuleProxy was configured with a different instance of the modules registry.");
+    }
+
     return mModulesProxy;
   }
 }

packages/expo-modules-core/android/src/main/java/expo/modules/adapters/react/NativeModulesProxy.java
@@ -1,5 +1,6 @@
 package expo.modules.adapters.react;
 
+import android.util.Log;
 import android.util.SparseArray;
 
 import com.facebook.react.bridge.Dynamic;
@@ -127,6 +128,9 @@ public class NativeModulesProxy extends ReactContextBaseJavaModule {
     constants.put(MODULES_CONSTANTS_KEY, modulesConstants);
     constants.put(EXPORTED_METHODS_KEY, exportedMethodsMap);
     constants.put(VIEW_MANAGERS_METADATA_KEY, viewManagersMetadata);
+
+    Log.i("ExpoModulesCore", "✅ Constants was exported");
+
     return constants;
   }
 
@@ -246,5 +250,9 @@ public class NativeModulesProxy extends ReactContextBaseJavaModule {
   public void onCatalystInstanceDestroy() {
     mModuleRegistry.onDestroy();
     mKotlinInteropModuleRegistry.onDestroy();
+  }
+
+  ModuleRegistry getModuleRegistry() {
+    return mModuleRegistry;
   }
 }

packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/AppContext.kt
@@ -5,12 +5,14 @@ package expo.modules.kotlin
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.turbomodule.core.CallInvokerHolderImpl
 import expo.modules.core.errors.ContextDestroyedException
 import expo.modules.core.interfaces.ActivityProvider
+import expo.modules.core.interfaces.JavaScriptContextProvider
 import expo.modules.interfaces.barcodescanner.BarCodeScannerInterface
 import expo.modules.interfaces.camera.CameraViewInterface
 import expo.modules.interfaces.constants.ConstantsInterface
@@ -86,18 +88,25 @@ class AppContext(
    * Initializes a JSI part of the module registry.
    * It will be a NOOP if the remote debugging was activated.
    */
-  fun installJSIInterop() {
-    jsiInterop = JSIInteropModuleRegistry(this)
-    val reactContext = reactContextHolder.get() ?: return
-    reactContext.javaScriptContextHolder?.get()
-      ?.takeIf { it != 0L }
-      ?.let {
-        jsiInterop.installJSI(
-          it,
-          reactContext.catalystInstance.jsCallInvokerHolder as CallInvokerHolderImpl,
-          reactContext.catalystInstance.nativeCallInvokerHolder as CallInvokerHolderImpl
-        )
-      }
+  fun installJSIInterop() = synchronized<Unit>(this) {
+    try {
+      jsiInterop = JSIInteropModuleRegistry(this)
+      val reactContext = reactContextHolder.get() ?: return
+      val jsContextHolder = legacyModule<JavaScriptContextProvider>()?.javaScriptContextRef
+      val catalystInstance = reactContext.catalystInstance ?: return
+      jsContextHolder
+        ?.takeIf { it != 0L }
+        ?.let {
+          jsiInterop.installJSI(
+            it,
+            catalystInstance.jsCallInvokerHolder as CallInvokerHolderImpl,
+            catalystInstance.nativeCallInvokerHolder as CallInvokerHolderImpl
+          )
+          Log.i("ExpoModulesCore", "✅ JSI interop was installed")
+        }
+    } catch (e: Throwable) {
+      Log.e("ExpoModulesCore", "Cannot install JSI interop: $e", e)
+    }
   }
 
   /**

packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/KotlinInteropModuleRegistry.kt
@@ -25,6 +25,9 @@ class KotlinInteropModuleRegistry(
   private val registry: ModuleRegistry
     get() = appContext.registry
 
+  var wasDestroyed = false
+    private set
+
   fun hasModule(name: String): Boolean = registry.hasModule(name)
 
   fun callMethod(moduleName: String, method: String, arguments: ReadableArray, promise: Promise) {
@@ -108,6 +111,7 @@ class KotlinInteropModuleRegistry(
 
   fun onDestroy() {
     appContext.onDestroy()
+    wasDestroyed = true
   }
 
   fun installJSIInterop() {

packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/ModuleRegistry.kt
@@ -74,5 +74,6 @@ class ModuleRegistry(
     forEach {
       it.cleanUp()
     }
+    registry.clear()
   }
 }