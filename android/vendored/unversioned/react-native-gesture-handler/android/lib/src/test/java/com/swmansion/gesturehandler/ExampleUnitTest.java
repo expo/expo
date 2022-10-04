ef1d27898ea8e0137bada8402ac7dfc9d63e3e18
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-10-04 17:53:59 +0200

    Migrate to the new vendoring mechanism

---
+++ android/vendored/unversioned/react-native-gesture-handler/android/lib/src/test/java/com/swmansion/gesturehandler/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.swmansion.gesturehandler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
