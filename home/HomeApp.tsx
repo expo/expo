0bb6a9088d03d010c79c2cbb0e7a4a96ce26bd78
lukmccall <kosmatylukasz@gmail.com>
2022-10-07 13:23:47 +0200

    [iOS][Android] Upgrade react-navigation

--- home/HomeApp.tsx
+++ home/HomeApp.tsx
@@ -2,13 +2,14 @@ import { darkTheme, lightTheme } from '@expo/styleguide-native';
 import Ionicons from '@expo/vector-icons/build/Ionicons';
 import MaterialIcons from '@expo/vector-icons/build/MaterialIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Assets as StackAssets } from '@react-navigation/stack';
+import { Assets as StackAssets } from '@react-navigation/elements';
 import { Asset } from 'expo-asset';
 import { ThemePreference, ThemeProvider } from 'expo-dev-client-components';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
-import { Linking, Platform, StyleSheet, View, useColorScheme } from 'react-native';
+import { Linking, Platform, StyleSheet, View, useColorScheme, Text } from 'react-native';
+import { enableScreens } from 'react-native-screens';
 import url from 'url';
 
 import ApolloClient from './api/ApolloClient';
