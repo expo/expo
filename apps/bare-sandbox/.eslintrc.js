874a24cca030bfad9566f0ef8b9ef999831f850d
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-10-07 14:06:45 +0200

    [apps] Remove bare-sandbox app (#19449)

--- apps/bare-sandbox/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    // Disable React JSX transform warnings because
-    // `jsxRuntime: 'automatic'` is passed to `babel-preset-expo`.
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-  },
-};
--- apps/bare-sandbox/metro.config.js
+++
@@ -1,7 +0,0 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces');
-
-const baseConfig = createMetroConfiguration(__dirname);
-
-module.exports = {
-  ...baseConfig,
-};
