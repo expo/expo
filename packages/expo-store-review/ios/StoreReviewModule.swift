7f3e31132a6d46c37322d255a85759c1539dde1f
Jakov Glavina <jakov.glavina@icloud.com>
2022-10-08 01:34:10 +0200

    [store-review] style changes according to swiftlint

--- packages/expo-store-review/ios/StoreReviewModule.swift
+++ packages/expo-store-review/ios/StoreReviewModule.swift
@@ -8,17 +8,17 @@ public class StoreReviewModule: Module {
     AsyncFunction("isAvailableAsync") { () -> Bool in
       return true
     }
-    
+
     AsyncFunction("requestReview") { (promise: Promise) -> Void in
       if #available(iOS 15, *) {
         guard let currentScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
           return promise.reject(MissingCurrentWindowSceneException())
-        }    
+        }
 
         SKStoreReviewController.requestReview(in: currentScene)
         promise.resolve(nil)
       } else {
-          SKStoreReviewController.requestReview();
+          SKStoreReviewController.requestReview()
           promise.resolve(nil)
         }
     }.runOnQueue(DispatchQueue.main)
