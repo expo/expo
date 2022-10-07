e0694585406fcb745f1eefbee3999ce816ee26fe
Cedric van Putten <me@bycedric.com>
2022-10-07 16:00:33 +0200

    [cli] Add assert with possibly intended variadic argument (#19396)
    * [cli] Add recover to show intended command for npx expo install
    
    * [cli] Fix extraneous space and tests
    
    * [cli] Add changelog entry
    
    * docs(cli): use proper changelog entry
    
    Co-authored-by: Evan Bacon <bacon@expo.io>
    
    Co-authored-by: Evan Bacon <bacon@expo.io>

--- packages/@expo/cli/src/utils/__tests__/variadic-test.ts
+++ packages/@expo/cli/src/utils/__tests__/variadic-test.ts
@@ -1,4 +1,4 @@
-import { parseVariadicArguments } from '../variadic';
+import { parseVariadicArguments, assertUnexpectedVariadicFlags } from '../variadic';
 
 describe(parseVariadicArguments, () => {
   it(`parses complex`, () => {
@@ -23,5 +23,51 @@ describe(parseVariadicArguments, () => {
     expect(() => parseVariadicArguments(['avo', '--', '--npm', '--'])).toThrow(
       /Unexpected multiple --/
     );
+  });
+});
+
+describe(assertUnexpectedVariadicFlags, () => {
+  it(`splits unknown flags into extras`, () => {
+    expect(() =>
+      assertUnexpectedVariadicFlags(['--yarn'], {
+        variadic: ['chalk'],
+        flags: { '-D': true },
+        extras: [],
+      })
+    ).toThrowError('Did you mean: chalk -- -D');
+  });
+
+  it(`splits unknown flags and combines existing extras`, () => {
+    expect(() =>
+      assertUnexpectedVariadicFlags(['--npm'], {
+        variadic: ['chalk'],
+        flags: { '-D': true },
+        extras: ['--ignore-scripts'],
+      })
+    ).toThrowError('Did you mean: chalk -- --ignore-scripts -D');
+  });
+
+  it(`accepts empty variadic`, () => {
+    expect(() =>
+      assertUnexpectedVariadicFlags(['--pnpm'], {
+        variadic: [],
+        flags: { '--ignore-scripts': true, '-D': true },
+        extras: [],
+      })
+    ).toThrowError('Did you mean: -- --ignore-scripts -D');
+  });
+
+  it('prepends command prefix', () => {
+    expect(() =>
+      assertUnexpectedVariadicFlags(
+        ['--yarn'],
+        {
+          variadic: ['chalk'],
+          flags: { '-D': true },
+          extras: [],
+        },
+        'npx expo install'
+      )
+    ).toThrowError('Did you mean: npx expo install chalk -- -D');
   });
 });
