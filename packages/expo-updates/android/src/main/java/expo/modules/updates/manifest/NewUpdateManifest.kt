5564dd19f79133e550ff90e2b92eb6c47497a734
Eric Samelson <eric@expo.io>
2022-10-10 16:35:11 -0700

    add initial docblocks on Android

--- packages/expo-updates/android/src/main/java/expo/modules/updates/manifest/NewUpdateManifest.kt
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/manifest/NewUpdateManifest.kt
@@ -20,6 +20,10 @@ import org.json.JSONObject
 import java.text.ParseException
 import java.util.*
 
+/**
+ * Class for manifests using the modern format defined in the Expo Updates specification
+ * (https://docs.expo.dev/technical-specs/expo-updates-0/). This is used by EAS Update.
+ */
 class NewUpdateManifest private constructor(
   override val manifest: NewManifest,
   private val mId: UUID,
