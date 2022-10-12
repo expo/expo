90150eec7ccac8a1337574ea88444e5b3bb11911
evanbacon <bacon@expo.io>
2022-10-11 17:03:22 -0700

    feat: code sign simulator builds with entitlements

--- packages/@expo/cli/src/run/ios/XcodeBuild.ts
+++ packages/@expo/cli/src/run/ios/XcodeBuild.ts
@@ -11,7 +11,10 @@ import { env } from '../../utils/env';
 import { AbortCommandError, CommandError } from '../../utils/errors';
 import { getUserTerminal } from '../../utils/terminal';
 import { BuildProps, ProjectInfo } from './XcodeBuild.types';
-import { ensureDeviceIsCodeSignedForDeploymentAsync } from './codeSigning/configureCodeSigning';
+import {
+  ensureDeviceIsCodeSignedForDeploymentAsync,
+  simulatorBuildRequiresCodeSigning,
+} from './codeSigning/configureCodeSigning';
 
 export function logPrettyItem(message: string) {
   Log.log(chalk`{whiteBright \u203A} ${message}`);
@@ -140,7 +143,7 @@ export async function getXcodeBuildArgsAsync(
     `id=${props.device.udid}`,
   ];
 
-  if (!props.isSimulator) {
+  if (!props.isSimulator || simulatorBuildRequiresCodeSigning(props.projectRoot)) {
     const developmentTeamId = await ensureDeviceIsCodeSignedForDeploymentAsync(props.projectRoot);
     if (developmentTeamId) {
       args.push(
