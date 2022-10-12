e84ca3d9b8c1c738ef3c57a7e6978a1616f27b17
Jakov Glavina <jakov.glavina@icloud.com>
2022-10-12 10:49:32 +0200

    [ios][battery] Migrate module to Sweet API (#19470)

--- packages/expo-battery/build/ExpoBattery.js
+++ packages/expo-battery/build/ExpoBattery.js
@@ -1,3 +1,3 @@
-import { NativeModulesProxy } from 'expo-modules-core';
-export default NativeModulesProxy.ExpoBattery;
+import { requireNativeModule } from 'expo-modules-core';
+export default requireNativeModule('ExpoBattery');
 //# sourceMappingURL=ExpoBattery.js.map
