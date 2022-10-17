05adcbe3ba6460e2e45142f649c91518c45369c9
Kudo Chien <kudo@expo.dev>
2022-10-18 00:40:21 +0800

    [tools] update ios versioning script for sdk 47

--- tools/src/versioning/ios/transforms/vendoredModulesTransforms.ts
+++ tools/src/versioning/ios/transforms/vendoredModulesTransforms.ts
@@ -92,11 +92,6 @@ export default function vendoredModulesTransformsFactory(prefix: string): Config
         },
         {
           paths: 'REAAnimationsManager.m',
-          find: `UIView+${prefix}React.h`,
-          replaceWith: `UIView+React.h`,
-        },
-        {
-          paths: 'REAAnimationsManager.m',
           // `dataComponenetsByName[@"ABI44_0_0RCTView"];` -> `dataComponenetsByName[@"RCTView"];`
           // the RCTComponentData internal view name is not versioned
           find: new RegExp(`(RCTComponentData .+)\\[@"${prefix}(RCT.+)"\\];`, 'g'),
@@ -132,10 +127,6 @@ export default function vendoredModulesTransformsFactory(prefix: string): Config
       ],
       content: [
         {
-          find: `UIView+${prefix}React.h`,
-          replaceWith: `${prefix}UIView+React.h`,
-        },
-        {
           // `RNG*` symbols are already prefixed at this point,
           // but there are some new symbols in RNGH that don't have "G".
           paths: '*.{h,m,mm}',
@@ -163,20 +154,8 @@ export default function vendoredModulesTransformsFactory(prefix: string): Config
       ],
       content: [
         {
-          find: `UIView+${prefix}React.h`,
-          replaceWith: `${prefix}UIView+React.h`,
-        },
-        {
           find: `${prefix}JKBigInteger.h`,
           replaceWith: `JKBigInteger.h`,
-        },
-      ],
-    },
-    '@react-native-segmented-control/segmented-control': {
-      content: [
-        {
-          find: `UIView+${prefix}React.h`,
-          replaceWith: `${prefix}UIView+React.h`,
         },
       ],
     },
@@ -186,7 +165,7 @@ export default function vendoredModulesTransformsFactory(prefix: string): Config
     '@shopify/react-native-skia': {
       path: [
         {
-          find: /\b(DisplayLink|PlatformContext|SkiaDrawView|SkiaDrawViewManager|SkiaManager)/g,
+          find: /\b(DisplayLink|PlatformContext|SkiaDrawView|SkiaDrawViewManager|SkiaManager|SkiaUIView|SkiaPictureViewManager)/g,
           replaceWith: `${prefix}$1`,
         },
       ],
@@ -197,7 +176,7 @@ export default function vendoredModulesTransformsFactory(prefix: string): Config
           replaceWith: `ReactCommon/${prefix}`,
         },
         {
-          find: /\b(DisplayLink|PlatformContext|SkiaDrawView|SkiaDrawViewManager|SkiaManager|RNJsi)/g,
+          find: /\b(DisplayLink|PlatformContext|SkiaDrawView|SkiaDrawViewManager|SkiaManager|RNJsi|SkiaUIView|SkiaPictureViewManager)/g,
           replaceWith: `${prefix}$1`,
         },
         {
@@ -213,6 +192,54 @@ export default function vendoredModulesTransformsFactory(prefix: string): Config
             'gm'
           ),
           replaceWith: '$1$2',
+        },
+      ],
+    },
+    'react-native-svg': {
+      content: [
+        {
+          find: new RegExp(`\\b(${prefix}RCTConvert)\\+${prefix}(RNSVG\.h)`, 'g'),
+          replaceWith: `$1+$2`,
+        },
+        {
+          paths: 'RNSVGRenderable.mm',
+          find: /\b(saturate)\(/g,
+          replaceWith: `${prefix}$1(`,
+        },
+        {
+          paths: 'RNSVGPainter.mm',
+          find: /\b(PatternFunction)\b/g,
+          replaceWith: `${prefix}$1`,
+        },
+        {
+          paths: 'RNSVGFontData.mm',
+          find: /\b(AbsoluteFontWeight|bolder|lighter|nearestFontWeight)\(/gi,
+          replaceWith: `${prefix}$1(`,
+        },
+        {
+          paths: 'RNSVGTSpan.mm',
+          find: new RegExp(`\\b(${prefix}RNSVGTopAlignedLabel\\s*\\*\\s*label)\\b`, 'gi'),
+          replaceWith: 'static $1',
+        },
+        {
+          paths: 'RNSVGMarker.mm',
+          find: /\b(deg2rad)\b/g,
+          replaceWith: `${prefix}$1`,
+        },
+        {
+          paths: 'RNSVGMarkerPosition.mm',
+          find: /\b(PathIsDone|rad2deg|SlopeAngleRadians|CurrentAngle|subtract|ExtractPathElementFeatures|UpdateFromPathElement)\b/g,
+          replaceWith: `${prefix}$1`,
+        },
+        {
+          paths: 'RNSVGMarkerPosition.mm',
+          find: /\b(positions_|element_index_|origin_|subpath_start_|in_slope_|out_slope_|auto_start_reverse_)\b/g,
+          replaceWith: `${prefix}$1`,
+        },
+        {
+          paths: 'RNSVGPathMeasure.mm',
+          find: /\b(distance|subdivideBezierAtT)\b/g,
+          replaceWith: `${prefix}$1`,
         },
       ],
     },
