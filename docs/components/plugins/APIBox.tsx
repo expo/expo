7ae4a26025607ec073b9551c27ed8c4436f962e5
Bartosz Kaszubowski <gosimek@gmail.com>
2022-10-19 16:55:53 +0200

    [docs] Authentication: refactor components, update UI (#19607)

--- docs/components/plugins/APIBox.tsx
+++ docs/components/plugins/APIBox.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { H3 } from '~/components/plugins/Headings';
 import { STYLES_APIBOX } from '~/components/plugins/api/APISectionUtils';
 import { PlatformName } from '~/types/common';
@@ -8,11 +6,12 @@ import { PlatformTags } from '~/ui/components/Tag';
 type APIBoxProps = React.PropsWithChildren<{
   header?: string;
   platforms?: PlatformName[];
+  className?: string;
 }>;
 
-export const APIBox = ({ header, platforms, children }: APIBoxProps) => {
+export const APIBox = ({ header, platforms, children, className }: APIBoxProps) => {
   return (
-    <div css={STYLES_APIBOX}>
+    <div css={STYLES_APIBOX} className={className}>
       {platforms && <PlatformTags prefix="Only for:" platforms={platforms} />}
       {header && <H3 tags={platforms}>{header}</H3>}
       {children}
