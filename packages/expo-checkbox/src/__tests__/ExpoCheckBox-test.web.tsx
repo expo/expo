730fcf041cc79c2565fae28fd39360306841533a
Brent Vatne <brentvatne@gmail.com>
2022-10-31 13:12:30 -0700

    Fix unused import

--- packages/expo-checkbox/src/__tests__/ExpoCheckBox-test.web.tsx
+++ packages/expo-checkbox/src/__tests__/ExpoCheckBox-test.web.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 
 import Checkbox from '../Checkbox';
