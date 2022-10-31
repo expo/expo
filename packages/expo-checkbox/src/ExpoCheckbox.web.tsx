9c1276c3e0583fe8ef635b3c951f923ed11a5976
Brent Vatne <brentvatne@gmail.com>
2022-10-31 13:46:23 -0700

    Fix web accessibility for checkbox

--- packages/expo-checkbox/src/ExpoCheckbox.web.tsx
+++ packages/expo-checkbox/src/ExpoCheckbox.web.tsx
@@ -42,7 +42,8 @@ const ExpoCheckbox: CheckboxComponent = (props) => {
   );
 
   const nativeControl = createElement('input', {
-    accessibilitystate: { disabled, checked: value },
+    accessibilityChecked: value,
+    accessibilityDisabled: disabled,
     checked: value,
     disabled,
     onChange: handleChange,
