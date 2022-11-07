3c6e52f86ac5df956d488c4924ed48b2c70aa99e
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-11-07 15:36:29 +0100

    [modules][template] Start using `ExpoView` instead of Android's`View` (#19901)
    # Why
    
    Start using `ExpoView` instead of Android's`View`.

--- packages/expo-module-template/android/src/main/java/{%= project.package %}/{%- project.name %}View.kt
+++ packages/expo-module-template/android/src/main/java/{%= project.package %}/{%- project.name %}View.kt
@@ -1,6 +1,7 @@
 package <%- project.package %>
 
 import android.content.Context
-import android.view.View
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
 
-class <%- project.name %>View(context: Context) : View(context)
+class <%- project.name %>View(context: Context, appContext: AppContext) : ExpoView(context, appContext)
