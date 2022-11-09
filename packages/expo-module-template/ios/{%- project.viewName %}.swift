fd0e650d20bc94908e1d974feebb241554ba2278
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-11-09 21:00:55 +0100

    [create-expo-module] Cleanly handle View/Module suffix (#19952)

--- packages/expo-module-template/ios/{%- project.viewName %}.swift
+++ packages/expo-module-template/ios/{%- project.viewName %}.swift
@@ -2,6 +2,6 @@ import ExpoModulesCore
 
 // This view will be used as a native component. Make sure to inherit from `ExpoView`
 // to apply the proper styling (e.g. border radius and shadows).
-class <%- project.name %>View: ExpoView {
+class <%- project.viewName %>: ExpoView {
   
 }
