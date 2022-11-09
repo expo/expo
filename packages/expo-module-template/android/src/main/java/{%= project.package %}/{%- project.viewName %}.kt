451c0ba2dcb2d2064744ead57956b3f3822ff3e6
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-11-09 21:00:55 +0100

    [create-expo-module] Cleanly handle View/Module suffix (#19952)

--- packages/expo-module-template/android/src/main/java/{%= project.package %}/{%- project.viewName %}.kt
+++ packages/expo-module-template/android/src/main/java/{%= project.package %}/{%- project.viewName %}.kt
@@ -4,4 +4,4 @@ import android.content.Context
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
 
-class <%- project.name %>View(context: Context, appContext: AppContext) : ExpoView(context, appContext)
+class <%- project.viewName %>(context: Context, appContext: AppContext) : ExpoView(context, appContext)
