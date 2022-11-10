620d86c5338e1e340f44595e43fe174867709b1b
Alan Hughes <hughesno1@gmail.com>
2022-11-10 11:17:39 +0000

    [andorid][modules] migrate expo-sms to new module api

--- packages/expo-sms/android/src/main/java/expo/modules/sms/SMSModule.kt
+++ packages/expo-sms/android/src/main/java/expo/modules/sms/SMSModule.kt
@@ -2,110 +2,88 @@ package expo.modules.sms
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Telephony
-import android.content.pm.PackageManager
-import android.os.Bundle
-
-import java.util.ArrayList
-
-import expo.modules.core.ExportedModule
+import androidx.core.os.bundleOf
 import expo.modules.core.interfaces.LifecycleEventListener
-import expo.modules.core.ModuleRegistry
-import expo.modules.core.Promise
 import expo.modules.core.interfaces.services.UIManager
-import expo.modules.core.interfaces.ExpoMethod
-import expo.modules.core.interfaces.ActivityProvider
+import expo.modules.kotlin.Promise
+import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
 
-private const val TAG = "ExpoSMS"
-private const val ERROR_TAG = "E_SMS"
 private const val OPTIONS_ATTACHMENTS_KEY = "attachments"
 
-class SMSModule(context: Context, private val smsPackage: String? = null) : ExportedModule(context), LifecycleEventListener {
-  private lateinit var mModuleRegistry: ModuleRegistry
+class SMSModule : Module(), LifecycleEventListener {
   private var mPendingPromise: Promise? = null
   private var mSMSComposerOpened = false
 
-  override fun getName(): String {
-    return TAG
-  }
+  private val context: Context
+    get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
 
-  override fun onCreate(moduleRegistry: ModuleRegistry) {
-    mModuleRegistry = moduleRegistry
-    mModuleRegistry.getModule(UIManager::class.java)?.registerLifecycleEventListener(this)
-  }
+  private val currentActivity
+    get() = appContext.activityProvider?.currentActivity
+      ?: throw MissingCurrentActivityException()
 
-  override fun onDestroy() {
-    // Unregister from old UIManager
-    mModuleRegistry.getModule(UIManager::class.java)?.unregisterLifecycleEventListener(this)
+  override fun definition() = ModuleDefinition {
+    Name("ExpoSMS")
+
+    OnCreate {
+      val uiManager = appContext.legacyModule<UIManager>()
+      uiManager?.registerLifecycleEventListener(this@SMSModule)
+    }
+
+    OnDestroy {
+      val uiManager = appContext.legacyModule<UIManager>()
+      uiManager?.unregisterLifecycleEventListener(this@SMSModule)
+    }
+
+    AsyncFunction("sendSMSAsync") { addresses: ArrayList<String>, message: String, options: Map<String, Any?>?, promise: Promise ->
+      val attachments = options?.get(OPTIONS_ATTACHMENTS_KEY) as? List<*>
+
+      // ACTION_SEND causes a weird flicker on Android 10 devices if the messaging app is not already
+      // open in the background, but it seems to be the only intent type that works for including
+      // attachments, so we use it if there are attachments and fall back to ACTION_SENDTO otherwise.
+      val smsIntent = if (attachments?.isNotEmpty() == true) {
+        Intent(Intent.ACTION_SEND).apply {
+          type = "text/plain"
+          putExtra("address", addresses.joinToString(separator = ";"))
+          val attachment = attachments[0] as? Map<String?, String?>
+          putExtra(Intent.EXTRA_STREAM, Uri.parse(getAttachment(attachment, "uri")))
+          type = getAttachment(attachment, "mimeType")
+          addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+      } else {
+        Intent(Intent.ACTION_SENDTO).apply {
+          data = Uri.parse("smsto:" + addresses.joinToString(separator = ";"))
+        }
+      }
+
+      val defaultSMSPackage = Telephony.Sms.getDefaultSmsPackage(context)
+
+      if (defaultSMSPackage != null) {
+        smsIntent.setPackage(defaultSMSPackage)
+      } else {
+        promise.reject(SMSNoSMSAppException())
+        return@AsyncFunction
+      }
+      smsIntent.putExtra("exit_on_sent", true)
+      smsIntent.putExtra("compose_mode", true)
+      smsIntent.putExtra("sms_body", message)
+      mPendingPromise = promise
+      currentActivity.startActivity(smsIntent)
+
+      mSMSComposerOpened = true
+    }
+
+    AsyncFunction("isAvailableAsync") { promise: Promise ->
+      promise.resolve(context.packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY))
+    }
   }
 
   private fun getAttachment(attachment: Map<String?, String?>?, key: String): String? {
     return attachment?.get(key)
-  }
-
-  @ExpoMethod
-  fun sendSMSAsync(
-    addresses: ArrayList<String>,
-    message: String,
-    options: Map<String?, Any?>?,
-    promise: Promise
-  ) {
-    if (mPendingPromise != null) {
-      promise.reject(
-        ERROR_TAG + "_SENDING_IN_PROGRESS",
-        "Different SMS sending in progress. Await the old request and then try again."
-      )
-      return
-    }
-
-    val attachments = options?.get(OPTIONS_ATTACHMENTS_KEY) as? List<*>
-
-    // ACTION_SEND causes a weird flicker on Android 10 devices if the messaging app is not already
-    // open in the background, but it seems to be the only intent type that works for including
-    // attachments, so we use it if there are attachments and fall back to ACTION_SENDTO otherwise.
-    val smsIntent = if (attachments?.isNotEmpty() == true) {
-      Intent(Intent.ACTION_SEND).apply {
-        type = "text/plain"
-        putExtra("address", addresses.joinToString(separator = ";"))
-        val attachment = attachments[0] as? Map<String?, String?>
-        putExtra(Intent.EXTRA_STREAM, Uri.parse(getAttachment(attachment, "uri")))
-        type = getAttachment(attachment, "mimeType")
-        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-      }
-    } else {
-      Intent(Intent.ACTION_SENDTO).apply {
-        data = Uri.parse("smsto:" + addresses.joinToString(separator = ";"))
-      }
-    }
-
-    val defaultSMSPackage: String?
-    if (smsPackage != null) {
-      defaultSMSPackage = smsPackage
-    } else {
-      defaultSMSPackage = Telephony.Sms.getDefaultSmsPackage(context)
-    }
-
-    if (defaultSMSPackage != null) {
-      smsIntent.setPackage(defaultSMSPackage)
-    } else {
-      promise.reject(ERROR_TAG + "_NO_SMS_APP", "No messaging application available")
-      return
-    }
-    smsIntent.putExtra("exit_on_sent", true)
-    smsIntent.putExtra("compose_mode", true)
-    smsIntent.putExtra("sms_body", message)
-    mPendingPromise = promise
-    val activityProvider = mModuleRegistry.getModule(
-      ActivityProvider::class.java
-    )
-    activityProvider.currentActivity.startActivity(smsIntent)
-    mSMSComposerOpened = true
-  }
-
-  @ExpoMethod
-  fun isAvailableAsync(promise: Promise) {
-    promise.resolve(context.packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY))
   }
 
   override fun onHostResume() {
@@ -115,9 +93,7 @@ class SMSModule(context: Context, private val smsPackage: String? = null) : Expo
       // but this requires READ_SMS permission, which Google is heavily restricting beginning Jan 2019
       // so we just resolve with an unknown value
       promise.resolve(
-        Bundle().apply {
-          putString("result", "unknown")
-        }
+        bundleOf(Pair("result", "unknown"))
       )
       mPendingPromise = null
     }
