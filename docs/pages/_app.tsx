7f031f474f1512ff38d041d8f536fa9a3be77550
Bartosz Kaszubowski <gosimek@gmail.com>
2022-11-10 12:07:20 +0100

    [docs] bump Sentry packages and setup (#19966)

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@expo/styleguide';
 import { MDXProvider } from '@mdx-js/react';
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import { AppProps } from 'next/app';
 import React from 'react';
 
@@ -13,10 +14,14 @@ import { AnalyticsProvider } from '~/providers/Analytics';
 import '@expo/styleguide/dist/expo-theme.css';
 import 'tippy.js/dist/tippy.css';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 Sentry.init({
   dsn: 'https://1a2f5c8cec574bcea3971b74f91504d6@o30871.ingest.sentry.io/1526800',
   beforeSend: preprocessSentryError,
-  environment: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+  environment: isDev ? 'development' : 'production',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
 });
 
 const rootMarkdownComponents = {
