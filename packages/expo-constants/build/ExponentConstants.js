aa86415883bc4aff1bfe51a728c4b5bf43a5525a
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2022-11-11 23:31:14 +0000

    [ios][constants] Migrate expo-constants to new modules API (#20002)

--- packages/expo-constants/build/ExponentConstants.js
+++ packages/expo-constants/build/ExponentConstants.js
@@ -1,3 +1,3 @@
-import { NativeModulesProxy } from 'expo-modules-core';
-export default NativeModulesProxy.ExponentConstants;
+import { requireNativeModule } from 'expo-modules-core';
+export default requireNativeModule('ExponentConstants');
 //# sourceMappingURL=ExponentConstants.js.map
