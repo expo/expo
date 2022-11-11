4131cb9022b3d3e837b09b76944953a5e8187f35
Alan Hughes <hughesno1@gmail.com>
2022-11-11 22:57:20 +0000

    [ios][expo-constants] apply requested changes

--- packages/expo-constants/ios/ConstantsModule.swift
+++ packages/expo-constants/ios/ConstantsModule.swift
@@ -1,36 +1,19 @@
-//
-//  ConstantsModule.swift
-//  ExpoConstants-ExpoConstants
-//
-//  Created by Alan Hughes on 11/11/2022.
-//
-
 import ExpoModulesCore
 import WebKit
 
 public class ConstantsModule: Module {
+  private lazy var constants = appContext?.constants?.constants() as? [String: Any] ?? [:]
 
-    private lazy var constants = {
-        let dict = appContext?.constants?.constants()
+  public func definition() -> ModuleDefinition {
+    Name("ExponentConstants")
 
-        if let result = dict as? [String: Any?] {
-            return result
-        }
-
-        return [String: Any?]()
-    }()
-
-    public func definition() -> ModuleDefinition {
-        Name("ExponentConstants")
-
-        Constants {
-            return constants
-        }
-
-        AsyncFunction("getWebViewUserAgentAsync") { (promise: Promise) in
-            let webView = WKWebView()
-            let result = webView.value(forKey: "userAgent")
-            promise.resolve(result)
-        }.runOnQueue(.main)
+    Constants {
+      return constants
     }
+
+    AsyncFunction("getWebViewUserAgentAsync") { () -> String? in
+      let webView = WKWebView()
+      return webView.value(forKey: "userAgent") as? String
+    }.runOnQueue(.main)
+  }
 }
