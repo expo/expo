91a6ccb8f30fc0eeda2792a9080be88b12508bef
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2022-11-14 14:44:31 +0000

    [android][expo-constants] migrate to new modules API (#19974)
    # Why
    Continue migration of expo modules to new modules API
    
    # How
    Followed a similar approach as with other modules.
    
    # Test Plan
    Ran the test app to verify it is working as before

--- packages/expo-constants/android/src/main/java/expo/modules/constants/ConstantsPackage.kt
+++ packages/expo-constants/android/src/main/java/expo/modules/constants/ConstantsPackage.kt
@@ -4,12 +4,8 @@ import android.content.Context
 
 import expo.modules.core.BasePackage
 import expo.modules.core.interfaces.InternalModule
-import expo.modules.core.ExportedModule
 
 class ConstantsPackage : BasePackage() {
   override fun createInternalModules(context: Context): List<InternalModule> =
     listOf(ConstantsService(context))
-
-  override fun createExportedModules(context: Context): List<ExportedModule> =
-    listOf(ConstantsModule(context))
 }
