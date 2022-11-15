2628682001abf24d9108f69c4386a7d284c24be1
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-11-15 18:17:51 +0200

    [image] Refactor existing types of expo-image and examples (#20008)

--- packages/expo-image/src/ExpoImage.web.tsx
+++ packages/expo-image/src/ExpoImage.web.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { ImageProps } from './Image';
-
-export default function ExpoImage({ source, ...props }: ImageProps) {
+export default function ExpoImage({ source, ...props }) {
   const resolvedSource = source ?? {};
 
-  // @ts-expect-error - expo-image is being reworked so these types should be revisited
   return <Image {...props} source={resolvedSource} />;
 }
