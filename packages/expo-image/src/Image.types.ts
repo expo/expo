9f622630975c3953719a3308eb10a8685d6f3592
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2022-11-15 18:03:35 +0200

    Apply review suggestions [skip ci]
    Co-authored-by: Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>

--- packages/expo-image/src/Image.types.ts
+++ packages/expo-image/src/Image.types.ts
@@ -90,7 +90,7 @@ export enum ImageResizeMode {
   CONTAIN = 'contain',
   /**
    * The image will be stretched to fill the entire frame of the view without clipping.
-   * This may change the aspect ratio of the image, distoring it.
+   * This may change the aspect ratio of the image, distorting it.
    *
    * @platform ios
    */
