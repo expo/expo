06ed534ae6c271d4a6ddde83244fe146adec9665
Simek <gosimek@gmail.com>
2022-11-18 15:20:55 +0100

    [emc] fix bar code scanner interfaces names

--- packages/expo-modules-core/ios/Interfaces/BarcodeScanner/EXBarCodeScannerProviderInterface.h
+++ packages/expo-modules-core/ios/Interfaces/BarcodeScanner/EXBarCodeScannerProviderInterface.h
@@ -1,6 +1,6 @@
 // Copyright 2016-present 650 Industries. All rights reserved.
 
-#import <ExpoModulesCore/EXBarcodeScannerInterface.h>
+#import <ExpoModulesCore/EXBarCodeScannerInterface.h>
 
 @protocol EXBarCodeScannerProviderInterface
 
