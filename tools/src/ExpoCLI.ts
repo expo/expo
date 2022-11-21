expo/expo

commit 722d7fd4ec9b60f1e6ec0ff9eecdaf03ae8b82e9

[expotools] Fix generate-bare-app (#19657)
* [expotools] Fix generate-bare-app

* Update tools/src/commands/GenerateBareApp.ts

Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

* Use create-expo-app; remove unneeded CLI dependency

Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

tools/src/ExpoCLI.ts
@@ -17,7 +17,26 @@ export async function runExpoCliAsync(
   process.on('SIGINT', () => {});
   process.on('SIGTERM', () => {});
 
-  await spawnAsync('expo', [command, ...args], {
+  await spawnAsync('npx', ['expo', command, ...args], {
+    cwd: options.cwd || EXPO_DIR,
+    stdio: options.stdio || 'inherit',
+    env: {
+      ...process.env,
+      EXPO_NO_DOCTOR: 'true',
+    },
+  });
+}
+
+export async function runCreateExpoAppAsync(
+  name: string,
+  args: string[] = [],
+  options: Options = {}
+): Promise<void> {
+  // Don't handle SIGINT/SIGTERM in this process...defer to expo-cli
+  process.on('SIGINT', () => {});
+  process.on('SIGTERM', () => {});
+
+  await spawnAsync('npx', ['create-expo-app', name, ...args], {
     cwd: options.cwd || EXPO_DIR,
     stdio: options.stdio || 'inherit',
     env: {

tools/src/commands/GenerateBareApp.ts
@@ -4,14 +4,14 @@ import fs from 'fs-extra';
 import path from 'path';
 
 import { EXPO_DIR, PACKAGES_DIR } from '../Constants';
-import { runExpoCliAsync } from '../ExpoCLI';
+import { runExpoCliAsync, runCreateExpoAppAsync } from '../ExpoCLI';
 import { GitDirectory } from '../Git';
 
 export type GenerateBareAppOptions = {
   name?: string;
   template?: string;
   clean?: boolean;
-  localTemplate?: boolean;
+  useLocalTemplate?: boolean;
   outDir?: string;
   rnVersion?: string;
 };
@@ -22,7 +22,7 @@ export async function action(
     name: appName = 'my-generated-bare-app',
     outDir = 'bare-apps',
     template = 'expo-template-bare-minimum',
-    localTemplate = false,
+    useLocalTemplate = false,
     clean = false,
     rnVersion,
   }: GenerateBareAppOptions
@@ -36,12 +36,12 @@ export async function action(
   const packagesToSymlink = await getPackagesToSymlink({ packageNames, workspaceDir });
 
   await cleanIfNeeded({ clean, projectDir, workspaceDir });
-  await createProjectDirectory({ workspaceDir, appName, template, localTemplate });
+  await createProjectDirectory({ workspaceDir, appName, template, useLocalTemplate });
   await modifyPackageJson({ packagesToSymlink, projectDir });
   await modifyAppJson({ projectDir, appName });
   await yarnInstall({ projectDir });
   await symlinkPackages({ packagesToSymlink, projectDir });
-  await runExpoPrebuild({ projectDir });
+  await runExpoPrebuild({ projectDir, useLocalTemplate });
   if (rnVersion != null) {
     await updateRNVersion({ rnVersion, projectDir });
   }
@@ -82,20 +82,27 @@ async function createProjectDirectory({
   workspaceDir,
   appName,
   template,
-  localTemplate,
+  useLocalTemplate,
 }: {
   workspaceDir: string;
   appName: string;
   template: string;
-  localTemplate: boolean;
+  useLocalTemplate: boolean;
 }) {
-  if (localTemplate) {
-    const pathToBareTemplate = path.resolve(EXPO_DIR, 'templates', 'expo-template-bare-minimum');
-    const pathToWorkspace = path.resolve(workspaceDir, appName);
-    return fs.copy(pathToBareTemplate, pathToWorkspace, { recursive: true });
+  if (useLocalTemplate) {
+    // If useLocalTemplate is selected, find the path to the local copy of the template and use that
+    const pathToLocalTemplate = path.resolve(EXPO_DIR, 'templates', template);
+    return await runCreateExpoAppAsync(
+      appName,
+      ['--no-install', '--template', pathToLocalTemplate],
+      {
+        cwd: workspaceDir,
+        stdio: 'inherit',
+      }
+    );
   }
 
-  return await runExpoCliAsync('init', [appName, '--no-install', '--template', template], {
+  return await runCreateExpoAppAsync(appName, ['--no-install', '--template', template], {
     cwd: workspaceDir,
     stdio: 'ignore',
   });
@@ -241,8 +248,30 @@ function getLocalReactNativeVersion() {
   return mainPkg.resolutions?.['react-native'];
 }
 
-async function runExpoPrebuild({ projectDir }: { projectDir: string }) {
+async function runExpoPrebuild({
+  projectDir,
+  useLocalTemplate,
+}: {
+  projectDir: string;
+  useLocalTemplate: boolean;
+}) {
   console.log('Applying config plugins');
+  if (useLocalTemplate) {
+    const pathToBareTemplate = path.resolve(EXPO_DIR, 'templates', 'expo-template-bare-minimum');
+    const templateVersion = require(path.join(pathToBareTemplate, 'package.json')).version;
+    await spawnAsync('npm', ['pack', '--pack-destination', projectDir], {
+      cwd: pathToBareTemplate,
+      stdio: 'ignore',
+    });
+    const tarFilePath = path.resolve(
+      projectDir,
+      `expo-template-bare-minimum-${templateVersion}.tgz`
+    );
+    await runExpoCliAsync('prebuild', ['--no-install', '--template', tarFilePath], {
+      cwd: projectDir,
+    });
+    return await fs.rm(tarFilePath);
+  }
   return await runExpoCliAsync('prebuild', ['--no-install'], { cwd: projectDir });
 }
 
@@ -329,10 +358,14 @@ export default (program: Command) => {
     .option('-c, --clean', 'Rebuilds the project from scratch')
     .option('--rnVersion <string>', 'Version of react-native to include')
     .option('-o, --outDir <string>', 'Specifies the directory to build the project in')
-    .option('-t, --template <string>', 'Specify the expo template to use as the project starter')
     .option(
-      '--localTemplate',
-      'Copy the localTemplate expo-template-bare-minimum from the expo repo',
+      '-t, --template <string>',
+      'Specify the expo template to use as the project starter',
+      'expo-template-bare-minimum'
+    )
+    .option(
+      '--useLocalTemplate',
+      'If true, use the local copy of the template instead of the published template in NPM',
       false
     )
     .description(`Generates a bare app with the specified packages symlinked`)

commit 954febcc7fe433db4c37a2a0d74960d35043720f

[expotools] fix publish-dev-home (#20076)
# Why

Temporary fix for the `publish-dev-home` tool, after it was broken by
#19657

# How

Add a new method in `ExpoCLI.ts` to call the legacy global expo-cli.

# Test Plan

- Test this locally
- Come up with a plan to migrate this tool to use EAS Update.

tools/src/ExpoCLI.ts
@@ -27,6 +27,29 @@ export async function runExpoCliAsync(
   });
 }
 
+/**
+ * Added to support tools that require the `expo publish` command
+ * that is only available in the old global expo-cli
+ */
+export async function runLegacyExpoCliAsync(
+  command: string,
+  args: string[] = [],
+  options: Options = {}
+): Promise<void> {
+  // Don't handle SIGINT/SIGTERM in this process...defer to expo-cli
+  process.on('SIGINT', () => {});
+  process.on('SIGTERM', () => {});
+
+  await spawnAsync('expo', [command, ...args], {
+    cwd: options.cwd || EXPO_DIR,
+    stdio: options.stdio || 'inherit',
+    env: {
+      ...process.env,
+      EXPO_NO_DOCTOR: 'true',
+    },
+  });
+}
+
 export async function runCreateExpoAppAsync(
   name: string,
   args: string[] = [],

tools/src/XDL.ts
@@ -29,7 +29,8 @@ export async function publishProjectWithExpoCliAsync(
 
     if (username && password) {
       Log.collapsed('Logging in...');
-      await ExpoCLI.runExpoCliAsync('login', ['-u', username, '-p', password]);
+      // TODO: rework this to use EAS update instead of expo publish
+      await ExpoCLI.runLegacyExpoCliAsync('login', ['-u', username, '-p', password]);
     } else {
       Log.collapsed('Expo username and password not specified. Using currently logged-in account.');
     }
@@ -42,7 +43,8 @@ export async function publishProjectWithExpoCliAsync(
     publishArgs.push('--max-workers', '1');
   }
 
-  await ExpoCLI.runExpoCliAsync('publish', publishArgs, {
+  // TODO: rework this to use EAS update instead of expo publish
+  await ExpoCLI.runLegacyExpoCliAsync('publish', publishArgs, {
     cwd: projectRoot,
   });
 }

tools/src/commands/PublishDevExpoHomeCommand.ts
@@ -182,7 +182,8 @@ async function action(options: ActionOptions): Promise<void> {
 
   if (cliUsername) {
     console.log(`Logging out from ${chalk.green(cliUsername)} account...`);
-    await ExpoCLI.runExpoCliAsync('logout', [], {
+    // TODO: rework this to use EAS update instead of expo publish
+    await ExpoCLI.runLegacyExpoCliAsync('logout', [], {
       stdio: 'ignore',
     });
   }