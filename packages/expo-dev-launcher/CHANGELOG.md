d0c2b85aa83f1956779df9f13fe9140e65e98203
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-11-21 11:03:34 +0100

    [dev-client] Fix `RCTStatusBarManager` module requires that the `UIViewControllerBasedStatusBarAppearance` to be false (#20104)
    # Why
    
    Fixes https://github.com/expo/expo/issues/20047.
    
    # How
    
    Made sure that `StatusBar` won't be imported by any dev-client code. 
    
    # Test Plan
    
    - bare-expo ✅
    - simple app outside of the expo/expo repo

--- packages/expo-dev-launcher/CHANGELOG.md
+++ packages/expo-dev-launcher/CHANGELOG.md
@@ -8,6 +8,8 @@
 
 ### 🐛 Bug fixes
 
+- Fixed `RCTStatusBarManager` module requires that the `UIViewControllerBasedStatusBarAppearance` to be false on iOS. ([#20104](https://github.com/expo/expo/pull/20104) by [@lukmccall](https://github.com/lukmccall))
+
 ### 💡 Others
 
 - Locked `layoutDirection` to LTR to prevent incorrect rendering when used together with a RTL enabled app. ([#19634](https://github.com/expo/expo/pull/19634) by [@aleqsio](https://github.com/aleqsio))
--- packages/expo-dev-menu/metro.config.js
+++ packages/expo-dev-menu/metro.config.js
@@ -1,5 +1,5 @@
 const { createMetroConfiguration } = require('expo-yarn-workspaces');
-
+const path = require('path');
 const config = createMetroConfiguration(__dirname);
 
 config.server = {
@@ -26,5 +26,14 @@ if (EXPO_BUNDLE_APP) {
 }
 
 config.resolver.blockList.push(/\breact-native-lab\b/);
-
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'ios' && /Components\/StatusBar\/StatusBar/.test(moduleName)) {
+    console.log(`Replacing ${moduleName} with NOOP`);
+    return {
+      type: 'sourceFile',
+      filePath: path.join(__dirname, 'app', 'StatusBarMock.js'),
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 module.exports = config;
