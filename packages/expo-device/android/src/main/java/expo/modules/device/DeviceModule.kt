836d9476b117bb893f94930cd1a7f18e8372e897
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2022-11-23 09:37:01 +0000

    [android][expo-device] migrate to new modules API (#20118)
    # Why
    Migrate module to use new modules API
    
    # How
    Followed the usual steps involved in migrating a module
    
    # Test Plan
    Tested using the bare expo app. Continues to work as before.

--- packages/expo-device/android/src/main/java/expo/modules/device/DeviceModule.kt
+++ packages/expo-device/android/src/main/java/expo/modules/device/DeviceModule.kt
@@ -1,12 +1,5 @@
 package expo.modules.device
 
-import expo.modules.core.ExportedModule
-import expo.modules.core.Promise
-import expo.modules.core.interfaces.ExpoMethod
-import expo.modules.core.utilities.EmulatorUtilities
-
-import com.facebook.device.yearclass.YearClass
-
 import android.app.ActivityManager
 import android.app.UiModeManager
 import android.content.Context
@@ -14,16 +7,20 @@ import android.content.res.Configuration
 import android.os.Build
 import android.os.SystemClock
 import android.provider.Settings
-import android.view.WindowManager
 import android.util.DisplayMetrics
-
+import android.view.WindowManager
+import com.facebook.device.yearclass.YearClass
+import expo.modules.core.utilities.EmulatorUtilities
+import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
 import java.io.File
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 private const val NAME = "ExpoDevice"
 
-class DeviceModule(private val mContext: Context) : ExportedModule(mContext) {
+class DeviceModule : Module() {
   // Keep this enum in sync with JavaScript
   enum class DeviceType(val JSValue: Int) {
     UNKNOWN(0),
@@ -33,78 +30,57 @@ class DeviceModule(private val mContext: Context) : ExportedModule(mContext) {
     TV(4);
   }
 
-  override fun getName(): String {
-    return NAME
-  }
+  private val context: Context
+    get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
 
-  override fun getConstants(): Map<String, Any> = mapOf(
-    "isDevice" to !isRunningOnEmulator,
-    "brand" to Build.BRAND,
-    "manufacturer" to Build.MANUFACTURER,
-    "modelName" to Build.MODEL,
-    "designName" to Build.DEVICE,
-    "productName" to Build.DEVICE,
-    "deviceYearClass" to deviceYearClass,
-    "totalMemory" to run {
-      val memoryInfo = ActivityManager.MemoryInfo()
-      (mContext.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getMemoryInfo(memoryInfo)
-      memoryInfo.totalMem
-    },
-    "supportedCpuArchitectures" to run {
-      var supportedAbis = Build.SUPPORTED_ABIS
-      if (supportedAbis != null && supportedAbis.isEmpty()) {
-        supportedAbis = null
-      }
-      supportedAbis
-    },
-    "osName" to systemName,
-    "osVersion" to Build.VERSION.RELEASE,
-    "osBuildId" to Build.DISPLAY,
-    "osInternalBuildId" to Build.ID,
-    "osBuildFingerprint" to Build.FINGERPRINT,
-    "platformApiLevel" to Build.VERSION.SDK_INT,
-    "deviceName" to run {
-      if (Build.VERSION.SDK_INT <= 31)
-        Settings.Secure.getString(mContext.contentResolver, "bluetooth_name")
-      else
-        Settings.Global.getString(mContext.contentResolver, Settings.Global.DEVICE_NAME)
-    },
-  )
+  override fun definition() = ModuleDefinition {
+    Name("ExpoDevice")
 
-  private val deviceYearClass: Int
-    get() = YearClass.get(mContext)
-
-  private val systemName: String
-    get() {
-      return if (Build.VERSION.SDK_INT < 23) {
-        "Android"
-      } else {
-        Build.VERSION.BASE_OS.takeIf { it.isNotEmpty() } ?: "Android"
-      }
+    Constants {
+      return@Constants mapOf(
+        "isDevice" to !isRunningOnEmulator,
+        "brand" to Build.BRAND,
+        "manufacturer" to Build.MANUFACTURER,
+        "modelName" to Build.MODEL,
+        "designName" to Build.DEVICE,
+        "productName" to Build.DEVICE,
+        "deviceYearClass" to deviceYearClass,
+        "totalMemory" to run {
+          val memoryInfo = ActivityManager.MemoryInfo()
+          (context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).getMemoryInfo(memoryInfo)
+          memoryInfo.totalMem
+        },
+        "supportedCpuArchitectures" to Build.SUPPORTED_ABIS?.takeIf { it.isNotEmpty() },
+        "osName" to systemName,
+        "osVersion" to Build.VERSION.RELEASE,
+        "osBuildId" to Build.DISPLAY,
+        "osInternalBuildId" to Build.ID,
+        "osBuildFingerprint" to Build.FINGERPRINT,
+        "platformApiLevel" to Build.VERSION.SDK_INT,
+        "deviceName" to if (Build.VERSION.SDK_INT <= 31)
+          Settings.Secure.getString(context.contentResolver, "bluetooth_name")
+        else
+          Settings.Global.getString(context.contentResolver, Settings.Global.DEVICE_NAME)
+      )
     }
 
-  @ExpoMethod
-  fun getDeviceTypeAsync(promise: Promise) {
-    promise.resolve(getDeviceType(mContext).JSValue)
-  }
+    AsyncFunction("getDeviceTypeAsync") {
+      return@AsyncFunction getDeviceType(context).JSValue
+    }
 
-  @ExpoMethod
-  fun getUptimeAsync(promise: Promise) {
-    promise.resolve(SystemClock.uptimeMillis().toDouble())
-  }
+    AsyncFunction("getUptimeAsync") {
+      return@AsyncFunction SystemClock.uptimeMillis().toDouble()
+    }
 
-  @ExpoMethod
-  fun getMaxMemoryAsync(promise: Promise) {
-    val maxMemory = Runtime.getRuntime().maxMemory()
-    promise.resolve(if (maxMemory != Long.MAX_VALUE) maxMemory.toDouble() else -1)
-  }
+    AsyncFunction("getMaxMemoryAsync") {
+      val maxMemory = Runtime.getRuntime().maxMemory()
+      return@AsyncFunction if (maxMemory != Long.MAX_VALUE) maxMemory.toDouble() else -1
+    }
 
-  @ExpoMethod
-  fun isRootedExperimentalAsync(promise: Promise) {
-    var isRooted = false
-    val isDevice = !isRunningOnEmulator
+    AsyncFunction("isRootedExperimentalAsync") {
+      val isRooted: Boolean
+      val isDevice = !isRunningOnEmulator
 
-    try {
       val buildTags = Build.TAGS
       isRooted = if (isDevice && buildTags != null && buildTags.contains("test-keys")) {
         true
@@ -115,44 +91,43 @@ class DeviceModule(private val mContext: Context) : ExportedModule(mContext) {
           isDevice && File("/system/xbin/su").exists()
         }
       }
-    } catch (se: SecurityException) {
-      promise.reject(
-        "ERR_DEVICE_ROOT_DETECTION",
-        "Could not access the file system to determine if the device is rooted.",
-        se
-      )
-      return
+
+      return@AsyncFunction isRooted
     }
 
-    promise.resolve(isRooted)
-  }
-
-  @ExpoMethod
-  fun isSideLoadingEnabledAsync(promise: Promise) {
-    val enabled: Boolean = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-      Settings.Global.getInt(
-        mContext.applicationContext.contentResolver,
-        Settings.Global.INSTALL_NON_MARKET_APPS,
-        0
-      ) == 1
-    } else {
-      mContext.applicationContext.packageManager.canRequestPackageInstalls()
+    AsyncFunction("isSideLoadingEnabledAsync") {
+      return@AsyncFunction if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+        Settings.Global.getInt(
+          context.applicationContext.contentResolver,
+          Settings.Global.INSTALL_NON_MARKET_APPS,
+          0
+        ) == 1
+      } else {
+        context.applicationContext.packageManager.canRequestPackageInstalls()
+      }
     }
 
-    promise.resolve(enabled)
+    AsyncFunction("getPlatformFeaturesAsync") {
+      val allFeatures = context.applicationContext.packageManager.systemAvailableFeatures
+      return@AsyncFunction allFeatures.filterNotNull().map { it.name }
+    }
+
+    AsyncFunction("hasPlatformFeatureAsync") { feature: String ->
+      return@AsyncFunction context.applicationContext.packageManager.hasSystemFeature(feature)
+    }
   }
 
-  @ExpoMethod
-  fun getPlatformFeaturesAsync(promise: Promise) {
-    val allFeatures = mContext.applicationContext.packageManager.systemAvailableFeatures
-    val featureList = allFeatures.filterNotNull().map { it.name }
-    promise.resolve(featureList)
-  }
+  private val deviceYearClass: Int
+    get() = YearClass.get(context)
 
-  @ExpoMethod
-  fun hasPlatformFeatureAsync(feature: String, promise: Promise) {
-    promise.resolve(mContext.applicationContext.packageManager.hasSystemFeature(feature))
-  }
+  private val systemName: String
+    get() {
+      return if (Build.VERSION.SDK_INT < 23) {
+        "Android"
+      } else {
+        Build.VERSION.BASE_OS.takeIf { it.isNotEmpty() } ?: "Android"
+      }
+    }
 
   companion object {
     private val TAG = DeviceModule::class.java.simpleName
@@ -172,7 +147,8 @@ class DeviceModule(private val mContext: Context) : ExportedModule(mContext) {
       }
 
       // Find the current window manager, if none is found we can't measure the device physical size.
-      val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager? ?: return DeviceType.UNKNOWN
+      val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager?
+        ?: return DeviceType.UNKNOWN
 
       // Get display metrics to see if we can differentiate phones and tablets.
       val metrics = DisplayMetrics()
@@ -182,7 +158,7 @@ class DeviceModule(private val mContext: Context) : ExportedModule(mContext) {
       val widthInches = metrics.widthPixels / metrics.xdpi.toDouble()
       val heightInches = metrics.heightPixels / metrics.ydpi.toDouble()
       val diagonalSizeInches = sqrt(widthInches.pow(2.0) + heightInches.pow(2.0))
-      return if (diagonalSizeInches >= 3.0 && diagonalSizeInches <= 6.9) {
+      return if (diagonalSizeInches in 3.0..6.9) {
         // Devices in a sane range for phones are considered to be phones.
         DeviceType.PHONE
       } else if (diagonalSizeInches > 6.9 && diagonalSizeInches <= 18.0) {
