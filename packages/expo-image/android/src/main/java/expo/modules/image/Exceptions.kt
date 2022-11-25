bb00f82b0c4a9a38ee0ef911770f7557f9e3c3f4
Kudo Chien <kudo@expo.dev>
2022-11-25 13:10:21 +0800

    [image] spotlessApply to fix ci failure

--- packages/expo-image/android/src/main/java/expo/modules/image/Exceptions.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/Exceptions.kt
@@ -5,5 +5,5 @@ import expo.modules.kotlin.exception.CodedException
 class ImagePrefetchFailure(message: String) :
   CodedException(message = "Failed to prefetch the image: $message")
 
-class MissingActivity
-  : CodedException(message = "The current activity is no longer available")
+class MissingActivity :
+  CodedException(message = "The current activity is no longer available")
