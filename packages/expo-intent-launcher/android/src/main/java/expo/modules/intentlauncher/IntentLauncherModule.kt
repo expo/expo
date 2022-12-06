d36b6c7f1d2a823535829c05bb5012a016f0a357
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2022-12-06 15:23:37 +0000

    [android][expo-intent-launcher] Migrate the new modules API (#20327)
    # Why
    Continue migrating modules to use the new API
    
    # How
    Followed the usual migration steps
    
    # Test Plan
    Tested with the bare expo app. Everything working as before.

--- packages/expo-intent-launcher/android/src/main/java/expo/modules/intentlauncher/IntentLauncherModule.kt
+++ packages/expo-intent-launcher/android/src/main/java/expo/modules/intentlauncher/IntentLauncherModule.kt
@@ -1,120 +1,84 @@
 package expo.modules.intentlauncher
 
-import expo.modules.core.Promise
-import expo.modules.core.ExportedModule
-import expo.modules.core.ModuleRegistry
-import expo.modules.core.interfaces.ExpoMethod
-import expo.modules.core.ModuleRegistryDelegate
-import expo.modules.core.arguments.ReadableArguments
-import expo.modules.core.interfaces.ActivityProvider
-import expo.modules.core.interfaces.services.UIManager
-import expo.modules.core.interfaces.ActivityEventListener
-import expo.modules.core.errors.CurrentActivityNotFoundException
-import expo.modules.intentlauncher.exceptions.ActivityAlreadyStartedException
-
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.app.Activity
-import android.content.Intent
-import android.content.ComponentName
-import android.content.ActivityNotFoundException
-import android.content.Context
-
-private const val NAME = "ExpoIntentLauncher"
+import androidx.core.os.bundleOf
+import expo.modules.intentlauncher.exceptions.ActivityAlreadyStartedException
+import expo.modules.kotlin.Promise
+import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
 
 private const val REQUEST_CODE = 12
-private const val ATTR_ACTION = "action"
-private const val ATTR_TYPE = "type"
-private const val ATTR_CATEGORY = "category"
 private const val ATTR_EXTRA = "extra"
 private const val ATTR_DATA = "data"
-private const val ATTR_FLAGS = "flags"
-private const val ATTR_PACKAGE_NAME = "packageName"
-private const val ATTR_CLASS_NAME = "className"
 
-class IntentLauncherModule(
-  context: Context,
-  private val moduleRegistryDelegate: ModuleRegistryDelegate = ModuleRegistryDelegate()
-) : ExportedModule(context), ActivityEventListener {
+class IntentLauncherModule : Module() {
+  private val context: Context
+    get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
+  private val currentActivity
+    get() = appContext.currentActivity ?: throw Exceptions.MissingActivity()
   private var pendingPromise: Promise? = null
-  private val uiManager: UIManager by moduleRegistry()
-  private val activityProvider: ActivityProvider by moduleRegistry()
 
-  private inline fun <reified T> moduleRegistry() = moduleRegistryDelegate.getFromModuleRegistry<T>()
+  override fun definition() = ModuleDefinition {
+    Name("ExpoIntentLauncher")
 
-  override fun getName() = NAME
-
-  override fun onCreate(moduleRegistry: ModuleRegistry) {
-    moduleRegistryDelegate.onCreate(moduleRegistry)
-  }
-
-  @ExpoMethod
-  fun startActivity(activityAction: String, params: ReadableArguments, promise: Promise) {
-    if (pendingPromise != null) {
-      promise.reject(ActivityAlreadyStartedException())
-      return
-    }
-
-    val activity = activityProvider.currentActivity
-    if (activity == null) {
-      promise.reject(CurrentActivityNotFoundException())
-      return
-    }
-
-    val intent = Intent(activityAction)
-
-    if (params.containsKey(ATTR_CLASS_NAME)) {
-      intent.component =
-        if (params.containsKey(ATTR_PACKAGE_NAME)) ComponentName(params.getString(ATTR_PACKAGE_NAME), params.getString(ATTR_CLASS_NAME))
-        else ComponentName(context, params.getString(ATTR_CLASS_NAME))
-    }
-
-    // `setData` and `setType` are exclusive, so we need to use `setDateAndType` in that case.
-    if (params.containsKey(ATTR_DATA) && params.containsKey(ATTR_TYPE)) {
-      intent.setDataAndType(Uri.parse(params.getString(ATTR_DATA)), params.getString(ATTR_TYPE))
-    } else {
-      if (params.containsKey(ATTR_DATA)) {
-        intent.data = Uri.parse(params.getString(ATTR_DATA))
-      } else if (params.containsKey(ATTR_TYPE)) {
-        intent.type = params.getString(ATTR_TYPE)
+    AsyncFunction("startActivity") { activityAction: String, params: IntentLauncherParams, promise: Promise ->
+      if (pendingPromise != null) {
+        throw ActivityAlreadyStartedException()
       }
+      val intent = Intent(activityAction)
+
+      params.className?.let {
+        intent.component =
+          if (params.packageName != null) {
+            ComponentName(params.packageName, params.className)
+          } else {
+            ComponentName(context, params.className)
+          }
+      }
+
+      // `setData` and `setType` are exclusive, so we need to use `setDateAndType` in that case.
+      if (params.data != null && params.type != null) {
+        intent.setDataAndType(Uri.parse(params.data), params.type)
+      } else {
+        intent.apply {
+          if (params.data != null) {
+            data = Uri.parse(params.data)
+          } else if (params.type != null) {
+            type = params.type
+          }
+        }
+      }
+
+      params.extra?.let { intent.putExtras(it.toBundle()) }
+      params.flags?.let { intent.addFlags(it) }
+      params.category?.let { intent.addCategory(it) }
+
+      pendingPromise = promise
+      currentActivity.startActivityForResult(intent, REQUEST_CODE)
     }
 
-    params.getArguments(ATTR_EXTRA)?.let { intent.putExtras(it.toBundle()) }
-    params.getInt(ATTR_FLAGS)?.let { intent.addFlags(it) }
-    params.getString(ATTR_CATEGORY)?.let { intent.addCategory(it) }
+    OnActivityResult { _, payload ->
+      if (payload.requestCode != REQUEST_CODE) {
+        return@OnActivityResult
+      }
 
-    uiManager.registerActivityEventListener(this)
-    pendingPromise = promise
+      val response = Bundle().apply {
+        putInt("resultCode", payload.resultCode)
+        if (payload.data != null) {
+          payload.data?.let { putString(ATTR_DATA, it.toString()) }
+          payload.data?.extras?.let { putBundle(ATTR_EXTRA, it) }
+        }
+      }
 
-    try {
-      activity.startActivityForResult(intent, REQUEST_CODE)
-    } catch (e: ActivityNotFoundException) {
-      promise.reject(e)
+      pendingPromise?.resolve(response)
       pendingPromise = null
     }
   }
-
-  //region ActivityEventListener
-
-  override fun onActivityResult(activity: Activity, requestCode: Int, resultCode: Int, intent: Intent?) {
-    if (requestCode != REQUEST_CODE) return
-
-    val response = Bundle().apply {
-      putInt("resultCode", resultCode)
-      if (intent != null) {
-        intent.data?.let { putString(ATTR_DATA, it.toString()) }
-        intent.extras?.let { putBundle(ATTR_EXTRA, it) }
-      }
-    }
-
-    pendingPromise?.resolve(response)
-    pendingPromise = null
-
-    uiManager.unregisterActivityEventListener(this)
-  }
-
-  override fun onNewIntent(intent: Intent) = Unit
-
-  //endregion
 }
+
+private fun Map<String, Any>.toBundle(): Bundle = bundleOf(*this.toList().toTypedArray())
--- packages/expo-intent-launcher/src/ExpoIntentLauncher.ts
+++ packages/expo-intent-launcher/src/ExpoIntentLauncher.ts
@@ -1,2 +1,2 @@
-import { NativeModulesProxy } from 'expo-modules-core';
-export default NativeModulesProxy.ExpoIntentLauncher || {};
+import { requireNativeModule } from 'expo-modules-core';
+export default requireNativeModule('ExpoIntentLauncher');
