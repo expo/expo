a67292b04c89349448627798f9c826b566018659
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2022-12-06 14:15:02 +0000

    [android][expo-sharing] migrate to new modules API (#20112)
    # Why
    Migrate module to new modules API
    
    # How
    Followed the usual steps involved in migrating a module
    
    # Test Plan
    Tested in the bare expo app and all works as before. I removed onActivityResult as all it did was resolve the promise with null but I don't think this is necessary anymore with the new API. It can easily be put back if that was not correct.

--- packages/expo-sharing/android/src/main/java/expo/modules/sharing/SharingModule.kt
+++ packages/expo-sharing/android/src/main/java/expo/modules/sharing/SharingModule.kt
@@ -1,81 +1,69 @@
 package expo.modules.sharing
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.core.content.FileProvider
-import expo.modules.core.ExportedModule
-import expo.modules.core.ModuleRegistry
-import expo.modules.core.ModuleRegistryDelegate
-import expo.modules.core.Promise
-import expo.modules.core.arguments.ReadableArguments
 import expo.modules.core.errors.InvalidArgumentException
-import expo.modules.core.interfaces.ActivityEventListener
-import expo.modules.core.interfaces.ActivityProvider
-import expo.modules.core.interfaces.ExpoMethod
-import expo.modules.core.interfaces.services.UIManager
-import expo.modules.interfaces.filesystem.FilePermissionModuleInterface
 import expo.modules.interfaces.filesystem.Permission
+import expo.modules.kotlin.Promise
+import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
 import java.io.File
 import java.net.URLConnection
 
-class SharingModule(
-  context: Context,
-  private val moduleRegistryDelegate: ModuleRegistryDelegate = ModuleRegistryDelegate()
-) : ExportedModule(context), ActivityEventListener {
+class SharingModule : Module() {
+  private val context: Context
+    get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
+  private val currentActivity
+    get() = appContext.currentActivity ?: throw MissingCurrentActivityException()
   private var pendingPromise: Promise? = null
-  private val uiManager: UIManager by moduleRegistry()
-  override fun getName() = "ExpoSharing"
 
-  private inline fun <reified T> moduleRegistry() =
-    moduleRegistryDelegate.getFromModuleRegistry<T>()
+  override fun definition() = ModuleDefinition {
+    Name("ExpoSharing")
 
-  override fun onCreate(moduleRegistry: ModuleRegistry) {
-    moduleRegistryDelegate.onCreate(moduleRegistry)
-    uiManager.registerActivityEventListener(this)
-  }
-
-  override fun onDestroy() {
-    uiManager.unregisterActivityEventListener(this)
-  }
-
-  @ExpoMethod
-  fun shareAsync(url: String?, params: ReadableArguments, promise: Promise) {
-    if (pendingPromise != null) {
-      promise.reject("ERR_SHARING_MUL", "Another share request is being processed now.")
-      return
-    }
-    try {
-      val fileToShare = getLocalFileFoUrl(url)
-      val contentUri = FileProvider.getUriForFile(
-        context,
-        context.applicationInfo.packageName + ".SharingFileProvider",
-        fileToShare
-      )
-      val mimeType = params.getString(MIME_TYPE_OPTIONS_KEY)
-        ?: URLConnection.guessContentTypeFromName(fileToShare.name)
-        ?: "*/*"
-      val intent = Intent.createChooser(
-        createSharingIntent(contentUri, mimeType),
-        params.getString(DIALOG_TITLE_OPTIONS_KEY)
-      )
-      val resInfoList = context.packageManager.queryIntentActivities(
-        intent,
-        PackageManager.MATCH_DEFAULT_ONLY
-      )
-      resInfoList.forEach {
-        val packageName = it.activityInfo.packageName
-        context.grantUriPermission(packageName, contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    AsyncFunction("shareAsync") { url: String?, params: SharingOptions, promise: Promise ->
+      if (pendingPromise != null) {
+        throw SharingInProgressException()
       }
-      val activityProvider: ActivityProvider by moduleRegistry()
-      activityProvider.currentActivity.startActivityForResult(intent, REQUEST_CODE)
-      pendingPromise = promise
-    } catch (e: InvalidArgumentException) {
-      promise.reject("ERR_SHARING_URL", e.message, e)
-    } catch (e: Exception) {
-      promise.reject("ERR_SHARING", "Failed to share the file: " + e.message, e)
+      try {
+        val fileToShare = getLocalFileFoUrl(url)
+        val contentUri = FileProvider.getUriForFile(
+          context,
+          context.applicationInfo.packageName + ".SharingFileProvider",
+          fileToShare
+        )
+        val mimeType = params.mimeType
+          ?: URLConnection.guessContentTypeFromName(fileToShare.name)
+          ?: "*/*"
+        val intent = Intent.createChooser(
+          createSharingIntent(contentUri, mimeType),
+          params.dialogTitle
+        )
+        val resInfoList = context.packageManager.queryIntentActivities(
+          intent,
+          PackageManager.MATCH_DEFAULT_ONLY
+        )
+        resInfoList.forEach {
+          val packageName = it.activityInfo.packageName
+          context.grantUriPermission(packageName, contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+        pendingPromise = promise
+        currentActivity.startActivity(intent)
+      } catch (e: InvalidArgumentException) {
+        throw SharingInvalidArgsException(e.message, e)
+      } catch (e: Exception) {
+        throw SharingFailedException("Failed to share the file: ${e.message}", e)
+      }
+    }
+
+    OnActivityResult { _, (requestCode) ->
+      if (requestCode == REQUEST_CODE && pendingPromise != null) {
+        pendingPromise?.resolve(null)
+        pendingPromise = null
+      }
     }
   }
 
@@ -97,8 +85,9 @@ class SharingModule(
   }
 
   private fun isAllowedToRead(url: String?): Boolean {
-    val permissionModuleInterface: FilePermissionModuleInterface by moduleRegistry()
-    return permissionModuleInterface.getPathPermissions(context, url).contains(Permission.READ)
+    val permissions = appContext.filePermission
+    return permissions?.getPathPermissions(context, url)?.contains(Permission.READ)
+      ?: false
   }
 
   private fun createSharingIntent(uri: Uri, mimeType: String?) =
@@ -108,18 +97,7 @@ class SharingModule(
       addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
     }
 
-  override fun onActivityResult(activity: Activity, requestCode: Int, resultCode: Int, data: Intent?) {
-    if (requestCode == REQUEST_CODE && pendingPromise != null) {
-      pendingPromise?.resolve(null)
-      pendingPromise = null
-    }
-  }
-
-  override fun onNewIntent(intent: Intent) = Unit
-
   companion object {
     private const val REQUEST_CODE = 8524
-    private const val MIME_TYPE_OPTIONS_KEY = "mimeType"
-    private const val DIALOG_TITLE_OPTIONS_KEY = "dialogTitle"
   }
 }
