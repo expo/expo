77ca2563711192a794ad16a38d335490c3840b5b
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-12-07 10:07:44 +0100

    [image][Android] Apply cache fix from fast image for local assets (#20347)
    # Why
    
    Closes ENG-6961.
    Related to https://github.com/DylanVann/react-native-fast-image/pull/472.
    
    # How
    
    More information was provided in the linked PR.
    
    # Test Plan
    
    - NCL with bare-expo ✅

--- packages/expo-image/android/src/main/java/expo/modules/image/records/SourceMap.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/records/SourceMap.kt
@@ -1,9 +1,14 @@
 package expo.modules.image.records
 
+import android.content.Context
+import android.net.Uri
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.Headers
 import com.bumptech.glide.load.model.LazyHeaders
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ApplicationVersionSignature
+import com.facebook.react.views.imagehelper.ResourceDrawableIdHelper
+import expo.modules.image.GlideOptions
 import expo.modules.kotlin.records.Field
 import expo.modules.kotlin.records.Record
 
@@ -18,13 +23,24 @@ data class SourceMap(
     GlideUrl(it, getCustomHeaders())
   }
 
-  internal fun createOptions(): RequestOptions {
+  internal fun createOptions(context: Context): RequestOptions {
     return RequestOptions()
       .apply {
         // Override the size for local assets. This ensures that
         // resizeMode "center" displays the image in the correct size.
         if (width != null && height != null && scale != null) {
           override((width * scale).toInt(), (height * scale).toInt())
+        }
+
+        if (isResourceUri(context)) {
+          // Every local resource (drawable) in Android has its own unique numeric id, which are
+          // generated at build time. Although these ids are unique, they are not guaranteed unique
+          // across builds. The underlying glide implementation caches these resources. To make
+          // sure the cache does not return the wrong image, we should clear the cache when the
+          // application version changes.
+          apply(
+            GlideOptions.signatureOf(ApplicationVersionSignature.obtain(context))
+          )
         }
       }
   }
@@ -42,5 +58,28 @@ data class SourceMap(
         }
       }
       .build()
+  }
+
+  private fun isResourceUri(context: Context): Boolean {
+    return "android.resource" == computeUri(context)?.scheme
+  }
+
+  private fun computeUri(context: Context): Uri? {
+    val stringUri = uri ?: return null
+    return try {
+      val uri: Uri = Uri.parse(stringUri)
+      // Verify scheme is set, so that relative uri (used by static resources) are not handled.
+      if (uri.scheme == null) {
+        computeLocalUri(stringUri, context)
+      } else {
+        uri
+      }
+    } catch (e: Exception) {
+      computeLocalUri(stringUri, context)
+    }
+  }
+
+  private fun computeLocalUri(stringUri: String, context: Context): Uri? {
+    return ResourceDrawableIdHelper.getInstance().getResourceDrawableUri(context, stringUri)
   }
 }
