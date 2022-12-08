4370292d0ed65e59c47ba2c904d1c4c6cda5f47c
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2022-12-08 12:26:12 +0100

    [image][Android] Improve `onLoad`, `onProgress` and `onError` events (#20364)
    # Why
    
    Improves and adds some small tweaks to `onLoad`, `onProgress`, and `onError` events
    
    # How
    
    - Fixed the `onProgress` was called twice when the image finished downloading. 
    - Fixed the `onLoad` returning incorrect cache type. 
    - Improved the error message in the `onError` event. Turn off annoying Glide's logs to improve general readability - we log those messages anyway 🤷 
    
    # Test Plan
    
    - event screen

--- packages/expo-image/android/src/main/java/expo/modules/image/events/GlideRequestListener.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/events/GlideRequestListener.kt
@@ -1,6 +1,7 @@
 package expo.modules.image.events
 
 import android.graphics.drawable.Drawable
+import android.util.Log
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
@@ -11,6 +12,7 @@ import expo.modules.image.records.ImageErrorEvent
 import expo.modules.image.records.ImageLoadEvent
 import expo.modules.image.records.ImageSource
 import java.lang.ref.WeakReference
+import java.util.*
 
 class GlideRequestListener(
   private val expoImageViewWrapper: WeakReference<ExpoImageViewWrapper>
@@ -21,10 +23,20 @@ class GlideRequestListener(
     target: Target<Drawable>,
     isFirstResource: Boolean
   ): Boolean {
+    val errorMessage = e
+      ?.message
+      // Glide always append that line to the end of the message.
+      // It's not possible to call `logRootCauses` from the JS, so we decided to remove it.
+      ?.removeSuffix("\n call GlideException#logRootCauses(String) for more detail")
+      ?: "Unknown error"
+
     expoImageViewWrapper
       .get()
       ?.onError
-      ?.invoke(ImageErrorEvent(e.toString()))
+      ?.invoke(ImageErrorEvent(errorMessage))
+
+    Log.e("ExpoImage", errorMessage)
+    e?.logRootCauses("ExpoImage")
     return false
   }
 
@@ -37,7 +49,7 @@ class GlideRequestListener(
   ): Boolean {
     expoImageViewWrapper.get()?.onLoad?.invoke(
       ImageLoadEvent(
-        cacheType = ImageCacheType.fromNativeValue(dataSource).enumValue,
+        cacheType = ImageCacheType.fromNativeValue(dataSource).name.lowercase(Locale.getDefault()),
         source = ImageSource(
           url = model.toString(),
           width = resource.intrinsicWidth,
