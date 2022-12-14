465648296f33b64d1d50610b80691563067b797f
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-12-14 15:29:09 +0100

    [image][ios] Add support for blurhash

---
+++ apps/native-component-list/src/screens/Image/ImageBlurHashScreen.tsx
@@ -0,0 +1,37 @@
+import { Image, ImageCachePolicy, ImageContentFit } from 'expo-image';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Colors } from '../../constants';
+
+// A blurhash of https://picsum.photos/seed/175/300/200 with 6x4 components
+const blurhash = 'WgF}G?az0fs.x[jat7xFRjNHt6s.4;oe-:RkVtkCi^Nbo|xZRjWB';
+
+export default function ImageBlurhashScreen() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{ uri: 'https://picsum.photos/seed/175/3000/2000' }}
+        placeholder={{
+          uri: encodeURI(`blurhash:/${blurhash}/18/12`).replace(/#/g, '%23').replace(/\?/g, '%3F'),
+        }}
+        transition={1000}
+        contentFit={ImageContentFit.COVER}
+        cachePolicy={ImageCachePolicy.NONE}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  image: {
+    height: 200,
+    borderWidth: 1,
+    borderColor: Colors.border,
+  },
+});
