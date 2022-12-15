b9aa8b98a4fc9bf958ec4b4068a9a76ad5bfc716
lukmccall <kosmatylukasz@gmail.com>
2022-12-15 18:25:21 +0100

    [image][Android] Fix reading width and height of the blurhash source

--- packages/expo-image/android/src/main/java/expo/modules/image/blurhash/BlurhashModelLoaderFactory.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/blurhash/BlurhashModelLoaderFactory.kt
@@ -4,9 +4,10 @@ import android.graphics.Bitmap
 import com.bumptech.glide.load.model.ModelLoader
 import com.bumptech.glide.load.model.ModelLoaderFactory
 import com.bumptech.glide.load.model.MultiModelLoaderFactory
+import expo.modules.image.GlideBlurhashModel
 
-class BlurhashModelLoaderFactory : ModelLoaderFactory<String, Bitmap> {
-  override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<String, Bitmap> =
+class BlurhashModelLoaderFactory : ModelLoaderFactory<GlideBlurhashModel, Bitmap> {
+  override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<GlideBlurhashModel, Bitmap> =
     BlurhashModelLoader()
 
   override fun teardown() = Unit
