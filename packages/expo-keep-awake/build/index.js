6387a72b91551779c0cdc3b562e774b9308bdafa
Bartosz Kaszubowski <gosimek@gmail.com>
2022-12-19 14:19:19 +0100

    [keep-awake] define `KeepAwakeOptions` type, update doc comments (#20489)

--- packages/expo-keep-awake/build/index.js
+++ packages/expo-keep-awake/build/index.js
@@ -1,19 +1,17 @@
 import { useEffect } from 'react';
 import ExpoKeepAwake from './ExpoKeepAwake';
+/**
+ * The default tag used, when no tag has been specified in keep awake method calls.
+ */
 export const ExpoKeepAwakeTag = 'ExpoKeepAwakeDefaultTag';
-// @needsAudit
 /**
  * A React hook to keep the screen awake for as long as the owner component is mounted.
  * The optionally provided `tag` argument is used when activating and deactivating the keep-awake
  * feature. If unspecified, the default `tag` is used. See the documentation for `activateKeepAwake`
  * below to learn more about the `tag` argument.
  *
- * @param tag *Optional* - Tag to lock screen sleep prevention. If not provided, the default tag is used.
- * @param options *Optional*
- *   - `suppressDeactivateWarnings` *Optional* -
- *      The call will throw an unhandled promise rejection on Android
- *      when the original Activity is dead or deactivated.
- *      Set the value to true for suppressing the uncaught exception.
+ * @param tag Tag to lock screen sleep prevention. If not provided, the default tag is used.
+ * @param options Additional options for the keep awake hook.
  */
 export function useKeepAwake(tag = ExpoKeepAwakeTag, options) {
     useEffect(() => {
@@ -35,7 +33,7 @@ export function useKeepAwake(tag = ExpoKeepAwakeTag, options) {
  * If the `tag` argument is specified, the screen will not sleep until you call `deactivateKeepAwake`
  * with the same `tag` argument. When using multiple `tags` for activation you'll have to deactivate
  * each one in order to re-enable screen sleep. If tag is unspecified, the default `tag` is used.
- * @param tag *Optional* - Tag to lock screen sleep prevention. If not provided, the default tag is used.
+ * @param tag Tag to lock screen sleep prevention. If not provided, the default tag is used.
  */
 export async function activateKeepAwake(tag = ExpoKeepAwakeTag) {
     await ExpoKeepAwake.activate?.(tag);
@@ -44,7 +42,7 @@ export async function activateKeepAwake(tag = ExpoKeepAwakeTag) {
 /**
  * Releases the lock on screen-sleep prevention associated with the given `tag` value. If `tag`
  * is unspecified, it defaults to the same default tag that `activateKeepAwake` uses.
- * @param tag *Optional* - Tag to release the lock on screen sleep prevention. If not provided,
+ * @param tag Tag to release the lock on screen sleep prevention. If not provided,
  * the default tag is used.
  */
 export async function deactivateKeepAwake(tag = ExpoKeepAwakeTag) {
