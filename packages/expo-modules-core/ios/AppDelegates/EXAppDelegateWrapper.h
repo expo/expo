expo/expo

commit d4cd4dfcaaeeec1eef15dd79d93b7e00c4f44642

[modules] Fix incompatible issue for react-native 0.71 (#20470)
# Why

sdk 48 may come next year and react-native 0.71 may be released this year. it's good to have react-native 0.71 support for our modules. if users want to upgrade 0.71, they could change their project to be a bare project for the early preview.

# How

- [core][av][gl] because 0.71 doesn't serve android aar along with npm package, the aar extraction from `node_modules/react-native/android/**/*.aar` will break on 0.71. 0.71 also ships modules with prefab support. it's good to rewrite the gradle/cmake files to link with prefab modules. that would reduce much complexity. however, to be backward compatible with 0.70, i moved most logic to `ExpoModulesCorePlugin.applyLegacyReactNativeLibsExtractionPlugin` and `legacy/CMakeLists.txt`. so that we could be backward compatible and easily remove the isolated code after we drop sdk 47 support.
- [dev-launcher] add 0.71 sources because new parameter to `DevSupportManagerBase`
- [core][dev-menu] fix ios build errors for jsc because 0.71 moved the header to `<React-jsc/JSCRuntime.h>`
- [core][autolinking] integrate the `RCTAppDelegate` with our `EXAppDelegateWrapper`. that would simply the install-expo-modules migration where we only need the change `RCTAppDelegate -> EXAppDelegateWrapper` in AppDelegate.h. however, integrate RCTAppDelegate comes with some issues from expo-modules-core. [this commit](https://github.com/expo/expo/pull/20470/commits/2959477c5782f5d3ef770fbe72b04c1770bc566b) changes many code and i'll try to explain here:
  - RCTAppDelegate is in `React-RCTAppDelegate` pod, which does not define module. we define module for it in our patch system in autolinking.
  - defines `RCT_NEW_ARCH_ENABLED` as RCTAppDelegate

packages/expo-modules-core/ios/AppDelegates/EXAppDelegateWrapper.h
@@ -3,6 +3,13 @@
 #import <UIKit/UIKit.h>
 #import <ExpoModulesCore/EXReactDelegateWrapper.h>
 
+#if __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>)
+#import <React-RCTAppDelegate/RCTAppDelegate.h>
+#elif __has_include(<React_RCTAppDelegate/RCTAppDelegate.h>)
+// for importing the header from framework, the dash will be transformed to underscore
+#import <React_RCTAppDelegate/RCTAppDelegate.h>
+#endif
+
 NS_ASSUME_NONNULL_BEGIN
 
 /**
@@ -10,7 +17,11 @@ NS_ASSUME_NONNULL_BEGIN
  written in Objective-C and that forwards all messages to the new `ExpoAppDelegate`.
  If your `AppDelegate` is in Swift, it should inherit from `ExpoAppDelegate` class instead.
  */
+#if __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>) || __has_include(<React_RCTAppDelegate/RCTAppDelegate.h>)
+@interface EXAppDelegateWrapper : RCTAppDelegate
+#else
 @interface EXAppDelegateWrapper : UIResponder <UIApplicationDelegate>
+#endif
 
 @property (nonatomic, strong, readonly) EXReactDelegateWrapper *reactDelegate;
 

packages/expo-modules-core/ios/AppDelegates/EXAppDelegateWrapper.mm
@@ -1,0 +1,80 @@
+// Copyright 2018-present 650 Industries. All rights reserved.
+
+#import <ExpoModulesCore/EXAppDelegateWrapper.h>
+#import <ExpoModulesCore/EXReactDelegateWrapper+Private.h>
+#import <ExpoModulesCore/Swift.h>
+
+
+@interface EXAppDelegateWrapper()
+
+@property (nonatomic, strong) EXReactDelegateWrapper *reactDelegate;
+
+@end
+
+@implementation EXAppDelegateWrapper {
+  EXExpoAppDelegate *_expoAppDelegate;
+}
+
+// Synthesize window, so the AppDelegate can synthesize it too.
+@synthesize window = _window;
+
+- (instancetype)init
+{
+  if (self = [super init]) {
+    _expoAppDelegate = [[EXExpoAppDelegate alloc] init];
+    _reactDelegate = [[EXReactDelegateWrapper alloc] initWithExpoReactDelegate:_expoAppDelegate.reactDelegate];
+  }
+  return self;
+}
+
+// This needs to be implemented, otherwise forwarding won't be called.
+// When the app starts, `UIApplication` uses it to check beforehand
+// which `UIApplicationDelegate` selectors are implemented.
+- (BOOL)respondsToSelector:(SEL)selector
+{
+  return [super respondsToSelector:selector]
+    || [_expoAppDelegate respondsToSelector:selector];
+}
+
+// Forwards all invocations to `ExpoAppDelegate` object.
+- (id)forwardingTargetForSelector:(SEL)selector
+{
+  return _expoAppDelegate;
+}
+
+#if __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>)
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [_expoAppDelegate application:application didFinishLaunchingWithOptions:launchOptions];
+  return YES;
+}
+
+- (RCTBridge *)createBridgeWithDelegate:(id<RCTBridgeDelegate>)delegate launchOptions:(NSDictionary *)launchOptions
+{
+  return [self.reactDelegate createBridgeWithDelegate:delegate launchOptions:launchOptions];
+}
+
+- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
+                          moduleName:(NSString *)moduleName
+                           initProps:(NSDictionary *)initProps
+{
+  BOOL enableFabric = NO;
+#if RN_FABRIC_ENABLED
+  enableFabric = self.fabricEnabled;
+#endif
+
+  return [self.reactDelegate createRootViewWithBridge:bridge
+                                         moduleName:moduleName
+                                    initialProperties:initProps
+                                        fabricEnabled:enableFabric];
+}
+
+- (UIViewController *)createRootViewController
+{
+  return [self.reactDelegate createRootViewController];
+}
+#endif // __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>)
+
+@end

packages/expo-av/android/CMakeLists.txt
@@ -1,10 +1,13 @@
 cmake_minimum_required(VERSION 3.4.1)
 
+project(expo-av)
+
 set (CMAKE_VERBOSE_MAKEFILE ON)
 set (CMAKE_CXX_STANDARD 14)
 
 set (PACKAGE_NAME "expo-av")
 set (BUILD_DIR ${CMAKE_SOURCE_DIR}/build)
+set (ignoreMe "${RN_SO_DIR}")
 
 # shared
 
@@ -16,65 +19,31 @@ add_library(
         src/main/cpp/JAVManager.cpp
 )
 
-# Extracted AAR: ${BUILD_DIR}/react-native-0*/jni/${ANDROID_ABI}
-file(GLOB LIBRN_DIR "${RN_SO_DIR}/${ANDROID_ABI}")
-if (NOT LIBRN_DIR)
-    # If /${ANDROID_ABI} dir not found, then ${RN_SO_DIR} is probably:
-    # ReactAndroid/build/react-ndk/exported
-    file(GLOB LIBRN_DIR "${RN_SO_DIR}")
-endif()
-
-file(GLOB libfbjni_include_DIRS "${BUILD_DIR}/fbjni-*-headers.jar/")
-
 # includes
 
 target_include_directories(
         ${PACKAGE_NAME}
         PRIVATE
-        "${REACT_NATIVE_DIR}/React"
-        "${REACT_NATIVE_DIR}/React/Base"
-        "${REACT_NATIVE_DIR}/ReactAndroid/src/main/jni"
+        # header only imports from turbomodule, e.g. CallInvokerHolder.h
         "${REACT_NATIVE_DIR}/ReactAndroid/src/main/jni/react/turbomodule"
-        "${REACT_NATIVE_DIR}/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/jni"
-        "${REACT_NATIVE_DIR}/ReactCommon"
         "${REACT_NATIVE_DIR}/ReactCommon/callinvoker"
-        "${REACT_NATIVE_DIR}/ReactCommon/jsi"
-        ${libfbjni_include_DIRS}
 )
 
 # find libraries
 
 find_library(LOG_LIB log)
 
-find_library(
-        FBJNI_LIB
-        fbjni
-        PATHS ${LIBRN_DIR}
-        NO_CMAKE_FIND_ROOT_PATH
-)
+find_package(ReactAndroid REQUIRED CONFIG)
 
-find_library(
-        JSI_LIB
-        jsi
-        PATHS ${LIBRN_DIR}
-        NO_CMAKE_FIND_ROOT_PATH
-)
-
-find_library(
-        REACT_NATIVE_JNI_LIB
-        reactnativejni
-        PATHS ${LIBRN_DIR}
-        NO_CMAKE_FIND_ROOT_PATH
-)
-
+find_package(fbjni REQUIRED CONFIG)
 
 # linking
 
 target_link_libraries(
         ${PACKAGE_NAME}
         ${LOG_LIB}
-        ${FBJNI_LIB}
-        ${JSI_LIB}
-        ${REACT_NATIVE_JNI_LIB}
+        fbjni::fbjni
+        ReactAndroid::jsi
+        ReactAndroid::reactnativejni
         android
 )

packages/expo-av/android/build.gradle
@@ -8,20 +8,18 @@ apply plugin: "de.undercouch.download"
 group = 'host.exp.exponent'
 version = '13.0.1'
 
-def customDownloadsDir = System.getenv("REACT_NATIVE_DOWNLOADS_DIR")
-def downloadsDir = customDownloadsDir ? new File(customDownloadsDir) : new File("$buildDir/downloads")
-
 def REACT_NATIVE_BUILD_FROM_SOURCE = findProject(":ReactAndroid") != null
 def REACT_NATIVE_DIR = REACT_NATIVE_BUILD_FROM_SOURCE
   ? findProject(":ReactAndroid").getProjectDir().parent
   : new File(["node", "--print", "require.resolve('react-native/package.json')"].execute(null, rootDir).text.trim()).parent
 def RN_SO_DIR = REACT_NATIVE_BUILD_FROM_SOURCE
   ? Paths.get(findProject(":ReactAndroid").getProjectDir().toString(), "build", "intermediates", "library_*", "*", "jni")
-  : "${buildDir}/react-native.aar/jni"
+  : "${buildDir}/react/jni"
 
 def reactProperties = new Properties()
 file("$REACT_NATIVE_DIR/ReactAndroid/gradle.properties").withInputStream { reactProperties.load(it) }
-def reactNativeIsNightly = reactProperties.getProperty("VERSION_NAME").startsWith("0.0.0-")
+def REACT_NATIVE_VERSION = System.getenv("REACT_NATIVE_OVERRIDE_VERSION") ?: reactProperties.getProperty("VERSION_NAME")
+def REACT_NATIVE_TARGET_VERSION = REACT_NATIVE_VERSION.split("\\.")[1].toInteger()
 
 def reactNativeArchitectures() {
     def value = project.getProperties().get("reactNativeArchitectures")
@@ -120,7 +118,11 @@ android {
 
   externalNativeBuild {
     cmake {
-      path "CMakeLists.txt"
+      if (REACT_NATIVE_TARGET_VERSION >= 71) {
+        path "CMakeLists.txt"
+      } else {
+        path "legacy/CMakeLists.txt"
+      }
     }
   }
 
@@ -142,10 +144,6 @@ android {
         "**/libjsi.so",
     ]
   }
-  configurations {
-    extractHeaders
-    extractJNI
-  }
 
   lintOptions {
     abortOnError false
@@ -166,8 +164,6 @@ dependencies {
   implementation 'com.facebook.react:react-native:+'
 
   compileOnly 'com.facebook.fbjni:fbjni:0.2.2'
-  extractHeaders 'com.facebook.fbjni:fbjni:0.2.2:headers'
-  extractJNI 'com.facebook.fbjni:fbjni:0.2.2'
 
   api 'com.google.android.exoplayer:exoplayer:2.18.1'
   api 'com.google.android.exoplayer:extension-okhttp:2.18.1'
@@ -181,99 +177,6 @@ dependencies {
   testImplementation 'io.mockk:mockk:1.12.3'
 }
 
-def downloadReactNativeNightlyAAR = { buildType, version, downloadFile ->
-  def classifier = buildType == 'Debug' ? 'debug' : 'release'
-  download.run {
-    src("https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=com.facebook.react&a=react-native&c=${classifier}&e=aar&v=${version}-SNAPSHOT")
-    onlyIfNewer(true)
-    overwrite(false)
-    dest(downloadFile)
-  }
-}
-
-def extractReactNativeAAR = { buildType ->
-  def suffix = buildType == 'Debug' ? '-debug' : '-release'
-  def rnAAR
-  if (reactNativeIsNightly) {
-    def downloadFile = file("${downloadsDir}/react-native-nightly.aar")
-    downloadReactNativeNightlyAAR(buildType, reactProperties.getProperty("VERSION_NAME"), downloadFile)
-    rnAAR = downloadFile
-  } else {
-    def rnAARs = fileTree(REACT_NATIVE_DIR).matching { include "**/react-native/**/*${suffix}.aar" }
-    if (rnAARs.isEmpty()) {
-      rnAARs = fileTree(REACT_NATIVE_DIR).matching { include "**/react-native/**/*.aar" }
-    }
-    if (rnAARs.any()) {
-      // node_modules/react-native has a .aar, extract headers
-      if (rnAARs.size() > 1) {
-        logger.error("More than one React Native AAR file has been found:")
-        rnAARs.each {println(it) }
-        throw new GradleException("Multiple React Native AARs found:\n${rnAARs.join("\n")}" +
-            "\nRemove the old ones and try again")
-      }
-    }
-    rnAAR = rnAARs.singleFile
-  }
-  def file = rnAAR.absoluteFile
-  def packageName = file.name.tokenize('-')[0]
-  copy {
-    from zipTree(file)
-    into "$buildDir/react-native.aar"
-    include "jni/**/*"
-  }
-}
-
-task extractReactNativeAARRelease {
-  doLast {
-    extractReactNativeAAR('Release')
-  }
-}
-
-task extractReactNativeAARDebug {
-  doLast {
-    extractReactNativeAAR('Debug')
-  }
-}
-
-task extractAARHeaders {
-  doLast {
-    configurations.extractHeaders.files.each {
-      def file = it.absoluteFile
-      copy {
-        from zipTree(file)
-        into "$buildDir/$file.name"
-        include "**/*.h"
-      }
-    }
-  }
-}
-
-task extractJNIFiles {
-  doLast {
-    configurations.extractJNI.files.each {
-      def file = it.absoluteFile
-      copy {
-        from zipTree(file)
-        into "$buildDir/$file.name"
-        include "jni/**/*"
-      }
-    }
-  }
-}
-
-tasks.whenTaskAdded { task ->
-  if (!task.name.contains("Clean") && (task.name.contains('externalNativeBuild') || task.name.startsWith('configureCMake'))) {
-    def buildType = task.name.endsWith('Debug') ? 'Debug' : 'Release'
-    task.dependsOn(extractAARHeaders)
-    task.dependsOn(extractJNIFiles)
-    if (REACT_NATIVE_BUILD_FROM_SOURCE) {
-      task.dependsOn(":ReactAndroid:copy${buildType}JniLibsProjectOnly")
-    } else {
-      task.dependsOn("extractReactNativeAAR${buildType}")
-    }
-  } else if (task.name.startsWith('generateJsonModel') && REACT_NATIVE_BUILD_FROM_SOURCE) {
-    def buildType = task.name.endsWith('Debug') ? 'Debug' : 'Release'
-    task.dependsOn(":ReactAndroid:copy${buildType}JniLibsProjectOnly")
-  }
-
+if (REACT_NATIVE_TARGET_VERSION < 71) {
+  applyLegacyReactNativeLibsExtractionPlugin()
 }

packages/expo-av/android/legacy/CMakeLists.txt
@@ -1,0 +1,76 @@
+# Remove this legacy folder when we drop SDK 47
+cmake_minimum_required(VERSION 3.4.1)
+
+project(expo-av)
+
+set (CMAKE_VERBOSE_MAKEFILE ON)
+set (CMAKE_CXX_STANDARD 14)
+
+set (PACKAGE_NAME "expo-av")
+set (BUILD_DIR ${CMAKE_SOURCE_DIR}/../build)
+set (SRC_DIR ${CMAKE_SOURCE_DIR}/../src)
+
+# shared
+
+add_library(
+        ${PACKAGE_NAME}
+        SHARED
+        ${SRC_DIR}/main/cpp/EXAV.cpp
+        ${SRC_DIR}/main/cpp/JPlayerData.cpp
+        ${SRC_DIR}/main/cpp/JAVManager.cpp
+)
+
+# Extracted AAR: ${BUILD_DIR}/react-native-0*/jni/${ANDROID_ABI}
+file(GLOB LIBRN_DIR "${RN_SO_DIR}/${ANDROID_ABI}")
+if (NOT LIBRN_DIR)
+    # If /${ANDROID_ABI} dir not found, then ${RN_SO_DIR} is probably:
+    # ReactAndroid/build/react-ndk/exported
+    file(GLOB LIBRN_DIR "${RN_SO_DIR}")
+endif()
+
+# includes
+
+target_include_directories(
+        ${PACKAGE_NAME}
+        PRIVATE
+        "${REACT_NATIVE_DIR}/React"
+        "${REACT_NATIVE_DIR}/React/Base"
+        "${REACT_NATIVE_DIR}/ReactAndroid/src/main/jni"
+        "${REACT_NATIVE_DIR}/ReactAndroid/src/main/jni/react/turbomodule"
+        "${REACT_NATIVE_DIR}/ReactAndroid/src/main/java/com/facebook/react/turbomodule/core/jni"
+        "${REACT_NATIVE_DIR}/ReactCommon"
+        "${REACT_NATIVE_DIR}/ReactCommon/callinvoker"
+        "${REACT_NATIVE_DIR}/ReactCommon/jsi"
+)
+
+# find libraries
+
+find_library(LOG_LIB log)
+
+find_package(fbjni REQUIRED CONFIG)
+
+find_library(
+        JSI_LIB
+        jsi
+        PATHS ${LIBRN_DIR}
+        NO_CMAKE_FIND_ROOT_PATH
+)
+
+find_library(
+        REACT_NATIVE_JNI_LIB
+        reactnativejni
+        PATHS ${LIBRN_DIR}
+        NO_CMAKE_FIND_ROOT_PATH
+)
+
+
+# linking
+
+target_link_libraries(
+        ${PACKAGE_NAME}
+        ${LOG_LIB}
+        fbjni::fbjni
+        ${JSI_LIB}
+        ${REACT_NATIVE_JNI_LIB}
+        android
+)

packages/expo-dev-launcher/android/build.gradle
@@ -78,7 +78,9 @@ android {
         }
 
         def rnVersion = getRNVersion()
-        if (rnVersion >= versionToNumber(0, 69, 0)) {
+        if (rnVersion >= versionToNumber(0, 71, 0)) {
+          srcDirs += "src/react-native-71"
+        } else if (rnVersion >= versionToNumber(0, 69, 0)) {
           srcDirs += "src/react-native-69"
         } else if (rnVersion >= versionToNumber(0, 67, 0)) {
           srcDirs += "src/react-native-67"

packages/expo-dev-launcher/android/src/react-native-71/expo/modules/devlauncher/rncompatibility/DevLauncherDevSupportManager.kt
@@ -1,0 +1,269 @@
+package expo.modules.devlauncher.rncompatibility
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.facebook.common.logging.FLog
+import com.facebook.debug.holder.PrinterHolder
+import com.facebook.debug.tags.ReactDebugOverlayTags
+import com.facebook.infer.annotation.Assertions
+import com.facebook.react.R
+import com.facebook.react.bridge.JSBundleLoader
+import com.facebook.react.bridge.JavaJSExecutor
+import com.facebook.react.bridge.ReactMarker
+import com.facebook.react.bridge.ReactMarkerConstants
+import com.facebook.react.bridge.UiThreadUtil
+import com.facebook.react.common.ReactConstants
+import com.facebook.react.common.futures.SimpleSettableFuture
+import com.facebook.react.devsupport.DevSupportManagerBase
+import com.facebook.react.devsupport.HMRClient
+import com.facebook.react.devsupport.ReactInstanceDevHelper
+import com.facebook.react.devsupport.WebsocketJavaScriptExecutor
+import com.facebook.react.devsupport.WebsocketJavaScriptExecutor.JSExecutorConnectCallback
+import com.facebook.react.devsupport.interfaces.DevBundleDownloadListener
+import com.facebook.react.devsupport.interfaces.DevSplitBundleCallback
+import com.facebook.react.devsupport.interfaces.RedBoxHandler
+import com.facebook.react.packagerconnection.RequestHandler
+import expo.modules.devlauncher.DevLauncherController
+import expo.modules.devlauncher.koin.DevLauncherKoinComponent
+import expo.modules.devlauncher.launcher.DevLauncherControllerInterface
+import expo.modules.devlauncher.launcher.errors.DevLauncherAppError
+import expo.modules.devlauncher.launcher.errors.DevLauncherErrorActivity
+import org.koin.core.component.inject
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+class DevLauncherDevSupportManager(
+  applicationContext: Context?,
+  val reactInstanceManagerHelper: ReactInstanceDevHelper?,
+  packagerPathForJSBundleName: String?,
+  enableOnCreate: Boolean,
+  redBoxHandler: RedBoxHandler?,
+  devBundleDownloadListener: DevBundleDownloadListener?,
+  minNumShakes: Int,
+  customPackagerCommandHandlers: MutableMap<String, RequestHandler>?,
+) : DevSupportManagerBase(
+  applicationContext,
+  reactInstanceManagerHelper,
+  packagerPathForJSBundleName,
+  enableOnCreate,
+  redBoxHandler,
+  devBundleDownloadListener,
+  minNumShakes,
+  customPackagerCommandHandlers,
+  null,
+  null
+),
+  DevLauncherKoinComponent {
+  private val controller: DevLauncherControllerInterface by inject()
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L65
+  private var mIsSamplingProfilerEnabled = false
+
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L88-L128
+  init {
+    if (devSettings.isStartSamplingProfilerOnInit) {
+      // Only start the profiler. If its already running, there is an error
+      if (!mIsSamplingProfilerEnabled) {
+        toggleJSSamplingProfiler()
+      } else {
+        Toast.makeText(
+          applicationContext,
+          "JS Sampling Profiler was already running, so did not start the sampling profiler",
+          Toast.LENGTH_LONG
+        )
+          .show()
+      }
+    }
+
+    addCustomDevOption(
+      if (mIsSamplingProfilerEnabled) applicationContext!!.getString(
+        R.string.catalyst_sample_profiler_disable
+      ) else applicationContext!!.getString(
+        R.string.catalyst_sample_profiler_enable
+      )
+    ) { toggleJSSamplingProfiler() }
+    if (!devSettings.isDeviceDebugEnabled) {
+      // For remote debugging, we open up Chrome running the app in a web worker.
+      // Note that this requires async communication, which will not work for Turbo Modules.
+      addCustomDevOption(
+        if (devSettings.isRemoteJSDebugEnabled) applicationContext.getString(R.string.catalyst_debug_stop)
+        else applicationContext.getString(R.string.catalyst_debug)
+      ) {
+        devSettings.isRemoteJSDebugEnabled = !devSettings.isRemoteJSDebugEnabled
+        handleReloadJS()
+      }
+    }
+  }
+
+  override fun showNewJavaError(message: String?, e: Throwable) {
+    if (!DevLauncherController.wasInitialized()) {
+      Log.e("DevLauncher", "DevLauncher wasn't initialized. Couldn't intercept native error handling.")
+      super.showNewJavaError(message, e)
+      return
+    }
+
+    val activity = reactInstanceManagerHelper?.currentActivity
+    if (activity == null || activity.isFinishing || activity.isDestroyed) {
+      return
+    }
+
+    controller.onAppLoadedWithError()
+    DevLauncherErrorActivity.showError(activity, DevLauncherAppError(message, e))
+  }
+
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L131-L134
+  override fun getUniqueTag() = "DevLauncherApp - Bridge"
+
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L136-L156
+  override fun loadSplitBundleFromServer(
+    bundlePath: String?,
+    callback: DevSplitBundleCallback
+  ) {
+    fetchSplitBundleAndCreateBundleLoader(
+      bundlePath,
+      object : CallbackWithBundleLoader {
+        override fun onSuccess(bundleLoader: JSBundleLoader) {
+          bundleLoader.loadScript(currentContext!!.catalystInstance)
+          currentContext!!
+            .getJSModule(HMRClient::class.java)
+            .registerBundle(devServerHelper.getDevServerSplitBundleURL(bundlePath))
+          callback.onSuccess()
+        }
+
+        override fun onError(url: String, cause: Throwable) {
+          callback.onError(url, cause)
+        }
+      }
+    )
+  }
+
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L158-L165
+  private fun getExecutorConnectCallback(
+    future: SimpleSettableFuture<Boolean>
+  ): JSExecutorConnectCallback {
+    return object : JSExecutorConnectCallback {
+      override fun onSuccess() {
+        future.set(true)
+        hideDevLoadingView()
+      }
+
+      override fun onFailure(cause: Throwable) {
+        hideDevLoadingView()
+        FLog.e(ReactConstants.TAG, "Failed to connect to debugger!", cause)
+        future.setException(
+          IOException(
+            applicationContext.getString(R.string.catalyst_debug_error),
+            cause
+          )
+        )
+      }
+    }
+  }
+
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L179-L204
+  private fun reloadJSInProxyMode() {
+    // When using js proxy, there is no need to fetch JS bundle as proxy executor will do that
+    // anyway
+    devServerHelper.launchJSDevtools()
+    val factory = JavaJSExecutor.Factory {
+      val executor = WebsocketJavaScriptExecutor()
+      val future = SimpleSettableFuture<Boolean>()
+      executor.connect(
+        devServerHelper.websocketProxyURL, getExecutorConnectCallback(future)
+      )
+      // TODO(t9349129) Don't use timeout
+      try {
+        future[90, TimeUnit.SECONDS]
+        return@Factory executor
+      } catch (e: ExecutionException) {
+        throw (e.cause as Exception)
+      } catch (e: InterruptedException) {
+        throw RuntimeException(e)
+      } catch (e: TimeoutException) {
+        throw RuntimeException(e)
+      }
+    }
+    reactInstanceDevHelper.onReloadWithJSDebugger(factory)
+  }
+
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L206-L231
+  override fun handleReloadJS() {
+    UiThreadUtil.assertOnUiThread()
+    ReactMarker.logMarker(
+      ReactMarkerConstants.RELOAD,
+      devSettings.packagerConnectionSettings.debugServerHost
+    )
+
+    // dismiss redbox if exists
+    hideRedboxDialog()
+    if (devSettings.isRemoteJSDebugEnabled) {
+      PrinterHolder.getPrinter()
+        .logMessage(ReactDebugOverlayTags.RN_CORE, "RNCore: load from Proxy")
+      showDevLoadingViewForRemoteJSEnabled()
+      reloadJSInProxyMode()
+    } else {
+      PrinterHolder.getPrinter()
+        .logMessage(ReactDebugOverlayTags.RN_CORE, "RNCore: load from Server")
+      val bundleURL = devServerHelper
+        .getDevServerBundleURL(Assertions.assertNotNull(jsAppBundleName))
+      reloadJSFromServer(bundleURL)
+    }
+  }
+
+  // copied from https://github.com/facebook/react-native/blob/aa4da248c12e3ba41ecc9f1c547b21c208d9a15f/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgeDevSupportManager.java#L233-L277
+  /** Starts of stops the sampling profiler  */
+  private fun toggleJSSamplingProfiler() {
+    val javaScriptExecutorFactory = reactInstanceDevHelper.javaScriptExecutorFactory
+    if (!mIsSamplingProfilerEnabled) {
+      try {
+        javaScriptExecutorFactory.startSamplingProfiler()
+        Toast.makeText(applicationContext, "Starting Sampling Profiler", Toast.LENGTH_SHORT)
+          .show()
+      } catch (e: UnsupportedOperationException) {
+        Toast.makeText(
+          applicationContext,
+          "$javaScriptExecutorFactory does not support Sampling Profiler",
+          Toast.LENGTH_LONG
+        )
+          .show()
+      } finally {
+        mIsSamplingProfilerEnabled = true
+      }
+    } else {
+      try {
+        val outputPath: String = File.createTempFile(
+          "sampling-profiler-trace", ".cpuprofile", applicationContext.cacheDir
+        )
+          .path
+        javaScriptExecutorFactory.stopSamplingProfiler(outputPath)
+        Toast.makeText(
+          applicationContext,
+          "Saved results from Profiler to $outputPath",
+          Toast.LENGTH_LONG
+        )
+          .show()
+      } catch (e: IOException) {
+        FLog.e(
+          ReactConstants.TAG,
+          "Could not create temporary file for saving results from Sampling Profiler"
+        )
+      } catch (e: UnsupportedOperationException) {
+        Toast.makeText(
+          applicationContext,
+          javaScriptExecutorFactory.toString() + "does not support Sampling Profiler",
+          Toast.LENGTH_LONG
+        )
+          .show()
+      } finally {
+        mIsSamplingProfilerEnabled = false
+      }
+    }
+  }
+
+  companion object {
+    fun getDevHelperInternalFieldName() = "mReactInstanceDevHelper"
+  }
+}

commit 4ef7fe1bbf4f71ff6344971dfb5ce2725ba7c3db

[core] Fix react-native 0.71 integration errors on ios (#20539)
# Why

fix further regressions from #20470

# How

- bare-expo is still running with legacy AppDelegate where it creates
bridge, rootView in AppDelegate.mm. even running with 0.71, the
EXAppDelegateWrapper should handle this case and not create the bridge
from RCTAppDelegate.
- workaround a build error when running with new arch mode

# Test Plan

ci passed

packages/expo-modules-core/ios/AppDelegates/EXAppDelegateWrapper.h
@@ -1,5 +1,7 @@
 // Copyright 2018-present 650 Industries. All rights reserved.
 
+#if __cplusplus
+
 #import <UIKit/UIKit.h>
 #import <ExpoModulesCore/EXReactDelegateWrapper.h>
 
@@ -28,3 +30,12 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 NS_ASSUME_NONNULL_END
+
+#else
+
+// Workaround the main.m build error when running with new architecture mode
+// Context: https://github.com/facebook/react-native/pull/35661
+@interface EXAppDelegateWrapper : UIResponder
+@end
+
+#endif

packages/expo-modules-core/ios/AppDelegates/EXAppDelegateWrapper.mm
@@ -44,10 +44,30 @@
 
 #if __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>)
 
+- (UIView *)findRootView:(UIApplication *)application
+{
+  UIWindow *mainWindow = application.delegate.window;
+  if (mainWindow == nil) {
+    return nil;
+  }
+  UIViewController *rootViewController = mainWindow.rootViewController;
+  if (rootViewController == nil) {
+    return nil;
+  }
+  UIView *rootView = rootViewController.view;
+  return rootView;
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [super application:application didFinishLaunchingWithOptions:launchOptions];
-  [_expoAppDelegate application:application didFinishLaunchingWithOptions:launchOptions];
+  UIView *rootView = [self findRootView:application];
+  // Backward compatible with react-native 0.71 running with legacy template,
+  // i.e. still creating bridge, rootView in AppDelegate.mm.
+  // In this case, we don't go through RCTAppDelegate's setup.
+  if (rootView == nil || ![rootView isKindOfClass:[RCTRootView class]]) {
+    [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [_expoAppDelegate application:application didFinishLaunchingWithOptions:launchOptions];
+  }
   return YES;
 }
 