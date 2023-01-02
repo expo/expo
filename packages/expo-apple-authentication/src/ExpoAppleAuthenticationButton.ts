aa36c5db1080ef025a96c81f4df1e62b8e1bfe1f
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-01-02 10:20:02 +0100

    Migrate expo-apple-authentication to the new modules API (#20600)

--- packages/expo-apple-authentication/src/ExpoAppleAuthenticationButton.ts
+++ packages/expo-apple-authentication/src/ExpoAppleAuthenticationButton.ts
@@ -1,54 +1,10 @@
 import { requireNativeViewManager } from 'expo-modules-core';
 import { Platform } from 'react-native';
 
-let ExpoAppleAuthenticationButtonSignInWhite: any;
-let ExpoAppleAuthenticationButtonSignInWhiteOutline: any;
-let ExpoAppleAuthenticationButtonSignInBlack: any;
-let ExpoAppleAuthenticationButtonContinueWhite: any;
-let ExpoAppleAuthenticationButtonContinueWhiteOutline: any;
-let ExpoAppleAuthenticationButtonContinueBlack: any;
-let ExpoAppleAuthenticationButtonSignUpWhite: any;
-let ExpoAppleAuthenticationButtonSignUpWhiteOutline: any;
-let ExpoAppleAuthenticationButtonSignUpBlack: any;
+let ExpoAppleAuthenticationButton: any;
 
 if (Platform.OS === 'ios') {
-  ExpoAppleAuthenticationButtonSignInWhite = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonSignInWhite'
-  );
-  ExpoAppleAuthenticationButtonSignInWhiteOutline = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonSignInWhiteOutline'
-  );
-  ExpoAppleAuthenticationButtonSignInBlack = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonSignInBlack'
-  );
-  ExpoAppleAuthenticationButtonContinueWhite = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonContinueWhite'
-  );
-  ExpoAppleAuthenticationButtonContinueWhiteOutline = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonContinueWhiteOutline'
-  );
-  ExpoAppleAuthenticationButtonContinueBlack = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonContinueBlack'
-  );
-  ExpoAppleAuthenticationButtonSignUpWhite = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonSignUpWhite'
-  );
-  ExpoAppleAuthenticationButtonSignUpWhiteOutline = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonSignUpWhiteOutline'
-  );
-  ExpoAppleAuthenticationButtonSignUpBlack = requireNativeViewManager(
-    'ExpoAppleAuthenticationButtonSignUpBlack'
-  );
+  ExpoAppleAuthenticationButton = requireNativeViewManager('ExpoAppleAuthentication');
 }
 
-export {
-  ExpoAppleAuthenticationButtonSignInWhite,
-  ExpoAppleAuthenticationButtonSignInWhiteOutline,
-  ExpoAppleAuthenticationButtonSignInBlack,
-  ExpoAppleAuthenticationButtonContinueWhite,
-  ExpoAppleAuthenticationButtonContinueWhiteOutline,
-  ExpoAppleAuthenticationButtonContinueBlack,
-  ExpoAppleAuthenticationButtonSignUpWhite,
-  ExpoAppleAuthenticationButtonSignUpWhiteOutline,
-  ExpoAppleAuthenticationButtonSignUpBlack,
-};
+export default ExpoAppleAuthenticationButton;
