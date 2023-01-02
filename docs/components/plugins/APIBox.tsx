a464561af1597e30339f5d9a406fd427ff77c850
Bartosz Kaszubowski <gosimek@gmail.com>
2023-01-02 15:38:32 +0100

    [docs] small batch of tweaks and fixes (#20673)
    Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

--- docs/components/plugins/APIBox.tsx
+++ docs/components/plugins/APIBox.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from 'react';
 
-import { STYLES_APIBOX } from '~/components/plugins/api/APISectionUtils';
+import { STYLES_APIBOX, STYLES_APIBOX_WRAPPER } from '~/components/plugins/api/APISectionUtils';
 import { PlatformName } from '~/types/common';
 import { PlatformTags } from '~/ui/components/Tag';
 import { H3 } from '~/ui/components/Text';
@@ -13,7 +13,7 @@ type APIBoxProps = PropsWithChildren<{
 
 export const APIBox = ({ header, platforms, children, className }: APIBoxProps) => {
   return (
-    <div css={STYLES_APIBOX} className={className}>
+    <div css={[STYLES_APIBOX, STYLES_APIBOX_WRAPPER]} className={className}>
       {platforms && <PlatformTags prefix="Only for:" platforms={platforms} />}
       {header && <H3 tags={platforms}>{header}</H3>}
       {children}
