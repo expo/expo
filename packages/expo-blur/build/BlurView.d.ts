020744d78b46861653ce98ec6adf9457f5068bd2
Bartosz Kaszubowski <gosimek@gmail.com>
2023-01-04 13:09:40 +0100

    [blur] add basic JSDoc comment to the exported component (#20698)

--- packages/expo-blur/build/BlurView.d.ts
+++ packages/expo-blur/build/BlurView.d.ts
@@ -1,5 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
+/**
+ * A React component that blurs everything underneath the view.
+ */
 declare const BlurViewWithForwardedRef: React.ForwardRefExoticComponent<{
     tint?: import("./BlurView.types").BlurTint | undefined;
     intensity?: number | undefined;
