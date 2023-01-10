8c1b0313a184923001969788c7abfd915efd0e21
Cedric van Putten <me@bycedric.com>
2023-01-10 22:44:28 +0100

    feature(cli): add used package manager to project details

--- packages/@expo/cli/src/utils/analytics/getProjectProperties.ts
+++ packages/@expo/cli/src/utils/analytics/getProjectProperties.ts
@@ -1,7 +1,9 @@
 import { ExpoConfig } from '@expo/config';
+import { resolvePackageManager } from '@expo/package-manager';
 
 export function getProjectProperties(projectRoot: string, exp: ExpoConfig) {
   return {
     sdkVersion: exp.sdkVersion ?? null,
+    packageManager: resolvePackageManager(projectRoot),
   };
 }
