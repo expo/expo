f01627fb3d710e3aebf22d3acf0fcb0f195e841f
evanbacon <bacon@expo.io>
2023-01-11 15:40:37 +0100

    update keep awake

--- packages/expo-keep-awake/build/KeepAwake.types.d.ts
+++ packages/expo-keep-awake/build/KeepAwake.types.d.ts
@@ -6,4 +6,13 @@ export declare enum KeepAwakeEventState {
     RELEASE = "release"
 }
 export type KeepAwakeListener = (event: KeepAwakeEvent) => void;
+export type KeepAwakeOptions = {
+    /**
+     * The call will throw an unhandled promise rejection on Android when the original Activity is dead or deactivated.
+     * Set the value to `true` for suppressing the uncaught exception.
+     */
+    suppressDeactivateWarnings?: boolean;
+    /** A callback that is invoked when the keep-awake state changes (web-only). */
+    listener?: KeepAwakeListener;
+};
 //# sourceMappingURL=KeepAwake.types.d.ts.map
--- packages/expo-keep-awake/src/KeepAwake.types.ts
+++ packages/expo-keep-awake/src/KeepAwake.types.ts
@@ -11,3 +11,14 @@ export enum KeepAwakeEventState {
 
 // @needsAudit
 export type KeepAwakeListener = (event: KeepAwakeEvent) => void;
+
+export type KeepAwakeOptions = {
+  /**
+   * The call will throw an unhandled promise rejection on Android when the original Activity is dead or deactivated.
+   * Set the value to `true` for suppressing the uncaught exception.
+   */
+  suppressDeactivateWarnings?: boolean;
+
+  /** A callback that is invoked when the keep-awake state changes (web-only). */
+  listener?: KeepAwakeListener;
+};
