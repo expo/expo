9700dbd86974511e7be274cb6cf984491c58fdb9
evanbacon <bacon@expo.io>
2023-01-12 12:34:38 +0100

    Update taskfile-swc.js

--- tools/taskfile-swc.js
+++ tools/taskfile-swc.js
@@ -17,7 +17,7 @@ module.exports = function (task) {
         },
         env: {
           targets: {
-            node: '16.14.2',
+            node: '14.17.6',
           },
         },
         jsc: {
