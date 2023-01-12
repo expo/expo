6729f71f7c1d15e30535bdea543f66c002b1aa70
Aman Mittal <amandeepmittal@live.com>
2023-01-13 03:55:10 +0530

    [docs][auth-session] Remove outdated info and add missing type definitions (#20817)

--- packages/expo-auth-session/build/providers/Google.d.ts
+++ packages/expo-auth-session/build/providers/Google.d.ts
@@ -13,68 +13,25 @@ export interface GoogleAuthRequestConfig extends ProviderAuthRequestConfig {
      */
     selectAccount?: boolean;
     /**
-     * Proxy client ID for use in the Expo client on iOS and Android.
-     *
-     * This Google Client ID must be setup as follows:
-     *
-     * - **Application Type**: Web Application
-     * - **URIs**: https://auth.expo.io
-     * - **Authorized redirect URIs**: https://auth.expo.io/@your-username/your-project-slug
+     * Proxy client ID for use in the Expo client on Android and iOS.
      */
     expoClientId?: string;
     /**
      * Expo web client ID for use in the browser.
-     *
-     * This Google Client ID must be setup as follows:
-     *
-     * - **Application Type**: Web Application
-     * - Give it a name (e.g. "Web App").
-     * - **URIs** (Authorized JavaScript origins): https://localhost:19006 & https://yourwebsite.com
-     * - **Authorized redirect URIs**: https://localhost:19006 & https://yourwebsite.com
-     * - To test this be sure to start your app with `npx expo start --https`.
      */
     webClientId?: string;
     /**
      * iOS native client ID for use in standalone, bare workflow, and custom clients.
-     *
-     * This Google Client ID must be setup as follows:
-     *
-     * - **Application Type**: iOS Application
-     * - Give it a name (e.g. "iOS App").
-     * - **Bundle ID**: Must match the value of `ios.bundleIdentifier` in your `app.json`.
-     * - Your app needs to conform to the URI scheme matching your bundle identifier.
-     *   - _Standalone_: Automatically added, do nothing.
-     *   - _Bare workflow_: Run `npx uri-scheme add <your bundle id> --ios`
-     * - To test this you can:
-     *   1. Prebuild to generate the native files: `expo prebuild` and run `yarn ios`
-     *   2. Create a custom client: `expo client:ios`
-     *   3. Build a production IPA: `expo build:ios`
-     * - Whenever you change the values in `app.json` you'll need to rebuild the native app.
      */
     iosClientId?: string;
     /**
      * Android native client ID for use in standalone, and bare workflow.
-     *
-     * This Google Client ID must be setup as follows:
-     *
-     * - **Application Type**: Android Application
-     * - Give it a name (e.g. "Android App").
-     * - **Package name**: Must match the value of `android.package` in your `app.json`.
-     * - Your app needs to conform to the URI scheme matching your `android.package` (ex. `com.myname.mycoolapp:/`).
-     *   - _Standalone_: Automatically added, do nothing.
-     *   - _Bare workflow_: Run `npx uri-scheme add <your android.package> --android`
-     * - **Signing-certificate fingerprint**:
-     *   - Run `expo credentials:manager -p android` then select "Update upload Keystore" -> "Generate new keystore" -> "Go back to experience overview"
-     *   - Copy your "Google Certificate Fingerprint", it will output a string that looks like `A1:B2:C3` but longer.
-     * - To test this you can:
-     *   1. Prebuild to generate the native files: `expo prebuild` and run `yarn ios`
-     *   2. Build a production IPA: `expo build:android`
      */
     androidClientId?: string;
     /**
      * Should the hook automatically exchange the response code for an authentication token.
      *
-     * Defaults to `true` on installed apps (iOS, Android) when `ResponseType.Code` is used (default).
+     * Defaults to `true` on installed apps (Android, iOS) when `ResponseType.Code` is used (default).
      */
     shouldAutoExchangeCode?: boolean;
     /**
