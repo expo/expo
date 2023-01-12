ed39392a1901566bb7f6721a0bc72735968352fe
Cedric van Putten <me@bycedric.com>
2023-01-12 16:15:32 +0100

    [create-expo-module] Add basic telemetry

---
+++ packages/create-expo-module/src/telemetry.ts
@@ -0,0 +1,99 @@
+import { getUserStatePath } from '@expo/config/build/getUserState';
+import JsonFile from '@expo/json-file';
+import TelemetryClient from '@expo/rudder-sdk-node';
+import crypto from 'crypto';
+import { boolish } from 'getenv';
+import os from 'os';
+
+import { CommandOptions } from './types';
+
+const packageJson = require('../package.json');
+
+/** If telemetry is disabled by the user */
+const EXPO_NO_TELEMETRY = boolish('EXPO_NO_TELEMETRY', false);
+
+/** The telemetry client instance to use */
+let client: TelemetryClient | null = null;
+/** The anonymous identity ID */
+let telemetryId: string | null = null;
+
+export function getTelemetryClient() {
+  if (!client) {
+    client = new TelemetryClient('TODO', 'https://cdp.expo.dev/v1/batch', {
+      flushInterval: 300,
+    });
+
+    // Empty the telemetry queue on exit
+    process.on('SIGINT', () => client?.flush?.());
+    process.on('SIGTERM', () => client?.flush?.());
+  }
+
+  return client;
+}
+
+/** Get the randomly generated anonymous ID from the persistent storage, see @expo/cli */
+async function getTelemetryIdAsync() {
+  const settings = new JsonFile<{ uuid?: string }>(getUserStatePath(), {
+    ensureDir: true,
+    jsonParseErrorDefault: {},
+    cantReadFileDefault: {},
+  });
+
+  let id = await settings.getAsync('uuid', null);
+
+  if (!id) {
+    id = crypto.randomUUID();
+    await settings.setAsync('uuid', id);
+  }
+
+  return id;
+}
+
+function getTelemetryContext() {
+  const PLATFORM_NAMES: Partial<Record<NodeJS.Platform, string>> = {
+    darwin: 'Mac',
+    win32: 'Windows',
+    linux: 'Linux',
+  };
+
+  return {
+    os: { name: PLATFORM_NAMES[os.platform()] ?? os.platform(), version: os.release() },
+    app: { name: 'create-expo-module', version: packageJson.version },
+  };
+}
+
+type Event = {
+  event: 'Create Expo Module';
+  properties: Record<string, any>;
+};
+
+export async function logEventAsync(event: Event) {
+  if (EXPO_NO_TELEMETRY) {
+    return;
+  }
+
+  if (!telemetryId) {
+    telemetryId = await getTelemetryIdAsync();
+    getTelemetryClient().identify({ anonymousId: telemetryId });
+  }
+
+  getTelemetryClient().track({
+    ...event,
+    anonymousId: telemetryId,
+    context: getTelemetryContext(),
+  });
+}
+
+export function eventCreateExpoModule(packageManager: string, options: CommandOptions) {
+  return {
+    event: 'Create Expo Module' as const,
+    properties: {
+      nodeVersion: process.version,
+      packageManager,
+      withTemplate: !!options.source,
+      withReadme: options.withReadme,
+      withChangelog: options.withChangelog,
+      withExample: options.example,
+    },
+  };
+}
