e98184a2672c4c945d6867c28949c4264458b4b5
Gabriel Chaney <gabriel.chaney@gmail.com>
2023-01-12 10:14:54 -0700

    Fix speech android onRange event parameters

--- packages/expo-speech/android/src/main/java/expo/modules/speech/SpeechModule.kt
+++ packages/expo-speech/android/src/main/java/expo/modules/speech/SpeechModule.kt
@@ -144,8 +144,8 @@ class SpeechModule(
             override fun onRangeStart(utteranceId: String, start: Int, end: Int, frame: Int) {
               var map = Bundle().apply {
                 putString("id", utteranceId)
-                putInt("characterLocation", frame)
-                putInt("characterLength", start + end)
+                putInt("characterLocation", start)
+                putInt("characterLength", end - start)
               }
               emitter.emit("Exponent.speakingWillSayNextString", map)
             }
