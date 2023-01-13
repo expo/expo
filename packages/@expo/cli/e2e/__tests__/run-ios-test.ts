248dcbf61136c77d10dcea08ce0639622a2fa35e
Kudo Chien <kudo@expo.dev>
2023-01-14 02:18:22 +0800

    [cli] fix e2e test errors

--- packages/@expo/cli/e2e/__tests__/run-ios-test.ts
+++ packages/@expo/cli/e2e/__tests__/run-ios-test.ts
@@ -20,8 +20,8 @@ afterAll(() => {
 it('loads expected modules by default', async () => {
   const modules = await getLoadedModulesAsync(`require('../../build/src/run/ios').expoRunIos`);
   expect(modules).toStrictEqual([
-    '../node_modules/ansi-styles/index.js',
     '../node_modules/arg/index.js',
+    '../node_modules/chalk/node_modules/ansi-styles/index.js',
     '../node_modules/chalk/source/index.js',
     '../node_modules/chalk/source/util.js',
     '../node_modules/has-flag/index.js',
