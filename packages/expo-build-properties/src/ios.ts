ed2ed080b05f7b11db7752957c1a93919bac0da5
gabrieldonadel <donadeldev@gmail.com>
2023-01-16 20:14:12 -0300

    [build-properties] Add support for enabling Fabric through expo-build-properties

--- packages/expo-build-properties/src/ios.ts
+++ packages/expo-build-properties/src/ios.ts
@@ -7,6 +7,10 @@ const { createBuildPodfilePropsConfigPlugin } = IOSConfig.BuildProperties;
 export const withIosBuildProperties = createBuildPodfilePropsConfigPlugin<PluginConfigType>(
   [
     {
+      propName: 'expo.newArchEnabled',
+      propValueGetter: (config) => config.ios?.newArchEnabled?.toString(),
+    },
+    {
       propName: 'ios.useFrameworks',
       propValueGetter: (config) => config.ios?.useFrameworks,
     },
