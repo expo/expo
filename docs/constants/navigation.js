665440ed48895a1caba484d4f1ec28bd74b389cb
Will Schurman <wschurman@expo.io>
2023-01-19 10:44:35 -0800

    [expo-updates] Update spec for version 1: message part (#20259)

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -198,6 +198,7 @@ const general = [
   makeSection('Regulatory compliance', sortAlphabetical(pagesFromDir('regulatory-compliance')), {}),
   makeSection('Technical specs', [
     makePage('technical-specs/expo-updates-0.mdx'),
+    makePage('technical-specs/expo-updates-1-draft.mdx'),
     makePage('technical-specs/expo-sfv-0.mdx'),
   ]),
   makeSection('Deprecated', [
