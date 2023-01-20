a8689b6e45ee40df7a631944d33df7ba04ef6a50
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2023-01-20 14:26:10 +0000

    [android][image-manipulator] Migrate to new API (#20683)
    # Why
    Continue migrating modules to new API
    
    # How
    Followed the usual migration steps
    
    # Test Plan
    All tests in the bare expo app passing as before.

--- packages/expo-image-manipulator/android/src/main/java/expo/modules/imagemanipulator/arguments/Actions.kt
+++ packages/expo-image-manipulator/android/src/main/java/expo/modules/imagemanipulator/arguments/Actions.kt
@@ -1,29 +1,29 @@
 package expo.modules.imagemanipulator.arguments
 
+import expo.modules.imagemanipulator.ManipulateAction
 import expo.modules.imagemanipulator.actions.Action
 import expo.modules.imagemanipulator.actions.CropAction
 import expo.modules.imagemanipulator.actions.FlipAction
 import expo.modules.imagemanipulator.actions.ResizeAction
 import expo.modules.imagemanipulator.actions.RotateAction
-import java.util.ArrayList
 
-private const val KEY_CROP = "crop"
-private const val KEY_FLIP = "flip"
-private const val KEY_RESIZE = "resize"
-private const val KEY_ROTATE = "rotate"
-
-class Actions(public val actions: List<Action>) {
+class Actions(val actions: List<Action>) {
   companion object {
-    fun fromArgument(rawList: ArrayList<Any?>): Actions {
+    fun fromArgument(actionList: List<ManipulateAction>): Actions {
       val actions: MutableList<Action> = mutableListOf()
 
-      for (rawObject in rawList) {
-        require(rawObject is Map<*, *>)
-        when {
-          rawObject.containsKey(KEY_CROP) -> actions.add(CropAction.fromObject(rawObject[KEY_CROP]!!))
-          rawObject.containsKey(KEY_FLIP) -> actions.add(FlipAction.fromObject(rawObject[KEY_FLIP]!! as String))
-          rawObject.containsKey(KEY_RESIZE) -> actions.add(ResizeAction.fromObject(rawObject[KEY_RESIZE]!!))
-          rawObject.containsKey(KEY_ROTATE) -> actions.add(RotateAction.fromObject((rawObject[KEY_ROTATE]!! as Double).toInt()))
+      for (action in actionList) {
+        action.flip?.let {
+          actions.add(FlipAction(it))
+        }
+        action.crop?.let {
+          actions.add(CropAction(it))
+        }
+        action.rotate?.let {
+          actions.add(RotateAction(it.toInt()))
+        }
+        action.resize?.let {
+          actions.add(ResizeAction(it))
         }
       }
 
