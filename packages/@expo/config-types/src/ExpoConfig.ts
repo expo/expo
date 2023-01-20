e42fd2dc7a9c4e142cfa0a6019fb8c8fe4669e2e
gabrieldonadel <donadeldev@gmail.com>
2023-01-20 09:49:17 -0300

    [config-plugins][config-types] align with server schema

--- packages/@expo/config-types/src/ExpoConfig.ts
+++ packages/@expo/config-types/src/ExpoConfig.ts
@@ -117,7 +117,7 @@ export interface ExpoConfig {
      */
     hidden?: boolean;
     /**
-     * Sets `android:windowTranslucentStatus` in `styles.xml`. When false, the system status bar pushes the content of your app down (similar to `position: relative`). When true, the status bar floats above the content in your app (similar to `position: absolute`). Defaults to `true` to match the iOS status bar behavior (which can only float above content).
+     * When false, the system status bar pushes the content of your app down (similar to `position: relative`). When true, the status bar floats above the content in your app (similar to `position: absolute`). Defaults to `true` to match the iOS status bar behavior (which can only float above content). Explicitly setting this property to `true` will add `android:windowTranslucentStatus` to `styles.xml` and may cause unexpected keyboard behavior on Android when using the `softwareKeyboardLayoutMode` set to `resize`. In this case you will have to use `KeyboardAvoidingView` to manage the keyboard layout.
      */
     translucent?: boolean;
   };
