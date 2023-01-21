abf872c53e40b5b8da2cd96ff60ad11ea333fa60
evanbacon <bacon@expo.io>
2023-01-21 17:41:55 -0600

    dynamic root

--- packages/@expo/cli/src/start/server/render-root.tsx
+++ packages/@expo/cli/src/start/server/render-root.tsx
@@ -11,32 +11,83 @@ export function serverRenderUrl(location: URL): string {
 
   const ref = React.createRef<ServerContainerRef>();
 
-  const html = ReactDOMServer.renderToString(
+  const out = React.createElement(Root, {
+    children: element,
+    // scripts: (
+    //   <>
+    //     {scripts.map((script, index) => (
+    //       <script key={String(index)} src={script} />
+    //     ))}
+    //   </>
+    // ),
+    scripts: <></>,
+    styles: (
+      <>
+        <style id="react-native-reset-style" dangerouslySetInnerHTML={{ __html: style }} />
+        {getStyleElement()}
+      </>
+    ),
+  });
+
+  let html = ReactDOMServer.renderToString(
     <ServerContainer ref={ref} location={location}>
-      {element}
+      {out}
     </ServerContainer>
   );
 
-  const css = ReactDOMServer.renderToStaticMarkup(getStyleElement());
-
   const options = ref.current?.getCurrentOptions();
+  html = html.replace('<head>', `<head><title>${options?.title}</title>`);
 
-  const document = `
-        <!DOCTYPE html>
-        <html style="height: 100%">
-        <meta charset="utf-8">
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge">
+  return html;
+}
+
+// Default Root component:
+
+// Follows the setup for react-native-web:
+// https://necolas.github.io/react-native-web/docs/setup/#root-element
+// Plus additional React Native scroll and text parity styles for various
+// browsers.
+// Force root DOM element to fill the parent's height
+const style = `
+html, body, #root {
+  -webkit-overflow-scrolling: touch;
+}
+#root {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+html {
+  scroll-behavior: smooth;
+  -webkit-text-size-adjust: 100%;
+}
+body {
+  /* Allows you to scroll below the viewport; default value is visible */
+  overflow-y: auto;
+  overscroll-behavior-y: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -ms-overflow-style: scrollbar;
+}
+`;
+
+export function Root({ children, scripts, styles }) {
+  return (
+    <html lang="en" style={{ height: '100%' }}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1.00001, viewport-fit=cover"
-        >
-        ${css}
-        <title>${options?.title}</title>
-        <body style="min-height: 100%">
-        <div id="root" style="display: flex; min-height: 100vh">
-        ${html}
-        </div>
-    `;
-
-  return document;
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1.00001,viewport-fit=cover"
+        />
+        {styles}
+      </head>
+      <body style={{ height: '100%', overflow: 'hidden' }}>
+        <div id="root">{children}</div>
+        {scripts}
+      </body>
+    </html>
+  );
 }
