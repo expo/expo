c42922fedbf7c504b9b141786b1bb84a4297b2c8
nishan (o^▽^o) <nishanbende@gmail.com>
2023-01-23 14:50:58 +0530

    [image][ios] Add Live Text interaction in expo-image (#20915)

--- packages/expo-image/src/Image.types.ts
+++ packages/expo-image/src/Image.types.ts
@@ -227,6 +227,13 @@ export type ImageProps = {
    * @platform ios
    */
   accessibilityLabel?: string | null;
+
+  /**
+   * Enables Live Text interaction with the image. Check official [Apple documentation](https://developer.apple.com/documentation/visionkit/enabling_live_text_interactions_with_images) for more details.
+   * @default false
+   * @platform ios 16.0+
+   */
+  enableLiveTextInteraction?: boolean;
 };
 
 /**
