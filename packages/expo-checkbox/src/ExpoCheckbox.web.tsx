ecd3b51850792c61c5e0a0f156197624840f241f
Bartosz Kaszubowski <gosimek@gmail.com>
2023-01-26 18:44:52 +0100

    [checkbox] remove deprecated `isAvailableAsync()` method (#20961)

--- packages/expo-checkbox/src/ExpoCheckbox.web.tsx
+++ packages/expo-checkbox/src/ExpoCheckbox.web.tsx
@@ -15,16 +15,6 @@ import { StyleSheet, View, unstable_createElement as createElement } from 'react
 import { CheckboxProps, CheckboxEvent } from './Checkbox.types';
 
 export default class ExpoCheckbox extends React.PureComponent<CheckboxProps> {
-  /**
-   * @deprecated Currently, `Checkbox` supports all the platforms, so `isAvailableAsync()` method is deprecated and will be removed in future releases.
-   */
-  static isAvailableAsync() {
-    console.warn(
-      'Checkbox.isAvailableAsync() is deprecated and will be removed in future releases'
-    );
-    return Promise.resolve(true);
-  }
-
   private handleChange = (event: React.SyntheticEvent<HTMLInputElement, CheckboxEvent>) => {
     const value = event.nativeEvent.target.checked;
     event.nativeEvent.value = value;
