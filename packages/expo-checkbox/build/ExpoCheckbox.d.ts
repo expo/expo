fd27228d47471e9b0f9f70694e2ba43aee880e6d
Bartosz Kaszubowski <gosimek@gmail.com>
2023-01-26 16:14:43 +0100

    [checkbox][docs] refactor code to fix types and autogenerated docs (#20956)
    Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

--- packages/expo-checkbox/build/ExpoCheckbox.d.ts
+++ packages/expo-checkbox/build/ExpoCheckbox.d.ts
@@ -1,4 +1,11 @@
-import { CheckboxComponent } from './Checkbox.types';
-declare const ExpoCheckbox: CheckboxComponent;
-export default ExpoCheckbox;
+import React from 'react';
+import { CheckboxProps } from './Checkbox.types';
+export default class ExpoCheckbox extends React.PureComponent<CheckboxProps> {
+    /**
+     * @deprecated Currently, `Checkbox` supports all the platforms, so `isAvailableAsync()` method is deprecated and will be removed in future releases.
+     */
+    static isAvailableAsync(): Promise<boolean>;
+    private handleChange;
+    render(): JSX.Element;
+}
 //# sourceMappingURL=ExpoCheckbox.d.ts.map
