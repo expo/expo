30c33940fe517e67eae2f10d852f8cd95a5de5fe
Kudo Chien <kudo@expo.dev>
2023-01-30 17:04:21 +0800

    [ncl] fix reanimated setImmediate issue

--- apps/native-component-list/metro.config.js
+++ apps/native-component-list/metro.config.js
@@ -1,4 +1,5 @@
 const { createMetroConfiguration } = require('expo-yarn-workspaces');
+const path = require('path');
 
 const baseConfig = createMetroConfiguration(__dirname);
 
@@ -6,6 +7,9 @@ if (process.env.EXPO_USE_EXOTIC) {
   // Use the custom transformer when exotic is enabled.
   baseConfig.transformer.babelTransformerPath = require.resolve('./metro.transformer.js');
 }
+
+// To test NCL from Expo Go, the react-native js source is from our fork.
+const reactNativeRoot = path.join(__dirname, '..', '..', 'react-native-lab', 'react-native');
 
 module.exports = {
   ...baseConfig,
@@ -41,8 +45,15 @@ module.exports = {
       // That is not ideal but should work for most cases if the two react-native versions do not have too much difference.
       // For example, `react-native-lab/react-native/node_modules/@react-native/polyfills` and `node_modules/@react-native/polyfills` may be different,
       // the metro config will use the transitive dependency from `node_modules/@react-native/polyfills`.
-      /\bnode_modules\/react-native\/\b/,
+      /\bnode_modules\/react-native\//,
       /\breact-native-lab\/react-native\/node_modules\b/,
     ],
+  },
+  serializer: {
+    ...baseConfig.serializer,
+    getModulesRunBeforeMainModule: () => [
+      require.resolve(path.join(reactNativeRoot, 'Libraries/Core/InitializeCore')),
+    ],
+    getPolyfills: () => require(path.join(reactNativeRoot, 'rn-get-polyfills'))(),
   },
 };
