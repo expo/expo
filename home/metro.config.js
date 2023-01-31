d86a40683ca38da0b57fafb1b9fa6200fa749547
Kudo Chien <kudo@expo.dev>
2023-01-31 18:48:58 +0800

    [home] use hermes engine (#21011)
    # Why
    
    close ENG-7162
    
    # How
    
    - [android] support hermes in loading home app
    - [home] use hermes engine
    
    # Test Plan
    
    - unversioned android/ios expo go + local home + `console.log(globalThis.HermesInternal)`

--- home/metro.config.js
+++ home/metro.config.js
@@ -1,6 +1,10 @@
 const { createMetroConfiguration } = require('expo-yarn-workspaces');
+const path = require('path');
 
 const baseConfig = createMetroConfiguration(__dirname);
+
+// To test home from Expo Go, the react-native js source is from our fork.
+const reactNativeRoot = path.join(__dirname, '..', 'react-native-lab', 'react-native');
 
 module.exports = {
   ...baseConfig,
@@ -21,5 +25,29 @@ module.exports = {
         return middleware(req, res, next);
       };
     },
+  },
+
+  resolver: {
+    ...baseConfig.resolver,
+    blockList: [
+      ...baseConfig.resolver.blockList,
+
+      // Because react-native versions may be different between node_modules/react-native and react-native-lab,
+      // metro and react-native cannot serve duplicated files from different paths.
+      // Assuming home only serves for Expo Go,
+      // the strategy here is to serve react-native imports from `react-native-lab/react-native` but not its transitive dependencies.
+      // That is not ideal but should work for most cases if the two react-native versions do not have too much difference.
+      // For example, `react-native-lab/react-native/node_modules/@react-native/polyfills` and `node_modules/@react-native/polyfills` may be different,
+      // the metro config will use the transitive dependency from `node_modules/@react-native/polyfills`.
+      /\bnode_modules\/react-native\//,
+      /\breact-native-lab\/react-native\/node_modules\b/,
+    ],
+  },
+  serializer: {
+    ...baseConfig.serializer,
+    getModulesRunBeforeMainModule: () => [
+      require.resolve(path.join(reactNativeRoot, 'Libraries/Core/InitializeCore')),
+    ],
+    getPolyfills: () => require(path.join(reactNativeRoot, 'rn-get-polyfills'))(),
   },
 };
