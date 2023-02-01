8b0b63e9654cda36f4fd6fe6036db388e8c160ba
Evan Bacon <bacon@expo.io>
2023-01-31 19:04:57 -0600

    feat(metro-config): block react-dom traces (#21005)
    # Why
    
    - react-dom spams the logbox in metro web.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/metro-config/build/ExpoMetroConfig.js
+++ packages/@expo/metro-config/build/ExpoMetroConfig.js
@@ -97,7 +97,9 @@ const INTERNAL_CALLSITES_REGEX = new RegExp(['/Libraries/Renderer/implementation
 // Hide Hermes internal bytecode
 '/InternalBytecode/InternalBytecode\\.js$',
 // Block native code invocations
-`\\[native code\\]`].join('|'));
+`\\[native code\\]`,
+// Hide react-dom (web)
+'node_modules/react-dom/.+\\.js$'].join('|'));
 exports.INTERNAL_CALLSITES_REGEX = INTERNAL_CALLSITES_REGEX;
 function isUrl(value) {
   try {
