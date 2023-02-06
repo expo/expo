a397df3fdabf9fb800b227028e960056f6b8785a
Jérémy Magrin <jeremy.magrin@gmail.com>
2023-02-06 11:49:16 +0100

    feat(expo-image): build module

--- packages/expo-image/build/Image.types.d.ts
+++ packages/expo-image/build/Image.types.d.ts
@@ -39,6 +39,11 @@ export type ImageSource = {
  */
 export type ImageStyle = RNImageStyle;
 /**
+ * Determines how the image should be resized to fit its container.
+ * @hidden Described in the {@link ImageProps['contentFit']}
+ */
+export type ImageContentFit = 'cover' | 'contain' | 'fill' | 'none' | 'scale-down';
+/**
  * Some props are from React Native Image that Expo Image supports (more or less) for easier migration,
  * but all of them are deprecated and might be removed in the future.
  */
@@ -74,7 +79,27 @@ export interface ImageProps extends ViewProps {
      *
      * @default 'cover'
      */
-    contentFit?: 'cover' | 'contain' | 'fill' | 'none' | 'scale-down';
+    contentFit?: ImageContentFit;
+    /**
+     * Determines how the placeholder should be resized to fit its container. This property tells the image to fill the container
+     * in a variety of ways; such as "preserve that aspect ratio" or "stretch up and take up as much space as possible".
+     * It mirrors the CSS [`object-fit`](https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit) property.
+     *
+     * - `'cover'` - The placeholder is sized to maintain its aspect ratio while filling the container box.
+     * If the image's aspect ratio does not match the aspect ratio of its box, then the object will be clipped to fit.
+     *
+     * - `'contain'` - The placeholder is scaled down or up to maintain its aspect ratio while fitting within the container box.
+     *
+     * - `'fill'` - The placeholder is sized to entirely fill the container box. If necessary, the image will be stretched or squished to fit.
+     *
+     * - `'none'` - The placeholder is not resized and is centered by default.
+     * When specified, the exact position can be controlled with [`contentPosition`](#contentposition) prop.
+     *
+     * - `'scale-down'` - The placeholder is sized as if `none` or `contain` were specified, whichever would result in a smaller concrete image size.
+     *
+     * @default 'scale-down'
+     */
+    placeholderContentFit?: ImageContentFit;
     /**
      * It is used together with [`contentFit`](#contentfit) to specify how the image should be positioned with x/y coordinates inside its own container.
      * An equivalent of the CSS [`object-position`](https://developer.mozilla.org/en-US/docs/Web/CSS/object-position) property.
@@ -216,11 +241,6 @@ export interface ImageNativeProps extends ImageProps {
     contentPosition?: ImageContentPositionObject;
     transition?: ImageTransition | null;
 }
-/**
- * Determines how the image should be resized to fit its container.
- * @hidden Described in the {@link ImageProps['contentFit']}
- */
-export type ImageContentFit = ImageProps['contentFit'];
 /**
  * A value that represents the relative position of a single axis.
  *
