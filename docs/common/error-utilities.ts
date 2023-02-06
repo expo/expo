48447f648ba217ea9fd8e59c121ff439aa55d852
Aman Mittal <amandeepmittal@live.com>
2023-02-07 02:11:58 +0530

    Update error-utilities & link to ExpoKit in Glossary

--- docs/common/error-utilities.ts
+++ docs/common/error-utilities.ts
@@ -142,9 +142,9 @@ const RENAMED_PAGES: Record<string, string> = {
   '/guides/genymotion/': '/workflow/android-studio-emulator/',
   '/workflow/upgrading-expo/': '/workflow/upgrading-expo-sdk-walkthrough/',
   '/workflow/create-react-native-app/': '/workflow/glossary-of-terms/#create-react-native-app',
-  '/expokit/': '/expokit/overview/',
-  '/guides/detach/': '/expokit/eject/',
-  '/expokit/detach/': '/expokit/eject/',
+  '/expokit/': '/archive/expokit/overview/',
+  '/guides/detach/': '/archive/expokit/eject/',
+  '/expokit/detach/': '/archive/expokit/eject/',
 
   // Development builds redirects
   '/development/build/': '/development/create-development-builds/',
@@ -159,7 +159,7 @@ const RENAMED_PAGES: Record<string, string> = {
 
   // Lots of old links pointing to guides when they have moved elsewhere
   '/guides/configuration/': '/workflow/configuration/',
-  '/guides/expokit/': '/expokit/overview/',
+  '/guides/expokit/': '/archive/expokit/overview/',
   '/guides/publishing/': '/archive/classic-updates/publishing/',
   '/workflow/publishing/': '/archive/classic-updates/publishing/',
   '/guides/linking/': '/workflow/linking/',
