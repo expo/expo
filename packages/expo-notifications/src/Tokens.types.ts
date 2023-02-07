7c7b18392bb8c94826ac0bdda911015bf1ac6e59
Simek <gosimek@gmail.com>
2023-02-07 12:04:23 +0100

    [notifications] rename and export `getExpoPushTokenAsync` parameter type

--- packages/expo-notifications/src/Tokens.types.ts
+++ packages/expo-notifications/src/Tokens.types.ts
@@ -62,3 +62,53 @@ export interface ExpoPushToken {
    */
   data: string;
 }
+
+export interface ExpoPushTokenOptions {
+  /**
+   * Endpoint URL override.
+   */
+  baseUrl?: string;
+  /**
+   * Request URL override.
+   */
+  url?: string;
+  /**
+   * Request body override.
+   */
+  type?: string;
+  // @missingDocs
+  deviceId?: string;
+  /**
+   * Makes sense only on iOS, where there are two push notification services: "sandbox" and "production".
+   * This defines whether the push token is supposed to be used with the sandbox platform notification service.
+   * Defaults to [`Application.getIosPushNotificationServiceEnvironmentAsync()`](/application/#applicationgetiospushnotificationserviceenvironmentasync)
+   * exposed by `expo-application` or `false`. Most probably you won't need to customize that.
+   * You may want to customize that if you don't want to install `expo-application` and still use the sandbox APNs.
+   * @platform ios
+   */
+  development?: boolean;
+  /**
+   * @deprecated Use `projectId` instead.
+   * The ID of the experience to which the token should be attributed.
+   * Defaults to [`Constants.manifest.id`](/constants/#constantsmanifest) exposed by `expo-constants`.
+   * When building with EAS Build, or in the bare workflow, **this is required** and you must provide a value which takes the shape `@username/projectSlug`,
+   * where `username` is the Expo account that the project is associated with, and `projectSlug` is your [`slug`](/config/app.mdx#slug) from Expo config.
+   */
+  experienceId?: string;
+  /**
+   * The ID of the project to which the token should be attributed.
+   * Defaults to [`Constants.expoConfig.extra.eas.projectId`](/constants/#easconfig) exposed by `expo-constants`.
+   * If you are not using EAS Build, it will fallback to [`Constants.manifest.projectId`](/constants/#constantsmanifest).
+   */
+  projectId?: string;
+  /**
+   * The ID of the application to which the token should be attributed.
+   * Defaults to [`Application.applicationId`](/application/#applicationapplicationid) exposed by `expo-application`.
+   */
+  applicationId?: string;
+  /**
+   * The device push token with which to register at the backend.
+   * Defaults to a token fetched with [`getDevicePushTokenAsync()`](#getdevicepushtokenasync-devicepushtoken).
+   */
+  devicePushToken?: DevicePushToken;
+}
