d62468846edff0f1c2d3c892b6f7c173a307fff9
Evan Bacon <bacon@expo.io>
2023-02-09 14:23:56 -0600

    Migrate tabs template to Expo Router (#21127)
    # Why
    
    Our navigation demo should use the latest version of Expo Router.
    
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    - TBD

--- templates/expo-template-tabs/app/(tabs)/index.tsx
+++ templates/expo-template-tabs/app/(tabs)/index.tsx
@@ -1,15 +1,14 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from '../../components/EditScreenInfo';
+import { Text, View } from '../../components/Themed';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
