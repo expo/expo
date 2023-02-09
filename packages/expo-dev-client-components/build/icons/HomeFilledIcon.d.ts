9fa1eef8f5643bc4d8fa016b5833f9889258fc8e
Brent Vatne <brentvatne@gmail.com>
2023-02-09 12:12:29 -0800

    [packages] Commit build files [skip ci]

---
+++ packages/expo-dev-client-components/build/icons/HomeFilledIcon.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Image } from '../Image';
+type TabBarIconProps = Partial<React.ComponentProps<typeof Image>> & {
+    focused?: boolean;
+};
+export declare function HomeFilledIcon(props: TabBarIconProps): JSX.Element;
+export {};
+//# sourceMappingURL=HomeFilledIcon.d.ts.map
