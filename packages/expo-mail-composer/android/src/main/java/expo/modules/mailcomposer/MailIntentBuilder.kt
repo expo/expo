f248cf0cf5935b8fee0eeeaae1eca311c08a77ca
Alan Hughes <hughesno1@gmail.com>
2023-02-09 22:57:21 +0000

    [android][mail-composer] module migration

--- packages/expo-mail-composer/android/src/main/java/expo/modules/mailcomposer/MailIntentBuilder.kt
+++ packages/expo-mail-composer/android/src/main/java/expo/modules/mailcomposer/MailIntentBuilder.kt
@@ -5,20 +5,15 @@ import android.content.ComponentName
 import android.content.Intent
 import android.net.Uri
 import android.text.Html
+import android.text.Html.FROM_HTML_MODE_LEGACY
 import android.util.Log
 import androidx.core.content.FileProvider
-import expo.modules.core.arguments.ReadableArguments
 import java.io.File
 
 class MailIntentBuilder(
-  private val options: ReadableArguments
+  private val options: MailComposerOptions
 ) {
   private val mailIntent = Intent(Intent.ACTION_SEND_MULTIPLE)
-
-  @Suppress("UNCHECKED_CAST")
-  private fun getStringArrayFrom(key: String): Array<String?> {
-    return (options.getList(key) as List<String?>).toTypedArray()
-  }
 
   private fun contentUriFromFile(file: File, application: Application): Uri = try {
     FileProvider.getUriForFile(
@@ -36,22 +31,36 @@ class MailIntentBuilder(
     mailIntent.component = ComponentName(pkg, cls)
   }
 
-  fun putExtraIfKeyExists(key: String, intentName: String) = apply {
-    if (options.containsKey(key)) {
-      if (options.getList(key) != null) {
-        mailIntent.putExtra(intentName, getStringArrayFrom(key))
-      } else {
-        mailIntent.putExtra(intentName, options.getString(key))
-      }
+  fun putRecipients(intentName: String) = apply {
+    options.recipients?.let {
+      mailIntent.putExtra(intentName, it.toTypedArray())
     }
   }
 
-  fun putExtraIfKeyExists(key: String, intentName: String, isBodyHtml: Boolean) = apply {
-    if (options.containsKey(key)) {
+  fun putCCRecipients(intentName: String) = apply {
+    options.ccRecipients?.let {
+      mailIntent.putExtra(intentName, it.toTypedArray())
+    }
+  }
+
+  fun putBCCRecipients(intentName: String) = apply {
+    options.bccRecipients?.let {
+      mailIntent.putExtra(intentName, it.toTypedArray())
+    }
+  }
+
+  fun putSubject(intentName: String) = apply {
+    options.subject?.let {
+      mailIntent.putExtra(intentName, it)
+    }
+  }
+
+  fun putBody(intentName: String, isBodyHtml: Boolean) = apply {
+    options.body?.let {
       val body = if (isBodyHtml) {
-        Html.fromHtml(options.getString(key))
+        Html.fromHtml(options.body)
       } else {
-        options.getString(key)
+        options.body
       }
       mailIntent.putExtra(intentName, body)
     }
@@ -63,11 +72,12 @@ class MailIntentBuilder(
     application: Application,
   ) = apply {
     try {
-      if (options.containsKey(key)) {
-        val requestedAttachments = getStringArrayFrom(key)
+      options.attachments?.let {
+        val requestedAttachments = it.toTypedArray()
         val attachments = requestedAttachments.map { requestedAttachment ->
           val path = Uri.parse(requestedAttachment).path
-          requireNotNull(path, { "Path to attachment can not be null" })
+          requireNotNull(path) { "Path to attachment can not be null" }
+
           val attachmentFile = File(path)
           contentUriFromFile(attachmentFile, application)
         }.toCollection(ArrayList())
