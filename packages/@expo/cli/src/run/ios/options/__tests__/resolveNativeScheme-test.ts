52538725c175a506661a384e6fdbd2a40c1f3641
Evan Bacon <bacon@expo.io>
2023-02-15 19:43:44 -0600

    feat(cli): improve ios multi-scheme resolution (#21240)
    # Why
    
    - Building app clips onto a native device will fail because the protocol
    appears to be unimplemented natively on-device.
    - When an iOS project has multiple schemes, the default scheme should be
    the first application scheme if available. This prevents the addition of
    something like App Clips from breaking the default behavior.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/cli/src/run/ios/options/__tests__/resolveNativeScheme-test.ts
+++ packages/@expo/cli/src/run/ios/options/__tests__/resolveNativeScheme-test.ts
@@ -1,7 +1,7 @@
 import { IOSConfig } from '@expo/config-plugins';
 
 import { selectAsync } from '../../../../utils/prompts';
-import { promptOrQueryNativeSchemeAsync } from '../resolveNativeScheme';
+import { promptOrQueryNativeSchemeAsync, getDefaultNativeScheme } from '../resolveNativeScheme';
 
 const asMock = <T extends (...args: any[]) => any>(fn: T): jest.MockedFunction<T> =>
   fn as jest.MockedFunction<T>;
@@ -16,6 +16,51 @@ jest.mock('@expo/config-plugins', () => {
       },
     },
   };
+});
+
+describe(getDefaultNativeScheme, () => {
+  it(`defaults to application scheme regardless of position in array`, () => {
+    asMock(IOSConfig.BuildScheme.getRunnableSchemesFromXcodeproj).mockReturnValueOnce([
+      // Ensure the wrong one is first...
+      { name: 'foobar2', osType: 'watchOS', type: 'com.apple.product-type.application.watchapp' },
+      { name: 'foobar', osType: 'iOS', type: 'com.apple.product-type.application' },
+    ]);
+    expect(
+      getDefaultNativeScheme(
+        '/',
+        {
+          configuration: 'Debug',
+        },
+        {
+          name: 'foo',
+        }
+      )
+    ).toEqual({
+      name: 'foobar',
+      osType: 'iOS',
+      type: 'com.apple.product-type.application',
+    });
+  });
+  it(`uses only scheme if available even if no application scheme is available`, () => {
+    asMock(IOSConfig.BuildScheme.getRunnableSchemesFromXcodeproj).mockReturnValueOnce([
+      { name: 'foobar2', osType: 'watchOS', type: 'com.apple.product-type.application.watchapp' },
+    ]);
+    expect(
+      getDefaultNativeScheme(
+        '/',
+        {
+          configuration: 'Debug',
+        },
+        {
+          name: 'foo',
+        }
+      )
+    ).toEqual({
+      name: 'foobar2',
+      osType: 'watchOS',
+      type: 'com.apple.product-type.application.watchapp',
+    });
+  });
 });
 
 describe(promptOrQueryNativeSchemeAsync, () => {
