0394357322dc156a21ead94f63a7693c61ac032b
Douglas Lowder <doug@expo.io>
2023-02-19 00:23:22 -0800

    [expo-updates] useUpdateEvents hook (#21258)
    A React hook to make it more convenient to use `Updates.addListener` to
    listen for events coming from native code that checks for updates on
    launch.
    
    New file `UpdatesHooks.ts` containing the new hook. API doc comments
    include an example.
    
    - Test the new API in `App-apitest.js`. This example app is enhanced
    significantly to exercise the Updates API interactively. Eventually I
    will use it as the basis for additions to the updates E2E tests that
    will test the API automatically with Detox.
    - Check generated unversioned API doc addition
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [x] This diff will work correctly for `expo prebuild` & EAS Build (eg:
    updated a module plugin).

---
+++ packages/expo-updates/e2e/__tests__/fixtures/App-apitest.tsx
@@ -0,0 +1,153 @@
+/**
+ * Test app that shows some features of the Updates API
+ */
+import { StatusBar } from 'expo-status-bar';
+import * as Updates from 'expo-updates';
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [updateMessage, setUpdateMessage] = React.useState('');
+  const [updateAvailable, setUpdateAvailable] = React.useState(false);
+
+  // Displays a message showing whether or not the app is running
+  // a downloaded update
+  const runTypeMessage = Updates.isEmbeddedLaunch
+    ? 'This app is running from built-in code'
+    : 'This app is running an update';
+
+  /**
+   * Async function to manually check for an available update from EAS.
+   */
+  const checkManuallyForUpdate = async () => {
+    setUpdateMessage('Calling checkForUpdateAsync...');
+    const checkResult = await Updates.checkForUpdateAsync();
+    if (checkResult.isAvailable) {
+      setUpdateMessage(
+        `checkForUpdateAsync found a new update: manifest = \n${manifestToString(
+          checkResult.manifest
+        )}...`
+      );
+    } else {
+      setUpdateMessage('No new update available');
+    }
+    setUpdateAvailable(checkResult.isAvailable);
+  };
+
+  /**
+   * Async function to fetch and load the most recent update from EAS.
+   */
+  const downloadAndRunUpdate = async () => {
+    setUpdateMessage('Downloading the new update...');
+    await Updates.fetchUpdateAsync();
+    setUpdateMessage('Downloaded update... launching it in 2 seconds.');
+    await delay(2000);
+    await Updates.reloadAsync();
+  };
+
+  /**
+   * Sample UpdateEvent listener that handles all three event types.
+   * These events occur during app startup, when expo-updates native code
+   * automatically checks for available updates from EAS.
+   * @param {} event The event to handle
+   */
+  const eventListener = (event: Updates.UpdateEvent) => {
+    if (event.type === Updates.UpdateEventType.ERROR) {
+      setUpdateMessage(`Error: ${event.message}`);
+    } else if (event.type === Updates.UpdateEventType.NO_UPDATE_AVAILABLE) {
+      setUpdateMessage('No new update available');
+    } else if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
+      setUpdateMessage(`New update available\n${manifestToString(event.manifest)}`);
+      setUpdateAvailable(true);
+    }
+  };
+  Updates.useUpdateEvents(eventListener);
+
+  const handleCheckButtonPress = () => {
+    checkManuallyForUpdate().catch((error) => {
+      setUpdateMessage(`Error checking for updates: ${error.message}`);
+    });
+  };
+
+  const handleDownloadButtonPress = () => {
+    downloadAndRunUpdate().catch((error) => {
+      setUpdateMessage(`Error downloading and running update: ${error.message}`);
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{runTypeMessage}</Text>
+      <Text style={styles.updateMessageText}>{updateMessage}</Text>
+      <Pressable style={styles.button} onPress={handleCheckButtonPress}>
+        <Text style={styles.buttonText}>Check manually for updates</Text>
+      </Pressable>
+      {updateAvailable ? (
+        <Pressable style={styles.button} onPress={handleDownloadButtonPress}>
+          <Text style={styles.buttonText}>Download and run update</Text>
+        </Pressable>
+      ) : null}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#4630EB',
+  },
+  buttonText: {
+    color: 'white',
+  },
+  updateMessageText: {
+    margin: 10,
+    height: 200,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    width: '90%',
+    borderColor: '#4630EB',
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+});
+
+///////////////////////////
+
+/**
+ * Promise wrapper for setTimeout()
+ * @param {delay} timeout Timeout in ms
+ * @returns a Promise that resolves after the timeout has elapsed
+ */
+const delay = (timeout: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+};
+
+const manifestToString = (manifest?: Updates.Manifest) => {
+  return manifest
+    ? JSON.stringify(
+        {
+          id: manifest.id,
+          createdAt: manifest.createdAt,
+          metadata: manifest.metadata,
+        },
+        null,
+        2
+      )
+    : 'null';
+};
