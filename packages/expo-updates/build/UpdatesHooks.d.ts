a861444c3b6e77d0f0c14111100a0a843e908ae7
Douglas Lowder <doug@expo.io>
2023-02-19 00:23:22 -0800

    [expo-updates] useUpdateEvents hook (#21258)
    # Why
    
    A React hook to make it more convenient to use `Updates.addListener` to
    listen for events coming from native code that checks for updates on
    launch.
    
    # How
    
    New file `UpdatesHooks.ts` containing the new hook. API doc comments
    include an example.
    
    # Test Plan
    
    - Test the new API in `App-apitest.js`. This example app is enhanced
    significantly to exercise the Updates API interactively. Eventually I
    will use it as the basis for additions to the updates E2E tests that
    will test the API automatically with Detox.
    - Check generated unversioned API doc addition
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [x] This diff will work correctly for `expo prebuild` & EAS Build (eg:
    updated a module plugin).

---
+++ packages/expo-updates/build/UpdatesHooks.d.ts
@@ -0,0 +1,27 @@
+import { UpdateEvent } from './Updates.types';
+/**
+ * React hook to create an [`UpdateEvent`](#updateevent) listener subscription on mount, using
+ * [`addListener`](#updatesaddlistenerlistener). It calls `remove()` on the subscription during unmount.
+ *
+ * @param listener A function that will be invoked with an [`UpdateEvent`](#updateevent) instance
+ * and should not return any value.
+ *
+ * @example
+ * ```ts
+ * function App() {
+ *   const eventListener = (event) => {
+ *     if (event.type === Updates.UpdateEventType.ERROR) {
+ *       // Handle error
+ *     } else if (event.type === Updates.UpdateEventType.NO_UPDATE_AVAILABLE) {
+ *       // Handle no update available
+ *     } else if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
+ *       // Handle update available
+ *     }
+ *   };
+ *   Updates.useUpdateEvents(eventListener);
+ *   // React Component...
+ * }
+ * ```
+ */
+export declare const useUpdateEvents: (listener: (event: UpdateEvent) => void) => void;
+//# sourceMappingURL=UpdatesHooks.d.ts.map
