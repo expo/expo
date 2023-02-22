88e03aae94ee5163a2d9daa55f34a6a13086ea52
Evan Bacon <baconbrix@gmail.com>
2023-02-22 02:15:04 -0600

    update fixtures to use latest

--- packages/@expo/config-plugins/src/ios/__tests__/fixtures/Podfile.ts
+++ packages/@expo/config-plugins/src/ios/__tests__/fixtures/Podfile.ts
@@ -1,26 +1,7 @@
-export const PodfileBasic = `
-require_relative '../node_modules/react-native/scripts/react_native_pods'
-require_relative '../node_modules/react-native-unimodules/cocoapods.rb'
-require_relative '../node_modules/@react-native-community/cli-platform-ios/native_modules'
+import fs from 'fs';
+import path from 'path';
 
-platform :ios, '11.0'
-
-target 'myapp' do
-  use_unimodules!
-  config = use_native_modules!
-
-  use_react_native!(:path => config["reactNativePath"])
-
-  # Uncomment the code below to enable Flipper.
-  #
-  # You should not install Flipper in CI environments when creating release
-  # builds, this will lead to significantly slower build times.
-  #
-  # Note that if you have use_frameworks! enabled, Flipper will not work.
-  #
-  #  use_flipper!
-  #  post_install do |installer|
-  #    flipper_post_install(installer)
-  #  end
-end
-`;
+export const PodfileBasic = fs.readFileSync(
+  path.join(__dirname, '../../../../../../../templates/expo-template-bare-minimum/ios/Podfile'),
+  'utf8'
+);
