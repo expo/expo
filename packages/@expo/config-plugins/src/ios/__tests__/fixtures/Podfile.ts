eb561335bc7c293c38a25fa215e9c256cc19f2d1
Evan Bacon <baconbrix@gmail.com>
2023-02-22 02:50:15 -0600

    use all latest fixtures

--- packages/@expo/config-plugins/src/ios/__tests__/fixtures/Podfile.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export const PodfileBasic = fs.readFileSync(
-  path.join(__dirname, '../../../../../../../templates/expo-template-bare-minimum/ios/Podfile'),
-  'utf8'
-);
