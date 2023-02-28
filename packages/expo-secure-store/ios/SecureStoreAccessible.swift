c92f7715bd5202603826e92b880b7d71fb57904d
Alan Hughes <hughesno1@gmail.com>
2023-02-28 23:25:44 +0000

    [ios][secure-store] make requested changes

--- packages/expo-secure-store/ios/SecureStoreAccessible.swift
+++ packages/expo-secure-store/ios/SecureStoreAccessible.swift
@@ -1,11 +1,11 @@
 import ExpoModulesCore
 
 enum SecureStoreAccessible: Int, Enumerable {
-  case afterFirstUnlock
-  case afterFirstUnlockThisDeviceOnly
-  case always
-  case whenPasscodeSetThisDeviceOnly
-  case alwaysThisDeviceOnly
-  case whenUnlocked
-  case whenUnlockedThisDeviceOnly
+  case afterFirstUnlock = 0
+  case afterFirstUnlockThisDeviceOnly = 1
+  case always = 2
+  case whenPasscodeSetThisDeviceOnly = 3
+  case alwaysThisDeviceOnly = 4
+  case whenUnlocked = 5
+  case whenUnlockedThisDeviceOnly = 6
 }
