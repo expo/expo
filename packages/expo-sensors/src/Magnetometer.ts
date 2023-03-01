fa41168ca4d9255c879764d8a9354a28d834efed
Vittor Javidan <vittor.javidan.t@gmail.com>
2023-03-01 19:58:49 -0300

    [docs] simple typo fixing inside Magnetometer.ts
    probably a copy and paste from barometer without changing names. My OCD could not stop tingling seeing this haha.

--- packages/expo-sensors/src/Magnetometer.ts
+++ packages/expo-sensors/src/Magnetometer.ts
@@ -28,7 +28,7 @@ export class MagnetometerSensor extends DeviceSensor<MagnetometerMeasurement> {
   /**
    * > **info** You should always check the sensor availability before attempting to use it.
    *
-   * Check the availability of the device barometer. Requires at least Android 2.3 (API Level 9) and iOS 8.
+   * Check the availability of the device magnetometer. Requires at least Android 2.3 (API Level 9) and iOS 8.
    * @return A promise that resolves to a `boolean` denoting the availability of the sensor.
    */
   async isAvailableAsync(): Promise<boolean> {
