e0637741a01beeddc9beef437d38144a6e7c0621
Will Schurman <wschurman@expo.io>
2023-03-02 16:38:59 -0800

    [exupdates][ios] Convert (unused) database integrity check to swift (#21467)
    # Why
    
    These aren't used on either android or iOS (and they're buggy?), but I
    don't know that we should delete them? Anyways, I only figured that out
    after converting them so here's the PR that converts them.
    
    # How
    
    Convert.
    
    # Test Plan
    
    Run test.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `expo prebuild` & EAS Build (eg:
    updated a module plugin).

---
+++ packages/expo-updates/ios/EXUpdates/Database/EXUpdatesDatabaseIntegrityCheck.swift
@@ -0,0 +1,40 @@
+// Copyright 2021-present 650 Industries. All rights reserved.
+
+import Foundation
+
+public class EXUpdatesDatabaseIntegrityCheck {
+  public func run(
+    withDatabase database: EXUpdatesDatabase,
+    directory: URL,
+    config: EXUpdatesConfig,
+    embeddedUpdate: EXUpdatesUpdate?
+  ) throws {
+    let assets = try database.allAssets()
+    var missingAssets: [EXUpdatesAsset] = []
+    EXUpdatesFileDownloader.assetFilesQueue().sync {
+      for asset in assets where !assetExists(asset, inDirectory: directory) {
+        missingAssets.append(asset)
+      }
+    }
+
+    if !missingAssets.isEmpty {
+      try database.markMissingAssets(missingAssets)
+    }
+
+    let updatesWithEmbeddedStatus = try database.allUpdates(withStatus: .StatusEmbedded, config: config)
+    let updatesToDelete = updatesWithEmbeddedStatus.filter { update in
+      guard let embeddedUpdate = embeddedUpdate else {
+        return true
+      }
+      return update.updateId != embeddedUpdate.updateId
+    }
+
+    if !updatesToDelete.isEmpty {
+      try database.deleteUpdates(updatesToDelete)
+    }
+  }
+
+  public func assetExists(_ asset: EXUpdatesAsset, inDirectory directory: URL) -> Bool {
+    return FileManager.default.fileExists(atPath: directory.appendingPathComponent(asset.filename).path)
+  }
+}
