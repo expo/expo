e614f6f2422a1abe272f87887d43a0280301d7f0
Cedric van Putten <github@cedric.dev>
2023-03-06 15:44:43 +0100

    feature(cli): add vscode compatibility handler to inspector proxy

---
+++ packages/@expo/cli/src/start/server/metro/inspector-proxy/handlers/VscodeCompat.ts
@@ -0,0 +1,64 @@
+import type { Protocol } from 'devtools-protocol';
+import type { DebuggerInfo } from 'metro-inspector-proxy';
+
+import { CdpMessage, DebuggerRequest, DeviceResponse, InspectorHandler } from './types';
+
+export class VscodeCompatHandler implements InspectorHandler {
+  /** Keep track of device messages to intercept, by request id */
+  interceptDeviceMessage = new Set<number>();
+
+  onDebuggerMessage(
+    message: DebuggerRequest<DebuggerGetPossibleBreakpoints | RuntimeGetProperties>,
+    { socket }: Pick<DebuggerInfo, 'socket'>
+  ) {
+    // Hermes doesn't seem to handle this request, but `locations` have to be returned.
+    // Respond with an empty location to make it "spec compliant" with Chrome DevTools.
+    if (message.method === 'Debugger.getPossibleBreakpoints') {
+      const response: DeviceResponse<DebuggerGetPossibleBreakpoints> = {
+        id: message.id,
+        result: { locations: [] },
+      };
+      socket.send(JSON.stringify(response));
+      return true;
+    }
+
+    // Vscode doesn't seem to work nicely with missing `description` fields on `RemoteObject` instances.
+    // See: https://github.com/microsoft/vscode-js-debug/issues/1583
+    if (message.method === 'Runtime.getProperties') {
+      this.interceptDeviceMessage.add(message.id);
+    }
+
+    return false;
+  }
+
+  onDeviceMessage(message: DeviceResponse<RuntimeGetProperties>) {
+    // Vscode doesn't seem to work nicely with missing `description` fields on `RemoteObject` instances.
+    // See: https://github.com/microsoft/vscode-js-debug/issues/1583
+    if (this.interceptDeviceMessage.has(message.id)) {
+      this.interceptDeviceMessage.delete(message.id);
+
+      // Force-fully format the properties description to be an empty string
+      for (const item of message.result.result ?? []) {
+        if (item.value) {
+          item.value.description = item.value.description ?? '';
+        }
+      }
+    }
+
+    return false;
+  }
+}
+
+/** @see https://chromedevtools.github.io/devtools-protocol/v8/Debugger/#method-getPossibleBreakpoints */
+export type DebuggerGetPossibleBreakpoints = CdpMessage<
+  'Debugger.getPossibleBreakpoints',
+  Protocol.Debugger.GetPossibleBreakpointsRequest,
+  Protocol.Debugger.GetPossibleBreakpointsResponse
+>;
+
+/** @see https://chromedevtools.github.io/devtools-protocol/v8/Runtime/#method-getProperties */
+export type RuntimeGetProperties = CdpMessage<
+  'Runtime.getProperties',
+  Protocol.Runtime.GetPropertiesRequest,
+  Protocol.Runtime.GetPropertiesResponse
+>;
---
+++ packages/@expo/cli/src/start/server/metro/inspector-proxy/handlers/__tests__/VscodeCompat.test.ts
@@ -0,0 +1,65 @@
+import { RuntimeGetProperties, VscodeCompatHandler } from '../VscodeCompat';
+
+describe(VscodeCompatHandler, () => {
+  it('responds to `Debugger.getPossibleBreakpoints` with empty `locations`', () => {
+    const handler = new VscodeCompatHandler();
+    const debuggerSocket = { send: jest.fn() };
+
+    expect(
+      handler.onDebuggerMessage(
+        {
+          id: 420,
+          method: 'Debugger.getPossibleBreakpoints',
+          params: {
+            start: { lineNumber: 13, columnNumber: 37, scriptId: '1337' },
+          },
+        },
+        { socket: debuggerSocket }
+      )
+    ).toBe(true);
+
+    expect(debuggerSocket.send).toBeCalledWith(
+      JSON.stringify({
+        id: 420,
+        result: { locations: [] },
+      })
+    );
+  });
+
+  it('mutates `Runtime.getProperties` device response with `description` properties', () => {
+    const handler = new VscodeCompatHandler();
+    const debuggerSocket = { send: jest.fn() };
+
+    // This message should still be propagated, it should return `false`
+    expect(
+      handler.onDebuggerMessage(
+        {
+          id: 420,
+          method: 'Runtime.getProperties',
+          params: {
+            objectId: '1337',
+            start: { lineNumber: 13, columnNumber: 37, scriptId: '1337' },
+          },
+        },
+        { socket: debuggerSocket }
+      )
+    ).toBe(false);
+
+    // The handler mutates the properties, we need to keep a reference
+    const descriptors: RuntimeGetProperties['result'] = {
+      result: [
+        {
+          name: 'foo',
+          configurable: true,
+          enumerable: true,
+          value: { type: 'function' },
+        },
+      ],
+    };
+
+    // This message should still be propagated, it should return `false`
+    expect(handler.onDeviceMessage({ id: 420, result: descriptors })).toBe(false);
+    // Expect the descriptor values to be mutated
+    expect(descriptors.result[0].value).toHaveProperty('description', '');
+  });
+});
