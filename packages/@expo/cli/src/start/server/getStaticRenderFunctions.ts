c8ddf0250792f5873eeeea654efcfbae833704cc
evanbacon <bacon@expo.io>
2023-03-07 13:13:50 -0600

    Update getStaticRenderFunctions.ts

--- packages/@expo/cli/src/start/server/getStaticRenderFunctions.ts
+++ packages/@expo/cli/src/start/server/getStaticRenderFunctions.ts
@@ -4,9 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import fs from 'fs';
 import fetch from 'node-fetch';
 import path from 'path';
-import fs from 'fs';
 import requireString from 'require-from-string';
 import resolveFrom from 'resolve-from';
 
