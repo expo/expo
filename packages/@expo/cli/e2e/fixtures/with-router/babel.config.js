0d8eb9e2ba76d8f957c294cd65fc036dbe035526
evanbacon <bacon@expo.io>
2023-03-07 11:43:43 -0600

    reduce fixture

--- packages/@expo/cli/e2e/fixtures/with-router/babel.config.js
+++ packages/@expo/cli/e2e/fixtures/with-router/babel.config.js
@@ -2,10 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',
-      require.resolve('expo-router/babel'),
-    ],
+    plugins: [require.resolve('expo-router/babel')],
   };
 };
