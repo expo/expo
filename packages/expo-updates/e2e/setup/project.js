322d021976b1e38ac205f96f3edef37ad098499d
Will Schurman <wschurman@gmail.com>
2023-03-08 16:47:52 -0800

    [exupdates][ios][e2e] Fix compilation errors for e2e test

--- packages/expo-updates/e2e/setup/project.js
+++ packages/expo-updates/e2e/setup/project.js
@@ -239,21 +239,13 @@ async function preparePackageJson(projectRoot, repoRoot, configureE2E) {
  */
 async function prepareLocalUpdatesModule(repoRoot) {
   // copy UpdatesE2ETest exported module into the local package
-  const iosE2ETestModuleHPath = path.join(
+  const iosE2ETestModuleSwiftPath = path.join(
     repoRoot,
     'packages',
     'expo-updates',
     'ios',
     'EXUpdates',
-    'EXUpdatesE2ETestModule.h'
-  );
-  const iosE2ETestModuleMPath = path.join(
-    repoRoot,
-    'packages',
-    'expo-updates',
-    'ios',
-    'EXUpdates',
-    'EXUpdatesE2ETestModule.m'
+    'E2ETestModule.swift'
   );
   const androidE2ETestModuleKTPath = path.join(
     repoRoot,
@@ -269,12 +261,8 @@ async function prepareLocalUpdatesModule(repoRoot) {
     'UpdatesE2ETestModule.kt'
   );
   await fs.copyFile(
-    path.resolve(dirName, '..', 'fixtures', 'EXUpdatesE2ETestModule.h'),
-    iosE2ETestModuleHPath
-  );
-  await fs.copyFile(
-    path.resolve(dirName, '..', 'fixtures', 'EXUpdatesE2ETestModule.m'),
-    iosE2ETestModuleMPath
+    path.resolve(dirName, '..', 'fixtures', 'E2ETestModule.swift'),
+    iosE2ETestModuleSwiftPath
   );
   await fs.copyFile(
     path.resolve(dirName, '..', 'fixtures', 'UpdatesE2ETestModule.kt'),
@@ -313,8 +301,7 @@ async function prepareLocalUpdatesModule(repoRoot) {
   // Return cleanup function
   return async () => {
     await fs.writeFile(updatesPackageFilePath, originalUpdatesPackageFileContents, 'utf8');
-    await fs.rm(iosE2ETestModuleHPath, { force: true });
-    await fs.rm(iosE2ETestModuleMPath, { force: true });
+    await fs.rm(iosE2ETestModuleSwiftPath, { force: true });
     await fs.rm(androidE2ETestModuleKTPath, { force: true });
   };
 }
