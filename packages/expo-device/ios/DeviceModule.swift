645fadabd7c13c8bdfef3e44cbaa653eaaf2ceb6
Robert Herber <robert@kingstinct.com>
2023-03-09 18:52:08 +0530

    Swiftlint fixes

--- packages/expo-device/ios/DeviceModule.swift
+++ packages/expo-device/ios/DeviceModule.swift
@@ -3,98 +3,98 @@ import UIKit
 import MachO
 
 public class DeviceModule: Module {
-  public func definition() -> ModuleDefinition {
-    Name("ExpoDevice")
+    public func definition() -> ModuleDefinition {
+        Name("ExpoDevice")
 
-    Constants([
-      "isDevice": isDevice(),
-      "brand": "Apple",
-      "manufacturer": "Apple",
-      "modelId": UIDevice.modelIdentifier,
-      "modelName": UIDevice.DeviceMap.modelName,
-      "deviceYearClass": UIDevice.DeviceMap.deviceYearClass,
-      "totalMemory": ProcessInfo.processInfo.physicalMemory,
-      "osName": UIDevice.current.systemName,
-      "osVersion": UIDevice.current.systemVersion,
-      "osBuildId": osBuildId(),
-      "osInternalBuildId": osBuildId(),
-      "deviceName": UIDevice.current.name,
-      "deviceType": getDeviceType(),
-      "supportedCpuArchitectures": cpuArchitectures()
-    ])
+        Constants([
+            "isDevice": isDevice(),
+            "brand": "Apple",
+            "manufacturer": "Apple",
+            "modelId": UIDevice.modelIdentifier,
+            "modelName": UIDevice.DeviceMap.modelName,
+            "deviceYearClass": UIDevice.DeviceMap.deviceYearClass,
+            "totalMemory": ProcessInfo.processInfo.physicalMemory,
+            "osName": UIDevice.current.systemName,
+            "osVersion": UIDevice.current.systemVersion,
+            "osBuildId": osBuildId(),
+            "osInternalBuildId": osBuildId(),
+            "deviceName": UIDevice.current.name,
+            "deviceType": getDeviceType(),
+            "supportedCpuArchitectures": cpuArchitectures()
+        ])
 
-    AsyncFunction("getDeviceTypeAsync") { () -> Int in
-      return getDeviceType()
+        AsyncFunction("getDeviceTypeAsync") { () -> Int in
+            return getDeviceType()
+        }
+
+        AsyncFunction("getUptimeAsync") { () -> Double in
+            let uptimeMs: Double = ProcessInfo.processInfo.systemUptime * 1000
+
+            return uptimeMs
+        }
+
+        AsyncFunction("isRootedExperimentalAsync") { () -> Bool in
+            return UIDevice.current.isJailbroken
+        }
     }
-
-    AsyncFunction("getUptimeAsync") { () -> Double in
-      let uptimeMs: Double = ProcessInfo.processInfo.systemUptime * 1000
-
-      return uptimeMs
-    }
-
-    AsyncFunction("isRootedExperimentalAsync") { () -> Bool in
-      return UIDevice.current.isJailbroken
-    }
-  }
 }
 
 func getDeviceType() -> Int {
-  switch UIDevice.current.userInterfaceIdiom {
+    switch UIDevice.current.userInterfaceIdiom {
     case UIUserInterfaceIdiom.phone:
-      return DeviceType.phone.rawValue
+        return DeviceType.phone.rawValue
     case UIUserInterfaceIdiom.pad:
-      return DeviceType.tablet.rawValue
+        return DeviceType.tablet.rawValue
     case UIUserInterfaceIdiom.tv:
-      return DeviceType.tv.rawValue
+        return DeviceType.tv.rawValue
     default:
-      return DeviceType.unknown.rawValue
-  }
+        return DeviceType.unknown.rawValue
+    }
 }
 
 func isDevice() -> Bool {
-  #if targetEnvironment(simulator)
-  return false
-  #else
-  return true
-  #endif
+    #if targetEnvironment(simulator)
+    return false
+    #else
+    return true
+    #endif
 }
 
 func osBuildId() -> String? {
-  #if os(tvOS)
-  return nil
-  #else
-  // Credit: https://stackoverflow.com/a/65858410
-  var mib: [Int32] = [CTL_KERN, KERN_OSVERSION]
-  let namelen = UInt32(MemoryLayout.size(ofValue: mib) / MemoryLayout.size(ofValue: mib[0]))
-  var bufferSize = 0
+    #if os(tvOS)
+    return nil
+    #else
+    // Credit: https://stackoverflow.com/a/65858410
+    var mib: [Int32] = [CTL_KERN, KERN_OSVERSION]
+    let namelen = UInt32(MemoryLayout.size(ofValue: mib) / MemoryLayout.size(ofValue: mib[0]))
+    var bufferSize = 0
 
-  // Get the size for the buffer
-  sysctl(&mib, namelen, nil, &bufferSize, nil, 0)
+    // Get the size for the buffer
+    sysctl(&mib, namelen, nil, &bufferSize, nil, 0)
 
-  var buildBuffer = [UInt8](repeating: 0, count: bufferSize)
-  let result = sysctl(&mib, namelen, &buildBuffer, &bufferSize, nil, 0)
+    var buildBuffer = [UInt8](repeating: 0, count: bufferSize)
+    let result = sysctl(&mib, namelen, &buildBuffer, &bufferSize, nil, 0)
 
-  if result >= 0 && bufferSize > 0 {
-    return String(bytesNoCopy: &buildBuffer, length: bufferSize - 1, encoding: .utf8, freeWhenDone: false)
-  }
+    if result >= 0 && bufferSize > 0 {
+        return String(bytesNoCopy: &buildBuffer, length: bufferSize - 1, encoding: .utf8, freeWhenDone: false)
+    }
 
-  return nil
-  #endif
+    return nil
+    #endif
 }
 
 func cpuArchitectures() -> [String]? {
-  // Credit: https://stackoverflow.com/a/70134518
-  guard let archRaw = NXGetLocalArchInfo().pointee.name else {
-    return nil
-  }
-  return [String(cString: archRaw)]
+    // Credit: https://stackoverflow.com/a/70134518
+    guard let archRaw = NXGetLocalArchInfo().pointee.name else {
+        return nil
+    }
+    return [String(cString: archRaw)]
 }
 
 enum DeviceType: Int {
-  case unknown = 0
-  case phone = 1
-  case tablet = 2
-  case desktop = 3
-  case tv = 4
+    case unknown = 0
+    case phone = 1
+    case tablet = 2
+    case desktop = 3
+    case tv = 4
 }
