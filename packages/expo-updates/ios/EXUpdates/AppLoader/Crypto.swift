c1d2fdfe9fe696369f893fa22ff7e7c2578af68a
Will Schurman <wschurman@gmail.com>
2023-03-10 09:20:29 -0800

    [exupdates][ios] Run swiftlint --fix

--- packages/expo-updates/ios/EXUpdates/AppLoader/Crypto.swift
+++ packages/expo-updates/ios/EXUpdates/AppLoader/Crypto.swift
@@ -43,7 +43,7 @@ internal final class Crypto {
   private static let PublicKeyTag = "exp.host.publickey"
   private static let PublicKeyFilename = "manifestPublicKey.pem"
 
-  public static func decodePEMToDER(pem: Data, pemType: PEMType) -> Data? {
+  static func decodePEMToDER(pem: Data, pemType: PEMType) -> Data? {
     // Mostly from ASN1Decoder with the fix for disallowing multiple bodies in the PEM.
 
     guard let pem = String(data: pem, encoding: .ascii) else {
@@ -75,7 +75,7 @@ internal final class Crypto {
     return nil
   }
 
-  public static func verifySignature(
+  static func verifySignature(
     withData data: String,
     signature: String,
     config: UpdatesConfig,
