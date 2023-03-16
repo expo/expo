3a20b5ae5cd62a0d7ada35a8e00672b1946af283
Bartosz Kaszubowski <gosimek@gmail.com>
2023-03-16 22:02:43 +0100

    [docs] Footer: tweaks based on feedback (#21737)

--- docs/ui/components/Footer/PageVote.tsx
+++ docs/ui/components/Footer/PageVote.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@expo/styleguide';
-import { ThumbsDownSolidIcon, ThumbsUpSolidIcon } from '@expo/styleguide-icons';
+import { ThumbsDownIcon, ThumbsUpIcon } from '@expo/styleguide-icons';
 import { useState } from 'react';
 
 import { CALLOUT } from '../Text';
@@ -9,38 +9,40 @@ import { reportPageVote } from '~/providers/Analytics';
 export const PageVote = () => {
   const [userVoted, setUserVoted] = useState(false);
   return (
-    <div className="min-w-[200px]">
-      <CALLOUT theme="secondary" weight="medium">
-        Was this doc helpful?
-      </CALLOUT>
+    <div>
       {userVoted ? (
-        <CALLOUT theme="secondary" className="py-3">
+        <CALLOUT theme="secondary" className="py-1">
           Thank you for your vote! 💙
         </CALLOUT>
       ) : (
-        <div className="flex flex-row">
-          <Button
-            theme="secondary"
-            size="xs"
-            aria-label="Vote up"
-            className="mt-2.5 mx-1 min-w-[40px] text-center"
-            leftSlot={<ThumbsUpSolidIcon className="icon-sm" />}
-            onClick={() => {
-              reportPageVote({ status: true });
-              setUserVoted(true);
-            }}
-          />
-          <Button
-            theme="secondary"
-            size="xs"
-            aria-label="Vote down"
-            className="mt-2.5 mx-1 min-w-[40px] text-center"
-            leftSlot={<ThumbsDownSolidIcon className="icon-sm" />}
-            onClick={() => {
-              reportPageVote({ status: false });
-              setUserVoted(true);
-            }}
-          />
+        <div className="flex flex-row items-center gap-2 max-large:flex-col">
+          <CALLOUT theme="secondary" weight="medium" className="px-2">
+            Was this doc helpful?
+          </CALLOUT>
+          <div>
+            <Button
+              theme="secondary"
+              size="xs"
+              aria-label="Vote up"
+              className="mx-1 min-w-[40px] text-center"
+              leftSlot={<ThumbsUpIcon className="icon-sm" />}
+              onClick={() => {
+                reportPageVote({ status: true });
+                setUserVoted(true);
+              }}
+            />
+            <Button
+              theme="secondary"
+              size="xs"
+              aria-label="Vote down"
+              className="mx-1 min-w-[40px] text-center"
+              leftSlot={<ThumbsDownIcon className="icon-sm" />}
+              onClick={() => {
+                reportPageVote({ status: false });
+                setUserVoted(true);
+              }}
+            />
+          </div>
         </div>
       )}
     </div>
