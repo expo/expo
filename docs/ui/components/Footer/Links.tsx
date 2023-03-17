5398fb6670c2a804c5fbeb7f4a1f530a68e07b51
Simek <gosimek@gmail.com>
2023-03-17 19:14:35 +0100

    [docs] refactor part of the components, use TW instead Emotion

--- docs/ui/components/Footer/Links.tsx
+++ docs/ui/components/Footer/Links.tsx
@@ -1,23 +1,19 @@
-import { css } from '@emotion/react';
-import { theme, typography } from '@expo/styleguide';
-import { spacing } from '@expo/styleguide-base';
 import { Edit05Icon, GithubIcon, MessageDotsSquareIcon } from '@expo/styleguide-icons';
 
-import { A, LI } from '../Text';
+import { A, CALLOUT, LI } from '../Text';
 import { githubUrl } from './utils';
 
 export const IssuesLink = ({ title, repositoryUrl }: { title: string; repositoryUrl?: string }) => (
   <LI>
     <A
-      css={linkStyle}
+      isStyled
       openInNewTab
       href={
         repositoryUrl ? `${repositoryUrl}/issues` : `https://github.com/expo/expo/labels/${title}`
-      }>
-      <span className="flex items-center mr-2.5">
-        <GithubIcon className="text-icon-secondary" />
-      </span>
-      View open bug reports for {title}
+      }
+      className="inline-flex items-center mb-1">
+      <GithubIcon className="flex items-center mr-2.5 text-icon-secondary" />
+      <CALLOUT theme="secondary">View open bug reports for {title}</CALLOUT>
     </A>
   </LI>
 );
@@ -25,44 +21,33 @@ export const IssuesLink = ({ title, repositoryUrl }: { title: string; repository
 export const ForumsLink = ({ isAPIPage, title }: { isAPIPage: boolean; title: string }) =>
   isAPIPage ? (
     <LI>
-      <A css={linkStyle} openInNewTab href={`https://forums.expo.dev/tag/${title}`}>
-        <span className="flex items-center mr-2.5">
-          <MessageDotsSquareIcon className="text-icon-secondary" />
-        </span>
-        Ask a question on the forums about {title}
+      <A
+        isStyled
+        openInNewTab
+        href={`https://forums.expo.dev/tag/${title}`}
+        className="inline-flex items-center mb-1">
+        <MessageDotsSquareIcon className="flex items-center mr-2.5 text-icon-secondary" />
+        <CALLOUT theme="secondary">Ask a question on the forums about {title}</CALLOUT>
       </A>
     </LI>
   ) : (
     <LI>
-      <A css={linkStyle} openInNewTab href="https://forums.expo.dev/">
-        <span className="flex items-center mr-2.5">
-          <MessageDotsSquareIcon className="text-icon-secondary" />
-        </span>
-        Ask a question on the forums
+      <A
+        isStyled
+        openInNewTab
+        href="https://forums.expo.dev/"
+        className="inline-flex items-center mb-1">
+        <MessageDotsSquareIcon className="flex items-center mr-2.5 text-icon-secondary" />
+        <CALLOUT theme="secondary">Ask a question on the forums</CALLOUT>
       </A>
     </LI>
   );
 
 export const EditPageLink = ({ pathname }: { pathname: string }) => (
   <LI>
-    <A css={linkStyle} openInNewTab href={githubUrl(pathname)}>
-      <span className="flex items-center mr-2.5">
-        <Edit05Icon className="text-icon-secondary" />
-      </span>
-      Edit this page
+    <A isStyled openInNewTab href={githubUrl(pathname)} className="inline-flex items-center mb-1">
+      <Edit05Icon className="flex items-center mr-2.5 text-icon-secondary" />
+      <CALLOUT theme="secondary">Edit this page</CALLOUT>
     </A>
   </LI>
 );
-
-const linkStyle = css({
-  ...typography.fontSizes[14],
-  textDecoration: 'none',
-  color: theme.text.secondary,
-  display: 'inline-flex',
-  alignItems: 'center',
-  marginBottom: spacing[1],
-
-  ':visited': {
-    color: theme.text.secondary,
-  },
-});
