a5e75a87cde3bf4d3f97cd46944f33abed812e0a
wojciechdrozdz <wojciech.drozdz@swmansion.com>
2023-03-20 15:09:24 +0100

    Throw exception when current activity not found

--- packages/expo-blur/android/src/main/java/expo/modules/blur/ExpoBlurView.kt
+++ packages/expo-blur/android/src/main/java/expo/modules/blur/ExpoBlurView.kt
@@ -7,6 +7,7 @@ import eightbitlab.com.blurview.BlurView
 import eightbitlab.com.blurview.RenderEffectBlur
 import eightbitlab.com.blurview.RenderScriptBlur
 import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.exception.Exceptions
 import expo.modules.kotlin.views.ExpoView
 
 class ExpoBlurView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
@@ -16,7 +17,7 @@ class ExpoBlurView(context: Context, appContext: AppContext) : ExpoView(context,
   private val blurView = BlurView(context).also {
     it.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
 
-    val decorView = appContext.currentActivity?.window?.decorView
+    val decorView = (appContext.currentActivity ?: throw Exceptions.MissingActivity()).window?.decorView
     val rootView = decorView?.findViewById(android.R.id.content) as ViewGroup
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
       it.setupWith(rootView, RenderEffectBlur())
