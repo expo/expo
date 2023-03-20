234e070fa20fac6f1b53402b87a6b2096ce734ef
Bartosz Kaszubowski <gosimek@gmail.com>
2023-03-20 09:08:23 +0100

    [docs] refactor part of the components, use TW instead Emotion (#21751)

--- docs/ui/components/Home/HomeButton.tsx
+++ docs/ui/components/Home/HomeButton.tsx
@@ -1,21 +1,11 @@
-import { typography, Button, ButtonProps } from '@expo/styleguide';
-import { spacing } from '@expo/styleguide-base';
+import { Button, ButtonProps, mergeClasses } from '@expo/styleguide';
 
-export const HomeButton = ({ children, style, href, ...rest }: ButtonProps) => (
+export const HomeButton = ({ children, style, href, className, ...rest }: ButtonProps) => (
   <Button
     {...rest}
     href={href}
     openInNewTab={href?.startsWith('http')}
-    style={{
-      ...typography.fontSizes[14],
-      height: 36,
-      paddingLeft: spacing[3.5],
-      paddingRight: spacing[3.5],
-      position: 'absolute',
-      bottom: 28,
-      zIndex: 1,
-      ...style,
-    }}>
+    className={mergeClasses('h-9 px-3.5 absolute bottom-7 z-10', className)}>
     {children}
   </Button>
 );
