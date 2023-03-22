1171baab01db6da42281de537f9fd450b05eeb4a
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2023-03-22 13:19:47 +0100

    [client] Remove vendored gesture handler (#21502)
    # Why
    
    Removes vendored gesture handler.
    Another step to support Fabric by the dev-client.
    
    > Note: To support fabric in the dev-client, we need to rewrite the safe area context, but that won't be difficult, because that module is pretty straightforward and we can use our API to support both renderers. 
    
    # How
    
    - Removed vendored code.
    - Mocked functions required by react-navigation.
    - Disabled gesture in the stack navigator.
    
    The swipe gesture won't longer close a screen, but I think it's something that we can sacrifice. Also, most of our screens are rendered inside the bottom tab navigator. 
    
    # Test Plan
    
    - bare-expo on both platforms ✅
    - ensures that the stack navigator works correctly ✅

--- packages/expo-dev-menu/vendored/react-native-gesture-handler/ios/DevMenuRNGestureHandlerDirection.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-typedef NS_ENUM(NSInteger, DevMenuRNGestureHandlerDirection) {
-    DevMenuRNGestureHandlerDirectionRight = 1,
-    DevMenuRNGestureHandlerDirectionLeft = 2,
-    DevMenuRNGestureHandlerDirectionUp = 4,
-    DevMenuRNGestureHandlerDirectionDown = 8,
-};
