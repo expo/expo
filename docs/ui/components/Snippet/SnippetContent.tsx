5f560ada446259142a4ecb4df480e0b8acd22871
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2023-03-28 21:38:38 +0300

    [docs] partially migrate Snippet component to Tailwind (#21864)
    # Why
    
    The plan is roughly to:
    1. Migrate snippet from using Emotion entirely to Tailwind.
    2. Copy snippet to styleguide repo.
    3. Switch to using snippet from styleguide.
    4. Remove local snippet implementation.
    
    We are in step 1.
    
    # How
    
    By rewriting class after class from Emotion to Styleguide.
    
    There are some classes left but this brings us one step closer to
    finishing point 1.
    
    I change I made: instead of 42px I used 40px to just use a TW class and
    skip adding too much custom styles. The difference is probably
    unnoticeable if you don't know what to look for, but of course it's an
    unplanned change so we might not want it. Let me know what you think.
    
    # Test Plan
    
    Visit snippets in docs and make sure that I didn't break anything.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [x] This diff will work correctly for `expo prebuild` & EAS Build (eg:
    updated a module plugin).

--- docs/ui/components/Snippet/SnippetContent.tsx
+++ docs/ui/components/Snippet/SnippetContent.tsx
@@ -1,6 +1,5 @@
 import { css } from '@emotion/react';
-import { theme } from '@expo/styleguide';
-import { borderRadius, spacing } from '@expo/styleguide-base';
+import { mergeClasses } from '@expo/styleguide';
 import { forwardRef, PropsWithChildren } from 'react';
 
 export type SnippetContentProps = PropsWithChildren<{
@@ -23,47 +22,28 @@ export const SnippetContent = forwardRef<HTMLDivElement, SnippetContentProps>(
   ) => (
     <div
       ref={ref}
-      css={[
-        contentStyle,
-        alwaysDark && contentDarkStyle,
-        hideOverflow && contentHideOverflow,
-        skipPadding && skipPaddingStyle,
-      ]}
-      className={className}>
+      css={[contentStyle, hideOverflow && contentHideOverflow]}
+      className={mergeClasses(
+        'text-default bg-subtle border border-default rounded-b-md overflow-x-auto p-4',
+        alwaysDark && 'bg-palette-black border-transparent whitespace-nowrap',
+        hideOverflow && 'overflow-hidden',
+        skipPadding && 'p-0',
+        className
+      )}>
       {children}
     </div>
   )
 );
 
 const contentStyle = css`
-  color: ${theme.text.default};
-  background-color: ${theme.background.subtle};
-  border: 1px solid ${theme.border.default};
-  border-bottom-left-radius: ${borderRadius.md}px;
-  border-bottom-right-radius: ${borderRadius.md}px;
-  padding: ${spacing[4]}px;
-  overflow-x: auto;
-
   code {
     padding-left: 0;
     padding-right: 0;
   }
 `;
 
-const contentDarkStyle = css`
-  background-color: ${theme.palette.black};
-  border-color: transparent;
-  white-space: nowrap;
-`;
-
 const contentHideOverflow = css`
-  overflow: hidden;
-
   code {
     white-space: nowrap;
   }
 `;
-
-const skipPaddingStyle = css({
-  padding: 0,
-});
