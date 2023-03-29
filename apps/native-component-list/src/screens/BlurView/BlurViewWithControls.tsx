1a6fd5aa7f9f6fb50b3ae27cf8b4f5828537b221
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2023-03-29 10:51:01 +0200

    [expo-blur][Android] Add support for BlurView on Android (#21744)
    # Why
    
    There was no support for `BlurView` on android. Instead of blurring the
    view would just set the background color to semitransparent.
    
    # How
    
    Using [BlurView](https://github.com/Dimezis/BlurView) library and
    Modules API added blur support for Android. Default prop settings match
    it quite closely with the iOS version.
    
    # Test Plan
    
    Tested on Android emulators (API 33 and 28), and a physical device (API
    31). Also compared the look of the blur on android emulator to the iOS
    simulator.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- apps/native-component-list/src/screens/BlurView/BlurViewWithControls.tsx
+++ apps/native-component-list/src/screens/BlurView/BlurViewWithControls.tsx
@@ -1,42 +1,60 @@
-import { BlurTint, BlurView } from 'expo-blur';
-import React, { useState, useCallback, useRef, memo } from 'react';
-import { View, StyleSheet, Text, Image, Animated } from 'react-native';
+import { BlurTint, BlurView, BlurViewProps } from 'expo-blur';
+import React, { useCallback, useRef, memo, useEffect } from 'react';
+import { View, StyleSheet, Text, Image } from 'react-native';
+import Animated, {
+  useAnimatedProps,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 
 import useResettingState from '../../utilities/useResettingState';
 import Slider from './Slider';
-import useLoopingAnimatedValue from './useLoopingAnimatedValue';
 
 const AnimatedBlurView = Animated.createAnimatedComponent(BlurView);
 
 export default memo((props: { tint: BlurTint }) => {
   const blurViewRef = useRef<View>(null);
-
-  const animatedIntensity = useLoopingAnimatedValue(1, 100);
-  const [manualIntensity, setManualIntensity] = useState(0);
+  const animatedIntensity = useSharedValue(0);
+  const manualIntensity = useSharedValue(0);
   const [manualIntensityIsActive, setManualIntensityIsActive] = useResettingState(false, 3000);
 
   const handleSliderChange = useCallback((value: number) => {
     setManualIntensityIsActive(true);
-    setManualIntensity(value);
+    manualIntensity.value = value;
   }, []);
+
+  useEffect(() => {
+    // Use two with timing animations to make sure the animation always runs from 0 to 100
+    animatedIntensity.value = withRepeat(
+      withSequence(withTiming(100, { duration: 2000 }), withTiming(0, { duration: 2000 })),
+      -1,
+      true
+    );
+  }, []);
+
+  const animatedProps = useAnimatedProps(() => {
+    return {
+      proxiedProperties: {
+        intensity: manualIntensityIsActive ? manualIntensity.value : animatedIntensity.value,
+        tint: props.tint,
+      },
+    } as BlurViewProps;
+  });
 
   return (
     <View style={styles.container}>
       <View style={styles.innerContainer}>
         <Image style={styles.image} source={{ uri: 'https://source.unsplash.com/300x300' }} />
         <Text style={styles.blurredText}>This text is blurred</Text>
-        <AnimatedBlurView
-          ref={blurViewRef}
-          style={styles.blurView}
-          intensity={manualIntensityIsActive ? manualIntensity : animatedIntensity}
-          tint={props.tint}>
+        <AnimatedBlurView ref={blurViewRef} style={styles.blurView} animatedProps={animatedProps}>
           <Text style={styles.nonBlurredText}>{props.tint}</Text>
-
           <Slider
             title="Manual intensity:"
             onChange={handleSliderChange}
             active={!!manualIntensityIsActive}
-            value={manualIntensity}
+            value={manualIntensity.value}
             style={styles.slider}
           />
         </AnimatedBlurView>
