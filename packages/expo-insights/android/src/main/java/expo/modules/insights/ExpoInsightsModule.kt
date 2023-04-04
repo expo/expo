493824e880023e85ebbf6a1919e739ffef2c9ec1
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-04-04 23:26:03 +0200

    Introduce expo-insights module for iOS (#21932)
    Co-authored-by: Tomasz Sapeta <tsapeta@users.noreply.github.com>

---
+++ packages/expo-insights/android/src/main/java/expo/modules/insights/ExpoInsightsModule.kt
@@ -0,0 +1,10 @@
+package expo.modules.insights
+
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class ExpoInsightsModule : Module() {
+  override fun definition() = ModuleDefinition {
+
+  }
+}
