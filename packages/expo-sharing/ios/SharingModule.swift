c10216a51e79273d2488020667e5efaca7bd34a6
Alan Hughes <hughesno1@gmail.com>
2023-04-05 15:36:48 +0100

    [ios][sharing] change UIDocumentInteractionController to UIActivityViewController

--- packages/expo-sharing/ios/SharingModule.swift
+++ packages/expo-sharing/ios/SharingModule.swift
@@ -1,22 +1,11 @@
 import ExpoModulesCore
 
-struct SharingContext {
-  let pendingPromise: Promise
-  let delegate: SharingDelegate
-}
-
-public final class SharingModule: Module, OnDocumentInteractionResult {
-  private var documentInteractionController: UIDocumentInteractionController?
-  private var sharingContext: SharingContext?
+public final class SharingModule: Module {
 
   public func definition() -> ModuleDefinition {
     Name("ExpoSharing")
 
     AsyncFunction("shareAsync") { (url: URL, options: SharingOptions, promise: Promise) in
-      guard sharingContext == nil else {
-        throw SharingInProgressException()
-      }
-
       guard let filePermissions: EXFilePermissionModuleInterface =
         appContext?.legacyModule(implementing: EXFilePermissionModuleInterface.self)
       else {
@@ -24,41 +13,29 @@ public final class SharingModule: Module, OnDocumentInteractionResult {
       }
 
       let grantedPermissions = filePermissions.getPathPermissions(url.absoluteString)
-      guard grantedPermissions.rawValue >= EXFileSystemPermissionFlags.read.rawValue else {
+      guard grantedPermissions.rawValue <= EXFileSystemPermissionFlags.read.rawValue else {
         throw FilePermissionException()
       }
 
-      let sharingDelegate = SharingDelegate(resultHandler: self)
-      documentInteractionController = UIDocumentInteractionController(url: url)
-      documentInteractionController?.delegate = sharingDelegate
-      documentInteractionController?.uti = options.UTI
+      let activityController = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+      activityController.title = options.dialogTitle
 
-      guard let currentViewcontroller = appContext?.utilities?.currentViewController(),
-        let rootView = currentViewcontroller.view,
-        let documentInteractionController = documentInteractionController
-      else {
+      activityController.completionWithItemsHandler = { [weak self] _, _, _, error in
+        guard let self else { return }
+
+        if error != nil {
+          promise.reject(UnsupportedTypeException())
+        }
+
+        promise.resolve(nil)
+      }
+
+      guard let currentViewcontroller = appContext?.utilities?.currentViewController() else {
         throw MissingCurrentViewControllerException()
       }
 
-      if documentInteractionController.presentOpenInMenu(from: .zero, in: rootView, animated: true) {
-        self.sharingContext = SharingContext(pendingPromise: promise, delegate: sharingDelegate)
-      } else {
-        self.documentInteractionController = nil
-        throw UnsupportedTypeException()
-      }
+      currentViewcontroller.present(activityController, animated: true)
     }
     .runOnQueue(.main)
-  }
-
-  func didDismissOpenInMenu() {
-    guard let promise = self.sharingContext?.pendingPromise else {
-      log.error("SharingModule: sharing context has been lost")
-      return
-    }
-
-    promise.resolve(nil)
-    self.sharingContext = nil
-    self.documentInteractionController?.dismissMenu(animated: true)
-    self.documentInteractionController = nil
   }
 }
