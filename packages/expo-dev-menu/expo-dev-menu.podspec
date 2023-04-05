expo/expo

commit e870aeeef5ac649d608ae7ca1bbdc8a99e994ee8

[menu] Reimplement the bottom sheet using native languages (#21268)
# Why

Closes ENG-7280.
Reimplemented the bottom sheet using native languages and removed reanimated from vendored modules. 
By doing that we improve the build speed.

# How

- On Android, I've used the material design bottom sheet. We may want to copy the implementation from the original repo to ours to avoid extra dependency.
- On iOS, I've copied the implementation from https://github.com/applidium/OverlayContainer. I managed to test a couple of different libraries and that one works the best with RN. 

# Test Plan

- bare-expo on both platforms ✅

packages/expo-dev-menu/expo-dev-menu.podspec
@@ -1,38 +1,4 @@
 require 'json'
-require_relative './scripts/reanimated_utils'
-
-# reanimated 2
-
-config = find_config()
-reactVersion = '0.0.0'
-
-begin
-  reactVersion = `node --print "require('react-native/package.json').version"`
-rescue
-  reactVersion = '0.66.0'
-end
-
-if ENV["REACT_NATIVE_OVERRIDE_VERSION"]
-  reactVersion = ENV["REACT_NATIVE_OVERRIDE_VERSION"]
-end
-
-splitedReactVersion = reactVersion.split('.')
-rnVersion = splitedReactVersion[1]
-rnPatchVersion = splitedReactVersion[2]
-
-folly_prefix = ""
-if rnVersion.to_i >= 64
-  folly_prefix = "RCT-"
-end
-
-folly_flags = '-DFOLLY_NO_CONFIG -DFOLLY_MOBILE=1 -DFOLLY_USE_LIBCPP=1 -DRNVERSION=' + rnVersion + ' -DRNPATCHVERSION=' + rnPatchVersion
-folly_compiler_flags = folly_flags + ' ' + '-Wno-comma -Wno-shorten-64-to-32'
-folly_version = '2021.04.26.00'
-boost_compiler_flags = '-Wno-documentation'
-
-
-require_relative 'TargetValidator'
-# end reanimated 2
 
 package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
 
@@ -81,58 +47,6 @@ Pod::Spec.new do |s|
     end
   end
 
-  s.subspec 'Reanimated' do |reanimated|
-    reanimated.compiler_flags = folly_compiler_flags + ' ' + boost_compiler_flags + ' -w -Xanalyzer -analyzer-disable-all-checks -x objective-c++'
-    reanimated.private_header_files = 'vendored/react-native-reanimated/**/*.h'
-    reanimated.source_files = 'vendored/react-native-reanimated/**/*.{h,m,mm,cpp}'
-    reanimated.preserve_paths = 'vendored/react-native-reanimated/Common/cpp/hidden_headers/**'
-    reanimated.pod_target_xcconfig = {
-      "USE_HEADERMAP" => "YES",
-      "HEADER_SEARCH_PATHS" => "\"$(PODS_TARGET_SRCROOT)/ReactCommon\" \"$(PODS_TARGET_SRCROOT)\" \"$(PODS_ROOT)/#{folly_prefix}Folly\" \"$(PODS_ROOT)/boost\"  \"$(PODS_ROOT)/boost-for-react-native\" \"$(PODS_ROOT)/DoubleConversion\" \"$(PODS_ROOT)/Headers/Private/React-Core\" ",
-      'CLANG_CXX_LIBRARY' => 'libc++',
-      "CLANG_CXX_LANGUAGE_STANDARD" => "c++17"
-    }
-    reanimated.xcconfig = {
-      "HEADER_SEARCH_PATHS" => "\"$(PODS_ROOT)/boost\" \"$(PODS_ROOT)/boost-for-react-native\" \"$(PODS_ROOT)/glog\" \"$(PODS_ROOT)/#{folly_prefix}Folly\" \"${PODS_ROOT}/Headers/Public/React-hermes\" \"${PODS_ROOT}/Headers/Public/hermes-engine\" \"$(PODS_ROOT)/#{config[:react_native_common_dir]}\"",
-                                 "OTHER_CFLAGS" => "$(inherited)" + " " + folly_flags
-    }
-
-    reanimated.dependency 'FBLazyVector'
-    reanimated.dependency 'FBReactNativeSpec'
-    reanimated.dependency 'RCTRequired'
-    reanimated.dependency 'RCTTypeSafety'
-    reanimated.dependency 'React-Core'
-    reanimated.dependency 'React-CoreModules'
-    reanimated.dependency 'React-Core/DevSupport'
-    reanimated.dependency 'React-RCTActionSheet'
-    reanimated.dependency 'React-RCTNetwork'
-    reanimated.dependency 'React-RCTAnimation'
-    reanimated.dependency 'React-RCTLinking'
-    reanimated.dependency 'React-RCTBlob'
-    reanimated.dependency 'React-RCTSettings'
-    reanimated.dependency 'React-RCTText'
-    reanimated.dependency 'React-RCTVibration'
-    reanimated.dependency 'React-RCTImage'
-    reanimated.dependency 'React-Core/RCTWebSocket'
-    reanimated.dependency 'React-cxxreact'
-    reanimated.dependency 'React-jsi'
-    reanimated.dependency 'React-jsiexecutor'
-    reanimated.dependency 'React-jsinspector'
-    reanimated.dependency 'ReactCommon/turbomodule/core'
-    reanimated.dependency 'Yoga'
-    reanimated.dependency 'DoubleConversion'
-    reanimated.dependency 'glog'
-
-    if reactVersion.match(/^0.62/)
-      reanimated.dependency 'ReactCommon/callinvoker'
-    else
-      reanimated.dependency 'React-callinvoker'
-    end
-
-    reanimated.dependency "#{folly_prefix}Folly"
-  end
-
-
   s.subspec 'SafeAreaView' do |safearea|
     if File.exist?("vendored/react-native-safe-area-context/dev-menu-react-native-safe-area-context.xcframework") && Gem::Version.new(Pod::VERSION) >= Gem::Version.new('1.10.0')
       safearea.source_files = "vendored/react-native-safe-area-context/**/*.{h}"
@@ -148,7 +62,6 @@ Pod::Spec.new do |s|
 
   s.subspec 'Vendored' do |vendored|
     vendored.dependency "expo-dev-menu/GestureHandler"
-    vendored.dependency "expo-dev-menu/Reanimated"
     vendored.dependency "expo-dev-menu/SafeAreaView"
   end
 

packages/expo-dev-menu/ios/DevMenuAppInstance.swift
@@ -3,7 +3,7 @@
 import React
 
 @objc
-class DevMenuAppInstance: DevMenuBaseAppInstance, RCTBridgeDelegate {
+class DevMenuAppInstance: NSObject, RCTBridgeDelegate {
   static private var CloseEventName = "closeDevMenu"
   static private var OpenEventName = "openDevMenu"
 
@@ -33,7 +33,7 @@ class DevMenuAppInstance: DevMenuBaseAppInstance, RCTBridgeDelegate {
   public func sendCloseEvent() {
     bridge?.enqueueJSCall("RCTDeviceEventEmitter.emit", args: [DevMenuAppInstance.CloseEventName])
   }
-  
+
   public func sendOpenEvent() {
     bridge?.enqueueJSCall("RCTDeviceEventEmitter.emit", args: [DevMenuAppInstance.OpenEventName])
   }
@@ -51,7 +51,7 @@ class DevMenuAppInstance: DevMenuBaseAppInstance, RCTBridgeDelegate {
 
   func extraModules(for bridge: RCTBridge!) -> [RCTBridgeModule]! {
     var modules: [RCTBridgeModule] = [DevMenuInternalModule(manager: manager)]
-    modules.append(contentsOf: DevMenuVendoredModulesUtils.vendoredModules(bridge, addReanimated2: true))
+    modules.append(contentsOf: DevMenuVendoredModulesUtils.vendoredModules(bridge))
     modules.append(DevMenuLoadingView.init())
     modules.append(DevMenuRCTDevSettings.init())
     return modules

commit 38967aeffa64381c59f0f2efa007b45a918e3fa6

[dev-menu] Fix hermes inspector not inspecting app target (#21978)
# Why

probably a regression from #21268. now the hermes inspector will inspect the dev menu bundle again.
close ENG-7928

# How

- add `DevMenuRCTCxxBridgeDelegate` to call `setEnableDebugger(false)`

# Test Plan

- ci passed
- check hermes inspector loads app target rather than dev-menu bundle
- build passed on use_frameworks! mode

packages/expo-dev-menu/expo-dev-menu.podspec
@@ -2,6 +2,8 @@ require 'json'
 
 package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
 
+folly_compiler_flags = '-DFOLLY_NO_CONFIG -DFOLLY_MOBILE=1 -DFOLLY_USE_LIBCPP=1 -Wno-comma -Wno-shorten-64-to-32'
+
 Pod::Spec.new do |s|
   s.name           = 'expo-dev-menu'
   s.version        = package['version']
@@ -31,8 +33,17 @@ Pod::Spec.new do |s|
     "HEADER_SEARCH_PATHS" => "\"${PODS_CONFIGURATION_BUILD_DIR}/expo-dev-menu/Swift Compatibility Header\"",
   }
 
-  # Swift/Objective-C compatibility
-  s.pod_target_xcconfig = { "DEFINES_MODULE" => "YES" }
+  header_search_paths = [
+    '"$(PODS_ROOT)/boost"',
+    '"${PODS_ROOT}/Headers/Private/React-Core"',
+    '"$(PODS_CONFIGURATION_BUILD_DIR)/ExpoModulesCore/Swift Compatibility Header"',
+    '"$(PODS_CONFIGURATION_BUILD_DIR)/expo-dev-menu-interface/Swift Compatibility Header"',
+  ]
+  s.pod_target_xcconfig = {
+    'DEFINES_MODULE' => 'YES',
+    'CLANG_CXX_LANGUAGE_STANDARD' => 'c++17',
+    'HEADER_SEARCH_PATHS' => header_search_paths.join(' '),
+  }
 
   s.subspec 'SafeAreaView' do |safearea|
     if File.exist?("vendored/react-native-safe-area-context/dev-menu-react-native-safe-area-context.xcframework") && Gem::Version.new(Pod::VERSION) >= Gem::Version.new('1.10.0')
@@ -55,6 +66,7 @@ Pod::Spec.new do |s|
     s.source_files   = 'ios/**/*.{h,m,mm,swift}'
     s.preserve_paths = 'ios/**/*.{h,m,mm,swift}'
     s.exclude_files  = 'ios/*Tests/**/*', 'vendored/**/*'
+    s.compiler_flags = folly_compiler_flags
 
     main.dependency 'React-Core'
     main.dependency "EXManifests"

packages/expo-dev-menu/ios/DevMenuAppInstance.swift
@@ -3,7 +3,7 @@
 import React
 
 @objc
-class DevMenuAppInstance: NSObject, RCTBridgeDelegate {
+class DevMenuAppInstance: DevMenuRCTCxxBridgeDelegate, RCTBridgeDelegate {
   static private var CloseEventName = "closeDevMenu"
   static private var OpenEventName = "openDevMenu"
 

packages/expo-dev-menu/ios/DevMenuRCTBridge.h
@@ -16,4 +16,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
+@interface DevMenuRCTCxxBridgeDelegate : NSObject
+
+@end
+
 NS_ASSUME_NONNULL_END

packages/expo-dev-menu/ios/DevMenuRCTBridge.mm
@@ -1,6 +1,19 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 
 #import <EXDevMenu/DevMenuRCTBridge.h>
+
+// The search path for the Swift generated headers are different
+// between use_frameworks and non_use_frameworks mode.
+#if __has_include(<EXDevMenuInterface/EXDevMenuInterface-Swift.h>)
+#import <EXDevMenuInterface/EXDevMenuInterface-Swift.h>
+#else
+#import <EXDevMenuInterface-Swift.h>
+#endif
+#if __has_include(<ExpoModulesCore/ExpoModulesCore-Swift.h>)
+#import <ExpoModulesCore/ExpoModulesCore-Swift.h>
+#else
+#import <ExpoModulesCore-Swift.h>
+#endif
 #if __has_include(<EXDevMenu/EXDevMenu-Swift.h>)
 #import <EXDevMenu/EXDevMenu-Swift.h>
 #else
@@ -12,8 +25,11 @@
 #import <React/RCTDevSettings.h>
 #import <React/RCTBridge+Private.h>
 #import <React/RCTDevMenu.h>
-
-@import EXDevMenuInterface;
+#import <React/RCTCxxBridgeDelegate.h>
+#import <React/RCTJSIExecutorRuntimeInstaller.h>
+#if __has_include(<reacthermes/HermesExecutorFactory.h>)
+#import <reacthermes/HermesExecutorFactory.h>
+#endif
 
 @implementation DevMenuRCTCxxBridge
 
@@ -87,5 +103,28 @@
 // This method is still used so we need to override it even if it's deprecated
 - (void)reloadWithReason:(NSString *)reason {}
 #pragma clang diagnostic pop
+
+@end
+
+@interface DevMenuRCTCxxBridgeDelegate () <RCTCxxBridgeDelegate>
+
+@end
+
+@implementation DevMenuRCTCxxBridgeDelegate
+
+- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
+{
+#if __has_include(<reacthermes/HermesExecutorFactory.h>)
+  // Disable Hermes debugger to prevent Hermes debugger uses dev-menu
+  // as inspecting target.
+  auto installBindings = facebook::react::RCTJSIExecutorRuntimeInstaller(nullptr);
+  auto *hermesExecutorFactory = new facebook::react::HermesExecutorFactory(installBindings);
+  hermesExecutorFactory->setEnableDebugger(false);
+  std::unique_ptr<facebook::react::JSExecutorFactory> jsExecutorFactory(hermesExecutorFactory);
+  return std::move(jsExecutorFactory);
+#else
+  return nullptr;
+#endif
+}
 
 @end