4bfce46f3ce17cd626fe6de93b28fa6c8e89321e
Alan Hughes <hughesno1@gmail.com>
2023-04-06 14:06:06 +0100

    [ios][sharing] add handler for dismissal

--- packages/expo-sharing/ios/SharingModule.swift
+++ packages/expo-sharing/ios/SharingModule.swift
@@ -5,7 +5,7 @@ public final class SharingModule: Module {
   public func definition() -> ModuleDefinition {
     Name("ExpoSharing")
 
-    AsyncFunction("shareAsync") { (url: URL, options: SharingOptions) in
+    AsyncFunction("shareAsync") { (url: URL, options: SharingOptions, promise: Promise) in
       guard let filePermissions: EXFilePermissionModuleInterface =
         appContext?.legacyModule(implementing: EXFilePermissionModuleInterface.self)
       else {
@@ -19,6 +19,18 @@ public final class SharingModule: Module {
 
       let activityController = UIActivityViewController(activityItems: [url], applicationActivities: nil)
       activityController.title = options.dialogTitle
+      
+      activityController.completionWithItemsHandler = { type, completed, _, _ in
+        // user shared an item
+        if type != nil && completed {
+          promise.resolve(nil)
+        }
+        
+        // dismissed without action
+        if type == nil && !completed {
+          promise.resolve(nil)
+        }
+      }
 
       guard let currentViewcontroller = appContext?.utilities?.currentViewController() else {
         throw MissingCurrentViewControllerException()
