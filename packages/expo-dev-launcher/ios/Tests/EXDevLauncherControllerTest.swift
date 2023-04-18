f46f800704d4e7dd827400c68ab6edd10ecad913
gabrieldonadel <donadeldev@gmail.com>
2023-04-18 13:33:22 -0300

    Delete DevMenuVendoredModulesUtils

--- packages/expo-dev-launcher/ios/Tests/EXDevLauncherControllerTest.swift
+++ packages/expo-dev-launcher/ios/Tests/EXDevLauncherControllerTest.swift
@@ -26,10 +26,6 @@ class EXDevLauncherControllerTest: QuickSpec {
       expect(modules.first { $0 is RCTDevMenu }).toNot(beNil())
       expect(modules.first { type(of: $0).moduleName() == "DevLoadingView" }).toNot(beNil())
       expect(modules.first { type(of: $0).moduleName() == "EXDevLauncherInternal" }).toNot(beNil())
-
-      // vendored
-      expect(modules.first { type(of: $0).moduleName() == "RNCSafeAreaProvider" }).toNot(beNil())
-      expect(modules.first { type(of: $0).moduleName() == "RNCSafeAreaView" }).toNot(beNil())
     }
 
     it("controller should have access to managers classes") {
