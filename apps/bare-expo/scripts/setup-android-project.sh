da2829bff7521cec90e5bf69767a77c5d26d3cd6
Kudo Chien <kudo@expo.dev>
2023-04-25 08:25:29 +0800

    [bare-expo] use android espresso for test-suite ci testing (#22214)
    # Why
    
    our android test-suite ci testing is flaky. most of time coming from detox hardcoded 45s timeout to install apk
    
    # How
    
    use android espresso rather than detox. detox will still work for reference.
    
    # Test Plan
    
    ci passed

--- apps/bare-expo/scripts/setup-android-project.sh
+++ apps/bare-expo/scripts/setup-android-project.sh
@@ -2,6 +2,9 @@
 
 echo " ☛  Ensuring Android project is setup..."
 
+CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+cd "${CURR_DIR}/.."
+
 if [ -d "./node_modules" ]; then
     echo " ✅ Node modules installed"
 else
@@ -9,6 +12,11 @@ else
     yarn
 fi
 
-CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 "${CURR_DIR}/../../../bin/expotools" android-generate-dynamic-macros --configuration $1 --bare
 echo " ✅ Generete dynamic macros"
+
+if [ ! -d "android/app/src/androidTest/assets" ]; then
+  mkdir -p android/app/src/androidTest/assets
+fi
+yarn --silent ts-node --print --transpile-only -e 'function describe() {}; JSON.stringify(require("./e2e/TestSuite-test.native.js").TESTS, null, 2)' > android/app/src/androidTest/assets/TestSuite.json
+echo " ✅ Generete e2e test cases"
