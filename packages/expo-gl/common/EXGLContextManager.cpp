4265af340b3a61f257378af13ee245b093094f33
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2023-05-09 16:36:12 +0200

    [gl] Fix race condition when creating gl context in close succession

--- packages/expo-gl/common/EXGLContextManager.cpp
+++ packages/expo-gl/common/EXGLContextManager.cpp
@@ -6,24 +6,38 @@ namespace gl_cpp {
 struct ContextState {
   EXGLContext *ctx;
   std::shared_mutex mutex;
+  bool isReady;
+  std::condition_variable cv;
 };
 
 struct ContextManager {
   std::unordered_map<EXGLContextId, ContextState> contextMap;
-  std::mutex contextLookupMutex;
+  std::shared_mutex contextLookupMutex;
   EXGLContextId nextId = 1;
 };
 
 ContextManager manager;
 
 ContextWithLock ContextGet(EXGLContextId id) {
-  std::lock_guard lock(manager.contextLookupMutex);
+  std::unique_lock lock(manager.contextLookupMutex);
   auto iter = manager.contextMap.find(id);
   // if ctx is null then destroy is in progress
   if (iter == manager.contextMap.end() || iter->second.ctx == nullptr) {
     return {nullptr, std::shared_lock<std::shared_mutex>()};
   }
   return {iter->second.ctx, std::shared_lock(iter->second.mutex)};
+}
+
+void ContextMarkReady(EXGLContextId id) {
+  std::shared_lock lock(manager.contextLookupMutex);
+  auto iter = manager.contextMap.find(id);
+  // if ctx is null then destroy is in progress
+  if (iter == manager.contextMap.end() || iter->second.ctx == nullptr) {
+    return;
+  }
+  std::shared_lock contextLock(iter->second.mutex);
+  iter->second.isReady = true;
+  iter->second.cv.notify_one();
 }
 
 EXGLContextId ContextCreate() {
@@ -33,7 +47,7 @@ EXGLContextId ContextCreate() {
     return 0;
   }
 
-  std::lock_guard<std::mutex> lock(manager.contextLookupMutex);
+  std::unique_lock lock(manager.contextLookupMutex);
   EXGLContextId ctxId = manager.nextId++;
   if (manager.contextMap.find(ctxId) != manager.contextMap.end()) {
     EXGLSysLog("Tried to reuse an EXGLContext id. This shouldn't really happen...");
@@ -44,13 +58,31 @@ EXGLContextId ContextCreate() {
 }
 
 void ContextDestroy(EXGLContextId id) {
-  std::lock_guard lock(manager.contextLookupMutex);
-
-  auto iter = manager.contextMap.find(id);
-  if (iter != manager.contextMap.end()) {
+  // Wait for isReady to be set.
+  {
+    std::shared_lock lock(manager.contextLookupMutex);
+    auto iter = manager.contextMap.find(id);
+    if (iter == manager.contextMap.end()) {
+      EXGLSysLog("Unable to destory context. Context not found.");
+      return;
+    }
+    std::mutex localMutex;
+    std::unique_lock cv_lock(localMutex);
+    iter->second.cv.wait(cv_lock, [&] { return iter->second.isReady; });
+  }
+  // When we know that context is initialized, it's safe to use unique_lock
+  // without risk of a deadlock.
+  {
+    std::unique_lock lock(manager.contextLookupMutex);
+    auto iter = manager.contextMap.find(id);
+    if (iter == manager.contextMap.end()) {
+      EXGLSysLog("Unable to destory context. Context not found.");
+      return;
+    }
     {
       std::unique_lock lock(iter->second.mutex);
       delete iter->second.ctx;
+      iter->second.ctx = nullptr;
     }
     manager.contextMap.erase(iter);
   }
--- packages/expo-gl/common/EXGLContextManager.h
+++ packages/expo-gl/common/EXGLContextManager.h
@@ -9,6 +9,7 @@ namespace gl_cpp {
 using ContextWithLock = std::pair<EXGLContext *, std::shared_lock<std::shared_mutex>>;
 
 EXGLContextId ContextCreate();
+void ContextMarkReady(EXGLContextId id);
 ContextWithLock ContextGet(EXGLContextId id);
 void ContextDestroy(EXGLContextId id);
 
