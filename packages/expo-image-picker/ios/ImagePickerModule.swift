7941268acb678fa63eeb1abc1e0a158856ba859e
Chee Kit <cheekit.chong98@gmail.com>
2023-05-12 00:30:20 +0800

    [ios][image-picker] Add preferredAssetRepresentationMode to image picker options

--- packages/expo-image-picker/ios/ImagePickerModule.swift
+++ packages/expo-image-picker/ios/ImagePickerModule.swift
@@ -142,6 +142,7 @@ public class ImagePickerModule: Module, OnMediaPickingResultHandler {
     // selection limit = 1 --> single selection, reflects the old picker behavior
     configuration.selectionLimit = options.allowsMultipleSelection ? options.selectionLimit : SINGLE_SELECTION
     configuration.filter = options.mediaTypes.toPickerFilter()
+    configuration.preferredAssetRepresentationMode = options.preferredAssetRepresentationMode.toAssetRepresentationMode()
     if #available(iOS 15, *) {
       configuration.selection = options.orderedSelection ? .ordered : .default
     }
