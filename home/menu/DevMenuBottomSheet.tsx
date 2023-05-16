6a4238de13b2e2fdaf983f3d9599c4d410ed88c8
aleqsio <mikucki@gmail.com>
2023-05-16 14:01:06 +0200

    Use gorhom's bottom sheet for Expo Go

--- home/menu/DevMenuBottomSheet.tsx
+++ home/menu/DevMenuBottomSheet.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import {
-  Dimensions,
-  EventSubscription,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
-import Animated from 'react-native-reanimated';
-import BottomSheet from 'reanimated-bottom-sheet';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetView,
+  useBottomSheetDynamicSnapPoints,
+} from '@gorhom/bottom-sheet';
+import React, { useEffect, useMemo, useRef } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import DevMenuBottomSheetContext from './DevMenuBottomSheetContext';
 import * as DevMenu from './DevMenuModule';
 
 type Props = {
@@ -17,148 +13,62 @@ type Props = {
   children?: React.ReactNode;
 };
 
-class DevMenuBottomSheet extends React.PureComponent<Props, any> {
-  // We need to track whether the bottom sheet is expanded to prevent
-  // collapsing on some unnecessary `onCloseEnd` calls.
-  hasExpandingFinished: boolean = false;
-
-  ref = React.createRef<BottomSheet>();
-
-  snapPoints = [0, Math.max(BottomSheet.renumber('50%'), 600), '90%'];
-
-  callbackNode = new Animated.Value(0);
-
-  backgroundOpacity = this.callbackNode.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0.5, 0],
-  });
-
-  closeSubscription: EventSubscription | null = null;
-
-  componentDidMount() {
-    this.expand();
-
-    // Before the dev menu can be actually closed, we need to collapse its sheet view,
-    // and this listens for close requests that come from native side to start collapsing the view.
-    // The awaited return value of this listener is then send back as a response
-    // so the native module knows when it can fully close dev menu (detach its root view).
-    this.closeSubscription = DevMenu.listenForCloseRequests(() => {
-      // `collapse` returns a promise, so this `return` is important to finish the close event once the view is fully collapsed.
-      return this.collapse();
-    });
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    // Make sure it gets expanded once we receive new identifier.
-    if (prevProps.uuid !== this.props.uuid) {
-      this.expand();
-    }
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeCloseSubscription();
-  }
-
-  collapse = (): Promise<void> => {
-    this.hasExpandingFinished = false;
-    this.ref.current && this.ref.current.snapTo(0);
-
-    // Use setTimeout until there is a better solution to execute something once the sheet is fully collapsed.
-    return new Promise((resolve) => setTimeout(resolve, 300));
-  };
-
-  collapseAndClose = async () => {
-    await this.collapse();
+function DevMenuBottomSheet({ children, uuid }: Props) {
+  const bottomSheetRef = useRef<BottomSheet | null>(null);
+  const onClose = async () => {
     await DevMenu.closeAsync();
   };
+  const initialSnapPoints = useMemo(() => ['CONTENT_HEIGHT'], []);
 
-  expand = () => {
-    this.ref.current && this.ref.current.snapTo(1);
+  useEffect(() => {
+    bottomSheetRef.current?.expand();
+  }, [uuid]);
 
-    setTimeout(() => {
-      this.hasExpandingFinished = true;
-    }, 300);
-  };
+  useEffect(() => {
+    const closeSubscription = DevMenu.listenForCloseRequests(() => {
+      bottomSheetRef.current?.collapse();
+      return new Promise((resolve) => {
+        resolve(true);
+      });
+    });
+    return () => {
+      closeSubscription.remove();
+    };
+  }, []);
 
-  unsubscribeCloseSubscription = () => {
-    if (this.closeSubscription) {
-      this.closeSubscription.remove();
-      this.closeSubscription = null;
-    }
-  };
+  const { animatedHandleHeight, animatedSnapPoints, animatedContentHeight, handleContentLayout } =
+    useBottomSheetDynamicSnapPoints(initialSnapPoints);
 
-  onCloseEnd = () => {
-    if (this.hasExpandingFinished) {
-      this.collapseAndClose();
-    }
-  };
-
-  providedContext = {
-    expand: this.expand,
-    collapse: this.collapse,
-  };
-
-  renderHeader = () => {
-    return (
-      <View style={styles.bottomSheetHeader}>
-        <View style={styles.bottomSheetHeaderBar} />
-      </View>
-    );
-  };
-
-  renderContent = () => {
-    return <View style={styles.bottomSheetContent}>{this.props.children}</View>;
-  };
-
-  render() {
-    return (
-      <DevMenuBottomSheetContext.Provider value={this.providedContext}>
-        <View style={styles.bottomSheetContainer}>
-          <TouchableWithoutFeedback onPress={this.collapseAndClose}>
-            <Animated.View
-              style={[styles.bottomSheetBackground, { opacity: this.backgroundOpacity }]}
-            />
-          </TouchableWithoutFeedback>
-          <BottomSheet
-            ref={this.ref}
-            initialSnap={0}
-            enabledInnerScrolling={false}
-            overdragResistanceFactor={1.5}
-            snapPoints={this.snapPoints}
-            callbackNode={this.callbackNode}
-            renderHeader={this.renderHeader}
-            renderContent={this.renderContent}
-            onCloseEnd={this.onCloseEnd}
-          />
-        </View>
-      </DevMenuBottomSheetContext.Provider>
-    );
-  }
+  return (
+    <BottomSheet
+      key={uuid}
+      ref={bottomSheetRef}
+      backdropComponent={(props) => (
+        <BottomSheetBackdrop {...props} opacity={0.5} appearsOnIndex={0} disappearsOnIndex={-1} />
+      )}
+      handleComponent={null}
+      snapPoints={animatedSnapPoints}
+      handleHeight={animatedHandleHeight}
+      contentHeight={animatedContentHeight}
+      backgroundStyle={styles.bottomSheetBackground}
+      enablePanDownToClose
+      onClose={onClose}>
+      <BottomSheetView style={styles.contentContainerStyle} onLayout={handleContentLayout}>
+        {children}
+      </BottomSheetView>
+      {/* Adds bottom offset so that no empty space is shown on overdrag */}
+      <View style={{ height: 100 }} />
+    </BottomSheet>
+  );
 }
 
 const styles = StyleSheet.create({
   bottomSheetContainer: {
     flex: 1,
   },
+  contentContainerStyle: {},
   bottomSheetBackground: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
-  bottomSheetHeader: {
-    alignItems: 'center',
-    paddingTop: 10,
-    paddingBottom: 6,
-  },
-  bottomSheetHeaderBar: {
-    flex: 1,
-    width: 40,
-    height: 5,
-    backgroundColor: '#fff',
-    borderRadius: 40,
-    opacity: 0.8,
-  },
-  bottomSheetContent: {
-    height: Dimensions.get('window').height,
+    backgroundColor: '#f8f8fa',
   },
 });
 
