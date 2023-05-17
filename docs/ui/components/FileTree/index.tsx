ec5880dde8a962b6f2a585fd137eec4b0674d804
Simek <gosimek@gmail.com>
2023-05-17 10:14:45 +0200

    type fix + small cleanup

--- docs/ui/components/FileTree/index.tsx
+++ docs/ui/components/FileTree/index.tsx
@@ -19,11 +19,9 @@ export function FileTree({ files = [], ...rest }: FileTreeProps) {
 
 function generateStructure(files: string[]): FileObject {
   const structure = {};
-  files.forEach(path => {
-    path.split('/').reduce(function (acc: any, key) {
-      return acc[key] ?? (acc[key] = {});
-    }, structure);
-  });
+  files.forEach(path =>
+    path.split('/').reduce((acc: FileObject, key) => acc[key] ?? (acc[key] = {}), structure)
+  );
   return structure;
 }
 
@@ -31,19 +29,19 @@ function renderStructure(structure: FileObject, level = 0): ReactNode {
   return Object.entries(structure).map(([key, value]) => {
     return Object.keys(value).length ? (
       <div className="mt-1 pt-1 px-2 rounded-sm flex flex-col">
-        <code className="flex items-center">
+        <div className="flex items-center">
           {' '.repeat(level)}
           <FolderIcon className="text-icon-tertiary mr-2 opacity-60" />
-          <span className="text-secondary">{key}</span>
-        </code>
+          <code className="text-secondary">{key}</code>
+        </div>
         {renderStructure(value, level + 1)}
       </div>
     ) : (
-      <code className="mt-1 pl-3 pt-1 px-2 rounded-sm flex items-center">
+      <div className="mt-1 pl-3 pt-1 px-2 rounded-sm flex items-center">
         {' '.repeat(level - 1)}
         <FileCode01Icon className="text-icon-tertiary mr-2" />
-        <span className="text-default">{key}</span>
-      </code>
+        <code className="text-default">{key}</code>
+      </div>
     );
   });
 }
