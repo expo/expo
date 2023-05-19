9c70c950cef24fff6bf7ec94070213d2a9a3c480
Gabriel Donadel <donadeldev@gmail.com>
2023-05-19 16:59:29 -0300

    Simplify EXDevLauncherBridgeDelegate

--- packages/expo-dev-launcher/ios/EXDevLauncherBridgeDelegate.h
+++ packages/expo-dev-launcher/ios/EXDevLauncherBridgeDelegate.h
@@ -1,8 +1,7 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTBridgeDelegate.h>
+#import <React-RCTAppDelegate/RCTAppDelegate.h>
 #import <React/RCTRootView.h>
 
-@interface EXDevLauncherBridgeDelegate : NSObject <RCTBridgeDelegate>
+@interface EXDevLauncherBridgeDelegate : RCTAppDelegate
 
 - (RCTRootView *)createRootViewWithModuleName:(NSString *)moduleName
                                 launchOptions:(NSDictionary *_Nullable)launchOptions
