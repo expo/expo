951446d06a4ae57e98d6e10eac0653dfbd6a6050
Gabriel Donadel <donadeldev@gmail.com>
2023-05-19 16:59:29 -0300

    Simplify EXDevLauncherBridgeDelegate

--- packages/expo-dev-launcher/ios/EXDevLauncherBridgeDelegate.mm
+++ packages/expo-dev-launcher/ios/EXDevLauncherBridgeDelegate.mm
@@ -1,9 +1,8 @@
-#import <React/RCTRootView.h>
-
 #import "EXDevLauncherBridgeDelegate.h"
-
 #import "EXDevLauncherController.h"
-#import "EXDevLauncherRCTBridge.h"
+
+#import <React/RCTBundleURLProvider.h>
+#import "React/RCTAppSetupUtils.h"
 
 #if RCT_NEW_ARCH_ENABLED
 #import <memory>
@@ -25,13 +24,9 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 #endif
 
-#import "React/RCTAppSetupUtils.h"
-
-@interface EXDevLauncherBridgeDelegate () <RCTTurboModuleManagerDelegate, RCTCxxBridgeDelegate, RCTBridgeDelegate> {
-  RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
+@interface EXDevLauncherBridgeDelegate () <RCTTurboModuleManagerDelegate, RCTCxxBridgeDelegate> {
   std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
   facebook::react::ContextContainer::Shared _contextContainer;
-  RCTTurboModuleManager *turboModuleManager;
 }
 @end
 
@@ -41,71 +36,39 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   return [[EXDevLauncherController sharedInstance] sourceURLForBridge:bridge];
 }
 
-- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge {
-  return [[EXDevLauncherController sharedInstance] extraModulesForBridge:bridge];
-}
-
-- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
-{
-  turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
-                                                                 delegate:self
-                                                                jsInvoker:bridge.jsCallInvoker];
-  return RCTAppSetupDefaultJsExecutorFactory(bridge, turboModuleManager);
-}
-
-- (Class)getModuleClassFromName:(const char *)name
-{
-  return RCTCoreModulesClassProvider(name);
-}
-
-
-- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
-{
-  return RCTAppSetupDefaultModuleFromClass(moduleClass);
-}
-
-- (BOOL)turboModuleEnabled
-{
-  return YES;
-}
-
-- (BOOL)fabricEnabled
-{
-  return YES;
-}
-
 - (RCTRootView *)createRootViewWithModuleName:(NSString *)moduleName launchOptions:(NSDictionary * _Nullable)launchOptions application:(UIApplication *)application{
     BOOL enableTM = NO;
-    #if RCT_NEW_ARCH_ENABLED
-        enableTM = YES;
-    #endif
+#if RCT_NEW_ARCH_ENABLED
+    enableTM = YES;
+#endif
 
     RCTAppSetupPrepareApp(application, enableTM);
 
-    RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+    if (!self.bridge) {
+        self.bridge = [self createBridgeWithDelegate:self launchOptions:launchOptions];
+    }
 
-    #if RCT_NEW_ARCH_ENABLED
-
+#if RCT_NEW_ARCH_ENABLED
     _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
     _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
     _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
-
-    _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge
-                                                                contextContainer:_contextContainer];
-    bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
-    #endif
+    self.bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:self.bridge
+                                                                 contextContainer:_contextContainer];
+    self.bridge.surfacePresenter = self.bridgeAdapter.surfacePresenter;
+#endif
 
     NSMutableDictionary *initProps = [NSMutableDictionary new];
-    #ifdef RCT_NEW_ARCH_ENABLED
-      initProps[kRNConcurrentRoot] = @YES;
-    #endif
+#ifdef RCT_NEW_ARCH_ENABLED
+    initProps[kRNConcurrentRoot] = @YES;
+#endif
 
-    BOOL enableFabric = NO;
-    #if RCT_NEW_ARCH_ENABLED
-        enableFabric = TRUE;
-    #endif
 
-    return RCTAppSetupDefaultRootView(bridge, moduleName, initProps, enableFabric);
+    return [super createRootViewWithBridge:self.bridge moduleName:moduleName initProps:initProps];
+}
+
+- (BOOL)concurrentRootEnabled
+{
+  return true;
 }
 
 @end
