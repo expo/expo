9311bddcc7eb3c27cfbfda6b583ce4a7db0b0bd5
Bartosz Kaszubowski <gosimek@gmail.com>
2023-05-20 15:01:55 +0200

    [docs] fix CodeBlocksTable appearance after latest header changes (#22571)

--- docs/ui/components/Snippet/SnippetHeader.tsx
+++ docs/ui/components/Snippet/SnippetHeader.tsx
@@ -15,22 +15,20 @@ export const SnippetHeader = ({
   Icon,
   alwaysDark = false,
 }: SnippetHeaderProps) => (
-  <div className={mergeClasses(alwaysDark && 'dark-theme')}>
-    <div
+  <div
+    className={mergeClasses(
+      'flex pl-4 overflow-hidden justify-between bg-default border border-default rounded-t-md border-b-0 min-h-[40px]',
+      Icon && 'pl-3',
+      alwaysDark && 'dark-theme pr-2 dark:border-transparent !bg-palette-gray3'
+    )}>
+    <LABEL
       className={mergeClasses(
-        'flex pl-4 overflow-hidden justify-between bg-default border border-default rounded-t-md border-b-0',
-        Icon && 'pl-3',
-        alwaysDark && 'pr-2 dark:border-transparent !bg-palette-gray3'
+        'flex items-center gap-2 h-10 !leading-10 pr-4 select-none font-medium text-ellipsis whitespace-nowrap overflow-hidden',
+        alwaysDark && 'text-palette-white'
       )}>
-      <LABEL
-        className={mergeClasses(
-          'flex items-center gap-2 h-10 !leading-10 pr-4 select-none font-medium text-ellipsis whitespace-nowrap overflow-hidden',
-          alwaysDark && 'text-palette-white'
-        )}>
-        {Icon && <Icon className="icon-sm" />}
-        {title}
-      </LABEL>
-      {!!children && <div className="flex justify-end items-center">{children}</div>}
-    </div>
+      {Icon && <Icon className="icon-sm" />}
+      {title}
+    </LABEL>
+    {!!children && <div className="flex justify-end items-center">{children}</div>}
   </div>
 );
