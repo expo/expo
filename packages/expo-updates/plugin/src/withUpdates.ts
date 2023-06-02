b07614f69b594f0670e789a77db42da850962e2a
Will Schurman <wschurman@gmail.com>
2023-06-02 14:51:16 -0400

    [@expo/config-plugins] Add comment to withUpdates to attempt to preempt bugs

--- packages/expo-updates/plugin/src/withUpdates.ts
+++ packages/expo-updates/plugin/src/withUpdates.ts
@@ -3,6 +3,8 @@ import { ConfigPlugin, createRunOncePlugin, AndroidConfig, IOSConfig } from 'exp
 
 const pkg = require('expo-updates/package.json');
 
+// when making changes to this config plugin, ensure the same changes are also made in eas-cli and build-tools
+
 const withUpdates: ConfigPlugin<{ expoUsername?: string } | void> = (config, props = {}) => {
   // The username will be passed from the CLI when the plugin is automatically used.
   const expoUsername = (props || {}).expoUsername ?? getAccountUsername(config);
