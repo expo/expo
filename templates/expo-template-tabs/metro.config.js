78851c2fd71b7af3bc81ece839f55c35d81c12c7
evanbacon <bacon@expo.io>
2023-06-02 18:59:38 -0500

    Create metro.config.js

---
+++ templates/expo-template-tabs/metro.config.js
@@ -0,0 +1,10 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
+
+module.exports = config;
