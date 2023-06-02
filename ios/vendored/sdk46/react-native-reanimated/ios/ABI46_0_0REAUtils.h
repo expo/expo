6bf9bde06c16a7d3d5c9bf878e03a17dcf735a8d
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2023-06-02 16:24:50 +0200

    [ios] Remove SDK 46 on iOS

--- ios/vendored/sdk46/react-native-reanimated/ios/ABI46_0_0REAUtils.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#define ABI46_0_0REA_LOG_ERROR_IF_NIL(value, errorMsg) \
-  ({                                          \
-    if (value == nil)                         \
-      ABI46_0_0RCTLogError(errorMsg);                  \
-  })
