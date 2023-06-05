724954dbb8b4b25dd0854c2364c0eb897170042e
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2023-06-05 14:29:37 +0200

    [gl] use jsi::Runtime from worklet only from UI thread (#22634)

--- packages/expo-gl/android/src/main/java/expo/modules/gl/cpp/EXGL.java
+++ packages/expo-gl/android/src/main/java/expo/modules/gl/cpp/EXGL.java
@@ -8,12 +8,8 @@ public class EXGL {
     SoLoader.loadLibrary("expo-gl");
   }
   public static native int EXGLContextCreate();
-  public static native void EXGLContextPrepare(
-          long jsCtxPtr,
-          int exglCtxId,
-          Object glContext,
-          boolean enableExperimentalWorkletSupport
-  );
+  public static native void EXGLContextPrepare(long jsCtxPtr, int exglCtxId, Object glContext);
+  public static native void EXGLContextPrepareWorklet(int exglCtxId);
 
   public static native void EXGLContextDestroy(int exglCtxId);
   public static native void EXGLContextFlush(int exglCtxId);
@@ -22,7 +18,6 @@ public class EXGL {
   public static native void EXGLContextDestroyObject(int exglCtxId, int exglObjId);
   public static native void EXGLContextMapObject(int exglCtxId, int exglObjId, int glObj);
   public static native int EXGLContextGetObject(int exglCtxId, int exglObjId);
-  public static native void EXGLRegisterThread();
   public static native boolean EXGLContextNeedsRedraw(int exglCtxId);
   public static native void EXGLContextDrawEnded(int exglCtxId);
 }
