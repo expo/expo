685150614ff3e58d403758a3b93dacfba9269b10
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-06-05 09:47:28 +0200

    [ios] Remove SDK 46 on iOS (#22731)

--- ios/vendored/sdk46/react-native-safe-area-context/ios/ABI46_0_0RNCSafeAreaViewManager.h
+++
@@ -1,9 +0,0 @@
-#import <ABI46_0_0React/ABI46_0_0RCTViewManager.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface ABI46_0_0RNCSafeAreaViewManager : ABI46_0_0RCTViewManager
-
-@end
-
-NS_ASSUME_NONNULL_END
