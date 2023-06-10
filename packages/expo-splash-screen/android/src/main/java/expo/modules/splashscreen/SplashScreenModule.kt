d162be1ec10531c91725336cae9871fe951537fb
Alan Hughes <hughesno1@gmail.com>
2023-06-10 14:28:46 +0100

    [android][splash-screen] Migrate to modules API

--- packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/SplashScreenModule.kt
+++ packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/SplashScreenModule.kt
@@ -1,13 +1,11 @@
 package expo.modules.splashscreen
 
-import android.content.Context
-
-import expo.modules.core.ExportedModule
-import expo.modules.core.ModuleRegistry
-import expo.modules.core.Promise
-import expo.modules.core.errors.CurrentActivityNotFoundException
-import expo.modules.core.interfaces.ActivityProvider
-import expo.modules.core.interfaces.ExpoMethod
+import expo.modules.kotlin.Promise
+import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.splashscreen.exceptions.HideAsyncException
+import expo.modules.splashscreen.exceptions.PreventAutoHideException
 
 // Below import must be kept unversioned even in versioned code to provide a redirection from
 // versioned code realm to unversioned code realm.
@@ -15,47 +13,30 @@ import expo.modules.core.interfaces.ExpoMethod
 // in versioned SplashScreen kotlin object that stores no information about the ExperienceActivity.
 import expo.modules.splashscreen.singletons.SplashScreen
 
-class SplashScreenModule(context: Context) : ExportedModule(context) {
-  companion object {
-    private const val NAME = "ExpoSplashScreen"
-    private const val ERROR_TAG = "ERR_SPLASH_SCREEN"
-  }
+class SplashScreenModule : Module() {
+  override fun definition() = ModuleDefinition {
+    Name("ExpoSplashScreen")
 
-  private lateinit var activityProvider: ActivityProvider
+    AsyncFunction("preventAutoHideAsync") { promise: Promise ->
+      val currentActivity
+        = appContext.currentActivity ?: throw Exceptions.MissingActivity()
 
-  override fun getName(): String {
-    return NAME
-  }
-
-  override fun onCreate(moduleRegistry: ModuleRegistry) {
-    activityProvider = moduleRegistry.getModule(ActivityProvider::class.java)
-  }
-
-  @ExpoMethod
-  fun preventAutoHideAsync(promise: Promise) {
-    val activity = activityProvider.currentActivity
-    if (activity == null) {
-      promise.reject(CurrentActivityNotFoundException())
-      return
+      SplashScreen.preventAutoHide(
+        currentActivity,
+        { hasEffect -> promise.resolve(hasEffect) },
+        { m -> promise.reject(PreventAutoHideException(m)) }
+      )
     }
-    SplashScreen.preventAutoHide(
-      activity,
-      { hasEffect -> promise.resolve(hasEffect) },
-      { m -> promise.reject(ERROR_TAG, m) }
-    )
-  }
 
-  @ExpoMethod
-  fun hideAsync(promise: Promise) {
-    val activity = activityProvider.currentActivity
-    if (activity == null) {
-      promise.reject(CurrentActivityNotFoundException())
-      return
+    AsyncFunction("hideAsync") { promise: Promise ->
+      val currentActivity
+        = appContext.currentActivity ?: throw Exceptions.MissingActivity()
+
+      SplashScreen.hide(
+        currentActivity,
+        { hasEffect -> promise.resolve(hasEffect) },
+        { m -> promise.reject(HideAsyncException(m)) }
+      )
     }
-    SplashScreen.hide(
-      activity,
-      { hasEffect -> promise.resolve(hasEffect) },
-      { m -> promise.reject(ERROR_TAG, m) }
-    )
   }
 }
