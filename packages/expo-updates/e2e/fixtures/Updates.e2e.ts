b0145e6eddf0da64d778893c1d8d810e66c41a9b
Douglas Lowder <douglowder@mac.com>
2023-06-10 12:22:56 -0700

    Reenable UpdateEvent E2E test

--- packages/expo-updates/e2e/fixtures/Updates.e2e.ts
+++ packages/expo-updates/e2e/fixtures/Updates.e2e.ts
@@ -490,7 +490,7 @@ describe('JS API tests', () => {
     jestExpect(isEmbeddedAfterUpdate).toEqual('false');
   });
 
-  xit('Receives expected events when update available on start', async () => {
+  it('Receives expected events when update available on start', async () => {
     jest.setTimeout(300000 * TIMEOUT_BIAS);
     const bundleFilename = 'bundle1.js';
     const newNotifyString = 'test-update-1';
