14e2d7beda849146f945be27b110c0eb2edca9be
Evan Bacon <bacon@expo.io>
2023-06-13 14:18:21 -0500

    fix(cli, metro-config): fix tests and types (#22867)

--- packages/@expo/metro-config/build/serializer/withExpoSerializers.d.ts
+++ packages/@expo/metro-config/build/serializer/withExpoSerializers.d.ts
@@ -1,15 +1,9 @@
-/**
- * Copyright © 2022 650 Industries.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-import { ConfigT, InputConfigT } from 'metro-config';
+import { InputConfigT, SerializerConfigT } from 'metro-config';
 import { SerialAsset } from './serializerAssets';
-export type Serializer = NonNullable<ConfigT['serializer']['customSerializer']>;
+export type Serializer = NonNullable<SerializerConfigT['customSerializer']>;
 export type SerializerParameters = Parameters<Serializer>;
 export type SerializerPlugin = (...props: SerializerParameters) => SerializerParameters;
 export declare function withExpoSerializers(config: InputConfigT): InputConfigT;
 export declare function withSerializerPlugins(config: InputConfigT, processors: SerializerPlugin[]): InputConfigT;
-export declare function createSerializerFromSerialProcessors(processors: (SerializerPlugin | undefined)[], originalSerializer?: Serializer): Serializer;
+export declare function createSerializerFromSerialProcessors(processors: (SerializerPlugin | undefined)[], originalSerializer?: Serializer | null): Serializer;
 export { SerialAsset };
--- packages/@expo/metro-config/src/traveling/metro-config.ts
+++ packages/@expo/metro-config/src/traveling/metro-config.ts
@@ -3,7 +3,9 @@
 import resolveFrom from 'resolve-from';
 
 // ensure that Metro can bundle the project's assets (see: `watchFolders`).
-export function importMetroConfig(projectRoot: string): typeof import('metro-config') {
+export function importMetroConfig(projectRoot: string): typeof import('metro-config') & {
+  getDefaultConfig: import('metro-config/src/defaults/index').default;
+} {
   const modulePath = resolveFrom.silent(projectRoot, 'metro-config');
 
   if (!modulePath) {
