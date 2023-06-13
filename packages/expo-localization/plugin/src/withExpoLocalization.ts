054ab2bc7e4bf73a297bf3409cb0b8fb042fec11
aleqsio <mikucki@gmail.com>
2023-06-13 11:06:30 +0200

    Add config plugin settings

--- packages/expo-localization/plugin/src/withExpoLocalization.ts
+++ packages/expo-localization/plugin/src/withExpoLocalization.ts
@@ -2,56 +2,61 @@ import { getMainActivityOrThrow } from '@expo/config-plugins/build/android/Manif
 import { ExpoConfig } from '@expo/config-types';
 import {
   AndroidConfig,
-  ConfigPlugin,
-  createRunOncePlugin,
   withAndroidManifest,
-  withMainActivity,
   withPlugins,
   withStringsXml,
 } from 'expo/config-plugins';
 
-const pkg = require('expo-localization/package.json');
+type ConfigPluginProps = {
+  supportsRTL?: boolean;
+  allowDynamicLocaleChangesAndroid?: boolean;
+};
 
-const withExpoLocalizationIos: ConfigPlugin = (config) => {
+function withExpoLocalizationIos(config: ExpoConfig) {
   if (config.extra?.supportsRTL == null) return config;
   if (!config.ios) config.ios = {};
   if (!config.ios.infoPlist) config.ios.infoPlist = {};
   config.ios.infoPlist.ExpoLocalization_supportsRTL = config.extra?.supportsRTL || false;
   return config;
-};
+}
 
-const withExpoLocalizationAndroid: ConfigPlugin = (config) => {
-  config = withAndroidManifest(config, (config) => {
-    const mainActivity = getMainActivityOrThrow(config.modResults);
-    if (!mainActivity.$['android:configChanges']?.includes('locale')) {
-      mainActivity.$['android:configChanges'] += '|locale';
-    }
-    if (!mainActivity.$['android:configChanges']?.includes('layoutDirection')) {
-      mainActivity.$['android:configChanges'] += '|layoutDirection';
-    }
-    return config;
-  });
+function withExpoLocalizationAndroid(config: ExpoConfig, data: ConfigPluginProps) {
+  if (data.allowDynamicLocaleChangesAndroid) {
+    config = withAndroidManifest(config, (config) => {
+      const mainActivity = getMainActivityOrThrow(config.modResults);
+      if (!mainActivity.$['android:configChanges']?.includes('locale')) {
+        mainActivity.$['android:configChanges'] += '|locale';
+      }
+      if (!mainActivity.$['android:configChanges']?.includes('layoutDirection')) {
+        mainActivity.$['android:configChanges'] += '|layoutDirection';
+      }
+      return config;
+    });
+  }
   return withStringsXml(config, (config) => {
     config.modResults = AndroidConfig.Strings.setStringItem(
       [
-        // XML represented as JSON
-        // <string name="expo_custom_value" translatable="false">value</string>
         {
           $: { name: 'ExpoLocalization_supportsRTL', translatable: 'false' },
-          _: String(config.extra?.supportsRTL),
+          _: String(data.supportsRTL ?? config.extra?.supportsRTL),
         },
       ],
       config.modResults
     );
     return config;
   });
-};
+}
 
-// export default createRunOncePlugin(withExpoLocalization, pkg.name, pkg.version);
-
-const withExpoLocalization: ConfigPlugin = (config: ExpoConfig, data) =>
-  withPlugins(config, [
+function withExpoLocalization(
+  config: ExpoConfig,
+  data: ConfigPluginProps = {
+    allowDynamicLocaleChangesAndroid: true,
+  }
+) {
+  return withPlugins(config, [
     [withExpoLocalizationIos, data],
     [withExpoLocalizationAndroid, data],
   ]);
+}
+
 export default withExpoLocalization;
