4333aee6d6803e6537e666224da946a3e9244ad2
evanbacon <bacon@expo.io>
2023-06-15 17:26:26 -0500

    Update interactiveActions.ts

--- packages/@expo/cli/src/start/interface/interactiveActions.ts
+++ packages/@expo/cli/src/start/interface/interactiveActions.ts
@@ -46,7 +46,12 @@ export class DevServerManagerActions {
             printItem('Scan the QR code above with Expo Go (Android) or the Camera app (iOS)')
           );
         } else {
-          Log.log(printItem('Scan the QR code above to open the project in your development build. Learn more: https://expo.fyi/start'))
+          Log.log(
+            printItem(
+              'Scan the QR code above to open the project in your development build. ' +
+                learnMore('https://expo.fyi/start')
+            )
+          );
         }
       } catch (error) {
         console.log('err', error);
