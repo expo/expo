65e90fc2cd877b21e94157acac8d001350d69d5e
Gabriel Donadel <donadeldev@gmail.com>
2023-06-15 15:36:25 -0300

    Fix expo go import

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/datetimepicker/RNDateTimePickerPackage.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/datetimepicker/RNDateTimePickerPackage.java
@@ -15,6 +15,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import host.exp.expoview.BuildConfig;
+
 public class RNDateTimePickerPackage extends TurboReactPackage {
   @Nullable
   @Override
