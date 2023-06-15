04259613f0356cbf65ff66bac411a34e6a453581
Gabriel Donadel <donadeldev@gmail.com>
2023-06-15 10:23:39 -0300

    [android][ios] Upgrade @react-native-community/datetimepicker to 7.1.0

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/datetimepicker/RNDateTimePickerPackage.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/components/datetimepicker/RNDateTimePickerPackage.java
@@ -1,25 +1,71 @@
 package versioned.host.exp.exponent.modules.api.components.datetimepicker;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
-import com.facebook.react.ReactPackage;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.module.model.ReactModuleInfo;
+import com.facebook.react.module.model.ReactModuleInfoProvider;
 
-public class RNDateTimePickerPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(
-        new RNDatePickerDialogModule(reactContext),
-        new RNTimePickerDialogModule(reactContext)
-      );
-    }
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+public class RNDateTimePickerPackage extends TurboReactPackage {
+  @Nullable
+  @Override
+  public NativeModule getModule(String name, ReactApplicationContext reactContext) {
+    if (name.equals(DatePickerModule.NAME)) {
+      return new DatePickerModule(reactContext);
+    } else if (name.equals(TimePickerModule.NAME)) {
+      return new TimePickerModule(reactContext);
+    } else {
+      return null;
     }
+  }
+
+  @Override
+  public ReactModuleInfoProvider getReactModuleInfoProvider() {
+    return () -> {
+      boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+      final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+      moduleInfos.put(
+        DatePickerModule.NAME,
+        new ReactModuleInfo(
+          DatePickerModule.NAME,
+          DatePickerModule.NAME,
+          false, // canOverrideExistingModule
+          false, // needsEagerInit
+          false, // hasConstants
+          false, // isCxxModule
+          isTurboModule // isTurboModule
+        ));
+      moduleInfos.put(
+        TimePickerModule.NAME,
+        new ReactModuleInfo(
+          TimePickerModule.NAME,
+          TimePickerModule.NAME,
+          false, // canOverrideExistingModule
+          false, // needsEagerInit
+          false, // hasConstants
+          false, // isCxxModule
+          isTurboModule // isTurboModule
+        ));
+      return moduleInfos;
+    };
+  }
+
+  @NonNull
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+    modules.add(new DatePickerModule(reactContext));
+    modules.add(new TimePickerModule(reactContext));
+
+    return modules;
+  }
 }
