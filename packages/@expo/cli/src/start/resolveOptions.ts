5e86f5bf7d6584978b08c8d6e709aa604305c130
Kudo Chien <kudo@expo.dev>
2023-06-16 11:27:04 +0800

    Consolidate expo go port to respect RCT_METRO_PORT

--- packages/@expo/cli/src/start/resolveOptions.ts
+++ packages/@expo/cli/src/start/resolveOptions.ts
@@ -153,13 +153,12 @@ export async function resolvePortsAsync(
     }
     multiBundlerSettings.webpackPort = webpackPort;
   } else {
-    const devClientDefaultPort = process.env.RCT_METRO_PORT
+    const fallbackPort = process.env.RCT_METRO_PORT
       ? parseInt(process.env.RCT_METRO_PORT, 10)
       : 8081;
-    const expoGoDefaultPort = 8081;
     const metroPort = await resolvePortAsync(projectRoot, {
       defaultPort: options.port,
-      fallbackPort: options.devClient ? devClientDefaultPort : expoGoDefaultPort,
+      fallbackPort,
     });
     if (!metroPort) {
       throw new AbortCommandError();
