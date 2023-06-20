b39bfce87093fc12aaf24948b91785340524f1a2
Gabriel Donadel <donadeldev@gmail.com>
2023-06-20 19:19:34 -0300

    [go] Improve onboarding messaging

--- home/menu/DevMenuOnboarding.tsx
+++ home/menu/DevMenuOnboarding.tsx
@@ -1,60 +1,71 @@
-import { borderRadius } from '@expo/styleguide-native';
-import { View, Text, Button, useExpoTheme, Spacer } from 'expo-dev-client-components';
+import { View, Text, Button, Spacer } from 'expo-dev-client-components';
 import { isDevice } from 'expo-device';
 import React from 'react';
-import { Platform, StyleSheet, TouchableOpacity as TouchableOpacityRN } from 'react-native';
-import { TouchableOpacity as TouchableOpacityGH } from 'react-native-gesture-handler';
+import { Platform, StyleSheet } from 'react-native';
 
 type Props = {
   onClose: () => void;
 };
 
-// When rendered inside bottom sheet, touchables from RN don't work on Android, but the ones from GH don't work on iOS.
-const TouchableOpacity = Platform.OS === 'android' ? TouchableOpacityGH : TouchableOpacityRN;
+const deviceMessage = Platform.select({
+  ios: `You can shake your device or long press anywhere on the screen with three fingers to get back to it at any time.`,
+  android: `You can shake your device to get back to it at any time.`,
+});
 
-const KEYBOARD_CODES: { [key: string]: string } = {
-  ios: '^\u2318Z',
-  android: '\u2318M on macOS or Ctrl+M on other platforms',
-};
-
-const ONBOARDING_MESSAGE = (() => {
-  let fragment;
-  if (isDevice) {
-    if (Platform.OS === 'ios') {
-      fragment =
-        'you can shake your device or long press anywhere on the screen with three fingers';
-    } else {
-      fragment = 'you can shake your device';
-    }
-  } else {
-    fragment = `in a simulator you can press ${KEYBOARD_CODES[Platform.OS]}`;
-  }
-  return `Since this is your first time opening Expo Go, we wanted to show you this menu and let you know that ${fragment} to get back to it at any time.`;
-})();
+const simulatorMessage = Platform.select({
+  ios: (
+    <Text size="medium">
+      You can open it at any time with the <Text weight="bold">{'\u2318 + d '}</Text> keyboard
+      shortcut{' '}
+      <Text color="secondary" size="medium">
+        ("Connect Hardware Keyboard" must be enabled on your simulator to use this shortcut, you can
+        toggle it with{' '}
+        <Text weight="bold" color="secondary" size="medium">
+          {'\u2318 + shift + K'}
+        </Text>
+        ).
+      </Text>
+    </Text>
+  ),
+  android: (
+    <Text size="medium">
+      You can press{' '}
+      <Text size="medium" weight="bold">
+        {'\u2318 + m'}
+      </Text>{' '}
+      on macOS or{' '}
+      <Text size="medium" weight="bold">
+        Ctrl + m
+      </Text>{' '}
+      on other platforms to get back to it at any time.
+    </Text>
+  ),
+});
 
 export function DevMenuOnboarding({ onClose }: Props) {
-  const theme = useExpoTheme();
-
   return (
     <View style={styles.onboardingContainer}>
-      <View style={styles.onboardingBackground} bg="default" />
-      <View style={styles.onboardingTopMargin} />
-      <View>
-        <Text type="InterBold" align="center" size="large">
-          Hello there, friend! 👋
-        </Text>
-        <Spacer.Vertical size="small" />
-        <Text type="InterRegular" align="center" style={styles.onboardingTooltip}>
-          {ONBOARDING_MESSAGE}
-        </Text>
-        <Spacer.Vertical size="medium" />
-        <TouchableOpacity
-          style={[styles.onboardingButton, { backgroundColor: theme.button.primary.background }]}
-          onPress={onClose}>
-          <Button.Text type="InterSemiBold" color="primary">
-            Got it
-          </Button.Text>
-        </TouchableOpacity>
+      <View flex="1" bg="default" py="medium" px="large">
+        <View>
+          <Text size="medium" maxFontSizeMultiplier={1.2}>
+            This is the developer menu. It gives you access to useful tools in your development
+            builds.
+          </Text>
+          <Spacer.Vertical size="medium" />
+          <Text size="medium" maxFontSizeMultiplier={1.2}>
+            {isDevice ? deviceMessage : simulatorMessage}
+          </Text>
+        </View>
+
+        <Spacer.Vertical size="large" />
+
+        <Button.FadeOnPressContainer bg="primary" onPress={onClose}>
+          <View py="small">
+            <Button.Text align="center" size="medium" color="primary" weight="medium">
+              Continue
+            </Button.Text>
+          </View>
+        </Button.FadeOnPressContainer>
       </View>
     </View>
   );
@@ -63,37 +74,11 @@ export function DevMenuOnboarding({ onClose }: Props) {
 const styles = StyleSheet.create({
   onboardingContainer: {
     flex: 1,
-    paddingHorizontal: 36,
     position: 'absolute',
     top: 0,
     right: 0,
     bottom: 0,
     left: 0,
     zIndex: 2,
-  },
-  onboardingBackground: {
-    borderTopLeftRadius: borderRadius.large,
-    borderTopRightRadius: borderRadius.large,
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    opacity: 0.9,
-  },
-  onboardingTopMargin: {
-    // Moves the actual onboarding content a little bit down.
-    // This percentage value is also a percentage of screen's height.
-    height: '20%',
-  },
-  onboardingTooltip: {
-    lineHeight: 20,
-    textAlign: 'center',
-  },
-  onboardingButton: {
-    alignItems: 'center',
-    marginVertical: 12,
-    paddingVertical: 12,
-    borderRadius: 4,
   },
 });
