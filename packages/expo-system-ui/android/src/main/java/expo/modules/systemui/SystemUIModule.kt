59dd90d15c0c3c9b851ca3f66bc439b4e46abc3e
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2023-06-21 23:16:08 +0100

    [modules][system-ui] Persist user selected background color (#22773)
    # Why
    Closes ENG-8736
    
    # How
    Setting the background color is now persisted and restored when the app
    is launched (on iOS). On Android, a new function had to be added to
    restore the color on app start. If no color is provided the default will
    be black when in dark mode or white otherwise.
    
    # Test Plan
    Tested on physical devices and emulators with bare expo.

--- packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIModule.kt
+++ packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIModule.kt
@@ -1,23 +1,53 @@
 package expo.modules.systemui
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatDelegate
 import expo.modules.kotlin.exception.Exceptions
 import expo.modules.kotlin.functions.Queues
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
+const val PREFERENCE_KEY = "expoRootBackgroundColor"
+
 class SystemUIModule : Module() {
   private val currentActivity
     get() = appContext.currentActivity ?: throw Exceptions.MissingActivity()
+  private val context: Context
+    get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
+  private val prefs: SharedPreferences
+    get() = context.getSharedPreferences("expo_ui_preferences", Context.MODE_PRIVATE)
+      ?: throw Exceptions.ReactContextLost()
+
+  private val systemBackgroundColor
+    get() = when (AppCompatDelegate.getDefaultNightMode()) {
+      AppCompatDelegate.MODE_NIGHT_YES -> Color.BLACK
+      AppCompatDelegate.MODE_NIGHT_NO -> Color.WHITE
+      AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> {
+        when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+          Configuration.UI_MODE_NIGHT_YES -> Color.BLACK
+          Configuration.UI_MODE_NIGHT_NO -> Color.WHITE
+          else -> Color.WHITE
+        }
+      }
+      else -> Color.WHITE
+    }
 
   override fun definition() = ModuleDefinition {
     Name("ExpoSystemUI")
 
-    AsyncFunction("setBackgroundColorAsync") { color: Int ->
-      val rootView = currentActivity.window.decorView
-      val colorInt = Color.parseColor(colorToHex(color))
-      rootView.setBackgroundColor(colorInt)
+    AsyncFunction("setBackgroundColorAsync") { color: Int? ->
+      color?.let {
+        prefs.edit()
+          .putInt(PREFERENCE_KEY, it)
+          .apply()
+      } ?: prefs.edit()
+        .remove(PREFERENCE_KEY)
+        .apply()
+      setBackgroundColor(color ?: systemBackgroundColor)
     }.runOnQueue(Queues.MAIN)
 
     AsyncFunction("getBackgroundColorAsync") {
@@ -28,6 +58,12 @@ class SystemUIModule : Module() {
         null
       }
     }
+  }
+
+  private fun setBackgroundColor(color: Int) {
+    val rootView = currentActivity.window?.decorView
+    val colorInt = Color.parseColor(colorToHex(color))
+    rootView?.setBackgroundColor(colorInt)
   }
 
   companion object {
