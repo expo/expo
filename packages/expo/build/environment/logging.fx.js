629f7838255a5ee8d4bd524f5c59c779ba2dc20b
Brent Vatne <brentvatne@gmail.com>
2023-06-22 10:59:40 -0700

    [expo] Migrate internal usage of Constants.manifest to Constants.expoGoConfig (#23019)
    # Why
    
    Otherwise we see a deprecation warning when opening a new app
    
    # How
    
    I believe the equivalent in this case of `manifest` is now called
    `expoGoConfig`
    
    # Test Plan
    
    I changed this in `node_modules` directly and there was no longer a
    warning and I could see logs as expected.

--- packages/expo/build/environment/logging.fx.js
+++ packages/expo/build/environment/logging.fx.js
@@ -18,8 +18,8 @@ if (__DEV__) {
             // The standard Expo logUrl is `http://localhost:8081/logs`, this code assumes that the `logs` endpoint doesn't change.
             const logUrl = url + 'logs';
             Constants.__unsafeNoWarnManifest.logUrl = logUrl;
-            if (Constants.manifest) {
-                Constants.manifest.logUrl = logUrl;
+            if (Constants.expoGoConfig) {
+                Constants.expoGoConfig.logUrl = logUrl;
             }
         }
     }
