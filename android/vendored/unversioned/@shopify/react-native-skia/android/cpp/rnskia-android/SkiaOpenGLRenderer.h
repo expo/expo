d2d1c436a8ab24fce9ae620516cd12cd6d51679f
Kudo Chien <kudo@expo.dev>
2023-06-22 22:54:59 +0800

    [go] update @shopify/react-native-skia to 0.1.195 (#23036)
    # Why
    
    update @shopify/react-native-skia vendoring module for sdk 49
    
    # How
    
    - `et uvm -m @shopify/react-native-skia -c 0.1.195`
    
    # Test Plan
    
    - unversioned expo go + unversioned ncl skia

--- android/vendored/unversioned/@shopify/react-native-skia/android/cpp/rnskia-android/SkiaOpenGLRenderer.h
+++ android/vendored/unversioned/@shopify/react-native-skia/android/cpp/rnskia-android/SkiaOpenGLRenderer.h
@@ -60,7 +60,7 @@ public:
    * @param width Width of surface to render if there is a picture
    * @param height Height of surface to render if there is a picture
    */
-  void run(const std::function<void(SkCanvas *)> &cb, int width, int height);
+  bool run(const std::function<void(SkCanvas *)> &cb, int width, int height);
 
   /**
    * Sets the state to finishing. Next time the renderer will be called it
@@ -103,15 +103,6 @@ private:
   bool initGLSurface();
 
   /**
-   * Ensures that we have a valid Skia surface to draw to. The surface will
-   * be recreated if the width/height change.
-   * @param width Width of the underlying view
-   * @param height Height of the underlying view
-   * @return True if initialization went well
-   */
-  bool ensureSkiaSurface(int width, int height);
-
-  /**
    * To be able to use static contexts (and avoid reloading the skia context for
    * each new view, we track the OpenGL and Skia drawing context per thread.
    * @return The drawing context for the current thread
@@ -121,8 +112,6 @@ private:
   EGLSurface _glSurface = EGL_NO_SURFACE;
 
   ANativeWindow *_nativeWindow = nullptr;
-  GrBackendRenderTarget _skRenderTarget;
-  sk_sp<SkSurface> _skSurface;
 
   int _prevWidth = 0;
   int _prevHeight = 0;
