023541a658b918ca4991dcf82dabc30229a67b93
Brent Vatne <brentvatne@gmail.com>
2023-06-21 17:03:20 -0700

    [expo-dev-client-components] Remove built files

--- packages/expo-dev-client-components/build/icons/HomeFilledIcon.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Image } from '../Image';
-type TabBarIconProps = Partial<React.ComponentProps<typeof Image>> & {
-    focused?: boolean;
-};
-export declare function HomeFilledIcon(props: TabBarIconProps): JSX.Element;
-export {};
-//# sourceMappingURL=HomeFilledIcon.d.ts.map
--- packages/expo-dev-client-components/build/icons/InspectElementIcon.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { Image } from '../Image';
-const icon = require('../../assets/inspect-element-icon.png');
-export function InspectElementIcon(props) {
-    return React.createElement(Image, { source: icon, ...props });
-}
-//# sourceMappingURL=InspectElementIcon.js.map
