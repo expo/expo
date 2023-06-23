137d9a0d76e9278e63513f798008646870133e6d
Evan Bacon <bacon@expo.io>
2023-06-23 14:49:36 -0500

    tabs: disable async routes and bump (#23066)

--- templates/expo-template-tabs/app/+html.tsx
+++ templates/expo-template-tabs/app/+html.tsx
@@ -26,9 +26,21 @@ export default function Root({ children }: { children: React.ReactNode }) {
         */}
         <ScrollViewStyleReset />
 
+        {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
+        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
         {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
       <body>{children}</body>
     </html>
   );
 }
+
+const responsiveBackground = `
+body {
+  background-color: #fff;
+}
+@media (prefers-color-scheme: dark) {
+  body {
+    background-color: #000;
+  }
+}`;
