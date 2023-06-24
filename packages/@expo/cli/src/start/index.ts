expo/expo

commit 921ca1b4e5ffa233b31bdc2319449978940a0370

[expo-cli] Add platform when running --dev-client (#19319)
* [expo-cli] Add platform when running --dev-client

* updates test

* remove .only

* Use "none" for platform when no platform is provided

* add  changelog

* Update error message

* fix unit test

packages/@expo/cli/src/start/server/middleware/ExpoGoManifestHandlerMiddleware.ts
@@ -21,12 +21,10 @@ import { CommandError } from '../../../utils/errors';
 import { memoize } from '../../../utils/fn';
 import { stripPort } from '../../../utils/url';
 import { ManifestMiddleware, ManifestRequestInfo } from './ManifestMiddleware';
-import {
-  assertMissingRuntimePlatform,
-  assertRuntimePlatform,
-  parsePlatformHeader,
-} from './resolvePlatform';
+import { assertRuntimePlatform, parsePlatformHeader } from './resolvePlatform';
 import { ServerHeaders, ServerRequest } from './server.types';
+
+const debug = require('debug')('expo:start:server:middleware:ExpoGoManifestHandlerMiddleware');
 
 interface ExpoGoManifestRequestInfo extends ManifestRequestInfo {
   explicitlyPrefersMultipartMixed: boolean;
@@ -35,8 +33,15 @@ interface ExpoGoManifestRequestInfo extends ManifestRequestInfo {
 
 export class ExpoGoManifestHandlerMiddleware extends ManifestMiddleware<ExpoGoManifestRequestInfo> {
   public getParsedHeaders(req: ServerRequest): ExpoGoManifestRequestInfo {
-    const platform = parsePlatformHeader(req);
-    assertMissingRuntimePlatform(platform);
+    let platform = parsePlatformHeader(req);
+
+    if (!platform) {
+      debug(
+        `No "expo-platform" header or "platform" query parameter specified. Falling back to "none".`
+      );
+      platform = 'none';
+    }
+
     assertRuntimePlatform(platform);
 
     // Expo Updates clients explicitly accept "multipart/mixed" responses while browsers implicitly

packages/@expo/cli/src/start/server/middleware/resolvePlatform.ts
@@ -52,10 +52,10 @@ export function assertMissingRuntimePlatform(platform?: any): asserts platform {
 /** Assert if the runtime platform is not correct. */
 export function assertRuntimePlatform(platform: string): asserts platform is RuntimePlatform {
   const stringifiedPlatform = String(platform);
-  if (!['android', 'ios'].includes(stringifiedPlatform)) {
+  if (!['android', 'ios', 'none'].includes(stringifiedPlatform)) {
     throw new CommandError(
       'PLATFORM_HEADER',
-      `platform must be "android" or "ios". Received: "${platform}"`
+      `platform must be "android", "ios", or "none". Received: "${platform}"`
     );
   }
 }

packages/@expo/cli/src/start/server/middleware/__tests__/ExpoGoManifestHandlerMiddleware-test.ts
@@ -104,16 +104,21 @@ beforeEach(() => {
 describe('getParsedHeaders', () => {
   const middleware = new ExpoGoManifestHandlerMiddleware('/', {} as any);
 
-  // The classic manifest middleware did not assert.
-  it('asserts platform is missing', () => {
-    expect(() =>
+  it('defaults to "none" with no platform header', () => {
+    expect(
       middleware.getParsedHeaders(
         asReq({
           url: 'http://localhost:3000',
           headers: {},
         })
       )
-    ).toThrowError(/Must specify "expo-platform" header or "platform" query parameter/);
+    ).toEqual({
+      acceptSignature: false,
+      expectSignature: null,
+      explicitlyPrefersMultipartMixed: false,
+      hostname: null,
+      platform: 'none',
+    });
   });
 
   it('returns default values from headers', () => {

packages/@expo/cli/src/start/server/middleware/__tests__/resolvePlatform-test.ts
@@ -146,6 +146,6 @@ describe(assertRuntimePlatform, () => {
     }).not.toThrow();
     expect(() => {
       assertRuntimePlatform('not-supported');
-    }).toThrowError('platform must be "android" or "ios". Received: "not-supported"');
+    }).toThrowError('platform must be "android", "ios", or "none". Received: "not-supported"');
   });
 });

packages/@expo/cli/src/start/server/middleware/resolveEntryPoint.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 import { CommandError } from '../../../utils/errors';
 
-const supportedPlatforms = ['ios', 'android', 'web'];
+const supportedPlatforms = ['ios', 'android', 'web', 'none'];
 
 /** Returns the relative entry file for the project.  */
 export function resolveEntryPoint(

commit d7ad395fe77f9071c27b6baf7e6df4ce706663fc

fix(cli): remove invalid "none" platform (#23080)
# Why

- fix regression introduced in https://github.com/expo/expo/pull/19319
- `none` is passed to the bundler and implies the existence of
`.none.js` files, and `Platform.select({ none: '...' })` which isn't
valid.
- The default in classic and all other implementations of the metro dev
server has been to fallback on `ios`.
- The most correct variation would be to fallback on `web` as the web is
a non-standard React Native client which doesn't implement the expo
manifest format. However, (metro) web isn't enabled in every project,
and if it were then the JSON manifest would be skipped in favor of the
HTML file.
- remove unneeded logs about offline and anonymous.
- add force-manifest-type options to help prompt.

<!--
Please describe the motivation for this PR, and link to relevant GitHub
issues, forums posts, or feature requests.
-->

# How

<!--
How did you build this feature or fix this bug and why?
-->

# Test Plan

<!--
Please describe how you tested this change and how a reviewer could
reproduce your test, especially if this PR does not include automated
tests! If possible, please also provide terminal output and/or
screenshots demonstrating your test/reproduction.
-->

# Checklist

<!--
Please check the appropriate items below if they apply to your diff.
This is required for changes to Expo modules.
-->

- [ ] Documentation is up to date to reflect these changes (eg:
https://docs.expo.dev and README.md).
- [ ] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
- [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).

---------

Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

packages/@expo/cli/src/start/index.ts
@@ -50,35 +50,35 @@ export const expoStart: Command = async (argv) => {
       `Start a local dev server for the app`,
       chalk`npx expo start {dim <dir>}`,
       [
-        chalk`<dir>                                  Directory of the Expo project. {dim Default: Current working directory}`,
-        `-a, --android                          Open on a connected Android device`,
-        `-i, --ios                              Open in an iOS simulator`,
-        `-w, --web                              Open in a web browser`,
+        chalk`<dir>                           Directory of the Expo project. {dim Default: Current working directory}`,
+        `-a, --android                   Open on a connected Android device`,
+        `-i, --ios                       Open in an iOS simulator`,
+        `-w, --web                       Open in a web browser`,
         ``,
-        chalk`-d, --dev-client                       Launch in a custom native app`,
-        chalk`-g, --go                               Launch in Expo Go`,
+        chalk`-d, --dev-client                Launch in a custom native app`,
+        chalk`-g, --go                        Launch in Expo Go`,
         ``,
-        `-c, --clear                            Clear the bundler cache`,
-        `--max-workers <number>                 Maximum number of tasks to allow Metro to spawn`,
-        `--no-dev                               Bundle in production mode`,
-        `--minify                               Minify JavaScript`,
+        `-c, --clear                     Clear the bundler cache`,
+        `--max-workers <number>          Maximum number of tasks to allow Metro to spawn`,
+        `--no-dev                        Bundle in production mode`,
+        `--minify                        Minify JavaScript`,
         ``,
-        chalk`-m, --host <mode>                      Dev server hosting type. {dim Default: lan}`,
-        chalk`                                       {bold lan}: Use the local network`,
-        chalk`                                       {bold tunnel}: Use any network by tunnel through ngrok`,
-        chalk`                                       {bold localhost}: Connect to the dev server over localhost`,
-        `--tunnel                               Same as --host tunnel`,
-        `--lan                                  Same as --host lan`,
-        `--localhost                            Same as --host localhost`,
+        chalk`-m, --host <string>             Dev server hosting type. {dim Default: lan}`,
+        chalk`                                {bold lan}: Use the local network`,
+        chalk`                                {bold tunnel}: Use any network by tunnel through ngrok`,
+        chalk`                                {bold localhost}: Connect to the dev server over localhost`,
+        `--tunnel                        Same as --host tunnel`,
+        `--lan                           Same as --host lan`,
+        `--localhost                     Same as --host localhost`,
         ``,
-        `--offline                              Skip network requests and use anonymous manifest signatures`,
-        `--https                                Start the dev server with https protocol`,
-        `--scheme <scheme>                      Custom URI protocol to use when launching an app`,
-        chalk`-p, --port <number>                    Port to start the dev server on (does not apply to web or tunnel). {dim Default: 8081}`,
+        `--offline                       Skip network requests and use anonymous manifest signatures`,
+        `--https                         Start the dev server with https protocol`,
+        `--scheme <scheme>               Custom URI protocol to use when launching an app`,
+        chalk`-p, --port <number>             Port to start the dev server on (does not apply to web or tunnel). {dim Default: 8081}`,
         ``,
-        `--force-manifest-type <manifest-type>  Override auto detection of manifest type`,
-        chalk`--private-key-path <path>              Path to private key for code signing. {dim Default: "private-key.pem" in the same directory as the certificate specified by the expo-updates configuration in app.json.}`,
-        `-h, --help                             Usage info`,
+        chalk`--force-manifest-type <string>  Override auto detection of manifest type. {dim Options: expo-updates, classic}`,
+        chalk`--private-key-path <path>       Path to private key for code signing. {dim Default: "private-key.pem" in the same directory as the certificate specified by the expo-updates configuration in app.json.}`,
+        `-h, --help                      Usage info`,
       ].join('\n')
     );
   }

packages/@expo/cli/src/start/server/middleware/ExpoGoManifestHandlerMiddleware.ts
@@ -7,14 +7,12 @@ import { serializeDictionary, Dictionary } from 'structured-headers';
 
 import UserSettings from '../../../api/user/UserSettings';
 import { ANONYMOUS_USERNAME } from '../../../api/user/user';
-import * as Log from '../../../log';
 import { logEventAsync } from '../../../utils/analytics/rudderstackClient';
 import {
   CodeSigningInfo,
   getCodeSigningInfoAsync,
   signManifestString,
 } from '../../../utils/codesigning';
-import { env } from '../../../utils/env';
 import { CommandError } from '../../../utils/errors';
 import { stripPort } from '../../../utils/url';
 import { ManifestMiddleware, ManifestRequestInfo } from './ManifestMiddleware';
@@ -41,9 +39,9 @@ export class ExpoGoManifestHandlerMiddleware extends ManifestMiddleware<ExpoGoMa
 
     if (!platform) {
       debug(
-        `No "expo-platform" header or "platform" query parameter specified. Falling back to "none".`
+        `No "expo-platform" header or "platform" query parameter specified. Falling back to "ios".`
       );
-      platform = 'none';
+      platform = 'ios';
     }
 
     assertRuntimePlatform(platform);
@@ -267,11 +265,11 @@ export class ExpoGoManifestHandlerMiddleware extends ManifestMiddleware<ExpoGoMa
       return scopeKeyFromCodeSigningInfo;
     }
 
-    Log.warn(
-      env.EXPO_OFFLINE
-        ? 'Using anonymous scope key in manifest for offline mode.'
-        : 'Using anonymous scope key in manifest.'
-    );
+    // Log.warn(
+    //   env.EXPO_OFFLINE
+    //     ? 'Using anonymous scope key in manifest for offline mode.'
+    //     : 'Using anonymous scope key in manifest.'
+    // );
     return await getAnonymousScopeKeyAsync(slug);
   }
 }

packages/@expo/cli/src/start/server/middleware/resolvePlatform.ts
@@ -52,10 +52,10 @@ export function assertMissingRuntimePlatform(platform?: any): asserts platform {
 /** Assert if the runtime platform is not correct. */
 export function assertRuntimePlatform(platform: string): asserts platform is RuntimePlatform {
   const stringifiedPlatform = String(platform);
-  if (!['android', 'ios', 'none'].includes(stringifiedPlatform)) {
+  if (!['android', 'ios', 'web'].includes(stringifiedPlatform)) {
     throw new CommandError(
       'PLATFORM_HEADER',
-      `platform must be "android", "ios", or "none". Received: "${platform}"`
+      `platform must be "android", "ios", or "web". Received: "${platform}"`
     );
   }
 }

packages/@expo/cli/e2e/__tests__/start-test.ts
@@ -59,35 +59,35 @@ it('runs `npx expo start --help`', async () => {
         $ npx expo start <dir>
 
       Options
-        <dir>                                  Directory of the Expo project. Default: Current working directory
-        -a, --android                          Open on a connected Android device
-        -i, --ios                              Open in an iOS simulator
-        -w, --web                              Open in a web browser
+        <dir>                           Directory of the Expo project. Default: Current working directory
+        -a, --android                   Open on a connected Android device
+        -i, --ios                       Open in an iOS simulator
+        -w, --web                       Open in a web browser
         
-        -d, --dev-client                       Launch in a custom native app
-        -g, --go                               Launch in Expo Go
+        -d, --dev-client                Launch in a custom native app
+        -g, --go                        Launch in Expo Go
         
-        -c, --clear                            Clear the bundler cache
-        --max-workers <number>                 Maximum number of tasks to allow Metro to spawn
-        --no-dev                               Bundle in production mode
-        --minify                               Minify JavaScript
+        -c, --clear                     Clear the bundler cache
+        --max-workers <number>          Maximum number of tasks to allow Metro to spawn
+        --no-dev                        Bundle in production mode
+        --minify                        Minify JavaScript
         
-        -m, --host <mode>                      Dev server hosting type. Default: lan
-                                               lan: Use the local network
-                                               tunnel: Use any network by tunnel through ngrok
-                                               localhost: Connect to the dev server over localhost
-        --tunnel                               Same as --host tunnel
-        --lan                                  Same as --host lan
-        --localhost                            Same as --host localhost
+        -m, --host <string>             Dev server hosting type. Default: lan
+                                        lan: Use the local network
+                                        tunnel: Use any network by tunnel through ngrok
+                                        localhost: Connect to the dev server over localhost
+        --tunnel                        Same as --host tunnel
+        --lan                           Same as --host lan
+        --localhost                     Same as --host localhost
         
-        --offline                              Skip network requests and use anonymous manifest signatures
-        --https                                Start the dev server with https protocol
-        --scheme <scheme>                      Custom URI protocol to use when launching an app
-        -p, --port <number>                    Port to start the dev server on (does not apply to web or tunnel). Default: 8081
+        --offline                       Skip network requests and use anonymous manifest signatures
+        --https                         Start the dev server with https protocol
+        --scheme <scheme>               Custom URI protocol to use when launching an app
+        -p, --port <number>             Port to start the dev server on (does not apply to web or tunnel). Default: 8081
         
-        --force-manifest-type <manifest-type>  Override auto detection of manifest type
-        --private-key-path <path>              Path to private key for code signing. Default: "private-key.pem" in the same directory as the certificate specified by the expo-updates configuration in app.json.
-        -h, --help                             Usage info
+        --force-manifest-type <string>  Override auto detection of manifest type. Options: expo-updates, classic
+        --private-key-path <path>       Path to private key for code signing. Default: "private-key.pem" in the same directory as the certificate specified by the expo-updates configuration in app.json.
+        -h, --help                      Usage info
     "
   `);
 });

packages/@expo/cli/src/start/server/middleware/__tests__/ExpoGoManifestHandlerMiddleware-test.ts
@@ -107,7 +107,7 @@ describe('getParsedHeaders', () => {
   });
   const middleware = new ExpoGoManifestHandlerMiddleware('/', {} as any);
 
-  it('defaults to "none" with no platform header', () => {
+  it('defaults to "ios" with no platform header', () => {
     expect(
       middleware.getParsedHeaders(
         asReq({
@@ -119,7 +119,7 @@ describe('getParsedHeaders', () => {
       expectSignature: null,
       responseContentType: ResponseContentType.TEXT_PLAIN,
       hostname: null,
-      platform: 'none',
+      platform: 'ios',
     });
   });
 

packages/@expo/cli/src/start/server/middleware/__tests__/resolvePlatform-test.ts
@@ -146,6 +146,6 @@ describe(assertRuntimePlatform, () => {
     }).not.toThrow();
     expect(() => {
       assertRuntimePlatform('not-supported');
-    }).toThrowError('platform must be "android", "ios", or "none". Received: "not-supported"');
+    }).toThrowError('platform must be "android", "ios", or "web". Received: "not-supported"');
   });
 });