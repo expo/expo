6124774ad56cb3001ecc0b2934912028a47df858
Brent Vatne <brentvatne@gmail.com>
2023-06-26 19:52:22 -0700

    [docs] Cut SDK 49 beta docs

---
+++ docs/public/static/examples/v49.0.0/tutorial/02-image-picker/ImageViewer.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Image } from 'react-native';
+
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+}
+
+const styles = StyleSheet.create({  
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
