3d9e08a5db64fcac064b8442394b5f7bdb6ff394
Aman Mittal <amandeepmittal@live.com>
2023-06-28 16:50:33 +0530

    Update unversioned and SDK 49 docs

--- packages/expo-auth-session/src/providers/Facebook.ts
+++ packages/expo-auth-session/src/providers/Facebook.ts
@@ -28,6 +28,9 @@ export const discovery: DiscoveryDocument = {
 };
 
 // @needsAudit @docsMissing
+/**
+ * @deprecated See [Facebook authentication](/guides/facebook-authentication/).
+ */
 export interface FacebookAuthRequestConfig extends ProviderAuthRequestConfig {
   /**
    * Expo web client ID for use in the browser.
