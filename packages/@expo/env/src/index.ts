78a6243a9f84d7b0585912c852f2f4b6239fa73e
Evan Bacon <bacon@expo.io>
2023-06-29 12:45:11 -0700

    feat(metro-config, env): add ability to disable environment variables (#23169)
    # Why
    
    Just in case! You can now disable all dotenv loading in Expo CLI with
    `EXPO_NO_DOTENV`
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    Co-authored-by: James Ide <ide@users.noreply.github.com>

--- packages/@expo/env/src/index.ts
+++ packages/@expo/env/src/index.ts
@@ -4,8 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { createControlledEnvironment, getFiles } from './env';
+import { createControlledEnvironment, getFiles, isEnabled } from './env';
 
 const { get, load } = createControlledEnvironment();
 
-export { getFiles, get, load };
+export { getFiles, get, load, isEnabled };
