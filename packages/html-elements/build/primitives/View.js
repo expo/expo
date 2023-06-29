31bcf1759d2b11c06ef51debb6f771c076ee65ef
Gabriel Donadel <donadeldev@gmail.com>
2023-06-29 14:35:19 -0300

    [html-elements] Update tsconfig

--- packages/html-elements/build/primitives/View.js
+++ packages/html-elements/build/primitives/View.js
@@ -2,7 +2,6 @@ import { View as NativeView, } from 'react-native';
 import { createSafeStyledView } from '../css/createSafeStyledView';
 import { createDevView } from './createDevView';
 let View = NativeView;
-// @ts-expect-error: Cannot find name 'process'
 if (process.env.NODE_ENV !== 'production') {
     // Add better errors and warnings in development builds.
     View = createDevView(NativeView);
