f34aa33ba86538509959def198b416a01309bd06
Kudo Chien <kudo@expo.dev>
2023-07-01 01:24:33 +0800

    [dev-menu] fix wrong js import after expo modules api migration (#23219)
    # Why
    
    since we've migrated expo-dev-menu to expo modules api. we should use the new way to get native module from js side.
    fixes #23183
    close ENG-9166
    
    # How
    
    use `requireNativeModule()` to get native module
    
    # Test Plan
    
    ```sh
    $ yarn create expo -t blank@sdk-49 sdk49
    $ cd sdk49
    $ npx expo install expo-dev-client
    $ npx expo prebuild -i ios
    # patch expo-dev-menu's code
    ```

--- packages/expo-dev-menu/src/ExpoDevMenu.ts
+++ packages/expo-dev-menu/src/ExpoDevMenu.ts
@@ -1,9 +1,3 @@
 import { requireNativeModule } from 'expo-modules-core';
-import { NativeModules, Platform } from 'react-native';
 
-import { ExpoDevMenu } from './ExpoDevMenu.types';
-
-const module =
-  Platform.OS === 'android' ? requireNativeModule('ExpoDevMenu') : NativeModules.ExpoDevMenu;
-
-export default module as ExpoDevMenu;
+export default requireNativeModule('ExpoDevMenu');
