1c635e28f38e12dcd257cc7de0eb7597e0ded34a
Jon Samp <sampjon@gmail.com>
2023-06-30 00:01:16 -0500

    Adds diagram component, updates deploy patterns doc

---
+++ docs/ui/components/Diagram/Diagram.tsx
@@ -0,0 +1,39 @@
+import { useTheme } from '@expo/styleguide';
+
+type Props = {
+  source: string;
+  darkSource?: string;
+};
+
+export const Diagram = ({ source, darkSource }: Props) => {
+  const { themeName } = useTheme();
+  const isDark = themeName === 'dark';
+
+  if (!source.match(/\.png$/)) {
+    return (
+      <div className="border border-default rounded-md overflow-hidden my-6 max-w-[750px] m-auto">
+        <picture>
+          {isDark && darkSource && <source srcSet={darkSource} />}
+          <img src={source} />
+        </picture>
+      </div>
+    );
+  }
+
+  return (
+    <div className="border border-default rounded-md overflow-hidden my-6 max-w-[750px] m-auto">
+      <picture>
+        <source srcSet={source.replace('.png', '.avif')} type="image/avif" />
+        {darkSource && isDark && (
+          <source srcSet={darkSource.replace('.png', '.avif')} type="image/avif" />
+        )}
+        <source srcSet={source.replace('.png', '.webp')} type="image/webp" />
+        {darkSource && isDark && (
+          <source srcSet={darkSource.replace('.png', '.webp')} type="image/webp" />
+        )}
+        {darkSource && isDark && <source srcSet={darkSource} />}
+        <img src={source} />
+      </picture>
+    </div>
+  );
+};
