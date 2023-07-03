96e565e76e92eda9a5ea60d3271bbae010d1b50e
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-07-03 14:37:36 +0200

    [iOS] Refactor expo-screen-orientation for versioning (#23228)

--- packages/expo-screen-orientation/ios/ScreenOrientationAppDelegate.swift
+++ packages/expo-screen-orientation/ios/ScreenOrientationAppDelegate.swift
@@ -2,6 +2,7 @@
 
 import ExpoModulesCore
 
+@objc(EXScreenOrientationAppDelegate)
 public class ScreenOrientationAppDelegate: ExpoAppDelegateSubscriber {
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
     ScreenOrientationRegistry.shared.updateCurrentScreenOrientation()
