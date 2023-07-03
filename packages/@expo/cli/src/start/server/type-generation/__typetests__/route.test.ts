b31f747d15f9059615e4f2150d3a415e9aa0a39e
Mark Lawlor <mwlawlor@gmail.com>
2023-07-04 07:57:09 +1000

    feat: add generated types for Expo Router useSegment (#22629)
    # Why
    
    * Add type generation for Expo Router's `useSegment`
    * Adds code comments and renames a few internal types.
    * Refactored the `useSearchParams` types
    * Add default to `Href<T>`
    
    Note that this is not a breaking change as the generated types are only
    present inside the users' cache and should not be committed. Next time
    the user starts the dev server they will get the new generated types.
    
    ## Questions
    
    @EvanBacon Given the route `/(app)/note/[note]/` does useSegments return
    the group?
    
    ## `useSearchParams`
    
    This hook was a bit strange as its signature changed when you enabled
    types routes.
    
    Without: `useSearchParams<T extends Record<string, string>(): T`
    With: `useSearchParams<T extends Route>(): SearchParams<T>`
    
    This PR updates the TypedRoutes version to accept either a `Route` or a
    `Record`. This `Record` is still typed checked and it must satisfy a
    possible SearchParams object.
    
    For example
    
    ```tsx
    // This will only work if you have a [test] route somewhere in your app
    const { test } = useSearchParams<{ test: string }>()
    ```
    
    I quite like this as it allows you to cherry-pick the param that you
    need.
    
    Note that this obviously doesn't do a runtime check for it maybe be
    correct, but its a start.
    
    ## Href<T>
    
    Href<T> has a default value and by default now represents all possible
    Hrefs. You can use the generic to reduce it down.
    
    ---------
    
    Co-authored-by: evanbacon <bacon@expo.io>

---
+++ packages/@expo/cli/src/start/server/type-generation/__typetests__/route.test.ts
@@ -0,0 +1,171 @@
+import { expectType, expectError } from 'tsd-lite';
+
+import { useGlobalSearchParams, useSegments, useRouter, useSearchParams } from './fixtures/basic';
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const router = useRouter();
+
+describe('router.push()', () => {
+  // router.push will return void when the type matches, otherwise it should error
+
+  describe('href', () => {
+    it('will error on non-urls', () => {
+      expectError(router.push('should-error'));
+    });
+
+    it('can accept an absolute url', () => {
+      expectType<void>(router.push('/apple'));
+      expectType<void>(router.push('/banana'));
+    });
+
+    it('can accept a ANY relative url', () => {
+      // We only type-check absolute urls
+      expectType<void>(router.push('./this/work/but/is/not/valid'));
+    });
+
+    it('works for dynamic urls', () => {
+      expectType<void>(router.push('/colors/blue'));
+    });
+
+    it('works for CatchAll routes', () => {
+      expectType<void>(router.push('/animals/bear'));
+      expectType<void>(router.push('/animals/bear/cat/dog'));
+      expectType<void>(router.push('/mix/apple/blue/cat/dog'));
+    });
+
+    it.skip('works for optional CatchAll routes', () => {
+      // CatchAll routes are not currently optional
+      // expectType<void>(router.push('/animals/'));
+    });
+
+    it('will error when providing extra parameters', () => {
+      expectError(router.push('/colors/blue/test'));
+    });
+
+    it('will error when providing too few parameters', () => {
+      expectError(router.push('/mix/apple'));
+      expectError(router.push('/mix/apple/cat'));
+    });
+
+    it('can accept any external url', () => {
+      expectType<void>(router.push('http://expo.dev'));
+    });
+
+    it('throws if using inline search params', () => {
+      // While this is allowed in Expo Router, the Typed Routes version throws as we cannot type it
+      expectError(router.push('/colors?color=blue'));
+      expectError(router.push('/animals/bear?color=blue'));
+    });
+  });
+
+  describe('HrefObject', () => {
+    it('will error on non-urls', () => {
+      expectError(router.push({ pathname: 'should-error' }));
+    });
+
+    it('can accept an absolute url', () => {
+      expectType<void>(router.push({ pathname: '/apple' }));
+      expectType<void>(router.push({ pathname: '/banana' }));
+    });
+
+    it('can accept a ANY relative url', () => {
+      // We only type-check absolute urls
+      expectType<void>(router.push({ pathname: './this/work/but/is/not/valid' }));
+    });
+
+    it('works for dynamic urls', () => {
+      expectType<void>(
+        router.push({
+          pathname: '/colors/[color]',
+          params: { color: 'blue' },
+        })
+      );
+    });
+
+    it('requires a valid pathname', () => {
+      expectError(
+        router.push({
+          pathname: '/colors/[invalid]',
+          params: { color: 'blue' },
+        })
+      );
+    });
+
+    it('requires a valid param', () => {
+      expectError(
+        router.push({
+          pathname: '/colors/[color]',
+          params: { invalid: 'blue' },
+        })
+      );
+    });
+
+    it('works for catch all routes', () => {
+      expectType<void>(
+        router.push({
+          pathname: '/animals/[...animal]',
+          params: { animal: ['cat', 'dog'] },
+        })
+      );
+    });
+
+    it('requires an array for catch all routes', () => {
+      expectError(
+        router.push({
+          pathname: '/animals/[...animal]',
+          params: { animal: 'cat' },
+        })
+      );
+    });
+
+    it('works for mixed routes', () => {
+      expectType<void>(
+        router.push({
+          pathname: '/mix/[fruit]/[color]/[...animals]',
+          params: { color: 'red', fruit: 'apple', animals: ['cat', 'dog'] },
+        })
+      );
+    });
+
+    it('requires all params in mixed routes', () => {
+      expectError(
+        router.push({
+          pathname: '/mix/[fruit]/[color]/[...animals]',
+          params: { color: 'red', animals: ['cat', 'dog'] },
+        })
+      );
+    });
+  });
+});
+
+describe('useSearchParams', () => {
+  expectType<Record<'color', string>>(useSearchParams<'/colors/[color]'>());
+  expectType<Record<'color', string>>(useSearchParams<Record<'color', string>>());
+  expectError(useSearchParams<'/invalid'>());
+  expectError(useSearchParams<Record<'custom', string>>());
+});
+
+describe('useGlobalSearchParams', () => {
+  expectType<Record<'color', string>>(useGlobalSearchParams<'/colors/[color]'>());
+  expectType<Record<'color', string>>(useGlobalSearchParams<Record<'color', string>>());
+  expectError(useGlobalSearchParams<'/invalid'>());
+  expectError(useGlobalSearchParams<Record<'custom', string>>());
+});
+
+describe('useSegments', () => {
+  it('can accept an absolute url', () => {
+    expectType<['apple']>(useSegments<'/apple'>());
+  });
+
+  it('only accepts valid possible urls', () => {
+    expectError(useSegments<'/invalid'>());
+  });
+
+  it('can accept an array of segments', () => {
+    expectType<['apple']>(useSegments<['apple']>());
+  });
+
+  it('only accepts valid possible segments', () => {
+    expectError(useSegments<['invalid segment']>());
+  });
+});
