04792e556e632b91646a429946db360a23b930be
Suvin Nimnaka <suvin@wso2.com>
2023-07-05 16:29:33 +0530

    Change menu item order alphabetically

--- docs/ui/components/Authentication/index.ts
+++ docs/ui/components/Authentication/index.ts
@@ -6,6 +6,7 @@ const ASSETS_PATH = '/static/images/sdk/auth-session/';
 
 export const ASSETS = {
   apple: ASSETS_PATH + 'apple.png',
+  asgardeo: ASSETS_PATH + 'asgardeo.png',
   azure: ASSETS_PATH + 'azure.png',
   beyondidentity: ASSETS_PATH + 'beyondidentity.png',
   cognito: ASSETS_PATH + 'cognito.png',
@@ -26,5 +27,4 @@ export const ASSETS = {
   twitch: ASSETS_PATH + 'twitch.png',
   twitter: ASSETS_PATH + 'twitter.png',
   uber: ASSETS_PATH + 'uber.png',
-  asgardeo: ASSETS_PATH + 'asgardeo.png',
 } as const;
