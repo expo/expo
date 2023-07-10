expo/expo

commit ab93f90890ab512668d6371bc8cc2269accfa9a6

Rewrite bottom sheet (#22219)

home/menu/DevMenuBottomSheet.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import {
-  Dimensions,
-  EventSubscription,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
-import Animated from 'react-native-reanimated';
-import BottomSheet from 'reanimated-bottom-sheet';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetView,
+  useBottomSheetDynamicSnapPoints,
+} from '@gorhom/bottom-sheet';
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import DevMenuBottomSheetContext from './DevMenuBottomSheetContext';
 import * as DevMenu from './DevMenuModule';
@@ -17,148 +14,93 @@ type Props = {
   children?: React.ReactNode;
 };
 
-class DevMenuBottomSheet extends React.PureComponent<Props, any> {
-  // We need to track whether the bottom sheet is expanded to prevent
-  // collapsing on some unnecessary `onCloseEnd` calls.
-  hasExpandingFinished: boolean = false;
+function DevMenuBottomSheet({ children, uuid }: Props) {
+  const bottomSheetRef = useRef<BottomSheet | null>(null);
 
-  ref = React.createRef<BottomSheet>();
+  const onCollapse = useCallback(
+    () =>
+      new Promise<void>((resolve) => {
+        bottomSheetRef.current?.close();
 
-  snapPoints = [0, Math.max(BottomSheet.renumber('50%'), 600), '90%'];
+        // still no way to wait for animation to end before the callback, so we wait for 300ms
+        setTimeout(() => {
+          resolve();
+          DevMenu.closeAsync();
+        }, 300);
+      }),
+    []
+  );
 
-  callbackNode = new Animated.Value(0);
+  const onExpand = useCallback(
+    () =>
+      new Promise<void>((resolve) => {
+        bottomSheetRef.current?.expand();
+        setTimeout(() => {
+          resolve();
+        }, 300);
+      }),
+    []
+  );
 
-  backgroundOpacity = this.callbackNode.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0.5, 0],
-  });
+  const onChange = useCallback((index: number) => {
+    if (index === -1) {
+      DevMenu.closeAsync();
+    }
+  }, []);
 
-  closeSubscription: EventSubscription | null = null;
+  const initialSnapPoints = useMemo(() => ['CONTENT_HEIGHT'], []);
 
-  componentDidMount() {
-    this.expand();
+  useEffect(() => {
+    bottomSheetRef.current?.expand();
+  }, [uuid]);
 
-    // Before the dev menu can be actually closed, we need to collapse its sheet view,
-    // and this listens for close requests that come from native side to start collapsing the view.
-    // The awaited return value of this listener is then send back as a response
-    // so the native module knows when it can fully close dev menu (detach its root view).
-    this.closeSubscription = DevMenu.listenForCloseRequests(() => {
-      // `collapse` returns a promise, so this `return` is important to finish the close event once the view is fully collapsed.
-      return this.collapse();
+  useEffect(() => {
+    const closeSubscription = DevMenu.listenForCloseRequests(() => {
+      bottomSheetRef.current?.collapse();
+      return new Promise((resolve) => {
+        resolve(true);
+      });
     });
-  }
+    return () => {
+      closeSubscription.remove();
+    };
+  }, []);
 
-  componentDidUpdate(prevProps: Props) {
-    // Make sure it gets expanded once we receive new identifier.
-    if (prevProps.uuid !== this.props.uuid) {
-      this.expand();
-    }
-  }
+  const { animatedHandleHeight, animatedSnapPoints, animatedContentHeight, handleContentLayout } =
+    useBottomSheetDynamicSnapPoints(initialSnapPoints);
 
-  componentWillUnmount() {
-    this.unsubscribeCloseSubscription();
-  }
-
-  collapse = (): Promise<void> => {
-    this.hasExpandingFinished = false;
-    this.ref.current && this.ref.current.snapTo(0);
-
-    // Use setTimeout until there is a better solution to execute something once the sheet is fully collapsed.
-    return new Promise((resolve) => setTimeout(resolve, 300));
-  };
-
-  collapseAndClose = async () => {
-    await this.collapse();
-    await DevMenu.closeAsync();
-  };
-
-  expand = () => {
-    this.ref.current && this.ref.current.snapTo(1);
-
-    setTimeout(() => {
-      this.hasExpandingFinished = true;
-    }, 300);
-  };
-
-  unsubscribeCloseSubscription = () => {
-    if (this.closeSubscription) {
-      this.closeSubscription.remove();
-      this.closeSubscription = null;
-    }
-  };
-
-  onCloseEnd = () => {
-    if (this.hasExpandingFinished) {
-      this.collapseAndClose();
-    }
-  };
-
-  providedContext = {
-    expand: this.expand,
-    collapse: this.collapse,
-  };
-
-  renderHeader = () => {
-    return (
-      <View style={styles.bottomSheetHeader}>
-        <View style={styles.bottomSheetHeaderBar} />
-      </View>
-    );
-  };
-
-  renderContent = () => {
-    return <View style={styles.bottomSheetContent}>{this.props.children}</View>;
-  };
-
-  render() {
-    return (
-      <DevMenuBottomSheetContext.Provider value={this.providedContext}>
-        <View style={styles.bottomSheetContainer}>
-          <TouchableWithoutFeedback onPress={this.collapseAndClose}>
-            <Animated.View
-              style={[styles.bottomSheetBackground, { opacity: this.backgroundOpacity }]}
-            />
-          </TouchableWithoutFeedback>
-          <BottomSheet
-            ref={this.ref}
-            initialSnap={0}
-            enabledInnerScrolling={false}
-            overdragResistanceFactor={1.5}
-            snapPoints={this.snapPoints}
-            callbackNode={this.callbackNode}
-            renderHeader={this.renderHeader}
-            renderContent={this.renderContent}
-            onCloseEnd={this.onCloseEnd}
-          />
-        </View>
+  return (
+    <BottomSheet
+      key={uuid}
+      ref={bottomSheetRef}
+      backdropComponent={(props) => (
+        <BottomSheetBackdrop {...props} opacity={0.5} appearsOnIndex={0} disappearsOnIndex={-1} />
+      )}
+      handleComponent={null}
+      snapPoints={animatedSnapPoints}
+      handleHeight={animatedHandleHeight}
+      contentHeight={animatedContentHeight}
+      backgroundStyle={styles.bottomSheetBackground}
+      enablePanDownToClose
+      onChange={onChange}>
+      <DevMenuBottomSheetContext.Provider value={{ collapse: onCollapse, expand: onExpand }}>
+        <BottomSheetView style={styles.contentContainerStyle} onLayout={handleContentLayout}>
+          {children}
+        </BottomSheetView>
       </DevMenuBottomSheetContext.Provider>
-    );
-  }
+      {/* Adds bottom offset so that no empty space is shown on overdrag */}
+      <View style={{ height: 100 }} />
+    </BottomSheet>
+  );
 }
 
 const styles = StyleSheet.create({
   bottomSheetContainer: {
     flex: 1,
   },
+  contentContainerStyle: {},
   bottomSheetBackground: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
-  bottomSheetHeader: {
-    alignItems: 'center',
-    paddingTop: 10,
-    paddingBottom: 6,
-  },
-  bottomSheetHeaderBar: {
-    flex: 1,
-    width: 40,
-    height: 5,
-    backgroundColor: '#fff',
-    borderRadius: 40,
-    opacity: 0.8,
-  },
-  bottomSheetContent: {
-    height: Dimensions.get('window').height,
+    backgroundColor: '#f8f8fa',
   },
 });
 

home/menu/DevMenuBottomSheetContext.ts
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export type Context = {
-  expand: () => any;
-  collapse: () => any;
+  expand: () => Promise<void>;
+  collapse: () => Promise<void>;
 };
 
 const DevMenuBottomSheetContext = React.createContext<Context | null>(null);

commit 488f56d223db89187db136dafea6fdb009f435fa

[go] fix android crash when returning back to home (#23435)
# Why

fixes #23393
close ENG-9266

# How

the resolved promise result did matter on android because on native side, we will [convert the result to ReadableMap](https://github.com/expo/expo/blob/1d68edad21eea82cc9e30f5fbc0c2add9fdd9c06/android/expoview/src/main/java/host/exp/exponent/kernel/DevMenuManager.kt#L156). if we passing true, we will receive a java exception where boolean cannot convert to ReadableMap.

this is a regression from #22219

# Test Plan

test "go home" on expo-go

home/menu/DevMenuBottomSheet.tsx
@@ -58,7 +58,7 @@ function DevMenuBottomSheet({ children, uuid }: Props) {
     const closeSubscription = DevMenu.listenForCloseRequests(() => {
       bottomSheetRef.current?.collapse();
       return new Promise((resolve) => {
-        resolve(true);
+        resolve();
       });
     });
     return () => {