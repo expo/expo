411bb80cd2a7ea4e2a7086b739a91b928c5169ba
Kudo Chien <kudo@expo.dev>
2023-07-11 00:24:41 +0800

    [go] backport #23405 to sdk 49

--- ios/versioned/sdk49/ExpoModulesCore/ios/Swift/DevTools/ExpoRequestCdpInterceptor.swift
+++ ios/versioned/sdk49/ExpoModulesCore/ios/Swift/DevTools/ExpoRequestCdpInterceptor.swift
@@ -34,30 +34,42 @@ public final class ExpoRequestCdpInterceptor: NSObject, ExpoRequestInterceptorPr
 
   // MARK: ExpoRequestInterceptorProtocolDelegate implementations
 
-  func willSendRequest(requestId: String, request: URLRequest, redirectResponse: HTTPURLResponse?) {
+  func willSendRequest(requestId: String, task: URLSessionTask, request: URLRequest, redirectResponse: HTTPURLResponse?) {
     let now = Date().timeIntervalSince1970
 
-    let params = CdpNetwork.RequestWillBeSentParams(now: now, requestId: requestId, request: request, redirectResponse: redirectResponse)
+    let params = CdpNetwork.RequestWillBeSentParams(
+      now: now,
+      requestId: requestId,
+      request: request,
+      encodedDataLength: task.countOfBytesReceived,
+      redirectResponse: redirectResponse)
     dispatchEvent(CdpNetwork.Event(method: "Network.requestWillBeSent", params: params))
 
     let params2 = CdpNetwork.RequestWillBeSentExtraInfoParams(now: now, requestId: requestId, request: request)
     dispatchEvent(CdpNetwork.Event(method: "Network.requestWillBeSentExtraInfo", params: params2))
   }
 
-  func didReceiveResponse(requestId: String, request: URLRequest, response: HTTPURLResponse) {
+  func didReceiveResponse(requestId: String, task: URLSessionTask, responseBody: Data, isText: Bool, responseBodyExceedsLimit: Bool) {
+    guard let request = task.currentRequest, let response = task.response as? HTTPURLResponse else {
+      return
+    }
     let now = Date().timeIntervalSince1970
 
-    let params = CdpNetwork.ResponseReceivedParams(now: now, requestId: requestId, request: request, response: response)
+    let params = CdpNetwork.ResponseReceivedParams(
+      now: now,
+      requestId: requestId,
+      request: request,
+      response: response,
+      encodedDataLength: task.countOfBytesReceived)
     dispatchEvent(CdpNetwork.Event(method: "Network.responseReceived", params: params))
 
-    let params2 = CdpNetwork.LoadingFinishedParams(now: now, requestId: requestId, request: request, response: response)
-    dispatchEvent(CdpNetwork.Event(method: "Network.loadingFinished", params: params2))
-  }
+    if !responseBodyExceedsLimit {
+      let params2 = CdpNetwork.ExpoReceivedResponseBodyParams(now: now, requestId: requestId, responseBody: responseBody, isText: isText)
+      dispatchEvent(CdpNetwork.Event(method: "Expo(Network.receivedResponseBody)", params: params2))
+    }
 
-  func didReceiveResponseBody(requestId: String, responseBody: Data, isText: Bool) {
-    let now = Date().timeIntervalSince1970
-    let params = CdpNetwork.ExpoReceivedResponseBodyParams(now: now, requestId: requestId, responseBody: responseBody, isText: isText)
-    dispatchEvent(CdpNetwork.Event(method: "Expo(Network.receivedResponseBody)", params: params))
+    let params3 = CdpNetwork.LoadingFinishedParams(now: now, requestId: requestId, encodedDataLength: task.countOfBytesReceived)
+    dispatchEvent(CdpNetwork.Event(method: "Network.loadingFinished", params: params3))
   }
 }
 
