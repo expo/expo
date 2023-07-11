6b8a14246b71b15edb004525e2e1fa5e5a5d4116
Will Schurman <wschurman@gmail.com>
2023-07-11 15:57:45 -0700

    Fix last commit to match expected review comment outcome

--- packages/@expo/cli/src/api/endpoint.ts
+++ packages/@expo/cli/src/api/endpoint.ts
@@ -6,7 +6,7 @@ export function getExpoApiBaseUrl(): string {
   if (env.EXPO_STAGING) {
     return `https://staging-api.expo.dev`;
   } else if (env.EXPO_LOCAL) {
-    return `http://api.expo.test`;
+    return `http://127.0.0.1:3000`;
   } else {
     return `https://api.expo.dev`;
   }
@@ -17,7 +17,7 @@ export function getExpoWebsiteBaseUrl(): string {
   if (env.EXPO_STAGING) {
     return `https://staging.expo.dev`;
   } else if (env.EXPO_LOCAL) {
-    return `http://expo.test`;
+    return `http://127.0.0.1:3001`;
   } else {
     return `https://expo.dev`;
   }
