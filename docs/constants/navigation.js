8fe87ccfa5b77028aa37f97a1b902831e29ec131
Bartosz Kaszubowski <gosimek@gmail.com>
2023-07-12 17:56:25 +0200

    [docs] add icons to certain sidebar titles (#23452)

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -50,7 +50,7 @@ const generalDirectories = fs
 // --- Navigation ---
 
 const home = [
-  makeSection('', [makePage('overview.mdx')]),
+  makePage('overview.mdx'),
   makeSection('Get started', [
     makePage('get-started/installation.mdx'),
     makePage('get-started/expo-go.mdx'),
