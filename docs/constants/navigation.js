8abad23b3746f43e781bf0c6ffe00c524cc1d16d
Evan Bacon <bacon@expo.io>
2023-07-12 11:48:25 -0700

    feat(docs): add react navigation migration doc (#23243)
    # Why
    
    A new version of the migration section from the Expo Router docs. This
    is generally based on my experience of converting Bluesky to Expo
    Router.
    
    ---------
    
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -194,6 +194,7 @@ const general = [
       makePage('router/reference/troubleshooting.mdx'),
       makePage('router/reference/faq.mdx'),
     ]),
+    makeGroup('Migration', [makePage('router/migrate/from-react-navigation.mdx')]),
   ]),
   makeSection('EAS', [makePage('eas/index.mdx')]),
   makeSection('EAS Build', [
