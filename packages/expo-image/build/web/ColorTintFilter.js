0ebf748676eb45ba545f1b1dd6977b5b4ac6c914
aleqsio <mikucki@gmail.com>
2023-07-13 12:14:12 +0200

    Set svg size to 0 to prevent disappearing tinted images

--- packages/expo-image/build/web/ColorTintFilter.js
+++ packages/expo-image/build/web/ColorTintFilter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 export function getColorTintStyle(tintColor) {
     if (!tintColor)
         return {};
@@ -9,10 +10,16 @@ export function getColorTintStyle(tintColor) {
 export default function ColorTintFilter({ tintColor }) {
     if (!tintColor)
         return null;
-    return (React.createElement("svg", null,
+    return (React.createElement("svg", { style: styles.svg },
         React.createElement("defs", null,
             React.createElement("filter", { id: `tint-${tintColor}`, x: "0", y: "0", width: "0", height: "0" },
                 React.createElement("feFlood", { floodColor: tintColor, floodOpacity: "1", result: "flood" }),
                 React.createElement("feComposite", { in: "flood", in2: "SourceAlpha", operator: "in" })))));
 }
+const styles = StyleSheet.create({
+    svg: {
+        width: 0,
+        height: 0,
+    },
+});
 //# sourceMappingURL=ColorTintFilter.js.map
--- packages/expo-image/src/web/ColorTintFilter.tsx
+++ packages/expo-image/src/web/ColorTintFilter.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 export function getColorTintStyle(tintColor?: string | null) {
   if (!tintColor) return {};
@@ -10,7 +11,7 @@ export function getColorTintStyle(tintColor?: string | null) {
 export default function ColorTintFilter({ tintColor }: { tintColor?: string | null }) {
   if (!tintColor) return null;
   return (
-    <svg>
+    <svg style={styles.svg}>
       <defs>
         <filter id={`tint-${tintColor}`} x="0" y="0" width="0" height="0">
           <feFlood floodColor={tintColor} floodOpacity="1" result="flood" />
@@ -20,3 +21,10 @@ export default function ColorTintFilter({ tintColor }: { tintColor?: string | nu
     </svg>
   );
 }
+
+const styles = StyleSheet.create({
+  svg: {
+    width: 0,
+    height: 0,
+  },
+});
