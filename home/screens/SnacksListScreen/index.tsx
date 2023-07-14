6af0e71ee9c8e8dbeb6e027ed5cf9e27bb0cb394
James Ide <ide@expo.dev>
2023-07-14 15:14:12 -0700

    Fix lint in home

--- home/screens/SnacksListScreen/index.tsx
+++ home/screens/SnacksListScreen/index.tsx
@@ -1,8 +1,8 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
 
-import { HomeStackRoutes } from '../../navigation/Navigation.types';
 import { SnacksList } from './SnacksList';
+import { HomeStackRoutes } from '../../navigation/Navigation.types';
 
 export function SnacksListScreen({ route }: StackScreenProps<HomeStackRoutes, 'SnacksList'>) {
   const { accountName } = route.params ?? {};
