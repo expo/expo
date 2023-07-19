56319418b18cacd6e07ca147ba5f892878aaf9a8
Douglas Lowder <doug@expo.io>
2023-07-19 12:03:56 -0700

    [expo-manifests] Fix error in handling nested array (#23562)
    # Why
    
    Fix crash on iOS if a plugin is passed in as an array, but with only the
    name element and not the props element.
    
    Also fix corresponding code on Android, where an exception is thrown.
    
    Fixes #23549.
    
    # How
    
    Modified `Manifest.swift` and `Manifest.kt` to handle the above case
    correctly.
    
    # Test Plan
    
    - Tested with Expo Go and the project provided in the above issue
    - New unit tests for both iOS and Android
    
    # Checklist
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/expo-manifests/android/src/test/java/expo/modules/manifests/core/ManifestTest.kt
+++ packages/expo-manifests/android/src/test/java/expo/modules/manifests/core/ManifestTest.kt
@@ -9,28 +9,35 @@ import org.junit.Test
 class ManifestTest {
   @Test
   fun getPluginProperties_emptyManifest_returnsNull() {
-    val manifestJson = JSONObject(emptyMap<String, Any>())
+    val manifestJson = JSONObject("{}")
     val manifest = Manifest.fromManifestJson(manifestJson)
     Truth.assertThat(manifest.getPluginProperties("test")).isNull()
   }
 
   @Test
   fun getPluginProperties_emptyPlugins_returnsNull() {
-    val manifestJson = JSONObject(mapOf<String, Any>("plugins" to emptyArray<Any>()))
+    val manifestJson = JSONObject("{\"plugins\": []}")
     val manifest = Manifest.fromManifestJson(manifestJson)
     Truth.assertThat(manifest.getPluginProperties("test")).isNull()
   }
 
   @Test
   fun getPluginProperties_nonMatchedPlugins_returnsNull() {
-    val manifestJson = JSONObject(mapOf<String, Any>("plugins" to arrayOf("hello")))
+    val manifestJson = JSONObject("{\"plugins\": [\"hello\"]}")
     val manifest = Manifest.fromManifestJson(manifestJson)
     Truth.assertThat(manifest.getPluginProperties("test")).isNull()
   }
 
   @Test
   fun getPluginProperties_matchedPluginWithoutProps_returnsNull() {
-    val manifestJson = JSONObject(mapOf<String, Any>("plugins" to arrayOf("test")))
+    val manifestJson = JSONObject("{\"plugins\": [\"test\"]}")
+    val manifest = Manifest.fromManifestJson(manifestJson)
+    Truth.assertThat(manifest.getPluginProperties("test")).isNull()
+  }
+
+  @Test
+  fun getPluginProperties_matchedPluginWithoutPropsAsNestedArray_returnsNull() {
+    val manifestJson = JSONObject("{\"plugins\": [[\"test\"]]}")
     val manifest = Manifest.fromManifestJson(manifestJson)
     Truth.assertThat(manifest.getPluginProperties("test")).isNull()
   }
@@ -38,8 +45,7 @@ class ManifestTest {
   @Test
   fun getPluginProperties_matchedPluginWithProps_returnsProps() {
     val props = mapOf<String, Any>("foo" to "bar")
-    val pluginWithProp = arrayOf("test", props)
-    val manifestJson = JSONObject(mapOf<String, Any>("plugins" to arrayOf(pluginWithProp)))
+    val manifestJson = JSONObject("{\"plugins\": [ [\"test\", {\"foo\":\"bar\"}] ]}")
     val manifest = Manifest.fromManifestJson(manifestJson)
     val result = manifest.getPluginProperties("test")
     Truth.assertThat(result).isNotNull()
@@ -49,8 +55,7 @@ class ManifestTest {
   @Test
   fun getPluginProperties_matchedPluginWithNestedProps_returnsNestedProps() {
     val props = mapOf<String, Any>("nested" to mapOf<String, Any>("insideNested" to true))
-    val pluginWithProp = arrayOf("test", props)
-    val manifestJson = JSONObject(mapOf<String, Any>("plugins" to arrayOf(pluginWithProp)))
+    val manifestJson = JSONObject("{\"plugins\":[[\"test\",{\"nested\":{\"insideNested\":true}}]]}")
     val manifest = Manifest.fromManifestJson(manifestJson)
     val result = manifest.getPluginProperties("test")
     Truth.assertThat(result).isNotNull()
