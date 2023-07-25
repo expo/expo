c787240bf9d4b3382e53e81e783610fad54cbccc
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2023-07-25 12:41:33 +0200

    [image][Android] Fix the image components rendering incorrect assets when the Proguard is enabled (#23704)
    # Why
    
    Fixes https://github.com/expo/expo/issues/22515.
    
    # How
    
    It turns out our proguard file is omitted and also not all rules are correct. I've managed to fix that, but invoking `consumerProguardFiles` function in `build.gradle` and checking all the rules manually. 
    
    # Test Plan
    
    - bare-expo ✅
    - https://gist.github.com/buraks/ed0e69c0267d1c92c9bdb0b61d4e21e7 ✅

--- packages/expo-image/android/src/main/java/expo/modules/image/ImageViewWrapperTarget.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/ImageViewWrapperTarget.kt
@@ -73,7 +73,7 @@ class ImageViewWrapperTarget(
     val isPlaceholder = if (request is ThumbnailRequestCoordinator) {
       (request as? ThumbnailRequestCoordinator)
         ?.getPrivateFullRequest()
-        ?.isComplete != true
+        ?.isComplete == false
     } else {
       false
     }
--- packages/expo-image/android/src/main/java/expo/modules/image/ThumbnailRequestCoordinatorExtension.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/ThumbnailRequestCoordinatorExtension.kt
@@ -1,18 +1,21 @@
 package expo.modules.image
 
+import android.util.Log
 import com.bumptech.glide.request.Request
 import com.bumptech.glide.request.ThumbnailRequestCoordinator
 
 fun ThumbnailRequestCoordinator.getPrivateFullRequest(): Request? {
-  return getPrivateRequest("full")
+  return getPrivateFiled("full")
 }
 
-private fun ThumbnailRequestCoordinator.getPrivateRequest(name: String): Request? {
+private fun <T> ThumbnailRequestCoordinator.getPrivateFiled(name: String): T? {
   return try {
     val filed = this.javaClass.getDeclaredField(name)
     filed.isAccessible = true
-    filed.get(this) as Request
+    @Suppress("UNCHECKED_CAST")
+    filed.get(this) as T
   } catch (e: Throwable) {
+    Log.e("ExpoImage", "Couldn't receive the `$name` field", e)
     null
   }
 }
