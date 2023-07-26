47a99a8015e9eef20e93ae2530c5b8a37d7c205c
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-07-26 18:17:41 +0200

    [iOS] Add a native module for Expo Go (#23730)

--- ios/Exponent/Versioned/Core/Modules/ExpoGoModule.swift
+++ ios/Exponent/Versioned/Core/Modules/ExpoGoModule.swift
@@ -1,14 +1,28 @@
 // Copyright 2023-present 650 Industries. All rights reserved.
 
 import ExpoModulesCore
+import EXManifests
 
 final class ExpoGoModule: Module {
+  let manifest: Manifest
+
+  // swiftlint:disable:next unavailable_function
+  required init(appContext: AppContext) {
+    fatalError("Initializer not implemented, use init(appContext:params:manifest:) instead")
+  }
+
+  init(appContext: AppContext, manifest: Manifest) {
+    self.manifest = manifest
+    super.init(appContext: appContext)
+  }
+
   func definition() -> ModuleDefinition {
-    Name("ExpoGoModule")
+    Name("ExpoGo")
 
     Constants {
       return [
-        "isDetached": false
+        "expoVersion": Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion"),
+        "projectConfig": manifest.expoGoConfigRootObject()
       ]
     }
   }
