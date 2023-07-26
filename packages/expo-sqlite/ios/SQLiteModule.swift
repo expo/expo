5c9d7465f911bd47c03a4590a903cddd80183f26
Alan Hughes <hughesno1@gmail.com>
2023-07-26 11:30:20 +0100

    [sqlite] lint

--- packages/expo-sqlite/ios/SQLiteModule.swift
+++ packages/expo-sqlite/ios/SQLiteModule.swift
@@ -235,5 +235,4 @@ public final class SQLiteModule: Module {
     let message = NSString(utf8String: sqlite3_errmsg(db)) ?? ""
     return NSString(format: "Error code %i: %@", code, message) as String
   }
-
 }
