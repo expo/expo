caacaa53b1bd05e3dd363f1f9442f9d987dd3a92
Alan Hughes <hughesno1@gmail.com>
2023-07-26 11:14:20 +0100

    [sqlite] remove function on js

--- packages/expo-sqlite/build/SQLite.js
+++ packages/expo-sqlite/build/SQLite.js
@@ -54,6 +54,7 @@ export class SQLiteDatabase {
         return ExpoSQLite.close(this._name);
     }
     /**
+<<<<<<< HEAD
      * Synchronously closes the database.
      */
     closeSync() {
@@ -61,6 +62,16 @@ export class SQLiteDatabase {
         return ExpoSQLite.closeSync(this._name);
     }
     /**
+||||||| parent of 14729a19cd ([sqlite] remove function on js)
+     * Synchoronsly close the database.
+     */
+    closeSync() {
+        this._closed = true;
+        return ExpoSQLite.closeSync(this._name);
+    }
+    /**
+=======
+>>>>>>> 14729a19cd ([sqlite] remove function on js)
      * Delete the database file.
      * > The database has to be closed prior to deletion.
      */
@@ -154,14 +165,9 @@ export function openDatabase(name, version = '1.0', description = name, size = 1
     const db = _openExpoSQLiteDatabase(name, version, description, size, callback);
     db.exec = db._db.exec.bind(db._db);
     db.execAsync = db._db.execAsync.bind(db._db);
-    db.closeSync = db._db.closeSync.bind(db._db);
     db.closeAsync = db._db.closeAsync.bind(db._db);
-<<<<<<< HEAD
     db.closeSync = db._db.closeSync.bind(db._db);
-||||||| parent of 8ef50d6e1f (add sync param)
-=======
     db.onDatabaseChange = db._db.onDatabaseChange.bind(db._db);
->>>>>>> 8ef50d6e1f (add sync param)
     db.deleteAsync = db._db.deleteAsync.bind(db._db);
     db.transactionAsync = db._db.transactionAsync.bind(db._db);
     return db;
