c3ec7c6d1e76368c4ffa25daa4752d3e74f8b99a
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2023-07-26 08:14:00 +0200

    Add comment

--- packages/expo-dev-launcher/ios/EXRCTAppDelegateInterceptor.mm
+++ packages/expo-dev-launcher/ios/EXRCTAppDelegateInterceptor.mm
@@ -60,7 +60,7 @@
 - (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
 {
 #if __has_include(<RNReanimated/REAInitializer.h>) \
-  && __has_include(<RNReanimated/UIResponder+Reanimated.h>) \
+  && __has_include(<RNReanimated/UIResponder+Reanimated.h>) \ // Removed in react-native-reanimated@3.4.0
   && !RCT_NEW_ARCH_ENABLED
   reanimated::REAInitializer(bridge);
 #endif // __has_inclide(<RNReanimated/REAInitializer.h>) && !RCT_NEW_ARCH_ENABLED
