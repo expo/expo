2db8af96c1ba0b28e370476df7ba1184fc844b04
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2023-07-26 14:55:59 +0200

    [iOS] Add a native module for Expo Go

--- packages/expo/src/Expo.ts
+++ packages/expo/src/Expo.ts
@@ -5,3 +5,4 @@ import * as Logs from './logs/Logs';
 export { Logs };
 export { disableErrorHandling } from './errors/ExpoErrorManager';
 export { default as registerRootComponent } from './launch/registerRootComponent';
+export { isRunningInExpoGo, getExpoGoProjectConfig } from './environment/ExpoGo';
