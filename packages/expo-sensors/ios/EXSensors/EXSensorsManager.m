70b38a3d5f2891a5adfe8766c000baec15970239
Justin Kaufman <57186+jkaufman@users.noreply.github.com>
2023-07-27 11:18:40 -0600

    Correct build error
    - Fixed. CI is not a substitute for an IDE.

--- packages/expo-sensors/ios/EXSensors/EXSensorsManager.m
+++ packages/expo-sensors/ios/EXSensors/EXSensorsManager.m
@@ -304,7 +304,7 @@ EX_REGISTER_MODULE();
 {
   float interval = 1.0f / 60.0f;
   [[self manager] setDeviceMotionUpdateInterval:interval];
-  CMAttitudeReferenceFrame referenceFrame = ([CMMotionManager availableAttitudeReferenceFrames] & CMAttitudeReferenceFrameXMagneticNorthZVertical) ? CMAttitudeReferenceFrameXMagneticNorthZVertical : CMAttitudeReferenceFrameXArbitraryCorrectedZVertical
+  CMAttitudeReferenceFrame referenceFrame = ([CMMotionManager availableAttitudeReferenceFrames] & CMAttitudeReferenceFrameXMagneticNorthZVertical) ? CMAttitudeReferenceFrameXMagneticNorthZVertical : CMAttitudeReferenceFrameXArbitraryCorrectedZVertical;
   __weak EXSensorsManager *weakSelf = self;
   [[self manager] startDeviceMotionUpdatesUsingReferenceFrame:referenceFrame toQueue:[NSOperationQueue mainQueue] withHandler:^(CMDeviceMotion *data, NSError *error) {
     __strong EXSensorsManager *strongSelf = weakSelf;
