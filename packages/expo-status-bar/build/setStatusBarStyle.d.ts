9abe46db49241652578ed894bcdbb518344b242e
Siarhei Haikou <9068073+haikov@users.noreply.github.com>
2023-07-27 19:12:17 +0300

    [expo-status-bar] Support animated property for setStatusBarStyle (#23408)
    # Why
    
    When using `setStatusBarStyle` from `expo-status-bar` I noticed that
    there is no support for passing `animated` value which exists in [React
    Native API](https://reactnative.dev/docs/statusbar#setbarstyle). Making
    a quick PR to add the support for it.
    
    # How
    
    Added additional boolean property to `setStatusBarStyle` to match the
    API with the one from RN core.
    
    # Test Plan
    
    I added a new unit tests which verifies that proper value is passed down
    to RN version of `setStatusBarStyle`. In addition, I had to update the
    exiting test with `undefined` as a second parameter in case if the value
    for `animated` is not present.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/expo-status-bar/build/setStatusBarStyle.d.ts
+++ packages/expo-status-bar/build/setStatusBarStyle.d.ts
@@ -2,6 +2,7 @@ import { StatusBarStyle } from './StatusBar.types';
 /**
  * Set the bar style of the status bar.
  * @param style The color of the status bar text.
+ * @param animated If the transition should be animated.
  */
-export default function setStatusBarStyle(style: StatusBarStyle): void;
+export default function setStatusBarStyle(style: StatusBarStyle, animated?: boolean): void;
 //# sourceMappingURL=setStatusBarStyle.d.ts.map
