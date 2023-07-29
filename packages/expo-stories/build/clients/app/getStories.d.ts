b36885752795f062c40e749bd47139f1ec3386ce
Evan Bacon <bacon@expo.io>
2023-07-28 22:43:52 -0500

    delete expo-stories (#23760)
    # Why
    
    - `expo-stories` is unmaintained and unused.
    - The dependency on react-navigation is blocking the expo-router
    migration https://github.com/expo/expo/pull/23725 because there are no
    maintainers it's unclear how to safely upgrade react-navigation in
    stories.

--- packages/expo-stories/build/clients/app/getStories.d.ts
+++
@@ -1,4 +0,0 @@
-import { StoriesExport, File, Story } from './types';
-export declare function getByFileId(stories: StoriesExport): Record<string, File>;
-export declare function getByStoryId(stories: StoriesExport): Record<string, Story>;
-//# sourceMappingURL=getStories.d.ts.map
--- packages/expo-stories/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './Button';
-export * from './Container';
-export * from './Json';
-export * from './ListRow';
-// export * from './TestRunner';
-export * from './Toggle';
