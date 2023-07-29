d3a20edb5e741faef0992a34c0479db99b60c3b3
Evan Bacon <bacon@expo.io>
2023-07-29 12:54:23 -0500

    feat(metro): support mjs extensions in Expo (#23528)
    # Why
    
    - A number of libraries are using this file, unclear how we should use
    it though.
    - This PR doesn't enable `unstable_packageExports` but it does pull in
    the default `unstable_conditionNames` from the recommended react-native
    metro config.
    - mjs is resolved after js/ts files when bundling for Node.js
    environments.
    
    # Test Plan
    
    - Expo Camera works in Expo Router on web.
    - Added tests for continuous functionality.
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/cli/src/start/server/metro/__tests__/withMetroMultiPlatform.test.ts
+++ packages/@expo/cli/src/start/server/metro/__tests__/withMetroMultiPlatform.test.ts
@@ -1,4 +1,199 @@
-import { shouldAliasAssetRegistryForWeb } from '../withMetroMultiPlatform';
+import { getBareExtensions } from '@expo/config/paths';
+import { vol } from 'memfs';
+import { ConfigT } from 'metro-config';
+import { CustomResolutionContext } from 'metro-resolver/src';
+
+import { importMetroResolverFromProject } from '../resolveFromProject';
+import {
+  getNodejsExtensions,
+  shouldAliasAssetRegistryForWeb,
+  withExtendedResolver,
+} from '../withMetroMultiPlatform';
+
+const asMetroConfig = (config: Partial<ConfigT> = {}): ConfigT => config as any;
+
+jest.mock('../resolveFromProject', () => {
+  const resolve = jest.fn(() => ({ type: 'empty' }));
+  return {
+    importMetroResolverFromProject: jest.fn(() => ({ resolve })),
+  };
+});
+
+function getDefaultRequestContext(): CustomResolutionContext {
+  return {
+    extraNodeModules: {},
+    mainFields: ['react-native', 'browser', 'main'],
+    nodeModulesPaths: ['/node_modules'],
+    preferNativePlatform: true,
+    sourceExts: ['mjs', 'ts', 'tsx', 'js', 'jsx', 'json', 'css'],
+    customResolverOptions: {},
+    originModulePath: '/index.js',
+  } as any;
+}
+
+function getResolveFunc() {
+  return importMetroResolverFromProject('/').resolve;
+}
+
+describe(withExtendedResolver, () => {
+  function mockMinFs() {
+    vol.fromJSON(
+      {
+        'node_modules/react-native/Libraries/Image/AssetRegistry.js': '',
+      },
+      '/'
+    );
+  }
+  afterEach(() => {
+    vol.reset();
+  });
+
+  it(`resolves a file for web`, async () => {
+    mockMinFs();
+
+    const modified = withExtendedResolver(asMetroConfig(), {
+      projectRoot: '/',
+      platforms: ['ios', 'web'],
+      tsconfig: null,
+      isTsconfigPathsEnabled: false,
+    });
+
+    const platform = 'ios';
+
+    modified.resolver.resolveRequest!(getDefaultRequestContext(), 'react-native', platform);
+
+    expect(getResolveFunc()).toBeCalledTimes(1);
+    expect(getResolveFunc()).toBeCalledWith(
+      expect.objectContaining({
+        extraNodeModules: {},
+        mainFields: ['react-native', 'browser', 'main'],
+        nodeModulesPaths: ['/node_modules'],
+        preferNativePlatform: true,
+        sourceExts: ['mjs', 'ts', 'tsx', 'js', 'jsx', 'json', 'css'],
+        customResolverOptions: {},
+        originModulePath: expect.anything(),
+        getPackageMainPath: expect.any(Function),
+      }),
+      'react-native',
+      platform
+    );
+  });
+
+  it(`resolves against tsconfig baseUrl`, async () => {
+    mockMinFs();
+
+    const modified = withExtendedResolver(asMetroConfig(), {
+      projectRoot: '/',
+      platforms: ['ios', 'web'],
+      tsconfig: { baseUrl: '/src', paths: { '/*': ['*'] } },
+      isTsconfigPathsEnabled: true,
+    });
+
+    const platform = 'ios';
+
+    modified.resolver.resolveRequest!(getDefaultRequestContext(), 'react-native', platform);
+
+    expect(getResolveFunc()).toBeCalledTimes(1);
+    expect(getResolveFunc()).toBeCalledWith(
+      expect.objectContaining({
+        nodeModulesPaths: ['/node_modules', '/src'],
+        extraNodeModules: {},
+        mainFields: ['react-native', 'browser', 'main'],
+        preferNativePlatform: true,
+      }),
+      'react-native',
+      platform
+    );
+  });
+
+  it(`resolves to react-native-web on web`, async () => {
+    mockMinFs();
+
+    const modified = withExtendedResolver(asMetroConfig(), {
+      projectRoot: '/',
+      platforms: ['ios', 'web'],
+      tsconfig: { baseUrl: '/src', paths: { '/*': ['*'] } },
+      isTsconfigPathsEnabled: true,
+    });
+
+    const platform = 'web';
+
+    modified.resolver.resolveRequest!(getDefaultRequestContext(), 'react-native', platform);
+
+    expect(getResolveFunc()).toBeCalledTimes(1);
+    expect(getResolveFunc()).toBeCalledWith(
+      expect.objectContaining({
+        nodeModulesPaths: ['/node_modules', '/src'],
+        extraNodeModules: {
+          'react-native': expect.stringContaining('node_modules/react-native-web'),
+        },
+        mainFields: ['browser', 'module', 'main'],
+        preferNativePlatform: false,
+      }),
+      'react-native-web',
+      platform
+    );
+  });
+
+  it(`modifies resolution for Node.js environments`, async () => {
+    mockMinFs();
+
+    const modified = withExtendedResolver(asMetroConfig(), {
+      projectRoot: '/',
+      platforms: ['ios', 'web'],
+      tsconfig: null,
+      isTsconfigPathsEnabled: false,
+    });
+
+    const platform = 'web';
+
+    modified.resolver.resolveRequest!(
+      {
+        ...getDefaultRequestContext(),
+        customResolverOptions: {
+          environment: 'node',
+        },
+      },
+      'react-native',
+      platform
+    );
+
+    expect(getResolveFunc()).toBeCalledTimes(1);
+    expect(getResolveFunc()).toBeCalledWith(
+      expect.objectContaining({
+        extraNodeModules: {
+          'react-native': expect.stringContaining('node_modules/react-native-web'),
+        },
+        mainFields: ['main', 'module'],
+        preferNativePlatform: false,
+        // Moved mjs to the back
+        sourceExts: ['ts', 'tsx', 'js', 'jsx', 'mjs', 'json', 'css'],
+      }),
+      'react-native-web',
+      platform
+    );
+  });
+});
+
+describe(getNodejsExtensions, () => {
+  it(`should return the correct extensions for the node.js platform`, () => {
+    const sourceExts = getBareExtensions([], { isTS: true, isReact: true, isModern: true });
+
+    expect(getNodejsExtensions(sourceExts)).not.toEqual(sourceExts);
+
+    // Ensure mjs comes after js
+    expect(getNodejsExtensions(sourceExts)).toMatchInlineSnapshot(`
+      [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "mjs",
+        "json",
+      ]
+    `);
+  });
+});
 
 describe(shouldAliasAssetRegistryForWeb, () => {
   it(`should return true if the incoming resolution is for the web platform and the AssetRegistry`, () => {
