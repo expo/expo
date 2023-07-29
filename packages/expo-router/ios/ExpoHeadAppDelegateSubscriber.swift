b0010ad98e68073df117f646e0088c01c714b03c
evanbacon <bacon@expo.io>
2023-07-29 17:15:23 -0500

    swiftlint

--- packages/expo-router/ios/ExpoHeadAppDelegateSubscriber.swift
+++ packages/expo-router/ios/ExpoHeadAppDelegateSubscriber.swift
@@ -4,13 +4,11 @@ import ExpoModulesCore
 import CoreSpotlight
 
 /// Represents the Info.plist.
-public struct InfoPlist
-{
-    public init(){}
-    
+public struct InfoPlist {
+    public init() {}
+
     /// Returns the custom URL schemes registered by the app ('CFBundleURLSchemes' array).
-    public static func bundleURLSchemes() -> [String]
-    {
+    public static func bundleURLSchemes() -> [String] {
         guard let path = Bundle.main.path(forResource: "Info", ofType: "plist") else {
             log.error("Can’t find path to Info.plist in the main bundle.")
             return []
@@ -35,10 +33,10 @@ func sendFakeDeepLinkEventToReactNative(obj: Any, url: String) {
     NotificationCenter.default.post(
         name: NSNotification.Name(rawValue: "RCTOpenURLNotification"),
         object: obj,
-        userInfo: ["url": url]);
+        userInfo: ["url": url])
 }
 
-func userInfoToQueryString(_ userInfo: [String : NSSecureCoding]?) -> String {
+func userInfoToQueryString(_ userInfo: [String: NSSecureCoding]?) -> String {
     guard let userInfo = userInfo else {
         return ""
     }
@@ -57,23 +55,23 @@ func prefixDeepLink(fragment: String) -> String {
     // This can happen when an NSUserActivity href is used to activate the app.
     if fragment.starts(with: "/") {
         let schemes = InfoPlist.bundleURLSchemes()
-        
+
         return "\(schemes[0]):/\(fragment)"
     }
-    
+
     return fragment
 }
 
 public class ExpoHeadAppDelegateSubscriber: ExpoAppDelegateSubscriber {
     public func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         launchedActivity = userActivity
-        
+
         if let wellKnownHref = userActivity.userInfo?["href"] as? String {
             // TODO: Maybe just use the `webpageUrl` prop from the NSUserActivity.
             // From a stored NSUserActivity, e.g. Quick Note or Siri Reminder
             // From other native device to app
             sendFakeDeepLinkEventToReactNative(obj: self, url: prefixDeepLink(fragment: wellKnownHref))
-        } else if (userActivity.activityType == CSQueryContinuationActionType) {
+        } else if userActivity.activityType == CSQueryContinuationActionType {
             // From Spotlight search
             if let query = userActivity.userInfo?[CSSearchQueryString] as? String {
                 let schemes = InfoPlist.bundleURLSchemes()
@@ -81,12 +79,12 @@ public class ExpoHeadAppDelegateSubscriber: ExpoAppDelegateSubscriber {
                 // TODO: Allow user to define the scheme using structured data or something.
                 // opensearch = Chrome. spotlight = custom thing we're using to identify iOS
                 let url = "\(schemes[0])://search?q=\(encodedQuery)&ref=spotlight"
-                
+
                 // https://github.com/search?q=
                 sendFakeDeepLinkEventToReactNative(obj: self, url: url)
             }
         }
-        
+
         return false
     }
 }
--- packages/expo-router/ios/ExpoHeadModule.swift
+++ packages/expo-router/ios/ExpoHeadModule.swift
@@ -45,12 +45,11 @@ internal class InvalidSchemeException: Exception {
 
 public class ExpoHeadModule: Module {
     private var activities = Set<NSUserActivity>()
-    
+
     public required init(appContext: AppContext) {
         super.init(appContext: appContext)
     }
-    
-    
+
     // Each module class must implement the definition function. The definition consists of components
     // that describes the module's functionality and behavior.
     // See https://docs.expo.dev/modules/module-api for more details about available components.
@@ -59,13 +58,13 @@ public class ExpoHeadModule: Module {
         // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
         // The module will be accessible from `requireNativeModule('ExpoHead')` in JavaScript.
         Name("ExpoHead")
-        
+
         Constants([
             "activities": [
                 "INDEXED_ROUTE": INDEXED_ROUTE
             ]
         ])
-        
+
         Function("getLaunchActivity") { () -> [String: Any]? in
             if let activity = launchedActivity {
                 return [
@@ -81,83 +80,82 @@ public class ExpoHeadModule: Module {
                     "imageUrl": activity.contentAttributeSet?.thumbnailURL,
                     "keywords": activity.keywords,
                     "dateModified": activity.contentAttributeSet?.metadataModificationDate,
-                    "userInfo": activity.userInfo,
+                    "userInfo": activity.userInfo
                 ]
             }
             return nil
         }
-        
+
         Function("createActivity") { (value: MetadataOptions) in
-            if (value.webpageURL?.absoluteString.starts(with: "file://") == true) {
+            if value.webpageURL?.absoluteString.starts(with: "file://") == true {
                 throw Exception(name: "Invalid webpageUrl", description: "Scheme file:// is not allowed for location origin (webpageUrl in NSUserActivity). URL: \(value.webpageURL!.absoluteString)")
             }
-            
+
             let activity = createOrUpdateActivity(value: value)
             activity.becomeCurrent()
         }
-        
+
         AsyncFunction("clearActivitiesAsync") { (ids: [String], promise: Promise) in
             ids.forEach { id in
                 self.revokeActivity(id: id)
             }
-            
+
             CSSearchableIndex.default().deleteSearchableItems(withIdentifiers: ids, completionHandler: { error in
-                if (error != nil) {
+                if error != nil {
                     promise.reject(error as! Exception)
                 } else {
                     promise.resolve()
                 }
             })
         }
-        
+
         Function("suspendActivity") { (id: String) in
             let activity = self.activities.first(where: { $0.persistentIdentifier == id })
             activity?.resignCurrent()
         }
-        
+
         Function("revokeActivity") { (id: String) in
             self.revokeActivity(id: id)
         }
     }
-    
+
     func createOrUpdateActivity(value: MetadataOptions) -> NSUserActivity {
         let att = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
         let existing = self.activities.first(where: { $0.persistentIdentifier == value.id })
         let activity = existing ?? NSUserActivity(activityType: value.activityType)
-        
-        if (existing == nil) {
+
+        if existing == nil {
             self.activities.insert(activity)
         }
-        
+
         // TODO: https://gist.github.com/alexruperez/ea81aa3e371f7d0d7ea5e594d7e9ad08
         activity.targetContentIdentifier = value.id
         activity.persistentIdentifier = value.id
         activity.isEligibleForHandoff = value.isEligibleForHandoff
-        activity.isEligibleForPrediction = value.isEligibleForPrediction;
+        activity.isEligibleForPrediction = value.isEligibleForPrediction
         activity.isEligibleForPublicIndexing = value.isEligibleForPublicIndexing
         activity.isEligibleForSearch = value.isEligibleForSearch
         activity.title = value.title
-        
+
         if let keywords = value.keywords {
             activity.keywords = Set(keywords)
         }
-        
+
         activity.userInfo = value.userInfo
         // Required for handling incoming requests
         //      activity.requiredUserInfoKeys = ["href"]
-        
-        if (value.webpageURL != nil) {
-            
+
+        if value.webpageURL != nil {
             // If you’re using all three APIs, it works well to use the URL of the relevant webpage as the value for uniqueIdentifier, relatedUniqueIdentifier, and webpageURL.
             // https://developer.apple.com/library/archive/documentation/General/Conceptual/AppSearch/CombiningAPIs.html#//apple_ref/doc/uid/TP40016308-CH10-SW1
             activity.webpageURL = value.webpageURL
             att.relatedUniqueIdentifier = value.webpageURL?.absoluteString
         }
-        
+
         att.title = value.title
         // Make all indexed routes deletable
         att.domainIdentifier = INDEXED_ROUTE
-        
+
         if let localUrl = value.imageUrl?.path {
             att.thumbnailURL = value.imageUrl
         }
@@ -165,12 +163,12 @@ public class ExpoHeadModule: Module {
         if let description = value.description {
             att.contentDescription = description
         }
-        
-        activity.contentAttributeSet = att;
-        
+
+        activity.contentAttributeSet = att
+
         return activity
     }
-    
+
     @discardableResult
     func revokeActivity(id: String) -> NSUserActivity? {
         let activity = self.activities.first(where: { $0.persistentIdentifier == id })
