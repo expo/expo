9ddc93201e4c761bf5d2d571893f6a6de9096b7a
evanbacon <bacon@expo.io>
2023-08-01 15:48:19 -0500

    merge more

--- apps/router-e2e/__e2e__/static-rendering/app/index.tsx
+++ apps/router-e2e/__e2e__/static-rendering/app/index.tsx
@@ -1,6 +1,4 @@
 import { Text } from 'react-native';
-// Test import aliases
-// import { HELLO } from '@/__e2e__/static-rendering/test-import';
 
 export default function Page() {
   return <Text>Index</Text>;
