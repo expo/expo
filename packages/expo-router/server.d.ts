439d7193d68fc9cfeab7eb6a08928ac3c7530db2
evanbacon <bacon@expo.io>
2023-08-04 15:16:22 -0500

    fix types

---
+++ packages/expo-router/server.d.ts
@@ -0,0 +1,7 @@
+// These should be installed on the global by the server runtime to ensure faster bundling and smaller bundles.
+
+import { ExpoRequest, ExpoResponse } from '@expo/server/build/environment';
+
+export type RequestHandler = (request: ExpoRequest) => ExpoResponse | Promise<ExpoResponse>;
+
+export { ExpoRequest, ExpoResponse };
--- packages/expo-router/server.ts
+++
@@ -1,9 +0,0 @@
-// These should be installed on the global by the server runtime to ensure faster bundling and smaller bundles.
-export const ExpoRequest =
-  global.ExpoResponse as import('@expo/server/build/environment').ExpoRequest;
-export const ExpoResponse =
-  global.ExpoResponse as import('@expo/server/build/environment').ExpoResponse;
-
-export type RequestHandler = (
-  request: typeof ExpoRequest
-) => typeof ExpoResponse | Promise<typeof ExpoResponse>;
