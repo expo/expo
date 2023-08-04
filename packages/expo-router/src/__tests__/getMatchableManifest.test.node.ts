2c001cf345452a263a826a4e6373075a38cbfbc0
evanbacon <bacon@expo.io>
2023-08-04 16:29:22 -0500

    wip -- idk

---
+++ packages/expo-router/src/__tests__/getMatchableManifest.test.node.ts
@@ -0,0 +1,75 @@
+import { getMatchableManifest } from '../getMatchableManifest';
+import { getExactRoutes, getRoutes } from '../getRoutes';
+import { RequireContext } from '../types';
+
+function createMockContextModule(map: Record<string, Record<string, any>> = {}) {
+  const contextModule = jest.fn((key) => map[key]);
+
+  Object.defineProperty(contextModule, 'keys', {
+    value: () => Object.keys(map),
+  });
+
+  return contextModule as unknown as RequireContext;
+}
+
+function getRoutesFor(files: string[]) {
+  return getExactRoutes(
+    createMockContextModule(Object.fromEntries(files.map((file) => [file, { default() {} }])))
+  )!;
+}
+
+xit(`converts single basic`, () => {
+  expect(getMatchableManifest(getRoutesFor(['./home.js']))).toEqual([
+    { groups: {}, namedRegex: '^/home(?:/)?$', re: /^\/home(?:\/)?$/, routeKeys: {} },
+  ]);
+});
+
+xit(`sorts`, () => {
+  const a = getMatchableManifest(getRoutesFor(['./b/c.tsx', './a.tsx']));
+  expect(a).toEqual(getMatchableManifest(getRoutesFor(['./a.tsx', './b/c.tsx'])));
+  expect(a.map((r) => r.namedRegex)).toEqual(['^/b/c(?:/)?$', '^/a(?:/)?$']);
+});
+
+it(`supports groups`, () => {
+  expect(getMatchableManifest(getRoutesFor(['./(a)/b.tsx']))).toEqual([
+    { groups: {}, namedRegex: '^/\\(a\\)/b(?:/)?$', re: /^\/\(a\)\/b(?:\/)?$/, routeKeys: {} },
+  ]);
+});
+
+xit(`converts index routes`, () => {
+  expect(
+    getMatchableManifest(getRoutesFor(['./index.tsx', './a/index/b.tsx', './a/index/index.js']))
+  ).toEqual([
+    { groups: {}, namedRegex: '^/(?:/)?$', re: /^\/(?:\/)?$/, routeKeys: {} },
+    { groups: {}, namedRegex: '^/a/index/b(?:/)?$', re: /^\/a\/index\/b(?:\/)?$/, routeKeys: {} },
+    { groups: {}, namedRegex: '^/a/index(?:/)?$', re: /^\/a\/index(?:\/)?$/, routeKeys: {} },
+  ]);
+});
+
+xit(`converts dynamic routes`, () => {
+  expect(
+    getMatchableManifest(getRoutesFor(['./[a].tsx', './[...b].tsx', './c/[d]/e/[...f].js']))
+  ).toEqual([
+    {
+      groups: { a: { optional: false, pos: 1, repeat: false } },
+      namedRegex: '^/(?<a>[^/]+?)(?:/)?$',
+      re: /^\/([^/]+?)(?:\/)?$/,
+      routeKeys: { a: 'a' },
+    },
+    {
+      groups: { b: { optional: false, pos: 1, repeat: true } },
+      namedRegex: '^/(?<b>.+?)(?:/)?$',
+      re: /^\/(.+?)(?:\/)?$/,
+      routeKeys: { b: 'b' },
+    },
+    {
+      groups: {
+        d: { optional: false, pos: 1, repeat: false },
+        f: { optional: false, pos: 2, repeat: true },
+      },
+      namedRegex: '^/c/(?<d>[^/]+?)/e/(?<f>.+?)(?:/)?$',
+      re: /^\/c\/([^/]+?)\/e\/(.+?)(?:\/)?$/,
+      routeKeys: { d: 'd', f: 'f' },
+    },
+  ]);
+});
---
+++ packages/expo-router/src/getMatchableManifest.ts
@@ -0,0 +1,193 @@
+import { DynamicConvention, RouteNode, sortRoutes } from './Route';
+import { getContextKey } from './matchers';
+async function recurseAndFlattenNodes<
+  T,
+  TProps,
+  TProcess extends (node: T, props: any) => Promise<T[]>
+>(nodes: T[], props: TProps, func: TProcess): Promise<T[]> {
+  const tarr = await Promise.all(nodes.map((node) => func(node, props)).flat());
+  return tarr.filter(Boolean) as T[];
+}
+
+type MatchableNode = {
+  page: string;
+  regex: string;
+  routeKeys: Record<string, string>;
+  namedRegex: string;
+};
+
+// Given a nested route tree, return a flattened array of all routes that can be matched.
+export function getMatchableManifest(route: RouteNode): MatchableNode[] {
+  function getFlatNodes(route: RouteNode): [string, RouteNode][] {
+    if (route.children.length) {
+      return route.children.map((child) => getFlatNodes(child)).flat();
+      // .sort(([, a], [, b]) => sortRoutes(a, b));
+    }
+
+    const key = getContextKey(route.contextKey).replace(/\/index$/, '') ?? '/';
+    return [[key, route]];
+  }
+
+  // TODO: Ensure routes are sorted
+  const flat = getFlatNodes(route)
+    .sort(([, a], [, b]) => sortRoutes(a, b))
+    .reverse();
+
+  return getMatchableManifestForPaths(flat.map(([normalizedRoutePath]) => normalizedRoutePath));
+}
+
+export function getMatchableManifestForPaths(paths: string[]) {
+  return paths.map((normalizedRoutePath) => {
+    const result = getNamedParametrizedRoute(normalizedRoutePath);
+
+    return {
+      ...getRouteRegex(normalizedRoutePath),
+      namedRegex: `^${result.namedParameterizedRoute}(?:/)?$`,
+      routeKeys: result.routeKeys,
+    };
+  });
+}
+
+/**
+ * From a normalized route this function generates a regular expression and
+ * a corresponding groups object intended to be used to store matching groups
+ * from the regular expression.
+ */
+export function getRouteRegex(normalizedRoute: string): RouteRegex {
+  const { parameterizedRoute, groups } = getParametrizedRoute(normalizedRoute);
+  return {
+    re: new RegExp(`^${parameterizedRoute}(?:/)?$`),
+    groups: groups,
+  };
+}
+
+export interface Group {
+  pos: number;
+  repeat: boolean;
+  optional: boolean;
+}
+
+export interface RouteRegex {
+  groups: { [groupName: string]: Group };
+  re: RegExp;
+}
+
+function getParametrizedRoute(route: string) {
+  const segments = removeTrailingSlash(route).slice(1).split('/');
+  const groups: { [groupName: string]: Group } = {};
+  let groupIndex = 1;
+  return {
+    parameterizedRoute: segments
+      .map((segment) => {
+        if (segment.startsWith('[') && segment.endsWith(']')) {
+          const { key, optional, repeat } = parseParameter(segment.slice(1, -1));
+          groups[key] = { pos: groupIndex++, repeat, optional };
+          return repeat ? (optional ? '(?:/(.+?))?' : '/(.+?)') : '/([^/]+?)';
+        } else {
+          return `/${escapeStringRegexp(segment)}`;
+        }
+      })
+      .join(''),
+    groups,
+  };
+}
+
+/**
+ * Builds a function to generate a minimal routeKey using only a-z and minimal
+ * number of characters.
+ */
+function buildGetSafeRouteKey() {
+  let routeKeyCharCode = 97;
+  let routeKeyCharLength = 1;
+
+  return () => {
+    let routeKey = '';
+    for (let i = 0; i < routeKeyCharLength; i++) {
+      routeKey += String.fromCharCode(routeKeyCharCode);
+      routeKeyCharCode++;
+
+      if (routeKeyCharCode > 122) {
+        routeKeyCharLength++;
+        routeKeyCharCode = 97;
+      }
+    }
+    return routeKey;
+  };
+}
+
+function removeTrailingSlash(route: string) {
+  return route.replace(/\/$/, '') || '/';
+}
+
+function getNamedParametrizedRoute(route: string) {
+  const segments = removeTrailingSlash(route).slice(1).split('/');
+  const getSafeRouteKey = buildGetSafeRouteKey();
+  const routeKeys: { [named: string]: string } = {};
+  return {
+    namedParameterizedRoute: segments
+      .map((segment) => {
+        if (segment.startsWith('[') && segment.endsWith(']')) {
+          const { key, optional, repeat } = parseParameter(segment.slice(1, -1));
+          // replace any non-word characters since they can break
+          // the named regex
+          let cleanedKey = key.replace(/\W/g, '');
+          let invalidKey = false;
+
+          // check if the key is still invalid and fallback to using a known
+          // safe key
+          if (cleanedKey.length === 0 || cleanedKey.length > 30) {
+            invalidKey = true;
+          }
+          if (!isNaN(parseInt(cleanedKey.slice(0, 1)))) {
+            invalidKey = true;
+          }
+
+          if (invalidKey) {
+            cleanedKey = getSafeRouteKey();
+          }
+
+          routeKeys[cleanedKey] = key;
+          return repeat
+            ? optional
+              ? `(?:/(?<${cleanedKey}>.+?))?`
+              : `/(?<${cleanedKey}>.+?)`
+            : `/(?<${cleanedKey}>[^/]+?)`;
+        } else {
+          return `/${escapeStringRegexp(segment)}`;
+        }
+      })
+      .join(''),
+    routeKeys,
+  };
+}
+
+// regexp is based on https://github.com/sindresorhus/escape-string-regexp
+const reHasRegExp = /[|\\{}()[\]^$+*?.-]/;
+const reReplaceRegExp = /[|\\{}()[\]^$+*?.-]/g;
+
+export function escapeStringRegexp(str: string) {
+  // see also: https://github.com/lodash/lodash/blob/2da024c3b4f9947a48517639de7560457cd4ec6c/escapeRegExp.js#L23
+  if (reHasRegExp.test(str)) {
+    return str.replace(reReplaceRegExp, '\\$&');
+  }
+  return str;
+}
+
+/**
+ * Parses a given parameter from a route to a data structure that can be used
+ * to generate the parametrized route. Examples:
+ *   - `[...slug]` -> `{ name: 'slug', repeat: true, optional: true }`
+ *   - `[foo]` -> `{ name: 'foo', repeat: false, optional: true }`
+ *   - `bar` -> `{ name: 'bar', repeat: false, optional: false }`
+ */
+function parseParameter(param: string) {
+  const optional = param.startsWith('[') && param.endsWith(']');
+  if (optional) {
+    param = param.slice(1, -1);
+  }
+  const repeat = param.startsWith('...');
+  if (repeat) {
+    param = param.slice(3);
+  }
+  return { key: param, repeat, optional };
+}
