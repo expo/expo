3a4619afc0092daecd5474d65d27427611b1bb03
lukmccall <kosmatylukasz@gmail.com>
2023-08-08 14:45:27 +0200

    [core] Move `defineProperty` and `ObjectDeallocator` to common code

--- packages/expo-modules-core/common/cpp/JSIUtils.cpp
+++ packages/expo-modules-core/common/cpp/JSIUtils.cpp
@@ -2,7 +2,7 @@
 
 #include "JSIUtils.h"
 
-namespace expo {
+namespace expo::common {
 
 std::vector<jsi::PropNameID> jsiArrayToPropNameIdsVector(jsi::Runtime &runtime, const jsi::Array &array) {
   size_t size = array.size(runtime);
@@ -17,4 +17,25 @@ std::vector<jsi::PropNameID> jsiArrayToPropNameIdsVector(jsi::Runtime &runtime, 
   return vector;
 }
 
-} // namespace expo
+void definePropertyOnJSIObject(
+  jsi::Runtime &runtime,
+  jsi::Object *jsthis,
+  const char *name,
+  jsi::Object descriptor
+) {
+  jsi::Object global = runtime.global();
+  jsi::Object objectClass = global.getPropertyAsObject(runtime, "Object");
+  jsi::Function definePropertyFunction = objectClass.getPropertyAsFunction(
+    runtime,
+    "defineProperty"
+  );
+
+  // This call is basically the same as `Object.defineProperty(object, name, descriptor)` in JS
+  definePropertyFunction.callWithThis(runtime, objectClass, {
+    jsi::Value(runtime, *jsthis),
+    jsi::String::createFromUtf8(runtime, name),
+    std::move(descriptor),
+  });
+}
+
+} // namespace expo::common
