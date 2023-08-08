a5fb1ba84dad722a8ece43d736b9954d1f32b58f
Evan Bacon <bacon@expo.io>
2023-08-08 17:47:01 -0400

    fix(metro-runtime): transpile to cjs (#23871)
    # Why
    
    - This ensures the package can be used as-is with Expo CLI. This was
    broken in the migration.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/metro-runtime/build/error-overlay/toast/ErrorToastContainer.web.js
+++ packages/@expo/metro-runtime/build/error-overlay/toast/ErrorToastContainer.web.js
@@ -1,3 +1,29 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ErrorToastContainer = void 0;
 /**
  * Copyright (c) 650 Industries.
  * Copyright (c) Meta Platforms, Inc. and affiliates.
@@ -5,28 +31,29 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React, { useCallback, useMemo } from 'react';
-import { StyleSheet, View } from 'react-native';
-import * as LogBoxData from '../Data/LogBoxData';
-import { useLogs } from '../Data/LogContext';
-import { useRejectionHandler } from '../useRejectionHandler';
-import { ErrorToast } from './ErrorToast';
-export function ErrorToastContainer() {
-    useRejectionHandler();
-    const { logs, isDisabled } = useLogs();
+const react_1 = __importStar(require("react"));
+const react_native_1 = require("react-native");
+const LogBoxData = __importStar(require("../Data/LogBoxData"));
+const LogContext_1 = require("../Data/LogContext");
+const useRejectionHandler_1 = require("../useRejectionHandler");
+const ErrorToast_1 = require("./ErrorToast");
+function ErrorToastContainer() {
+    (0, useRejectionHandler_1.useRejectionHandler)();
+    const { logs, isDisabled } = (0, LogContext_1.useLogs)();
     if (!logs.length || isDisabled) {
         return null;
     }
-    return React.createElement(ErrorToastStack, { logs: logs });
+    return react_1.default.createElement(ErrorToastStack, { logs: logs });
 }
+exports.ErrorToastContainer = ErrorToastContainer;
 function ErrorToastStack({ logs }) {
-    const onDismissWarns = useCallback(() => {
+    const onDismissWarns = (0, react_1.useCallback)(() => {
         LogBoxData.clearWarnings();
     }, []);
-    const onDismissErrors = useCallback(() => {
+    const onDismissErrors = (0, react_1.useCallback)(() => {
         LogBoxData.clearErrors();
     }, []);
-    const setSelectedLog = useCallback((index) => {
+    const setSelectedLog = (0, react_1.useCallback)((index) => {
         LogBoxData.setSelectedLog(index);
     }, []);
     function openLog(log) {
@@ -37,13 +64,13 @@ function ErrorToastStack({ logs }) {
         }
         setSelectedLog(index);
     }
-    const warnings = useMemo(() => logs.filter((log) => log.level === 'warn'), [logs]);
-    const errors = useMemo(() => logs.filter((log) => log.level === 'error' || log.level === 'fatal'), [logs]);
-    return (React.createElement(View, { style: styles.list },
-        warnings.length > 0 && (React.createElement(ErrorToast, { log: warnings[warnings.length - 1], level: "warn", totalLogCount: warnings.length, onPressOpen: () => openLog(warnings[warnings.length - 1]), onPressDismiss: onDismissWarns })),
-        errors.length > 0 && (React.createElement(ErrorToast, { log: errors[errors.length - 1], level: "error", totalLogCount: errors.length, onPressOpen: () => openLog(errors[errors.length - 1]), onPressDismiss: onDismissErrors }))));
+    const warnings = (0, react_1.useMemo)(() => logs.filter((log) => log.level === 'warn'), [logs]);
+    const errors = (0, react_1.useMemo)(() => logs.filter((log) => log.level === 'error' || log.level === 'fatal'), [logs]);
+    return (react_1.default.createElement(react_native_1.View, { style: styles.list },
+        warnings.length > 0 && (react_1.default.createElement(ErrorToast_1.ErrorToast, { log: warnings[warnings.length - 1], level: "warn", totalLogCount: warnings.length, onPressOpen: () => openLog(warnings[warnings.length - 1]), onPressDismiss: onDismissWarns })),
+        errors.length > 0 && (react_1.default.createElement(ErrorToast_1.ErrorToast, { log: errors[errors.length - 1], level: "error", totalLogCount: errors.length, onPressOpen: () => openLog(errors[errors.length - 1]), onPressDismiss: onDismissErrors }))));
 }
-const styles = StyleSheet.create({
+const styles = react_native_1.StyleSheet.create({
     list: {
         bottom: 6,
         left: 10,
@@ -52,5 +79,5 @@ const styles = StyleSheet.create({
         maxWidth: 320,
     },
 });
-export default LogBoxData.withSubscription(ErrorToastContainer);
+exports.default = LogBoxData.withSubscription(ErrorToastContainer);
 //# sourceMappingURL=ErrorToastContainer.web.js.map
