bc2b4d49055c8d649b59be431ac3f7df19d56414
aleqsio <mikucki@gmail.com>
2023-08-09 13:35:18 +0200

    Add draft android splashscreen implementation

--- packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/SplashScreenModule.kt
+++ packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/SplashScreenModule.kt
@@ -1,43 +1,39 @@
 package expo.modules.splashscreen
 
+import android.util.Log
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import expo.modules.splashscreen.exceptions.HideAsyncException
-import expo.modules.splashscreen.exceptions.PreventAutoHideException
+import expo.modules.kotlin.records.Field
+import expo.modules.kotlin.records.Record
 
 // Below import must be kept unversioned even in versioned code to provide a redirection from
 // versioned code realm to unversioned code realm.
 // Without this import any `SplashScreen.anyMethodName(...)` invocation on JS side ends up
 // in versioned SplashScreen kotlin object that stores no information about the ExperienceActivity.
-import expo.modules.splashscreen.singletons.SplashScreen
+
+class HideSplashScreenOptions : Record {
+  @Field
+  val duration: Float = 200.0F
+}
+
+class ModuleOptions : Record {
+    val delay: Float? = 3000.0F
+}
 
 class SplashScreenModule : Module() {
+
   override fun definition() = ModuleDefinition {
     Name("ExpoSplashScreen")
 
-    AsyncFunction("preventAutoHideAsync") { promise: Promise ->
-      appContext.currentActivity?.let {
-        SplashScreen.preventAutoHide(
-          it,
-          { hasEffect -> promise.resolve(hasEffect) },
-          { m -> promise.reject(PreventAutoHideException(m)) }
-        )
-      } ?: run {
-        promise.resolve(false)
+    OnCreate {
+      SplashScreenManager.reportWarningToLogBox = { message: String ->
+        appContext.errorManager?.reportWarningToLogBox(message)
       }
     }
 
-    AsyncFunction("hideAsync") { promise: Promise ->
-      appContext.currentActivity?.let {
-        SplashScreen.hide(
-          it,
-          { hasEffect -> promise.resolve(hasEffect) },
-          { m -> promise.reject(HideAsyncException(m)) }
-        )
-      } ?: run {
-        promise.resolve(false)
-      }
+    AsyncFunction("hideAsync") { options: HideSplashScreenOptions, promise: Promise ->
+      SplashScreenManager.hide(options, appContext.currentActivity)
     }
   }
 }
--- packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/SplashScreenPackage.kt
+++ packages/expo-splash-screen/android/src/main/java/expo/modules/splashscreen/SplashScreenPackage.kt
@@ -1,22 +1,32 @@
 package expo.modules.splashscreen
 
+import android.app.Activity
 import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import expo.modules.core.interfaces.Package
 import expo.modules.core.interfaces.ReactActivityHandler
 import expo.modules.core.interfaces.ReactActivityLifecycleListener
 import expo.modules.core.interfaces.SingletonModule
-import expo.modules.splashscreen.singletons.SplashScreen
+import expo.modules.splashscreen.SplashScreenManager
 
 class SplashScreenPackage : Package {
-  override fun createSingletonModules(context: Context?): List<SingletonModule> {
-    return listOf(SplashScreen)
-  }
+
+
 
   override fun createReactActivityLifecycleListeners(activityContext: Context): List<ReactActivityLifecycleListener> {
-    return listOf(SplashScreenReactActivityLifecycleListener())
-  }
+    return listOf(object : ReactActivityLifecycleListener {
+      override fun onCreate(activity: Activity, savedInstanceState: Bundle?) {
+        super.onCreate(activity, savedInstanceState)
+        SplashScreenManager.registerOnActivity(activity);
+      }
 
-  override fun createReactActivityHandlers(activityContext: Context?): List<ReactActivityHandler> {
-    return listOf(SplashScreenReactActivityHandler())
+      override fun onContentChanged(activity: Activity?) {
+        super.onContentChanged(activity)
+        SplashScreenManager.onContentChanged(activity)
+      }
+    })
   }
 }
