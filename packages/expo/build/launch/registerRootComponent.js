5fb1ffcbb69b82605ed5724bfe0d291ee3c09ede
evanbacon <bacon@expo.io>
2023-08-10 14:33:18 -0400

    fix double import

--- packages/expo/build/launch/registerRootComponent.js
+++ packages/expo/build/launch/registerRootComponent.js
@@ -7,20 +7,22 @@ export default function registerRootComponent(component) {
         qualifiedComponent = withDevTools(component);
     }
     AppRegistry.registerComponent('main', () => qualifiedComponent);
-    if (Platform.OS === 'web' &&
+    if (Platform.OS === 'web') {
+        // Use two if statements for better dead code elimination.
+        if (
         // Skip querying the DOM if we're in a Node.js environment.
         typeof document !== 'undefined') {
-        AppRegistry.registerComponent('main', () => qualifiedComponent);
-        const rootTag = document.getElementById('root');
-        if (process.env.NODE_ENV !== 'production') {
-            if (!rootTag) {
-                throw new Error('Required HTML element with id "root" was not found in the document HTML.');
+            const rootTag = document.getElementById('root');
+            if (process.env.NODE_ENV !== 'production') {
+                if (!rootTag) {
+                    throw new Error('Required HTML element with id "root" was not found in the document HTML.');
+                }
             }
+            AppRegistry.runApplication('main', {
+                rootTag,
+                hydrate: process.env.EXPO_PUBLIC_USE_STATIC === '1',
+            });
         }
-        AppRegistry.runApplication('main', {
-            rootTag,
-            hydrate: process.env.EXPO_PUBLIC_USE_STATIC === '1',
-        });
     }
 }
 //# sourceMappingURL=registerRootComponent.js.map
