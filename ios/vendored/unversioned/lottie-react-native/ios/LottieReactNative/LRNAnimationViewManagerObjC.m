48650d23955989f91f892c9938fea249369d2064
Matin Zadeh Dolatabad <zadehdolatabad@gmail.com>
2023-08-10 13:27:36 +0200

    [go] update lottie-react-native to 6.1.2 (#23843)
    # Why
    
    There have been a lot of changes in Lottie React Native and there are some issues for users using Expo Go. 
    
    May fix:
    - https://github.com/lottie-react-native/lottie-react-native/issues/1075
    - https://github.com/lottie-react-native/lottie-react-native/issues/999
    - https://github.com/lottie-react-native/lottie-react-native/issues/1005
    
    # How
    
    - Changed source type to `npm` in expoGoConfig
    - Ran `et uvm -m lottie-react-native -c 6.1.2`
    - Updated NCL example
    
    # Test Plan
    
    versioned expo go + NCL lottie test case
    
    ---------
    
    Co-authored-by: Kudo Chien <kudo@expo.dev>

--- ios/vendored/unversioned/lottie-react-native/ios/LottieReactNative/LRNAnimationViewManagerObjC.m
+++ ios/vendored/unversioned/lottie-react-native/ios/LottieReactNative/LRNAnimationViewManagerObjC.m
@@ -7,12 +7,16 @@ RCT_EXPORT_VIEW_PROPERTY(resizeMode, NSString);
 RCT_EXPORT_VIEW_PROPERTY(sourceJson, NSString);
 RCT_EXPORT_VIEW_PROPERTY(sourceName, NSString);
 RCT_EXPORT_VIEW_PROPERTY(sourceURL, NSString);
+RCT_EXPORT_VIEW_PROPERTY(sourceDotLottieURI, NSString);
 RCT_EXPORT_VIEW_PROPERTY(progress, CGFloat);
 RCT_EXPORT_VIEW_PROPERTY(loop, BOOL);
+RCT_EXPORT_VIEW_PROPERTY(autoPlay, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(speed, CGFloat);
 RCT_EXPORT_VIEW_PROPERTY(onAnimationFinish, RCTBubblingEventBlock);
+RCT_EXPORT_VIEW_PROPERTY(onAnimationFailure, RCTBubblingEventBlock);
 RCT_EXPORT_VIEW_PROPERTY(colorFilters, LRNColorFilters);
 RCT_EXPORT_VIEW_PROPERTY(textFiltersIOS, NSArray);
+RCT_EXPORT_VIEW_PROPERTY(renderMode, NSString);
 
 RCT_EXTERN_METHOD(play:(nonnull NSNumber *)reactTag fromFrame:(nonnull NSNumber *) startFrame toFrame:(nonnull NSNumber *) endFrame);
 
