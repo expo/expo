669aa82fa2e37576b867aaf599bf8e1cd92fa173
James Ide <ide@users.noreply.github.com>
2023-08-10 20:48:33 -0700

    [jest] Ignore transforming react-native-reanimated/plugin (#23912)
    Why
    ---
    We use Jest projects to run multi-platform tests. For instance, there is
    an Android Jest project and an iOS Jest project. Some tests are run
    under multiple projects, like `test.native.js`. Such tests would cause
    Jest to fail with:
    `
    Error: [BABEL]: [BABEL]
    <path>/expo/node_modules/react-native-reanimated/plugin/index.js:
    Reentrant plugin detected trying to load
    "<path>/expo/node_modules/react-native-reanimated/plugin/index.js". This
    module is not ignored and is trying to load itself while compiling
    itself, leading to a dependency cycle. We recommend adding it to your
    "ignore" list in your babelrc, or to a .babelignore. (While processing:
    <path>/expo/node_modules/react-native-reanimated/plugin/index.js)
    `
    
    How
    ---
    Added `'/node_modules/react-native-reanimated/plugin/'` to the
    `transformIgnorePatterns` setting in the preset defined by `jest-expo`.
    
    Two other small enhancements: added a leading slash to the existing
    `node_modules`, which more closely matches how Jest's [default
    rule](https://jestjs.io/docs/configuration#transformignorepatterns-arraystring)
    is formatted, and replaced a `reduce()` that wasn't reducing with a
    `flatMap()` call that is mapping.
    
    Test Plan
    ---
    - `jest-expo`'s own tests pass
    - Run `yarn test` under `expo-sensors`. Previously it would fail. Now it
    passes.

--- packages/jest-expo/jest-preset.js
+++ packages/jest-expo/jest-preset.js
@@ -42,7 +42,8 @@ if (!Array.isArray(jestPreset.transformIgnorePatterns)) {
 
 // Also please keep `testing-with-jest.md` file up to date
 jestPreset.transformIgnorePatterns = [
-  'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  '/node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  '/node_modules/react-native-reanimated/plugin/',
 ];
 
 // setupFiles
