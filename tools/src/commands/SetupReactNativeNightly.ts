55b7a3f940e09ce4000109bfa34080b2334e785d
Kudo Chien <kudo@expo.dev>
2023-08-11 22:26:13 +0800

    [tools] update SetupReactNativeNightly

--- tools/src/commands/SetupReactNativeNightly.ts
+++ tools/src/commands/SetupReactNativeNightly.ts
@@ -2,18 +2,13 @@ import { Command } from '@expo/commander';
 import JsonFile from '@expo/json-file';
 import spawnAsync from '@expo/spawn-async';
 import assert from 'assert';
-import fs from 'fs-extra';
-import glob from 'glob-promise';
 import path from 'path';
 
-import { EXPO_DIR, EXPOTOOLS_DIR } from '../Constants';
+import { EXPO_DIR } from '../Constants';
 import logger from '../Logger';
 import { getPackageViewAsync } from '../Npm';
 import { transformFileAsync } from '../Transforms';
-import { applyPatchAsync } from '../Utils';
 import { installAsync as workspaceInstallAsync } from '../Workspace';
-
-const PATCHES_ROOT = path.join(EXPOTOOLS_DIR, 'src', 'react-native-nightlies', 'patches');
 
 export default (program: Command) => {
   program
@@ -34,6 +29,8 @@ async function main() {
   logger.info('Adding pinned packages:');
   const pinnedPackages = {
     'react-native': nightlyVersion,
+    '@react-native-async-storage/async-storage': '~1.19.1', // fix AGP 8 build error
+    '@react-native-community/netinfo': '~9.4.1', // fix AGP 8 build error
   };
   await addPinnedPackagesAsync(pinnedPackages);
 
@@ -42,9 +39,11 @@ async function main() {
 
   await updateReactNativePackageAsync();
 
-  await patchReanimatedAsync(nightlyVersion);
-  await patchDetoxAsync();
+  await patchAndroidBuildConfigAsync();
   await patchReactNavigationAsync();
+  await patchDetoxAsync();
+  await patchReanimatedAsync();
+  await patchScreensAsync();
   await patchGestureHandlerAsync();
 
   logger.info('Setting up Expo modules files');
@@ -68,8 +67,6 @@ async function addBareExpoOptionalPackagesAsync() {
   const versionMap = {
     ...(packageJsonNCL.devDependencies as object),
     ...(packageJsonNCL.dependencies as object),
-    // override @shopify/react-native-skia version to fix xcode 14.3 build error
-    '@shopify/react-native-skia': '0.1.184',
   };
 
   const installPackages = OPTIONAL_PKGS.map((pkg) => {
@@ -122,70 +119,28 @@ async function updateReactNativePackageAsync() {
   );
 
   // Workaround build error for React-bridging depending on butter
-  const bridgingFiles = await glob('ReactCommon/react/bridging/*.{h,cpp}', {
-    cwd: reactNativeRoot,
-    absolute: true,
-  });
-  await Promise.all(
-    bridgingFiles.map((file) =>
-      transformFileAsync(file, [
-        {
-          find: /<butter\/map\.h>/g,
-          replaceWith: '<map>',
-        },
-        {
-          find: /<butter\/function\.h>/g,
-          replaceWith: '<functional>',
-        },
-        {
-          find: /butter::(map|function)/g,
-          replaceWith: 'std::$1',
-        },
-      ])
-    )
-  );
-
-  // Workaround build error for outdated `@react-native/gradle-plugin`
-  const reactGradlePluginRoot = path.join(
-    EXPO_DIR,
-    'node_modules',
-    '@react-native',
-    'gradle-plugin'
-  );
-  await transformFileAsync(
-    path.join(reactGradlePluginRoot, 'src/main/kotlin/com/facebook/react/utils/DependencyUtils.kt'),
-    [
-      {
-        find: 'return if (versionString.startsWith("0.0.0")) {',
-        replaceWith:
-          'return if (versionString.startsWith("0.0.0") || "-nightly-" in versionString) {',
-      },
-    ]
-  );
-}
-
-async function patchReanimatedAsync(nightlyVersion: string) {
-  const root = path.join(EXPO_DIR, 'node_modules', 'react-native-reanimated');
-
-  await transformFileAsync(path.join(root, 'android', 'build.gradle'), [
-    {
-      find: /\$minor/g,
-      replaceWith: '$rnMinorVersion',
-    },
-  ]);
-
-  await transformFileAsync(path.join(root, 'RNReanimated.podspec'), [
-    {
-      find: /^(\s*['"]USE_HEADERMAP['"]\s+=>\s+['"]YES['"],\s*)$/gm,
-      replaceWith: `$1\n    "CLANG_CXX_LANGUAGE_STANDARD" => "c++17",`,
-    },
-  ]);
-}
-
-async function patchDetoxAsync() {
-  const patchFile = path.join(PATCHES_ROOT, 'detox.patch');
-  const patchContent = await fs.readFile(patchFile, 'utf8');
-  await applyPatchAsync({ patchContent, cwd: EXPO_DIR, stripPrefixNum: 1 });
+  // const bridgingFiles = await glob('ReactCommon/react/bridging/*.{h,cpp}', {
+  //   cwd: reactNativeRoot,
+  //   absolute: true,
+  // });
+  // await Promise.all(
+  //   bridgingFiles.map((file) =>
+  //     transformFileAsync(file, [
+  //       {
+  //         find: /<butter\/map\.h>/g,
+  //         replaceWith: '<map>',
+  //       },
+  //       {
+  //         find: /<butter\/function\.h>/g,
+  //         replaceWith: '<functional>',
+  //       },
+  //       {
+  //         find: /butter::(map|function)/g,
+  //         replaceWith: 'std::$1',
+  //       },
+  //     ])
+  //   )
+  // );
 }
 
 async function patchReactNavigationAsync() {
@@ -197,6 +152,55 @@ async function patchReactNavigationAsync() {
         find: 'style={[{ height, minHeight, maxHeight, opacity, transform }]}',
         replaceWith:
           'style={[{ height, minHeight, maxHeight, opacity, transform: transform ?? [] }]}',
+      },
+    ]
+  );
+}
+
+async function patchDetoxAsync() {
+  await transformFileAsync(
+    path.join(EXPO_DIR, 'node_modules', 'detox', 'android/detox/build.gradle'),
+    [
+      {
+        // namespace
+        find: /^(android \{[\s\S]*?)(\n})/gm,
+        replaceWith: '$1\n  namespace "com.wix.detox"\n$2',
+      },
+    ]
+  );
+}
+
+async function patchReanimatedAsync() {
+  await transformFileAsync(
+    path.join(
+      EXPO_DIR,
+      'node_modules',
+      'react-native-reanimated',
+      'android/src/main/java/com/swmansion/reanimated/keyboardObserver/ReanimatedKeyboardEventListener.java'
+    ),
+    [
+      {
+        // AGP 8 `nonTransitiveRClass`
+        find: /\bcom\.swmansion\.reanimated\.(R\.id\.action_bar_root)/g,
+        replaceWith: 'androidx.appcompat.$1',
+      },
+    ]
+  );
+}
+
+async function patchScreensAsync() {
+  await transformFileAsync(
+    path.join(
+      EXPO_DIR,
+      'node_modules',
+      'react-native-screens',
+      'android/src/main/java/com/swmansion/rnscreens/ScreenStackHeaderConfig.kt'
+    ),
+    [
+      {
+        // AGP 8 `nonTransitiveRClass`
+        find: /\b(R\.attr\.colorPrimary)/g,
+        replaceWith: 'android.$1',
       },
     ]
   );
@@ -225,17 +229,77 @@ async function updateExpoModulesAsync() {
 
 async function updateBareExpoAsync(nightlyVersion: string) {
   const root = path.join(EXPO_DIR, 'apps', 'bare-expo');
-  await transformFileAsync(path.join(root, 'android', 'settings.gradle'), [
-    {
-      find: /react-native-gradle-plugin/g,
-      replaceWith: '@react-native/gradle-plugin',
-    },
-  ]);
-
   await transformFileAsync(path.join(root, 'ios', 'Podfile'), [
     {
       find: /(platform :ios, )['"]13\.0['"]/g,
       replaceWith: "$1'13.4'",
     },
   ]);
+
+  // flipper-integration
+  await transformFileAsync(path.join(root, 'android', 'app', 'build.gradle'), [
+    {
+      find: 'debugImplementation("com.facebook.flipper:flipper-fresco-plugin:${FLIPPER_VERSION}")',
+      replaceWith: 'debugImplementation("com.facebook.fresco:flipper-fresco-plugin:3.0.0")',
+    },
+  ]);
+  await transformFileAsync(path.join(root, 'android', 'gradle.properties'), [
+    {
+      find: /FLIPPER_VERSION=0\.182\.0/,
+      replaceWith: 'FLIPPER_VERSION=0.201.0',
+    },
+  ]);
+}
+
+async function patchAndroidBuildConfigAsync() {
+  const missingBuildConfigModules = [
+    '@react-native-async-storage/async-storage',
+    '@react-native-community/datetimepicker',
+    '@react-native-community/netinfo',
+    '@react-native-community/slider',
+    'lottie-react-native',
+    'react-native-gesture-handler',
+    'react-native-maps',
+    'react-native-pager-view',
+    'react-native-reanimated',
+    'react-native-safe-area-context',
+    'react-native-screens',
+    'react-native-svg',
+    'react-native-webview',
+  ];
+  const searchPattern = /^(android \{[\s\S]*?)(\n})/gm;
+  const replacement = `$1
+    buildFeatures {
+        buildConfig true
+    }$2`;
+  for (const module of missingBuildConfigModules) {
+    const gradleFile = path.join(EXPO_DIR, 'node_modules', module, 'android', 'build.gradle');
+    await transformFileAsync(gradleFile, [
+      {
+        find: searchPattern,
+        replaceWith: replacement,
+      },
+    ]);
+  }
+
+  const missingNamespaceModules = {
+    '@shopify/flash-list': 'com.shopify.reactnative.flash_list',
+    '@shopify/react-native-skia': 'com.shopify.reactnative.skia',
+    '@react-native-community/slider': 'com.reactnativecommunity.slider',
+    '@react-native-masked-view/masked-view': 'org.reactnative.maskedview',
+    '@react-native-picker/picker': 'com.reactnativecommunity.picker',
+    'react-native-maps': 'com.rnmaps.maps',
+    'react-native-pager-view': 'com.reactnativepagerview',
+    'react-native-view-shot': 'fr.greweb.reactnativeviewshot',
+    'react-native-webview': 'com.reactnativecommunity.webview',
+  };
+  for (const [module, namespace] of Object.entries(missingNamespaceModules)) {
+    const gradleFile = path.join(EXPO_DIR, 'node_modules', module, 'android', 'build.gradle');
+    await transformFileAsync(gradleFile, [
+      {
+        find: searchPattern,
+        replaceWith: `$1\n  namespace "${namespace}"\n$2`,
+      },
+    ]);
+  }
 }
