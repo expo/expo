9a449a192754860d77732f5cd7d6d61cfc71c487
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2023-08-14 11:13:29 +0200

    [battery][Android] Migrated codebase to use Expo modules API (#23883)
    # Why
    
    Migrated codebase to use Expo modules API
    
    # Test Plan
    
    - bare-expo ✅

--- packages/expo-battery/android/src/test/java/expo/modules/battery/MockedEventEmitter.java
+++
@@ -1,7 +0,0 @@
-package expo.modules.battery;
-
-import expo.modules.core.interfaces.InternalModule;
-import expo.modules.core.interfaces.services.EventEmitter;
-
-public interface MockedEventEmitter extends InternalModule, EventEmitter {
-}
