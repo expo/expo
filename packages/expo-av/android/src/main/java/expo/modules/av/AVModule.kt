130d2e68a9d1bdde4183dc98d6931b93b0a74df2
lukmccall <kosmatylukasz@gmail.com>
2023-08-14 10:38:31 +0200

    Fix

--- packages/expo-av/android/src/main/java/expo/modules/av/AVModule.kt
+++ packages/expo-av/android/src/main/java/expo/modules/av/AVModule.kt
@@ -1,6 +1,7 @@
 package expo.modules.av
 
 import android.Manifest
+import expo.modules.core.arguments.MapArguments
 import expo.modules.core.arguments.ReadableArguments
 import expo.modules.interfaces.permissions.Permissions
 import expo.modules.kotlin.exception.CodedException
@@ -24,52 +25,52 @@ class AVModule : Module() {
       avManager.setAudioIsEnabled(value)
     }
 
-    AsyncFunction("setAudioMode") { map: ReadableArguments ->
-      avManager.setAudioMode(map)
+    AsyncFunction("setAudioMode") { map: Map<String, Any?> ->
+      avManager.setAudioMode(map.toReadableArguments())
     }
 
-    AsyncFunction("loadForSound") { source: ReadableArguments, status: ReadableArguments, promise: KotlinPromise ->
-      avManager.loadForSound(source, status, promise.toLegacyPromise())
+    AsyncFunction("loadForSound") { source: Map<String, Any?>, status: Map<String, Any?>, promise: KotlinPromise ->
+      avManager.loadForSound(source.toReadableArguments(), status.toReadableArguments(), promise.toLegacyPromise())
     }
 
     AsyncFunction("unloadForSound") { key: Int, promise: KotlinPromise ->
       avManager.unloadForSound(key, promise.toLegacyPromise())
     }
 
-    AsyncFunction("setStatusForSound") { key: Int, status: ReadableArguments, promise: KotlinPromise ->
-      avManager.setStatusForSound(key, status, promise.toLegacyPromise())
+    AsyncFunction("setStatusForSound") { key: Int, status: Map<String, Any?>, promise: KotlinPromise ->
+      avManager.setStatusForSound(key, status.toReadableArguments(), promise.toLegacyPromise())
     }
 
-    AsyncFunction("replaySound") { key: Int, status: ReadableArguments, promise: KotlinPromise ->
-      avManager.replaySound(key, status, promise.toLegacyPromise())
+    AsyncFunction("replaySound") { key: Int, status: Map<String, Any?>, promise: KotlinPromise ->
+      avManager.replaySound(key, status.toReadableArguments(), promise.toLegacyPromise())
     }
 
     AsyncFunction("getStatusForSound") { key: Int, promise: KotlinPromise ->
       avManager.getStatusForSound(key, promise.toLegacyPromise())
     }
 
-    AsyncFunction("loadForVideo") { tag: Int, source: ReadableArguments?, status: ReadableArguments?, promise: KotlinPromise ->
-      avManager.loadForVideo(tag, source, status, promise.toLegacyPromise())
+    AsyncFunction("loadForVideo") { tag: Int, source: Map<String, Any?>?, status: Map<String, Any?>?, promise: KotlinPromise ->
+      avManager.loadForVideo(tag, source?.toReadableArguments(), status?.toReadableArguments(), promise.toLegacyPromise())
     }
 
     AsyncFunction("unloadForVideo") { tag: Int, promise: KotlinPromise ->
       avManager.unloadForVideo(tag, promise.toLegacyPromise())
     }
 
-    AsyncFunction("setStatusForVideo") { tag: Int, status: ReadableArguments, promise: KotlinPromise ->
-      avManager.setStatusForVideo(tag, status, promise.toLegacyPromise())
+    AsyncFunction("setStatusForVideo") { tag: Int, status: Map<String, Any?>, promise: KotlinPromise ->
+      avManager.setStatusForVideo(tag, status.toReadableArguments(), promise.toLegacyPromise())
     }
 
-    AsyncFunction("replayVideo") { tag: Int, status: ReadableArguments, promise: KotlinPromise ->
-      avManager.replayVideo(tag, status, promise.toLegacyPromise())
+    AsyncFunction("replayVideo") { tag: Int, status: Map<String, Any?>, promise: KotlinPromise ->
+      avManager.replayVideo(tag, status.toReadableArguments(), promise.toLegacyPromise())
     }
 
     AsyncFunction("getStatusForVideo") { tag: Int, promise: KotlinPromise ->
       avManager.getStatusForVideo(tag, promise.toLegacyPromise())
     }
 
-    AsyncFunction("prepareAudioRecorder") { options: ReadableArguments, promise: KotlinPromise ->
-      avManager.prepareAudioRecorder(options, promise.toLegacyPromise())
+    AsyncFunction("prepareAudioRecorder") { options: Map<String, Any?>, promise: KotlinPromise ->
+      avManager.prepareAudioRecorder(options.toReadableArguments(), promise.toLegacyPromise())
     }
 
     AsyncFunction("getAvailableInputs") { promise: KotlinPromise ->
@@ -117,12 +118,16 @@ class AVModule : Module() {
 private fun KotlinPromise.toLegacyPromise(): LegacyPromise {
   val newPromise = this
   return object : LegacyPromise {
-    override fun resolve(value: Any) {
+    override fun resolve(value: Any?) {
       newPromise.resolve(value)
     }
 
-    override fun reject(c: String, m: String, e: Throwable) {
-      newPromise.reject(c, m, e)
+    override fun reject(c: String?, m: String?, e: Throwable?) {
+      newPromise.reject(c ?: "unknown", m, e)
     }
   }
+}
+
+private fun Map<String, Any?>.toReadableArguments(): ReadableArguments {
+  return MapArguments(this)
 }
