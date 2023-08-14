66b7ad3162a7548f5405172ba2de211193ede2dc
Alan Hughes <hughesno1@gmail.com>
2023-08-14 10:12:45 +0100

    [android][navigation-bar] make requested changes

--- packages/expo-navigation-bar/android/src/main/java/expo/modules/navigationbar/NavigationBarModule.kt
+++ packages/expo-navigation-bar/android/src/main/java/expo/modules/navigationbar/NavigationBarModule.kt
@@ -1,6 +1,5 @@
 package expo.modules.navigationbar
 
-import android.app.Activity
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -10,154 +9,120 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.functions.Queues
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import expo.modules.navigationbar.singletons.NavigationBar
 
 class NavigationBarModule : Module() {
+  private val activity
+    get() = appContext.currentActivity
+      ?: throw Exceptions.MissingActivity()
+
   override fun definition() = ModuleDefinition {
     Name("ExpoNavigationBar")
 
     Events(VISIBILITY_EVENT_NAME)
 
     AsyncFunction("setBackgroundColorAsync") { color: Int, promise: Promise ->
-      safeRunOnUiThread {
-        NavigationBar.setBackgroundColor(it, color, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
-      }
-    }
+      NavigationBar.setBackgroundColor(activity, color, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
+    }.runOnQueue(Queues.MAIN)
 
-    AsyncFunction("getBackgroundColorAsync") { promise: Promise ->
-      safeRunOnUiThread {
-        val color = colorToHex(it.window.navigationBarColor)
-        promise.resolve(color)
-      }
-    }
+    AsyncFunction("getBackgroundColorAsync") {
+      return@AsyncFunction colorToHex(activity.window.navigationBarColor)
+    }.runOnQueue(Queues.MAIN)
 
     AsyncFunction("setBorderColorAsync") { color: Int, promise: Promise ->
-      safeRunOnUiThread {
-        NavigationBar.setBorderColor(it, color, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
-      }
-    }
+      NavigationBar.setBorderColor(activity, color, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
+    }.runOnQueue(Queues.MAIN)
 
-    AsyncFunction("getBorderColorAsync") { promise: Promise ->
-      safeRunOnUiThread {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-          val color = colorToHex(it.window.navigationBarDividerColor)
-          promise.resolve(color)
-        } else {
-          promise.reject(NavigationBarException("'getBorderColorAsync' is only available on Android API 28 or higher"))
-        }
+    AsyncFunction("getBorderColorAsync") {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        return@AsyncFunction colorToHex(activity.window.navigationBarDividerColor)
+      } else {
+        throw NavigationBarException("'getBorderColorAsync' is only available on Android API 28 or higher")
       }
-    }
+    }.runOnQueue(Queues.MAIN)
 
     AsyncFunction("setButtonStyleAsync") { buttonStyle: String, promise: Promise ->
-      safeRunOnUiThread {
-        NavigationBar.setButtonStyle(
-          it, buttonStyle,
-          {
-            promise.resolve(null)
-          },
-          { m -> promise.reject(NavigationBarException(m)) }
-        )
-      }
-    }
+      NavigationBar.setButtonStyle(
+        activity, buttonStyle,
+        {
+          promise.resolve(null)
+        },
+        { m -> promise.reject(NavigationBarException(m)) }
+      )
+    }.runOnQueue(Queues.MAIN)
 
-    AsyncFunction("getButtonStyleAsync") { promise: Promise ->
-      safeRunOnUiThread {
-        WindowInsetsControllerCompat(it.window, it.window.decorView).let { controller ->
-          val style = if (controller.isAppearanceLightNavigationBars) "dark" else "light"
-          promise.resolve(style)
-        }
+    AsyncFunction("getButtonStyleAsync") {
+      WindowInsetsControllerCompat(activity.window, activity.window.decorView).let { controller ->
+        return@AsyncFunction if (controller.isAppearanceLightNavigationBars) "dark" else "light"
       }
-    }
+    }.runOnQueue(Queues.MAIN)
 
     AsyncFunction("setVisibilityAsync") { visibility: String, promise: Promise ->
-      safeRunOnUiThread {
-        NavigationBar.setVisibility(it, visibility, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
-      }
-    }
+      NavigationBar.setVisibility(activity, visibility, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
+    }.runOnQueue(Queues.MAIN)
 
-    AsyncFunction("getVisibilityAsync") { promise: Promise ->
-      safeRunOnUiThread {
-        val isVisible = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-          it.window.decorView.rootWindowInsets.isVisible(WindowInsets.Type.navigationBars())
-        } else {
-          @Suppress("DEPRECATION")
-          (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION and it.window.decorView.systemUiVisibility) == 0
-        }
-        val visibility = if (isVisible) "visible" else "hidden"
-        promise.resolve(visibility)
+    AsyncFunction("getVisibilityAsync") {
+      val isVisible = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        activity.window.decorView.rootWindowInsets.isVisible(WindowInsets.Type.navigationBars())
+      } else {
+        @Suppress("DEPRECATION")
+        (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION and activity.window.decorView.systemUiVisibility) == 0
       }
-    }
+      return@AsyncFunction if (isVisible) "visible" else "hidden"
+    }.runOnQueue(Queues.MAIN)
 
     AsyncFunction("setPositionAsync") { position: String, promise: Promise ->
-      safeRunOnUiThread {
-        NavigationBar.setPosition(it, position, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
-      }
-    }
+      NavigationBar.setPosition(activity, position, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
+    }.runOnQueue(Queues.MAIN)
 
-    AsyncFunction("unstable_getPositionAsync") { promise: Promise ->
-      safeRunOnUiThread {
-        val position = if (ViewCompat.getFitsSystemWindows(it.window.decorView)) "relative" else "absolute"
-        promise.resolve(position)
-      }
-    }
+    AsyncFunction("unstable_getPositionAsync") {
+      return@AsyncFunction if (ViewCompat.getFitsSystemWindows(activity.window.decorView)) "relative" else "absolute"
+    }.runOnQueue(Queues.MAIN)
 
     AsyncFunction("setBehaviorAsync") { behavior: String, promise: Promise ->
-      safeRunOnUiThread {
-        NavigationBar.setBehavior(it, behavior, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
-      }
-    }
+      NavigationBar.setBehavior(activity, behavior, { promise.resolve(null) }, { m -> promise.reject(NavigationBarException(m)) })
+    }.runOnQueue(Queues.MAIN)
 
-    AsyncFunction("getBehaviorAsync") { promise: Promise ->
-      safeRunOnUiThread {
-        WindowInsetsControllerCompat(it.window, it.window.decorView).let { controller ->
-          val behavior = when (controller.systemBarsBehavior) {
-            // TODO: Maybe relative / absolute
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE -> "overlay-swipe"
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE -> "inset-swipe"
-            // WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_TOUCH -> "inset-touch"
-            else -> "inset-touch"
+    AsyncFunction("getBehaviorAsync") {
+      WindowInsetsControllerCompat(activity.window, activity.window.decorView).let { controller ->
+        val behavior = when (controller.systemBarsBehavior) {
+          // TODO: Maybe relative / absolute
+          WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE -> "overlay-swipe"
+          WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE -> "inset-swipe"
+          // WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_TOUCH -> "inset-touch"
+          else -> "inset-touch"
+        }
+
+        return@AsyncFunction behavior
+      }
+    }.runOnQueue(Queues.MAIN)
+
+    // We are not using `OnStartObserving` and `OnStopObserving` here because when switching threads
+    // they will resolve the promise quicker and the JS won't wait for observer removal
+    AsyncFunction("startObserving") {
+      val decorView = activity.window.decorView
+      @Suppress("DEPRECATION")
+      decorView.setOnSystemUiVisibilityChangeListener { visibility: Int ->
+        val isNavigationBarVisible = (visibility and View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0
+        val stringVisibility = if (isNavigationBarVisible) "visible" else "hidden"
+        sendEvent(
+          VISIBILITY_EVENT_NAME,
+          Bundle().apply {
+            putString("visibility", stringVisibility)
+            putInt("rawVisibility", visibility)
           }
-          promise.resolve(behavior)
-        }
+        )
       }
-    }
+    }.runOnQueue(Queues.MAIN)
 
-    OnStartObserving {
-      safeRunOnUiThread {
-        val decorView = it.window.decorView
-        @Suppress("DEPRECATION")
-        decorView.setOnSystemUiVisibilityChangeListener { visibility: Int ->
-          val isNavigationBarVisible = (visibility and View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) == 0
-          val stringVisibility = if (isNavigationBarVisible) "visible" else "hidden"
-          sendEvent(
-            VISIBILITY_EVENT_NAME,
-            Bundle().apply {
-              putString("visibility", stringVisibility)
-              putInt("rawVisibility", visibility)
-            }
-          )
-        }
-      }
-    }
-
-    OnStopObserving {
-      safeRunOnUiThread {
-        val decorView = it.window.decorView
-        @Suppress("DEPRECATION")
-        decorView.setOnSystemUiVisibilityChangeListener(null)
-      }
-    }
-  }
-
-  // Ensure that rejections are passed up to JS rather than terminating the native client.
-  private fun safeRunOnUiThread(block: (activity: Activity) -> Unit) {
-    val activity = appContext.currentActivity
-      ?: throw Exceptions.MissingActivity()
-    activity.runOnUiThread {
-      block(activity)
-    }
+    AsyncFunction("stopObserving") {
+      val decorView = activity.window.decorView
+      @Suppress("DEPRECATION")
+      decorView.setOnSystemUiVisibilityChangeListener(null)
+    }.runOnQueue(Queues.MAIN)
   }
 
   companion object {
