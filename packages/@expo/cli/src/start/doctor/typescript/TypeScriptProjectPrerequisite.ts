87669a95077dae9e9b1e49bb7b5d23ec6d85d503
Mark Lawlor <mwlawlor@gmail.com>
2023-08-16 01:00:41 +1000

    feat(cli): add `expo typescript` command (#23642)
    # Why
    
    The command serves two purposes
      - Allow users to manually start the Expo Typescript bootstrap process
      - Generate Expo Router types within CI environments. 
     
    Unlike the automatic Typescript detection, this command will bypass
    prompts and simply enable Typescript support.
    
    # How
    
    Reuses `TypeScriptProjectPrerequisite` and the Typescript services from
    `MetroBundlerDevServer`.
    
    This PR also modifies the
    `MetroBundlerDevServer.startTypeScriptServices()` to run without a
    started server (so file watching will be disabled)
    
    # Test Plan
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/@expo/cli/src/start/doctor/typescript/TypeScriptProjectPrerequisite.ts
+++ packages/@expo/cli/src/start/doctor/typescript/TypeScriptProjectPrerequisite.ts
@@ -43,7 +43,7 @@ export class TypeScriptProjectPrerequisite extends ProjectPrerequisite<boolean> 
     await this._ensureDependenciesInstalledAsync();
 
     // Update the config
-    await updateTSConfigAsync({ tsConfigPath, isBootstrapping: intent.isBootstrapping });
+    await updateTSConfigAsync({ tsConfigPath });
 
     return true;
   }
@@ -56,12 +56,13 @@ export class TypeScriptProjectPrerequisite extends ProjectPrerequisite<boolean> 
     // Ensure TypeScript packages are installed
     await this._ensureDependenciesInstalledAsync({
       skipPrompt: true,
+      isProjectMutable: true,
     });
 
     const tsConfigPath = path.join(this.projectRoot, 'tsconfig.json');
 
     // Update the config
-    await updateTSConfigAsync({ tsConfigPath, isBootstrapping: true });
+    await updateTSConfigAsync({ tsConfigPath });
   }
 
   /** Exposed for testing. */
@@ -95,11 +96,17 @@ export class TypeScriptProjectPrerequisite extends ProjectPrerequisite<boolean> 
   async _ensureDependenciesInstalledAsync({
     exp,
     skipPrompt,
-  }: { exp?: ExpoConfig; skipPrompt?: boolean } = {}): Promise<boolean> {
+    isProjectMutable,
+  }: {
+    exp?: ExpoConfig;
+    skipPrompt?: boolean;
+    isProjectMutable?: boolean;
+  } = {}): Promise<boolean> {
     try {
       return await ensureDependenciesAsync(this.projectRoot, {
         exp,
         skipPrompt,
+        isProjectMutable,
         installMessage: `It looks like you're trying to use TypeScript but don't have the required dependencies installed.`,
         warningMessage:
           "If you're not using TypeScript, please remove the TypeScript files from your project",
