33a8285684f4e24cb38b2b2f094e90748874932e
Kudo Chien <kudo@expo.dev>
2023-08-19 15:45:03 +0800

    [core] fix missing fabric startup property (#24004)
    # Why
    
    fixes #23994
    close ENG-9778
    close ENG-9792
    
    # How
    
    the `RCTAppSetupUtils.h` was moved to `React-RCTAppDelegate` pod since react-native 0.72 but i didn't update the import path to the EXReactCompatibleHelper. we ultimately go to the `[RCTRootView alloc] initWithBridge:` call path which was for react-native 0.67. this pr also drops the react-native 0.67 support. in case we cannot find `RCTAppSetupDefaultRootView` in the future, we will be noticed by build errors.
    
    # Test Plan
    
    test #23994 repro

--- packages/expo-modules-core/ios/ReactDelegates/EXReactCompatibleHelpers.m
+++ packages/expo-modules-core/ios/ReactDelegates/EXReactCompatibleHelpers.m
@@ -4,22 +4,22 @@
 
 #import <React/RCTRootView.h>
 
-#if __has_include(<React/RCTAppSetupUtils.h>)
+#if __has_include(<React-RCTAppDelegate/RCTAppSetupUtils.h>)
+#import <React-RCTAppDelegate/RCTAppSetupUtils.h>
+#elif __has_include(<React_RCTAppDelegate/RCTAppSetupUtils.h>)
+// for importing the header from framework, the dash will be transformed to underscore
+#import <React_RCTAppDelegate/RCTAppSetupUtils.h>
+#else
+// react-native < 0.72
 #import <React/RCTAppSetupUtils.h>
 #endif
 
 UIView *EXAppSetupDefaultRootView(RCTBridge *bridge, NSString *moduleName, NSDictionary *initialProperties, BOOL fabricEnabled)
 {
-#if __has_include(<React/RCTAppSetupUtils.h>)
-
 #if REACT_NATIVE_MINOR_VERSION >= 71
   return RCTAppSetupDefaultRootView(bridge, moduleName, initialProperties, fabricEnabled);
 #else
   return RCTAppSetupDefaultRootView(bridge, moduleName, initialProperties);
 #endif // REACT_NATIVE_MINOR_VERSION >= 71
-
-#else
-  return [[RCTRootView alloc] initWithBridge:bridge moduleName:moduleName initialProperties:initialProperties];
-#endif // __has_include(<React/RCTAppSetupUtils.h>)
 }
 
