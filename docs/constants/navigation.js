5f90cec75daa36baf5706ad38f29bd0a14322dfb
Mark Lawlor <mwlawlor@gmail.com>
2023-08-21 22:55:54 +1000

    [expo-router] - add testing docs

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -198,6 +198,7 @@ const general = [
       makePage('router/reference/screen-tracking.mdx'),
       makePage('router/reference/src-directory.mdx'),
       makePage('router/reference/troubleshooting.mdx'),
+      makePage('router/reference/testing.mdx'),
       makePage('router/reference/faq.mdx'),
     ]),
     makeGroup('Migration', [makePage('router/migrate/from-react-navigation.mdx')]),
