d8a2ea6856aa3c3c7271db63e6f66fb25b70b5f1
Kudo Chien <kudo@expo.dev>
2023-08-22 00:32:31 +0800

    chore: update react-native to 0.72.4 (#24044)
    close ENG-9814
    
    - update react-native to 0.72.4
    - [react-native-lab] cherry pick native changes
    - [babel-preset-expo] bump `metro-react-native-babel-preset@0.76.8`
    - [android][ios] re-version sdk-49 code
    - [dev-menu][dev-launcher] rebuild js bundles
    
    versioned expo go + sdk 49 project smoke test

--- android/versioned-abis/expoview-abi49_0_0/src/main/java/abi49_0_0/expo/modules/filesystem/FileSystemModule.kt
+++ android/versioned-abis/expoview-abi49_0_0/src/main/java/abi49_0_0/expo/modules/filesystem/FileSystemModule.kt
@@ -284,7 +284,7 @@ open class FileSystemModule : Module() {
 
     AsyncFunction("copyAsync") { options: RelocatingOptions ->
       val fromUri = Uri.parse(slashifyFilePath(options.from))
-      ensurePermission(fromUri, Permission.WRITE, "Location '$fromUri' isn't movable.")
+      ensurePermission(fromUri, Permission.READ, "Location '$fromUri' isn't readable.")
       val toUri = Uri.parse(slashifyFilePath(options.to))
       ensurePermission(toUri, Permission.WRITE)
 
--- ios/versioned/sdk49/EXManifests/EXManifests/Manifest.swift
+++ ios/versioned/sdk49/EXManifests/EXManifests/Manifest.swift
@@ -3,8 +3,6 @@
 // this uses abstract class patterns
 // swiftlint:disable unavailable_function
 
-// swiftlint:disable type_body_length file_length
-
 import Foundation
 import UIKit
 
@@ -315,9 +313,8 @@ public class Manifest: NSObject {
       let sdkMajorVersion = expoGoSDKMajorVersion()
       if sdkMajorVersion > 0 && sdkMajorVersion < 48 {
         return "jsc"
-      } else {
-        return "hermes"
       }
+      return "hermes"
     }
     return jsEngine
   }
@@ -337,8 +334,14 @@ public class Manifest: NSObject {
           return nil
         }
         if let valueArray = value as? [Any],
-          let name = valueArray[0] as? String, let props = valueArray[1] as? [String: Any] {
-          return .withProps((name, props))
+          let name = valueArray[0] as? String {
+          if valueArray.count > 1 {
+            guard let props = valueArray[1] as? [String: Any] else {
+              return .withoutProps((name))
+            }
+            return .withProps((name, props))
+          }
+          return .withoutProps((name))
         }
         if let value = value as? String {
           return .withoutProps(value)
@@ -411,3 +414,5 @@ public class Manifest: NSObject {
     return nil
   }
 }
+
+// swiftlint:enable unavailable_function
