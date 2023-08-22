92eb3f868418e0be7f025843067149922a370772
aleqsio <mikucki@gmail.com>
2023-08-22 17:24:38 +0200

    Add sample mock for expo-crypto

---
+++ packages/expo-crypto/mocks/ExpoCrypto.js
@@ -0,0 +1,19 @@
+export function digestString(algorithm, str, options) {
+  return '';
+}
+export function getRandomBase64String(length) {
+  return '';
+}
+export function getRandomValues(array) {}
+
+export function digest(algorithm, output, data) {}
+
+export function randomUUID() {
+  return '';
+}
+export async function digestStringAsync(algorithm, str, options) {
+  return new Promise(() => '');
+}
+export async function getRandomBase64StringAsync(length) {
+  return new Promise(() => '');
+}
