aaa8033ff3429ed9c046a15b1cf5e22d59f7305c
Evan Bacon <bacon@expo.io>
2023-08-30 16:11:44 -0500

    cli(fix): prevent `expo config --json` from logging env var logs (#24192)
    # Why
    
    - fix #23325
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    - Mock the logs before and after the config evaluation.
    
    # Test Plan
    
    - Updated E2E tests to include an env var file and value.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/cli/src/config/configAsync.ts
+++ packages/@expo/cli/src/config/configAsync.ts
@@ -32,6 +32,18 @@ export function logConfig(config: ExpoConfig | ProjectConfig) {
 }
 
 export async function configAsync(projectRoot: string, options: Options) {
+  const loggingFunctions = {
+    log: console.log,
+    warn: console.warn,
+    error: console.error,
+  };
+  // Disable logging for this command if the user wants to get JSON output.
+  // This will ensure that only the JSON is printed to stdout.
+  if (options.json) {
+    console.log = function () {};
+    console.warn = function () {};
+    console.error = function () {};
+  }
   setNodeEnv('development');
   require('@expo/env').load(projectRoot);
 
@@ -87,6 +99,11 @@ export async function configAsync(projectRoot: string, options: Options) {
     logConfig(configOutput);
     Log.log();
   } else {
-    Log.log(JSON.stringify(configOutput));
+    process.stdout.write(JSON.stringify(configOutput));
+
+    // Re-enable logging functions for testing.
+    console.log = loggingFunctions.log;
+    console.warn = loggingFunctions.warn;
+    console.error = loggingFunctions.error;
   }
 }
