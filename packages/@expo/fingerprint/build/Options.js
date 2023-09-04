8340d33fdd5d3e3ceefe3639a6e34bc63bbc7388
Kudo Chien <kudo@expo.dev>
2023-09-04 23:29:14 +0800

    rebuild js

--- packages/@expo/fingerprint/build/Options.js
+++ packages/@expo/fingerprint/build/Options.js
@@ -3,21 +3,46 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.normalizeOptions = void 0;
+exports.normalizeOptionsAsync = exports.DEFAULT_IGNORES = exports.FINGERPRINT_IGNORE_FILENAME = void 0;
+const promises_1 = __importDefault(require("fs/promises"));
 const os_1 = __importDefault(require("os"));
-function normalizeOptions(options) {
+const path_1 = __importDefault(require("path"));
+exports.FINGERPRINT_IGNORE_FILENAME = '.fingerprintignore';
+exports.DEFAULT_IGNORES = [
+    exports.FINGERPRINT_IGNORE_FILENAME,
+    '**/android/build/**/*',
+    '**/android/app/build/**/*',
+    '**/android/app/.cxx/**/*',
+    '**/ios/Pods/**/*',
+];
+async function normalizeOptionsAsync(projectRoot, options) {
     return {
         ...options,
         platforms: options?.platforms ?? ['android', 'ios'],
         concurrentIoLimit: options?.concurrentIoLimit ?? os_1.default.cpus().length,
         hashAlgorithm: options?.hashAlgorithm ?? 'sha1',
-        dirExcludes: options?.dirExcludes ?? [
-            '**/android/build',
-            '**/android/app/build',
-            '**/android/app/.cxx',
-            'ios/Pods',
-        ],
+        ignores: await collectIgnoresAsync(projectRoot, options),
     };
 }
-exports.normalizeOptions = normalizeOptions;
+exports.normalizeOptionsAsync = normalizeOptionsAsync;
+async function collectIgnoresAsync(projectRoot, options) {
+    const ignores = [
+        ...exports.DEFAULT_IGNORES,
+        ...(options?.ignores ?? []),
+        ...(options?.dirExcludes?.map((dirExclude) => `${dirExclude}/**/*`) ?? []),
+    ];
+    const fingerprintIgnorePath = path_1.default.join(projectRoot, exports.FINGERPRINT_IGNORE_FILENAME);
+    try {
+        const fingerprintIgnore = await promises_1.default.readFile(fingerprintIgnorePath, 'utf8');
+        const fingerprintIgnoreLines = fingerprintIgnore.split('\n');
+        for (const line of fingerprintIgnoreLines) {
+            const trimmedLine = line.trim();
+            if (trimmedLine) {
+                ignores.push(trimmedLine);
+            }
+        }
+    }
+    catch { }
+    return ignores;
+}
 //# sourceMappingURL=Options.js.map
