f69191a2638983eb2698b3990f091357fa38aac5
Evan Bacon <bacon@expo.io>
2023-09-05 14:52:29 -0500

    feat(metro-runtime): Production bundle splitting runtime for web (#24291)
    # Why
    
    - Split out of the bundle splitting branch. These changes ensure that
    the code fetching end evaluation logic is included in production apps
    web apps.
    - Also reduces use of `react-native-web` for simplicity and sanity.
    
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/metro-runtime/src/LoadingView.ts
+++ packages/@expo/metro-runtime/src/LoadingView.ts
@@ -1,4 +1,11 @@
-import { SyntheticPlatformEmitter } from 'expo-modules-core';
+/**
+ * Copyright © 2023 650 Industries.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import SyntheticPlatformEmitter from 'expo-modules-core/build/SyntheticPlatformEmitter';
 
 // Ensure events are sent so custom Fast Refresh views are shown.
 function showMessage(message: string, type: 'load' | 'refresh') {
