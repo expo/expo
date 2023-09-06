0b9e7cd5a2695130b04f2ec118d2bf21d9cab3d3
Evan Bacon <bacon@expo.io>
2023-09-06 15:05:20 -0500

    chore!(expo-splash-screen): Delete deprecated `hide` and `preventAutoHide` methods. (#24296)
    # Why
    
    - These methods have been deprecated for years.
    - The default native module (web) mocks all methods, this means the
    unavailability errors cannot be thrown. As such, I dropped them.
    - Dropped the extra layers of re-exporting code. These add bundling time
    and not much else.
    - Dropped the tests as they weren't testing anything.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

---
+++ packages/expo-splash-screen/build/index.native.js
@@ -0,0 +1,3 @@
+import { requireNativeModule } from 'expo-modules-core';
+export const { preventAutoHideAsync, hideAsync } = requireNativeModule('ExpoSplashScreen');
+//# sourceMappingURL=index.native.js.map
