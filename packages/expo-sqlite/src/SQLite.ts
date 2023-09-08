113be9e069663531f87b5fb2629fb429e670d5c3
Kudo Chien <kudo@expo.dev>
2023-09-08 21:59:27 +0800

    [sqlite] only return successful result from executeSqlAsync (#24336)
    # Why
    
    only return successful `ResultSet` from `executeSqlAsync`
    fixes #23884
    
    # How
    
    as commented at https://github.com/expo/expo/issues/23884#issuecomment-1678448739, we should only return successful `ResultSet` from `executeSqlAsync`.
    
    # Test Plan
    
    update test-suite and make sure it passed on both ios and android

--- packages/expo-sqlite/src/SQLite.ts
+++ packages/expo-sqlite/src/SQLite.ts
@@ -264,14 +264,19 @@ export class ExpoSQLTransactionAsync implements SQLTransactionAsync {
     private readonly readOnly: boolean
   ) {}
 
-  async executeSqlAsync(
-    sqlStatement: string,
-    args?: (number | string)[]
-  ): Promise<ResultSetError | ResultSet> {
+  async executeSqlAsync(sqlStatement: string, args?: (number | string)[]): Promise<ResultSet> {
     const resultSets = await this.db.execAsync(
       [{ sql: sqlStatement, args: args ?? [] }],
       this.readOnly
     );
-    return resultSets[0];
+    const result = resultSets[0];
+    if (isResultSetError(result)) {
+      throw result.error;
+    }
+    return result;
   }
+}
+
+function isResultSetError(result: ResultSet | ResultSetError): result is ResultSetError {
+  return 'error' in result;
 }
