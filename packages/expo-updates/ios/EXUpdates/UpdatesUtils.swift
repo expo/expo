9d9e2da5d2f87918bfac015a22e2593d8b426df4
Douglas Lowder <doug@expo.io>
2023-09-11 13:13:34 -0700

    [expo-updates] Prevent failed updates in checkForUpdateAsync() (#24112)
    # Why
    
    If an update has a launch failure, our startup code detects the problem
    and launches the previous update or the embedded bundle instead. On
    future restarts of the app, the failed update will be ignored.
    
    However, the JS API (checkForUpdateAsync()) does not do the same check,
    as it is not running the full startup flow that includes an attempt at
    launch.
    
    # How
    
    If a new update is detected in checkForUpdateAsync(), we now search for
    it in the DB to see if it is already there, and if so, execute
    `shouldLaunchNewUpdate()` against that. Launch failures will cause this
    method to return false, and the update will be ignored as expected.
    
    In order for developers to know when this occurs, this PR includes a
    change to the `UpdateCheckResult` type returned by
    `checkForUpdateAsync()`. A new optional `reason` property is now
    included in the result, so that this and other possible conditions are
    surfaced to the calling JS application code. At the moment, we are not
    adding the message to the state machine context, but only in
    `UpdateCheckResult`.
    
    # Test Plan
    
    - Manual testing with the JS API
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>

--- packages/expo-updates/ios/EXUpdates/UpdatesUtils.swift
+++ packages/expo-updates/ios/EXUpdates/UpdatesUtils.swift
@@ -99,13 +99,19 @@ public final class UpdatesUtils: NSObject {
             return
           case let rollBackUpdateDirective as RollBackToEmbeddedUpdateDirective:
             if !constants.config.hasEmbeddedUpdate {
-              block([:])
+              let reason = RemoteCheckResultNotAvailableReason.rollbackNoEmbedded
+              block([
+                "reason": "\(reason)"
+              ])
               sendStateEvent(UpdatesStateEventCheckComplete())
               return
             }
 
             guard let embeddedManifest = EmbeddedAppLoader.embeddedManifest(withConfig: constants.config, database: constants.database) else {
-              block([:])
+              let reason = RemoteCheckResultNotAvailableReason.rollbackNoEmbedded
+              block([
+                "reason": "\(reason)"
+              ])
               sendStateEvent(UpdatesStateEventCheckComplete())
               return
             }
@@ -116,7 +122,10 @@ public final class UpdatesUtils: NSObject {
               launchedUpdate: launchedUpdate,
               filters: manifestFilters
             ) {
-              block([:])
+              let reason = RemoteCheckResultNotAvailableReason.rollbackRejectedBySelectionPolicy
+              block([
+                "reason": "\(reason)"
+              ])
               sendStateEvent(UpdatesStateEventCheckComplete())
               return
             }
@@ -136,22 +145,50 @@ public final class UpdatesUtils: NSObject {
         }
 
         guard let update = updateResponse.manifestUpdateResponsePart?.updateManifest else {
-          block([:])
+          let reason = RemoteCheckResultNotAvailableReason.noUpdateAvailableOnServer
+          block([
+            "reason": "\(reason)"
+          ])
           sendStateEvent(UpdatesStateEventCheckComplete())
           return
         }
 
+        var shouldLaunch = false
+        var failedPreviously = false
         if constants.selectionPolicy.shouldLoadNewUpdate(
           update,
           withLaunchedUpdate: launchedUpdate,
           filters: manifestFilters
         ) {
+          // If "update" has failed to launch previously, then
+          // "launchedUpdate" will be an earlier update, and the test above
+          // will return true (incorrectly).
+          // We check to see if the new update is already in the DB, and if so,
+          // only allow the update if it has had no launch failures.
+          shouldLaunch = true
+          constants.database.databaseQueue.sync {
+            do {
+              let storedUpdate = try constants.database.update(withId: update.updateId, config: constants.config)
+              if let storedUpdate = storedUpdate {
+                shouldLaunch = storedUpdate.failedLaunchCount == 0 || storedUpdate.successfulLaunchCount > 0
+                failedPreviously = !shouldLaunch
+                AppController.sharedInstance.logger.info(message: "Stored update found: ID = \(update.updateId), failureCount = \(storedUpdate.failedLaunchCount)")
+              }
+            } catch {}
+          }
+        }
+        if shouldLaunch {
           block([
             "manifest": update.manifest.rawManifestJSON()
           ])
           sendStateEvent(UpdatesStateEventCheckCompleteWithUpdate(manifest: update.manifest.rawManifestJSON()))
         } else {
-          block([:])
+          let reason = failedPreviously ?
+            RemoteCheckResultNotAvailableReason.updatePreviouslyFailed :
+            RemoteCheckResultNotAvailableReason.updateRejectedBySelectionPolicy
+          block([
+            "reason": "\(reason)"
+          ])
           sendStateEvent(UpdatesStateEventCheckComplete())
         }
       } errorBlock: { error in
