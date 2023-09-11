3df67b592edb4c1e407c4359e660d96788dc3f99
aleqsio <mikucki@gmail.com>
2023-09-11 17:39:46 +0200

    [expo-module-scripts] Generate expo modules structure and mocks from swift (#24064)

---
+++ packages/expo-modules-test-core/build/getStructure.d.ts
@@ -0,0 +1,3 @@
+import { OutputModuleDefinition } from './types';
+export declare function getAllExpoModulesInWorkingDirectory(): OutputModuleDefinition[];
+//# sourceMappingURL=getStructure.d.ts.map
