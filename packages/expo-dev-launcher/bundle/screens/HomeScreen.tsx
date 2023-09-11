ce8f35df0e2d4a06e63b82dbe2e69da054f40120
Gabriel Donadel <donadeldev@gmail.com>
2023-09-11 12:04:49 -0300

    [dev-launcher] Fetch dev sessions whenever navigating to Home screen

--- packages/expo-dev-launcher/bundle/screens/HomeScreen.tsx
+++ packages/expo-dev-launcher/bundle/screens/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import { useFocusEffect } from '@react-navigation/core';
 import {
   Heading,
   Text,
@@ -37,18 +38,11 @@ import { useUpdatesConfig } from '../providers/UpdatesConfigProvider';
 import { DevSession } from '../types';
 
 export type HomeScreenProps = {
-  fetchOnMount?: boolean;
-  pollInterval?: number;
   pollAmount?: number;
   navigation?: any;
 };
 
-export function HomeScreen({
-  fetchOnMount = false,
-  pollInterval = 1000,
-  pollAmount = 5,
-  navigation,
-}: HomeScreenProps) {
+export function HomeScreen({ pollAmount = 5, navigation }: HomeScreenProps) {
   const modalStack = useModalStack();
   const [inputValue, setInputValue] = React.useState('');
   const [loadingUrl, setLoadingUrl] = React.useState('');
@@ -59,13 +53,14 @@ export function HomeScreen({
 
   const crashReport = useCrashReport();
 
-  const initialDevSessionData = React.useRef(devSessions);
-
-  React.useEffect(() => {
-    if (initialDevSessionData.current.length === 0 && fetchOnMount) {
-      pollAsync({ pollAmount, pollInterval });
-    }
-  }, [fetchOnMount, pollInterval, pollAmount, pollAsync]);
+  const hasDevSessions = devSessions?.length > 0;
+  useFocusEffect(
+    React.useCallback(() => {
+      if (!hasDevSessions) {
+        pollAsync({ pollAmount });
+      }
+    }, [hasDevSessions, pollAmount])
+  );
 
   const onLoadUrl = async (url: string) => {
     setLoadingUrl(url);
@@ -87,7 +82,7 @@ export function HomeScreen({
   };
 
   const onRefetchPress = () => {
-    pollAsync({ pollAmount, pollInterval });
+    pollAsync({ pollAmount });
   };
 
   const onRecentAppPress = async (app: RecentApp) => {
