477f8ff2c6013d2b2d08e19eaa8e4233102880fe
Alan Hughes <hughesno1@gmail.com>
2023-09-11 16:54:35 +0100

    [ios][blur] support more effects

--- apps/native-component-list/src/screens/BlurView/BlurViewScreen.tsx
+++ apps/native-component-list/src/screens/BlurView/BlurViewScreen.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, Platform } from 'react-native';
 
 import BlurViewWithControls from './BlurViewWithControls';
+
+const blurStyles =
+  Platform.OS === 'ios'
+    ? ([
+        'light',
+        'dark',
+        'default',
+        'extraLight',
+        'regular',
+        'prominent',
+        'systemUltraThinMaterial',
+        'systemThinMaterial',
+        'systemMaterial',
+        'systemThickMaterial',
+        'systemChromeMaterial',
+        'systemUltraThinMaterialLight',
+        'systemThinMaterialLight',
+        'systemMaterialLight',
+        'systemThickMaterialLight',
+        'systemChromeMaterialLight',
+        'systemUltraThinMaterialDark',
+        'systemThinMaterialDark',
+        'systemMaterialDark',
+        'systemThickMaterialDark',
+        'systemChromeMaterialDark',
+      ] as const)
+    : (['light', 'dark', 'default'] as const);
 
 export default function BlurViewScreen() {
   return (
     <ScrollView style={styles.container}>
-      {(['default', 'light', 'dark'] as const).map((tint) => (
+      {blurStyles.map((tint) => (
         <BlurViewWithControls key={tint} tint={tint} />
       ))}
     </ScrollView>
---
+++ packages/expo-blur/android/src/main/java/expo/modules/blur/TintStyle.kt
@@ -0,0 +1,9 @@
+package expo.modules.blur
+
+import expo.modules.kotlin.types.Enumerable
+
+enum class TintStyle(val value: String): Enumerable {
+  LIGHT("light"),
+  DARK("dark"),
+  DEFAULT("default")
+}
