c03a45d96a950d42542365abb3038efb14eb4027
evanbacon <bacon@expo.io>
2023-09-11 17:49:21 -0700

    update regex

--- packages/expo-router/_ctx.js
+++ packages/expo-router/_ctx.js
@@ -2,5 +2,5 @@ export const ctx = require.context(
   process.env.EXPO_ROUTER_APP_ROOT,
   true,
   // Ignore root `./+html.js` and API route files `./generate+api.tsx`.
-  /^(?:\.\/)(?!(.*\+api)|\+html\.[tj]sx?$).*\.[tj]sx?$/
+  /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+html)))\.[tj]sx?$).*\.[tj]sx?$/
 );
