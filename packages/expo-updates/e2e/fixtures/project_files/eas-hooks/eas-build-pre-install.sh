dee0a0bf9b3e40a2942dd7d2ff43af626da10d99
Douglas Lowder <douglowder@mac.com>
2023-09-12 12:59:28 -0700

    Support TV in updates E2E

--- packages/expo-updates/e2e/fixtures/project_files/eas-hooks/eas-build-pre-install.sh
+++ packages/expo-updates/e2e/fixtures/project_files/eas-hooks/eas-build-pre-install.sh
@@ -39,5 +39,6 @@ if [[ "$EAS_BUILD_RUNNER" == "eas-build" && "$EAS_BUILD_PROFILE" == "updates_tes
   else
     brew tap wix/brew
     brew install applesimutils
+    # xcodebuild -downloadPlatform tvOS
   fi
 fi
--- packages/expo-updates/e2e/setup/create-updates-test.js
+++ packages/expo-updates/e2e/setup/create-updates-test.js
@@ -22,7 +22,7 @@ function transformAppJson(appJson, projectName, runtimeVersion) {
       ...appJson.expo,
       name: projectName,
       runtimeVersion,
-      jsEngine: 'jsc',
+      // jsEngine: 'jsc',
       updates: {
         ...appJson.expo.updates,
         requestHeaders: {
