54fae676eb59bc16782a2c1081db4e10b3076801
Cedric van Putten <github@cedric.dev>
2023-09-12 16:27:06 +0200

    refactor(create-expo): split up jest normal and e2e tests

--- packages/create-expo/e2e/jest.config.js
+++ packages/create-expo/e2e/jest.config.js
@@ -7,6 +7,6 @@ module.exports = {
   testRegex: '/__tests__/.*(test|spec)\\.[jt]sx?$',
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
   rootDir: path.resolve(__dirname),
-  displayName: require('./package').name,
-  roots: ['./src'],
+  displayName: require('../package').name,
+  roots: ['.'],
 };
