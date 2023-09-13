6d66b7735c44f43caee75c2064f12cadd7de0149
evanbacon <bacon@expo.io>
2023-09-13 11:50:56 -0700

    update test

--- apps/router-e2e/app.config.js
+++ apps/router-e2e/app.config.js
@@ -27,7 +27,7 @@ module.exports = {
     typedRoutes: true,
   },
   web: {
-    output: process.env.EXPO_USE_STATIC,
+    output: process.env.EXPO_USE_STATIC ?? 'static',
     bundler: 'metro',
   },
   plugins: [
