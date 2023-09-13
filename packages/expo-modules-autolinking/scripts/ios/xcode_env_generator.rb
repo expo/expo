d61d1380c184431fe201530a80ef06574c08746b
Douglas Lowder <doug@expo.io>
2023-09-12 17:51:11 -0700

    Apply suggestions from code review
    Co-authored-by: James Ide <ide@users.noreply.github.com>

--- packages/expo-modules-autolinking/scripts/ios/xcode_env_generator.rb
+++ packages/expo-modules-autolinking/scripts/ios/xcode_env_generator.rb
@@ -5,13 +5,19 @@ def generate_or_remove_xcode_env_updates_file!()
   project_directory = Pod::Config.instance.project_root
   xcode_env_file = File.join(project_directory, '.xcode.env.updates')
 
-  ex_updates_native_debug = ENV['EX_UPDATES_NATIVE_DEBUG'] == '1'
+  ex_updates_native_debug = ENV['EX_UPDATES_NATIVE_DEBUG'] == '1' ||
+                              ENV['EX_UPDATES_NATIVE_DEBUG'] == 'true'
   if ex_updates_native_debug
     Pod::UI.info "EX_UPDATES_NATIVE_DEBUG is set; auto-generating `.xcode.env.updates` to disable packager and generate debug bundle"
     if File.exist?(xcode_env_file)
       File.delete(xcode_env_file)
     end
-    File.write(xcode_env_file, "export FORCE_BUNDLING=1\nunset SKIP_BUNDLING\nexport RCT_NO_LAUNCH_PACKAGER=1\n")
+    File.write(xcode_env_file, <<~EOS
+      export FORCE_BUNDLING=1
+      unset SKIP_BUNDLING
+      export RCT_NO_LAUNCH_PACKAGER=1
+EOS
+    )
   else
     if File.exist?(xcode_env_file)
       Pod::UI.info "EX_UPDATES_NATIVE_DEBUG has been unset; removing `.xcode.env.updates`"
