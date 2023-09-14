6e021b288405021f8ece9507400b78a168b04b17
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2023-09-14 19:28:49 +0200

    [core] Add native functions for UUIDv4 generation (#24199)
    # Why
    
    For Android and iOS we want to use native UUIDv4 generation methods.
    
    # How
    
    Exposed the common methods using the `CoreModule` file. 
    Changed the way uuids are exported. Now `expo-modules-core` exports a
    `uuid` object which contains `v4` function.
    
    # Test Plan
    
    Tested in BareExpo on Android, ios and web

---
+++ packages/expo-modules-core/src/uuid/uuid.types.ts
@@ -0,0 +1,18 @@
+/**
+ * Collection of utilities used for generating Universally Unique Identifiers.
+ */
+export type UUID = {
+  /**
+   * A UUID generated randomly.
+   */
+  v4: () => string;
+  /**
+   * A UUID generated based on the `value` and `namespace` parameters, which always produces the same result for the same inputs.
+   */
+  v5: (
+    value: number[] | string,
+    namespace: number[] | string,
+    buf?: number[],
+    offset?: number
+  ) => string;
+};
