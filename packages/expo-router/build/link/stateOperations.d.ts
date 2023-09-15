e3bfd68301bce370a3266126b8a755ee52b8e553
Mark Lawlor <mwlawlor@gmail.com>
2023-09-15 17:43:00 +1000

    build files

--- packages/expo-router/build/link/stateOperations.d.ts
+++ packages/expo-router/build/link/stateOperations.d.ts
@@ -1,5 +1,4 @@
-import { InitialState, NavigationState, ParamListBase, PartialState, getActionFromState } from '@react-navigation/native';
-import { ResultState } from '../fork/getStateFromPath';
+import { getActionFromState } from '@react-navigation/native';
 export type NavigateAction = Extract<ReturnType<typeof getActionFromState>, {
     type: 'NAVIGATE';
 }> & {
@@ -12,70 +11,4 @@ export type NavigateActionParams = {
     screen: string;
     name?: string;
 };
-/** Return the absolute last route to move to. */
-export declare function findTopRouteForTarget(state: ResultState): Omit<import("@react-navigation/native").Route<string, object | undefined>, "key"> & {
-    state?: Readonly<Partial<Omit<Readonly<{
-        key: string;
-        index: number;
-        routeNames: string[];
-        history?: unknown[] | undefined;
-        routes: (Readonly<{
-            key: string;
-            name: string;
-            path?: string | undefined;
-        }> & Readonly<{
-            params?: Readonly<object | undefined>;
-        }> & {
-            state?: Readonly<any> | PartialState<Readonly<any>> | undefined;
-        })[];
-        type: string;
-        stale: false;
-    }>, "stale" | "routes">> & {
-        routes: (Omit<import("@react-navigation/native").Route<string, object | undefined>, "key"> & any)[];
-    }> | undefined;
-};
-/** @returns true if moving to a sibling inside the same navigator. */
-export declare function isMovingToSiblingRoute(currentState: NavigationState | PartialState<NavigationState> | undefined, targetState: ResultState | undefined): boolean;
-export declare function getQualifiedStateForTopOfTargetState(rootState: InitialState, targetState: ResultState): Readonly<Partial<Omit<Readonly<{
-    key: string;
-    index: number;
-    routeNames: string[];
-    history?: unknown[] | undefined;
-    routes: (Readonly<{
-        key: string;
-        name: string;
-        path?: string | undefined;
-    }> & Readonly<{
-        params?: Readonly<object | undefined>;
-    }> & {
-        state?: Readonly<any> | PartialState<Readonly<any>> | undefined;
-    })[];
-    type: string;
-    stale: false;
-}>, "stale" | "routes">> & {
-    routes: (Omit<import("@react-navigation/native").Route<string, object | undefined>, "key"> & {
-        state?: Readonly<Partial<Omit<Readonly<{
-            key: string;
-            index: number;
-            routeNames: string[];
-            history?: unknown[] | undefined;
-            routes: (Readonly<{
-                key: string;
-                name: string;
-                path?: string | undefined;
-            }> & Readonly<{
-                params?: Readonly<object | undefined>;
-            }> & {
-                state?: Readonly<any> | PartialState<Readonly<any>> | undefined;
-            })[];
-            type: string;
-            stale: false;
-        }>, "stale" | "routes">> & any> | undefined;
-    })[];
-}>;
-export declare function getEarliestMismatchedRoute<T extends ParamListBase>(rootState: NavigationState<T> | undefined, actionParams: NavigateActionParams): {
-    name: string;
-    params?: any;
-    type?: string;
-} | null;
 //# sourceMappingURL=stateOperations.d.ts.map
