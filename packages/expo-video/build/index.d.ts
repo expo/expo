b25ace3f25cf515e956d05823c42cdf788efd33e
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-09-17 00:25:18 +0200

    [iOS] Experimental new Video component (#24428)

---
+++ packages/expo-video/build/index.d.ts
@@ -0,0 +1,4 @@
+import Video from './NativeVideoModule';
+export { VideoView, useVideoPlayer } from './VideoView';
+export { Video };
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/expo-video/ios/VideoPlayer.swift
@@ -0,0 +1,6 @@
+// Copyright 2023-present 650 Industries. All rights reserved.
+
+import AVFoundation
+import ExpoModulesCore
+
+internal final class VideoPlayer: SharedRef<AVPlayer> {}
