1812128c095185475af0567f063c63b9662c0f91
Mark Lawlor <mwlawlor@gmail.com>
2023-09-18 16:02:31 +1000

    fix(router): Handle deep dynamic route colliding with passed query parameter

--- packages/expo-router/src/fork/getStateFromPath.ts
+++ packages/expo-router/src/fork/getStateFromPath.ts
@@ -747,10 +747,18 @@ const createNestedStateObject = (
   const params = parseQueryParams(route.path, findParseConfigForRoute(route.name, routeConfigs));
 
   if (params) {
-    const resolvedParams = { ...route.params, ...params };
-    if (Object.keys(resolvedParams).length > 0) {
-      route.params = resolvedParams;
-    } else {
+    route.params ??= {};
+    for (const [name, value] of Object.entries(params)) {
+      if (!route.params?.[name]) {
+        route.params[name] = value;
+      } else if (process.env.NODE_ENV !== 'production') {
+        console.warn(
+          `Route '/${route.name}' with param '${name}' was specified both in the path and as a param, removing from path`
+        );
+      }
+    }
+
+    if (Object.keys(route.params).length === 0) {
       delete route.params;
     }
   }
