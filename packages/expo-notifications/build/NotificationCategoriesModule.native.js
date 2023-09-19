6a533bad52cf3ddbd8ec4f050a6a9865590e0911
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2023-09-19 11:15:41 +0200

    [notifications] Use `requireNativeModule` instead of `NativeModulesProxy` (#24495)
    # Why
    
    Uses `requireNativeModule` instead of the `NativeModulesProxy`. 
    
    # Test Plan
    
    - bare-expo ✅

--- packages/expo-notifications/build/NotificationCategoriesModule.native.js
+++ packages/expo-notifications/build/NotificationCategoriesModule.native.js
@@ -1,3 +1,3 @@
-import { NativeModulesProxy } from 'expo-modules-core';
-export default NativeModulesProxy.ExpoNotificationCategoriesModule;
+import { requireNativeModule } from 'expo-modules-core';
+export default requireNativeModule('ExpoNotificationCategoriesModule');
 //# sourceMappingURL=NotificationCategoriesModule.native.js.map
