49d160957f5ee4b148a9a853d66f61d44b2c1538
Mark Lawlor <mwlawlor@gmail.com>
2023-09-20 07:45:11 +1000

    fix(router): fix router.replace() navigation across nested navigators (#24457)
    # Why
    
    Fix ENG-10061
    Fix https://github.com/expo/router/issues/793
    Fix https://github.com/expo/router/issues/858
    Fix https://github.com/expo/router/issues/832
    
    The `linkTo` function has two issues.
    
    * It has trouble navigating to screens in different navigators with
    identical names
    * it cannot perform a replace navigation while in a nested navigator (it
    can only replace at the currently level).
    
    # How
    
    The original plan was to change `linkTo` to do navigation by
    deterministic keys, but React Navigation 7.0 is removing key based
    navigation, so we'll keep with screen names.
    
    This PR rewrites the `linkTo` function and simplifies the logic into two
    paths.
    
    **push**
    
    `linkTo` will fire a dispatch action on the root navigator with the
    desired state.
    
    **replace**
    
    We compare the current navigation state vs the desired state and find
    the lowest common navigator that can perform a replace action. This
    would be the `<Stack/>` or `<Tabs />` navigator that the two states
    share in common. The common navigator (it could be the root navigator if
    they have nothing in common!) is the source of the replace event
    
    # Test Plan
    
    Manually tested and wrote a new test.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-router/src/__tests__/smoke.test.tsx
+++ packages/expo-router/src/__tests__/smoke.test.tsx
@@ -1,10 +1,10 @@
 import React, { Text } from 'react-native';
 
-import { Slot, useGlobalSearchParams } from '../exports';
+import { Slot, router, useGlobalSearchParams } from '../exports';
 import { Stack } from '../layouts/Stack';
 import { Tabs } from '../layouts/Tabs';
 import { Redirect } from '../link/Link';
-import { renderRouter, screen } from '../testing-library';
+import { act, renderRouter, screen } from '../testing-library';
 
 it('404', async () => {
   const Index = jest.fn(() => <Redirect href="/404" />);
@@ -122,4 +122,48 @@ it('nested layouts', async () => {
   expect(Index).toHaveBeenCalledTimes(1);
   expect(Home).toHaveBeenCalledTimes(1);
   expect(HomeNested).toHaveBeenCalledTimes(1);
+});
+
+it('deep linking nested groups', async () => {
+  const RootLayout = jest.fn(() => <Slot />);
+  const AppLayout = jest.fn(() => <Stack />);
+  const TabsLayout = jest.fn(() => <Tabs />);
+  const HomeLayout = jest.fn(() => <Stack />);
+
+  const Home = jest.fn(() => <Text testID="Home" />);
+
+  const OtherTabsLayout = jest.fn(() => <Stack />);
+  const NestedTabsLayout = jest.fn(() => <Tabs />);
+  const OtherTabsIndex = jest.fn(() => <Text testID="OtherTabsHome" />);
+
+  renderRouter(
+    {
+      _layout: RootLayout,
+      '(app)/_layout': AppLayout,
+      '(app)/(tabs)/_layout': TabsLayout,
+      '(app)/(tabs)/home/_layout': HomeLayout,
+      '(app)/(tabs)/home/index': Home,
+      '(app)/(other_tabs)/_layout': OtherTabsLayout,
+      '(app)/(other_tabs)/(nested_tabs)/_layout': NestedTabsLayout,
+      '(app)/(other_tabs)/(nested_tabs)/home/index': OtherTabsIndex,
+    },
+    {
+      initialUrl: '/(app)/(other_tabs)/(nested_tabs)/home',
+    }
+  );
+
+  // Start in a deeply nested navigator
+  expect(await screen.getByTestId('OtherTabsHome')).toBeOnTheScreen();
+
+  act(() => router.replace('/(app)/(tabs)/home'));
+
+  expect(await screen.getByTestId('Home')).toBeOnTheScreen();
+
+  expect(RootLayout).toHaveBeenCalledTimes(1);
+  expect(AppLayout).toHaveBeenCalledTimes(2);
+  expect(TabsLayout).toHaveBeenCalledTimes(1);
+  expect(HomeLayout).toHaveBeenCalledTimes(1);
+  expect(OtherTabsLayout).toHaveBeenCalledTimes(1);
+  expect(NestedTabsLayout).toHaveBeenCalledTimes(1);
+  expect(OtherTabsIndex).toHaveBeenCalledTimes(1);
 });
