0f07c0f8cc819c79f0397bd53042746b8f4fbf0d
Gabriel Donadel <donadeldev@gmail.com>
2023-09-25 13:27:08 -0300

    [dev-launcher][ios] Launch directly into previously opened project

--- packages/expo-dev-launcher/ios/EXDevLauncherController.m
+++ packages/expo-dev-launcher/ios/EXDevLauncherController.m
@@ -57,6 +57,7 @@
 @property (nonatomic, strong) EXDevLauncherNetworkInterceptor *networkInterceptor;
 @property (nonatomic, assign) BOOL isStarted;
 @property (nonatomic, strong) EXDevLauncherBridgeDelegate *bridgeDelegate;
+@property (nonatomic, strong) NSURL *lastOpenedAppUrl;
 
 @end
 
@@ -249,18 +250,37 @@
   _window = window;
   EXDevLauncherUncaughtExceptionHandler.isInstalled = true;
 
-  if (!launchOptions[UIApplicationLaunchOptionsURLKey]) {
-    [self navigateToLauncher];
-  } else {
-    // For deeplink launch, we need the keyWindow for expo-splash-screen to setup correctly.
-    [_window makeKeyWindow];
+  if (launchOptions[UIApplicationLaunchOptionsURLKey]) {
+      // For deeplink launch, we need the keyWindow for expo-splash-screen to setup correctly.
+      [_window makeKeyWindow];
+      return;
   }
+
+  if(_lastOpenedAppUrl != nil){
+      [self loadApp:_lastOpenedAppUrl withProjectUrl:nil onSuccess:nil onError:^(NSError *error) {
+          __weak typeof(self) weakSelf = self;
+          dispatch_async(dispatch_get_main_queue(), ^{
+              typeof(self) self = weakSelf;
+              if (!self) {
+                  return;
+              }
+
+              [self navigateToLauncher];
+          });
+      }];
+      return;
+  }
+  [self navigateToLauncher];
 }
 
 - (void)autoSetupPrepare:(id<EXDevLauncherControllerDelegate>)delegate launchOptions:(NSDictionary * _Nullable)launchOptions
 {
   _delegate = delegate;
   _launchOptions = launchOptions;
+  NSDictionary *lastOpenedApp = [self.recentlyOpenedAppsRegistry mostRecentApp];
+  if(lastOpenedApp != nil){
+    _lastOpenedAppUrl = [NSURL URLWithString:lastOpenedApp[@"url"]];
+  }
   EXDevLauncherBundleURLProviderInterceptor.isInstalled = true;
 }
 
