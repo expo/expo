24a4dc8bb017096c10a0d85b1808b5b97ed94f3f
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2023-09-28 09:59:59 +0100

    [ios][calendar] fix: add os check (#24655)

--- packages/expo-calendar/ios/EXCalendar/EXRemindersPermissionRequester.m
+++ packages/expo-calendar/ios/EXCalendar/EXRemindersPermissionRequester.m
@@ -53,15 +53,27 @@
 {
   EKEventStore *eventStore = [[EKEventStore alloc] init];
   EX_WEAKIFY(self)
-#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 170000
-  [eventStore requestFullAccessToRemindersWithCompletion:^(BOOL granted, NSError * _Nullable error) {
-    EX_STRONGIFY(self)
-    if (error && error.code != 100) {
-      reject(@"E_CALENDAR_ERROR_UNKNOWN", error.localizedDescription, error);
-    } else {
-      resolve([self getPermissions]);
-    }
-  }];
+#if defined(__IPHONE_17_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_17_0
+  if (@available(iOS 17.0, *)) {
+    [eventStore requestFullAccessToRemindersWithCompletion:^(BOOL granted, NSError * _Nullable error) {
+      EX_STRONGIFY(self)
+      if (error && error.code != 100) {
+        reject(@"E_CALENDAR_ERROR_UNKNOWN", error.localizedDescription, error);
+      } else {
+        resolve([self getPermissions]);
+      }
+    }];
+  } else {
+    [eventStore requestAccessToEntityType:EKEntityTypeReminder completion:^(BOOL granted, NSError *error) {
+      EX_STRONGIFY(self)
+      // Error code 100 is a when the user denies permission; in that case we don't want to reject.
+      if (error && error.code != 100) {
+        reject(@"E_REMINDERS_ERROR_UNKNOWN", error.localizedDescription, error);
+      } else {
+        resolve([self getPermissions]);
+      }
+    }];
+  }
 #else
   [eventStore requestAccessToEntityType:EKEntityTypeReminder completion:^(BOOL granted, NSError *error) {
     EX_STRONGIFY(self)
