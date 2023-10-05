dd5f215b199d659f025c2af7b28d581442300186
wojciechdrozdz <wojciech.drozdz@swmansion.com>
2023-10-05 12:32:06 +0200

    Update the tests

--- packages/expo-blur/android/src/main/java/expo/modules/blur/enums/BlurMethod.kt
+++ packages/expo-blur/android/src/main/java/expo/modules/blur/enums/BlurMethod.kt
@@ -2,7 +2,7 @@ package expo.modules.blur.enums
 
 import expo.modules.kotlin.types.Enumerable
 
-enum class BlurMethod(val method: String) : Enumerable {
+enum class BlurMethod(val value: String) : Enumerable {
   NONE("none"),
   DIMEZIS_BLUR_VIEW("dimezisBlurView")
 }
