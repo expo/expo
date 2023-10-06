11a5de1d317c7075da7030c538170d180aa6b950
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2023-10-06 13:49:17 +0200

    [core] Add native functions for UUIDv5 generation (#24200)
    # Why
    
    We want to generate UUIDv5 natively and be able to use that
    functionality in JS without importing additional modules.
    
    # How
    
    Created native implementations of the UUIDv5 algorithm (platforms don't
    provide it by default) and exposed them to JS with to
    `expo-modules-core`
    
    # Test Plan
    
    Tested in BareExpo on Android, iOS and web. UUIDs are consistent on all
    platforms for the same arguments.

--- packages/expo-modules-core/build/uuid/v5.js
+++
@@ -1,7 +0,0 @@
-import sha1 from './lib/sha1';
-import v35 from './lib/v35';
-/**
- * DO NOT USE this function in security-sensitive contexts.
- */
-export default v35('v5', 0x50, sha1);
-//# sourceMappingURL=v5.js.map
