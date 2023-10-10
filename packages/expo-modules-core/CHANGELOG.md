ea3b540754cb44e0bd7512406c43c39022ceddfe
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-10-10 09:47:41 -0700

    [core] Ensure native modules are installed before required (#24746)

--- packages/expo-modules-core/CHANGELOG.md
+++ packages/expo-modules-core/CHANGELOG.md
@@ -16,6 +16,7 @@
 
 - Improve tracking on Android. ([#24625](https://github.com/expo/expo/pull/24625) by [@lukmccall](https://github.com/lukmccall))
 - Use native UUIDv5 generation on `Android` and `iOS`. ([#24200](https://github.com/expo/expo/pull/24200) by [@behenate](https://github.com/behenate))
+- Ensure native modules are installed before required. ([#24746](https://github.com/expo/expo/pull/24746) by [@tsapeta](https://github.com/tsapeta))
 
 ## 1.8.0 — 2023-09-15
 
--- packages/expo-modules-core/src/requireNativeModule.ts
+++ packages/expo-modules-core/src/requireNativeModule.ts
@@ -1,3 +1,5 @@
+import { NativeModules } from 'react-native';
+
 import NativeModulesProxy from './NativeModulesProxy';
 
 type ExpoObject = {
@@ -53,10 +55,30 @@ export function requireNativeModule<ModuleType = any>(moduleName: string): Modul
 export function requireOptionalNativeModule<ModuleType = any>(
   moduleName: string
 ): ModuleType | null {
+  ensureNativeModulesAreInstalled();
+
   return (
     globalThis.expo?.modules?.[moduleName] ??
     globalThis.ExpoModules?.[moduleName] ??
     NativeModulesProxy[moduleName] ??
     null
   );
+}
+
+/**
+ * Ensures that the native modules are installed in the current runtime.
+ * Otherwise, it synchronously calls a native function that installs them.
+ */
+function ensureNativeModulesAreInstalled(): void {
+  if (globalThis.expo) {
+    return;
+  }
+  try {
+    // TODO: ExpoModulesCore shouldn't be optional here,
+    // but to keep backwards compatibility let's just ignore it in SDK 50.
+    // In most cases the modules were already installed from the native side.
+    NativeModules.ExpoModulesCore?.installModules();
+  } catch (error) {
+    console.error(`Unable to install Expo modules: ${error}`);
+  }
 }
