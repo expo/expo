1979837c600d4569ea2b24ed68cbb05b41846ebd
aleqsio <mikucki@gmail.com>
2023-10-11 04:37:40 -0700

    Add improvements to mockgen (#24762)
    Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

--- packages/expo-modules-test-core/build/mockgen.d.ts
+++ packages/expo-modules-test-core/build/mockgen.d.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import { OutputModuleDefinition } from './types';
-export declare function generateMocks(modules: OutputModuleDefinition[]): Promise<void>;
+export declare function generateMocks(modules: OutputModuleDefinition[], outputLanguage?: 'javascript' | 'typescript'): Promise<void>;
 //# sourceMappingURL=mockgen.d.ts.map
