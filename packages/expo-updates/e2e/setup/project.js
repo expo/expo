56a2472ddea5d40b67e43400aa3d94cbea107211
Douglas Lowder <douglowder@mac.com>
2023-10-13 16:40:05 -0700

    [expo-updates][iOS] Fix Updates E2E CI
    - Attempt to fix by running Detox headless on iOS
    - Remove outdated Detox config file
    - Reenable iOS in GitHub workflow

--- packages/expo-updates/e2e/setup/project.js
+++ packages/expo-updates/e2e/setup/project.js
@@ -123,7 +123,6 @@ async function copyCommonFixturesToProject(projectRoot, { appJsFileName, repoRoo
       projectFilesTarballPath,
       'tsconfig.json',
       '.detoxrc.json',
-      'detox.config.js',
       'eas.json',
       'eas-hooks',
       'e2e',
