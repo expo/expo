8faf61c649adfe362f1c70360a9b74bed77be078
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2023-10-17 16:24:41 +0200

    [ios][application] Migrate to Expo Modules API (#24871)

--- packages/expo-application/build/Application.js
+++ packages/expo-application/build/Application.js
@@ -1,4 +1,5 @@
 import { Platform, UnavailabilityError } from 'expo-modules-core';
+import { ApplicationReleaseType } from './Application.types';
 import ExpoApplication from './ExpoApplication';
 // @needsAudit
 /**
@@ -106,18 +107,8 @@ export async function getIosIdForVendorAsync() {
     if (!ExpoApplication.getIosIdForVendorAsync) {
         throw new UnavailabilityError('expo-application', 'getIosIdForVendorAsync');
     }
-    return (await ExpoApplication.getIosIdForVendorAsync()) ?? null;
+    return await ExpoApplication.getIosIdForVendorAsync();
 }
-// @docsMissing
-export var ApplicationReleaseType;
-(function (ApplicationReleaseType) {
-    ApplicationReleaseType[ApplicationReleaseType["UNKNOWN"] = 0] = "UNKNOWN";
-    ApplicationReleaseType[ApplicationReleaseType["SIMULATOR"] = 1] = "SIMULATOR";
-    ApplicationReleaseType[ApplicationReleaseType["ENTERPRISE"] = 2] = "ENTERPRISE";
-    ApplicationReleaseType[ApplicationReleaseType["DEVELOPMENT"] = 3] = "DEVELOPMENT";
-    ApplicationReleaseType[ApplicationReleaseType["AD_HOC"] = 4] = "AD_HOC";
-    ApplicationReleaseType[ApplicationReleaseType["APP_STORE"] = 5] = "APP_STORE";
-})(ApplicationReleaseType || (ApplicationReleaseType = {}));
 // @needsAudit
 /**
  * Gets the iOS application release type.
@@ -189,4 +180,5 @@ export async function getLastUpdateTimeAsync() {
     const lastUpdateTime = await ExpoApplication.getLastUpdateTimeAsync();
     return new Date(lastUpdateTime);
 }
+export { ApplicationReleaseType };
 //# sourceMappingURL=Application.js.map
