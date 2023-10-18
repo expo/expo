106c18142be5abbdf271351a93230852d4b495db
Evan Bacon <bacon@expo.io>
2023-10-18 12:09:31 -0500

    feat(router): add target, rel, and download to Link component (#24908)
    # Why
    
    Resolve ENG-10305
    
    # How
    
    Add support for the `target`, `rel`, and `download` attributes. The only
    caveat is that if you use `<Link asChild><Text /></Link>` the values
    won't work, they would need to be transformed to `hrefAttrs: { ... }`
    for React Native Web.
    
    # Test Plan
    
    Added tests for the rendering on native and web.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-router/src/link/Link.tsx
+++ packages/expo-router/src/link/Link.tsx
@@ -9,7 +9,59 @@ import useLinkToPathProps from './useLinkToPathProps';
 import { useRouter } from '../hooks';
 import { useFocusEffect } from '../useFocusEffect';
 
-export interface LinkProps extends Omit<TextProps, 'href'> {
+interface WebAnchorProps {
+  /**
+   * **Web only:** Specifies where to open the `href`.
+   *
+   * - **_self**: the current tab.
+   * - **_blank**: opens in a new tab or window.
+   * - **_parent**: opens in the parent browsing context. If no parent, defaults to **_self**.
+   * - **_top**: opens in the highest browsing context ancestor. If no ancestors, defaults to **_self**.
+   *
+   * This property is passed to the underlying anchor (`<a>`) tag.
+   *
+   * @default '_self'
+   *
+   * @example
+   * <Link href="https://expo.dev" target="_blank">Go to Expo in new tab</Link>
+   */
+  target?: '_self' | '_blank' | '_parent' | '_top' | (string & object);
+
+  /**
+   * **Web only:** Specifies the relationship between the `href` and the current route.
+   *
+   * Common values:
+   * - **nofollow**: Indicates to search engines that they should not follow the `href`. This is often used for user-generated content or links that should not influence search engine rankings.
+   * - **noopener**: Suggests that the `href` should not have access to the opening window's `window.opener` object, which is a security measure to prevent potentially harmful behavior in cases of links that open new tabs or windows.
+   * - **noreferrer**: Requests that the browser not send the `Referer` HTTP header when navigating to the `href`. This can enhance user privacy.
+   *
+   * The `rel` property is primarily used for informational and instructive purposes, helping browsers and web
+   * crawlers make better decisions about how to handle and interpret the links on a web page. It is important
+   * to use appropriate `rel` values to ensure that links behave as intended and adhere to best practices for web
+   * development and SEO (Search Engine Optimization).
+   *
+   * This property is passed to the underlying anchor (`<a>`) tag.
+   *
+   * @example
+   * <Link href="https://expo.dev" rel="nofollow">Go to Expo</Link>
+   */
+  rel?: string;
+
+  /**
+   * **Web only:** Specifies that the `href` should be downloaded when the user clicks on the link,
+   * instead of navigating to it. It is typically used for links that point to files that the user should download,
+   * such as PDFs, images, documents, etc.
+   *
+   * The value of the `download` property, which represents the filename for the downloaded file.
+   * This property is passed to the underlying anchor (`<a>`) tag.
+   *
+   * @example
+   * <Link href="/image.jpg" download="my-image.jpg">Download image</Link>
+   */
+  download?: string;
+}
+
+export interface LinkProps extends Omit<TextProps, 'href'>, WebAnchorProps {
   /** Path to route to. */
   href: Href;
 
@@ -82,18 +134,47 @@ function useInteropClassName(props: { style?: TextProps['style']; className?: st
   }, [props.style, props.className]);
 }
 
+const useHrefAttrs = Platform.select<
+  (props: Partial<LinkProps>) => { hrefAttrs?: any } & Partial<LinkProps>
+>({
+  web: function useHrefAttrs({ asChild, rel, target, download }: Partial<LinkProps>) {
+    return React.useMemo(() => {
+      const hrefAttrs = {
+        rel,
+        target,
+        download,
+      };
+      if (asChild) {
+        return hrefAttrs;
+      }
+      return {
+        hrefAttrs,
+      };
+    }, [asChild, rel, target, download]);
+  },
+  default: function useHrefAttrs() {
+    return {};
+  },
+});
+
 function ExpoRouterLink(
   {
     href,
     replace,
     // TODO: This does not prevent default on the anchor tag.
     asChild,
+    rel,
+    target,
+    download,
     ...rest
   }: LinkProps,
   ref: React.ForwardedRef<Text>
 ) {
   // Mutate the style prop to add the className on web.
   const style = useInteropClassName(rest);
+
+  // If not passing asChild, we need to forward the props to the anchor tag using React Native Web's `hrefAttrs`.
+  const hrefAttrs = useHrefAttrs({ asChild, rel, target, download });
 
   const resolvedHref = React.useMemo(() => {
     if (href == null) {
@@ -118,6 +199,7 @@ function ExpoRouterLink(
     <Element
       ref={ref}
       {...props}
+      {...hrefAttrs}
       {...rest}
       style={style}
       {...Platform.select({
