3ed51916f0e081422fdc89d32a4e4e16ca96751d
Evan Bacon <bacon@expo.io>
2023-10-19 09:51:44 -0500

    chore(metro-config): noop exotic mode (#24927)
    # Why
    
    Most of the [Exotic
    mode](https://blog.expo.dev/drastically-faster-bundling-in-react-native-a54f268e0ed1)
    performance benefits have been integrated in the default Expo CLI
    bundling pipeline (e.g. [less AST
    cloning](https://github.com/facebook/metro/pull/854), [faster worker
    creation](https://github.com/facebook/metro/pull/856)), and as such, the
    feature no longer needs to be enabled/disabled. Setting `mode: "exotic"`
    will no longer have any additional effects over the default.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/metro-config/build/transformer/generateFunctionMap.js
+++ packages/@expo/metro-config/build/transformer/generateFunctionMap.js
@@ -4,35 +4,24 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.generateFunctionMap = generateFunctionMap;
-function _env() {
-  const data = require("../env");
-  _env = function () {
-    return data;
-  };
-  return data;
-}
 function generateFunctionMap(...props) {
-  //  `x_facebook_sources` is a source map feature that we disable by default since it isn't documented
-  // and doesn't appear to add much value to the DX, it also increases bundle time, and source map size.
-  // The feature supposedly provides improved function names for anonymous functions, but we will opt towards
-  // linting to prevent users from adding anonymous functions for important features like React components.
+  // Unlike default sourcemaps, `metro-source-map` adds the `x_facebook_sources` field to the sourcemap.
+  // The feature provides improved function names for anonymous functions.
   //
   // Here is an example stack trace for a component that throws an error
   // in the root component (which is an anonymous function):
   //
-  // Before:
+  // Without `x_facebook_sources`:
   // - <anonymous> App.js:5:9
   // - renderApplication renderApplication.js:54:5
   // - runnables.appKey.run AppRegistry.js:117:26
   //
-  // After:
+  // With `x_facebook_sources`:
   // - _default App.js:5:9
   // - renderApplication renderApplication.js:54:5
   // - run AppRegistry.js:117:26
   //
-  if (_env().env.EXPO_USE_FB_SOURCES) {
-    return require('metro-source-map').generateFunctionMap(...props);
-  }
-  return null;
+
+  return require('metro-source-map').generateFunctionMap(...props);
 }
 //# sourceMappingURL=generateFunctionMap.js.map
