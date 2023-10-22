a5a575841dbb27edeb812d8bcc841271dbcec73b
Gavin <me@gkasdorf.com>
2023-10-21 20:00:18 -0700

    [chore] Update documentation

--- packages/expo-image/build/Image.d.ts
+++ packages/expo-image/build/Image.d.ts
@@ -28,12 +28,13 @@ export declare class Image extends React.PureComponent<ImageProps> {
     /**
      * Asynchronously checks if an image exists in the disk cache and resolves to
      * the path of the cached image if it does.
+     * @param source - The source URL to retrieve the cached version's path for
+     * @platform android
      * @platform ios
      * @return A promise resolving to the path of the cached image. It will resolve
-     * to `false` if the image does not exist in the cache. Resolves to `false` on
-     * Web.
+     * to `false` if the image does not exist in the cache.
      */
-    static getCachePathAsync(url: string): Promise<string | false>;
+    static getCachePathAsync(source: string): Promise<string | false>;
     render(): JSX.Element;
 }
 //# sourceMappingURL=Image.d.ts.map
