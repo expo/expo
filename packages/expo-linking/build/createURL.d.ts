a7679b44017f7703ec87da4d3a8f9354a2fded24
Will Schurman <wschurman@expo.io>
2023-10-27 15:12:43 -0700

    [docs] Fix Linking.createURL docs for Expo Go (#25018)

--- packages/expo-linking/build/createURL.d.ts
+++ packages/expo-linking/build/createURL.d.ts
@@ -3,16 +3,20 @@ import { CreateURLOptions, ParsedURL } from './Linking.types';
  * Helper method for constructing a deep link into your app, given an optional path and set of query
  * parameters. Creates a URI scheme with two slashes by default.
  *
- * The scheme in bare and standalone must be defined in the Expo config (`app.config.js` or `app.json`)
- * under `expo.scheme`.
+ * The scheme must be defined in the Expo config (`app.config.js` or `app.json`) under `expo.scheme`
+ * or `expo.{android,ios}.scheme`. Platform-specific schemes defined under `expo.{android,ios}.scheme`
+ * take precedence over universal schemes defined under `expo.scheme`.
  *
  * # Examples
- * - Bare: `<scheme>://path` - uses provided scheme or scheme from Expo config `scheme`.
- * - Standalone, Custom: `yourscheme://path`
+ * - Development and production builds: `<scheme>://path` - uses the optional `scheme` property if provided, and otherwise uses the first scheme defined by your Expo config
  * - Web (dev): `https://localhost:19006/path`
  * - Web (prod): `https://myapp.com/path`
- * - Expo Client (dev): `exp://128.0.0.1:8081/--/path`
- * - Expo Client (prod): `exp://exp.host/@yourname/your-app/--/path`
+ * - Expo Go (dev): `exp://128.0.0.1:8081/--/path`
+ *
+ * The behavior of this method in Expo Go for published updates is undefined and should not be relied upon.
+ * The created URL in this case is neither stable nor predictable during the lifetime of the app.
+ * If a stable URL is needed, for example in authorization callbacks, a build (or development build)
+ * of your application should be used and the scheme provided.
  *
  * @param path Addition path components to append to the base URL.
  * @param namedParameters Additional options object.
