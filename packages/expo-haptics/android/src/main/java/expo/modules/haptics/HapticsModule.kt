8d4d709d33f1ee0628960646b63e53ea7b274add
Alexander Pataridze <s.pataridze@gmail.com>
2023-10-29 18:56:23 +0400

    refactor: haptics on Android

--- packages/expo-haptics/android/src/main/java/expo/modules/haptics/HapticsModule.kt
+++ packages/expo-haptics/android/src/main/java/expo/modules/haptics/HapticsModule.kt
@@ -2,8 +2,8 @@ package expo.modules.haptics
 
 import android.content.Context
 import android.os.Build
-import android.os.VibrationEffect
 import android.os.Vibrator
+import android.os.VibratorManager
 import expo.modules.haptics.arguments.HapticsImpactType
 import expo.modules.haptics.arguments.HapticsNotificationType
 import expo.modules.haptics.arguments.HapticsSelectionType
@@ -13,32 +13,40 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class HapticsModule : Module() {
-  private val context: Context
-    get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
-  private val vibrator
-    get() = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    private val context: Context
+        get() = appContext.reactContext ?: throw Exceptions.ReactContextLost()
+    private val vibrator: Vibrator
+        get() {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                (context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager).defaultVibrator
+            } else {
+                @Suppress("DEPRECATION")
+                context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            }
+        }
 
-  override fun definition() = ModuleDefinition {
-    Name("ExpoHaptics")
+    override fun definition() = ModuleDefinition {
+        Name("ExpoHaptics")
 
-    AsyncFunction("notificationAsync") { type: String ->
-      vibrate(HapticsNotificationType.fromString(type))
+        AsyncFunction("notificationAsync") { type: String ->
+            vibrate(HapticsNotificationType.fromString(type))
+        }
+
+        AsyncFunction("selectionAsync") {
+            vibrate(HapticsSelectionType)
+        }
+
+        AsyncFunction("impactAsync") { style: String ->
+            vibrate(HapticsImpactType.fromString(style))
+        }
     }
 
-    AsyncFunction("selectionAsync") {
-      vibrate(HapticsSelectionType)
+    private fun vibrate(type: HapticsVibrationType) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createWaveform(type.timings, type.amplitudes, -1))
+        } else {
+            @Suppress("DEPRECATION")
+            vibrator.vibrate(type.oldSDKPattern, -1)
+        }
     }
-
-    AsyncFunction("impactAsync") { style: String ->
-      vibrate(HapticsImpactType.fromString(style))
-    }
-  }
-
-  private fun vibrate(type: HapticsVibrationType) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      vibrator.vibrate(VibrationEffect.createWaveform(type.timings, type.amplitudes, -1))
-    } else {
-      vibrator.vibrate(type.oldSDKPattern, -1)
-    }
-  }
 }
