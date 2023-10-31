d9de2c1975ec8986e39a73cfedeb1b017d8742db
evanbacon <bacon@expo.io>
2023-10-31 04:02:38 -0500

    Add expo-winter to bare-expo for testing

--- packages/expo-winter/metro.config.js
+++ packages/expo-winter/metro.config.js
@@ -1,5 +1,3 @@
-// const { createMetroConfiguration } = require("expo-yarn-workspaces");
-
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = {};
 const path = require('path');
@@ -7,5 +5,20 @@ const path = require('path');
 const root = path.join(__dirname, '../..');
 
 config.watchFolders = [__dirname, ...['packages', 'node_modules'].map((v) => path.join(root, v))];
+
+const banner = `var process=this.process||{},__METRO_GLOBAL_PREFIX__='';`;
+
+const baseJSBundle = require('metro/src/DeltaBundler/Serializers/baseJSBundle');
+const bundleToString = require('metro/src/lib/bundleToString');
+
+config.serializer = {
+  customSerializer: (entryPoint, preModules, graph, options) => {
+    const prelude = preModules.find((module) => module.path === '__prelude__');
+    if (prelude) {
+      prelude.output[0].data.code = banner;
+    }
+    return bundleToString(baseJSBundle(entryPoint, preModules, graph, options)).code;
+  },
+};
 
 module.exports = config;
