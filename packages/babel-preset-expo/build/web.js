b4a1e5edffac9fa082b49afd97603c14b9237f1e
evanbacon <bacon@expo.io>
2023-10-31 19:11:58 -0500

    add license

--- packages/babel-preset-expo/build/web.js
+++ packages/babel-preset-expo/build/web.js
@@ -1,4 +1,11 @@
 "use strict";
+/**
+ * Copyright © 2023-present 650 Industries, Inc. (aka Expo)
+ * Copyright © Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.babelPresetExpoWeb = void 0;
 const common_1 = require("./common");
--- packages/babel-preset-expo/src/web.ts
+++ packages/babel-preset-expo/src/web.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright © 2023-present 650 Industries, Inc. (aka Expo)
+ * Copyright © Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { ConfigAPI, PluginItem, TransformOptions } from '@babel/core';
 
 import { BabelPresetExpoOptions, BabelPresetExpoPlatformOptions, getBundler } from './common';
