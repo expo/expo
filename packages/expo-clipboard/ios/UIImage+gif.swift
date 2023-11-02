edc2cc6fb2796306f36d2abc1ac4ffb0f3b7ca98
Arif Ali <51419655+arifali123@users.noreply.github.com>
2023-11-02 11:35:02 -0700

    Opening Brace Spacing Violation

--- packages/expo-clipboard/ios/UIImage+gif.swift
+++ packages/expo-clipboard/ios/UIImage+gif.swift
@@ -9,39 +9,32 @@ extension UIImage {
     }
     var images = [UIImage]()
     var totalDuration: TimeInterval = 0.0
-    
     let count = CGImageSourceGetCount(source)
     for i in 0 ..< count {
       if let cgImage = CGImageSourceCreateImageAtIndex(source, i, nil) {
         let image = UIImage(cgImage: cgImage)
         images.append(image)
-        
         let delaySeconds = UIImage.delayForImageAtIndex(index: Int(i), source: source)
         totalDuration += delaySeconds
       }
     }
-    
     return UIImage.animatedImage(with: images, duration: totalDuration)
   }
-  
   static func delayForImageAtIndex(index: Int, source: CGImageSource) -> TimeInterval {
     var delay = 0.1
-    
     let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
-    let gifProperties: CFDictionary? = CFDictionaryGetValue(cfProperties, Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque())?.assumingMemoryBound(to: CFDictionary.self).pointee
-    
+    let gifProperties: CFDictionary? = CFDictionaryGetValue(cfProperties, Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque())?
+    .assumingMemoryBound(to: CFDictionary.self).pointee
     if let gifProperties = gifProperties {
       var delayTime: Double = 0
       if let properties = CGImageSourceCopyPropertiesAtIndex(source, index, nil) as? [String: Any],
-         let gifProperties = properties[kCGImagePropertyGIFDictionary as String] as? [String: Any]
-      {
+        let gifProperties = properties[kCGImagePropertyGIFDictionary as String] as? [String: Any] {
         if let delayTimeUnclampedProp = gifProperties[kCGImagePropertyGIFUnclampedDelayTime as String] as? NSNumber {
           delayTime = delayTimeUnclampedProp.doubleValue
         } else if let delayTimeProp = gifProperties[kCGImagePropertyGIFDelayTime as String] as? NSNumber {
           delayTime = delayTimeProp.doubleValue
         }
       }
-      
       if let delayTime = delayTime as? NSNumber {
         delay = delayTime.doubleValue
         
@@ -50,38 +43,30 @@ extension UIImage {
         }
       }
     }
-    
     return delay
   }
-  
   var isAnimated: Bool {
     return images != nil
   }
-  
   func gifData(loopCount: Int = 0) -> Data? {
     let imagesToUse = images ?? [self]
     let gifLoopCount = [kCGImagePropertyGIFLoopCount as String: loopCount]
     let gifProperties = [kCGImagePropertyGIFDictionary as String: gifLoopCount]
-    
     // Assuming a default frame delay for single frame if not animated.
     let defaultFrameDelay = 0.1
     let frameDelay = duration.isZero ? defaultFrameDelay : duration / Double(imagesToUse.count)
     let frameProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: frameDelay]]
-    
     let data = NSMutableData()
     if let destination = CGImageDestinationCreateWithData(data as CFMutableData, kUTTypeGIF, imagesToUse.count, nil) {
       CGImageDestinationSetProperties(destination, gifProperties as CFDictionary)
-      
       for image in imagesToUse {
         guard let cgImage = image.cgImage else { continue }
         CGImageDestinationAddImage(destination, cgImage, frameProperties as CFDictionary)
       }
-      
       if !CGImageDestinationFinalize(destination) {
         return nil
       }
     }
-    
     return data as Data
   }
 }
