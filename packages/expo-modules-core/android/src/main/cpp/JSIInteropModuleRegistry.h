57307774690238f0838dde158715494c364696d6
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2023-11-02 16:01:36 +0100

    [core][Android] Remove unused native call invoker (#25185)
    # Why
    
    Removes unused native call invoker.
    
    # How
    
    I confirmed that we are not using a native call invoker anywhere. So we should remove it. 
    
    # Test Plan
    
    - CI ✅

--- packages/expo-modules-core/android/src/main/cpp/JSIInteropModuleRegistry.h
+++ packages/expo-modules-core/android/src/main/cpp/JSIInteropModuleRegistry.h
@@ -26,12 +26,6 @@ namespace react = facebook::react;
 
 namespace expo {
 
-#if REACT_NATIVE_TARGET_VERSION >= 73
-using NativeMethodCallInvokerHolderCompatible = react::NativeMethodCallInvokerHolder;
-#else
-using NativeMethodCallInvokerHolderCompatible = react::CallInvokerHolder;
-#endif
-
 /**
  * A JNI wrapper to initialize CPP part of modules and access all data from the module registry.
  */
@@ -51,8 +45,7 @@ public:
   void installJSI(
     jlong jsRuntimePointer,
     jni::alias_ref<JNIDeallocator::javaobject> jniDeallocator,
-    jni::alias_ref<react::CallInvokerHolder::javaobject> jsInvokerHolder,
-    jni::alias_ref<NativeMethodCallInvokerHolderCompatible::javaobject> nativeInvokerHolder
+    jni::alias_ref<react::CallInvokerHolder::javaobject> jsInvokerHolder
   );
 
   /**
@@ -113,7 +106,6 @@ public:
   void drainJSEventLoop();
 
   std::shared_ptr<react::CallInvoker> jsInvoker;
-  std::shared_ptr<react::CallInvoker> nativeInvoker;
   std::shared_ptr<JavaScriptRuntime> runtimeHolder;
   std::unique_ptr<JSReferencesCache> jsRegistry;
   jni::global_ref<JNIDeallocator::javaobject> jniDeallocator;
