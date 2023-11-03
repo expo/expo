8205e99730ce76ed984b220680ac534b5070f880
Douglas Lowder <douglowder@mac.com>
2023-11-02 17:04:35 -0700

    Better typing

--- packages/@expo/cli/src/export/exportAssets.ts
+++ packages/@expo/cli/src/export/exportAssets.ts
@@ -158,13 +158,13 @@ export async function exportAssetsAsync(
   // Add google services file if it exists
   await resolveGoogleServicesFile(projectRoot, exp);
 
-  bundles.ios?.assets.forEach((asset: Asset & { embedded?: true }) => {
+  bundles.ios?.assets.forEach((asset: Asset) => {
     // Mark assets to be removed from metadata
     if (!assetShouldBeIncludedInExport(asset, bundledAssetsSet)) {
       asset.embedded = true;
     }
   });
-  bundles.android?.assets.forEach((asset: Asset & { embedded?: true }) => {
+  bundles.android?.assets.forEach((asset: Asset) => {
     // Mark assets to be removed from metadata
     if (!assetShouldBeIncludedInExport(asset, bundledAssetsSet)) {
       asset.embedded = true;
