c34b36b1bce80b7e04c9cfdde176243f17fe2f12
Douglas Lowder <douglowder@mac.com>
2023-11-03 08:19:28 -0700

    Revert "[@expo/cli] export only selected assets"
    This reverts commit e450a4fcb7f7d52f7cedc3b001ba4e7da78c2f8e.

--- packages/@expo/cli/src/export/exportAssets.ts
+++ packages/@expo/cli/src/export/exportAssets.ts
@@ -12,57 +12,26 @@ import { uniqBy } from '../utils/array';
 
 const debug = require('debug')('expo:export:exportAssets') as typeof console.log;
 
-function mapAssetHashToAssetString(asset: Asset, hash: string) {
-  return 'asset_' + hash + ('type' in asset && asset.type ? '.' + asset.type : '');
-}
-
-export function assetPatternsToBeBundled(
-  exp: ExpoConfig & { extra?: { assetPatternsToBeBundled?: string[] } }
-): string[] | undefined {
-  return exp?.extra?.assetPatternsToBeBundled?.length
-    ? exp?.extra?.assetPatternsToBeBundled
-    : undefined;
-}
-
 /**
- * Given an asset and a set of strings representing the assets to be bundled, returns true if
- * the asset is part of the set to be bundled.
- * @param asset Asset object
- * @param bundledAssetsSet Set of strings
- * @returns true if the asset should be bundled
+ * Resolves the assetBundlePatterns from the manifest and returns a list of assets to bundle.
+ *
+ * @modifies {exp}
  */
-export function assetShouldBeIncludedInExport(
-  asset: Asset,
-  bundledAssetsSet: Set<string> | undefined
-) {
-  if (!bundledAssetsSet) {
-    return true;
+export async function resolveAssetBundlePatternsAsync<T extends ExpoConfig>(
+  projectRoot: string,
+  exp: T,
+  assets: Asset[]
+): Promise<Omit<T, 'assetBundlePatterns'> & { bundledAssets?: string[] }> {
+  if (!exp.assetBundlePatterns?.length || !assets.length) {
+    delete exp.assetBundlePatterns;
+    return exp;
   }
-  return (
-    asset.fileHashes.filter((hash) => bundledAssetsSet?.has(mapAssetHashToAssetString(asset, hash)))
-      .length > 0 ?? false
-  );
-}
-
-/**
- * Computes a set of strings representing the assets to be bundled with an export, given an array of assets,
- * and a set of patterns to match
- * @param assets The asset array
- * @param assetPatternsToBeBundled An array of strings with glob patterns to match
- * @param projectRoot The project root
- * @returns A set of asset strings
- */
-export function setOfAssetsToBeBundled(
-  assets: Asset[],
-  assetPatternsToBeBundled: string[],
-  projectRoot: string
-): Set<string> {
   // Convert asset patterns to a list of asset strings that match them.
   // Assets strings are formatted as `asset_<hash>.<type>` and represent
   // the name that the file will have in the app bundle. The `asset_` prefix is
   // needed because android doesn't support assets that start with numbers.
 
-  const fullPatterns: string[] = assetPatternsToBeBundled.map((p: string) =>
+  const fullPatterns: string[] = exp.assetBundlePatterns.map((p: string) =>
     path.join(projectRoot, p)
   );
 
@@ -73,7 +42,9 @@ export function setOfAssetsToBeBundled(
       const shouldBundle = shouldBundleAsset(asset, fullPatterns);
       if (shouldBundle) {
         debug(`${shouldBundle ? 'Include' : 'Exclude'} asset ${asset.files?.[0]}`);
-        return asset.fileHashes.map((hash) => mapAssetHashToAssetString(asset, hash));
+        return asset.fileHashes.map(
+          (hash) => 'asset_' + hash + ('type' in asset && asset.type ? '.' + asset.type : '')
+        );
       }
       return [];
     })
@@ -81,27 +52,7 @@ export function setOfAssetsToBeBundled(
 
   // The assets returned by the RN packager has duplicates so make sure we
   // only bundle each once.
-  return new Set(allBundledAssets);
-}
-
-/**
- * Resolves the assetBundlePatterns from the manifest and returns a list of assets to bundle.
- *
- * @modifies {exp}
- */
-export async function resolveAssetPatternsToBeBundledAsync<T extends ExpoConfig>(
-  projectRoot: string,
-  exp: T,
-  assets: Asset[]
-): Promise<T & { bundledAssets?: Set<string> }> {
-  if (!assetPatternsToBeBundled(exp)) {
-    return exp;
-  }
-  (exp as any).bundledAssets = setOfAssetsToBeBundled(
-    assets,
-    assetPatternsToBeBundled(exp) ?? ['**/*'],
-    projectRoot
-  );
+  (exp as any).bundledAssets = [...new Set(allBundledAssets)];
   delete exp.assetBundlePatterns;
 
   return exp;
@@ -141,39 +92,16 @@ export async function exportAssetsAsync(
   );
 
   if (assets[0]?.fileHashes) {
-    debug(`Assets = ${JSON.stringify(assets, null, 2)}`);
-    // Updates the manifest to reflect additional asset bundling + configs
-    // Get only asset strings for assets we will save
-    await resolveAssetPatternsToBeBundledAsync(projectRoot, exp, assets);
-    const bundledAssetsSet = (exp as any).bundledAssets;
-    let filteredAssets = assets;
-    if (bundledAssetsSet) {
-      debug(`Bundled assets = ${JSON.stringify([...bundledAssetsSet], null, 2)}`);
-      // Filter asset objects to only ones that include assetBundlePatterns matches
-      filteredAssets = assets.filter((asset) =>
-        assetShouldBeIncludedInExport(asset, bundledAssetsSet)
-      );
-      debug(`Filtered assets count = ${filteredAssets.length}`);
-    }
     Log.log('Saving assets');
-    await saveAssetsAsync(projectRoot, { assets: filteredAssets, outputDir });
+    await saveAssetsAsync(projectRoot, { assets, outputDir });
   }
 
   // Add google services file if it exists
   await resolveGoogleServicesFile(projectRoot, exp);
 
-  bundles.ios?.assets.forEach((asset: any) => {
-    // Mark assets to be removed from metadata
-    if (!assetShouldBeIncludedInExport(asset, (exp as any).bundledAssets)) {
-      asset.embedded = true;
-    }
-  });
-  bundles.android?.assets.forEach((asset: any) => {
-    // Mark assets to be removed from metadata
-    if (!assetShouldBeIncludedInExport(asset, (exp as any).bundledAssets)) {
-      asset.embedded = true;
-    }
-  });
+  // Updates the manifest to reflect additional asset bundling + configs
+  await resolveAssetBundlePatternsAsync(projectRoot, exp, assets);
+
   return { exp, assets };
 }
 
