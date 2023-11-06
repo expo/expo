8f5157042ca5bb417a80a26ff7f6a9cff65b6056
Mark Lawlor <mwlawlor@gmail.com>
2023-11-07 08:09:04 +1000

    fix(router): update jest testing-library mocks (#25110)
    # Why
    
    Update `expo-router/testing-library` mock to use `mock*` prefix. When
    not using `jest-expo`, the `renderRouter` fails as `initialUrl` is
    undefined. When running locally I encountered this error
    
    ```
    The module factory of `jest.mock()` is not allowed to reference any out-of-scope variables. <...> Note: This is a precaution to guard against uninitialized mock variables. If it is ensured that the mock is required lazily, variable names prefixed with `mock` (case insensitive) are permitted.
    ```
    
    I'm assuming `jest-expo` transforms the code in someway that this
    safe-guard isn't triggered :man_shrugging:
    
    Fix: #25059
    Fix: #24786
    
    # How
    
    Jest mocks will set any out-of-scope variables to `undefined` unless
    they start with the `mock` prefix.
    
    # Test Plan
    
    Run existing test suite
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-router/build/testing-library/mocks.js
+++ packages/expo-router/build/testing-library/mocks.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.initialUrlRef = void 0;
+exports.setInitialUrl = void 0;
 require("@testing-library/jest-native/extend-expect");
 // include this section and the NativeAnimatedHelper section for mocking react-native-reanimated
 jest.mock('react-native-reanimated', () => {
@@ -12,9 +12,11 @@ jest.mock('react-native-reanimated', () => {
 });
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-exports.initialUrlRef = {
-    value: '',
-};
+let mockInitialUrl = '';
+function setInitialUrl(value) {
+    mockInitialUrl = value;
+}
+exports.setInitialUrl = setInitialUrl;
 jest.mock('expo-linking', () => {
     const module = {
         ...jest.requireActual('expo-linking'),
@@ -28,7 +30,7 @@ jest.mock('expo-linking', () => {
             return { remove() { } };
         },
         async getInitialURL() {
-            return exports.initialUrlRef.value;
+            return mockInitialUrl;
         },
     };
     return module;
