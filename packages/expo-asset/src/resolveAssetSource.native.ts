5cd9211cc9a4f6f01513213adb07321cae9ad496
evanbacon <bacon@expo.io>
2023-11-08 00:00:11 -0600

    swap default module platform

---
+++ packages/expo-asset/src/resolveAssetSource.native.ts
@@ -0,0 +1,3 @@
+import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
+export default resolveAssetSource;
+export * from 'react-native/Libraries/Image/resolveAssetSource'; // eslint-disable-line import/export
