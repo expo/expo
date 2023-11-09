0d78b65081e051effb1460d64fb32dcb84f5c783
Alan Hughes <hughesno1@gmail.com>
2023-11-09 09:31:09 +0000

    [test-suite] fix metro config

--- apps/bare-expo/metro.config.js
+++ apps/bare-expo/metro.config.js
@@ -1,7 +1,5 @@
 const { createMetroConfiguration } = require('expo-yarn-workspaces');
-
 const baseConfig = createMetroConfiguration(__dirname);
-
 const path = require('path');
 
 const root = path.join(__dirname, '../..');
