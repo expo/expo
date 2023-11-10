f7cb65ac9d4c2b55f83c3c7c47c07b0d15f36a5d
Cedric van Putten <github@cedric.dev>
2023-11-10 17:23:25 +0100

    fix(cli): update global `arg` error handling

--- packages/@expo/cli/src/utils/__tests__/arg-test.ts
+++ packages/@expo/cli/src/utils/__tests__/arg-test.ts
@@ -7,12 +7,12 @@ describe(assertWithOptionsArgs, () => {
   it('handles unknown options errors', () => {
     jest.mocked(Log.exit).mockImplementation();
     expect(() => assertWithOptionsArgs({ '--help': Boolean }, { argv: ['--unknown'] })).toThrow();
-    expect(Log.exit).toHaveBeenCalledWith('Unknown or unexpected option: --unknown', 1);
+    expect(Log.exit).toHaveBeenCalledWith('unknown or unexpected option: --unknown', 1);
   });
 
   it('handles missing arguments errors', () => {
     jest.mocked(Log.exit).mockImplementation();
-    expect(() => assertWithOptionsArgs({ '--name': String }, { argv: ['--name'] })).toThrow('kln');
-    expect(Log.exit).toHaveBeenCalledWith('Option requires argument: --name', 1);
+    expect(() => assertWithOptionsArgs({ '--name': String }, { argv: ['--name'] })).toThrow();
+    expect(Log.exit).toHaveBeenCalledWith('option requires argument: --name', 1);
   });
 });
