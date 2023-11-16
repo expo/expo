27ae74bb14f7a92aba46653167dc402f38a06c13
Cedric van Putten <github@cedric.dev>
2023-11-16 12:52:26 +0100

    refactor(env): replace `@expo/babel-preset-cli` with `expo-module-scripts`

--- packages/@expo/env/build/index.js
+++ packages/@expo/env/build/index.js
@@ -1,40 +1,15 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.get = void 0;
-Object.defineProperty(exports, "getFiles", {
-  enumerable: true,
-  get: function () {
-    return _env().getFiles;
-  }
-});
-Object.defineProperty(exports, "isEnabled", {
-  enumerable: true,
-  get: function () {
-    return _env().isEnabled;
-  }
-});
-exports.load = void 0;
-function _env() {
-  const data = require("./env");
-  _env = function () {
-    return data;
-  };
-  return data;
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isEnabled = exports.load = exports.get = exports.getFiles = void 0;
 /**
  * Copyright © 2023 650 Industries.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-const {
-  get,
-  load
-} = (0, _env().createControlledEnvironment)();
-exports.load = load;
+const env_1 = require("./env");
+Object.defineProperty(exports, "getFiles", { enumerable: true, get: function () { return env_1.getFiles; } });
+Object.defineProperty(exports, "isEnabled", { enumerable: true, get: function () { return env_1.isEnabled; } });
+const { get, load } = (0, env_1.createControlledEnvironment)();
 exports.get = get;
-//# sourceMappingURL=index.js.map
+exports.load = load;
