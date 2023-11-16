090593bb6314ca201ac7059356e19bfd14a65af0
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2023-11-16 13:48:00 +0100

    [image][Android] Changed how the `tintColor` is applied to the SVG (#25377)
    # Why
    
     Changes how the `tintColor` is applied to the SVG
    
    # How
    
    Handling tint color in SVG required us to implement a lot of custom logic, which led to several bugs and inconsistent behavior. To address this, I've introduced a new approach in this PR. Instead of decoding the SVG into the bitmap to use filters, I modify the content of the parsed SVG to change color. Although most of the API is package private, I have applied a quick workaround for now. I plan to submit a PR to the Android library to change it upstream. However, for now, this solution should be enough.
    
    # Test Plan
    
    - bare-expo with NCL ✅

--- packages/expo-image/android/src/main/java/expo/modules/image/svg/SVGDrawableTranscoder.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/svg/SVGDrawableTranscoder.kt
@@ -1,26 +1,22 @@
 package expo.modules.image.svg
 
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.Canvas
 import android.graphics.Picture
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.PictureDrawable
 import com.bumptech.glide.load.Options
 import com.bumptech.glide.load.engine.Resource
 import com.bumptech.glide.load.resource.SimpleResource
 import com.bumptech.glide.load.resource.transcode.ResourceTranscoder
-import com.caverock.androidsvg.PreserveAspectRatio
-import com.caverock.androidsvg.RenderOptions
 import com.caverock.androidsvg.SVG
+import com.caverock.androidsvg.applyTintColor
+import expo.modules.image.CustomOptions
 
 /**
-* We have to use the intrinsicWidth/Height from the bitmap to render the image at a high enough resolution, but at the same time we want to return the actual
-* preferred width and height of the SVG to JS. This class allows us to do that.
-*/
-class SVGBitmapDrawable(res: Resources?, bitmap: Bitmap?, val svgIntrinsicWidth: Int, val svgIntrinsicHeight: Int) : BitmapDrawable(res, bitmap)
+ * We have to use the intrinsicWidth/Height from the Picture to render the image at a high enough resolution, but at the same time we want to return the actual
+ * preferred width and height of the SVG to JS. This class allows us to do that.
+ */
+class SVGPictureDrawable(picture: Picture, val svgIntrinsicWidth: Int, val svgIntrinsicHeight: Int) : PictureDrawable(picture)
 
 /**
  * Convert the [SVG]'s internal representation to an Android-compatible one ([Picture]).
@@ -31,29 +27,21 @@ class SVGBitmapDrawable(res: Resources?, bitmap: Bitmap?, val svgIntrinsicWidth:
 class SVGDrawableTranscoder(val context: Context) : ResourceTranscoder<SVG?, Drawable> {
   override fun transcode(toTranscode: Resource<SVG?>, options: Options): Resource<Drawable> {
     val svgData = toTranscode.get()
-    val svgIntrinsicWidth = svgData.documentViewBox.width()
-    val svgIntrinsicHeight = svgData.documentViewBox.height()
-    val documentWidth = svgData.documentWidth
-    val documentHeight = svgData.documentHeight
-    val aspectRatio = svgIntrinsicWidth / svgIntrinsicHeight
+    val intrinsicWidth = svgData.documentViewBox.width().toInt()
+    val intrinsicHeight = svgData.documentViewBox.height().toInt()
 
-    // We have no information on what content fit the user wants, so when choosing render resolution we assume
-    // "cover" in order to prevent loss of quality after the bitmap is transformed to appropriate `contentFit` later.
-    val shouldUseHeightReference = documentWidth / aspectRatio > documentHeight
-    val renderWidth = if (shouldUseHeightReference) documentWidth else documentHeight * aspectRatio
-    val renderHeight = if (shouldUseHeightReference) documentWidth / aspectRatio else documentHeight
-    val renderOptions = RenderOptions().apply {
-      viewPort(0f, 0f, renderWidth, renderHeight)
-      preserveAspectRatio(PreserveAspectRatio.FULLSCREEN_START)
+    val tintColor = options.get(CustomOptions.tintColor)
+    if (tintColor != null) {
+      applyTintColor(svgData, tintColor)
     }
 
-    val picture = svgData.renderToPicture(renderWidth.toInt(), renderHeight.toInt(), renderOptions)
-    val drawable = PictureDrawable(picture)
-    val bitmap = Bitmap.createBitmap(renderWidth.toInt(), renderHeight.toInt(), Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(bitmap)
-    drawable.setBounds(0, 0, canvas.width, canvas.height)
-    drawable.draw(canvas)
-
-    return SimpleResource(SVGBitmapDrawable(context.resources, bitmap, svgIntrinsicWidth.toInt(), svgIntrinsicHeight.toInt()))
+    val picture = SVGPictureDrawable(
+      svgData.renderToPicture(),
+      intrinsicWidth,
+      intrinsicHeight
+    )
+    return SimpleResource(
+      picture
+    )
   }
 }
