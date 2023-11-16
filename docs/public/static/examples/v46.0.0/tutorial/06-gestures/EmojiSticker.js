0af1b2a4287103a42717c31923b1e0471abe0b76
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2023-11-16 12:11:09 +0100

    [docs] update `Add gestures` to use RNGH 2 API (#25356)
    # Why
    
    The Expo documentation in [Add
    gestures](https://docs.expo.dev/tutorial/gestures/) pages teaches the
    old React Native Gesture Handler v1 API with now [deprecated
    useAnimatedGestureHandler](https://github.com/software-mansion/react-native-reanimated/blob/main/src/reanimated2/hook/useAnimatedGestureHandler.ts#L73)
    hook.
    
    Since the release [RNGH
    2.0](https://www.npmjs.com/package/react-native-gesture-handler/v/2.0.0)
    (2 years ago) we started recommending to use the new
    `Gesture`/`GestureDetector` API.
    
    This PR migrates the `Add gestures` section of Expo documentation to the
    simpler v2 API.
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    I've updated `docs/pages/tutorial/gestures.mdx` in `docs/` alongside
    `CompleteEmojiSticker.js` and `EmojiSticker.js` in all the consecutive
    documentation versions up to SDK46
    
    # Test Plan
    
    I've run the updated code in a Snack and locally in a new expo project.
    Here's the link to Snack:
    
    https://snack.expo.dev/@kacperkapusciak/handle-pan-gesture---rngh-2-api
    
    The updated code should work exactly the same as the old one.
    
    
    
    https://github.com/expo/expo/assets/39658211/70681626-49bf-4b23-9b4f-8e2533208d91
    
    
    
    # Checklist
    
    
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    
    <!-- disable:changelog-checks -->
    
    ---------
    
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>

--- docs/public/static/examples/v46.0.0/tutorial/06-gestures/EmojiSticker.js
+++ docs/public/static/examples/v46.0.0/tutorial/06-gestures/EmojiSticker.js
@@ -1,24 +1,21 @@
-import { View, Image } from 'react-native';
-import { TapGestureHandler } from 'react-native-gesture-handler';
+import { View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
   useSharedValue,
-  useAnimatedGestureHandler,
   useAnimatedStyle,
   withSpring,
 } from 'react-native-reanimated';
 
-const AnimatedImage = Animated.createAnimatedComponent(Image);
-
 export default function EmojiSticker({ imageSize, stickerSource }) {
   const scaleImage = useSharedValue(imageSize);
 
-  const onDoubleTap = useAnimatedGestureHandler({
-    onActive: () => {
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
       if (scaleImage.value !== imageSize * 2) {
         scaleImage.value = scaleImage.value * 2;
       }
-    },
-  });
+    });
 
   const imageStyle = useAnimatedStyle(() => {
     return {
@@ -29,13 +26,13 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
 
   return (
     <View style={{ top: -350 }}>
-      <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
-        <AnimatedImage
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
           source={stickerSource}
           resizeMode="contain"
           style={[imageStyle, { width: imageSize, height: imageSize }]}
         />
-      </TapGestureHandler>
+      </GestureDetector>
     </View>
   );
 }
