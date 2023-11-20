31759eff5268a97a9cf9d3f02e6c7e926c499509
Cedric van Putten <github@cedric.dev>
2023-11-20 16:14:02 +0100

    refactor(expo-module-scripts): drop `@babel/preset-env`-included plugins

--- packages/expo-module-scripts/babel.config.node.js
+++ packages/expo-module-scripts/babel.config.node.js
@@ -14,14 +14,11 @@ module.exports = () => ({
   plugins: [
     require('babel-plugin-dynamic-import-node'),
     require('@babel/plugin-proposal-export-namespace-from'),
-    require('@babel/plugin-proposal-class-properties'),
     [
       require('@babel/plugin-transform-modules-commonjs'),
       {
         lazy: /* istanbul ignore next */ (source) => true,
       },
     ],
-    require('@babel/plugin-proposal-optional-chaining'),
-    require('@babel/plugin-proposal-nullish-coalescing-operator'),
   ],
 });
