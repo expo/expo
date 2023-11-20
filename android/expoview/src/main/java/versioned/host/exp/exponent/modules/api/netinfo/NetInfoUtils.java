b32ad435a8824d8e8bc9e5d4545d4073669e720c
Gabriel Donadel <donadeldev@gmail.com>
2023-11-20 10:01:07 -0300

    [android][ios] Upgrade @react-native-community/netinfo to 11.1.0

--- android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/netinfo/NetInfoUtils.java
+++ android/expoview/src/main/java/versioned/host/exp/exponent/modules/api/netinfo/NetInfoUtils.java
@@ -7,8 +7,12 @@
 package versioned.host.exp.exponent.modules.api.netinfo;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Build;
 
 import androidx.core.content.ContextCompat;
 
@@ -24,5 +28,23 @@ public class NetInfoUtils {
     public static boolean isAccessWifiStatePermissionGranted(Context context) {
         return ContextCompat.checkSelfPermission(context,
                 Manifest.permission.ACCESS_WIFI_STATE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+
+    /**
+     * Starting with Android 14, apps and services that target Android 14 and use context-registered
+     * receivers are required to specify a flag to indicate whether or not the receiver should be
+     * exported to all other apps on the device: either RECEIVER_EXPORTED or RECEIVER_NOT_EXPORTED
+     * <a href="https://developer.android.com/about/versions/14/behavior-changes-14#runtime-receivers-exported"/>
+     */
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
+    public static void compatRegisterReceiver(
+            Context context, BroadcastReceiver receiver, IntentFilter filter, boolean exported) {
+        if (Build.VERSION.SDK_INT >= 34 && context.getApplicationInfo().targetSdkVersion >= 34) {
+            context.registerReceiver(
+                    receiver, filter, exported ? Context.RECEIVER_EXPORTED : Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            context.registerReceiver(receiver, filter);
+        }
     }
 }
