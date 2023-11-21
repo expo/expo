f07267dcc92476790702de77a79a93e024f5c2e4
Cedric van Putten <github@cedric.dev>
2023-11-21 15:30:59 +0100

    refactor(expo-module-scripts): rename babel and jest presets `node` to `cli`

--- packages/expo-module-scripts/jest-preset-cli.js
+++ packages/expo-module-scripts/jest-preset-cli.js
@@ -1,13 +1,12 @@
-const nodePreset = {
+/** @type {import('jest').Config} */
+module.exports = {
   testEnvironment: 'node',
   testRegex: '/__tests__/.*(test|spec)\\.[jt]sx?$',
   transform: {
-    '^.+\\.[jt]sx?$': ['babel-jest', { configFile: require.resolve('./babel.config.node.js') }],
+    '^.+\\.[jt]sx?$': ['babel-jest', { configFile: require.resolve('./babel.config.cli.js') }],
   },
   watchPlugins: [
     require.resolve('jest-watch-typeahead/filename'),
     require.resolve('jest-watch-typeahead/testname'),
   ],
 };
-
-module.exports = nodePreset;
