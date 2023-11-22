c35035fbad82c3e8a9e94133b63cbbafe90d47e4
Cedric van Putten <github@cedric.dev>
2023-11-22 14:56:44 +0100

    fix(env-info): use ESM compatible jest config

--- packages/expo-env-info/jest.config.js
+++ packages/expo-env-info/jest.config.js
@@ -1,7 +1,17 @@
+import cliPreset from 'expo-module-scripts/jest-preset-cli.js';
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+const rootDir = path.dirname(url.fileURLToPath(import.meta.url));
+const { name: displayName } = JSON.parse(
+  fs.readFileSync(path.join(rootDir, 'package.json'), 'utf8')
+);
+
 /** @type {import('jest').Config} */
-module.exports = {
-  ...require('expo-module-scripts/jest-preset-cli'),
-  displayName: require('./package.json').name,
+export default {
+  ...cliPreset,
+  displayName,
   extensionsToTreatAsEsm: ['.ts'],
-  rootDir: __dirname,
+  rootDir,
 };
