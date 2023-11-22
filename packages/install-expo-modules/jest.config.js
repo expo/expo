54a99025731132fdcfdfbfa991c5258679977a7f
Cedric van Putten <github@cedric.dev>
2023-11-22 15:33:14 +0100

    fix(install-expo-modules): use `ts-jest`

--- packages/install-expo-modules/jest.config.js
+++ packages/install-expo-modules/jest.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   ...require('expo-module-scripts/jest-preset-cli'),
+  preset: 'ts-jest',
   displayName: require('./package').name,
   rootDir: path.resolve(__dirname),
   roots: ['src'],
