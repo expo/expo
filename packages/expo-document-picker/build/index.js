7e5f92e0a00d32a2fbd578cb7be91e2f969b40c8
Aman Mittal <amandeepmittal@live.com>
2023-11-27 22:24:33 +0530

    [document-picker][docs] Fix broken internal link to FileSystem reference (#25592)
    # Why
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    Closes ENG-10085
    
    <!-- disable:changelog-checks -->
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    Fix broken links to FileSystem API doc methods from Expo Document
    Picker. Changes applied to unversioned and SDK 49 docs.
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    Run docs locally and visit:
    http://localhost:3002/versions/unversioned/sdk/document-picker/#documentpickeroptions
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/expo-document-picker/build/index.js
+++ packages/expo-document-picker/build/index.js
@@ -1,7 +1,7 @@
 import ExpoDocumentPicker from './ExpoDocumentPicker';
 // @needsAudit
 /**
- * Display the system UI for choosing a document. By default, the chosen file is copied to [the app's internal cache directory](filesystem.md#filesystemcachedirectory).
+ * Display the system UI for choosing a document. By default, the chosen file is copied to [the app's internal cache directory](filesystem/#filesystemcachedirectory).
  * > **Notes for Web:** The system UI can only be shown after user activation (e.g. a `Button` press).
  * > Therefore, calling `getDocumentAsync` in `componentDidMount`, for example, will **not** work as
  * > intended. The `cancel` event will not be returned in the browser due to platform restrictions and
