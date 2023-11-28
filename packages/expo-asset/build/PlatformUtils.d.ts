b3577ceb4eaa27a5c76d3fbb029ca9ee866c8170
James Ide <ide@expo.dev>
2023-11-27 19:06:10 -0800

    [rfc][asset] Remove MD5 checksumming from expo-asset
    Why
    ---
    expo-updates already performs integrity checks using SHA-1. Other assets outside the asset system are not processed by Metro and therefore won't have the `hash` property specified in their asset metadata objects unless the developer has gone out of their way to manually craft or modify the metadata objects after Metro.
    
    See ENG-10755
    
    How
    ---
    Removed the checksumming code. This allowed for the downloading logic for the "managed" and "unmanaged" scenarios to be combined into a single function.
    
    Test Plan
    ---
    Open NCL in Expo Go -> Components -> GL -> Pixi sprite example, which uses expo-asset to load a sprite

--- packages/expo-asset/build/PlatformUtils.d.ts
+++ packages/expo-asset/build/PlatformUtils.d.ts
@@ -9,5 +9,14 @@ export declare function getManifest(): {
 };
 export declare function getManifest2(): typeof Constants.__unsafeNoWarnManifest2 | undefined;
 export declare const manifestBaseUrl: string | null;
-export declare function downloadAsync(uri: any, hash: any, type: any, name: any): Promise<string>;
+/**
+ * Downloads the asset from the given URL to a local cache and returns the local URL of the cached
+ * file.
+ *
+ * If there is already a locally cached file and its MD5 hash matches the given `md5Hash` parameter,
+ * if present, the remote asset is not downloaded. The `hash` property is included in Metro's asset
+ * metadata objects when this module's `hashAssetFiles` plugin is used, which is the typical way the
+ * `md5Hash` parameter of this function is provided.
+ */
+export declare function downloadAsync(url: string, md5Hash: string | null, type: string): Promise<string>;
 //# sourceMappingURL=PlatformUtils.d.ts.map
