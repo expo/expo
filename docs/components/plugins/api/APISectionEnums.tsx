496fbc4269771374d0a86c0259ac4ac31855e2c8
Bartosz Kaszubowski <gosimek@gmail.com>
2023-11-29 16:44:56 +0100

    [docs] APISectionEnums: remove nesting, streamline appearance (#25642)
    # Why
    
    We are moving off nested boxes in our design patterns. Let's start
    altering the API reference components, so they follow the latest design
    direction.
    
    # How
    
    Remove nested boxes with enum values, replace them with box sections,
    add values section header, tweaks.
    
    # Test Plan
    
    The changes have been tested locally. All lint checks and tests are
    passing.
    
    # Preview
    
    <img width="981" alt="Screenshot 2023-11-29 at 15 24 09"
    src="https://github.com/expo/expo/assets/719641/a9b4f9df-6ac4-4bc2-bd5a-1f950ac792ab">
    <img width="981" alt="Screenshot 2023-11-29 at 15 22 59"
    src="https://github.com/expo/expo/assets/719641/fbcf0b94-25c0-41f7-8343-549d48783041">
    <img width="981" alt="Screenshot 2023-11-29 at 15 23 26"
    src="https://github.com/expo/expo/assets/719641/d70ec4b2-5b57-47f5-a6eb-4cb28c736bd9">

--- docs/components/plugins/api/APISectionEnums.tsx
+++ docs/components/plugins/api/APISectionEnums.tsx
@@ -1,5 +1,3 @@
-import { css } from '@emotion/react';
-
 import { EnumDefinitionData, EnumValueData } from '~/components/plugins/api/APIDataTypes';
 import { APISectionDeprecationNote } from '~/components/plugins/api/APISectionDeprecationNote';
 import { APISectionPlatformTags } from '~/components/plugins/api/APISectionPlatformTags';
@@ -7,8 +5,8 @@ import {
   CommentTextBlock,
   getTagNamesList,
   STYLES_APIBOX,
-  STYLES_APIBOX_NESTED,
   H3Code,
+  BoxSectionHeader,
 } from '~/components/plugins/api/APISectionUtils';
 import { H2, H4, CODE, MONOSPACE } from '~/ui/components/Text';
 
@@ -30,18 +28,21 @@ const sortByValue = (a: EnumValueData, b: EnumValueData) => {
 const renderEnumValue = (value: any) => (typeof value === 'string' ? `"${value}"` : value);
 
 const renderEnum = ({ name, children, comment }: EnumDefinitionData): JSX.Element => (
-  <div key={`enum-definition-${name}`} css={[STYLES_APIBOX, enumContentStyles]}>
-    <APISectionDeprecationNote comment={comment} />
-    <APISectionPlatformTags comment={comment} prefix="Only for:" />
-    <H3Code tags={getTagNamesList(comment)}>
-      <MONOSPACE weight="medium">{name}</MONOSPACE>
-    </H3Code>
-    <CommentTextBlock comment={comment} includePlatforms={false} />
+  <div key={`enum-definition-${name}`} css={STYLES_APIBOX} className="!p-0">
+    <div className="px-5 pt-4">
+      <APISectionDeprecationNote comment={comment} />
+      <APISectionPlatformTags comment={comment} prefix="Only for:" />
+      <H3Code tags={getTagNamesList(comment)}>
+        <MONOSPACE weight="medium">{name}</MONOSPACE>
+      </H3Code>
+      <CommentTextBlock comment={comment} includePlatforms={false} />
+      <BoxSectionHeader text={`${name} Values`} className="!mb-0 !border-b-0" />
+    </div>
     {children.sort(sortByValue).map((enumValue: EnumValueData) => (
-      <div css={[STYLES_APIBOX, STYLES_APIBOX_NESTED]} key={enumValue.name}>
+      <div className="border-t border-t-secondary p-5 pb-0 pt-4" key={enumValue.name}>
         <APISectionDeprecationNote comment={enumValue.comment} />
         <APISectionPlatformTags comment={enumValue.comment} prefix="Only for:" />
-        <H4 css={enumValueNameStyle}>
+        <H4 className="!mt-0">
           <CODE>{enumValue.name}</CODE>
         </H4>
         <CODE theme="secondary" className="mb-4">
@@ -60,15 +61,5 @@ const APISectionEnums = ({ data }: APISectionEnumsProps) =>
       {data.map(renderEnum)}
     </>
   ) : null;
-
-const enumValueNameStyle = css({
-  h4: {
-    marginTop: 0,
-  },
-});
-
-const enumContentStyles = css({
-  paddingBottom: 0,
-});
 
 export default APISectionEnums;
