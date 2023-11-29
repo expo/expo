d52fad4ab2cde6522340df28b48f8af7623829dc
Simek <gosimek@gmail.com>
2023-11-29 15:52:46 +0100

    add workaround for Radix, Jest and ESM combo

--- docs/ui/components/Dropdown/index.tsx
+++ docs/ui/components/Dropdown/index.tsx
@@ -1,8 +1,13 @@
 import { mergeClasses } from '@expo/styleguide';
-import DropdownMenu from '@radix-ui/react-dropdown-menu';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import type { ReactNode } from 'react';
 
 import { Item } from '~/ui/components/Dropdown/Item';
+
+// note(simek): Radix Jest ESM issue workaround: https://github.com/radix-ui/primitives/issues/1848
+let sanitizedRadixDropdownMenu = { default: undefined, ...DropdownMenu };
+sanitizedRadixDropdownMenu = sanitizedRadixDropdownMenu.default ?? sanitizedRadixDropdownMenu;
+const { Trigger, Root, Portal, Content, Arrow } = sanitizedRadixDropdownMenu;
 
 type Props = DropdownMenu.DropdownMenuContentProps & {
   trigger: ReactNode;
@@ -18,10 +23,10 @@ export function Dropdown({
   ...rest
 }: Props) {
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>{trigger}</DropdownMenu.Trigger>
-      <DropdownMenu.Portal className="bg-danger">
-        <DropdownMenu.Content
+    <Root>
+      <Trigger asChild>{trigger}</Trigger>
+      <Portal className="bg-danger">
+        <Content
           className={mergeClasses(
             'flex min-w-[180px] flex-col gap-0.5 rounded-md border border-default bg-default p-1 shadow-md',
             'will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFadeIn',
@@ -31,13 +36,13 @@ export function Dropdown({
           sideOffset={sideOffset}
           collisionPadding={collisionPadding}
           {...rest}>
-          <DropdownMenu.Arrow asChild>
+          <Arrow asChild>
             <div className="relative -top-1 h-2.5 w-2.5 rotate-45 border-b border-r border-default bg-default" />
-          </DropdownMenu.Arrow>
+          </Arrow>
           {children}
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
+        </Content>
+      </Portal>
+    </Root>
   );
 }
 
