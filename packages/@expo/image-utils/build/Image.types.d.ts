4727539c293e563d5d6de8ad0adb4f24f0b404e8
Cedric van Putten <me@bycedric.com>
2023-12-04 14:11:19 +0100

    refactor(image-utils): move over from `expo/expo-cli` (#25556)
    # Why
    
    Moving the actively used `@expo/image-utils` over from `expo/expo-cli`.
    
    Counter-part PR: https://github.com/expo/expo-cli/pull/4787
    
    # How
    
    - Copied `@expo/image-utils` from `expo/expo-cli`
    - Integrated `expo-module-scripts`
    - Splitted Jest unit and e2e tests
    - Resolved lining and typing issues
    
    # Test Plan
    
    See if CI passes.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

---
+++ packages/@expo/image-utils/build/Image.types.d.ts
@@ -0,0 +1,13 @@
+export type ResizeMode = 'contain' | 'cover' | 'fill' | 'inside' | 'outside';
+export type ImageFormat = 'input' | 'jpeg' | 'jpg' | 'png' | 'raw' | 'tiff' | 'webp';
+export type ImageOptions = {
+    src: string;
+    name?: string;
+    resizeMode: ResizeMode;
+    backgroundColor: string;
+    removeTransparency?: boolean;
+    width: number;
+    height: number;
+    padding?: number;
+    borderRadius?: number;
+};
