6b5151f18c5ad9cb3f067bbb4e7d090ff1bd907c
Kudo Chien <kudo@expo.dev>
2023-12-06 19:51:46 +0800

    [core] try to improve the stability for ios unit test (#25786)

--- packages/expo-modules-core/ios/Tests/BlobConvertiblesSpec.swift
+++ packages/expo-modules-core/ios/Tests/BlobConvertiblesSpec.swift
@@ -46,9 +46,7 @@ final class DataUint8ArrayConvertiblesSpec: ExpoSpec {
         .eval(
           "expo.modules.BlobModule.echoAsync(new Uint8Array([0x00, 0xff])).then((result) => { globalThis.result = result; })"
         )
-      expect(try runtime.eval("globalThis.result !== undefined").getBool()).toEventually(beTrue())
-      let isUint8Array = try runtime.eval("globalThis.result instanceof Uint8Array").asBool()
-      expect(isUint8Array) == true
+      expect(try runtime.eval("globalThis.result instanceof Uint8Array").getBool()).toEventually(beTrue())
       let array = try runtime.eval("Array.from(globalThis.result)").asArray()
       expect(array[0]?.getInt()) == 0x00
       expect(array[1]?.getInt()) == 0xff
@@ -59,9 +57,7 @@ final class DataUint8ArrayConvertiblesSpec: ExpoSpec {
         .eval(
           "expo.modules.BlobModule.echoMapAsync({ key: new Uint8Array([0x00, 0xff]) }).then((result) => { globalThis.result = result; })"
         )
-      expect(try runtime.eval("globalThis.result !== undefined").getBool()).toEventually(beTrue())
-      let isUint8Array = try runtime.eval("globalThis.result.key instanceof Uint8Array").asBool()
-      expect(isUint8Array) == true
+      expect(try runtime.eval("globalThis.result != null && globalThis.result.key instanceof Uint8Array").getBool()).toEventually(beTrue())
       let array = try runtime.eval("Array.from(globalThis.result.key)").asArray()
       expect(array[0]?.getInt()) == 0x00
       expect(array[1]?.getInt()) == 0xff
