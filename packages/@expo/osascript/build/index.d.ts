fbfc2c9837800c2fe56207fa7afde81374b3a0cd
Cedric van Putten <me@bycedric.com>
2023-12-08 22:49:33 +0100

    refactor(osascript): move to `expo/expo` (#25746)
    # Why
    
    Moving the actively used `@expo/osascript` over from `expo/expo-cli`.
    
    Counter-part PR: https://github.com/expo/expo-cli/pull/4793
    
    # How
    
    - Copied `@expo/osascript` from `expo/expo-cli`
    - Updated `tsconfig.json` to point at this monorepo config
    - Integrated `expo-module-scripts`
    - Added missing `exec-async` type definitons
    
    # Test Plan
    
    See if CI passes.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

---
+++ packages/@expo/osascript/build/index.d.ts
@@ -0,0 +1,16 @@
+import { SpawnOptions, SpawnResult } from '@expo/spawn-async';
+import { ExecAsyncOptions } from 'exec-async';
+declare function osascriptExecAsync(script: string | string[], opts?: ExecAsyncOptions): Promise<string>;
+declare function osascriptSpawnAsync(script: string | string[], opts?: SpawnOptions): Promise<SpawnResult>;
+declare function isAppRunningAsync(appName: string): Promise<boolean>;
+declare function safeIdOfAppAsync(appName: string): Promise<string | null>;
+declare function openFinderToFolderAsync(dir: string, activate?: boolean): Promise<void>;
+declare function openInAppAsync(appName: string, pth: string): Promise<SpawnResult>;
+declare function chooseAppAsync(listOfAppNames: string[]): Promise<string | null>;
+declare function chooseEditorAppAsync(preferredEditor?: string): Promise<string | null>;
+declare function chooseTerminalAppAsync(): Promise<string | null>;
+declare function openInEditorAsync(pth: string, preferredEditor?: string): Promise<SpawnResult>;
+declare function openItermToSpecificFolderAsync(dir: string): Promise<SpawnResult>;
+declare function openTerminalToSpecificFolderAsync(dir: string, inTab?: boolean): Promise<SpawnResult>;
+declare function openFolderInTerminalAppAsync(dir: string, inTab?: boolean): Promise<SpawnResult>;
+export { chooseAppAsync, chooseEditorAppAsync, chooseTerminalAppAsync, osascriptExecAsync as execAsync, isAppRunningAsync, openFinderToFolderAsync, openFolderInTerminalAppAsync, openInAppAsync, openInEditorAsync, openItermToSpecificFolderAsync, openTerminalToSpecificFolderAsync, safeIdOfAppAsync, osascriptSpawnAsync as spawnAsync, };
