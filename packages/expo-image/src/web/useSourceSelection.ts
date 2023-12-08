dc221594cf3b084f49907f1aa6dc59886719a6f1
aleqsio <mikucki@gmail.com>
2023-12-08 16:13:09 +0100

    Fix unnecessary resizeObserver calls

--- packages/expo-image/src/web/useSourceSelection.ts
+++ packages/expo-image/src/web/useSourceSelection.ts
@@ -101,7 +101,7 @@ type UseSourceSelectionReturn = {
 export default function useSourceSelection(
   sources?: ImageSource[],
   responsivePolicy: ImageProps['responsivePolicy'] = 'static',
-  measurementCallback?: (target: HTMLElement, size: DOMRect) => void
+  measurementCallback: ((target: HTMLElement, size: DOMRect) => void) | null = null
 ): UseSourceSelectionReturn {
   const hasMoreThanOneSource = (sources?.length ?? 0) > 1;
   // null - not calculated yet, DOMRect - size available
@@ -116,7 +116,6 @@ export default function useSourceSelection(
 
   const containerRef = React.useCallback(
     (element: HTMLDivElement) => {
-      // we can't short circuit here since we need to read the size for better animated transitions
       if (!hasMoreThanOneSource && !measurementCallback) {
         return;
       }
@@ -136,7 +135,7 @@ export default function useSourceSelection(
         resizeObserver.current.observe(element);
       }
     },
-    [hasMoreThanOneSource, responsivePolicy]
+    [hasMoreThanOneSource, responsivePolicy, measurementCallback]
   );
 
   const source = selectSource(sources, size, responsivePolicy);
