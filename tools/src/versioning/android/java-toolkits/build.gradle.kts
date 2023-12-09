d6dd4f172501330765bacd00ceae6128cee6a921
Kudo Chien <kudo@expo.dev>
2023-12-09 22:50:16 +0800

    [android][tools] fix expo-go versioning for android (#25811)
    # Why
    
    fix android expo go versioning for sdk 50
    
    # How
    
    - fix transforms
    - introduce a `kotlin-metadata-stripper` tool because `jarjar` cannot
    rename package names in kotlin.Metadata annotations. the react-native
    and some 3rd party code use kotlin will read the package name from
    kotlin metadata. the new tool will strip the kotlin metadata from
    react-native aar.
    
    # Test Plan
    
    `et add-sdk -p android -s 50.0.0` + versioned expo go + sdk 50 NCL
    
    ---------
    
    Co-authored-by: Gabriel Donadel <donadeldev@gmail.com>
    Co-authored-by: Will Schurman <wschurman@expo.io>

---
+++ tools/src/versioning/android/java-toolkits/build.gradle.kts
@@ -0,0 +1,13 @@
+allprojects {
+  repositories {
+    mavenCentral()
+  }
+}
+
+repositories {
+  // Use the plugin portal to apply community plugins in convention plugins.
+  gradlePluginPortal()
+}
+
+dependencies {
+}
