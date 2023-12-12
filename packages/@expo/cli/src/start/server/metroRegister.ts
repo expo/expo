16f704f6dbfc3128ba47ef6f774f62d3072c043a
evanbacon <bacon@expo.io>
2023-12-12 17:03:33 -0600

    Update metroRegister.ts

--- packages/@expo/cli/src/start/server/metroRegister.ts
+++ packages/@expo/cli/src/start/server/metroRegister.ts
@@ -7,12 +7,14 @@ export function registerRuntime() {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     // Don't compile node_modules.
     ignore: [],
+
     only: [
       // // File paths that **don't** match this regex are not compiled
       /(node_modules|packages)\/expo\/build\//,
       /(node_modules|packages)\/@expo\//,
       /(node_modules|packages)\/expo-[\w\d-_.]+\/build\//,
       /(node_modules|packages)\/expo-router\//,
+      /\/node_modules\/react-native-web\//,
       /\/node_modules\/react-native\//,
       /\/node_modules\/@react-native\//,
     ],
@@ -23,6 +25,42 @@ export function registerRuntime() {
     // Use the default babel config.
     babelrc: false,
     configFile: false,
+
+    caller: {
+      name: 'metro',
+      bundler: 'metro',
+      platform: 'web',
+      // Empower the babel preset to know the env it's bundling for.
+      // Metro automatically updates the cache to account for the custom transform options.
+      isServer: true,
+
+      // The base url to make requests from, used for hosting from non-standard locations.
+      baseUrl: '',
+
+      // Ensure we always use a mostly-valid router root.
+      routerRoot: 'app',
+
+      isDev: true,
+
+      // This value indicates if the user has disabled the feature or not.
+      // Other criteria may still cause the feature to be disabled, but all inputs used are
+      // already considered in the cache key.
+      preserveEnvVars: false,
+      asyncRoutes: false,
+      // Pass the engine to babel so we can automatically transpile for the correct
+      // target environment.
+      engine: 'default',
+
+      // Provide the project root for accurately reading the Expo config.
+      projectRoot: process.cwd(),
+
+      // isNodeModule,
+
+      isHMREnabled: false,
+
+      // Set the standard Babel flag to disable ESM transformations.
+      // supportsStaticESM: false,
+    },
     presets: [require.resolve('babel-preset-expo')],
     plugins: [[require('babel-plugin-transform-require-context')]],
   });
