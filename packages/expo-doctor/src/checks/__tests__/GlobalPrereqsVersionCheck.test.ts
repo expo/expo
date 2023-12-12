a6a544cc2dfcaef9454cb1e2c50346017d84698d
Keith Kurak <keith.kurak@gmail.com>
2023-12-12 10:27:02 -0500

    [doctor] Report if project has unused static config (#25674)
    # Why
    We added a flag in `@expo/config`
    (https://github.com/expo/expo/pull/24308) to report if there was a
    possible unused static config, with the intent of exposing the warning
    in doctor. This change exposes the warning.
    
    # How
    Passed the additional parameter to the Expo Config common issues check
    along with the config filenames, added the warning if the flag is true.
    
    # Test Plan
    <img width="801" alt="image"
    src="https://github.com/expo/expo/assets/8053974/8a40fbba-519a-4c6b-8f9e-97ab33601982">
    
    # Checklist
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/expo-doctor/src/checks/__tests__/GlobalPrereqsVersionCheck.test.ts
+++ packages/expo-doctor/src/checks/__tests__/GlobalPrereqsVersionCheck.test.ts
@@ -13,6 +13,9 @@ const additionalProjectProps = {
     slug: 'slug',
   },
   pkg: {},
+  hasUnusedStaticConfig: false,
+  staticConfigPath: null,
+  dynamicConfigPath: null,
 };
 
 describe('runAsync', () => {
