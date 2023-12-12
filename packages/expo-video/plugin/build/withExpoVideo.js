f88b5f51ebb24be9b53edb6e976642ca56398d7a
Brent Vatne <brentvatne@gmail.com>
2023-12-11 19:23:29 -0800

    [video] Commit build file for plugin [skip ci]

--- packages/expo-video/plugin/build/withExpoVideo.js
+++ packages/expo-video/plugin/build/withExpoVideo.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const config_plugins_1 = require("@expo/config-plugins");
 const withExpoVideo = (config) => {
-    (0, config_plugins_1.withInfoPlist)(config, config => {
+    (0, config_plugins_1.withInfoPlist)(config, (config) => {
         const currentBackgroundModes = config.modResults.UIBackgroundModes ?? [];
         if (!currentBackgroundModes.includes('audio')) {
             config.modResults.UIBackgroundModes = [...currentBackgroundModes, 'audio'];
