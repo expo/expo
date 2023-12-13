5a7c8374225d6d5e9c9f28c86dfe49b09f2f4ed3
Gabriel Donadel <donadeldev@gmail.com>
2023-12-12 23:57:24 -0300

    [store-review][ios] Simplify isAvailableAsync logic

--- packages/expo-store-review/ios/StoreReviewModule.swift
+++ packages/expo-store-review/ios/StoreReviewModule.swift
@@ -2,8 +2,6 @@ import ExpoModulesCore
 import StoreKit
 
 public class StoreReviewModule: Module {
-  let plist = StoreReviewModule.readProvisioningProfilePlist()
-
   public func definition() -> ModuleDefinition {
     Name("ExpoStoreReview")
 
@@ -25,52 +23,14 @@ public class StoreReviewModule: Module {
   }
 
   private func isRunningFromTestFlight() -> Bool {
-    let isSandbox = Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
+    #if targetEnvironment(simulator)
+    return false
+    #endif
 
-    if Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") == nil {
-      #if targetEnvironment(simulator)
-      return false
-      #else
-      return isSandbox
-      #endif
-    }
-
-    guard let mobileProvision = plist else {
-      return false
-    }
-
-    if let provisionsAllDevices = mobileProvision["ProvisionsAllDevices"] as? Bool, provisionsAllDevices {
-      return false
-    }
-
-    if let provisionedDevices = mobileProvision["ProvisionedDevices"] as? [String], !provisionedDevices.isEmpty {
-      return false
-    }
-
-    return isSandbox
+    return Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt" && !hasEmbeddedMobileProvision()
   }
 
-  private static func readProvisioningProfilePlist() -> [String: Any]? {
-    guard let profilePath = Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") else {
-      return nil
-    }
-
-    do {
-      let profileString = try String(contentsOfFile: profilePath, encoding: .ascii)
-      guard let plistStart = profileString.range(of: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"),
-        let plistEnd = profileString.range(of: "</plist>") else {
-        return nil
-      }
-
-      let plistString = String(profileString[plistStart.lowerBound..<plistEnd.upperBound])
-      if let plistData = plistString.data(using: .utf8) {
-        return try PropertyListSerialization.propertyList(from: plistData, options: [], format: nil) as? [String: Any]
-      }
-      log.error("Failed to convert plistString to UTF-8 encoded data object.")
-      return nil
-    } catch {
-      log.error("Error reading provisioning profile: \(error.localizedDescription)")
-      return nil
-    }
+  private func hasEmbeddedMobileProvision() -> Bool {
+    return (Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") != nil)
   }
 }
