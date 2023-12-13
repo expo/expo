06d97b3d85a19c809ca5726c2ebd872d1c606715
Gabriel Donadel Dall'Agnol <gabriel@expo.io>
2023-12-13 19:48:25 -0300

    [asset] Fix asset source uri tests (#25916)

--- packages/jest-expo/src/preset/internalExpoModules.js
+++ packages/jest-expo/src/preset/internalExpoModules.js
@@ -1,9 +1,4 @@
 module.exports = {
-  ExponentKernel: {
-    getSessionAsync: { type: 'function', functionType: 'async' },
-    removeSessionAsync: { type: 'function', functionType: 'async' },
-    setSessionAsync: { type: 'function', functionType: 'async' },
-  },
   DevLoadingView: {
     getConstants: { type: 'function' },
     addListener: { type: 'function', functionType: 'async' },
