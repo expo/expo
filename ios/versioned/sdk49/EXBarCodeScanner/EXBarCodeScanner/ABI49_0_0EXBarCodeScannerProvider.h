9eb8cd4f861e0383a05283f1352e8a4e2b537ba4
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2023-12-13 12:49:22 +0000

    Remove SDK 49 on iOS

--- ios/versioned/sdk49/EXBarCodeScanner/EXBarCodeScanner/ABI49_0_0EXBarCodeScannerProvider.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-present 650 Industries. All rights reserved.
-
-#import <Foundation/Foundation.h>
-#import <ABI49_0_0ExpoModulesCore/ABI49_0_0EXInternalModule.h>
-#import <ABI49_0_0ExpoModulesCore/ABI49_0_0EXBarcodeScannerProviderInterface.h>
-
-@interface ABI49_0_0EXBarCodeScannerProvider : NSObject <ABI49_0_0EXInternalModule, ABI49_0_0EXBarCodeScannerProviderInterface>
-
-@end
