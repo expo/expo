89a878abdbe54f99a414611744feb9addac0eb4b
Gabriel Donadel <donadeldev@gmail.com>
2023-12-13 09:42:01 -0300

    Add comments and changelog entry

--- packages/expo-store-review/ios/StoreReviewModule.swift
+++ packages/expo-store-review/ios/StoreReviewModule.swift
@@ -27,7 +27,13 @@ public class StoreReviewModule: Module {
     return false
     #endif
 
-    return Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt" && !hasEmbeddedMobileProvision()
+    // For apps distributed through TestFlight or intalled from Xcode the receipt file is named "StoreKit/sandboxReceipt"
+    // instead of "StoreKit/receipt"
+    let isSandboxEnv = Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt"
+
+    // Apps distributed through TestFlight or the App Store will not have an embedded provisioning profile
+    // Source: https://developer.apple.com/documentation/technotes/tn3125-inside-code-signing-provisioning-profiles#Profile-location
+    return isSandboxEnv && !hasEmbeddedMobileProvision()
   }
 
   private func hasEmbeddedMobileProvision() -> Bool {
