b029de00ba7006db5bfc5f6ce9091229ef0e859c
Alfonso Curbelo <alfonso@curbelo.me>
2023-12-15 14:17:11 -0500

    minor fix and changelog

--- packages/@expo/config-plugins/src/utils/generateCode.ts
+++ packages/@expo/config-plugins/src/utils/generateCode.ts
@@ -10,8 +10,8 @@ function getGeneratedSectionIndexes(
   tag: string
 ): { contents: string[]; start: number; end: number } {
   const contents = src.split('\n');
-  const start = contents.findIndex((line) => RegExp(`@generated begin ${tag} -`).test(line));
-  const end = contents.findIndex((line) => RegExp(`@generated end ${tag}$`).test(line));
+  const start = contents.findIndex((line) => new RegExp(`@generated begin ${tag} -`).test(line));
+  const end = contents.findIndex((line) => new RegExp(`@generated end ${tag}$`).test(line));
 
   return { contents, start, end };
 }
