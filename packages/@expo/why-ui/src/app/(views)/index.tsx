2f3668e494b8f109f8d447d5258cb8372f7157e8
evanbacon <bacon@expo.io>
2023-12-16 23:43:24 -0600

    wip - better profile

--- packages/@expo/why-ui/src/app/(views)/index.tsx
+++ packages/@expo/why-ui/src/app/(views)/index.tsx
@@ -1,73 +1,23 @@
 import { useFilteredModules } from '@/components/deps-context';
-import moment from 'moment';
+import * as echarts from 'echarts';
+import ReactECharts from 'echarts-for-react';
 import * as React from 'react';
-import ReactApexChart from 'react-apexcharts';
-// make sure you include the timeline stylesheet or the timeline will not be styled
-// import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react';
-// import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react';
-const TimelineItem = ({ item, totalDuration }) => {
-  // Calculate width and left offset based on start/end times
-  const width = ((item.end - item.start) / totalDuration) * 100;
-  const left = (item.start / totalDuration) * 100;
 
-  return (
-    <div
-      className={`absolute ${item.color} text-white p-1 text-xs`}
-      style={{
-        left: `${left}%`,
-        width: `${width}%`,
-        top: `${item.thread * 1.5}rem`, // Adjust 1.5rem to control the spacing between threads
-      }}>
-      {item.name}
-    </div>
-  );
-};
-
-const ProfilerUI = ({ items }) => {
-  const totalDuration = Math.max(...items.map((item) => item.end));
-
-  return (
-    <div className="relative h-64 w-full bg-black overflow-x-auto">
-      {items.map((item, index) => (
-        <TimelineItem key={index} item={item} totalDuration={totalDuration} />
-      ))}
-    </div>
-  );
-};
-
-const groups = [
-  { id: 1, title: 'group 1' },
-  { id: 2, title: 'group 2' },
-];
-
-const items = [
-  {
-    id: 1,
-    group: 1,
-    title: 'item 1',
-    start_time: moment(),
-    end_time: moment().add(1, 'hour'),
-  },
-  {
-    id: 2,
-    group: 2,
-    title: 'item 2',
-    start_time: moment().add(-0.5, 'hour'),
-    end_time: moment().add(0.5, 'hour'),
-  },
-  {
-    id: 3,
-    group: 1,
-    title: 'item 3',
-    start_time: moment().add(2, 'hour'),
-    end_time: moment().add(3, 'hour'),
-  },
-];
+function getColor(fileName: string): string {
+  if (fileName.match(/node_modules/)) {
+    return '#008FFB';
+    // ['#008FFB', '#fff', '#546E7A', '#26a69a', '#775DD0']
+  }
+  if (fileName.endsWith('.js')) {
+    return '#26a69a';
+  }
+  return '#775DD0';
+}
 
 export default function DataTableDemo() {
   const data = useFilteredModules();
 
-  const items = React.useMemo(() => {
+  const { threads, items } = React.useMemo(() => {
     let items = data
       .map((m) => {
         const profilingData = m.output[0]?.data?.profiling;
@@ -89,7 +39,7 @@ export default function DataTableDemo() {
 
     // thread is in the format of random numbers like `20361`, `20356`, `20355`. We need to normalize them to be `0`, `1`, `2` etc.
     const threads = items.map((item) => item.thread);
-    const uniqueThreads = [...new Set(threads)];
+    const uniqueThreads = [...new Set(threads)].sort().reverse();
     const threadMap = uniqueThreads.reduce((acc, thread, index) => {
       acc[thread] = index + 1;
       return acc;
@@ -98,128 +48,125 @@ export default function DataTableDemo() {
     items.forEach((item) => {
       item.thread = threadMap[item.thread];
     });
-    return items;
+    return { threads: Object.values(threadMap).sort().reverse(), items };
   }, [data]);
 
+  function renderItem(params, api) {
+    const categoryIndex = api.value(0);
+    const start = api.coord([api.value(1), categoryIndex]);
+    const end = api.coord([api.value(2), categoryIndex]);
+    const height = api.size([0, 1])[1] * 0.6;
+    // return null;
+    const rectShape = echarts.graphic.clipRectByRect(
+      {
+        x: start[0],
+        y: start[1] - height / 2,
+        width: end[0] - start[0],
+        height,
+      },
+      {
+        x: params.coordSys.x,
+        y: params.coordSys.y,
+        width: params.coordSys.width,
+        height: params.coordSys.height,
+      }
+    );
+    return (
+      rectShape && {
+        type: 'rect',
+        transition: ['shape'],
+        shape: rectShape,
+        style: api.style(),
+      }
+    );
+  }
+
+  const start = items.reduce((acc, item) => Math.min(acc, item.start), Infinity);
+
+  const container = React.useRef<HTMLDivElement>(null);
+
+  const containerHeight = React.useMemo(() => {
+    if (!container.current) {
+      return 300;
+    }
+    return container.current.clientHeight;
+  }, [container.current]);
+
   return (
-    <div className="w-full p-4">
-      {/* <div className="flex flex-1 space-x-2 py-4"> */}
-      {/* <ProfilerUI items={items} /> */}
-
-      <ReactApexChart
-        options={{
-          chart: {
-            height: 600,
-            type: 'rangeBar',
-          },
-          xaxis: {
-            type: 'datetime',
-
-            axisBorder: {
-              show: false,
-              color: '#78909C',
-            },
-            labels: {
-              style: {
-                fontSize: '8px',
-                colors: '#78909C',
-              },
-              formatter(val, options) {
-                if (typeof options === 'object' && 'dataPointIndex' in options) {
-                  return items[options.dataPointIndex].name;
-                }
-
-                return '';
-              },
-              show: true,
-            },
-            // Adjust to maximize the space for the items
-            min: Math.min(...items.map((item) => item.start)),
-            max: Math.max(...items.map((item) => item.end)),
-          },
-          yaxis: {
-            show: false,
-
-            // labels: {
-            //   // formatter: function (val) {
-            //   //   console.log('ar', arguments);
-            //   //   return '';
-            //   //   // return items[dataPointIndex].name;
-            //   // },
-            // },
-          },
-          grid: {
-            // Style grid for black background
-            borderColor: '#535A60',
-            strokeDashArray: 0,
-            xaxis: {
-              lines: {
-                show: true,
+    <div className="w-full m-4 relative flex flex-1 bg-white" ref={container}>
+      <div className="flex-1">
+        <ReactECharts
+          lazyUpdate
+          opts={{
+            // renderer: 'svg',
+            height: containerHeight,
+          }}
+          theme="dark"
+          loadingOption={{}}
+          option={{
+            tooltip: {
+              formatter(params) {
+                return params.marker + params.name + ': ' + params.value[3] + ' ms';
               },
             },
-            yaxis: {
-              lines: {
-                show: false,
+            title: {
+              text: '',
+              left: 'center',
+            },
+            dataZoom: [
+              {
+                type: 'slider',
+                filterMode: 'weakFilter',
+                showDataShadow: false,
+                top: 500,
+                labelFormatter: '',
+              },
+              {
+                type: 'inside',
+                filterMode: 'weakFilter',
+              },
+            ],
+            grid: {
+              height: 400,
+            },
+            xAxis: {
+              min: start,
+              scale: true,
+              axisLabel: {
+                formatter(val) {
+                  return Math.max(0, val - start) + ' ms';
+                },
               },
             },
-            padding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
+            yAxis: {
+              boundaryGap: false,
+              data: threads.map((thread) => 'Thread: ' + String(thread)),
             },
-          },
-          dataLabels: {
-            enabled: true,
-
-            formatter: function (val, { dataPointIndex }) {
-              return items[dataPointIndex].name;
-            },
-          },
-
-          colors: [
-            '#008FFB',
-            '#fff',
-            '#546E7A',
-            '#26a69a',
-            // '#775DD0',
-          ],
-          markers: {
-            size: 0,
-          },
-          plotOptions: {
-            bar: {
-              borderRadius: 1,
-              distributed: true,
-              horizontal: true,
-              barHeight: '90%',
-
-              rangeBarGroupRows: true,
-              dataLabels: {
-                // TODO: Better labels
-                hideOverflowingLabels: true,
+            series: [
+              {
+                type: 'custom',
+                renderItem,
+                itemStyle: {
+                  opacity: 1,
+                },
+                encode: {
+                  x: [1, 2],
+                  y: 0,
+                },
+                data: items.map((item) => ({
+                  name: item.name,
+                  value: [item.thread, item.start, item.end, item.end - item.start],
+                  itemStyle: {
+                    normal: {
+                      color: getColor(item.name),
+                    },
+                  },
+                })),
               },
-              // Normalize the data points
-            },
-          },
-          // xaxis: {
-          //   type: 'datetime',
-          // },
-        }}
-        series={[
-          {
-            data: items.map((item) => ({
-              x: 'Thread: ' + String(item.thread),
-              y: [item.start, item.end],
-              meta: item.name,
-              columnWidthOffset: 0,
-            })),
-          },
-        ]}
-        type="rangeBar"
-        height={350}
-      />
-      {/* </div> */}
+            ],
+          }}
+        />
+      </div>
     </div>
   );
 }
