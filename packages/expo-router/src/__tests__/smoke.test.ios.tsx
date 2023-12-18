b5bdb2b9a43e3a6e5065bd9b9dc5314930fa9c38
Mark Lawlor <mwlawlor@gmail.com>
2023-12-19 09:57:12 +1000

    (router): fix drawer navigator navigation (#25985)
    # Why
    
    Fix: #25981
    
    # How
    
    To be honest, I'm not 100% sure why its broken in the current state. It
    fires a navigation events on the shared Stack with what seems like the
    correct params.
    
    But we also don't need to track the last common Stack/Tab
    lastCommonNavigator. Pre-#24457, we used to fire navigation events on
    the root navigator, so #24457 tried to not deviate from this behaviour
    by only changing the behaviour Stacks&Tabs doing replace actions. #24600
    cleaned up the logic of #24457 and unified the logic for all navigation
    types, but still only tracked the last common Stack/Tabs. I don't know
    why this breaks Drawer, it might be an internal implementation detail,
    but we can simply fire the event on the last common navigator - no
    matter what type it is.
    
    # Test Plan
    
    Add a smoke test for `<Drawer />`. I don't believe it was being
    previously tested.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-router/src/__tests__/smoke.test.ios.tsx
+++ packages/expo-router/src/__tests__/smoke.test.ios.tsx
@@ -1,6 +1,7 @@
 import React, { Text } from 'react-native';
 
 import { Slot, router, useGlobalSearchParams } from '../exports';
+import { Drawer } from '../layouts/Drawer';
 import { Stack } from '../layouts/Stack';
 import { Tabs } from '../layouts/Tabs';
 import { Redirect } from '../link/Link';
@@ -166,4 +167,46 @@ it('deep linking nested groups', async () => {
   expect(OtherTabsLayout).toHaveBeenCalledTimes(1);
   expect(NestedTabsLayout).toHaveBeenCalledTimes(1);
   expect(OtherTabsIndex).toHaveBeenCalledTimes(1);
+});
+
+it('can navigate across the drawer navigator', () => {
+  renderRouter({
+    _layout: () => <Stack />,
+    index: () => <Text testID="index" />,
+    '(group)/_layout': () => <Drawer useLegacyImplementation={false} />,
+    '(group)/one': () => <Text testID="one" />,
+    '(group)/two': () => <Text testID="two" />,
+    '(group_two)/three': () => <Text testID="three" />,
+    '(group_two)/_layout': () => <Drawer useLegacyImplementation={false} />,
+    '(group_two)/nested/folder/_layout': () => <Drawer useLegacyImplementation={false} />,
+    '(group_two)/nested/folder/four': () => <Text testID="four" />,
+  });
+
+  expect(screen).toHavePathname('/');
+  expect(screen.getByTestId('index')).toBeOnTheScreen();
+
+  // Navigate to a drawer screen
+  act(() => router.push('/one'));
+  expect(screen).toHavePathname('/one');
+  expect(screen.getByTestId('one')).toBeOnTheScreen();
+
+  // Navigate within the drawer
+  act(() => router.push('/two'));
+  expect(screen).toHavePathname('/two');
+  expect(screen.getByTestId('two')).toBeOnTheScreen();
+
+  // Navigate to a different drawer
+  act(() => router.push('/three'));
+  expect(screen).toHavePathname('/three');
+  expect(screen.getByTestId('three')).toBeOnTheScreen();
+
+  // Navigate to a nested folder
+  act(() => router.push('/nested/folder/four'));
+  expect(screen).toHavePathname('/nested/folder/four');
+  expect(screen.getByTestId('four')).toBeOnTheScreen();
+
+  // Navigate back to one
+  act(() => router.push('/one'));
+  expect(screen).toHavePathname('/one');
+  expect(screen.getByTestId('one')).toBeOnTheScreen();
 });
