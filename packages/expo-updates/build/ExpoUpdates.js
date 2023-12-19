828cfb9b4c92b01d6e3a7328b235822ebf74f9c2
Will Schurman <wschurman@expo.io>
2023-12-19 12:36:34 -0800

    [expo-updates] Migrate to requireNativeModule/requireOptionalNativeModule (#25648)

--- packages/expo-updates/build/ExpoUpdates.js
+++ packages/expo-updates/build/ExpoUpdates.js
@@ -1,3 +1,3 @@
-import { NativeModulesProxy } from 'expo-modules-core';
-export default NativeModulesProxy.ExpoUpdates ?? {};
+import { requireNativeModule } from 'expo-modules-core';
+export default requireNativeModule('ExpoUpdates');
 //# sourceMappingURL=ExpoUpdates.js.map
