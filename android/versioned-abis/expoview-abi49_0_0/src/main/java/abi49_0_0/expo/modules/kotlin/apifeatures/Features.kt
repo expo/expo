de483590ac2a9348f20e62cecc7631a8ff48a97e
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-12-19 18:05:11 +0100

    [Android] Remove SDK 49 code (#25911)

--- android/versioned-abis/expoview-abi49_0_0/src/main/java/abi49_0_0/expo/modules/kotlin/apifeatures/Features.kt
+++
@@ -1,6 +0,0 @@
-package abi49_0_0.expo.modules.kotlin.apifeatures
-
-@RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.", level = RequiresOptIn.Level.WARNING)
-@Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-annotation class EitherType
--- android/versioned-abis/expoview-abi49_0_0/src/main/java/abi49_0_0/expo/modules/kotlin/typedarray/TypedArrayIterator.kt
+++
@@ -1,9 +0,0 @@
-package abi49_0_0.expo.modules.kotlin.typedarray
-
-class TypedArrayIterator<T>(private val typedArray: GenericTypedArray<T>) : Iterator<T> {
-  private var current = 0
-
-  override fun hasNext(): Boolean = current < typedArray.length
-
-  override fun next(): T = typedArray[current++]
-}
