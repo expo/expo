2d8ca8d63f74e1bd8feed4273b1baa4c8b6dc12c
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2023-12-20 15:23:12 +0100

    Remove exponent-view-template folder (#26053)

--- exponent-view-template/ios/Build-Phases/prepare-detached-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-yarn run expo prepare-detached-build --platform ios "${SRCROOT}/.."
--- exponent-view-template/ios/exponent-view-template/AppDelegate.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
-
-#import <UIKit/UIKit.h>
-#import <ExpoKit/EXStandaloneAppDelegate.h>
-
-@interface AppDelegate : EXStandaloneAppDelegate <UIApplicationDelegate>
-
-@end
