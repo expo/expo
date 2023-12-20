dadeb70f33f7454cd9aca2b1a176db94d293ce4c
Will Schurman <wschurman@gmail.com>
2023-12-19 17:19:45 -0800

    [expo-updates][ios][3/n] Remove classic updates

--- packages/expo-manifests/ios/EXManifests/ManifestFactory.swift
+++ packages/expo-manifests/ios/EXManifests/ManifestFactory.swift
@@ -6,9 +6,7 @@ import Foundation
 @objcMembers
 public class ManifestFactory: NSObject {
   public static func manifest(forManifestJSON: [String: Any]) -> Manifest {
-    if forManifestJSON["releaseId"] != nil {
-      return LegacyManifest(rawManifestJSON: forManifestJSON)
-    } else if forManifestJSON["metadata"] != nil {
+    if forManifestJSON["metadata"] != nil {
       return NewManifest(rawManifestJSON: forManifestJSON)
     } else {
       return BareManifest(rawManifestJSON: forManifestJSON)
