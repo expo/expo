d5cbb9d0261f2585896b1f8718a7d01d717fcfaf
Cedric van Putten <github@cedric.dev>
2023-12-20 22:56:23 +0100

    refactor(bare-expo): remove unnecessary configuration from metro config

--- apps/bare-expo/metro.config.js
+++ apps/bare-expo/metro.config.js
@@ -1,19 +1,12 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+// Learn more https://docs.expo.dev/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-
 const config = getDefaultConfig(__dirname);
-const root = path.join(__dirname, '../..');
 
-config.watchFolders = [
-  __dirname,
-  ...['packages', 'apps/test-suite', 'apps/native-component-list', 'node_modules'].map((folder) =>
-    path.join(root, folder)
-  ),
-];
+config.resolver.assetExts.push(
+  'db', // See: ../test-suite/assets/asset-db.db
+  'kml' // See: ../native-component-list/assets/expo-maps/sample_kml.kml
+);
 
-config.transformer.enableBabelRCLookup = false;
-config.resolver.assetExts.push('db', 'kml');
 config.resolver.blockList = [
   // Exclude react-native-lab from haste map.
   // Because react-native versions may be different between node_modules/react-native and react-native-lab,
@@ -25,32 +18,5 @@ config.resolver.blockList = [
   /\/android\/React(Android|Common)\//,
   /\/versioned-react-native\//,
 ];
-
-config.server.enhanceMiddleware = (middleware) => {
-  return (req, res, next) => {
-    // When an asset is imported outside the project root, it has wrong path on Android
-    // This happens for the back button in stack, so we fix the path to correct one
-    const assets = '/node_modules/@react-navigation/elements/src/assets';
-
-    if (req.url.startsWith(assets)) {
-      req.url = req.url.replace(assets, `/assets/../..${assets}`);
-    }
-
-    // Same as above when testing anything required via Asset.downloadAsync() in test-suite
-    const testSuiteAssets = '/test-suite/assets/';
-
-    if (req.url.startsWith(testSuiteAssets)) {
-      req.url = req.url.replace(testSuiteAssets, '/assets/../test-suite/assets/');
-    }
-
-    const nclAssets = '/native-component-list/';
-
-    if (req.url.startsWith(nclAssets)) {
-      req.url = req.url.replace(nclAssets, '/assets/../native-component-list/');
-    }
-
-    return middleware(req, res, next);
-  };
-};
 
 module.exports = config;
