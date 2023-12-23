fafffe28bd0249f2640bac7acfd2061b9eb308c5
Jakov Glavina <jakov.glavina@icloud.com>
2023-12-23 15:09:03 +0100

    run swiftlint

--- packages/expo-screen-capture/ios/ScreenCaptureModule.swift
+++ packages/expo-screen-capture/ios/ScreenCaptureModule.swift
@@ -9,20 +9,20 @@ public final class ScreenCaptureModule: Module {
 
   public func definition() -> ModuleDefinition {
     Name("ExpoScreenCapture")
-    
+
     Events(onScreenshotEventName)
-    
+
     OnCreate {
       let boundLength = max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)
       _blockView.frame = CGRect(x: 0, y: 0, width: boundLength, height: boundLength)
       _blockView.backgroundColor = UIColor.black
-      
+
     }
-    
+
     OnStartObserving {
       self.setIsBeingObserved(isBeingObserved: true)
     }
-    
+
     OnStopObserving {
       self.setIsBeingObserved(isBeingObserved: false)
     }
@@ -30,43 +30,42 @@ public final class ScreenCaptureModule: Module {
     AsyncFunction("preventScreenCapture") {
       // If already recording, block it
       self.preventScreenRecording()
-      
+
       NotificationCenter.default.addObserver(self, selector: #selector(self.preventScreenRecording), name: UIScreen.capturedDidChangeNotification, object: nil)
     }.runOnQueue(.main)
-    
+
     AsyncFunction("allowScreenCapture") {
       NotificationCenter.default.removeObserver(self, name: UIScreen.capturedDidChangeNotification, object: nil)
     }
   }
-  
+
   func setIsBeingObserved(isBeingObserved: Bool) {
     _isBeingObserved = isBeingObserved
     let shouldListen = _isBeingObserved
-    
-    if (shouldListen && !_isListening) {
+
+    if shouldListen && !_isListening {
       NotificationCenter.default.addObserver(self, selector: #selector(self.listenForScreenCapture), name: UIApplication.userDidTakeScreenshotNotification, object: nil)
       _isListening = true
-    } else if (!shouldListen && _isListening) {
+    } else if !shouldListen && _isListening {
       NotificationCenter.default.removeObserver(self, name: UIApplication.userDidTakeScreenshotNotification, object: nil)
       _isListening = false
     }
   }
-  
+
   @objc
   func preventScreenRecording() {
     let isCaptured = UIScreen.main.isCaptured
-  
-    if (isCaptured) {
+
+    if isCaptured {
       UIApplication.shared.keyWindow?.subviews.first?.addSubview(_blockView)
     } else {
       _blockView.removeFromSuperview()
     }
   }
-  
+
   @objc func listenForScreenCapture() {
     sendEvent(onScreenshotEventName, [
       "body": nil
     ])
   }
 }
-
