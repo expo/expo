7a992ff350f066dd0a2992a7d39afa4e85938d84
Cedric van Putten <me@bycedric.com>
2023-12-27 14:55:44 +0100

    refactor(expo-dev-client): drop `expo-yarn-workspaces` (#25845)
    # Why
    
    This PR drops `expo-yarn-workspace` usage within the `expo-dev-client`
    packages.
    
    # How
    
    - Dropped `expo-yarn-workspaces` from:
      - `packages/expo-dev-client`
      - `packages/expo-dev-launcher`
      - `packages/expo-dev-menu`
    - Updated `metro.config.js` _with_ modifications from
    `expo-yarn-workspaces` (should be identical configs)
    
    # Test Plan
    
    TBD
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/expo-dev-launcher/metro.config.js
+++ packages/expo-dev-launcher/metro.config.js
@@ -1,15 +1,26 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces');
+const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
-const config = createMetroConfiguration(__dirname);
+const config = getDefaultConfig(__dirname);
 
 const { EXPO_BUNDLE_APP } = process.env;
 
 if (EXPO_BUNDLE_APP) {
   config.transformer.enableBabelRCLookup = true;
+} else {
+  // Copied from expo-yarn-workspaces
+  config.transformer.enableBabelRCLookup = false;
 }
 
-config.resolver.blockList.push(/\breact-native-lab\b/);
+config.resolver.blockList = [
+  // Copied from expo-yarn-workspaces
+  /\/__tests__\//,
+  /\/android\/React(Android|Common)\//,
+  /\/versioned-react-native\//,
+
+  /\breact-native-lab\b/,
+];
+
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   if (platform === 'ios' && /Components\/StatusBar\/StatusBar/.test(moduleName)) {
     console.log(`Replacing ${moduleName} with NOOP`);
@@ -20,5 +31,8 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
   }
   return context.resolveRequest(context, moduleName, platform);
 };
+
+// Copied from expo-yarn-workspaces
+config.resolver.assetExts.push('db');
 
 module.exports = config;
