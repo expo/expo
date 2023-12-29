c19c145765f30a7216ed23a42364480cb8ec699a
evanbacon <bacon@expo.io>
2023-12-29 14:46:55 +0100

    update

--- packages/@expo/why-ui/metro.config.js
+++ packages/@expo/why-ui/metro.config.js
@@ -1,10 +1,10 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces');
+const { getDefaultConfig } = require('expo/metro-config');
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = createMetroConfiguration(projectRoot, {
+const config = getDefaultConfig(projectRoot, {
   isCSSEnabled: true,
 });
 
@@ -16,7 +16,7 @@ config.resolver.unstable_enablePackageExports = true;
 config.watchFolders = [projectRoot, ...['packages', 'node_modules'].map((v) => path.join(root, v))];
 
 config.resolver.blockList = [
-  ...config.resolver.blockList,
+  // ...config.resolver.blockList,
 
   /node_modules\/@react-navigation\/native-stack\/node_modules\/@react-navigation\//,
   /packages\/expo-router\/node_modules\/@react-navigation/,
