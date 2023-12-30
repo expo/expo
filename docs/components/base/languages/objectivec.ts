a4469dcc062386e99b128689e6ca193b590261fa
Aman Mittal <amandeepmittal@live.com>
2023-12-30 21:22:49 +0530

    [docs] Revert bump Prism and Prism React Renderer, fix regressions and tests (#26171)
    ## Why
    
    Reverting it due to
    [Slack](https://exponent-internal.slack.com/archives/C1QMWQ1P0/p1703950927936539)

---
+++ docs/components/base/languages/objectivec.ts
@@ -0,0 +1,10 @@
+export function installObjectiveC(Prism: any) {
+  Prism.languages['objc'] = Prism.languages.extend('c', {
+    keyword:
+      /\b(?:asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|in|self|super)\b|(?:@interface|@end|@implementation|@protocol|@class|@public|@protected|@private|@property|@try|@catch|@finally|@throw|@synthesize|@dynamic|@selector)\b/,
+    string: /("|')(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1|@"(?:\\(?:\r\n|[\s\S])|[^"\\\r\n])*"/,
+    operator: /-[->]?|\+\+?|!=?|<<?=?|>>?=?|==?|&&?|\|\|?|[~^%?*/@]/,
+  });
+
+  delete Prism.languages['objc']['class-name'];
+}
