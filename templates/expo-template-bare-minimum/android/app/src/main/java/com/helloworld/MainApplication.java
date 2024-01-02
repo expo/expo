8e379dd3d3ae02c41c78cc06beeea504a0731866
Kudo Chien <kudo@expo.dev>
2024-01-02 15:23:22 +0800

    [templates] fix delay loading for expo-updates (#26120)
    expo-updates's `EX_UPDATES_ANDROID_DELAY_LOAD_APP` is broken and it
    still initializes at main thread and potentially cause ANR. the #20273
    approach calls `loadApp` when expo-updates is ready. the line
    `reactNativeHost.reactInstanceManager` will initialize the bridge and
    equivalent to `loadApp`.
    
    since flipper is debug only and the
    `ReactNativeFlipper.initializeFlipper()` is a no-op on release build. it
    is safe to add the statement only in debug variant. we should also
    prevent the `reactNativeHost.reactInstanceManager` call and make
    expo-updates's lazy loading works

--- templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
+++ templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -68,7 +68,9 @@ public class MainApplication extends Application implements ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    if (BuildConfig.DEBUG) {
+      ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    }
     ApplicationLifecycleDispatcher.onApplicationCreate(this);
   }
 
