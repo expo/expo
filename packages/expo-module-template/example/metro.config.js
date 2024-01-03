cf68104bdc3fe6cdeb5d6653cb9d47783a1edcfd
Kudo Chien <kudo@expo.dev>
2024-01-03 13:53:07 +0800

    [create-expo-modules] fix multiple react version issue from npm (#26209)
    # Why
    
    fixes #26162
    
    # How
    
    react hooks will throw errors from multiple react versions. this happens
    because npm will implicitly install react. this pr excludes the react
    for metro.
    
    # Test Plan
    
    test repro from #26162

--- packages/expo-module-template/example/metro.config.js
+++ packages/expo-module-template/example/metro.config.js
@@ -4,11 +4,12 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// npm v7+ will install ../node_modules/react-native because of peerDependencies.
+// npm v7+ will install ../node_modules/react and ../node_modules/react-native because of peerDependencies.
 // To prevent the incompatible react-native bewtween ./node_modules/react-native and ../node_modules/react-native,
 // excludes the one from the parent folder when bundling.
 config.resolver.blockList = [
   ...Array.from(config.resolver.blockList ?? []),
+  new RegExp(path.resolve('..', 'node_modules', 'react')),
   new RegExp(path.resolve('..', 'node_modules', 'react-native')),
 ];
 
