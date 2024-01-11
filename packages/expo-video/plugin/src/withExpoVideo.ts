7c545af239d5cbc88829d480e3071795e466a405
Wojciech Dróżdż <behenate@gmail.com>
2024-01-11 18:19:33 +0100

    Update the plugin to add supportsPictureInPicture to manifest

--- packages/expo-video/plugin/src/withExpoVideo.ts
+++ packages/expo-video/plugin/src/withExpoVideo.ts
@@ -1,4 +1,28 @@
-import { ConfigPlugin, withInfoPlist } from '@expo/config-plugins';
+import { ConfigPlugin, withInfoPlist, withAndroidManifest } from '@expo/config-plugins';
+import { Manifest } from '@expo/config-plugins/build/android';
+
+function enablePiPSupport(application: Manifest.ManifestApplication) {
+  if (application.$['android:name'] !== '.MainApplication') {
+    return;
+  }
+
+  if (!application.activity) {
+    return console.error("Couldn't apply the expo-video plugin for Android");
+  }
+
+  let applied = false;
+  application.activity = application.activity.map((activity) => {
+    if (activity.$['android:name'] === '.MainActivity') {
+      activity.$['android:supportsPictureInPicture'] = 'true';
+      applied = true;
+    }
+    return activity;
+  });
+
+  if (!applied) {
+    console.error("Couldn't apply the expo-video plugin for Android");
+  }
+}
 
 const withExpoVideo: ConfigPlugin = (config) => {
   withInfoPlist(config, (config) => {
@@ -9,6 +33,20 @@ const withExpoVideo: ConfigPlugin = (config) => {
     return config;
   });
 
+  withAndroidManifest(config, (config) => {
+    const { manifest } = config.modResults;
+
+    if (!manifest || !manifest.application || manifest.application.length === 0) {
+      console.error("Couldn't apply the expo-video plugin for Android");
+      return config;
+    }
+
+    manifest.application.forEach(enablePiPSupport);
+
+    config.modResults.manifest = manifest;
+
+    return config;
+  });
   return config;
 };
 
