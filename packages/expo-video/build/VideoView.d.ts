28e447878d686361a56ca990b2c97c23247577c6
Wojciech Dróżdż <behenate@gmail.com>
2024-01-11 18:19:49 +0100

    Update JS side and docs

--- packages/expo-video/build/VideoView.d.ts
+++ packages/expo-video/build/VideoView.d.ts
@@ -6,14 +6,20 @@ export declare class VideoView extends PureComponent<VideoViewProps> {
     enterFullscreen(): void;
     exitFullscreen(): void;
     /**
+     * Returns whether the current device supports Picture in Picture (PiP) mode.
+     * @returns A Promise that fulfills to `true` if the device supports PiP mode, and `false` otherwise.
+     * @platform android, ios
+     */
+    isPictureInPictureSupportedAsync(): Promise<boolean>;
+    /**
      * Enters Picture in Picture (PiP) mode. Throws an exception if the device does not support PiP.
      * > **Note:** Only one player can be in Picture in Picture (PiP) mode at a time.
-     * @platform ios 14+
+     * @platform android, ios 14+
      */
     startPictureInPicture(): any;
     /**
      * Exits Picture in Picture (PiP) mode.
-     * @platform ios 14+
+     * @platform android, ios 14+
      */
     stopPictureInPicture(): any;
     render(): ReactNode;
