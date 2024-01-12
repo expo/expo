482df10a6b7fd0150152c4a2993312d235e2b81c
Quinlan Jung <quin@expo.io>
2024-01-12 12:11:14 -0800

    fix(serializer): Default serializer uses the same premodules for bund… (#26382)
    …le and map (#26375)
    
    # Why
    Cherry picking @krystofwoldrich's PR that was already merged into the
    sdk 50 branch here: https://github.com/expo/expo/pull/26375
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    Before this PR Expo CLI command `export:embed` would output broken
    source maps when used with `unstable_beforeAssetSerializationPlugins`
    option.
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why? -->
    
    The `premodules` updated in `unstable_beforeAssetSerializationPlugins`
    were not passed to the source map function. Therefore the added module
    would be missing in the map.
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    Run `export:embed` with `unstable_beforeAssetSerializationPlugins` and
    verify that the outputted source map contains the added modules.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    # Why
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    Co-authored-by: Kryštof Woldřich <31292499+krystofwoldrich@users.noreply.github.com>

--- packages/@expo/metro-config/src/serializer/withExpoSerializers.ts
+++ packages/@expo/metro-config/src/serializer/withExpoSerializers.ts
@@ -107,13 +107,15 @@ export function createDefaultExportCustomSerializer(
       return debugId;
     };
 
+    let premodulesToBundle = [...preModules];
+
     let bundleCode: string | null = null;
     let bundleMap: string | null = null;
 
     if (config.serializer?.customSerializer) {
       const bundle = await config.serializer?.customSerializer(
         entryPoint,
-        preModules,
+        premodulesToBundle,
         graph,
         options
       );
@@ -125,7 +127,6 @@ export function createDefaultExportCustomSerializer(
       }
     } else {
       const debugId = loadDebugId();
-      let premodulesToBundle = [...preModules];
       if (configOptions.unstable_beforeAssetSerializationPlugins) {
         for (const plugin of configOptions.unstable_beforeAssetSerializationPlugins) {
           premodulesToBundle = plugin({ graph, premodules: [...premodulesToBundle], debugId });
@@ -153,7 +154,7 @@ export function createDefaultExportCustomSerializer(
 
     if (!bundleMap) {
       bundleMap = sourceMapString(
-        [...preModules, ...getSortedModules([...graph.dependencies.values()], options)],
+        [...premodulesToBundle, ...getSortedModules([...graph.dependencies.values()], options)],
         {
           // TODO: Surface this somehow.
           excludeSource: false,
