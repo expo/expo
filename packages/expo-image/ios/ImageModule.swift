94a065fd2c164bbd9c89ff1165686b5c98abb56a
aleqsio <mikucki@gmail.com>
2024-01-15 19:12:00 +0100

    Cleanup code

--- packages/expo-image/ios/ImageModule.swift
+++ packages/expo-image/ios/ImageModule.swift
@@ -133,11 +133,11 @@ public final class ImageModule: Module {
     AsyncFunction("getBlurhashAsync") {
       (url: URL, numberOfComponents: CGSize, promise: Promise) in
       var downloader = SDWebImageDownloader()
+      var parsedNumberOfComponents = (Int(numberOfComponents.width), Int(numberOfComponents.height))
       downloader.downloadImage(with: url, progress: nil, completed: { image, _, _, _ in
         DispatchQueue.global(qos: .userInitiated).async {
-          // Handle the completion of image download here
           if let downloadedImage = image {
-            let blurhashString = blurhash(fromImage: downloadedImage, numberOfComponents: (Int(numberOfComponents.width), Int(numberOfComponents.height)))
+            let blurhashString = blurhash(fromImage: downloadedImage, numberOfComponents: parsedNumberOfComponents)
             promise.resolve(blurhashString)
           } else {
             promise.reject(BlurhashGenerationException())
