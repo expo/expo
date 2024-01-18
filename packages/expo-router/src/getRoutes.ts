61d045c6b7e971186dd7f0725019859c0db80492
Mark Lawlor <mwlawlor@gmail.com>
2024-01-18 12:23:34 +1000

    additional comments

--- packages/expo-router/src/getRoutes.ts
+++ packages/expo-router/src/getRoutes.ts
@@ -12,13 +12,13 @@ export type Options = {
   preserveApiRoutes?: boolean;
   ignoreRequireErrors?: boolean;
   ignoreEntryPoints?: boolean;
-  /* Used using testing for easier comparison */
+  /* Used using testing for toEqual() comparison */
   internal_stripLoadRoute?: boolean;
 };
 
 type DirectoryNode = {
   layout?: RouteNode[];
-  name: string; // Just used for debugging
+  name: string;
   files: Map<string, RouteNode[]>;
   subdirectories: Map<string, DirectoryNode>;
 };
@@ -47,18 +47,25 @@ export function getRoutes(contextModule: RequireContext, options: Options = {}):
 }
 
 /**
- * Converts a RequireContext keys (file paths) into a directory tree.
+ * Converts the RequireContext keys (file paths) into a directory tree.
  */
 function getDirectoryTree(contextModule: RequireContext, options: Options) {
-  const ignoreList = getIgnoreList(options);
-  let hasRoutes = false;
-  let hasLayout = false;
+  const ignoreList = [/^\.\/\+html\.[tj]sx?$/];
+  if (options.ignore) {
+    ignoreList.push(...options.ignore);
+  }
+  if (options.preserveApiRoutes !== true) {
+    ignoreList.push(/\+api\.[tj]sx?$/);
+  }
 
-  const directory: DirectoryNode = {
+  const rootDirectory: DirectoryNode = {
     name: '/',
     files: new Map(),
     subdirectories: new Map(),
   };
+
+  let hasRoutes = false;
+  let hasLayout = false;
 
   for (const filePath of contextModule.keys()) {
     if (ignoreList.some((regex) => regex.test(filePath))) {
@@ -67,38 +74,17 @@ function getDirectoryTree(contextModule: RequireContext, options: Options) {
 
     const meta = getFileMeta(filePath, options);
 
-    // This is a file that should be ignored. e.g maybe it has an invalid platform
+    // This is a file that should be ignored. e.g maybe it has an invalid platform?
     if (meta.specificity < 0) {
       continue;
     }
 
-    // A single file may be extrapolated into multiple routes if it contains array syntax
-    const leaves: Set<DirectoryNode> = new Set();
-    for (const key of extrapolateGroups(meta.key)) {
-      let directoryNode = directory;
-
-      // Traverse the directory tree to its leaf node, creating any missing directories along the way
-      const subdirectoryParts = key.replace(meta.filename, '').split('/').filter(Boolean);
-      let name = '/';
-
-      for (const part of subdirectoryParts) {
-        name += part + '/';
-        let subDirectory = directoryNode.subdirectories.get(part);
-        if (!subDirectory) {
-          subDirectory = {
-            name,
-            files: new Map(),
-            subdirectories: new Map(),
-          };
-          directoryNode.subdirectories.set(part, subDirectory);
-        }
-        directoryNode = subDirectory;
-      }
-
-      // Add the leaf node to the list of leaves
-      leaves.add(directoryNode);
-    }
-
+    /**
+     * A single filepath may be extrapolated into multiple routes if it contains array syntax.
+     * Another way to thinking about is that a filepath node is present in multiple leaves of the directory tree.
+     *
+     * First we create the node and then form the collection of leaves it should be present in
+     */
     const node: RouteNode = {
       loadRoute() {
         if (options.ignoreRequireErrors) {
@@ -118,13 +104,45 @@ function getDirectoryTree(contextModule: RequireContext, options: Options) {
       children: [], // While we are building the directory tree, we don't know the node's children just yet. This is added during hoisting
     };
 
+    const leaves = new Set<DirectoryNode>();
+    for (const virtualFilePath of extrapolateGroups(meta.key)) {
+      // Traverse the directory tree to its leaf node, creating any missing directories along the way
+      const subdirectoryParts = virtualFilePath
+        .replace(meta.filename, '')
+        .split('/')
+        .filter(Boolean);
+
+      // Start at the root directory and traverse the path to the leaf directory
+      let directory = rootDirectory;
+      let name = rootDirectory.name;
+
+      for (const part of subdirectoryParts) {
+        name += part + '/';
+        let subDirectory = directory.subdirectories.get(part);
+
+        // Create any missing subdirectories
+        if (!subDirectory) {
+          subDirectory = {
+            name,
+            files: new Map(),
+            subdirectories: new Map(),
+          };
+          directory.subdirectories.set(part, subDirectory);
+        }
+
+        directory = subDirectory;
+      }
+
+      // Add the leaf node to the list of leaves
+      leaves.add(directory);
+    }
+
     if (meta.isLayout) {
       hasLayout ||= leaves.size > 0;
       for (const leaf of leaves) {
         leaf.layout ??= [];
 
         const existing = leaf.layout[meta.specificity];
-
         if (existing) {
           // In production, use the first route found
           if (process.env.NODE_ENV !== 'production') {
@@ -141,17 +159,22 @@ function getDirectoryTree(contextModule: RequireContext, options: Options) {
         let nodes = leaf.files.get(meta.name);
 
         if (!nodes) {
-          nodes = [];
-          leaf.files.set(meta.name, [node]);
-        } else {
-          const existing = nodes[0];
+          nodes = [node];
+          leaf.files.set(meta.name, nodes);
+        }
+
+        // TODO(Platform Route): Throw error if specificity > 0, as you cannot specify platform extensions for api routes
+
+        const existing = nodes[0];
+        if (existing) {
+          // In production, use the first route found
           if (process.env.NODE_ENV === 'production') {
-            nodes[0] = node;
-          } else {
             throw new Error(
               `The API route "${filePath}" and ${existing.contextKey} conflict. Please remove or rename one of these files.`
             );
           }
+        } else {
+          nodes[0] = node;
         }
       }
     } else {
@@ -165,21 +188,22 @@ function getDirectoryTree(contextModule: RequireContext, options: Options) {
           leaf.files.set(name, nodes);
         }
 
+        /**
+         * If there is an existing node with the same specificity, then we have a conflict.
+         * NOTE(Platform Routes):
+         *    We cannot check for specificity conflicts here, as we haven't processed all the context keys yet!
+         *    This will be checked during hoisting, as well as enforcing that all routes have a non-platform route.
+         */
         const existing = nodes[meta.specificity];
-
-        if (process.env.NODE_ENV === 'production') {
-          nodes[meta.specificity] = node;
-        } else {
-          if (existing) {
-            // In production, use the first route found
-            if (process.env.NODE_ENV !== 'production') {
-              throw new Error(
-                `The route files "${filePath}" and ${existing.contextKey} conflict on the route "${name}". Please remove or rename one of these files.`
-              );
-            }
-          } else {
-            nodes[meta.specificity] = node;
+        if (existing) {
+          // In production, use the first route found
+          if (process.env.NODE_ENV !== 'production') {
+            throw new Error(
+              `The route files "${filePath}" and ${existing.contextKey} conflict on the route "${name}". Please remove or rename one of these files.`
+            );
           }
+        } else {
+          nodes[meta.specificity] = node;
         }
       }
     }
@@ -191,8 +215,8 @@ function getDirectoryTree(contextModule: RequireContext, options: Options) {
   }
 
   // If there are no top-level _layout, add a default _layout
-  if (!directory.layout) {
-    directory.layout = [
+  if (!rootDirectory.layout) {
+    rootDirectory.layout = [
       {
         loadRoute: () => ({
           default: (require('./views/Navigator') as typeof import('./views/Navigator'))
@@ -211,12 +235,12 @@ function getDirectoryTree(contextModule: RequireContext, options: Options) {
 
   // Only include the sitemap if there are routes.
   if (hasRoutes) {
-    appendSitemapRoute(directory);
+    appendSitemapRoute(rootDirectory);
   }
 
-  appendNotFoundRoute(directory);
+  appendNotFoundRoute(rootDirectory);
 
-  return directory;
+  return rootDirectory;
 }
 
 /**
@@ -236,7 +260,7 @@ function flattenDirectoryTreeToRoutes(
    * All routes get "hoisted" to the nearest layout.
    */
   if (directory.layout) {
-    // TODO: In this implementation, everything has a specificity of 0. When we introduce Platform Routes, we need to pick the most specific layout.
+    // TODO(Platform Routes): We need to pick the most specific layout.
     const layout = directory.layout[0];
 
     // The first _layout is the root layout. It doesn't have a parent
@@ -269,7 +293,7 @@ function flattenDirectoryTreeToRoutes(
   if (!nearestLayout) throw new Error('Expo Router Internal Error: No nearest layout');
 
   for (const routes of directory.files.values()) {
-    // TODO: In this implementation, everything has a specificity of 0. When we introduce Platform Routes, we need to pick the most specific route.
+    // TODO(Platform Routes): We need to pick the most specific layout and ensure that all routes have a non-platform route.
     const route = routes[0];
 
     // `route` is the absolute pathname. We need to make this relative to the parent layout
@@ -335,7 +359,7 @@ function getFileMeta(key: string, options: Options) {
   };
 }
 
-function getIgnoreList(options?: Options) {
+export function getIgnoreList(options?: Options) {
   const ignore: RegExp[] = [/^\.\/\+html\.[tj]sx?$/, ...(options?.ignore ?? [])];
   if (options?.preserveApiRoutes !== true) {
     ignore.push(/\+api\.[tj]sx?$/);
@@ -343,6 +367,11 @@ function getIgnoreList(options?: Options) {
   return ignore;
 }
 
+/**
+ * Generates a set of strings which have the router array syntax extrapolated.
+ *
+ * /(a,b)/(c,d)/e.tsx => new Set(['a/c/e.tsx', 'a/d/e.tsx', 'b/c/e.tsx', 'b/d/e.tsx'])
+ */
 function extrapolateGroups(key: string, keys: Set<string> = new Set()): Set<string> {
   const match = matchGroupName(key);
 
