expo/expo

commit 30072b9637f0d2b964fe7f47f99f0e369336913a

[packages][bare-expo][tools] fix react-native 0.74 nightlies support (#26357)
# Why

fix react-native nightlies support for 0.74

# How

summarize here, please check each commit messages for details

- [BREAKING CHANGE] remove `unstable_useRuntimeSchedulerAlways` or
`REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS` support
- [BREAKING CHANGE] remove
`ReactNativeHostHandler.onRegisterJSIModules()` interface
- [expo][dev-menu][dev-launcher] fixed for
`ReactNativeHost.getJSIModulePackage()` removal and adding
`ReactNativeHost.getUIManagerProvider()`
- [dev-client] add compatible methods to address
https://github.com/facebook/react-native/pull/42236 changes

To be resolved and currently workarounded by SetupReactNativeNightly
- `import com.facebook.react.turbomodule.core.CallInvokerHolderImpl` ->
``import
com.facebook.react.internal.turbomodule.core.CallInvokerHolderImpl`

# Test Plan

ci passed

packages/expo-dev-menu/ios/ReactNativeCompatibles/ReactNative/DevClientAppDelegate.mm
@@ -37,12 +37,6 @@
 
 #endif
 
-@interface RCTAppDelegate (DevClientAppDelegate)
-
-- (void)unstable_registerLegacyComponents;
-
-@end
-
 @implementation DevClientAppDelegate {
 #if RCT_NEW_ARCH_ENABLED
   RCTHost *_reactHost;
@@ -72,7 +66,6 @@
                                                                contextContainer:_contextContainer];
   self.bridge.surfacePresenter = self.bridgeAdapter.surfacePresenter;
 
-  [self unstable_registerLegacyComponents];
   [RCTComponentViewFactory currentComponentViewFactory].thirdPartyFabricComponentsProvider = self;
 #endif
 
@@ -95,5 +88,29 @@
   return RCTAppSetupDefaultJsExecutorFactory(bridge, turboModuleManager, _runtimeScheduler);
 }
 #endif // RCT_NEW_ARCH_ENABLED
+
+#pragma mark - Remove these method when we drop SDK 49
+
+- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
+                          moduleName:(NSString *)moduleName
+                           initProps:(NSDictionary *)initProps
+{
+  return [super createRootViewWithBridge:bridge moduleName:moduleName initProps:initProps];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [super sourceURLForBridge:bridge];
+}
+
+- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
+{
+  return [super extraModulesForBridge:bridge];
+}
+
+- (BOOL)bridge:(RCTBridge *)bridge didNotFindModule:(NSString *)moduleName
+{
+  return [super bridge:bridge didNotFindModule:moduleName];
+}
 
 @end

.github/workflows/test-suite-nightly.yml
@@ -51,11 +51,8 @@ jobs:
       - name: 🌳 Display pod environment
         run: pod env
         working-directory: apps/bare-expo/ios
-      - name: 🥥 Update boost, RCT-Folly and SocketRocket pods in apps/bare-expo/ios
-        run: pod update boost RCT-Folly SocketRocket --no-repo-update
-        working-directory: apps/bare-expo/ios
-      - name: 🥥 Install pods in apps/bare-expo/ios
-        run: npx pod-install
+      - name: 🥥 Update RCT-Folly and SocketRocket pods in apps/bare-expo/ios
+        run: pod update RCT-Folly SocketRocket --no-repo-update
         working-directory: apps/bare-expo/ios
       - name: 🍏 Build iOS Project
         run: ./scripts/start-ios-e2e-test.ts --build

apps/bare-expo/android/app/build.gradle
@@ -112,8 +112,6 @@ android {
         versionCode 1
         versionName "1.0.0"
 
-        buildConfigField("boolean", "REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS", (findProperty("reactNative.unstable_useRuntimeSchedulerAlways") ?: true).toString())
-
         manifestPlaceholders = [
             // [Custom]: Required for expo-app-auth
             appAuthRedirectScheme: "dev.expo.payments"

apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainApplication.kt
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.config.ReactFeatureFlags
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
@@ -45,9 +44,6 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
-    }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()

apps/bare-expo/android/build.gradle
@@ -33,15 +33,6 @@ buildscript {
     }
 }
 
-
-def packageJsonFile = new File(rootProject.projectDir, '../package.json')
-def packageJson = new JsonSlurper().parseText(packageJsonFile.text)
-
-// [Custom]
-ext {
-    reactNativeVersion = packageJson.dependencies["react-native"]
-}
-
 allprojects {
     // [Custom]
     configurations.all {

apps/bare-expo/ios/Podfile
@@ -74,6 +74,7 @@ abstract_target 'BareExpoMain' do
     use_react_native!(
       :path => config[:reactNativePath],
       :hermes_enabled => podfile_properties['expo.jsEngine'] == nil || podfile_properties['expo.jsEngine'] == 'hermes',
+      :new_arch_enabled => false,
       # An absolute path to your application root.
       :app_path => "#{Pod::Config.instance.installation_root}/..",
       #
@@ -87,6 +88,7 @@ abstract_target 'BareExpoMain' do
     use_react_native!(
       :path => config[:reactNativePath],
       :hermes_enabled => podfile_properties['expo.jsEngine'] == nil || podfile_properties['expo.jsEngine'] == 'hermes',
+      :new_arch_enabled => false,
       # An absolute path to your application root.
       :app_path => "#{Pod::Config.instance.installation_root}/..",
       :flipper_configuration => FlipperConfiguration.disabled,
@@ -97,6 +99,7 @@ abstract_target 'BareExpoMain' do
     use_react_native!(
       :path => config[:reactNativePath],
       :hermes_enabled => podfile_properties['expo.jsEngine'] == nil || podfile_properties['expo.jsEngine'] == 'hermes',
+      :new_arch_enabled => false,
       # An absolute path to your application root.
       :app_path => "#{Pod::Config.instance.installation_root}/..",
       :flipper_configuration => FlipperConfiguration.disabled,

apps/fabric-tester/android/app/build.gradle
@@ -90,8 +90,6 @@ android {
         targetSdkVersion rootProject.ext.targetSdkVersion
         versionCode 1
         versionName "1.0.0"
-
-        buildConfigField("boolean", "REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS", (findProperty("reactNative.unstable_useRuntimeSchedulerAlways") ?: true).toString())
     }
     signingConfigs {
         debug {

apps/fabric-tester/android/app/src/main/java/com/community/fabrictester/MainApplication.kt
@@ -9,7 +9,6 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
-import com.facebook.react.config.ReactFeatureFlags
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
@@ -45,9 +44,6 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    if (!BuildConfig.REACT_NATIVE_UNSTABLE_USE_RUNTIME_SCHEDULER_ALWAYS) {
-      ReactFeatureFlags.unstable_useRuntimeSchedulerAlways = false
-    }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()

commit 8e4054324d2016211277f06403d324950d696054

[dev-client] fix dev-launcher crash (#26523)
# Why

a regression from #26357, when using dev-launcher to load an app, it
will crash because `RCTAppDelegate` does not implement the
`extraModulesForBridge:` selector.

# How

rather than calling super, return empty array with fallback

packages/expo-dev-menu/ios/ReactNativeCompatibles/ReactNative/DevClientAppDelegate.mm
@@ -105,12 +105,19 @@
 
 - (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge
 {
-  return [super extraModulesForBridge:bridge];
+  // Since we defined this selector but pretend that we don't implement it,
+  // we should also fallthrough modouleProvider case at
+  // https://github.com/facebook/react-native/blob/fd0ca4dd6209d79ac8c93dbffac2e3dca1caeadc/packages/react-native/React/CxxBridge/RCTCxxBridge.mm#L774-L778
+  RCTBridgeModuleListProvider moduleProvider = [bridge valueForKey:@"_moduleProvider"];
+  if (moduleProvider) {
+    return moduleProvider();
+  }
+  return @[];
 }
 
 - (BOOL)bridge:(RCTBridge *)bridge didNotFindModule:(NSString *)moduleName
 {
-  return [super bridge:bridge didNotFindModule:moduleName];
+  return NO;
 }
 
 @end