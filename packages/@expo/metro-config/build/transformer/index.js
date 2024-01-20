57989fd8cc89ea3a9797603d9b5fe6295bc70405
Evan Bacon <bacon@expo.io>
2024-01-20 16:02:57 -0600

    chore(metro-config): Remove lazy loading in build process for easier development. (#26559)
    # Why
    
    The two different bundling processes makes development and switching
    branches tricky. This was added for xdl and is no longer needed in the
    new CLI.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/metro-config/build/transformer/index.js
+++ packages/@expo/metro-config/build/transformer/index.js
@@ -1,64 +1,13 @@
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "createExoticTransformer", {
-  enumerable: true,
-  get: function () {
-    return _createExoticTransformer().createExoticTransformer;
-  }
-});
-Object.defineProperty(exports, "createModuleMatcher", {
-  enumerable: true,
-  get: function () {
-    return _createMatcher().createModuleMatcher;
-  }
-});
-Object.defineProperty(exports, "createMultiRuleTransformer", {
-  enumerable: true,
-  get: function () {
-    return _createMultiRuleTransformer().createMultiRuleTransformer;
-  }
-});
-Object.defineProperty(exports, "getCacheKey", {
-  enumerable: true,
-  get: function () {
-    return _getCacheKey().getCacheKey;
-  }
-});
-Object.defineProperty(exports, "loaders", {
-  enumerable: true,
-  get: function () {
-    return _createMultiRuleTransformer().loaders;
-  }
-});
-function _createExoticTransformer() {
-  const data = require("./createExoticTransformer");
-  _createExoticTransformer = function () {
-    return data;
-  };
-  return data;
-}
-function _getCacheKey() {
-  const data = require("./getCacheKey");
-  _getCacheKey = function () {
-    return data;
-  };
-  return data;
-}
-function _createMultiRuleTransformer() {
-  const data = require("./createMultiRuleTransformer");
-  _createMultiRuleTransformer = function () {
-    return data;
-  };
-  return data;
-}
-function _createMatcher() {
-  const data = require("./createMatcher");
-  _createMatcher = function () {
-    return data;
-  };
-  return data;
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createModuleMatcher = exports.createMultiRuleTransformer = exports.loaders = exports.getCacheKey = exports.createExoticTransformer = void 0;
+var createExoticTransformer_1 = require("./createExoticTransformer");
+Object.defineProperty(exports, "createExoticTransformer", { enumerable: true, get: function () { return createExoticTransformer_1.createExoticTransformer; } });
+var getCacheKey_1 = require("./getCacheKey");
+Object.defineProperty(exports, "getCacheKey", { enumerable: true, get: function () { return getCacheKey_1.getCacheKey; } });
+var createMultiRuleTransformer_1 = require("./createMultiRuleTransformer");
+Object.defineProperty(exports, "loaders", { enumerable: true, get: function () { return createMultiRuleTransformer_1.loaders; } });
+Object.defineProperty(exports, "createMultiRuleTransformer", { enumerable: true, get: function () { return createMultiRuleTransformer_1.createMultiRuleTransformer; } });
+var createMatcher_1 = require("./createMatcher");
+Object.defineProperty(exports, "createModuleMatcher", { enumerable: true, get: function () { return createMatcher_1.createModuleMatcher; } });
 //# sourceMappingURL=index.js.map
