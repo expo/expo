expo/expo

commit 8d0fb3c15b0fd1d9c476b320f0c7d410c642f79e

[cli][prebuild-config] add devtools plugins support (#24650)
# Why

add devtools plugins support to cli
close ENG-9981

having series prs to add devtools plugins support:
- #24649
- #24650 
- #24667

# How

- [prebuild-config] update expo-modules-autolinking integrate because now it exports public apis from #24649
- [cli] add `DevToolsPluginManager` and `DevToolsPluginMiddleware` for devtools plugins support

# Test Plan

add unit tests and ci passed

packages/@expo/cli/src/start/interface/interactiveActions.ts
@@ -6,7 +6,7 @@ import * as Log from '../../log';
 import { delayAsync } from '../../utils/delay';
 import { learnMore } from '../../utils/link';
 import { openBrowserAsync } from '../../utils/open';
-import { selectAsync } from '../../utils/prompts';
+import { ExpoChoice, selectAsync } from '../../utils/prompts';
 import { DevServerManager } from '../server/DevServerManager';
 import {
   addReactDevToolsReloadListener,
@@ -18,6 +18,10 @@ import {
 } from '../server/middleware/inspector/JsInspector';
 
 const debug = require('debug')('expo:start:interface:interactiveActions') as typeof console.log;
+
+interface MoreToolMenuItem extends ExpoChoice<string> {
+  action?: () => unknown;
+}
 
 /** Wraps the DevServerManager and adds an interface for user actions. */
 export class DevServerManagerActions {
@@ -111,22 +115,43 @@ export class DevServerManagerActions {
   }
 
   async openMoreToolsAsync() {
+    // Options match: Chrome > View > Developer
     try {
-      // Options match: Chrome > View > Developer
-      const value = await selectAsync(chalk`Dev tools {dim (native only)}`, [
+      const defaultMenuItems: MoreToolMenuItem[] = [
         { title: 'Inspect elements', value: 'toggleElementInspector' },
         { title: 'Toggle performance monitor', value: 'togglePerformanceMonitor' },
         { title: 'Toggle developer menu', value: 'toggleDevMenu' },
         { title: 'Reload app', value: 'reload' },
-        { title: 'Start React devtools', value: 'startReactDevTools' },
+        {
+          title: 'Open React devtools',
+          value: 'openReactDevTools',
+          action: this.openReactDevToolsAsync,
+        },
         // TODO: Maybe a "View Source" option to open code.
-        // Toggling Remote JS Debugging is pretty rough, so leaving it disabled.
-        // { title: 'Toggle Remote Debugging', value: 'toggleRemoteDebugging' },
-      ]);
-      if (value === 'startReactDevTools') {
-        this.startReactDevToolsAsync();
-      } else {
-        this.devServerManager.broadcastMessage('sendDevCommand', { name: value });
+      ];
+      const pluginMenuItems = (
+        await this.devServerManager.devtoolsPluginManager.queryPluginsAsync()
+      ).map((plugin) => ({
+        title: chalk`Open devtools plugin - {bold ${plugin.packageName}}`,
+        value: `devtoolsPlugin:${plugin.packageName}`,
+        action: async () => {
+          const url = new URL(
+            plugin.webpageEndpoint,
+            this.devServerManager
+              .getDefaultDevServer()
+              .getUrlCreator()
+              .constructUrl({ scheme: 'http' })
+          );
+          await openBrowserAsync(url.toString());
+        },
+      }));
+      const menuItems = [...defaultMenuItems, ...pluginMenuItems];
+      const value = await selectAsync(chalk`Dev tools {dim (native only)}`, menuItems);
+      const menuItem = menuItems.find((item) => item.value === value);
+      if (menuItem?.action) {
+        menuItem.action();
+      } else if (menuItem?.value) {
+        this.devServerManager.broadcastMessage('sendDevCommand', { name: menuItem.value });
       }
     } catch (error: any) {
       debug(error);
@@ -136,7 +161,7 @@ export class DevServerManagerActions {
     }
   }
 
-  async startReactDevToolsAsync() {
+  async openReactDevToolsAsync() {
     await startReactDevToolsProxyAsync();
     const url = this.devServerManager.getDefaultDevServer().getReactDevToolsUrl();
     await openBrowserAsync(url);

packages/@expo/cli/src/customize/typescript.ts
@@ -20,9 +20,7 @@ export async function typescript(projectRoot: string) {
   }
 
   const { exp } = getConfig(projectRoot, { skipSDKVersionRequirement: true });
-  await new MetroBundlerDevServer(
-    projectRoot,
-    getPlatformBundlers(exp),
-    true
-  ).startTypeScriptServices();
+  await new MetroBundlerDevServer(projectRoot, getPlatformBundlers(exp), {
+    isDevClient: true,
+  }).startTypeScriptServices();
 }

packages/@expo/cli/src/export/web/exportWebAsync.ts
@@ -19,7 +19,7 @@ export async function exportWebAsync(projectRoot: string, options: Options) {
   const { exp } = getConfig(projectRoot);
   const platformBundlers = getPlatformBundlers(exp);
   // Create a bundler interface
-  const bundler = new WebpackBundlerDevServer(projectRoot, platformBundlers, false);
+  const bundler = new WebpackBundlerDevServer(projectRoot, platformBundlers);
 
   // If the user set `web.bundler: 'metro'` then they should use `expo export` instead.
   if (!bundler.isTargetingWeb()) {

packages/@expo/cli/src/start/server/BundlerDevServer.ts
@@ -2,6 +2,7 @@ import assert from 'assert';
 import resolveFrom from 'resolve-from';
 
 import { AsyncNgrok } from './AsyncNgrok';
+import DevToolsPluginManager from './DevToolsPluginManager';
 import { DevelopmentSession } from './DevelopmentSession';
 import { CreateURLOptions, UrlCreator } from './UrlCreator';
 import { PlatformBundlers } from './platformBundlers';
@@ -101,15 +102,29 @@ export abstract class BundlerDevServer {
   protected urlCreator?: UrlCreator | null = null;
 
   private notifier: FileNotifier | null = null;
+  protected readonly devToolsPluginManager: DevToolsPluginManager;
+  public isDevClient: boolean;
 
   constructor(
     /** Project root folder. */
     public projectRoot: string,
     /** A mapping of bundlers to platforms. */
     public platformBundlers: PlatformBundlers,
-    // TODO: Replace with custom scheme maybe...
-    public isDevClient?: boolean
-  ) {}
+    /** Advanced options */
+    options?: {
+      /**
+       * The instance of DevToolsPluginManager
+       * @default new DevToolsPluginManager(projectRoot)
+       */
+      devToolsPluginManager?: DevToolsPluginManager;
+      // TODO: Replace with custom scheme maybe...
+      isDevClient?: boolean;
+    }
+  ) {
+    this.devToolsPluginManager =
+      options?.devToolsPluginManager ?? new DevToolsPluginManager(projectRoot);
+    this.isDevClient = options?.isDevClient ?? false;
+  }
 
   protected setInstance(instance: DevServerInstance) {
     this.instance = instance;

packages/@expo/cli/src/start/server/DevServerManager.ts
@@ -3,6 +3,7 @@ import assert from 'assert';
 import chalk from 'chalk';
 
 import { BundlerDevServer, BundlerStartOptions } from './BundlerDevServer';
+import DevToolsPluginManager from './DevToolsPluginManager';
 import { getPlatformBundlers } from './platformBundlers';
 import { Log } from '../../log';
 import { FileNotifier } from '../../utils/FileNotifier';
@@ -35,6 +36,7 @@ const BUNDLERS = {
 /** Manages interacting with multiple dev servers. */
 export class DevServerManager {
   private projectPrerequisites: ProjectPrerequisite<any, void>[] = [];
+  public readonly devtoolsPluginManager: DevToolsPluginManager;
 
   private notifier: FileNotifier | null = null;
 
@@ -44,6 +46,7 @@ export class DevServerManager {
     public options: BundlerStartOptions
   ) {
     this.notifier = this.watchBabelConfig();
+    this.devtoolsPluginManager = new DevToolsPluginManager(projectRoot);
   }
 
   private watchBabelConfig() {
@@ -166,11 +169,10 @@ export class DevServerManager {
     // Start all dev servers...
     for (const { type, options } of startOptions) {
       const BundlerDevServerClass = await BUNDLERS[type]();
-      const server = new BundlerDevServerClass(
-        this.projectRoot,
-        platformBundlers,
-        !!options?.devClient
-      );
+      const server = new BundlerDevServerClass(this.projectRoot, platformBundlers, {
+        devToolsPluginManager: this.devtoolsPluginManager,
+        isDevClient: !!options?.devClient,
+      });
       await server.startAsync(options ?? this.options);
       devServers.push(server);
     }

packages/@expo/cli/src/start/server/DevToolsPluginManager.ts
@@ -1,0 +1,69 @@
+import type { ModuleDescriptorDevTools } from 'expo-modules-autolinking/exports';
+import path from 'path';
+import resolveFrom from 'resolve-from';
+
+const debug = require('debug')('expo:start:server:devtools');
+
+export const DevToolsPluginEndpoint = '/_expo/plugins';
+
+interface AutolinkingPlugin {
+  packageName: string;
+  packageRoot: string;
+  webpageRoot: string;
+}
+
+export interface DevToolsPlugin extends AutolinkingPlugin {
+  webpageEndpoint: string;
+}
+
+export default class DevToolsPluginManager {
+  private plugins: DevToolsPlugin[] | null = null;
+
+  constructor(private projectRoot: string) {}
+
+  public async queryPluginsAsync(): Promise<DevToolsPlugin[]> {
+    if (this.plugins) {
+      return this.plugins;
+    }
+    const plugins = (await this.queryAutolinkedPluginsAsync(this.projectRoot)).map((plugin) => ({
+      ...plugin,
+      webpageEndpoint: `${DevToolsPluginEndpoint}/${plugin.packageName}`,
+    }));
+    this.plugins = plugins;
+    return this.plugins;
+  }
+
+  public async queryPluginWebpageRootAsync(pluginName: string): Promise<string | null> {
+    const plugins = await this.queryPluginsAsync();
+    const plugin = plugins.find((p) => p.packageName === pluginName);
+    return plugin?.webpageRoot ?? null;
+  }
+
+  private async queryAutolinkedPluginsAsync(projectRoot: string): Promise<AutolinkingPlugin[]> {
+    const expoPackagePath = resolveFrom.silent(projectRoot, 'expo/package.json');
+    if (!expoPackagePath) {
+      return [];
+    }
+    const resolvedPath = resolveFrom.silent(
+      path.dirname(expoPackagePath),
+      'expo-modules-autolinking/exports'
+    );
+    if (!resolvedPath) {
+      return [];
+    }
+    const autolinkingModule = require(
+      resolvedPath
+    ) as typeof import('expo-modules-autolinking/exports');
+    if (!autolinkingModule.queryAutolinkingModulesFromProjectAsync) {
+      throw new Error(
+        'Missing exported `queryAutolinkingModulesFromProjectAsync()` function from `expo-modules-autolinking`'
+      );
+    }
+    const plugins = (await autolinkingModule.queryAutolinkingModulesFromProjectAsync(projectRoot, {
+      platform: 'devtools',
+      onlyProjectDeps: false,
+    })) as ModuleDescriptorDevTools[];
+    debug('Found autolinked plugins', this.plugins);
+    return plugins;
+  }
+}

packages/@expo/cli/src/start/server/__tests__/BundlerDevServer-test.ts
@@ -100,6 +100,10 @@ class MockBundlerDevServer extends BundlerDevServer {
   protected getConfigModuleIds(): string[] {
     return ['./fake.config.js'];
   }
+
+  public startTypeScriptServices(): Promise<void> {
+    throw new Error('Unimplemented');
+  }
 }
 
 class MockMetroBundlerDevServer extends MockBundlerDevServer {
@@ -205,12 +209,9 @@ describe('isRedirectPageEnabled', () => {
   it(`is redirect enabled`, async () => {
     mockDevClientInstalled();
 
-    const server = new MockBundlerDevServer(
-      '/',
-      getPlatformBundlers({}),
-      // is Dev Client
-      false
-    );
+    const server = new MockBundlerDevServer('/', getPlatformBundlers({}), {
+      isDevClient: false,
+    });
     expect(server['isRedirectPageEnabled']()).toBe(true);
   });
 
@@ -219,46 +220,34 @@ describe('isRedirectPageEnabled', () => {
 
     process.env.EXPO_NO_REDIRECT_PAGE = '1';
 
-    const server = new MockBundlerDevServer(
-      '/',
-      getPlatformBundlers({}),
-      // is Dev Client
-      false
-    );
+    const server = new MockBundlerDevServer('/', getPlatformBundlers({}), {
+      isDevClient: false,
+    });
     expect(server['isRedirectPageEnabled']()).toBe(false);
   });
 
   it(`redirect is disabled when running in dev client mode`, async () => {
     mockDevClientInstalled();
 
-    const server = new MockBundlerDevServer(
-      '/',
-      getPlatformBundlers({}),
-      // is Dev Client
-      true
-    );
+    const server = new MockBundlerDevServer('/', getPlatformBundlers({}), {
+      isDevClient: true,
+    });
     expect(server['isRedirectPageEnabled']()).toBe(false);
   });
 
   it(`redirect is disabled when expo-dev-client is not installed in the project`, async () => {
-    const server = new MockBundlerDevServer(
-      '/',
-      getPlatformBundlers({}),
-      // is Dev Client
-      false
-    );
+    const server = new MockBundlerDevServer('/', getPlatformBundlers({}), {
+      isDevClient: false,
+    });
     expect(server['isRedirectPageEnabled']()).toBe(false);
   });
 });
 
 describe('getRedirectUrl', () => {
   it(`returns null when the redirect page functionality is disabled`, async () => {
-    const server = new MockBundlerDevServer(
-      '/',
-      getPlatformBundlers({}),
-      // is Dev Client
-      false
-    );
+    const server = new MockBundlerDevServer('/', getPlatformBundlers({}), {
+      isDevClient: false,
+    });
     server['isRedirectPageEnabled'] = () => false;
     expect(server['getRedirectUrl']()).toBe(null);
   });
@@ -311,7 +300,9 @@ describe('getNativeRuntimeUrl', () => {
     expect(server.getNativeRuntimeUrl({ scheme: 'foobar' })).toBe('exp://100.100.1.100:3000');
   });
   it(`gets the native runtime URL for dev client`, async () => {
-    const server = new MockBundlerDevServer('/', getPlatformBundlers({}), true);
+    const server = new MockBundlerDevServer('/', getPlatformBundlers({}), {
+      isDevClient: true,
+    });
     await server.startAsync({
       location: {
         scheme: 'my-app',

packages/@expo/cli/src/start/server/metro/MetroBundlerDevServer.ts
@@ -27,6 +27,7 @@ import { BundlerDevServer, BundlerStartOptions, DevServerInstance } from '../Bun
 import { getStaticRenderFunctions } from '../getStaticRenderFunctions';
 import { ContextModuleSourceMapsMiddleware } from '../middleware/ContextModuleSourceMapsMiddleware';
 import { CreateFileMiddleware } from '../middleware/CreateFileMiddleware';
+import { DevToolsPluginMiddleware } from '../middleware/DevToolsPluginMiddleware';
 import { FaviconMiddleware } from '../middleware/FaviconMiddleware';
 import { HistoryFallbackMiddleware } from '../middleware/HistoryFallbackMiddleware';
 import { InterstitialPageMiddleware } from '../middleware/InterstitialPageMiddleware';
@@ -400,6 +401,9 @@ export class MetroBundlerDevServer extends BundlerDevServer {
       }).getHandler()
     );
     middleware.use(new ReactDevToolsPageMiddleware(this.projectRoot).getHandler());
+    middleware.use(
+      new DevToolsPluginMiddleware(this.projectRoot, this.devToolsPluginManager).getHandler()
+    );
 
     const deepLinkMiddleware = new RuntimeRedirectMiddleware(this.projectRoot, {
       onDeepLink: getDeepLinkHandler(this.projectRoot),

commit 64fc7821ae631e0d7bc98f50e57dd5558afc535f

[cli] fix react devtools launching error (#26550)
# Why

when launching react-devtools from cli on sdk 50, it will crash cli with
the following error:
```
/Users/kudo/sdk50/node_modules/@expo/cli/build/src/start/interface/interactiveActions.js:189
        const url = this.devServerManager.getDefaultDevServer().getReactDevToolsUrl();
                                          ^

TypeError: Cannot read properties of undefined (reading 'getDefaultDevServer')
    at Object.openReactDevToolsAsync [as action] (/Users/kudo/sdk50/node_modules/@expo/cli/build/src/start/interface/interactiveActions.js:189:43)
```

# How

that's a regression from #24650. we should make sure the function point
to valid "this".

# Test Plan

on sdk 50 project and trying to launch react-devtools

# Checklist

- [x] Documentation is up to date to reflect these changes (eg:
https://docs.expo.dev and README.md).
- [x] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
- [x] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).

packages/@expo/cli/src/start/interface/interactiveActions.ts
@@ -145,7 +145,7 @@ export class DevServerManagerActions {
         {
           title: 'Open React devtools',
           value: 'openReactDevTools',
-          action: this.openReactDevToolsAsync,
+          action: this.openReactDevToolsAsync.bind(this),
         },
         // TODO: Maybe a "View Source" option to open code.
       ];