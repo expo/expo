5853e0a869c3ac83ab3103d05e57efb79c905974
Mark Lawlor <mwlawlor@gmail.com>
2024-01-23 22:24:45 +1000

    rewrite _ctx-shared as commonjs

--- packages/expo-router/_ctx-shared.js
+++ packages/expo-router/_ctx-shared.js
@@ -1,3 +1,4 @@
 // Ignore root `./+html.js` and API route files `./generate+api.tsx`.
-export const EXPO_ROUTER_CTX_IGNORE =
-  /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+html)))\.[tj]sx?$).*\.[tj]sx?$/;
+module.exports = {
+  EXPO_ROUTER_CTX_IGNORE: /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+html)))\.[tj]sx?$).*\.[tj]sx?$/,
+};
