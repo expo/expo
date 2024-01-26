60be971c7f3a3ce58be6a757c6b92ea606bcfde2
lukmccall <kosmatylukasz@gmail.com>
2024-01-26 13:54:22 +0100

    [core][Android] Refactor android tests

--- packages/expo-modules-core/android/src/androidTest/java/expo/modules/kotlin/jni/JavaScriptModuleObjectTest.kt
+++ packages/expo-modules-core/android/src/androidTest/java/expo/modules/kotlin/jni/JavaScriptModuleObjectTest.kt
@@ -1,56 +1,44 @@
-@file:OptIn(ExperimentalCoroutinesApi::class)
-
 package expo.modules.kotlin.jni
 
 import com.google.common.truth.Truth
 import expo.modules.kotlin.Promise
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import expo.modules.kotlin.jni.extensions.addSingleQuotes
 import org.junit.Test
 
 class JavaScriptModuleObjectTest {
   @Test
-  fun hostObject_should_expose_addListener_when_events_dsl_exist() = withJSIInterop(
-    inlineModule {
-      Name("TestModule")
-      Events("onFoo")
-    }
-  ) {
-    val addListenerFunction = evaluateScript("expo.modules.TestModule.addListener")
-    val removeListenersFunction = evaluateScript("expo.modules.TestModule.removeListeners")
+  fun hostObject_should_expose_addListener_when_events_dsl_exist() = withSingleModule({
+    Events("onFoo")
+  }) {
+    val addListenerFunction = property("addListener")
+    val removeListenersFunction = property("removeListeners")
     Truth.assertThat(addListenerFunction.isFunction()).isTrue()
     Truth.assertThat(removeListenersFunction.isFunction()).isTrue()
   }
 
   @Test
-  fun hostObject_should_not_expose_addListener_by_default() = withJSIInterop(
-    inlineModule {
-      Name("TestModule")
-    }
-  ) {
-    val addListenerFunction = evaluateScript("expo.modules.TestModule.addListener")
-    val removeListenersFunction = evaluateScript("expo.modules.TestModule.removeListeners")
+  fun hostObject_should_not_expose_addListener_by_default() = withSingleModule {
+    val addListenerFunction = property("addListener")
+    val removeListenersFunction = property("removeListeners")
     Truth.assertThat(addListenerFunction.isUndefined()).isTrue()
     Truth.assertThat(removeListenersFunction.isUndefined()).isTrue()
   }
 
   @Test
-  fun hostObject_should_not_override_existing_addListener() = withJSIInterop(
-    inlineModule {
-      Name("TestModule")
-      Events("onFoo")
-      Function("addListener") { name: String ->
-        return@Function "echo $name"
-      }
-      AsyncFunction("removeListeners") { count: Int, promise: Promise ->
-        promise.resolve("remove $count")
-      }
+  fun hostObject_should_not_override_existing_addListener() = withSingleModule({
+    Events("onFoo")
+    Function("addListener") { name: String ->
+      return@Function "echo $name"
     }
-  ) { methodQueue ->
-    val addListenerResult = evaluateScript("expo.modules.TestModule.addListener('test')")
+    AsyncFunction("removeListeners") { count: Int, promise: Promise ->
+      promise.resolve("remove $count")
+    }
+  }) {
+    val addListenerResult = call("addListener", "test".addSingleQuotes())
     Truth.assertThat(addListenerResult.isString()).isTrue()
     Truth.assertThat(addListenerResult.getString()).isEqualTo("echo test")
 
-    val removedListenersResult = waitForAsyncFunction(methodQueue, "expo.modules.TestModule.removeListeners(7)")
+    val removedListenersResult = callAsync("removeListeners", "7")
     Truth.assertThat(removedListenersResult.isString()).isTrue()
     Truth.assertThat(removedListenersResult.getString()).isEqualTo("remove 7")
   }
--- packages/expo-modules-core/android/src/androidTest/java/expo/modules/kotlin/jni/JavaScriptViewModule.kt
+++ packages/expo-modules-core/android/src/androidTest/java/expo/modules/kotlin/jni/JavaScriptViewModule.kt
@@ -1,23 +1,17 @@
-@file:OptIn(ExperimentalCoroutinesApi::class)
-
 package expo.modules.kotlin.jni
 
 import com.google.common.truth.Truth
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Test
 
 class JavaScriptViewModule {
   @Test
-  fun should_export_view_prototype() = withJSIInterop(
-    inlineModule {
-      Name("TestModule")
-      View(android.view.View::class) {
-        AsyncFunction("viewFunction") { _: Int -> }
-        AsyncFunction("anotherViewFunction") { -> 20 }
-      }
+  fun should_export_view_prototype() = withSingleModule({
+    View(android.view.View::class) {
+      AsyncFunction("viewFunction") { _: Int -> }
+      AsyncFunction("anotherViewFunction") { 20 }
     }
-  ) {
-    val viewFunctions = evaluateScript("Object.getOwnPropertyNames(expo.modules.TestModule.ViewPrototype)")
+  }) {
+    val viewFunctions = evaluateScript("Object.getOwnPropertyNames($moduleRef.ViewPrototype)")
       .getArray()
       .map { it.getString() }
 
@@ -25,37 +19,28 @@ class JavaScriptViewModule {
   }
 
   @Test
-  fun view_functions_should_be_callable() = withJSIInterop(
-    inlineModule {
-      Name("TestModule")
-      View(android.view.View::class) {
-        AsyncFunction("viewFunction") { ->
-          123
-        }
-      }
+  fun view_functions_should_be_callable() = withSingleModule({
+    View(android.view.View::class) {
+      AsyncFunction("viewFunction") { 123 }
     }
-  ) { methodQueue ->
-    val result = waitForAsyncFunction(methodQueue, "expo.modules.TestModule.ViewPrototype.viewFunction()").getInt()
+  }) {
+    val result = callViewAsync("ViewPrototype", "viewFunction").getInt()
     Truth.assertThat(result).isEqualTo(123)
   }
 
   @Test
-  fun view_functions_should_be_able_to_receives_view_as_argument() = withJSIInterop(
-    inlineModule {
-      Name("TestModule")
-      View(android.view.View::class) {
-        AsyncFunction("viewFunction") { view: android.view.View ->
-          Truth.assertThat(view).isNotNull()
-          123
-        }
+  fun view_functions_should_be_able_to_receives_view_as_argument() = withSingleModule({
+    View(android.view.View::class) {
+      AsyncFunction("viewFunction") { view: android.view.View ->
+        Truth.assertThat(view).isNotNull()
+        123
       }
     }
-  ) { methodQueue ->
+  }) {
     val result = waitForAsyncFunction(
-      methodQueue,
       """
       const nativeView = { nativeTag: 1 };
-      Object.assign(nativeView.__proto__, expo.modules.TestModule.ViewPrototype);
+      Object.assign(nativeView.__proto__, $moduleRef.ViewPrototype);
       nativeView.viewFunction()
       """.trimIndent()
     ).getInt()
