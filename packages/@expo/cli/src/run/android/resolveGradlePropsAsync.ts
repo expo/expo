b21547fcafd23adc912e18224908a8f90a182b75
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2024-02-01 16:40:13 +0000

    [cli] Add --active-arch-only flag (#26800)

--- packages/@expo/cli/src/run/android/resolveGradlePropsAsync.ts
+++ packages/@expo/cli/src/run/android/resolveGradlePropsAsync.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 
+import { Device, getDeviceABIsAsync } from '../../start/platforms/android/adb';
 import { CommandError } from '../../utils/errors';
 
 export type GradleProps = {
@@ -11,6 +12,8 @@ export type GradleProps = {
   buildType: string;
   /** Used to assemble the APK, also included in the output APK filename. */
   flavors?: string[];
+  /** Architectures to build for. */
+  architectures?: string;
 };
 
 function assertVariant(variant?: string) {
@@ -20,10 +23,11 @@ function assertVariant(variant?: string) {
   return variant ?? 'debug';
 }
 
-export function resolveGradleProps(
+export async function resolveGradlePropsAsync(
   projectRoot: string,
-  options: { variant?: string }
-): GradleProps {
+  options: { variant?: string; allArch?: boolean },
+  device: Device
+): Promise<GradleProps> {
   const variant = assertVariant(options.variant);
   // NOTE(EvanBacon): Why would this be different? Can we get the different name?
   const appName = 'app';
@@ -34,11 +38,27 @@ export function resolveGradleProps(
   // This won't work for non-standard flavor names like "myFlavor" would be treated as "my", "flavor".
   const [buildType, ...flavors] = variant.split(/(?=[A-Z])/).map((v) => v.toLowerCase());
   const apkVariantDirectory = path.join(apkDirectory, ...flavors, buildType);
+  const architectures = await getConnectedDeviceABIS(buildType, device, options.allArch);
 
   return {
     appName,
     buildType,
     flavors,
     apkVariantDirectory,
+    architectures,
   };
+}
+
+async function getConnectedDeviceABIS(
+  buildType: string,
+  device: Device,
+  allArch?: boolean
+): Promise<string> {
+  // Follow the same behavior as iOS, only enable this for debug builds
+  if (allArch || buildType !== 'debug') {
+    return '';
+  }
+
+  const abis = await getDeviceABIsAsync(device);
+  return abis.filter((abi, i, arr) => arr.indexOf(abi) === i).join(',');
 }
