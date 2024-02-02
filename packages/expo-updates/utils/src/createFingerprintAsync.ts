d10d754437fdb1a7e6fb566ee021db7e9606858e
Will Schurman <wschurman@gmail.com>
2024-02-01 17:27:33 -0800

    [expo-updates] Fix fingerprint runtime version policy

---
+++ packages/expo-updates/utils/src/createFingerprintAsync.ts
@@ -0,0 +1,57 @@
+import { getConfig } from '@expo/config';
+import * as Fingerprint from '@expo/fingerprint';
+import fs from 'fs';
+import path from 'path';
+
+import { resolveWorkflowAsync } from './workflow';
+
+export async function createFingerprintAsync(
+  platform: 'ios' | 'android',
+  possibleProjectRoot: string,
+  destinationDir: string
+): Promise<void> {
+  // Remove projectRoot validation when we no longer support React Native <= 62
+  let projectRoot;
+  if (fs.existsSync(path.join(possibleProjectRoot, 'package.json'))) {
+    projectRoot = possibleProjectRoot;
+  } else if (fs.existsSync(path.join(possibleProjectRoot, '..', 'package.json'))) {
+    projectRoot = path.resolve(possibleProjectRoot, '..');
+  } else {
+    throw new Error('Error loading app package. Ensure there is a package.json in your app.');
+  }
+
+  process.chdir(projectRoot);
+
+  const { exp: config } = getConfig(projectRoot, {
+    isPublicConfig: true,
+    skipSDKVersionRequirement: true,
+  });
+
+  const runtimeVersion = config[platform]?.runtimeVersion ?? config.runtimeVersion;
+  if (!runtimeVersion || typeof runtimeVersion === 'string') {
+    return;
+  }
+
+  if (runtimeVersion.policy !== 'fingerprintExperimental') {
+    // not a policy that needs fingerprinting
+    return;
+  }
+
+  const workflow = await resolveWorkflowAsync(projectRoot, platform);
+
+  let fingerprint: string;
+  if (workflow === 'generic') {
+    fingerprint = await Fingerprint.createProjectHashAsync(projectRoot, {
+      platforms: [platform],
+    });
+  } else {
+    // ignore everything in native directories to ensure fingerprint is the same
+    // no matter whether project has been prebuilt
+    fingerprint = await Fingerprint.createProjectHashAsync(projectRoot, {
+      platforms: [platform],
+      ignorePaths: ['/android/**/*', '/ios/**/*'],
+    });
+  }
+
+  fs.writeFileSync(path.join(destinationDir, 'fingerprint'), fingerprint);
+}
