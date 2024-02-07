0bc0017a0e54ba49d5a9521093738759c02b3a23
Dan Wood <daniel4wood@icloud.com>
2024-02-07 13:18:14 +1100

    Lint react hooks exhaustive dependency array
    This is a very important rule that specifically prevents bugs, and should absolutely not be turned off in any react project. It has been mentioned that it should almost always be followed by the react team, and if it needs to be disabled in an edge case, it can do so with an ignore comment.
    
    Seems much more important to be linting this rather than stylistic things like `import/order`, `prettier/prettier` or `react/jsx-boolean-value`

--- packages/eslint-config-universe/shared/react.js
+++ packages/eslint-config-universe/shared/react.js
@@ -37,7 +37,7 @@ module.exports = {
     'react/self-closing-comp': 'warn',
 
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
     react: { version: 'detect' },
