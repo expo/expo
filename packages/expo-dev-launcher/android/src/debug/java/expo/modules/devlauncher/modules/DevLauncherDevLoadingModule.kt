bb1380805a33b06c29f78d643002bb179afae365
Gabriel Donadel <donadeldev@gmail.com>
2024-02-08 17:37:54 -0300

    Extend DevLoadingModule

--- packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/modules/DevLauncherDevLoadingModule.kt
+++ packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/modules/DevLauncherDevLoadingModule.kt
@@ -3,16 +3,16 @@ package expo.modules.devlauncher.modules
 import com.facebook.fbreact.specs.NativeDevLoadingViewSpec
 import com.facebook.react.bridge.JSExceptionHandler
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.UiThreadUtil
 import com.facebook.react.devsupport.DefaultDevLoadingViewImplementation
 import com.facebook.react.devsupport.interfaces.DevLoadingViewManager
 import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.modules.devloading.DevLoadingModule
 import expo.modules.devlauncher.rncompatibility.DevLauncherDevSupportManager
 
 
 // Based on https://github.com/facebook/react-native/blob/0.72-stable/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/devloading/DevLoadingModule.java
 @ReactModule(name = NativeDevLoadingViewSpec.NAME)
-class DevLauncherDevLoadingModule(reactContext: ReactApplicationContext) : NativeDevLoadingViewSpec(reactContext) {
+class DevLauncherDevLoadingModule(reactContext: ReactApplicationContext) : DevLoadingModule(reactContext) {
   private val mJSExceptionHandler: JSExceptionHandler?
   private var mDevLoadingViewManager: DevLoadingViewManager? = null
 
@@ -20,16 +20,6 @@ class DevLauncherDevLoadingModule(reactContext: ReactApplicationContext) : Nativ
     mJSExceptionHandler = reactContext.jsExceptionHandler
     if (mJSExceptionHandler != null && mJSExceptionHandler is DevLauncherDevSupportManager) {
       mDevLoadingViewManager = DefaultDevLoadingViewImplementation((mJSExceptionHandler as DevLauncherDevSupportManager).reactInstanceManagerHelper)
-    }
-  }
-
-  override fun showMessage(message: String, color: Double?, backgroundColor: Double?) {
-    UiThreadUtil.runOnUiThread { mDevLoadingViewManager?.showMessage(message) }
-  }
-
-  override fun hide() {
-    UiThreadUtil.runOnUiThread {
-      mDevLoadingViewManager?.hide()
     }
   }
 }
