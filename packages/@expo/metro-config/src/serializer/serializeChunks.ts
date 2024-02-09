5faa8b940721a1bae6f3200a30fdd793ac2e6b9e
Evan Bacon <bacon@expo.io>
2024-02-08 18:36:27 -0600

    feat(metro-config): Add support for splitting on `require.resolveWeak` syntax. (#27014)
    # Why
    
    - Split out of RSC branch. Supports adding references to async imports
    without bundling them.
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    - Added an e2e bundling test.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/metro-config/src/serializer/serializeChunks.ts
+++ packages/@expo/metro-config/src/serializer/serializeChunks.ts
@@ -279,7 +279,7 @@ export class Chunk {
 
     this.deps.forEach((module) => {
       module.dependencies.forEach((dependency) => {
-        if (dependency.data.data.asyncType === 'async') {
+        if (dependency.data.data.asyncType) {
           const chunkContainingModule = chunks.find((chunk) =>
             chunk.hasAbsolutePath(dependency.absolutePath)
           );
@@ -565,7 +565,7 @@ function gatherChunks(
   function includeModule(entryModule: Module<MixedOutput>) {
     for (const dependency of entryModule.dependencies.values()) {
       if (
-        dependency.data.data.asyncType === 'async' &&
+        dependency.data.data.asyncType &&
         // Support disabling multiple chunks.
         splitChunks
       ) {
