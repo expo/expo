3b5a91e28822e662e69635e2227d9eca64194b8f
Cedric van Putten <github@cedric.dev>
2024-02-12 20:57:00 +0100

    test(cli): remove libraries from expected modules loaded

--- packages/@expo/cli/e2e/__tests__/run-test.ts
+++ packages/@expo/cli/e2e/__tests__/run-test.ts
@@ -19,13 +19,6 @@ afterAll(() => {
 it('loads expected modules by default', async () => {
   const modules = await getLoadedModulesAsync(`require('../../build/src/run').expoRun`);
   expect(modules).toStrictEqual([
-    '../node_modules/ansi-styles/index.js',
-    '../node_modules/arg/index.js',
-    '../node_modules/chalk/source/index.js',
-    '../node_modules/chalk/source/util.js',
-    '../node_modules/getenv/index.js',
-    '../node_modules/has-flag/index.js',
-    '../node_modules/supports-color/index.js',
     '@expo/cli/build/src/log.js',
     '@expo/cli/build/src/run/hints.js',
     '@expo/cli/build/src/run/index.js',
