314a3994548a5c39aea44920efc4fead1e9a74a6
Kadi Kraman <kadi@expo.io>
2024-02-19 17:03:02 +0000

    Prevent app info from overflowing

--- packages/expo-dev-menu/app/components/Main.tsx
+++ packages/expo-dev-menu/app/components/Main.tsx
@@ -496,10 +496,12 @@ type AppInfoRowProps = {
 
 function AppInfoRow({ title, value }: AppInfoRowProps) {
   return (
-    <Row px="medium" py="small" align="center" bg="default">
+    <Row px="medium" py="small" align="center" bg="default" justify="between" flex="1">
       <Text size="medium">{title}</Text>
-      <Spacer.Horizontal />
-      <Text>{value}</Text>
+      <Spacer.Horizontal size='small' />
+      <View style={{ flex: 1, alignItems: 'flex-end' }}>
+        <Text size="medium" numberOfLines={1}>{value}</Text>
+      </View>
     </Row>
   );
 }
