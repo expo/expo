a50c2e8427154a6436447e700e432962275d312f
Kudo Chien <kudo@expo.dev>
2024-02-20 02:06:42 +0800

    [bare-expo] enable dev-client

--- apps/bare-expo/ios/BareExpo/AppDelegate.mm
+++ apps/bare-expo/ios/BareExpo/AppDelegate.mm
@@ -17,7 +17,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 #if DEBUG
-  BOOL useDevClient = NO;
+  BOOL useDevClient = YES;
 
   if (!useDevClient) {
     ExpoDevLauncherReactDelegateHandler.enableAutoSetup = NO;
