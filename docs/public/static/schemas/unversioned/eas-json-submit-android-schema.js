a80e49da74dcfda195a9702e24e931593efb08bc
Aman Mittal <amandeepmittal@live.com>
2024-02-21 18:54:04 +0530

    [docs] Fix formatting issues and links in EAS JSON reference (#27202)
    # Why
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    Currently, the **eas.json** reference does not follow formatting as per
    our writing style guide and has some broken internal links. Came across
    these issues while working on EAS Tutorial.
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    This PR fixes aforementioned issues and also adds some new internal
    links to support the concept/reference in all schema table files.
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    Run docs locally and visit: http://localhost:3002/eas/json or see
    **Preview**.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [x] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- docs/public/static/schemas/unversioned/eas-json-submit-android-schema.js
+++ docs/public/static/schemas/unversioned/eas-json-submit-android-schema.js
@@ -2,7 +2,7 @@ export default [
   {
     name: 'serviceAccountKeyPath',
     type: 'string',
-    description: ['Path to the JSON file with service account key used to authenticate with Google Play. [See how to create one](https://expo.fyi/creating-google-service-account).'],
+    description: ['Path to the JSON file with [Google Service Account Key](https://expo.fyi/creating-google-service-account) used to authenticate with Google Play.'],
   },
   {
     name: 'track',
@@ -12,23 +12,23 @@ export default [
   {
     name: 'releaseStatus',
     enum: ['completed', 'draft', 'halted', 'inProgress'],
-    description: ['The status of a release. [Learn more](https://developers.google.com/android-publisher/api-ref/rest/v3/edits.tracks).'],
+    description: ['The [status of a release](https://developers.google.com/android-publisher/api-ref/rest/v3/edits.tracks#status).'],
   },
   {
     name: 'rollout',
     type: 'number',
-    description: ['The initial fraction of users who are eligible to receive the release. Should be a value from 0 (no users) to 1 (all users). Works only with `inProgress` release status. [Learn more](https://developers.google.com/android-publisher/api-ref/rest/v3/edits.tracks)'],
+    description: ['The initial fraction of users who are eligible to receive the release. Should be a value from 0 (no users) to 1 (all users). Works only with `inProgress` [release status](https://developers.google.com/android-publisher/api-ref/rest/v3/edits.tracks#status).'],
   },
   {
     name: 'changesNotSentForReview',
     type: 'boolean',
-    description: ['Indicates that the changes sent with this submission will not be reviewed until they are explicitly sent for review from the Google Play Console UI. Defaults to false.'],
+    description: ['Indicates that the changes sent with this submission will not be reviewed until they are explicitly sent for review from the Google Play Console UI. Defaults to `false`.'],
   },
   {
     name: 'applicationId',
     type: 'string',
     description: [
-      'The application id that will be used when accessing Service Account Keys managed by Expo, it does not have any effect if you are using local credentials. In most cases this value will be autodetected, but if you have multiple product flavors, this value might be necessary.',
+      'The application ID that is used when accessing Service Account Key managed by Expo. It does not have any effect if you are using local credentials. In most cases this value will be autodetected. However, if you have multiple product flavors, this value might be necessary.',
     ],
   },
 ];
