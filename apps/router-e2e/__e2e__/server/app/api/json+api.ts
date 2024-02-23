5c8dc4b365b20260f18eb1ee07da8402de8ec45e
Phil Pluckthun <phil@kitten.sh>
2024-02-23 19:00:13 +0000

    Convert over remaining API test routes using ExpoResponse

--- apps/router-e2e/__e2e__/server/app/api/json+api.ts
+++ apps/router-e2e/__e2e__/server/app/api/json+api.ts
@@ -1,4 +1,5 @@
 export async function POST(req: Request): Promise<Response> {
+  console.log(req);
   // Can serialize JSON -- this was a problem during development due to some earlier middleware (/logs) that were corrupting the request.
   const json = await req.json();
   return new Response(JSON.stringify(json));
