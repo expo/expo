58419af1ba5dd889425a8da2f9010805448c38f6
Phil Pluckthun <phil@kitten.sh>
2024-02-23 17:52:01 +0000

    Remove remaining references to ExpoRequest/Response

--- apps/router-e2e/__e2e__/server/app/api/json+api.ts
+++ apps/router-e2e/__e2e__/server/app/api/json+api.ts
@@ -1,7 +1,5 @@
-import { ExpoRequest, ExpoResponse } from 'expo-router/server';
-
-export async function POST(req: ExpoRequest): Promise<ExpoResponse> {
+export async function POST(req: Request): Promise<Response> {
   // Can serialize JSON -- this was a problem during development due to some earlier middleware (/logs) that were corrupting the request.
   const json = await req.json();
-  return ExpoResponse.json(json);
+  return new Response(JSON.stringify(json));
 }
--- packages/expo-router/server.d.ts
+++ packages/expo-router/server.d.ts
@@ -1,10 +1,6 @@
-// These should be installed on the global by the server runtime to ensure faster bundling and smaller bundles.
-
-import { ExpoRequest, ExpoResponse } from '@expo/server/build/environment';
+import '@expo/server/build/environment';
 
 export type RequestHandler = (
-  request: ExpoRequest,
+  request: Request,
   params: Record<string, string>
-) => ExpoResponse | Promise<ExpoResponse>;
-
-export { ExpoRequest, ExpoResponse };
+) => Response | Promise<Response>;
