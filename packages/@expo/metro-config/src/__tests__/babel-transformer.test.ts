04edb3419d61f76075ab55800760945886bf7cdf
evanbacon <bacon@expo.io>
2024-02-23 16:45:58 -0600

    add react server test

--- packages/@expo/metro-config/src/__tests__/babel-transformer.test.ts
+++ packages/@expo/metro-config/src/__tests__/babel-transformer.test.ts
@@ -62,6 +62,7 @@ it(`passes the environment as isServer to the babel preset`, () => {
     babelrc: true,
     caller: {
       // HERE IS THE MAGIC
+      isReactServer: false,
       isServer: true,
       isDev: true,
       bundler: 'metro',
@@ -75,6 +76,71 @@ it(`passes the environment as isServer to the babel preset`, () => {
       projectRoot: expect.any(String),
       routerRoot: 'app',
     },
+    cloneInputAst: false,
+    code: false,
+    cwd: '/',
+    extends: undefined,
+    filename: 'foo.js',
+    highlightCode: true,
+    presets: [expect.anything()],
+    plugins: [],
+    sourceType: 'unambiguous',
+  });
+});
+
+it(`passes the environment as isReactServer to the babel preset`, () => {
+  console.warn = jest.fn();
+  vol.fromJSON({}, '/');
+
+  const fixture = `import { Platform } from 'react-native';
+    
+    export default function App() {
+        return <div>Hello</div>
+    }`;
+
+  const results = transformer.transform({
+    filename: 'foo.js',
+    options: {
+      globalPrefix: '',
+      enableBabelRuntime: true,
+      enableBabelRCLookup: true,
+      dev: true,
+      projectRoot: '/',
+      hot: true,
+      inlineRequires: false,
+      minify: false,
+      platform: 'ios',
+      publicPath: '/',
+      customTransformOptions: Object.create({
+        environment: 'react-server',
+      }),
+    },
+    src: fixture,
+    plugins: [],
+  });
+
+  expect(console.warn).toBeCalledTimes(0);
+  expect(generate(results.ast).code).toMatchSnapshot();
+
+  expect(babel.transformSync).toBeCalledWith(fixture, {
+    ast: true,
+    babelrc: true,
+    caller: expect.objectContaining({
+      // HERE IS THE MAGIC
+      isReactServer: true,
+      isServer: true,
+      isDev: true,
+      bundler: 'metro',
+      engine: undefined,
+      name: 'metro',
+      platform: 'ios',
+      baseUrl: '',
+      isNodeModule: false,
+      isHMREnabled: true,
+      preserveEnvVars: undefined,
+      projectRoot: expect.any(String),
+      routerRoot: 'app',
+    }),
     cloneInputAst: false,
     code: false,
     cwd: '/',
