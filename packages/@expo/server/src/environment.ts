1b50ec20aaccf91aece4d7456b897382fa476d56
Phil Pluckthun <phil@kitten.sh>
2024-02-23 16:57:38 +0000

    Add missing Response static methods (json, error, redirect)

--- packages/@expo/server/src/environment.ts
+++ packages/@expo/server/src/environment.ts
@@ -1,5 +1,47 @@
 import { installGlobals as installRemixGlobals } from '@remix-run/node';
 
+declare const Response: {
+  prototype: Response;
+  new(body?: BodyInit | null, init?: ResponseInit): Response;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Response/error_static) */
+  error(): Response;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Response/json_static) */
+  json(data: any, init?: ResponseInit): Response;
+  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Response/redirect_static) */
+  redirect(url: string | URL, status?: number): Response;
+}
+
 export function installGlobals() {
   installRemixGlobals();
+
+  if (typeof Response.error !== 'function') {
+    Response.error = function error() {
+      return new Response(null, { status: 500 });
+    };
+  }
+
+  if (typeof Response.json !== 'function') {
+    Response.json = function json(data: any, init?: ResponseInit) {
+      return new Response(JSON.stringify(data), init);
+    };
+  }
+
+  if (typeof Response.redirect !== 'function') {
+    Response.redirect = function redirect(url: string | URL, status?: number) {
+      if (!status) status = 302;
+      switch (status) {
+        case 301:
+        case 302:
+        case 303:
+        case 307:
+        case 308:
+          return new Response(null, {
+            headers: { Location: new URL(url).toString() },
+            status,
+          });
+        default:
+          throw new RangeError(`Invalid status code ${status}`);
+      }
+    };
+  }
 }
