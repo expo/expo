5cab3403309690cd4cfb5b7ce0991c7fe8a786e1
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2024-03-01 05:50:18 +0100

    [core] Set bridge on AppContext (#27378)

--- packages/expo-modules-core/ios/Core/AppContext.swift
+++ packages/expo-modules-core/ios/Core/AppContext.swift
@@ -43,7 +43,7 @@ public final class AppContext: NSObject {
    or when the app context is "bridgeless" (for example in native unit tests).
    */
   @objc
-  public internal(set) weak var reactBridge: RCTBridge?
+  public weak var reactBridge: RCTBridge?
 
   /**
    Underlying JSI runtime of the running app.
