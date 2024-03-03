a614ce9f37d1a8cd13f882ab5b2a4828123a9c6f
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2024-03-03 11:33:34 +0100

    [core] Implement startObserving and stopObserving in the new EventEmitter class

--- packages/expo-modules-core/ios/Tests/EventEmitterSpec.swift
+++ packages/expo-modules-core/ios/Tests/EventEmitterSpec.swift
@@ -86,6 +86,82 @@ final class EventEmitterSpec: ExpoSpec {
         expect(args[1]) == 2
         expect(args[2]) == 24
       }
+
+      it("calls startObserving on addListener") {
+        var calls: Int = 0
+        let eventName = "testEvent"
+        let listenerA = runtime.createSyncFunction("listenerA") { _, _ in }
+        let listenerB = runtime.createSyncFunction("listenerB") { _, _ in }
+        let observer = try setupEventObserver(runtime: runtime, functionName: "startObserving") { arguments in
+          expect(try arguments.first?.asString()) == eventName
+          calls = calls + 1
+        }
+
+        observer.addListener.call(withArguments: [eventName, listenerA], thisObject: observer.emitter, asConstructor: false)
+        observer.addListener.call(withArguments: [eventName, listenerB], thisObject: observer.emitter, asConstructor: false)
+
+        expect(calls) == 1
+      }
+
+      it("calls stopObserving on removeListener") {
+        var calls: Int = 0
+        let eventName = "testEvent"
+        let listener = runtime.createSyncFunction("listener") { _, _ in }
+        let observer = try setupEventObserver(runtime: runtime, functionName: "stopObserving") { arguments in
+          expect(try arguments.first?.asString()) == eventName
+          calls = calls + 1
+        }
+
+        observer.addListener.call(withArguments: [eventName, listener], thisObject: observer.emitter, asConstructor: false)
+        observer.removeListener.call(withArguments: [eventName, listener], thisObject: observer.emitter, asConstructor: false)
+        observer.removeListener.call(withArguments: [eventName, listener], thisObject: observer.emitter, asConstructor: false)
+
+        expect(calls) == 1
+      }
+
+      it("calls stopObserving on removeAllListeners") {
+        var calls: Int = 0
+        let eventName = "testEvent"
+        let listener = runtime.createSyncFunction("listener") { _, _ in }
+        let observer = try setupEventObserver(runtime: runtime, functionName: "stopObserving") { arguments in
+          expect(try arguments.first?.asString()) == eventName
+          calls = calls + 1
+        }
+
+        observer.addListener.call(withArguments: [eventName, listener], thisObject: observer.emitter, asConstructor: false)
+        observer.removeAllListeners.call(withArguments: [eventName], thisObject: observer.emitter, asConstructor: false)
+        observer.removeAllListeners.call(withArguments: [eventName], thisObject: observer.emitter, asConstructor: false)
+
+        expect(calls) == 1
+      }
     }
   }
+}
+
+private typealias EventObserver = (
+  emitter: JavaScriptObject,
+  addListener: RawJavaScriptFunction,
+  removeListener: RawJavaScriptFunction,
+  removeAllListeners: RawJavaScriptFunction
+)
+
+private func setupEventObserver(
+  runtime: ExpoRuntime,
+  functionName: String,
+  callback: @escaping (_ arguments: [JavaScriptValue]) throws -> Void
+) throws -> EventObserver {
+  let emitter = try! runtime.eval("new expo.EventEmitter()").asObject()
+  let observingFunction = runtime.createSyncFunction(functionName) { [callback] this, arguments in
+    try callback(arguments)
+    return Optional<Any>.none as Any
+  }
+
+  emitter.setProperty(functionName, value: observingFunction)
+
+  return (
+    emitter,
+    addListener: try emitter.getProperty("addListener").asFunction(),
+    removeListener: try emitter.getProperty("removeListener").asFunction(),
+    removeAllListeners: try emitter.getProperty("removeAllListeners").asFunction()
+  )
 }
