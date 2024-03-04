08321334cc3aaf66642b0b77619acaf84cb27a66
Cedric van Putten <github@cedric.dev>
2024-03-04 15:00:53 +0100

    docs(cli): add changelog entry

--- packages/@expo/cli/src/start/server/metro/debugging/deviceMiddlewares/types.ts
+++ packages/@expo/cli/src/start/server/metro/debugging/deviceMiddlewares/types.ts
@@ -85,14 +85,6 @@ export abstract class DeviceMiddleware {
   }
 
   /**
-   * Determine if the device has the native capability, and can handle specific features itself.
-   * These target capabilities are defined in `@react-native/dev-middleware`.
-   */
-  hasCapability(flag: keyof TargetCapabilityFlags): boolean {
-    return this.deviceInfo.page.capabilities[flag] === true;
-  }
-
-  /**
    * Intercept a message coming from the device, modify or respond to it through `this._sendMessageToDevice`.
    * Return `true` if the message was handled, this will stop the message propagation.
    */
