10c2d14717f329d75789418478b62eaf41b41883
aleqsio <mikucki@gmail.com>
2024-03-07 18:27:34 +0100

    Rebuild

--- packages/expo-asset/build/AssetModule.web.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    downloadAsync(url: string, _hash: string | null, _type: string): Promise<string>;
-};
-export default _default;
-//# sourceMappingURL=AssetModule.web.d.ts.map
--- packages/expo-asset/build/AssetModule.web.js
+++
@@ -1,6 +0,0 @@
-export default {
-    async downloadAsync(url, _hash, _type) {
-        return url;
-    },
-};
-//# sourceMappingURL=AssetModule.web.js.map
