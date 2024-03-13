bc51c525f5ebe686684e1ccd73accddf53e64c36
Evan Bacon <bacon@expo.io>
2024-03-13 12:22:21 -0500

    feat(babel): minify typeof window (#27530)
    # Why
    
    It's common to use `typeof window === 'undefined'` to conditionally
    enable/disable code for server/client environments.
    
    `babel-preset-expo` automatically transforms `typeof window ===
    'undefined` to `true` when bundling for server environments, and `false`
    when bundling for websites. The check is left as-is when bundling for
    native client environments to support apps that polyfill `window`.
    
    This transform is run in both development and production, but only
    removes conditional requires in production.
    
    You can configure `babel-preset-expo` to skip the transform by passing
    `{ preserveTypeofWindow: false }`.
    
    # How
    
    Add `babel-plugin-transform-define` and conditionally enable when
    bundling for websites and server code. Skip entirely for native client
    bundles as these are ambiguous. We also expose the ability to force the
    flag on/off for native client in case users want a more strict runtime
    protection against `typeof window`.
    
    Additionally, I merged `process.env.EXPO_OS` and BASE_URL in this
    transform to reduce passes. In a follow up PR, I'll merge
    `process.env.NODE_ENV` and `__DEV__` too.
    
    # Test Plan
    
    - Added a bunch of babel tests for this functionality.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>

--- packages/babel-preset-expo/src/__tests__/platform-shaking.test.ts
+++ packages/babel-preset-expo/src/__tests__/platform-shaking.test.ts
@@ -56,6 +56,31 @@ it(`does not remove Platform module in development`, () => {
   );
 });
 
+it(`does not remove Platform module in development`, () => {
+  const options = {
+    ...DEFAULT_OPTS,
+    caller: getCaller({ name: 'metro', engine: 'hermes', platform: 'web', isDev: true }),
+  };
+
+  const sourceCode = `
+    import { Platform } from 'react-native';
+  
+    if (Platform.OS === 'ios') {
+      console.log('ios')
+    }
+    
+    Platform.select({
+      ios: () => console.log('ios'),
+      web: () => console.log('web'),
+      android: () => console.log('android'),
+    })
+    `;
+
+  expect(stripReactNativeImport(babel.transform(sourceCode, options)!.code!)).toMatch(
+    /_Platform\.default\.OS===/
+  );
+});
+
 it(`removes Platform module without import (undefined behavior)`, () => {
   const options = {
     ...DEFAULT_OPTS,
@@ -183,6 +208,242 @@ it(`removes __DEV__ usage`, () => {
 
   // No minfication needed here, the babel plugin does it to ensure the imports are removed before dependencies are collected.
   expect(babel.transform(sourceCode, options)!.code!).toEqual(``);
+});
+
+describe('SSR window check', () => {
+  const originalEnv = process.env;
+  beforeEach(() => {
+    process.env = { ...originalEnv };
+  });
+  afterAll(() => {
+    process.env = originalEnv;
+  });
+
+  [true, false].forEach((isDev) => {
+    ['development', 'test', 'production'].forEach((env) => {
+      it(`inlines typeof window usage in NODE_ENV=${env} when bundling for servers with dev=${isDev}`, async () => {
+        process.env.NODE_ENV = env;
+        const options = {
+          babelrc: false,
+          presets: [preset],
+          filename: 'unknown',
+          // compact: true,
+          // Make the snapshot easier to read
+          retainLines: true,
+          compact: true,
+          caller: getCaller({ name: 'metro', platform: 'web', isDev, isServer: true }),
+        };
+
+        const src = `
+        if (typeof window === 'undefined') {
+          console.log('ssr.1');
+        }
+        if ("undefined" === typeof window) {
+          console.log('ssr.2');
+        }
+        ("undefined" === typeof window) ? console.log('ssr.3') : undefined;
+        `;
+
+        const res = babel.transform(src, options);
+        expect(res?.code).toMatch('if(true){');
+
+        // Code is fully minified away
+        expect((await minifyLikeMetroAsync(res!)).code).toBe(
+          `console.log('ssr.1'),console.log('ssr.2'),console.log('ssr.3');`
+        );
+      });
+    });
+  });
+
+  it(`preserves typeof window usage in client bundles`, async () => {
+    const options = {
+      babelrc: false,
+      presets: [preset],
+      filename: 'unknown',
+      // compact: true,
+      // Make the snapshot easier to read
+      retainLines: true,
+      compact: true,
+      caller: getCaller({ name: 'metro', platform: 'web', isDev: false, isServer: false }),
+    };
+
+    const src = `
+    if (typeof window !== 'undefined') {
+      console.log('ssr.1');
+    }
+    `;
+
+    const res = babel.transform(src, options);
+    expect(res?.code).toMatch('if(true){');
+
+    // Code is fully minified away
+    expect((await minifyLikeMetroAsync(res!)).code).toBe(`console.log('ssr.1');`);
+  });
+  it(`preserves typeof window usage in client bundles`, async () => {
+    const options = {
+      babelrc: false,
+      presets: [preset],
+      filename: 'unknown',
+      // compact: true,
+      // Make the snapshot easier to read
+      retainLines: true,
+      compact: true,
+      caller: getCaller({ name: 'metro', platform: 'web', isDev: false, isServer: false }),
+    };
+
+    const src = `
+    if (typeof window !== 'undefined') {
+      console.log('ssr.1');
+    }
+    `;
+
+    const res = babel.transform(src, options);
+    expect(res?.code).toMatch('if(true){');
+
+    // Code is fully minified away
+    expect((await minifyLikeMetroAsync(res!)).code).toBe(`console.log('ssr.1');`);
+  });
+
+  it(`preserves typeof window runtime check in native client bundles`, async () => {
+    const options = {
+      babelrc: false,
+      presets: [preset],
+      filename: 'unknown',
+      // compact: true,
+      // Make the snapshot easier to read
+      retainLines: true,
+      compact: true,
+      caller: getCaller({ name: 'metro', platform: 'ios', isDev: false, isServer: false }),
+    };
+
+    const src = `
+    if (typeof window !== 'undefined') {
+      console.log('ssr.1');
+    }
+    `;
+
+    const res = babel.transform(src, options);
+    expect(res?.code).toMatch('if(typeof window');
+  });
+  it(`removes typeof window runtime check in native server bundles`, async () => {
+    const options = {
+      babelrc: false,
+      presets: [preset],
+      filename: 'unknown',
+      // compact: true,
+      // Make the snapshot easier to read
+      retainLines: true,
+      compact: true,
+      caller: getCaller({ name: 'metro', platform: 'ios', isDev: false, isServer: true }),
+    };
+
+    const src = `
+    if (false) {
+      console.log('ssr.1');
+    }
+    `;
+
+    const res = babel.transform(src, options);
+    expect(res?.code).toMatch('if(false');
+  });
+
+  it(`supports disabling typeof window inline`, async () => {
+    const options = {
+      babelrc: false,
+      presets: [[preset, { minifyTypeofWindow: false }]],
+      filename: 'unknown',
+      // compact: true,
+      // Make the snapshot easier to read
+      retainLines: true,
+      compact: true,
+      caller: getCaller({ name: 'metro', platform: 'android', isDev: false, isServer: false }),
+    };
+
+    const src = `
+    if (typeof window !== 'undefined') {
+      console.log('ssr.1');
+    }
+    `;
+
+    const res = babel.transform(src, options);
+    expect(res?.code).toMatch('typeof window');
+  });
+});
+
+describe('process.env.EXPO_OS', () => {
+  const originalEnv = process.env;
+  beforeEach(() => {
+    process.env = { ...originalEnv };
+  });
+  afterAll(() => {
+    process.env = originalEnv;
+  });
+
+  [true, false].forEach((isDev) => {
+    ['development', 'test', 'production'].forEach((env) => {
+      it(`inlines process.env.EXPO_OS usage in NODE_ENV=${env} when bundling for dev=${isDev}`, () => {
+        process.env.NODE_ENV = env;
+        const options = {
+          babelrc: false,
+          presets: [preset],
+          filename: 'unknown',
+          // Make the snapshot easier to read
+          retainLines: true,
+          caller: getCaller({ name: 'metro', platform: 'ios', isDev }),
+        };
+
+        expect(babel.transform('process.env.EXPO_OS', options)!.code).toBe('"ios";');
+        expect(
+          babel.transform('process.env.EXPO_OS', {
+            ...options,
+            caller: getCaller({ name: 'metro', platform: 'web', isDev }),
+          })!.code
+        ).toBe('"web";');
+      });
+    });
+  });
+
+  it(`can use process.env.EXPO_OS to minify`, async () => {
+    const options = {
+      babelrc: false,
+      presets: [preset],
+      filename: 'unknown',
+      // Make the snapshot easier to read
+      compact: true,
+      caller: getCaller({ name: 'metro', platform: 'ios', isDev: false }),
+    };
+
+    const src = `
+    if (process.env.EXPO_OS === 'ios') {
+      console.log('ios');
+    }
+    `;
+
+    const results = babel.transform(src, options)!;
+    const min = await minifyLikeMetroAsync(results);
+    expect(min.code).toBe("console.log('ios');");
+  });
+
+  it(`can use process.env.EXPO_OS to remove conditional`, async () => {
+    const options = {
+      babelrc: false,
+      presets: [preset],
+      filename: 'unknown',
+      // Make the snapshot easier to read
+      compact: true,
+      caller: getCaller({ name: 'metro', platform: 'android', isDev: false }),
+    };
+
+    const src = `
+    if (process.env.EXPO_OS === 'ios') {
+      require('thing');
+    }
+    `;
+
+    const results = babel.transform(src, options)!;
+    const min = await minifyLikeMetroAsync(results);
+    expect(min.code).toBe('0;');
+  });
 });
 
 describe('process.env.EXPO_OS', () => {
