baf06cc41240f6b49a0dba60d8109560739bc7f9
Mark Rickert <139261+markrickert@users.noreply.github.com>
2024-03-13 17:49:53 -0400

    [module-template] Fix typo in `metro.config.js` comments (#27639)

--- packages/expo-module-template/example/metro.config.js
+++ packages/expo-module-template/example/metro.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 const config = getDefaultConfig(__dirname);
 
 // npm v7+ will install ../node_modules/react and ../node_modules/react-native because of peerDependencies.
-// To prevent the incompatible react-native bewtween ./node_modules/react-native and ../node_modules/react-native,
+// To prevent the incompatible react-native between ./node_modules/react-native and ../node_modules/react-native,
 // excludes the one from the parent folder when bundling.
 config.resolver.blockList = [
   ...Array.from(config.resolver.blockList ?? []),
