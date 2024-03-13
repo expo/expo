b123a98817df605092658a16b7c5eb5c41593cb8
Alan Hughes <alan@expo.io>
2024-03-13 14:37:07 +0000

    [audio] rebuild js

--- packages/expo-audio/build/AudioModule.web.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: {
-    PI: number;
-    setValueAsync(value: string): Promise<void>;
-    hello(): string;
-};
-export default _default;
-//# sourceMappingURL=AudioModule.web.d.ts.map
