bd8b80ef9040575899f2883bc47296a639df92da
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2024-03-13 15:50:44 +0000

    [audio] Create module (#27626)

---
+++ packages/expo-audio/android/src/main/java/expo/modules/audio/AudioModule.kt
@@ -0,0 +1,10 @@
+package expo.modules.audio
+
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class AudioModule : Module() {
+  override fun definition() = ModuleDefinition {
+    Name("AudioModule")
+  }
+}
