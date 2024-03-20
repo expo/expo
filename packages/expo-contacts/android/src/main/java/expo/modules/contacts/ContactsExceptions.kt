2b8be570b218e6115ab775c7e26b0f92a9fb80b0
Jakov Glavina <jakov.glavina@icloud.com>
2024-03-20 13:57:07 +0100

    move android exceptions to separate file

--- packages/expo-contacts/android/src/main/java/expo/modules/contacts/ContactsExceptions.kt
+++ packages/expo-contacts/android/src/main/java/expo/modules/contacts/ContactsExceptions.kt
@@ -7,3 +7,15 @@ class ContactPickingInProgressException :
 
 class ContactManipulationInProgressException :
   CodedException("Different contact manipulation in progress. Await other contact manipulation first.")
+
+class MissingPermissionException(permission: String) : CodedException("Missing $permission permission")
+
+class RetrieveIdException : CodedException("Couldn't get the contact id")
+
+class AddContactException : CodedException("Given contact couldn't be added")
+
+class ContactNotFoundException : CodedException("Couldn't find contact")
+
+class ContactUpdateException : CodedException("Given contact couldn't be updated")
+
+class LookupKeyNotFoundException : CodedException("Couldn't find lookup key for contact")
