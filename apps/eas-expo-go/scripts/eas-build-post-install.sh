aa7191ea8bc475a738941ca815f2be13f744c114
Alan Hughes <alan@expo.io>
2024-03-22 10:56:22 +0000

    edit post install

--- apps/eas-expo-go/scripts/eas-build-post-install.sh
+++ apps/eas-expo-go/scripts/eas-build-post-install.sh
@@ -26,7 +26,7 @@ if [ "$EAS_BUILD_PLATFORM" = "ios" ]; then
   et ios-generate-dynamic-macros
 elif [ "$EAS_BUILD_PLATFORM" = "android" ]; then
   if [ "$EAS_BUILD_PROFILE" != "versioned-client-add-sdk" ]; then
-    maybe_prebuild_hermes
+    # maybe_prebuild_hermes
   fi
 fi
 
