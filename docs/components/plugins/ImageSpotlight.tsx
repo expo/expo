7d599599a3d8da67b2d3459f2d1c263c38b9964e
Bartosz Kaszubowski <gosimek@gmail.com>
2024-03-23 21:26:22 +0100

    [docs] add rounded borders for ImageSpotlight and Video components (#27808)

--- docs/components/plugins/ImageSpotlight.tsx
+++ docs/components/plugins/ImageSpotlight.tsx
@@ -13,8 +13,9 @@ type Props = {
 
 export default function ImageSpotlight({ alt, src, style, containerClassName, caption }: Props) {
   return (
-    <figure className={mergeClasses('text-center bg-subtle py-2.5 my-5', containerClassName)}>
-      <LightboxImage src={src} alt={alt} style={style} className="inline" />
+    <figure
+      className={mergeClasses('text-center bg-subtle py-2.5 my-5 rounded-lg', containerClassName)}>
+      <LightboxImage src={src} alt={alt} style={style} className="inline rounded-md" />
       {caption && (
         <figcaption className="mt-[14px] text-secondary text-center text-xs px-8 py-2">
           {caption}
