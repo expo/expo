b99df9bfad78388a3508e07ca4c397bbc31c0053
Kadi Kraman <kadi@expo.io>
2024-03-27 18:47:17 +0000

    Code review comments

--- packages/@expo/cli/src/lint/index.ts
+++ packages/@expo/cli/src/lint/index.ts
@@ -1,9 +1,6 @@
-#!/usr/bin/env node
-
-import chalk from 'chalk';
-
-import { Command } from '../../bin/cli';
+import type { Command } from '../../bin/cli';
 import { assertArgs, getProjectRoot, printHelp } from '../utils/args';
+import { logCmdError } from '../utils/errors';
 
 export const expoLint: Command = async (argv) => {
   const args = assertArgs(
@@ -18,15 +15,14 @@ export const expoLint: Command = async (argv) => {
 
   if (args['--help']) {
     printHelp(
-      `Utility for setting up linting on an Expo project`,
-      chalk`npx expo lint`,
-      [`-h, --help                 Usage info`].join('\n')
+      'Utility for setting up linting on an Expo project',
+      'npx expo lint',
+      '-h, --help                 Usage info'
     );
   }
 
   // Load modules after the help prompt so `npx expo lint -h` shows as fast as possible.
-  const { lintAsync } = require('./lintAsync') as typeof import('./lintAsync');
-  const { logCmdError } = require('../utils/errors') as typeof import('../utils/errors');
+  const { lintAsync } = await import('./lintAsync.js');
   const projectRoot = getProjectRoot(args);
 
   return lintAsync(projectRoot).catch(logCmdError);
