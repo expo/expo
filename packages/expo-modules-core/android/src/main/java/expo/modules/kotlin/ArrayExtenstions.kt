953a048c3c02892c4cad0fae57e96cde3c22ce28
lukmccall <kosmatylukasz@gmail.com>
2024-03-27 15:42:23 +0100

    [core][Android] Add direct resolve functions for simple types

---
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/ArrayExtenstions.kt
@@ -0,0 +1,15 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package expo.modules.kotlin
+
+inline operator fun <T> Array<out T>.component6(): T {
+  return get(5)
+}
+
+inline operator fun <T> Array<out T>.component7(): T {
+  return get(6)
+}
+
+inline operator fun <T> Array<out T>.component8(): T {
+  return get(7)
+}
--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/functions/AsyncFunctionComponent.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/functions/AsyncFunctionComponent.kt
@@ -4,12 +4,70 @@ import com.facebook.react.bridge.ReadableArray
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.Promise
 import expo.modules.kotlin.exception.CodedException
+import expo.modules.kotlin.objects.enforceType
 import expo.modules.kotlin.types.AnyType
 
-class AsyncFunctionComponent(
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T> createAsyncFunctionComponent(
   name: String,
   desiredArgsTypes: Array<AnyType>,
-  private val body: (args: Array<out Any?>) -> Any?
+  noinline body: (args: Array<out Any?>) -> T
+): AsyncFunction {
+  if (null is T) {
+    return AsyncFunctionComponent<Any?>(name, desiredArgsTypes, body)
+  }
+  return when (T::class.java) {
+    Int::class.java -> {
+      enforceType<(args: Array<out Any?>) -> Int>(body)
+      IntAsyncFunctionComponent(name, desiredArgsTypes, body)
+    }
+    Boolean::class.java -> BoolAsyncFunctionComponent(name, desiredArgsTypes, body as (Array<out Any?>) -> Boolean)
+    Double::class.java -> DoubleAsyncFunctionComponent(name, desiredArgsTypes, body as (Array<out Any?>) -> Double)
+    Float::class.java -> FloatAsyncFunctionComponent(name, desiredArgsTypes, body as (Array<out Any?>) -> Float)
+    String::class.java -> StringAsyncFunctionComponent(name, desiredArgsTypes, body as (Array<out Any?>) -> String)
+    else -> AsyncFunctionComponent<Any?>(name, desiredArgsTypes, body)
+  }
+}
+
+class IntAsyncFunctionComponent(name: String, desiredArgsTypes: Array<AnyType>, body: (args: Array<out Any?>) -> Int) :
+  AsyncFunctionComponent<Int>(name, desiredArgsTypes, body) {
+  override fun callUserImplementation(args: Array<Any?>, promise: Promise, appContext: AppContext) {
+    promise.resolve(body(convertArgs(args, appContext)))
+  }
+}
+
+class BoolAsyncFunctionComponent(name: String, desiredArgsTypes: Array<AnyType>, body: (args: Array<out Any?>) -> Boolean) :
+  AsyncFunctionComponent<Boolean>(name, desiredArgsTypes, body) {
+  override fun callUserImplementation(args: Array<Any?>, promise: Promise, appContext: AppContext) {
+    promise.resolve(body(convertArgs(args, appContext)))
+  }
+}
+
+class DoubleAsyncFunctionComponent(name: String, desiredArgsTypes: Array<AnyType>, body: (args: Array<out Any?>) -> Double) :
+  AsyncFunctionComponent<Double>(name, desiredArgsTypes, body) {
+  override fun callUserImplementation(args: Array<Any?>, promise: Promise, appContext: AppContext) {
+    promise.resolve(body(convertArgs(args, appContext)))
+  }
+}
+
+class FloatAsyncFunctionComponent(name: String, desiredArgsTypes: Array<AnyType>, body: (args: Array<out Any?>) -> Float) :
+  AsyncFunctionComponent<Float>(name, desiredArgsTypes, body) {
+  override fun callUserImplementation(args: Array<Any?>, promise: Promise, appContext: AppContext) {
+    promise.resolve(body(convertArgs(args, appContext)))
+  }
+}
+
+class StringAsyncFunctionComponent(name: String, desiredArgsTypes: Array<AnyType>, body: (args: Array<out Any?>) -> String) :
+  AsyncFunctionComponent<String>(name, desiredArgsTypes, body) {
+  override fun callUserImplementation(args: Array<Any?>, promise: Promise, appContext: AppContext) {
+    promise.resolve(body(convertArgs(args, appContext)))
+  }
+}
+
+open class AsyncFunctionComponent<ReturnType>(
+  name: String,
+  desiredArgsTypes: Array<AnyType>,
+  protected val body: (args: Array<out Any?>) -> ReturnType
 ) : AsyncFunction(name, desiredArgsTypes) {
   @Throws(CodedException::class)
   override fun callUserImplementation(args: ReadableArray, promise: Promise) {
