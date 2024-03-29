cab42271215be7c2109ea0f1357b8e820612e8d4
Cedric van Putten <github@cedric.dev>
2024-03-29 18:07:12 +0100

    test(cli): ensure `serializeHtml` adds no extranous starting slashes when exporting with custom baseurl

--- packages/@expo/cli/src/start/server/metro/__tests__/serializeHtml.test.ts
+++ packages/@expo/cli/src/start/server/metro/__tests__/serializeHtml.test.ts
@@ -63,3 +63,27 @@ it('serializes development export static html with correct script src', () => {
     /<script src="\/Users\/path\/to\/expo\/app\/node_modules\/expo\/AppEntry.js" defer>/
   );
 });
+
+it('serializes development export static html with correct baseUrl and script src', () => {
+  const res = serializeHtmlWithAssets({
+    resources: [
+      {
+        filename: '/Users/path/to/expo/app/node_modules/expo/AppEntry.js',
+        originFilename: 'node_modules/expo/AppEntry.js',
+        type: 'js',
+        metadata: { isAsync: false, requires: [], modulePaths: [] },
+        source: '',
+      },
+    ],
+    template: '<!DOCTYPE html><html><head></head><body><div id="root"></div></body></html>',
+    // Note, when `isExporting` is true, we combine `baseUrl` with the filename
+    // Wehn empty, this caused an additional `/` to be added at the beginning of the `<script src="" />` tag
+    baseUrl: 'custom/base/url',
+    isExporting: true,
+  });
+  expect(res).toMatchSnapshot();
+  expect(res).toMatch(
+    // Note the explicit `/Users` part, not adding a double `//` by accident
+    /<script src="custom\/base\/url\/Users\/path\/to\/expo\/app\/node_modules\/expo\/AppEntry.js" defer>/
+  );
+});
