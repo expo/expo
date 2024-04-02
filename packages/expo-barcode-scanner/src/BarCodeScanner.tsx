26dac96af017bb8c24a22a3fae49f019d310e9f9
Aman Mittal <amandeepmittal@live.com>
2024-04-03 00:39:40 +0530

    Fix library names

--- packages/expo-barcode-scanner/src/BarCodeScanner.tsx
+++ packages/expo-barcode-scanner/src/BarCodeScanner.tsx
@@ -132,7 +132,7 @@ export type BarCodeScannerProps = ViewProps & {
 /**
  * @deprecated
  * BarCodeScanner has been deprecated and will be removed in a future SDK version. Use `expo-camera` instead.
- * See [How to migrate from expo-barcode-scanner to expo-camera](https://expo.fyi/barcode-scanner-to-expo-camera)
+ * See [How to migrate from `expo-barcode-scanner` to `expo-camera`](https://expo.fyi/barcode-scanner-to-expo-camera)
  * for more details on how to migrate.
  */
 export class BarCodeScanner extends React.Component<BarCodeScannerProps> {
