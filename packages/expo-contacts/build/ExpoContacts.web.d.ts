74b64faf9343d7991c02344679565e6c73757f66
Jakov Glavina <jakov.glavina@icloud.com>
2024-04-02 12:24:04 +0200

    run yarn build

---
+++ packages/expo-contacts/build/ExpoContacts.web.d.ts
@@ -0,0 +1,7 @@
+import { PermissionResponse } from 'expo-modules-core';
+declare const _default: {
+    getPermissionsAsync(): Promise<PermissionResponse>;
+    requestPermissionsAsync(): Promise<PermissionResponse>;
+};
+export default _default;
+//# sourceMappingURL=ExpoContacts.web.d.ts.map
