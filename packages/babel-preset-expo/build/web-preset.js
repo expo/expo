f482510f20e6797e7174c1350763fd3d72629870
Evan Bacon <bacon@expo.io>
2024-04-03 15:36:38 -0500

    feat(babel): rewrite for web (attempt 2) (#27907)
    # Why
    
    - Alternative to https://github.com/expo/expo/pull/24725 which takes a
    less radical approach.
    - Instead of reimplementing all the functionality, we'll simply remove
    the unused babel transforms from the existing preset.
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # Test Plan
    
    - Hopefully all language tests catch any changes.
    - This only applies to web and ssr.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

---
+++ packages/babel-preset-expo/build/web-preset.js
@@ -0,0 +1,73 @@
+"use strict";
+/**
+ * Copyright © 2024 650 Industries.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * A fork of `@react-native/babel-preset` but with everything unrelated to web/ssr removed.
+ * https://github.com/facebook/react-native/blob/2af1da42ff517232f1309efed7565fe9ddbbac77/packages/react-native-babel-preset/src/configs/main.js#L1
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// use `this.foo = bar` instead of `this.defineProperty('foo', ...)`
+const loose = true;
+const defaultPlugins = [
+    [require('@babel/plugin-syntax-flow')],
+    [require('babel-plugin-transform-flow-enums')],
+    [require('@babel/plugin-transform-private-methods'), { loose }],
+    [require('@babel/plugin-transform-private-property-in-object'), { loose }],
+    // [require('@babel/plugin-syntax-dynamic-import')],
+    [require('@babel/plugin-syntax-export-default-from')],
+    // [require('@babel/plugin-syntax-nullish-coalescing-operator')],
+    // [require('@babel/plugin-syntax-optional-chaining')],
+    // [require('@babel/plugin-transform-unicode-regex')],
+];
+module.exports = function (babel, options) {
+    const extraPlugins = [];
+    // NOTE: We also remove `@react-native/babel-plugin-codegen` since it doesn't seem needed on web.
+    if (!options || !options.disableImportExportTransform) {
+        extraPlugins.push([require('@babel/plugin-proposal-export-default-from')], [
+            require('@babel/plugin-transform-modules-commonjs'),
+            {
+                strict: false,
+                strictMode: false, // prevent "use strict" injections
+                lazy: options.lazyImportExportTransform,
+                allowTopLevelThis: true, // dont rewrite global `this` -> `undefined`
+            },
+        ]);
+    }
+    if (!options || options.enableBabelRuntime !== false) {
+        // Allows configuring a specific runtime version to optimize output
+        const isVersion = typeof options?.enableBabelRuntime === 'string';
+        extraPlugins.push([
+            require('@babel/plugin-transform-runtime'),
+            {
+                helpers: true,
+                regenerator: false,
+                ...(isVersion && { version: options.enableBabelRuntime }),
+            },
+        ]);
+    }
+    return {
+        comments: false,
+        compact: true,
+        presets: [
+            // TypeScript support
+            [require('@babel/preset-typescript'), { allowNamespaces: true }],
+        ],
+        overrides: [
+            // the flow strip types plugin must go BEFORE class properties!
+            // there'll be a test case that fails if you don't.
+            {
+                plugins: [require('@babel/plugin-transform-flow-strip-types')],
+            },
+            {
+                plugins: defaultPlugins,
+            },
+            {
+                plugins: extraPlugins,
+            },
+        ],
+    };
+};
