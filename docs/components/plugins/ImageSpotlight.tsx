aec4256fbdecd04e308720f59781b0a0eb50c60f
Aman Mittal <amandeepmittal@live.com>
2024-04-03 17:50:41 +0530

    [docs] Add a hover effect in image when using ImageSpotlight

--- docs/components/plugins/ImageSpotlight.tsx
+++ docs/components/plugins/ImageSpotlight.tsx
@@ -15,7 +15,12 @@ export default function ImageSpotlight({ alt, src, style, containerClassName, ca
   return (
     <figure
       className={mergeClasses('text-center bg-subtle py-2.5 my-5 rounded-lg', containerClassName)}>
-      <LightboxImage src={src} alt={alt} style={style} className="inline rounded-md" />
+      <LightboxImage
+        src={src}
+        alt={alt}
+        style={style}
+        className="inline rounded-md transition duration-300 ease-in-out hover:opacity-70"
+      />
       {caption && (
         <figcaption className="mt-[14px] text-secondary text-center text-xs px-8 py-2">
           {caption}
