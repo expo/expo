d411e2be916bd3542de457cca8107c482ddb661a
Kadi Kraman <kadi@expo.io>
2024-04-03 14:33:59 +0100

    Use a prerequisite file

--- packages/@expo/cli/src/lint/lintAsync.ts
+++ packages/@expo/cli/src/lint/lintAsync.ts
@@ -1,96 +1,19 @@
-import JsonFile, { type JSONObject } from '@expo/json-file';
 import * as PackageManager from '@expo/package-manager';
 import spawnAsync from '@expo/spawn-async';
-import fs from 'fs/promises';
-import path from 'path';
 
+import { ESLintProjectPrerequisite } from './EslintPrerequisite';
 import { Log } from '../log';
 import { isInteractive } from '../utils/interactive';
-import { confirmAsync } from '../utils/prompts';
-
-const WITH_PRETTIER = `module.exports = {
-  root: true,
-  extends: ["expo", "prettier"],
-  plugins: ["expo", "prettier"],
-  rules: {
-    "prettier/prettier": ["warn"],
-  },
-};
-
-`;
-
-const ESLINT_ONLY = `module.exports = {
-  root: true,
-  extends: ["expo"],
-};
-`;
-
-const setupLinting = async (projectRoot: string) => {
-  const shouldSetupLint = await confirmAsync({
-    message: 'No ESLint config found. Install and configure ESLint in this project?',
-  });
-
-  if (!shouldSetupLint) {
-    return;
-  }
-
-  const shouldIncludePrettier = await confirmAsync({
-    message: 'Include prettier?',
-  });
-
-  const packages = [
-    'eslint',
-    // TODO(Kadi) uncomment once published
-    // 'eslint-config-expo',
-  ];
-
-  if (shouldIncludePrettier) {
-    packages.push('prettier');
-    packages.push('eslint-config-prettier');
-    packages.push('eslint-plugin-prettier');
-  }
-
-  const manager = PackageManager.createForProject(projectRoot);
-
-  // TODO(Kadi): how to add this as dev dependencies?
-  await manager.addAsync(packages);
-
-  await fs.writeFile(
-    path.join(projectRoot, '.eslintrc.js'),
-    shouldIncludePrettier ? WITH_PRETTIER : ESLINT_ONLY,
-    'utf8'
-  );
-
-  if (shouldIncludePrettier) {
-    await fs.writeFile(path.join(projectRoot, '.prettierrc'), '{}', 'utf8');
-  }
-
-  const scripts = JsonFile.read(path.join(projectRoot, 'package.json')).scripts;
-
-  if ((scripts as JSONObject)?.lint) {
-    Log.log('Skipped adding the lint script as one exists already');
-  } else {
-    await JsonFile.setAsync(
-      path.join(projectRoot, 'package.json'),
-      'scripts',
-      typeof scripts === 'object' ? { ...scripts, lint: 'eslint .' } : { lint: 'eslint .' },
-      { json5: false }
-    );
-  }
-
-  Log.log();
-  Log.log('Your eslint config has been set up 🎉');
-  Log.log('Run "npx expo lint" again to lint your code');
-};
 
 export const lintAsync = async (projectRoot: string) => {
-  try {
-    // TODO(Kadi): check for all config files https://eslint.org/docs/latest/use/configure/configuration-files#configuration-file-formats
-    await fs.readFile(path.join(projectRoot, '.eslintrc.js'), 'utf8');
-  } catch {
-    if (isInteractive()) {
-      return setupLinting(projectRoot);
-    } else {
+  const prerequisite = new ESLintProjectPrerequisite(projectRoot);
+
+  const hasESLintConfigured = await prerequisite.assertImplementation();
+
+  if (!hasESLintConfigured) {
+    const configured = await prerequisite.bootstrapAsync();
+
+    if (!configured && !isInteractive()) {
       Log.log('No ESLint setup found. Skipping linting.');
     }
   }
@@ -98,9 +21,7 @@ export const lintAsync = async (projectRoot: string) => {
   const packageManager = PackageManager.resolvePackageManager(projectRoot) || 'npm';
 
   // TODO(Kadi): check if there's a lint command first?
-  const commands = packageManager === 'npm' ? ['run', 'lint'] : ['lint'];
-
-  await spawnAsync(packageManager, commands, {
+  await spawnAsync(packageManager, ['run', 'lint'], {
     stdio: 'inherit',
     cwd: projectRoot,
   });
