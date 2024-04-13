expo/expo

commit d5079bb84dc5a56b0016317683cd0b83f9aa4184

[core][updates] Add bridgeless support for ReactNativeHostHandler (#27629)
# Why

add bridgeless support for our react delegate on android
close ENG-11646

# How

- we cannot use `getDefaultReactHost()` because we will fit the `You can
call getDefaultReactHost only with instances of DefaultReactNativeHost`
exception. since our reactNativeHost is actually a
ReactNativeHostWrapper. this pr tries to add a `ExpoReactHostFactory` to
handle that.
- refactor `ReactNativeHostHandler` a little bit to be
ReactHost/ReactInstanceManager independent
- drop react-native < 0.74.0 support

---------

Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ReactNativeHostHandler.java
@@ -1,22 +1,11 @@
 package expo.modules.core.interfaces;
 
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
-
 import androidx.annotation.Nullable;
 
-public interface ReactNativeHostHandler {
-  /**
-   * Given chance for modules to customize {@link ReactInstanceManager}
-   *
-   * @param useDeveloperSupport true if {@link com.facebook.react.ReactNativeHost} enabled developer support
-   * @return instance of {@link ReactInstanceManager}, or null if not to override
-   */
-  @Nullable
-  default ReactInstanceManager createReactInstanceManager(boolean useDeveloperSupport) {
-    return null;
-  }
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.bridge.ReactContext;
 
+public interface ReactNativeHostHandler {
   /**
    * Given chance for modules to override react bundle file.
    * e.g. for expo-updates
@@ -60,12 +49,16 @@ public interface ReactNativeHostHandler {
    * doesn't exist in the React Native 0.66 or below.
    *
    * @return custom DevSupportManagerFactory, or null if not to override
+   *
+   * NOTE: This callback is not support in bridgeless mode
    */
   @Nullable
   default Object getDevSupportManagerFactory() { return null; }
 
   /**
    * Given chance for modules to override the javascript executor factory.
+   *
+   * NOTE: This callback is not support in bridgeless mode
    */
   @Nullable
   default JavaScriptExecutorFactory getJavaScriptExecutorFactory() { return null; }
@@ -73,14 +66,14 @@ public interface ReactNativeHostHandler {
   //region event listeners
 
   /**
-   * Callback before {@link ReactInstanceManager} creation
+   * Callback before react instance creation
    */
-  default void onWillCreateReactInstanceManager(boolean useDeveloperSupport) {}
+  default void onWillCreateReactInstance(boolean useDeveloperSupport) {}
 
   /**
-   * Callback after {@link ReactInstanceManager} creation
+   * Callback after react instance creation
    */
-  default void onDidCreateReactInstanceManager(ReactInstanceManager reactInstanceManager, boolean useDeveloperSupport) {}
+  default void onDidCreateReactInstance(boolean useDeveloperSupport, ReactContext reactContext) {}
 
   //endregion
 }

packages/expo-updates/android/src/main/java/expo/modules/updates/DisabledUpdatesController.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.WritableMap
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
@@ -74,7 +74,7 @@ class DisabledUpdatesController(
   override val bundleAssetName: String?
     get() = launcher?.bundleAssetName
 
-  override fun onDidCreateReactInstanceManager(reactInstanceManager: ReactInstanceManager) {}
+  override fun onDidCreateReactInstanceManager(reactContext: ReactContext) {}
 
   @Synchronized
   override fun start() {

packages/expo-updates/android/src/main/java/expo/modules/updates/EnabledUpdatesController.kt
@@ -5,9 +5,9 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.WritableMap
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
@@ -24,8 +24,8 @@ import expo.modules.updates.manifest.ManifestMetadata
 import expo.modules.updates.procedures.CheckForUpdateProcedure
 import expo.modules.updates.procedures.FetchUpdateProcedure
 import expo.modules.updates.procedures.RelaunchProcedure
-import expo.modules.updates.selectionpolicy.SelectionPolicyFactory
 import expo.modules.updates.procedures.StartupProcedure
+import expo.modules.updates.selectionpolicy.SelectionPolicyFactory
 import expo.modules.updates.statemachine.UpdatesStateChangeEventSender
 import expo.modules.updates.statemachine.UpdatesStateContext
 import expo.modules.updates.statemachine.UpdatesStateEventType
@@ -143,8 +143,8 @@ class EnabledUpdatesController(
   override val bundleAssetName: String?
     get() = startupProcedure.bundleAssetName
 
-  override fun onDidCreateReactInstanceManager(reactInstanceManager: ReactInstanceManager) {
-    startupProcedure.onDidCreateReactInstanceManager(reactInstanceManager)
+  override fun onDidCreateReactInstanceManager(reactContext: ReactContext) {
+    startupProcedure.onDidCreateReactInstanceManager(reactContext)
   }
 
   @Synchronized

packages/expo-updates/android/src/main/java/expo/modules/updates/IUpdatesController.kt
@@ -2,7 +2,7 @@ package expo.modules.updates
 
 import android.os.Bundle
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactInstanceManager
+import com.facebook.react.bridge.ReactContext
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
 import expo.modules.updates.db.entity.AssetEntity
@@ -46,7 +46,7 @@ interface IUpdatesController {
    */
   var appContext: WeakReference<AppContext>?
 
-  fun onDidCreateReactInstanceManager(reactInstanceManager: ReactInstanceManager)
+  fun onDidCreateReactInstanceManager(reactContext: ReactContext)
 
   /**
    * Starts the update process to launch a previously-loaded update and (if configured to do so)

packages/expo-updates/android/src/main/java/expo/modules/updates/UpdatesDevLauncherController.kt
@@ -3,7 +3,7 @@ package expo.modules.updates
 import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
-import com.facebook.react.ReactInstanceManager
+import com.facebook.react.bridge.ReactContext
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
 import expo.modules.updates.db.DatabaseHolder
@@ -23,8 +23,8 @@ import expo.modules.updates.selectionpolicy.ReaperSelectionPolicyDevelopmentClie
 import expo.modules.updates.selectionpolicy.SelectionPolicy
 import expo.modules.updates.selectionpolicy.SelectionPolicyFactory
 import expo.modules.updates.statemachine.UpdatesStateContext
-import expo.modules.updatesinterface.UpdatesInterfaceCallbacks
 import expo.modules.updatesinterface.UpdatesInterface
+import expo.modules.updatesinterface.UpdatesInterfaceCallbacks
 import org.json.JSONObject
 import java.io.File
 import java.lang.ref.WeakReference
@@ -80,7 +80,7 @@ class UpdatesDevLauncherController(
   override val bundleAssetName: String
     get() = throw Exception("IUpdatesController.bundleAssetName should not be called in dev client")
 
-  override fun onDidCreateReactInstanceManager(reactInstanceManager: ReactInstanceManager) {}
+  override fun onDidCreateReactInstanceManager(reactContext: ReactContext) {}
 
   override fun start() {
     throw Exception("IUpdatesController.start should not be called in dev client")

packages/expo-updates/android/src/main/java/expo/modules/updates/UpdatesPackage.kt
@@ -7,13 +7,12 @@ import android.util.Log
 import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
 import com.facebook.react.ReactActivity
-import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.bridge.ReactContext
 import expo.modules.core.interfaces.ApplicationLifecycleListener
 import expo.modules.core.interfaces.Package
 import expo.modules.core.interfaces.ReactActivityHandler
 import expo.modules.core.interfaces.ReactNativeHostHandler
-
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -38,17 +37,17 @@ class UpdatesPackage : Package {
         return if (shouldAutoSetup(context) && (useNativeDebug || !useDeveloperSupport)) UpdatesController.instance.bundleAssetName else null
       }
 
-      override fun onWillCreateReactInstanceManager(useDeveloperSupport: Boolean) {
+      override fun onWillCreateReactInstance(useDeveloperSupport: Boolean) {
         if (shouldAutoSetup(context) && (useNativeDebug || !useDeveloperSupport)) {
           UpdatesController.initialize(context)
         }
       }
 
-      override fun onDidCreateReactInstanceManager(reactInstanceManager: ReactInstanceManager, useDeveloperSupport: Boolean) {
+      override fun onDidCreateReactInstance(useDeveloperSupport: Boolean, reactContext: ReactContext) {
         // WHEN_VERSIONING_REMOVE_FROM_HERE
         // This code path breaks versioning and is not necessary for Expo Go.
         if (shouldAutoSetup(context) && (useNativeDebug || !useDeveloperSupport)) {
-          UpdatesController.instance.onDidCreateReactInstanceManager(reactInstanceManager)
+          UpdatesController.instance.onDidCreateReactInstanceManager(reactContext)
         }
         // WHEN_VERSIONING_REMOVE_TO_HERE
       }

packages/expo-updates/android/src/main/java/expo/modules/updates/errorrecovery/ErrorRecovery.kt
@@ -4,16 +4,17 @@ import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
-import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactApplication
 import com.facebook.react.bridge.DefaultJSExceptionHandler
+import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactMarker
 import com.facebook.react.bridge.ReactMarker.MarkerListener
 import com.facebook.react.bridge.ReactMarkerConstants
 import com.facebook.react.devsupport.DisabledDevSupportManager
+import com.facebook.react.devsupport.interfaces.DevSupportManager
 import expo.modules.updates.logging.UpdatesErrorCode
 import expo.modules.updates.logging.UpdatesLogger
 import java.lang.ref.WeakReference
-import kotlin.Exception
 
 /**
  * Entry point for the error recovery flow. Responsible for initializing the error recovery handler
@@ -35,7 +36,7 @@ class ErrorRecovery(
   internal lateinit var handler: Handler
   internal val logger = UpdatesLogger(context)
 
-  private var weakReactInstanceManager: WeakReference<ReactInstanceManager>? = null
+  private var weakReactContext: WeakReference<ReactContext>? = null
   private var previousExceptionHandler: DefaultJSExceptionHandler? = null
 
   fun initialize(delegate: ErrorRecoveryDelegate) {
@@ -45,9 +46,9 @@ class ErrorRecovery(
     }
   }
 
-  fun startMonitoring(reactInstanceManager: ReactInstanceManager) {
+  fun startMonitoring(reactContext: ReactContext) {
     registerContentAppearedListener()
-    registerErrorHandler(reactInstanceManager)
+    registerErrorHandler(reactContext)
   }
 
   fun notifyNewRemoteLoadStatus(newStatus: ErrorRecoveryDelegate.RemoteLoadStatus) {
@@ -88,13 +89,19 @@ class ErrorRecovery(
     ReactMarker.removeListener(contentAppearedListener)
   }
 
-  private fun registerErrorHandler(reactInstanceManager: ReactInstanceManager) {
-    if (reactInstanceManager.devSupportManager !is DisabledDevSupportManager) {
+  private fun getDevSupportManager(reactContext: ReactContext): DevSupportManager {
+    val reactApplication = reactContext.applicationContext as ReactApplication
+    return reactApplication.reactHost?.devSupportManager
+      ?: reactApplication.reactNativeHost.reactInstanceManager.devSupportManager
+  }
+
+  private fun registerErrorHandler(reactContext: ReactContext) {
+    val devSupportManager = getDevSupportManager(reactContext)
+    if (devSupportManager !is DisabledDevSupportManager) {
       Log.d(TAG, "Unexpected type of ReactInstanceManager.DevSupportManager. expo-updates error recovery will not behave properly.")
       return
     }
 
-    val devSupportManager = reactInstanceManager.devSupportManager as DisabledDevSupportManager
     val defaultJSExceptionHandler = object : DefaultJSExceptionHandler() {
       override fun handleException(e: Exception?) {
         this@ErrorRecovery.handleException(e!!)
@@ -107,12 +114,13 @@ class ErrorRecovery(
       field[devSupportManager] = defaultJSExceptionHandler
       return@let previousValue as DefaultJSExceptionHandler
     }
-    weakReactInstanceManager = WeakReference(reactInstanceManager)
+    weakReactContext = WeakReference(reactContext)
   }
 
   private fun unregisterErrorHandler() {
-    weakReactInstanceManager?.get()?.let { reactInstanceManager ->
-      if (reactInstanceManager.devSupportManager !is DisabledDevSupportManager) {
+    weakReactContext?.get()?.let { reactContext ->
+      val devSupportManager = getDevSupportManager(reactContext)
+      if (devSupportManager !is DisabledDevSupportManager) {
         Log.d(TAG, "Unexpected type of ReactInstanceManager.DevSupportManager. expo-updates could not unregister its error handler")
         return
       }
@@ -120,13 +128,12 @@ class ErrorRecovery(
         return
       }
 
-      val devSupportManager = reactInstanceManager.devSupportManager as DisabledDevSupportManager
       val devSupportManagerClass = devSupportManager.javaClass
       devSupportManagerClass.getDeclaredField("mDefaultJSExceptionHandler").let { field ->
         field.isAccessible = true
         field[devSupportManager] = previousExceptionHandler
       }
-      weakReactInstanceManager = null
+      weakReactContext = null
     }
     // quitSafely will wait for processing messages to finish but cancel all messages scheduled for
     // a future time, so delay for a few more seconds in case there are any scheduled messages

commit b33e8c3bf148e4b0a908501e8277ed81cd8ece98

[core][updates] fix error recovery on android (#28177)
# Why

the error recovery of updates was broken since
https://github.com/expo/expo/pull/27629/files#diff-b034a3a37c87b036bb0d9c0549d6701774c79fedb4892dd44807d8947d75d4fbL27-R37.
apparently when ReactContext is available, it's too late for error
recovery because bundle is loaded and may throw exceptions already.

# How

- i feel #27629's change is correct for `onDidCreateReactInstance`. for
error recovery we sound like need to call point between
onWillCreateReactInstance and DevSupportManager available (error
recovery requires DevSupportManager to detect exceptions). this pr
introduces a new `onDidCreateDevSupportManager` for this purpose.
- also fix incorrect onWillCreateReactInstance timing on bridgeless mode
- seen a detox failed from proguard that i have to add an additional
proguard rule to fix that.

packages/expo-modules-core/android/src/main/java/expo/modules/core/interfaces/ReactNativeHostHandler.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.devsupport.interfaces.DevSupportManager;
 
 public interface ReactNativeHostHandler {
   /**
@@ -70,6 +71,11 @@ public interface ReactNativeHostHandler {
    * Callback before react instance creation
    */
   default void onWillCreateReactInstance(boolean useDeveloperSupport) {}
+
+  /**
+   * Callback when the {@link DevSupportManager} is available
+   */
+  default void onDidCreateDevSupportManager(@NonNull DevSupportManager devSupportManager) {}
 
   /**
    * Callback after react instance creation

packages/expo-updates/android/src/main/java/expo/modules/updates/DisabledUpdatesController.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.WritableMap
+import com.facebook.react.devsupport.interfaces.DevSupportManager
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
 import expo.modules.kotlin.exception.toCodedException
@@ -68,7 +69,9 @@ class DisabledUpdatesController(
   override val bundleAssetName: String?
     get() = launcher?.bundleAssetName
 
-  override fun onDidCreateReactInstanceManager(reactContext: ReactContext) {
+  override fun onDidCreateDevSupportManager(devSupportManager: DevSupportManager) {}
+
+  override fun onDidCreateReactInstance(reactContext: ReactContext) {
     weakActivity = WeakReference(reactContext.currentActivity)
   }
 

packages/expo-updates/android/src/main/java/expo/modules/updates/EnabledUpdatesController.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.WritableMap
+import com.facebook.react.devsupport.interfaces.DevSupportManager
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
 import expo.modules.kotlin.exception.toCodedException
@@ -118,8 +119,11 @@ class EnabledUpdatesController(
   override val bundleAssetName: String?
     get() = startupProcedure.bundleAssetName
 
-  override fun onDidCreateReactInstanceManager(reactContext: ReactContext) {
-    startupProcedure.onDidCreateReactInstanceManager(reactContext)
+  override fun onDidCreateDevSupportManager(devSupportManager: DevSupportManager) {
+    startupProcedure.onDidCreateDevSupportManager(devSupportManager)
+  }
+
+  override fun onDidCreateReactInstance(reactContext: ReactContext) {
     weakActivity = WeakReference(reactContext.currentActivity)
   }
 

packages/expo-updates/android/src/main/java/expo/modules/updates/IUpdatesController.kt
@@ -3,6 +3,7 @@ package expo.modules.updates
 import android.os.Bundle
 import com.facebook.react.ReactApplication
 import com.facebook.react.bridge.ReactContext
+import com.facebook.react.devsupport.interfaces.DevSupportManager
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
 import expo.modules.updates.db.entity.AssetEntity
@@ -44,7 +45,9 @@ interface IUpdatesController {
    */
   var appContext: WeakReference<AppContext>?
 
-  fun onDidCreateReactInstanceManager(reactContext: ReactContext)
+  fun onDidCreateDevSupportManager(devSupportManager: DevSupportManager)
+
+  fun onDidCreateReactInstance(reactContext: ReactContext)
 
   fun onReactInstanceException(exception: java.lang.Exception)
 

packages/expo-updates/android/src/main/java/expo/modules/updates/UpdatesDevLauncherController.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
 import com.facebook.react.bridge.ReactContext
+import com.facebook.react.devsupport.interfaces.DevSupportManager
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.CodedException
 import expo.modules.updates.db.DatabaseHolder
@@ -79,7 +80,9 @@ class UpdatesDevLauncherController(
   override val bundleAssetName: String
     get() = throw Exception("IUpdatesController.bundleAssetName should not be called in dev client")
 
-  override fun onDidCreateReactInstanceManager(reactContext: ReactContext) {}
+  override fun onDidCreateDevSupportManager(devSupportManager: DevSupportManager) {}
+
+  override fun onDidCreateReactInstance(reactContext: ReactContext) {}
 
   override fun onReactInstanceException(exception: java.lang.Exception) {}
 

packages/expo-updates/android/src/main/java/expo/modules/updates/UpdatesPackage.kt
@@ -7,6 +7,7 @@ import androidx.annotation.WorkerThread
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.bridge.ReactContext
+import com.facebook.react.devsupport.interfaces.DevSupportManager
 import expo.modules.core.interfaces.ApplicationLifecycleListener
 import expo.modules.core.interfaces.Package
 import expo.modules.core.interfaces.ReactActivityHandler
@@ -38,8 +39,12 @@ class UpdatesPackage : Package {
         UpdatesController.initialize(context)
       }
 
+      override fun onDidCreateDevSupportManager(devSupportManager: DevSupportManager) {
+        UpdatesController.instance.onDidCreateDevSupportManager(devSupportManager)
+      }
+
       override fun onDidCreateReactInstance(useDeveloperSupport: Boolean, reactContext: ReactContext) {
-        UpdatesController.instance.onDidCreateReactInstanceManager(reactContext)
+        UpdatesController.instance.onDidCreateReactInstance(reactContext)
       }
 
       override fun onReactInstanceException(useDeveloperSupport: Boolean, exception: Exception) {

packages/expo-updates/android/src/main/java/expo/modules/updates/errorrecovery/ErrorRecovery.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
-import com.facebook.react.ReactApplication
 import com.facebook.react.bridge.DefaultJSExceptionHandler
-import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactMarker
 import com.facebook.react.bridge.ReactMarker.MarkerListener
 import com.facebook.react.bridge.ReactMarkerConstants
@@ -37,7 +35,7 @@ class ErrorRecovery(
   internal lateinit var handler: Handler
   internal val logger = UpdatesLogger(context)
 
-  private var weakReactContext: WeakReference<ReactContext>? = null
+  private var weakDevSupportManager: WeakReference<DevSupportManager>? = null
   private var previousExceptionHandler: DefaultJSExceptionHandler? = null
   private var shouldHandleReactInstanceException = false
 
@@ -48,9 +46,9 @@ class ErrorRecovery(
     }
   }
 
-  fun startMonitoring(reactContext: ReactContext) {
+  fun startMonitoring(devSupportManager: DevSupportManager) {
     registerContentAppearedListener()
-    registerErrorHandler(reactContext)
+    registerErrorHandler(devSupportManager)
   }
 
   /**
@@ -101,31 +99,19 @@ class ErrorRecovery(
     ReactMarker.removeListener(contentAppearedListener)
   }
 
-  private fun getDevSupportManager(reactContext: ReactContext): DevSupportManager {
-    val reactApplication = reactContext.applicationContext as ReactApplication
+  private fun registerErrorHandler(devSupportManager: DevSupportManager) {
     if (ReactFeatureFlags.enableBridgelessArchitecture) {
-      val reactHost = reactApplication.reactHost
-      check(reactHost != null)
-      return reactHost.devSupportManager ?: throw IllegalStateException("Unable to get DevSupportManager from ReactHost")
-    }
-
-    return reactApplication.reactNativeHost.reactInstanceManager.devSupportManager
-  }
-
-  private fun registerErrorHandler(reactContext: ReactContext) {
-    if (ReactFeatureFlags.enableBridgelessArchitecture) {
-      registerErrorHandlerImplBridgeless(reactContext)
+      registerErrorHandlerImplBridgeless(devSupportManager)
     } else {
-      registerErrorHandlerImplBridge(reactContext)
+      registerErrorHandlerImplBridge(devSupportManager)
     }
   }
 
-  private fun registerErrorHandlerImplBridgeless(reactContext: ReactContext) {
+  private fun registerErrorHandlerImplBridgeless(devSupportManager: DevSupportManager) {
     shouldHandleReactInstanceException = true
   }
 
-  private fun registerErrorHandlerImplBridge(reactContext: ReactContext) {
-    val devSupportManager = getDevSupportManager(reactContext)
+  private fun registerErrorHandlerImplBridge(devSupportManager: DevSupportManager) {
     if (devSupportManager !is DisabledDevSupportManager) {
       Log.d(TAG, "Unexpected type of ReactInstanceManager.DevSupportManager. expo-updates error recovery will not behave properly.")
       return
@@ -143,7 +129,7 @@ class ErrorRecovery(
       field[devSupportManager] = defaultJSExceptionHandler
       return@let previousValue as DefaultJSExceptionHandler
     }
-    weakReactContext = WeakReference(reactContext)
+    weakDevSupportManager = WeakReference(devSupportManager)
   }
 
   private fun unregisterErrorHandler() {
@@ -159,8 +145,7 @@ class ErrorRecovery(
   }
 
   private fun unregisterErrorHandlerImplBridge() {
-    weakReactContext?.get()?.let { reactContext ->
-      val devSupportManager = getDevSupportManager(reactContext)
+    weakDevSupportManager?.get()?.let { devSupportManager ->
       if (devSupportManager !is DisabledDevSupportManager) {
         Log.d(TAG, "Unexpected type of ReactInstanceManager.DevSupportManager. expo-updates could not unregister its error handler")
         return
@@ -174,7 +159,7 @@ class ErrorRecovery(
         field.isAccessible = true
         field[devSupportManager] = previousExceptionHandler
       }
-      weakReactContext = null
+      weakDevSupportManager = null
     }
     // quitSafely will wait for processing messages to finish but cancel all messages scheduled for
     // a future time, so delay for a few more seconds in case there are any scheduled messages

packages/expo-updates/android/src/main/java/expo/modules/updates/procedures/StartupProcedure.kt
@@ -3,7 +3,7 @@ package expo.modules.updates.procedures
 import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
-import com.facebook.react.bridge.ReactContext
+import com.facebook.react.devsupport.interfaces.DevSupportManager
 import expo.modules.updates.UpdatesConfiguration
 import expo.modules.updates.db.DatabaseHolder
 import expo.modules.updates.db.entity.AssetEntity
@@ -220,11 +220,11 @@ class StartupProcedure(
     callback.onFinished()
   }
 
-  fun onDidCreateReactInstanceManager(reactContext: ReactContext) {
+  fun onDidCreateDevSupportManager(devSupportManager: DevSupportManager) {
     if (emergencyLaunchException != null) {
       return
     }
-    errorRecovery.startMonitoring(reactContext)
+    errorRecovery.startMonitoring(devSupportManager)
   }
 
   fun onReactInstanceException(exception: Exception) {

packages/expo/android/src/main/java/expo/modules/ExpoReactHostFactory.kt
@@ -89,6 +89,10 @@ object ExpoReactHostFactory {
       val componentFactory = ComponentFactory()
       DefaultComponentsRegistry.register(componentFactory)
 
+      reactNativeHost.reactNativeHostHandlers.forEach { handler ->
+        handler.onWillCreateReactInstance(useDeveloperSupport)
+      }
+
       val reactHostImpl =
         ReactHostImpl(
           context,
@@ -101,6 +105,10 @@ object ExpoReactHostFactory {
           .apply {
             jsEngineResolutionAlgorithm = reactNativeHost.jsEngineResolutionAlgorithm
           }
+
+      reactNativeHost.reactNativeHostHandlers.forEach { handler ->
+        handler.onDidCreateDevSupportManager(reactHostImpl.devSupportManager)
+      }
 
       reactHostImpl.addReactInstanceEventListener(object : ReactInstanceEventListener {
         override fun onReactContextInitialized(context: ReactContext) {

packages/expo/android/src/main/java/expo/modules/ReactActivityDelegateWrapper.kt
@@ -89,7 +89,6 @@ class ReactActivityDelegateWrapper(
       mReactDelegate.isAccessible = true
       val reactDelegate = mReactDelegate[delegate] as ReactDelegate
 
-      dispatchWillCreateReactInstanceIfNeeded()
       reactDelegate.loadApp(appKey)
       rootViewContainer.addView(reactDelegate.reactRootView, ViewGroup.LayoutParams.MATCH_PARENT)
       activity.setContentView(rootViewContainer)
@@ -106,7 +105,6 @@ class ReactActivityDelegateWrapper(
       shouldEmitPendingResume = true
       delayLoadAppHandler.whenReady {
         Utils.assertMainThread()
-        dispatchWillCreateReactInstanceIfNeeded()
         invokeDelegateMethod<Unit, String?>("loadApp", arrayOf(String::class.java), arrayOf(appKey))
         reactActivityLifecycleListeners.forEach { listener ->
           listener.onContentChanged(activity)
@@ -117,7 +115,6 @@ class ReactActivityDelegateWrapper(
       return
     }
 
-    dispatchWillCreateReactInstanceIfNeeded()
     invokeDelegateMethod<Unit, String?>("loadApp", arrayOf(String::class.java), arrayOf(appKey))
     reactActivityLifecycleListeners.forEach { listener ->
       listener.onContentChanged(activity)
@@ -330,15 +327,6 @@ class ReactActivityDelegateWrapper(
       methodMap[name] = method
     }
     return method!!.invoke(delegate, *args) as T
-  }
-
-  private fun dispatchWillCreateReactInstanceIfNeeded() {
-    if (_reactHost != null) {
-      val useDeveloperSupport = _reactNativeHost.useDeveloperSupport
-      (_reactNativeHost as? ReactNativeHostWrapper)?.reactNativeHostHandlers?.forEach {
-        it.onWillCreateReactInstance(useDeveloperSupport)
-      }
-    }
   }
 
   //endregion

packages/expo/android/src/main/java/expo/modules/ReactNativeHostWrapperBase.kt
@@ -28,6 +28,10 @@ open class ReactNativeHostWrapperBase(
 
     val result = super.createReactInstanceManager()
 
+    reactNativeHostHandlers.forEach { handler ->
+      handler.onDidCreateDevSupportManager(result.devSupportManager)
+    }
+
     result.addReactInstanceEventListener(object : ReactInstanceEventListener {
       override fun onReactContextInitialized(context: ReactContext) {
         reactNativeHostHandlers.forEach { handler ->

packages/expo-updates/e2e/setup/project.ts
@@ -718,6 +718,7 @@ export async function initAsync(
       '-dontwarn javax.lang.model.element.Modifier',
       '-dontwarn org.checkerframework.checker.nullness.qual.EnsuresNonNullIf',
       '-dontwarn org.checkerframework.dataflow.qual.Pure',
+      '-keep class com.google.common.util.concurrent.ListenableFuture { *; }',
       '',
     ].join('\n'),
     'utf-8'