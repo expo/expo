4b1a69f2d3546f2de5431900c7057cfcc98e2d6a
Mark Lawlor <mwlawlor@gmail.com>
2024-04-15 12:26:13 +1000

    update context files

--- packages/expo-router/_ctx-shared.js
+++ packages/expo-router/_ctx-shared.js
@@ -1,5 +1,5 @@
 // Ignore root `./+html.js` and API route files `./generate+api.tsx`.
 module.exports = {
   EXPO_ROUTER_CTX_IGNORE:
-    /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+(html|native))))\.[tj]sx?$).*\.[tj]sx?$/,
+    /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+(html|native-intent))))\.[tj]sx?$).*\.[tj]sx?$/,
 };
--- packages/expo-router/_ctx.web.js
+++ packages/expo-router/_ctx.web.js
@@ -1,10 +1,6 @@
 export const ctx = require.context(
   process.env.EXPO_ROUTER_APP_ROOT,
   true,
-<<<<<<< HEAD
-  /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+html)))\.[tj]sx?$).*(?:\.android|\.ios|\.native)?\.[tj]sx?$/,
-=======
-  /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+(html|native))))\.[tj]sx?$).*\.[tj]sx?$/,
->>>>>>> 26e79911e22 ([router]: add +native file support)
+  /^(?:\.\/)(?!(?:(?:(?:.*\+api)|(?:\+(html|native-intent))))\.[tj]sx?$).*(?:\.android|\.ios|\.native)?\.[tj]sx?$/,
   process.env.EXPO_ROUTER_IMPORT_MODE
 );
