expo/expo

commit b98973745e8c4e748d8f64a91555b044e67ed878

[core][dev-client][updates] add bridgeless support for ExpoReactDelegate (#27601)
# Why

bridgeless mode has a totally different setup and breaks our
ExpoReactDelegate plugin system. this pr tries to add bridgeless mode
support.
expo-dev-client unfortunately has tightly coupled with RCTBridge and not
covered in this pr.
close ENG-11645

# How

- 0.74.0-rc.3 introduces the `RCTRootViewFactory`. this pr tries to
leverage it and introduces a derived `EXReactRootViewFactory`.
- drop < react-native 0.74 support because it has too much hassle.
- `RCTHost` cannot pass to swift because it contains c++ code. i think
for expo-dev-client and expo-updates, we don't actually need the
RCTBridge or RCTHost. so i revamped the `ExpoReactDelegateHandler` to
remove the bridge creation callback. that would align to
`RCTRootViewFactory` design.

---------

Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

packages/expo-modules-core/ios/ReactDelegates/EXReactDelegateWrapper.h
@@ -1,7 +1,5 @@
 // Copyright 2018-present 650 Industries. All rights reserved.
 
-#import <React/RCTBridge.h>
-#import <React/RCTRootView.h>
 #import <ExpoModulesCore/Platform.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -11,17 +9,9 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface EXReactDelegateWrapper : NSObject
 
-- (RCTBridge *)createBridgeWithDelegate:(id<RCTBridgeDelegate>)delegate
-                          launchOptions:(nullable NSDictionary *)launchOptions;
-
-- (RCTRootView *)createRootViewWithBridge:(RCTBridge *)bridge
-                               moduleName:(NSString *)moduleName
-                        initialProperties:(nullable NSDictionary *)initialProperties;
-
-- (RCTRootView *)createRootViewWithBridge:(RCTBridge *)bridge
-                               moduleName:(NSString *)moduleName
-                        initialProperties:(nullable NSDictionary *)initialProperties
-                            fabricEnabled:(BOOL)fabricEnabled;
+- (UIView *)createReactRootView:(NSString *)moduleName
+              initialProperties:(nullable NSDictionary *)initialProperties
+                  launchOptions:(nullable NSDictionary *)launchOptions;
 
 - (UIViewController *)createRootViewController;
 

packages/expo-modules-core/ios/ReactDelegates/EXReactDelegateWrapper.mm
@@ -1,0 +1,36 @@
+// Copyright 2018-present 650 Industries. All rights reserved.
+
+#import <ExpoModulesCore/EXReactDelegateWrapper.h>
+#import <ExpoModulesCore/EXReactDelegateWrapper+Private.h>
+
+@interface EXReactDelegateWrapper()
+
+@property (nonatomic, weak) EXReactDelegate *expoReactDelegate;
+
+@end
+
+@implementation EXReactDelegateWrapper
+
+- (instancetype)initWithExpoReactDelegate:(EXReactDelegate *)expoReactDelegate
+{
+  if (self = [super init]) {
+    _expoReactDelegate = expoReactDelegate;
+  }
+  return self;
+}
+
+- (UIView *)createReactRootView:(NSString *)moduleName
+              initialProperties:(nullable NSDictionary *)initialProperties
+                  launchOptions:(nullable NSDictionary *)launchOptions
+{
+  return [_expoReactDelegate createReactRootViewWithModuleName:moduleName
+                                             initialProperties:initialProperties
+                                                 launchOptions:launchOptions];
+}
+
+- (UIViewController *)createRootViewController
+{
+  return [_expoReactDelegate createRootViewController];
+}
+
+@end

packages/expo-modules-core/ios/ReactDelegates/EXReactRootViewFactory.mm
@@ -1,0 +1,83 @@
+// Copyright 2018-present 650 Industries. All rights reserved.
+
+#import <ExpoModulesCore/EXReactRootViewFactory.h>
+
+#import <ExpoModulesCore/EXReactDelegateWrapper+Private.h>
+#import <ReactCommon/RCTTurboModuleManager.h>
+
+#if __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>)
+#import <React-RCTAppDelegate/RCTAppDelegate.h>
+#elif __has_include(<React_RCTAppDelegate/RCTAppDelegate.h>)
+// for importing the header from framework, the dash will be transformed to underscore
+#import <React_RCTAppDelegate/RCTAppDelegate.h>
+#endif
+
+@interface RCTAppDelegate () <RCTTurboModuleManagerDelegate>
+
+@end
+
+@implementation EXReactRootViewFactory
+
+- (instancetype)initWithReactDelegate:(nullable EXReactDelegateWrapper *)reactDelegate
+                        configuration:(RCTRootViewFactoryConfiguration *)configuration
+           turboModuleManagerDelegate:(id<RCTTurboModuleManagerDelegate>)turboModuleManagerDelegate
+{
+  if (self = [super initWithConfiguration:configuration andTurboModuleManagerDelegate:turboModuleManagerDelegate]) {
+    self.reactDelegate = reactDelegate;
+  }
+  return self;
+}
+
+- (UIView *)viewWithModuleName:(NSString *)moduleName
+             initialProperties:(nullable NSDictionary *)initialProperties
+                 launchOptions:(nullable NSDictionary *)launchOptions
+{
+  if (self.reactDelegate != nil) {
+    return [self.reactDelegate createReactRootView:moduleName initialProperties:initialProperties launchOptions:launchOptions];
+  }
+  return [super viewWithModuleName:moduleName initialProperties:initialProperties launchOptions:launchOptions];
+}
+
++ (UIView *)createDefaultReactRootView:(nullable NSURL *)_bundleURL
+                            moduleName:(nullable NSString *)_moduleName
+                     initialProperties:(nullable NSDictionary *)_initialProperties
+                         launchOptions:(nullable NSDictionary *)launchOptions
+{
+  RCTAppDelegate *appDelegate = [self getRCTAppDelegate];
+  NSURL *bundleURL = _bundleURL ?: appDelegate.bundleURL;
+  NSString *moduleName = _moduleName ?: appDelegate.moduleName;
+  NSDictionary *initialProperties = _initialProperties ?: appDelegate.initialProps;
+  RCTRootViewFactoryConfiguration *configuration =
+  [[RCTRootViewFactoryConfiguration alloc] initWithBundleURL:bundleURL
+                                              newArchEnabled:appDelegate.fabricEnabled
+                                          turboModuleEnabled:appDelegate.turboModuleEnabled
+                                           bridgelessEnabled:appDelegate.bridgelessEnabled];
+
+  __weak RCTAppDelegate *weakDelegate = appDelegate;
+  configuration.createRootViewWithBridge = ^UIView *(RCTBridge *bridge, NSString *moduleName, NSDictionary *initProps)
+  {
+    return [weakDelegate createRootViewWithBridge:bridge moduleName:moduleName initProps:initProps];
+  };
+
+  configuration.createBridgeWithDelegate = ^RCTBridge *(id<RCTBridgeDelegate> delegate, NSDictionary *launchOptions)
+  {
+    return [weakDelegate createBridgeWithDelegate:delegate launchOptions:launchOptions];
+  };
+
+  RCTRootViewFactory *factory = [[RCTRootViewFactory alloc] initWithConfiguration:configuration andTurboModuleManagerDelegate:appDelegate];
+  return [factory viewWithModuleName:moduleName initialProperties:initialProperties launchOptions:launchOptions];
+}
+
++ (RCTAppDelegate *)getRCTAppDelegate
+{
+  UIApplication *application = UIApplication.sharedApplication;
+  id delegate = application.delegate;
+  if (![delegate isKindOfClass:RCTAppDelegate.class]) {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:@"The UIApplicationDelegate is a RCTAppDelegate."
+                                 userInfo:nil];
+  }
+  return (RCTAppDelegate *)delegate;
+}
+
+@end

packages/expo-modules-core/ios/ReactDelegates/ExpoReactDelegate.swift
@@ -12,25 +12,14 @@ public class ExpoReactDelegate: NSObject {
   }
 
   @objc
-  public func createBridge(delegate: RCTBridgeDelegate, launchOptions: [AnyHashable: Any]?) -> RCTBridge {
-    self.handlers.forEach { $0.bridgeWillCreate() }
-    let result = self.handlers.lazy
-      .compactMap { $0.createBridge(reactDelegate: self, bridgeDelegate: delegate, launchOptions: launchOptions) }
-      .first(where: { _ in true }) ?? RCTBridge(delegate: delegate, launchOptions: launchOptions)!
-    self.handlers.forEach { $0.bridgeDidCreate(bridge: result) }
-    return result
-  }
-
-  @objc
-  public func createRootView(
-    bridge: RCTBridge,
+  public func createReactRootView(
     moduleName: String,
     initialProperties: [AnyHashable: Any]?,
-    fabricEnabled: Bool = EXAppDefines.APP_NEW_ARCH_ENABLED
+    launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> UIView {
     return self.handlers.lazy
-      .compactMap { $0.createRootView(reactDelegate: self, bridge: bridge, moduleName: moduleName, initialProperties: initialProperties) }
-      .first(where: { _ in true }) ?? EXAppSetupDefaultRootView(bridge, moduleName, initialProperties, fabricEnabled)
+      .compactMap { $0.createReactRootView(reactDelegate: self, moduleName: moduleName, initialProperties: initialProperties, launchOptions: launchOptions) }
+      .first(where: { _ in true }) ?? ExpoReactRootViewFactory.createDefaultReactRootView(nil, moduleName: moduleName, initialProperties: initialProperties)
   }
 
   @objc

packages/expo-modules-core/ios/ReactDelegates/ExpoReactDelegateHandler.swift
@@ -1,7 +1,5 @@
 // Copyright 2018-present 650 Industries. All rights reserved.
 
-import React
-
 /**
  The handler for `ExpoReactDelegate`. A module can implement a handler to process react instance creation.
  */
@@ -10,20 +8,16 @@ open class ExpoReactDelegateHandler: NSObject {
   public override required init() {}
 
   /**
-   If this module wants to handle `RCTBridge` creation, it can return the instance.
+   If this module wants to handle React instance and the root view creation, it can return the instance.
    Otherwise return nil.
    */
   @objc
-  open func createBridge(reactDelegate: ExpoReactDelegate, bridgeDelegate: RCTBridgeDelegate, launchOptions: [AnyHashable: Any]?) -> RCTBridge? {
-    return nil
-  }
-
-  /**
-   If this module wants to handle `RCTRootView` creation, it can return the instance.
-   Otherwise return nil.
-   */
-  @objc
-  open func createRootView(reactDelegate: ExpoReactDelegate, bridge: RCTBridge, moduleName: String, initialProperties: [AnyHashable: Any]?) -> RCTRootView? {
+  open func createReactRootView(
+    reactDelegate: ExpoReactDelegate,
+    moduleName: String,
+    initialProperties: [AnyHashable: Any]?,
+    launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> UIView? {
     return nil
   }
 
@@ -35,18 +29,4 @@ open class ExpoReactDelegateHandler: NSObject {
   open func createRootViewController(reactDelegate: ExpoReactDelegate) -> UIViewController? {
     return nil
   }
-
-  // MARK: - event callbacks
-
-  /**
-   Callback before bridge creation
-   */
-  @objc
-  open func bridgeWillCreate() {}
-
-  /**
-   Callback after bridge creation
-   */
-  @objc
-  open func bridgeDidCreate(bridge: RCTBridge) {}
 }

packages/expo-updates/ios/EXUpdates/ReactDelegateHandler/ExpoUpdatesReactDelegateHandler.swift
@@ -10,10 +10,8 @@ import EXUpdatesInterface
  * RCTBridge and RCTRootView objects to return to the ReactDelegate, replacing them with the real
  * objects when expo-updates is ready.
  */
-public final class ExpoUpdatesReactDelegateHandler: ExpoReactDelegateHandler, AppControllerDelegate, RCTBridgeDelegate {
+public final class ExpoUpdatesReactDelegateHandler: ExpoReactDelegateHandler, AppControllerDelegate {
   private weak var reactDelegate: ExpoReactDelegate?
-  private var bridgeDelegate: RCTBridgeDelegate?
-  private var launchOptions: [AnyHashable: Any]?
   private var deferredRootView: EXDeferredRCTRootView?
   private var rootViewModuleName: String?
   private var rootViewInitialProperties: [AnyHashable: Any]?
@@ -44,7 +42,12 @@ public final class ExpoUpdatesReactDelegateHandler: ExpoReactDelegateHandler, Ap
     return true
   }()
 
-  public override func createBridge(reactDelegate: ExpoReactDelegate, bridgeDelegate: RCTBridgeDelegate, launchOptions: [AnyHashable: Any]?) -> RCTBridge? {
+  public override func createReactRootView(
+    reactDelegate: ExpoReactDelegate,
+    moduleName: String,
+    initialProperties: [AnyHashable: Any]?,
+    launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> UIView? {
     if EXAppDefines.APP_DEBUG && !UpdatesUtils.isNativeDebuggingEnabled() {
       // In development builds with expo-dev-client, completes the auto-setup for development
       // builds with the expo-updates integration by passing a reference to DevLauncherController
@@ -60,26 +63,11 @@ public final class ExpoUpdatesReactDelegateHandler: ExpoReactDelegateHandler, Ap
     AppController.initializeWithoutStarting()
     let controller = AppController.sharedInstance
     controller.delegate = self
-
-    // TODO: launch screen should move to expo-splash-screen
-    // or assuming expo-splash-screen KVO will make it works even we don't show it explicitly.
-    // controller.startAndShowLaunchScreen(UIApplication.shared.delegate!.window!!)
     controller.start()
-
-    self.bridgeDelegate = EXRCTBridgeDelegateInterceptor(bridgeDelegate: bridgeDelegate, interceptor: self)
-    self.launchOptions = launchOptions
-
-    return EXDeferredRCTBridge(delegate: self.bridgeDelegate!, launchOptions: self.launchOptions)
-  }
-
-  public override func createRootView(reactDelegate: ExpoReactDelegate, bridge: RCTBridge, moduleName: String, initialProperties: [AnyHashable: Any]?) -> RCTRootView? {
-    if !shouldEnableAutoSetup {
-      return nil
-    }
 
     self.rootViewModuleName = moduleName
     self.rootViewInitialProperties = initialProperties
-    self.deferredRootView = EXDeferredRCTRootView(bridge: bridge, moduleName: moduleName, initialProperties: initialProperties)
+    self.deferredRootView = EXDeferredRCTRootView()
     return self.deferredRootView
   }
 
@@ -90,9 +78,12 @@ public final class ExpoUpdatesReactDelegateHandler: ExpoReactDelegateHandler, Ap
       fatalError("`reactDelegate` should not be nil")
     }
 
-    let rootView = EXCreateReactBindingRootView(self.bridgeDelegate, self.rootViewInitialProperties, self.launchOptions)
+    let rootView = ExpoReactRootViewFactory.createDefaultReactRootView(
+      AppController.sharedInstance.launchAssetUrl(),
+      moduleName: self.rootViewModuleName,
+      initialProperties: self.rootViewInitialProperties)
     rootView.backgroundColor = self.deferredRootView?.backgroundColor ?? UIColor.white
-    let window = UIApplication.shared.delegate!.window!!
+    let window = getWindow()
     let rootViewController = reactDelegate.createRootViewController()
     rootViewController.view = rootView
     window.rootViewController = rootViewController
@@ -101,23 +92,22 @@ public final class ExpoUpdatesReactDelegateHandler: ExpoReactDelegateHandler, Ap
     self.cleanup()
   }
 
-  // MARK: RCTBridgeDelegate implementations
-
-  public func sourceURL(for bridge: RCTBridge!) -> URL! {
-    return AppController.sharedInstance.launchAssetUrl()
-  }
-
   // MARK: Internals
 
   /**
-   Cleanup unused resources after `RCTBridge` created.
-   We should keep `bridgeDelegate` alive because it's a wrapper of `RCTBridgeDelegate` from `AppDelegate` and somehow bridge may access it after.
+   Cleanup unused resources after react instance created.
    */
   private func cleanup() {
     self.reactDelegate = nil
-    self.launchOptions = nil
     self.deferredRootView = nil
     self.rootViewModuleName = nil
     self.rootViewInitialProperties = nil
+  }
+
+  private func getWindow() -> UIWindow {
+    guard let window = UIApplication.shared.windows.filter(\.isKeyWindow).first ?? UIApplication.shared.delegate?.window as? UIWindow else {
+      fatalError("Cannot find the current window.")
+    }
+    return window
   }
 }

packages/expo-dev-launcher/ios/EXDevLauncherController.m
@@ -8,6 +8,7 @@
 #import <React/RCTConstants.h>
 #import <React/RCTKeyCommands.h>
 
+#import <ExpoModulesCore/EXReactRootViewFactory.h>
 #import <EXDevLauncher/EXDevLauncherController.h>
 #import <EXDevLauncher/EXDevLauncherRCTBridge.h>
 #import <EXDevLauncher/EXDevLauncherManifestParser.h>
@@ -200,7 +201,6 @@
   return [[NSBundle bundleWithURL:bundleURL] URLForResource:@"main" withExtension:@"jsbundle"];
 }
 
-
 - (void)clearRecentlyOpenedApps
 {
   return [_recentlyOpenedAppsRegistry clearRegistry];
@@ -328,7 +328,7 @@
   }
 
   [self _removeInitModuleObserver];
-  UIView *rootView = [_bridgeDelegate createRootViewWithModuleName:@"main" launchOptions:_launchOptions application:UIApplication.sharedApplication];
+  UIView *rootView = [EXReactRootViewFactory createDefaultReactRootView:[self getSourceURL] moduleName:nil initialProperties:nil launchOptions:_launchOptions];
   _launcherBridge = _bridgeDelegate.bridge;
 
   [[NSNotificationCenter defaultCenter] addObserver:self

packages/expo-dev-launcher/ios/ReactDelegateHandler/EXDevLauncherDeferredRCTBridge.h
@@ -1,14 +1,0 @@
-// Copyright 2018-present 650 Industries. All rights reserved.
-
-#import <React/RCTBridge.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-/**
- EXDevLauncherDeferredRCTBridge is a special no-op class for expo-dev-launcher to defer bridge creation until `EXDevLauncherController` finishing setup.
- */
-@interface EXDevLauncherDeferredRCTBridge : RCTBridge
-
-@end
-
-NS_ASSUME_NONNULL_END

commit 3e1af46cdc519965581fbee79806cf609f66853e

[core][updates] fix Updates.reloadAsync not working on ios (#28256)
# Why

Updates.reloadAsync() does not use the latest bundleURL on ios. it's a
regression from #27601
close ENG-11975

# How

before #27601, the `RCTBridgeDelegate.sourceURLForBridge:` is inside expo-updates.
it's no longer the case since #27601 and we were using the cached
bundleURL inside RCTRootViewFactory. that's why reload still using the
previous bundleURL.

this pr tries to introduce a new `bundleURL` handler to react delegate
handlers to override. then expo-updates could serve the newer bundleURL.

packages/expo-modules-core/ios/ReactDelegates/EXReactDelegateWrapper.h
@@ -13,6 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
               initialProperties:(nullable NSDictionary *)initialProperties
                   launchOptions:(nullable NSDictionary *)launchOptions;
 
+- (NSURL *)bundleURL;
+
 - (UIViewController *)createRootViewController;
 
 @end

packages/expo-modules-core/ios/ReactDelegates/EXReactDelegateWrapper.mm
@@ -28,6 +28,11 @@
                                                  launchOptions:launchOptions];
 }
 
+- (NSURL *)bundleURL
+{
+  return [_expoReactDelegate bundleURL];
+}
+
 - (UIViewController *)createRootViewController
 {
   return [_expoReactDelegate createRootViewController];

packages/expo-modules-core/ios/ReactDelegates/EXReactRootViewFactory.mm
@@ -11,6 +11,12 @@
 #import <React_RCTAppDelegate/RCTAppDelegate.h>
 #endif
 
+@interface RCTRootViewFactory ()
+
+- (NSURL *)bundleURL;
+
+@end
+
 @implementation EXReactRootViewFactory
 
 - (instancetype)initWithReactDelegate:(nullable EXReactDelegateWrapper *)reactDelegate
@@ -38,6 +44,11 @@
                       launchOptions:(NSDictionary *)launchOptions
 {
   return [super viewWithModuleName:moduleName initialProperties:initialProperties launchOptions:launchOptions];
+}
+
+- (NSURL *)bundleURL
+{
+  return [self.reactDelegate bundleURL] ?: [super bundleURL];
 }
 
 @end

packages/expo-modules-core/ios/ReactDelegates/ExpoReactDelegate.swift
@@ -34,6 +34,13 @@ public class ExpoReactDelegate: NSObject {
   }
 
   @objc
+  public func bundleURL() -> URL? {
+    return self.handlers.lazy
+      .compactMap { $0.bundleURL(reactDelegate: self) }
+      .first(where: { _ in true })
+  }
+
+  @objc
   public func createRootViewController() -> UIViewController {
     return self.handlers.lazy
       .compactMap { $0.createRootViewController(reactDelegate: self) }

packages/expo-modules-core/ios/ReactDelegates/ExpoReactDelegateHandler.swift
@@ -22,6 +22,15 @@ open class ExpoReactDelegateHandler: NSObject {
   }
 
   /**
+   Clients could override this getter to serve the latest bundleURL for React instance.
+   For example, expo-updates uses this to serve the newer bundleURL from `Updates.reloadAsync()`.
+   */
+  @objc
+  open func bundleURL(reactDelegate: ExpoReactDelegate) -> URL? {
+    return nil
+  }
+
+  /**
    If this module wants to handle `UIViewController` creation for `RCTRootView`, it can return the instance.
    Otherwise return nil.
    */

packages/expo-updates/ios/EXUpdates/ReactDelegateHandler/ExpoUpdatesReactDelegateHandler.swift
@@ -39,6 +39,10 @@ public final class ExpoUpdatesReactDelegateHandler: ExpoReactDelegateHandler, Ap
     return self.deferredRootView
   }
 
+  public override func bundleURL(reactDelegate: ExpoReactDelegate) -> URL? {
+    AppController.sharedInstance.launchAssetUrl()
+  }
+
   // MARK: AppControllerDelegate implementations
 
   public func appController(_ appController: AppControllerInterface, didStartWithSuccess success: Bool) {