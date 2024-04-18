a47e793d0ff269549daccbf34923116415a3fbdf
Jon Samp <sampjon@gmail.com>
2024-04-18 00:24:48 -0500

    Adds expo template default

---
+++ templates/expo-template-default/components/ThemedText.tsx
@@ -0,0 +1,60 @@
+import { Text, StyleSheet } from "react-native";
+
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+export type TextProps = {
+  lightColor?: string;
+  darkColor?: string;
+} & { type?: "default" | "title" | "defaultSemiBold" | "subtitle" | "link" } & Text["props"];
+
+export function ThemedText(props: TextProps) {
+  const {
+    style,
+    lightColor,
+    darkColor,
+    type = "default",
+    ...otherProps
+  } = props;
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+
+  return (
+    <Text
+      style={[
+        { color },
+        type === "default" ? styles.default : undefined,
+        type === "title" ? styles.title : undefined,
+        type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
+        type === "subtitle" ? styles.subtitle : undefined,
+        type === "link" ? styles.link : undefined,
+        style,
+      ]}
+      {...otherProps}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  default: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  defaultSemiBold: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: "600",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    lineHeight: 32,
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  link: {
+    lineHeight: 30,
+    fontSize: 16,
+    color: "#2e78b7",
+  },
+});
