93970c02227013f8554bbb7f808faf34d80f0b40
Pietro Tamburini <pietro.tamburini.3@gmail.com>
2024-04-18 15:07:51 +0200

    Update expo-template-bare-minimum MainApplication.kt

--- templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.kt
+++ templates/expo-template-bare-minimum/android/app/src/main/java/com/helloworld/MainApplication.kt
@@ -20,11 +20,11 @@ class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
-            return PackageList(this).packages
-          }
+          override fun getPackages(): List<ReactPackage> =
+              PackageList(this).packages.apply {
+                // Packages that cannot be autolinked yet can be added manually here, for example:
+                // add(MyReactNativePackage())
+              }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
