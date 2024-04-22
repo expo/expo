e3155cd4513cd12cddfd8ac3f6db252eac8b5e43
Brent Vatne <brentvatne@gmail.com>
2024-04-22 14:53:30 -0700

    [packages] Commit build file changes

--- packages/expo-asset/build/AssetSourceResolver.native.d.ts
+++ packages/expo-asset/build/AssetSourceResolver.native.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="../src/ts-declarations/react-native-assets.d.ts" />
 import AssetSourceResolver from 'react-native/Libraries/Image/AssetSourceResolver';
 export default AssetSourceResolver;
 export * from 'react-native/Libraries/Image/AssetSourceResolver';
--- packages/expo-asset/build/resolveAssetSource.native.d.ts
+++ packages/expo-asset/build/resolveAssetSource.native.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="../src/ts-declarations/react-native-assets.d.ts" />
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 export default resolveAssetSource;
 export * from 'react-native/Libraries/Image/resolveAssetSource';
