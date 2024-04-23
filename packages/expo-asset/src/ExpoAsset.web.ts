2ce9614684cd8a7f6eb2fd4ed09a400e165a7e36
James Elias Sigurdarson <jamiees2@gmail.com>
2024-04-23 23:46:51 +0000

    lint

--- packages/expo-asset/src/ExpoAsset.web.ts
+++ packages/expo-asset/src/ExpoAsset.web.ts
@@ -1,3 +1,7 @@
-export async function downloadAsync(url: string, _hash: string | null, _type: string): Promise<string> {
+export async function downloadAsync(
+  url: string,
+  _hash: string | null,
+  _type: string
+): Promise<string> {
   return url;
 }
