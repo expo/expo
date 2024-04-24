868273ebdb141e8f9129432a8ad7af9b416aaf93
Brent Vatne <brentvatne@gmail.com>
2024-04-23 18:32:06 -0700

    😮‍💨

--- packages/expo-font/ios/UIFont+FontFamilyAlias.swift
+++ packages/expo-font/ios/UIFont+FontFamilyAlias.swift
@@ -15,9 +15,8 @@ public extension UIFont {
     }
     return fontNames
   }
- 
   @objc
-  static dynamic func _expo_init(name fontName: String, size fontSize: CGFloat) -> Optional<UIFont> {
+  static dynamic func _expo_init(name fontName: String, size fontSize: CGFloat) -> UIFont? {
     let font = UIFont._expo_init(name: fontName, size: fontSize)
 
     if let aliasedFamilyName = FontFamilyAliasManager.familyName(forAlias: fontName) {
