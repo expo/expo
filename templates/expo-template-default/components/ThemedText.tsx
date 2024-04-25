8f01e60e5d2dba4833e9eb7953ce7d13eda7dac5
Brent Vatne <brentvatne@gmail.com>
2024-04-25 13:12:15 -0700

    [templates] Run prettier

--- templates/expo-template-default/components/ThemedText.tsx
+++ templates/expo-template-default/components/ThemedText.tsx
@@ -1,31 +1,25 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from 'react-native';
 
-import { useThemeColor } from "@/hooks/useThemeColor";
+import { useThemeColor } from '@/hooks/useThemeColor';
 
 export type TextProps = {
   lightColor?: string;
   darkColor?: string;
-} & { type?: "default" | "title" | "defaultSemiBold" | "subtitle" | "link" } & Text["props"];
+} & { type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link' } & Text['props'];
 
 export function ThemedText(props: TextProps) {
-  const {
-    style,
-    lightColor,
-    darkColor,
-    type = "default",
-    ...otherProps
-  } = props;
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+  const { style, lightColor, darkColor, type = 'default', ...otherProps } = props;
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return (
     <Text
       style={[
         { color },
-        type === "default" ? styles.default : undefined,
-        type === "title" ? styles.title : undefined,
-        type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
-        type === "subtitle" ? styles.subtitle : undefined,
-        type === "link" ? styles.link : undefined,
+        type === 'default' ? styles.default : undefined,
+        type === 'title' ? styles.title : undefined,
+        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
+        type === 'subtitle' ? styles.subtitle : undefined,
+        type === 'link' ? styles.link : undefined,
         style,
       ]}
       {...otherProps}
@@ -41,20 +35,20 @@ const styles = StyleSheet.create({
   defaultSemiBold: {
     fontSize: 16,
     lineHeight: 24,
-    fontWeight: "600",
+    fontWeight: '600',
   },
   title: {
     fontSize: 32,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     lineHeight: 32,
   },
   subtitle: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   link: {
     lineHeight: 30,
     fontSize: 16,
-    color: "#2e78b7",
+    color: '#2e78b7',
   },
 });
--- templates/expo-template-default/hooks/useThemeColor.ts
+++ templates/expo-template-default/hooks/useThemeColor.ts
@@ -3,14 +3,14 @@
  * https://docs.expo.io/guides/color-schemes/
  */
 
-import { useColorScheme } from "react-native";
-import { Colors } from "@/constants/Colors";
+import { useColorScheme } from 'react-native';
+import { Colors } from '@/constants/Colors';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark,
+  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? "light";
+  const theme = useColorScheme() ?? 'light';
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
