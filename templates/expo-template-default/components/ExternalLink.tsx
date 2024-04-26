fc2adb1946001d44b21d257fc39de02a4001b40d
Bartosz Kaszubowski <gosimek@gmail.com>
2024-04-26 13:49:54 +0200

    [template-default] backport missed changes after code move (#28473)

--- templates/expo-template-default/components/ExternalLink.tsx
+++ templates/expo-template-default/components/ExternalLink.tsx
@@ -1,23 +1,22 @@
 import { Link } from 'expo-router';
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import { openBrowserAsync } from 'expo-web-browser';
+import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
 
-export function ExternalLink(
-  props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string }
-) {
+type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
+
+export function ExternalLink({ href, ...rest }: Props) {
   return (
     <Link
       target="_blank"
-      {...props}
-      // @ts-expect-error: External URLs are not typed.
-      href={props.href}
-      onPress={(e) => {
+      {...rest}
+      href={href}
+      onPress={async (event) => {
         if (Platform.OS !== 'web') {
           // Prevent the default behavior of linking to the default browser on native.
-          e.preventDefault();
+          event.preventDefault();
           // Open the link in an in-app browser.
-          WebBrowser.openBrowserAsync(props.href as string);
+          await openBrowserAsync(href);
         }
       }}
     />
