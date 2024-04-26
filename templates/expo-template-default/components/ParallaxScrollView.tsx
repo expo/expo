b48bac079492a24b7e5c8aa267d8e45c645b27ec
Bartosz Kaszubowski <gosimek@gmail.com>
2024-04-26 13:49:54 +0200

    [template-default] backport missed changes after code move (#28473)

--- templates/expo-template-default/components/ParallaxScrollView.tsx
+++ templates/expo-template-default/components/ParallaxScrollView.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import type { PropsWithChildren, ReactElement } from 'react';
 import { StyleSheet, useColorScheme } from 'react-native';
 import Animated, {
   interpolate,
@@ -11,15 +11,17 @@ import { ThemedView } from '@/components/ThemedView';
 
 const HEADER_HEIGHT = 250;
 
+type Props = PropsWithChildren<{
+  headerImage: ReactElement;
+  headerBackgroundColor: { dark: string; light: string };
+}>;
+
 export default function ParallaxScrollView({
   children,
   headerImage,
   headerBackgroundColor,
-}: PropsWithChildren<{
-  headerImage: React.ReactElement;
-  headerBackgroundColor: { dark: string; light: string };
-}>) {
-  const colorScheme = useColorScheme() || 'light';
+}: Props) {
+  const colorScheme = useColorScheme() ?? 'light';
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
 
@@ -68,9 +70,7 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     padding: 32,
-    gap: 24,
-    borderRadius: 16,
+    gap: 16,
     overflow: 'hidden',
-    marginTop: -12,
   },
 });
