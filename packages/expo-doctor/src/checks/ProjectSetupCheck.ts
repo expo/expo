d02e54d68fbae2fabc59f53d2aadade5bec73776
Keith Kurak <keith.kurak@gmail.com>
2024-04-26 10:21:33 -0400

    fix naming and comments

--- packages/expo-doctor/src/checks/ProjectSetupCheck.ts
+++ packages/expo-doctor/src/checks/ProjectSetupCheck.ts
@@ -16,13 +16,17 @@ export class ProjectSetupCheck implements DoctorCheck {
     // ** check that expo modules native projects aren't getting gitignored **
 
     if (fs.existsSync(path.join(projectRoot, 'modules'))) {
+      // Glob returns matching files and `git check-ignore` checks files, as well, but we want to check if the path is gitignored,
+      // so we pick vital files to match off of (e.g., .podspec, build.gradle).
       const keyFilePathsForModules = [
         path.join(projectRoot, 'modules', '**', 'ios', '*.podspec'),
         path.join(projectRoot, 'modules', '**', 'android', 'build.gradle'),
       ];
 
       if (
-        (await Promise.all(keyFilePathsForModules.map(isPathIgnoredAsync))).find((result) => result)
+        (await Promise.all(keyFilePathsForModules.map(areAnyMatchingPathsIgnoredAsync))).find(
+          (result) => result
+        )
       ) {
         issues.push(
           'This project contains local Expo modules, but the android/ios folders inside the modules are gitignored. These files are required to build your native module into your app. Use patterns like "/android" and "/ios" in your .gitignore file to exclude only the top-level android and ios folders.'
@@ -89,11 +93,7 @@ async function getRootPathAsync(): Promise<string> {
   return (await spawnAsync('git', ['rev-parse', '--show-toplevel'])).stdout.trim();
 }
 
-/**
- * Glob returns matching files and `git check-ignore` checks files, as well, but we want to check if the path is gitignored,
- * so we pick vital files to match off of (e.g., .podspec, build.gradle).
- */
-async function isPathIgnoredAsync(filePath: string): Promise<boolean> {
+async function areAnyMatchingPathsIgnoredAsync(filePath: string): Promise<boolean> {
   const matchingNativeFiles = await glob(filePath);
   if (!matchingNativeFiles.length) return false;
   // multiple matches may occur if there are multiple modules
