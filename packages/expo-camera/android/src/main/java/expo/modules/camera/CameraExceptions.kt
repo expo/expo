0a8c904e1c2a22ca941b5512a47c157e8386bd8b
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2024-04-27 10:28:58 +0100

    [camera] Support scanning barcode from URL (#28445)

--- packages/expo-camera/android/src/main/java/expo/modules/camera/CameraExceptions.kt
+++ packages/expo-camera/android/src/main/java/expo/modules/camera/CameraExceptions.kt
@@ -6,4 +6,7 @@ class CameraExceptions {
   class ImageCaptureFailed : CodedException(message = "Failed to capture image")
 
   class VideoRecordingFailed(cause: String?) : CodedException("Video recording failed: $cause")
+
+  class ImageRetrievalException(url: String) :
+    CodedException("Could not get the image from given url: '$url'")
 }
