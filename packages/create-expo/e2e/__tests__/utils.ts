3d8058e1dcc7e93760700e35cf6005a0e0486b80
Cedric van Putten <github@cedric.dev>
2024-04-29 16:01:16 +0200

    fix(create-expo): remove nested `it` statements

--- packages/create-expo/e2e/__tests__/utils.ts
+++ packages/create-expo/e2e/__tests__/utils.ts
@@ -51,30 +51,18 @@ export function forcePackageManagerEnv(packageManager: string) {
 
 /** Expect the received spawn result to be ok or "passing" */
 export function expectExecutePassing(spawn: SpawnResult) {
-  // NOTE(cedric): this is wrapped in an `it` statement to add a custom message
-  it(`Spawn result should be ok, received status: ${spawn.status}`, () => {
-    expect(spawn.status).toBe(0);
-  });
-
+  expect(spawn.status).toBe(0);
   return spawn;
 }
 
 /** Expect the file to exist, using proper failure message when it does not */
 export function expectFileExists(projectName: string, ...filePath: string[]) {
   const targetPath = getTestPath(projectName, ...filePath);
-
-  // NOTE(cedric): this is wrapped in an `it` statement to add a custom message
-  it(`File path should exist: ${targetPath}`, () => {
-    expect(fs.existsSync(targetPath)).toBe(true);
-  });
+  expect(fs.existsSync(targetPath)).toBe(true);
 }
 
 /** Expect the file to not-exist, using proper failure message when it does not */
 export function expectFileNotExists(projectName: string, ...filePath: string[]) {
   const targetPath = getTestPath(projectName, ...filePath);
-
-  // NOTE(cedric): this is wrapped in an `it` statement to add a custom message
-  it(`File path should not exist: ${targetPath}`, () => {
-    expect(fs.existsSync(targetPath)).toBe(false);
-  });
+  expect(fs.existsSync(targetPath)).toBe(false);
 }
