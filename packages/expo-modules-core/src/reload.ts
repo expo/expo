ccfd14246a37b76c9c09cfee72d2aa844d38f8c1
Kudo Chien <kudo@expo.dev>
2024-05-02 10:19:48 +0800

    add comment to describe the difference between Updates.reloadAsync

--- packages/expo-modules-core/src/reload.ts
+++ packages/expo-modules-core/src/reload.ts
@@ -1,5 +1,11 @@
 /**
  * Reloads the app.
+ *
+ * This function should work for both release and debug builds.
+ *
+ * Unlike the [`Updates.reloadAsync()`](https://docs.expo.dev/versions/latest/sdk/updates/#updatesreloadasync),
+ * this function does not change the loading bundle. It only reloads with the same JavaScript bundle.
+ *
  * @param reason The reason for reloading the app. This is used only for some platforms.
  */
 export async function reloadAppAsync(reason: string = 'Reloaded from JS call'): Promise<void> {
