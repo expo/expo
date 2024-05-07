d002a1f54737cf04cfbedb289c140c3d6317adbd
Evan Bacon <bacon@expo.io>
2024-05-07 11:30:08 -0500

    add basic e2e tests for Metro web Fast Refresh (#28237)
    # Why
    
    - It's important we keep Metro web working continuously. The behavior of
    Fast Refresh is a bit of a black box, so having a test gives us a
    clearer list of expected steps that must work.
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    - I've added Playwright to the CLI and an additional script for running
    playwright tests.
    - The CI job is split out of the E2E CLI job so we can get faster
    failures. Pinging @byCedric for advice on the structure and how we could
    potentially reuse work between these jobs.
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    - Running `yarn test:playwright` in `packages/@expo/cli` works as
    expected locally.
    - CI should run the playwright test continuously. We'll also be keeping
    an eye on the timings to ensure this test runs fast and doesn't become
    another burden on our merge process.
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/@expo/cli/src/run/startBundler.ts
+++ packages/@expo/cli/src/run/startBundler.ts
@@ -5,6 +5,7 @@ import * as Log from '../log';
 import { startInterfaceAsync } from '../start/interface/startInterface';
 import { BundlerStartOptions } from '../start/server/BundlerDevServer';
 import { DevServerManager } from '../start/server/DevServerManager';
+import { env } from '../utils/env';
 import { isInteractive } from '../utils/interactive';
 
 export async function startBundlerAsync(
@@ -54,7 +55,12 @@ export async function startBundlerAsync(
   } else {
     // Display the server location in CI...
     const url = devServerManager.getDefaultDevServer()?.getDevServerUrl();
+
     if (url) {
+      if (env.__EXPO_E2E_TEST) {
+        // Print the URL to stdout for tests
+        console.info(`[__EXPO_E2E_TEST:server] ${JSON.stringify({ url })}`);
+      }
       Log.log(chalk`Waiting on {underline ${url}}`);
     }
   }
