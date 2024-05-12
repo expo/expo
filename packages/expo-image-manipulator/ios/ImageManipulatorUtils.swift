7435c685211a75409f967cbd97cbbb86703c6cfc
Alan Hughes <alan@expo.io>
2024-05-12 13:31:25 +0100

    fix typo, add comment [skip ci]

--- packages/expo-image-manipulator/ios/ImageManipulatorUtils.swift
+++ packages/expo-image-manipulator/ios/ImageManipulatorUtils.swift
@@ -16,11 +16,11 @@ func imageData(from image: UIImage, format: ImageFormat, compression: Double) ->
 }
 
 /**
- Checks is we are dealing with a ph asset URL and uses the correct method to fetch it.
+ Checks if we are dealing with a ph asset URL and uses the correct method to fetch it.
  */
 func retreiveAsset(from url: URL) -> PHAsset? {
   if url.scheme == "ph" {
-    let identifier = String(url.absoluteString.dropFirst(5))
+    let identifier = String(url.absoluteString.dropFirst(5)) // removes ph://
     return PHAsset.fetchAssets(withLocalIdentifiers: [identifier], options: nil).firstObject
   }
   return PHAsset.fetchAssets(withALAssetURLs: [url], options: nil).firstObject
