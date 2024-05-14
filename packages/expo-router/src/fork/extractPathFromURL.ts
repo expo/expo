b3f2f818fbf0d901d86d254d36674e7169c3b261
Mark Lawlor <mwlawlor@gmail.com>
2024-05-15 09:24:29 +1000

    [router] fix deep links from expo.dev QR codes (#28881)
    # Why
    
    PR https://github.com/expo/expo/pull/28376 changed the Expo Router deep
    linking to a more generic solution. What it didn't account for was
    deeplinking to custom development clients using an `https:` update
    route.
    
    Example problem URL
    `exp+custom-scheme://expo-development-client/?url=https://u.expo.dev/{uuid}/group/{uuid}`
    
    Diff that caused issue
    https://github.com/expo/expo/pull/28376/files#diff-9d5c1297e56e932ab981af95f6ec5a51ff51b9a103e2123c11b37471001c936fL95-R110
    
    # How
    
    Previous logic
    
    - Is the URL for a `expo-development-client` 
      - Loop again with using the `url` search param as the URL
      - Is the URL a `https` url 
        - return the path
    
    New Logic: 
    
    - Is the URL for a `expo-development-client` 
      - loop again with using the `url` search param as the URL
      - Is the URL a `https` url and is the hostname an Expo host?
        - Yes: Perform the old logic, stripping the path segments
        - No: Return the pathname
    
    # Test Plan
    
    Added URL to test suite
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-router/src/fork/extractPathFromURL.ts
+++ packages/expo-router/src/fork/extractPathFromURL.ts
@@ -26,7 +26,20 @@ function extractExactPathFromURL(url: string): string {
     // from the URL, while stripping the origin.
     url.match(/^https?:\/\//)
   ) {
-    const { origin, href } = new URL(url);
+    const { origin, href, hostname, pathname } = new URL(url);
+
+    if (hostname === 'exp.host' || hostname === 'u.expo.dev') {
+      // drop the first two segments from pathname:
+      const segments = pathname.split('/');
+
+      if (segments[2] === 'group') {
+        // Is this u.expo.dev/{uuid}/group/{uuid}
+        return segments.slice(4).join('/');
+      } else {
+        return segments.slice(2).join('/');
+      }
+    }
+
     return href.replace(origin, '');
   }
 
