43a0f628e30e3e0a894698a3701d792f7b7706bc
Simek <gosimek@gmail.com>
2024-05-17 12:05:57 +0200

    [docs] introduce Expo package page, replace register-root-component

--- packages/expo/src/errors/ExpoErrorManager.ts
+++ packages/expo/src/errors/ExpoErrorManager.ts
@@ -2,4 +2,8 @@ export function createErrorHandler(originalHandler) {
   return (error, isFatal) => originalHandler(error, isFatal);
 }
 
+/**
+ * @hidden
+ * @deprecated Remove in the future.
+ */
 export function disableErrorHandling() {}
