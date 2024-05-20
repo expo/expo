abd2f9ef3d67e89be7af82ae5752adef51530584
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2024-05-20 13:41:08 +0200

    Re-export types from expo-modules-core in expo package

--- packages/expo/build/Expo.d.ts
+++ packages/expo/build/Expo.d.ts
@@ -2,5 +2,5 @@ import './Expo.fx';
 export { disableErrorHandling } from './errors/ExpoErrorManager';
 export { default as registerRootComponent } from './launch/registerRootComponent';
 export { isRunningInExpoGo, getExpoGoProjectConfig } from './environment/ExpoGo';
-export { requireNativeModule, requireOptionalNativeModule, reloadAppAsync, } from 'expo-modules-core';
+export { EventEmitter, SharedObject, NativeModule, requireNativeModule, requireOptionalNativeModule, reloadAppAsync, } from 'expo-modules-core';
 //# sourceMappingURL=Expo.d.ts.map
