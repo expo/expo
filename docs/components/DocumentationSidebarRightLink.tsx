5f211c1fbbe1ae79ef3eea740b0af70a430abb83
Simek <gosimek@gmail.com>
2024-05-22 16:13:06 +0200

    fix methods with rest param ToC entries

--- docs/components/DocumentationSidebarRightLink.tsx
+++ docs/components/DocumentationSidebarRightLink.tsx
@@ -82,8 +82,10 @@ const DocumentationSidebarRightLink = forwardRef<HTMLAnchorElement, SidebarLinkP
  * Replaces `Module.someFunction(arguments: argType)` with `someFunction()`
  */
 const trimCodedTitle = (str: string) => {
-  const dotIdx = str.indexOf('.');
-  if (dotIdx > 0) str = str.substring(dotIdx + 1);
+  if (!str.includes('...')) {
+    const dotIdx = str.indexOf('.');
+    if (dotIdx > 0) str = str.substring(dotIdx + 1);
+  }
 
   const parIdx = str.indexOf('(');
   if (parIdx > 0) str = str.substring(0, parIdx + 1) + ')';
