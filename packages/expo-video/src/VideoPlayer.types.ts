41cb439c5d322df1629b2b84cb817ad3f5bbf96b
Bartosz Kaszubowski <gosimek@gmail.com>
2024-05-28 09:31:33 +0200

    [modules-core][video] improve typing, regenerate docs data (#29112)

--- packages/expo-video/src/VideoPlayer.types.ts
+++ packages/expo-video/src/VideoPlayer.types.ts
@@ -232,7 +232,7 @@ export type DRMOptions = {
   /**
    * Determines headers sent to the license server on license requests.
    */
-  headers?: { [key: string]: string };
+  headers?: Record<string, string>;
 
   /**
    * Specifies whether the DRM is a multi-key DRM.
