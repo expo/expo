0bbe61f36f3b522f67faeba7333a9b4bab506e95
evanbacon <bacon@expo.io>
2024-06-03 18:53:03 +0200

    automate

--- packages/expo-linear-gradient/src/__tests__/__rsc__/react-native.test.web.tsx
+++ packages/expo-linear-gradient/src/__tests__/__rsc__/react-native.test.web.tsx
@@ -1,17 +1,27 @@
-import { SafeAreaView, TouchableNativeFeedback } from 'react-native';
+import { SafeAreaView, TouchableNativeFeedback, Switch, Pressable } from 'react-native';
 
-it(`renders SafeAreaView to RSC`, async () => {
-  const jsx = <SafeAreaView />;
+Object.entries({ SafeAreaView, TouchableNativeFeedback, Switch, Pressable }).forEach(
+  ([name, Component]) => {
+    it(`renders ${name} to RSC`, async () => {
+      const jsx = <Component />;
 
-  await expect(jsx)
-    .toMatchFlight(`2:I[\"../../node_modules/react-native-web/dist/exports/View/index.js\",[],\"\"]
-1:{\"name\":\"SafeAreaView\",\"env\":\"Server\",\"owner\":null}
-0:D\"$1\"
-0:[\"$\",\"$L2\",null,{\"ref\":\"$undefined\",\"style\":[{\"paddingTop\":\"env(safe-area-inset-top)\",\"paddingRight\":\"env(safe-area-inset-right)\",\"paddingBottom\":\"env(safe-area-inset-bottom)\",\"paddingLeft\":\"env(safe-area-inset-left)\"},\"$undefined\"]},\"$1\"]`);
-});
+      await expect(jsx).toMatchFlightSnapshot();
+    });
+  }
+);
 
-it(`renders TouchableNativeFeedback to RSC`, async () => {
-  const jsx = <TouchableNativeFeedback />;
+// it(`renders SafeAreaView to RSC`, async () => {
+//   const jsx = <SafeAreaView />;
 
-  await expect(jsx).toMatchFlightSnapshot();
-});
+//   await expect(jsx)
+//     .toMatchFlight(`2:I[\"../../node_modules/react-native-web/dist/exports/View/index.js\",[],\"\"]
+// 1:{\"name\":\"SafeAreaView\",\"env\":\"Server\",\"owner\":null}
+// 0:D\"$1\"
+// 0:[\"$\",\"$L2\",null,{\"ref\":\"$undefined\",\"style\":[{\"paddingTop\":\"env(safe-area-inset-top)\",\"paddingRight\":\"env(safe-area-inset-right)\",\"paddingBottom\":\"env(safe-area-inset-bottom)\",\"paddingLeft\":\"env(safe-area-inset-left)\"},\"$undefined\"]},\"$1\"]`);
+// });
+
+// it(`renders TouchableNativeFeedback to RSC`, async () => {
+//   const jsx = <TouchableNativeFeedback />;
+
+//   await expect(jsx).toMatchFlightSnapshot();
+// });
