88a25cce1cb5bfd0c92ab65f4c7a9642ab3c836e
Mykola Skorenkyi <mykola.skorenkyi@avenga.com>
2024-06-03 15:14:06 +0300

    fix import path to "ExpoConfig"

--- packages/babel-preset-expo/build/expo-inline-manifest-plugin.js
+++ packages/babel-preset-expo/build/expo-inline-manifest-plugin.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.expoInlineManifestPlugin = void 0;
-const config_1 = require("expo/config");
+const config_1 = require("@expo/config");
 const common_1 = require("./common");
 const debug = require('debug')('expo:babel:inline-manifest');
 // Convert expo value to PWA value
