f4a56b1c30f80ac2404dc8b0c1d3be6a1b152153
evanbacon <bacon@expo.io>
2024-06-03 19:01:37 +0200

    add more RNW tests

--- packages/expo-linear-gradient/src/__tests__/__rsc__/react-native.test.web.tsx
+++ packages/expo-linear-gradient/src/__tests__/__rsc__/react-native.test.web.tsx
@@ -1,27 +1,130 @@
-import { SafeAreaView, TouchableNativeFeedback, Switch, Pressable } from 'react-native';
+import React from 'react';
+import {
+  AccessibilityInfo,
+  ActivityIndicator,
+  Alert,
+  Animated,
+  AppRegistry,
+  AppState,
+  Appearance,
+  BackHandler,
+  Button,
+  // CheckBox,
+  Clipboard,
+  DeviceEventEmitter,
+  // DeviceInfo,
+  Dimensions,
+  DrawerLayoutAndroid,
+  Easing,
+  FlatList,
+  I18nManager,
+  Image,
+  ImageBackground,
+  InputAccessoryView,
+  InteractionManager,
+  Keyboard,
+  KeyboardAvoidingView,
+  LayoutAnimation,
+  Linking,
+  LogBox,
+  Modal,
+  NativeEventEmitter,
+  NativeModules,
+  PanResponder,
+  PermissionsAndroid,
+  // Picker,
+  PixelRatio,
+  Platform,
+  Pressable,
+  // ProgressBar,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  SectionList,
+  Settings,
+  Share,
+  StatusBar,
+  StyleSheet,
+  Switch,
+  Systrace,
+  // TVEventHandler,
+  Text,
+  TextInput,
+  ToastAndroid,
+  Touchable,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  UIManager,
+  Vibration,
+  View,
+  VirtualizedList,
+  YellowBox,
+  // createElement,
+  findNodeHandle,
+  processColor,
+  // render,
+  // unmountComponentAtNode,
+  // useColorScheme,
+  // useLocaleContext,
+  // useWindowDimensions,
+} from 'react-native';
 
-Object.entries({ SafeAreaView, TouchableNativeFeedback, Switch, Pressable }).forEach(
-  ([name, Component]) => {
-    it(`renders ${name} to RSC`, async () => {
-      const jsx = <Component />;
+it(`can use utility functions`, () => {
+  expect(findNodeHandle()).toBe(undefined);
+  expect(processColor('red')).toBe(4294901760);
+});
 
-      await expect(jsx).toMatchFlightSnapshot();
-    });
-  }
-);
+Object.entries({
+  View,
+  ActivityIndicator,
+  StatusBar,
+  Text,
+  ImageBackground,
+  InputAccessoryView,
+  KeyboardAvoidingView,
+  ScrollView,
+  SafeAreaView,
+  Switch,
+  Pressable,
+  Modal,
+}).forEach(([name, Component]) => {
+  it(`renders ${name} to RSC`, async () => {
+    const jsx = <Component />;
 
-// it(`renders SafeAreaView to RSC`, async () => {
-//   const jsx = <SafeAreaView />;
+    await expect(jsx).toMatchFlightSnapshot();
+  });
+});
+Object.entries({
+  TouchableNativeFeedback,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+}).forEach(([name, Component]) => {
+  it(`renders touchable ${name} to RSC`, async () => {
+    await expect(
+      <Component>
+        <Text>{name}</Text>
+      </Component>
+    ).toMatchFlightSnapshot();
+  });
+});
 
-//   await expect(jsx)
-//     .toMatchFlight(`2:I[\"../../node_modules/react-native-web/dist/exports/View/index.js\",[],\"\"]
-// 1:{\"name\":\"SafeAreaView\",\"env\":\"Server\",\"owner\":null}
-// 0:D\"$1\"
-// 0:[\"$\",\"$L2\",null,{\"ref\":\"$undefined\",\"style\":[{\"paddingTop\":\"env(safe-area-inset-top)\",\"paddingRight\":\"env(safe-area-inset-right)\",\"paddingBottom\":\"env(safe-area-inset-bottom)\",\"paddingLeft\":\"env(safe-area-inset-left)\"},\"$undefined\"]},\"$1\"]`);
-// });
-
-// it(`renders TouchableNativeFeedback to RSC`, async () => {
-//   const jsx = <TouchableNativeFeedback />;
-
-//   await expect(jsx).toMatchFlightSnapshot();
-// });
+it(`renders Button to RSC`, async () => {
+  await expect(<Button title="button" />).toMatchFlightSnapshot();
+});
+it(`renders Image to RSC`, async () => {
+  await expect(
+    <Image source={{ uri: 'https://github.com/evanbacon.png' }} />
+  ).toMatchFlightSnapshot();
+});
+it(`renders TextInput to RSC`, async () => {
+  await expect(<TextInput value="Text Input" />).toMatchFlightSnapshot();
+});
+it(`renders Text to RSC`, async () => {
+  await expect(<Text>Hello</Text>).toMatchFlightSnapshot();
+});
+it(`renders Text to RSC`, async () => {
+  await expect(<Text>Hello</Text>).toMatchFlightSnapshot();
+});
