22956a8cfb596323d958dcd388c0d4a4c934f1d0
evanbacon <bacon@expo.io>
2024-06-03 18:43:30 +0200

    clean up testing utils

--- packages/jest-expo/src/rsc-expect.ts
+++ packages/jest-expo/src/rsc-expect.ts
@@ -7,26 +7,37 @@ import { streamToString, renderJsxToFlightStringAsync } from './rsc-utils';
 
 matchers.customTesters = [];
 
+async function resolveFlightInputAsync(data: React.ReactNode | ReadableStream) {
+  if ('getReader' in data) {
+    return (await streamToString(data)).trim();
+  }
+  const resolved = await renderJsxToFlightStringAsync(data);
+  return resolved.trim();
+}
+
+function flightInputAsStringOrPromise(data: any) {
+  if (typeof data === 'string') {
+    return data;
+  }
+  return new Promise(async (res, rej) => {
+    resolveFlightInputAsync(data).then(res).catch(rej);
+  });
+}
+
 expect.extend({
   toMatchFlight(data: string | React.ReactNode | ReadableStream, input: string) {
-    if (typeof data === 'string') {
-      return matchers.toEqual(data, input);
+    const resolved = flightInputAsStringOrPromise(data);
+
+    if (typeof resolved === 'string') {
+      return matchers.toEqual(resolved, input);
     }
+
     return new Promise(async (res, rej) => {
-      if ('getReader' in data) {
-        try {
-          const resolved = (await streamToString(data)).trim();
-          res(matchers.toEqual(resolved, input));
-        } catch (e) {
-          rej(e);
-        }
-      } else {
-        try {
-          const resolved = (await renderJsxToFlightStringAsync(data)).trim();
-          res(matchers.toEqual(resolved, input));
-        } catch (e) {
-          rej(e);
-        }
+      try {
+        const resolvedString = await resolved;
+        res(matchers.toEqual(resolvedString, input));
+      } catch (e) {
+        rej(e);
       }
     });
   },
