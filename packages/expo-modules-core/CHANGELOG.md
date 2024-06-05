29ee21a3b162e341f739a898dbf6c34b2efdc6aa
Evan Bacon <bacon@expo.io>
2024-06-05 23:38:47 +0200

    feat: Use `process.env.EXPO_OS` for `Platform.OS` and `Platform.select` (#29429)
    # Why
    
    - In React Server Components, the Platform.OS and Platform.select values
    aren't available on native because they currently depend on native code.
    This isn't required because bundles are always rolled for a specific
    platform, because of this we inject the constant value
    `process.env.EXPO_OS` in `babel-preset-expo` (which bundlers can support
    by passing `platform` to the caller).
    - This PR detects if `process.env.EXPO_OS` is available and uses it,
    otherwise falling back on the native modules.
    - I've also updated the latest module with new "use client" directives.
    
    # Test Plan
    
    - These changes were split out of the testing in
    https://github.com/expo/expo/pull/29404 I don't expect them to continue
    working until we land continuous tests that warn about expected
    behavior.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-modules-core/CHANGELOG.md
+++ packages/expo-modules-core/CHANGELOG.md
@@ -15,6 +15,7 @@
 
 ### 💡 Others
 
+- Use `process.env.EXPO_OS` for `Platform.OS` and `Platform.select`, when available. ([#29429](https://github.com/expo/expo/pull/29429) by [@EvanBacon](https://github.com/EvanBacon))
 - [Android] Removed unneeded in-between function call when resolving promise without arguments. ([#28944](https://github.com/expo/expo/pull/28944) by [@lukmccall](https://github.com/lukmccall))
 - Exported some TypeScript declarations (e.g. `EventEmitter`) from `expo-modules-core/types`. ([#28994](https://github.com/expo/expo/pull/28994) by [@tsapeta](https://github.com/tsapeta))
 
--- packages/expo-modules-core/build/Platform.js
+++ packages/expo-modules-core/build/Platform.js
@@ -1,11 +1,33 @@
 import { Platform as ReactNativePlatform } from 'react-native';
 import { isDOMAvailable, canUseEventListeners, canUseViewport, isAsyncDebugging, } from './environment/browser';
+if (__DEV__ && typeof process.env.EXPO_OS === 'undefined') {
+    console.warn(`The global process.env.EXPO_OS is not defined. This should be inlined by babel-preset-expo during transformation.`);
+}
+const nativeSelect = typeof window !== 'undefined'
+    ? ReactNativePlatform.select
+    : // process.env.EXPO_OS is injected by `babel-preset-expo` and available in both client and `react-server` environments.
+        // Opt to use the env var when possible, and fallback to the React Native Platform module when it's not (arbitrary bundlers and transformers).
+        function select(specifics) {
+            if (!process.env.EXPO_OS)
+                return undefined;
+            if (specifics.hasOwnProperty(process.env.EXPO_OS)) {
+                return specifics[process.env.EXPO_OS];
+            }
+            else if (process.env.EXPO_OS !== 'web' && specifics.hasOwnProperty('native')) {
+                return specifics.native;
+            }
+            else if (specifics.hasOwnProperty('default')) {
+                return specifics.default;
+            }
+            // do nothing...
+            return undefined;
+        };
 const Platform = {
     /**
      * Denotes the currently running platform.
      * Can be one of ios, android, web.
      */
-    OS: ReactNativePlatform.OS,
+    OS: process.env.EXPO_OS || ReactNativePlatform.OS,
     /**
      * Returns the value with the matching platform.
      * Object keys can be any of ios, android, native, web, default.
@@ -14,7 +36,7 @@ const Platform = {
      * @android android, native, default
      * @web web, default
      */
-    select: ReactNativePlatform.select,
+    select: nativeSelect,
     /**
      * Denotes if the DOM API is available in the current environment.
      * The DOM is not available in native React runtimes and Node.js.
