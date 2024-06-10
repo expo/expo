558de7c3eace98f5b8a5ba5f8985d21f9003464f
Phil Pluckthun <phil@kitten.sh>
2024-06-10 13:34:53 +0100

    Add tests

--- packages/@expo/metro-runtime/src/async-require/__tests__/buildUrlForBundle.test.web.ts
+++ packages/@expo/metro-runtime/src/async-require/__tests__/buildUrlForBundle.test.web.ts
@@ -6,3 +6,17 @@ it(`returns an expected URL`, () => {
 it(`returns an expected URL with non standard root`, () => {
   expect(buildUrlForBundle('/more/than/one')).toEqual('/more/than/one');
 });
+describe(`encodes special characters relevant for S3`, () => {
+  it('encodes a "+" symbol', () => {
+    expect(buildUrlForBundle('/+not-found')).toEqual('/%2bnot-found');
+  });
+  it('ignores query parameters', () => {
+    expect(buildUrlForBundle('/+test?param=+true')).toEqual('/%2btest?param=+true');
+  });
+  it('encodes pathnames of URIs', () => {
+    expect(buildUrlForBundle('http://test.com/+not-found')).toEqual('http://test.com/%2bnot-found');
+  });
+  it('encodes various characters of pathnames', () => {
+    expect(buildUrlForBundle('/+!"#$&\'()*+,:;=@')).toMatchSnapshot();
+  });
+});
