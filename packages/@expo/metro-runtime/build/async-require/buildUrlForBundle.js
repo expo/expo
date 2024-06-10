1dbaa2907b430c9ee9176f7be9e5468c6a5a7899
Phil Pluckthun <phil@kitten.sh>
2024-06-10 13:35:51 +0100

    Update built files

--- packages/@expo/metro-runtime/build/async-require/buildUrlForBundle.js
+++ packages/@expo/metro-runtime/build/async-require/buildUrlForBundle.js
@@ -7,6 +7,29 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildUrlForBundle = void 0;
+/** Given a path encodes the pathname part to be a safe URI.
+ *
+ * @remarks
+ * On file services, especially S3, special characters in the pathname
+ * part must be encoded to be recognized properly.
+ * We list a regular expression with the specifically problematic
+ * characters manually to be encoded here.
+ */
+function encodeBundlePath(filename) {
+    if (typeof window === 'undefined') {
+        return encodeURI(filename);
+    }
+    const encode = (pathname) => pathname.replace(/[+!"#$&'()*+,:;=?@]/g, (match) => `%${match.charCodeAt(0).toString(16)}`);
+    if (filename.match(/^https?:\/\//)) {
+        const url = new URL(filename, window.location.origin);
+        url.pathname = encode(url.pathname);
+        return url.toString();
+    }
+    else {
+        const [pathname, query] = filename.split('?');
+        return query ? `${encode(pathname)}?${query}` : encode(pathname);
+    }
+}
 /**
  * Given a path and some optional additional query parameters, create the dev server bundle URL.
  * @param bundlePath like `/foobar`
@@ -15,11 +38,11 @@ exports.buildUrlForBundle = void 0;
  */
 function buildUrlForBundle(bundlePath) {
     if (bundlePath.match(/^https?:\/\//)) {
-        return bundlePath;
+        return encodeBundlePath(bundlePath);
     }
     // NOTE(EvanBacon): This must come from the window origin (at least in dev mode).
     // Otherwise Metro will crash from attempting to load a bundle that doesn't exist.
-    return '/' + bundlePath.replace(/^\/+/, '');
+    return encodeBundlePath('/' + bundlePath.replace(/^\/+/, ''));
 }
 exports.buildUrlForBundle = buildUrlForBundle;
 //# sourceMappingURL=buildUrlForBundle.js.map
