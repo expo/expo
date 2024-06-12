32d81c95535f050d790f2a73bf03a91f3c1765f1
Alan Hughes <alan@expo.io>
2024-06-12 11:02:12 +0100

    [ios][camera] Return correct exif orientation

--- packages/expo-camera/ios/Common/ExpoCameraUtils.swift
+++ packages/expo-camera/ios/Common/ExpoCameraUtils.swift
@@ -110,6 +110,29 @@ struct ExpoCameraUtils {
       return 0
     }
   }
+  
+  static func toExifOrientation(orientation: UIImage.Orientation) -> Int {
+    switch orientation {
+    case .up:
+      return 1
+    case .down:
+      return 3
+    case .left:
+      return 8
+    case .right:
+      return 6
+    case .upMirrored:
+      return 2
+    case .downMirrored:
+      return 4
+    case .leftMirrored:
+      return 5
+    case .rightMirrored:
+      return 7
+    @unknown default:
+      return 1
+    }
+  }
 
   static func exportImage(orientation: UIImage.Orientation) -> Int {
     switch orientation {
