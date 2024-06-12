55a057c7bbe7457c3c8dece739347839fe8840b9
Brent Vatne <brentvatne@gmail.com>
2024-06-11 17:50:27 -0700

    Revert "fix(cli, metro-config): Fix source map generation in development. (#29463)"
    This reverts commit 6c5afe73329f871faefe8fb22c88966335128a93.

--- packages/@expo/metro-config/src/serializer/__tests__/environmentVariableSerializerPlugin.test.ts
+++ packages/@expo/metro-config/src/serializer/__tests__/environmentVariableSerializerPlugin.test.ts
@@ -1,7 +1,4 @@
-import {
-  getTransformEnvironment,
-  getEnvVarDevString,
-} from '../environmentVariableSerializerPlugin';
+import { getTransformEnvironment } from '../environmentVariableSerializerPlugin';
 
 describe(getTransformEnvironment, () => {
   [
@@ -21,32 +18,5 @@ describe(getTransformEnvironment, () => {
       )
     ).toBe(null);
     expect(getTransformEnvironment('/index.bundle?&platform=web&dev=true&hot=false')).toBe(null);
-  });
-});
-
-describe(getEnvVarDevString, () => {
-  it(`always formats env var code in one line`, () => {
-    expect(getEnvVarDevString({})).toMatchInlineSnapshot(
-      `"/* HMR env vars from Expo CLI (dev-only) */ process.env=Object.defineProperties(process.env, {});"`
-    );
-  });
-  it(`formats env vars with new line characters in them`, () => {
-    expect(
-      getEnvVarDevString({
-        EXPO_PUBLIC_TEST: 'test\nvalue',
-      })
-    ).toMatchInlineSnapshot(
-      `"/* HMR env vars from Expo CLI (dev-only) */ process.env=Object.defineProperties(process.env, {"EXPO_PUBLIC_TEST": { value: "test\\nvalue" }});"`
-    );
-  });
-  it(`formats multiple env vars`, () => {
-    expect(
-      getEnvVarDevString({
-        EXPO_PUBLIC_A: 'a',
-        EXPO_PUBLIC_B: 'b',
-      })
-    ).toMatchInlineSnapshot(
-      `"/* HMR env vars from Expo CLI (dev-only) */ process.env=Object.defineProperties(process.env, {"EXPO_PUBLIC_A": { value: "a" },"EXPO_PUBLIC_B": { value: "b" }});"`
-    );
   });
 });
