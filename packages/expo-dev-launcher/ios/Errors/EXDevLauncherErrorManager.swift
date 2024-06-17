e7ccfa8369b900f6b2312d2f3193759fb02a83ef
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2024-06-17 17:43:56 +0200

    [dev-menu] Remove unused native extensions (#29611)

--- packages/expo-dev-launcher/ios/Errors/EXDevLauncherErrorManager.swift
+++ packages/expo-dev-launcher/ios/Errors/EXDevLauncherErrorManager.swift
@@ -8,7 +8,7 @@ public class EXDevLauncherErrorManager: NSObject {
   private weak var currentVC: EXDevLauncherErrorViewController?
   private var error: EXDevLauncherAppError?
   private static let VIEW_TAG = 6634
-  
+
   @objc
   public init(controller: EXDevLauncherController) {
     self.controller = controller
@@ -33,10 +33,10 @@ public class EXDevLauncherErrorManager: NSObject {
     currentVC = nextViewController
     controller?.currentWindow()?.rootViewController = currentVC
     controller?.currentWindow()?.makeKeyAndVisible()
-    
+
     // remove splash screen
     currentVC?.view.subviews.forEach {
-      if ($0.tag != EXDevLauncherErrorManager.VIEW_TAG) {
+      if $0.tag != EXDevLauncherErrorManager.VIEW_TAG {
         $0.removeFromSuperview()
       }
     }
