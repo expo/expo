1aff1609b5db128e9f8ab0a2eb0aba3597904dc9
Simek <gosimek@gmail.com>
2024-06-22 13:43:32 +0200

    fix renamed exports

--- packages/expo-location/build/index.js
+++ packages/expo-location/build/index.js
@@ -1,8 +1,8 @@
-import { LocationEventEmitter } from './LocationEventEmitter';
 export { PermissionStatus } from 'expo-modules-core';
-export { LocationEventEmitter as EventEmitter };
+export { LocationEventEmitter as EventEmitter } from './LocationEventEmitter';
 export { _getCurrentWatchId } from './LocationSubscribers';
 export { installWebGeolocationPolyfill } from './GeolocationPolyfill';
 export * from './Location';
 export * from './Location.types';
+export { LocationAccuracy as Accuracy, LocationActivityType as ActivityType, LocationGeofencingEventType as GeofencingEventType, LocationGeofencingRegionState as GeofencingRegionState, } from './Location.types';
 //# sourceMappingURL=index.js.map
--- packages/expo-location/src/index.ts
+++ packages/expo-location/src/index.ts
@@ -1,23 +1,15 @@
-import type {
-  LocationAccuracy,
-  LocationActivityType,
-  LocationGeofencingEventType,
-  LocationGeofencingRegionState,
-} from './Location.types';
-import { LocationEventEmitter } from './LocationEventEmitter';
-
 export { PermissionStatus, type PermissionHookOptions } from 'expo-modules-core';
+
+export { LocationEventEmitter as EventEmitter } from './LocationEventEmitter';
+export { _getCurrentWatchId } from './LocationSubscribers';
+export { installWebGeolocationPolyfill } from './GeolocationPolyfill';
+
+export * from './Location';
+export * from './Location.types';
 
 export {
   LocationAccuracy as Accuracy,
   LocationActivityType as ActivityType,
   LocationGeofencingEventType as GeofencingEventType,
   LocationGeofencingRegionState as GeofencingRegionState,
-};
-
-export { LocationEventEmitter as EventEmitter };
-export { _getCurrentWatchId } from './LocationSubscribers';
-export { installWebGeolocationPolyfill } from './GeolocationPolyfill';
-
-export * from './Location';
-export * from './Location.types';
+} from './Location.types';
