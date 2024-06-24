expo/expo

commit 86cb248cc4368023786e24715f42cbaa70043f62

Create new stub module to replace legacy one in appContext (#27069)
Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

packages/expo-modules-core/ios/FileSystemUtilities/FileSystemLegacyUtilities.swift
@@ -1,0 +1,111 @@
+// Copyright 2023-present 650 Industries. All rights reserved.
+
+import Foundation
+
+@objc(EXFileSystemLegacyUtilities)
+public class FileSystemLegacyUtilities: NSObject, EXInternalModule, EXFileSystemInterface, EXFilePermissionModuleInterface {
+  @objc
+  public var documentDirectory: String
+
+  @objc
+  public var cachesDirectory: String
+
+  var isScoped: Bool = false
+
+  @objc
+  public init(documentDirectory: String, cachesDirectory: String) {
+    self.documentDirectory = documentDirectory
+    self.cachesDirectory = cachesDirectory
+    self.isScoped = true
+
+    super.init()
+    ensureDirExists(withPath: self.cachesDirectory)
+    ensureDirExists(withPath: self.documentDirectory)
+  }
+
+  required public override init() {
+    let documentPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+    self.documentDirectory = documentPaths[0]
+
+    let cachesPaths = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
+    self.cachesDirectory = cachesPaths[0]
+
+    super.init()
+    ensureDirExists(withPath: self.cachesDirectory)
+    ensureDirExists(withPath: self.documentDirectory)
+  }
+
+  public static func exportedInterfaces() -> [Protocol] {
+    return [EXFileSystemInterface.self, EXFilePermissionModuleInterface.self]
+  }
+
+  @objc
+  public func permissions(forURI uri: URL) -> EXFileSystemPermissionFlags {
+    let validSchemas = [
+      "assets-library",
+      "http",
+      "https",
+      "ph"
+    ]
+
+    if validSchemas.contains(uri.scheme ?? "") {
+      return EXFileSystemPermissionFlags.read
+    }
+    if uri.scheme == "file" {
+      return getPathPermissions(uri.absoluteString)
+    }
+    return []
+  }
+
+  @objc
+  public func generatePath(inDirectory directory: String, withExtension ext: String) -> String {
+    let fileName = "\(UUID().uuidString)\(ext)"
+    ensureDirExists(withPath: directory)
+    return (directory as NSString).appendingPathComponent(fileName)
+  }
+
+  @objc
+  public func ensureDirExists(withPath path: String) -> Bool {
+    let url = URL(fileURLWithPath: path)
+    return FileSystemUtilities.ensureDirExists(at: url)
+  }
+
+  @objc
+  public func getPathPermissions(_ path: String) -> EXFileSystemPermissionFlags {
+    guard let url = URL(string: path) else {
+      return []
+    }
+    let permissionsForInternalDirectories = getInternalPathPermissions(url)
+    if !permissionsForInternalDirectories.isEmpty {
+      return permissionsForInternalDirectories
+    }
+    return getExternalPathPermissions(url)
+  }
+
+  @objc
+  public func getInternalPathPermissions(_ url: URL) -> EXFileSystemPermissionFlags {
+    let scopedDirs: [String] = [cachesDirectory, documentDirectory]
+    let standardizedPath = url.standardized.path
+    for scopedDirectory in scopedDirs {
+      if standardizedPath.hasPrefix(scopedDirectory + "/") || standardizedPath == scopedDirectory {
+        return [.read, .write]
+      }
+    }
+    return []
+  }
+
+  @objc
+  public func getExternalPathPermissions(_ url: URL) -> EXFileSystemPermissionFlags {
+    if self.isScoped && url.path.contains("ExponentExperienceData") {
+      return []
+    }
+    var filePermissions: EXFileSystemPermissionFlags = []
+    if FileManager.default.isReadableFile(atPath: url.absoluteString) {
+      filePermissions.insert(.read)
+    }
+    if FileManager.default.isWritableFile(atPath: url.absoluteString) {
+      filePermissions.insert(.write)
+    }
+    return filePermissions
+  }
+}

apps/expo-go/ios/Exponent/Versioned/Core/UniversalModules/EXScopedFileSystem/EXScopedFilePermissionModule.h
@@ -1,16 +1,0 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
-
-#if __has_include(<ExpoFileSystem/EXFilePermissionModule.h>)
-#import <ExpoFileSystem/EXFilePermissionModule.h>
-#import "EXConstantsBinding.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface EXScopedFilePermissionModule : EXFilePermissionModule
-
-- (instancetype)initWithConstantsBinding:(EXConstantsBinding *)constantsBinding;
-
-@end
-
-NS_ASSUME_NONNULL_END
-#endif

apps/expo-go/ios/Exponent/Versioned/Core/UniversalModules/EXScopedFileSystem/EXScopedFilePermissionModule.m
@@ -1,38 +1,0 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
-
-#if __has_include(<ExpoFileSystem/EXFilePermissionModule.h>)
-#import "EXScopedFilePermissionModule.h"
-#import <ExpoModulesCore/EXFileSystemInterface.h>
-
-@interface EXFilePermissionModule (Protected)
-
-- (EXFileSystemPermissionFlags)getExternalPathPermissions:(NSString *)path;
-
-@end
-
-@interface EXScopedFilePermissionModule ()
-
-@end
-
-@implementation EXScopedFilePermissionModule
-
-- (instancetype)initWithConstantsBinding:(EXConstantsBinding *)constantsBinding
-{
-  return [super init];
-}
-
-- (EXFileSystemPermissionFlags)getExternalPathPermissions:(NSString *)path
-{
-  // may block access to external paths which contain "ExponentExperienceData" 
-  if (![self shouldForbidAccessToExternalDirectories] || (![path containsString:@"ExponentExperienceData"])) {
-    return [super getExternalPathPermissions:path];
-  }
-  return EXFileSystemPermissionNone;
-}
-
-- (BOOL)shouldForbidAccessToExternalDirectories {
-  return true;
-}
-
-@end
-#endif

apps/expo-go/ios/Exponent/Versioned/Core/UniversalModules/EXScopedModuleRegistryAdapter.m
@@ -27,11 +27,11 @@
 #import "EXScopedNotificationCategoriesModule.h"
 #import "EXScopedServerRegistrationModule.h"
 
-#import <ExpoFileSystem/EXFileSystem.h>
-
 #if __has_include(<EXTaskManager/EXTaskManager.h>)
 #import <EXTaskManager/EXTaskManager.h>
 #endif
+
+#import <ExpoModulesCore-Swift.h>
 
 @implementation EXScopedModuleRegistryAdapter
 
@@ -53,17 +53,13 @@
   [moduleRegistry registerExportedModule:scopedFacebook];
 #endif
 
-#if __has_include(<ExpoFileSystem/EXFileSystem.h>)
-  if (params[@"fileSystemDirectories"]) {
-    // Override the FileSystem module with custom document and cache directories
-    NSString *documentDirectory = params[@"fileSystemDirectories"][@"documentDirectory"];
-    NSString *cachesDirectory = params[@"fileSystemDirectories"][@"cachesDirectory"];
-    EXFileSystem *fileSystemModule = [[EXFileSystem alloc] initWithDocumentDirectory:documentDirectory
-                                                                     cachesDirectory:cachesDirectory];
-    [moduleRegistry registerExportedModule:fileSystemModule];
-    [moduleRegistry registerInternalModule:fileSystemModule];
-  }
-#endif
+if (params[@"fileSystemDirectories"]) {
+  // Override the FileSystem module with custom document and cache directories
+  NSString *documentDirectory = params[@"fileSystemDirectories"][@"documentDirectory"];
+  NSString *cachesDirectory = params[@"fileSystemDirectories"][@"cachesDirectory"];
+  EXFileSystemLegacyUtilities *fileSystemModule = [[EXFileSystemLegacyUtilities alloc] initWithDocumentDirectory:documentDirectory cachesDirectory:cachesDirectory];
+  [moduleRegistry registerInternalModule:fileSystemModule];
+}
 
 #if __has_include(<EXFont/EXFontLoader.h>)
   EXScopedFontLoader *fontModule = [[EXScopedFontLoader alloc] init];
@@ -80,11 +76,6 @@
 
   EXExpoUserNotificationCenterProxy *userNotificationCenter = [[EXExpoUserNotificationCenterProxy alloc] initWithUserNotificationCenter:kernelServices[EX_UNVERSIONED(@"EXUserNotificationCenter")]];
   [moduleRegistry registerInternalModule:userNotificationCenter];
-
-#if __has_include(<ExpoFileSystem/EXFilePermissionModule.h>)
-  EXScopedFilePermissionModule *filePermissionModule = [[EXScopedFilePermissionModule alloc] init];
-  [moduleRegistry registerInternalModule:filePermissionModule];
-#endif
 
 #if __has_include(<EXSecureStore/EXSecureStore.h>)
   EXScopedSecureStore *secureStoreModule = [[EXScopedSecureStore alloc] initWithScopeKey:scopeKey andConstantsBinding:constantsBinding];

packages/expo-file-system/ios/EXFilePermissionModule.h
@@ -1,16 +1,0 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
-
-#import <Foundation/Foundation.h>
-#import <ExpoModulesCore/EXFilePermissionModuleInterface.h>
-#import <ExpoModulesCore/EXInternalModule.h>
-#import <ExpoModulesCore/EXModuleRegistryConsumer.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface EXFilePermissionModule : NSObject <EXInternalModule, EXFilePermissionModuleInterface, EXModuleRegistryConsumer>
-
-- (EXFileSystemPermissionFlags)getPathPermissions:(NSString *)path;
-
-@end
-
-NS_ASSUME_NONNULL_END

packages/expo-file-system/ios/EXFilePermissionModule.m
@@ -1,63 +1,0 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
-#import <ExpoFileSystem/EXFilePermissionModule.h>
-#import <ExpoModulesCore/EXFileSystemInterface.h>
-
-@interface EXFilePermissionModule ()
-
-@property (nonatomic, weak) EXModuleRegistry *moduleRegistry;
-
-@end
-
-@implementation EXFilePermissionModule
-
-EX_REGISTER_MODULE();
-
-+ (const NSArray<Protocol *> *)exportedInterfaces
-{
-  return @[@protocol(EXFilePermissionModuleInterface)];
-}
-
-- (EXFileSystemPermissionFlags)getPathPermissions:(NSString *)path
-{
-  EXFileSystemPermissionFlags permissionsForInternalDirectories = [self getInternalPathPermissions:path];
-  if (permissionsForInternalDirectories != EXFileSystemPermissionNone) {
-    return permissionsForInternalDirectories;
-  } else {
-    return [self getExternalPathPermissions:path];
-  }
-}
-
-- (EXFileSystemPermissionFlags)getInternalPathPermissions:(NSString *)path
-{
-  id<EXFileSystemInterface> fileSystem = [_moduleRegistry getModuleImplementingProtocol:@protocol(EXFileSystemInterface)];
-  NSArray<NSString *> *scopedDirs = @[fileSystem.cachesDirectory, fileSystem.documentDirectory];
-  NSString *standardizedPath = [path stringByStandardizingPath];
-  for (NSString *scopedDirectory in scopedDirs) {
-    if ([standardizedPath hasPrefix:[scopedDirectory stringByAppendingString:@"/"]] ||
-        [standardizedPath isEqualToString:scopedDirectory]) {
-      return EXFileSystemPermissionRead | EXFileSystemPermissionWrite;
-    }
-  }
-
-  return EXFileSystemPermissionNone;
-}
-
-- (EXFileSystemPermissionFlags)getExternalPathPermissions:(NSString *)path
-{
-  EXFileSystemPermissionFlags filePermissions = EXFileSystemPermissionNone;
-  if ([[NSFileManager defaultManager] isReadableFileAtPath:path]) {
-    filePermissions |= EXFileSystemPermissionRead;
-  }
-
-  if ([[NSFileManager defaultManager] isWritableFileAtPath:path]) {
-    filePermissions |= EXFileSystemPermissionWrite;
-  }
-
-  return filePermissions;
-}
-
-- (void)setModuleRegistry:(EXModuleRegistry *)moduleRegistry {
-  _moduleRegistry = moduleRegistry;
-}
-
-@end

packages/expo-file-system/ios/EXFileSystem.h
@@ -1,24 +1,0 @@
-// Copyright 2016-present 650 Industries. All rights reserved.
-
-#import <Foundation/Foundation.h>
-#import <ExpoModulesCore/EXExportedModule.h>
-#import <ExpoModulesCore/EXModuleRegistryConsumer.h>
-#import <ExpoModulesCore/EXEventEmitter.h>
-#import <ExpoModulesCore/EXFileSystemInterface.h>
-
-@interface EXFileSystem : EXExportedModule <EXEventEmitter, EXModuleRegistryConsumer, EXFileSystemInterface>
-
-@property (nonatomic, readonly) NSString *documentDirectory;
-@property (nonatomic, readonly) NSString *cachesDirectory;
-
-- (instancetype)initWithDocumentDirectory:(NSString *)documentDirectory cachesDirectory:(NSString *)cachesDirectory;
-
-- (EXFileSystemPermissionFlags)permissionsForURI:(NSURL *)uri;
-
-- (nullable NSURL *)percentEncodedURLFromURIString:(nonnull NSString *)uri;
-
-- (BOOL)ensureDirExistsWithPath:(NSString *)path;
-
-- (NSString *)generatePathInDirectory:(NSString *)directory withExtension:(NSString *)extension;
-
-@end

commit 0801eb3fc5eca3dbb8f6157233a58073fd1268ee

[core] fix local path paring error on ios version lower than 17 (#29958)
# Why

possibly fix for #29759

# How

on ios < 17, the `URL(string:)` cannot parse an URL that has spaces and
without scheme, it returns nil instead. before SDK 51, we used
`URL(fileURLWithPath:)` instead. that's a regression from #27069.
this pr tries to use the `URLComponents(string:)` as fallback for ios <
16 which supports RFC 3986 with spaces.

packages/expo-modules-core/ios/FileSystemUtilities/FileSystemLegacyUtilities.swift
@@ -83,7 +83,7 @@ public class FileSystemLegacyUtilities: NSObject, EXInternalModule, EXFileSystem
 
   @objc
   public func getPathPermissions(_ path: String) -> EXFileSystemPermissionFlags {
-    guard let url = URL(string: path) else {
+    guard let url = convertToUrl(string: path) else {
       return []
     }
     let permissionsForInternalDirectories = getInternalPathPermissions(url)

packages/expo-modules-core/ios/Utilities.swift
@@ -60,9 +60,23 @@ internal func isFileUrlPath(_ path: String) -> Bool {
 }
 
 internal func convertToUrl(string value: String) -> URL? {
-  // URLComponents parses and constructs URLs according to RFC 3986.
-  // For some unusual urls URL(string:) will fail incorrectly
-  guard let url = URLComponents(string: value)?.url ?? URL(string: value) else {
+  let url: URL?
+  if #available(iOS 17, *) {
+    // URL(string:) supports RFC 3986 as URLComponents from iOS 17
+    url = URL(string: value)
+  } else if #available(iOS 16, *) {
+    // URLComponents parses and constructs URLs according to RFC 3986.
+    // For some unusual urls URL(string:) will fail incorrectly
+    url = URLComponents(string: value)?.url ?? URL(string: value)
+  } else {
+    // URLComponents on iOS 15 and lower does not well support RFC 3986.
+    // We have to fallback URL(fileURLWithPath:) first.
+    url = value.first == "/"
+      ? URL(fileURLWithPath: value)
+      : URLComponents(string: value)?.url ?? URL(string: value)
+  }
+
+  guard let url else {
     return nil
   }
   // If it has no scheme, we assume it was the file path which needs to be recreated to be recognized as the file url.

packages/expo-modules-core/ios/Tests/FileSystemLegacyUtilitiesSpec.swift
@@ -1,0 +1,29 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+import ExpoModulesTestCore
+
+@testable import ExpoModulesCore
+
+final class FileSystemLegacyUtilitiesSpec: ExpoSpec {
+  override class func spec() {
+    let fsUtils = FileSystemLegacyUtilities()
+
+    describe("getPathPermissions") {
+      it("should return read/write permissions for filePath with `file:` scheme") {
+        let dirUrl = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        let fileUrl = dirUrl.appendingPathComponent("dir/test.txt")
+        let filePath = fileUrl.absoluteString
+        expect(filePath.starts(with: "file:")) == true
+        expect(fsUtils.getPathPermissions(filePath)) == [.read, .write]
+      }
+
+      it("should return read/write permissions for filePath without `file:` scheme") {
+        let dirUrl = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        let fileUrl = dirUrl.appendingPathComponent("dir/test.txt")
+        let filePath = fileUrl.path
+        expect(filePath.starts(with: "file:")) == false
+        expect(fsUtils.getPathPermissions(filePath)) == [.read, .write]
+      }
+    }
+  }
+}