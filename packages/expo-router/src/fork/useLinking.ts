3985c7e6c5a0c77ff06f8e04c330ef20ee9e0546
Mark Lawlor <mwlawlor@gmail.com>
2024-06-26 06:49:17 +1000

    prevest null prototype for params

--- packages/expo-router/src/fork/useLinking.ts
+++ packages/expo-router/src/fork/useLinking.ts
@@ -275,7 +275,7 @@ export default function useLinking(
           if (
             focusedRoute &&
             focusedRoute.name === route.name &&
-            isEqual(focusedRoute.params, route.params)
+            isEqual({ ...focusedRoute.params }, { ...route.params })
           ) {
             /* Start of fork. Source: https://github.com/react-navigation/react-navigation/blob/13d4aa270b301faf07960b4cd861ffc91e9b2c46/packages/native/src/useLinking.tsx#L280  */
             return appendBaseUrl(route.path);
