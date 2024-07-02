8b0b1faac7613e5b8433c4abed8491e93c407f63
Alan Hughes <alan@expo.io>
2024-07-02 15:25:47 +0100

    [ios][audio] User ref instead of pointer

--- packages/expo-audio/ios/AudioModule.swift
+++ packages/expo-audio/ios/AudioModule.swift
@@ -64,7 +64,7 @@ public class AudioModule: Module {
         let player = AudioPlayer(avPlayer)
         players[player.id] = player
         // Gets the duration of the item on load
-        player.pointer
+        player.ref
           .publisher(for: \.currentItem?.status)
           .sink { status in
             guard let status else {
@@ -103,9 +103,9 @@ public class AudioModule: Module {
       }
 
       Property("mute") { player in
-        player.pointer.isMuted
+        player.ref.isMuted
       }.set { (player, isMuted: Bool) in
-        player.pointer.isMuted = isMuted
+        player.ref.isMuted = isMuted
       }
 
       Property("shouldCorrectPitch") { player in
@@ -115,25 +115,25 @@ public class AudioModule: Module {
       }
 
       Property("currentTime") { player in
-        player.pointer.currentItem?.currentTime().seconds
+        player.ref.currentItem?.currentTime().seconds
       }
 
       Property("duration") { player in
-        player.pointer.currentItem?.duration.seconds
+        player.ref.currentItem?.duration.seconds
       }
 
       Property("playbackRate") { player in
-        player.pointer.rate
+        player.ref.rate
       }
 
       Property("paused") { player in
-        return player.pointer.rate == 0.0
+        return player.ref.rate == 0.0
       }
 
       Property("volume") { player in
-        player.pointer.volume
+        player.ref.volume
       }.set { (player, volume: Double) in
-        player.pointer.volume = Float(volume)
+        player.ref.volume = Float(volume)
       }
 
       Function("play") { player in
@@ -143,36 +143,36 @@ public class AudioModule: Module {
         let rate = player.currentRate > 0 ? player.currentRate : 1.0
         addPlaybackEndNotification(player: player)
         registerTimeObserver(player: player)
-        player.pointer.playImmediately(atRate: rate)
+        player.ref.playImmediately(atRate: rate)
       }
 
       Function("setPlaybackRate") { (player, rate: Double, pitchCorrectionQuality: PitchCorrectionQuality?) in
         let playerRate = rate < 0 ? 0.0 : Float(min(rate, 2.0))
         if player.playing {
-          player.pointer.rate = playerRate
+          player.ref.rate = playerRate
         }
         player.currentRate = playerRate
         if player.shouldCorrectPitch {
           player.pitchCorrectionQuality = pitchCorrectionQuality?.toPitchAlgorithm() ?? .varispeed
-          player.pointer.currentItem?.audioTimePitchAlgorithm = player.pitchCorrectionQuality
+          player.ref.currentItem?.audioTimePitchAlgorithm = player.pitchCorrectionQuality
         }
       }
 
       Function("pause") { player in
-        player.pointer.pause()
+        player.ref.pause()
       }
 
       Function("release") { player in
         let id = player.id
         if let token = timeTokens[id] {
-          player.pointer.removeTimeObserver(token as Any)
+          player.ref.removeTimeObserver(token as Any)
         }
-        player.pointer.pause()
+        player.ref.pause()
         players.removeValue(forKey: player.id)
       }
 
       AsyncFunction("seekTo") { (player: AudioPlayer, seconds: Double) in
-        await player.pointer.currentItem?.seek(
+        await player.ref.currentItem?.seek(
           to: CMTime(
             seconds: seconds / 1000,
             preferredTimescale: CMTimeScale(NSEC_PER_SEC)
@@ -203,7 +203,7 @@ public class AudioModule: Module {
       }
 
       Property("currentTime") { recorder in
-        recorder.pointer.currentTime
+        recorder.ref.currentTime
       }
 
       Property("uri") { recorder in
@@ -212,14 +212,14 @@ public class AudioModule: Module {
 
       Function("record") { (recorder: AudioRecorder) -> [String: Any] in
         try checkPermissions()
-        recorder.pointer.record()
+        recorder.ref.record()
         recorder.startTimestamp = Int(recorder.deviceCurrentTime)
         return recorder.getRecordingStatus()
       }
 
       Function("pause") { recorder in
         try checkPermissions()
-        recorder.pointer.pause()
+        recorder.ref.pause()
         let current = recorder.deviceCurrentTime
         recorder.previousRecordingDuration += (current - recorder.startTimestamp)
         recorder.startTimestamp = 0
@@ -227,13 +227,13 @@ public class AudioModule: Module {
 
       Function("stop") { recorder in
         try checkPermissions()
-        recorder.pointer.stop()
+        recorder.ref.stop()
         recorder.startTimestamp = 0
         recorder.previousRecordingDuration = 0
       }
 
       Function("release") { recorder in
-        recorder.pointer.stop()
+        recorder.ref.stop()
         recorders.removeValue(forKey: recorder.id)
       }
 
@@ -243,12 +243,12 @@ public class AudioModule: Module {
 
       Function("startRecordingAtTime") { (recorder, seconds: Double) in
         try checkPermissions()
-        recorder.pointer.record(atTime: TimeInterval(seconds))
+        recorder.ref.record(atTime: TimeInterval(seconds))
       }
 
       Function("recordForDuration") { (recorder, seconds: Double) in
         try checkPermissions()
-        recorder.pointer.record(forDuration: TimeInterval(seconds))
+        recorder.ref.record(forDuration: TimeInterval(seconds))
       }
 
       Function("getAvailableInputs") {
@@ -268,7 +268,7 @@ public class AudioModule: Module {
   private func setIsAudioActive(_ isActive: Bool) throws {
     if !isActive {
       for player in players.values {
-        player.pointer.pause()
+        player.ref.pause()
       }
     }
 
@@ -288,7 +288,7 @@ public class AudioModule: Module {
     if !mode.allowsRecording {
       recorders.values.forEach { recorder in
         if recorder.isRecording {
-          recorder.pointer.stop()
+          recorder.ref.stop()
         }
       }
     }
@@ -338,12 +338,12 @@ public class AudioModule: Module {
     }
     endObservers[player.id] = NotificationCenter.default.addObserver(
       forName: .AVPlayerItemDidPlayToEndTime,
-      object: player.pointer.currentItem,
+      object: player.ref.currentItem,
       queue: nil
     ) { _ in
       if player.isLooping {
-        player.pointer.seek(to: CMTime.zero)
-        player.pointer.play()
+        player.ref.seek(to: CMTime.zero)
+        player.ref.play()
       } else {
         player.updateStatus(with: [
           "isPlaying": false
@@ -354,7 +354,7 @@ public class AudioModule: Module {
 
   private func registerTimeObserver(player: AudioPlayer) {
     let interval = CMTime(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
-    timeTokens[player.id] = player.pointer.addPeriodicTimeObserver(forInterval: interval, queue: nil) { time in
+    timeTokens[player.id] = player.ref.addPeriodicTimeObserver(forInterval: interval, queue: nil) { time in
       player.updateStatus(with: [
         "currentPosition": time.seconds * 1000
       ])
