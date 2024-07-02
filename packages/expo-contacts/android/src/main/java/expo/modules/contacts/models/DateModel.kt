5bb360b59ba0f8bfe27a04c9545aec98a8f55ae6
Eric Sanchez <eric@azzapp.com>
2024-07-02 15:14:09 +0200

    Fixed creation of a contact with a birthday on Android

--- packages/expo-contacts/android/src/main/java/expo/modules/contacts/models/DateModel.kt
+++ packages/expo-contacts/android/src/main/java/expo/modules/contacts/models/DateModel.kt
@@ -1,6 +1,8 @@
 package expo.modules.contacts.models
 
+import android.content.ContentProviderOperation
 import android.database.Cursor
+import android.provider.ContactsContract
 import android.provider.ContactsContract.CommonDataKinds
 import expo.modules.contacts.Columns
 import java.text.SimpleDateFormat
@@ -22,7 +24,7 @@ class DateModel : BaseModel() {
 
   override fun fromMap(readableMap: Map<String, Any?>) {
     super.fromMap(readableMap)
-    val dateString = readableMap["date"] as String?
+    val dateString = parseDateFromMap(readableMap) as String?
     val hasYear = !dateString!!.startsWith("--")
     val calendar = Calendar.getInstance()
     val datePattern = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
@@ -52,5 +54,50 @@ class DateModel : BaseModel() {
         CommonDataKinds.Event.TYPE_OTHER -> "other"
         else -> "unknown"
       }
+  }
+
+  override fun getInsertOperation(rawId: String?): ContentProviderOperation {
+    val op = ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+    if (rawId == null) {
+      op.withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+    } else {
+      op.withValue(ContactsContract.Data.RAW_CONTACT_ID, rawId)
+    }
+
+    return op.withValue(Columns.MIMETYPE, contentType)
+            .withValue(ContactsContract.CommonDataKinds.Event.TYPE, mapStringToType(label))
+            .withValue(ContactsContract.CommonDataKinds.Event.START_DATE, formatDateString())
+            .withValue(ContactsContract.CommonDataKinds.Event.LABEL, label)
+            .build()
+  }
+
+  private fun parseDateFromMap(dateMap: Map<String, Any?>): String? {
+    val year = (dateMap["year"] as? Double)?.toInt()
+    val month = (dateMap["month"] as? Double)?.toInt()
+    val day = (dateMap["day"] as? Double)?.toInt()
+
+    return if (year != null && month != null && day != null) {
+      val calendar = Calendar.getInstance()
+      calendar.set(Calendar.YEAR, year)
+      calendar.set(Calendar.MONTH, month) 
+      calendar.set(Calendar.DAY_OF_MONTH, day)
+
+      val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+      dateFormat.format(calendar.time)
+    } else {
+      null
+    }
+  }
+
+  private fun formatDateString(): String? {
+    val year = map.getInt("year", -1)
+    val month = map.getInt("month", -1)
+    val day = map.getInt("day", -1)
+
+    return if (year > 0 && month > 0 && day > 0) {
+      String.format(Locale.getDefault(), "%04d-%02d-%02d", year, month, day)
+    } else {
+      null
+    }
   }
 }
