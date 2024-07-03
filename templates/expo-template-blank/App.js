43bce0be78a8eb4bd7a431edd6c8b7ae88b72232
owusuprince <94824951+owusuprince@users.noreply.github.com>
2024-07-03 05:57:42 -1200

    App.js

--- templates/expo-template-blank/App.js
+++ templates/expo-template-blank/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor: '#00fff' }}>
+      <Text style={{ fontSize: 24 }}>
+        My name is <Text style={{ fontWeight: 'bold' }}>OWUSU PRINCE</Text>
+      </Text>
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
