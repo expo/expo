953c26ee3bab864b690e3633c56f569f62a92d71
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2024-07-04 16:03:20 +0200

    [menu][Android] Add movable FAB (#30184)

--- packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/DevMenuReactRootViewContainer.kt
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/DevMenuReactRootViewContainer.kt
@@ -2,11 +2,29 @@ package expo.modules.devmenu
 
 import android.content.Context
 import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
+import com.facebook.react.ReactRootView
+import expo.modules.devmenu.fab.MovableFloatingActionButton
+
+/**
+ * We want to hide the FAB behind the feature flag for now.
+ */
+private const val enableFAB = false
 
 class DevMenuReactRootViewContainer(context: Context) : FrameLayout(context) {
+  private val fab by lazy { MovableFloatingActionButton(context) }
+
   override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
     DevMenuManager.onTouchEvent(ev)
     return super.dispatchTouchEvent(ev)
+  }
+
+  override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) {
+    super.addView(child, index, params)
+    if (enableFAB && child is ReactRootView) {
+      addView(fab)
+    }
   }
 }
