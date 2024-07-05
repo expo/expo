0ca6996c423ee53e15d57c2815af07fe0ddae647
Thibault Malbranche <malbranche.thibault@gmail.com>
2024-07-05 15:58:05 +0200

    [font] Allow woff and woff2 for iOS (#30220)

--- packages/expo-font/plugin/build/withFonts.d.ts
+++ packages/expo-font/plugin/build/withFonts.d.ts
@@ -1,6 +1,12 @@
 import { ConfigPlugin } from 'expo/config-plugins';
 export type FontProps = {
     fonts?: string[];
+    android?: {
+        fonts?: string[];
+    };
+    ios?: {
+        fonts?: string[];
+    };
 };
 declare const _default: ConfigPlugin<FontProps>;
 export default _default;
