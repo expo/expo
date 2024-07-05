6ef55f5ec02042ca6143f15316661867899a7602
Vojtech Novak <vonovak@gmail.com>
2024-07-05 12:27:14 +0200

    lint

--- packages/expo-calendar/android/src/main/java/expo/modules/calendar/dialogs/CreateEventIntentResult.kt
+++ packages/expo-calendar/android/src/main/java/expo/modules/calendar/dialogs/CreateEventIntentResult.kt
@@ -2,4 +2,4 @@ package expo.modules.calendar.dialogs
 
 import expo.modules.kotlin.records.Field
 
-class CreateEventIntentResult(@Field val id: String? = null): ViewEventIntentResult()
+class CreateEventIntentResult(@Field val id: String? = null) : ViewEventIntentResult()
