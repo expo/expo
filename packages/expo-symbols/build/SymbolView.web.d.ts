7cedc44bc9aa12b29a4c441d1835cc000712f1c3
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2024-07-07 00:42:12 +0100

    [symbols] Fix import error on web (#30249)

---
+++ packages/expo-symbols/build/SymbolView.web.d.ts
@@ -0,0 +1,3 @@
+import { SymbolViewProps } from './SymbolModule.types';
+export declare function SymbolView(props: SymbolViewProps): import("react").ReactNode;
+//# sourceMappingURL=SymbolView.web.d.ts.map
