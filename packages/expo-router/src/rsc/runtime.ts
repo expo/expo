7c5859b26de00c85dbd6ccfeeb962594f6e7a694
evanbacon <bacon@expo.io>
2024-07-09 15:52:59 -0500

    fix types

--- packages/expo-router/src/rsc/runtime.ts
+++ packages/expo-router/src/rsc/runtime.ts
@@ -5,5 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+declare var __METRO_GLOBAL_PREFIX__: string;
+
 globalThis.__webpack_chunk_load__ = global[`${__METRO_GLOBAL_PREFIX__}__loadBundleAsync`];
 globalThis.__webpack_require__ = global[`${__METRO_GLOBAL_PREFIX__}__r`];
