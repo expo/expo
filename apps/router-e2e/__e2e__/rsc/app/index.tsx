7a437f247de2a4be321f540a971ca237e896809d
evanbacon <bacon@expo.io>
2024-07-10 20:43:40 -0500

    add e2e web test

--- apps/router-e2e/__e2e__/rsc/app/index.tsx
+++ apps/router-e2e/__e2e__/rsc/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from '../lib/react-native';
 export default function Page() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hey!</Text>
+      <Text testID="main-text">Hey!</Text>
     </View>
   );
 }
