89e95bc648506448e27b2d80d44e2e08851ae4d8
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2024-07-11 13:54:30 +0200

    [video] Make the Picture in Picture support configurable (#30068)
    # Why
    
    We should make it possible to configure if the config plugin should add
    the `audio` key into `UIBackgroundModes` in `Info.plist`, so that app's
    which don't use PiP don't get rejected from the App Store.
    
    Fixes https://github.com/expo/expo/issues/30013
    
    
    # How
    
    Made the config plugin configurable, updated the docs and native Android
    code to work without PiP permission.
    
    # Test Plan
    
    Tested in Bare Expo on Android and iOS

--- packages/expo-video/android/src/main/java/expo/modules/video/VideoExceptions.kt
+++ packages/expo-video/android/src/main/java/expo/modules/video/VideoExceptions.kt
@@ -15,6 +15,9 @@ internal class MethodUnsupportedException(methodName: String) :
 internal class PictureInPictureEnterException(message: String?) :
   CodedException("Failed to enter Picture in Picture mode${message?.let { ". $message" } ?: ""}")
 
+internal class PictureInPictureConfigurationException :
+  CodedException("Current activity does not support picture-in-picture. Make sure you have configured the `expo-video` config plugin correctly.")
+
 internal class PictureInPictureUnsupportedException :
   CodedException("Picture in Picture mode is not supported on this device")
 
