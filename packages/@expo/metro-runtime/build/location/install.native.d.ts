db699a29a45ace6e85c9ba90dc5acc99b1991198
evanbacon <bacon@expo.io>
2024-07-11 21:48:39 -0500

    update

--- packages/@expo/metro-runtime/build/location/install.native.d.ts
+++ packages/@expo/metro-runtime/build/location/install.native.d.ts
@@ -1,3 +1,8 @@
 import 'react-native/Libraries/Core/InitializeCore';
 import 'expo';
+export declare function wrapFetchWithWindowLocation(fetch: Function & {
+    __EXPO_BASE_URL_POLYFILLED?: boolean;
+}): Function & {
+    __EXPO_BASE_URL_POLYFILLED?: boolean | undefined;
+};
 //# sourceMappingURL=install.native.d.ts.map
