eaddd8b60ba644fcb0200861f20fca770f51006d
evanbacon <bacon@expo.io>
2024-07-11 22:08:32 -0500

    fixup

--- apps/router-e2e/metro.config.js
+++ apps/router-e2e/metro.config.js
@@ -25,4 +25,11 @@ config.resolver.blockList = [
 // Copied from expo-yarn-workspaces
 config.transformer.enableBabelRCLookup = false;
 
+config.transformer.getTransformOptions = () => ({
+  transform: {
+    experimentalImportSupport: require('getenv').boolish('EXPO_UNSTABLE_TREE_SHAKING', false),
+    inlineRequires: false,
+  },
+});
+
 module.exports = config;
