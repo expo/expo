ba9599ff99c6a034fc3cfe9369f013c4d5d5dc08
evanbacon <bacon@expo.io>
2024-07-15 22:23:36 -0500

    revert constants changes

--- packages/expo-constants/build/Constants.react-server.js
+++
@@ -1,5 +0,0 @@
-import { AppOwnership, ExecutionEnvironment, UserInterfaceIdiom, } from './Constants.types';
-export { AppOwnership, ExecutionEnvironment, UserInterfaceIdiom, };
-const constants = {};
-export default constants;
-//# sourceMappingURL=Constants.react-server.js.map
