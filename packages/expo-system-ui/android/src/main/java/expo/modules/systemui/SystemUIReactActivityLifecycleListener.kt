4cbeabec0b75e41fb580d99b62319a63d7e8ad66
Mathieu Acthernoene <zoontek@gmail.com>
2024-07-18 22:23:47 +0200

    Rollback test

--- packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIReactActivityLifecycleListener.kt
+++ packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIReactActivityLifecycleListener.kt
@@ -3,28 +3,12 @@ package expo.modules.systemui
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import expo.modules.core.AppConfig
+
 import expo.modules.core.interfaces.ReactActivityLifecycleListener
 import expo.modules.systemui.singletons.SystemUI
-import org.json.JSONObject
 
 class SystemUIReactActivityLifecycleListener : ReactActivityLifecycleListener {
   override fun onCreate(activity: Activity, savedInstanceState: Bundle?) {
-    val appConfigString = AppConfig.get(activity);
-
-    if (appConfigString.isNullOrEmpty()) {
-      Log.w("ExpoSystemUI", "Unable to read the app config")
-    } else {
-      val manifest = JSONObject(appConfigString)
-      val experiments = manifest.optJSONObject("experiments")
-      val edgeToEdge = experiments?.optBoolean("edgeToEdge")
-
-      if (edgeToEdge == true) {
-        Log.i("ExpoSystemUI", "Enable edge to edge")
-      }
-    }
-
     SystemUI.setUserInterfaceStyle(getUserInterfaceStyle(activity))
   }
 
