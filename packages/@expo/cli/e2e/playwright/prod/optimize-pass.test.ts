98732cc16e6d24017499eb152aba4af98bd2fed6
Evan Bacon <bacon@expo.io>
2024-07-23 11:44:44 -0500

    feat(router): add virtual client boundary (#30534)
    # Why
    
    - Split out of https://github.com/expo/expo/pull/30334
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/cli/e2e/playwright/prod/optimize-pass.test.ts
+++ packages/@expo/cli/e2e/playwright/prod/optimize-pass.test.ts
@@ -52,7 +52,7 @@ test.describe(inputDir, () => {
     const largestFile = fs.readFileSync(largest, 'utf8');
 
     // Sanity
-    expect(largestFile).toMatch(/__r\("index.js"\);/);
+    expect(largestFile).toMatch(/__r\("apps\/router-e2e\/index.js"\);/);
     // This icon will remain because tree shaking is disabled.
     expect(largestFile).toMatch(/test-icon-apple/);
     // This icon remains.
