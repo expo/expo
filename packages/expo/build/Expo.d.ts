288f912ecf91f0e4346b09239c4996b3029554b1
Cedric van Putten <github@cedric.dev>
2024-07-23 17:29:36 +0200

    chore(expo): rebuild files

--- packages/expo/build/Expo.d.ts
+++ packages/expo/build/Expo.d.ts
@@ -2,6 +2,6 @@ import './Expo.fx';
 export { disableErrorHandling } from './errors/ExpoErrorManager';
 export { default as registerRootComponent } from './launch/registerRootComponent';
 export { isRunningInExpoGo, getExpoGoProjectConfig } from './environment/ExpoGo';
-export { EventEmitter, SharedObject, SharedRef, NativeModule, requireNativeModule, requireOptionalNativeModule, requireNativeViewManager, reloadAppAsync, Platform, useReleasingSharedObject, } from 'expo-modules-core';
+export { EventEmitter, type EventSubscription, SharedObject, SharedRef, NativeModule, requireNativeModule, requireOptionalNativeModule, requireNativeViewManager, reloadAppAsync, Platform, useReleasingSharedObject, } from 'expo-modules-core';
 export { useEvent } from './hooks/useEvent';
 //# sourceMappingURL=Expo.d.ts.map
