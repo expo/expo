3222fa2c9edf3a46764fb10f5c79ad8a6c153255
evanbacon <bacon@expo.io>
2024-07-24 14:17:06 -0500

    Update metroOptions.test.ts

--- packages/@expo/cli/src/start/server/middleware/__tests__/metroOptions.test.ts
+++ packages/@expo/cli/src/start/server/middleware/__tests__/metroOptions.test.ts
@@ -28,7 +28,7 @@ describe(getMetroDirectBundleOptions, () => {
       })
     ).toEqual({
       customResolverOptions: {},
-      customTransformOptions: { preserveEnvVars: 'false', baseUrl: '/foo/' },
+      customTransformOptions: { baseUrl: '/foo/' },
       serializerOptions: {},
       dev: true,
       entryFile: '/index.js',
@@ -53,7 +53,7 @@ describe(getMetroDirectBundleOptions, () => {
       sourceUrl:
         'http://localhost:8081/index.js.bundle?platform=ios&dev=true&hot=false&serializer.map=true',
       customResolverOptions: {},
-      customTransformOptions: { preserveEnvVars: 'false' },
+      customTransformOptions: {},
       serializerOptions: {
         includeSourceMaps: true,
       },
