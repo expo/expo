d4a40cfa812d22fc046aabdf752c0c6d31904b0f
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2024-07-29 09:00:59 +0530

    SSR support for useColorScheme hook (default template)

--- templates/expo-template-default/hooks/useColorScheme.web.ts
+++ templates/expo-template-default/hooks/useColorScheme.web.ts
@@ -1,8 +1,27 @@
-// NOTE: The default React Native styling doesn't support server rendering.
-// Server rendered styles should not change between the first render of the HTML
-// and the first render on the client. Typically, web developers will use CSS media queries
-// to render different styles on the client and server, these aren't directly supported in React Native
-// but can be achieved using a styling library like Nativewind.
+import { useColorScheme as useRNColorScheme } from 'react-native'
+import { useState, useEffect } from 'react'
+
 export function useColorScheme() {
-  return 'light';
+  // NOTE: The default React Native styling doesn't support server rendering.
+  // Server rendered styles should not change between the first render of the HTML
+  // and the first render on the client. So, we need to ensure that the color scheme
+  // is only determined on the client side after hydration. This way, we avoid any
+  // mismatch in styles that could occur if the server-rendered styles were different
+  // from the client-rendered styles.
+  const hasHydrated = useHasHydrated()
+
+  const colorScheme = useRNColorScheme()
+
+  if (hasHydrated) return colorScheme
+  return 'light'
+}
+
+export const useHasHydrated = () => {
+  const [hasHydrated, setHasHydrated] = useState(false)
+
+  useEffect(() => {
+    setHasHydrated(true)
+  }, [])
+
+  return hasHydrated
 }
