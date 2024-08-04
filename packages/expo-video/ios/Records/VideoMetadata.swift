7c95fb13030978a37bbb4ecf6a3aa99086aae3d5
Alireza Hadjar <asiahadjar@gmail.com>
2024-08-04 19:55:15 +0330

    feat(expo-video): add artwork to video metadata

--- packages/expo-video/ios/Records/VideoMetadata.swift
+++ packages/expo-video/ios/Records/VideoMetadata.swift
@@ -9,5 +9,8 @@ internal struct VideoMetadata: Record {
 
   @Field
   var artist: String? = nil
+    
+  @Field
+  var artwork: String? = nil
 }
 // swiftlint:enable redundant_optional_initialization
