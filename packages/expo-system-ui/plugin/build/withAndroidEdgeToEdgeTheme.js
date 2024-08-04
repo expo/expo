375a881eeca67ff89ca0be12b75e35c55301d06c
Mathieu Acthernoene <zoontek@gmail.com>
2024-08-04 12:05:59 +0200

    Add withAndroidEdgeToEdgeTheme test

--- packages/expo-system-ui/plugin/build/withAndroidEdgeToEdgeTheme.js
+++ packages/expo-system-ui/plugin/build/withAndroidEdgeToEdgeTheme.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.withAndroidEdgeToEdgeTheme = void 0;
 const config_plugins_1 = require("expo/config-plugins");
 const withAndroidEdgeToEdgeTheme = (config) => {
-    return (0, config_plugins_1.withAndroidStyles)(config, async (config) => {
+    return (0, config_plugins_1.withAndroidStyles)(config, (config) => {
         const { experiments = {}, userInterfaceStyle = 'automatic' } = config;
         const { edgeToEdge = false } = experiments;
         config.modResults.resources.style = config.modResults.resources.style?.map((style) => {
             if (style.$.name === 'AppTheme') {
                 style.$.parent = edgeToEdge ? 'Theme.EdgeToEdge' : 'Theme.AppCompat.Light.NoActionBar';
-                style.item = style.item.filter((item) => {
+                style.item = (style.item ?? []).filter((item) => {
                     if (item.$.name === 'windowLightSystemBars') {
                         return false;
                     }
@@ -21,7 +21,7 @@ const withAndroidEdgeToEdgeTheme = (config) => {
                         item.$.name !== 'android:navigationBarColor' &&
                         item.$.name !== 'android:windowLightNavigationBar');
                 });
-                if (edgeToEdge) {
+                if (edgeToEdge && userInterfaceStyle !== 'automatic') {
                     style.item.push({
                         $: { name: 'windowLightSystemBars' },
                         _: String(userInterfaceStyle === 'light'),
---
+++ packages/expo-system-ui/plugin/src/__tests__/withAndroidEdgeToEdgeTheme-test.ts
@@ -0,0 +1,131 @@
+import { AndroidConfig, withAndroidStyles, XML } from 'expo/config-plugins';
+
+import { compileMockModWithResultsAsync } from './mockMods';
+import { withAndroidEdgeToEdgeTheme } from '../withAndroidEdgeToEdgeTheme';
+
+const { parseXMLAsync } = XML;
+
+jest.mock('expo/config-plugins', () => {
+  const plugins = jest.requireActual('expo/config-plugins');
+  return {
+    ...plugins,
+    withAndroidStyles: jest.fn(),
+  };
+});
+
+describe(withAndroidEdgeToEdgeTheme, () => {
+  it(`update AppTheme style parent when edgeToEdge is enabled`, async () => {
+    const { modResults } = await compileMockModWithResultsAsync(
+      { experiments: { edgeToEdge: true } },
+      {
+        plugin: withAndroidEdgeToEdgeTheme,
+        mod: withAndroidStyles,
+        modResults: (await parseXMLAsync(`
+<resources>
+  <style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar">
+  </style>
+</resources>
+`)) as AndroidConfig.Resources.ResourceXML,
+      }
+    );
+
+    expect(modResults.resources.style?.at(0)).toStrictEqual({
+      $: { name: 'AppTheme', parent: 'Theme.EdgeToEdge' },
+      item: [],
+    });
+  });
+
+  it(`add windowLightSystemBars if userInterfaceStyle is set`, async () => {
+    const { modResults: lightModResults } = await compileMockModWithResultsAsync(
+      {
+        userInterfaceStyle: 'light',
+        experiments: { edgeToEdge: true },
+      },
+      {
+        plugin: withAndroidEdgeToEdgeTheme,
+        mod: withAndroidStyles,
+        modResults: (await parseXMLAsync(`
+<resources>
+  <style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar">
+  </style>
+</resources>
+`)) as AndroidConfig.Resources.ResourceXML,
+      }
+    );
+
+    expect(lightModResults.resources.style?.at(0)).toStrictEqual({
+      $: { name: 'AppTheme', parent: 'Theme.EdgeToEdge' },
+      item: [{ $: { name: 'windowLightSystemBars' }, _: 'true' }],
+    });
+
+    const { modResults: darkModResults } = await compileMockModWithResultsAsync(
+      {
+        userInterfaceStyle: 'dark',
+        experiments: { edgeToEdge: true },
+      },
+      {
+        plugin: withAndroidEdgeToEdgeTheme,
+        mod: withAndroidStyles,
+        modResults: (await parseXMLAsync(`
+<resources>
+  <style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar">
+  </style>
+</resources>
+`)) as AndroidConfig.Resources.ResourceXML,
+      }
+    );
+
+    expect(darkModResults.resources.style?.at(0)).toStrictEqual({
+      $: { name: 'AppTheme', parent: 'Theme.EdgeToEdge' },
+      item: [{ $: { name: 'windowLightSystemBars' }, _: 'false' }],
+    });
+  });
+
+  it(`filter unwanted attributes`, async () => {
+    const { modResults } = await compileMockModWithResultsAsync(
+      { experiments: { edgeToEdge: true } },
+      {
+        plugin: withAndroidEdgeToEdgeTheme,
+        mod: withAndroidStyles,
+        modResults: (await parseXMLAsync(`
+<resources>
+  <style name="AppTheme" parent="Theme.AppCompat.Light.NoActionBar">
+    <item name="colorPrimary">@color/colorPrimary</item>
+    <item name="android:statusBarColor">@android:color/red</item>
+    <item name="android:windowLightStatusBar">false</item>
+    <item name="android:navigationBarColor">@android:color/red</item>
+  	<item name="android:windowLightStatusBar">false</item>
+  </style>
+</resources>
+`)) as AndroidConfig.Resources.ResourceXML,
+      }
+    );
+
+    expect(modResults.resources.style?.at(0)).toStrictEqual({
+      $: { name: 'AppTheme', parent: 'Theme.EdgeToEdge' },
+      item: [{ _: '@color/colorPrimary', $: { name: 'colorPrimary' } }],
+    });
+  });
+
+  it(`remove Theme.EdgeToEdge if edgeToEdge is disabled`, async () => {
+    const { modResults } = await compileMockModWithResultsAsync(
+      { experiments: { edgeToEdge: false } },
+      {
+        plugin: withAndroidEdgeToEdgeTheme,
+        mod: withAndroidStyles,
+        modResults: (await parseXMLAsync(`
+<resources>
+  <style name="AppTheme" parent="Theme.EdgeToEdge">
+    <item name="windowLightSystemBars">true</item>
+  </style>
+</resources>
+`)) as AndroidConfig.Resources.ResourceXML,
+      }
+    );
+
+    expect(modResults.resources.style?.at(0)).toStrictEqual({
+      $: { name: 'AppTheme', parent: 'Theme.AppCompat.Light.NoActionBar' },
+      item: [],
+    });
+  });
+});
