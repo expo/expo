49b648a1f98360f2ac4d7a6f41301116b785ef56
Mathieu Acthernoene <zoontek@gmail.com>
2024-08-04 12:05:59 +0200

    Add withAndroidEdgeToEdgeTheme test

--- packages/expo-system-ui/plugin/src/withAndroidEdgeToEdgeTheme.ts
+++ packages/expo-system-ui/plugin/src/withAndroidEdgeToEdgeTheme.ts
@@ -1,7 +1,7 @@
 import { ConfigPlugin, withAndroidStyles } from 'expo/config-plugins';
 
 export const withAndroidEdgeToEdgeTheme: ConfigPlugin = (config) => {
-  return withAndroidStyles(config, async (config) => {
+  return withAndroidStyles(config, (config) => {
     const { experiments = {}, userInterfaceStyle = 'automatic' } = config;
     const { edgeToEdge = false } = experiments;
 
@@ -10,7 +10,7 @@ export const withAndroidEdgeToEdgeTheme: ConfigPlugin = (config) => {
         if (style.$.name === 'AppTheme') {
           style.$.parent = edgeToEdge ? 'Theme.EdgeToEdge' : 'Theme.AppCompat.Light.NoActionBar';
 
-          style.item = style.item.filter((item) => {
+          style.item = (style.item ?? []).filter((item) => {
             if (item.$.name === 'windowLightSystemBars') {
               return false;
             }
@@ -26,7 +26,7 @@ export const withAndroidEdgeToEdgeTheme: ConfigPlugin = (config) => {
             );
           });
 
-          if (edgeToEdge) {
+          if (edgeToEdge && userInterfaceStyle !== 'automatic') {
             style.item.push({
               $: { name: 'windowLightSystemBars' },
               _: String(userInterfaceStyle === 'light'),
