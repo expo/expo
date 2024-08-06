942baf56525da811bcb520af34be74999f642075
Vojtech Novak <vonovak@gmail.com>
2024-08-06 19:30:05 +0200

    rename cache var

--- packages/expo-font/src/memory.ts
+++ packages/expo-font/src/memory.ts
@@ -3,14 +3,14 @@ import ExpoFontLoader from './ExpoFontLoader';
 export const loadPromises: { [name: string]: Promise<void> } = {};
 
 // cache the value on the js side for fast access to the fonts that are loaded
-let _cache: { [name: string]: boolean } = {};
+let cache: { [name: string]: boolean } = {};
 
 export function markLoaded(fontFamily: string) {
-  _cache[fontFamily] = true;
-};
+  cache[fontFamily] = true;
+}
 
 export const isLoadedInCache = (fontFamily: string) => {
-  return fontFamily in _cache;
+  return fontFamily in cache;
 };
 
 export const isLoadedNative = (fontFamily: string) => {
@@ -19,15 +19,15 @@ export const isLoadedNative = (fontFamily: string) => {
   } else {
     const loadedNativeFonts: string[] = ExpoFontLoader.loadedFonts;
     loadedNativeFonts.forEach((font) => {
-      _cache[font] = true;
+      cache[font] = true;
     });
-    return fontFamily in _cache;
+    return fontFamily in cache;
   }
 };
 
 export const purgeFontFamilyFromCache = (fontFamily: string) => {
-  delete _cache[fontFamily];
+  delete cache[fontFamily];
 };
 export const purgeCache = () => {
-  _cache = {};
+  cache = {};
 };
