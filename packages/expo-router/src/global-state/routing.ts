fa21c78edd7eaee23eb8fe6028cdd224f0bcb642
Mark Lawlor <mwlawlor@gmail.com>
2024-08-07 05:23:36 +1000

    Update packages/expo-router/src/global-state/routing.ts
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>

--- packages/expo-router/src/global-state/routing.ts
+++ packages/expo-router/src/global-state/routing.ts
@@ -85,7 +85,7 @@ export type LinkToOptions = {
 
   /**
    * Relative URL references are either relative to the directory or the document. By default, relative paths are relative to the document.
-   * @see: https://developer.mozilla.org/en-US/docs/Web/API/URL_API/Resolving_relative_references
+   * @see: [MDN's documentation on Resolving relative references to a URL](https://developer.mozilla.org/en-US/docs/Web/API/URL_API/Resolving_relative_references).
    */
   relativeToDirectory?: boolean;
 };
