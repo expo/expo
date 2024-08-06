7707c5d4e8a92b6f3145aea9fc893bb09926bb05
Cedric van Putten <github@cedric.dev>
2024-08-06 16:44:50 +0200

    refactor: move `react@19` workaround to `expo-modules-script` Jest preset

--- packages/jest-expo/src/preset/setup-web.js
+++ packages/jest-expo/src/preset/setup-web.js
@@ -4,6 +4,3 @@ global.__DEV__ = true;
 
 // Workaround undefined ShadowRoot in react-native-web
 global.ShadowRoot = function () {};
-
-// Installs the temporary workaround to disable the deprecation warning for `react-test-renderer@19+`.
-require('./setup-react-19');
