f0aa3182074fc4567a106ab427364add6cb3ecae
evanbacon <bacon@expo.io>
2024-08-06 20:11:09 -0500

    Update [...rsc]+api.ts

--- apps/router-e2e/__e2e__/01-rsc/app/_flight/[...rsc]+api.ts
+++ apps/router-e2e/__e2e__/01-rsc/app/_flight/[...rsc]+api.ts
@@ -11,20 +11,3 @@ const rscMiddleware = getRscMiddleware({
 });
 
 module.exports = rscMiddleware;
-
-// import { getRscMiddleware } from '@expo/server/build/middleware/rsc';
-// import { renderRscWithImportsAsync } from '@expo/server/build/middleware/rsc-standalone';
-
-// const distFolder = process.env.EXPO_SERVER_DIST;
-
-// const rscMiddleware = getRscMiddleware({
-//   config: {},
-//   baseUrl: '',
-//   rscPath: '/_flight/',
-//   renderRsc: renderRscWithImportsAsync.bind(null, distFolder, {
-//     router: () => require('expo-router/build/rsc/router/expo-definedRouter.js'),
-//     renderer: () => require('expo-router/build/rsc/rsc-renderer.js'),
-//   }),
-// });
-
-// module.exports = rscMiddleware;
