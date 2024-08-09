a2c216f70872677831ede97447dec9b675d6bc22
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2024-08-09 13:11:59 +0200

    Refactor expo-image-picker on iOS

--- packages/expo-image-picker/ios/ImagePickerPermissionRequesters.swift
+++ packages/expo-image-picker/ios/ImagePickerPermissionRequesters.swift
@@ -59,7 +59,6 @@ public class MediaLibraryWriteOnlyPermissionRequester: DefaultMediaLibraryPermis
     return "mediaLibraryWriteOnly"
   }
 
-  @available(iOS 14, *)
   override internal func accessLevel() -> PHAccessLevel {
     return PHAccessLevel.addOnly
   }
@@ -83,25 +82,14 @@ public class DefaultMediaLibraryPermissionRequester: NSObject {}
 extension DefaultMediaLibraryPermissionRequester {
   @objc
   public func requestPermissions(resolver resolve: @escaping EXPromiseResolveBlock, rejecter reject: EXPromiseRejectBlock) {
-    let authorizationHandler = { [weak self] (_: PHAuthorizationStatus) in
+    PHPhotoLibrary.requestAuthorization(for: self.accessLevel()) { [weak self] (_: PHAuthorizationStatus) in
       resolve(self?.getPermissions())
-    }
-    if #available(iOS 14.0, *) {
-      PHPhotoLibrary.requestAuthorization(for: self.accessLevel(), handler: authorizationHandler)
-    } else {
-      PHPhotoLibrary.requestAuthorization(authorizationHandler)
     }
   }
 
   @objc
   public func getPermissions() -> [AnyHashable: Any] {
-    var authorizationStatus: PHAuthorizationStatus
-    if #available(iOS 14.0, *) {
-      authorizationStatus = PHPhotoLibrary.authorizationStatus(for: self.accessLevel())
-    } else {
-      authorizationStatus = PHPhotoLibrary.authorizationStatus()
-    }
-
+    let authorizationStatus = PHPhotoLibrary.authorizationStatus(for: self.accessLevel())
     var status: EXPermissionStatus
     var scope: String
 
@@ -128,7 +116,6 @@ extension DefaultMediaLibraryPermissionRequester {
     ]
   }
 
-  @available(iOS 14, *)
   @objc
   internal func accessLevel() -> PHAccessLevel {
     return PHAccessLevel.readWrite
