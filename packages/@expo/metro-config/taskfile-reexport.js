87203bbac8fd7e0d2a00542f6e5914c991eb3036
Cedric van Putten <github@cedric.dev>
2024-08-09 14:13:25 +0200

    fix(metro-config): automate re-exporting all metro files

---
+++ packages/@expo/metro-config/taskfile-reexport.js
@@ -0,0 +1,36 @@
+/* eslint-env node */
+
+/**
+ * @typedef {Object} TaskrFile
+ * @property {string} dir - The directory of the file.
+ * @property {string} base - The (base) file name of the file.
+ * @property {import('node:buffer').Buffer} buffer - The contents of the file.
+ */
+
+/** Generate all files that re-exports a single package. */
+module.exports = function reexportPlugin(task) {
+  /** @param {TaskrFile} file */
+  task.plugin('reexportModule', { every: false }, function* (files, { packageName } = {}) {
+    for (const file of files) {
+      const fileRelativePath = `${file.dir}/${file.base}`.split(packageName).pop();
+
+      if (file.base.endsWith('.js')) {
+        file.data = createJsFile(packageName, fileRelativePath);
+      } else if (file.base.endsWith('.d.ts')) {
+        file.data = createTsDefinitionFile(packageName, fileRelativePath);
+      } else {
+        throw new Error(`Unknown file "${file.base}", can't create a re-exported module file.`);
+      }
+    }
+
+    this.$.log('> Re-exported files for', packageName);
+  });
+};
+
+function createJsFile(packageName, relativePath) {
+  return `module.exports = require('${packageName}${relativePath.replace('.js', '')}');\n`;
+}
+
+function createTsDefinitionFile(packageName, relativePath) {
+  return `export * from '${packageName}${relativePath.replace('.d.ts', '')}';\n`;
+}
---
+++ packages/@expo/metro-config/taskfile.js
@@ -0,0 +1,44 @@
+/* eslint-env node */
+import path from 'node:path';
+
+const ROOT_DIR = path.resolve(__dirname);
+
+const REEXPORT_PACKAGES = [
+  'metro',
+  'metro-babel-transformer',
+  'metro-cache',
+  'metro-cache-key',
+  'metro-config',
+  'metro-core',
+  'metro-file-map',
+  'metro-resolver',
+  'metro-runtime',
+  'metro-source-map',
+  'metro-transform-plugins',
+  'metro-transform-worker',
+];
+
+export default async function tasks(task) {
+  await task.serial(['clean', 'vendor']);
+}
+
+export async function clean(task) {
+  for (const packageName of REEXPORT_PACKAGES) {
+    await task.clear(path.join(ROOT_DIR, packageName));
+  }
+}
+
+export async function vendor(task) {
+  for (const packageName of REEXPORT_PACKAGES) {
+    const packageDir = path.dirname(require.resolve(`${packageName}/package.json`));
+    const outputDir = path.join(ROOT_DIR, packageName);
+
+    // Generate the re-exporting files and type definitions
+    await task
+      .source(`${packageDir}/src/**/*.{js,d.ts}`, {
+        ignore: ['**/*.flow.js', '**/__tests__/**', '**/__mocks__/**', '**/integration_tests/**'],
+      })
+      .reexportModule({ packageName, outputDir })
+      .target(outputDir);
+  }
+}
