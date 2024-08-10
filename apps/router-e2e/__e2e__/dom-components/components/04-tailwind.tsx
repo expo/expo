776faaeaf633c7a1f88631e28cdbdc2ee2f0f16b
evanbacon <bacon@expo.io>
2024-08-10 14:28:03 -0500

    update

--- apps/router-e2e/__e2e__/dom-components/components/04-tailwind.tsx
+++ apps/router-e2e/__e2e__/dom-components/components/04-tailwind.tsx
@@ -6,6 +6,9 @@ export default function Page() {
   return (
     <div className="bg-slate-100 rounded-xl">
       <p className="text-lg text-blue-400 font-medium">Welcome to Tailwind</p>
+      <p className="text-lg text-blue-400 font-medium">
+        DOM: {String(!!(typeof ReactNativeWebView !== 'undefined'))}
+      </p>
     </div>
   );
 }
--- packages/expo/src/dom/dom.web.ts
+++ packages/expo/src/dom/dom.web.ts
@@ -1,5 +1,4 @@
 // TODO: Maybe this could be a bundler global instead.
 export const IS_DOM =
-  typeof window !== 'undefined' &&
   // @ts-expect-error: Added via react-native-webview
-  typeof window.ReactNativeWebView !== 'undefined';
+  typeof ReactNativeWebView !== 'undefined';
