cbaa4835f34233a5eb81dc4ed369c0940db1732d
Mathieu Acthernoene <zoontek@gmail.com>
2024-08-12 10:40:14 +0200

    Adjust to Android API 35+ style (semi transparent navigation bar on no gesture navigation)

--- packages/expo-system-ui/android/src/main/java/expo/modules/systemui/singletons/SystemUI.kt
+++ packages/expo-system-ui/android/src/main/java/expo/modules/systemui/singletons/SystemUI.kt
@@ -1,10 +1,11 @@
 package expo.modules.systemui.singletons
 
 import android.app.Activity
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Build
 import android.util.Log
-import android.util.TypedValue
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.WindowCompat
@@ -44,47 +45,39 @@ object SystemUI {
     setUserInterfaceStyle(style, {}, { m -> Log.e(TAG, m) })
   }
 
+  private fun detectDarkMode(resources: Resources) =
+    resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
   @JvmStatic
   fun enableEdgeToEdge(activity: Activity) {
-    val typedValue = TypedValue()
     val window = activity.window
-    val insetsController = WindowInsetsControllerCompat(window, window.decorView)
-
-    val isAppearanceLightSystemBars = activity
-      .theme
-      .resolveAttribute(R.attr.windowLightSystemBars, typedValue, true) &&
-      typedValue.data != 0
+    val decorView = window.decorView
+    val isDarkMode = detectDarkMode(decorView.resources)
 
     WindowCompat.setDecorFitsSystemWindows(window, false)
 
     activity.runOnUiThread {
-      with(window) {
-        statusBarColor = Color.TRANSPARENT
-        insetsController.isAppearanceLightStatusBars = isAppearanceLightSystemBars
+      window.statusBarColor = Color.TRANSPARENT
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-          navigationBarColor = Color.TRANSPARENT
-          insetsController.isAppearanceLightNavigationBars = isAppearanceLightSystemBars
+      window.navigationBarColor = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> Color.TRANSPARENT
+        else -> R.color.systemBarBackgroundSemiTransparent
+      }
 
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            isStatusBarContrastEnforced = false
-            isNavigationBarContrastEnforced = false
-          }
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        window.isStatusBarContrastEnforced = false
+        window.isNavigationBarContrastEnforced = true
+      }
 
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            attributes = attributes.apply {
-              layoutInDisplayCutoutMode = when {
-                Build.VERSION.SDK_INT >= Build.VERSION_CODES.R ->
-                  WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
-                else -> WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
-              }
-            }
-          }
-        } else {
-          // The dark scrim color used in the platform.
-          // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/res/res/color/system_bar_background_semi_transparent.xml
-          // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/res/remote_color_resources_res/values/colors.xml;l=67
-          navigationBarColor = Color.argb(0x80, 0x1b, 0x1b, 0x1b)
+      WindowInsetsControllerCompat(window, decorView).run {
+        isAppearanceLightStatusBars = !isDarkMode
+        isAppearanceLightNavigationBars = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !isDarkMode // has no effect on API < 26
+      }
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        window.attributes.layoutInDisplayCutoutMode = when {
+          Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+          else -> WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
         }
       }
     }
