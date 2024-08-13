9cbb6e87dcf935080f3e0ddd87087168b19ef49f
Evan Bacon <bacon@expo.io>
2024-08-13 11:24:04 -0500

    chore: update e2e tests (#30967)
    # Why
    
    - Update tests to skip loading the default routes in client-only mode
    when RSC is enabled.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- apps/router-e2e/__e2e__/rsc/app/index.tsx
+++ apps/router-e2e/__e2e__/rsc/app/index.tsx
@@ -1,9 +1,10 @@
+import 'server-only';
 import { View, Image, Text, Button } from '../lib/react-native';
 
 export default function Page() {
   return (
     <View style={{ flex: 1, gap: 8, alignItems: 'center', justifyContent: 'center' }}>
-      <Text testID="main-text">Hey!</Text>
+      <Text testID="main-text">Hey RSC</Text>
       {/* local Metro asset */}
       <Image
         testID="main-image"
--- packages/@expo/cli/e2e/playwright/dev/rsc.test.ts
+++ packages/@expo/cli/e2e/playwright/dev/rsc.test.ts
@@ -48,6 +48,10 @@ test.describe(inputDir, () => {
 
     console.time('Open page');
 
+    const serverResponsePromise = page.waitForResponse((response) => {
+      return new URL(response.url()).pathname.startsWith('/_flight/web/index.txt');
+    });
+
     // Listen for console errors
     const errorLogs: string[] = [];
     page.on('console', (msg) => {
@@ -66,8 +70,12 @@ test.describe(inputDir, () => {
     await page.goto(expo.url);
     console.timeEnd('Open page');
 
+    await serverResponsePromise;
+
+    await page.waitForSelector('[data-testid="main-text"]');
+
     // Ensure the initial state is correct
-    await expect(page.locator('[data-testid="main-text"]')).toHaveText('Hey!');
+    await expect(page.locator('[data-testid="main-text"]')).toHaveText('Hey RSC');
 
     expect(errorLogs).toEqual([]);
     expect(errors).toEqual([]);
