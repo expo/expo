d645f0106ae021c46b1abe2d09f140094d1063d0
Gabriel Donadel <donadeldev@gmail.com>
2024-08-14 12:14:38 -0300

    Add changelog entry

--- packages/@expo/metro-runtime/build/LoadingView.native.js
+++ packages/@expo/metro-runtime/build/LoadingView.native.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let LoadingView;
 try {
-    LoadingView = require('react-native/Libraries/Utilities/LoadingView').default;
+    LoadingView = require('react-native/Libraries/Utilities/LoadingView');
 }
 catch {
     // In react-native 0.75.0 LoadingView was renamed to DevLoadingView
-    LoadingView = require('react-native/Libraries/Utilities/DevLoadingView').default;
+    LoadingView = require('react-native/Libraries/Utilities/DevLoadingView');
 }
 exports.default = LoadingView;
 //# sourceMappingURL=LoadingView.native.js.map
--- packages/@expo/metro-runtime/src/LoadingView.native.ts
+++ packages/@expo/metro-runtime/src/LoadingView.native.ts
@@ -1,10 +1,10 @@
 let LoadingView: typeof import('react-native/Libraries/Utilities/LoadingView').default;
 
 try {
-  LoadingView = require('react-native/Libraries/Utilities/LoadingView').default;
+  LoadingView = require('react-native/Libraries/Utilities/LoadingView');
 } catch {
   // In react-native 0.75.0 LoadingView was renamed to DevLoadingView
-  LoadingView = require('react-native/Libraries/Utilities/DevLoadingView').default;
+  LoadingView = require('react-native/Libraries/Utilities/DevLoadingView');
 }
 
 export default LoadingView;
