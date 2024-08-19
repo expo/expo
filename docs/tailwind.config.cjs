74048f6d4b1b8ceee40a703d4851fe7981546a00
Simek <gosimek@gmail.com>
2024-08-19 19:19:37 +0200

    restore missing custom animation

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -34,8 +34,19 @@ module.exports = {
           transform: 'rotate(20deg)',
         },
       },
+      slideUpAndFadeIn: {
+        '0%': {
+          opacity: 0,
+          transform: 'translateY(16px)',
+        },
+        '100%': {
+          opacity: 1,
+          transform: 'translateY(0)',
+        },
+      },
     },
     animation: {
+      slideUpAndFadeIn: 'slideUpAndFadeIn 0.25s ease-out',
       wave: 'wave 0.25s ease-in-out 4',
     },
   }),
