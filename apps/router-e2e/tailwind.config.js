146907cd113ea011c5aeac64675e45ec7661299d
evanbacon <bacon@expo.io>
2024-08-20 13:48:25 +0200

    added tailwind plugins

--- apps/router-e2e/tailwind.config.js
+++ apps/router-e2e/tailwind.config.js
@@ -1,8 +1,54 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./__e2e__/{rsc,tailwind-postcss,dom-components}/**/*.{js,tsx,ts,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // sans: ['var(--font-geist-sans)']
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        blink: {
+          '0%': { opacity: 0.2 },
+          '20%': { opacity: 1 },
+          '100% ': { opacity: 0.2 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn .3s ease-in-out',
+        carousel: 'marquee 60s linear infinite',
+        blink: 'blink 1.4s both infinite',
+      },
+    },
   },
-  plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [
+    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/typography'),
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          'animation-delay': (value) => {
+            return {
+              'animation-delay': value,
+            };
+          },
+        },
+        {
+          values: theme('transitionDelay'),
+        }
+      );
+    }),
+  ],
 };
