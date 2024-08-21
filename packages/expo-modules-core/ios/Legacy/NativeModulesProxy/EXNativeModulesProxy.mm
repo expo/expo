a8d077228f86c8e6ca222f011b6445501f49e1e1
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2024-08-21 14:11:19 +0200

    [core][iOS] Clean up ExpoModulesCore.podspec (#31044)

--- packages/expo-modules-core/ios/Legacy/NativeModulesProxy/EXNativeModulesProxy.mm
+++ packages/expo-modules-core/ios/Legacy/NativeModulesProxy/EXNativeModulesProxy.mm
@@ -8,7 +8,7 @@
 #import <React/RCTModuleData.h>
 #import <React/RCTEventDispatcherProtocol.h>
 
-#ifdef RN_FABRIC_ENABLED
+#ifdef RCT_NEW_ARCH_ENABLED
 #import <React/RCTComponentViewFactory.h>
 #endif
 
@@ -351,7 +351,7 @@ RCT_EXPORT_METHOD(callMethod:(NSString *)moduleName methodNameOrKey:(id)methodNa
                                                                         bridge:bridge];
   componentDataByName[className] = componentData;
 
-#ifdef RN_FABRIC_ENABLED
+#ifdef RTC_NEW_ARCH_ENABLED
   Class viewClass = [ExpoFabricView makeViewClassForAppContext:_appContext className:className];
   [[RCTComponentViewFactory currentComponentViewFactory] registerComponentViewClass:viewClass];
 #endif
