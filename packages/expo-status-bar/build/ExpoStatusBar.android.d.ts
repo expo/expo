d75cda3cb5bceebd11a0aa18cf1d8acb058f27ff
Evan Bacon <bacon@expo.io>
2024-08-22 21:04:31 +0200

    chore(expo-status-bar): reduce package (#31088)
    # Why
    
    - expo-status-bar has very little functionality compared to the amount
    of code it contains. The package is used by +95% of projects so this is
    a meaningful amount of bloat. We should probably consider moving to
    `expo/status-bar`.
    - Combined all modules to a single file for native to reduce the number
    of bundler resolutions that are required for a base app.
    - Ship as typescript code.
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/expo-status-bar/build/ExpoStatusBar.android.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { StatusBarProps } from './StatusBar.types';
-export default function ExpoStatusBar(props: StatusBarProps): React.JSX.Element;
-//# sourceMappingURL=ExpoStatusBar.android.d.ts.map
