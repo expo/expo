743201d1283caf6a22e9d76ad59e709481da0e8d
Vojtech Novak <vonovak@gmail.com>
2024-08-27 17:03:13 +0200

    [notifications] remove toLegacyPromise

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/Exceptions.kt
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/Exceptions.kt
@@ -7,16 +7,3 @@ class ModuleNotFoundException(moduleClass: KClass<*>) :
   CodedException(message = "$moduleClass module not found")
 
 class NotificationWasAlreadyHandledException(val id: String) : CodedException("Failed to handle notification $id, it has already been handled.")
-
-fun expo.modules.kotlin.Promise.toLegacyPromise(): expo.modules.core.Promise {
-  val newPromise = this
-  return object : expo.modules.core.Promise {
-    override fun resolve(value: Any?) {
-      newPromise.resolve(value)
-    }
-
-    override fun reject(c: String?, m: String?, e: Throwable?) {
-      newPromise.reject(c ?: "unknown", m, e)
-    }
-  }
-}
