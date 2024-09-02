bac2beb3e38708d1f41f844c53aa05f6318dc366
Kudo Chien <kudo@expo.dev>
2024-09-02 22:30:06 +0800

    Apply suggestions from code review
    Co-authored-by: Tomasz Sapeta <tomasz.sapeta@swmansion.com>

--- packages/expo-sqlite/build/ExpoSQLiteNext.js
+++ packages/expo-sqlite/build/ExpoSQLiteNext.js
@@ -1,3 +1,3 @@
-import { requireNativeModule } from 'expo-modules-core';
+import { requireNativeModule } from 'expo';
 export default requireNativeModule('ExpoSQLiteNext');
 //# sourceMappingURL=ExpoSQLiteNext.js.map
