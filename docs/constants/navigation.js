dbbb0511c3fe79dd3e4f7317cb503f11bf2bf9da
Brent Vatne <brentvatne@gmail.com>
2024-09-06 12:54:01 -0700

    More shuffling stuff around

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -341,6 +341,7 @@ const general = [
     makePage('eas-update/getting-started.mdx'),
     makeGroup('Previews', [
       makePage('eas-update/develop-faster.mdx'),
+      makePage('eas-update/expo-dev-client.mdx'),
       makePage('eas-update/github-actions.mdx'),
     ]),
     makeGroup('Deployment', [
@@ -350,8 +351,6 @@ const general = [
     ]),
     makeGroup('Troubleshooting', [
       makePage('eas-update/debug.mdx'),
-      makePage('eas-update/expo-dev-client.mdx'),
-      makePage('eas-update/build-locally.mdx'),
       makePage('eas-update/error-recovery.mdx'),
     ]),
     makeGroup('Concepts', [
@@ -360,7 +359,6 @@ const general = [
       makePage('eas-update/runtime-versions.mdx'),
     ]),
     makeGroup('Reference', [
-      makePage('eas-update/faq.mdx'),
       makePage('eas-update/optimize-assets.mdx'),
       makePage('eas-update/environment-variables.mdx'),
       makePage('eas-update/code-signing.mdx'),
@@ -370,6 +368,8 @@ const general = [
       makePage('eas-update/migrate-from-classic-updates.mdx'),
       makePage('eas-update/codepush.mdx'),
       makePage('eas-update/updating-your-app.mdx'),
+      makePage('eas-update/build-locally.mdx'),
+      makePage('eas-update/faq.mdx'),
     ]),
   ]),
   makeSection('EAS Metadata', [
