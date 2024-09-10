c9672e0a96e4d24e0af30ebaa92715355e82f26c
betomoedano <betomoedano@outlook.com>
2024-09-10 15:25:59 -0500

    [doctor] Add checkEasignore flag as suggested from code review

--- packages/expo-doctor/src/utils/files.ts
+++ packages/expo-doctor/src/utils/files.ts
@@ -10,25 +10,33 @@ const GITIGNORE_FILENAME = '.gitignore';
 /**
  * Checks if a file exists and is not ignored by EAS (.easignore) or git (.gitignore).
  * @param filePath The path to the file to check.
+ * @param checkEasignore Whether to check .easignore or not (defaults to true).
  * @returns `true` if the file exists and is not ignored.
  */
-export async function existsAndIsNotIgnoredAsync(filePath: string): Promise<boolean> {
-  return fs.existsSync(filePath) && !(await isFileIgnoredAsync(filePath));
+export async function existsAndIsNotIgnoredAsync(
+  filePath: string,
+  checkEasignore: boolean = true
+): Promise<boolean> {
+  return fs.existsSync(filePath) && !(await isFileIgnoredAsync(filePath, checkEasignore));
 }
 
 /**
  * Checks if a file is ignored by EAS (.easignore) or git (.gitignore).
  * Prioritizes .easignore if it exists, otherwise falls back to .gitignore.
  * @param filePath The path to the file to check.
+ * @param checkEasignore Whether to check .easignore or not (defaults to true).
  * @returns `true` if the file is ignored, `false` otherwise.
  */
-export async function isFileIgnoredAsync(filePath: string): Promise<boolean> {
+export async function isFileIgnoredAsync(
+  filePath: string,
+  checkEasignore: boolean = true
+): Promise<boolean> {
   const rootPath = await getRootPathAsync();
   const easIgnorePath = path.join(rootPath, EASIGNORE_FILENAME);
   const gitIgnorePath = path.join(rootPath, GITIGNORE_FILENAME);
 
   try {
-    if (fs.existsSync(easIgnorePath)) {
+    if (fs.existsSync(easIgnorePath) && checkEasignore) {
       return isFileIgnoredByRules(filePath, easIgnorePath, rootPath);
     } else if (fs.existsSync(gitIgnorePath)) {
       await spawnAsync('git', ['check-ignore', '-q', filePath], {
