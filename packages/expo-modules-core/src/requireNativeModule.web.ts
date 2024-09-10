80b7b7e9b3362d384f9172121fcdf54b2323af2d
Kudo Chien <kudo@expo.dev>
2024-09-10 19:28:37 +0800

    [core][haptics][sqlite] loose web native modules exports (#31294)
    loose and align web implementation exports as native to support DOM components when using `@expo/dom-webview` as jsi proxy
    
    - [haptics][sqlite] remove empty web exports
    - [core] `requireOptionalNativeModule` will export jsi proxy when using `@expo/dom-webview`

--- packages/expo-modules-core/src/requireNativeModule.web.ts
+++ packages/expo-modules-core/src/requireNativeModule.web.ts
@@ -1,7 +1,17 @@
-export function requireNativeModule(moduleName: string) {
-  throw new Error(`Cannot find native module '${moduleName}'`);
+export function requireNativeModule<ModuleType = any>(moduleName: string): ModuleType {
+  const nativeModule = requireOptionalNativeModule<ModuleType>(moduleName);
+
+  if (!nativeModule) {
+    throw new Error(`Cannot find native module '${moduleName}'`);
+  }
+  return nativeModule;
 }
 
-export function requireOptionalNativeModule() {
+export function requireOptionalNativeModule<ModuleType = any>(
+  moduleName: string
+): ModuleType | null {
+  if (typeof globalThis.ExpoDomWebView === 'object' && globalThis?.expo?.modules != null) {
+    return globalThis.expo?.modules?.[moduleName] ?? null;
+  }
   return null;
 }
