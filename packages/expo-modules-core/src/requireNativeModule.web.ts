099dea591e81e8d45b661bb82ed2ebd5d64a285d
Kudo Chien <kudo@expo.dev>
2024-09-12 01:50:59 +0800

    Revert "[core][haptics][sqlite] loose web native modules exports (#31294)"
    This reverts commit da3b9636e681732268f940b22c8abface4b50e69.

--- packages/expo-modules-core/src/requireNativeModule.web.ts
+++ packages/expo-modules-core/src/requireNativeModule.web.ts
@@ -1,17 +1,7 @@
-export function requireNativeModule<ModuleType = any>(moduleName: string): ModuleType {
-  const nativeModule = requireOptionalNativeModule<ModuleType>(moduleName);
-
-  if (!nativeModule) {
-    throw new Error(`Cannot find native module '${moduleName}'`);
-  }
-  return nativeModule;
+export function requireNativeModule(moduleName: string) {
+  throw new Error(`Cannot find native module '${moduleName}'`);
 }
 
-export function requireOptionalNativeModule<ModuleType = any>(
-  moduleName: string
-): ModuleType | null {
-  if (typeof globalThis.ExpoDomWebView === 'object' && globalThis?.expo?.modules != null) {
-    return globalThis.expo?.modules?.[moduleName] ?? null;
-  }
+export function requireOptionalNativeModule() {
   return null;
 }
