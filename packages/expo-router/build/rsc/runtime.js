1ad85c72c97ca01ef44cec3069d06127026f3e31
evanbacon <bacon@expo.io>
2024-09-15 15:05:42 +0100

    add prod error handling on ios

--- packages/expo-router/build/rsc/runtime.js
+++ packages/expo-router/build/rsc/runtime.js
@@ -4,6 +4,28 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+// React Native's error handling is full of bugs which cause the app to crash in production.
+// We'll disable their handling in production native builds to ensure missing modules are shown to the user.
+const disableReactNativeMissingModuleHandling = !__DEV__ && process.env.EXPO_OS !== 'web';
 globalThis.__webpack_chunk_load__ = global[`${__METRO_GLOBAL_PREFIX__}__loadBundleAsync`];
-globalThis.__webpack_require__ = global[`${__METRO_GLOBAL_PREFIX__}__r`];
+globalThis.__webpack_require__ = (id) => {
+    // This logic can be tested by running a production iOS build without virtual client boundaries. This will result in all split chunks being missing and
+    // errors being thrown on RSC load.
+    const original = global.ErrorUtils.reportFatalError;
+    if (disableReactNativeMissingModuleHandling) {
+        global.ErrorUtils.reportFatalError = (err) => {
+            // Throw the error so the __r function exits as expected. The error will then be caught by the nearest error boundary.
+            throw err;
+        };
+    }
+    try {
+        return global[`${__METRO_GLOBAL_PREFIX__}__r`](id);
+    }
+    finally {
+        // Restore the original error handling.
+        if (disableReactNativeMissingModuleHandling) {
+            global.ErrorUtils.reportFatalError = original;
+        }
+    }
+};
 //# sourceMappingURL=runtime.js.map
