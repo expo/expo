c512fc3b6905d95ac3786567a49ea23f4b0bd427
Simek <gosimek@gmail.com>
2024-09-19 12:19:37 +0200

    improve fallbacks

--- docs/components/Head.tsx
+++ docs/components/Head.tsx
@@ -9,7 +9,7 @@ const BASE_TITLE = 'Expo Documentation';
 const BASE_DESCRIPTION = `Expo is an open-source platform for making universal native apps for Android, iOS, and the web with JavaScript and React.`;
 
 const Head = ({ title, description, canonicalUrl, children }: HeadProps) => {
-  const OGImageURL = `${BASE_OG_URL}&title=${encodeURIComponent(title ?? '')}&description=${encodeURIComponent(description ?? '')}`;
+  const OGImageURL = `${BASE_OG_URL}&title=${encodeURIComponent(title ?? BASE_TITLE)}&description=${encodeURIComponent(description ?? BASE_DESCRIPTION)}`;
 
   return (
     <NextHead>
