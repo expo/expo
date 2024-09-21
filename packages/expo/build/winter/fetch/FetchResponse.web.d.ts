13f9e9821db1edafce8ae869da9697a8f1e82178
Cedric van Putten <github@cedric.dev>
2024-09-21 17:47:18 +0200

    refactor(expo): avoid exposing `FetchResponse` in `expo/winter/fetch/index.ts`

--- packages/expo/build/winter/fetch/FetchResponse.web.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const FetchResponse: {
-    new (body?: BodyInit | null | undefined, init?: ResponseInit | undefined): Response;
-    prototype: Response;
-    error(): Response;
-    redirect(url: string | URL, status?: number | undefined): Response;
-};
-//# sourceMappingURL=FetchResponse.web.d.ts.map
--- packages/expo/src/winter/fetch/FetchResponse.web.ts
+++
@@ -1,1 +0,0 @@
-export const FetchResponse = globalThis.Response;
