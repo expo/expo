b4d1653277432e656c4cb93f6a43ff2c57db1fd1
Simek <gosimek@gmail.com>
2024-09-24 15:56:47 +0200

    [tools][docs] update TypeDoc, fix issues, regenerate data

--- packages/expo-constants/src/Constants.types.ts
+++ packages/expo-constants/src/Constants.types.ts
@@ -226,7 +226,7 @@ export interface NativeConstants {
 export interface Constants extends NativeConstants {
   /**
    * @hidden
-   * @warning do not use this property. Use `manifest` by default.
+   * > **Warning**: Do not use this property. Use `manifest` by default.
    *
    * In certain cases accessing manifest via this property
    * suppresses important warning about missing manifest.
@@ -234,7 +234,7 @@ export interface Constants extends NativeConstants {
   __unsafeNoWarnManifest?: EmbeddedManifest;
   /**
    * @hidden
-   * @warning do not use this property. Use `manifest2` by default.
+   * > **Warning**: Do not use this property. Use `manifest2` by default.
    *
    * In certain cases accessing manifest via this property
    * suppresses important warning about missing manifest.
