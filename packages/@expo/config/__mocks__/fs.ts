46096a74c48870038253b1973237bb1fdfef472b
Cedric van Putten <github@cedric.dev>
2024-09-25 17:42:00 +0200

    test(config): ensure `node:fs` is mocked within the test

--- packages/@expo/config/__mocks__/fs.ts
+++ packages/@expo/config/__mocks__/fs.ts
@@ -1,4 +1,4 @@
 module.exports = require('memfs').fs;
 
 // NOTE(cedric): workaround to also mock `node:fs`
-jest.mock('node:fs', () => require('memfs').fs);
+jest.mock('node:fs', () => require('fs'));
