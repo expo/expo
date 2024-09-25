f7bc74aa787e09c1fa23d7370a311f3af59a4b81
Cedric van Putten <github@cedric.dev>
2024-09-25 17:04:52 +0200

    chore(expo-updates): mock `node:fs` when mocking `fs` for `@expo/json-file`

--- packages/expo-updates/cli/__mocks__/fs.js
+++ packages/expo-updates/cli/__mocks__/fs.js
@@ -1,1 +1,6 @@
-module.exports = require('memfs').fs;
+const { fs } = require('memfs');
+
+module.exports = fs;
+
+// NOTE(cedric): workaround to also mock `node:fs`
+jest.mock('node:fs', () => require('memfs').fs);
