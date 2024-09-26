3fab96dc1174faefa6b2573f5bc61a7a90a8866a
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2024-09-26 11:26:15 -0700

    Remove no-op font preprocessor

--- packages/expo/src/Expo.fx.tsx
+++ packages/expo/src/Expo.fx.tsx
@@ -2,16 +2,8 @@
 import './winter';
 import 'expo-asset';
 
-import * as Font from 'expo-font';
-import { StyleSheet } from 'react-native';
-
 import { isRunningInExpoGo } from './environment/ExpoGo';
 import { createErrorHandler } from './errors/ExpoErrorManager';
-
-// If expo-font is installed and the style preprocessor is available, use it to parse fonts.
-if (StyleSheet.setStyleAttributePreprocessor) {
-  StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
-}
 
 if (isRunningInExpoGo()) {
   // set up some improvements to commonly logged error messages stemming from react-native
