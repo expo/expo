de1dad9ad553dde4b3df59cb44b1bb38b746ba76
aleqsio <mikucki@gmail.com>
2024-09-26 14:41:36 +0200

    Update types

--- packages/expo-file-system/src/next/ExpoFileSystem.types.ts
+++ packages/expo-file-system/src/next/ExpoFileSystem.types.ts
@@ -1,8 +1,16 @@
 export declare class Directory {
+  /**
+   * Creates an instance of a directory.
+   * @param uris -  An array of: `file:///` string URIs, `File` instances, `Directory` instances representing an arbitrary location on the file system. The location does not need to exist, or it may already contain a file.
+   * @example
+   * ```ts
+   * const directory = new Directory("file:///path/to/directory");
+   * ```
+   */
   constructor(...uris: (string | File | Directory)[]);
 
   /**
-   * Represents the directory URI.
+   * Represents the directory URI. The field is read-only, but it may change as a result of calling some methods such as `move`.
    */
   readonly uri: string;
 
@@ -13,43 +21,57 @@ export declare class Directory {
   validatePath(): void;
 
   /**
-   * Deletes a directory.
+   * Deletes a directory. Also deletes all files and directories inside the directory.
+   *
+   * @throws Error if the directory does not exist or cannot be deleted.
    */
   delete(): void;
 
   /**
    * A boolean representing if a directory exists. `true` if the directory exists, `false` otherwise.
+   * Also `false` if the application does not have read access to the file.
    */
   exists: boolean;
 
   /**
-   * Creates a directory.
+   * Creates a directory that the current uri points to.
+   *
+   * @throws Error if the containing folder doesn't exist, the application has no read access to it or the directory (or a file with the same path) already exists.
    */
   create(): void;
-
   /**
    * Copies a directory.
    */
   copy(destination: Directory | File);
-
   /**
    * Moves a directory. Updates the `uri` property that now points to the new location.
    */
   move(destination: Directory | File);
-
   /**
    * @hidden
    * Lists the contents of a directory. Should not be used directly, as it returns a list of paths.
    * This function is internal and will be removed in the future (when returning arrays of shared objects is supported).
    */
   listAsRecords(): { isDirectory: string; path: string }[];
+  /**
+   * Lists the contents of a directory.
+   */
+  list(): (Directory | File)[];
 }
 
+/**
+ * Represents a file on the file system.
+ */
 export declare class File {
+  /**
+   * Creates an instance of File.
+   *
+   * @param uri - A `file:///` URI representing an arbitrary location on the file system. The location does not need to exist, or it may already contain a directory.
+   */
   constructor(...uris: (string | File | Directory)[]);
 
   /**
-   * Represents the file URI.
+   * Represents the file URI. The field is read-only, but it may change as a result of calling some methods such as `move`.
    */
   readonly uri: string;
 
@@ -79,16 +101,21 @@ export declare class File {
 
   /**
    * Deletes a file.
+   *
+   * @throws Error if the directory does not exist or cannot be deleted.
    */
   delete(): void;
 
   /**
    * A boolean representing if a file exists. `true` if the file exists, `false` otherwise.
+   * Also `false` if the application does not have read access to the file.
    */
   exists: boolean;
 
   /**
    * Creates a file.
+   *
+   * @throws Error if the containing folder doesn't exist, the application has no read access to it or the file (or directory with the same path) already exists.
    */
   create(): void;
 
@@ -103,23 +130,16 @@ export declare class File {
   move(destination: Directory | File);
 
   /**
-   * A static method that downloads a file from the network.
+   * Downloads a file from the network.
    * @param url - The URL of the file to download.
-   * @param destination - The destination directory or file. If a directory is provided, the resulting filename will be determined based on the response headers.
+   * @param destination - The destination directory or file. If a destination is provided, the resulting filename will be determined based on the response headers.
    * @returns A promise that resolves to the downloaded file.
-   *
-   * @example
-   * ```ts
-   * const file = await File.downloadFileAsync("https://example.com/image.png", new Directory(Paths.document));
-   * ```
    */
   static downloadFileAsync(url: string, destination: Directory | File): Promise<File>;
-
   /**
    * A size of the file in bytes. Null if the file does not exist or it cannot be read.
    */
   size: number | null;
-
   /**
    * An md5 hash of the file. Null if the file does not exist or it cannot be read.
    */
