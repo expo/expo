0b739b22bf7ce312cd0399a7147f2f11da7f75ef
Evan Bacon <bacon@expo.io>
2024-09-26 11:08:40 +0200

    feat(cli): eager bundle JS during run:ios (#31670)
    # Why
    
    - iOS builds are disproportionately longer than JS bundles and have
    substantially less user-defined complexity at scale, so we should bundle
    the JS first to optimize for failure cases being revealed sooner.
    - Xcode run scripts distort the error results making it harder to debug
    bundler errors. This change runs the bundling code plainly in the
    terminal process so you see the exact error information.
    - Xcode run scripts execute `export:embed` in a shell environment that
    does not contain the user's entire path, meaning global scripts are not
    easily available. This makes server publishing more difficult. By
    swapping the bundling order, we can bundle and publish the server, then
    embed the client assets later.
    - 1/n of auto server deployment. https://github.com/expo/expo/pull/31663
    https://github.com/expo/expo/pull/31475
    https://github.com/expo/universe/pull/16877 and [WIP
    branch](https://github.com/expo/expo/compare/%40evanbacon/cli/export-embed-deploy).
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    - Attempt to bundle the exact same results before the build, store a
    hash of the options so we can reconcile later. If the hash misses, then
    we'll bundle again as expected but with the Metro cache preserved.
    - Add new `--eager` flag which can be used like `npx expo export:embed
    --eager --platform ios` to bundle the app plainly before the native
    build. This can be used in tools like EAS Build to replicate the "throw
    errors early and clearly" behavior of `npx expo run`.
    - Fix deallocating dev server instances.
    - Ultimately we'll want a version of this that can be used by default in
    EAS Build, it's unclear what the best way forward for that is right now.
    - Didn't implement the Android version of this, but it'll likely be
    identical.
    - [x] Add hermes detection based on the Pod source.
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    - `npx expo run:ios --configuration Release` -> Should see bundling,
    then building, then skipped bundle step.
      - JS errors will throw plainly at the beginning of the build.
      - Builds from Xcode and EAS Build will be unchanged.
    
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    
    ---------
    
    Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

--- packages/@expo/cli/src/export/embed/exportEmbedAsync.ts
+++ packages/@expo/cli/src/export/embed/exportEmbedAsync.ts
@@ -17,7 +17,7 @@ import type { BundleOptions } from 'metro/src/shared/types';
 import path from 'path';
 import resolveFrom from 'resolve-from';
 
-import { Options } from './resolveOptions';
+import { deserializeEagerKey, getExportEmbedOptionsKey, Options } from './resolveOptions';
 import { isExecutingFromXcodebuild, logMetroErrorInXcode } from './xcodeCompilerLogger';
 import { Log } from '../../log';
 import { DevServerManager } from '../../start/server/DevServerManager';
@@ -31,7 +31,7 @@ import {
 } from '../../start/server/middleware/DomComponentsMiddleware';
 import { getMetroDirectBundleOptionsForExpoConfig } from '../../start/server/middleware/metroOptions';
 import { stripAnsi } from '../../utils/ansi';
-import { removeAsync } from '../../utils/dir';
+import { copyAsync, removeAsync } from '../../utils/dir';
 import { env } from '../../utils/env';
 import { setNodeEnv } from '../../utils/nodeEnv';
 import { isEnableHermesManaged } from '../exportHermes';
@@ -74,6 +74,44 @@ export async function exportEmbedAsync(projectRoot: string, options: Options) {
   setNodeEnv(options.dev ? 'development' : 'production');
   require('@expo/env').load(projectRoot);
 
+  // This is an optimized codepath that can occur during `npx expo run` and does not occur during builds from Xcode or Android Studio.
+  // Here we reconcile a bundle pass that was run before the native build process. This order can fail faster and is show better errors since the logs won't be obscured by Xcode and Android Studio.
+  // This path is also used for automatically deploying server bundles to a remote host.
+  const eagerBundleOptions = env.__EXPO_EAGER_BUNDLE_OPTIONS
+    ? deserializeEagerKey(env.__EXPO_EAGER_BUNDLE_OPTIONS)
+    : null;
+  if (eagerBundleOptions) {
+    // Get the cache key for the current process to compare against the eager key.
+    const inputKey = getExportEmbedOptionsKey(options);
+
+    // If the app was bundled previously in the same process, then we should reuse the Metro cache.
+    options.resetCache = false;
+
+    if (eagerBundleOptions.key === inputKey) {
+      // Copy the eager bundleOutput and assets to the new locations.
+      await removeAsync(options.bundleOutput);
+
+      copyAsync(eagerBundleOptions.options.bundleOutput, options.bundleOutput);
+
+      if (eagerBundleOptions.options.assetsDest && options.assetsDest) {
+        copyAsync(eagerBundleOptions.options.assetsDest, options.assetsDest);
+      }
+
+      console.log('info: Copied output to binary:', options.bundleOutput);
+      return;
+    }
+    // TODO: sourcemapOutput is set on Android but not during eager. This is tolerable since it doesn't invalidate the Metro cache.
+    console.log('  Eager key:', eagerBundleOptions.key);
+    console.log('Request key:', inputKey);
+
+    // TODO: We may want an analytic event here in the future to understand when this happens.
+    console.warn('warning: Eager bundle does not match new options, bundling again.');
+  }
+
+  return exportEmbedInternalAsync(projectRoot, options);
+}
+
+export async function exportEmbedInternalAsync(projectRoot: string, options: Options) {
   // Ensure we delete the old bundle to trigger a failure if the bundle cannot be created.
   await removeAsync(options.bundleOutput);
 
