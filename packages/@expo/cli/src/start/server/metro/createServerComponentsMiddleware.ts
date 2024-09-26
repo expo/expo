1dc8cd1065544d4bd99b5ef9530ae4d66fa309a8
evanbacon <bacon@expo.io>
2024-09-27 00:30:53 +0200

    Update createServerComponentsMiddleware.ts

--- packages/@expo/cli/src/start/server/metro/createServerComponentsMiddleware.ts
+++ packages/@expo/cli/src/start/server/metro/createServerComponentsMiddleware.ts
@@ -306,7 +306,6 @@ export function createServerComponentsMiddleware(
     return renderRsc(
       {
         body,
-        searchParams,
         decodedBody,
         context: getRscRenderContext(platform),
         config: {},
