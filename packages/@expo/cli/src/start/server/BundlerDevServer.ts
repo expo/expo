587becca86fc4582a163c6d563e5ec8f155ed712
Cedric van Putten <me@bycedric.com>
2024-09-30 12:28:54 +0200

    refactor(cli): avoid retrying `development-sessions` requests (#31772)
    If the server is unavailable, or something else is going wrong in the
    development session calls, do not retry. It's on a best effort basis,
    and if it fails, there isn't a lot we can do.
    
    - Dropped `onError` callback in `DevelopmentSession`
    
    - In `packages/@expo/cli/src/api/updateDevelopmentSession.ts`
    - Create a module variable, like `let calledCount = 0;`
    - Modify both `updateDevelopmentSessionAsync` and
    `closeDevelopmentSessionAsync`
    - Add to the top of the function: `if (calledCount <= 50) throw new
    Error('Test')`
    - `DEBUG="expo:start:server:developmentSession" expod start
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/@expo/cli/src/start/server/BundlerDevServer.ts
+++ packages/@expo/cli/src/start/server/BundlerDevServer.ts
@@ -252,26 +252,12 @@ export abstract class BundlerDevServer {
   protected async startDevSessionAsync() {
     // This is used to make Expo Go open the project in either Expo Go, or the web browser.
     // Must come after ngrok (`startTunnelAsync`) setup.
-    this.devSession?.stopNotifying?.();
     this.devSession = new DevelopmentSession(
       this.projectRoot,
       // This URL will be used on external devices so the computer IP won't be relevant.
       this.isTargetingNative()
         ? this.getNativeRuntimeUrl()
-        : this.getDevServerUrl({ hostType: 'localhost' }),
-      () => {
-        // TODO: This appears to be happening consistently after an hour.
-        // We should investigate why this is happening and fix it on our servers.
-        // Log.error(
-        //   chalk.red(
-        //     '\nAn unexpected error occurred while updating the Dev Session API. This project will not appear in the "Development servers" section of the Expo Go app until this process has been restarted.'
-        //   )
-        // );
-        // Log.exception(error);
-        this.devSession?.closeAsync().catch((error) => {
-          debug('[dev-session] error closing: ' + error.message);
-        });
-      }
+        : this.getDevServerUrl({ hostType: 'localhost' })
     );
 
     await this.devSession.startAsync({
