9605fbe349d83b78d7dfafebb282faaaf39c1d7e
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2024-09-30 11:09:36 +0200

    Apply suggestions from code review
    Co-authored-by: Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>

--- packages/expo-modules-core/src/ts-declarations/SharedRef.ts
+++ packages/expo-modules-core/src/ts-declarations/SharedRef.ts
@@ -7,7 +7,7 @@ import type { SharedObject } from './SharedObject';
  *
  * For instance, `ImageRef` from `expo-image` references a [Drawable](https://developer.android.com/reference/android/graphics/drawable/Drawable)
  * on Android and an [UIImage](https://developer.apple.com/documentation/uikit/uiimage) on iOS. Since both types are common on these platforms,
- * different native modules can use them without depending on each other. In particular, this enables `expo-image-manipulator` to pass the resulted image
+ * different native modules can use them without depending on each other. In particular, this enables the `expo-image-manipulator` to pass the resulted image
  * directly to the image view from `expo-image` without any additional writes and reads from the file system.
  */
 export declare class SharedRef<TEventsMap extends EventsMap = Record<never, never>>
