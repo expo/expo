8d7547e4879eec47ad972fe0ea0261dcf1f626f0
aleqsio <mikucki@gmail.com>
2024-10-03 18:34:26 +0200

    rebuild

--- packages/expo-file-system/build/next/FileSystem.d.ts
+++ packages/expo-file-system/build/next/FileSystem.d.ts
@@ -31,7 +31,8 @@ export declare class File extends ExpoFileSystem.FileSystemFile {
      * File name. Includes the extension.
      */
     get name(): string;
-    stream(): ReadableStream<any>;
+    readableStream(): ReadableStream<Uint8Array>;
+    writableStream(): WritableStream<Uint8Array>;
 }
 /**
  * Represents a directory on the filesystem.
