fd6c467ed7023250a83097258c5401f3b27a2cf6
Kudo Chien <kudo@expo.dev>
2024-10-10 23:35:29 +0800

    [cli] throw error when bundling dom components when no webview installed

--- packages/@expo/cli/src/start/server/middleware/DomComponentsMiddleware.ts
+++ packages/@expo/cli/src/start/server/middleware/DomComponentsMiddleware.ts
@@ -15,6 +15,17 @@ const warnUnstable = memoize(() =>
   Log.warn('Using experimental DOM Components API. Production exports may not work as expected.')
 );
 
+const checkWebViewInstalled = memoize((projectRoot: string) => {
+  const webViewInstalled =
+    resolveFrom.silent(projectRoot, 'react-native-webview') ||
+    resolveFrom.silent(projectRoot, '@expo/dom-webview');
+  if (!webViewInstalled) {
+    throw new Error(
+      `To use DOM Components, you must install the 'react-native-webview' package. Run 'npx expo install react-native-webview' to install it.`
+    );
+  }
+});
+
 type CreateDomComponentsMiddlewareOptions = {
   /** The absolute metro or server root, used to calculate the relative dom entry path */
   metroRoot: string;
@@ -26,7 +37,9 @@ export function createDomComponentsMiddleware(
   { metroRoot, projectRoot }: CreateDomComponentsMiddlewareOptions,
   instanceMetroOptions: PickPartial<ExpoMetroOptions, 'mainModuleName' | 'platform' | 'bytecode'>
 ) {
-  return async (req: ServerRequest, res: ServerResponse, next: (err?: Error) => void) => {
+  const virtualEntry = resolveFrom(projectRoot, 'expo/dom/entry.js');
+
+  return (req: ServerRequest, res: ServerResponse, next: (err?: Error) => void) => {
     if (!req.url) return next();
 
     const url = coerceUrl(req.url);
@@ -45,11 +58,11 @@ export function createDomComponentsMiddleware(
       return res.end();
     }
 
+    checkWebViewInstalled(projectRoot);
     warnUnstable();
 
     // Generate a unique entry file for the webview.
     const generatedEntry = file.startsWith('file://') ? fileURLToFilePath(file) : file;
-    const virtualEntry = resolveFrom(projectRoot, 'expo/dom/entry.js');
     const relativeImport = './' + path.relative(path.dirname(virtualEntry), generatedEntry);
     // Create the script URL
     const requestUrlBase = `http://${req.headers.host}`;
