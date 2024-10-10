f372a690eae031f271ed9bbb5ed947eabf54833f
aleqsio <mikucki@gmail.com>
2024-10-10 12:20:15 +0200

    Change how we check for expo go

--- packages/expo-file-system/src/next/ExpoFileSystem.ts
+++ packages/expo-file-system/src/next/ExpoFileSystem.ts
@@ -1,4 +1,5 @@
-import { NativeModule, requireNativeModule, requireOptionalNativeModule } from 'expo-modules-core';
+import { isRunningInExpoGo } from 'expo';
+import { NativeModule, requireNativeModule } from 'expo-modules-core';
 
 import type { Directory, File } from './ExpoFileSystem.types';
 import ExpoGoFileSystemNextStub from './ExpoGoFileSystemNextStub';
@@ -9,7 +10,6 @@ declare class ExpoFileSystemNextModule extends NativeModule {
   downloadFileAsync(url: string, destination: File | Directory): Promise<string>;
 }
 
-const isExpoGo = requireOptionalNativeModule('ExponentConstants')?.appOwnership === 'expo';
-export default isExpoGo
+export default isRunningInExpoGo()
   ? (ExpoGoFileSystemNextStub as any as ExpoFileSystemNextModule)
   : requireNativeModule<ExpoFileSystemNextModule>('FileSystemNext');
