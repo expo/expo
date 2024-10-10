b3127c50b33130b9e7f7f2dab19e19d05e89f962
Cedric van Putten <github@cedric.dev>
2024-10-10 19:50:24 +0200

    test(cli): use correct asset url in bundle to validate on

--- packages/@expo/cli/e2e/__tests__/export/asset-prefix.test.ts
+++ packages/@expo/cli/e2e/__tests__/export/asset-prefix.test.ts
@@ -1,8 +1,8 @@
 /* eslint-env jest */
 import execa from 'execa';
+import fs from 'fs';
 import klawSync from 'klaw-sync';
 import path from 'path';
-import fs from 'fs';
 
 import { runExportSideEffects } from './export-side-effects';
 import { bin, getRouterE2ERoot } from '../utils';
@@ -17,7 +17,7 @@ describe('static-rendering with a custom asset prefix url', () => {
   it(
     'asset httpServerFileLocation contains assetPrefix',
     async () => {
-      await execa('node', [bin, 'export', '-p', 'web', '--max-workers=1', '--output-dir', outputName], {
+      await execa('node', [bin, 'export', '-p', 'web', '--output-dir', outputName], {
         cwd: projectRoot,
         env: {
           NODE_ENV: 'production',
@@ -29,17 +29,17 @@ describe('static-rendering with a custom asset prefix url', () => {
         },
       });
 
-      const bundleFile = klawSync(outputDir)
-        .find((entry) => entry.path.match(/\_expo\/static\/js\/web\/index-.*\.js/));
-
+      // Find the main bundle
+      const bundleFile = klawSync(outputDir).find((entry) =>
+        entry.path.match(/_expo\/static\/js\/web\/index-.*\.js/)
+      );
       expect(bundleFile).toBeDefined();
 
       // Load the bundle to test the asset data on
       const bundleContent = fs.readFileSync(bundleFile?.path!, 'utf8');
-
       // Ensure that our URL is listed as httpServerLocation in the JS
       expect(bundleContent).toMatch(
-        /uri\:"https\:\/\/cdn\.example\.dev\/assets\/__e2e__\/asset\/assets\/react-logo\.[a-z0-9]+\.png\:/i
+        /uri:"https:\/\/cdn\.example\.dev\/assets\/__e2e__\/asset\/assets\/react-logo\.[a-z0-9]+\.png"/i
       );
     },
     // Could take 45s depending on how fast the bundler resolves
