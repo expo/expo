7ddace08141a1c24468be661d90bd14f4329d394
aleqsio <mikucki@gmail.com>
2024-10-15 14:35:54 +0200

    Update module template

--- packages/expo-module-template/example/App.tsx
+++ packages/expo-module-template/example/App.tsx
@@ -1,20 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEvent } from 'expo';
+import <%- project.name %>, { <%- project.viewName %> } from '<%- project.slug %>';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import * as <%- project.name %> from '<%- project.slug %>';
 
 export default function App() {
+  const onChangePayload = useEvent(<%- project.name %>, "onChange");
+
   return (
-    <View style={styles.container}>
-      <Text>{<%- project.name %>.hello()}</Text>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Module API Example</Text>
+      <Group name="Constants">
+        <Text>{<%- project.name %>.PI}</Text>
+      </Group>
+      <Group name="Functions">
+        <Text>{<%- project.name %>.hello()}</Text>
+      </Group>
+      <Group name="Async functions">
+        <Button
+          title="Set value"
+          onPress={async () => {
+            await <%- project.name %>.setValueAsync("Hello from JS!");
+          }}
+        />
+      </Group>
+      <Group name="Events">
+        <Text>{onChangePayload?.value}</Text>
+      </Group>
+      <Group name="Views">
+        <<%- project.viewName %>
+          url="https://www.example.com"
+          onLoad={({ url }) => console.log(`Loaded: ${url}`)}
+        />
+      </Group>
+    </ScrollView>
+  );
+}
+
+function Group(props: { name: string; children: React.ReactNode }) {
+  return (
+    <View style={styles.group}>
+      <Text style={styles.groupHeader}>{props.name}</Text>
+      {props.children}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    fontSize: 30,
+    margin: 20,
+  },
+  groupHeader: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  group: {
+    margin: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eee",
   },
 });
--- packages/expo-module-template/ios/{%- project.name %}.podspec
+++ packages/expo-module-template/ios/{%- project.name %}.podspec
@@ -23,7 +23,6 @@ Pod::Spec.new do |s|
   # Swift/Objective-C compatibility
   s.pod_target_xcconfig = {
     'DEFINES_MODULE' => 'YES',
-    'SWIFT_COMPILATION_MODE' => 'wholemodule'
   }
 
   s.source_files = "**/*.{h,m,swift}"
