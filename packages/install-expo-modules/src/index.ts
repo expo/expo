expo/expo

commit dbcca8695a16cc4fb485db9b0d9ff855ff35258b

[workspace] update Commander dependency to prepare for future changes (#29603)

packages/install-expo-modules/src/index.ts
@@ -25,8 +25,6 @@ import { normalizeProjectRootAsync } from './utils/projectRoot';
 
 const packageJSON = require('../package.json');
 
-let inputProjectRoot: string = '';
-
 const program = new Command(packageJSON.name)
   .version(packageJSON.version)
   .arguments('<project-directory>')
@@ -34,11 +32,10 @@ const program = new Command(packageJSON.name)
   .description('Install expo-modules into your project')
   .option('-s, --sdk-version <version>', 'Install specified expo-modules sdk version')
   .option('--non-interactive', 'Disable interactive prompts')
-  .action((_inputProjectRoot: string) => (inputProjectRoot = _inputProjectRoot))
   .parse(process.argv);
 
 function getSdkVersionInfo(projectRoot: string): VersionInfo {
-  const { sdkVersion } = program;
+  const { sdkVersion } = program.opts();
   if (sdkVersion) {
     const versionInfo = getVersionInfo(sdkVersion);
     if (!versionInfo) {
@@ -60,7 +57,7 @@ async function promptUpgradeAgpVersionAsync(projectRoot: string, agpVersion: str
   }
 
   const deploymentTargetMessage = `The minimum Android Gradle Plugin version for Expo modules is ${agpVersion}. This tool will change your AGP version to ${agpVersion}.`;
-  if (program.nonInteractive) {
+  if (program.opts().nonInteractive) {
     console.log(chalk.yellow(`⚠️  ${deploymentTargetMessage}`));
     return true;
   } else {
@@ -85,7 +82,7 @@ async function promptUpgradeIosDeployTargetAsync(projectRoot: string, iosDeploym
   }
 
   const deploymentTargetMessage = `Expo modules minimum iOS requirement is ${iosDeploymentTarget}. This tool will change your iOS deployment target to ${iosDeploymentTarget}.`;
-  if (program.nonInteractive) {
+  if (program.opts().nonInteractive) {
     console.log(chalk.yellow(`⚠️  ${deploymentTargetMessage}`));
     return true;
   } else {
@@ -112,7 +109,7 @@ Using Expo CLI has some benefits over the the default CLI in bare React Native p
 ${learnMore('https://docs.expo.dev/bare/using-expo-cli/')}
 Do you want to install the Expo CLI integration?`;
 
-  if (program.nonInteractive) {
+  if (program.opts().nonInteractive) {
     return true;
   }
   const { value } = await prompts({
@@ -124,9 +121,10 @@ Do you want to install the Expo CLI integration?`;
   return !!value;
 }
 
-async function runAsync(programName: string) {
-  const { projectRoot, platformAndroid, platformIos } =
-    await normalizeProjectRootAsync(inputProjectRoot);
+async function runAsync() {
+  const { projectRoot, platformAndroid, platformIos } = await normalizeProjectRootAsync(
+    process.cwd()
+  );
 
   const {
     expoSdkVersion: sdkVersion,
@@ -200,7 +198,7 @@ async function runAsync(programName: string) {
 (async () => {
   program.parse(process.argv);
   try {
-    await runAsync(packageJSON.name);
+    await runAsync();
   } catch (e) {
     console.error('Uncaught Error', e);
     process.exit(1);

packages/pod-install/src/index.ts
@@ -11,8 +11,6 @@ import { learnMore } from './utils';
 
 const packageJSON = require('../package.json');
 
-let projectRoot: string = '';
-
 const program = new Command(packageJSON.name)
   .version(packageJSON.version)
   .arguments('<project-directory>')
@@ -20,23 +18,22 @@ const program = new Command(packageJSON.name)
   .description('Install pods in your project')
   .option('--quiet', 'Only print errors')
   .option('--non-interactive', 'Disable interactive prompts')
-  .action((inputProjectRoot: string) => (projectRoot = inputProjectRoot))
   .allowUnknownOption()
   .parse(process.argv);
 
 const info = (message: string) => {
-  if (!program.quiet) {
+  if (!program.opts().quiet) {
     console.log(message);
   }
 };
 
 async function runAsync(): Promise<void> {
-  projectRoot = resolve(projectRoot.trim());
-
   if (process.platform !== 'darwin') {
     info(chalk.red('CocoaPods is only supported on darwin machines'));
     return;
   }
+
+  let projectRoot = resolve(process.cwd());
 
   const possibleProjectRoot = CocoaPodsPackageManager.getPodProjectRoot(projectRoot);
   if (!possibleProjectRoot) {
@@ -64,7 +61,9 @@ async function runAsync(): Promise<void> {
   }
 
   if (!(await CocoaPodsPackageManager.isCLIInstalledAsync())) {
-    await CocoaPodsPackageManager.installCLIAsync({ nonInteractive: program.nonInteractive });
+    await CocoaPodsPackageManager.installCLIAsync({
+      nonInteractive: program.opts().nonInteractive,
+    });
   }
   const manager = new CocoaPodsPackageManager({ cwd: projectRoot });
   try {
@@ -84,7 +83,7 @@ async function runAsync(): Promise<void> {
   info('Scanning for pods...');
   try {
     await runAsync();
-    if (!program.quiet) {
+    if (!program.opts().quiet) {
       await shouldUpdate();
     }
   } catch (reason: any) {
@@ -97,7 +96,7 @@ async function runAsync(): Promise<void> {
       console.log(reason);
     }
     console.log();
-    if (!program.quiet) {
+    if (!program.opts().quiet) {
       await shouldUpdate();
     }
     process.exit(1);

packages/expo-module-scripts/bin/expo-module.js
@@ -1,30 +1,28 @@
 #!/usr/bin/env node
 'use strict';
 
-const commander = require('commander');
+const { program } = require('commander');
 const process = require('process');
 
-commander
-  // Common scripts
-  .command('configure', `Generate common configuration files`)
-  .command('readme', `Generate README`)
-  .command(
-    'typecheck',
-    `Type check the source TypeScript without emitting JS and watch for file changes`
-  )
-  .command('build', `Compile the source JS or TypeScript and watch for file changes`)
-  .command('lint', `Lint the files for syntax errors, style guidance, and common warnings`)
-  .command('test', `Run unit tests with an interactive watcher`)
-  .command('clean', `Removes compiled files`)
+program.command('configure', `Generate common configuration files`);
+program.command('readme', `Generate README`);
+program.command(
+  'typecheck',
+  `Type check the source TypeScript without emitting JS and watch for file changes`
+);
+program.command('build', `Compile the source JS or TypeScript and watch for file changes`);
+program.command('lint', `Lint the files for syntax errors, style guidance, and common warnings`);
+program.command('test', `Run unit tests with an interactive watcher`);
+program.command('clean', `Removes compiled files`);
 
-  // Lifecycle scripts
-  .command('prepare', `Scripts to run during the "prepare" phase`)
-  .command('prepublishOnly', `Scripts to run during the "prepublishOnly" phase`)
+// Lifecycle scripts
+program.command('prepare', `Scripts to run during the "prepare" phase`);
+program.command('prepublishOnly', `Scripts to run during the "prepublishOnly" phase`);
 
-  // Pass-through scripts
-  .command('babel', `Runs Babel CLI with the given arguments`)
-  .command('eslint', `Runs ESLint with the given arguments`)
-  .command('jest', `Runs Jest with the given arguments`)
-  .command('tsc', `Runs tsc with the given arguments`)
+// Pass-through scripts
+program.command('babel', `Runs Babel CLI with the given arguments`);
+program.command('eslint', `Runs ESLint with the given arguments`);
+program.command('jest', `Runs Jest with the given arguments`);
+program.command('tsc', `Runs tsc with the given arguments`);
 
-  .parse(process.argv);
+program.parse(process.argv);

packages/uri-scheme/src/CLI.ts
@@ -44,9 +44,9 @@ buildCommand('add', ['com.app', 'myapp'])
     try {
       const options = await parseArgsAsync(uri, args);
       await URIScheme.addAsync(options);
-      shouldUpdate();
+      await shouldUpdate();
     } catch (error) {
-      commandDidThrowAsync(error);
+      await commandDidThrowAsync(error);
     }
   });
 
@@ -62,9 +62,9 @@ buildCommand('remove', ['com.app', 'myapp'])
     try {
       const options = await parseArgsAsync(uri, args);
       await URIScheme.removeAsync(options);
-      shouldUpdate();
+      await shouldUpdate();
     } catch (error) {
-      commandDidThrowAsync(error);
+      await commandDidThrowAsync(error);
     }
   });
 
@@ -82,9 +82,9 @@ buildCommand('open', ['com.app://oauth', 'http://expo.dev'])
         androidPackage: args['package'],
         uri,
       });
-      shouldUpdate();
+      await shouldUpdate();
     } catch (error) {
-      commandDidThrowAsync(error);
+      await commandDidThrowAsync(error);
     }
   });
 
@@ -99,9 +99,9 @@ buildCommand('list')
     try {
       const options = await parseArgsAsync(uri, args);
       await URIScheme.listAsync(options);
-      shouldUpdate();
+      await shouldUpdate();
     } catch (error) {
-      commandDidThrowAsync(error);
+      await commandDidThrowAsync(error);
     }
   });
 

commit 7250a6a01404e86af73798862b1b84e3785a5fb9

[install-expo-modules] fix launch regression (#32054)
# Why

fixed a regression since #29603 where it requires a project-directory
argument after that.

# How

use an optional argument for the `[project-directory]`

packages/install-expo-modules/src/index.ts
@@ -28,8 +28,8 @@ const packageJSON = require('../package.json');
 
 const program = new Command(packageJSON.name)
   .version(packageJSON.version)
-  .arguments('<project-directory>')
-  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .arguments('[project-directory]')
+  .usage(`${chalk.green('[project-directory]')} [options]`)
   .description('Install expo-modules into your project')
   .option('-s, --sdk-version <version>', 'Install specified expo-modules sdk version')
   .option('--non-interactive', 'Disable interactive prompts')
@@ -119,7 +119,7 @@ Install the Expo CLI integration?`;
 
 async function runAsync() {
   const { projectRoot, platformAndroid, platformIos } = await normalizeProjectRootAsync(
-    process.cwd()
+    program.args[0] || process.cwd()
   );
 
   const {

packages/install-expo-modules/src/utils/projectRoot.ts
@@ -4,9 +4,9 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export async function normalizeProjectRootAsync(
-  projectRoot?: string
+  projectRoot: string
 ): Promise<{ projectRoot: string; platformAndroid: boolean; platformIos: boolean }> {
-  const root = path.dirname(findUpPackageJson(projectRoot ?? process.cwd()));
+  const root = path.dirname(findUpPackageJson(projectRoot));
   const platformAndroid = await pathExistsAsync(path.join(root, 'android'));
   const platformIos = await pathExistsAsync(path.join(root, 'ios'));
   return {