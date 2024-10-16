95dec52592e24b45ed056cc4599640fb6bd97726
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2024-10-16 04:55:34 +0200

    [video][android] Fix `FullscreenPlayerActivity` not closing after re-opening app via app icon (#32044)
    # Why
    
    When BareExpo is backgrounded and a fullscreen video is playing
    re-opening it using the app icon in the drawer will return to
    `MainActivity.kt` without closing the `FullscreenPlayerActivity`.
    
    # How
    
    All activities are now registered in `VideoManager` and closed when the
    `MainActivity` is resumed.
    
    # Test Plan
    
    Tested in BareExopo on a Pixel 8

--- packages/expo-video/android/src/main/java/expo/modules/video/VideoManager.kt
+++ packages/expo-video/android/src/main/java/expo/modules/video/VideoManager.kt
@@ -4,6 +4,7 @@ import androidx.annotation.OptIn
 import androidx.media3.common.util.UnstableApi
 import expo.modules.kotlin.AppContext
 import expo.modules.video.player.VideoPlayer
+import java.lang.ref.WeakReference
 
 // Helper class used to keep track of all existing VideoViews and VideoPlayers
 @OptIn(UnstableApi::class)
@@ -12,6 +13,7 @@ object VideoManager {
 
   // Used for sharing videoViews between VideoView and FullscreenPlayerActivity
   private var videoViews = mutableMapOf<String, VideoView>()
+  private var fullscreenPlayerActivities = mutableMapOf<String, WeakReference<FullscreenPlayerActivity>>()
 
   // Keeps track of all existing VideoPlayers, and whether they are attached to a VideoView
   private var videoPlayersToVideoViews = mutableMapOf<VideoPlayer, MutableList<VideoView>>()
@@ -44,6 +46,14 @@ object VideoManager {
     audioFocusManager.unregisterPlayer(videoPlayer)
   }
 
+  fun registerFullscreenPlayerActivity(id: String, fullscreenActivity: FullscreenPlayerActivity) {
+    fullscreenPlayerActivities[id] = WeakReference(fullscreenActivity)
+  }
+
+  fun unregisterFullscreenPlayerActivity(id: String) {
+    fullscreenPlayerActivities.remove(id)
+  }
+
   fun onVideoPlayerAttachedToView(videoPlayer: VideoPlayer, videoView: VideoView) {
     if (videoPlayersToVideoViews[videoPlayer]?.contains(videoView) == true) {
       return
@@ -73,6 +83,12 @@ object VideoManager {
   fun onAppForegrounded() {
     for (videoView in videoViews.values) {
       videoView.playerView.useController = videoView.useNativeControls
+    }
+
+    // Pressing the app icon will bring up the mainActivity instead of the fullscreen activity (at least for BareExpo)
+    // In this case we have to manually finish the fullscreen activity
+    for (fullscreenActivity in fullscreenPlayerActivities.values) {
+      fullscreenActivity.get()?.finish()
     }
   }
 
