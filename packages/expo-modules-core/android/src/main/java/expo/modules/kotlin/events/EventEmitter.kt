5fb44604b98cccf8719746ecfd15ef24eb5dd2aa
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2024-10-22 14:05:18 +0200

    [core][Android] Use event dispatcher with the `surfaceId` (#32227)

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/events/EventEmitter.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/events/EventEmitter.kt
@@ -1,5 +1,6 @@
 package expo.modules.kotlin.events
 
+import android.view.View
 import com.facebook.react.bridge.WritableMap
 import expo.modules.kotlin.records.Record
 
@@ -10,4 +11,5 @@ interface EventEmitter : expo.modules.core.interfaces.services.EventEmitter {
   fun emit(eventName: String, eventBody: Record?)
   fun emit(eventName: String, eventBody: Map<*, *>?)
   fun emit(viewId: Int, eventName: String, eventBody: WritableMap?, coalescingKey: Short? = null)
+  fun emit(view: View, eventName: String, eventBody: WritableMap?, coalescingKey: Short? = null)
 }
