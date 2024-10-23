8b6a5e32f821a343b356036aa74652ff6575660f
Will Schurman <wschurman@expo.io>
2024-10-23 09:15:33 -0700

    [expo-updates] Move event emitting responsibility to module (#32248)

--- packages/expo-updates/android/src/main/java/expo/modules/updates/events/UpdatesEventManager.kt
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/events/UpdatesEventManager.kt
@@ -1,31 +1,27 @@
 package expo.modules.updates.events
 
-import expo.modules.kotlin.events.EventEmitter
 import expo.modules.updates.logging.UpdatesErrorCode
 import expo.modules.updates.logging.UpdatesLogger
 import expo.modules.updates.statemachine.UpdatesStateContext
+import java.lang.ref.WeakReference
 
 class UpdatesEventManager(private val logger: UpdatesLogger) : IUpdatesEventManager {
-  override var eventEmitter: EventEmitter? = null
+  override var observer: WeakReference<IUpdatesEventManagerObserver>? = null
 
   override fun sendStateMachineContextEvent(context: UpdatesStateContext) {
-    val eventName = UpdatesJSEvent.StateChange.eventName
+    logger.debug("Sending state machine context to observer")
 
-    val eventEmitter = eventEmitter
-    if (eventEmitter == null) {
-      logger.info(
-        "Could not emit $eventName event; no event emitter was found.",
-        UpdatesErrorCode.JSRuntimeError
-      )
+    val observer = observer?.get() ?: run {
+      logger.debug("Unable to send state machine context to observer, no observer", UpdatesErrorCode.JSRuntimeError)
       return
     }
 
     try {
-      eventEmitter.emit(eventName, context.writableMap)
-      logger.info("Emitted event: name = $eventName")
+      observer.onStateMachineContextEvent(context)
+      logger.debug("Sent state machine context to observer")
     } catch (e: Exception) {
       logger.error(
-        "Could not emit $eventName event",
+        "Could not send state machine context to observer",
         e,
         UpdatesErrorCode.JSRuntimeError
       )
