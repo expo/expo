41a8f332f7a1d8f36e65e9b962253cf6c7d2cab0
Bartosz Kaszubowski <gosimek@gmail.com>
2024-10-23 15:46:00 +0200

    [docs] another batch of Emotion styling refactors (#32189)

--- docs/components/plugins/api/APISectionUtils.tsx
+++ docs/components/plugins/api/APISectionUtils.tsx
@@ -85,7 +85,7 @@ export const mdComponents: MDComponents = {
         {children}
       </PrismCodeBlock>
     ) : (
-      <CODE css={css({ display: 'inline' })}>{children}</CODE>
+      <CODE className="!inline">{children}</CODE>
     );
   },
   pre: ({ children }) => <>{children}</>,
@@ -666,8 +666,9 @@ export const listParams = (parameters: MethodParamData[]) =>
 
 export const renderDefaultValue = (defaultValue?: string) =>
   defaultValue && defaultValue !== '...' ? (
-    <div css={defaultValueContainerStyle}>
-      <DEMI theme="secondary">Default:</DEMI> <CODE className="!text-[90%]">{defaultValue}</CODE>
+    <div className="flex items-start gap-1">
+      <DEMI theme="secondary">Default:</DEMI>
+      <CODE className="!text-[90%]">{defaultValue}</CODE>
     </div>
   ) : undefined;
 
@@ -1022,15 +1023,6 @@ export const STYLES_NOT_EXPOSED_HEADER = css({
   display: 'inline-block',
 
   code: {
-    marginBottom: 0,
-  },
-});
-
-const defaultValueContainerStyle = css({
-  marginTop: spacing[2],
-  marginBottom: spacing[2],
-
-  '&:last-child': {
     marginBottom: 0,
   },
 });
