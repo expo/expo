e70a1053a0f51ae226753c7a76f48311af63cb5d
Bartosz Kaszubowski <gosimek@gmail.com>
2024-10-24 12:51:20 +0200

    [docs] refactor code and appearance of Tabs component (#32280)

--- docs/ui/components/Tabs/TabButton.tsx
+++ docs/ui/components/Tabs/TabButton.tsx
@@ -1,35 +1,78 @@
-import { css } from '@emotion/react';
-import { theme, typography } from '@expo/styleguide';
-import { spacing } from '@expo/styleguide-base';
-import { Tab as ReachTab, TabProps } from '@reach/tabs';
+import { mergeClasses } from '@expo/styleguide';
+import { Tab as ReachTab } from '@reach/tabs';
+import { motion, useReducedMotion } from 'framer-motion';
+import * as React from 'react';
+import { ComponentType, ReactElement } from 'react';
 
-type Props = TabProps & {
-  selected?: boolean;
+import { P } from '../Text';
+
+import { TextComponentProps } from '~/ui/components/Text/types';
+
+export type TabProps = {
+  label: string;
+  active: boolean;
+  href?: string;
+  icon?: ReactElement;
+  disabled?: boolean;
+  className?: string;
+  layoutId?: string;
+  rightSlot?: ReactElement;
+  theme?: 'default' | 'secondary';
+  LabelElement?: ComponentType<TextComponentProps>;
 };
 
-export const TabButton = ({ selected, ...props }: Props) => (
-  <ReachTab
-    {...props}
-    css={tabButtonStyles}
-    style={{
-      borderBottomColor: selected ? theme.palette.blue9 : 'transparent',
-      color: selected ? theme.text.default : theme.text.secondary,
-    }}
-  />
-);
+export function TabButton({
+  label,
+  active,
+  disabled,
+  icon,
+  className,
+  rightSlot,
+  layoutId,
+  theme = 'secondary',
+  LabelElement = P,
+}: TabProps) {
+  const shouldReduceMotion = useReducedMotion();
 
-const tabButtonStyles = css({
-  ...typography.fontSizes[15],
-  fontWeight: 500,
-  transition: 'all 0.05s ease 0s',
-  padding: `${spacing[2.5]}px ${spacing[6]}px ${spacing[2] - 1}px`,
-  border: 0,
-  borderBottom: '0.2rem solid transparent',
-  borderRight: `1px solid ${theme.border.default}`,
-  backgroundColor: 'transparent',
-  cursor: 'pointer',
-
-  '&:hover': {
-    backgroundColor: theme.background.subtle,
-  },
-});
+  return (
+    <div className="relative">
+      {active && (
+        <motion.div
+          layoutId={`active-indicator-${layoutId}`}
+          transition={{
+            ease: 'linear',
+            duration: shouldReduceMotion ? 0 : 0.2,
+          }}
+          className={mergeClasses(
+            'absolute inset-0 rounded-md border',
+            theme === 'default' && 'border-secondary bg-screen dark:bg-hover dark:drop-shadow-none',
+            theme === 'secondary' && 'border-button-secondary bg-default shadow-xs dark:bg-subtle'
+          )}
+        />
+      )}
+      <ReachTab
+        disabled={disabled}
+        className={mergeClasses(
+          'relative z-10 rounded-md transition-colors',
+          !active && theme === 'default' && 'hocus:bg-selected dark:hocus:bg-element',
+          !active && theme === 'secondary' && 'hocus:bg-element dark:hocus:bg-subtle',
+          className
+        )}>
+        <div
+          className={mergeClasses(
+            'flex items-center gap-2 px-4 py-1.5',
+            rightSlot && 'w-full justify-between'
+          )}>
+          {icon}
+          <LabelElement
+            theme={active ? 'default' : 'tertiary'}
+            weight="medium"
+            className="transition-colors max-md-gutters:text-sm max-sm-gutters:text-xs">
+            {label}
+          </LabelElement>
+          {rightSlot}
+        </div>
+      </ReachTab>
+    </div>
+  );
+}
