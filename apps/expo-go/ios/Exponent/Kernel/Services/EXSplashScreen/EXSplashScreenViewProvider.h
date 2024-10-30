0d55509a22f6466f3d3ac5931f57466ae10f4bf1
Alan Hughes <alan@expo.io>
2024-10-30 17:54:15 +0000

    [ios][expo-go] Add legacy splashscreen

---
+++ apps/expo-go/ios/Exponent/Kernel/Services/EXSplashScreen/EXSplashScreenViewProvider.h
@@ -0,0 +1,14 @@
+// Copyright © 2018 650 Industries. All rights reserved.
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol EXSplashScreenViewProvider
+
+- (UIView *)createSplashScreenView;
+
+@end
+
+NS_ASSUME_NONNULL_END
