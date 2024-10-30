e45dcbe57426841016f16bae546e4844a3838450
Alan Hughes <alan@expo.io>
2024-10-30 18:03:59 +0000

    update autolinking

--- apps/expo-go/android/expoview/src/main/java/host/exp/exponent/experience/splashscreen/ManagedAppSplashScreenViewController.kt
+++ apps/expo-go/android/expoview/src/main/java/host/exp/exponent/experience/splashscreen/ManagedAppSplashScreenViewController.kt
@@ -8,8 +8,8 @@ import android.os.Looper
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.snackbar.Snackbar
-import expo.modules.splashscreen.BuildConfig
 import host.exp.exponent.experience.splashscreen.legacy.SplashScreenViewController
+import host.exp.expoview.BuildConfig
 
 class ManagedAppSplashScreenViewController(
   activity: Activity,
