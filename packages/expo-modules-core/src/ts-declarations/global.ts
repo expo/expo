63b63942d42519e7914199010549cf08ff6c6b2a
Tomasz Sapeta <tomasz.sapeta@swmansion.com>
2024-10-31 11:53:36 +0000

    [core] Make TS declaration for `process` an interface (#32464)

--- packages/expo-modules-core/src/ts-declarations/global.ts
+++ packages/expo-modules-core/src/ts-declarations/global.ts
@@ -60,6 +60,27 @@ type ViewConfig = {
   directEventTypes: Record<string, { registrationName: string }>;
 };
 
+export interface ExpoProcess {
+  env: {
+    NODE_ENV: string;
+    /** Used in `@expo/metro-runtime`. */
+    EXPO_DEV_SERVER_ORIGIN?: string;
+
+    EXPO_ROUTER_IMPORT_MODE?: string;
+    EXPO_ROUTER_ABS_APP_ROOT?: string;
+    EXPO_ROUTER_APP_ROOT?: string;
+
+    /** Maps to the `experiments.baseUrl` property in the project Expo config. This is injected by `babel-preset-expo` and supports automatic cache invalidation. */
+    EXPO_BASE_URL?: string;
+
+    /** Build-time representation of the `Platform.OS` value that the current JavaScript was bundled for. Does not support platform shaking wrapped require statements. */
+    EXPO_OS?: string;
+
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
 /* eslint-disable no-var */
 
 declare global {
@@ -69,24 +90,5 @@ declare global {
    */
   var expo: ExpoGlobal;
 
-  const process: {
-    env: {
-      NODE_ENV: string;
-      /** Used in `@expo/metro-runtime`. */
-      EXPO_DEV_SERVER_ORIGIN?: string;
-
-      EXPO_ROUTER_IMPORT_MODE?: string;
-      EXPO_ROUTER_ABS_APP_ROOT?: string;
-      EXPO_ROUTER_APP_ROOT?: string;
-
-      /** Maps to the `experiments.baseUrl` property in the project Expo config. This is injected by `babel-preset-expo` and supports automatic cache invalidation. */
-      EXPO_BASE_URL?: string;
-
-      /** Build-time representation of the `Platform.OS` value that the current JavaScript was bundled for. Does not support platform shaking wrapped require statements. */
-      EXPO_OS?: string;
-
-      [key: string]: any;
-    };
-    [key: string]: any;
-  };
+  var process: ExpoProcess;
 }
