789181f3ccda3ae00c728461bcf3d913cf81a72d
Will Schurman <wschurman@expo.io>
2024-10-31 12:20:39 -0700

    [fingerprint] Add ability to diff two fingeprint files to CLI (#32488)

--- packages/@expo/fingerprint/cli/src/cli.ts
+++ packages/@expo/fingerprint/cli/src/cli.ts
@@ -2,26 +2,38 @@
 import fs from 'fs';
 import path from 'path';
 
+import { Fingerprint } from '../../build/Fingerprint.types.js';
+
+function readFingerprintFile(path: string): Fingerprint {
+  try {
+    return JSON.parse(fs.readFileSync(path, 'utf-8'));
+  } catch (e: any) {
+    console.log(`Unable to read fingerprint file ${path}: ${e.message}`);
+    process.exit(1);
+  }
+}
+
 (async () => {
   const Fingerprint = await import('../../build/index.js');
 
-  if (process.argv.length !== 3 && process.argv.length !== 4) {
-    console.log(`Usage: ${path.basename(process.argv[1])} projectRoot [fingerprintFileToDiff]`);
+  if (process.argv.length !== 3 && process.argv.length !== 4 && process.argv.length !== 5) {
+    console.log(
+      `Usage: ${path.basename(process.argv[1])} projectRoot [fingerprintFile1ToDiff] [fingerprintFile2ToDiff]`
+    );
     process.exit(1);
   }
 
-  let comparatorFingerprint;
-  if (process.argv.length === 4) {
-    const comparator = process.argv[3];
-    try {
-      comparatorFingerprint = JSON.parse(fs.readFileSync(comparator, 'utf-8'));
-    } catch (e: any) {
-      console.log(`Unable to diff with fingerprint file ${comparator}: ${e.message}`);
-      process.exit(1);
-    }
-  }
-
   const projectRoot = process.argv[2];
+
+  const fingerprintFile1ToDiff = process.argv[3];
+  const fingerprintFile2ToDiff = process.argv[4];
+
+  const fingeprint1ToDiff = fingerprintFile1ToDiff
+    ? readFingerprintFile(fingerprintFile1ToDiff)
+    : undefined;
+  const fingeprint2ToDiff = fingerprintFile2ToDiff
+    ? readFingerprintFile(fingerprintFile2ToDiff)
+    : undefined;
 
   const options = {
     debug: !!process.env.DEBUG,
@@ -30,9 +42,12 @@ import path from 'path';
       : undefined,
   };
   try {
-    if (comparatorFingerprint) {
+    if (fingeprint1ToDiff && fingeprint2ToDiff) {
+      const diff = Fingerprint.diffFingerprints(fingeprint1ToDiff, fingeprint2ToDiff);
+      console.log(JSON.stringify(diff, null, 2));
+    } else if (fingeprint1ToDiff) {
       const diff = await Fingerprint.diffFingerprintChangesAsync(
-        comparatorFingerprint,
+        fingeprint1ToDiff,
         projectRoot,
         options
       );
