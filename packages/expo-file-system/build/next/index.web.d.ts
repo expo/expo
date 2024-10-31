190a402b84edf365fa67e77a42ddacbff829016d
aleqsio <mikucki@gmail.com>
2024-10-31 11:26:14 +0100

    Add basic fs next stub to make bare expo work on web.

---
+++ packages/expo-file-system/build/next/index.web.d.ts
@@ -0,0 +1,8 @@
+declare class Directory {
+}
+declare class File {
+}
+declare class Paths {
+}
+export { Directory, File, Paths };
+//# sourceMappingURL=index.web.d.ts.map
---
+++ packages/expo-file-system/src/next/index.web.ts
@@ -0,0 +1,5 @@
+class Directory {}
+class File {}
+class Paths {}
+
+export { Directory, File, Paths };
