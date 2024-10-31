7a3b4de1a00640c8d8a4fdb2c0ea1061a4451825
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2024-10-31 00:30:40 +0000

    [ios][expo-go] Add legacy splashscreen (#32476)

---
+++ apps/expo-go/ios/Exponent/Kernel/Services/EXSplashScreen/EXSplashScreenModule.h
@@ -0,0 +1,9 @@
+//  Copyright © 2018 650 Industries. All rights reserved.
+
+#import <ExpoModulesCore/EXExportedModule.h>
+#import <ExpoModulesCore/EXAppLifecycleListener.h>
+#import <ExpoModulesCore/EXModuleRegistryConsumer.h>
+
+@interface EXSplashScreenModule : EXExportedModule <EXModuleRegistryConsumer, EXAppLifecycleListener>
+
+@end
