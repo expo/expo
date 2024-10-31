b87a65715566a2162e2eae561f618f8045b20b1c
Will Schurman <wschurman@gmail.com>
2024-10-30 19:39:23 -0700

    [fingerprint] Add ability to diff two fingeprint files to CLI

--- packages/@expo/fingerprint/cli/build/cli.js
+++ packages/@expo/fingerprint/cli/build/cli.js
@@ -6,24 +6,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-(async () => {
-    const Fingerprint = await import('../../build/index.js');
-    if (process.argv.length !== 3 && process.argv.length !== 4) {
-        console.log(`Usage: ${path_1.default.basename(process.argv[1])} projectRoot [fingerprintFileToDiff]`);
+function readFingerprintFile(path) {
+    try {
+        return JSON.parse(fs_1.default.readFileSync(path, 'utf-8'));
+    }
+    catch (e) {
+        console.log(`Unable to read fingerprint file ${path}: ${e.message}`);
         process.exit(1);
     }
-    let comparatorFingerprint;
-    if (process.argv.length === 4) {
-        const comparator = process.argv[3];
-        try {
-            comparatorFingerprint = JSON.parse(fs_1.default.readFileSync(comparator, 'utf-8'));
-        }
-        catch (e) {
-            console.log(`Unable to diff with fingerprint file ${comparator}: ${e.message}`);
-            process.exit(1);
-        }
+}
+(async () => {
+    const Fingerprint = await import('../../build/index.js');
+    if (process.argv.length !== 3 && process.argv.length !== 4 && process.argv.length !== 5) {
+        console.log(`Usage: ${path_1.default.basename(process.argv[1])} projectRoot [fingerprintFile1ToDiff] [fingerprintFile2ToDiff]`);
+        process.exit(1);
     }
     const projectRoot = process.argv[2];
+    const fingerprintFile1ToDiff = process.argv[3];
+    const fingerprintFile2ToDiff = process.argv[4];
+    const fingeprint1ToDiff = fingerprintFile1ToDiff
+        ? readFingerprintFile(fingerprintFile1ToDiff)
+        : undefined;
+    const fingeprint2ToDiff = fingerprintFile2ToDiff
+        ? readFingerprintFile(fingerprintFile2ToDiff)
+        : undefined;
     const options = {
         debug: !!process.env.DEBUG,
         useRNCoreAutolinkingFromExpo: process.env.USE_RNCORE_AUTOLINKING_FROM_EXPO
@@ -31,8 +37,12 @@ const path_1 = __importDefault(require("path"));
             : undefined,
     };
     try {
-        if (comparatorFingerprint) {
-            const diff = await Fingerprint.diffFingerprintChangesAsync(comparatorFingerprint, projectRoot, options);
+        if (fingeprint1ToDiff && fingeprint2ToDiff) {
+            const diff = Fingerprint.diffFingerprints(fingeprint1ToDiff, fingeprint2ToDiff);
+            console.log(JSON.stringify(diff, null, 2));
+        }
+        else if (fingeprint1ToDiff) {
+            const diff = await Fingerprint.diffFingerprintChangesAsync(fingeprint1ToDiff, projectRoot, options);
             console.log(JSON.stringify(diff, null, 2));
         }
         else {
