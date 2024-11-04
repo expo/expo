01b3426f20cae827a382da95bfc42a3af38e7648
Kudo Chien <kudo@expo.dev>
2024-11-05 03:38:54 +0800

    Apply suggestions from code review

--- packages/expo/src/dom/base.ts
+++ packages/expo/src/dom/base.ts
@@ -9,22 +9,6 @@ export function getBaseURL(): string {
   }
 
   // Serving from updates
-  const ExpoUpdates = globalThis.expo?.modules?.['ExpoUpdates'] as
-    | import('expo-updates').ExpoUpdatesModule
-    | undefined;
-  const updatesIsInstalledAndEnabled = ExpoUpdates?.isEnabled ?? false;
-  const updatesIsEmbeddedLaunch = ExpoUpdates?.isEmbeddedLaunch ?? false;
-  const shouldServeDomFromUpdates = updatesIsInstalledAndEnabled && !updatesIsEmbeddedLaunch;
-  // If updates is installed and enabled, and we're not running from an embedded launch, we should serve the DOM Components from the `.expo-internal` directory
-  if (shouldServeDomFromUpdates) {
-    const localAssets = ExpoUpdates?.localAssets ?? {};
-    const anyLocalAsset = Object.values(localAssets)[0];
-    if (anyLocalAsset) {
-      // Try to get the `.expo-internal` directory from the first local asset
-      cachedBaseUrl = anyLocalAsset.slice(0, anyLocalAsset.lastIndexOf('/'));
-      return cachedBaseUrl;
-    }
-  }
 
   if (process.env.EXPO_OS === 'web') {
     cachedBaseUrl = process.env.EXPO_BASE_URL ?? '';
@@ -48,4 +32,26 @@ export function getBaseURL(): string {
   const devServer = getDevServer();
   cachedBaseUrl = new URL('/_expo/@dom', devServer.url).toString();
   return cachedBaseUrl;
+}
+
+/**
+ * Get the base URL for the DOM Components when serving from updates
+ */
+function getUpdatesBaseURL(): string | null {
+  const ExpoUpdates = globalThis.expo?.modules?.['ExpoUpdates'] as
+    | import('expo-updates').ExpoUpdatesModule
+    | undefined;
+  const updatesIsInstalledAndEnabled = ExpoUpdates?.isEnabled ?? false;
+  const updatesIsEmbeddedLaunch = ExpoUpdates?.isEmbeddedLaunch ?? false;
+  const shouldServeDomFromUpdates = updatesIsInstalledAndEnabled && !updatesIsEmbeddedLaunch;
+  // If updates is installed and enabled, and we're not running from an embedded launch, we should serve the DOM Components from the `.expo-internal` directory
+  if (shouldServeDomFromUpdates) {
+    const localAssets = ExpoUpdates?.localAssets ?? {};
+    const anyLocalAsset = Object.values(localAssets)[0];
+    if (anyLocalAsset) {
+      // Try to get the `.expo-internal` directory from the first local asset
+      return anyLocalAsset.slice(0, anyLocalAsset.lastIndexOf('/'));
+    }
+  }
+  return null;
 }
