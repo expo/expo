3403965719a7ca0e7e400928aee1a8c89d3e8df4
Cedric van Putten <github@cedric.dev>
2024-11-04 17:03:47 +0100

    refactor(cli): add more info about stuck (background) processes when force-quitting after bundling

--- packages/@expo/cli/src/utils/exit.ts
+++ packages/@expo/cli/src/utils/exit.ts
@@ -1,3 +1,4 @@
+import { ChildProcess } from 'node:child_process';
 import process from 'node:process';
 
 import { guardAsync } from './fn';
@@ -112,7 +113,7 @@ export function ensureProcessExitsAfterDelay(waitUntilExitMs = 10000, startedAtM
   const elapsedTime = Date.now() - startedAtMs;
   if (elapsedTime > waitUntilExitMs) {
     debug('active handles detected past the exit delay, forcefully exiting:', activeResources);
-    warn('Something prevented the process from exiting, forcefully exiting.');
+    tryWarnActiveProcesses();
     return process.exit(0);
   }
 
@@ -122,4 +123,43 @@ export function ensureProcessExitsAfterDelay(waitUntilExitMs = 10000, startedAtM
     // Check if the process can exit
     ensureProcessExitsAfterDelay(waitUntilExitMs, startedAtMs);
   }, 100);
+}
+
+/**
+ * Try to warn the user about unexpected active processes running in the background.
+ * This uses the internal `process._getActiveHandles` method, within a try-catch block.
+ * If active child processes are detected, the commands of these processes are logged.
+ *
+ * @example ```bash
+ * Done writing bundle output
+ * Detected 2 processes preventing Expo from exiting, forcefully exiting now.
+ *   - node /Users/cedric/../node_modules/nativewind/dist/metro/tailwind/v3/child.js
+ *   - node /Users/cedric/../node_modules/nativewind/dist/metro/tailwind/v3/child.js
+ * ```
+ */
+function tryWarnActiveProcesses() {
+  let activeProcesses: string[] = [];
+
+  try {
+    const children: ChildProcess[] = process
+      // @ts-expect-error - This is an internal method, not designed to be exposed. It's also our only way to get this info
+      ._getActiveHandles()
+      .filter((handle: any) => handle instanceof ChildProcess);
+
+    if (children.length) {
+      activeProcesses = children.map((child) => child.spawnargs.join(' '));
+    }
+  } catch (error) {
+    debug('failed to get active process information:', error);
+  }
+
+  if (!activeProcesses.length) {
+    warn('Something prevented Expo from exiting, forcefully exiting now.');
+  } else {
+    const singularOrPlural =
+      activeProcesses.length === 1 ? '1 process' : `${activeProcesses.length} processes`;
+
+    warn(`Detected ${singularOrPlural} preventing Expo from exiting, forcefully exiting now.`);
+    warn('  - ' + activeProcesses.join('\n  - '));
+  }
 }
