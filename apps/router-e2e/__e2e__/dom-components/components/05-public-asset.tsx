6dd2efbbcbbc37c7f7e82529a7c6e036884b579f
Kudo Chien <kudo@expo.dev>
2024-11-05 11:09:57 +0800

    [expo][doc] replace EXPO_DOM_BASE_URL with EXPO_BASE_URL (#32596)
    # Why
    
    we have introduced many base urls in process.env. as
    `process.env.EXPO_BASE_URL` is not being used in the webview runtime of
    dom componets. let's just use it.
    
    # How
    
    - replace `process.env.EXPO_DOM_BASE_URL` with
    `process.env.EXPO_BASE_URL`. as sdk 52 is not officially released yet,
    let's just remove it without backward compatibility.
    - also fix original `process.env.EXPO_DOM_BASE_URL` undefined issue on
    web. close ENG-13383

--- apps/router-e2e/__e2e__/dom-components/components/05-public-asset.tsx
+++ apps/router-e2e/__e2e__/dom-components/components/05-public-asset.tsx
@@ -3,7 +3,7 @@
 export default function Page(_: { dom?: import('expo/dom').DOMProps }) {
   return (
     <img
-      src={`${process.env.EXPO_DOM_BASE_URL}/react-logo.png`}
+      src={`${process.env.EXPO_BASE_URL}/react-logo.png`}
       width={128}
       height={128}
       alt="React logo"
