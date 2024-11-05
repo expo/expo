c3c5173f9a4e7ca8e2085933872aca4fe87a8a6e
Brent Vatne <brentvatne@gmail.com>
2024-11-05 10:41:28 -0800

    Update packages/expo-splash-screen/ios/SplashScreenModule.swift

--- packages/expo-splash-screen/ios/SplashScreenModule.swift
+++ packages/expo-splash-screen/ios/SplashScreenModule.swift
@@ -28,7 +28,7 @@ public class SplashScreenModule: Module {
       // The user has manually invoked prevent autohide, this is used to allow libraries
       // such as expo-router to know whether it's safe to hide or if they should wait for
       // the user to do it.
-      _userControlledAutoHideEnabled = true
+      userControlledAutoHideEnabled = true
       SplashScreenManager.shared.preventAutoHideCalled = true
     }
 
