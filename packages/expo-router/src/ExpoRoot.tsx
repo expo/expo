c3952e8ca8b3f9a50c6543c7061efe3ab8626a1c
Mathieu Acthernoene <zoontek@gmail.com>
2024-11-13 11:24:22 +0100

    Prevent expo-router from disabling edge-to-edge

--- packages/expo-router/src/ExpoRoot.tsx
+++ packages/expo-router/src/ExpoRoot.tsx
@@ -11,7 +11,7 @@ import { useInitializeExpoRouter } from './global-state/router-store';
 import { ServerContext, ServerContextType } from './global-state/serverLocationContext';
 import { useDomComponentNavigation } from './link/useDomComponentNavigation';
 import { RequireContext } from './types';
-import { hasViewControllerBasedStatusBarAppearance } from './utils/statusbar';
+import { canOverrideStatusBarBehavior } from './utils/statusbar';
 import * as SplashScreen from './views/Splash';
 
 export type ExpoRootProps = {
@@ -54,7 +54,7 @@ export function ExpoRoot({ wrapper: ParentWrapper = Fragment, ...props }: ExpoRo
           // SSR support
           initialMetrics={INITIAL_METRICS}>
           {/* Users can override this by adding another StatusBar element anywhere higher in the component tree. */}
-          {!hasViewControllerBasedStatusBarAppearance && <AutoStatusBar />}
+          {canOverrideStatusBarBehavior && <AutoStatusBar />}
           {children}
         </SafeAreaProvider>
       </ParentWrapper>
