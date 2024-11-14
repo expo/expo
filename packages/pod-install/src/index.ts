1854348fb66f9fd5f2a31abd67b21436f7492701
Simek <gosimek@gmail.com>
2024-11-14 19:55:33 +0100

    [pod-install] better handled unknown opts

--- packages/pod-install/src/index.ts
+++ packages/pod-install/src/index.ts
@@ -19,7 +19,28 @@ const program = new Command(packageJSON.name)
   .option('--quiet', 'Only print errors')
   .option('--non-interactive', 'Disable interactive prompts')
   .allowUnknownOption()
-  .parse(process.argv);
+  .parse(process.argv)
+  .action(async (projectDirectory?: string) => {
+    try {
+      await runAsync(projectDirectory);
+      if (!program.opts().quiet) {
+        await shouldUpdate();
+      }
+    } catch (reason: any) {
+      console.log('\nAborting run');
+      if (reason.command) {
+        console.log(`  ${chalk.magenta(reason.command)} has failed.`);
+      } else {
+        console.log(chalk.red`An unexpected error was encountered. Please report it as a bug:`);
+        console.log(reason);
+      }
+      console.log();
+      if (!program.opts().quiet) {
+        await shouldUpdate();
+      }
+      process.exit(1);
+    }
+  });
 
 function info(message: string) {
   if (!program.opts().quiet) {
@@ -27,13 +48,14 @@ function info(message: string) {
   }
 }
 
-async function runAsync(projectDirectory: string): Promise<void> {
+async function runAsync(projectDirectory?: string): Promise<void> {
   if (process.platform !== 'darwin') {
     info(chalk.red('\nCocoaPods is only supported on darwin machines\n'));
     process.exit(1);
   }
 
-  const possibleProjectRoot = resolve(projectDirectory ?? process.cwd());
+  const hasProjectDirectory = projectDirectory && !projectDirectory.startsWith('--');
+  const possibleProjectRoot = resolve(hasProjectDirectory ? projectDirectory : process.cwd());
 
   if (!existsSync(possibleProjectRoot)) {
     info(chalk.red(`\nTarget directory does not exist! (${possibleProjectRoot})\n`));
@@ -64,7 +86,7 @@ async function runAsync(projectDirectory: string): Promise<void> {
       process.exit(0);
     }
 
-    if (projectDirectory) {
+    if (hasProjectDirectory) {
       info(chalk.yellow(`CocoaPods is not supported in project at ${possibleProjectRoot}`));
     } else {
       info(chalk.yellow('CocoaPods is not supported in this project'));
@@ -93,24 +115,5 @@ async function runAsync(projectDirectory: string): Promise<void> {
 }
 
 (async () => {
-  program.parse(process.argv);
-  try {
-    await runAsync(program.args[0]);
-    if (!program.opts().quiet) {
-      await shouldUpdate();
-    }
-  } catch (reason: any) {
-    console.log('\nAborting run');
-    if (reason.command) {
-      console.log(`  ${chalk.magenta(reason.command)} has failed.`);
-    } else {
-      console.log(chalk.red`An unexpected error was encountered. Please report it as a bug:`);
-      console.log(reason);
-    }
-    console.log();
-    if (!program.opts().quiet) {
-      await shouldUpdate();
-    }
-    process.exit(1);
-  }
+  await program.parseAsync(process.argv);
 })();
