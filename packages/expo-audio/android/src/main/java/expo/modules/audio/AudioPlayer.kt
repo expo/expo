8f69b23d8c32609b53dcd49fbdcf970938e32a15
Alan Hughes <hughesno1@gmail.com>
2024-11-18 21:14:50 +0000

    review changes

--- packages/expo-audio/android/src/main/java/expo/modules/audio/AudioPlayer.kt
+++ packages/expo-audio/android/src/main/java/expo/modules/audio/AudioPlayer.kt
@@ -172,8 +172,7 @@ class AudioPlayer(
               }
             }
 
-            override fun onFftDataCapture(visualizer: Visualizer?, fft: ByteArray?, samplingRate: Int) {
-            }
+            override fun onFftDataCapture(visualizer: Visualizer?, fft: ByteArray?, samplingRate: Int) = Unit
           },
           Visualizer.getMaxCaptureRate() / 2,
           true,
