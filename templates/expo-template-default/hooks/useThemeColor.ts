c9b2f37eba0bccffc05c2ddb1cfe283834ec5b5b
Vojtech Novak <vonovak@gmail.com>
2024-11-19 09:02:52 +0100

    Revert "[template-default] Fix import in useThemeColor.ts (#32988)"
    This reverts commit f6acb664908ff447ddd06c71236c8efab60973c0.

--- templates/expo-template-default/hooks/useThemeColor.ts
+++ templates/expo-template-default/hooks/useThemeColor.ts
@@ -3,7 +3,7 @@
  * https://docs.expo.dev/guides/color-schemes/
  */
 
-import { useColorScheme } from './useColorScheme';
+import { useColorScheme } from 'react-native';
 
 import { Colors } from '@/constants/Colors';
 
