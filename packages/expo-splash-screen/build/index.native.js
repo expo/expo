a6b4b0649a5147c25f096cb13cdb719f2e0ac2c2
Alan Hughes <alan@expo.io>
2024-11-20 09:53:27 +0000

    use helper

--- packages/expo-splash-screen/build/index.native.js
+++ packages/expo-splash-screen/build/index.native.js
@@ -1,11 +1,11 @@
+import { isRunningInExpoGo } from 'expo';
 import { requireOptionalNativeModule } from 'expo-modules-core';
 const SplashModule = requireOptionalNativeModule('ExpoSplashScreen');
-const isExpoGo = typeof expo !== 'undefined' && globalThis.expo?.modules?.ExpoGo;
 export function setOptions(options) {
     if (!SplashModule) {
         return;
     }
-    if (isExpoGo) {
+    if (isRunningInExpoGo()) {
         console.warn("'Splashscreen.setOptions' cannot be used in Expo Go. To customize the splash screen, you can use development builds.");
         return;
     }
--- packages/expo-splash-screen/src/index.native.ts
+++ packages/expo-splash-screen/src/index.native.ts
@@ -1,17 +1,16 @@
+import { isRunningInExpoGo } from 'expo';
 import { requireOptionalNativeModule } from 'expo-modules-core';
 
 import { SplashScreenNativeModule, SplashScreenOptions } from './SplashScreen.types';
 
 const SplashModule = requireOptionalNativeModule<SplashScreenNativeModule>('ExpoSplashScreen');
 
-const isExpoGo = typeof expo !== 'undefined' && globalThis.expo?.modules?.ExpoGo;
-
 export function setOptions(options: SplashScreenOptions) {
   if (!SplashModule) {
     return;
   }
 
-  if (isExpoGo) {
+  if (isRunningInExpoGo()) {
     console.warn(
       "'Splashscreen.setOptions' cannot be used in Expo Go. To customize the splash screen, you can use development builds."
     );
