8fa976317687d7e6bee9ca4613cb6ebbe3156d64
Kudo Chien <kudo@expo.dev>
2024-11-22 03:21:04 +0800

    Apply suggestions from code review

--- packages/expo-modules-core/src/ts-declarations/global.ts
+++ packages/expo-modules-core/src/ts-declarations/global.ts
@@ -90,7 +90,7 @@ declare global {
    */
   var expo: ExpoGlobal;
 
-  // @ts-ignore - Suppress incompatible `NodeJS.Process` type if people includes process type from @types/node
+  // @ts-ignore - Suppress incompatible `NodeJS.Process` type if people include process type from `@types/node`
   var process: ExpoProcess;
 
   /**
