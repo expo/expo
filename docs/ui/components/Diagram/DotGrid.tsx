95789a10bc4badf79a8d8c07d3cc4e50cbe71924
Simek <gosimek@gmail.com>
2024-11-22 18:48:17 +0100

    [docs] fix DotGrid color, improve Diagram 'auto' theme handling

--- docs/ui/components/Diagram/DotGrid.tsx
+++ docs/ui/components/Diagram/DotGrid.tsx
@@ -6,7 +6,7 @@ export function DotGrid() {
       <svg width="100%" height="100%">
         <defs>
           <pattern id="dot-grid" x="0" y="0" width="10" height="10" patternUnits="userSpaceOnUse">
-            <circle fill="var(--slate4)" cx="1" cy="1" r="1" />
+            <circle fill="var(--slate-4)" cx="1" cy="1" r="1" />
           </pattern>
         </defs>
         <rect x="0" y="0" width="100%" height="100%" fill="url(#dot-grid)" />
