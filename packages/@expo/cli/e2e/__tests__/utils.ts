40ad76103a79e2f25d14c95b6ee7b1804ae9bff0
Cedric van Putten <github@cedric.dev>
2024-11-23 14:47:41 +0100

    test(cli): use package tarball instead of linking to avoid windows permission errors

--- packages/@expo/cli/e2e/__tests__/utils.ts
+++ packages/@expo/cli/e2e/__tests__/utils.ts
@@ -6,6 +6,7 @@ import assert from 'assert';
 import execa from 'execa';
 import findProcess from 'find-process';
 import fs from 'fs';
+import klawSync from 'klaw-sync';
 import * as htmlParser from 'node-html-parser';
 import os from 'os';
 import path from 'path';
@@ -14,11 +15,12 @@ import { promisify } from 'util';
 
 import { copySync } from '../../src/utils/dir';
 import { convertPathToPosix } from '../jest/expect-path';
-import klawSync from 'klaw-sync';
 
 export const bin = require.resolve('../../build/bin/cli');
 
 export const projectRoot = getTemporaryPath();
+
+export const EXPO_MONOREPO_ROOT = path.resolve(__dirname, '../../../../..');
 
 export function getTemporaryPath() {
   return path.join(os.tmpdir(), Math.random().toString(36).substring(2));
@@ -146,7 +148,6 @@ export async function createFromFixtureAsync(
     if (pkg || linkExpoPackages || linkExpoPackagesDev) {
       pkg ??= {};
       const pkgPath = path.join(projectRoot, 'package.json');
-      const expoPackagesPath = path.join(__dirname, '../../../../');
       const fixturePkg = (await JsonFile.readAsync(pkgPath)) as PackageJSONConfig;
 
       const dependencies = Object.assign({}, fixturePkg.dependencies, pkg.dependencies);
@@ -154,13 +155,15 @@ export async function createFromFixtureAsync(
 
       if (linkExpoPackages) {
         for (const pkg of linkExpoPackages) {
-          dependencies[pkg] = `file:${path.join(expoPackagesPath, pkg)}`;
+          const tarball = await createPackageTarball(projectRoot, pkg);
+          dependencies[pkg] = tarball.packageReference;
         }
       }
 
       if (linkExpoPackagesDev) {
         for (const pkg of linkExpoPackagesDev) {
-          devDependencies[pkg] = path.join(expoPackagesPath, pkg);
+          const tarball = await createPackageTarball(projectRoot, pkg);
+          devDependencies[pkg] = tarball.packageReference;
         }
       }
 
@@ -335,4 +338,47 @@ export function findProjectFiles(projectRoot: string) {
     )
     .filter(Boolean)
     .sort();
+}
+
+/**
+ * Create a tarball from a package within the Expo monorepo.
+ * This creates the tarball from source, and moves it within the fixture directory using `_tarball`.
+ * It can be used to test any of our packages directly from source, without publishing or leaking other dependencies.
+ */
+export async function createPackageTarball(fixtureRoot: string, packageName: string) {
+  // Resolve the package directory in the monorepo
+  const packageDir = path.join(EXPO_MONOREPO_ROOT, 'packages', packageName);
+  if (!fs.existsSync(packageDir)) {
+    throw new Error(`Cannot find package "${packageName}" in the Expo monorepo`);
+  }
+
+  // Prepare the destination of the tarball within the fixture
+  const outputDir = path.join(fixtureRoot, '_tarballs');
+  await fs.promises.mkdir(outputDir, { recursive: true });
+
+  // Create the tarball with npm pack
+  const { stdout } = await execa(
+    'npm',
+    // Run `npm pack --json` without the script logging (see: https://github.com/npm/cli/issues/7354)
+    ['--foreground-scripts=false', 'pack', '--json', '--pack-destination', outputDir],
+    { cwd: packageDir, stdio: testingLocally ? undefined : 'pipe' }
+  );
+
+  // Validate the tarball output
+  const output = JSON.parse(stdout);
+  if (output.length !== 1) {
+    throw new Error(`Expected a single tarball to be created, received: ${output.length}`);
+  }
+
+  // Return the tarball information
+  const absolutePath = path.join(outputDir, output[0].filename);
+  const relativePath = path.relative(fixtureRoot, absolutePath);
+
+  return {
+    name: output[0].name as string,
+    version: output[0].version as string,
+    relativePath,
+    absolutePath,
+    packageReference: `file:./${convertPathToPosix(relativePath)}`,
+  };
 }
