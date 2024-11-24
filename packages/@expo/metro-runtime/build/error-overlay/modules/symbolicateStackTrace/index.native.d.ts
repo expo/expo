493f52c75cc9375f01c92665d0fbabc7e83ef407
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2024-11-24 11:48:45 +0100

    Revert "[chore] Rebuild all packages (#33195)"
    This reverts commit 52d4d976f390897bf7003992ba1f7eab8a6988fd.

--- packages/@expo/metro-runtime/build/error-overlay/modules/symbolicateStackTrace/index.native.d.ts
+++ packages/@expo/metro-runtime/build/error-overlay/modules/symbolicateStackTrace/index.native.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react-native/types/modules/devtools" />
 import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 export default symbolicateStackTrace;
 //# sourceMappingURL=index.native.d.ts.map
--- packages/expo-router/build/ui/Slot.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-export declare const Slot: import("react").ForwardRefExoticComponent<import("@radix-ui/react-slot").SlotProps & import("react").RefAttributes<HTMLElement>>;
-//# sourceMappingURL=Slot.d.ts.map
