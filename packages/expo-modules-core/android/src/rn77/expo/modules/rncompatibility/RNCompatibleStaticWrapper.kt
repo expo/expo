3099245033969302bb32280e4593bf9158a6297b
Kudo Chien <kudo@expo.dev>
2024-11-27 02:36:27 +0800

    fix

--- packages/expo-modules-core/android/src/rn77/expo/modules/rncompatibility/RNCompatibleStaticWrapper.kt
+++ packages/expo-modules-core/android/src/rn77/expo/modules/rncompatibility/RNCompatibleStaticWrapper.kt
@@ -8,6 +8,6 @@ import com.facebook.react.uimanager.style.BoxShadow
  * A compatible wrapper of [BoxShadow.parse] for React Native compatibility
  * TODO(kudo,20241127): Remove this when we drop react-native 0.76 support
  */
-fun parseBoxShadow(boxShadow: ReadableMap, context: Context): BoxShadow? {
+fun parseBoxShadow(boxShadow: ReadableMap?, context: Context): BoxShadow? {
   return BoxShadow.parse(boxShadow, context)
 }
