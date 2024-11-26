2169862fdcc455fbb34373aa923597d41277c8be
entiendoNull <jacobnclausen@gmail.com>
2024-11-26 17:46:20 +0100

    Clean up

--- packages/expo-media-library/ios/MediaLibraryModule.swift
+++ packages/expo-media-library/ios/MediaLibraryModule.swift
@@ -20,7 +20,6 @@ public class MediaLibraryModule: Module, PhotoLibraryObserverHandler {
           "photo": "photo",
           "video": "video",
           "unknown": "unknown",
-          "livePhoto": "livePhoto",
           "all": "all"
         ],
         "SortBy": [
