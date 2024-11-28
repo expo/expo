4660eb06818891e878f97f31fc55f869ee4e2ea1
Cedric van Putten <github@cedric.dev>
2024-11-28 16:47:22 +0100

    test(cli): run tests serially to avoid heavy IO congestion

--- packages/@expo/cli/e2e/jest.config.js
+++ packages/@expo/cli/e2e/jest.config.js
@@ -14,4 +14,7 @@ module.exports = {
   // Set the global timeout (2m) to allow for setting up Expo projects, and exporting them
   // Windows IO is very slow, so we need to increase this timeout (5m) for Windows
   testTimeout: process.platform === 'win32' ? 600_000 : 120_000,
+  // Because most tests invoke heavy IO operations (Metro), we have to run them serially.
+  // Otherwise we suffer from IO congestion which slows all tests down
+  maxWorkers: 1,
 };
