74a6fa647f36193c6e103b652e9dbde17127c3c1
Cedric van Putten <github@cedric.dev>
2024-12-03 17:47:24 +0100

    test(cli): add managed background process helpers to avoid leaky processes in tests

---
+++ packages/@expo/cli/e2e/utils/process.ts
@@ -0,0 +1,164 @@
+import assert from 'node:assert';
+import type { ChildProcess } from 'node:child_process';
+import { once } from 'node:events';
+import { stripVTControlCharacters } from 'node:util';
+import treeKill from 'tree-kill';
+
+/**
+ * Stop or kill a running the child process, and possible sub-processes it started.
+ * This uses `tree-kill` to stop all processes on Linux, macOS, and Windows.
+ * When using `force = true`, the child process ID assertion check is skipped.
+ */
+export async function killProcessAsync(
+  child?: ChildProcess,
+  signal: NodeJS.Signals = 'SIGKILL',
+  force = false
+) {
+  if (!child || child.exitCode !== null) return;
+
+  const killed = once(child, 'close');
+  await new Promise<void>((resolve, reject) => {
+    if (force && !child.pid) return resolve();
+    assert(child.pid, 'Child process has no process ID, unable to kill process');
+    treeKill(child.pid, signal, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+
+  await killed;
+}
+
+/**
+ * Wait for a specific output value from a child process, using either `stderr` or `stdout`.
+ * When the resolver returns a truthy value, this promise is resolved.
+ */
+export async function waitForProcessOutput<T>(
+  child: ChildProcess,
+  resolver: (output: any) => T | null | undefined
+) {
+  assert(child.stderr, 'Process has no available stderr stream, unable to wait for output');
+  assert(child.stdout, 'Process has no available stdout stream, unable to wait for output');
+
+  // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/withResolvers
+  let resolve: (value: T | PromiseLike<T>) => void;
+  const promise = new Promise<T>((res) => {
+    resolve = res;
+  });
+
+  const onProcessOutput = (chunk: any) => {
+    const result = resolver(chunk);
+    if (result) resolve(result);
+  };
+
+  try {
+    child.stderr.on('data', onProcessOutput);
+    child.stdout.on('data', onProcessOutput);
+    return await promise;
+  } finally {
+    child.stderr.off('data', onProcessOutput);
+    child.stdout.off('data', onProcessOutput);
+  }
+}
+
+/**
+ * Wait for the child process to become "ready", defined through the resolver's promise.
+ * This listens for possible spawn errors or unexepcted exits of the child process.
+ */
+export async function waitForProcessReady<T>(child: ChildProcess, resolver: () => Promise<T>) {
+  // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/withResolvers
+  let resolve: (value: T | PromiseLike<T>) => void;
+  let reject: (reason: any) => void;
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  const onProcessError = (spawnError: any) => {
+    reject(processErrorToError(spawnError));
+  };
+
+  const onProcessExit = (code: number | null, signal: NodeJS.Signals | null) => {
+    reject(processExitToError(code ?? signal));
+  };
+
+  try {
+    resolver().then(resolve!);
+    child.once('error', onProcessError);
+    child.once('exit', onProcessExit);
+    return await promise;
+  } finally {
+    child.off('error', onProcessError);
+    child.off('exit', onProcessExit);
+  }
+}
+
+/**
+ * Convert the spawn error into an actual error that renders all available information.
+ * Errors thrown through `child.on('error')` are not actual Error instances, and may obfuscate the information.
+ */
+export function processErrorToError(spawnError: any) {
+  if (spawnError instanceof Error) return spawnError;
+
+  const cause = new Error(
+    'syscall' in spawnError && 'spawnargs' in spawnError
+      ? `${spawnError.code} ${spawnError.syscall} ${spawnError.spawnargs}`
+      : `${spawnError.code} ${spawnError.message}`
+  );
+
+  Object.defineProperty(cause, 'code', {
+    value: spawnError.code,
+  });
+
+  return new Error('Process failed to spawn', { cause });
+}
+
+/**
+ * Convert the unexpected `child.on('exit')` information into a descriptive error instance.
+ * This should render the received exit code or signal, with a human-readable message.
+ */
+export function processExitToError(
+  codeOrSignal: number | NodeJS.Signals | null,
+  message = 'Process exited unexpectedly with:'
+) {
+  return new Error(`${message} ${codeOrSignal || 'unknown exit reason'}`);
+}
+
+/**
+ * Pipe the received output of the child process into the current process output stream.
+ * This will append a prefix for each line, to make it more readable.
+ * Once the process exits or errors, the listeners are removed automatically.
+ */
+export function processPipeOutput(child: ChildProcess, prefix = '[child process]') {
+  const prefixLines = (chunk: any) =>
+    `${prefix} ` + chunk.toString().split('\n').join(`\n${prefix} `);
+
+  const onProcessStderr = (chunk: any) => process.stderr.write(prefixLines(chunk));
+  const onProcessStdout = (chunk: any) => process.stdout.write(prefixLines(chunk));
+
+  child.stderr?.on('data', onProcessStderr);
+  child.stdout?.on('data', onProcessStdout);
+
+  child.once('close', () => {
+    child.stderr?.off('data', onProcessStderr);
+    child.stdout?.off('data', onProcessStdout);
+  });
+}
+
+/**
+ * Find a prefixed value from the process output chunk, and return it.
+ * When the prefixed value isn't found, this returns `null`.
+ */
+export function processFindPrefixedValue(chunk: any, prefix: string) {
+  const lines = stripVTControlCharacters(chunk.toString()).split('\n');
+  const value = lines.find((line) => line.includes(prefix));
+
+  if (value) {
+    return value.split(prefix).pop()?.trim() ?? null;
+  }
+
+  return null;
+}
---
+++ packages/@expo/cli/e2e/utils/server.ts
@@ -0,0 +1,198 @@
+import spawn from 'cross-spawn';
+import assert from 'node:assert';
+import type { ChildProcess, SpawnOptions } from 'node:child_process';
+import { createServer } from 'node:net';
+import { env } from 'node:process';
+
+import {
+  killProcessAsync,
+  processExitToError,
+  processFindPrefixedValue,
+  processPipeOutput,
+  waitForProcessOutput,
+  waitForProcessReady,
+} from './process';
+
+export type BackgroundServerOptions = SpawnOptions & {
+  /**
+   * The command to spawn as background process.
+   * You can also provide a function that receives the configured port.
+   *
+   * @example command: ['yarn', 'expo', 'start']
+   * @example command: (port) => ['yarn', 'expo', 'start', '--port', port]
+   */
+  command: string[] | ((port: number) => string[]);
+  /**
+   * The port where the server should bind to, passed as PORT environment variable to the command.
+   * Either set this to a number, or provide a method returning a number-promise.
+   * By default, a random available port is used.
+   *
+   * @example port: 1337
+   * @example port: freePortAsync
+   */
+  port?: number | (() => Promise<number>);
+  /**
+   * The host derrived from the child process output chunks (stdout or stderr).
+   * When starting the server, the promise won't resolve before the host is resolved.
+   * When passing a URL, the port will be overriden using the configured port.
+   */
+  host(chunk: any): URL | string | null;
+  /** Fully show the child process output */
+  verbose?: boolean;
+};
+
+export type BackgroundServer = {
+  options: SpawnOptions;
+  readonly process: ChildProcess;
+  readonly url: URL;
+  /** Fetch using URL pathnames from the server, the full URL of the server will be added */
+  fetchAsync(url: string | URL, init?: RequestInit): Promise<Response>;
+  /** Start the background server, and wait until the server URL is resolved */
+  startAsync(flags?: string[]): Promise<void>;
+  /** Stop the background server, when passing `true` will ensure the server is killed - even when not started */
+  stopAsync(force?: boolean): Promise<void>;
+};
+
+/**
+ * Create a managed (backgrounded) process for long-running commands, like servers.
+ * When running servers in Jest, we need to ensure the process started and closed to avoid test leaking.
+ */
+export function createBackgroundServer({
+  command,
+  host: resolveHost,
+  port: resolvePort = findFreePortAsync,
+  verbose = false,
+  ...spawnOptions
+}: BackgroundServerOptions): BackgroundServer {
+  let child: ChildProcess | null = null;
+  let url: URL | null = null;
+  let exitHandler: ((code: number | null, signal: NodeJS.Signals | null) => void) | null = null;
+
+  return {
+    options: spawnOptions,
+    get process() {
+      assert(child, 'Server process is unavailable, likely not fully started');
+      return child;
+    },
+    get url() {
+      assert(url, 'Server URL is unavailable, likely not fully started');
+      return url;
+    },
+    fetchAsync(url, init) {
+      return fetch(new URL(url, this.url), init);
+    },
+    async startAsync(flags = []) {
+      if (child) {
+        throw new Error('Server was already started, unable to start the server');
+      }
+
+      const port = typeof resolvePort === 'number' ? resolvePort : await resolvePort();
+      const [bin, ...commandOrFlags] = Array.isArray(command)
+        ? command.concat(flags)
+        : command(port).concat(flags);
+
+      // Define the port through the environment variable, setting this here outputs the port in verbose mode
+      spawnOptions.env ??= {};
+      spawnOptions.env.PORT = String(port);
+
+      child = spawn(bin, commandOrFlags, {
+        shell: false,
+        stdio: ['ignore', 'pipe', 'pipe'],
+        ...spawnOptions,
+        env: {
+          ...env, // Pipe through all environment variables from the host by default
+          ...spawnOptions.env,
+        },
+      });
+
+      if (verbose) {
+        console.log('[server] startAsync -', bin, ...commandOrFlags, this.options);
+        processPipeOutput(child, '[server]');
+      }
+
+      try {
+        // Wait until the host is resolved based on the process output
+        const host = await waitForProcessReady(child, () =>
+          waitForProcessOutput(child!, resolveHost)
+        );
+
+        // Store the host and set the resolved port
+        url = new URL(host, 'http://127.0.0.1');
+        url.port = String(port);
+
+        // Attach an exit handler to monitor if the process closes before intentionally closing
+        exitHandler = (code, signal) => {
+          throw processExitToError(code ?? signal, '[server] exited unexpectedly with:');
+        };
+        child.once('exit', exitHandler);
+      } catch (error) {
+        // When an error occurred, ensure the process is closed
+        await this.stopAsync(true);
+        throw error;
+      }
+    },
+    async stopAsync(force = false) {
+      if (!child && !force) {
+        throw new Error('Server was not started, unable to stop server process');
+      }
+
+      if (exitHandler) child?.off('exit', exitHandler);
+      if (verbose) {
+        console.log('[server] stopAsync -', force ? 'by force' : '');
+      }
+
+      try {
+        await killProcessAsync(child ?? undefined, 'SIGKILL', force);
+      } catch (error) {
+        throw new Error('Server could not be stopped', { cause: error });
+      } finally {
+        child = null;
+        url = null;
+      }
+    },
+  };
+}
+
+/**
+ * Find a random available port through Node's own `net.createServer`.
+ * This highly increases the randomness compared to `freeport-async`,
+ * and avoids overlapping ports in parallel tests.
+ */
+export async function findFreePortAsync() {
+  // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/withResolvers
+  let resolve: (value: number) => void;
+  let reject: (reason: any) => void;
+  const promise = new Promise<number>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  const server = createServer();
+  server.once('error', reject!);
+  server.listen(0, () => {
+    const address = server.address();
+    const url = typeof address === 'string' ? new URL(address) : address;
+    server.close(() => {
+      if (!url) {
+        reject(new Error('Could not find the address of a temporary server'));
+      } else {
+        resolve(Number(url.port));
+      }
+    });
+  });
+
+  try {
+    return await promise;
+  } finally {
+    server.off('error', reject!);
+  }
+}
+
+/** Create a managed background server running `npx serve` */
+export function createStaticServe(options: Partial<BackgroundServerOptions> = {}) {
+  return createBackgroundServer({
+    command: ['npx', 'serve'], // Port is defined through the PORT environment variable
+    host: (chunk) => processFindPrefixedValue(chunk, 'Accepting connections at'),
+    ...options,
+  });
+}
