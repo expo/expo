dba35b1aa2a4d3fef7e2a0760ef84bf2583eb434
Cedric van Putten <github@cedric.dev>
2024-12-03 18:57:37 +0100

    test(cli): increase global test timeout for E2E tests

--- packages/@expo/cli/e2e/jest.config.js
+++ packages/@expo/cli/e2e/jest.config.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
-const path = require('path');
+const path = require('node:path');
+const process = require('node:process');
 
 const roots = ['../__mocks__', '.'];
 
@@ -13,4 +14,6 @@ module.exports = {
   displayName: require('../package').name,
   roots,
   setupFilesAfterEnv: [path.resolve(__dirname, './jest.setup.ts')],
+  // Configure the global jest timeout to 3m, on Windows increase this to 5m
+  testTimeout: process.platform === 'win32' ? 300_000 : 180_000,
 };
