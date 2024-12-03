ad0bb5224f71126f74911a7c592252a6eae170f2
Cedric van Putten <github@cedric.dev>
2024-12-03 18:44:24 +0100

    test(cli): enable verbose background server logging when running GitHub Actions in debug mode

--- packages/@expo/cli/e2e/utils/server.ts
+++ packages/@expo/cli/e2e/utils/server.ts
@@ -1,4 +1,5 @@
 import spawn from 'cross-spawn';
+import { boolish } from 'getenv';
 import assert from 'node:assert';
 import type { ChildProcess, SpawnOptions } from 'node:child_process';
 import { createServer } from 'node:net';
@@ -37,7 +38,7 @@ export type BackgroundServerOptions = SpawnOptions & {
    * When passing a URL, the port will be overriden using the configured port.
    */
   host(chunk: any): URL | string | null;
-  /** Fully show the child process output */
+  /** Fully show the child process output, enabled when re-running GitHub Actions with debug mode */
   verbose?: boolean;
 };
 
@@ -61,7 +62,7 @@ export function createBackgroundServer({
   command,
   host: resolveHost,
   port: resolvePort = findFreePortAsync,
-  verbose = false,
+  verbose = boolish('ACTIONS_RUNNER_DEBUG', false),
   ...spawnOptions
 }: BackgroundServerOptions): BackgroundServer {
   let child: ChildProcess | null = null;
