9c34fbf1addbb27cbe6daf4f5a8d9dadb24eedf7
Cedric van Putten <github@cedric.dev>
2024-12-06 00:37:36 +0100

    test(cli): update invalid project root assertions for Windows support

--- packages/@expo/cli/e2e/__tests__/config-test.ts
+++ packages/@expo/cli/e2e/__tests__/config-test.ts
@@ -94,6 +94,7 @@ it('throws on invalid project root', async () => {
     await execute('config', 'very---invalid', '--json');
   } catch (e) {
     const error = e as ExecaError;
-    expect(error.stderr).toMatch(/Invalid project root: \//);
+    // Test on the invalid project prefix, and absolute path ending in the expected project folder
+    expect(error.stderr).toMatch(/^Invalid project root: .*very---invalid$/);
   }
 });
