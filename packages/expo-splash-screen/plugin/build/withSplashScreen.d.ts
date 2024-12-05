expo/expo

commit 4f35497da8f3632fb66113b0d9780f23c5c0357b

[splashscreen] Update the ios colors json (#33472)

packages/expo-splash-screen/plugin/build/withSplashScreen.js
@@ -6,22 +6,23 @@ const config_plugins_1 = require("expo/config-plugins");
 const pkg = require('expo-splash-screen/package.json');
 const withSplashScreen = (config, props) => {
     const resizeMode = props?.resizeMode || 'contain';
+    const { ios: iosProps, android: androidProps, ...otherProps } = props;
     const android = {
-        ...props,
-        ...props?.android,
-        resizeMode,
+        ...otherProps,
+        ...androidProps,
+        resizeMode: androidProps?.resizeMode || resizeMode,
         dark: {
-            ...props?.android?.dark,
-            ...props?.dark,
+            ...otherProps?.dark,
+            ...androidProps?.dark,
         },
     };
     const ios = {
-        ...props,
-        ...props?.ios,
-        resizeMode: resizeMode === 'native' ? 'contain' : resizeMode,
+        ...otherProps,
+        ...iosProps,
+        resizeMode: iosProps?.resizeMode || (resizeMode === 'native' ? 'contain' : resizeMode),
         dark: {
-            ...props?.ios?.dark,
-            ...props?.dark,
+            ...otherProps?.dark,
+            ...iosProps?.dark,
         },
     };
     // Need to pass null here if we don't receive any props. This means that the plugin has not been used.

packages/expo-splash-screen/plugin/src/withSplashScreen.ts
@@ -22,22 +22,25 @@ type PluginConfig = {
 
 const withSplashScreen: ConfigPlugin<PluginConfig> = (config, props) => {
   const resizeMode = props?.resizeMode || 'contain';
+
+  const { ios: iosProps, android: androidProps, ...otherProps } = props;
+
   const android: AndroidSplashConfig = {
-    ...props,
-    ...props?.android,
-    resizeMode,
+    ...otherProps,
+    ...androidProps,
+    resizeMode: androidProps?.resizeMode || resizeMode,
     dark: {
-      ...props?.android?.dark,
-      ...props?.dark,
+      ...otherProps?.dark,
+      ...androidProps?.dark,
     },
   };
   const ios: IOSSplashConfig = {
-    ...props,
-    ...props?.ios,
-    resizeMode: resizeMode === 'native' ? 'contain' : resizeMode,
+    ...otherProps,
+    ...iosProps,
+    resizeMode: iosProps?.resizeMode || (resizeMode === 'native' ? 'contain' : resizeMode),
     dark: {
-      ...props?.ios?.dark,
-      ...props?.dark,
+      ...otherProps?.dark,
+      ...iosProps?.dark,
     },
   };
 

packages/@expo/prebuild-config/build/plugins/icons/AssetContents.d.ts
@@ -1,5 +1,5 @@
 export type ContentsJsonImageIdiom = 'iphone' | 'ipad' | 'watchos' | 'ios' | 'ios-marketing' | 'universal';
-export type ContentsJsonImageAppearanceLuminosityType = 'dark' | 'tinted';
+export type ContentsJsonImageAppearanceLuminosityType = 'light' | 'dark' | 'tinted';
 export type ContentsJsonAppearance = {
     appearance: 'luminosity';
     value: ContentsJsonImageAppearanceLuminosityType;

packages/@expo/prebuild-config/build/plugins/icons/AssetContents.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"AssetContents.js","names":["_fsExtra","data","_interopRequireDefault","require","_path","e","__esModule","default","createContentsJsonItem","item","writeContentsJsonAsync","directory","images","fs","ensureDir","writeFile","join","JSON","stringify","info","version","author"],"sources":["../../../src/plugins/icons/AssetContents.ts"],"sourcesContent":["import fs from 'fs-extra';\nimport { join } from 'path';\n\nexport type ContentsJsonImageIdiom =\n  | 'iphone'\n  | 'ipad'\n  | 'watchos'\n  | 'ios'\n  | 'ios-marketing'\n  | 'universal';\n\nexport type ContentsJsonImageAppearanceLuminosityType = 'dark' | 'tinted';\n\nexport type ContentsJsonAppearance = {\n  appearance: 'luminosity';\n  value: ContentsJsonImageAppearanceLuminosityType;\n};\n\nexport type ContentsJsonImageScale = '1x' | '2x' | '3x';\n\nexport interface ContentsJsonImage {\n  appearances?: ContentsJsonAppearance[];\n  idiom: ContentsJsonImageIdiom;\n  size?: string;\n  scale?: ContentsJsonImageScale;\n  filename?: string;\n  platform?: ContentsJsonImageIdiom;\n}\n\nexport interface ContentsJsonColor {\n  appearances?: ContentsJsonAppearance[];\n  idiom: ContentsJsonImageIdiom;\n  color: {\n    'color-space': 'srgb';\n    components: {\n      alpha: string;\n      blue: string;\n      green: string;\n      red: string;\n    };\n  };\n}\n\nexport interface ContentsJson {\n  images: ContentsJsonImage[];\n  colors: ContentsJsonColor[];\n  info: {\n    version: number;\n    author: string;\n  };\n}\n\nexport function createContentsJsonItem(item: ContentsJsonImage): ContentsJsonImage {\n  return item;\n}\n\n/**\n * Writes the Config.json which is used to assign images to their respective platform, dpi, and idiom.\n *\n * @param directory path to add the Contents.json to.\n * @param contents image json data\n */\nexport async function writeContentsJsonAsync(\n  directory: string,\n  { images }: Pick<ContentsJson, 'images'>\n): Promise<void> {\n  await fs.ensureDir(directory);\n\n  await fs.writeFile(\n    join(directory, 'Contents.json'),\n    JSON.stringify(\n      {\n        images,\n        info: {\n          version: 1,\n          // common practice is for the tool that generated the icons to be the \"author\"\n          author: 'expo',\n        },\n      },\n      null,\n      2\n    )\n  );\n}\n"],"mappings":";;;;;;;AAAA,SAAAA,SAAA;EAAA,MAAAC,IAAA,GAAAC,sBAAA,CAAAC,OAAA;EAAAH,QAAA,YAAAA,CAAA;IAAA,OAAAC,IAAA;EAAA;EAAA,OAAAA,IAAA;AAAA;AACA,SAAAG,MAAA;EAAA,MAAAH,IAAA,GAAAE,OAAA;EAAAC,KAAA,YAAAA,CAAA;IAAA,OAAAH,IAAA;EAAA;EAAA,OAAAA,IAAA;AAAA;AAA4B,SAAAC,uBAAAG,CAAA,WAAAA,CAAA,IAAAA,CAAA,CAAAC,UAAA,GAAAD,CAAA,KAAAE,OAAA,EAAAF,CAAA;AAmDrB,SAASG,sBAAsBA,CAACC,IAAuB,EAAqB;EACjF,OAAOA,IAAI;AACb;;AAEA;AACA;AACA;AACA;AACA;AACA;AACO,eAAeC,sBAAsBA,CAC1CC,SAAiB,EACjB;EAAEC;AAAqC,CAAC,EACzB;EACf,MAAMC,kBAAE,CAACC,SAAS,CAACH,SAAS,CAAC;EAE7B,MAAME,kBAAE,CAACE,SAAS,CAChB,IAAAC,YAAI,EAACL,SAAS,EAAE,eAAe,CAAC,EAChCM,IAAI,CAACC,SAAS,CACZ;IACEN,MAAM;IACNO,IAAI,EAAE;MACJC,OAAO,EAAE,CAAC;MACV;MACAC,MAAM,EAAE;IACV;EACF,CAAC,EACD,IAAI,EACJ,CACF,CACF,CAAC;AACH","ignoreList":[]}
+{"version":3,"file":"AssetContents.js","names":["_fsExtra","data","_interopRequireDefault","require","_path","e","__esModule","default","createContentsJsonItem","item","writeContentsJsonAsync","directory","images","fs","ensureDir","writeFile","join","JSON","stringify","info","version","author"],"sources":["../../../src/plugins/icons/AssetContents.ts"],"sourcesContent":["import fs from 'fs-extra';\nimport { join } from 'path';\n\nexport type ContentsJsonImageIdiom =\n  | 'iphone'\n  | 'ipad'\n  | 'watchos'\n  | 'ios'\n  | 'ios-marketing'\n  | 'universal';\n\nexport type ContentsJsonImageAppearanceLuminosityType = 'light' | 'dark' | 'tinted';\n\nexport type ContentsJsonAppearance = {\n  appearance: 'luminosity';\n  value: ContentsJsonImageAppearanceLuminosityType;\n};\n\nexport type ContentsJsonImageScale = '1x' | '2x' | '3x';\n\nexport interface ContentsJsonImage {\n  appearances?: ContentsJsonAppearance[];\n  idiom: ContentsJsonImageIdiom;\n  size?: string;\n  scale?: ContentsJsonImageScale;\n  filename?: string;\n  platform?: ContentsJsonImageIdiom;\n}\n\nexport interface ContentsJsonColor {\n  appearances?: ContentsJsonAppearance[];\n  idiom: ContentsJsonImageIdiom;\n  color: {\n    'color-space': 'srgb';\n    components: {\n      alpha: string;\n      blue: string;\n      green: string;\n      red: string;\n    };\n  };\n}\n\nexport interface ContentsJson {\n  images: ContentsJsonImage[];\n  colors: ContentsJsonColor[];\n  info: {\n    version: number;\n    author: string;\n  };\n}\n\nexport function createContentsJsonItem(item: ContentsJsonImage): ContentsJsonImage {\n  return item;\n}\n\n/**\n * Writes the Config.json which is used to assign images to their respective platform, dpi, and idiom.\n *\n * @param directory path to add the Contents.json to.\n * @param contents image json data\n */\nexport async function writeContentsJsonAsync(\n  directory: string,\n  { images }: Pick<ContentsJson, 'images'>\n): Promise<void> {\n  await fs.ensureDir(directory);\n\n  await fs.writeFile(\n    join(directory, 'Contents.json'),\n    JSON.stringify(\n      {\n        images,\n        info: {\n          version: 1,\n          // common practice is for the tool that generated the icons to be the \"author\"\n          author: 'expo',\n        },\n      },\n      null,\n      2\n    )\n  );\n}\n"],"mappings":";;;;;;;AAAA,SAAAA,SAAA;EAAA,MAAAC,IAAA,GAAAC,sBAAA,CAAAC,OAAA;EAAAH,QAAA,YAAAA,CAAA;IAAA,OAAAC,IAAA;EAAA;EAAA,OAAAA,IAAA;AAAA;AACA,SAAAG,MAAA;EAAA,MAAAH,IAAA,GAAAE,OAAA;EAAAC,KAAA,YAAAA,CAAA;IAAA,OAAAH,IAAA;EAAA;EAAA,OAAAA,IAAA;AAAA;AAA4B,SAAAC,uBAAAG,CAAA,WAAAA,CAAA,IAAAA,CAAA,CAAAC,UAAA,GAAAD,CAAA,KAAAE,OAAA,EAAAF,CAAA;AAmDrB,SAASG,sBAAsBA,CAACC,IAAuB,EAAqB;EACjF,OAAOA,IAAI;AACb;;AAEA;AACA;AACA;AACA;AACA;AACA;AACO,eAAeC,sBAAsBA,CAC1CC,SAAiB,EACjB;EAAEC;AAAqC,CAAC,EACzB;EACf,MAAMC,kBAAE,CAACC,SAAS,CAACH,SAAS,CAAC;EAE7B,MAAME,kBAAE,CAACE,SAAS,CAChB,IAAAC,YAAI,EAACL,SAAS,EAAE,eAAe,CAAC,EAChCM,IAAI,CAACC,SAAS,CACZ;IACEN,MAAM;IACNO,IAAI,EAAE;MACJC,OAAO,EAAE,CAAC;MACV;MACAC,MAAM,EAAE;IACV;EACF,CAAC,EACD,IAAI,EACJ,CACF,CACF,CAAC;AACH","ignoreList":[]}

packages/@expo/prebuild-config/build/plugins/unversioned/expo-splash-screen/withIosSplashAssets.js
@@ -174,6 +174,10 @@ async function generateImagesAssetsAsync({
   const items = [[anyItem, PNG_FILENAME], [darkItem, DARK_PNG_FILENAME], [tabletItem, TABLET_PNG_FILENAME], [darkTabletItem, DARK_TABLET_PNG_FILENAME]].filter(([item]) => !!item);
   await Promise.all(items.map(([item, fileName]) => generateImageAsset(item, fileName)));
 }
+const lightAppearances = [{
+  appearance: 'luminosity',
+  value: 'light'
+}];
 const darkAppearances = [{
   appearance: 'luminosity',
   value: 'dark'
@@ -188,14 +192,17 @@ function buildContentsJsonImages({
   // Phone light
   (0, _AssetContents().createContentsJsonItem)({
     idiom: 'universal',
+    appearances: lightAppearances,
     filename: `${image}.png`,
     scale: '1x'
   }), (0, _AssetContents().createContentsJsonItem)({
     idiom: 'universal',
+    appearances: lightAppearances,
     filename: `${image}@2x.png`,
     scale: '2x'
   }), (0, _AssetContents().createContentsJsonItem)({
     idiom: 'universal',
+    appearances: lightAppearances,
     filename: `${image}@3x.png`,
     scale: '3x'
   }),

packages/@expo/prebuild-config/build/plugins/unversioned/expo-splash-screen/withIosSplashColors.js
@@ -93,7 +93,11 @@ async function writeColorsContentsJsonFileAsync({
       },
       'color-space': 'srgb'
     },
-    idiom: 'universal'
+    idiom: 'universal',
+    appearances: [{
+      appearance: 'luminosity',
+      value: 'light'
+    }]
   }];
   if (darkColor) {
     colors.push({

commit 337f78b419436297056f1773dda931e55cfb3e6e

[splashscreen] Guard againist null props (#33487)
# Why
Fixes a regression from #33472

# How
Don't attempt to destructure the props when they are null and skip
flattening them

# Test Plan
Sandbox

packages/expo-splash-screen/plugin/build/withSplashScreen.d.ts
@@ -14,5 +14,5 @@ type PluginConfig = {
     android?: AndroidSplashConfig;
     ios?: IOSSplashConfig;
 };
-declare const _default: ConfigPlugin<PluginConfig>;
+declare const _default: ConfigPlugin<PluginConfig | null>;
 export default _default;

packages/expo-splash-screen/plugin/build/withSplashScreen.js
@@ -5,6 +5,11 @@ const withIosSplashScreen_1 = require("@expo/prebuild-config/build/plugins/unver
 const config_plugins_1 = require("expo/config-plugins");
 const pkg = require('expo-splash-screen/package.json');
 const withSplashScreen = (config, props) => {
+    if (!props) {
+        config = (0, withAndroidSplashScreen_1.withAndroidSplashScreen)(config, null);
+        config = (0, withIosSplashScreen_1.withIosSplashScreen)(config, null);
+        return config;
+    }
     const resizeMode = props?.resizeMode || 'contain';
     const { ios: iosProps, android: androidProps, ...otherProps } = props;
     const android = {
@@ -27,7 +32,7 @@ const withSplashScreen = (config, props) => {
     };
     // Need to pass null here if we don't receive any props. This means that the plugin has not been used.
     // This only happens on Android. On iOS, if you don't use the plugin, this function won't be called.
-    config = (0, withAndroidSplashScreen_1.withAndroidSplashScreen)(config, props ? android : null);
+    config = (0, withAndroidSplashScreen_1.withAndroidSplashScreen)(config, android);
     config = (0, withIosSplashScreen_1.withIosSplashScreen)(config, ios);
     return config;
 };

packages/expo-splash-screen/plugin/src/withSplashScreen.ts
@@ -20,7 +20,13 @@ type PluginConfig = {
   ios?: IOSSplashConfig;
 };
 
-const withSplashScreen: ConfigPlugin<PluginConfig> = (config, props) => {
+const withSplashScreen: ConfigPlugin<PluginConfig | null> = (config, props) => {
+  if (!props) {
+    config = withAndroidSplashScreen(config, null);
+    config = withIosSplashScreen(config, null);
+    return config;
+  }
+
   const resizeMode = props?.resizeMode || 'contain';
 
   const { ios: iosProps, android: androidProps, ...otherProps } = props;
@@ -46,7 +52,7 @@ const withSplashScreen: ConfigPlugin<PluginConfig> = (config, props) => {
 
   // Need to pass null here if we don't receive any props. This means that the plugin has not been used.
   // This only happens on Android. On iOS, if you don't use the plugin, this function won't be called.
-  config = withAndroidSplashScreen(config, props ? android : null);
+  config = withAndroidSplashScreen(config, android);
   config = withIosSplashScreen(config, ios);
   return config;
 };