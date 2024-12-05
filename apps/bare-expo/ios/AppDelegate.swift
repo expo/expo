51c1f8a7fa6c235fbe2419e8732aeecf83867c7f
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2024-12-05 10:55:33 +0100

    Add app delegate functions for Linking API

--- apps/bare-expo/ios/AppDelegate.swift
+++ apps/bare-expo/ios/AppDelegate.swift
@@ -1,7 +1,7 @@
 import React
 import Expo
 
-@main
+@UIApplicationMain
 public class AppDelegate: ExpoAppDelegate {
   public override func application(
     _ application: UIApplication,
@@ -19,5 +19,24 @@ public class AppDelegate: ExpoAppDelegate {
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
+  }
+
+  // Linking API
+  public override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    return super.application(app, open: url, options: options) || RCTLinkingManager.application(app, open: url, options: options)
+  }
+
+  // Universal Links
+  public override func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    let result = RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
+    return super.application(application, continue: userActivity, restorationHandler: restorationHandler) || result
   }
 }
