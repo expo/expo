27cc64641b5c95f5ae93c365691ee75155887418
Cedric van Putten <github@cedric.dev>
2024-12-09 15:00:23 +0100

    test(cli): simplify and clean `01-rsc` playwright test

--- packages/@expo/cli/e2e/playwright/prod/01-rsc.test.ts
+++ packages/@expo/cli/e2e/playwright/prod/01-rsc.test.ts
@@ -1,12 +1,11 @@
 import { expect, test } from '@playwright/test';
-import execa from 'execa';
-import fs from 'fs';
-import klawSync from 'klaw-sync';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 
 import { clearEnv, restoreEnv } from '../../__tests__/export/export-side-effects';
-import { getRouterE2ERoot } from '../../__tests__/utils';
-import { bin, ExpoServeLocalCommand } from '../../utils/command-instance';
+import { findProjectFiles, getRouterE2ERoot } from '../../__tests__/utils';
+import { createExpoServe, executeExpoAsync } from '../../utils/expo';
+import { pageCollectErrors } from '../page';
 
 test.beforeAll(() => clearEnv());
 test.afterAll(() => restoreEnv());
@@ -14,17 +13,19 @@ test.afterAll(() => restoreEnv());
 const projectRoot = getRouterE2ERoot();
 const inputDir = 'dist-01-rsc';
 
-test.beforeAll(async () => {
-  // Could take 45s depending on how fast the bundler resolves
-  test.setTimeout(560 * 1000);
+const expo = createExpoServe({
+  cwd: projectRoot,
+  env: {
+    NODE_ENV: 'production',
+    TEST_SECRET_VALUE: 'test-secret-dynamic',
+  },
 });
 
-let serveCmd: ExpoServeLocalCommand;
-
+// These tests modify the same files in the file system, so run them in serial
+test.describe.configure({ mode: 'serial' });
 test.beforeAll('bundle and serve', async () => {
   console.time('expo export');
-  await execa('node', [bin, 'export', '-p', 'web', '--output-dir', inputDir], {
-    cwd: projectRoot,
+  await executeExpoAsync(projectRoot, ['export', '-p', 'web', '--output-dir', inputDir], {
     env: {
       E2E_ROUTER_JS_ENGINE: 'hermes',
       EXPO_USE_METRO_REQUIRE: '1',
@@ -44,121 +45,94 @@ test.beforeAll('bundle and serve', async () => {
     path.join(projectRoot, inputDir, 'client/second.html')
   );
 
-  serveCmd = new ExpoServeLocalCommand(projectRoot, {
-    NODE_ENV: 'production',
-    TEST_SECRET_VALUE: 'test-secret-dynamic',
-  });
-
-  console.time('npx serve');
-  await serveCmd.startAsync([inputDir, '--port=' + 3034]);
-  console.timeEnd('npx serve');
-  console.log('Server running:', serveCmd.url);
+  console.time('expo serve');
+  await expo.startAsync([inputDir]);
+  console.timeEnd('expo serve');
 });
-
 test.afterAll(async () => {
-  await serveCmd.stopAsync();
+  await expo.stopAsync();
 });
 
 const STATIC_RSC_PATH = path.join(projectRoot, inputDir, 'client/_flight');
 
-test.describe.serial(inputDir, () => {
-  // This test generally ensures no errors are thrown during an export loading.
-  test('loads without hydration errors', async ({ page }) => {
-    // Ensure the JS code has string module IDs
-    const rscFiles = klawSync(STATIC_RSC_PATH, {
-      nodir: true,
-    }).map((entry) => path.relative(STATIC_RSC_PATH, entry.path));
-    expect(rscFiles).toEqual(['web/index.txt']);
+// This test generally ensures no errors are thrown during an export loading.
+test('loads without hydration errors', async ({ page }) => {
+  // Ensure the JS code has string module IDs
+  expect(findProjectFiles(STATIC_RSC_PATH)).toEqual(['web/index.txt']);
 
-    console.time('Open page');
-    // Navigate to the app
-    await page.goto(serveCmd.url!);
+  // Listen for console logs and errors
+  const pageErrors = pageCollectErrors(page);
 
-    console.timeEnd('Open page');
+  console.time('Open page');
+  // Navigate to the app
+  await page.goto(expo.url.href);
+  console.timeEnd('Open page');
 
-    // Listen for console errors
-    const errorLogs: string[] = [];
-    page.on('console', (msg) => {
-      if (msg.type() === 'error') {
-        errorLogs.push(msg.text());
-      }
-    });
+  console.time('hydrate');
+  // Wait for the app to load
+  await page.waitForSelector('[data-testid="index-text"]');
 
-    // Listen for uncaught exceptions and console errors
-    const errors: string[] = [];
-    page.on('pageerror', (error) => {
-      errors.push(error.message);
-    });
+  await expect(page.locator('[data-testid="secret-text"]')).toHaveText('Secret: test-secret');
+  await expect(page.locator('[data-testid="index-path"]')).toHaveText('/');
+  await expect(page.locator('[data-testid="index-query"]')).toHaveText('');
 
-    console.time('hydrate');
-    // Wait for the app to load
-    await page.waitForSelector('[data-testid="index-text"]');
+  console.timeEnd('hydrate');
 
-    await expect(page.locator('[data-testid="secret-text"]')).toHaveText('Secret: test-secret');
-    await expect(page.locator('[data-testid="index-path"]')).toHaveText('/');
-    await expect(page.locator('[data-testid="index-query"]')).toHaveText('');
+  expect(pageErrors.all).toEqual([]);
 
-    console.timeEnd('hydrate');
+  // NOTE: I had issues splitting up the tests, so consider this the next test:
+  // it 'hydrates the client component'
 
-    expect(errorLogs).toEqual([]);
-    expect(errors).toEqual([]);
+  // Wait for the app to load
+  await page.waitForSelector('[data-testid="client-button"]');
 
-    // NOTE: I had issues splitting up the tests, so consider this the next test:
-    // it 'hydrates the client component'
+  await expect(page.locator('[data-testid="client-button"]')).toHaveText('Count: 0');
 
-    // Wait for the app to load
-    await page.waitForSelector('[data-testid="client-button"]');
+  // Click button
+  await page.locator('[data-testid="client-button"]').click();
 
-    await expect(page.locator('[data-testid="client-button"]')).toHaveText('Count: 0');
+  await expect(page.locator('[data-testid="client-button"]')).toHaveText('Count: 1');
 
-    // Click button
-    await page.locator('[data-testid="client-button"]').click();
+  // CSS styles exist
+  await expect(page.locator('[data-testid="layout-global-style"]')).toHaveCSS(
+    'background-color',
+    'rgb(0, 128, 0)'
+  );
+  await expect(page.locator('[data-testid="layout-module-style"]')).toHaveCSS(
+    'background-color',
+    'rgb(127, 255, 212)'
+  );
 
-    await expect(page.locator('[data-testid="client-button"]')).toHaveText('Count: 1');
+  // Ensure head has preloaded CSS files:
+  // <link rel="preload" href="/_expo/static/css/global-9c7022062f03d614bbbb2e534c66e10a.css" as="style"><link rel="stylesheet" href="/_expo/static/css/global-9c7022062f03d614bbbb2e534c66e10a.css"><link rel="preload" href="/_expo/static/css/home.module-8cd85e4c745d413359c336799092a7ea.css" as="style"><link rel="stylesheet" href="/_expo/static/css/home.module-8cd85e4c745d413359c336799092a7ea.css"></head>
+  await expect(page.locator('link[rel="preload"][as="style"][href*="global"]')).toBeAttached();
+  await expect(page.locator('link[rel="stylesheet"][href*="global"]')).toBeAttached();
+  await expect(page.locator('link[rel="preload"][as="style"][href*="home.module"]')).toBeAttached();
+  await expect(page.locator('link[rel="stylesheet"][href*="home.module"]')).toBeAttached();
+});
 
-    // CSS styles exist
-    await expect(page.locator('[data-testid="layout-global-style"]')).toHaveCSS(
-      'background-color',
-      'rgb(0, 128, 0)'
-    );
-    await expect(page.locator('[data-testid="layout-module-style"]')).toHaveCSS(
-      'background-color',
-      'rgb(127, 255, 212)'
-    );
+test('dynamically renders RSC', async ({ page }) => {
+  // Navigate to the app
+  await page.goto(new URL('/second', expo.url).href);
 
-    // Ensure head has preloaded CSS files:
-    // <link rel="preload" href="/_expo/static/css/global-9c7022062f03d614bbbb2e534c66e10a.css" as="style"><link rel="stylesheet" href="/_expo/static/css/global-9c7022062f03d614bbbb2e534c66e10a.css"><link rel="preload" href="/_expo/static/css/home.module-8cd85e4c745d413359c336799092a7ea.css" as="style"><link rel="stylesheet" href="/_expo/static/css/home.module-8cd85e4c745d413359c336799092a7ea.css"></head>
-    await expect(page.locator('link[rel="preload"][as="style"][href*="global"]')).toBeAttached();
-    await expect(page.locator('link[rel="stylesheet"][href*="global"]')).toBeAttached();
-    await expect(
-      page.locator('link[rel="preload"][as="style"][href*="home.module"]')
-    ).toBeAttached();
-    await expect(page.locator('link[rel="stylesheet"][href*="home.module"]')).toBeAttached();
-  });
+  // Wait for the app to load
+  await page.waitForSelector('[data-testid="second-text"]');
 
-  test('dynamically renders RSC', async ({ page }) => {
-    // Navigate to the app
-    await page.goto(new URL('/second', serveCmd.url).toString());
+  await expect(page.locator('[data-testid="secret-text"]')).toHaveText(
+    // Value should match the env var that we pass to the server after the build was completed, this will only work with dynamic rendering.
+    'Secret: test-secret-dynamic'
+  );
+});
 
-    // Wait for the app to load
-    await page.waitForSelector('[data-testid="second-text"]');
+test('has dynamic headers', async ({ page }) => {
+  // Navigate to the app
+  await page.goto(new URL('/second', expo.url).href);
 
-    await expect(page.locator('[data-testid="secret-text"]')).toHaveText(
-      // Value should match the env var that we pass to the server after the build was completed, this will only work with dynamic rendering.
-      'Secret: test-secret-dynamic'
-    );
-  });
+  // Wait for the app to load
+  await page.waitForSelector('[data-testid="second-header-platform"]');
 
-  test('has dynamic headers', async ({ page }) => {
-    // Navigate to the app
-    await page.goto(new URL('/second', serveCmd.url).toString());
-
-    // Wait for the app to load
-    await page.waitForSelector('[data-testid="second-header-platform"]');
-
-    await expect(page.locator('[data-testid="second-header-platform"]')).toHaveText(
-      // Ensure the headers are rendered as expected
-      'expo-platform: web'
-    );
-  });
+  await expect(page.locator('[data-testid="second-header-platform"]')).toHaveText(
+    // Ensure the headers are rendered as expected
+    'expo-platform: web'
+  );
 });
