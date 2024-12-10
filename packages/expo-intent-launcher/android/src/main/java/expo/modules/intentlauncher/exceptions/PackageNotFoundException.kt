60a04c103405e54ccdec74ce502b735d3ee27fb4
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2024-12-10 11:46:35 +0100

    Update packages/expo-intent-launcher/android/src/main/java/expo/modules/intentlauncher/exceptions/PackageNotFoundException.kt
    Co-authored-by: Łukasz Kosmaty <kosmatylukasz@gmail.com>

--- packages/expo-intent-launcher/android/src/main/java/expo/modules/intentlauncher/exceptions/PackageNotFoundException.kt
+++ packages/expo-intent-launcher/android/src/main/java/expo/modules/intentlauncher/exceptions/PackageNotFoundException.kt
@@ -1,10 +1,6 @@
 package expo.modules.intentlauncher.exceptions
 
-import expo.modules.core.errors.CodedException
+import expo.modules.kotlin.exception.CodedException
 
 class PackageNotFoundException(packageName: String) :
-  CodedException("Package not found: $packageName") {
-  override fun getCode(): String {
-    return "E_PACKAGE_NOT_FOUND"
-  }
-}
+  CodedException(message = "Package not found: $packageName")
