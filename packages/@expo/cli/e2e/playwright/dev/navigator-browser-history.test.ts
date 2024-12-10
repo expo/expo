9736b7cb102f80287b218e9283c358ba427aa858
Mark Lawlor <mwlawlor@gmail.com>
2024-12-10 17:48:35 -0300

    Update e2e test to new format

--- packages/@expo/cli/e2e/playwright/dev/navigator-browser-history.test.ts
+++ packages/@expo/cli/e2e/playwright/dev/navigator-browser-history.test.ts
@@ -1,8 +1,8 @@
-import { test, Page, WebSocket, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 import { clearEnv, restoreEnv } from '../../__tests__/export/export-side-effects';
 import { getRouterE2ERoot } from '../../__tests__/utils';
-import { ExpoStartCommand } from '../../utils/command-instance';
+import { createExpoStart } from '../../utils/expo';
 
 test.beforeAll(() => clearEnv());
 test.afterAll(() => restoreEnv());
@@ -10,16 +10,12 @@ test.afterAll(() => restoreEnv());
 const projectRoot = getRouterE2ERoot();
 const inputDir = 'navigator-browser-history';
 
+test.setTimeout(560 * 1000);
+
 test.describe(inputDir, () => {
-  test.beforeAll(async () => {
-    // Could take 45s depending on how fast the bundler resolves
-    test.setTimeout(560 * 1000);
-  });
-
-  let expo: ExpoStartCommand;
-
-  test.beforeEach(async () => {
-    expo = new ExpoStartCommand(projectRoot, {
+  const expoStart = createExpoStart({
+    cwd: projectRoot,
+    env: {
       NODE_ENV: 'production',
       EXPO_USE_STATIC: 'single',
       E2E_ROUTER_JS_ENGINE: 'hermes',
@@ -28,22 +24,24 @@ test.describe(inputDir, () => {
 
       // Ensure CI is disabled otherwise the file watcher won't run.
       CI: '0',
-    });
+    },
+  });
+
+  test.beforeEach(async () => {
+    await expoStart.startAsync();
+    await expoStart.fetchBundleAsync('/');
   });
 
   test.afterEach(async () => {
-    await expo.stopAsync();
+    await expoStart.stopAsync();
   });
 
   // Test expo router history by navigating through <Link>,
   // then using the browser back/forward actions
   test('navigator browser history', async ({ page }) => {
-    await expo.startAsync(['--port=8085']);
-    console.log('Server running:', expo.url);
-    await expo.fetchAsync('/');
     page.on('console', (msg) => console.log(msg.text()));
 
-    await page.goto(`${expo.url}/`);
+    await page.goto(`${expoStart.url}/`);
 
     // <Stack> in the browser currently works by setting hidden
     // screens to `display: none`, so we could just use 'home-content'
