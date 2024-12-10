c1731cfefc7049699de241525aaf03ccef182e09
Noel Blaschke <noel.blaschke@9elements.com>
2024-12-10 12:13:16 +0100

    [core] Declare specific interface for ExpoProcess's env

--- packages/expo-modules-core/src/ts-declarations/global.ts
+++ packages/expo-modules-core/src/ts-declarations/global.ts
@@ -60,24 +60,26 @@ type ViewConfig = {
   directEventTypes: Record<string, { registrationName: string }>;
 };
 
+export interface ExpoProcessEnv {
+  NODE_ENV: string;
+  /** Used in `@expo/metro-runtime`. */
+  EXPO_DEV_SERVER_ORIGIN?: string;
+
+  EXPO_ROUTER_IMPORT_MODE?: string;
+  EXPO_ROUTER_ABS_APP_ROOT?: string;
+  EXPO_ROUTER_APP_ROOT?: string;
+
+  /** Maps to the `experiments.baseUrl` property in the project Expo config. This is injected by `babel-preset-expo` and supports automatic cache invalidation. */
+  EXPO_BASE_URL?: string;
+
+  /** Build-time representation of the `Platform.OS` value that the current JavaScript was bundled for. Does not support platform shaking wrapped require statements. */
+  EXPO_OS?: string;
+
+  [key: string]: any;
+}
+
 export interface ExpoProcess {
-  env: {
-    NODE_ENV: string;
-    /** Used in `@expo/metro-runtime`. */
-    EXPO_DEV_SERVER_ORIGIN?: string;
-
-    EXPO_ROUTER_IMPORT_MODE?: string;
-    EXPO_ROUTER_ABS_APP_ROOT?: string;
-    EXPO_ROUTER_APP_ROOT?: string;
-
-    /** Maps to the `experiments.baseUrl` property in the project Expo config. This is injected by `babel-preset-expo` and supports automatic cache invalidation. */
-    EXPO_BASE_URL?: string;
-
-    /** Build-time representation of the `Platform.OS` value that the current JavaScript was bundled for. Does not support platform shaking wrapped require statements. */
-    EXPO_OS?: string;
-
-    [key: string]: any;
-  };
+  env: ExpoProcessEnv;
   [key: string]: any;
 }
 
