44d5b1f5370485086fe0c0731491a418edb6c78f
betomoedano <betomoedano@outlook.com>
2024-12-11 10:37:50 -0600

    [env-info] add expo-updates

--- packages/expo-env-info/src/diagnosticsAsync.ts
+++ packages/expo-env-info/src/diagnosticsAsync.ts
@@ -17,6 +17,7 @@ function getEnvironmentInfoAsync(): Promise<string> {
       npmPackages: [
         'expo',
         'expo-router',
+        'expo-updates',
         'react',
         'react-dom',
         'react-native',
