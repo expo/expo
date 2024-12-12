6bc1011bfa434514f917ddad200a579638e8305b
lukmccall <kosmatylukasz@gmail.com>
2024-12-12 17:18:04 +0100

    Add try-catch

--- packages/@expo/cli/src/start/server/middleware/inspector/JsInspector.ts
+++ packages/@expo/cli/src/start/server/middleware/inspector/JsInspector.ts
@@ -97,10 +97,18 @@ export async function queryAllInspectorAppsAsync(
     if (!pageIsSupported(app)) {
       continue;
     }
-    // Hide targets that are marked as hidden from the inspector, e.g. instances from expo-dev-menu and expo-dev-launcher.
-    if (await appShouldBeIgnoredAsync(app)) {
+
+    try {
+      // Hide targets that are marked as hidden from the inspector, e.g. instances from expo-dev-menu and expo-dev-launcher.
+      if (await appShouldBeIgnoredAsync(app)) {
+        continue;
+      }
+    } catch (e: unknown) {
+      // If we can't evaluate the JS, we just ignore the error and skips the target.
+      debug(`Can't evaluate the JS on the app:`, JSON.stringify(e, null, 2));
       continue;
     }
+
     results.push(app);
   }
   return results;
