180d39f0a41873fae72f9ebc15213a442acef136
Kudo Chien <kudo@expo.dev>
2024-12-13 02:00:19 +0800

    [test-suite] increase maestro timeout

--- apps/bare-expo/scripts/start-ios-e2e-test.ts
+++ apps/bare-expo/scripts/start-ios-e2e-test.ts
@@ -178,7 +178,7 @@ appId: dev.expo.Payments
 - extendedWaitUntil:
     visible:
       id: "test_suite_container"
-    timeout: 60000
+    timeout: 180000
 - assertVisible:
     text: "Success!"
 `);
