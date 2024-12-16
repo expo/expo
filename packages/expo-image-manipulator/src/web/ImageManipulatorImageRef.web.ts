980d226dcfaec7d5fdaee9a61a20f61b584ff660
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2024-12-16 15:05:53 +0100

    [web][image-manipulator] Fix `saveAsync` not respecting the format and compression quality

--- packages/expo-image-manipulator/src/web/ImageManipulatorImageRef.web.ts
+++ packages/expo-image-manipulator/src/web/ImageManipulatorImageRef.web.ts
@@ -1,30 +1,48 @@
 import { SharedRef } from 'expo';
 
-import { ImageResult, SaveOptions } from '../ImageManipulator.types';
+import { ImageResult, SaveFormat, SaveOptions } from '../ImageManipulator.types';
 import { blobToBase64String } from './utils.web';
 
 export default class ImageManipulatorImageRef extends SharedRef<'image'> {
   readonly nativeRefType: string = 'image';
 
   readonly uri: string;
-  readonly width: number;
-  readonly height: number;
+  readonly canvas: HTMLCanvasElement;
 
-  constructor(uri: string, width: number, height: number) {
+  constructor(uri: string, canvas: HTMLCanvasElement) {
     super();
     this.uri = uri;
-    this.width = width;
-    this.height = height;
+    this.canvas = canvas;
+  }
+
+  get width() {
+    return this.canvas.width;
+  }
+
+  get height() {
+    return this.canvas.height;
   }
 
   async saveAsync(options: SaveOptions = { base64: false }): Promise<ImageResult> {
-    return {
-      uri: this.uri,
-      width: this.width,
-      height: this.height,
-      base64: options.base64
-        ? await blobToBase64String(await fetch(this.uri).then((response) => response.blob()))
-        : undefined,
-    };
+    return new Promise((resolve, reject) => {
+      this.canvas.toBlob(
+        async (blob) => {
+          if (!blob) {
+            return reject(new Error(`Unable to save image: ${this.uri}`));
+          }
+          const base64 = options.base64 ? await blobToBase64String(blob) : undefined;
+          const uri = blob ? URL.createObjectURL(blob) : this.canvas.toDataURL();
+
+          resolve({
+            uri,
+            width: this.width,
+            height: this.height,
+            base64,
+          });
+        },
+        `image/${options.format ?? SaveFormat.JPEG}`,
+        options.compress
+      );
+    });
   }
 }
