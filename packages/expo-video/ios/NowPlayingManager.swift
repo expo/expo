expo/expo

commit dd4432c04ca68f095259b05de15f229780f3851c

[video] Add `audioMixingMode` property (#32428)
# Why

Right now we will always pause other audio in the system if the video is
outputting audio.

# How

I added a few options to allow the users to specify the desired
behaviour for the player.

- `mixWithOthers`: The player will mix its audio output with other apps.
- `duckOthers`: The player will lower the volume of other apps if any of
the active players is outputting audio.
- `auto`: The player will allow other apps to keep playing audio only
when it is muted.
- `doNotMix`: The player will pause playback in other apps, even when
it's muted.

where `auto` is the default and is basically the current behaviour. 
Also did a small fix in the NowPlayingManager. We have to call an update
every time the AudioSession changes to make sure it shows up correctly.
The whole thing is a bit messy and I'd like to rework it in the future
but we don't have the time for this now.

# Test Plan

Tested in BareExpo on Android and iOS

packages/expo-video/ios/NowPlayingManager.swift
@@ -69,8 +69,7 @@ class NowPlayingManager: VideoPlayerObserverDelegate {
     artworkDataTask = nil
 
     self.mostRecentInteractionPlayer = player
-    self.setupNowPlayingControls()
-    self.updateNowPlayingInfo()
+    refreshNowPlaying()
 
     timeObserver = player?.addPeriodicTimeObserver(
       forInterval: CMTimeMake(value: 1, timescale: 4),
@@ -220,8 +219,7 @@ class NowPlayingManager: VideoPlayerObserverDelegate {
   }
 
   func onItemChanged(player: AVPlayer, oldVideoPlayerItem: VideoPlayerItem?, newVideoPlayerItem: VideoPlayerItem?) {
-    setupNowPlayingControls()
-    updateNowPlayingInfo()
+    refreshNowPlaying()
   }
 
   func onRateChanged(player: AVPlayer, oldRate: Float?, newRate: Float) {
@@ -237,9 +235,13 @@ class NowPlayingManager: VideoPlayerObserverDelegate {
   func onPlayerItemStatusChanged(player: AVPlayer, oldStatus: AVPlayerItem.Status?, newStatus: AVPlayerItem.Status) {
     // The player can be registered before it's item has loaded. We have to re-update the notification when item data is loaded
     if player == mostRecentInteractionPlayer && newStatus == .readyToPlay {
-      setupNowPlayingControls()
-      updateNowPlayingInfo()
+      refreshNowPlaying()
     }
+  }
+
+  func refreshNowPlaying() {
+    setupNowPlayingControls()
+    updateNowPlayingInfo()
   }
 }
 

apps/native-component-list/src/screens/Video/VideoAudioScreen.tsx
@@ -1,0 +1,151 @@
+import Slider from '@react-native-community/slider';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import { useVideoPlayer, VideoView, AudioMixingMode } from 'expo-video';
+import React, { useCallback, useRef, useState } from 'react';
+import { ScrollView, View, Text } from 'react-native';
+
+import { bigBuckBunnySource, elephantsDreamSource } from './videoSources';
+import { styles } from './videoStyles';
+import Button from '../../components/Button';
+import TitledSwitch from '../../components/TitledSwitch';
+
+const mixingModes: AudioMixingMode[] = ['mixWithOthers', 'duckOthers', 'auto', 'doNotMix'];
+export default function VideoNowPlayingScreen() {
+  const view1Ref = useRef<VideoView>(null);
+  const [volume, setVolume] = useState(1);
+  const [showNowPlaying, setShowNowPlaying] = useState(false);
+  const [player1MixingModeIndex, setPlayer1MixingModeIndex] = useState(2);
+  const [player2MixingModeIndex, setPlayer2MixingModeIndex] = useState(2);
+  const [player1IsMuted, setPlayer1IsMuted] = useState(true);
+  const [player2IsMuted, setPlayer2IsMuted] = useState(true);
+
+  const player = useVideoPlayer(bigBuckBunnySource, (player) => {
+    player.loop = true;
+    player.muted = player1IsMuted;
+    player.showNowPlayingNotification = showNowPlaying;
+    player.staysActiveInBackground = true;
+    player.audioMixingMode = mixingModes[player1MixingModeIndex];
+    player.play();
+  });
+
+  const player2 = useVideoPlayer(elephantsDreamSource, (player) => {
+    player.loop = true;
+    player.muted = player2IsMuted;
+    player.showNowPlayingNotification = showNowPlaying;
+    player.staysActiveInBackground = true;
+    player.audioMixingMode = mixingModes[player2MixingModeIndex];
+    player.play();
+  });
+
+  const applyShowNowPlaying = useCallback(
+    (value: boolean) => {
+      player.showNowPlayingNotification = value;
+      player2.showNowPlayingNotification = value;
+      setShowNowPlaying(value);
+    },
+    [player, showNowPlaying]
+  );
+
+  const applyPlayer1Mute = useCallback(
+    (value: boolean) => {
+      player.muted = value;
+      setPlayer1IsMuted(value);
+    },
+    [player]
+  );
+
+  const applyPlayer2Mute = useCallback(
+    (value: boolean) => {
+      player2.muted = value;
+      setPlayer2IsMuted(value);
+    },
+    [player2]
+  );
+
+  const applyVolume = useCallback(
+    (value: number) => {
+      player.volume = value;
+      player2.volume = value;
+      setVolume(value);
+    },
+    [player, player2]
+  );
+
+  return (
+    <View style={styles.contentContainer}>
+      <View style={[styles.row, { marginBottom: 20 }]}>
+        <VideoView
+          ref={view1Ref}
+          player={player}
+          style={{ width: 160, height: 90 }}
+          allowsPictureInPicture
+        />
+        <VideoView player={player2} style={{ width: 160, height: 90 }} />
+      </View>
+      <ScrollView style={styles.controlsContainer}>
+        <Text style={styles.switchTitle}>Volume</Text>
+        <Slider
+          style={{ alignSelf: 'stretch' }}
+          minimumValue={0}
+          maximumValue={1}
+          value={volume}
+          onValueChange={applyVolume}
+        />
+
+        <Text style={styles.switchTitle}>Player 1 AudioMode</Text>
+        <SegmentedControl
+          values={mixingModes}
+          selectedIndex={player1MixingModeIndex}
+          onValueChange={(value) => {
+            const audioMode = value as AudioMixingMode;
+            player.audioMixingMode = audioMode;
+            setPlayer1MixingModeIndex(mixingModes.indexOf(audioMode));
+          }}
+          backgroundColor="#e5e5e5"
+        />
+        <Text style={styles.switchTitle}>Player 2 AudioMode</Text>
+
+        <SegmentedControl
+          values={mixingModes}
+          selectedIndex={player2MixingModeIndex}
+          onValueChange={(value) => {
+            const audioMode = value as AudioMixingMode;
+            player2.audioMixingMode = audioMode;
+            setPlayer2MixingModeIndex(mixingModes.indexOf(audioMode));
+          }}
+          backgroundColor="#e5e5e5"
+        />
+        <TitledSwitch
+          title="Shows Now Playing Notification"
+          value={showNowPlaying}
+          setValue={applyShowNowPlaying}
+          style={styles.switch}
+          titleStyle={styles.switchTitle}
+        />
+        <View style={styles.row}>
+          <TitledSwitch
+            value={player1IsMuted}
+            setValue={applyPlayer1Mute}
+            title="Mute Player 1"
+            style={styles.switch}
+          />
+          <TitledSwitch
+            value={player2IsMuted}
+            setValue={applyPlayer2Mute}
+            title="Mute Player 2"
+            style={styles.switch}
+          />
+        </View>
+        <Button
+          style={styles.button}
+          title="Enter PiP"
+          onPress={() => view1Ref.current?.startPictureInPicture()}
+        />
+        <Text style={styles.centerText}>
+          Picture in Picture and Now Playing Notification rely on the AudioMode on iOS for "auto"
+          and "doNotMix" modes the notification should be visible.
+        </Text>
+      </ScrollView>
+    </View>
+  );
+}

apps/native-component-list/src/screens/Video/VideoScreen.tsx
@@ -5,6 +5,14 @@ import ComponentListScreen, { ListElement } from '../ComponentListScreen';
 
 export const VideoScreens = [
   {
+    name: 'Audio Options',
+    route: 'video/audio',
+    options: {},
+    getComponent() {
+      return optionalRequire(() => require('./VideoAudioScreen'));
+    },
+  },
+  {
     name: 'DRM',
     route: 'video/drm',
     options: {},

packages/expo-video/android/src/main/java/expo/modules/video/AudioFocusManager.kt
@@ -7,6 +7,7 @@ import android.media.AudioManager
 import android.os.Build
 import androidx.media3.common.util.UnstableApi
 import expo.modules.kotlin.AppContext
+import expo.modules.video.enums.AudioMixingMode
 import expo.modules.video.player.VideoPlayer
 import expo.modules.video.player.VideoPlayerListener
 import kotlinx.coroutines.launch
@@ -22,19 +23,37 @@ class AudioFocusManager(private val appContext: AppContext) : AudioManager.OnAud
 
   private var players: MutableList<WeakReference<VideoPlayer>> = mutableListOf()
   private var currentFocusRequest: AudioFocusRequest? = null
+  private var currentMixingMode: AudioMixingMode = AudioMixingMode.MIX_WITH_OTHERS
   private val anyPlayerRequiresFocus: Boolean
     get() = players.any {
       playerRequiresFocus(it)
     }
 
   private fun requestAudioFocus() {
+    val audioMixingMode = findAudioMixingMode()
+
+    // We don't request AudioFocus if we want to mix the audio with others
+    if (audioMixingMode == AudioMixingMode.MIX_WITH_OTHERS || !anyPlayerRequiresFocus) {
+      abandonAudioFocus()
+      currentMixingMode = audioMixingMode
+      return
+    }
+    val audioFocusType = when (audioMixingMode) {
+      AudioMixingMode.DUCK_OTHERS -> AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK
+      AudioMixingMode.AUTO -> AudioManager.AUDIOFOCUS_GAIN
+      AudioMixingMode.DO_NOT_MIX -> AudioManager.AUDIOFOCUS_GAIN
+      else -> AudioManager.AUDIOFOCUS_GAIN
+    }
+
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       // We already have audio focus
-      if (currentFocusRequest != null) {
-        return
+      currentFocusRequest?.let {
+        if (it.focusGain == audioFocusType) {
+          return
+        }
       }
 
-      val newFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN).run {
+      val newFocusRequest = AudioFocusRequest.Builder(audioFocusType).run {
         setAudioAttributes(
           AudioAttributes.Builder().run {
             setUsage(AudioAttributes.USAGE_MEDIA)
@@ -44,16 +63,17 @@ class AudioFocusManager(private val appContext: AppContext) : AudioManager.OnAud
           }
         ).build()
       }
-      this.currentFocusRequest = newFocusRequest
+      currentFocusRequest = newFocusRequest
       audioManager.requestAudioFocus(newFocusRequest)
     } else {
       @Suppress("DEPRECATION")
       audioManager.requestAudioFocus(
         this,
         AudioManager.STREAM_MUSIC,
-        AudioManager.AUDIOFOCUS_GAIN
+        audioFocusType
       )
     }
+    currentMixingMode = audioMixingMode
   }
 
   private fun abandonAudioFocus() {
@@ -83,6 +103,11 @@ class AudioFocusManager(private val appContext: AppContext) : AudioManager.OnAud
   }
 
   // VideoPlayerListener
+
+  override fun onAudioMixingModeChanged(player: VideoPlayer, audioMixingMode: AudioMixingMode, oldAudioMixingMode: AudioMixingMode?) {
+    requestAudioFocus()
+    super.onAudioMixingModeChanged(player, audioMixingMode, oldAudioMixingMode)
+  }
 
   override fun onIsPlayingChanged(player: VideoPlayer, isPlaying: Boolean, oldIsPlaying: Boolean?) {
     // we can't use `updateAudioFocus`, because when losing focus the videos are paused sequentially,
@@ -116,6 +141,12 @@ class AudioFocusManager(private val appContext: AppContext) : AudioManager.OnAud
       }
 
       AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
+        // W could pause/mix the players here individually, but we will keep the behaviour in line with iOS,
+        // find the dominant audioMixingMode and apply it to all players.
+        val audioMixingMode = findAudioMixingMode()
+        if (audioMixingMode == AudioMixingMode.MIX_WITH_OTHERS) {
+          return
+        }
         appContext.mainQueue.launch {
           players.forEach {
             pausePlayerIfUnmuted(it)
@@ -125,9 +156,15 @@ class AudioFocusManager(private val appContext: AppContext) : AudioManager.OnAud
       }
 
       AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
+        val audioMixingMode = findAudioMixingMode()
+
         appContext.mainQueue.launch {
           players.forEach {
-            duckPlayer(it)
+            if (audioMixingMode == AudioMixingMode.DO_NOT_MIX) {
+              pausePlayerIfUnmuted(it)
+            } else {
+              duckPlayer(it)
+            }
           }
         }
       }
@@ -148,10 +185,8 @@ class AudioFocusManager(private val appContext: AppContext) : AudioManager.OnAud
 
   private fun playerRequiresFocus(weakPlayer: WeakReference<VideoPlayer>): Boolean {
     return weakPlayer.get()?.let {
-      !it.muted && it.playing && it.volume > 0
-    } ?: run {
-      false
-    }
+      (!it.muted && it.playing && it.volume > 0) || it.audioMixingMode == AudioMixingMode.DO_NOT_MIX
+    } ?: false
   }
 
   private fun pausePlayerIfUnmuted(weakPlayer: WeakReference<VideoPlayer>) {
@@ -183,10 +218,23 @@ class AudioFocusManager(private val appContext: AppContext) : AudioManager.OnAud
   }
 
   private fun updateAudioFocus() {
-    if (anyPlayerRequiresFocus) {
+    if (anyPlayerRequiresFocus || findAudioMixingMode() != currentMixingMode) {
       requestAudioFocus()
     } else {
       abandonAudioFocus()
+    }
+  }
+
+  private fun findAudioMixingMode(): AudioMixingMode {
+    val mixingModes = players.mapNotNull { player ->
+      player.get()?.takeIf { it.playing }?.audioMixingMode
+    }
+    if (mixingModes.isEmpty()) {
+      return AudioMixingMode.MIX_WITH_OTHERS
+    }
+
+    return mixingModes.reduce { currentAudioMixingMode, next ->
+      next.takeIf { it.priority > currentAudioMixingMode.priority } ?: currentAudioMixingMode
     }
   }
 }

packages/expo-video/android/src/main/java/expo/modules/video/VideoModule.kt
@@ -16,6 +16,7 @@ import expo.modules.kotlin.functions.Coroutine
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import expo.modules.kotlin.types.Either
+import expo.modules.video.enums.AudioMixingMode
 import expo.modules.video.enums.ContentFit
 import expo.modules.video.player.VideoPlayer
 import expo.modules.video.records.BufferOptions
@@ -316,6 +317,16 @@ class VideoModule : Module() {
         }
         .set { ref: VideoPlayer, intervalSeconds: Float ->
           ref.intervalUpdateClock.interval = (intervalSeconds * 1000).toLong()
+        }
+
+      Property("audioMixingMode")
+        .get { ref: VideoPlayer ->
+          ref.audioMixingMode
+        }
+        .set { ref: VideoPlayer, audioMixingMode: AudioMixingMode ->
+          appContext.mainQueue.launch {
+            ref.audioMixingMode = audioMixingMode
+          }
         }
 
       Function("replace") { ref: VideoPlayer, source: Either<Uri, VideoSource>? ->

packages/expo-video/android/src/main/java/expo/modules/video/enums/AudioMixingMode.kt
@@ -1,0 +1,20 @@
+package expo.modules.video.enums
+
+import expo.modules.kotlin.types.Enumerable
+
+enum class AudioMixingMode(val value: String) : Enumerable {
+  MIX_WITH_OTHERS("mixWithOthers"),
+  DUCK_OTHERS("duckOthers"),
+  AUTO("auto"),
+  DO_NOT_MIX("doNotMix");
+
+  val priority: Int
+    get() {
+      return when (this) {
+        DO_NOT_MIX -> 3
+        AUTO -> 2
+        DUCK_OTHERS -> 1
+        MIX_WITH_OTHERS -> 0
+      }
+    }
+}

packages/expo-video/android/src/main/java/expo/modules/video/player/PlayerEvent.kt
@@ -3,6 +3,7 @@ package expo.modules.video.player
 import androidx.annotation.OptIn
 import androidx.media3.common.Tracks
 import androidx.media3.common.util.UnstableApi
+import expo.modules.video.enums.AudioMixingMode
 import expo.modules.video.enums.PlayerStatus
 import expo.modules.video.records.IsPlayingEventPayload
 import expo.modules.video.records.MutedChangedEventPayload
@@ -61,6 +62,11 @@ sealed class PlayerEvent {
     override val jsEventPayload = timeUpdate
   }
 
+  data class AudioMixingModeChanged(val audioMixingMode: AudioMixingMode, val oldAudioMixingMode: AudioMixingMode?) : PlayerEvent() {
+    override val name = "audioMixingModeChange"
+    override val emitToJS = false
+  }
+
   class PlayedToEnd : PlayerEvent() {
     override val name = "playToEnd"
   }
@@ -76,6 +82,7 @@ sealed class PlayerEvent {
       is TimeUpdated -> listeners.forEach { it.onTimeUpdate(player, timeUpdate) }
       is PlayedToEnd -> listeners.forEach { it.onPlayedToEnd(player) }
       is MutedChanged -> listeners.forEach { it.onMutedChanged(player, muted, oldMuted) }
+      is AudioMixingModeChanged -> listeners.forEach { it.onAudioMixingModeChanged(player, audioMixingMode, oldAudioMixingMode) }
     }
   }
 }

packages/expo-video/android/src/main/java/expo/modules/video/player/VideoPlayer.kt
@@ -22,6 +22,7 @@ import expo.modules.video.IntervalUpdateClock
 import expo.modules.video.IntervalUpdateEmitter
 import expo.modules.video.VideoManager
 import expo.modules.video.delegates.IgnoreSameSet
+import expo.modules.video.enums.AudioMixingMode
 import expo.modules.video.enums.PlayerStatus
 import expo.modules.video.enums.PlayerStatus.*
 import expo.modules.video.playbackService.ExpoVideoPlaybackService
@@ -138,6 +139,13 @@ class VideoPlayer(val context: Context, appContext: AppContext, source: VideoSou
         return 0.0
       }
       return player.bufferedPosition / 1000.0
+    }
+
+  var audioMixingMode: AudioMixingMode = AudioMixingMode.AUTO
+    set(value) {
+      val old = field
+      field = value
+      sendEvent(PlayerEvent.AudioMixingModeChanged(value, old))
     }
 
   private val playerListener = object : Player.Listener {

commit ce6ebf478e50951786236017b8c044408b1a137a

[video][ios] Fix now playing notification being always on (#33698)
# Why

Reported by a person from Software Mansion, seems to be a regression,
most likely caused by https://github.com/expo/expo/pull/32428

# How

Make sure not to setup the controls when there is no player registered

# Test Plan

Tested in BareExpo on a physical iOS18 device

---------

Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

packages/expo-video/ios/NowPlayingManager.swift
@@ -84,6 +84,10 @@ class NowPlayingManager: VideoPlayerObserverDelegate {
 
     removeExistingTargets(commandCenter: commandCenter)
 
+    guard self.mostRecentInteractionPlayer != nil else {
+      return
+    }
+
     playTarget = commandCenter.playCommand.addTarget { [weak self] _ in
       guard let self, let player = self.mostRecentInteractionPlayer else {
         return .commandFailed