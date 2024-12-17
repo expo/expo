c0b9d84d852b8d15e1f9a6b63c4568a8921a490b
Christian Falch <christian.falch@gmail.com>
2024-12-17 12:09:05 +0100

    [codereview] Made exceptions internal

--- packages/expo-background-task/android/src/main/java/expo/modules/backgroundtask/BackgroundTaskExceptions.kt
+++ packages/expo-background-task/android/src/main/java/expo/modules/backgroundtask/BackgroundTaskExceptions.kt
@@ -2,12 +2,12 @@ package expo.modules.backgroundtask
 
 import expo.modules.kotlin.exception.CodedException
 
-class MissingContextException : CodedException(message = "Application context not found")
+internal class MissingContextException : CodedException(message = "Application context not found")
 
-class MissingTaskServiceException : CodedException(message = "TaskService not available.")
+internal class MissingTaskServiceException : CodedException(message = "TaskService not available.")
 
-class MissingAppScopeKey : CodedException(message = "Could not find required appScopeKey in worker.")
+internal class MissingAppScopeKey : CodedException(message = "Could not find required appScopeKey in worker.")
 
-class TaskMangerInterfaceNotFoundException : CodedException(message = "TaskManagerInterface not found")
+internal class TaskMangerInterfaceNotFoundException : CodedException(message = "TaskManagerInterface not found")
 
-class TestMethodNotAvailableInProductionBuild : CodedException(message = "Background tasks cannot be triggered in production builds")
+internal class TestMethodNotAvailableInProductionBuild : CodedException(message = "Background tasks cannot be triggered in production builds")
