70d8dcb228aaa8bd5d3495a94e523d8651e9ee90
Christian Falch <christian.falch@gmail.com>
2024-12-18 12:36:14 +0100

    [codereview] regenerated build files

--- packages/expo-background-task/build/BackgroundTask.d.ts
+++ packages/expo-background-task/build/BackgroundTask.d.ts
@@ -1,9 +1,9 @@
 import { BackgroundTaskOptions, BackgroundTaskStatus } from './BackgroundTask.types';
 /**
  * Returns the status for the Background Task API. On web, it always returns `BackgroundTaskStatus.Restricted`,
- * while on native platforms it returns `BackgroundTaskStatus.Available`. There is
+ * while on native platforms it returns `BackgroundTaskStatus.Available`.
  *
- * @returns A BackgroundTaskStatus enum value or null if not available.
+ * @returns A BackgroundTaskStatus enum value or `null` if not available.
  */
 export declare const getStatusAsync: () => Promise<BackgroundTaskStatus>;
 /**
