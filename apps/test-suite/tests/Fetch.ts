expo/expo

commit 04c6ae2398dbb42269b659b046d177b572d596d3

[expo] fix fetch abort error for streaming requests (#33577)
# Why

i unregistered the abort event listener earlier before the request
finished.
fixes #33553

# How

unregister the abort event listener when native request is finished. i
introduced a new `readyForJSFinalization` event that emits from both
streaming and non-streaming requests.

packages/expo/android/src/main/java/expo/modules/fetch/NativeResponse.kt
@@ -72,8 +72,13 @@ internal class NativeResponse(appContext: AppContext, private val coroutineScope
   }
 
   fun emitRequestCancelled() {
-    error = FetchRequestCancelledException()
+    val error = FetchRequestCancelledException()
+    this.error = error
+    if (state == ResponseState.BODY_STREAMING_STARTED) {
+      emit("didFailWithError", error)
+    }
     state = ResponseState.ERROR_RECEIVED
+    emit("readyForJSFinalization")
   }
 
   fun waitForStates(states: List<ResponseState>, callback: (ResponseState) -> Unit) {
@@ -108,6 +113,7 @@ internal class NativeResponse(appContext: AppContext, private val coroutineScope
     }
     error = e
     state = ResponseState.ERROR_RECEIVED
+    emit("readyForJSFinalization")
   }
 
   override fun onResponse(call: Call, response: Response) {
@@ -123,6 +129,7 @@ internal class NativeResponse(appContext: AppContext, private val coroutineScope
         emit("didComplete")
       }
       this@NativeResponse.state = ResponseState.BODY_COMPLETED
+      emit("readyForJSFinalization")
     }
   }
 
@@ -158,22 +165,30 @@ internal class NativeResponse(appContext: AppContext, private val coroutineScope
   }
 
   private fun pumpResponseBodyStream(stream: BufferedSource) {
-    while (!stream.exhausted()) {
-      if (isInvalidState(
-          ResponseState.RESPONSE_RECEIVED,
-          ResponseState.BODY_STREAMING_STARTED,
-          ResponseState.BODY_STREAMING_CANCELLED
-        )
-      ) {
-        break
+    try {
+      while (!stream.exhausted()) {
+        if (isInvalidState(
+            ResponseState.RESPONSE_RECEIVED,
+            ResponseState.BODY_STREAMING_STARTED,
+            ResponseState.BODY_STREAMING_CANCELLED
+          )
+        ) {
+          break
+        }
+        if (state == ResponseState.RESPONSE_RECEIVED) {
+          sink.appendBufferBody(stream.buffer.readByteArray())
+        } else if (state == ResponseState.BODY_STREAMING_STARTED) {
+          emit("didReceiveResponseData", stream.buffer.readByteArray())
+        } else {
+          break
+        }
       }
-      if (state == ResponseState.RESPONSE_RECEIVED) {
-        sink.appendBufferBody(stream.buffer.readByteArray())
-      } else if (state == ResponseState.BODY_STREAMING_STARTED) {
-        emit("didReceiveResponseData", stream.buffer.readByteArray())
-      } else {
-        break
+    } catch (e: IOException) {
+      this.error = e
+      if (state == ResponseState.BODY_STREAMING_STARTED) {
+        emit("didFailWithError", e)
       }
+      state = ResponseState.ERROR_RECEIVED
     }
   }
 

packages/expo/build/winter/fetch/FetchResponse.d.ts
@@ -1,11 +1,14 @@
 import { ReadableStream } from 'web-streams-polyfill';
 import type { NativeResponse } from './NativeRequest';
 declare const ConcreteNativeResponse: typeof NativeResponse;
+export type AbortSubscriptionCleanupFunction = () => void;
 /**
  * A response implementation for the `fetch.Response` API.
  */
 export declare class FetchResponse extends ConcreteNativeResponse implements Response {
+    private readonly abortCleanupFunction;
     private streamingStarted;
+    constructor(abortCleanupFunction: AbortSubscriptionCleanupFunction);
     get body(): ReadableStream<Uint8Array> | null;
     get headers(): Headers;
     get ok(): boolean;
@@ -16,7 +19,7 @@ export declare class FetchResponse extends ConcreteNativeResponse implements Res
     toString(): string;
     toJSON(): object;
     clone(): FetchResponse;
-    private removeAllRegisteredListeners;
+    private finalize;
 }
 export {};
 //# sourceMappingURL=FetchResponse.d.ts.map

packages/expo/build/winter/fetch/FetchResponse.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"FetchResponse.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/FetchResponse.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,cAAc,EAAE,MAAM,sBAAsB,CAAC;AAGtD,OAAO,KAAK,EAAE,cAAc,EAAE,MAAM,iBAAiB,CAAC;AAEtD,QAAA,MAAM,sBAAsB,uBAA0D,CAAC;AAEvF;;GAEG;AACH,qBAAa,aAAc,SAAQ,sBAAuB,YAAW,QAAQ;IAC3E,OAAO,CAAC,gBAAgB,CAAS;IAEjC,IAAI,IAAI,IAAI,cAAc,CAAC,UAAU,CAAC,GAAG,IAAI,CA6B5C;IAED,IAAI,OAAO,IAAI,OAAO,CAErB;IAED,IAAI,EAAE,IAAI,OAAO,CAEhB;IAED,SAAgB,IAAI,aAAa;IAE3B,IAAI,IAAI,OAAO,CAAC,IAAI,CAAC;IAKrB,QAAQ,IAAI,OAAO,CAAC,QAAQ,CAAC;IAY7B,IAAI,IAAI,OAAO,CAAC,GAAG,CAAC;IAK1B,QAAQ,IAAI,MAAM;IAIlB,MAAM,IAAI,MAAM;IAShB,KAAK,IAAI,aAAa;IAItB,OAAO,CAAC,4BAA4B;CAKrC"}
+{"version":3,"file":"FetchResponse.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/FetchResponse.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,cAAc,EAAE,MAAM,sBAAsB,CAAC;AAGtD,OAAO,KAAK,EAAE,cAAc,EAAE,MAAM,iBAAiB,CAAC;AAEtD,QAAA,MAAM,sBAAsB,uBAA0D,CAAC;AAEvF,MAAM,MAAM,gCAAgC,GAAG,MAAM,IAAI,CAAC;AAE1D;;GAEG;AACH,qBAAa,aAAc,SAAQ,sBAAuB,YAAW,QAAQ;IAG/D,OAAO,CAAC,QAAQ,CAAC,oBAAoB;IAFjD,OAAO,CAAC,gBAAgB,CAAS;gBAEJ,oBAAoB,EAAE,gCAAgC;IAKnF,IAAI,IAAI,IAAI,cAAc,CAAC,UAAU,CAAC,GAAG,IAAI,CA0B5C;IAED,IAAI,OAAO,IAAI,OAAO,CAErB;IAED,IAAI,EAAE,IAAI,OAAO,CAEhB;IAED,SAAgB,IAAI,aAAa;IAE3B,IAAI,IAAI,OAAO,CAAC,IAAI,CAAC;IAKrB,QAAQ,IAAI,OAAO,CAAC,QAAQ,CAAC;IAY7B,IAAI,IAAI,OAAO,CAAC,GAAG,CAAC;IAK1B,QAAQ,IAAI,MAAM;IAIlB,MAAM,IAAI,MAAM;IAShB,KAAK,IAAI,aAAa;IAItB,OAAO,CAAC,QAAQ,CAQd;CACH"}

packages/expo/build/winter/fetch/NativeRequest.d.ts
@@ -13,6 +13,7 @@ export type NativeResponseEvents = {
     didReceiveResponseData(data: Uint8Array): void;
     didComplete(): void;
     didFailWithError(error: string): void;
+    readyForJSFinalization(): void;
 };
 export declare class NativeResponse extends SharedObject<NativeResponseEvents> {
     readonly bodyUsed: boolean;

packages/expo/build/winter/fetch/NativeRequest.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"NativeRequest.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/NativeRequest.ts"],"names":[],"mappings":"AAAA,OAAO,KAAK,EAAE,YAAY,EAAE,MAAM,mBAAmB,CAAC;AAEtD,MAAM,MAAM,iBAAiB,GAAG,CAAC,MAAM,EAAE,MAAM,CAAC,EAAE,CAAC;AAEnD,MAAM,CAAC,OAAO,OAAO,aAAc,SAAQ,YAAY;IAC9C,KAAK,CACV,GAAG,EAAE,MAAM,EACX,WAAW,EAAE,iBAAiB,EAC9B,WAAW,EAAE,UAAU,GAAG,IAAI,GAC7B,OAAO,CAAC,cAAc,CAAC;IACnB,MAAM,IAAI,IAAI;CACtB;AAED,MAAM,WAAW,iBAAiB;IAChC,WAAW,CAAC,EAAE,kBAAkB,CAAC;IACjC,OAAO,CAAC,EAAE,iBAAiB,CAAC;IAC5B,MAAM,CAAC,EAAE,MAAM,CAAC;CACjB;AAED,MAAM,MAAM,oBAAoB,GAAG;IACjC,sBAAsB,CAAC,IAAI,EAAE,UAAU,GAAG,IAAI,CAAC;IAC/C,WAAW,IAAI,IAAI,CAAC;IACpB,gBAAgB,CAAC,KAAK,EAAE,MAAM,GAAG,IAAI,CAAC;CACvC,CAAC;AAEF,MAAM,CAAC,OAAO,OAAO,cAAe,SAAQ,YAAY,CAAC,oBAAoB,CAAC;IAC5E,QAAQ,CAAC,QAAQ,EAAE,OAAO,CAAC;IAC3B,QAAQ,CAAC,WAAW,EAAE,iBAAiB,CAAC;IACxC,QAAQ,CAAC,MAAM,EAAE,MAAM,CAAC;IACxB,QAAQ,CAAC,UAAU,EAAE,MAAM,CAAC;IAC5B,QAAQ,CAAC,GAAG,EAAE,MAAM,CAAC;IACrB,QAAQ,CAAC,UAAU,EAAE,OAAO,CAAC;IAC7B,cAAc,IAAI,IAAI;IACtB,eAAe,CAAC,MAAM,EAAE,MAAM,GAAG,IAAI;IACrC,WAAW,IAAI,OAAO,CAAC,WAAW,CAAC;IACnC,IAAI,IAAI,OAAO,CAAC,MAAM,CAAC;CACxB"}
+{"version":3,"file":"NativeRequest.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/NativeRequest.ts"],"names":[],"mappings":"AAAA,OAAO,KAAK,EAAE,YAAY,EAAE,MAAM,mBAAmB,CAAC;AAEtD,MAAM,MAAM,iBAAiB,GAAG,CAAC,MAAM,EAAE,MAAM,CAAC,EAAE,CAAC;AAEnD,MAAM,CAAC,OAAO,OAAO,aAAc,SAAQ,YAAY;IAC9C,KAAK,CACV,GAAG,EAAE,MAAM,EACX,WAAW,EAAE,iBAAiB,EAC9B,WAAW,EAAE,UAAU,GAAG,IAAI,GAC7B,OAAO,CAAC,cAAc,CAAC;IACnB,MAAM,IAAI,IAAI;CACtB;AAED,MAAM,WAAW,iBAAiB;IAChC,WAAW,CAAC,EAAE,kBAAkB,CAAC;IACjC,OAAO,CAAC,EAAE,iBAAiB,CAAC;IAC5B,MAAM,CAAC,EAAE,MAAM,CAAC;CACjB;AAED,MAAM,MAAM,oBAAoB,GAAG;IACjC,sBAAsB,CAAC,IAAI,EAAE,UAAU,GAAG,IAAI,CAAC;IAC/C,WAAW,IAAI,IAAI,CAAC;IACpB,gBAAgB,CAAC,KAAK,EAAE,MAAM,GAAG,IAAI,CAAC;IACtC,sBAAsB,IAAI,IAAI,CAAC;CAChC,CAAC;AAEF,MAAM,CAAC,OAAO,OAAO,cAAe,SAAQ,YAAY,CAAC,oBAAoB,CAAC;IAC5E,QAAQ,CAAC,QAAQ,EAAE,OAAO,CAAC;IAC3B,QAAQ,CAAC,WAAW,EAAE,iBAAiB,CAAC;IACxC,QAAQ,CAAC,MAAM,EAAE,MAAM,CAAC;IACxB,QAAQ,CAAC,UAAU,EAAE,MAAM,CAAC;IAC5B,QAAQ,CAAC,GAAG,EAAE,MAAM,CAAC;IACrB,QAAQ,CAAC,UAAU,EAAE,OAAO,CAAC;IAC7B,cAAc,IAAI,IAAI;IACtB,eAAe,CAAC,MAAM,EAAE,MAAM,GAAG,IAAI;IACrC,WAAW,IAAI,OAAO,CAAC,WAAW,CAAC;IACnC,IAAI,IAAI,OAAO,CAAC,MAAM,CAAC;CACxB"}

packages/expo/ios/Fetch/NativeResponse.swift
@@ -59,8 +59,13 @@ internal final class NativeResponse: SharedObject, ExpoURLSessionTaskDelegate {
   }
 
   func emitRequestCanceled() {
-    error = FetchRequestCanceledException()
+    let error = FetchRequestCanceledException()
+    self.error = error
+    if state == .bodyStreamingStarted {
+      emit(event: "didFailWithError", arguments: error.localizedDescription)
+    }
     state = .errorReceived
+    emit(event: "readyForJSFinalization")
   }
 
   /**
@@ -197,5 +202,7 @@ internal final class NativeResponse: SharedObject, ExpoURLSessionTaskDelegate {
     } else {
       state = .bodyCompleted
     }
+
+    emit(event: "readyForJSFinalization")
   }
 }

packages/expo/src/winter/fetch/FetchResponse.ts
@@ -5,11 +5,18 @@ import type { NativeResponse } from './NativeRequest';
 
 const ConcreteNativeResponse = ExpoFetchModule.NativeResponse as typeof NativeResponse;
 
+export type AbortSubscriptionCleanupFunction = () => void;
+
 /**
  * A response implementation for the `fetch.Response` API.
  */
 export class FetchResponse extends ConcreteNativeResponse implements Response {
   private streamingStarted = false;
+
+  constructor(private readonly abortCleanupFunction: AbortSubscriptionCleanupFunction) {
+    super();
+    this.addListener('readyForJSFinalization', this.finalize);
+  }
 
   get body(): ReadableStream<Uint8Array> | null {
     const response = this;
@@ -20,12 +27,10 @@ export class FetchResponse extends ConcreteNativeResponse implements Response {
         });
 
         response.addListener('didComplete', () => {
-          response.removeAllRegisteredListeners();
           controller.close();
         });
 
         response.addListener('didFailWithError', (error: string) => {
-          response.removeAllRegisteredListeners();
           controller.error(new Error(error));
         });
       },
@@ -36,7 +41,6 @@ export class FetchResponse extends ConcreteNativeResponse implements Response {
         }
       },
       cancel(reason) {
-        response.removeAllRegisteredListeners();
         response.cancelStreaming(String(reason));
       },
     });
@@ -91,9 +95,13 @@ export class FetchResponse extends ConcreteNativeResponse implements Response {
     throw new Error('Not implemented');
   }
 
-  private removeAllRegisteredListeners() {
+  private finalize = (): void => {
+    this.removeListener('readyForJSFinalization', this.finalize);
+
+    this.abortCleanupFunction();
+
     this.removeAllListeners('didReceiveResponseData');
     this.removeAllListeners('didComplete');
     this.removeAllListeners('didFailWithError');
-  }
+  };
 }

packages/expo/src/winter/fetch/NativeRequest.ts
@@ -21,6 +21,7 @@ export type NativeResponseEvents = {
   didReceiveResponseData(data: Uint8Array): void;
   didComplete(): void;
   didFailWithError(error: string): void;
+  readyForJSFinalization(): void;
 };
 
 export declare class NativeResponse extends SharedObject<NativeResponseEvents> {

commit e636448063f2d78792e24757b45f300cb17ee2f8

[expo] fix fetch streaming does not complete (#33756)
# Why

fix streaming request does not complete if the request is complete
before streaming started. this is a regression from my #33577

# How

streaming requests rely on event listeners. if the request is complete,
we unregister all event listeners. the caller will never return then.
this pr tries to return the queuedData from startStreaming. for
completed requests, we don't use event listeners anymore.

packages/expo/android/src/main/java/expo/modules/fetch/ExpoFetchModule.kt
@@ -59,7 +59,7 @@ class ExpoFetchModule : Module() {
       }
 
       AsyncFunction("startStreaming") { response: NativeResponse ->
-        response.startStreaming()
+        return@AsyncFunction response.startStreaming()
       }
 
       AsyncFunction("cancelStreaming") { response: NativeResponse ->

packages/expo/android/src/main/java/expo/modules/fetch/NativeResponse.kt
@@ -49,9 +49,9 @@ internal class NativeResponse(appContext: AppContext, private val coroutineScope
     state = ResponseState.STARTED
   }
 
-  fun startStreaming() {
+  fun startStreaming(): ByteArray? {
     if (isInvalidState(ResponseState.RESPONSE_RECEIVED, ResponseState.BODY_COMPLETED)) {
-      return
+      return null
     }
     if (state == ResponseState.RESPONSE_RECEIVED) {
       state = ResponseState.BODY_STREAMING_STARTED
@@ -59,9 +59,9 @@ internal class NativeResponse(appContext: AppContext, private val coroutineScope
       emit("didReceiveResponseData", queuedData)
     } else if (state == ResponseState.BODY_COMPLETED) {
       val queuedData = this.sink.finalize()
-      emit("didReceiveResponseData", queuedData)
-      emit("didComplete")
+      return queuedData
     }
+    return null
   }
 
   fun cancelStreaming() {

packages/expo/build/winter/fetch/FetchResponse.d.ts
@@ -7,7 +7,8 @@ export type AbortSubscriptionCleanupFunction = () => void;
  */
 export declare class FetchResponse extends ConcreteNativeResponse implements Response {
     private readonly abortCleanupFunction;
-    private streamingStarted;
+    private streamingState;
+    private bodyStream;
     constructor(abortCleanupFunction: AbortSubscriptionCleanupFunction);
     get body(): ReadableStream<Uint8Array> | null;
     get headers(): Headers;

packages/expo/build/winter/fetch/FetchResponse.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"FetchResponse.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/FetchResponse.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,cAAc,EAAE,MAAM,sBAAsB,CAAC;AAGtD,OAAO,KAAK,EAAE,cAAc,EAAE,MAAM,iBAAiB,CAAC;AAEtD,QAAA,MAAM,sBAAsB,uBAA0D,CAAC;AAEvF,MAAM,MAAM,gCAAgC,GAAG,MAAM,IAAI,CAAC;AAE1D;;GAEG;AACH,qBAAa,aAAc,SAAQ,sBAAuB,YAAW,QAAQ;IAG/D,OAAO,CAAC,QAAQ,CAAC,oBAAoB;IAFjD,OAAO,CAAC,gBAAgB,CAAS;gBAEJ,oBAAoB,EAAE,gCAAgC;IAKnF,IAAI,IAAI,IAAI,cAAc,CAAC,UAAU,CAAC,GAAG,IAAI,CA0B5C;IAED,IAAI,OAAO,IAAI,OAAO,CAErB;IAED,IAAI,EAAE,IAAI,OAAO,CAEhB;IAED,SAAgB,IAAI,aAAa;IAE3B,IAAI,IAAI,OAAO,CAAC,IAAI,CAAC;IAKrB,QAAQ,IAAI,OAAO,CAAC,QAAQ,CAAC;IAY7B,IAAI,IAAI,OAAO,CAAC,GAAG,CAAC;IAK1B,QAAQ,IAAI,MAAM;IAIlB,MAAM,IAAI,MAAM;IAShB,KAAK,IAAI,aAAa;IAItB,OAAO,CAAC,QAAQ,CAQd;CACH"}
+{"version":3,"file":"FetchResponse.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/FetchResponse.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,cAAc,EAAE,MAAM,sBAAsB,CAAC;AAGtD,OAAO,KAAK,EAAE,cAAc,EAAE,MAAM,iBAAiB,CAAC;AAEtD,QAAA,MAAM,sBAAsB,uBAA0D,CAAC;AAEvF,MAAM,MAAM,gCAAgC,GAAG,MAAM,IAAI,CAAC;AAE1D;;GAEG;AACH,qBAAa,aAAc,SAAQ,sBAAuB,YAAW,QAAQ;IAI/D,OAAO,CAAC,QAAQ,CAAC,oBAAoB;IAHjD,OAAO,CAAC,cAAc,CAA4C;IAClE,OAAO,CAAC,UAAU,CAA2C;gBAEhC,oBAAoB,EAAE,gCAAgC;IAKnF,IAAI,IAAI,IAAI,cAAc,CAAC,UAAU,CAAC,GAAG,IAAI,CAwC5C;IAED,IAAI,OAAO,IAAI,OAAO,CAErB;IAED,IAAI,EAAE,IAAI,OAAO,CAEhB;IAED,SAAgB,IAAI,aAAa;IAE3B,IAAI,IAAI,OAAO,CAAC,IAAI,CAAC;IAKrB,QAAQ,IAAI,OAAO,CAAC,QAAQ,CAAC;IAY7B,IAAI,IAAI,OAAO,CAAC,GAAG,CAAC;IAK1B,QAAQ,IAAI,MAAM;IAIlB,MAAM,IAAI,MAAM;IAShB,KAAK,IAAI,aAAa;IAItB,OAAO,CAAC,QAAQ,CAQd;CACH"}

packages/expo/build/winter/fetch/NativeRequest.d.ts
@@ -22,7 +22,7 @@ export declare class NativeResponse extends SharedObject<NativeResponseEvents> {
     readonly statusText: string;
     readonly url: string;
     readonly redirected: boolean;
-    startStreaming(): void;
+    startStreaming(): Promise<Uint8Array | null>;
     cancelStreaming(reason: string): void;
     arrayBuffer(): Promise<ArrayBuffer>;
     text(): Promise<string>;

packages/expo/build/winter/fetch/NativeRequest.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"NativeRequest.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/NativeRequest.ts"],"names":[],"mappings":"AAAA,OAAO,KAAK,EAAE,YAAY,EAAE,MAAM,mBAAmB,CAAC;AAEtD,MAAM,MAAM,iBAAiB,GAAG,CAAC,MAAM,EAAE,MAAM,CAAC,EAAE,CAAC;AAEnD,MAAM,CAAC,OAAO,OAAO,aAAc,SAAQ,YAAY;IAC9C,KAAK,CACV,GAAG,EAAE,MAAM,EACX,WAAW,EAAE,iBAAiB,EAC9B,WAAW,EAAE,UAAU,GAAG,IAAI,GAC7B,OAAO,CAAC,cAAc,CAAC;IACnB,MAAM,IAAI,IAAI;CACtB;AAED,MAAM,WAAW,iBAAiB;IAChC,WAAW,CAAC,EAAE,kBAAkB,CAAC;IACjC,OAAO,CAAC,EAAE,iBAAiB,CAAC;IAC5B,MAAM,CAAC,EAAE,MAAM,CAAC;CACjB;AAED,MAAM,MAAM,oBAAoB,GAAG;IACjC,sBAAsB,CAAC,IAAI,EAAE,UAAU,GAAG,IAAI,CAAC;IAC/C,WAAW,IAAI,IAAI,CAAC;IACpB,gBAAgB,CAAC,KAAK,EAAE,MAAM,GAAG,IAAI,CAAC;IACtC,sBAAsB,IAAI,IAAI,CAAC;CAChC,CAAC;AAEF,MAAM,CAAC,OAAO,OAAO,cAAe,SAAQ,YAAY,CAAC,oBAAoB,CAAC;IAC5E,QAAQ,CAAC,QAAQ,EAAE,OAAO,CAAC;IAC3B,QAAQ,CAAC,WAAW,EAAE,iBAAiB,CAAC;IACxC,QAAQ,CAAC,MAAM,EAAE,MAAM,CAAC;IACxB,QAAQ,CAAC,UAAU,EAAE,MAAM,CAAC;IAC5B,QAAQ,CAAC,GAAG,EAAE,MAAM,CAAC;IACrB,QAAQ,CAAC,UAAU,EAAE,OAAO,CAAC;IAC7B,cAAc,IAAI,IAAI;IACtB,eAAe,CAAC,MAAM,EAAE,MAAM,GAAG,IAAI;IACrC,WAAW,IAAI,OAAO,CAAC,WAAW,CAAC;IACnC,IAAI,IAAI,OAAO,CAAC,MAAM,CAAC;CACxB"}
+{"version":3,"file":"NativeRequest.d.ts","sourceRoot":"","sources":["../../../src/winter/fetch/NativeRequest.ts"],"names":[],"mappings":"AAAA,OAAO,KAAK,EAAE,YAAY,EAAE,MAAM,mBAAmB,CAAC;AAEtD,MAAM,MAAM,iBAAiB,GAAG,CAAC,MAAM,EAAE,MAAM,CAAC,EAAE,CAAC;AAEnD,MAAM,CAAC,OAAO,OAAO,aAAc,SAAQ,YAAY;IAC9C,KAAK,CACV,GAAG,EAAE,MAAM,EACX,WAAW,EAAE,iBAAiB,EAC9B,WAAW,EAAE,UAAU,GAAG,IAAI,GAC7B,OAAO,CAAC,cAAc,CAAC;IACnB,MAAM,IAAI,IAAI;CACtB;AAED,MAAM,WAAW,iBAAiB;IAChC,WAAW,CAAC,EAAE,kBAAkB,CAAC;IACjC,OAAO,CAAC,EAAE,iBAAiB,CAAC;IAC5B,MAAM,CAAC,EAAE,MAAM,CAAC;CACjB;AAED,MAAM,MAAM,oBAAoB,GAAG;IACjC,sBAAsB,CAAC,IAAI,EAAE,UAAU,GAAG,IAAI,CAAC;IAC/C,WAAW,IAAI,IAAI,CAAC;IACpB,gBAAgB,CAAC,KAAK,EAAE,MAAM,GAAG,IAAI,CAAC;IACtC,sBAAsB,IAAI,IAAI,CAAC;CAChC,CAAC;AAEF,MAAM,CAAC,OAAO,OAAO,cAAe,SAAQ,YAAY,CAAC,oBAAoB,CAAC;IAC5E,QAAQ,CAAC,QAAQ,EAAE,OAAO,CAAC;IAC3B,QAAQ,CAAC,WAAW,EAAE,iBAAiB,CAAC;IACxC,QAAQ,CAAC,MAAM,EAAE,MAAM,CAAC;IACxB,QAAQ,CAAC,UAAU,EAAE,MAAM,CAAC;IAC5B,QAAQ,CAAC,GAAG,EAAE,MAAM,CAAC;IACrB,QAAQ,CAAC,UAAU,EAAE,OAAO,CAAC;IAC7B,cAAc,IAAI,OAAO,CAAC,UAAU,GAAG,IAAI,CAAC;IAC5C,eAAe,CAAC,MAAM,EAAE,MAAM,GAAG,IAAI;IACrC,WAAW,IAAI,OAAO,CAAC,WAAW,CAAC;IACnC,IAAI,IAAI,OAAO,CAAC,MAAM,CAAC;CACxB"}

packages/expo/ios/Fetch/ExpoFetchModule.swift
@@ -27,8 +27,8 @@ public final class ExpoFetchModule: Module {
         return NativeResponse(dispatchQueue: fetchRequestQueue)
       }
 
-      AsyncFunction("startStreaming") { (response: NativeResponse) in
-        response.startStreaming()
+      AsyncFunction("startStreaming") { (response: NativeResponse) -> Data? in
+        return response.startStreaming()
       }.runOnQueue(fetchRequestQueue)
 
       AsyncFunction("cancelStreaming") { (response: NativeResponse, _ reason: String) in

packages/expo/ios/Fetch/NativeResponse.swift
@@ -36,9 +36,9 @@ internal final class NativeResponse: SharedObject, ExpoURLSessionTaskDelegate {
     self.dispatchQueue = dispatchQueue
   }
 
-  func startStreaming() {
+  func startStreaming() -> Data? {
     if isInvalidState(.responseReceived, .bodyCompleted) {
-      return
+      return nil
     }
     if state == .responseReceived {
       state = .bodyStreamingStarted
@@ -46,9 +46,9 @@ internal final class NativeResponse: SharedObject, ExpoURLSessionTaskDelegate {
       emit(event: "didReceiveResponseData", arguments: queuedData)
     } else if state == .bodyCompleted {
       let queuedData = self.sink.finalize()
-      emit(event: "didReceiveResponseData", arguments: queuedData)
-      emit(event: "didComplete")
+      return queuedData
     }
+    return nil
   }
 
   func cancelStreaming() {

packages/expo/src/winter/fetch/FetchResponse.ts
@@ -11,7 +11,8 @@ export type AbortSubscriptionCleanupFunction = () => void;
  * A response implementation for the `fetch.Response` API.
  */
 export class FetchResponse extends ConcreteNativeResponse implements Response {
-  private streamingStarted = false;
+  private streamingState: 'none' | 'started' | 'completed' = 'none';
+  private bodyStream: ReadableStream<Uint8Array> | null = null;
 
   constructor(private readonly abortCleanupFunction: AbortSubscriptionCleanupFunction) {
     super();
@@ -19,31 +20,45 @@ export class FetchResponse extends ConcreteNativeResponse implements Response {
   }
 
   get body(): ReadableStream<Uint8Array> | null {
-    const response = this;
-    return new ReadableStream({
-      start(controller) {
-        response.addListener('didReceiveResponseData', (data: Uint8Array) => {
-          controller.enqueue(data);
-        });
+    if (this.bodyStream == null) {
+      const response = this;
+      this.bodyStream = new ReadableStream({
+        start(controller) {
+          if (response.streamingState === 'completed') {
+            return;
+          }
+          response.addListener('didReceiveResponseData', (data: Uint8Array) => {
+            controller.enqueue(data);
+          });
 
-        response.addListener('didComplete', () => {
-          controller.close();
-        });
+          response.addListener('didComplete', () => {
+            controller.close();
+          });
 
-        response.addListener('didFailWithError', (error: string) => {
-          controller.error(new Error(error));
-        });
-      },
-      pull() {
-        if (!response.streamingStarted) {
-          response.startStreaming();
-          response.streamingStarted = true;
-        }
-      },
-      cancel(reason) {
-        response.cancelStreaming(String(reason));
-      },
-    });
+          response.addListener('didFailWithError', (error: string) => {
+            controller.error(new Error(error));
+          });
+        },
+        async pull(controller) {
+          if (response.streamingState === 'none') {
+            const completedData = await response.startStreaming();
+            if (completedData != null) {
+              controller.enqueue(completedData);
+              controller.close();
+              response.streamingState = 'completed';
+            } else {
+              response.streamingState = 'started';
+            }
+          } else if (response.streamingState === 'completed') {
+            controller.close();
+          }
+        },
+        cancel(reason) {
+          response.cancelStreaming(String(reason));
+        },
+      });
+    }
+    return this.bodyStream;
   }
 
   get headers(): Headers {

packages/expo/src/winter/fetch/NativeRequest.ts
@@ -31,7 +31,7 @@ export declare class NativeResponse extends SharedObject<NativeResponseEvents> {
   readonly statusText: string;
   readonly url: string;
   readonly redirected: boolean;
-  startStreaming(): void;
+  startStreaming(): Promise<Uint8Array | null>;
   cancelStreaming(reason: string): void;
   arrayBuffer(): Promise<ArrayBuffer>;
   text(): Promise<string>;

apps/test-suite/tests/Fetch.ts
@@ -34,6 +34,36 @@ export function test({ describe, expect, it, ...t }) {
       const buffer = await resp.arrayBuffer();
       expect(buffer.byteLength).toBe(20);
     });
+
+    it('should process response in readablestream from late get reader call', async () => {
+      const resp = await fetch('https://httpbin.test.k6.io/get');
+      expect(resp.ok).toBe(true);
+      expect(resp.body).not.toBeNull();
+
+      // Delay 0.5s to ensure the response is completed before streaming started
+      await delayAsync(500);
+
+      const chunks = [];
+      const reader = resp.body.getReader();
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          break;
+        }
+        chunks.push(value);
+      }
+      const buffer = new Uint8Array(chunks.reduce((acc, chunk) => acc + chunk.length, 0));
+      let offset = 0;
+      for (const chunk of chunks) {
+        buffer.set(chunk, offset);
+        offset += chunk.length;
+      }
+
+      const text = new TextDecoder().decode(buffer);
+      expect(text).not.toBe('');
+      const json = JSON.parse(text);
+      expect(json.url).toMatch(/^https?:\/\/httpbin\.test\.k6\.io\/get$/);
+    });
   });
 
   describe('Request body', () => {
@@ -369,4 +399,8 @@ function setupTestTimeout(t: Record<string, any>, timeout: number = 30000) {
   t.afterAll(() => {
     t.jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
   });
+}
+
+function delayAsync(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }