d58c507bd07951fc1c19dacd3210cd402a453bed
Alan Hughes <alan@expo.io>
2025-01-03 15:34:07 +0000

    rebuild js

--- packages/expo-audio/build/ExpoAudio.js
+++ packages/expo-audio/build/ExpoAudio.js
@@ -7,6 +7,7 @@ import { resolveSource } from './utils/resolveSource';
 export const PLAYBACK_STATUS_UPDATE = 'playbackStatusUpdate';
 export const AUDIO_SAMPLE_UPDATE = 'audioSampleUpdate';
 export const RECORDING_STATUS_UPDATE = 'recordingStatusUpdate';
+// TODO: Temporary solution until we develop a way of overriding prototypes that won't break the lazy loading of the module.
 const replace = AudioModule.AudioPlayer.prototype.replace;
 AudioModule.AudioPlayer.prototype.replace = function (source) {
     return replace.call(this, resolveSource(source));
