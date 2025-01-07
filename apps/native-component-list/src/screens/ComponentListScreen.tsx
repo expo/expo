expo/expo

commit 22c6c5bf2721bdf7b3c37789881e06e8c756e8f7

[expo] Bump `react-native-screens` and `@react-navigation/*` (#31992)

apps/native-component-list/src/screens/ComponentListScreen.tsx
@@ -1,5 +1,11 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Link, useLinkProps } from '@react-navigation/native';
+import {
+  Link,
+  NavigationAction,
+  StackActions,
+  useLinkBuilder,
+  useLinkProps,
+} from '@react-navigation/native';
 import React from 'react';
 import {
   FlatList,
@@ -29,12 +35,23 @@ interface Props {
 }
 
 function LinkButton({
-  to,
-  action,
+  href,
   children,
+  screenName,
   ...rest
-}: React.ComponentProps<typeof Link> & { disabled?: boolean; children?: React.ReactNode }) {
-  const { onPress, ...props } = useLinkProps({ to, action });
+}: Omit<React.ComponentProps<typeof Link>, 'action'> & {
+  href: string;
+  disabled?: boolean;
+  children?: React.ReactNode;
+  screenName?: string;
+}) {
+  const { buildAction } = useLinkBuilder();
+  let action: NavigationAction = buildAction(href);
+  if (screenName) {
+    action = StackActions.push(screenName);
+  }
+
+  const { onPress, ...props } = useLinkProps({ href, action });
 
   const [isPressed, setIsPressed] = React.useState(false);
 
@@ -83,7 +100,11 @@ export default function ComponentListScreen(props: Props) {
   const renderExampleSection: ListRenderItem<ListElement> = ({ item }) => {
     const { route, name: exampleName, isAvailable } = item;
     return (
-      <LinkButton disabled={!isAvailable} to={route ?? exampleName} style={[styles.rowTouchable]}>
+      <LinkButton
+        disabled={!isAvailable}
+        href={route ?? exampleName}
+        screenName={exampleName}
+        style={[styles.rowTouchable]}>
         <View
           pointerEvents="none"
           style={[styles.row, !isAvailable && styles.disabledRow, { paddingRight: 10 + right }]}>

commit b02b0312b3a2d643eefc2b20b5df15aaf5006efd

[bare-expo] fix search navigation (#33858)
# Why

The navigation via the search screen of the `bare-expo` app is not
working. Seems like a regression from #31992 during upgrading
`@react-navigation`.

# How

Was setting up the local development environment to get familiar with
the internals and noticed this when trying to navigate through the
components and APIs.

# Test Plan

<details>
<summary>Recording before the fix</summary>



https://github.com/user-attachments/assets/1665c222-d8fa-402c-ad10-96a90a6b9a27


</details>

<details>
<summary>Recording after the fix</summary>



https://github.com/user-attachments/assets/0374c16a-495f-44c9-a1fe-f4285502c258


</details>

I've also verified the normal navigation still works as expected.

apps/native-component-list/src/screens/ComponentListScreen.tsx
@@ -1,11 +1,5 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import {
-  Link,
-  NavigationAction,
-  StackActions,
-  useLinkBuilder,
-  useLinkProps,
-} from '@react-navigation/native';
+import { Link, NavigationAction, useLinkBuilder, useLinkProps } from '@react-navigation/native';
 import React from 'react';
 import {
   FlatList,
@@ -38,19 +32,14 @@ interface Props {
 function LinkButton({
   href,
   children,
-  screenName,
   ...rest
 }: Omit<React.ComponentProps<typeof Link>, 'action'> & {
   href: string;
   disabled?: boolean;
   children?: React.ReactNode;
-  screenName?: string;
 }) {
   const { buildAction } = useLinkBuilder();
-  let action: NavigationAction = buildAction(href);
-  if (screenName) {
-    action = StackActions.push(screenName);
-  }
+  const action: NavigationAction = buildAction(href);
 
   const { onPress, ...props } = useLinkProps({ href, action });
 
@@ -104,7 +93,6 @@ export default function ComponentListScreen(props: Props) {
       <LinkButton
         disabled={!isAvailable}
         href={route ?? screenName ?? exampleName}
-        screenName={screenName ?? exampleName}
         style={[styles.rowTouchable]}>
         <View
           pointerEvents="none"