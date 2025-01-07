52ab23fbdff35e5a39eb16b2c821c04108f4fa43
Jean-Baptiste LARRIVIERE <76693792+jblarriviere@users.noreply.github.com>
2025-01-07 16:52:38 +0100

    [web-browser][ios] Fix crash when using popover presentation on iPad

--- packages/expo-web-browser/ios/WebBrowserSession.swift
+++ packages/expo-web-browser/ios/WebBrowserSession.swift
@@ -32,6 +32,18 @@ internal class WebBrowserSession: NSObject, SFSafariViewControllerDelegate, UIAd
     while currentViewController?.presentedViewController != nil {
       currentViewController = currentViewController?.presentedViewController
     }
+  
+    if UIDevice.current.userInterfaceIdiom == .pad {
+     let viewFrame = currentViewController!.view.frame
+      viewController.popoverPresentationController?.sourceRect = CGRect(
+        x: viewFrame.midX,
+        y: viewFrame.maxY,
+        width: 0,
+        height: 0
+      )
+      viewController.popoverPresentationController?.sourceView = currentViewController?.view
+    }
+
     currentViewController?.present(viewController, animated: true) {
       self.didPresent()
     }
