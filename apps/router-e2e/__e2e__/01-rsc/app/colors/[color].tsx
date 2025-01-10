2f0cd17abe275e27635daa612a73b7a02566566c
evanbacon <bacon@expo.io>
2025-01-10 11:42:01 -0600

    Create [color].tsx

---
+++ apps/router-e2e/__e2e__/01-rsc/app/colors/[color].tsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native';
+
+export const unstable_settings = {
+  render: 'static',
+};
+
+export async function generateStaticParams() {
+  return [{ color: 'red' }, { color: 'blue' }];
+}
+
+export default function ColorRoute({ color }) {
+  return <Text testID="color">{color}</Text>;
+}
