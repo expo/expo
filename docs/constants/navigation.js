cb3e8057dbf3d23cf0372c6a359267e397e533e3
Kadi Kraman <kadi@expo.io>
2025-01-10 11:13:17 +0000

    Add custom domain

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -385,6 +385,7 @@ const eas = [
     makePage('eas-hosting/get-started.mdx'),
     makePage('eas-hosting/deployments-and-aliases.mdx'),
     makePage('eas-hosting/environment-variables.mdx'),
+    makePage('eas-hosting/custom-domain.mdx'),
   ]),
   makeSection('EAS Workflows', [
     makePage('eas-workflows/get-started.mdx'),
