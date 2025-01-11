2da8c7f226b72505847c3709efceb85724948288
Vojtech Novak <vonovak@gmail.com>
2025-01-11 23:21:02 +0800

    chore: changelogs

--- packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/launcher/DevLauncherReactHost.kt
+++ packages/expo-dev-launcher/android/src/debug/java/expo/modules/devlauncher/launcher/DevLauncherReactHost.kt
@@ -22,7 +22,6 @@ import expo.modules.devlauncher.DevLauncherController
 import expo.modules.devlauncher.DevLauncherPackage
 import expo.modules.devlauncher.helpers.findDevMenuPackage
 import expo.modules.devlauncher.helpers.injectDebugServerHost
-import expo.modules.devlauncher.helpers.injectDevServerHelper
 import expo.modules.devmenu.modules.DevMenuPreferences
 import expo.modules.kotlin.ModulesProvider
 
@@ -39,7 +38,7 @@ object DevLauncherReactHost {
     } else {
       HermesInstance()
     }
-    val jsMainModuleName = "packages/expo-dev-launcher/bundle/index"
+    val jsMainModuleName = "index"
     val defaultReactHostDelegate =
       DefaultReactHostDelegate(
         jsMainModulePath = jsMainModuleName,
@@ -60,11 +59,7 @@ object DevLauncherReactHost {
     )
 
     if (useDeveloperSupport) {
-      injectDevServerHelper(application.applicationContext, reactHost.devSupportManager, DevLauncherController.instance);
-      val success = injectDebugServerHost(application.applicationContext, reactHost, launcherIp!!, jsMainModuleName)
-      if (!success) {
-        throw IllegalStateException("Failed to inject debug server host")
-      }
+      injectDebugServerHost(application.applicationContext, reactHost, launcherIp!!, jsMainModuleName)
     }
     return reactHost
   }
--- packages/expo-dev-menu/CHANGELOG.md
+++ packages/expo-dev-menu/CHANGELOG.md
@@ -6,6 +6,8 @@
 
 ### 🎉 New features
 
+- support RN 0.77 in SDK 52 ([#34088](https://github.com/expo/expo/pull/34088) by [@vonovak](https://github.com/vonovak))
+
 ### 🐛 Bug fixes
 
 ### 💡 Others
