9db4dfc3417e33a73597d25a1910aeb1fffcc536
Kudo Chien <kudo@expo.dev>
2025-01-13 22:56:48 +0800

    add comments

--- apps/native-component-list/ts-declarations/env.d.ts
+++ apps/native-component-list/ts-declarations/env.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      // Paired with `.env` to test custom environment variable types
       EXPO_PUBLIC_TEST: string;
     }
   }
