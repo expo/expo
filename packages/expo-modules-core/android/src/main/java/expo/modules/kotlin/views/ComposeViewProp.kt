71e1b2eb05cb6f3e74b18381fba8e92ec0b52fd9
aleqsio <mikucki@gmail.com>
2025-01-13 13:27:27 +0100

    Add logger

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/ComposeViewProp.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/ComposeViewProp.kt
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.Dynamic
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.PropSetException
 import expo.modules.kotlin.exception.exceptionDecorator
+import expo.modules.kotlin.logger
 import expo.modules.kotlin.types.AnyType
 import kotlin.reflect.KProperty1
 
@@ -24,6 +25,8 @@ class ComposeViewProp(
       val mutableState = property.getter.call(props)
       if (mutableState is MutableState<*>) {
         (mutableState as MutableState<Any?>).value = type.convert(prop, appContext)
+      } else {
+        logger.warn("⚠️ Property $name is not a MutableState in ${onView::class.java}")
       }
     }
   }
