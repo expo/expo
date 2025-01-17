1767358f02456b3bff357814e913e48aa0598f7a
evanbacon <bacon@expo.io>
2025-01-17 13:14:42 -0800

    Remove need for `expo-asset/tools/hashAssetFiles` and add legacy support for projects that still have it.

--- packages/@expo/metro-config/src/transform-worker/getAssets.ts
+++ packages/@expo/metro-config/src/transform-worker/getAssets.ts
@@ -5,14 +5,20 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { AssetData, Module } from 'metro';
+import type { AssetData, Module } from 'metro';
 import { getAssetData } from 'metro/src/Assets';
 import { getJsOutput, isJsModule } from 'metro/src/DeltaBundler/Serializers/helpers/js';
-import assert from 'node:assert';
 import crypto from 'node:crypto';
+import fs from 'node:fs';
 import path from 'node:path';
 
 import { ReadOnlyDependencies } from '../serializer/getCssDeps';
+
+const debug = require('debug')('expo:metro-config:assets') as typeof console.log;
+
+type ExpoAssetData = AssetData & {
+  fileHashes?: string[];
+};
 
 type Options = {
   processModuleFilter: (modules: Module) => boolean;
@@ -22,18 +28,67 @@ type Options = {
   publicPath: string;
 };
 
-function md5Hash(data: string[]) {
+function getMD5ForDataAsync(data: string[]) {
   if (data.length === 1) return data[0];
   const hash = crypto.createHash('md5');
   hash.update(data.join(''));
   return hash.digest('hex');
 }
 
-function assertHashedAssetData(data: AssetData): asserts data is HashedAssetData {
-  assert(
-    'fileHashes' in data,
-    'Assets must have hashed files. Ensure the expo-asset plugin is installed.'
+function getMD5ForFilePathAsync(path: string) {
+  return new Promise<string>((resolve, reject) => {
+    const output = crypto.createHash('md5');
+    const input = fs.createReadStream(path);
+    input.on('error', (err) => reject(err));
+    output.once('readable', () => resolve(output.read().toString('hex')));
+    input.pipe(output);
+  });
+}
+
+function isHashedAssetData(asset: ExpoAssetData): asset is HashedAssetData {
+  if ('fileHashes' in asset && Array.isArray(asset.fileHashes)) {
+    return true;
+  }
+  return false;
+}
+
+async function ensureOtaAssetHashesAsync(asset: ExpoAssetData): Promise<HashedAssetData> {
+  // Legacy cases where people have the `expo-asset/tools/hashAssetFiles` set still.
+  if (isHashedAssetData(asset)) {
+    debug('fileHashes already added, skipping injection for: ' + asset.name);
+    return asset;
+  }
+
+  const hashes = await Promise.all(asset.files.map(getMD5ForFilePathAsync));
+  // New version where we run the asset plugin every time.
+
+  asset.fileHashes = hashes;
+
+  // Convert the `../` segments of the server URL to `_` to support monorepos.
+  // This same transformation takes place in `AssetSourceResolver.web` (expo-assets, expo-image) and `persistMetroAssets` of Expo CLI,
+  // this originally came from the Metro opinion https://github.com/react-native-community/cli/blob/2204d357379e2067cebe2791e90388f7e97fc5f5/packages/cli-plugin-metro/src/commands/bundle/getAssetDestPathIOS.ts#L19C5-L19C10
+  if (asset.httpServerLocation.includes('?export_path=')) {
+    // @ts-expect-error: marked as read-only
+    asset.httpServerLocation = asset.httpServerLocation
+      .match(/\?export_path=(.*)/)[1]
+      .replace(/\.\.\//g, '_');
+  }
+
+  // URL encode asset paths defined as `?export_path` or `?unstable_path` query parameters.
+  // Decoding should be done automatically when parsing the URL through Node or the browser.
+  const assetPathQueryParameter = asset.httpServerLocation.match(
+    /\?(export_path|unstable_path)=(.*)/
   );
+  if (assetPathQueryParameter && assetPathQueryParameter[2]) {
+    const assetPath = assetPathQueryParameter[2];
+    // @ts-expect-error: marked as read-only
+    asset.httpServerLocation = asset.httpServerLocation.replace(
+      assetPath,
+      encodeURIComponent(assetPath)
+    );
+  }
+
+  return asset as HashedAssetData;
 }
 
 export async function getUniversalAssetData(
@@ -43,15 +98,21 @@ export async function getUniversalAssetData(
   platform: string | null | undefined,
   publicPath: string
 ): Promise<HashedAssetData> {
-  const data = await getAssetData(assetPath, localPath, assetDataPlugins, platform, publicPath);
-  assertHashedAssetData(data);
+  const metroAssetData = await getAssetData(
+    assetPath,
+    localPath,
+    assetDataPlugins,
+    platform,
+    publicPath
+  );
+  const data = await ensureOtaAssetHashesAsync(metroAssetData);
 
   // NOTE(EvanBacon): This is where we modify the asset to include a hash in the name for web cache invalidation.
   if (platform === 'web' && publicPath.includes('?export_path=')) {
     // `local-image.[contenthash]`. Using `.` but this won't work if we ever apply to Android because Android res files cannot contain `.`.
     // TODO: Prevent one multi-res image from updating the hash in all images.
     // @ts-expect-error: name is typed as readonly.
-    data.name = `${data.name}.${md5Hash(data.fileHashes)}`;
+    data.name = `${data.name}.${getMD5ForDataAsync(data.fileHashes)}`;
   }
 
   return data;
