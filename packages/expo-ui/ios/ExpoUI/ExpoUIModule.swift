0ab56486b6a4b25cc36f583a14f6fe86b2d2e46b
aleqsio <mikucki@gmail.com>
2025-01-17 12:34:13 +0100

    Add picker on iOS to ExpoUI

---
+++ packages/expo-ui/ios/ExpoUI/ExpoUIModule.swift
@@ -0,0 +1,12 @@
+// Copyright 2025-present 650 Industries. All rights reserved.
+
+
+import ExpoModulesCore
+
+public class ExpoUIModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("ExpoUI")
+
+    View(SingleChoiceSegmentedControlView.self)
+  }
+}
