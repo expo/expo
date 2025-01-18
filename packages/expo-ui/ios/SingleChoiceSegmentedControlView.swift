5a9cfe81988712c684345a1cf564fa94b5523455
aleqsio <mikucki@gmail.com>
2025-01-18 10:17:12 +0100

    Fix indents

--- packages/expo-ui/ios/SingleChoiceSegmentedControlView.swift
+++ packages/expo-ui/ios/SingleChoiceSegmentedControlView.swift
@@ -11,19 +11,19 @@ struct SingleChoiceSegmentedControlView: ExpoSwiftUI.View {
       ForEach(Array(props.options.enumerated()), id: \.element) { index, option in
         Text(option).tag(index)
       }
-    }.pickerStyle(SegmentedPickerStyle())
-      .onChange(of: selection, perform: {
-      newValue in
-        if props.selectedIndex == newValue {
-          return
-        }
-        props.onOptionSelected([
-          "index": newValue ?? 0,
-          "label": props.options[newValue ?? 0]
-        ])
-      })
-    .onReceive(props.selectedIndex.publisher, perform: {
-      newVal in selection = newVal
+    }
+    .pickerStyle(SegmentedPickerStyle())
+    .onChange(of: selection, perform: { newValue in
+      if props.selectedIndex == newValue {
+        return
+      }
+      props.onOptionSelected([
+        "index": newValue ?? 0,
+        "label": props.options[newValue ?? 0]
+      ])
+    })
+    .onReceive(props.selectedIndex.publisher, perform: { newValue in
+      selection = newValue
     })
   }
 }
