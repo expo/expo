6db9fb687228e8d2c231e01d9f8464bd782ed351
Phil Pluckthun <phil@kitten.sh>
2025-01-19 12:44:59 +0000

    Add comments to `createExpoFallbackResolver`

--- packages/@expo/cli/src/start/server/metro/createExpoFallbackResolver.ts
+++ packages/@expo/cli/src/start/server/metro/createExpoFallbackResolver.ts
@@ -4,11 +4,12 @@ import path from 'path';
 import type { StrictResolverFactory } from './withMetroMultiPlatform';
 import type { ExpoCustomMetroResolver } from './withMetroResolvers';
 
+/** A record of dependencies that we know are only used for scripts and config-plugins */
 const EXCLUDE_ORIGIN_MODULES: Record<string, true | undefined> = {
   '@expo/config': true,
   '@expo/config-plugins': true,
-  'schema-utils': true,
-  semver: true,
+  'schema-utils': true, // Used by `expo-router/plugin`
+  semver: true, // Used by `expo-router/doctor`
 };
 
 interface PackageMetaPeerDependenciesMetaEntry {
@@ -31,14 +32,18 @@ interface ModuleDescription {
 
 const debug = require('debug')('expo:start:server:metro:fallback-resolver') as typeof console.log;
 
+/** Converts a list of module names to a regex that may either match bare module names or sub-modules of modules */
 const dependenciesToRegex = (dependencies: string[]) =>
   new RegExp(`^(?:${dependencies.join('|')})(?:$|/)`);
 
+/** Resolves an origin module and outputs a filter regex and target path for it */
 const getModuleDescription = (originModuleName: string): ModuleDescription | null => {
   const metaPath = path.join(originModuleName, 'package.json');
   let originModulePath: string;
   let packageMeta: PackageMeta;
   try {
+    // We attempt to resolve all origin modules via `@expo/cli`. Due to normal Node resolution
+    // this may include dependencies of `@expo/cli` or of the project.
     originModulePath = path.dirname(require.resolve(metaPath));
     packageMeta = require(metaPath);
   } catch (error: any) {
@@ -50,6 +55,13 @@ const getModuleDescription = (originModuleName: string): ModuleDescription | nul
   if (packageMeta.peerDependencies) {
     const peerDependenciesMeta = packageMeta.peerDependenciesMeta;
     let peerDependencies = Object.keys(packageMeta.peerDependencies);
+    // We explicitly include non-optional peer dependencies. Non-optional peer dependencies of
+    // `expo` and `expo-router` are either expected to be accessible on a project-level, since
+    // both are meant to be installed is direct dependencies, or shouldn't be accessible when
+    // they're fulfilled as isolated dependencies.
+    // The exception are only *optional* peer dependencies, since when they're installed
+    // automatically by newer package manager behaviour, they may become isolated dependencies
+    // that we still wish to access.
     if (peerDependenciesMeta) {
       peerDependencies = peerDependencies.filter((dependency) => {
         const peerMeta = peerDependenciesMeta[dependency];
@@ -58,6 +70,7 @@ const getModuleDescription = (originModuleName: string): ModuleDescription | nul
     }
     dependencies.push(...peerDependencies);
   }
+  // We deduplicate the dependencies and exclude modules that we know are only used for scripts or config-plugins
   dependencies = dependencies.filter((moduleName, index, dependenciesArr) => {
     if (EXCLUDE_ORIGIN_MODULES[moduleName]) return false;
     return dependenciesArr.indexOf(moduleName) === index;
@@ -67,6 +80,21 @@ const getModuleDescription = (originModuleName: string): ModuleDescription | nul
     : null;
 };
 
+/** Creates a fallback module resolver that resolves dependencis of modules named in `originModuleNames` via their path.
+ * @remarks
+ * The fallback resolver targets modules dependended on by modules named in `originModuleNames` and resolves
+ * them from the module root of these origin modules instead.
+ * It should only be used as a fallback after normal Node resolution (and other resolvers) have failed for:
+ * - the `expo` package
+ * - the `expo-router` package
+ * Dependencies mentioned as either optional peer dependencies or direct dependencies by these modules may be isolated
+ * and inaccessible via standard Node module resolution. This may happen when either transpilation adds these
+ * dependencies to other parts of the tree (e.g. `@babel/runtime`) or when a dependency fails to hoist due to either
+ * a corrupted dependency tree or when a peer dependency is fulfilled incorrectly (e.g. `expo-asset`)
+ * @privateRemarks
+ * This does NOT follow Node resolution and is *only* intended to provide a fallback for modules that we depend on
+ * ourselves and know we can resolve (via expo or expo-router)!
+ */
 export function createFallbackModuleResolver({
   originModuleNames,
   getStrictResolver,
@@ -81,12 +109,14 @@ export function createFallbackModuleResolver({
     );
 
   return function requestFallbackModule(immutableContext, moduleName, platform) {
+    // Early return if `moduleName` cannot be a module specifier
     if (moduleName[0] === '.' || moduleName[0] === '/') {
       return null;
     }
 
     for (const { originModuleName, originModulePath, moduleTestRe } of moduleDescriptions) {
       if (moduleTestRe.test(moduleName)) {
+        // We instead resolve as if it was depended on by the `originModulePath` (the module named in `originModuleNames`)
         const context: ResolutionContext = {
           ...immutableContext,
           nodeModulesPaths: [originModulePath],
