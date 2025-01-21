972af1d217c3f2f9e32b0400bfa134456d4e0873
Keith Kurak <keith.kurak@gmail.com>
2025-01-21 17:32:03 -0500

    [document-picker] allow override of kv-store-identifier entitlement

--- packages/expo-document-picker/plugin/src/__tests__/withDocumentPickerIOS-test.ts
+++ packages/expo-document-picker/plugin/src/__tests__/withDocumentPickerIOS-test.ts
@@ -21,4 +21,20 @@ describe(setICloudEntitlements, () => {
       'com.apple.developer.ubiquity-kvstore-identifier': '$(TeamIdentifierPrefix)com.bacon.foobar',
     });
   });
+
+  it(`Overrides com.apple.developer.ubiquity-kvstore-identifier when kvStoreIdentifier is set`, () => {
+    expect(
+      setICloudEntitlements(
+        { ios: { usesIcloudStorage: true, bundleIdentifier: 'com.bacon.foobar' } },
+        { iCloudContainerEnvironment: 'Production', kvstoreIdentifier: 'ABC123.com.bacon.foobar' },
+        {}
+      )
+    ).toStrictEqual({
+      'com.apple.developer.icloud-container-environment': 'Production',
+      'com.apple.developer.icloud-container-identifiers': ['iCloud.com.bacon.foobar'],
+      'com.apple.developer.icloud-services': ['CloudDocuments'],
+      'com.apple.developer.ubiquity-container-identifiers': ['iCloud.com.bacon.foobar'],
+      'com.apple.developer.ubiquity-kvstore-identifier': 'ABC123.com.bacon.foobar',
+    });
+  });
 });
