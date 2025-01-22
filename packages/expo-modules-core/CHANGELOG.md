8e51bc13b10bd77eb2ef2df06e7ff8f56abf07a9
lukmccall <kosmatylukasz@gmail.com>
2025-01-22 14:13:02 -0800

    [core][Android] Fix `AppContext` is lost in nested converters

--- packages/expo-modules-core/CHANGELOG.md
+++ packages/expo-modules-core/CHANGELOG.md
@@ -13,6 +13,7 @@
 ### 🐛 Bug fixes
 
 - [iOS] Fixes view managers not deallocating when reloading. ([#33760](https://github.com/expo/expo/pull/33760) by [@alanjhughes](https://github.com/alanjhughes))
+- [Android] Fixes `AppContext` is lost in nested converters.
 
 ### 💡 Others
 
--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/types/DurationTypeConverter.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/types/DurationTypeConverter.kt
@@ -2,6 +2,7 @@ package expo.modules.kotlin.types
 
 import com.facebook.react.bridge.Dynamic
 import com.facebook.react.bridge.ReadableType
+import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.jni.CppType
 import expo.modules.kotlin.jni.ExpectedType
 import kotlin.time.Duration
@@ -9,14 +10,14 @@ import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
 class DurationTypeConverter(isOptional: Boolean) : DynamicAwareTypeConverters<Duration>(isOptional) {
-  override fun convertFromDynamic(value: Dynamic): Duration {
+  override fun convertFromDynamic(value: Dynamic, context: AppContext?): Duration {
     if (value.type != ReadableType.Number) {
       throw IllegalArgumentException("Expected a number, but received ${value.type}")
     }
     return value.asDouble().toDuration(DurationUnit.SECONDS)
   }
 
-  override fun convertFromAny(value: Any): Duration {
+  override fun convertFromAny(value: Any, context: AppContext?): Duration {
     return (value as Double).toDuration(DurationUnit.SECONDS)
   }
 
