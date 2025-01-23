110086e9f566687b3316db0878919a6b0e1984ec
Christian Falch <christian.falch@gmail.com>
2025-01-23 19:28:42 +0100

    [android][ci][e2e] fix missing interface declaration for 0.74-0.77
    In the PR for fixing the e2e tests breaking now. This was a declaration I forgot since I was running under 0.77 when testing this.
    
    The PR that introduced this was: #34363

--- packages/expo-modules-core/android/src/rn74/expo/modules/rncompatibility/ReactNativeFeatureFlags.kt
+++ packages/expo-modules-core/android/src/rn74/expo/modules/rncompatibility/ReactNativeFeatureFlags.kt
@@ -4,11 +4,15 @@ package expo.modules.rncompatibility
 
 import com.facebook.react.config.ReactFeatureFlags
 
+interface IReactNativeFeatureFlagsProvider {
+  val enableBridgelessArchitecture: Boolean
+}
+
 /**
  * A compatibility helper of
  * `com.facebook.react.config.ReactFeatureFlags` and
  * `com.facebook.react.internal.featureflags.ReactNativeFeatureFlags`
  */
-object ReactNativeFeatureFlags {
+object ReactNativeFeatureFlags : IReactNativeFeatureFlagsProvider {
   val enableBridgelessArchitecture = ReactFeatureFlags.enableBridgelessArchitecture
 }
