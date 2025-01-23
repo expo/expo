f4b36677c0454c953d36ccdf6c48f61119df8d25
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2025-01-23 01:16:44 +0100

    [core][Android] Fix `AppContext` is lost in nested converters (#34373)

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/types/net/UriTypeConverter.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/types/net/UriTypeConverter.kt
@@ -2,18 +2,19 @@ package expo.modules.kotlin.types.net
 
 import android.net.Uri
 import com.facebook.react.bridge.Dynamic
+import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.jni.CppType
 import expo.modules.kotlin.jni.ExpectedType
 import expo.modules.kotlin.types.DynamicAwareTypeConverters
 import java.net.URI
 
 class UriTypeConverter(isOptional: Boolean) : DynamicAwareTypeConverters<Uri>(isOptional) {
-  override fun convertFromDynamic(value: Dynamic): Uri {
+  override fun convertFromDynamic(value: Dynamic, context: AppContext?): Uri {
     val stringUri = value.asString()
     return Uri.parse(stringUri)
   }
 
-  override fun convertFromAny(value: Any): Uri {
+  override fun convertFromAny(value: Any, context: AppContext?): Uri {
     val stringUri = value as String
     return Uri.parse(stringUri)
   }
@@ -24,12 +25,12 @@ class UriTypeConverter(isOptional: Boolean) : DynamicAwareTypeConverters<Uri>(is
 }
 
 class JavaURITypeConverter(isOptional: Boolean) : DynamicAwareTypeConverters<URI>(isOptional) {
-  override fun convertFromDynamic(value: Dynamic): URI {
+  override fun convertFromDynamic(value: Dynamic, context: AppContext?): URI {
     val stringUri = value.asString()
     return URI.create(stringUri)
   }
 
-  override fun convertFromAny(value: Any): URI {
+  override fun convertFromAny(value: Any, context: AppContext?): URI {
     val stringUri = value as String
     return URI.create(stringUri)
   }
