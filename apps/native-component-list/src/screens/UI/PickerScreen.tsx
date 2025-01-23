775164ec7b42eedaa228b8c1c580a013f37a5818
aleqsio <mikucki@gmail.com>
2025-01-23 02:54:56 +0100

    Add expo ui switch and cleanup package (#34374)
    Co-authored-by: Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>

---
+++ apps/native-component-list/src/screens/UI/PickerScreen.tsx
@@ -0,0 +1,65 @@
+import { Picker } from '@expo/ui/components/Picker';
+import * as React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+import { Page, Section } from '../../components/Page';
+
+export default function PickerScreen() {
+  const [selectedIndex, setSelectedIndex] = React.useState<number | null>(null);
+  const options = ['$', '$$', '$$$', '$$$$'];
+  return (
+    <ScrollView>
+      <Page>
+        <Section title="Selected value">
+          <Text>{[...options, 'unset'][selectedIndex ?? options.length]}</Text>
+        </Section>
+        <Section title="Segmented picker">
+          <Picker
+            options={options}
+            selectedIndex={selectedIndex}
+            onOptionSelected={({ nativeEvent: { index } }) => {
+              setSelectedIndex(index);
+            }}
+            variant="segmented"
+            style={{
+              width: 300,
+              height: 100,
+            }}
+          />
+        </Section>
+        <Section title="Menu picker">
+          <Picker
+            options={options}
+            selectedIndex={selectedIndex}
+            onOptionSelected={({ nativeEvent: { index } }) => {
+              setSelectedIndex(index);
+            }}
+            variant="menu"
+            style={{
+              width: 300,
+              height: 100,
+            }}
+          />
+        </Section>
+        <Section title="Wheel picker">
+          <Picker
+            options={options}
+            selectedIndex={selectedIndex}
+            onOptionSelected={({ nativeEvent: { index } }) => {
+              setSelectedIndex(index);
+            }}
+            variant="wheel"
+            style={{
+              width: 300,
+              height: 200,
+            }}
+          />
+        </Section>
+      </Page>
+    </ScrollView>
+  );
+}
+
+PickerScreen.navigationOptions = {
+  title: 'Picker',
+};
---
+++ apps/native-component-list/src/screens/UI/SwitchScreen.tsx
@@ -0,0 +1,49 @@
+import { Switch } from '@expo/ui/components/Switch';
+import * as React from 'react';
+
+import { Page, Section } from '../../components/Page';
+
+export default function SwitchScreen() {
+  const [checked, setChecked] = React.useState<boolean>(false);
+  return (
+    <Page>
+      <Section title="Switch">
+        <Switch
+          checked={checked}
+          style={{ width: 300, height: 50 }}
+          onCheckedChanged={({ nativeEvent: { checked } }) => {
+            setChecked(checked);
+          }}
+          label="Never gonna give you up"
+          variant="switch"
+        />
+      </Section>
+      <Section title="Checkbox Switch">
+        <Switch
+          checked={checked}
+          style={{ width: 300, height: 50 }}
+          onCheckedChanged={({ nativeEvent: { checked } }) => {
+            setChecked(checked);
+          }}
+          label="Never gonna let you down"
+          variant="checkbox"
+        />
+      </Section>
+      <Section title="Button Switch">
+        <Switch
+          checked={checked}
+          style={{ width: 300, height: 50 }}
+          onCheckedChanged={({ nativeEvent: { checked } }) => {
+            setChecked(checked);
+          }}
+          label="Never gonna run around and desert you"
+          variant="button"
+        />
+      </Section>
+    </Page>
+  );
+}
+
+SwitchScreen.navigationOptions = {
+  title: 'Switch',
+};
