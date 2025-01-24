3a1719531eb1c4188287f8477329b7b812d5002e
Kudo Chien <kudo@expo.dev>
2025-01-23 18:07:18 -0800

    Apply suggestions from code review

--- packages/expo/build/dom/webview/useDebugZeroHeight.d.ts
+++ packages/expo/build/dom/webview/useDebugZeroHeight.d.ts
@@ -1,9 +1,12 @@
-import { type DOMProps } from '../dom.types';
+import { type ViewProps } from 'react-native';
+import { type DOMProps, type WebViewProps } from '../dom.types';
+type UseDebugZeroHeightType = (dom: DOMProps) => {
+    debugZeroHeightStyle: WebViewProps['containerStyle'] | undefined;
+    debugOnLayout: ViewProps['onLayout'];
+};
 /**
  * Debug only hook to help identify zero height issues in the DOM component.
  */
-export declare function useDebugZeroHeight(dom: DOMProps): {
-    debugZeroHeightStyle: import("react-native").StyleProp<import("react-native").ViewStyle>;
-    debugOnLayout: (event: import("react-native").LayoutChangeEvent) => void;
-};
+export declare const useDebugZeroHeight: UseDebugZeroHeightType;
+export {};
 //# sourceMappingURL=useDebugZeroHeight.d.ts.map
