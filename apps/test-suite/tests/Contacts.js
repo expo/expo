52536fcf5daec1c9f5c28a5be6f5d6fc65c776cc
Christian Falch <christian.falch@gmail.com>
2025-01-24 15:21:54 +0100

    added unit test
    Added test from #32097

--- apps/test-suite/tests/Contacts.js
+++ apps/test-suite/tests/Contacts.js
@@ -648,5 +648,16 @@ export async function test({ describe, it, xdescribe, jasmine, expect, afterAll 
         }
       }
     });
+
+    it('Contacts.updateContactAsync() with image', async () => {
+      const contactId = await createContactWithImage({
+        [Contacts.Fields.FirstName]: 'Kenny',
+        [Contacts.Fields.LastName]: 'Bday guy',
+      });
+      expect(typeof contactId).toBe('string');
+      const contact = await Contacts.getContactByIdAsync(contactId);
+      const modifiedId = await Contacts.updateContactAsync(contact);
+      expect(typeof modifiedId).toBe('string');
+    });
   });
 }
