f330e47a41e5489a456c959b86c0a5cf73f3540b
aleqsio <mikucki@gmail.com>
2025-01-24 20:09:15 +0100

    Add section and slider components (#34381)

--- packages/expo-ui/build/src/index.d.ts
+++ packages/expo-ui/build/src/index.d.ts
@@ -1,4 +1,6 @@
 export type { Switch, SwitchProps } from '../components/Switch';
 export type { Picker, PickerProps } from '../components/Picker';
 export type { Button, ButtonProps } from '../components/Button';
+export type { Section, SectionProps } from '../components/Section';
+export type { Slider, SliderProps } from '../components/Slider';
 //# sourceMappingURL=index.d.ts.map
