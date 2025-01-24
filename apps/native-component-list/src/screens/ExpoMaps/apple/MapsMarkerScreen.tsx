60aa34b3d63999d4bff94878fc15fdeb087da762
lukmccall <kosmatylukasz@gmail.com>
2025-01-24 10:21:25 -0800

    [maps] Replace old version

---
+++ apps/native-component-list/src/screens/ExpoMaps/apple/MapsMarkerScreen.tsx
@@ -0,0 +1,41 @@
+import { AppleMaps } from 'expo-maps';
+import { View } from 'react-native';
+
+export default function MapsCameraControlsScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <AppleMaps.View
+        style={{ width: 'auto', height: '100%' }}
+        cameraPosition={{
+          coordinates: {
+            latitude: 50,
+            longitude: 10,
+          },
+          zoom: 7,
+        }}
+        markers={[
+          {
+            coordinates: { latitude: 50, longitude: 10 },
+            title: 'Marker #1',
+          },
+          {
+            coordinates: { latitude: 51, longitude: 9 },
+            title: 'Marker #2',
+          },
+          {
+            coordinates: { latitude: 49, longitude: 11 },
+            title: 'Marker #3',
+          },
+          {
+            coordinates: { latitude: 51, longitude: 10 },
+            title: 'Marker #4',
+          },
+          {
+            coordinates: { latitude: 49, longitude: 9 },
+            title: 'Marker #4',
+          },
+        ]}
+      />
+    </View>
+  );
+}
---
+++ apps/native-component-list/src/screens/ExpoMaps/apple/MapsPropertiesScreen.tsx
@@ -0,0 +1,70 @@
+import { AppleMaps } from 'expo-maps';
+import { View, StyleSheet } from 'react-native';
+
+import { FunctionParameter, useArguments } from '../../../components/FunctionDemo';
+import Configurator from '../../../components/FunctionDemo/Configurator';
+
+const parameters: FunctionParameter[] = [
+  {
+    name: 'mapType',
+    type: 'enum',
+    values: [
+      { name: 'STANDARD', value: AppleMaps.MapType.STANDARD },
+      { name: 'HYBRID', value: AppleMaps.MapType.HYBRID },
+      { name: 'IMAGERY', value: AppleMaps.MapType.IMAGERY },
+    ],
+  },
+  {
+    name: 'selectionEnabled',
+    type: 'boolean',
+    initial: true,
+  },
+  {
+    name: 'isTrafficEnabled',
+    type: 'boolean',
+    initial: false,
+  },
+];
+
+export default function MapsCameraControlsScreen() {
+  const [args, updateArgument] = useArguments(parameters);
+  const [mapType, selectionEnabled, isTrafficEnabled] = args as [
+    AppleMaps.MapType,
+    boolean,
+    boolean,
+  ];
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <AppleMaps.View
+          style={{ width: 'auto', height: '100%' }}
+          cameraPosition={{
+            coordinates: {
+              latitude: 37.78825,
+              longitude: -122.4324,
+            },
+            zoom: 8,
+          }}
+          properties={{
+            mapType,
+            selectionEnabled,
+            isTrafficEnabled,
+          }}
+        />
+      </View>
+
+      <View style={styles.configurator}>
+        <Configurator parameters={parameters} onChange={updateArgument} value={args} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  configurator: {
+    paddingHorizontal: 15,
+  },
+});
