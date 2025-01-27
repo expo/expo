971657ff07da245221275dbdf29b67336a2854d7
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2025-01-27 22:25:36 +0900

    [templates] Replace react-test-renderer with @testing-library/react-native

--- templates/expo-template-default/components/__tests__/ThemedText-test.tsx
+++ templates/expo-template-default/components/__tests__/ThemedText-test.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 
 import { ThemedText } from '../ThemedText';
 
 it(`renders correctly`, () => {
-  const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
+  const { getByText } = render(<ThemedText>Themed Text</ThemedText>);
+  expect(getByText('Themed Text')).toBeTruthy();
 });
