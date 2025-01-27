expo/expo

commit fdfc47a61a6461c81cb7acf4c7e185238393ec9c

[iOS][core] Retire EXAppDelegateWrapper in favor of ExpoAppDelegate (#33348)

packages/expo-modules-core/ios/AppDelegates/EXAppDelegateWrapper.mm
@@ -9,20 +9,7 @@
 #import <React/RCTComponentViewFactory.h> // Allows non-umbrella since it's coming from React-RCTFabric
 #import <ReactCommon/RCTHost.h> // Allows non-umbrella because the header is not inside a clang module
 
-
 @interface RCTAppDelegate () <RCTComponentViewFactoryComponentProvider, RCTHostDelegate>
-@end
-
-@interface RCTRootViewFactoryConfiguration ()
-
-- (void)setCustomizeRootView:(void (^)(UIView *rootView))customizeRootView;
-
-@end
-
-@interface EXAppDelegateWrapper()
-
-@property (nonatomic, strong) EXReactDelegateWrapper *reactDelegate;
-
 @end
 
 @implementation EXAppDelegateWrapper {
@@ -33,7 +20,6 @@
 {
   if (self = [super init]) {
     _expoAppDelegate = [[EXExpoAppDelegate alloc] init];
-    _reactDelegate = [[EXReactDelegateWrapper alloc] initWithExpoReactDelegate:_expoAppDelegate.reactDelegate];
   }
   return self;
 }
@@ -43,8 +29,7 @@
 // which `UIApplicationDelegate` selectors are implemented.
 - (BOOL)respondsToSelector:(SEL)selector
 {
-  return [super respondsToSelector:selector]
-    || [_expoAppDelegate respondsToSelector:selector];
+  return [super respondsToSelector:selector] || [_expoAppDelegate respondsToSelector:selector];
 }
 
 // Forwards all invocations to `ExpoAppDelegate` object.
@@ -53,83 +38,36 @@
   return _expoAppDelegate;
 }
 
-#if !TARGET_OS_OSX
+#pragma mark - RCTAppDelegate
+
+// Make sure to override all necessary methods from `RCTAppDelegate` here, explicitly forwarding everything to `_expoAppDelegate`.
+// `forwardingTargetForSelector` works only for methods that are not specified in this and `RCTAppDelegate` classes.
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [super application:application didFinishLaunchingWithOptions:launchOptions];
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-result"
-  [_expoAppDelegate application:application didFinishLaunchingWithOptions:launchOptions];
-#pragma clang diagnostic pop
-  return YES;
+  return [_expoAppDelegate application:application didFinishLaunchingWithOptions:launchOptions];
 }
-#endif // !TARGET_OS_OSX
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+  return [_expoAppDelegate applicationDidBecomeActive:application];
+}
 
 - (UIViewController *)createRootViewController
 {
-  return [self.reactDelegate createRootViewController];
+  return [_expoAppDelegate createRootViewController];
 }
 
 - (RCTRootViewFactory *)createRCTRootViewFactory
 {
-  __weak __typeof(self) weakSelf = self;
-  RCTBundleURLBlock bundleUrlBlock = ^{
-    RCTAppDelegate *strongSelf = weakSelf;
-    return strongSelf.bundleURL;
-  };
-
-  RCTRootViewFactoryConfiguration *configuration =
-      [[RCTRootViewFactoryConfiguration alloc] initWithBundleURLBlock:bundleUrlBlock
-                                                       newArchEnabled:self.newArchEnabled
-                                                   turboModuleEnabled:self.newArchEnabled
-                                                    bridgelessEnabled:self.newArchEnabled];
-
-  configuration.createRootViewWithBridge = ^UIView *(RCTBridge *bridge, NSString *moduleName, NSDictionary *initProps)
-  {
-    return [weakSelf createRootViewWithBridge:bridge moduleName:moduleName initProps:initProps];
-  };
-
-  configuration.createBridgeWithDelegate = ^RCTBridge *(id<RCTBridgeDelegate> delegate, NSDictionary *launchOptions)
-  {
-    return [weakSelf createBridgeWithDelegate:delegate launchOptions:launchOptions];
-  };
-
-  configuration.customizeRootView = ^(UIView *_Nonnull rootView) {
-    [weakSelf customizeRootView:(RCTRootView *)rootView];
-  };
-
-  // NOTE(kudo): `sourceURLForBridge` is not referenced intentionally because it does not support New Architecture.
-  configuration.sourceURLForBridge = nil;
-
-  if ([self respondsToSelector:@selector(extraModulesForBridge:)]) {
-    configuration.extraModulesForBridge = ^NSArray<id<RCTBridgeModule>> *_Nonnull(RCTBridge *_Nonnull bridge)
-    {
-      return [weakSelf extraModulesForBridge:bridge];
-    };
-  }
-
-  if ([self respondsToSelector:@selector(extraLazyModuleClassesForBridge:)]) {
-    configuration.extraLazyModuleClassesForBridge =
-        ^NSDictionary<NSString *, Class> *_Nonnull(RCTBridge *_Nonnull bridge)
-    {
-      return [weakSelf extraLazyModuleClassesForBridge:bridge];
-    };
-  }
-
-  if ([self respondsToSelector:@selector(bridge:didNotFindModule:)]) {
-    configuration.bridgeDidNotFindModule = ^BOOL(RCTBridge *_Nonnull bridge, NSString *_Nonnull moduleName) {
-      return [weakSelf bridge:bridge didNotFindModule:moduleName];
-    };
-  }
-
-  return [[EXReactRootViewFactory alloc] initWithReactDelegate:self.reactDelegate configuration:configuration turboModuleManagerDelegate:self];
+  return [_expoAppDelegate createRCTRootViewFactory];
 }
 
-#if !TARGET_OS_OSX
-- (void)customizeRootView:(UIView *)rootView {
+- (void)customizeRootView:(UIView *)rootView
+{
   [_expoAppDelegate customizeRootView:rootView];
 }
-#endif // !TARGET_OS_OSX
 
 #pragma mark - RCTComponentViewFactoryComponentProvider
 
@@ -150,6 +88,13 @@
                exceptionId:(NSUInteger)exceptionId
                    isFatal:(BOOL)isFatal
 {
+}
+
+#pragma mark - Helpers
+
++ (void)customizeRootView:(nonnull UIView *)rootView byAppDelegate:(nonnull RCTAppDelegate *)appDelegate
+{
+  [appDelegate customizeRootView:(RCTRootView *)rootView];
 }
 
 @end

packages/expo-modules-core/ios/AppDelegates/ExpoAppInstance.swift
@@ -1,0 +1,86 @@
+import React_RCTAppDelegate
+
+private var reactDelegateHandlers = [ExpoReactDelegateHandler]()
+
+@objc(EXAppInstance)
+open class ExpoAppInstance: RCTAppDelegate {
+  @objc
+  public let reactDelegate = ExpoReactDelegate(handlers: reactDelegateHandlers)
+
+  @objc
+  open override func createRootViewController() -> UIViewController {
+    return reactDelegate.createRootViewController()
+  }
+
+  @objc
+  open override func createRCTRootViewFactory() -> RCTRootViewFactory {
+    let bundleUrlBlock: RCTBundleURLBlock = { [weak self] in
+      return self?.bundleURL()
+    }
+
+    let configuration = RCTRootViewFactoryConfiguration(
+      bundleURLBlock: bundleUrlBlock,
+      newArchEnabled: newArchEnabled(),
+      turboModuleEnabled: turboModuleEnabled(),
+      bridgelessEnabled: bridgelessEnabled()
+    )
+
+    configuration.createRootViewWithBridge = { bridge, moduleName, initProps in
+      return self.createRootView(with: bridge, moduleName: moduleName, initProps: initProps)
+    }
+
+    configuration.createBridgeWithDelegate = { delegate, launchOptions in
+      return self.createBridge(with: delegate, launchOptions: launchOptions)
+    }
+
+    configuration.customizeRootView = { rootView in
+      // @tsapeta: We cannot just call `self.customize(rootView)` – see the comment of the `customizeRootView:byAppDelegate:` function in EXAppDelegateWrapper.h
+      return EXAppDelegateWrapper.customizeRootView(rootView, by: self)
+    }
+
+    // NOTE(kudo): `sourceURLForBridge` is not referenced intentionally because it does not support New Architecture.
+    configuration.sourceURLForBridge = nil
+
+    if responds(to: #selector(extraModules(for:))) {
+      configuration.extraModulesForBridge = { bridge in
+        return self.extraModules(for: bridge)
+      }
+    }
+
+    if responds(to: #selector(extraLazyModuleClasses(for:))) {
+      configuration.extraLazyModuleClassesForBridge = { bridge in
+        return self.extraLazyModuleClasses(for: bridge)
+      }
+    }
+
+    if responds(to: #selector(bridge(_:didNotFindModule:))) {
+      configuration.bridgeDidNotFindModule = { bridge, moduleName in
+        return self.bridge(bridge, didNotFindModule: moduleName)
+      }
+    }
+
+    return ExpoReactRootViewFactory(
+      reactDelegate: reactDelegate,
+      configuration: configuration,
+      turboModuleManagerDelegate: self
+    )
+  }
+
+  open override func sourceURL(for bridge: RCTBridge) -> URL? {
+    // This method is called only in the old architecture. For compatibility just use the result of a new `bundleURL` method.
+    return bundleURL()
+  }
+
+  // MARK: - Statics
+
+  @objc
+  public static func registerReactDelegateHandlersFrom(modulesProvider: ModulesProvider) {
+    modulesProvider.getReactDelegateHandlers()
+      .sorted { tuple1, tuple2 -> Bool in
+        return ModulePriorities.get(tuple1.packageName) > ModulePriorities.get(tuple2.packageName)
+      }
+      .forEach { handlerTuple in
+        reactDelegateHandlers.append(handlerTuple.handler.init())
+      }
+  }
+}

packages/expo-modules-core/ios/AppDelegates/EXAppDelegateWrapper.h
@@ -6,9 +6,23 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@interface RCTAppDelegate ()
+
+- (RCTRootViewFactory *)createRCTRootViewFactory;
+
+@end
+
+__deprecated_msg("EXAppDelegateWrapper is deprecated as of SDK 52. Migrate your AppDelegate to Swift and use ExpoAppDelegate instead.")
 @interface EXAppDelegateWrapper : RCTAppDelegate
 
 @property (nonatomic, strong, readonly) EXReactDelegateWrapper *reactDelegate;
+
+/**
+ Currently (RN 0.76) `customizeRootView` signature in `RCTAppDelegate` is broken as it uses `RCTRootView` type,
+ but this type is no longer used. It should rather be `RCTSurfaceHostingView`, but for simplicity it could be just `UIView`.
+ We need a helper function in Objective-C to actually make it to work, otherwise the types will conflict in Swift.
+ */
++ (void)customizeRootView:(nonnull UIView *)rootView byAppDelegate:(nonnull RCTAppDelegate *)appDelegate;
 
 @end
 

packages/expo-modules-core/ios/AppDelegates/ExpoAppDelegate.swift
@@ -1,8 +1,8 @@
 import Dispatch
 import Foundation
+import React_RCTAppDelegate
 
 var subscribers = [ExpoAppDelegateSubscriberProtocol]()
-var reactDelegateHandlers = [ExpoReactDelegateHandler]()
 
 /**
  Allows classes extending `ExpoAppDelegateSubscriber` to hook into project's app delegate
@@ -11,16 +11,14 @@ var reactDelegateHandlers = [ExpoReactDelegateHandler]()
  Keep functions and markers in sync with https://developer.apple.com/documentation/uikit/uiapplicationdelegate
  */
 @objc(EXExpoAppDelegate)
-open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
-  open var window: UIWindow?
-
-  @objc
-  public let reactDelegate = ExpoReactDelegate(handlers: reactDelegateHandlers)
-
+open class ExpoAppDelegate: ExpoAppInstance {
   #if os(iOS) || os(tvOS)
   // MARK: - Initializing the App
 
-  open func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+  open override func application(
+    _ application: UIApplication,
+    willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
     let parsedSubscribers = subscribers.filter {
       $0.responds(to: #selector(application(_:willFinishLaunchingWithOptions:)))
     }
@@ -37,10 +35,21 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
     }
   }
 
-  open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-    return subscribers.reduce(false) { result, subscriber in
-      return subscriber.application?(application, didFinishLaunchingWithOptions: launchOptions) ?? false || result
+  open override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    if UIApplication.shared.delegate is ExpoAppDelegate {
+      // Super function from `RCTAppDelegate` must be called only when the class is used as the base class of the AppDelegate.
+      // For backwards compatibility `EXAppDelegateWrapper` creates a detached instance of `ExpoAppDelegate`, in which case this call must be skipped.
+      super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
+
+    subscribers.forEach { subscriber in
+      // Subscriber result is ignored as it doesn't matter if any subscriber handled the incoming URL – we always return `true` anyway.
+      _ = subscriber.application?(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    return true
   }
 
   // TODO: - Configuring and Discarding Scenes
@@ -48,30 +57,26 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
   // MARK: - Responding to App Life-Cycle Events
 
   @objc
-  open func applicationDidBecomeActive(_ application: UIApplication) {
+  open override func applicationDidBecomeActive(_ application: UIApplication) {
     subscribers.forEach { $0.applicationDidBecomeActive?(application) }
   }
 
   @objc
-  open func applicationWillResignActive(_ application: UIApplication) {
+  open override func applicationWillResignActive(_ application: UIApplication) {
     subscribers.forEach { $0.applicationWillResignActive?(application) }
   }
 
   @objc
-  open func applicationDidEnterBackground(_ application: UIApplication) {
+  open override func applicationDidEnterBackground(_ application: UIApplication) {
     subscribers.forEach { $0.applicationDidEnterBackground?(application) }
   }
 
-  open func applicationWillEnterForeground(_ application: UIApplication) {
+  open override func applicationWillEnterForeground(_ application: UIApplication) {
     subscribers.forEach { $0.applicationWillEnterForeground?(application) }
   }
 
-  open func applicationWillTerminate(_ application: UIApplication) {
+  open override func applicationWillTerminate(_ application: UIApplication) {
     subscribers.forEach { $0.applicationWillTerminate?(application) }
-  }
-
-  @objc public func customizeRootView(_ rootView: UIView) {
-    subscribers.forEach { $0.customizeRootView?(rootView) }
   }
 
   // TODO: - Responding to Environment Changes
@@ -80,7 +85,11 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
 
   // MARK: - Downloading Data in the Background
 
-  open func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+  open override func application(
+    _ application: UIApplication,
+    handleEventsForBackgroundURLSession identifier: String,
+    completionHandler: @escaping () -> Void
+  ) {
     let selector = #selector(application(_:handleEventsForBackgroundURLSession:completionHandler:))
     let subs = subscribers.filter { $0.responds(to: selector) }
     var subscribersLeft = subs.count
@@ -107,15 +116,15 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
 
   // MARK: - Handling Remote Notification Registration
 
-  open func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+  open override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     subscribers.forEach { $0.application?(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken) }
   }
 
-  open func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+  open override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
     subscribers.forEach { $0.application?(application, didFailToRegisterForRemoteNotificationsWithError: error) }
   }
 
-  open func application(
+  open override func application(
     _ application: UIApplication,
     didReceiveRemoteNotification userInfo: [AnyHashable: Any],
     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
@@ -160,13 +169,13 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
 
   // MARK: - Continuing User Activity and Handling Quick Actions
 
-  open func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
+  open override func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
     return subscribers.reduce(false) { result, subscriber in
       return subscriber.application?(application, willContinueUserActivityWithType: userActivityType) ?? false || result
     }
   }
 
-  open func application(
+  open override func application(
     _ application: UIApplication,
     continue userActivity: NSUserActivity,
     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
@@ -196,18 +205,22 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
     }
   }
 
-  open func application(_ application: UIApplication, didUpdate userActivity: NSUserActivity) {
+  open override func application(_ application: UIApplication, didUpdate userActivity: NSUserActivity) {
     return subscribers.forEach { $0.application?(application, didUpdate: userActivity) }
   }
 
-  open func application(_ application: UIApplication, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {
+  open override func application(_ application: UIApplication, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {
     return subscribers.forEach {
       $0.application?(application, didFailToContinueUserActivityWithType: userActivityType, error: error)
     }
   }
 
 #if !os(tvOS)
-  open func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+  open override func application(
+    _ application: UIApplication,
+    performActionFor shortcutItem: UIApplicationShortcutItem,
+    completionHandler: @escaping (Bool) -> Void
+  ) {
     let selector = #selector(application(_:performActionFor:completionHandler:))
     let subs = subscribers.filter { $0.responds(to: selector) }
     var subscribersLeft = subs.count
@@ -237,7 +250,10 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
 
   // MARK: - Background Fetch
 
-  open func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+  open override func application(
+    _ application: UIApplication,
+    performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+  ) {
     let selector = #selector(application(_:performFetchWithCompletionHandler:))
     let subs = subscribers.filter { $0.responds(to: selector) }
     var subscribersLeft = subs.count
@@ -282,7 +298,7 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
 
   // MARK: - Opening a URL-Specified Resource
 
-  open func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+  open override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
     return subscribers.reduce(false) { result, subscriber in
       return subscriber.application?(app, open: url, options: options) ?? false || result
     }
@@ -301,7 +317,7 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
    * a different orientation.
    */
 #if !os(tvOS)
-  open func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+  open override func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
     let deviceOrientationMask = allowedOrientations(for: UIDevice.current.userInterfaceIdiom)
     let universalOrientationMask = allowedOrientations(for: .unspecified)
     let infoPlistOrientations = deviceOrientationMask.isEmpty ? universalOrientationMask : deviceOrientationMask
@@ -322,6 +338,13 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
 #endif
 
   #endif // os(iOS)
+
+  // MARK: - ExpoAppDelegateSubscriberProtocol
+
+  @objc
+  open override func customize(_ rootView: RCTRootView) {
+    subscribers.forEach { $0.customizeRootView?(rootView) }
+  }
 
   // MARK: - Statics
 
@@ -347,17 +370,6 @@ open class ExpoAppDelegate: UIResponder, UIApplicationDelegate {
 
   public static func getSubscriberOfType<Subscriber>(_ type: Subscriber.Type) -> Subscriber? {
     return subscribers.first { $0 is Subscriber } as? Subscriber
-  }
-
-  @objc
-  public static func registerReactDelegateHandlersFrom(modulesProvider: ModulesProvider) {
-    modulesProvider.getReactDelegateHandlers()
-      .sorted { tuple1, tuple2 -> Bool in
-        return ModulePriorities.get(tuple1.packageName) > ModulePriorities.get(tuple2.packageName)
-      }
-      .forEach { handlerTuple in
-        reactDelegateHandlers.append(handlerTuple.handler.init())
-      }
   }
 }
 

packages/expo-modules-core/ios/ReactDelegates/EXReactRootViewFactory.h
@@ -3,22 +3,23 @@
 #pragma once
 
 #import <ExpoModulesCore/Platform.h>
-#import <ExpoModulesCore/EXReactDelegateWrapper.h>
 #import <ExpoModulesCore/RCTAppDelegateUmbrella.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class EXReactDelegate;
+
 NS_SWIFT_NAME(ExpoReactRootViewFactory)
 @interface EXReactRootViewFactory : RCTRootViewFactory
 
-@property (nonatomic, weak, nullable) EXReactDelegateWrapper *reactDelegate;
+@property (nonatomic, weak, nullable) EXReactDelegate *reactDelegate;
 
 /**
  Initializer for EXAppDelegateWrapper integration
  */
-- (instancetype)initWithReactDelegate:(nullable EXReactDelegateWrapper *)reactDelegate
+- (instancetype)initWithReactDelegate:(nullable EXReactDelegate *)reactDelegate
                         configuration:(RCTRootViewFactoryConfiguration *)configuration
-           turboModuleManagerDelegate:(id<RCTTurboModuleManagerDelegate>)turboModuleManagerDelegate;
+           turboModuleManagerDelegate:(nullable id)turboModuleManagerDelegate;
 
 /**
  Calls super `viewWithModuleName:initialProperties:launchOptions:` from `RCTRootViewFactory`.

packages/expo-modules-core/ios/ReactDelegates/EXReactRootViewFactory.mm
@@ -1,9 +1,9 @@
 // Copyright 2018-present 650 Industries. All rights reserved.
 
 #import <ExpoModulesCore/EXReactRootViewFactory.h>
-
 #import <ExpoModulesCore/EXReactDelegateWrapper+Private.h>
 #import <ExpoModulesCore/RCTAppDelegateUmbrella.h>
+#import <ExpoModulesCore/Swift.h>
 
 @interface RCTRootViewFactory ()
 
@@ -13,9 +13,9 @@
 
 @implementation EXReactRootViewFactory
 
-- (instancetype)initWithReactDelegate:(nullable EXReactDelegateWrapper *)reactDelegate
+- (instancetype)initWithReactDelegate:(nullable EXReactDelegate *)reactDelegate
                         configuration:(RCTRootViewFactoryConfiguration *)configuration
-           turboModuleManagerDelegate:(id<RCTTurboModuleManagerDelegate>)turboModuleManagerDelegate
+           turboModuleManagerDelegate:(nullable id<RCTTurboModuleManagerDelegate>)turboModuleManagerDelegate
 {
   if (self = [super initWithConfiguration:configuration andTurboModuleManagerDelegate:turboModuleManagerDelegate]) {
     self.reactDelegate = reactDelegate;
@@ -28,7 +28,7 @@
                  launchOptions:(nullable NSDictionary *)launchOptions
 {
   if (self.reactDelegate != nil) {
-    return [self.reactDelegate createReactRootView:moduleName initialProperties:initialProperties launchOptions:launchOptions];
+    return [self.reactDelegate createReactRootViewWithModuleName:moduleName initialProperties:initialProperties launchOptions:launchOptions];
   }
   return [super viewWithModuleName:moduleName initialProperties:initialProperties launchOptions:launchOptions];
 }

commit a325e5ffa58bb0411609e24deaa3afc51ef9533e

[core] fix AppDelegate.mm methods not be called (#34464)
# Why

regression from #33348 that delegate methods from AppDelegate.mm are not
be called

# How

When using AppDelegate.mm, the `self` in ExpoAppInstance is not the true
AppDelegate.mm, since the hierarchy is

```
AppDelegate.mm -> derive from ExpoAppDelegateWrapper -> composite ExpoAppInstance
```

where in AppDelegate.swift it's

```
AppDelegate.swift -> derive from ExpoAppDelegate -> derive from ExpoAppInstance
```

to make AppDelegate.mm case works, we pass the true AppDelegate to
ExpoAppInstance and call it when it's available.

# Test Plan

- test `expo@52.0.27` with react-native-bootsplash and add a break point
at `customizeRootView`
- ci passed

# Checklist

- [x] I added a `changelog.md` entry and rebuilt the package sources
according to [this short
guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [x] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).
- [x] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

---------

Co-authored-by: Vojtech Novak <vonovak@gmail.com>

packages/expo-modules-core/ios/AppDelegates/EXAppDelegateWrapper.mm
@@ -24,7 +24,8 @@
 - (instancetype)init
 {
   if (self = [super init]) {
-    _expoAppDelegate = [[EXExpoAppDelegate alloc] init];
+    // TODO(kudo) to remove the `initWithAppDelegate` initializer when `EXAppDelegateWrapper` is removed
+    _expoAppDelegate = [[EXExpoAppDelegate alloc] initWithAppDelegate:self];
     _expoAppDelegate.shouldCallReactNativeSetup = NO;
   }
   return self;

packages/expo-modules-core/ios/AppDelegates/ExpoAppInstance.swift
@@ -4,6 +4,18 @@ private var reactDelegateHandlers = [ExpoReactDelegateHandler]()
 
 @objc(EXAppInstance)
 open class ExpoAppInstance: RCTAppDelegate {
+  /**
+   When using AppDelegate.mm which does not inherit ExpoAppInstance directly,
+   we pass an appDelegate to allow execute functions upon the true AppDelegate.
+   */
+  private weak var appDelegate: RCTAppDelegate?
+
+  @objc
+  public convenience init(appDelegate: RCTAppDelegate) {
+    self.init()
+    self.appDelegate = appDelegate
+  }
+
   @objc
   public let reactDelegate = ExpoReactDelegate(handlers: reactDelegateHandlers)
 
@@ -22,8 +34,11 @@ open class ExpoAppInstance: RCTAppDelegate {
 
   @objc
   open override func createRCTRootViewFactory() -> RCTRootViewFactory {
+    let appDelegate = self.appDelegate ?? self
+
     let bundleUrlBlock: RCTBundleURLBlock = { [weak self] in
-      return self?.bundleURL()
+      let appDelegateWeak = self?.appDelegate ?? self
+      return appDelegateWeak?.bundleURL()
     }
 
     let configuration = RCTRootViewFactoryConfiguration(
@@ -34,16 +49,16 @@ open class ExpoAppInstance: RCTAppDelegate {
     )
 
     configuration.createRootViewWithBridge = { bridge, moduleName, initProps in
-      return self.createRootView(with: bridge, moduleName: moduleName, initProps: initProps)
+      return appDelegate.createRootView(with: bridge, moduleName: moduleName, initProps: initProps)
     }
 
     configuration.createBridgeWithDelegate = { delegate, launchOptions in
-      return self.createBridge(with: delegate, launchOptions: launchOptions)
+      return appDelegate.createBridge(with: delegate, launchOptions: launchOptions)
     }
 
     configuration.customizeRootView = { rootView in
       // @tsapeta: We cannot just call `self.customize(rootView)` – see the comment of the `customizeRootView:byAppDelegate:` function in EXAppDelegateWrapper.h
-      return EXAppDelegateWrapper.customizeRootView(rootView, by: self)
+      return EXAppDelegateWrapper.customizeRootView(rootView, by: appDelegate)
     }
 
     // NOTE(kudo): `sourceURLForBridge` is not referenced intentionally because it does not support New Architecture.
@@ -51,26 +66,26 @@ open class ExpoAppInstance: RCTAppDelegate {
 
     if responds(to: #selector(extraModules(for:))) {
       configuration.extraModulesForBridge = { bridge in
-        return self.extraModules(for: bridge)
+        return appDelegate.extraModules(for: bridge)
       }
     }
 
     if responds(to: #selector(extraLazyModuleClasses(for:))) {
       configuration.extraLazyModuleClassesForBridge = { bridge in
-        return self.extraLazyModuleClasses(for: bridge)
+        return appDelegate.extraLazyModuleClasses(for: bridge)
       }
     }
 
     if responds(to: #selector(bridge(_:didNotFindModule:))) {
       configuration.bridgeDidNotFindModule = { bridge, moduleName in
-        return self.bridge(bridge, didNotFindModule: moduleName)
+        return appDelegate.bridge(bridge, didNotFindModule: moduleName)
       }
     }
 
     return ExpoReactRootViewFactory(
       reactDelegate: reactDelegate,
       configuration: configuration,
-      turboModuleManagerDelegate: self
+      turboModuleManagerDelegate: appDelegate
     )
   }
 