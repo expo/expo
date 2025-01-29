72adeae65b2ab0619d2e0bde0b0f03ebd798a2c7
Vojtech Novak <vonovak@gmail.com>
2025-01-29 11:48:12 +0100

    feat: move rootViewFactory to ExpoAppInstance.swift

--- packages/expo-modules-core/ios/AppDelegates/ExpoAppInstance.swift
+++ packages/expo-modules-core/ios/AppDelegates/ExpoAppInstance.swift
@@ -9,6 +9,7 @@ open class ExpoAppInstance: RCTAppDelegate {
    we pass an appDelegate to allow execute functions upon the true AppDelegate.
    */
   private weak var appDelegate: RCTAppDelegate?
+	private lazy var rootViewFactoryInstance: RCTRootViewFactory = createRCTRootViewFactoryPrivate()
 
   @objc
   public convenience init(appDelegate: RCTAppDelegate) {
@@ -32,8 +33,12 @@ open class ExpoAppInstance: RCTAppDelegate {
 #endif
   }
 
+	open override func rootViewFactory() -> RCTRootViewFactory {
+		return rootViewFactoryInstance
+	}
+
   @objc
-  open override func createRCTRootViewFactory() -> RCTRootViewFactory {
+  open func createRCTRootViewFactoryPrivate() -> RCTRootViewFactory {
     let appDelegate = self.appDelegate ?? self
 
     let bundleUrlBlock: RCTBundleURLBlock = { [weak self] in
