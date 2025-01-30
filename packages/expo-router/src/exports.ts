9ad9e63128f2de83e5f7311fa8b7442a17fc170c
Mark Lawlor <mwlawlor@gmail.com>
2025-01-30 17:30:07 +1000

    Add preload export

--- packages/expo-router/src/exports.ts
+++ packages/expo-router/src/exports.ts
@@ -16,6 +16,7 @@ export {
 export { router, Router } from './imperative-api';
 
 export { Link, Redirect, RedirectProps, LinkProps, WebAnchorProps } from './link/Link';
+export { Preload } from './Preload';
 
 export { withLayoutContext } from './layouts/withLayoutContext';
 export { Navigator, Slot };
