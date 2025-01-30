feccff7468e1809e51202e32ca864f3a49724099
Phil Pluckthun <phil@kitten.sh>
2025-01-30 06:42:16 +0000

    Merge spawnSudoAsync into relevant command and remove sudoPrompt invocation

--- packages/@expo/package-manager/src/utils/spawn.ts
+++ packages/@expo/package-manager/src/utils/spawn.ts
@@ -42,29 +42,3 @@ export function createPendingSpawnAsync<V, T>(
   (pendingPromise as PendingSpawnPromise<T>).child = child;
   return pendingPromise as PendingSpawnPromise<T>;
 }
-
-/**
- * Spawn a command with sudo privileges.
- * On windows, this uses the `sudo-prompt` package.
- * on other systems, this uses the `sudo` binary.
- */
-export async function spawnSudoAsync(command: string[], spawnOptions: SpawnOptions): Promise<void> {
-  // sudo prompt only seems to work on win32 machines.
-  if (process.platform === 'win32') {
-    return new Promise((resolve, reject) => {
-      sudo.exec(command.join(' '), { name: 'pod install' }, (error) => {
-        if (error) {
-          reject(error);
-        }
-        resolve();
-      });
-    });
-  } else {
-    // Attempt to use sudo to run the command on Mac and Linux.
-    // TODO(Bacon): Make a v of sudo-prompt that's win32 only for better bundle size.
-    console.log(
-      'Your password might be needed to install CocoaPods CLI: https://guides.cocoapods.org/using/getting-started.html#installation'
-    );
-    await spawnAsync('sudo', command, spawnOptions);
-  }
-}
