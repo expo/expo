bd240eb3137bd0bba0183194fbc7d93acdaecdc4
Mark Lawlor <mwlawlor@gmail.com>
2025-01-31 06:33:45 +1000

    Rebuild

---
+++ packages/expo-router/build/fork/native-stack/createNativeStackNavigator.d.ts
@@ -0,0 +1,17 @@
+import { type NavigatorTypeBagBase, type ParamListBase, type StackNavigationState, type StaticConfig, type TypedNavigator } from '@react-navigation/native';
+import { NativeStackNavigatorProps, NativeStackNavigationOptions, NativeStackNavigationEventMap, NativeStackNavigationProp } from '@react-navigation/native-stack';
+import * as React from 'react';
+declare function NativeStackNavigator({ id, initialRouteName, children, layout, screenListeners, screenOptions, screenLayout, UNSTABLE_getStateForRouteNamesChange, ...rest }: NativeStackNavigatorProps): React.JSX.Element;
+export declare function createNativeStackNavigator<const ParamList extends ParamListBase, const NavigatorID extends string | undefined = undefined, const TypeBag extends NavigatorTypeBagBase = {
+    ParamList: ParamList;
+    NavigatorID: NavigatorID;
+    State: StackNavigationState<ParamList>;
+    ScreenOptions: NativeStackNavigationOptions;
+    EventMap: NativeStackNavigationEventMap;
+    NavigationList: {
+        [RouteName in keyof ParamList]: NativeStackNavigationProp<ParamList, RouteName, NavigatorID>;
+    };
+    Navigator: typeof NativeStackNavigator;
+}, const Config extends StaticConfig<TypeBag> = StaticConfig<TypeBag>>(config?: Config): TypedNavigator<TypeBag, Config>;
+export {};
+//# sourceMappingURL=createNativeStackNavigator.d.ts.map
