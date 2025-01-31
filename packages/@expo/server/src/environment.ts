bca5b5254fa6a47381f657115ea1f747f24a47df
Phil Pluckthun <phil@kitten.sh>
2025-01-31 12:10:02 +0000

    Add global type declarations from undici

--- packages/@expo/server/src/environment.ts
+++ packages/@expo/server/src/environment.ts
@@ -1,18 +1,27 @@
 /* eslint-disable no-var */
 import './assertion';
 
-declare const Response: {
-  prototype: Response;
-  new (body?: BodyInit | null, init?: ResponseInit): Response;
-  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Response/error_static) */
-  error(): Response;
-  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Response/json_static) */
-  json(data: any, init?: ResponseInit): Response;
-  /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Response/redirect_static) */
-  redirect(url: string | URL, status?: number): Response;
-};
+import type * as undici from 'undici';
 
 declare global {
+  interface Request extends undici.Request {}
+  var Request: typeof Request;
+
+  interface Response extends undici.Response {}
+  var Response: typeof Response;
+
+  interface Headers extends undici.Headers {}
+  var Headers: typeof Headers;
+
+  interface File extends undici.File {}
+  var File: typeof File;
+
+  interface Headers extends undici.Headers {}
+  var Headers: typeof Headers;
+
+  interface FormData extends undici.FormData {}
+  var FormData: typeof FormData;
+
   /** @deprecated */
   var ExpoRequest: typeof Request;
   /** @deprecated */
