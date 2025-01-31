77e9d90f33f9c95ca51532baf569479fed2a86f7
Christian Falch <christian.falch@gmail.com>
2025-01-31 10:23:34 +0100

    removed unused function

--- packages/expo-image-picker/ios/ImagePickerUtils.swift
+++ packages/expo-image-picker/ios/ImagePickerUtils.swift
@@ -14,22 +14,3 @@ func asyncMap<ItemsType: Sequence, ResultType>(
   }
   return values
 }
-
-/**
- Concurrently maps the given sequence. todo: Remove - This function is no longer in use since it caused
- concurrency issues on low end devices when doing a lot of IO in parallell from the handleMultipleMedia
- function in the MediaHandler struct
- */
-func concurrentMap<ItemsType: Sequence, ResultType>(
-  _ items: ItemsType,
-  _ transform: @escaping (ItemsType.Element) async throws -> ResultType
-) async rethrows -> [ResultType] {
-  let tasks = items.map { item in
-    Task {
-      try await transform(item)
-    }
-  }
-  return try await asyncMap(tasks) { task in
-    try await task.value
-  }
-}
