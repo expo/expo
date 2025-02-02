6ae5601d56de6323076e660b9695feb609c7924c
balogunofafrica <balogunakanbi.k@gmail.com>
2025-02-02 15:33:41 +0100

    add util

--- packages/expo-font/plugin/src/utils.ts
+++ packages/expo-font/plugin/src/utils.ts
@@ -18,3 +18,46 @@ export async function resolveFontPaths(fonts: string[], projectRoot: string) {
       (p) => p.endsWith('.ttf') || p.endsWith('.otf') || p.endsWith('.woff') || p.endsWith('.woff2')
     );
 }
+
+const weightMap: Record<string, number> = {
+  thin: 100,
+  extralight: 200,
+  ultralight: 200,
+  light: 300,
+  regular: 400,
+  normal: 400,
+  book: 400,
+  medium: 500,
+  semibold: 600,
+  demibold: 600,
+  bold: 700,
+  extrabold: 800,
+  ultrabold: 800,
+  black: 900,
+  heavy: 900,
+};
+
+export function getFontWeight(filename: string) {
+  for (const weight in weightMap) {
+    if (filename.toLowerCase().includes(weight)) {
+      return weightMap[weight];
+    }
+  }
+
+  return 400;
+}
+
+export function generateFontFamilyXml(files: string[]) {
+  let xml = `<?xml version="1.0" encoding="utf-8"?>\n<font-family xmlns:app="http://schemas.android.com/apk/res-auto">\n`;
+
+  files.forEach((file) => {
+    const filename = path.basename(file, path.extname(file));
+    const fontWeight = getFontWeight(filename);
+    const fontStyle = filename.toLowerCase().includes('italic') ? 'italic' : 'normal';
+
+    xml += `    <font app:fontStyle="${fontStyle}" app:fontWeight="${fontWeight}" app:font="@font/${filename}" />\n`;
+  });
+
+  xml += `</font-family>\n`;
+  return xml;
+}
