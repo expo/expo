fcb31eba65d1c2ed1e2bd57a030e555b4a0f8dd8
Kadi Kraman <kadi@expo.io>
2025-02-05 13:27:07 +0000

    Move old config to root level

--- packages/eslint-config-expo/default.js
+++ packages/eslint-config-expo/default.js
@@ -1,3 +1,45 @@
-const legacyConfig = require('./legacy/default');
+const {
+  jsExtensions,
+  tsExtensions,
+  platformSubextensions,
+  computeExpoExtensions,
+} = require('./utils/extensions');
 
-module.exports = legacyConfig;
+const allExtensions = computeExpoExtensions(
+  [...jsExtensions, ...tsExtensions],
+  platformSubextensions
+);
+
+module.exports = {
+  extends: ['./utils/core.js', './utils/typescript.js', './utils/react.js', './utils/expo.js'],
+  globals: {
+    __DEV__: 'readonly',
+    ErrorUtils: false,
+    FormData: false,
+    XMLHttpRequest: false,
+    alert: false,
+    cancelAnimationFrame: false,
+    cancelIdleCallback: false,
+    clearImmediate: false,
+    fetch: false,
+    navigator: false,
+    process: false,
+    requestAnimationFrame: false,
+    requestIdleCallback: false,
+    setImmediate: false,
+    window: false,
+    'shared-node-browser': true,
+  },
+  settings: {
+    'import/extensions': allExtensions,
+    'import/resolver': {
+      node: { extensions: allExtensions },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.web.*'],
+      env: { browser: true },
+    },
+  ],
+};
