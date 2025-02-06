79381e86058601bef1e87efd509392f12cfd2820
Aman Mittal <amandeepmittal@live.com>
2025-02-06 23:49:51 +0530

    [system-ui][docs] Fix typo in method description

--- packages/expo-system-ui/build/SystemUI.js
+++ packages/expo-system-ui/build/SystemUI.js
@@ -2,7 +2,7 @@ import { Platform, processColor } from 'react-native';
 import ExpoSystemUI from './ExpoSystemUI';
 /**
  * Changes the root view background color.
- * Call this function in the root file outside of you component.
+ * Call this function in the root file outside of your component.
  *
  * @example
  * ```ts
