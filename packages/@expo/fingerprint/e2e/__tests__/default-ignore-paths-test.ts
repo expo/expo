expo/expo

commit 7a8194329a922b2b5153703a9283548ea8a6c90d

[fingerprint] load .env for ExpoConfigLoader (#33967)
# Why

follow up with https://github.com/expo/expo/pull/33800#issuecomment-2571282152

# How

in case people using .env in their app.config.js, we should also load .env in the ExpoConfigLoader. the logic is replicated from expo-cli: https://github.com/expo/expo/blob/5b828039afdb5447093020babdf1084aa44d68fb/packages/%40expo/cli/src/config/configAsync.ts#L47-L48

packages/@expo/fingerprint/build/Options.js
@@ -64,6 +64,7 @@ exports.DEFAULT_IGNORE_PATHS = [
         'imurmurhash',
         'js-tokens',
         'json5',
+        'ms',
         'picocolors',
         'lines-and-columns',
         'require-from-string',

packages/@expo/fingerprint/build/Options.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"Options.js","sourceRoot":"","sources":["../src/Options.ts"],"names":[],"mappings":";;;;;;AAAA,2DAA6B;AAE7B,4CAAoB;AACpB,gDAAwB;AAExB,qCAA2C;AAC3C,iDAAoD;AAEpD,uDAAoD;AACpD,uCAA2E;AAE9D,QAAA,2BAA2B,GAAG,oBAAoB,CAAC;AAEnD,QAAA,oBAAoB,GAAG;IAClC,mCAA2B;IAC3B,UAAU;IACV,uBAAuB;IACvB,sBAAsB;IACtB,yBAAyB;IACzB,2BAA2B;IAC3B,0BAA0B;IAC1B,6BAA6B;IAC7B,kCAAkC;IAClC,iCAAiC;IACjC,oCAAoC;IACpC,4CAA4C;IAC5C,2CAA2C;IAC3C,8CAA8C;IAE9C,8DAA8D;IAC9D,wBAAwB;IAExB,yBAAyB;IACzB,+BAA+B;IAC/B,8BAA8B;IAC9B,iCAAiC;IAEjC,MAAM;IACN,kBAAkB;IAClB,mBAAmB;IACnB,yBAAyB;IACzB,+BAA+B;IAC/B,sCAAsC;IAEtC,mDAAmD;IACnD,cAAc;IAEd,2FAA2F;IAC3F,eAAe;IACf,eAAe;IACf,iBAAiB;IACjB,UAAU;IAEV,6BAA6B;IAC7B,oCAAoC;IAEpC,6DAA6D;IAC7D,6BAA6B;IAC7B,4BAA4B;IAC5B,kCAAkC;IAClC,gCAAgC;IAChC,wCAAwC;IACxC,oBAAoB;QAClB,OAAO;QACP,OAAO;QACP,sBAAsB;QACtB,QAAQ;QACR,aAAa;QACb,UAAU;QACV,aAAa;QACb,WAAW;QACX,OAAO;QACP,YAAY;QACZ,mBAAmB;QACnB,qBAAqB;QACrB,cAAc;QACd,aAAa;QACb,SAAS;QACT,gBAAgB;QAChB,sBAAsB;QACtB,mBAAmB;KACpB,CAAC,IAAI,CAAC,GAAG,CAAC,QAAQ;CACpB,CAAC;AAEW,QAAA,oBAAoB,GAAG,yBAAW,CAAC,8CAA8C,CAAC;AAExF,KAAK,UAAU,qBAAqB,CACzC,WAAmB,EACnB,OAAiB;IAEjB,MAAM,MAAM,GAAG,MAAM,IAAA,wBAAe,EAAC,WAAW,EAAE,OAAO,EAAE,MAAM,IAAI,KAAK,CAAC,CAAC;IAC5E,MAAM,sBAAsB,GAAG,MAAM,uBAAuB,CAC1D,WAAW,EACX,MAAM,EAAE,WAAW,EACnB,OAAO,CACR,CAAC;IACF,OAAO;QACL,WAAW;QACX,SAAS,EAAE,CAAC,SAAS,EAAE,KAAK,CAAC;QAC7B,iBAAiB,EAAE,YAAE,CAAC,IAAI,EAAE,CAAC,MAAM;QACnC,aAAa,EAAE,MAAM;QACrB,WAAW,EAAE,4BAAoB;QACjC,sBAAsB;QACtB,GAAG,MAAM;QACT,mBAAmB;QACnB,GAAG,OAAO;QACV,wFAAwF;QACxF,yBAAyB,EACvB,OAAO,EAAE,yBAAyB;YAClC,MAAM,EAAE,yBAAyB;YACjC,IAAA,iCAAkB,EAAC,WAAW,EAAE,SAAS,CAAC;YAC1C,KAAK;QACP,sBAAsB;QACtB,qBAAqB,EAAE,IAAA,2BAAoB,EAAC,sBAAsB,CAAC;KACpE,CAAC;AACJ,CAAC;AA7BD,sDA6BC;AAED,KAAK,UAAU,uBAAuB,CACpC,WAAmB,EACnB,eAAsC,EACtC,OAA4B;IAE5B,MAAM,WAAW,GAAG;QAClB,GAAG,4BAAoB;QACvB,GAAG,CAAC,eAAe,IAAI,EAAE,CAAC;QAC1B,GAAG,CAAC,OAAO,EAAE,WAAW,IAAI,EAAE,CAAC;QAC/B,GAAG,CAAC,OAAO,EAAE,WAAW,EAAE,GAAG,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,GAAG,UAAU,OAAO,CAAC,IAAI,EAAE,CAAC;KAC3E,CAAC;IAEF,MAAM,qBAAqB,GAAG,cAAI,CAAC,IAAI,CAAC,WAAW,EAAE,mCAA2B,CAAC,CAAC;IAClF,IAAI;QACF,MAAM,iBAAiB,GAAG,MAAM,kBAAE,CAAC,QAAQ,CAAC,qBAAqB,EAAE,MAAM,CAAC,CAAC;QAC3E,MAAM,sBAAsB,GAAG,iBAAiB,CAAC,KAAK,CAAC,IAAI,CAAC,CAAC;QAC7D,KAAK,MAAM,IAAI,IAAI,sBAAsB,EAAE;YACzC,MAAM,WAAW,GAAG,IAAI,CAAC,IAAI,EAAE,CAAC;YAChC,IAAI,WAAW,EAAE;gBACf,WAAW,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;aAC/B;SACF;KACF;IAAC,MAAM,GAAE;IAEV,OAAO,IAAA,4BAAqB,EAAC,WAAW,CAAC,CAAC;AAC5C,CAAC"}
+{"version":3,"file":"Options.js","sourceRoot":"","sources":["../src/Options.ts"],"names":[],"mappings":";;;;;;AAAA,2DAA6B;AAE7B,4CAAoB;AACpB,gDAAwB;AAExB,qCAA2C;AAC3C,iDAAoD;AAEpD,uDAAoD;AACpD,uCAA2E;AAE9D,QAAA,2BAA2B,GAAG,oBAAoB,CAAC;AAEnD,QAAA,oBAAoB,GAAG;IAClC,mCAA2B;IAC3B,UAAU;IACV,uBAAuB;IACvB,sBAAsB;IACtB,yBAAyB;IACzB,2BAA2B;IAC3B,0BAA0B;IAC1B,6BAA6B;IAC7B,kCAAkC;IAClC,iCAAiC;IACjC,oCAAoC;IACpC,4CAA4C;IAC5C,2CAA2C;IAC3C,8CAA8C;IAE9C,8DAA8D;IAC9D,wBAAwB;IAExB,yBAAyB;IACzB,+BAA+B;IAC/B,8BAA8B;IAC9B,iCAAiC;IAEjC,MAAM;IACN,kBAAkB;IAClB,mBAAmB;IACnB,yBAAyB;IACzB,+BAA+B;IAC/B,sCAAsC;IAEtC,mDAAmD;IACnD,cAAc;IAEd,2FAA2F;IAC3F,eAAe;IACf,eAAe;IACf,iBAAiB;IACjB,UAAU;IAEV,6BAA6B;IAC7B,oCAAoC;IAEpC,6DAA6D;IAC7D,6BAA6B;IAC7B,4BAA4B;IAC5B,kCAAkC;IAClC,gCAAgC;IAChC,wCAAwC;IACxC,oBAAoB;QAClB,OAAO;QACP,OAAO;QACP,sBAAsB;QACtB,QAAQ;QACR,aAAa;QACb,UAAU;QACV,aAAa;QACb,WAAW;QACX,OAAO;QACP,IAAI;QACJ,YAAY;QACZ,mBAAmB;QACnB,qBAAqB;QACrB,cAAc;QACd,aAAa;QACb,SAAS;QACT,gBAAgB;QAChB,sBAAsB;QACtB,mBAAmB;KACpB,CAAC,IAAI,CAAC,GAAG,CAAC,QAAQ;CACpB,CAAC;AAEW,QAAA,oBAAoB,GAAG,yBAAW,CAAC,8CAA8C,CAAC;AAExF,KAAK,UAAU,qBAAqB,CACzC,WAAmB,EACnB,OAAiB;IAEjB,MAAM,MAAM,GAAG,MAAM,IAAA,wBAAe,EAAC,WAAW,EAAE,OAAO,EAAE,MAAM,IAAI,KAAK,CAAC,CAAC;IAC5E,MAAM,sBAAsB,GAAG,MAAM,uBAAuB,CAC1D,WAAW,EACX,MAAM,EAAE,WAAW,EACnB,OAAO,CACR,CAAC;IACF,OAAO;QACL,WAAW;QACX,SAAS,EAAE,CAAC,SAAS,EAAE,KAAK,CAAC;QAC7B,iBAAiB,EAAE,YAAE,CAAC,IAAI,EAAE,CAAC,MAAM;QACnC,aAAa,EAAE,MAAM;QACrB,WAAW,EAAE,4BAAoB;QACjC,sBAAsB;QACtB,GAAG,MAAM;QACT,mBAAmB;QACnB,GAAG,OAAO;QACV,wFAAwF;QACxF,yBAAyB,EACvB,OAAO,EAAE,yBAAyB;YAClC,MAAM,EAAE,yBAAyB;YACjC,IAAA,iCAAkB,EAAC,WAAW,EAAE,SAAS,CAAC;YAC1C,KAAK;QACP,sBAAsB;QACtB,qBAAqB,EAAE,IAAA,2BAAoB,EAAC,sBAAsB,CAAC;KACpE,CAAC;AACJ,CAAC;AA7BD,sDA6BC;AAED,KAAK,UAAU,uBAAuB,CACpC,WAAmB,EACnB,eAAsC,EACtC,OAA4B;IAE5B,MAAM,WAAW,GAAG;QAClB,GAAG,4BAAoB;QACvB,GAAG,CAAC,eAAe,IAAI,EAAE,CAAC;QAC1B,GAAG,CAAC,OAAO,EAAE,WAAW,IAAI,EAAE,CAAC;QAC/B,GAAG,CAAC,OAAO,EAAE,WAAW,EAAE,GAAG,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,GAAG,UAAU,OAAO,CAAC,IAAI,EAAE,CAAC;KAC3E,CAAC;IAEF,MAAM,qBAAqB,GAAG,cAAI,CAAC,IAAI,CAAC,WAAW,EAAE,mCAA2B,CAAC,CAAC;IAClF,IAAI;QACF,MAAM,iBAAiB,GAAG,MAAM,kBAAE,CAAC,QAAQ,CAAC,qBAAqB,EAAE,MAAM,CAAC,CAAC;QAC3E,MAAM,sBAAsB,GAAG,iBAAiB,CAAC,KAAK,CAAC,IAAI,CAAC,CAAC;QAC7D,KAAK,MAAM,IAAI,IAAI,sBAAsB,EAAE;YACzC,MAAM,WAAW,GAAG,IAAI,CAAC,IAAI,EAAE,CAAC;YAChC,IAAI,WAAW,EAAE;gBACf,WAAW,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;aAC/B;SACF;KACF;IAAC,MAAM,GAAE;IAEV,OAAO,IAAA,4BAAqB,EAAC,WAAW,CAAC,CAAC;AAC5C,CAAC"}

commit f7b8f65d86b33f2fa966ad43ac98d790df357bd7

[fingerprint] add more ignores from .env loading (#34695)
# Why

seeing a regression from #33967 that coming with new node modules that should be ignored

# How

check fingerprint from a `bunx create-expo-app -t default` project and add non-native module paths

packages/@expo/fingerprint/build/Options.js
@@ -48,33 +48,6 @@ exports.DEFAULT_IGNORE_PATHS = [
     'app.json',
     // Ignore nested node_modules
     '**/node_modules/**/node_modules/**',
-    // Ignore default javascript files when calling `getConfig()`
-    '**/node_modules/@babel/**/*',
-    '**/node_modules/@expo/**/*',
-    '**/node_modules/@jridgewell/**/*',
-    '**/node_modules/expo/config.js',
-    '**/node_modules/expo/config-plugins.js',
-    `**/node_modules/{${[
-        'chalk',
-        'debug',
-        'escape-string-regexp',
-        'getenv',
-        'graceful-fs',
-        'has-flag',
-        'imurmurhash',
-        'js-tokens',
-        'json5',
-        'ms',
-        'picocolors',
-        'lines-and-columns',
-        'require-from-string',
-        'resolve-from',
-        'signal-exit',
-        'sucrase',
-        'supports-color',
-        'ts-interface-checker',
-        'write-file-atomic',
-    ].join(',')}}/**/*`,
 ];
 exports.DEFAULT_SOURCE_SKIPS = SourceSkips_1.SourceSkips.PackageJsonAndroidAndIosScriptsIfNotContainRun;
 async function normalizeOptionsAsync(projectRoot, options) {

packages/@expo/fingerprint/build/Options.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"Options.js","sourceRoot":"","sources":["../src/Options.ts"],"names":[],"mappings":";;;;;;AAAA,2DAA6B;AAE7B,4CAAoB;AACpB,gDAAwB;AAExB,qCAA2C;AAC3C,iDAAoD;AAEpD,uDAAoD;AACpD,uCAA2E;AAE9D,QAAA,2BAA2B,GAAG,oBAAoB,CAAC;AAEnD,QAAA,oBAAoB,GAAG;IAClC,mCAA2B;IAC3B,UAAU;IACV,uBAAuB;IACvB,sBAAsB;IACtB,yBAAyB;IACzB,2BAA2B;IAC3B,0BAA0B;IAC1B,6BAA6B;IAC7B,kCAAkC;IAClC,iCAAiC;IACjC,oCAAoC;IACpC,4CAA4C;IAC5C,2CAA2C;IAC3C,8CAA8C;IAE9C,8DAA8D;IAC9D,wBAAwB;IAExB,yBAAyB;IACzB,+BAA+B;IAC/B,8BAA8B;IAC9B,iCAAiC;IAEjC,MAAM;IACN,kBAAkB;IAClB,mBAAmB;IACnB,yBAAyB;IACzB,+BAA+B;IAC/B,sCAAsC;IAEtC,mDAAmD;IACnD,cAAc;IAEd,2FAA2F;IAC3F,eAAe;IACf,eAAe;IACf,iBAAiB;IACjB,UAAU;IAEV,6BAA6B;IAC7B,oCAAoC;IAEpC,6DAA6D;IAC7D,6BAA6B;IAC7B,4BAA4B;IAC5B,kCAAkC;IAClC,gCAAgC;IAChC,wCAAwC;IACxC,oBAAoB;QAClB,OAAO;QACP,OAAO;QACP,sBAAsB;QACtB,QAAQ;QACR,aAAa;QACb,UAAU;QACV,aAAa;QACb,WAAW;QACX,OAAO;QACP,IAAI;QACJ,YAAY;QACZ,mBAAmB;QACnB,qBAAqB;QACrB,cAAc;QACd,aAAa;QACb,SAAS;QACT,gBAAgB;QAChB,sBAAsB;QACtB,mBAAmB;KACpB,CAAC,IAAI,CAAC,GAAG,CAAC,QAAQ;CACpB,CAAC;AAEW,QAAA,oBAAoB,GAAG,yBAAW,CAAC,8CAA8C,CAAC;AAExF,KAAK,UAAU,qBAAqB,CACzC,WAAmB,EACnB,OAAiB;IAEjB,MAAM,MAAM,GAAG,MAAM,IAAA,wBAAe,EAAC,WAAW,EAAE,OAAO,EAAE,MAAM,IAAI,KAAK,CAAC,CAAC;IAC5E,MAAM,sBAAsB,GAAG,MAAM,uBAAuB,CAC1D,WAAW,EACX,MAAM,EAAE,WAAW,EACnB,OAAO,CACR,CAAC;IACF,OAAO;QACL,WAAW;QACX,SAAS,EAAE,CAAC,SAAS,EAAE,KAAK,CAAC;QAC7B,iBAAiB,EAAE,YAAE,CAAC,IAAI,EAAE,CAAC,MAAM;QACnC,aAAa,EAAE,MAAM;QACrB,WAAW,EAAE,4BAAoB;QACjC,sBAAsB;QACtB,GAAG,MAAM;QACT,mBAAmB;QACnB,GAAG,OAAO;QACV,wFAAwF;QACxF,yBAAyB,EACvB,OAAO,EAAE,yBAAyB;YAClC,MAAM,EAAE,yBAAyB;YACjC,IAAA,iCAAkB,EAAC,WAAW,EAAE,SAAS,CAAC;YAC1C,KAAK;QACP,sBAAsB;QACtB,qBAAqB,EAAE,IAAA,2BAAoB,EAAC,sBAAsB,CAAC;KACpE,CAAC;AACJ,CAAC;AA7BD,sDA6BC;AAED,KAAK,UAAU,uBAAuB,CACpC,WAAmB,EACnB,eAAsC,EACtC,OAA4B;IAE5B,MAAM,WAAW,GAAG;QAClB,GAAG,4BAAoB;QACvB,GAAG,CAAC,eAAe,IAAI,EAAE,CAAC;QAC1B,GAAG,CAAC,OAAO,EAAE,WAAW,IAAI,EAAE,CAAC;QAC/B,GAAG,CAAC,OAAO,EAAE,WAAW,EAAE,GAAG,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,GAAG,UAAU,OAAO,CAAC,IAAI,EAAE,CAAC;KAC3E,CAAC;IAEF,MAAM,qBAAqB,GAAG,cAAI,CAAC,IAAI,CAAC,WAAW,EAAE,mCAA2B,CAAC,CAAC;IAClF,IAAI;QACF,MAAM,iBAAiB,GAAG,MAAM,kBAAE,CAAC,QAAQ,CAAC,qBAAqB,EAAE,MAAM,CAAC,CAAC;QAC3E,MAAM,sBAAsB,GAAG,iBAAiB,CAAC,KAAK,CAAC,IAAI,CAAC,CAAC;QAC7D,KAAK,MAAM,IAAI,IAAI,sBAAsB,EAAE;YACzC,MAAM,WAAW,GAAG,IAAI,CAAC,IAAI,EAAE,CAAC;YAChC,IAAI,WAAW,EAAE;gBACf,WAAW,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;aAC/B;SACF;KACF;IAAC,MAAM,GAAE;IAEV,OAAO,IAAA,4BAAqB,EAAC,WAAW,CAAC,CAAC;AAC5C,CAAC"}
+{"version":3,"file":"Options.js","sourceRoot":"","sources":["../src/Options.ts"],"names":[],"mappings":";;;;;;AAAA,2DAA6B;AAE7B,4CAAoB;AACpB,gDAAwB;AAExB,qCAA2C;AAC3C,iDAAoD;AAEpD,uDAAoD;AACpD,uCAA2E;AAE9D,QAAA,2BAA2B,GAAG,oBAAoB,CAAC;AAEnD,QAAA,oBAAoB,GAAG;IAClC,mCAA2B;IAC3B,UAAU;IACV,uBAAuB;IACvB,sBAAsB;IACtB,yBAAyB;IACzB,2BAA2B;IAC3B,0BAA0B;IAC1B,6BAA6B;IAC7B,kCAAkC;IAClC,iCAAiC;IACjC,oCAAoC;IACpC,4CAA4C;IAC5C,2CAA2C;IAC3C,8CAA8C;IAE9C,8DAA8D;IAC9D,wBAAwB;IAExB,yBAAyB;IACzB,+BAA+B;IAC/B,8BAA8B;IAC9B,iCAAiC;IAEjC,MAAM;IACN,kBAAkB;IAClB,mBAAmB;IACnB,yBAAyB;IACzB,+BAA+B;IAC/B,sCAAsC;IAEtC,mDAAmD;IACnD,cAAc;IAEd,2FAA2F;IAC3F,eAAe;IACf,eAAe;IACf,iBAAiB;IACjB,UAAU;IAEV,6BAA6B;IAC7B,oCAAoC;CACrC,CAAC;AAEW,QAAA,oBAAoB,GAAG,yBAAW,CAAC,8CAA8C,CAAC;AAExF,KAAK,UAAU,qBAAqB,CACzC,WAAmB,EACnB,OAAiB;IAEjB,MAAM,MAAM,GAAG,MAAM,IAAA,wBAAe,EAAC,WAAW,EAAE,OAAO,EAAE,MAAM,IAAI,KAAK,CAAC,CAAC;IAC5E,MAAM,sBAAsB,GAAG,MAAM,uBAAuB,CAC1D,WAAW,EACX,MAAM,EAAE,WAAW,EACnB,OAAO,CACR,CAAC;IACF,OAAO;QACL,WAAW;QACX,SAAS,EAAE,CAAC,SAAS,EAAE,KAAK,CAAC;QAC7B,iBAAiB,EAAE,YAAE,CAAC,IAAI,EAAE,CAAC,MAAM;QACnC,aAAa,EAAE,MAAM;QACrB,WAAW,EAAE,4BAAoB;QACjC,sBAAsB;QACtB,GAAG,MAAM;QACT,mBAAmB;QACnB,GAAG,OAAO;QACV,wFAAwF;QACxF,yBAAyB,EACvB,OAAO,EAAE,yBAAyB;YAClC,MAAM,EAAE,yBAAyB;YACjC,IAAA,iCAAkB,EAAC,WAAW,EAAE,SAAS,CAAC;YAC1C,KAAK;QACP,sBAAsB;QACtB,qBAAqB,EAAE,IAAA,2BAAoB,EAAC,sBAAsB,CAAC;KACpE,CAAC;AACJ,CAAC;AA7BD,sDA6BC;AAED,KAAK,UAAU,uBAAuB,CACpC,WAAmB,EACnB,eAAsC,EACtC,OAA4B;IAE5B,MAAM,WAAW,GAAG;QAClB,GAAG,4BAAoB;QACvB,GAAG,CAAC,eAAe,IAAI,EAAE,CAAC;QAC1B,GAAG,CAAC,OAAO,EAAE,WAAW,IAAI,EAAE,CAAC;QAC/B,GAAG,CAAC,OAAO,EAAE,WAAW,EAAE,GAAG,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,GAAG,UAAU,OAAO,CAAC,IAAI,EAAE,CAAC;KAC3E,CAAC;IAEF,MAAM,qBAAqB,GAAG,cAAI,CAAC,IAAI,CAAC,WAAW,EAAE,mCAA2B,CAAC,CAAC;IAClF,IAAI;QACF,MAAM,iBAAiB,GAAG,MAAM,kBAAE,CAAC,QAAQ,CAAC,qBAAqB,EAAE,MAAM,CAAC,CAAC;QAC3E,MAAM,sBAAsB,GAAG,iBAAiB,CAAC,KAAK,CAAC,IAAI,CAAC,CAAC;QAC7D,KAAK,MAAM,IAAI,IAAI,sBAAsB,EAAE;YACzC,MAAM,WAAW,GAAG,IAAI,CAAC,IAAI,EAAE,CAAC;YAChC,IAAI,WAAW,EAAE;gBACf,WAAW,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;aAC/B;SACF;KACF;IAAC,MAAM,GAAE;IAEV,OAAO,IAAA,4BAAqB,EAAC,WAAW,CAAC,CAAC;AAC5C,CAAC"}

packages/@expo/fingerprint/build/sourcer/ExpoConfigLoader.js
@@ -35,7 +35,10 @@ async function runAsync(programName, args = []) {
     const loadedModules = Object.keys(module_1.default._cache)
         .filter((modulePath) => !loadedModulesBefore.has(modulePath))
         .map((modulePath) => path_1.default.relative(projectRoot, modulePath));
-    const ignoredPaths = await loadIgnoredPathsAsync(ignoredFile);
+    const ignoredPaths = [
+        ...DEFAULT_CONFIG_LOADING_IGNORE_PATHS,
+        ...(await loadIgnoredPathsAsync(ignoredFile)),
+    ];
     const filteredLoadedModules = loadedModules.filter((modulePath) => !(0, Path_1.isIgnoredPath)(modulePath, ignoredPaths));
     const result = JSON.stringify({ config, loadedModules: filteredLoadedModules });
     if (node_process_1.default.send) {
@@ -96,4 +99,43 @@ function setNodeEnv(mode) {
     // @ts-expect-error: Add support for external React libraries being loaded in the same process.
     globalThis.__DEV__ = node_process_1.default.env.NODE_ENV !== 'production';
 }
+// Ignore default javascript files when calling `getConfig()`
+const DEFAULT_CONFIG_LOADING_IGNORE_PATHS = [
+    '**/node_modules/@babel/**/*',
+    '**/node_modules/@expo/**/*',
+    '**/node_modules/@jridgewell/**/*',
+    '**/node_modules/expo/config.js',
+    '**/node_modules/expo/config-plugins.js',
+    `**/node_modules/{${[
+        'ajv',
+        'ajv-formats',
+        'ajv-keywords',
+        'ansi-styles',
+        'chalk',
+        'debug',
+        'dotenv',
+        'dotenv-expand',
+        'escape-string-regexp',
+        'getenv',
+        'graceful-fs',
+        'fast-deep-equal',
+        'fast-uri',
+        'has-flag',
+        'imurmurhash',
+        'js-tokens',
+        'json5',
+        'json-schema-traverse',
+        'ms',
+        'picocolors',
+        'lines-and-columns',
+        'require-from-string',
+        'resolve-from',
+        'schema-utils',
+        'signal-exit',
+        'sucrase',
+        'supports-color',
+        'ts-interface-checker',
+        'write-file-atomic',
+    ].join(',')}}/**/*`,
+];
 //# sourceMappingURL=ExpoConfigLoader.js.map

packages/@expo/fingerprint/build/sourcer/ExpoConfigLoader.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"ExpoConfigLoader.js","sourceRoot":"","sources":["../../src/sourcer/ExpoConfigLoader.ts"],"names":[],"mappings":";AAAA;;GAEG;;;;;;AAEH,2DAA6B;AAC7B,oDAA4B;AAC5B,8DAAiC;AACjC,gEAAmC;AACnC,gDAAwB;AACxB,gEAAuC;AAEvC,kDAAqD;AACrD,wCAAkD;AAClD,wCAA8C;AAE9C,KAAK,UAAU,QAAQ,CAAC,WAAmB,EAAE,OAAiB,EAAE;IAC9D,IAAI,IAAI,CAAC,MAAM,GAAG,CAAC,EAAE;QACnB,OAAO,CAAC,GAAG,CAAC,UAAU,WAAW,8BAA8B,CAAC,CAAC;QACjE,OAAO;KACR;IAED,MAAM,WAAW,GAAG,cAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC,CAAC;IAC1C,MAAM,WAAW,GAAG,IAAI,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,cAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,IAAI,CAAC;IAE3D,2CAA2C;IAC3C,MAAM,mBAAmB,GAAG,IAAI,GAAG,CAAC,MAAM,CAAC,IAAI,CAAC,gBAAM,CAAC,MAAM,CAAC,CAAC,CAAC;IAEhE,MAAM,WAAW,GAAG,IAAA,iCAAkB,EAAC,WAAW,CAAC,CAAC;IACpD,IAAA,qBAAM,EAAC,WAAW,EAAE,2DAA2D,WAAW,GAAG,CAAC,CAAC;IAC/F,OAAO,CAAC,WAAW,CAAC,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;IACvC,UAAU,CAAC,aAAa,CAAC,CAAC;IAC1B,MAAM,EAAE,SAAS,EAAE,GAAG,OAAO,CAAC,IAAA,sBAAW,EAAC,cAAI,CAAC,OAAO,CAAC,WAAW,CAAC,EAAE,aAAa,CAAC,CAAC,CAAC;IACrF,MAAM,MAAM,GAAG,MAAM,SAAS,CAAC,WAAW,EAAE,EAAE,yBAAyB,EAAE,IAAI,EAAE,CAAC,CAAC;IACjF,2CAA2C;IAC3C,MAAM,aAAa,GAAG,MAAM,CAAC,IAAI,CAAC,gBAAM,CAAC,MAAM,CAAC;SAC7C,MAAM,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,CAAC,mBAAmB,CAAC,GAAG,CAAC,UAAU,CAAC,CAAC;SAC5D,GAAG,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,cAAI,CAAC,QAAQ,CAAC,WAAW,EAAE,UAAU,CAAC,CAAC,CAAC;IAE/D,MAAM,YAAY,GAAG,MAAM,qBAAqB,CAAC,WAAW,CAAC,CAAC;IAC9D,MAAM,qBAAqB,GAAG,aAAa,CAAC,MAAM,CAChD,CAAC,UAAU,EAAE,EAAE,CAAC,CAAC,IAAA,oBAAa,EAAC,UAAU,EAAE,YAAY,CAAC,CACzD,CAAC;IACF,MAAM,MAAM,GAAG,IAAI,CAAC,SAAS,CAAC,EAAE,MAAM,EAAE,aAAa,EAAE,qBAAqB,EAAE,CAAC,CAAC;IAChF,IAAI,sBAAO,CAAC,IAAI,EAAE;QAChB,sBAAO,CAAC,IAAI,CAAC,MAAM,CAAC,CAAC;KACtB;SAAM;QACL,OAAO,CAAC,GAAG,CAAC,MAAM,CAAC,CAAC;KACrB;AACH,CAAC;AAED,mCAAmC;AACnC,IAAI,OAAO,CAAC,IAAI,EAAE,QAAQ,KAAK,UAAU,EAAE;IACzC,CAAC,KAAK,IAAI,EAAE;QACV,MAAM,YAAY,GAAG,sBAAO,CAAC,IAAI,CAAC,SAAS,CAAC,CAAC,GAAG,EAAE,EAAE,CAAC,GAAG,KAAK,UAAU,CAAC,CAAC;QACzE,IAAI;YACF,MAAM,QAAQ,CAAC,sBAAO,CAAC,IAAI,CAAC,YAAY,CAAC,EAAE,sBAAO,CAAC,IAAI,CAAC,KAAK,CAAC,YAAY,GAAG,CAAC,CAAC,CAAC,CAAC;SAClF;QAAC,OAAO,CAAC,EAAE;YACV,OAAO,CAAC,KAAK,CAAC,gBAAgB,EAAE,CAAC,CAAC,CAAC;YACnC,sBAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC;SACjB;IACH,CAAC,CAAC,EAAE,CAAC;CACN;AAED;;GAEG;AACH,KAAK,UAAU,qBAAqB,CAAC,WAA0B;IAC7D,IAAI,CAAC,WAAW,EAAE;QAChB,OAAO,8BAAoB,CAAC;KAC7B;IAED,MAAM,WAAW,GAAG,EAAE,CAAC;IACvB,IAAI;QACF,MAAM,iBAAiB,GAAG,MAAM,kBAAE,CAAC,QAAQ,CAAC,WAAW,EAAE,MAAM,CAAC,CAAC;QACjE,MAAM,sBAAsB,GAAG,iBAAiB,CAAC,KAAK,CAAC,IAAI,CAAC,CAAC;QAC7D,KAAK,MAAM,IAAI,IAAI,sBAAsB,EAAE;YACzC,MAAM,WAAW,GAAG,IAAI,CAAC,IAAI,EAAE,CAAC;YAChC,IAAI,WAAW,EAAE;gBACf,WAAW,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;aAC/B;SACF;KACF;IAAC,MAAM,GAAE;IAEV,OAAO,WAAW,CAAC;AACrB,CAAC;AAED;;GAEG;AACH,SAAgB,uBAAuB;IACrC,OAAO,cAAI,CAAC,IAAI,CAAC,SAAS,EAAE,qBAAqB,CAAC,CAAC;AACrD,CAAC;AAFD,0DAEC;AAED;;;GAGG;AACH,SAAS,UAAU,CAAC,IAAkC;IACpD,sBAAO,CAAC,GAAG,CAAC,QAAQ,GAAG,sBAAO,CAAC,GAAG,CAAC,QAAQ,IAAI,IAAI,CAAC;IACpD,sBAAO,CAAC,GAAG,CAAC,SAAS,GAAG,sBAAO,CAAC,GAAG,CAAC,SAAS,IAAI,sBAAO,CAAC,GAAG,CAAC,QAAQ,CAAC;IAEtE,+FAA+F;IAC/F,UAAU,CAAC,OAAO,GAAG,sBAAO,CAAC,GAAG,CAAC,QAAQ,KAAK,YAAY,CAAC;AAC7D,CAAC"}
+{"version":3,"file":"ExpoConfigLoader.js","sourceRoot":"","sources":["../../src/sourcer/ExpoConfigLoader.ts"],"names":[],"mappings":";AAAA;;GAEG;;;;;;AAEH,2DAA6B;AAC7B,oDAA4B;AAC5B,8DAAiC;AACjC,gEAAmC;AACnC,gDAAwB;AACxB,gEAAuC;AAEvC,kDAAqD;AACrD,wCAAkD;AAClD,wCAA8C;AAE9C,KAAK,UAAU,QAAQ,CAAC,WAAmB,EAAE,OAAiB,EAAE;IAC9D,IAAI,IAAI,CAAC,MAAM,GAAG,CAAC,EAAE;QACnB,OAAO,CAAC,GAAG,CAAC,UAAU,WAAW,8BAA8B,CAAC,CAAC;QACjE,OAAO;KACR;IAED,MAAM,WAAW,GAAG,cAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC,CAAC;IAC1C,MAAM,WAAW,GAAG,IAAI,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,cAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,IAAI,CAAC;IAE3D,2CAA2C;IAC3C,MAAM,mBAAmB,GAAG,IAAI,GAAG,CAAC,MAAM,CAAC,IAAI,CAAC,gBAAM,CAAC,MAAM,CAAC,CAAC,CAAC;IAEhE,MAAM,WAAW,GAAG,IAAA,iCAAkB,EAAC,WAAW,CAAC,CAAC;IACpD,IAAA,qBAAM,EAAC,WAAW,EAAE,2DAA2D,WAAW,GAAG,CAAC,CAAC;IAC/F,OAAO,CAAC,WAAW,CAAC,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;IACvC,UAAU,CAAC,aAAa,CAAC,CAAC;IAC1B,MAAM,EAAE,SAAS,EAAE,GAAG,OAAO,CAAC,IAAA,sBAAW,EAAC,cAAI,CAAC,OAAO,CAAC,WAAW,CAAC,EAAE,aAAa,CAAC,CAAC,CAAC;IACrF,MAAM,MAAM,GAAG,MAAM,SAAS,CAAC,WAAW,EAAE,EAAE,yBAAyB,EAAE,IAAI,EAAE,CAAC,CAAC;IACjF,2CAA2C;IAC3C,MAAM,aAAa,GAAG,MAAM,CAAC,IAAI,CAAC,gBAAM,CAAC,MAAM,CAAC;SAC7C,MAAM,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,CAAC,mBAAmB,CAAC,GAAG,CAAC,UAAU,CAAC,CAAC;SAC5D,GAAG,CAAC,CAAC,UAAU,EAAE,EAAE,CAAC,cAAI,CAAC,QAAQ,CAAC,WAAW,EAAE,UAAU,CAAC,CAAC,CAAC;IAE/D,MAAM,YAAY,GAAG;QACnB,GAAG,mCAAmC;QACtC,GAAG,CAAC,MAAM,qBAAqB,CAAC,WAAW,CAAC,CAAC;KAC9C,CAAC;IACF,MAAM,qBAAqB,GAAG,aAAa,CAAC,MAAM,CAChD,CAAC,UAAU,EAAE,EAAE,CAAC,CAAC,IAAA,oBAAa,EAAC,UAAU,EAAE,YAAY,CAAC,CACzD,CAAC;IACF,MAAM,MAAM,GAAG,IAAI,CAAC,SAAS,CAAC,EAAE,MAAM,EAAE,aAAa,EAAE,qBAAqB,EAAE,CAAC,CAAC;IAChF,IAAI,sBAAO,CAAC,IAAI,EAAE;QAChB,sBAAO,CAAC,IAAI,CAAC,MAAM,CAAC,CAAC;KACtB;SAAM;QACL,OAAO,CAAC,GAAG,CAAC,MAAM,CAAC,CAAC;KACrB;AACH,CAAC;AAED,mCAAmC;AACnC,IAAI,OAAO,CAAC,IAAI,EAAE,QAAQ,KAAK,UAAU,EAAE;IACzC,CAAC,KAAK,IAAI,EAAE;QACV,MAAM,YAAY,GAAG,sBAAO,CAAC,IAAI,CAAC,SAAS,CAAC,CAAC,GAAG,EAAE,EAAE,CAAC,GAAG,KAAK,UAAU,CAAC,CAAC;QACzE,IAAI;YACF,MAAM,QAAQ,CAAC,sBAAO,CAAC,IAAI,CAAC,YAAY,CAAC,EAAE,sBAAO,CAAC,IAAI,CAAC,KAAK,CAAC,YAAY,GAAG,CAAC,CAAC,CAAC,CAAC;SAClF;QAAC,OAAO,CAAC,EAAE;YACV,OAAO,CAAC,KAAK,CAAC,gBAAgB,EAAE,CAAC,CAAC,CAAC;YACnC,sBAAO,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC;SACjB;IACH,CAAC,CAAC,EAAE,CAAC;CACN;AAED;;GAEG;AACH,KAAK,UAAU,qBAAqB,CAAC,WAA0B;IAC7D,IAAI,CAAC,WAAW,EAAE;QAChB,OAAO,8BAAoB,CAAC;KAC7B;IAED,MAAM,WAAW,GAAG,EAAE,CAAC;IACvB,IAAI;QACF,MAAM,iBAAiB,GAAG,MAAM,kBAAE,CAAC,QAAQ,CAAC,WAAW,EAAE,MAAM,CAAC,CAAC;QACjE,MAAM,sBAAsB,GAAG,iBAAiB,CAAC,KAAK,CAAC,IAAI,CAAC,CAAC;QAC7D,KAAK,MAAM,IAAI,IAAI,sBAAsB,EAAE;YACzC,MAAM,WAAW,GAAG,IAAI,CAAC,IAAI,EAAE,CAAC;YAChC,IAAI,WAAW,EAAE;gBACf,WAAW,CAAC,IAAI,CAAC,WAAW,CAAC,CAAC;aAC/B;SACF;KACF;IAAC,MAAM,GAAE;IAEV,OAAO,WAAW,CAAC;AACrB,CAAC;AAED;;GAEG;AACH,SAAgB,uBAAuB;IACrC,OAAO,cAAI,CAAC,IAAI,CAAC,SAAS,EAAE,qBAAqB,CAAC,CAAC;AACrD,CAAC;AAFD,0DAEC;AAED;;;GAGG;AACH,SAAS,UAAU,CAAC,IAAkC;IACpD,sBAAO,CAAC,GAAG,CAAC,QAAQ,GAAG,sBAAO,CAAC,GAAG,CAAC,QAAQ,IAAI,IAAI,CAAC;IACpD,sBAAO,CAAC,GAAG,CAAC,SAAS,GAAG,sBAAO,CAAC,GAAG,CAAC,SAAS,IAAI,sBAAO,CAAC,GAAG,CAAC,QAAQ,CAAC;IAEtE,+FAA+F;IAC/F,UAAU,CAAC,OAAO,GAAG,sBAAO,CAAC,GAAG,CAAC,QAAQ,KAAK,YAAY,CAAC;AAC7D,CAAC;AAED,6DAA6D;AAC7D,MAAM,mCAAmC,GAAG;IAC1C,6BAA6B;IAC7B,4BAA4B;IAC5B,kCAAkC;IAClC,gCAAgC;IAChC,wCAAwC;IACxC,oBAAoB;QAClB,KAAK;QACL,aAAa;QACb,cAAc;QACd,aAAa;QACb,OAAO;QACP,OAAO;QACP,QAAQ;QACR,eAAe;QACf,sBAAsB;QACtB,QAAQ;QACR,aAAa;QACb,iBAAiB;QACjB,UAAU;QACV,UAAU;QACV,aAAa;QACb,WAAW;QACX,OAAO;QACP,sBAAsB;QACtB,IAAI;QACJ,YAAY;QACZ,mBAAmB;QACnB,qBAAqB;QACrB,cAAc;QACd,cAAc;QACd,aAAa;QACb,SAAS;QACT,gBAAgB;QAChB,sBAAsB;QACtB,mBAAmB;KACpB,CAAC,IAAI,CAAC,GAAG,CAAC,QAAQ;CACpB,CAAC"}

packages/@expo/fingerprint/src/Options.ts
@@ -53,34 +53,6 @@ export const DEFAULT_IGNORE_PATHS = [
 
   // Ignore nested node_modules
   '**/node_modules/**/node_modules/**',
-
-  // Ignore default javascript files when calling `getConfig()`
-  '**/node_modules/@babel/**/*',
-  '**/node_modules/@expo/**/*',
-  '**/node_modules/@jridgewell/**/*',
-  '**/node_modules/expo/config.js',
-  '**/node_modules/expo/config-plugins.js',
-  `**/node_modules/{${[
-    'chalk',
-    'debug',
-    'escape-string-regexp',
-    'getenv',
-    'graceful-fs',
-    'has-flag',
-    'imurmurhash',
-    'js-tokens',
-    'json5',
-    'ms',
-    'picocolors',
-    'lines-and-columns',
-    'require-from-string',
-    'resolve-from',
-    'signal-exit',
-    'sucrase',
-    'supports-color',
-    'ts-interface-checker',
-    'write-file-atomic',
-  ].join(',')}}/**/*`,
 ];
 
 export const DEFAULT_SOURCE_SKIPS = SourceSkips.PackageJsonAndroidAndIosScriptsIfNotContainRun;

packages/@expo/fingerprint/src/sourcer/ExpoConfigLoader.ts
@@ -36,7 +36,10 @@ async function runAsync(programName: string, args: string[] = []) {
     .filter((modulePath) => !loadedModulesBefore.has(modulePath))
     .map((modulePath) => path.relative(projectRoot, modulePath));
 
-  const ignoredPaths = await loadIgnoredPathsAsync(ignoredFile);
+  const ignoredPaths = [
+    ...DEFAULT_CONFIG_LOADING_IGNORE_PATHS,
+    ...(await loadIgnoredPathsAsync(ignoredFile)),
+  ];
   const filteredLoadedModules = loadedModules.filter(
     (modulePath) => !isIgnoredPath(modulePath, ignoredPaths)
   );
@@ -102,3 +105,43 @@ function setNodeEnv(mode: 'development' | 'production') {
   // @ts-expect-error: Add support for external React libraries being loaded in the same process.
   globalThis.__DEV__ = process.env.NODE_ENV !== 'production';
 }
+
+// Ignore default javascript files when calling `getConfig()`
+const DEFAULT_CONFIG_LOADING_IGNORE_PATHS = [
+  '**/node_modules/@babel/**/*',
+  '**/node_modules/@expo/**/*',
+  '**/node_modules/@jridgewell/**/*',
+  '**/node_modules/expo/config.js',
+  '**/node_modules/expo/config-plugins.js',
+  `**/node_modules/{${[
+    'ajv',
+    'ajv-formats',
+    'ajv-keywords',
+    'ansi-styles',
+    'chalk',
+    'debug',
+    'dotenv',
+    'dotenv-expand',
+    'escape-string-regexp',
+    'getenv',
+    'graceful-fs',
+    'fast-deep-equal',
+    'fast-uri',
+    'has-flag',
+    'imurmurhash',
+    'js-tokens',
+    'json5',
+    'json-schema-traverse',
+    'ms',
+    'picocolors',
+    'lines-and-columns',
+    'require-from-string',
+    'resolve-from',
+    'schema-utils',
+    'signal-exit',
+    'sucrase',
+    'supports-color',
+    'ts-interface-checker',
+    'write-file-atomic',
+  ].join(',')}}/**/*`,
+];

packages/@expo/fingerprint/e2e/__tests__/default-ignore-paths-test.ts
@@ -1,0 +1,68 @@
+import spawnAsync from '@expo/spawn-async';
+import fs from 'fs/promises';
+import path from 'path';
+import rimraf from 'rimraf';
+
+import getFingerprintHashFromCLIAsync from './utils/CLIUtils';
+import {
+  createFingerprintAsync,
+  createProjectHashAsync,
+  diffFingerprintChangesAsync,
+} from '../../src/Fingerprint';
+import { normalizeOptionsAsync } from '../../src/Options';
+import { getHashSourcesAsync } from '../../src/sourcer/Sourcer';
+
+jest.mock('../../src/sourcer/ExpoConfigLoader', () => ({
+  // Mock the getExpoConfigLoaderPath to use the built version rather than the typescript version from src
+  getExpoConfigLoaderPath: jest.fn(() =>
+    jest
+      .requireActual('path')
+      .resolve(__dirname, '..', '..', 'build', 'sourcer', 'ExpoConfigLoader.js')
+  ),
+}));
+
+describe('default template ignore paths', () => {
+  jest.setTimeout(600000);
+  const tmpDir = require('temp-dir');
+  const projectName = 'fingerprint-e2e-default-ignore-paths';
+  const projectRoot = path.join(tmpDir, projectName);
+
+  beforeAll(async () => {
+    rimraf.sync(projectRoot);
+    await spawnAsync('bunx', ['create-expo-app', '-t', 'default', projectName], {
+      stdio: 'inherit',
+      cwd: tmpDir,
+      env: {
+        ...process.env,
+        // Do not inherit the package manager from this repository
+        npm_config_user_agent: undefined,
+      },
+    });
+  });
+
+  afterAll(async () => {
+    rimraf.sync(projectRoot);
+  });
+
+  it('should not contain non-native node modules', async () => {
+    const fingerprint = await createFingerprintAsync(projectRoot);
+    for (const source of fingerprint.sources) {
+      if (source.type === 'contents') {
+        continue;
+      }
+      const { filePath } = source;
+      const nodeModulesIndex = filePath.indexOf('node_modules/');
+      if (nodeModulesIndex < 0) {
+        continue;
+      }
+      const moduleName = filePath.split('/')[1];
+
+      // Heuristic to check if it's a native module
+      // We ignore expo modules first and then check react-native- prefix.
+      if (moduleName === 'expo' || moduleName === '@expo' || moduleName.startsWith('expo-')) {
+        continue;
+      }
+      expect(moduleName).toMatch(/^(react-native-)/);
+    }
+  });
+});

packages/@expo/fingerprint/src/__tests__/Options-test.ts
@@ -16,10 +16,12 @@ describe(normalizeOptionsAsync, () => {
 
   it('should return the default options if no options are provided', async () => {
     const options = await normalizeOptionsAsync('/app');
-    // @ts-expect-error: mutate the objects to only show patterns in the snapshot
-    options.ignorePathMatchObjects = options.ignorePathMatchObjects.map(({ pattern }) => pattern);
-    // @ts-expect-error: mutate the objects to only show patterns in the snapshot
-    options.ignoreDirMatchObjects = options.ignoreDirMatchObjects.map(({ pattern }) => pattern);
+    expect(options.ignorePathMatchObjects.length).toBeGreaterThan(0);
+    expect(options.ignoreDirMatchObjects.length).toBeGreaterThan(0);
+
+    // Because the default ignored paths change over time, we don't want to snapshot them.
+    delete options.ignorePathMatchObjects;
+    delete options.ignoreDirMatchObjects;
     expect(options).toMatchSnapshot();
   });
 

packages/@expo/fingerprint/src/__tests__/__snapshots__/Options-test.ts.snap
@@ -5,67 +5,6 @@ exports[`normalizeOptionsAsync should return the default options if no options a
   "concurrentIoLimit": 1,
   "enableReactImportsPatcher": false,
   "hashAlgorithm": "sha1",
-  "ignoreDirMatchObjects": [
-    "**/android/build",
-    "**/android/.cxx",
-    "**/android/.gradle",
-    "**/android/app/build",
-    "**/android/app/.cxx",
-    "**/android/app/.gradle",
-    "**/android-annotation/build",
-    "**/android-annotation/.cxx",
-    "**/android-annotation/.gradle",
-    "**/android-annotation-processor/build",
-    "**/android-annotation-processor/.cxx",
-    "**/android-annotation-processor/.gradle",
-    "**/*-gradle-plugin/build",
-    "**/*-gradle-plugin/.cxx",
-    "**/*-gradle-plugin/.gradle",
-    "**/ios/Pods",
-    "**/ios/build",
-    "**/ios/*.xcworkspace/xcuserdata",
-    "**/node_modules/**/node_modules",
-    "**/node_modules/@babel",
-    "**/node_modules/@expo",
-    "**/node_modules/@jridgewell",
-    "**/node_modules/{chalk,debug,escape-string-regexp,getenv,graceful-fs,has-flag,imurmurhash,js-tokens,json5,ms,picocolors,lines-and-columns,require-from-string,resolve-from,signal-exit,sucrase,supports-color,ts-interface-checker,write-file-atomic}",
-  ],
-  "ignorePathMatchObjects": [
-    ".fingerprintignore",
-    "**/android/build/**/*",
-    "**/android/.cxx/**/*",
-    "**/android/.gradle/**/*",
-    "**/android/app/build/**/*",
-    "**/android/app/.cxx/**/*",
-    "**/android/app/.gradle/**/*",
-    "**/android-annotation/build/**/*",
-    "**/android-annotation/.cxx/**/*",
-    "**/android-annotation/.gradle/**/*",
-    "**/android-annotation-processor/build/**/*",
-    "**/android-annotation-processor/.cxx/**/*",
-    "**/android-annotation-processor/.gradle/**/*",
-    "**/android/gradlew.bat",
-    "**/*-gradle-plugin/build/**/*",
-    "**/*-gradle-plugin/.cxx/**/*",
-    "**/*-gradle-plugin/.gradle/**/*",
-    "**/ios/Pods/**/*",
-    "**/ios/build/**/*",
-    "**/ios/.xcode.env.local",
-    "**/ios/**/project.xcworkspace",
-    "**/ios/*.xcworkspace/xcuserdata/**/*",
-    "**/.DS_Store",
-    "app.config.ts",
-    "app.config.js",
-    "app.config.json",
-    "app.json",
-    "**/node_modules/**/node_modules/**",
-    "**/node_modules/@babel/**/*",
-    "**/node_modules/@expo/**/*",
-    "**/node_modules/@jridgewell/**/*",
-    "**/node_modules/expo/config.js",
-    "**/node_modules/expo/config-plugins.js",
-    "**/node_modules/{chalk,debug,escape-string-regexp,getenv,graceful-fs,has-flag,imurmurhash,js-tokens,json5,ms,picocolors,lines-and-columns,require-from-string,resolve-from,signal-exit,sucrase,supports-color,ts-interface-checker,write-file-atomic}/**/*",
-  ],
   "platforms": [
     "android",
     "ios",