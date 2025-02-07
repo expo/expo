33439701081f2ee73252bd765a3d64f85e6724a8
Aman Mittal <amandeepmittal@live.com>
2025-02-07 18:25:09 +0530

    [docs] Update `llms.txt` script to read links from additional resources page (#34623)
    # Why
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    Currently, the podcasts and YouTube video links from additional
    resources page aren't included in `llms.txt` file
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    Update the `llms-txt.js` script and add helper methods to convert the
    `talks.ts` file and include links in the generated output.
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    - Run `yarn run generate-llms`
    - In `public/llms-txt` file, the links from `talks.ts` file are now
    included:
    
    ![CleanShot 2025-02-03 at 01 51
    46@2x](https://github.com/user-attachments/assets/ffa8f2a5-8644-453e-8383-d12942bf5bd2)
    
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    
    ---------
    
    Co-authored-by: Simek <gosimek@gmail.com>

--- docs/scripts/generate-llms/index.js
+++ docs/scripts/generate-llms/index.js
@@ -1,8 +1,11 @@
+import { compileTalksFile } from './compileTalks.js';
 import { generateLlmsEasTxt } from './llms-eas-txt.js';
 import { generateLlmsFullTxt } from './llms-full-txt.js';
 import { generateLlmsTxt } from './llms-txt.js';
 
-Promise.all([
+await compileTalksFile();
+
+Promise.allSettled([
   await generateLlmsEasTxt(),
   await generateLlmsFullTxt(),
   await generateLlmsTxt(),
