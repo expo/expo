707cec24ae4190514a98ebbf497a0fd8228971c9
betomoedano <betomoedano@outlook.com>
2025-02-07 11:48:29 -0600

    [docs] rename file to notifications-services

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -351,7 +351,7 @@ export const general = [
         makePage('guides/building-for-tv.mdx'),
         makePage('guides/typescript.mdx'),
         makePage('guides/in-app-purchases.mdx'),
-        makePage('guides/alternatives-for-expo-notifications.mdx'),
+        makePage('guides/notifications-services.mdx'),
       ]),
       makeSection('Troubleshooting', [
         makePage('troubleshooting/overview.mdx'),
