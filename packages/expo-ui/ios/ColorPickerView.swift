a3993a0e4313bea57a8581c29da26602a323c1fa
Andrew <alevy97@gmail.com>
2025-02-10 19:34:43 -0500

    fix swiftlint warnings

--- packages/expo-ui/ios/ColorPickerView.swift
+++ packages/expo-ui/ios/ColorPickerView.swift
@@ -7,7 +7,6 @@ class ColorPickerProps: ExpoSwiftUI.ViewProps {
   @Field var selection: UIColor = .clear
   @Field var label: String?
   @Field var supportsOpacity: Bool = true
-  
   var onValueChanged = EventDispatcher()
 }
 
@@ -15,7 +14,6 @@ struct ColorPickerView: ExpoSwiftUI.View {
   @EnvironmentObject var props: ColorPickerProps
   @State var color: Color = .clear
   @State private var previousHex: String = ""
-  
   var body: some View {
     ColorPicker(props.label ?? "", selection: $color, supportsOpacity: props.supportsOpacity)
       .onAppear {
@@ -31,17 +29,13 @@ struct ColorPickerView: ExpoSwiftUI.View {
         }
       }
   }
-  
   private func colorToHex(_ color: UIColor) -> String {
     var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
     color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-    
     let components = [red, green, blue, alpha].map { Int($0 * 255) }
     let format = props.supportsOpacity ? "#%02X%02X%02X%02X" : "#%02X%02X%02X"
-    
     return props.supportsOpacity 
       ? String(format: format, components[0], components[1], components[2], components[3])
       : String(format: format, components[0], components[1], components[2])
   }
 }
-
