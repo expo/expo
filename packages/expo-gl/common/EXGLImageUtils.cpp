b2dba01148daedf41129a7599792622bd82b380a
Henry Allen <hmallen99@gmail.com>
2025-02-11 11:13:54 -0500

    Add calculateImageSize image utility

--- packages/expo-gl/common/EXGLImageUtils.cpp
+++ packages/expo-gl/common/EXGLImageUtils.cpp
@@ -135,5 +135,23 @@ std::shared_ptr<uint8_t> loadImage(
   }
   return std::shared_ptr<uint8_t>(nullptr);
 }
+
+GLsizei calculateImageSize(GLsizei width, GLsizei height, GLint internalFormat) {
+    switch (internalFormat) {
+        case GL_COMPRESSED_R11_EAC:
+        case GL_COMPRESSED_SIGNED_R11_EAC:
+        case GL_COMPRESSED_RGB8_ETC2:
+        case GL_COMPRESSED_SRGB8_ETC2:
+        case GL_COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+        case GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
+            return (width / 4) * (height / 4) * 8;
+        case GL_COMPRESSED_RG11_EAC:
+        case GL_COMPRESSED_SIGNED_RG11_EAC:
+        case GL_COMPRESSED_RGBA8_ETC2_EAC:
+        case GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
+        default:
+            return (width / 4) * (height / 4) * 16;
+    }
+}
 } // namespace gl_cpp
 } // namespace expo
