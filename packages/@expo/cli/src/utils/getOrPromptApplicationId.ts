expo/expo

commit 577182e336eee5777db1beeb26c623af35261285

[@expo/cli] Remove last use of config owner field (#34209)

packages/@expo/cli/src/utils/getOrPromptApplicationId.ts
@@ -1,4 +1,6 @@
+import { GraphQLError } from '@0no-co/graphql.web';
 import { ExpoConfig, getConfig } from '@expo/config';
+import { CombinedError } from '@urql/core';
 import chalk from 'chalk';
 
 import { memoize } from './fn';
@@ -16,14 +18,28 @@ import {
   validatePackage,
   validatePackageWithWarning,
 } from './validateApplicationId';
+import { AppQuery } from '../api/graphql/queries/AppQuery';
 import * as Log from '../log';
-import { getAccountUsername } from './getAccountUsername';
 
 const debug = require('debug')('expo:app-id') as typeof console.log;
 
-function getExpoUsername(exp: ExpoConfig) {
-  // Account for if the environment variable was an empty string.
-  return getAccountUsername(exp) || 'anonymous';
+async function getRecommendedReverseDomainNameSecondPartAsync(
+  exp: ExpoConfig
+): Promise<string | null> {
+  const easProjectId = exp.extra?.eas?.projectId;
+  if (!easProjectId) {
+    return null;
+  }
+
+  try {
+    const app = await AppQuery.byIdAsync(easProjectId);
+    return app.ownerAccount.name;
+  } catch (e) {
+    if (e instanceof GraphQLError || e instanceof CombinedError) {
+      return null;
+    }
+    throw e;
+  }
 }
 
 const NO_BUNDLE_ID_MESSAGE = `Project must have a \`ios.bundleIdentifier\` set in the Expo config (app.json or app.config.js).`;
@@ -35,7 +51,7 @@ const NO_PACKAGE_MESSAGE = `Project must have a \`android.package\` set in the E
  * Prompted value will be validated against the App Store and a local regex.
  * If the project Expo config is a static JSON file, the bundle identifier will be updated in the config automatically.
  */
-export async function getOrPromptForBundleIdentifier(
+export async function getOrPromptForBundleIdentifierAsync(
   projectRoot: string,
   exp: ExpoConfig = getConfig(projectRoot).exp
 ): Promise<string> {
@@ -45,7 +61,11 @@ export async function getOrPromptForBundleIdentifier(
     return current;
   }
 
-  return promptForBundleIdWithInitialAsync(projectRoot, exp, getRecommendedBundleId(exp));
+  return promptForBundleIdWithInitialAsync(
+    projectRoot,
+    exp,
+    await getRecommendedBundleIdAsync(exp)
+  );
 }
 
 const memoLog = memoize(Log.log);
@@ -120,8 +140,8 @@ async function warnAndConfirmAsync(warning: string): Promise<boolean> {
   return true;
 }
 
-// Recommend a bundle identifier based on the username and project slug.
-function getRecommendedBundleId(exp: ExpoConfig): string | undefined {
+// Recommend a bundle identifier based on the account name of the owner of the project and project slug.
+async function getRecommendedBundleIdAsync(exp: ExpoConfig): Promise<string | undefined> {
   const possibleIdFromAndroid = exp.android?.package
     ? getSanitizedBundleIdentifier(exp.android.package)
     : undefined;
@@ -129,18 +149,25 @@ function getRecommendedBundleId(exp: ExpoConfig): string | undefined {
   if (possibleIdFromAndroid && validateBundleId(possibleIdFromAndroid)) {
     return possibleIdFromAndroid;
   } else {
-    const username = getExpoUsername(exp);
-    const possibleId = getSanitizedBundleIdentifier(`com.${username}.${exp.slug}`);
-    if (validateBundleId(possibleId)) {
-      return possibleId;
+    const recommendedReverseDomainNameSecondPart =
+      await getRecommendedReverseDomainNameSecondPartAsync(exp);
+    if (recommendedReverseDomainNameSecondPart) {
+      const possibleId = getSanitizedBundleIdentifier(
+        `com.${recommendedReverseDomainNameSecondPart}.${exp.slug}`
+      );
+      if (validateBundleId(possibleId)) {
+        return possibleId;
+      }
+    } else {
+      debug('Could not generate recommended name from EAS');
     }
   }
 
   return undefined;
 }
 
-// Recommend a package name based on the username and project slug.
-function getRecommendedPackageName(exp: ExpoConfig): string | undefined {
+// Recommend a package name based on the account name of the owner of the project and project slug.
+async function getRecommendedPackageNameAsync(exp: ExpoConfig): Promise<string | undefined> {
   const possibleIdFromApple = exp.ios?.bundleIdentifier
     ? getSanitizedPackage(exp.ios.bundleIdentifier)
     : undefined;
@@ -149,14 +176,21 @@ function getRecommendedPackageName(exp: ExpoConfig): string | undefined {
   if (possibleIdFromApple && validatePackage(possibleIdFromApple)) {
     return possibleIdFromApple;
   } else {
-    const username = getExpoUsername(exp);
-    const possibleId = getSanitizedPackage(`com.${username}.${exp.slug}`);
-    if (validatePackage(possibleId)) {
-      return possibleId;
-    } else {
-      debug(
-        `Recommended package name is invalid: "${possibleId}" (username: ${username}, slug: ${exp.slug})`
+    const recommendedReverseDomainNameSecondPart =
+      await getRecommendedReverseDomainNameSecondPartAsync(exp);
+    if (recommendedReverseDomainNameSecondPart) {
+      const possibleId = getSanitizedPackage(
+        `com.${recommendedReverseDomainNameSecondPart}.${exp.slug}`
       );
+      if (validatePackage(possibleId)) {
+        return possibleId;
+      } else {
+        debug(
+          `Recommended package name is invalid: "${possibleId}" (owner: ${recommendedReverseDomainNameSecondPart}, slug: ${exp.slug})`
+        );
+      }
+    } else {
+      debug('Could not generate recommended name from EAS');
     }
   }
   return undefined;
@@ -167,7 +201,7 @@ function getRecommendedPackageName(exp: ExpoConfig): string | undefined {
  * Prompted value will be validated against the Play Store and a local regex.
  * If the project Expo config is a static JSON file, the package name will be updated in the config automatically.
  */
-export async function getOrPromptForPackage(
+export async function getOrPromptForPackageAsync(
   projectRoot: string,
   exp: ExpoConfig = getConfig(projectRoot).exp
 ): Promise<string> {
@@ -180,8 +214,12 @@ export async function getOrPromptForPackage(
   return await promptForPackageAsync(projectRoot, exp);
 }
 
-function promptForPackageAsync(projectRoot: string, exp: ExpoConfig): Promise<string> {
-  return promptForPackageWithInitialAsync(projectRoot, exp, getRecommendedPackageName(exp));
+async function promptForPackageAsync(projectRoot: string, exp: ExpoConfig): Promise<string> {
+  return promptForPackageWithInitialAsync(
+    projectRoot,
+    exp,
+    await getRecommendedPackageNameAsync(exp)
+  );
 }
 
 async function promptForPackageWithInitialAsync(

packages/@expo/cli/src/api/graphql/types/App.ts
@@ -6,6 +6,7 @@ export const AppFragmentNode: TypedDocumentNode = gql`
     scopeKey
     ownerAccount {
       id
+      name
     }
   }
 `;

packages/@expo/cli/src/prebuild/configureProjectAsync.ts
@@ -6,8 +6,8 @@ import { logConfig } from '../config/configAsync';
 import * as Log from '../log';
 import { env } from '../utils/env';
 import {
-  getOrPromptForBundleIdentifier,
-  getOrPromptForPackage,
+  getOrPromptForBundleIdentifierAsync,
+  getOrPromptForPackageAsync,
 } from '../utils/getOrPromptApplicationId';
 
 export async function configureProjectAsync(
@@ -25,12 +25,12 @@ export async function configureProjectAsync(
   let bundleIdentifier: string | undefined;
   if (platforms.includes('ios')) {
     // Check bundle ID before reading the config because it may mutate the config if the user is prompted to define it.
-    bundleIdentifier = await getOrPromptForBundleIdentifier(projectRoot, exp);
+    bundleIdentifier = await getOrPromptForBundleIdentifierAsync(projectRoot, exp);
   }
   let packageName: string | undefined;
   if (platforms.includes('android')) {
     // Check package before reading the config because it may mutate the config if the user is prompted to define it.
-    packageName = await getOrPromptForPackage(projectRoot, exp);
+    packageName = await getOrPromptForPackageAsync(projectRoot, exp);
   }
 
   let { exp: config } = await getPrebuildConfigAsync(projectRoot, {

packages/@expo/cli/src/prebuild/ensureConfigAsync.ts
@@ -2,8 +2,8 @@ import { ExpoConfig, getConfig, PackageJSONConfig } from '@expo/config';
 import { ModPlatform } from '@expo/config-plugins';
 
 import {
-  getOrPromptForBundleIdentifier,
-  getOrPromptForPackage,
+  getOrPromptForBundleIdentifierAsync,
+  getOrPromptForPackageAsync,
 } from '../utils/getOrPromptApplicationId';
 
 /** Ensure config is written, and prompts for application identifiers. */
@@ -18,11 +18,11 @@ export async function ensureConfigAsync(
   // Prompt for the Android package first because it's more strict than the bundle identifier
   // this means you'll have a better chance at matching the bundle identifier with the package name.
   if (platforms.includes('android')) {
-    await getOrPromptForPackage(projectRoot);
+    await getOrPromptForPackageAsync(projectRoot);
   }
 
   if (platforms.includes('ios')) {
-    await getOrPromptForBundleIdentifier(projectRoot);
+    await getOrPromptForBundleIdentifierAsync(projectRoot);
   }
 
   // Read config again because prompting for bundle id or package name may have mutated the results.

packages/@expo/cli/src/utils/__tests__/getAccountUsername-test.ts
@@ -1,52 +1,0 @@
-import fs from 'fs';
-import { vol } from 'memfs';
-
-import { getExpoHomeDirectory, getSettings } from '../../api/user/UserSettings';
-import { getAccountUsername } from '../getAccountUsername';
-
-jest.mock('os');
-jest.mock('fs');
-
-// NOTE(cedric): this is a workaround to also mock `node:fs`
-jest.mock('node:fs', () => require('fs'));
-
-describe(getAccountUsername, () => {
-  beforeEach(() => {
-    delete process.env.EXPO_CLI_USERNAME;
-    delete process.env.EAS_BUILD_USERNAME;
-  });
-  afterEach(() => vol.reset());
-
-  it(`gets the account name from EXPO_CLI_USERNAME`, () => {
-    process.env.EXPO_CLI_USERNAME = 'expo-cli-username';
-    process.env.EAS_BUILD_USERNAME = 'eas-build-username';
-    expect(getAccountUsername()).toBe('expo-cli-username');
-  });
-  it(`gets the account name from EAS_BUILD_USERNAME`, () => {
-    process.env.EAS_BUILD_USERNAME = 'eas-build-username';
-    expect(getAccountUsername()).toBe('eas-build-username');
-  });
-  it(`gets the account name from owner`, () => {
-    process.env.EXPO_CLI_USERNAME = 'expo-cli-username';
-    process.env.EAS_BUILD_USERNAME = 'eas-build-username';
-    expect(getAccountUsername({ owner: 'owner-username' })).toBe('owner-username');
-  });
-  it(`gets the account name from owner 2`, () => {
-    expect(getAccountUsername({ owner: 'owner-username' })).toBe('owner-username');
-  });
-  it(`uses anonymous name`, () => {
-    // Ensure the test doesn't interact with the developer's state.json
-    expect(getExpoHomeDirectory()).toBe('/home/.expo');
-    expect(getAccountUsername()).toBe('anonymous');
-  });
-  it(`uses previously authenticated username`, async () => {
-    // Ensure the test doesn't interact with the developer's state.json
-    expect(getExpoHomeDirectory()).toBe('/home/.expo');
-    // Ensure the dir exists
-    fs.mkdirSync(getExpoHomeDirectory(), { recursive: true });
-    // Set a username...
-    await getSettings().setAsync('auth', { username: 'bacon-boi' });
-    // Check the username...
-    expect(getAccountUsername()).toBe('bacon-boi');
-  });
-});

packages/@expo/cli/src/utils/getAccountUsername.ts
@@ -1,23 +1,0 @@
-import { ExpoConfig } from '@expo/config';
-
-import { getSettings } from '../api/user/UserSettings';
-
-const ANONYMOUS_USERNAME = 'anonymous';
-
-/**
- * Get the owner of the project from the manifest if specified, falling back to a bunch of different things
- * which may or may not be the owner of the project.
- *
- * @deprecated This may not actually be the owner of the project. Prefer to fetch the project owner using
- * the EAS project ID, falling back to the `owner` field.
- */
-export function getAccountUsername(manifest: Pick<ExpoConfig, 'owner'> = {}): string {
-  // TODO: Must match what's generated in Expo Go.
-  const username =
-    manifest.owner || process.env.EXPO_CLI_USERNAME || process.env.EAS_BUILD_USERNAME;
-  if (username) {
-    return username;
-  }
-  // Statically get the username from the global user state.
-  return getSettings().read().auth?.username || ANONYMOUS_USERNAME;
-}

commit 69fb1b4d7d419d285aa6bed50ed7e511c99dd9cc

fix(cli): Fix application identifier suggestion. (#34843)
# Why

When trying the latest canary, I found that app id suggestions no longer
work. This is a regression from https://github.com/expo/expo/pull/34209
where we now make some sort of network request for the username instead
of using the cached value and falling back on "anonymous".

---------

Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

packages/@expo/cli/src/utils/getOrPromptApplicationId.ts
@@ -19,13 +19,21 @@ import {
   validatePackageWithWarning,
 } from './validateApplicationId';
 import { AppQuery } from '../api/graphql/queries/AppQuery';
+import { getSettings } from '../api/user/UserSettings';
 import * as Log from '../log';
 
 const debug = require('debug')('expo:app-id') as typeof console.log;
 
+const ANONYMOUS_USERNAME = 'anonymous';
+
 async function getRecommendedReverseDomainNameSecondPartAsync(
   exp: ExpoConfig
 ): Promise<string | null> {
+  // Get the cached username.
+  const cachedUsername = getSettings().read().auth?.username;
+  if (cachedUsername) {
+    return cachedUsername;
+  }
   const easProjectId = exp.extra?.eas?.projectId;
   if (!easProjectId) {
     return null;
@@ -150,16 +158,12 @@ async function getRecommendedBundleIdAsync(exp: ExpoConfig): Promise<string | un
     return possibleIdFromAndroid;
   } else {
     const recommendedReverseDomainNameSecondPart =
-      await getRecommendedReverseDomainNameSecondPartAsync(exp);
-    if (recommendedReverseDomainNameSecondPart) {
-      const possibleId = getSanitizedBundleIdentifier(
-        `com.${recommendedReverseDomainNameSecondPart}.${exp.slug}`
-      );
-      if (validateBundleId(possibleId)) {
-        return possibleId;
-      }
-    } else {
-      debug('Could not generate recommended name from EAS');
+      (await getRecommendedReverseDomainNameSecondPartAsync(exp)) ?? ANONYMOUS_USERNAME;
+    const possibleId = getSanitizedBundleIdentifier(
+      `com.${recommendedReverseDomainNameSecondPart}.${exp.slug}`
+    );
+    if (validateBundleId(possibleId)) {
+      return possibleId;
     }
   }
 
@@ -177,20 +181,17 @@ async function getRecommendedPackageNameAsync(exp: ExpoConfig): Promise<string |
     return possibleIdFromApple;
   } else {
     const recommendedReverseDomainNameSecondPart =
-      await getRecommendedReverseDomainNameSecondPartAsync(exp);
-    if (recommendedReverseDomainNameSecondPart) {
-      const possibleId = getSanitizedPackage(
-        `com.${recommendedReverseDomainNameSecondPart}.${exp.slug}`
-      );
-      if (validatePackage(possibleId)) {
-        return possibleId;
-      } else {
-        debug(
-          `Recommended package name is invalid: "${possibleId}" (owner: ${recommendedReverseDomainNameSecondPart}, slug: ${exp.slug})`
-        );
-      }
+      (await getRecommendedReverseDomainNameSecondPartAsync(exp)) ?? ANONYMOUS_USERNAME;
+
+    const possibleId = getSanitizedPackage(
+      `com.${recommendedReverseDomainNameSecondPart}.${exp.slug}`
+    );
+    if (validatePackage(possibleId)) {
+      return possibleId;
     } else {
-      debug('Could not generate recommended name from EAS');
+      debug(
+        `Recommended package name is invalid: "${possibleId}" (owner: ${recommendedReverseDomainNameSecondPart}, slug: ${exp.slug})`
+      );
     }
   }
   return undefined;