97d7e43929ca44e5bb0165eb4a9771b830eab441
evanbacon <bacon@expo.io>
2025-02-14 14:52:26 -0800

    drop

--- apps/router-e2e/__e2e__/worker/app/index.tsx
+++ apps/router-e2e/__e2e__/worker/app/index.tsx
@@ -6,22 +6,15 @@ const url = new URL(entry, window.location.href);
 console.log('url:', url, entry);
 const worker = new Worker(url);
 console.log('res:', worker);
-///
+
 worker.onmessage = (event) => {
   console.log(`Worker responded: ${event.data}`);
 };
 
 worker.postMessage(5); // Send a number to the worker
 
-//////
 export default function Page() {
   const [data, setData] = useState([]);
-
-  useEffect(() => {
-    // worker.onmessage = function (event) {
-    //   setData((data) => [...data, JSON.stringify(event.data)]);
-    // };
-  }, []);
 
   return (
     <>
--- apps/router-e2e/__e2e__/worker/worker-one.ts
+++ apps/router-e2e/__e2e__/worker/worker-one.ts
@@ -1,12 +1,5 @@
-// import '@expo/metro-runtime';
-
 self.onmessage = (event) => {
   const { data } = event;
-  const result = data * 4; // Example: double the number
+  const result = data * 2; // Example: double the number
   postMessage(result);
 };
-
-//
-
-// /packages/expo-router/entry.bundle?platform=web&dev=true&hot=false&lazy=true&transform.engine=hermes&transform.routerRoot=__e2e__%2Fworker%2Fapp&unstable_transformProfile=hermes-stable
-// /apps/router-e2e/__e2e__/worker/worker-one.map?platform=web&dev=true&hot=false&lazy=true&transform.engine=hermes&transform.routerRoot=__e2e__%2Fworker%2Fapp&unstable_transformProfile=hermes-stable&modulesOnly=false&runModule=true
