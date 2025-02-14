b021affdb7b98e95e3f15eca190fc64607aad17f
Albus <jjjj-design@gmx.de>
2025-02-14 16:41:52 +0100

    [expo-ui] Adds a discloure group

---
+++ packages/expo-ui/ios/DisclosureGroupView.swift
@@ -0,0 +1,32 @@
+// Copyright 2025-present 650 Industries. All rights reserved.
+
+import SwiftUI
+import ExpoModulesCore
+
+class DisclosureGroupProps: ExpoSwiftUI.ViewProps {
+  @Field var title: String?
+  var onStateChange = EventDispatcher()
+}
+
+struct DisclosureGroupView: ExpoSwiftUI.View {
+    @EnvironmentObject var props: DisclosureGroupProps
+    @State private var isExpanded: Bool = false
+  var body: some View {
+
+      DisclosureGroup(props.title ?? "", isExpanded: $isExpanded) {
+                 
+        Children()
+           
+           .onChange(of: isExpanded) { newValue in
+               let payload = ["isExpnaded": newValue == true ? true : false]
+               props.onStateChange(payload)
+
+              }
+          
+      }
+    
+
+    
+  }
+}
+
