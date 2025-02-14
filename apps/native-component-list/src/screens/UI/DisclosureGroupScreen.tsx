88f32874ef4fcc5fa4d90bab9e311d6068c5e494
Albus <jjjj-design@gmx.de>
2025-02-14 17:01:05 +0100

    [expo-ui] a few changes

---
+++ apps/native-component-list/src/screens/UI/DisclosureGroupScreen.tsx
@@ -0,0 +1,26 @@
+
+
+
+
+import { DisclosureGroup } from '@expo/ui/components/DisclosureGroup';
+import { Section } from '@expo/ui/components/Section';
+import * as React from 'react';
+import {  Text } from 'react-native';
+
+export default function DisclosureGroupScreen() {
+  return (
+    <>
+<Section title=''>
+    <DisclosureGroup title='Click me!' style={{flex: 1}}>
+      <Text>Hello, how are you?</Text>
+      </DisclosureGroup>
+  </Section>
+
+
+</>
+  );
+}
+
+DisclosureGroupScreen.navigationOptions = {
+  title: 'DisclosureGroup',
+};
