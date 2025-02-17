4740a3a280c70defa16d1ae8910a5e04d17bd63b
Alan Hughes <hughesno1@gmail.com>
2025-02-17 09:22:04 +0000

    use model provider

--- packages/expo-image/android/src/main/java/expo/modules/image/ImageLoadTask.kt
+++ packages/expo-image/android/src/main/java/expo/modules/image/ImageLoadTask.kt
@@ -1,16 +1,12 @@
 package expo.modules.image
 
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.load.model.LazyHeaders
 import expo.modules.image.records.ImageLoadOptions
 import expo.modules.image.records.SourceMap
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.exception.Exceptions
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlin.collections.component1
-import kotlin.collections.component2
 
 open class ImageLoadTask(
   private val appContext: AppContext,
@@ -21,20 +17,15 @@ open class ImageLoadTask(
     val context =
       this@ImageLoadTask.appContext.reactContext ?: throw Exceptions.ReactContextLost()
 
-    val headers = source.headers?.let {
-      LazyHeaders.Builder().apply {
-        it.forEach { (key, value) ->
-          addHeader(key, value)
-        }
-      }.build()
-    }
+    val sourceToLoad = source.createGlideModelProvider(context)
+    val model = sourceToLoad?.getGlideModel()
 
     try {
       val bitmap = withContext(Dispatchers.IO) {
         Glide
           .with(context)
           .asDrawable()
-          .load(headers?.let { GlideUrl(source.uri, headers) } ?: source.uri)
+          .load(model)
           .centerInside()
           .submit(options.maxWidth, options.maxHeight)
           .get()
