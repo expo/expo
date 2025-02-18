73f53fa5ac8339508797ac8888bca100468482fa
Henry Allen <hmallen99@gmail.com>
2025-02-18 17:19:26 -0500

    Add test for webgl extensions

--- apps/test-suite/tests/GLView.js
+++ apps/test-suite/tests/GLView.js
@@ -52,6 +52,32 @@ export async function test(
       ).toBe(true);
     });
 
+    it('gets WebGL extensions', async () => {
+      const context = await getContextAsync();
+      expect(
+        context instanceof WebGLRenderingContext || context instanceof WebGL2RenderingContext
+      ).toBe(true);
+
+      const etcExtensions = context.getExtension('WEBGL_compressed_texture_etc');
+
+      if (context instanceof WebGL2RenderingContext) {
+        expect(etcExtensions).toEqual({
+          COMPRESSED_R11_EAC: 37488,
+          COMPRESSED_SIGNED_R11_EAC: 37489,
+          COMPRESSED_RG11_EAC: 37490,
+          COMPRESSED_SIGNED_RG11_EAC: 37491,
+          COMPRESSED_RGB8_ETC2: 37492,
+          COMPRESSED_SRGB8_ETC2: 37493,
+          COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2: 37494,
+          COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2: 37495,
+          COMPRESSED_RGBA8_ETC2_EAC: 37496,
+          COMPRESSED_SRGB8_ALPHA8_ETC2_EAC: 37497,
+        });
+      } else {
+        expect(etcExtensions).toEqual(null);
+      }
+    });
+
     it('takes a snapshot', async () => {
       await getContextAsync();
 
