24d6d0497858c57ed186c6803ea83fb211ad66a8
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-02-19 12:34:31 +0100

    fix lint

--- apps/native-component-list/src/screens/UI/UIScreen.tsx
+++ apps/native-component-list/src/screens/UI/UIScreen.tsx
@@ -1,4 +1,5 @@
 import { Platform } from 'react-native';
+
 import { optionalRequire } from '../../navigation/routeBuilder';
 import ComponentListScreen, { ListElement } from '../ComponentListScreen';
 
