2abd18aa38aab735075d2b70ff28651b27acc009
Albus <jjjj-design@gmx.de>
2025-02-19 17:31:04 +0100

    fix SwiftLint

--- packages/expo-ui/ios/SectionView.swift
+++ packages/expo-ui/ios/SectionView.swift
@@ -4,25 +4,25 @@ import ExpoModulesCore
 import SwiftUI
 
 class SectionProps: ExpoSwiftUI.ViewProps {
-    @Field var title: String?
-    @Field var displayTitleUppercase: Bool = true
-    @Field var heightOffset: CGFloat = 0
+  @Field var title: String?
+  @Field var displayTitleUppercase: Bool = true
+  @Field var heightOffset: CGFloat = 0
 }
 
 struct SectionView: ExpoSwiftUI.View {
-    @EnvironmentObject var props: SectionProps
+  @EnvironmentObject var props: SectionProps
 
-    var body: some View {
-        let form = Form {
-            Section(header: Text(props.title ?? "").textCase(props.displayTitleUppercase ? .uppercase : nil)) {
-                Children().padding(EdgeInsets(top: 0, leading: 0, bottom: props.heightOffset, trailing: 0))
-            }
-        }
-
-        if #available(iOS 16.0, tvOS 16.0, *) {
-            form.scrollDisabled(true)
-        } else {
-            form
-        }
+  var body: some View {
+    let form = Form {
+      Section(header: Text(props.title ?? "").textCase(props.displayTitleUppercase ? .uppercase : nil)) {
+        Children().padding(EdgeInsets(top: 0, leading: 0, bottom: props.heightOffset, trailing: 0))
+      }
     }
+
+    if #available(iOS 16.0, tvOS 16.0, *) {
+      form.scrollDisabled(true)
+    } else {
+      form
+    }
+  }
 }
