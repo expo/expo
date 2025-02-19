e7ccc5f4471c509d9a6ed61ea18e94dae4ac426b
Albus <jjjj-design@gmx.de>
2025-02-19 17:41:55 +0100

    fix SwftLint

--- packages/expo-ui/ios/ContentUnavailableView.swift
+++ packages/expo-ui/ios/ContentUnavailableView.swift
@@ -15,23 +15,23 @@ struct ExpoContentUnavailableView: ExpoSwiftUI.View {
   var body: some View {
     if #available(iOS 17.0, *) {
       ContentUnavailableView(props.title ?? "", systemImage: props.systemImage ?? "questionmark.square", description: Text(props.description ?? ""))
-        } else {
-          VStack(spacing: 10) {
-            Image(systemName: props.systemImage ?? "questionmark.square")
-              .font(.system(size: 50))
-              .foregroundColor(.gray)
+    } else {
+      VStack(spacing: 10) {
+        Image(systemName: props.systemImage ?? "questionmark.square")
+          .font(.system(size: 50))
+          .foregroundColor(.gray)
 
-            Text(props.title ?? "")
-              .font(.title2)
-              .fontWeight(.semibold)
-              .multilineTextAlignment(.center)
+        Text(props.title ?? "")
+          .font(.title2)
+          .fontWeight(.semibold)
+          .multilineTextAlignment(.center)
 
-            Text(props.description ?? "")
-              .font(.body)
-              .multilineTextAlignment(.center)
-              .padding(.horizontal, 20)
-            }
-            .padding()
-        }
+        Text(props.description ?? "")
+          .font(.body)
+          .multilineTextAlignment(.center)
+          .padding(.horizontal, 20)
+      }
+      .padding()
     }
+  }
 }
