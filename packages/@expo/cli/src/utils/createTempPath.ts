0d58a5f50cbf3e0225aa2b3ce289b846a7de9a21
Phil Pluckthun <phil@kitten.sh>
2025-02-20 11:54:38 +0000

    chore(cli): Drop temp-dir and unique-string

--- packages/@expo/cli/src/utils/createTempPath.ts
+++ packages/@expo/cli/src/utils/createTempPath.ts
@@ -1,9 +1,13 @@
+import { randomBytes } from 'crypto';
 import fs from 'fs';
+import { tmpdir } from 'os';
 import path from 'path';
-import tempDir from 'temp-dir';
-import uniqueString from 'unique-string';
 
-const uniqueTempPath = (): string => path.join(tempDir, uniqueString());
+const uniqueTempPath = (): string => {
+  const tempDir = fs.realpathSync(tmpdir());
+  const uniqueString = randomBytes(16).toString('hex');
+  return path.join(tempDir, uniqueString);
+};
 
 // Functionally equivalent to: https://github.com/sindresorhus/tempy/blob/943ade0c935367117adbe2b690516ebc94139c6d/index.js#L43-L47
 export function createTempDirectoryPath(): string {
