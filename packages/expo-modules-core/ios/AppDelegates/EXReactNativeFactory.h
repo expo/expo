41ae96e5439ffb12b0a0a6670669ec2e1f12a4ea
Vojtech Novak <vonovak@gmail.com>
2025-02-20 11:28:47 +0100

    fix some more CI checks?

--- packages/expo-modules-core/ios/AppDelegates/EXReactNativeFactory.h
+++ packages/expo-modules-core/ios/AppDelegates/EXReactNativeFactory.h
@@ -1,6 +1,11 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 
-#import "RCTReactNativeFactory.h"
+#if __has_include(<React-RCTAppDelegate/RCTReactNativeFactory.h>)
+#import <React-RCTAppDelegate/RCTReactNativeFactory.h>
+#elif __has_include(<React_RCTAppDelegate/RCTReactNativeFactory.h>)
+// for importing the header from framework, the dash will be transformed to underscore
+#import <React_RCTAppDelegate/RCTReactNativeFactory.h>
+#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
