68f39178ae42091c9a31137ae4b9f829dfe1bf17
Phil Pluckthun <phil@kitten.sh>
2025-02-21 11:35:24 +0000

    chore(expo-modules-autolinking): Rebuild

--- packages/expo-modules-autolinking/build/reactNativeConfig/iosResolver.js
+++ packages/expo-modules-autolinking/build/reactNativeConfig/iosResolver.js
@@ -4,15 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.resolveDependencyConfigImplIosAsync = void 0;
-const fast_glob_1 = __importDefault(require("fast-glob"));
 const promises_1 = __importDefault(require("fs/promises"));
+const glob_1 = require("glob");
 const path_1 = __importDefault(require("path"));
 async function resolveDependencyConfigImplIosAsync(packageRoot, reactNativeConfig) {
     if (reactNativeConfig === null) {
         // Skip autolinking for this package.
         return null;
     }
-    const podspecs = await (0, fast_glob_1.default)('*.podspec', { cwd: packageRoot });
+    const podspecs = await (0, glob_1.glob)('*.podspec', { cwd: packageRoot });
     if (!podspecs?.length) {
         return null;
     }
