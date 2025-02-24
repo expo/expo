66bf24ce2a4ff30ac80fd0e0c963ef2596831700
Albus <jjjj-design@gmx.de>
2025-02-24 19:55:28 +0100

    tsbuilt

--- packages/expo-ui/build/components/Section/index.ios.d.ts
+++ packages/expo-ui/build/components/Section/index.ios.d.ts
@@ -1,5 +1,8 @@
 import { StyleProp, ViewStyle } from 'react-native';
 export type SectionProps = {
+    /**
+     * @note On iOS, section titles are usually capitalized for consistency with platform conventions.
+     */
     title: string;
     style?: StyleProp<ViewStyle>;
     children: React.ReactNode;
