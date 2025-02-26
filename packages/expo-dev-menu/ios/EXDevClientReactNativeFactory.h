8c9d29362d0feedaa22a03068ac2212ad20f0664
Vojtech Novak <vonovak@gmail.com>
2025-02-26 14:17:44 +0100

    fix failing test

--- packages/expo-dev-menu/ios/EXDevClientReactNativeFactory.h
+++ packages/expo-dev-menu/ios/EXDevClientReactNativeFactory.h
@@ -3,14 +3,10 @@
 #ifndef EXDevClientReactNativeFactory_h
 #define EXDevClientReactNativeFactory_h
 
-#if __has_include(<React-RCTAppDelegate/RCTReactNativeFactory.h>)
-#import <React-RCTAppDelegate/RCTReactNativeFactory.h>
-#elif __has_include(<React_RCTAppDelegate/RCTReactNativeFactory.h>)
-// for importing the header from framework, the dash will be transformed to underscore
-#import <React_RCTAppDelegate/RCTReactNativeFactory.h>
-#endif
+#import <ExpoModulesCore/EXReactNativeFactory.h>
 
-@interface EXDevClientReactNativeFactory : RCTReactNativeFactory
+// subclassing RCTReactNativeFactory appears to work in Bare Expo too, but it fails tests
+@interface EXDevClientReactNativeFactory : EXReactNativeFactory
 
 @end
 #endif /* EXDevClientReactNativeFactory_h */
