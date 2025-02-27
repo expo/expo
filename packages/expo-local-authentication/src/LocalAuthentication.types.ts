bdce0fe50e08ae22dc5b3a153c3620e8355260e2
bwentzel-linum <bevan.wentzel@linumlabs.com>
2025-02-27 18:36:11 +0200

    Format list of items correctly
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>

--- packages/expo-local-authentication/src/LocalAuthentication.types.ts
+++ packages/expo-local-authentication/src/LocalAuthentication.types.ts
@@ -5,7 +5,7 @@ export type LocalAuthenticationResult =
   | {
       success: false;
       /**
-       * One of the following values: `not_enrolled` `user_cancel` `app_cancel` `not_available` `lockout` `no_space` `timeout` `unable_to_process` `unknown` `system_cancel` `user_fallback` `invalid_context` `passcode_not_set` `authentication_failed`
+       * One of the following values: `not_enrolled`, `user_cancel`, `app_cancel`, `not_available`, `lockout`, `no_space`, `timeout`, `unable_to_process`, `unknown`, `system_cancel`, `user_fallback`, `invalid_context`, `passcode_not_set`, `authentication_failed`.
        */
       error: string;
       warning?: string;
