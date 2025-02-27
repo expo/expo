ebbf922dab17de52b0efbc490707fac25e6ba553
Kudo Chien <kudo@expo.dev>
2025-02-27 23:04:06 +0800

    [sqlite] add web support

---
+++ packages/expo-sqlite/build/ExpoSQLite.web.js
@@ -0,0 +1,13 @@
+import { requireNativeModule } from 'expo';
+let ExpoSQLite;
+if (typeof window === 'undefined') {
+    throw new Error('expo-sqlite is not supported on server runtime.');
+}
+else if (typeof globalThis.ExpoDomWebView !== 'undefined') {
+    ExpoSQLite = requireNativeModule('ExpoSQLite');
+}
+else {
+    ExpoSQLite = require('../web/SQLiteModule').default;
+}
+export default ExpoSQLite;
+//# sourceMappingURL=ExpoSQLite.web.js.map
