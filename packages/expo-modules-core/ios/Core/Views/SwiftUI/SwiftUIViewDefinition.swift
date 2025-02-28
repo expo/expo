f1bbbeb448ff0d158cb017a781a30c44af343832
Pflaumenbaum <182399538+Pflaumenbaum@users.noreply.github.com>
2025-02-28 10:33:54 +0100

    SwiftLint

--- packages/expo-modules-core/ios/Core/Views/SwiftUI/SwiftUIViewDefinition.swift
+++ packages/expo-modules-core/ios/Core/Views/SwiftUI/SwiftUIViewDefinition.swift
@@ -2,31 +2,37 @@
 
 import SwiftUI
 import Combine
+// Copyright 2024-present 650 Industries. All rights reserved.
 
-/// A protocol for SwiftUI views that need to access props.
+import SwiftUI
+import Combine
+
+/**
+ A protocol for SwiftUI views that need to access props.
+ */
 public protocol ExpoSwiftUIView<Props>: SwiftUI.View, AnyArgument {
   associatedtype Props: ExpoSwiftUI.ViewProps
-  
+
   var props: Props { get }
   static func getDynamicType() -> AnyDynamicType
-  
+
   init()
 }
 
-extension ExpoSwiftUIView {
+public extension ExpoSwiftUIView {
   /**
    Returns React's children as SwiftUI views.
    */
-  public func Children() -> some View {  // swiftlint:disable:this identifier_name
+  func Children() -> some View { // swiftlint:disable:this identifier_name
     ZStack(alignment: .topLeading) {
       ForEach(props.children ?? []) { $0 }
     }
   }
-  
+
   /**
    Returns React's children as SwiftUI views, with any nested HostingViews stripped out.
    */
-  public func UnwrappedChildren<T: View>(  // swiftlint:disable:this identifier_name
+  func UnwrappedChildren<T: View>(  // swiftlint:disable:this identifier_name
     @ViewBuilder transform: @escaping (
       _ child: AnyView, _ isHostingView: Bool
     ) -> T = { child, _ in child }
@@ -38,27 +44,21 @@ extension ExpoSwiftUIView {
     }
     
     let childrenArray = Array(children)
-    
     return ForEach(0..<childrenArray.count, id: \.self) { index in
       let child = childrenArray[index]
-      
       // Wrap if ... else in AnyView to match return type's requirements
       AnyView(
         Group {
-          if let hostingView = child.view
-              as? (any ExpoSwiftUI.AnyHostingView) {
+          if let hostingView = child.view as? (any ExpoSwiftUI.AnyHostingView) {
             // If it's a hosting view, extract content and props to apply the transformation
             let content = hostingView.getContentView()
             let propsObject =
             hostingView.getProps() as any ObservableObject
-            
             transform(
               AnyView(
                 content
                   .environmentObject(propsObject)
-                  .environmentObject(
-                    ExpoSwiftUI.ShadowNodeProxy
-                      .SHADOW_NODE_MOCK_PROXY)
+                  .environmentObject(ExpoSwiftUI.ShadowNodeProxy.SHADOW_NODE_MOCK_PROXY)
               ),
               true
             )
@@ -70,76 +70,65 @@ extension ExpoSwiftUIView {
       )
     }
   }
-  public static func getDynamicType() -> AnyDynamicType {
-    return DynamicSwiftUIViewType(innerType: Self.self)
+  
+  static func getDynamicType() -> AnyDynamicType {
+      return DynamicSwiftUIViewType(innerType: Self.self)
+    }
   }
-}
 
-extension ExpoSwiftUI {
-  public typealias View = ExpoSwiftUIView
-  /**
-   A definition representing the native SwiftUI view to export to React.
-   */
-  public final class ViewDefinition<Props: ViewProps, ViewType: View<Props>>:
-    ExpoModulesCore.ViewDefinition<HostingView<Props, ViewType>>
-  {
-    // To obtain prop and event names from the props object we need to create a dummy instance first.
-    // This is not ideal, but RN requires us to provide all names before the view is created
-    // and there doesn't seem to be a better way to do this right now.
-    private lazy var dummyPropsMirror: Mirror = Mirror(reflecting: Props())
-    
-    init(_ viewType: ViewType.Type) {
-      // We assume SwiftUI views are exported as named views under the class name
-      let nameDefinitionElement = ViewNameDefinition(
-        name: String(describing: viewType))
-      super.init(
-        HostingView<Props, ViewType>.self,
-        elements: [nameDefinitionElement])
-    }
-    
-    init(_ viewType: ViewType.Type, elements: [AnyViewDefinitionElement]) {
-      super.init(HostingView<Props, ViewType>.self, elements: elements)
-    }
-    
-    public override func createView(appContext: AppContext) -> UIView? {
-#if RCT_NEW_ARCH_ENABLED
-      let props = Props()
-      let view = HostingView(
-        viewType: ViewType.self, props: props,
-        appContext: appContext)
-      
-      // Set up events to call view's `dispatchEvent` method.
-      // This is supported only on the new architecture, `dispatchEvent` exists only there.
-      props.setUpEvents(view.dispatchEvent(_:payload:))
-      
-      return view
-#else
-      return UnimplementedExpoView(
-        appContext: appContext,
-        text:
-          "Rendering SwiftUI views is possible only with the New Architecture enabled"
-      )
-#endif
-    }
-    
-    public override func getSupportedPropNames() -> [String] {
-      return dummyPropsMirror.children.compactMap {
-        (label: String?, value: Any) in
-        guard let field = value as? AnyFieldInternal else {
-          return nil
-        }
-        return field.key ?? convertLabelToKey(label)
+  extension ExpoSwiftUI {
+    public typealias View = ExpoSwiftUIView
+
+    /**
+     A definition representing the native SwiftUI view to export to React.
+     */
+    public final class ViewDefinition<Props: ViewProps, ViewType: View<Props>>: ExpoModulesCore.ViewDefinition<HostingView<Props, ViewType>> {
+      // To obtain prop and event names from the props object we need to create a dummy instance first.
+      // This is not ideal, but RN requires us to provide all names before the view is created
+      // and there doesn't seem to be a better way to do this right now.
+      private lazy var dummyPropsMirror: Mirror = Mirror(reflecting: Props())
+
+      init(_ viewType: ViewType.Type) {
+        // We assume SwiftUI views are exported as named views under the class name
+        let nameDefinitionElement = ViewNameDefinition(name: String(describing: viewType))
+        super.init(HostingView<Props, ViewType>.self, elements: [nameDefinitionElement])
       }
-    }
-    
-    public override func getSupportedEventNames() -> [String] {
-      return dummyPropsMirror.children.compactMap {
-        (label: String?, value: Any) in
-        guard let event = value as? EventDispatcher else {
-          return nil
+
+      init(_ viewType: ViewType.Type, elements: [AnyViewDefinitionElement]) {
+        super.init(HostingView<Props, ViewType>.self, elements: elements)
+      }
+
+      public override func createView(appContext: AppContext) -> UIView? {
+  #if RCT_NEW_ARCH_ENABLED
+        let props = Props()
+        let view = HostingView(viewType: ViewType.self, props: props, appContext: appContext)
+
+        // Set up events to call view's `dispatchEvent` method.
+        // This is supported only on the new architecture, `dispatchEvent` exists only there.
+        props.setUpEvents(view.dispatchEvent(_:payload:))
+
+        return view
+  #else
+        return UnimplementedExpoView(appContext: appContext, text: "Rendering SwiftUI views is possible only with the New Architecture enabled")
+  #endif
+      }
+
+      public override func getSupportedPropNames() -> [String] {
+        return dummyPropsMirror.children.compactMap { (label: String?, value: Any) in
+          guard let field = value as? AnyFieldInternal else {
+            return nil
+          }
+          return field.key ?? convertLabelToKey(label)
         }
-        return event.customName ?? convertLabelToKey(label)
+      }
+
+      public override func getSupportedEventNames() -> [String] {
+        return dummyPropsMirror.children.compactMap { (label: String?, value: Any) in
+          guard let event = value as? EventDispatcher else {
+            return nil
+          }
+          return event.customName ?? convertLabelToKey(label)
+        }
       }
     }
   }
-}
