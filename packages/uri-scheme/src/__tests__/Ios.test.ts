f94ceb443ec058ab9dcf4079e0e9d86e4896ca83
Cedric van Putten <github@cedric.dev>
2025-02-28 19:58:01 +0100

    test(uri-scheme): add tests for URI escaping

---
+++ packages/uri-scheme/src/__tests__/Ios.test.ts
@@ -0,0 +1,11 @@
+import { escapeUri } from '../Ios';
+
+describe(escapeUri, () => {
+  it('escapes special charactes in URI search parameter values', () => {
+    expect(escapeUri('myapp://home?test=normal')).toBe('myapp://home?test=normal');
+    expect(escapeUri('myapp://(app)/home?test=@1')).toBe('myapp://(app)/home?test=%25401');
+    expect(escapeUri('myapp://(app)/home?test=a|b|c')).toBe(
+      'myapp://(app)/home?test=a%257Cb%257Cc'
+    );
+  });
+});
