08f75f80cc5ea99837f6ebd2754447aebb416275
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-03-03 11:37:39 +0100

    remove duplicated GL screens

--- apps/native-component-list/src/navigation/ExpoComponentsStackNavigator.tsx
+++ apps/native-component-list/src/navigation/ExpoComponentsStackNavigator.tsx
@@ -11,6 +11,7 @@ import { Layout } from '../constants';
 import { CameraScreens } from '../screens/Camera/CameraScreen';
 import ExpoComponents from '../screens/ExpoComponentsScreen';
 import { MapsScreens } from '../screens/ExpoMaps/MapsScreen';
+import { GLScreens } from '../screens/GL/GLScreen';
 import { ImageScreens } from '../screens/Image/ImageScreen';
 import { UIScreens } from '../screens/UI/UIScreen';
 import { VideoScreens } from '../screens/Video/VideoScreen';
@@ -134,150 +135,6 @@ const ScreensList: ScreenConfig[] = [
     },
     name: 'GL',
     options: { title: 'Examples of GL use' },
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/ClearToBlueScreen'));
-    },
-    name: 'ClearToBlue',
-    options: { title: 'Clear to blue' },
-    route: 'gl/cleartoblue',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/BasicTextureScreen'));
-    },
-    name: 'BasicTexture',
-    options: { title: 'Basic texture use' },
-    route: 'gl/basictexture',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLViewScreen'));
-    },
-    name: 'GLViewScreen',
-    options: { title: 'GLView example' },
-    route: 'gl/glviewscreen',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLMaskScreen'));
-    },
-    name: 'Mask',
-    options: { title: 'MaskedView integration' },
-    route: 'gl/mask',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLSnapshotsScreen'));
-    },
-    name: 'Snapshots',
-    options: { title: 'Taking snapshots' },
-    route: 'gl/snapshots',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLThreeComposerScreen'));
-    },
-    name: 'THREEComposer',
-    options: { title: 'three.js glitch and film effects' },
-    route: 'gl/threecomposer',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLThreeDepthStencilBufferScreen'));
-    },
-    name: 'THREEDepthStencilBuffer',
-    options: { title: 'three.js depth and stencil buffer' },
-    route: 'gl/threedepthstencilbuffer',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLThreeSpriteScreen'));
-    },
-    name: 'THREESprite',
-    options: { title: 'three.js sprite rendering' },
-    route: 'gl/threesprite',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/ProcessingInAndOutScreen'));
-    },
-    name: 'ProcessingInAndOut',
-    options: { title: "'In and out' from openprocessing.org" },
-    route: 'gl/processinginandout',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/ProcessingNoClearScreen'));
-    },
-    name: 'ProcessingNoClear',
-    options: { title: 'Draw without clearing screen with processing.js' },
-    route: 'gl/processingnoclear',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/PIXIBasicScreen'));
-    },
-    name: 'PIXIBasic',
-    options: { title: 'Basic pixi.js use' },
-    route: 'gl/pixibasic',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/PIXISpriteScreen'));
-    },
-    name: 'PIXISprite',
-    options: { title: 'pixi.js sprite rendering' },
-    route: 'gl/pixisprite',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLCameraScreen'));
-    },
-    name: 'GLCamera',
-    options: { title: 'Expo.Camera integration' },
-    route: 'gl/glcamera',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/WebGL2TransformFeedbackScreen'));
-    },
-    name: 'WebGL2TransformFeedback',
-    options: { title: 'WebGL2 - Transform feedback' },
-    route: 'gl/webgl2transformfeedback',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/CanvasScreen'));
-    },
-    name: 'Canvas',
-    options: { title: 'Canvas example - expo-2d-context' },
-    route: 'gl/canvas',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLHeadlessRenderingScreen'));
-    },
-    name: 'HeadlessRendering',
-    options: { title: 'Headless rendering' },
-    route: 'gl/headlessrendering',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLReanimatedExample'));
-    },
-    name: 'ReanimatedWorklets',
-    options: { title: 'Reanimated worklets + gesture handler' },
-    route: 'gl/reanimated',
-  },
-  {
-    getComponent() {
-      return optionalRequire(() => require('../screens/GL/GLViewOnBusyThread'));
-    },
-    name: 'GLViewOnBusyThread',
-    options: { title: 'Creating GLView when a thread is busy' },
-    route: 'gl/busythread',
   },
   {
     getComponent() {
@@ -440,6 +297,7 @@ const ScreensList: ScreenConfig[] = [
 export const Screens: ScreenConfig[] = [
   ...ScreensList,
 
+  ...GLScreens,
   ...CameraScreens,
   ...ImageScreens,
   ...VideoScreens,
