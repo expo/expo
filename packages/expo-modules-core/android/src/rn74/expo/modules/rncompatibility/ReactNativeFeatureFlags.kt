b6be18ee9371a7660665a822b805d76bbd113135
Christian Falch <christian.falch@gmail.com>
2025-03-04 15:37:38 +0100

    [fix][android][tests][updates] fixed complicated solution
    In #34363 a bit of an over-engineered solution was added to fix a failing ErrorRecovery test after we added RN 0.77 (ReactNativeFeatureFlags was implemented a bit differently).
    
    The fix removes the complex solution and adds a much simpler one as suggested by @Kudo.

--- packages/expo-modules-core/android/src/rn74/expo/modules/rncompatibility/ReactNativeFeatureFlags.kt
+++ packages/expo-modules-core/android/src/rn74/expo/modules/rncompatibility/ReactNativeFeatureFlags.kt
@@ -4,15 +4,11 @@ package expo.modules.rncompatibility
 
 import com.facebook.react.config.ReactFeatureFlags
 
-interface IReactNativeFeatureFlagsProvider {
-  val enableBridgelessArchitecture: Boolean
-}
-
 /**
  * A compatibility helper of
  * `com.facebook.react.config.ReactFeatureFlags` and
  * `com.facebook.react.internal.featureflags.ReactNativeFeatureFlags`
  */
-object ReactNativeFeatureFlags : IReactNativeFeatureFlagsProvider {
-  override val enableBridgelessArchitecture = ReactFeatureFlags.enableBridgelessArchitecture
+object ReactNativeFeatureFlags {
+  val enableBridgelessArchitecture = ReactFeatureFlags.enableBridgelessArchitecture
 }
