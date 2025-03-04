102934ed11a71f4766a03151229d5d1836953d0b
Cedric van Putten <me@bycedric.com>
2025-03-04 12:45:37 +0100

    refactor(cli): drop `tempy` as dependency for SDK 53 (#35270)
    # Why
    
    We've removed this dependency in the past in #30832, but added it back
    for `npx expo install expo@next --fix`. Since SDK 52, we don't use this
    library anymore - so it should be safe to remove for SDK 53.
    
    # How
    
    - Dropped `tempy` as a dependency from `@expo/cli`.
    
    # Test Plan
    
    - Hard to test now, but should be straight forward for SDK 53.
    - `bun create expo ./test-upgrade`
    - `cd ./test-upgrade`
    - `bun expo install expo@52 --fix`
    - `bun expo install expo@53 --fix` (or maybe canary version?)
    - Should work fine.
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    
    ---------
    
    Co-authored-by: Phil Pluckthun <phil@kitten.sh>

--- packages/@expo/cli/src/utils/__mocks__/createTempPath.ts
+++ packages/@expo/cli/src/utils/__mocks__/createTempPath.ts
@@ -1,9 +1,9 @@
+import { randomBytes } from 'crypto';
 import os from 'os';
 import path from 'path';
-import uniqueString from 'unique-string';
 
 export function createTempDirectoryPath(): string {
-  return path.join(os.tmpdir(), uniqueString());
+  return path.join(os.tmpdir(), randomBytes(16).toString('hex'));
 }
 
 export function createTempFilePath(name = ''): string {
