f87980725766e4373ef3de3639837d1d8e0b22dd
Emanuel Quimper <quimperemanuel@gmail.com>
2025-03-06 12:59:54 -0500

    Add types

--- packages/expo-ui/build/src/index.d.ts
+++ packages/expo-ui/build/src/index.d.ts
@@ -1,6 +1,7 @@
 export type { Switch, SwitchProps } from '../components/Switch';
 export type { Picker, PickerProps } from '../components/Picker';
 export type { Button, ButtonProps } from '../components/Button';
+export type { List, ListProps } from '../components/List';
 export type { ContextMenu, ContextMenuProps, Submenu, SubmenuProps, ActivationMethod, } from '../components/ContextMenu';
 export type { Section, SectionProps } from '../components/Section';
 export type { Slider, SliderProps } from '../components/Slider';
