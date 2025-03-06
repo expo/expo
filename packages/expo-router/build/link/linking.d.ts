09522dc5b6d8047e78ded55ce2e14f71e8c5d368
Mark Lawlor <mwlawlor@gmail.com>
2025-03-06 17:44:09 +1000

    Fix lint errors

--- packages/expo-router/build/link/linking.d.ts
+++ packages/expo-router/build/link/linking.d.ts
@@ -1,8 +1,8 @@
 import { LinkingOptions } from '@react-navigation/native';
 import { getPathFromState } from '../fork/getPathFromState';
 import { getStateFromPath } from '../fork/getStateFromPath';
-import { NativeIntent } from '../types';
 import { RouterStore } from '../global-state/router-store';
+import { NativeIntent } from '../types';
 export declare function getInitialURL(): ReturnType<NonNullable<LinkingOptions<Record<string, unknown>>['getInitialURL']>>;
 export declare function getRootURL(): string;
 export declare function addEventListener(nativeLinking: NativeIntent | undefined, store: RouterStore): (listener: (url: string) => void) => () => void;
