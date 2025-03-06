d9d3e024d8742099c307754673f17117a20c1dea
Aman Mittal <amandeepmittal@live.com>
2025-03-06 22:55:19 +0530

    [docs] Add EAS Update in an existing native app (#35343)
    # Why
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    @brentvatne shared a draft to convert to a guide on Using EAS Update in
    an existing native app under EAS Update > Reference section in docs.
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    - Add a new page under EAS Update > Reference section called Using EAS
    Update in an existing native app
    - Add a link of this new guide in the introductory paragraph in the
    CodePush reference guide
    - Fix formatting, typos, and grammatical errors when converting the
    draft to the guide in docs
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    See preview:
    -
    http://docs.expo.dev-pr-35343.s3-website-us-east-1.amazonaws.com/eas-update/integration-in-existing-native-apps/
    (New guide)
    -
    http://docs.expo.dev-pr-35343.s3-website-us-east-1.amazonaws.com/eas-update/codepush/
    (CodePush guide link update)
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -505,6 +505,7 @@ export const eas = [
       makePage('eas-update/migrate-from-classic-updates.mdx'),
       makePage('eas-update/trace-update-id-expo-dashboard.mdx'),
       makePage('eas-update/estimate-bandwidth.mdx'),
+      makePage('eas-update/integration-in-existing-native-apps.mdx'),
       makePage('eas-update/faq.mdx'),
     ]),
   ]),
