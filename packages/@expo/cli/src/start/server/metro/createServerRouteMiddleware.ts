bf502d3e85fa15c95b9a46aead81b97c907b78de
Mark Lawlor <mwlawlor@gmail.com>
2025-03-06 15:35:53 +1000

    Fix type error

--- packages/@expo/cli/src/start/server/metro/createServerRouteMiddleware.ts
+++ packages/@expo/cli/src/start/server/metro/createServerRouteMiddleware.ts
@@ -65,6 +65,7 @@ export function createRouteHandlerMiddleware(
             apiRoutes: [],
             notFoundRoutes: [],
             redirects: [],
+            rewrites: [],
           }
         );
       },
