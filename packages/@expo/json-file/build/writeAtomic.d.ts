c44baf53b0af6f9e331fc4a61ddd4739a9bf2e8b
Phil Pluckthun <phil@kitten.sh>
2025-03-10 10:31:51 +0000

    chore(json-file): Rebuild

---
+++ packages/@expo/json-file/build/writeAtomic.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="node" />
+export declare function writeFileAtomicSync(filename: string, data: string | Buffer): void;
+export declare function writeFileAtomic(filename: string, data: string | Buffer): Promise<void>;
