1385c654a4ef1d40d5ed5fed8e305f6101fdd90f
Kudo Chien <kudo@expo.dev>
2025-03-13 01:02:11 +0800

    [sqlite] add android implementation for session extension

---
+++ packages/expo-sqlite/android/src/main/java/expo/modules/sqlite/NativeSession.kt
@@ -0,0 +1,16 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+package expo.modules.sqlite
+
+import expo.modules.kotlin.sharedobjects.SharedRef
+
+internal class NativeSession : SharedRef<NativeSessionBinding>(NativeSessionBinding()) {
+  override fun sharedObjectDidRelease() {
+    super.sharedObjectDidRelease()
+    this.ref.close()
+  }
+
+  override fun equals(other: Any?): Boolean {
+    return other is NativeSession && this.ref == other.ref
+  }
+}
