24d6322b283ca518db91ebe5da70f49137bd7e9c
Cedric van Putten <github@cedric.dev>
2025-03-18 18:27:15 +0100

    test(cli): add `expo export --platform=web --dev` regression test

--- packages/@expo/cli/e2e/__tests__/export.test.ts
+++ packages/@expo/cli/e2e/__tests__/export.test.ts
@@ -70,6 +70,12 @@ describe('server', () => {
 
   beforeAll(async () => {
     projectRoot = await setupTestProjectWithOptionsAsync('basic-export', 'with-assets');
+
+    // TODO(cedric): Remove this once we publish `@expo/metro-config` with `export --dev` fixes
+    // Or when we can build `@expo/metro-config` on Windows
+    const srcMetroConfig = path.resolve(__dirname, '../../../metro-config/build');
+    const destMetroConfig = path.join(projectRoot, 'node_modules/@expo/metro-config/build');
+    await fs.cp(srcMetroConfig, destMetroConfig, { recursive: true, force: true });
   });
 
   it('runs `npx expo export`', async () => {
@@ -290,5 +296,27 @@ describe('server', () => {
     // log and diff the `bundle` with the a previous version from a branch
     // where this passes.
     expect(bundle.split('\n').length).toBeLessThan(700);
+  });
+
+  // Regression test for: https://github.com/expo/expo/issues/35471
+  it('runs `npx expo export --platform=web --dev`', async () => {
+    await executeExpoAsync(
+      projectRoot,
+      ['export', '--source-maps', '--dump-assetmap', '--platform=web', '--dev'],
+      {
+        verbose: true,
+        env: {
+          NODE_ENV: 'production',
+          TEST_BABEL_PRESET_EXPO_MODULE_ID: require.resolve('babel-preset-expo'),
+          EXPO_USE_FAST_RESOLVER: 'true',
+        },
+      }
+    );
+
+    // Ensure the app entry has the expected export name
+    expect(findProjectFiles(path.join(projectRoot, 'dist'))).toEqual([
+      expect.stringMatching(/_expo\/static\/js\/ios\/AppEntry-[\w\d]+\.js/),
+      expect.stringMatching(/_expo\/static\/js\/ios\/AppEntry-[\w\d]+\.js\.map/),
+    ]);
   });
 });
