0fd87be60aab209a9a73da6bcddf90e6f740e9e4
Douglas Lowder <douglowder@mac.com>
2025-03-18 11:12:09 -0700

    [expo-notifications][iOS] Swift conversion 7: Handler and Emitter

---
+++ packages/expo-notifications/ios/EXNotifications/Notifications/Emitter/EmitterModule.swift
@@ -0,0 +1,48 @@
+//  Copyright © 2024 650 Industries. All rights reserved.
+
+import ExpoModulesCore
+import UIKit
+import MachO
+
+let onDidReceiveNotification = "onDidReceiveNotification"
+let onDidReceiveNotificationResponse = "onDidReceiveNotificationResponse"
+let onDidClearNotificationResponse = "onDidClearNotificationResponse"
+
+public class EmitterModule: Module, NotificationDelegate {
+  var presentedNotifications: Set<String> = []
+
+  public func definition() -> ModuleDefinition {
+    Name("ExpoNotificationsEmitter")
+
+    Events([onDidReceiveNotification, onDidReceiveNotificationResponse, onDidClearNotificationResponse])
+
+    OnStartObserving {
+      NotificationCenterManager.shared.addDelegate(self)
+    }
+
+    OnStopObserving {
+      NotificationCenterManager.shared.removeDelegate(self)
+    }
+  }
+
+  public func didReceiveNotification(_ notification: UNNotification, completionHandler: @escaping (UIBackgroundFetchResult) -> Void) -> Bool {
+    completionHandler(.noData)
+    return true
+  }
+
+  public func didReceiveResponse(_ response: UNNotificationResponse, completionHandler: @escaping () -> Void) -> Bool {
+    NotificationCenterManager.shared.lastResponse = response
+    // TODO: convert serialization to Records
+    let serializedResponse = EXNotificationSerializer.serializedNotificationResponse(response)
+    self.sendEvent(onDidReceiveNotificationResponse, serializedResponse as [String: Any])
+    return true
+  }
+
+  public func willPresent(_ notification: UNNotification, completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) -> Bool {
+    // TODO: convert serialization to Records
+    let serializedNotification = EXNotificationSerializer.serializedNotification(notification)
+    self.sendEvent(onDidReceiveNotification, serializedNotification as [String: Any])
+    completionHandler([.badge, .banner, .sound])
+    return true
+  }
+}
---
+++ packages/expo-notifications/ios/EXNotifications/Notifications/Handler/SingleNotificationHandlerTask.swift
@@ -0,0 +1,86 @@
+//  Copyright © 2024 650 Industries. All rights reserved.
+
+import ExpoModulesCore
+import UIKit
+
+let shouldShowAlertKey = "shouldShowAlert"
+let shouldPlaySoundKey = "shouldPlaySound"
+let shouldSetBadgeKey = "shouldSetBadge"
+
+public protocol SingleNotificationHandlerTaskDelegate {
+  func taskDidFinish(_ task: SingleNotificationHandlerTask)
+  func handleNotification(_ notification: UNNotification)
+  func handleNotificationTimeout(_ notification: UNNotification)
+}
+
+public class SingleNotificationHandlerTask {
+  public let identifier: String
+
+  private let notification: UNNotification
+  private var completionHandler: ((UNNotificationPresentationOptions) -> Void)?
+  private let delegate: SingleNotificationHandlerTaskDelegate
+
+  private var timer: Timer?
+
+  public init(
+    notification: UNNotification,
+    completionHandler: @escaping (UNNotificationPresentationOptions) -> Void,
+    delegate: SingleNotificationHandlerTaskDelegate
+  ) {
+    self.identifier = notification.request.identifier
+    self.delegate = delegate
+    self.notification = notification
+    self.completionHandler = completionHandler
+  }
+
+  public func start() {
+    delegate.handleNotification(notification)
+    timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(timeout), userInfo: nil, repeats: false)
+  }
+
+  @objc
+  public func timeout() {
+    delegate.handleNotificationTimeout(notification)
+    finish()
+  }
+
+  public func handleResponse(_ behavior: [String: Any]) -> Bool {
+    if let completionHandler = completionHandler {
+      completionHandler(presentationOptions(behavior))
+      finish()
+      return true
+    } else {
+      finish()
+      return false
+    }
+  }
+
+  public func finish() {
+    timer?.invalidate()
+    self.completionHandler = nil
+    delegate.taskDidFinish(self)
+  }
+
+
+  func presentationOptions(_ behavior: [String: Any]) -> UNNotificationPresentationOptions {
+    var options: UNNotificationPresentationOptions = []
+
+    /*
+    if let shouldShowAlert = behavior[shouldShowAlertKey] as? Bool, shouldShowAlert {
+      options.insert(.alert)
+    }
+     */
+
+    if let shouldPlaySound = behavior[shouldPlaySoundKey] as? Bool, shouldPlaySound {
+      options.insert(.sound)
+    }
+
+    if let shouldSetBadge = behavior[shouldSetBadgeKey] as? Bool, shouldSetBadge {
+      options.insert(.badge)
+    }
+
+    // TODO(iOS 14): use UNNotificationPresentationOptionList and UNNotificationPresentationOptionBanner
+
+    return options
+  }
+}
