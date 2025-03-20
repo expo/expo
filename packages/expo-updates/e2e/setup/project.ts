30c010080b691b0251517768ab6186250de00224
Douglas Lowder <douglowder@mac.com>
2025-03-20 09:13:32 -0700

    Revert new arch change

--- packages/expo-updates/e2e/setup/project.ts
+++ packages/expo-updates/e2e/setup/project.ts
@@ -495,7 +495,7 @@ function transformAppJsonForE2E(
       owner: 'expo-ci',
       runtimeVersion,
       plugins,
-      newArchEnabled: true,
+      newArchEnabled: false,
       android: { ...appJson.expo.android, package: 'dev.expo.updatese2e' },
       ios: { ...appJson.expo.ios, bundleIdentifier: 'dev.expo.updatese2e' },
       updates: {
