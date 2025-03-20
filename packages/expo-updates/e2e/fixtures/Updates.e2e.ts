65a66e67a8dcb02fdd135e820746320c003e7325
Douglas Lowder <douglowder@mac.com>
2025-03-20 14:56:59 -0700

    Revert "Disable reload test for Android"
    This reverts commit 97f6f48fe6942e0d468970fe80a9c88122e3402f.

--- packages/expo-updates/e2e/fixtures/Updates.e2e.ts
+++ packages/expo-updates/e2e/fixtures/Updates.e2e.ts
@@ -113,10 +113,6 @@ describe('Basic tests', () => {
   });
 
   it('reloads', async () => {
-    if (platform === 'android') {
-      console.warn('Reload not currently working on Android with RN 0.79');
-      return;
-    }
     Server.start(Update.serverPort, protocolVersion);
     await device.installApp();
     await device.launchApp({
