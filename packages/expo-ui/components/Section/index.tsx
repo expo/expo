047654d13eae0e35dc197b875e05f530785aa307
Pflaumenbaum <182399538+Pflaumenbaum@users.noreply.github.com>
2025-03-22 22:47:26 +0100

    rebase index.tsx

--- packages/expo-ui/components/Section/index.tsx
+++ packages/expo-ui/components/Section/index.tsx
@@ -1,17 +1,12 @@
 import { StyleProp, ViewStyle } from 'react-native';
 
 export type SectionProps = {
- /**
-  * @note On iOS, section titles are usually capitalized for consistency with platform conventions.
-  */
-  title: string;
-  style?: StyleProp<ViewStyle>;
   /**
    * @note On iOS, section titles are usually capitalized for consistency with platform conventions.
    */
   title?: string;
   children: any;
-  children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
 };
 
 /**
