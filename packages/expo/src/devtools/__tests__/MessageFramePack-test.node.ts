expo/expo

commit 6867a389ecb2c7c48dae4585dd3a2ac3ab46f32e

[expo] add useTransportationNext for devtools plugin client to support binary payload (#30935)
# Why

add `useTransportationNext` option to support binary payload for devtools plugins

# How

- i want to test it on sdk 51 for livestore integration, so to introduce the `useTransportationNext` as feature flag.
- use the `/expo-dev-plugins/broadcast` websocket endpoint that don't serialize/deserialize payload as JSON
- add `MessageFramePacker` class to support JSON + binary payload

packages/expo/build/devtools/DevToolsPluginClient.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"DevToolsPluginClient.d.ts","sourceRoot":"","sources":["../../src/devtools/DevToolsPluginClient.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,YAAY,EAAE,iBAAiB,EAAE,MAAM,WAAW,CAAC;AAE5D,OAAO,EAAE,qBAAqB,EAAE,MAAM,yBAAyB,CAAC;AAEhE,OAAO,KAAK,EAAE,cAAc,EAAE,2BAA2B,EAAE,MAAM,kBAAkB,CAAC;AAIpF,eAAO,MAAM,wBAAwB,IAAI,CAAC;AAE1C,eAAO,MAAM,oBAAoB,wBAAwB,CAAC;AAE1D;;;GAGG;AACH,8BAAsB,oBAAoB;aAUtB,cAAc,EAAE,cAAc;IAC9C,OAAO,CAAC,QAAQ,CAAC,OAAO,CAAC;IAV3B,SAAS,CAAC,YAAY,EAAE,YAAY,CAAsB;IAE1D,OAAO,CAAC,MAAM,CAAC,cAAc,CAAsD;IACnF,OAAO,CAAC,QAAQ,CAAC,OAAO,CAA8D;IAEtF,SAAS,CAAC,QAAQ,UAAS;IAC3B,SAAS,CAAC,OAAO,SAAK;gBAGJ,cAAc,EAAE,cAAc,EAC7B,OAAO,CAAC,yCAA6B;IAKxD;;;OAGG;IACU,SAAS,IAAI,OAAO,CAAC,IAAI,CAAC;IAQvC;;OAEG;IACU,UAAU,IAAI,OAAO,CAAC,IAAI,CAAC;IAWxC;;;;OAIG;IACI,WAAW,CAAC,MAAM,EAAE,MAAM,EAAE,MAAM,EAAE,GAAG;IAkB9C;;;;OAIG;IACI,kBAAkB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,iBAAiB;IAI7F;;;;OAIG;IACI,sBAAsB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,IAAI;IAIpF;;OAEG;IACI,WAAW,IAAI,OAAO;IAI7B;;OAEG;IACH,SAAS,CAAC,YAAY,IAAI,OAAO,CAAC,SAAS,CAAC;IAwB5C,SAAS,CAAC,aAAa,UAAW,qBAAqB,KAAG,IAAI,CAiB5D;IAEF;;;OAGG;IACI,wBAAwB,IAAI,qBAAqB;CAGzD"}
+{"version":3,"file":"DevToolsPluginClient.d.ts","sourceRoot":"","sources":["../../src/devtools/DevToolsPluginClient.ts"],"names":[],"mappings":"AAAA,OAAO,EAAE,YAAY,EAAE,iBAAiB,EAAE,MAAM,WAAW,CAAC;AAG5D,OAAO,EAAE,qBAAqB,EAAE,MAAM,yBAAyB,CAAC;AAGhE,OAAO,KAAK,EAAE,cAAc,EAAE,2BAA2B,EAAE,MAAM,kBAAkB,CAAC;AAIpF,eAAO,MAAM,wBAAwB,IAAI,CAAC;AAE1C,eAAO,MAAM,oBAAoB,wBAAwB,CAAC;AAO1D;;;GAGG;AACH,8BAAsB,oBAAoB;aAYtB,cAAc,EAAE,cAAc;IAC9C,OAAO,CAAC,QAAQ,CAAC,OAAO,CAAC;IAZ3B,SAAS,CAAC,YAAY,EAAE,YAAY,CAAsB;IAE1D,OAAO,CAAC,MAAM,CAAC,cAAc,CAAsD;IACnF,OAAO,CAAC,QAAQ,CAAC,OAAO,CAA8D;IAEtF,SAAS,CAAC,QAAQ,UAAS;IAC3B,SAAS,CAAC,OAAO,SAAK;IACtB,OAAO,CAAC,QAAQ,CAAC,qBAAqB,CAAU;IAChD,OAAO,CAAC,QAAQ,CAAC,kBAAkB,CAA0D;gBAG3E,cAAc,EAAE,cAAc,EAC7B,OAAO,CAAC,yCAA6B;IAOxD;;;OAGG;IACU,SAAS,IAAI,OAAO,CAAC,IAAI,CAAC;IAQvC;;OAEG;IACU,UAAU,IAAI,OAAO,CAAC,IAAI,CAAC;IAWxC;;;;OAIG;IACI,WAAW,CAAC,MAAM,EAAE,MAAM,EAAE,MAAM,EAAE,GAAG;IAY9C,OAAO,CAAC,qBAAqB;YAaf,iCAAiC;IAa/C;;;;OAIG;IACI,kBAAkB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,iBAAiB;IAI7F;;;;OAIG;IACI,sBAAsB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,IAAI;IAIpF;;OAEG;IACI,WAAW,IAAI,OAAO;IAI7B;;OAEG;IACH,SAAS,CAAC,YAAY,IAAI,OAAO,CAAC,SAAS,CAAC;IAyB5C,SAAS,CAAC,aAAa,UAAW,qBAAqB,UAMrD;IAEF,OAAO,CAAC,uBAAuB,CAiB7B;IAEF,OAAO,CAAC,mCAAmC,CAmBzC;IAEF;;;OAGG;IACI,wBAAwB,IAAI,qBAAqB;CAGzD"}

commit 2e4d15ffb1b24d892523be50b02b684a25419bbf

[expo] improve devtools plugin performance (#35581)
# Why

fixes performance regression on devtools plugins from #30935 and #31852
fixes https://github.com/expo/dev-plugins/issues/60

# How

i was thinking that binary format is good enough for all data type. string/number can all be represented as binary. however, that is not quite true on react-native world.
- `TextEncoder.encode` has some cost on [hermes](https://github.com/facebook/hermes/blob/f68129e40766975213d3b6ff252cbe2637a10b43/lib/VM/JSLib/TextEncoder.cpp#L131-L199)
- `JSON.stringify` and `JSON.parse` have some optimization on js engines.
- react-native WebSocket will actually convert [Uint8Array to base64](https://github.com/facebook/react-native/blob/81e47af7648c489e29982221552086db4a807b8d/packages/react-native/Libraries/WebSocket/WebSocket_new.js#L202). that would be much slower. i think that is the major root cause.

based on these observations. this pr tries to do some optimizations for devtools plugins transport:
- [expo] for null, undefined, number, string, and object, we will just use `JSON.stringify` / `JSON.parse` to serialize/deserialize. we only do slower custom packing for binary formats.
- [expo] the other optimization is to remove unnecessary async/await. this is needed only for `Blob`.
- [cli] fix `DevToolsPluginWebsocketEndpoint` that don't support websocket text. the received `message` is Buffer, it will send out with binary opcode. we should pass `isBinary` flag to support text correctly.

packages/@expo/cli/src/start/server/metro/DevToolsPluginWebsocketEndpoint.ts
@@ -4,11 +4,11 @@ export function createDevToolsPluginWebsocketEndpoint(): Record<string, WebSocke
   const wss = new WebSocketServer({ noServer: true });
 
   wss.on('connection', (ws: WebSocket) => {
-    ws.on('message', (message: string) => {
+    ws.on('message', (message, isBinary) => {
       // Broadcast the received message to all other connected clients
       wss.clients.forEach((client) => {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(message);
+          client.send(message, { binary: isBinary });
         }
       });
     });

packages/expo/build/devtools/DevToolsPluginClient.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"DevToolsPluginClient.d.ts","sourceRoot":"","sources":["../../src/devtools/DevToolsPluginClient.ts"],"names":[],"mappings":";AACA,OAAO,EAAE,qBAAqB,EAAE,MAAM,yBAAyB,CAAC;AAEhE,OAAO,KAAK,EAAE,cAAc,EAAE,2BAA2B,EAAE,MAAM,kBAAkB,CAAC;AASpF,MAAM,WAAW,iBAAiB;IAChC,MAAM,IAAI,IAAI,CAAC;CAChB;AAED;;;GAGG;AACH,8BAAsB,oBAAoB;aAYtB,cAAc,EAAE,cAAc;IAC9C,OAAO,CAAC,QAAQ,CAAC,OAAO,CAAC;IAZ3B,OAAO,CAAC,SAAS,CAAyD;IAE1E,OAAO,CAAC,MAAM,CAAC,cAAc,CAAsD;IACnF,OAAO,CAAC,QAAQ,CAAC,OAAO,CAA8D;IAEtF,SAAS,CAAC,QAAQ,UAAS;IAC3B,SAAS,CAAC,OAAO,SAAK;IACtB,OAAO,CAAC,QAAQ,CAAC,kBAAkB,CACR;gBAGT,cAAc,EAAE,cAAc,EAC7B,OAAO,CAAC,yCAA6B;IAMxD;;;OAGG;IACU,SAAS,IAAI,OAAO,CAAC,IAAI,CAAC;IAQvC;;OAEG;IACU,UAAU,IAAI,OAAO,CAAC,IAAI,CAAC;IAWxC;;;;OAIG;IACI,WAAW,CAAC,MAAM,EAAE,MAAM,EAAE,MAAM,EAAE,GAAG;YAQhC,eAAe;IAS7B;;;;OAIG;IACI,kBAAkB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,iBAAiB;IAU7F;;;;OAIG;IACI,sBAAsB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,IAAI;IAQpF;;OAEG;IACI,WAAW,IAAI,OAAO;IAI7B;;OAEG;IACH,SAAS,CAAC,YAAY,IAAI,OAAO,CAAC,SAAS,CAAC;IAyB5C,SAAS,CAAC,aAAa,UAAW,qBAAqB,UAErD;IAEF,OAAO,CAAC,iBAAiB,CAuBvB;IAEF;;;OAGG;IACI,wBAAwB,IAAI,qBAAqB;CAGzD"}
+{"version":3,"file":"DevToolsPluginClient.d.ts","sourceRoot":"","sources":["../../src/devtools/DevToolsPluginClient.ts"],"names":[],"mappings":";AACA,OAAO,EAAE,qBAAqB,EAAE,MAAM,yBAAyB,CAAC;AAEhE,OAAO,KAAK,EAAE,cAAc,EAAE,2BAA2B,EAAE,MAAM,kBAAkB,CAAC;AASpF,MAAM,WAAW,iBAAiB;IAChC,MAAM,IAAI,IAAI,CAAC;CAChB;AAED;;;GAGG;AACH,8BAAsB,oBAAoB;aAYtB,cAAc,EAAE,cAAc;IAC9C,OAAO,CAAC,QAAQ,CAAC,OAAO,CAAC;IAZ3B,OAAO,CAAC,SAAS,CAAyD;IAE1E,OAAO,CAAC,MAAM,CAAC,cAAc,CAAsD;IACnF,OAAO,CAAC,QAAQ,CAAC,OAAO,CAA8D;IAEtF,SAAS,CAAC,QAAQ,UAAS;IAC3B,SAAS,CAAC,OAAO,SAAK;IACtB,OAAO,CAAC,QAAQ,CAAC,kBAAkB,CACR;gBAGT,cAAc,EAAE,cAAc,EAC7B,OAAO,CAAC,yCAA6B;IAMxD;;;OAGG;IACU,SAAS,IAAI,OAAO,CAAC,IAAI,CAAC;IAQvC;;OAEG;IACU,UAAU,IAAI,OAAO,CAAC,IAAI,CAAC;IAWxC;;;;OAIG;IACI,WAAW,CAAC,MAAM,EAAE,MAAM,EAAE,MAAM,EAAE,GAAG;IAQ9C,OAAO,CAAC,eAAe;IAevB;;;;OAIG;IACI,kBAAkB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,iBAAiB;IAU7F;;;;OAIG;IACI,sBAAsB,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,EAAE,CAAC,MAAM,EAAE,GAAG,KAAK,IAAI,GAAG,IAAI;IAQpF;;OAEG;IACI,WAAW,IAAI,OAAO;IAI7B;;OAEG;IACH,SAAS,CAAC,YAAY,IAAI,OAAO,CAAC,SAAS,CAAC;IAyB5C,SAAS,CAAC,aAAa,UAAW,qBAAqB,UAErD;IAEF,OAAO,CAAC,iBAAiB,CAyBvB;IAEF;;;OAGG;IACI,wBAAwB,IAAI,qBAAqB;CAGzD"}

packages/expo/build/devtools/MessageFramePacker.d.ts
@@ -1,25 +1,33 @@
 /**
- * A message frame packer that serializes a messageKey and a payload into a binary format.
+ * A message frame packer that serializes a messageKey and a payload into either a JSON string
+ * (fast path) or a binary format (for complex payloads).
  *
- * +------------------+-------------------+----------------------------+--------------------------+
- * | 4 bytes (Uint32) | Variable length   | 1 byte (Uint8)             | Variable length          |
- * | MessageKeyLength | MessageKey (JSON) | PayloadTypeIndicator (enum)| Payload (binary data)    |
- * +------------------+-------------------+----------------------------+--------------------------+
+ * Fast Path (JSON.stringify/JSON.parse):
+ * - For simple payloads (e.g., strings, numbers, null, undefined, or plain objects), the packer
+ *   uses `JSON.stringify` for serialization and `JSON.parse` for deserialization, ensuring
+ *   optimal performance.
  *
- * MessageFrame Format:
+ * Binary Format:
+ * - For more complex payloads (e.g., Uint8Array, ArrayBuffer, Blob), the packer uses a binary
+ *   format with the following structure:
  *
- * 1. MessageKeyLength (4 bytes):
- *    - A 4-byte unsigned integer indicating the length of the MessageKey JSON string.
+ *   +------------------+-------------------+----------------------------+--------------------------+
+ *   | 4 bytes (Uint32) | Variable length   | 1 byte (Uint8)             | Variable length          |
+ *   | MessageKeyLength | MessageKey (JSON) | PayloadTypeIndicator (enum)| Payload (binary data)    |
+ *   +------------------+-------------------+----------------------------+--------------------------+
  *
- * 2. MessageKey (Variable length):
- *    - The JSON string representing the message key, encoded as UTF-8.
+ *   1. MessageKeyLength (4 bytes):
+ *      - A 4-byte unsigned integer indicating the length of the MessageKey JSON string.
  *
- * 3. PayloadTypeIndicator (1 byte):
- *    - A single byte enum value representing the type of the payload (e.g., Uint8Array, String, Object, ArrayBuffer, Blob).
+ *   2. MessageKey (Variable length):
+ *      - The JSON string representing the message key, encoded as UTF-8.
  *
- * 4. Payload (Variable length):
- *    - The actual payload data, which can vary in type and length depending on the PayloadType.
+ *   3. PayloadTypeIndicator (1 byte):
+ *      - A single byte enum value representing the type of the payload (e.g., Uint8Array, String,
+ *        Object, ArrayBuffer, Blob).
  *
+ *   4. Payload (Variable length):
+ *      - The actual payload data, which can vary in type and length depending on the PayloadType.
  */
 type MessageKeyTypeBase = string | object;
 type PayloadType = Uint8Array | string | number | null | undefined | object | ArrayBuffer | Blob;
@@ -30,9 +38,11 @@ interface MessageFrame<T extends MessageKeyTypeBase> {
 export declare class MessageFramePacker<T extends MessageKeyTypeBase> {
     private textEncoder;
     private textDecoder;
-    pack({ messageKey, payload }: MessageFrame<T>): Promise<Uint8Array>;
-    unpack(packedData: ArrayBuffer): Promise<MessageFrame<T>>;
+    pack({ messageKey, payload }: MessageFrame<T>): string | Uint8Array | Promise<Uint8Array>;
+    unpack(packedData: string | ArrayBuffer): MessageFrame<T>;
+    private isFastPathPayload;
     private payloadToUint8Array;
+    private packImpl;
     private deserializePayload;
     private static getPayloadTypeIndicator;
 }

packages/expo/build/devtools/MessageFramePacker.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"MessageFramePacker.d.ts","sourceRoot":"","sources":["../../src/devtools/MessageFramePacker.ts"],"names":[],"mappings":"AAAA;;;;;;;;;;;;;;;;;;;;;;GAsBG;AAIH,KAAK,kBAAkB,GAAG,MAAM,GAAG,MAAM,CAAC;AAC1C,KAAK,WAAW,GAAG,UAAU,GAAG,MAAM,GAAG,MAAM,GAAG,IAAI,GAAG,SAAS,GAAG,MAAM,GAAG,WAAW,GAAG,IAAI,CAAC;AAajG,UAAU,YAAY,CAAC,CAAC,SAAS,kBAAkB;IACjD,UAAU,EAAE,CAAC,CAAC;IACd,OAAO,CAAC,EAAE,WAAW,CAAC;CACvB;AAED,qBAAa,kBAAkB,CAAC,CAAC,SAAS,kBAAkB;IAC1D,OAAO,CAAC,WAAW,CAAqB;IACxC,OAAO,CAAC,WAAW,CAAqB;IAE3B,IAAI,CAAC,EAAE,UAAU,EAAE,OAAO,EAAE,EAAE,YAAY,CAAC,CAAC,CAAC,GAAG,OAAO,CAAC,UAAU,CAAC;IA2BnE,MAAM,CAAC,UAAU,EAAE,WAAW,GAAG,OAAO,CAAC,YAAY,CAAC,CAAC,CAAC,CAAC;YAqBxD,mBAAmB;YAuBnB,kBAAkB;IAoChC,OAAO,CAAC,MAAM,CAAC,uBAAuB;CAqBvC"}
+{"version":3,"file":"MessageFramePacker.d.ts","sourceRoot":"","sources":["../../src/devtools/MessageFramePacker.ts"],"names":[],"mappings":"AAAA;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;GA8BG;AAIH,KAAK,kBAAkB,GAAG,MAAM,GAAG,MAAM,CAAC;AAC1C,KAAK,WAAW,GAAG,UAAU,GAAG,MAAM,GAAG,MAAM,GAAG,IAAI,GAAG,SAAS,GAAG,MAAM,GAAG,WAAW,GAAG,IAAI,CAAC;AAajG,UAAU,YAAY,CAAC,CAAC,SAAS,kBAAkB;IACjD,UAAU,EAAE,CAAC,CAAC;IACd,OAAO,CAAC,EAAE,WAAW,CAAC;CACvB;AAED,qBAAa,kBAAkB,CAAC,CAAC,SAAS,kBAAkB;IAC1D,OAAO,CAAC,WAAW,CAAqB;IACxC,OAAO,CAAC,WAAW,CAAqB;IAEjC,IAAI,CAAC,EAAE,UAAU,EAAE,OAAO,EAAE,EAAE,YAAY,CAAC,CAAC,CAAC,GAAG,MAAM,GAAG,UAAU,GAAG,OAAO,CAAC,UAAU,CAAC;IA2BzF,MAAM,CAAC,UAAU,EAAE,MAAM,GAAG,WAAW,GAAG,YAAY,CAAC,CAAC,CAAC;IA2BhE,OAAO,CAAC,iBAAiB;IAczB,OAAO,CAAC,mBAAmB;IAuB3B,OAAO,CAAC,QAAQ;IA8BhB,OAAO,CAAC,kBAAkB;IAoC1B,OAAO,CAAC,MAAM,CAAC,uBAAuB;CAqBvC"}

packages/expo/src/devtools/DevToolsPluginClient.ts
@@ -76,13 +76,19 @@ export abstract class DevToolsPluginClient {
     this.sendMessageImpl(method, params);
   }
 
-  private async sendMessageImpl(method: string, params: any) {
+  private sendMessageImpl(method: string, params: any) {
     const messageKey: MessageFramePackerMessageKey = {
       pluginName: this.connectionInfo.pluginName,
       method,
     };
-    const packedData = await this.messageFramePacker.pack({ messageKey, payload: params });
-    this.wsStore.ws?.send(packedData);
+    const packedData = this.messageFramePacker.pack({ messageKey, payload: params });
+    if (!(packedData instanceof Promise)) {
+      this.wsStore.ws?.send(packedData);
+      return;
+    }
+    packedData.then((data) => {
+      this.wsStore.ws?.send(data);
+    });
   }
 
   /**
@@ -153,18 +159,20 @@ export abstract class DevToolsPluginClient {
   };
 
   private handleMessageImpl = async (event: WebSocketMessageEvent) => {
-    let buffer: ArrayBuffer;
-    if (event.data instanceof ArrayBuffer) {
-      buffer = event.data;
+    let data: ArrayBuffer | string;
+    if (typeof event.data === 'string') {
+      data = event.data;
+    } else if (event.data instanceof ArrayBuffer) {
+      data = event.data;
     } else if (ArrayBuffer.isView(event.data)) {
-      buffer = event.data.buffer;
+      data = event.data.buffer;
     } else if (event.data instanceof Blob) {
-      buffer = await blobToArrayBufferAsync(event.data);
+      data = await blobToArrayBufferAsync(event.data);
     } else {
       logger.warn('Unsupported received data type in handleMessageImpl');
       return;
     }
-    const { messageKey, payload } = await this.messageFramePacker.unpack(buffer);
+    const { messageKey, payload } = this.messageFramePacker.unpack(data);
     if (messageKey.pluginName && messageKey.pluginName !== this.connectionInfo.pluginName) {
       return;
     }

packages/expo/src/devtools/MessageFramePacker.ts
@@ -1,25 +1,33 @@
 /**
- * A message frame packer that serializes a messageKey and a payload into a binary format.
+ * A message frame packer that serializes a messageKey and a payload into either a JSON string
+ * (fast path) or a binary format (for complex payloads).
  *
- * +------------------+-------------------+----------------------------+--------------------------+
- * | 4 bytes (Uint32) | Variable length   | 1 byte (Uint8)             | Variable length          |
- * | MessageKeyLength | MessageKey (JSON) | PayloadTypeIndicator (enum)| Payload (binary data)    |
- * +------------------+-------------------+----------------------------+--------------------------+
+ * Fast Path (JSON.stringify/JSON.parse):
+ * - For simple payloads (e.g., strings, numbers, null, undefined, or plain objects), the packer
+ *   uses `JSON.stringify` for serialization and `JSON.parse` for deserialization, ensuring
+ *   optimal performance.
  *
- * MessageFrame Format:
+ * Binary Format:
+ * - For more complex payloads (e.g., Uint8Array, ArrayBuffer, Blob), the packer uses a binary
+ *   format with the following structure:
  *
- * 1. MessageKeyLength (4 bytes):
- *    - A 4-byte unsigned integer indicating the length of the MessageKey JSON string.
+ *   +------------------+-------------------+----------------------------+--------------------------+
+ *   | 4 bytes (Uint32) | Variable length   | 1 byte (Uint8)             | Variable length          |
+ *   | MessageKeyLength | MessageKey (JSON) | PayloadTypeIndicator (enum)| Payload (binary data)    |
+ *   +------------------+-------------------+----------------------------+--------------------------+
  *
- * 2. MessageKey (Variable length):
- *    - The JSON string representing the message key, encoded as UTF-8.
+ *   1. MessageKeyLength (4 bytes):
+ *      - A 4-byte unsigned integer indicating the length of the MessageKey JSON string.
  *
- * 3. PayloadTypeIndicator (1 byte):
- *    - A single byte enum value representing the type of the payload (e.g., Uint8Array, String, Object, ArrayBuffer, Blob).
+ *   2. MessageKey (Variable length):
+ *      - The JSON string representing the message key, encoded as UTF-8.
  *
- * 4. Payload (Variable length):
- *    - The actual payload data, which can vary in type and length depending on the PayloadType.
+ *   3. PayloadTypeIndicator (1 byte):
+ *      - A single byte enum value representing the type of the payload (e.g., Uint8Array, String,
+ *        Object, ArrayBuffer, Blob).
  *
+ *   4. Payload (Variable length):
+ *      - The actual payload data, which can vary in type and length depending on the PayloadType.
  */
 
 import { blobToArrayBufferAsync } from '../utils/blobUtils';
@@ -47,34 +55,40 @@ export class MessageFramePacker<T extends MessageKeyTypeBase> {
   private textEncoder = new TextEncoder();
   private textDecoder = new TextDecoder();
 
-  public async pack({ messageKey, payload }: MessageFrame<T>): Promise<Uint8Array> {
-    const messageKeyString = JSON.stringify(messageKey);
-    const messageKeyBytes = this.textEncoder.encode(messageKeyString);
-    const messageKeyLength = messageKeyBytes.length;
-    const payloadBinary = await this.payloadToUint8Array(payload);
+  public pack({ messageKey, payload }: MessageFrame<T>): string | Uint8Array | Promise<Uint8Array> {
+    // Fast path to pack as string given `JSON.stringify` is fast.
+    if (this.isFastPathPayload(payload)) {
+      return JSON.stringify({ messageKey, payload });
+    }
 
-    const totalLength = 4 + messageKeyLength + 1 + payloadBinary.byteLength;
-    const buffer = new ArrayBuffer(totalLength);
-    const packedArray = new Uint8Array(buffer);
+    // Slowest path for Blob returns a promise.
+    if (payload instanceof Blob) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const arrayBuffer = await blobToArrayBufferAsync(payload);
+          resolve(
+            this.packImpl(
+              { messageKey, payload: new Uint8Array(arrayBuffer) },
+              PayloadTypeIndicator.Blob
+            )
+          );
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
 
-    // [0] messageKeyLength (4 bytes)
-    const messageKeyLengthView = new DataView(buffer, 0, 4);
-    messageKeyLengthView.setUint32(0, messageKeyLength, false);
-
-    // [1] messageKey (variable length)
-    packedArray.set(messageKeyBytes, 4);
-
-    // [2] payloadTypeIndicator (1 byte)
-    const payloadTypeView = new DataView(buffer, 4 + messageKeyLength, 1);
-    payloadTypeView.setUint8(0, MessageFramePacker.getPayloadTypeIndicator(payload));
-
-    // [3] payload (variable length)
-    packedArray.set(payloadBinary, 4 + messageKeyLength + 1);
-
-    return packedArray;
+    // Slow path for other types returns a Uint8Array.
+    return this.packImpl({ messageKey, payload }, undefined);
   }
 
-  public async unpack(packedData: ArrayBuffer): Promise<MessageFrame<T>> {
+  public unpack(packedData: string | ArrayBuffer): MessageFrame<T> {
+    // Fast path to unpack as string given `JSON.parse` is fast.
+    if (typeof packedData === 'string') {
+      const { messageKey, payload } = JSON.parse(packedData);
+      return { messageKey, payload };
+    }
+
     // [0] messageKeyLength (4 bytes)
     const messageKeyLengthView = new DataView(packedData, 0, 4);
     const messageKeyLength = messageKeyLengthView.getUint32(0, false);
@@ -90,12 +104,26 @@ export class MessageFramePacker<T extends MessageKeyTypeBase> {
 
     // [3] payload (variable length)
     const payloadBinary = packedData.slice(4 + messageKeyLength + 1);
-    const payload = await this.deserializePayload(payloadBinary, payloadType);
+    const payload = this.deserializePayload(payloadBinary, payloadType);
 
     return { messageKey, payload };
   }
 
-  private async payloadToUint8Array(payload: PayloadType): Promise<Uint8Array> {
+  private isFastPathPayload(payload: PayloadType): boolean {
+    if (payload == null) {
+      return true;
+    }
+    const payloadType = typeof payload;
+    if (payloadType === 'string' || payloadType === 'number') {
+      return true;
+    }
+    if (payloadType === 'object' && payload.constructor === Object) {
+      return true;
+    }
+    return false;
+  }
+
+  private payloadToUint8Array(payload: PayloadType): Uint8Array {
     if (payload instanceof Uint8Array) {
       return payload;
     } else if (typeof payload === 'string') {
@@ -112,16 +140,46 @@ export class MessageFramePacker<T extends MessageKeyTypeBase> {
     } else if (payload instanceof ArrayBuffer) {
       return new Uint8Array(payload);
     } else if (payload instanceof Blob) {
-      return new Uint8Array(await blobToArrayBufferAsync(payload));
+      throw new Error('Blob is not supported in this callsite.');
     } else {
       return this.textEncoder.encode(JSON.stringify(payload));
     }
   }
 
-  private async deserializePayload(
+  private packImpl(
+    { messageKey, payload }: MessageFrame<T>,
+    payloadType: PayloadTypeIndicator | undefined
+  ): Promise<Uint8Array> | Uint8Array {
+    const messageKeyString = JSON.stringify(messageKey);
+    const messageKeyBytes = this.textEncoder.encode(messageKeyString);
+    const messageKeyLength = messageKeyBytes.length;
+    const payloadBinary = this.payloadToUint8Array(payload);
+
+    const totalLength = 4 + messageKeyLength + 1 + payloadBinary.byteLength;
+    const buffer = new ArrayBuffer(totalLength);
+    const packedArray = new Uint8Array(buffer);
+
+    // [0] messageKeyLength (4 bytes)
+    const messageKeyLengthView = new DataView(buffer, 0, 4);
+    messageKeyLengthView.setUint32(0, messageKeyLength, false);
+
+    // [1] messageKey (variable length)
+    packedArray.set(messageKeyBytes, 4);
+
+    // [2] payloadTypeIndicator (1 byte)
+    const payloadTypeView = new DataView(buffer, 4 + messageKeyLength, 1);
+    payloadTypeView.setUint8(0, payloadType ?? MessageFramePacker.getPayloadTypeIndicator(payload));
+
+    // [3] payload (variable length)
+    packedArray.set(payloadBinary, 4 + messageKeyLength + 1);
+
+    return packedArray;
+  }
+
+  private deserializePayload(
     payloadBinary: ArrayBuffer,
     payloadTypeIndicator: PayloadTypeIndicator
-  ): Promise<PayloadType> {
+  ): PayloadType {
     switch (payloadTypeIndicator) {
       case PayloadTypeIndicator.Uint8Array: {
         return new Uint8Array(payloadBinary);

packages/expo/src/devtools/__tests__/MessageFramePack-test.node.ts
@@ -13,8 +13,11 @@ describe(MessageFramePacker, () => {
       payload: 'testPayload',
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedData = packer.pack(messageFrame);
+    if (packedData instanceof Promise || packedData instanceof Uint8Array) {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packer.unpack(packedData);
     expect(unpackedData).toEqual(messageFrame);
   });
 
@@ -24,8 +27,11 @@ describe(MessageFramePacker, () => {
       payload: 12345,
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedData = packer.pack(messageFrame);
+    if (packedData instanceof Promise || packedData instanceof Uint8Array) {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packer.unpack(packedData);
     expect(unpackedData).toEqual(messageFrame);
   });
 
@@ -35,8 +41,11 @@ describe(MessageFramePacker, () => {
       payload: null,
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedData = packer.pack(messageFrame);
+    if (packedData instanceof Promise || packedData instanceof Uint8Array) {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packer.unpack(packedData);
     expect(unpackedData).toEqual(messageFrame);
   });
 
@@ -45,8 +54,11 @@ describe(MessageFramePacker, () => {
       messageKey: 'testKey',
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedData = packer.pack(messageFrame);
+    if (packedData instanceof Promise || packedData instanceof Uint8Array) {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packer.unpack(packedData);
     expect(unpackedData).toEqual(messageFrame);
   });
 
@@ -56,8 +68,11 @@ describe(MessageFramePacker, () => {
       payload: { key: 'value' },
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedData = packer.pack(messageFrame);
+    if (packedData instanceof Promise || packedData instanceof Uint8Array) {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packer.unpack(packedData);
     expect(unpackedData).toEqual(messageFrame);
   });
 
@@ -67,8 +82,11 @@ describe(MessageFramePacker, () => {
       payload: new Uint8Array([1, 2, 3, 4]),
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedData = packer.pack(messageFrame);
+    if (packedData instanceof Promise || typeof packedData === 'string') {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packer.unpack(packedData.buffer);
     expect(unpackedData.messageKey).toEqual(messageFrame.messageKey);
     expect(unpackedData.payload).toEqual(messageFrame.payload);
   });
@@ -83,8 +101,11 @@ describe(MessageFramePacker, () => {
       payload: buffer,
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedData = packer.pack(messageFrame);
+    if (packedData instanceof Promise || typeof packedData === 'string') {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packer.unpack(packedData.buffer);
     expect(unpackedData.messageKey).toEqual(messageFrame.messageKey);
     expect(new Uint8Array(unpackedData.payload as ArrayBuffer)).toEqual(view);
   });
@@ -97,8 +118,12 @@ describe(MessageFramePacker, () => {
       payload: blob,
     };
 
-    const packedData = await packer.pack(messageFrame);
-    const unpackedData = await packer.unpack(packedData.buffer);
+    const packedDataPromise = packer.pack(messageFrame);
+    if (packedDataPromise instanceof Uint8Array || typeof packedDataPromise === 'string') {
+      throw new Error('Unexpected packed data type');
+    }
+    const packedData = await packedDataPromise;
+    const unpackedData = packer.unpack(packedData.buffer);
     expect(unpackedData.messageKey).toEqual(messageFrame.messageKey);
     const unpackedBlob = unpackedData.payload as Blob;
     expect(await unpackedBlob.text()).toEqual(await blob.text());
@@ -111,8 +136,11 @@ describe(MessageFramePacker, () => {
       payload: 'testPayload',
     };
 
-    const packedData = await packerWithObjectKey.pack(messageFrame);
-    const unpackedData = await packerWithObjectKey.unpack(packedData.buffer);
+    const packedData = packerWithObjectKey.pack(messageFrame);
+    if (packedData instanceof Promise || packedData instanceof Uint8Array) {
+      throw new Error('Unexpected packed data type');
+    }
+    const unpackedData = packerWithObjectKey.unpack(packedData);
     expect(unpackedData).toEqual(messageFrame);
   });
 
@@ -122,6 +150,8 @@ describe(MessageFramePacker, () => {
       payload: () => {},
     };
 
-    await expect(packer.pack(messageFrame)).rejects.toThrow('Unsupported payload type');
+    expect(() => {
+      packer.pack(messageFrame);
+    }).toThrow('Unsupported payload type');
   });
 });