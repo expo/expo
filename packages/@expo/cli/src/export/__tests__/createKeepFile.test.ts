05d1dff1122639686709443d61c137deeec30ad8
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-03-24 12:25:16 +0100

    cleanup from copied file

--- packages/@expo/cli/src/export/__tests__/createKeepFile.test.ts
+++ packages/@expo/cli/src/export/__tests__/createKeepFile.test.ts
@@ -1,10 +1,3 @@
-/**
- * Copyright © 2025 650 Industries.
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import { vol } from 'memfs';
 import type { AssetData } from 'metro';
 
