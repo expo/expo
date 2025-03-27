2012ed96e2ba2f65b0cb29d7e9d66b2bf6bf2cdd
Gabriel Donadel Dall'Agnol <gabriel@expo.io>
2025-03-27 00:17:39 -0300

    [13/n] upgrade react-native 0.76 (#35716)
    # Why
    
    Follow up of https://github.com/expo/expo/pull/34727
    Closes [ENG-13531](https://linear.app/expo/issue/ENG-13531)
    
    # How
    
    - update package versions
      - `react-native 0.76.7 -> 0.76.8`  
      - `@react-native/normalize-colors 0.76.7 -> 0.76.8` 
      - `@react-native/babel-preset 0.76.7 -> 0.76.8` 
      - `@react-native/dev-middleware 0.76.7 -> 0.76.8` 
      
    
    # Test Plan
     
    bare-expo ios / android
    fabric ios / android
    ci passed
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    This is required for changes to Expo modules.
    -->
    
    - [ ] Documentation is up to date to reflect these changes (eg:
    https://docs.expo.dev and README.md).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).

--- packages/expo-location/android/src/main/java/expo/modules/location/LocationModule.kt
+++ packages/expo-location/android/src/main/java/expo/modules/location/LocationModule.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.hardware.GeomagneticField
 import android.hardware.Sensor
 import android.hardware.SensorEvent
