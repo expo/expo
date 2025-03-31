93b7e9b1724a7be11b9d79c0313a2e5a2fd5e5bf
Bartosz Kaszubowski <gosimek@gmail.com>
2025-03-31 16:58:28 +0200

    [docs] Home: add examples sections, typography/spacing tweaks (#35700)

--- docs/ui/components/Home/components/GridContainer.tsx
+++ docs/ui/components/Home/components/GridContainer.tsx
@@ -19,18 +19,3 @@ export function GridContainer({ children, className }: GridContainerProps) {
     </div>
   );
 }
-
-type GridCellProps = PropsWithChildren<{
-  className?: string;
-}>;
-
-export const GridCell = ({ children, className }: GridCellProps) => (
-  <div
-    className={mergeClasses(
-      'relative min-h-[200px] overflow-hidden rounded-lg border border-default p-8 shadow-xs',
-      '[&_h2]:!my-0 [&_h3]:!mt-0',
-      className
-    )}>
-    {children}
-  </div>
-);
