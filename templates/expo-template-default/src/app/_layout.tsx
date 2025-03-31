15d21b13b106d0973c764f7867219b0289b92f9e
Kadi Kraman <kadi@expo.io>
2025-03-31 11:29:51 +0100

    Keep assets at the root level

--- templates/expo-template-default/src/app/_layout.tsx
+++ templates/expo-template-default/src/app/_layout.tsx
@@ -14,7 +14,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   useEffect(() => {
