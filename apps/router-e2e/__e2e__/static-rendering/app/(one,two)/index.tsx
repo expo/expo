86b8e7f8b2b37c52f6c239e0faeae85059fd1af2
evanbacon <bacon@expo.io>
2025-04-02 18:36:13 -0500

    Update index.tsx

--- apps/router-e2e/__e2e__/static-rendering/app/(one,two)/index.tsx
+++ apps/router-e2e/__e2e__/static-rendering/app/(one,two)/index.tsx
@@ -3,19 +3,21 @@ import { Text, View } from 'react-native';
 export default function Page() {
   const func = function () {};
 
+  console.warn('foo:' + new Error().stack);
   return (
     <>
       <Text testID="index-text" style={{ fontFamily: 'sweet' }}>
         Index
       </Text>
-      <View>xyz</View>
+
+      {/* <View>xyz</View>
       {Array.from({ length: 3 }, (_, i) => (
         <Text testID={`index-text-${i}`} style={{ fontFamily: 'sweet' }}>
           Index {i}
         </Text>
       ))}
 
-      {func}
+      {func} */}
 
       {/* <InvalidAsyncClientComponent /> */}
     </>
