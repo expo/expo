8aee4cf0f96c5b8b5506bd336612114ce3f31258
aleqsio <mikucki@gmail.com>
2025-04-08 11:42:19 +0200

    Remove unnecessary name from module template

--- packages/expo-module-template/src/{%- project.moduleName %}.web.ts
+++ packages/expo-module-template/src/{%- project.moduleName %}.web.ts
@@ -3,8 +3,6 @@ import { registerWebModule, NativeModule } from 'expo';
 import { <%- project.moduleName %>Events } from './<%- project.name %>.types';
 
 class <%- project.moduleName %> extends NativeModule<<%- project.moduleName %>Events> {
-  // @ts-ignore: Overrides the built-in `Function.name` property
-  static name = '<%- project.moduleName %>';
   PI = Math.PI;
   async setValueAsync(value: string): Promise<void> {
     this.emit('onChange', { value });
