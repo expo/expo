69806ef7e0c276ec92dc07829a30a373050a8303
Simek <gosimek@gmail.com>
2025-04-08 15:48:17 +0200

    [docs] add information about bundled version on reference pages

--- docs/ui/components/PageHeader/PageHeader.test.tsx
+++ docs/ui/components/PageHeader/PageHeader.test.tsx
@@ -1,12 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { createRequire } from 'node:module';
 
 import { renderWithTestRouter } from '~/common/test-utilities';
 
-import { PageTitle } from './PageTitle';
+import { PageHeader } from './PageHeader';
 
-describe('PageTitle', () => {
+const require = createRequire(import.meta.url);
+
+describe(PageHeader, () => {
   test('displays npm registry link', () => {
-    render(<PageTitle title="test-title" packageName="expo-av" />);
+    render(<PageHeader title="test-title" packageName="expo-av" testRequire={require} />);
     const linkElement = screen.getAllByRole('link', { hidden: false })[0];
     expect(linkElement.getAttribute('href')).toEqual('https://www.npmjs.com/package/expo-av');
     expect(linkElement.getAttribute('title')).toEqual('View package in npm Registry');
@@ -14,10 +17,11 @@ describe('PageTitle', () => {
 
   test('displays GitHub source code link', () => {
     render(
-      <PageTitle
+      <PageHeader
         title="test-title"
         packageName="expo-av"
         sourceCodeUrl="https://github.com/expo/expo/tree/main/packages/expo-av"
+        testRequire={require}
       />
     );
     const linkElement = screen.getAllByRole('link', { hidden: false })[0];
@@ -27,10 +31,22 @@ describe('PageTitle', () => {
     expect(linkElement.getAttribute('title')).toEqual('View source code of expo-av on GitHub');
   });
 
+  test('displays bundled version when packageName provided', () => {
+    render(
+      <PageHeader
+        title="test-title"
+        packageName="expo-av"
+        sourceCodeUrl="https://github.com/expo/expo/tree/main/packages/expo-av"
+        testRequire={require}
+      />
+    );
+    void screen.findByText(/bundled version:/i);
+  });
+
   test('displays edit page link for non-API docs', () => {
     const router = { pathname: '/router/reference/hooks/' };
 
-    renderWithTestRouter(<PageTitle title="test-title" />, router);
+    renderWithTestRouter(<PageHeader title="test-title" />, router);
     const linkElement = screen.getAllByRole('link', { hidden: false })[0];
 
     expect(linkElement.getAttribute('href')).toEqual(
