94d88a9d7015a1094cd3a278284d2248cea2378d
evanbacon <bacon@expo.io>
2025-04-08 13:10:34 -0500

    wip -- 84239da896fd7395a667ab1e7ef1ef338a32de8f

--- packages/@expo/cli/static/canary-full/react-dom/cjs/react-dom-test-utils.development.js
+++ packages/@expo/cli/static/canary-full/react-dom/cjs/react-dom-test-utils.development.js
@@ -8,63 +8,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
-
-if (process.env.NODE_ENV !== "production") {
-  (function() {
-'use strict';
-
-var React = require('react');
-
-var ReactSharedInternals = React.__CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE;
-
-function error(format) {
-  {
-    {
-      for (var _len2 = arguments.length, args = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-        args[_key2 - 1] = arguments[_key2];
-      }
-
-      printWarning('error', format, args);
-    }
-  }
-}
-
-function printWarning(level, format, args) {
-  // When changing this logic, you might want to also
-  // update consoleWithStackDev.www.js as well.
-  {
-    var stack = ReactSharedInternals.getStackAddendum();
-
-    if (stack !== '') {
-      format += '%s';
-      args = args.concat([stack]);
-    } // eslint-disable-next-line react-internal/safe-string-coercion
-
-
-    var argsWithFormat = args.map(function (item) {
-      return String(item);
-    }); // Careful: RN currently depends on this prefix
-
-    argsWithFormat.unshift('Warning: ' + format); // We intentionally don't use spread (or .apply) directly because it
-    // breaks IE9: https://github.com/facebook/react/issues/13610
-    // eslint-disable-next-line react-internal/no-production-logging
-
-    Function.prototype.apply.call(console[level], console, argsWithFormat);
-  }
-}
-
-var didWarnAboutUsingAct = false;
-function act(callback) {
-  if (didWarnAboutUsingAct === false) {
-    didWarnAboutUsingAct = true;
-
-    error('`ReactDOMTestUtils.act` is deprecated in favor of `React.act`. ' + 'Import `act` from `react` instead of `react-dom/test-utils`. ' + 'See https://react.dev/warnings/react-dom-test-utils for more info.');
-  }
-
-  return React.act(callback);
-}
-
-exports.act = act;
+"use strict";
+"production" !== process.env.NODE_ENV &&
+  (function () {
+    var React = require("react"),
+      didWarnAboutUsingAct = !1;
+    exports.act = function (callback) {
+      !1 === didWarnAboutUsingAct &&
+        ((didWarnAboutUsingAct = !0),
+        console.error(
+          "`ReactDOMTestUtils.act` is deprecated in favor of `React.act`. Import `act` from `react` instead of `react-dom/test-utils`. See https://react.dev/warnings/react-dom-test-utils for more info."
+        ));
+      return React.act(callback);
+    };
   })();
-}
