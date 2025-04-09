34d6cd46ec91f1ab7f24bcf601f72279571b16f7
Clark Gredoña <cgredona@gmail.com>
2025-04-09 08:54:07 -0600

    Update packages/expo-contacts/src/Contacts.ts
    Co-authored-by: Łukasz Kosmaty <kosmatylukasz@gmail.com>

--- packages/expo-contacts/src/Contacts.ts
+++ packages/expo-contacts/src/Contacts.ts
@@ -14,6 +14,7 @@ export type ContactsPermissionResponse = PermissionResponse & {
    * Indicates if your app has access to the whole or only part of the contact library. Possible values are:
    * - `'all'` if the user granted your app access to the whole contact library
    * - `'limited'` if the user granted your app access only to selected contacts (only available on iOS 18+)
+   * - `'none'`
    */
   accessPrivileges?: 'all' | 'limited' | 'none';
 };
