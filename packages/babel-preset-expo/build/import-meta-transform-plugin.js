9c30ec78b3f6d075e2196b95d07204e6d78a52a6
Kudo Chien <kudo@expo.dev>
2025-04-11 00:35:09 +0800

    rebuild js files

--- packages/babel-preset-expo/build/import-meta-transform-plugin.js
+++ packages/babel-preset-expo/build/import-meta-transform-plugin.js
@@ -1,19 +1,24 @@
 "use strict";
 // Copyright 2015-present 650 Industries. All rights reserved.
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.expoImportMetaTransformPlugin = expoImportMetaTransformPlugin;
-function expoImportMetaTransformPlugin(api) {
-    const { types: t } = api;
-    return {
-        name: 'expo-import-meta-transform',
-        visitor: {
-            MetaProperty(path) {
-                const { node } = path;
-                if (node.meta.name === 'import' && node.property.name === 'meta') {
-                    const replacement = t.memberExpression(t.identifier('globalThis'), t.identifier('__ExpoImportMetaRegistry'));
-                    path.replaceWith(replacement);
-                }
+exports.expoImportMetaTransformPluginFactory = expoImportMetaTransformPluginFactory;
+function expoImportMetaTransformPluginFactory(pluginEnabled) {
+    return (api) => {
+        const { types: t } = api;
+        return {
+            name: 'expo-import-meta-transform',
+            visitor: {
+                MetaProperty(path) {
+                    const { node } = path;
+                    if (node.meta.name === 'import' && node.property.name === 'meta') {
+                        if (!pluginEnabled) {
+                            throw path.buildCodeFrameError('Your code uses `import.meta` which is not supported in the React Native runtime yet. Please enable the `unstable_transformImportMeta` option in babel-preset-expo to use import.meta.');
+                        }
+                        const replacement = t.memberExpression(t.identifier('globalThis'), t.identifier('__ExpoImportMetaRegistry'));
+                        path.replaceWith(replacement);
+                    }
+                },
             },
-        },
+        };
     };
 }
