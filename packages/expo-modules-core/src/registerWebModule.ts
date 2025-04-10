13b42de69ae0871b4fa43dd908d28e42a4ad15ea
aleqsio <mikucki@gmail.com>
2025-04-10 17:17:26 +0200

    Update packages/expo-modules-core/src/registerWebModule.ts
    Co-authored-by: Kudo Chien <kudo@expo.dev>

--- packages/expo-modules-core/src/registerWebModule.ts
+++ packages/expo-modules-core/src/registerWebModule.ts
@@ -4,7 +4,7 @@ import type { NativeModule } from './ts-declarations/NativeModule';
 /**
  * Registers a web module.
  * @param moduleImplementation A class that extends `NativeModule`. The class is registered under `globalThis.expo.modules[className]`.
- * @param moduleName – a name to register the module under.
+ * @param moduleName – a name to register the module under `globalThis.expo.modules[className]`.
  * @returns A singleton instance of the class passed into arguments.
  */
 
