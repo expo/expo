7788a77ef0f7aa2dc87221de8bf18c514cc8fe56
Kudo Chien <kudo@expo.dev>
2025-04-11 02:50:01 +0800

    rebuild js files

--- packages/babel-preset-expo/build/import-meta-transform-plugin.js
+++ packages/babel-preset-expo/build/import-meta-transform-plugin.js
@@ -12,7 +12,7 @@ function expoImportMetaTransformPluginFactory(pluginEnabled) {
                     const { node } = path;
                     if (node.meta.name === 'import' && node.property.name === 'meta') {
                         if (!pluginEnabled) {
-                            throw path.buildCodeFrameError('Your code uses `import.meta` which is not supported in the React Native runtime yet. Please enable the `unstable_transformImportMeta` option in babel-preset-expo to use import.meta.');
+                            throw path.buildCodeFrameError('Your code uses `import.meta` which is not supported in the React Native runtime yet. Enable the `unstable_transformImportMeta` option in babel-preset-expo to use `import.meta`.');
                         }
                         const replacement = t.memberExpression(t.identifier('globalThis'), t.identifier('__ExpoImportMetaRegistry'));
                         path.replaceWith(replacement);
