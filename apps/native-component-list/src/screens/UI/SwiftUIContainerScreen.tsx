expo/expo

commit 981c3fd69f40078100266e82dd41139b0a5dbae7

[core] remove extra uiview from swiftui views (#35553)
# Why

remove extra UIView for SwiftUI integration
close ENG-15236

# How

- [core] introduce `SwiftUIVirtualView` as a fake UIView for fabric integration. this is basically mixing `UIView` + `UIView+ComponentViewProtocol` + `ExpoFabricView`
- [core] introduce `AppleView` that abstract the views. it supports both UIView and SwiftUI.View
- [core] for swiftui integration passing props by `@ObservedObject` than `@EnvironmentObject`. this may improve a littie bit from performance.
- [ui] add `ExpoSwiftUI.WithHostingView` that to keep original behavior without breaking change. i'll create another pr to demonstrate the case without extra uiview.

packages/expo-modules-core/ios/Core/Views/SwiftUI/SwiftUIVirtualViewObjC.mm
@@ -1,0 +1,365 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+/**
+ This class is the Objective-C component of SwiftUIVirtualView. This file essentially combines:
+   - iOS `UIView`
+   - react-native's `UIView+ComponentViewProtocol`
+   - Expo's `ExpoFabricViewObjC`
+ */
+
+#import <ExpoModulesCore/SwiftUIVirtualViewObjC.h>
+
+#import <ExpoModulesCore/ExpoViewComponentDescriptor.h>
+#import <ExpoModulesCore/EXJSIConversions.h>
+#import <React/RCTAssert.h>
+#import <React/RCTComponentViewProtocol.h>
+
+namespace react = facebook::react;
+
+namespace {
+
+id convertFollyDynamicToId(const folly::dynamic &dyn)
+{
+  // I could imagine an implementation which avoids copies by wrapping the
+  // dynamic in a derived class of NSDictionary.  We can do that if profiling
+  // implies it will help.
+
+  switch (dyn.type()) {
+    case folly::dynamic::NULLT:
+      return (id)kCFNull;
+    case folly::dynamic::BOOL:
+      return dyn.getBool() ? @YES : @NO;
+    case folly::dynamic::INT64:
+      return @(dyn.getInt());
+    case folly::dynamic::DOUBLE:
+      return @(dyn.getDouble());
+    case folly::dynamic::STRING:
+      return [[NSString alloc] initWithBytes:dyn.c_str() length:dyn.size() encoding:NSUTF8StringEncoding];
+    case folly::dynamic::ARRAY: {
+      NSMutableArray *array = [[NSMutableArray alloc] initWithCapacity:dyn.size()];
+      for (const auto &elem : dyn) {
+        id value = convertFollyDynamicToId(elem);
+        if (value) {
+          [array addObject:value];
+        }
+      }
+      return array;
+    }
+    case folly::dynamic::OBJECT: {
+      NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithCapacity:dyn.size()];
+      for (const auto &elem : dyn.items()) {
+        id key = convertFollyDynamicToId(elem.first);
+        id value = convertFollyDynamicToId(elem.second);
+        if (key && value) {
+          dict[key] = value;
+        }
+      }
+      return dict;
+    }
+  }
+}
+
+/**
+ React Native doesn't use the "on" prefix internally. Instead, it uses "top" but it's on the roadmap to get rid of it too.
+ We're still using "on" in a few places, so let's make sure we normalize that.
+ */
+static NSString *normalizeEventName(NSString *eventName)
+{
+  if ([eventName hasPrefix:@"on"]) {
+    NSString *firstLetter = [[eventName substringWithRange:NSMakeRange(2, 1)] lowercaseString];
+    return [firstLetter stringByAppendingString:[eventName substringFromIndex:3]];
+  }
+  return eventName;
+}
+
+} // namespace
+
+/**
+ Cache for component flavors, where the key is a view class name and value is the flavor.
+ Flavors must be cached in order to keep using the same component handle after app reloads.
+ */
+static std::unordered_map<std::string, expo::ExpoViewComponentDescriptor::Flavor> _componentFlavorsCache;
+
+@implementation SwiftUIVirtualViewObjC {
+  react::SharedViewProps _props;
+  react::SharedViewEventEmitter _eventEmitter;
+  expo::ExpoViewShadowNode::ConcreteState::Shared _state;
+}
+
+- (instancetype)init
+{
+  if (self = [super init]) {
+    static const auto defaultProps = std::make_shared<const expo::ExpoViewProps>();
+    _props = defaultProps;
+  }
+  return self;
+}
+
+#pragma mark - UIView(UIViewHierarchy)
+
+- (nullable UIView *)superview
+{
+  return nil;
+}
+
+- (NSArray<UIView *> *)subviews
+{
+  return @[];
+}
+
+- (nullable UIWindow *)window
+{
+  return nil;
+}
+
+- (void)removeFromSuperview
+{
+}
+
+- (void)insertSubview:(UIView *)view atIndex:(NSInteger)index
+{
+}
+
+- (void)exchangeSubviewAtIndex:(NSInteger)index1 withSubviewAtIndex:(NSInteger)index2
+{
+}
+
+- (void)addSubview:(UIView *)view
+{
+}
+
+- (void)insertSubview:(UIView *)view belowSubview:(UIView *)siblingSubview
+{
+}
+
+- (void)insertSubview:(UIView *)view aboveSubview:(UIView *)siblingSubview
+{
+}
+
+- (void)bringSubviewToFront:(UIView *)view
+{
+}
+
+- (void)sendSubviewToBack:(UIView *)view
+{
+}
+
+- (void)didAddSubview:(UIView *)subview
+{
+}
+
+- (void)willRemoveSubview:(UIView *)subview
+{
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview
+{
+}
+
+- (void)didMoveToSuperview
+{
+}
+
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow
+{
+}
+
+- (void)didMoveToWindow
+{
+}
+
+- (BOOL)isDescendantOfView:(UIView *)view
+{
+  return NO;
+}
+
+#if DEBUG
+- (BOOL)_isAncestorOfFirstResponder
+{
+  // UIKit internal selector called from `insertSubview:atIndex:`
+  // when a VirtualView inserted to a standard UIView.
+  // We use this call point here for sanity check.
+  @throw [NSException exceptionWithName:@"SwiftUIVirtualViewException"
+                                 reason:@"A SwiftUI view is inserted as a child of a standard UIView. Please check that you have wrapped the SwiftUI view in an WithHostingView."
+                               userInfo:nil];
+  return NO;
+}
+#endif
+
+- (nullable UIView *)viewWithTag:(NSInteger)tag
+{
+  return nil;
+}
+
+- (void)setNeedsLayout
+{
+}
+
+- (void)layoutIfNeeded
+{
+}
+
+- (void)layoutSubviews
+{
+}
+
+#pragma mark - RCTComponentViewProtocol implementations
+
++ (react::ComponentDescriptorProvider)componentDescriptorProvider
+{
+  std::string className([NSStringFromClass([self class]) UTF8String]);
+
+  // We're caching the flavor pointer so that the component handle stay the same for the same class name.
+  // Otherwise, the component handle would change after reload which may cause memory leaks and unexpected view recycling behavior.
+  expo::ExpoViewComponentDescriptor::Flavor flavor = _componentFlavorsCache[className];
+
+  if (flavor == nullptr) {
+    flavor = _componentFlavorsCache[className] = std::make_shared<std::string const>(className);
+  }
+
+  ComponentName componentName = ComponentName { flavor->c_str() };
+  ComponentHandle componentHandle = reinterpret_cast<ComponentHandle>(componentName);
+
+  return ComponentDescriptorProvider {
+    componentHandle,
+    componentName,
+    flavor,
+    &facebook::react::concreteComponentDescriptorConstructor<expo::ExpoViewComponentDescriptor>
+  };
+}
+
++ (std::vector<react::ComponentDescriptorProvider>)supplementalComponentDescriptorProviders
+{
+  return {};
+}
+
+- (void)mountChildComponentView:(nonnull UIView *)childComponentView index:(NSInteger)index
+{
+  // Implemented in `SwiftUIVirtualView.swift`
+}
+
+- (void)unmountChildComponentView:(nonnull UIView *)childComponentView index:(NSInteger)index
+{
+  // Implemented in `SwiftUIVirtualView.swift`
+}
+
+- (void)updateProps:(const react::Props::Shared &)props oldProps:(const react::Props::Shared &)oldProps
+{
+  _props = std::static_pointer_cast<const ViewProps>(props);
+}
+
+- (void)updateEventEmitter:(const react::EventEmitter::Shared &)eventEmitter
+{
+  assert(std::dynamic_pointer_cast<const ViewEventEmitter>(eventEmitter));
+  _eventEmitter = std::static_pointer_cast<const ViewEventEmitter>(eventEmitter);
+}
+
+- (void)handleCommand:(NSString *)commandName args:(NSArray *)args
+{
+  // Default implementation does nothing.
+}
+
+- (void)updateLayoutMetrics:(const react::LayoutMetrics &)layoutMetrics
+           oldLayoutMetrics:(const react::LayoutMetrics &)oldLayoutMetrics
+{
+  // Yoga layout is not respected in SwiftUI integration.
+}
+
+- (void)finalizeUpdates:(RNComponentViewUpdateMask)updateMask
+{
+  if (updateMask & RNComponentViewUpdateMaskProps) {
+    const auto &newProps = static_cast<const expo::ExpoViewProps &>(*_props);
+    NSMutableDictionary<NSString *, id> *propsMap = [[NSMutableDictionary alloc] init];
+
+    for (const auto &item : newProps.propsMap) {
+      NSString *propName = [NSString stringWithUTF8String:item.first.c_str()];
+
+      // Ignore props inherited from the base view and Yoga.
+      if ([self supportsPropWithName:propName]) {
+        propsMap[propName] = convertFollyDynamicToId(item.second);
+      }
+    }
+
+    [self updateProps:propsMap];
+    [self viewDidUpdateProps];
+  }
+}
+
+- (void)prepareForRecycle
+{
+  // Default implementation does nothing.
+  _eventEmitter.reset();
+}
+
+- (react::Props::Shared)props
+{
+  RCTAssert(NO, @"props access should be implemented by RCTViewComponentView.");
+  return nullptr;
+}
+
+- (BOOL)isJSResponder
+{
+  // Default implementation always returns `NO`.
+  return NO;
+}
+
+- (void)setIsJSResponder:(BOOL)isJSResponder
+{
+  // Default implementation does nothing.
+}
+
+- (void)setPropKeysManagedByAnimated_DO_NOT_USE_THIS_IS_BROKEN:(nullable NSSet<NSString *> *)propKeys
+{
+  // Default implementation does nothing.
+}
+
+- (nullable NSSet<NSString *> *)propKeysManagedByAnimated_DO_NOT_USE_THIS_IS_BROKEN
+{
+  return nil;
+}
+
+- (void)updateClippedSubviewsWithClipRect:(CGRect)clipRect relativeToView:(UIView *)clipView
+{
+  // Clipped subviews are not supported in SwiftUI integration.
+}
+
+- (void)dispatchEvent:(nonnull NSString *)eventName payload:(nullable id)payload
+{
+  const auto &eventEmitter = static_cast<const expo::ExpoViewEventEmitter &>(*_eventEmitter);
+
+  eventEmitter.dispatch([normalizeEventName(eventName) UTF8String], [payload](jsi::Runtime &runtime) {
+    return jsi::Value(runtime, expo::convertObjCObjectToJSIValue(runtime, payload));
+  });
+}
+
+#pragma mark - Methods to override in Swift
+
+- (void)updateProps:(nonnull NSDictionary<NSString *, id> *)props
+{
+  // Implemented in `SwiftUIVirtualView.swift`
+}
+
+- (void)updateState:(react::State::Shared const &)state oldState:(react::State::Shared const &)oldState
+{
+  _state = std::static_pointer_cast<const expo::ExpoViewShadowNode::ConcreteState>(state);
+}
+
+- (void)viewDidUpdateProps
+{
+  // Implemented in `SwiftUIVirtualView.swift`
+}
+
+- (void)setShadowNodeSize:(float)width height:(float)height
+{
+  if (_state) {
+    _state->updateState(expo::ExpoViewState(width, height));
+  }
+}
+
+- (BOOL)supportsPropWithName:(nonnull NSString *)name
+{
+  // Implemented in `SwiftUIVirtualView.swift`
+  return NO;
+}
+
+@end

packages/expo-contacts/ios/ContactAccessButton/ContactAccessButtonView.swift
@@ -4,8 +4,8 @@ import ContactsUI
 import SwiftUI
 import ExpoModulesCore
 
-internal struct ExpoContactAccessButton: ExpoSwiftUI.View {
-  @EnvironmentObject
+internal struct ExpoContactAccessButton: ExpoSwiftUI.View, ExpoSwiftUI.WithHostingView {
+  @ObservedObject
   internal var props: ContactAccessButtonProps
 
   var body: some View {

packages/expo-maps/ios/AppleMapsView.swift
@@ -19,11 +19,12 @@ protocol AppleMapsViewProtocol: View {
   func setCameraPosition(config: CameraPosition?)
 }
 
-struct AppleMapsViewWrapper: ExpoSwiftUI.View, AppleMapsViewProtocol {
-  @EnvironmentObject var props: AppleMapsViewProps
+struct AppleMapsViewWrapper: ExpoSwiftUI.View, ExpoSwiftUI.WithHostingView, AppleMapsViewProtocol {
+  @ObservedObject var props: AppleMapsViewProps
   var appleMapsView: (any AppleMapsViewProtocol)?
 
-  init() {
+  init(props: AppleMapsViewProps) {
+    self.props = props
     if #available(iOS 18.0, *) {
       appleMapsView = AppleMapsView()
     } else {

packages/expo-mesh-gradient/ios/MeshGradientView.swift
@@ -3,8 +3,8 @@
 import SwiftUI
 import ExpoModulesCore
 
-struct MeshGradientView: ExpoSwiftUI.View {
-  @EnvironmentObject var props: MeshGradientProps
+struct MeshGradientView: ExpoSwiftUI.View, ExpoSwiftUI.WithHostingView {
+  @ObservedObject var props: MeshGradientProps
 
   var body: some View {
     ZStack(alignment: .topLeading) {

packages/expo-modules-core/ios/Api/Factories/ViewFactories.swift
@@ -1,5 +1,9 @@
 /// Here we implement factories for the definitions exclusive for native views.
 
+// Function names should start with a lowercase character, but in this one case
+// we want it to be uppercase for Expo Modules DSL
+// swiftlint:disable identifier_name
+
 /**
  Creates a view definition describing the native view exported to React.
  */
@@ -13,13 +17,13 @@ public func View<ViewType: UIView>(
 /**
  Creates a view definition describing the native SwiftUI view exported to React.
  */
-public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View<Props>>(
+public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View>(
   _ viewType: ViewType.Type
 ) -> ExpoSwiftUI.ViewDefinition<Props, ViewType> {
   return ExpoSwiftUI.ViewDefinition(ViewType.self)
 }
 
-public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View<Props>>(
+public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View>(
   _ viewType: ViewType.Type,
   @ExpoSwiftUI.ViewDefinitionBuilder<ViewType> _ elements: @escaping () -> [AnyViewDefinitionElement]
 ) -> ExpoSwiftUI.ViewDefinition<Props, ViewType> {
@@ -59,3 +63,5 @@ public func OnViewDidUpdateProps<ViewType: UIView>(
 public func ViewName(_ name: String) -> ViewNameDefinition {
   return ViewNameDefinition(name: name)
 }
+
+// swiftlint:enable identifier_name

packages/expo-modules-core/ios/Core/Protocols/AnyViewDefinition.swift
@@ -17,7 +17,7 @@ public protocol AnyViewDefinition {
   /**
    Creates an instance of the native view.
    */
-  func createView(appContext: AppContext) -> UIView?
+  func createView(appContext: AppContext) -> AppleView?
 
   /**
    Returns props definitions as a dictionary where the keys are the prop names.
@@ -37,7 +37,7 @@ public protocol AnyViewDefinition {
   /**
    Calls defined lifecycle methods with the given type.
    */
-  func callLifecycleMethods(withType type: ViewLifecycleMethodType, forView view: UIView)
+  func callLifecycleMethods(withType type: ViewLifecycleMethodType, forView view: AppleView)
 
   /**
    Creates a JavaScript object that may be used as a React component prototype.

packages/expo-modules-core/ios/Core/Views/AppleView.swift
@@ -1,0 +1,48 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+import SwiftUI
+
+/**
+ An abstract view supports both UIKit and SwiftUI.
+ */
+public enum AppleView {
+  case uikit(UIView)
+  case swiftui(any ExpoSwiftUI.View)
+
+  static func from(_ view: UIView?) -> AppleView? {
+    guard let view else {
+      return nil
+    }
+    return .uikit(view)
+  }
+
+  static func from(_ view: (any ExpoSwiftUI.View)?) -> AppleView? {
+    guard let view else {
+      return nil
+    }
+    return .swiftui(view)
+  }
+
+  func toUIView() throws -> UIView {
+    switch self {
+    case .uikit(let view):
+      return view
+    case .swiftui:
+      throw AppleViewConversionError.cannotConvertSwiftUIViewToUIView
+    }
+  }
+
+  func toSwiftUIView() throws -> any ExpoSwiftUI.View {
+    switch self {
+    case .uikit:
+      throw AppleViewConversionError.cannotConvertUIKitViewToSwiftUIView
+    case .swiftui(let swiftUIView):
+      return swiftUIView
+    }
+  }
+}
+
+public enum AppleViewConversionError: Error {
+  case cannotConvertSwiftUIViewToUIView
+  case cannotConvertUIKitViewToSwiftUIView
+}

packages/expo-modules-core/ios/Core/Views/ComponentData.swift
@@ -86,7 +86,7 @@ public final class ComponentData: RCTComponentDataSwiftAdapter {
     // Let the base class `RCTComponentData` handle all remaining props.
     super.setProps(remainingProps, forView: view)
 
-    viewDefinition.callLifecycleMethods(withType: .didUpdateProps, forView: view)
+    viewDefinition.callLifecycleMethods(withType: .didUpdateProps, forView: AppleView.uikit(view))
   }
 
   /**

commit 0f247c79283b2060330de2bac010f91a41b34daf

[core] disable collapsableChildren for SwiftUI container by default (#36153)
# Why

fixed the other regression from #35553 where ContextMenu preview was
broken because content is collapsed.

# How

for swiftui views, disable `collapsableChildren` by default

# Test Plan

test swift ui NCL

# Checklist

- [x] I added a `changelog.md` entry and rebuilt the package sources
according to [this short
guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [x] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).
- [x] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

apps/native-component-list/src/screens/UI/SwiftUIContainerScreen.tsx
@@ -71,9 +71,9 @@ export default function SwiftUIContainerScreen() {
             <SwiftUI.Text>H1V1</SwiftUI.Text>
           </SwiftUI.HStack>
 
-          {/* NOTE: To host UIView inside SwiftUI, we may need fixed size and `collapsable={false}` */}
+          {/* NOTE: To host UIView inside SwiftUI, we may need fixed size */}
           <SwiftUI.HStack frame={{ width: 300, height: 100 }}>
-            <View style={[styles.uiView, { width: 300, height: 100 }]} collapsable={false}>
+            <View style={[styles.uiView, { width: 300, height: 100 }]}>
               <Text style={styles.uiViewText}>Text in UIView</Text>
             </View>
           </SwiftUI.HStack>

packages/expo-modules-core/common/cpp/fabric/ExpoViewProps.h
@@ -10,7 +10,7 @@
 
 namespace expo {
 
-class ExpoViewProps final : public facebook::react::ViewProps {
+class ExpoViewProps : public facebook::react::ViewProps {
 public:
   ExpoViewProps() = default;
   ExpoViewProps(const facebook::react::PropsParserContext &context,

packages/expo-modules-core/common/cpp/fabric/ExpoViewShadowNode.cpp
@@ -2,8 +2,35 @@
 
 #include "ExpoViewShadowNode.h"
 
+namespace react = facebook::react;
+
 namespace expo {
 
-  extern const char ExpoViewComponentName[] = "ExpoFabricView";
+extern const char ExpoViewComponentName[] = "ExpoFabricView";
+
+ExpoViewShadowNode::ExpoViewShadowNode(
+    const react::ShadowNodeFragment &fragment,
+    const react::ShadowNodeFamily::Shared &family,
+    react::ShadowNodeTraits traits)
+    : ConcreteViewShadowNode(fragment, family, traits) {
+  initialize();
+}
+
+ExpoViewShadowNode::ExpoViewShadowNode(
+    const react::ShadowNode &sourceShadowNode,
+    const react::ShadowNodeFragment &fragment)
+    : ConcreteViewShadowNode(sourceShadowNode, fragment) {
+  initialize();
+}
+
+void ExpoViewShadowNode::initialize() noexcept {
+  auto &viewProps = static_cast<const ExpoViewProps &>(*props_);
+
+  if (viewProps.collapsableChildren) {
+    traits_.set(react::ShadowNodeTraits::Trait::ChildrenFormStackingContext);
+  } else {
+    traits_.unset(react::ShadowNodeTraits::Trait::ChildrenFormStackingContext);
+  }
+}
 
 } // namespace expo

packages/expo-modules-core/common/cpp/fabric/ExpoViewShadowNode.h
@@ -6,17 +6,35 @@
 
 #include <react/renderer/components/view/ConcreteViewShadowNode.h>
 
-#include "ExpoViewProps.h"
 #include "ExpoViewEventEmitter.h"
+#include "ExpoViewProps.h"
 #include "ExpoViewState.h"
 
 namespace expo {
 
 extern const char ExpoViewComponentName[];
 
-class ExpoViewShadowNode final : public facebook::react::ConcreteViewShadowNode<ExpoViewComponentName, ExpoViewProps, ExpoViewEventEmitter, ExpoViewState> {
+class ExpoViewShadowNode final : public facebook::react::ConcreteViewShadowNode<
+                                     ExpoViewComponentName, ExpoViewProps,
+                                     ExpoViewEventEmitter, ExpoViewState> {
 public:
   using ConcreteViewShadowNode::ConcreteViewShadowNode;
+
+  ExpoViewShadowNode(const facebook::react::ShadowNodeFragment &fragment,
+                     const facebook::react::ShadowNodeFamily::Shared &family,
+                     facebook::react::ShadowNodeTraits traits);
+
+  ExpoViewShadowNode(const facebook::react::ShadowNode &sourceShadowNode,
+                     const facebook::react::ShadowNodeFragment &fragment);
+
+public:
+  static facebook::react::ShadowNodeTraits BaseTraits() {
+    auto traits = ConcreteViewShadowNode::BaseTraits();
+    return traits;
+  }
+
+private:
+  void initialize() noexcept;
 };
 
 } // namespace expo

packages/expo-modules-core/ios/Core/Views/SwiftUI/SwiftUIViewProps.h
@@ -1,0 +1,30 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#pragma once
+
+#ifdef __cplusplus
+
+#include <ExpoModulesCore/ExpoViewProps.h>
+
+namespace expo {
+
+/**
+ An `ExpoViewProps` derived class specific for SwiftUI that disables `collapsableChildren` by default
+ */
+class SwiftUIViewProps final : public ExpoViewProps {
+public:
+  SwiftUIViewProps() : ExpoViewProps() {
+    this->collapsableChildren = false;
+  }
+
+  SwiftUIViewProps(const facebook::react::PropsParserContext &context,
+                       const ExpoViewProps &sourceProps,
+                       const facebook::react::RawProps &rawProps)
+      : ExpoViewProps(context, sourceProps, rawProps) {
+    this->collapsableChildren = false;
+  }
+};
+
+} // namespace expo
+
+#endif // __cplusplus

packages/expo-modules-core/ios/Core/Views/SwiftUI/SwiftUIVirtualViewObjC.mm
@@ -11,6 +11,7 @@
 
 #import <ExpoModulesCore/ExpoViewComponentDescriptor.h>
 #import <ExpoModulesCore/EXJSIConversions.h>
+#import <ExpoModulesCore/SwiftUIViewProps.h>
 #import <React/RCTAssert.h>
 #import <React/RCTComponentViewProtocol.h>
 
@@ -89,7 +90,7 @@ static std::unordered_map<std::string, expo::ExpoViewComponentDescriptor::Flavor
 - (instancetype)init
 {
   if (self = [super init]) {
-    static const auto defaultProps = std::make_shared<const expo::ExpoViewProps>();
+    static const auto defaultProps = std::make_shared<const expo::SwiftUIViewProps>();
     _props = defaultProps;
   }
   return self;