expo/expo

commit 981c3fd69f40078100266e82dd41139b0a5dbae7

[core] remove extra uiview from swiftui views (#35553)
# Why

remove extra UIView for SwiftUI integration
close ENG-15236

# How

- [core] introduce `SwiftUIVirtualView` as a fake UIView for fabric integration. this is basically mixing `UIView` + `UIView+ComponentViewProtocol` + `ExpoFabricView`
- [core] introduce `AppleView` that abstract the views. it supports both UIView and SwiftUI.View
- [core] for swiftui integration passing props by `@ObservedObject` than `@EnvironmentObject`. this may improve a littie bit from performance.
- [ui] add `ExpoSwiftUI.WithHostingView` that to keep original behavior without breaking change. i'll create another pr to demonstrate the case without extra uiview.

packages/expo-modules-core/ios/Core/Views/SwiftUI/SwiftUIViewDefinition.swift
@@ -6,13 +6,13 @@ import Combine
 /**
  A protocol for SwiftUI views that need to access props.
  */
-public protocol ExpoSwiftUIView<Props>: SwiftUI.View, AnyArgument {
+public protocol ExpoSwiftUIView<Props>: SwiftUI.View, AnyArgument, ExpoSwiftUI.AnyChild {
   associatedtype Props: ExpoSwiftUI.ViewProps
 
   var props: Props { get }
   static func getDynamicType() -> AnyDynamicType
 
-  init()
+  init(props: Props)
 }
 
 public extension ExpoSwiftUIView {
@@ -20,8 +20,9 @@ public extension ExpoSwiftUIView {
    Returns React's children as SwiftUI views.
    */
   func Children() -> some View { // swiftlint:disable:this identifier_name
-    ZStack(alignment: .topLeading) {
-      ForEach(props.children ?? []) { $0 }
+    ForEach(props.children ?? [], id: \.id) { child in
+      let view: any View = child.childView
+      AnyView(view)
     }
   }
 
@@ -29,7 +30,7 @@ public extension ExpoSwiftUIView {
    Returns React's children as SwiftUI views, with any nested HostingViews stripped out.
    */
   func UnwrappedChildren<T: View>( // swiftlint:disable:this identifier_name
-    children: [ExpoSwiftUI.Child?]? = nil,
+    children: [(any ExpoSwiftUI.AnyChild)?]? = nil,
     @ViewBuilder transform: @escaping (_ child: AnyView, _ isHostingView: Bool)
     -> T = { child, _ in  child }
   ) -> ForEach<Range<Int>, Int, AnyView> {
@@ -39,19 +40,20 @@ public extension ExpoSwiftUIView {
     let childrenArray = Array(children)
 
     return ForEach(0..<childrenArray.count, id: \.self) { index in
-      let child = childrenArray[index]
-      AnyView(
+      guard let child = childrenArray[index] else {
+        return AnyView(EmptyView())
+      }
+      return AnyView(
         Group {
-          if let hostingView = child?.view as? (any ExpoSwiftUI.AnyHostingView) {
-            let content = hostingView.getContentView()
-            let propsObject = hostingView.getProps() as any ObservableObject
+          if let hostingView = child as? ExpoSwiftUI.UIViewHost {
+            let content = hostingView.childView
             transform(
               AnyView(
                 content
-                  .environmentObject(propsObject)
                   .environmentObject(ExpoSwiftUI.ShadowNodeProxy.SHADOW_NODE_MOCK_PROXY)), true)
           } else {
-            transform(AnyView(child), false)
+            let view: any View = child
+            transform(AnyView(view), false)
           }
         }
       )
@@ -69,34 +71,37 @@ extension ExpoSwiftUI {
   /**
    A definition representing the native SwiftUI view to export to React.
    */
-  public final class ViewDefinition<Props: ViewProps, ViewType: View<Props>>: ExpoModulesCore.ViewDefinition<HostingView<Props, ViewType>> {
+  public final class ViewDefinition<Props: ViewProps, ViewType: View<Props>>: ExpoModulesCore.ViewDefinition<ViewType> {
     // To obtain prop and event names from the props object we need to create a dummy instance first.
     // This is not ideal, but RN requires us to provide all names before the view is created
     // and there doesn't seem to be a better way to do this right now.
     private lazy var dummyPropsMirror: Mirror = Mirror(reflecting: Props())
 
-    init(_ viewType: ViewType.Type) {
+    convenience init(_ viewType: ViewType.Type) {
       // We assume SwiftUI views are exported as named views under the class name
       let nameDefinitionElement = ViewNameDefinition(name: String(describing: viewType))
-      super.init(HostingView<Props, ViewType>.self, elements: [nameDefinitionElement])
+      self.init(viewType, elements: [nameDefinitionElement])
     }
 
-    init(_ viewType: ViewType.Type, elements: [AnyViewDefinitionElement]) {
-      super.init(HostingView<Props, ViewType>.self, elements: elements)
-    }
-
-    public override func createView(appContext: AppContext) -> UIView? {
+    public override func createView(appContext: AppContext) -> AppleView? {
 #if RCT_NEW_ARCH_ENABLED
       let props = Props()
-      let view = HostingView(viewType: ViewType.self, props: props, appContext: appContext)
 
+      if ViewType.self is WithHostingView.Type {
+        let view = HostingView(viewType: ViewType.self, props: props, appContext: appContext)
+        // Set up events to call view's `dispatchEvent` method.
+        // This is supported only on the new architecture, `dispatchEvent` exists only there.
+        props.setUpEvents(view.dispatchEvent(_:payload:))
+        return AppleView.from(view)
+      }
+
+      let view = SwiftUIVirtualView(viewType: ViewType.self, props: props, viewDefinition: self, appContext: appContext)
       // Set up events to call view's `dispatchEvent` method.
       // This is supported only on the new architecture, `dispatchEvent` exists only there.
       props.setUpEvents(view.dispatchEvent(_:payload:))
-
-      return view
+      return AppleView.from(view)
 #else
-      return UnimplementedExpoView(appContext: appContext, text: "Rendering SwiftUI views is possible only with the New Architecture enabled")
+      return AppleView.from(UnimplementedExpoView(appContext: appContext, text: "Rendering SwiftUI views is possible only with the New Architecture enabled"))
 #endif
     }
 

packages/expo-contacts/ios/ContactAccessButton/ContactAccessButtonView.swift
@@ -4,8 +4,8 @@ import ContactsUI
 import SwiftUI
 import ExpoModulesCore
 
-internal struct ExpoContactAccessButton: ExpoSwiftUI.View {
-  @EnvironmentObject
+internal struct ExpoContactAccessButton: ExpoSwiftUI.View, ExpoSwiftUI.WithHostingView {
+  @ObservedObject
   internal var props: ContactAccessButtonProps
 
   var body: some View {

packages/expo-maps/ios/AppleMapsView.swift
@@ -19,11 +19,12 @@ protocol AppleMapsViewProtocol: View {
   func setCameraPosition(config: CameraPosition?)
 }
 
-struct AppleMapsViewWrapper: ExpoSwiftUI.View, AppleMapsViewProtocol {
-  @EnvironmentObject var props: AppleMapsViewProps
+struct AppleMapsViewWrapper: ExpoSwiftUI.View, ExpoSwiftUI.WithHostingView, AppleMapsViewProtocol {
+  @ObservedObject var props: AppleMapsViewProps
   var appleMapsView: (any AppleMapsViewProtocol)?
 
-  init() {
+  init(props: AppleMapsViewProps) {
+    self.props = props
     if #available(iOS 18.0, *) {
       appleMapsView = AppleMapsView()
     } else {

packages/expo-mesh-gradient/ios/MeshGradientView.swift
@@ -3,8 +3,8 @@
 import SwiftUI
 import ExpoModulesCore
 
-struct MeshGradientView: ExpoSwiftUI.View {
-  @EnvironmentObject var props: MeshGradientProps
+struct MeshGradientView: ExpoSwiftUI.View, ExpoSwiftUI.WithHostingView {
+  @ObservedObject var props: MeshGradientProps
 
   var body: some View {
     ZStack(alignment: .topLeading) {

packages/expo-modules-core/ios/Api/Factories/ViewFactories.swift
@@ -1,5 +1,9 @@
 /// Here we implement factories for the definitions exclusive for native views.
 
+// Function names should start with a lowercase character, but in this one case
+// we want it to be uppercase for Expo Modules DSL
+// swiftlint:disable identifier_name
+
 /**
  Creates a view definition describing the native view exported to React.
  */
@@ -13,13 +17,13 @@ public func View<ViewType: UIView>(
 /**
  Creates a view definition describing the native SwiftUI view exported to React.
  */
-public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View<Props>>(
+public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View>(
   _ viewType: ViewType.Type
 ) -> ExpoSwiftUI.ViewDefinition<Props, ViewType> {
   return ExpoSwiftUI.ViewDefinition(ViewType.self)
 }
 
-public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View<Props>>(
+public func View<Props: ExpoSwiftUI.ViewProps, ViewType: ExpoSwiftUI.View>(
   _ viewType: ViewType.Type,
   @ExpoSwiftUI.ViewDefinitionBuilder<ViewType> _ elements: @escaping () -> [AnyViewDefinitionElement]
 ) -> ExpoSwiftUI.ViewDefinition<Props, ViewType> {
@@ -59,3 +63,5 @@ public func OnViewDidUpdateProps<ViewType: UIView>(
 public func ViewName(_ name: String) -> ViewNameDefinition {
   return ViewNameDefinition(name: name)
 }
+
+// swiftlint:enable identifier_name

packages/expo-modules-core/ios/Core/Protocols/AnyViewDefinition.swift
@@ -17,7 +17,7 @@ public protocol AnyViewDefinition {
   /**
    Creates an instance of the native view.
    */
-  func createView(appContext: AppContext) -> UIView?
+  func createView(appContext: AppContext) -> AppleView?
 
   /**
    Returns props definitions as a dictionary where the keys are the prop names.
@@ -37,7 +37,7 @@ public protocol AnyViewDefinition {
   /**
    Calls defined lifecycle methods with the given type.
    */
-  func callLifecycleMethods(withType type: ViewLifecycleMethodType, forView view: UIView)
+  func callLifecycleMethods(withType type: ViewLifecycleMethodType, forView view: AppleView)
 
   /**
    Creates a JavaScript object that may be used as a React component prototype.

packages/expo-modules-core/ios/Core/Views/AppleView.swift
@@ -1,0 +1,48 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+import SwiftUI
+
+/**
+ An abstract view supports both UIKit and SwiftUI.
+ */
+public enum AppleView {
+  case uikit(UIView)
+  case swiftui(any ExpoSwiftUI.View)
+
+  static func from(_ view: UIView?) -> AppleView? {
+    guard let view else {
+      return nil
+    }
+    return .uikit(view)
+  }
+
+  static func from(_ view: (any ExpoSwiftUI.View)?) -> AppleView? {
+    guard let view else {
+      return nil
+    }
+    return .swiftui(view)
+  }
+
+  func toUIView() throws -> UIView {
+    switch self {
+    case .uikit(let view):
+      return view
+    case .swiftui:
+      throw AppleViewConversionError.cannotConvertSwiftUIViewToUIView
+    }
+  }
+
+  func toSwiftUIView() throws -> any ExpoSwiftUI.View {
+    switch self {
+    case .uikit:
+      throw AppleViewConversionError.cannotConvertUIKitViewToSwiftUIView
+    case .swiftui(let swiftUIView):
+      return swiftUIView
+    }
+  }
+}
+
+public enum AppleViewConversionError: Error {
+  case cannotConvertSwiftUIViewToUIView
+  case cannotConvertUIKitViewToSwiftUIView
+}

packages/expo-modules-core/ios/Core/Views/ComponentData.swift
@@ -86,7 +86,7 @@ public final class ComponentData: RCTComponentDataSwiftAdapter {
     // Let the base class `RCTComponentData` handle all remaining props.
     super.setProps(remainingProps, forView: view)
 
-    viewDefinition.callLifecycleMethods(withType: .didUpdateProps, forView: view)
+    viewDefinition.callLifecycleMethods(withType: .didUpdateProps, forView: AppleView.uikit(view))
   }
 
   /**

commit 47d3e962e429100174c440e190c3c81ae10b01f4

[core] fix UnwrappedChildren regression (#36112)
# Why

fix `UnwrappedChildren` regression from #35553

# How

the `UnwrappedChildren` does not actually get the unwrapped children. we
should further get the unwrapped content view

# Test Plan

NCL expo-ui List component

# Checklist

- [x] I added a `changelog.md` entry and rebuilt the package sources
according to [this short
guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [x] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).
- [x] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

packages/expo-modules-core/ios/Core/Views/SwiftUI/SwiftUIViewDefinition.swift
@@ -45,8 +45,9 @@ public extension ExpoSwiftUIView {
       }
       return AnyView(
         Group {
-          if let hostingView = child as? ExpoSwiftUI.UIViewHost {
-            let content = hostingView.childView
+          if let hostingView = child as? ExpoSwiftUI.UIViewHost,
+            let hostingUIView = hostingView.view as? (any ExpoSwiftUI.AnyHostingView) {
+            let content = hostingUIView.getContentView()
             transform(
               AnyView(
                 content