7e5025047c74e1910afa5ce1f49b7c872d4b97b6
Kudo Chien <kudo@expo.dev>
2025-04-16 23:32:07 +0800

    [config-plugins] add gradle support to android codeMod

--- packages/@expo/config-plugins/build/android/codeMod.d.ts
+++ packages/@expo/config-plugins/build/android/codeMod.d.ts
@@ -18,3 +18,19 @@ export declare function findNewInstanceCodeBlock(contents: string, classDeclarat
  */
 export declare function appendContentsInsideDeclarationBlock(srcContents: string, declaration: string, insertion: string): string;
 export declare function addImports(source: string, imports: string[], isJava: boolean): string;
+/**
+ * Find code block of Gradle plugin block, will return only {} part
+ *
+ * @param contents source contents
+ * @param plugin plugin declaration name, e.g. `plugins` or `pluginManagement`
+ * @returns found CodeBlock, or null if not found.
+ */
+export declare function findGradlePluginCodeBlock(contents: string, plugin: string): CodeBlock | null;
+/**
+ * Append contents to the end of Gradle plugin block
+ * @param srcContents source contents
+ * @param plugin plugin declaration name, e.g. `plugins` or `pluginManagement`
+ * @param insertion code to append
+ * @returns updated contents
+ */
+export declare function appendContentsInsideGradlePluginBlock(srcContents: string, plugin: string, insertion: string): string;
