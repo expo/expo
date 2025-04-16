91382ccb40cacbe7dbe4cea9456ab22b21c76938
evanbacon <bacon@expo.io>
2025-04-16 13:31:39 -0500

    Update index.tsx

--- apps/router-e2e/__e2e__/static-rendering/app/index.tsx
+++ apps/router-e2e/__e2e__/static-rendering/app/index.tsx
@@ -1,20 +1,9 @@
 import { Text } from 'react-native';
 
 export default function Page() {
-  const env = {
-    EXPO_PUBLIC_FOO: process.env.EXPO_PUBLIC_FOO,
-  };
   return (
-    <>
-      <Text testID="index-text" style={{ fontFamily: 'sweet' }}>
-        Index
-      </Text>
-
-      {Object.keys(env).map((key) => (
-        <Text key={key} testID={key}>
-          {key}: {env[key]}
-        </Text>
-      ))}
-    </>
+    <Text testID="index-text" style={{ fontFamily: 'sweet' }}>
+      Index
+    </Text>
   );
 }
