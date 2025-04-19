11708bc8023e1ae9e7c049e8f0ec58e063b11e16
Brent Vatne <brentvatne@gmail.com>
2025-04-18 17:54:34 -0700

    Add fallback UIScreen

---
+++ apps/native-component-list/src/screens/UI/UIScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+export default function UIScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Expo UI is only available on Android and iOS currently.</Text>
+    </View>
+  );
+}
+
+UIScreen.navigationOptions = {
+  title: 'Expo UI',
+};
