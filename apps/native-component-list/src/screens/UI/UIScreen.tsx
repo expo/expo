9a700da46b3d8e6ba0edb6046584f564893d3426
Brent Vatne <brentvatne@gmail.com>
2025-04-18 18:00:23 -0700

    Add missing export

--- apps/native-component-list/src/screens/UI/UIScreen.tsx
+++ apps/native-component-list/src/screens/UI/UIScreen.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from 'react-native';
 
+export const UIScreens = [];
+
 export default function UIScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Expo UI is only available on Android and iOS currently.</Text>
+      <Text>
+        Expo UI is only available on Android and iOS currently.Example temporarily disabled on
+        Android.
+      </Text>
     </View>
   );
 }
