0da3bde8ce086c858c09c7fd56b840621e03cc79
Kadi Kraman <kadi@expo.io>
2025-04-23 14:13:33 +0100

    Add whitespace

--- packages/eslint-config-universe/eslint.config.js
+++ packages/eslint-config-universe/eslint.config.js
@@ -4,6 +4,7 @@ const globals = require('globals');
 const coreConfig = require('./flat/shared/core.js');
 const prettierConfig = require('./flat/shared/prettier.js');
 const typescriptConfig = require('./flat/shared/typescript.js');
+
 module.exports = defineConfig([
   coreConfig,
   typescriptConfig,
