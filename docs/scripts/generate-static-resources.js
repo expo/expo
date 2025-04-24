204da9e283485a2961da3d2beeb768614fd4b791
Bartosz Kaszubowski <gosimek@gmail.com>
2025-04-24 15:18:50 +0200

    [docs] fix new lint warnings after migration to flat config (#36331)

--- docs/scripts/generate-static-resources.js
+++ docs/scripts/generate-static-resources.js
@@ -1,12 +1,10 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import navigation from '../constants/navigation.js';
 import * as versions from '../constants/versions.js';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const basePath = path.join(dirname, '../', 'public', 'static', 'constants');
+const basePath = path.join(import.meta.dirname, '../', 'public', 'static', 'constants');
 
 function writeResource(filename, data) {
   const filePath = path.join(basePath, filename);
