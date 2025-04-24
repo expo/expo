53c99a671c52991af164caa3a2c3a7fe2bfff544
aleqsio <mikucki@gmail.com>
2025-04-24 03:26:24 -0700

    [expo-modules-core] Fix call invocation in tests (#36237)
    Co-authored-by: Kudo Chien <kudo@expo.dev>

--- packages/expo-modules-core/common/cpp/TestingSyncJSCallInvoker.h
+++ packages/expo-modules-core/common/cpp/TestingSyncJSCallInvoker.h
@@ -21,7 +21,6 @@ class TestingSyncJSCallInvoker : public react::CallInvoker {
 public:
   explicit TestingSyncJSCallInvoker(const std::shared_ptr<jsi::Runtime>& runtime) : runtime(runtime) {}
 
-#if REACT_NATIVE_TARGET_VERSION >= 75
   void invokeAsync(react::CallFunc &&func) noexcept override {
     func(*runtime.lock());
   }
@@ -29,15 +28,6 @@ public:
   void invokeSync(react::CallFunc &&func) override {
     func(*runtime.lock());
   }
-#else
-  void invokeAsync(std::function<void()> &&func) noexcept override {
-    func();
-  }
-
-  void invokeSync(std::function<void()> &&func) override {
-    func();
-  }
-#endif
 
   ~TestingSyncJSCallInvoker() override = default;
 
