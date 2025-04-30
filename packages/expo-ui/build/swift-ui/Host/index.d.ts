da68f8c79ac33f303de190f2ac64e2bc62d57d8e
Kudo Chien <kudo@expo.dev>
2025-04-30 16:22:01 +0800

    [ui] add vertical/horizontal support to matchContents

--- packages/expo-ui/build/swift-ui/Host/index.d.ts
+++ packages/expo-ui/build/swift-ui/Host/index.d.ts
@@ -4,7 +4,10 @@ export type HostProps = {
      * When true, the host view will update its size in the React Native view tree to match the content's layout from SwiftUI.
      * @default false
      */
-    matchContents?: boolean;
+    matchContents?: boolean | {
+        vertical?: boolean;
+        horizontal?: boolean;
+    };
     /**
      * When true and no explicit size is provided, the host will use the viewport size as the proposed size for SwiftUI layout.
      * This is particularly useful for SwiftUI views that need to fill their available space, such as `Form`.
--- packages/expo-ui/src/swift-ui/Host/index.tsx
+++ packages/expo-ui/src/swift-ui/Host/index.tsx
@@ -7,7 +7,7 @@ export type HostProps = {
    * When true, the host view will update its size in the React Native view tree to match the content's layout from SwiftUI.
    * @default false
    */
-  matchContents?: boolean;
+  matchContents?: boolean | { vertical?: boolean; horizontal?: boolean };
 
   /**
    * When true and no explicit size is provided, the host will use the viewport size as the proposed size for SwiftUI layout.
@@ -32,22 +32,28 @@ const HostNativeView: React.ComponentType<HostProps> = requireNativeView('ExpoUI
  * A hosting component for SwiftUI views.
  */
 export function Host(props: HostProps) {
-  const { matchContents, useViewportSizeMeasurement, onLayoutContent, style, ...restProps } = props;
+  const { matchContents, onLayoutContent, style, ...restProps } = props;
   const [containerStyle, setContainerStyle] = useState<ViewStyle | null>(null);
   return (
     <HostNativeView
+      style={[style, containerStyle]}
       onLayoutContent={(e) => {
         onLayoutContent?.(e);
         if (matchContents) {
-          setContainerStyle({
-            width: e.nativeEvent.width,
-            height: e.nativeEvent.height,
-          });
+          const matchVertical =
+            typeof matchContents === 'object' ? matchContents.vertical : matchContents;
+          const matchHorizontal =
+            typeof matchContents === 'object' ? matchContents.horizontal : matchContents;
+          const newContainerStyle: ViewStyle = {};
+          if (matchVertical) {
+            newContainerStyle.height = e.nativeEvent.height;
+          }
+          if (matchHorizontal) {
+            newContainerStyle.width = e.nativeEvent.width;
+          }
+          setContainerStyle(newContainerStyle);
         }
       }}
-      style={[style, containerStyle]}
-      matchContents={matchContents}
-      useViewportSizeMeasurement={useViewportSizeMeasurement}
       {...restProps}
     />
   );
