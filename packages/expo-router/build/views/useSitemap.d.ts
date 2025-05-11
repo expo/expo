a2c1ec743a99c57b02380d4ba55015a19485ce77
undefined <undefined>
2025-05-10 19:57:46 -0700

    [expo-router] Headless sitemap with useSitemap() hook

---
+++ packages/expo-router/build/views/useSitemap.d.ts
@@ -0,0 +1,13 @@
+import { Href } from '../types';
+export type SitemapType = {
+    contextKey: string;
+    filename: string;
+    href: string | Href;
+    isInitial: boolean;
+    isVirtual: boolean;
+    isInternal: boolean;
+    isGenerated: boolean;
+    children: SitemapType[];
+};
+export declare function useSitemap(): SitemapType | null;
+//# sourceMappingURL=useSitemap.d.ts.map
