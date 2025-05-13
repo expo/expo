19af1b88d9ed6ed4b107409efdb1281911eaee99
Jakub Tkacz <jtkacz@expo.io>
2025-05-13 21:01:16 +0200

    test: add tests for links

--- packages/expo-router/src/utils/__tests__/sitemap.test.ios.tsx
+++ packages/expo-router/src/utils/__tests__/sitemap.test.ios.tsx
@@ -1,7 +1,7 @@
 import { Text } from 'react-native';
 
 import { router } from '../../imperative-api';
-import { act, renderRouter, screen, within } from '../../testing-library';
+import { act, fireEvent, renderRouter, screen, within } from '../../testing-library';
 import { Slot } from '../../views/Navigator';
 
 test('given no routes, unmatched route', () => {
@@ -96,4 +96,47 @@ test('given deeply nested route, renders all levels of ', () => {
   expect(nestedItems[1]).toHaveTextContent('index.js');
   expect(nestedItems[2]).toHaveTextContent('secondLevel/_layout.js');
   expect(nestedItems[3]).toHaveTextContent('index.js');
+});
+
+describe('links', () => {
+  let containers: ReturnType<typeof screen.getAllByTestId>;
+  beforeEach(() => {
+    renderRouter({
+      _layout: () => <Slot />,
+      index: () => <Text />,
+      about: () => <Text />,
+      'nested/_layout': () => <Slot />,
+      'nested/index': () => <Text />,
+    });
+    act(() => router.replace('/_sitemap'));
+    containers = screen.getAllByTestId('sitemap-item-container');
+    expect(containers).toHaveLength(3);
+  });
+  test('clicking the first sitemap item navigates to the index page', () => {
+    const link = within(containers[0]).getByRole('link');
+    act(() => fireEvent.press(link));
+    expect(screen).toHavePathname('/');
+  });
+  test('clicking the second sitemap item navigates to the about page', () => {
+    const link = within(containers[1]).getByRole('link');
+    act(() => fireEvent.press(link));
+    expect(screen).toHavePathname('/about');
+  });
+  // The first link in the nested container is the layout route, which should not be clickable.
+  describe('nested container', () => {
+    test('clicking the first link (nested _layout) within the nested container keeps the user on the sitemap', () => {
+      const links = within(containers[2]).getAllByRole('link');
+      const link = links[0];
+      expect(link).toHaveTextContent('nested/_layout.js');
+      act(() => fireEvent.press(link));
+      expect(screen).toHavePathname('/_sitemap');
+    });
+    test('clicking the second link (nested index) within the nested container navigates to the nested index page', () => {
+      const links = within(containers[2]).getAllByRole('link');
+      const link = links[1];
+      expect(link).toHaveTextContent('index.js');
+      act(() => fireEvent.press(link));
+      expect(screen).toHavePathname('/nested');
+    });
+  });
 });
