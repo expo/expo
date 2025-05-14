fe7911013deb1dc2b1a9dc0abc666071e2cc17f6
lukmccall <kosmatylukasz@gmail.com>
2025-05-14 13:05:10 +0200

    [apps] Recreate the paper-tester android app from the current template

--- apps/paper-tester/android/app/src/main/java/com/community/papertester/MainActivity.kt
+++ apps/paper-tester/android/app/src/main/java/com/community/papertester/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.community.papertester
+import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
 import android.os.Bundle
@@ -15,7 +16,10 @@ class MainActivity : ReactActivity() {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme);
+    // setTheme(R.style.AppTheme);
+    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
+    SplashScreenManager.registerOnActivity(this)
+    // @generated end expo-splashscreen
     super.onCreate(null)
   }
 
