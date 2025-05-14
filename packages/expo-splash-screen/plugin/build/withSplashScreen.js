052df3a62e400d56c285fdbb3dedabd65eca2755
Brent Vatne <brentvatne@gmail.com>
2025-05-14 13:08:34 -0700

    [splash-screen][plugin] If config plugin is only configured for one platform, continue to fallback to old config for the other

--- packages/expo-splash-screen/plugin/build/withSplashScreen.js
+++ packages/expo-splash-screen/plugin/build/withSplashScreen.js
@@ -5,33 +5,35 @@ const withIosSplashScreen_1 = require("@expo/prebuild-config/build/plugins/unver
 const config_plugins_1 = require("expo/config-plugins");
 const pkg = require('expo-splash-screen/package.json');
 const withSplashScreen = (config, props) => {
-    if (!props) {
-        config = (0, withAndroidSplashScreen_1.withAndroidSplashScreen)(config, null);
-        config = (0, withIosSplashScreen_1.withIosSplashScreen)(config, null);
-        return config;
-    }
+    let android = null;
+    let ios = null;
     const resizeMode = props?.resizeMode || 'contain';
-    const { ios: iosProps, android: androidProps, ...otherProps } = props;
-    const android = {
-        ...otherProps,
-        ...androidProps,
-        resizeMode: androidProps?.resizeMode || resizeMode,
-        dark: {
-            ...otherProps?.dark,
-            ...androidProps?.dark,
-        },
-    };
-    const ios = {
-        ...otherProps,
-        ...iosProps,
-        resizeMode: iosProps?.resizeMode || (resizeMode === 'native' ? 'contain' : resizeMode),
-        dark: {
-            ...otherProps?.dark,
-            ...iosProps?.dark,
-        },
-    };
-    // Need to pass null here if we don't receive any props. This means that the plugin has not been used.
-    // This only happens on Android. On iOS, if you don't use the plugin, this function won't be called.
+    const { ios: iosProps, android: androidProps, ...otherProps } = props ?? {};
+    const usesLegacySplashConfigIOS = !props || (androidProps && !iosProps && Object.keys(otherProps).length === 0);
+    const usesLegacySplashConfigAndroid = !props || (iosProps && !androidProps && Object.keys(otherProps).length === 0);
+    android = usesLegacySplashConfigAndroid
+        ? null
+        : {
+            ...otherProps,
+            ...androidProps,
+            resizeMode: androidProps?.resizeMode || resizeMode,
+            dark: {
+                ...otherProps?.dark,
+                ...androidProps?.dark,
+            },
+        };
+    ios = usesLegacySplashConfigIOS
+        ? null
+        : {
+            ...otherProps,
+            ...iosProps,
+            resizeMode: iosProps?.resizeMode || (resizeMode === 'native' ? 'contain' : resizeMode),
+            dark: {
+                ...otherProps?.dark,
+                ...iosProps?.dark,
+            },
+        };
+    // Passing null here will result in the legacy splash config being used.
     config = (0, withAndroidSplashScreen_1.withAndroidSplashScreen)(config, android);
     config = (0, withIosSplashScreen_1.withIosSplashScreen)(config, ios);
     return config;
