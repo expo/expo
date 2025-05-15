2d34d4df5abc02c4d14ccd0b952b3411f17fbf78
bang9 <gusrn1423@naver.com>
2025-05-16 00:07:42 +0900

    chore: rebuild plugin

--- packages/expo-media-library/plugin/build/withMediaLibrary.d.ts
+++ packages/expo-media-library/plugin/build/withMediaLibrary.d.ts
@@ -1,11 +1,11 @@
 import { ConfigPlugin, AndroidConfig } from 'expo/config-plugins';
-type GranularTypes = 'photo' | 'video' | 'audio';
+type GranularPermission = 'photo' | 'video' | 'audio';
 export declare function modifyAndroidManifest(manifest: AndroidConfig.Manifest.AndroidManifest): AndroidConfig.Manifest.AndroidManifest;
 declare const _default: ConfigPlugin<void | {
     photosPermission?: string | false;
     savePhotosPermission?: string | false;
     isAccessMediaLocationEnabled?: boolean;
     preventAutomaticLimitedAccessAlert?: boolean;
-    granularPermissions?: GranularTypes[];
+    granularPermissions?: GranularPermission[];
 }>;
 export default _default;
