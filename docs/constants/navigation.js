9a7765a64de56c1939c0404a80d3a0a07b088388
Jon Samp <sampjon@gmail.com>
2025-05-19 16:56:47 -0500

    adds doc

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -395,10 +395,11 @@ export const eas = [
   makeSection('EAS Workflows', [
     makePage('eas/workflows/get-started.mdx'),
     makePage('eas/workflows/examples.mdx'),
+    makePage('eas/workflows/pre-packaged-jobs.mdx'),
     makePage('eas/workflows/syntax.mdx'),
     makePage('eas/workflows/automating-eas-cli.mdx'),
     makePage('eas/workflows/limitations.mdx'),
-    makeGroup('Reference', [makePage('eas/workflows/reference/e2e-tests.mdx')]),
+    makeGroup('More', [makePage('eas/workflows/reference/e2e-tests.mdx')]),
   ]),
   makeSection('EAS Build', [
     makePage('build/introduction.mdx'),
