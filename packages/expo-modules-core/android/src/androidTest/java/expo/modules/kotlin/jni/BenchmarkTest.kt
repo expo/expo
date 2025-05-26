52d5199af0d389b3061cfbecf7b1aa7c0c418bf2
lukmccall <kosmatylukasz@gmail.com>
2025-05-26 10:54:54 +0200

    [core][Android] Add benchmark test

---
+++ packages/expo-modules-core/android/src/androidTest/java/expo/modules/kotlin/jni/BenchmarkTest.kt
@@ -0,0 +1,39 @@
+package expo.modules.kotlin.jni
+
+import org.junit.Ignore
+import org.junit.Test
+import kotlin.time.DurationUnit
+import kotlin.time.measureTime
+import kotlin.time.toDuration
+
+@Ignore("Benchmark tests are not run by default. They are used to measure performance of the JSI bridge and should be run manually when needed.")
+class BenchmarkTest {
+  @Test
+  fun benchmark() {
+    val numberOfTries = 10
+    var totalAvg = 0.toDuration(unit = DurationUnit.MILLISECONDS)
+    repeat(numberOfTries) {
+      withSingleModule({
+        Function("add") { a: Int, b: Int ->
+          a + b
+        }
+      }) {
+        val numberOfCalls = 10_000
+        var total = 0.toDuration(unit = DurationUnit.MILLISECONDS)
+        repeat(numberOfCalls) {
+          val time = measureTime {
+            callVoid("add", "1, 2")
+          }
+
+          total += time
+        }
+
+        val average = total / numberOfCalls
+        totalAvg += average
+        println("Average time: $average")
+      }
+    }
+    val avg = totalAvg / numberOfTries
+    println("Average time for $numberOfTries tries: $avg")
+  }
+}
