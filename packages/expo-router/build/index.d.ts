0f3cb91a20779a5311293115cd1f4f347230a60a
Jakub Tkacz <jtkacz@expo.io>
2025-05-27 16:50:15 +0200

    remove changes in router

--- packages/expo-router/build/index.d.ts
+++ packages/expo-router/build/index.d.ts
@@ -1,5 +1,4 @@
 export { Stack } from './layouts/Stack';
 export { Tabs } from './layouts/Tabs';
 export * from './exports';
-export { ContextMenu, Button } from '@expo/ui/swift-ui';
 //# sourceMappingURL=index.d.ts.map
--- packages/expo-router/src/index.tsx
+++ packages/expo-router/src/index.tsx
@@ -1,5 +1,3 @@
 export { Stack } from './layouts/Stack';
 export { Tabs } from './layouts/Tabs';
 export * from './exports';
-
-export { ContextMenu, Button } from '@expo/ui/swift-ui';
