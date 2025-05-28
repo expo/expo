0725ad82ee4558a3da2ebc30a6d5d6212a7320d5
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2025-05-28 10:14:35 +0200

    [mesh-gradient][Android] Add mesh gradient on Android (#37056)
    # Why
    
    Mesh gradient was only available on Apple platforms, but it's possible
    to implement it on Android manually using compose canvas.
    
    # How
    
    Code is partially based on
    https://gist.github.com/sinasamaki/05725557c945c5329fdba4a3494aaecb.
    * Mask is not possible as compose component can not have a child
    * `smoothsColors` uses smoothstep cubic polynomial function to achieve
    similar effect as on iOS
    
    # Test Plan
    
    Bare Expo -> MeshGradient screen
    
    | `smoothsColors` | Screenshot |
    | --- | --- |
    | `"true"` | <img width="924" alt="image"
    src="https://github.com/user-attachments/assets/6c9db12c-e1ac-43e6-875d-3a5bcd1f35cb"
    /> |
    | `"false"` | <img width="924" alt="image"
    src="https://github.com/user-attachments/assets/bba619c8-00bf-419e-9a2c-bd059d26a151"
    /> |

--- packages/expo-mesh-gradient/src/MeshGradientView.tsx
+++ packages/expo-mesh-gradient/src/MeshGradientView.tsx
@@ -1,5 +1,23 @@
 import { requireNativeView } from 'expo';
+import { Platform, processColor, ProcessedColorValue } from 'react-native';
 
 import { MeshGradientViewProps } from './MeshGradient.types';
 
-export default requireNativeView<MeshGradientViewProps>('ExpoMeshGradient');
+const NativeView = requireNativeView<
+  Omit<MeshGradientViewProps, 'colors'> & {
+    colors?: (ProcessedColorValue | null | undefined)[];
+  }
+>('ExpoMeshGradient');
+
+const MeshGradientView = (props: MeshGradientViewProps) => {
+  const { colors, children, ...restProps } = props;
+  return (
+    <NativeView
+      {...restProps}
+      colors={colors?.map((color) => processColor(color))}
+      children={Platform.OS !== 'android' ? children : undefined}
+    />
+  );
+};
+
+export default MeshGradientView;
