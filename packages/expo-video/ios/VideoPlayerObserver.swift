a79c8c461b5d9e5be0aafd2f7d1c9a517326aa72
Wojciech Dróżdż <behenate@gmail.com>
2025-06-02 18:34:13 +0200

    Fix player getting stuck in `loading` state for null sources

--- packages/expo-video/ios/VideoPlayerObserver.swift
+++ packages/expo-video/ios/VideoPlayerObserver.swift
@@ -408,11 +408,17 @@ class VideoPlayerObserver: VideoSourceLoaderListener {
 
     if player.timeControlStatus != .waitingToPlayAtSpecifiedRate && player.status == .readyToPlay && currentItem?.isPlaybackBufferEmpty != true {
       status = .readyToPlay
-    }
-    // Every time the player is unpaused timeControlStatus goes into .waitingToPlayAtSpecifiedRate while evaluating buffering rate.
-    // This takes less than a frame and we can ignore this change to avoid unnecessary status changes.
-    else if player.timeControlStatus == .waitingToPlayAtSpecifiedRate && player.reasonForWaitingToPlay != .evaluatingBufferingRate {
-      status = .loading
+    } else if player.timeControlStatus == .waitingToPlayAtSpecifiedRate {
+      switch player.reasonForWaitingToPlay {
+      case .noItemToPlay:
+        status = .idle
+      case .evaluatingBufferingRate:
+        // Every time the player is unpaused timeControlStatus goes into .waitingToPlayAtSpecifiedRate while evaluating buffering rate.
+        // This takes less than a frame and we can ignore this change to avoid unnecessary status changes.
+        break
+      default:
+        status = .loading
+      }
     }
 
     if isPlaying != (player.timeControlStatus == .playing) {
