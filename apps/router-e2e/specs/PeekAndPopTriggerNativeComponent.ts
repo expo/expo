2d556877e2da99e9603261e18faeadb0af6c02d2
Jakub Tkacz <jtkacz@expo.io>
2025-06-04 17:33:06 +0200

    [router] revert router-e2e to main

--- apps/router-e2e/specs/PeekAndPopTriggerNativeComponent.ts
+++
@@ -1,8 +0,0 @@
-import type { HostComponent, ViewProps } from 'react-native';
-import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-
-export interface NativeProps extends ViewProps {}
-
-export default codegenNativeComponent<NativeProps>(
-  'PeekAndPopTrigger'
-) as HostComponent<NativeProps>;
