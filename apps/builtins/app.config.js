d8e2f331c23df03d729a3fdbe22bd6eb66d879b7
evanbacon <bacon@expo.io>
2025-06-07 01:10:34 +0200

    use experimental import support

---
+++ apps/builtins/app.config.js
@@ -0,0 +1,12 @@
+const path = require('node:path');
+/** @type {import('expo/config').ExpoConfig} */
+module.exports = {
+  jsEngine: "hermes",
+  newArchEnabled: true,
+  experiments: {
+    reactCanary: false,
+    reactCompiler: false,
+  },
+  
+  
+};
