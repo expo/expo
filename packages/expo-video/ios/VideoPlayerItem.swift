52f61b0eded3f6634630a0bc4f4acb0ff1f41684
Vitor <vitorclelis96@gmail.com>
2025-06-12 12:11:40 -0300

    Fixing lint issues (again)

--- packages/expo-video/ios/VideoPlayerItem.swift
+++ packages/expo-video/ios/VideoPlayerItem.swift
@@ -80,7 +80,7 @@ class VideoPlayerItem: AVPlayerItem {
       return
     }
     if let audioGroup = self.asset.mediaSelectionGroup(forMediaCharacteristic: .audible),
-      let audioOption = audioGroup.defaultOption ?? audioGroup.options.first  {
+      let audioOption = audioGroup.defaultOption ?? audioGroup.options.first {
         self.select(audioOption, in: audioGroup)
       }
   }
