a062b48a6d0a6749908a7baf56811ec22babb42b
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2025-06-16 13:47:03 +0200

    [menu][Android] Rewrite using jetpack compose (#37422)

---
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/compose/DevMenuViewModel.kt
@@ -0,0 +1,30 @@
+package expo.modules.devmenu.compose
+
+import androidx.compose.runtime.mutableStateOf
+
+class DevMenuViewModel(
+  appInfo: DevMenuState.AppInfo
+) {
+  private val _state = mutableStateOf(
+    DevMenuState(
+      appInfo = appInfo
+    )
+  )
+
+  val state
+    get() = _state.value
+
+  fun onAction(action: DevMenuAction) {
+    when (action) {
+      DevMenuAction.Open -> TODO()
+      DevMenuAction.Reload -> TODO()
+      DevMenuAction.GoHome -> TODO()
+      DevMenuAction.TogglePerformanceMonitor -> TODO()
+      DevMenuAction.Close -> TODO()
+      DevMenuAction.OpenJSDebugger -> TODO()
+      DevMenuAction.OpenReactNativeDevMenu -> TODO()
+      DevMenuAction.ToggleElementInspector -> TODO()
+      is DevMenuAction.ToggleFastRefresh -> TODO()
+    }
+  }
+}
---
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/compose/theme/Pallet.kt
@@ -0,0 +1,250 @@
+package expo.modules.devmenu.compose.theme
+
+import androidx.compose.ui.graphics.Color
+import expo.modules.devmenu.compose.fromHex
+
+// Generate from @radix-ui/colors
+object Pallet {
+  val white = Color.fromHex("#ffffff")
+  val black = Color.fromHex("#1b1f23")
+
+  object Light {
+    object Blue {
+      val blue000 = Color.fromHex("#F1F8FF")
+      val blue100 = Color.fromHex("#dbedff")
+      val blue200 = Color.fromHex("#c8e1ff")
+      val blue300 = Color.fromHex("#79b8ff")
+      val blue400 = Color.fromHex("#2188ff")
+      val blue500 = Color.fromHex("#0366d6")
+      val blue600 = Color.fromHex("#005cc5")
+      val blue700 = Color.fromHex("#044289")
+      val blue800 = Color.fromHex("#032f62")
+      val blue900 = Color.fromHex("#05264c")
+    }
+
+    object Gray {
+      val gray000 = Color.fromHex("#fdfdfe")
+      val gray100 = Color.fromHex("#f8f8fa")
+      val gray200 = Color.fromHex("#f0f1f2")
+      val gray300 = Color.fromHex("#e1e4e8")
+      val gray400 = Color.fromHex("#d1d5da")
+      val gray500 = Color.fromHex("#9b9fa3")
+      val gray600 = Color.fromHex("#6c737c")
+      val gray700 = Color.fromHex("#596068")
+      val gray800 = Color.fromHex("#464d55")
+      val gray900 = Color.fromHex("#30363c")
+      val gray1000 = Color.fromHex("#25292e")
+    }
+
+    object Green {
+      val green000 = Color.fromHex("#f0fff4")
+      val green100 = Color.fromHex("#dcffe4")
+      val green200 = Color.fromHex("#bef5cb")
+      val green300 = Color.fromHex("#85e89d")
+      val green400 = Color.fromHex("#34d058")
+      val green500 = Color.fromHex("#28a745")
+      val green600 = Color.fromHex("#22863a")
+      val green700 = Color.fromHex("#176f2c")
+      val green800 = Color.fromHex("#165c26")
+      val green900 = Color.fromHex("#144620")
+    }
+
+    object Orange {
+      val orange000 = Color.fromHex("#fff8f2")
+      val orange100 = Color.fromHex("#ffebda")
+      val orange200 = Color.fromHex("#ffd1ac")
+      val orange300 = Color.fromHex("#ffab70")
+      val orange400 = Color.fromHex("#fb8e41")
+      val orange500 = Color.fromHex("#fa7c25")
+      val orange600 = Color.fromHex("#de6614")
+      val orange700 = Color.fromHex("#c45408")
+      val orange800 = Color.fromHex("#96430b")
+      val orange900 = Color.fromHex("#7a4018")
+    }
+
+    object Pink {
+      val pink000 = Color.fromHex("#ffeef8")
+      val pink100 = Color.fromHex("#fedbf0")
+      val pink200 = Color.fromHex("#f9b3dd")
+      val pink300 = Color.fromHex("#f692ce")
+      val pink400 = Color.fromHex("#ec6cb9")
+      val pink500 = Color.fromHex("#ea4aaa")
+      val pink600 = Color.fromHex("#d03592")
+      val pink700 = Color.fromHex("#b93a86")
+      val pink800 = Color.fromHex("#99306f")
+      val pink900 = Color.fromHex("#6d224f")
+    }
+
+    object Purple {
+      val purple000 = Color.fromHex("#f5f0ff")
+      val purple100 = Color.fromHex("#e6dcfd")
+      val purple200 = Color.fromHex("#d1bcf9")
+      val purple300 = Color.fromHex("#b392f0")
+      val purple400 = Color.fromHex("#8a63d2")
+      val purple500 = Color.fromHex("#6f42c1")
+      val purple600 = Color.fromHex("#5a32a3")
+      val purple700 = Color.fromHex("#4c2888")
+      val purple800 = Color.fromHex("#3a1d6e")
+      val purple900 = Color.fromHex("#29134e")
+    }
+
+    object Primary {
+      val primary000 = Color.fromHex("#f9f7ff")
+      val primary100 = Color.fromHex("#ede9ff")
+      val primary200 = Color.fromHex("#d2cafd")
+      val primary300 = Color.fromHex("#a193f9")
+      val primary400 = Color.fromHex("#664fff")
+      val primary500 = Color.fromHex("#4630eb")
+      val primary600 = Color.fromHex("#3929c4")
+      val primary700 = Color.fromHex("#2e2496")
+      val primary800 = Color.fromHex("#261f73")
+      val primary900 = Color.fromHex("#221e5c")
+    }
+
+    object Red {
+      val red000 = Color.fromHex("#ffeeee")
+      val red100 = Color.fromHex("#ffdcdc")
+      val red200 = Color.fromHex("#fdaeae")
+      val red300 = Color.fromHex("#f97575")
+      val red400 = Color.fromHex("#ea4a4a")
+      val red500 = Color.fromHex("#d73a3a")
+      val red600 = Color.fromHex("#cb2424")
+      val red700 = Color.fromHex("#b31d1d")
+      val red800 = Color.fromHex("#9e1e1c")
+      val red900 = Color.fromHex("#861c18")
+    }
+
+    object Yellow {
+      val yellow000 = Color.fromHex("#fffdef")
+      val yellow100 = Color.fromHex("#fffbdd")
+      val yellow200 = Color.fromHex("#fff5b1")
+      val yellow300 = Color.fromHex("#ffeb87")
+      val yellow400 = Color.fromHex("#ffdf5d")
+      val yellow500 = Color.fromHex("#ffd33d")
+      val yellow600 = Color.fromHex("#f2c012")
+      val yellow700 = Color.fromHex("#cfa108")
+      val yellow800 = Color.fromHex("#9c7800")
+      val yellow900 = Color.fromHex("#735c0f")
+    }
+  }
+
+  object Dark {
+    object Blue {
+      val blue000 = Color.fromHex("#122447")
+      val blue100 = Color.fromHex("#173366")
+      val blue200 = Color.fromHex("#1e4894")
+      val blue300 = Color.fromHex("#2362c4")
+      val blue400 = Color.fromHex("#2d73e0")
+      val blue500 = Color.fromHex("#4794fd")
+      val blue600 = Color.fromHex("#61abff")
+      val blue700 = Color.fromHex("#8fcaff")
+      val blue800 = Color.fromHex("#addaff")
+      val blue900 = Color.fromHex("#cae8ff")
+    }
+
+    object Gray {
+      val gray000 = Color.fromHex("#0d1117")
+      val gray100 = Color.fromHex("#161b22")
+      val gray200 = Color.fromHex("#21262d")
+      val gray300 = Color.fromHex("#30363d")
+      val gray400 = Color.fromHex("#484f58")
+      val gray500 = Color.fromHex("#6e7681")
+      val gray600 = Color.fromHex("#8b949e")
+      val gray700 = Color.fromHex("#b1bac4")
+      val gray800 = Color.fromHex("#c9d1d9")
+      val gray900 = Color.fromHex("#f0f6fc")
+      val gray1000 = Color.fromHex("#fdfdfe")
+    }
+
+    object Green {
+      val green000 = Color.fromHex("#1a2b20")
+      val green100 = Color.fromHex("#1d3b27")
+      val green200 = Color.fromHex("#255232")
+      val green300 = Color.fromHex("#256936")
+      val green400 = Color.fromHex("#2e853f")
+      val green500 = Color.fromHex("#38a04b")
+      val green600 = Color.fromHex("#4ab959")
+      val green700 = Color.fromHex("#65d372")
+      val green800 = Color.fromHex("#8ae592")
+      val green900 = Color.fromHex("#b9f0bd")
+    }
+
+    object Orange {
+      val orange000 = Color.fromHex("#2e1e17")
+      val orange100 = Color.fromHex("#472a1d")
+      val orange200 = Color.fromHex("#66361f")
+      val orange300 = Color.fromHex("#8f4824")
+      val orange400 = Color.fromHex("#ad5a2b")
+      val orange500 = Color.fromHex("#d67233")
+      val orange600 = Color.fromHex("#eb853d")
+      val orange700 = Color.fromHex("#f5a35b")
+      val orange800 = Color.fromHex("#fcc279")
+      val orange900 = Color.fromHex("#fcd49f")
+    }
+
+    object Pink {
+      val pink000 = Color.fromHex("#3d182f")
+      val pink100 = Color.fromHex("#571e40")
+      val pink200 = Color.fromHex("#782a57")
+      val pink300 = Color.fromHex("#9c356e")
+      val pink400 = Color.fromHex("#bf4b8a")
+      val pink500 = Color.fromHex("#de62a4")
+      val pink600 = Color.fromHex("#f77ebd")
+      val pink700 = Color.fromHex("#ff9bce")
+      val pink800 = Color.fromHex("#ffbedd")
+      val pink900 = Color.fromHex("#ffcfe6")
+    }
+
+    object Purple {
+      val purple000 = Color.fromHex("#2b1d47")
+      val purple100 = Color.fromHex("#3c2563")
+      val purple200 = Color.fromHex("#553096")
+      val purple300 = Color.fromHex("#6e40c9")
+      val purple400 = Color.fromHex("#8957e5")
+      val purple500 = Color.fromHex("#a371f7")
+      val purple600 = Color.fromHex("#bc8cff")
+      val purple700 = Color.fromHex("#d2a8ff")
+      val purple800 = Color.fromHex("#e2c5ff")
+      val purple900 = Color.fromHex("#e9d6ff")
+    }
+
+    object Primary {
+      val primary000 = Color.fromHex("#1b183d")
+      val primary100 = Color.fromHex("#201d52")
+      val primary200 = Color.fromHex("#2a2375")
+      val primary300 = Color.fromHex("#342a9c")
+      val primary400 = Color.fromHex("#4436c7")
+      val primary500 = Color.fromHex("#5c49eb")
+      val primary600 = Color.fromHex("#7766e8")
+      val primary700 = Color.fromHex("#a498ed")
+      val primary800 = Color.fromHex("#c9c2f2")
+      val primary900 = Color.fromHex("#e4e0f5")
+    }
+
+    object Red {
+      val red000 = Color.fromHex("#3d1515")
+      val red100 = Color.fromHex("#541c1f")
+      val red200 = Color.fromHex("#732225")
+      val red300 = Color.fromHex("#962926")
+      val red400 = Color.fromHex("#bd3939")
+      val red500 = Color.fromHex("#e0514a")
+      val red600 = Color.fromHex("#f76f65")
+      val red700 = Color.fromHex("#ff8d82")
+      val red800 = Color.fromHex("#ffb1a8")
+      val red900 = Color.fromHex("#ffd2cc")
+    }
+
+    object Yellow {
+      val yellow000 = Color.fromHex("#30250a")
+      val yellow100 = Color.fromHex("#473510")
+      val yellow200 = Color.fromHex("#664b14")
+      val yellow300 = Color.fromHex("#856018")
+      val yellow400 = Color.fromHex("#a3751a")
+      val yellow500 = Color.fromHex("#bf8a1f")
+      val yellow600 = Color.fromHex("#d4a035")
+      val yellow700 = Color.fromHex("#e8bb51")
+      val yellow800 = Color.fromHex("#f2d06d")
+      val yellow900 = Color.fromHex("#fae296")
+    }
+  }
+}
