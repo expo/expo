bc06a673635516add95c40f60f6e18ad9843bd9e
Wiktor Smaga <wiktor.smaga13@gmail.com>
2025-06-17 09:39:31 +0200

    Remove size option

--- packages/expo-file-system/src/FileSystem.types.ts
+++ packages/expo-file-system/src/FileSystem.types.ts
@@ -204,7 +204,7 @@ export type FileInfo =
        */
       uri: string;
       /**
-       * The size of the file in bytes. If operating on a source such as an iCloud file, only present if the `size` option was truthy.
+       * The size of the file in bytes.
        */
       size: number;
       /**
@@ -284,12 +284,6 @@ export type InfoOptions = {
    * @default false
    */
   md5?: boolean;
-  /**
-   * Explicitly specify that the file size should be included. For example, skipping this can prevent downloading the file if it's stored in iCloud.
-   * The size is always returned for `file://` locations.
-   * @deprecated
-   */
-  size?: boolean;
 };
 
 export type RelocatingOptions = {
