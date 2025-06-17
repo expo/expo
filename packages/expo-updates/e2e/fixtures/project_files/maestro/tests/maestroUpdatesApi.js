021dfcd702bc89f21f1db3e430f034c5bfe96240
undefined <undefined>
2025-06-16 21:33:35 -0700

    Updates E2E Maestro tests and scripts

---
+++ packages/expo-updates/e2e/fixtures/project_files/maestro/tests/maestroUpdatesApi.js
@@ -0,0 +1,42 @@
+const serverBaseUrl = 'http://localhost:4747';
+
+function serveManifest(name, platform) {
+  var requestString = `${serverBaseUrl}/serve-manifest?name=${name}&platform=${platform}`;
+  const response = http.get(requestString);
+  return response.body;
+}
+
+function lastRequestHeaders() {
+  var requestString = `${serverBaseUrl}/last-request-headers`;
+  const response = http.get(requestString);
+  return JSON.parse(response.body);
+}
+
+function stopUpdatesServer() {
+  http.get(`${serverBaseUrl}/stop-server`);
+}
+
+function restartUpdatesServer() {
+  http.get(`${serverBaseUrl}/restart-server`);
+  // Delay 0.5 second to allow server restart to complete
+  http.get(`${serverBaseUrl}/delay?ms=500`);
+}
+
+function delay(ms) {
+  http.get(`${serverBaseUrl}/delay?ms=${ms}`);
+}
+
+function staticFileCount() {
+  var requestString = `${serverBaseUrl}/static-file-count`;
+  const response = http.get(requestString);
+  return JSON.parse(response.body).count;
+}
+
+output.api = {
+  delay: delay,
+  lastRequestHeaders: lastRequestHeaders,
+  restartUpdatesServer: restartUpdatesServer,
+  serveManifest: serveManifest,
+  staticFileCount: staticFileCount,
+  stopUpdatesServer: stopUpdatesServer,
+};
---
+++ packages/expo-updates/e2e/fixtures/project_files/maestro/updates-e2e-enabled.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [[ "$#" -ne 2 ]]; then
+  echo "Usage: $0 <ios|android> <debug|release>"
+  exit 1
+fi
+
+export MAESTRO_PLATFORM=$1
+export E2E_CONFIGURATION=$2
+
+function killUpdatesServerIfNeeded() {
+  UPDATES_SERVER_PID=$(lsof -t -i:4747)
+  if [[ -n "$UPDATES_SERVER_PID" ]]; then
+    echo "Killing updates server with PID $UPDATES_SERVER_PID"
+    kill -9 $UPDATES_SERVER_PID
+  fi
+}
+
+function cleanup()
+{
+  echo 'Cleaning up...'
+  killUpdatesServerIfNeeded
+  yarn maestro:$MAESTRO_PLATFORM:uninstall || true
+}
+
+# Fail if anything errors
+set -eox pipefail
+# If this script exits, trap it first and clean up
+trap cleanup EXIT
+
+function beforeAll() {
+  npx ts-node ./maestro/updates-server/start.ts &
+}
+
+function beforeTest()
+{
+  yarn maestro:$MAESTRO_PLATFORM:uninstall || true
+  yarn maestro:$MAESTRO_PLATFORM:$E2E_CONFIGURATION:install
+  sleep 2
+}
+
+beforeAll
+beforeTest
+maestro test maestro/tests/startAndStop.yml
+beforeTest
+maestro test maestro/tests/checkLastRequestHeaders.yml
+beforeTest
+maestro test maestro/tests/reload.yml
+beforeTest
+maestro test maestro/tests/runUpdate.yml
+beforeTest
+maestro test maestro/tests/runUpdateWithInvalidHash.yml
+beforeTest
+maestro test maestro/tests/runUpdateWithInvalidAssetHash.yml
+beforeTest
+maestro test maestro/tests/runUpdateWithMultipleAssets.yml
+beforeTest
+maestro test maestro/tests/runUpdateWithOldCommitTime.yml
+beforeTest
+maestro test maestro/tests/runUpdateWithRollback.yml
+beforeTest
+maestro test maestro/tests/runUpdateWithJSAPI.yml
