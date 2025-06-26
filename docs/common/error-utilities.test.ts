728829196d93fc898696467249c6530f3c22622c
Aman Mittal <amandeepmittal@live.com>
2025-06-26 19:21:00 +0530

    Update error-utilities.test.ts

--- docs/common/error-utilities.test.ts
+++ docs/common/error-utilities.test.ts
@@ -1,62 +1,62 @@
 import { getRedirectPath } from './error-utilities';
 
 test('redirects old building-standalone-apps paths versioned path', () => {
-  const redirectPath = '/versions/latest/distribution/building-standalone-apps/';
+  const redirectPath = '/versions/latest/distribution/building-standalone-apps';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/build/setup/');
+  expect(newPath).toEqual('/build/setup');
 
   // The path with guides instead of distribution is very old
-  expect(getRedirectPath('/versions/latest/guides/building-standalone-apps/')).toEqual(newPath);
+  expect(getRedirectPath('/versions/latest/guides/building-standalone-apps')).toEqual(newPath);
 });
 
 test('redirects version vX.0.0 renamed path', () => {
-  const redirectPath = '/versions/v32.0.0/guides/push-notifications/';
+  const redirectPath = '/versions/v32.0.0/guides/push-notifications';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/push-notifications/overview/');
+  expect(newPath).toEqual('/push-notifications/overview');
 });
 
 test('redirects version latest renamed path', () => {
-  const redirectPath = '/versions/latest/guides/push-notifications/';
+  const redirectPath = '/versions/latest/guides/push-notifications';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/push-notifications/overview/');
+  expect(newPath).toEqual('/push-notifications/overview');
 });
 
 test('redirects versionless renamed path', () => {
-  const redirectPath = '/guides/push-notifications/';
+  const redirectPath = '/guides/push-notifications';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/push-notifications/overview/');
+  expect(newPath).toEqual('/push-notifications/overview');
 });
 
 test('redirects versioned non-renamed path', () => {
-  const redirectPath = '/versions/latest/workflow/expo-cli/';
+  const redirectPath = '/versions/latest/workflow/expo-cli';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/more/expo-cli/');
+  expect(newPath).toEqual('/more/expo-cli');
 });
 
 test('does not redirect non-renamed path', () => {
-  const redirectPath = '/workflow/expo-cli/';
+  const redirectPath = '/workflow/expo-cli';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/more/expo-cli/');
+  expect(newPath).toEqual('/more/expo-cli');
 });
 
 test('adds forward slash to end of path', () => {
   const redirectPath = '/workflow/expo-cli';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/more/expo-cli/');
+  expect(newPath).toEqual('/more/expo-cli');
 });
 
 test('redirects old versions to latest', () => {
-  const redirectPath = '/versions/v32.0.0/sdk/camera/';
+  const redirectPath = '/versions/v32.0.0/sdk/camera';
   const newPath = getRedirectPath(redirectPath);
 
-  expect(newPath).toEqual('/versions/latest/sdk/camera/');
+  expect(newPath).toEqual('/versions/latest/sdk/camera');
 });
 
 test('removes null from end of paths', () => {
@@ -67,5 +67,5 @@ test('removes null from end of paths', () => {
 });
 
 test('redirect SDK permissions to the permission guide', () => {
-  expect(getRedirectPath('/versions/latest/sdk/permissions/')).toEqual('/guides/permissions/');
+  expect(getRedirectPath('/versions/latest/sdk/permissions')).toEqual('/guides/permissions');
 });
