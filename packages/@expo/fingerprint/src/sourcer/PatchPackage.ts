25ba760b4e0f14fa168e5cdabe047def61ca1aee
Hugo EXTRAT <extrat.h@gmail.com>
2025-06-30 03:21:25 +0200

    [fingerprint] Respects ignore paths for patches dir (#37702)
    # Why
    
    fixes: https://github.com/expo/expo/issues/37667
    
    Ensures the fingerprinting process respects ignore paths when searching
    for patch-package files. This prevents unnecessary processing of ignored
    directories.

--- packages/@expo/fingerprint/src/sourcer/PatchPackage.ts
+++ packages/@expo/fingerprint/src/sourcer/PatchPackage.ts
@@ -2,6 +2,7 @@ import chalk from 'chalk';
 
 import { getFileBasedHashSourceAsync } from './Utils';
 import type { HashSource, NormalizedOptions } from '../Fingerprint.types';
+import { isIgnoredPathWithMatchObjects } from '../utils/Path';
 
 const debug = require('debug')('expo:fingerprint:sourcer:PatchPackage');
 
@@ -9,6 +10,10 @@ export async function getPatchPackageSourcesAsync(
   projectRoot: string,
   options: NormalizedOptions
 ): Promise<HashSource[]> {
+  if (isIgnoredPathWithMatchObjects('patches', options.ignoreDirMatchObjects)) {
+    debug(`Skipping dir - ${chalk.dim('patches')} (ignored by ignoreDirMatchObjects)`);
+    return [];
+  }
   const result = await getFileBasedHashSourceAsync(projectRoot, 'patches', 'patchPackage');
   if (result != null) {
     debug(`Adding dir - ${chalk.dim('patches')}`);
