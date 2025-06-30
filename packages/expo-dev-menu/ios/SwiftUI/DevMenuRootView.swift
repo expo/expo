f6ab0c185a10d50d62deae59213a98861c0be14b
Alan Hughes <alan@expo.io>
2025-06-30 12:25:47 +0100

    [ios][dev-menu] Onboarding view ui

--- packages/expo-dev-menu/ios/SwiftUI/DevMenuRootView.swift
+++ packages/expo-dev-menu/ios/SwiftUI/DevMenuRootView.swift
@@ -6,23 +6,23 @@ struct DevMenuRootView: View {
   @StateObject private var viewModel = DevMenuViewModel()
 
   var body: some View {
-    ZStack {
-      NavigationView {
-        VStack(spacing: 0) {
-          HeaderView()
-            .environmentObject(viewModel)
+    NavigationView {
+      VStack(spacing: 0) {
+        HeaderView()
+          .environmentObject(viewModel)
 
-          Divider()
+        Divider()
 
+        ZStack {
           DevMenuMainView()
             .environmentObject(viewModel)
-        }
-        .background(Color(.systemGroupedBackground))
-      }
 
-      if !viewModel.isOnboardingFinished {
-        DevMenuOnboardingView(onFinish: viewModel.finishOnboarding)
+          if !viewModel.isOnboardingFinished {
+            DevMenuOnboardingView(onFinish: viewModel.finishOnboarding)
+          }
+        }
       }
+      .background(Color(.systemGroupedBackground))
     }
   }
 }
