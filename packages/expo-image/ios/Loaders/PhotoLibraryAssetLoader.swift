4c98c64299e272440e6e1aa6157246e9f7876799
aleqsio <mikucki@gmail.com>
2025-07-02 12:34:47 +0200

    fix

--- packages/expo-image/ios/Loaders/PhotoLibraryAssetLoader.swift
+++ packages/expo-image/ios/Loaders/PhotoLibraryAssetLoader.swift
@@ -113,7 +113,6 @@ private func requestAsset(
   if let scale = context?[ImageView.screenScaleKey] as? Double,
      let containerSize = context?[ImageView.frameSizeKey] as? CGSize,
      let contentFit = context?[ImageView.contentFitKey] as? ContentFit {
-
     let targetSize = idealSize(
         contentPixelSize: CGSize(width: asset.pixelWidth, height: asset.pixelHeight),
         containerSize: containerSize,
