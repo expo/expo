3b16fce361c80ac3e203aaa4e4fe559dc3c2b30e
Jakub Tkacz <32908614+Ubax@users.noreply.github.com>
2025-07-08 09:32:18 +0200

    [expo-router] fix screen freeze after preview navigation (#37881)
    # Why
    
    After navigation from preview, a screen was sometimes frozen. This only happened in case when other screen was preloaded before current preview was opened - with prefetch or by opening different preview before.
    
    The reason for the was reshuffling of screens after navigation.
    
    Here is an example of such situation:
    
    Let's say user is on screen B. They first open the preview C and then D. They tap on D, performing native navigation to D.
    
    - visible routes: A, B
    - preloaded routes: C, D
    - screens order before navigation: A,B,C,D
    - screens order after navigation: A,B,D,C
    
    ## Before
    
    https://github.com/user-attachments/assets/c92b5b58-5547-4260-9745-da787c27da68
    
    ## After
    
    https://github.com/user-attachments/assets/a8f6c869-b295-4230-9c69-74d5c5409d47
    
    # How
    
    1. Changing the logic of preload, so that preloaded screens will be added to the beginning of preloaded array. 
    
    # Test Plan
    
    1. Manual testing 
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources according to [this short guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo-router/build/layouts/StackClient.js
+++ packages/expo-router/build/layouts/StackClient.js
@@ -18,7 +18,8 @@ function isStackAction(action) {
         action.type === 'NAVIGATE' ||
         action.type === 'POP' ||
         action.type === 'POP_TO_TOP' ||
-        action.type === 'REPLACE');
+        action.type === 'REPLACE' ||
+        action.type === 'PRELOAD');
 }
 const isPreviewAction = (action) => !!action.payload && 'previewKey' in action.payload && !!action.payload.previewKey;
 /**
@@ -209,6 +210,75 @@ const stackRouterOverride = (original) => {
                     //   routes,
                     // };
                     // END FORK
+                }
+                case 'PRELOAD': {
+                    const getId = options.routeGetIdList[action.payload.name];
+                    const id = getId?.({ params: action.payload.params });
+                    let route;
+                    if (id !== undefined) {
+                        route = state.routes.find((route) => route.name === action.payload.name && id === getId?.({ params: route.params }));
+                    }
+                    if (route) {
+                        return {
+                            ...state,
+                            routes: state.routes.map((r) => {
+                                if (r.key !== route?.key) {
+                                    return r;
+                                }
+                                return {
+                                    ...r,
+                                    params: routeParamList[action.payload.name] !== undefined
+                                        ? {
+                                            ...routeParamList[action.payload.name],
+                                            ...action.payload.params,
+                                        }
+                                        : action.payload.params,
+                                };
+                            }),
+                        };
+                    }
+                    else {
+                        // START FORK
+                        const currentPreloadedRoute = {
+                            key: `${action.payload.name}-${(0, non_secure_1.nanoid)()}`,
+                            name: action.payload.name,
+                            params: routeParamList[action.payload.name] !== undefined
+                                ? {
+                                    ...routeParamList[action.payload.name],
+                                    ...action.payload.params,
+                                }
+                                : action.payload.params,
+                        };
+                        // END FORK
+                        return {
+                            ...state,
+                            // START FORK
+                            // Adding the current preloaded route to the beginning of the preloadedRoutes array
+                            // This ensures that the preloaded route will be the next one after the visible route
+                            // and when navigation will happen, there will be no reshuffling
+                            // This is a workaround for the link preview navigation issue, when screen would freeze after navigation from native side
+                            // and reshuffling from react-navigation
+                            preloadedRoutes: [currentPreloadedRoute].concat(state.preloadedRoutes.filter((r) => r.name !== action.payload.name || id !== getId?.({ params: r.params }))),
+                            // preloadedRoutes: state.preloadedRoutes
+                            //   .filter(
+                            //     (r) =>
+                            //       r.name !== action.payload.name ||
+                            //       id !== getId?.({ params: r.params })
+                            //   )
+                            //   .concat({
+                            //     key: `${action.payload.name}-${nanoid()}`,
+                            //     name: action.payload.name,
+                            //     params:
+                            //       routeParamList[action.payload.name] !== undefined
+                            //         ? {
+                            //             ...routeParamList[action.payload.name],
+                            //             ...action.payload.params,
+                            //           }
+                            //         : action.payload.params,
+                            //   }),
+                            // END FORK
+                        };
+                    }
                 }
                 default: {
                     return original.getStateForAction(state, action, options);
