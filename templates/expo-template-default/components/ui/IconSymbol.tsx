9c2d8406c636778b5f2d0a46ca8b8a171bfc38b2
Vojtech Novak <vonovak@gmail.com>
2025-07-10 16:27:39 +0200

    [templates] remove @expo/vector-icons from default template

--- templates/expo-template-default/components/ui/IconSymbol.tsx
+++ templates/expo-template-default/components/ui/IconSymbol.tsx
@@ -1,6 +1,6 @@
 // Fallback for using MaterialIcons on Android and web.
 
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
 import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
 import { ComponentProps } from 'react';
 import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
