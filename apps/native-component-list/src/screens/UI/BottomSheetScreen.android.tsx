5b95a3e4a93a30dd7c77e8c68b1853d31de207da
Jeroen <jgroenendijk@enrise.com>
2025-07-11 13:44:35 +0200

    [android][ui] add type, fix indentation

--- apps/native-component-list/src/screens/UI/BottomSheetScreen.android.tsx
+++ apps/native-component-list/src/screens/UI/BottomSheetScreen.android.tsx
@@ -1,27 +1,27 @@
-import { Button, BottomSheet } from '@expo/ui/jetpack-compose';
+import {Button, BottomSheet} from '@expo/ui/jetpack-compose';
 import * as React from 'react';
-import { ScrollView, Text } from 'react-native';
+import {ScrollView, Text} from 'react-native';
 
 export default function BottomSheetScreen() {
-    const [isOpened, setIsOpened] = React.useState<boolean>(false);
+  const [isOpened, setIsOpened] = React.useState<boolean>(false);
 
-    return (
-        <ScrollView
-            contentContainerStyle={{
-                flex: 1,
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-                padding: 8,
-            }}>
-            <Button onPress={() => setIsOpened((h) => !h)}>Toggle</Button>
-            <Text>isOpened: {isOpened ? 'yes' : 'no'}</Text>
-            <BottomSheet isOpened={isOpened} onIsOpenedChange={(e) => setIsOpened(e)}>
-                <Text>Hello world</Text>
-            </BottomSheet>
-        </ScrollView>
-    );
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        padding: 8,
+      }}>
+      <Button onPress={() => setIsOpened((h) => !h)}>Toggle</Button>
+      <Text>isOpened: {isOpened ? 'yes' : 'no'}</Text>
+      <BottomSheet isOpened={isOpened} onIsOpenedChange={(e) => setIsOpened(e)}>
+        <Text>Hello world</Text>
+      </BottomSheet>
+    </ScrollView>
+  );
 }
 
 BottomSheetScreen.navigationOptions = {
-    title: 'BottomSheet',
+  title: 'BottomSheet',
 };
