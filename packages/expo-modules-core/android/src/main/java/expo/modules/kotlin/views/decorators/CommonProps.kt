1cfe37b702fbba89171e20dbc97a2f82248c501a
Mateo Guzmán <info@mateoguzman.net>
2025-07-15 23:16:59 +0200

    [android][ui] limit UseCommonProps usage to ExpoComposeView's

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/decorators/CommonProps.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/decorators/CommonProps.kt
@@ -2,23 +2,21 @@
 
 package expo.modules.kotlin.views.decorators
 
-import android.view.View
 import com.facebook.react.uimanager.ViewProps
 import expo.modules.kotlin.types.enforceType
+import expo.modules.kotlin.views.ExpoComposeView
 import expo.modules.kotlin.views.ViewDefinitionBuilder
 
-inline fun <reified T : View> ViewDefinitionBuilder<T>.UseTestIDProp(crossinline body: (view: T, testID: String?) -> Unit) {
+inline fun <reified T : ExpoComposeView<*>> ViewDefinitionBuilder<T>.UseTestIDProp(crossinline body: (view: T, testID: String?) -> Unit) {
   Prop(ViewProps.TEST_ID) { view: T, testID: String? ->
     body(view, testID)
   }
 }
 
-private fun <T : View> ViewDefinitionBuilder<T>.UseTestIDProp() {
-  enforceType<ViewDefinitionBuilder<View>>(this)
+private fun <T : ExpoComposeView<*>> ViewDefinitionBuilder<T>.UseTestIDProp() {
+  enforceType<ViewDefinitionBuilder<ExpoComposeView<*>>>(this)
   UseTestIDProp { view, testID ->
-    if (view is expo.modules.kotlin.views.ExpoComposeView<*>) {
-      view.testID = testID
-    }
+    view.testID = testID
   }
 }
 
@@ -26,6 +24,6 @@ private fun <T : View> ViewDefinitionBuilder<T>.UseTestIDProp() {
  * Decorates the view definition builder with common props.
  */
 @PublishedApi
-internal fun <T : View> ViewDefinitionBuilder<T>.UseCommonProps() {
+internal fun <T : ExpoComposeView<*>> ViewDefinitionBuilder<T>.UseCommonProps() {
   UseTestIDProp()
 }
