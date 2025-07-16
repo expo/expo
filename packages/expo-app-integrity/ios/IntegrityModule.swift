6124e9b2bdbc737f743a5de5a55e8a874efaf6ed
nishan (o^▽^o) <nishanbende@gmail.com>
2025-07-16 20:02:53 +0530

    swiftlint fixes

--- packages/expo-app-integrity/ios/IntegrityModule.swift
+++ packages/expo-app-integrity/ios/IntegrityModule.swift
@@ -4,10 +4,10 @@ import CryptoKit
 
 public class IntegrityModule: Module {
   private let service = DCAppAttestService.shared
-  
+
   public func definition() -> ModuleDefinition {
     Name("ExpoAppIntegrity")
-    
+
     AsyncFunction("generateKey") {
       do {
         return try await service.generateKey()
@@ -15,31 +15,37 @@ public class IntegrityModule: Module {
         throw handleIntegrityCheckError(error)
       }
     }
-    
+
     AsyncFunction("attestKey") { (key: String, challenge: String) in
       let data = Data(challenge.utf8)
       let clientDataHash = Data(SHA256.hash(data: data))
-  
+
       do {
         let result = try await service.attestKey(key, clientDataHash: clientDataHash)
-        return String(decoding: result, as: UTF8.self)
+        guard let attestation = String(data: result, encoding: .utf8) else {
+          throw IntegrityException("Failed to decode attestation result from data")
+        }
+        return attestation
       } catch let error {
         throw handleIntegrityCheckError(error)
       }
     }
-    
+
     AsyncFunction("generateAssertion") { (key: String, challenge: String) -> String in
       let data = Data(challenge.utf8)
       let clientDataHash = Data(SHA256.hash(data: data))
       do {
         let result = try await service.generateAssertion(key, clientDataHash: clientDataHash)
-        return String(decoding: result, as: UTF8.self)
+        guard let assertion = String(data: result, encoding: .utf8) else {
+          throw IntegrityException("Failed to decode assertion result from data")
+        }
+        return assertion
       } catch let error {
         throw handleIntegrityCheckError(error)
       }
     }
   }
-  
+
   private func handleIntegrityCheckError(_ error: Error) -> Exception {
     if let error = error as? DCError {
       switch error.code {
@@ -57,7 +63,7 @@ public class IntegrityModule: Module {
         return IntegrityException("Unknown error")
       }
     }
-    
+
     return IntegrityException("Unknown error")
   }
 }
