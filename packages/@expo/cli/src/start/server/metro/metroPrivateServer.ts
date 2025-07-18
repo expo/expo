d06c9cf683ce3b23b4d2fc954613d89afe222e57
Phil Pluckthun <phil@kitten.sh>
2025-07-18 12:11:50 +0100

    @expo/cli: Switch to `@expo/metro`

--- packages/@expo/cli/src/start/server/metro/metroPrivateServer.ts
+++ packages/@expo/cli/src/start/server/metro/metroPrivateServer.ts
@@ -1,22 +1,22 @@
-import { TransformInputOptions } from 'metro';
-import type Metro from 'metro';
-import { ConfigT } from 'metro-config';
+import type MetroServer from '@expo/metro/metro/Server';
+import type { ConfigT } from '@expo/metro/metro-config';
 import assert from 'node:assert';
 
-export type MetroPrivateServer = import('metro').Server & {
-  _bundler: import('metro/src/IncrementalBundler').default;
+// TODO(@kitten): Delete since private types are exposed on @expo/metro
+export type MetroPrivateServer = MetroServer & {
+  _bundler: import('@expo/metro/metro/IncrementalBundler').default;
   _config: ConfigT;
   _createModuleId: (path: string, context?: { environment?: string; platform: string }) => number;
   _isEnded: boolean;
   _nextBundleBuildNumber: number;
   _platforms: Set<string>;
-  _reporter: import('metro/src/lib/reporting').Reporter;
-  _serverOptions: import('metro').ServerOptions | void;
+  _reporter: import('@expo/metro/metro/lib/reporting').Reporter;
+  _serverOptions: import('@expo/metro/metro/Server').ServerOptions | void;
 
   getNewBuildNumber(): number;
   _getSortedModules(
-    graph: import('metro/src/IncrementalBundler').OutputGraph
-  ): import('metro/src/DeltaBundler/types').Module[];
+    graph: import('@expo/metro/metro/IncrementalBundler').OutputGraph
+  ): import('@expo/metro/metro/DeltaBundler/types.flow').Module[];
 
   _resolveRelativePath(
     filePath: string,
@@ -26,15 +26,15 @@ export type MetroPrivateServer = import('metro').Server & {
       transformOptions,
     }: {
       relativeTo: 'project' | 'server';
-      resolverOptions: import('metro/src/shared/types').ResolverInputOptions;
-      transformOptions: TransformInputOptions;
+      resolverOptions: import('@expo/metro/metro/shared/types.flow').ResolverInputOptions;
+      transformOptions: import('@expo/metro/metro/DeltaBundler/types.flow').TransformInputOptions;
     }
   ): Promise<string>;
 
-  _shouldAddModuleToIgnoreList(module: import('metro/src/DeltaBundler/types').Module<any>): boolean;
+  _shouldAddModuleToIgnoreList(module: import('@expo/metro/metro/DeltaBundler/types.flow').Module): boolean;
 };
 
-export function assertMetroPrivateServer(metro: Metro.Server): asserts metro is MetroPrivateServer {
+export function assertMetroPrivateServer(metro: MetroServer): asserts metro is MetroPrivateServer {
   assert(metro, 'Metro server undefined.');
   assert(
     '_config' in metro && '_bundler' in metro,
