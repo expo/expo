06da462355caa253fb75515d9253418815c88b98
Kadi Kraman <hellokadi@gmail.com>
2025-07-20 12:29:05 +0100

    Add a rule for preferring box shadow

--- packages/eslint-plugin-expo/src/rules/index.ts
+++ packages/eslint-plugin-expo/src/rules/index.ts
@@ -1,9 +1,11 @@
 import { noDynamicEnvVar } from './noDynamicEnvVar';
 import { noEnvVarDestructuring } from './noEnvVarDestructuring';
+import { preferBoxShadow } from './prefer-box-shadow';
 import { useDomExports } from './use-dom-exports';
 
 export const rules = {
   'no-dynamic-env-var': noDynamicEnvVar,
   'no-env-var-destructuring': noEnvVarDestructuring,
+  'prefer-box-shadow': preferBoxShadow,
   'use-dom-exports': useDomExports,
 };
