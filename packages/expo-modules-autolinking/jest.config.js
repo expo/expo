0f16c8dd123c53ecdf5749350632ab46e51cdef5
Phil Pluckthun <phil@kitten.sh>
2025-07-21 13:21:09 +0100

    Mock fs in Jest by default

--- packages/expo-modules-autolinking/jest.config.js
+++ packages/expo-modules-autolinking/jest.config.js
@@ -2,4 +2,6 @@ const sharedPreset = require('expo-module-scripts/jest-preset-plugin');
 module.exports = {
   ...sharedPreset,
   roots: ['__mocks__', 'src', 'e2e'],
+  setupFiles: ['<rootDir>/jest.setup.ts'],
+  clearMocks: true,
 };
