091cd2eb163130bb773450b2cf10815cd383327a
Krystof Woldrich <krystofwoldrich@gmail.com>
2025-07-21 15:52:49 +0200

    add transformations checks to the react compiler playwright tests

--- packages/@expo/cli/e2e/playwright/dev/react-compiler.test.ts
+++ packages/@expo/cli/e2e/playwright/dev/react-compiler.test.ts
@@ -1,9 +1,13 @@
 import { expect, test } from '@playwright/test';
+import klawSync from 'klaw-sync';
+import fs from 'node:fs';
+import path from 'node:path';
 
 import { clearEnv, restoreEnv } from '../../__tests__/export/export-side-effects';
 import { getRouterE2ERoot } from '../../__tests__/utils';
 import { createExpoServe, executeExpoAsync } from '../../utils/expo';
 import { pageCollectErrors } from '../page';
+import { assert } from 'node:console';
 
 test.beforeAll(() => clearEnv());
 test.afterAll(() => restoreEnv());
@@ -40,6 +44,22 @@ test.describe(baseDir, () => {
     });
     test.afterEach(async () => {
       await expoServe.stopAsync();
+    });
+
+    test('bundle contains live bindings', async () => {
+      const jsFiles = klawSync(path.join(projectRoot, inputDir, '_expo/static/js'), {
+        nodir: true,
+      });
+      const bundleFile = jsFiles[0]?.path;
+
+      // Sanity check
+      assert(jsFiles.length === 1, 'This test expects a single JS bundle file to be generated.');
+      assert(bundleFile, 'No JS bundle file found.');
+
+      const bundleContent = fs.readFileSync(bundleFile, 'utf8');
+
+      // The useBananas code which otherwise causes the app to crash uses live bindings.
+      expect(bundleContent).toContain('Object.defineProperty(e,"useBananas",{enumerable:!0,get:function(){return n.useBananas}})}');
     });
 
     // This test generally ensures no errors are thrown during an export loading.
@@ -91,6 +111,23 @@ test.describe(baseDir, () => {
       await expoServe.stopAsync();
     });
 
+    test('bundle does not have live bindings', async () => {
+      const jsFiles = klawSync(path.join(projectRoot, inputDir, '_expo/static/js'), {
+        nodir: true,
+      });
+      const bundleFile = jsFiles[0]?.path;
+
+      // Sanity check
+      assert(jsFiles.length === 1, 'This test expects a single JS bundle file to be generated.');
+      assert(bundleFile, 'No JS bundle file found.');
+
+      const bundleContent = fs.readFileSync(bundleFile, 'utf8');
+
+      // The useBananas code which causes the application to crash uses static bindings.
+      expect(bundleContent).not.toContain('Object.defineProperty(e,"useBananas",{enumerable:!0,get:function(){return n.useBananas}})}');
+      expect(bundleContent).toContain('e.useBananas=function()');
+    });
+
     // This test generally ensures no errors are thrown during an export loading.
     test('fails to load', async ({ page }) => {
       // Listen for console logs and errors
@@ -101,7 +138,14 @@ test.describe(baseDir, () => {
       await page.goto(expoServe.url.href);
       console.timeEnd('Open page');
 
-      await page.waitForTimeout(500); // Wait for the runtime error
+      // Check for errors up to 4 times
+      for (let i = 0; i < 4; i++) {
+        if (pageErrors.errors.length > 0) {
+          break;
+        }
+        console.log(`Waiting for errors, attempt ${i + 1}/4`);
+        await page.waitForTimeout(500);
+      }
 
       expect(pageErrors.errors).toContainEqual(new Error('Prefix is not defined.'));
     });
