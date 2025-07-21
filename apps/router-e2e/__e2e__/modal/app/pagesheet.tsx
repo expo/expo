2def8a2e5672718c4384136abc6822a14e496880
Jakub Tkacz <jtkacz@expo.io>
2025-07-21 12:34:33 +0200

    [expo-router] Improve modal demo

---
+++ apps/router-e2e/__e2e__/modal/app/pagesheet.tsx
@@ -0,0 +1,63 @@
+import { Link, Modal } from 'expo-router';
+import { useState } from 'react';
+import { Button, ScrollView, Text, View } from 'react-native';
+
+import { MutateButton } from '../components/MutateButton';
+
+export default function FormsheetDemos() {
+  return (
+    <ScrollView contentInsetAdjustmentBehavior="automatic" style={{ padding: 20, gap: 12 }}>
+      <PagesheetBaseDemo />
+    </ScrollView>
+  );
+}
+
+function PagesheetBaseDemo() {
+  const [open, setIsOpen] = useState(false);
+  const [count, setCount] = useState(1);
+  return (
+    <>
+      <Button title="Pagesheet" onPress={() => setIsOpen((open) => !open)} />
+      <Modal visible={open} onClose={() => setIsOpen(false)} presentationStyle="pageSheet">
+        <ScrollView
+          style={{ gap: 8 }}
+          contentContainerStyle={{
+            padding: 16,
+            gap: 8,
+          }}
+          automaticallyAdjustsScrollIndicatorInsets
+          contentInsetAdjustmentBehavior="automatic">
+          {Array.from({ length: count }).map((_, index) => (
+            <View
+              key={String(index)}
+              style={{
+                padding: 24,
+                backgroundColor: '#ccc',
+                borderRadius: 16,
+                borderCurve: 'continuous',
+              }}
+            />
+          ))}
+
+          <View
+            style={{
+              gap: 4,
+              flex: 1,
+              flexDirection: 'row',
+            }}>
+            <MutateButton onPress={() => setCount((count) => count + 1)}>Add</MutateButton>
+            <MutateButton onPress={() => setCount(Math.max(count - 1, 0))}>Remove</MutateButton>
+          </View>
+          <Link href="/random">Go to Different Screen</Link>
+          <Button
+            title="Close"
+            onPress={() => {
+              console.log('Closing modal');
+              setIsOpen(false);
+            }}
+          />
+        </ScrollView>
+      </Modal>
+    </>
+  );
+}
