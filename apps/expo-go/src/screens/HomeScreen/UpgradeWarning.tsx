f4b964c3589b29f867716aad44cd0d14a96c3431
Kadi Kraman <hellokadi@gmail.com>
2025-07-21 11:48:48 +0100

    Show the alert conditionally

--- apps/expo-go/src/screens/HomeScreen/UpgradeWarning.tsx
+++ apps/expo-go/src/screens/HomeScreen/UpgradeWarning.tsx
@@ -1,16 +1,70 @@
 import { iconSize, XIcon, InfoIcon, spacing } from '@expo/styleguide-native';
 import { Row, Spacer, Text, useExpoTheme, View } from 'expo-dev-client-components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Pressable, StyleSheet } from 'react-native';
+import Environment from 'src/utils/Environment';
+
+const SDK_VERSION_REGEXP = new RegExp(/\b(\d*)\.0\.0/);
+
+type SdkVersionFromApiType = {
+  androidClientUrl?: string;
+  androidClientVersion?: string;
+  expoVersion?: string;
+  facebookReactNativeVersion?: string;
+  facebookReactVersion?: string;
+  iosClientUrl?: string;
+  iosClientVersion?: string;
+  releaseNoteUrl?: string;
+};
+
+type SdkVersionTypeWithSdkType = SdkVersionFromApiType & {
+  sdk: string;
+  isLatest?: boolean;
+  isBeta?: boolean;
+};
+
+type VersionsApiResponseType = {
+  sdkVersions: Record<string, SdkVersionFromApiType>;
+};
+
+// Show the message if current SDK !== latest SDK AND the latest SDK is yet to be released
+async function shouldShowUpgradeWarningAsync(): Promise<boolean> {
+  const result = await fetch('https://api.expo.dev/v2/versions');
+
+  try {
+    const data: VersionsApiResponseType = await result.json();
+
+    const publishedVersions = Object.keys(data.sdkVersions)
+      .map((sdk) => ({
+        ...data.sdkVersions[sdk],
+        sdk: sdk.match(SDK_VERSION_REGEXP)?.[1],
+      }))
+      .filter((version) => !!version.sdk) as SdkVersionTypeWithSdkType[];
+
+    const lastVersion = publishedVersions[publishedVersions.length - 1];
+    const currentIsOutdated = Environment.supportedSdksString !== lastVersion.sdk;
+    const latestIsBeta = !lastVersion.releaseNoteUrl;
+
+    return Boolean(currentIsOutdated && latestIsBeta);
+  } catch {}
+
+  return false;
+}
 
 export function UpgradeWarning() {
-  const [isDismissed, setIsDismissed] = useState(false);
+  const [shouldShow, setShouldShow] = useState(false);
   const theme = useExpoTheme();
   const dismissUpgradeWarning = () => {
-    setIsDismissed(true);
+    setShouldShow(false);
   };
 
-  if (isDismissed) {
+  useEffect(() => {
+    shouldShowUpgradeWarningAsync().then((shouldShow) => {
+      setShouldShow(shouldShow);
+    });
+  }, []);
+
+  if (shouldShow) {
     return null;
   }
 
