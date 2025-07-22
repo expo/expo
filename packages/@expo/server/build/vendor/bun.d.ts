f28c25351e9b1c28f22440542b1b657c6a9aecfb
Phil Pluckthun <phil@kitten.sh>
2025-07-22 17:34:27 +0100

    Rebuild

--- packages/@expo/server/build/vendor/bun.d.ts
+++ packages/@expo/server/build/vendor/bun.d.ts
@@ -1,4 +1,4 @@
-import { createRequestHandler as createExpoHandler } from '@expo/server';
+import { createRequestHandler as createExpoHandler } from '../index';
 export type RequestHandler = (req: Request) => Promise<Response>;
 /**
  * Returns a request handler for Express that serves the response using Remix.
