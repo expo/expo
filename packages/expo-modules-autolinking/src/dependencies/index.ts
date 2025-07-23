4498214415a4fbdebe7428339a864095fbd56000
Phil Pluckthun <phil@kitten.sh>
2025-07-23 12:42:25 +0100

    Add initial dependency resolution logic

---
+++ packages/expo-modules-autolinking/src/dependencies/index.ts
@@ -0,0 +1,4 @@
+export { scanDependenciesRecursively } from './resolution';
+export { scanDependenciesInSearchPath } from './scanning';
+export { filterMapResolutionResult, mergeResolutionResults } from './utils';
+export type * from './types';
