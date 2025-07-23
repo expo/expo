e7971a024b095388cf01aea4483b3337253dcc25
Phil Pluckthun <phil@kitten.sh>
2025-07-23 13:18:39 +0100

    Add tests for scanning.ts

---
+++ packages/expo-modules-autolinking/src/dependencies/__tests__/scanning-test.ts
@@ -0,0 +1,237 @@
+import { vol } from 'memfs';
+import type { NestedDirectoryJSON } from 'memfs/lib/volume';
+import { join } from 'path';
+
+import { scanDependenciesInSearchPath } from '../scanning';
+
+function mockedNodeModule(
+  name: string,
+  options?: {
+    pkgVersion?: string;
+    pkgDependencies?: Record<string, string>;
+  }
+): NestedDirectoryJSON {
+  return {
+    'package.json': JSON.stringify({
+      name,
+      version: options?.pkgVersion ?? '0.0.1',
+      dependencies: options?.pkgDependencies ?? {},
+    }),
+  };
+}
+
+const projectRoot = '/fake/project';
+
+describe(scanDependenciesInSearchPath, () => {
+  afterEach(() => {
+    vol.reset();
+  });
+
+  it('discovers unscoped and scoped dependencies', async () => {
+    vol.fromNestedJSON(
+      {
+        node_modules: {
+          'react-native-third-party': mockedNodeModule('react-native-third-party'),
+          '@my/expo-module': mockedNodeModule('@my/expo-module'),
+        },
+      },
+      projectRoot
+    );
+
+    const result = await scanDependenciesInSearchPath(join(projectRoot, 'node_modules'));
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "@my/expo-module": {
+          "depth": 0,
+          "duplicates": null,
+          "name": "@my/expo-module",
+          "originPath": "/fake/project/node_modules/@my/expo-module",
+          "path": "/fake/project/node_modules/@my/expo-module",
+        },
+        "react-native-third-party": {
+          "depth": 0,
+          "duplicates": null,
+          "name": "react-native-third-party",
+          "originPath": "/fake/project/node_modules/react-native-third-party",
+          "path": "/fake/project/node_modules/react-native-third-party",
+        },
+      }
+    `);
+  });
+
+  it('discovers symlinked dependencies', async () => {
+    vol.fromNestedJSON(
+      {
+        'react-native-third-party': mockedNodeModule('react-native-third-party'),
+        node_modules: {
+          '.keep': '',
+        },
+      },
+      projectRoot
+    );
+
+    vol.symlinkSync(
+      join(projectRoot, 'react-native-third-party'),
+      join(projectRoot, 'node_modules/react-native-third-party')
+    );
+
+    const result = await scanDependenciesInSearchPath(join(projectRoot, 'node_modules'));
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "react-native-third-party": {
+          "depth": 0,
+          "duplicates": null,
+          "name": "react-native-third-party",
+          "originPath": "/fake/project/node_modules/react-native-third-party",
+          "path": "/fake/project/react-native-third-party",
+        },
+      }
+    `);
+  });
+
+  it('discovers transitive, isolated dependencies', async () => {
+    vol.fromNestedJSON(
+      {
+        ...mockedNodeModule('root', {
+          pkgDependencies: { 'react-native-third-party': '*' },
+        }),
+        node_modules: {
+          '.pnpm': {
+            'react-native-third-party@x.x.x/node_modules': {
+              'react-native-third-party': mockedNodeModule('react-native-third-party'),
+            },
+          },
+        },
+      },
+      projectRoot
+    );
+
+    vol.symlinkSync(
+      join(
+        projectRoot,
+        'node_modules/.pnpm/react-native-third-party@x.x.x/node_modules/react-native-third-party'
+      ),
+      join(projectRoot, 'node_modules/react-native-third-party')
+    );
+
+    const result = await scanDependenciesInSearchPath(join(projectRoot, 'node_modules'));
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "react-native-third-party": {
+          "depth": 0,
+          "duplicates": null,
+          "name": "react-native-third-party",
+          "originPath": "/fake/project/node_modules/react-native-third-party",
+          "path": "/fake/project/node_modules/.pnpm/react-native-third-party@x.x.x/node_modules/react-native-third-party",
+        },
+      }
+    `);
+  });
+
+  it('resolves conflicts as duplicates', async () => {
+    vol.fromNestedJSON(
+      {
+        node_modules: {
+          b: mockedNodeModule('duplicate'),
+          a: mockedNodeModule('duplicate'),
+        },
+      },
+      projectRoot
+    );
+
+    const result = await scanDependenciesInSearchPath(join(projectRoot, 'node_modules'));
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "duplicate": {
+          "depth": 0,
+          "duplicates": [
+            "/fake/project/node_modules/b",
+          ],
+          "name": "duplicate",
+          "originPath": "/fake/project/node_modules/a",
+          "path": "/fake/project/node_modules/a",
+        },
+      }
+    `);
+  });
+
+  it('ignores missing package.json files on dependencies', async () => {
+    vol.fromNestedJSON(
+      {
+        node_modules: {
+          'react-native-third-party': { '.keep': '' },
+          '@my/expo-module': { '.keep': '' },
+        },
+      },
+      projectRoot
+    );
+
+    const result = await scanDependenciesInSearchPath(join(projectRoot, 'node_modules'));
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "@my/expo-module": {
+          "depth": 0,
+          "duplicates": null,
+          "name": "@my/expo-module",
+          "originPath": "/fake/project/node_modules/@my/expo-module",
+          "path": "/fake/project/node_modules/@my/expo-module",
+        },
+        "react-native-third-party": {
+          "depth": 0,
+          "duplicates": null,
+          "name": "react-native-third-party",
+          "originPath": "/fake/project/node_modules/react-native-third-party",
+          "path": "/fake/project/node_modules/react-native-third-party",
+        },
+      }
+    `);
+  });
+
+  it('ignores files', async () => {
+    vol.fromNestedJSON(
+      {
+        node_modules: {
+          'react-native-third-party': 'content',
+          '@my/expo-module': 'content',
+        },
+      },
+      projectRoot
+    );
+
+    const result = await scanDependenciesInSearchPath(join(projectRoot, 'node_modules'));
+    expect(result).toEqual({});
+  });
+
+  it('ignores dependency names from filter', async () => {
+    vol.fromNestedJSON(
+      {
+        node_modules: {
+          ignored: mockedNodeModule('ignored'),
+          included: mockedNodeModule('included'),
+        },
+      },
+      projectRoot
+    );
+
+    const result = await scanDependenciesInSearchPath(join(projectRoot, 'node_modules'), {
+      shouldIncludeDependency: (dependencyName) => dependencyName !== 'ignored',
+    });
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "included": {
+          "depth": 0,
+          "duplicates": null,
+          "name": "included",
+          "originPath": "/fake/project/node_modules/included",
+          "path": "/fake/project/node_modules/included",
+        },
+      }
+    `);
+  });
+});
