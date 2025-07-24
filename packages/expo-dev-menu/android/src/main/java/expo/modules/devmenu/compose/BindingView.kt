85e8bd7e636ca82a3512aad4a2bada6d1313d2ce
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2025-07-24 10:52:00 +0100

    [dev-menu][android] Add FAB to pull up the dev menu (#38246)
    # Why
    
    Adds a Floating Action Button that shows the dev menu. This is mostly
    necessary for Oculus Quest support, but also makes development easier on
    Android phones.
    
    # How
    
    Added a custom FAB to the view hierarchy. It uses spring-based
    animations and snaps to one of the sides of the phone based on movement
    speed and current position.
    
    # Test Plan
    
    Tested in BareExpo on Android and Quest
    
    
    
    https://github.com/user-attachments/assets/8b91c734-ecb9-49ec-9dff-93216e8917b4

--- packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/compose/BindingView.kt
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/compose/BindingView.kt
@@ -5,17 +5,20 @@ import android.content.Context
 import android.widget.LinearLayout
 import androidx.compose.ui.platform.ComposeView
 import expo.modules.devmenu.compose.theme.AppTheme
+import expo.modules.devmenu.fab.ComposeMovableFloatingActionButton
 
 @SuppressLint("ViewConstructor")
 class BindingView(context: Context, lazyViewModel: Lazy<DevMenuViewModel>) : LinearLayout(context) {
   val viewModel by lazyViewModel
 
   init {
+    z = Float.MAX_VALUE
     addView(
       ComposeView(context).apply {
         setContent {
           AppTheme {
             DevMenuScreen(viewModel.state, viewModel::onAction)
+            ComposeMovableFloatingActionButton(context, viewModel.state)
           }
         }
       }
