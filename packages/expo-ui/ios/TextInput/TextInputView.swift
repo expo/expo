169cc626799ca5f1fcb6b2eda0df58b22f6c7cb1
João Pedro <joaomorais.ext@e-ai.com.br>
2025-07-28 10:21:45 -0300

    feat(TKDS-568): Finalização da implementação da máscara e estilos e limpeza de arquivos da lib de máscara

--- packages/expo-ui/ios/TextInput/TextInputView.swift
+++ packages/expo-ui/ios/TextInput/TextInputView.swift
@@ -77,8 +77,10 @@ struct TextInputView: ExpoSwiftUI.View {
 
   var body: some View {
       
+    let maskFormatter = MaskFormatter(mask: props.mask ?? "")
+      
     if props.secureEntry{
-        SecureField(props.defaultValue, text: $value)
+        SecureField(props.placeholder, text: $value)
             .onChange(of: value) { newValue in
               props.onValueChanged(["value": "newValue"])
             }
@@ -104,7 +106,7 @@ struct TextInputView: ExpoSwiftUI.View {
             .onAppear { value = props.defaultValue }
             .onChange(of: value) { newValue in
               if let mask = props.mask{
-                  value = MaskManager().applyMask(text: newValue,mask:mask)
+                  value = maskFormatter.applyMask(text: newValue,mask:mask)
               }
               props.onValueChanged(["value": "newValue"])
             }
@@ -124,46 +126,5 @@ struct TextInputView: ExpoSwiftUI.View {
           Text("Unsupported iOS version. Please update your iOS version to use this feature.")
         }
     }
-  }
-}
-
-private struct TextStyleModifier: ViewModifier {
-  let style: TextStyleProps?
-  
-  func body(content: Content) -> some View {
-    var view = AnyView(content)
-    
-    guard let ts = style else {
-      return view
-    }
-    if let color = ts.color{
-        view = AnyView(view.foregroundColor(Color.convert(color)))
-    }
-    if let letterSpacing = ts.letterSpacing{
-        if #available(iOS 16.0, *) {
-          view = AnyView(view.tracking(CGFloat(letterSpacing)))
-        }
-    }
-    let fontSize = CGFloat(ts.size ?? 15)
-    if let fontFamily = ts.fontFamily {
-        view = AnyView(view.font(.custom(fontFamily, size: fontSize)))
-    } else {
-        view = AnyView(view.font(.system(size: fontSize)))
-    }
-    if let weight = ts.fontWeight{
-        if #available(iOS 16.0, *) {
-            view = AnyView(view.fontWeight(getFontWeight(weight)))
-        }
-    }
-    if let height = ts.lineHeight {
-      view = AnyView(view.lineSpacing(CGFloat(height)))
-    }
-    return view
-  }
-}
-
-private extension View {
-  func applyTextStyle(_ style: TextStyleProps?) -> some View {
-    self.modifier(TextStyleModifier(style: style))
   }
 }
