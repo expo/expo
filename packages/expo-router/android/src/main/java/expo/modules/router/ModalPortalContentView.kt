1136f7a804d4fb399895d7a3e3dcffca3fea11bb
Jakub Tkacz <jtkacz@expo.io>
2025-07-28 16:28:40 +0200

    [expo-router] shadow node RootKind

--- packages/expo-router/android/src/main/java/expo/modules/router/ModalPortalContentView.kt
+++ packages/expo-router/android/src/main/java/expo/modules/router/ModalPortalContentView.kt
@@ -1,6 +1,8 @@
 package expo.modules.router
 
 import android.content.Context
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.uimanager.StateWrapper
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
 import java.lang.ref.WeakReference
@@ -12,6 +14,11 @@ class ModalPortalContentView(context: Context, appContext: AppContext) :
 
   fun setHostComponent(hostComponent: ModalPortalHostView) {
     this.hostComponent = WeakReference(hostComponent)
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    super.stateWrapper?.updateState(Arguments.makeNativeMap(mapOf("isRootKind" to true)))
   }
 
   override fun onDetachedFromWindow() {
