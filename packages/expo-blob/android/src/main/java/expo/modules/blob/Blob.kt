a16755986e468337766cf3cd04081dfc670eb57d
HubertBer <bernackihubert03@gmail.com>
2025-07-28 12:35:48 +0200

    reformatting

--- packages/expo-blob/android/src/main/java/expo/modules/blob/Blob.kt
+++ packages/expo-blob/android/src/main/java/expo/modules/blob/Blob.kt
@@ -10,178 +10,179 @@ import expo.modules.kotlin.types.Enumerable
 import java.io.ByteArrayOutputStream
 
 class Blob() : SharedObject() {
-    var blobParts: List<InternalBlobPart> = listOf()
-    var type: String = ""
-    val size: Int by lazy {
-        var size = 0
-        for (bp in blobParts) {
-            size += bp.size()
-        }
-        return@lazy size
+  var blobParts: List<InternalBlobPart> = listOf()
+  var type: String = ""
+  val size: Int by lazy {
+    var size = 0
+    for (bp in blobParts) {
+      size += bp.size()
+    }
+    return@lazy size
+  }
+
+  constructor(blobParts: List<InternalBlobPart>, type: String) : this() {
+    this.blobParts = blobParts
+    this.type = if (validType(type)) type.lowercase() else ""
+  }
+
+  fun bytesToStream(byteStream: ByteArrayOutputStream) {
+    for (bp in blobParts) {
+      bp.bytesToStream(byteStream)
+    }
+  }
+
+  fun bytes(): ByteArray {
+    val byteStream = ByteArrayOutputStream(size)
+    bytesToStream(byteStream)
+    return byteStream.toByteArray()
+  }
+
+  private fun InternalBlobPart.offsetSlice(start: Int, end: Int, offset: Int): InternalBlobPart {
+    var s: Int = start - offset
+    var e: Int = end - offset
+    if (s <= 0 && e >= size()) {
+      return this
+    }
+    if (s < 0) {
+      s = 0
+    }
+    if (e > this.size()) {
+      e = this.size()
+    }
+    return when (this) {
+      is InternalBlobPart.StringPart -> InternalBlobPart.StringPart(string.substring(s, e))
+      is InternalBlobPart.BlobPart -> InternalBlobPart.BlobPart(blob.slice(s, e, ""))
+      is InternalBlobPart.BufferPart -> InternalBlobPart.BufferPart(
+        buffer.slice(s..<e).toByteArray()
+      )
+    }
+  }
+
+  fun slice(start: Int, end: Int, contentType: String): Blob {
+    if (start <= 0 && end >= size) {
+      return Blob(blobParts, contentType)
+    }
+    if (start >= end) {
+      return Blob(listOf(), contentType)
+    }
+    var i: Int = 0
+    val bps: MutableList<InternalBlobPart> = mutableListOf()
+
+    for (bp in blobParts) {
+      if (i + bp.size() <= start) {
+        i += bp.size()
+        continue
+      }
+      if (i >= end) {
+        break
+      }
+      bps.add(bp.offsetSlice(start, end, i))
+      i += bp.size()
     }
 
-    constructor(blobParts: List<InternalBlobPart>, type: String) : this() {
-        this.blobParts = blobParts
-        this.type = if(validType(type)) type.lowercase() else ""
-    }
-
-    fun bytesToStream(byteStream: ByteArrayOutputStream) {
-        for (bp in blobParts) {
-            bp.bytesToStream(byteStream)
-        }
-    }
-
-    fun bytes(): ByteArray {
-        val byteStream = ByteArrayOutputStream(size)
-        bytesToStream(byteStream)
-        return byteStream.toByteArray()
-    }
-
-    private fun InternalBlobPart.offsetSlice(start: Int, end: Int, offset: Int): InternalBlobPart {
-        var s: Int = start - offset
-        var e: Int = end - offset
-        if (s <= 0 && e >= size()) {
-          return this
-        }
-        if (s < 0) {
-            s = 0
-        }
-        if (e > this.size()) {
-            e = this.size()
-        }
-        return when (this) {
-            is InternalBlobPart.StringPart -> InternalBlobPart.StringPart(string.substring(s, e))
-            is InternalBlobPart.BlobPart -> InternalBlobPart.BlobPart(blob.slice(s, e, ""))
-            is InternalBlobPart.BufferPart -> InternalBlobPart.BufferPart(
-                buffer.slice(s..<e).toByteArray()
-            )
-        }
-    }
-
-    fun slice(start: Int, end: Int, contentType: String): Blob {
-        if(start <= 0 && end >= size) {
-            return Blob(blobParts, contentType)
-        }
-        if (start >= end) {
-          return Blob(listOf(), contentType)
-        }
-        var i: Int = 0
-        val bps: MutableList<InternalBlobPart> = mutableListOf()
-
-        for (bp in blobParts) {
-            if (i + bp.size() <= start) {
-                i += bp.size()
-                continue
-            }
-            if (i >= end) {
-                break
-            }
-            bps.add(bp.offsetSlice(start, end, i))
-            i += bp.size()
-        }
-
-        return Blob(bps, contentType)
-    }
+    return Blob(bps, contentType)
+  }
 }
 
-private fun validType(type : String): Boolean {
-    Log.d("UT", "type: " + type + ", type length: " + type.length.toString())
-    for (c in type) {
-        if (c.code < 0x20 || c.code > 0x7E) {
-            return false;
-        }
+private fun validType(type: String): Boolean {
+  Log.d("UT", "type: " + type + ", type length: " + type.length.toString())
+  for (c in type) {
+    if (c.code < 0x20 || c.code > 0x7E) {
+      return false
     }
-    return true;
+  }
+  return true
 }
 
 typealias BlobPart = EitherOfThree<String, Blob, TypedArray>
 
 private fun TypedArray.bytes(): ByteArray {
-    val ba = ByteArray(this.byteLength)
+  val ba = ByteArray(this.byteLength)
 
-    for (i in 0..<this.byteLength) {
-        ba[i] = this.readByte(i)
-    }
+  for (i in 0..<this.byteLength) {
+    ba[i] = this.readByte(i)
+  }
 
-    return ba
+  return ba
 }
 
 const val CR = '\r'
 const val LF = '\n'
 private fun String.toNativeNewlines(): String {
-    var i = 0
-    var str = ""
+  var i = 0
+  var str = ""
 
-    while (i < this.length) {
-        if (this[i] == CR) {
-            str += LF
-            i += 1
-            if (i < this.length && this[i] == LF) {
-                i += 1
-            }
-            continue
-        }
-        str += this[i]
+  while (i < this.length) {
+    if (this[i] == CR) {
+      str += LF
+      i += 1
+      if (i < this.length && this[i] == LF) {
         i += 1
+      }
+      continue
     }
-    return str
+    str += this[i]
+    i += 1
+  }
+  return str
 }
 
 internal fun List<BlobPart>.internal(nativeNewlines: Boolean): List<InternalBlobPart> {
-    return this.map() { bp: BlobPart ->
-        if (bp.`is`(String::class)) {
-            bp.get(String::class).let {
-                val str = if (nativeNewlines) {
-                    it.toNativeNewlines()
-                } else {
-                    it
-                }
-                InternalBlobPart.StringPart(str)
-            }
-        } else if (bp.`is`(Blob::class)) {
-            bp.get(Blob::class).let {
-                InternalBlobPart.BlobPart(it)
-            }
+  return this.map { bp: BlobPart ->
+    if (bp.`is`(String::class)) {
+      bp.get(String::class).let {
+        val str = if (nativeNewlines) {
+          it.toNativeNewlines()
         } else {
-            bp.get(TypedArray::class).let {
-                InternalBlobPart.BufferPart(it.bytes())
-            }
+          it
         }
+        InternalBlobPart.StringPart(str)
+      }
+    } else if (bp.`is`(Blob::class)) {
+      bp.get(Blob::class).let {
+        InternalBlobPart.BlobPart(it)
+      }
+    } else {
+      bp.get(TypedArray::class).let {
+        InternalBlobPart.BufferPart(it.bytes())
+      }
     }
+  }
 }
 
 sealed class InternalBlobPart() {
-    data class StringPart(val string: String) : InternalBlobPart()
-    data class BlobPart(val blob: Blob) : InternalBlobPart()
-    data class BufferPart(val buffer: ByteArray) : InternalBlobPart()
+  data class StringPart(val string: String) : InternalBlobPart()
+  data class BlobPart(val blob: Blob) : InternalBlobPart()
+  data class BufferPart(val buffer: ByteArray) : InternalBlobPart()
 
-    fun size(): Int {
-        return when (this) {
-            is StringPart -> string.toByteArray().size
-            is BlobPart -> blob.size
-            is BufferPart -> buffer.size
-        }
+  fun size(): Int {
+    return when (this) {
+      is StringPart -> string.toByteArray().size
+      is BlobPart -> blob.size
+      is BufferPart -> buffer.size
     }
+  }
 
-    fun bytesToStream(byteStream: ByteArrayOutputStream) {
-        when (this) {
-            is StringPart -> byteStream.write(string.toByteArray())
-            is BlobPart -> blob.bytesToStream(byteStream)
-            is BufferPart -> byteStream.write(buffer)
-        }
+  fun bytesToStream(byteStream: ByteArrayOutputStream) {
+    when (this) {
+      is StringPart -> byteStream.write(string.toByteArray())
+      is BlobPart -> blob.bytesToStream(byteStream)
+      is BufferPart -> byteStream.write(buffer)
     }
+  }
 }
 
 enum class EndingType(val str: String = "transparent") : Enumerable {
-    TRANSPARENT("transparent"), NATIVE("native"),
+  TRANSPARENT("transparent"),
+  NATIVE("native")
 }
 
 internal const val DEFAULT_TYPE = ""
 
 class BlobOptionsBag : Record {
-    @Field
-    val type: String = ""
+  @Field
+  val type: String = ""
 
-    @Field
-    val endings: EndingType = EndingType.TRANSPARENT
+  @Field
+  val endings: EndingType = EndingType.TRANSPARENT
 }
--- packages/expo-blob/android/src/main/java/expo/modules/blob/BlobModule.kt
+++ packages/expo-blob/android/src/main/java/expo/modules/blob/BlobModule.kt
@@ -2,52 +2,51 @@ package expo.modules.blob
 
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import java.io.ByteArrayOutputStream
 import kotlin.math.max
 import kotlin.math.min
 
 class BlobModule : Module() {
-    override fun definition() = ModuleDefinition {
-        Name("ExpoBlob")
+  override fun definition() = ModuleDefinition {
+    Name("ExpoBlob")
 
-        Class(Blob::class) {
-            Constructor() { blobParts: List<BlobPart>?, options: BlobOptionsBag? ->
-                val type = options?.type ?: DEFAULT_TYPE
-                val endings = options?.endings ?: EndingType.TRANSPARENT
-                Blob((blobParts ?: listOf()).internal(endings == EndingType.NATIVE), type)
-            }
+    Class(Blob::class) {
+      Constructor { blobParts: List<BlobPart>?, options: BlobOptionsBag? ->
+        val type = options?.type ?: DEFAULT_TYPE
+        val endings = options?.endings ?: EndingType.TRANSPARENT
+        Blob((blobParts ?: listOf()).internal(endings == EndingType.NATIVE), type)
+      }
 
-            Property("size") { blob: Blob ->
-                blob.size
-            }
+      Property("size") { blob: Blob ->
+        blob.size
+      }
 
-            Property("type") { blob: Blob ->
-                blob.type
-            }
+      Property("type") { blob: Blob ->
+        blob.type
+      }
 
-            Function("slice") { blob: Blob, start: Int?, end: Int?, contentType: String? ->
-                var sliceStart: Int = start ?: 0
-                var sliceEnd: Int = end ?: blob.size
-                if (sliceStart < 0) {
-                    sliceStart = max(blob.size + sliceStart, 0)
-                } else {
-                  sliceStart = min(sliceStart, blob.size)
-                }
-                if (sliceEnd < 0) {
-                    sliceEnd = max(blob.size + sliceEnd, 0)
-                } else {
-                  sliceEnd = min(sliceEnd, blob.size)
-                }
-                blob.slice(sliceStart, sliceEnd, contentType ?: "")
-            }
-
-            AsyncFunction("bytes") { blob: Blob ->
-                blob.bytes()
-            }
-
-            AsyncFunction("text") { blob: Blob ->
-                blob.bytes().decodeToString()
-            }
+      Function("slice") { blob: Blob, start: Int?, end: Int?, contentType: String? ->
+        var sliceStart: Int = start ?: 0
+        var sliceEnd: Int = end ?: blob.size
+        if (sliceStart < 0) {
+          sliceStart = max(blob.size + sliceStart, 0)
+        } else {
+          sliceStart = min(sliceStart, blob.size)
         }
+        if (sliceEnd < 0) {
+          sliceEnd = max(blob.size + sliceEnd, 0)
+        } else {
+          sliceEnd = min(sliceEnd, blob.size)
+        }
+        blob.slice(sliceStart, sliceEnd, contentType ?: "")
+      }
+
+      AsyncFunction("bytes") { blob: Blob ->
+        blob.bytes()
+      }
+
+      AsyncFunction("text") { blob: Blob ->
+        blob.bytes().decodeToString()
+      }
     }
+  }
 }
