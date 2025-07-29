94f54ce83f9a2f5620cafc3d20ddb6cd333a83a6
aleqsio <mikucki@gmail.com>
2025-07-29 15:51:47 +0200

    [ncl] Fix linter

--- apps/native-component-list/src/screens/Reanimated/ReanimatedFlipCard.tsx
+++ apps/native-component-list/src/screens/Reanimated/ReanimatedFlipCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, SafeAreaView, View, StyleSheet, Text } from 'react-native';
+import { Pressable, View, StyleSheet, Text } from 'react-native';
 import Animated, {
   interpolate,
   SharedValue,
