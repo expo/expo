4cb55efd65876a827e43cdbb6713fe8d16d80047
Jakub Tkacz <jtkacz@expo.io>
2025-07-29 19:38:39 +0200

    [expo-router] Move not-found and site map to root stack navigator

--- packages/expo-router/build/testing-library/mock-config.d.ts
+++ packages/expo-router/build/testing-library/mock-config.d.ts
@@ -5,6 +5,40 @@ export type MockContextConfig = string | string[] | MemoryContext | {
 };
 export declare function getMockConfig(context: MockContextConfig, metaOnly?: boolean): {
     screens: {
+        "+not-found"?: undefined;
+        _sitemap?: undefined;
+        __root: {
+            initialRouteName: undefined;
+            screens: Record<string, import("../getReactNavigationConfig").Screen>;
+            path: string;
+        };
+    } | {
+        "+not-found": {
+            path: string;
+        };
+        _sitemap?: undefined;
+        __root: {
+            initialRouteName: undefined;
+            screens: Record<string, import("../getReactNavigationConfig").Screen>;
+            path: string;
+        };
+    } | {
+        "+not-found"?: undefined;
+        _sitemap: {
+            path: string;
+        };
+        __root: {
+            initialRouteName: undefined;
+            screens: Record<string, import("../getReactNavigationConfig").Screen>;
+            path: string;
+        };
+    } | {
+        "+not-found": {
+            path: string;
+        };
+        _sitemap: {
+            path: string;
+        };
         __root: {
             initialRouteName: undefined;
             screens: Record<string, import("../getReactNavigationConfig").Screen>;
