bb4803b8712f67e075f821420bd42aa61850ccf4
Kudo Chien <kudo@expo.dev>
2025-08-01 00:46:00 +0800

    [devtools] fix internal imports (#38459)
    # Why
    
    fixing internal imports to `expo/devtools`
    
    # How
    
    since we have formal `packages:exports` now and re-report from
    `expo/devtools`. i'm reusing the exports but having unstable_ prefix

--- packages/@expo/devtools/CHANGELOG.md
+++ packages/@expo/devtools/CHANGELOG.md
@@ -8,6 +8,8 @@
 
 ### 🐛 Bug fixes
 
+- Fixed internal exports for testing. ([#38459](https://github.com/expo/expo/pull/38459) by [@kudo](https://github.com/kudo))
+
 ### 💡 Others
 
 - Moved **src/devtools** code to the `@expo/devtools` package. ([#38438](https://github.com/expo/expo/pull/38438) by [@kudo](https://github.com/kudo))
--- packages/@expo/devtools/build/index.d.ts
+++ packages/@expo/devtools/build/index.d.ts
@@ -3,4 +3,8 @@ export { setEnableLogging } from './logger';
 export { getDevToolsPluginClientAsync } from './DevToolsPluginClientFactory';
 export { DevToolsPluginClient } from './DevToolsPluginClient';
 export type { EventSubscription } from './DevToolsPluginClient';
+export type * from './devtools.types';
+export { createDevToolsPluginClient as unstable_createDevToolsPluginClient } from './DevToolsPluginClientFactory';
+export { WebSocketBackingStore as unstable_WebSocketBackingStore } from './WebSocketBackingStore';
+export { getConnectionInfo as unstable_getConnectionInfo } from './getConnectionInfo';
 //# sourceMappingURL=index.d.ts.map
