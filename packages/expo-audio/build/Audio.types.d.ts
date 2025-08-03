5845d7e9dff27b5410e3630b37a8d6fa1e018e94
Ben Awad <benawad97@gmail.com>
2025-08-03 08:23:50 -0700

    [audio] build

--- packages/expo-audio/build/Audio.types.d.ts
+++ packages/expo-audio/build/Audio.types.d.ts
@@ -410,5 +410,5 @@ export type InterruptionMode = 'mixWithOthers' | 'doNotMix' | 'duckOthers';
  *
  * @platform android
  */
-export type InterruptionModeAndroid = 'doNotMix' | 'duckOthers';
+export type InterruptionModeAndroid = 'mixWithOthers' | 'doNotMix' | 'duckOthers';
 //# sourceMappingURL=Audio.types.d.ts.map
