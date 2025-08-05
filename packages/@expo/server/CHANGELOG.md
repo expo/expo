de3df6498bd6b2e6f3da3fd96aa4e85145b3d90a
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2025-08-05 20:28:13 +0200

    [expo/server] Port implementation from `eas-hosting/deployment-worker` (#38465)

--- packages/@expo/server/CHANGELOG.md
+++ packages/@expo/server/CHANGELOG.md
@@ -6,12 +6,14 @@
 
 ### 🎉 New features
 
+- Add Bun adapter to expo server runtime. ([#38240](https://github.com/expo/expo/pull/38240) by [@daavidaviid](https://github.com/daavidaviid))
+
 ### 🐛 Bug fixes
 
 ### 💡 Others
 
-- Add Bun adapter to expo server runtime. ([#38240](https://github.com/expo/expo/pull/38240) by [@daavidaviid](https://github.com/daavidaviid))
 - Bump Express types to `@types/express@5`. ([#37635](https://github.com/expo/expo/pull/37635) by [@byCedric](https://github.com/byCedric))
+- Sync development and self host server behavior with EAS Hosting ([#38465](https://github.com/expo/expo/pull/38465) by [@krystofwoldrich](https://github.com/krystofwoldrich))
 
 ## 0.6.3 - 2025-06-18
 
--- packages/@expo/server/build/index.d.ts
+++ packages/@expo/server/build/index.d.ts
@@ -1,10 +1,10 @@
 import './install';
-import type { ExpoRoutesManifestV1, RouteInfo } from 'expo-router/build/routes-manifest';
-export declare function getRoutesManifest(distFolder: string): ExpoRoutesManifestV1<RegExp>;
-export declare function createRequestHandler(distFolder: string, { getRoutesManifest: getInternalRoutesManifest, getHtml, getApiRoute, logApiRouteExecutionError, handleApiRouteError, }?: {
-    getHtml?: (request: Request, route: RouteInfo<RegExp>) => Promise<string | Response | null>;
-    getRoutesManifest?: (distFolder: string) => Promise<ExpoRoutesManifestV1<RegExp> | null>;
-    getApiRoute?: (route: RouteInfo<RegExp>) => Promise<any>;
+import { Manifest, Route } from './types';
+export declare function getRoutesManifest(distFolder: string): Manifest;
+export declare function createRequestHandler(distFolder: string, { getRoutesManifest: getInternalRoutesManifest, getHtml, getApiRoute, handleRouteError, }?: {
+    getHtml?: (request: Request, route: Route) => Promise<string | Response | null>;
+    getRoutesManifest?: (distFolder: string) => Promise<Manifest | null>;
+    getApiRoute?: (route: Route) => Promise<any>;
     logApiRouteExecutionError?: (error: Error) => void;
-    handleApiRouteError?: (error: Error) => Promise<Response>;
+    handleRouteError?: (error: Error) => Promise<Response>;
 }): (request: Request) => Promise<Response>;
