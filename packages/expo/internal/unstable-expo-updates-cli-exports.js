7324dd27087b1f1d30744392c8899bfe9517c1e1
Phil Pluckthun <phil@kitten.sh>
2025-08-05 19:11:00 +0100

    expo: Rename to `expo/internal/unstable-expo-updates-cli-exports`

--- packages/expo/internal/unstable-expo-updates-cli-exports.js
+++ packages/expo/internal/unstable-expo-updates-cli-exports.js
@@ -1,2 +1,2 @@
 // WARN: Internal re-export, don't rely on this to be a public API or use it outside of `expo/expo`'s monorepo
-module.exports = require('@expo/cli/internal/exports');
+module.exports = require('@expo/cli/internal/unstable-expo-updates-exports');
