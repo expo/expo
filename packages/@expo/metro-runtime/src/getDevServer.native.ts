49aed48983e5951922a736eb6240a51af37ab973
evanbacon <bacon@expo.io>
2025-08-05 16:29:23 -0500

    fork away from react-native/Libraries/Core/Devtools/getDevServer import

--- packages/@expo/metro-runtime/src/getDevServer.native.ts
+++ packages/@expo/metro-runtime/src/getDevServer.native.ts
@@ -1,3 +1,32 @@
-import getDevServer from 'react-native/Libraries/Core/Devtools/getDevServer';
+import { TurboModuleRegistry } from 'react-native';
 
-export default getDevServer;
+const NativeSourceCode = TurboModuleRegistry.getEnforcing<{
+  getConstants: () => { scriptURL: string };
+}>('SourceCode');
+
+const FALLBACK = 'http://localhost:8081/';
+let cachedDevServerURL: string | null | undefined;
+let cachedFullBundleURL: string | null = null;
+
+/**
+ * Many RN development tools rely on the development server (packager) running
+ * @return URL to packager with trailing slash
+ */
+export default function getDevServer(): {
+  url: string;
+  fullBundleUrl: string | null;
+  bundleLoadedFromServer: boolean;
+} {
+  if (cachedDevServerURL === undefined) {
+    const scriptUrl = NativeSourceCode.getConstants().scriptURL;
+    const match = scriptUrl.match(/^https?:\/\/.*?\//);
+    cachedDevServerURL = match ? match[0] : null;
+    cachedFullBundleURL = match ? scriptUrl : null;
+  }
+
+  return {
+    url: cachedDevServerURL ?? FALLBACK,
+    fullBundleUrl: cachedFullBundleURL,
+    bundleLoadedFromServer: cachedDevServerURL !== null,
+  };
+}
