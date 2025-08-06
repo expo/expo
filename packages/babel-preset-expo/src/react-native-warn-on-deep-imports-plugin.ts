d0673e48858bb02ff72ab1886f1d2b2f7dccfc7d
Phil Pluckthun <phil@kitten.sh>
2025-08-06 11:48:38 +0100

    Add forked version of warn-on-deep-imports-plugin

---
+++ packages/babel-preset-expo/src/react-native-warn-on-deep-imports-plugin.ts
@@ -0,0 +1,157 @@
+/**
+ * Copyright © 2024 650 Industries.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// Original: https://github.com/facebook/react-native/blob/00175bd/packages/react-native-babel-preset/src/plugin-warn-on-deep-imports.js#L106
+
+import type { ConfigAPI, PluginObj, types as t } from '@babel/core';
+
+function getWarningMessage(
+  importPath: string,
+  loc: t.SourceLocation | undefined | null,
+  source: string,
+) {
+  const message = `Deep imports from the 'react-native' package are deprecated ('${importPath}').`;
+
+  if (source !== undefined) {
+    return `${message} Source: ${source} ${loc ? `${loc.start.line}:${loc.start.column}` : ''}`;
+  }
+
+  return message;
+}
+
+function createWarning(
+  t: typeof import('@babel/types'),
+  importPath: string,
+  loc: t.SourceLocation | undefined | null,
+  source: string
+) {
+  const warningMessage = getWarningMessage(importPath, loc, source);
+
+  const warning = t.expressionStatement(
+    t.callExpression(
+      t.memberExpression(t.identifier('console'), t.identifier('warn')),
+      [t.stringLiteral(warningMessage)],
+    ),
+  );
+
+  return warning;
+}
+
+function isDeepReactNativeImport(source: string) {
+  const parts = source.split('/');
+  return parts.length > 1 && parts[0] === 'react-native';
+}
+
+function isInitializeCoreImport(source: string) {
+  return source === 'react-native/Libraries/Core/InitializeCore';
+}
+
+function withLocation<T extends t.Node>(node: T, loc: t.SourceLocation | null | undefined): T {
+  if (!node.loc) {
+    return {...node, loc};
+  }
+  return node;
+}
+
+interface ImportRef {
+  source: string;
+  loc?: t.SourceLocation | null;
+}
+
+interface State {
+  filename: string;
+  require: ImportRef[];
+  import: ImportRef[];
+  export: ImportRef[];
+}
+
+export const reactNativeWarnOnDeepImportsPlugin =
+  ({ types: t }: ConfigAPI & typeof import('@babel/core')): PluginObj<State> => ({
+  name: 'warn-on-deep-imports',
+  visitor: {
+    ImportDeclaration(path, state) {
+      const source = path.node.source.value;
+
+      if (isDeepReactNativeImport(source) && !isInitializeCoreImport(source)) {
+        const loc = path.node.loc;
+        state.import.push({source, loc});
+      }
+    },
+    CallExpression(path, state) {
+      const callee = path.get('callee');
+      const args = path.get('arguments');
+
+      if (
+        callee.isIdentifier({name: 'require'}) &&
+        args.length === 1 &&
+        args[0].isStringLiteral()
+      ) {
+        const source =
+          args[0].node.type === 'StringLiteral' ? args[0].node.value : '';
+        if (
+          isDeepReactNativeImport(source) &&
+          !isInitializeCoreImport(source)
+        ) {
+          const loc = path.node.loc;
+          state.require.push({source, loc});
+        }
+      }
+    },
+    ExportNamedDeclaration(path, state) {
+      const source = path.node.source;
+
+      if (
+        source &&
+        isDeepReactNativeImport(source.value) &&
+        !isInitializeCoreImport(source.value) // NOTE: This contained a bug before (source v source.value)
+      ) {
+        const loc = path.node.loc;
+        state.export.push({source: source.value, loc});
+      }
+    },
+    Program: {
+      enter(path, state) {
+        state.require = [];
+        state.import = [];
+        state.export = [];
+      },
+      exit(path, state) {
+        const {body} = path.node;
+
+        const requireWarnings = state.require.map(value =>
+          withLocation(
+            createWarning(t, value.source, value.loc, state.filename),
+            value.loc,
+          ),
+        );
+
+        const importWarnings = state.import.map(value =>
+          withLocation(
+            createWarning(t, value.source, value.loc, state.filename),
+            value.loc,
+          ),
+        );
+
+        const exportWarnings = state.export.map(value =>
+          withLocation(
+            createWarning(t, value.source, value.loc, state.filename),
+            value.loc,
+          ),
+        );
+
+        const warnings = [
+          ...requireWarnings,
+          ...importWarnings,
+          ...exportWarnings,
+        ];
+
+        body.push(...warnings);
+      },
+    },
+  },
+});
