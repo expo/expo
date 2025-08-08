ea154052764dbee194e07c9c76c761ff80ca1d1e
nishan (o^▽^o) <nishanbende@gmail.com>
2025-08-09 00:24:41 +0530

    [ios][maps] add poi filter to apple maps (#38514)
    # Why
    
    Adds filtering for points of interest categories (include/exclude),
    elevation and emphasis styling support using
    [mapStyle](https://developer.apple.com/documentation/mapkit/mapstyle).
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    Uses
    [mapStyle](https://developer.apple.com/documentation/mapkit/mapstyle)
    API
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    Test newly added screen. Test on iOS 17 and 18.
    
    
    https://github.com/user-attachments/assets/41f06799-4475-4708-a11d-eaedb3ebab7a
    
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo-maps/build/apple/AppleMaps.types.d.ts
+++ packages/expo-maps/build/apple/AppleMaps.types.d.ts
@@ -84,6 +84,130 @@ export declare enum AppleMapsContourStyle {
 /**
  * @platform ios
  */
+export declare enum AppleMapsMapStyleElevation {
+    /**
+     * The default elevation style, that renders a flat, 2D map.
+     */
+    AUTOMATIC = "AUTOMATIC",
+    /**
+     * A flat elevation style.
+     */
+    FLAT = "FLAT",
+    /**
+     * A value that renders a realistic, 3D map.
+     */
+    REALISTIC = "REALISTIC"
+}
+/**
+ * @platform ios
+ */
+export declare enum AppleMapsMapStyleEmphasis {
+    /**
+     * The default level of emphasis.
+     */
+    AUTOMATIC = "AUTOMATIC",
+    /**
+     * A muted emphasis style, that deemphasizes the map’s imagery.
+     */
+    MUTED = "MUTED"
+}
+/**
+ * @platform ios
+ * @see https://developer.apple.com/documentation/mapkit/AppleMapPointOfInterestCategory
+ */
+export declare enum AppleMapPointOfInterestCategory {
+    MUSEUM = "MUSEUM",
+    MUSIC_VENUE = "MUSIC_VENUE",
+    THEATER = "THEATER",
+    LIBRARY = "LIBRARY",
+    PLANETARIUM = "PLANETARIUM",
+    SCHOOL = "SCHOOL",
+    UNIVERSITY = "UNIVERSITY",
+    MOVIE_THEATER = "MOVIE_THEATER",
+    NIGHTLIFE = "NIGHTLIFE",
+    FIRE_STATION = "FIRE_STATION",
+    HOSPITAL = "HOSPITAL",
+    PHARMACY = "PHARMACY",
+    POLICE = "POLICE",
+    CASTLE = "CASTLE",
+    FORTRESS = "FORTRESS",
+    LANDMARK = "LANDMARK",
+    NATIONAL_MONUMENT = "NATIONAL_MONUMENT",
+    BAKERY = "BAKERY",
+    BREWERY = "BREWERY",
+    CAFE = "CAFE",
+    DISTILLERY = "DISTILLERY",
+    FOOD_MARKET = "FOOD_MARKET",
+    RESTAURANT = "RESTAURANT",
+    WINERY = "WINERY",
+    ANIMAL_SERVICE = "ANIMAL_SERVICE",
+    ATM = "ATM",
+    AUTOMOTIVE_REPAIR = "AUTOMOTIVE_REPAIR",
+    BANK = "BANK",
+    BEAUTY = "BEAUTY",
+    EV_CHARGER = "EV_CHARGER",
+    FITNESS_CENTER = "FITNESS_CENTER",
+    LAUNDRY = "LAUNDRY",
+    MAILBOX = "MAILBOX",
+    POST_OFFICE = "POST_OFFICE",
+    RESTROOM = "RESTROOM",
+    SPA = "SPA",
+    STORE = "STORE",
+    AMUSEMENT_PARK = "AMUSEMENT_PARK",
+    AQUARIUM = "AQUARIUM",
+    BEACH = "BEACH",
+    CAMPGROUND = "CAMPGROUND",
+    FAIRGROUND = "FAIRGROUND",
+    MARINA = "MARINA",
+    NATIONAL_PARK = "NATIONAL_PARK",
+    PARK = "PARK",
+    RV_PARK = "RV_PARK",
+    ZOO = "ZOO",
+    BASEBALL = "BASEBALL",
+    BASKETBALL = "BASKETBALL",
+    BOWLING = "BOWLING",
+    GO_KART = "GO_KART",
+    GOLF = "GOLF",
+    HIKING = "HIKING",
+    MINI_GOLF = "MINI_GOLF",
+    ROCK_CLIMBING = "ROCK_CLIMBING",
+    SKATE_PARK = "SKATE_PARK",
+    SKATING = "SKATING",
+    SKIING = "SKIING",
+    SOCCER = "SOCCER",
+    STADIUM = "STADIUM",
+    TENNIS = "TENNIS",
+    VOLLEYBALL = "VOLLEYBALL",
+    AIRPORT = "AIRPORT",
+    CAR_RENTAL = "CAR_RENTAL",
+    CONVENTION_CENTER = "CONVENTION_CENTER",
+    GAS_STATION = "GAS_STATION",
+    HOTEL = "HOTEL",
+    PARKING = "PARKING",
+    PUBLIC_TRANSPORT = "PUBLIC_TRANSPORT",
+    FISHING = "FISHING",
+    KAYAKING = "KAYAKING",
+    SURFING = "SURFING",
+    SWIMMING = "SWIMMING"
+}
+/**
+ * @platform ios
+ */
+export type AppleMapsPointOfInterestCategories = {
+    /**
+     * The POI categories to include.
+     * To hide all POIs, set this to an empty array.
+     */
+    including?: AppleMapPointOfInterestCategory[];
+    /**
+     * The POI categories to exclude.
+     * To show all POIs, set this to an empty array.
+     */
+    excluding?: AppleMapPointOfInterestCategory[];
+};
+/**
+ * @platform ios
+ */
 export type AppleMapsProperties = {
     /**
      * Whether the user location is shown on the map.
@@ -98,6 +222,18 @@ export type AppleMapsProperties = {
      * Defines which map type should be used.
      */
     mapType?: AppleMapsMapType;
+    /**
+     * A structure you use to define points of interest to include or exclude on a map.
+     */
+    pointsOfInterest?: AppleMapsPointOfInterestCategories;
+    /**
+     * Values you use to determine whether a map renders elevation.
+     */
+    elevation?: AppleMapsMapStyleElevation;
+    /**
+     * Values that control how the framework emphasizes map features.
+     */
+    emphasis?: AppleMapsMapStyleEmphasis;
     /**
      * If true, the user can select a location on the map to get more information.
      */
