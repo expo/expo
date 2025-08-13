bb0bde79e1521eeea113b9cc7ea63fd80c976ee1
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2025-08-13 10:02:13 +0200

    [dev-menu][Android] UI refresh (#38759)

---
+++ packages/expo-dev-menu/android/src/main/java/expo/modules/devmenu/compose/newtheme/BorderRadius.kt
@@ -0,0 +1,15 @@
+package expo.modules.devmenu.compose.newtheme
+
+import androidx.compose.ui.unit.dp
+
+object BorderRadius {
+  val none = 0.dp
+  val xs = 2.dp
+  val sm = 4.dp
+  val md = 6.dp
+  val lg = 10.dp
+  val xl = 16.dp
+  val xxl = 20.dp
+  val xxxl = 24.dp
+  val full = 9999.dp
+}
