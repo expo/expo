0855df9bab5cda26ca7c33ad4ac1c2eaf1179da4
evanbacon <bacon@expo.io>
2025-08-14 12:20:42 -0500

    update backgrounds

--- templates/expo-template-default/components/external-link.tsx
+++ templates/expo-template-default/components/external-link.tsx
@@ -1,7 +1,6 @@
 import { Href, Link } from 'expo-router';
-import { openBrowserAsync } from 'expo-web-browser';
+import { openBrowserAsync, WebBrowserPresentationStyle } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
-import { Platform } from 'react-native';
 
 type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: Href & string };
 
@@ -12,11 +11,13 @@ export function ExternalLink({ href, ...rest }: Props) {
       {...rest}
       href={href}
       onPress={async (event) => {
-        if (Platform.OS !== 'web') {
+        if (process.env.EXPO_OS !== 'web') {
           // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
           // Open the link in an in-app browser.
-          await openBrowserAsync(href);
+          await openBrowserAsync(href, {
+            presentationStyle: WebBrowserPresentationStyle.AUTOMATIC,
+          });
         }
       }}
     />
