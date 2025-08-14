c88dcbd82c53c23a1273f4663eae68eb7d809747
evanbacon <bacon@expo.io>
2025-08-14 12:16:52 -0500

    Update hello-wave.tsx

--- templates/expo-template-default/components/hello-wave.tsx
+++ templates/expo-template-default/components/hello-wave.tsx
@@ -1,27 +1,19 @@
-import { StyleSheet } from 'react-native';
 import Animated from 'react-native-reanimated';
-import { ThemedText } from '@/components/themed-text';
 
 export function HelloWave() {
   return (
-    <Animated.View
+    <Animated.Text
       style={{
+        fontSize: 28,
+        lineHeight: 32,
+        marginTop: -6,
         animationName: {
           '50%': { transform: [{ rotate: '25deg' }] },
         },
-        animationFillMode: 'none',
         animationIterationCount: 4,
         animationDuration: '300ms',
       }}>
-      <ThemedText style={styles.text}>👋</ThemedText>
-    </Animated.View>
+      👋
+    </Animated.Text>
   );
 }
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 28,
-    lineHeight: 32,
-    marginTop: -6,
-  },
-});
