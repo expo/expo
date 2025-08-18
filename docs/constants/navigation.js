828896fc7882a7fdd14be22bcfd2525257d8655c
Gabriel Donadel <donadeldev@gmail.com>
2025-08-18 17:28:58 -0300

    [docs] Add brownfield instructions using create-expo-app
    Update steps

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -229,6 +229,7 @@ export const general = [
     ]),
     makeSection('Existing native apps', [
       makePage('brownfield/overview.mdx'),
+      makePage('brownfield/integration-with-existing-apps.mdx'),
       makePage('brownfield/installing-expo-modules.mdx'),
     ]),
     makeGroup(
