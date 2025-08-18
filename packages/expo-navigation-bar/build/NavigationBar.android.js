30987146094141decdc06125464636c3a7539b29
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2025-08-18 13:13:32 +0100

    [expo-navigation-bar][android][4/n] Use React Native's edge-to-edge - Remove react-native-edge-to-edge #38768 (#38769)
    # Why
    
    Same as for `expo-status-bar` we want to remove the
    `react-native-edge-to-edge` dependency to avoid issues.
    
    # How
    
    Removed the `SystemBars` our previous implementations seem to work fine
    for everything that the ones from `SystemBars` worked for. Kept the
    `react-native-is-edge-to-edge` dependency to be able to check whether
    edge-to-edge is enabled.
    
    # Test Plan
    
    Tested in BareExpo on Android

--- packages/expo-navigation-bar/build/NavigationBar.android.js
+++ packages/expo-navigation-bar/build/NavigationBar.android.js
@@ -1,21 +1,28 @@
 import { useEffect, useState } from 'react';
-import { processColor } from 'react-native';
+import { Appearance, processColor } from 'react-native';
 import { isEdgeToEdge } from 'react-native-is-edge-to-edge';
 import ExpoNavigationBar from './ExpoNavigationBar';
-// This line only imports the type information for TypeScript type checking.  It
-// doesn't import the actual module in the compiled JavaScript code.  The actual
-// module is imported conditionally with require() below, in order to avoid
-// importing the module if edge-to-edge is not enabled (which could throw if
-// it's not linked).
-let SystemBars = null;
-if (isEdgeToEdge()) {
-    SystemBars = require('react-native-edge-to-edge').SystemBars;
+function isLightColorScheme() {
+    const colorScheme = Appearance?.getColorScheme() ?? 'light';
+    return colorScheme === 'light';
+}
+function navigationBarStyleToButtonStyle(navigationBarStyle) {
+    switch (navigationBarStyle) {
+        case 'auto':
+            return isLightColorScheme() ? 'dark' : 'light';
+        case 'light':
+            return 'dark';
+        case 'dark':
+            return 'light';
+        case 'inverted':
+            return isLightColorScheme() ? 'light' : 'dark';
+    }
 }
 export function addVisibilityListener(listener) {
     return ExpoNavigationBar.addListener('ExpoNavigationBar.didChange', listener);
 }
 export async function setBackgroundColorAsync(color) {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`setBackgroundColorAsync` is not supported with edge-to-edge enabled.');
         return;
     }
@@ -23,14 +30,14 @@ export async function setBackgroundColorAsync(color) {
     await ExpoNavigationBar.setBackgroundColorAsync(colorNumber);
 }
 export async function getBackgroundColorAsync() {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`getBackgroundColorAsync` is not supported with edge-to-edge enabled.');
         return `#00000000`;
     }
     return await ExpoNavigationBar.getBackgroundColorAsync();
 }
 export async function setBorderColorAsync(color) {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`setBorderColorAsync` is not supported with edge-to-edge enabled.');
         return;
     }
@@ -38,70 +45,58 @@ export async function setBorderColorAsync(color) {
     await ExpoNavigationBar.setBorderColorAsync(colorNumber);
 }
 export async function getBorderColorAsync() {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`getBorderColorAsync` is not supported with edge-to-edge enabled.');
         return `#00000000`;
     }
     return await ExpoNavigationBar.getBorderColorAsync();
 }
 export async function setVisibilityAsync(visibility) {
-    if (SystemBars != null) {
-        SystemBars.setHidden({ navigationBar: visibility === 'hidden' });
-        return;
-    }
     await ExpoNavigationBar.setVisibilityAsync(visibility);
 }
 export async function getVisibilityAsync() {
     return ExpoNavigationBar.getVisibilityAsync();
 }
 export async function setButtonStyleAsync(style) {
-    if (SystemBars != null) {
-        SystemBars.setStyle({ navigationBar: style });
-        return;
-    }
     await ExpoNavigationBar.setButtonStyleAsync(style);
 }
 export async function getButtonStyleAsync() {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`getButtonStyleAsync` is not supported with edge-to-edge enabled.');
         return 'light';
     }
     return await ExpoNavigationBar.getButtonStyleAsync();
 }
 export async function setPositionAsync(position) {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`setPositionAsync` is not supported with edge-to-edge enabled.');
         return;
     }
     await ExpoNavigationBar.setPositionAsync(position);
 }
 export async function unstable_getPositionAsync() {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`unstable_getPositionAsync` is not supported with edge-to-edge enabled.');
         return 'relative';
     }
     return await ExpoNavigationBar.unstable_getPositionAsync();
 }
 export async function setBehaviorAsync(behavior) {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`setBehaviorAsync` is not supported with edge-to-edge enabled.');
         return;
     }
     await ExpoNavigationBar.setBehaviorAsync(behavior);
 }
 export async function getBehaviorAsync() {
-    if (SystemBars != null) {
+    if (isEdgeToEdge()) {
         console.warn('`getBehaviorAsync` is not supported with edge-to-edge enabled.');
         return 'inset-touch';
     }
     return await ExpoNavigationBar.getBehaviorAsync();
 }
 export function setStyle(style) {
-    if (SystemBars != null) {
-        SystemBars.setStyle({ navigationBar: style });
-        return;
-    }
-    throw new Error('`setStyle` is only supported on Android when edge-to-edge is enabled. Enable edge-to-edge or use the `setButtonStyle` function instead.');
+    ExpoNavigationBar.setButtonStyleAsync(navigationBarStyleToButtonStyle(style));
 }
 export function useVisibility() {
     const [visibility, setVisible] = useState(null);
