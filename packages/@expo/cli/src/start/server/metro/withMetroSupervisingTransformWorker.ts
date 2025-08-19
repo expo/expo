761fa53d7f46677396e36c6191a5c0891628e1a0
Phil Pluckthun <phil@kitten.sh>
2025-08-19 09:41:58 +0100

    Add supervising-transform-worker wrapper

---
+++ packages/@expo/cli/src/start/server/metro/withMetroSupervisingTransformWorker.ts
@@ -0,0 +1,25 @@
+import type { ConfigT as MetroConfig } from '@expo/metro/metro-config';
+import { unstable_transformerPath } from '@expo/metro-config';
+
+const debug = require('debug')('expo:metro:withMetroSupervisingTransformWorker') as typeof console.log;
+
+declare module '@expo/metro/metro-transform-worker' {
+  export interface JsTransformerConfig {
+    expo_customTransformerPath?: string;
+  }
+}
+
+export function withMetroSupervisingTransformWorker(config: MetroConfig): MetroConfig {
+  const originalTransformerPath = config.transformerPath;
+  if (originalTransformerPath === unstable_transformerPath) {
+    return config;
+  }
+  debug('Detected customized "transformerPath": Wrapping transformer with supervisor');
+  return {
+    ...config,
+    transformer: {
+      ...config.transformer,
+      expo_customTransformerPath: originalTransformerPath,
+    },
+  };
+}
