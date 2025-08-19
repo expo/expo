801f3f7fe7f5bb501660dc3c6beb09e31759b82d
Aman Mittal <amandeepmittal@live.com>
2025-08-19 21:41:27 +0530

    Add a clarification popover for Node.js version

--- docs/ui/components/SDKTables/ReactNativeCompatibilityTable.tsx
+++ docs/ui/components/SDKTables/ReactNativeCompatibilityTable.tsx
@@ -1,6 +1,8 @@
 import { usePageApiVersion } from '~/providers/page-api-version';
 import versions from '~/public/static/constants/versions.json';
 import { TableHead, Row, Cell, Table, HeaderCell } from '~/ui/components/Table';
+import { FOOTNOTE } from '~/ui/components/Text';
+import * as Tooltip from '~/ui/components/Tooltip';
 
 import { getThreeVersions } from './utils';
 
@@ -20,7 +22,19 @@ export const ReactNativeCompatibilityTable = () => {
           <HeaderCell>React Native version</HeaderCell>
           <HeaderCell>React version</HeaderCell>
           <HeaderCell>React Native Web version</HeaderCell>
-          <HeaderCell>Node.js version</HeaderCell>
+          <HeaderCell>
+            <Tooltip.Root delayDuration={500}>
+              <Tooltip.Trigger asChild>
+                <span className="cursor-help">Node.js version (?)</span>
+              </Tooltip.Trigger>
+              <Tooltip.Content side="top" className="max-w-[300px]">
+                <FOOTNOTE>
+                  This is the recommended Node.js version. The Expo SDK is not strictly compatible
+                  only with this version.
+                </FOOTNOTE>
+              </Tooltip.Content>
+            </Tooltip.Root>
+          </HeaderCell>
         </Row>
       </TableHead>
       <tbody>
