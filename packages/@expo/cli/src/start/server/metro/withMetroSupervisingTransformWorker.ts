f54c9b4a5f47446c2fb28e88b38c6fc5522df7b6
Phil Pluckthun <phil@kitten.sh>
2025-08-20 21:51:11 +0100

    Add comment to CLI

--- packages/@expo/cli/src/start/server/metro/withMetroSupervisingTransformWorker.ts
+++ packages/@expo/cli/src/start/server/metro/withMetroSupervisingTransformWorker.ts
@@ -11,14 +11,40 @@ declare module '@expo/metro/metro-transform-worker' {
   }
 }
 
+// The default babel transformer is either `@expo/metro-config/babel-transformer` set by the user
+// or @expo/metro-config/build/babel-transformer
 const defaultBabelTransformerPaths = [
   require.resolve('@expo/metro-config/babel-transformer'),
   require.resolve('@expo/metro-config/build/babel-transformer'),
 ];
 
+/** Adds a wrapper around a user's transformerPath or babelTransformerPath
+ * @remarks
+ * It's relatively common still for people to set custom transformerPath
+ * or babelTransformerPaths. These are used to customize how files are transformed
+ * and wrap around Expo's transformer/babel-transformer.
+ *
+ * This config override customizes them.
+ * If a user has a custom transformer or babelTransformerPath set, we
+ * first load the "supervising transformer"
+ *
+ * This is a transformer entrypoint that's always supposed to load first and
+ * loads the user's custom transformer from `config.transformer.expo_customTransformerPath`
+ * instead of `config.transformerPath`
+ *
+ * This supervisor has two tasks:
+ * - It adds a try-catch. When the user's transformer fails to load, we output a better error message
+ * - It forces the same versions of Metro and @expo/metro-config to load that we depend on
+ *   (unless threading is disabled, which makes this override unsafe)
+ *
+ * We do this because transformers and babel transformers *must not* use different
+ * versions of Metro. This is unsupported and undefined behavior and will lead to
+ * bugs and errors.
+ */
 export function withMetroSupervisingTransformWorker(config: MetroConfig): MetroConfig {
-  const originalTransformerPath = config.transformerPath;
+  // NOTE: This is usually a required property, but we don't always set it in mocks
   const originalBabelTransformerPath = config.transformer?.babelTransformerPath;
+  const originalTransformerPath = config.transformerPath;
 
   const hasDefaultTransformerPath = originalTransformerPath === unstable_transformerPath;
   const hasDefaultBabelTransformerPath =
@@ -28,6 +54,10 @@ export function withMetroSupervisingTransformWorker(config: MetroConfig): MetroC
     return config;
   }
 
+  // We modify the config if the user either has a custom transformerPath or
+  // a custom transformer.babelTransformerPath
+  // NOTE: It's not a bad thing if we load the superivising transformer even if
+  // we don't need to. It will do nothing to our transformer
   if (!hasDefaultTransformerPath) {
     debug('Detected customized "transformerPath"');
   }
