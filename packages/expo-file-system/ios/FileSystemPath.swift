16771d3c76d3c4e63703cc4f6543a7f08fd157b0
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2025-08-26 10:45:04 +0200

    clean up

--- packages/expo-file-system/ios/FileSystemPath.swift
+++ packages/expo-file-system/ios/FileSystemPath.swift
@@ -76,12 +76,16 @@ internal class FileSystemPath: SharedObject {
     url = destinationUrl
   }
 
+  func getRenamedUrl(newName: String) -> URL {
+    return url.deletingLastPathComponent().appendingPathComponent(newName)
+  }
+
   func rename(_ newName: String) throws {
     try validatePermission(.write)
-    let newUrl = url.deletingLastPathComponent().appendingPathComponent(newName)
+    let newUrl = getRenamedUrl(newName: newName)
     try FileManager.default.moveItem(at: url, to: newUrl)
     // Refetch the URL to ensure it has the correct trailing slash, which differs for directories and files.
-    let updatedUrl = url.deletingLastPathComponent().appendingPathComponent(newName)
+    let updatedUrl = getRenamedUrl(newName: newName)
     url = updatedUrl
   }
 
