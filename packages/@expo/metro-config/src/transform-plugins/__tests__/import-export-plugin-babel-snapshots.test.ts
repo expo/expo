61bc18ee8ef8d2893b59c01bde6bd637cc764dbe
Phil Pluckthun <phil@kitten.sh>
2025-09-01 17:56:15 +0100

    temp: Update after merge

--- packages/@expo/metro-config/src/transform-plugins/__tests__/import-export-plugin-babel-snapshots.test.ts
+++ packages/@expo/metro-config/src/transform-plugins/__tests__/import-export-plugin-babel-snapshots.test.ts
@@ -1,6 +1,6 @@
 import generate from '@babel/generator';
 
-import { importExportPlugin } from '../import-export-plugin';
+import { importExportLiveBindingsPlugin as importExportPlugin } from '../index';
 import { transformToAst } from './__mocks__/test-helpers-upstream';
 
 const opts = {
