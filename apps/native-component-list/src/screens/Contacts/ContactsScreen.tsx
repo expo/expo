cd691280a37d08e98dcb0c29c2adffa88ba8a3a0
Gregory Moskaliuk <mosckalyuck@gmail.com>
2025-09-01 17:25:58 +0200

    [contacts] Improve type safety for Contact type (#39269)

--- apps/native-component-list/src/screens/Contacts/ContactsScreen.tsx
+++ apps/native-component-list/src/screens/Contacts/ContactsScreen.tsx
@@ -86,12 +86,14 @@ export default function ContactsScreen({ navigation }: Props) {
 }
 
 function ContactsView({ navigation }: Props) {
-  let rawContacts: Record<string, Contacts.Contact> = {};
+  let rawContacts: Record<string, Contacts.ExistingContact> = {};
 
-  const [contacts, setContacts] = React.useState<Contacts.Contact[]>([]);
+  const [contacts, setContacts] = React.useState<Contacts.ExistingContact[]>([]);
   const [hasNextPage, setHasNextPage] = React.useState(true);
   const [refreshing, setRefreshing] = React.useState(false);
-  const [selectedContact, setSelectedContact] = React.useState<Contacts.Contact | null>(null);
+  const [selectedContact, setSelectedContact] = React.useState<Contacts.ExistingContact | null>(
+    null
+  );
 
   const onPressItem = React.useCallback(
     (id: string) => {
