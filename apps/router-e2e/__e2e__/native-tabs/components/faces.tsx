b442e36a3d85a863e2b4abf96a8031ed88d0e517
Jakub Tkacz <jtkacz@expo.io>
2025-09-02 10:12:44 +0200

    [SDK-55] Material You colors

--- apps/router-e2e/__e2e__/native-tabs/components/faces.tsx
+++ apps/router-e2e/__e2e__/native-tabs/components/faces.tsx
@@ -1,8 +1,12 @@
 import { Link } from 'expo-router';
+import { Material3DynamicColor } from 'expo-system-ui';
 import { useMemo } from 'react';
 import { Pressable, View } from 'react-native';
 
 const colors = [
+  Material3DynamicColor('Primary'),
+  Material3DynamicColor('Secondary'),
+  Material3DynamicColor('Tertiary'),
   '#1e3a8a',
   '#fef7ed',
   '#9ca3af',
