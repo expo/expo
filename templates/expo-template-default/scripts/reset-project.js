4a640428d77a27ab802cc2124f3d14d4047cd95d
kzmgit <62587552+kzhgit@users.noreply.github.com>
2025-09-03 23:38:51 +0900

    [docs][template] Reorder props in generated app/index.tsx to match tutorial docs

--- templates/expo-template-default/scripts/reset-project.js
+++ templates/expo-template-default/scripts/reset-project.js
@@ -29,8 +29,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
   },
 });
 `;
