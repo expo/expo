00304818067ccebd5d32fb08663421eb0ae1dd52
nishan (o^▽^o) <nishanbende@gmail.com>
2025-09-03 14:48:30 +0530

    rebuild

--- packages/expo-glass-effect/build/GlassContainer.types.d.ts
+++ packages/expo-glass-effect/build/GlassContainer.types.d.ts
@@ -4,7 +4,6 @@ export type GlassContainerProps = {
      * The distance at which glass elements start affecting each other.
      * Controls when glass elements begin to merge together.
      * @default undefined
-     * @platform ios
      */
     spacing?: number;
 } & ViewProps;
--- packages/expo-glass-effect/build/GlassView.types.d.ts
+++ packages/expo-glass-effect/build/GlassView.types.d.ts
@@ -4,18 +4,15 @@ export type GlassViewProps = {
     /**
      * Glass effect style to apply to the view.
      * @default 'regular'
-     * @platform ios
      */
     glassEffectStyle?: GlassStyle;
     /**
      * Tint color to apply to the glass effect.
-     * @platform ios
      */
     tintColor?: string;
     /**
      * Whether the glass effect should be interactive.
      * @default false
-     * @platform ios
      */
     isInteractive?: boolean;
 } & ViewProps;
