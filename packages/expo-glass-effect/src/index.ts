901954b29b8b6cda472a0bebc31aa9c9da55ac53
nishan (o^▽^o) <nishanbende@gmail.com>
2025-09-03 21:00:10 +0530

    [glass-effect] new package to render Glass effects on iOS (#39322)
    # Why
    
    Adds `GlassView` and `GlassContainer` using
    [UIVisualEffect](https://developer.apple.com/documentation/uikit/uiglasseffect?changes=_9)
    view and
    [UIGlassContainerEffect](https://developer.apple.com/documentation/UIKit/UIGlassContainerEffect)
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    Uses
    [UIGlassEffect](https://developer.apple.com/documentation/uikit/uiglasseffect?changes=_9)
    and
    [UIGlassContainerEffect](https://developer.apple.com/documentation/UIKit/UIGlassContainerEffect)
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    Test the newly added Glass screen.
    
    
    
    https://github.com/user-attachments/assets/bd3bcf50-fca7-46f1-b10d-2a6258346625
    
    
    
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    
    
    ### Known issue
    
    Dynamically toggling `isInteractive` does not work, we can only set it
    once on a View. I faced same issue while implementing `glassEffect` in
    expo-ui. Seems some internal issue. Will wait to see if it gets fixed
    internally. For now one can use `key` prop to unmount/mount component if
    interactive prop changes

---
+++ packages/expo-glass-effect/src/index.ts
@@ -0,0 +1,4 @@
+export { default as GlassView } from './GlassView';
+export { GlassStyle, GlassViewProps } from './GlassView.types';
+export { default as GlassContainer } from './GlassContainer';
+export { GlassContainerProps } from './GlassContainer.types';
