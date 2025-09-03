b7eb6258f16094f66ca457c26056b336f6a518b7
Krystof Woldrich <krystofwoldrich@gmail.com>
2025-09-03 15:17:48 +0200

    remove interop-node, lazy, strict and unrelated regressions tests, add notes

--- packages/@expo/metro-config/src/transform-plugins/__tests__/import-export-plugin-babel-snapshots.test.ts
+++ packages/@expo/metro-config/src/transform-plugins/__tests__/import-export-plugin-babel-snapshots.test.ts
@@ -51,37 +51,6 @@ const cases = [
     import "foo-bar";
     import "./directory/foo-bar";
   `,
-  test('importInterop-node export-default-from')`
-    export { default } from 'dep';
-  `,
-  test('importInterop-node export-named-and-default-from')`
-    export { default, name } from 'dep';
-  `,
-  test('importInterop-node export-named-from')`
-    export { name } from 'dep';
-  `,
-  test('importInterop-node import-default')`
-    import foo from "foo";
-
-    foo();
-  `,
-  test('importInterop-node import-named-and-default')`
-    import foo, { named } from "foo";
-
-    foo();
-    named();
-  `,
-  test('importInterop-node import-named')`
-    import { named } from "foo";
-
-    named();
-  `,
-  test('importInterop-node import-wildcard')`
-    import * as foo from 'foo';
-
-    foo.bar();
-    foo.baz();
-  `,
   test('importInterop-none export-from')`
     export { default } from 'foo';
   `,
@@ -341,161 +310,11 @@ const cases = [
     export { d as e, d as f };
     d = 4;
   `,
-  test('lazy-dep import-default')`
-    import foo from "foo";
-
-    console.log(foo);
-  `,
-  test('lazy-dep import-named')`
-    import { foo } from "foo";
-
-    console.log(foo);
-  `,
-  test('lazy-dep import-namespace-and-named')`
-    import * as foo from "foo";
-    import { bar } from "foo";
-
-    console.log(foo, bar);
-  `,
-  test('lazy-dep import-namespace-multiple')`
-    import * as foo from "foo";
-    import * as foo2 from "foo";
-
-    console.log(foo, foo2);
-  `,
-  test('lazy-dep import-namespace')`
-    import * as foo from "foo";
-
-    console.log(foo);
-  `,
-  test('lazy-dep reexport-all')`
-    export * from "foo";
-  `,
-  test('lazy-dep reexport-default')`
-    import foo from "foo";
-    export { foo as default };
-  `,
-  test('lazy-dep reexport-named')`
-    import { named } from "foo";
-    export { named };
-  `,
-  test('lazy-dep reexport-namespace')`
-    import * as namespace from "foo";
-    export { namespace };
-  `,
-  test('lazy-dep sideeffect')`
-    import "foo";
-  `,
-  test('lazy-dep unused')`
-    import { a } from "a";
-    import b from "b";
-    import * as c from "c";
-
-    // This is included explicitly for the side effects, so we keep it
-    import "d";
-
-    // Only f is unused, we must keep the require call
-    import { e, f } from "e";
-    e;
-
-    // The first import is unused, but we keep the require call
-    // because of the second one
-    import { g } from "g";
-    import { h } from "g";
-    h;
-  `,
-  test('lazy-local import-default')`
-    import foo from "./foo";
-
-    console.log(foo);
-  `,
-  test('lazy-local import-named')`
-    import { foo } from "./foo";
-
-    console.log(foo);
-  `,
-  test('lazy-local import-namespace')`
-    import * as foo from "./foo";
-
-    console.log(foo);
-  `,
-  test('lazy-local reexport-all')`
-    export * from "./foo";
-  `,
-  test('lazy-local reexport-default')`
-    import foo from "./foo";
-    export { foo as default };
-  `,
-  test('lazy-local reexport-named')`
-    import { named } from "./foo";
-    export { named };
-  `,
-  test('lazy-local reexport-namespace')`
-    import * as namespace from "./foo";
-    export { namespace };
-  `,
-  test('lazy-local sideeffect')`
-    import "./a";
-  `,
-  test('lazy-whitelist import-default')`
-    import foo1 from "white";
-
-    console.log(foo1);
-
-    import foo2 from "black";
-
-    console.log(foo2);
-  `,
-  test('lazy-whitelist import-named')`
-    import { foo1 } from "white";
-
-    console.log(foo1);
-
-    import { foo2 } from "black";
-
-    console.log(foo2);
-  `,
-  test('lazy-whitelist import-namespace')`
-    import * as foo1 from "white";
-
-    console.log(foo1);
-
-    import * as foo2 from "black";
-
-    console.log(foo2);
-  `,
-  test('lazy-whitelist reexport-all')`
-    export * from "white";
-
-    export * from "black";
-  `,
-  test('lazy-whitelist reexport-default')`
-    import foo from "white";
-    export { foo as default };
-  `,
-  test('lazy-whitelist reexport-named')`
-    import { named1 } from "white";
-    export { named1 };
-
-    import { named2 } from "black";
-    export { named2 };
-  `,
-  test('lazy-whitelist reexport-namespace')`
-    import * as namespace1 from "white";
-    export { namespace1 };
-
-    import * as namespace2 from "black";
-    export { namespace2 };
-  `,
-  test('lazy-whitelist sideeffect')`
-    import "white";
-    import "black";
-  `,
   test('misc binding-for-new-vars')`
     import {x} from './foo.js';
     var y = true;
     function f() {
-    return [x, y, console];
+      return [x, y, console];
     }
   `,
   test('misc class-static-block')`
@@ -515,12 +334,13 @@ const cases = [
 
     export { Foo, baz };
   `,
-  test('misc export-expr-with-same-name')`
-    import someFunction from './b';
+  // NOTE(krystofwoldrich): Causes duplicate Identifier node in the output error
+  // test('misc export-expr-with-same-name')`
+  //   import someFunction from './b';
 
-    export default (function someFunction () {
-    });
-  `,
+  //   export default (function someFunction () {
+  //   });
+  // `,
   test('misc export-let-function-name-with-fn-name-transform')`
     export let a = function () {};
     export let b = function X() {};
@@ -632,6 +452,8 @@ const cases = [
     for ([Bar] in {}) {}
     for ([...Baz] in {}) {}
   `,
+  // NOTE(krystofwoldrich): Babel creates a runtime error when reassigning imported value
+  // Rollup throws during bundling
   test('misc import-shadowed-assign')`
     import { foo } from "x";
 
@@ -697,6 +519,8 @@ const cases = [
       }
     }
   `,
+  // NOTE(krystofwoldrich): Babel throws an error exports doesn't exist
+  // Rollup does nothing
   test('misc module-exports')`
     import "foo";
 
@@ -735,350 +559,34 @@ const cases = [
     console.log(anAliased());
     console.log(aNamespace());
   `,
-  test('misc undefined-this-arrow-function')`
+  test('misc keep-this-arrow-function')`
     var foo = () => this;
 
   `,
-  test('misc undefined-this-computed-class-method-1')`
+  test('misc keep-this-computed-class-method-1')`
     export class C { [this.name]() {} }
   `,
-  test('misc undefined-this-computed-class-method-2')`
+  test('misc keep-this-computed-class-method-2')`
     class A {
       [() => this.name]() {}
     }
   `,
-  test('misc undefined-this-computed-class-method-3')`
+  test('misc keep-this-computed-class-method-3')`
     class A {
       [function () { this.name; }]() {}
     }
   `,
-  test('misc undefined-this-computed-class-property-name')`
+  test('misc keep-this-computed-class-property-name')`
     export class C { [this.name] = 42 }
   `,
-  test('misc undefined-this-root-call')`
+  test('misc keep-this-root-call')`
     this.foo();
   `,
-  test('misc undefined-this-root-declaration')`
+  test('misc keep-this-root-declaration')`
     var self = this;
   `,
-  test('misc undefined-this-root-reference')`
+  test('misc keep-this-root-reference')`
     this;
-  `,
-  test('regression 4462-T7565')`
-    export { yy, zz };
-
-    var yy = 0;
-    var zz = yy++;
-  `,
-  test('regression es3-compatibility-class')`
-    export default class {}
-  `,
-  test('regression es3-compatibility-function')`
-    export default function () {}
-  `,
-  test('regression es3-compatibility-named-class')`
-    export default class Foo {}
-  `,
-  test('regression es3-compatibility-named-function')`
-    export default function bat () {}
-  `,
-  test('regression es3-compatibility')`
-
-    import foo from 'foo';
-    console.log(foo);
-
-    export default 5;
-  `,
-  test('regression issue-9155')`
-    export {};
-  `,
-  test('regression issue-9611')`
-    import x from './x';
-
-    export default function(x) {}
-  `,
-  test('regression lazy-7176')`
-    import * as mod from "mod";
-    import { named } from "mod";
-
-    named;
-    mod;
-  `,
-  test('regression many-exports-chunked')`
-    export const a000 = 0;
-    export const a001 = 1;
-    export const a002 = 2;
-    export const a003 = 3;
-    export const a004 = 4;
-    export const a005 = 5;
-    export const a006 = 6;
-    export const a007 = 7;
-    export const a008 = 8;
-    export const a009 = 9;
-    export const a010 = 10;
-    export const a011 = 11;
-    export const a012 = 12;
-    export const a013 = 13;
-    export const a014 = 14;
-    export const a015 = 15;
-    export const a016 = 16;
-    export const a017 = 17;
-    export const a018 = 18;
-    export const a019 = 19;
-    export const a020 = 20;
-    export const a021 = 21;
-    export const a022 = 22;
-    export const a023 = 23;
-    export const a024 = 24;
-    export const a025 = 25;
-    export const a026 = 26;
-    export const a027 = 27;
-    export const a028 = 28;
-    export const a029 = 29;
-    export const a030 = 30;
-    export const a031 = 31;
-    export const a032 = 32;
-    export const a033 = 33;
-    export const a034 = 34;
-    export const a035 = 35;
-    export const a036 = 36;
-    export const a037 = 37;
-    export const a038 = 38;
-    export const a039 = 39;
-    export const a040 = 40;
-    export const a041 = 41;
-    export const a042 = 42;
-    export const a043 = 43;
-    export const a044 = 44;
-    export const a045 = 45;
-    export const a046 = 46;
-    export const a047 = 47;
-    export const a048 = 48;
-    export const a049 = 49;
-    export const a050 = 50;
-    export const a051 = 51;
-    export const a052 = 52;
-    export const a053 = 53;
-    export const a054 = 54;
-    export const a055 = 55;
-    export const a056 = 56;
-    export const a057 = 57;
-    export const a058 = 58;
-    export const a059 = 59;
-    export const a060 = 60;
-    export const a061 = 61;
-    export const a062 = 62;
-    export const a063 = 63;
-    export const a064 = 64;
-    export const a065 = 65;
-    export const a066 = 66;
-    export const a067 = 67;
-    export const a068 = 68;
-    export const a069 = 69;
-    export const a070 = 70;
-    export const a071 = 71;
-    export const a072 = 72;
-    export const a073 = 73;
-    export const a074 = 74;
-    export const a075 = 75;
-    export const a076 = 76;
-    export const a077 = 77;
-    export const a078 = 78;
-    export const a079 = 79;
-    export const a080 = 80;
-    export const a081 = 81;
-    export const a082 = 82;
-    export const a083 = 83;
-    export const a084 = 84;
-    export const a085 = 85;
-    export const a086 = 86;
-    export const a087 = 87;
-    export const a088 = 88;
-    export const a089 = 89;
-    export const a090 = 90;
-    export const a091 = 91;
-    export const a092 = 92;
-    export const a093 = 93;
-    export const a094 = 94;
-    export const a095 = 95;
-    export const a096 = 96;
-    export const a097 = 97;
-    export const a098 = 98;
-    export const a099 = 99;
-    export const a100 = 100;
-    export const a101 = 101;
-    export const a102 = 102;
-    export const a103 = 103;
-    export const a104 = 104;
-    export const a105 = 105;
-    export const a106 = 106;
-    export const a107 = 107;
-    export const a108 = 108;
-    export const a109 = 109;
-    export const a110 = 110;
-    export const a111 = 111;
-    export const a112 = 112;
-    export const a113 = 113;
-    export const a114 = 114;
-    export const a115 = 115;
-    export const a116 = 116;
-    export const a117 = 117;
-    export const a118 = 118;
-    export const a119 = 119;
-    export const a120 = 120;
-    export const a121 = 121;
-    export const a122 = 122;
-    export const a123 = 123;
-    export const a124 = 124;
-    export const a125 = 125;
-    export const a126 = 126;
-    export const a127 = 127;
-    export const a128 = 128;
-    export const a129 = 129;
-    export const a130 = 130;
-    export const a131 = 131;
-    export const a132 = 132;
-    export const a133 = 133;
-    export const a134 = 134;
-    export const a135 = 135;
-    export const a136 = 136;
-    export const a137 = 137;
-    export const a138 = 138;
-    export const a139 = 139;
-    export const a140 = 140;
-    export const a141 = 141;
-    export const a142 = 142;
-    export const a143 = 143;
-    export const a144 = 144;
-    export const a145 = 145;
-    export const a146 = 146;
-    export const a147 = 147;
-    export const a148 = 148;
-    export const a149 = 149;
-    export const a150 = 150;
-    export const a151 = 151;
-    export const a152 = 152;
-    export const a153 = 153;
-    export const a154 = 154;
-    export const a155 = 155;
-    export const a156 = 156;
-    export const a157 = 157;
-    export const a158 = 158;
-    export const a159 = 159;
-    export const a160 = 160;
-    export const a161 = 161;
-    export const a162 = 162;
-    export const a163 = 163;
-    export const a164 = 164;
-    export const a165 = 165;
-    export const a166 = 166;
-    export const a167 = 167;
-    export const a168 = 168;
-    export const a169 = 169;
-    export const a170 = 170;
-    export const a171 = 171;
-    export const a172 = 172;
-    export const a173 = 173;
-    export const a174 = 174;
-    export const a175 = 175;
-    export const a176 = 176;
-    export const a177 = 177;
-    export const a178 = 178;
-    export const a179 = 179;
-    export const a180 = 180;
-    export const a181 = 181;
-    export const a182 = 182;
-    export const a183 = 183;
-    export const a184 = 184;
-    export const a185 = 185;
-    export const a186 = 186;
-    export const a187 = 187;
-    export const a188 = 188;
-    export const a189 = 189;
-    export const a190 = 190;
-    export const a191 = 191;
-    export const a192 = 192;
-    export const a193 = 193;
-    export const a194 = 194;
-    export const a195 = 195;
-    export const a196 = 196;
-    export const a197 = 197;
-    export const a198 = 198;
-    export const a199 = 199;
-    export const a200 = 200;
-    export const a201 = 201;
-    export const a202 = 202;
-    export const a203 = 203;
-    export const a204 = 204;
-    export const a205 = 205;
-    export const a206 = 206;
-    export const a207 = 207;
-    export const a208 = 208;
-    export const a209 = 209;
-    export const a210 = 210;
-    export const a211 = 211;
-    export const a212 = 212;
-    export const a213 = 213;
-    export const a214 = 214;
-    export const a215 = 215;
-    export const a216 = 216;
-    export const a217 = 217;
-    export const a218 = 218;
-    export const a219 = 219;
-    export const a220 = 220;
-    export const a221 = 221;
-    export const a222 = 222;
-    export const a223 = 223;
-    export const a224 = 224;
-    export const a225 = 225;
-    export const a226 = 226;
-    export const a227 = 227;
-    export const a228 = 228;
-    export const a229 = 229;
-    export const a230 = 230;
-    export const a231 = 231;
-    export const a232 = 232;
-    export const a233 = 233;
-    export const a234 = 234;
-    export const a235 = 235;
-    export const a236 = 236;
-    export const a237 = 237;
-    export const a238 = 238;
-    export const a239 = 239;
-    export const a240 = 240;
-    export const a241 = 241;
-    export const a242 = 242;
-    export const a243 = 243;
-    export const a244 = 244;
-    export const a245 = 245;
-    export const a246 = 246;
-    export const a247 = 247;
-    export const a248 = 248;
-    export const a249 = 249;
-  `,
-  test('regression T7160')`
-    export var foo = function(gen, ctx = null) {
-
-    }
-
-    export var bar = (gen, ctx = null) => {
-
-    }
-  `,
-  test('regression T7165')`
-    import foo from 'foo';
-    import { something } from 'bar';
-
-    const anything = {};
-
-    export * from 'bar';
-  `,
-  test('regression T7199')`
-    import foo from 'foo';
-    const [x] = bar;
-  `,
-  test('regression T7272')`
-    export const state = (state) => state.a
   `,
   test('shadowed-namespace-import transformed-name')`
     import * as e from "mod";
@@ -1090,71 +598,8 @@ const cases = [
       };
     };
   `,
-  test('strict export-1')`
-    export default foo;
-  `,
-  test('strict export-2')`
-    var foo;
-    export { foo as default };
-  `,
-  test('strict export-3')`
-    export {};
-
-    export {} from 'foo';
-  `,
-  test('strict export-all')`
-    export let z = 100;
-    export * from 'mod';
-    export class a {}
-    export function b() {}
-    export { c } from 'mod';
-    export let d = 42;
-    export let e = 1, f = 2;
-    export default function() {}
-  `,
-  test('strict export-const-destructuring-array-default-params')`
-    export const [foo, bar = 2] = [];
-  `,
-  test('strict export-const-destructuring-array-rest')`
-    export const [foo, bar, ...baz] = [];
-  `,
-  test('strict export-const-destructuring-array')`
-    export const [foo, bar] = [];
-  `,
-  test('strict export-const-destructuring-deep')`
-    export const { foo: { bar: [baz, qux] } } = {};
-  `,
-  test('strict export-const-destructuring-object-default-params')`
-    export const { foo, bar = 1 } = {};
-  `,
-  test('strict export-const-destructuring-object-rest')`
-    export const { foo, ...bar } = {};
-  `,
-  test('strict export-const-destructuring-object')`
-    export const { foo: bar, baz } = {};
-  `,
-  test('strict export')`
-    export function foo() {}
-  `,
-  test('strict import-wildcard')`
-    import * as foo from 'foo';
-
-    foo.bar();
-    foo.baz();
-  `,
-  test('strict import')`
-    import foo from "foo";
-    import { default as foo2 } from "foo";
-    import { foo3 } from "foo";
-    import * as foo4 from "foo";
-
-    foo;
-    foo2;
-    foo3;
-    foo3();
-    foo3\`\`;
-    foo3?.();
-  `,
+  // TODO(krystofwoldrich): updates to foo, bar and baz are technically done before the export,
+  // but Babel and Rollup do the updates directly on exports
   test('update-expression bigint')`
     export let foo = 1n;
 
@@ -1173,6 +618,7 @@ const cases = [
     bar--;
     baz--;
   `,
+  // TODO(krystofwoldrich): --diffLevel should update the exported value
   test('update-expression negative-suffix')`
     export let diffLevel = 0;
 
@@ -1182,6 +628,7 @@ const cases = [
       }
     }
   `,
+  // TODO(krystofwoldrich): ++diffLevel should update the exported value
   test('update-expression positive-suffix')`
     export let diffLevel = 0;
 
