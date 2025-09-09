cb7ab748e927ac8277b6c5aa9a3ed619976c8463
Phil Pluckthun <phil@kitten.sh>
2025-09-09 08:00:58 +0100

    install-expo-modules: Remove find-up

--- packages/install-expo-modules/src/utils/projectRoot.ts
+++ packages/install-expo-modules/src/utils/projectRoot.ts
@@ -1,6 +1,5 @@
 import assert from 'assert';
-import findUp from 'find-up';
-import fs from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 
 export async function normalizeProjectRootAsync(
@@ -17,14 +16,21 @@ export async function normalizeProjectRootAsync(
 }
 
 function findUpPackageJson(root: string): string {
-  const packageJson = findUp.sync('package.json', { cwd: root });
+  let packageJson: string | null = null;
+  for (let dir = root; path.dirname(dir) !== dir; dir = path.dirname(dir)) {
+    const file = path.resolve(dir, 'package.json');
+    if (fs.existsSync(file)) {
+      packageJson = file;
+      break;
+    }
+  }
   assert(packageJson, `No package.json found for module "${root}"`);
   return packageJson;
 }
 
 async function pathExistsAsync(filePath: string): Promise<boolean> {
   try {
-    await fs.stat(filePath);
+    await fs.promises.stat(filePath);
     return true;
   } catch {
     return false;
