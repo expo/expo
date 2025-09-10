718358343161c3a4db111b4c0d78314079a588ac
aleqsio <mikucki@gmail.com>
2025-09-10 17:16:51 +0200

    Move grandchildren, rename method

--- packages/expo-modules-core/android/src/compose/expo/modules/kotlin/views/ExpoComposeView.kt
+++ packages/expo-modules-core/android/src/compose/expo/modules/kotlin/views/ExpoComposeView.kt
@@ -62,15 +62,6 @@ abstract class ExpoComposeView<T : ComposeProps>(
       }
       return
     }
-
-    for (index in 0..<this.size) {
-      val child = getChildAt(index) as? ExpoComposeView<*> ?: continue
-      with(composableScope) {
-        with(child) {
-          Content()
-        }
-      }
-    }
   }
 
   init {
