6c9709cabbf463b5decbfaa38c1241ccfca5835a
Gregory Moskaliuk <mosckalyuck@gmail.com>
2025-09-14 22:27:56 +0200

    Clean up whitespace in RemoteImageUriException for improved readability

--- packages/expo-contacts/ios/ContactsExceptions.swift
+++ packages/expo-contacts/ios/ContactsExceptions.swift
@@ -116,12 +116,12 @@ internal final class AccessPickerAlreadyPresentedException: Exception {
 
 internal final class RemoteImageUriException: Exception {
   private let providedUri: String
-  
+
   init(_ uri: String) {
     self.providedUri = uri
     super.init()
   }
-  
+
   override var reason: String {
     "Only file:// URIs are supported for contact images. Provided URI: '\(providedUri)'. Please download the image first using File.downloadFileAsync from expo-file-system and provide a local file URI."
   }
