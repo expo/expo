c457165d7d82f988b3a29b711927da78db8cf0f1
Phil Pluckthun <phil@kitten.sh>
2025-09-14 16:13:43 +0100

    Add initial tests

---
+++ packages/@expo/cli/src/start/server/metro/__tests__/prependMainFields.test.ts
@@ -0,0 +1,103 @@
+import { prependMainFields } from '../prependMainFields';
+
+// NOTE: As defined in `@expo/metro-config`
+const DEFAULT_FIELDS = ['react-native', 'browser', 'main'];
+
+it('it constructs default fields', () => {
+  let mainFields: readonly string[];
+
+  // CommonJS + client-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: false,
+    isServerEnv: false,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['react-native', 'browser', 'main']);
+  // ESM + client-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: true,
+    isServerEnv: false,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['react-native', 'browser', 'module', 'main']);
+  // CommonJS + server-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: false,
+    isServerEnv: true,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['react-native', 'main', 'module']);
+  // ESM + server-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: true,
+    isServerEnv: true,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['react-native', 'module', 'main']);
+});
+
+it('it constructs web fields', () => {
+  let mainFields: readonly string[];
+
+  // CommonJS + client-side
+  mainFields = prependMainFields(['browser'], {
+    isESMImport: false,
+    isServerEnv: false,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['browser', 'main']);
+  // ESM + client-side
+  mainFields = prependMainFields(['browser'], {
+    isESMImport: true,
+    isServerEnv: false,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['browser', 'module', 'main']);
+  // CommonJS + server-side
+  mainFields = prependMainFields(['browser'], {
+    isESMImport: false,
+    isServerEnv: true,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['main', 'module']);
+  // ESM + server-side
+  mainFields = prependMainFields(['browser'], {
+    isESMImport: true,
+    isServerEnv: true,
+    userMainFields: DEFAULT_FIELDS,
+  });
+  expect(mainFields).toEqual(['module', 'main']);
+});
+
+it('it constructs customized fields', () => {
+  let mainFields: readonly string[];
+
+  // CommonJS + client-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: false,
+    isServerEnv: false,
+    userMainFields: ['custom'],
+  });
+  expect(mainFields).toEqual(['custom', 'react-native', 'main']);
+  // ESM + client-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: true,
+    isServerEnv: false,
+    userMainFields: ['custom'],
+  });
+  expect(mainFields).toEqual(['custom', 'react-native', 'module', 'main']);
+  // CommonJS + server-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: false,
+    isServerEnv: true,
+    userMainFields: ['custom'],
+  });
+  expect(mainFields).toEqual(['custom', 'react-native', 'main', 'module']);
+  // ESM + server-side
+  mainFields = prependMainFields(['react-native'], {
+    isESMImport: true,
+    isServerEnv: true,
+    userMainFields: ['custom'],
+  });
+  expect(mainFields).toEqual(['custom', 'react-native', 'module', 'main']);
+});
