c51418094d2923ec5ff6707db8c3f9a9c94fa650
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-09-16 20:29:19 +0200

    [expo-modules-core] avoid TurboModuleRegistry import on web
    react-native-web doesn't export TurboModuleRegistry. so importing it fails the build on bundler such as vite which validate whether the export exists.
    
    this splits web and native code to separate files to avoid the issue.

---
+++ packages/expo-modules-core/src/ensureNativeModulesAreInstalled.native.ts
@@ -0,0 +1,22 @@
+import { TurboModuleRegistry } from 'react-native';
+
+/**
+ * Ensures that the native modules are installed in the current runtime.
+ * Otherwise, it synchronously calls a native function that installs them.
+ */
+export function ensureNativeModulesAreInstalled(): void {
+  if (globalThis.expo) {
+    return;
+  }
+
+  try {
+    // TODO: ExpoModulesCore shouldn't be optional here,
+    // but to keep backwards compatibility let's just ignore it in SDK 50.
+    // In most cases the modules were already installed from the native side.
+    (
+      TurboModuleRegistry.get('ExpoModulesCore') as { installModules: () => void } | null
+    )?.installModules();
+  } catch (error) {
+    console.error(`Unable to install Expo modules: ${error}`);
+  }
+}
