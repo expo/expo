2c895dc95fe49092652044b362ccf1ec072436dc
Hugo EXTRAT <extrat.h@gmail.com>
2025-09-18 09:31:28 +0200

    missing import gradle dsl

--- packages/expo-dev-launcher/expo-dev-launcher-gradle-plugin/build.gradle.kts
+++ packages/expo-dev-launcher/expo-dev-launcher-gradle-plugin/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
--- packages/expo-modules-core/expo-module-gradle-plugin/build.gradle.kts
+++ packages/expo-modules-core/expo-module-gradle-plugin/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
