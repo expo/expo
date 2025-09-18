42c70bdf692df05002e5b4de5d79c2daa9fd1582
Christian Falch <875252+chrfalch@users.noreply.github.com>
2025-09-18 13:41:01 +0200

    Update packages/expo-background-task/src/BackgroundTask.ts
    Co-authored-by: Aman Mittal <amandeepmittal@live.com>

--- packages/expo-background-task/src/BackgroundTask.ts
+++ packages/expo-background-task/src/BackgroundTask.ts
@@ -144,7 +144,6 @@ export async function triggerTaskWorkerForTestingAsync(): Promise<boolean> {
  * for minutes, but the system can interrupt the process at any time. This listener is called
  * when the system decides to stop the background tasks and should be used to clean up resources
  * or save state. When the expiry handler is called, the main task runner is rescheduled automatically.
- * This listener is only available on iOS.
  * @platform ios
  * @return An object with a `remove` method to unsubscribe the listener.
  */
