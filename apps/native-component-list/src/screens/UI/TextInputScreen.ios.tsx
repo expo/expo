60505de49d24f9c90f2efcf5a63c1f89ab6305e8
nishan (o^▽^o) <nishanbende@gmail.com>
2025-09-19 21:15:38 +0530

    rn text input vs expo ui textinput

--- apps/native-component-list/src/screens/UI/TextInputScreen.ios.tsx
+++ apps/native-component-list/src/screens/UI/TextInputScreen.ios.tsx
@@ -7,7 +7,7 @@ import {
   TextFieldProps,
 } from '@expo/ui/swift-ui';
 import * as React from 'react';
-import { Text, TextInput as RNTextInput, View } from 'react-native';
+import { Text, TextInput as RNTextInput, View, TextInput } from 'react-native';
 
 import { Page, Section } from '../../components/Page';
 
@@ -20,12 +20,17 @@ export default function TextInputScreen() {
     } else if (value === '2') {
       setValue(value);
     } else if (value === '3') {
+      console.log('will set in a sec');
       await new Promise((resolve) => setTimeout(resolve, 1000));
       setValue('three');
     }
 
     if (value === '4') {
       setValue('four');
+    }
+
+    if (value === '5') {
+      setValue('hello world');
     }
 
     if (value === '') {
@@ -35,15 +40,18 @@ export default function TextInputScreen() {
 
   console.log('value', value);
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Page>
-        <Section title="Current value">
-          <Text>{JSON.stringify(value)}</Text>
-        </Section>
-        <Host matchContents>
-          <ControlledTextField value={value} onChangeText={onChangeText} placeholder="Type here" />
-        </Host>
-      </Page>
+    <View style={{ flex: 1, justifyContent: 'center', gap: 16 }}>
+      <Text>Expo UITextInput</Text>
+      <Host matchContents>
+        <ControlledTextField value={value} onChangeText={onChangeText} placeholder="Type here" />
+      </Host>
+      <Text>React Native TextInput</Text>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        placeholder="Type here"
+        style={{ height: 40 }}
+      />
     </View>
   );
 }
