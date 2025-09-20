b6055c313099b9740907c71203824d7e24592af2
Dennis Morello <dennismorello@gmail.com>
2025-09-20 10:00:59 +0200

    [ios][ui] add variable value support for Image component

--- packages/expo-ui/ios/ImageView.swift
+++ packages/expo-ui/ios/ImageView.swift
@@ -13,6 +13,7 @@ internal final class ImageViewProps: ExpoSwiftUI.ViewProps, CommonViewModifierPr
   @Field var systemName: String = ""
   @Field var size: Double?
   @Field var color: Color?
+  @Field var variableValue: Double?
   @Field var useTapGesture: Bool?
   var onTap = EventDispatcher()
 }
@@ -21,7 +22,7 @@ internal struct ImageView: ExpoSwiftUI.View {
   @ObservedObject var props: ImageViewProps
 
   var body: some View {
-    Image(systemName: props.systemName)
+    Image(systemName: props.systemName, variableValue: props.variableValue)
       .font(.system(size: CGFloat(props.size ?? 24)))
       .foregroundColor(props.color)
       .modifier(CommonViewModifiers(props: props))
--- packages/expo-ui/src/swift-ui/Image/index.tsx
+++ packages/expo-ui/src/swift-ui/Image/index.tsx
@@ -20,6 +20,11 @@ export interface ImageProps extends CommonViewModifierProps {
    * Can be a color name like '#ff00ff', 'red', 'blue', etc.
    */
   color?: string;
+  /**
+   * The variable value for SF Symbols with variable color support.
+   * Can be a number between 0.0 and 1.0.
+   */
+  variableValue?: number;
 
   /**
    * Callback triggered when the view is pressed.
