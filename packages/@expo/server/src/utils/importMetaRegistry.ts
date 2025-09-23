456d4ff195d791764a72e6b38fe3c477e84585f0
Phil Pluckthun <phil@kitten.sh>
2025-09-23 17:58:35 +0100

    Add fallback for getCallSites

--- packages/@expo/server/src/utils/importMetaRegistry.ts
+++ packages/@expo/server/src/utils/importMetaRegistry.ts
@@ -1,4 +1,4 @@
-import { getCallSites } from 'node:util';
+import * as node_util from 'node:util';
 
 const DEFAULT_SCRIPT_NAME = 'file:///__main.js';
 
@@ -7,10 +7,28 @@ const DEFAULT_SCRIPT_NAME = 'file:///__main.js';
 // - <source>
 const CALL_DEPTH = 3;
 
+function getFileName(offset = 0): any {
+  const originalStackFormatter = Error.prepareStackTrace;
+  const originalStackTraceLimit = Error.stackTraceLimit;
+  try {
+    Error.stackTraceLimit = offset;
+    Error.prepareStackTrace = (_err, stack) => stack[offset - 1]?.getFileName();
+    return new Error().stack as any;
+  } finally {
+    Error.prepareStackTrace = originalStackFormatter;
+    Error.stackTraceLimit = originalStackTraceLimit;
+  }
+}
+
 export const importMetaRegistry = {
   get url() {
-    const callSites = getCallSites(CALL_DEPTH);
-    let scriptName = callSites[callSites.length - 1]?.scriptName;
+    let scriptName: string;
+    if (node_util.getCallSites) {
+      const callSites = node_util.getCallSites(CALL_DEPTH);
+      scriptName = callSites[callSites.length - 1]?.scriptName;
+    } else {
+      scriptName = getFileName(CALL_DEPTH);
+    }
     if (scriptName?.[0] === '/') scriptName = `file://${scriptName}`;
     return scriptName || DEFAULT_SCRIPT_NAME;
   },
