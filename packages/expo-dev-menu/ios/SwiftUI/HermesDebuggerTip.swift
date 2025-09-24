3647a8cf3c2108e4786d3cc7a8dc2999dfde331b
Vojtech Novak <vonovak@gmail.com>
2025-09-24 12:40:55 +0200

    move iOS colors to a new file

--- packages/expo-dev-menu/ios/SwiftUI/HermesDebuggerTip.swift
+++ packages/expo-dev-menu/ios/SwiftUI/HermesDebuggerTip.swift
@@ -1,20 +1,12 @@
 import SwiftUI
 
-extension Color {
-  static let tipBlue = Color(UIColor { traitCollection in
-    traitCollection.userInterfaceStyle == .dark
-      ? UIColor(displayP3Red: 0.49, green: 0.72, blue: 1.0, alpha: 1.0) // blue.11 dark
-      : UIColor(displayP3Red: 0.15, green: 0.44, blue: 0.84, alpha: 1.0) // blue.11 light
-  })
-
-  static let tipBackground = Color(UIColor { traitCollection in
-    traitCollection.userInterfaceStyle == .dark
-      ? UIColor(displayP3Red: 0.078, green: 0.154, blue: 0.27, alpha: 1.0) // blue.3 dark
-      : UIColor(displayP3Red: 0.912, green: 0.956, blue: 0.991, alpha: 1.0) // blue.3 light
-  })
-}
-
 struct HermesDebuggerTip: View {
+  @Environment(\.colorScheme) private var colorScheme
+
+  private var colors: DevMenuColors {
+    DevMenuColors(colorScheme: colorScheme)
+  }
+
   var body: some View {
     VStack(alignment: .leading, spacing: 8) {
       HStack {
@@ -24,13 +16,13 @@ struct HermesDebuggerTip: View {
 
         Spacer()
       }
-      .foregroundColor(Color.tipBlue)
+      .foregroundColor(colors.tipBlue)
 
       Text("Debugging not working? Try manually reloading first.")
         .font(.caption)
     }
     .padding()
-    .background(Color.tipBackground)
+    .background(colors.tipBackground)
     .cornerRadius(18)
   }
 }
