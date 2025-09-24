97e26615d8bc1104786138fd7bb6a8298b84497c
nishan (o^▽^o) <nishanbende@gmail.com>
2025-09-24 16:02:54 +0530

    add reanimated example back

--- apps/native-component-list/src/screens/UI/BottomSheetScreen.ios.tsx
+++ apps/native-component-list/src/screens/UI/BottomSheetScreen.ios.tsx
@@ -1,7 +1,8 @@
 import { Button, BottomSheet, Host, VStack, HStack, Switch, Text } from '@expo/ui/swift-ui';
 import { fixedSize, frame, padding } from '@expo/ui/swift-ui/modifiers';
 import * as React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, useWindowDimensions, View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 export default function BottomSheetScreen() {
   const [isOpened, setIsOpened] = React.useState<boolean>(false);
@@ -45,7 +46,36 @@ export default function BottomSheetScreen() {
           </HStack>
         </BottomSheet>
       </Host>
+      <BottomSheetReanimatedExample />
     </ScrollView>
+  );
+}
+
+function BottomSheetReanimatedExample() {
+  const [isOpened, setIsOpened] = React.useState<boolean>(true);
+  const { width } = useWindowDimensions();
+  const height = useSharedValue(100);
+
+  const handleIncreaseHeight = () => {
+    height.value = height.value > 500 ? 100 : height.value + 100;
+  };
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    height: withTiming(height.value, { duration: 1000 }),
+  }));
+
+  return (
+    <View>
+      <Host style={{ position: 'absolute', width }}>
+        <BottomSheet isOpened={isOpened} onIsOpenedChange={(e) => setIsOpened(e)}>
+          <Animated.View style={[{ padding: 20 }, animatedStyles]}>
+            <Host matchContents>
+              <Button onPress={handleIncreaseHeight}>Increase height</Button>
+            </Host>
+          </Animated.View>
+        </BottomSheet>
+      </Host>
+    </View>
   );
 }
 
