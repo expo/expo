3eda48cefa1730dd4744e3e5e001cffef686c8e8
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-09-29 15:03:49 +0200

    Dont provide name to JSLogger and NativeModulesProxy to force initialize on startup

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/AppContext.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/AppContext.kt
@@ -30,9 +30,7 @@ import expo.modules.kotlin.defaultmodules.AppDirectoriesModule
 import expo.modules.kotlin.defaultmodules.ErrorManagerModule
 import expo.modules.kotlin.defaultmodules.FilePermissionModule
 import expo.modules.kotlin.defaultmodules.JSLoggerModule
-import expo.modules.kotlin.defaultmodules.JSLoggerModuleName
 import expo.modules.kotlin.defaultmodules.NativeModulesProxyModule
-import expo.modules.kotlin.defaultmodules.NativeModulesProxyModuleName
 import expo.modules.kotlin.events.EventEmitter
 import expo.modules.kotlin.events.EventName
 import expo.modules.kotlin.events.KEventEmitterWrapper
@@ -112,8 +110,8 @@ class AppContext(
       // Registering modules has to happen at the very end of `AppContext` creation. Some modules need to access
       // `AppContext` during their initialisation, so we need to ensure all `AppContext`'s
       // properties are initialized first. Not having that would trigger NPE.
-      registry.register(NativeModulesProxyModule(), NativeModulesProxyModuleName)
-      registry.register(JSLoggerModule(), JSLoggerModuleName)
+      registry.register(NativeModulesProxyModule(), null)
+      registry.register(JSLoggerModule(), null)
 
       // Registering modules that were previously provided by legacy FileSystem module.
       legacyModuleRegistry.registerInternalModule(FilePermissionModule())
