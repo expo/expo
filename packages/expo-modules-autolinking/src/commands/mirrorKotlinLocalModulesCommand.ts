8b70133abe037c9b87059018e8ab89782468a604
HubertBer <115428831+HubertBer@users.noreply.github.com>
2025-09-29 12:14:11 +0200

    refactor (#21)

--- packages/expo-modules-autolinking/src/commands/mirrorKotlinLocalModulesCommand.ts
+++ packages/expo-modules-autolinking/src/commands/mirrorKotlinLocalModulesCommand.ts
@@ -14,25 +14,32 @@ interface ResolveArguments extends AutolinkingCommonArguments {
 export function mirrorKotlinLocalModulesCommand(cli: commander.CommanderStatic) {
   return registerAutolinkingArguments(
     cli.command(
-      'mirror-kotlin-local-modules <mirrorPath> <localModulesListPath> <watchedDirsSerialized>'
+      'mirror-kotlin-local-modules <kotlinFilesMirrorDirectory> <localModulesListPath> <watchedDirsSerialized>'
     )
   ).action(
-    async (mirrorPath: string, localModulesListPath: string, watchedDirsSerialized: string) => {
+    async (
+      kotlinFilesMirrorDirectory: string,
+      localModulesListPath: string,
+      watchedDirsSerialized: string
+    ) => {
       const watchedDirs = JSON.parse(watchedDirsSerialized);
-      if (!mirrorPath || !localModulesListPath) {
-        throw new Error('Need to provide mirrorPath and localModulesListPath!');
+      if (!kotlinFilesMirrorDirectory || !localModulesListPath) {
+        throw new Error('Need to provide kotlinFilesMirrorDirectory and localModulesListPath!');
       }
-      if (!/.android./.test(mirrorPath) || !/.android./.test(localModulesListPath)) {
+      if (
+        !/.android./.test(kotlinFilesMirrorDirectory) ||
+        !/.android./.test(localModulesListPath)
+      ) {
         throw new Error('Generation path is not inside any android directory!');
       }
-      if (!path.isAbsolute(mirrorPath) || !path.isAbsolute(localModulesListPath)) {
+      if (!path.isAbsolute(kotlinFilesMirrorDirectory) || !path.isAbsolute(localModulesListPath)) {
         throw new Error(
           'Need to provide the absolute path to both the local modules src mirror and generated mirror directory!'
         );
       }
 
-      fs.rmSync(mirrorPath, { recursive: true, force: true });
-      await createSymlinksToKotlinFiles(mirrorPath, watchedDirs);
+      fs.rmSync(kotlinFilesMirrorDirectory, { recursive: true, force: true });
+      await createSymlinksToKotlinFiles(kotlinFilesMirrorDirectory, watchedDirs);
       await generateLocalModulesListFile(localModulesListPath, watchedDirs);
     }
   );
