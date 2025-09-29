7fab05f1a05f959c8f98ba5474476044e241455b
Phil Pluckthun <phil@kitten.sh>
2025-09-29 15:05:33 +0100

    Remove ExpoRequest/ExpoResponse type reexports

--- packages/@expo/server/src/index.ts
+++ packages/@expo/server/src/index.ts
@@ -1,6 +1,1 @@
-/** @deprecated Use Fetch API `Request` instead. */
-export type ExpoRequest = Request;
-/** @deprecated Use Fetch API `Response` instead. */
-export type ExpoResponse = Request;
-
 export type * from './types';
