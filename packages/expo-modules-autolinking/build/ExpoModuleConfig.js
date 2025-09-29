4b5331570886bd2e79cc1c46e87aca827d064005
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-09-29 14:32:57 +0200

    Rebuild

--- packages/expo-modules-autolinking/build/ExpoModuleConfig.js
+++ packages/expo-modules-autolinking/build/ExpoModuleConfig.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.discoverExpoModuleConfigAsync = exports.ExpoModuleConfig = exports.ExpoAndroidProjectConfig = void 0;
+exports.discoverExpoModuleConfigAsync = exports.ExpoModuleConfig = exports.ExpoAndroidProjectConfig = exports.ExpoAndroidModuleConfig = void 0;
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const utils_1 = require("./utils");
