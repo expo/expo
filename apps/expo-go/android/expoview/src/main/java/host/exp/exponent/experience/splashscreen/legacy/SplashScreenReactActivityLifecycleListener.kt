addbb47372293a526d94e57d612b5f0681caf08d
Alan Hughes <alan@expo.io>
2025-09-30 15:05:01 +0100

    [expo-go] Fix getDelayLoadAppHandler signature

--- apps/expo-go/android/expoview/src/main/java/host/exp/exponent/experience/splashscreen/legacy/SplashScreenReactActivityLifecycleListener.kt
+++ apps/expo-go/android/expoview/src/main/java/host/exp/exponent/experience/splashscreen/legacy/SplashScreenReactActivityLifecycleListener.kt
@@ -3,6 +3,7 @@ package host.exp.exponent.experience.splashscreen.legacy
 import android.app.Activity
 import android.content.Context
 import com.facebook.react.ReactActivity
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactRootView
 import expo.modules.core.interfaces.ReactActivityHandler
@@ -23,15 +24,17 @@ class SplashScreenReactActivityLifecycleListener : ReactActivityLifecycleListene
 
 class SplashScreenReactActivityHandler : ReactActivityHandler {
   override fun getDelayLoadAppHandler(
-    activity: ReactActivity,
-    reactNativeHost: ReactNativeHost
+    activity: ReactActivity?,
+    reactHost: ReactHost?
   ): ReactActivityHandler.DelayLoadAppHandler? {
-    SplashScreen.ensureShown(
-      activity,
-      getResizeMode(activity),
-      ReactRootView::class.java,
-      getStatusBarTranslucent(activity)
-    )
+    activity?.let {
+      SplashScreen.ensureShown(
+        it,
+        getResizeMode(it),
+        ReactRootView::class.java,
+        getStatusBarTranslucent(it)
+      )
+    }
     return null
   }
 }
