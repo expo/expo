2b9bcdeb22c9b484bf467e535df7ba5acddf0ad6
nishan (o^▽^o) <nishanbende@gmail.com>
2025-10-01 17:37:09 +0530

    use enum instead of string

--- packages/expo-ui/ios/Modifiers/ViewModifierRegistry.swift
+++ packages/expo-ui/ios/Modifiers/ViewModifierRegistry.swift
@@ -826,22 +826,32 @@ internal struct ContainerShapeModifier: ViewModifier, Record {
   }
 }
 
+internal enum ButtonStyle: String, Enumerable {
+  case automatic
+  case bordered
+  case borderedProminent
+  case borderless
+  case glass
+  case glassProminent
+  case plain
+}
+
 internal struct ButtonStyleModifier: ViewModifier, Record {
-  @Field var style: String = "automatic"
+  @Field var style: ButtonStyle = .automatic
 
   func body(content: Content) -> some View {
     switch style {
-    case "bordered":
+    case .bordered:
       content.buttonStyle(.bordered)
-    case "borderedProminent":
+    case .borderedProminent:
       content.buttonStyle(.borderedProminent)
-    case "borderless":
+    case .borderless:
       if #available(iOS 13.0, macOS 10.15, tvOS 17.0, *) {
         content.buttonStyle(.borderless)
       } else {
         content.buttonStyle(.automatic)
       }
-    case "glass":
+    case .glass:
       if #available(iOS 26.0, macOS 26.0, tvOS 26.0, *) {
         #if compiler(>=6.2) // Xcode 26
         content.buttonStyle(.glass)
@@ -851,7 +861,7 @@ internal struct ButtonStyleModifier: ViewModifier, Record {
       } else {
         content.buttonStyle(.automatic)
       }
-    case "glassProminent":
+    case .glassProminent:
       if #available(iOS 26.0, macOS 26.0, tvOS 26.0, *) {
         #if compiler(>=6.2) // Xcode 26
         content.buttonStyle(.glassProminent)
@@ -861,7 +871,7 @@ internal struct ButtonStyleModifier: ViewModifier, Record {
       } else {
         content.buttonStyle(.automatic)
       }
-    case "plain":
+    case .plain:
       content.buttonStyle(.plain)
     default:
       content.buttonStyle(.automatic)
