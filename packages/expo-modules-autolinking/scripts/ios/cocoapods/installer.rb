f5d8a7f065519c6589b54443854c107a11c787de
Christian Falch <christian.falch@gmail.com>
2025-10-07 12:43:13 +0200

    codereview: updated script
    Updated to use same patterns as original rn script

--- packages/expo-modules-autolinking/scripts/ios/cocoapods/installer.rb
+++ packages/expo-modules-autolinking/scripts/ios/cocoapods/installer.rb
@@ -52,25 +52,40 @@ module Pod
           phase = react_codegen_native_target.new_shell_script_build_phase(_script_phase_name)
           phase.shell_path = '/bin/sh'
           phase.shell_script = <<~SH
+            # Remove this step when the fix is merged and released.
+            # See: https://github.com/facebook/react-native/pull/54066
+
+            # This re-runs Codegen without the broken "scripts/react_native_pods_utils/script_phases.sh" script, causing Codegen to run without autolinking.
+            # Instead of using "script_phases.sh" which always runs inside DerivedData, we run it in the normal "/ios" folder
+            # See: https://github.com/facebook/react-native/blob/main/packages/react-native/scripts/react_native_pods_utils/script_phases.sh
             pushd "$PODS_ROOT/../" > /dev/null
-            RCT_SCRIPT_POD_INSTALLATION_ROOT=$(pwd)
+              RCT_SCRIPT_POD_INSTALLATION_ROOT="$PODS_ROOT/.."
             popd >/dev/null
 
+            export RCT_SCRIPT_RN_DIR="$REACT_NATIVE_PATH" # This is set by Expo
             export RCT_SCRIPT_APP_PATH="$RCT_SCRIPT_POD_INSTALLATION_ROOT/.."
             export RCT_SCRIPT_OUTPUT_DIR="$RCT_SCRIPT_POD_INSTALLATION_ROOT"
             export RCT_SCRIPT_TYPE="withCodegenDiscovery"
 
-            # This is a workaround to not let Codegen run incorrect paths
-            # Instead of running "/scripts/react_native_pods_utils/script_phases.sh", we run the JS script executed within this shell script
-            # That uses the correct paths for Codegen
-            # Based on: https://github.com/facebook/react-native/blob/3f7f9d8fb8beb41408d092870a7c7cac58029a4d/packages/react-native/scripts/react_native_pods_utils/script_phases.sh#L98C4-L100C30
-            # Based on: https://github.com/facebook/react-native/blob/b9baddfd5d25af27d82c15c0cba09f286335f1d3/packages/react-native/scripts/codegen/generate-artifacts-executor/generateReactCodegenPodspec.js#L82-L93
+            # This is the broken script that runs inside DerivedData, meaning it can't find the autolinking result in `ios/build/generated/autolinking.json`.
+            # Resulting in Codegen running with it's own autolinking, not discovering transitive peer dependencies.
+            # export SCRIPT_PHASES_SCRIPT="$RCT_SCRIPT_RN_DIR/scripts/react_native_pods_utils/script_phases.sh"
+            export WITH_ENVIRONMENT="$RCT_SCRIPT_RN_DIR/scripts/xcode/with-environment.sh"
 
-            source "$REACT_NATIVE_PATH/scripts/xcode/with-environment.sh"
+            # Start of workaround code
 
-            pushd "$REACT_NATIVE_PATH" >/dev/null || exit 1
+            # Load the $NODE_BINARY from the "with-environment.sh" script
+            source "$WITH_ENVIRONMENT"
+
+            # Run this script directly in the right folders:
+            # https://github.com/facebook/react-native/blob/3f7f9d8fb8beb41408d092870a7c7cac58029a4d/packages/react-native/scripts/react_native_pods_utils/script_phases.sh#L96-L101
+            pushd "$RCT_SCRIPT_RN_DIR" >/dev/null || exit 1
+              set -x
               "$NODE_BINARY" "scripts/generate-codegen-artifacts.js" --path "$RCT_SCRIPT_APP_PATH" --outputPath "$RCT_SCRIPT_OUTPUT_DIR" --targetPlatform "ios"
+              set +x
             popd >/dev/null || exit 1
+
+            # End of workaround code
           SH
 
           # Find the index of the "Compile Sources" phase (PBXSourcesBuildPhase)
