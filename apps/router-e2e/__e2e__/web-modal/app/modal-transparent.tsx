454faf4701c00c2002b28dc458595530c910941c
Jakub Tkacz <jtkacz@expo.io>
2025-10-08 14:56:33 +0200

    [expo-router] support usePreventRemove in web modal

--- apps/router-e2e/__e2e__/web-modal/app/modal-transparent.tsx
+++ apps/router-e2e/__e2e__/web-modal/app/modal-transparent.tsx
@@ -1,6 +1,27 @@
-import { Text, View } from 'react-native';
+import { usePreventRemove } from '@react-navigation/native';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    width: 180,
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 12,
+    alignItems: 'center',
+    backgroundColor: '#333',
+    color: 'white',
+    fontSize: 14,
+  },
+});
 
 export default function Page() {
+  const [isPrevented, setIsPrevented] = useState(false);
+  const router = useRouter();
+  usePreventRemove(isPrevented, () => {
+    Alert.alert('Prevented!', 'You cannot dismiss this modal right now.');
+  });
   return (
     <View style={{ flex: 1, padding: 60, backgroundColor: 'rgba(255, 0, 0, 0.3)' }}>
       <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>Transparent Modal</Text>
@@ -12,6 +33,24 @@ export default function Page() {
         The modal is transparent, so you can see the background behind it. The background color is
         not part of the modal, it's the background of the page.
       </Text>
+      <Pressable
+        onPress={() => {
+          router.back();
+        }}>
+        <Text style={styles.button}>Go back</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          router.dismissAll();
+        }}>
+        <Text style={styles.button}>Dismiss all</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          setIsPrevented((p) => !p);
+        }}>
+        <Text style={styles.button}>{isPrevented ? 'Disable' : 'Enable'} preventRemove</Text>
+      </Pressable>
     </View>
   );
 }
