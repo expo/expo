fae74ecffea87f8d25a6d9bf7b8bb8b9a9f59177
Wojciech Dróżdż <behenate@gmail.com>
2025-10-08 17:24:09 +0200

    Run spotless

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/edgeToEdge/EdgeToEdgePackage.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/edgeToEdge/EdgeToEdgePackage.kt
@@ -13,7 +13,7 @@ import expo.modules.core.BasePackage
 import expo.modules.core.interfaces.ReactActivityLifecycleListener
 import kotlin.Unit
 
-class EdgeToEdgePackage: BasePackage() {
+class EdgeToEdgePackage : BasePackage() {
   override fun createReactActivityLifecycleListeners(activityContext: Context?): List<ReactActivityLifecycleListener?>? {
     return listOf(object : ReactActivityLifecycleListener {
       override fun onCreate(activity: Activity?, savedInstanceState: Bundle?) {
