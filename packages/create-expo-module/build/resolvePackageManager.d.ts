a902faac23cfb2b18d5f0750ee3c8c11e541306a
Cedric van Putten <github@cedric.dev>
2025-10-09 11:44:09 +0200

    refactor(create-expo-module): build the package to a single file and drop `build` folder

--- packages/create-expo-module/build/resolvePackageManager.d.ts
+++
@@ -1,4 +0,0 @@
-export type PackageManagerName = 'npm' | 'pnpm' | 'yarn' | 'bun';
-/** Determine which package manager to use for installing dependencies based on how the process was started. */
-export declare function resolvePackageManager(): PackageManagerName;
-export declare function formatRunCommand(manager: PackageManagerName, cmd: string): string;
