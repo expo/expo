9c2e54c81d9c58cec14894b161e2306690039f8d
Tomasz Sapeta <tsapeta@expo.dev>
2025-10-14 18:56:36 +0200

    Fix native tests

--- packages/expo-modules-core/ios/Core/ModuleHolder.swift
+++ packages/expo-modules-core/ios/Core/ModuleHolder.swift
@@ -54,6 +54,7 @@ public final class ModuleHolder {
 
   // MARK: Calling functions
 
+  @preconcurrency
   func call(function functionName: String, args: [Any], _ callback: @Sendable @escaping (FunctionCallResult) -> () = { _ in }) {
     guard let appContext else {
       callback(.failure(Exceptions.AppContextLost()))
