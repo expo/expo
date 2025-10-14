aab39335abe222ca3c5a0cabaa6598773d449bd7
Phil Pluckthun <phil@kitten.sh>
2025-10-14 12:00:29 +0100

    Reformat unit tests

--- packages/expo-server/src/runtime/__tests__/index.test.ts
+++ packages/expo-server/src/runtime/__tests__/index.test.ts
@@ -47,123 +47,131 @@ it('provides specified origin as a global', async () => {
   expect(result).toBeInstanceOf(Response);
 });
 
-it('calls waitUntil with specified runTask invocation', async () => {
-  const mockTask = jest.fn().mockResolvedValue(undefined);
-  const waitUntil = jest.fn();
-  const run = createRequestScope(STORE, () => ({ waitUntil }));
-  const result = await run(async () => {
-    runTask(mockTask);
+describe('runTask', () => {
+  it('calls waitUntil with specified runTask invocation', async () => {
+    const mockTask = jest.fn().mockResolvedValue(undefined);
+    const waitUntil = jest.fn();
+    const run = createRequestScope(STORE, () => ({ waitUntil }));
+    const result = await run(async () => {
+      runTask(mockTask);
+      expect(mockTask).toHaveBeenCalled();
+      expect(waitUntil).toHaveBeenCalled();
+      return Response.json({ ok: true });
+    });
+    expect(result).toBeInstanceOf(Response);
+  });
+
+  it('provides mock waitUntil implementation if none is provided', async () => {
+    const mockTask = jest.fn().mockResolvedValue(undefined);
+    const run = createRequestScope(STORE, () => ({}));
+    const result = await run(async () => {
+      runTask(mockTask);
+      expect(mockTask).toHaveBeenCalled();
+      return Response.json({ ok: true });
+    });
+    expect(result).toBeInstanceOf(Response);
+  });
+});
+
+describe('deferTask', () => {
+  it('calls waitUntil with specified deferTask invocation after response resolved', async () => {
+    const mockTask = jest.fn().mockResolvedValue(undefined);
+    const waitUntil = jest.fn();
+    const run = createRequestScope(STORE, () => ({ waitUntil }));
+    const result = await run(async () => {
+      deferTask(mockTask);
+      expect(mockTask).not.toHaveBeenCalled();
+      expect(waitUntil).not.toHaveBeenCalled();
+      return Response.json({ ok: true });
+    });
+    expect(result).toBeInstanceOf(Response);
     expect(mockTask).toHaveBeenCalled();
     expect(waitUntil).toHaveBeenCalled();
-    return Response.json({ ok: true });
   });
-  expect(result).toBeInstanceOf(Response);
-});
 
-it('calls waitUntil with specified deferTask invocation after response resolved', async () => {
-  const mockTask = jest.fn().mockResolvedValue(undefined);
-  const waitUntil = jest.fn();
-  const run = createRequestScope(STORE, () => ({ waitUntil }));
-  const result = await run(async () => {
-    deferTask(mockTask);
+  it('ignores deferred tasks on error', async () => {
+    const mockTask = jest.fn().mockResolvedValue(undefined);
+    const waitUntil = jest.fn();
+    const run = createRequestScope(STORE, () => ({ waitUntil }));
+    const result = await run(async () => {
+      deferTask(mockTask);
+      throw new StatusError();
+    });
+    expect(result).toBeInstanceOf(Response);
     expect(mockTask).not.toHaveBeenCalled();
     expect(waitUntil).not.toHaveBeenCalled();
-    return Response.json({ ok: true });
   });
-  expect(result).toBeInstanceOf(Response);
-  expect(mockTask).toHaveBeenCalled();
-  expect(waitUntil).toHaveBeenCalled();
 });
 
-it('provides mock waitUntil implementation if none is provided', async () => {
-  const mockTask = jest.fn().mockResolvedValue(undefined);
-  const run = createRequestScope(STORE, () => ({}));
-  const result = await run(async () => {
-    runTask(mockTask);
-    expect(mockTask).toHaveBeenCalled();
-    return Response.json({ ok: true });
-  });
-  expect(result).toBeInstanceOf(Response);
-});
-
-it('ignores deferred tasks on error', async () => {
-  const mockTask = jest.fn().mockResolvedValue(undefined);
-  const waitUntil = jest.fn();
-  const run = createRequestScope(STORE, () => ({ waitUntil }));
-  const result = await run(async () => {
-    deferTask(mockTask);
-    throw new StatusError();
-  });
-  expect(result).toBeInstanceOf(Response);
-  expect(mockTask).not.toHaveBeenCalled();
-  expect(waitUntil).not.toHaveBeenCalled();
-});
-
-it('uses StatusError to construct error response if one is thrown', async () => {
-  const run = createRequestScope(STORE, () => ({}));
-  const result = await run(async () => {
-    throw new StatusError(418, 'I might be a teapot');
-  });
-  expect(result).toBeInstanceOf(Response);
-  expect(result.status).toBe(418);
-  expect(await result.text()).toBe('I might be a teapot');
-});
-
-it('uses StatusError to construct a JSON response if one is thrown', async () => {
-  const run = createRequestScope(STORE, () => ({}));
-  const result = await run(async () => {
-    throw new StatusError(418, { test: 'I might be a teapot' });
-  });
-  expect(result).toBeInstanceOf(Response);
-  expect(result.status).toBe(418);
-  expect(await result.json()).toEqual({ test: 'I might be a teapot' });
-});
-
-it('uses thrown Response as response if thrown', async () => {
-  const run = createRequestScope(STORE, () => ({}));
-  const result = await run(async () => {
-    throw new Response(null, { status: 204 });
-  });
-  expect(result).toBeInstanceOf(Response);
-  expect(result.status).toBe(204);
-});
-
-it('updates response headers when setResponseHeaders is called with record', async () => {
-  const run = createRequestScope(STORE, () => ({}));
-  const result = await run(async () => {
-    setResponseHeaders({ 'X-Test': 'true' });
-    return new Response(null, { status: 204 });
-  });
-  expect(result).toBeInstanceOf(Response);
-  expect(result.status).toBe(204);
-  expect(result.headers.get('X-Test')).toBe('true');
-});
-
-it('updates response headers when setResponseHeaders is called with headers', async () => {
-  const run = createRequestScope(STORE, () => ({}));
-  const result = await run(async () => {
-    setResponseHeaders(new Headers({ 'X-Test': 'true' }));
-    return new Response(null, { status: 204 });
-  });
-  expect(result).toBeInstanceOf(Response);
-  expect(result.status).toBe(204);
-  expect(result.headers.get('X-Test')).toBe('true');
-});
-
-it('can mutate response headers when setResponseHeaders is called with function', async () => {
-  const run = createRequestScope(STORE, () => ({}));
-  const result = await run(async () => {
-    setResponseHeaders((headers) => {
-      headers.append('X-Test', '2');
+describe('try-catch', () => {
+  it('uses StatusError to construct error response if one is thrown', async () => {
+    const run = createRequestScope(STORE, () => ({}));
+    const result = await run(async () => {
+      throw new StatusError(418, 'I might be a teapot');
     });
-    return new Response(null, {
-      status: 204,
-      headers: {
-        'X-Test': '1',
-      },
-    });
+    expect(result).toBeInstanceOf(Response);
+    expect(result.status).toBe(418);
+    expect(await result.text()).toBe('I might be a teapot');
   });
-  expect(result).toBeInstanceOf(Response);
-  expect(result.status).toBe(204);
-  expect(result.headers.get('X-Test')).toBe('1, 2');
+
+  it('uses StatusError to construct a JSON response if one is thrown', async () => {
+    const run = createRequestScope(STORE, () => ({}));
+    const result = await run(async () => {
+      throw new StatusError(418, { test: 'I might be a teapot' });
+    });
+    expect(result).toBeInstanceOf(Response);
+    expect(result.status).toBe(418);
+    expect(await result.json()).toEqual({ test: 'I might be a teapot' });
+  });
+
+  it('uses thrown Response as response if thrown', async () => {
+    const run = createRequestScope(STORE, () => ({}));
+    const result = await run(async () => {
+      throw new Response(null, { status: 204 });
+    });
+    expect(result).toBeInstanceOf(Response);
+    expect(result.status).toBe(204);
+  });
+});
+
+describe('setResponseHeaders', () => {
+  it('updates response headers when setResponseHeaders is called with record', async () => {
+    const run = createRequestScope(STORE, () => ({}));
+    const result = await run(async () => {
+      setResponseHeaders({ 'X-Test': 'true' });
+      return new Response(null, { status: 204 });
+    });
+    expect(result).toBeInstanceOf(Response);
+    expect(result.status).toBe(204);
+    expect(result.headers.get('X-Test')).toBe('true');
+  });
+
+  it('updates response headers when setResponseHeaders is called with headers', async () => {
+    const run = createRequestScope(STORE, () => ({}));
+    const result = await run(async () => {
+      setResponseHeaders(new Headers({ 'X-Test': 'true' }));
+      return new Response(null, { status: 204 });
+    });
+    expect(result).toBeInstanceOf(Response);
+    expect(result.status).toBe(204);
+    expect(result.headers.get('X-Test')).toBe('true');
+  });
+
+  it('can mutate response headers when setResponseHeaders is called with function', async () => {
+    const run = createRequestScope(STORE, () => ({}));
+    const result = await run(async () => {
+      setResponseHeaders((headers) => {
+        headers.append('X-Test', '2');
+      });
+      return new Response(null, {
+        status: 204,
+        headers: {
+          'X-Test': '1',
+        },
+      });
+    });
+    expect(result).toBeInstanceOf(Response);
+    expect(result.status).toBe(204);
+    expect(result.headers.get('X-Test')).toBe('1, 2');
+  });
 });
