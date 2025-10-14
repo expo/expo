ba77eac26ce197f4e30dda88dddf2a9eb0979c2d
Gabriel Donadel <donadeldev@gmail.com>
2025-10-14 19:12:14 -0300

    [docs] Add brownfield instructions for configuring lifecycle listeners

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -223,6 +223,7 @@ export const general = [
     makeSection('Existing native apps', [
       makePage('brownfield/overview.mdx'),
       makePage('brownfield/get-started.mdx'),
+      makePage('brownfield/lifecycle-listeners.mdx'),
     ]),
     makeGroup(
       'Reference',
