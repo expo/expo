743db0fe0fa51a5e187dec6875e29c99afaa4852
Alan Hughes <30924086+alanjhughes@users.noreply.github.com>
2025-10-15 23:01:43 +0100

    [updates] Bundle Diffing 2. bspatch gradle and podspec  (#40091)
    # Why
    Initial integration with the native platforms. This was further refined in upstack PRs so this can mostly be disregarded. It was just enough to get it compiling.

---
+++ packages/expo-updates/ios/EXUpdates/BSPatch.swift
@@ -0,0 +1,26 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+import Foundation
+
+struct BSPatch {
+  static func applyPatch(oldPath: String,
+                  newPath: String,
+                  patchPath: String) throws {
+    
+    guard let oldPath = oldPath.cString(using: .utf8),
+      let newPath = newPath.cString(using: .utf8),
+      let patchPath = patchPath.cString(using: .utf8) else {
+      throw BSPatchError.failed(message: "Invalid file path provided.")
+    }
+
+    let result = EXUpdatesApplyBSDiffPatch(
+      oldPath,
+      newPath,
+      patchPath
+    )
+
+    if result != 0 {
+      throw BSPatchError.failed(message: "BSPatch failed with code \(result)")
+    }
+  }
+}
---
+++ packages/expo-updates/ios/EXUpdates/EXUpdatesBSPatch.h
@@ -0,0 +1,10 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+int EXUpdatesApplyBSDiffPatch(const char * _Nonnull oldFilePath,
+                              const char * _Nonnull newFilePath,
+                              const char * _Nonnull patchFilePath);
+                              
