ea1932dcde50d58c27dacf185ef0ad6d25e3b35c
HubertBer <bernackihubert03@gmail.com>
2025-10-16 20:09:43 +0200

    changes to apps

--- apps/native-component-list/src/navigation/ExpoApisStackNavigator.tsx
+++ apps/native-component-list/src/navigation/ExpoApisStackNavigator.tsx
@@ -151,6 +151,12 @@ export const ScreensList: ScreenConfig[] = [
   },
   {
     getComponent() {
+      return optionalRequire(() => require('../screens/LocalModules/LocalModulesScreen'));
+    },
+    name: 'LocalModules',
+  },
+  {
+    getComponent() {
       return optionalRequire(() => require('../screens/Blob/BlobScreen'));
     },
     name: 'Blob',
