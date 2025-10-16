ab5817b8e2d1913bb4802245aa5b7d8bdb8e4e4c
HubertBer <bernackihubert03@gmail.com>
2025-10-16 16:50:31 +0200

    merge artifacts fixes 1

--- packages/expo-modules-autolinking/src/localModules/androidLocalModules.ts
+++ packages/expo-modules-autolinking/src/localModules/androidLocalModules.ts
@@ -18,28 +18,39 @@ export async function createSymlinksToKotlinFiles(mirrorPath: string, watchedDir
   }
 }
 
+function getClassName(classNameWithPackage: string): string {
+  const index = classNameWithPackage.lastIndexOf('.');
+  if (index < 0 || index > classNameWithPackage.length) {
+    return classNameWithPackage;
+  }
+  return classNameWithPackage.substring(index + 1);
+}
+
 export async function generateLocalModulesListFile(
   localModulesListPath: string,
   watchedDirs: string[]
 ) {
   const localModulesObject = await getMirrorStateObject(watchedDirs);
-  const fileContent = `
-package local.modules;
+  const fileContent = `package local.modules;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import expo.modules.kotlin.ModulesProvider;
 import expo.modules.kotlin.modules.Module;
 
 public class ExpoLocalModulesList implements ModulesProvider {
+
   @Override
-  public List<Class<? extends Module>> getModulesList() {
-    return Arrays.<Class<? extends Module>>asList(
-${localModulesObject.kotlinClasses.map((moduleClass) => `      ${moduleClass}.class`).join(',\n')}
+  public Map<Class<? extends Module>, String> getModulesMap() {
+    return Map.of(
+${localModulesObject.kotlinClasses.map((moduleClass) => `      ${moduleClass}.class, "${getClassName(moduleClass)}"`).join(',\n')}
     );
   }
+
 }
+
 `;
 
   fs.mkdirSync(localModulesListPath, { recursive: true });
