94faf25691d7829e64420f3a54cea456bb8b386b
Jakub Tkacz <jtkacz@expo.io>
2025-10-17 09:51:55 +0200

    [expo-router] add flag to router config plugin

--- apps/router-e2e/app.config.js
+++ apps/router-e2e/app.config.js
@@ -84,6 +84,7 @@ module.exports = {
             : undefined,
         unstable_useServerDataLoaders: process.env.E2E_ROUTER_SERVER_LOADERS === 'true',
         unstable_useServerMiddleware: process.env.E2E_ROUTER_SERVER_MIDDLEWARE === 'true',
+        unstable_splitView: true
       },
     ],
   ],
