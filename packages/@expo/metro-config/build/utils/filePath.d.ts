12edc5ae94886f2bdaddb8d93c6ef30a2a6ae1bb
Cedric van Putten <me@bycedric.com>
2025-10-20 16:44:02 +0200

    chore: Bump `@expo/metro@54.1.0` for `metro@0.83.2` (#39826)
    # Why
    
    Maintenance release to move Expo over to
    [`metro@0.83.2`](https://github.com/facebook/metro/releases/tag/v0.83.2)
    
    # How
    
    Edit by @kitten:
    
    - Update import paths for types (several internals `/types.flow` paths
    changes to `/types`)
    - Update `hmrJSBundle` fork to support non-ASCII paths
    (`packages/@expo/metro-config/src/serializer/fork/hmrJSBundle.ts`)
    - `clientUrl` is now parsed as `URL` (`new URL`) rather than with
    `node:url` (deprecated)
    - Added `globalPrefix` parameter which is passed to
    `getRunModuleStatement` in serializer options
    - The `hot` parameter has been removed and is now assumed to be always
    `true` (where appropriate) which affects the serializer, babel preset,
    and transform worker (multiple tests had to be updated too)
    - Fix module path hints not using proper posix path normalisation (Metro
    URL encoding issues remain in some places)
    - Fix `findUpPackageJsonPath` in `@expo/metro-config` not using correct
    implementation and hence being able to fail on Windows
    - Update `bundlePath` in `async-require` fork not being turned into a
    full URL in two cases on web (which is now a requirement due to the
    switch to `URL`)
    - Fix `mode` being removed and needing to be remapped in
    `serverLogLikeMetro`
    
    # Test Plan
    
    See CI (unit tests and E2E are now passing)
    - Unit tests updated where necessary due to fixture changes (after
    rebased on `main`)
    - E2E tests for RSC/server actions manually checked (failure in server
    actions prior to fixes)
    - Manually checked DOM components E2E `router-e2e` test app
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    
    ---------
    
    Co-authored-by: Phil Pluckthun <phil@kitten.sh>

--- packages/@expo/metro-config/build/utils/filePath.d.ts
+++ packages/@expo/metro-config/build/utils/filePath.d.ts
@@ -1,4 +1,5 @@
-/**
- * Convert any platform-specific path to a POSIX path.
+/** Convert any platform-specific path to a POSIX path.
+ * @privateRemarks
+ * Metro's equivalent is `normalizePathSeparatorsToPosix`
  */
 export declare function toPosixPath(filePath: string): string;
--- packages/expo-module-scripts/types/expo-metro-augmentations.d.ts
+++ packages/expo-module-scripts/types/expo-metro-augmentations.d.ts
@@ -4,8 +4,8 @@
 /// <reference types="@expo/metro/metro-babel-transformer" />
 /// <reference types="@expo/metro/metro-source-map" />
 
-import * as __metroDeltaBundlerTypes from '@expo/metro/metro/DeltaBundler/types.flow';
-declare module '@expo/metro/metro/DeltaBundler/types.flow' {
+import * as __metroDeltaBundlerTypes from '@expo/metro/metro/DeltaBundler/types';
+declare module '@expo/metro/metro/DeltaBundler/types' {
   const enum _AsyncDependencyType {
     /** @privateRemarks Augmentation adds `asyncType: 'worker'` to possible values for worker chunk splitting */
     worker = 'worker'
@@ -46,4 +46,3 @@ declare module '@expo/metro/metro-source-map/source-map' {
   /** @privateRemarks Augmented to switch argument type to `BabelSourceMapSegment` */
   export function toSegmentTuple(mapping: BabelSourceMapSegment): MetroSourceMapSegmentTuple;
 }
-
