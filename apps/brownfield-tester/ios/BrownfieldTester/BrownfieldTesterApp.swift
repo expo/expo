797038a6da71e816157c9cfa506e89a61211f9c1
Gabriel Donadel <donadeldev@gmail.com>
2025-10-22 15:31:18 -0300

    Add a brownfield tester app for iOS

---
+++ apps/brownfield-tester/ios/BrownfieldTester/BrownfieldTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrownfieldTesterApp.swift
+//  BrownfieldTester
+//
+//  Created by Gabriel Donadel on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BrownfieldTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
