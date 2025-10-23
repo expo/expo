86e00a82d73b1c0d97af28eaea98e2570e262e7c
HubertBer <bernackihubert03@gmail.com>
2025-10-23 16:19:04 +0200

    Revert "renaming and fixes"
    This reverts commit 4168b279e0db58a5d4fe248b21ce9f56aff2497e.

--- apps/native-component-list/src/navigation/ExpoApisStackNavigator.tsx
+++ apps/native-component-list/src/navigation/ExpoApisStackNavigator.tsx
@@ -136,12 +136,6 @@ export const ScreensList: ScreenConfig[] = [
   },
   {
     getComponent() {
-      return optionalRequire(() => require('../screens/inlineModules/InlineModulesScreen'));
-    },
-    name: 'InlineModules',
-  },
-  {
-    getComponent() {
       return optionalRequire(() => require('../screens/Blob/BlobScreen'));
     },
     name: 'Blob',
