3003f9b41b60c52c26726a656af5340e1bcfe015
HubertBer <bernackihubert03@gmail.com>
2025-10-24 16:45:27 +0200

    prettier bare-expo inline modules api page

--- apps/native-component-list/src/screens/inlineModules/InlineModulesScreen.tsx
+++ apps/native-component-list/src/screens/inlineModules/InlineModulesScreen.tsx
@@ -1,13 +1,50 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 import SimpleModule from './inlineModulesExamples/SimpleModule.module';
 import TestView from './inlineModulesExamples/TestView.view';
+import HeadingText from '../../components/HeadingText';
+import MonoText from '../../components/MonoText';
+import { Page } from '../../components/Page';
 
 export default function InlineModulesScreen() {
+  const [inlineModuleConstant] = useState(
+    Platform.OS === 'android' ? 'Kotlin constant 7610' : 'Swift constant 1283'
+  );
+
   return (
-    <>
-      <Text>{SimpleModule.test}</Text>
-      <TestView style={{ flex: 1 }} url="https://docs.expo.dev/modules/" />
-    </>
+    <Page>
+      <View style={styles.moduleContainer}>
+        <HeadingText>Inline module</HeadingText>
+        <Text>
+          In the inline module there is a 'test' constant set to "{inlineModuleConstant}".
+        </Text>
+        <MonoText>
+          {`Constant("test") {
+  return "${inlineModuleConstant}"
+}`}
+        </MonoText>
+        <Text>Below you should see this constant accessed from the module </Text>
+        <MonoText>{SimpleModule.test}</MonoText>
+      </View>
+      <HeadingText> Inline view </HeadingText>
+      <Text>
+        Below you should see an inline view - expo web view showing Expo Modules API docs.
+      </Text>
+      <TestView style={styles.viewContainer} url="https://docs.expo.dev/modules/" />
+    </Page>
   );
 }
+
+const styles = StyleSheet.create({
+  moduleContainer: {
+    flex: 1,
+    minHeight: '30%',
+    overflow: 'hidden',
+  },
+  viewContainer: {
+    flex: 1,
+    minHeight: '70%',
+    overflow: 'hidden',
+  },
+});
