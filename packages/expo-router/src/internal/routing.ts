61969cf6808eba260a2885934548ec53e0f62724
Hassan Khan <hassan@expo.dev>
2025-10-24 03:56:19 +0100

    chore: fix lint issues

--- packages/expo-router/src/internal/routing.ts
+++ packages/expo-router/src/internal/routing.ts
@@ -7,6 +7,12 @@ export {
   type Options as GetRoutesCoreOptions,
   type RewriteConfig,
 } from '../getRoutesCore';
-export { getContextKey, isTypedRoute, matchGroupName, removeSupportedExtensions, stripGroupSegmentsFromPath } from '../matchers';
+export {
+  getContextKey,
+  isTypedRoute,
+  matchGroupName,
+  removeSupportedExtensions,
+  stripGroupSegmentsFromPath,
+} from '../matchers';
 export type { DynamicConvention, RouteNode } from '../Route';
 export { sortRoutes } from '../sortRoutes';
