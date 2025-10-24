f1792da4d6440052d74085e425b03ef95934ce32
HubertBer <bernackihubert03@gmail.com>
2025-10-24 07:29:04 +0200

    remove renaming artifacts

--- packages/@expo/cli/src/localModules/generation.types.ts
+++
@@ -1,9 +0,0 @@
-export type Event = {
-  filePath: string;
-  metadata?: {
-    type: 'f' | 'd' | 'l'; // Regular file / Directory / Symlink
-  } | null;
-  type: string;
-};
-
-export type EventsQueue = Event[];
