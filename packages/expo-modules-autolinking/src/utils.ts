b25cf0cbe465fc88fefe6d34f877c1bb2005f207
Phil Pluckthun <phil@kitten.sh>
2025-10-28 20:37:27 +0000

    [autolinking] Refactor file discovery, sort platform results, and drop `glob` (#40601)
    # Why
    
    This has various motivations:
    - In all of the cases where we use `glob`, we don't really need it. In
    many cases, it's doing very simple tasks
    - The `glob` upgrade doesn't sort results anymore, but we sometimes care
    about sorted results for fingerprint stability
    - While we could wait for the built-in `glob` to be available on all of
    our supported Node.js versions (LTS), because our usage of `glob` is
    quite simpl, we can replace it
    
    Additionally, replacing `glob` simplifies the tests (since we can
    consistently use filesystem mocks, which are easier to maintain), and we
    should get a nice performance uplift in large projects.
    
    # How
    
    Generally, the approach is to replace all uses of `glob` with either
    `fsPromises.readdir` (for sorted results) or `fsPromises.opendir` (for
    unsorted results; when results can be sorted later on). Sometimes we
    need recursive searches, but they're implemented quite trivially with a
    queue.
    
    This PR doesn't include refactors to parallelise reading files (we have
    a few regex matches on file contents), but this PR alone seems to speed
    up autolinking quite a bit. Here's a naive comparison:
    
    <img width="1182" height="418" alt="image"
    src="https://github.com/user-attachments/assets/27a1130a-8ff5-4c66-9cf4-97064f1100d1"
    />
    
    # Test Plan
    
    - Existing unit tests have been updated, and their logic still applies,
    so little changes were needed
    - **NOTE:** I may need some input and help on which outputs we
    specifically want to capture, that aren't captured by other test suites,
    for which our unit tests are insufficient
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo-modules-autolinking/src/utils.ts
+++ packages/expo-modules-autolinking/src/utils.ts
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+
 const MAX_SIZE = 5_000;
 
 export function memoize<const Fn extends (input: string, ...args: any[]) => Promise<any>>(fn: Fn) {
@@ -15,3 +18,81 @@ export function memoize<const Fn extends (input: string, ...args: any[]) => Prom
     }
   };
 }
+
+/** List filtered top-level files in `targetPath` (returns absolute paths) */
+export async function listFilesSorted(
+  targetPath: string,
+  filter: (basename: string) => boolean
+): Promise<string[]> {
+  try {
+    // `readdir` isn't guaranteed to be sorted on Windows
+    return (await fs.promises.readdir(targetPath, { withFileTypes: true }))
+      .filter((entry) => entry.isFile() && filter(entry.name))
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((entry) => path.join(targetPath, entry.name));
+  } catch {
+    return [];
+  }
+}
+
+/** List nested files in top-level directories in `targetPath` (returns relative paths) */
+export async function listFilesInDirectories(
+  targetPath: string,
+  filter: (basename: string) => boolean
+): Promise<string[]> {
+  return (
+    await Promise.all(
+      (await fs.promises.readdir(targetPath, { withFileTypes: true }))
+        .filter((entry) => entry.isDirectory() && entry.name !== 'node_modules')
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map(async (directory) => {
+          const entries = await fs.promises.readdir(path.join(targetPath, directory.name), {
+            withFileTypes: true,
+          });
+          return entries
+            .filter((entry) => entry.isFile() && filter(entry.name))
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((entry) => path.join(directory.name, entry.name));
+        })
+    )
+  ).flat(1);
+}
+
+/** Iterate folders recursively for files, optionally sorting results and filtering directories */
+export async function* scanFilesRecursively(
+  parentPath: string,
+  includeDirectory?: (parentPath: string, name: string) => boolean,
+  sort = !fs.opendir
+) {
+  const queue = [parentPath];
+  let targetPath: string | undefined;
+  while (queue.length > 0 && (targetPath = queue.shift()) != null) {
+    try {
+      const entries = sort
+        ? (await fs.promises.readdir(targetPath, { withFileTypes: true })).sort((a, b) =>
+            a.name.localeCompare(b.name)
+          )
+        : await fs.promises.opendir(targetPath);
+      for await (const entry of entries) {
+        if (entry.isDirectory() && entry.name !== 'node_modules') {
+          if (!includeDirectory || includeDirectory(targetPath, entry.name)) {
+            queue.push(path.join(targetPath, entry.name));
+          }
+        } else if (entry.isFile()) {
+          yield {
+            path: path.join(targetPath, entry.name),
+            parentPath: targetPath,
+            name: entry.name,
+          } as const;
+        }
+      }
+    } catch {
+      continue;
+    }
+  }
+}
+
+export const fileExistsAsync = async (file: string): Promise<string | null> => {
+  const stat = await fs.promises.stat(file).catch(() => null);
+  return stat?.isFile() ? file : null;
+};
