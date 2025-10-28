f0c34088096ab9b26bf7a5f9b57ae9861675b502
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2025-10-28 13:17:50 +0100

    [ncl][5/n] BlurView audit - Add new test screens (#40063)
    # Why
    
    Adds two new test screens to expo-blur library.
    
    # How
    
    - Kept the OG animated screeen
    - Added a more navbar example for performance testing without the
    overhead of animating 20 components at the same time.
    - Added a BlurView Compat example - tests if the blur behaves correctly
    when no BlurTarget was defined
    
    # Test Plan
    
    Tested in BareExpo on Android and iOS

--- apps/native-component-list/src/navigation/ExpoComponentsStackNavigator.tsx
+++ apps/native-component-list/src/navigation/ExpoComponentsStackNavigator.tsx
@@ -8,6 +8,7 @@ import { optionalRequire } from './routeBuilder';
 import { TabBackground } from '../components/TabBackground';
 import TabIcon from '../components/TabIcon';
 import { Layout } from '../constants';
+import { BlurScreens } from '../screens/BlurView/BlurViewScreen';
 import { CameraScreens } from '../screens/Camera/CameraScreen';
 import { componentScreensToListElements } from '../screens/ComponentListScreen';
 import ExpoComponents from '../screens/ExpoComponentsScreen';
@@ -52,6 +53,7 @@ const ScreensList: ScreenConfig[] = [
       return optionalRequire(() => require('../screens/BlurView/BlurViewScreen'));
     },
     name: 'BlurView',
+    route: 'blur',
   },
   {
     getComponent() {
@@ -303,6 +305,7 @@ const ScreensList: ScreenConfig[] = [
 export const Screens: ScreenConfig[] = [
   ...ScreensList,
 
+  ...BlurScreens,
   ...GLScreens,
   ...CameraScreens,
   ...ImageScreens,
