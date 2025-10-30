52fa37ed70122d658cc89770c3a2c62369800e5f
HubertBer <115428831+HubertBer@users.noreply.github.com>
2025-10-30 17:50:55 +0100

    Reformat SimpleModule.kt
    Co-authored-by: Łukasz Kosmaty <kosmatylukasz@gmail.com>

--- apps/native-component-list/src/screens/inlineModules/inlineModulesExamples/SimpleModule.kt
+++ apps/native-component-list/src/screens/inlineModules/inlineModulesExamples/SimpleModule.kt
@@ -4,9 +4,9 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 public class SimpleModule : Module() {
-    override fun definition() = ModuleDefinition {
-        Constant("test") { ->
-            "Kotlin constant 7610"
-        }
+  override fun definition() = ModuleDefinition {
+    Constant("test") { ->
+      "Kotlin constant 7610"
     }
+  }
 }
