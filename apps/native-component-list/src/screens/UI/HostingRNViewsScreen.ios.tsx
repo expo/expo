1ae1ac211e31a6352f7e8d753fed801469fe33e6
nishan (o^▽^o) <nishanbende@gmail.com>
2025-11-03 19:42:48 +0530

    hosting rn components in swiftui

--- apps/native-component-list/src/screens/UI/HostingRNViewsScreen.ios.tsx
+++ apps/native-component-list/src/screens/UI/HostingRNViewsScreen.ios.tsx
@@ -1,48 +1,102 @@
-import { Host, Text, VStack, HStack } from '@expo/ui/swift-ui';
-import { frame } from '@expo/ui/swift-ui/modifiers';
-import { ScrollView, StyleSheet, Text as RNText, View } from 'react-native';
+import { Host, Text as SwiftUIText, VStack, HStack, List, Section, Label } from '@expo/ui/swift-ui';
+import { frame, padding } from '@expo/ui/swift-ui/modifiers';
+import { useState } from 'react';
+import { StyleSheet, Text as RNText, View, Pressable } from 'react-native';
 
 export default function HostingRNViewsScreen() {
-  return (
-    <ScrollView>
-      <Host style={{ height: 300 }}>
-        <VStack spacing={20} modifiers={[frame({ height: 300 })]}>
-          <HStack spacing={20}>
-            <Text>H0V0</Text>
-            <Text>H1V0</Text>
-          </HStack>
-          <HStack>
-            <HStack spacing={20}>
-              <Text>H0V1</Text>
-              <Text>H1V1</Text>
-            </HStack>
-          </HStack>
+  const [counter, setCounter] = useState(0);
+  const [boxSize, setBoxSize] = useState(200);
 
-          {/* NOTE: To host UIView inside SwiftUI, we may need fixed size */}
-          <HStack modifiers={[frame({ width: 300, height: 100 })]}>
-            <View style={[styles.uiView, { width: 300, height: 100 }]}>
-              <RNText style={styles.uiViewText}>Text in UIView</RNText>
-            </View>
-          </HStack>
-        </VStack>
-      </Host>
-    </ScrollView>
+  return (
+    <Host style={{ flex: 1 }}>
+      <List>
+        <Section title="Mixing RN Components with SwiftUI">
+          <VStack spacing={12} modifiers={[padding({ all: 12 })]}>
+            <HStack spacing={24}>
+              <Pressable
+                onPress={() => setCounter((prev) => prev - 1)}
+                style={{
+                  height: 50,
+                  width: 50,
+                  borderRadius: 100,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: '#9B59B6',
+                }}>
+                <RNText
+                  style={{
+                    color: 'white',
+                    fontSize: 24,
+                  }}>
+                  -
+                </RNText>
+              </Pressable>
+              <SwiftUIText>{counter}</SwiftUIText>
+              <Pressable
+                onPress={() => setCounter((prev) => prev + 1)}
+                style={{
+                  height: 50,
+                  width: 50,
+                  borderRadius: 100,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: '#9B59B6',
+                }}>
+                <RNText style={{ color: 'white', fontSize: 24 }}>+</RNText>
+              </Pressable>
+            </HStack>
+          </VStack>
+        </Section>
+        <Section title="Dynamically increasing size">
+          <VStack spacing={12} modifiers={[padding({ all: 12 })]}>
+            <HStack spacing={24}>
+              <Pressable
+                onPress={() => setBoxSize((prev) => prev + 10)}
+                style={{
+                  height: boxSize,
+                  width: boxSize,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: '#9B59B6',
+                  gap: 10,
+                }}
+                onLongPress={() => setBoxSize(200)}>
+                <RNText style={{ color: 'white' }}>Tap to increase size</RNText>
+                <View style={{ height: 1, width: '100%', backgroundColor: 'white' }} />
+                <RNText style={{ color: 'white' }}>Long press to reset size</RNText>
+              </Pressable>
+            </HStack>
+          </VStack>
+        </Section>
+        <Section title="RN components without explicit size">
+          <VStack>
+            <HStack spacing={20} modifiers={[padding({ all: 12 })]}>
+              <View
+                style={{
+                  padding: 20,
+                  backgroundColor: '#9B59B6',
+                  borderRadius: 10,
+                  alignSelf: 'flex-start',
+                }}
+              />
+              <View
+                style={{
+                  padding: 20,
+                  backgroundColor: '#9B59B6',
+                  borderRadius: 10,
+                  alignSelf: 'flex-start',
+                }}
+              />
+            </HStack>
+            <RNText style={{ textAlign: 'center' }}>
+              RN component boxes separated by SwiftUI HStack
+            </RNText>
+          </VStack>
+        </Section>
+      </List>
+    </Host>
   );
 }
-
-const styles = StyleSheet.create({
-  uiView: {
-    backgroundColor: '#90EE90',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 16,
-  },
-  uiViewText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
 
 HostingRNViewsScreen.navigationOptions = {
   title: 'Hosting RN Views',
