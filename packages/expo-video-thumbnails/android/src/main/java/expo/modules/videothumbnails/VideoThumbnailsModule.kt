expo/expo

commit 79fd5fa6a2159e32dd363634d07264633ac27a01

[video thumbnails][Android] Release the `MediaMetadataRetriever` safely (#29015)
# Why

Releases the `MediaMetadataRetriever` safely.

# How

The documentation mentioned that `MediaMetadataRetriever` can throw when
realeaseing the underlying asset. That can lead to a crash. We can use
`use` function from Kotlin to close it safely.

---------

Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

packages/expo-video-thumbnails/android/src/main/java/expo/modules/videothumbnails/VideoThumbnailsModule.kt
@@ -71,35 +71,36 @@ class VideoThumbnailsModule : Module() {
     }
   }
 
-  private class GetThumbnail(private val sourceFilename: String, private val videoOptions: VideoThumbnailOptions, private val context: Context) {
-    fun execute(): Bitmap? {
-      val retriever = MediaMetadataRetriever()
-
-      try {
-        if (URLUtil.isFileUrl(sourceFilename)) {
-          retriever.setDataSource(Uri.decode(sourceFilename).replace("file://", ""))
-        } else if (URLUtil.isContentUrl(sourceFilename)) {
-          val fileUri = Uri.parse(sourceFilename)
-          context.contentResolver.openFileDescriptor(fileUri, "r")?.use { parcelFileDescriptor ->
-            FileInputStream(parcelFileDescriptor.fileDescriptor).use { inputStream ->
-              retriever.setDataSource(inputStream.fd)
+  private class GetThumbnail(
+    private val sourceFilename: String,
+    private val videoOptions: VideoThumbnailOptions,
+    private val context: Context
+  ) {
+    fun execute(): Bitmap? = MediaMetadataRetriever()
+      .use { retriever ->
+        try {
+          if (URLUtil.isFileUrl(sourceFilename)) {
+            retriever.setDataSource(Uri.decode(sourceFilename).replace("file://", ""))
+          } else if (URLUtil.isContentUrl(sourceFilename)) {
+            val fileUri = Uri.parse(sourceFilename)
+            context.contentResolver.openFileDescriptor(fileUri, "r")?.use { parcelFileDescriptor ->
+              FileInputStream(parcelFileDescriptor.fileDescriptor).use { inputStream ->
+                retriever.setDataSource(inputStream.fd)
+              }
             }
+          } else {
+            retriever.setDataSource(sourceFilename, videoOptions.headers)
           }
-        } else {
-          retriever.setDataSource(sourceFilename, videoOptions.headers)
-        }
 
-        return retriever.getFrameAtTime(
-          videoOptions.time.toLong() * 1000,
-          MediaMetadataRetriever.OPTION_CLOSEST_SYNC
-        )
-      } catch (e: Exception) {
-        Log.e(ERROR_TAG, "Unable to retrieve source file")
-        return null
-      } finally {
-        retriever.release()
+          return retriever.getFrameAtTime(
+            videoOptions.time.toLong() * 1000,
+            MediaMetadataRetriever.OPTION_CLOSEST_SYNC
+          )
+        } catch (e: Exception) {
+          Log.e(ERROR_TAG, "Unable to retrieve source file")
+          return null
+        }
       }
-    }
   }
 
   private fun isAllowedToRead(url: String): Boolean {

commit 7a1376252b68dad31f1fe933f9f76b70fba9a1d7

[video-thumbnails][android] - fix Android 9 regression (#40816)
# Why

<!--
Please describe the motivation for this PR, and link to relevant GitHub
issues, forums posts, or feature requests.
-->
Regression caused by #29015 - it uses `MediaMetadataRetriever().use`
syntax, not supported on Android 9 or lower causing thumbnail generation
to no longer work.

Fixes #34780

# How

<!--
How did you build this feature or fix this bug and why?
-->
Replace `MediaMetadataRetriever().use` with `try/catch`

# Test Plan

<!--
Please describe how you tested this change and how a reviewer could
reproduce your test, especially if this PR does not include automated
tests! If possible, please also provide terminal output and/or
screenshots demonstrating your test/reproduction.
-->

Tried Android 9 and Android 16 and it works well.

# Checklist

<!--
Please check the appropriate items below if they apply to your diff.
-->

- [x] I added a `changelog.md` entry and rebuilt the package sources
according to [this short
guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [x] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).
- [x] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

packages/expo-video-thumbnails/android/src/main/java/expo/modules/videothumbnails/VideoThumbnailsModule.kt
@@ -76,8 +76,9 @@ class VideoThumbnailsModule : Module() {
     private val videoOptions: VideoThumbnailOptions,
     private val context: Context
   ) {
-    fun execute(): Bitmap? = MediaMetadataRetriever()
-      .use { retriever ->
+    fun execute(): Bitmap? {
+      val retriever = MediaMetadataRetriever()
+      try {
         try {
           if (URLUtil.isFileUrl(sourceFilename)) {
             retriever.setDataSource(Uri.decode(sourceFilename).replace("file://", ""))
@@ -100,7 +101,15 @@ class VideoThumbnailsModule : Module() {
           Log.e(ERROR_TAG, "Unable to retrieve source file")
           return null
         }
+      } finally {
+        try {
+          retriever.release()
+        } catch (e: Exception) {
+          Log.e(ERROR_TAG, "Error releasing MediaMetadataRetriever for source file")
+          return null
+        }
       }
+    }
   }
 
   private fun isAllowedToRead(url: String): Boolean {