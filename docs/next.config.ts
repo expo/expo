edd2034d13e3e0a1e296059a1950787e95e41af6
Aman Mittal <amandeepmittal@live.com>
2025-11-05 17:38:14 +0530

    Update next.config.ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -39,7 +39,7 @@ const removeConsoleConfig =
     : false;
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: join(__dirname, '..'),
+  outputFileTracingRoot: join(__dirname),
   transpilePackages: [
     '@expo/*',
     '@radix-ui/react-dropdown-menu',
