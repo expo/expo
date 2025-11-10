3a3dcae31a1fd92f0447afe758e5e8451f50666c
nishan (o^▽^o) <nishanbende@gmail.com>
2025-11-10 23:07:35 +0530

    host props

--- packages/expo-ui/build/swift-ui/RNHost.d.ts
+++ packages/expo-ui/build/swift-ui/RNHost.d.ts
@@ -1,2 +1,7 @@
-export declare function RNHost(props: any): import("react").JSX.Element;
+interface RNHostProps {
+    matchContents?: boolean;
+    children: React.ReactElement;
+}
+export declare function RNHost(props: RNHostProps): import("react").JSX.Element;
+export {};
 //# sourceMappingURL=RNHost.d.ts.map
--- packages/expo-ui/src/swift-ui/RNHost.tsx
+++ packages/expo-ui/src/swift-ui/RNHost.tsx
@@ -1,6 +1,13 @@
 import { requireNativeView } from 'expo';
 const HostNativeView: React.ComponentType<any> = requireNativeView('ExpoUI', 'RNHost');
 
-export function RNHost(props: any) {
-  return <HostNativeView {...props} />;
+interface RNHostProps {
+  matchContents?: boolean;
+  children: React.ReactElement;
+}
+
+export function RNHost(props: RNHostProps) {
+  return (
+    <HostNativeView {...props} key={props.matchContents ? 'matchContents' : 'noMatchContents'} />
+  );
 }
