21181e18176ce04af2d9390a2af991873d1cabd6
nishan (o^▽^o) <nishanbende@gmail.com>
2025-11-13 18:02:06 +0530

    fix tvos builds

--- packages/expo-ui/ios/Modifiers/PickerStyleModifier.swift
+++ packages/expo-ui/ios/Modifiers/PickerStyleModifier.swift
@@ -18,23 +18,35 @@ internal enum PickerStyleType: String, Enumerable {
     case .inline:
       content.pickerStyle(.inline)
     case .menu:
-      content.pickerStyle(.menu)
+      if #available(iOS 14.0, tvOS 17.0, *) {
+        content.pickerStyle(.menu)
+      } else {
+        content.pickerStyle(.automatic)
+      }
     case .navigationLink:
-      if #available(iOS 16.0, *) {
+      if #available(iOS 16.0, tvOS 16.0, *) {
         content.pickerStyle(.navigationLink)
       } else {
         content.pickerStyle(.automatic)
       }
     case .palette:
+      #if !os(tvOS)
       if #available(iOS 17.0, *) {
         content.pickerStyle(.palette)
       } else {
         content.pickerStyle(.automatic)
       }
+      #else
+      content.pickerStyle(.automatic)
+      #endif
     case .segmented:
       content.pickerStyle(.segmented)
     case .wheel:
+      #if !os(tvOS)
       content.pickerStyle(.wheel)
+      #else
+      content.pickerStyle(.automatic)
+      #endif
     default:
       content.pickerStyle(.automatic)
     }
