c80e39840c414e025198fa9cd7940a3961ce9448
Tomasz Sapeta <tsapeta@expo.dev>
2025-11-25 15:13:51 +0100

    [tools] Ignore errors caused by git diff being too large

--- tools/src/commands/CodeReviewCommand.ts
+++ tools/src/commands/CodeReviewCommand.ts
@@ -17,9 +17,26 @@ async function action(options: ActionOptions) {
   try {
     await reviewPullRequestAsync(+options.pr);
   } catch (error) {
+    if (isDiffTooLargeError(error)) {
+      // If the diff is too large, we can't do much. It's better not to fail the workflow though.
+      logger.info(error.message);
+      return;
+    }
     logger.error(error, error.stack);
     throw error;
   }
+}
+
+/**
+ * Checks if the error is a GitHub API error caused by the diff being too large.
+ * Currently it's limited to 20000 lines.
+ */
+function isDiffTooLargeError(error: any) {
+  const apiErrors = error.response?.data?.errors ?? [];
+
+  return apiErrors.some((apiError) => {
+    return apiError.field === 'diff' && apiError.code === 'too_large';
+  });
 }
 
 export default (program: Command) => {
