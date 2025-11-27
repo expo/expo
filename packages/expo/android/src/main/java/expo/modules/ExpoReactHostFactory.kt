expo/expo

commit c8754f6258fd67a0dcf55180b721c224d035c1a9

[expo][android] Delete ReactNativeHostWrapper (#40222)
# Why

From react-native 0.82, users no longer declare a ReactNativeHost in
their MainApplication, so we need to start migrating our methods to use
ReactHost instead. This is part 5 of a series of PR to allow us to
remove ReactNativeHostWrapper from our template.

# How

- Update Android templates to use
`ExpoReactHostFactory.getDefaultReactHost()`
- Delete `ReactNativeHostWrapper` class
- Update Expo Go to no longer use `ReactNativeHostWrapper`

# Test Plan

- Expo Go
- BareExpo

# Checklist

<!--
Please check the appropriate items below if they apply to your diff.
-->

- [ ] I added a `changelog.md` entry and rebuilt the package sources
according to [this short
guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).
- [ ] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

packages/expo/android/src/main/java/expo/modules/ExpoReactHostFactory.kt
@@ -5,7 +5,6 @@ package expo.modules
 import android.content.Context
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactInstanceEventListener
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactPackageTurboModuleManagerDelegate
 import com.facebook.react.bridge.JSBundleLoader
@@ -40,6 +39,24 @@ object ExpoReactHostFactory {
     private val hostHandlers: List<ReactNativeHostHandler>
   ) : ReactHostDelegate {
 
+    val hostDelegateJsBundleFilePath: String? by lazy {
+      hostHandlers.asSequence()
+        .mapNotNull { it.getJSBundleFile(useDevSupport) }
+        .firstOrNull() ?: jsBundleFilePath
+    }
+
+    val hostDelegateJSBundleAssetPath: String? by lazy {
+      hostHandlers.asSequence()
+        .mapNotNull { it.getBundleAssetName(useDevSupport) }
+        .firstOrNull() ?: jsBundleAssetPath
+    }
+
+    val hostDelegateUseDeveloperSupport: Boolean by lazy {
+      hostHandlers.asSequence()
+        .mapNotNull { it.useDeveloperSupport }
+        .firstOrNull() ?: useDevSupport
+    }
+
     // Keeps this `_jsBundleLoader` backing property for DevLauncher to replace its internal value
     private var _jsBundleLoader: JSBundleLoader? = null
     override val jsBundleLoader: JSBundleLoader
@@ -49,14 +66,14 @@ object ExpoReactHostFactory {
           return backingJSBundleLoader
         }
         val context = weakContext.get() ?: throw IllegalStateException("Unable to get concrete Context")
-        jsBundleFilePath?.let { jsBundleFile ->
+        hostDelegateJsBundleFilePath?.let { jsBundleFile ->
           if (jsBundleFile.startsWith("assets://")) {
             return JSBundleLoader.createAssetLoader(context, jsBundleFile, true)
           }
           return JSBundleLoader.createFileLoader(jsBundleFile)
         }
 
-        return JSBundleLoader.createAssetLoader(context, "assets://$jsBundleAssetPath", true)
+        return JSBundleLoader.createAssetLoader(context, "assets://$hostDelegateJSBundleAssetPath", true)
       }
 
     override val jsRuntimeFactory: JSRuntimeFactory
@@ -70,63 +87,9 @@ object ExpoReactHostFactory {
         throw error
       }
       hostHandlers.forEach { handler ->
-        handler.onReactInstanceException(useDevSupport, error)
+        handler.onReactInstanceException(hostDelegateUseDeveloperSupport, error)
       }
     }
-  }
-
-  @OptIn(UnstableReactNativeAPI::class)
-  @JvmStatic
-  fun createFromReactNativeHost(
-    context: Context,
-    reactNativeHost: ReactNativeHost
-  ): ReactHost {
-    require(reactNativeHost is ReactNativeHostWrapper) {
-      "You can call createFromReactNativeHost only with instances of ReactNativeHostWrapper"
-    }
-    if (reactHost == null) {
-      val useDeveloperSupport = reactNativeHost.useDeveloperSupport
-      val componentFactory = ComponentFactory()
-      DefaultComponentsRegistry.register(componentFactory)
-
-      reactNativeHost.reactNativeHostHandlers.forEach { handler ->
-        handler.onWillCreateReactInstance(useDeveloperSupport)
-      }
-
-      val reactHostDelegate = ExpoReactHostDelegate(
-        WeakReference(context),
-        reactNativeHost.packages,
-        reactNativeHost.jsMainModuleName,
-        reactNativeHost.bundleAssetName,
-        reactNativeHost.jsBundleFile,
-        reactNativeHost.useDeveloperSupport,
-        hostHandlers = reactNativeHost.reactNativeHostHandlers
-      )
-
-      val reactHostImpl =
-        ReactHostImpl(
-          context,
-          reactHostDelegate,
-          componentFactory,
-          true,
-          useDeveloperSupport
-        )
-
-      reactNativeHost.reactNativeHostHandlers.forEach { handler ->
-        handler.onDidCreateDevSupportManager(reactHostImpl.devSupportManager)
-      }
-
-      reactHostImpl.addReactInstanceEventListener(object : ReactInstanceEventListener {
-        override fun onReactContextInitialized(context: ReactContext) {
-          reactNativeHost.reactNativeHostHandlers.forEach { handler ->
-            handler.onDidCreateReactInstance(useDeveloperSupport, context)
-          }
-        }
-      })
-
-      reactHost = reactHostImpl
-    }
-    return reactHost as ReactHost
   }
 
   @OptIn(UnstableReactNativeAPI::class)

apps/bare-expo/android/app/src/main/java/dev/expo/payments/MainApplication.kt
@@ -8,35 +8,24 @@ import android.os.Bundle
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
 
 import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
+import expo.modules.ExpoReactHostFactory
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-    this,
-    object : DefaultReactNativeHost(this) {
-      override fun getPackages(): List<ReactPackage> {
-        // Packages that cannot be autolinked yet can be added manually here, for example:
-        // add(MyReactNativePackage())
-        return expo.modules.benchmark.withBenchmarkingPackages(PackageList(this).packages)
-      }
-
-      override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-
-      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-    }
-  )
-
-  override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
+  override val reactHost: ReactHost by lazy {
+    ExpoReactHostFactory.getDefaultReactHost(
+      context = applicationContext,
+      packageList =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
+        }
+    )
+  }
 
   override fun onCreate() {
     super.onCreate()

apps/expo-go/android/expoview/src/main/java/host/exp/exponent/experience/ExpoGoReactNativeHost.kt
@@ -5,9 +5,9 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.shell.MainReactPackage
 import com.swmansion.worklets.WorkletsPackage
+import com.swmansion.reanimated.ReanimatedPackage
 import host.exp.exponent.ExponentManifest
 import host.exp.expoview.Exponent
-import versioned.host.exp.exponent.ExpoReanimatedPackage
 import versioned.host.exp.exponent.ExpoTurboPackage
 import versioned.host.exp.exponent.ExponentPackage
 
@@ -27,11 +27,11 @@ class ExpoGoReactNativeHost(
     return devSupportEnabled
   }
 
-  override fun getJSMainModuleName(): String {
+  public override fun getJSMainModuleName(): String {
     return mainModuleName ?: super.getJSMainModuleName()
   }
 
-  override fun getJSBundleFile(): String? {
+  public override fun getJSBundleFile(): String? {
     return instanceManagerBuilderProperties.jsBundlePath
   }
 
@@ -39,11 +39,11 @@ class ExpoGoReactNativeHost(
 
   override val isNewArchEnabled = true
 
-  override fun getPackages(): MutableList<ReactPackage> {
+  public override fun getPackages(): MutableList<ReactPackage> {
     return mutableListOf(
       MainReactPackage(null),
       WorkletsPackage(),
-      ExpoReanimatedPackage(),
+      ReanimatedPackage(),
       ExponentPackage(
         instanceManagerBuilderProperties.experienceProperties,
         instanceManagerBuilderProperties.manifest,
@@ -82,11 +82,11 @@ class KernelReactNativeHost(
 
   override val isNewArchEnabled = true
 
-  override fun getJSBundleFile(): String? {
+  public override fun getJSBundleFile(): String? {
     return data?.localBundlePath
   }
 
-  override fun getJSMainModuleName(): String {
+  public override fun getJSMainModuleName(): String {
     return if (devSupportEnabled) {
       exponentManifest.getKernelManifestAndAssetRequestHeaders().manifest.getMainModuleName()
     } else {
@@ -94,11 +94,11 @@ class KernelReactNativeHost(
     }
   }
 
-  override fun getPackages(): MutableList<ReactPackage> {
+  public override fun getPackages(): MutableList<ReactPackage> {
     return mutableListOf(
       MainReactPackage(null),
       WorkletsPackage(),
-      ExpoReanimatedPackage(),
+      ReanimatedPackage(),
       ExponentPackage.kernelExponentPackage(
         application.applicationContext,
         exponentManifest.getKernelManifestAndAssetRequestHeaders().manifest,

apps/expo-go/android/expoview/src/main/java/host/exp/exponent/experience/HomeActivity.kt
@@ -113,7 +113,6 @@ open class HomeActivity : BaseExperienceActivity() {
 
   fun onEventMainThread(event: KernelStartedRunningEvent?) {
     reactHost = kernel.reactHost
-    reactNativeHost = kernel.reactNativeHost
     reactSurface = kernel.surface
 
     reactHost?.onHostResume(this, this)

apps/expo-go/android/expoview/src/main/java/host/exp/exponent/experience/ReactNativeActivity.kt
@@ -20,7 +20,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.contains
 import com.facebook.infer.annotation.Assertions
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.bridge.ReactContext.RCTDeviceEventEmitter
 import com.facebook.react.devsupport.DefaultDevLoadingViewImplementation
 import com.facebook.react.devsupport.DevInternalSettings
@@ -32,7 +31,6 @@ import com.facebook.react.modules.core.PermissionAwareActivity
 import com.facebook.react.modules.core.PermissionListener
 import com.facebook.react.runtime.ReactSurfaceImpl
 import de.greenrobot.event.EventBus
-import expo.modules.ReactNativeHostWrapper
 import expo.modules.core.interfaces.Package
 import expo.modules.manifests.core.Manifest
 import host.exp.exponent.ExponentManifest
@@ -83,7 +81,6 @@ abstract class ReactNativeActivity :
   // Will be called after waitForDrawOverOtherAppPermission
   protected open fun startReactInstance() {}
 
-  var reactNativeHost: ReactNativeHost? = null
   var reactHost: ReactHost? = null
   var reactSurface: ReactSurface? = null
 
@@ -382,7 +379,6 @@ abstract class ReactNativeActivity :
     )
 
     val devBundleDownloadListener = ExponentDevBundleDownloadListener(progressListener)
-    val hostWrapper = ReactNativeHostWrapper(application, nativeHost)
 
     if (delegate.isDebugModeEnabled) {
       val debuggerHost = manifest!!.getDebuggerHost()
@@ -392,8 +388,14 @@ abstract class ReactNativeActivity :
       waitForReactAndFinishLoading()
     }
 
-    val reactHost = ReactHostFactory.createFromReactNativeHost(this, hostWrapper, devBundleDownloadListener)
-    reactNativeHost = nativeHost
+    val reactHost = ReactHostFactory.getDefaultReactHost(
+      context = application,
+      packageList = nativeHost.packages,
+      jsMainModulePath = nativeHost.jsMainModuleName,
+      jsBundleFilePath = nativeHost.jsBundleFile,
+      useDevSupport = nativeHost.useDeveloperSupport,
+      devBundleDownloadListener = devBundleDownloadListener
+    )
 
     val bundle = Bundle()
     val exponentProps = JSONObject()
@@ -442,7 +444,7 @@ abstract class ReactNativeActivity :
       return reactHost
     }
 
-    val devSettings = reactHost.devSupportManager.devSettings as? DevInternalSettings
+    val devSettings = reactHost.devSupportManager?.devSettings as? DevInternalSettings
     devSettings?.setExponentActivityId(activityId)
 
     val appKey = manifest!!.getAppKey()
@@ -459,7 +461,7 @@ abstract class ReactNativeActivity :
     val buildProps = (manifest!!.getPluginProperties("expo-build-properties")?.get("android") as? Map<*, *>)
       ?.mapKeys { it.key.toString() }
     val enableNetworkInspector = buildProps?.get("networkInspector") as? Boolean ?: true
-    if (enableNetworkInspector && reactHost.devSupportManager.devSupportEnabled) {
+    if (enableNetworkInspector && reactHost.devSupportManager?.devSupportEnabled == true) {
       networkInterceptor = ExpoNetworkInterceptor(Uri.parse(manifest!!.getBundleURL()))
     }
     return reactHost

apps/expo-go/android/expoview/src/main/java/host/exp/exponent/factories/ReactHostFactory.kt
@@ -2,12 +2,12 @@ package host.exp.exponent.factories
 
 import android.content.Context
 import com.facebook.react.ReactInstanceEventListener
-import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactPackageTurboModuleManagerDelegate
 import com.facebook.react.bridge.JSBundleLoader
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.common.annotations.UnstableReactNativeAPI
+import com.facebook.react.common.build.ReactBuildConfig
 import com.facebook.react.defaults.DefaultComponentsRegistry
 import com.facebook.react.defaults.DefaultTurboModuleManagerDelegate
 import com.facebook.react.devsupport.interfaces.DevBundleDownloadListener
@@ -18,7 +18,8 @@ import com.facebook.react.runtime.ReactHostDelegate
 import com.facebook.react.runtime.ReactHostImpl
 import com.facebook.react.runtime.hermes.HermesInstance
 import com.facebook.react.runtime.internal.bolts.Task
-import expo.modules.ReactNativeHostWrapper
+import expo.modules.ExpoModulesPackage
+import expo.modules.core.interfaces.ReactNativeHostHandler
 import java.lang.ref.WeakReference
 import java.util.concurrent.Executors
 
@@ -27,10 +28,15 @@ object ReactHostFactory {
   @UnstableReactNativeAPI
   private class ExpoReactHostDelegate(
     private val weakContext: WeakReference<Context>,
-    private val reactNativeHostWrapper: ReactNativeHostWrapper,
+    private val packageList: List<ReactPackage>,
+    override val jsMainModulePath: String,
+    private val jsBundleAssetPath: String?,
+    private val jsBundleFilePath: String? = null,
+    private val useDevSupport: Boolean,
     override val bindingsInstaller: BindingsInstaller? = null,
     override val turboModuleManagerDelegateBuilder: ReactPackageTurboModuleManagerDelegate.Builder =
-      DefaultTurboModuleManagerDelegate.Builder()
+      DefaultTurboModuleManagerDelegate.Builder(),
+    private val hostHandlers: List<ReactNativeHostHandler>
   ) : ReactHostDelegate {
 
     // Keeps this `_jsBundleLoader` backing property for DevLauncher to replace its internal value
@@ -38,50 +44,60 @@ object ReactHostFactory {
       get() {
         val context =
           weakContext.get() ?: throw IllegalStateException("Unable to get concrete Context")
-        reactNativeHostWrapper.jsBundleFile?.let { jsBundleFile ->
+        jsBundleFilePath?.let { jsBundleFile ->
           if (jsBundleFile.startsWith("assets://")) {
             return JSBundleLoader.createAssetLoader(context, jsBundleFile, true)
           }
           return JSBundleLoader.createFileLoader(jsBundleFile)
         }
-        val jsBundleAssetPath = reactNativeHostWrapper.bundleAssetName
+
         return JSBundleLoader.createAssetLoader(context, "assets://$jsBundleAssetPath", true)
       }
-
-    override val jsMainModulePath: String
-      get() = reactNativeHostWrapper.jsMainModuleName
 
     override val jsRuntimeFactory: JSRuntimeFactory
       get() = HermesInstance()
 
     override val reactPackages: List<ReactPackage>
-      get() = reactNativeHostWrapper.packages
+      get() = packageList
 
     override fun handleInstanceException(error: Exception) {
-      val useDeveloperSupport = reactNativeHostWrapper.useDeveloperSupport
-      reactNativeHostWrapper.reactNativeHostHandlers.forEach { handler ->
-        handler.onReactInstanceException(useDeveloperSupport, error)
+      hostHandlers.forEach { handler ->
+        handler.onReactInstanceException(useDevSupport, error)
       }
     }
   }
 
   @OptIn(UnstableReactNativeAPI::class)
   @JvmStatic
-  fun createFromReactNativeHost(
+  fun getDefaultReactHost(
     context: Context,
-    reactNativeHost: ReactNativeHost,
+    packageList: List<ReactPackage>,
+    jsMainModulePath: String = ".expo/.virtual-metro-entry",
+    jsBundleAssetPath: String = "index.android.bundle",
+    jsBundleFilePath: String? = null,
+    jsRuntimeFactory: JSRuntimeFactory? = null,
+    useDevSupport: Boolean = ReactBuildConfig.DEBUG,
+    bindingsInstaller: BindingsInstaller? = null,
     devBundleDownloadListener: DevBundleDownloadListener? = null
   ): ReactHostImpl {
-    require(reactNativeHost is ReactNativeHostWrapper) {
-      "You can call createFromReactNativeHost only with instances of ReactNativeHostWrapper"
-    }
-    val useDeveloperSupport = reactNativeHost.useDeveloperSupport
-    val reactHostDelegate = ExpoReactHostDelegate(WeakReference(context), reactNativeHost)
+    val hostHandlers = ExpoModulesPackage.packageList
+      .flatMap { it.createReactNativeHostHandlers(context) }
+
+    val reactHostDelegate = ExpoReactHostDelegate(
+      WeakReference(context),
+      packageList,
+      jsMainModulePath,
+      jsBundleAssetPath,
+      jsBundleFilePath,
+      useDevSupport,
+      bindingsInstaller,
+      hostHandlers = hostHandlers
+    )
     val componentFactory = ComponentFactory()
     DefaultComponentsRegistry.register(componentFactory)
 
-    reactNativeHost.reactNativeHostHandlers.forEach { handler ->
-      handler.onWillCreateReactInstance(useDeveloperSupport)
+    hostHandlers.forEach { handler ->
+      handler.onWillCreateReactInstance(useDevSupport)
     }
 
     val reactHostImpl =
@@ -92,18 +108,18 @@ object ReactHostFactory {
         Executors.newSingleThreadExecutor(),
         Task.UI_THREAD_EXECUTOR,
         true,
-        useDeveloperSupport,
+        useDevSupport,
         ExpoGoDevSupportFactory(devBundleDownloadListener)
       )
 
-    reactNativeHost.reactNativeHostHandlers.forEach { handler ->
+    hostHandlers.forEach { handler ->
       handler.onDidCreateDevSupportManager(reactHostImpl.devSupportManager)
     }
 
     reactHostImpl.addReactInstanceEventListener(object : ReactInstanceEventListener {
       override fun onReactContextInitialized(context: ReactContext) {
-        reactNativeHost.reactNativeHostHandlers.forEach { handler ->
-          handler.onDidCreateReactInstance(useDeveloperSupport, context)
+        hostHandlers.forEach { handler ->
+          handler.onDidCreateReactInstance(useDevSupport, context)
         }
       }
     })

apps/expo-go/android/expoview/src/main/java/host/exp/exponent/headless/InternalHeadlessAppLoader.kt
@@ -10,7 +10,6 @@ import com.facebook.react.bridge.UiThreadUtil
 import com.facebook.react.devsupport.DevInternalSettings
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import expo.modules.ReactNativeHostWrapper
 import expo.modules.apploader.AppLoaderProvider
 import expo.modules.core.interfaces.Package
 import expo.modules.core.interfaces.SingletonModule
@@ -246,11 +245,16 @@ class InternalHeadlessAppLoader(private val context: Context) :
       Exponent.enableDeveloperSupport(debuggerHost, mainModuleName, host)
     }
 
-    val wrapper = ReactNativeHostWrapper(context, host)
-    val reactHost = ReactHostFactory.createFromReactNativeHost(context, wrapper)
+    val reactHost = ReactHostFactory.getDefaultReactHost(
+      context = context,
+      packageList = host.packages,
+      jsMainModulePath = host.jsMainModuleName,
+      jsBundleFilePath = host.jsBundleFile,
+      useDevSupport = host.useDeveloperSupport
+    )
 
     val devSupportManager = reactHost.devSupportManager
-    val devSettings = devSupportManager.devSettings as? DevInternalSettings
+    val devSettings = devSupportManager?.devSettings as? DevInternalSettings
     devSettings?.setExponentActivityId(activityId)
 
     // keep a reference in app record, so it can be invalidated through AppRecord.invalidate()

apps/expo-go/android/expoview/src/main/java/host/exp/exponent/kernel/Kernel.kt
@@ -27,7 +27,6 @@ import com.facebook.react.runtime.ReactSurfaceImpl
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import de.greenrobot.event.EventBus
-import expo.modules.ReactNativeHostWrapper
 import expo.modules.jsonutils.require
 import expo.modules.manifests.core.ExpoUpdatesManifest
 import expo.modules.manifests.core.Manifest
@@ -245,8 +244,13 @@ class Kernel : KernelInterface() {
             )
           }
 
-          val hostWrapper = ReactNativeHostWrapper(applicationContext, nativeHost)
-          reactHost = ReactHostFactory.createFromReactNativeHost(applicationContext, hostWrapper)
+          reactHost = ReactHostFactory.getDefaultReactHost(
+            context = applicationContext,
+            packageList = nativeHost.packages,
+            jsMainModulePath = nativeHost.jsMainModuleName,
+            jsBundleFilePath = nativeHost.jsBundleFile,
+            useDevSupport = nativeHost.useDeveloperSupport
+          )
 
           reactNativeHost = nativeHost
           reactHost?.onHostResume(activityContext, null)
@@ -867,10 +871,8 @@ class Kernel : KernelInterface() {
     if (activity is ExperienceActivity) {
       // Invalidate the experience that is not longer needed.
       activity.reactHost?.invalidate()
-      activity.reactNativeHost?.clear()
 
       activity.reactHost = null
-      activity.reactNativeHost = null
     }
   }
 

commit 495400061b78aa370b97d0ec444d050da48536bf

[expo] fix broken reloading for expo-updates (#41260)
# Why

fix broken `Updates.reloadAsync` on android. a regression since #40222

# How

the bundle configurations cannot be lazy. the `Updates.reloadAsync()` may switch bundle url dynamically

# Test Plan

updates e2e ci passed

# Checklist

- [x] I added a `changelog.md` entry and rebuilt the package sources according to [this short guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [x] This diff will work correctly for `npx expo prebuild` & EAS Build (eg: updated a module plugin).
- [x] Conforms with the [Documentation Writing Style Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

packages/expo/android/src/main/java/expo/modules/ExpoReactHostFactory.kt
@@ -39,23 +39,23 @@ object ExpoReactHostFactory {
     private val hostHandlers: List<ReactNativeHostHandler>
   ) : ReactHostDelegate {
 
-    val hostDelegateJsBundleFilePath: String? by lazy {
-      hostHandlers.asSequence()
-        .mapNotNull { it.getJSBundleFile(useDevSupport) }
-        .firstOrNull() ?: jsBundleFilePath
-    }
+    val hostDelegateJsBundleFilePath: String?
+      get() =
+        hostHandlers.asSequence()
+          .mapNotNull { it.getJSBundleFile(useDevSupport) }
+          .firstOrNull() ?: jsBundleFilePath
 
-    val hostDelegateJSBundleAssetPath: String? by lazy {
-      hostHandlers.asSequence()
-        .mapNotNull { it.getBundleAssetName(useDevSupport) }
-        .firstOrNull() ?: jsBundleAssetPath
-    }
+    val hostDelegateJSBundleAssetPath: String?
+      get() =
+        hostHandlers.asSequence()
+          .mapNotNull { it.getBundleAssetName(useDevSupport) }
+          .firstOrNull() ?: jsBundleAssetPath
 
-    val hostDelegateUseDeveloperSupport: Boolean by lazy {
-      hostHandlers.asSequence()
-        .mapNotNull { it.useDeveloperSupport }
-        .firstOrNull() ?: useDevSupport
-    }
+    val hostDelegateUseDeveloperSupport: Boolean
+      get() =
+        hostHandlers.asSequence()
+          .mapNotNull { it.useDeveloperSupport }
+          .firstOrNull() ?: useDevSupport
 
     // Keeps this `_jsBundleLoader` backing property for DevLauncher to replace its internal value
     private var _jsBundleLoader: JSBundleLoader? = null