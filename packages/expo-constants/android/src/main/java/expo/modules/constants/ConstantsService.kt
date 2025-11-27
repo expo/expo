expo/expo

commit 94d82c04c02cc67cec3af15a1319743669478113

[constants][Android] Remove unused native dependencies (#39763)

packages/expo-constants/android/src/main/java/expo/modules/constants/ConstantsService.kt
@@ -1,18 +1,12 @@
 package expo.modules.constants
 
-import org.apache.commons.io.IOUtils
-
-import expo.modules.core.interfaces.InternalModule
-import expo.modules.interfaces.constants.ConstantsInterface
-
+import android.content.Context
 import android.os.Build
 import android.util.Log
-import android.content.Context
-
+import expo.modules.core.interfaces.InternalModule
+import expo.modules.interfaces.constants.ConstantsInterface
 import java.io.FileNotFoundException
-import java.lang.Exception
-import java.nio.charset.StandardCharsets
-import java.util.*
+import java.util.UUID
 
 private val TAG = ConstantsService::class.java.simpleName
 private const val CONFIG_FILE_NAME = "app.config"
@@ -73,11 +67,15 @@ open class ConstantsService(private val context: Context) : InternalModule, Cons
   private val appConfig: String?
     get() {
       try {
-        context.assets.open(CONFIG_FILE_NAME).use {
-            stream ->
-          return IOUtils.toString(stream, StandardCharsets.UTF_8)
-        }
-      } catch (e: FileNotFoundException) {
+        context
+          .assets
+          .open(CONFIG_FILE_NAME)
+          .use { input ->
+            input
+              .bufferedReader(charset = Charsets.UTF_8)
+              .use { it.readText() }
+          }
+      } catch (_: FileNotFoundException) {
         // do nothing, expected in managed apps
       } catch (e: Exception) {
         Log.e(TAG, "Error reading embedded app config", e)

packages/expo-constants/android/build.gradle
@@ -20,8 +20,3 @@ android {
     versionName "18.0.9"
   }
 }
-
-dependencies {
-  api "androidx.annotation:annotation:1.0.0"
-  implementation "commons-io:commons-io:2.6"
-}

commit 164a2c4ddf6ed7764eba175fb5755d0e4891cec8

[constants] fix empty expoConfig (#41259)
# Why 

fixed that `expoConfig` is null. a regression from #39763
it's also the root cause of breaking updates e2e: https://expo.dev/accounts/expo-ci/projects/expo-workflow-testing/workflows/019ac060-9da7-75f8-a1d5-6bbcba60624c

# How

return something

# Test Plan

updates e2e ci passed (in upstack pr)

# Checklist

- [x] I added a `changelog.md` entry and rebuilt the package sources according to [this short guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [x] This diff will work correctly for `npx expo prebuild` & EAS Build (eg: updated a module plugin).
- [x] Conforms with the [Documentation Writing Style Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

packages/expo-constants/android/src/main/java/expo/modules/constants/ConstantsService.kt
@@ -67,7 +67,7 @@ open class ConstantsService(private val context: Context) : InternalModule, Cons
   private val appConfig: String?
     get() {
       try {
-        context
+        return context
           .assets
           .open(CONFIG_FILE_NAME)
           .use { input ->