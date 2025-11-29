ccefe828e42e9e8720a3007e3507b6f226080b33
Phil Pluckthun <phil@kitten.sh>
2025-11-29 14:55:14 +0000

    Support copying template from `expo/internal/unstable-template`

--- packages/@expo/cli/src/prebuild/resolveLocalTemplate.ts
+++ packages/@expo/cli/src/prebuild/resolveLocalTemplate.ts
@@ -2,7 +2,7 @@ import type { ExpoConfig } from '@expo/config';
 import fs from 'fs';
 import resolveFrom from 'resolve-from';
 
-import { extractNpmTarballAsync } from '../utils/npm';
+import { copyNodeModuleAsync, extractNpmTarballAsync } from '../utils/npm';
 
 const debug = require('debug')('expo:prebuild:resolveLocalTemplate') as typeof console.log;
 
@@ -15,11 +15,22 @@ export async function resolveLocalTemplateAsync({
   projectRoot: string;
   exp: Pick<ExpoConfig, 'name'>;
 }): Promise<string> {
-  const templatePath = resolveFrom(projectRoot, 'expo/template.tgz');
-  debug('Using local template from Expo package:', templatePath);
-  const stream = fs.createReadStream(templatePath);
-  return await extractNpmTarballAsync(stream, {
-    cwd: templateDirectory,
-    name: exp.name,
-  });
+  try {
+    // In our monorepo, `expo/template.tgz` may not exist, or may be outdated, but we allow the template
+    // to be resolved by manually installing `expo-template-bare-minimum`
+    const templateRawPath = require(resolveFrom(projectRoot, 'expo/internal/unstable-template'));
+    return await copyNodeModuleAsync(templateRawPath, {
+      cwd: templateDirectory,
+      name: exp.name,
+    });
+  } catch (error) {
+    // The default is to use `expo/template.tgz` which exists in all published versions of it
+    const templatePath = resolveFrom(projectRoot, 'expo/template.tgz');
+    debug('Using local template from Expo package:', templatePath);
+    const stream = fs.createReadStream(templatePath);
+    return await extractNpmTarballAsync(stream, {
+      cwd: templateDirectory,
+      name: exp.name,
+    });
+  }
 }
