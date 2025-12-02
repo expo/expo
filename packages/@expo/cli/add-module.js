b2454005f1f4b1a0fabaad4c6b591a50ec2ed55b
Kornelijus Šliubauskas <kornelijus.sliubauskas@outlook.com>
2025-12-02 14:23:25 +0200

    Added required dependencies

--- packages/@expo/cli/add-module.js
+++ packages/@expo/cli/add-module.js
@@ -1,3 +1,6 @@
+const path = require('node:path');
+const { pathToFileURL } = require('node:url');
+
 // A wrapper that allows to import an ESM module from a CJS module.
 // This works because the `import` in this wrapper is not transpiled by SWC.
 module.exports = function (name) {
