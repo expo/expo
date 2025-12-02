c2e23afc23978cf841310d069229aed2f68250e5
Jakub Tkacz <32908614+Ubax@users.noreply.github.com>
2025-12-02 09:30:49 +0100

    [expo-router] apply react-navigation background color to NativeTabsView (#41068)
    # Why
    
    Opposed to Stack where we use react-navigation's theme background color,
    in NativeTabs we fallback to what react-native-screens exposes as a
    background color. This PR adds `contentStyle` prop and changes the
    default background to `colors.background`.
    
    Fixes: https://github.com/expo/expo/issues/39969 
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    1. Manual testing
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- apps/router-e2e/__e2e__/native-tabs/app/faces/index.tsx
+++ apps/router-e2e/__e2e__/native-tabs/app/faces/index.tsx
@@ -1,19 +1,14 @@
-import { useTheme } from '@react-navigation/native';
 import { ScrollView } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Faces } from '../../components/faces';
 
 export default function Index() {
-  const { colors } = useTheme();
-  const insets = useSafeAreaInsets();
   return (
     <ScrollView
-      style={{ flex: 1, backgroundColor: colors.background }}
+      style={{ flex: 1 }}
       contentContainerStyle={{
         alignItems: 'center',
         padding: 32,
-        paddingTop: insets.top,
       }}>
       <Faces numberOfFaces={60} />
     </ScrollView>
