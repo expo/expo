11fd6cff7ad9c83f1dccdd418db6f913e942ccf2
Phil Pluckthun <phil@kitten.sh>
2025-12-03 17:06:49 +0000

    Add preferred local template resolution from expo/expo monorepo template

--- packages/@expo/cli/src/prebuild/resolveLocalTemplate.ts
+++ packages/@expo/cli/src/prebuild/resolveLocalTemplate.ts
@@ -1,8 +1,9 @@
 import type { ExpoConfig } from '@expo/config';
 import fs from 'fs';
+import path from 'path';
 import resolveFrom from 'resolve-from';
 
-import { extractNpmTarballAsync } from '../utils/npm';
+import { copyNodeModuleAsync, extractNpmTarballAsync } from '../utils/npm';
 
 const debug = require('debug')('expo:prebuild:resolveLocalTemplate') as typeof console.log;
 
@@ -15,11 +16,24 @@ export async function resolveLocalTemplateAsync({
   projectRoot: string;
   exp: Pick<ExpoConfig, 'name'>;
 }): Promise<string> {
-  const templatePath = resolveFrom(projectRoot, 'expo/template.tgz');
-  debug('Using local template from Expo package:', templatePath);
-  const stream = fs.createReadStream(templatePath);
-  return await extractNpmTarballAsync(stream, {
-    cwd: templateDirectory,
-    name: exp.name,
-  });
+  try {
+    // In our monorepo, `expo/template.tgz` may not exist, or may be outdated, so we first check
+    // `expo-template-bare-minimum`, which is a dev-dependency in `@expo/cli` that's only fulfilled
+    // in our monorepo
+    const templateRawPath = path.dirname(require.resolve('expo-template-bare-minimum'));
+    debug('Using local template from expo-template-bare-minimum path:', templateRawPath);
+    return await copyNodeModuleAsync(templateRawPath, {
+      cwd: templateDirectory,
+      name: exp.name,
+    });
+  } catch (error) {
+    // The default is to use `expo/template.tgz` which exists in all published versions of it
+    const templatePath = resolveFrom(projectRoot, 'expo/template.tgz');
+    debug('Using local template from Expo package:', templatePath);
+    const stream = fs.createReadStream(templatePath);
+    return await extractNpmTarballAsync(stream, {
+      cwd: templateDirectory,
+      name: exp.name,
+    });
+  }
 }
