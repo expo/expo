d48af82176b850a7698050dcb727fd1c4d02c41d
lukmccall <kosmatylukasz@gmail.com>
2025-12-04 11:49:10 +0100

    [skip ci] Fix spotless

--- packages/expo-dev-menu/android/src/release/java/expo/modules/devmenu/modules/DevMenuModule.kt
+++ packages/expo-dev-menu/android/src/release/java/expo/modules/devmenu/modules/DevMenuModule.kt
@@ -3,7 +3,7 @@ package expo.modules.devmenu.modules
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
-class DevMenuModule: Module() {
+class DevMenuModule : Module() {
   override fun definition() = ModuleDefinition {
     Name("DevMenuModule")
   }
