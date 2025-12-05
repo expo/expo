9085a9484d087f615fa7d5cb75d672d22c8f0c2e
HubertBer <bernackihubert03@gmail.com>
2025-12-05 10:58:55 +0100

    limit exported functions

--- packages/expo-type-information/build/index.d.ts
+++ packages/expo-type-information/build/index.d.ts
@@ -1,4 +1,3 @@
 export * from './typeInformation';
-export * from './typescriptGeneration';
-export * from './mockgen';
+export { generateMocks, getAllExpoModulesInWorkingDirectory } from './mockgen';
 //# sourceMappingURL=index.d.ts.map
