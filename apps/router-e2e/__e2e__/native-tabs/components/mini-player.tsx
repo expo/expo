41779007014ffd4d06e60b4689392696adbf17fa
evanbacon <bacon@expo.io>
2025-12-08 21:19:09 -0800

    Update mini-player.tsx

--- apps/router-e2e/__e2e__/native-tabs/components/mini-player.tsx
+++ apps/router-e2e/__e2e__/native-tabs/components/mini-player.tsx
@@ -2,7 +2,7 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { NativeTabs } from 'expo-router/unstable-native-tabs';
 import { Image, ColorValue, DynamicColorIOS, Platform, Pressable, Text, View } from 'react-native';
 import { ShimmerText } from './shimmer';
-import { Color } from 'expo-router';
+import { Color, Link } from 'expo-router';
 
 interface MiniPlayerProps {
   isPlaying: boolean;
@@ -16,63 +16,66 @@ export function MiniPlayer({ isPlaying, setIsPlaying }: MiniPlayerProps) {
     default: 'white',
   });
   return (
-    <View style={{ flex: 1 }}>
-      Unwrapped Text
-      <View
-        style={{
-          // width,
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          paddingVertical: 8,
-          paddingHorizontal: 16,
-          gap: 12,
-        }}>
-        <View style={{ flex: 1, flexDirection: 'row', gap: 8 }}>
-          <Image
-            source={require('../../../assets/echo.png')}
+    <Link href="/faces" asChild>
+      <Pressable style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <View
             style={{
-              width: 30,
-              // backgroundColor: 'blue',
-              height: '100%',
-              aspectRatio: 1,
-              borderRadius: 8,
-            }}
-          />
-          <View style={{ gap: 2 }}>
-            <ShimmerText
-              style={{
-                fontSize: 14,
-                color: textColor,
-                width: placement === 'regular' ? 300 : 175,
-              }}
-              ellipsizeMode="clip"
-              lineBreakMode="middle"
-              numberOfLines={1}
-              highlightColor="#9DA3AE"
-              speed={0.7}>
-              {placement === 'regular'
-                ? 'Designing Liquid Glass accessory...'
-                : 'Designing accessory...'}
-            </ShimmerText>
-            <Text
-              style={{
-                fontSize: 10,
-                color: Color.ios.secondaryLabel,
-              }}>
-              Claude Opus 4.5
-            </Text>
+              // width,
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              paddingVertical: 8,
+              paddingHorizontal: 16,
+              gap: 12,
+            }}>
+            <View style={{ flex: 1, flexDirection: 'row', gap: 8 }}>
+              <Image
+                source={require('../../../assets/echo.png')}
+                style={{
+                  width: 30,
+                  // backgroundColor: 'blue',
+                  height: '100%',
+                  aspectRatio: 1,
+                  borderRadius: 8,
+                }}
+              />
+              <View style={{ gap: 2 }}>
+                <ShimmerText
+                  style={{
+                    fontSize: 14,
+                    color: textColor,
+                    width: placement === 'regular' ? 300 : 175,
+                  }}
+                  ellipsizeMode="clip"
+                  lineBreakMode="middle"
+                  numberOfLines={1}
+                  highlightColor="#9DA3AE"
+                  speed={0.7}>
+                  {placement === 'regular'
+                    ? 'Designing Liquid Glass accessory...'
+                    : 'Designing accessory...'}
+                </ShimmerText>
+                <Text
+                  style={{
+                    fontSize: 10,
+                    color: Color.ios.secondaryLabel,
+                  }}>
+                  Claude Opus 4.5
+                </Text>
+              </View>
+            </View>
+            <Pressable onPress={() => setIsPlaying(!isPlaying)}>
+              <Ionicons name={'stop'} size={18} color={Color.ios.label} />
+            </Pressable>
+            {placement === 'regular' && (
+              <Pressable onPress={() => console.log('forward pressed')}>
+                <Ionicons name="ellipsis-horizontal" size={20} color={textColor} />
+              </Pressable>
+            )}
           </View>
         </View>
-        <Pressable onPress={() => setIsPlaying(!isPlaying)}>
-          <Ionicons name={'stop'} size={18} color={Color.ios.label} />
-        </Pressable>
-        {placement === 'regular' && (
-          <Pressable onPress={() => console.log('forward pressed')}>
-            <Ionicons name="ellipsis-horizontal" size={20} color={textColor} />
-          </Pressable>
-        )}
-      </View>
-    </View>
+      </Pressable>
+    </Link>
   );
 }
