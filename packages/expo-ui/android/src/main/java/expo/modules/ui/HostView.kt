51d5b5249288fc01d3b3d2520ebef68de35c435e
Kudo Chien <kudo@expo.dev>
2025-12-10 18:00:36 +0800

    [core][ui] add matchContents support to jetpack-compose

--- packages/expo-ui/android/src/main/java/expo/modules/ui/HostView.kt
+++ packages/expo-ui/android/src/main/java/expo/modules/ui/HostView.kt
@@ -3,7 +3,12 @@ package expo.modules.ui
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -13,10 +18,18 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.view.size
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.IntSize
 import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.records.Field
+import expo.modules.kotlin.records.Record
 import expo.modules.kotlin.types.Enumerable
+import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.views.ComposableScope
 import expo.modules.kotlin.views.ComposeProps
 import expo.modules.kotlin.views.ExpoComposeView
@@ -46,29 +59,101 @@ internal enum class ExpoColorScheme(val value: String) : Enumerable {
 }
 
 internal data class HostProps(
-  val colorScheme: MutableState<ExpoColorScheme?> = mutableStateOf(null)
+  val colorScheme: MutableState<ExpoColorScheme?> = mutableStateOf(null),
+  val matchContentsHorizontal: MutableState<Boolean?> = mutableStateOf(null),
+  val matchContentsVertical: MutableState<Boolean?> = mutableStateOf(null)
 ) : ComposeProps
 
 @SuppressLint("ViewConstructor")
 internal class HostView(context: Context, appContext: AppContext) :
   ExpoComposeView<HostProps>(context, appContext, withHostingView = true) {
   override val props = HostProps()
+  private val onLayoutContent by EventDispatcher<LayoutContentEvent>()
 
   @Composable
   override fun ComposableScope.Content() {
     val context = LocalContext.current
+    val density = LocalDensity.current
     val colorScheme = props.colorScheme.value?.toColorScheme(context)
       ?: ExpoColorScheme.defaultColorScheme(context, isSystemInDarkTheme())
 
     MaterialTheme(colorScheme = colorScheme) {
-      for (index in 0..<this@HostView.size) {
-        val child = getChildAt(index) as? ExpoComposeView<*> ?: continue
-        with(this) {
-          with(child) {
-            Content()
-          }
-        }
+      Box(
+        modifier = Modifier
+          .then(if (props.matchContentsHorizontal.value == true) Modifier.wrapContentWidth() else Modifier)
+          .then(if (props.matchContentsVertical.value == true) Modifier.wrapContentHeight() else Modifier)
+          .onSizeChanged { size -> dispatchOnLayoutContent(size, density) }
+      ) {
+        Children(this@Content)
       }
     }
   }
+
+  private fun dispatchOnLayoutContent(size: IntSize, density: Density) {
+    val matchContentsHorizontal = this.props.matchContentsHorizontal.value
+    val matchContentsVertical = this.props.matchContentsVertical.value
+
+    with(density) {
+      val width = size.width.toDp().value
+      val height = size.height.toDp().value
+
+      if (matchContentsHorizontal == true || matchContentsVertical == true) {
+        val styleWidth = if (matchContentsHorizontal == true && width > 0) width else null
+        val styleHeight = if (matchContentsVertical == true && height > 0) height else null
+        shadowNodeProxy.setStyleSize(styleWidth?.toDouble(), styleHeight?.toDouble())
+      }
+
+      onLayoutContent(LayoutContentEvent(width.toDouble(), height.toDouble()))
+    }
+  }
+
+  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+    val matchContentsHorizontal = props.matchContentsHorizontal.value
+    val matchContentsVertical = props.matchContentsVertical.value
+
+    // Measure with UNSPECIFIED to get intrinsic size for matchContents
+    if (matchContentsHorizontal == true || matchContentsVertical == true) {
+      val widthSpec = if (matchContentsHorizontal == true) {
+        MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+      } else {
+        widthMeasureSpec
+      }
+      val heightSpec = if (matchContentsVertical == true) {
+        MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+      } else {
+        heightMeasureSpec
+      }
+      super.onMeasure(widthSpec, heightSpec)
+    } else {
+      super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+  }
+
+  internal fun onViewDidUpdateProps() {
+    val matchContentsHorizontal = props.matchContentsHorizontal.value
+    val matchContentsVertical = props.matchContentsVertical.value
+    val composeView = findComposeView()
+    composeView.layoutParams = LayoutParams(
+      if (matchContentsHorizontal == true) LayoutParams.WRAP_CONTENT else LayoutParams.MATCH_PARENT,
+      if (matchContentsVertical == true) LayoutParams.WRAP_CONTENT else LayoutParams.MATCH_PARENT
+    )
+  }
+
+  private fun findComposeView(): ComposeView {
+    for (i in childCount - 1 downTo 0) {
+      val child = getChildAt(i) as? ComposeView
+      if (child != null) {
+        return child
+      }
+    }
+    throw IllegalStateException("No ComposeView found in HostView")
+  }
 }
+
+internal data class LayoutContentEvent(
+  @Field
+  val width: Double?,
+
+  @Field
+  val height: Double?
+) : Record
