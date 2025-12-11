13851d4428edcea0bd9a7bc0db786f0bb62bf749
Wojciech Dróżdż <behenate@gmail.com>
2025-12-11 11:42:04 +0100

    Fix "functional interface can't have just one abstract method" build error

--- packages/expo-video/android/src/main/java/expo/modules/video/listeners/VideoViewListener.kt
+++ packages/expo-video/android/src/main/java/expo/modules/video/listeners/VideoViewListener.kt
@@ -3,7 +3,7 @@ package expo.modules.video.listeners
 import expo.modules.video.VideoView
 import expo.modules.video.records.PiPParams
 
-fun interface VideoViewListener {
+interface VideoViewListener {
   /**
    * Called when the ability of the view to autoEnterPip has changed.
    * Note: This can be called when `autoEnterPiP` doesn't change, but other factors
