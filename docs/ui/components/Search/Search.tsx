1390e208f9997e70e57875deb0cfdb4165eaf909
Aman Mittal <amandeepmittal@live.com>
2025-12-11 15:21:30 +0530

    [docs] Fix custom EAS dashboard section ordering in search ui (#41550)

--- docs/ui/components/Search/Search.tsx
+++ docs/ui/components/Search/Search.tsx
@@ -35,7 +35,7 @@ export const Search = () => {
             heading: 'EAS dashboard',
             items: expoDashboardItems,
             getItemsAsync: getExpoItemsAsync,
-            sectionIndex: 2,
+            sectionIndex: Number.MAX_SAFE_INTEGER,
           },
         ]}
       />
