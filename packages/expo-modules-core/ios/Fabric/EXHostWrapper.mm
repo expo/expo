31cd35162760343dbd6c326f7a48b7c5dd201dcf
Alan Hughes <alan@expo.io>
2025-12-15 14:34:20 +0000

    make requested changes

--- packages/expo-modules-core/ios/Fabric/EXHostWrapper.mm
+++ packages/expo-modules-core/ios/Fabric/EXHostWrapper.mm
@@ -2,37 +2,27 @@
 
 #import <ExpoModulesCore/EXHostWrapper.h>
 
-#ifdef RCT_NEW_ARCH_ENABLED
 #import <ReactCommon/RCTHost.h>
 #import <React/RCTSurfacePresenter.h>
 #import <React/RCTMountingManager.h>
 #import <React/RCTComponentViewRegistry.h>
-#endif
 
 @implementation EXHostWrapper {
-#ifdef RCT_NEW_ARCH_ENABLED
   __weak RCTHost *_host;
-#endif
 }
 
 - (instancetype)initWithHost:(RCTHost *)host
 {
   if (self = [super init]) {
-#ifdef RCT_NEW_ARCH_ENABLED
     _host = host;
-#endif
   }
   return self;
 }
 
 - (nullable UIView *)findViewWithTag:(NSInteger)tag
 {
-#ifdef RCT_NEW_ARCH_ENABLED
   RCTComponentViewRegistry *componentViewRegistry = _host.surfacePresenter.mountingManager.componentViewRegistry;
   return [componentViewRegistry findComponentViewWithTag:tag];
-#else
-  return nil;
-#endif
 }
 
 @end
