6ce40e226ecc476acda4698b2406f3acdad084b0
Gabriel Donadel <donadeldev@gmail.com>
2025-12-16 17:17:45 -0300

    [expo] Remove Expo Go new architecture warning

--- packages/expo/src/Expo.fx.tsx
+++ packages/expo/src/Expo.fx.tsx
@@ -26,29 +26,6 @@ if (isRunningInExpoGo()) {
   ErrorUtils.setGlobalHandler(createErrorHandler(globalHandler));
 }
 
-// Warn if the New Architecture is not explicitly enabled in the app config and we are running in Expo Go.
-// This could be problematic because you will be developing your app with the New Architecture enabled and
-// but your builds will have the New Architecture disabled.
-if (__DEV__ && isRunningInExpoGo() && process.env.NODE_ENV === 'development') {
-  (['android', 'ios'] as const).forEach((platform) => {
-    const newArchPlatformConfig = Constants.expoConfig?.[platform]?.newArchEnabled;
-    const newArchRootConfig = Constants.expoConfig?.newArchEnabled;
-
-    const isNewArchExplicitlyDisabled =
-      newArchPlatformConfig === false ||
-      (newArchPlatformConfig === undefined && newArchRootConfig === false);
-
-    if (Platform.OS === platform && isNewArchExplicitlyDisabled) {
-      // Wrap it in rAF to show the warning after the React Native DevTools message
-      requestAnimationFrame(() => {
-        console.warn(
-          `🚨 React Native's New Architecture is always enabled in Expo Go, but it is explicitly disabled in your project's app config. This may lead to unexpected behavior when creating a production or development build. Remove "newArchEnabled": false from your app.json.\nLearn more: https://docs.expo.dev/guides/new-architecture/`
-        );
-      });
-    }
-  });
-}
-
 // Disable the "Open debugger to view warnings" React Native DevTools warning in
 // Expo Go and expo-dev-client, because launching the debugger from there will not
 // get the correct JS target.
