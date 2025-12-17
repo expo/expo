9df88eb6ee9dcbe3d7fbde640e8967f05ac81222
Phil Pluckthun <phil@kitten.sh>
2025-12-17 11:46:10 +0000

    cli/e2e: Sanitize RSC payloads

--- packages/@expo/cli/e2e/playwright/dev/02-server-actions.test.ts
+++ packages/@expo/cli/e2e/playwright/dev/02-server-actions.test.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 import { clearEnv, restoreEnv } from '../../__tests__/export/export-side-effects';
 import { getRouterE2ERoot } from '../../__tests__/utils';
 import { createExpoStart } from '../../utils/expo';
+import { sanitizeRSCPayloadString } from '../../utils/rsc';
 import { pageCollectErrors } from '../page';
 
 test.beforeAll(() => clearEnv());
@@ -101,7 +102,7 @@ test.describe(inputDir, () => {
 
     const rscPayload = new TextDecoder().decode(await response.body());
 
-    expect(rscPayload)
+    expect(sanitizeRSCPayloadString(rscPayload))
       .toBe(`1:I["node_modules/react-native-web/dist/exports/Text/index.js",["/node_modules/react-native-web/dist/exports/Text/index.js.bundle?platform=web&dev=true&hot=false&transform.asyncRoutes=true&transform.routerRoot=__e2e__%2F02-server-actions%2Fapp&modulesOnly=true&runModule=false&resolver.clientboundary=true&xRSC=1"],"",1]
 0:{"_value":[["$","$L1",null,{"style":{"color":"darkcyan"},"testID":"server-action-props","children":"c=0"},null],["$","$L1",null,{"testID":"server-action-platform","children":"web"},null]]}
 `);
