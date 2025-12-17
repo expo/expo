a887b497db560af6b64e6a1950cac97d129e983b
lukmccall <kosmatylukasz@gmail.com>
2025-12-17 14:40:38 +0100

    [core][Android] Fix spotless

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/ViewFunctionHolder.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/views/ViewFunctionHolder.kt
@@ -1,7 +1,7 @@
 package expo.modules.kotlin.views
 
-/* 
- * A marker interface identifying views reusing a single class (like ComposeFunctionHolder) 
+/*
+ * A marker interface identifying views reusing a single class (like ComposeFunctionHolder)
  * that should be identified by name for things like event validation.
  */
 interface ViewFunctionHolder {
