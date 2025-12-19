b7aa5422cd4c730dee0b8b0a962be6c86309f29f
Jakub Tkacz <jtkacz@expo.io>
2025-12-19 16:29:28 +0100

    [expo-router] remove new arch checks

--- packages/expo-router/ios/LinkPreview/LinkPreviewNativePreviewView.swift
+++ packages/expo-router/ios/LinkPreview/LinkPreviewNativePreviewView.swift
@@ -4,8 +4,6 @@ class NativeLinkPreviewContentView: RouterViewWithLogger {
   var preferredContentSize: CGSize = .zero
 
   func setInitialSize(bounds: CGRect) {
-#if RCT_NEW_ARCH_ENABLED
     self.setShadowNodeSize(Float(bounds.width), height: Float(bounds.height))
-#endif
   }
 }
