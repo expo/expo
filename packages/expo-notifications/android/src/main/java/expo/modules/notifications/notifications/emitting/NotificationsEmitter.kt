d15c837ab0cb5ab5353b573407e73e19e6a42980
Vojtech Novak <vonovak@gmail.com>
2025-12-19 22:17:36 +0000

    [notifications] migrate off of legacy core apis on Android (#41731)
    # Why
    
    - this removes the legacy classes that were created in `NotificationsPackage.java`
    
    ```
      @Override
      public List<SingletonModule> createSingletonModules(Context context) {
        return Arrays.asList(
          mNotificationManager
        );
      }
    
      @Override
      public List<InternalModule> createInternalModules(Context context) {
        return Arrays.asList(
          new AndroidXNotificationsChannelsProvider(context),
          new ExpoNotificationsCategoriesSerializer()
        );
      }
    ```
    
    # How
    
    - `mNotificationManager` was converted into a kotlin `object` - it's just a plain kotlin class with some internal state (not dependent on react or context)
    - `AndroidXNotificationsChannelsProvider` - turned that into an expo module
    - `ExpoNotificationsCategoriesSerializer` - it doesn't need to be an expo module, so I kept it as it was - a regular Java class
    - some other code was converted to kotlin or slightly refactored
    
    # Test Plan
    
    - tested manually on device with the `notification-tester` app and NCL test-suite
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources according to [this short guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/emitting/NotificationsEmitter.kt
+++ packages/expo-notifications/android/src/main/java/expo/modules/notifications/notifications/emitting/NotificationsEmitter.kt
@@ -15,7 +15,6 @@ private const val NEW_RESPONSE_EVENT_NAME = "onDidReceiveNotificationResponse"
 private const val MESSAGES_DELETED_EVENT_NAME = "onNotificationsDeleted"
 
 open class NotificationsEmitter : Module(), NotificationListener {
-  private lateinit var notificationManager: NotificationManager
   private var lastNotificationResponseBundle: Bundle? = null
 
   override fun definition() = ModuleDefinition {
@@ -28,14 +27,12 @@ open class NotificationsEmitter : Module(), NotificationListener {
     )
 
     OnCreate {
-      // Register the module as a listener in NotificationManager singleton module.
-      // Deregistration happens in onDestroy callback.
-      notificationManager = requireNotNull(appContext.legacyModuleRegistry.getSingletonModule("NotificationManager", NotificationManager::class.java))
-      notificationManager.addListener(this@NotificationsEmitter)
+      // Register the module as a listener in NotificationManager singleton.
+      NotificationManager.addListener(this@NotificationsEmitter)
     }
 
     OnDestroy {
-      notificationManager.removeListener(this@NotificationsEmitter)
+      NotificationManager.removeListener(this@NotificationsEmitter)
     }
 
     Function<Bundle?>("getLastNotificationResponse") {
