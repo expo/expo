e273cad00ba3d86faccb285916656600acaa93a1
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-12-20 11:03:03 +0100

    Spotless

--- packages/expo-calendar/android/src/main/java/expo/modules/calendar/next/utils/CalendarNextUtils.kt
+++ packages/expo-calendar/android/src/main/java/expo/modules/calendar/next/utils/CalendarNextUtils.kt
@@ -16,11 +16,13 @@ import java.util.Date
 val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").apply {
   timeZone = TimeZone.getTimeZone("GMT")
 }
+
 /**
  * [SimpleDateFormat] used in native recurrence rule string.
  * The format corresponds to the 'date-time' type defined by RFC-5455 section 3.3.5.
  */
 val rrFormat = SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'")
+
 /**
  * [SimpleDateFormat] used in native recurrence rule string for all-day events.
  * The format corresponds to the 'date' type defined by RFC-5455 section 3.3.4.
