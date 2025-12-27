b525e31a4f36e1ba3b5a9ebd6204b238a6d056e0
Christian Falch <christian.falch@gmail.com>
2025-12-27 16:45:19 +0100

    [ios][precompile][spm] refactor AppContext/ReactDelegate into interfaces
    In SPM we can't have mixed language targets meaning that we cannot have circular dependencies between obj-c and swift targets: objective-c -> swift -> objective-c.
    
    This PR adds support for the AppContextProtocol, ReactDelegateProtocol and an AppContextFactory.

--- packages/expo-modules-core/ios/JS/EXJSIInstaller.h
+++ packages/expo-modules-core/ios/JS/EXJSIInstaller.h
@@ -6,9 +6,10 @@
 @class RCTBridge;
 #endif
 
-// Swift classes need forward-declaration in the headers.
+// Forward declarations for types - use protocol for AppContext to avoid Swift.h import
 @class EXAppContext;
 @class EXRuntime;
+@protocol EXAppContextProtocol;
 
 #if __has_include(<ReactCommon/RCTRuntimeExecutor.h>)
 @class RCTRuntimeExecutor;
@@ -35,7 +36,7 @@ extern NSString *_Nonnull const EXGlobalCoreObjectPropertyName;
  Installs ExpoModules host object in the runtime of the given app context.
  Returns a bool value whether the installation succeeded.
  */
-+ (BOOL)installExpoModulesHostObject:(nonnull EXAppContext *)appContext;
++ (BOOL)installExpoModulesHostObject:(nonnull id<EXAppContextProtocol>)appContext;
 
 /**
  Installs the base class for shared objects, i.e. `global.expo.SharedObject`.
