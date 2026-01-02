2fbf603cf1accbb28a7f6ca61017fc64a88ec831
evanbacon <bacon@expo.io>
2026-01-02 11:37:01 -0800

    Add expo-image and update static rendering page
    Added 'expo-image' to dependencies and updated the static rendering index page to display an image using expo-image. Also updated .env and README.md with minor improvements for local development and Apple Team ID setup.

--- apps/router-e2e/__e2e__/static-rendering/app/index.tsx
+++ apps/router-e2e/__e2e__/static-rendering/app/index.tsx
@@ -1,9 +1,24 @@
 import { Text } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function Page() {
   return (
-    <Text testID="index-text" style={{ fontFamily: 'sweet' }}>
-      Index
-    </Text>
+    <>
+      <Text testID="index-text" style={{ fontFamily: 'sweet' }}>
+        Index
+      </Text>
+      <Image
+        source="sf:figure.fencing"
+        contentFit="contain"
+        style={{
+          width: 36,
+          aspectRatio: 1,
+          height: 36 * 2,
+          backgroundColor: 'blue',
+          tintColor: 'orange',
+          fontWeight: 'bold',
+        }}
+      />
+    </>
   );
 }
