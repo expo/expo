3e364e82142826dcd22e26ec884f6ea5a21c1173
Aman Mittal <amandeepmittal@live.com>
2026-01-07 17:14:35 +0530

    [expo-modules-core][docs] Fix internal links and typo (#41949)

--- packages/expo-modules-core/src/ts-declarations/NativeModule.ts
+++ packages/expo-modules-core/src/ts-declarations/NativeModule.ts
@@ -1,7 +1,7 @@
 import type { EventEmitter, EventsMap } from './EventEmitter';
 
 /**
- * A class for all native modules. Extends the {@link EventEmitter} class.
+ * A class for all native modules. Extends the [`EventEmitter`](#eventemittertype) class.
  */
 export declare class NativeModule<
   TEventsMap extends EventsMap = Record<never, never>,
