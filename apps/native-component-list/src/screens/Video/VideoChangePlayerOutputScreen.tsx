0edbc7d6ecc79acd6c05ea6ba8798400f0b968d0
Wojciech Dróżdż <31368152+behenate@users.noreply.github.com>
2026-01-09 11:02:01 +0100

    [video] Add e2e tests for expo-video (#41513)
    # Why
    
    We have recently added CI support for e2e tests. We can leverage them to
    test some previously untestable expo-video functionality.
    
    # How
    
    Added a few maestro tests. 
    
    # Test Plan
    
    Tested in pixel 7 pro emulator and iPhone 17 pro simulator (same devices
    as CI)

--- apps/native-component-list/src/screens/Video/VideoChangePlayerOutputScreen.tsx
+++ apps/native-component-list/src/screens/Video/VideoChangePlayerOutputScreen.tsx
@@ -5,6 +5,7 @@ import { View, StyleSheet, Text } from 'react-native';
 import { bigBuckBunnySource, elephantsDreamSource } from './videoSources';
 import { styles } from './videoStyles';
 import Button from '../../components/Button';
+import { E2EViewShotContainer } from '../../components/E2EViewShotContainer';
 import TitledSwitch from '../../components/TitledSwitch';
 
 const playerFactory = (player: VideoPlayer) => {
@@ -19,6 +20,7 @@ export default function VideoChangePlayerOutputScreen() {
   // in real usage by unaware users. We want to check if expo-video gracefully handles
   // this by displaying the video in the last `VideoView` to use it.
   const [useIncorrectReplace, setUseIncorrectReplace] = useState(false);
+  const [nativeControls, setNativeControls] = useState(true);
 
   const player = useVideoPlayer(bigBuckBunnySource, playerFactory);
   const player2 = useVideoPlayer(elephantsDreamSource, playerFactory);
@@ -55,18 +57,24 @@ export default function VideoChangePlayerOutputScreen() {
 
   return (
     <View style={styles.contentContainer}>
-      {Array(4)
-        .fill(0)
-        .map((_, i) => (
-          <VideoView
-            key={i}
-            style={screenStyles.smallVideo}
-            player={viewPlayers[i]}
-            nativeControls={false}
-          />
-        ))}
+      <E2EViewShotContainer
+        testID="view-player-output"
+        mode="keep-originals"
+        screenshotOutputPath="expo-video/screenshots">
+        {Array(4)
+          .fill(0)
+          .map((_, i) => (
+            <VideoView
+              key={i}
+              style={screenStyles.smallVideo}
+              player={viewPlayers[i]}
+              nativeControls={nativeControls}
+              allowsVideoFrameAnalysis={false}
+            />
+          ))}
+      </E2EViewShotContainer>
       <Button title="Move player 1 to the next video view" onPress={() => advancePlayer(0)} />
-      <Button title="Move player 2 from the next video view" onPress={() => advancePlayer(1)} />
+      <Button title="Move player 2 to the next video view" onPress={() => advancePlayer(1)} />
 
       <Text style={styles.centerText}>
         Use buttons below to check the following sequence: {'\n'} 1st button {'->'} 2nd button{' '}
@@ -75,11 +83,23 @@ export default function VideoChangePlayerOutputScreen() {
       <Button title="Move player 1 to first video view" onPress={() => movePlayerTo(0, 0)} />
       <Button title="Move player 2 to first video view" onPress={() => movePlayerTo(1, 0)} />
       <TitledSwitch
+        testID="switch-1"
         title="Move to new view without unmounting from the old one first"
         value={useIncorrectReplace}
         setValue={setUseIncorrectReplace}
         style={screenStyles.switch}
         titleStyle={styles.switchTitle}
+      />
+      <Button
+        id="e2e-prepare"
+        title="Prepare for e2e"
+        onPress={() => {
+          setNativeControls(false);
+          player.pause();
+          player2.pause();
+          player.currentTime = 10;
+          player2.currentTime = 10;
+        }}
       />
     </View>
   );
