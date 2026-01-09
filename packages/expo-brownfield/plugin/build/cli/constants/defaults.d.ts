5f0091817d30e5a3ff4ba502b50e6f027d82cf63
Gabriel Donadel <donadeldev@gmail.com>
2026-01-09 10:47:59 -0300

    [brownfield][iOS] Fix hermes framework path

--- packages/expo-brownfield/plugin/build/cli/constants/defaults.d.ts
+++ packages/expo-brownfield/plugin/build/cli/constants/defaults.d.ts
@@ -1,5 +1,5 @@
 export declare const Defaults: {
     readonly artifactsPath: "./artifacts";
-    readonly hermesFrameworkPath: "Pods/hermes-engine/destroot/Library/Frameworks/universal/hermes.xcframework";
+    readonly hermesFrameworkPath: "Pods/hermes-engine/destroot/Library/Frameworks/universal/hermesvm.xcframework";
     readonly libraryName: "brownfield";
 };
--- packages/expo-brownfield/plugin/build/cli/constants/defaults.js
+++ packages/expo-brownfield/plugin/build/cli/constants/defaults.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Defaults = void 0;
 exports.Defaults = {
     artifactsPath: './artifacts',
-    hermesFrameworkPath: 'Pods/hermes-engine/destroot/Library/Frameworks/universal/hermes.xcframework',
+    hermesFrameworkPath: 'Pods/hermes-engine/destroot/Library/Frameworks/universal/hermesvm.xcframework',
     libraryName: 'brownfield',
 };
