a074cc2d2045634feeab31328a2af2566addcc1b
nishan (o^▽^o) <nishanbende@gmail.com>
2026-01-09 08:07:46 +0530

    update context menu docs

--- packages/expo-ui/build/swift-ui/ContextMenu/types.d.ts
+++ packages/expo-ui/build/swift-ui/ContextMenu/types.d.ts
@@ -2,22 +2,12 @@ import { ReactElement, ReactNode } from 'react';
 import { type ButtonProps } from '../Button';
 import { type CommonViewModifierProps } from '../types';
 /**
- * Activation method of the context menu.
- * - `singlePress` - The context menu is opened with a single tap. Does not isolate the content.
- * - `longPress` - The context menu is opened with a long press. On iOS additionally Highlights the content by blurring the background.
- */
-export type ActivationMethod = 'singlePress' | 'longPress';
-/**
  * Props of the `ContextMenu` component.
  */
 export type ContextMenuProps = {
     /**
-     * Determines how the context menu will be activated.
-     */
-    activationMethod?: ActivationMethod;
-    /**
-     * The contents of the submenu are used as an anchor for the context menu.
-     * The children will be wrapped in a `<Pressable>` component, which triggers opening of the context menu.
+     * The contents of the context menu.
+     * Should include `ContextMenu.Trigger`, `ContextMenu.Items`, and optionally `ContextMenu.Preview`.
      */
     children: ReactNode;
 } & CommonViewModifierProps;
