80a2e35e6c295c1ee685a7cb1da594e30520dc60
nishan (o^▽^o) <nishanbende@gmail.com>
2026-01-12 12:17:48 +0530

    [ui][iOS] - `Form` docs and improvements (#41728)
    # Why
    
    Form docs and API improvements
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    - Added examples in `Form` docs
    - Removed `scrollEnabled` prop and added `scrollDisabled` modifier which
    can be used for disabling scroll.
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    Tested form examples
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo-ui/ios/List.swift
+++ packages/expo-ui/ios/List.swift
@@ -45,7 +45,6 @@ struct ListView: ExpoSwiftUI.View {
       .onChange(of: selection) { selection in
         handleSelectionChange(selection: selection)
       }
-      .modifier(ScrollDisabledModifier(scrollEnabled: props.scrollEnabled))
       .environment(\.editMode, $editModeEnabled)
     if #available(iOS 16.0, tvOS 16.0, *) {
       list.scrollDisabled(!props.scrollEnabled)
@@ -102,18 +101,6 @@ struct ListStyleModifer: ViewModifier {
       content.listStyle(.sidebar)
 #endif
     default:
-      content
-    }
-  }
-}
-
-struct ScrollDisabledModifier: ViewModifier {
-  let scrollEnabled: Bool
-
-  func body(content: Content) -> some View {
-    if #available(iOS 16.0, tvOS 16.0, *) {
-      content.scrollDisabled(!scrollEnabled)
-    } else {
       content
     }
   }
