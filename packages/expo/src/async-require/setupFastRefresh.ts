af044a0fc642ba95577d47d3974656b2fd3cfc7b
Vojtech Novak <vonovak@gmail.com>
2026-01-12 14:20:16 +0000

    [expo] change `global` for `globalThis` to fix failing `auth-session` tests (#42083)
    # Why
    
    
    auth-session tests currently fail with a TS error
    
    ```
     FAIL   Web  src/__tests__/AuthSession-test.web.ts
      ● Test suite failed to run
    
        ../expo/src/Expo.fx.web.tsx:12:36 - error TS2304: Cannot find name 'global'.
    
        12     !('__fbBatchedBridgeConfig' in global)
                                              ~~~~~~
        ../expo/src/Expo.fx.web.tsx:14:27 - error TS2304: Cannot find name 'global'.
    
        14     Object.defineProperty(global, '__fbBatchedBridgeConfig', {
                                     ~~~~~~
    
     FAIL   Node  src/__tests__/AuthSession-test.web.ts
      ● Test suite failed to run
    
        ../expo/src/Expo.fx.web.tsx:12:36 - error TS2304: Cannot find name 'global'.
    
        12     !('__fbBatchedBridgeConfig' in global)
                                              ~~~~~~
        ../expo/src/Expo.fx.web.tsx:14:27 - error TS2304: Cannot find name 'global'.
    
        14     Object.defineProperty(global, '__fbBatchedBridgeConfig', {
            
    ```
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    - use updated typings
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    - `et cp -a` checks okay all packages
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo/src/async-require/setupFastRefresh.ts
+++ packages/expo/src/async-require/setupFastRefresh.ts
@@ -1,7 +1,7 @@
 // This needs to run before the renderer initializes.
 
 const ReactRefreshRuntime = require('react-refresh/runtime');
-ReactRefreshRuntime.injectIntoGlobalHook(global);
+ReactRefreshRuntime.injectIntoGlobalHook(globalThis);
 
 const Refresh = {
   performFullRefresh() {
@@ -27,4 +27,5 @@ const Refresh = {
 
 // The metro require polyfill can not have dependencies (applies for all polyfills).
 // Expose `Refresh` by assigning it to global to make it available in the polyfill.
-global[(global.__METRO_GLOBAL_PREFIX__ || '') + '__ReactRefresh'] = Refresh;
+// @ts-ignore: ignore the global which may not always be defined in jest environments.
+globalThis[(globalThis.__METRO_GLOBAL_PREFIX__ ?? '') + '__ReactRefresh'] = Refresh;
