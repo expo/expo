30966bfd7d51ec662b2108fdb1d01142041f187e
Vojtech Novak <vonovak@gmail.com>
2026-01-13 23:15:25 +0000

    [notifications] fix named exports (#42116)

--- packages/expo-notifications/src/deleteNotificationChannelGroupAsync.android.ts
+++ packages/expo-notifications/src/deleteNotificationChannelGroupAsync.android.ts
@@ -2,7 +2,7 @@ import { UnavailabilityError } from 'expo-modules-core';
 
 import NotificationChannelGroupManager from './NotificationChannelGroupManager';
 
-export default async function deleteNotificationChannelAsync(groupId: string): Promise<void> {
+export async function deleteNotificationChannelAsync(groupId: string): Promise<void> {
   if (!NotificationChannelGroupManager.deleteNotificationChannelGroupAsync) {
     throw new UnavailabilityError('Notifications', 'deleteNotificationChannelGroupAsync');
   }
