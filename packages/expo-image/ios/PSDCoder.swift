e23e4db547cfae7fb01769ec6fe614514b5e61a5
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2026-01-13 07:41:53 +0100

    Apply suggestions from code review
    Co-authored-by: Tomasz Sapeta <tsapeta@expo.dev>

--- packages/expo-image/ios/PSDCoder.swift
+++ packages/expo-image/ios/PSDCoder.swift
@@ -6,7 +6,9 @@ internal final class PSDCoder: NSObject, SDImageCoder {
   nonisolated(unsafe) static let shared = PSDCoder()
   
   func canDecode(from data: Data?) -> Bool {
-    guard let data = data, data.count >= 4 else { return false }
+    guard let data, data.count >= 4 else {
+      return false
+    }
     
     // verify PSD magic bytes
     let signatureData = data[0..<4]
@@ -15,7 +17,9 @@ internal final class PSDCoder: NSObject, SDImageCoder {
   }
   
   func decodedImage(with data: Data?, options: [SDImageCoderOption : Any]? = nil) -> UIImage? {
-    guard let data = data else { return nil }
+    guard let data else {
+      return nil
+    }
     
     if let scale = options?[SDImageCoderOption.decodeScaleFactor] as? CGFloat {
       return UIImage(data: data, scale: scale)
@@ -26,7 +30,7 @@ internal final class PSDCoder: NSObject, SDImageCoder {
   }
   
   func canEncode(to format: SDImageFormat) -> Bool {
-    false
+    return false
   }
   
   func encodedData(with image: UIImage?, format: SDImageFormat, options: [SDImageCoderOption : Any]? = nil) -> Data? {
