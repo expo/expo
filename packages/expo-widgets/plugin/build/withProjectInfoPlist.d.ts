ea0345195e2da0ad862a9348b81d34e4e8bb035b
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2026-01-15 16:49:01 +0100

    Fix build

--- packages/expo-widgets/plugin/build/withProjectInfoPlist.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigPlugin } from 'expo/config-plugins';
-interface withAppInfoPlistProps {
-    frequentUpdates?: boolean;
-    groupIdentifier: string;
-}
-declare const withAppInfoPlist: ConfigPlugin<withAppInfoPlistProps>;
-export default withAppInfoPlist;
