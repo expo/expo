3b7e5e2202b5c3671a92cd828d1e656592e30a77
Gabriel Donadel <donadeldev@gmail.com>
2026-01-15 16:10:44 -0300

    [brownfield][plugin] Simplify withXcodeProjectPlugin

--- packages/expo-brownfield/plugin/src/ios/plugins/withXcodeProjectPlugin.ts
+++ packages/expo-brownfield/plugin/src/ios/plugins/withXcodeProjectPlugin.ts
@@ -30,28 +30,27 @@ const withXcodeProjectPlugin: ConfigPlugin<PluginConfig> = (config, pluginConfig
     // Create a directory for the framework files
     const groupPath = path.join(projectRoot, 'ios', pluginConfig.targetName);
     mkdir(groupPath);
-    // Create the React Native host manager based on the template
-    createFileFromTemplate('ReactNativeHostManager.swift', groupPath);
-    // Create the messaging proxy based on the template
-    createFileFromTemplate('Messaging.swift', groupPath);
-    // Create the SwiftUI brownfield entrypoint based on the template
-    createFileFromTemplate('ReactNativeView.swift', groupPath);
-    // Create the UIKit brownfield view controller based on the template
-    createFileFromTemplate('ReactNativeViewController.swift', groupPath);
-    // Create the BrownfieldAppDelegate based on the template
-    createFileFromTemplate('BrownfieldAppDelegate.swift', groupPath);
-    // Create the ReactNativeDelegate based on the template
-    createFileFromTemplate('ReactNativeDelegate.swift', groupPath);
+
+    const templateFiles = [
+      // React Native host manager
+      'ReactNativeHostManager.swift',
+      // Messaging proxy
+      'Messaging.swift',
+      //SwiftUI brownfield entrypoint
+      'ReactNativeView.swift',
+      // UIKit brownfield view controller
+      'ReactNativeViewController.swift',
+      // BrownfieldAppDelegate
+      'BrownfieldAppDelegate.swift',
+      // ReactNativeDelegate
+      'ReactNativeDelegate.swift',
+    ];
+
+    // Create files from templates
+    templateFiles.forEach((templateFile) => createFileFromTemplate(templateFile, groupPath));
 
     // Create and properly add a new group for the framework
-    createGroup(xcodeProject, pluginConfig.targetName, groupPath, [
-      'ReactNativeHostManager.swift',
-      'Messaging.swift',
-      'ReactNativeView.swift',
-      'ReactNativeViewController.swift',
-      'BrownfieldAppDelegate.swift',
-      'ReactNativeDelegate.swift',
-    ]);
+    createGroup(xcodeProject, pluginConfig.targetName, groupPath, templateFiles);
 
     // Create 'Info.plist' and '<target-name>.entitlements' based on the templates
     createFileFromTemplate('Info.plist', groupPath, {
@@ -67,18 +66,14 @@ const withXcodeProjectPlugin: ConfigPlugin<PluginConfig> = (config, pluginConfig
     // Configure build phases:
     // - Reference Expo app target's RN bundle script
     // - Add custom script for patching ExpoModulesProvider
-    // - Add 'ReactNativeHostManager.swift', 'ReactNativeView.swift',
-    //   'Messaging.swift', 'ReactNativeViewController.swift' and
-    //   'BrownfieldAppDelegate.swift'
-    //   to the compile sources phase
-    configureBuildPhases(xcodeProject, target, pluginConfig.targetName, projectName, [
-      `${pluginConfig.targetName}/ReactNativeHostManager.swift`,
-      `${pluginConfig.targetName}/Messaging.swift`,
-      `${pluginConfig.targetName}/ReactNativeView.swift`,
-      `${pluginConfig.targetName}/ReactNativeViewController.swift`,
-      `${pluginConfig.targetName}/BrownfieldAppDelegate.swift`,
-      `${pluginConfig.targetName}/ReactNativeDelegate.swift`,
-    ]);
+    // - Add template files to the compile sources phase
+    configureBuildPhases(
+      xcodeProject,
+      target,
+      pluginConfig.targetName,
+      projectName,
+      templateFiles.map((file) => `${pluginConfig.targetName}/${file}`)
+    );
     // Add the required build settings
     configureBuildSettings(
       xcodeProject,
