24b959cf07fdeb48e5f2a27614e17989d5098524
Jakub Tkacz <32908614+Ubax@users.noreply.github.com>
2026-01-18 13:37:35 +0100

    [expo-router] add unstable_navigationEvents to globalThis.expo (#42238)
    # Why
    
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    
    ---------
    
    Co-authored-by: Tomasz Sapeta <tsapeta@expo.dev>

--- apps/router-e2e/__e2e__/native-navigation/app/params/index.tsx
+++ apps/router-e2e/__e2e__/native-navigation/app/params/index.tsx
@@ -4,7 +4,6 @@ import { Text, Pressable, ScrollView, View } from 'react-native';
 
 const HomeIndex = () => {
   const pathname = usePathname();
-
   useEffect(() => {
     console.log(
       'globalThis.expo.router',
@@ -19,14 +18,19 @@ const HomeIndex = () => {
       contentContainerStyle={{ alignItems: 'center', gap: 16 }}
       contentInsetAdjustmentBehavior="automatic">
       <View>
-        <Text>Native navigation - Index</Text>
+        <Text>Params</Text>
         <Text>Current Path: {pathname}</Text>
       </View>
-      <CaseLink href="/tabs" text="Native Tabs" />
-      <CaseLink href="/toolbar" text="Toolbar" />
-      <CaseLink href="/header-items" text="Header Items" />
-      <CaseLink href="/modals" text="Modals" />
-      <CaseLink href="/params" text="Params" />
+      <CaseLink href="/params/123" text="/params/123" />
+      <CaseLink href="/params/123?a=x&b=0" text="/params/123?a=x&b=0" />
+      <CaseLink
+        href={{
+          pathname: '/params/[path]',
+          params: { a: 'x', b: '0', path: '123' },
+        }}
+        text="/params/123?a=x&b=0 object"
+      />
+      <CaseLink href="/params/456/aaa?x=1&y=2" text="/params/456/aaa?x=1&y=2" />
     </ScrollView>
   );
 };
