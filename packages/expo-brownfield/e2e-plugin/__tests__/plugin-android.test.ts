f9a1ce13d98af03bded7ab9c007e754c7490b44c
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-01-20 20:32:36 +0100

    [test][brownfield] set up e2e tests for expo-brownfield config plugin

---
+++ packages/expo-brownfield/e2e-plugin/__tests__/plugin-android.test.ts
@@ -0,0 +1,7 @@
+describe('plugin for android', () => {
+  it('2 + 2 = 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  // TODO(pmleczek): Add tests for the plugin
+});
---
+++ packages/expo-brownfield/e2e-plugin/__tests__/plugin-ios.test.ts
@@ -0,0 +1,13 @@
+describe('plugin for ios', () => {
+  it('2 + 2 = 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  // Test no values
+
+  // Test bundle identifier
+
+  // Test target name
+
+  // Test bundle identifier + target name
+});
