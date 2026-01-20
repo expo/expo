973f87702e8574298ac3753ef40d583b9dda3da2
nishan (o^▽^o) <nishanbende@gmail.com>
2026-01-20 23:32:59 +0530

    lint fix

--- packages/expo-ui/src/swift-ui/index.tsx
+++ packages/expo-ui/src/swift-ui/index.tsx
@@ -37,4 +37,4 @@ export * from './Shapes';
 export * from './Popover';
 export * from './Grid';
 export * from './RNHostView';
-export {type CommonViewModifierProps} from './types';
+export { type CommonViewModifierProps } from './types';
