ea61e45fd8d78dc97f758fd88e02c4236cc20dca
Gabriel Donadel <donadeldev@gmail.com>
2026-01-20 14:41:27 -0300

    [brownfield-tester] Add android project to test integrated approach

---
+++ apps/brownfield-tester/android-integrated/app/src/main/java/dev/expo/brownfieldintegratedtester/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.expo.brownfieldintegratedtester
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.expo.brownfieldintegratedtester.ui.theme.BrownfieldIntegratedTesterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BrownfieldIntegratedTesterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BrownfieldIntegratedTesterTheme {
+        Greeting("Android")
+    }
+}
