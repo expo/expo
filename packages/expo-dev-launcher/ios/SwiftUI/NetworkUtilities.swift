d1ac1f69437d35fb49b2d2417db80400fed1ef65
Phil Pluckthun <phil@kitten.sh>
2026-01-21 14:49:45 +0000

    Implement initial bonjour service discovery

--- packages/expo-dev-launcher/ios/SwiftUI/NetworkUtilities.swift
+++ packages/expo-dev-launcher/ios/SwiftUI/NetworkUtilities.swift
@@ -3,6 +3,33 @@
 import Foundation
 import Network
 
+enum ConnectionError: Error {
+    case failedConnection
+    case noPathToHost
+    case invalidResponse
+}
+
+func buildHttpHost(endpoint: NWEndpoint) -> String? {
+  guard case let .hostPort(remoteHost, remotePort) = endpoint else {
+    return nil
+  }
+  let hostname: String? = switch remoteHost {
+    case .name(let name, _):
+      name
+    case .ipv4(IPv4Address.loopback):
+      "localhost"
+    case .ipv4(let ip):
+      IPv4Address(ip.rawValue)!.debugDescription // drop interface suffix
+    case .ipv6(IPv6Address.loopback):
+      "localhost"
+    case .ipv6(let ip):
+      "[\(ip.debugDescription)]"
+    default:
+      nil
+  }
+  return hostname.map { "http://\($0):\(remotePort)" }
+}
+
 class NetworkUtilities {
   // Same approach as expo-network just without throwing.
   static func getLocalIPAddress() -> String? {
@@ -69,5 +96,101 @@ class NetworkUtilities {
       "localhost",
       "\(subnet).1"
     ]
+  }
+
+  static func resolveBundlerEndpoint(
+    endpoint: NWEndpoint,
+    queue: DispatchQueue,
+  ) async throws -> String? {
+    let params = NWParameters.tcp
+    params.includePeerToPeer = true
+    params.allowFastOpen = true
+    params.allowLocalEndpointReuse = true
+    params.preferNoProxies = true
+    params.requiredInterface = endpoint.interface
+    params.expiredDNSBehavior = NWParameters.ExpiredDNSBehavior.allow
+
+    let connection = NWConnection(to: endpoint, using: params)
+    defer { connection.cancel() }
+
+    try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Void, Error>) in
+      connection.stateUpdateHandler = { state in
+        let handler = connection.stateUpdateHandler
+        connection.stateUpdateHandler = nil
+        switch state {
+          case .ready:
+            cont.resume()
+          case .failed(let error):
+            cont.resume(throwing: error)
+          case .cancelled:
+            cont.resume(throwing: ConnectionError.failedConnection)
+          default:
+            connection.stateUpdateHandler = handler
+        }
+      }
+      connection.start(queue: queue)
+    }
+
+    guard let host = connection.currentPath
+      .flatMap({ $0.remoteEndpoint })
+      .flatMap({ buildHttpHost(endpoint: $0) })
+    else {
+      throw ConnectionError.noPathToHost
+    }
+
+    let requestString = """
+    GET /status HTTP/1.1\r
+    Host: \(host)\r
+    Connection: close\r
+    \r
+
+    """
+
+    let requestData = requestString.data(using: .utf8)!
+
+    try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Void, Error>) in
+      connection.send(content: requestData, completion: .contentProcessed { error in
+        if let error = error {
+          cont.resume(throwing: error)
+        } else {
+          cont.resume()
+        }
+      })
+    }
+
+    let responseData = try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Data, Error>) in
+      var responseData = Data()
+      func receiveLoop() {
+        connection.receive(minimumIncompleteLength: 1, maximumLength: 4096) { data, _, isComplete, error in
+          if let error = error {
+            cont.resume(throwing: error)
+            return
+          }
+
+          if let data = data {
+            responseData.append(data)
+          }
+
+          if isComplete {
+            cont.resume(returning: responseData)
+          } else {
+            receiveLoop()
+          }
+        }
+      }
+      receiveLoop()
+    }
+
+    guard let responseString = String(data: responseData, encoding: .utf8) else {
+      throw ConnectionError.invalidResponse
+    }
+
+    let parts = responseString.components(separatedBy: "\r\n\r\n")
+    let body = parts.dropFirst().joined(separator: "\r\n\r\n")
+    if body.contains("packager-status:running") {
+      return host
+    } else {
+      return nil
+    }
   }
 }
