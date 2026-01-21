723d650cecea89138d79dd02211e6aab00758ed8
Aman Mittal <amandeepmittal@live.com>
2026-01-22 00:17:50 +0530

    [docs] Add support for compound subcomponents  in TypeDoc (#42391)

--- docs/components/plugins/api/APISectionCompoundNames.test.ts
+++ docs/components/plugins/api/APISectionCompoundNames.test.ts
@@ -1,5 +1,5 @@
 import { GeneratedData, PropData, TypeDefinitionData, TypeDocKind } from './APIDataTypes';
-import { buildCompoundNameByComponent } from './APISectionCompoundNames';
+import { buildCompoundNameByComponent, deriveComponentsFromProps } from './APISectionCompoundNames';
 
 const makeComponentType = (propsName: string): TypeDefinitionData => ({
   type: 'reference',
@@ -62,5 +62,38 @@ describe('buildCompoundNameByComponent', () => {
     expect(result).toEqual({
       Tab: 'Tabs.Tab',
     });
+  });
+});
+
+describe('deriveComponentsFromProps', () => {
+  test('derives components from prop types when not exported', () => {
+    const menu = makeComponent('Menu', 'MenuProps', [
+      makeProp('Item', makeComponentType('MenuItemProps')),
+    ]);
+
+    const result = deriveComponentsFromProps([menu]).map(entry => entry.name);
+
+    expect(result).toEqual(['Menu', 'MenuItem']);
+  });
+
+  test('derives nested components from compound component properties', () => {
+    const menuItem = makeComponent('MenuItem', 'MenuItemProps', [
+      makeProp('Icon', makeComponentType('MenuItemIconProps')),
+    ]);
+    const menu = makeComponent('Menu', 'MenuProps', [
+      makeProp('Item', makeComponentType('MenuItemProps')),
+    ]);
+
+    const result = deriveComponentsFromProps([menu, menuItem]).map(entry => entry.name);
+
+    expect(result).toEqual(['Menu', 'MenuItem', 'MenuItemIcon']);
+  });
+
+  test('derives components from string default values', () => {
+    const tabs = makeComponent('Tabs', 'TabsProps', [makeProp('Tab', undefined, 'Tab')]);
+
+    const result = deriveComponentsFromProps([tabs]).map(entry => entry.name);
+
+    expect(result).toEqual(['Tabs', 'Tab']);
   });
 });
