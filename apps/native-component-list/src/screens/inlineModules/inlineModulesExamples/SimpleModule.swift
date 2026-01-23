eaaf99f4a58b807180fbca1e97b96d1cea94b840
HubertBer <bernackihubert03@gmail.com>
2026-01-23 15:21:39 +0100

    rebase fixes

--- apps/native-component-list/src/screens/inlineModules/inlineModulesExamples/SimpleModule.swift
+++ apps/native-component-list/src/screens/inlineModules/inlineModulesExamples/SimpleModule.swift
@@ -1,7 +1,7 @@
-import ExpoModulesCore
+internal import ExpoModulesCore
 import WebKit
 
-public class SimpleModule: Module {
+class SimpleModule: Module {
   public func definition() -> ModuleDefinition {
     Constant("test") {
       return "Swift constant 1283"
