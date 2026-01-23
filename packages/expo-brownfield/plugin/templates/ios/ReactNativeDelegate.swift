1d6972c2c8748c101e197ad85093f304c68bd329
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-01-23 14:48:32 +0100

    [ios][brownfield] tidy up ReactNativeDelegate.swift template

--- packages/expo-brownfield/plugin/templates/ios/ReactNativeDelegate.swift
+++ packages/expo-brownfield/plugin/templates/ios/ReactNativeDelegate.swift
@@ -2,7 +2,7 @@ internal import Expo
 internal import React
 
 class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
-  /// Extension point for config-plugins
+  // Extension point for config-plugins
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     // Needed to return the correct URL for expo-dev-client
     bridge.bundleURL ?? bundleURL()
@@ -13,8 +13,8 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
       return RCTBundleURLProvider.sharedSettings().jsBundleURL(
         forBundleRoot: ".expo/.virtual-metro-entry")
     #else
-      /// `main.jsbundle` isn't part of the main app bundle
-      /// so we need to load it from the framework bundle
+      // `main.jsbundle` isn't part of the main app bundle
+      // so we need to load it from the framework bundle
       let frameworkBundle = Bundle(for: ReactNativeHostManager.self)
       return frameworkBundle.url(forResource: "main", withExtension: "jsbundle")
     #endif
