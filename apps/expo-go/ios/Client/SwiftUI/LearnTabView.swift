d794da45eaccb994a9e7b1cd4b3acfab761c3bdb
evanbacon <bacon@expo.io>
2026-01-23 15:46:03 -0800

    Update LearnTabView.swift

--- apps/expo-go/ios/Client/SwiftUI/LearnTabView.swift
+++ apps/expo-go/ios/Client/SwiftUI/LearnTabView.swift
@@ -14,16 +14,36 @@ struct LearnTabView: View {
 struct DocsWebView: UIViewRepresentable {
   let url: URL
 
+  func makeCoordinator() -> Coordinator {
+    Coordinator()
+  }
+
   func makeUIView(context: Context) -> WKWebView {
     let configuration = WKWebViewConfiguration()
     configuration.allowsInlineMediaPlayback = true
     configuration.mediaTypesRequiringUserActionForPlayback = .all
 
     let webView = WKWebView(frame: .zero, configuration: configuration)
+    webView.navigationDelegate = context.coordinator
     webView.load(URLRequest(url: url))
     return webView
   }
 
   func updateUIView(_ webView: WKWebView, context: Context) {
+  }
+
+  class Coordinator: NSObject, WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+      guard let url = navigationAction.request.url, let host = url.host?.lowercased() else {
+        decisionHandler(.cancel)
+        return
+      }
+
+      if host == "expo.dev" || host.hasSuffix(".expo.dev") {
+        decisionHandler(.allow)
+      } else {
+        decisionHandler(.cancel)
+      }
+    }
   }
 }
