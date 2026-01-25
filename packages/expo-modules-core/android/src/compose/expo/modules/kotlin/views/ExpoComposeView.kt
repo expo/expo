0bea9a32684b1b6cb25f7c7ef0f610eda04b82ce
Kudo Chien <kudo@expo.dev>
2026-01-26 02:43:42 +0800

    [core] fix fast refresh for expo-ui jetpack-compose

--- packages/expo-modules-core/android/src/compose/expo/modules/kotlin/views/ExpoComposeView.kt
+++ packages/expo-modules-core/android/src/compose/expo/modules/kotlin/views/ExpoComposeView.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.RecomposeScope
+import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.ComposeView
@@ -45,6 +47,7 @@ abstract class ExpoComposeView<T : ComposeProps>(
   private val withHostingView: Boolean = false
 ) : ExpoView(context, appContext) {
   open val props: T? = null
+  protected var recomposeScope: RecomposeScope? = null
 
   @Composable
   abstract fun ComposableScope.Content()
@@ -78,6 +81,7 @@ abstract class ExpoComposeView<T : ComposeProps>(
 
   @Composable
   fun Children(composableScope: ComposableScope?) {
+    recomposeScope = currentRecomposeScope
     for (index in 0..<this.size) {
       val child = getChildAt(index) as? ExpoComposeView<*> ?: continue
       with(composableScope ?: ComposableScope()) {
@@ -90,6 +94,7 @@ abstract class ExpoComposeView<T : ComposeProps>(
 
   @Composable
   fun Child(composableScope: ComposableScope, index: Int) {
+    recomposeScope = currentRecomposeScope
     val child = getChildAt(index) as? ExpoComposeView<*> ?: return
     with(composableScope) {
       with(child) {
@@ -139,6 +144,16 @@ abstract class ExpoComposeView<T : ComposeProps>(
       child
     }
     super.addView(view, index, params)
+  }
+
+  override fun onViewAdded(child: View?) {
+    super.onViewAdded(child)
+    recomposeScope?.invalidate()
+  }
+
+  override fun onViewRemoved(child: View?) {
+    super.onViewRemoved(child)
+    recomposeScope?.invalidate()
   }
 }
 
