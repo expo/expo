dfb1382e98c95a0d91de770dbb72a1fb0eed6dd2
Phil Pluckthun <phil@kitten.sh>
2026-01-26 16:40:11 +0000

    Reset cache if headers change

--- packages/@expo/cli/src/api/graphql/client.ts
+++ packages/@expo/cli/src/api/graphql/client.ts
@@ -74,7 +74,12 @@ export const query = (() => {
         }, {} as JSONObject)
     );
 
-  const cache: Record<string, Map<string, unknown>> = {};
+  let cache: Record<string, Map<string, unknown>> = {};
+  let cacheKey: string | undefined;
+
+  function resetCache() {
+    cache = {};
+  }
 
   return async function query<Result extends JSONObject, Variables extends JSONObject>(
     query: StaticDocumentNode<Result, Variables>,
@@ -86,21 +91,26 @@ export const query = (() => {
     let data: Result | null | undefined;
     let error: unknown;
 
+    // Pre-instantiate headers and reset the cache if they've changed
+    const headers = { ...getFetchHeaders(), ...options?.headers };
+    const headersKey = stringifySorted(headers);
+    if (!cacheKey || cacheKey !== headersKey) {
+      resetCache();
+    }
+
+    // Retrieve a cached result, if we have any via a `query => variables => Result` cache key
     const variablesKey = stringifySorted(variables);
     const queryCache = cache[query] || (cache[query] = new Map());
     if (queryCache.has(variablesKey)) {
       data = queryCache.get(variablesKey) as Result;
     }
 
+    // Retry the query if it fails due to an unknown or transient error
     for (let attemptCount = 0; attemptCount < 3 && !data; attemptCount++) {
+      // Add a random delay on each subsequent attempt
       if (attemptCount > 0) {
         await randomDelay(attemptCount);
       }
-
-      const headers = {
-        ...getFetchHeaders(),
-        ...options?.headers,
-      };
 
       try {
         response = await wrappedFetch(url, {
@@ -116,9 +126,12 @@ export const query = (() => {
 
       const json = await response.json();
       if (typeof json === 'object' && json) {
+        // If we have a transient error, we retry immediately and discard the data
+        // Otherwise, we store the first available error and get the data
         if ('errors' in json && Array.isArray(json.errors)) {
           isTransient = json.errors.some((e) => e?.extensions?.isTransient);
           if (isTransient) {
+            data = undefined;
             continue;
           } else {
             error = json.errors[0] || error;
@@ -128,21 +141,25 @@ export const query = (() => {
         try {
           data = getResponseDataOrThrow<Result | null>(json);
         } catch (dataError) {
-          error = dataError || error;
+          // We only use the data error, if we don't have an error already
+          if (!error) {
+            error = dataError || error;
+          }
           continue;
         }
       }
     }
 
+    // Store the data in the cache, and only return a result if we have any values
     if (data) {
       queryCache.set(variablesKey, data);
-
       const keys = Object.keys(data);
       if (keys.length > 0 && keys.some((key) => data[key as keyof typeof data] != null)) {
         return data;
       }
     }
 
+    // If we have an error, rethrow it wrapped in our custom errors
     if (error) {
       if (isTransient) {
         Log.error(`We've encountered a transient error, please try again shortly.`);
