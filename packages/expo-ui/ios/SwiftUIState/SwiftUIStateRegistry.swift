31e06014878810052ea36ce20c545eaa734f61b8
nishan (o^▽^o) <nishanbende@gmail.com>
2026-01-26 19:40:16 +0530

    wip

--- packages/expo-ui/ios/SwiftUIState/SwiftUIStateRegistry.swift
+++ packages/expo-ui/ios/SwiftUIState/SwiftUIStateRegistry.swift
@@ -22,10 +22,13 @@ internal final class SwiftUIState: ObservableObject {
 internal final class SwiftUIStateRegistry {
   static let shared = SwiftUIStateRegistry()
 
+  private let lock = NSLock()
   private var states: [Int: SwiftUIState] = [:]
   private var nextId: Int = 0
 
   func createState(initialValue: Any?) -> Int {
+    lock.lock()
+    defer { lock.unlock() }
     let id = nextId
     nextId += 1
     states[id] = SwiftUIState(id: id, initialValue: initialValue)
@@ -33,26 +36,38 @@ internal final class SwiftUIStateRegistry {
   }
 
   func getState(id: Int) -> SwiftUIState? {
+    lock.lock()
+    defer { lock.unlock() }
     return states[id]
   }
 
   func deleteState(id: Int) {
+    lock.lock()
+    defer { lock.unlock() }
     states.removeValue(forKey: id)
   }
 
   func getValue(id: Int) -> Any? {
+    lock.lock()
+    defer { lock.unlock() }
     return states[id]?.value
   }
 
   func setValue(id: Int, value: Any?) {
+    lock.lock()
+    defer { lock.unlock() }
     states[id]?.value = value
   }
 
   func onChange(id: Int, callback: @escaping (Any?) -> Void) {
+    lock.lock()
+    defer { lock.unlock() }
     states[id]?.onChange = callback
   }
 
   func clear() {
+    lock.lock()
+    defer { lock.unlock() }
     states.removeAll()
   }
 }
