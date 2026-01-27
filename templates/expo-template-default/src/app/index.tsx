c647e7d0e80448dd47811966ae0800c6c8f6e9b5
aleqsio <mikucki@gmail.com>
2026-01-27 18:09:50 +0200

    Update default template

--- templates/expo-template-default/src/app/index.tsx
+++ templates/expo-template-default/src/app/index.tsx
@@ -10,14 +10,21 @@ import { WebBadge } from '@/components/web-badge';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
 function getDevMenuHint() {
+  if (Platform.OS === 'web') {
+    return <ThemedText type="small">use browser devtools</ThemedText>;
+  }
   if (Device.isDevice) {
-    return 'shake device';
+    return (
+      <ThemedText type="small">
+        shake device or press <ThemedText type="code">m</ThemedText> in terminal
+      </ThemedText>
+    );
   }
   const shortcut = Platform.OS === 'android' ? 'cmd+m' : 'cmd+d';
   return (
-    <>
+    <ThemedText type="small">
       press <ThemedText type="code">{shortcut}</ThemedText>
-    </>
+    </ThemedText>
   );
 }
 
@@ -37,9 +44,15 @@ export default function HomeScreen() {
         </ThemedText>
 
         <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow title="Try editing" hint={<ThemedText type="code">src/app/index.tsx</ThemedText>} />
-          {Platform.OS !== 'web' && <HintRow title="Dev tools" hint={getDevMenuHint()} />}
-          <HintRow title="Fresh start" hint={<ThemedText type="code">npm run reset-project</ThemedText>} />
+          <HintRow
+            title="Try editing"
+            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
+          />
+          <HintRow title="Dev tools" hint={getDevMenuHint()} />
+          <HintRow
+            title="Fresh start"
+            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
+          />
         </ThemedView>
 
         {Platform.OS === 'web' && <WebBadge />}
