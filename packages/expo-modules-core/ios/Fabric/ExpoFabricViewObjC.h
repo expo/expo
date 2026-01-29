bf4c39c5c6dd4e38a6ab8f65952db115fb03deed
Christian Falch <875252+chrfalch@users.noreply.github.com>
2026-01-29 15:34:38 +0100

    [ios][precompile][spm] refactor to avoid re-export of React in Expo Views (#41850)
    # Why
    
    When declaring Expo base views we're re-exporting React through the RCTView inheritance.
    
    This PR Fixes this by inheriting from UIVIew when building clang modules.

--- packages/expo-modules-core/ios/Fabric/ExpoFabricViewObjC.h
+++ packages/expo-modules-core/ios/Fabric/ExpoFabricViewObjC.h
@@ -2,8 +2,6 @@
 
 #import <ExpoModulesCore/Platform.h>
 
-#ifdef RCT_NEW_ARCH_ENABLED
-
 #ifdef __cplusplus
 
 #import <React/RCTViewComponentView.h> // Allows non-umbrella since it's coming from React-RCTFabric
@@ -13,22 +11,31 @@
 
 #else
 
-// Interface visible in Swift
-@interface ExpoFabricViewObjC
+// Interface visible in Swift - we expose UIView inheritance so Swift sees this as a UIView subclass.
+// The actual C++ implementation inherits from RCTViewComponentView (which is a UIView subclass).
+// Methods that Swift needs to override must be declared here (not in a category) so Swift
+// can properly override them.
+@interface ExpoFabricViewObjC : UIView
+
+/*
+ * Called for mounting (attaching) a child component view inside `self` component view.
+ * Declared in main interface (not category) so Swift subclasses can override.
+ */
+- (void)mountChildComponentView:(nonnull UIView *)childComponentView index:(NSInteger)index;
+
+/*
+ * Called for unmounting (detaching) a child component view from `self` component view.
+ * Declared in main interface (not category) so Swift subclasses can override.
+ */
+- (void)unmountChildComponentView:(nonnull UIView *)childComponentView index:(NSInteger)index;
+
 @end
 
 #endif // __cplusplus
-#else  // Paper
 
-#import <React/RCTView.h>
-
-@interface ExpoFabricViewObjC : RCTView
-@end
-
-#endif // !RCT_NEW_ARCH_ENABLED
-
-@class EXAppContext;
-@class EXViewModuleWrapper;
+// Forward declarations for ObjC compatibility - these are Swift classes with @objc(EX...) names
+// We use the protocol for AppContext to allow proper Swift type bridging
+@protocol EXAppContextProtocol;
 
 // Addition to the interface that is visible in both Swift and Objective-C
 @interface ExpoFabricViewObjC (ExpoFabricViewInterface)
@@ -49,21 +56,12 @@
 
 - (void)prepareForRecycle;
 
-/*
- * Called for mounting (attaching) a child component view inside `self` component view.
- */
-- (void)mountChildComponentView:(nonnull UIView *)childComponentView index:(NSInteger)index;
-
-/*
- * Called for unmounting (detaching) a child component view from `self` component view.
- */
-- (void)unmountChildComponentView:(nonnull UIView *)childComponentView index:(NSInteger)index;
-
 #pragma mark - Component registration
 
 /**
  Registers given view module in the global `RCTComponentViewFactory`.
+ Uses `id` types to allow proper bridging of Swift classes with @objc(EX...) names.
  */
-+ (void)registerComponent:(nonnull EXViewModuleWrapper *)viewModule appContext:(nonnull EXAppContext *)appContext;
++ (void)registerComponent:(nonnull id)viewModule appContext:(nonnull id<EXAppContextProtocol>)appContext;
 
 @end
