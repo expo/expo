47311b887825eb70d398e51895057a2df59203d6
Christian Falch <christian.falch@gmail.com>
2026-01-29 10:20:26 +0100

    Added use of AppContext protocol for new Worklet files

--- packages/expo-modules-core/ios/Worklets/WorkletRuntimeFactory.h
+++ packages/expo-modules-core/ios/Worklets/WorkletRuntimeFactory.h
@@ -1,5 +1,6 @@
 // Copyright 2025-present 650 Industries. All rights reserved.
 
+#import <ExpoModulesCore/EXAppContextProtocol.h>
 #import <ExpoModulesCore/EXRuntime.h>
 #import <ExpoModulesJSI/EXJavaScriptValue.h>
 #import <ExpoModulesCore/EXWorkletRuntime.h>
@@ -8,7 +9,7 @@
 
 @interface WorkletRuntimeFactory : NSObject
 
-+ (nonnull EXWorkletRuntime *)createWorkletRuntime:(nonnull EXAppContext *)appContext fromPointer:(nullable void *)pointer;
++ (nonnull EXWorkletRuntime *)createWorkletRuntime:(nonnull id<EXAppContextProtocol>)appContext fromPointer:(nullable void *)pointer;
 
 + (nullable void *)extractRuntimePointer:(nonnull EXJavaScriptValue *)jsValue runtime:(nonnull EXJavaScriptRuntime *)runtime;
 
