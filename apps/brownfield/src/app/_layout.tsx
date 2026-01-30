8843d97c9ffac7654ac80da29dd398aa64028163
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-01-30 17:05:46 +0100

    [apps][brownfield] reset 'brownfield' app

--- apps/brownfield/src/app/_layout.tsx
+++ apps/brownfield/src/app/_layout.tsx
@@ -1,16 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Stack } from 'expo-router';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
