e71de7dbe52277a8204f07e856fe56f4509be4b8
nishan (o^▽^o) <nishanbende@gmail.com>
2026-02-02 14:21:08 +0530

    ncl lint fix

--- apps/native-component-list/src/screens/UI/ExtendingExpoUIScreen.tsx
+++ apps/native-component-list/src/screens/UI/ExtendingExpoUIScreen.tsx
@@ -1,21 +1,21 @@
 import { Host, Text, VStack } from '@expo/ui/swift-ui';
 import { padding, cornerRadius, background } from '@expo/ui/swift-ui/modifiers';
-import { MyCustomView, customBorder } from 'test-expo-ui';
 import { requireNativeModule } from 'expo';
+import { MyCustomView, customBorder } from 'test-expo-ui';
 
-let hasNativeMyCustomView = false;
+let hasTextExpoUiModule = false;
 try {
   requireNativeModule('TestExpoUi');
-  hasNativeMyCustomView = true;
+  hasTextExpoUiModule = true;
 } catch {
-  hasNativeMyCustomView = false;
+  hasTextExpoUiModule = false;
 }
 
 export default function ExtendingExpoUIScreen() {
   return (
     <Host style={{ flex: 1 }}>
       <VStack modifiers={[padding({ all: 16 })]}>
-        {hasNativeMyCustomView ? (
+        {hasTextExpoUiModule ? (
           <MyCustomView
             title="Hello from MyCustomView"
             modifiers={[
