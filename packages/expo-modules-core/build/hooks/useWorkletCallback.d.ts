658b855b0e1ef2cf82bd277e734745d40c9e6171
nishan (o^▽^o) <nishanbende@gmail.com>
2026-02-03 12:55:01 +0530

    rebuild

--- packages/expo-modules-core/build/hooks/useWorkletCallback.d.ts
+++
@@ -1,7 +0,0 @@
-import { DependencyList } from 'react';
-/**
- * Wraps a worklet function in a `WorkletCallback` SharedObject and returns its numeric ID,
- * which can be passed as a prop through React Native's serialization.
- */
-export declare function useWorkletCallback(workletFn: ((...args: any[]) => any) | undefined, deps: DependencyList): number | undefined;
-//# sourceMappingURL=useWorkletCallback.d.ts.map
