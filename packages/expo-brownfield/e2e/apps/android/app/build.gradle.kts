d1ec6c2293ab271a8a397804f1c65a3d9246cff8
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-02-04 16:04:44 +0100

    [test][brownfield] add test flow for the communication api

--- packages/expo-brownfield/e2e/apps/android/app/build.gradle.kts
+++ packages/expo-brownfield/e2e/apps/android/app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.brownfield)
+    implementation(libs.expo.brownfield)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
