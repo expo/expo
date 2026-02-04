569c8198b3b44bc442e3b069cf7d5044c8dbf2c3
Gary Chiu <gary.gc.chiu@gmail.com>
2026-02-04 00:47:58 -0500

    fix: widget preview content

--- packages/expo-widgets/ios/Widgets/TimelineProvider.swift
+++ packages/expo-widgets/ios/Widgets/TimelineProvider.swift
@@ -1,31 +1,45 @@
 import WidgetKit
 
 public struct WidgetsTimelineProvider: TimelineProvider {
-  public func placeholder(in context: Context) -> WidgetsTimelineEntry {
-    WidgetsTimelineEntry(date: Date(), source: name, node: nil)
-  }
-
-  public func getSnapshot(in context: Context, completion: @escaping @Sendable (WidgetsTimelineEntry) -> Void) {
-    completion(WidgetsTimelineEntry(date: Date(), source: name, node: nil))
-  }
-
-  public func getTimeline(in context: Context, completion: @escaping @Sendable (Timeline<WidgetsTimelineEntry>) -> Void) {
-    let groupIdentifier = Bundle.main.object(forInfoDictionaryKey: "ExpoWidgetsAppGroupIdentifier") as? String
-    guard let groupIdentifier else {
-      fatalError("Could not get the app group identifier from Info.plist")
+    public func placeholder(in context: Context) -> WidgetsTimelineEntry {
+        WidgetsTimelineEntry(date: Date(), source: name, node: nil)
     }
 
-    let entries = parseTimeline(identifier: groupIdentifier, name: name, family: context.family)
+    public func getSnapshot(
+        in context: Context, completion: @escaping @Sendable (WidgetsTimelineEntry) -> Void
+    ) {
+        let groupIdentifier =
+            Bundle.main.object(forInfoDictionaryKey: "ExpoWidgetsAppGroupIdentifier") as? String
+        guard let groupIdentifier else {
+            completion(WidgetsTimelineEntry(date: Date(), source: name, node: nil))
+            return
+        }
 
-    let timeline = Timeline<WidgetsTimelineEntry>(entries: entries, policy: .atEnd)
-    completion(timeline)
-  }
+        let entries = parseTimeline(identifier: groupIdentifier, name: name, family: context.family)
+        completion(entries.first ?? WidgetsTimelineEntry(date: Date(), source: name, node: nil))
+    }
 
-  public typealias Entry = WidgetsTimelineEntry
+    public func getTimeline(
+        in context: Context,
+        completion: @escaping @Sendable (Timeline<WidgetsTimelineEntry>) -> Void
+    ) {
+        let groupIdentifier =
+            Bundle.main.object(forInfoDictionaryKey: "ExpoWidgetsAppGroupIdentifier") as? String
+        guard let groupIdentifier else {
+            fatalError("Could not get the app group identifier from Info.plist")
+        }
 
-  let name: String
+        let entries = parseTimeline(identifier: groupIdentifier, name: name, family: context.family)
 
-  public init(name: String) {
-    self.name = name
-  }
+        let timeline = Timeline<WidgetsTimelineEntry>(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+
+    public typealias Entry = WidgetsTimelineEntry
+
+    let name: String
+
+    public init(name: String) {
+        self.name = name
+    }
 }
