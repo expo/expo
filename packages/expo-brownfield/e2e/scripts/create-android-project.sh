c1424c85fb4dfcb6c0d341aee2074ce3bdb8dfdf
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-02-05 22:10:29 +0100

    [brownfield][test] drop e2e scripts for android

--- packages/expo-brownfield/e2e/scripts/create-android-project.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_TEMPLATE="$DIR/../apps/android"
-
-PROJECT_DIR=$1
-
-mkdir -p $PROJECT_DIR/android
-cp -r $PROJECT_TEMPLATE/* $PROJECT_DIR/android/
-echo " ✅ Native Android project created at $PROJECT_DIR"
