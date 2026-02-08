ec594eb7617d1f2e4234ba1888f3a3d6fa52b0d3
Gary Chiu <gary.gc.chiu@gmail.com>
2026-02-07 19:07:50 -0500

    fix: prettier

--- packages/expo-widgets/plugin/src/withEasConfig.ts
+++ packages/expo-widgets/plugin/src/withEasConfig.ts
@@ -11,13 +11,11 @@ const withEasConfig: ConfigPlugin<EasConfigProps> = (
   { targetName, bundleIdentifier, groupIdentifier }
 ) => {
   let configIndex: null | number = null;
-  config.extra?.eas?.build?.experimental?.ios?.appExtensions?.forEach(
-    (ext: any, index: number) => {
-      if (ext.targetName === targetName) {
-        configIndex = index;
-      }
+  config.extra?.eas?.build?.experimental?.ios?.appExtensions?.forEach((ext: any, index: number) => {
+    if (ext.targetName === targetName) {
+      configIndex = index;
     }
-  );
+  });
 
   if (configIndex === null) {
     config.extra = {
@@ -47,8 +45,7 @@ const withEasConfig: ConfigPlugin<EasConfigProps> = (
     };
   } else if (config.extra) {
     // Ensure the entitlements are up to date for an existing entry
-    const existingEntry =
-      config.extra.eas.build.experimental.ios.appExtensions[configIndex];
+    const existingEntry = config.extra.eas.build.experimental.ios.appExtensions[configIndex];
     const existingGroups =
       (existingEntry.entitlements?.['com.apple.security.application-groups'] as string[]) ?? [];
     if (!existingGroups.includes(groupIdentifier)) {
