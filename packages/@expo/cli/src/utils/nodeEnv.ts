672fc263ad6f3c4781387b15009e6be34ba077e8
Phil Pluckthun <phil@kitten.sh>
2026-02-10 15:55:04 +0000

    Update `loadEnvFiles` in `src/utils/nodeEnv.ts`

--- packages/@expo/cli/src/utils/nodeEnv.ts
+++ packages/@expo/cli/src/utils/nodeEnv.ts
@@ -1,5 +1,7 @@
 import * as env from '@expo/env';
 
+type EnvOutput = Record<string, string | undefined>;
+
 // TODO(@kitten): We assign this here to run server-side code bundled by metro
 // It's not isolated into a worker thread yet
 declare namespace globalThis {
@@ -16,10 +18,49 @@ export function setNodeEnv(mode: 'development' | 'production') {
   globalThis.__DEV__ = process.env.NODE_ENV !== 'production';
 }
 
+interface LoadEnvFilesOptions {
+  force?: boolean;
+  silent?: boolean;
+  mode?: string;
+}
+
 /**
  * Load the dotenv files into the current `process.env` scope.
  * Note, this requires `NODE_ENV` being set through `setNodeEnv`.
  */
-export function loadEnvFiles(projectRoot: string, options?: Parameters<typeof env.load>[1]) {
-  return env.load(projectRoot, options);
+export function loadEnvFiles(projectRoot: string, options?: LoadEnvFilesOptions) {
+  const isEnabled = env.isEnabled();
+  if (isEnabled) {
+    const params = {
+      ...options,
+      force: !!options?.force,
+      silent: !!options?.silent,
+      mode: process.env.NODE_ENV,
+    };
+
+    const envInfo = env.parseProjectEnv(projectRoot, params);
+    const envOutput: EnvOutput = {};
+    const skipped: string[] = [];
+    for (const key in envInfo.env) {
+      if (typeof process.env[key] !== 'undefined') {
+        skipped.push(key);
+      } else {
+        process.env[key] = envInfo.env[key];
+        envOutput[key] = envInfo.env[key] ?? undefined;
+      }
+    }
+
+    if (!params.silent) {
+      env.logLoadedEnv(
+        {
+          ...envInfo,
+          result: 'loaded',
+          loaded: Object.keys(envOutput),
+        },
+        params
+      );
+    }
+  }
+
+  return process.env;
 }
