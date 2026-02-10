8fb7d61b61f7d9e2fb40a9e8e40488ef33c11f58
Phil Pluckthun <phil@kitten.sh>
2026-02-10 14:13:59 +0000

    Add events for `src/start/server/metro/instantiateMetro.ts`
    - `metro:config` for `loadMetroConfigAsync`
    - `metro:instantiate` for `instantiateMetro`

--- packages/@expo/cli/src/events/types.ts
+++ packages/@expo/cli/src/events/types.ts
@@ -1,10 +1,13 @@
 import type { rootEvent } from './index';
 import type { collectEventLoggers } from '../events/builder';
 import type { event as metroTerminalReporterEvent } from '../start/server/metro/MetroTerminalReporter';
+import type { event as instantiateMetroEvent } from '../start/server/metro/instantiateMetro';
 
 /** Collection of all event logger events
  * @privateRemarks
  * When creating a new logger with `events()`, import it here and
  * add it to add its types to this union type.
  */
-export type Events = collectEventLoggers<[typeof rootEvent, typeof metroTerminalReporterEvent]>;
+export type Events = collectEventLoggers<
+  [typeof rootEvent, typeof metroTerminalReporterEvent, typeof instantiateMetroEvent]
+>;
