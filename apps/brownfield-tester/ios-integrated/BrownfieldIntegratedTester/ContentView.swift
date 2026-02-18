f6b39e22a17adf5677dc8c5c0c3ad5331c0458e4
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-02-18 16:18:43 +0100

    [brownfield][apps] tidy up ios test app

--- apps/brownfield-tester/ios-integrated/BrownfieldIntegratedTester/ContentView.swift
+++ apps/brownfield-tester/ios-integrated/BrownfieldIntegratedTester/ContentView.swift
@@ -2,11 +2,7 @@ import SwiftUI
 import expoappbrownfield
 
 struct ContentView: View {
-    @State private var listenerId: String?
-    @State private var alertMessage: String = ""
-    @State private var showAlert: Bool = false
-    @State private var messageTimer: Timer?
-    @State private var messageCounter = 0
+    @StateObject private var brownfieldTester = BrownfieldTester()
     
     var body: some View {
         NavigationStack {
@@ -16,54 +12,13 @@ struct ContentView: View {
                     .font(.largeTitle)
             })
         }
-        .onAppear(perform: onAppear)
-        .onDisappear(perform: onDisappear)
-        .alert("Message from Native", isPresented: $showAlert) {
+        .onAppear { brownfieldTester.start() }
+        .onDisappear { brownfieldTester.stop() }
+        .alert("Message from Native", isPresented: $brownfieldTester.showAlert) {
             Button("OK", role: .cancel) { }
         } message: {
-            Text(alertMessage)
+            Text(brownfieldTester.alertMessage)
         }
-    }
-    
-    private func onAppear() {
-        listenerId = BrownfieldMessaging.addListener { message in
-            let sender = message["sender"] as? String ?? "Unknown"
-            let nested = message["source"] as? [String: Any?] ?? [:]
-            let platform = nested["platform"] as? String ?? "Unknown"
-            
-            DispatchQueue.main.async {
-                self.alertMessage = "\(platform)(\(sender))"
-                self.showAlert = true
-            }
-            
-            messageTimer = Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true) { _ in
-                sendMessage()
-            }
-        }
-    }
-    
-    private func onDisappear() {
-        if let listenerId = listenerId {
-            BrownfieldMessaging.removeListener(id: listenerId)
-        }
-        messageTimer?.invalidate()
-        messageTimer = nil
-    }
-    
-    private func sendMessage() {
-        messageCounter += 1
-        
-        let nativeMessage: [String: Any] = [
-            "source": [
-                "platform": "iOS"
-            ],
-            "counter": messageCounter,
-            "timestamp": Int64(Date().timeIntervalSince1970 * 1000),
-            "array": ["ab", "c", false, 1, 2.45] as [Any]
-        ]
-        
-        BrownfieldMessaging.sendMessage(nativeMessage)
-        print("Sent: \(nativeMessage)")
     }
 }
 
