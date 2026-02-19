34f300613620608f8e86e059b07d33de2215b280
Christian Falch <christian.falch@gmail.com>
2026-02-19 15:59:29 +0100

    fixed build and config for cli extension
    - Removed package.json, no longer needed after fix in @expo/devtools
    - Updated tsconfig.json (removed module/moduleResolution).
    - Rebuilt

--- packages/expo-background-task/cli/build/index.js
+++ packages/expo-background-task/cli/build/index.js
@@ -1,20 +1,25 @@
-import { queryAllInspectorAppsAsync, runCliExtension, sendCliMessageAsync } from '@expo/devtools';
-import chalk from 'chalk';
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const devtools_1 = require("@expo/devtools");
+const chalk_1 = __importDefault(require("chalk"));
 const PLUGIN_NAME = 'expo-background-task-cli-extension';
 const GET_REGISTERED_TASKS = 'getRegisteredBackgroundTasks';
 const TRIGGER_TASKS = 'triggerBackgroundTasks';
-runCliExtension(async ({ command, metroServerOrigin }, console) => {
-    const apps = await queryAllInspectorAppsAsync(metroServerOrigin);
+(0, devtools_1.runCliExtension)(async ({ command, metroServerOrigin }, console) => {
+    const apps = await (0, devtools_1.queryAllInspectorAppsAsync)(metroServerOrigin);
     if (apps.length === 0) {
         console.error(`No connected apps found at ${metroServerOrigin}`);
         return;
     }
     if (command === 'list') {
         try {
-            const response = await sendCliMessageAsync(GET_REGISTERED_TASKS, PLUGIN_NAME, apps);
+            const response = await (0, devtools_1.sendCliMessageAsync)(GET_REGISTERED_TASKS, PLUGIN_NAME, apps);
             Object.keys(response).forEach((appId) => {
                 const app = apps.find((a) => a.id === appId);
-                console.info(`${chalk.blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
+                console.info(`${chalk_1.default.blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
             });
         }
         catch (error) {
@@ -24,10 +29,10 @@ runCliExtension(async ({ command, metroServerOrigin }, console) => {
     else if (command === 'trigger-test') {
         // Trigger background tasks
         try {
-            const response = await sendCliMessageAsync(TRIGGER_TASKS, PLUGIN_NAME, apps);
+            const response = await (0, devtools_1.sendCliMessageAsync)(TRIGGER_TASKS, PLUGIN_NAME, apps);
             Object.keys(response).forEach((appId) => {
                 const app = apps.find((a) => a.id === appId);
-                console.info(`${chalk.blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
+                console.info(`${chalk_1.default.blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
             });
         }
         catch (error) {
