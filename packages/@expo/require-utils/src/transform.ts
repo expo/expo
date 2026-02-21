c091b2586ac50dca8f362d1516993086485d48ea
Phil Pluckthun <phil@kitten.sh>
2026-02-21 12:21:35 +0000

    Add note on why we use `importInterop: 'node'`

--- packages/@expo/require-utils/src/transform.ts
+++ packages/@expo/require-utils/src/transform.ts
@@ -8,6 +8,12 @@ export function toCommonJS(filename: string, code: string) {
       [
         require('@babel/plugin-transform-modules-commonjs'),
         {
+          // NOTE(@kitten): We used to use sucrase to transform, which is why
+          // we're doing this CJS-to-ESM transform in the first place. Our
+          // previous transformation isn't 100% compatible with the standard
+          // Node ESM loading. In Babel, this is the "node" flag (although
+          // node behaviour is explicitly different from this). This skips
+          // the `__esModule -> default` wrapper
           importInterop: 'node',
           loose: true,
         },
