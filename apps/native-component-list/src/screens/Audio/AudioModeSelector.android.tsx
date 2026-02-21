5fc98325d9fd793b3028c07061b0b0323acd5380
Blazej Kustra <blazej.kustra@swmansion.com>
2026-02-21 23:47:52 +0100

    Enable playsInSilentMode by default

--- apps/native-component-list/src/screens/Audio/AudioModeSelector.android.tsx
+++ apps/native-component-list/src/screens/Audio/AudioModeSelector.android.tsx
@@ -12,14 +12,14 @@ export default function AudioModeSelector() {
   }>({
     next: {
       interruptionMode: 'doNotMix',
-      playsInSilentMode: false,
+      playsInSilentMode: true,
       shouldPlayInBackground: false,
       shouldRouteThroughEarpiece: false,
       allowsBackgroundRecording: false,
     },
     current: {
       interruptionMode: 'doNotMix',
-      playsInSilentMode: false,
+      playsInSilentMode: true,
       shouldPlayInBackground: false,
       shouldRouteThroughEarpiece: false,
       allowsBackgroundRecording: false,
