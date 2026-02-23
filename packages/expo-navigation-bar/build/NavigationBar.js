f0039369d134d3abc9a30d8e2ce7ed8f2a180a3c
Mathieu Acthernoene <zoontek@gmail.com>
2026-02-23 13:55:09 +0100

    update setStyle documentation

--- packages/expo-navigation-bar/build/NavigationBar.js
+++ packages/expo-navigation-bar/build/NavigationBar.js
@@ -208,8 +208,7 @@ export async function getBehaviorAsync() {
 /**
  * Sets the style of the navigation bar.
  * > This will have an effect when the following conditions are met:
- * > - Edge-to-edge is enabled
- * > - The `enforceNavigationBarContrast` option of the `react-native-edge-to-edge` plugin is set to `false`.
+ * > - The `enforceContrast` option of the `expo-navigation-bar` plugin is set to `false`.
  * > - The device is using the three-button navigation bar.
  *
  * > Due to a bug in the Android 15 emulator this function may have no effect. Try a physical device or an emulator with a different version of Android.
