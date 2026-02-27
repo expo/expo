d9d22101c88e7a922b161915b2f06f6f38013d55
Alan Hughes <alan@expo.io>
2026-02-27 19:50:12 +0000

    Don't promote expo-template-default for now

--- tools/src/promote-packages/tasks/prepareParcels.ts
+++ tools/src/promote-packages/tasks/prepareParcels.ts
@@ -5,6 +5,11 @@ import { createParcelAsync } from '../helpers';
 import { CommandOptions, Parcel, TaskArgs } from '../types';
 
 /**
+ * Packages that should never be promoted to `latest` by this command.
+ */
+const ALWAYS_EXCLUDED_PACKAGES = ['expo-template-default'];
+
+/**
  * Gets a list of public packages in the monorepo, downloads `npm view` result of them,
  * creates their Changelog instance and fills in given parcels array (it's empty at the beginning).
  */
@@ -17,10 +22,11 @@ export const prepareParcels = new Task<TaskArgs>(
 
     const { exclude, packageNames } = options;
     const allPackages = await getListOfPackagesAsync();
+    const allExcludes = [...exclude, ...ALWAYS_EXCLUDED_PACKAGES];
     const filteredPackages = allPackages.filter((pkg) => {
       const isPrivate = pkg.packageJson.private === true;
       const isIncluded = packageNames.length === 0 || packageNames.includes(pkg.packageName);
-      const isExcluded = exclude.includes(pkg.packageName);
+      const isExcluded = allExcludes.includes(pkg.packageName);
 
       return !isPrivate && isIncluded && !isExcluded;
     });
