1e5d2fa84a5205b6950f5ee003155e5d8e98bf03
Vojtech Novak <vonovak@gmail.com>
2026-02-27 14:46:02 +0100

    [docs] update expo-symbols docs

--- packages/expo-symbols/build/materialImageSource.d.ts
+++ packages/expo-symbols/build/materialImageSource.d.ts
@@ -1,7 +1,7 @@
 import type { ImageSourcePropType } from 'react-native';
 import { type AndroidSymbol } from './android';
 /**
- * Renders a Material Symbol to an image source.
+ * Renders a Material Symbol to an image source. Useful for APIs that require an `ImageSourcePropType` instead of a component, such as tab bar icons.
  *
  * @platform android
  */
