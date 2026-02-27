eb13d0fd4decc4315676f1396e2b50bd6ff1d32f
nishan (o^▽^o) <nishanbende@gmail.com>
2026-02-27 11:51:39 +0530

    add rnhostview

--- packages/expo-ui/android/src/main/java/expo/modules/ui/RNHostView.kt
+++ packages/expo-ui/android/src/main/java/expo/modules/ui/RNHostView.kt
@@ -4,84 +4,99 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.viewinterop.AndroidView
-import com.facebook.react.ReactRootView
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ComposableScope
 import expo.modules.kotlin.views.ComposeProps
 import expo.modules.kotlin.views.ExpoComposeView
-import expo.modules.kotlin.views.RNHostViewInterface
-import expo.modules.kotlin.views.ShadowNodeProxy
-import java.lang.ref.WeakReference
 
 internal data class RNHostProps(
-  val matchContents: MutableState<Boolean?> = mutableStateOf(null),
-  val verticalScrollEnabled: MutableState<Boolean?> = mutableStateOf(null),
-  val modifiers: MutableState<ModifierList> = mutableStateOf(emptyList())
+  val matchContents: MutableState<Boolean?> = mutableStateOf(null)
 ) : ComposeProps
 
 @SuppressLint("ViewConstructor")
 internal class RNHostView(context: Context, appContext: AppContext) :
-  ExpoComposeView<RNHostProps>(context, appContext), RNHostViewInterface {
+  ExpoComposeView<RNHostProps>(context, appContext) {
   override val props = RNHostProps()
-  override var matchContents: Boolean = false
-    get() = props.matchContents.value ?: false
 
-  private val container = RNHostContainerView(context, WeakReference(shadowNodeProxy))
+  private var childView: View? = null
+
+  override fun addView(child: View, index: Int, params: ViewGroup.LayoutParams) {
+    childView = child
+    super.addView(child, index, params)
+  }
 
   @Composable
   override fun ComposableScope.Content() {
-    val (verticalScrollEnabled) = props.verticalScrollEnabled
-    val (modifiers) = props.modifiers
+    val matchContents = props.matchContents.value ?: false
+    val density = LocalDensity.current
 
-    AndroidView(
-      factory = {
-        container
-      },
-      modifier = ModifierRegistry.applyModifiers(modifiers, appContext, this@Content, globalEventDispatcher)
-        .then(if (verticalScrollEnabled == true) Modifier.verticalScroll(rememberScrollState()) else Modifier)
-    )
-  }
+    val sizeModifier = Modifier.onSizeChanged { size ->
+      with(density) {
+        val widthDp = size.width.toDp().value.toDouble()
+        val heightDp = size.height.toDp().value.toDouble()
+        if (matchContents) {
+          shadowNodeProxy.setStyleSize(widthDp, heightDp)
+        } else {
+          shadowNodeProxy.setViewSize(widthDp, heightDp)
+        }
+      }
+    }
 
-  override fun addView(child: View, index: Int, params: ViewGroup.LayoutParams) {
-    container.addView(child, index, params)
-  }
+    if (matchContents) {
+      childView?.let { view ->
+        // Seed with the child's current Yoga-set dimensions so requiredSize
+        // is applied from the very first composition
+        val childSize = remember {
+          mutableStateOf(
+            if (view.width > 0 && view.height > 0) IntSize(view.width, view.height)
+            else IntSize.Zero
+          )
+        }
 
-  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    container.measure(widthMeasureSpec, heightMeasureSpec)
-  }
+        DisposableEffect(view) {
+          // Observe the child's Yoga-set dimensions reactively.
+          // When Yoga re-layouts the child, the listener fires, updating Compose state,
+          // which triggers recomposition with the correct requiredSize.
+          val listener = View.OnLayoutChangeListener { _, l, t, r, b, _, _, _, _ ->
+            childSize.value = IntSize(r - l, b - t)
+          }
+          view.addOnLayoutChangeListener(listener)
+          onDispose { view.removeOnLayoutChangeListener(listener) }
+        }
 
-  override fun onLayout(
-    changed: Boolean,
-    left: Int,
-    top: Int,
-    right: Int,
-    bottom: Int
-  ) {
-    super.onLayout(changed, left, top, right, bottom)
-    val offsetX = paddingLeft
-    val offsetY = paddingRight
-    container.layout(offsetX, offsetY, offsetX + width, offsetY + height)
-  }
-}
+        val yogaSizeModifier = with(density) {
+          if (childSize.value.width > 0 && childSize.value.height > 0) {
+            Modifier.requiredSize(
+              childSize.value.width.toDp(),
+              childSize.value.height.toDp()
+            )
+          } else {
+            Modifier
+          }
+        }
 
-internal class RNHostContainerView(context: Context, private val shadowNodeProxy: WeakReference<ShadowNodeProxy>) : ReactRootView(context) {
-  var matchContents = false
-
-  override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-    super.onLayout(changed, left, top, right, bottom)
-    if (matchContents && childCount > 1) {
-      val subview = getChildAt(0)
-      shadowNodeProxy.get()?.setViewSize(subview.width.toDouble(), subview.height.toDouble())
+        AndroidView(
+          factory = { view },
+          modifier = yogaSizeModifier.then(sizeModifier)
+        )
+      }
     } else {
-      shadowNodeProxy.get()?.setViewSize(width.toDouble(), height.toDouble())
+      Box(modifier = Modifier.fillMaxSize().then(sizeModifier)) {
+        Children(this@Content)
+      }
     }
   }
 }
