7d3ca2f339f5976e5b25db76b373819a4b3999f4
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-03-02 16:23:27 +0100

    [github][brownfield] update e2e wotkflow to include dev menu tests

--- packages/expo-brownfield/e2e/scripts/run-e2e-ios.sh
+++ packages/expo-brownfield/e2e/scripts/run-e2e-ios.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+APP_ID=${APP_ID:-dev.expo.BrownfieldIntegratedTester}
+CONFIGURATION=${CONFIGURATION:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Setup simulator
@@ -10,4 +12,7 @@ start_simulator
 source $DIR/install-app-ios.sh $DEVICE_ID
 
 # Run the tests
-maestro test $DIR/../maestro/__tests__/ios
+maestro test -e APP_ID=$APP_ID $DIR/../maestro/__tests__/ios
+if [ "$CONFIGURATION" = "Debug" ]; then
+  maestro test -e APP_ID=$APP_ID $DIR/../maestro/__tests__/common
+fi
