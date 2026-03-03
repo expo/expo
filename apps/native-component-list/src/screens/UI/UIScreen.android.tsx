d24679ed4c1d9169a4f29532ca4b620c32daf096
Jakub Tkacz <jtkacz@expo.io>
2026-03-03 20:13:22 +0100

    [expo-ui][android] add AnimatedVisibility transition API

--- apps/native-component-list/src/screens/UI/UIScreen.android.tsx
+++ apps/native-component-list/src/screens/UI/UIScreen.android.tsx
@@ -3,6 +3,14 @@ import ComponentListScreen, { componentScreensToListElements } from '../Componen
 
 export const UIScreens = [
   {
+    name: 'AnimatedVisibility component',
+    route: 'ui/animated-visibility',
+    options: {},
+    getComponent() {
+      return optionalRequire(() => require('./AnimatedVisibilityScreen'));
+    },
+  },
+  {
     name: 'AlertDialog component',
     route: 'ui/alert-dialog',
     options: {},
