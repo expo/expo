f7e1446643ecb852f89491f71a331dec4be7022a
Łukasz Kosmaty <kosmatylukasz@gmail.com>
2026-03-04 09:43:08 +0100

    [core][Android] Add state props (#42822)

--- packages/expo-modules-core/common/cpp/fabric/ExpoViewShadowNode.h
+++ packages/expo-modules-core/common/cpp/fabric/ExpoViewShadowNode.h
@@ -14,27 +14,62 @@ namespace expo {
 
 extern const char ExpoViewComponentName[];
 
-class ExpoViewShadowNode final : public facebook::react::ConcreteViewShadowNode<
-                                     ExpoViewComponentName, ExpoViewProps,
-                                     ExpoViewEventEmitter, ExpoViewState> {
+template<typename ViewProps = ExpoViewProps, typename ViewState = ExpoViewState>
+class ExpoViewShadowNode : public facebook::react::ConcreteViewShadowNode<
+  ExpoViewComponentName,
+  ViewProps,
+  ExpoViewEventEmitter,
+  ViewState
+> {
 public:
+  typedef facebook::react::ConcreteViewShadowNode<
+    ExpoViewComponentName,
+    ViewProps,
+    ExpoViewEventEmitter,
+    ViewState
+  > ConcreteViewShadowNode;
+
   using ConcreteViewShadowNode::ConcreteViewShadowNode;
 
-  ExpoViewShadowNode(const facebook::react::ShadowNodeFragment &fragment,
-                     const facebook::react::ShadowNodeFamily::Shared &family,
-                     facebook::react::ShadowNodeTraits traits);
+  ExpoViewShadowNode(
+    const facebook::react::ShadowNodeFragment &fragment,
+    const facebook::react::ShadowNodeFamily::Shared &family,
+    facebook::react::ShadowNodeTraits traits
+  ) : ConcreteViewShadowNode(fragment, family, traits) {
+    initialize();
+  }
 
-  ExpoViewShadowNode(const facebook::react::ShadowNode &sourceShadowNode,
-                     const facebook::react::ShadowNodeFragment &fragment);
+  ExpoViewShadowNode(
+    const facebook::react::ShadowNode &sourceShadowNode,
+    const facebook::react::ShadowNodeFragment &fragment
+  ) : ConcreteViewShadowNode(sourceShadowNode, fragment) {
+    initialize();
+  }
 
-public:
   static facebook::react::ShadowNodeTraits BaseTraits() {
     auto traits = ConcreteViewShadowNode::BaseTraits();
     return traits;
   }
 
 private:
-  void initialize() noexcept;
+  void initialize() noexcept {
+    auto &viewProps = static_cast<const ExpoViewProps &>(*this->props_);
+
+    if (viewProps.collapsableChildren) {
+      this->traits_.set(react::ShadowNodeTraits::Trait::ChildrenFormStackingContext);
+    } else {
+      this->traits_.unset(react::ShadowNodeTraits::Trait::ChildrenFormStackingContext);
+    }
+
+    if (YGNodeStyleGetDisplay(&this->yogaNode_) == YGDisplayContents) {
+      auto it = viewProps.propsMap.find("disableForceFlatten");
+      bool disableForceFlatten = (it != viewProps.propsMap.end()) && it->second.getBool();
+
+      if (disableForceFlatten) {
+        this->traits_.unset(react::ShadowNodeTraits::Trait::ForceFlattenView);
+      }
+    }
+  }
 };
 
 } // namespace expo
