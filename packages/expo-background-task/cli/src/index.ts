8f86fa4e9e8723f53bde20cd934af5d13db62b12
Christian Falch <christian.falch@gmail.com>
2026-03-04 18:47:52 +0100

    [expo-background-task] refactored with new apps argument
    After refactoring with new app argument in the CLI lib, we update the implementation here.

--- packages/expo-background-task/cli/src/index.ts
+++ packages/expo-background-task/cli/src/index.ts
@@ -9,31 +9,19 @@ const blue = (s: string) => `\x1b[34m${s}\x1b[0m`;
 runCliExtension<{
   list: Record<string, never>;
   'trigger-test': Record<string, never>;
-}>(async ({ command, metroServerOrigin }, console) => {
-  const apps = await queryAllInspectorAppsAsync(metroServerOrigin);
-  if (apps.length === 0) {
-    console.error(`No connected apps found at ${metroServerOrigin}`);
-    return;
-  }
-
+}>(async ({ command, metroServerOrigin, app }, console) => {
   if (command === 'list') {
     try {
-      const response = await sendCliMessageAsync(GET_REGISTERED_TASKS, PLUGIN_NAME, apps);
-      Object.keys(response).forEach((appId) => {
-        const app = apps.find((a) => a.id === appId);
-        console.info(`${blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
-      });
+      const response = await sendCliMessageAsync(GET_REGISTERED_TASKS, PLUGIN_NAME, app);
+      console.info(`${blue(app.title)}: ${response}`);
     } catch (error) {
       throw new Error('An error occured connecting to the app.', { cause: error });
     }
   } else if (command === 'trigger-test') {
     // Trigger background tasks
     try {
-      const response = await sendCliMessageAsync(TRIGGER_TASKS, PLUGIN_NAME, apps);
-      Object.keys(response).forEach((appId) => {
-        const app = apps.find((a) => a.id === appId);
-        console.info(`${blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
-      });
+      const response = await sendCliMessageAsync(TRIGGER_TASKS, PLUGIN_NAME, app);
+      console.info(`${blue(app.title)}: ${response}`);
     } catch (error) {
       throw new Error('An error occured connecting to the app.', { cause: error });
     }
