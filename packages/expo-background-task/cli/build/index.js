5eb6a81b7a69e4d418e8284cbf257c9448217a55
Christian Falch <christian.falch@gmail.com>
2026-03-04 18:47:52 +0100

    [expo-background-task] refactored with new apps argument
    After refactoring with new app argument in the CLI lib, we update the implementation here.

--- packages/expo-background-task/cli/build/index.js
+++ packages/expo-background-task/cli/build/index.js
@@ -1,21 +1,13 @@
-import { queryAllInspectorAppsAsync, runCliExtension, sendCliMessageAsync } from '@expo/devtools';
+import { runCliExtension, sendCliMessageAsync } from '@expo/devtools';
 const PLUGIN_NAME = 'expo-background-task-cli-extension';
 const GET_REGISTERED_TASKS = 'getRegisteredBackgroundTasks';
 const TRIGGER_TASKS = 'triggerBackgroundTasks';
 const blue = (s) => `\x1b[34m${s}\x1b[0m`;
-runCliExtension(async ({ command, metroServerOrigin }, console) => {
-    const apps = await queryAllInspectorAppsAsync(metroServerOrigin);
-    if (apps.length === 0) {
-        console.error(`No connected apps found at ${metroServerOrigin}`);
-        return;
-    }
+runCliExtension(async ({ command, metroServerOrigin, app }, console) => {
     if (command === 'list') {
         try {
-            const response = await sendCliMessageAsync(GET_REGISTERED_TASKS, PLUGIN_NAME, apps);
-            Object.keys(response).forEach((appId) => {
-                const app = apps.find((a) => a.id === appId);
-                console.info(`${blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
-            });
+            const response = await sendCliMessageAsync(GET_REGISTERED_TASKS, PLUGIN_NAME, app);
+            console.info(`${blue(app.title)}: ${response}`);
         }
         catch (error) {
             throw new Error('An error occured connecting to the app.', { cause: error });
@@ -24,11 +16,8 @@ runCliExtension(async ({ command, metroServerOrigin }, console) => {
     else if (command === 'trigger-test') {
         // Trigger background tasks
         try {
-            const response = await sendCliMessageAsync(TRIGGER_TASKS, PLUGIN_NAME, apps);
-            Object.keys(response).forEach((appId) => {
-                const app = apps.find((a) => a.id === appId);
-                console.info(`${blue(app?.title ?? appId)}: ${response[appId] ?? ''}`);
-            });
+            const response = await sendCliMessageAsync(TRIGGER_TASKS, PLUGIN_NAME, app);
+            console.info(`${blue(app.title)}: ${response}`);
         }
         catch (error) {
             throw new Error('An error occured connecting to the app.', { cause: error });
