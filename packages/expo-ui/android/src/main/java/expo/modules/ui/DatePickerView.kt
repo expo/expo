9f2c6bae5e2eb21e78c2fff73bc42a1a36dbd70a
Ian Berry <iberry@raxnet.net>
2026-03-09 15:01:20 -0400

    [ui] [Android] Add elementColors prop to the DateTimePicker component (#43787)
    # Why
    
    Currently the `DateTimePicker` component from expo-ui supports a `color`
    prop that only affects a few of the [underlying
    colors](https://developer.android.com/reference/kotlin/androidx/compose/material3/DatePickerDefaults#colors())
    that can be customized (title/selected day/time). I wanted to also
    customize the background/container color to match my app so I figured it
    would be worth exposing all available colors similar to how other
    components such as `Slider` and `Switch` work.
    
    # How
    
    I added an optional `elementColors` prop that accepts an object
    containing colors to override for the date/time picker. Any values not
    specified will continue to use the platform defaults. Additionally, the
    existing `color` prop will continue to function in the same way -
    overriding the same values as before. The values specified in
    `elementColors` take priority over the `color` prop if there are
    conflicts.
    
    # Test Plan
    
    Specify one or more values to the `elementColors` prop to see the colors
    change in the component:
    
    ```tsx
    <DateTimePicker
        variant="picker"
        showVariantToggle={false}
        displayedComponents="date"
        elementColors={{
            containerColor: '#cccccc',
            dividerColor: '#5856D6',
            headlineContentColor: '#ff0000',
            navigationContentColor: '#0A84FF',
            selectedDayContainerColor: '#FF9500',
            titleContentColor: '#0000ff',
        }}
        initialDate={value.toISOString()}
        onDateSelected={(date) => onChange(date)}
    />
    ```
    
    Screenshot:
    
    <img width="359" height="766" alt="image"
    src="https://github.com/user-attachments/assets/99d2a8fd-6cbe-4246-abd4-e8bb858766df"
    />
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo-ui/android/src/main/java/expo/modules/ui/DatePickerView.kt
+++ packages/expo-ui/android/src/main/java/expo/modules/ui/DatePickerView.kt
@@ -47,6 +47,49 @@ enum class Variant(val value: String) : Enumerable {
   }
 }
 
+class DateTimePickerColorOverrides : Record {
+  // DatePicker colors
+  @Field val containerColor: AndroidColor? = null
+  @Field val titleContentColor: AndroidColor? = null
+  @Field val headlineContentColor: AndroidColor? = null
+  @Field val weekdayContentColor: AndroidColor? = null
+  @Field val subheadContentColor: AndroidColor? = null
+  @Field val navigationContentColor: AndroidColor? = null
+  @Field val yearContentColor: AndroidColor? = null
+  @Field val disabledYearContentColor: AndroidColor? = null
+  @Field val currentYearContentColor: AndroidColor? = null
+  @Field val selectedYearContentColor: AndroidColor? = null
+  @Field val disabledSelectedYearContentColor: AndroidColor? = null
+  @Field val selectedYearContainerColor: AndroidColor? = null
+  @Field val disabledSelectedYearContainerColor: AndroidColor? = null
+  @Field val dayContentColor: AndroidColor? = null
+  @Field val disabledDayContentColor: AndroidColor? = null
+  @Field val selectedDayContentColor: AndroidColor? = null
+  @Field val disabledSelectedDayContentColor: AndroidColor? = null
+  @Field val selectedDayContainerColor: AndroidColor? = null
+  @Field val disabledSelectedDayContainerColor: AndroidColor? = null
+  @Field val todayContentColor: AndroidColor? = null
+  @Field val todayDateBorderColor: AndroidColor? = null
+  @Field val dayInSelectionRangeContentColor: AndroidColor? = null
+  @Field val dayInSelectionRangeContainerColor: AndroidColor? = null
+  @Field val dividerColor: AndroidColor? = null
+
+  // TimePicker colors
+  @Field val clockDialColor: AndroidColor? = null
+  @Field val clockDialSelectedContentColor: AndroidColor? = null
+  @Field val clockDialUnselectedContentColor: AndroidColor? = null
+  @Field val selectorColor: AndroidColor? = null
+  @Field val periodSelectorBorderColor: AndroidColor? = null
+  @Field val periodSelectorSelectedContainerColor: AndroidColor? = null
+  @Field val periodSelectorUnselectedContainerColor: AndroidColor? = null
+  @Field val periodSelectorSelectedContentColor: AndroidColor? = null
+  @Field val periodSelectorUnselectedContentColor: AndroidColor? = null
+  @Field val timeSelectorSelectedContainerColor: AndroidColor? = null
+  @Field val timeSelectorUnselectedContainerColor: AndroidColor? = null
+  @Field val timeSelectorSelectedContentColor: AndroidColor? = null
+  @Field val timeSelectorUnselectedContentColor: AndroidColor? = null
+}
+
 data class DateTimePickerProps(
   val title: String = "",
   val initialDate: Long? = null,
@@ -55,6 +98,7 @@ data class DateTimePickerProps(
   val showVariantToggle: Boolean = true,
   val is24Hour: Boolean = true,
   val color: AndroidColor? = null,
+  val elementColors: DateTimePickerColorOverrides = DateTimePickerColorOverrides(),
   val modifiers: ModifierList = emptyList()
 ) : ComposeProps
 
@@ -94,15 +138,39 @@ fun ExpoDatePicker(modifier: Modifier = Modifier, props: DateTimePickerProps, on
     onDateSelected(DatePickerResult(date = state.selectedDateMillis))
   }
 
+  val elementColors = props.elementColors
+  val colorProp = props.color.composeOrNull
+  val defaults = DatePickerDefaults.colors()
+
   DatePicker(
     modifier = modifier,
     state = state,
     showModeToggle = props.showVariantToggle,
     colors = DatePickerDefaults.colors().copy(
-      titleContentColor = colorToComposeColor(props.color),
-      selectedDayContainerColor = colorToComposeColor(props.color),
-      todayDateBorderColor = colorToComposeColor(props.color),
-      headlineContentColor = colorToComposeColor(props.color)
+      containerColor = elementColors.containerColor.composeOrNull ?: defaults.containerColor,
+      titleContentColor = elementColors.titleContentColor.composeOrNull ?: colorProp ?: defaults.titleContentColor,
+      headlineContentColor = elementColors.headlineContentColor.composeOrNull ?: colorProp ?: defaults.headlineContentColor,
+      weekdayContentColor = elementColors.weekdayContentColor.composeOrNull ?: defaults.weekdayContentColor,
+      subheadContentColor = elementColors.subheadContentColor.composeOrNull ?: defaults.subheadContentColor,
+      navigationContentColor = elementColors.navigationContentColor.composeOrNull ?: defaults.navigationContentColor,
+      yearContentColor = elementColors.yearContentColor.composeOrNull ?: defaults.yearContentColor,
+      disabledYearContentColor = elementColors.disabledYearContentColor.composeOrNull ?: defaults.disabledYearContentColor,
+      currentYearContentColor = elementColors.currentYearContentColor.composeOrNull ?: defaults.currentYearContentColor,
+      selectedYearContentColor = elementColors.selectedYearContentColor.composeOrNull ?: defaults.selectedYearContentColor,
+      disabledSelectedYearContentColor = elementColors.disabledSelectedYearContentColor.composeOrNull ?: defaults.disabledSelectedYearContentColor,
+      selectedYearContainerColor = elementColors.selectedYearContainerColor.composeOrNull ?: defaults.selectedYearContainerColor,
+      disabledSelectedYearContainerColor = elementColors.disabledSelectedYearContainerColor.composeOrNull ?: defaults.disabledSelectedYearContainerColor,
+      dayContentColor = elementColors.dayContentColor.composeOrNull ?: defaults.dayContentColor,
+      disabledDayContentColor = elementColors.disabledDayContentColor.composeOrNull ?: defaults.disabledDayContentColor,
+      selectedDayContentColor = elementColors.selectedDayContentColor.composeOrNull ?: defaults.selectedDayContentColor,
+      disabledSelectedDayContentColor = elementColors.disabledSelectedDayContentColor.composeOrNull ?: defaults.disabledSelectedDayContentColor,
+      selectedDayContainerColor = elementColors.selectedDayContainerColor.composeOrNull ?: colorProp ?: defaults.selectedDayContainerColor,
+      disabledSelectedDayContainerColor = elementColors.disabledSelectedDayContainerColor.composeOrNull ?: defaults.disabledSelectedDayContainerColor,
+      todayContentColor = elementColors.todayContentColor.composeOrNull ?: defaults.todayContentColor,
+      todayDateBorderColor = elementColors.todayDateBorderColor.composeOrNull ?: colorProp ?: defaults.todayDateBorderColor,
+      dayInSelectionRangeContentColor = elementColors.dayInSelectionRangeContentColor.composeOrNull ?: defaults.dayInSelectionRangeContentColor,
+      dayInSelectionRangeContainerColor = elementColors.dayInSelectionRangeContainerColor.composeOrNull ?: defaults.dayInSelectionRangeContainerColor,
+      dividerColor = elementColors.dividerColor.composeOrNull ?: defaults.dividerColor
     )
   )
 }
@@ -137,14 +205,29 @@ fun ExpoTimePicker(modifier: Modifier = Modifier, props: DateTimePickerProps, on
     onDateSelected(DatePickerResult(date = cal.time.time))
   }
 
+  val elementColors = props.elementColors
+  val colorProp = props.color.composeOrNull
+  val defaults = TimePickerDefaults.colors()
+
   TimePicker(
     modifier = modifier,
     state = state,
     layoutType = TimePickerLayoutType.Vertical,
     colors = TimePickerDefaults.colors().copy(
-      selectorColor = colorToComposeColor(props.color),
-      timeSelectorSelectedContainerColor = colorToComposeColor(props.color),
-      clockDialColor = colorToComposeColor(props.color).copy(alpha = 0.3f)
+      containerColor = elementColors.containerColor.composeOrNull ?: defaults.containerColor,
+      clockDialColor = elementColors.clockDialColor.composeOrNull ?: colorProp?.copy(alpha = 0.3f) ?: defaults.clockDialColor,
+      clockDialSelectedContentColor = elementColors.clockDialSelectedContentColor.composeOrNull ?: defaults.clockDialSelectedContentColor,
+      clockDialUnselectedContentColor = elementColors.clockDialUnselectedContentColor.composeOrNull ?: defaults.clockDialUnselectedContentColor,
+      selectorColor = elementColors.selectorColor.composeOrNull ?: colorProp ?: defaults.selectorColor,
+      periodSelectorBorderColor = elementColors.periodSelectorBorderColor.composeOrNull ?: defaults.periodSelectorBorderColor,
+      periodSelectorSelectedContainerColor = elementColors.periodSelectorSelectedContainerColor.composeOrNull ?: defaults.periodSelectorSelectedContainerColor,
+      periodSelectorUnselectedContainerColor = elementColors.periodSelectorUnselectedContainerColor.composeOrNull ?: defaults.periodSelectorUnselectedContainerColor,
+      periodSelectorSelectedContentColor = elementColors.periodSelectorSelectedContentColor.composeOrNull ?: defaults.periodSelectorSelectedContentColor,
+      periodSelectorUnselectedContentColor = elementColors.periodSelectorUnselectedContentColor.composeOrNull ?: defaults.periodSelectorUnselectedContentColor,
+      timeSelectorSelectedContainerColor = elementColors.timeSelectorSelectedContainerColor.composeOrNull ?: colorProp ?: defaults.timeSelectorSelectedContainerColor,
+      timeSelectorUnselectedContainerColor = elementColors.timeSelectorUnselectedContainerColor.composeOrNull ?: defaults.timeSelectorUnselectedContainerColor,
+      timeSelectorSelectedContentColor = elementColors.timeSelectorSelectedContentColor.composeOrNull ?: defaults.timeSelectorSelectedContentColor,
+      timeSelectorUnselectedContentColor = elementColors.timeSelectorUnselectedContentColor.composeOrNull ?: defaults.timeSelectorUnselectedContentColor
     )
   )
 }
