3ae4a316ade7193fe052dab38ffda8c36f97818e
Wojciech Dróżdż <behenate@gmail.com>
2026-03-12 12:41:51 +0100

    Fix prettier template, update view template to fix lint error

--- packages/expo-module-template/src/{%- project.viewName %}.tsx
+++ packages/expo-module-template/src/{%- project.viewName %}.tsx
@@ -3,8 +3,7 @@ import * as React from 'react';
 
 import { <%- project.viewName %>Props } from './<%- project.name %>.types';
 
-const NativeView: React.ComponentType<<%- project.viewName %>Props> =
-  requireNativeView('<%- project.name %>');
+const NativeView: React.ComponentType<<%- project.viewName %>Props> = requireNativeView('<%- project.name %>');
 
 export default function <%- project.viewName %>(props: <%- project.viewName %>Props) {
   return <NativeView {...props} />;
