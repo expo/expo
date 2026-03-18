79a2c3668d0ca9eb976571e34d63395e3f9eac5f
nishan (o^▽^o) <nishanbende@gmail.com>
2026-03-18 23:32:41 +0530

    fix icon import

--- apps/native-component-list/src/screens/UI/AlertDialogScreen.android.tsx
+++ apps/native-component-list/src/screens/UI/AlertDialogScreen.android.tsx
@@ -12,7 +12,7 @@ import {
 import { fillMaxWidth, padding } from '@expo/ui/jetpack-compose/modifiers';
 import * as React from 'react';
 
-import infoIcon from '../../../assets/icons/api/Camera.png';
+const infoIcon = require('../../../assets/icons/api/Camera.png');
 
 const longText =
   'We are in the alert dialog, this is a very long text that should be wrapped in the alert dialog. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
