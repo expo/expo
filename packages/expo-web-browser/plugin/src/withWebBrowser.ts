27c254751e9047cb02280b66a62988808bd35a5e
Łukasz Kosmaty <kosmatylukasz@gmail.com>
2026-03-19 12:57:06 +0100

    [browser][Android] Bring back the `experimentalLauncherActivity` config plugin option (#44073)

--- packages/expo-web-browser/plugin/src/withWebBrowser.ts
+++ packages/expo-web-browser/plugin/src/withWebBrowser.ts
@@ -1,20 +1,19 @@
 import { ConfigPlugin, createRunOncePlugin } from 'expo/config-plugins';
 
-type PluginConfig = {
-  experimentalLauncherActivity?: boolean;
-};
+import { withWebBrowserAndroid, PluginConfig } from './withWebBrowserAndroid';
 
 const pkg = require('expo-web-browser/package.json');
 
 const withWebBrowser: ConfigPlugin<PluginConfig | null> = (config, props) => {
-  if (props?.experimentalLauncherActivity) {
-    console.warn(
-      'The `experimentalLauncherActivity` option has been removed. To achieve similar behaviour, set both `createTask` and `useProxyActivity` to true when using `openBrowserAsync` or `openAuthSessionAsync`.'
-    );
+  if (!props) {
     return config;
   }
 
-  return config;
+  if (!props.experimentalLauncherActivity) {
+    return config;
+  }
+
+  return withWebBrowserAndroid(config);
 };
 
 export default createRunOncePlugin(withWebBrowser, pkg.name, pkg.version);
