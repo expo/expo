d9252da3f458c877cd7d28651a94fb5ed83f2d8f
Phil Pluckthun <phil@kitten.sh>
2026-03-20 10:09:52 +0000

    fix(cli/e2e): Update snapshots

--- packages/@expo/cli/e2e/playwright/dev/03-server-actions-only.test.ts
+++ packages/@expo/cli/e2e/playwright/dev/03-server-actions-only.test.ts
@@ -85,7 +85,7 @@ test.describe(inputDir, () => {
     const rscPayload = new TextDecoder().decode(await response.body());
 
     expect(sanitizeRSCPayloadString(rscPayload)).toMatch(
-      '2:I["react-native-web/dist/exports/Text/index.js",["/react-native-web/dist/exports/Text/index.js.bundle?platform=web&dev=true&hot=false&transform.asyncRoutes=true&transform.routerRoot=__e2e__%2F03-server-actions-only%2Fapp&modulesOnly=true&runModule=false&resolver.clientboundary=true&xRSC=1"]'
+      '2:I["node_modules/react-native-web/dist/exports/Text/index.js",["/node_modules/react-native-web/dist/exports/Text/index.js.bundle?platform=web&dev=true&hot=false&transform.asyncRoutes=true&transform.routerRoot=__e2e__%2F03-server-actions-only%2Fapp&modulesOnly=true&runModule=false&resolver.clientboundary=true&xRSC=1"]'
     );
 
     expect(pageErrors.all).toEqual([]);
