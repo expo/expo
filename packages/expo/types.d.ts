970847be8962234a7d4751c83cf120a563f44858
Phil Pluckthun <phil@kitten.sh>
2026-03-23 17:34:56 +0000

    fix(expo): Add/Fix type reference to globals in expo

--- packages/expo/types.d.ts
+++
@@ -1,2 +0,0 @@
-// Re-exports type declarations that may conflict with classes (values) exported from the main index.ts
-import './types/index.d.ts';
--- packages/expo/types/index.d.ts
+++ packages/expo/types/index.d.ts
@@ -1,5 +1,9 @@
 /// <reference types="expo-modules-core/types" />
+import 'expo-modules-core/types';
 
 /// <reference types="./global" />
 /// <reference types="./metro-require" />
 /// <reference types="./react-native-web" />
+import './global';
+import './metro-require';
+import './react-native-web';
