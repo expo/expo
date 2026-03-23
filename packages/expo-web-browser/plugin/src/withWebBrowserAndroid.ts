53efaf46d06283b7fb1e362a338a40100c35010d
Phil Pluckthun <phil@kitten.sh>
2026-03-23 09:49:56 +0000

    expo-web-browser: Fix imports in expo-web-browser (no internal imports)
    The dependencies here are undeclared, but also unnecessary

--- packages/expo-web-browser/plugin/src/withWebBrowserAndroid.ts
+++ packages/expo-web-browser/plugin/src/withWebBrowserAndroid.ts
@@ -1,5 +1,4 @@
-import type { ManifestActivity } from '@expo/config-plugins/build/android/Manifest';
-import type { ExpoConfig } from '@expo/config-types';
+import type { ExpoConfig } from 'expo/config';
 import {
   AndroidConfig,
   ConfigPlugin,
@@ -34,7 +33,7 @@ function addActivityToManifest(config: ExpoConfig) {
     const theme = application?.activity?.[0]?.$['android:theme'];
     application?.activity?.[0]['intent-filter']?.splice(0, 1);
 
-    const launcherActivity: ManifestActivity = {
+    const launcherActivity: AndroidConfig.Manifest.ManifestActivity = {
       $: {
         'android:name': '.BrowserLauncherActivity',
         'android:theme': theme,
