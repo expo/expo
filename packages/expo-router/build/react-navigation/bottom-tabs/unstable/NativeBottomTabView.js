17aa4907f06c9a094ced6961c082cb55b369d6c2
Jakub Tkacz <jtkacz@expo.io>
2026-03-24 15:25:17 -0700

    [expo-router] remove react-navigation/bottom-tabs/unstable

--- packages/expo-router/build/react-navigation/bottom-tabs/unstable/NativeBottomTabView.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NativeBottomTabView = NativeBottomTabView;
-function NativeBottomTabView(_) {
-    throw new Error('Native Bottom Tabs are not supported on this platform.');
-}
-//# sourceMappingURL=NativeBottomTabView.js.map
