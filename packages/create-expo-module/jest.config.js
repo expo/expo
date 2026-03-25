3630d4cdc711aff984730bf2c34cca6d1722d641
Wojciech Dróżdż <behenate@gmail.com>
2026-03-25 12:46:38 +0100

    Don't remove scripts from `create-expo-module`

--- packages/create-expo-module/jest.config.js
+++ packages/create-expo-module/jest.config.js
@@ -1,16 +1,6 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: 'node',
-  testRegex: '/__tests__/.*(test|spec)\\.[jt]sx?$',
-  transform: {
-    '^.+\\.[jt]sx?$': ['babel-jest', { configFile: require.resolve('./babel.config.js') }],
-  },
-  watchPlugins: [
-    require.resolve('jest-watch-typeahead/filename'),
-    require.resolve('jest-watch-typeahead/testname'),
-  ],
-  // See: https://jestjs.io/docs/configuration#prettierpath-string
-  prettierPath: require.resolve('jest-snapshot-prettier'),
+  ...require('expo-module-scripts/jest-preset-cli'),
   displayName: require('./package').name,
   rootDir: __dirname,
   testPathIgnorePatterns: ['<rootDir>/e2e/', '<rootDir>/node_modules/'],
