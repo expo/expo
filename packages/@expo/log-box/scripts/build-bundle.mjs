e4481000ea0504a0e2e178081b6f19c26c924a17
Phil Pluckthun <phil@kitten.sh>
2026-03-25 09:59:50 +0000

    log-box: Update yarn refs and CLAUDE.md

--- packages/@expo/log-box/scripts/build-bundle.mjs
+++ packages/@expo/log-box/scripts/build-bundle.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Build the ExpoLogBox.bundle DOM Component.
-// Use `yarn build` to run this script.
+// Use `pnpm build` to run this script.
 
 import spawn from '@expo/spawn-async';
 import { globSync } from 'glob';
