9ff671c9d1ec498f69d34ef5b85b6769f93fd60f
Mathieu Acthernoene <zoontek@gmail.com>
2026-03-31 11:23:52 +0200

    Fix download-dependencies.sh (#44392)
    # Why
    
    - Currently, `download-dependencies.sh` try to use `yarn` to install
    dependencies, not `pnpm`
    - `yarn` is still needed for `docs`
    - `yarn install` (when running `setup:docs`) should be ran in `./docs`,
    not at root
    
    # How
    
    - I updated the script to either pick `pnpm` or `yarn`
    
    # Test Plan
    
    - Run `setup:native` or `setup:docs` at root
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- scripts/download-dependencies.sh
+++ scripts/download-dependencies.sh
@@ -10,9 +10,6 @@ require() {
 require node
 require npm
 require git-lfs
-# We can install yarn because `npm install` is cross-platform
-require yarn || npm install -g yarn
-
 require direnv
 
 # Set up submodules
@@ -22,5 +19,20 @@ git submodule foreach --recursive git checkout .
 # Pull the large git files
 git lfs pull
 
-# Install the dependencies
-yarn
+if [[ "$1" == "--native" ]]; then
+  # We can install pnpm because `npm install` is cross-platform
+  require pnpm || npm install -g pnpm
+
+  # Install the dependencies
+  pnpm install
+
+elif [[ "$1" == "--docs" ]]; then
+  # We can install yarn because `npm install` is cross-platform
+  require yarn || npm install -g yarn
+
+  # Install the dependencies
+  (cd ./docs && yarn)
+
+elif [[ -n "$1" ]]; then
+  echo "Unknown option: $1"
+fi
