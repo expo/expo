ceb2a0b4b76a6fbfa068208456a6e99b2bf3b116
nishan (o^▽^o) <nishanbende@gmail.com>
2026-04-07 16:44:28 +0530

    lint fix

--- packages/expo-ui/src/swift-ui/State/index.fx.ts
+++ packages/expo-ui/src/swift-ui/State/index.fx.ts
@@ -1,6 +1,7 @@
 // Side effect file to load shared object support in worklets
 
 import { installOnUIRuntime } from 'expo';
+
 import { registerSharedObjectSerializer } from './index';
 
 installOnUIRuntime();
