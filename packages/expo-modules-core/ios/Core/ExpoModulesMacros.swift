53a2a4f37b71d4785c18d51b451148cefa4ac1d7
Christian Falch <christian.falch@gmail.com>
2026-04-14 16:07:47 +0200

    fix(expo-modules-core): fixed re-export of ExpoModulesMacros from ExpoModulesCore
    When building everything from source this works well - but the fix (#44354) for doing the same when using it from prebuilt XCFrameworks was wrong.
    
    The issue is that when building E-M-C as an XCFramework we don't have the `ExpoModulesMacros` library installed as an XCFramework so we can't depend on it.
    
    This commit fixes this by re-introducing the ExpoModulesMacros.swift file in E-M-C in its prebuilt config (it was excluded in #44354).
    
    In addition it has a conditional import that also works for precompiled - where the Macro doesn't need to be loaded to be referenced and declared:
    
    ```swift
     #if canImport(ExpoModulesMacros)
     @_exported import ExpoModulesMacros
     #else
     @attached(peer, names: arbitrary)
     public macro OptimizedFunction() =
       #externalMacro(module: "ExpoModulesMacros", type: "OptimizedFunctionAttachedMacro")
     #endif
    ```
    
    Tested with/without precompiled XCFrameworks in BareExpo.
    
    The file BenchmarkingExpoModule.swift tests this throught the APIs/Benchmarks in BareExpo.

--- packages/expo-modules-core/ios/Core/ExpoModulesMacros.swift
+++ packages/expo-modules-core/ios/Core/ExpoModulesMacros.swift
@@ -2,5 +2,13 @@
  * Re-export the `@OptimizedFunction` or other macros from the standalone macros plugin package
  * (`@expo/expo-modules-macros-plugin`). This allows module authors to use the macro
  * by importing ExpoModulesCore alone, without adding a direct dependency on the plugin package.
+ * Note: The plugin is not be available at compile time for prebuilts, so we need to 
+ * conditionally import it and provide a fallback implementation of the macro.
  */
-@_exported import ExpoModulesMacros
+ #if canImport(ExpoModulesMacros)
+ @_exported import ExpoModulesMacros
+ #else
+ @attached(peer, names: arbitrary)
+ public macro OptimizedFunction() =
+   #externalMacro(module: "ExpoModulesMacros", type: "OptimizedFunctionAttachedMacro")
+ #endif
