05943b1e502b408501be3acf813712cdc3543eb4
Aman Mittal <amandeepmittal@live.com>
2026-04-16 00:23:26 +0530

    Update _document.tsx

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,30 +1,44 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { getThemeFromCookieHeader } from '@expo/styleguide';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentProps,
+} from 'next/document';
 
-export default class DocsDocument extends Document {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: <>{initialProps.styles}</>,
-    };
-  }
+type Props = DocumentProps & {
+  serverTheme: 'dark' | 'light' | null;
+};
 
-  render() {
-    return (
-      <Html lang="en" data-expo-theme>
-        <Head>
+export default function DocsDocument({ serverTheme }: Props) {
+  return (
+    <Html
+      lang="en"
+      className={serverTheme === 'dark' ? 'dark-theme' : undefined}
+      data-expo-theme={serverTheme ?? undefined}
+      suppressHydrationWarning>
+      <Head>
+        {!serverTheme && (
           <script
             dangerouslySetInnerHTML={{
               __html: `(function(){if(window.matchMedia("(prefers-color-scheme:dark)").matches){document.documentElement.classList.add("dark-theme")}})()`,
             }}
           />
-        </Head>
-        <body className="text-pretty">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        )}
+      </Head>
+      <body className="text-pretty">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
+
+DocsDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const initialProps = await Document.getInitialProps(ctx);
+  const cookieHeader = ctx.req?.headers?.cookie;
+  const serverTheme = cookieHeader ? getThemeFromCookieHeader(cookieHeader) : null;
+  return { ...initialProps, serverTheme };
+};
