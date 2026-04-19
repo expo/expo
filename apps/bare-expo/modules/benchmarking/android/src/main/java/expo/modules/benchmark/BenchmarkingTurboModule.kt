5421879ac9db41d7ae0e7a540dafbc495d8a3b72
Tomasz Sapeta <tsapeta@expo.dev>
2026-04-19 18:57:29 +0200

    [ncl] Redesign modules benchmarks screen

--- apps/bare-expo/modules/benchmarking/android/src/main/java/expo/modules/benchmark/BenchmarkingTurboModule.kt
+++ apps/bare-expo/modules/benchmarking/android/src/main/java/expo/modules/benchmark/BenchmarkingTurboModule.kt
@@ -1,7 +1,10 @@
 package expo.modules.benchmark
 
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.WritableMap
 import expo.modules.kotlin.iterator
 
 class BenchmarkingTurboModule(reactContext: ReactApplicationContext) : NativeBenchmarkingTurboModuleSpec(reactContext) {
@@ -33,5 +36,12 @@ class BenchmarkingTurboModule(reactContext: ReactApplicationContext) : NativeBen
     }
 
     return sum
+  }
+
+  override fun echoObject(point: ReadableMap): WritableMap {
+    val result = Arguments.createMap()
+    result.putDouble("x", point.getDouble("x"))
+    result.putDouble("y", point.getDouble("y"))
+    return result
   }
 }
