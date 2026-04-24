a002f79169ee8726f3f5a2c8085cf493284e95a9
lukmccall <kosmatylukasz@gmail.com>
2026-04-24 20:13:16 +0200

    Fix CI

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/types/TypeConverterCollection.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/types/TypeConverterCollection.kt
@@ -94,15 +94,3 @@ class TypeConverterCollection<Type : Any>(
     )
   }
 }
-
-private fun Class<*>.toBoxedIfPrimitive(): Class<*> = when (this) {
-  Int::class.java -> Integer::class.java
-  Long::class.java -> java.lang.Long::class.java
-  Double::class.java -> java.lang.Double::class.java
-  Float::class.java -> java.lang.Float::class.java
-  Boolean::class.java -> java.lang.Boolean::class.java
-  Byte::class.java -> java.lang.Byte::class.java
-  Short::class.java -> java.lang.Short::class.java
-  Char::class.java -> java.lang.Character::class.java
-  else -> this
-}
