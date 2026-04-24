9597d072d2fb74ac19ab97aeb2e60f742d827ef5
Christian Falch <christian.falch@gmail.com>
2026-04-24 15:43:23 +0200

    fix(devtools): split node and native entrypoints

--- packages/@expo/devtools/build/index.native.d.ts
+++ packages/@expo/devtools/build/index.native.d.ts
@@ -1,2 +1,10 @@
-export * from './index.js';
+export * from './hooks.js';
+export { setEnableLogging } from './logger.js';
+export { getDevToolsPluginClientAsync } from './DevToolsPluginClientFactory.js';
+export { DevToolsPluginClient } from './DevToolsPluginClient.js';
+export type { EventSubscription } from './DevToolsPluginClient.js';
+export type * from './devtools.types.js';
+export { createDevToolsPluginClient as unstable_createDevToolsPluginClient } from './DevToolsPluginClientFactory.js';
+export { WebSocketBackingStore as unstable_WebSocketBackingStore } from './WebSocketBackingStore.js';
+export { getConnectionInfo as unstable_getConnectionInfo } from './getConnectionInfo.native.js';
 //# sourceMappingURL=index.native.d.ts.map
--- packages/@expo/devtools/src/index.native.ts
+++ packages/@expo/devtools/src/index.native.ts
@@ -4,4 +4,16 @@ import { getConnectionInfo } from './getConnectionInfo.native.js';
 // Initialize the platform-specific getConnectionInfo implementation
 setGetConnectionInfo(getConnectionInfo);
 
-export * from './index.js';
+export * from './hooks.js';
+export { setEnableLogging } from './logger.js';
+export { getDevToolsPluginClientAsync } from './DevToolsPluginClientFactory.js';
+export { DevToolsPluginClient } from './DevToolsPluginClient.js';
+
+// Export the EventSubscription type if people need to use explicit type from `addMessageListener`
+export type { EventSubscription } from './DevToolsPluginClient.js';
+export type * from './devtools.types.js';
+
+// Unstable APIs exported for testing purposes.
+export { createDevToolsPluginClient as unstable_createDevToolsPluginClient } from './DevToolsPluginClientFactory.js';
+export { WebSocketBackingStore as unstable_WebSocketBackingStore } from './WebSocketBackingStore.js';
+export { getConnectionInfo as unstable_getConnectionInfo } from './getConnectionInfo.native.js';
