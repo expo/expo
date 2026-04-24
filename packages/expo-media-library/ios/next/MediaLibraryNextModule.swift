expo/expo

commit 9be1b0d701bbd96ef20fea9dc38d562b9b746bab

[media-library][next] Add presentPermissionsPicker (#44432)
# Why
Add `presentPermissionsPicker` similar to the existing method in the
legacy module.

This method is added without the Async suffix. I plan to follow up with
a separate PR to remove the suffix from `getPermissionsAsync` and
`requestPermissionsAsync`.

# How
Add `presentPermissionsPicker()`

# Test Plan
Tested on Bare Expo.

packages/expo-media-library/ios/next/MediaLibraryNextModule.swift
@@ -1,5 +1,6 @@
 import ExpoModulesCore
 import Photos
+import PhotosUI
 
 public final class MediaLibraryNextModule: Module {
   public func definition() -> ModuleDefinition {
@@ -247,6 +248,13 @@ public final class MediaLibraryNextModule: Module {
           reject: promise.legacyRejecter
         )
     }
+
+    AsyncFunction("presentPermissionsPicker") { (_ mediaTypes: [String]?) in
+      guard let vc = appContext?.utilities?.currentViewController() else {
+        return
+      }
+      PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: vc)
+    }.runOnQueue(.main)
   }
 
   private func getAssetIdsFromAssetRefs(from assetRefs: Either<[Asset], [URL]>) async throws -> [String] {

packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/next/MediaLibraryNextModule.kt
@@ -261,6 +261,10 @@ class MediaLibraryNextModule : Module() {
       systemPermissionsDelegate.getPermissions(writeOnly ?: false, permissions, promise)
     }
 
+    AsyncFunction("presentPermissionsPicker") { permissions: List<GranularPermission>?, promise: Promise ->
+      systemPermissionsDelegate.presentPermissionsPicker(permissions, promise)
+    }
+
     RegisterActivityContracts {
       with(mediaStorePermissionsDelegate) {
         registerMediaStoreContracts(this@MediaLibraryNextModule)

packages/expo-media-library/android/src/main/java/expo/modules/medialibrary/next/permissions/SystemPermissionsDelegate.kt
@@ -43,6 +43,14 @@ class SystemPermissionsDelegate(private val appContext: AppContext) {
     )
   }
 
+  fun presentPermissionsPicker(permissions: List<GranularPermission>?, promise: Promise) {
+    if (Build.VERSION.SDK_INT < 34) {
+      throw PermissionException("presentPermissionsPicker is only available on Android 14+")
+    }
+    val pickerPermissions = permissions ?: listOf(GranularPermission.PHOTO, GranularPermission.VIDEO)
+    requestPermissions(writeOnly = false, permissions = pickerPermissions, promise = promise)
+  }
+
   fun requireReadPermissions() {
     val granted = appContext.permissions?.hasGrantedPermissions(READ_EXTERNAL_STORAGE)
     if (granted != true) {

packages/expo-media-library/build/next/ExpoMediaLibraryNext.d.ts
@@ -1,15 +1,17 @@
 import type { PermissionResponse } from 'expo-modules-core';
 import { NativeModule } from 'expo-modules-core';
 import type { GranularPermission } from './MediaLibraryNext.types';
-import type { Album } from './types/Album';
-import type { Asset } from './types/Asset';
-import type { Query } from './types/Query';
+import { Album } from './types/Album';
+import { Asset } from './types/Asset';
+import type { MediaTypeFilter } from './types/MediaTypeFilter';
+import { Query } from './types/Query';
 declare class ExpoMediaLibraryNextModule extends NativeModule {
     Asset: typeof Asset;
     Album: typeof Album;
     Query: typeof Query;
     getPermissionsAsync(writeOnly?: boolean, granularPermissions?: GranularPermission[]): Promise<PermissionResponse>;
     requestPermissionsAsync(writeOnly?: boolean, granularPermissions?: GranularPermission[]): Promise<PermissionResponse>;
+    presentPermissionsPicker(mediaTypes?: MediaTypeFilter[]): Promise<void>;
 }
 declare const _default: ExpoMediaLibraryNextModule;
 export default _default;

packages/expo-media-library/build/next/ExpoMediaLibraryNext.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"ExpoMediaLibraryNext.d.ts","sourceRoot":"","sources":["../../src/next/ExpoMediaLibraryNext.ts"],"names":[],"mappings":"AAAA,OAAO,KAAK,EAAE,kBAAkB,EAAE,MAAM,mBAAmB,CAAC;AAC5D,OAAO,EAAE,YAAY,EAAuB,MAAM,mBAAmB,CAAC;AAEtE,OAAO,KAAK,EAAE,kBAAkB,EAAE,MAAM,0BAA0B,CAAC;AACnE,OAAO,KAAK,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AAC3C,OAAO,KAAK,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AAC3C,OAAO,KAAK,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AAE3C,OAAO,OAAO,0BAA2B,SAAQ,YAAY;IAC3D,KAAK,EAAE,OAAO,KAAK,CAAC;IACpB,KAAK,EAAE,OAAO,KAAK,CAAC;IACpB,KAAK,EAAE,OAAO,KAAK,CAAC;IAEpB,mBAAmB,CACjB,SAAS,CAAC,EAAE,OAAO,EACnB,mBAAmB,CAAC,EAAE,kBAAkB,EAAE,GACzC,OAAO,CAAC,kBAAkB,CAAC;IAC9B,uBAAuB,CACrB,SAAS,CAAC,EAAE,OAAO,EACnB,mBAAmB,CAAC,EAAE,kBAAkB,EAAE,GACzC,OAAO,CAAC,kBAAkB,CAAC;CAC/B;;AAED,wBAAuF"}
+{"version":3,"file":"ExpoMediaLibraryNext.d.ts","sourceRoot":"","sources":["../../src/next/ExpoMediaLibraryNext.ts"],"names":[],"mappings":"AAAA,OAAO,KAAK,EAAE,kBAAkB,EAAE,MAAM,mBAAmB,CAAC;AAC5D,OAAO,EAAE,YAAY,EAAuB,MAAM,mBAAmB,CAAC;AAEtE,OAAO,KAAK,EAAE,kBAAkB,EAAE,MAAM,0BAA0B,CAAC;AACnE,OAAO,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AACtC,OAAO,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AACtC,OAAO,KAAK,EAAE,eAAe,EAAE,MAAM,yBAAyB,CAAC;AAC/D,OAAO,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AAEtC,OAAO,OAAO,0BAA2B,SAAQ,YAAY;IAC3D,KAAK,EAAE,OAAO,KAAK,CAAC;IACpB,KAAK,EAAE,OAAO,KAAK,CAAC;IACpB,KAAK,EAAE,OAAO,KAAK,CAAC;IAEpB,mBAAmB,CACjB,SAAS,CAAC,EAAE,OAAO,EACnB,mBAAmB,CAAC,EAAE,kBAAkB,EAAE,GACzC,OAAO,CAAC,kBAAkB,CAAC;IAC9B,uBAAuB,CACrB,SAAS,CAAC,EAAE,OAAO,EACnB,mBAAmB,CAAC,EAAE,kBAAkB,EAAE,GACzC,OAAO,CAAC,kBAAkB,CAAC;IAC9B,wBAAwB,CAAC,UAAU,CAAC,EAAE,eAAe,EAAE,GAAG,OAAO,CAAC,IAAI,CAAC;CACxE;;AAED,wBAAuF"}

packages/expo-media-library/build/next/ExpoMediaLibraryNext.js
@@ -1,3 +1,6 @@
 import { NativeModule, requireNativeModule } from 'expo-modules-core';
+import { Album } from './types/Album';
+import { Asset } from './types/Asset';
+import { Query } from './types/Query';
 export default requireNativeModule('ExpoMediaLibraryNext');
 //# sourceMappingURL=ExpoMediaLibraryNext.js.map

packages/expo-media-library/build/next/ExpoMediaLibraryNext.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"ExpoMediaLibraryNext.js","sourceRoot":"","sources":["../../src/next/ExpoMediaLibraryNext.ts"],"names":[],"mappings":"AACA,OAAO,EAAE,YAAY,EAAE,mBAAmB,EAAE,MAAM,mBAAmB,CAAC;AAsBtE,eAAe,mBAAmB,CAA6B,sBAAsB,CAAC,CAAC","sourcesContent":["import type { PermissionResponse } from 'expo-modules-core';\nimport { NativeModule, requireNativeModule } from 'expo-modules-core';\n\nimport type { GranularPermission } from './MediaLibraryNext.types';\nimport type { Album } from './types/Album';\nimport type { Asset } from './types/Asset';\nimport type { Query } from './types/Query';\n\ndeclare class ExpoMediaLibraryNextModule extends NativeModule {\n  Asset: typeof Asset;\n  Album: typeof Album;\n  Query: typeof Query;\n\n  getPermissionsAsync(\n    writeOnly?: boolean,\n    granularPermissions?: GranularPermission[]\n  ): Promise<PermissionResponse>;\n  requestPermissionsAsync(\n    writeOnly?: boolean,\n    granularPermissions?: GranularPermission[]\n  ): Promise<PermissionResponse>;\n}\n\nexport default requireNativeModule<ExpoMediaLibraryNextModule>('ExpoMediaLibraryNext');\n"]}
+{"version":3,"file":"ExpoMediaLibraryNext.js","sourceRoot":"","sources":["../../src/next/ExpoMediaLibraryNext.ts"],"names":[],"mappings":"AACA,OAAO,EAAE,YAAY,EAAE,mBAAmB,EAAE,MAAM,mBAAmB,CAAC;AAGtE,OAAO,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AACtC,OAAO,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AAEtC,OAAO,EAAE,KAAK,EAAE,MAAM,eAAe,CAAC;AAkBtC,eAAe,mBAAmB,CAA6B,sBAAsB,CAAC,CAAC","sourcesContent":["import type { PermissionResponse } from 'expo-modules-core';\nimport { NativeModule, requireNativeModule } from 'expo-modules-core';\n\nimport type { GranularPermission } from './MediaLibraryNext.types';\nimport { Album } from './types/Album';\nimport { Asset } from './types/Asset';\nimport type { MediaTypeFilter } from './types/MediaTypeFilter';\nimport { Query } from './types/Query';\n\ndeclare class ExpoMediaLibraryNextModule extends NativeModule {\n  Asset: typeof Asset;\n  Album: typeof Album;\n  Query: typeof Query;\n\n  getPermissionsAsync(\n    writeOnly?: boolean,\n    granularPermissions?: GranularPermission[]\n  ): Promise<PermissionResponse>;\n  requestPermissionsAsync(\n    writeOnly?: boolean,\n    granularPermissions?: GranularPermission[]\n  ): Promise<PermissionResponse>;\n  presentPermissionsPicker(mediaTypes?: MediaTypeFilter[]): Promise<void>;\n}\n\nexport default requireNativeModule<ExpoMediaLibraryNextModule>('ExpoMediaLibraryNext');\n"]}

packages/expo-media-library/build/next/MediaLibraryNext.types.d.ts
@@ -2,6 +2,7 @@ export * from './types/SortDescriptor';
 export * from './types/AssetField';
 export * from './types/MediaSubtype';
 export * from './types/MediaType';
+export * from './types/MediaTypeFilter';
 export * from './types/GranularPermission';
 export * from './types/AssetInfo';
 //# sourceMappingURL=MediaLibraryNext.types.d.ts.map

commit fa2c26e39549edc144657c50a189271ca56d1ab9

[media-library] Fix failing tvOS job (#45071)
# Why

fixes a failing CI job on tvOS introduced in #44432.

# How

Adds a platform check to `presentPermissionsPicker` to exclude tvOS.

# Test Plan
Green CI

packages/expo-media-library/ios/next/MediaLibraryNextModule.swift
@@ -250,10 +250,12 @@ public final class MediaLibraryNextModule: Module {
     }
 
     AsyncFunction("presentPermissionsPicker") { (_ mediaTypes: [String]?) in
+      #if os(iOS)
       guard let vc = appContext?.utilities?.currentViewController() else {
         return
       }
       PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: vc)
+      #endif
     }.runOnQueue(.main)
   }
 