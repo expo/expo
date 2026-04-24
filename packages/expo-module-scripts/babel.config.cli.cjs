89d330829f6b1ec25c7be71bf142d61d8591cd70
Phil Pluckthun <phil@kitten.sh>
2026-04-24 12:09:33 +0100

    chore(expo-module-scripts,expo): Remove cyclical dependencies (#45060)
    # Why
    
    Follow-up to #45048
    
    `expo` is a special package, almost like a "meta" package. While it
    depends on a few packages, we should avoid it depending on
    `expo-module-scripts`, since it can re-enter dependencies that
    themselves depend on `expo-module-scripts`. This shouldn't strictly
    speaking be necessary, but is a convenient "circuit-breaker" for
    dependency graph building, and happens to be trivial for this dependency
    for now.
    
    `expo-module-scripts` had a leftover dependency on `babel-preset-expo`
    which re-enters `expo-module-scripts`. However, the resulting Babel
    preset is only used for Jest testing and should be removable for the
    purpose of `expo-module-scripts`.
    
    # How
    
    - Drop `expo-module-scripts` from `expo` in favour of `npm-run-all2` and
    `rimraf` (already used by `@expo/log-box` and others)
    - Drop `babel-preset-expo` from `expo-module-scripts` and instead have a
    relative require there
    
    # Test Plan
    
    - CI should pass unchanged
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

--- packages/expo-module-scripts/babel.config.cli.cjs
+++ packages/expo-module-scripts/babel.config.cli.cjs
@@ -1,4 +1,4 @@
-export default function (api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
--- packages/expo-module-scripts/babel.config.plugin.cjs
+++ packages/expo-module-scripts/babel.config.plugin.cjs
@@ -1,4 +1,4 @@
-export default function (api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     plugins: ['@babel/plugin-transform-flow-strip-types'],
