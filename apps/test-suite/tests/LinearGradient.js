1e2c7f066385871994ab943af4d31b15b6362b8a
lukmccall <kosmatylukasz@gmail.com>
2026-04-28 22:05:22 +0200

    [test-suite] Remove `LinearGradient` on Android

--- apps/test-suite/tests/LinearGradient.js
+++ apps/test-suite/tests/LinearGradient.js
@@ -7,6 +7,8 @@ import { mountAndWaitFor as originalMountAndWaitFor } from './helpers';
 export const name = 'LinearGradient';
 const style = { width: 200, height: 200 };
 
+// This tests are excluded on Android because they frequently break the emulator on GitHub Actions, and we don't know why.
+// See TestModules.ts
 export async function test(
   { it, describe, beforeAll, jasmine, afterAll, expect, afterEach, beforeEach },
   { setPortalChild, cleanupPortal }
@@ -19,12 +21,8 @@ export async function test(
     originalMountAndWaitFor(child, propName, setPortalChild);
 
   describe(name, () => {
-    // Skip on Android because the test frequently breaks the emulator on GitHub
-    // Actions, and we don't know why
-    if (Platform.OS !== 'android') {
-      it(`renders`, async () => {
-        await mountAndWaitFor(<LinearGradient colors={['red', 'blue']} style={style} />);
-      });
-    }
+    it(`renders`, async () => {
+      await mountAndWaitFor(<LinearGradient colors={['red', 'blue']} style={style} />);
+    });
   });
 }
