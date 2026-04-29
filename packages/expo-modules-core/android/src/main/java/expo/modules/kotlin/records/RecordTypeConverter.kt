e744a4f5a142f8f4917a0e819e29661249380e72
lukmccall <kosmatylukasz@gmail.com>
2026-04-29 13:20:11 +0200

    [skip ci] run spotless

--- packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/records/RecordTypeConverter.kt
+++ packages/expo-modules-core/android/src/main/java/expo/modules/kotlin/records/RecordTypeConverter.kt
@@ -181,9 +181,11 @@ class IntrospectableRecordConversionStrategy<T : Record>(
           .firstOrNull { annotation -> annotation.jClass == Field::class.java }
           ?: return@mapNotNull null
 
-        val propertyName = (fieldAnnotation
-          .arguments
-          .getOrDefault("key", property.name) as String)
+        val propertyName = (
+          fieldAnnotation
+            .arguments
+            .getOrDefault("key", property.name) as String
+          )
           .ifEmpty { property.name }
 
         val isRequired = property
