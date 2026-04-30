395667fe4ecec6a7219da4bc138edd58b3fcb3bd
Vojtech Novak <vonovak@gmail.com>
2026-04-30 13:59:48 +0200

    remove old export

--- packages/expo-ui/build/datetime-picker/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { DateTimePicker } from '../community/datetime-picker/DateTimePicker';
-export { type DateTimePickerEvent, type DateTimePickerChangeEvent, type DateTimePickerProps, } from '../community/datetime-picker/types';
-export default DateTimePicker;
-export { DateTimePicker };
-//# sourceMappingURL=index.d.ts.map
