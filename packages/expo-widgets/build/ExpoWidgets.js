e23fd58518c662a733a7317e1cc43485d7f56635
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2026-05-04 14:29:40 +0200

    [widgets][android] Add stub methods

--- packages/expo-widgets/build/ExpoWidgets.js
+++ packages/expo-widgets/build/ExpoWidgets.js
@@ -1,3 +1,39 @@
-import { NativeModule, requireNativeModule } from 'expo';
-export default requireNativeModule('ExpoWidgets');
+const noopSubscription = { remove() { } };
+class WidgetStub {
+    constructor(_name, _layout) { }
+    reload() { }
+    updateTimeline(_entries) { }
+    async getTimeline() {
+        return [];
+    }
+}
+class LiveActivityStub {
+    async update(_props) { }
+    async end(_dismissalPolicy, _afterDate, _state, _contentDate) { }
+    async getPushToken() {
+        return null;
+    }
+    addListener(_eventName, _listener) {
+        return noopSubscription;
+    }
+}
+class LiveActivityFactoryStub {
+    constructor(_name, _layout) { }
+    start(_props, _url) {
+        return new LiveActivityStub();
+    }
+    getInstances() {
+        return [];
+    }
+}
+const ExpoWidgetsModule = {
+    reloadAllWidgets() { },
+    Widget: WidgetStub,
+    LiveActivityFactory: LiveActivityFactoryStub,
+    LiveActivity: LiveActivityStub,
+    addListener(_eventName, _listener) {
+        return noopSubscription;
+    },
+};
+export default ExpoWidgetsModule;
 //# sourceMappingURL=ExpoWidgets.js.map
--- packages/expo-widgets/src/ExpoWidgets.ts
+++ packages/expo-widgets/src/ExpoWidgets.ts
@@ -1,17 +1,61 @@
-import { NativeModule, requireNativeModule } from 'expo';
+import type { EventSubscription } from 'expo-modules-core';
 
 import type {
+  ExpoTimelineEntry,
   ExpoWidgetsEvents,
   NativeLiveActivity,
   NativeLiveActivityFactory,
   NativeWidgetObject,
 } from './Widgets.types';
 
-declare class ExpoWidgetsModule extends NativeModule<ExpoWidgetsEvents> {
-  reloadAllWidgets(): void;
-  readonly Widget: typeof NativeWidgetObject;
-  readonly LiveActivityFactory: typeof NativeLiveActivityFactory;
-  readonly LiveActivity: typeof NativeLiveActivity;
+const noopSubscription: EventSubscription = { remove() {} };
+
+class WidgetStub {
+  constructor(_name: string, _layout: string) {}
+  reload(): void {}
+  updateTimeline(_entries: ExpoTimelineEntry[]): void {}
+  async getTimeline(): Promise<ExpoTimelineEntry[]> {
+    return [];
+  }
 }
 
-export default requireNativeModule<ExpoWidgetsModule>('ExpoWidgets');
+class LiveActivityStub {
+  async update(_props: string): Promise<void> {}
+  async end(
+    _dismissalPolicy?: string,
+    _afterDate?: number,
+    _state?: string,
+    _contentDate?: number
+  ): Promise<void> {}
+  async getPushToken(): Promise<string | null> {
+    return null;
+  }
+  addListener(_eventName: string, _listener: (...args: unknown[]) => void): EventSubscription {
+    return noopSubscription;
+  }
+}
+
+class LiveActivityFactoryStub {
+  constructor(_name: string, _layout: string) {}
+  start(_props: string, _url?: string): NativeLiveActivity {
+    return new LiveActivityStub() as NativeLiveActivity;
+  }
+  getInstances(): NativeLiveActivity[] {
+    return [];
+  }
+}
+
+const ExpoWidgetsModule = {
+  reloadAllWidgets(): void {},
+  Widget: WidgetStub as typeof NativeWidgetObject,
+  LiveActivityFactory: LiveActivityFactoryStub as typeof NativeLiveActivityFactory,
+  LiveActivity: LiveActivityStub as typeof NativeLiveActivity,
+  addListener<EventName extends keyof ExpoWidgetsEvents>(
+    _eventName: EventName,
+    _listener: ExpoWidgetsEvents[EventName]
+  ): EventSubscription {
+    return noopSubscription;
+  },
+};
+
+export default ExpoWidgetsModule;
