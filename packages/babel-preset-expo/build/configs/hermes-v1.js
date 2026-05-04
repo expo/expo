a6798ec4e09d76887779c4f8bbaa62ed519eea3e
Phil Pluckthun <phil@kitten.sh>
2026-05-04 16:23:41 +0100

    Rebuild

--- packages/babel-preset-expo/build/configs/hermes-v1.js
+++ packages/babel-preset-expo/build/configs/hermes-v1.js
@@ -14,28 +14,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // use `this.foo = bar` instead of `this.defineProperty('foo', ...)`
 const loose = true;
 /** The JS syntax preset used with Hermes v1 (SDK 56+) */
-module.exports = function (_api, options) {
-    // We enable regenerator in dev builds for the time being because
-    // Hermes V1 doesn't yet fully support debugging native generators.
-    // Use native generators in release mode because it has already yielded perf wins.
-    const enableRegenerator = options.dev ?? false;
+module.exports = function (_api, _options) {
     const plugins = [
         [require('@babel/plugin-transform-block-scoping')],
-        // NOTE: Hermes V1 preserves classes, so no class-properties or transform-classes plugins.
         [require('@babel/plugin-transform-class-static-block'), { loose }],
-        [require('@babel/plugin-transform-private-methods'), { loose }],
-        [require('@babel/plugin-transform-private-property-in-object'), { loose }],
-        [require('@babel/plugin-transform-unicode-regex')],
-        [require('@babel/plugin-transform-named-capturing-groups-regex')],
+        [require('@babel/plugin-transform-async-generator-functions')],
     ];
-    // Needed for regenerator
-    if (enableRegenerator) {
-        plugins.push([require('@babel/plugin-transform-optional-catch-binding')]);
-    }
-    plugins.push([require('@babel/plugin-transform-destructuring'), { useBuiltIns: true }], [require('@babel/plugin-transform-async-generator-functions')], [require('@babel/plugin-transform-async-to-generator')], [require('@babel/plugin-transform-react-display-name')]);
-    if (enableRegenerator) {
-        plugins.push([require('@babel/plugin-transform-optional-chaining'), { loose: true }], [require('@babel/plugin-transform-nullish-coalescing-operator'), { loose: true }], [require('@babel/plugin-transform-for-of'), { loose: true }]);
-    }
     return {
         comments: false,
         compact: true,
