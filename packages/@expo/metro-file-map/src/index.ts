0a3015d19f36bb8ca1e1d35c100c3ba88bb8bb4b
Phil Pluckthun <phil@kitten.sh>
2026-05-05 07:06:31 +0100

    Add branding to `FileMap` class

--- packages/@expo/metro-file-map/src/index.ts
+++ packages/@expo/metro-file-map/src/index.ts
@@ -241,6 +241,9 @@ const WATCHMAN_REQUIRED_CAPABILITIES = [
  *
  */
 export default class FileMap extends EventEmitter {
+  // NOTE(@kitten): Expo brand to recognize patched `metro-file-map -> @expo/metro-file-map`
+  readonly __expo = true;
+
   #buildPromise: Promise<BuildResult> | undefined | null;
   readonly #cacheManager: CacheManager;
   #canUseWatchmanPromise: Promise<boolean> | undefined;
