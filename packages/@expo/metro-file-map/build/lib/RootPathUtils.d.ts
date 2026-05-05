a08355fa7ac4d96344bb6be52bc6ad9241f37f0c
Phil Pluckthun <phil@kitten.sh>
2026-05-05 06:57:40 +0100

    Build

---
+++ packages/@expo/metro-file-map/build/lib/RootPathUtils.d.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+export declare class RootPathUtils {
+    #private;
+    constructor(rootDir: string);
+    getBasenameOfNthAncestor(n: number): string;
+    getParts(): readonly string[];
+    absoluteToNormal(absolutePath: string): string;
+    normalToAbsolute(normalPath: string): string;
+    relativeToNormal(relativePath: string): string;
+    getAncestorOfRootIdx(normalPath: string): number | null;
+    joinNormalToRelative(normalPath: string, relativePath: string): {
+        normalPath: string;
+        collapsedSegments: number;
+    };
+    relative(from: string, to: string): string;
+}
