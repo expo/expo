620e238df158565bcc7912b4bd7319659d01d76e
Mathieu Acthernoene <zoontek@gmail.com>
2026-05-06 15:19:57 +0200

    Update Button

---
+++ packages/expo-ui/src/ts-declarations/react-native-web.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-native' {
+  interface PressableStateCallbackType {
+    /** @platform web */
+    readonly hovered: boolean;
+    /** @platform web */
+    readonly focused: boolean;
+  }
+}
+
+export {};
