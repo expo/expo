b0a445e490d39b7d7addcaa5c170e59423500f27
Phil Pluckthun <phil@kitten.sh>
2026-05-08 15:46:25 +0100

    Rebuild

--- packages/@expo/metro-config/build/file-store.d.ts
+++ packages/@expo/metro-config/build/file-store.d.ts
@@ -1,4 +1,9 @@
 import UpstreamFileStore from '@expo/metro/metro-cache/stores/FileStore';
 export declare class FileStore<T> extends UpstreamFileStore<T> {
+    private readonly _root;
+    constructor(options: {
+        root: string;
+    });
     set(key: Buffer, value: any): Promise<void>;
+    clear(): void;
 }
