4acd15ad1973a3bb7221ddc657dd0d111b66a325
Kudo Chien <kudo@expo.dev>
2026-05-12 17:34:10 +0800

    [go] disable hbc (#45617)

--- apps/expo-go/android/expoview/src/main/java/versioned/host/exp/exponent/ExponentDevBundleDownloadListener.kt
+++ apps/expo-go/android/expoview/src/main/java/versioned/host/exp/exponent/ExponentDevBundleDownloadListener.kt
@@ -2,15 +2,28 @@ package versioned.host.exp.exponent
 
 import com.facebook.react.devsupport.interfaces.DevBundleDownloadListener
 import host.exp.exponent.experience.DevBundleDownloadProgressListener
+import host.exp.exponent.utils.HermesBundleUtils
+import java.io.File
 import kotlin.Exception
 
 /**
  * Acts as a bridge between the versioned DevBundleDownloadListener and unversioned
- * DevBundleDownloadProgressListener
+ * DevBundleDownloadProgressListener. Optionally sniffs the downloaded bundle for the
+ * Hermes magic header and fails instead of succeeding when matched.
  */
-class ExponentDevBundleDownloadListener(private val listener: DevBundleDownloadProgressListener) :
-  DevBundleDownloadListener {
+class ExponentDevBundleDownloadListener(
+  private val listener: DevBundleDownloadProgressListener,
+  private val downloadedBundleFileProvider: (() -> File?)? = null,
+  private val onHermesDetected: ((Exception) -> Unit)? = null
+) : DevBundleDownloadListener {
   override fun onSuccess() {
+    val bundleFile = downloadedBundleFileProvider?.invoke()
+    if (bundleFile != null && HermesBundleUtils.isHermesBundle(bundleFile)) {
+      val error = Exception("hermes bytecode bundle is not supported by expo-go")
+      onHermesDetected?.invoke(error)
+      listener.onFailure(error)
+      return
+    }
     listener.onSuccess()
   }
 
