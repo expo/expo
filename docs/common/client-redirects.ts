7681611c8aff36bf76026b1ef67f453f42025da0
Aman Mittal <amandeepmittal@live.com>
2026-05-14 13:24:24 +0530

    Remove/update other files

--- docs/common/client-redirects.ts
+++ docs/common/client-redirects.ts
@@ -540,12 +540,8 @@ const RENAMED_PAGES: Record<string, string> = {
   '/config-plugins/plugins-and-mods/': '/config-plugins/plugins/',
 
   // After merging registerRootComponent info in `expo` API reference
-  '/versions/v53.0.0/sdk/register-root-component/':
-    '/versions/v53.0.0/sdk/expo/#registerrootcomponentcomponent',
   '/versions/latest/sdk/register-root-component/':
     '/versions/latest/sdk/expo/#registerrootcomponentcomponent',
-  '/versions/v53.0.0/sdk/url/': '/versions/v53.0.0/sdk/expo/#url-api',
-  '/versions/v53.0.0/sdk/encoding/': '/versions/v53.0.0/sdk/expo/#encoding-api',
 
   // Temporary redirects
   '/router/advanced/singular/': '/preview/singular/',
