1c8fc4cc0281a940d07091a93364a9ad1295abdf
Tomasz Sapeta <tsapeta@expo.dev>
2026-05-14 12:58:32 +0200

    [app-metrics][observe] emit declarations only (#45767)

--- packages/expo-app-metrics/build/module.js
+++
@@ -1,3 +0,0 @@
-import { requireNativeModule } from 'expo';
-export default requireNativeModule('ExpoAppMetrics');
-//# sourceMappingURL=module.js.map
--- packages/expo-observe/build/module.web.js
+++
@@ -1,9 +0,0 @@
-import { NativeModule, registerWebModule } from 'expo';
-export * from './types';
-class ExpoObserveModule extends NativeModule {
-    async dispatchEvents() { }
-    configure(config) { }
-    setBundleDefaults(defaults) { }
-}
-export default registerWebModule(ExpoObserveModule, 'ExpoObserve');
-//# sourceMappingURL=module.web.js.map
