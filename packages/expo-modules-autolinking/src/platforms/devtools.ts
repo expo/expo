69efa1e2be51e58918d48f2dbbabd6b67711c9ee
Phil Pluckthun <phil@kitten.sh>
2026-05-15 14:41:52 +0100

    autolinking: Sanitize devtools web-root path

--- packages/expo-modules-autolinking/src/platforms/devtools.ts
+++ packages/expo-modules-autolinking/src/platforms/devtools.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 
 import type { ExtraDependencies, ModuleDescriptorDevTools, PackageRevision } from '../types';
+import { isPathInside } from '../utils';
 
 export async function resolveModuleAsync(
   packageName: string,
@@ -14,11 +15,20 @@ export async function resolveModuleAsync(
   return {
     packageName,
     packageRoot: revision.path,
-    webpageRoot: devtoolsConfig.webpageRoot
-      ? path.join(revision.path, devtoolsConfig.webpageRoot)
-      : undefined,
+    webpageRoot: resolveWebpageRoot(revision.path, devtoolsConfig.webpageRoot),
     cliExtensions: devtoolsConfig.cliExtensions,
   };
+}
+
+function resolveWebpageRoot(
+  packageRoot: string,
+  configured: string | undefined
+): string | undefined {
+  if (!configured) {
+    return undefined;
+  }
+  const joined = path.join(packageRoot, configured);
+  return isPathInside(joined, packageRoot) ? joined : undefined;
 }
 
 export async function resolveExtraBuildDependenciesAsync(
