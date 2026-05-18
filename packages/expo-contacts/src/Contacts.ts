16b3b14bfae44ca9645e1eb665e314b99cb8f04f
Aman Mittal <amandeepmittal@live.com>
2026-05-18 22:55:09 +0530

    [contacts][docs] Fix nested callouts issue in `addContactAsync`

--- packages/expo-contacts/src/Contacts.ts
+++ packages/expo-contacts/src/Contacts.ts
@@ -715,13 +715,16 @@ export async function getContactByIdAsync(
 }
 
 /**
- * Creates a new contact and adds it to the system.
- * @deprecated This legacy `expo-contacts` API is deprecated. Use `Contact.create()` from `expo-contacts/next` instead.
+ *  Creates a new contact and adds it to the system.
+ *
  * > **Note**: For Android users, the Expo Go app does not have the required `WRITE_CONTACTS` permission to write to Contacts.
- * > You will need to create a [development build](/develop/development-builds/create-a-build/) and add permission in there manually to use this method.
+ * > You will need to create a [development build](/develop/development-builds/create-a-build/) and add permission in there
+ * > manually to use this method.
+ *
  * @param contact A contact with the changes you wish to persist. The `id` parameter will not be used.
  * @param containerId @tag-ios The container that will parent the contact.
  * @return A promise that fulfills with ID of the new system contact.
+ *
  * @example
  * ```js
  * const contact = {
@@ -731,6 +734,7 @@ export async function getContactByIdAsync(
  * };
  * const contactId = await Contacts.addContactAsync(contact);
  * ```
+ * @deprecated This legacy `expo-contacts` API is deprecated. Use `Contact.create()` from `expo-contacts/next` instead.
  */
 export async function addContactAsync(contact: Contact, containerId?: string): Promise<string> {
   if (!ExpoContacts.addContactAsync) {
