da88bd54f29ab980069aa65d21c7783a22770769
Jakub Tkacz <jtkacz@expo.io>
2026-05-19 11:32:04 +0200

    [expo-observe] add react-navigation integration

---
+++ packages/expo-observe/src/integrations/react-navigation/types.ts
@@ -0,0 +1,6 @@
+import type { NavigationRoute, NavigationState } from '@react-navigation/native';
+
+export type NavigationRouteLike = NavigationRoute<any, string> & { state: NavigationState };
+export type NavigationStateLike = NavigationState;
+
+export type GetPathname = (state: NavigationStateLike | undefined) => string | undefined;
