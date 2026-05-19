07eb86e95ed8dd5ccf5914fb3e61ca486def2077
HubertBer <bernackihubert03@gmail.com>
2026-05-19 11:37:17 +0200

    create output directory if it doesn't exist

--- packages/expo-type-information/build/commands/inlineModulesInterfaceCommand.js
+++ packages/expo-type-information/build/commands/inlineModulesInterfaceCommand.js
@@ -102,6 +102,7 @@ async function inlineModulesInterfaceCommand(cli) {
         if (!parsedArgs) {
             return;
         }
+        (0, commandUtils_1.maybePrepareOutputDirectory)(parsedArgs?.realOutputPath);
         const { appJsonPath, watcher } = parsedArgs;
         if (!appJsonPath) {
             return;
