965d41f3617e605900cb946c9351e626555d8733
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2026-05-19 16:41:49 +0200

    [expo-go][Android] Fix scoped path permissions [EXP-47] (#45972)

--- apps/expo-go/android/expoview/src/main/java/versioned/host/exp/exponent/modules/universal/ScopedFilePermissionModule.kt
+++ apps/expo-go/android/expoview/src/main/java/versioned/host/exp/exponent/modules/universal/ScopedFilePermissionModule.kt
@@ -1,16 +1,42 @@
 package versioned.host.exp.exponent.modules.universal
 
+import android.content.Context
 import expo.modules.core.ModuleRegistry
 import expo.modules.filesystem.legacy.FilePermissionModule
 import expo.modules.interfaces.filesystem.Permission
 import host.exp.exponent.utils.ScopedContext
 import java.io.File
 import java.io.IOException
-import java.util.*
+import java.util.EnumSet
 
 class ScopedFilePermissionModule(private val scopedContext: ScopedContext) : FilePermissionModule() {
 
   private lateinit var moduleRegistry: ModuleRegistry
+
+  override fun getPathPermissions(context: Context, path: String): EnumSet<Permission> {
+    return try {
+      val canonicalPath = File(path).canonicalPath
+      val scopedDirectories = listOf(
+        scopedContext.filesDir,
+        scopedContext.cacheDir,
+        scopedContext.noBackupFilesDir
+      ).map { it.canonicalPath }
+
+      if (scopedDirectories.any { canonicalPath.isInDirectory(it) }) {
+        return EnumSet.of(Permission.READ, Permission.WRITE)
+      }
+
+      val dataDirCanonicalPath = File(scopedContext.context.applicationInfo.dataDir).canonicalPath
+      if (canonicalPath.isInDirectory(dataDirCanonicalPath)) {
+        return EnumSet.noneOf(Permission::class.java)
+      }
+
+      getExternalPathPermissions(path)
+    } catch (e: IOException) {
+      // Something's not right, let's be cautious.
+      EnumSet.noneOf(Permission::class.java)
+    }
+  }
 
   override fun getExternalPathPermissions(path: String): EnumSet<Permission> {
     try {
@@ -34,4 +60,7 @@ class ScopedFilePermissionModule(private val scopedContext: ScopedContext) : Fil
   override fun onCreate(moduleRegistry: ModuleRegistry) {
     this.moduleRegistry = moduleRegistry
   }
+
+  private fun String.isInDirectory(directory: String): Boolean =
+    startsWith("$directory/") || this == directory
 }
