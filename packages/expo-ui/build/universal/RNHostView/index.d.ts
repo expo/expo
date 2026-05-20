cebd18e2c28f89dcd92bb15cac66a26be8574609
nishan (o^▽^o) <nishanbende@gmail.com>
2026-05-20 13:17:18 +0530

    [ui] add universal RNHostView (#46011)
    # Why
    
    RNHostView is useful when putting RN views inside of swiftui/compose
    view layout.
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    # How
    
    - Added web support and exported SwiftUI and Compose from universal
    entry point.
    - Added example and docs.
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    Tested the universal component in NCL on all platforms.
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

---
+++ packages/expo-ui/build/universal/RNHostView/index.d.ts
@@ -0,0 +1,7 @@
+import type { RNHostViewProps } from './types';
+/**
+ * Hosts React Native views inside SwiftUI or Jetpack Compose views.
+ */
+export declare function RNHostView({ children, style, onAppear, onDisappear, hidden, testID, matchContents, }: RNHostViewProps): import("react/jsx-runtime").JSX.Element;
+export * from './types';
+//# sourceMappingURL=index.d.ts.map
