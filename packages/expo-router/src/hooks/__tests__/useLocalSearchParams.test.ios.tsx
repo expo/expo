5c73ccf0e69b81e5a54b9a812ae73891ffd4008f
Jakub Tkacz <32908614+Ubax@users.noreply.github.com>
2026-05-26 12:50:58 +0200

    [expo-router][refactor] split hooks.test.ios into multiple smaller test files (#45820)
    # Why
    
    Follow-up to https://github.com/expo/expo/pull/45748 splitting the hooks
    tests
    
    # How
    
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    # Test Plan
    
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    # Checklist
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [ ] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [ ] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

---
+++ packages/expo-router/src/hooks/__tests__/useLocalSearchParams.test.ios.tsx
@@ -0,0 +1,129 @@
+import { act } from '@testing-library/react-native';
+import { expectType } from 'tsd';
+
+import { router, Slot } from '../../exports';
+import { renderRouter } from '../../testing-library';
+import { useLocalSearchParams } from '../useLocalSearchParams';
+import { renderHook, renderHookOnce } from './renderHook';
+
+describe(useLocalSearchParams, () => {
+  it(`return styles of deeply nested routes`, () => {
+    const { result } = renderHook(() => useLocalSearchParams(), ['[fruit]/[shape]/[...veg?]'], {
+      initialUrl: '/apple/square',
+    });
+
+    expect(result.current).toEqual({
+      fruit: 'apple',
+      shape: 'square',
+    });
+
+    act(() => router.push('/banana/circle/carrot'));
+
+    expect(result.current).toEqual({
+      fruit: 'banana',
+      shape: 'circle',
+      veg: ['carrot'],
+    });
+  });
+
+  it('passes values down navigators', () => {
+    const results1: [] = [];
+    const results2: [] = [];
+
+    renderRouter(
+      {
+        index: () => null,
+        '[id]/_layout': () => <Slot />,
+        '[id]/index': function Protected() {
+          results1.push(useLocalSearchParams());
+          return null;
+        },
+        '[id]/[fruit]/_layout': () => <Slot />,
+        '[id]/[fruit]/index': function Protected() {
+          results2.push(useLocalSearchParams());
+          return null;
+        },
+      },
+      {
+        initialUrl: '/1',
+      }
+    );
+
+    expect(results1).toEqual([{ id: '1' }]);
+    act(() => router.push('/2'));
+    expect(results1).toEqual([{ id: '1' }, { id: '2' }]);
+
+    act(() => router.push('/3/apple'));
+    // The first screen has not rerendered
+    expect(results1).toEqual([{ id: '1' }, { id: '2' }]);
+    expect(results2).toEqual([{ id: '3', fruit: 'apple' }]);
+  });
+
+  it(`defaults abstract types`, () => {
+    const params = renderHookOnce(() => useLocalSearchParams());
+    expectType<Record<string, string | string[] | undefined>>(params);
+    expectType<string | string[] | undefined>(params.a);
+  });
+  it(`allows abstract types`, () => {
+    const params = renderHookOnce(() => useLocalSearchParams<{ a: string }>());
+    expectType<{ a?: string }>(params);
+    expectType<string | undefined>(params.a);
+  });
+
+  it('does not return undefined search params', () => {
+    const { result } = renderHook(() => useLocalSearchParams(), ['index'], {
+      initialUrl: '/?test=1&test=2',
+    });
+
+    expect(result.current).toEqual({
+      test: ['1', '2'],
+    });
+
+    act(() => router.setParams({ test: undefined }));
+
+    expect(result.current).toEqual({});
+  });
+
+  it('passes null search params through without stringifying them', () => {
+    const { result } = renderHook(() => useLocalSearchParams(), ['index'], {
+      initialUrl: '/?test=1',
+    });
+
+    expect(result.current).toEqual({
+      test: '1',
+    });
+
+    act(() => router.setParams({ test: null }));
+
+    expect(result.current).toEqual({ test: null });
+  });
+
+  it(`handles encoded params`, () => {
+    const { result } = renderHook(() => useLocalSearchParams(), ['index'], {
+      initialUrl: '/?test=%2Fhello%2Fworld%2F',
+    });
+
+    expect(result.current).toEqual({
+      test: '/hello/world/',
+    });
+
+    act(() => router.setParams({ test: '%2Fhello%2Fworld%2Fagain' }));
+
+    expect(result.current).toEqual({
+      test: '/hello/world/again',
+    });
+
+    act(() =>
+      router.push({
+        pathname: '/',
+        params: {
+          test: '%2Ffoo%2Fbar%2F',
+        },
+      })
+    );
+
+    expect(result.current).toEqual({
+      test: '/foo/bar/',
+    });
+  });
+});
