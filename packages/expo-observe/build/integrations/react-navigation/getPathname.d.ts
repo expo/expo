f9f8b7b93a6efb101331e9bdefdb3d4e74cc3999
Jakub Tkacz <jtkacz@expo.io>
2026-05-26 12:49:11 +0200

    [expo-observe] remove direct imports from react-navigation

--- packages/expo-observe/build/integrations/react-navigation/getPathname.d.ts
+++ packages/expo-observe/build/integrations/react-navigation/getPathname.d.ts
@@ -1,4 +1,4 @@
-import { type LinkingOptions, type ParamListBase } from '@react-navigation/native';
+import type { LinkingOptions, ParamListBase } from '@react-navigation/native';
 import type { GetPathname } from './types';
 export declare function createGetPathname<ParamList extends ParamListBase>(linking: LinkingOptions<ParamList> | undefined): GetPathname;
 //# sourceMappingURL=getPathname.d.ts.map
