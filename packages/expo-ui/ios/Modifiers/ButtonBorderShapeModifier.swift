30b7cbb18124d04240b24f4c326cdaec7e1642cb
nishan (o^▽^o) <nishanbende@gmail.com>
2026-05-27 18:19:25 +0200

    add buttonbordershape

---
+++ packages/expo-ui/ios/Modifiers/ButtonBorderShapeModifier.swift
@@ -0,0 +1,38 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+import ExpoModulesCore
+import SwiftUI
+
+internal enum ButtonBorderShapeType: String, Enumerable {
+  case automatic
+  case capsule
+  case roundedRectangle
+  case circle
+}
+
+internal struct ButtonBorderShapeModifier: ViewModifier, Record {
+  @Field var shape: ButtonBorderShapeType = .automatic
+  @Field var cornerRadius: CGFloat?
+
+  @ViewBuilder
+  func body(content: Content) -> some View {
+    switch shape {
+    case .automatic:
+      content.buttonBorderShape(.automatic)
+    case .capsule:
+      content.buttonBorderShape(.capsule)
+    case .roundedRectangle:
+      if let cornerRadius {
+        content.buttonBorderShape(.roundedRectangle(radius: cornerRadius))
+      } else {
+        content.buttonBorderShape(.roundedRectangle)
+      }
+    case .circle:
+      if #available(iOS 17.0, macOS 14.0, tvOS 16.4, *) {
+        content.buttonBorderShape(.circle)
+      } else {
+        content.buttonBorderShape(.automatic)
+      }
+    }
+  }
+}
