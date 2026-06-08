a9d5220a74d30e99af0a2663d58157689427d7d5
Krystof Woldrich <31292499+krystofwoldrich@users.noreply.github.com>
2026-06-08 17:59:13 +0200

    test(metro-config): cover node_modules frame collapse

--- packages/@expo/metro-config/src/__tests__/customizeFrame.test.ts
+++ packages/@expo/metro-config/src/__tests__/customizeFrame.test.ts
@@ -39,6 +39,54 @@ describe(getDefaultCustomizeFrame, () => {
     });
   });
 
+  it('collapses node_modules frames with POSIX separators', () => {
+    const customizeFrame = getDefaultCustomizeFrame();
+
+    expect(
+      customizeFrame({
+        file: '/Users/app/node_modules/react/index.js',
+        lineNumber: 10,
+        column: 20,
+        methodName: 'render',
+      })
+    ).toEqual({
+      file: '/Users/app/node_modules/react/index.js',
+      lineNumber: 10,
+      column: 20,
+      methodName: 'render',
+      collapse: true,
+    });
+  });
+
+  it('collapses node_modules frames with Windows separators', () => {
+    jest.isolateModules(() => {
+      const path = jest.requireActual<typeof import('node:path')>('node:path');
+
+      jest.doMock('node:path', () => ({
+        __esModule: true,
+        default: { ...path, sep: '\\' },
+      }));
+
+      const { getDefaultCustomizeFrame } = require('../customizeFrame') as typeof import('../customizeFrame');
+      const customizeFrame = getDefaultCustomizeFrame();
+
+      expect(
+        customizeFrame({
+          file: 'C:\\Users\\app\\node_modules\\react\\index.js',
+          lineNumber: 10,
+          column: 20,
+          methodName: 'render',
+        })
+      ).toEqual({
+        file: 'C:\\Users\\app\\node_modules\\react\\index.js',
+        lineNumber: 10,
+        column: 20,
+        methodName: 'render',
+        collapse: true,
+      });
+    });
+  });
+
   it.each([
     'C:\\Users\\app\\src\\index.tsx',
     'D:/Users/app/src/index.tsx',
