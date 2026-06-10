4650e3fed66d130d5e8956ebb91f17bf95c59675
Phil Pluckthun <phil@kitten.sh>
2026-06-10 12:44:11 +0100

    Apply lints

--- packages/@expo/log-box/src/index.tsx
+++ packages/@expo/log-box/src/index.tsx
@@ -1,6 +1,7 @@
 import 'expo/types';
 
 import { createElement } from 'react';
+
 import { renderInShadowRoot } from './utils/renderInShadowRoot';
 
 if (process.env.NODE_ENV === 'development' && process.env.EXPO_OS === 'web') {
