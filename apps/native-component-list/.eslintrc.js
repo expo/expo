63ddb7d87be5460381a4dd062756df5f7b9d881e
Phil Pluckthun <phil@kitten.sh>
2026-06-19 08:23:35 +0100

    Add more manual eslint overrides

--- apps/native-component-list/.eslintrc.js
+++ apps/native-component-list/.eslintrc.js
@@ -3,12 +3,6 @@ module.exports = {
   extends: ['universe/native'],
   env: { browser: true },
   rules: {
-    // TODO(@kitten): Disable in universe in general; redundant with TypeScript
-    'import/default': 'off',
-    'import/export': 'off',
-    'import/named': 'off',
-    'import/namespace': 'off',
-    'import/no-duplicates': 'off',
   },
   overrides: [
     {
--- packages/expo-module-scripts/eslintrc.base.cjs
+++ packages/expo-module-scripts/eslintrc.base.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
   extends: ['universe/native'],
+  settings: {
+    react: {
+      version: '19'
+    },
+  },
   overrides: [
     {
       files: ['**/__tests__/*'],
@@ -33,7 +38,45 @@ module.exports = {
         'no-dupe-args': 'off',
         '@typescript-eslint/no-unused-expressions': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
+        'import/default': 'off',
+        'import/export': 'off',
+        'import/named': 'off',
+        'import/namespace': 'off',
+        'import/no-duplicates': 'off',
       }
+    },
+    {
+      // TODO(@kitten): Scope these to JSX files in universe instead
+      files: ['*.js', '*.ts', '*.d.ts'],
+      rules: {
+        'react/jsx-boolean-value': 'off',
+        'react/jsx-closing-bracket-location': 'off',
+        'react/jsx-curly-brace-presence': 'off',
+        'react/jsx-curly-spacing': 'off',
+        'react/jsx-equals-spacing': 'off',
+        'react/jsx-first-prop-new-line': 'off',
+        'react/jsx-fragments': 'off',
+        'react/jsx-indent': 'off',
+        'react/jsx-indent-props': 'off',
+        'react/jsx-no-bind': 'off',
+        'react/jsx-no-duplicate-props': 'off',
+        'react/jsx-no-undef': 'off',
+        'react/jsx-props-no-multi-spaces': 'off',
+        'react/jsx-tag-spacing': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/jsx-uses-vars': 'off',
+        'react/jsx-wrap-multilines': 'off',
+        'react/no-access-state-in-setstate': 'off',
+        'react/no-did-mount-set-state': 'off',
+        'react/no-did-update-set-state': 'off',
+        'react/no-direct-mutation-state': 'off',
+        'react/no-redundant-should-component-update': 'off',
+        'react/no-this-in-sfc': 'off',
+        'react/no-unknown-property': 'off',
+        'react/no-will-update-set-state': 'off',
+        'react/require-render-return': 'off',
+        'react/self-closing-comp': 'off',
+      },
     },
   ],
   rules: {
