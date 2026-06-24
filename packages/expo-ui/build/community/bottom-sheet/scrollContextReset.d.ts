6c8972e1717acd5d06fda7804c64ff457b930bff
nishan (o^▽^o) <nishanbende@gmail.com>
2026-06-24 11:32:28 +0530

    rebuild expoui

---
+++ packages/expo-ui/build/community/bottom-sheet/scrollContextReset.d.ts
@@ -0,0 +1,5 @@
+import type { ReactNode } from 'react';
+export declare function SheetScrollContextReset({ children }: {
+    children: ReactNode;
+}): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=scrollContextReset.d.ts.map
