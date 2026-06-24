e3fcbeb687a326dc34adb6a394950ad8e1d6a517
HubertBer <bernackihubert03@gmail.com>
2026-06-24 14:40:28 +0200

    add support for multiple modules generation in module-interface funciton

--- packages/expo-type-information/src/commands/moduleInterfaceCommand.ts
+++ packages/expo-type-information/src/commands/moduleInterfaceCommand.ts
@@ -1,7 +1,7 @@
-import commander from 'commander';
-import path from 'path';
+import commander from "commander";
+import path from "path";
 
-import { generateFullTsInterface } from '../typescriptGeneration';
+import { generateFullTsInterface, OutputFile } from "../typescriptGeneration";
 import {
   addCommonOptions,
   getFileTypeInformationFromArgs,
@@ -10,11 +10,11 @@ import {
   runCommandOnWatch,
   TypeInformationCommandCommonAllArguments,
   writeToStableFile,
-} from './commandUtils';
+} from "./commandUtils";
 
 export function moduleInterfaceCommand(cli: commander.Command) {
-  return addCommonOptions(cli.command('module-interface'))
-    .summary('generate a full TypeScript interface for a Swift module')
+  return addCommonOptions(cli.command("module-interface"))
+    .summary("generate a full TypeScript interface for a Swift module")
     .description(
       `Generates a full TypeScript interface for a Swift module. It consists of:
 
@@ -22,7 +22,7 @@ export function moduleInterfaceCommand(cli: commander.Command) {
 - **module.ts** with the native module definition
 - **view.tsx** for each view defined in the module
 - **index.ts** file which reexports some functions
-`
+`,
     )
     .action(async (options: TypeInformationCommandCommonAllArguments) => {
       const parsedArgs = await parseCommandArguments(options, false);
@@ -37,23 +37,49 @@ export function moduleInterfaceCommand(cli: commander.Command) {
         if (!typeInfo) {
           return;
         }
-        const generatedFiles = await generateFullTsInterface(typeInfo);
-        if (!generatedFiles) {
-          return;
-        }
-        const { moduleTypesFile, moduleViewsFiles, moduleNativeFile, indexFile } = generatedFiles;
-
-        const dirName = realOutputPath ?? path.dirname(realInputPaths[0] as string);
+        const moduleInterfaceFiles = await generateFullTsInterface(typeInfo);
+        const dirName =
+          realOutputPath ?? path.dirname(realInputPaths[0] as string);
         const writeFilePromises = [];
-        for (const outputFile of [
-          moduleTypesFile,
-          ...moduleViewsFiles,
-          moduleNativeFile,
-          indexFile,
-        ]) {
-          const outputFilePath = path.resolve(dirName, outputFile.name);
+        const finalIndexFile: OutputFile = {
+          content: "",
+          name: "index.ts",
+        };
+        for (const moduleGeneratedFiles of moduleInterfaceFiles.moduleInterfaces) {
+          const {
+            moduleTypesFile,
+            moduleViewsFiles,
+            moduleNativeFile,
+            indexFile,
+          } = moduleGeneratedFiles;
+
+          for (const outputFile of [
+            moduleTypesFile,
+            ...moduleViewsFiles,
+            moduleNativeFile,
+          ]) {
+            const outputFilePath = path.resolve(dirName, outputFile.name);
+            writeFilePromises.push(
+              writeToStableFile({
+                filePath: outputFilePath,
+                content: outputFile.content,
+              }),
+            );
+          }
+          finalIndexFile.content += indexFile.content;
+        }
+        const indexFilePath = path.resolve(dirName, finalIndexFile.name);
+        writeFilePromises.push(
+          writeToStableFile({
+            filePath: indexFilePath,
+            content: finalIndexFile.content,
+          }),
+        );
+        const commonInterface = moduleInterfaceFiles.commonTypesInterface;
+        if (commonInterface) {
+          const filePath = path.resolve(dirName, commonInterface.name);
           writeFilePromises.push(
-            writeToStableFile({ filePath: outputFilePath, content: outputFile.content })
+            writeToStableFile({ filePath, content: commonInterface.content }),
           );
         }
         await Promise.all(writeFilePromises);
