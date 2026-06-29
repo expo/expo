39e44c089cd1db15582b0cc8cdf5ec7a36911686
Aman Mittal <amandeepmittal@live.com>
2026-06-29 15:37:21 +0530

    Update AskPageAILazyMount.tsx

--- docs/ui/components/AskPageAI/AskPageAILazyMount.tsx
+++ docs/ui/components/AskPageAI/AskPageAILazyMount.tsx
@@ -1,18 +1,11 @@
 import { KapaProvider } from '@kapaai/react-sdk';
+import { type ComponentProps } from 'react';
 
 import { AskPageAIOverlay } from './AskPageAIOverlay';
 
 const KAPA_INTEGRATION_ID = '2063233f-1e70-45e8-b1b5-a872c9887afc';
 
-type AskPageAILazyMountProps = {
-  onClose: () => void;
-  onMinimize: () => void;
-  pageTitle?: string;
-  isExpoSdkPage?: boolean;
-  isVisible: boolean;
-  isExpanded: boolean;
-  onExpandedChange?: (expanded: boolean) => void;
-};
+type AskPageAILazyMountProps = ComponentProps<typeof AskPageAIOverlay>;
 
 export default function AskPageAILazyMount(props: AskPageAILazyMountProps) {
   return (
