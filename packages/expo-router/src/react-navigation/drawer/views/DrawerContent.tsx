af075d2d7753368e76bb46d157abfd8decf4d7f3
Jakub Tkacz <jtkacz@expo.io>
2026-07-14 16:36:34 +0200

    Align with main

--- packages/expo-router/src/react-navigation/drawer/views/DrawerContent.tsx
+++ packages/expo-router/src/react-navigation/drawer/views/DrawerContent.tsx
@@ -2,7 +2,7 @@ import type { DrawerContentComponentProps } from '../types';
 import { DrawerContentScrollView } from './DrawerContentScrollView';
 import { DrawerItemList } from './DrawerItemList';
 
-export function DrawerContent({ descriptors, state, ...actions }: DrawerContentComponentProps) {
+export function DrawerContent({ descriptors, state, ...rest }: DrawerContentComponentProps) {
   const focusedRoute = state.routes[state.index]!;
   const focusedDescriptor = descriptors[focusedRoute.key]!;
   const focusedOptions = focusedDescriptor.options;
@@ -11,9 +11,10 @@ export function DrawerContent({ descriptors, state, ...actions }: DrawerContentC
 
   return (
     <DrawerContentScrollView
+      {...rest}
       contentContainerStyle={drawerContentContainerStyle}
       style={drawerContentStyle}>
-      <DrawerItemList descriptors={descriptors} state={state} {...actions} />
+      <DrawerItemList descriptors={descriptors} state={state} {...rest} />
     </DrawerContentScrollView>
   );
 }
