cc89b79c0c14262e5d53b81fe603476812722f1b
Phil Pluckthun <phil@kitten.sh>
2026-07-21 00:17:44 +0100

    Add conect/disconnect events for command socket

--- packages/@expo/cli/src/start/server/metro/hmrEvents.ts
+++ packages/@expo/cli/src/start/server/metro/hmrEvents.ts
@@ -5,7 +5,6 @@ declare module '2g' {
   interface EventRegistry {
     'hmr:broadcast_failed': { socketId: string; error: SerializedError };
     'hmr:socket_not_found': { id: string };
-    'hmr:untrusted_broadcast_refused': { method: string };
     'hmr:message_parse_failed': { error: SerializedError };
     'hmr:protocol_version_mismatch': { expected: number; received: unknown };
     'hmr:events_unknown_message': { type: string };
