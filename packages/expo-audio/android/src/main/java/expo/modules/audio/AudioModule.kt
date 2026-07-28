expo/expo

commit a5e16be166397ace5f13e0f46eeb3cb176541895

[audio] Fix issues with `replace` method (#33708)

packages/expo-audio/android/src/main/java/expo/modules/audio/AudioModule.kt
@@ -260,17 +260,23 @@ class AudioModule : Module() {
         }
       }
 
-      Function("replace") { ref: AudioPlayer, source: AudioSource? ->
-        if (ref.player.availableCommands.contains(Player.COMMAND_CHANGE_MEDIA_ITEMS)) {
-          val mediaSource = createMediaItem(source)
-          mediaSource?.let {
-            ref.player.replaceMediaItem(0, it.mediaItem)
+      Function("replace") { ref: AudioPlayer, source: AudioSource ->
+        runOnMain {
+          if (ref.player.availableCommands.contains(Player.COMMAND_CHANGE_MEDIA_ITEMS)) {
+            val mediaSource = createMediaItem(source)
+            val wasPlaying = ref.player.isPlaying
+            mediaSource?.let {
+              ref.player.replaceMediaItem(0, it.mediaItem)
+              if (wasPlaying) {
+                ref.player.play()
+              }
+            }
           }
         }
       }
 
       Function("setAudioSamplingEnabled") { ref: AudioPlayer, enabled: Boolean ->
-        appContext.mainQueue.launch {
+        runOnMain {
           ref.setSamplingEnabled(enabled)
         }
       }

packages/expo-audio/android/src/main/java/expo/modules/audio/AudioPlayer.kt
@@ -8,7 +8,9 @@ import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
+import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import androidx.media3.common.Player.MEDIA_ITEM_TRANSITION_REASON_PLAYLIST_CHANGED
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.MediaSource
@@ -85,6 +87,14 @@ class AudioPlayer(
       override fun onPlaybackStateChanged(playbackState: Int) {
         playerScope.launch {
           sendPlayerUpdate(mapOf("status" to playbackStateToString(playbackState)))
+        }
+      }
+
+      override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+        if (reason == MEDIA_ITEM_TRANSITION_REASON_PLAYLIST_CHANGED) {
+          playerScope.launch {
+            sendPlayerUpdate()
+          }
         }
       }
     })

packages/expo-audio/ios/AudioModule.swift
@@ -151,7 +151,7 @@ public class AudioModule: Module {
       }
 
       Function("replace") { (player, source: AudioSource) in
-        player.ref.replaceCurrentItem(with: AudioUtils.createAVPlayerItem(from: source))
+        player.replaceCurrentSource(source: source)
       }
 
       Function("pause") { player in

packages/expo-audio/ios/AudioPlayer.swift
@@ -25,6 +25,7 @@ public class AudioPlayer: SharedRef<AVPlayer> {
   private var audioProcessor: AudioTapProcessor?
   private var samplingEnabled = false
   private var tapInstalled = false
+  private var shouldInstallAudioTap = false
 
   var duration: Double {
     ref.currentItem?.duration.seconds ?? 0.0
@@ -97,17 +98,40 @@ public class AudioPlayer: SharedRef<AVPlayer> {
 
   private func setupPublisher() {
     ref.publisher(for: \.currentItem?.status)
-      .sink { status in
-        guard let status else {
+      .sink { [weak self] status in
+        guard let self, let status else {
           return
         }
         if status == .readyToPlay {
           self.updateStatus(with: [
             "isLoaded": true
           ])
+          // We can't add the audio tap until the asset has loaded, otherwise the asset track will be empty.
+          // This is particularly important after replacing the audio source
+          if shouldInstallAudioTap {
+            setSamplingEnabled(enabled: shouldInstallAudioTap)
+            shouldInstallAudioTap = false
+          }
         }
       }
       .store(in: &cancellables)
+  }
+
+  func replaceCurrentSource(source: AudioSource) {
+    let wasPlaying = ref.timeControlStatus == .playing
+    let wasSamplingEnabled = samplingEnabled
+    ref.pause()
+
+    // Remove the audio tap if it is active
+    if samplingEnabled {
+      setSamplingEnabled(enabled: false)
+    }
+    ref.replaceCurrentItem(with: AudioUtils.createAVPlayerItem(from: source))
+    shouldInstallAudioTap = wasSamplingEnabled
+
+    if wasPlaying {
+      ref.play()
+    }
   }
 
   private func playerIsBuffering() -> Bool {
@@ -126,8 +150,8 @@ public class AudioPlayer: SharedRef<AVPlayer> {
   }
 
   private func installTap() {
-    if let item = ref.currentItem, !tapInstalled {
-      audioProcessor = AudioTapProcessor(playerItem: item)
+    if !tapInstalled {
+      audioProcessor = AudioTapProcessor(player: ref)
       tapInstalled = audioProcessor?.installTap() ?? false
       audioProcessor?.sampleBufferCallback = { [weak self] buffer, frameCount, timestamp in
         guard let self,

apps/native-component-list/src/screens/Audio/expo-audio/AudioPlayer.tsx
@@ -1,4 +1,5 @@
 import { useAudioPlayer, AudioSource, useAudioPlayerStatus } from 'expo-audio';
+import React from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 
 import { JsiAudioBar } from './JsiAudioBar';
@@ -9,7 +10,12 @@ type AudioPlayerProps = {
   style?: StyleProp<ViewStyle>;
 };
 
+const localSource = require('../../../../assets/sounds/polonez.mp3');
+const remoteSource =
+  'https://p.scdn.co/mp3-preview/f7a8ab9c5768009b65a30e9162555e8f21046f46?cid=162b7dc01f3a4a2ca32ed3cec83d1e02';
+
 export default function AudioPlayer({ source, style }: AudioPlayerProps) {
+  const [currentSource, setCurrentSource] = React.useState(source);
   const player = useAudioPlayer(source);
   const status = useAudioPlayerStatus(player);
   const setVolume = (volume: number) => {
@@ -29,6 +35,12 @@ export default function AudioPlayer({ source, style }: AudioPlayerProps) {
     player.setPlaybackRate(rate);
   };
 
+  const replaceSource = () => {
+    const source = currentSource === localSource ? remoteSource : localSource;
+    player.replace(source);
+    setCurrentSource(source);
+  };
+
   return (
     <Player
       {...status}
@@ -37,6 +49,7 @@ export default function AudioPlayer({ source, style }: AudioPlayerProps) {
       style={style}
       play={() => player.play()}
       pause={() => player.pause()}
+      replace={() => replaceSource()}
       replay={() => {
         return player.seekTo(0);
       }}

apps/native-component-list/src/screens/Audio/expo-audio/Player.tsx
@@ -37,6 +37,7 @@ interface Props {
   pause: () => void;
   replay: () => void;
   next?: () => void;
+  replace?: () => void;
   setRate: (rate: number, shouldCorrectPitch: boolean) => void;
   setIsMuted: (isMuted: boolean) => void;
   setPosition: (position: number) => Promise<any>;
@@ -117,6 +118,26 @@ export default function Player(props: Props) {
       );
     }
     return null;
+  };
+
+  const _renderReplaceButton = () => {
+    return (
+      <View>
+        <TouchableOpacity style={styles.button} onPress={props.replace}>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontWeight: 'bold',
+              marginLeft: 8,
+              fontSize: 12,
+              height: 36,
+              color: Colors.tintColor,
+            }}>
+            Replace Source
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
   };
 
   const _renderAuxiliaryButton = ({
@@ -253,6 +274,7 @@ export default function Player(props: Props) {
             active: false,
           })}
       </View>
+      {_renderReplaceButton()}
       {_maybeRenderErrorOverlay()}
     </View>
   );

packages/expo-audio/build/ExpoAudio.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"ExpoAudio.d.ts","sourceRoot":"","sources":["../src/ExpoAudio.ts"],"names":[],"mappings":"AACA,OAAO,EAAE,kBAAkB,EAA4B,MAAM,mBAAmB,CAAC;AAGjF,OAAO,EACL,SAAS,EACT,WAAW,EACX,WAAW,EACX,aAAa,EACb,gBAAgB,EAChB,eAAe,EAChB,MAAM,eAAe,CAAC;AACvB,OAAO,WAAW,MAAM,eAAe,CAAC;AACxC,OAAO,EAAE,WAAW,EAAE,aAAa,EAAE,WAAW,EAAE,MAAM,qBAAqB,CAAC;AAI9E,eAAO,MAAM,sBAAsB,yBAAyB,CAAC;AAC7D,eAAO,MAAM,mBAAmB,sBAAsB,CAAC;AACvD,eAAO,MAAM,uBAAuB,0BAA0B,CAAC;AAG/D,wBAAgB,cAAc,CAC5B,MAAM,GAAE,WAAkB,EAC1B,cAAc,GAAE,MAAY,GAC3B,WAAW,CAMb;AAGD,wBAAgB,oBAAoB,CAAC,MAAM,EAAE,WAAW,GAAG,WAAW,CAGrE;AAGD,wBAAgB,sBAAsB,CAAC,MAAM,EAAE,WAAW,EAAE,QAAQ,EAAE,CAAC,IAAI,EAAE,WAAW,KAAK,IAAI,QAWhG;AAGD,wBAAgB,gBAAgB,CAC9B,OAAO,EAAE,gBAAgB,EACzB,cAAc,CAAC,EAAE,CAAC,MAAM,EAAE,eAAe,KAAK,IAAI,GACjD,aAAa,CAcf;AAGD,wBAAgB,qBAAqB,CAAC,QAAQ,EAAE,aAAa,EAAE,QAAQ,GAAE,MAAY,iBAYpF;AAED;;;;;;;GAOG;AACH,wBAAgB,iBAAiB,CAC/B,MAAM,GAAE,WAAW,GAAG,MAAM,GAAG,MAAM,GAAG,IAAW,EACnD,cAAc,GAAE,MAAY,GAC3B,WAAW,CAGb;AAGD,wBAAsB,qBAAqB,CAAC,MAAM,EAAE,OAAO,GAAG,OAAO,CAAC,IAAI,CAAC,CAE1E;AAGD,wBAAsB,iBAAiB,CAAC,IAAI,EAAE,OAAO,CAAC,SAAS,CAAC,GAAG,OAAO,CAAC,IAAI,CAAC,CAE/E;AAGD,wBAAsB,gCAAgC,IAAI,OAAO,CAAC,kBAAkB,CAAC,CAEpF;AAGD,wBAAsB,4BAA4B,IAAI,OAAO,CAAC,kBAAkB,CAAC,CAEhF;AAED,OAAO,EAAE,WAAW,EAAE,CAAC"}
+{"version":3,"file":"ExpoAudio.d.ts","sourceRoot":"","sources":["../src/ExpoAudio.ts"],"names":[],"mappings":"AACA,OAAO,EAAE,kBAAkB,EAA4B,MAAM,mBAAmB,CAAC;AAGjF,OAAO,EACL,SAAS,EACT,WAAW,EACX,WAAW,EACX,aAAa,EACb,gBAAgB,EAChB,eAAe,EAChB,MAAM,eAAe,CAAC;AACvB,OAAO,WAAW,MAAM,eAAe,CAAC;AACxC,OAAO,EAAE,WAAW,EAAE,aAAa,EAAE,WAAW,EAAE,MAAM,qBAAqB,CAAC;AAI9E,eAAO,MAAM,sBAAsB,yBAAyB,CAAC;AAC7D,eAAO,MAAM,mBAAmB,sBAAsB,CAAC;AACvD,eAAO,MAAM,uBAAuB,0BAA0B,CAAC;AAS/D,wBAAgB,cAAc,CAC5B,MAAM,GAAE,WAAkB,EAC1B,cAAc,GAAE,MAAY,GAC3B,WAAW,CAMb;AAGD,wBAAgB,oBAAoB,CAAC,MAAM,EAAE,WAAW,GAAG,WAAW,CAGrE;AAGD,wBAAgB,sBAAsB,CAAC,MAAM,EAAE,WAAW,EAAE,QAAQ,EAAE,CAAC,IAAI,EAAE,WAAW,KAAK,IAAI,QAWhG;AAGD,wBAAgB,gBAAgB,CAC9B,OAAO,EAAE,gBAAgB,EACzB,cAAc,CAAC,EAAE,CAAC,MAAM,EAAE,eAAe,KAAK,IAAI,GACjD,aAAa,CAcf;AAGD,wBAAgB,qBAAqB,CAAC,QAAQ,EAAE,aAAa,EAAE,QAAQ,GAAE,MAAY,iBAYpF;AAED;;;;;;;GAOG;AACH,wBAAgB,iBAAiB,CAC/B,MAAM,GAAE,WAAW,GAAG,MAAM,GAAG,MAAM,GAAG,IAAW,EACnD,cAAc,GAAE,MAAY,GAC3B,WAAW,CAGb;AAGD,wBAAsB,qBAAqB,CAAC,MAAM,EAAE,OAAO,GAAG,OAAO,CAAC,IAAI,CAAC,CAE1E;AAGD,wBAAsB,iBAAiB,CAAC,IAAI,EAAE,OAAO,CAAC,SAAS,CAAC,GAAG,OAAO,CAAC,IAAI,CAAC,CAE/E;AAGD,wBAAsB,gCAAgC,IAAI,OAAO,CAAC,kBAAkB,CAAC,CAEpF;AAGD,wBAAsB,4BAA4B,IAAI,OAAO,CAAC,kBAAkB,CAAC,CAEhF;AAED,OAAO,EAAE,WAAW,EAAE,CAAC"}

packages/expo-audio/build/ExpoAudio.js
@@ -7,6 +7,11 @@ import { resolveSource } from './utils/resolveSource';
 export const PLAYBACK_STATUS_UPDATE = 'playbackStatusUpdate';
 export const AUDIO_SAMPLE_UPDATE = 'audioSampleUpdate';
 export const RECORDING_STATUS_UPDATE = 'recordingStatusUpdate';
+// TODO: Temporary solution until we develop a way of overriding prototypes that won't break the lazy loading of the module.
+const replace = AudioModule.AudioPlayer.prototype.replace;
+AudioModule.AudioPlayer.prototype.replace = function (source) {
+    return replace.call(this, resolveSource(source));
+};
 // @docsMissing
 export function useAudioPlayer(source = null, updateInterval = 500) {
     const parsedSource = resolveSource(source);
@@ -19,11 +24,11 @@ export function useAudioPlayerStatus(player) {
 }
 // @docsMissing
 export function useAudioSampleListener(player, listener) {
-    player.setAudioSamplingEnabled(true);
     useEffect(() => {
         if (!player.isAudioSamplingSupported) {
             return;
         }
+        player.setAudioSamplingEnabled(true);
         const subscription = player.addListener(AUDIO_SAMPLE_UPDATE, listener);
         return () => {
             subscription.remove();

commit d052a9fa7901a2785fc2863347bb3364b7bad992

[expo-audio] Fix `player.replace(null)` throwing due to a mismatch between native and TypeScript types (#48219)
# Why

Fixes #48093.

`player.replace(null)` throws on both platforms, because the native
`replace` functions declared `source` as non-optional even though
`AudioSource` includes `null`. Regression from #33708, which reverted
the nullable support #33854 added.

# How

- iOS: `Function("replace")` and `AudioPlayer.replaceCurrentSource` now
take `AudioSource?`. `AudioUtils.createAVPlayerItem(from:)` already
handles `nil`, returning `nil` to clear the item.
- Android: `Function("replace")` now takes `AudioSource?`. On `null`, it
calls a new `AudioPlayer.clearMediaSource()` (pause +
`ExoPlayer.clearMediaItems()`), matching `AudioPlaylist.clear()`.

# Test Plan

- Run `et check-packages expo-audio`: lint and build pass.
- Verify `player.replace(null)` on both platforms using
https://github.com/felipe-software/expo-audio-replace-null-repro.

# Checklist

<!--
Please check the appropriate items below if they apply to your diff.
-->

- [x] I added a `changelog.md` entry and rebuilt the package sources
according to [this short
guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
- [ ] This diff will work correctly for `npx expo prebuild` & EAS Build
(eg: updated a module plugin).
- [ ] Conforms with the [Documentation Writing Style
Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)

---------

Co-authored-by: Expo Bot <34669131+expo-bot@users.noreply.github.com>

packages/expo-audio/android/src/main/java/expo/modules/audio/AudioModule.kt
@@ -509,9 +509,13 @@ class AudioModule : Module() {
         }
       }
 
-      Function("replace") { player: AudioPlayer, source: AudioSource ->
+      Function("replace") { player: AudioPlayer, source: AudioSource? ->
         runOnMain {
           if (player.ref.availableCommands.contains(Player.COMMAND_CHANGE_MEDIA_ITEMS)) {
+            if (source == null) {
+              player.clearMediaSource()
+              return@runOnMain
+            }
             val mediaSource = createMediaItem(source)
             val wasPlaying = player.ref.isPlaying
             mediaSource?.let {

packages/expo-audio/android/src/main/java/expo/modules/audio/AudioPlayer.kt
@@ -98,6 +98,12 @@ class AudioPlayer(
     startUpdating()
   }
 
+  fun clearMediaSource() {
+    previousPlaybackState = Player.STATE_IDLE
+    ref.pause()
+    ref.clearMediaItems()
+  }
+
   override fun onPlaybackStateUpdated(playbackState: Int, justFinished: Boolean) {
     val updateMap = mutableMapOf<String, Any?>(
       "playbackState" to playbackStateToString(playbackState)

packages/expo-audio/ios/AudioModule.swift
@@ -231,8 +231,8 @@ public class AudioModule: Module {
         }
       }
 
-      Function("replace") { (player, source: AudioSource) in
-        if let uri = source.uri?.absoluteString, let cachedPlayer = self.registry.removePreloadedPlayer(forKey: uri) {
+      Function("replace") { (player, source: AudioSource?) in
+        if let uri = source?.uri?.absoluteString, let cachedPlayer = self.registry.removePreloadedPlayer(forKey: uri) {
           let cachedItem = cachedPlayer.currentItem
           cachedPlayer.replaceCurrentItem(with: nil)
           player.replaceWithPreloadedItem(cachedItem)

packages/expo-audio/ios/AudioPlayer.swift
@@ -266,7 +266,7 @@ public class AudioPlayer: SharedRef<AVPlayer>, Playable, LockScreenPlayable {
     }
   }
 
-  func replaceCurrentSource(source: AudioSource) {
+  func replaceCurrentSource(source: AudioSource?) {
     self.source = source
     let wasPlaying = ref.timeControlStatus == .playing
     let wasSamplingEnabled = samplingEnabled