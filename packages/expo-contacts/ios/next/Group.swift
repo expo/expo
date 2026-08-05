33ec6a7d4a9067a13cb77ec3838f7a2faf840e66
Wiktor Smaga <wiktor.smaga13@gmail.com>
2026-08-05 14:01:08 +0200

    refactor

--- packages/expo-contacts/ios/next/Group.swift
+++ packages/expo-contacts/ios/next/Group.swift
@@ -41,7 +41,7 @@ class Group: SharedObject {
       sortOrder: queryOptions?.sortOrder.map {
         CNContactSortOrderMapper.map($0)
       },
-      unifyResults: !(queryOptions?.rawContacts ?? false)
+      unifyResults: queryOptions?.rawContacts != true
     )
     .map { contactFactory.create(id: $0.identifier) }
   }
