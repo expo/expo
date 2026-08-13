d37ae4f576444fa5e6db88e31c2b533d40209792
Vojtech Novak <vonovak@gmail.com>
2026-08-13 13:17:45 +0200

    chore: migrate from @expo/vector-icons

--- apps/native-component-list/src/utilities/loadAssetsAsync.ts
+++ apps/native-component-list/src/utilities/loadAssetsAsync.ts
@@ -4,8 +4,6 @@ import {
   Inter_800ExtraBold,
   Inter_900Black,
 } from '@expo-google-fonts/inter';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import { Assets as StackAssets } from 'expo-router/react-navigation';
@@ -15,8 +13,6 @@ async function loadAssetsAsync() {
   const assetPromises: Promise<any>[] = [
     // The assets are `require` results, so they are numbers at runtime.
     Asset.loadAsync(StackAssets as number[]),
-    Font.loadAsync(Ionicons.font),
-    Font.loadAsync(MaterialIcons.font),
     Font.loadAsync({
       'space-mono': require('../../assets/fonts/SpaceMono-Regular.ttf'),
     }),
