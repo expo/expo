cca8730080aa1f9142d8ea75c030aec02a7e775c
Gabe Debes <pacheco@expo.dev>
2026-08-14 15:01:14 -0700

    [docs] Remove Vexo guide and references

--- docs/constants/navigation.js
+++ docs/constants/navigation.js
@@ -377,7 +377,6 @@ export const general = [
       makePage('guides/using-sentry.mdx'),
       makePage('guides/using-bugsnag.mdx'),
       makePage('guides/using-logrocket.mdx'),
-      makePage('guides/using-vexo.mdx'),
       makeGroup(
         'Using PostHog',
         [makePage('guides/using-posthog/index.mdx'), makePage('guides/using-posthog/recipes.mdx')],
