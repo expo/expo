9d04521f7620e92099cab1c0aab239d0b6bdf74e
Phil Pluckthun <phil@kitten.sh>
2026-08-17 10:30:42 +0100

    metro-config: Add cache vary metadata to noxcturnal transforms

--- packages/@expo/metro-config/src/transform-worker/metro-transform-worker.ts
+++ packages/@expo/metro-config/src/transform-worker/metro-transform-worker.ts
@@ -786,6 +786,7 @@ async function completeFullNoxcturnalTransform(
   context: TransformationContext,
   fullNoxcturnal: Extract<NoxcturnalMetroTransformAttempt, { status: 'complete' }>
 ): Promise<TransformResponse> {
+  const cacheVary = fullNoxcturnal.result.metadata.cacheVary as readonly CacheVaryDim[] | undefined;
   if (String(context.options.customTransformOptions?.optimize) === 'true') {
     return transformJS(
       {
@@ -817,6 +818,7 @@ async function completeFullNoxcturnalTransform(
             ? fullNoxcturnal.result.metadata.loaderReference
             : file.loaderReference,
         functionMap: fullNoxcturnal.result.functionMap ?? file.functionMap,
+        cacheVary,
       },
       context
     );
@@ -881,6 +883,7 @@ async function completeFullNoxcturnalTransform(
             typeof fullNoxcturnal.result.metadata.loaderReference === 'string'
               ? fullNoxcturnal.result.metadata.loaderReference
               : file.loaderReference,
+          expoCacheVary: await embedCurrentFingerprints(cacheVary),
         },
       },
     ],
