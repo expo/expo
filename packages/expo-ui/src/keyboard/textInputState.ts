c2d80175aad12152a14735f052ed548434dc3776
Aman Mittal <amandeepmittal@live.com>
2026-08-24 22:45:22 +0530

    [ui] Fix TextInputState declaration not reaching dependent packages

--- packages/expo-ui/src/keyboard/textInputState.ts
+++ packages/expo-ui/src/keyboard/textInputState.ts
@@ -1,1 +1,3 @@
+/// <reference path="../ts-declarations/react-native-text-input-state.d.ts" />
+
 export { default as TextInputState } from 'react-native/Libraries/Components/TextInput/TextInputState';
