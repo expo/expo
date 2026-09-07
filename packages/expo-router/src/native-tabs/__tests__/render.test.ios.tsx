b521fb68179c6b9ebe4fb83c6371ca12ba08a2a9
Jakub Tkacz <jtkacz@expo.io>
2026-09-07 19:07:18 +0200

    [router] oder tabs by .Trigger order for all renders

--- packages/expo-router/src/native-tabs/__tests__/render.test.ios.tsx
+++ packages/expo-router/src/native-tabs/__tests__/render.test.ios.tsx
@@ -245,8 +245,8 @@ describe('First focused tab', () => {
     expect(screen.getByTestId('index')).toBeVisible();
     expect(screen.getByTestId('second')).toBeVisible();
     expect(TabsScreen).toHaveBeenCalledTimes(4);
-    expect(TabsScreen.mock.calls[0][0].screenKey).toBe('index');
-    expect(TabsScreen.mock.calls[1][0].screenKey).toBe('second');
+    expect(TabsScreen.mock.calls[0][0].screenKey).toBe('second');
+    expect(TabsScreen.mock.calls[1][0].screenKey).toBe('index');
     expect(TabsHost).toHaveBeenCalledTimes(2);
     expect(TabsHost.mock.calls[0][0].navStateRequest.selectedScreenKey).toBe('index');
   });
