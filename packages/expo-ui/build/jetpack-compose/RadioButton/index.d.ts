f289036a50a35f8b3a590bca74054ca99b68bf6c
Felipe Castor <89278308+felipe-software@users.noreply.github.com>
2026-09-08 05:32:49 -0300

    [ui][android] Expose enabled and colors on RadioButton (#49852)
    Because currently the `RadioButton` component cannot be disabled or have
    it's colors customized
    <!--
    Please describe the motivation for this PR, and link to relevant GitHub
    issues, forums posts, or feature requests.
    -->
    
    Added `enabled` and `colors` props, just like in the Checkbox, Slider,
    Togglebutton and many other existing components
    <!--
    How did you build this feature or fix this bug and why?
    -->
    
    Tested the new props with unit tests and the Android demo.
    <!--
    Please describe how you tested this change and how a reviewer could
    reproduce your test, especially if this PR does not include automated
    tests! If possible, please also provide terminal output and/or
    screenshots demonstrating your test/reproduction.
    -->
    
    <!--
    Please check the appropriate items below if they apply to your diff.
    -->
    
    - [x] I added a `changelog.md` entry and rebuilt the package sources
    according to [this short
    guide](https://github.com/expo/expo/blob/main/CONTRIBUTING.md#-before-submitting)
    - [x] This diff will work correctly for `npx expo prebuild` & EAS Build
    (eg: updated a module plugin).
    - [x] Conforms with the [Documentation Writing Style
    Guide](https://github.com/expo/expo/blob/main/guides/Expo%20Documentation%20Writing%20Style%20Guide.md)
    
    (cherry picked from commit 9cf39def4b6da729158ae3b92ef5c034e62515eb)

--- packages/expo-ui/build/jetpack-compose/RadioButton/index.d.ts
+++ packages/expo-ui/build/jetpack-compose/RadioButton/index.d.ts
@@ -1,13 +1,32 @@
+import type { ColorValue } from 'react-native';
 import { type ModifierConfig } from '../../types';
+/**
+ * Colors for the radio button in different states.
+ */
+export type RadioButtonColors = {
+    selectedColor?: ColorValue;
+    unselectedColor?: ColorValue;
+    disabledSelectedColor?: ColorValue;
+    disabledUnselectedColor?: ColorValue;
+};
 export interface RadioButtonProps {
     /**
      * Whether the radio button is selected.
      */
     selected: boolean;
     /**
+     * Whether the radio button is enabled.
+     * @default true
+     */
+    enabled?: boolean;
+    /**
      * Callback that is called when the radio button is clicked.
      */
     onClick?: () => void;
+    /**
+     * Colors for the radio button in different states.
+     */
+    colors?: RadioButtonColors;
     /**
      * Modifiers for the component.
      */
