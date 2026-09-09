14ab3fe0ee35493a9ef92773879c58d64cbf3d37
Wiktor Smaga <wiktor.smaga13@gmail.com>
2026-09-09 09:55:07 +0200

    [contacts][iOS] Fix mapping between native and JS labels (#49874)
    fixes: https://github.com/expo/expo/issues/49569
    
    On iOS, if a value (e.g. phone number) had a system-defined label, its
    plain string was returned to the JS side (e.g. `_$!<HomeLabel>!$_`). It
    should return the parsed value instead.
    
    Added `ContactLabelMapper`, which contains every `CNLabel` defined in
    the Contacts Framework and maps them as follows:
    * native: `CNLabelHome` -> JS: `home`
    * JS: `home`/`Home`/`HOME` -> native: `CNLabelHome` (case-insensitive,
    same as Android)
    * JS: `Beach House` -> native: `Beach House`
    
    Also handled empty values by mapping them to `other`:
    * `""` -> `CNLabelOther`
    * `CNLabelOther` -> `other`
    
    Made labels in records nullable to match TS type.
    
    ✅ Added iOS Unit Tests
    
    ---------
    
    (cherry picked from commit 6f26a36d8be9fcf335af0a1febb53bc1eefebb21)

--- packages/expo-contacts/ios/next/mappers/fields/PhoneMapper.swift
+++ packages/expo-contacts/ios/next/mappers/fields/PhoneMapper.swift
@@ -8,26 +8,26 @@ struct PhoneMapper: ContactRecordMapper {
 
   func newRecordToCNLabeledValue(_ record: NewPhoneRecord) -> CNLabeledValue<CNPhoneNumber> {
     let phoneNumber = CNPhoneNumber(stringValue: record.number ?? "")
-    return CNLabeledValue(label: record.label, value: phoneNumber)
+    return CNLabeledValue(label: ContactLabelMapper.toCNLabel(record.label), value: phoneNumber)
   }
 
   func existingRecordToCNLabeledValue(_ record: ExistingPhoneRecord) -> CNLabeledValue<CNPhoneNumber> {
     let phoneNumber = CNPhoneNumber(stringValue: record.number ?? "")
-    return CNLabeledValue(label: record.label, value: phoneNumber)
+    return CNLabeledValue(label: ContactLabelMapper.toCNLabel(record.label), value: phoneNumber)
   }
 
   func cnLabeledValueToExistingRecord(_ labeledValue: CNLabeledValue<CNPhoneNumber>) -> ExistingPhoneRecord {
     return ExistingPhoneRecord(
       id: labeledValue.identifier,
       number: labeledValue.value.stringValue,
-      label: labeledValue.label
+      label: ContactLabelMapper.toRecordLabel(labeledValue.label)
     )
   }
 
   func apply(patch: PatchPhoneRecord, to cnLabeledValue: CNLabeledValue<CNPhoneNumber>) -> CNLabeledValue<CNPhoneNumber> {
     var toModify = cnLabeledValue
     if case .value(let label) = patch.label {
-      toModify = toModify.settingLabel(label)
+      toModify = toModify.settingLabel(ContactLabelMapper.toCNLabel(label))
     }
     if case .value(let number) = patch.number {
       let newValue = CNPhoneNumber(stringValue: number ?? "")
