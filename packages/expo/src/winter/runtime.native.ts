33bcb5ef076d637bf9325516c04da3f41ae51216
Gabriel Donadel <donadeldev@gmail.com>
2026-09-10 18:21:48 -0300

    [expo] Use react-native/setup-env instead of InitializeCore

--- packages/expo/src/winter/runtime.native.ts
+++ packages/expo/src/winter/runtime.native.ts
@@ -3,7 +3,7 @@
 
 // WARN(@kitten): We must ensure that the core react-native globals are initialized before ours
 // Otherwise we're relying on `getModulesRunBeforeMainModule` which is unstable or can be missing
-import 'react-native/Libraries/Core/InitializeCore';
+import 'react-native/setup-env';
 import '../../types';
 import { installAbortSignalPatch } from './AbortSignal';
 import { installFormDataPatch } from './FormData';
