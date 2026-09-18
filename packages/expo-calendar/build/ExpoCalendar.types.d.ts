f5e857d760b2d666941152aaaf94071ea2e99f7a
Alan Hughes <alan@expo.io>
2026-09-18 12:40:54 +0100

    build calendar js

--- packages/expo-calendar/build/ExpoCalendar.types.d.ts
+++ packages/expo-calendar/build/ExpoCalendar.types.d.ts
@@ -156,6 +156,10 @@ export declare class ExpoCalendar {
     delete(): Promise<void>;
     /**
      * Gets a calendar by its ID. Throws an error if the calendar with the given ID does not exist.
+     *
+     * > **iOS 17+:** With write-only calendar access, only the virtual calendar's ID can be
+     * > resolved - real calendars require full calendar access.
+     *
      * @param calendarId The ID of the calendar to get.
      * @returns An [`ExpoCalendar`](#expocalendar) object representing the calendar.
      */
