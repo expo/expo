88e8a795a2df3fa1d9e7afc569cd40f9eff12f72
ramen2020 <vfvsfv.dlk22@gmail.com>
2026-09-24 21:37:32 +0900

    [router] Keep the server-side Stack.Toolbar re-export as is
    The API docs are generated from StackClient.tsx, so the Object.assign in
    StackToolbar.tsx was not needed and duplicated the sub-component members
    in the resulting type.

--- packages/expo-router/src/layouts/stack-utils/toolbar/StackToolbar.tsx
+++ packages/expo-router/src/layouts/stack-utils/toolbar/StackToolbar.tsx
@@ -1,21 +1,19 @@
 import { StackToolbarButton } from './StackToolbarButton';
-import StackToolbarClient from './StackToolbarClient';
+import StackToolbar from './StackToolbarClient';
 import { StackToolbarMenu, StackToolbarMenuAction } from './StackToolbarMenu';
 import { StackToolbarSearchBarSlot } from './StackToolbarSearchBarSlot';
 import { StackToolbarSpacer } from './StackToolbarSpacer';
 import { StackToolbarView } from './StackToolbarView';
 import { StackToolbarBadge, StackToolbarIcon, StackToolbarLabel } from './toolbar-primitives';
 
-// Re-attach the sub-components on the server-side reference of the client component.
-// `Object.assign` (like `Stack.Screen`) keeps them resolvable by the API docs generator.
-export const StackToolbar = Object.assign(StackToolbarClient, {
-  Button: StackToolbarButton,
-  Menu: StackToolbarMenu,
-  MenuAction: StackToolbarMenuAction,
-  SearchBarSlot: StackToolbarSearchBarSlot,
-  Spacer: StackToolbarSpacer,
-  View: StackToolbarView,
-  Label: StackToolbarLabel,
-  Icon: StackToolbarIcon,
-  Badge: StackToolbarBadge,
-});
+StackToolbar.Button = StackToolbarButton;
+StackToolbar.Menu = StackToolbarMenu;
+StackToolbar.MenuAction = StackToolbarMenuAction;
+StackToolbar.SearchBarSlot = StackToolbarSearchBarSlot;
+StackToolbar.Spacer = StackToolbarSpacer;
+StackToolbar.View = StackToolbarView;
+StackToolbar.Label = StackToolbarLabel;
+StackToolbar.Icon = StackToolbarIcon;
+StackToolbar.Badge = StackToolbarBadge;
+
+export { StackToolbar };
